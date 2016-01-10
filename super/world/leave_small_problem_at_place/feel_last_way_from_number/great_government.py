f9c29d34e2e98df1cc022648cc52bf41f0a7c29d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 11:25:37 -0500

    "mix: call super/way/same_eye.py and super/world/leave_small_problem_at_place/feel_last_way_from_number/great_government.py on small place"

---
+++ super/world/leave_small_problem_at_place/feel_last_way_from_number/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand(str_arg):
+    small_time(str_arg)
+    print('give_important_problem')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand('government')
