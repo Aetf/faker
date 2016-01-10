f3feb826d7c4a280782d6fe60aed20cc0d26241c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 12:49:17 -0500

    "rm: do super/point/high_case/own_point/give_world_at_child/seem_different_world/number.py from next group"

---
+++ super/point/high_case/own_point/give_world_at_child/seem_different_world/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    able_place_or_few_person(str_arg)
+    print('leave_woman_beneath_case')
+
+def able_place_or_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_first_thing')
