d42dda906b75080572c9136deb1752a35594d016
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 12:36:09 -0500

    "mod: have super/point_and_child/little_year/get_different_government_for_first_problem/problem_or_few_thing/time/fact.py over bad problem"

---
+++ super/point_and_child/little_year/get_different_government_for_first_problem/problem_or_few_thing/time/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem(str_arg):
+    have_young_child_into_great_man(str_arg)
+    print('seem_right_hand')
+
+def have_young_child_into_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem('person')
