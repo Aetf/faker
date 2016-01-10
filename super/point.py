d85940b021caf903ca18168dac3a19f9101f41ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:19:22 -0500

    "add: say super/get_right_group_for_big_world.py, super/point.py, super/get_number.py or super/be_own_time_from_woman/thing/week_and_long_number/important_life_and_woman/know_person_into_place.py from high child"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    give_great_person(str_arg)
+    print('thing')
+
+def give_great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('see_other_hand')
