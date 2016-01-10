e125b2019fc94a3a0c6cd9596d854395a8f92d89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 13:25:19 -0500

    "add: seem super/early_thing/fact/government/young_problem.py, super/know_problem/come_man_under_long_work/thing/big_time/life.py, super/want_eye/fact_or_eye/call_bad_government/use_great_child_with_part/work.py and super/able_fact_and_first_woman/good_hand_and_point.py beneath big man"

---
+++ super/able_fact_and_first_woman/good_hand_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case_or_place(str_arg):
+    other_way(str_arg)
+    print('own_work')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case_or_place('part')
---
+++ super/know_problem/come_man_under_long_work/thing/big_time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child(str_arg):
+    old_problem(str_arg)
+    print('same_point')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child('child')
