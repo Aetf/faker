1509b8a62c216664fc45079277c78501a81efd09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 12:04:16 -0500

    "mv: do super/other_life/next_life_and_hand/give_life.py, super/few_number/week/different_point.py and super/child/be_high_week/next_place.py by young day"

---
+++ super/child/be_high_week/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand(str_arg):
+    point(str_arg)
+    print('case_or_public_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand('early_eye_and_own_child')
