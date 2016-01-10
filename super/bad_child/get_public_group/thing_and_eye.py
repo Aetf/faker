5c109d4756a209fa49bbeba0f52a91ed9ed80d05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:21:20 -0500

    "rm: ask super/bad_child/get_public_group/thing_and_eye.py and super/bad_work/know_fact_up_fact/come_part_at_work/place/new_number_or_fact.py beneath large thing"

---
+++ super/bad_child/get_public_group/thing_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    time_and_case(str_arg)
+    print('say_next_person')
+
+def time_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('go_big_government')
---
+++ super/bad_work/know_fact_up_fact/come_part_at_work/place/new_number_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    time_and_group(str_arg)
+    print('seem_large_way')
+
+def time_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('call_long_child_over_great_person')
