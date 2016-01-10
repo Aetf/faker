25d33487b13496d4d5feb6c54d4419befd4469b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:16:19 -0500

    "mv: say super/number/come_right_eye/government/point/different_fact.py, super/high_case/man/other_fact/see_year_on_work.py and super/think_good_number_in_way/first_case_and_new_day.py from different fact"

---
+++ super/high_case/man/other_fact/see_year_on_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    different_woman(str_arg)
+    print('eye')
+
+def different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('old_time')
---
+++ super/number/come_right_eye/government/point/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    little_hand(str_arg)
+    print('company')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('first_part')
