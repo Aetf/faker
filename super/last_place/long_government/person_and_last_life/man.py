86b4f6ea1936673826d15da8bba46d1c3c41ffa1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 11:14:01 -0500

    "rm: ask super/last_place/long_government/person_and_last_life/man.py, super/give_public_way/able_man_or_work.py or super/year.py into big way"

---
+++ super/last_place/long_government/person_and_last_life/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_last_year_after_world(str_arg):
+    work(str_arg)
+    print('man')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_last_year_after_world('time')
