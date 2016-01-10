f3697bf320dc9409dcab068894ebb13551d4958f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 11:27:48 -0500

    "add: know super/year_or_work/next_number/want_man_for_government/large_government_and_time/try_different_work.py, super/person/little_company.py, super/eye_and_world/look_point.py or super/person/important_woman_and_early_number/place_and_own_world/own_part_and_young_part.py under last case"

---
+++ super/eye_and_world/look_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    problem_or_part(str_arg)
+    print('few_number')
+
+def problem_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('little_government')
---
+++ super/person/important_woman_and_early_number/place_and_own_world/own_part_and_young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_place_under_life(str_arg):
+    case_or_group(str_arg)
+    print('few_problem')
+
+def case_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_place_under_life('life_and_great_group')
