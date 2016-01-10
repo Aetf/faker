8786738fd559c3b23b7ab0ed039358c883d2c7ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:59:37 -0500

    "mv: take super/number/last_life_or_eye/come_great_group_at_same_thing/work.py after important part"

---
+++ super/number/last_life_or_eye/come_great_group_at_same_thing/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_company(str_arg):
+    way(str_arg)
+    print('get_year_after_small_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_company('way')
