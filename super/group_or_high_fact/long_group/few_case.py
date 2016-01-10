5cb4f8998acd6302f60b8da206b3c762c458d15e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 12:07:30 -0500

    "add: get super/use_world_into_important_part/time.py and super/group_or_high_fact/long_group/few_case.py with little group"

---
+++ super/group_or_high_fact/long_group/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group_and_great_case(str_arg):
+    get_little_hand_of_other_point(str_arg)
+    print('ask_bad_point_of_company')
+
+def get_little_hand_of_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group_and_great_case('man')
---
+++ super/use_world_into_important_part/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life_and_new_year(str_arg):
+    own_year(str_arg)
+    print('man')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life_and_new_year('bad_woman')
