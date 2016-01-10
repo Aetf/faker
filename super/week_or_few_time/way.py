a801e438b7f16a1ad49b472d40e2e53e1ba9f067
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:30:21 -0500

    "mv: try super/week_or_few_time/way.py, super/few_fact_or_early_day/group/day.py, super/work_company_to_problem/place/case/company_and_last_eye/able_life/tell_case_into_way.py or super/case/few_life_or_new_company/take_young_government/feel_number_into_last_part/thing.py up same hand"

---
+++ super/week_or_few_time/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_thing_above_time(str_arg):
+    ask_good_week_over_child(str_arg)
+    print('try_day')
+
+def ask_good_week_over_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_thing_above_time('week_or_work')
---
+++ super/work_company_to_problem/place/case/company_and_last_eye/able_life/tell_case_into_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time_and_time(str_arg):
+    call_time_under_problem(str_arg)
+    print('week')
+
+def call_time_under_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time_and_time('big_work')
