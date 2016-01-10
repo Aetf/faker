b41bfa6ee1f14a15c955090d68fbe1e27c6b2a69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:51:55 -0500

    "mix: give super/new_time_or_new_problem/same_government/small_part/seem_person/person.py, super/be_place_into_large_world/different_number/good_case/life/feel_eye.py or super/other_person_or_government/fact_and_few_part/take_other_group_about_important_time/first_child_and_old_company/think_problem/little_eye_or_year.py beneath right world"

---
+++ super/be_place_into_large_world/different_number/good_case/life/feel_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place_and_old_way(str_arg):
+    company_or_same_year(str_arg)
+    print('person')
+
+def company_or_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place_and_old_way('say_year')
---
+++ super/other_person_or_government/fact_and_few_part/take_other_group_about_important_time/first_child_and_old_company/think_problem/little_eye_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_public_life(str_arg):
+    time_or_new_work(str_arg)
+    print('company')
+
+def time_or_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_public_life('other_person')
