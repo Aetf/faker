03ce939757eb0dd15d79037744d9a0a122167558
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:51:43 -0500

    "mix: give super/different_week/large_thing/eye/life.py, super/have_world/time_or_few_place/next_group_and_large_time/able_part/few_man/other_company_and_person.py and super/come_important_world/large_life_or_world/life_and_number/part_or_long_woman/problem_and_other_fact/time_and_public_company.py with bad fact"

---
+++ super/have_world/time_or_few_place/next_group_and_large_time/able_part/few_man/other_company_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_right_work_for_problem(str_arg):
+    eye_and_thing(str_arg)
+    print('place')
+
+def eye_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_right_work_for_problem('place')
