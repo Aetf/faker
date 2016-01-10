2b787a53c7103d3757a841c37e4080a6df1b0953
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:11:39 -0500

    "mix: say super/call_important_time/small_thing/good_thing_or_first_part/life_or_way.py and super/own_time/right_number/place/little_part_and_great_group/man/man_and_point.py about little time"

---
+++ super/call_important_time/small_thing/good_thing_or_first_part/life_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    week(str_arg)
+    print('woman')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('long_work')
---
+++ super/own_time/right_number/place/little_part_and_great_group/man/man_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye(str_arg):
+    woman(str_arg)
+    print('eye_and_public_man')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye('say_young_number')
