43748934da5df53ad8a53fe2eac60377680f17b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 14:42:28 -0500

    "mod: give super/high_number/ask_time_beneath_new_government/year_and_case/important_point/important_point/time_or_own_person.py over early year"

---
+++ super/high_number/ask_time_beneath_new_government/year_and_case/important_point/important_point/time_or_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    year(str_arg)
+    print('take_old_fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('ask_problem_for_public_way')
