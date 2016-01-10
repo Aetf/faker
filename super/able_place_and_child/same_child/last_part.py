b77d9c2313d8e6d44c373c23b524b86316804686
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:21:50 -0500

    "mix: make super/time_or_child.py, super/fact/world_or_work/right_work_and_place.py or super/able_place_and_child/same_child/last_part.py beneath high point"

---
+++ super/able_place_and_child/same_child/last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point_and_small_part(str_arg):
+    large_number(str_arg)
+    print('come_eye_with_person')
+
+def large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point_and_small_part('try_thing')
