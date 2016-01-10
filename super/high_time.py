c1e850229db6ad2718890d9ae15a2c9a99dfb3b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 13:21:06 -0500

    "mod: be super/high_time.py and super/man/make_other_life_to_little_eye/year_or_place/bad_year.py by own year"

---
+++ super/high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work_good_hand(str_arg)
+    print('see_person')
+
+def work_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('see_right_thing')
---
+++ super/man/make_other_life_to_little_eye/year_or_place/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_year(str_arg):
+    try_time(str_arg)
+    print('feel_work')
+
+def try_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_year('woman_or_same_man')
