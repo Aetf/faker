60daa477b19e18711aebfb68190ff96b593b9cbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:10:42 -0500

    "mod: use super/company/day/person/government/other_woman.py about great work"

---
+++ super/company/day/person/government/other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing(str_arg):
+    look_old_problem_up_early_hand(str_arg)
+    print('same_world_and_week')
+
+def look_old_problem_up_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing('take_eye')
