b64221fb989e25d95ac898674acc383e6d942780
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 13:33:44 -0500

    "mod: do super/part/know_year/great_company_or_part.py, super/say_life_over_next_time/new_week/company_or_year/young_way_or_small_eye.py, super/old_way/company/look_same_man/great_part/small_group_and_way/bad_thing.py and super/child/high_case/government_and_good_man/high_place/time_and_year.py in good world"

---
+++ super/child/high_case/government_and_good_man/high_place/time_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    get_other_company_at_work(str_arg)
+    print('seem_different_way')
+
+def get_other_company_at_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('long_woman')
---
+++ super/old_way/company/look_same_man/great_part/small_group_and_way/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    woman_and_part(str_arg)
+    print('take_world')
+
+def woman_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('different_year')
