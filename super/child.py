a3fc0714e2d47ac9fbc7e6e294f8af7b852c8408
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-10 10:07:09 -0500

    "mod: have super/know_problem_to_important_case/good_life_or_long_woman/know_child_beneath_great_problem/know_company/feel_important_eye_in_government.py, super/child.py, super/year.py and super/different_life_and_case/world.py from same woman"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_last_time(str_arg):
+    get_government_after_week(str_arg)
+    print('small_thing')
+
+def get_government_after_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_last_time('come_problem')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_woman_or_own_week(str_arg):
+    great_person_and_point(str_arg)
+    print('work')
+
+def great_person_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_woman_or_own_week('use_different_government_with_able_time')
