f4a81233d086a2ee9a5d751c4adc27b422c0e814
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:58:56 -0500

    "mix: make super/world_and_way/woman/next_week_and_good_part.py and super/problem.py in first place"

---
+++ super/world_and_way/woman/next_week_and_good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_few_part(str_arg):
+    big_eye(str_arg)
+    print('time')
+
+def big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_few_part('point')
