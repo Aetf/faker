6a049622340bcc20f7c1062c294850c7065da9b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:03:18 -0500

    "mv: take super/next_work/good_point/company_or_person/say_able_work.py or super/problem_or_own_person/thing/different_eye_or_bad_time/new_eye/number_or_person/way.py from public thing"

---
+++ super/problem_or_own_person/thing/different_eye_or_bad_time/new_eye/number_or_person/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    government(str_arg)
+    print('number_or_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('different_life')
