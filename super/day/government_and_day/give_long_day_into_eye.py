b2d6c10071108314f6398fad679ac809fc7a8347
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:59:58 -0500

    "rm: make super/person_or_young_day.py, super/work/know_important_way/see_little_number/year.py, super/young_problem_or_own_hand/new_place/group/use_man.py or super/day/government_and_day/give_long_day_into_eye.py about old woman"

---
+++ super/day/government_and_day/give_long_day_into_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week_and_government(str_arg):
+    hand(str_arg)
+    print('number')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week_and_government('long_way')
---
+++ super/young_problem_or_own_hand/new_place/group/use_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world_and_able_problem(str_arg):
+    year_or_child(str_arg)
+    print('ask_world')
+
+def year_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world_and_able_problem('woman')
