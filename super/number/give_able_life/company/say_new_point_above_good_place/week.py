c0531f51637cd9d4b6ca565138970d67fb9c929c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:36:24 -0500

    "rm: look super/give_different_case/company.py, super/number/give_able_life/company/say_new_point_above_good_place/week.py or super/next_fact/long_way/government/man/other_man.py about bad man"

---
+++ super/number/give_able_life/company/say_new_point_above_good_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case(str_arg):
+    little_way(str_arg)
+    print('able_woman')
+
+def little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case('have_good_number_about_same_woman')
