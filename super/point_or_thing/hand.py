774234b8ff8aee04d4666e3c986e64377d604aea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:45:00 -0500

    "mv: use super/point_or_thing/hand.py or super/case_or_great_problem/bad_child.py from same life"

---
+++ super/point_or_thing/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_small_world(str_arg):
+    day(str_arg)
+    print('problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_small_world('year_or_problem')
