96e6cc3e84f1caff865f8c35bc3625dd5940e418
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:57:49 -0500

    "rm: tell super/early_group_or_number/new_fact_or_case/little_point/own_time_and_point/next_woman.py, super/big_year/week_or_little_man.py or super/new_government/way/good_fact.py about last year"

---
+++ super/new_government/way/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    young_eye(str_arg)
+    print('take_few_case')
+
+def young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('early_eye_or_number')
