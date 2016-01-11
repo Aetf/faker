5cee594f6de067da77b04f5f0d0714911ccf6e15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:56:20 -0500

    "mv: want super/be_same_day_into_thing/do_high_group_under_other_eye.py, super/number_and_eye/week/group_or_point/try_woman/woman_or_work.py, super/different_fact_and_small_problem.py and super/young_problem/point/able_thing.py on first eye"

---
+++ super/be_same_day_into_thing/do_high_group_under_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    woman(str_arg)
+    print('own_group')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('way')
---
+++ super/number_and_eye/week/group_or_point/try_woman/woman_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_way_and_early_way(str_arg):
+    problem(str_arg)
+    print('next_man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_way_and_early_way('large_group_and_world')
