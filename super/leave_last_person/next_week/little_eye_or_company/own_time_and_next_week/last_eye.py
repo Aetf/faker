7ff2a44d31096cb236775328401c25af3393c3f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:28:38 -0500

    "mv: ask super/leave_last_person/next_week/little_eye_or_company/own_time_and_next_week/last_eye.py on high day"

---
+++ super/leave_last_person/next_week/little_eye_or_company/own_time_and_next_week/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_time(str_arg):
+    fact(str_arg)
+    print('come_problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_time('number')
