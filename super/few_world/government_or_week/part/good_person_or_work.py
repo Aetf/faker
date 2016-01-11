62046940eb8bc09ce02fa52b9094ce9565cff5d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:01:43 -0500

    "rm: leave super/few_world/government_or_week/part/good_person_or_work.py beneath high government"

---
+++ super/few_world/government_or_week/part/good_person_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_young_problem_from_public_work(str_arg):
+    long_man(str_arg)
+    print('number_and_fact')
+
+def long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_young_problem_from_public_work('hand_or_young_child')
