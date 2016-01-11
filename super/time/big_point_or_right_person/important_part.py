eef60da75a70b979d1b69653230548ac035882e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:37:59 -0500

    "mix: leave super/time/big_point_or_right_person/important_part.py on bad time"

---
+++ super/time/big_point_or_right_person/important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    own_work_and_public_company(str_arg)
+    print('great_woman')
+
+def own_work_and_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('hand')
