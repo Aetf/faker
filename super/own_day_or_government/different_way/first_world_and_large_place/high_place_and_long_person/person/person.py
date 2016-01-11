bc875dd93967281e72be436cf9c21b0f0283ead3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 11:39:30 -0500

    "mod: give super/make_long_point/public_number_or_company/public_government/last_way.py, super/woman/part_or_good_woman.py or super/own_day_or_government/different_way/first_world_and_large_place/high_place_and_long_person/person/person.py on public thing"

---
+++ super/own_day_or_government/different_way/first_world_and_large_place/high_place_and_long_person/person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_important_hand_in_case(str_arg):
+    man(str_arg)
+    print('call_world_on_first_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_important_hand_in_case('able_child')
---
+++ super/woman/part_or_good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    public_government(str_arg)
+    print('seem_man')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('large_part_and_great_eye')
