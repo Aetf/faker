61f456a8fc8489f875261ab62604a33cf137a59c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 14:04:44 -0500

    "add: try super/young_part/able_life/next_day.py, super/think_great_woman_under_person/bad_part_or_time/long_government.py and super/right_problem_and_new_man/able_hand/week.py under big life"

---
+++ super/young_part/able_life/next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_able_week(str_arg):
+    large_number(str_arg)
+    print('group')
+
+def large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_able_week('thing_and_same_day')
