4561748176092185be1edc591b9a05f3a5dafaae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:57:20 -0500

    "mix: look super/go_young_company_after_hand.py after first part"

---
+++ super/go_young_company_after_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    go_place_from_fact(str_arg)
+    print('number_or_problem')
+
+def go_place_from_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('think_last_number')
