7c15b5a56c177d70bea4465853677ae59d540a7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 12:44:32 -0500

    "mix: give super/big_part.py and super/feel_new_week_about_great_part/week.py from old point"

---
+++ super/feel_new_week_about_great_part/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_public_government_for_long_fact(str_arg):
+    good_day(str_arg)
+    print('last_case')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_public_government_for_long_fact('woman')
