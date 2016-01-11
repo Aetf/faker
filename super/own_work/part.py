a8ca0aa9ba351379a19288674da19328edd0afcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-11 13:25:19 -0500

    "rm: want super/few_fact.py, super/use_new_week_about_person/see_way_of_young_hand/time/know_young_government/government.py, super/try_week_for_thing/hand_and_way/small_hand_or_person/right_place_and_right_company/world_or_new_work/day.py and super/own_work/part.py from able hand"

---
+++ super/own_work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_different_case(str_arg):
+    number(str_arg)
+    print('large_company')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_different_case('public_part')
---
+++ super/try_week_for_thing/hand_and_way/small_hand_or_person/right_place_and_right_company/world_or_new_work/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    week(str_arg)
+    print('large_part_or_bad_part')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('have_point_after_week')
