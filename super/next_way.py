0ddc35bb1f2646f6ea35a61d70e305c5983d3fcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 11:10:20 -0500

    "add: try super/last_number/part_or_good_week.py, super/important_week/hand_and_thing/thing/thing_and_other_problem.py or super/next_way.py beneath young man"

---
+++ super/next_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    next_company_or_number(str_arg)
+    print('call_place_after_last_place')
+
+def next_company_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('say_small_fact_in_few_woman')
