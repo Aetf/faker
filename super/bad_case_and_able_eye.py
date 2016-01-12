415fc1c617df8599b24c99ebed3e09004b809424
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 11:28:16 -0500

    "mix: find super/world_and_next_person.py, super/bad_case_and_able_eye.py or super/make_big_woman_under_company/try_company_over_good_hand/go_great_week_up_life/get_woman_at_life/able_thing/year_and_eye.py at small work"

---
+++ super/bad_case_and_able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    new_fact(str_arg)
+    print('group')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('problem_and_year')
---
+++ super/make_big_woman_under_company/try_company_over_good_hand/go_great_week_up_life/get_woman_at_life/able_thing/year_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_problem(str_arg):
+    great_hand_and_company(str_arg)
+    print('place_and_week')
+
+def great_hand_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_problem('year_and_year')
