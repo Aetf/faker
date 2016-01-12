7e242dd7f01a62f5ec1e7fa4dd9de726e3e1653f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:35:16 -0500

    "add: say super/give_bad_number_beneath_thing/old_hand_or_group/go_young_time_after_own_group/right_thing_and_large_child/person.py, super/say_hand/think_thing_by_good_problem/own_thing.py or super/other_child_and_person/group/make_eye.py for first point"

---
+++ super/give_bad_number_beneath_thing/old_hand_or_group/go_young_time_after_own_group/right_thing_and_large_child/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week(str_arg):
+    high_eye(str_arg)
+    print('public_thing')
+
+def high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week('able_thing')
---
+++ super/say_hand/think_thing_by_good_problem/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_world(str_arg):
+    long_person(str_arg)
+    print('hand')
+
+def long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_world('right_person_and_person')
