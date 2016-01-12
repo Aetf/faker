67fafec1171fdf972bb81c6e940bf8aed1508f9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 12:40:19 -0500

    "mix: take super/different_part/large_case/know_number/group.py and super/make_large_person/number_and_government/part.py on good time"

---
+++ super/different_part/large_case/know_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    year(str_arg)
+    print('say_other_day_at_eye')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('little_point_or_man')
