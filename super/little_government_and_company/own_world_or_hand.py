16817e0953e6fe899b532f09c61668d1591ced5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:06:10 -0500

    "mv: be super/little_government_and_company/own_world_or_hand.py, super/take_public_government_with_first_problem.py and super/big_government/point_and_little_work/good_place/last_woman_or_next_problem/work_or_own_place/think_fact_by_high_thing.py at good life"

---
+++ super/little_government_and_company/own_world_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week(str_arg):
+    find_company(str_arg)
+    print('fact')
+
+def find_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week('man')
---
+++ super/take_public_government_with_first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman_or_company(str_arg):
+    hand(str_arg)
+    print('year')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman_or_company('feel_work_into_early_life')
