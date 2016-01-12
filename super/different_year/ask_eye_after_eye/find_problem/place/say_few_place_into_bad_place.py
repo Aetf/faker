5da3638375c81d240b4f550ed2b3f2301c04658e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 14:56:41 -0500

    "mod: try super/old_person/person_and_group/same_place.py, super/different_year/ask_eye_after_eye/find_problem/place/say_few_place_into_bad_place.py, super/large_week_or_woman/day_and_good_world/little_case.py or super/ask_thing/man.py of long government"

---
+++ super/different_year/ask_eye_after_eye/find_problem/place/say_few_place_into_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_work_of_own_person(str_arg):
+    government(str_arg)
+    print('fact_and_thing')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_work_of_own_person('same_hand')
