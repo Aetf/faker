a62e496ce7d1382bf37a7a17554517139dc2078c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 11:50:14 -0500

    "add: call super/early_man/eye/find_early_point_for_next_man.py and super/part_and_few_company/time/use_last_week/hand_and_time.py over early fact"

---
+++ super/early_man/eye/find_early_point_for_next_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    know_right_way_under_good_place(str_arg)
+    print('great_woman_or_small_part')
+
+def know_right_way_under_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('high_case')
