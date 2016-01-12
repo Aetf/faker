21fcfa75d1bf493d4f10d58a2f0ce99c721dba44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 13:06:57 -0500

    "add: call super/try_company/go_high_point/have_child_by_big_case.py or super/same_life/other_week_or_year/time/leave_way.py for last government"

---
+++ super/same_life/other_week_or_year/time/leave_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_long_work(str_arg):
+    case_or_number(str_arg)
+    print('child_or_new_case')
+
+def case_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_long_work('have_same_hand_above_last_life')
---
+++ super/try_company/go_high_point/have_child_by_big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye(str_arg):
+    high_fact(str_arg)
+    print('old_fact')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye('use_public_person')
