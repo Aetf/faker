c470fa69f9a94f69f34493164d6934fa3f944b47
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 11:07:43 -0500

    "mv: leave super/bad_company_and_different_life/point/right_world/fact/few_fact.py, super/young_company_and_government/different_world_or_young_point/small_fact/man.py and super/large_hand.py above own group"

---
+++ super/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_point_above_eye(str_arg):
+    large_company(str_arg)
+    print('work_or_place')
+
+def large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_point_above_eye('see_small_eye')
---
+++ super/young_company_and_government/different_world_or_young_point/small_fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    want_year(str_arg)
+    print('good_child_and_same_year')
+
+def want_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('large_eye_or_good_world')
