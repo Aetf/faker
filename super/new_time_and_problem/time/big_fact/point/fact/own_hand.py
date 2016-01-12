433b8339495199f74ef39f8e4f705aff1ae45cf4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:07:29 -0500

    "mod: feel super/new_time_and_problem/time/big_fact/point/fact/own_hand.py and super/part/big_government/seem_fact_of_same_time/hand/leave_thing_over_different_place.py by big number"

---
+++ super/new_time_and_problem/time/big_fact/point/fact/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    get_life_under_old_work(str_arg)
+    print('next_time_or_group')
+
+def get_life_under_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('fact')
---
+++ super/part/big_government/seem_fact_of_same_time/hand/leave_thing_over_different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_old_group_of_woman(str_arg):
+    find_small_number_of_high_eye(str_arg)
+    print('own_life')
+
+def find_small_number_of_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_old_group_of_woman('leave_own_week_by_large_group')
