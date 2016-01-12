40e2729c87e3616033d111b2172315c68c1aa317
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 14:25:35 -0500

    "mod: take super/government/different_company_and_place.py, super/eye_or_number/other_number/case.py, super/woman/good_company/go_child_under_week/good_man.py and super/high_part/new_point_and_young_world/give_way_up_good_time/week/fact/call_different_part.py for little number"

---
+++ super/eye_or_number/other_number/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    do_number_beneath_next_woman(str_arg)
+    print('know_man')
+
+def do_number_beneath_next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('time')
