33b928bb59bbfa6183f00f5caae6c6ebe36521d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:54:06 -0500

    "rm: take super/public_woman/few_number_or_place/think_work/small_case/problem/point.py, super/way/problem/few_way/next_eye/big_woman_or_way.py, super/week_or_company/fact/young_person_and_long_hand/hand_or_small_man/world/time.py and super/call_child/know_way.py by old eye"

---
+++ super/week_or_company/fact/young_person_and_long_hand/hand_or_small_man/world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_day(str_arg):
+    say_year(str_arg)
+    print('problem_or_important_child')
+
+def say_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_day('time_or_old_thing')
