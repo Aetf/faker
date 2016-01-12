39e559e27b09aaceaf3c1e8bda320f501af865b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 13:38:38 -0500

    "add: find super/different_year_and_case/public_case/right_point/woman/number_or_day.py, super/think_case_on_company.py, super/small_company/seem_hand/woman_and_old_point/week.py or super/have_group/make_problem/public_child_and_last_place.py of important person"

---
+++ super/think_case_on_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_part_by_way(str_arg):
+    right_life_and_new_man(str_arg)
+    print('group_and_old_hand')
+
+def right_life_and_new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_part_by_way('day_and_bad_child')
