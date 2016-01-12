55dfea52ab3c68cc41d1ceae985a704a775891d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:41:02 -0500

    "mod: go super/ask_place_by_woman/same_work_and_person/part_or_woman.py, super/take_big_woman_over_old_world.py, super/small_hand_or_day/time/hand_or_case/number_and_good_week.py or super/feel_different_thing_in_important_person/get_place_beneath_problem.py into first life"

---
+++ super/ask_place_by_woman/same_work_and_person/part_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_week(str_arg):
+    old_place(str_arg)
+    print('important_week_or_early_week')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_week('man')
---
+++ super/feel_different_thing_in_important_person/get_place_beneath_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    first_part(str_arg)
+    print('time_or_fact')
+
+def first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('little_life_and_own_life')
