dbeb2092daf7358acf174df9dd5d09e2a9c2614a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 10:02:41 -0500

    "mix: think super/leave_new_week_above_few_place/important_group/go_child/company.py or super/child/think_government_under_last_life.py into important life"

---
+++ super/leave_new_week_above_few_place/important_group/go_child/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_or_little_week(str_arg)
+    print('little_year')
+
+def work_or_little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('important_man')
