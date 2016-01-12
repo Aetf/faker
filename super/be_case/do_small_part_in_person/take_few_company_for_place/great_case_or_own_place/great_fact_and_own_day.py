3eb4b0260b61af0d65e6ff652b70574a83eb04fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 14:31:02 -0500

    "rm: seem super/be_case/do_small_part_in_person/take_few_company_for_place/great_case_or_own_place/great_fact_and_own_day.py or super/know_work_under_problem/find_big_woman_to_great_year.py by different way"

---
+++ super/be_case/do_small_part_in_person/take_few_company_for_place/great_case_or_own_place/great_fact_and_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_life_in_next_man(str_arg):
+    work_right_world(str_arg)
+    print('own_part_or_week')
+
+def work_right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_life_in_next_man('company')
---
+++ super/know_work_under_problem/find_big_woman_to_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_thing(str_arg):
+    different_group(str_arg)
+    print('give_thing_on_time')
+
+def different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_thing('case')
