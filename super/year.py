3b631025f8151829e3ab356a43b69fd804381de0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 14:44:17 -0500

    "mod: go super/year.py and super/problem/way.py to high day"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_company(str_arg):
+    world(str_arg)
+    print('bad_problem')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_company('right_eye')
