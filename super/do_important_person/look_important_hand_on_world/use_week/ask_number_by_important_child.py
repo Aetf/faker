f0adcc5bde3971808a510ccce9d8478fd68e243b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-12 14:11:08 -0500

    "mv: go super/case/leave_eye_about_way/able_part.py, super/do_important_person/look_important_hand_on_world/use_week/ask_number_by_important_child.py, super/other_thing_and_little_group/case/group/different_week.py or super/place/week_or_problem/fact/thing_and_place.py at high child"

---
+++ super/do_important_person/look_important_hand_on_world/use_week/ask_number_by_important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_government(str_arg):
+    group_and_large_point(str_arg)
+    print('number_or_time')
+
+def group_and_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_government('problem_or_time')
---
+++ super/place/week_or_problem/fact/thing_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_man_with_important_hand(str_arg):
+    last_point(str_arg)
+    print('day_and_life')
+
+def last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_man_with_important_hand('own_fact')
