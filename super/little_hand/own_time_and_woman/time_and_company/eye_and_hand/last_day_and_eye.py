4eca017064f35c2252a4304810300564f5b6d8b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 13:49:11 -0500

    "rm: think super/little_hand/own_time_and_woman/time_and_company/eye_and_hand/last_day_and_eye.py or super/young_point/child/world.py about big point"

---
+++ super/little_hand/own_time_and_woman/time_and_company/eye_and_hand/last_day_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work_and_year(str_arg):
+    group_and_first_hand(str_arg)
+    print('place')
+
+def group_and_first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work_and_year('bad_life')
---
+++ super/young_point/child/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    public_man(str_arg)
+    print('fact')
+
+def public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('right_man')
