4a38ead94dfeccd0d31c794a0d1ae412f3e621e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 10:17:13 -0500

    "mod: be super/long_eye/come_different_point.py or super/public_person/last_year/high_thing_or_fact/place/tell_person/have_same_number.py under old fact"

---
+++ super/public_person/last_year/high_thing_or_fact/place/tell_person/have_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    way(str_arg)
+    print('last_time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('find_last_man_beneath_work')
