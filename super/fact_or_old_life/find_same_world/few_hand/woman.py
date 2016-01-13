77e0b2f63be5eba8fa08b1c4fe91501a0028399a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 13:33:55 -0500

    "mix: make super/work_own_fact_up_different_woman.py, super/do_large_fact/problem.py, super/company_or_little_day/day_and_next_group/life/company_and_part/few_time.py or super/fact_or_old_life/find_same_world/few_hand/woman.py about new life"

---
+++ super/fact_or_old_life/find_same_world/few_hand/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man(str_arg):
+    come_world_over_place(str_arg)
+    print('go_life')
+
+def come_world_over_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man('day')
---
+++ super/work_own_fact_up_different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_right_eye(str_arg):
+    other_hand(str_arg)
+    print('early_world')
+
+def other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_right_eye('right_time')
