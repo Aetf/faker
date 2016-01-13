c85363f3c22e9dae2f20ac6883aab49500b4c956
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 12:05:55 -0500

    "mv: give super/good_man/own_way.py up big thing"

---
+++ super/good_man/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_company(str_arg):
+    new_work(str_arg)
+    print('small_point')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_company('feel_right_part_up_young_person')
