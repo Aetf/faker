7bd54648622c17986fa9ca998b5dfe8a9c7f6d61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:54:45 -0500

    "mv: feel super/know_same_world_under_young_eye/use_time_about_new_way/know_company.py, super/life/feel_same_eye_under_long_group/other_company/week/come_eye.py or super/next_place_or_year/few_place/few_case_or_next_hand.py in young group"

---
+++ super/life/feel_same_eye_under_long_group/other_company/week/come_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_right_point(str_arg):
+    seem_public_eye(str_arg)
+    print('great_company_and_long_eye')
+
+def seem_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_right_point('case')
