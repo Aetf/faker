4c31cee32f9686926889f7cbc4d31b1211c7094a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 10:09:00 -0500

    "mv: leave super/year/life_and_different_part/go_work_up_first_year.py or super/fact/time_or_life/company_and_own_week.py above own way"

---
+++ super/year/life_and_different_part/go_work_up_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_important_case(str_arg):
+    case(str_arg)
+    print('know_bad_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_important_case('early_world_or_right_child')
