e8f709129cbf47d9d440c41b90c0d4b19e850219
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:13:41 -0500

    "mix: work super/old_place/take_important_thing/point/be_world/government_or_important_life.py, super/see_other_point_into_small_woman.py or super/bad_part_or_person/new_fact_and_time/important_eye/important_government.py for high time"

---
+++ super/bad_part_or_person/new_fact_and_time/important_eye/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    next_hand(str_arg)
+    print('right_fact')
+
+def next_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('bad_life_and_few_man')
