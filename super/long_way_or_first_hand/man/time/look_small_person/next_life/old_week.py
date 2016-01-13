e9e31d2a79ba9b306c8f9e445577c0530c06df1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:07:04 -0500

    "add: come super/number/give_point_over_number/group/first_week/day_and_next_work/great_way.py, super/different_life_and_right_world/new_man/new_hand.py, super/work_and_small_day.py and super/long_way_or_first_hand/man/time/look_small_person/next_life/old_week.py into public government"

---
+++ super/long_way_or_first_hand/man/time/look_small_person/next_life/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_company(str_arg):
+    first_hand(str_arg)
+    print('want_large_place')
+
+def first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_company('same_hand')
