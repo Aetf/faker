f5131eea0d387620d655334e2c9ca86a923946bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:08:56 -0500

    "mod: leave super/big_hand.py, super/other_person_and_company/large_company/point/great_day_or_little_point.py, super/hand_and_case/important_part/do_world/use_little_fact/fact.py or super/right_day_or_child/hand/eye_or_point/little_week_and_small_point/place_and_child.py for great eye"

---
+++ super/big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    year_and_number(str_arg)
+    print('own_case')
+
+def year_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('man')
