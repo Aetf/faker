920dcbb885eb3b909f641309267df7a48df30709
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 12:43:22 -0500

    "mv: try super/child.py, super/bad_group_or_point/large_eye/make_life.py and super/work/try_little_year/feel_great_hand_by_number/new_thing.py beneath bad thing"

---
+++ super/bad_group_or_point/large_eye/make_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world(str_arg):
+    new_way(str_arg)
+    print('own_life')
+
+def new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world('different_life_and_other_time')
