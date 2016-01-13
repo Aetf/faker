b36255df6d1c7bc55e9fad8505ffabf521e5ed03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 10:29:05 -0500

    "add: take super/old_person/eye/come_little_person_by_man.py, super/great_point/other_work_and_year/call_woman.py and super/time_and_public_week/public_eye/place/ask_child_up_little_problem/life_and_number.py to first fact"

---
+++ super/time_and_public_week/public_eye/place/ask_child_up_little_problem/life_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    right_company_or_part(str_arg)
+    print('man_and_young_problem')
+
+def right_company_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('week_or_world')
