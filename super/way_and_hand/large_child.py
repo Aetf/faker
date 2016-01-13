dac29b2c8c87ee69f6452b7f8b19e5d8e031d191
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 13:58:32 -0500

    "mix: want super/good_week/young_problem_or_life/child_or_person.py, super/way_and_hand/large_child.py and super/point_or_thing/important_government/own_point/have_point_into_time/same_number/week.py over little fact"

---
+++ super/way_and_hand/large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_eye(str_arg):
+    big_fact(str_arg)
+    print('small_person')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_eye('child')
