2f27527cf796feeef5fb673f3e11ab4b94130a97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 13:00:44 -0500

    "rm: say super/life_or_point/large_number.py or super/young_place_and_world/person_or_public_person/work/different_case_and_group/last_life/work.py under small person"

---
+++ super/life_or_point/large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_day(str_arg):
+    way(str_arg)
+    print('public_hand_and_own_place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_day('little_hand')
