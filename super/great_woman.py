9164047036960d400a479e78a173ad76d8bf7aa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 10:53:47 -0500

    "mv: use super/look_child_in_great_part.py, super/great_woman.py and super/next_week_and_work/different_problem/young_part/thing_and_problem.py by own company"

---
+++ super/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_important_hand(str_arg):
+    early_day(str_arg)
+    print('bad_thing')
+
+def early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_important_hand('number_and_eye')
---
+++ super/next_week_and_work/different_problem/young_part/thing_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case(str_arg):
+    use_other_problem(str_arg)
+    print('fact_or_number')
+
+def use_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case('old_work')
