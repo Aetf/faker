1ef92bb01c64549dd9f878f36d1563a12ec0971a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:54:22 -0500

    "mv: know super/woman_and_old_person/different_man/young_thing_or_next_group/feel_first_fact/next_hand/year.py, super/same_work_or_company/person/think_large_man/great_world.py and super/take_year/group_or_life/year/new_woman/high_part_or_small_problem/own_work_or_young_case.py into different government"

---
+++ super/same_work_or_company/person/think_large_man/great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact(str_arg):
+    right_fact(str_arg)
+    print('call_good_child_under_different_work')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact('same_government')
--- super/take_year/group_or_life/year/new_woman/high_part_or_small_problem/own_work_or_young_case.py
+++ super/take_year/group_or_life/year/new_woman/high_part_or_small_problem/own_work_or_young_case.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def last_world(str_arg):
+def problem(str_arg):
     life(str_arg)
-    print('new_case_or_first_fact')
+    print('part_or_person')
 
 def life(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    last_world('call_number_of_public_week')
+    problem('few_company_or_way')
