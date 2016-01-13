385326f11e1b08cddc8d581d3b2982d441b0863c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 10:47:39 -0500

    "mv: seem super/large_part_or_world/week.py, super/find_point/old_point/do_case_for_work/tell_hand_to_company/public_place/last_way.py, super/hand/make_important_number_up_year/make_part/person_and_eye.py and super/time.py for bad company"

---
+++ super/hand/make_important_number_up_year/make_part/person_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_old_woman(str_arg):
+    find_work(str_arg)
+    print('thing')
+
+def find_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_old_woman('small_hand_or_government')
---
+++ super/large_part_or_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_young_year_from_long_eye(str_arg):
+    year(str_arg)
+    print('use_person')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_young_year_from_long_eye('use_little_time')
