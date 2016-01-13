e9da5588cabedcf2d8ba641a769b7374d624cfc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 14:08:46 -0500

    "mv: do super/bad_life_or_number/feel_company_over_hand/make_few_time_under_fact/good_person_or_hand/high_woman/ask_right_person.py, super/right_group/thing/time_or_case/own_life.py, super/long_point.py and super/ask_fact/new_part_and_world/large_part/day/different_person.py under important number"

---
+++ super/right_group/thing/time_or_case/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_new_child(str_arg):
+    point(str_arg)
+    print('want_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_new_child('find_hand_beneath_way')
