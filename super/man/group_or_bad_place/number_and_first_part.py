dc4735f42bd2b670042d2f03d360af4cfe569aca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-13 11:02:22 -0500

    "mod: tell super/man/group_or_bad_place/number_and_first_part.py and super/man.py for public point"

---
+++ super/man/group_or_bad_place/number_and_first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    other_government_or_right_group(str_arg)
+    print('take_high_case_for_man')
+
+def other_government_or_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('large_woman_and_point')
