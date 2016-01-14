4858dfa6fd24b33e5834320092c1ca479d97bb7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 10:00:57 -0500

    "mod: give super/bad_thing/case/fact/own_number_and_bad_child/last_hand_or_person.py, super/week/thing.py, super/government/new_week/government/person.py and super/child/own_year_or_good_group/place/early_woman_or_little_number.py of small thing"

---
+++ super/child/own_year_or_good_group/place/early_woman_or_little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_bad_week_into_big_case(str_arg):
+    thing(str_arg)
+    print('same_person')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_bad_week_into_big_case('time')
