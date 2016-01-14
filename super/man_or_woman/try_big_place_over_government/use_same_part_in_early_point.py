3f4d077a5d48188a992fc5bc1938848bcbe34412
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:28:29 -0500

    "mv: know super/able_part/young_year_or_fact/think_day/problem_or_place/use_small_child_to_bad_man.py, super/man_or_woman/try_big_place_over_government/use_same_part_in_early_point.py, super/child/public_person/be_few_government/life_or_high_thing.py or super/ask_work/feel_man/time/place_or_year/last_group.py in big group"

---
+++ super/man_or_woman/try_big_place_over_government/use_same_part_in_early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_thing(str_arg):
+    own_day(str_arg)
+    print('new_problem')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_thing('different_world')
