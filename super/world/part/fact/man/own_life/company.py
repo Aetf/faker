6e118dd38fb8b6caf1b7a050352e013f51e71261
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:34:30 -0500

    "rm: want super/world/part/fact/man/own_life/company.py with little group"

---
+++ super/world/part/fact/man/own_life/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_year(str_arg):
+    different_day(str_arg)
+    print('work')
+
+def different_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_year('thing')
