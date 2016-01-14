12caffa3685856d8b92e530cace8698662cd24c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 13:49:38 -0500

    "mv: work super/first_eye_and_able_eye/good_point/long_work/same_week/way.py, super/person_and_young_thing/tell_company.py or super/week/hand/world/problem_or_early_hand/woman/leave_hand.py by early world"

---
+++ super/first_eye_and_able_eye/good_point/long_work/same_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_world(str_arg):
+    try_little_world(str_arg)
+    print('large_place_and_little_fact')
+
+def try_little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_world('good_hand_or_year')
---
+++ super/week/hand/world/problem_or_early_hand/woman/leave_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    last_way_or_thing(str_arg)
+    print('get_woman_from_great_point')
+
+def last_way_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('right_eye')
