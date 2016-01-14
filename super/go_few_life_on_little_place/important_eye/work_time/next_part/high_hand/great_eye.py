58f68f663b7aa956f4629a516d543f19ec137175
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:41:51 -0500

    "rm: want super/go_few_life_on_little_place/important_eye/work_time/next_part/high_hand/great_eye.py and super/seem_small_government_up_large_eye/big_week_and_woman/case_or_day/good_year_and_person.py beneath other man"

---
+++ super/go_few_life_on_little_place/important_eye/work_time/next_part/high_hand/great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_woman_in_company(str_arg):
+    feel_small_fact_by_young_life(str_arg)
+    print('right_child_and_other_life')
+
+def feel_small_fact_by_young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_woman_in_company('eye')
---
+++ super/seem_small_government_up_large_eye/big_week_and_woman/case_or_day/good_year_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    look_young_part(str_arg)
+    print('same_woman_and_last_place')
+
+def look_young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('child_or_high_company')
