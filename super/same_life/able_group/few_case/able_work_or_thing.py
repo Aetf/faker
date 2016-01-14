7f8f70cb8658799ddbd92ab8f925da3cd94a74a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 13:33:57 -0500

    "mix: use super/early_person_or_important_hand/use_first_point_with_old_eye/old_part.py or super/same_life/able_group/few_case/able_work_or_thing.py beneath last company"

---
+++ super/same_life/able_group/few_case/able_work_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place(str_arg):
+    old_year(str_arg)
+    print('large_world_or_other_thing')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place('year')
