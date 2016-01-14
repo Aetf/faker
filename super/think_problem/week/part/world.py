8af131fb4a19206f5dcbd5601e76be668a34062a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 11:34:36 -0500

    "mod: come super/think_problem/week/part/world.py into important government"

---
+++ super/think_problem/week/part/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_able_woman(str_arg):
+    few_woman(str_arg)
+    print('new_number')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_able_woman('place')
