7a46ed5a4c83ee58d3299c1bc792492d9b5ffcc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 11:49:17 -0500

    "mix: work super/world_and_high_eye/old_world_or_high_government/man_or_hand/part.py in same part"

---
+++ super/world_and_high_eye/old_world_or_high_government/man_or_hand/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_life_beneath_child(str_arg):
+    part(str_arg)
+    print('work')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_life_beneath_child('next_day_and_time')
