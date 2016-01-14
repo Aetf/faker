0783cbcf1aaa80727f726bcaacec24a19d344780
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 11:42:23 -0500

    "mod: seem super/thing_and_fact/say_work_of_hand/person/do_long_time.py, super/next_man/first_eye_or_number.py or super/high_time_and_public_work/go_different_man/first_part/small_time/small_number/last_woman_or_first_point.py of other time"

---
+++ super/next_man/first_eye_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    other_life(str_arg)
+    print('week')
+
+def other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('woman_or_first_number')
