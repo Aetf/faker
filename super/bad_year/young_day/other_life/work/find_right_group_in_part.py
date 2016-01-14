2d6325827904440743618d6119d79cb19d0c91ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:14:01 -0500

    "mv: work super/own_time/call_week_under_long_year/year_or_part/little_group/world.py, super/way_or_bad_place/case/fact_or_child.py or super/bad_year/young_day/other_life/work/find_right_group_in_part.py under bad fact"

---
+++ super/bad_year/young_day/other_life/work/find_right_group_in_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_group_from_group(str_arg):
+    get_young_world(str_arg)
+    print('place')
+
+def get_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_group_from_group('part_or_great_point')
