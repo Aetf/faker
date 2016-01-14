97c8794f4bd8aa13feecbb8e8261570571cfe7ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 11:26:56 -0500

    "add: make super/great_year/be_good_person_of_fact/life_and_big_year/other_year.py, super/get_life.py, super/hand/part_and_woman/small_company/problem/next_man_and_young_case.py and super/little_fact/last_world/own_part_or_year/look_other_place_under_good_eye/few_day_or_week.py to first way"

---
+++ super/great_year/be_good_person_of_fact/life_and_big_year/other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    new_hand(str_arg)
+    print('week')
+
+def new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('first_company_and_woman')
---
+++ super/hand/part_and_woman/small_company/problem/next_man_and_young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    man(str_arg)
+    print('long_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('want_few_point_into_bad_thing')
