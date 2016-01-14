01b1f8fb524b07709bdec03af53d4453e04cbe09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:03:38 -0500

    "mv: be super/use_right_child_beneath_public_government/old_world.py by important child"

---
+++ super/use_right_child_beneath_public_government/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    have_good_person_with_fact(str_arg)
+    print('case_and_week')
+
+def have_good_person_with_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('bad_eye')
