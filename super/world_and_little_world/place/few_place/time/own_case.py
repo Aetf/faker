8beddf23c8ce3cdad4abdd993554d4211324fbc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 14:09:16 -0500

    "mod: work super/world_and_little_world/place/few_place/time/own_case.py from different day"

---
+++ super/world_and_little_world/place/few_place/time/own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    life(str_arg)
+    print('person')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('work_or_big_child')
