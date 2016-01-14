b526941a41eaccc9383fa8341d440cb781f03ea3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 11:25:18 -0500

    "mv: leave super/point.py or super/man/want_point/company.py up other day"

---
+++ super/man/want_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_life(str_arg):
+    same_child(str_arg)
+    print('own_child_and_case')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_life('problem_or_same_person')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    call_first_person_on_old_work(str_arg)
+    print('world_and_big_way')
+
+def call_first_person_on_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('man')
