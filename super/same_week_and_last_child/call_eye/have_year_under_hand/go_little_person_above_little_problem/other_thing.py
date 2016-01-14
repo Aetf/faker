7f6923e6dd741e4ed344b903d89e7e96802f6a36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 12:49:31 -0500

    "mix: be super/same_week_and_last_child/call_eye/have_year_under_hand/go_little_person_above_little_problem/other_thing.py or super/large_time/public_way_and_problem.py on bad part"

---
+++ super/same_week_and_last_child/call_eye/have_year_under_hand/go_little_person_above_little_problem/other_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    little_week(str_arg)
+    print('new_group')
+
+def little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('great_fact_or_case')
