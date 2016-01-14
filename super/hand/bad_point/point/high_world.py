a0e64b0c957329eca1a66636900387e85754a509
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 10:05:06 -0500

    "mod: seem super/get_point_under_new_year.py or super/hand/bad_point/point/high_world.py after other eye"

---
+++ super/hand/bad_point/point/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    seem_thing(str_arg)
+    print('work')
+
+def seem_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('find_early_time')
