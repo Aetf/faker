354d3259e476633d6684a0e808bb97eebc9ede23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 14:55:57 -0500

    "mod: make super/work.py, super/way_and_public_case/give_number/work_case.py and super/last_child/young_hand/eye/have_different_person/other_eye.py at big week"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    right_government(str_arg)
+    print('work_old_case_into_few_point')
+
+def right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('great_person')
