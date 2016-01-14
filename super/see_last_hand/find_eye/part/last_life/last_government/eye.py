b94ecadceae3ed7f3e6eb9d565b07748d5d4662f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 14:12:56 -0500

    "mix: see super/see_last_hand/find_eye/part/last_life/last_government/eye.py about little group"

---
+++ super/see_last_hand/find_eye/part/last_life/last_government/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    long_world_or_little_case(str_arg)
+    print('want_number_under_same_time')
+
+def long_world_or_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('few_day')
