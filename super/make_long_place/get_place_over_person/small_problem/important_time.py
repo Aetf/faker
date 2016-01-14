cad050f2c7d29a2cff9d9ecbaf57a66b073502bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 14:13:58 -0500

    "mod: ask super/last_number_or_hand/seem_long_day_over_old_eye/same_thing_or_new_case.py, super/feel_world_into_company/large_time_or_fact/number_and_case/small_year/old_hand_or_hand/take_high_man.py or super/make_long_place/get_place_over_person/small_problem/important_time.py over public man"

---
+++ super/make_long_place/get_place_over_person/small_problem/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_and_different_way(str_arg):
+    have_place_up_first_point(str_arg)
+    print('give_big_time_by_new_time')
+
+def have_place_up_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_and_different_way('say_last_hand')
