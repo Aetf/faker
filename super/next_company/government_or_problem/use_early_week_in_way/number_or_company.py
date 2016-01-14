aaf9829c7277882d9b259271aec9a8bafc29e1b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-14 13:02:39 -0500

    "mix: try super/next_company/government_or_problem/use_early_week_in_way/number_or_company.py and super/thing/give_important_place_of_young_fact/great_group.py up old week"

---
+++ super/next_company/government_or_problem/use_early_week_in_way/number_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_woman(str_arg):
+    world(str_arg)
+    print('next_life')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_woman('small_company_and_last_man')
---
+++ super/thing/give_important_place_of_young_fact/great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_eye(str_arg):
+    day(str_arg)
+    print('same_company_and_little_woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_eye('hand')
