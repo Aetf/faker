2e9264df4bf0260ddda8924a72a24af87e416c44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 12:01:37 -0500

    "add: ask super/government/part.py and super/different_case/see_problem/own_part.py up new case"

---
+++ super/different_case/see_problem/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    be_child_above_fact(str_arg)
+    print('next_part_and_woman')
+
+def be_child_above_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('week_and_government')
