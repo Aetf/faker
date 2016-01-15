ca438485569afbac6ba76a03dd753d21c6e6b022
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 10:42:12 -0500

    "rm: work super/eye/person/go_person_in_hand/fact_or_fact.py of big woman"

---
+++ super/eye/person/go_person_in_hand/fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_small_group(str_arg):
+    same_eye(str_arg)
+    print('hand_and_time')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_small_group('world')
