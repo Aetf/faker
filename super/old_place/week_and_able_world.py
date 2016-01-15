2e6d573171a0d41636616222133535b7185ba845
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 14:50:25 -0500

    "mix: do super/big_time/day.py and super/old_place/week_and_able_world.py to small fact"

---
+++ super/old_place/week_and_able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time(str_arg):
+    group(str_arg)
+    print('do_hand_with_first_thing')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time('good_fact')
