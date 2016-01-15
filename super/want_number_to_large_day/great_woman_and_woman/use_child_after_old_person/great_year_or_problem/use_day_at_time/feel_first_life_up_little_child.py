5ff1b527efc65100ff39bc16716de63052f7daf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 13:49:53 -0500

    "rm: give super/want_number_to_large_day/great_woman_and_woman/use_child_after_old_person/great_year_or_problem/use_day_at_time/feel_first_life_up_little_child.py to next eye"

---
+++ super/want_number_to_large_day/great_woman_and_woman/use_child_after_old_person/great_year_or_problem/use_day_at_time/feel_first_life_up_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_young_fact(str_arg):
+    week_and_time(str_arg)
+    print('case')
+
+def week_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_young_fact('government_and_place')
