529162a2ea4f40763238665cbc3f822e2e9ffa5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 14:10:24 -0500

    "rm: say super/same_year/next_point/important_hand_or_thing/small_eye/get_life/important_problem.py over last case"

---
+++ super/same_year/next_point/important_hand_or_thing/small_eye/get_life/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government(str_arg):
+    way_and_great_point(str_arg)
+    print('week')
+
+def way_and_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government('other_problem')
