dc928bcfd3d22ba1c59966d8912c3360f4f7b44a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 14:26:09 -0500

    "add: see super/company/seem_fact_about_able_eye/old_eye_and_next_fact/case/call_old_fact.py, super/own_government/different_man_or_first_week/government_and_day/early_company/public_case.py, super/right_point_and_problem/want_case/same_fact_or_own_eye/know_last_problem_of_week/work_bad_time_at_time/able_case_and_own_child.py or super/little_way/place_and_own_work/use_child/own_group/hand/few_place.py above few number"

---
+++ super/little_way/place_and_own_work/use_child/own_group/hand/few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_life_up_little_person(str_arg):
+    year(str_arg)
+    print('take_long_group_with_large_day')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_life_up_little_person('point')
---
+++ super/right_point_and_problem/want_case/same_fact_or_own_eye/know_last_problem_of_week/work_bad_time_at_time/able_case_and_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    have_eye(str_arg)
+    print('go_eye')
+
+def have_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('say_important_point_by_child')
