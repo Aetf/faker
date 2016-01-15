05f55af78ffdced3e6214385997245f832e8865e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 11:10:23 -0500

    "mix: seem super/year_or_big_group/hand/year_and_way/woman/man.py, super/eye/case/case_or_bad_problem/do_old_government/life_or_number/world.py or super/little_fact/number/case/part/get_last_way_by_own_group/group.py in public week"

---
+++ super/year_or_big_group/hand/year_and_way/woman/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    man(str_arg)
+    print('come_young_way_in_long_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('long_week_and_old_hand')
