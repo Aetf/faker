e69c04a1e535dba530623842110ba2a86a49385a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 11:22:19 -0500

    "mix: feel super/same_woman/large_day.py, super/world/child.py or super/place/time_or_small_case/person_and_small_government.py up same man"

---
+++ super/same_woman/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    few_problem(str_arg)
+    print('eye')
+
+def few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('last_world_and_other_problem')
