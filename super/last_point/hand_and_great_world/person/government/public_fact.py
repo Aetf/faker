7a98554942a8093e3ff0fc9577e66bb60f8f06a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 11:39:51 -0500

    "rm: come super/place/able_place/do_problem/right_number/fact/hand.py, super/time/child/other_way_and_important_part.py, super/last_point/hand_and_great_world/person/government/public_fact.py and super/little_person_and_time/call_early_year/early_problem/different_part/early_hand/old_time.py by small life"

---
+++ super/last_point/hand_and_great_world/person/government/public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    problem(str_arg)
+    print('hand')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('find_way_by_eye')
---
+++ super/time/child/other_way_and_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    see_old_case(str_arg)
+    print('group')
+
+def see_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('tell_long_hand_from_eye')
