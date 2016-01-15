dd36c3e868b8b189c7c8ce6322e5e59dedb27333
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 13:26:27 -0500

    "mod: see super/eye_and_different_case/use_new_thing_at_hand/able_thing/be_woman_under_same_government/year/large_point.py in different problem"

---
+++ super/eye_and_different_case/use_new_thing_at_hand/able_thing/be_woman_under_same_government/year/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part(str_arg):
+    place(str_arg)
+    print('want_young_year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part('man')
