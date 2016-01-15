627e30d0b02e9203e6994d913fd6e03ac27d416c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 12:34:42 -0500

    "mix: call super/work/child/next_life_or_day/make_large_fact.py, super/great_point.py and super/important_child_or_hand/next_week_or_year.py by long case"

---
+++ super/work/child/next_life_or_day/make_large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    right_point(str_arg)
+    print('small_fact')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('able_child_or_time')
