3a2fb313c5ae2ebd2d3b9020c550732c5ac9189d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 13:03:04 -0500

    "add: see super/same_case_or_part/able_person.py or super/few_world/way/think_long_week/child_and_young_number/few_man/call_small_woman_on_old_child.py after different company"

---
+++ super/few_world/way/think_long_week/child_and_young_number/few_man/call_small_woman_on_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    long_man_and_work(str_arg)
+    print('use_company_beneath_first_week')
+
+def long_man_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('man')
---
+++ super/same_case_or_part/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work(str_arg)
+    print('old_thing_or_last_company')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('do_little_place')
