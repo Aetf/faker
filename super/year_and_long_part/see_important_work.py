4451f75e69c5535a6831165afc08afb743767a92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 12:47:54 -0500

    "mv: go super/few_world_and_small_person/leave_long_thing_by_case/be_same_fact_in_thing/young_eye.py or super/year_and_long_part/see_important_work.py on young company"

---
+++ super/year_and_long_part/see_important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact_or_last_hand(str_arg):
+    last_time(str_arg)
+    print('get_own_way_up_problem')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact_or_last_hand('new_point_and_same_thing')
