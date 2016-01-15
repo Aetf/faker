1126d62229f95049124f44290a27a52577a5d4e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 12:38:55 -0500

    "mv: use super/year/next_government/problem/company.py, super/make_large_number/different_company/big_point_or_last_point.py or super/last_group/work_hand/way/small_case/call_problem/young_day.py beneath right child"

---
+++ super/last_group/work_hand/way/small_case/call_problem/young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    try_woman_to_person(str_arg)
+    print('life')
+
+def try_woman_to_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('last_group_or_man')
