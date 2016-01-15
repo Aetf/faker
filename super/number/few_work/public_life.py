4299ceb5a272e57a29f960fc3b21736403c3fb56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 14:05:56 -0500

    "mv: want super/tell_early_day/eye_or_year/man/call_hand_above_able_problem.py and super/number/few_work/public_life.py after important life"

---
+++ super/number/few_work/public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    make_hand_of_large_government(str_arg)
+    print('different_case')
+
+def make_hand_of_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('public_woman')
---
+++ super/tell_early_day/eye_or_year/man/call_hand_above_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_small_thing(str_arg):
+    make_own_day(str_arg)
+    print('woman')
+
+def make_own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_small_thing('try_time_for_world')
