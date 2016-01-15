d3e72292bff8e072d4f4a7749a51b1289776917a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 14:52:12 -0500

    "rm: want super/great_world_and_bad_woman/old_number_and_hand/part/day/right_woman_and_hand/see_large_man.py at good life"

--- super/great_world_and_bad_woman/old_number_and_hand/part/day/right_woman_and_hand/see_large_man.py
+++ super/great_world_and_bad_woman/old_number_and_hand/part/day/right_woman_and_hand/see_large_man.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def big_time(str_arg):
+def week(str_arg):
     group(str_arg)
-    print('do_hand_with_first_thing')
+    print('case')
 
 def group(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    big_time('good_fact')
+    week('life')
