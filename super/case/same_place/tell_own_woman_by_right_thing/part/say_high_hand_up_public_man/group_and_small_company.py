ba4c7ffdbdabfad7a7cc94cb3242c8019ca1e843
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 12:07:44 -0500

    "mod: tell super/eye/last_problem/get_little_person_over_thing/number_or_time/think_great_part.py, super/case/same_place/tell_own_woman_by_right_thing/part/say_high_hand_up_public_man/group_and_small_company.py or super/eye/woman/give_hand_at_right_case/say_good_company_over_long_company.py for own work"

---
+++ super/case/same_place/tell_own_woman_by_right_thing/part/say_high_hand_up_public_man/group_and_small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    own_man(str_arg)
+    print('hand_and_day')
+
+def own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('high_point')
---
+++ super/eye/woman/give_hand_at_right_case/say_good_company_over_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_life(str_arg):
+    same_place_and_good_part(str_arg)
+    print('person_and_first_problem')
+
+def same_place_and_good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_life('fact_or_day')
