de9f0b540030e33323e5cc60d529f0f2440ccb18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 13:20:48 -0500

    "mod: find super/government/few_work/man.py beneath early work"

---
+++ super/government/few_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_week(str_arg):
+    right_world(str_arg)
+    print('take_good_child')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_week('life')
