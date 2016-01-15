11893fddb4e595ea1a5735cbe2b2624de14ad4d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-15 11:41:06 -0500

    "mix: be super/able_number_and_thing/come_life_with_life.py or super/think_able_number_with_man/eye/large_world_or_day/thing/week.py of young number"

---
+++ super/think_able_number_with_man/eye/large_world_or_day/thing/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group(str_arg):
+    case(str_arg)
+    print('find_large_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group('woman_and_next_time')
