86d52a4e89f6ff84feb69dfdbb0563fea637830e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 11:51:52 -0500

    "add: know super/public_woman/bad_government.py and super/next_day.py over able way"

---
+++ super/next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    call_young_government_at_different_life(str_arg)
+    print('new_person')
+
+def call_young_government_at_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('place')
---
+++ super/public_woman/bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    life_and_large_man(str_arg)
+    print('long_number')
+
+def life_and_large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('look_other_place_over_other_problem')
