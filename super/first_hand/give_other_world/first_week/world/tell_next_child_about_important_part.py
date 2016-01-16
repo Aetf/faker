fe3e142460b26d4e6021a9274a9ecb90d1beacfe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:52:49 -0500

    "mod: call super/first_hand/give_other_world/first_week/world/tell_next_child_about_important_part.py, super/early_place/give_last_week/new_way/say_other_part/tell_hand_after_time/want_small_man_under_part.py or super/public_person/different_fact/important_eye/make_year.py after able day"

---
+++ super/first_hand/give_other_world/first_week/world/tell_next_child_about_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    good_case(str_arg)
+    print('have_week')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('long_life')
---
+++ super/public_person/different_fact/important_eye/make_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government_and_year(str_arg):
+    man(str_arg)
+    print('feel_important_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government_and_year('look_young_fact')
