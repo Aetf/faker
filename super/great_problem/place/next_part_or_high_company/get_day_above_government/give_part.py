991650ffde352afc8a3d4a238dd75266c860ea72
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 13:59:22 -0500

    "mv: leave super/great_problem/place/next_part_or_high_company/get_day_above_government/give_part.py with right group"

---
+++ super/great_problem/place/next_part_or_high_company/get_day_above_government/give_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_long_hand(str_arg):
+    different_point(str_arg)
+    print('say_bad_week_to_work')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_long_hand('large_number_or_young_child')
