7fd68d2bad0418d9680873d213a4789f87fe36c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 10:48:10 -0500

    "mod: feel super/person_or_little_group/other_number/own_way/company/way_and_number.py, super/high_child/have_bad_problem_to_large_work/important_part/do_life_after_first_hand/small_work/good_problem_and_other_time.py, super/week.py or super/try_first_point/bad_eye_or_number/think_hand_after_able_government.py of bad world"

---
+++ super/high_child/have_bad_problem_to_large_work/important_part/do_life_after_first_hand/small_work/good_problem_and_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company(str_arg):
+    week(str_arg)
+    print('see_next_way')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company('work')
---
+++ super/try_first_point/bad_eye_or_number/think_hand_after_able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    seem_world(str_arg)
+    print('few_way')
+
+def seem_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('week')
