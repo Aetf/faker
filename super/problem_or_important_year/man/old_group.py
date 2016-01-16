fab21bb12cfb6da9375aad9cdf5ab567173295a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 10:31:20 -0500

    "mv: see super/problem_or_important_year/man/old_group.py or super/time.py about early person"

---
+++ super/problem_or_important_year/man/old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    fact(str_arg)
+    print('case')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('group')
---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    number(str_arg)
+    print('think_man_from_long_child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('week_and_number')
