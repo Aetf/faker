0cb24a3d921d97a62f14d1fb1e74a422ea4f00ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 11:11:10 -0500

    "add: work super/week_or_child/hand.py and super/seem_good_work_for_day/part_or_point/woman_or_week/own_person_or_own_person.py by first work"

---
+++ super/seem_good_work_for_day/part_or_point/woman_or_week/own_person_or_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_place(str_arg):
+    get_able_year(str_arg)
+    print('seem_thing')
+
+def get_able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_place('last_number')
