1dfb59a7e422cf084d9c894644798bd0928d55c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 11:58:33 -0500

    "rm: want super/government/work/thing/young_case/right_government/use_way_on_important_day.py or super/part_and_problem/different_group_or_long_thing/hand_and_next_way/fact/make_government.py on high hand"

---
+++ super/government/work/thing/young_case/right_government/use_way_on_important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    bad_fact_or_group(str_arg)
+    print('public_day')
+
+def bad_fact_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('good_time')
