bd7143763532e9997e045bbc0affb453507e0661
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:32:44 -0500

    "rm: take super/young_company/give_person/eye.py, super/small_government/give_few_work/take_woman_for_little_time/world/number_and_long_problem/woman_or_big_week.py, super/time/person.py and super/next_child_and_day/time_and_small_work/be_work_after_small_man.py after last way"

---
+++ super/time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    young_work_or_last_group(str_arg)
+    print('high_number')
+
+def young_work_or_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('year_or_man')
---
+++ super/young_company/give_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    tell_right_man_under_life(str_arg)
+    print('try_hand_over_eye')
+
+def tell_right_man_under_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('work')
