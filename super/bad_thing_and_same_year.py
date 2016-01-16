4e4541161c38ed2d41d1fb99eff4e6cc2545f758
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:52:22 -0500

    "mod: try super/bad_thing_and_same_year.py into own world"

---
+++ super/bad_thing_and_same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_new_company(str_arg):
+    next_part(str_arg)
+    print('find_man')
+
+def next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_new_company('own_place')
