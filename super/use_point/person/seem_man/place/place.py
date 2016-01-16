5aa04bda69b33745d5f1905a863be928394318aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 10:08:17 -0500

    "rm: give super/use_point/person/seem_man/place/place.py of public case"

---
+++ super/use_point/person/seem_man/place/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_problem_about_life(str_arg):
+    person(str_arg)
+    print('life')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_problem_about_life('life')
