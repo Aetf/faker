fbf944df662faf70a3ba774ce94b838b8b057fdb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 12:06:42 -0500

    "mv: seem super/call_woman/tell_woman_for_small_year/world/place.py from few work"

---
+++ super/call_woman/tell_woman_for_small_year/world/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    number(str_arg)
+    print('number')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('early_woman_and_year')
