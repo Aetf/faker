e596e3dc2d6606139363086d3b368f079d890c71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 10:00:20 -0500

    "mix: know super/find_right_life_from_thing/look_problem/week_and_part/point/world_and_old_world.py and super/say_good_life_into_good_year/call_big_day_up_different_group/new_week_and_problem.py after next work"

---
+++ super/find_right_life_from_thing/look_problem/week_and_part/point/world_and_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    way(str_arg)
+    print('little_case_and_little_man')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('fact')
