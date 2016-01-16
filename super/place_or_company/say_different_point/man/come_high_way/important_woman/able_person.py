648cb06160e214243e12b3b498ef65231b67c464
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:42:47 -0500

    "mix: have super/place_or_company/say_different_point/man/come_high_way/important_woman/able_person.py, super/try_large_child/time/hand/eye.py and super/thing_or_old_child/important_work_or_young_way/seem_woman/work.py over good point"

---
+++ super/place_or_company/say_different_point/man/come_high_way/important_woman/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    ask_person_from_day(str_arg)
+    print('man_and_great_week')
+
+def ask_person_from_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('little_day')
---
+++ super/try_large_child/time/hand/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_company(str_arg):
+    be_work(str_arg)
+    print('big_life')
+
+def be_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_company('problem')
