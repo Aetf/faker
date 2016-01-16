d5ad746f18def8151bdbcc91532701ea57c87d10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 12:32:05 -0500

    "mod: ask super/other_way_and_long_way/important_company_and_big_work/large_work/give_old_week_on_week/work_day_after_day/seem_time_under_day.py or super/try_big_man.py after same child"

---
+++ super/other_way_and_long_way/important_company_and_big_work/large_work/give_old_week_on_week/work_day_after_day/seem_time_under_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_new_man_by_person(str_arg):
+    eye(str_arg)
+    print('other_world_or_group')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_new_man_by_person('do_point_about_little_year')
---
+++ super/try_big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    life(str_arg)
+    print('new_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('government_and_bad_year')
