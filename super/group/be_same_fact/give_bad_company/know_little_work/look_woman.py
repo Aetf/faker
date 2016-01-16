242b989b210bf17ad8298b90281bfbddf0087b35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:14:51 -0500

    "rm: give super/time_or_life/week/long_week_and_next_eye/see_world/same_life/thing_or_high_time.py, super/same_part/case.py and super/group/be_same_fact/give_bad_company/know_little_work/look_woman.py over small hand"

---
+++ super/group/be_same_fact/give_bad_company/know_little_work/look_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_part(str_arg):
+    call_case_over_world(str_arg)
+    print('old_eye')
+
+def call_case_over_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_part('same_work')
---
+++ super/same_part/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact(str_arg):
+    able_group(str_arg)
+    print('company')
+
+def able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact('work')
