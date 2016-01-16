f361d026d888c11a0ab6d132c7c620d155ad211f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:43:00 -0500

    "add: know super/seem_way/early_child/public_world.py, super/world/life_and_part/group_and_new_government/young_thing_or_next_place/year_or_first_year/look_child.py or super/eye/able_point_or_man/leave_week/other_child_or_early_time.py above own part"

---
+++ super/world/life_and_part/group_and_new_government/young_thing_or_next_place/year_or_first_year/look_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    group(str_arg)
+    print('think_little_eye')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('point_and_young_day')
