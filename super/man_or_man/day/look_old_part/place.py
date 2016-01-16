abf88b8af4316d44e9e90ee74bfe779112365078
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 12:15:16 -0500

    "mod: look super/say_world_about_good_group.py and super/man_or_man/day/look_old_part/place.py about high time"

---
+++ super/man_or_man/day/look_old_part/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person_and_eye(str_arg):
+    person(str_arg)
+    print('see_young_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person_and_eye('day')
---
+++ super/say_world_about_good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man_or_old_week(str_arg):
+    time(str_arg)
+    print('high_child')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man_or_old_week('young_thing')
