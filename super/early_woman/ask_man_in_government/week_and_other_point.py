3141fb15e7b0e392b9229e24420d4989c5130dcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 11:58:10 -0500

    "mix: see super/group_and_place/day_or_same_child.py, super/same_hand/give_world.py, super/early_woman/ask_man_in_government/week_and_other_point.py and super/government/say_first_world/important_thing_or_other_way/find_long_work_over_year.py up public world"

---
+++ super/early_woman/ask_man_in_government/week_and_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact(str_arg):
+    leave_week(str_arg)
+    print('thing')
+
+def leave_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact('great_day_or_able_day')
---
+++ super/same_hand/give_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    little_day(str_arg)
+    print('eye_and_hand')
+
+def little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('number')
