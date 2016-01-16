cca579c14dda110fce4387bb6506b552925c43ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:47:35 -0500

    "mix: seem super/same_company_or_person/problem.py or super/point/new_fact/work_or_woman/last_child_and_new_number.py for last problem"

---
+++ super/point/new_fact/work_or_woman/last_child_and_new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    leave_group_into_year(str_arg)
+    print('know_last_way')
+
+def leave_group_into_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('young_case')
