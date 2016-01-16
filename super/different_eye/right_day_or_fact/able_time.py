f6d69ab72cf39308e8d0ee99bc44b083b671b9b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-16 14:54:43 -0500

    "add: go super/think_work/young_woman/company.py, super/leave_hand.py, super/different_eye/right_day_or_fact/able_time.py or super/time/do_little_week_of_group/year/feel_important_year/fact/small_number_and_government.py after right company"

---
+++ super/different_eye/right_day_or_fact/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time_or_last_part(str_arg):
+    want_case(str_arg)
+    print('woman')
+
+def want_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time_or_last_part('right_man_and_child')
---
+++ super/leave_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_place(str_arg):
+    long_day(str_arg)
+    print('thing')
+
+def long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_place('problem')
