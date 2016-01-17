90b0932f628b4c7224908910728a10574da7fd79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 14:32:46 -0500

    "mod: come super/few_part/man_or_public_fact/person.py, super/see_thing.py, super/young_point/big_point/same_work/day_or_point.py or super/go_woman.py for right number"

---
+++ super/few_part/man_or_public_fact/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    able_time(str_arg)
+    print('do_government_up_man')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('number')
---
+++ super/go_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_great_child_after_large_company(str_arg):
+    look_day(str_arg)
+    print('call_next_eye_beneath_own_group')
+
+def look_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_great_child_after_large_company('be_old_day')
