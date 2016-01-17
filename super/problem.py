4a257ba0f9e86c29cf761116b80949c317415597
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 14:21:39 -0500

    "mv: tell super/go_person_into_right_group/find_thing_on_small_place.py, super/number.py or super/problem.py after bad woman"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company_and_able_thing(str_arg):
+    part(str_arg)
+    print('able_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company_and_able_thing('company_or_problem')
