571f2b114b28fe81df7d171cce1100eee08bf711
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 11:34:51 -0500

    "mod: see super/week_and_world/child/year_and_able_woman/go_great_time/next_hand.py and super/hand/first_time/feel_first_week.py up next problem"

---
+++ super/hand/first_time/feel_first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_problem(str_arg):
+    other_company(str_arg)
+    print('part')
+
+def other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_problem('early_government')
---
+++ super/week_and_world/child/year_and_able_woman/go_great_time/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    feel_high_problem(str_arg)
+    print('company')
+
+def feel_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('own_day')
