e1c436369ecf1506d320a046d8f0c89f6f29b78a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 10:35:47 -0500

    "mod: think super/next_week_and_eye/high_woman/child/work.py and super/good_hand/woman/leave_year_beneath_first_government/right_part.py under different place"

---
+++ super/next_week_and_eye/high_woman/child/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_little_person(str_arg):
+    same_part_and_way(str_arg)
+    print('person_or_group')
+
+def same_part_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_little_person('world')
