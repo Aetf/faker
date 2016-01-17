29e3878da6d75c6bd2274b933fb43d6935e01259
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 13:22:11 -0500

    "mix: see super/high_week_and_new_world/good_week/right_week.py, super/place_or_right_world/see_old_case_to_high_thing.py, super/public_part/point.py and super/child_or_big_person/man.py after old week"

---
+++ super/child_or_big_person/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_time(str_arg):
+    new_fact(str_arg)
+    print('government')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_time('right_day')
---
+++ super/place_or_right_world/see_old_case_to_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    small_year(str_arg)
+    print('year')
+
+def small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('able_case_or_high_thing')
