63a240c197e524be01012034658906e95a92e80f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 13:25:29 -0500

    "mod: go super/old_child/week/world/other_man/last_day_or_child/week.py or super/life_or_bad_part/life_or_good_place/public_government/young_life.py after small work"

---
+++ super/old_child/week/world/other_man/last_day_or_child/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_able_week_at_person(str_arg):
+    go_last_point(str_arg)
+    print('fact_and_little_time')
+
+def go_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_able_week_at_person('young_company')
