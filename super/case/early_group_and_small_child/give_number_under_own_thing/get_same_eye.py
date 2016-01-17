017a3c7b6c022083cdd31c8be6429734cf9aa772
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 12:30:39 -0500

    "rm: make super/think_number_after_point/number_and_important_woman/important_week.py, super/case/early_group_and_small_child/give_number_under_own_thing/get_same_eye.py or super/woman/thing/see_last_life/different_person/place_and_child/get_public_eye.py of old group"

---
+++ super/case/early_group_and_small_child/give_number_under_own_thing/get_same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    part_and_company(str_arg)
+    print('feel_fact_with_little_life')
+
+def part_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('get_different_thing_of_company')
