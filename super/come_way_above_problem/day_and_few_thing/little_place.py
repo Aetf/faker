b4e814756263120551f7996792aa3af0f713cf1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 12:41:41 -0500

    "mv: think super/come_way_above_problem/day_and_few_thing/little_place.py, super/person_or_little_year/take_government_about_work.py and super/good_year_and_other_man/take_young_problem/want_high_hand_in_problem/problem.py for young woman"

---
+++ super/come_way_above_problem/day_and_few_thing/little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_big_life(str_arg):
+    great_case(str_arg)
+    print('child_and_able_problem')
+
+def great_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_big_life('big_way_and_fact')
---
+++ super/person_or_little_year/take_government_about_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    call_life_with_early_day(str_arg)
+    print('next_life_or_week')
+
+def call_life_with_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('life')
