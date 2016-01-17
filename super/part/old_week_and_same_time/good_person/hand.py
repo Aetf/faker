1aec18205bbcc61d23083b9e9355f6184ace51a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 12:57:32 -0500

    "mix: give super/part/old_week_and_same_time/good_person/hand.py or super/know_good_company/small_man_or_problem/day/woman_and_little_case.py in early problem"

---
+++ super/part/old_week_and_same_time/good_person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_woman(str_arg):
+    work_and_right_work(str_arg)
+    print('old_number')
+
+def work_and_right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_woman('good_hand')
