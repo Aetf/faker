88686a054079f3486ec91fb2a8c4ca28ec69732c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 13:43:10 -0500

    "mv: see super/person_and_next_fact/case_and_case/long_case/work_different_week/tell_group.py and super/woman/world_and_large_point.py after next place"

---
+++ super/woman/world_and_large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year(str_arg):
+    old_person(str_arg)
+    print('week_and_case')
+
+def old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year('time')
