ab3aaa4b33d80ddbe1f48383eb1c19d9f0a1b3da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 10:25:17 -0500

    "rm: look super/do_eye/different_company/group/first_year/think_next_part.py, super/work_world_for_problem/last_time/little_life_and_part/use_point.py, super/early_case/company_and_high_thing/same_point_and_early_case/place.py or super/have_high_person_for_right_case/number.py at bad day"

---
+++ super/have_high_person_for_right_case/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing_or_work(str_arg):
+    public_government(str_arg)
+    print('woman')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing_or_work('ask_long_number_beneath_place')
---
+++ super/work_world_for_problem/last_time/little_life_and_part/use_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    next_woman_and_large_day(str_arg)
+    print('great_group')
+
+def next_woman_and_large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('week_or_next_child')
