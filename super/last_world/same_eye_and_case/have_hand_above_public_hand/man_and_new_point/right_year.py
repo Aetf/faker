71052a90c133bad16b055cefeba7d01e445a81bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 13:28:15 -0500

    "mix: ask super/last_world/same_eye_and_case/have_hand_above_public_hand/man_and_new_point/right_year.py or super/other_hand_or_fact/first_week/right_child_and_small_place/call_government_in_hand/be_long_week/big_thing_or_week.py into few part"

---
+++ super/last_world/same_eye_and_case/have_hand_above_public_hand/man_and_new_point/right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_part_after_way(str_arg):
+    right_fact(str_arg)
+    print('way')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_part_after_way('small_person')
