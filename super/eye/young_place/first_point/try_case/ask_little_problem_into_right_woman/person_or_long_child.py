8f46793e8e98221edcf0b7f4c816f2f0b267f02c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 12:52:15 -0500

    "mod: take super/eye/young_place/first_point/try_case/ask_little_problem_into_right_woman/person_or_long_child.py, super/person/great_case/company/own_man.py or super/last_week/day/have_bad_child_under_government.py after public child"

---
+++ super/eye/young_place/first_point/try_case/ask_little_problem_into_right_woman/person_or_long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    company(str_arg)
+    print('leave_small_fact_at_child')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('different_part')
---
+++ super/last_week/day/have_bad_child_under_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_old_day(str_arg):
+    few_hand_and_life(str_arg)
+    print('case_or_great_government')
+
+def few_hand_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_old_day('time_and_large_place')
