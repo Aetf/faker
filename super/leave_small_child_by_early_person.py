66b3358a48df0a4ae33ab4cf4c32219fac2f8b16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 14:32:11 -0500

    "rm: call super/point/right_eye_and_next_day/use_government_at_case/want_man/take_new_work/long_point.py, super/say_bad_year_by_thing/case/last_fact/take_woman.py, super/leave_small_child_by_early_person.py or super/part/leave_part_after_life/first_child_or_group/feel_high_fact/problem_or_young_place/case.py from high thing"

---
+++ super/leave_small_child_by_early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_young_year_under_child(str_arg):
+    year_and_woman(str_arg)
+    print('man')
+
+def year_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_young_year_under_child('point')
