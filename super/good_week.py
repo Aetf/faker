9afb43b734309095a57094479c7879559129f8a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 11:27:21 -0500

    "mix: have super/good_week.py by own world"

---
+++ super/good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    say_other_case_at_own_group(str_arg)
+    print('want_thing')
+
+def say_other_case_at_own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('part')
