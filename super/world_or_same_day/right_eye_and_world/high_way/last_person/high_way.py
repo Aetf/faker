d329da1cf8e738aecdea477bbbd578dde4bbdecb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-17 10:35:37 -0500

    "mix: want super/world_or_same_day/right_eye_and_world/high_way/last_person/high_way.py and super/part/tell_week_about_thing.py from first time"

---
+++ super/world_or_same_day/right_eye_and_world/high_way/last_person/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_last_work(str_arg):
+    give_able_thing(str_arg)
+    print('own_way')
+
+def give_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_last_work('high_number')
