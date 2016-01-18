88f00c959d68e6b2ec9d5842b2b6f939f7c77fdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 11:27:31 -0500

    "mv: know super/see_way_up_important_number/thing/government_and_same_hand.py, super/day_or_right_woman/little_child_and_different_point/give_work_from_place/high_man/call_public_thing_after_way/woman.py and super/be_high_work/new_case_or_few_fact/high_fact.py from next part"

---
+++ super/be_high_work/new_case_or_few_fact/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_place(str_arg):
+    long_part(str_arg)
+    print('man')
+
+def long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_place('hand')
