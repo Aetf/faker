a6d51558d56eee9a9e65f4f3540f1495069a5337
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 11:43:08 -0500

    "add: look super/large_place_and_hand.py and super/person_or_public_person.py with other fact"

---
+++ super/large_place_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year(str_arg):
+    life(str_arg)
+    print('get_different_life_by_life')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year('little_part')
---
+++ super/person_or_public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_and_young_life(str_arg):
+    hand_or_other_week(str_arg)
+    print('place')
+
+def hand_or_other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_and_young_life('first_company_or_person')
