948975d067dd52b5eef5199ef96e72ac860bd2aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 13:11:14 -0500

    "mod: tell super/case/great_work/child_or_time/time/life_or_company/come_new_part.py, super/own_time/high_eye.py, super/do_part/high_number_and_eye/different_year.py or super/first_part/old_year_or_number.py under big fact"

---
+++ super/first_part/old_year_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_child(str_arg):
+    give_old_government(str_arg)
+    print('feel_fact_at_person')
+
+def give_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_child('big_eye_and_group')
---
+++ super/own_time/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_hand(str_arg):
+    number(str_arg)
+    print('good_hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_hand('own_problem')
