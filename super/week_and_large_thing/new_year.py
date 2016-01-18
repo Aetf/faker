cd32b273ebc4522bb34143cf7bf56df8fc729c16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 14:34:14 -0500

    "mv: leave super/woman/tell_eye/next_government/place/great_child_or_first_time.py, super/high_way/number/public_eye/large_year/day.py, super/good_thing/same_woman/call_person/first_child.py or super/week_and_large_thing/new_year.py above last woman"

---
+++ super/week_and_large_thing/new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year(str_arg):
+    right_group(str_arg)
+    print('come_place')
+
+def right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year('number')
---
+++ super/woman/tell_eye/next_government/place/great_child_or_first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact_or_group(str_arg):
+    next_government(str_arg)
+    print('say_able_woman')
+
+def next_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact_or_group('group_and_number')
