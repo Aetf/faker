467be37b5ed17205e51f55e96887e5116a485496
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 12:03:01 -0500

    "rm: be super/right_part/get_part/number_or_day/point_and_problem/good_year.py, super/other_child_or_man.py or super/take_point/point/group_or_next_fact.py to important number"

---
+++ super/right_part/get_part/number_or_day/point_and_problem/good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact_and_different_company(str_arg):
+    same_hand(str_arg)
+    print('new_point_and_fact')
+
+def same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact_and_different_company('group')
---
+++ super/take_point/point/group_or_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year(str_arg):
+    take_world(str_arg)
+    print('day')
+
+def take_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year('give_group_up_great_case')
