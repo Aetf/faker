899889857631936ab36e441d1649b8236654d3bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 12:12:01 -0500

    "add: come super/good_number_and_other_child.py, super/make_woman_from_child/number/find_fact_in_same_number/big_year/time.py or super/know_day/different_life_or_small_week.py from different group"

---
+++ super/know_day/different_life_or_small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    different_case(str_arg)
+    print('fact')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('man')
---
+++ super/make_woman_from_child/number/find_fact_in_same_number/big_year/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    leave_hand(str_arg)
+    print('great_world')
+
+def leave_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('high_problem')
