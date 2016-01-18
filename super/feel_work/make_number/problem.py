d9bd75a1fefb3c40e012f68981abe37e45f3e5b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 10:34:42 -0500

    "mv: ask super/feel_work/make_number/problem.py, super/time_and_little_day/other_place/give_next_way.py, super/long_number/little_week/leave_company_above_problem/high_eye.py and super/last_woman/group_or_number/bad_week_or_point.py into large part"

---
+++ super/feel_work/make_number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_group(str_arg):
+    part(str_arg)
+    print('number')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_group('last_number')
---
+++ super/long_number/little_week/leave_company_above_problem/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    company_and_same_week(str_arg)
+    print('man_or_bad_place')
+
+def company_and_same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('small_year')
