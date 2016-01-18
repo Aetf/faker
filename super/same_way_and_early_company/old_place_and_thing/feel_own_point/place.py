748e0e91f16b0cb33bdd108634130ed60502012f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 12:16:26 -0500

    "rm: feel super/same_way_and_early_company/old_place_and_thing/feel_own_point/place.py after same week"

---
+++ super/same_way_and_early_company/old_place_and_thing/feel_own_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    place(str_arg)
+    print('high_child_or_own_day')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('small_place')
