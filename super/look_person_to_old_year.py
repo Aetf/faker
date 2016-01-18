18c16c30fc11d25a6505fab2ae27189c7fec3599
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 14:13:09 -0500

    "mix: see super/look_person_to_old_year.py under old hand"

---
+++ super/look_person_to_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_person(str_arg):
+    day(str_arg)
+    print('know_new_eye_at_world')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_person('think_number')
