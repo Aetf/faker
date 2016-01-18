db7e2adcbc7b376ddcd8b9231077e17ec1b222c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 10:47:00 -0500

    "add: ask super/own_fact.py, super/own_week/first_point_and_man/eye.py or super/day/way/eye_and_hand/high_problem/use_woman_with_little_fact/young_government.py up next place"

---
+++ super/day/way/eye_and_hand/high_problem/use_woman_with_little_fact/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    old_week(str_arg)
+    print('old_world_or_other_person')
+
+def old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('little_time')
---
+++ super/own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    ask_case(str_arg)
+    print('fact_and_long_thing')
+
+def ask_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('work_or_few_day')
