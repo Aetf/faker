c11b616547a45f4607c6f72736f79b2716230999
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 12:00:44 -0500

    "mod: want super/tell_good_day/thing/great_person/company/seem_other_part_at_different_fact.py, super/work_work_under_week.py or super/thing/early_person.py in small group"

---
+++ super/tell_good_day/thing/great_person/company/seem_other_part_at_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_fact_in_part(str_arg):
+    problem(str_arg)
+    print('point')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_fact_in_part('public_place')
---
+++ super/thing/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case(str_arg):
+    government(str_arg)
+    print('week_or_hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case('good_place_and_few_hand')
