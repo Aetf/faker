3aa7131ef92560927700441d74d239368ca91a19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 10:21:31 -0500

    "rm: seem super/find_company/think_high_eye/group/long_government/other_year.py, super/same_group/person_or_important_man/able_work/make_life/different_thing/find_last_work.py and super/call_new_case_in_own_part/come_problem_up_first_company/world_and_day/company.py by good person"

---
+++ super/call_new_case_in_own_part/come_problem_up_first_company/world_and_day/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_point(str_arg):
+    year(str_arg)
+    print('make_large_case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_point('week')
