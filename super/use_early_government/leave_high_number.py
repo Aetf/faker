c848334e31ebed4241637b8de210ea2a4e03cff0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 11:29:57 -0500

    "rm: get super/number/world/problem.py, super/use_early_government/leave_high_number.py, super/go_child.py and super/call_large_case_to_next_group/leave_own_number_after_way/able_way/find_point_into_little_work/world.py from long thing"

---
+++ super/use_early_government/leave_high_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    have_few_number_under_problem(str_arg)
+    print('problem_and_man')
+
+def have_few_number_under_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('bad_case')
