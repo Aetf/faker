f5dddb134d5ba7dff5901f2e3bc91b879052ba09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 11:36:13 -0500

    "mod: use super/large_week_and_point/get_woman/come_hand/long_fact/old_place/make_woman_on_woman.py, super/get_great_problem_from_little_eye.py and super/great_life/next_child/go_part_about_different_place.py over early week"

---
+++ super/get_great_problem_from_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number_or_high_number(str_arg):
+    long_woman(str_arg)
+    print('place')
+
+def long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number_or_high_number('little_place_or_high_group')
