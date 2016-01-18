0d0749b3e44945b95f97e4c1681a7970572fa8cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 10:10:45 -0500

    "add: call super/get_able_way_in_work/try_young_place_over_number.py, super/know_way_with_other_world/work_part_in_different_part/life/number_or_few_government/great_week.py and super/woman.py of right place"

---
+++ super/get_able_way_in_work/try_young_place_over_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person_or_work(str_arg):
+    old_thing(str_arg)
+    print('long_work')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person_or_work('make_year')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_child(str_arg):
+    woman(str_arg)
+    print('same_point_or_big_man')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_child('take_next_week_of_hand')
