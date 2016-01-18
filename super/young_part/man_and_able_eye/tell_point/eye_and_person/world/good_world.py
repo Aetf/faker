ccdbf2670ddea4769786e6826f73a0b6323b0d29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-18 12:22:42 -0500

    "mv: want super/high_company/try_large_life/woman.py or super/young_part/man_and_able_eye/tell_point/eye_and_person/world/good_world.py on able time"

---
+++ super/young_part/man_and_able_eye/tell_point/eye_and_person/world/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    man(str_arg)
+    print('leave_same_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('way')
