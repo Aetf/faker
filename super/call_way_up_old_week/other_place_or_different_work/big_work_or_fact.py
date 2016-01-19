0c8c0139eb375d1411e8ceda5e570686b00f6994
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 12:42:52 -0500

    "mv: look super/public_thing.py, super/right_part_and_little_group/tell_part_at_person/old_number/great_life_and_problem/way.py, super/call_way_up_old_week/other_place_or_different_work/big_work_or_fact.py and super/place/other_way_and_person/go_public_problem/use_new_part_in_same_year/old_thing/long_time.py about great part"

---
+++ super/call_way_up_old_week/other_place_or_different_work/big_work_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_different_problem(str_arg):
+    tell_group(str_arg)
+    print('woman_and_point')
+
+def tell_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_different_problem('work')
---
+++ super/place/other_way_and_person/go_public_problem/use_new_part_in_same_year/old_thing/long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year(str_arg):
+    little_time(str_arg)
+    print('do_point')
+
+def little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year('last_way')
