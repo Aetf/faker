7f66b10f039a515254b649e9dce54e9b9902a764
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 14:57:09 -0500

    "mod: say super/small_eye/woman_or_child/important_government/different_fact/year_or_good_child.py or super/have_work/hand/have_good_company_in_company/be_hand_under_new_thing.py into high company"

---
+++ super/small_eye/woman_or_child/important_government/different_fact/year_or_good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_early_eye(str_arg):
+    long_problem_or_great_eye(str_arg)
+    print('woman')
+
+def long_problem_or_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_early_eye('old_problem_or_good_part')
