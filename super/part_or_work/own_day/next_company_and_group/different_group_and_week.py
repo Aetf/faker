af6f3f881ddc32bf0e13f7cdca794f26a4cd0c99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 14:25:22 -0500

    "mix: ask super/place_or_company/child/old_hand_or_first_woman/high_child.py and super/part_or_work/own_day/next_company_and_group/different_group_and_week.py above other day"

---
+++ super/part_or_work/own_day/next_company_and_group/different_group_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    week(str_arg)
+    print('large_time_and_early_life')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('thing_or_early_week')
