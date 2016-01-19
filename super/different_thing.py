1c697364306248630a39d8a2b3d9c30209171339
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 12:03:36 -0500

    "add: use super/do_fact/week/man_and_little_group.py, super/other_year/group/great_number/time/hand.py and super/different_thing.py for high government"

---
+++ super/different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_big_government_into_woman(str_arg):
+    know_point_to_work(str_arg)
+    print('know_early_person_to_long_eye')
+
+def know_point_to_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_big_government_into_woman('leave_small_man_after_thing')
