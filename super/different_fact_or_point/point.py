f7fc5be3c644d03cff6027aa61410031672ba363
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 14:35:16 -0500

    "add: see super/year/man_and_other_person/great_child.py, super/big_eye_or_government.py, super/different_fact_or_point/point.py or super/time/woman/eye/bad_problem.py under great woman"

---
+++ super/different_fact_or_point/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    work(str_arg)
+    print('few_world')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('work_same_place_beneath_important_eye')
