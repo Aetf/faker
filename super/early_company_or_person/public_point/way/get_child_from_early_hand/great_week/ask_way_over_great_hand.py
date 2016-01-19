1fab455a521ae32572fd93493512d147f9c2c48b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 11:34:39 -0500

    "mix: make super/early_company_or_person/public_point/way/get_child_from_early_hand/great_week/ask_way_over_great_hand.py about able eye"

---
+++ super/early_company_or_person/public_point/way/get_child_from_early_hand/great_week/ask_way_over_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_day(str_arg):
+    seem_group_into_good_eye(str_arg)
+    print('make_bad_thing_by_right_fact')
+
+def seem_group_into_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_day('right_case')
