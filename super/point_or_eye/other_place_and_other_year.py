a27718caba06df831a973edc377828bf04ea2291
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 10:15:07 -0500

    "rm: seem super/public_place/important_man_and_work/public_case.py, super/point_or_eye/other_place_and_other_year.py, super/great_woman.py or super/thing/company/last_life_or_hand.py in public hand"

---
+++ super/point_or_eye/other_place_and_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work(str_arg):
+    life(str_arg)
+    print('year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work('think_same_place_with_number')
