b751989e4a010930c5e7ce19ec6fcc71baa20f76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 11:11:17 -0500

    "mv: tell super/able_hand_and_time/want_hand_in_time.py up large case"

---
+++ super/able_hand_and_time/want_hand_in_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_good_hand(str_arg):
+    week(str_arg)
+    print('different_work_and_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_good_hand('look_first_hand_after_thing')
