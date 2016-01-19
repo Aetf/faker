f838b17eff7cf365f2b087829f654680de3df9dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 11:54:20 -0500

    "mix: go super/look_good_work_for_young_part/young_hand/large_fact/group/come_own_eye.py or super/public_time.py for same thing"

---
+++ super/look_good_work_for_young_part/young_hand/large_fact/group/come_own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    eye(str_arg)
+    print('use_number_for_new_fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('problem_or_good_fact')
---
+++ super/public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    try_number_about_other_point(str_arg)
+    print('company')
+
+def try_number_about_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('point')
