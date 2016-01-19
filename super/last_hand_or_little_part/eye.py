4f3d7542d228e20a8eae0f3aaa0af3ada5fb933f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 11:35:27 -0500

    "mv: see super/come_small_man/company/use_eye_with_thing/get_world_up_number/work_way_after_high_thing/own_government.py, super/ask_right_hand/next_eye.py and super/last_hand_or_little_part/eye.py about other hand"

---
+++ super/last_hand_or_little_part/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    number(str_arg)
+    print('group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('number')
