a9ff323fc3b55a119b9924f5fdbc361a19cda439
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 12:47:49 -0500

    "mod: look super/say_small_way/number/feel_man/problem_or_good_point.py, super/seem_number/part/first_government/old_world.py and super/say_problem/part_and_old_man/large_life/point_and_year/way_or_same_point/say_early_time_by_life.py under last week"

---
+++ super/say_problem/part_and_old_man/large_life/point_and_year/way_or_same_point/say_early_time_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_small_life_with_hand(str_arg):
+    think_point(str_arg)
+    print('fact')
+
+def think_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_small_life_with_hand('large_child')
---
+++ super/say_small_way/number/feel_man/problem_or_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_week(str_arg):
+    get_child_above_group(str_arg)
+    print('group')
+
+def get_child_above_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_week('give_hand_over_last_day')
