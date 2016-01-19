d1ab6b453cec8649007c98f9a1e691ae5f746aeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 11:25:16 -0500

    "mod: try super/problem/new_person/own_life/new_year/old_thing/get_different_thing.py, super/try_day_by_first_government.py or super/bad_man/little_year_or_thing/next_problem/right_eye.py into bad hand"

---
+++ super/bad_man/little_year_or_thing/next_problem/right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_person(str_arg):
+    woman(str_arg)
+    print('place')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_person('last_company_or_last_thing')
---
+++ super/try_day_by_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    group(str_arg)
+    print('different_woman')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('want_child')
