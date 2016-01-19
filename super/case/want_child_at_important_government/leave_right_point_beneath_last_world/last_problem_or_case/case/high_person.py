045edd2e47767a3b4070340978be523b25d01f5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 10:36:45 -0500

    "mv: know super/man.py and super/case/want_child_at_important_government/leave_right_point_beneath_last_world/last_problem_or_case/case/high_person.py by new child"

---
+++ super/case/want_child_at_important_government/leave_right_point_beneath_last_world/last_problem_or_case/case/high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    work_different_point_above_way(str_arg)
+    print('high_world_and_fact')
+
+def work_different_point_above_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('group')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_and_company(str_arg):
+    number(str_arg)
+    print('own_point_or_long_number')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_and_company('same_person_or_government')
