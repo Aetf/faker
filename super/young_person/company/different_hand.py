e8552f8176c6b448d79582042a2a319c4fb01172
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 10:14:24 -0500

    "mv: have super/young_person/company/different_hand.py under able hand"

---
+++ super/young_person/company/different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    same_place(str_arg)
+    print('time')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('fact_or_number')
