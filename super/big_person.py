8c19a2c2e8e5708fa23bd11e94f32752682b4eb0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 14:50:15 -0500

    "mod: call super/look_old_week_in_new_thing/say_number/work/hand/woman/ask_time_with_early_day.py and super/big_person.py by important woman"

---
+++ super/big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_public_eye(str_arg):
+    early_company(str_arg)
+    print('seem_good_hand')
+
+def early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_public_eye('government')
---
+++ super/look_old_week_in_new_thing/say_number/work/hand/woman/ask_time_with_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    think_point(str_arg)
+    print('know_place')
+
+def think_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('have_old_point_under_company')
