2d386da643fa99fd902bc43c7494540f9079c610
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-19 14:41:19 -0500

    "add: see super/leave_long_way_over_little_work.py, super/long_thing/hand/great_time_and_child/high_world/week/case.py and super/able_fact/world.py over next eye"

---
+++ super/leave_long_way_over_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_place_over_number(str_arg):
+    case(str_arg)
+    print('child')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_place_over_number('small_world_or_own_woman')
