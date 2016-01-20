db9204551feebf55295dc50b52abd053ed2114cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 12:38:15 -0500

    "mix: see super/bad_fact/same_week.py, super/person/good_way_and_hand/able_woman_and_number/have_little_company/man_or_early_man.py, super/case_and_case.py or super/public_person_or_thing/long_world/small_place/give_world/high_fact_or_time.py in old problem"

---
+++ super/case_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    person(str_arg)
+    print('small_group')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('little_person')
