e228755554e1886834019547fa2e9b2ef56810d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 13:10:56 -0500

    "mod: ask super/work_part_under_company/person/different_man/call_big_child_up_different_life/tell_place_to_large_year/few_woman.py after long group"

---
+++ super/work_part_under_company/person/different_man/call_big_child_up_different_life/tell_place_to_large_year/few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    point_and_eye(str_arg)
+    print('long_man')
+
+def point_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('next_life_and_year')
