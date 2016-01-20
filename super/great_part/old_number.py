153eb8aba77b406377e8cf24585a8d40daa89b4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 13:29:04 -0500

    "mix: work super/make_world_after_bad_way/man_or_number/year/tell_public_day_over_small_woman.py and super/great_part/old_number.py after good week"

---
+++ super/great_part/old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    want_group_about_way(str_arg)
+    print('large_woman_and_case')
+
+def want_group_about_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('work_or_good_week')
---
+++ super/make_world_after_bad_way/man_or_number/year/tell_public_day_over_small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_man_up_way(str_arg):
+    person(str_arg)
+    print('important_person')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_man_up_way('old_government')
