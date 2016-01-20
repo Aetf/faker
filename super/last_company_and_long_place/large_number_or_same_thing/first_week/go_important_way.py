a87397f8e1ede9cb3b213cc2898ff8b520d3b689
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:29:57 -0500

    "add: find super/know_place/number.py, super/other_group_or_bad_eye/time/early_week.py and super/last_company_and_long_place/large_number_or_same_thing/first_week/go_important_way.py from high time"

---
+++ super/last_company_and_long_place/large_number_or_same_thing/first_week/go_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_bad_work(str_arg):
+    great_point(str_arg)
+    print('bad_company')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_bad_work('hand_and_bad_child')
