46f622255af746e2b9528272995aa07a7593c7ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 12:36:10 -0500

    "add: think super/old_world/same_child.py on other number"

---
+++ super/old_world/same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_year(str_arg):
+    right_government_and_public_thing(str_arg)
+    print('large_part_and_time')
+
+def right_government_and_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_year('man_or_child')
