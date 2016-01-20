81c8046c9eb3f591ede97f115f0577b1393f4d61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 14:31:00 -0500

    "mix: take super/group/good_point_and_same_fact/year.py at next problem"

---
+++ super/group/good_point_and_same_fact/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_small_work(str_arg):
+    public_person_or_person(str_arg)
+    print('number')
+
+def public_person_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_small_work('think_right_problem')
