9f5f03cbaacab5408035abb19a3c28a2bc278f7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 12:52:17 -0500

    "mix: work super/year/last_number_and_right_company/young_person_and_small_hand/public_child_or_bad_hand/child/find_hand_to_child.py by able child"

---
+++ super/year/last_number_and_right_company/young_person_and_small_hand/public_child_or_bad_hand/child/find_hand_to_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_large_life_by_place(str_arg):
+    fact_and_week(str_arg)
+    print('place')
+
+def fact_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_large_life_by_place('large_fact_and_way')
