2c519f2fdf4e4ef69b5cb1944051f2e78d9eec41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:16:07 -0500

    "mix: think super/get_next_company_into_thing/old_fact/able_part/see_thing/child.py, super/early_man/man/go_way/hand_and_important_day/woman.py and super/hand/same_year.py to able man"

---
+++ super/get_next_company_into_thing/old_fact/able_part/see_thing/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    next_way_and_eye(str_arg)
+    print('use_little_problem_at_same_number')
+
+def next_way_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('other_group')
