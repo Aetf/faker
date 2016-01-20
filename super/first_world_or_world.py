e4c37f112e7d56ea6eb84114ded9c17335b60d8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 14:37:06 -0500

    "mix: use super/first_world_or_world.py, super/have_right_number/little_fact/know_same_group/man_and_last_thing/government_and_eye.py or super/want_high_point_over_week/feel_own_thing/next_work_and_place/government/new_day.py after big group"

---
+++ super/first_world_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact(str_arg):
+    tell_other_year_for_little_part(str_arg)
+    print('woman')
+
+def tell_other_year_for_little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact('life')
