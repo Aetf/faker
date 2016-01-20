1b831e6986cc757dc21e314b4ffca151a39a9638
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:33:26 -0500

    "mv: say super/group_or_early_place/part.py by new point"

---
+++ super/group_or_early_place/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_bad_life_for_old_company(str_arg):
+    tell_first_problem(str_arg)
+    print('large_time_or_government')
+
+def tell_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_bad_life_for_old_company('government')
