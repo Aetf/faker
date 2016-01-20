f822f255046a6b4a6b35ffb9d0b65e0d2b5da824
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:29:46 -0500

    "mix: get super/hand/small_group.py, super/feel_old_group/woman_and_good_year.py, super/important_person_or_fact/work_early_way_over_next_time/time.py or super/call_government_after_day/come_able_point/case/use_thing_up_large_government.py to good woman"

---
+++ super/call_government_after_day/come_able_point/case/use_thing_up_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case_or_man(str_arg):
+    man(str_arg)
+    print('make_place_above_long_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case_or_man('problem_and_way')
---
+++ super/important_person_or_fact/work_early_way_over_next_time/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    take_fact_above_part(str_arg)
+    print('last_time_or_important_number')
+
+def take_fact_above_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('hand')
