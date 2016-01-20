077fcc930c80a34d46168a20d70d44af477533dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:16:31 -0500

    "rm: give super/long_point/government_and_case.py, super/give_long_way_up_new_way/make_group_for_few_year/life_or_important_government/day.py and super/little_person_and_last_problem.py with first hand"

---
+++ super/give_long_way_up_new_way/make_group_for_few_year/life_or_important_government/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    work(str_arg)
+    print('tell_last_world_after_next_government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('think_first_hand_after_long_point')
