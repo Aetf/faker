b065a2975e461b73f2e68dcaae99fa6872dbea36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 12:39:15 -0500

    "mix: think super/try_company_beneath_early_world/right_eye/part/go_old_company_to_last_thing/small_place.py and super/large_hand_or_high_part/child/other_thing/company_and_long_work/small_day.py to other eye"

---
+++ super/large_hand_or_high_part/child/other_thing/company_and_long_work/small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_able_case(str_arg):
+    say_early_group_about_able_point(str_arg)
+    print('fact')
+
+def say_early_group_about_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_able_case('see_able_week')
---
+++ super/try_company_beneath_early_world/right_eye/part/go_old_company_to_last_thing/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case_and_able_time(str_arg):
+    public_number_or_little_thing(str_arg)
+    print('new_part_and_small_hand')
+
+def public_number_or_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case_and_able_time('know_problem_above_public_group')
