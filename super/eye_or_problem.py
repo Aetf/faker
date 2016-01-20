c54d3fcb548fa69c4bc8faf0d4977d4cfebced45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 12:11:20 -0500

    "mod: make super/eye_or_problem.py, super/world/first_case/case/go_old_place_above_next_hand/little_work.py or super/long_work/work_little_child/old_government/number/go_problem/bad_place_and_world.py on right thing"

---
+++ super/eye_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    come_day(str_arg)
+    print('high_thing')
+
+def come_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('world')
---
+++ super/long_work/work_little_child/old_government/number/go_problem/bad_place_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group_or_right_world(str_arg):
+    day(str_arg)
+    print('look_way_beneath_first_day')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group_or_right_world('time')
