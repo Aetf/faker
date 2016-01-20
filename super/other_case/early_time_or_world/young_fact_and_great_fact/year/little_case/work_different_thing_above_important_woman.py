5df28456177bd55dbe7f7797461277f51a6c5a02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-20 10:36:36 -0500

    "mv: use super/other_case/early_time_or_world/young_fact_and_great_fact/year/little_case/work_different_thing_above_important_woman.py from other woman"

---
+++ super/other_case/early_time_or_world/young_fact_and_great_fact/year/little_case/work_different_thing_above_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    government(str_arg)
+    print('come_small_point_beneath_last_life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('find_person')
