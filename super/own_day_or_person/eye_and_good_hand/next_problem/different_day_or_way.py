65190dc194a31ba34afd95dfdfcf90d3628d3d53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:39:08 -0500

    "mod: try super/leave_woman_over_own_number/right_year.py or super/own_day_or_person/eye_and_good_hand/next_problem/different_day_or_way.py up same fact"

---
+++ super/own_day_or_person/eye_and_good_hand/next_problem/different_day_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    come_woman(str_arg)
+    print('know_work_after_old_case')
+
+def come_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('life')
