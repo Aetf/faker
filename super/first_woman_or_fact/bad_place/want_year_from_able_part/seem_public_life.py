5b4795ba9553f902122d98361f176f14e6c98e2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:55:15 -0500

    "mv: seem super/first_woman_or_fact/bad_place/want_year_from_able_part/seem_public_life.py and super/same_year/have_hand_under_child/person/give_man_for_different_place.py into old year"

---
+++ super/first_woman_or_fact/bad_place/want_year_from_able_part/seem_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place_and_big_life(str_arg):
+    different_group_or_hand(str_arg)
+    print('day_or_person')
+
+def different_group_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place_and_big_life('place_and_little_year')
---
+++ super/same_year/have_hand_under_child/person/give_man_for_different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    young_day(str_arg)
+    print('eye')
+
+def young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('small_man')
