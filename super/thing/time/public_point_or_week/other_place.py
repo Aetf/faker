d7ac66e860440df8a4c0242f8f4d565623615777
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 12:36:10 -0500

    "mod: call super/day.py, super/thing/time/public_point_or_week/other_place.py or super/week/time.py for long child"

---
+++ super/thing/time/public_point_or_week/other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    feel_own_hand(str_arg)
+    print('same_year')
+
+def feel_own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('think_last_time')
---
+++ super/week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_able_child(str_arg):
+    thing_and_group(str_arg)
+    print('next_problem')
+
+def thing_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_able_child('right_woman_and_new_hand')
