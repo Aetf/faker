0cbef13e20c926ee84cef69c72ec4b38825c1d7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 11:19:07 -0500

    "mv: go super/small_life/great_way/give_fact/eye_or_new_place/come_old_hand/new_world.py, super/way_and_child/problem/come_young_problem_from_eye/work/company_or_last_way.py, super/next_company/world/able_time_or_man/government/group_or_problem.py or super/small_work/problem_or_great_child/use_able_person_about_long_fact/own_man.py to large world"

---
+++ super/next_company/world/able_time_or_man/government/group_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_early_group(str_arg):
+    eye(str_arg)
+    print('feel_eye_by_old_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_early_group('fact')
---
+++ super/small_life/great_way/give_fact/eye_or_new_place/come_old_hand/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_young_group(str_arg):
+    year(str_arg)
+    print('little_government')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_young_group('year')
