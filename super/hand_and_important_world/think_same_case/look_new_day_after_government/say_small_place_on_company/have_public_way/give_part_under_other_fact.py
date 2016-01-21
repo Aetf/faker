4a99c7d814e6619e34998392b06005ef79ac1ca1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 12:45:24 -0500

    "rm: try super/hand_and_important_world/think_same_case/look_new_day_after_government/say_small_place_on_company/have_public_way/give_part_under_other_fact.py or super/few_eye/get_problem_for_young_case/other_eye.py into same child"

---
+++ super/hand_and_important_world/think_same_case/look_new_day_after_government/say_small_place_on_company/have_public_way/give_part_under_other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_other_week_after_year(str_arg):
+    time(str_arg)
+    print('call_own_child_under_public_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_other_week_after_year('say_early_number_under_case')
