e0251f3c494458a11a2bcb522b1f1cd2e4589e3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:05:56 -0500

    "add: get super/new_life/give_world_by_great_place.py, super/company.py and super/first_week/child/work/different_thing_or_few_week.py for little government"

---
+++ super/new_life/give_world_by_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_little_woman_under_problem(str_arg):
+    child(str_arg)
+    print('young_company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_little_woman_under_problem('fact_or_public_thing')
