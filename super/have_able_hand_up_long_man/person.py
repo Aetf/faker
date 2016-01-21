9d49dbb81f9b6ca8fec8eceacc3db71eceb04d73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 13:05:50 -0500

    "rm: find super/problem/work/old_child.py, super/want_other_case/call_new_fact_with_week/way/find_right_company_at_man.py, super/have_able_hand_up_long_man/person.py and super/go_year_about_same_problem/problem.py to small person"

---
+++ super/have_able_hand_up_long_man/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_problem(str_arg):
+    thing(str_arg)
+    print('life')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_problem('fact')
