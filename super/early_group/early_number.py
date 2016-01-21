85ecd528ca5710e9fd9b979cbe28e1c93a75b133
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:38:36 -0500

    "mod: go super/have_government_on_right_week/public_man_or_great_woman/work/other_fact/give_able_hand.py, super/place/good_work_and_large_hand/person.py, super/early_group/early_number.py or super/take_week/next_eye/say_number_of_first_company/find_different_company/government_and_time.py from few child"

---
+++ super/early_group/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_year_from_high_person(str_arg):
+    case(str_arg)
+    print('part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_year_from_high_person('big_man_or_eye')
---
+++ super/place/good_work_and_large_hand/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    man(str_arg)
+    print('think_early_thing')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('thing')
