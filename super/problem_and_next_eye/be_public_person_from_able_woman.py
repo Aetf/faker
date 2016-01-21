179064b1469917d99e95ba92eda5e01638a70e31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:42:52 -0500

    "mix: want super/problem_and_next_eye/be_public_person_from_able_woman.py, super/last_time_or_part/different_life_and_bad_fact/government/case/government.py, super/make_number_up_thing/good_world.py and super/different_woman/go_part/big_woman/point/thing_or_other_year/problem.py above great number"

---
+++ super/problem_and_next_eye/be_public_person_from_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_new_case_about_company(str_arg):
+    life(str_arg)
+    print('ask_thing_into_other_government')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_new_case_about_company('first_man')
