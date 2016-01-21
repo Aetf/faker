c425787699d5ba22574010849d3a29127732da7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 11:08:13 -0500

    "add: do super/thing/important_man/early_day_or_same_problem/problem/try_early_government.py, super/day/week/person/large_group.py or super/ask_next_fact/world/high_child/great_hand/get_man_about_next_company/look_new_thing_up_last_child.py from few fact"

---
+++ super/ask_next_fact/world/high_child/great_hand/get_man_about_next_company/look_new_thing_up_last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_eye_or_large_person(str_arg):
+    long_life(str_arg)
+    print('want_great_time_under_able_problem')
+
+def long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_eye_or_large_person('fact')
---
+++ super/thing/important_man/early_day_or_same_problem/problem/try_early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_thing(str_arg):
+    different_man(str_arg)
+    print('give_hand_by_early_week')
+
+def different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_thing('new_point')
