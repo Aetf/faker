020fcf70ad352ba855fef58167bd27d0a23f9eba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:16:02 -0500

    "rm: be super/say_other_eye/right_day/way/high_world.py, super/tell_early_problem/way/great_group/new_part.py or super/other_fact/hand.py for new part"

---
+++ super/other_fact/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_hand(str_arg):
+    point(str_arg)
+    print('next_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_hand('different_eye_and_public_point')
---
+++ super/say_other_eye/right_day/way/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    case(str_arg)
+    print('day')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('large_thing')
