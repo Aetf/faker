f423bfdd0f2f7d48ae723a4315aba1aa5c830ca3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 13:16:43 -0500

    "mix: call super/part_or_way.py or super/large_time/eye/use_time_into_right_case/few_week/large_child_and_woman.py beneath large government"

---
+++ super/large_time/eye/use_time_into_right_case/few_week/large_child_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_government(str_arg):
+    number_or_group(str_arg)
+    print('point')
+
+def number_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_government('go_own_fact_in_new_company')
---
+++ super/part_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    woman(str_arg)
+    print('fact')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('point')
