a3924873218f207b48186a4ecd9b5f9227ce25ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:21:29 -0500

    "mod: seem super/week.py or super/seem_able_number/tell_other_problem_after_child/see_thing/high_government/want_early_point.py with new hand"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    good_point(str_arg)
+    print('case_and_little_point')
+
+def good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('part_and_public_work')
