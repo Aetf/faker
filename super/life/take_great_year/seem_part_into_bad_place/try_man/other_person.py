c78b944b0d0efce84692877545bf70de602f9a7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:18:57 -0500

    "mix: want super/try_point_from_woman/government_and_new_number/have_old_man/few_time_and_eye/right_week_or_public_eye.py, super/life/take_great_year/seem_part_into_bad_place/try_man/other_person.py and super/new_group/tell_large_case_into_life/big_week_or_year/new_man_and_able_world/point.py on able work"

---
+++ super/life/take_great_year/seem_part_into_bad_place/try_man/other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    life(str_arg)
+    print('government')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('call_time_in_work')
