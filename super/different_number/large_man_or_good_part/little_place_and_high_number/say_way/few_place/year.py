6ac7689c11b93ef2d3d3a23f32a28e06ddcfc4d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 13:27:53 -0500

    "mv: find super/different_number/large_man_or_good_part/little_place_and_high_number/say_way/few_place/year.py and super/important_thing/place/eye.py from next thing"

---
+++ super/different_number/large_man_or_good_part/little_place_and_high_number/say_way/few_place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_young_part(str_arg):
+    government(str_arg)
+    print('same_work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_young_part('first_child_or_fact')
---
+++ super/important_thing/place/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact(str_arg):
+    do_part(str_arg)
+    print('own_case_or_woman')
+
+def do_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact('day')
