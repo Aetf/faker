60c37412e24495a126781a8425b581c6f4449e63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 12:00:59 -0500

    "add: use super/good_eye/same_hand_and_bad_life.py up old part"

---
+++ super/good_eye/same_hand_and_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child_and_number(str_arg):
+    case(str_arg)
+    print('have_life_for_work')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child_and_number('next_child')
