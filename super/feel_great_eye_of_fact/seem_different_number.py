007e522a291920749eab78066c3230b452869b6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:43:27 -0500

    "rm: look super/other_world_or_great_week/be_high_place_beneath_old_time.py, super/good_woman_or_world/case_and_problem/tell_man_above_right_company/big_woman_or_child/important_government.py and super/feel_great_eye_of_fact/seem_different_number.py on old time"

---
+++ super/feel_great_eye_of_fact/seem_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government(str_arg):
+    public_fact(str_arg)
+    print('feel_man_of_young_day')
+
+def public_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government('few_group_or_government')
---
+++ super/other_world_or_great_week/be_high_place_beneath_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place(str_arg):
+    find_other_woman(str_arg)
+    print('take_world_into_new_problem')
+
+def find_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place('right_man')
