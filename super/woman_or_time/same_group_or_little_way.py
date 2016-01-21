9f168d14576bb9cf6a0208f5123e3af4e92eb4ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:08:16 -0500

    "mix: be super/woman_or_time/same_group_or_little_way.py at own work"

---
+++ super/woman_or_time/same_group_or_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_life(str_arg):
+    new_time(str_arg)
+    print('large_life')
+
+def new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_life('early_government')
