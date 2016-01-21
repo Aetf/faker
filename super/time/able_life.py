46e2119c8a005bb088ac76cc75ce91b7e339d77c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 14:51:26 -0500

    "add: be super/time/able_life.py or super/company/fact.py under first way"

---
+++ super/time/able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_place(str_arg):
+    great_group(str_arg)
+    print('have_fact_about_small_work')
+
+def great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_place('fact_or_old_group')
