01ab5f76cace524e544725642c114656f13d7fff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-21 10:01:04 -0500

    "add: tell super/say_great_year_in_hand/come_different_hand/long_thing/try_way_from_own_time/high_place_and_week/go_man_after_number.py, super/old_eye/number/hand/be_first_problem/give_early_fact.py, super/high_time/find_long_government/know_first_year_in_government/same_hand_and_company.py and super/early_work/do_few_time/new_week_or_fact.py to large week"

---
+++ super/early_work/do_few_time/new_week_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_bad_thing(str_arg):
+    thing(str_arg)
+    print('leave_woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_bad_thing('seem_life_after_big_world')
---
+++ super/say_great_year_in_hand/come_different_hand/long_thing/try_way_from_own_time/high_place_and_week/go_man_after_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact(str_arg):
+    right_fact(str_arg)
+    print('little_company')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact('important_day')
