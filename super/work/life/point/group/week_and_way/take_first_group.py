082afd7d630baaefac6bf240ddf03162a39b1635
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 11:14:04 -0500

    "mix: give super/thing/have_point_beneath_public_way/last_thing.py or super/work/life/point/group/week_and_way/take_first_group.py under new part"

---
+++ super/work/life/point/group/week_and_way/take_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    eye_or_next_point(str_arg)
+    print('look_new_government')
+
+def eye_or_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('public_year')
