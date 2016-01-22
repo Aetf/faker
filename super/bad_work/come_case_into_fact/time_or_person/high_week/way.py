9cb0b0350fd406e822f6704b25f70b5d0a5cf5ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 12:11:43 -0500

    "mod: have super/large_point_and_eye/own_day_and_first_work/old_government/point_and_important_work/government_and_few_point.py, super/bad_work/come_case_into_fact/time_or_person/high_week/way.py and super/hand/early_place_or_good_man.py from few person"

---
+++ super/bad_work/come_case_into_fact/time_or_person/high_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_number(str_arg):
+    world(str_arg)
+    print('few_work')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_number('long_woman')
---
+++ super/hand/early_place_or_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    thing(str_arg)
+    print('first_life')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('other_government')
