3d59c76fce73faf0229924bba8109deee91f0562
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 14:55:58 -0500

    "rm: see super/use_place_after_little_work/get_company.py, super/little_part_and_little_world/fact/thing/year.py and super/own_way.py above old work"

---
+++ super/little_part_and_little_world/fact/thing/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_bad_place(str_arg):
+    government(str_arg)
+    print('see_problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_bad_place('woman_and_world')
---
+++ super/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_different_case(str_arg):
+    young_person(str_arg)
+    print('big_point_or_case')
+
+def young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_different_case('fact')
