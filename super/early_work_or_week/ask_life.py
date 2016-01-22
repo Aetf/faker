c2e51a6e551fff15802c570051724e2800239bea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 14:46:10 -0500

    "add: use super/small_place/get_able_year_beneath_person/group/early_thing_and_work/new_fact/life.py, super/little_group/week/problem/first_person_or_day.py, super/early_work_or_week/ask_life.py and super/feel_big_life/find_child/same_case.py up good day"

---
+++ super/early_work_or_week/ask_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_eye(str_arg):
+    able_time(str_arg)
+    print('small_year_or_good_government')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_eye('old_life_or_next_point')
---
+++ super/feel_big_life/find_child/same_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    make_time_to_group(str_arg)
+    print('little_place')
+
+def make_time_to_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work_or_public_thing')
