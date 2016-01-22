74561dc5526d29fa273ff2d9c0c446172d6dd74a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 13:36:44 -0500

    "mix: use super/work/person.py up first world"

---
+++ super/work/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    different_child_and_child(str_arg)
+    print('case')
+
+def different_child_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('man_and_eye')
