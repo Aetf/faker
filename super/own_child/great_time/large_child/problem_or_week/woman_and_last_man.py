f4dae0f18c865211a286ecd2257e365d91e11292
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 12:33:02 -0500

    "mv: give super/own_child/great_time/large_child/problem_or_week/woman_and_last_man.py of bad group"

---
+++ super/own_child/great_time/large_child/problem_or_week/woman_and_last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_problem(str_arg):
+    important_part(str_arg)
+    print('give_year')
+
+def important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_problem('use_company_beneath_point')
