42ba5375add8b8d2a05b2ff6b4dc4531794c7e7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 10:43:29 -0500

    "mod: have super/work/great_life.py above big woman"

---
+++ super/work/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_government(str_arg):
+    time(str_arg)
+    print('big_man_and_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_government('big_day')
