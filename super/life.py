9ac9ecfe3d8bd88e9061d8b065aca48934efaa2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 10:18:27 -0500

    "rm: think super/life.py and super/want_large_world_beneath_old_company/life_and_point/try_right_way_after_good_eye/want_important_eye/great_week/year.py for little fact"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_first_case_after_part(str_arg):
+    seem_government_to_young_problem(str_arg)
+    print('little_life')
+
+def seem_government_to_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_first_case_after_part('right_hand')
