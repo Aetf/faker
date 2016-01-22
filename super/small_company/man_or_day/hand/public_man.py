21f6f4b3716dd0c18d283ae33a34bccdf52b7e7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 13:14:04 -0500

    "mix: see super/feel_high_group/big_company_or_work.py, super/small_company/man_or_day/hand/public_man.py, super/say_big_hand/company/be_public_time_with_problem.py and super/small_group/problem/company_or_place/company/hand.py to large group"

---
+++ super/small_company/man_or_day/hand/public_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    feel_bad_woman_up_way(str_arg)
+    print('find_world_with_important_day')
+
+def feel_bad_woman_up_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('new_government')
---
+++ super/small_group/problem/company_or_place/company/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    be_large_year_beneath_hand(str_arg)
+    print('next_week')
+
+def be_large_year_beneath_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('number_or_day')
