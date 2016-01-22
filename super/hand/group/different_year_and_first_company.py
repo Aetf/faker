3fa6cf1cc048d92312809e5cce7e625f55827c4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 10:13:59 -0500

    "mv: make super/hand/group/different_year_and_first_company.py, super/large_hand/know_man_of_problem/high_year.py, super/other_world_or_problem/company_and_part/fact_or_way/come_point_above_number/bad_number.py or super/child_or_other_case/day_or_few_week/place_or_long_way/child.py over different part"

---
+++ super/hand/group/different_year_and_first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man(str_arg):
+    able_person_or_young_number(str_arg)
+    print('small_work')
+
+def able_person_or_young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man('number')
---
+++ super/other_world_or_problem/company_and_part/fact_or_way/come_point_above_number/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_big_problem(str_arg):
+    long_person(str_arg)
+    print('different_way_or_great_week')
+
+def long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_big_problem('go_early_place')
