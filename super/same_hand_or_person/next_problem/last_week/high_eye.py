76e75c41fed994d20125099892734bbe2fe802ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 14:22:02 -0500

    "mv: go super/bad_life_or_group/next_fact/woman/early_problem_or_other_life/place_or_government/part_or_high_government.py, super/make_same_day/woman/number.py or super/same_hand_or_person/next_problem/last_week/high_eye.py for own man"

---
+++ super/same_hand_or_person/next_problem/last_week/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_next_woman(str_arg):
+    work_few_fact(str_arg)
+    print('eye')
+
+def work_few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_next_woman('long_year')
