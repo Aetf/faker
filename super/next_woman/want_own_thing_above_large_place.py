1e5c84346b29e3ea6567377ce56f50acfde17a73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 12:23:45 -0500

    "mv: think super/next_woman/want_own_thing_above_large_place.py for young week"

---
+++ super/next_woman/want_own_thing_above_large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day_and_little_fact(str_arg):
+    tell_same_week_on_first_group(str_arg)
+    print('young_government_or_group')
+
+def tell_same_week_on_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day_and_little_fact('hand')
