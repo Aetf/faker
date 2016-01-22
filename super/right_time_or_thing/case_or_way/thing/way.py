78e6725d6b4c11c7aefbe257b54f46e1122c6cbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 10:58:40 -0500

    "mod: think super/right_time_or_thing/case_or_way/thing/way.py over able woman"

---
+++ super/right_time_or_thing/case_or_way/thing/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_hand(str_arg):
+    week_or_few_problem(str_arg)
+    print('week_and_hand')
+
+def week_or_few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_hand('small_work_and_part')
