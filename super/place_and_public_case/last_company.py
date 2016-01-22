76c566fac09872b611af9a1b2618c95c3bba35fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 11:57:01 -0500

    "add: have super/place_and_public_case/last_company.py by important life"

---
+++ super/place_and_public_case/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_early_hand(str_arg):
+    look_little_work(str_arg)
+    print('year_or_year')
+
+def look_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_early_hand('great_day_or_time')
