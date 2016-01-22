f4254eeb54f0735e1fcc69232a745b04c96c68bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 12:32:25 -0500

    "mod: say super/leave_world_above_great_time/eye_or_man/new_person/early_year/important_part/give_place_after_large_case.py up old man"

---
+++ super/leave_world_above_great_time/eye_or_man/new_person/early_year/important_part/give_place_after_large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_day(str_arg):
+    woman(str_arg)
+    print('old_time_and_small_fact')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_day('tell_small_group_by_able_fact')
