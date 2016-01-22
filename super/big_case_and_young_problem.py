ca6a5015d46c0ec04e1bdb9adab0441f27522ac3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 13:42:40 -0500

    "mod: go super/big_case_and_young_problem.py or super/look_public_time/part/right_fact_or_point.py by large life"

---
+++ super/big_case_and_young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_case(str_arg):
+    next_eye_or_big_time(str_arg)
+    print('able_day')
+
+def next_eye_or_big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_case('world')
---
+++ super/look_public_time/part/right_fact_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_little_time(str_arg):
+    right_day(str_arg)
+    print('point')
+
+def right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_little_time('eye_and_few_eye')
