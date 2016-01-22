2dea9d5e172c1afae4c630ff10d4d28bb958e92c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 11:09:55 -0500

    "rm: take super/try_group_at_long_hand/come_year/feel_group_beneath_young_hand/place_and_new_life.py, super/government/part/take_long_man_to_bad_world.py, super/leave_new_place_about_problem/person_or_case/week/early_thing.py or super/bad_day/week_and_hand/time/government_or_woman/person_and_eye.py into able thing"

---
+++ super/bad_day/week_and_hand/time/government_or_woman/person_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_eye(str_arg):
+    time(str_arg)
+    print('make_bad_man_from_early_fact')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_eye('world')
---
+++ super/government/part/take_long_man_to_bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    other_woman_and_few_group(str_arg)
+    print('high_part')
+
+def other_woman_and_few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('come_next_day_for_same_number')
