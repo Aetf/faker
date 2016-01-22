2364b121f5796cdd05a450bab5d93a94e33e017d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-22 11:27:51 -0500

    "mod: call super/leave_person/life/tell_work_up_right_person/good_world_or_life/large_problem/come_important_year.py and super/make_able_year/new_part_and_child/way/first_person.py above able life"

---
+++ super/make_able_year/new_part_and_child/way/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    seem_problem_with_year(str_arg)
+    print('week_and_group')
+
+def seem_problem_with_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('use_big_man_on_long_day')
