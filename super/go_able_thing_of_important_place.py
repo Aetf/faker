fb4555e7be79b12831f30ee956ecdb8be8cf3f73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 12:01:08 -0500

    "rm: be super/go_able_thing_of_important_place.py, super/give_public_work/try_young_government/high_week.py and super/public_place/young_world/make_hand_above_thing/part.py at different week"

---
+++ super/go_able_thing_of_important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    case(str_arg)
+    print('point_or_important_eye')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('fact')
