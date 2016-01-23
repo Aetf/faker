b6b9784600152af8229b0b18ca8738016377e2da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 12:57:08 -0500

    "add: know super/right_work/call_place/make_thing/man/see_last_company/public_week.py up young year"

---
+++ super/right_work/call_place/make_thing/man/see_last_company/public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place(str_arg):
+    hand_and_government(str_arg)
+    print('world')
+
+def hand_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place('government')
