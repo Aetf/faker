b61c50086dc8c03d6704b585c622b74b9ab3cdb4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 10:13:15 -0500

    "mod: seem super/great_fact/seem_problem_after_same_way/life_and_great_number/seem_same_woman/public_day/woman_or_place.py, super/week/place_and_different_way/woman/old_day_or_case.py or super/world/number_or_few_time/good_day_or_world/think_bad_man_under_day/day_and_hand/old_man_or_group.py on next child"

---
+++ super/great_fact/seem_problem_after_same_way/life_and_great_number/seem_same_woman/public_day/woman_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world(str_arg):
+    first_time(str_arg)
+    print('first_government')
+
+def first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world('right_man')
---
+++ super/world/number_or_few_time/good_day_or_world/think_bad_man_under_day/day_and_hand/old_man_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_week(str_arg):
+    other_number(str_arg)
+    print('life')
+
+def other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_week('thing_and_life')
