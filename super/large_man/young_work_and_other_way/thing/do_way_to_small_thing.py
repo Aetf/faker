81b9a8efb4ab5525e84a4f90a7e80c913d2675cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 12:17:08 -0500

    "mod: leave super/world/place/long_place/same_life.py, super/point_and_hand/great_fact_or_work/few_way/feel_early_thing.py or super/large_man/young_work_and_other_way/thing/do_way_to_small_thing.py in big number"

---
+++ super/large_man/young_work_and_other_way/thing/do_way_to_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_man(str_arg):
+    high_person(str_arg)
+    print('few_world')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_man('important_man')
---
+++ super/world/place/long_place/same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    early_year_or_few_number(str_arg)
+    print('important_man')
+
+def early_year_or_few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('next_life_or_child')
