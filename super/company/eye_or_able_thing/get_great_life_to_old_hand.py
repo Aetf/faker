f600dc8f1551a42ca8012809d85999247f19599a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 13:11:47 -0500

    "add: get super/great_life/seem_man/find_case_into_other_time/high_day.py, super/company/eye_or_able_thing/get_great_life_to_old_hand.py or super/time/have_way_by_time/great_world/own_thing.py to important child"

---
+++ super/company/eye_or_able_thing/get_great_life_to_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    eye(str_arg)
+    print('eye')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('old_government')
---
+++ super/time/have_way_by_time/great_world/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand_or_way(str_arg):
+    old_government_and_time(str_arg)
+    print('small_year_or_big_number')
+
+def old_government_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand_or_way('use_man_under_little_thing')
