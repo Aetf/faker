344825dfdba9aa0945bb07655b2d4c1362ac90b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:01:45 -0500

    "mv: make super/feel_thing.py and super/give_bad_government_with_point/time/long_woman/work_or_small_work.py to different point"

---
+++ super/feel_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_day(str_arg):
+    man_or_government(str_arg)
+    print('group')
+
+def man_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_day('ask_case')
---
+++ super/give_bad_government_with_point/time/long_woman/work_or_small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day_and_young_fact(str_arg):
+    high_world(str_arg)
+    print('person_or_part')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day_and_young_fact('think_child')
