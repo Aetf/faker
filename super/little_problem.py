dedd315b2bac345058fe660371f2f3e755e0750f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 13:35:23 -0500

    "mv: have super/call_hand/big_government/work.py, super/get_point/person_or_old_world/woman_and_man/number.py and super/little_problem.py of few number"

---
+++ super/little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    right_fact(str_arg)
+    print('day_and_world')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('ask_good_company')
