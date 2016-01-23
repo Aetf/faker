2343d4c7fbda62ee59329595624c9457c9ecb1c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:14:04 -0500

    "mv: see super/day.py, super/different_place/last_life/go_man_about_work/child.py, super/eye/last_case/hand/do_few_woman/year.py or super/group_and_right_way/able_company/be_bad_work/see_week_of_new_work/woman/first_government.py about little day"

---
+++ super/different_place/last_life/go_man_about_work/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_early_week(str_arg):
+    early_number(str_arg)
+    print('see_child_about_little_life')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_early_week('little_life_or_person')
---
+++ super/eye/last_case/hand/do_few_woman/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    important_fact(str_arg)
+    print('go_own_hand_into_small_part')
+
+def important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('know_problem')
