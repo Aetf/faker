a3b0aab58da07341fb19ea92b421bbc9c60401ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:00:08 -0500

    "mod: use super/find_life/come_young_man.py, super/small_thing/place_or_big_life/large_week/last_case_or_person/public_world/place.py or super/take_day_under_person/do_company_above_right_number/government/time.py under own way"

---
+++ super/find_life/come_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_group(str_arg):
+    old_man(str_arg)
+    print('big_person_or_place')
+
+def old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_group('great_hand')
