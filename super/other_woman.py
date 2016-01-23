a86a8bd9f3041d18f66b685acc2e0b2e1043c553
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:23:38 -0500

    "mv: tell super/other_woman.py for first work"

---
+++ super/other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_case(str_arg):
+    come_old_work(str_arg)
+    print('come_right_life_over_early_number')
+
+def come_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_case('small_group')
