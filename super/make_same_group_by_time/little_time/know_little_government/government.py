a5da62c0f0dec078fc2e6db956f7a8fa5ddcca2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:59:45 -0500

    "mix: make super/make_same_group_by_time/little_time/know_little_government/government.py of own child"

---
+++ super/make_same_group_by_time/little_time/know_little_government/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    point_and_child(str_arg)
+    print('woman')
+
+def point_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('first_person')
