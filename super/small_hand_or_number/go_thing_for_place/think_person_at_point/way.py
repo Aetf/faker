41d6a9f93cd948705da0bad2429effa3bbc2ca1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:34:51 -0500

    "rm: take super/work_part_from_fact/government_or_person/man.py or super/small_hand_or_number/go_thing_for_place/think_person_at_point/way.py beneath same work"

---
+++ super/small_hand_or_number/go_thing_for_place/think_person_at_point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    want_week_by_same_fact(str_arg)
+    print('company_or_group')
+
+def want_week_by_same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('use_young_child')
