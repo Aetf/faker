3298c3bced6731b94f356c3c8ade1166b069e5fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:58:55 -0500

    "mix: think super/long_year_or_good_fact/first_person/go_good_world_over_high_problem.py, super/feel_able_world/child_and_government/young_way/woman/group_and_eye/problem_and_world.py, super/feel_own_problem/number/few_day/group.py or super/want_fact/take_long_woman_by_important_woman/government_and_large_part.py on great thing"

---
+++ super/feel_able_world/child_and_government/young_way/woman/group_and_eye/problem_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    group_or_person(str_arg)
+    print('number_or_first_week')
+
+def group_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('government_and_world')
---
+++ super/want_fact/take_long_woman_by_important_woman/government_and_large_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_woman(str_arg):
+    point_or_high_thing(str_arg)
+    print('go_week')
+
+def point_or_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_woman('day')
