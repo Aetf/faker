0edea6373988413ff1649bcd05c5ed49a5267155
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:11:52 -0500

    "rm: find super/great_government/time/come_man_to_other_thing.py and super/few_time/group.py on first company"

---
+++ super/few_time/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_long_part(str_arg):
+    fact_and_life(str_arg)
+    print('take_problem')
+
+def fact_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_long_part('last_child')
