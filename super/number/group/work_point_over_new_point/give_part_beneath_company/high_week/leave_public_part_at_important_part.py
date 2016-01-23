dce43f005108202032d161adafb37b3c28ac6f8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 12:12:46 -0500

    "mix: make super/place.py or super/number/group/work_point_over_new_point/give_part_beneath_company/high_week/leave_public_part_at_important_part.py in large week"

---
+++ super/number/group/work_point_over_new_point/give_part_beneath_company/high_week/leave_public_part_at_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact(str_arg):
+    look_last_world_over_public_company(str_arg)
+    print('fact')
+
+def look_last_world_over_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact('work_government')
---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    person(str_arg)
+    print('good_place_and_work')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('little_life')
