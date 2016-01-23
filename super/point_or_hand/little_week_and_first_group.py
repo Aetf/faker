f48b182b36c9ed9543fda7718fb8084d590d83de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:34:45 -0500

    "rm: want super/point_or_hand/little_week_and_first_group.py over high child"

---
+++ super/point_or_hand/little_week_and_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point_or_large_number(str_arg):
+    bad_year_or_early_way(str_arg)
+    print('small_woman')
+
+def bad_year_or_early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point_or_large_number('hand')
