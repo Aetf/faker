372fe52a1a9780d659021e3405f877a42fce5a11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 11:42:28 -0500

    "mv: come super/few_group.py, super/large_thing/tell_little_government/government/place/way_and_world/case_or_new_problem.py, super/know_right_fact/leave_fact_with_different_company.py or super/company.py at other day"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    government(str_arg)
+    print('hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('big_life_and_time')
---
+++ super/few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    life_or_high_child(str_arg)
+    print('company')
+
+def life_or_high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('company')
