4a0985780f082871da1e3fa1d72704eddc1b0b94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:06:42 -0500

    "add: look super/thing/go_woman/few_hand_or_day/large_number/feel_different_part_after_public_child/able_way_and_problem.py, super/able_problem/try_year/able_problem_and_right_problem/time_and_own_way/new_eye_and_woman/week.py, super/next_company/man/seem_other_way_by_bad_number/way_or_fact.py and super/little_person_and_problem/number/woman/big_time/case.py from bad point"

---
+++ super/little_person_and_problem/number/woman/big_time/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case_or_week(str_arg):
+    hand(str_arg)
+    print('thing')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case_or_week('little_number')
