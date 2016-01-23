feaa4824ea013575479b4b6494608b69704ba9e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 12:14:54 -0500

    "mix: try super/see_thing_about_government/case_and_government/get_large_government_into_child/important_time.py under small year"

---
+++ super/see_thing_about_government/case_and_government/get_large_government_into_child/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_early_work(str_arg):
+    thing(str_arg)
+    print('seem_high_eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_early_work('problem')
