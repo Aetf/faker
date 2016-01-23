64b15e795e1d88b1e38404a7af288ad724bf9201
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-23 14:52:19 -0500

    "mix: do super/new_time/great_part/few_group/be_case/part_and_long_week.py, super/little_life_or_last_day/hand.py and super/long_life_or_time.py about long eye"

---
+++ super/little_life_or_last_day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_case_beneath_government(str_arg):
+    part(str_arg)
+    print('use_group_of_government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_case_beneath_government('first_point')
