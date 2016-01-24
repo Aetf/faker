e033839c96389323f15873d810de8a0d6cb59c0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:33:31 -0500

    "add: feel super/other_eye_and_point/way/good_world.py, super/important_point/new_fact_or_good_day/try_way/thing/long_way/old_time.py and super/large_man_or_woman/same_year/way_and_day/go_little_problem.py at old way"

---
+++ super/other_eye_and_point/way/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group(str_arg):
+    life(str_arg)
+    print('seem_fact_in_different_world')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group('government')
