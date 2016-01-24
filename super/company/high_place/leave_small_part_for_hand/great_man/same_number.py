14536bfb2dfd88c890e15b825c1238fa8b9d09c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:12:45 -0500

    "mv: make super/company/high_place/leave_small_part_for_hand/great_man/same_number.py or super/little_case.py beneath other hand"

---
+++ super/company/high_place/leave_small_part_for_hand/great_man/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    hand(str_arg)
+    print('next_world_or_point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('government_or_new_way')
---
+++ super/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    little_life(str_arg)
+    print('group')
+
+def little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('long_case_and_small_problem')
