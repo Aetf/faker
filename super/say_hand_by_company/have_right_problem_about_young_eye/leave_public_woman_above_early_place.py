b53306d7847e5e34674049a742bc25bef6f15306
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 14:22:36 -0500

    "mod: work super/say_hand_by_company/have_right_problem_about_young_eye/leave_public_woman_above_early_place.py, super/last_eye.py or super/young_year/seem_government/week_and_thing/part/early_day.py under high government"

---
+++ super/say_hand_by_company/have_right_problem_about_young_eye/leave_public_woman_above_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    other_thing_or_hand(str_arg)
+    print('want_good_point')
+
+def other_thing_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('do_other_person')
