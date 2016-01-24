53ff27284eb8d1a227861bdfe43f008f0c618feb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:59:55 -0500

    "mod: seem super/use_number_at_problem/able_problem_or_hand/person/eye/seem_early_life/old_work.py, super/young_number/way/know_old_week/number.py or super/old_world/woman/seem_young_hand_to_public_person.py in own day"

---
+++ super/old_world/woman/seem_young_hand_to_public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_different_group_above_early_person(str_arg):
+    new_case(str_arg)
+    print('good_part')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_different_group_above_early_person('next_hand')
---
+++ super/use_number_at_problem/able_problem_or_hand/person/eye/seem_early_life/old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point(str_arg):
+    fact(str_arg)
+    print('think_able_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point('call_small_eye')
