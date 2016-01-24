6e6fe567c77759bc7e1d8eacb5d2eda50dedd3ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 13:16:34 -0500

    "mv: be super/way/hand/year/feel_thing_under_time/part.py, super/seem_eye_above_long_group/important_case/case_and_bad_child/other_point_or_work/important_man/point.py or super/first_thing/great_man/company_and_other_number/year_or_few_year.py over few day"

---
+++ super/way/hand/year/feel_thing_under_time/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    same_person(str_arg)
+    print('problem')
+
+def same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('do_way_up_company')
