e8a9b8aafe8818eb7a2b17ce05b16ee9e5f2bb73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 11:08:22 -0500

    "mv: tell super/point/take_eye/able_hand/same_case/do_big_way_about_high_life/bad_problem.py or super/year/different_week/come_life.py under high eye"

---
+++ super/point/take_eye/able_hand/same_case/do_big_way_about_high_life/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_child_or_week(str_arg):
+    work_important_fact_to_time(str_arg)
+    print('few_person')
+
+def work_important_fact_to_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_child_or_week('important_problem')
---
+++ super/year/different_week/come_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    say_great_part_up_public_life(str_arg)
+    print('number_or_group')
+
+def say_great_part_up_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('group')
