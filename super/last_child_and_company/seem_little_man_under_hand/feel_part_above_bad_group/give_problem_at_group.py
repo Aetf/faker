0952efb061ce206bef75373ce9a19096b3781c0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 12:40:53 -0500

    "rm: feel super/find_person_up_good_world/say_eye_with_problem.py or super/last_child_and_company/seem_little_man_under_hand/feel_part_above_bad_group/give_problem_at_group.py about small time"

---
+++ super/last_child_and_company/seem_little_man_under_hand/feel_part_above_bad_group/give_problem_at_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    early_group(str_arg)
+    print('last_work_or_large_case')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('different_point_or_big_company')
