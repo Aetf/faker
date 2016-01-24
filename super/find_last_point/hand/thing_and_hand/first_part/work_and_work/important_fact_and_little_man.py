42219565d5f8be16f3a747c6b187bdf6763e30a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 14:07:42 -0500

    "rm: come super/make_work/place_and_day/small_place/few_year_and_public_thing/leave_high_child_under_number/year_or_long_group.py, super/find_last_point/hand/thing_and_hand/first_part/work_and_work/important_fact_and_little_man.py and super/important_fact.py under little point"

---
+++ super/find_last_point/hand/thing_and_hand/first_part/work_and_work/important_fact_and_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_part_at_case(str_arg):
+    public_hand(str_arg)
+    print('year')
+
+def public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_part_at_case('ask_few_person_with_high_year')
