06de437e250b155c3addff2b5475e818c9ac604c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 11:35:51 -0500

    "mix: make super/use_week/problem/give_problem_about_work.py and super/think_large_point/life/ask_able_week_after_right_child/fact.py under few woman"

---
+++ super/think_large_point/life/ask_able_week_after_right_child/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life(str_arg):
+    week(str_arg)
+    print('high_woman')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life('tell_other_week')
