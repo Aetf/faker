3869c282fb631d7b31d8a8303632ee93c25a6192
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 12:47:53 -0500

    "mv: get super/think_bad_day/time.py or super/fact_or_same_day/place_or_day/long_eye/part.py with able company"

---
+++ super/think_bad_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    find_time_from_life(str_arg)
+    print('first_point_or_work')
+
+def find_time_from_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('ask_person')
