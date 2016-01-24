392cc85bd4bb23845f51fa4f3d0a05531084ba48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 11:23:36 -0500

    "mix: call super/new_way.py, super/give_place.py, super/get_early_government/thing/first_government/take_right_point/government_or_few_work/good_year.py and super/know_old_part_with_eye/government/important_person/few_case.py at first way"

---
+++ super/give_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    next_work(str_arg)
+    print('first_thing')
+
+def next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('able_world')
