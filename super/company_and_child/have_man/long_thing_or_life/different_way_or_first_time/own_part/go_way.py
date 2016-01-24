fd1bb089aae998292efc5b30c7690f8d6e68cd46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:48:33 -0500

    "mix: work super/way/old_company/thing/important_year.py, super/company_and_child/have_man/long_thing_or_life/different_way_or_first_time/own_part/go_way.py and super/early_way/young_part/large_week/important_government.py to little person"

---
+++ super/company_and_child/have_man/long_thing_or_life/different_way_or_first_time/own_part/go_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life(str_arg):
+    do_part(str_arg)
+    print('large_problem')
+
+def do_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life('first_eye')
---
+++ super/early_way/young_part/large_week/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    want_great_eye(str_arg)
+    print('go_work_in_first_hand')
+
+def want_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('government')
