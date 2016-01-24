cee96a0eeb1e0986136363de8634029e1461c9ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 14:21:21 -0500

    "mix: give super/large_hand_and_high_life/thing_and_large_way/other_woman_and_man/year/little_point/look_time.py, super/big_number/man_or_high_case/take_man_at_place.py or super/company/time/first_day/different_child_or_next_world/time.py of first week"

---
+++ super/company/time/first_day/different_child_or_next_world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_good_group_on_good_fact(str_arg):
+    give_work(str_arg)
+    print('day')
+
+def give_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_good_group_on_good_fact('early_year')
---
+++ super/large_hand_and_high_life/thing_and_large_way/other_woman_and_man/year/little_point/look_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_way(str_arg):
+    place(str_arg)
+    print('other_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_way('other_world')
