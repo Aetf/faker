54a68f9383b51d93b26786533229a29f09f3e81c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 14:40:54 -0500

    "add: take super/know_high_day_of_able_work/public_child/seem_child/little_work.py, super/try_year_on_big_life/think_place_up_child/high_man_and_place.py or super/other_week_and_work/try_right_man_at_next_eye/know_little_child_with_young_hand.py under first child"

---
+++ super/other_week_and_work/try_right_man_at_next_eye/know_little_child_with_young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_child_in_little_person(str_arg):
+    get_little_day_about_fact(str_arg)
+    print('year')
+
+def get_little_day_about_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_child_in_little_person('long_point_or_problem')
---
+++ super/try_year_on_big_life/think_place_up_child/high_man_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_world(str_arg):
+    point(str_arg)
+    print('problem_and_few_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_world('go_part')
