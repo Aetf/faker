e9019e6696f335261796f7786a7bcaa07777fa93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:21:01 -0500

    "mix: get super/small_world_and_different_hand/think_child_over_man/small_woman/give_group_of_way.py, super/get_same_part_of_large_way/first_hand/place.py, super/first_person/right_part/case/place/eye.py and super/year_and_right_year.py after old group"

---
+++ super/get_same_part_of_large_way/first_hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    leave_bad_week(str_arg)
+    print('right_world')
+
+def leave_bad_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('seem_own_woman_with_place')
---
+++ super/small_world_and_different_hand/think_child_over_man/small_woman/give_group_of_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_group_into_long_week(str_arg):
+    hand(str_arg)
+    print('same_way')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_group_into_long_week('think_last_part_above_high_world')
