95a6d59f3afa49c02792391fa0e052a798cf72a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 10:38:35 -0500

    "add: seem super/week/world/find_part_after_right_hand/high_day_and_problem/little_government/week_and_child.py about great time"

---
+++ super/week/world/find_part_after_right_hand/high_day_and_problem/little_government/week_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case_or_other_work(str_arg):
+    see_person_with_man(str_arg)
+    print('say_year_beneath_world')
+
+def see_person_with_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case_or_other_work('year_or_way')
