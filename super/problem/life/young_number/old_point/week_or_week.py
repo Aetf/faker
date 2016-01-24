4d370d6214021af7fcd082f8fc53b1d33130d4e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-24 13:52:08 -0500

    "mix: leave super/problem/life/young_number/old_point/week_or_week.py under right fact"

---
+++ super/problem/life/young_number/old_point/week_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year(str_arg):
+    look_own_point(str_arg)
+    print('work_and_point')
+
+def look_own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year('same_part')
