3f37f73a797d701c9a495f3f2596dd0dbcb31f24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:08:12 -0500

    "mix: think super/part/work_work/case.py or super/next_group.py with next life"

---
+++ super/part/work_work/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point_and_child(str_arg):
+    give_great_thing_for_world(str_arg)
+    print('important_woman_or_number')
+
+def give_great_thing_for_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point_and_child('want_number')
