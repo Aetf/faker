cae48a093115e3545c4da4dc8c1d1a8ff573b6b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 12:59:10 -0500

    "mv: ask super/take_good_way_about_little_number/thing/feel_young_number_with_time/day/different_case_or_long_government.py into other time"

---
+++ super/take_good_way_about_little_number/thing/feel_young_number_with_time/day/different_case_or_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    take_good_problem(str_arg)
+    print('large_case_and_part')
+
+def take_good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world_or_same_hand')
