56a48a8aba1ffd0a1b5d573b00f582e0fa3fec49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:38:29 -0500

    "rm: have super/different_way.py or super/last_company/work.py by different company"

---
+++ super/different_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    young_case(str_arg)
+    print('leave_new_point_with_able_way')
+
+def young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('find_company')
---
+++ super/last_company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group_and_large_life(str_arg):
+    try_woman(str_arg)
+    print('last_place')
+
+def try_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group_and_large_life('right_problem')
