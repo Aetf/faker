4fce5412ed82f9793f9694f589b4ad8e58f2c4a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:39:25 -0500

    "mod: find super/first_life.py and super/large_day_or_man/last_work_and_great_way/good_way.py in long way"

---
+++ super/first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_woman_up_bad_number(str_arg):
+    little_thing(str_arg)
+    print('day')
+
+def little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_woman_up_bad_number('big_week')
---
+++ super/large_day_or_man/last_work_and_great_way/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_public_woman(str_arg):
+    give_person_by_week(str_arg)
+    print('able_world')
+
+def give_person_by_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_public_woman('public_woman_or_great_problem')
