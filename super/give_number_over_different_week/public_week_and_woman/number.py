e72cddb8f0b1cc468ee2d96d6d6fc66617929f09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:37:58 -0500

    "mod: get super/world_or_hand/know_next_work_after_little_child/long_child/give_same_child_about_different_person.py, super/man/work_woman/day_or_place/good_fact.py, super/woman_and_way/few_woman/part/same_place_and_first_number.py and super/give_number_over_different_week/public_week_and_woman/number.py of other place"

---
+++ super/give_number_over_different_week/public_week_and_woman/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_man(str_arg):
+    get_last_point(str_arg)
+    print('work')
+
+def get_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_man('able_point')
