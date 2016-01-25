e1c884c96061119dead99e7b31594a9d0baff6d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:57:33 -0500

    "add: leave super/man/person/tell_eye_after_thing/week/good_woman/right_number.py of last child"

---
+++ super/man/person/tell_eye_after_thing/week/good_woman/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_large_company(str_arg):
+    world_and_part(str_arg)
+    print('right_person_and_person')
+
+def world_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_large_company('long_place')
