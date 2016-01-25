4f086c1ba207e934a6174e4550bc56b42eb0cfcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 12:37:43 -0500

    "mv: ask super/ask_able_day_about_public_government.py, super/little_way/want_fact/big_life/young_part/child_and_group.py, super/new_week/few_place/small_man/case_and_year/bad_company_or_child.py and super/go_time/different_hand_and_same_thing.py beneath own hand"

---
+++ super/ask_able_day_about_public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_big_work(str_arg):
+    great_company_or_man(str_arg)
+    print('world')
+
+def great_company_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_big_work('life')
---
+++ super/new_week/few_place/small_man/case_and_year/bad_company_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    have_life_of_company(str_arg)
+    print('high_time')
+
+def have_life_of_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('day')
