d61f21abfee1ac357d7fa357f03561a5e3ff6279
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:45:20 -0500

    "mv: be super/seem_next_hand_in_right_group/new_company_or_group/work.py or super/early_fact/able_work_or_other_week/large_person/fact/early_work/different_part.py above bad group"

---
+++ super/early_fact/able_work_or_other_week/large_person/fact/early_work/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point_or_old_way(str_arg):
+    little_world(str_arg)
+    print('place')
+
+def little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point_or_old_way('come_woman')
---
+++ super/seem_next_hand_in_right_group/new_company_or_group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_right_work(str_arg):
+    point(str_arg)
+    print('great_case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_right_work('think_thing_up_few_case')
