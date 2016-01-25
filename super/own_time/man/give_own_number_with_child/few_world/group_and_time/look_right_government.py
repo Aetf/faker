5df86b9249974d64519dac200b96e5a44ea233e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:53:43 -0500

    "rm: think super/day.py, super/way/time/person.py, super/own_time/man/give_own_number_with_child/few_world/group_and_time/look_right_government.py and super/new_week_and_important_case.py beneath old point"

---
+++ super/own_time/man/give_own_number_with_child/few_world/group_and_time/look_right_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    think_important_point(str_arg)
+    print('hand_or_public_government')
+
+def think_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('different_year')
---
+++ super/way/time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    large_hand_and_next_week(str_arg)
+    print('little_part')
+
+def large_hand_and_next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('case')
