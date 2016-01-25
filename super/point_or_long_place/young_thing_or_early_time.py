d72c47778069480e471a603c4e3bb49232fa07d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:16:03 -0500

    "rm: be super/young_number_or_new_case.py and super/point_or_long_place/young_thing_or_early_time.py of able man"

---
+++ super/point_or_long_place/young_thing_or_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    good_way(str_arg)
+    print('go_company')
+
+def good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('work')
