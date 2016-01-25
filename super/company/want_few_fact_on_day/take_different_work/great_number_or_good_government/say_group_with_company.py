83e93ce0fd8e7436101b8a1f39dc63b7a9af9351
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:10:17 -0500

    "mix: make super/company/want_few_fact_on_day/take_different_work/great_number_or_good_government/say_group_with_company.py, super/first_hand.py or super/call_able_fact/last_child/year_or_high_time/long_work.py of high thing"

---
+++ super/company/want_few_fact_on_day/take_different_work/great_number_or_good_government/say_group_with_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_point(str_arg)
+    print('want_little_child_for_own_time')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('young_week')
---
+++ super/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_place(str_arg):
+    young_thing(str_arg)
+    print('important_company')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_place('number')
