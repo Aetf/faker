d82daee898f45b7f32dc524de22bbad87b78a6b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:03:55 -0500

    "add: see super/good_way/try_company/problem_or_person.py from few fact"

---
+++ super/good_way/try_company/problem_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    same_eye(str_arg)
+    print('bad_thing')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('hand')
