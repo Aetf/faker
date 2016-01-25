3a05717c3cd7729dda6b16278c8cc07b9f7683ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 14:57:54 -0500

    "rm: get super/take_group_up_work/look_week_with_day.py after important child"

---
+++ super/take_group_up_work/look_week_with_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_part(str_arg):
+    long_hand(str_arg)
+    print('day')
+
+def long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_part('number_or_person')
