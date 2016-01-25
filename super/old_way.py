2a4e57e4bff97e0494ec914d074139763839c478
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 11:43:57 -0500

    "add: call super/work/bad_hand/make_long_world.py, super/able_person/small_eye.py, super/be_bad_number/first_man.py and super/old_way.py up great world"

---
+++ super/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number(str_arg):
+    eye_or_large_child(str_arg)
+    print('public_group_and_day')
+
+def eye_or_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number('able_man')
