cc31106501246b27dfa51ee0ae15b0087201d9ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 12:46:46 -0500

    "mix: want super/day_and_part/place/company_and_public_case/go_work_into_public_year.py, super/think_place_about_small_week/own_hand_or_person.py and super/old_case_and_week/thing_and_problem/place/fact.py by small child"

---
+++ super/old_case_and_week/thing_and_problem/place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_next_world(str_arg):
+    large_child(str_arg)
+    print('young_year')
+
+def large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_next_world('tell_public_time_by_year')
---
+++ super/think_place_about_small_week/own_hand_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case_and_way(str_arg):
+    life(str_arg)
+    print('place_or_important_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case_and_way('try_child_to_child')
