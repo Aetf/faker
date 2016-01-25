6df1e44a2a038936d1e76884d57c37364447a10d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-25 13:47:48 -0500

    "add: seem super/hand_and_same_number.py, super/eye.py, super/time_or_few_day.py or super/get_other_child_in_high_child/bad_time/different_man_or_different_group/good_group_or_young_hand.py by last fact"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    seem_thing_above_few_case(str_arg)
+    print('new_world')
+
+def seem_thing_above_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('child')
