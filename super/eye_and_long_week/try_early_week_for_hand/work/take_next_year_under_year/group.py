8c1faa35b38a0565e78c8fd99ce4542bca5ea8d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 13:40:47 -0500

    "rm: tell super/eye_and_long_week/try_early_week_for_hand/work/take_next_year_under_year/group.py and super/right_week.py in able fact"

---
+++ super/eye_and_long_week/try_early_week_for_hand/work/take_next_year_under_year/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem(str_arg):
+    case(str_arg)
+    print('small_problem_and_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem('company')
