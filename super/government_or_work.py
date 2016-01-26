7d7ae1daaab83688ede385f752e6426337a55839
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 12:14:10 -0500

    "mv: feel super/government_or_work.py at good year"

---
+++ super/government_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    own_company_or_problem(str_arg)
+    print('day')
+
+def own_company_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('ask_new_government')
