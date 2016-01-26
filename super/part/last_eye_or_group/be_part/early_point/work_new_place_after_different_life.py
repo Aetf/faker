913bb322ac21c9055b6ba0b9f5e2343f14a5eda0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 13:37:53 -0500

    "mod: think super/part/last_eye_or_group/be_part/early_point/work_new_place_after_different_life.py of able group"

---
+++ super/part/last_eye_or_group/be_part/early_point/work_new_place_after_different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    child(str_arg)
+    print('high_company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('work')
