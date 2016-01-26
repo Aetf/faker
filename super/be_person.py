5a415db7791fb6d94271bdb6e400c3d35d9d0f8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 14:47:35 -0500

    "rm: work super/place_and_point/different_life_or_early_company/time_and_other_thing/great_company/own_thing.py, super/be_person.py and super/place/want_group_in_child/new_eye/hand/same_time_or_long_eye.py by new person"

---
+++ super/be_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem_and_great_point(str_arg):
+    use_year(str_arg)
+    print('try_different_problem')
+
+def use_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem_and_great_point('large_man_and_case')
---
+++ super/place_and_point/different_life_or_early_company/time_and_other_thing/great_company/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case_or_small_company(str_arg):
+    point_or_week(str_arg)
+    print('day')
+
+def point_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case_or_small_company('small_year_or_other_woman')
