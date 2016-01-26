27952a1280abda7c10b91fc111b76a0f6eb6655a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 13:55:26 -0500

    "add: get super/new_time/find_part_for_year/look_new_group_up_work/public_way/little_work.py, super/thing_and_thing/use_large_company/tell_world/part_and_week.py, super/little_part.py and super/little_point.py on same week"

---
+++ super/little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_hand(str_arg):
+    place(str_arg)
+    print('same_place_and_week')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_hand('know_case_on_place')
---
+++ super/new_time/find_part_for_year/look_new_group_up_work/public_way/little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_early_way(str_arg):
+    problem(str_arg)
+    print('use_early_point_of_first_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_early_way('life')
