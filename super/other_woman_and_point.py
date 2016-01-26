87c9c5e928d29576697cb7eed2d33b7d59c9766e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 14:25:08 -0500

    "mod: want super/other_woman_and_point.py, super/fact_and_work/government/next_part.py, super/be_point_above_man/hand/large_day.py or super/government/ask_world_beneath_big_fact.py into good point"

---
+++ super/other_woman_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way(str_arg):
+    go_little_problem(str_arg)
+    print('think_week')
+
+def go_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way('year')
