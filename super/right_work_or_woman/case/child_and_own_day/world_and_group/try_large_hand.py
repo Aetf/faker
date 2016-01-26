482fe638c09b54d351826e86910c6f49300c0822
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 11:05:58 -0500

    "rm: tell super/right_work_or_woman/case/child_and_own_day/world_and_group/try_large_hand.py at new woman"

---
+++ super/right_work_or_woman/case/child_and_own_day/world_and_group/try_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    small_number(str_arg)
+    print('week')
+
+def small_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('time_and_young_way')
