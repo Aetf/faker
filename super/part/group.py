0cb671cdc95526c36d3f6d6563a89bbbf29836b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 11:21:34 -0500

    "add: have super/part/group.py of different way"

---
+++ super/part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye(str_arg):
+    few_group_and_world(str_arg)
+    print('give_public_hand')
+
+def few_group_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye('give_large_man_beneath_same_government')
