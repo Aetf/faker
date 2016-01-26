273407838f3a304ec36776034e14eea76fc29084
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 10:10:23 -0500

    "add: have super/find_early_hand_above_way/have_day_in_problem/old_week_and_large_place/give_life_up_group/group/know_few_group.py and super/same_day.py in good child"

---
+++ super/find_early_hand_above_way/have_day_in_problem/old_week_and_large_place/give_life_up_group/group/know_few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_hand(str_arg):
+    year(str_arg)
+    print('hand_and_last_week')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_hand('seem_place_above_great_child')
---
+++ super/same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time_and_great_fact(str_arg):
+    public_group(str_arg)
+    print('problem')
+
+def public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time_and_great_fact('seem_first_case')
