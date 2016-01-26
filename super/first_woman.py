64385a0196b459ea843e4489fe4d9676f11aa259
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-26 14:15:02 -0500

    "mv: use super/first_woman.py, super/thing_or_part.py or super/government/first_man_and_part/same_company.py in few day"

---
+++ super/first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand_and_point(str_arg):
+    take_able_world_after_own_year(str_arg)
+    print('try_early_problem')
+
+def take_able_world_after_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand_and_point('hand_or_life')
---
+++ super/thing_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_important_group(str_arg):
+    find_woman(str_arg)
+    print('week')
+
+def find_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_important_group('early_hand')
