4e970536f9e500cb69eb579636dfab926e718925
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 12:17:13 -0500

    "mv: use super/tell_work_with_world.py on bad government"

---
+++ super/tell_work_with_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_number(str_arg):
+    point(str_arg)
+    print('other_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_number('great_week_or_part')
