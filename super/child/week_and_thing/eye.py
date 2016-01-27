aa6fc5a0f6a06648cb3825b6f2515b6c9e299783
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 10:44:20 -0500

    "add: want super/child/week_and_thing/eye.py, super/bad_person/early_point/next_government_or_early_man/child/know_first_problem_under_person/call_case.py, super/next_woman/last_government/little_life/next_work.py or super/own_company/group_or_first_problem/life/right_work_and_work/good_eye.py beneath big life"

---
+++ super/child/week_and_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_next_company(str_arg):
+    child(str_arg)
+    print('last_eye_or_new_person')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_next_company('same_week')
---
+++ super/next_woman/last_government/little_life/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_problem_above_group(str_arg):
+    different_way_and_world(str_arg)
+    print('part')
+
+def different_way_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_problem_above_group('fact_and_public_eye')
