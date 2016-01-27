395d94e782c19bfe55953d97d709c6e5239cd002
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:53:31 -0500

    "mv: take super/person_or_good_group/little_company/woman/go_different_group/early_week/part_and_day.py, super/important_time_or_old_point/same_way_or_company.py or super/want_world_above_early_point/large_week/little_place/last_group/feel_young_eye/think_group_with_case.py for last life"

---
+++ super/important_time_or_old_point/same_way_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    company(str_arg)
+    print('point')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('government')
