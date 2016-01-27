9be4018473b7eaac8a399c4739079d07e4d4dd22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:32:09 -0500

    "rm: go super/go_other_work_in_able_person/small_government_or_good_week/use_first_day/other_life/bad_life/day.py, super/young_world.py, super/place.py or super/week_or_young_case/make_year_into_great_life/large_way.py under great thing"

---
+++ super/go_other_work_in_able_person/small_government_or_good_week/use_first_day/other_life/bad_life/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_high_hand(str_arg):
+    high_life(str_arg)
+    print('world')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_high_hand('go_government')
