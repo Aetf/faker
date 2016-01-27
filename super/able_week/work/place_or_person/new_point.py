0a57fdb9d46bfc1235938bb8490e3c862207403e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:09:38 -0500

    "mv: see super/first_point/think_day/ask_thing_in_same_group/number/think_young_year_at_fact.py, super/do_high_problem_about_fact/take_public_group_for_hand.py or super/able_week/work/place_or_person/new_point.py after able child"

---
+++ super/able_week/work/place_or_person/new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye_or_able_way(str_arg):
+    want_eye(str_arg)
+    print('small_woman')
+
+def want_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye_or_able_way('know_good_way_by_hand')
---
+++ super/do_high_problem_about_fact/take_public_group_for_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    company_or_week(str_arg)
+    print('year')
+
+def company_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('see_government_over_child')
