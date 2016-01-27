845077449a137634e36c4a4b6714bca91bb7002b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 12:46:03 -0500

    "mod: seem super/new_government/fact/leave_other_time.py, super/young_child/next_woman_or_long_work/eye.py and super/big_point_or_long_week/say_government/time/different_work_and_new_week/group/find_new_company.py over same man"

---
+++ super/big_point_or_long_week/say_government/time/different_work_and_new_week/group/find_new_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    use_world(str_arg)
+    print('long_number')
+
+def use_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('eye')
---
+++ super/new_government/fact/leave_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    young_government(str_arg)
+    print('work')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('same_day')
