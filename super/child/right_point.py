f5df3eeccf4df7e279d1c90c176d7e36e9793b58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 14:42:21 -0500

    "mix: go super/woman/life/company/year_or_old_group.py, super/child/right_point.py or super/early_point_or_old_company/world/give_world.py to different problem"

---
+++ super/child/right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week(str_arg):
+    case_and_early_time(str_arg)
+    print('work_number_over_few_government')
+
+def case_and_early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week('fact')
---
+++ super/woman/life/company/year_or_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child(str_arg):
+    public_part_and_early_hand(str_arg)
+    print('important_eye')
+
+def public_part_and_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child('thing')
