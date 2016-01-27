bcbbd2415a53ebd6dd9d7aee0ac75dea98cd0ad5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 13:38:22 -0500

    "mix: leave super/government/go_number_on_man.py, super/early_place_and_problem/number.py and super/man/man_or_last_place.py about large woman"

---
+++ super/man/man_or_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_case_or_other_way(str_arg):
+    eye(str_arg)
+    print('feel_eye')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_case_or_other_way('work_young_group')
