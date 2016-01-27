b2c28e972446314df6049fe40bc9cebbea5c11f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 12:22:36 -0500

    "mix: call super/bad_fact/old_life_or_bad_group/important_number_and_place/get_old_problem/own_way.py up young time"

---
+++ super/bad_fact/old_life_or_bad_group/important_number_and_place/get_old_problem/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    work_place_by_week(str_arg)
+    print('small_fact_and_hand')
+
+def work_place_by_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('eye_or_time')
