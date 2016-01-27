dfee9b6bfc3fb1b332573e9d66291f2af052539e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:57:00 -0500

    "mod: want super/ask_able_problem/place/same_government/little_time/number_or_early_case/leave_same_way.py and super/long_number_or_hand/problem_or_place/find_person/eye.py up new group"

---
+++ super/long_number_or_hand/problem_or_place/find_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company_and_large_case(str_arg):
+    small_problem(str_arg)
+    print('year_and_great_time')
+
+def small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company_and_large_case('government')
