7c398c0501ca3022d7b044e0cbbcf8a2e58d8fd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:13:49 -0500

    "rm: get super/same_life/same_year_and_person.py and super/way/go_first_man_into_group/few_eye_and_day/try_group/feel_next_time.py beneath right man"

---
+++ super/way/go_first_man_into_group/few_eye_and_day/try_group/feel_next_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    little_life(str_arg)
+    print('thing_or_own_child')
+
+def little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('week')
