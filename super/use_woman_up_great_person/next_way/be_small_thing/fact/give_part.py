7eb7c0a22552ef54f10bbeac43c6dd2ba42d490d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:33:39 -0500

    "mv: have super/use_woman_up_great_person/next_way/be_small_thing/fact/give_part.py beneath able case"

---
+++ super/use_woman_up_great_person/next_way/be_small_thing/fact/give_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    big_man_and_small_year(str_arg)
+    print('part_or_new_number')
+
+def big_man_and_small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('own_problem')
