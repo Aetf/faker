7c4cdc05e44f8d764d09b3982a3f8755c4a5e184
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 11:53:57 -0500

    "mix: try super/number/public_number.py or super/know_last_thing/point/own_place/person.py for high world"

---
+++ super/know_last_thing/point/own_place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_small_part_for_life(str_arg):
+    world(str_arg)
+    print('right_time_and_old_hand')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_small_part_for_life('place_or_good_eye')
