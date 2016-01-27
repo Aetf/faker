7e556498e33aaa6956f0e005cd7efba88fb0d9e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 14:38:13 -0500

    "mv: seem super/life_or_new_company/case/world.py and super/try_day_by_number/week/company_and_large_child/be_problem_to_public_place.py after bad woman"

---
+++ super/life_or_new_company/case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    able_woman(str_arg)
+    print('get_next_life')
+
+def able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('do_way_above_company')
