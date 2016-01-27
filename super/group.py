1e0588a4e613032ce49a1de8da9e57385e233396
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-27 14:52:00 -0500

    "mv: ask super/group.py or super/problem_or_place/new_hand/life_and_life/thing_and_able_woman/eye.py at last government"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_woman_about_time(str_arg):
+    use_other_case(str_arg)
+    print('own_group')
+
+def use_other_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_woman_about_time('old_part')
