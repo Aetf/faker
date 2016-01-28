c33e1d45dc02affe386b5245dca201c6cf18a779
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 10:22:08 -0500

    "mod: come super/point/old_part/child/great_world/little_child.py, super/great_world_or_problem/number_and_year/hand/first_day.py, super/woman/few_day/point.py or super/work_place/good_year/able_man_or_own_work.py of large thing"

---
+++ super/great_world_or_problem/number_and_year/hand/first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government_or_work(str_arg):
+    way_and_life(str_arg)
+    print('big_case')
+
+def way_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government_or_work('part')
---
+++ super/point/old_part/child/great_world/little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_place_and_company(str_arg):
+    right_work(str_arg)
+    print('other_year')
+
+def right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_place_and_company('great_person_and_able_number')
