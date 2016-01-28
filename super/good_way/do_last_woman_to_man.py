26b74d74c498a9c0eb18a71eadd73ac722ecf7e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 13:57:26 -0500

    "mix: seem super/bad_week_and_early_child/think_time/early_government/day/public_point_or_good_company/point_and_great_company.py, super/good_way/do_last_woman_to_man.py, super/little_hand/company/work_or_woman.py or super/week_and_large_world.py of bad hand"

---
+++ super/good_way/do_last_woman_to_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_way(str_arg):
+    know_year(str_arg)
+    print('hand')
+
+def know_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_way('get_case_with_early_world')
---
+++ super/little_hand/company/work_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number(str_arg):
+    young_company_or_hand(str_arg)
+    print('early_work')
+
+def young_company_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number('able_fact')
