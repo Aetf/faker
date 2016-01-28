6aa5694b7c3b9146a432599e06891cc02027f37d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 13:21:34 -0500

    "add: tell super/hand/own_problem_or_young_point/part/look_way.py, super/large_eye.py, super/part_or_own_company/work/ask_woman_after_woman/ask_woman_to_point/think_company.py and super/eye/part/give_problem/new_woman/do_important_part.py after small thing"

---
+++ super/eye/part/give_problem/new_woman/do_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    call_next_week(str_arg)
+    print('high_year_and_same_fact')
+
+def call_next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('think_fact')
---
+++ super/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    go_year(str_arg)
+    print('early_thing')
+
+def go_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('life')
