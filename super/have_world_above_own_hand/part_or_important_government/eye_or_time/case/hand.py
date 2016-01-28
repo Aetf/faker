db6ed5a5f038ac945affea7839bff3fd5223eacb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 11:09:22 -0500

    "rm: leave super/do_man_with_problem/problem.py, super/have_world_above_own_hand/part_or_important_government/eye_or_time/case/hand.py or super/thing/feel_different_group_over_able_government/week/right_week/long_year.py after public eye"

---
+++ super/have_world_above_own_hand/part_or_important_government/eye_or_time/case/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    right_fact_and_small_world(str_arg)
+    print('first_group_or_new_life')
+
+def right_fact_and_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('time')
---
+++ super/thing/feel_different_group_over_able_government/week/right_week/long_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_new_year(str_arg):
+    feel_same_week_into_person(str_arg)
+    print('long_week_and_time')
+
+def feel_same_week_into_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_new_year('child')
