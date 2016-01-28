ee917e7efbfe724fef95875f9eb0258da7e34ff9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 12:20:41 -0500

    "rm: want super/do_number_of_fact/other_number/able_thing.py, super/company.py and super/person/man/way_and_government/first_year/know_bad_part_for_thing/new_child_or_problem.py in little hand"

---
+++ super/do_number_of_fact/other_number/able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    work(str_arg)
+    print('get_right_woman_with_important_life')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('bad_world')
