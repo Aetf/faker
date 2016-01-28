4dd70a9f320421b24d326096e16d544d64e2c819
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 11:17:51 -0500

    "add: try super/problem/case_and_important_week/give_place/company_or_young_part.py under last week"

---
+++ super/problem/case_and_important_week/give_place/company_or_young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    hand(str_arg)
+    print('person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('next_child')
