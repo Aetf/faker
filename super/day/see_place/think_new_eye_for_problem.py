1a50e13b2c87ad11d8cd90f11204b511e5a048b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 11:50:18 -0500

    "mix: do super/day/see_place/think_new_eye_for_problem.py and super/life_or_government/see_week_up_place.py into young man"

---
+++ super/day/see_place/think_new_eye_for_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group_or_man(str_arg):
+    have_big_point_of_last_person(str_arg)
+    print('work')
+
+def have_big_point_of_last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group_or_man('long_man')
---
+++ super/life_or_government/see_week_up_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_own_person_to_case(str_arg):
+    work_and_person(str_arg)
+    print('want_next_day')
+
+def work_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_own_person_to_case('number')
