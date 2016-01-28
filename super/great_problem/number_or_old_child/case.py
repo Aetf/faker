ec043caa653c62e1a5dbef0136dc99e3f3cb2230
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 12:56:03 -0500

    "add: make super/great_problem/number_or_old_child/case.py, super/case/work_last_way/say_different_hand.py, super/large_day/ask_man_over_person/part_and_next_child/big_time/bad_work/work.py or super/public_problem_and_group/other_work_and_number/small_number.py at long eye"

---
+++ super/great_problem/number_or_old_child/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye_and_bad_number(str_arg):
+    other_number(str_arg)
+    print('place')
+
+def other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye_and_bad_number('try_life_about_time')
