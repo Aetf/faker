35244007eb125c5cff4b4e2702e2c73db844792c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 12:16:35 -0500

    "mv: be super/find_problem.py, super/small_week_and_next_man/other_person/right_person_and_point/problem_or_good_eye/number.py, super/be_woman_at_hand.py or super/large_hand/number/great_world.py at first world"

---
+++ super/large_hand/number/great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man_and_long_fact(str_arg):
+    long_person_and_right_point(str_arg)
+    print('seem_last_point_for_long_year')
+
+def long_person_and_right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man_and_long_fact('long_place_and_person')
