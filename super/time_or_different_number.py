dce2b0dee3604bf02b9de1dc1f2b28ee01daa2c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 13:28:35 -0500

    "add: feel super/time_or_different_number.py, super/number.py and super/point.py under early work"

---
+++ super/time_or_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_hand(str_arg):
+    use_young_case(str_arg)
+    print('problem')
+
+def use_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_hand('way')
