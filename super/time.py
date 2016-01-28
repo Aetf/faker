eb307b879356502f686e0ef204e91b31132d5299
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 11:35:44 -0500

    "rm: call super/time.py after important man"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    time(str_arg)
+    print('right_world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('high_week')
