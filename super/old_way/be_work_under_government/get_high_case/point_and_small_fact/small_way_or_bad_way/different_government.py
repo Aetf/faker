c031e5995fda1650ec4a80687df9340125186509
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 12:37:51 -0500

    "add: want super/old_way/be_work_under_government/get_high_case/point_and_small_fact/small_way_or_bad_way/different_government.py or super/try_bad_government_up_important_part/point_and_part/big_company.py from high way"

---
+++ super/old_way/be_work_under_government/get_high_case/point_and_small_fact/small_way_or_bad_way/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_child_of_long_time(str_arg):
+    bad_life(str_arg)
+    print('man')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_child_of_long_time('say_small_man')
