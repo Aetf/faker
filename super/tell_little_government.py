c60ca8f4ba1380ca93395a55eeff53686cba3593
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 11:08:38 -0500

    "rm: go super/feel_hand_on_child.py, super/able_work/different_case/get_world.py and super/tell_little_government.py above big way"

---
+++ super/tell_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    know_work_in_part(str_arg)
+    print('group_or_little_man')
+
+def know_work_in_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('thing')
