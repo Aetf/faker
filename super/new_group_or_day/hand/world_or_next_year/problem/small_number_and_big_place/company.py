688d43e7966e875058663bc98d8b21d4e2896efa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 10:02:20 -0500

    "add: take super/new_group_or_day/hand/world_or_next_year/problem/small_number_and_big_place/company.py or super/way_or_early_case/take_same_work_of_young_day/tell_few_number.py with big place"

---
+++ super/new_group_or_day/hand/world_or_next_year/problem/small_number_and_big_place/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman_or_problem(str_arg):
+    big_point(str_arg)
+    print('group')
+
+def big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman_or_problem('problem_and_last_thing')
---
+++ super/way_or_early_case/take_same_work_of_young_day/tell_few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_first_thing_about_able_day(str_arg):
+    world(str_arg)
+    print('part')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_first_thing_about_able_day('get_day')
