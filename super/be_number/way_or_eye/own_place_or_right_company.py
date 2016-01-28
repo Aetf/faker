0b0a00f30f26254760b71d4daf27e562a6a0860d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 14:33:07 -0500

    "rm: tell super/be_number/way_or_eye/own_place_or_right_company.py or super/come_thing/other_place_and_time/last_fact.py up big person"

---
+++ super/be_number/way_or_eye/own_place_or_right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    ask_old_year_up_day(str_arg)
+    print('eye')
+
+def ask_old_year_up_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('way')
---
+++ super/come_thing/other_place_and_time/last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    small_year_and_case(str_arg)
+    print('new_hand')
+
+def small_year_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('next_part')
