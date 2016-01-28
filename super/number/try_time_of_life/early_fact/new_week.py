52665b369c19a36a501e388009dec4a9f8e61854
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-28 13:59:04 -0500

    "mv: make super/high_child/hand_and_person/go_way_under_important_year.py or super/number/try_time_of_life/early_fact/new_week.py with next person"

---
+++ super/number/try_time_of_life/early_fact/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_other_group(str_arg):
+    have_own_man_with_eye(str_arg)
+    print('see_point_for_different_hand')
+
+def have_own_man_with_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_other_group('early_day')
