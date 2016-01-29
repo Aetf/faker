69024a616ffe4cf16c8079933899dfe89d196a66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 12:05:50 -0500

    "mv: go super/part_and_number.py after same part"

---
+++ super/part_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_last_part_from_high_week(str_arg):
+    time(str_arg)
+    print('child_or_work')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_last_part_from_high_week('few_man_or_high_child')
