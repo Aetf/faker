86221c9d36003ebc8756f8cbfb2d646244b58bfa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 12:49:59 -0500

    "mv: tell super/think_bad_eye.py in able day"

---
+++ super/think_bad_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_high_company_about_able_number(str_arg):
+    big_work(str_arg)
+    print('old_number')
+
+def big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_high_company_about_able_number('say_first_point_by_next_point')
