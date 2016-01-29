7f1a50b69921160c448f46572b191c01393a3682
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 14:17:27 -0500

    "mv: see super/world_and_man/public_group_or_group/person/year.py in last world"

---
+++ super/world_and_man/public_group_or_group/person/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week(str_arg):
+    point(str_arg)
+    print('hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week('early_way')
