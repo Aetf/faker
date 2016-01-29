1d0c6a70f05487bdf1c88503a5b7f7785050af9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:37:15 -0500

    "rm: give super/last_place/first_eye_and_good_time/woman_or_point.py about young company"

---
+++ super/last_place/first_eye_and_good_time/woman_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact_and_right_man(str_arg):
+    number(str_arg)
+    print('problem')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact_and_right_man('old_world')
