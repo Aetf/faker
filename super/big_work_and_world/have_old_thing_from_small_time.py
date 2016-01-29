2baf4b43ee806a8d815f99f4a157da32dcb9dce8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 11:30:57 -0500

    "rm: try super/big_work_and_world/have_old_thing_from_small_time.py or super/get_life_about_child/tell_way_above_point.py into first hand"

---
+++ super/big_work_and_world/have_old_thing_from_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point(str_arg):
+    company_and_new_life(str_arg)
+    print('right_day_or_place')
+
+def company_and_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point('problem')
