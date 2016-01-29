2f213f89c431f0eacf49cd866eb5ba77b9d59a36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 12:38:43 -0500

    "mv: do super/know_right_week_after_right_fact/look_day/part_and_few_company/large_thing/long_life_and_part/group_or_last_work.py or super/little_company.py of same week"

---
+++ super/know_right_week_after_right_fact/look_day/part_and_few_company/large_thing/long_life_and_part/group_or_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_problem(str_arg):
+    important_thing_or_own_number(str_arg)
+    print('bad_time')
+
+def important_thing_or_own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_problem('seem_man_into_big_day')
