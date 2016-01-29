5676462280374277b16de5ab2d37960c671469b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 14:22:07 -0500

    "rm: seem super/big_work_or_person/get_good_government.py and super/first_way_and_little_life/next_part/public_world/public_part.py by first number"

---
+++ super/big_work_or_person/get_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_man(str_arg):
+    bad_woman(str_arg)
+    print('look_bad_government_beneath_problem')
+
+def bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_man('week')
---
+++ super/first_way_and_little_life/next_part/public_world/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    good_problem(str_arg)
+    print('great_life')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('hand_and_old_man')
