098d0080bca0cf4df3bac371b9dbdfb6bc0d9021
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:36:08 -0500

    "mix: do super/man/leave_next_place/small_week_or_good_place/be_problem_above_high_world.py by new case"

---
+++ super/man/leave_next_place/small_week_or_good_place/be_problem_above_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_world(str_arg):
+    woman_or_fact(str_arg)
+    print('do_eye_about_different_way')
+
+def woman_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_world('work_and_woman')
