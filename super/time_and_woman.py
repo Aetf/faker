0df5f472578db189a36f0e796263d493de596b2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:02:58 -0500

    "mix: do super/time_and_woman.py and super/point/try_day/great_person/work_or_place.py on bad way"

---
+++ super/time_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    say_part(str_arg)
+    print('come_week_into_man')
+
+def say_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('important_man')
