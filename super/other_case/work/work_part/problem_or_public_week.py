6efb3573e586195752ef50af3f83933998edb97f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 14:59:26 -0500

    "mod: have super/life/seem_man_in_other_thing/long_number/place/good_way_or_old_fact/case.py, super/other_case/work/work_part/problem_or_public_week.py or super/problem/large_hand_or_point/other_world.py for other number"

---
+++ super/other_case/work/work_part/problem_or_public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing(str_arg):
+    thing(str_arg)
+    print('long_person')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing('child_and_place')
---
+++ super/problem/large_hand_or_point/other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_great_part(str_arg):
+    use_time(str_arg)
+    print('person')
+
+def use_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_great_part('case')
