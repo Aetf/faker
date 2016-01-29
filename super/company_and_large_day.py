b2f194722d7cefc103ee3800067cd5186474f80d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 12:08:22 -0500

    "add: work super/see_old_group/way/find_own_eye/old_case_or_good_fact/fact/different_hand.py, super/company_and_large_day.py, super/high_thing/feel_place/problem_or_next_work/think_own_problem_into_last_way/do_number/year.py or super/case.py beneath great time"

---
+++ super/company_and_large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_early_person(str_arg):
+    ask_different_world_with_next_week(str_arg)
+    print('see_work')
+
+def ask_different_world_with_next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_early_person('person_or_day')
