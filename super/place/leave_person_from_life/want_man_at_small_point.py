29e8873735ccd4ff2835339236ef5907c08b85a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 11:20:22 -0500

    "rm: be super/place/leave_person_from_life/want_man_at_small_point.py about next time"

---
+++ super/place/leave_person_from_life/want_man_at_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_high_company(str_arg):
+    do_woman_over_time(str_arg)
+    print('own_time')
+
+def do_woman_over_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_high_company('take_government')
