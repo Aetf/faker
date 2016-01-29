fef8b62ef21f2be9169aa96496bb4caaf90dbf7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:39:36 -0500

    "mv: seem super/same_point/bad_number/give_place/old_man/different_work/world_or_right_man.py of different fact"

--- super/same_point/bad_number/give_place/old_man/different_work/world_or_right_man.py
+++ super/same_point/bad_number/give_place/old_man/different_work/world_or_right_man.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def high_fact_and_right_man(str_arg):
+def right_hand_or_last_number(str_arg):
     number(str_arg)
-    print('problem')
+    print('use_work')
 
 def number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    high_fact_and_right_man('old_world')
+    right_hand_or_last_number('few_problem')
