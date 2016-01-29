ca48c338ef7d397cfa00549ba7163878ad485247
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 12:03:05 -0500

    "mod: give super/high_woman/see_own_person.py or super/child/different_part.py above big hand"

---
+++ super/high_woman/see_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_important_group(str_arg):
+    able_eye_or_government(str_arg)
+    print('think_small_work_of_government')
+
+def able_eye_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_important_group('person')
