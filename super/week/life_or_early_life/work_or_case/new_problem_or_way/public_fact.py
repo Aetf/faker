e5198333bf47321bc11f412ead18fb574335bdb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:06:40 -0500

    "rm: see super/week/life_or_early_life/work_or_case/new_problem_or_way/public_fact.py, super/say_high_person_for_new_woman/long_place/old_day/able_group_or_problem/thing/government.py, super/good_child.py or super/year/place/great_week_and_company.py from next group"

---
+++ super/week/life_or_early_life/work_or_case/new_problem_or_way/public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_case(str_arg):
+    year(str_arg)
+    print('public_number_and_public_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_case('different_time')
