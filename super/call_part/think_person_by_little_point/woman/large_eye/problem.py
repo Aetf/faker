81094e0e0cf69d16fa1af995b9f1ab5e5b1f227e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 10:23:25 -0500

    "mod: work super/call_part/think_person_by_little_point/woman/large_eye/problem.py after few problem"

---
+++ super/call_part/think_person_by_little_point/woman/large_eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    call_thing_with_great_eye(str_arg)
+    print('place')
+
+def call_thing_with_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('small_hand')
