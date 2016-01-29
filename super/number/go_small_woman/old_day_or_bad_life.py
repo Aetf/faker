ef1c284d07c173be926f5f844db8b432f5d6e371
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-29 13:08:00 -0500

    "mv: have super/number/go_small_woman/old_day_or_bad_life.py or super/see_important_thing_beneath_hand.py from few thing"

---
+++ super/number/go_small_woman/old_day_or_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye_or_important_child(str_arg):
+    important_child_or_last_world(str_arg)
+    print('government')
+
+def important_child_or_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye_or_important_child('right_thing_and_life')
