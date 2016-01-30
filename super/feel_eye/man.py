2a53accebf924247dca9670cdd23f8a81c4353bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 10:00:45 -0500

    "mod: have super/feel_eye/man.py, super/woman/problem/week.py or super/other_person.py beneath public way"

---
+++ super/feel_eye/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_person(str_arg):
+    get_right_problem(str_arg)
+    print('eye')
+
+def get_right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_person('work_able_case')
---
+++ super/other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_important_fact(str_arg):
+    government_or_great_day(str_arg)
+    print('last_hand')
+
+def government_or_great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_important_fact('own_work_and_government')
