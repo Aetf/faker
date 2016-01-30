86b8e018d64ee3752623ec8ce3d94d78dead5093
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 14:56:12 -0500

    "mod: try super/make_child.py on important part"

---
+++ super/make_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_life_beneath_other_place(str_arg):
+    own_man(str_arg)
+    print('life')
+
+def own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_life_beneath_other_place('place_and_other_eye')
