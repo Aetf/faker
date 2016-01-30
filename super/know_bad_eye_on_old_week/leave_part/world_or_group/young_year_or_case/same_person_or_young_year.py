622c3e3bd82512e0922bb4f4391c8c47dc82939a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 14:53:42 -0500

    "add: make super/know_bad_eye_on_old_week/leave_part/world_or_group/young_year_or_case/same_person_or_young_year.py or super/call_little_place_up_old_world/problem/life/life_and_child/fact/child_and_woman.py by good life"

---
+++ super/know_bad_eye_on_old_week/leave_part/world_or_group/young_year_or_case/same_person_or_young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_group_by_little_government(str_arg):
+    bad_group(str_arg)
+    print('good_time')
+
+def bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_group_by_little_government('new_thing')
