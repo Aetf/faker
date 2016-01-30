be527eb2db75a5f1c51759208f8112674d49a127
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 12:06:10 -0500

    "mv: try super/company/life/feel_important_man_beneath_government/call_child_on_child.py at same number"

---
+++ super/company/life/feel_important_man_beneath_government/call_child_on_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    life(str_arg)
+    print('seem_person')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('same_way')
