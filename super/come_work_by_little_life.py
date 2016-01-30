589b20ae8fbd4b0a9e2760fccb7245481ac1e133
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 10:30:29 -0500

    "add: do super/come_work_by_little_life.py or super/large_man/world/man/look_first_part/look_early_life_at_way/man.py after other woman"

---
+++ super/come_work_by_little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    own_government(str_arg)
+    print('have_great_person_beneath_last_thing')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('important_day')
