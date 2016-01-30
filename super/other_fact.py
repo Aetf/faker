0c987d5222a7b1c3c0dad9fc533567e448f4e5fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 14:46:21 -0500

    "mv: have super/other_fact.py under able hand"

---
+++ super/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_important_work(str_arg):
+    great_man(str_arg)
+    print('different_part')
+
+def great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_important_work('work_early_point_under_important_group')
