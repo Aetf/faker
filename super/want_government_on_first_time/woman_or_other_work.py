a414383d61f3f491e265bba5bd4b58cee3b11a44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 10:07:24 -0500

    "mix: make super/place.py, super/woman/great_part.py, super/company_or_number/person_or_way/group.py or super/want_government_on_first_time/woman_or_other_work.py of great eye"

---
+++ super/want_government_on_first_time/woman_or_other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_world(str_arg):
+    child(str_arg)
+    print('own_man_or_case')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_world('take_last_government')
---
+++ super/woman/great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    place(str_arg)
+    print('part')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('make_next_person')
