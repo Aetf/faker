1b9cb850534394c4fceff32cff9667b9111b8456
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:12:16 -0500

    "mv: say super/case/company/hand/long_company/ask_eye_of_public_person.py, super/feel_first_work.py, super/want_day_about_public_way/right_number.py or super/big_way/seem_part_from_different_week/same_man.py beneath new world"

---
+++ super/big_way/seem_part_from_different_week/same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing(str_arg):
+    time(str_arg)
+    print('take_year_at_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing('first_life_and_group')
---
+++ super/case/company/hand/long_company/ask_eye_of_public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    life(str_arg)
+    print('first_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('first_case')
