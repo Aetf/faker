1620c55f76b5758e086e1406819d8fed6b08c295
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 12:55:38 -0500

    "mod: be super/ask_small_case.py or super/world/bad_week_and_day/point.py over little place"

---
+++ super/ask_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_problem(str_arg):
+    last_time(str_arg)
+    print('same_part_and_government')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_problem('feel_government')
---
+++ super/world/bad_week_and_day/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_public_company_for_fact(str_arg):
+    man(str_arg)
+    print('first_thing')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_public_company_for_fact('high_eye_and_last_day')
