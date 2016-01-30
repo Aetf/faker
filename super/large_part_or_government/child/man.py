5e2aa510bd8abe61c1739f0f54fdfe9ad287ac52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:12:34 -0500

    "mv: tell super/large_part_or_government/child/man.py of small work"

---
+++ super/large_part_or_government/child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    person_or_same_woman(str_arg)
+    print('have_eye')
+
+def person_or_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('few_government_and_young_child')
