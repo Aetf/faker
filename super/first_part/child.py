91a934aa82e3925c3a566640b02661ceec816182
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:41:51 -0500

    "mv: try super/first_part/child.py above early day"

---
+++ super/first_part/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_able_point(str_arg):
+    thing(str_arg)
+    print('important_part')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_able_point('work')
