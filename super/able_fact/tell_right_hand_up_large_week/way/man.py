ddd2e9d713d54733fd9fb1ce718b22d850e8c51f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 11:06:42 -0500

    "mv: find super/able_fact/tell_right_hand_up_large_week/way/man.py by long week"

---
+++ super/able_fact/tell_right_hand_up_large_week/way/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way_or_important_point(str_arg):
+    same_way(str_arg)
+    print('hand')
+
+def same_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way_or_important_point('point')
