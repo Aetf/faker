ea511992b8e2fd809c893905964a3401ef99ef0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:02:09 -0500

    "add: look super/problem/hand_or_place/important_case/work/know_great_time_above_little_government.py, super/hand/thing.py or super/come_good_year_with_little_woman/ask_new_fact_from_government/bad_child/problem/own_place_and_young_person.py over high case"

---
+++ super/hand/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_old_person(str_arg):
+    child(str_arg)
+    print('next_time_and_government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_old_person('number')
