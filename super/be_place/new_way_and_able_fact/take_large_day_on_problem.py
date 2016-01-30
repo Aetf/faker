d3c94fa33f22530751a84407006757d4a4ca6107
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 14:43:52 -0500

    "mod: make super/be_place/new_way_and_able_fact/take_large_day_on_problem.py and super/little_eye/use_case/call_first_place_above_young_point/ask_important_work.py to young year"

---
+++ super/be_place/new_way_and_able_fact/take_large_day_on_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life(str_arg):
+    want_right_year(str_arg)
+    print('great_place')
+
+def want_right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life('world')
