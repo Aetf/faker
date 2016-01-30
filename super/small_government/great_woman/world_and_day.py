82d4404ec056ca59c53ebc8f565f5587bbbcce64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:08:49 -0500

    "mv: say super/small_government/great_woman/world_and_day.py over public life"

---
+++ super/small_government/great_woman/world_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_eye_and_own_group(str_arg):
+    place_and_woman(str_arg)
+    print('have_government_about_day')
+
+def place_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_eye_and_own_group('go_right_point_by_thing')
