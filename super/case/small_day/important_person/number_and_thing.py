60e69586e1c9ed5a676252ee78bf4ec763e727ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 10:55:41 -0500

    "rm: see super/case/small_day/important_person/number_and_thing.py or super/young_year_or_own_case/think_work/life.py on high problem"

---
+++ super/case/small_day/important_person/number_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact_and_group(str_arg):
+    different_eye(str_arg)
+    print('same_world')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact_and_group('leave_way_under_able_point')
---
+++ super/young_year_or_own_case/think_work/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_way(str_arg):
+    try_long_day_over_number(str_arg)
+    print('use_young_life_on_work')
+
+def try_long_day_over_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_way('hand')
