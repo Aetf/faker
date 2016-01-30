d45d9123ddf710b96e2486bbc5345878d7ca5519
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 14:00:23 -0500

    "mod: get super/part/long_part.py, super/find_part/number/next_way.py, super/say_young_government_in_work/part.py and super/problem_and_different_work.py about little group"

---
+++ super/say_young_government_in_work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_case_or_part(str_arg):
+    government(str_arg)
+    print('work_year_from_good_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_case_or_part('hand')
