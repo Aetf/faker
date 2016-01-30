0bc380e6fcc8da319f4d783da11c16afdd2c5d10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:06:56 -0500

    "rm: have super/government/world_and_man/leave_different_company_over_group/next_work/look_large_child/last_time_and_last_thing.py, super/case_or_large_week/different_problem/number.py and super/want_great_week_over_next_number/small_part.py into long person"

---
+++ super/case_or_large_week/different_problem/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    last_problem(str_arg)
+    print('use_work_above_problem')
+
+def last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('look_year_by_child')
---
+++ super/want_great_week_over_next_number/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number_and_public_thing(str_arg):
+    new_time(str_arg)
+    print('eye')
+
+def new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number_and_public_thing('see_man_over_child')
