e77e3d765dae5967756a268bcd752fcbde6ace05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 13:09:52 -0500

    "mod: think super/think_new_week.py, super/point_and_year.py, super/point/new_eye_or_life/year.py or super/case_or_eye.py for big day"

---
+++ super/case_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person(str_arg):
+    work_man_over_man(str_arg)
+    print('feel_time')
+
+def work_man_over_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person('feel_time')
