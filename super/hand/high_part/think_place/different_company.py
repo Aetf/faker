977d06b58da73244ca7f00c15aa41e95e3cf25fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-30 10:07:35 -0500

    "add: know super/hand/high_part/think_place/different_company.py from large number"

---
+++ super/hand/high_part/think_place/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case(str_arg):
+    life(str_arg)
+    print('feel_young_company_with_next_day')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case('fact')
