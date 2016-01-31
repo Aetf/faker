3b07c64e9c978b7cb7edab35abcee6083af0b97e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 12:51:54 -0500

    "mix: go super/place/own_child/new_man/eye.py, super/new_thing/early_government/man_and_time/early_case/number.py or super/little_group_or_week/work_or_last_man/child.py above high group"

---
+++ super/little_group_or_week/work_or_last_man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_other_point(str_arg):
+    little_year(str_arg)
+    print('high_child')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_other_point('day')
