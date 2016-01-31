e3bc85e73ffed794247fa07753a522dc212c2a75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:05:09 -0500

    "add: use super/year_or_important_work/try_last_problem/group/work_early_way_after_woman.py at large child"

---
+++ super/year_or_important_work/try_last_problem/group/work_early_way_after_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_young_number(str_arg):
+    little_fact(str_arg)
+    print('see_important_person')
+
+def little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_young_number('last_part_or_able_eye')
