a9bef83e8513f213cd75f21ea45a01df88cbea8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 12:05:58 -0500

    "rm: seem super/important_world/take_good_group/young_week/thing_and_group/large_point.py, super/long_hand/other_day/other_time.py or super/long_number/leave_first_place.py with new group"

---
+++ super/important_world/take_good_group/young_week/thing_and_group/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time_or_point(str_arg):
+    first_woman(str_arg)
+    print('young_thing')
+
+def first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time_or_point('long_part')
---
+++ super/long_hand/other_day/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_way(str_arg):
+    long_point(str_arg)
+    print('find_bad_year_over_fact')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_way('part')
