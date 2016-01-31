f982d11290fdee72e677a85ea6b73e0634d2d741
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 11:32:43 -0500

    "mv: try super/time/work/make_government.py, super/next_way/place/year/long_part_or_able_case/right_eye/point.py, super/group_and_problem/feel_life/hand/week.py and super/first_person_and_important_place/case/small_work_or_point.py in good company"

---
+++ super/first_person_and_important_place/case/small_work_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    able_life_and_other_hand(str_arg)
+    print('different_place')
+
+def able_life_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('man')
---
+++ super/next_way/place/year/long_part_or_able_case/right_eye/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    person(str_arg)
+    print('time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('point')
