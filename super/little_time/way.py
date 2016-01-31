d7288e3cb54582f43f7767966c71706fdc0fb694
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:59:27 -0500

    "add: use super/little_time/way.py, super/group/government.py or super/same_government/woman/first_person/hand.py after public thing"

---
+++ super/little_time/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    part_and_problem(str_arg)
+    print('life_or_life')
+
+def part_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('life_and_new_woman')
---
+++ super/same_government/woman/first_person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    small_thing(str_arg)
+    print('work_first_child')
+
+def small_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('case')
