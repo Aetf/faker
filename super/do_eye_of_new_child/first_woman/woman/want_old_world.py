6be61565821b22eee1e14fc5f82b6b7db67bcd6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:41:20 -0500

    "rm: be super/do_eye_of_new_child/first_woman/woman/want_old_world.py, super/part.py, super/few_child_and_great_number.py and super/good_woman/fact/public_woman.py for other point"

---
+++ super/do_eye_of_new_child/first_woman/woman/want_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point_or_small_life(str_arg):
+    good_problem(str_arg)
+    print('get_little_work')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point_or_small_life('young_eye')
