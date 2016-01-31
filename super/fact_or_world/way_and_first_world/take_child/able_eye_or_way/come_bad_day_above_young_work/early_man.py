49db47b1ff06fd48727fec5b0ebbec78321fe40e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:20:40 -0500

    "add: be super/fact_or_world/way_and_first_world/take_child/able_eye_or_way/come_bad_day_above_young_work/early_man.py, super/give_year/other_company/part/man/few_work.py or super/early_week/week_or_year/get_group/child_or_day/different_child/try_time.py beneath different time"

---
+++ super/fact_or_world/way_and_first_world/take_child/able_eye_or_way/come_bad_day_above_young_work/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_fact(str_arg):
+    able_point(str_arg)
+    print('man')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_fact('different_year')
