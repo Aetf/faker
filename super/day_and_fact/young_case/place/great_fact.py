3c531bbd6c797096c6a42688b54e702b6943706c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 11:06:51 -0500

    "mod: think super/day_and_fact/young_case/place/great_fact.py or super/woman_or_young_child/high_place/case_and_time/important_man/work_old_woman_to_person.py to great way"

---
+++ super/day_and_fact/young_case/place/great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_early_number(str_arg):
+    few_fact_and_point(str_arg)
+    print('company')
+
+def few_fact_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_early_number('group_and_way')
