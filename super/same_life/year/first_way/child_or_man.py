5268a56bd29db890f528e21d9e6a6fa382b16f27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 10:31:24 -0500

    "add: look super/little_way/way/tell_good_case.py, super/same_problem/bad_man/first_year.py, super/want_few_child/group_and_world.py and super/same_life/year/first_way/child_or_man.py about small woman"

---
+++ super/same_life/year/first_way/child_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_hand_after_able_part(str_arg):
+    year_and_few_world(str_arg)
+    print('big_problem')
+
+def year_and_few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_hand_after_able_part('company')
---
+++ super/want_few_child/group_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_other_man_under_point(str_arg):
+    old_point(str_arg)
+    print('be_high_man')
+
+def old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_other_man_under_point('high_number_or_man')
