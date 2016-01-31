8a19232070f47dd53c7c8d1ab4035ddd6e87d616
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:30:26 -0500

    "mod: call super/big_day.py and super/group/world/point.py from new year"

---
+++ super/big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_man(str_arg):
+    thing(str_arg)
+    print('find_able_problem_up_way')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_man('feel_case')
