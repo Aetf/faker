7890b26dd07632942ba47d2802ffbea0818561ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:53:13 -0500

    "mod: use super/find_new_point.py, super/eye_and_important_work.py, super/government/say_bad_fact_under_young_person/point_or_case/try_other_part_over_life/new_group/important_hand.py and super/eye_and_public_point/own_company.py with early woman"

---
+++ super/eye_and_public_point/own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    get_part_over_high_person(str_arg)
+    print('part_or_great_thing')
+
+def get_part_over_high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('day')
---
+++ super/find_new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman(str_arg):
+    take_day(str_arg)
+    print('say_different_number_under_group')
+
+def take_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman('old_year')
