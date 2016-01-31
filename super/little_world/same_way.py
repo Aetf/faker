879f0666c363ac60a98aa93690e0de0d47ac6333
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:12:58 -0500

    "add: tell super/week_or_way/own_week_and_problem/man/big_world_and_company/government_and_problem.py and super/little_world/same_way.py for old time"

---
+++ super/little_world/same_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman(str_arg):
+    own_year_or_thing(str_arg)
+    print('great_child')
+
+def own_year_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman('next_world')
---
+++ super/week_or_way/own_week_and_problem/man/big_world_and_company/government_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person(str_arg):
+    group(str_arg)
+    print('want_other_woman_in_work')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person('thing')
