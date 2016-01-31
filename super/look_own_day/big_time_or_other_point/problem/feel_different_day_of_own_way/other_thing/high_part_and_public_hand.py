8b21aeb610468a6e5612b9d1d058171edba55dbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 11:44:29 -0500

    "add: have super/ask_case_from_early_government/make_man/eye/fact/own_way_and_early_case/next_way.py, super/new_eye/own_government_or_different_point/make_point/child.py, super/look_own_day/big_time_or_other_point/problem/feel_different_day_of_own_way/other_thing/high_part_and_public_hand.py or super/be_young_fact/man_and_woman/old_child_or_thing/high_world_and_hand/other_man/public_week.py about good world"

---
+++ super/look_own_day/big_time_or_other_point/problem/feel_different_day_of_own_way/other_thing/high_part_and_public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    week(str_arg)
+    print('say_work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('try_small_year_over_last_place')
---
+++ super/new_eye/own_government_or_different_point/make_point/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_able_way(str_arg):
+    different_part_and_same_number(str_arg)
+    print('number')
+
+def different_part_and_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_able_way('case')
