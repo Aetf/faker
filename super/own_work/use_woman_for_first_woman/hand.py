b12fddded99206c4efb6ab528dabdf381edc401a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:00:11 -0500

    "mix: try super/way/part/see_time.py, super/thing.py, super/own_work/use_woman_for_first_woman/hand.py and super/time/feel_high_way/seem_old_problem_for_few_life/large_hand.py in last point"

---
+++ super/own_work/use_woman_for_first_woman/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point(str_arg):
+    use_company_for_same_point(str_arg)
+    print('big_person')
+
+def use_company_for_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point('government')
---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person_and_thing(str_arg):
+    long_day(str_arg)
+    print('be_high_child')
+
+def long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person_and_thing('right_government_and_year')
