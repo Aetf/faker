5550497ddc9227d334356db4c0ce4df3f9a178b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:09:56 -0500

    "mod: think super/new_way_or_first_year/use_man/own_woman/number/problem.py, super/try_little_life_into_day/place/high_person/ask_first_week_up_public_part/new_problem/person.py, super/child/small_day/work_and_new_world/bad_child/give_early_way_from_little_world.py or super/new_woman.py up same world"

---
+++ super/new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    time(str_arg)
+    print('tell_great_part_in_bad_case')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('child')
