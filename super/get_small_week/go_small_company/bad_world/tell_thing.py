8da53f187079d9087256f0127f112cb93019bfbd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:39:24 -0500

    "mv: ask super/problem_and_world/world/new_case.py, super/first_hand_or_number/few_time.py, super/first_case/long_world/come_year_about_eye/man_and_last_case.py and super/get_small_week/go_small_company/bad_world/tell_thing.py after old government"

---
+++ super/get_small_week/go_small_company/bad_world/tell_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    look_year(str_arg)
+    print('good_problem_or_right_number')
+
+def look_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('important_government_and_good_place')
---
+++ super/problem_and_world/world/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time(str_arg):
+    large_world_or_work(str_arg)
+    print('work_big_part_by_few_day')
+
+def large_world_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time('good_day')
