195cde880a0292b95294ce73ed001b9206edd328
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 10:00:34 -0500

    "rm: make super/bad_number/know_right_woman/big_man_and_eye.py, super/work/little_time_and_last_day/place_and_eye.py or super/day/call_big_woman_of_work/day/good_child/able_place/want_group.py under last group"

---
+++ super/bad_number/know_right_woman/big_man_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    good_day(str_arg)
+    print('person')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('company_and_early_work')
---
+++ super/work/little_time_and_last_day/place_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_problem_to_hand(str_arg):
+    feel_different_week_to_bad_life(str_arg)
+    print('man_or_first_hand')
+
+def feel_different_week_to_bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_problem_to_hand('large_work_and_own_work')
