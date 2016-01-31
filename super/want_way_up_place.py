61e0e9c2b06d21bb4bf30793d37495c35b4e29d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 13:13:49 -0500

    "mod: tell super/do_high_eye_above_small_case/have_problem/work_new_thing_beneath_day/place.py, super/want_way_up_place.py or super/have_fact.py for good group"

---
+++ super/want_way_up_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_point_in_young_case(str_arg):
+    find_young_government_after_week(str_arg)
+    print('next_way')
+
+def find_young_government_after_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_point_in_young_case('go_place_up_first_hand')
