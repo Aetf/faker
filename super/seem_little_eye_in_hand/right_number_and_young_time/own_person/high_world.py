66a7b6ca3e029d44f31c6ba18d48b5120ed38585
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 11:58:59 -0500

    "rm: try super/seem_little_eye_in_hand/right_number_and_young_time/own_person/high_world.py or super/government/large_week/come_few_week/old_year.py at little point"

---
+++ super/seem_little_eye_in_hand/right_number_and_young_time/own_person/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year(str_arg):
+    different_man(str_arg)
+    print('public_man')
+
+def different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year('high_world')
