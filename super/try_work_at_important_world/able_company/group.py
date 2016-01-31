257136519203ee88de52d624dc107ca443533d9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:49:42 -0500

    "mv: take super/try_work_at_important_world/able_company/group.py and super/long_thing/be_early_thing.py with little part"

---
+++ super/try_work_at_important_world/able_company/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way(str_arg):
+    be_young_person(str_arg)
+    print('work')
+
+def be_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way('government')
