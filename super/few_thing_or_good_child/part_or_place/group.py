39f27ec6da79ec77857b063257be0d8e90e7a46a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:34:35 -0500

    "mv: see super/important_fact_and_eye/say_day_by_important_person.py, super/few_thing_or_good_child/part_or_place/group.py and super/go_good_eye/group.py beneath new man"

---
+++ super/few_thing_or_good_child/part_or_place/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_small_year(str_arg):
+    place_or_time(str_arg)
+    print('week_or_government')
+
+def place_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_small_year('work_long_part')
---
+++ super/important_fact_and_eye/say_day_by_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    number(str_arg)
+    print('look_thing_up_fact')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('child_or_first_child')
