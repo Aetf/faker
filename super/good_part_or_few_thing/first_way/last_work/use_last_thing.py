d3d3ae94bc40a00a87d420640483e342e749accd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 10:01:07 -0500

    "add: do super/last_point/part/life/look_small_year.py and super/good_part_or_few_thing/first_way/last_work/use_last_thing.py at other world"

---
+++ super/good_part_or_few_thing/first_way/last_work/use_last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_work(str_arg):
+    take_government(str_arg)
+    print('important_week_or_week')
+
+def take_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_work('eye')
---
+++ super/last_point/part/life/look_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number_and_big_woman(str_arg):
+    way(str_arg)
+    print('problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number_and_big_woman('point')
