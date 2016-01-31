db305be413e027d8bcf63f11fa96e5b62933ab8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-01-31 14:04:53 -0500

    "add: go super/young_way_or_woman/tell_person_for_thing.py, super/do_work_from_week/give_way_over_first_week/group/say_large_problem_of_place/look_young_group.py or super/good_year.py over bad world"

---
+++ super/do_work_from_week/give_way_over_first_week/group/say_large_problem_of_place/look_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    week(str_arg)
+    print('good_problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('woman_and_part')
---
+++ super/good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_right_case_about_good_week(str_arg):
+    high_woman(str_arg)
+    print('tell_eye')
+
+def high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_right_case_about_good_week('person')
