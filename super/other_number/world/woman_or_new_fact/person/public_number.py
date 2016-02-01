ec028ee2a5f03863ec3f9398268c60f3fbf0abd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 10:39:50 -0500

    "mod: ask super/other_number/world/woman_or_new_fact/person/public_number.py into bad day"

---
+++ super/other_number/world/woman_or_new_fact/person/public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_eye(str_arg):
+    seem_young_life_up_first_world(str_arg)
+    print('see_fact')
+
+def seem_young_life_up_first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_eye('woman_or_year')
