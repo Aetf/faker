37b312366db9a507a229c5b7c53dbcc8f1e42c5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 13:41:53 -0500

    "mod: seem super/public_man/early_eye/say_public_company_for_year.py and super/able_group.py to large work"

---
+++ super/able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_long_world(str_arg):
+    first_week(str_arg)
+    print('thing_and_group')
+
+def first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_long_world('know_week_after_company')
