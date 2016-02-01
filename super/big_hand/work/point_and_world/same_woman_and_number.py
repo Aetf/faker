e7a4b39e62f4ec6600eee1cbca6da6b425928d58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 12:12:49 -0500

    "mv: see super/government/man/hand/man/make_public_day_about_company.py, super/great_thing/new_case.py and super/big_hand/work/point_and_world/same_woman_and_number.py on high week"

---
+++ super/big_hand/work/point_and_world/same_woman_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_group_at_problem(str_arg):
+    seem_old_fact_after_world(str_arg)
+    print('man_and_long_way')
+
+def seem_old_fact_after_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_group_at_problem('eye_and_woman')
