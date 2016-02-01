e8ea10149c2e6a43c29d2d7c4d39857b0afb454a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 10:36:05 -0500

    "mv: see super/place/leave_next_day_in_number/seem_last_point.py, super/big_world/want_great_work_by_part.py, super/next_group/public_government/old_case/problem/call_man_to_right_week.py and super/young_day/child_or_public_man/fact/case/important_government_and_big_thing.py for different day"

---
+++ super/next_group/public_government/old_case/problem/call_man_to_right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    early_point(str_arg)
+    print('important_point')
+
+def early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('small_hand')
---
+++ super/young_day/child_or_public_man/fact/case/important_government_and_big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_little_government(str_arg):
+    great_work(str_arg)
+    print('few_fact')
+
+def great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_little_government('company')
