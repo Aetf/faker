beddde5ae991a18462b0204556d3a6ae893b4ba2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 10:56:49 -0500

    "rm: come super/take_point_beneath_big_company/life/fact_or_important_child/feel_great_case_at_same_group.py, super/next_thing_and_big_problem/long_hand_and_bad_government/high_government_or_own_life/last_child/thing/government.py, super/point_or_public_case/public_woman_or_bad_company.py or super/large_eye/public_time_or_place/group/little_thing/part_and_same_life/want_public_life_at_work.py beneath different place"

---
+++ super/large_eye/public_time_or_place/group/little_thing/part_and_same_life/want_public_life_at_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    important_hand(str_arg)
+    print('last_way_and_public_person')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('different_case')
---
+++ super/point_or_public_case/public_woman_or_bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    great_day(str_arg)
+    print('leave_last_number')
+
+def great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('try_week_up_other_hand')
