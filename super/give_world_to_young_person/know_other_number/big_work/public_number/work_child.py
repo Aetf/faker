2da985f3360f7d76ac358c32331cacc96a5934b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 12:24:37 -0500

    "mix: give super/give_world_to_young_person/know_other_number/big_work/public_number/work_child.py by bad group"

---
+++ super/give_world_to_young_person/know_other_number/big_work/public_number/work_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_hand_up_year(str_arg):
+    different_time_and_own_world(str_arg)
+    print('child')
+
+def different_time_and_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_hand_up_year('last_person')
