e08794e7db54055defbf1cb78803fe602489e819
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 12:32:12 -0500

    "mv: ask super/find_work_at_early_part/know_week_at_person/find_little_place/go_different_person/group_or_own_man.py, super/long_fact/work/next_day.py and super/young_person/way/world/want_way.py with first work"

---
+++ super/young_person/way/world/want_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_big_year(str_arg):
+    say_place(str_arg)
+    print('person')
+
+def say_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_big_year('world')
