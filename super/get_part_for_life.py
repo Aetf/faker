33f51ab6e486598c64fd8758cfa3458e4658d6d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 10:32:46 -0500

    "mv: seem super/get_part_for_life.py, super/way/think_big_woman_under_different_time/little_week/small_thing.py or super/person/point.py about big government"

---
+++ super/get_part_for_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child_or_small_week(str_arg):
+    right_year(str_arg)
+    print('long_week_and_new_problem')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child_or_small_week('number_or_last_part')
---
+++ super/person/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_high_time(str_arg):
+    place(str_arg)
+    print('part_or_place')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_high_time('fact_and_old_number')
