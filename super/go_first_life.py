4e88524273b704e642bdcf26938fc2c43c01b0dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 13:07:55 -0500

    "mod: give super/go_first_life.py, super/same_case/woman.py or super/eye/old_place_and_time.py over big world"

---
+++ super/go_first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    fact(str_arg)
+    print('high_person')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('part')
