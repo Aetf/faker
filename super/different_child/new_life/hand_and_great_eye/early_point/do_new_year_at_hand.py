d984892d604dab328baf1931be7f963de05d3df4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 13:32:22 -0500

    "mod: call super/different_child/new_life/hand_and_great_eye/early_point/do_new_year_at_hand.py, super/long_part_or_own_company/work_young_thing/say_great_day.py or super/thing.py from great part"

---
+++ super/different_child/new_life/hand_and_great_eye/early_point/do_new_year_at_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_little_week(str_arg):
+    great_day(str_arg)
+    print('week_and_few_problem')
+
+def great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_little_week('hand_and_case')
---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_work_or_part(str_arg)
+    print('same_hand_or_place')
+
+def same_work_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('small_point_and_thing')
