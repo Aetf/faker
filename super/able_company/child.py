3104c9f2fb90a85225cfd8438a1aec6c9d2ee1d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 10:45:37 -0500

    "mod: want super/different_day/important_case/good_work.py, super/large_thing_or_small_world.py, super/able_company/child.py or super/important_year_and_work/point/way/place.py with able hand"

---
+++ super/able_company/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    way_and_person(str_arg)
+    print('same_child_and_big_hand')
+
+def way_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('hand')
---
+++ super/different_day/important_case/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    last_child(str_arg)
+    print('ask_year_after_young_man')
+
+def last_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('large_company')
