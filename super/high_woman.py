5da7e72e0b4c3c06f7faeeb37ad42a57ed7bcade
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 11:11:04 -0500

    "mod: know super/high_woman.py, super/big_problem/able_number.py or super/other_world/life/case/large_fact/big_problem/big_woman.py with young number"

---
+++ super/high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_time(str_arg):
+    other_person(str_arg)
+    print('number')
+
+def other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_time('company')
