4e44480c96f4f4dd726fd33a6e3fb3a8e984ed7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-01 11:28:29 -0500

    "mv: think super/year.py, super/get_thing/way.py or super/early_person_and_small_work.py above little eye"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group(str_arg):
+    year(str_arg)
+    print('say_child')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group('ask_number_after_company')
