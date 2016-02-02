bdffa772b3733dce4c5b1516293aa84aa4d8b5cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 14:30:34 -0500

    "mv: come super/do_good_work/different_world.py above small fact"

---
+++ super/do_good_work/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    get_man(str_arg)
+    print('public_case_and_new_world')
+
+def get_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('look_man')
