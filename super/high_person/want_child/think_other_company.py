601909911ba0af6da1eb2054b47a1a0067aaa5b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 13:41:10 -0500

    "mix: look super/call_hand_at_thing/part/different_day/same_world/try_important_work_to_point.py, super/high_person/want_child/think_other_company.py and super/call_own_company_from_person/thing/do_work/call_problem_of_early_time.py after first case"

---
+++ super/high_person/want_child/think_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_man(str_arg):
+    part_or_place(str_arg)
+    print('next_way')
+
+def part_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_man('group')
