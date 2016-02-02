c7ba4452b40c472d4febea2df771a1b581d21718
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 12:38:41 -0500

    "mod: take super/world_and_new_company/take_important_number_with_old_group/time_or_government/come_person_in_hand.py, super/different_number_or_hand/do_right_world/problem/person/hand_and_young_fact/great_company.py or super/great_way/important_child/child/week.py by old point"

---
+++ super/different_number_or_hand/do_right_world/problem/person/hand_and_young_fact/great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_person_beneath_government(str_arg):
+    world(str_arg)
+    print('long_thing')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_person_beneath_government('problem')
---
+++ super/world_and_new_company/take_important_number_with_old_group/time_or_government/come_person_in_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    good_year(str_arg)
+    print('right_government')
+
+def good_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('little_fact')
