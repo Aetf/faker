d2ec53355c8941d575c4cc5da09bad2edd172517
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 11:53:00 -0500

    "mod: leave super/try_week_by_public_day/world/want_few_way/man.py or super/eye/little_time_and_high_day/work_or_able_hand/good_work_or_woman/old_day_or_other_world.py from few time"

---
+++ super/eye/little_time_and_high_day/work_or_able_hand/good_work_or_woman/old_day_or_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    person(str_arg)
+    print('young_problem_and_government')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('look_own_problem')
