98c0c552bde271a666df22a8b87a0bb1c3bd9687
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 11:01:48 -0500

    "add: know super/man_or_point/bad_child.py on same fact"

---
+++ super/man_or_point/bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_point_up_company(str_arg):
+    new_place(str_arg)
+    print('number_and_world')
+
+def new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_point_up_company('number_and_other_hand')
