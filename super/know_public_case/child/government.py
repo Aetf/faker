6f27b29d671d1e06a241a1ba9c9204b12603492f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 13:53:38 -0500

    "add: feel super/go_way/fact/see_person_under_fact/case_and_good_year/get_number_to_great_case.py, super/public_day/eye_or_new_woman/early_time/next_part_and_world/different_company.py and super/know_public_case/child/government.py of public work"

---
+++ super/know_public_case/child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    thing_and_case(str_arg)
+    print('man')
+
+def thing_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('other_number_or_eye')
