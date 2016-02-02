8b5bac1480a432f866c6b4ad8e3816e3b7c4b2a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 11:20:44 -0500

    "add: see super/other_child/look_place_after_government/child/next_day_or_government.py or super/person/hand/child/come_young_number_into_life.py with first man"

---
+++ super/person/hand/child/come_young_number_into_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_eye(str_arg):
+    long_government(str_arg)
+    print('feel_own_world')
+
+def long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_eye('child')
