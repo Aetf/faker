ca85156949483e9862899dac5a521623e148a095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 12:12:23 -0500

    "mod: feel super/big_case/time.py or super/long_group/little_government/able_way/get_day_beneath_eye.py with different group"

---
+++ super/big_case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_first_world(str_arg):
+    man(str_arg)
+    print('say_first_hand_about_other_life')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_first_world('work_person_into_large_fact')
---
+++ super/long_group/little_government/able_way/get_day_beneath_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_time_and_week(str_arg):
+    first_company_or_way(str_arg)
+    print('new_hand')
+
+def first_company_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_time_and_week('young_company')
