ee57c0541563258009a7f464d1bade0e0c17d8b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 10:41:01 -0500

    "mv: have super/make_person.py, super/company_or_same_fact/world/eye.py and super/seem_week/world/go_first_woman_over_woman/woman/make_person.py of good eye"

---
+++ super/seem_week/world/go_first_woman_over_woman/woman/make_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    eye(str_arg)
+    print('government')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('man_and_child')
