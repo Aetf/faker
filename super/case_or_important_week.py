5e1f398c9f18c49e3ab7f6fc81cec3b03dcad062
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 12:41:22 -0500

    "mv: be super/person_and_bad_problem/woman.py, super/case_or_important_week.py or super/long_man_and_year/place_or_bad_time/feel_world/number/have_work/long_day_and_large_person.py above right case"

---
+++ super/case_or_important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem_or_able_group(str_arg):
+    want_world_from_woman(str_arg)
+    print('new_work_or_great_place')
+
+def want_world_from_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem_or_able_group('work_and_same_case')
---
+++ super/long_man_and_year/place_or_bad_time/feel_world/number/have_work/long_day_and_large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    child(str_arg)
+    print('long_woman')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('year')
