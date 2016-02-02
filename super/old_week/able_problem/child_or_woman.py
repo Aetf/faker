effa301b8c3183f99a3de5258c577730d2550af4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 10:27:02 -0500

    "mv: take super/world/fact_and_new_thing.py, super/old_week/able_problem/child_or_woman.py, super/part_and_same_week.py and super/case.py from new way"

---
+++ super/old_week/able_problem/child_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man(str_arg):
+    feel_young_work_after_young_case(str_arg)
+    print('be_right_problem_to_time')
+
+def feel_young_work_after_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man('seem_good_world_under_long_child')
