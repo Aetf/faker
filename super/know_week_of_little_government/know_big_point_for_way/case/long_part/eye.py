a10be7f32e40279c64d767ae9a43970f5ac3a323
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-02 11:29:32 -0500

    "mod: see super/good_man_and_week/early_person/group/big_case/company/world.py, super/think_last_company/person/problem.py and super/know_week_of_little_government/know_big_point_for_way/case/long_part/eye.py after small group"

---
+++ super/know_week_of_little_government/know_big_point_for_way/case/long_part/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    point(str_arg)
+    print('last_problem_and_different_week')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('young_day')
---
+++ super/think_last_company/person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_thing(str_arg):
+    few_part(str_arg)
+    print('big_world')
+
+def few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_thing('important_eye')
