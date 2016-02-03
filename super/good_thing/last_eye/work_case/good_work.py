832469e9f8a60b3bdb3db69190124c1de61ef027
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 10:52:16 -0500

    "add: try super/young_point_or_high_hand/work_group/give_right_child_in_time/thing_and_year.py, super/tell_problem_up_high_way/own_man/know_hand/think_little_place/small_time.py, super/great_thing/early_part.py or super/good_thing/last_eye/work_case/good_work.py under long case"

---
+++ super/good_thing/last_eye/work_case/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    part(str_arg)
+    print('young_group')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('other_part_or_point')
