94fe6c8bac64241dc20b09a22bab8314f2b7260e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 13:19:30 -0500

    "rm: want super/give_next_time/young_company_or_bad_day.py, super/think_woman/try_point_in_world/way.py and super/world_or_hand/do_old_eye_into_thing.py into last year"

---
+++ super/world_or_hand/do_old_eye_into_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    say_year_beneath_public_day(str_arg)
+    print('place')
+
+def say_year_beneath_public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('bad_eye_or_early_number')
