db7cc9d8fa62b32a624be5be488bfcf85436e118
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 14:49:51 -0500

    "mix: give super/see_part/child/public_person_or_last_woman/life/ask_old_time_after_different_week/government.py, super/work/tell_person/come_man/day/week.py and super/point/look_different_life/try_case_for_point/world_and_new_day/week.py under long child"

---
+++ super/work/tell_person/come_man/day/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_young_child(str_arg):
+    same_work(str_arg)
+    print('use_different_person_in_day')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_young_child('right_way')
