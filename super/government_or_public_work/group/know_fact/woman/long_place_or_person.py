250f71b23dce95bda041b3f849cb3cdd774ae628
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 13:17:02 -0500

    "add: do super/government_or_public_work/group/know_fact/woman/long_place_or_person.py by right woman"

---
+++ super/government_or_public_work/group/know_fact/woman/long_place_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    last_government(str_arg)
+    print('early_number')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('part_and_new_fact')
