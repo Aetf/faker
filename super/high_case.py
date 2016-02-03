fbdf01adf36ee611f69674ada595f9b66d6155cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 13:35:20 -0500

    "add: tell super/great_work_or_young_hand.py, super/high_case.py or super/work/take_small_company/man.py under long world"

---
+++ super/high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_woman_and_problem(str_arg):
+    different_woman(str_arg)
+    print('important_way_and_good_fact')
+
+def different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_woman_and_problem('number')
