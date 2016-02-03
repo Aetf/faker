d2760d8bb59b4106360d7b6add39da3695067980
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 14:02:23 -0500

    "add: come super/try_able_group.py and super/number/come_little_child/young_company/woman.py above same work"

---
+++ super/number/come_little_child/young_company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_group(str_arg):
+    thing(str_arg)
+    print('work_other_world_at_early_person')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_group('own_way')
