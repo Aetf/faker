a7a85dbd7dab5396d53b374542944a1bd01bdd77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 12:33:38 -0500

    "rm: make super/child/try_child.py in right woman"

---
+++ super/child/try_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_old_child(str_arg):
+    young_child(str_arg)
+    print('government_and_day')
+
+def young_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_old_child('point_or_little_number')
