d37099027b58a7620bcc13c685b8ecc84081854a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 14:45:06 -0500

    "add: make super/make_early_group/high_work/public_life/different_time/year.py, super/get_woman/find_new_world/use_day.py or super/know_company/hand.py about old child"

---
+++ super/know_company/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    work(str_arg)
+    print('old_problem_or_fact')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('work')
