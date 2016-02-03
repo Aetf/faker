4a8c402a200398a65a7c5e3c0a7d9a55dac4ab73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-03 12:10:48 -0500

    "mv: take super/thing_and_year/find_part/small_problem_and_case.py and super/woman/company.py from high life"

---
+++ super/woman/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_world(str_arg):
+    tell_last_man(str_arg)
+    print('go_place_to_other_group')
+
+def tell_last_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_world('big_hand_and_good_woman')
