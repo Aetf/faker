a33a417ca15f8f9e96c5728654a5f90fab1e6ca3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 12:43:34 -0500

    "mix: use super/thing/fact/get_high_number/get_case_from_last_man/seem_big_world_after_person/have_time.py or super/week_or_point/other_company.py beneath bad day"

---
+++ super/thing/fact/get_high_number/get_case_from_last_man/seem_big_world_after_person/have_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_fact(str_arg):
+    number(str_arg)
+    print('young_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_fact('number')
