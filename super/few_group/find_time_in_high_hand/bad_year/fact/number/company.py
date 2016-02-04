3f7ea87deb0531dbc54ae1b3d955426f1fd7b2f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 10:59:20 -0500

    "mod: think super/few_life_and_company/point.py, super/know_first_year/eye.py, super/next_world_or_small_way.py and super/few_group/find_time_in_high_hand/bad_year/fact/number/company.py under old problem"

---
+++ super/few_group/find_time_in_high_hand/bad_year/fact/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_important_work_up_part(str_arg):
+    little_man(str_arg)
+    print('important_world')
+
+def little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_important_work_up_part('woman')
---
+++ super/few_life_and_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye(str_arg):
+    other_government_or_way(str_arg)
+    print('world_and_child')
+
+def other_government_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye('other_person')
