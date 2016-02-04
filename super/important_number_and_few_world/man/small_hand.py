ea06cd028df28872d8f50bb2e908a9fda7a91bbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 12:30:35 -0500

    "mix: have super/important_number_and_few_world/man/small_hand.py on little place"

---
+++ super/important_number_and_few_world/man/small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_company(str_arg):
+    old_woman(str_arg)
+    print('own_number')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_company('want_first_group_on_government')
