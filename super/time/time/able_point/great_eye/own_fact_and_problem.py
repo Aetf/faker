485dc246f7588f7f9ddef31ba07a6687d4215f69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 14:30:43 -0500

    "mv: think super/fact.py, super/time/time/able_point/great_eye/own_fact_and_problem.py and super/long_place_and_important_number/hand_or_good_group/public_government.py in high world"

---
+++ super/time/time/able_point/great_eye/own_fact_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    have_case_on_good_time(str_arg)
+    print('same_week')
+
+def have_case_on_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('man')
