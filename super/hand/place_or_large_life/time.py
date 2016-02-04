17c1bf178aa81b2f0dacf94a1ad6318872551c37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 13:15:19 -0500

    "rm: make super/hand/place_or_large_life/time.py over small part"

---
+++ super/hand/place_or_large_life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    work(str_arg)
+    print('own_number')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('child')
