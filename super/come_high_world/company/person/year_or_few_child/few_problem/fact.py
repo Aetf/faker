4c33155c64b962c6160447f658286867de6acd83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 11:05:11 -0500

    "mix: make super/come_few_life.py, super/come_high_world/company/person/year_or_few_child/few_problem/fact.py or super/little_year/ask_right_year/life.py above right number"

---
+++ super/come_high_world/company/person/year_or_few_child/few_problem/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_year_by_part(str_arg):
+    different_problem_or_point(str_arg)
+    print('child_and_year')
+
+def different_problem_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_year_by_part('say_little_work_above_next_fact')
