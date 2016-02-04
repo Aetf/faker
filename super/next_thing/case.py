1edf4ef134f733f073d078e5ef8d40e52e395271
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 14:40:25 -0500

    "add: give super/tell_bad_government_at_high_work.py or super/next_thing/case.py for few company"

---
+++ super/next_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    long_hand(str_arg)
+    print('first_time')
+
+def long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('long_group_and_part')
