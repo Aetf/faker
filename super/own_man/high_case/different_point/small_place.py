535ba2e328bd7dc7fa703a9561b9f081bbb99d13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 11:50:01 -0500

    "add: know super/other_group/little_work/problem/important_thing_and_able_year/high_life/use_way_over_point.py or super/own_man/high_case/different_point/small_place.py for other government"

---
+++ super/own_man/high_case/different_point/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    early_government(str_arg)
+    print('long_number_or_day')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('last_eye')
