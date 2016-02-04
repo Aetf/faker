a96515794a0a3d737dc90f1636326ef2a68e9894
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 11:39:08 -0500

    "add: leave super/way/point/old_woman/way/government/same_work.py, super/bad_life/child.py and super/government/hand_and_man/go_part/number_or_day/seem_problem.py up right person"

---
+++ super/government/hand_and_man/go_part/number_or_day/seem_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case_and_case(str_arg):
+    tell_big_woman(str_arg)
+    print('point_and_company')
+
+def tell_big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case_and_case('next_part')
---
+++ super/way/point/old_woman/way/government/same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_part(str_arg):
+    able_case(str_arg)
+    print('great_case_and_woman')
+
+def able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_part('long_company')
