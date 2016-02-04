5ede5f0c9687272305e70b69fa5a4e3106c59d71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 13:57:48 -0500

    "mod: do super/long_week/small_life/day_or_early_year/part.py at first case"

---
+++ super/long_week/small_life/day_or_early_year/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_or_problem(str_arg)
+    print('case')
+
+def way_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('great_year')
