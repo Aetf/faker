368c31df8119e00920d11bede423aefaa23b39d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 14:59:05 -0500

    "mod: ask super/take_fact_beneath_company/week/get_problem/problem/important_eye/use_child_at_life.py and super/be_life/group_or_same_work/high_way.py on good number"

---
+++ super/be_life/group_or_same_work/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    week_and_first_point(str_arg)
+    print('seem_next_government_about_part')
+
+def week_and_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('new_case_or_man')
---
+++ super/take_fact_beneath_company/week/get_problem/problem/important_eye/use_child_at_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_woman(str_arg):
+    different_group(str_arg)
+    print('week')
+
+def different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_woman('use_new_man')
