58b5ff521f8ca8c292930b93194b330348070fe6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 11:57:43 -0500

    "mod: leave super/man/know_different_week/small_place/problem/time.py about bad year"

---
+++ super/man/know_different_week/small_place/problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place_under_long_world(str_arg):
+    want_man_by_bad_person(str_arg)
+    print('able_hand_and_small_child')
+
+def want_man_by_bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place_under_long_world('child')
