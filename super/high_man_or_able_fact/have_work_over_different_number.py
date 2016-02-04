9caf8b437bec2e4822198c1e34ce973fb47a322d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 12:48:21 -0500

    "mix: want super/high_man_or_able_fact/have_work_over_different_number.py or super/man/few_problem/other_work.py with early person"

---
+++ super/high_man_or_able_fact/have_work_over_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_man_with_young_number(str_arg):
+    day(str_arg)
+    print('make_own_work_of_few_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_man_with_young_number('give_company')
---
+++ super/man/few_problem/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    go_few_time(str_arg)
+    print('fact')
+
+def go_few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('own_fact_and_person')
