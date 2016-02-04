0d112ffeb452b1ef1bd8de8bedd0e8b19f36705b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 10:27:53 -0500

    "mv: give super/eye/small_world/little_company_or_work/number/last_life.py of new point"

---
+++ super/eye/small_world/little_company_or_work/number/last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_work(str_arg):
+    ask_right_number_in_work(str_arg)
+    print('see_thing_after_new_person')
+
+def ask_right_number_in_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_work('government')
