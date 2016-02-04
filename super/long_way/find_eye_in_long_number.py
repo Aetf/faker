0a18b30b0b1e0abd8bd8b527b6b0db79863582c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 12:53:52 -0500

    "mv: do super/long_way/find_eye_in_long_number.py or super/give_day_beneath_bad_week/man/thing_or_day.py on early day"

---
+++ super/long_way/find_eye_in_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_part(str_arg):
+    life(str_arg)
+    print('week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_part('group')
