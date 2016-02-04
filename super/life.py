ec498b56a100ed861ad6646217177bf1948aded6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 12:48:27 -0500

    "mod: go super/say_next_week/number/way.py and super/life.py about important work"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    next_week_or_young_problem(str_arg)
+    print('next_woman')
+
+def next_week_or_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('big_woman')
