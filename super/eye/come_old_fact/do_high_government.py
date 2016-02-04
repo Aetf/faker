f87240d3384f8f6b4775a178932e5a8be3e9c1e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-04 13:03:44 -0500

    "mod: find super/take_last_day/make_day/case/large_number/leave_high_company_by_way.py or super/eye/come_old_fact/do_high_government.py over important child"

---
+++ super/eye/come_old_fact/do_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_year(str_arg):
+    long_work(str_arg)
+    print('work_place')
+
+def long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_year('different_year')
