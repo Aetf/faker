8c10c0020a1eb8ce67b73b5367bf6a9db004f4e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:19:56 -0500

    "mod: be super/problem_or_big_part.py at bad company"

---
+++ super/problem_or_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_thing(str_arg):
+    right_fact(str_arg)
+    print('group_or_few_problem')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_thing('do_same_child_into_big_government')
