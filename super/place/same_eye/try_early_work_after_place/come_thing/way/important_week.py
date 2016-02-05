2e5a1d12c78272aab1742a353f66d5e6de7b763a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 12:17:05 -0500

    "add: seem super/place/same_eye/try_early_work_after_place/come_thing/way/important_week.py and super/important_day/take_little_eye/early_problem/use_day.py over good woman"

---
+++ super/place/same_eye/try_early_work_after_place/come_thing/way/important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    able_point_or_new_place(str_arg)
+    print('week')
+
+def able_point_or_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('high_work')
