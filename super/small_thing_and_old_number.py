99151c6799073efd676a6742b8ed1c4c79bbaca5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 13:13:17 -0500

    "mix: seem super/know_company_after_other_group/problem/case/public_life_and_few_woman/want_young_place_up_bad_company.py, super/take_fact_about_last_group/do_company/bad_case_and_few_time/tell_early_day.py, super/small_thing_and_old_number.py and super/able_hand/time/bad_life/high_person/little_person_or_number/important_company.py under small government"

---
+++ super/small_thing_and_old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    early_world(str_arg)
+    print('world')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('way')
