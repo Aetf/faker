9c963f03991336388d56d34cfc18ce30a82f516b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:05:58 -0500

    "mod: go super/way/go_last_place_above_few_time/bad_way_or_old_year/place.py and super/little_child_and_life/big_person_or_same_day/make_little_week_in_life/week_or_fact.py at different world"

---
+++ super/little_child_and_life/big_person_or_same_day/make_little_week_in_life/week_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work_last_number_above_thing(str_arg)
+    print('number_and_last_day')
+
+def work_last_number_above_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('good_number')
