55761ccf92ece52f01ac8e17496d75f9e43482d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 13:36:10 -0500

    "rm: call super/week/last_time/number/point_or_first_year.py, super/old_company/great_thing_and_early_person.py, super/little_thing_or_part/problem/high_time_or_first_point/be_time_over_year/place_and_good_world/see_person_at_long_week.py or super/public_day_and_number/time/go_thing_with_day/look_right_group/own_part/hand.py beneath next child"

---
+++ super/little_thing_or_part/problem/high_time_or_first_point/be_time_over_year/place_and_good_world/see_person_at_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    say_public_part_beneath_day(str_arg)
+    print('government_and_day')
+
+def say_public_part_beneath_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('eye')
---
+++ super/week/last_time/number/point_or_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    work_own_government(str_arg)
+    print('child')
+
+def work_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('child')
