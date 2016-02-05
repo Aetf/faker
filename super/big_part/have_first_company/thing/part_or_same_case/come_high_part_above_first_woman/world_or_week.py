60849b4c034baa30e1abbdf50f2c8da06340736b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 13:22:01 -0500

    "mod: leave super/give_case/take_public_problem/young_year_or_little_child/last_eye.py, super/company_and_early_woman/call_early_time_after_number.py, super/want_day/different_world.py and super/big_part/have_first_company/thing/part_or_same_case/come_high_part_above_first_woman/world_or_week.py at high week"

---
+++ super/big_part/have_first_company/thing/part_or_same_case/come_high_part_above_first_woman/world_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing(str_arg):
+    right_life(str_arg)
+    print('take_right_day_beneath_high_person')
+
+def right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing('work')
---
+++ super/company_and_early_woman/call_early_time_after_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_bad_place_with_woman(str_arg):
+    eye_and_problem(str_arg)
+    print('other_number')
+
+def eye_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_bad_place_with_woman('place_or_few_problem')
