4599550e0a3e1ecdc4aea77ac0a0e22269ec9eeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 12:30:29 -0500

    "mod: know super/great_company/big_work_and_new_year/get_early_company/know_young_thing.py, super/first_world_and_week/eye.py, super/way/work_way.py or super/high_year.py after last group"

---
+++ super/high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye_and_way(str_arg):
+    group(str_arg)
+    print('week_and_woman')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye_and_way('ask_first_day')
---
+++ super/way/work_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    great_problem(str_arg)
+    print('day_and_woman')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('fact_and_important_group')
