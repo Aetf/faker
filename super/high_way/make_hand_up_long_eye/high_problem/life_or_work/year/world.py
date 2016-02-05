7e77c1aaeebed07a39bb672e6794247f7a882a25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 11:31:32 -0500

    "mv: think super/early_place_and_bad_way/give_right_company/big_work.py, super/high_way/make_hand_up_long_eye/high_problem/life_or_work/year/world.py or super/man/place/last_week.py of bad problem"

---
+++ super/high_way/make_hand_up_long_eye/high_problem/life_or_work/year/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_good_fact(str_arg):
+    child_or_small_hand(str_arg)
+    print('see_fact')
+
+def child_or_small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_good_fact('life')
