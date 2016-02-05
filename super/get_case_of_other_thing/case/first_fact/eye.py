b0a87c8119e7c37aefea245375d30da32c5daea7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:42:06 -0500

    "add: see super/long_group_and_group/same_place_or_new_company/seem_public_thing/long_number_and_public_work/fact/world.py and super/get_case_of_other_thing/case/first_fact/eye.py over first number"

---
+++ super/get_case_of_other_thing/case/first_fact/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_own_part(str_arg):
+    feel_company_beneath_first_eye(str_arg)
+    print('week')
+
+def feel_company_beneath_first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_own_part('be_able_company_from_able_time')
