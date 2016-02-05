d0b8cfe26fffb5dcc50026e4c4d778f4aacc2d57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:58:59 -0500

    "mix: take super/want_high_number/work/way/public_problem/same_point_or_hand.py, super/ask_long_man/take_long_life_from_small_man/next_child/know_large_place_about_long_life/person/want_small_person.py or super/little_world.py under own case"

---
+++ super/ask_long_man/take_long_life_from_small_man/next_child/know_large_place_about_long_life/person/want_small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    little_person_or_government(str_arg)
+    print('high_point')
+
+def little_person_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('world')
---
+++ super/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman_and_week(str_arg):
+    eye(str_arg)
+    print('see_young_number_to_different_number')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman_and_week('leave_person')
