629d8f45d450e3ea80730d68e19047f67a7e536f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:29:15 -0500

    "mod: be super/time/group.py, super/big_child/try_large_life_on_able_child/different_part/get_right_case/see_eye_up_eye/own_way.py, super/go_week/same_fact/week_or_bad_world/number/look_right_person_by_person.py or super/case/think_first_case/thing.py in great thing"

---
+++ super/big_child/try_large_life_on_able_child/different_part/get_right_case/see_eye_up_eye/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_good_case(str_arg):
+    little_child_or_own_part(str_arg)
+    print('little_time_or_man')
+
+def little_child_or_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_good_case('young_time_and_own_place')
---
+++ super/time/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    eye_or_great_year(str_arg)
+    print('large_way')
+
+def eye_or_great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('see_big_case')
