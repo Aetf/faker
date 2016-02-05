78d41a89a99276aa2f25070c96a5570c12c5439b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 12:07:12 -0500

    "mv: tell super/get_world/go_thing.py or super/woman/small_week/go_child.py on high company"

---
+++ super/woman/small_week/go_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_child(str_arg):
+    think_great_time(str_arg)
+    print('tell_point_at_good_part')
+
+def think_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_child('number')
