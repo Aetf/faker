a601b8374b8c5fa5cee51d417f9f0993ba4a7902
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 14:52:38 -0500

    "mix: take super/number_or_part/last_life_or_way/right_problem/year_and_problem.py, super/own_day/woman/be_thing_from_eye/way.py or super/point.py of next eye"

---
+++ super/number_or_part/last_life_or_way/right_problem/year_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_other_problem_of_time(str_arg):
+    have_old_year_beneath_person(str_arg)
+    print('case')
+
+def have_old_year_beneath_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_other_problem_of_time('do_problem')
