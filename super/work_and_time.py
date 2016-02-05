55c6789ef5a07fd1168a08cdea405a018e2c7891
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 14:04:42 -0500

    "mod: ask super/work_and_time.py to old company"

---
+++ super/work_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    time(str_arg)
+    print('woman')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('part_or_day')
