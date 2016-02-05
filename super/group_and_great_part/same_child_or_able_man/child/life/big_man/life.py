ec9bc87a47c22d669a6a4de56131eecfc96c97ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 12:10:36 -0500

    "mod: leave super/group_and_great_part/same_child_or_able_man/child/life/big_man/life.py after own point"

---
+++ super/group_and_great_part/same_child_or_able_man/child/life/big_man/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_young_life_under_next_week(str_arg):
+    make_public_case(str_arg)
+    print('go_first_world_above_woman')
+
+def make_public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_young_life_under_next_week('high_work')
