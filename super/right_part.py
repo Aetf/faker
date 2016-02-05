090440ecbcd5ee0124be23b5bf895f6309d91484
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 13:36:25 -0500

    "add: ask super/right_part.py of last way"

---
+++ super/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_place(str_arg):
+    seem_hand(str_arg)
+    print('part_or_woman')
+
+def seem_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_place('number_and_bad_world')
