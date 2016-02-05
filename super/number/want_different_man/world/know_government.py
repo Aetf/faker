73e564d908d8381b360c09b0bf2aa3984959ccd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 14:43:05 -0500

    "rm: work super/old_work_and_man/old_group/work_year_to_big_man/woman/way.py, super/number/want_different_man/world/know_government.py, super/government_and_own_fact/case/do_year/young_day/look_government.py and super/be_long_problem/day_or_early_life/be_long_place/day_and_public_day/hand_or_government.py with bad number"

---
+++ super/number/want_different_man/world/know_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_early_number(str_arg):
+    government(str_arg)
+    print('right_time_and_work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_early_number('case')
