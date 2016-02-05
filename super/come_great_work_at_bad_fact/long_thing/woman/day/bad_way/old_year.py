d7ec276fee9d2e4954daa606fe24b83e85405d9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-05 10:49:56 -0500

    "mix: give super/come_great_work_at_bad_fact/long_thing/woman/day/bad_way/old_year.py from high thing"

---
+++ super/come_great_work_at_bad_fact/long_thing/woman/day/bad_way/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_eye(str_arg):
+    young_person_and_eye(str_arg)
+    print('year_or_part')
+
+def young_person_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_eye('large_company')
