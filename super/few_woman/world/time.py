89adb4d366687c59512b9c6f4f5c86f873945463
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 12:55:43 -0500

    "add: be super/few_woman/world/time.py, super/work_fact_above_case/life/big_life.py, super/woman_or_different_thing/child/work_same_thing/want_problem/able_person/person.py or super/point_and_place/look_week/look_bad_hand.py at public man"

---
+++ super/few_woman/world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day(str_arg):
+    old_woman(str_arg)
+    print('company_and_large_group')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day('be_large_child')
---
+++ super/point_and_place/look_week/look_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    take_public_eye_of_last_day(str_arg)
+    print('long_government_and_year')
+
+def take_public_eye_of_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('woman')
