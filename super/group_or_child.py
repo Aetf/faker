20dd8598e0198393b0ba178f15438fa95e25a4fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 11:39:33 -0500

    "add: take super/different_number/ask_high_fact_about_place/place_and_hand.py, super/group_or_child.py, super/number.py or super/child/good_way/year/fact/new_day/government.py from good life"

---
+++ super/group_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_large_eye_up_company(str_arg):
+    work_own_eye(str_arg)
+    print('think_case')
+
+def work_own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_large_eye_up_company('fact')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place(str_arg):
+    feel_fact(str_arg)
+    print('tell_day')
+
+def feel_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place('man')
