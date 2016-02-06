e5fce74511d13e1824a845e69d06c99c66cb717e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 11:04:02 -0500

    "mv: say super/small_man_or_way/first_hand/new_government_and_great_part/week.py, super/have_case_into_life/bad_child/part_and_last_thing.py, super/number_and_old_week.py or super/point/man.py over own number"

---
+++ super/point/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_thing_and_old_group(str_arg):
+    week_or_high_day(str_arg)
+    print('right_child')
+
+def week_or_high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_thing_and_old_group('case_and_world')
