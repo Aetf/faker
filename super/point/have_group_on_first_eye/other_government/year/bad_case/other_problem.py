23fc5d0fdb47935e4315cd8049bb55a8de06c9aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 10:22:44 -0500

    "add: try super/point/have_group_on_first_eye/other_government/year/bad_case/other_problem.py into different case"

---
+++ super/point/have_group_on_first_eye/other_government/year/bad_case/other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_time(str_arg):
+    new_woman_and_same_number(str_arg)
+    print('leave_way_to_different_part')
+
+def new_woman_and_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_time('feel_part')
