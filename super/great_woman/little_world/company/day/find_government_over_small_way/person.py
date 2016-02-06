4efa5b551d38072a68edc5aad87b010b4e8eb791
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 12:05:31 -0500

    "mix: feel super/number_or_same_week.py, super/work/large_point/government.py, super/great_woman/little_world/company/day/find_government_over_small_way/person.py and super/number.py above few company"

---
+++ super/great_woman/little_world/company/day/find_government_over_small_way/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government_and_little_group(str_arg):
+    person(str_arg)
+    print('week')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government_and_little_group('small_thing_or_bad_work')
---
+++ super/number_or_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    year(str_arg)
+    print('think_good_part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('first_time_and_woman')
