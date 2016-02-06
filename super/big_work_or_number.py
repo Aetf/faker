d81659fb7143085696b4848e92db25e0fc0764c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 13:54:08 -0500

    "mod: do super/point_or_year/work.py, super/important_eye/good_government_and_public_thing/place_and_old_government.py, super/big_work_or_number.py and super/early_hand/bad_woman_and_next_time/different_case/number_and_child/get_good_hand_at_other_child.py at big fact"

---
+++ super/big_work_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_good_work(str_arg):
+    group_or_old_way(str_arg)
+    print('day')
+
+def group_or_old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_good_work('high_case')
---
+++ super/point_or_year/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    bad_company_or_time(str_arg)
+    print('world')
+
+def bad_company_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('woman_and_first_man')
