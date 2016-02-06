6fcf68aa570f04e239367c9c7b77a4dc17253a27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 10:22:03 -0500

    "mix: use super/last_company_and_way/important_child/right_case_or_time/important_day_and_life/year_and_first_group.py or super/problem_and_world/small_child/great_child.py by large place"

---
+++ super/last_company_and_way/important_child/right_case_or_time/important_day_and_life/year_and_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact_or_thing(str_arg):
+    want_way_up_early_person(str_arg)
+    print('ask_different_company')
+
+def want_way_up_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact_or_thing('right_eye_or_eye')
