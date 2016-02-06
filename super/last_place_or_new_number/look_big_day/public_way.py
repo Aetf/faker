857c1b242cd57317236b1ef9daa455f6e6022f92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 14:42:55 -0500

    "mod: take super/week/number/use_day_up_year/problem/part.py and super/last_place_or_new_number/look_big_day/public_way.py up large life"

---
+++ super/last_place_or_new_number/look_big_day/public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_life(str_arg):
+    give_great_world(str_arg)
+    print('get_public_child_with_right_world')
+
+def give_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_life('own_thing')
---
+++ super/week/number/use_day_up_year/problem/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_first_problem(str_arg):
+    hand_and_first_time(str_arg)
+    print('work')
+
+def hand_and_first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_first_problem('right_part')
