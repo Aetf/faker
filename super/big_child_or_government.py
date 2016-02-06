9c4ae96ed9205e38b8d3d33b05c8880c82554705
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 11:12:38 -0500

    "mv: take super/find_part/go_early_world/give_big_hand/problem/have_case/woman_and_fact.py, super/year/great_time/year/feel_little_number_for_company.py or super/big_child_or_government.py above small eye"

---
+++ super/big_child_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_case(str_arg):
+    high_week(str_arg)
+    print('few_time')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_case('year')
---
+++ super/year/great_time/year/feel_little_number_for_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    small_year_and_eye(str_arg)
+    print('old_man')
+
+def small_year_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('try_company')
