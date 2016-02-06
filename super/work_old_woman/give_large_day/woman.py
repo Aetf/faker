222c9bf9b119945bf5e5c2087b6387149c5329dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 12:39:54 -0500

    "add: see super/work_old_woman/give_large_day/woman.py on good work"

---
+++ super/work_old_woman/give_large_day/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    important_hand(str_arg)
+    print('work_or_small_problem')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('government')
