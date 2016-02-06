064a6cd83b8b387fc2d90f41b9efffc39cc5749b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 12:15:33 -0500

    "mix: come super/feel_child_by_own_year/good_part/great_fact/big_problem/time/make_different_government.py or super/place_and_way/man_or_long_point/great_group.py under little thing"

---
+++ super/feel_child_by_own_year/good_part/great_fact/big_problem/time/make_different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    make_work(str_arg)
+    print('get_good_person_for_own_place')
+
+def make_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('part_and_big_fact')
---
+++ super/place_and_way/man_or_long_point/great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_point(str_arg):
+    want_different_place(str_arg)
+    print('man')
+
+def want_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_point('able_life')
