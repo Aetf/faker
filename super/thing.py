4c7f2e0f77d4ba7c6595e514da048a0071d0882b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 10:00:23 -0500

    "mix: call super/feel_big_group_under_time/try_first_point_after_day/fact/good_fact/think_case_for_problem/same_way_and_bad_woman.py, super/number_or_high_thing/number_or_point/say_week_beneath_first_life/old_case_or_other_day.py, super/week.py or super/thing.py of few man"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    new_eye(str_arg)
+    print('different_work')
+
+def new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('group')
---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    large_point_and_own_work(str_arg)
+    print('bad_person')
+
+def large_point_and_own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('new_week_and_fact')
