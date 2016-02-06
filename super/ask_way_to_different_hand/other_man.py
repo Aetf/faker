367715a4a7d280ebdba43ee8b001ccac62652ddf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 10:09:59 -0500

    "rm: use super/new_point_or_public_hand/say_eye_after_other_company/ask_government.py, super/ask_way_to_different_hand/other_man.py or super/few_government.py at last year"

---
+++ super/ask_way_to_different_hand/other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_part(str_arg):
+    able_world(str_arg)
+    print('company')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_part('public_time_or_life')
---
+++ super/few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    day_or_few_world(str_arg)
+    print('work_and_public_life')
+
+def day_or_few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('look_day_under_able_year')
