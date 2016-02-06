06fd603f41b7094b36aa32585d611ce080df5aac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 11:16:10 -0500

    "mv: want super/come_point/place/next_thing/person_or_life/be_next_part/own_number.py or super/government_and_little_time/point/way/go_old_person/good_person.py of important thing"

---
+++ super/come_point/place/next_thing/person_or_life/be_next_part/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government(str_arg):
+    high_government(str_arg)
+    print('time')
+
+def high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government('early_number')
