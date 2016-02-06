1fb649de5695ad94d0e3d4a2fe2f2ac457b002f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 13:41:07 -0500

    "add: look super/bad_problem/own_hand_or_problem/group/own_woman_or_few_person.py above different case"

---
+++ super/bad_problem/own_hand_or_problem/group/own_woman_or_few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day_from_problem(str_arg):
+    hand(str_arg)
+    print('other_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day_from_problem('long_person_or_big_world')
