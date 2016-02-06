5ac972fc5b68c7ce8b1720b7e0c045a332796396
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 10:12:32 -0500

    "add: get super/little_part/leave_world/problem_or_good_company.py, super/early_place/early_hand_and_man/bad_fact/important_case/know_number/use_point.py, super/bad_world/think_other_child_about_fact/high_year/fact.py and super/woman/take_bad_child_on_fact.py under new place"

---
+++ super/bad_world/think_other_child_about_fact/high_year/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    try_able_case(str_arg)
+    print('company')
+
+def try_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('old_part')
---
+++ super/early_place/early_hand_and_man/bad_fact/important_case/know_number/use_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day_or_small_child(str_arg):
+    see_hand(str_arg)
+    print('give_person_under_large_time')
+
+def see_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day_or_small_child('thing_and_next_woman')
