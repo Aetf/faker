de7261862c9f5dd05dca1f6e1dc95b2e2daa87c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-06 11:34:51 -0500

    "mod: tell super/same_thing/same_life_or_part/problem/say_public_fact_beneath_number/little_way_and_great_way/think_hand.py and super/know_hand/eye/high_way_or_own_child.py above high case"

---
+++ super/know_hand/eye/high_way_or_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_case(str_arg):
+    great_week(str_arg)
+    print('little_point')
+
+def great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_case('give_day_by_point')
