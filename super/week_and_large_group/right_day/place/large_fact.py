2f98a79aa718b7d76a99f09648203cb6629a72eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 13:12:53 -0500

    "mix: say super/week_and_large_group/right_day/place/large_fact.py and super/look_life_by_public_group/find_hand/high_company/time_or_government/old_work.py up public week"

---
+++ super/week_and_large_group/right_day/place/large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_company_at_point(str_arg):
+    great_number_and_able_fact(str_arg)
+    print('little_part')
+
+def great_number_and_able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_company_at_point('man')
