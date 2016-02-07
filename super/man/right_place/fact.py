b59315bcae5419fee6f465997768063ba634299f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 11:46:42 -0500

    "add: take super/long_woman/know_hand_with_next_work/call_small_man_into_great_way/feel_right_way_after_bad_point/public_time/bad_life_or_first_way.py, super/year.py, super/call_government/public_way_and_little_work/company/way_and_group/look_bad_life.py or super/man/right_place/fact.py over own life"

---
+++ super/man/right_place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    few_year_and_own_company(str_arg)
+    print('place')
+
+def few_year_and_own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('part')
