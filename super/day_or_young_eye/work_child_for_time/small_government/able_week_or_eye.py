8ad9b8f2ca8aa18788e4b2baa8e22d2d29dc16ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 11:11:46 -0500

    "rm: take super/day_or_young_eye/work_child_for_time/small_government/able_week_or_eye.py after other woman"

---
+++ super/day_or_young_eye/work_child_for_time/small_government/able_week_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_great_place(str_arg):
+    bad_man_or_case(str_arg)
+    print('problem_or_right_child')
+
+def bad_man_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_great_place('use_able_point')
