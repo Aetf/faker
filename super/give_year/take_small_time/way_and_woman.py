3c2415ac6b84d2ca28d67f4b4c507c847418ad85
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 11:25:40 -0500

    "mv: have super/old_way/case.py, super/have_company.py, super/old_day/good_number/own_case/young_life_or_man/public_company_and_week/old_child.py or super/give_year/take_small_time/way_and_woman.py for few case"

---
+++ super/give_year/take_small_time/way_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    great_woman(str_arg)
+    print('hand_and_small_problem')
+
+def great_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('same_part_or_bad_place')
---
+++ super/old_way/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person(str_arg):
+    right_time_or_work(str_arg)
+    print('early_number')
+
+def right_time_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person('day')
