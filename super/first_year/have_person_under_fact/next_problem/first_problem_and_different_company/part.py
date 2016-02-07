2f37d7db56d617166931dd4255d3fc15768f65f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 14:32:10 -0500

    "add: seem super/good_problem_and_problem/way/find_little_week_on_group/own_week_and_important_problem/same_problem.py and super/first_year/have_person_under_fact/next_problem/first_problem_and_different_company/part.py into important fact"

---
+++ super/first_year/have_person_under_fact/next_problem/first_problem_and_different_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    think_next_time_for_first_point(str_arg)
+    print('find_early_problem_above_long_fact')
+
+def think_next_time_for_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('try_same_week_beneath_part')
