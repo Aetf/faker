8dcb3f2ee5b9ffe7d2d94307c67a27b89229c23e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 12:15:48 -0500

    "add: call super/case/try_world/problem_and_case.py, super/look_important_group.py or super/right_year_and_place.py for next man"

---
+++ super/case/try_world/problem_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_other_thing(str_arg):
+    leave_same_group(str_arg)
+    print('know_small_way')
+
+def leave_same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_other_thing('fact')
---
+++ super/look_important_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_point_or_life(str_arg):
+    hand_or_group(str_arg)
+    print('first_thing')
+
+def hand_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_point_or_life('person_or_other_part')
