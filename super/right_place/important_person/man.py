30d7909d0fa07b498781489e9e977d7c42a13e14
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 10:10:05 -0500

    "rm: ask super/small_company_or_right_work/own_year_or_first_group.py, super/find_number_into_day.py, super/right_place/important_person/man.py or super/same_work/same_child_and_same_person/long_eye_or_eye.py into few case"

---
+++ super/right_place/important_person/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_work_and_few_problem(str_arg):
+    look_public_fact_above_day(str_arg)
+    print('fact_and_place')
+
+def look_public_fact_above_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_work_and_few_problem('few_work_or_new_year')
---
+++ super/same_work/same_child_and_same_person/long_eye_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    eye_and_thing(str_arg)
+    print('make_point')
+
+def eye_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('man_and_year')
