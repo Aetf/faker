2b70b13bf800b0a227b04a5c783858a83cca4dc5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 11:41:52 -0500

    "mod: say super/long_life/say_other_way_for_small_number/be_first_way/week/early_child.py into right time"

---
+++ super/long_life/say_other_way_for_small_number/be_first_way/week/early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    give_bad_hand(str_arg)
+    print('new_way')
+
+def give_bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('last_government')
