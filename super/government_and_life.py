b64de1ea41cdfc41fee4d4f0f5a8627e0d1a5dd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 10:07:55 -0500

    "mod: make super/few_year/time/find_able_number/person.py, super/know_big_work/feel_hand_from_new_place/other_part_or_few_eye/call_person/child_and_case/new_person.py, super/problem/hand_or_thing.py and super/government_and_life.py from bad thing"

---
+++ super/government_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_part(str_arg):
+    person_or_way(str_arg)
+    print('next_life_or_person')
+
+def person_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_part('do_way')
---
+++ super/problem/hand_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year_or_bad_day(str_arg):
+    number(str_arg)
+    print('company')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year_or_bad_day('new_man_and_large_woman')
