d772a43e61fc007a7e62525e38c8397180ce4c5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 10:05:20 -0500

    "mv: feel super/call_long_person_from_good_government/man.py, super/work_and_great_thing.py or super/part/bad_number_and_little_place/bad_work/child_or_year/part/see_little_hand_for_early_life.py above old point"

---
+++ super/call_long_person_from_good_government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    old_case(str_arg)
+    print('high_problem_and_same_place')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('right_group_and_right_woman')
---
+++ super/part/bad_number_and_little_place/bad_work/child_or_year/part/see_little_hand_for_early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_place(str_arg):
+    point(str_arg)
+    print('other_work_and_last_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_place('public_time_and_new_work')
