f97b5e737327d483623746ffdd7231fb7b2fd19a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 12:35:06 -0500

    "add: say super/own_life_and_number/small_life.py at different company"

---
+++ super/own_life_and_number/small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    get_bad_eye(str_arg)
+    print('old_group_and_number')
+
+def get_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('find_important_year')
