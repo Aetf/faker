bbd9b82912b50608846faf736db170540eae74a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 13:06:31 -0500

    "rm: have super/man_and_point/high_company/use_hand/work_old_world/have_few_group_into_company/small_person.py from right hand"

---
+++ super/man_and_point/high_company/use_hand/work_old_world/have_few_group_into_company/small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    child(str_arg)
+    print('know_public_work')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('big_group_and_thing')
