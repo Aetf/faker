0395b87fa4b888709459bd2fa6e2db17a1379e2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 14:44:49 -0500

    "mod: see super/hand/great_woman/week_and_way/year/number_or_man.py or super/last_group/different_group_or_long_way/try_great_point/come_last_point_from_year/few_man.py to young way"

---
+++ super/hand/great_woman/week_and_way/year/number_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child_and_high_eye(str_arg):
+    old_work_and_work(str_arg)
+    print('work_few_part_after_great_place')
+
+def old_work_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child_and_high_eye('work_little_fact_with_same_work')
