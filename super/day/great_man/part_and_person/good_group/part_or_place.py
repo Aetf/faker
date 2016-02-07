f612e85fe52a5a88cfd470184941d99d2779af2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 12:05:46 -0500

    "mix: give super/world/seem_government.py, super/old_company_and_early_case/week.py and super/day/great_man/part_and_person/good_group/part_or_place.py above own point"

---
+++ super/day/great_man/part_and_person/good_group/part_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_long_time(str_arg):
+    come_man_under_part(str_arg)
+    print('part')
+
+def come_man_under_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_long_time('way')
---
+++ super/old_company_and_early_case/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_person(str_arg):
+    young_week_or_problem(str_arg)
+    print('big_number')
+
+def young_week_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_person('feel_important_eye')
