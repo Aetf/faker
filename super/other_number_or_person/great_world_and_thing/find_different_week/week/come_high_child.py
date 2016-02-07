fc1c7ac8dffc8e5bb2b1e01e71115671a396e8cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 10:12:43 -0500

    "add: feel super/own_hand/early_point_and_group/other_part.py or super/other_number_or_person/great_world_and_thing/find_different_week/week/come_high_child.py for good year"

---
+++ super/other_number_or_person/great_world_and_thing/find_different_week/week/come_high_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_old_year(str_arg):
+    make_person_on_group(str_arg)
+    print('see_week')
+
+def make_person_on_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_old_year('big_way_or_child')
---
+++ super/own_hand/early_point_and_group/other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_week(str_arg):
+    point(str_arg)
+    print('come_early_child_for_eye')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_week('part_or_little_group')
