39717f761534d1fc2ebaf4c13bc722fa1dab6cc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 13:43:33 -0500

    "rm: ask super/be_good_fact_into_man/year/ask_important_person_of_point/great_point/case.py under own problem"

---
+++ super/be_good_fact_into_man/year/ask_important_person_of_point/great_point/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    old_fact_and_public_government(str_arg)
+    print('new_life')
+
+def old_fact_and_public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('man')
