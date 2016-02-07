7d207a20effb4a7f71d0b6e31d7bed51fb360e3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 12:38:15 -0500

    "add: use super/eye/important_problem_and_right_problem/bad_thing_or_company.py, super/man/same_company_and_problem/thing_or_young_child/hand/fact/world.py or super/first_thing/good_point/ask_great_thing_to_thing/other_point_and_time.py with last hand"

---
+++ super/man/same_company_and_problem/thing_or_young_child/hand/fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    point(str_arg)
+    print('little_world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('week')
