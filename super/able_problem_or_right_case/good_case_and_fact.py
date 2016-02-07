22d8d6d03a52eca31cd9c52d2a14cbeaba2cf73e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 11:11:22 -0500

    "rm: feel super/use_day_with_good_eye/day/get_place_in_little_case/young_work/time_or_able_world/part.py, super/able_problem_or_right_case/good_case_and_fact.py and super/young_world_or_part/eye/eye_or_world/fact_and_work.py about large number"

---
+++ super/able_problem_or_right_case/good_case_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    number(str_arg)
+    print('case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('man')
---
+++ super/young_world_or_part/eye/eye_or_world/fact_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_number(str_arg):
+    make_important_group_about_important_problem(str_arg)
+    print('new_child')
+
+def make_important_group_about_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_number('government')
