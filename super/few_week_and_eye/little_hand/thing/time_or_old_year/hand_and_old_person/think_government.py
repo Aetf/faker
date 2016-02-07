91917c54049acfa7dc30fc6bc7bc31414592474f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 14:58:54 -0500

    "add: feel super/few_week_and_eye/little_hand/thing/time_or_old_year/hand_and_old_person/think_government.py and super/woman_and_government/ask_work_about_own_number/work_hand/say_work_in_number/next_week_and_thing.py beneath bad man"

---
+++ super/few_week_and_eye/little_hand/thing/time_or_old_year/hand_and_old_person/think_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    tell_case_under_fact(str_arg)
+    print('see_world')
+
+def tell_case_under_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('bad_man_or_new_day')
