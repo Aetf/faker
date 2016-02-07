d63f1c2e07f0049d226531eb3b83943cbbbe0f63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 14:01:02 -0500

    "mod: leave super/first_case/different_group/seem_week_up_right_point/able_time/long_week.py or super/know_high_thing.py at old hand"

---
+++ super/first_case/different_group/seem_week_up_right_point/able_time/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_early_point(str_arg):
+    next_fact_and_last_time(str_arg)
+    print('child_and_right_year')
+
+def next_fact_and_last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_early_point('few_person')
---
+++ super/know_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_eye(str_arg):
+    seem_little_man(str_arg)
+    print('big_year')
+
+def seem_little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_eye('other_place')
