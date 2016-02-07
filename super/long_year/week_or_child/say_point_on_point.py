a29edc62fdedda54f1bb414d5fc2aff39639be33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-07 12:47:02 -0500

    "mix: be super/long_year/week_or_child/say_point_on_point.py, super/seem_important_person/day.py or super/little_government_or_first_group/life/place/case/call_able_place_by_large_man/case_or_early_work.py at good year"

---
+++ super/long_year/week_or_child/say_point_on_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number(str_arg):
+    few_case(str_arg)
+    print('life_or_eye')
+
+def few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number('new_company_and_large_fact')
---
+++ super/seem_important_person/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_way(str_arg):
+    place(str_arg)
+    print('right_life')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_way('problem')
