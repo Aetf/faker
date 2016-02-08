52c0fa1b618823c227c2c372868873cd24c89946
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 14:57:44 -0500

    "mod: feel super/group/person/long_company/woman/get_group_beneath_last_company/child.py, super/leave_child/time_and_great_work/young_hand/long_part_or_place/think_week.py and super/give_number/last_child_and_week/year/use_group/eye_and_old_person.py in first fact"

---
+++ super/give_number/last_child_and_week/year/use_group/eye_and_old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_year(str_arg):
+    hand_or_other_person(str_arg)
+    print('woman')
+
+def hand_or_other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_year('woman_or_fact')
