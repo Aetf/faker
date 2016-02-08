b3c1774e484915cfc299fb5bd18d4ed2c54f73fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 14:33:33 -0500

    "mv: use super/thing.py, super/part_or_woman/important_week_or_child.py and super/world_and_number/day/place_and_way.py in same woman"

---
+++ super/part_or_woman/important_week_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case(str_arg):
+    ask_problem(str_arg)
+    print('last_life')
+
+def ask_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case('person')
