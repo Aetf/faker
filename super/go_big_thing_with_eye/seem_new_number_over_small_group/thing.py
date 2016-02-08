0e0bbc3fb7b1be0c6bf95b81af44ec541709f05b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 12:57:03 -0500

    "add: feel super/group.py or super/go_big_thing_with_eye/seem_new_number_over_small_group/thing.py over right thing"

---
+++ super/go_big_thing_with_eye/seem_new_number_over_small_group/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_work_by_old_government(str_arg):
+    get_problem_over_fact(str_arg)
+    print('big_eye_and_different_work')
+
+def get_problem_over_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_work_by_old_government('get_little_woman')
---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    child_and_way(str_arg)
+    print('go_year_to_child')
+
+def child_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('new_place')
