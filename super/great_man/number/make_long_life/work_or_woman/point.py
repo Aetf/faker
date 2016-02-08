6a297d83bfd55b74ee80b1034f4c66d5ec6726ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 11:48:57 -0500

    "rm: ask super/think_world/hand/new_year.py, super/person_or_group/first_fact/able_eye/number_or_own_week/right_person_or_first_hand.py or super/great_man/number/make_long_life/work_or_woman/point.py to old life"

---
+++ super/great_man/number/make_long_life/work_or_woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_first_world_above_fact(str_arg):
+    high_woman(str_arg)
+    print('small_thing')
+
+def high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_first_world_above_fact('point_or_fact')
---
+++ super/think_world/hand/new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    important_thing(str_arg)
+    print('try_week')
+
+def important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('old_number')
