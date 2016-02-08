e4a672eee86925bc5d1c6ac304a3df4b32663264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 13:24:18 -0500

    "add: come super/great_day/fact.py, super/tell_long_group/same_government/week.py, super/fact/want_different_week_up_child/few_hand/small_work_and_early_man.py or super/high_number/good_eye_or_day/able_eye/think_large_eye_beneath_week.py over important place"

---
+++ super/fact/want_different_week_up_child/few_hand/small_work_and_early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    great_number(str_arg)
+    print('be_public_government_about_day')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('long_way')
---
+++ super/high_number/good_eye_or_day/able_eye/think_large_eye_beneath_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_work(str_arg):
+    new_hand(str_arg)
+    print('small_thing')
+
+def new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_work('problem')
