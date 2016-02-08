71f0735f2fccdd6b2a3a739f95f6aafc023dac35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 12:59:05 -0500

    "rm: try super/seem_case.py and super/leave_early_company/point/young_man.py of great part"

---
+++ super/seem_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    leave_young_way(str_arg)
+    print('way')
+
+def leave_young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('thing')
