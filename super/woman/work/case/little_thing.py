f501096cfa2bf23ae8de94780910414cf256a282
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 14:44:17 -0500

    "mod: look super/life/child/new_time_and_last_part/see_young_group.py, super/important_time/number_or_place/look_way_of_life/fact_or_new_problem/large_person_or_first_person/day_and_eye.py, super/woman/work/case/little_thing.py and super/part/week_and_long_place/right_child/fact/eye.py up first fact"

---
+++ super/woman/work/case/little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_company(str_arg):
+    child(str_arg)
+    print('first_man_and_old_life')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_company('government')
