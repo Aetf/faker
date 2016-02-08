6a7d67089404aa85231345fcb32c4162c27383ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 12:52:27 -0500

    "mix: call super/big_thing/young_hand_or_man/early_work/go_case_under_early_child/way.py over early week"

---
+++ super/big_thing/young_hand_or_man/early_work/go_case_under_early_child/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case(str_arg):
+    next_number_and_own_company(str_arg)
+    print('call_high_group')
+
+def next_number_and_own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case('problem')
