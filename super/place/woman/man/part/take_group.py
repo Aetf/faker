9843c6c6666b52dfcf0bd22e20a7d8793cd69653
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 10:58:16 -0500

    "mix: know super/place/woman/man/part/take_group.py above good time"

---
+++ super/place/woman/man/part/take_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    year(str_arg)
+    print('small_thing')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('high_case')
