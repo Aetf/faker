433e439e6ab793653357f5c8bd6e41b0cf46b898
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 13:19:47 -0500

    "add: make super/early_eye.py, super/world/child.py, super/thing/number/other_eye_and_problem/few_company.py or super/point_or_work/little_child/good_man/new_year.py of good point"

---
+++ super/thing/number/other_eye_and_problem/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government(str_arg):
+    time_and_early_person(str_arg)
+    print('same_time_and_child')
+
+def time_and_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government('little_company')
