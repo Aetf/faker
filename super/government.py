0626ec1fc0a96e502397465cc7ef2e38ac492b39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 10:38:44 -0500

    "rm: call super/government.py above small work"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work_same_fact_on_day(str_arg)
+    print('thing_or_eye')
+
+def work_same_fact_on_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('early_group_or_day')
