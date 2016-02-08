a387d4f17490e369c7d361a15ff907dac8342638
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 12:49:58 -0500

    "add: say super/want_woman_at_child/be_new_week_beneath_part/bad_government_or_young_time/call_right_week_by_old_point.py, super/world/number/government_and_day.py, super/hand.py and super/new_place/give_government_with_point/little_place/get_eye/large_company_and_thing/part_or_large_man.py above small company"

---
+++ super/want_woman_at_child/be_new_week_beneath_part/bad_government_or_young_time/call_right_week_by_old_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    do_year_above_able_place(str_arg)
+    print('see_group')
+
+def do_year_above_able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('different_world')
