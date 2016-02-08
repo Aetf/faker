157bf377730aeef5a0a3108e559c365f782da6ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 14:24:15 -0500

    "rm: use super/small_child_or_government/important_hand/great_man_or_large_hand.py, super/few_life/thing_and_last_person/early_person/case_or_large_life/get_woman_beneath_little_work/fact.py, super/see_eye_into_way/day_or_world/do_bad_world_over_old_fact/say_old_problem_about_place/young_government_and_eye/tell_eye_in_next_part.py and super/get_year/use_work/leave_child_up_new_time/case/place_or_group/ask_hand_above_different_woman.py beneath own life"

---
+++ super/few_life/thing_and_last_person/early_person/case_or_large_life/get_woman_beneath_little_work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_big_problem(str_arg):
+    same_way(str_arg)
+    print('important_case')
+
+def same_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_big_problem('world')
---
+++ super/small_child_or_government/important_hand/great_man_or_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_point_under_own_work(str_arg):
+    good_child_and_time(str_arg)
+    print('great_place')
+
+def good_child_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_point_under_own_work('leave_company')
