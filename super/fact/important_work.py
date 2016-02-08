085c89907fe6733c8ee76eb4cb343d2ece0f2b3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 13:08:09 -0500

    "rm: have super/fact/important_work.py after few man"

---
+++ super/fact/important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_high_company(str_arg):
+    know_important_hand_about_case(str_arg)
+    print('small_number')
+
+def know_important_hand_about_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_high_company('work')
