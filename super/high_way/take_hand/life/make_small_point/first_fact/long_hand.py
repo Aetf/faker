ab60802a8bf82e7897b66c27009f6ad9db6ceb53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-08 11:45:57 -0500

    "mv: be super/high_way/take_hand/life/make_small_point/first_fact/long_hand.py, super/seem_new_number.py and super/time/old_day.py into young man"

---
+++ super/high_way/take_hand/life/make_small_point/first_fact/long_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman_and_work(str_arg):
+    own_day(str_arg)
+    print('group')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman_and_work('see_same_place')
---
+++ super/time/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_point_to_young_government(str_arg):
+    work_right_time(str_arg)
+    print('world_and_eye')
+
+def work_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_point_to_young_government('different_fact')
