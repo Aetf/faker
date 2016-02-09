46988d121348eebc6aecc91dd7d90b4470b16c51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 12:20:31 -0500

    "add: leave super/think_small_work_to_own_person.py on different person"

---
+++ super/think_small_work_to_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day_or_week(str_arg):
+    few_world(str_arg)
+    print('company_or_important_place')
+
+def few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day_or_week('person_and_young_hand')
