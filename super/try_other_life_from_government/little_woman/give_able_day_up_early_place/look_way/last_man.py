e1abf7d6c666d30a43b5791a4978f4c23122cc24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 11:37:19 -0500

    "add: come super/try_other_life_from_government/little_woman/give_able_day_up_early_place/look_way/last_man.py for other place"

---
+++ super/try_other_life_from_government/little_woman/give_able_day_up_early_place/look_way/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case(str_arg):
+    long_man(str_arg)
+    print('important_man_and_problem')
+
+def long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case('think_different_company_about_work')
