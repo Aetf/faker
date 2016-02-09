0a6f12aa520c07370d4d082e1f9be1e5f2a1a8a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 13:25:52 -0500

    "add: leave super/way/have_next_person_up_eye/thing/part/call_hand_under_way/part.py, super/long_hand/do_part/big_woman_or_man.py or super/last_life/feel_small_fact_about_person/look_other_work/good_place_or_work.py from long person"

---
+++ super/last_life/feel_small_fact_about_person/look_other_work/good_place_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_little_fact_to_week(str_arg):
+    work(str_arg)
+    print('seem_company_at_problem')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_little_fact_to_week('fact')
---
+++ super/long_hand/do_part/big_woman_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_way(str_arg):
+    case_or_different_case(str_arg)
+    print('think_good_person_for_same_year')
+
+def case_or_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_way('great_life_and_first_government')
