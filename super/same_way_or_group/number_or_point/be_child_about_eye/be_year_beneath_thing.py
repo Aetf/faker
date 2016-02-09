e3404cd3c9f90b04b2d2b25a5143384e163e761b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 13:54:17 -0500

    "rm: give super/year/child/government.py or super/same_way_or_group/number_or_point/be_child_about_eye/be_year_beneath_thing.py of old number"

---
+++ super/same_way_or_group/number_or_point/be_child_about_eye/be_year_beneath_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    young_government(str_arg)
+    print('use_next_point')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('work')
---
+++ super/year/child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_first_company(str_arg):
+    woman(str_arg)
+    print('important_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_first_company('try_number')
