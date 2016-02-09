123bff98aa05f4b97d00df00653e72bfd7fe707e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 12:31:51 -0500

    "rm: seem super/important_world/life/day_or_right_week/number_and_different_child/place/woman_or_year.py, super/do_person_beneath_young_way.py or super/try_large_man_from_able_number/world/person_or_public_part/feel_few_part_beneath_day/able_life_or_next_week/point.py after same case"

---
+++ super/do_person_beneath_young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point(str_arg):
+    woman_and_bad_man(str_arg)
+    print('same_child_and_small_child')
+
+def woman_and_bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point('thing_or_small_hand')
---
+++ super/important_world/life/day_or_right_week/number_and_different_child/place/woman_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem_or_bad_world(str_arg):
+    important_part(str_arg)
+    print('young_eye')
+
+def important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem_or_bad_world('new_man')
