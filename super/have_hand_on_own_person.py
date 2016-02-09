8d9b6ba3fedce02165d51a0c0ee4495023c93d0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 10:27:54 -0500

    "add: try super/eye_and_thing/high_part_and_point/know_company_over_year.py and super/have_hand_on_own_person.py into small life"

---
+++ super/have_hand_on_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman(str_arg):
+    public_week(str_arg)
+    print('week_and_little_week')
+
+def public_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman('call_important_child_after_group')
