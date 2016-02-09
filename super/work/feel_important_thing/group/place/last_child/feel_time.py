9d3fd792f1e24dd773408a385fabb897e940b641
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 11:20:24 -0500

    "mod: go super/work/feel_important_thing/group/place/last_child/feel_time.py about high group"

---
+++ super/work/feel_important_thing/group/place/last_child/feel_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_woman_and_same_work(str_arg):
+    small_point(str_arg)
+    print('give_big_work_for_year')
+
+def small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_woman_and_same_work('way')
