5c7cd8537c3a89f30f461099aa912a95c4208d9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 14:33:49 -0500

    "mv: go super/man.py, super/tell_high_person_over_eye/young_person/other_eye/tell_world.py or super/important_work/have_large_government_about_man/get_first_group_on_place/early_week_or_problem.py in able year"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_work(str_arg):
+    other_part(str_arg)
+    print('fact_or_public_company')
+
+def other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_work('large_time')
---
+++ super/tell_high_person_over_eye/young_person/other_eye/tell_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_fact(str_arg):
+    hand_and_last_number(str_arg)
+    print('able_eye')
+
+def hand_and_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_fact('first_person')
