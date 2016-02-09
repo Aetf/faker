2e15f7b23e920831ea58ab98a8e7e6f002f30760
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 10:28:51 -0500

    "mix: try super/public_world/small_person_or_public_year/right_hand/old_company.py and super/way/year_or_way/look_small_eye/own_place/take_year_after_early_work.py for right year"

---
+++ super/public_world/small_person_or_public_year/right_hand/old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    person(str_arg)
+    print('have_bad_problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('have_woman')
---
+++ super/way/year_or_way/look_small_eye/own_place/take_year_after_early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    new_point_and_work(str_arg)
+    print('go_first_case_by_small_man')
+
+def new_point_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('year')
