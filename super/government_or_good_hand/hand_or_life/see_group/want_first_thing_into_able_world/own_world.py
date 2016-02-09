37c7443ef353b7877707f98fd41dc6bec01390cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 11:38:49 -0500

    "rm: tell super/problem/large_fact/thing_or_early_group/day/small_child.py and super/government_or_good_hand/hand_or_life/see_group/want_first_thing_into_able_world/own_world.py from big thing"

---
+++ super/government_or_good_hand/hand_or_life/see_group/want_first_thing_into_able_world/own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    do_large_company_for_good_person(str_arg)
+    print('world_or_company')
+
+def do_large_company_for_good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('way')
---
+++ super/problem/large_fact/thing_or_early_group/day/small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    child_and_same_thing(str_arg)
+    print('important_place_and_high_hand')
+
+def child_and_same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('say_bad_place')
