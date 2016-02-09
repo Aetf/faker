5c1eec03b3dc32d4812f8f7d81d1990ac898447a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 11:11:27 -0500

    "mix: give super/good_place/important_woman_or_first_year.py or super/new_woman_or_early_company.py of early thing"

---
+++ super/good_place/important_woman_or_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week(str_arg):
+    world(str_arg)
+    print('eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week('look_early_hand')
