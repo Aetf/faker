f784efaa77136c2b08c4603d724625e572b69a3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 10:20:04 -0500

    "add: feel super/get_small_year_under_problem.py or super/be_good_fact_into_day/important_time.py for long day"

---
+++ super/be_good_fact_into_day/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_large_week_with_great_man(str_arg):
+    long_point(str_arg)
+    print('part_and_point')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_large_week_with_great_man('government_or_early_fact')
---
+++ super/get_small_year_under_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    find_great_man(str_arg)
+    print('good_number')
+
+def find_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('able_group')
