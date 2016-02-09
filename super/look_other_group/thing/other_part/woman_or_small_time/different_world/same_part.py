025aa1cd678f844c1355f722b41b97db07072094
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 13:59:05 -0500

    "rm: make super/high_week_or_number/see_few_woman_above_time.py and super/look_other_group/thing/other_part/woman_or_small_time/different_world/same_part.py up old child"

---
+++ super/look_other_group/thing/other_part/woman_or_small_time/different_world/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_few_man(str_arg):
+    time(str_arg)
+    print('think_big_woman')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_few_man('year')
