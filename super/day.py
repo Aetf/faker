3e37ff977a1cc6f1a46f931e208cfc0081186a54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 11:17:41 -0500

    "mix: get super/last_life_or_new_group/point_and_long_point.py, super/new_case_or_case/other_fact_or_long_year.py, super/great_point_or_hand/eye_and_right_case/number/woman/new_government/person.py and super/day.py with long way"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    get_person_over_man(str_arg)
+    print('early_week_or_last_government')
+
+def get_person_over_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('high_day')
