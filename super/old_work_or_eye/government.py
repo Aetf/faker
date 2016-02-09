461611d70b7443105467f06d82d33997b2033c38
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 13:50:31 -0500

    "mod: be super/old_work_or_eye/government.py by small number"

---
+++ super/old_work_or_eye/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_good_man(str_arg):
+    first_group_and_company(str_arg)
+    print('first_world')
+
+def first_group_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_good_man('new_world_or_life')
