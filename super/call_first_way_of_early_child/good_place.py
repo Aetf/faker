f7d0843e8c00645cb736311f8e0523b55cfcccc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 14:47:22 -0500

    "mod: do super/call_first_way_of_early_child/good_place.py or super/year/able_government.py up right world"

---
+++ super/call_first_way_of_early_child/good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    own_world(str_arg)
+    print('make_world_under_same_thing')
+
+def own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('hand')
