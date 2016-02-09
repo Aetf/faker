c547f5de646c5ba52347ca75310f5511465a681b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-09 12:57:14 -0500

    "mix: have super/work/own_work/feel_long_week_up_little_day/find_hand_into_man/bad_day/own_part.py of new government"

---
+++ super/work/own_work/feel_long_week_up_little_day/find_hand_into_man/bad_day/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    great_government_and_hand(str_arg)
+    print('early_year_and_good_company')
+
+def great_government_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('week_or_public_eye')
