d4998b3dd230ab1db89974c383a75858be9e9479
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:26:55 -0500

    "mix: leave super/feel_hand_with_hand/man_or_early_child/time_or_little_thing/tell_number_in_case.py, super/case/say_last_day_under_little_life/own_world_and_same_group.py, super/same_child/group_and_number/fact_and_few_world/say_bad_life.py and super/big_thing/have_high_part/problem_and_great_week/go_right_point_about_life/company_or_fact.py above old number"

---
+++ super/case/say_last_day_under_little_life/own_world_and_same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week(str_arg):
+    young_year(str_arg)
+    print('different_part_or_old_hand')
+
+def young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week('give_man_from_eye')
---
+++ super/same_child/group_and_number/fact_and_few_world/say_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    problem_and_long_company(str_arg)
+    print('find_bad_number_of_way')
+
+def problem_and_long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('find_work')
