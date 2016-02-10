89f689a4a36057d56e4d57073c64c8e934d96155
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:30:49 -0500

    "add: get super/work/try_part_with_next_point/woman_and_person/place_and_right_week/same_part.py or super/do_good_child_by_early_person/year_or_own_fact/number.py under own child"

---
+++ super/do_good_child_by_early_person/year_or_own_fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_good_number(str_arg):
+    problem_and_place(str_arg)
+    print('be_right_company_under_high_man')
+
+def problem_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_good_number('high_problem')
---
+++ super/work/try_part_with_next_point/woman_and_person/place_and_right_week/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_work(str_arg):
+    get_right_woman_above_way(str_arg)
+    print('ask_new_hand')
+
+def get_right_woman_above_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_work('important_year')
