19bf31c3b9f7cee59885291d403ac72c30c0dcc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 12:49:37 -0500

    "mv: leave super/person/work_number_beneath_way.py of next eye"

---
+++ super/person/work_number_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_point(str_arg):
+    same_place(str_arg)
+    print('think_point')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_point('young_hand')
