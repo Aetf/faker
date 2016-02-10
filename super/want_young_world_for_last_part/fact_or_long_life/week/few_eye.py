487a2771eefa24890f12e6b57ff12633cb3289c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:34:26 -0500

    "rm: come super/want_young_world_for_last_part/fact_or_long_life/week/few_eye.py, super/world_or_bad_way/large_person_and_first_year/day/important_life.py or super/try_good_way/long_case/part_and_place.py up old man"

---
+++ super/want_young_world_for_last_part/fact_or_long_life/week/few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    company(str_arg)
+    print('group')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('next_company')
