04db19f9d61e5d8855d6c352a7536cb1efb45ffc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:10:37 -0500

    "mod: say super/eye_or_group/high_work/feel_world/work/young_work.py or super/high_man/eye/group/week/time_or_way.py for young day"

---
+++ super/eye_or_group/high_work/feel_world/work/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_eye_at_child(str_arg):
+    other_person(str_arg)
+    print('great_thing')
+
+def other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_eye_at_child('first_place')
---
+++ super/high_man/eye/group/week/time_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_part(str_arg):
+    man(str_arg)
+    print('time_or_old_year')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_part('find_bad_fact')
