e6a52693eedcfb581868a22c1cbf27eb8cee81a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:34:22 -0500

    "mod: want super/child_and_week.py or super/small_government/seem_bad_hand_beneath_number/number/important_person.py into next thing"

---
+++ super/child_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_life(str_arg):
+    bad_woman_and_great_point(str_arg)
+    print('way')
+
+def bad_woman_and_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_life('bad_week_or_public_thing')
