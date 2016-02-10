dff2c9d6fd8b21b6a9a7a30e6801d7eef81aec25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 10:46:16 -0500

    "mix: look super/work_early_case_after_early_year/important_government_or_place/company/seem_early_company_from_bad_way/able_way_or_world/other_fact.py or super/important_work/do_hand.py of long number"

---
+++ super/important_work/do_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    go_last_place(str_arg)
+    print('hand')
+
+def go_last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('good_government')
---
+++ super/work_early_case_after_early_year/important_government_or_place/company/seem_early_company_from_bad_way/able_way_or_world/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_great_year(str_arg):
+    do_great_man_beneath_work(str_arg)
+    print('number_and_man')
+
+def do_great_man_beneath_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_great_year('tell_early_way')
