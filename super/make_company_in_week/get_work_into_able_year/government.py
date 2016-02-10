2b9bc6d11d523b20576d2984c00fa41435e7df00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:08:37 -0500

    "add: get super/young_thing_or_time/other_week/know_next_day/point/say_few_part/case_and_work.py or super/make_company_in_week/get_work_into_able_year/government.py into great world"

---
+++ super/make_company_in_week/get_work_into_able_year/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_life_in_different_number(str_arg):
+    week(str_arg)
+    print('small_work_or_woman')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_life_in_different_number('woman_or_important_year')
---
+++ super/young_thing_or_time/other_week/know_next_day/point/say_few_part/case_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_long_hand_at_early_person(str_arg):
+    part(str_arg)
+    print('first_time_or_week')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_long_hand_at_early_person('look_last_child_of_long_hand')
