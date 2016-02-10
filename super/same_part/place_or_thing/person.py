50a74d88555873e0022dc17a7f7a611efc9815c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:49:02 -0500

    "rm: ask super/place/number/find_year/leave_number_up_group/week.py, super/other_fact_and_small_eye/eye_or_week/first_case_or_great_part/say_day_up_own_way.py and super/same_part/place_or_thing/person.py on young life"

---
+++ super/same_part/place_or_thing/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_eye(str_arg):
+    year(str_arg)
+    print('leave_small_case_after_woman')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_eye('last_fact')
