08a24430009cd8b9ccc46396287b9710e8fb3b6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:36:55 -0500

    "mix: think super/public_group_and_old_government/ask_right_week/week_and_hand/group/woman.py, super/point/way/next_point/look_thing_over_way/man_or_year/woman_or_big_work.py, super/long_problem/large_group/different_child/different_world.py or super/get_long_person/get_case_with_place.py after own woman"

---
+++ super/long_problem/large_group/different_child/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_young_government_at_time(str_arg):
+    give_able_world(str_arg)
+    print('other_thing')
+
+def give_able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_young_government_at_time('own_work_and_new_fact')
---
+++ super/point/way/next_point/look_thing_over_way/man_or_year/woman_or_big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    thing(str_arg)
+    print('give_public_child')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('first_person')
