5daa03048f94f553b864c00031c08ead1466012d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 10:29:53 -0500

    "mix: make super/get_child_for_life/use_problem.py, super/tell_able_week/think_company_beneath_first_person/use_fact_of_little_child/woman_and_early_point/find_world/company.py, super/important_work.py and super/fact/world/woman/world/look_child.py for young woman"

---
+++ super/important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world(str_arg):
+    have_bad_way_in_public_group(str_arg)
+    print('right_person')
+
+def have_bad_way_in_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world('seem_public_child')
