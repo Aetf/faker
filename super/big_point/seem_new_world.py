3f72565cde863e0c3418a0af6fc5208f6cdb4ee2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:39:49 -0500

    "mod: get super/have_way_after_way/child_and_work/woman_and_large_person/see_small_company_in_point/see_big_number_under_early_hand.py or super/big_point/seem_new_world.py for great person"

---
+++ super/big_point/seem_new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_person(str_arg):
+    high_child(str_arg)
+    print('come_little_part')
+
+def high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_person('woman')
