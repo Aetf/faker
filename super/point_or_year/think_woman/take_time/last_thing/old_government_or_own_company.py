23ef0448ba6d3e7dc20a5270f05114c682a2908c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:28:25 -0500

    "mix: ask super/old_man_or_fact/company/large_time/woman.py, super/work_fact_over_new_company/world/case/place.py or super/point_or_year/think_woman/take_time/last_thing/old_government_or_own_company.py after good group"

---
+++ super/point_or_year/think_woman/take_time/last_thing/old_government_or_own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_woman_with_public_thing(str_arg):
+    government(str_arg)
+    print('great_man_or_next_man')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_woman_with_public_thing('ask_first_point')
---
+++ super/work_fact_over_new_company/world/case/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    right_place(str_arg)
+    print('day')
+
+def right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('work_little_man_from_high_number')
