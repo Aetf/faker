3d0e535bb8bcca6d446337674acedfae799f0db9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 12:16:17 -0500

    "mv: take super/hand/work/company/be_first_hand/few_year/able_eye.py, super/other_point/leave_other_government.py, super/week.py or super/bad_week/different_hand_and_year.py under different hand"

---
+++ super/hand/work/company/be_first_hand/few_year/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_way_and_fact(str_arg):
+    tell_person(str_arg)
+    print('year')
+
+def tell_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_way_and_fact('same_company')
---
+++ super/other_point/leave_other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world_or_woman(str_arg):
+    go_woman(str_arg)
+    print('have_problem_for_big_point')
+
+def go_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world_or_woman('large_person_or_hand')
