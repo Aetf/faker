127193286b9ce4d47d9ba048f6ab2a6d84670dcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 12:32:07 -0500

    "mod: try super/public_point/government/child_or_government/big_child/number_or_way.py with same thing"

---
+++ super/public_point/government/child_or_government/big_child/number_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    group_or_big_child(str_arg)
+    print('try_person_with_world')
+
+def group_or_big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_world')
