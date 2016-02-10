1e4fcf4b0d04c5528c35ab079eda3ac2bbdcbaf2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 10:19:02 -0500

    "mod: feel super/go_important_man/thing.py or super/point.py for small case"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_bad_problem(str_arg):
+    leave_company_into_company(str_arg)
+    print('person')
+
+def leave_company_into_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_bad_problem('right_man')
