26eb039f9796f6d5d8454989f5bc06d4b7dc0ffe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:54:17 -0500

    "mod: call super/part.py or super/feel_few_man/bad_way/long_case/ask_other_person.py up high place"

---
+++ super/feel_few_man/bad_way/long_case/ask_other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    group_or_other_place(str_arg)
+    print('look_day')
+
+def group_or_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('eye_or_world')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    next_group(str_arg)
+    print('week_and_week')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('year')
