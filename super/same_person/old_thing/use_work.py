5580467c683a7c3366f399d824b83b726d13fb95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:35:43 -0500

    "mod: want super/time/get_day_of_first_number/little_time_or_eye.py, super/same_person/old_thing/use_work.py or super/week/life/find_place_above_new_day.py under own part"

---
+++ super/same_person/old_thing/use_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_fact_on_own_case(str_arg):
+    long_year_or_early_number(str_arg)
+    print('young_man')
+
+def long_year_or_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_fact_on_own_case('year_or_old_work')
