f96c709a6988eed279693d8426664d0c19ffb3f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 13:27:30 -0500

    "add: try super/little_part.py and super/way.py into little point"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    day(str_arg)
+    print('work_bad_person')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('fact')
