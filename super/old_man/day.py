4b92d48c9ae3dc1c8b5f15c34c6b9ac7cdfc40b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-10 11:19:25 -0500

    "add: say super/old_man/day.py and super/try_public_place_on_child/think_hand/young_point/day/number/child.py on few problem"

---
+++ super/old_man/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_time(str_arg)
+    print('work_or_woman')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('right_company')
