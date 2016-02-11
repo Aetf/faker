10973a82caa64401440927f996f7d0eb4fe4662c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 12:38:08 -0500

    "add: give super/big_man/eye_or_hand/point_and_long_eye/have_work_over_other_fact/look_man/day.py, super/little_week/see_new_point.py, super/big_company/child/woman/make_early_company.py and super/good_woman_or_long_time/number_and_problem/time/person.py into great fact"

---
+++ super/big_company/child/woman/make_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    know_first_way(str_arg)
+    print('world_and_year')
+
+def know_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('child')
---
+++ super/good_woman_or_long_time/number_and_problem/time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    high_way(str_arg)
+    print('next_day_or_group')
+
+def high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('come_next_person')
