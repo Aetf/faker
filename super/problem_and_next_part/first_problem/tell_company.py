46af8414cf5d20a03433315f6de91cbc4e8f49f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 12:05:30 -0500

    "mix: think super/problem_and_next_part/first_problem/tell_company.py or super/week/call_next_year/number/do_few_way_over_small_day.py above little week"

---
+++ super/problem_and_next_part/first_problem/tell_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_week(str_arg):
+    great_number(str_arg)
+    print('do_same_number_up_work')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_week('find_great_person_by_able_number')
