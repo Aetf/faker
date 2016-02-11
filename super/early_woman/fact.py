d2886dc5c102d919b63dc1d35da01bcdd11bcbff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 12:45:32 -0500

    "mv: look super/early_woman/fact.py up big point"

---
+++ super/early_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_and_eye(str_arg):
+    early_case(str_arg)
+    print('look_eye')
+
+def early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_and_eye('woman_and_high_world')
