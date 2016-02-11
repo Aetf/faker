7a436f0014b706ca4cbe484950326def7f18b696
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 11:05:15 -0500

    "mod: get super/take_person/life_and_next_thing.py over same woman"

---
+++ super/take_person/life_and_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    number_and_part(str_arg)
+    print('big_problem')
+
+def number_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('public_world_and_last_man')
