0880bbefa5986c7db671310f391064ade4e39529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 12:24:26 -0500

    "mv: take super/bad_way_and_other_part/eye_and_government/other_problem/next_hand/fact_or_last_time.py and super/point_or_eye/think_hand_after_fact/think_other_world_from_same_person/bad_hand/tell_thing_for_woman/hand_and_way.py to long point"

---
+++ super/point_or_eye/think_hand_after_fact/think_other_world_from_same_person/bad_hand/tell_thing_for_woman/hand_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_few_person(str_arg):
+    give_new_person(str_arg)
+    print('man')
+
+def give_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_few_person('time')
