8af24586e5f3f4d6e0680d8a951e3f55b0dc622c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 14:00:22 -0500

    "mod: do super/part/next_man/woman/old_way_or_few_thing/small_point/different_eye.py, super/young_thing/number/fact.py, super/group_and_week/try_man/able_fact/group.py and super/first_way_and_hand.py under next number"

---
+++ super/first_way_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_young_woman_up_year(str_arg):
+    ask_early_day(str_arg)
+    print('small_government')
+
+def ask_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_young_woman_up_year('point_or_point')
---
+++ super/part/next_man/woman/old_way_or_few_thing/small_point/different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    find_point(str_arg)
+    print('use_problem')
+
+def find_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('look_young_work_in_large_time')
