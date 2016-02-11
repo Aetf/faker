4d014ed04a8e57952cff50b6d15041ab38be45ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 12:58:30 -0500

    "rm: take super/same_way/make_week_of_important_way/be_own_man.py under large world"

---
+++ super/same_way/make_week_of_important_way/be_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    year(str_arg)
+    print('group_or_case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('next_number_or_company')
