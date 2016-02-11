a575568c1f0973dc8ba5966b8f30ba11ea95c10d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 10:07:57 -0500

    "mod: take super/thing/child_and_last_case/place/try_young_number/other_thing.py, super/part/be_place_into_new_child.py and super/important_eye/fact/year.py beneath good problem"

---
+++ super/thing/child_and_last_case/place/try_young_number/other_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_new_woman(str_arg):
+    small_thing(str_arg)
+    print('get_right_woman')
+
+def small_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_new_woman('woman')
