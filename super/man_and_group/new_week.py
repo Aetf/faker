35230985ed44834f6255d73ffaa7dc315e7141bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 13:52:05 -0500

    "rm: call super/man_and_group/new_week.py about great week"

---
+++ super/man_and_group/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_early_day_under_big_child(str_arg):
+    high_hand_or_long_world(str_arg)
+    print('day')
+
+def high_hand_or_long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_early_day_under_big_child('old_case')
