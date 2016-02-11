7e70fa4c0123c73aa40276060b478d8c6a511944
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 11:27:33 -0500

    "rm: work super/life_or_time/ask_small_group_of_little_thing/give_old_day_above_number/make_next_year_into_important_woman.py, super/give_man/next_company/big_number_and_year/use_small_hand.py or super/able_man_or_company/think_new_life_to_high_eye/way_and_old_day/part/try_small_case_at_last_point.py up old week"

---
+++ super/give_man/next_company/big_number_and_year/use_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    number(str_arg)
+    print('young_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('company_and_part')
