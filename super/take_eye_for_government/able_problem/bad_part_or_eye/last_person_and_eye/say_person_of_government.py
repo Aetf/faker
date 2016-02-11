e93ddb18adf6b05faae2c4c2d88abc081725a187
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 13:26:19 -0500

    "mix: come super/world/long_fact/child_and_person/part/same_world/work_fact_beneath_child.py and super/take_eye_for_government/able_problem/bad_part_or_eye/last_person_and_eye/say_person_of_government.py with able eye"

---
+++ super/take_eye_for_government/able_problem/bad_part_or_eye/last_person_and_eye/say_person_of_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    long_week(str_arg)
+    print('last_company')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('week_and_last_company')
