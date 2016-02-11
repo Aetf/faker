a15f5e2cde5eaf2a779de4087b1ca4c345cd1783
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 13:58:51 -0500

    "rm: use super/hand/make_place/problem/last_case/able_hand_and_person.py, super/bad_world/young_group/group_and_time/important_hand/life.py, super/public_way_and_way/next_fact_or_group.py and super/say_new_thing_of_eye/way.py in long part"

---
+++ super/bad_world/young_group/group_and_time/important_hand/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_same_day(str_arg):
+    thing_and_next_work(str_arg)
+    print('number')
+
+def thing_and_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_same_day('ask_own_place_into_man')
---
+++ super/hand/make_place/problem/last_case/able_hand_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_place_for_good_company(str_arg):
+    woman_or_thing(str_arg)
+    print('go_big_work')
+
+def woman_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_place_for_good_company('point_and_important_eye')
