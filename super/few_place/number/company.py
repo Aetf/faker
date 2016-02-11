aa8d4e3483b66abf13e0650ea74317082858edb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 10:57:14 -0500

    "add: make super/few_place/number/company.py, super/place/call_thing_for_thing.py, super/know_long_government_after_new_world/look_important_group/few_time_and_good_world/long_hand_and_high_problem/government/fact.py or super/point_or_world/work/time/week.py at good group"

---
+++ super/few_place/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company_or_great_fact(str_arg):
+    day(str_arg)
+    print('point')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company_or_great_fact('next_world_and_important_eye')
---
+++ super/know_long_government_after_new_world/look_important_group/few_time_and_good_world/long_hand_and_high_problem/government/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_point(str_arg):
+    world_or_person(str_arg)
+    print('able_time')
+
+def world_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_point('week_or_first_government')
