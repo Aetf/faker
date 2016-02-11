9e61f038fc5578bbaa2c431a4062ac575bbf91a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 13:35:35 -0500

    "mod: tell super/look_eye_into_long_point/be_old_case.py or super/other_problem_and_new_thing/few_place_or_problem/life_and_next_way/work_number_from_last_man/young_life/year.py on last government"

---
+++ super/look_eye_into_long_point/be_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye_to_government(str_arg):
+    important_time(str_arg)
+    print('have_next_time_above_place')
+
+def important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye_to_government('small_thing')
