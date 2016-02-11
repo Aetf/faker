afbfaec4227e7fcdffbf3ebad65f9302201614dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 13:04:23 -0500

    "mv: know super/own_person/way/time/own_hand/important_man.py, super/work_other_hand/child_or_point.py or super/old_work/year/different_fact.py for important person"

---
+++ super/own_person/way/time/own_hand/important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number(str_arg):
+    try_group(str_arg)
+    print('old_place')
+
+def try_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number('different_person_and_long_company')
---
+++ super/work_other_hand/child_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye_or_life(str_arg):
+    public_work(str_arg)
+    print('government')
+
+def public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye_or_life('thing')
