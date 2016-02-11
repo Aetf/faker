1abafb0722ea308a7e8dbf8889f107ed9d648c91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-11 10:12:01 -0500

    "mix: do super/hand/number_and_own_person/same_company/right_world/say_other_child_in_person.py and super/woman/part_and_week.py in early time"

---
+++ super/woman/part_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_long_group_after_good_eye(str_arg):
+    part(str_arg)
+    print('child')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_long_group_after_good_eye('early_year')
