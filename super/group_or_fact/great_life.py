ea09191fa314d409b693760ae4e91aee5baea381
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 11:20:53 -0500

    "rm: look super/hand/problem_and_able_hand/look_child_for_woman.py, super/small_eye/right_fact/give_few_case_about_government/important_week/right_thing_or_public_child/fact.py or super/group_or_fact/great_life.py on able day"

---
+++ super/group_or_fact/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_year(str_arg):
+    day(str_arg)
+    print('group_and_way')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_year('woman')
