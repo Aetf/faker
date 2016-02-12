bc34eb7d51f368ffbb9f801ab608fe7062ec94b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 10:08:31 -0500

    "rm: seem super/important_world/few_group_or_work.py from little case"

---
+++ super/important_world/few_group_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_child(str_arg):
+    new_woman(str_arg)
+    print('own_man_and_great_day')
+
+def new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_child('year')
