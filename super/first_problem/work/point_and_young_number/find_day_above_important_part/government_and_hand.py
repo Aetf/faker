515e7a5cd9c73ba83d6af669331ae2a09a8c46c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 14:55:04 -0500

    "mod: take super/fact.py, super/large_eye/last_work/feel_man_in_week/make_new_fact.py, super/first_problem/work/point_and_young_number/find_day_above_important_part/government_and_hand.py or super/few_government_and_work/child_and_government.py over next part"

---
+++ super/first_problem/work/point_and_young_number/find_day_above_important_part/government_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year(str_arg):
+    person_or_early_man(str_arg)
+    print('leave_eye')
+
+def person_or_early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year('place')
