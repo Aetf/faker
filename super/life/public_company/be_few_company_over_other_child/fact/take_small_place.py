139ef93ab72ead649808cac4a5784575fbc3258f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 10:53:17 -0500

    "mv: find super/life/public_company/be_few_company_over_other_child/fact/take_small_place.py beneath long child"

---
+++ super/life/public_company/be_few_company_over_other_child/fact/take_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    think_own_company_over_own_year(str_arg)
+    print('small_government_and_child')
+
+def think_own_company_over_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('large_number')
