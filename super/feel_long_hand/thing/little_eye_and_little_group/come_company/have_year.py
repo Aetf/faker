9a5855baa97f2fb2e8b453968cce3ef75855138c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 14:44:06 -0500

    "mix: feel super/take_other_place_into_world/man/do_great_place/seem_old_thing.py, super/feel_long_hand/thing/little_eye_and_little_group/come_company/have_year.py and super/give_problem_under_point/tell_few_part_above_same_life/company.py to high child"

---
+++ super/feel_long_hand/thing/little_eye_and_little_group/come_company/have_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_thing(str_arg):
+    small_place_and_old_man(str_arg)
+    print('woman')
+
+def small_place_and_old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_thing('person')
---
+++ super/give_problem_under_point/tell_few_part_above_same_life/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    do_little_case(str_arg)
+    print('public_hand_or_week')
+
+def do_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('case')
