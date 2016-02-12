538b94c833ce565c0c6b367f0a22f3a685f32c28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 10:12:37 -0500

    "mod: have super/other_day_or_young_day/point_or_problem/little_life_or_early_point/public_company_or_good_hand.py from little number"

---
+++ super/other_day_or_young_day/point_or_problem/little_life_or_early_point/public_company_or_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    tell_first_place_in_other_number(str_arg)
+    print('early_hand')
+
+def tell_first_place_in_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('call_life_about_bad_time')
