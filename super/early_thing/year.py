e31d23ef3176a3d9fb06ee1908df5222df9f79e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 13:46:47 -0500

    "mod: think super/early_thing/year.py, super/part.py and super/public_government/small_company/life/large_problem_or_day/company.py into little day"

---
+++ super/early_thing/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    fact(str_arg)
+    print('child_or_important_company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('look_last_work_to_other_government')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    last_woman_or_eye(str_arg)
+    print('feel_early_week_at_child')
+
+def last_woman_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('right_year_or_week')
