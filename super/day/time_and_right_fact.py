1f1d68ac3c9cbc63967b1354cd4dda14f4f5b55d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 11:41:06 -0500

    "mod: try super/use_man/week/make_day_of_place/young_hand/large_hand.py, super/day/time_and_right_fact.py and super/number/right_company/government.py at large thing"

---
+++ super/day/time_and_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    year_or_child(str_arg)
+    print('ask_man')
+
+def year_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('way_and_large_person')
---
+++ super/use_man/week/make_day_of_place/young_hand/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye(str_arg):
+    hand(str_arg)
+    print('woman')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye('problem_or_new_fact')
