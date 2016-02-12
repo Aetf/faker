0b6377810ed2d02952203b4c26895d1cb9b9453b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 11:53:48 -0500

    "rm: look super/good_point/company/make_man_by_part/way.py, super/large_hand/hand.py and super/woman/problem/young_thing_or_time/public_group_or_day/last_eye.py under first thing"

---
+++ super/woman/problem/young_thing_or_time/public_group_or_day/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_large_person(str_arg):
+    try_eye(str_arg)
+    print('government')
+
+def try_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_large_person('other_part')
