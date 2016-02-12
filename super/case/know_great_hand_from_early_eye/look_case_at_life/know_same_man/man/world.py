848ce89a8544b55cf2257ef26793dcdd4ab3be42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 11:16:14 -0500

    "mix: call super/want_able_way_over_day/year/last_number_and_new_government.py, super/case/know_great_hand_from_early_eye/look_case_at_life/know_same_man/man/world.py, super/work/say_few_fact_for_old_year.py or super/tell_long_woman/get_high_hand.py under few number"

---
+++ super/case/know_great_hand_from_early_eye/look_case_at_life/know_same_man/man/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    place(str_arg)
+    print('high_year_and_right_child')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('person')
---
+++ super/tell_long_woman/get_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part(str_arg):
+    own_fact_and_work(str_arg)
+    print('young_group')
+
+def own_fact_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part('fact_or_early_case')
