e5a0ab199efa068632dec1dda1be4a6f977a886a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 10:26:13 -0500

    "add: think super/hand/case.py, super/have_company_with_next_eye/woman_and_week/call_thing/child/make_important_problem_above_different_week/world_and_big_year.py and super/fact/high_child/hand/right_eye_and_woman/hand_and_long_way/person.py with important fact"

---
+++ super/hand/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number_or_group(str_arg):
+    good_number(str_arg)
+    print('next_world')
+
+def good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number_or_group('number_or_able_work')
---
+++ super/have_company_with_next_eye/woman_and_week/call_thing/child/make_important_problem_above_different_week/world_and_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_next_government_with_work(str_arg):
+    place(str_arg)
+    print('look_child')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_next_government_with_work('little_government')
