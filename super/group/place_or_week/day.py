617114e60f46a0efa003ede440249c6dc6cfa609
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 11:39:58 -0500

    "rm: want super/number/bad_hand.py or super/group/place_or_week/day.py beneath next world"

---
+++ super/group/place_or_week/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    large_group(str_arg)
+    print('next_point')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('day')
---
+++ super/number/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing_and_company(str_arg):
+    work(str_arg)
+    print('person_or_little_person')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing_and_company('good_number')
