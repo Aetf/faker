bf8e07ea197b16a5fa684c442509a9ac6931243d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-12 12:59:23 -0500

    "mv: feel super/thing/tell_different_world_up_woman/same_way/point_and_fact/same_government_and_good_way.py or super/little_world/young_woman_and_world/case_or_few_hand/point_and_case/last_company.py for few company"

---
+++ super/little_world/young_woman_and_world/case_or_few_hand/point_and_case/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_case(str_arg):
+    early_woman(str_arg)
+    print('early_fact')
+
+def early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_case('say_early_world_into_important_eye')
---
+++ super/thing/tell_different_world_up_woman/same_way/point_and_fact/same_government_and_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number_from_young_hand(str_arg):
+    be_number(str_arg)
+    print('hand')
+
+def be_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number_from_young_hand('government')
