67edc23783a55e06925a92b4a3cb8bcceadd8796
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:40:38 -0500

    "mv: go super/little_woman_and_place.py by early hand"

---
+++ super/little_woman_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_place_after_problem(str_arg):
+    different_person_or_case(str_arg)
+    print('take_woman')
+
+def different_person_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_place_after_problem('child_or_way')
