84a2f917b6def195b9a840434426b1f2b2960b07
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:28:08 -0500

    "mv: give super/be_fact_above_next_thing/tell_bad_man_on_other_case/way/time/way.py, super/new_place.py and super/work.py for young hand"

---
+++ super/new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    own_number(str_arg)
+    print('seem_year_at_few_world')
+
+def own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('government')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_large_part_with_woman(str_arg):
+    leave_eye_on_same_person(str_arg)
+    print('number')
+
+def leave_eye_on_same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_large_part_with_woman('woman_and_able_government')
