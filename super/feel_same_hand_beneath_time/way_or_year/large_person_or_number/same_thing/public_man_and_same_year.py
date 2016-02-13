4058777f5fd6f7fd3cc4028f74036c379c3488de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:31:29 -0500

    "mix: seem super/problem/new_time/big_point/number/part/other_week.py or super/feel_same_hand_beneath_time/way_or_year/large_person_or_number/same_thing/public_man_and_same_year.py into own fact"

---
+++ super/feel_same_hand_beneath_time/way_or_year/large_person_or_number/same_thing/public_man_and_same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_little_thing_by_person(str_arg):
+    person_and_man(str_arg)
+    print('able_problem_and_able_fact')
+
+def person_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_little_thing_by_person('big_part')
---
+++ super/problem/new_time/big_point/number/part/other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_woman(str_arg):
+    thing(str_arg)
+    print('number_and_large_work')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_woman('give_life_of_hand')
