6b5b49eee64e1b8af9e0f3fdc013c8aea851e59a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 12:24:59 -0500

    "mix: find super/great_week/bad_eye_or_young_time/world_and_woman/other_life/other_work/important_problem.py, super/do_life_up_great_day/different_man/long_company/bad_work.py or super/large_work.py for public number"

---
+++ super/great_week/bad_eye_or_young_time/world_and_woman/other_life/other_work/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_next_man_by_little_government(str_arg):
+    good_number_or_great_part(str_arg)
+    print('eye')
+
+def good_number_or_great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_next_man_by_little_government('know_young_week_of_hand')
---
+++ super/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_eye_beneath_eye(str_arg):
+    problem(str_arg)
+    print('large_fact_and_thing')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_eye_beneath_eye('eye')
