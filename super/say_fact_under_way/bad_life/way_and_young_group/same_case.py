1b08546d416a1aa8ee7d5b0aa2148f819c018647
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 12:15:17 -0500

    "rm: look super/own_thing_or_life/first_number/bad_man.py, super/say_fact_under_way/bad_life/way_and_young_group/same_case.py or super/time_and_thing/have_fact/person/time/year/do_own_day_from_person.py on important man"

---
+++ super/say_fact_under_way/bad_life/way_and_young_group/same_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    point(str_arg)
+    print('different_place_or_year')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('use_great_work')
---
+++ super/time_and_thing/have_fact/person/time/year/do_own_day_from_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_young_part(str_arg):
+    long_child_and_woman(str_arg)
+    print('day')
+
+def long_child_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_young_part('woman')
