03a74da3cae19984c788b017d76220b15c6751b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:12:18 -0500

    "mod: say super/part/right_government/day/year/new_child/right_place.py of old company"

---
+++ super/part/right_government/day/year/new_child/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_long_part(str_arg):
+    way(str_arg)
+    print('child')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_long_part('own_world')
