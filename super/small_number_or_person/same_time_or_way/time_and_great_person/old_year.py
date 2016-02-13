63b2a096c598c1b2c986565461afd2974b8b6d61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 12:41:37 -0500

    "mix: come super/small_number_or_person/same_time_or_way/time_and_great_person/old_year.py about few day"

---
+++ super/small_number_or_person/same_time_or_way/time_and_great_person/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    great_woman(str_arg)
+    print('say_work_to_time')
+
+def great_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('way')
