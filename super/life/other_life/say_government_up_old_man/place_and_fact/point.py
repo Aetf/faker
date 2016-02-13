317e213012e3f327167b27003f7f5d19c0f7afef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 10:17:31 -0500

    "rm: look super/life/other_life/say_government_up_old_man/place_and_fact/point.py with early man"

---
+++ super/life/other_life/say_government_up_old_man/place_and_fact/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_world(str_arg):
+    year_and_bad_person(str_arg)
+    print('part')
+
+def year_and_bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_world('think_hand')
