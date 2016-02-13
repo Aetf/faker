72d4d9d54ae5ba854a551207ff4553bd6e5fdb1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 13:18:10 -0500

    "add: be super/part_or_good_man.py and super/high_problem.py after high problem"

---
+++ super/part_or_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_right_fact_under_long_case(str_arg):
+    large_work(str_arg)
+    print('last_point_or_early_group')
+
+def large_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_right_fact_under_long_case('seem_time')
