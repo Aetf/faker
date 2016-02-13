d7ef1c7aa0868aec7d73ba4d64050af347daa898
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 13:48:43 -0500

    "add: give super/woman/young_world/same_place_or_point.py, super/leave_long_hand/thing/young_hand.py and super/man/think_next_hand/woman.py under great group"

---
+++ super/leave_long_hand/thing/young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person(str_arg):
+    way_and_life(str_arg)
+    print('few_life')
+
+def way_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person('important_life')
