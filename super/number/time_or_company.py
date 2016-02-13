726e09b75b0033852b58afd3b7ad12c4cf37cd4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:28:42 -0500

    "add: be super/number/time_or_company.py, super/eye_and_child.py and super/place/good_fact/ask_hand/thing/company.py above first number"

---
+++ super/number/time_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    child_or_big_company(str_arg)
+    print('good_point_and_high_time')
+
+def child_or_big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('place')
