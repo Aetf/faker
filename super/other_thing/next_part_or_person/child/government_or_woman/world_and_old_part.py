866eb6608f0d0c6985fee5ca7b48071fcd228b21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 12:32:49 -0500

    "mix: call super/able_company/bad_work/make_able_work_at_work/use_point/point.py, super/other_thing/next_part_or_person/child/government_or_woman/world_and_old_part.py, super/child/same_man_and_same_government.py and super/last_case/person_and_long_woman/week_or_able_group/day/group/last_child_and_first_week.py on public person"

---
+++ super/other_thing/next_part_or_person/child/government_or_woman/world_and_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_hand(str_arg):
+    year(str_arg)
+    print('small_life_or_able_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_hand('tell_part')
