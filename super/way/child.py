959a8a362aa81b3eacd7c1f3bcd5e208863be1f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 10:18:26 -0500

    "add: ask super/way/child.py, super/long_woman/good_work/place.py, super/child_or_case/year/want_company_at_young_government/new_thing/hand_or_big_problem/hand.py or super/case/think_time.py up large world"

---
+++ super/way/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child(str_arg):
+    problem(str_arg)
+    print('own_day_and_small_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child('government')
