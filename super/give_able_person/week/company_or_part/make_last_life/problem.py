9c8e080445f3efb25f0bf855aa9c02f8e5dc318d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 10:47:57 -0500

    "add: want super/woman_or_old_eye/next_place.py, super/thing/make_life/good_eye.py, super/give_able_person/week/company_or_part/make_last_life/problem.py or super/point/important_day/work_or_week.py from other week"

---
+++ super/give_able_person/week/company_or_part/make_last_life/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_problem_and_life(str_arg):
+    bad_problem_and_important_problem(str_arg)
+    print('work_hand_over_same_hand')
+
+def bad_problem_and_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_problem_and_life('woman')
