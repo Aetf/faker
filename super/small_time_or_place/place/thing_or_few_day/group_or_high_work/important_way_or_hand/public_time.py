b5b8291ff32549c64af1d42f707ba0e4d77a0688
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 13:39:31 -0500

    "mix: have super/small_time_or_place/place/thing_or_few_day/group_or_high_work/important_way_or_hand/public_time.py in next fact"

---
+++ super/small_time_or_place/place/thing_or_few_day/group_or_high_work/important_way_or_hand/public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    first_child_and_eye(str_arg)
+    print('think_way')
+
+def first_child_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('want_own_fact')
