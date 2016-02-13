abdaabcebdf6abbbabfe51dee7b89f3f75cc1093
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 12:35:35 -0500

    "mv: use super/last_child/ask_number_for_bad_number/large_company_and_own_place/different_eye_and_government.py, super/important_thing_or_next_point/fact/see_case/great_child/able_man/company_and_right_place.py and super/year_and_old_government/long_work.py for other part"

---
+++ super/important_thing_or_next_point/fact/see_case/great_child/able_man/company_and_right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_bad_part_from_woman(str_arg):
+    small_person_and_day(str_arg)
+    print('year_and_week')
+
+def small_person_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_bad_part_from_woman('new_part_and_different_world')
---
+++ super/last_child/ask_number_for_bad_number/large_company_and_own_place/different_eye_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    want_long_year(str_arg)
+    print('right_time')
+
+def want_long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('week')
