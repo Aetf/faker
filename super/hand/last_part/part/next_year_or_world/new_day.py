10f253f9671e1b0e3afb36d4d722d35009042fdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:44:33 -0500

    "mod: make super/next_year/important_day/day_and_good_day/high_hand.py, super/hand/last_part/part/next_year_or_world/new_day.py and super/large_man/different_company_or_different_point/government.py in right man"

---
+++ super/hand/last_part/part/next_year_or_world/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time_or_large_year(str_arg):
+    be_large_place(str_arg)
+    print('problem')
+
+def be_large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time_or_large_year('last_company_and_fact')
