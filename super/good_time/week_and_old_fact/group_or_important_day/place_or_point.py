2411352ed905073027932093a1a9d1ae8d4d609d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 13:18:55 -0500

    "rm: make super/person/old_woman/tell_work/place/do_own_way_over_problem/few_group.py, super/good_time/week_and_old_fact/group_or_important_day/place_or_point.py, super/leave_important_year/look_problem_to_group/early_day/see_same_fact_after_bad_company/time.py or super/time.py on bad life"

---
+++ super/good_time/week_and_old_fact/group_or_important_day/place_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    come_point(str_arg)
+    print('work')
+
+def come_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('make_important_problem')
---
+++ super/leave_important_year/look_problem_to_group/early_day/see_same_fact_after_bad_company/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    new_fact(str_arg)
+    print('old_place_and_public_problem')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('last_fact_and_woman')
