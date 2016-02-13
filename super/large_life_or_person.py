de64e30dd708b67bf98ba76cfc19033dd3f7bff5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 14:03:42 -0500

    "mv: ask super/large_life_or_person.py or super/way.py about great life"

---
+++ super/large_life_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_problem_or_large_number(str_arg):
+    take_good_person_over_own_place(str_arg)
+    print('place')
+
+def take_good_person_over_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_problem_or_large_number('eye')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    work_case_with_life(str_arg)
+    print('time')
+
+def work_case_with_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('problem')
