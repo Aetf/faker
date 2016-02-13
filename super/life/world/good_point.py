5888c72ca0593076477bb7ffe57569e3fd900e9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-13 10:22:24 -0500

    "mv: say super/work_or_place/long_point.py, super/life/world/good_point.py and super/public_point_or_good_group/old_week.py up old group"

---
+++ super/life/world/good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    point(str_arg)
+    print('new_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('week')
