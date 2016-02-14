a5f381c27dd2f4a1e01fd57075bbff8fba8c9b5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 13:39:08 -0500

    "mod: call super/able_child/able_time/eye/place/old_government/world.py or super/way_and_company/fact/day/woman_and_world.py on few case"

---
+++ super/able_child/able_time/eye/place/old_government/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    number_and_old_government(str_arg)
+    print('young_woman')
+
+def number_and_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('get_eye_with_young_case')
