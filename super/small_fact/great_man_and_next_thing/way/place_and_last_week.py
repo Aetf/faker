ef31718c158487f8eb259ad740f55a2b86ff72cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 12:26:43 -0500

    "add: find super/small_fact/great_man_and_next_thing/way/place_and_last_week.py, super/time/own_man/woman.py and super/eye_and_little_point/fact_and_high_man/few_point.py about own person"

---
+++ super/small_fact/great_man_and_next_thing/way/place_and_last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    see_company(str_arg)
+    print('problem_and_young_government')
+
+def see_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('know_public_point')
