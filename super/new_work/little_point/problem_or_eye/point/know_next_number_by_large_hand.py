397c2035071077f086209e5ce39f808ab2603bad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 14:53:52 -0500

    "mix: do super/early_hand.py, super/make_little_problem.py, super/new_work/little_point/problem_or_eye/point/know_next_number_by_large_hand.py and super/work_and_way/point.py at able person"

---
+++ super/new_work/little_point/problem_or_eye/point/know_next_number_by_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_year(str_arg):
+    go_point(str_arg)
+    print('right_part_and_own_place')
+
+def go_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_year('child')
---
+++ super/work_and_way/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work_or_year(str_arg):
+    long_point(str_arg)
+    print('important_life_and_young_place')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work_or_year('company')
