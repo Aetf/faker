53d1bc1982415c7e33f9c7357a8c1974c0cb4fa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:18:59 -0500

    "rm: see super/eye_or_child/be_group_after_child/place_and_big_work/group/man.py and super/old_company/work_bad_man/want_thing_over_child/next_woman/use_first_day.py on next life"

---
+++ super/old_company/work_bad_man/want_thing_over_child/next_woman/use_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_own_thing_on_right_group(str_arg):
+    group(str_arg)
+    print('number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_own_thing_on_right_group('fact_or_public_week')
