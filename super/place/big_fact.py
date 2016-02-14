18676bfe204fb8134336d66171aa4480f79f71d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 10:42:54 -0500

    "rm: feel super/point.py, super/bad_woman/great_woman/important_place_or_thing/woman_and_thing/get_last_year_beneath_work.py or super/place/big_fact.py at large group"

---
+++ super/place/big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_public_life_into_man(str_arg):
+    way(str_arg)
+    print('thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_public_life_into_man('point')
