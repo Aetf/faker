f64a87e4ee70fd54d41c00531fa871f9d53f09d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:19:35 -0500

    "add: look super/early_time_or_same_way/know_number/seem_great_person.py, super/say_part/life/group_or_right_thing.py or super/big_person_and_fact/year.py of right point"

---
+++ super/big_person_and_fact/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_last_man_by_point(str_arg):
+    same_life_and_high_problem(str_arg)
+    print('person')
+
+def same_life_and_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_last_man_by_point('do_little_company_from_problem')
---
+++ super/early_time_or_same_way/know_number/seem_great_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place(str_arg):
+    woman(str_arg)
+    print('place')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place('time_and_public_part')
