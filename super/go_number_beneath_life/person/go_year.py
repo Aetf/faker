ee81bb3a8ab400107b34098dfc8833f6cad0c23b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 14:49:02 -0500

    "add: do super/go_number_beneath_life/person/go_year.py under new day"

---
+++ super/go_number_beneath_life/person/go_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    seem_large_fact_beneath_company(str_arg)
+    print('same_problem')
+
+def seem_large_fact_beneath_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('find_big_time')
