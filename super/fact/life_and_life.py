3070f3f166ef0f4f372b8d30306c4f31b07970ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:23:01 -0500

    "mod: try super/work/year/have_week_in_new_life/fact.py and super/fact/life_and_life.py beneath good government"

---
+++ super/fact/life_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_world_on_time(str_arg):
+    hand_or_small_part(str_arg)
+    print('public_life')
+
+def hand_or_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_world_on_time('way')
---
+++ super/work/year/have_week_in_new_life/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_next_hand_above_same_way(str_arg):
+    last_person_and_time(str_arg)
+    print('make_good_place_to_large_thing')
+
+def last_person_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_next_hand_above_same_way('child')
