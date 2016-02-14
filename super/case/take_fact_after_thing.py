34debbc9e13dd237f184500e7c6c387308cb7e38
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 10:46:58 -0500

    "mv: work super/case/take_fact_after_thing.py and super/large_child_or_time/point/last_world/want_few_part.py up old week"

---
+++ super/case/take_fact_after_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_eye(str_arg):
+    public_place(str_arg)
+    print('small_way')
+
+def public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_eye('tell_case')
