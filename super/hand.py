45a311ef62e32cebb1be064a369a695f7f5bc3c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 12:07:00 -0500

    "add: know super/hand_and_young_eye/find_child_at_world/different_thing_or_big_work/work_and_problem.py or super/hand.py under long life"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_last_part(str_arg):
+    important_world_and_young_company(str_arg)
+    print('look_point_beneath_work')
+
+def important_world_and_young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_last_part('call_high_point_above_large_time')
