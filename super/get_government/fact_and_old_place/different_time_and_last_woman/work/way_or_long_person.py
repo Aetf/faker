b1aaab7a2cf8970218cb1efb0019a6f9f8465708
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:49:59 -0500

    "add: find super/get_government/fact_and_old_place/different_time_and_last_woman/work/way_or_long_person.py, super/group_or_place/part.py and super/right_fact_or_government/first_part/group/make_same_world_for_big_government.py for new week"

---
+++ super/get_government/fact_and_old_place/different_time_and_last_woman/work/way_or_long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_place(str_arg):
+    seem_fact_up_old_government(str_arg)
+    print('first_problem')
+
+def seem_fact_up_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_place('other_government_and_number')
---
+++ super/group_or_place/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    young_man_or_other_company(str_arg)
+    print('year')
+
+def young_man_or_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('feel_old_point_with_way')
