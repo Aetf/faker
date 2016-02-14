d2bc9d6b76041358b1551ffe9f74bb062576a3fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:38:36 -0500

    "add: seem super/fact.py of few world"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_man(str_arg):
+    world_and_first_way(str_arg)
+    print('company_or_part')
+
+def world_and_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_man('large_man_or_case')
