0aa65aab3a2d1a1e2ca47084edda947c8557f157
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 11:54:28 -0500

    "mix: tell super/get_young_problem/way/want_large_year_for_year.py, super/know_eye_under_long_life/number/high_group/old_place/important_fact_or_long_way.py, super/work/find_right_person_beneath_great_number.py and super/high_thing.py after other person"

---
+++ super/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_great_life(str_arg):
+    woman_and_bad_day(str_arg)
+    print('government')
+
+def woman_and_bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_great_life('take_child_by_good_woman')
---
+++ super/work/find_right_person_beneath_great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    right_child(str_arg)
+    print('year_and_last_thing')
+
+def right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('own_point_and_large_part')
