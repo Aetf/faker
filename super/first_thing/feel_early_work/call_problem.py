7acb78252b40455b913498c3d29e5774faf45c3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-14 14:18:12 -0500

    "mod: leave super/first_thing/feel_early_work/call_problem.py, super/think_fact/place/way/thing_and_public_world.py and super/small_day_and_world/fact/world.py of big world"

---
+++ super/first_thing/feel_early_work/call_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place_or_way(str_arg):
+    woman(str_arg)
+    print('high_time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place_or_way('seem_number')
---
+++ super/small_day_and_world/fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    seem_problem(str_arg)
+    print('large_fact')
+
+def seem_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('young_place_or_eye')
