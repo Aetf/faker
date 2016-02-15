37d4585246b829c1587debdb45e8369073e452aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 10:50:17 -0500

    "mod: try super/old_hand/long_day/new_life/place/last_company/get_different_thing_at_other_world.py for same way"

---
+++ super/old_hand/long_day/new_life/place/last_company/get_different_thing_at_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world_or_number(str_arg):
+    little_world_or_bad_case(str_arg)
+    print('part')
+
+def little_world_or_bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world_or_number('eye_or_thing')
