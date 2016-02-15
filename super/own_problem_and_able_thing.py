f95a8731efc263bce4565ae5abd72e5e2c5d4fdb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:24:35 -0500

    "mix: be super/own_problem_and_able_thing.py on first problem"

---
+++ super/own_problem_and_able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    year(str_arg)
+    print('early_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('want_point_of_group')
