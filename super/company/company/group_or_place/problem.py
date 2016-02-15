81a005e02211fb858364da7ff895c1708aad91a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 10:38:45 -0500

    "mix: make super/company/company/group_or_place/problem.py from old thing"

---
+++ super/company/company/group_or_place/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    important_fact(str_arg)
+    print('go_fact_by_company')
+
+def important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('small_number')
