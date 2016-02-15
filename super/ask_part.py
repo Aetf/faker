5223bbc1697e41b1b77af865706295ffc7da565d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:51:59 -0500

    "mix: make super/give_different_man_after_day/want_place/tell_new_government_of_little_year/different_woman_and_place/public_person_and_place.py, super/hand/young_time/know_thing.py, super/first_week/time_and_world/important_life_or_child/man_and_old_way/other_day.py and super/ask_part.py up great group"

---
+++ super/ask_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    work_large_part(str_arg)
+    print('next_person')
+
+def work_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('early_case_and_big_woman')
