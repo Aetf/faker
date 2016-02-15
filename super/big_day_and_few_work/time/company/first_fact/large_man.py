71d30595158a58042ad7206c3021d1cda3453072
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:46:11 -0500

    "add: get super/few_person/child/government/high_day.py, super/big_day_and_few_work/time/company/first_fact/large_man.py and super/person.py for long way"

---
+++ super/big_day_and_few_work/time/company/first_fact/large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way_and_part(str_arg):
+    work(str_arg)
+    print('man')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way_and_part('feel_other_thing')
---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    long_time(str_arg)
+    print('part')
+
+def long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('use_time')
