896d1b4fe985fa149977f56750f999e03fa94cc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 11:01:04 -0500

    "rm: come super/few_point/same_place_or_life.py, super/little_child/tell_way/long_thing/other_way.py or super/early_company/life_and_part.py of next part"

---
+++ super/early_company/life_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point(str_arg):
+    woman_and_first_work(str_arg)
+    print('good_person')
+
+def woman_and_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point('try_government')
