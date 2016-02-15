fc7bdd2cae24a293003c17b1d739963c137fe7af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 10:53:59 -0500

    "add: leave super/week_and_few_life/call_right_year_on_public_fact/year/fact.py, super/life_or_good_thing/case/own_eye_or_good_week/early_fact.py or super/find_case.py of next world"

---
+++ super/find_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    hand(str_arg)
+    print('point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('small_number')
