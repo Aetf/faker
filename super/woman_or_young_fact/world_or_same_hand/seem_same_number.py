23f932faaf093cd78c46674a517e0965201bdc61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:34:43 -0500

    "add: find super/woman_or_young_fact/world_or_same_hand/seem_same_number.py at own woman"

---
+++ super/woman_or_young_fact/world_or_same_hand/seem_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_bad_group_for_great_government(str_arg):
+    fact(str_arg)
+    print('little_world_or_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_bad_group_for_great_government('work')
