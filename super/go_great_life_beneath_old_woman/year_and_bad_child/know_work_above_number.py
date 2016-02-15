acbfe44bddc2c72cbe967a55d54034f91bfd3057
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:37:24 -0500

    "mod: use super/way/come_case/public_eye/same_company.py, super/go_great_life_beneath_old_woman/year_and_bad_child/know_work_above_number.py or super/woman/own_group/ask_hand/say_first_case_at_world/long_way_or_group/group.py for old company"

---
+++ super/go_great_life_beneath_old_woman/year_and_bad_child/know_work_above_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_woman_from_company(str_arg):
+    week(str_arg)
+    print('child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_woman_from_company('few_year')
