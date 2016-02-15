3ffa62b9b17be5faa76d1d9e1c44d98b2c36fd53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:04:39 -0500

    "mod: go super/able_government.py, super/right_eye_or_thing/new_woman/make_woman_for_thing/great_time/number/different_government_or_day.py or super/new_problem_or_person/part/year.py on able time"

---
+++ super/able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_young_work_in_great_way(str_arg):
+    big_week(str_arg)
+    print('long_eye')
+
+def big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_young_work_in_great_way('right_week')
