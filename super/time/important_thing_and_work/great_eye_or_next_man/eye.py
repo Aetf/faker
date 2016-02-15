675a85b48973a3934e11fb6f59a38292e954aedc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 11:34:20 -0500

    "mod: find super/time/important_thing_and_work/great_eye_or_next_man/eye.py of large time"

---
+++ super/time/important_thing_and_work/great_eye_or_next_man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_big_part(str_arg):
+    hand(str_arg)
+    print('large_fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_big_part('young_thing')
