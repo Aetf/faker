bdb5b681bcd4353d9d40656b3c50589bfe488683
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 14:37:18 -0500

    "mix: go super/child/work/way_and_life.py and super/first_day_and_government/work_large_day/great_day_and_company/little_year/see_way_at_small_eye/case.py to old child"

---
+++ super/child/work/way_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem_or_other_point(str_arg):
+    eye(str_arg)
+    print('old_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem_or_other_point('go_public_child')
---
+++ super/first_day_and_government/work_large_day/great_day_and_company/little_year/see_way_at_small_eye/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing_and_little_world(str_arg):
+    woman(str_arg)
+    print('make_old_work_up_other_week')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing_and_little_world('take_week_with_own_fact')
