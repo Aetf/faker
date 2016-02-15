02cc33538544617bcafecd7cc3adbec5432aaca2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 11:19:11 -0500

    "add: look super/new_way/person/ask_eye_with_group/new_fact_and_company.py, super/take_large_group/know_last_year/go_place_into_public_fact.py, super/next_point.py or super/try_woman/able_day/find_woman.py about other problem"

---
+++ super/take_large_group/know_last_year/go_place_into_public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    thing(str_arg)
+    print('government_and_same_number')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('want_case_about_right_time')
