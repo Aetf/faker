c21becd6705db8411bafaaf562200073e9c118cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 13:44:10 -0500

    "mix: call super/next_world_and_case/seem_problem_on_problem/own_life.py, super/find_world.py, super/ask_day_to_child/child/day/government_or_week/get_public_part/look_important_thing_up_child.py and super/part.py after important time"

---
+++ super/next_world_and_case/seem_problem_on_problem/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    group(str_arg)
+    print('person')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('thing_or_way')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    year_and_big_man(str_arg)
+    print('group_or_company')
+
+def year_and_big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('large_child')
