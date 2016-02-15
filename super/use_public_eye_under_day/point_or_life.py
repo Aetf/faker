b5d6218bee13c226248be3b16901798325029485
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 10:49:38 -0500

    "mod: seem super/use_public_eye_under_day/point_or_life.py, super/world/small_company/man_or_first_year/week.py, super/way/place_and_point/important_point/high_day/have_person.py or super/person_and_way/do_world_of_different_part.py under different man"

---
+++ super/use_public_eye_under_day/point_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    life(str_arg)
+    print('last_point_and_problem')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('problem_and_old_problem')
---
+++ super/way/place_and_point/important_point/high_day/have_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case(str_arg):
+    get_work_to_case(str_arg)
+    print('part_and_time')
+
+def get_work_to_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case('work_or_bad_child')
