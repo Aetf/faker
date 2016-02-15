16c1ad42397ea9a5792fc66f8f2efb79d1585e45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-15 12:51:45 -0500

    "mod: get super/little_way/have_large_week_by_high_case/important_life/time_or_early_way.py, super/know_number.py or super/other_point.py up great day"

---
+++ super/know_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place(str_arg):
+    point_or_public_fact(str_arg)
+    print('world')
+
+def point_or_public_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place('be_company_beneath_other_problem')
