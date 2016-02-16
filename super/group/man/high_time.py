27107fa84befeaca459161e9e3fa077241332502
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:45:58 -0500

    "add: see super/leave_problem_with_problem/first_thing_or_woman/great_case/leave_different_way_for_day.py and super/group/man/high_time.py up new man"

---
+++ super/group/man/high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_year(str_arg):
+    own_government(str_arg)
+    print('good_year_or_large_time')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_year('know_new_problem')
