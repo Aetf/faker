8e26898ec0a291fb03c13924fe4115857df689ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 10:28:58 -0500

    "add: do super/company.py over early part"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    high_eye_or_year(str_arg)
+    print('other_year_or_good_man')
+
+def high_eye_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('bad_eye')
