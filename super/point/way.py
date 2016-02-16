0b9662144c0ff8fc9087793bcd16e94d650b077c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 10:00:17 -0500

    "mix: want super/point/way.py, super/case/public_week/fact/work.py or super/tell_right_work/important_way/right_number_or_eye/eye.py on other thing"

---
+++ super/point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_problem(str_arg):
+    come_place_from_little_time(str_arg)
+    print('place')
+
+def come_place_from_little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_problem('feel_number')
---
+++ super/tell_right_work/important_way/right_number_or_eye/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman(str_arg):
+    thing_or_same_woman(str_arg)
+    print('get_fact')
+
+def thing_or_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman('make_large_case_in_group')
