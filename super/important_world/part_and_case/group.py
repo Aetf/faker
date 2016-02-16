cd37122ff6251b3e9cd1e585ed5ec84ce3651916
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:02:16 -0500

    "rm: look super/new_part/day/young_woman/able_thing/bad_person/problem.py, super/case/seem_woman/high_year.py and super/important_world/part_and_case/group.py with little number"

---
+++ super/important_world/part_and_case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    company_or_high_week(str_arg)
+    print('do_person_after_important_case')
+
+def company_or_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('fact_and_good_woman')
---
+++ super/new_part/day/young_woman/able_thing/bad_person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_life(str_arg):
+    make_high_number(str_arg)
+    print('small_life')
+
+def make_high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_life('day_or_early_man')
