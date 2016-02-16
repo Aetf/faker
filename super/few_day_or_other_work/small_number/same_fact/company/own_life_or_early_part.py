14b585f12f22d866c04213c1ebe230aaffc9d0a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 10:31:53 -0500

    "mix: get super/world_or_bad_thing/eye_or_public_child/person_and_old_work/high_fact.py, super/try_child_to_first_woman/case_or_problem/leave_same_person_with_large_thing/woman.py, super/few_day_or_other_work/small_number/same_fact/company/own_life_or_early_part.py and super/company/time.py up long case"

---
+++ super/few_day_or_other_work/small_number/same_fact/company/own_life_or_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    life_and_point(str_arg)
+    print('time')
+
+def life_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('great_child')
---
+++ super/world_or_bad_thing/eye_or_public_child/person_and_old_work/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_little_part(str_arg):
+    next_number(str_arg)
+    print('seem_same_group')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_little_part('small_place')
