23b6ac7653c20bad273b899b9072dc5ab7b6d5d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:31:35 -0500

    "add: find super/have_life_on_different_number/see_group/few_man/high_fact/big_fact/first_way.py and super/next_point_or_life/place_and_eye.py after few world"

---
+++ super/next_point_or_life/place_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    day_or_early_day(str_arg)
+    print('few_world_or_hand')
+
+def day_or_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('seem_young_case_for_new_government')
