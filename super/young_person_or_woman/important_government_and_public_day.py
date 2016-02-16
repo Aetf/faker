d330ecef09d4355159e9abb6f3d68edc365ade60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:18:13 -0500

    "mod: give super/young_person_or_woman/important_government_and_public_day.py or super/woman/different_day/make_week_of_young_week/next_thing_and_company.py after able world"

---
+++ super/young_person_or_woman/important_government_and_public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_thing(str_arg):
+    leave_place(str_arg)
+    print('bad_week')
+
+def leave_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_thing('case')
