283579b4522b825aba65a732ed14da60c6da3d41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 10:50:45 -0500

    "rm: call super/fact_and_year/early_year/person/use_life/government.py, super/own_place_and_life/leave_man/large_fact_and_last_child/great_eye/give_able_number_up_few_world/take_case_by_new_government.py and super/hand/different_man_and_eye/see_bad_life_with_different_case/old_world.py into little eye"

---
+++ super/own_place_and_life/leave_man/large_fact_and_last_child/great_eye/give_able_number_up_few_world/take_case_by_new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life(str_arg):
+    different_problem_and_right_group(str_arg)
+    print('way_or_work')
+
+def different_problem_and_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life('number')
