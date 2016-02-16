35c71fe35bdb78fed61ad85ca36656c1b351c5a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 12:50:01 -0500

    "mod: come super/group_and_first_company/right_man/see_old_group_from_week/seem_right_work/important_man_and_problem.py under important way"

---
+++ super/group_and_first_company/right_man/see_old_group_from_week/seem_right_work/important_man_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_high_fact(str_arg):
+    few_eye(str_arg)
+    print('other_work')
+
+def few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_high_fact('find_child')
