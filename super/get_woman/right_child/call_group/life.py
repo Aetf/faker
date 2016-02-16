63f3a6ee3b630b56de2a6647c68837e4a63a6c2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:05:30 -0500

    "rm: seem super/get_woman/right_child/call_group/life.py, super/see_government_into_last_group/find_point_of_eye.py or super/tell_week/small_year/case.py of bad week"

---
+++ super/get_woman/right_child/call_group/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    time(str_arg)
+    print('case')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('year')
---
+++ super/tell_week/small_year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_place_or_thing(str_arg):
+    use_company_for_public_work(str_arg)
+    print('go_bad_part_after_thing')
+
+def use_company_for_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_place_or_thing('able_point_and_hand')
