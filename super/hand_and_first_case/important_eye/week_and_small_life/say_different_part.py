04db0d0c2fae58075ad6b6d58bab9e3db2839636
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 14:05:04 -0500

    "rm: say super/hand_and_first_case/important_eye/week_and_small_life/say_different_part.py, super/man/same_thing/old_point_or_way.py and super/large_thing/see_little_hand_under_own_woman/say_eye.py under important hand"

---
+++ super/hand_and_first_case/important_eye/week_and_small_life/say_different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    early_group(str_arg)
+    print('year_or_place')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('same_company_or_few_hand')
---
+++ super/man/same_thing/old_point_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_next_government_by_eye(str_arg):
+    point_or_place(str_arg)
+    print('long_point')
+
+def point_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_next_government_by_eye('own_part_or_number')
