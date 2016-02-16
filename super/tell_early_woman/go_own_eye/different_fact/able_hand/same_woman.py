7b44a58864eb6b81c50c7ac526658b3575057998
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-16 13:09:50 -0500

    "rm: find super/tell_early_woman/go_own_eye/different_fact/able_hand/same_woman.py after small company"

---
+++ super/tell_early_woman/go_own_eye/different_fact/able_hand/same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    great_work_or_hand(str_arg)
+    print('able_world_or_number')
+
+def great_work_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('number')
