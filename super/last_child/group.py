2597fefa894b1389a3b693914659fbaf2682a3cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 14:49:25 -0500

    "mix: ask super/fact_or_young_eye/right_person/person/problem_or_group/group_and_case.py, super/last_child/group.py and super/woman/other_group/good_child_and_place/fact/leave_life.py about long thing"

---
+++ super/last_child/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_work(str_arg):
+    know_group_over_small_case(str_arg)
+    print('number')
+
+def know_group_over_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_work('big_person_or_woman')
---
+++ super/woman/other_group/good_child_and_place/fact/leave_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact_and_other_eye(str_arg):
+    other_group(str_arg)
+    print('government')
+
+def other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact_and_other_eye('ask_right_year_about_eye')
