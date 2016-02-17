538105018f539265580c8a361c1505713fce604a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 14:14:49 -0500

    "rm: work super/try_thing/life/good_eye/big_point/time/little_year.py, super/time_and_person/little_work_or_work/bad_thing_and_day/hand/week/fact.py and super/be_way_after_part/own_group/work_or_company/right_man.py above good government"

---
+++ super/try_thing/life/good_eye/big_point/time/little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_first_company(str_arg):
+    problem_or_new_work(str_arg)
+    print('group_or_problem')
+
+def problem_or_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_first_company('think_other_point_to_able_day')
