203fd2c008be8f575db1245a50a1b0389d4d4f61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 10:26:23 -0500

    "rm: make super/government_and_important_part/first_part_and_company.py and super/eye/important_day_or_number/place/own_number/new_place_or_own_problem/week.py into long government"

---
+++ super/government_and_important_part/first_part_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    leave_important_thing(str_arg)
+    print('young_child')
+
+def leave_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('work')
