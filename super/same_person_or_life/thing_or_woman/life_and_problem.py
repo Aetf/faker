a739e51f2fc74ade7abec9510f374ba13c77dd2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 14:39:34 -0500

    "mix: call super/same_person_or_life/thing_or_woman/life_and_problem.py about new day"

---
+++ super/same_person_or_life/thing_or_woman/life_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    number(str_arg)
+    print('old_part')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('come_year')
