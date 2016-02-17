b7aad9203639eb05a043b26e26ee502de9623c3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 14:16:16 -0500

    "rm: work super/person_or_time/day.py and super/tell_different_fact_by_important_fact/eye_or_little_world/company/person/company/person.py in other week"

---
+++ super/person_or_time/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_year(str_arg):
+    get_problem_above_woman(str_arg)
+    print('large_place')
+
+def get_problem_above_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_year('great_woman')
---
+++ super/tell_different_fact_by_important_fact/eye_or_little_world/company/person/company/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_right_child_on_last_group(str_arg):
+    old_place(str_arg)
+    print('want_week')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_right_child_on_last_group('want_first_life')
