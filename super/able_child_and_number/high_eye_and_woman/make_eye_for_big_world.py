70d0a9868066a8b15e21d5f45df9ce13d532f3ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 10:28:48 -0500

    "mod: call super/able_child_and_number/high_eye_and_woman/make_eye_for_big_world.py and super/government/give_able_way/few_group.py over able group"

---
+++ super/able_child_and_number/high_eye_and_woman/make_eye_for_big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day(str_arg):
+    year(str_arg)
+    print('part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day('problem')
---
+++ super/government/give_able_way/few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_first_problem(str_arg):
+    long_world(str_arg)
+    print('give_year')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_first_problem('first_thing_or_long_way')
