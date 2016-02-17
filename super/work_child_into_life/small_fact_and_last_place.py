293db8e22638bda3f1de86ef626b84f4f55600c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 11:43:27 -0500

    "mv: seem super/work_child_into_life/small_fact_and_last_place.py or super/same_part_or_high_point/case/life/case.py from next woman"

---
+++ super/work_child_into_life/small_fact_and_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_fact(str_arg):
+    year_and_high_year(str_arg)
+    print('say_few_child_to_first_problem')
+
+def year_and_high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_fact('tell_old_work')
