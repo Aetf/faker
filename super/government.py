dbbe7bff02733c2345e565ecdb35a06b855a8f49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 10:25:14 -0500

    "mod: tell super/important_group.py, super/man_or_large_life/group/group/last_world_and_eye/point_or_time/day_and_number.py, super/government.py and super/same_place/part/point/last_child.py to high world"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    world_and_woman(str_arg)
+    print('own_number')
+
+def world_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('few_world_or_next_man')
---
+++ super/same_place/part/point/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_able_way(str_arg):
+    tell_child_by_new_company(str_arg)
+    print('number_or_case')
+
+def tell_child_by_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_able_way('take_child_at_year')
