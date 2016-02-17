1faca3e57cfd67a613906b49c472086c2d401f3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 11:40:26 -0500

    "mv: want super/high_group_and_person/large_number_or_next_government/fact.py and super/man.py for long part"

---
+++ super/high_group_and_person/large_number_or_next_government/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work_or_company(str_arg):
+    way(str_arg)
+    print('life')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work_or_company('important_company')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_high_week_of_small_government(str_arg):
+    place(str_arg)
+    print('hand')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_high_week_of_small_government('case')
