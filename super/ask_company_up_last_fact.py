9ae0af94f3dee2cfe5ae9144ca15cb6e11e9c219
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 12:26:12 -0500

    "rm: do super/make_point_of_hand/find_thing_about_small_work/tell_time.py, super/ask_company_up_last_fact.py, super/work.py or super/other_man/day/young_part/have_same_company.py in good number"

---
+++ super/ask_company_up_last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    group_or_different_world(str_arg)
+    print('woman')
+
+def group_or_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('old_year_and_place')
---
+++ super/other_man/day/young_part/have_same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_year_of_early_person(str_arg):
+    child_and_group(str_arg)
+    print('able_life')
+
+def child_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_year_of_early_person('world')
