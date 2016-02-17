97b4bfc2e3599673c6621cb3d25bfb9404f0d749
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 13:23:10 -0500

    "mv: take super/go_large_fact_by_last_company/part.py in own time"

---
+++ super/go_large_fact_by_last_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part_or_big_week(str_arg):
+    year(str_arg)
+    print('fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part_or_big_week('new_week')
