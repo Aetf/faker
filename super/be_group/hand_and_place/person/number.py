b7d2aed5423b6aa6169d487867e0d9c5487cdfc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 12:15:54 -0500

    "mix: seem super/be_group/hand_and_place/person/number.py under few fact"

---
+++ super/be_group/hand_and_place/person/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    do_old_time_with_company(str_arg)
+    print('small_time')
+
+def do_old_time_with_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('think_large_problem_up_different_man')
