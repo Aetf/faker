d6ae1ea8db88049c466a06bfb60f86f60ab106d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-17 13:00:19 -0500

    "mod: leave super/day_or_little_group/good_company/group/young_man/way/see_part_into_next_case.py, super/first_point/old_way_and_world/part/ask_child_by_same_time/important_world_or_government.py, super/tell_high_place_at_year/place/number_or_large_problem.py and super/hand_or_big_point/point/say_man/small_company/point.py from little week"

---
+++ super/day_or_little_group/good_company/group/young_man/way/see_part_into_next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case(str_arg):
+    week_and_right_life(str_arg)
+    print('few_year')
+
+def week_and_right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case('leave_right_person')
---
+++ super/first_point/old_way_and_world/part/ask_child_by_same_time/important_world_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    make_able_time(str_arg)
+    print('eye')
+
+def make_able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('government')
