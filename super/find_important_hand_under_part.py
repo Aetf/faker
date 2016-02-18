8e8efb656f1c2ed00f5ae99ef37581e46aad90c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:24:23 -0500

    "mv: use super/have_other_time/first_week/way/get_week/first_company.py and super/find_important_hand_under_part.py into bad company"

---
+++ super/find_important_hand_under_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    large_woman(str_arg)
+    print('call_big_life_at_big_work')
+
+def large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_child')
