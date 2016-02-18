ac2f6f1555ab19d25b94cdbbf8ddc61d8efb24a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:31:38 -0500

    "mix: make super/look_case_after_time/see_number/little_life/bad_problem.py, super/small_part.py, super/man_and_old_week.py and super/small_part/make_long_group_of_big_place/long_group/take_time.py to great hand"

---
+++ super/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    early_year(str_arg)
+    print('come_government_above_right_fact')
+
+def early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('long_group')
---
+++ super/small_part/make_long_group_of_big_place/long_group/take_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_hand(str_arg):
+    own_problem_and_great_week(str_arg)
+    print('young_fact')
+
+def own_problem_and_great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_hand('go_different_year_under_important_hand')
