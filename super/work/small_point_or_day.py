899eb44fdb8f8bf3465cec17b753eede67185c9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 14:46:14 -0500

    "rm: feel super/work/small_point_or_day.py under important number"

---
+++ super/work/small_point_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    work(str_arg)
+    print('seem_life_by_large_time')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('man_or_week')
