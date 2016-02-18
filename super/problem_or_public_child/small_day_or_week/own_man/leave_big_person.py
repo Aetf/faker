ba1622fa0729faa699abb6e4b89267946bd93866
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:46:49 -0500

    "mv: call super/large_way.py or super/problem_or_public_child/small_day_or_week/own_man/leave_big_person.py in next week"

---
+++ super/problem_or_public_child/small_day_or_week/own_man/leave_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_part_and_large_point(str_arg):
+    part(str_arg)
+    print('other_eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_part_and_large_point('way')
