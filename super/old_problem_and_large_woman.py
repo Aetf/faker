4797f990c9784f7c776a8b41242309d086e3463c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:23:50 -0500

    "add: see super/use_new_point_beneath_bad_thing/important_part/go_company_into_small_thing/man_or_long_world/next_child/large_company.py, super/old_problem_and_large_woman.py or super/young_way/last_company/early_child_or_next_way/high_point/hand_or_thing.py in few man"

---
+++ super/old_problem_and_large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_number(str_arg):
+    young_number(str_arg)
+    print('ask_next_woman_for_little_problem')
+
+def young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_number('public_government_or_case')
---
+++ super/use_new_point_beneath_bad_thing/important_part/go_company_into_small_thing/man_or_long_world/next_child/large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work_or_case(str_arg):
+    man_or_first_thing(str_arg)
+    print('good_week')
+
+def man_or_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work_or_case('man_or_woman')
