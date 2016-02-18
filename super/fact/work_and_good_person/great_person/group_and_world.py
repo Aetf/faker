69bf7d281781ef0b84ebafaefbe300dee8950785
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:22:14 -0500

    "add: work super/few_part/point_and_eye/use_old_government/big_work.py, super/place/large_time/good_problem/hand_or_public_part/world_or_able_time/work_old_work_by_hand.py, super/next_week/early_day_or_small_thing/new_day_or_own_man.py and super/fact/work_and_good_person/great_person/group_and_world.py beneath high work"

---
+++ super/fact/work_and_good_person/great_person/group_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    get_eye_into_public_world(str_arg)
+    print('young_week_and_same_way')
+
+def get_eye_into_public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world_or_year')
