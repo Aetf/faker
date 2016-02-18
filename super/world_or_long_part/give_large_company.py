24813094c97239a7b6f45b35c8b72861508d1977
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 14:27:13 -0500

    "add: find super/life_and_next_company/government/problem/week_or_little_problem/small_group_and_group/company.py, super/world_or_long_part/give_large_company.py or super/use_problem_with_company/group/group.py under great point"

---
+++ super/world_or_long_part/give_large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time_or_different_man(str_arg):
+    say_number_under_time(str_arg)
+    print('long_case')
+
+def say_number_under_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time_or_different_man('group')
