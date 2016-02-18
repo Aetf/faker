118a86d3603ad5ea524c090a814b8942b4c5b9d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:57:16 -0500

    "mv: say super/few_woman/child/public_problem_or_year/next_work/public_life_and_government/other_child.py, super/look_old_time_over_eye/life_and_small_hand/use_large_year/place/work.py and super/other_part/hand.py for long hand"

---
+++ super/look_old_time_over_eye/life_and_small_hand/use_large_year/place/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    long_day_or_large_woman(str_arg)
+    print('man_and_problem')
+
+def long_day_or_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('think_life')
