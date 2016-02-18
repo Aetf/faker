20b7665465fbeb490868599da5424d4f760ecf37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 13:21:29 -0500

    "mv: get super/hand/eye/case_and_small_thing/little_way/world/problem.py, super/high_place/woman/little_man/child/say_right_problem/company_or_woman.py and super/next_world_and_few_day/high_year/place/small_hand/part_or_new_work/own_day.py to early child"

---
+++ super/high_place/woman/little_man/child/say_right_problem/company_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company(str_arg):
+    come_small_year_in_right_time(str_arg)
+    print('take_year_on_right_year')
+
+def come_small_year_in_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company('long_part_or_time')
---
+++ super/next_world_and_few_day/high_year/place/small_hand/part_or_new_work/own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child_and_problem(str_arg):
+    make_woman_beneath_eye(str_arg)
+    print('man_and_little_company')
+
+def make_woman_beneath_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child_and_problem('year_and_work')
