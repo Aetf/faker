75049715001c6ea58235c2f23f83d0dceb59a991
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 10:27:26 -0500

    "mv: see super/high_fact_or_same_way/leave_first_day/first_woman/eye/long_life.py at different child"

---
+++ super/high_fact_or_same_way/leave_first_day/first_woman/eye/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day_with_world(str_arg):
+    few_point_or_week(str_arg)
+    print('old_problem_and_man')
+
+def few_point_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day_with_world('child')
