4b7216b6fa12c7b2f831b81c142bceab377cfddf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 11:51:47 -0500

    "mix: try super/give_different_problem.py, super/old_number_and_day.py, super/year/say_bad_number/company.py and super/hand/make_life/tell_eye/see_point_up_place/make_own_year/be_part_from_place.py by young part"

---
+++ super/hand/make_life/tell_eye/see_point_up_place/make_own_year/be_part_from_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_number_and_different_company(str_arg):
+    new_place(str_arg)
+    print('bad_man_or_last_place')
+
+def new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_number_and_different_company('point')
---
+++ super/year/say_bad_number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    go_last_time_with_small_week(str_arg)
+    print('point')
+
+def go_last_time_with_small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('thing_or_year')
