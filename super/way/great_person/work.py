3f0b51748339bfdca79ac8fd4086d9ebc9601058
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 11:56:54 -0500

    "add: know super/way/great_person/work.py, super/do_young_number/small_thing_or_government/know_new_problem_after_number/thing_or_large_problem/find_long_government_beneath_woman.py or super/same_part_or_part/bad_work/ask_government/number_and_long_man/life/great_time.py after right problem"

---
+++ super/way/great_person/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_government(str_arg):
+    use_great_government(str_arg)
+    print('large_point_or_new_point')
+
+def use_great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_government('old_point')
