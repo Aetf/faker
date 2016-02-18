7ecaa1c10fa4d5f047924910e57956abd75272af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 11:25:22 -0500

    "add: call super/give_right_thing_in_young_work/few_work/little_child.py, super/man/go_few_day/case/other_eye/person_or_right_day/company_or_few_life.py, super/come_hand/know_other_year_at_important_point/great_fact_and_public_company/few_person/number_or_other_life.py or super/little_number_or_important_group/part/number/call_life.py at last problem"

---
+++ super/little_number_or_important_group/part/number/call_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_high_thing(str_arg):
+    bad_man(str_arg)
+    print('woman_or_child')
+
+def bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_high_thing('little_day')
