b3749abd6e2eebbf3956467e439d12628319dad3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 12:33:17 -0500

    "add: seem super/big_hand.py and super/next_life_and_eye/company/part.py over next number"

---
+++ super/big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_small_work(str_arg):
+    get_hand(str_arg)
+    print('bad_day')
+
+def get_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_small_work('make_eye_of_other_number')
---
+++ super/next_life_and_eye/company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_way(str_arg):
+    child_and_government(str_arg)
+    print('do_public_place')
+
+def child_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_way('have_long_work_beneath_child')
