87a2d0e84c324c650f7ea82e1720a563f6194f6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 14:44:57 -0500

    "rm: feel super/hand.py with different problem"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_week(str_arg):
+    do_life(str_arg)
+    print('make_place')
+
+def do_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_week('good_way_and_new_number')
