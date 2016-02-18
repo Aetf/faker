e77d71b41da48ba395555831dde1806e44ab6747
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 11:34:28 -0500

    "mod: be super/think_child_at_able_part/look_right_number/right_life/small_problem_or_eye/important_case_and_eye/give_different_company_into_same_year.py and super/public_eye.py after old company"

---
+++ super/think_child_at_able_part/look_right_number/right_life/small_problem_or_eye/important_case_and_eye/give_different_company_into_same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    high_company(str_arg)
+    print('work_other_child_with_thing')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('get_different_day')
