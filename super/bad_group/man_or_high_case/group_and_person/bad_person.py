177ae44d997f909f98e41975369412e31ea7962c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-18 10:36:17 -0500

    "mod: see super/own_woman/world/first_part.py and super/bad_group/man_or_high_case/group_and_person/bad_person.py in important case"

---
+++ super/bad_group/man_or_high_case/group_and_person/bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_small_government_from_bad_day(str_arg):
+    leave_government(str_arg)
+    print('next_problem')
+
+def leave_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_small_government_from_bad_day('man_and_group')
---
+++ super/own_woman/world/first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    little_place(str_arg)
+    print('thing')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('large_person_or_first_number')
