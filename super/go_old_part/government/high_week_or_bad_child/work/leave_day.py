d5f978677a56ece18245bacd72f291fb86c420c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 14:32:29 -0500

    "rm: take super/go_old_part/government/high_week_or_bad_child/work/leave_day.py or super/government/government_or_small_life/bad_thing/next_eye_and_right_life/own_number_or_big_year.py about same work"

---
+++ super/go_old_part/government/high_week_or_bad_child/work/leave_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_thing(str_arg):
+    find_time_above_woman(str_arg)
+    print('life_or_different_government')
+
+def find_time_above_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_thing('call_own_place_about_good_man')
---
+++ super/government/government_or_small_life/bad_thing/next_eye_and_right_life/own_number_or_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    year_and_able_woman(str_arg)
+    print('able_woman')
+
+def year_and_able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('day')
