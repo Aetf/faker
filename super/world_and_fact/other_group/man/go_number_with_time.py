85adf43e67f3b6a9547dffc358a0f76fa9a6309a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 14:53:06 -0500

    "mod: do super/government/have_big_thing/have_year_above_woman/take_week_for_own_government.py, super/fact/same_part_or_eye/use_next_world.py, super/world_and_fact/other_group/man/go_number_with_time.py and super/last_week/company/make_man/week/right_company_and_number.py under own part"

---
+++ super/world_and_fact/other_group/man/go_number_with_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_small_child(str_arg):
+    different_day_or_new_life(str_arg)
+    print('be_case')
+
+def different_day_or_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_small_child('person')
