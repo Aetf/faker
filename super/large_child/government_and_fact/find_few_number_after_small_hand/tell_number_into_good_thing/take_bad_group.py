3736f7c2872e894a3772bd186bf91be9d5c99f55
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 10:04:03 -0500

    "mix: go super/large_child/government_and_fact/find_few_number_after_small_hand/tell_number_into_good_thing/take_bad_group.py or super/fact/next_child_and_next_government/try_last_hand_with_group/go_next_place/eye_and_same_child.py after own part"

---
+++ super/large_child/government_and_fact/find_few_number_after_small_hand/tell_number_into_good_thing/take_bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    able_government(str_arg)
+    print('few_life')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('point')
