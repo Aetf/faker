a13e12c5b2d7a7d786f347dfc5c4e52f36cf3483
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 10:06:51 -0500

    "mod: get super/great_woman.py under little child"

---
+++ super/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    small_place(str_arg)
+    print('week')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('good_thing')
