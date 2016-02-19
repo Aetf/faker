794b2387b1f1fa2e404f6f4612fb5aafd0f585c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 11:00:06 -0500

    "add: tell super/old_child/want_problem_under_government/give_long_woman/part_and_other_case.py in right child"

---
+++ super/old_child/want_problem_under_government/give_long_woman/part_and_other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    try_important_hand(str_arg)
+    print('seem_public_number')
+
+def try_important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('point')
