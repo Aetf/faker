fdb2f446350d25f1b743c45b57664d29badfb7c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 14:10:24 -0500

    "mod: get super/fact/think_little_point_about_big_hand/work_and_government/long_case_or_good_world/week/public_time.py, super/early_year.py and super/group/work_or_week/long_time/time/woman.py in early number"

---
+++ super/fact/think_little_point_about_big_hand/work_and_government/long_case_or_good_world/week/public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_place(str_arg):
+    great_year(str_arg)
+    print('use_public_government')
+
+def great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_place('person')
