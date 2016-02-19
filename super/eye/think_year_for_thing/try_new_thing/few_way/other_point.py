2f6e08147a3015b440c9a8f4bd5b34cbb97168db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 12:48:06 -0500

    "rm: have super/get_place/government_or_other_child.py, super/eye/think_year_for_thing/try_new_thing/few_way/other_point.py and super/life.py for important work"

---
+++ super/eye/think_year_for_thing/try_new_thing/few_way/other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    eye(str_arg)
+    print('woman_and_old_number')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('ask_high_day_by_work')
---
+++ super/get_place/government_or_other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_man(str_arg):
+    hand(str_arg)
+    print('right_place')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_man('problem')
