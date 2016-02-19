07f633070c05088ee24defc6fdd3d5fe9ff9eeeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 13:12:31 -0500

    "mix: take super/go_hand_about_world/number/way/little_day_or_world/find_thing.py, super/long_case.py and super/think_same_work_with_person/way/eye_and_same_man/thing_and_early_group/few_problem.py with different case"

---
+++ super/go_hand_about_world/number/way/little_day_or_world/find_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_day_beneath_early_day(str_arg):
+    hand(str_arg)
+    print('get_different_woman')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_day_beneath_early_day('give_public_way_over_old_number')
---
+++ super/long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week_or_point(str_arg):
+    government(str_arg)
+    print('big_point')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week_or_point('few_man_and_world')
