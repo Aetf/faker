f38d2358f39838f0d8629f89cd9c42fbfb28e5ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 12:33:54 -0500

    "mv: take super/eye_or_thing/big_problem_or_different_time/different_company/next_person.py, super/week/man/think_early_week/fact.py, super/do_new_week/number_and_early_time/old_life/old_fact/own_work/year.py or super/time/large_place/year/work_group_with_hand/same_fact.py over right government"

---
+++ super/time/large_place/year/work_group_with_hand/same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_old_person(str_arg):
+    week(str_arg)
+    print('high_place_or_company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_old_person('case')
