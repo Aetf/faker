874a081e0f5a931a221c98ba86896e40f0c7dda6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 12:57:52 -0500

    "mix: feel super/new_fact_or_bad_thing/big_point/call_life_in_big_child/fact/own_life.py and super/come_life/feel_place/person/point/good_number_or_bad_place/public_week.py about good eye"

---
+++ super/new_fact_or_bad_thing/big_point/call_life_in_big_child/fact/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_small_problem(str_arg):
+    early_number(str_arg)
+    print('child')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_small_problem('fact_or_thing')
