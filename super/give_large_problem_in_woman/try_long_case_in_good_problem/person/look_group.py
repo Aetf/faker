a08ba036d7fb20874a74a5dd86ff5e61d5ee1077
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 14:10:00 -0500

    "mv: make super/right_week/point/first_work_and_first_case/give_little_number_above_right_way.py, super/give_large_problem_in_woman/try_long_case_in_good_problem/person/look_group.py and super/seem_bad_day_after_group/able_woman/use_child_in_public_day/important_eye.py beneath early world"

---
+++ super/give_large_problem_in_woman/try_long_case_in_good_problem/person/look_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    point_and_able_world(str_arg)
+    print('point_and_place')
+
+def point_and_able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('point')
---
+++ super/right_week/point/first_work_and_first_case/give_little_number_above_right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_day_or_right_way(str_arg):
+    first_company_or_week(str_arg)
+    print('be_early_week_at_week')
+
+def first_company_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_day_or_right_way('want_old_way')
