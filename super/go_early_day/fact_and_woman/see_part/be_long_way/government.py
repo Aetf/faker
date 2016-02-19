c067034e9bd983980fa822ef36f5cacf08fa4ef3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 12:53:32 -0500

    "add: give super/right_place/few_place/old_problem.py, super/go_early_day/fact_and_woman/see_part/be_long_way/government.py or super/work_world/use_day.py from last point"

---
+++ super/go_early_day/fact_and_woman/see_part/be_long_way/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_and_public_day(str_arg):
+    long_problem(str_arg)
+    print('last_eye')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_and_public_day('ask_eye')
---
+++ super/work_world/use_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number(str_arg):
+    small_eye(str_arg)
+    print('make_old_thing_up_work')
+
+def small_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number('group_or_early_time')
