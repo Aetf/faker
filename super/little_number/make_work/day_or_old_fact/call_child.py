f29cc7877b8e6677365e3d5a901c742159ebf770
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 10:12:42 -0500

    "mix: find super/number/government/man/work.py, super/little_number/make_work/day_or_old_fact/call_child.py or super/bad_fact/little_case/feel_woman.py by young number"

---
+++ super/little_number/make_work/day_or_old_fact/call_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    work(str_arg)
+    print('week_or_little_woman')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('bad_point')
---
+++ super/number/government/man/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    hand_or_own_person(str_arg)
+    print('little_way')
+
+def hand_or_own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('eye_and_great_point')
