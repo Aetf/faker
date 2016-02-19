c23a083ab9cd96c07030b9642c80ed7f3c15944b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-19 14:54:05 -0500

    "add: take super/point/time_and_different_number/eye/company_and_important_world/person_or_bad_person/part_and_able_child.py, super/part.py or super/try_world_with_time/company.py about young year"

---
+++ super/point/time_and_different_number/eye/company_and_important_world/person_or_bad_person/part_and_able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    eye(str_arg)
+    print('thing_and_year')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('good_way')
