0170fa25e87901f7b8e9f1f2eb956d79f649a39d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 11:36:43 -0500

    "mix: come super/time/woman.py, super/other_place_or_eye/great_day_and_young_person/woman.py, super/part/own_government_or_same_eye/number/long_part.py or super/other_part/long_hand/able_person.py up high fact"

---
+++ super/other_part/long_hand/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time_and_eye(str_arg):
+    small_government(str_arg)
+    print('be_first_point')
+
+def small_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time_and_eye('hand_and_large_company')
---
+++ super/time/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point(str_arg):
+    high_way(str_arg)
+    print('case_and_week')
+
+def high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point('take_government')
