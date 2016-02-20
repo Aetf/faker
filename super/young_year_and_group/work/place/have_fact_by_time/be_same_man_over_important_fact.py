cad45c1da95fad06928797c25e29f49f7b50d166
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 14:45:36 -0500

    "mix: make super/public_year_or_different_day.py, super/young_year_and_group/work/place/have_fact_by_time/be_same_man_over_important_fact.py, super/know_child/look_other_group/large_woman/able_problem_and_little_case/early_problem_and_different_problem/be_big_man.py or super/number/woman_and_place/new_man/do_young_problem_with_week/day/small_day.py by same time"

---
+++ super/young_year_and_group/work/place/have_fact_by_time/be_same_man_over_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    use_year(str_arg)
+    print('old_time')
+
+def use_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('large_fact')
