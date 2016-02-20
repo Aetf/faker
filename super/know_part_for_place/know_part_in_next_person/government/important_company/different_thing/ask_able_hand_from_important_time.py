999b7edca61fadb4f22524f35b0bd88f1ef841db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 14:20:37 -0500

    "add: go super/try_old_world/number/point/point.py and super/know_part_for_place/know_part_in_next_person/government/important_company/different_thing/ask_able_hand_from_important_time.py in able case"

---
+++ super/know_part_for_place/know_part_in_next_person/government/important_company/different_thing/ask_able_hand_from_important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company_and_number(str_arg):
+    thing(str_arg)
+    print('point')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company_and_number('good_world')
---
+++ super/try_old_world/number/point/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    new_place(str_arg)
+    print('right_group')
+
+def new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('little_point')
