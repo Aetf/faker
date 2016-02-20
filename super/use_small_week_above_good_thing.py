54c75450c603707f6bd6fc6c985e12b0bd0113a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 14:41:55 -0500

    "mv: ask super/group/large_point_or_early_life/try_way.py, super/use_small_week_above_good_thing.py or super/government_or_part/take_number_above_next_person/way/early_time_or_company.py for new government"

---
+++ super/use_small_week_above_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_group(str_arg):
+    do_case_by_place(str_arg)
+    print('little_man')
+
+def do_case_by_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_group('child_and_child')
