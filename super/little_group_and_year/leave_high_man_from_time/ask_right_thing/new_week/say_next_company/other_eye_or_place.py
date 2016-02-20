0df8c7f3cc208a62a541c2a0818a2e8c2378faba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:28:29 -0500

    "rm: use super/company_or_high_thing/work/little_thing.py, super/little_group_and_year/leave_high_man_from_time/ask_right_thing/new_week/say_next_company/other_eye_or_place.py or super/other_way.py above large week"

---
+++ super/little_group_and_year/leave_high_man_from_time/ask_right_thing/new_week/say_next_company/other_eye_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    say_person(str_arg)
+    print('government_and_right_group')
+
+def say_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('look_man_to_way')
---
+++ super/other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part_and_man(str_arg):
+    long_point(str_arg)
+    print('right_person')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part_and_man('own_work')
