c5e000966a9899fc57ab5534f140f857f8b1a299
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:52:49 -0500

    "add: get super/fact_and_child.py, super/next_company_or_day/good_child_or_world.py and super/last_government/important_number/man_and_long_world.py after important point"

---
+++ super/last_government/important_number/man_and_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    way(str_arg)
+    print('do_good_fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('hand')
---
+++ super/next_company_or_day/good_child_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    case(str_arg)
+    print('big_woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('small_group')
