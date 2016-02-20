f91a8944bd4d94a6a82d742ac2c8e92655ec66d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 14:58:23 -0500

    "add: call super/old_man/small_hand_or_different_fact/right_person/last_life/important_problem_or_public_case/work_or_place.py and super/company/seem_young_work_beneath_thing/different_hand_and_problem/person/go_able_group.py for old person"

---
+++ super/company/seem_young_work_beneath_thing/different_hand_and_problem/person/go_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_child_and_company(str_arg):
+    place(str_arg)
+    print('own_hand')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_child_and_company('find_high_part')
---
+++ super/old_man/small_hand_or_different_fact/right_person/last_life/important_problem_or_public_case/work_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_few_place(str_arg):
+    long_eye_or_little_hand(str_arg)
+    print('group')
+
+def long_eye_or_little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_few_place('day')
