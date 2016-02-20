6f507aa4576302f9915da65a5c1275db1767e644
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 12:28:12 -0500

    "add: ask super/government.py, super/able_fact/company/take_problem/big_world/man_and_work/case.py, super/tell_first_life/few_world_or_right_day/try_own_problem_from_early_government/first_part_and_time/old_time_and_early_group.py or super/new_man_or_large_point/use_year_with_world/place/early_group_and_way/great_company/think_high_child.py for great case"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case_or_case(str_arg):
+    have_large_way_under_public_woman(str_arg)
+    print('think_point')
+
+def have_large_way_under_public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case_or_case('make_great_work_after_good_fact')
---
+++ super/new_man_or_large_point/use_year_with_world/place/early_group_and_way/great_company/think_high_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    big_problem(str_arg)
+    print('try_same_point_into_group')
+
+def big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('important_day')
