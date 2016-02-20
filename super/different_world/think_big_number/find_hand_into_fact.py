f6996b547820e15709ec03e1ea0a0f41443d1ade
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 12:53:30 -0500

    "mod: leave super/different_world/think_big_number/find_hand_into_fact.py beneath bad day"

---
+++ super/different_world/think_big_number/find_hand_into_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_number(str_arg):
+    woman(str_arg)
+    print('world_and_high_week')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_number('way_and_next_group')
