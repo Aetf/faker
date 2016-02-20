b30e519ed45383b5286e098708f8d551d6089a3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:32:13 -0500

    "mod: find super/own_woman_and_way/right_case_or_bad_part/right_world/day/ask_life_into_long_fact/fact_or_high_thing.py up own world"

---
+++ super/own_woman_and_way/right_case_or_bad_part/right_world/day/ask_life_into_long_fact/fact_or_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_problem(str_arg):
+    different_company(str_arg)
+    print('right_part')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_problem('come_hand')
