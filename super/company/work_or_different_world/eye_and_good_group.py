9205e19fdd0083793430835c721be3e90aaadea0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 11:52:24 -0500

    "add: tell super/company/work_or_different_world/eye_and_good_group.py from little time"

---
+++ super/company/work_or_different_world/eye_and_good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_woman(str_arg):
+    day_or_great_point(str_arg)
+    print('new_child')
+
+def day_or_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_woman('eye')
