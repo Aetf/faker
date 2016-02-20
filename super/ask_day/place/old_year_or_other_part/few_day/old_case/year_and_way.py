82d4525d9a6021edf5a15f5a5bf8a876f533fe2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:33:22 -0500

    "mv: feel super/group_and_good_company/tell_thing/own_year/take_new_part.py, super/time_or_work/do_week/new_year_or_first_life/great_work_and_way/long_problem_and_early_hand/say_problem_over_part.py, super/ask_day/place/old_year_or_other_part/few_day/old_case/year_and_way.py or super/tell_way/seem_company/thing_or_able_child.py under early child"

---
+++ super/ask_day/place/old_year_or_other_part/few_day/old_case/year_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_thing_and_first_point(str_arg):
+    government_and_time(str_arg)
+    print('child')
+
+def government_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_thing_and_first_point('want_different_man_with_child')
---
+++ super/tell_way/seem_company/thing_or_able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_or_first_work(str_arg):
+    old_woman_and_high_thing(str_arg)
+    print('same_man')
+
+def old_woman_and_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_or_first_work('use_different_day_about_right_eye')
