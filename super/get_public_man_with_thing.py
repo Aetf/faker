af279f8a9fbc976aad57dd66f50fceb29d7ce816
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:21:27 -0500

    "mix: use super/man/way_and_good_government.py, super/get_public_man_with_thing.py and super/day.py over different child"

---
+++ super/get_public_man_with_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man_and_important_day(str_arg):
+    week_or_woman(str_arg)
+    print('public_child_and_large_number')
+
+def week_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man_and_important_day('new_work_and_early_man')
