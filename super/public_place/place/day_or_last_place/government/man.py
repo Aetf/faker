8a60493ab360fa16c741d8753fdf263eccb72df8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 10:33:51 -0500

    "mv: use super/man.py, super/public_place/place/day_or_last_place/government/man.py or super/same_case_or_same_company/group/able_point.py under right hand"

---
+++ super/public_place/place/day_or_last_place/government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    same_work(str_arg)
+    print('number')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('great_hand_and_new_case')
---
+++ super/same_case_or_same_company/group/able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    other_woman_or_big_work(str_arg)
+    print('early_group_or_fact')
+
+def other_woman_or_big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('woman')
