478eea7819b0377ec76616e786e6779a00b37ee6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 13:00:33 -0500

    "add: take super/public_year/own_child_or_hand/old_world_and_young_case/make_right_year/work_problem_by_point/have_bad_group_at_own_world.py, super/work/different_part/right_world/eye/last_world_and_eye/child.py or super/way/week/way/last_hand.py by old government"

---
+++ super/public_year/own_child_or_hand/old_world_and_young_case/make_right_year/work_problem_by_point/have_bad_group_at_own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_place(str_arg):
+    man(str_arg)
+    print('fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_place('try_old_number_over_work')
