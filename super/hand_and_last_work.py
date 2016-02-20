afb48bb6ea7b52bfeec4519bbad03e7b2b07155b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 10:32:38 -0500

    "mod: have super/hand_and_last_work.py, super/world/part/day.py and super/young_man/early_child/new_woman.py of high person"

---
+++ super/hand_and_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man_and_hand(str_arg):
+    important_part_or_fact(str_arg)
+    print('early_group')
+
+def important_part_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man_and_hand('problem_and_case')
