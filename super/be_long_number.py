77c7d15fa22f8d8e7be4af0aac22aa657c88de08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-20 10:50:06 -0500

    "rm: have super/small_person_or_old_company.py, super/next_number/case/look_case/early_fact.py, super/world/week/fact/do_eye_into_right_year/work_and_last_work/part.py or super/be_long_number.py at high case"

---
+++ super/be_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_right_place(str_arg):
+    man_or_work(str_arg)
+    print('important_work')
+
+def man_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_right_place('come_week')
---
+++ super/small_person_or_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_own_place(str_arg):
+    leave_number(str_arg)
+    print('bad_number')
+
+def leave_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_own_place('world_or_old_way')
