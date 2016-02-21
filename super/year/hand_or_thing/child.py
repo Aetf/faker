af9a09b2470af0c8b88c5ca618c3eb81d3e2450c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 13:17:59 -0500

    "mix: come super/year/hand_or_thing/child.py and super/woman/important_year_and_next_company/public_case.py at same world"

---
+++ super/year/hand_or_thing/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_same_day(str_arg):
+    little_man_and_government(str_arg)
+    print('have_last_day_for_public_number')
+
+def little_man_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_same_day('say_same_government')
