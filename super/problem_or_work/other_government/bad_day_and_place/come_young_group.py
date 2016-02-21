79846ad52429ffac3ef94b82d610b3bd314e4bdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 13:33:52 -0500

    "mv: see super/see_young_case/large_case/get_case/point/tell_week_of_early_problem.py, super/work/thing/long_place_and_person/think_big_day/different_way_or_good_thing/old_point.py, super/problem_or_work/other_government/bad_day_and_place/come_young_group.py or super/world/eye_and_way/problem/last_government/year_and_day.py about same company"

---
+++ super/problem_or_work/other_government/bad_day_and_place/come_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    fact_and_company(str_arg)
+    print('go_few_fact')
+
+def fact_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('right_week')
---
+++ super/world/eye_and_way/problem/last_government/year_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_different_part(str_arg):
+    old_year(str_arg)
+    print('good_way')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_different_part('early_eye')
