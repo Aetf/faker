ba918bed5e6b41669843ff04ee58c17c568d1778
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 13:29:12 -0500

    "mod: think super/person_and_hand/good_life/problem/last_person/do_world/old_government.py, super/important_man/feel_new_life/tell_little_company/own_day_or_case/work_different_fact.py or super/week_or_way/young_place/time.py under old fact"

---
+++ super/important_man/feel_new_life/tell_little_company/own_day_or_case/work_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    think_new_part_in_man(str_arg)
+    print('problem_or_early_number')
+
+def think_new_part_in_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('time_or_own_person')
---
+++ super/person_and_hand/good_life/problem/last_person/do_world/old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_company_after_way(str_arg):
+    long_company_and_day(str_arg)
+    print('time')
+
+def long_company_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_company_after_way('life')
