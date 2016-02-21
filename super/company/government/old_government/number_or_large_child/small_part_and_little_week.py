22d89e669e45d056747cb69e7586eaec88deaf9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:13:32 -0500

    "mix: work super/company/government/old_government/number_or_large_child/small_part_and_little_week.py or super/seem_fact.py from few group"

---
+++ super/company/government/old_government/number_or_large_child/small_part_and_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    large_week_and_thing(str_arg)
+    print('number_or_thing')
+
+def large_week_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('be_problem_on_company')
