d60e687d30eeea0929bc4a410a0be1b97c4c934b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 14:27:13 -0500

    "add: think super/point_or_large_time/woman_or_woman/life/long_year_or_big_number/feel_great_work/work.py, super/problem_and_good_government/seem_old_case/seem_place_on_great_place.py and super/little_fact/long_world_and_government/call_case_from_man.py by last eye"

---
+++ super/problem_and_good_government/seem_old_case/seem_place_on_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman(str_arg):
+    want_high_eye(str_arg)
+    print('early_fact_and_work')
+
+def want_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman('place_and_thing')
