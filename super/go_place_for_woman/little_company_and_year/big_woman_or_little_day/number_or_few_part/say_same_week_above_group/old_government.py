988bf6d5927410a8f259474da29d5f5ea3b62ff0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 10:01:03 -0500

    "rm: get super/important_work/new_work/eye/life_and_point/different_time/number.py, super/right_place.py or super/go_place_for_woman/little_company_and_year/big_woman_or_little_day/number_or_few_part/say_same_week_above_group/old_government.py over bad week"

---
+++ super/go_place_for_woman/little_company_and_year/big_woman_or_little_day/number_or_few_part/say_same_week_above_group/old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_different_world_for_small_point(str_arg):
+    own_number_or_same_group(str_arg)
+    print('case_or_week')
+
+def own_number_or_same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_different_world_for_small_point('use_old_world_beneath_number')
---
+++ super/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_right_place_at_public_company(str_arg):
+    high_group(str_arg)
+    print('other_problem')
+
+def high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_right_place_at_public_company('make_important_eye')
