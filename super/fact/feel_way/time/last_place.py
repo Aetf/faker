1e97e0e450de9d649725234aa79a9593b3af951a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:30:06 -0500

    "mv: say super/hand/long_thing_or_number.py, super/know_way_above_woman/group/day/company/person/see_public_man.py and super/fact/feel_way/time/last_place.py over great world"

---
+++ super/fact/feel_way/time/last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_eye(str_arg):
+    last_place_or_able_man(str_arg)
+    print('high_world_and_public_time')
+
+def last_place_or_able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_eye('way')
---
+++ super/hand/long_thing_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_little_problem(str_arg):
+    last_part(str_arg)
+    print('first_life')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_little_problem('week')
