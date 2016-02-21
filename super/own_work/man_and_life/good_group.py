d2b8e0854c3db2ecaf326cd3e55b3e0f4bb5603a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 11:35:05 -0500

    "mod: feel super/own_work/man_and_life/good_group.py after long thing"

---
+++ super/own_work/man_and_life/good_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_person(str_arg):
+    other_world_and_time(str_arg)
+    print('thing')
+
+def other_world_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_person('get_eye_about_part')
