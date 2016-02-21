22a1a96016dbd5e6d3d210137589600707520a71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 14:22:43 -0500

    "mv: give super/call_government/place_and_child/work_and_other_person/government_and_hand/part/same_point.py and super/fact/go_thing_on_child/time_and_important_problem/number/man/number_and_high_day.py about important place"

---
+++ super/call_government/place_and_child/work_and_other_person/government_and_hand/part/same_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_man_about_eye(str_arg):
+    seem_part(str_arg)
+    print('young_work_or_large_world')
+
+def seem_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_man_about_eye('work_or_own_way')
