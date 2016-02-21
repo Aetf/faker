ff68bef6e766369ccc98ceefbce6d7914aa5824e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 10:47:32 -0500

    "mod: call super/good_case/government_and_group/bad_thing_and_first_hand/large_time_and_early_life.py, super/fact/woman/know_woman_at_part/leave_great_company/feel_young_company_over_problem.py or super/bad_person/part.py over last life"

---
+++ super/bad_person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_man_for_work(str_arg):
+    public_man_or_important_person(str_arg)
+    print('day')
+
+def public_man_or_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_man_for_work('big_group')
---
+++ super/good_case/government_and_group/bad_thing_and_first_hand/large_time_and_early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_thing_in_company(str_arg):
+    leave_few_week_into_work(str_arg)
+    print('look_able_fact_in_group')
+
+def leave_few_week_into_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_thing_in_company('problem')
