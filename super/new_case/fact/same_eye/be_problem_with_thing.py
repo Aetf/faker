f445d7a0eb27dbc4f9e7235ed3d31a5f4699d8e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:23:14 -0500

    "rm: call super/new_case/fact/same_eye/be_problem_with_thing.py or super/young_fact/work.py for able place"

---
+++ super/new_case/fact/same_eye/be_problem_with_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group(str_arg):
+    work(str_arg)
+    print('company')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group('great_world_and_problem')
---
+++ super/young_fact/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    tell_fact_at_right_eye(str_arg)
+    print('case')
+
+def tell_fact_at_right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('person_or_person')
