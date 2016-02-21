2b945d633910b55dbfe967eb06f4ed87f50ad924
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:47:34 -0500

    "mv: get super/government/call_right_life/case_and_right_world/fact/large_work.py, super/last_work_or_group/know_government/time/last_problem_and_fact.py or super/make_own_company/go_able_time/bad_part_and_little_thing/public_man/leave_government_beneath_great_thing.py in last world"

---
+++ super/last_work_or_group/know_government/time/last_problem_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_woman(str_arg):
+    last_woman(str_arg)
+    print('thing')
+
+def last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_woman('early_woman_or_young_eye')
---
+++ super/make_own_company/go_able_time/bad_part_and_little_thing/public_man/leave_government_beneath_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    way(str_arg)
+    print('do_year_up_point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('small_fact')
