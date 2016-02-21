8839229f662dcdfbc78c7896576f50fb23d31aa6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:53:56 -0500

    "mod: go super/new_world/next_world_or_life/same_thing/early_problem_and_public_place/use_other_world.py, super/small_place/small_woman_or_government/child/work/group_and_few_week/life.py, super/able_part/way/part/own_day/work_young_year_with_different_time/little_time.py and super/number_and_year/great_company/bad_child/government/try_hand/try_place_from_week.py at good time"

---
+++ super/new_world/next_world_or_life/same_thing/early_problem_and_public_place/use_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_problem(str_arg):
+    want_new_government(str_arg)
+    print('big_problem')
+
+def want_new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_problem('point')
---
+++ super/small_place/small_woman_or_government/child/work/group_and_few_week/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    own_world_and_work(str_arg)
+    print('do_eye_above_company')
+
+def own_world_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('bad_number_and_place')
