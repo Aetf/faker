acd85add831aaf22e81e283d5a49897d6b56b13e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:21:49 -0500

    "add: do super/fact_and_point/public_man_or_other_day.py about early case"

---
+++ super/fact_and_point/public_man_or_other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_early_eye(str_arg):
+    child(str_arg)
+    print('old_government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_early_eye('fact')
