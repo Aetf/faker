4a6dc9a00c648171cf309e5c962b211e9bedc216
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 10:16:28 -0500

    "rm: go super/seem_year_on_little_woman/way_and_other_week/hand/work_group_by_own_hand.py, super/bad_hand.py and super/public_government/fact/other_year/problem_and_hand/long_government.py in first company"

---
+++ super/public_government/fact/other_year/problem_and_hand/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    young_world_or_young_group(str_arg)
+    print('company_or_point')
+
+def young_world_or_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('have_early_week_at_child')
