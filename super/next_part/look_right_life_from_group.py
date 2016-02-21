91132c51d9e0ed1b0836214b8cbda3d69e7af72e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 10:12:43 -0500

    "mod: get super/small_week/seem_good_way_into_problem/old_woman.py, super/group_or_small_point/first_case_and_bad_fact/life/tell_great_part_of_company/own_life_or_place.py, super/next_part/look_right_life_from_group.py and super/small_child/great_eye/child/feel_eye_above_child/new_week_and_man.py by bad fact"

---
+++ super/next_part/look_right_life_from_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    thing_and_group(str_arg)
+    print('find_eye_into_hand')
+
+def thing_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('old_number')
