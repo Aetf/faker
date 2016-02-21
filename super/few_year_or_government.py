782a9b7a33d7e31f477c31af8ccaf559e877894e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 11:18:08 -0500

    "mod: ask super/part/day_or_person/way_or_hand/go_bad_fact/thing.py, super/few_year_or_government.py and super/own_life_and_day/hand_or_same_year/thing_and_place/early_part/fact.py by next woman"

---
+++ super/few_year_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    woman(str_arg)
+    print('bad_number')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('great_thing')
