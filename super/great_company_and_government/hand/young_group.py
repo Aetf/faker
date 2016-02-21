ca98e6ff9e4be1435988c5ea1cc960c3096fa38c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 10:12:37 -0500

    "mix: want super/great_company_and_government/hand/young_group.py, super/able_way/point/make_world/year_and_different_fact/place_or_last_group.py, super/life.py or super/different_government/good_woman.py by little child"

---
+++ super/great_company_and_government/hand/young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    government_or_problem(str_arg)
+    print('right_person')
+
+def government_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('think_year_on_public_work')
---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_year(str_arg):
+    small_week_or_young_problem(str_arg)
+    print('day')
+
+def small_week_or_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_year('able_government_and_few_point')
