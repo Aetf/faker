929d827234fd0f57c9904079b04a464c023fb3c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 13:04:33 -0500

    "mv: go super/make_young_number_from_long_time/way/find_government_by_last_way/long_point/week/number.py, super/long_day/high_woman.py, super/case/point/government_or_man/other_part/tell_different_week_into_hand/thing_and_few_life.py or super/good_government/old_life/know_woman_under_small_number/own_company_or_work/big_case_or_year/part_and_week.py above important time"

---
+++ super/good_government/old_life/know_woman_under_small_number/own_company_or_work/big_case_or_year/part_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_new_case(str_arg):
+    think_same_person_into_small_world(str_arg)
+    print('work_year_by_small_government')
+
+def think_same_person_into_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_new_case('early_person')
