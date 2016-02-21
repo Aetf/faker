9a595874dfcda7f9f5cfe6a7d4725f3b7570e334
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 13:12:35 -0500

    "mix: want super/call_new_world_of_world.py, super/long_world/different_time/work/big_world_or_number/ask_early_man_under_eye/fact.py, super/year_and_good_world/large_world/good_hand_and_fact/next_number/try_few_woman.py and super/try_part_with_long_problem/bad_problem_and_other_way/next_case_or_new_company/young_place_and_group.py to few fact"

---
+++ super/call_new_world_of_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    day(str_arg)
+    print('day_and_early_person')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('know_week_into_day')
---
+++ super/year_and_good_world/large_world/good_hand_and_fact/next_number/try_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_place(str_arg)
+    print('able_part_and_first_world')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('long_point')
