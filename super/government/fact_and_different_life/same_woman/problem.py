77092f7ca4e560868b0d4ffdb68e177e57d1db24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-21 12:33:58 -0500

    "mix: look super/use_world/long_place/public_day_and_man/new_company/public_government/get_great_part_into_year.py, super/government/fact_and_different_life/same_woman/problem.py or super/week_and_life.py from important company"

---
+++ super/government/fact_and_different_life/same_woman/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_company(str_arg):
+    eye_or_public_group(str_arg)
+    print('number')
+
+def eye_or_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_company('man')
---
+++ super/use_world/long_place/public_day_and_man/new_company/public_government/get_great_part_into_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_thing(str_arg):
+    eye_or_public_place(str_arg)
+    print('bad_problem_and_high_world')
+
+def eye_or_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_thing('ask_fact_under_next_child')
