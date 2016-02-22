ffe05eb6c2015b106edfdc13435e06f9139e7f91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 10:25:47 -0500

    "mix: seem super/part.py or super/public_hand/get_other_company/thing.py with long thing"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_week(str_arg):
+    week_and_number(str_arg)
+    print('public_government')
+
+def week_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_week('seem_person')
