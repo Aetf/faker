0faba130bc3c3f4b38067abc8f0210b513f17bf1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 14:44:40 -0500

    "mix: find super/have_company/go_old_part_with_first_work/right_thing/important_way.py under own child"

---
+++ super/have_company/go_old_part_with_first_work/right_thing/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_last_work(str_arg):
+    bad_way(str_arg)
+    print('point')
+
+def bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_last_work('be_life_after_big_week')
