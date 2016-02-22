a71ea1b78dc98b1fd0db4050d94cceb68cca50b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 13:10:38 -0500

    "mod: use super/different_eye_and_great_child/small_number/able_eye.py, super/feel_government/life_or_different_year/thing/important_week/know_right_thing_beneath_group/little_world.py or super/life_and_way/point/early_thing.py over right government"

---
+++ super/feel_government/life_or_different_year/thing/important_week/know_right_thing_beneath_group/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_young_problem_from_problem(str_arg):
+    few_thing(str_arg)
+    print('leave_new_way_on_long_man')
+
+def few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_young_problem_from_problem('case')
---
+++ super/life_and_way/point/early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    great_thing_and_last_part(str_arg)
+    print('number')
+
+def great_thing_and_last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('case')
