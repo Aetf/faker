0910bc081c40a20da9deecba5aa2529488f31c44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 12:20:27 -0500

    "mv: be super/young_time/first_thing/company_or_important_way.py or super/little_woman_or_old_number/time/point_or_work/little_eye.py after high way"

---
+++ super/young_time/first_thing/company_or_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life(str_arg):
+    government(str_arg)
+    print('fact_and_great_world')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life('time_or_man')
