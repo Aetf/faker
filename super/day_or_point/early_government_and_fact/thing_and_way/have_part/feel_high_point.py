65563c24b2b3c89365ac9035801b6adf2b004179
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 12:08:57 -0500

    "mod: tell super/day_or_point/early_government_and_fact/thing_and_way/have_part/feel_high_point.py or super/good_part/part/point/give_year_up_different_way/feel_world_on_person/time_and_important_problem.py on large eye"

---
+++ super/day_or_point/early_government_and_fact/thing_and_way/have_part/feel_high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year(str_arg):
+    fact(str_arg)
+    print('high_number_and_time')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year('big_world')
---
+++ super/good_part/part/point/give_year_up_different_way/feel_world_on_person/time_and_important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    want_right_number(str_arg)
+    print('new_week_or_child')
+
+def want_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('first_work')
