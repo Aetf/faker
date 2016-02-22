97c4f84968f2139f5bf9c7337249655f3a8f7edd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 10:01:56 -0500

    "mv: tell super/large_world/find_way_to_last_thing.py from new place"

---
+++ super/large_world/find_way_to_last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_part(str_arg):
+    be_group(str_arg)
+    print('next_problem_and_large_company')
+
+def be_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_part('company')
