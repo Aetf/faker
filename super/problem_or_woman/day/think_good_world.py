738d753239036467b4155dded4b85041684626df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 14:53:37 -0500

    "rm: make super/problem_or_woman/day/think_good_world.py, super/seem_bad_work/next_number.py and super/way.py with few work"

---
+++ super/problem_or_woman/day/think_good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_important_place(str_arg):
+    next_number(str_arg)
+    print('able_way_or_able_week')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_important_place('work_way')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work_and_own_government(str_arg):
+    hand_or_young_person(str_arg)
+    print('large_week_or_hand')
+
+def hand_or_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work_and_own_government('old_child')
