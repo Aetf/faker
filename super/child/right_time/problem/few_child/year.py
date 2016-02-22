645191dffe610b8e52efd57981ddb4c18aabe361
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 14:28:21 -0500

    "rm: take super/child/right_time/problem/few_child/year.py above own person"

---
+++ super/child/right_time/problem/few_child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    long_point(str_arg)
+    print('group')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('few_group')
