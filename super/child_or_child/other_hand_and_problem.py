957a1266f02de5791b9bce3c54804c94af1c8208
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 11:01:32 -0500

    "add: ask super/child_or_child/other_hand_and_problem.py into few man"

---
+++ super/child_or_child/other_hand_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_part_in_great_part(str_arg):
+    own_point(str_arg)
+    print('part')
+
+def own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_part_in_great_part('early_number')
