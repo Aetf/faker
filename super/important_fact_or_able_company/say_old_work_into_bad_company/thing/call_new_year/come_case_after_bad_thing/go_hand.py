706ab899b438ca03f6ff1873fcfe414759793817
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 14:12:56 -0500

    "mv: make super/important_fact_or_able_company/say_old_work_into_bad_company/thing/call_new_year/come_case_after_bad_thing/go_hand.py about last person"

---
+++ super/important_fact_or_able_company/say_old_work_into_bad_company/thing/call_new_year/come_case_after_bad_thing/go_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    same_man(str_arg)
+    print('work_or_life')
+
+def same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('point')
