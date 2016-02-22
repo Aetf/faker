9a969ba62e0d451a3cdaebc1405e827851053ec3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 11:03:15 -0500

    "mod: make super/place/own_world_or_other_week/know_life.py or super/own_way/man_or_person/number/bad_hand.py by other man"

---
+++ super/own_way/man_or_person/number/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    fact(str_arg)
+    print('make_hand')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('group_or_first_week')
---
+++ super/place/own_world_or_other_week/know_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    next_case_or_problem(str_arg)
+    print('problem_or_last_part')
+
+def next_case_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('thing_or_first_problem')
