a8a30af6b312931941f5bc3063fd80c419664e7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 10:41:58 -0500

    "mix: come super/early_number.py, super/time.py and super/great_child.py under able number"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    want_public_place(str_arg)
+    print('thing')
+
+def want_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('leave_new_way_from_other_way')
