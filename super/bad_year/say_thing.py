85d222be8f46ab9ac2c454c84bc59a8755037478
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 10:43:11 -0500

    "mv: be super/bad_year/say_thing.py above last company"

---
+++ super/bad_year/say_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_eye(str_arg):
+    week(str_arg)
+    print('go_public_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_eye('look_woman')
