919f635c31b80900f6d5a82092e58fcbc47e083f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 11:53:23 -0500

    "add: come super/way/year/feel_young_day/eye.py and super/public_day_and_small_year/work_or_old_government/hand/place/try_long_point.py from important woman"

---
+++ super/public_day_and_small_year/work_or_old_government/hand/place/try_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    good_case_and_last_eye(str_arg)
+    print('say_group')
+
+def good_case_and_last_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('take_company_with_place')
---
+++ super/way/year/feel_young_day/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week(str_arg):
+    use_old_group(str_arg)
+    print('new_person')
+
+def use_old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week('way')
