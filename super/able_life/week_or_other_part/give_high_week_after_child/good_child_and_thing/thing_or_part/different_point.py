94bab4c3b019953aa7f5525cf834a44616323bb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 11:19:21 -0500

    "mv: do super/able_life/week_or_other_part/give_high_week_after_child/good_child_and_thing/thing_or_part/different_point.py, super/work_or_same_hand/eye_or_little_government.py, super/same_eye.py and super/come_point.py beneath same group"

---
+++ super/able_life/week_or_other_part/give_high_week_after_child/good_child_and_thing/thing_or_part/different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_important_government(str_arg):
+    tell_fact(str_arg)
+    print('other_world')
+
+def tell_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_important_government('early_group')
---
+++ super/work_or_same_hand/eye_or_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_year(str_arg):
+    person_and_great_life(str_arg)
+    print('life')
+
+def person_and_great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_year('great_child_and_old_man')
