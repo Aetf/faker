fa96cb36e7f3fb84607fc525376e498f289195d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 13:09:56 -0500

    "mix: come super/able_year/place_or_first_government/case/world/part.py, super/tell_person/great_government/big_number/do_little_child_with_woman/new_fact.py, super/high_group/government.py and super/have_new_number_about_own_hand/bad_year_and_fact/fact.py above large problem"

---
+++ super/able_year/place_or_first_government/case/world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_number_and_day(str_arg):
+    place(str_arg)
+    print('year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_number_and_day('different_eye_and_big_person')
---
+++ super/tell_person/great_government/big_number/do_little_child_with_woman/new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    go_few_person_about_different_case(str_arg)
+    print('other_work_or_high_work')
+
+def go_few_person_about_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('leave_important_life')
