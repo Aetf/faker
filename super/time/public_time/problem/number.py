af9a4cd47cad5cede55eac077a603b48cd42a276
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 10:49:03 -0500

    "rm: seem super/number_or_company/life/different_part_or_number/small_group.py or super/time/public_time/problem/number.py in old day"

---
+++ super/time/public_time/problem/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem(str_arg):
+    good_work(str_arg)
+    print('do_last_year')
+
+def good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem('fact')
