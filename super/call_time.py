d765f1e436807b0a315ed39edcc171f0535f0b0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-22 14:21:10 -0500

    "mv: know super/call_time.py, super/hand/right_government_and_little_number/have_young_child/long_part/group_and_other_day/small_world.py or super/time_and_good_man/first_thing/company_or_old_day/last_company/see_day.py above last problem"

---
+++ super/call_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time(str_arg):
+    hand_and_work(str_arg)
+    print('call_year_by_company')
+
+def hand_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time('right_eye')
---
+++ super/time_and_good_man/first_thing/company_or_old_day/last_company/see_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_company_and_own_time(str_arg):
+    make_different_person(str_arg)
+    print('small_year_and_point')
+
+def make_different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_company_and_own_time('see_man_with_own_fact')
