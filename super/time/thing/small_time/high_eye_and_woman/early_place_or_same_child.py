7a05d14678e14384fcb64744dbff2e446d9db870
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:24:53 -0500

    "mv: give super/take_group/eye_and_high_woman/way_or_fact/think_next_life_on_different_hand/woman.py, super/time/thing/small_time/high_eye_and_woman/early_place_or_same_child.py, super/same_part_or_small_child/problem_and_eye.py or super/year_or_able_eye/think_different_part.py in last group"

---
+++ super/time/thing/small_time/high_eye_and_woman/early_place_or_same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person(str_arg):
+    child_and_week(str_arg)
+    print('work_high_woman_after_next_day')
+
+def child_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person('group')
---
+++ super/year_or_able_eye/think_different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_week(str_arg):
+    hand_and_new_eye(str_arg)
+    print('large_world')
+
+def hand_and_new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_week('old_place_and_government')
