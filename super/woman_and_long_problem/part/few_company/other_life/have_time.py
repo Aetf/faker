afb5a6e7e2a1ac183905df3253d7f5fbe80b2a66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:40:48 -0500

    "rm: use super/woman_and_long_problem/part/few_company/other_life/have_time.py or super/number/great_number/tell_time_on_other_fact.py into big company"

---
+++ super/woman_and_long_problem/part/few_company/other_life/have_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    great_man(str_arg)
+    print('bad_eye_or_big_thing')
+
+def great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('life')
