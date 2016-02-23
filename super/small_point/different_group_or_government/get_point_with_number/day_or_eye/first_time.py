fe78c6a3b39eb2784cbf156f8ce9c2c6a0041601
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 10:09:36 -0500

    "mv: get super/same_day/next_place/young_government/year_or_day/place.py, super/small_point/different_group_or_government/get_point_with_number/day_or_eye/first_time.py and super/give_same_child/time_and_government/give_great_case_above_number/leave_world.py of first world"

---
+++ super/small_point/different_group_or_government/get_point_with_number/day_or_eye/first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    find_high_way_over_able_year(str_arg)
+    print('other_case_or_good_day')
+
+def find_high_way_over_able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('company')
