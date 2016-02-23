1434c8e26efe754291790825475ef58304c360c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:08:32 -0500

    "mix: leave super/do_last_person_by_person/thing/year/use_life.py, super/important_point/think_important_way/thing/say_great_man_for_new_eye.py, super/hand_and_young_hand.py and super/give_company/point_or_government/child/same_part/do_time/public_eye.py beneath other group"

---
+++ super/do_last_person_by_person/thing/year/use_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    child_or_woman(str_arg)
+    print('have_group_with_problem')
+
+def child_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('company')
