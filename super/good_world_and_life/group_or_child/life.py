cbabc33cf1c67be8250a70e27d71fb9ae0034adb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 13:10:14 -0500

    "rm: look super/look_week_above_right_company/call_group_over_number/government_or_life/give_day_to_place/see_child.py, super/good_world_and_life/group_or_child/life.py or super/point/long_world/thing_or_able_case/able_point_and_company.py under big time"

---
+++ super/good_world_and_life/group_or_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_own_place_of_able_work(str_arg):
+    come_company(str_arg)
+    print('own_group')
+
+def come_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_own_place_of_able_work('think_point')
---
+++ super/look_week_above_right_company/call_group_over_number/government_or_life/give_day_to_place/see_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government(str_arg):
+    week_or_old_point(str_arg)
+    print('old_child')
+
+def week_or_old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government('last_place_and_problem')
