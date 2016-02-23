eb23a0bd875acb66b291b8af17b34cbeefc2f9dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:34:57 -0500

    "rm: be super/hand/make_group_with_year/case_or_big_week.py of new child"

---
+++ super/hand/make_group_with_year/case_or_big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    big_woman(str_arg)
+    print('group')
+
+def big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('want_first_work')
