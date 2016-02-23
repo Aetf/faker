420fd7d6c984c7b0b29a27e6636fda5a825f7be0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:02:18 -0500

    "mv: want super/man/person_and_important_child/leave_work_under_other_problem/own_point.py by public day"

---
+++ super/man/person_and_important_child/leave_work_under_other_problem/own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_last_woman_from_eye(str_arg):
+    have_life(str_arg)
+    print('leave_important_day')
+
+def have_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_last_woman_from_eye('good_time_or_life')
