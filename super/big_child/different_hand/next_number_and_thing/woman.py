81d319b4621b1e9a381a42baf1678df9deb10f6b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:07:52 -0500

    "mv: want super/government/big_case_and_part/man/group/old_fact_or_big_fact.py, super/find_new_year_to_company/old_company_or_public_way/group/high_case_and_part/good_number_and_old_place.py, super/public_point/old_company.py and super/big_child/different_hand/next_number_and_thing/woman.py under small day"

---
+++ super/big_child/different_hand/next_number_and_thing/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    get_week_at_big_company(str_arg)
+    print('new_number_and_first_world')
+
+def get_week_at_big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('point')
