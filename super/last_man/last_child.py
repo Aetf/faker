4aca01dfb99637cc2054944754f3c9e0d40b349c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 13:26:56 -0500

    "mv: be super/last_man/last_child.py over great part"

---
+++ super/last_man/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_world_of_large_group(str_arg):
+    point(str_arg)
+    print('take_place')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_world_of_large_group('government_or_fact')
