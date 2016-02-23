c1b8e7c9946729f1ee94f3eadd61dbbf38081843
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:54:37 -0500

    "mod: leave super/group_or_next_child/high_company.py and super/come_company_about_young_place/right_person/small_time_or_world/thing/other_work/right_fact.py of small part"

---
+++ super/come_company_about_young_place/right_person/small_time_or_world/thing/other_work/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_and_old_person(str_arg):
+    take_bad_time_at_big_company(str_arg)
+    print('great_work')
+
+def take_bad_time_at_big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_and_old_person('important_point')
---
+++ super/group_or_next_child/high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    man(str_arg)
+    print('next_person_and_important_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('thing_and_right_group')
