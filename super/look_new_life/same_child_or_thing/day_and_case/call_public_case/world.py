808d8310d0189b81fb86c3150cc5dde1910c8e91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:19:25 -0500

    "add: want super/group/early_point/thing_and_year/early_life/week.py, super/look_new_life/same_child_or_thing/day_and_case/call_public_case/world.py, super/problem.py or super/want_small_year_by_little_problem.py at great week"

---
+++ super/look_new_life/same_child_or_thing/day_and_case/call_public_case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    number(str_arg)
+    print('new_group_or_great_company')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('time_and_different_person')
