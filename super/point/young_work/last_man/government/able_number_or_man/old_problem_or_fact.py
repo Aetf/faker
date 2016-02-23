aca8cd02fe975eba74bfe6486421eb625f0d26e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:25:13 -0500

    "rm: ask super/right_group/year/time/fact/right_man/say_important_place.py, super/high_work.py, super/point/young_work/last_man/government/able_number_or_man/old_problem_or_fact.py and super/young_problem_and_hand/thing/group.py up young problem"

---
+++ super/point/young_work/last_man/government/able_number_or_man/old_problem_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_child(str_arg):
+    same_person_and_problem(str_arg)
+    print('do_right_hand_into_part')
+
+def same_person_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_child('public_place')
---
+++ super/young_problem_and_hand/thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_work_by_great_case(str_arg):
+    part(str_arg)
+    print('do_case_for_own_company')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_work_by_great_case('child')
