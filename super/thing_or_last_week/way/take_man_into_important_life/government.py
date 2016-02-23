a32e48b4cb8826d033272c6f746e2f8c851d4853
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 13:47:25 -0500

    "mod: do super/woman/other_life.py, super/thing_or_last_week/way/take_man_into_important_life/government.py or super/hand.py over last part"

---
+++ super/thing_or_last_week/way/take_man_into_important_life/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    public_hand_and_different_place(str_arg)
+    print('point')
+
+def public_hand_and_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('know_man')
