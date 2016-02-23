b037d4ca6f749d96f57d0f8143d7de5ddb610643
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:31:23 -0500

    "mix: go super/man_or_public_time/able_year/number/old_problem/make_company_about_great_group.py, super/call_big_point_under_way/able_child/last_world.py or super/able_group/small_woman.py in same number"

---
+++ super/able_group/small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day_or_high_problem(str_arg):
+    world(str_arg)
+    print('say_case_above_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day_or_high_problem('small_person_or_number')
---
+++ super/call_big_point_under_way/able_child/last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    life(str_arg)
+    print('fact_and_week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('large_day')
