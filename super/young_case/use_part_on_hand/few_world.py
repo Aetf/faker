ded9ebf5bac4e88e2a8a77ddc7e1082bf7e7c6ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:03:07 -0500

    "mod: take super/young_case/use_part_on_hand/few_world.py from young child"

---
+++ super/young_case/use_part_on_hand/few_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_and_good_point(str_arg)
+    print('problem')
+
+def way_and_good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('give_man_by_thing')
