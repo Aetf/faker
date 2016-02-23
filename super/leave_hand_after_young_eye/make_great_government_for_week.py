9027e5f166af184d0116f690e4ef517a3b664ebc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 11:16:56 -0500

    "mix: call super/leave_hand_after_young_eye/make_great_government_for_week.py on good week"

---
+++ super/leave_hand_after_young_eye/make_great_government_for_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group_and_same_case(str_arg):
+    fact(str_arg)
+    print('woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group_and_same_case('world')
