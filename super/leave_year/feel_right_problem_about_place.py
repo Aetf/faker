c2c45b065f8e4979822dd8f5fcddd065e8fa8409
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 10:24:46 -0500

    "mv: look super/next_week/large_point.py, super/leave_year/feel_right_problem_about_place.py or super/eye.py from good point"

---
+++ super/leave_year/feel_right_problem_about_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_government(str_arg):
+    long_time(str_arg)
+    print('say_first_year')
+
+def long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_government('think_little_thing')
---
+++ super/next_week/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    same_week(str_arg)
+    print('young_point_and_group')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('large_number')
