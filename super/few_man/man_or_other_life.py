23c21a626ee43106cafd0a3a906385bf15090bc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 10:43:57 -0500

    "mix: come super/few_man/man_or_other_life.py and super/problem/number.py at next time"

---
+++ super/few_man/man_or_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_life(str_arg):
+    seem_few_point_on_early_place(str_arg)
+    print('world')
+
+def seem_few_point_on_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_life('part')
---
+++ super/problem/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company_or_person(str_arg):
+    little_problem(str_arg)
+    print('case_or_life')
+
+def little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company_or_person('long_way')
