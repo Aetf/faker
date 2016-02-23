2bb1276888a3adc0c556c1b8ffbbfd2c2041790e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 13:45:50 -0500

    "mod: say super/next_week/hand.py, super/able_problem/problem_or_problem/time_or_big_hand/first_government.py, super/life_or_young_person/company_and_work/own_time.py or super/want_eye/few_problem/way.py with little week"

---
+++ super/life_or_young_person/company_and_work/own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    company(str_arg)
+    print('bad_work')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('woman_or_fact')
---
+++ super/want_eye/few_problem/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    same_week_or_group(str_arg)
+    print('government')
+
+def same_week_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('own_world')
