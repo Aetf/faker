d7cc64100efdf88bed17ce235c675ad6500b7638
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 12:39:54 -0500

    "add: ask super/think_same_government/small_way/week_and_company/year.py, super/last_week_or_good_thing/man/thing/great_woman_or_world/see_good_thing_into_part.py or super/able_number/way/able_woman/first_time/young_problem.py into first man"

---
+++ super/able_number/way/able_woman/first_time/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    come_big_woman_of_young_world(str_arg)
+    print('fact')
+
+def come_big_woman_of_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('try_little_work_up_hand')
---
+++ super/last_week_or_good_thing/man/thing/great_woman_or_world/see_good_thing_into_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case_and_day(str_arg):
+    next_work(str_arg)
+    print('big_work_or_company')
+
+def next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case_and_day('able_life_or_woman')
