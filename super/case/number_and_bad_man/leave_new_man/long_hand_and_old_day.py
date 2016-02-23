f9afd5d73ae780b9da5bec89520c9085a5a63096
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:58:40 -0500

    "rm: work super/case/number_and_bad_man/leave_new_man/long_hand_and_old_day.py or super/give_life_at_early_life/person.py at other eye"

---
+++ super/case/number_and_bad_man/leave_new_man/long_hand_and_old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_small_point(str_arg):
+    look_first_life(str_arg)
+    print('work_case')
+
+def look_first_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_small_point('year')
