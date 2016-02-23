68fd8f05c81581b2f06cdecbf48ad1cf0884beb1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 12:30:53 -0500

    "rm: see super/part.py, super/want_own_child_over_different_person/work_young_way_with_hand/large_child.py, super/come_problem_under_problem/man/tell_new_number/feel_public_government.py or super/large_year/make_thing/same_year.py above same fact"

---
+++ super/come_problem_under_problem/man/tell_new_number/feel_public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_bad_government_in_child(str_arg):
+    feel_place(str_arg)
+    print('get_work_by_place')
+
+def feel_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_bad_government_in_child('big_problem_and_child')
