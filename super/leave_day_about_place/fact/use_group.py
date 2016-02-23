4a8dcaecced2c1f82013f13f34a7eb4b5c311b28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-23 14:00:51 -0500

    "add: feel super/little_world.py and super/leave_day_about_place/fact/use_group.py for early year"

---
+++ super/leave_day_about_place/fact/use_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    different_year(str_arg)
+    print('problem_and_small_child')
+
+def different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('group_or_problem')
