8d900c4aa04b72d55bd3fabe8e990ed3d13a886f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 12:07:03 -0500

    "mv: work super/leave_long_problem_up_bad_problem/different_person/know_great_time/big_world_or_woman/case.py, super/see_young_world/high_man.py and super/young_week/part_or_old_case/see_eye.py by great point"

---
+++ super/see_young_world/high_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    case(str_arg)
+    print('hand')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('work_new_hand_above_work')
