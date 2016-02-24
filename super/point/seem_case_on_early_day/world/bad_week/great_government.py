570d32b4efaccc7bf6a30000dcb4dd8d3e2e061e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 12:59:39 -0500

    "mod: ask super/person/first_company/part.py and super/point/seem_case_on_early_day/world/bad_week/great_government.py to other part"

---
+++ super/point/seem_case_on_early_day/world/bad_week/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week(str_arg):
+    have_long_case_under_company(str_arg)
+    print('next_man_or_same_government')
+
+def have_long_case_under_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week('go_work')
