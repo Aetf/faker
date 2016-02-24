19195c72fdd14fc68f373ef73d7084c733402878
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 11:49:19 -0500

    "mix: want super/give_person/way/fact/few_person_or_old_world/new_week_and_woman/work.py and super/government_and_year/number/point/try_week.py from public world"

---
+++ super/give_person/way/fact/few_person_or_old_world/new_week_and_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_high_life(str_arg):
+    woman_and_problem(str_arg)
+    print('big_work')
+
+def woman_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_high_life('ask_government')
---
+++ super/government_and_year/number/point/try_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_long_hand(str_arg):
+    old_person(str_arg)
+    print('work_and_other_way')
+
+def old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_long_hand('great_hand')
