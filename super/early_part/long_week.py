e714c357180b00bbbcefe9265bb615748366b10a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 11:26:06 -0500

    "mv: get super/problem/important_way/part/small_place_or_big_fact/place/part.py or super/early_part/long_week.py of small eye"

---
+++ super/early_part/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    large_group(str_arg)
+    print('few_fact')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('other_place')
