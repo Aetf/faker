006f0a2be70b99e38e73d281d97d4d399d3f80fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 10:37:31 -0500

    "mod: think super/old_world/man_and_problem/come_woman.py at same number"

---
+++ super/old_world/man_and_problem/come_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    find_other_hand_for_work(str_arg)
+    print('seem_large_child_with_good_woman')
+
+def find_other_hand_for_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('old_place_or_number')
