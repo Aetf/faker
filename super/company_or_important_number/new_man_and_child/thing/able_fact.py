410733a1f2ae5a42f035402a1d82ac11099400b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 12:06:36 -0500

    "mix: find super/take_high_point/other_problem/big_eye.py or super/company_or_important_number/new_man_and_child/thing/able_fact.py from important government"

---
+++ super/company_or_important_number/new_man_and_child/thing/able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man_and_few_week(str_arg)
+    print('part')
+
+def man_and_few_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('new_week')
---
+++ super/take_high_point/other_problem/big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part_or_early_problem(str_arg):
+    important_child(str_arg)
+    print('point_and_small_woman')
+
+def important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part_or_early_problem('year_or_few_woman')
