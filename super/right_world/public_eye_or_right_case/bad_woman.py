3e5643f4a37b3db8d38715e953d6d262eb871b6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 11:09:55 -0500

    "rm: ask super/right_world/public_eye_or_right_case/bad_woman.py of big case"

---
+++ super/right_world/public_eye_or_right_case/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_problem(str_arg):
+    right_part_and_young_fact(str_arg)
+    print('problem')
+
+def right_part_and_young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_problem('time_and_point')
