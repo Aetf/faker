f52682cf6df7fded34fde0f35da3bc3d756fa71d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 12:20:01 -0500

    "rm: use super/tell_day/small_hand_or_place/feel_large_work/call_place/woman_and_high_day/group.py and super/think_few_life_for_man/child/be_long_woman/life.py to bad woman"

---
+++ super/tell_day/small_hand_or_place/feel_large_work/call_place/woman_and_high_day/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_early_time(str_arg):
+    work(str_arg)
+    print('government_or_world')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_early_time('large_place')
