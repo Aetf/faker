da92b9f6b160dc99b73e903b3e03dc98d93ba797
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 14:57:11 -0500

    "mv: come super/other_person/next_woman/make_woman.py or super/government.py in next day"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    government(str_arg)
+    print('big_woman')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('problem')
