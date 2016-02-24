29825fef73b55fd1d0b4ad68842a018c4fc4eed7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 14:09:03 -0500

    "rm: take super/new_problem_and_different_week/great_life.py and super/try_bad_thing_by_own_case/fact_or_part/try_able_company_over_long_way/high_place/take_large_hand_in_right_world.py into able government"

---
+++ super/new_problem_and_different_week/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_time_of_place(str_arg):
+    day(str_arg)
+    print('few_woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_time_of_place('able_hand')
