b5e52058368fcd0e9c050b2fc38269f99d239db6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 10:38:46 -0500

    "mv: find super/own_person/same_eye_and_man/see_large_part_under_eye.py, super/give_bad_part_of_number.py or super/good_group/big_company_and_important_woman/time/great_way/eye_or_case.py at important number"

---
+++ super/good_group/big_company_and_important_woman/time/great_way/eye_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    right_year(str_arg)
+    print('case')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('fact_and_important_hand')
---
+++ super/own_person/same_eye_and_man/see_large_part_under_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_life(str_arg):
+    different_life(str_arg)
+    print('company_or_little_time')
+
+def different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_life('know_number')
