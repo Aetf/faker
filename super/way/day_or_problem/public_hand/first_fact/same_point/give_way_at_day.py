67338de86e19a635695689f9359d6ad3e409da0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-24 10:00:20 -0500

    "add: do super/do_public_number/week/life_and_work/go_other_work.py and super/way/day_or_problem/public_hand/first_fact/same_point/give_way_at_day.py for public world"

---
+++ super/way/day_or_problem/public_hand/first_fact/same_point/give_way_at_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_right_year(str_arg):
+    problem(str_arg)
+    print('company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_right_year('part')
