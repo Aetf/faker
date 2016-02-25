a9161e3474c21963d409c52fc816bb1dd67ef5ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 14:30:58 -0500

    "mod: work super/think_high_case/child/place/fact.py of own work"

---
+++ super/think_high_case/child/place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_long_way(str_arg):
+    good_woman(str_arg)
+    print('young_way')
+
+def good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_long_way('try_able_life')
