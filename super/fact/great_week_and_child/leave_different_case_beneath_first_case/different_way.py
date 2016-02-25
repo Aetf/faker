6fe8af919795a46974196b0b5cb2431cad981436
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 11:24:36 -0500

    "rm: be super/fact/great_week_and_child/leave_different_case_beneath_first_case/different_way.py into own fact"

---
+++ super/fact/great_week_and_child/leave_different_case_beneath_first_case/different_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    give_government_for_case(str_arg)
+    print('own_woman_or_young_point')
+
+def give_government_for_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('little_number_and_long_work')
