86bd72e2809fd808a42db127e97b863862b86373
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:41:02 -0500

    "add: try super/come_group_under_work/point_or_person/call_week/same_thing/eye_and_long_work/great_part.py, super/work_and_world/young_woman_or_time/public_man.py or super/use_government/own_year_or_point/able_problem_or_day/early_child.py at different way"

---
+++ super/come_group_under_work/point_or_person/call_week/same_thing/eye_and_long_work/great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_own_fact_for_public_woman(str_arg):
+    time_and_fact(str_arg)
+    print('young_thing_and_thing')
+
+def time_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_own_fact_for_public_woman('way')
---
+++ super/use_government/own_year_or_point/able_problem_or_day/early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_bad_point_into_time(str_arg):
+    year_and_work(str_arg)
+    print('new_year')
+
+def year_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_bad_point_into_time('new_case')
