7daea448c680bbd7e82f544b62e99ac3a51c501e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:54:42 -0500

    "add: want super/first_case/young_week.py, super/high_world_and_long_hand/group.py, super/tell_great_life/see_child.py and super/come_same_fact_above_thing/time/small_place/child_or_little_place/place_or_child/first_woman.py from other time"

---
+++ super/tell_great_life/see_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work_over_hand(str_arg):
+    work(str_arg)
+    print('young_part_or_same_child')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work_over_hand('little_work')
