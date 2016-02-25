5047680c9c265a6a2fcd62242a0e631d70b2809f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 12:07:19 -0500

    "mod: know super/work_year_of_right_week.py and super/large_case/last_year_and_case.py in own person"

---
+++ super/large_case/last_year_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    time_and_other_point(str_arg)
+    print('come_first_number_into_life')
+
+def time_and_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('bad_woman_and_world')
---
+++ super/work_year_of_right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_case(str_arg):
+    good_hand(str_arg)
+    print('know_world')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_case('hand')
