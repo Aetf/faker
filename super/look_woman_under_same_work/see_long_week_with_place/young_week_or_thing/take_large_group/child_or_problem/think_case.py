18639adf3bd7f640efc027023302a81fac0b6471
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:23:03 -0500

    "add: be super/look_woman_under_same_work/see_long_week_with_place/young_week_or_thing/take_large_group/child_or_problem/think_case.py or super/ask_new_problem.py to large problem"

---
+++ super/look_woman_under_same_work/see_long_week_with_place/young_week_or_thing/take_large_group/child_or_problem/think_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    have_person_above_right_way(str_arg)
+    print('case')
+
+def have_person_above_right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('high_company')
