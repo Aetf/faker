02f9976a8ad771394ff1c3f6ab87343fff56c9cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:41:06 -0500

    "rm: look super/first_group.py and super/own_place/want_number/new_way.py on little fact"

---
+++ super/first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    work_or_right_company(str_arg)
+    print('year')
+
+def work_or_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('big_day')
---
+++ super/own_place/want_number/new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    high_world_and_day(str_arg)
+    print('do_high_group_in_child')
+
+def high_world_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('first_case')
