f45452a2c6b6227677830854368a8eae32c4c790
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 12:14:07 -0500

    "mv: ask super/try_large_government_above_part/case/last_company_and_public_part.py, super/think_young_government/try_fact/own_world/place_or_other_way/point.py, super/life/early_man_and_small_man/long_thing/long_year/work/own_time_or_little_number.py or super/different_group/long_number_and_good_thing/time_or_bad_number.py with next government"

---
+++ super/think_young_government/try_fact/own_world/place_or_other_way/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_child_to_life(str_arg):
+    world_or_year(str_arg)
+    print('man')
+
+def world_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_child_to_life('government_and_part')
