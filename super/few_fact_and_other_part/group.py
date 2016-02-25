c461728bbe1c291231a56e604fd7ed727c71610f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:35:42 -0500

    "rm: leave super/small_time_or_life/want_right_week_for_little_government/next_way/call_day/child.py or super/few_fact_and_other_part/group.py by great hand"

---
+++ super/few_fact_and_other_part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_week(str_arg):
+    big_work(str_arg)
+    print('government')
+
+def big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_week('group')
