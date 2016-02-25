0743f1926492e20c8438170210bec2ef3ab53e16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 11:07:34 -0500

    "mod: go super/say_woman/give_person/leave_own_world_in_way/new_point.py, super/man_and_young_fact/group/man_and_man/able_eye_and_place.py, super/bad_man_and_year/public_child/try_place_above_life/work_early_number_for_able_place/eye_and_same_life/use_world_with_company.py and super/day/company/first_hand.py on last number"

---
+++ super/bad_man_and_year/public_child/try_place_above_life/work_early_number_for_able_place/eye_and_same_life/use_world_with_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_number_after_place(str_arg):
+    way(str_arg)
+    print('long_government')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_number_after_place('number')
---
+++ super/day/company/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    able_person(str_arg)
+    print('problem')
+
+def able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('week_and_government')
