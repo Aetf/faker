7194d35775eff1832276e382a1815ec42f40b930
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:52:08 -0500

    "mod: look super/fact/same_work.py, super/new_life/take_public_part_above_big_case/man/different_time/child.py, super/go_child_under_new_world/ask_old_part_beneath_place/year_or_great_part/right_way/have_right_way/know_first_woman_by_small_government.py and super/high_day/time_and_case/call_same_part/other_thing_or_big_day.py of able fact"

---
+++ super/new_life/take_public_part_above_big_case/man/different_time/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    few_week(str_arg)
+    print('new_day_and_public_eye')
+
+def few_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('different_number')
