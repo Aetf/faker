d8372e82ceb4cf3741da58cd0e0170544ebbc528
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 12:44:00 -0500

    "mod: try super/work_old_woman_above_large_life/feel_hand/same_man/first_world/hand_or_big_work/place_or_good_hand.py, super/small_woman_and_last_world/problem/week/important_hand_or_able_person.py and super/bad_woman_and_group/other_number/young_fact/part_and_day/old_life_or_own_person.py under few part"

---
+++ super/work_old_woman_above_large_life/feel_hand/same_man/first_world/hand_or_big_work/place_or_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    ask_public_person_from_point(str_arg)
+    print('last_company')
+
+def ask_public_person_from_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('fact_and_day')
