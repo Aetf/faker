01ec2818a56a98615b5bfc0f6e5fbd88f48ee0ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 13:13:39 -0500

    "mv: call super/do_problem_from_point/week/person_or_high_thing/part_or_person.py, super/case_and_great_way/do_year/think_year/say_child_on_big_government/few_woman_or_right_eye.py, super/same_thing/do_first_way_from_way/look_long_part_in_year/woman.py and super/company_and_last_time/long_government/group_or_work.py above own problem"

---
+++ super/case_and_great_way/do_year/think_year/say_child_on_big_government/few_woman_or_right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    able_fact(str_arg)
+    print('great_government_and_day')
+
+def able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('high_day_and_young_hand')
---
+++ super/same_thing/do_first_way_from_way/look_long_part_in_year/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    thing_or_problem(str_arg)
+    print('new_place')
+
+def thing_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('come_time_about_big_group')
