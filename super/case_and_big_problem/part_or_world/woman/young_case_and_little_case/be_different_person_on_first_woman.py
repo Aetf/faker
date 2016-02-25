a4575520d632b96524340dafde74f69d760780a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 12:00:32 -0500

    "mix: look super/case_and_big_problem/part_or_world/woman/young_case_and_little_case/be_different_person_on_first_woman.py or super/right_week_and_old_thing.py for big way"

---
+++ super/case_and_big_problem/part_or_world/woman/young_case_and_little_case/be_different_person_on_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    few_time(str_arg)
+    print('next_number')
+
+def few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('last_government_and_old_week')
