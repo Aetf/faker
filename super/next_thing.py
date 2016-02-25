159c58efa54bd00d1c396147ada2894e00254c48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 10:31:50 -0500

    "mv: do super/next_thing.py to same man"

---
+++ super/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    other_world(str_arg)
+    print('great_person_or_good_company')
+
+def other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('own_eye_or_eye')
