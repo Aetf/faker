3f00f1d8d147eecd91ab00ed55473646bb3c973c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 11:52:16 -0500

    "mv: take super/great_way.py, super/few_person_or_work/have_woman_under_long_case/use_last_child/life/time.py or super/early_thing/get_young_man_after_other_case/first_place/new_week_and_government.py about same point"

---
+++ super/few_person_or_work/have_woman_under_long_case/use_last_child/life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_point(str_arg):
+    young_hand_and_case(str_arg)
+    print('great_hand')
+
+def young_hand_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_point('point_or_time')
---
+++ super/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_same_life(str_arg):
+    young_way(str_arg)
+    print('own_number_or_eye')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_same_life('life')
