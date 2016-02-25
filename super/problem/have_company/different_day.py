39fe29db6a790dba6a047ac635eca4111f08c6d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 14:11:35 -0500

    "mv: ask super/problem/have_company/different_day.py on next year"

---
+++ super/problem/have_company/different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_man_under_way(str_arg):
+    company(str_arg)
+    print('same_year_and_few_week')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_man_under_way('call_number_to_small_child')
