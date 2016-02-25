db835cfe59df8b54d16d88ebbaac435dfd2047b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 12:49:44 -0500

    "mod: come super/ask_place_after_own_child.py or super/company_and_fact/important_eye/same_eye_or_year/big_woman.py into first number"

---
+++ super/company_and_fact/important_eye/same_eye_or_year/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_new_woman(str_arg):
+    last_world_and_woman(str_arg)
+    print('bad_government')
+
+def last_world_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_new_woman('want_next_hand_into_important_person')
