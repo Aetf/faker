53e170fa3eb9be9e2b322314ba46e785e3f1a87d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 11:06:11 -0500

    "mod: get super/year/get_number/person_and_company/way_and_big_case/woman/work.py, super/work_child_to_part/same_way/thing.py and super/new_way/place/high_group/week.py from own thing"

---
+++ super/year/get_number/person_and_company/way_and_big_case/woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    next_problem(str_arg)
+    print('woman')
+
+def next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('same_day')
