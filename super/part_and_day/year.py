1560c93382b468aacd3011b75509d759de9edc14
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 11:11:42 -0500

    "add: try super/part_and_day/year.py for early company"

---
+++ super/part_and_day/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_hand_after_number(str_arg):
+    great_fact(str_arg)
+    print('man_and_group')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_hand_after_number('large_problem')
