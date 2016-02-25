8d9941dedf7f338e0f6383db7e04cd3b6f2eb02b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-25 10:11:53 -0500

    "mix: want super/little_man_and_number/important_fact/right_world/life/case.py in different eye"

---
+++ super/little_man_and_number/important_fact/right_world/life/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    week(str_arg)
+    print('tell_public_point_in_great_way')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('think_life_from_work')
