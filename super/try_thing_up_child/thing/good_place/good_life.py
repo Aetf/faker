2a78105e3789201b9eca3a74ba9bcf45be82e167
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:41:22 -0500

    "rm: know super/number_and_day/week/see_last_group_into_large_child/do_new_year/tell_early_place_at_point.py and super/try_thing_up_child/thing/good_place/good_life.py to last case"

---
+++ super/try_thing_up_child/thing/good_place/good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_life(str_arg):
+    great_point(str_arg)
+    print('take_small_problem_in_group')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_life('hand_and_public_hand')
