5253151fdb27a61043e51ae36e23174be3265fc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 14:36:52 -0500

    "mix: make super/work_early_place.py from new government"

---
+++ super/work_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_person(str_arg)
+    print('bad_woman')
+
+def young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('world_or_part')
