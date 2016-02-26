a1498979b3f745884df3448f2baccb14a51d56c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 10:32:55 -0500

    "mv: seem super/take_new_world/own_week.py about last child"

---
+++ super/take_new_world/own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_right_woman(str_arg):
+    ask_small_part(str_arg)
+    print('hand')
+
+def ask_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_right_woman('place')
