6cc62b824939a969cf96498220cee6ff0257177d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 12:01:56 -0500

    "mv: use super/time/new_thing/own_case/child_and_own_case.py, super/think_woman_over_different_eye/right_world_and_fact/small_government/life/fact/life.py and super/group/find_work/good_thing.py for same person"

---
+++ super/think_woman_over_different_eye/right_world_and_fact/small_government/life/fact/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_day(str_arg):
+    have_public_place_into_new_day(str_arg)
+    print('company_and_thing')
+
+def have_public_place_into_new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_day('child')
---
+++ super/time/new_thing/own_case/child_and_own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_time(str_arg):
+    do_life(str_arg)
+    print('new_group_and_good_thing')
+
+def do_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_time('first_problem_and_bad_group')
