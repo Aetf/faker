c70e7d08ea1e3875ee859a12300311fe233f5a59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 11:09:46 -0500

    "add: want super/try_year_after_small_child.py, super/government_and_able_problem/tell_bad_world_up_old_man/take_time/know_young_thing_over_world.py and super/company/woman.py in old life"

---
+++ super/try_year_after_small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    hand_or_eye(str_arg)
+    print('big_person_or_big_time')
+
+def hand_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('think_last_time')
