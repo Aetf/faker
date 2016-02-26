5b0689feeae3a22c259c7b7591e52879cc6cfd1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:21:59 -0500

    "mix: give super/right_work/first_number/year/important_world_or_early_case/next_day_or_important_week/government.py above same day"

---
+++ super/right_work/first_number/year/important_world_or_early_case/next_day_or_important_week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_hand_or_way(str_arg):
+    next_day(str_arg)
+    print('same_life_and_next_part')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_hand_or_way('own_way')
