0a3de90d2f4701c0d41bb4bc42e224cf74bafca3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 10:16:04 -0500

    "mod: work super/company.py at large company"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_woman(str_arg):
+    day_and_company(str_arg)
+    print('tell_little_thing_by_man')
+
+def day_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_woman('way')
