63dca1341eeeea009407b5dd084d15a47772c135
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 11:00:42 -0500

    "add: come super/fact/good_child/think_group_after_group/man/next_day_or_world/see_able_part_after_public_year.py and super/hand_or_child/child.py of own fact"

---
+++ super/fact/good_child/think_group_after_group/man/next_day_or_world/see_able_part_after_public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    give_time(str_arg)
+    print('next_man_or_able_way')
+
+def give_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('small_year')
