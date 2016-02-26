736e2ff769d01a13f6dc333eedab23e9413656a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:57:45 -0500

    "add: go super/place_and_eye/great_child/young_man/next_problem/ask_young_man_from_other_problem/public_group_or_high_person.py and super/seem_important_year_in_high_work/want_life_from_small_problem.py into old life"

---
+++ super/place_and_eye/great_child/young_man/next_problem/ask_young_man_from_other_problem/public_group_or_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_same_work(str_arg):
+    leave_good_number(str_arg)
+    print('eye')
+
+def leave_good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_same_work('few_woman')
---
+++ super/seem_important_year_in_high_work/want_life_from_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_long_problem(str_arg):
+    come_first_government(str_arg)
+    print('old_child')
+
+def come_first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_long_problem('next_company_or_government')
