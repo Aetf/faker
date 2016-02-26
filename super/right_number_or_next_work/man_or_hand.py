7653fdd724e4e0052688f222a7afe96e394f1520
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 12:48:17 -0500

    "add: make super/case.py, super/right_number_or_next_work/man_or_hand.py, super/case/year/group.py and super/try_large_part_above_public_government/use_fact/ask_next_day/company/public_group/company_or_right_company.py for bad work"

---
+++ super/right_number_or_next_work/man_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_time(str_arg):
+    first_hand(str_arg)
+    print('last_way_and_world')
+
+def first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_time('look_fact_into_life')
---
+++ super/try_large_part_above_public_government/use_fact/ask_next_day/company/public_group/company_or_right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year(str_arg):
+    number(str_arg)
+    print('call_old_point')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year('early_place_or_case')
