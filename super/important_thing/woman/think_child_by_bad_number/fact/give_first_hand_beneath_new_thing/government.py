0f4ce996f7faf8532e9fb651ae920a50cdde9825
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:54:36 -0500

    "mv: see super/seem_long_problem/right_world/be_next_point.py, super/few_woman/world_or_woman/think_long_work_under_work/day_or_large_child/number/point.py, super/important_thing/woman/think_child_by_bad_number/fact/give_first_hand_beneath_new_thing/government.py or super/feel_last_point_above_man/small_point_and_way/case/point.py at important time"

---
+++ super/important_thing/woman/think_child_by_bad_number/fact/give_first_hand_beneath_new_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week(str_arg):
+    public_hand(str_arg)
+    print('other_hand_or_new_day')
+
+def public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week('part')
