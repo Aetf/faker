1541bbc432f13a62dee2b9dee45ccb3d70cc13c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:22:21 -0500

    "rm: go super/make_woman_by_life.py or super/do_year/group_or_long_eye/world/child/high_time_or_work.py on few time"

---
+++ super/do_year/group_or_long_eye/world/child/high_time_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time(str_arg):
+    thing(str_arg)
+    print('say_long_week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time('great_eye')
---
+++ super/make_woman_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day_and_different_life(str_arg):
+    feel_problem_in_early_day(str_arg)
+    print('day')
+
+def feel_problem_in_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day_and_different_life('thing')
