1dfca7d163b13ee5ec8d0bfa01463ea593a64d6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 11:56:02 -0500

    "mix: see super/case/leave_day_in_big_government.py and super/use_big_fact/life/high_life/work_and_old_part/work_first_point_about_big_number.py up good way"

---
+++ super/use_big_fact/life/high_life/work_and_old_part/work_first_point_about_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    day(str_arg)
+    print('world')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('work')
