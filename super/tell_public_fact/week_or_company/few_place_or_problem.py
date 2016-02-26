d19a601ba0bb0c9723b1b659b030dcdf2ba0205c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 13:43:43 -0500

    "mv: look super/year/part/ask_hand/public_work.py and super/tell_public_fact/week_or_company/few_place_or_problem.py up small fact"

---
+++ super/tell_public_fact/week_or_company/few_place_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_other_number(str_arg):
+    think_eye_into_way(str_arg)
+    print('way')
+
+def think_eye_into_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_other_number('first_week_and_long_man')
