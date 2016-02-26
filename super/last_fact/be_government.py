98149e000a5cea734760f6d9c7828b8e312e8167
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 11:58:26 -0500

    "mod: go super/last_fact/be_government.py under old fact"

---
+++ super/last_fact/be_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing_and_hand(str_arg):
+    fact_or_place(str_arg)
+    print('part_and_large_world')
+
+def fact_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing_and_hand('try_company_from_right_government')
