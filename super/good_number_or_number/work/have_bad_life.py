eef5f6d0d6924b9dfc41f112075deac6c35f7b97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 14:56:23 -0500

    "add: tell super/good_number_or_number/work/have_bad_life.py, super/eye_or_day/eye/same_world_or_week/take_big_week/tell_long_week.py or super/few_hand.py into first world"

---
+++ super/good_number_or_number/work/have_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_young_world(str_arg):
+    try_high_child_in_place(str_arg)
+    print('leave_able_hand_to_part')
+
+def try_high_child_in_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_young_world('own_point')
