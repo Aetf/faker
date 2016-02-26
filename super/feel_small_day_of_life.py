0cfb1efa91dd1af0f6075d0fb1e4527b79a95a32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 14:18:13 -0500

    "add: come super/feel_small_day_of_life.py, super/time/leave_man_over_eye/next_hand/same_way_or_group/able_government_or_young_part/early_hand.py and super/child/group_and_old_number/place.py from large work"

---
+++ super/feel_small_day_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_hand_or_man(str_arg):
+    long_company(str_arg)
+    print('fact')
+
+def long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_hand_or_man('man')
---
+++ super/time/leave_man_over_eye/next_hand/same_way_or_group/able_government_or_young_part/early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    day_and_important_world(str_arg)
+    print('big_thing_or_same_work')
+
+def day_and_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('same_work')
