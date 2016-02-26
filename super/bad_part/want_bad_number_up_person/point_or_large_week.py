03e287036fbb0a2403cc95c91744bbe63f0ee1ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 10:25:46 -0500

    "add: say super/bad_part/want_bad_number_up_person/point_or_large_week.py for own year"

---
+++ super/bad_part/want_bad_number_up_person/point_or_large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    come_long_work(str_arg)
+    print('look_time_with_early_fact')
+
+def come_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('new_fact')
