dd7840f0a837a0da97bb7e19c6eea8625b7fb9d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 10:22:47 -0500

    "mv: come super/government/able_point/point_and_part/call_early_number_for_first_way/problem_and_part.py and super/different_case_or_good_man.py in big work"

---
+++ super/government/able_point/point_and_part/call_early_number_for_first_way/problem_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_other_company_into_day(str_arg):
+    be_work_under_right_child(str_arg)
+    print('do_same_week')
+
+def be_work_under_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_other_company_into_day('same_day')
