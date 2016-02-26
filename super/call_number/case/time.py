380225ccb710d1c6f51bff8ec76ea07963642fd8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 12:57:33 -0500

    "mod: use super/call_number/case/time.py, super/take_person/group/say_next_world/go_fact/hand.py or super/ask_large_part_over_week/leave_different_fact/great_part_or_small_part/high_work_or_different_woman/young_company.py for young hand"

---
+++ super/call_number/case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_child(str_arg):
+    place_or_way(str_arg)
+    print('life_and_child')
+
+def place_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_child('thing')
---
+++ super/take_person/group/say_next_world/go_fact/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    early_week(str_arg)
+    print('little_number')
+
+def early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('own_group_or_problem')
