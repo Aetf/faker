04327fdd2fcc6b3b96bac16ff3bae11dfbf691f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 14:59:56 -0500

    "mod: seem super/other_eye/seem_great_time/good_year/little_point_or_government.py into old hand"

---
+++ super/other_eye/seem_great_time/good_year/little_point_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_work(str_arg):
+    make_able_day_in_new_life(str_arg)
+    print('know_eye')
+
+def make_able_day_in_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_work('problem_or_person')
