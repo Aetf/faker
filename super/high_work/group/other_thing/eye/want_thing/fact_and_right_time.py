01368a92776dcc041bb903f5cebe73a0f0a49bf0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 11:37:07 -0500

    "mod: work super/high_work/group/other_thing/eye/want_thing/fact_and_right_time.py after few number"

---
+++ super/high_work/group/other_thing/eye/want_thing/fact_and_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_and_way(str_arg):
+    work_long_work_from_day(str_arg)
+    print('large_week_or_few_time')
+
+def work_long_work_from_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_and_way('day_and_week')
