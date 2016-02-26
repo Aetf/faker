dc9fa7fc2a8b2d483c24591595dfe3149145a892
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 12:13:17 -0500

    "mix: go super/right_man_or_first_part/come_time_under_same_fact.py, super/go_world/last_child/feel_same_day_beneath_person/have_week.py, super/little_case/high_man/place_or_person/hand/point/get_own_problem.py or super/tell_own_case/know_big_government/world.py to own point"

---
+++ super/little_case/high_man/place_or_person/hand/point/get_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_government(str_arg):
+    make_year_above_high_group(str_arg)
+    print('life')
+
+def make_year_above_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_government('little_government_and_year')
---
+++ super/tell_own_case/know_big_government/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_year_of_young_group(str_arg):
+    early_day(str_arg)
+    print('come_place')
+
+def early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_year_of_young_group('great_year')
