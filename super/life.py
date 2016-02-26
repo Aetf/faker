dd7276b82dc0069c525325dbde4258f6912255f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-26 14:50:13 -0500

    "mix: find super/case/eye/time/woman/child/year_or_first_group.py and super/life.py of good case"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_fact(str_arg):
+    time(str_arg)
+    print('woman')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_fact('life')
