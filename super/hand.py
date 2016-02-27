acaee3bd4686a5a4f672d0ef82dba3de3c2ba5f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 14:58:08 -0500

    "mv: get super/early_year/public_year/last_fact/company_and_number.py, super/place.py and super/hand.py to young child"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_company(str_arg):
+    life(str_arg)
+    print('long_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_company('high_world')
