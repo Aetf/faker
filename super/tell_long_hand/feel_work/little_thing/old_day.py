b866c2c6f46385ea5ac7ba4484f8b31056849281
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 14:10:02 -0500

    "add: have super/tell_long_hand/feel_work/little_thing/old_day.py, super/point/go_man_over_day/different_world/company/woman/problem_or_work.py and super/public_week_or_old_place/life.py in great government"

---
+++ super/tell_long_hand/feel_work/little_thing/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_company(str_arg):
+    want_person(str_arg)
+    print('think_old_place')
+
+def want_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_company('say_big_company_on_great_problem')
