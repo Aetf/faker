2b61a3b4d9ce07c271fac881f1fd4133a6fcc3f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:18:06 -0500

    "mv: think super/public_company.py or super/go_problem/great_life_and_important_place/next_hand/number.py at first week"

---
+++ super/go_problem/great_life_and_important_place/next_hand/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    different_way_and_same_year(str_arg)
+    print('hand_and_hand')
+
+def different_way_and_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('big_man')
