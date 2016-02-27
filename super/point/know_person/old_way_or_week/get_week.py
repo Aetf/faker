17115cd9433f8b4b5206698e8447f23915840bd7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:58:31 -0500

    "mod: have super/point/know_person/old_way_or_week/get_week.py to public way"

---
+++ super/point/know_person/old_way_or_week/get_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_woman(str_arg):
+    big_eye(str_arg)
+    print('government')
+
+def big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_woman('world')
