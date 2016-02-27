7b52755f63a50c616bcb3958fbe0f810ce8f3fa7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 14:00:16 -0500

    "rm: give super/company_and_problem/little_hand/do_woman_about_woman/first_work_and_good_thing/public_hand/day.py, super/say_important_place_up_own_group.py and super/want_part_up_year/important_eye/man/last_part_or_week.py at large problem"

---
+++ super/company_and_problem/little_hand/do_woman_about_woman/first_work_and_good_thing/public_hand/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    leave_hand_of_few_person(str_arg)
+    print('man')
+
+def leave_hand_of_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('say_year')
