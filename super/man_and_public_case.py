a6ad8e1bb7037d1b65a1f3a3e3df3d9d81594b87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 14:28:45 -0500

    "mv: tell super/man_and_public_case.py and super/person/thing/year_or_long_government/year.py above big work"

---
+++ super/man_and_public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_problem(str_arg):
+    think_problem_beneath_case(str_arg)
+    print('take_little_place_with_new_world')
+
+def think_problem_beneath_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_problem('great_world')
---
+++ super/person/thing/year_or_long_government/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    thing(str_arg)
+    print('old_eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('important_week')
