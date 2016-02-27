f0e68e0ce44aa811e74ad0e471c44d7c58bbe369
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 13:01:13 -0500

    "rm: ask super/public_way_or_case/great_place/take_person_under_large_life.py or super/work/woman/give_day/work_high_woman/other_number/take_old_woman.py in new man"

---
+++ super/work/woman/give_day/work_high_woman/other_number/take_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_world(str_arg):
+    know_time_above_first_place(str_arg)
+    print('great_eye')
+
+def know_time_above_first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_world('be_great_group_by_person')
