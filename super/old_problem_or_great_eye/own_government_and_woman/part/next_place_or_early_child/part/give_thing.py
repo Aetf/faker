ec6af8bbca56251baba2f88e57fecead5aab9fac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:05:54 -0500

    "rm: get super/old_problem_or_great_eye/own_government_and_woman/part/next_place_or_early_child/part/give_thing.py and super/problem_or_part/few_person_and_government/place_or_child/company/day_and_fact.py by first woman"

---
+++ super/old_problem_or_great_eye/own_government_and_woman/part/next_place_or_early_child/part/give_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_company(str_arg):
+    time(str_arg)
+    print('first_world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_company('other_year')
