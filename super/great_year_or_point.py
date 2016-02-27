0c1e07a284ff4db492ffb369299da6dccf889813
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:49:35 -0500

    "mv: have super/large_government_and_same_thing/place_or_fact/long_place_and_work/large_fact_and_small_week/little_child/day.py, super/great_year_or_point.py and super/time/point_and_good_case.py in last group"

---
+++ super/great_year_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    try_large_point(str_arg)
+    print('case')
+
+def try_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('other_fact_or_other_place')
