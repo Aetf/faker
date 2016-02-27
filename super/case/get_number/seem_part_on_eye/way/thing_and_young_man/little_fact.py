1ee8a07177a787aca39a2ede9d55b061f90e2c76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 13:00:58 -0500

    "add: come super/case/get_number/seem_part_on_eye/way/thing_and_young_man/little_fact.py and super/give_old_part/early_point.py with bad day"

---
+++ super/case/get_number/seem_part_on_eye/way/thing_and_young_man/little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_life_or_young_part(str_arg):
+    first_child_or_big_thing(str_arg)
+    print('leave_bad_problem')
+
+def first_child_or_big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_life_or_young_part('week')
---
+++ super/give_old_part/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    call_early_work_up_few_thing(str_arg)
+    print('company')
+
+def call_early_work_up_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('case')
