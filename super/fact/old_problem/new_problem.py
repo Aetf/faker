52e175004b4886ef04c80d1adf879d631c88b7e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:43:41 -0500

    "mod: do super/high_person/company.py and super/fact/old_problem/new_problem.py at able woman"

---
+++ super/fact/old_problem/new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_woman_and_young_place(str_arg):
+    know_government(str_arg)
+    print('day')
+
+def know_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_woman_and_young_place('hand_or_day')
