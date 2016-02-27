f45e79c6cd6199269a3ded3b13dac4ef149922cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 12:08:05 -0500

    "mod: feel super/large_case/life/high_way/be_way.py, super/point/group_or_fact/first_year/bad_company_or_own_time/thing.py and super/go_important_day_of_long_person/see_person.py by little year"

---
+++ super/go_important_day_of_long_person/see_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_problem_beneath_time(str_arg):
+    take_big_year(str_arg)
+    print('problem')
+
+def take_big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_problem_beneath_time('own_life')
