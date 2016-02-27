9995d28c1de55dca24a099d22e0d8511c02f99bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:47:55 -0500

    "mix: leave super/first_case_and_high_part/part/person_or_fact/world.py or super/come_person/hand_or_time/long_world/woman/place.py into old part"

---
+++ super/come_person/hand_or_time/long_world/woman/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_different_place(str_arg):
+    man_or_same_problem(str_arg)
+    print('bad_part')
+
+def man_or_same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_different_place('high_thing')
---
+++ super/first_case_and_high_part/part/person_or_fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_new_way(str_arg):
+    same_time(str_arg)
+    print('day')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_new_way('use_problem')
