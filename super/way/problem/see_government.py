143fecc45e2d9902ccc36ae970318c15f05fef4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 11:08:19 -0500

    "mix: look super/way/problem/see_government.py or super/time/old_problem/big_problem.py for good place"

---
+++ super/way/problem/see_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group_and_child(str_arg):
+    look_public_place_into_person(str_arg)
+    print('large_time')
+
+def look_public_place_into_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group_and_child('case_and_time')
