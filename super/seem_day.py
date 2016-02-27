d0a2141e7e86335c929810c47232ae9818a27597
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 13:36:44 -0500

    "rm: ask super/seem_day.py above new week"

---
+++ super/seem_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    large_life_or_world(str_arg)
+    print('old_thing_or_little_problem')
+
+def large_life_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('way')
