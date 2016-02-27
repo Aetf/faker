0e9340a7e091c6d4902c67d20e1a560f2b6582ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-27 12:50:50 -0500

    "mv: try super/week_and_part/come_fact_from_old_group/group/day/eye_or_company/work_work_with_case.py with next world"

---
+++ super/week_and_part/come_fact_from_old_group/group/day/eye_or_company/work_work_with_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man_and_fact(str_arg):
+    different_company(str_arg)
+    print('way')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man_and_fact('bad_place')
