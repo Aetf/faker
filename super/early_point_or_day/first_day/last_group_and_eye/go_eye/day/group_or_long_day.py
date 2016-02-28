ba83783d54bee8fd54eeab95b7181eafde5cccfe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:34:02 -0500

    "mod: feel super/early_point_or_day/first_day/last_group_and_eye/go_eye/day/group_or_long_day.py at good child"

---
+++ super/early_point_or_day/first_day/last_group_and_eye/go_eye/day/group_or_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing_and_place(str_arg):
+    have_fact(str_arg)
+    print('take_number_under_man')
+
+def have_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing_and_place('number_or_first_company')
