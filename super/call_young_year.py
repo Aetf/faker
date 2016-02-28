c18b2c00b76b087edc4ea416399bcf33083eaa51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 12:18:16 -0500

    "mv: look super/child/long_thing.py and super/call_young_year.py about last hand"

---
+++ super/call_young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_public_part(str_arg):
+    large_eye(str_arg)
+    print('high_week')
+
+def large_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_public_part('public_year')
---
+++ super/child/long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    want_next_man_about_other_way(str_arg)
+    print('problem_and_number')
+
+def want_next_man_about_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('fact')
