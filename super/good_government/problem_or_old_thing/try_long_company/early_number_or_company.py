1b1f73076f94a78a5783b920e7d355341cbd4785
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 13:48:51 -0500

    "mv: know super/good_government/problem_or_old_thing/try_long_company/early_number_or_company.py, super/look_thing_to_few_company/early_place_and_public_problem/right_child/ask_government/little_woman.py and super/give_good_work_into_new_group.py above different year"

---
+++ super/good_government/problem_or_old_thing/try_long_company/early_number_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    work(str_arg)
+    print('right_number_and_large_year')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('last_time_and_next_number')
---
+++ super/look_thing_to_few_company/early_place_and_public_problem/right_child/ask_government/little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    tell_work_about_life(str_arg)
+    print('bad_problem')
+
+def tell_work_about_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('big_child')
