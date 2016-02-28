24ef8efecb89cdd19fbf69b21fc5c3f4bd39f0bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:56:23 -0500

    "rm: leave super/woman_and_group/try_hand/woman/woman_or_world.py and super/young_time/great_week/day/number/use_long_number_up_own_time.py in first eye"

---
+++ super/woman_and_group/try_hand/woman/woman_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_public_hand_for_government(str_arg):
+    world_and_first_hand(str_arg)
+    print('part')
+
+def world_and_first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_public_hand_for_government('be_government')
