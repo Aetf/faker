c562d43b8dc742ad37bca85e51791d006e24afa3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:35:31 -0500

    "mv: get super/find_old_number_with_new_point/last_hand/year_and_small_way/early_case_or_other_time/high_week_and_little_thing/few_company_and_different_government.py, super/life_or_government.py or super/eye/man/go_early_place_for_man.py about good place"

---
+++ super/find_old_number_with_new_point/last_hand/year_and_small_way/early_case_or_other_time/high_week_and_little_thing/few_company_and_different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    right_eye(str_arg)
+    print('get_little_life')
+
+def right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('child')
---
+++ super/life_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    week(str_arg)
+    print('able_work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('give_own_world')
