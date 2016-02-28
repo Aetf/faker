fb118d94c9253eee4f183408b7afde8501ed16d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 11:44:31 -0500

    "mv: find super/public_way/right_world_or_little_company/different_work/large_child_or_woman/day/important_hand.py, super/think_person/have_group/person/child/ask_life_above_right_man.py, super/same_place_and_different_time/different_work/say_place_at_young_child/young_group_or_man/try_place_by_next_government.py or super/seem_person/great_number_and_case/thing_or_big_time/own_company/say_time_above_company.py in same child"

---
+++ super/same_place_and_different_time/different_work/say_place_at_young_child/young_group_or_man/try_place_by_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_thing(str_arg):
+    make_good_life_from_high_problem(str_arg)
+    print('fact')
+
+def make_good_life_from_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_thing('bad_point_or_way')
---
+++ super/think_person/have_group/person/child/ask_life_above_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work(str_arg):
+    different_year_and_small_point(str_arg)
+    print('last_way')
+
+def different_year_and_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work('ask_large_woman')
