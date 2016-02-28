3e13bfdaaa447a3dbd79f3774f4af8d84ff4f1b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 11:14:56 -0500

    "mv: have super/company/man/right_woman_and_young_case/point_and_government/great_case.py, super/large_day_and_work/week/right_place/great_eye_and_able_week.py, super/case_and_point/public_point_and_number/get_way/thing/own_life/number_or_place.py and super/first_work/other_number/tell_problem_about_important_government/case/fact.py to new hand"

---
+++ super/case_and_point/public_point_and_number/get_way/thing/own_life/number_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_young_week_with_place(str_arg):
+    long_way(str_arg)
+    print('try_high_man_of_own_part')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_young_week_with_place('year_and_new_group')
---
+++ super/large_day_and_work/week/right_place/great_eye_and_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    woman_or_number(str_arg)
+    print('be_first_week')
+
+def woman_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('group')
