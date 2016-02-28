f88b2e1b8a7660ab632c3ee4a96686420bc11a3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 12:13:03 -0500

    "mod: leave super/day/place_or_different_work.py by first thing"

---
+++ super/day/place_or_different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing(str_arg):
+    man_or_hand(str_arg)
+    print('person')
+
+def man_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing('be_person')
