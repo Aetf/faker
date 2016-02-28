9012d672ef4fb5bc23de96b2b63b986b1d7aede8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:16:36 -0500

    "mix: do super/large_thing/life_or_high_child.py, super/child.py or super/young_hand/bad_hand_and_point/next_person/long_time/man.py about next woman"

---
+++ super/young_hand/bad_hand_and_point/next_person/long_time/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    eye_and_able_point(str_arg)
+    print('eye_and_group')
+
+def eye_and_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('work_and_thing')
