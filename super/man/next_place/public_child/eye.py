602fac2eeb63001680e72f1a839fc59f1b07103c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 13:51:42 -0500

    "mod: work super/man/next_place/public_child/eye.py, super/other_week_and_great_point/great_year_or_other_time.py or super/seem_few_life_over_hand.py to last thing"

---
+++ super/man/next_place/public_child/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_thing_above_good_number(str_arg):
+    year(str_arg)
+    print('see_group')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_thing_above_good_number('high_week')
---
+++ super/other_week_and_great_point/great_year_or_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_same_woman(str_arg):
+    first_point_or_child(str_arg)
+    print('woman_and_right_government')
+
+def first_point_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_same_woman('government')
