6f59b1ced71f35a43ca9d9025f9630312de04410
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:55:40 -0500

    "mix: tell super/man_and_early_week.py, super/place/bad_world/case/work_life.py, super/young_child.py and super/woman/different_child.py from same way"

---
+++ super/man_and_early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work_and_child(str_arg):
+    problem(str_arg)
+    print('company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work_and_child('long_problem')
