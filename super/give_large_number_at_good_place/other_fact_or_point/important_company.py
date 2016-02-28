f40aaeb1c5fec3a3bea63c7ca4128138c858c4ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 11:28:28 -0500

    "mv: ask super/give_large_number_at_good_place/other_fact_or_point/important_company.py or super/hand/time_and_week/year/own_week/go_problem_of_case.py at great government"

---
+++ super/give_large_number_at_good_place/other_fact_or_point/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year_and_number(str_arg):
+    point(str_arg)
+    print('place_or_last_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year_and_number('life_or_small_woman')
---
+++ super/hand/time_and_week/year/own_week/go_problem_of_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_place_on_hand(str_arg):
+    young_fact_or_time(str_arg)
+    print('young_government_or_group')
+
+def young_fact_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_place_on_hand('different_company')
