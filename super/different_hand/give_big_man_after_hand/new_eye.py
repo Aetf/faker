144400d51c777b5757de6bacb0771d4dacd14876
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 11:35:39 -0500

    "mv: try super/different_hand/give_big_man_after_hand/new_eye.py in little man"

---
+++ super/different_hand/give_big_man_after_hand/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_world(str_arg):
+    way(str_arg)
+    print('last_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_world('small_life')
