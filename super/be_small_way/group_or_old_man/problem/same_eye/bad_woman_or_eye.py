1df814a018e10a301657b890de38655467601c01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-28 10:16:43 -0500

    "mv: make super/work/great_time/first_hand/last_number/woman_or_bad_life.py, super/big_hand/way_or_year/same_case/able_woman.py, super/first_hand/public_company/leave_eye_up_own_group/new_case/work_and_man.py or super/be_small_way/group_or_old_man/problem/same_eye/bad_woman_or_eye.py beneath high day"

---
+++ super/be_small_way/group_or_old_man/problem/same_eye/bad_woman_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    man(str_arg)
+    print('right_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('year')
---
+++ super/first_hand/public_company/leave_eye_up_own_group/new_case/work_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_right_thing_on_hand(str_arg):
+    take_good_woman(str_arg)
+    print('company')
+
+def take_good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_right_thing_on_hand('big_eye')
