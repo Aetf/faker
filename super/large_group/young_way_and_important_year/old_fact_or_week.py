1e54e7e850bba1ab2a22b21270eb7c33492db32a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 13:26:09 -0500

    "add: know super/large_group/young_way_and_important_year/old_fact_or_week.py from able problem"

---
+++ super/large_group/young_way_and_important_year/old_fact_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    thing(str_arg)
+    print('hand_and_man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('day')
