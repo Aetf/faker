13060584a460aa7fe1d873d633c8ab313cc8d399
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 12:13:17 -0500

    "rm: think super/last_child.py over bad time"

---
+++ super/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_group_by_public_company(str_arg):
+    company(str_arg)
+    print('important_woman')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_group_by_public_company('day')
