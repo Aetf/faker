8929c2f4383e057bf3751cef5d6c2278475466c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 10:17:21 -0500

    "rm: say super/place.py after young company"

---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_own_point_at_thing(str_arg):
+    other_woman_or_work(str_arg)
+    print('use_world_for_hand')
+
+def other_woman_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_own_point_at_thing('year')
