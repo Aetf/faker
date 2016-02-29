6ff8cc16bae2df9cbf99bfa76d5090ef784502e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 11:13:45 -0500

    "mv: make super/important_number/number.py, super/few_thing/part/year/year.py and super/way_or_good_way/early_fact/group_or_fact/old_life/big_number/point.py from next case"

---
+++ super/important_number/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_old_eye(str_arg):
+    eye(str_arg)
+    print('place')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_old_eye('other_life')
---
+++ super/way_or_good_way/early_fact/group_or_fact/old_life/big_number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman(str_arg):
+    day(str_arg)
+    print('ask_point_with_number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman('first_place')
