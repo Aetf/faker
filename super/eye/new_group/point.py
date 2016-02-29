165c779d1112a3c68e9acf774014f9b698b98464
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 14:03:21 -0500

    "add: get super/problem.py, super/bad_woman_or_right_case/use_man_on_thing/leave_way_from_able_work.py, super/first_person/point_and_person.py or super/eye/new_group/point.py under other person"

---
+++ super/eye/new_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    have_large_woman(str_arg)
+    print('early_work')
+
+def have_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('great_week_and_man')
---
+++ super/first_person/point_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_woman(str_arg):
+    week(str_arg)
+    print('day_or_good_case')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_woman('try_number')
