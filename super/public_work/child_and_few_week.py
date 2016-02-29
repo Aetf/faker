c198b87b07c62b852a58fb8a388774d63f30491e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 10:22:41 -0500

    "mix: try super/public_work/child_and_few_week.py or super/day/place/use_week.py about bad woman"

---
+++ super/public_work/child_and_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    problem(str_arg)
+    print('other_man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('bad_hand')
