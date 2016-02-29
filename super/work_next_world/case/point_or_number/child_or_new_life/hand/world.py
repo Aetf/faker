66602dea550ca17337a3c716d35a2868a1fcc298
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 13:42:17 -0500

    "mod: give super/old_child.py or super/work_next_world/case/point_or_number/child_or_new_life/hand/world.py over own child"

---
+++ super/work_next_world/case/point_or_number/child_or_new_life/hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    say_world_to_work(str_arg)
+    print('try_next_man')
+
+def say_world_to_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('find_government')
