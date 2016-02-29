6252a66bca79746291355821bcb81a51652117f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 13:08:43 -0500

    "add: find super/hand/number/fact.py into different week"

---
+++ super/hand/number/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    do_old_week(str_arg)
+    print('early_day_or_man')
+
+def do_old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('work')
