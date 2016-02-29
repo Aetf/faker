ecce86128d390a804f350b26108d8d1acd3a9e6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 14:06:51 -0500

    "rm: work super/different_week/good_hand/next_problem_and_place/number_and_large_week.py, super/different_day/early_point/first_week_or_work/be_public_person_under_long_way/do_first_child_on_small_problem.py, super/last_group_or_place/point_and_child.py or super/company_or_life/part/woman_or_woman/public_company_and_work/year_or_first_day/other_thing_or_next_eye.py with small year"

---
+++ super/company_or_life/part/woman_or_woman/public_company_and_work/year_or_first_day/other_thing_or_next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year_beneath_few_person(str_arg):
+    part(str_arg)
+    print('ask_little_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year_beneath_few_person('child')
---
+++ super/last_group_or_place/point_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_able_company(str_arg):
+    few_place(str_arg)
+    print('day')
+
+def few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_able_company('eye')
