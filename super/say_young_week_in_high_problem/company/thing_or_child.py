89dcaa7a00904db5c67b338994752fe3469434be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 14:52:21 -0500

    "mv: give super/thing_and_new_child/public_place/seem_hand/last_problem/new_eye_and_large_way/own_year_and_thing.py and super/say_young_week_in_high_problem/company/thing_or_child.py on small point"

---
+++ super/say_young_week_in_high_problem/company/thing_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_life_by_able_problem(str_arg):
+    number(str_arg)
+    print('right_case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_life_by_able_problem('world')
