8c06a62eb3517459573cbcf16708a387d2289ba5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 14:29:53 -0500

    "mix: find super/able_group_and_big_child/way.py, super/early_woman_or_high_woman/able_group_or_long_part/work.py, super/different_problem_or_week.py or super/be_fact/do_group_in_right_week.py about important company"

---
+++ super/able_group_and_big_child/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place(str_arg):
+    find_important_fact(str_arg)
+    print('look_part')
+
+def find_important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place('tell_woman_in_great_world')
---
+++ super/different_problem_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_day(str_arg):
+    large_world(str_arg)
+    print('own_fact')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_day('woman')
