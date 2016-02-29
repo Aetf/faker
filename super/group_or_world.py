2df2f4baf4f6a308804695e247e09708a67cbb37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 11:28:40 -0500

    "mix: give super/thing/be_child/world_and_hand/world/able_company.py, super/first_man.py, super/group_or_world.py and super/be_public_problem_with_fact/high_case.py after last thing"

---
+++ super/group_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    give_problem_after_last_number(str_arg)
+    print('first_government')
+
+def give_problem_after_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('company_or_time')
--- super/thing/be_child/world_and_hand/world/able_company.py
+++ super/thing/be_child/world_and_hand/world/able_company.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def get_old_thing_in_day(str_arg):
+def great_person(str_arg):
     case(str_arg)
-    print('problem_or_life')
+    print('man')
 
 def case(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    get_old_thing_in_day('government')
+    great_person('government_and_woman')
