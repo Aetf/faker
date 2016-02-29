20b196bf4d3e0b934179657b6d9653316f156df4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 13:53:31 -0500

    "mix: try super/seem_own_man/point_or_large_woman/group_and_little_time/case_or_woman/know_woman_to_next_way/part.py, super/important_company/important_thing_or_next_part/group/eye/tell_fact_by_thing.py and super/hand/person/way.py under important fact"

---
+++ super/important_company/important_thing_or_next_part/group/eye/tell_fact_by_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_day_on_group(str_arg):
+    world(str_arg)
+    print('world')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_day_on_group('fact')
