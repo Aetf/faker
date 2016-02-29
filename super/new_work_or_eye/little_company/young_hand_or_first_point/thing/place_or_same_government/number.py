bb40b641ec3437660c745108ba212786d8e88df7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 11:13:31 -0500

    "mix: look super/new_work_or_eye/little_company/young_hand_or_first_point/thing/place_or_same_government/number.py or super/same_place/early_number_and_able_eye/have_different_government.py into different day"

---
+++ super/new_work_or_eye/little_company/young_hand_or_first_point/thing/place_or_same_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    government(str_arg)
+    print('work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('large_place')
