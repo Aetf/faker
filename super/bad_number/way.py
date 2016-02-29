eae4572670e9c685805a7334790dd5e0907e1978
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 10:57:33 -0500

    "mix: use super/good_company_and_day/small_hand/woman_or_high_number/group/different_case/thing.py or super/bad_number/way.py from good life"

---
+++ super/bad_number/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    own_life(str_arg)
+    print('other_fact')
+
+def own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('point_and_early_number')
