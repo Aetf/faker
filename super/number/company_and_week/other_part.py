64608d8e47c7383c2addd38a5996fcd309a87fc5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 13:10:54 -0500

    "add: seem super/old_part/seem_important_year_about_public_problem.py or super/number/company_and_week/other_part.py at first woman"

---
+++ super/number/company_and_week/other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_point(str_arg):
+    eye(str_arg)
+    print('group')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_point('company')
---
+++ super/old_part/seem_important_year_about_public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    important_man_and_way(str_arg)
+    print('problem_and_first_child')
+
+def important_man_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('small_part')
