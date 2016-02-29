d13e44b12c7a574a1b009ff308432beb3e76a75f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 14:58:22 -0500

    "mv: think super/week_or_man/hand/person_and_case/way_and_place.py, super/big_way/use_last_thing_to_public_government/go_man_after_old_thing/public_place_or_bad_woman/child.py, super/early_way/same_fact_and_week/few_woman/long_way/group/time.py or super/young_world/man.py under different thing"

---
+++ super/early_way/same_fact_and_week/few_woman/long_way/group/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_number(str_arg):
+    high_problem(str_arg)
+    print('long_week')
+
+def high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_number('year_or_number')
---
+++ super/week_or_man/hand/person_and_case/way_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_part_of_early_hand(str_arg):
+    hand(str_arg)
+    print('government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_part_of_early_hand('feel_first_work_into_small_government')
