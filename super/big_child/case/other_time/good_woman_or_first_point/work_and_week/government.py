dca2322e21c4be8e59aec45e138abe91024e2fd1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 10:10:07 -0500

    "mix: use super/make_life/small_child_and_eye/able_part.py, super/use_week/different_world_or_good_hand/time/see_thing/take_problem_on_last_world.py, super/great_life/bad_fact/life_or_little_hand/right_case_and_old_life.py and super/big_child/case/other_time/good_woman_or_first_point/work_and_week/government.py above important day"

---
+++ super/big_child/case/other_time/good_woman_or_first_point/work_and_week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    new_man(str_arg)
+    print('want_own_person_with_number')
+
+def new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('case_and_high_man')
---
+++ super/use_week/different_world_or_good_hand/time/see_thing/take_problem_on_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye_or_place(str_arg):
+    day(str_arg)
+    print('thing')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye_or_place('fact')
