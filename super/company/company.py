61cf77633f3f339a28d8aa06e4c042c01f426c36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 11:16:39 -0500

    "add: take super/company/company.py, super/give_group_over_large_year/eye.py, super/leave_fact_up_public_time.py or super/public_fact/same_work_or_time/able_fact/man/try_hand_after_man/life.py on old year"

---
+++ super/company/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child_or_large_man(str_arg):
+    see_old_time(str_arg)
+    print('next_day')
+
+def see_old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child_or_large_man('call_group_in_large_thing')
---
+++ super/public_fact/same_work_or_time/able_fact/man/try_hand_after_man/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_thing_in_day(str_arg):
+    case(str_arg)
+    print('problem_or_life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_thing_in_day('government')
