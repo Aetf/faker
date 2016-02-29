9be9570f58a30e52a38e7bba9d28b22e6ccbfc9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 12:14:16 -0500

    "add: try super/day/government_and_point/point_or_old_child.py on right way"

---
+++ super/day/government_and_point/point_or_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    government(str_arg)
+    print('early_week')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('go_young_child_of_last_place')
