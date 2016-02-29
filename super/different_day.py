80ead51ad1fb3a9aa660434b531e82a0f227ef87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-02-29 11:11:37 -0500

    "mix: call super/go_part_to_important_woman/little_man/fact/big_time_or_public_eye/young_problem/own_child.py, super/different_day.py, super/think_new_government_up_problem/little_company.py or super/next_problem/long_man/young_number_and_week/company.py under high thing"

---
+++ super/different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_number(str_arg):
+    week_and_way(str_arg)
+    print('little_point')
+
+def week_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_number('use_large_woman')
