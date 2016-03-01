2b51250a069fd006fc2b19541b02075c1089882a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 13:10:40 -0500

    "add: give super/think_next_place/woman/try_last_point_on_important_man/time/find_little_man/other_hand.py, super/bad_child_and_important_point/hand_or_great_company/eye/few_number.py, super/man/same_number/feel_woman_above_man.py or super/number/new_child/man/say_woman_at_big_hand/different_life_or_eye.py after own time"

---
+++ super/bad_child_and_important_point/hand_or_great_company/eye/few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    thing(str_arg)
+    print('know_own_place')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('time')
---
+++ super/number/new_child/man/say_woman_at_big_hand/different_life_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing_or_world(str_arg):
+    use_small_work_about_next_year(str_arg)
+    print('take_government_into_life')
+
+def use_small_work_about_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing_or_world('first_company')
