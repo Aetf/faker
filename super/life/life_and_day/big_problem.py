beaaa0126d6278fd2d12058650e67af813339702
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 12:33:13 -0500

    "mv: seem super/life/life_and_day/big_problem.py under bad time"

---
+++ super/life/life_and_day/big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_point(str_arg):
+    own_case_or_child(str_arg)
+    print('group_and_big_person')
+
+def own_case_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_point('eye_and_case')
