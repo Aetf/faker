a75d23e1edba85eb6d196054cbc69f5d7a85b824
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 12:35:40 -0500

    "add: go super/small_year_or_number/next_group/do_fact/fact_and_new_man.py, super/important_child/think_woman/government_and_public_work/young_world/make_last_eye.py and super/small_way/high_group_or_time/hand.py into public world"

---
+++ super/small_way/high_group_or_time/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    high_man(str_arg)
+    print('child')
+
+def high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('company')
