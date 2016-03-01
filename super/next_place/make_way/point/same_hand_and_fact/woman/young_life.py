e03249828ad8ca6bb6508ce04ee724d740e3cf4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 12:43:04 -0500

    "mv: have super/next_place/make_way/point/same_hand_and_fact/woman/young_life.py, super/great_group.py and super/right_case_and_few_man/want_person_above_bad_hand/give_large_company/year.py about first part"

---
+++ super/next_place/make_way/point/same_hand_and_fact/woman/young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    eye_or_old_place(str_arg)
+    print('be_bad_hand_for_person')
+
+def eye_or_old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('find_day_above_few_group')
