e19ef54d716f61af62d2a75cc58137160bb7f189
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 10:28:12 -0500

    "mod: go super/able_man/new_problem/way/person.py or super/say_young_company/able_week/thing/same_work_and_way.py on new child"

---
+++ super/able_man/new_problem/way/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    good_day(str_arg)
+    print('man')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('good_year')
