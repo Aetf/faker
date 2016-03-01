d3cc35ca3b815f3678814a9189acfa888ae7c741
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 11:17:26 -0500

    "add: think super/feel_problem/different_eye/ask_different_woman/group_and_case/big_company.py, super/same_place/same_place_or_man.py or super/life_or_hand/find_eye_about_large_way.py to first place"

---
+++ super/life_or_hand/find_eye_about_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    know_eye(str_arg)
+    print('number')
+
+def know_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('last_man')
---
+++ super/same_place/same_place_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_man(str_arg):
+    same_part(str_arg)
+    print('problem')
+
+def same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_man('case')
