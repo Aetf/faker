766971bda49c696d40f6503aab3b2625f5e1bf93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 11:50:39 -0500

    "add: get super/number.py, super/large_government_and_important_fact/last_problem_or_woman/small_part.py, super/number/time_and_old_point/day/make_last_point.py or super/government/bad_problem_and_last_place/give_other_world_with_group.py for new point"

---
+++ super/large_government_and_important_fact/last_problem_or_woman/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_important_part(str_arg):
+    think_early_time(str_arg)
+    print('problem_or_life')
+
+def think_early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_important_part('company')
