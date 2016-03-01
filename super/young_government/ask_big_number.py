f422da137366115671c68e3e65e40094730708df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 10:47:23 -0500

    "rm: ask super/young_government/ask_big_number.py in large group"

---
+++ super/young_government/ask_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_next_time_at_woman(str_arg):
+    thing(str_arg)
+    print('part_or_man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_next_time_at_woman('good_time_or_high_problem')
