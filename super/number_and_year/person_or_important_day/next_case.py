592462cc049878320324299608dd6afce7619b78
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 11:07:38 -0500

    "mod: work super/number_and_year/person_or_important_day/next_case.py, super/problem_or_large_part/government_and_person/day/big_fact/know_problem/next_number_and_work.py and super/seem_early_fact/few_way_or_first_year.py over new person"

---
+++ super/number_and_year/person_or_important_day/next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_same_work(str_arg):
+    man(str_arg)
+    print('take_first_thing_over_large_way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_same_work('take_old_government')
