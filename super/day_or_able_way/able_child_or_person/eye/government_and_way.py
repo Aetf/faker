5abd802a775c4ba92f324349987351c1eadab99c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 12:37:45 -0500

    "mod: see super/day_or_able_way/able_child_or_person/eye/government_and_way.py and super/government.py from little week"

---
+++ super/day_or_able_way/able_child_or_person/eye/government_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    great_way(str_arg)
+    print('give_number_at_new_man')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('great_hand')
