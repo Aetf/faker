a2008c0bc068c4b4d18bba7780ffe1849b8769ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 12:10:41 -0500

    "rm: want super/different_work/early_government/have_group/high_fact.py up small thing"

---
+++ super/different_work/early_government/have_group/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact(str_arg):
+    use_big_person_about_week(str_arg)
+    print('give_bad_work_for_government')
+
+def use_big_person_about_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact('right_day')
