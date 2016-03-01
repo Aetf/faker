b633f9f9f0a5e910ac47a5d9ad7a90e89d62a65e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 14:21:45 -0500

    "mod: say super/same_woman/little_thing/look_government_by_eye/important_work/see_case/first_man.py at big fact"

---
+++ super/same_woman/little_thing/look_government_by_eye/important_work/see_case/first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_problem(str_arg):
+    go_case(str_arg)
+    print('eye')
+
+def go_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_problem('hand')
