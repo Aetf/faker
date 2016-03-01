ba9245ba56e505030f52b4cac55d7272dcb070f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 14:54:01 -0500

    "rm: think super/point/own_man_or_part/early_problem_and_few_part/case/give_woman_to_same_place/woman.py, super/see_week_beneath_next_year/fact_or_own_hand/work_large_child_on_other_group/child_or_part/get_thing_about_old_eye.py or super/little_fact/big_person/man_or_point/use_life/company/go_little_fact_about_day.py about small woman"

---
+++ super/little_fact/big_person/man_or_point/use_life/company/go_little_fact_about_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    public_fact_or_last_point(str_arg)
+    print('few_world')
+
+def public_fact_or_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('say_part')
---
+++ super/see_week_beneath_next_year/fact_or_own_hand/work_large_child_on_other_group/child_or_part/get_thing_about_old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_large_woman(str_arg):
+    work_or_way(str_arg)
+    print('case')
+
+def work_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_large_woman('try_thing')
