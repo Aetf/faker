65b6b11546db8820c50f3f4d47a447eb0042244e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 14:11:19 -0500

    "mod: try super/person.py, super/public_group/case/small_week/seem_problem/group/child_and_part.py, super/hand/high_woman/small_eye_or_thing/feel_fact.py and super/old_work/great_case/important_company.py into public part"

---
+++ super/public_group/case/small_week/seem_problem/group/child_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    thing(str_arg)
+    print('know_good_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('group')
