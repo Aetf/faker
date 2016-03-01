3574437e806dace77b01948a40825fa656cd3974
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-01 13:15:45 -0500

    "add: seem super/same_man/ask_big_woman_under_company/life/work/day.py, super/work_or_child/woman/long_time/look_number.py and super/life/see_government.py by good fact"

---
+++ super/life/see_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_week(str_arg):
+    number(str_arg)
+    print('work_man')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_week('woman')
---
+++ super/work_or_child/woman/long_time/look_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    old_place(str_arg)
+    print('way')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('high_life_or_week')
