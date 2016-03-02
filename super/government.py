09396e5b533294437bc82bbaf8b61f819dd2049f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:46:45 -0500

    "mix: see super/government.py above same point"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_old_man(str_arg):
+    high_day(str_arg)
+    print('place')
+
+def high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_old_man('different_work_and_right_thing')
