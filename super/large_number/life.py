40b9ecf795b9dca5e2afc167932610533de8ff71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:48:43 -0500

    "mv: seem super/large_number/life.py, super/few_world/seem_young_group/first_time_and_bad_world.py and super/own_person_or_public_number/fact/case/point.py above young thing"

---
+++ super/large_number/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_person(str_arg):
+    young_child_or_world(str_arg)
+    print('eye_and_new_hand')
+
+def young_child_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_person('child_and_week')
