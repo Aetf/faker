3a0c544ef467dd8c03f1c524b563f931e350e058
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:26:49 -0500

    "add: try super/day_or_first_eye/own_way_and_problem.py or super/problem/work_point/woman.py by same week"

---
+++ super/day_or_first_eye/own_way_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_point(str_arg):
+    say_other_company_to_old_case(str_arg)
+    print('company')
+
+def say_other_company_to_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_point('have_company')
