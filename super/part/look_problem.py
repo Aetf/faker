494d58c41a28e37829f18eb42df946db8deb0e0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 12:44:02 -0500

    "add: go super/go_fact/use_early_part.py and super/part/look_problem.py from old part"

---
+++ super/part/look_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_last_work(str_arg):
+    first_group_and_way(str_arg)
+    print('leave_case')
+
+def first_group_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_last_work('come_high_case')
