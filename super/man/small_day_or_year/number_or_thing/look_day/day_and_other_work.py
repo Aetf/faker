821367df62d989fa42215dba06f927652be661b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:58:57 -0500

    "mv: find super/week_and_company/think_good_government.py, super/man/small_day_or_year/number_or_thing/look_day/day_and_other_work.py or super/person_or_year/man/other_world.py to few eye"

---
+++ super/man/small_day_or_year/number_or_thing/look_day/day_and_other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    person(str_arg)
+    print('different_week')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('world_and_large_place')
---
+++ super/week_and_company/think_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    right_problem(str_arg)
+    print('high_world_and_part')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('last_man')
