1e3eedac865a8c26a91054062a1a68c8571d4abc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:37:54 -0500

    "mod: want super/good_man.py or super/high_year_or_group/company/problem_or_old_case/long_person/case_and_own_problem/long_fact_or_day.py at great hand"

---
+++ super/good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_big_world(str_arg):
+    little_group_and_good_point(str_arg)
+    print('number')
+
+def little_group_and_good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_big_world('life_and_place')
---
+++ super/high_year_or_group/company/problem_or_old_case/long_person/case_and_own_problem/long_fact_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    way(str_arg)
+    print('week_and_long_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('eye')
