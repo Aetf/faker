9cf5b3750f8b18340254cbd91261a904e89f9c35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 12:51:01 -0500

    "mv: do super/feel_important_thing_after_next_day/high_place_or_same_group/big_point_or_few_person/early_part.py from important way"

---
+++ super/feel_important_thing_after_next_day/high_place_or_same_group/big_point_or_few_person/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    other_place(str_arg)
+    print('man')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('time')
