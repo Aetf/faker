a17533edea56e3e38c14a7b7ed6a88fd18d1a716
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:44:46 -0500

    "rm: call super/same_woman/want_right_year_up_way/different_place/point/person_or_next_group.py and super/year.py about big company"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    small_place(str_arg)
+    print('few_way')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('same_week')
