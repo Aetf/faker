0f4991b47486395e65c384df73ce980d985b668a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 13:25:45 -0500

    "add: ask super/feel_case/same_woman_and_own_world/different_company_or_small_man.py, super/other_eye/way/bad_work/problem.py and super/ask_problem_by_old_part.py on old child"

---
+++ super/ask_problem_by_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    last_problem(str_arg)
+    print('important_work')
+
+def last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('day')
