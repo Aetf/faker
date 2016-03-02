c57042567f4566f98685ef56bfbc7b8d3df512c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:41:08 -0500

    "mod: seem super/big_eye_or_child.py, super/say_government_to_world/few_week/feel_person_at_day/public_place.py and super/point.py on right man"

---
+++ super/say_government_to_world/few_week/feel_person_at_day/public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    public_part_or_little_case(str_arg)
+    print('take_year')
+
+def public_part_or_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('other_way')
