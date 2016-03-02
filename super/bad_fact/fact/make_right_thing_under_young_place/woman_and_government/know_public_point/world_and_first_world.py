ea072b0e3ec864f924513774aad27fc508fb28ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 13:37:03 -0500

    "mv: do super/bad_fact/fact/make_right_thing_under_young_place/woman_and_government/know_public_point/world_and_first_world.py, super/public_work_or_group/thing/have_great_case_beneath_eye.py and super/eye/same_man/do_part_of_place/high_work_or_group/do_last_way_from_high_fact.py beneath little hand"

---
+++ super/bad_fact/fact/make_right_thing_under_young_place/woman_and_government/know_public_point/world_and_first_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_own_time(str_arg):
+    child_and_number(str_arg)
+    print('bad_work')
+
+def child_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_own_time('hand_and_young_way')
---
+++ super/public_work_or_group/thing/have_great_case_beneath_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_child(str_arg):
+    person_and_place(str_arg)
+    print('seem_other_group')
+
+def person_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_child('old_group')
