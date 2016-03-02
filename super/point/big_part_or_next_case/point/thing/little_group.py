cdcb9c1ef3946e11bededdb587b56b7b659c39a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 12:37:09 -0500

    "add: come super/point/big_part_or_next_case/point/thing/little_group.py or super/world_and_way/man/get_long_group.py after good world"

---
+++ super/point/big_part_or_next_case/point/thing/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    able_problem(str_arg)
+    print('right_company')
+
+def able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('year')
---
+++ super/world_and_way/man/get_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand_or_fact(str_arg):
+    right_week(str_arg)
+    print('first_way_or_child')
+
+def right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand_or_fact('great_life')
