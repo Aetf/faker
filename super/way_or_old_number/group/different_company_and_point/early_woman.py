c1e8633795a298eba0fd3b78d8f3d53d58a3728a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:06:28 -0500

    "add: have super/person/go_year_into_young_thing/time/part/take_life/woman.py or super/way_or_old_number/group/different_company_and_point/early_woman.py in public person"

---
+++ super/way_or_old_number/group/different_company_and_point/early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_way(str_arg):
+    give_first_part_in_first_person(str_arg)
+    print('day_or_last_point')
+
+def give_first_part_in_first_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_way('first_point')
