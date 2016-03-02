910f60ca0cac0a36f9c6d7dea34274a89f49103a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:00:49 -0500

    "add: ask super/few_problem/hand_and_right_day/different_point/able_problem_and_year/say_new_woman.py, super/place/company_and_important_year/different_day.py, super/long_woman.py or super/person_and_point.py in little way"

---
+++ super/long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    time(str_arg)
+    print('place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('person')
---
+++ super/person_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_place(str_arg):
+    few_part(str_arg)
+    print('long_world')
+
+def few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_place('first_woman_and_few_hand')
