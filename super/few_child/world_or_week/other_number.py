df9c0fbb86d11a7d3fa3780b3ec00e2eded85b56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:18:07 -0500

    "mod: come super/few_child/world_or_week/other_number.py, super/day/thing.py or super/way/world.py at good man"

---
+++ super/few_child/world_or_week/other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_way(str_arg):
+    world(str_arg)
+    print('say_own_child_by_small_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_way('first_week')
---
+++ super/way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    use_time_on_good_hand(str_arg)
+    print('part')
+
+def use_time_on_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('find_week')
