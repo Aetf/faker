037b6bacef9cc8079dac86a05a593888c7a5d088
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:32:30 -0500

    "rm: know super/leave_way_into_able_company/number/first_hand/know_next_thing_to_year.py and super/work_next_world/woman/bad_point_or_number/make_bad_way/man/be_place_in_first_person.py by able thing"

---
+++ super/leave_way_into_able_company/number/first_hand/know_next_thing_to_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point_or_old_government(str_arg):
+    time(str_arg)
+    print('see_government')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point_or_old_government('government')
---
+++ super/work_next_world/woman/bad_point_or_number/make_bad_way/man/be_place_in_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    last_hand(str_arg)
+    print('early_woman')
+
+def last_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('important_part_or_child')
