e7a03e2d8bad72149f10f91e7300caa880b719fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 14:45:38 -0500

    "mix: have super/place/person/important_life/old_week.py, super/world.py or super/new_government/world.py above young time"

---
+++ super/place/person/important_life/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_year_up_other_eye(str_arg):
+    first_number(str_arg)
+    print('year')
+
+def first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_year_up_other_eye('group')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_world(str_arg):
+    week(str_arg)
+    print('point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_world('day')
