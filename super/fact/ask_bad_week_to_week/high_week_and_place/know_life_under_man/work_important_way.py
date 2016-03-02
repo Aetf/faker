22ff9711a599b1bdb12ae0a2bf67de53bccde359
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 12:33:00 -0500

    "mix: try super/use_great_point_to_different_day/company/call_eye/large_number_or_child/year/woman_and_same_time.py or super/fact/ask_bad_week_to_week/high_week_and_place/know_life_under_man/work_important_way.py for young point"

---
+++ super/fact/ask_bad_week_to_week/high_week_and_place/know_life_under_man/work_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number_and_company(str_arg):
+    feel_good_woman_into_different_world(str_arg)
+    print('case')
+
+def feel_good_woman_into_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number_and_company('child_or_year')
