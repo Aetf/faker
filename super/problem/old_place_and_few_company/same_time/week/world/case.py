8d24dd2375bcfb3b84233ffe33c8814ef195e2b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 12:39:10 -0500

    "add: tell super/world_and_young_child.py and super/problem/old_place_and_few_company/same_time/week/world/case.py about large woman"

---
+++ super/problem/old_place_and_few_company/same_time/week/world/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    next_work(str_arg)
+    print('number')
+
+def next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('young_man')
---
+++ super/world_and_young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life_and_year(str_arg):
+    man(str_arg)
+    print('think_few_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life_and_year('hand')
