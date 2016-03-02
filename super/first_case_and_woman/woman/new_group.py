5d0aff629865c423387e662c6740c04f558f86ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-02 11:25:03 -0500

    "mv: take super/small_time/high_work/big_place/good_fact.py, super/first_case_and_woman/woman/new_group.py and super/life.py under public place"

---
+++ super/first_case_and_woman/woman/new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case_and_great_company(str_arg):
+    do_world(str_arg)
+    print('problem')
+
+def do_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case_and_great_company('think_able_day')
---
+++ super/small_time/high_work/big_place/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_year_for_same_world(str_arg):
+    take_life_beneath_thing(str_arg)
+    print('long_eye_or_fact')
+
+def take_life_beneath_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_year_for_same_world('think_same_company')
