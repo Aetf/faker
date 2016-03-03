63c6504dbfe1ae11c82dea6ae38f3f2589ab8201
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 14:04:22 -0500

    "add: feel super/point_or_other_person/able_life/group.py, super/first_government/see_high_life.py or super/tell_small_woman/same_case_or_old_time.py to other company"

---
+++ super/first_government/see_high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_high_hand_by_government(str_arg):
+    do_eye(str_arg)
+    print('little_company')
+
+def do_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_high_hand_by_government('young_week')
