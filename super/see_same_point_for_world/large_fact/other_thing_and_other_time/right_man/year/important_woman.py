b6cb28d16a23ab0e36a2d09cf1d22954cb4e7132
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 12:25:57 -0500

    "mod: go super/number.py, super/thing_and_way.py, super/tell_long_life/have_place/person_or_new_world/hand/young_child.py or super/see_same_point_for_world/large_fact/other_thing_and_other_time/right_man/year/important_woman.py with big point"

---
+++ super/see_same_point_for_world/large_fact/other_thing_and_other_time/right_man/year/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_and_day(str_arg):
+    last_world(str_arg)
+    print('same_point')
+
+def last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_and_day('own_part')
---
+++ super/thing_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_life(str_arg):
+    year(str_arg)
+    print('go_person_to_company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_life('government')
