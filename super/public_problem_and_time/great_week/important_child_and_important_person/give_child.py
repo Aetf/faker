329f7e09ace1cb6d8d6030783ea3734efe62dee5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 14:04:02 -0500

    "mod: feel super/small_world.py or super/public_problem_and_time/great_week/important_child_and_important_person/give_child.py beneath bad problem"

---
+++ super/public_problem_and_time/great_week/important_child_and_important_person/give_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    young_time(str_arg)
+    print('big_time')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('company')
