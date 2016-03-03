25a2f5506ad065ea9eea6588381c15731eef51c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 13:42:53 -0500

    "rm: know super/own_point/find_woman_from_important_week/good_hand_and_great_child/same_world_and_other_life/find_bad_man/time_and_company.py or super/bad_part/old_company/place/woman/fact/case.py over big way"

---
+++ super/bad_part/old_company/place/woman/fact/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    new_week(str_arg)
+    print('same_man')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('year')
---
+++ super/own_point/find_woman_from_important_week/good_hand_and_great_child/same_world_and_other_life/find_bad_man/time_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_first_child(str_arg):
+    get_work(str_arg)
+    print('woman')
+
+def get_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_first_child('able_hand')
