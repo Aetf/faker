3eafebac9c742d5312a1d5a7143e49f9eecddd0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 13:15:57 -0500

    "add: take super/day.py beneath early case"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    man(str_arg)
+    print('bad_hand')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('large_world_and_world')
