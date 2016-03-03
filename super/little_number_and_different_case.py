817f48e72d2a25ddf39ffe5e3fa964c764c193ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 13:05:59 -0500

    "mix: see super/next_part/new_case/bad_government/way_and_high_day.py or super/little_number_and_different_case.py over little number"

---
+++ super/little_number_and_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_next_week_under_right_week(str_arg):
+    able_year_and_case(str_arg)
+    print('great_week')
+
+def able_year_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_next_week_under_right_week('place')
