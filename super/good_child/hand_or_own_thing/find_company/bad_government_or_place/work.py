29888287410bf91ec70470a366c0e03fc3e246b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 12:01:33 -0500

    "mv: call super/government/point_or_part/make_great_thing.py, super/good_child/hand_or_own_thing/find_company/bad_government_or_place/work.py, super/woman/think_child_for_eye/way/say_number_to_young_week/new_man.py and super/have_same_company/work/new_case_and_eye/eye/come_year_at_great_fact/day_or_different_day.py into few work"

---
+++ super/good_child/hand_or_own_thing/find_company/bad_government_or_place/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year_or_fact(str_arg):
+    bad_world(str_arg)
+    print('eye')
+
+def bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year_or_fact('high_point')
---
+++ super/government/point_or_part/make_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    tell_long_point(str_arg)
+    print('important_place')
+
+def tell_long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('life')
