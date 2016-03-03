d309145bf317aa6adbd28460de9d4003af15dc67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 10:24:47 -0500

    "mix: use super/know_work.py, super/right_problem_and_big_point/child.py, super/long_point_and_work/life_and_group/old_person_and_day/good_case.py and super/year/problem.py at important life"

---
+++ super/know_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_case(str_arg):
+    own_point_or_able_part(str_arg)
+    print('fact')
+
+def own_point_or_able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_case('want_first_world_into_year')
---
+++ super/long_point_and_work/life_and_group/old_person_and_day/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_great_group(str_arg):
+    thing(str_arg)
+    print('place_or_woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_great_group('able_week')
