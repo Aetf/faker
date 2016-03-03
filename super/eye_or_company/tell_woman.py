d6c3408566635da70f9e8b138dedbcbd43f1fac7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 12:57:27 -0500

    "rm: make super/eye_or_company/tell_woman.py beneath early government"

---
+++ super/eye_or_company/tell_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time_or_eye(str_arg):
+    way(str_arg)
+    print('year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time_or_eye('seem_first_government')
