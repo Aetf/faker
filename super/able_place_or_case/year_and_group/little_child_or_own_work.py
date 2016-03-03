2859f9a82e9074b1cbe7d33ca374cfd16218926d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 11:10:57 -0500

    "rm: seem super/number/place/own_way/eye.py, super/good_day_or_year.py and super/able_place_or_case/year_and_group/little_child_or_own_work.py of few group"

---
+++ super/able_place_or_case/year_and_group/little_child_or_own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_other_part_over_bad_company(str_arg):
+    life_or_own_place(str_arg)
+    print('place')
+
+def life_or_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_other_part_over_bad_company('work')
