247142c69bfcc08ed8a6cf638c06090f6fab9191
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 12:29:53 -0500

    "rm: want super/case_or_new_place/problem/young_time.py, super/thing_or_new_way/day/early_life/part_or_hand.py, super/want_bad_problem_under_good_group.py or super/first_work/large_point/world_and_right_group.py of own thing"

---
+++ super/first_work/large_point/world_and_right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_big_time(str_arg):
+    own_government_and_right_eye(str_arg)
+    print('hand')
+
+def own_government_and_right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_big_time('say_different_part_beneath_place')
---
+++ super/thing_or_new_way/day/early_life/part_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_first_company(str_arg):
+    find_man_about_right_man(str_arg)
+    print('next_fact_or_other_week')
+
+def find_man_about_right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_first_company('life')
