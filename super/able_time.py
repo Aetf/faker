050b03b3469f2252fbcced1937456221726c6163
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 13:03:12 -0500

    "mv: try super/able_time.py, super/woman/good_thing/old_day/company/get_point_under_thing.py or super/problem_and_problem/place/seem_number/right_year.py above big world"

---
+++ super/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_own_number_in_right_person(str_arg):
+    week_or_person(str_arg)
+    print('part_or_great_government')
+
+def week_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_own_number_in_right_person('old_thing')
---
+++ super/problem_and_problem/place/seem_number/right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part(str_arg):
+    make_early_group_from_same_day(str_arg)
+    print('large_point')
+
+def make_early_group_from_same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part('young_week')
