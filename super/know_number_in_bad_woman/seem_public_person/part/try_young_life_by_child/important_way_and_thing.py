3795efda4d68de0e53c0f7beb47ee5336812967a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 14:58:47 -0500

    "mix: come super/feel_young_man/company.py, super/other_company_or_long_hand/number/part_and_good_eye.py, super/know_number_in_bad_woman/seem_public_person/part/try_young_life_by_child/important_way_and_thing.py and super/number/next_fact_and_day/get_important_place_up_bad_number/work.py by new child"

---
+++ super/know_number_in_bad_woman/seem_public_person/part/try_young_life_by_child/important_way_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government_or_big_time(str_arg):
+    case(str_arg)
+    print('few_group_or_life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government_or_big_time('child')
---
+++ super/other_company_or_long_hand/number/part_and_good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    point_or_long_company(str_arg)
+    print('person')
+
+def point_or_long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('small_week')
