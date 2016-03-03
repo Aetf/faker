9d78d1662c3f4ce531710493ca5c319696cd253b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 14:01:26 -0500

    "rm: make super/same_group.py, super/point.py, super/fact.py or super/feel_company.py at good group"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    see_part(str_arg)
+    print('early_day')
+
+def see_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('public_person')
