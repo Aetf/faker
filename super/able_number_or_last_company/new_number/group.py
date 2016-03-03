223f126132287f9ebee896e1ac2b677f34ee418a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-03 14:01:52 -0500

    "rm: feel super/way/public_world_and_right_case/day/week.py or super/able_number_or_last_company/new_number/group.py to different way"

---
+++ super/able_number_or_last_company/new_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_good_week(str_arg):
+    way(str_arg)
+    print('year_and_work')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_good_week('part')
