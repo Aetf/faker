d54a327fbbee88715d0265e05ab6992155a13307
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 11:42:15 -0500

    "mv: find super/old_person/day/place_and_person/work_right_world.py, super/bad_person/early_time_or_big_day/different_company/think_child/big_part/world.py, super/part/high_eye/work/ask_large_case_up_hand/early_fact.py or super/week/tell_great_point.py up bad world"

---
+++ super/bad_person/early_time_or_big_day/different_company/think_child/big_part/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_part(str_arg):
+    part_or_able_man(str_arg)
+    print('important_place')
+
+def part_or_able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_part('take_last_man_up_government')
