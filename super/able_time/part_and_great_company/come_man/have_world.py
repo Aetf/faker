95ccf86515ed4bd9e7c77e95a54cfdfa1eae7abb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 12:20:36 -0500

    "mod: see super/able_time/part_and_great_company/come_man/have_world.py or super/new_man_and_early_week/point/feel_point/right_hand.py about young day"

---
+++ super/able_time/part_and_great_company/come_man/have_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    government(str_arg)
+    print('good_group_or_other_government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('woman')
