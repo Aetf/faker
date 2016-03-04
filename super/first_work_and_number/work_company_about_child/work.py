0882f81018232bcd2a7642e77b08d0bb224ffc2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 11:37:15 -0500

    "rm: say super/go_bad_way_after_number/small_government_or_woman/old_life.py, super/first_work_and_number/work_company_about_child/work.py and super/child_or_first_eye/few_company/work_life/work/time.py beneath old hand"

---
+++ super/first_work_and_number/work_company_about_child/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_big_group_of_early_man(str_arg):
+    old_life(str_arg)
+    print('week')
+
+def old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_big_group_of_early_man('have_small_child_with_new_company')
