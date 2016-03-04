31971c8be2739851bd44c9ad7e744300f1ce5f8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:27:58 -0500

    "mod: get super/work/week/able_government/right_world/say_time_about_point/old_child.py or super/last_thing.py beneath young day"

---
+++ super/last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_place_with_day(str_arg):
+    hand(str_arg)
+    print('right_thing')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_place_with_day('place')
