3ba30b3aeabbaacc2aabdc2e7f539e7ef0da30da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 10:22:05 -0500

    "rm: use super/give_early_child_over_few_number/important_day_or_large_life/government/child/public_year/company.py or super/different_thing_or_place/be_life/time.py under old problem"

---
+++ super/different_thing_or_place/be_life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    leave_same_point(str_arg)
+    print('different_woman')
+
+def leave_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('first_life_and_good_case')
