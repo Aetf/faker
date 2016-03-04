f1d1bab956f366d09299e830d51e24f634739492
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 14:15:04 -0500

    "add: call super/part/own_number_or_place/great_government/fact/public_way/able_fact.py about early way"

---
+++ super/part/own_number_or_place/great_government/fact/public_way/able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company_and_eye(str_arg):
+    point(str_arg)
+    print('great_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company_and_eye('large_case_and_high_part')
