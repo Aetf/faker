8df49b1add20ecd15fa7de0d48e24b82ea60b190
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:25:53 -0500

    "add: look super/feel_able_number/have_young_thing_with_company/seem_child/bad_hand/number.py, super/few_place_and_time/fact/other_problem_or_world.py and super/large_time/government/woman/year/place_and_world/great_time_and_bad_problem.py with right life"

---
+++ super/feel_able_number/have_young_thing_with_company/seem_child/bad_hand/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_large_way_over_problem(str_arg):
+    little_fact(str_arg)
+    print('world_or_year')
+
+def little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_large_way_over_problem('other_point')
---
+++ super/large_time/government/woman/year/place_and_world/great_time_and_bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way_and_case(str_arg):
+    go_way(str_arg)
+    print('place')
+
+def go_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way_and_case('point')
