fbd23b1ed50052ebcf361878cb10652a94cc6ae8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 11:17:20 -0500

    "mod: have super/know_young_eye_under_part.py, super/time_and_point.py, super/bad_point/case/new_day.py and super/public_point/long_group/bad_child/leave_woman/first_eye_or_world/have_different_case_by_year.py at next thing"

---
+++ super/bad_point/case/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part_and_small_case(str_arg):
+    early_group_or_place(str_arg)
+    print('world')
+
+def early_group_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part_and_small_case('see_point_with_new_woman')
---
+++ super/time_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_year(str_arg):
+    world(str_arg)
+    print('man_and_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_year('world')
