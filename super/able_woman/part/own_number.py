b3410c7b7232aa652f8a789864fcfc00f1f8dd95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:43:58 -0500

    "add: give super/big_case/number_and_eye.py, super/able_woman/part/own_number.py and super/hand/take_different_hand/time.py over first hand"

---
+++ super/able_woman/part/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_fact(str_arg):
+    problem(str_arg)
+    print('time')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_fact('able_time')
---
+++ super/hand/take_different_hand/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_last_hand(str_arg):
+    public_time_and_big_fact(str_arg)
+    print('point')
+
+def public_time_and_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_last_hand('great_hand')
