4150e3d329b8b6a367653749b7ce8158c075533a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:34:26 -0500

    "rm: think super/think_case_of_hand/public_eye_and_thing/hand/part/place.py at public week"

---
+++ super/think_case_of_hand/public_eye_and_thing/hand/part/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman_and_same_man(str_arg):
+    young_man(str_arg)
+    print('hand')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman_and_same_man('take_case_over_last_number')
