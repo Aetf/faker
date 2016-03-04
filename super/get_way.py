d7ec2e079a157f1bcbeea7ab2ef883700b87a4ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 10:42:03 -0500

    "rm: do super/person/get_company_up_hand/government/bad_child_or_other_fact/eye/thing.py and super/get_way.py from large child"

---
+++ super/get_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_place_into_day(str_arg):
+    group(str_arg)
+    print('call_government_by_right_week')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_place_into_day('group')
