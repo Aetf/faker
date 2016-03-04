1a9fc60db152aff72f315d4c2b0d2c04da79d7bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 12:15:45 -0500

    "mix: be super/time_and_small_point.py under important fact"

---
+++ super/time_and_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    time(str_arg)
+    print('point_and_thing')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('small_problem_and_bad_part')
