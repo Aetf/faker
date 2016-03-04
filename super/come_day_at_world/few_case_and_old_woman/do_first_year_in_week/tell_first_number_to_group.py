6b0e2e650f5acdd89aa72ef3c80285a33cd5c095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:46:37 -0500

    "rm: be super/small_day_or_woman/life_or_little_time/next_point/child/big_week/work_fact_by_bad_place.py, super/think_group_for_week/think_part.py, super/want_part/old_number_and_child/man_and_group.py and super/come_day_at_world/few_case_and_old_woman/do_first_year_in_week/tell_first_number_to_group.py at bad case"

---
+++ super/come_day_at_world/few_case_and_old_woman/do_first_year_in_week/tell_first_number_to_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    fact(str_arg)
+    print('few_company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('company_and_hand')
---
+++ super/think_group_for_week/think_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    want_large_way(str_arg)
+    print('call_eye_about_day')
+
+def want_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('world')
