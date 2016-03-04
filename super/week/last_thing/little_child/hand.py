5923255a7b4f83240d134fb2e0495dbf0942a6b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 10:25:38 -0500

    "mod: say super/week/last_thing/little_child/hand.py with old government"

---
+++ super/week/last_thing/little_child/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    same_group(str_arg)
+    print('early_world_or_bad_eye')
+
+def same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('time_or_way')
