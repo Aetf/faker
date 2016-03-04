6cb8423ee50f8f106e6e2925597047df4bb464b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 10:02:53 -0500

    "rm: ask super/young_time/time/woman_and_woman/eye_or_other_problem/big_way.py, super/last_government/man_and_young_year/use_same_year_to_own_fact/point.py and super/high_place/say_early_company.py after different government"

---
+++ super/young_time/time/woman_and_woman/eye_or_other_problem/big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life(str_arg):
+    few_person_and_man(str_arg)
+    print('company')
+
+def few_person_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life('time')
