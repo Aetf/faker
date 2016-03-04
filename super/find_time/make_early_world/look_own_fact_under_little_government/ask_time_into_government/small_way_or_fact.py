073c7d1ccd2c3f37c24217f526ff056409274eb8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 14:01:47 -0500

    "mix: know super/find_time/make_early_world/look_own_fact_under_little_government/ask_time_into_government/small_way_or_fact.py or super/woman_or_part/man/eye_or_hand.py beneath little child"

---
+++ super/find_time/make_early_world/look_own_fact_under_little_government/ask_time_into_government/small_way_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_early_point_to_world(str_arg):
+    use_child(str_arg)
+    print('old_point_or_woman')
+
+def use_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_early_point_to_world('week')
---
+++ super/woman_or_part/man/eye_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_great_day(str_arg):
+    other_part_and_thing(str_arg)
+    print('look_early_part_into_high_government')
+
+def other_part_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_great_day('case')
