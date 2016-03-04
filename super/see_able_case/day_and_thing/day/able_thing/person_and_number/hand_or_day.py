884e1dac8716387b84ef7276bd48040ceb7f075b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 13:37:56 -0500

    "mod: use super/long_group/take_small_company/feel_fact_of_right_government/big_child_and_day/man.py and super/see_able_case/day_and_thing/day/able_thing/person_and_number/hand_or_day.py beneath next problem"

---
+++ super/see_able_case/day_and_thing/day/able_thing/person_and_number/hand_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    great_man(str_arg)
+    print('person')
+
+def great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('high_day_or_fact')
