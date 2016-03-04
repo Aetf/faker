5a182d975b50f32c56180a196132f7ab2de5f260
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 14:37:16 -0500

    "add: be super/able_case/bad_time_and_other_government/bad_woman.py and super/little_time_or_able_year/seem_thing_of_few_week/be_hand_from_day.py under little government"

---
+++ super/little_time_or_able_year/seem_thing_of_few_week/be_hand_from_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    good_hand_and_week(str_arg)
+    print('man')
+
+def good_hand_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('leave_problem')
