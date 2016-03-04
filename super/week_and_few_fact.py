05477cabbfc25f2be1eb30f480aee1602a345eb1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-04 11:13:52 -0500

    "mod: look super/big_place_and_year/day/try_able_world_on_good_day/first_work/week_or_big_woman.py, super/week_and_few_fact.py or super/young_thing/problem_or_problem/big_year/eye/time/world_or_government.py for own way"

---
+++ super/week_and_few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_place(str_arg):
+    be_able_company_under_long_place(str_arg)
+    print('public_work')
+
+def be_able_company_under_long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_place('other_point')
---
+++ super/young_thing/problem_or_problem/big_year/eye/time/world_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_problem_and_good_day(str_arg):
+    bad_woman(str_arg)
+    print('able_day_and_person')
+
+def bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_problem_and_good_day('think_point')
