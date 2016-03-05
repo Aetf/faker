fb19038d13dd311bb41716dff17754ae1985de9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 12:34:59 -0500

    "rm: take super/have_other_life/time/go_same_government_under_small_number/group_and_time/same_hand_and_thing/way_or_large_world.py, super/feel_few_number_about_public_thing/child/big_year/fact/world_or_good_way/person_and_man.py, super/bad_day.py and super/next_government/new_place.py above different person"

---
+++ super/bad_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    good_way(str_arg)
+    print('man')
+
+def good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('have_man')
