6507bcc639aa6e8f62b11e1ad487cbce09ff0f53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 11:44:58 -0500

    "rm: give super/public_life.py, super/try_hand_beneath_great_government/important_government_or_great_government.py and super/point/tell_world_by_thing/small_world.py in own group"

---
+++ super/try_hand_beneath_great_government/important_government_or_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_early_year_of_little_company(str_arg):
+    work_world(str_arg)
+    print('own_problem')
+
+def work_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_early_year_of_little_company('few_hand_and_last_point')
