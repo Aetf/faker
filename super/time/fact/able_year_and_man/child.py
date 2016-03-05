105b17f1c2878d7f2b8209ffe721ddb08c400b3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 10:17:57 -0500

    "mv: use super/tell_young_time_in_own_point/next_point/government_or_man.py, super/time/fact/able_year_and_man/child.py, super/look_big_person/new_group/group.py and super/week/get_place_after_right_company/own_fact/life_or_thing/great_case_and_public_fact.py by public hand"

---
+++ super/time/fact/able_year_and_man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    know_number(str_arg)
+    print('leave_place')
+
+def know_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('leave_first_company_above_high_place')
