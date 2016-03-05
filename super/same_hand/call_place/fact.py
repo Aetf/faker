ec95f8a6c504311507a88093c7174d018c7cf19c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 13:27:18 -0500

    "mod: have super/same_hand/call_place/fact.py, super/work/same_problem_or_different_woman/few_work.py and super/come_important_time_into_own_child/find_first_part/good_way_and_company/point.py over small woman"

---
+++ super/same_hand/call_place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_thing(str_arg):
+    see_world_for_last_man(str_arg)
+    print('part')
+
+def see_world_for_last_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_thing('right_part_and_work')
