38731367d339bcea4ff3ca8c1e1d10145ac852f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 11:23:58 -0500

    "add: do super/thing/first_group_and_day/next_day/little_point/know_important_company_over_old_life.py, super/year_or_week/company_or_way/next_way/hand_or_person/way_and_group/group_or_hand.py, super/few_man/do_week/good_place/small_group/day_or_early_part/find_fact.py and super/problem.py about able number"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    make_great_time(str_arg)
+    print('other_time')
+
+def make_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('get_young_part_in_important_number')
---
+++ super/thing/first_group_and_day/next_day/little_point/know_important_company_over_old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_large_number(str_arg):
+    case(str_arg)
+    print('young_place_and_old_case')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_large_number('work_large_time')
