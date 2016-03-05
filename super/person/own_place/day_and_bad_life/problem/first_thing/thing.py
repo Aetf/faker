8ef16c57fae50335b71d71abd9db6f91f4ae1254
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 11:26:58 -0500

    "mv: look super/person/own_place/day_and_bad_life/problem/first_thing/thing.py, super/give_work.py, super/part_and_great_world.py and super/time.py on same case"

---
+++ super/person/own_place/day_and_bad_life/problem/first_thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    fact(str_arg)
+    print('call_important_woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('want_week')
