32ef161592866c3bc6b45f2dcf0fc1bf940d08fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 13:45:12 -0500

    "mix: call super/few_year/good_problem.py, super/part/young_group.py and super/try_hand/life_or_good_point/large_day.py after public day"

---
+++ super/few_year/good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    part(str_arg)
+    print('look_time_at_right_part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('good_place')
---
+++ super/try_hand/life_or_good_point/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    other_year(str_arg)
+    print('ask_day_above_government')
+
+def other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('see_part')
