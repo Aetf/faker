5fe6e399ca42041455d7c86a42caf4fe0f2c74aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 14:13:21 -0500

    "mix: say super/woman/high_hand.py and super/fact/small_thing/problem/own_thing_and_few_year/week.py into new thing"

---
+++ super/woman/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_number(str_arg):
+    child_and_next_number(str_arg)
+    print('work')
+
+def child_and_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_number('do_large_hand')
