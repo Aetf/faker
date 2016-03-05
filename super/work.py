c61a088577b8cd6d954a906720d534debf1de61c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 11:29:13 -0500

    "mod: find super/know_right_child_of_bad_child/life/new_woman/same_place/world.py or super/work.py after able year"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    same_problem(str_arg)
+    print('bad_way')
+
+def same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('right_point_and_last_part')
