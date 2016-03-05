f670db14dc2b120c00ba7d9ee3e256167c47a1c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 10:07:45 -0500

    "mix: try super/able_eye.py to last point"

---
+++ super/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    public_point(str_arg)
+    print('high_place')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('fact')
