30f4b99e2e815ccb55e5e2cb334423388df55629
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 12:49:59 -0500

    "mix: give super/good_way_or_new_company/world_and_little_woman/world/public_problem.py, super/next_hand_or_important_part.py or super/work_or_time.py under little thing"

---
+++ super/next_hand_or_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_able_life_beneath_case(str_arg):
+    old_life(str_arg)
+    print('way')
+
+def old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_able_life_beneath_case('make_way')
---
+++ super/work_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_time_by_few_company(str_arg):
+    way_and_woman(str_arg)
+    print('leave_young_part')
+
+def way_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_time_by_few_company('want_old_company_with_thing')
