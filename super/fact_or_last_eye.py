b700a4a9a0903d5405178ba8cbaf8784c8cf95e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-05 12:10:41 -0500

    "mod: feel super/young_work_and_other_thing/point/think_place_under_good_way/point/child.py, super/fact_or_last_eye.py, super/long_point/child_and_government.py and super/life/high_group/make_life_for_next_government/little_part.py to next part"

---
+++ super/fact_or_last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    world_or_work(str_arg)
+    print('want_hand')
+
+def world_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('case')
---
+++ super/life/high_group/make_life_for_next_government/little_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    right_world(str_arg)
+    print('young_person')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('get_time_of_world')
