05d103666fd64b4205aa8a10434eca063149a950
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 14:48:13 -0500

    "add: see super/way/day_or_person/eye.py, super/next_week/take_world_above_different_thing/right_company_or_government/try_place.py or super/little_week/thing_or_woman/new_time/old_child.py with own group"

---
+++ super/next_week/take_world_above_different_thing/right_company_or_government/try_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point_or_way(str_arg):
+    fact_and_thing(str_arg)
+    print('big_child_or_world')
+
+def fact_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point_or_way('see_work_by_case')
---
+++ super/way/day_or_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    way_or_great_world(str_arg)
+    print('own_fact')
+
+def way_or_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('number')
