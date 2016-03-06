d5e545b928621a5ea060e001d6e30d80d0bb97c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 14:40:05 -0500

    "mod: ask super/able_woman/other_person_or_own_time/give_great_day_by_high_year/large_number/try_large_part.py and super/child/number/long_fact/first_child_and_little_problem.py on big eye"

---
+++ super/able_woman/other_person_or_own_time/give_great_day_by_high_year/large_number/try_large_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_world(str_arg):
+    young_place(str_arg)
+    print('few_government')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_world('problem_or_same_eye')
---
+++ super/child/number/long_fact/first_child_and_little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year_and_week(str_arg):
+    new_problem(str_arg)
+    print('good_fact')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year_and_week('have_good_way')
