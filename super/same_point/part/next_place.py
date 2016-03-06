7b59892c576d7fc48c04b2522138748d7547bb9b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:43:36 -0500

    "rm: feel super/same_point/part/next_place.py, super/week/different_world_and_world.py and super/make_child_on_case/try_small_life_for_person/small_case/problem/place_and_work/new_problem.py from able woman"

---
+++ super/same_point/part/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    year_or_public_company(str_arg)
+    print('important_part_or_government')
+
+def year_or_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('government')
---
+++ super/week/different_world_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye_and_company(str_arg):
+    woman(str_arg)
+    print('place_or_man')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye_and_company('important_year')
