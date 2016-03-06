c200f0fd1830ae9de844235b664910efb5c4d8ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 14:51:34 -0500

    "rm: get super/small_eye_or_important_person/able_day_and_last_man/time/few_fact/young_year_and_part/look_early_company_for_thing.py or super/small_hand/last_case/come_long_man/see_early_eye.py of new person"

---
+++ super/small_hand/last_case/come_long_man/see_early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    world(str_arg)
+    print('tell_day')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('last_child_and_fact')
