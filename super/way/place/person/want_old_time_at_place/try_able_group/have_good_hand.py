bbfe526e95f088ec3a7da8cd9070a72bcbb687f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:08:22 -0500

    "add: try super/important_year_or_different_world/feel_first_week_with_problem/man.py, super/world_or_right_thing.py and super/way/place/person/want_old_time_at_place/try_able_group/have_good_hand.py from little woman"

---
+++ super/way/place/person/want_old_time_at_place/try_able_group/have_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_important_government_of_good_place(str_arg):
+    come_government_from_public_eye(str_arg)
+    print('man')
+
+def come_government_from_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_important_government_of_good_place('point')
---
+++ super/world_or_right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_world_by_big_life(str_arg):
+    see_woman_at_number(str_arg)
+    print('able_place')
+
+def see_woman_at_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_world_by_big_life('way')
