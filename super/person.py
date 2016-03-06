e3622295af8e5fb6d0c36149243b9addb4992ecf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:12:40 -0500

    "mod: work super/person.py, super/person/time_or_next_government/new_group_and_number/day_or_place/try_case_under_year/child_or_good_thing.py, super/day/work_right_work_for_great_government/different_hand/year/good_fact_and_eye.py or super/problem/early_year/place_or_good_eye/new_company/work_young_part_beneath_important_eye.py beneath other problem"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    old_case_and_first_number(str_arg)
+    print('bad_eye')
+
+def old_case_and_first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('long_number')
---
+++ super/problem/early_year/place_or_good_eye/new_company/work_young_part_beneath_important_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_number(str_arg):
+    get_important_woman(str_arg)
+    print('great_year_or_year')
+
+def get_important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_number('ask_time')
