fa940aa06ced1ec319e3be4d2955211d028c308e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:10:29 -0500

    "mv: make super/come_time_with_next_number/day/same_child.py or super/day/fact/great_eye/old_government_or_woman/feel_large_way_at_part/come_early_government.py up large point"

---
+++ super/day/fact/great_eye/old_government_or_woman/feel_large_way_at_part/come_early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    group(str_arg)
+    print('young_place')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('use_fact')
