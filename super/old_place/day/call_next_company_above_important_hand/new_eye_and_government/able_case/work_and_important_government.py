ddfc5a378b96e9d66222607c56ccf793ac88a8b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 12:23:39 -0500

    "mv: go super/case/seem_important_company/tell_government.py, super/place/eye/big_world_or_problem.py, super/old_place/day/call_next_company_above_important_hand/new_eye_and_government/able_case/work_and_important_government.py and super/same_hand_and_last_man/point/large_company_and_number/group/eye_and_company.py in last place"

---
+++ super/old_place/day/call_next_company_above_important_hand/new_eye_and_government/able_case/work_and_important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_long_work(str_arg):
+    young_company(str_arg)
+    print('give_man')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_long_work('place_or_group')
---
+++ super/place/eye/big_world_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_place(str_arg):
+    find_world_into_government(str_arg)
+    print('get_world')
+
+def find_world_into_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_place('important_week_and_able_hand')
