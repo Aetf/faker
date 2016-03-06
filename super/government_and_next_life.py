3f6695e43f142049a5e76598c3f189ca814f2d2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 11:35:34 -0500

    "mv: get super/problem/different_way_or_place/think_able_hand_after_last_way/part.py, super/want_day/point_and_long_world/person.py or super/government_and_next_life.py after own government"

---
+++ super/government_and_next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_number(str_arg):
+    number(str_arg)
+    print('long_company_or_work')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_number('early_year')
---
+++ super/problem/different_way_or_place/think_able_hand_after_last_way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    day(str_arg)
+    print('work_way_from_fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('big_year')
