29d1947be742a90b06bdfafdc06f15a2c2efe603
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 14:45:42 -0500

    "mod: go super/old_way.py for other case"

---
+++ super/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_place_or_first_life(str_arg):
+    fact_or_last_fact(str_arg)
+    print('day')
+
+def fact_or_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_place_or_first_life('same_place')
