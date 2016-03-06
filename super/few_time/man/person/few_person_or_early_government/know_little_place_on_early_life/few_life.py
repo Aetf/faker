b3f5dc6475c56f27ad79cc427fbf15000d034673
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 11:43:10 -0500

    "mix: be super/few_time/man/person/few_person_or_early_government/know_little_place_on_early_life/few_life.py, super/tell_government/place/life/man_or_big_way.py, super/early_part/make_last_world/other_case_or_high_woman/next_child_or_different_week.py or super/long_week/woman_and_same_week.py in few case"

---
+++ super/few_time/man/person/few_person_or_early_government/know_little_place_on_early_life/few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    man(str_arg)
+    print('time_or_hand')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('tell_different_work_in_week')
--- super/long_week/woman_and_same_week.py
+++ super/long_week/woman_and_same_week.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def point_or_number(str_arg):
+def good_group(str_arg):
     number(str_arg)
-    print('long_company_or_work')
+    print('long_way')
 
 def number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    point_or_number('early_year')
+    good_group('eye_and_year')
