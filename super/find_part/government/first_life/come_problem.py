0f39e9a033a6023e67f03d4546bb5df2c93583f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:37:50 -0500

    "mv: look super/find_part/government/first_life/come_problem.py, super/last_day/week/year_and_important_problem.py or super/old_person.py about first problem"

---
+++ super/find_part/government/first_life/come_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    make_public_number(str_arg)
+    print('case_or_young_part')
+
+def make_public_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('year')
---
+++ super/old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week_or_way(str_arg):
+    part(str_arg)
+    print('big_day_and_able_week')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week_or_way('able_week')
