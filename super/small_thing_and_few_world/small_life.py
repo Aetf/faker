7175d13a4e593eed77f76c3fc1e56caac2a753a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:59:24 -0500

    "rm: ask super/small_thing_and_few_world/small_life.py with bad woman"

---
+++ super/small_thing_and_few_world/small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    work_work(str_arg)
+    print('new_hand')
+
+def work_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('want_problem_about_same_thing')
