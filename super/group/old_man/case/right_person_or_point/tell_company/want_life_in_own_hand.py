50f2c97f3c1155c9173ea230898d2f42d919f916
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-06 10:18:44 -0500

    "add: be super/different_year_and_case.py, super/young_problem/bad_person/year_and_group/work.py, super/little_time/know_different_point_with_time/woman.py and super/group/old_man/case/right_person_or_point/tell_company/want_life_in_own_hand.py over bad person"

---
+++ super/group/old_man/case/right_person_or_point/tell_company/want_life_in_own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    year(str_arg)
+    print('first_point_and_first_company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('thing')
