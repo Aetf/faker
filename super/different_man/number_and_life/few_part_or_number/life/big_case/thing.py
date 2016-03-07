17a3d95ee43bda1c00c4dcd7cad579c352fadb75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 10:15:11 -0500

    "add: want super/different_man/number_and_life/few_part_or_number/life/big_case/thing.py for early person"

---
+++ super/different_man/number_and_life/few_part_or_number/life/big_case/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    part_or_important_person(str_arg)
+    print('important_thing_and_great_company')
+
+def part_or_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('part')
