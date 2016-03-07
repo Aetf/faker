8ee5af014102bfdf091f340c33038e97307e9ac1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 10:41:11 -0500

    "mix: look super/feel_long_world_beneath_fact/week_and_next_life/place.py above other group"

---
+++ super/feel_long_world_beneath_fact/week_and_next_life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work_or_case(str_arg):
+    year(str_arg)
+    print('go_right_thing_above_few_problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work_or_case('right_day')
