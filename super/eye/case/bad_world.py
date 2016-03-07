98bfb65f25a8a3017e23fdb2f03f9d5cce669322
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 10:28:33 -0500

    "mv: have super/little_group_or_same_year/week/part_or_few_fact/number_or_person.py, super/time/day/say_hand_after_other_person/good_week_and_small_problem/little_group/long_world.py, super/eye/case/bad_world.py and super/last_group_and_part/get_company/hand/time_or_thing/think_public_person_into_able_point.py over other case"

---
+++ super/eye/case/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    way(str_arg)
+    print('old_group')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('high_life')
---
+++ super/time/day/say_hand_after_other_person/good_week_and_small_problem/little_group/long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    seem_number_to_good_week(str_arg)
+    print('good_hand')
+
+def seem_number_to_good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('next_world')
