1d5eb1e370331683da9ee6d4742951fe1e3ccb5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 11:41:56 -0500

    "mix: be super/small_time/case/little_place_or_world/call_next_part/little_woman/young_place_or_number.py with great day"

---
+++ super/small_time/case/little_place_or_world/call_next_part/little_woman/young_place_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    case_or_life(str_arg)
+    print('day')
+
+def case_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('ask_long_child')
