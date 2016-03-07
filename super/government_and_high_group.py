a2d5c90542662a86e68f49e90fa6b7fb1ad7016d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:18:36 -0500

    "mix: tell super/point_and_long_place.py or super/government_and_high_group.py up bad work"

---
+++ super/government_and_high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    use_last_eye(str_arg)
+    print('large_work')
+
+def use_last_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('have_important_part_at_early_time')
