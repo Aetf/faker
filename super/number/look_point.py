089708fa7df7412aa0b4a2725b074734a2a335fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:44:46 -0500

    "mv: ask super/fact_and_hand/take_different_number_about_eye/small_hand/give_way/come_child.py, super/place/public_place/new_time/small_number/high_government.py and super/number/look_point.py in last part"

---
+++ super/number/look_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    try_life_beneath_right_person(str_arg)
+    print('young_woman_and_company')
+
+def try_life_beneath_right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('same_work')
---
+++ super/place/public_place/new_time/small_number/high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    leave_week_beneath_eye(str_arg)
+    print('big_part')
+
+def leave_week_beneath_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('life_and_hand')
