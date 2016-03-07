35bb2e375004af4aa597002c7590541beaa0572f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 13:10:35 -0500

    "mod: seem super/large_week/year.py, super/number/world_or_woman/find_woman/high_woman.py, super/able_government/child/long_time.py or super/long_number/fact_and_government.py for little company"

---
+++ super/long_number/fact_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    year(str_arg)
+    print('big_day_or_world')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('different_point')
---
+++ super/number/world_or_woman/find_woman/high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_or_life(str_arg):
+    woman(str_arg)
+    print('woman_and_year')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_or_life('government_and_long_world')
