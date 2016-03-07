2bd556b3a648cd4f27387c8a9e215af3b5d90f18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:23:47 -0500

    "mod: ask super/case/look_large_child_for_eye.py under early time"

---
+++ super/case/look_large_child_for_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    important_week_or_hand(str_arg)
+    print('see_next_life')
+
+def important_week_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('able_man')
