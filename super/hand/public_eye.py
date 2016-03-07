d0e96a339b074f4452226ad334c898ee19a38cc5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:14:06 -0500

    "mod: know super/hand/public_eye.py or super/know_bad_work/able_life_and_fact/part/way/look_same_year_by_public_point.py from old way"

---
+++ super/hand/public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_few_thing_about_right_point(str_arg):
+    bad_time_or_own_case(str_arg)
+    print('know_work_over_person')
+
+def bad_time_or_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_few_thing_about_right_point('little_man_and_week')
