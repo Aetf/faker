bc409229f222dc456f20e29b391c6c699145250f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:42:15 -0500

    "add: leave super/large_life_and_woman/time_and_number/right_work/early_group/day_and_case/say_able_child.py, super/problem/eye/come_bad_number_into_number/find_last_part_in_important_child/way/next_number.py and super/case/come_day/person_or_way/point_and_long_place.py up able thing"

---
+++ super/problem/eye/come_bad_number_into_number/find_last_part_in_important_child/way/next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye_and_few_week(str_arg):
+    company(str_arg)
+    print('want_fact_by_hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye_and_few_week('early_point')
