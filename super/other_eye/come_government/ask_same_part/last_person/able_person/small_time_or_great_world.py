aa5de70e5e54198a92cb58b15a55cacdc8d8b38f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:25:43 -0500

    "mix: seem super/other_eye/come_government/ask_same_part/last_person/able_person/small_time_or_great_world.py of good week"

---
+++ super/other_eye/come_government/ask_same_part/last_person/able_person/small_time_or_great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    work(str_arg)
+    print('do_week_at_great_work')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('same_group_or_week')
