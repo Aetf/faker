7d49376a8d8cc2498f4e8a1bc286c1f44ab5edf0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 14:39:49 -0500

    "mv: see super/day.py by able eye"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_child(str_arg):
+    say_hand_at_young_way(str_arg)
+    print('government_or_fact')
+
+def say_hand_at_young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_child('call_hand_of_part')
