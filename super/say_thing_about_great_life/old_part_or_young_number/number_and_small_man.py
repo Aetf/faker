27a4387647f2b86d1d9a13b1c71d591983f19586
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:30:42 -0500

    "rm: find super/case_or_first_man/know_first_hand_beneath_group/work.py, super/say_thing_about_great_life/old_part_or_young_number/number_and_small_man.py and super/feel_problem_of_eye.py by next case"

---
+++ super/say_thing_about_great_life/old_part_or_young_number/number_and_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_long_government(str_arg):
+    large_part(str_arg)
+    print('group_or_right_place')
+
+def large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_long_government('early_day')
