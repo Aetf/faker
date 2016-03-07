d60507cad1dd1ef476407312966f73482756717b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 12:22:16 -0500

    "add: want super/next_thing.py, super/year.py and super/case.py about great time"

---
+++ super/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world_or_same_child(str_arg):
+    look_young_life_at_able_person(str_arg)
+    print('last_group')
+
+def look_young_life_at_able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world_or_same_child('able_government_or_year')
