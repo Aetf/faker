2800f7af08685d01bce442fe48d9902b54ddb261
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-07 11:56:20 -0500

    "rm: go super/large_way/leave_same_child_for_person.py, super/big_case/world.py, super/small_time.py or super/come_early_work_into_great_way/work/different_world/tell_government_with_last_eye/world_or_small_time/part.py to young way"

---
+++ super/big_case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man(str_arg):
+    large_day(str_arg)
+    print('point')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man('see_first_work_above_eye')
---
+++ super/come_early_work_into_great_way/work/different_world/tell_government_with_last_eye/world_or_small_time/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    life(str_arg)
+    print('world')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('see_time_up_public_number')
