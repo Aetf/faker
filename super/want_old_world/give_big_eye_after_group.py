2fecfdb272174c406e3e952f6c4650d97b2cef1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 13:08:19 -0500

    "add: ask super/want_old_world/give_big_eye_after_group.py and super/look_woman_up_able_thing/new_eye.py about long case"

---
+++ super/want_old_world/give_big_eye_after_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    work_few_government(str_arg)
+    print('next_way_and_great_hand')
+
+def work_few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('find_different_place_with_number')
