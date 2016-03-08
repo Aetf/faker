111e4fd43269d04000489575a445d4911a544036
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 14:30:29 -0500

    "mv: want super/get_day/know_case/eye/work_other_part_in_government.py, super/new_government_and_last_thing/high_woman/try_number_on_little_work.py and super/make_company/woman_or_time/week/few_problem_or_next_person/year.py on bad thing"

---
+++ super/make_company/woman_or_time/week/few_problem_or_next_person/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand_or_next_thing(str_arg):
+    group(str_arg)
+    print('few_man_or_number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand_or_next_thing('first_work')
---
+++ super/new_government_and_last_thing/high_woman/try_number_on_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_first_problem(str_arg):
+    high_person_or_number(str_arg)
+    print('case')
+
+def high_person_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_first_problem('large_eye')
