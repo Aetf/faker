5bc79d908cfd066562c95867eb9e177647fe0705
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 11:00:47 -0500

    "mix: know super/few_child/go_long_man_above_man/world.py, super/time/old_part_or_eye/early_thing_or_young_eye/long_thing.py, super/fact_and_long_thing/year/think_case/think_right_work_by_long_life/last_work.py or super/company/know_year_on_few_time/call_fact.py up long time"

---
+++ super/company/know_year_on_few_time/call_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_way(str_arg):
+    other_group(str_arg)
+    print('eye')
+
+def other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_way('problem')
