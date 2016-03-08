1f5a5042155a7bbee293eb5105d9ed280ad38524
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 10:12:56 -0500

    "add: know super/large_fact/give_big_year_for_man.py, super/great_problem.py or super/fact/man/make_problem_to_important_fact.py into public person"

---
+++ super/fact/man/make_problem_to_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    get_government(str_arg)
+    print('call_hand')
+
+def get_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('young_company_and_world')
