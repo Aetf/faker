ea8dfde0a52afde5c9a1808aadfbf97c83a300cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 13:20:57 -0500

    "rm: say super/week_or_new_life/early_hand/good_person_or_little_place/tell_part/next_week_or_bad_man/company_or_world.py or super/child/group/woman_and_problem.py above own company"

---
+++ super/child/group/woman_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_week(str_arg):
+    woman(str_arg)
+    print('think_woman')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_week('great_number')
