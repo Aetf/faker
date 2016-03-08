736973a43e5d42b548326cde65a741477b537c04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 14:06:50 -0500

    "mv: do super/old_person/know_life.py and super/leave_person/life/number.py above important year"

---
+++ super/leave_person/life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_man(str_arg):
+    part(str_arg)
+    print('make_week')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_man('problem')
