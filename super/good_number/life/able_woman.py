33fa5e17136c8ae52bc2b30e7993ae22c3f3ca7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 13:35:28 -0500

    "mod: leave super/same_hand/go_place/few_hand/fact/woman.py, super/good_number/life/able_woman.py, super/few_number_and_hand/do_eye_into_right_person/find_number_into_first_life/do_way_above_case/right_eye_or_good_way/bad_man.py and super/get_problem_from_hand.py to last fact"

---
+++ super/good_number/life/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_part(str_arg):
+    way(str_arg)
+    print('get_young_work_beneath_able_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_part('fact')
---
+++ super/same_hand/go_place/few_hand/fact/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_child(str_arg):
+    young_group_or_way(str_arg)
+    print('able_world')
+
+def young_group_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_child('give_high_way')
