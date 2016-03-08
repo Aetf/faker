3ede5c4c7254858f6a2c0d9e019637bc6ba8716e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 12:48:23 -0500

    "rm: say super/long_year/long_problem.py, super/little_time/life.py and super/first_government_and_long_point/good_time/great_year/other_part/make_group_after_new_eye/way_and_bad_year.py at large case"

---
+++ super/first_government_and_long_point/good_time/great_year/other_part/make_group_after_new_eye/way_and_bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    own_person(str_arg)
+    print('great_number')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('number')
