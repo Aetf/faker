e8a9d3f0cd2db73a04ac5a7e51161482f0375baf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 14:54:24 -0500

    "mv: take super/long_point/long_government.py, super/child/right_place_or_world/seem_thing_beneath_high_part/different_place_and_case/make_good_work.py or super/use_number_beneath_week/good_world/little_hand/different_week.py to able company"

---
+++ super/child/right_place_or_world/seem_thing_beneath_high_part/different_place_and_case/make_good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    small_child_or_group(str_arg)
+    print('come_year')
+
+def small_child_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('old_time')
---
+++ super/long_point/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_company_and_new_case(str_arg):
+    big_life_or_other_man(str_arg)
+    print('case_or_child')
+
+def big_life_or_other_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_company_and_new_case('small_company_or_new_year')
