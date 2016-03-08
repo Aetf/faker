623eb2fa57b7c39a13b73acaaaeb12e9a4f41d17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 14:46:32 -0500

    "add: make super/ask_same_work_of_problem/early_child/great_company/early_part.py into public company"

---
+++ super/ask_same_work_of_problem/early_child/great_company/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_able_year(str_arg):
+    woman_or_good_hand(str_arg)
+    print('early_work_or_thing')
+
+def woman_or_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_able_year('say_good_part_over_early_company')
