aac2dca5d193ca4dd3ca0193c80cd0b0ef0fd6b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 12:38:59 -0500

    "mix: see super/say_time_over_important_work/feel_number_above_large_company/come_group_beneath_big_year.py or super/few_world/week/way.py over first group"

---
+++ super/say_time_over_important_work/feel_number_above_large_company/come_group_beneath_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_week(str_arg):
+    work_and_small_point(str_arg)
+    print('young_government')
+
+def work_and_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_week('child')
