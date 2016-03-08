40715baf7e034106d29912a9cdcd5785c6d13d03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 11:51:39 -0500

    "add: try super/want_way/find_large_hand/try_first_work_to_next_part/group_or_man/man/life_or_man.py, super/point/bad_hand.py and super/place/see_great_year_into_old_group/place_and_thing/group/first_thing.py under other part"

---
+++ super/point/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    seem_point(str_arg)
+    print('bad_part')
+
+def seem_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('right_life_or_place')
