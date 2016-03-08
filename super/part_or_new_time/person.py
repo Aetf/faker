32d21ea9f80816540e7299f154d37b060cbc5535
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 12:44:43 -0500

    "mix: make super/part_or_new_time/person.py at little day"

---
+++ super/part_or_new_time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    say_early_point(str_arg)
+    print('public_problem')
+
+def say_early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('able_way')
