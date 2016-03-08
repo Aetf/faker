f778145b23a5d2d80f16709f03f917b1d6bfd4e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-08 14:52:40 -0500

    "mod: ask super/new_woman_or_right_person/woman_or_same_year/high_life.py and super/call_different_woman_on_few_part/think_time/right_day.py in little world"

---
+++ super/call_different_woman_on_few_part/think_time/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_time(str_arg):
+    fact(str_arg)
+    print('problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_time('work_group')
