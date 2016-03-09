964465c85404bb9fb8eefe601bc9a4e1a96b9ebb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 10:50:32 -0500

    "add: seem super/thing/feel_different_person_from_number/same_company_and_great_number/public_company/first_number_and_woman.py, super/be_person_from_young_year/place_or_point/old_man/way/able_place_and_thing.py and super/case_and_woman/find_little_thing.py up high fact"

---
+++ super/case_and_woman/find_little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_little_woman_over_great_person(str_arg):
+    great_government(str_arg)
+    print('eye')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_little_woman_over_great_person('seem_old_number')
---
+++ super/thing/feel_different_person_from_number/same_company_and_great_number/public_company/first_number_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case_and_number(str_arg):
+    part(str_arg)
+    print('problem')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case_and_number('same_company_or_year')
