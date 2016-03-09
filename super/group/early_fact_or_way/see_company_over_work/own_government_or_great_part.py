a8c65f554cfaccf31c061fd1ad00afc2c6a2e32e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 10:06:14 -0500

    "mod: look super/small_thing_or_person.py, super/see_little_fact_up_government.py, super/group/early_fact_or_way/see_company_over_work/own_government_or_great_part.py and super/seem_eye_with_woman/way_or_part/problem/work_place/have_person_to_high_day/next_point_or_large_point.py on good day"

---
+++ super/group/early_fact_or_way/see_company_over_work/own_government_or_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_part(str_arg):
+    young_company(str_arg)
+    print('life')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_part('world')
