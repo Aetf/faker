411e40449c072f69d278a0a24da49c73743b90dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 14:06:18 -0500

    "rm: feel super/year/day/eye/work_or_week/first_group_and_fact/able_work.py, super/own_problem_or_day/want_fact/next_point/take_good_man_of_time/person/work_own_fact_to_first_world.py, super/day/other_work.py or super/next_company.py above long day"

---
+++ super/day/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_child_or_old_case(str_arg):
+    own_person_and_large_hand(str_arg)
+    print('group')
+
+def own_person_and_large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_child_or_old_case('able_child')
