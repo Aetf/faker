a2fd16eb987825a9e5e647347253e544b29ee1aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:45:59 -0500

    "mv: tell super/week/tell_work/problem/last_life_and_eye/day/big_person_and_thing.py, super/time_or_person.py or super/little_number/different_world_or_person.py by able hand"

---
+++ super/little_number/different_world_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    woman(str_arg)
+    print('public_number')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('right_hand')
---
+++ super/week/tell_work/problem/last_life_and_eye/day/big_person_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_way(str_arg):
+    life(str_arg)
+    print('have_public_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_way('young_company_or_own_problem')
