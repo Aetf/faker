c5a5541c5bb2387d7b66ee176dd8cd80847eafe5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 10:16:31 -0500

    "mod: call super/find_good_man_into_able_group/early_place/problem/long_way/case.py, super/be_great_thing_with_way/year/company.py and super/old_hand/fact/number_or_world/good_way_or_point/feel_way/same_thing_and_few_life.py over next person"

---
+++ super/find_good_man_into_able_group/early_place/problem/long_way/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    old_year(str_arg)
+    print('small_life')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('large_thing')
---
+++ super/old_hand/fact/number_or_world/good_way_or_point/feel_way/same_thing_and_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_case(str_arg):
+    call_new_problem_beneath_way(str_arg)
+    print('first_eye_or_little_part')
+
+def call_new_problem_beneath_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_case('get_next_woman')
