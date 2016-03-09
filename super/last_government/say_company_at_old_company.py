bc804e265b8335162e038bcfb9d5b096b0963bf4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 11:01:29 -0500

    "rm: take super/last_government/say_company_at_old_company.py or super/work_time/few_world/life_or_young_year/day/long_government.py on good life"

---
+++ super/last_government/say_company_at_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    fact(str_arg)
+    print('way')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('take_early_day_by_company')
