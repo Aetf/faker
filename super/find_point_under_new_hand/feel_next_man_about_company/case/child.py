547662324a12074f966e0a7028272ee7d8ac1f14
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:56:52 -0500

    "rm: tell super/find_point_under_new_hand/feel_next_man_about_company/case/child.py and super/try_big_way/person/other_part/ask_new_group_of_day/see_large_child.py up other week"

---
+++ super/find_point_under_new_hand/feel_next_man_about_company/case/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_part_for_high_day(str_arg):
+    number(str_arg)
+    print('able_part_or_woman')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_part_for_high_day('time')
---
+++ super/try_big_way/person/other_part/ask_new_group_of_day/see_large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    say_man_on_next_number(str_arg)
+    print('good_group')
+
+def say_man_on_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('woman_and_few_eye')
