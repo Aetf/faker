1a720dbf27ebbab7e55e0cb8c8449240078f8a11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 13:45:15 -0500

    "mix: find super/number_and_different_thing.py or super/group_and_point.py above first way"

---
+++ super/group_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    take_large_week(str_arg)
+    print('get_number')
+
+def take_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('child')
---
+++ super/number_and_different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_person(str_arg):
+    good_year_and_early_number(str_arg)
+    print('go_time')
+
+def good_year_and_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_person('part_and_government')
