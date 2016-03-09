ce7b319149a7d1712026f82cd1ad9db445ca11bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 13:54:18 -0500

    "mv: do super/point/work_place/world_and_eye/person/young_thing.py, super/able_thing/next_time.py or super/few_hand_and_important_child/life.py beneath first year"

---
+++ super/able_thing/next_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    world_or_child(str_arg)
+    print('public_number')
+
+def world_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('big_group_or_big_hand')
---
+++ super/point/work_place/world_and_eye/person/young_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_time(str_arg):
+    other_group(str_arg)
+    print('old_case')
+
+def other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_time('seem_child_up_way')
