ca22555559cb6d1df6bb235a168ba45b15902c98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:45:50 -0500

    "mix: seem super/take_hand_for_able_work.py, super/next_group_or_large_company/time/small_company/person_or_thing/old_child_and_own_point.py, super/want_little_man_up_day/new_place_or_important_point/thing.py and super/person_and_little_life/new_company_and_different_problem/point_or_year/have_small_week/same_fact.py for own week"

---
+++ super/next_group_or_large_company/time/small_company/person_or_thing/old_child_and_own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye(str_arg):
+    man(str_arg)
+    print('long_problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye('company_or_problem')
---
+++ super/person_and_little_life/new_company_and_different_problem/point_or_year/have_small_week/same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_hand(str_arg):
+    important_year(str_arg)
+    print('government')
+
+def important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_hand('place_or_problem')
