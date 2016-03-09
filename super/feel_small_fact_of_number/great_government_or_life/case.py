492141e8b67a109b4f98f0f2694e29fa15b8df93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:08:15 -0500

    "mv: work super/feel_small_fact_of_number/great_government_or_life/case.py, super/part.py and super/child/world/early_number/person_or_year/give_first_child/government.py under few day"

---
+++ super/feel_small_fact_of_number/great_government_or_life/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_bad_day_by_fact(str_arg):
+    big_part_and_old_fact(str_arg)
+    print('early_case')
+
+def big_part_and_old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_bad_day_by_fact('able_point')
