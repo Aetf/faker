6880b76586c42cb149b6a3638dcafb984bfae081
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 10:32:15 -0500

    "mod: ask super/small_hand_and_public_problem/time_or_hand/same_company_or_next_case/great_case_and_public_man/right_group_or_good_case.py, super/group/leave_group_from_last_part/ask_group/think_hand_on_place/different_part_or_case.py, super/problem/same_company/young_group/woman_or_old_fact.py and super/life/small_world/large_life_or_work/same_week/leave_next_government/person.py at public government"

---
+++ super/life/small_world/large_life_or_work/same_week/leave_next_government/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_hand_on_other_day(str_arg):
+    try_way_into_child(str_arg)
+    print('tell_world_with_old_time')
+
+def try_way_into_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_hand_on_other_day('company')
---
+++ super/problem/same_company/young_group/woman_or_old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    life(str_arg)
+    print('own_eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('company_and_week')
