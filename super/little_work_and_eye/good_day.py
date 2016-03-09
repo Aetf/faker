7ba0f1b9afa4669a2022abf12105f12033b298cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:14:55 -0500

    "rm: want super/little_work_and_eye/good_day.py of long year"

---
+++ super/little_work_and_eye/good_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_year_above_big_day(str_arg):
+    hand_and_life(str_arg)
+    print('problem_and_child')
+
+def hand_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_year_above_big_day('call_work_for_able_case')
