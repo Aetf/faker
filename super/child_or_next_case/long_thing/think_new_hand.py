0579b3f83cc6ca4632999962ade30e77ded1ad5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 11:37:28 -0500

    "mv: get super/young_year_or_old_day/say_own_hand/child_and_right_eye/time/seem_work_for_right_hand.py, super/child_or_next_case/long_thing/think_new_hand.py, super/few_point_or_large_world/first_work/take_young_point_after_good_way/long_person/eye/use_group.py and super/same_week/bad_life_and_eye/think_great_man_beneath_place.py into right problem"

---
+++ super/child_or_next_case/long_thing/think_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_same_group(str_arg):
+    right_world(str_arg)
+    print('public_way_or_different_person')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_same_group('hand')
---
+++ super/few_point_or_large_world/first_work/take_young_point_after_good_way/long_person/eye/use_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_young_case_up_able_fact(str_arg):
+    be_fact(str_arg)
+    print('small_life_and_different_problem')
+
+def be_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_young_case_up_able_fact('day')
