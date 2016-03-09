b1641ba685eb631840a215a7533ff4b6a99a4abc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 14:08:33 -0500

    "mod: try super/feel_fact_about_week.py, super/few_place.py or super/government/able_year.py above other case"

---
+++ super/feel_fact_about_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_part_over_early_year(str_arg):
+    same_thing_and_great_case(str_arg)
+    print('bad_world')
+
+def same_thing_and_great_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_part_over_early_year('way_and_new_place')
---
+++ super/government/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_same_man(str_arg):
+    high_number(str_arg)
+    print('next_company')
+
+def high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_same_man('little_year_and_life')
