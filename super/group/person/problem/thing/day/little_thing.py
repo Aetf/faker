ad634387900e9ac589b1a2b4a32d460f26702607
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 12:05:14 -0500

    "mod: leave super/public_year_and_hand/old_number/part/be_group.py, super/long_man.py, super/group/person/problem/thing/day/little_thing.py or super/other_woman/try_other_group/go_year/large_man/big_problem.py from bad way"

---
+++ super/group/person/problem/thing/day/little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    able_problem(str_arg)
+    print('high_point')
+
+def able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('place')
---
+++ super/long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    see_point_with_case(str_arg)
+    print('able_hand')
+
+def see_point_with_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('person')
