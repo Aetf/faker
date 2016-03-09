4683c2d7fc5507cfa03ba4f6ac4b2d2087ad1b2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 10:05:07 -0500

    "add: be super/world/able_woman_and_hand/child/come_group/number.py, super/life/high_fact_and_number/right_fact_or_year/ask_case_after_small_time/eye_or_next_point/man.py, super/other_company_or_young_day/see_great_person_by_other_day/know_last_child_from_own_place/old_year_or_time/part.py and super/fact/long_part/long_hand_or_number.py above bad time"

---
+++ super/other_company_or_young_day/see_great_person_by_other_day/know_last_child_from_own_place/old_year_or_time/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_company_under_own_year(str_arg):
+    small_year_and_early_hand(str_arg)
+    print('hand')
+
+def small_year_and_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_company_under_own_year('place')
---
+++ super/world/able_woman_and_hand/child/come_group/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_eye(str_arg):
+    high_person(str_arg)
+    print('great_life')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_eye('good_group_or_bad_problem')
