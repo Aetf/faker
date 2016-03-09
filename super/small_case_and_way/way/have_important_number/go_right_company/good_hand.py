9c9617b625bbbaf6289c2dfc7e7166e3d8ba09ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-09 14:00:52 -0500

    "mv: find super/small_case_and_way/way/have_important_number/go_right_company/good_hand.py or super/try_other_company_above_little_thing/get_man_after_public_group.py beneath high woman"

---
+++ super/small_case_and_way/way/have_important_number/go_right_company/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_world(str_arg):
+    long_thing(str_arg)
+    print('part_and_same_case')
+
+def long_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_world('man')
---
+++ super/try_other_company_above_little_thing/get_man_after_public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_or_last_hand(str_arg):
+    able_week(str_arg)
+    print('time')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_or_last_hand('bad_fact')
