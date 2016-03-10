c3c389aa23fe0481b3780418d2b2929cea3c9f51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 10:26:04 -0500

    "mv: try super/next_world_and_place/say_child/new_fact.py or super/life_or_world/problem/day/few_time_and_man.py into right problem"

---
+++ super/next_world_and_place/say_child/new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_public_woman_on_new_eye(str_arg):
+    go_fact(str_arg)
+    print('little_life_and_work')
+
+def go_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_public_woman_on_new_eye('have_high_week')
