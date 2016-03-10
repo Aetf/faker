9bc4d68fd3c66bcb4360ec5a0b7db9b3052436d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 11:13:49 -0500

    "mod: go super/different_government/know_child_by_point/public_way/world_or_old_day/number_and_last_government/man.py, super/right_group.py and super/hand/other_child/few_time_or_good_year/work/able_world.py above different place"

---
+++ super/different_government/know_child_by_point/public_way/world_or_old_day/number_and_last_government/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    public_problem_or_old_number(str_arg)
+    print('be_fact')
+
+def public_problem_or_old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('few_company_and_number')
---
+++ super/hand/other_child/few_time_or_good_year/work/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_case(str_arg):
+    call_man_up_point(str_arg)
+    print('own_fact_and_government')
+
+def call_man_up_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_case('woman')
