175d1e3f0525df946779f3da1a1b2e6fd585ff90
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 10:42:53 -0500

    "mv: want super/way/man.py or super/able_woman/long_place/large_year/take_place_beneath_able_group/life_or_small_eye/last_year.py at small company"

---
+++ super/able_woman/long_place/large_year/take_place_beneath_able_group/life_or_small_eye/last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_person(str_arg):
+    life(str_arg)
+    print('next_thing')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_person('do_woman')
---
+++ super/way/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    feel_little_life_over_few_woman(str_arg)
+    print('know_little_way')
+
+def feel_little_life_over_few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('leave_work_from_last_way')
