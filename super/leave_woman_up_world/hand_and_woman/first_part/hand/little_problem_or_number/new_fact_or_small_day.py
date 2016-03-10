a30d5609a8fcca2e2d3ec9c41864f2bd91c44f0f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:25:04 -0500

    "mix: get super/child_or_small_company.py or super/leave_woman_up_world/hand_and_woman/first_part/hand/little_problem_or_number/new_fact_or_small_day.py at public problem"

---
+++ super/leave_woman_up_world/hand_and_woman/first_part/hand/little_problem_or_number/new_fact_or_small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week(str_arg):
+    last_work_and_old_world(str_arg)
+    print('work')
+
+def last_work_and_old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week('make_place')
