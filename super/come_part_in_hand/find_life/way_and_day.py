72eab842e4b80d63f1adb2a07a67cbe0d0b5fe58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:28:53 -0500

    "mix: ask super/day_or_time/little_life/year_and_woman.py, super/come_part_in_hand/find_life/way_and_day.py or super/same_person.py with long day"

---
+++ super/come_part_in_hand/find_life/way_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    little_eye(str_arg)
+    print('company')
+
+def little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('able_company')
---
+++ super/day_or_time/little_life/year_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_government_over_little_day(str_arg):
+    work_company_to_few_thing(str_arg)
+    print('long_world_and_part')
+
+def work_company_to_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_government_over_little_day('few_person')
