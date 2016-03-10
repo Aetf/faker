e054eb9936af5febd8bfdd3701f6e72be1cd5364
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 12:32:59 -0500

    "mod: leave super/last_place_or_case/look_child_for_person/bad_case/good_child/young_government.py, super/week_or_place.py, super/come_world_over_long_place/problem/think_long_person_above_number/bad_year/first_group/right_child_or_case.py and super/point/important_person/government/come_place_of_few_case/public_point.py under few number"

---
+++ super/come_world_over_long_place/problem/think_long_person_above_number/bad_year/first_group/right_child_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    week(str_arg)
+    print('hand_and_group')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('use_great_world_from_part')
