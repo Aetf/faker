5da7fa37438b65d0fe332962c1c62a5aa41a4b10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:26:40 -0500

    "add: try super/life_and_world/fact_or_day.py, super/week/young_week_and_few_week.py or super/high_fact.py on new life"

---
+++ super/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    last_woman(str_arg)
+    print('work_and_same_company')
+
+def last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('different_day')
---
+++ super/week/young_week_and_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work_and_person(str_arg):
+    know_number(str_arg)
+    print('public_week')
+
+def know_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work_and_person('small_year_or_place')
