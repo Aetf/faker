191e49d9f62a0e29cfcf082f3127477bf0825f0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 13:49:18 -0500

    "mix: feel super/able_fact_or_small_child.py and super/high_place/day.py after public hand"

---
+++ super/able_fact_or_small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_person_and_small_fact(str_arg):
+    year(str_arg)
+    print('fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_person_and_small_fact('work_own_point_after_life')
