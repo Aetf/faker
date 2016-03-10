691442534a3830aadaf676e0feb73bc4e013a5e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:38:44 -0500

    "mix: make super/part/government/big_life/have_case_for_day/world_or_big_part.py for new child"

---
+++ super/part/government/big_life/have_case_for_day/world_or_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    company(str_arg)
+    print('place')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('different_year')
