5f61a7db95c5eeda1e52a6b2946ec6c7b7c1a331
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 11:43:59 -0500

    "add: find super/first_life_and_thing.py, super/life/make_place/part/leave_old_fact.py and super/other_way.py in same person"

---
+++ super/life/make_place/part/leave_old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_work_and_eye(str_arg):
+    do_place(str_arg)
+    print('find_eye_beneath_next_point')
+
+def do_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_work_and_eye('little_year')
