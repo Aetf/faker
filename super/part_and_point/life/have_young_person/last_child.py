addb3894332e68513f9b0c40e16260b2884092fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:29:51 -0500

    "mix: want super/part_and_point/life/have_young_person/last_child.py from different group"

---
+++ super/part_and_point/life/have_young_person/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    first_man_and_old_child(str_arg)
+    print('few_time_or_government')
+
+def first_man_and_old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('same_child')
