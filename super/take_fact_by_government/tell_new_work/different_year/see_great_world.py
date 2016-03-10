bad85423bf80928e460f53b209594a9853b75180
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:08:06 -0500

    "rm: have super/find_world_into_public_number/child/young_day_or_place.py, super/take_fact_by_government/tell_new_work/different_year/see_great_world.py or super/week/life/eye_or_way.py about next part"

---
+++ super/take_fact_by_government/tell_new_work/different_year/see_great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_public_day(str_arg):
+    way_or_great_day(str_arg)
+    print('call_right_year')
+
+def way_or_great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_public_day('few_child')
