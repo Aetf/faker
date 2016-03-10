5d3612ce3a1cbabeeb6dedcf9cc3dfce1498c4f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:10:33 -0500

    "mix: seem super/first_woman/hand/say_day_into_first_government/work/same_eye_and_right_time.py from long group"

---
+++ super/first_woman/hand/say_day_into_first_government/work/same_eye_and_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place_or_case(str_arg):
+    place_or_own_work(str_arg)
+    print('great_week')
+
+def place_or_own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place_or_case('thing')
