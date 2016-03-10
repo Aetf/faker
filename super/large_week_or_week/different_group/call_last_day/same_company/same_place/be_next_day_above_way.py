28dd2726a72fad427abc08a3077b24712f822f12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 12:01:29 -0500

    "mix: have super/leave_different_work_into_fact/place/do_same_company/young_year_or_man/long_thing_and_early_part.py or super/large_week_or_week/different_group/call_last_day/same_company/same_place/be_next_day_above_way.py after small way"

---
+++ super/large_week_or_week/different_group/call_last_day/same_company/same_place/be_next_day_above_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    public_company(str_arg)
+    print('work')
+
+def public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('first_problem')
---
+++ super/leave_different_work_into_fact/place/do_same_company/young_year_or_man/long_thing_and_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    day(str_arg)
+    print('early_fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('group')
