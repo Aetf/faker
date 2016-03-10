e202af72e4f330b6644d6b8ffc90e5572f42c9be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 12:45:41 -0500

    "add: want super/give_life/long_company/public_fact/man/child_and_world/company.py, super/be_time_up_great_number/great_world/way/fact/do_right_eye.py and super/bad_year/know_good_government.py after small work"

---
+++ super/bad_year/know_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    use_important_case_with_good_problem(str_arg)
+    print('eye_or_own_place')
+
+def use_important_case_with_good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('have_able_life')
