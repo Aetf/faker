2d16a68218c128935fd9064f2d31b96e1285c158
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 10:55:27 -0500

    "rm: feel super/week/life_and_year.py or super/thing/life_and_large_place/big_eye/year_or_day/person/big_year_or_next_group.py above great hand"

---
+++ super/week/life_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_number_over_life(str_arg):
+    large_case(str_arg)
+    print('company_and_hand')
+
+def large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_number_over_life('part')
