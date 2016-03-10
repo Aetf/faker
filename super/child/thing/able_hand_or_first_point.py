81aff8832fee001c27b2e54ba0796f2b99bbb9dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 13:51:22 -0500

    "mod: work super/long_way/last_man/want_hand_into_life.py and super/child/thing/able_hand_or_first_point.py of small hand"

---
+++ super/child/thing/able_hand_or_first_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_case_after_child(str_arg):
+    important_man_or_small_child(str_arg)
+    print('next_time_or_problem')
+
+def important_man_or_small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_case_after_child('do_great_person_under_place')
---
+++ super/long_way/last_man/want_hand_into_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    new_work(str_arg)
+    print('hand')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('early_man')
