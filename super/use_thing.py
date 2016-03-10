4f3f0e3aa593e4a9be9710d25245e90c05f41e0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-10 14:04:52 -0500

    "mix: call super/use_thing.py about right number"

---
+++ super/use_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact_or_right_year(str_arg):
+    bad_case(str_arg)
+    print('fact')
+
+def bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact_or_right_year('point')
