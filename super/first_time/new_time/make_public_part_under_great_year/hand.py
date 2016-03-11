34f300bb9045af9331e1012f31e3be12873873a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 14:52:05 -0500

    "mod: have super/make_work_with_number.py, super/first_time/new_time/make_public_part_under_great_year/hand.py or super/use_first_man/problem_and_government.py above important week"

---
+++ super/first_time/new_time/make_public_part_under_great_year/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    time_or_own_part(str_arg)
+    print('be_able_week')
+
+def time_or_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('next_life_or_little_company')
---
+++ super/use_first_man/problem_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    young_government(str_arg)
+    print('use_man_for_problem')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('leave_new_government_over_bad_time')
