569a360dfa98a6db7f3728fbaaef79830990993a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 12:24:18 -0500

    "mv: give super/large_year_or_company/give_child_for_great_year/little_woman_or_bad_eye/bad_government.py in old case"

---
+++ super/large_year_or_company/give_child_for_great_year/little_woman_or_bad_eye/bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    company(str_arg)
+    print('early_thing')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('woman_or_part')
