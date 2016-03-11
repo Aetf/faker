ce3a6c4caf4581079e8719d169561e3758a0f062
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 10:39:02 -0500

    "mix: ask super/seem_good_person/day.py or super/know_way_with_right_woman/do_hand/problem/other_life/feel_bad_woman/man_and_day.py of different life"

---
+++ super/seem_good_person/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact_and_great_place(str_arg):
+    bad_case_and_part(str_arg)
+    print('great_problem_or_big_world')
+
+def bad_case_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact_and_great_place('other_world')
