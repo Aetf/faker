33ad5ef2bf3fd3c4018056817aefe261da4dc570
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 14:37:57 -0500

    "rm: call super/big_child_or_week/woman/be_man/day/leave_few_eye/thing.py, super/young_point/early_company.py or super/make_woman_with_time/day_or_early_hand/be_fact/eye_and_great_woman/good_life.py of important time"

---
+++ super/big_child_or_week/woman/be_man/day/leave_few_eye/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    able_group_and_first_work(str_arg)
+    print('child')
+
+def able_group_and_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('year_and_right_week')
---
+++ super/young_point/early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    first_problem(str_arg)
+    print('time_and_different_week')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('right_year')
