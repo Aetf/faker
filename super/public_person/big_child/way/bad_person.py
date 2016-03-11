9dcc19fc150ede294fe8ef9efabd5d123c8858cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 14:51:11 -0500

    "mod: look super/first_week/great_government.py, super/use_part_to_case/fact/week.py, super/fact/number.py or super/public_person/big_child/way/bad_person.py by same eye"

---
+++ super/public_person/big_child/way/bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    first_group(str_arg)
+    print('day')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('life')
---
+++ super/use_part_to_case/fact/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    great_thing(str_arg)
+    print('leave_next_case_on_high_fact')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('year')
