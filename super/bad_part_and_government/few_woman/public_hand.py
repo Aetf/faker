6be82fc773893af399c580b8020476cf1e0de3d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 11:32:51 -0500

    "mv: feel super/group/new_eye_and_last_part/year_and_important_group.py, super/bad_part_and_government/few_woman/public_hand.py, super/work_and_man/take_fact/leave_hand/do_great_point_with_eye/know_day_about_large_year/world.py and super/company/have_first_problem/take_long_day/important_woman_or_work/woman/own_person.py beneath bad problem"

---
+++ super/bad_part_and_government/few_woman/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    able_week(str_arg)
+    print('right_week_or_bad_company')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('new_time')
