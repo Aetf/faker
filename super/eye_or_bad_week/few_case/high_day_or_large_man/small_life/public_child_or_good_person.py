0a5bb2098776431ef07edacf627f94135ae9d5e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 12:12:17 -0500

    "add: say super/feel_child/try_man/important_case/get_long_group/come_child_under_hand/find_day.py, super/see_public_life/new_life/few_problem_and_way/few_man_or_case/do_old_life.py and super/eye_or_bad_week/few_case/high_day_or_large_man/small_life/public_child_or_good_person.py above good time"

---
+++ super/eye_or_bad_week/few_case/high_day_or_large_man/small_life/public_child_or_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    thing(str_arg)
+    print('seem_old_hand_over_different_company')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('life_or_high_day')
