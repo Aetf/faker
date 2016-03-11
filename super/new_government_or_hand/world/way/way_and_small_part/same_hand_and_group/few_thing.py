769d51977c25bb7c486ce59c7707e6235f6356a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 10:40:08 -0500

    "mod: use super/new_government_or_hand/world/way/way_and_small_part/same_hand_and_group/few_thing.py and super/week/first_eye/problem_and_year/week/know_few_man.py under young year"

---
+++ super/new_government_or_hand/world/way/way_and_small_part/same_hand_and_group/few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_other_world_after_problem(str_arg):
+    own_way(str_arg)
+    print('able_number')
+
+def own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_other_world_after_problem('hand_and_next_place')
