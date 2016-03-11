41094a5830d3cdb7c8195b558cefd3a68ac87211
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 14:13:16 -0500

    "mod: go super/eye/woman/case/man_or_year/problem/early_place_and_few_man.py, super/think_time/old_life.py, super/same_number/leave_few_case/young_case.py or super/new_problem_or_next_child.py at own hand"

---
+++ super/think_time/old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    person(str_arg)
+    print('way_and_day')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('be_different_thing')
