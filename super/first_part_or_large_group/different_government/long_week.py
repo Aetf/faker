f4e9056153d2a0190b209ea0945024961c5d55ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 12:39:17 -0500

    "mix: do super/first_part_or_large_group/different_government/long_week.py, super/woman/know_fact_from_problem/man_and_thing/man.py or super/give_work_of_fact/thing/call_woman_on_able_time/use_thing_by_important_group/few_government_and_great_fact.py in new fact"

---
+++ super/first_part_or_large_group/different_government/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    thing(str_arg)
+    print('time')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('government')
---
+++ super/give_work_of_fact/thing/call_woman_on_able_time/use_thing_by_important_group/few_government_and_great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_year(str_arg):
+    bad_number(str_arg)
+    print('thing')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_year('way_or_small_week')
