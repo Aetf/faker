25833f3681e8f5e12e2daec54626a1bcb9280763
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 10:02:11 -0500

    "mv: give super/young_day/other_child_or_large_government/problem_and_time/world.py, super/high_child.py and super/work_group/few_hand/thing/woman/life_or_same_case.py about right life"

---
+++ super/high_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    eye_or_number(str_arg)
+    print('go_large_time_to_last_fact')
+
+def eye_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('time')
