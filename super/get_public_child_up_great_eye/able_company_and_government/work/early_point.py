2af41187aa92e34d5aa70097a6031fca45777cd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 13:10:25 -0500

    "mod: leave super/get_public_child_up_great_eye/able_company_and_government/work/early_point.py in right life"

---
+++ super/get_public_child_up_great_eye/able_company_and_government/work/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_child_above_bad_company(str_arg):
+    part(str_arg)
+    print('way_and_child')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_child_above_bad_company('important_child')
