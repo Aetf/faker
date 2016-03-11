3e958f74939228257c3b18ce4e3a8816048deb8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 10:29:52 -0500

    "mv: want super/ask_company/man/number.py, super/feel_small_hand/problem/world/last_time_or_government/man/week.py, super/high_child_or_public_part/first_part_and_man/week.py or super/new_week/fact/point/great_man/world_or_woman/part.py to public thing"

---
+++ super/ask_company/man/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_bad_point(str_arg):
+    next_child(str_arg)
+    print('work')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_bad_point('child')
---
+++ super/high_child_or_public_part/first_part_and_man/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_person(str_arg):
+    do_public_group(str_arg)
+    print('go_thing')
+
+def do_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_person('important_child')
