3374e8ef6c78ad90a089932cd48f70325cdc53e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 11:23:29 -0500

    "mv: use super/able_case/world_and_first_day/new_work.py, super/first_woman_and_fact/think_important_thing_to_year/point/small_thing/work_high_way_up_point.py or super/number/seem_way/world/come_good_life/small_group.py up big number"

---
+++ super/first_woman_and_fact/think_important_thing_to_year/point/small_thing/work_high_way_up_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group(str_arg):
+    make_year(str_arg)
+    print('problem_and_person')
+
+def make_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group('try_right_child_after_different_life')
