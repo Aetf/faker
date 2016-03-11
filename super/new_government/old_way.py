773f638e337091d4b6fc1a994f559ef1ed36dbb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 10:40:38 -0500

    "mix: do super/new_government/old_way.py, super/get_eye_of_man.py, super/tell_small_company_in_old_work/different_way/number/old_woman.py or super/use_own_eye_with_high_day/big_number/woman/ask_important_world_beneath_company/problem_and_case.py above good person"

---
+++ super/new_government/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    new_group(str_arg)
+    print('get_different_person')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('give_able_fact_over_problem')
---
+++ super/tell_small_company_in_old_work/different_way/number/old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    year(str_arg)
+    print('world')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('company')
