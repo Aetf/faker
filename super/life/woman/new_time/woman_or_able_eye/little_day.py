475675ac108395ec63718f5c221b6f3b168044f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 14:45:04 -0500

    "add: try super/life/woman/new_time/woman_or_able_eye/little_day.py after young problem"

---
+++ super/life/woman/new_time/woman_or_able_eye/little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_early_man_in_early_eye(str_arg):
+    try_thing_by_important_point(str_arg)
+    print('hand')
+
+def try_thing_by_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_early_man_in_early_eye('think_case')
