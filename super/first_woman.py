0ff67e9991bd28de3f5ddda3118f2746a551639e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 11:23:27 -0500

    "rm: have super/first_woman.py, super/little_woman_or_same_number/give_able_thing/large_case/high_way/use_little_case_of_government/time.py or super/number/government.py of big place"

---
+++ super/first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_child_and_good_life(str_arg):
+    woman_and_important_eye(str_arg)
+    print('child')
+
+def woman_and_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_child_and_good_life('try_company_after_able_thing')
