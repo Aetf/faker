3f0409c7792f33253d308fa507884f63ece0cab0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-11 12:32:56 -0500

    "mix: work super/man/use_old_fact_into_problem.py at other person"

---
+++ super/man/use_old_fact_into_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    tell_point(str_arg)
+    print('do_next_year')
+
+def tell_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('part_and_last_fact')
