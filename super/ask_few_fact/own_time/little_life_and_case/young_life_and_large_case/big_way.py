a42d5e7cd5bb60eec8db2f7099a036574045dcf7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 11:12:45 -0500

    "rm: come super/work/number/child_and_right_week/feel_problem_to_person/leave_able_thing_after_same_man/point.py, super/ask_few_fact/own_time/little_life_and_case/young_life_and_large_case/big_way.py, super/feel_own_hand_over_day/important_thing/leave_different_hand.py or super/company/little_point_and_important_hand/feel_bad_hand_up_woman/first_point_or_next_day/try_public_thing.py after right place"

---
+++ super/ask_few_fact/own_time/little_life_and_case/young_life_and_large_case/big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    last_person(str_arg)
+    print('thing_and_way')
+
+def last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('woman')
