dd85cd20050287b9e1f74b21d9f6b9c4fa726fd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:48:51 -0500

    "add: say super/see_year_up_group/thing_and_thing.py after important person"

---
+++ super/see_year_up_group/thing_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_problem(str_arg):
+    same_child(str_arg)
+    print('leave_eye')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_problem('long_person')
