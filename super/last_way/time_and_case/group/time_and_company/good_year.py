9afea610d31b97f57fa104d18e3b029a68b9ecfe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 11:55:04 -0500

    "mix: give super/last_way/time_and_case/group/time_and_company/good_year.py or super/problem/big_way_or_small_time/woman/bad_life_and_long_part.py up first life"

---
+++ super/last_way/time_and_case/group/time_and_company/good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_case_on_point(str_arg):
+    eye_and_fact(str_arg)
+    print('get_important_work')
+
+def eye_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_case_on_point('work')
