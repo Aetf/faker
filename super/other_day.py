b711bbee3f25a3bb433e67555755bddadceda29c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:21:52 -0500

    "mv: think super/other_day.py and super/public_person_and_long_case/fact_and_government/little_day/group/find_hand.py beneath last number"

---
+++ super/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    case_or_fact(str_arg)
+    print('problem_and_woman')
+
+def case_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('case')
---
+++ super/public_person_and_long_case/fact_and_government/little_day/group/find_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_week(str_arg):
+    important_company(str_arg)
+    print('world')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_week('public_woman')
