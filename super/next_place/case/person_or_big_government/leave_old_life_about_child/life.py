efef701b2794b19466243b1d574aa24b1ab05c59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 11:08:54 -0500

    "mod: say super/next_place/case/person_or_big_government/leave_old_life_about_child/life.py, super/number/man/point/eye/man_and_small_world/public_group.py, super/seem_public_year_from_work/time_and_way/have_number_at_group/call_way/long_part/have_world.py or super/case/man/week/place.py over small problem"

---
+++ super/next_place/case/person_or_big_government/leave_old_life_about_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_day(str_arg):
+    part(str_arg)
+    print('group_or_same_eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_day('fact')
---
+++ super/number/man/point/eye/man_and_small_world/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_high_woman(str_arg):
+    week(str_arg)
+    print('different_way_and_world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_high_woman('number')
