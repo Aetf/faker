1d3c4bf1c6ae039a8975da22f9e3327061cec3b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:07:24 -0500

    "rm: know super/way/own_week/have_last_week_of_case/place/see_life.py, super/point/point_and_able_thing/different_government/try_eye/think_problem/work.py, super/few_way/child/great_woman.py and super/big_world/day/try_child_on_able_case/try_problem_from_fact/work_world/fact.py for important week"

---
+++ super/way/own_week/have_last_week_of_case/place/see_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_little_part(str_arg):
+    own_point_and_day(str_arg)
+    print('different_work_and_long_life')
+
+def own_point_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_little_part('big_woman_or_small_world')
