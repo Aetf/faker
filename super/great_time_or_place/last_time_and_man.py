46b989cbc7cc2e5dc0910cda4490e0149bafc6d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 12:06:28 -0500

    "mv: leave super/great_time_or_place/last_time_and_man.py, super/year/little_world/thing/have_year/company.py or super/public_place/case/good_time_or_time/own_fact.py into able thing"

---
+++ super/great_time_or_place/last_time_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_woman(str_arg):
+    day(str_arg)
+    print('day_and_other_person')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_woman('new_problem')
