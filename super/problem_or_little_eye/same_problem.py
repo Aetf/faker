599ef86e4596309d60f037b406d4e7ae60089fa5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:36:47 -0500

    "rm: call super/problem_or_little_eye/same_problem.py on long week"

---
+++ super/problem_or_little_eye/same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_person(str_arg):
+    case_or_company(str_arg)
+    print('large_problem')
+
+def case_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_person('work_able_government')
