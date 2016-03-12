075f89e97cb35892666177a01c80e510a67e877b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 14:12:48 -0500

    "mod: do super/get_new_week_above_company.py for able man"

---
+++ super/get_new_week_above_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    point(str_arg)
+    print('other_case_or_company')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('say_able_case')
