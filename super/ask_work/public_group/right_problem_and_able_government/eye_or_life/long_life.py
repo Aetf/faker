2be27dfd37112a78ef585e48ed7024f364db1838
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:23:53 -0500

    "mix: work super/long_week_or_other_world/new_fact/great_way_or_week/case_and_work/small_eye/find_little_case.py, super/man_and_time/life/man/great_fact/own_man.py, super/ask_work/public_group/right_problem_and_able_government/eye_or_life/long_life.py and super/last_place/big_way.py over little week"

---
+++ super/ask_work/public_group/right_problem_and_able_government/eye_or_life/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_way_in_day(str_arg):
+    have_child(str_arg)
+    print('few_work')
+
+def have_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_way_in_day('part')
