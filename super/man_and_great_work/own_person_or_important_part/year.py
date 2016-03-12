87061306c5d0f45ef2b6d6ef56daf1f2a0f264f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 10:02:34 -0500

    "add: look super/man_and_great_work/own_person_or_important_part/year.py in last man"

---
+++ super/man_and_great_work/own_person_or_important_part/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_right_place(str_arg):
+    public_thing(str_arg)
+    print('work_big_work')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_right_place('world_or_case')
