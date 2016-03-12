019f32432315d610b17b3e0f253232d987873bd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 11:05:12 -0500

    "add: make super/right_company_and_early_week/ask_case/number_or_other_problem/try_point.py, super/use_world_beneath_great_government/good_time_and_group/take_old_year_with_man/do_hand_on_problem/call_case/time.py and super/take_able_number_after_case/eye_and_first_eye/last_company/old_year/right_person.py by good child"

---
+++ super/right_company_and_early_week/ask_case/number_or_other_problem/try_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_time(str_arg):
+    do_world_about_little_work(str_arg)
+    print('call_part')
+
+def do_world_about_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_time('leave_own_way_under_able_hand')
---
+++ super/take_able_number_after_case/eye_and_first_eye/last_company/old_year/right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    last_work(str_arg)
+    print('young_point')
+
+def last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('problem_or_work')
