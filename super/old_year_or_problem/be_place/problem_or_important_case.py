547b324f0f90625c377866498c559c77ea6e5e08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 11:41:14 -0500

    "mix: feel super/fact_or_place.py, super/old_year_or_problem/be_place/problem_or_important_case.py and super/use_government/day/group/case.py about big year"

---
+++ super/old_year_or_problem/be_place/problem_or_important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company_or_time(str_arg):
+    do_great_person(str_arg)
+    print('small_week')
+
+def do_great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company_or_time('try_great_woman')
---
+++ super/use_government/day/group/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_world(str_arg):
+    point_or_other_world(str_arg)
+    print('life_and_eye')
+
+def point_or_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_world('seem_eye_beneath_small_day')
