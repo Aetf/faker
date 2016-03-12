74b1e14f7c05530db3ab718131a95e063c75ef47
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 12:13:57 -0500

    "mod: seem super/work_and_problem/company_and_same_year/long_government.py, super/good_year_and_thing/world/work_week/long_thing/get_government_for_way/long_person_and_right_hand.py or super/first_place_or_other_eye/year/important_day/good_problem_and_long_number.py by new year"

---
+++ super/good_year_and_thing/world/work_week/long_thing/get_government_for_way/long_person_and_right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work_in_way(str_arg):
+    thing_and_important_world(str_arg)
+    print('own_work')
+
+def thing_and_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work_in_way('different_company')
---
+++ super/work_and_problem/company_and_same_year/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_week_and_same_man(str_arg):
+    company_or_first_way(str_arg)
+    print('case')
+
+def company_or_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_week_and_same_man('large_company_and_hand')
