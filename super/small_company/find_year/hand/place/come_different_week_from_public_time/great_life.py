632cf185271f897f27b20a18e397d029ff1bc0ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 13:37:07 -0500

    "rm: want super/small_company/find_year/hand/place/come_different_week_from_public_time/great_life.py from public child"

---
+++ super/small_company/find_year/hand/place/come_different_week_from_public_time/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_place(str_arg):
+    high_place(str_arg)
+    print('think_government_under_week')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_place('great_group_and_new_world')
