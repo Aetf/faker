1ec1901d104fef756ec6d6b346f4d3e6648128ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 14:38:33 -0500

    "mv: want super/work/group.py up first woman"

---
+++ super/work/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    large_man(str_arg)
+    print('young_place')
+
+def large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('find_life')
