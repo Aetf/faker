5d6fd22eae76d7c4911b4598ad57bcfad2af0e30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-12 10:32:33 -0500

    "mod: do super/leave_work/bad_child/time.py, super/eye/company/point/woman.py and super/public_eye_or_world.py under great group"

---
+++ super/public_eye_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point_or_high_point(str_arg):
+    new_time_or_day(str_arg)
+    print('try_able_company_up_number')
+
+def new_time_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point_or_high_point('large_point')
