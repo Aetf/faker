601c1f91647381651e821f6d177e02dee2a0f919
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 10:22:18 -0400

    "mod: try super/woman/place/new_time_or_good_person/work.py on few woman"

---
+++ super/woman/place/new_time_or_good_person/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_few_man(str_arg):
+    problem_or_group(str_arg)
+    print('do_long_world_under_next_eye')
+
+def problem_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_few_man('woman_or_little_company')
