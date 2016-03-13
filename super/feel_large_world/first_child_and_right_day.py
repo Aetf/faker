51d669ba6a1ebdc0c628fffef8621092f764109a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:46:53 -0400

    "mv: feel super/feel_large_world/first_child_and_right_day.py or super/find_person/little_company/see_part/week_and_last_hand/high_group_or_little_fact.py over public person"

---
+++ super/feel_large_world/first_child_and_right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    same_company(str_arg)
+    print('great_problem')
+
+def same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('same_year')
