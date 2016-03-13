b766858dd203ef234a41eb04d35a9795305c324f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:00:40 -0400

    "mix: say super/seem_same_man_after_number/small_case_or_hand/high_number_or_group/few_woman/child.py and super/few_work/few_part/important_eye_or_number/own_place/company_or_day/good_part.py about same way"

---
+++ super/few_work/few_part/important_eye_or_number/own_place/company_or_day/good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    young_world(str_arg)
+    print('company')
+
+def young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('young_life_or_fact')
