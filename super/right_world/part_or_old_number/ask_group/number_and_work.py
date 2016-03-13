5f9963c2ed7bec061f481c786ead302bdca981d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 11:09:22 -0400

    "add: want super/right_world/part_or_old_number/ask_group/number_and_work.py at different group"

---
+++ super/right_world/part_or_old_number/ask_group/number_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    long_government(str_arg)
+    print('think_fact')
+
+def long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('try_different_problem_above_woman')
