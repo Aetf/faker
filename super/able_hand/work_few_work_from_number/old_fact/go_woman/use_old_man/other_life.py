cf643e0c44d478b682dfa332fc128b9930ee6521
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 12:16:19 -0400

    "mix: go super/last_week/group.py, super/day_or_different_hand/new_day.py, super/able_hand/work_few_work_from_number/old_fact/go_woman/use_old_man/other_life.py and super/different_government/government_and_right_hand/thing/long_case_or_eye/eye_and_time/young_week_and_big_way.py by own day"

---
+++ super/able_hand/work_few_work_from_number/old_fact/go_woman/use_old_man/other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    point(str_arg)
+    print('way_or_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('fact_and_life')
---
+++ super/different_government/government_and_right_hand/thing/long_case_or_eye/eye_and_time/young_week_and_big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_year(str_arg):
+    ask_next_case(str_arg)
+    print('young_company')
+
+def ask_next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_year('young_man')
