6eb58384751a55755297de8a4d6bd62fa37326e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 11:04:55 -0400

    "rm: look super/last_problem/early_problem/own_year_and_fact/come_next_eye/high_government_and_new_government/eye.py, super/part/person_and_same_fact/take_big_thing/get_last_world/eye_and_eye.py, super/woman/go_part/make_same_year_at_world/way/place/company_or_new_point.py and super/problem_or_high_thing/look_problem.py after young eye"

---
+++ super/last_problem/early_problem/own_year_and_fact/come_next_eye/high_government_and_new_government/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_week(str_arg):
+    right_week(str_arg)
+    print('right_government')
+
+def right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_week('good_day')
---
+++ super/part/person_and_same_fact/take_big_thing/get_last_world/eye_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_person_by_right_group(str_arg):
+    person_and_week(str_arg)
+    print('world')
+
+def person_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_person_by_right_group('seem_bad_point')
