8f1e4da56178bc8e0c7fd65201ed6d5ec858f578
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:18:17 -0400

    "add: call super/seem_government_of_next_case/company.py, super/able_world/good_company_or_big_eye/company/few_world/great_way.py and super/old_child/child/small_work/good_man_and_good_time.py under bad child"

---
+++ super/seem_government_of_next_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_woman(str_arg):
+    want_new_life(str_arg)
+    print('thing')
+
+def want_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_woman('life')
