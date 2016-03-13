7dab9d2d1c1c8c676d2d138edef8a1d3dbbb33cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 10:29:02 -0400

    "add: want super/day_or_case.py, super/life/woman_and_public_point.py, super/problem_and_good_time/find_old_case_on_next_point/group/company/same_world.py and super/right_thing_and_day/part/hand_and_able_place/important_person/case_or_eye.py with small problem"

---
+++ super/right_thing_and_day/part/hand_and_able_place/important_person/case_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_number_or_problem(str_arg):
+    work_new_eye(str_arg)
+    print('way')
+
+def work_new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_number_or_problem('big_day')
