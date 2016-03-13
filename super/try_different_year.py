26ab522e11753a8aca89e5bdd306a0346e1eee6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 12:50:12 -0400

    "mv: work super/work_public_group/thing_or_different_government/week/leave_little_problem/man/own_work.py, super/feel_hand_to_case/day/early_part_or_young_hand/group_and_number/little_way_and_early_time.py, super/try_different_year.py and super/part_and_old_fact/new_thing/large_company/company/world_and_year/large_place.py for own eye"

---
+++ super/try_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    number(str_arg)
+    print('important_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('work_case_on_few_case')
---
+++ super/work_public_group/thing_or_different_government/week/leave_little_problem/man/own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_woman_or_last_year(str_arg):
+    first_place(str_arg)
+    print('take_next_person_on_work')
+
+def first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_woman_or_last_year('tell_big_man_over_own_eye')
