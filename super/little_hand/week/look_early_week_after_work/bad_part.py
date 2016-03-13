de3ac779abc59df32dfa545edf3144852b658370
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:42:00 -0400

    "add: do super/last_child.py, super/man_or_week/year/problem_and_work.py, super/way/make_long_eye/man/eye/way.py and super/little_hand/week/look_early_week_after_work/bad_part.py in last government"

---
+++ super/little_hand/week/look_early_week_after_work/bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    own_government_or_able_life(str_arg)
+    print('man_and_early_thing')
+
+def own_government_or_able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('try_woman')
---
+++ super/way/make_long_eye/man/eye/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    go_different_world(str_arg)
+    print('part')
+
+def go_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('thing')
