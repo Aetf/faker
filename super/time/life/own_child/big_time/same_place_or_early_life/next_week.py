32ce70c446dd29e28649b6806de5ad6679385b61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:37:04 -0400

    "mix: call super/time/life/own_child/big_time/same_place_or_early_life/next_week.py, super/eye/think_other_group.py, super/thing/part/see_eye/good_number/right_work/long_person_and_early_problem.py or super/first_person_or_young_case/man.py at public man"

---
+++ super/time/life/own_child/big_time/same_place_or_early_life/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    long_part(str_arg)
+    print('early_problem')
+
+def long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('see_hand')
