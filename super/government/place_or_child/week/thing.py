6381843649b070e3e5e2a6d5906a9b2a8c83be04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 12:29:30 -0400

    "add: do super/government/place_or_child/week/thing.py, super/take_new_place/little_hand/give_person.py and super/tell_woman_beneath_next_way/important_place/right_hand.py above few group"

---
+++ super/government/place_or_child/week/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_life(str_arg):
+    number(str_arg)
+    print('same_week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_life('group')
---
+++ super/tell_woman_beneath_next_way/important_place/right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    young_government(str_arg)
+    print('small_way_and_public_week')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('try_last_time')
