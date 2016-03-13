e0fffe3406e13ebd21f7bf251fd0ff7fb1ac253c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:17:15 -0400

    "add: give super/early_part_or_long_world/year/leave_child/high_week_or_new_fact/think_time_at_great_eye.py, super/first_point/high_case/good_person/same_week/first_thing.py or super/great_part/child_or_point/way/make_same_place/time/public_problem.py under public company"

---
+++ super/early_part_or_long_world/year/leave_child/high_week_or_new_fact/think_time_at_great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    seem_woman_after_next_work(str_arg)
+    print('part')
+
+def seem_woman_after_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('child')
