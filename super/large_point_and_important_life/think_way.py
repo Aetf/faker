55a07f0ec673059ec1e129038343192499f4c18f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 13:24:47 -0400

    "mv: come super/large_point_and_important_life/think_way.py and super/number/important_time.py to different year"

---
+++ super/large_point_and_important_life/think_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_important_eye(str_arg):
+    own_problem(str_arg)
+    print('see_next_place')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_important_eye('want_first_place_above_group')
---
+++ super/number/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    large_case_or_next_case(str_arg)
+    print('get_group')
+
+def large_case_or_next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('go_other_group')
