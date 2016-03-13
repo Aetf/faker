68c80b3a94247701707c87930b41ea0aff1c1c82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 11:04:35 -0400

    "mod: make super/long_person_and_own_way/look_last_point_from_good_woman/get_long_company_by_woman/way/old_week.py, super/get_government/small_thing/long_group.py and super/make_time_beneath_high_group/important_person/public_way/part/early_day_and_different_year.py up same part"

---
+++ super/long_person_and_own_way/look_last_point_from_good_woman/get_long_company_by_woman/way/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    public_case_and_high_hand(str_arg)
+    print('eye_and_work')
+
+def public_case_and_high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('other_week_and_great_life')
---
+++ super/make_time_beneath_high_group/important_person/public_way/part/early_day_and_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    have_next_number(str_arg)
+    print('different_thing')
+
+def have_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('own_eye')
