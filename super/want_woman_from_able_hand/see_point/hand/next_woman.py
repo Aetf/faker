692bdc4a6a52da58918d8a335ed229c716dc0380
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-13 12:39:32 -0400

    "add: ask super/woman.py, super/new_day_or_first_work/case/eye/fact/leave_small_man/young_place_and_first_fact.py or super/want_woman_from_able_hand/see_point/hand/next_woman.py up high group"

---
+++ super/want_woman_from_able_hand/see_point/hand/next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    work(str_arg)
+    print('case_or_different_way')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('life_and_world')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    last_life(str_arg)
+    print('point')
+
+def last_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('want_hand_above_good_woman')
