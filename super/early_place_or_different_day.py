c128b9fb5e67d22d305ca7d96e6bf238d5f7be5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:33:18 -0400

    "mv: look super/work/thing_and_old_way/long_government.py, super/early_place_or_different_day.py, super/way_or_day.py or super/way_and_last_world.py with long part"

---
+++ super/early_place_or_different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_fact(str_arg):
+    case_or_large_work(str_arg)
+    print('world')
+
+def case_or_large_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_fact('way')
---
+++ super/way_and_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group(str_arg):
+    old_person(str_arg)
+    print('see_big_part')
+
+def old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group('next_government_and_early_woman')
