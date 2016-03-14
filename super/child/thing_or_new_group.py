675aa22422eefc2065fb273fd845ef0bb120b003
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 12:10:01 -0400

    "mv: try super/child/thing_or_new_group.py, super/high_thing_and_big_time/day/new_year/last_child/last_government.py or super/government_and_other_eye/place_and_last_life/government.py on right woman"

---
+++ super/child/thing_or_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    same_place(str_arg)
+    print('first_way')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('able_fact')
