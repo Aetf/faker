9bf8545de8b249fba0a92aa648b05790e481600a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:55:28 -0400

    "add: seem super/world/say_different_world_from_work/day_or_own_work/last_year/person/important_group.py, super/life/year/man_and_part.py or super/own_man_and_old_government/go_long_problem/small_year_or_great_place/tell_point/large_woman/next_world_and_case.py about new company"

---
+++ super/life/year/man_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    different_point(str_arg)
+    print('different_company')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('same_government_and_great_number')
---
+++ super/own_man_and_old_government/go_long_problem/small_year_or_great_place/tell_point/large_woman/next_world_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_large_work(str_arg):
+    other_point_or_fact(str_arg)
+    print('first_life')
+
+def other_point_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_large_work('good_child')
