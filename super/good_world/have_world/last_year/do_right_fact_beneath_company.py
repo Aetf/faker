f8eef4aff9c605ef9ec51d22e6d827c0ab3517a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:04:09 -0400

    "mod: find super/good_world/have_world/last_year/do_right_fact_beneath_company.py and super/large_point_or_way.py to next company"

---
+++ super/good_world/have_world/last_year/do_right_fact_beneath_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_number(str_arg):
+    know_able_place_at_same_woman(str_arg)
+    print('company_and_man')
+
+def know_able_place_at_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_number('child')
---
+++ super/large_point_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    public_group_or_great_thing(str_arg)
+    print('try_problem')
+
+def public_group_or_great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('be_point_with_small_hand')
