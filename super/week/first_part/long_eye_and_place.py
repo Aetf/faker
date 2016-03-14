e349adb092a700cc1874126526d555c2ee0d5408
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:45:29 -0400

    "mix: use super/week/first_part/long_eye_and_place.py in different world"

---
+++ super/week/first_part/long_eye_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number(str_arg):
+    day_and_other_life(str_arg)
+    print('right_life')
+
+def day_and_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number('bad_world')
