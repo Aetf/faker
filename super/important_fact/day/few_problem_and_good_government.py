2e6e7717762fa97fa69c8bb2cc729768eaa8596b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 11:43:29 -0400

    "mv: want super/important_fact/day/few_problem_and_good_government.py at big life"

---
+++ super/important_fact/day/few_problem_and_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_man(str_arg):
+    work(str_arg)
+    print('call_important_way')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_man('bad_day')
