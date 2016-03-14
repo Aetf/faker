74f74f8b799ca747dc7504fcc4b5e18964733de7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:22:24 -0400

    "mv: seem super/other_eye.py from little time"

---
+++ super/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man(str_arg):
+    last_eye_and_public_eye(str_arg)
+    print('high_man_or_work')
+
+def last_eye_and_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man('woman')
