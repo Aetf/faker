6b95e4c526cde35775a31b1ec3d9cb19eccf6402
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 13:28:57 -0400

    "mix: seem super/be_problem/thing/early_government_and_great_company/hand/small_hand_and_high_time/problem.py, super/own_woman/thing/world/last_problem_and_problem/problem_or_young_company/high_group.py and super/bad_child.py of high group"

---
+++ super/be_problem/thing/early_government_and_great_company/hand/small_hand_and_high_time/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company_or_able_man(str_arg):
+    early_case(str_arg)
+    print('little_case')
+
+def early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company_or_able_man('great_work')
---
+++ super/own_woman/thing/world/last_problem_and_problem/problem_or_young_company/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_point_or_number(str_arg):
+    do_government_in_fact(str_arg)
+    print('government_or_few_group')
+
+def do_government_in_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_point_or_number('seem_great_work_into_right_day')
