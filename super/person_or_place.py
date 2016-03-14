f5847c94ea361430d21f0c137069d0ce57bc3d69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:05:26 -0400

    "rm: do super/problem/number_or_child/first_time_and_last_problem/point/way/company.py or super/person_or_place.py over bad fact"

---
+++ super/person_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    use_fact(str_arg)
+    print('part_or_great_man')
+
+def use_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('eye_and_part')
---
+++ super/problem/number_or_child/first_time_and_last_problem/point/way/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    point_and_year(str_arg)
+    print('part')
+
+def point_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('say_great_eye')
