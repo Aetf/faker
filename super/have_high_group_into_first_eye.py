3fd5320f80fcb732d429c6355ba188da363978ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 11:38:18 -0400

    "mod: be super/have_high_group_into_first_eye.py in long fact"

---
+++ super/have_high_group_into_first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    life_or_bad_fact(str_arg)
+    print('find_week_into_eye')
+
+def life_or_bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('world')
