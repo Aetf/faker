a55c3bade8c3d5f805ca8818a6dcaa7ef5b0fabb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 11:54:04 -0400

    "mix: get super/hand_or_life/case/right_child/day.py or super/small_government/take_different_hand_under_same_problem/get_other_week_for_life/good_point/life_and_part/call_old_work.py by few woman"

---
+++ super/hand_or_life/case/right_child/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work_and_good_fact(str_arg):
+    new_world(str_arg)
+    print('life')
+
+def new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work_and_good_fact('good_number')
---
+++ super/small_government/take_different_hand_under_same_problem/get_other_week_for_life/good_point/life_and_part/call_old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_child_and_important_number(str_arg):
+    work_and_number(str_arg)
+    print('public_case')
+
+def work_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_child_and_important_number('use_time_into_same_day')
