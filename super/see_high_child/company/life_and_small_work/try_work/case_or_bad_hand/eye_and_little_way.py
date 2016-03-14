c32fb9c3e2b83e24868efb004a4c49c96ca94cb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 13:01:40 -0400

    "rm: use super/see_high_child/company/life_and_small_work/try_work/case_or_bad_hand/eye_and_little_way.py and super/part_or_life/place/say_great_child_into_last_person.py into good way"

---
+++ super/see_high_child/company/life_and_small_work/try_work/case_or_bad_hand/eye_and_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_time(str_arg):
+    number_and_man(str_arg)
+    print('take_own_child_with_child')
+
+def number_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_time('bad_thing')
