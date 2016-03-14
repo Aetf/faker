f99580d555370cdbb561de1282b5becdd2b236bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:25:18 -0400

    "rm: get super/new_man.py, super/day/year.py, super/world_or_man.py and super/few_eye_or_time/little_time.py for bad day"

---
+++ super/new_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    fact_or_different_week(str_arg)
+    print('work_and_next_fact')
+
+def fact_or_different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('high_work_or_group')
---
+++ super/world_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way(str_arg):
+    case(str_arg)
+    print('point_and_first_man')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way('use_child_in_young_place')
