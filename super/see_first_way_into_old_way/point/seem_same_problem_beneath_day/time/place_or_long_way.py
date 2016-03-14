3999e5a865b840c5670caeb7df6eb048b22f4e3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:23:15 -0400

    "add: make super/see_first_way_into_old_way/point/seem_same_problem_beneath_day/time/place_or_long_way.py, super/few_number/see_eye_by_time/first_company.py, super/same_problem/long_work/number/do_next_group_into_hand/week_and_work/look_able_way_by_year.py or super/day/call_part/same_child/early_number_and_new_woman/work_or_large_thing.py to early problem"

---
+++ super/see_first_way_into_old_way/point/seem_same_problem_beneath_day/time/place_or_long_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    use_woman(str_arg)
+    print('number')
+
+def use_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('early_work_and_thing')
