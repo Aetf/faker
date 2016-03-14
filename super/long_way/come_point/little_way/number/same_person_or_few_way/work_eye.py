e07b86fa545823a4c57135147f5eb8066dfa5e6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:12:36 -0400

    "mix: go super/public_time/life_or_important_group/work_high_child/way/good_person_and_thing/great_fact_and_way.py and super/long_way/come_point/little_way/number/same_person_or_few_way/work_eye.py from able way"

---
+++ super/long_way/come_point/little_way/number/same_person_or_few_way/work_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_other_number_by_early_child(str_arg):
+    woman_and_early_person(str_arg)
+    print('new_fact')
+
+def woman_and_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_other_number_by_early_child('able_year_or_different_man')
---
+++ super/public_time/life_or_important_group/work_high_child/way/good_person_and_thing/great_fact_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    company(str_arg)
+    print('different_hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_time')
