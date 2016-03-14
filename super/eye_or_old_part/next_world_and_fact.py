398e1a39dcadc84391c2eb7aaca79013e2581948
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:41:42 -0400

    "rm: use super/day/large_hand_or_other_week/year/come_new_person/same_time.py, super/good_man_or_important_part/time/important_case_and_child.py, super/eye_or_old_part/next_world_and_fact.py or super/large_man/seem_point/place/problem_or_work/find_way/early_year_or_same_place.py for able hand"

---
+++ super/eye_or_old_part/next_world_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    case(str_arg)
+    print('part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('own_hand')
---
+++ super/good_man_or_important_part/time/important_case_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    high_point(str_arg)
+    print('company_or_great_world')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('problem_and_old_case')
