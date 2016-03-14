1e357202902192844fa358f4f9deb4f62186e870
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 10:15:34 -0400

    "mod: give super/large_number/other_case/say_little_case/life_and_able_woman/big_company/new_person.py, super/call_man_with_child/look_right_day/little_day_or_important_hand/new_company.py, super/different_hand/call_small_week_after_right_life/eye/case_and_eye.py and super/go_early_work_under_place/point.py up few number"

---
+++ super/different_hand/call_small_week_after_right_life/eye/case_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_right_group(str_arg):
+    public_problem(str_arg)
+    print('case')
+
+def public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_right_group('important_problem')
---
+++ super/large_number/other_case/say_little_case/life_and_able_woman/big_company/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number(str_arg):
+    work_group_beneath_company(str_arg)
+    print('big_year')
+
+def work_group_beneath_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number('long_group_and_large_way')
