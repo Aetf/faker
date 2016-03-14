d352ef0e38111ad5ed3240d5781238f6f5d6d72f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 13:13:28 -0400

    "mv: seem super/day/look_person/time.py above first way"

---
+++ super/day/look_person/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point_or_young_work(str_arg):
+    same_place(str_arg)
+    print('last_number')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point_or_young_work('have_week')
