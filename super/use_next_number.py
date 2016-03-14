7219bb2ba6ac7bba850581f62b5ec7fa693b72ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-14 14:23:37 -0400

    "mod: try super/use_next_number.py on public hand"

---
+++ super/use_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_new_way_about_other_company(str_arg):
+    world(str_arg)
+    print('point')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_new_way_about_other_company('time')
