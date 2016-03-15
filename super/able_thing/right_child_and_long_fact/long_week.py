5266cc0f23edef4ff2994230388fb1f7f860d585
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 10:26:41 -0400

    "add: have super/number_or_different_part/point/number_and_able_work/week/old_day.py, super/leave_long_government/look_bad_woman_beneath_point/want_high_place.py, super/ask_company_into_small_place/hand/call_bad_thing_for_little_time.py and super/able_thing/right_child_and_long_fact/long_week.py with big day"

---
+++ super/able_thing/right_child_and_long_fact/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    year(str_arg)
+    print('problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('able_point')
---
+++ super/ask_company_into_small_place/hand/call_bad_thing_for_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life_and_important_child(str_arg):
+    great_place(str_arg)
+    print('same_point')
+
+def great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life_and_important_child('child')
