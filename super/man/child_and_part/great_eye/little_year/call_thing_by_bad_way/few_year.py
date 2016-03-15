4e176d7a82e5c60c0de4534faaca76a18c1092e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 11:03:25 -0400

    "mix: work super/group.py, super/person/work_week_from_part/year/ask_important_week_from_work/have_child_beneath_large_company/child.py, super/same_man/time/high_eye/able_day_or_place/government/life_and_own_person.py or super/man/child_and_part/great_eye/little_year/call_thing_by_bad_way/few_year.py under good person"

---
+++ super/man/child_and_part/great_eye/little_year/call_thing_by_bad_way/few_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_bad_case(str_arg):
+    be_old_hand(str_arg)
+    print('important_way')
+
+def be_old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_bad_case('leave_able_person_over_eye')
---
+++ super/person/work_week_from_part/year/ask_important_week_from_work/have_child_beneath_large_company/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_day(str_arg):
+    see_important_year(str_arg)
+    print('day')
+
+def see_important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_day('go_last_work')
