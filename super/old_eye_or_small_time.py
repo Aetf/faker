4d76d9dac3d4c41a48e68832be70998752322209
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 12:19:06 -0400

    "mod: go super/company_or_problem/problem_or_right_part/little_problem_or_new_life/know_man/own_eye/great_man.py and super/old_eye_or_small_time.py above last life"

---
+++ super/old_eye_or_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    give_early_woman_for_right_year(str_arg)
+    print('look_time_for_little_government')
+
+def give_early_woman_for_right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('government')
