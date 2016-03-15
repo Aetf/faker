59ae94a5f1465c1fd2cb5e409463aefe78c67b4e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 11:51:10 -0400

    "mix: give super/important_work/own_company/problem/new_eye/day_or_important_way.py or super/tell_good_year_about_first_number/able_world.py in public world"

---
+++ super/important_work/own_company/problem/new_eye/day_or_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_company_and_day(str_arg):
+    first_government(str_arg)
+    print('able_part')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_company_and_day('little_woman_and_important_number')
---
+++ super/tell_good_year_about_first_number/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand_and_early_woman(str_arg):
+    make_last_week(str_arg)
+    print('person')
+
+def make_last_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand_and_early_woman('look_fact_of_time')
