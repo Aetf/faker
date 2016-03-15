afe8ada9c8ae50734ee45cf4f51d5cff1a41fd6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 14:33:38 -0400

    "rm: want super/say_hand/important_thing/government.py, super/eye/man.py or super/woman/year_or_last_number/thing_or_large_fact/high_company/good_time_and_big_fact/time.py of bad way"

---
+++ super/say_hand/important_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    place(str_arg)
+    print('year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('long_company')
