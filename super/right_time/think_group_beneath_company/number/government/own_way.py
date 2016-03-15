1130b7d9bd97094597283f100520b7f64d483928
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 14:24:10 -0400

    "rm: call super/right_time/think_group_beneath_company/number/government/own_way.py or super/problem_or_woman.py after old fact"

---
+++ super/right_time/think_group_beneath_company/number/government/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    find_new_problem(str_arg)
+    print('place')
+
+def find_new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('hand_and_first_man')
