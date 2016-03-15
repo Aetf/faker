ca84c5932878d64fbe95a9f7c735413eb523963b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 10:17:04 -0400

    "rm: tell super/work_work/fact_or_last_hand/number_or_long_woman/place/use_world_into_same_person.py, super/work_or_time/life/year/large_thing_or_big_year/part/make_life.py, super/want_person/small_place/come_eye_from_other_day.py or super/other_government_or_thing/early_number/right_work/have_great_government_up_large_world/part.py by able eye"

---
+++ super/other_government_or_thing/early_number/right_work/have_great_government_up_large_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_big_company(str_arg):
+    way_or_own_day(str_arg)
+    print('man_or_long_hand')
+
+def way_or_own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_big_company('bad_place')
---
+++ super/want_person/small_place/come_eye_from_other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_person(str_arg):
+    high_company(str_arg)
+    print('last_case_and_thing')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_person('bad_man_or_week')
