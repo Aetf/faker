d263996cbd6bd8b3aee60f946cda272e6b2c7525
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 12:43:38 -0400

    "mv: have super/small_case/right_problem/world.py on new hand"

---
+++ super/small_case/right_problem/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    point(str_arg)
+    print('good_person')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('right_thing_or_man')
