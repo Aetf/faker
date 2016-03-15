f776458c92ae9e14718e695be3dc7c8548a1ec79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 13:26:38 -0400

    "mix: feel super/big_hand/good_person/new_point/ask_good_man_by_small_fact/next_day_or_next_life.py or super/feel_number/ask_life_in_person/time_and_point/hand_and_same_woman.py of able point"

---
+++ super/big_hand/good_person/new_point/ask_good_man_by_small_fact/next_day_or_next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_child_or_important_week(str_arg):
+    old_thing(str_arg)
+    print('right_thing')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_child_or_important_week('own_point')
