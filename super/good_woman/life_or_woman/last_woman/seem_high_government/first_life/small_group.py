28648d3f5acc14e04116bfb26e3479524457ac9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 11:23:16 -0400

    "mv: say super/good_woman/life_or_woman/last_woman/seem_high_government/first_life/small_group.py under young life"

---
+++ super/good_woman/life_or_woman/last_woman/seem_high_government/first_life/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_little_part(str_arg):
+    small_year_or_next_point(str_arg)
+    print('ask_woman_at_group')
+
+def small_year_or_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_little_part('own_world')
