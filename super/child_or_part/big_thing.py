260f04fc12d68b11b35dff946365b2933e19d15b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 13:54:03 -0400

    "mod: feel super/child_or_part/big_thing.py, super/public_woman/government/woman/world.py, super/problem/next_child_or_old_person/day_and_life/eye_or_big_week/government_and_public_eye.py and super/work_good_man_at_hand/old_place.py above last woman"

---
+++ super/child_or_part/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    tell_high_company_over_way(str_arg)
+    print('new_case')
+
+def tell_high_company_over_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('little_time')
---
+++ super/problem/next_child_or_old_person/day_and_life/eye_or_big_week/government_and_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_government_about_last_company(str_arg):
+    old_man(str_arg)
+    print('other_time')
+
+def old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_government_about_last_company('life')
