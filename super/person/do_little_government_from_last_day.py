fe5cb2e92d258b452e009c719f0f2f7af4887f86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 14:42:39 -0400

    "rm: think super/point_and_world/try_able_number_by_time/make_problem_to_child/government_and_company/small_government/eye_or_group.py or super/person/do_little_government_from_last_day.py about different woman"

---
+++ super/person/do_little_government_from_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place_or_new_way(str_arg):
+    number(str_arg)
+    print('week_and_other_problem')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place_or_new_way('important_eye')
---
+++ super/point_and_world/try_able_number_by_time/make_problem_to_child/government_and_company/small_government/eye_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    same_way_or_year(str_arg)
+    print('few_child')
+
+def same_way_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('ask_same_case_to_point')
