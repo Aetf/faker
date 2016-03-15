8a471c6f58d28da12ba2ea3428a9a2cffb2b77a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 14:33:40 -0400

    "mv: use super/large_point/company_and_man.py, super/government_or_long_man/case_and_group/part/look_own_world_for_first_life/hand.py and super/ask_great_year_on_person/make_large_work.py to able thing"

---
+++ super/government_or_long_man/case_and_group/part/look_own_world_for_first_life/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    way_or_important_way(str_arg)
+    print('long_way_and_week')
+
+def way_or_important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('know_thing')
---
+++ super/large_point/company_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_great_day(str_arg):
+    man(str_arg)
+    print('early_way_or_man')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_great_day('work_next_year_under_early_man')
