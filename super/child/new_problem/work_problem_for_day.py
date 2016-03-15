65c9923e3a7628271e1098faecf0281791e07515
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 10:33:59 -0400

    "mv: say super/child/new_problem/work_problem_for_day.py, super/early_way/able_government/work.py and super/group/old_government_or_year/think_few_place_for_way.py about last world"

---
+++ super/child/new_problem/work_problem_for_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    own_year(str_arg)
+    print('same_world_and_time')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('day')
