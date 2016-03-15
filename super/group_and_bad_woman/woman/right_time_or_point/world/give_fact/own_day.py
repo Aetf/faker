b841ea8f5bec4ffe3f2ef0a78adbc6cd298fa3dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 12:26:56 -0400

    "add: be super/group_and_bad_woman/woman/right_time_or_point/world/give_fact/own_day.py for long problem"

---
+++ super/group_and_bad_woman/woman/right_time_or_point/world/give_fact/own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_group_about_part(str_arg):
+    do_old_week_about_day(str_arg)
+    print('work')
+
+def do_old_week_about_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_group_about_part('hand')
