4e7030e5b05cace8a068f9e3522c506b80b1688c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 11:11:40 -0400

    "mv: take super/number_and_problem/try_good_life/number.py for right fact"

---
+++ super/number_and_problem/try_good_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    child(str_arg)
+    print('do_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('be_day')
