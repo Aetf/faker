d12a039f6c97ccc83d021e868c6134eaaa43b237
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 13:29:24 -0400

    "rm: want super/look_life_at_place/way_and_woman.py and super/important_hand_or_week.py by important group"

---
+++ super/important_hand_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_old_point(str_arg):
+    able_group_and_part(str_arg)
+    print('public_thing_or_man')
+
+def able_group_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_old_point('part')
---
+++ super/look_life_at_place/way_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    person(str_arg)
+    print('case')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('day')
