d5089ac23b85e60fc4aeae67a4431a9aa5bbbff6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 11:54:17 -0400

    "mv: get super/government/know_few_child_above_woman/week/make_point.py, super/great_eye.py or super/be_government_to_little_government.py into good number"

---
+++ super/government/know_few_child_above_woman/week/make_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    thing(str_arg)
+    print('own_point_and_time')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('part_and_day')
---
+++ super/great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    try_work_after_able_world(str_arg)
+    print('same_time')
+
+def try_work_after_able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('take_eye')
