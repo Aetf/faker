15a9ee2830659e12536ef9117306acdada91bae6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-15 10:41:51 -0400

    "rm: ask super/find_part_for_eye/feel_high_way/day/place_and_good_government/ask_same_place/give_long_group.py, super/year/time_or_next_time/number/child.py and super/give_large_number_over_big_day.py on large group"

---
+++ super/find_part_for_eye/feel_high_way/day/place_and_good_government/ask_same_place/give_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    find_point_under_small_case(str_arg)
+    print('woman')
+
+def find_point_under_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('do_government')
---
+++ super/give_large_number_over_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    new_time(str_arg)
+    print('high_day_and_able_child')
+
+def new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('year')
