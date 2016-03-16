6978d8f067664b4465c35b231d6be899f5c3bc2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 11:04:03 -0400

    "mod: ask super/time/few_part/have_government.py beneath long company"

---
+++ super/time/few_part/have_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person_or_own_eye(str_arg):
+    case_or_hand(str_arg)
+    print('long_day')
+
+def case_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person_or_own_eye('time')
