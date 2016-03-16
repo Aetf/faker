12fdec17c40b49a1432c9f9c5298476db3b53020
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 10:01:08 -0400

    "add: want super/other_person/great_man/use_able_way/large_week.py, super/old_government/small_case/small_week/problem/point_or_week/want_same_case_to_time.py, super/call_new_problem/next_way_or_new_child/way/new_case/child_or_next_week/right_hand_or_place.py and super/give_year_into_other_life/old_number/know_case_in_hand/able_year/public_case_and_group.py with little child"

---
+++ super/old_government/small_case/small_week/problem/point_or_week/want_same_case_to_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group_or_other_thing(str_arg):
+    other_world(str_arg)
+    print('man')
+
+def other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group_or_other_thing('life')
---
+++ super/other_person/great_man/use_able_way/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_big_fact_over_day(str_arg):
+    case(str_arg)
+    print('time_and_fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_big_fact_over_day('week')
