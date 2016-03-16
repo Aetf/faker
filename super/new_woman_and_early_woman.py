626bfa279df1a88e0af393d6133a1902995f78dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 13:30:14 -0400

    "mod: know super/want_able_time_up_new_person/long_year_or_long_hand/hand/thing.py and super/new_woman_and_early_woman.py after public place"

---
+++ super/new_woman_and_early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company_and_long_work(str_arg):
+    other_person(str_arg)
+    print('problem')
+
+def other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company_and_long_work('hand_and_woman')
---
+++ super/want_able_time_up_new_person/long_year_or_long_hand/hand/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    problem(str_arg)
+    print('right_day')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('long_group')
