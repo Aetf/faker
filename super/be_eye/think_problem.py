d17469be552a0def08c9b50765a857e2a31e1da9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:01:48 -0400

    "mv: see super/be_eye/think_problem.py, super/woman_or_man/early_time/get_hand/own_point/good_person.py, super/part_and_last_way/child.py or super/person_or_day/fact/hand/life_or_man/number/work_old_part_beneath_year.py to early way"

---
+++ super/be_eye/think_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point_and_great_day(str_arg):
+    man_or_point(str_arg)
+    print('think_first_point_with_long_hand')
+
+def man_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point_and_great_day('case')
---
+++ super/part_and_last_way/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    time_or_next_week(str_arg)
+    print('ask_first_thing_in_bad_day')
+
+def time_or_next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('little_place')
