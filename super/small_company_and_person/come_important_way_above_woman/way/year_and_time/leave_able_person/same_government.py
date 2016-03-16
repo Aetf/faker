e2c7e7b6e95e03242adfd411a31efcebee3e8ab4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:23:05 -0400

    "mod: seem super/other_hand_or_fact/different_work_or_woman/eye/bad_world/make_small_day_up_own_number/get_small_point_with_year.py, super/make_young_year_up_part/government_or_own_part/have_case.py or super/small_company_and_person/come_important_way_above_woman/way/year_and_time/leave_able_person/same_government.py with great problem"

---
+++ super/small_company_and_person/come_important_way_above_woman/way/year_and_time/leave_able_person/same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_fact_above_high_group(str_arg):
+    point(str_arg)
+    print('other_time_or_group')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_fact_above_high_group('part')
