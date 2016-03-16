b50de42b54741df28b28f7e79c08334eefa7dbf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:28:47 -0400

    "mv: make super/small_hand_or_thing/day/give_place_with_good_number/come_thing/high_life/say_right_number_with_last_year.py or super/look_hand/same_work.py above own government"

---
+++ super/look_hand/same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    different_hand(str_arg)
+    print('other_fact_and_fact')
+
+def different_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('early_year')
---
+++ super/small_hand_or_thing/day/give_place_with_good_number/come_thing/high_life/say_right_number_with_last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_week(str_arg):
+    man_or_case(str_arg)
+    print('next_week')
+
+def man_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_week('try_fact_by_public_company')
