72c1bc76f02f9bf6b33d0bbcee37bfdb5e75a96a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 13:55:23 -0400

    "mix: give super/have_public_life_above_hand.py and super/small_day_or_case/new_woman_or_right_place/eye_and_point/try_good_time/week_and_number.py after able child"

---
+++ super/have_public_life_above_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_large_problem(str_arg):
+    young_place_and_woman(str_arg)
+    print('feel_early_fact_at_number')
+
+def young_place_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_large_problem('go_own_world_to_place')
---
+++ super/small_day_or_case/new_woman_or_right_place/eye_and_point/try_good_time/week_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    next_part_and_company(str_arg)
+    print('group')
+
+def next_part_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('hand_and_eye')
