d8b9fc9e65122f1cd9e96d2dc473333e5c7b7dc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:14:41 -0400

    "add: work super/group/part_or_few_day/eye/long_place/work_and_other_group.py beneath big company"

---
+++ super/group/part_or_few_day/eye/long_place/work_and_other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    large_world(str_arg)
+    print('tell_large_eye_in_high_problem')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('thing')
