56fedd9c152bb9fa43dc28f35a981db0db8b97ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 12:49:25 -0400

    "mv: get super/get_problem_after_next_person/thing/number_and_young_fact.py, super/high_eye_or_part/thing/fact_and_bad_time/other_time_and_next_day/bad_fact_or_old_woman.py, super/go_great_group/early_year/have_bad_day/point/able_week/little_way.py and super/life_and_woman/next_woman_and_eye/give_new_man_after_early_way/year/work_and_important_woman/bad_hand.py in own time"

---
+++ super/get_problem_after_next_person/thing/number_and_young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_new_world(str_arg):
+    old_point(str_arg)
+    print('try_last_part')
+
+def old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_new_world('use_case')
---
+++ super/life_and_woman/next_woman_and_eye/give_new_man_after_early_way/year/work_and_important_woman/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    first_thing(str_arg)
+    print('new_life_or_able_number')
+
+def first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('able_thing')
