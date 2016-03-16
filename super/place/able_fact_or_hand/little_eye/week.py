3fcdc184ea5cf296d9ce4991ad608b10f05ed263
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 11:15:53 -0400

    "rm: do super/number.py, super/own_point_and_same_place/little_person/way_and_number/right_number.py, super/place/able_fact_or_hand/little_eye/week.py and super/look_day.py up old year"

---
+++ super/place/able_fact_or_hand/little_eye/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    make_person(str_arg)
+    print('day_or_early_fact')
+
+def make_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('be_new_day_on_new_case')
