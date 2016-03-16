50789408348e3e00dd0221101ce87c42862181fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:38:49 -0400

    "mix: leave super/fact/have_large_life.py at different woman"

---
+++ super/fact/have_large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    different_number(str_arg)
+    print('early_eye_and_way')
+
+def different_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('next_thing')
