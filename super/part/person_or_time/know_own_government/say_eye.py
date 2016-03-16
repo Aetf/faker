8f89e3f84596fbcc44d69c97537d190db2538df9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 13:07:22 -0400

    "mix: seem super/bad_thing_and_government/want_small_week/ask_next_fact/be_case/fact_or_small_man/try_group.py, super/small_year/hand/few_woman/different_hand.py and super/part/person_or_time/know_own_government/say_eye.py after same place"

---
+++ super/part/person_or_time/know_own_government/say_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_hand_by_early_point(str_arg):
+    want_hand_with_good_point(str_arg)
+    print('high_fact_and_problem')
+
+def want_hand_with_good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_hand_by_early_point('world_and_fact')
---
+++ super/small_year/hand/few_woman/different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_little_government_above_woman(str_arg):
+    life(str_arg)
+    print('first_life_and_day')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_little_government_above_woman('day')
