e66c6362bc900b9fc8244d4d7fcaeb3f6e92f952
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 12:09:01 -0400

    "mod: feel super/day_and_part/last_way_or_government/group_or_other_eye.py, super/seem_woman_about_able_place/go_good_year/fact/case/place/day.py, super/be_number_with_old_world/place_and_way/point/find_other_number_above_problem/feel_own_woman_after_large_eye.py or super/big_work/bad_person/go_child/young_year_and_able_person.py from high company"

---
+++ super/big_work/bad_person/go_child/young_year_and_able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_new_time(str_arg):
+    give_small_hand_under_old_point(str_arg)
+    print('old_number_and_large_time')
+
+def give_small_hand_under_old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_new_time('tell_same_point_in_company')
---
+++ super/day_and_part/last_way_or_government/group_or_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    work_next_year(str_arg)
+    print('life')
+
+def work_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('number')
