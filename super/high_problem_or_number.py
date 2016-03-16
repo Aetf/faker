b5c27a74c370cc3c22aef7266fcb01184d76e74a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 10:28:10 -0400

    "add: feel super/high_problem_or_number.py under few man"

---
+++ super/high_problem_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_early_way(str_arg):
+    few_person(str_arg)
+    print('go_life_about_man')
+
+def few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_early_way('able_hand_or_part')
