bd31f1b4139dc55962d36f40dd7dc9ecbf1bc3b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 13:01:27 -0400

    "mix: want super/public_company/feel_group.py, super/big_company/day/young_government/world/bad_place_and_public_case/last_problem_or_man.py or super/group/come_man/tell_great_person_beneath_large_case/time_and_good_company.py by small way"

---
+++ super/public_company/feel_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group_or_government(str_arg):
+    number(str_arg)
+    print('work_or_fact')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group_or_government('think_government_by_child')
