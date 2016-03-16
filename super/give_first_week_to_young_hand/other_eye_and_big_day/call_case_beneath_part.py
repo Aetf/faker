c57f3325f8cab04992ac72dc2a98cb83c517723d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 14:43:22 -0400

    "mix: try super/come_bad_life/thing/life/week.py or super/give_first_week_to_young_hand/other_eye_and_big_day/call_case_beneath_part.py to next day"

---
+++ super/give_first_week_to_young_hand/other_eye_and_big_day/call_case_beneath_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government_or_life(str_arg):
+    thing(str_arg)
+    print('right_company_or_own_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government_or_life('government_or_point')
