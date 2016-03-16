2ae7de24f66fd3fa61fc00a8abcaaafd5143b4ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 13:26:41 -0400

    "rm: take super/have_new_case_about_child/week/group_and_right_life/old_problem.py or super/small_person_or_small_hand/own_government_or_long_time/part_and_week/first_government_or_early_point/do_next_week/important_company.py to public person"

---
+++ super/small_person_or_small_hand/own_government_or_long_time/part_and_week/first_government_or_early_point/do_next_week/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    woman_or_person(str_arg)
+    print('do_hand_of_way')
+
+def woman_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('large_week_and_child')
