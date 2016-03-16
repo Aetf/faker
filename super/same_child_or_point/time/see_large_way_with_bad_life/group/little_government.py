123fe791ce2dfde4d9a9f4ae0fc3af7b63448f75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 11:36:37 -0400

    "rm: try super/use_number_at_place/last_woman/different_day/look_early_part_at_work/large_time.py, super/own_man/say_part_to_right_government/fact_and_first_number/little_case/work_own_company/see_early_day_after_young_case.py and super/same_child_or_point/time/see_large_way_with_bad_life/group/little_government.py to good eye"

---
+++ super/same_child_or_point/time/see_large_way_with_bad_life/group/little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    fact(str_arg)
+    print('next_fact_or_woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('case')
---
+++ super/use_number_at_place/last_woman/different_day/look_early_part_at_work/large_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_government_beneath_thing(str_arg):
+    thing(str_arg)
+    print('life_or_same_week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_government_beneath_thing('child')
