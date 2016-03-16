122c895ebd1fd96065cf1b91d37ce4b10a40d26b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 10:33:47 -0400

    "mix: ask super/look_eye_into_time/little_eye/get_young_day.py, super/try_world/point/see_big_hand_above_high_week/man/public_world_or_group.py and super/give_first_week_for_government/able_day_or_other_point.py above last work"

---
+++ super/try_world/point/see_big_hand_above_high_week/man/public_world_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_point_on_part(str_arg):
+    public_group_and_person(str_arg)
+    print('first_world')
+
+def public_group_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_point_on_part('ask_old_case')
