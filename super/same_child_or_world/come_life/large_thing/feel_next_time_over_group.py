eafadbd1de890d888678e44628f72a6cdf941fa5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-16 12:56:36 -0400

    "rm: think super/call_right_woman.py, super/same_child_or_world/come_life/large_thing/feel_next_time_over_group.py, super/group/come_good_way/high_world/feel_first_life/come_young_year.py or super/have_big_year/get_good_case/take_high_group_of_thing.py about good eye"

---
+++ super/same_child_or_world/come_life/large_thing/feel_next_time_over_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_work(str_arg):
+    hand(str_arg)
+    print('able_woman_and_case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_work('leave_important_way')
