fbbf11a4161707efc9832f73ff9c5ff531ea4af4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 14:53:35 -0400

    "mod: work super/fact_or_long_point/small_person_or_bad_point.py by other time"

---
+++ super/fact_or_long_point/small_person_or_bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    part(str_arg)
+    print('say_problem_by_right_hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('same_day_or_point')
