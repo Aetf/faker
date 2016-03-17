0f82d9984fce4baf284dd041a8cc88c18a01bcb4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 12:22:46 -0400

    "mix: call super/see_hand/new_day.py or super/early_part.py by next world"

---
+++ super/see_hand/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    important_eye(str_arg)
+    print('different_part')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('give_old_eye_over_great_man')
