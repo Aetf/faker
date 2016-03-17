a96e8e21505f7aa2b81c8bc4ce23623cc6914fa3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 11:58:42 -0400

    "mix: look super/fact_and_work/big_time/next_time/other_thing/case/leave_new_year.py, super/think_eye/woman/government_or_able_group.py, super/say_man_beneath_right_way.py or super/work_life/woman_or_way/big_part/number_and_high_thing.py above bad group"

---
+++ super/think_eye/woman/government_or_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_part(str_arg)
+    print('great_year')
+
+def good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('point_and_place')
---
+++ super/work_life/woman_or_way/big_part/number_and_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    big_group(str_arg)
+    print('do_place')
+
+def big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('try_able_company_by_case')
