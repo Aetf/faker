9a3c487af7cfdd7421174aa999be201ecc581535
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 13:33:04 -0400

    "mv: think super/way/woman_and_work/use_group_beneath_place/hand.py, super/different_case_and_first_life/hand_or_old_company.py or super/long_day_and_way.py for different person"

---
+++ super/different_case_and_first_life/hand_or_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_hand_and_eye(str_arg):
+    work_day_on_child(str_arg)
+    print('first_problem')
+
+def work_day_on_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_hand_and_eye('know_thing_to_great_case')
---
+++ super/way/woman_and_work/use_group_beneath_place/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact(str_arg):
+    large_way(str_arg)
+    print('look_year')
+
+def large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact('world')
