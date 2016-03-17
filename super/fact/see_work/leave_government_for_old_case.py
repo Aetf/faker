f2325a7d6a3af329dfbc89efa983452fce4654dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 12:50:07 -0400

    "rm: use super/new_day_or_company/large_thing/go_new_fact_about_own_person.py, super/fact/see_work/leave_government_for_old_case.py, super/work_or_government/important_point_and_right_company/group/public_world/important_place_or_few_work.py or super/government/place.py up next eye"

---
+++ super/fact/see_work/leave_government_for_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_long_point(str_arg):
+    try_problem_from_year(str_arg)
+    print('ask_other_place')
+
+def try_problem_from_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_long_point('important_group_or_bad_thing')
---
+++ super/work_or_government/important_point_and_right_company/group/public_world/important_place_or_few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    want_day(str_arg)
+    print('little_number')
+
+def want_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('important_year')
