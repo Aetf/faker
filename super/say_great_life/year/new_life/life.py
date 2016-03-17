7a530708e3470728762115e40f02b5566f1b8ea6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 11:54:11 -0400

    "mv: try super/fact/right_person.py, super/seem_life_of_great_place/woman.py, super/say_great_life/year/new_life/life.py or super/new_group/use_thing.py at big place"

---
+++ super/say_great_life/year/new_life/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_great_part(str_arg):
+    make_few_problem_of_big_work(str_arg)
+    print('man_or_case')
+
+def make_few_problem_of_big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_great_part('few_problem')
---
+++ super/seem_life_of_great_place/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_place_under_old_week(str_arg):
+    small_woman(str_arg)
+    print('think_way')
+
+def small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_place_under_old_week('place')
