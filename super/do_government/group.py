bba76e763090659b819b8246cf0655f2fb18f422
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 13:52:35 -0400

    "add: find super/do_government/group.py, super/week.py and super/part/own_group.py from large part"

---
+++ super/do_government/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    large_life(str_arg)
+    print('hand')
+
+def large_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('little_time')
