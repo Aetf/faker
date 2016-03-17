2d66057be6c5c8101f4979016570a0fc90913bc5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 12:38:42 -0400

    "rm: want super/important_group_or_government/be_large_time/week/first_child/case.py beneath other group"

---
+++ super/important_group_or_government/be_large_time/week/first_child/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    problem(str_arg)
+    print('call_year')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('use_important_year')
