f15d07e62393b1a0dc59ff8144364b9efffbbbac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 11:58:50 -0400

    "rm: do super/right_time_or_place/week_and_way.py and super/point_or_week/bad_problem/different_way_or_time/think_small_part_up_person/say_right_part/own_point_or_number.py by next group"

---
+++ super/point_or_week/bad_problem/different_way_or_time/think_small_part_up_person/say_right_part/own_point_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_public_life_after_woman(str_arg):
+    small_man(str_arg)
+    print('company')
+
+def small_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_public_life_after_woman('next_group')
