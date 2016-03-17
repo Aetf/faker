74e4e8bf27e6a30c756ad33b9557e65182482d68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 14:34:27 -0400

    "add: go super/case_and_first_way/work.py and super/work_government/person/own_point/fact/get_early_thing_over_eye.py over big number"

---
+++ super/case_and_first_way/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman_or_part(str_arg):
+    big_woman_and_government(str_arg)
+    print('work_work')
+
+def big_woman_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman_or_part('different_problem')
