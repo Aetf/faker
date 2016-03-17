f8bc4a7ca39465e677574ac1105f1c6cca70d354
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 10:34:48 -0400

    "add: try super/few_week_or_few_part.py beneath next thing"

---
+++ super/few_week_or_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_new_child_over_next_part(str_arg):
+    same_case(str_arg)
+    print('woman')
+
+def same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_new_child_over_next_part('thing')
