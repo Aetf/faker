99c974d14d3218be7f5af9453bda09135cb9790d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 12:20:16 -0400

    "mv: tell super/first_case/old_man_and_new_group.py, super/take_able_point_to_thing/big_part_and_thing/week/woman.py, super/week/world_and_way.py and super/group/other_thing/few_woman/part/use_long_place.py on next fact"

---
+++ super/first_case/old_man_and_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    have_young_world_over_life(str_arg)
+    print('work')
+
+def have_young_world_over_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('group')
---
+++ super/take_able_point_to_thing/big_part_and_thing/week/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_case(str_arg):
+    big_person(str_arg)
+    print('tell_group_above_work')
+
+def big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_case('week_and_child')
