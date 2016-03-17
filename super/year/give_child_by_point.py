b08441950c0c6e51046f7b9190e1255daff7aa04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 10:16:34 -0400

    "mix: give super/find_young_company_from_woman/early_woman_and_man.py or super/year/give_child_by_point.py of good time"

---
+++ super/year/give_child_by_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_high_number_in_company(str_arg):
+    little_government(str_arg)
+    print('thing_or_fact')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_high_number_in_company('few_fact')
