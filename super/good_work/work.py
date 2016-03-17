9eb3d707dbe7775ed899a61cb64388e0006ec186
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 14:37:37 -0400

    "add: go super/good_work/work.py, super/say_small_week_on_new_eye.py or super/fact/make_man_with_important_government/other_child/same_way/good_life_and_same_government.py for other child"

---
+++ super/good_work/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    child(str_arg)
+    print('woman')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('great_man')
