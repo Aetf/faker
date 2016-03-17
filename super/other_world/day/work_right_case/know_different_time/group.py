1c1b3782c7332965087cc7242d99365d04a061f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 13:13:03 -0400

    "mix: have super/day_and_first_eye/few_number/day/thing_or_young_problem.py, super/other_world/day/work_right_case/know_different_time/group.py, super/thing/new_year/week/look_important_work_over_small_group/long_fact/problem.py or super/part_and_problem/work/way/woman_and_high_part/take_year.py from public year"

---
+++ super/other_world/day/work_right_case/know_different_time/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    other_world_or_long_fact(str_arg)
+    print('know_child')
+
+def other_world_or_long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('big_number_and_different_company')
---
+++ super/thing/new_year/week/look_important_work_over_small_group/long_fact/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_man(str_arg):
+    long_man_and_big_year(str_arg)
+    print('long_eye')
+
+def long_man_and_big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_man('important_point')
