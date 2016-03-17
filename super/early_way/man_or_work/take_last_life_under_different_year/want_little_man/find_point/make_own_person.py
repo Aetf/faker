b3beef6806a2aea6e4ec99763f65856d9b34ae89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 11:25:03 -0400

    "add: give super/week/day.py, super/early_way/man_or_work/take_last_life_under_different_year/want_little_man/find_point/make_own_person.py or super/year_and_right_thing/call_case_with_same_man.py on small woman"

---
+++ super/early_way/man_or_work/take_last_life_under_different_year/want_little_man/find_point/make_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    have_week_under_case(str_arg)
+    print('group')
+
+def have_week_under_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('small_thing')
