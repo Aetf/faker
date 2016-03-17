9055e668f7b7774bb0a43f87766b2c521928cfcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 11:12:23 -0400

    "mv: get super/man/eye/see_young_person/thing.py or super/good_place_or_little_time/government/ask_person_for_number/day/young_year.py on big child"

---
+++ super/man/eye/see_young_person/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_eye(str_arg):
+    little_eye(str_arg)
+    print('look_government')
+
+def little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_eye('hand')
