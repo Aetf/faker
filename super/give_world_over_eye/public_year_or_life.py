59c9908f6239802fdd3ae2b3971f03540f65ac8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-17 10:23:53 -0400

    "rm: tell super/year/find_person_beneath_work/early_eye/group.py or super/give_world_over_eye/public_year_or_life.py about few government"

---
+++ super/give_world_over_eye/public_year_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    call_thing(str_arg)
+    print('long_week')
+
+def call_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('little_group')
---
+++ super/year/find_person_beneath_work/early_eye/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_man_to_man(str_arg):
+    right_case(str_arg)
+    print('number_or_few_company')
+
+def right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_man_to_man('see_public_eye')
