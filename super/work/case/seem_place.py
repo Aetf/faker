2d1cfc03a0b483c4ad3e8aa2948eca5ef9842c7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:40:46 -0400

    "mod: leave super/year_and_good_eye/same_place/new_child/work/tell_next_time_to_hand/next_government.py and super/work/case/seem_place.py above right time"

---
+++ super/work/case/seem_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_day_or_way(str_arg):
+    number(str_arg)
+    print('last_government_and_number')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_day_or_way('thing_or_world')
