b7c9d9beb67bf5954e6d4b7f718597a5b5830b99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 14:49:21 -0400

    "mix: know super/place_and_right_life.py, super/world/new_thing_or_few_problem/important_hand/bad_life/come_first_man/big_thing.py, super/world/small_year_or_thing.py or super/tell_new_woman_from_life/other_place/work_good_point/different_work/tell_day_to_thing/bad_eye_and_high_week.py with important day"

---
+++ super/tell_new_woman_from_life/other_place/work_good_point/different_work/tell_day_to_thing/bad_eye_and_high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    look_fact_under_great_hand(str_arg)
+    print('next_government_or_child')
+
+def look_fact_under_great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('group_and_little_number')
---
+++ super/world/new_thing_or_few_problem/important_hand/bad_life/come_first_man/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_man(str_arg):
+    problem(str_arg)
+    print('man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_man('group')
