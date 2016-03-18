d82849eda49d4b74d22c66b6a0d744c2de7d13e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 12:12:02 -0400

    "add: know super/bad_child.py and super/first_work/world/bad_time_or_high_work/bad_part/able_eye_or_place.py for old problem"

---
+++ super/first_work/world/bad_time_or_high_work/bad_part/able_eye_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day(str_arg):
+    feel_number(str_arg)
+    print('little_place_or_hand')
+
+def feel_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day('company')
