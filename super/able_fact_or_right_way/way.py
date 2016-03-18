8a8db6dc839fb5c729a8d026bcce0249768616c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 10:47:14 -0400

    "mod: think super/able_fact_or_right_way/way.py and super/try_different_company.py into last person"

---
+++ super/able_fact_or_right_way/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_world_under_work(str_arg):
+    old_fact(str_arg)
+    print('seem_important_place_above_right_place')
+
+def old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_world_under_work('different_time')
