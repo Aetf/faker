97897788ac50860f815edfe4b5a80f3f1046733d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 13:29:54 -0400

    "add: feel super/eye/high_part/old_year_or_child.py, super/way/great_thing/work_small_fact_with_way/public_thing_or_group.py and super/other_case_or_part.py in same company"

---
+++ super/eye/high_part/old_year_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_large_case(str_arg):
+    great_life(str_arg)
+    print('man')
+
+def great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_large_case('problem')
