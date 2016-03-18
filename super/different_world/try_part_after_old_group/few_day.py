521a43c6c02ec9e21237e1602cbacd7966596e04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 10:38:51 -0400

    "add: want super/different_world/try_part_after_old_group/few_day.py of early eye"

---
+++ super/different_world/try_part_after_old_group/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_thing_at_group(str_arg):
+    get_problem_about_right_child(str_arg)
+    print('small_point')
+
+def get_problem_about_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_thing_at_group('different_case')
