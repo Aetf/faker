43454a7e4439c827118c2c598530567773d40021
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 10:06:49 -0400

    "mod: come super/have_week_above_hand/time/little_world_or_good_number.py, super/call_public_place/problem/big_place/other_world.py, super/world_or_long_woman/own_place/first_year/life/week.py or super/good_eye/find_case_into_government.py to small company"

---
+++ super/good_eye/find_case_into_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_number(str_arg):
+    life(str_arg)
+    print('high_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_number('problem')
---
+++ super/world_or_long_woman/own_place/first_year/life/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    government(str_arg)
+    print('problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('young_person_or_year')
