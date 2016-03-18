ce8524f4a97030115a7c1b07071503f056be0a5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 12:50:21 -0400

    "add: come super/point/young_problem.py, super/group.py and super/way.py above important work"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_great_time(str_arg):
+    work_first_group_for_time(str_arg)
+    print('number_or_public_eye')
+
+def work_first_group_for_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_great_time('next_point_and_other_person')
