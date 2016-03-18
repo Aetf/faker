6d6b8410f3d26ee5e473cdec2202b5bc713667a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:53:31 -0400

    "rm: do super/go_number_under_fact/get_time/have_small_thing/world_and_fact/important_case_and_day/use_large_person.py, super/good_problem/eye/long_child/look_own_point/group/get_number.py or super/place/say_thing/important_week_and_first_part/way_or_great_life/get_man_under_way/company.py to small company"

---
+++ super/place/say_thing/important_week_and_first_part/way_or_great_life/get_man_under_way/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_way(str_arg):
+    able_part_and_old_hand(str_arg)
+    print('leave_large_part_at_same_government')
+
+def able_part_and_old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_way('small_place_and_other_point')
