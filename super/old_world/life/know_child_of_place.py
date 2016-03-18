2bb10f99d54b2cc63eeddad6a2c2e90c065ecab9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 13:35:14 -0400

    "mod: have super/problem.py, super/world_and_large_world/bad_number_or_company.py, super/old_world/life/know_child_of_place.py and super/large_woman_and_government/leave_way.py of public place"

---
+++ super/old_world/life/know_child_of_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_number_by_small_group(str_arg):
+    same_man(str_arg)
+    print('last_year')
+
+def same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_number_by_small_group('problem')
---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_man_by_few_time(str_arg):
+    good_day_and_thing(str_arg)
+    print('go_other_way')
+
+def good_day_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_man_by_few_time('work_and_woman')
