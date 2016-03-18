d2308edb6aa4233a586b07bc94aa8c17f4259741
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:24:18 -0400

    "add: feel super/child/point/part/public_point/thing/same_eye.py after great day"

---
+++ super/child/point/part/public_point/thing/same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    child(str_arg)
+    print('able_place_or_case')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('large_point_and_world')
