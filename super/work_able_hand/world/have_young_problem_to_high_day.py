e68420acbf75fea9c7565a84fec2d4dbfde1300b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 13:09:32 -0400

    "mv: make super/year/time_or_child/give_work_under_high_week/man/public_thing/come_old_government_in_able_way.py and super/work_able_hand/world/have_young_problem_to_high_day.py of good company"

---
+++ super/work_able_hand/world/have_young_problem_to_high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life(str_arg):
+    come_little_week(str_arg)
+    print('do_time')
+
+def come_little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life('tell_old_point')
---
+++ super/year/time_or_child/give_work_under_high_week/man/public_thing/come_old_government_in_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case(str_arg):
+    group(str_arg)
+    print('have_important_number_above_day')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case('group_and_little_time')
