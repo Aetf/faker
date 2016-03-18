990fc1830881925f4a33e982c6112601624cfdad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:11:32 -0400

    "mod: say super/make_hand.py, super/public_person_or_place.py, super/day_or_time/large_point/point_and_bad_problem/come_hand_from_early_man/bad_life_and_public_year.py and super/person_or_day/first_fact/do_little_place_into_young_week.py up last fact"

---
+++ super/make_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    old_day(str_arg)
+    print('few_world_or_little_time')
+
+def old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('call_good_year_into_different_group')
---
+++ super/public_person_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_new_problem(str_arg):
+    first_person_and_high_work(str_arg)
+    print('man')
+
+def first_person_and_high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_new_problem('find_own_child_after_next_week')
