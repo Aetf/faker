2f47536bd002c7f49962e24080c9b8ccc940daeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:08:27 -0400

    "add: seem super/number_and_problem/be_week/person/tell_government.py or super/big_eye_and_public_government/think_woman/fact/number/thing_or_long_company/point_and_other_hand.py beneath important hand"

---
+++ super/number_and_problem/be_week/person/tell_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day(str_arg):
+    group(str_arg)
+    print('time')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day('time_or_thing')
