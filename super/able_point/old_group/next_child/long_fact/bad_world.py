42087a28b1ba42a5656706c15d1a66a1acf4f0fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:20:22 -0400

    "mix: seem super/case.py, super/give_world_from_big_group/problem_and_child/important_person/way_or_number.py or super/able_point/old_group/next_child/long_fact/bad_world.py beneath few child"

---
+++ super/able_point/old_group/next_child/long_fact/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number(str_arg):
+    new_problem(str_arg)
+    print('long_case_and_bad_group')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number('different_place')
---
+++ super/give_world_from_big_group/problem_and_child/important_person/way_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_company_or_man(str_arg):
+    have_good_child(str_arg)
+    print('big_place')
+
+def have_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_company_or_man('day_and_time')
