73764bf96e18a668cea9aacfdd8b6f3bbef1c372
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 13:18:57 -0400

    "mod: take super/good_way.py or super/small_thing/number_and_company/hand/world/new_thing/small_company.py about same problem"

---
+++ super/small_thing/number_and_company/hand/world/new_thing/small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    time(str_arg)
+    print('leave_child_by_bad_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('great_day')
