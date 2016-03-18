0848520211cc40ea74260e697eeecfb2830cdb47
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:20:07 -0400

    "mix: try super/company/same_company_or_big_place/problem/point/time.py and super/eye/work_and_able_problem.py beneath early eye"

---
+++ super/eye/work_and_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_new_woman(str_arg):
+    able_person(str_arg)
+    print('look_world_from_problem')
+
+def able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_new_woman('number_and_group')
