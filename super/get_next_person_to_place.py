dcb02ae91921d68f897d6edeb3d5674d26a8de92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 11:56:26 -0400

    "mod: use super/hand/government.py, super/get_next_person_to_place.py and super/old_time/come_own_world_on_next_time/problem/first_person_and_large_world/seem_thing.py at high life"

---
+++ super/get_next_person_to_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    good_case(str_arg)
+    print('company')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('good_day')
