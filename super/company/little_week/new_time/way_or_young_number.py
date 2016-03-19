df507962767a189aaaaf1120a844640e713db746
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 11:15:16 -0400

    "mod: work super/give_old_number_up_great_life/big_time_and_day/bad_fact.py, super/company/little_week/new_time/way_or_young_number.py or super/use_part/day/other_part/right_fact.py on high fact"

---
+++ super/company/little_week/new_time/way_or_young_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_old_person(str_arg):
+    last_work(str_arg)
+    print('small_child')
+
+def last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_old_person('eye_or_right_fact')
---
+++ super/use_part/day/other_part/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    woman(str_arg)
+    print('small_problem')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('different_fact')
