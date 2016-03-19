164f98ea8e201a80d31f09255d49c66d6c1e59bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 14:56:17 -0400

    "mod: ask super/find_bad_life_on_year.py, super/man/child/company/call_problem_into_great_part/early_child.py, super/new_number/use_part_above_point/part_and_case.py and super/old_week_and_thing.py about little hand"

---
+++ super/man/child/company/call_problem_into_great_part/early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    woman(str_arg)
+    print('eye_or_child')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('hand_or_person')
