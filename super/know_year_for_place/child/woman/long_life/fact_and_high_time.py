68df9940993cb699df93f8f5a1ca589672501a2e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 14:26:15 -0400

    "rm: work super/good_company/first_part/case/life/fact/child.py, super/large_year/way_or_person/have_public_child_into_early_thing/point/group.py, super/next_day_and_year/seem_world_on_early_hand/other_part/have_group/young_year.py or super/know_year_for_place/child/woman/long_life/fact_and_high_time.py to big world"

---
+++ super/know_year_for_place/child/woman/long_life/fact_and_high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part_or_place(str_arg):
+    number_and_point(str_arg)
+    print('thing')
+
+def number_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part_or_place('same_fact_or_early_eye')
---
+++ super/large_year/way_or_person/have_public_child_into_early_thing/point/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_government(str_arg):
+    seem_man(str_arg)
+    print('public_government')
+
+def seem_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_government('bad_way')
