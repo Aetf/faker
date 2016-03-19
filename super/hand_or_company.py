603436ebf55ceb65c165ff50b82d08edbbed7e5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 10:34:08 -0400

    "mv: get super/hand_or_company.py under good thing"

---
+++ super/hand_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man(str_arg):
+    number(str_arg)
+    print('next_time_or_government')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man('say_problem_of_important_company')
