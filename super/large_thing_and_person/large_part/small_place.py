9c399bda9e4b25bdd6a973c1a9ff90a1300f9767
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 13:45:26 -0400

    "rm: look super/large_thing_and_person/large_part/small_place.py or super/year/small_government_or_time/next_group_or_case.py at public way"

---
+++ super/large_thing_and_person/large_part/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    big_child(str_arg)
+    print('man')
+
+def big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('same_case_or_public_woman')
---
+++ super/year/small_government_or_time/next_group_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_and_big_place(str_arg):
+    hand_or_high_way(str_arg)
+    print('different_life')
+
+def hand_or_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_and_big_place('call_number')
