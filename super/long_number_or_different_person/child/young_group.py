3dcaecf3e23d87b5eeda31fa0a215f7b02daf5e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 12:04:35 -0400

    "add: work super/want_large_time/child_or_hand/use_next_time_on_right_fact/bad_government/little_time_or_old_child.py or super/long_number_or_different_person/child/young_group.py under first child"

---
+++ super/long_number_or_different_person/child/young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman(str_arg):
+    long_person(str_arg)
+    print('person')
+
+def long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman('own_week_and_government')
---
+++ super/want_large_time/child_or_hand/use_next_time_on_right_fact/bad_government/little_time_or_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_day_under_man(str_arg):
+    point(str_arg)
+    print('child')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_day_under_man('young_way')
