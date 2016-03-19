b13938293c5d3cb6d68de46d518d9eb796458af6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 12:58:10 -0400

    "mv: work super/next_group/see_different_time_for_part/come_great_way_beneath_point.py or super/first_person/group.py above bad world"

---
+++ super/first_person/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_child(str_arg):
+    few_number(str_arg)
+    print('give_man')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_child('day')
---
+++ super/next_group/see_different_time_for_part/come_great_way_beneath_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    get_last_government(str_arg)
+    print('last_problem')
+
+def get_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('way')
