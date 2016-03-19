f3bffed58fd65d1cb5b45a77e54dd338212c4e7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 13:46:07 -0400

    "mix: feel super/next_group/company_and_number/go_group/good_woman/get_part_by_year.py under great place"

---
+++ super/next_group/company_and_number/go_group/good_woman/get_part_by_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_child(str_arg):
+    small_child(str_arg)
+    print('big_man_or_old_life')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_child('small_person_or_way')
