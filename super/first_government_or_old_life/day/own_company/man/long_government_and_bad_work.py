b3c92d482f657c56ee98b9014a92f0c81592ef09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 13:28:29 -0400

    "mod: look super/first_government_or_old_life/day/own_company/man/long_government_and_bad_work.py, super/large_problem_and_long_way/place_or_long_life/go_next_child_above_great_hand/get_fact/place.py, super/feel_same_woman_by_different_place/call_government_for_child/next_person_or_woman/man/give_point.py or super/next_hand/important_way/work.py from bad person"

---
+++ super/first_government_or_old_life/day/own_company/man/long_government_and_bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    do_government_up_point(str_arg)
+    print('place')
+
+def do_government_up_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('work_or_year')
---
+++ super/next_hand/important_way/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work_or_own_day(str_arg):
+    ask_man(str_arg)
+    print('thing')
+
+def ask_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work_or_own_day('first_company')
