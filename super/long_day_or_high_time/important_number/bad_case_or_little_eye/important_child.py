81a49f488fe0f6cb27938c29c567ab1ae406ca8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 10:43:21 -0400

    "mix: want super/man/able_hand_and_small_woman/ask_work.py, super/look_number/point/first_child_and_world.py and super/long_day_or_high_time/important_number/bad_case_or_little_eye/important_child.py over good fact"

---
+++ super/long_day_or_high_time/important_number/bad_case_or_little_eye/important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_small_man_above_large_hand(str_arg):
+    old_fact(str_arg)
+    print('point_and_place')
+
+def old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_small_man_above_large_hand('big_year')
---
+++ super/look_number/point/first_child_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_year(str_arg):
+    long_place(str_arg)
+    print('way')
+
+def long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_year('large_part_or_hand')
