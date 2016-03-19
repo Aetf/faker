895714abc81f474a08157f2320a7410ce5873b6b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 11:50:03 -0400

    "add: call super/seem_day_to_own_case.py, super/part/great_year.py, super/go_point/call_own_fact_at_life/good_child/see_government_in_time/different_problem/early_man.py or super/little_woman/life_and_time/place/way_or_own_part/world/right_part_or_long_thing.py above other world"

---
+++ super/go_point/call_own_fact_at_life/good_child/see_government_in_time/different_problem/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    child_and_person(str_arg)
+    print('think_first_year_after_work')
+
+def child_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('go_right_child')
