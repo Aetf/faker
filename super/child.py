84982c38f94ff6af5641d018f8721753c9f078e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 10:57:10 -0400

    "rm: work super/child.py over next man"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_person(str_arg):
+    number(str_arg)
+    print('leave_right_thing')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_person('child')
