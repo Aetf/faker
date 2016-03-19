0b7aefa89460f8b352dad437481d9fb6041dac94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 13:43:54 -0400

    "add: know super/ask_company_over_important_place/important_work/group/look_early_woman_under_able_man/child.py to new world"

---
+++ super/ask_company_over_important_place/important_work/group/look_early_woman_under_able_man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_place(str_arg):
+    eye_or_small_part(str_arg)
+    print('do_bad_life')
+
+def eye_or_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_place('use_life_with_group')
