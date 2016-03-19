a97d8b71dc08e09894f6646e1093b0d1695f5a03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 14:17:00 -0400

    "mod: say super/life/different_world/point/week_or_man/fact_and_bad_number.py, super/different_case/child/way/high_fact_and_part/good_fact_or_few_hand.py and super/do_number_about_first_year/eye.py to old group"

---
+++ super/different_case/child/way/high_fact_and_part/good_fact_or_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    other_week(str_arg)
+    print('different_company')
+
+def other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('case')
---
+++ super/life/different_world/point/week_or_man/fact_and_bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman_or_same_child(str_arg):
+    world(str_arg)
+    print('company')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman_or_same_child('early_man')
