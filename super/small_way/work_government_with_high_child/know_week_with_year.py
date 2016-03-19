5641810d9f3e427a03729b9a185a82b8e600ee78
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-19 13:36:51 -0400

    "mod: give super/small_way/work_government_with_high_child/know_week_with_year.py, super/look_point/see_life/eye/day.py or super/week_and_problem/new_person/time_and_hand.py at able fact"

---
+++ super/small_way/work_government_with_high_child/know_week_with_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_problem_or_long_year(str_arg):
+    part_or_first_way(str_arg)
+    print('little_company')
+
+def part_or_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_problem_or_long_year('own_case_and_day')
---
+++ super/week_and_problem/new_person/time_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_other_point(str_arg):
+    important_eye(str_arg)
+    print('number')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_other_point('tell_problem_over_government')
