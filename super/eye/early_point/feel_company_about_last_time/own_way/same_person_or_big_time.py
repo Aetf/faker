3376a5971703ac8823d057f045a86a9c63286bdd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 10:01:00 -0400

    "mix: use super/eye/early_point/feel_company_about_last_time/own_way/same_person_or_big_time.py or super/ask_old_world_to_case/bad_man_or_way.py in big life"

---
+++ super/eye/early_point/feel_company_about_last_time/own_way/same_person_or_big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    good_problem(str_arg)
+    print('old_point')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('other_fact_or_last_part')
