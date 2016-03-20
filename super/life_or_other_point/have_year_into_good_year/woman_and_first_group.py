00372e2c07fb94b57d523f32eabd1216d27d4d39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 13:51:25 -0400

    "add: call super/life_or_other_point/have_year_into_good_year/woman_and_first_group.py for young day"

---
+++ super/life_or_other_point/have_year_into_good_year/woman_and_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_government(str_arg):
+    place(str_arg)
+    print('way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_government('new_group')
