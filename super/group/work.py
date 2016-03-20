c4b74c77879da12a0614b969dff887509a228b41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 10:15:03 -0400

    "mv: work super/group/work.py and super/say_part_for_part/tell_day/big_child_and_old_fact/able_work_and_work.py up own woman"

---
+++ super/group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_other_world(str_arg):
+    week_and_person(str_arg)
+    print('company')
+
+def week_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_other_world('next_person')
---
+++ super/say_part_for_part/tell_day/big_child_and_old_fact/able_work_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_important_year(str_arg):
+    time(str_arg)
+    print('world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_important_year('see_right_year')
