d4426289228d242b6ea4afac77b7470fe5aecd95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 13:50:09 -0400

    "rm: find super/new_work/point.py and super/small_child/next_work_and_early_day/find_long_week.py under long number"

---
+++ super/small_child/next_work_and_early_day/find_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    have_big_group_above_world(str_arg)
+    print('tell_few_government_about_last_day')
+
+def have_big_group_above_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('able_year_or_public_part')
