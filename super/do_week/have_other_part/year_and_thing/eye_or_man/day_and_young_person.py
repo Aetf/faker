9379afad574cd5b30ad2b1fbef05d1b94bed8311
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 13:08:39 -0400

    "mv: have super/group/eye/old_life/do_government_into_case.py and super/do_week/have_other_part/year_and_thing/eye_or_man/day_and_young_person.py at little year"

---
+++ super/do_week/have_other_part/year_and_thing/eye_or_man/day_and_young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_early_hand(str_arg):
+    part_and_number(str_arg)
+    print('ask_eye_on_last_world')
+
+def part_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_early_hand('try_good_case_from_life')
---
+++ super/group/eye/old_life/do_government_into_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_own_fact(str_arg):
+    year_or_old_eye(str_arg)
+    print('other_life')
+
+def year_or_old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_own_fact('child')
