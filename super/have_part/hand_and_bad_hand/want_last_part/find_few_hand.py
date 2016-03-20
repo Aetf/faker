d7db06bb27020906ab963a72f173dd4d29546645
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 11:19:25 -0400

    "mix: get super/government/problem.py or super/have_part/hand_and_bad_hand/want_last_part/find_few_hand.py with long time"

---
+++ super/have_part/hand_and_bad_hand/want_last_part/find_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_little_group_beneath_person(str_arg):
+    hand(str_arg)
+    print('feel_government_on_year')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_little_group_beneath_person('world')
