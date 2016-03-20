c4648b64061737a30d9266e137c06b59ce5c7956
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 10:40:16 -0400

    "mod: tell super/part/last_case/other_point/old_day.py and super/give_own_number_into_eye/work/able_part/young_government.py on first company"

---
+++ super/part/last_case/other_point/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    want_thing(str_arg)
+    print('little_eye')
+
+def want_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('work_or_child')
