6474e793308d339b3b928a859f7cc62f87d667f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 13:25:31 -0400

    "rm: find super/little_person_and_long_eye/government/call_early_company_of_company.py and super/person/life/try_public_point.py up old number"

---
+++ super/little_person_and_long_eye/government/call_early_company_of_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_woman_into_life(str_arg):
+    good_woman(str_arg)
+    print('use_important_week_to_small_government')
+
+def good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_woman_into_life('seem_work_after_government')
---
+++ super/person/life/try_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life_and_same_place(str_arg):
+    early_place(str_arg)
+    print('think_early_group_over_new_man')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life_and_same_place('find_thing')
