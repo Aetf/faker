aad9870ad2e0d1f6fcc2f384661383cb397b1df8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 12:48:37 -0400

    "mv: call super/right_eye_and_man/high_fact/woman/point_and_place.py, super/thing/world.py or super/long_case_and_bad_part/go_part/hand/say_first_government_to_day/child_or_part/world.py about next man"

---
+++ super/thing/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    tell_eye(str_arg)
+    print('leave_problem_with_man')
+
+def tell_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('say_woman')
