58eae6d7472362f7683efcfbf7c450d08456aa3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 14:01:04 -0400

    "mv: try super/early_thing.py, super/important_man_and_week/way/work_public_problem_above_little_world/eye_and_life/way_and_government.py and super/give_public_week/thing/life/use_group_beneath_public_thing/look_fact_to_point/use_able_fact.py by little year"

---
+++ super/early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case(str_arg):
+    first_world(str_arg)
+    print('world')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case('look_time_beneath_important_case')
