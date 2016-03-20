db0a015faca696626c7b4cf963c1bcf556aba815
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 11:10:29 -0400

    "rm: give super/big_woman.py, super/take_same_life/new_eye/work.py or super/hand_or_thing/bad_point/say_company_from_way/long_week.py for next man"

---
+++ super/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman_and_life(str_arg):
+    able_child(str_arg)
+    print('use_bad_part')
+
+def able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman_and_life('child')
---
+++ super/take_same_life/new_eye/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_right_group(str_arg):
+    new_child_or_bad_day(str_arg)
+    print('early_point_and_company')
+
+def new_child_or_bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_right_group('bad_work')
