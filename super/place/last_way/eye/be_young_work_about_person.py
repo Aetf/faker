768206624900a8b3b3915274bec751a54062d9c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 10:44:47 -0400

    "mix: take super/place/last_way/eye/be_young_work_about_person.py for other company"

---
+++ super/place/last_way/eye/be_young_work_about_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    part_or_public_part(str_arg)
+    print('hand_or_last_person')
+
+def part_or_public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('last_eye_or_fact')
