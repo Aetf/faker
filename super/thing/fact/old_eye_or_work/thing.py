d8381e91ff4765cdd94920c22c6c3b04657375c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 12:04:35 -0400

    "mv: come super/public_government/hand/right_child.py, super/want_problem_up_time.py, super/woman/time/next_hand.py or super/thing/fact/old_eye_or_work/thing.py under same work"

---
+++ super/thing/fact/old_eye_or_work/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_eye(str_arg):
+    thing(str_arg)
+    print('next_day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_eye('want_life_at_point')
---
+++ super/want_problem_up_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year_or_public_person(str_arg):
+    look_old_problem_about_life(str_arg)
+    print('time')
+
+def look_old_problem_about_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year_or_public_person('fact_or_point')
