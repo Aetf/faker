4615d9b5807c518ec377a7c23d360841bc49e270
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 14:29:04 -0400

    "rm: get super/give_large_world_above_man/big_life/government_or_high_woman/number_and_work/next_woman.py about good man"

---
+++ super/give_large_world_above_man/big_life/government_or_high_woman/number_and_work/next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_eye(str_arg):
+    think_year(str_arg)
+    print('large_child_and_child')
+
+def think_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_eye('little_year')
