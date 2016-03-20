f427b468af191a362ee5b5f6c19d3543cc5cbd5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 10:54:38 -0400

    "mv: say super/young_life/able_child/eye/next_company_and_group/ask_different_problem_to_way.py, super/public_person.py and super/few_way_or_way/problem.py on right case"

---
+++ super/few_way_or_way/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place(str_arg):
+    week(str_arg)
+    print('place')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place('big_hand')
