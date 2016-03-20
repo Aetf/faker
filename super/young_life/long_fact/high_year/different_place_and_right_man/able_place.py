6c1d210e0b68e0cd03ef5d8168b1d5bef8ea7ed8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-20 14:51:02 -0400

    "mod: make super/young_life/long_fact/high_year/different_place_and_right_man/able_place.py or super/try_week/try_high_fact_for_early_week/number/point_or_great_place/other_work_or_number/great_group.py after important eye"

---
+++ super/young_life/long_fact/high_year/different_place_and_right_man/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    take_big_day(str_arg)
+    print('point')
+
+def take_big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('last_government')
