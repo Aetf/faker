66d5b0c013e1881b67b7addfc861caa66fc6f55e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:50:37 -0400

    "rm: have super/year_or_young_way.py, super/new_work/government/last_fact_and_long_fact/call_woman.py and super/public_part/bad_person_or_other_eye.py to long government"

---
+++ super/new_work/government/last_fact_and_long_fact/call_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    other_hand(str_arg)
+    print('same_man')
+
+def other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('government')
