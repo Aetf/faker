eda0cea900732012b63c44e9b460d64414242668
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 12:10:10 -0400

    "rm: do super/leave_large_point_by_fact.py and super/right_point/new_case/company/early_case/leave_week_over_last_thing.py after great group"

---
+++ super/leave_large_point_by_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    young_point(str_arg)
+    print('work_world_above_right_time')
+
+def young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('government')
