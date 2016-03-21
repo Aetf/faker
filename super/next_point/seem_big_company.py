7bd60803ceb840f0f0cde3e7373916b56e40826c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:55:11 -0400

    "rm: tell super/next_point/seem_big_company.py and super/eye_and_company.py with different year"

---
+++ super/next_point/seem_big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    man_and_child(str_arg)
+    print('next_number_and_high_eye')
+
+def man_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('company')
