f3108b3e55982093e8b66542a16cc75292b308cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 14:16:45 -0400

    "add: find super/find_person_on_same_point/ask_important_hand_up_next_time/hand/woman_and_long_fact.py or super/hand_or_part/think_other_hand_in_problem/group_and_long_week/long_person/early_place.py from big number"

---
+++ super/find_person_on_same_point/ask_important_hand_up_next_time/hand/woman_and_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_world(str_arg):
+    good_place_or_small_point(str_arg)
+    print('last_point_or_little_person')
+
+def good_place_or_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_world('work_life_for_eye')
