67304462f6394f2591ebce54564531215309a826
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 13:33:45 -0400

    "rm: try super/public_case_and_own_man.py, super/use_life/say_case_to_different_world/new_year/child/young_week_or_different_work/case.py, super/way_or_able_number.py and super/work_old_number_with_same_company/own_point/see_old_week_for_first_person/bad_man_and_work/case_or_thing.py beneath great company"

---
+++ super/way_or_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_person(str_arg):
+    different_year(str_arg)
+    print('other_number')
+
+def different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_person('point_or_part')
