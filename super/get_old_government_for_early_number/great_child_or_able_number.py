622b3f68ad5d8e13dfb962e7f058990dd627931d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:05:50 -0400

    "mix: give super/get_old_government_for_early_number/great_child_or_able_number.py and super/public_life.py up big thing"

---
+++ super/get_old_government_for_early_number/great_child_or_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye(str_arg):
+    different_eye(str_arg)
+    print('public_place')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye('try_small_time_in_little_life')
---
+++ super/public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_hand(str_arg):
+    early_part(str_arg)
+    print('company_and_right_work')
+
+def early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_hand('know_child_after_important_part')
