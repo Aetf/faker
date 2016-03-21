8aef0e2a0e69ba6eb931eca2e1242a682629bbf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 11:06:29 -0400

    "add: find super/next_fact/right_year/case/same_work.py on old way"

---
+++ super/next_fact/right_year/case/same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    take_other_woman(str_arg)
+    print('few_world')
+
+def take_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('person')
