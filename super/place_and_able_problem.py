9b2d8dac3de40664726b71e420d67a64cfd67afe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 11:05:52 -0400

    "mv: make super/place_and_able_problem.py and super/ask_day_at_work/important_thing/point/case_or_long_number/life/company.py by little hand"

---
+++ super/place_and_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    big_week(str_arg)
+    print('make_same_point_from_first_group')
+
+def big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('time')
