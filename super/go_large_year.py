2373645c985326d548c0711c6bf6653067d356f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:41:20 -0400

    "mix: have super/place_or_young_eye.py, super/use_last_way_into_year/good_problem_or_eye/young_point.py and super/go_large_year.py beneath few case"

---
+++ super/go_large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work(str_arg)
+    print('day')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('great_group')
---
+++ super/place_or_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    part(str_arg)
+    print('large_year')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('same_life_or_work')
