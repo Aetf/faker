a8611b38470ccda405c0551c66d47e8497b78654
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 11:05:04 -0400

    "rm: call super/first_number_or_company/big_work/old_life/work_and_person/public_point/great_hand_or_right_child.py or super/child.py by early part"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_high_place(str_arg):
+    own_child(str_arg)
+    print('child_or_point')
+
+def own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_high_place('own_woman')
