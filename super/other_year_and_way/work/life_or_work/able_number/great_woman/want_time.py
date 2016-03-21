374738c4439cc748b5a5968ec570e4c4f0b8d438
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 13:01:24 -0400

    "mix: get super/do_long_week_to_right_day/young_work_or_right_group/call_year/little_number/early_man.py and super/other_year_and_way/work/life_or_work/able_number/great_woman/want_time.py to next problem"

---
+++ super/other_year_and_way/work/life_or_work/able_number/great_woman/want_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number(str_arg):
+    own_problem(str_arg)
+    print('seem_good_thing_to_man')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number('man')
