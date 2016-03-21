2b780d8d95308934d6f2e77a47f0e28baba1ec42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 11:37:46 -0400

    "mv: look super/go_new_hand.py and super/high_person_and_child/see_group/world.py for small man"

---
+++ super/high_person_and_child/see_group/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work(str_arg)
+    print('try_small_year')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('child')
