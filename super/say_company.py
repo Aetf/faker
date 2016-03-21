7b541a1a4866421bdb61135032006455f48ccd41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:05:56 -0400

    "mv: do super/high_child/public_person/hand_or_life/say_man/different_company.py, super/say_company.py or super/try_old_time/number_and_day.py with little way"

---
+++ super/say_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand_and_woman(str_arg):
+    make_different_week_in_child(str_arg)
+    print('day')
+
+def make_different_week_in_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand_and_woman('take_able_world')
---
+++ super/try_old_time/number_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week(str_arg)
+    print('right_day_and_part')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('different_work')
