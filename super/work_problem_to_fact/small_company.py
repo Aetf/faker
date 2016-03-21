b63f29969501f81143b1022a99407ff2f2ee4289
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 13:18:31 -0400

    "mv: come super/work_problem_to_fact/small_company.py in public time"

---
+++ super/work_problem_to_fact/small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_world(str_arg):
+    get_company_from_person(str_arg)
+    print('small_child')
+
+def get_company_from_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_world('work_time')
