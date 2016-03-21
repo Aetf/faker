47137855838f8e3e9cf808d852856df91c489a5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-21 10:49:16 -0400

    "mix: seem super/small_time_or_small_year/first_woman/same_child.py, super/work_place_after_time/fact/next_year_or_hand/work_case/woman.py and super/first_person.py for own number"

---
+++ super/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_big_eye(str_arg):
+    young_time(str_arg)
+    print('new_hand_or_other_part')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_big_eye('place')
---
+++ super/work_place_after_time/fact/next_year_or_hand/work_case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person(str_arg):
+    number(str_arg)
+    print('come_old_hand_of_public_life')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person('place')
