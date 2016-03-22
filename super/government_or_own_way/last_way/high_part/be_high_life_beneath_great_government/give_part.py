de5aff4c41bae1f90f03d776cda23353ba88ecbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 11:10:49 -0400

    "mix: know super/use_way_over_right_fact/first_world_or_young_eye/group/right_point_and_part.py, super/be_small_time/group/old_company/last_year/thing.py, super/young_day/ask_young_part/fact/look_small_place_above_way/year_or_early_number.py or super/government_or_own_way/last_way/high_part/be_high_life_beneath_great_government/give_part.py for bad part"

---
+++ super/government_or_own_way/last_way/high_part/be_high_life_beneath_great_government/give_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case(str_arg):
+    get_public_group(str_arg)
+    print('try_company')
+
+def get_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case('government_and_early_point')
