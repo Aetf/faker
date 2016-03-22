34d7f506de06bbe54870a1d1c8784c32470441ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 12:03:22 -0400

    "rm: give super/small_work/first_man.py and super/other_work.py with same year"

---
+++ super/small_work/first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_next_part(str_arg):
+    be_week_at_part(str_arg)
+    print('group')
+
+def be_week_at_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_next_part('man')
