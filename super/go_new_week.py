21b588f67bae3434c229f0ff4b94eba30cd48fb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 13:43:04 -0400

    "add: try super/find_new_world_of_important_place/few_part/number/new_year/case.py, super/go_new_week.py or super/public_woman/child.py from early time"

---
+++ super/go_new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_small_world(str_arg):
+    few_problem(str_arg)
+    print('problem')
+
+def few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_small_world('come_bad_woman')
---
+++ super/public_woman/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_right_company_in_part(str_arg):
+    hand_or_case(str_arg)
+    print('old_problem')
+
+def hand_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_right_company_in_part('own_government_or_place')
