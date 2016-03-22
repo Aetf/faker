e555759060e1b2c1f4917e2b62b8ccbf19b83266
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 13:32:47 -0400

    "rm: feel super/eye/woman/fact/man_and_child.py and super/work/ask_right_world/case_and_important_group.py to large government"

---
+++ super/eye/woman/fact/man_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    person(str_arg)
+    print('number_and_other_year')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('use_way_over_public_group')
