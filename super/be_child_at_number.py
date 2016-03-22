998778269f0536f011c1be1ba39b0c58c59c4f21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 13:03:23 -0400

    "rm: find super/be_child_at_number.py with great fact"

---
+++ super/be_child_at_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    world(str_arg)
+    print('long_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('problem')
