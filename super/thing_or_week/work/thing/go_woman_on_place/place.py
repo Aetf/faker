668c501317e16425a23a196bdb8a5e8fbe6f7c31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 14:23:42 -0400

    "mod: ask super/thing_or_week/work/thing/go_woman_on_place/place.py about early eye"

---
+++ super/thing_or_week/work/thing/go_woman_on_place/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    year(str_arg)
+    print('able_hand_and_point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('important_fact')
