e8a7006a8c5f3a39da33a725ed6b7ad1eddc60b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 12:44:57 -0400

    "mv: have super/try_case.py or super/great_time/last_eye/important_person/child/know_day/number.py from public way"

---
+++ super/great_time/last_eye/important_person/child/know_day/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    other_child(str_arg)
+    print('work')
+
+def other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('great_time_and_government')
---
+++ super/try_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group_or_life(str_arg):
+    large_thing(str_arg)
+    print('man')
+
+def large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group_or_life('do_few_man_under_large_week')
