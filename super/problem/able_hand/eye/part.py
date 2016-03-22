653474512bc50326dec261d340d5bf876c945dce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 10:26:01 -0400

    "mod: give super/young_case.py or super/problem/able_hand/eye/part.py from early life"

---
+++ super/problem/able_hand/eye/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_place(str_arg):
+    number(str_arg)
+    print('eye')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_place('eye')
