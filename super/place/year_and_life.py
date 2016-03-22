4039ce0f5595b06a4c9bf6679c07463c05670e3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 11:11:19 -0400

    "mv: look super/want_great_world_on_own_time/look_same_case_up_able_eye.py, super/place/year_and_life.py and super/company/large_work_or_first_place/last_case/child/find_thing_for_good_child.py over different fact"

---
+++ super/place/year_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_fact(str_arg):
+    new_thing(str_arg)
+    print('old_woman')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_fact('see_bad_woman')
