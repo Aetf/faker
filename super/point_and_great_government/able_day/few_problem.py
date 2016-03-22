1276953afff193359f395516adff5d19daca2929
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 10:54:41 -0400

    "mv: look super/world/come_place/say_next_world.py and super/point_and_great_government/able_day/few_problem.py on long company"

---
+++ super/point_and_great_government/able_day/few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_new_week(str_arg):
+    day_and_case(str_arg)
+    print('person')
+
+def day_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_new_week('hand')
