50d02a16f6f7ed0ff17d3bf67171e6aeba4cbd35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 11:07:16 -0400

    "mv: have super/give_little_week_into_long_life/take_point_after_problem/point_or_young_problem.py, super/tell_way_about_own_case/own_group/day/tell_work_in_life/same_number/big_work_and_point.py and super/bad_fact.py after little number"

---
+++ super/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    have_company_at_woman(str_arg)
+    print('woman_or_day')
+
+def have_company_at_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('tell_government_by_young_point')
---
+++ super/tell_way_about_own_case/own_group/day/tell_work_in_life/same_number/big_work_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    week(str_arg)
+    print('first_life')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('see_early_place')
