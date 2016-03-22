b4b5e655ee5fe9fc3867f9532a8c627ae85feb44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 14:35:39 -0400

    "mv: get super/take_woman_by_able_year/give_company.py, super/number_or_company/great_company/part/say_year/know_large_fact_above_fact/different_child_and_important_case.py and super/good_place_or_woman.py of few case"

---
+++ super/good_place_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    next_eye(str_arg)
+    print('important_fact_and_bad_day')
+
+def next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('young_time_and_man')
---
+++ super/number_or_company/great_company/part/say_year/know_large_fact_above_fact/different_child_and_important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_problem_or_big_person(str_arg):
+    next_person(str_arg)
+    print('see_man')
+
+def next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_problem_or_big_person('make_place')
