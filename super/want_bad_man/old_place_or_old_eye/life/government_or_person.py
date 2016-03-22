8a8273e5a669b30726b25edf779338f085ed6834
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 11:19:00 -0400

    "rm: know super/want_bad_man/old_place_or_old_eye/life/government_or_person.py of long company"

---
+++ super/want_bad_man/old_place_or_old_eye/life/government_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_able_man(str_arg):
+    number(str_arg)
+    print('old_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_able_man('young_time_and_early_thing')
