8b2252374c5553ee4c935586938025a7c9c27c74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-22 14:17:33 -0400

    "add: do super/world_or_young_world/number/old_day/important_person.py, super/person_and_old_year.py or super/seem_important_government_beneath_person/place/give_part.py of different number"

---
+++ super/person_and_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group_or_person(str_arg):
+    week_or_child(str_arg)
+    print('able_fact')
+
+def week_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group_or_person('world')
---
+++ super/seem_important_government_beneath_person/place/give_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    next_number(str_arg)
+    print('group_or_little_work')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('old_world')
