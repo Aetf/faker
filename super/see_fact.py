209102742582197218df4d5ffa2847ce20650fcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:33:11 -0400

    "rm: be super/thing.py, super/do_number_at_large_person/number/ask_problem_beneath_early_work/great_time/little_hand/thing.py and super/see_fact.py under long eye"

---
+++ super/see_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    different_way_and_problem(str_arg)
+    print('week')
+
+def different_way_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('woman_or_own_company')
