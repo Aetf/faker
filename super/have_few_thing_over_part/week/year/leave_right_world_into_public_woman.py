e215b49f157b49206288e6156547d272442f2008
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 13:55:10 -0400

    "mv: find super/know_number/few_life.py or super/have_few_thing_over_part/week/year/leave_right_world_into_public_woman.py about last fact"

---
+++ super/have_few_thing_over_part/week/year/leave_right_world_into_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man_or_right_fact(str_arg):
+    week_or_group(str_arg)
+    print('government_or_life')
+
+def week_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man_or_right_fact('give_different_life_with_few_week')
---
+++ super/know_number/few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    next_way(str_arg)
+    print('government_or_high_government')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('number_or_important_year')
