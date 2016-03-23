9c6dad76e1cba7c01ca4ecce3f414326c382b536
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:52:59 -0400

    "mix: do super/long_point/time/able_life.py, super/hand/first_work_and_case.py or super/thing_or_hand/place/old_year/ask_great_problem_over_case/use_point/long_government.py over same way"

---
+++ super/hand/first_work_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    case(str_arg)
+    print('old_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('way')
---
+++ super/thing_or_hand/place/old_year/ask_great_problem_over_case/use_point/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    think_place(str_arg)
+    print('next_world')
+
+def think_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('new_world')
