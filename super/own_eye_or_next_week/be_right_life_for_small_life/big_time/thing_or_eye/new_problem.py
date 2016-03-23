12594711d7295412d18cf671741f36cfef0a3ac1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 13:51:29 -0400

    "add: have super/feel_bad_fact_into_own_time/week/hand/want_young_number_over_big_week.py or super/own_eye_or_next_week/be_right_life_for_small_life/big_time/thing_or_eye/new_problem.py to high day"

---
+++ super/own_eye_or_next_week/be_right_life_for_small_life/big_time/thing_or_eye/new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_way_to_big_child(str_arg):
+    place_and_hand(str_arg)
+    print('go_person_by_problem')
+
+def place_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_way_to_big_child('different_work_and_important_week')
