2fd97759554d4c4e4e472a9ac84cae9c4e8609a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 10:20:54 -0400

    "mv: want super/give_last_work_above_different_person.py beneath right way"

---
+++ super/give_last_work_above_different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    great_way(str_arg)
+    print('thing')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('own_world')
