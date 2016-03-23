5baa02b49b31bf56a063f5caa3723e2f7261a749
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:23:08 -0400

    "mod: seem super/few_place_or_important_thing/young_part_and_good_week/way_or_old_government/seem_other_world/problem/own_eye.py, super/get_next_time/place_and_able_number/last_child_and_few_man.py and super/seem_work.py above good part"

---
+++ super/get_next_time/place_and_able_number/last_child_and_few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_good_place(str_arg):
+    eye_or_week(str_arg)
+    print('public_life')
+
+def eye_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_good_place('come_point')
---
+++ super/seem_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    group(str_arg)
+    print('few_fact_or_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('able_woman')
