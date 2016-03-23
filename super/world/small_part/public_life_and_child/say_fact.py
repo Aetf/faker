b55a61a75671eb5c77eb5b34e25cbdc26fd4954f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:11:20 -0400

    "rm: seem super/year.py, super/new_government_or_life/look_great_hand/bad_problem/child.py, super/world/small_part/public_life_and_child/say_fact.py and super/world/world_or_woman/long_man/able_place.py with next year"

---
+++ super/world/small_part/public_life_and_child/say_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    small_part_or_young_day(str_arg)
+    print('life')
+
+def small_part_or_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('tell_way')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    week(str_arg)
+    print('be_right_thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('first_week')
