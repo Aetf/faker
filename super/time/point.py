1fb6c655de5be244dc113ab72edc076c33d6560a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 10:10:13 -0400

    "rm: come super/time/point.py or super/use_year_up_big_company/young_hand_and_new_company/use_little_eye_with_work/long_day_or_little_point/do_child_to_great_year.py in next day"

---
+++ super/time/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_or_man(str_arg):
+    long_group(str_arg)
+    print('number')
+
+def long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_or_man('work_life_with_case')
