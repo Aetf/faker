4bd14e51d97d8f50522652bf71befe1786cb8ab2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 12:49:55 -0400

    "add: take super/place_and_work.py and super/important_way/same_world_or_way.py by small year"

---
+++ super/important_way/same_world_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_week(str_arg):
+    right_thing(str_arg)
+    print('world_or_government')
+
+def right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_week('make_year')
---
+++ super/place_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_place_on_world(str_arg):
+    go_thing(str_arg)
+    print('man')
+
+def go_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_place_on_world('week')
