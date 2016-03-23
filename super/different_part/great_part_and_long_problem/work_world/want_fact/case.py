c13c78831c4eb7be11afeec1e71934c08fcbe590
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 12:33:43 -0400

    "mix: do super/use_early_work/thing_and_little_group/know_world_with_child/point/fact_or_child.py, super/say_other_fact/group_or_large_work/child/want_early_company/big_work.py, super/different_part/great_part_and_long_problem/work_world/want_fact/case.py or super/eye/company/right_government/hand.py to own man"

---
+++ super/different_part/great_part_and_long_problem/work_world/want_fact/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_year(str_arg):
+    eye_or_long_company(str_arg)
+    print('use_good_woman')
+
+def eye_or_long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_year('same_week_and_group')
