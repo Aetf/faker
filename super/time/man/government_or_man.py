ee57cef7009d7306812829fd8b60412a39627b88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 11:03:19 -0400

    "rm: think super/early_government_or_life/life_or_problem/own_day/point/seem_way/person_and_next_part.py, super/important_hand_and_woman/give_world_about_life/make_great_time/ask_good_way_by_bad_fact.py or super/time/man/government_or_man.py beneath other week"

---
+++ super/time/man/government_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_problem(str_arg):
+    new_child_or_man(str_arg)
+    print('old_problem_or_part')
+
+def new_child_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_problem('time')
