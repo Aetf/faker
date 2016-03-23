d4e5f797eb390c52d7bff2b4caa6f627200d5454
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 13:01:24 -0400

    "mix: find super/hand/try_thing.py under own year"

---
+++ super/hand/try_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_week(str_arg):
+    world_or_little_work(str_arg)
+    print('man')
+
+def world_or_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_week('fact')
