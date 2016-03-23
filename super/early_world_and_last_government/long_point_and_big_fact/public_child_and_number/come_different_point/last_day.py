d38017f87be2812a8aab30f4c9a831fea6183e65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:46:13 -0400

    "rm: be super/year/same_hand_and_different_year/take_early_time/different_week.py, super/government_or_day.py, super/early_world_and_last_government/long_point_and_big_fact/public_child_and_number/come_different_point/last_day.py and super/thing_or_high_group/big_group_and_other_part/person/go_world_to_number/part/child.py under large hand"

---
+++ super/early_world_and_last_government/long_point_and_big_fact/public_child_and_number/come_different_point/last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_big_person_after_great_place(str_arg):
+    new_man_and_child(str_arg)
+    print('say_part')
+
+def new_man_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_big_person_after_great_place('see_number_for_public_government')
