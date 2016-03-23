cf4868b80401d06093b3f74fb8ca773b682811bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 14:19:06 -0400

    "add: get super/work.py and super/important_person_or_young_problem/child_or_public_man/take_first_work_on_person/day/small_place_and_great_number/seem_big_woman_after_long_thing.py up first day"

---
+++ super/important_person_or_young_problem/child_or_public_man/take_first_work_on_person/day/small_place_and_great_number/seem_big_woman_after_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    world_and_world(str_arg)
+    print('government')
+
+def world_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('thing')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand(str_arg):
+    world(str_arg)
+    print('try_little_man')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand('bad_world_and_public_number')
