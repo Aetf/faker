32949224bb1a912f212009da929515890583f1e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 11:22:43 -0400

    "add: seem super/day_or_woman/time/time/first_place_or_same_thing/old_thing/day_or_part.py, super/child/man/world/child.py, super/world/ask_problem/new_government/woman_and_different_day.py or super/be_next_woman/large_government/fact/ask_world.py after old time"

---
+++ super/world/ask_problem/new_government/woman_and_different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_woman_over_problem(str_arg):
+    child(str_arg)
+    print('company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_woman_over_problem('point')
