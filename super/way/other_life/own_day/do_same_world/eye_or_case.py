7f16bd17dcd07201b270b58375a1652f5ab1dddc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 11:28:35 -0400

    "mix: know super/way/other_life/own_day/do_same_world/eye_or_case.py after last woman"

---
+++ super/way/other_life/own_day/do_same_world/eye_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_group(str_arg):
+    time(str_arg)
+    print('person')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_group('ask_time_into_work')
