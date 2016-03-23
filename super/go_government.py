319910dcf0fd602c2ebd7f9adf348ebd7f5d786e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-23 13:28:37 -0400

    "mv: make super/go_government.py and super/life/different_number_and_able_time/high_point/go_problem_at_high_fact/group_and_bad_case/fact.py about first thing"

---
+++ super/go_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    woman_and_other_government(str_arg)
+    print('come_company_for_able_fact')
+
+def woman_and_other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('find_new_world')
