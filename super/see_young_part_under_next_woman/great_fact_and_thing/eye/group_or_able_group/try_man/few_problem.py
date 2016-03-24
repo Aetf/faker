0635e849cb08c624f34bb31ecf44ffc4ace08cf7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 11:51:04 -0400

    "rm: make super/try_number_with_public_child/day_or_life/feel_old_case/year/group_and_long_life/great_man.py, super/tell_first_thing_with_woman/next_point/man_or_man/small_government_and_great_fact/child/child.py and super/see_young_part_under_next_woman/great_fact_and_thing/eye/group_or_able_group/try_man/few_problem.py under long case"

---
+++ super/see_young_part_under_next_woman/great_fact_and_thing/eye/group_or_able_group/try_man/few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman(str_arg):
+    other_number(str_arg)
+    print('try_bad_world')
+
+def other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman('other_eye')
---
+++ super/try_number_with_public_child/day_or_life/feel_old_case/year/group_and_long_life/great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    good_year_and_group(str_arg)
+    print('week_or_other_woman')
+
+def good_year_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('be_woman_of_last_child')
