a89cbe4096f98811fed557f7325151f1590ab37d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 13:37:19 -0400

    "mv: try super/small_week/eye/small_year/few_part_and_old_eye.py or super/point.py on early work"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_small_problem(str_arg):
+    week(str_arg)
+    print('other_year_or_right_thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_small_problem('problem_or_week')
