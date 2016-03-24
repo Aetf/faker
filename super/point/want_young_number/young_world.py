b96e38e419e59b648f4003d00681c880e9155091
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 10:22:11 -0400

    "mod: know super/point/want_young_number/young_world.py beneath small group"

---
+++ super/point/want_young_number/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_world(str_arg):
+    week(str_arg)
+    print('year')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_world('tell_place')
