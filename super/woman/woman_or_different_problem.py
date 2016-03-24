366ddc7ab96dc39764810ed689ba4392460ed0c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 14:30:16 -0400

    "add: tell super/woman/woman_or_different_problem.py and super/early_case.py about same point"

---
+++ super/woman/woman_or_different_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    come_early_eye(str_arg)
+    print('good_company')
+
+def come_early_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('small_point')
