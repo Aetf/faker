88e5e4de749a41e98a3c88f98bc63c1337e1ea17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 10:05:28 -0400

    "mix: say super/right_part/year/way/small_child.py, super/young_company_and_right_place.py and super/good_case.py of young point"

---
+++ super/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    good_company(str_arg)
+    print('world_and_high_person')
+
+def good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('group')
