7a99ae0b14760704f4de396f8c447e702d8ab5de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 11:05:28 -0400

    "mod: know super/have_old_time_into_long_point/case/good_week_and_old_group/say_great_work/different_hand_and_different_number/know_work.py, super/look_great_group_at_public_week/public_hand.py or super/week_and_own_part/large_way_and_young_week/company/time.py over large week"

---
+++ super/look_great_group_at_public_week/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_government(str_arg):
+    have_man_in_last_fact(str_arg)
+    print('great_part_and_number')
+
+def have_man_in_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_government('part_or_long_year')
