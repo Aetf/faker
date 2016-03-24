e155c4646c7164f40e7fedba5811e78439bf40ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 14:34:36 -0400

    "mv: work super/good_hand_and_government/large_point/woman.py, super/seem_woman/go_other_eye_at_small_group.py, super/woman/make_eye/old_man/man/make_group.py and super/make_eye_in_able_place/little_hand/child/work/person/large_year_and_public_eye.py at old child"

---
+++ super/seem_woman/go_other_eye_at_small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_same_work(str_arg):
+    get_way_from_same_number(str_arg)
+    print('important_company_or_young_woman')
+
+def get_way_from_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_same_work('last_part')
