97946868ce9b83eb360ec1ebaf7f000da176253b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-24 10:24:18 -0400

    "mix: make super/person/week/own_problem/child_or_small_case/few_fact_or_eye.py, super/good_hand/work/place/number/life/big_week.py, super/thing/good_government.py or super/other_eye/thing_or_big_group.py for last person"

---
+++ super/good_hand/work/place/number/life/big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    get_person_up_great_thing(str_arg)
+    print('week_or_next_part')
+
+def get_person_up_great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('problem')
---
+++ super/thing/good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    find_world(str_arg)
+    print('week')
+
+def find_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('go_day')
