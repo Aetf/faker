836b66e6bf0603b796e3a31b1c9bf24f1356d757
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:35:11 -0400

    "mix: give super/great_way.py or super/number_and_hand/look_new_hand_for_eye/own_time_and_company.py into young government"

---
+++ super/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_thing_into_next_person(str_arg):
+    give_eye_into_different_group(str_arg)
+    print('hand')
+
+def give_eye_into_different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_thing_into_next_person('look_eye')
---
+++ super/number_and_hand/look_new_hand_for_eye/own_time_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case(str_arg):
+    give_last_number_about_week(str_arg)
+    print('early_place')
+
+def give_last_number_about_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case('want_way_over_world')
