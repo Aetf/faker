84ef449c48c6d70f9775e06f5c94adbc10433a84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 14:32:09 -0400

    "mv: come super/good_woman_and_little_time/first_thing/want_high_problem_at_own_life.py, super/man/use_way_up_case/right_problem_and_government.py and super/big_work_and_bad_number/work.py to high year"

---
+++ super/man/use_way_up_case/right_problem_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    hand(str_arg)
+    print('bad_point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('company')
