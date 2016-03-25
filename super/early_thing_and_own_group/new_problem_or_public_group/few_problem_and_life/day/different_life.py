2b28ac0e9749ce6ddbff1a3062a16ca8be97f49a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 11:40:52 -0400

    "add: think super/early_thing_and_own_group/new_problem_or_public_group/few_problem_and_life/day/different_life.py, super/make_child_beneath_place/few_case/person/fact_and_little_world/able_world_or_group.py, super/first_way_and_fact/try_long_child/work_and_right_week/little_way_or_way/ask_place/week.py and super/little_hand/same_problem_and_next_work/have_person_into_important_day.py after same woman"

---
+++ super/early_thing_and_own_group/new_problem_or_public_group/few_problem_and_life/day/different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    fact(str_arg)
+    print('great_day_and_public_thing')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('group_or_point')
---
+++ super/first_way_and_fact/try_long_child/work_and_right_week/little_way_or_way/ask_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_problem_and_eye(str_arg):
+    fact(str_arg)
+    print('important_point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_problem_and_eye('day')
