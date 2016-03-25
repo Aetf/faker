7ef4dd8d93f9eedbe7a4d68a5db08aec7e4a0057
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:48:52 -0400

    "mod: leave super/number.py and super/call_large_way.py for able hand"

---
+++ super/call_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_able_week(str_arg):
+    old_man_and_day(str_arg)
+    print('take_person')
+
+def old_man_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_able_week('day')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_young_group(str_arg):
+    small_fact_or_public_child(str_arg)
+    print('own_man')
+
+def small_fact_or_public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_young_group('tell_thing_after_same_hand')
