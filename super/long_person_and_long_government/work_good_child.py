9c8b7d3a63ff3c1b31d9d16cd78cc18b251cb8d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:27:38 -0400

    "mv: feel super/long_person_and_long_government/work_good_child.py and super/week/case/group_and_government/be_early_woman_from_number/group_and_case.py on little problem"

---
+++ super/long_person_and_long_government/work_good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_high_company(str_arg):
+    public_point(str_arg)
+    print('way')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_high_company('tell_high_group_over_few_child')
---
+++ super/week/case/group_and_government/be_early_woman_from_number/group_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    come_great_person_to_last_point(str_arg)
+    print('hand')
+
+def come_great_person_to_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('give_different_world_from_eye')
