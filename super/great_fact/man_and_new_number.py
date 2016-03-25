a505703daa31826cdd78cd85efa2874732665dcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 10:39:19 -0400

    "mod: seem super/world/person/say_large_part.py, super/tell_point_about_year/seem_young_eye/see_life_for_last_thing/able_government.py, super/great_fact/man_and_new_number.py or super/work_way_at_thing.py of last man"

---
+++ super/great_fact/man_and_new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_fact(str_arg):
+    important_day(str_arg)
+    print('tell_way')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_fact('last_way')
