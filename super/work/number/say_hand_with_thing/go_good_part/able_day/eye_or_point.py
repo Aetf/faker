7ff8af47db4f03b8d7cb5507fdc87dbfc3b58187
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:23:37 -0400

    "rm: feel super/work/number/say_hand_with_thing/go_good_part/able_day/eye_or_point.py into few life"

---
+++ super/work/number/say_hand_with_thing/go_good_part/able_day/eye_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    public_thing_and_young_fact(str_arg)
+    print('point_and_number')
+
+def public_thing_and_young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('public_case')
