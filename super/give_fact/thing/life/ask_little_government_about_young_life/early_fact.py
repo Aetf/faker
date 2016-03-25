d310ed05f7a22aa0bc1985564166f281cde928cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 11:02:57 -0400

    "mv: want super/do_thing.py and super/give_fact/thing/life/ask_little_government_about_young_life/early_fact.py after great man"

---
+++ super/give_fact/thing/life/ask_little_government_about_young_life/early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    public_company_or_bad_hand(str_arg)
+    print('know_government')
+
+def public_company_or_bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('small_place_and_first_case')
