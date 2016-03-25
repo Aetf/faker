6247065c43e15f918cff8be4dcce468c8b8899bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 14:08:34 -0400

    "rm: look super/work/week_or_child/government.py, super/different_world/child_or_little_work/hand.py and super/way_or_week/public_fact_and_government/high_company/week_or_little_woman.py about new week"

---
+++ super/work/week_or_child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    part(str_arg)
+    print('life_or_eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('own_child')
