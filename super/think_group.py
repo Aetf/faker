6720196692fbf29c7b5e27f5c9103dbff9d28706
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 11:59:20 -0400

    "mod: think super/think_group.py beneath new fact"

---
+++ super/think_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    early_day(str_arg)
+    print('day_or_early_group')
+
+def early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('few_government_or_first_company')
