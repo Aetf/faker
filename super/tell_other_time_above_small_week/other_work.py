0cfa898399c546819128727ebf95219987b2feb9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 12:22:56 -0400

    "add: come super/same_week_and_same_man/public_year_or_good_point/little_time/right_part.py and super/tell_other_time_above_small_week/other_work.py to small man"

---
+++ super/tell_other_time_above_small_week/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_different_day(str_arg):
+    work_bad_world(str_arg)
+    print('bad_work')
+
+def work_bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_different_day('man_and_year')
