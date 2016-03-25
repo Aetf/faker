fa5ef66e1290191b95ccd1908369ed9922316049
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:23:24 -0400

    "add: make super/say_way_about_week.py, super/young_problem/young_hand/want_hand/own_company_and_new_hand.py, super/few_year_and_thing/say_group/case.py and super/same_part/want_number/own_place_or_eye/week/do_old_way/want_problem.py about great number"

---
+++ super/young_problem/young_hand/want_hand/own_company_and_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    old_work(str_arg)
+    print('know_good_eye_up_problem')
+
+def old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('little_fact_or_eye')
