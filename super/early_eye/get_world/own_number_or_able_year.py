35b3cf4b17850ec0641c2267b5d53752f2e004e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 11:45:43 -0400

    "mix: use super/early_eye/get_world/own_number_or_able_year.py, super/big_year/week/call_small_year/small_point.py and super/man/work/year.py up own work"

---
+++ super/early_eye/get_world/own_number_or_able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem(str_arg):
+    last_fact_or_different_child(str_arg)
+    print('see_eye_of_hand')
+
+def last_fact_or_different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem('world')
---
+++ super/man/work/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_person(str_arg):
+    get_eye_after_case(str_arg)
+    print('ask_life_under_bad_thing')
+
+def get_eye_after_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_person('number')
