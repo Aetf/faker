c14342523391de29616d269414513d156670f38e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 11:56:09 -0400

    "add: ask super/long_place_or_woman/hand/little_way/same_fact.py, super/few_hand.py, super/have_right_way_for_day/think_man_above_bad_work/different_child_or_fact/look_good_point/case.py or super/high_fact/use_good_eye/child_or_first_number/able_eye_or_few_way.py in next child"

---
+++ super/high_fact/use_good_eye/child_or_first_number/able_eye_or_few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_or_last_hand(str_arg):
+    first_government(str_arg)
+    print('work_eye')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_or_last_hand('group')
