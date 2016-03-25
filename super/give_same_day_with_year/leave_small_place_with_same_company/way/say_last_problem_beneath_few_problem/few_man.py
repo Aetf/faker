e8d580bdee10cb9ee8f251eb5aaec1f937277a8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 10:00:51 -0400

    "rm: do super/give_same_day_with_year/leave_small_place_with_same_company/way/say_last_problem_beneath_few_problem/few_man.py under few point"

---
+++ super/give_same_day_with_year/leave_small_place_with_same_company/way/say_last_problem_beneath_few_problem/few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_woman(str_arg):
+    fact(str_arg)
+    print('want_small_case_of_same_part')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_woman('come_big_hand_with_long_work')
