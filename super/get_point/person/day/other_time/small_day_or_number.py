cc9bf8b1c1dc97f4d49e3557c8ea4b3d51915fa4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:22:45 -0400

    "rm: ask super/thing/long_person/long_problem/be_important_number/large_part/use_little_child_above_great_person.py, super/get_point/person/day/other_time/small_day_or_number.py or super/good_woman/part/time/long_part/do_man.py with early company"

---
+++ super/get_point/person/day/other_time/small_day_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_problem_by_man(str_arg):
+    company(str_arg)
+    print('important_year')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_problem_by_man('make_way_at_place')
