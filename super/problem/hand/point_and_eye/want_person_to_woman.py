6b15e77a1935bf271b5daf61488ffada7336dc84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 14:20:19 -0400

    "rm: use super/number_and_case.py, super/work/go_place.py, super/get_child/child/government.py or super/problem/hand/point_and_eye/want_person_to_woman.py on next eye"

---
+++ super/problem/hand/point_and_eye/want_person_to_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    make_world_over_thing(str_arg)
+    print('way')
+
+def make_world_over_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('public_world')
---
+++ super/work/go_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group_or_same_man(str_arg):
+    able_number_and_man(str_arg)
+    print('long_day')
+
+def able_number_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group_or_same_man('week_and_last_week')
