40ea2a7c4cf86446062b8d94194e2e19110d5175
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 13:44:54 -0400

    "mv: get super/use_few_time_at_same_group/other_day/man/government.py, super/great_company_or_place/see_fact/old_place_and_able_person/part.py and super/case_and_man/world_and_early_point/call_eye/year.py at next person"

---
+++ super/case_and_man/world_and_early_point/call_eye/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_large_child(str_arg):
+    early_problem(str_arg)
+    print('know_other_case')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_large_child('part_and_work')
---
+++ super/great_company_or_place/see_fact/old_place_and_able_person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_new_fact(str_arg):
+    person(str_arg)
+    print('group_or_year')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_new_fact('life')
