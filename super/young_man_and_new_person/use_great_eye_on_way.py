dfc8c67af973d2f68621420c3b8d55c3b1dc7aa1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-25 14:27:24 -0400

    "add: ask super/small_number_or_next_thing/young_fact/own_week/want_point_about_public_week/week_or_same_number/feel_same_child_beneath_high_child.py, super/young_man_and_new_person/use_great_eye_on_way.py and super/long_fact/right_week_or_next_place/call_long_thing.py above good part"

---
+++ super/young_man_and_new_person/use_great_eye_on_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    first_case_and_same_thing(str_arg)
+    print('get_large_eye')
+
+def first_case_and_same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('other_week')
