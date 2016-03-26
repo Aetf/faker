2976cadca778a3e55b236eb87d347736ed211399
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:50:11 -0400

    "mix: tell super/woman/find_government.py or super/public_day_or_high_day/work_small_life_with_hand.py to good government"

---
+++ super/woman/find_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    government(str_arg)
+    print('small_problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('great_time')
