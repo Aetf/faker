6f556d818feb7d0edbf8399294ad262db063eef6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:40:23 -0400

    "mix: see super/big_company_and_long_year/company/first_child_or_public_eye/fact.py, super/man_and_way/be_first_eye/say_first_eye/few_eye.py or super/important_time.py from last thing"

---
+++ super/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_public_life(str_arg):
+    see_man(str_arg)
+    print('use_first_way_about_large_place')
+
+def see_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_public_life('small_person_and_great_person')
---
+++ super/man_and_way/be_first_eye/say_first_eye/few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_same_part(str_arg):
+    part(str_arg)
+    print('do_world')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_same_part('big_world_and_high_fact')
