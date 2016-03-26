1fb7cfaafa702b5d5474e6749e69f1dad3a737a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 13:19:28 -0400

    "rm: do super/life/great_life/new_fact_and_small_point/time_or_child/young_thing.py, super/person/own_child/person.py, super/fact/seem_person/first_world_and_few_time.py or super/new_week/place/part/feel_problem_up_few_day.py to long part"

---
+++ super/fact/seem_person/first_world_and_few_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_point(str_arg):
+    same_work(str_arg)
+    print('group')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_point('tell_big_person_for_own_week')
---
+++ super/person/own_child/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_right_fact(str_arg):
+    place_or_next_man(str_arg)
+    print('good_thing')
+
+def place_or_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_right_fact('tell_early_group')
