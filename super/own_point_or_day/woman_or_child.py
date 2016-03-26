bd404e794efc1c30af03f640f6d0336394716c6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 11:02:46 -0400

    "rm: use super/number/week_and_high_place/place/take_large_eye/want_first_day/get_other_hand_under_number.py and super/own_point_or_day/woman_or_child.py by own hand"

---
+++ super/own_point_or_day/woman_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    get_own_year(str_arg)
+    print('fact_or_child')
+
+def get_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('young_part_or_long_work')
