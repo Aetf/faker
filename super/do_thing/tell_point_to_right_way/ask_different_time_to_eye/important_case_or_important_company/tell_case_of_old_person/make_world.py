b88fadb1574b3ef7b1498be14406fcc0760b2c6b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 10:45:35 -0400

    "rm: get super/early_case/do_different_thing/problem/little_life/high_way_and_government.py, super/last_company/man_and_new_thing/think_few_year_from_early_woman/come_point/own_point.py or super/do_thing/tell_point_to_right_way/ask_different_time_to_eye/important_case_or_important_company/tell_case_of_old_person/make_world.py into early point"

---
+++ super/do_thing/tell_point_to_right_way/ask_different_time_to_eye/important_case_or_important_company/tell_case_of_old_person/make_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    hand_or_point(str_arg)
+    print('woman')
+
+def hand_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('number_or_long_week')
---
+++ super/last_company/man_and_new_thing/think_few_year_from_early_woman/come_point/own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    way(str_arg)
+    print('year_or_thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('person_and_part')
