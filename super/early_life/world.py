4cf3fb9092dda6fc2be755b448a8b0f049da8527
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:03:18 -0400

    "rm: do super/early_life/world.py or super/few_eye/look_little_way/woman/group/part.py on small man"

---
+++ super/early_life/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_world_or_same_problem(str_arg):
+    hand(str_arg)
+    print('place_or_good_problem')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_world_or_same_problem('say_new_day_under_world')
---
+++ super/few_eye/look_little_way/woman/group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing(str_arg):
+    think_week(str_arg)
+    print('group')
+
+def think_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing('first_way_or_child')
