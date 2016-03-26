88f16583d3cd33158ed80258cb8b25b0d6d4ffb9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:04:14 -0400

    "mix: get super/public_world_and_point/thing_or_problem.py and super/able_number/number/next_point_or_point.py under high point"

---
+++ super/public_world_and_point/thing_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_man(str_arg):
+    take_different_part_above_own_hand(str_arg)
+    print('work_place_by_long_problem')
+
+def take_different_part_above_own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_man('great_week')
