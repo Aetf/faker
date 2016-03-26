41af722c2493589da1a27dc8b84c247d5d3cd1c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:16:47 -0400

    "mv: use super/person/problem_or_place/place_and_old_child/old_life_or_place/long_eye_or_next_person/important_fact_and_company.py, super/young_government/think_other_man.py and super/different_day/tell_thing_for_long_work/good_week.py on few way"

---
+++ super/different_day/tell_thing_for_long_work/good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    case_or_good_week(str_arg)
+    print('week')
+
+def case_or_good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('child')
---
+++ super/young_government/think_other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man_or_time(str_arg):
+    point_or_group(str_arg)
+    print('long_group')
+
+def point_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man_or_time('woman')
