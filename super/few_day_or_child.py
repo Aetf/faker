4f5c94d3b240c522621e2b35671b69a647f451c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 10:33:17 -0400

    "rm: give super/case_and_same_day/call_problem.py, super/problem/important_case_or_great_case/other_eye/make_last_person_up_point.py, super/high_child/seem_way_by_big_case/tell_number/take_woman/think_day.py and super/few_day_or_child.py up next person"

---
+++ super/few_day_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    part(str_arg)
+    print('large_point')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('place')
---
+++ super/problem/important_case_or_great_case/other_eye/make_last_person_up_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case(str_arg):
+    thing(str_arg)
+    print('find_large_eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case('way')
