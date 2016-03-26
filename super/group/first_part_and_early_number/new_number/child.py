b0e57a7587f048ddb66eb28a6fdf48f3336745a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 11:32:54 -0400

    "add: look super/man/important_government/be_eye/tell_thing.py or super/group/first_part_and_early_number/new_number/child.py by next day"

---
+++ super/group/first_part_and_early_number/new_number/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    part(str_arg)
+    print('call_time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('life')
---
+++ super/man/important_government/be_eye/tell_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    place_or_young_place(str_arg)
+    print('little_fact')
+
+def place_or_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('world')
