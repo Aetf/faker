bb0cb6494c6ac6bb99db1fda505212aa994ee3b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 12:16:28 -0400

    "mod: be super/leave_public_life/year/thing_or_able_eye.py, super/use_man_above_first_group/person/say_fact_over_good_week.py, super/point/new_thing.py and super/eye/woman_or_public_work/come_hand_at_government/able_year/man/come_world_up_other_company.py up important hand"

---
+++ super/eye/woman_or_public_work/come_hand_at_government/able_year/man/come_world_up_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    try_way_over_last_time(str_arg)
+    print('week')
+
+def try_way_over_last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('woman')
---
+++ super/point/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    good_time_or_point(str_arg)
+    print('world_and_able_world')
+
+def good_time_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('group_and_company')
