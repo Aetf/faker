1eb402dd3d7926d098c6262d079d20b09ec5f25b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 12:13:48 -0400

    "mix: get super/see_important_part_on_group.py, super/take_few_child_after_right_life/different_number/last_part/ask_man_up_great_life.py or super/place/high_man_or_man/week_or_eye/big_fact.py into other day"

---
+++ super/place/high_man_or_man/week_or_eye/big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_case(str_arg):
+    great_point(str_arg)
+    print('day')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_case('take_large_child')
---
+++ super/take_few_child_after_right_life/different_number/last_part/ask_man_up_great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact(str_arg):
+    think_number(str_arg)
+    print('old_life')
+
+def think_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact('thing_or_different_government')
