93d6aa1b00200c0667d45a0b23688e80c62139f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 14:22:37 -0400

    "mix: look super/hand/own_world.py over new work"

---
+++ super/hand/own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_day_under_public_week(str_arg):
+    right_way(str_arg)
+    print('other_world')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_day_under_public_week('year_or_good_work')
