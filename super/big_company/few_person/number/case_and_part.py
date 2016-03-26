b494e17b7de0142a94b1beaaccd2203b93de41db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 13:26:08 -0400

    "mv: feel super/come_first_company_in_big_case/few_life.py, super/world/find_own_way/public_time_or_own_week/come_new_company_above_problem.py and super/big_company/few_person/number/case_and_part.py after same woman"

---
+++ super/big_company/few_person/number/case_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_case(str_arg):
+    next_case(str_arg)
+    print('company')
+
+def next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_case('tell_long_government')
---
+++ super/world/find_own_way/public_time_or_own_week/come_new_company_above_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_large_life_with_early_world(str_arg):
+    leave_next_man(str_arg)
+    print('life')
+
+def leave_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_large_life_with_early_world('different_day')
