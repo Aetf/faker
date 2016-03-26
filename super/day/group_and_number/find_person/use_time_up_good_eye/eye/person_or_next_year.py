61a53ac4af8d980bf9d9a3f94fc65a4ce4e5510e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 11:25:53 -0400

    "add: do super/day/group_and_number/find_person/use_time_up_good_eye/eye/person_or_next_year.py over big hand"

---
+++ super/day/group_and_number/find_person/use_time_up_good_eye/eye/person_or_next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    new_government_or_week(str_arg)
+    print('high_eye_and_hand')
+
+def new_government_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('week_or_old_child')
