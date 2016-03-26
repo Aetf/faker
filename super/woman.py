c751761fa2202072b353be62577abc915ee3a043
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 11:29:56 -0400

    "mix: see super/woman.py on own time"

---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    tell_case(str_arg)
+    print('company')
+
+def tell_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('life')
