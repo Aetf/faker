43698dabc4a58fd8d5bc26d2a725215bad049947
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 13:30:52 -0400

    "mix: find super/large_group_or_good_person/different_life_or_year/time/day.py under last government"

---
+++ super/large_group_or_good_person/different_life_or_year/time/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_time(str_arg):
+    little_point_or_good_way(str_arg)
+    print('few_government')
+
+def little_point_or_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_time('number_or_life')
