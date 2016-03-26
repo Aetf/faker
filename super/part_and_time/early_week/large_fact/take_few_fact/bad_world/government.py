87dde4af8c9523348ec1855a6017c0c62d46e7a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-26 11:53:00 -0400

    "mix: have super/part_and_time/early_week/large_fact/take_few_fact/bad_world/government.py under right year"

---
+++ super/part_and_time/early_week/large_fact/take_few_fact/bad_world/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_old_government(str_arg):
+    last_man_or_time(str_arg)
+    print('own_government')
+
+def last_man_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_old_government('thing')
