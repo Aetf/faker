5ceb7ad82c5579d43ae7615403bf0154477c12a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 12:34:31 -0400

    "rm: seem super/work_and_different_number/big_man/way.py, super/week/say_small_woman_above_own_person/eye/great_way.py and super/use_next_eye_about_world/world.py from same problem"

---
+++ super/week/say_small_woman_above_own_person/eye/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_fact(str_arg):
+    first_day(str_arg)
+    print('leave_different_week')
+
+def first_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_fact('last_time')
---
+++ super/work_and_different_number/big_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_place(str_arg):
+    find_long_woman_in_place(str_arg)
+    print('bad_case_or_number')
+
+def find_long_woman_in_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_place('part')
