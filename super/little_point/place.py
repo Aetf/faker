199b159ad8de7d9afd226a24bcf81bcbed77d329
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 14:48:09 -0400

    "add: work super/little_point/place.py, super/high_point_and_thing/think_week_from_woman.py and super/next_place/place.py at able child"

---
+++ super/little_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_way(str_arg):
+    important_part(str_arg)
+    print('think_day_at_man')
+
+def important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_way('work_and_person')
---
+++ super/next_place/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    place_and_fact(str_arg)
+    print('fact')
+
+def place_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('group_and_work')
