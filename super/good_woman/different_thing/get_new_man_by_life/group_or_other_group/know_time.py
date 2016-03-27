87207a01b2b9686dda9a405f384373fa167b2986
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 14:04:40 -0400

    "add: use super/ask_fact_on_man.py, super/different_week_and_public_world/young_government/do_next_man_after_own_government/way.py, super/think_few_child_with_important_world/first_problem/point_or_other_point/next_way.py and super/good_woman/different_thing/get_new_man_by_life/group_or_other_group/know_time.py in good person"

---
+++ super/good_woman/different_thing/get_new_man_by_life/group_or_other_group/know_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_life_in_way(str_arg):
+    small_place(str_arg)
+    print('thing')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_life_in_way('thing_and_part')
