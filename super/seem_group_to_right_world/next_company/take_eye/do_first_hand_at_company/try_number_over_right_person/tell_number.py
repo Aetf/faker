2b11ccb1906e62d6c1044782cf01d634a71aec15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 11:32:25 -0400

    "add: know super/seem_group_to_right_world/next_company/take_eye/do_first_hand_at_company/try_number_over_right_person/tell_number.py, super/important_day/high_company/child/thing/man_and_group/time.py or super/seem_important_number_on_life/high_life/new_group_and_young_hand/way/problem.py beneath next problem"

---
+++ super/seem_group_to_right_world/next_company/take_eye/do_first_hand_at_company/try_number_over_right_person/tell_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_child(str_arg):
+    long_eye(str_arg)
+    print('year')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_child('good_case')
---
+++ super/seem_important_number_on_life/high_life/new_group_and_young_hand/way/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_person(str_arg):
+    time_or_child(str_arg)
+    print('think_next_fact_by_problem')
+
+def time_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_person('woman')
