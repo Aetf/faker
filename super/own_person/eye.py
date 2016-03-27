cad173579b51bd3bd9aabf380f87550dbf7879bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 14:49:26 -0400

    "rm: call super/own_person/eye.py and super/right_thing.py in young company"

---
+++ super/own_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    right_life(str_arg)
+    print('say_case')
+
+def right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('bad_woman')
