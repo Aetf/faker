02e6fc69a97407fe90762028f27b54b48ed698bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 10:22:48 -0400

    "mod: get super/early_part/number/fact/able_company/old_part/small_group.py in right man"

---
+++ super/early_part/number/fact/able_company/old_part/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_week_from_man(str_arg):
+    different_time(str_arg)
+    print('work_or_company')
+
+def different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_week_from_man('number')
