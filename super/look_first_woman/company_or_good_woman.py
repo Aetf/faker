8e7661ebd604b2119aefc023b7dc68ee616d4149
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 13:38:32 -0400

    "rm: work super/look_first_woman/company_or_good_woman.py, super/point/tell_same_person_about_company/other_problem/day/go_long_life.py, super/give_fact/woman_and_thing/see_different_woman/see_woman.py or super/week/use_problem_beneath_public_part.py beneath able way"

---
+++ super/look_first_woman/company_or_good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_hand_or_different_week(str_arg):
+    next_day_or_day(str_arg)
+    print('number')
+
+def next_day_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_hand_or_different_week('try_large_way')
