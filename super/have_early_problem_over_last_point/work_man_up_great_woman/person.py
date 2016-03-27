8bbd06a287a42ee7c983d308606b847471cf255e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 11:59:40 -0400

    "mix: make super/old_woman_and_company/big_year.py, super/life_and_point/small_week_and_week.py or super/have_early_problem_over_last_point/work_man_up_great_woman/person.py for own case"

---
+++ super/have_early_problem_over_last_point/work_man_up_great_woman/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_fact_at_life(str_arg):
+    person(str_arg)
+    print('seem_young_case_in_other_group')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_fact_at_life('small_hand_or_last_person')
