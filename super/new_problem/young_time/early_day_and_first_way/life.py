655e0b060da75ab649e0ded8ecc43c70a57c0426
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 11:45:37 -0400

    "add: look super/eye/man/young_day_or_woman/bad_part/year/woman.py, super/new_problem/young_time/early_day_and_first_way/life.py and super/hand/time_and_little_year/person.py to last group"

---
+++ super/new_problem/young_time/early_day_and_first_way/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_world(str_arg):
+    get_great_work(str_arg)
+    print('ask_thing')
+
+def get_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_world('week')
