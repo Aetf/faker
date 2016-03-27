ae9c32c5ff15894d9183712301ede432d193dd7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 12:59:27 -0400

    "mv: get super/public_way_or_week/group.py, super/good_year.py or super/make_problem.py about bad man"

---
+++ super/make_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    own_fact(str_arg)
+    print('work_or_next_place')
+
+def own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('same_person')
---
+++ super/public_way_or_week/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye_or_person(str_arg):
+    man_and_old_group(str_arg)
+    print('child')
+
+def man_and_old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye_or_person('public_thing')
