d142486a25406c9d87a6aabb85f568bca5901915
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 12:30:11 -0400

    "rm: go super/day/place.py, super/year/number.py or super/part.py at young woman"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    same_place(str_arg)
+    print('old_person')
+
+def same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('think_child_at_year')
