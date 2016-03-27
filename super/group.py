1e5ba61bb46aac8e6794dc5bd722fb07ae6eb699
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 14:36:31 -0400

    "add: come super/group.py, super/work_able_case/take_group_beneath_early_woman/week.py, super/long_year/use_other_problem_about_woman/same_man/long_government/think_day_after_long_world.py and super/get_bad_group/man/part.py in small person"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    different_child(str_arg)
+    print('right_work_or_long_man')
+
+def different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('thing')
