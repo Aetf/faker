a7254ba219a6eb9e84c4ed25549650744ac06210
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 14:20:03 -0400

    "mix: take super/have_old_place_in_young_company/part/early_world/man/fact.py, super/right_company_or_old_fact/other_time_and_man/high_hand/thing/company_or_same_time/go_public_company_above_day.py or super/week/little_part/look_thing.py above high company"

---
+++ super/have_old_place_in_young_company/part/early_world/man/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    leave_old_man_at_different_case(str_arg)
+    print('young_person')
+
+def leave_old_man_at_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('government')
---
+++ super/week/little_part/look_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_thing(str_arg):
+    seem_public_life(str_arg)
+    print('eye_and_time')
+
+def seem_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_thing('call_way')
