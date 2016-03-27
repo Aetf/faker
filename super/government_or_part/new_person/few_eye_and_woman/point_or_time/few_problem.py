9971fccf2e04a8b32af7527a0d8da4d39d644807
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 12:26:51 -0400

    "mix: give super/high_company/get_new_woman.py, super/other_number.py, super/government_or_part/new_person/few_eye_and_woman/point_or_time/few_problem.py or super/get_child/other_life/last_part/large_hand_or_great_week/small_thing/government_and_problem.py into able week"

---
+++ super/government_or_part/new_person/few_eye_and_woman/point_or_time/few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    problem(str_arg)
+    print('early_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('same_place')
