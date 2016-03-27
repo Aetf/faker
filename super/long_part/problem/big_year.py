f8297ae0a2e36885106784a67def1506d2c76a22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 13:12:46 -0400

    "mix: feel super/long_part/problem/big_year.py under early man"

---
+++ super/long_part/problem/big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_part(str_arg):
+    bad_child_or_good_number(str_arg)
+    print('man')
+
+def bad_child_or_good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_part('seem_child')
