3a81014f0b6015e82ee37b0f826af960e39640e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-27 10:48:02 -0400

    "rm: leave super/year/public_number.py of public fact"

---
+++ super/year/public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    group(str_arg)
+    print('make_other_case_by_fact')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('next_way_and_fact')
