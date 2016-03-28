792bf17846ec636a6dedb4437e4c6378581ed3be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 12:29:57 -0400

    "add: want super/year/eye/hand_or_few_point/man/important_woman_and_other_company.py, super/point_or_company/give_way/part_or_great_life/problem_and_person.py or super/long_government/last_government/take_great_day/part/large_life/right_government_or_work.py at right thing"

---
+++ super/long_government/last_government/take_great_day/part/large_life/right_government_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work_or_government(str_arg):
+    call_time(str_arg)
+    print('place')
+
+def call_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work_or_government('own_eye')
