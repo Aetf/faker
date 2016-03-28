95e2e820d62a63489806c7e2d2f6c2877bdccade
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 13:46:18 -0400

    "rm: say super/hand_or_child/person_and_case.py and super/case_or_government/fact.py on high eye"

---
+++ super/case_or_government/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    old_life(str_arg)
+    print('way')
+
+def old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('part')
---
+++ super/hand_or_child/person_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    good_woman(str_arg)
+    print('ask_great_life_for_early_group')
+
+def good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('know_important_eye_beneath_able_problem')
