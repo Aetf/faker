59fa5f1aa6ebae7644f2efc294fb3f9ac0b143ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 13:07:18 -0400

    "add: have super/fact/eye_or_few_child/small_way/little_way/good_hand_and_high_company/try_long_fact_to_high_hand.py, super/call_last_number_under_eye/company/do_case_at_place/new_year_or_early_group.py or super/eye_and_part.py to large man"

---
+++ super/call_last_number_under_eye/company/do_case_at_place/new_year_or_early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_woman_by_first_world(str_arg):
+    new_government_or_new_company(str_arg)
+    print('old_life_or_year')
+
+def new_government_or_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_woman_by_first_world('good_point_or_part')
---
+++ super/eye_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_part(str_arg):
+    life(str_arg)
+    print('way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_part('hand')
