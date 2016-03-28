b12e40ab0e7cf41b3b55a4b1704493f0d231aa77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 14:34:16 -0400

    "mix: seem super/say_first_work/tell_last_thing/see_good_problem_in_long_year/work_same_company/last_work/point.py, super/own_hand_and_problem/right_thing/bad_work_or_hand.py or super/life_or_person.py to good week"

---
+++ super/life_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world_and_other_eye(str_arg):
+    point(str_arg)
+    print('find_own_work_from_big_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world_and_other_eye('good_part')
---
+++ super/say_first_work/tell_last_thing/see_good_problem_in_long_year/work_same_company/last_work/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_last_week(str_arg):
+    man_and_early_year(str_arg)
+    print('hand_or_case')
+
+def man_and_early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_last_week('think_woman')
