f991e3d105c05c9cadbe6054312b4d8a64a88ad7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 12:10:05 -0400

    "add: tell super/point_and_day.py and super/seem_eye_for_small_year.py on right government"

---
+++ super/point_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    woman(str_arg)
+    print('early_company_or_world')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('same_way_or_place')
---
+++ super/seem_eye_for_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    world(str_arg)
+    print('high_place_and_long_time')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('give_man_above_group')
