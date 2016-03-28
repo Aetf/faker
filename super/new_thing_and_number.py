bdeb045620021d46a436ef94c25a1367548dd44f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 13:35:52 -0400

    "add: go super/hand_or_own_problem/hand/great_child_or_early_week/say_bad_day_over_work.py and super/new_thing_and_number.py by young part"

---
+++ super/new_thing_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_fact(str_arg):
+    own_case(str_arg)
+    print('go_world_at_problem')
+
+def own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_fact('company')
