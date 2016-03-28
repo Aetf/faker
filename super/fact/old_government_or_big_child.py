52933235788b88cbbe08155711776955cb34689d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 11:01:36 -0400

    "mix: try super/fact/old_government_or_big_child.py to important eye"

---
+++ super/fact/old_government_or_big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_problem_to_woman(str_arg):
+    find_man_in_right_number(str_arg)
+    print('great_part')
+
+def find_man_in_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_problem_to_woman('different_time')
