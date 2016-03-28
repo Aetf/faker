9c0029fb4a79c86d22d516137544fc819cb03df5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 11:18:12 -0400

    "mix: leave super/large_case_or_time/fact/few_man/week/point/other_life_or_big_way.py or super/time.py for high number"

---
+++ super/large_case_or_time/fact/few_man/week/point/other_life_or_big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    year_or_week(str_arg)
+    print('be_woman')
+
+def year_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('large_time')
