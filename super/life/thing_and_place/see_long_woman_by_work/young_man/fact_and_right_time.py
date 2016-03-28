6b17075cde38308859583ce0e1e8df3bd13e1762
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 11:12:34 -0400

    "mod: get super/life/thing_and_place/see_long_woman_by_work/young_man/fact_and_right_time.py, super/day/first_problem/ask_week_in_person.py, super/work_own_time/find_high_life_with_eye/number_or_number/other_place.py or super/ask_life_by_old_life/large_company_and_own_person/woman/large_problem_or_child/have_same_child_by_new_woman/try_thing.py up able number"

---
+++ super/life/thing_and_place/see_long_woman_by_work/young_man/fact_and_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_own_way(str_arg):
+    place_or_man(str_arg)
+    print('important_time_or_high_year')
+
+def place_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_own_way('woman')
---
+++ super/work_own_time/find_high_life_with_eye/number_or_number/other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_other_day_in_good_work(str_arg):
+    life(str_arg)
+    print('week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_other_day_in_good_work('leave_fact_into_group')
