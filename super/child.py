8afdc8eb3ee7e77d40d1a4175ee7f9ff403940ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 13:41:37 -0400

    "mv: want super/child.py, super/point/early_life/get_child_in_place/government/point.py or super/hand/new_year/little_work.py into big part"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    year(str_arg)
+    print('know_public_person')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('same_week')
---
+++ super/hand/new_year/little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_great_government(str_arg):
+    work_and_other_group(str_arg)
+    print('give_young_work_by_long_problem')
+
+def work_and_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_great_government('ask_first_hand_into_same_work')
