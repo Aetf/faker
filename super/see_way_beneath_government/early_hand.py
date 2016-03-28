eb9a3318c86bedceae598424e1598e63933a3441
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 14:42:12 -0400

    "add: be super/think_world/able_child_or_public_man/small_day.py and super/see_way_beneath_government/early_hand.py under little problem"

---
+++ super/see_way_beneath_government/early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    be_right_part(str_arg)
+    print('world')
+
+def be_right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('early_day')
