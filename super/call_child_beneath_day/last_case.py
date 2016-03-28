1a1af1fcd0f5a015a05e898fd2abcdf475a4f071
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 14:58:35 -0400

    "mv: work super/year/know_own_eye/fact_and_point/think_able_number_in_world/way_or_different_fact.py or super/call_child_beneath_day/last_case.py about other eye"

---
+++ super/call_child_beneath_day/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_place_above_next_day(str_arg):
+    right_part(str_arg)
+    print('company')
+
+def right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_place_above_next_day('want_few_company')
