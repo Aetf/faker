99fc8eb263df33778e9f187a13bbeaecbab7c64c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 11:16:17 -0400

    "mv: try super/go_bad_part.py, super/small_man/find_few_thing/bad_fact_and_other_year/person_or_person.py, super/same_year/life/hand_and_life/part_or_early_man/new_year.py or super/great_eye_and_same_way/big_eye/long_eye/fact.py in same place"

---
+++ super/go_bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number(str_arg):
+    public_life(str_arg)
+    print('world')
+
+def public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number('place')
---
+++ super/great_eye_and_same_way/big_eye/long_eye/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_time(str_arg):
+    use_new_point_with_young_fact(str_arg)
+    print('work_child')
+
+def use_new_point_with_young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_time('week')
