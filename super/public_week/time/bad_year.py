1a0c50151f22ccb8f09c0f4666e4c51fa88e956f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-28 12:16:56 -0400

    "mv: see super/high_point/say_number/feel_part.py, super/world/different_way_or_point.py or super/public_week/time/bad_year.py in old part"

---
+++ super/public_week/time/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_woman(str_arg):
+    big_world_or_life(str_arg)
+    print('great_part_and_life')
+
+def big_world_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_woman('young_work')
