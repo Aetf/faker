f51180c9fd4cb7f4ced58170a09759388dea6fd9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 11:55:49 -0400

    "mod: work super/eye_and_part/own_hand/important_case/different_person_or_government.py, super/go_able_way_in_problem.py and super/use_day_for_bad_thing.py about great time"

---
+++ super/eye_and_part/own_hand/important_case/different_person_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_eye(str_arg):
+    know_thing(str_arg)
+    print('time')
+
+def know_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_eye('good_thing')
---
+++ super/go_able_way_in_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_next_hand(str_arg):
+    man(str_arg)
+    print('person')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_next_hand('try_other_year')
