791a3f40e58a658037b3eadca0fa65539c1b0281
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 10:23:23 -0400

    "rm: find super/bad_time/problem/give_eye/first_man/right_company/call_new_week_above_public_company.py, super/good_company_and_day.py, super/person_and_great_child/point/take_place.py and super/world_and_point/good_day/point_and_good_fact/work_child.py with able case"

---
+++ super/good_company_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    go_point(str_arg)
+    print('life_or_group')
+
+def go_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('man_and_number')
---
+++ super/world_and_point/good_day/point_and_good_fact/work_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    case_or_small_world(str_arg)
+    print('say_day_of_life')
+
+def case_or_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('high_number')
