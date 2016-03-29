ab9caefd04c28a081c8ce18e018c82c096ad7953
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 11:06:47 -0400

    "rm: think super/child_and_fact/point/child_and_little_eye.py, super/work.py or super/man/other_year/get_company_under_hand/old_part/group.py above high part"

---
+++ super/child_and_fact/point/child_and_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_fact_over_different_fact(str_arg):
+    good_problem(str_arg)
+    print('number_or_last_work')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_fact_over_different_fact('look_place')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company(str_arg):
+    old_thing(str_arg)
+    print('different_life')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company('case')
