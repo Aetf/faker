6b0fd9ef4200f79064b66c91437816ff7a532888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 14:43:18 -0400

    "add: try super/government/young_woman.py and super/hand_and_point.py on public week"

---
+++ super/government/young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_and_time(str_arg):
+    person(str_arg)
+    print('woman')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_and_time('seem_problem_about_part')
