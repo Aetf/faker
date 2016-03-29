3dd8063910324f1228ef1a32dc9a32e12a2a185a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 12:18:09 -0400

    "add: come super/bad_part/thing_or_problem.py, super/day_or_right_week.py or super/child/time/point/small_number_or_long_year.py to old day"

---
+++ super/child/time/point/small_number_or_long_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    good_day(str_arg)
+    print('first_child')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('tell_long_world')
---
+++ super/day_or_right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_first_year(str_arg):
+    big_place(str_arg)
+    print('day')
+
+def big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_first_year('world')
