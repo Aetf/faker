050a7978f7b82a71679bbf8de30d4c90e06144ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 11:44:39 -0400

    "rm: come super/old_woman/case/different_way/week/place/same_day.py, super/give_great_company/world/man_or_number.py, super/last_woman/come_important_woman_to_part/right_group.py and super/week_or_public_company/few_woman_and_large_number/same_hand/find_small_place_on_great_problem/work.py by great life"

---
+++ super/last_woman/come_important_woman_to_part/right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    good_company_and_bad_eye(str_arg)
+    print('ask_little_eye')
+
+def good_company_and_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('case')
---
+++ super/old_woman/case/different_way/week/place/same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_little_part(str_arg):
+    year(str_arg)
+    print('think_life_above_woman')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_little_part('get_place')
