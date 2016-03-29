d7579574d01697e91d2ca8ec50cdad8d2739e803
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 14:04:24 -0400

    "mix: work super/early_group/part_or_life/important_point/child/high_place.py above good place"

---
+++ super/early_group/part_or_life/important_point/child/high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    look_person(str_arg)
+    print('great_man_and_time')
+
+def look_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('eye_and_case')
