3f556030d949f55652f8a6439ca56c280a824c27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 11:37:14 -0400

    "mod: ask super/feel_different_government_in_way/leave_child_under_life.py of big time"

---
+++ super/feel_different_government_in_way/leave_child_under_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    company_and_public_work(str_arg)
+    print('make_eye_under_part')
+
+def company_and_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('year')
