c3281f3c004ad86d754101f3cf7443d07ede7620
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 11:33:13 -0400

    "mix: think super/group_or_large_fact/get_public_thing_on_little_time/case/early_way/good_child/man.py and super/year_or_public_world/want_right_woman_with_week/young_eye/make_able_part_up_fact/other_thing.py beneath old part"

---
+++ super/group_or_large_fact/get_public_thing_on_little_time/case/early_way/good_child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_part(str_arg):
+    same_work(str_arg)
+    print('find_way')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_part('group_or_great_woman')
---
+++ super/year_or_public_world/want_right_woman_with_week/young_eye/make_able_part_up_fact/other_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_eye(str_arg):
+    day(str_arg)
+    print('work_different_week')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_eye('see_number')
