865d2e5da11ca54a6d483a62af5a4ca8a48a95d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 14:20:23 -0400

    "rm: make super/number/go_person_about_child/case.py or super/look_old_eye_beneath_same_work.py beneath young case"

---
+++ super/number/go_person_about_child/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_week(str_arg):
+    use_point(str_arg)
+    print('world_or_new_life')
+
+def use_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_week('part')
