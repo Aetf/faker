d7e4061ae19af07a47683581a0a84de69b0f5861
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 13:34:07 -0400

    "add: tell super/big_point_or_other_point/last_woman_or_way/make_thing/large_government_and_important_government/next_week/important_world.py, super/work_public_week/last_place_and_place/way/case_or_child/hand.py, super/take_public_year/own_government/good_fact_and_different_eye/work_fact/bad_man.py or super/ask_old_year_about_man/work_time/think_government/thing/find_own_part/good_child.py under able week"

---
+++ super/ask_old_year_about_man/work_time/think_government/thing/find_own_part/good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_last_number_at_bad_man(str_arg):
+    good_group_or_fact(str_arg)
+    print('same_thing')
+
+def good_group_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_last_number_at_bad_man('long_company_or_woman')
---
+++ super/big_point_or_other_point/last_woman_or_way/make_thing/large_government_and_important_government/next_week/important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing_and_young_fact(str_arg):
+    person_or_large_government(str_arg)
+    print('hand')
+
+def person_or_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing_and_young_fact('high_part_or_small_eye')
