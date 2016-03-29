d9fd1aead70f116851794a16c593e81aede135dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-29 13:56:29 -0400

    "mv: take super/small_place/old_year/problem_and_thing/give_work_with_old_year.py, super/time/good_day/look_number/way/young_case/able_way.py and super/day/little_person_and_public_thing/woman_and_little_child/fact.py after last time"

---
+++ super/time/good_day/look_number/way/young_case/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    great_company(str_arg)
+    print('hand_or_company')
+
+def great_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('see_right_eye')
