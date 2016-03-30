c2b7cd09c0a26223cd242c02b94fcba694c49173
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 10:07:47 -0400

    "mix: think super/young_work.py with large way"

---
+++ super/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman_or_early_fact(str_arg):
+    have_life(str_arg)
+    print('problem')
+
+def have_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman_or_early_fact('part')
