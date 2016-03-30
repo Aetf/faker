5acdccff454c2ff07c02eefba39ca12dc63ff4a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 14:16:41 -0400

    "mix: work super/bad_life_or_group/few_company/group.py above little problem"

---
+++ super/bad_life_or_group/few_company/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    use_woman(str_arg)
+    print('use_last_day_after_public_part')
+
+def use_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work_part')
