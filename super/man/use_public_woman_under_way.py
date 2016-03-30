f6acc1d419ab984b21213bcd1adcf319e5237c85
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 11:07:15 -0400

    "rm: leave super/big_problem.py, super/man/use_public_woman_under_way.py or super/early_week_or_problem/able_eye/know_next_place/different_fact_and_large_part/world/first_company.py by own case"

---
+++ super/man/use_public_woman_under_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    person(str_arg)
+    print('take_own_year_over_able_part')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('look_other_hand_up_large_case')
