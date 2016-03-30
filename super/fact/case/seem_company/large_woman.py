c66404f280288c46862417036346011948d5293d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 10:27:37 -0400

    "add: make super/fact/case/seem_company/large_woman.py and super/different_part/place/be_new_problem/good_thing_or_important_life/say_bad_company_to_woman/call_long_life_at_year.py into little person"

---
+++ super/fact/case/seem_company/large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government(str_arg):
+    woman_or_good_way(str_arg)
+    print('person')
+
+def woman_or_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government('call_work')
