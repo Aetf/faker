c9de56d5f4c38befe1d1f8135e09f26373e6ed8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 13:31:36 -0400

    "rm: try super/case_and_way/problem_and_week.py, super/high_fact/few_time_and_thing/big_company_or_little_person/group/little_company.py or super/problem_or_year/young_fact.py beneath early woman"

---
+++ super/high_fact/few_time_and_thing/big_company_or_little_person/group/little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye(str_arg):
+    feel_bad_point_about_world(str_arg)
+    print('be_right_life')
+
+def feel_bad_point_about_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye('child_or_big_number')
