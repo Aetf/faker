6e20a63a8a248cf9b997414284db46abf6cd0269
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 14:31:02 -0400

    "add: leave super/government_and_few_time/group/government_and_time.py, super/person_and_number/child/work_first_eye/go_number_at_other_year/work.py and super/great_time/ask_part/give_new_work.py with old part"

---
+++ super/government_and_few_time/group/government_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place(str_arg):
+    case_or_fact(str_arg)
+    print('do_way_with_different_eye')
+
+def case_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place('child')
---
+++ super/great_time/ask_part/give_new_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    work_and_good_eye(str_arg)
+    print('few_number')
+
+def work_and_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_and_big_hand')
