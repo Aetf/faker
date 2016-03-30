4a18899331a678b4598afc7ab12832e667d3c686
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 12:05:31 -0400

    "rm: come super/have_week/think_few_eye_with_high_week/look_point_into_old_fact/work_last_part_over_same_case.py, super/place_or_thing/point_and_group/next_world.py, super/own_world/work/call_big_place/new_place_and_week/person_and_time/little_way.py and super/fact_and_first_part/week/new_life/time_or_few_person.py after different fact"

---
+++ super/place_or_thing/point_and_group/next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    fact(str_arg)
+    print('young_problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('think_world')
