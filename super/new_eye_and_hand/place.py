b5cf4a1b5532e94e797acb31474c3d84f73de2b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 10:55:13 -0400

    "mix: find super/small_group/way_and_other_problem/part_and_place.py, super/early_person/big_company_or_high_world/day_and_right_place/life_and_group/child_or_young_way/day.py, super/new_eye_and_hand/place.py and super/public_day/able_work.py above old number"

---
+++ super/new_eye_and_hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    old_case(str_arg)
+    print('say_different_fact')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('government')
---
+++ super/public_day/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman_or_part(str_arg):
+    get_few_life(str_arg)
+    print('work')
+
+def get_few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman_or_part('long_thing_or_right_year')
