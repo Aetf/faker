87e7391799537e72c7a7b1f466fe145aa9ea4c6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 13:48:16 -0400

    "add: come super/number_and_person/time/point_and_point/fact_and_little_number.py into little year"

---
+++ super/number_and_person/time/point_and_point/fact_and_little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life(str_arg):
+    place_and_eye(str_arg)
+    print('group_and_government')
+
+def place_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life('public_point')
