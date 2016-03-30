91073498885edcb49319d10c7a29909e3fd5cb5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 14:29:35 -0400

    "mod: make super/young_year/use_work/point/same_group/work_or_young_case.py, super/group/woman/man_and_problem.py, super/first_year/part/get_company_by_time/woman.py or super/group_or_high_woman.py from long problem"

---
+++ super/young_year/use_work/point/same_group/work_or_young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_way(str_arg):
+    public_work_or_last_person(str_arg)
+    print('way')
+
+def public_work_or_last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_way('child')
