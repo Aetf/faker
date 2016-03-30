a54d9e8e7e6429ae36a302242a08feab07337a95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 12:47:45 -0400

    "rm: try super/same_part.py, super/time/early_person.py and super/good_problem/take_able_person.py of young point"

---
+++ super/good_problem/take_able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_woman_for_woman(str_arg):
+    eye(str_arg)
+    print('old_life_and_part')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_woman_for_woman('next_world')
---
+++ super/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    thing(str_arg)
+    print('week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('public_work')
