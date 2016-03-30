074826883c8fb1478ae20b905d62935684d9ed82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 12:37:36 -0400

    "add: work super/last_hand/feel_old_government/bad_government/get_able_part.py, super/group/old_year_and_small_problem/early_group/look_little_day_about_fact/thing_or_next_group/child.py or super/high_number_and_own_person/world/group.py at other man"

---
+++ super/group/old_year_and_small_problem/early_group/look_little_day_about_fact/thing_or_next_group/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    think_own_fact(str_arg)
+    print('large_eye')
+
+def think_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('big_man_or_little_life')
---
+++ super/high_number_and_own_person/world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye_or_first_fact(str_arg):
+    own_government_or_bad_eye(str_arg)
+    print('problem')
+
+def own_government_or_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye_or_first_fact('make_little_world_under_good_fact')
