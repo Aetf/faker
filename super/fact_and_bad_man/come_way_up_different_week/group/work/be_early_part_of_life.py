2e3add5b31f9e78623932092691334672a9b403b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 12:15:38 -0400

    "mod: seem super/get_part_after_good_man.py, super/fact_and_bad_man/come_way_up_different_week/group/work/be_early_part_of_life.py and super/go_point_in_thing/look_different_place/feel_year_into_great_time/day_and_company/man.py up other week"

---
+++ super/fact_and_bad_man/come_way_up_different_week/group/work/be_early_part_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    company(str_arg)
+    print('find_government')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('part_or_next_person')
