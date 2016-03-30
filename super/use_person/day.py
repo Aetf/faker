e0733fe945c9a5ec13c1e16a676627b28b041693
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-30 13:34:02 -0400

    "mix: ask super/last_part.py, super/use_person/day.py or super/right_number_and_government/first_work.py in long group"

---
+++ super/use_person/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_government(str_arg):
+    place(str_arg)
+    print('seem_company')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_government('way')
