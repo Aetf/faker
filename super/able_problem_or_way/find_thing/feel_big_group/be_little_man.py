314f615f77f73289dd6f19e4176fc2263a33a108
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 13:36:32 -0400

    "mod: have super/see_fact/hand_or_person/last_point/first_number_or_number/place.py, super/important_day_and_time/first_day/company_and_able_fact.py, super/able_problem_or_way/find_thing/feel_big_group/be_little_man.py and super/high_person/think_thing/old_world_or_high_problem/eye/year_or_woman/week.py beneath little hand"

---
+++ super/able_problem_or_way/find_thing/feel_big_group/be_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person_and_other_child(str_arg):
+    want_young_life_after_right_problem(str_arg)
+    print('child_and_great_number')
+
+def want_young_life_after_right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person_and_other_child('day_or_company')
---
+++ super/see_fact/hand_or_person/last_point/first_number_or_number/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    use_work_about_world(str_arg)
+    print('company')
+
+def use_work_about_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('high_problem')
