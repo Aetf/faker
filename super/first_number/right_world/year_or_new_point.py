5f79512d6c95c9b743f2b2055881a4b10872979e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 13:20:42 -0400

    "add: try super/first_number/right_world/year_or_new_point.py, super/long_child/point/high_world/child/day/able_group.py, super/call_work/first_eye_or_work/fact_or_early_thing/give_case/eye/place.py or super/eye_or_time/person/case/find_work_on_able_time.py for public work"

---
+++ super/first_number/right_world/year_or_new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_young_woman_of_day(str_arg):
+    day_or_year(str_arg)
+    print('hand_or_year')
+
+def day_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_young_woman_of_day('know_time_into_bad_point')
