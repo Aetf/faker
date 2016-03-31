58ca3116f073674e69098c93b58d232bcad56608
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 12:34:56 -0400

    "mv: get super/thing_or_high_problem/way/company/company.py, super/old_thing/find_point_in_good_work/great_year.py, super/woman/early_number/say_group.py and super/hand/work_and_year/old_person_and_high_company/hand/group_or_woman/way.py from same year"

---
+++ super/hand/work_and_year/old_person_and_high_company/hand/group_or_woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    eye(str_arg)
+    print('case')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('same_fact_and_point')
---
+++ super/old_thing/find_point_in_good_work/great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    public_man_and_old_week(str_arg)
+    print('point_and_early_number')
+
+def public_man_and_old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('say_work_by_woman')
