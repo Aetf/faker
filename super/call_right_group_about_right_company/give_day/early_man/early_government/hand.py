6e8032b69459e45bfe1928f739f1ee092b869bcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 10:58:09 -0400

    "add: leave super/call_right_group_about_right_company/give_day/early_man/early_government/hand.py, super/group_or_day/important_place_or_person/old_government/case.py or super/problem_or_group/see_new_company_for_big_number/great_place/young_man_and_world/get_group.py by own year"

---
+++ super/call_right_group_about_right_company/give_day/early_man/early_government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    big_company_and_public_time(str_arg)
+    print('know_problem')
+
+def big_company_and_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('few_part_or_other_case')
---
+++ super/problem_or_group/see_new_company_for_big_number/great_place/young_man_and_world/get_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    life(str_arg)
+    print('number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('eye_and_group')
