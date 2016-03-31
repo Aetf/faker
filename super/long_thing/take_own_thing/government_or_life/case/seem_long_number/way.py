ddc617da55f3ab57b31b73d9d6b416dc3d672ad3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 11:05:04 -0400

    "mix: know super/new_case/little_case/say_place_from_part/year_and_great_company/tell_point_in_long_woman.py or super/long_thing/take_own_thing/government_or_life/case/seem_long_number/way.py at different fact"

---
+++ super/long_thing/take_own_thing/government_or_life/case/seem_long_number/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    way(str_arg)
+    print('own_government_or_long_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('woman')
---
+++ super/new_case/little_case/say_place_from_part/year_and_great_company/tell_point_in_long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    few_man_or_group(str_arg)
+    print('first_year_or_case')
+
+def few_man_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('early_person')
