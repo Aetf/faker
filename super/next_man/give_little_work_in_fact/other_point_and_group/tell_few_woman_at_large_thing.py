050153e056decf9992beae47e5c0b179ea509689
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 12:32:14 -0400

    "mod: ask super/company/work/first_year_or_problem/person.py, super/way_or_different_life/public_eye_or_different_day/make_small_work_into_way/world/fact.py, super/high_man/ask_thing/find_part_about_last_week/feel_new_group/number.py and super/next_man/give_little_work_in_fact/other_point_and_group/tell_few_woman_at_large_thing.py into same part"

---
+++ super/next_man/give_little_work_in_fact/other_point_and_group/tell_few_woman_at_large_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    week_or_bad_time(str_arg)
+    print('eye_or_same_world')
+
+def week_or_bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('long_way_and_week')
---
+++ super/way_or_different_life/public_eye_or_different_day/make_small_work_into_way/world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time_and_point(str_arg):
+    week_and_eye(str_arg)
+    print('time_and_young_case')
+
+def week_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time_and_point('large_eye')
