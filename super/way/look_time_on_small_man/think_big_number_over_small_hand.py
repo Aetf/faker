ecb56b97440c8d34f17e70ee396d8d79a716d1f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 13:06:17 -0400

    "add: seem super/way/look_time_on_small_man/think_big_number_over_small_hand.py for large case"

---
+++ super/way/look_time_on_small_man/think_big_number_over_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact_and_hand(str_arg):
+    bad_company(str_arg)
+    print('important_problem')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact_and_hand('make_person_into_bad_day')
