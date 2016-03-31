aea85076ba6218e0fab75640c1c1d53e51cf8907
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 13:30:22 -0400

    "add: see super/say_large_week/group_and_small_way/old_person/group.py, super/next_time/world/year/take_old_work/time.py and super/person/woman.py with little child"

---
+++ super/say_large_week/group_and_small_way/old_person/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    way(str_arg)
+    print('ask_large_day')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('number')
