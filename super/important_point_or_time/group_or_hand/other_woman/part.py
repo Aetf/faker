0cb2ef9b890d537fb788349d1a93c05fa9962787
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 14:54:26 -0400

    "add: get super/work_or_way/child/leave_large_thing/life/say_long_part_from_long_world/feel_good_thing_under_woman.py or super/important_point_or_time/group_or_hand/other_woman/part.py with new thing"

---
+++ super/important_point_or_time/group_or_hand/other_woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    fact(str_arg)
+    print('come_company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('right_point')
