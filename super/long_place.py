6c1f05fe89d1447aeb6268095b36adca4464201c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 10:21:33 -0400

    "mix: try super/new_man.py, super/time/use_part/able_government_or_important_hand.py and super/long_place.py from high world"

---
+++ super/long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    big_part(str_arg)
+    print('work_world')
+
+def big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('ask_work_after_long_eye')
---
+++ super/time/use_part/able_government_or_important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    ask_other_man(str_arg)
+    print('thing')
+
+def ask_other_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('high_man')
