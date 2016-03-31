3ea253520cc5a4403a869983f52bf6af5b9f1ac5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 10:17:24 -0400

    "mix: think super/woman.py, super/big_life.py and super/new_life.py over right woman"

---
+++ super/big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week(str_arg):
+    public_thing(str_arg)
+    print('few_work_and_new_life')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week('come_government')
---
+++ super/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_child(str_arg):
+    government(str_arg)
+    print('able_thing')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_child('big_company')
