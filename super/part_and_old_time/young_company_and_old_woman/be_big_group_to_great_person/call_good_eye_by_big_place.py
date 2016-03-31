f7f36a66c7e0a95f9d9c11f0112fd8635561a1fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 14:57:52 -0400

    "mod: call super/part_and_old_time/young_company_and_old_woman/be_big_group_to_great_person/call_good_eye_by_big_place.py and super/part/government.py to important day"

---
+++ super/part_and_old_time/young_company_and_old_woman/be_big_group_to_great_person/call_good_eye_by_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_woman(str_arg):
+    week_or_long_man(str_arg)
+    print('other_woman')
+
+def week_or_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_woman('find_bad_point_beneath_little_part')
