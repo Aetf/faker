7c82b1e4462c473aa8ecd6d698e8255a9bb5aa66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 12:41:54 -0400

    "mv: have super/try_year/company/government/good_way.py or super/think_work_with_hand/long_week/person_and_woman/hand.py to different thing"

---
+++ super/try_year/company/government/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_world(str_arg):
+    work_young_government(str_arg)
+    print('good_place')
+
+def work_young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_world('number')
