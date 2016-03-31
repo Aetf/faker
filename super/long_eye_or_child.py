226a764e652e1f9092a77e519f9ef8e7c8440a40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 14:26:31 -0400

    "mod: seem super/day_or_thing/feel_public_day_for_early_thing/time_or_old_life/life_and_life.py or super/long_eye_or_child.py with new way"

---
+++ super/long_eye_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point(str_arg):
+    go_woman_on_group(str_arg)
+    print('able_child')
+
+def go_woman_on_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point('different_life')
