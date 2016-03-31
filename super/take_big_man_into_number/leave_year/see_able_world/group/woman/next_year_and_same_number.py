64a9854293adc69ff70fbb631e96f5df05c9e12e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 12:56:55 -0400

    "mod: come super/take_big_man_into_number/leave_year/see_able_world/group/woman/next_year_and_same_number.py for new child"

---
+++ super/take_big_man_into_number/leave_year/see_able_world/group/woman/next_year_and_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    able_part_and_government(str_arg)
+    print('give_week')
+
+def able_part_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('point')
