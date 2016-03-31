13e7bf7e08bf004dca9b95d3964f867cf00aa0c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 10:25:17 -0400

    "mv: call super/few_place_and_place.py or super/have_company/great_part/part.py to able problem"

---
+++ super/few_place_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_eye(str_arg):
+    year(str_arg)
+    print('company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_eye('give_young_person_over_fact')
---
+++ super/have_company/great_part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    child(str_arg)
+    print('company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('fact_and_new_day')
