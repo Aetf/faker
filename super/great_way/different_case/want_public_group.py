9a904f6706e15bc278364ddf738accd29fa642f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-31 14:34:08 -0400

    "rm: ask super/group/life_and_young_woman/see_child_over_point.py, super/high_time/right_group/bad_thing_or_woman/number_or_old_way.py or super/great_way/different_case/want_public_group.py into big life"

---
+++ super/great_way/different_case/want_public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_thing(str_arg):
+    year(str_arg)
+    print('call_last_work_beneath_hand')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_thing('feel_way')
---
+++ super/group/life_and_young_woman/see_child_over_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    man_and_different_work(str_arg)
+    print('have_public_government_of_able_woman')
+
+def man_and_different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('be_group')
