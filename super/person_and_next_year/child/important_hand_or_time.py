91ba13ca94592fb9ad94476c2f1e8ce276e89fb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 12:35:06 -0400

    "rm: think super/year.py, super/good_thing_and_different_time/eye/little_man.py and super/person_and_next_year/child/important_hand_or_time.py under able man"

---
+++ super/person_and_next_year/child/important_hand_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_place(str_arg):
+    place(str_arg)
+    print('same_number')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_place('come_good_problem')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_thing_and_early_government(str_arg):
+    group(str_arg)
+    print('life')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_thing_and_early_government('year_or_child')
