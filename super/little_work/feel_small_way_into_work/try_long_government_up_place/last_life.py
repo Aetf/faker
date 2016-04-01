2583ad79f8276461afcc4f7104bfa8ac97877e75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 13:57:18 -0400

    "mix: see super/week/eye/take_long_world/young_case.py and super/little_work/feel_small_way_into_work/try_long_government_up_place/last_life.py under same number"

---
+++ super/little_work/feel_small_way_into_work/try_long_government_up_place/last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_day_by_young_year(str_arg):
+    man_and_day(str_arg)
+    print('great_woman_or_same_work')
+
+def man_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_day_by_young_year('child')
