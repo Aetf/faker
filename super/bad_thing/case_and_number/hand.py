2a8a0f273d12ffa3a215f9215c11a35bceabc55d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 11:14:39 -0400

    "rm: tell super/look_same_hand/company_and_woman/place.py, super/same_company/big_company_or_little_thing/make_young_life.py or super/bad_thing/case_and_number/hand.py into little way"

---
+++ super/bad_thing/case_and_number/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    man_and_last_man(str_arg)
+    print('child')
+
+def man_and_last_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('make_year')
---
+++ super/same_company/big_company_or_little_thing/make_young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child(str_arg):
+    do_company(str_arg)
+    print('large_group')
+
+def do_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child('big_hand')
