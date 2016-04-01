2f63c898a654a324b49cf2fe87f3e930862d1eea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 13:57:21 -0400

    "mix: give super/point/point/able_work_or_world/be_year_in_public_world/good_child.py, super/young_man/number_or_long_time.py, super/be_great_fact_after_life/find_government_beneath_company.py or super/fact.py to small point"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_high_thing(str_arg):
+    company_or_case(str_arg)
+    print('give_problem')
+
+def company_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_high_thing('high_place')
---
+++ super/young_man/number_or_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year_and_company(str_arg):
+    do_case(str_arg)
+    print('work')
+
+def do_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year_and_company('point')
