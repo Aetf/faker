e52d59775420079bd22a5cf05a790e6381da3ba1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 11:01:49 -0400

    "rm: try super/last_world_and_public_world/try_year_to_woman/way/thing_or_problem/group.py and super/important_year_or_thing/big_year/own_part_and_point/able_year.py of bad thing"

---
+++ super/important_year_or_thing/big_year/own_part_and_point/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    hand(str_arg)
+    print('use_man_above_old_person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('thing')
