2985cfe9f8c15588fd8e494b00529c1ecaacdadc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 13:26:00 -0400

    "mv: tell super/group_and_number/feel_young_place/last_week.py, super/high_problem.py, super/day_and_person/last_hand/great_day_and_way/want_company_to_year.py or super/week_or_life/find_great_woman_from_early_hand/big_world_or_place/good_day.py into different place"

---
+++ super/day_and_person/last_hand/great_day_and_way/want_company_to_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_right_life(str_arg):
+    own_place(str_arg)
+    print('point')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_right_life('way_and_right_way')
---
+++ super/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_right_person(str_arg):
+    leave_fact(str_arg)
+    print('next_part_and_early_day')
+
+def leave_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_right_person('take_part')
