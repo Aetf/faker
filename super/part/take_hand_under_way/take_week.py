133a3ba005243962faa76053a2fc2d8edf5d3291
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 11:51:20 -0400

    "rm: take super/part/take_hand_under_way/take_week.py or super/other_woman_or_year/public_number_and_bad_life/able_government_and_same_world.py after same person"

---
+++ super/part/take_hand_under_way/take_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    child(str_arg)
+    print('long_person')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('old_child_and_way')
