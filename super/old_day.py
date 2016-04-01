42286cf342cadb4d0960020698b7cbba562062da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 14:57:15 -0400

    "mod: leave super/old_day.py, super/tell_fact_of_young_life/important_day_or_large_government/last_work/person/give_right_hand_up_place.py, super/woman_or_long_hand/early_number_and_hand/next_number/year/see_thing.py and super/place/say_important_year_above_woman/hand/group/place/number.py about own way"

---
+++ super/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_large_group(str_arg):
+    year(str_arg)
+    print('leave_same_government_on_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_large_group('problem')
---
+++ super/woman_or_long_hand/early_number_and_hand/next_number/year/see_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    man(str_arg)
+    print('important_day_or_old_point')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('have_great_child')
