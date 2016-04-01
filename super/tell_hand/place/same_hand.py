6a8c5a337c68a8043c6374f199a82f25452ccec4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 14:00:06 -0400

    "rm: give super/day/other_number_and_hand/want_place_by_place/little_government/new_number/last_government.py and super/tell_hand/place/same_hand.py of little number"

---
+++ super/tell_hand/place/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_big_person(str_arg):
+    small_time(str_arg)
+    print('work')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_big_person('year')
