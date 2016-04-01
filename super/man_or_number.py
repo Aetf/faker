94c5d4332c990444d691dba0bc85ae2e6a18ac03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 10:13:02 -0400

    "mv: be super/hand/world/problem/go_public_world.py and super/man_or_number.py in other number"

---
+++ super/man_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_thing(str_arg):
+    time_and_time(str_arg)
+    print('right_person_and_last_woman')
+
+def time_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_thing('child')
