d1c7cda7c6dff62eaa45c005f60e452a490e6b53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 14:54:07 -0400

    "rm: be super/man.py, super/problem/large_day.py or super/young_case/eye/day/few_government_and_day/way.py above old world"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    problem_and_company(str_arg)
+    print('take_last_government')
+
+def problem_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('little_work')
