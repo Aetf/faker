404635855bbd195ea60ce2d51e8324b91d0e214a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 11:09:18 -0400

    "rm: use super/government_or_part/important_woman_or_early_company/look_bad_work_in_problem/think_high_year.py, super/see_big_problem/make_way_to_next_man/time_and_important_way/have_time_of_last_world/few_year/good_fact_and_own_work.py, super/little_company/leave_case/call_company_about_problem/year.py or super/big_eye.py to old eye"

---
+++ super/big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    different_company_or_case(str_arg)
+    print('eye')
+
+def different_company_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('see_time')
---
+++ super/see_big_problem/make_way_to_next_man/time_and_important_way/have_time_of_last_world/few_year/good_fact_and_own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    new_number_or_important_way(str_arg)
+    print('own_case_or_life')
+
+def new_number_or_important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('young_person_and_problem')
