e17c5466eecc450ad08f7982cedd30a090de8ef8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 10:06:12 -0400

    "mv: think super/place_and_early_day/same_week/other_time.py, super/small_case/part.py or super/want_long_government.py after little time"

---
+++ super/place_and_early_day/same_week/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man_or_point(str_arg):
+    last_thing_or_hand(str_arg)
+    print('work')
+
+def last_thing_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man_or_point('old_year')
---
+++ super/want_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    other_place_or_fact(str_arg)
+    print('life_or_good_government')
+
+def other_place_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('young_point')
