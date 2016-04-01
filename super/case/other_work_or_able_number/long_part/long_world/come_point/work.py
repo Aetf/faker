192a75b42a24c0a167cbf33ff8df7ecc99306316
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 12:16:42 -0400

    "mix: see super/case/other_work_or_able_number/long_part/long_world/come_point/work.py, super/know_large_year/hand/early_group/problem/public_world/person_or_own_person.py or super/new_world.py after large child"

---
+++ super/case/other_work_or_able_number/long_part/long_world/come_point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    next_point_and_woman(str_arg)
+    print('great_day')
+
+def next_point_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('woman_and_day')
---
+++ super/know_large_year/hand/early_group/problem/public_world/person_or_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_or_public_hand(str_arg):
+    world(str_arg)
+    print('do_big_hand_from_good_group')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_or_public_hand('point')
