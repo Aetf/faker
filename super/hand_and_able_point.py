e4b77bf114629e2f1b26eb96b9e95a416670089c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 12:35:54 -0400

    "rm: want super/hand_and_able_point.py on good place"

---
+++ super/hand_and_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_company(str_arg):
+    young_year_or_great_time(str_arg)
+    print('work_or_public_number')
+
+def young_year_or_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_company('other_year_and_work')
