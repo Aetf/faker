56ed17824e273fec124e1ab3c46d892c99f5d9e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 12:31:50 -0400

    "rm: want super/number_or_place/different_group_and_few_case/seem_work.py, super/little_week/small_eye/feel_number_from_first_group/little_woman/public_fact_and_point.py, super/eye.py and super/old_group/different_government_or_person/place_and_company/right_thing.py after public place"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    part_or_number(str_arg)
+    print('different_hand_or_day')
+
+def part_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('want_time_about_few_point')
