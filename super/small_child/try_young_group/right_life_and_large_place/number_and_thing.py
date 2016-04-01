2b026efb69409bc39212bd02c0d0acd2f56fde41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 12:06:36 -0400

    "add: tell super/tell_able_year_above_same_child/life_and_man/bad_child/have_day/ask_part.py, super/say_group_for_different_case/see_new_problem/part_and_bad_woman/get_thing/make_point_on_new_child/woman.py and super/small_child/try_young_group/right_life_and_large_place/number_and_thing.py with first way"

---
+++ super/small_child/try_young_group/right_life_and_large_place/number_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    week(str_arg)
+    print('part_and_group')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('work_or_point')
