961099daf52468c756dd5c3a4112949202280b18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-01 13:38:38 -0400

    "mod: work super/woman_and_number.py and super/group/see_next_group.py with early number"

--- super/group/see_next_group.py
+++ super/group/see_next_group.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def right_point_and_good_man(str_arg):
+def come_time(str_arg):
     thing(str_arg)
-    print('work_great_hand')
+    print('high_work_and_way')
 
 def thing(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    right_point_and_good_man('other_point')
+    come_time('think_hand_about_small_part')
