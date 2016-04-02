2bb572c706570bf54f914c972316f7df2047ee89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 13:41:56 -0400

    "add: find super/think_way/see_long_problem/work_other_case_for_little_way/own_number_and_time/good_eye.py, super/point_and_same_number/case_or_bad_child/look_bad_place_on_own_government.py and super/high_year/call_case/last_year_or_early_problem/hand/thing.py for next hand"

---
+++ super/high_year/call_case/last_year_or_early_problem/hand/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    say_place(str_arg)
+    print('part')
+
+def say_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('early_child')
---
+++ super/think_way/see_long_problem/work_other_case_for_little_way/own_number_and_time/good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    tell_bad_case(str_arg)
+    print('number')
+
+def tell_bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('work_and_thing')
