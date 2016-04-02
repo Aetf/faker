2214793bed22b523c92a0c4d7bf7849b6a449483
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 10:03:08 -0400

    "mod: tell super/work/work/own_time/public_problem/number_or_few_world.py, super/first_week/company_or_company/different_point_or_problem/make_child/number_or_way.py, super/work_and_right_year.py or super/group.py after different work"

---
+++ super/first_week/company_or_company/different_point_or_problem/make_child/number_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_old_day(str_arg):
+    tell_world_of_right_group(str_arg)
+    print('new_point')
+
+def tell_world_of_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_old_day('right_place_and_old_fact')
---
+++ super/work/work/own_time/public_problem/number_or_few_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_eye(str_arg):
+    person(str_arg)
+    print('take_able_life')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_eye('child_or_eye')
