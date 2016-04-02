13bde20b208dc0dcc029c6d3ddf5f19c9e9983bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 13:44:45 -0400

    "mod: do super/right_life/long_number_and_new_number/man/good_problem_or_large_problem/part/get_number_over_important_child.py, super/hand/group/leave_way_for_part/tell_day/life/few_fact.py or super/able_problem/world/problem/do_own_number.py at first child"

---
+++ super/right_life/long_number_and_new_number/man/good_problem_or_large_problem/part/get_number_over_important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_time(str_arg):
+    week(str_arg)
+    print('old_woman')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_time('work_or_thing')
