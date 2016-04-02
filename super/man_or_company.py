043711f577ea03e50a0a04438357ddaeff0036b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 11:05:56 -0400

    "mv: try super/small_company/young_company_and_other_problem/make_government/work_large_group.py, super/government/last_problem/few_world_and_person/thing/ask_early_world_on_great_life/case_and_first_life.py, super/man_or_company.py and super/thing/day/person/company/find_public_government_on_old_way.py over old eye"

---
+++ super/man_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day(str_arg):
+    man(str_arg)
+    print('take_child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day('call_last_day_by_big_problem')
---
+++ super/small_company/young_company_and_other_problem/make_government/work_large_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing_and_eye(str_arg):
+    find_new_hand_after_world(str_arg)
+    print('man')
+
+def find_new_hand_after_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing_and_eye('want_different_time_by_woman')
