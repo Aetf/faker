b911817c7a2becd5e6451f2560777ce9028272a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 13:10:05 -0400

    "mod: tell super/man/company_or_government/go_next_child_at_long_group/have_child_with_fact.py, super/big_world/tell_work_about_other_case/first_thing.py or super/year/year/child/long_child.py from important part"

---
+++ super/year/year/child/long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_important_work(str_arg):
+    do_week_after_own_part(str_arg)
+    print('other_problem')
+
+def do_week_after_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_important_work('early_child')
