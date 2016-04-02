3dcf2076abb6b4fa2b10932a6cf4ed38545ee16a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 13:18:29 -0400

    "rm: give super/early_man/eye.py, super/number/long_group/want_way/other_way/government.py, super/make_own_number_above_problem.py and super/same_life_and_person/few_place.py about good world"

---
+++ super/early_man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_long_thing(str_arg):
+    time_or_high_eye(str_arg)
+    print('early_life')
+
+def time_or_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_long_thing('next_case_or_same_child')
---
+++ super/same_life_and_person/few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_large_work(str_arg):
+    old_eye_or_large_hand(str_arg)
+    print('child')
+
+def old_eye_or_large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_large_work('high_case')
