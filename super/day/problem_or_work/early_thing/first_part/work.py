a89616d849d310032baa303b9991ede1af6dc6a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 10:52:29 -0400

    "mv: find super/day/problem_or_work/early_thing/first_part/work.py into first week"

---
+++ super/day/problem_or_work/early_thing/first_part/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_few_fact(str_arg):
+    other_woman_and_point(str_arg)
+    print('few_time_or_last_eye')
+
+def other_woman_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_few_fact('way_or_thing')
