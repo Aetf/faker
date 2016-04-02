5b004ba08253db6b9e6e1baf631af9c3b87ede01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 12:28:39 -0400

    "mv: find super/go_first_year_about_point/make_new_government.py, super/long_day/important_day/work_year/thing/see_other_group_beneath_way.py and super/go_thing/get_little_world_into_last_person/group_or_early_way/public_group/world.py after new child"

---
+++ super/go_thing/get_little_world_into_last_person/group_or_early_way/public_group/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_case(str_arg)
+    print('long_eye')
+
+def young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('call_point_up_year')
---
+++ super/long_day/important_day/work_year/thing/see_other_group_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time_and_able_year(str_arg):
+    group_or_small_eye(str_arg)
+    print('year_or_old_case')
+
+def group_or_small_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time_and_able_year('large_group')
