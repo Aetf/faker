05eceaba7affb01d7c7922cfddc3c72643c0f7cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:01:16 -0400

    "mod: see super/say_company_of_next_man/world_or_important_eye/problem.py and super/fact/same_life/eye.py of same part"

---
+++ super/say_company_of_next_man/world_or_important_eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_world(str_arg):
+    give_great_place(str_arg)
+    print('other_life')
+
+def give_great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_world('last_child_or_woman')
