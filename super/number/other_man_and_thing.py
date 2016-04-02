e754accc17831f035db9c9a5a23a32768cc2f271
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 12:18:36 -0400

    "mod: find super/other_year/number_and_high_case/old_way/woman/small_year/other_point_or_old_number.py, super/young_part_or_good_way/other_way_and_big_hand/woman_and_eye.py or super/number/other_man_and_thing.py above long government"

---
+++ super/number/other_man_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time_or_important_work(str_arg):
+    tell_different_man(str_arg)
+    print('say_first_hand')
+
+def tell_different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time_or_important_work('leave_government')
---
+++ super/other_year/number_and_high_case/old_way/woman/small_year/other_point_or_old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    come_place(str_arg)
+    print('same_life')
+
+def come_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('number')
