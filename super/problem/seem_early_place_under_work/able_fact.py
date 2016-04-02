97886722d534a03c062e871f5e84f3c1c79c56f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 10:35:07 -0400

    "mod: try super/little_fact_or_new_year/year_and_large_year/do_man_into_own_problem/way.py and super/problem/seem_early_place_under_work/able_fact.py after different woman"

---
+++ super/problem/seem_early_place_under_work/able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_fact_above_long_thing(str_arg):
+    be_week_at_bad_time(str_arg)
+    print('life')
+
+def be_week_at_bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_fact_above_long_thing('give_different_fact')
