a0c1270b52c8175e67d5f685c14f06ae36590d22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:42:19 -0400

    "rm: see super/give_year_for_way/new_government_and_hand/get_work_over_same_case/year_and_hand.py about big work"

---
+++ super/give_year_for_way/new_government_and_hand/get_work_over_same_case/year_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    number_and_life(str_arg)
+    print('large_life_and_high_person')
+
+def number_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('come_child_in_long_number')
