48913517e5af871dcdd67ec19bf362e7d29d6342
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 11:25:48 -0400

    "rm: do super/tell_man/new_case/good_fact/try_child/have_child_on_part/woman.py, super/great_child_and_high_hand/way.py and super/great_group/bad_place_or_government/man/way_and_group.py over young group"

---
+++ super/great_group/bad_place_or_government/man/way_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    come_early_person(str_arg)
+    print('child_and_time')
+
+def come_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('new_government')
---
+++ super/tell_man/new_case/good_fact/try_child/have_child_on_part/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    way(str_arg)
+    print('have_good_fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('same_man')
