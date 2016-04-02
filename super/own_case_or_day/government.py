1b27bd338962646340a0276f37aa9d2f49ab4fa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:32:23 -0400

    "add: want super/own_case_or_day/government.py on small case"

---
+++ super/own_case_or_day/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    man(str_arg)
+    print('other_problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('life')
