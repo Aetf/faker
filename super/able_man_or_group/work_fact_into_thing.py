8cb3226c3ed786886e42ec9a8bd2adf0aeac84b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:32:39 -0400

    "mod: have super/long_time/eye/time/work_and_case/own_thing_and_number/group_or_company.py and super/able_man_or_group/work_fact_into_thing.py of next part"

---
+++ super/able_man_or_group/work_fact_into_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week(str_arg):
+    young_problem_or_old_number(str_arg)
+    print('little_day')
+
+def young_problem_or_old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week('great_life_and_own_point')
