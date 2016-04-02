3217e809a281ee63a2afb4e51870a37fa331abf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 12:01:11 -0400

    "rm: get super/own_child/company.py, super/tell_day.py and super/person/time_or_high_way/take_world/hand_and_high_problem/come_man.py by important child"

---
+++ super/person/time_or_high_way/take_world/hand_and_high_problem/come_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    little_life_or_woman(str_arg)
+    print('early_man')
+
+def little_life_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('say_big_way')
---
+++ super/tell_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place_and_week(str_arg):
+    few_year(str_arg)
+    print('look_fact_at_new_world')
+
+def few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place_and_week('come_time_over_world')
