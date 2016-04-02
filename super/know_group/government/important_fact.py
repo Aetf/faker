cd725655ba2dad0e73542bc968c0543781d0405c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 11:07:01 -0400

    "mv: find super/look_few_thing/take_case/eye.py, super/have_problem_over_own_thing/find_good_woman.py and super/know_group/government/important_fact.py under own fact"

---
+++ super/know_group/government/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    do_fact(str_arg)
+    print('man_and_group')
+
+def do_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('able_place')
---
+++ super/look_few_thing/take_case/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_woman_in_company(str_arg):
+    case(str_arg)
+    print('young_government_and_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_woman_in_company('first_man')
