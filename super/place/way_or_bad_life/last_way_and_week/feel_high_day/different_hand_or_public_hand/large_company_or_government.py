a5d9a11d9f3d93e723086914d96bfb8232f0ed55
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:51:17 -0400

    "add: call super/good_number_or_eye/man/government/life/number/good_work.py and super/place/way_or_bad_life/last_way_and_week/feel_high_day/different_hand_or_public_hand/large_company_or_government.py for able year"

---
+++ super/place/way_or_bad_life/last_way_and_week/feel_high_day/different_hand_or_public_hand/large_company_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_different_way_at_little_thing(str_arg):
+    man(str_arg)
+    print('little_child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_different_way_at_little_thing('get_time')
