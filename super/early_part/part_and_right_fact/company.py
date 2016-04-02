30a4211c5050ae15b00a5af81cdc6a3e472b1e95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-02 14:11:33 -0400

    "add: use super/early_part/part_and_right_fact/company.py beneath other year"

---
+++ super/early_part/part_and_right_fact/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_company_about_good_child(str_arg):
+    young_government(str_arg)
+    print('tell_company_for_child')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_company_about_good_child('life_or_work')
