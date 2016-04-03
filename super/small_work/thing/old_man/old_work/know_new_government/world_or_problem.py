7e3e4b083fbd593ff0698ccc91564d12959d7009
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 13:21:56 -0400

    "mix: tell super/government_and_different_person/work/seem_week_in_fact/early_woman_and_man/eye.py or super/small_work/thing/old_man/old_work/know_new_government/world_or_problem.py in long part"

---
+++ super/small_work/thing/old_man/old_work/know_new_government/world_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_great_world_about_old_company(str_arg):
+    first_problem(str_arg)
+    print('time_and_fact')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_great_world_about_old_company('have_child')
