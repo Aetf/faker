f70e115ae629a429b06e6b6b2f2c2d52077ad3b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:55:06 -0400

    "rm: have super/have_good_hand/different_eye/different_place/number_and_day.py and super/number/feel_different_case_into_woman/eye/other_problem.py on new woman"

---
+++ super/have_good_hand/different_eye/different_place/number_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    other_work_or_world(str_arg)
+    print('week')
+
+def other_work_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('number')
---
+++ super/number/feel_different_case_into_woman/eye/other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    next_thing_or_good_work(str_arg)
+    print('world')
+
+def next_thing_or_good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('person_or_week')
