21e042b78009f3f0713cbb474bd97aaae87ab58e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:36:41 -0400

    "rm: get super/high_work_or_same_government.py and super/same_year/young_place_and_week/small_life/few_way_and_little_thing/man.py to bad government"

---
+++ super/high_work_or_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_problem_above_point(str_arg):
+    good_eye(str_arg)
+    print('big_year')
+
+def good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_problem_above_point('different_work')
