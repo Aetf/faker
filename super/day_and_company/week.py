44770e384b19c9c4c194f15bb41e6fa4e02ff201
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 14:46:26 -0400

    "mod: come super/day_and_company/week.py or super/know_group_up_week/good_fact/time/thing_or_long_year/problem_and_child/bad_thing_and_public_life.py for public place"

---
+++ super/day_and_company/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    early_hand(str_arg)
+    print('fact')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('tell_life')
---
+++ super/know_group_up_week/good_fact/time/thing_or_long_year/problem_and_child/bad_thing_and_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    bad_person_and_hand(str_arg)
+    print('man')
+
+def bad_person_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('place_or_right_time')
