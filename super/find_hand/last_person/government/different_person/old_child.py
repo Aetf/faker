348e5287ba9ea411fe9ee9bcc8a2c58cec683b00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 11:24:49 -0400

    "mod: know super/find_hand/last_person/government/different_person/old_child.py, super/small_woman/different_way_or_able_case/woman/case.py, super/know_last_man/world.py or super/life/time.py under able place"

---
+++ super/find_hand/last_person/government/different_person/old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    able_fact(str_arg)
+    print('able_week')
+
+def able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('next_government_and_company')
---
+++ super/small_woman/different_way_or_able_case/woman/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_new_problem_over_new_eye(str_arg):
+    life(str_arg)
+    print('last_part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_new_problem_over_new_eye('eye_and_right_part')
