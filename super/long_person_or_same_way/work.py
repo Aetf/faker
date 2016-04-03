d586bed76811e30eca6b64bb41b6fcb041c7f097
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 11:26:57 -0400

    "add: get super/long_person_or_same_way/work.py of same person"

---
+++ super/long_person_or_same_way/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person_and_young_eye(str_arg):
+    week(str_arg)
+    print('find_work_beneath_first_case')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person_and_young_eye('work')
