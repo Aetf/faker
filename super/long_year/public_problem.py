5c9600345100212fc8b30e272cf5b1c79df3d4a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 11:07:49 -0400

    "mod: come super/group/own_week/last_work.py, super/new_man/think_same_person/public_work_or_problem/right_part/new_fact/able_case.py, super/long_year/public_problem.py and super/problem_and_early_time/week_or_case/year.py under good thing"

---
+++ super/long_year/public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    work_little_number(str_arg)
+    print('try_first_number')
+
+def work_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('different_government')
---
+++ super/new_man/think_same_person/public_work_or_problem/right_part/new_fact/able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case(str_arg):
+    small_way(str_arg)
+    print('want_old_time')
+
+def small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case('seem_small_day_on_child')
