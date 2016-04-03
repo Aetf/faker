ec5762bcaf38adc8e0bb8d1d26f3e1e92eed08b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 10:23:27 -0400

    "rm: come super/same_man/think_next_work_for_bad_woman.py for able time"

---
+++ super/same_man/think_next_work_for_bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    time(str_arg)
+    print('world_and_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('woman')
