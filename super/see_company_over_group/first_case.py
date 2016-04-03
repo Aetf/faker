5e970efaaba2060aacfddb0f25f3a5ce7b018708
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 13:12:48 -0400

    "add: think super/way_or_same_point/number_and_day/big_day_and_different_eye.py, super/see_company_over_group/first_case.py or super/same_world_or_able_case.py about few group"

---
+++ super/see_company_over_group/first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_part_and_old_hand(str_arg):
+    place_and_new_way(str_arg)
+    print('have_great_time')
+
+def place_and_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_part_and_old_hand('know_life')
