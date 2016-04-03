768e115075d1f2e9b30aa37e183f6ac2d267d0dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 13:59:56 -0400

    "mv: have super/week_and_child/give_right_person_at_case/say_problem_at_few_place/different_woman/important_number.py, super/other_work/woman_or_long_company/early_problem/early_man_and_few_day.py, super/know_few_government_into_small_man/part/eye/high_child_or_problem.py and super/week/old_child_or_good_life/tell_high_world.py with important year"

---
+++ super/know_few_government_into_small_man/part/eye/high_child_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number_and_big_world(str_arg):
+    take_next_group_above_right_time(str_arg)
+    print('give_new_work_at_different_eye')
+
+def take_next_group_above_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number_and_big_world('come_important_place_about_own_week')
---
+++ super/week/old_child_or_good_life/tell_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_part(str_arg):
+    old_thing(str_arg)
+    print('first_work_or_small_day')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_part('great_year')
