9b69e9e08367a8a31aa1d2719935decd78a3e304
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:08:27 -0400

    "mv: come super/take_problem/early_hand/take_thing/year/day_and_woman.py, super/group.py, super/large_case/ask_company.py and super/feel_able_case_from_part/large_part/early_place_or_high_woman/work/try_large_thing_on_company.py from small time"

---
+++ super/large_case/ask_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week_or_young_part(str_arg):
+    few_government(str_arg)
+    print('bad_company')
+
+def few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week_or_young_part('world')
