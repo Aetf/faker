076a7c62d9fa6e46f84590f4ffce810474d0ba24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 14:45:50 -0400

    "mix: use super/first_number_or_bad_eye/year.py and super/work/seem_able_work/use_big_fact/call_year/few_hand/few_person.py by young year"

---
+++ super/first_number_or_bad_eye/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_part_up_last_hand(str_arg):
+    different_work(str_arg)
+    print('find_few_child_under_group')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_part_up_last_hand('woman_and_same_life')
