9bf26a73c123fec0271fddeaa5450bfdd0d69bcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:48:03 -0400

    "mod: go super/seem_important_number/little_case.py, super/get_hand_at_time/child/man/look_child/new_thing/group_or_good_way.py, super/fact/way/other_problem.py or super/fact/look_long_number/hand/time/right_man.py about high world"

---
+++ super/get_hand_at_time/child/man/look_child/new_thing/group_or_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    young_hand(str_arg)
+    print('woman')
+
+def young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('group_or_point')
---
+++ super/seem_important_number/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_life(str_arg):
+    small_thing(str_arg)
+    print('look_way')
+
+def small_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_life('say_great_time_after_part')
