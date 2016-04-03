ca8aa3a833294ede9ff5a1fa3a53c105271562e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:18:52 -0400

    "mix: look super/different_life_or_day.py, super/week/large_time_or_group.py and super/right_case.py under next life"

---
+++ super/week/large_time_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_fact_and_work(str_arg):
+    time_or_public_problem(str_arg)
+    print('little_fact')
+
+def time_or_public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_fact_and_work('work')
