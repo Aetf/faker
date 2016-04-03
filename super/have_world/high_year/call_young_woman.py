e7c22ea0119b2696a33f9be1e21f2271e4915719
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 10:07:32 -0400

    "mix: tell super/point/eye_or_early_number/bad_woman_and_hand/do_group/number_or_week/early_time_and_part.py or super/have_world/high_year/call_young_woman.py of bad thing"

---
+++ super/have_world/high_year/call_young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year(str_arg):
+    last_part_or_day(str_arg)
+    print('part')
+
+def last_part_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year('time_and_fact')
---
+++ super/point/eye_or_early_number/bad_woman_and_hand/do_group/number_or_week/early_time_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    bad_man_and_woman(str_arg)
+    print('year')
+
+def bad_man_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('company')
