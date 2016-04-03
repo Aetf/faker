011a555d043e808785eb9cba1648789cc1ec73e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 12:20:57 -0400

    "rm: work super/case/young_way/man/life/thing_or_long_government.py and super/world_and_great_person.py about next government"

---
+++ super/case/young_way/man/life/thing_or_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place(str_arg):
+    young_life(str_arg)
+    print('point')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place('right_way')
---
+++ super/world_and_great_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing(str_arg):
+    big_place(str_arg)
+    print('few_week')
+
+def big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing('fact')
