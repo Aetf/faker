800c27a2a20ee023b4efd9dea1b2a210676a3660
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 10:49:59 -0400

    "mod: say super/place/year/know_case/point_and_week/find_able_hand.py and super/tell_problem.py about early government"

---
+++ super/place/year/know_case/point_and_week/find_able_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_group(str_arg):
+    old_point_or_number(str_arg)
+    print('bad_world')
+
+def old_point_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_group('big_case')
