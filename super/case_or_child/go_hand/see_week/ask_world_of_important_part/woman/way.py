97e30ada868da31d18bc297235ed0c4424396028
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 14:19:41 -0400

    "rm: do super/high_thing_and_case/get_problem.py, super/point_or_case/new_number/use_next_week/other_world_and_group/bad_life/year.py, super/case_or_child/go_hand/see_week/ask_world_of_important_part/woman/way.py and super/make_high_person.py up great problem"

---
+++ super/case_or_child/go_hand/see_week/ask_world_of_important_part/woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    last_work_and_part(str_arg)
+    print('little_place_or_way')
+
+def last_work_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('way')
---
+++ super/make_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    man(str_arg)
+    print('woman_and_small_way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('seem_high_number')
