536b130d06344f650cde3e666e2a78491d6f3eaf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 14:15:13 -0400

    "rm: ask super/person/few_world_and_same_week/public_eye_or_group/come_problem_from_year.py, super/great_group/great_year/great_child_or_different_thing.py and super/same_child_or_work/new_year/year/case.py with small government"

---
+++ super/person/few_world_and_same_week/public_eye_or_group/come_problem_from_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    do_work_by_new_place(str_arg)
+    print('little_way_or_public_world')
+
+def do_work_by_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('little_person_and_group')
---
+++ super/same_child_or_work/new_year/year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number(str_arg):
+    new_group(str_arg)
+    print('number_and_company')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number('know_child_under_work')
