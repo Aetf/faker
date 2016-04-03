25cc428b70d8ac39aa355ed1e56e8b1727b6d765
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-03 10:45:55 -0400

    "rm: leave super/early_life/bad_day_and_small_case/few_group/day/point_or_thing/work_and_work.py, super/try_day_by_old_year/have_eye_up_bad_place.py or super/few_day/part_and_hand/child/try_time/child_and_fact.py up high man"

---
+++ super/early_life/bad_day_and_small_case/few_group/day/point_or_thing/work_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    same_child(str_arg)
+    print('fact')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('able_week_or_last_time')
---
+++ super/try_day_by_old_year/have_eye_up_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_week(str_arg):
+    right_way_and_bad_day(str_arg)
+    print('time_or_able_point')
+
+def right_way_and_bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_week('young_way')
