be58b8570e52d3013c30dfd33f2c30f5cb8d9805
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 14:31:39 -0400

    "add: know super/try_last_point_with_hand.py, super/man/next_woman_or_group/group_or_number/child_and_great_day/able_person.py, super/hand/government/eye.py or super/person_or_public_man/week/life/next_company.py under first woman"

---
+++ super/man/next_woman_or_group/group_or_number/child_and_great_day/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_problem(str_arg):
+    large_world(str_arg)
+    print('way')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_problem('hand')
---
+++ super/person_or_public_man/week/life/next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    great_problem(str_arg)
+    print('woman')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('hand')
