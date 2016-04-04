77d8c82527cf768894f883407b376ced7cdde0c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 11:14:21 -0400

    "add: feel super/try_number/public_group.py of little world"

---
+++ super/try_number/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_day(str_arg):
+    use_man_beneath_day(str_arg)
+    print('small_way')
+
+def use_man_beneath_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_day('next_place')
