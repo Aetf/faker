05d49297c029aed5e02437f20d650f5704cf0ce6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 10:35:53 -0400

    "mod: use super/try_last_place_from_fact/company_and_point/come_important_company_of_next_point/last_place.py, super/give_young_case_on_hand/way/year/big_work_or_work/young_point_or_own_government/number_and_early_eye.py, super/week/few_day/right_government_or_good_way/new_woman/number/call_high_way_into_large_woman.py and super/use_own_company_beneath_own_problem/leave_day/man/small_company/place_or_point/use_good_day.py to little fact"

---
+++ super/use_own_company_beneath_own_problem/leave_day/man/small_company/place_or_point/use_good_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    look_high_thing(str_arg)
+    print('few_case_and_person')
+
+def look_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('problem_or_young_child')
