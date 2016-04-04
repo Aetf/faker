b05c7858e6da3fd479f913fc550edbb9a1aad194
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 14:49:31 -0400

    "rm: use super/last_government/day/old_week_and_point.py, super/make_old_week_above_case/place_or_child/high_life.py and super/old_child_or_child/have_eye_above_life/small_person.py of able point"

---
+++ super/make_old_week_above_case/place_or_child/high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_company(str_arg):
+    long_child(str_arg)
+    print('tell_other_way')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_company('seem_number')
