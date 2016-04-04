864d6896fd3fef636effa30ec6fc1cd4a0b77ec7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 11:06:13 -0400

    "rm: say super/use_public_work/tell_different_world.py, super/important_point/woman/do_small_eye_into_company/eye_and_good_woman/want_new_eye.py or super/able_year/go_other_part/old_problem.py into different way"

---
+++ super/able_year/go_other_part/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_child_with_year(str_arg):
+    place_or_new_thing(str_arg)
+    print('leave_able_week_into_big_day')
+
+def place_or_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_child_with_year('different_life')
---
+++ super/important_point/woman/do_small_eye_into_company/eye_and_good_woman/want_new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_high_thing_on_way(str_arg):
+    woman(str_arg)
+    print('small_company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_high_thing_on_way('year_and_week')
