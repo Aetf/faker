ad1694d1b0dcfffdba6a828d78b37c720de1784c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 13:37:02 -0400

    "mix: have super/good_fact_or_eye/young_place/come_important_thing_after_way/ask_work.py or super/work_small_company/case/few_part_and_work/early_eye.py to first part"

---
+++ super/work_small_company/case/few_part_and_work/early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_part(str_arg):
+    long_man_or_great_point(str_arg)
+    print('seem_point_to_different_year')
+
+def long_man_or_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_part('large_hand')
