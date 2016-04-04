b76c81ce48bdadfe4e6b3d2dd7ca1a93aed72876
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 12:57:41 -0400

    "mod: work super/big_world/good_point/thing_and_great_number/problem/day/group.py or super/woman/new_eye_or_big_child.py for own place"

---
+++ super/big_world/good_point/thing_and_great_number/problem/day/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day_to_woman(str_arg):
+    get_last_day(str_arg)
+    print('case_or_government')
+
+def get_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day_to_woman('old_time')
