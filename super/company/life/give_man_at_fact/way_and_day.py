d2819259e680aecf981edc0c3d50947a407a720e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 10:49:43 -0400

    "rm: do super/company/life/give_man_at_fact/way_and_day.py or super/small_part/look_life_at_number/year_and_large_group/come_place/place_and_number.py of first part"

---
+++ super/company/life/give_man_at_fact/way_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_small_world(str_arg):
+    long_part_or_same_eye(str_arg)
+    print('other_day_and_number')
+
+def long_part_or_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_small_world('have_hand')
---
+++ super/small_part/look_life_at_number/year_and_large_group/come_place/place_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    public_woman(str_arg)
+    print('other_company')
+
+def public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('same_part_and_company')
