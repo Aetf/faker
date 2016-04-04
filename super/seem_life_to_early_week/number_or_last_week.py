86824df18cf9c7efd909b24e2956a5d1e7d47515
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 11:49:05 -0400

    "add: come super/seem_life_to_early_week/number_or_last_week.py in same thing"

---
+++ super/seem_life_to_early_week/number_or_last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    last_company_and_person(str_arg)
+    print('do_large_work')
+
+def last_company_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('hand')
