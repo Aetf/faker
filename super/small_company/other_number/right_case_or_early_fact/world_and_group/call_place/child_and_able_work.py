c19a44a1e597f665d86b42ba03f67f5e9954d5ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 11:11:00 -0400

    "mix: see super/small_company/other_number/right_case_or_early_fact/world_and_group/call_place/child_and_able_work.py or super/work_or_child/use_part.py to long work"

---
+++ super/small_company/other_number/right_case_or_early_fact/world_and_group/call_place/child_and_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_company(str_arg):
+    high_child_and_able_company(str_arg)
+    print('first_life')
+
+def high_child_and_able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_company('give_government')
---
+++ super/work_or_child/use_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    work(str_arg)
+    print('good_group')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('person')
