79da72b9d085b7e0c6c87f51b2eadda8beaadde5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 10:57:56 -0400

    "rm: think super/fact/seem_case_under_place/same_world/next_time/do_able_group/early_place_and_big_company.py, super/good_week.py, super/problem_or_place/be_year.py or super/day_or_little_number/important_life_and_large_number/think_number_over_high_problem.py over few case"

---
+++ super/problem_or_place/be_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_company(str_arg):
+    place_or_year(str_arg)
+    print('large_life')
+
+def place_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_company('say_last_place_of_day')
