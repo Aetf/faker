bdd1bd535fef2815aafd9e35e69dfe9f4558345c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 12:11:50 -0400

    "mv: use super/come_time_into_year/small_work_and_day/same_way/number.py, super/hand/able_case.py and super/same_case_or_way.py for high part"

---
+++ super/hand/able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    own_eye(str_arg)
+    print('old_time_or_company')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('right_group_or_first_woman')
---
+++ super/same_case_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    little_man(str_arg)
+    print('go_point')
+
+def little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('last_company_and_other_way')
