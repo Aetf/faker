6c4f288910f834fd058e836993138c1f2908639f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 13:05:17 -0400

    "mv: be super/small_week_or_same_place/other_case/world_and_bad_hand/group/tell_eye_by_long_government.py, super/child/long_group_or_different_day/large_place_or_right_child.py, super/last_part_or_person/small_life/same_eye/day/woman/same_problem.py or super/do_old_world/long_week_and_government/get_little_number/company/take_year.py to last government"

---
+++ super/last_part_or_person/small_life/same_eye/day/woman/same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    group(str_arg)
+    print('last_number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('young_part_and_man')
---
+++ super/small_week_or_same_place/other_case/world_and_bad_hand/group/tell_eye_by_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_good_place(str_arg):
+    be_point_on_first_person(str_arg)
+    print('make_large_problem_with_part')
+
+def be_point_on_first_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_good_place('tell_few_work')
