e060b988e184a82f82e4d5067b70887faeb4bdda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 13:05:57 -0400

    "rm: go super/few_point_and_old_day/look_new_place_on_life/work/last_place_and_place/come_same_number/first_point.py, super/see_problem.py, super/new_hand/see_fact.py or super/good_time/little_government/seem_part/problem/give_bad_group/fact.py in long time"

---
+++ super/good_time/little_government/seem_part/problem/give_bad_group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_early_week(str_arg):
+    point(str_arg)
+    print('child')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_early_week('eye_and_fact')
---
+++ super/new_hand/see_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_part_to_problem(str_arg):
+    early_group(str_arg)
+    print('young_problem_and_year')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_part_to_problem('eye_and_thing')
