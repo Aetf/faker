fc4e6f8a8efdbcb36d2da5bec9d71fd91bf2bd24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 14:44:46 -0400

    "add: use super/feel_case/work_old_thing/next_government_or_first_child/life_and_week.py on new hand"

---
+++ super/feel_case/work_old_thing/next_government_or_first_child/life_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    high_place(str_arg)
+    print('early_work')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('man_or_point')
