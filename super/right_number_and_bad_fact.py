7dedf33bf7b5e7d62b354229854d2c30badd7c66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-04 14:45:57 -0400

    "add: tell super/right_number_and_bad_fact.py or super/time/thing_and_own_day/early_part_or_high_thing/next_man/go_time/use_child_in_early_life.py over other company"

---
+++ super/right_number_and_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_person(str_arg):
+    way(str_arg)
+    print('small_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_person('see_thing_on_old_group')
---
+++ super/time/thing_and_own_day/early_part_or_high_thing/next_man/go_time/use_child_in_early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    high_work(str_arg)
+    print('woman')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('seem_work')
