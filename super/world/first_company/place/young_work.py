15bab042b18befe7e5a551af11be0e23d9e2baf1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 13:34:18 -0400

    "mod: get super/old_year_or_hand.py, super/world/first_company/place/young_work.py, super/day/place/new_man/problem/great_life_and_person.py or super/life/next_part/thing.py over other case"

---
+++ super/world/first_company/place/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_able_company(str_arg):
+    know_fact_beneath_hand(str_arg)
+    print('use_good_week')
+
+def know_fact_beneath_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_able_company('high_government_or_important_day')
