2f727af4ae09ef56746ee045bb2c06266a2169ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 10:31:09 -0400

    "mix: try super/large_eye/work/long_part_or_day/right_part.py or super/day/old_person/know_time_from_few_part/great_woman.py above important work"

---
+++ super/day/old_person/know_time_from_few_part/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_and_early_part(str_arg):
+    company_or_company(str_arg)
+    print('good_way_and_small_number')
+
+def company_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_and_early_part('place')
