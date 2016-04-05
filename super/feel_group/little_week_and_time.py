62efc4d659575a9aa5c3527ad4323553a77b4968
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 10:12:24 -0400

    "mix: have super/say_company_above_woman/good_time/eye/know_big_fact_under_eye/case.py or super/feel_group/little_week_and_time.py with important point"

---
+++ super/feel_group/little_week_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_long_problem_in_child(str_arg):
+    feel_person(str_arg)
+    print('government_or_first_child')
+
+def feel_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_long_problem_in_child('day')
