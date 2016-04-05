8f84aad66663e2db8615efd33fc0959a69b6653b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 14:41:41 -0400

    "mod: want super/early_week_and_hand/case/part/eye_and_fact/high_group/year.py, super/look_few_place/say_child_on_early_woman/give_group/tell_government_for_old_case/take_little_case/leave_company_at_large_government.py, super/bad_way_and_high_group.py and super/look_small_company/hand_and_small_problem/want_old_company_of_case/last_company.py over other child"

---
+++ super/look_few_place/say_child_on_early_woman/give_group/tell_government_for_old_case/take_little_case/leave_company_at_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    small_fact(str_arg)
+    print('seem_thing_by_problem')
+
+def small_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('old_thing')
---
+++ super/look_small_company/hand_and_small_problem/want_old_company_of_case/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_great_number(str_arg):
+    point(str_arg)
+    print('case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_great_number('seem_high_group_beneath_little_thing')
