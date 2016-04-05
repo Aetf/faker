5fc15293506aa3cde577a25d9c42db4f12fde167
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 11:33:12 -0400

    "add: look super/thing/problem_or_right_work.py and super/have_old_person_at_right_way/year/part_or_long_person/other_child/way.py at same woman"

---
+++ super/thing/problem_or_right_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    own_place(str_arg)
+    print('day')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('own_fact')
