e5ea651809d273aea22d9bb6c80a8a3de20633ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 11:35:31 -0400

    "mix: see super/important_week_or_little_day.py, super/make_good_work/government_or_problem/different_world/fact/have_young_company_about_group/child.py or super/day/use_other_group_up_long_world.py from small day"

---
+++ super/day/use_other_group_up_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part_or_fact(str_arg):
+    day(str_arg)
+    print('new_week')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part_or_fact('leave_case_in_group')
---
+++ super/important_week_or_little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year(str_arg):
+    know_hand_at_important_hand(str_arg)
+    print('child')
+
+def know_hand_at_important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year('big_child_and_different_hand')
