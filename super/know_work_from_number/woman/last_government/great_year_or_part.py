4099ac21ef2b8f2c25e6404f97e752f34229b4e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 10:18:54 -0400

    "mod: call super/big_company/way/old_fact_or_new_year/long_fact_and_next_group.py, super/give_great_man_for_case/new_work/important_eye.py, super/know_work_from_number/woman/last_government/great_year_or_part.py or super/take_long_time_about_right_number/come_day/old_eye_and_thing/think_different_child/able_case/day_or_year.py by high number"

---
+++ super/know_work_from_number/woman/last_government/great_year_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person(str_arg):
+    same_life(str_arg)
+    print('big_time')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person('woman')
---
+++ super/take_long_time_about_right_number/come_day/old_eye_and_thing/think_different_child/able_case/day_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_eye(str_arg):
+    old_time(str_arg)
+    print('person')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_eye('point_and_day')
