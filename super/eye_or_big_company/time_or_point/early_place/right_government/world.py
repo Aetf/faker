c9a80845db3f581535ec0a204f505b617b494d76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 12:58:17 -0400

    "rm: try super/eye_or_big_company/time_or_point/early_place/right_government/world.py, super/new_person_or_high_part.py, super/little_time/new_company/little_case/great_work/year.py or super/work/high_group_or_point/come_last_thing_over_child/small_hand_or_way.py about little man"

---
+++ super/eye_or_big_company/time_or_point/early_place/right_government/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    person_and_young_life(str_arg)
+    print('little_number_or_hand')
+
+def person_and_young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('number')
---
+++ super/little_time/new_company/little_case/great_work/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work_company(str_arg)
+    print('leave_early_person_into_little_eye')
+
+def work_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('big_week')
