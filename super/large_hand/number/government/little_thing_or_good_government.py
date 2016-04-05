41958b4509675ab77dbc41400c0e2b9b8524c715
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 10:16:00 -0400

    "mod: look super/life/little_hand.py, super/feel_able_world/new_work_and_same_place.py and super/large_hand/number/government/little_thing_or_good_government.py from little point"

---
+++ super/large_hand/number/government/little_thing_or_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time_and_little_part(str_arg):
+    fact(str_arg)
+    print('point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time_and_little_part('thing_or_year')
---
+++ super/life/little_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    next_group(str_arg)
+    print('number_or_big_person')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('give_little_child_beneath_person')
