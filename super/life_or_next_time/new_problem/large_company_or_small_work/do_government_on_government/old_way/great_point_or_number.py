5f0c19b3dcf7e01e52a6b23430b5bd477d579ebd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 11:10:39 -0400

    "rm: feel super/point/good_problem_or_hand/long_point.py, super/life_or_next_time/new_problem/large_company_or_small_work/do_government_on_government/old_way/great_point_or_number.py, super/different_way/world_or_way/bad_way_or_place/last_week/first_case.py or super/early_thing/other_man/find_number/want_old_person/child/thing.py to old thing"

---
+++ super/life_or_next_time/new_problem/large_company_or_small_work/do_government_on_government/old_way/great_point_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_number(str_arg):
+    case_and_eye(str_arg)
+    print('child_or_great_government')
+
+def case_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_number('right_company_and_hand')
