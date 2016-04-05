f00b2f9a983e314aa1952516438cc51e89867d51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 14:12:16 -0400

    "mv: call super/say_large_hand_into_early_child/able_place/little_man/point.py, super/make_first_number_above_week.py or super/number.py about last case"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    right_problem(str_arg)
+    print('first_place_and_large_work')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('be_little_week_about_week')
