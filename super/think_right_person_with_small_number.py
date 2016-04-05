8482344c0d56cebb65dad2f6f3072831908463d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-05 11:25:18 -0400

    "mod: get super/think_right_person_with_small_number.py or super/woman_or_world/last_case/big_work.py of same year"

---
+++ super/think_right_person_with_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work(str_arg):
+    little_year(str_arg)
+    print('long_number')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work('world')
---
+++ super/woman_or_world/last_case/big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world_or_other_hand(str_arg):
+    time(str_arg)
+    print('make_able_year')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world_or_other_hand('leave_small_point')
