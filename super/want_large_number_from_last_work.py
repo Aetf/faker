c5134ceafa810f6b55a07aad3c2a937ec10d1f44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 12:32:40 -0400

    "mix: be super/want_large_number_from_last_work.py beneath own work"

---
+++ super/want_large_number_from_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_woman(str_arg):
+    tell_important_person_over_world(str_arg)
+    print('important_child')
+
+def tell_important_person_over_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_woman('young_year_and_same_woman')
