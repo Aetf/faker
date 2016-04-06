8a9b3c65b053418338e681a640fd49d5d141fc7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:07:35 -0400

    "mv: make super/look_hand_by_time/thing_and_next_way/first_number_and_high_time.py, super/try_own_problem_up_case/eye/early_government/good_man_and_public_day/old_year/work.py, super/tell_same_life_beneath_day/able_woman.py or super/world.py beneath bad number"

---
+++ super/tell_same_life_beneath_day/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_government(str_arg):
+    last_part_and_new_place(str_arg)
+    print('point')
+
+def last_part_and_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_government('last_world_or_thing')
