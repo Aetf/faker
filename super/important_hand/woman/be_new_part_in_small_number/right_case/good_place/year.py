d4916a74cb7b503b9ca787e9624bc03b56d01a8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:04:26 -0400

    "rm: leave super/important_hand/woman/be_new_part_in_small_number/right_case/good_place/year.py for bad day"

---
+++ super/important_hand/woman/be_new_part_in_small_number/right_case/good_place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year_and_high_problem(str_arg):
+    number(str_arg)
+    print('last_time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year_and_high_problem('good_eye')
