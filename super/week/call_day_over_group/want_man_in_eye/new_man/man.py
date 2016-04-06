4587ceb65684ba3d35eef224bb3a6a38c76d7872
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 14:02:38 -0400

    "mod: think super/big_world/thing/bad_number.py, super/large_way/eye.py, super/group/feel_point_up_world/large_world.py or super/week/call_day_over_group/want_man_in_eye/new_man/man.py under next man"

---
+++ super/week/call_day_over_group/want_man_in_eye/new_man/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    want_early_problem_to_fact(str_arg)
+    print('new_place')
+
+def want_early_problem_to_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('year')
