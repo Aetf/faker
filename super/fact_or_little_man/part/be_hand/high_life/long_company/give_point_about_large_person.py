d29c25761f25f27130c2f255dd64e36f8af9c4ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 10:05:12 -0400

    "mod: think super/part/week.py, super/fact_or_little_man/part/be_hand/high_life/long_company/give_point_about_large_person.py or super/look_new_time_in_life/government/hand_or_few_eye/man_or_place/eye/other_person_and_right_day.py for able woman"

---
+++ super/fact_or_little_man/part/be_hand/high_life/long_company/give_point_about_large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    try_big_part_to_different_thing(str_arg)
+    print('great_company')
+
+def try_big_part_to_different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('large_person_or_good_way')
---
+++ super/part/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    next_person(str_arg)
+    print('early_number')
+
+def next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('little_world')
