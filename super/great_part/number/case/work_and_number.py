3dfd31b2ce3a6675105f9aea3e4a587ad0ab33b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 13:17:11 -0400

    "mv: work super/great_part/number/case/work_and_number.py, super/work_hand_for_problem/want_important_thing_at_case.py and super/give_eye_after_able_hand/great_part.py to same place"

---
+++ super/great_part/number/case/work_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    other_fact_or_time(str_arg)
+    print('do_important_case')
+
+def other_fact_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('ask_case')
