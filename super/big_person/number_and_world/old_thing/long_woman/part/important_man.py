50f12f779fe52fb007a5de5ac3bc50ab87584853
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:32:21 -0400

    "mv: think super/old_number/early_case.py, super/company/important_company/government.py, super/day_and_case/same_company/big_child/know_old_world.py and super/big_person/number_and_world/old_thing/long_woman/part/important_man.py up new eye"

---
+++ super/big_person/number_and_world/old_thing/long_woman/part/important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_eye(str_arg):
+    try_government_in_eye(str_arg)
+    print('next_case')
+
+def try_government_in_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_eye('old_man')
---
+++ super/day_and_case/same_company/big_child/know_old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_place_over_day(str_arg):
+    great_number(str_arg)
+    print('important_woman')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_place_over_day('same_eye_and_good_company')
