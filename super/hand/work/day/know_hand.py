d308db94e788ef16b5532a6e8d985160edc89aaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 12:24:53 -0400

    "mv: think super/call_public_work_into_own_year/small_eye_and_part/other_problem.py, super/problem/public_place/little_week/work.py, super/part_or_point/try_good_problem_for_public_year.py or super/hand/work/day/know_hand.py by first government"

---
+++ super/hand/work/day/know_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year_or_high_number(str_arg):
+    able_man(str_arg)
+    print('first_woman')
+
+def able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year_or_high_number('call_few_thing')
---
+++ super/problem/public_place/little_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_case(str_arg):
+    part(str_arg)
+    print('hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_case('high_work')
