ab4e431fd5cf52698e63c57daf3ca880f5b1f396
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:14:43 -0400

    "mv: work super/other_case_and_young_fact/company.py of few woman"

---
+++ super/other_case_and_young_fact/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    week_or_little_problem(str_arg)
+    print('good_man')
+
+def week_or_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('work_woman_into_big_day')
