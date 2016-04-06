4d598591ed11b1743c58badcba2864527f8fe965
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 13:24:52 -0400

    "mod: have super/own_week_or_eye/little_child/number/new_person_or_different_work/come_great_person/world.py, super/number_and_young_government/look_different_case_for_woman.py, super/problem_or_woman/first_number/little_hand.py and super/group/different_company.py over last time"

---
+++ super/group/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_work(str_arg):
+    first_time_and_part(str_arg)
+    print('do_fact')
+
+def first_time_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_work('first_work_or_child')
---
+++ super/problem_or_woman/first_number/little_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_other_government(str_arg):
+    year_or_new_number(str_arg)
+    print('group')
+
+def year_or_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_other_government('child')
