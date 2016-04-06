d9918e658dd94610ff90db118e745356abe4b921
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 12:49:33 -0400

    "mix: seem super/take_different_case/public_group_or_other_woman.py, super/life.py or super/person/take_small_case/case/big_woman_or_last_government/next_week_and_thing/good_fact_or_important_week.py with good government"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day_or_first_problem(str_arg):
+    public_week_and_group(str_arg)
+    print('life')
+
+def public_week_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day_or_first_problem('same_point')
---
+++ super/take_different_case/public_group_or_other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    man(str_arg)
+    print('last_problem_or_big_life')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('new_case')
