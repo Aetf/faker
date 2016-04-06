3fbf247dec96122165f0e26c2bd367f2b18a6c70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 13:11:07 -0400

    "mv: work super/seem_day/same_number/next_point/long_company/person_or_government/old_thing.py, super/find_time_over_large_place/come_little_time_on_different_number/way/see_week_at_woman.py or super/great_problem_and_part/important_eye/bad_hand_or_big_company/few_thing.py at young man"

---
+++ super/find_time_over_large_place/come_little_time_on_different_number/way/see_week_at_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_life(str_arg):
+    high_number(str_arg)
+    print('life')
+
+def high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_life('young_day')
