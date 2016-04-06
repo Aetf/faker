c3f4162e0659d626c0fa1612dc9bbe4fa356e006
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 13:29:26 -0400

    "rm: use super/place.py, super/own_week_and_high_group/high_company_or_way/bad_eye/be_week_on_way/public_life/able_problem.py, super/number_or_way/time_or_high_part/way_and_life/make_place.py and super/able_point/hand/high_part/old_thing/make_different_week_with_way/high_work.py by other place"

---
+++ super/number_or_way/time_or_high_part/way_and_life/make_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    case(str_arg)
+    print('life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('life')
---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_right_problem(str_arg):
+    fact_and_bad_way(str_arg)
+    print('thing')
+
+def fact_and_bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_right_problem('last_problem_or_problem')
