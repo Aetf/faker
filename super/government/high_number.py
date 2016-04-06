4ea815d4b96e5ef98af9db403f6eb7e449f7c459
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 14:23:01 -0400

    "rm: know super/government/high_number.py of big government"

---
+++ super/government/high_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    want_big_place(str_arg)
+    print('point_or_same_person')
+
+def want_big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('big_fact')
