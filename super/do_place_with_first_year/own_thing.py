822fa1caaa079c7161a95f8f45604b5dd9fd22dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:41:23 -0400

    "mv: see super/work.py or super/do_place_with_first_year/own_thing.py about last place"

---
+++ super/do_place_with_first_year/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact_and_same_place(str_arg):
+    point_and_same_group(str_arg)
+    print('place')
+
+def point_and_same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact_and_same_place('important_point')
