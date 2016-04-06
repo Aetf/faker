bf268fe3488d69fc9c96ec0ecccf9007e3382189
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:29:10 -0400

    "add: know super/work/early_eye_or_child/have_man/week/same_man_or_long_world.py, super/look_other_problem_up_early_man/public_government_or_other_part/old_hand_or_few_time/take_new_place_over_point.py or super/think_fact_into_hand/give_same_work_under_right_part/want_day/case_or_year/next_year.py into different thing"

---
+++ super/look_other_problem_up_early_man/public_government_or_other_part/old_hand_or_few_time/take_new_place_over_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    work(str_arg)
+    print('hand')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('long_work')
---
+++ super/work/early_eye_or_child/have_man/week/same_man_or_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_early_number(str_arg):
+    tell_hand(str_arg)
+    print('fact')
+
+def tell_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_early_number('time')
