1b9ae8700001024988ca77c30b60fda0d54684fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-06 11:31:57 -0400

    "mix: give super/woman.py and super/small_thing/use_same_place_about_government/eye.py up young life"

---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way(str_arg):
+    different_number(str_arg)
+    print('work')
+
+def different_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way('own_week')
