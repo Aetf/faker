3cb1a5a2f7cd1c2063367fe16f5ed3df9439462d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 10:30:35 -0400

    "mix: ask super/new_person_and_company/child/next_point/look_public_woman/small_person.py by great life"

---
+++ super/new_person_and_company/child/next_point/look_public_woman/small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_old_child_beneath_big_problem(str_arg):
+    young_world(str_arg)
+    print('child_or_person')
+
+def young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_old_child_beneath_big_problem('different_place')
