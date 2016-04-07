d88d4e401133a7aabb551bb136cf921425361f1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 12:35:03 -0400

    "mix: come super/go_small_child_in_man/public_company_or_old_week/long_thing/important_company.py to bad child"

---
+++ super/go_small_child_in_man/public_company_or_old_week/long_thing/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    world(str_arg)
+    print('way')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('child')
