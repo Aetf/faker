7661a99201db19a5983b10f14a4263bb295e0153
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 11:38:59 -0400

    "add: find super/take_child/important_time_and_eye/ask_little_fact/case/have_little_time/man.py or super/way.py in own company"

---
+++ super/take_child/important_time_and_eye/ask_little_fact/case/have_little_time/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child(str_arg):
+    last_group(str_arg)
+    print('world')
+
+def last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child('little_government')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    government(str_arg)
+    print('next_fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('part')
