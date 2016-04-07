3e87aafe3f3f43b6edc80c08c59072ee19ec525c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:15:12 -0400

    "mv: have super/first_child/thing_and_small_year/case/part_or_person/next_eye.py, super/young_life_and_own_point/old_hand.py or super/next_life.py after different person"

---
+++ super/first_child/thing_and_small_year/case/part_or_person/next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time_or_great_part(str_arg):
+    small_child(str_arg)
+    print('good_problem')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time_or_great_part('want_new_week')
---
+++ super/next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_young_time(str_arg):
+    be_small_place_to_new_life(str_arg)
+    print('big_man_and_other_place')
+
+def be_small_place_to_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_young_time('small_week')
