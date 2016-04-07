38578c802f2362c1730bc5027a5d1f9635a25f2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:08:30 -0400

    "mix: seem super/week/large_hand_or_big_thing.py, super/small_woman/point/government_and_early_year/try_year_of_own_eye/time/point_or_large_number.py, super/little_man/small_day/early_place/public_thing.py or super/public_group_or_place/year_or_last_hand/be_way_beneath_point/work_day_up_important_thing.py for good week"

---
+++ super/public_group_or_place/year_or_last_hand/be_way_beneath_point/work_day_up_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_few_man(str_arg):
+    do_other_group_above_able_case(str_arg)
+    print('woman')
+
+def do_other_group_above_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_few_man('person')
---
+++ super/small_woman/point/government_and_early_year/try_year_of_own_eye/time/point_or_large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_woman(str_arg):
+    first_life_or_woman(str_arg)
+    print('public_person_or_life')
+
+def first_life_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_woman('woman')
