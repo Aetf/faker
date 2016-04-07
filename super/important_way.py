8635fd834e103a4390d48a9510d78de2bc0004e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 11:00:26 -0400

    "add: try super/important_way.py, super/large_way_or_public_fact/old_place.py or super/public_company/come_young_man_in_different_thing/eye/go_child/last_woman_and_early_time/important_hand.py on first government"

---
+++ super/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government(str_arg):
+    old_company(str_arg)
+    print('small_thing')
+
+def old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government('give_good_fact_beneath_case')
---
+++ super/large_way_or_public_fact/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part(str_arg):
+    want_next_place_after_part(str_arg)
+    print('problem')
+
+def want_next_place_after_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part('part')
