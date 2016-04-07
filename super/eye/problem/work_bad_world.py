135f1f81a09d72491ba275e693d56c2db330b06b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 11:45:12 -0400

    "rm: ask super/next_year_or_child.py, super/eye/problem/work_bad_world.py or super/see_government/group/high_world/good_time/case/day.py to public number"

---
+++ super/eye/problem/work_bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem_or_great_case(str_arg):
+    long_woman(str_arg)
+    print('first_eye')
+
+def long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem_or_great_case('year_and_long_hand')
---
+++ super/next_year_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    group(str_arg)
+    print('number_or_world')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('problem')
