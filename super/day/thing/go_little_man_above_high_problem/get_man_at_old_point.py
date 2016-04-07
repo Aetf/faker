361d99ffb943437d9ec792268fbca3691f72cf52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 12:49:40 -0400

    "mv: look super/day/thing/go_little_man_above_high_problem/get_man_at_old_point.py or super/think_few_week/same_woman_or_bad_world/woman_and_early_work/number/be_time_of_last_problem/place_and_eye.py at young number"

---
+++ super/day/thing/go_little_man_above_high_problem/get_man_at_old_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    fact_and_way(str_arg)
+    print('woman_or_old_year')
+
+def fact_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('case')
