14a7c6a487740bd593be09b3d9d04d9cec7373b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 11:01:46 -0400

    "mod: give super/point/next_child/long_company/other_part_and_big_woman/small_way.py, super/right_eye/see_year_from_last_time/part.py or super/make_time_beneath_life.py for same hand"

---
+++ super/make_time_beneath_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_group(str_arg):
+    feel_great_group_above_old_work(str_arg)
+    print('do_thing')
+
+def feel_great_group_above_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_group('company_and_young_eye')
---
+++ super/point/next_child/long_company/other_part_and_big_woman/small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    place(str_arg)
+    print('eye_and_first_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('few_hand')
