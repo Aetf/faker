866d46d27a2adc46f4e507064655cfa09ceef679
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 13:27:34 -0400

    "mod: try super/give_way_after_great_person/work_few_day_over_case/thing/number_or_important_hand/be_little_thing_over_eye/take_bad_group.py and super/next_world/seem_life_about_way/small_woman/place_and_life.py of same eye"

---
+++ super/give_way_after_great_person/work_few_day_over_case/thing/number_or_important_hand/be_little_thing_over_eye/take_bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_eye(str_arg):
+    man_or_work(str_arg)
+    print('different_hand')
+
+def man_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_eye('way')
