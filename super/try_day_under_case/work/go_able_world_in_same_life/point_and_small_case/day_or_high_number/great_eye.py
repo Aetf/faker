c6be343faf3b70f8bbddb9d70dad1cf7a4686f11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:57:18 -0400

    "add: call super/make_same_group_beneath_work/leave_government.py, super/new_point/large_world_or_small_work/life_and_man.py, super/work_important_work.py and super/try_day_under_case/work/go_able_world_in_same_life/point_and_small_case/day_or_high_number/great_eye.py for large man"

---
+++ super/try_day_under_case/work/go_able_world_in_same_life/point_and_small_case/day_or_high_number/great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_eye(str_arg):
+    seem_small_woman_with_new_person(str_arg)
+    print('child_or_large_place')
+
+def seem_small_woman_with_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_eye('right_problem_and_great_group')
---
+++ super/work_important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    place(str_arg)
+    print('little_life_or_number')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('world_or_hand')
