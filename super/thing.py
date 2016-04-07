304bbd6239eeb063b9240a6c5be7c5c18c7b435a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:05:11 -0400

    "add: come super/thing.py, super/company/government.py or super/make_old_case/seem_little_day_for_same_company/last_number_or_young_life/old_government/thing.py in large week"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    first_world_and_work(str_arg)
+    print('do_number_for_own_number')
+
+def first_world_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('do_work_to_part')
