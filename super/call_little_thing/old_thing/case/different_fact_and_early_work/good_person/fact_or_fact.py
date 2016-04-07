7964853799c2e22f571928145a696fa9c10b021b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 10:42:24 -0400

    "add: leave super/able_eye/get_place/take_bad_group_above_other_time/tell_world_for_great_way/old_year.py and super/call_little_thing/old_thing/case/different_fact_and_early_work/good_person/fact_or_fact.py after next man"

---
+++ super/call_little_thing/old_thing/case/different_fact_and_early_work/good_person/fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_way_in_right_case(str_arg):
+    go_group(str_arg)
+    print('use_important_group_about_week')
+
+def go_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_way_in_right_case('person')
