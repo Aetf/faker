46735b0a3bd2b76baeaf7633e589b89f57e4ed23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 10:34:08 -0400

    "rm: see super/person/point_and_world/high_place/own_life/great_problem_and_young_company/leave_group_with_eye.py and super/week.py after small company"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    work(str_arg)
+    print('thing')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('feel_new_hand_to_fact')
