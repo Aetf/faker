b3c5464c2a30614642ee06c3f44a77f2d7f8f92c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:26:36 -0400

    "mod: take super/government/way/way/long_world_and_week.py above first company"

---
+++ super/government/way/way/long_world_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    case_or_big_case(str_arg)
+    print('public_way')
+
+def case_or_big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('group_or_week')
