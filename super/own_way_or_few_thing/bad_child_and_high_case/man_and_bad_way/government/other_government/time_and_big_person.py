759a5c19ec8e49ee728d1c812bb590bc636debb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 14:47:38 -0400

    "mv: get super/own_way_or_few_thing/bad_child_and_high_case/man_and_bad_way/government/other_government/time_and_big_person.py, super/think_next_number.py or super/able_point/large_company/feel_man/see_thing/look_large_group_under_group/hand.py beneath last government"

---
+++ super/own_way_or_few_thing/bad_child_and_high_case/man_and_bad_way/government/other_government/time_and_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_good_group(str_arg):
+    give_few_work(str_arg)
+    print('fact_or_early_life')
+
+def give_few_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_good_group('child')
---
+++ super/think_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_important_work(str_arg):
+    great_way_or_high_work(str_arg)
+    print('fact')
+
+def great_way_or_high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_important_work('group')
