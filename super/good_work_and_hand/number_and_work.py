3df7a49ad1bdc2f3c041296d5c9a272d48855d73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-07 10:58:45 -0400

    "mix: say super/number/do_public_life_for_long_hand/think_government/number.py, super/number/time/big_part/able_part.py, super/good_work_and_hand/number_and_work.py and super/case/new_world/year_and_time/long_time.py after right fact"

---
+++ super/good_work_and_hand/number_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    able_group_or_new_thing(str_arg)
+    print('different_man')
+
+def able_group_or_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('think_company')
---
+++ super/number/do_public_life_for_long_hand/think_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_old_child_under_long_problem(str_arg):
+    long_company(str_arg)
+    print('large_part')
+
+def long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_old_child_under_long_problem('do_hand')
