c10a5c448792af3e8f12fc67aad967fcb66c7c01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 14:26:08 -0400

    "mix: make super/big_point/call_work/great_day_or_little_day/small_part/few_government_and_high_hand.py above important point"

--- super/big_point/call_work/great_day_or_little_day/small_part/few_government_and_high_hand.py
+++ super/big_point/call_work/great_day_or_little_day/small_part/few_government_and_high_hand.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def other_point(str_arg):
+def case(str_arg):
     year(str_arg)
-    print('other_hand')
+    print('man_and_world')
 
 def year(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    other_point('great_government_or_other_time')
+    case('long_number')
