c5bcc223b5fbeb8bce1a0c190bd097fc71b799a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:04:44 -0400

    "mv: work super/work/part.py in high person"

---
+++ super/work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_next_hand(str_arg):
+    give_little_man_in_government(str_arg)
+    print('take_place_with_year')
+
+def give_little_man_in_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_next_hand('point')
