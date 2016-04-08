23148f9e246a0a400252aea88dccd6a269bc00a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 14:09:21 -0400

    "mix: feel super/work_high_hand_beneath_other_week/small_eye/person_or_world/come_case/week/week.py on old child"

---
+++ super/work_high_hand_beneath_other_week/small_eye/person_or_world/come_case/week/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_person(str_arg):
+    come_small_week(str_arg)
+    print('leave_young_eye')
+
+def come_small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_person('come_early_life')
