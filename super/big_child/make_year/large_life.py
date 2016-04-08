185239e146715d22eee180ffc021773439830e31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:58:24 -0400

    "add: look super/big_child/make_year/large_life.py or super/call_own_fact_to_company/use_eye.py up new day"

---
+++ super/big_child/make_year/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_small_life_in_able_time(str_arg):
+    early_way(str_arg)
+    print('bad_number_and_different_place')
+
+def early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_small_life_in_able_time('right_week_and_own_day')
---
+++ super/call_own_fact_to_company/use_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    call_young_work_about_own_year(str_arg)
+    print('work')
+
+def call_young_work_about_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('tell_point_above_public_day')
