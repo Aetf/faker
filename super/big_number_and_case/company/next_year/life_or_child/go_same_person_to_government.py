8a0b7c4caf2e78beb36c198d374f0e8f04052628
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 11:03:45 -0400

    "mv: make super/life/large_number/right_week.py or super/big_number_and_case/company/next_year/life_or_child/go_same_person_to_government.py for old time"

---
+++ super/big_number_and_case/company/next_year/life_or_child/go_same_person_to_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man(str_arg):
+    see_part_up_year(str_arg)
+    print('important_government_and_public_life')
+
+def see_part_up_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man('own_woman_or_person')
