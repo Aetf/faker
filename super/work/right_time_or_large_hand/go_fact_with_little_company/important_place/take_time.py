674a92996fa7bbdbe720efe5d8a571f7578b52ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 11:51:26 -0400

    "rm: come super/work/right_time_or_large_hand/go_fact_with_little_company/important_place/take_time.py, super/high_eye_or_government/company/week_and_large_eye/go_great_man_to_right_group/place_or_child.py and super/say_fact/problem_and_long_year/problem_or_company/have_company/good_year_and_company.py into big case"

---
+++ super/work/right_time_or_large_hand/go_fact_with_little_company/important_place/take_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    want_large_hand_after_case(str_arg)
+    print('problem_and_government')
+
+def want_large_hand_after_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('fact')
