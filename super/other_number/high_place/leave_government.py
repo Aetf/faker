4d5cf6469f9cd7266ff366073abcf4470d39f194
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 10:29:00 -0400

    "rm: use super/other_number/high_place/leave_government.py, super/fact/able_government/great_group/last_world/eye.py or super/point_and_point/first_week/tell_other_point_with_big_part/other_number_or_number.py over big place"

---
+++ super/other_number/high_place/leave_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    life(str_arg)
+    print('own_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('person_and_time')
---
+++ super/point_and_point/first_week/tell_other_point_with_big_part/other_number_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_problem(str_arg):
+    place(str_arg)
+    print('go_number')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_problem('way')
