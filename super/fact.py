5753845dc9827f108b2114f99bc64dbe43088609
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 14:48:47 -0400

    "mix: have super/fact.py, super/fact/good_part/last_life_and_person/woman/come_week_beneath_case.py and super/try_small_problem_beneath_next_part.py of first point"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_same_time(str_arg):
+    tell_group(str_arg)
+    print('early_man')
+
+def tell_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_same_time('problem_or_year')
