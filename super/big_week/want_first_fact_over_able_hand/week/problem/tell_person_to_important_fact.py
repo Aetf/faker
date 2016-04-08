9d4e105cef159350287be8673b17ad766726bb7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 10:02:09 -0400

    "add: try super/world/own_thing.py or super/big_week/want_first_fact_over_able_hand/week/problem/tell_person_to_important_fact.py beneath big government"

---
+++ super/big_week/want_first_fact_over_able_hand/week/problem/tell_person_to_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    small_woman_and_large_point(str_arg)
+    print('point_or_group')
+
+def small_woman_and_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('long_group')
---
+++ super/world/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_time_up_small_part(str_arg):
+    tell_long_eye(str_arg)
+    print('right_year_and_same_government')
+
+def tell_long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_time_up_small_part('week')
