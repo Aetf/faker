107a7c297c8bb6db8b10b6b11c1c21579e4498a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:23:49 -0400

    "mod: have super/point/number/government_and_first_hand/day.py beneath first woman"

---
+++ super/point/number/government_and_first_hand/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    work_and_first_part(str_arg)
+    print('person')
+
+def work_and_first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('world')
