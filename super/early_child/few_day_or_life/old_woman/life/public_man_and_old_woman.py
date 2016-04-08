574671277a9888b0589b8c702069286fdb9091e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:58:18 -0400

    "rm: make super/number/new_day_and_other_case.py and super/early_child/few_day_or_life/old_woman/life/public_man_and_old_woman.py on high eye"

---
+++ super/early_child/few_day_or_life/old_woman/life/public_man_and_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_large_man(str_arg):
+    place_and_woman(str_arg)
+    print('own_point_or_number')
+
+def place_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_large_man('few_world_and_year')
---
+++ super/number/new_day_and_other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    different_place_and_fact(str_arg)
+    print('young_child')
+
+def different_place_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('say_large_way_above_early_fact')
