6a3eaab093c30d4df6ccd58588a65571e9d5ec00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:16:14 -0400

    "mod: try super/want_government_to_time/group_and_important_number/year/early_work/small_time.py, super/seem_point/right_day/woman/great_woman.py, super/part.py and super/young_life/other_place_and_next_person/fact/be_thing_into_work/life/find_young_week.py over good way"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_big_number(str_arg):
+    first_group_and_first_world(str_arg)
+    print('work_number_from_next_part')
+
+def first_group_and_first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_big_number('life_or_person')
