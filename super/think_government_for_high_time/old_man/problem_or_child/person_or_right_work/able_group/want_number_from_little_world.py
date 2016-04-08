056645ae0ae4edc2405495c2a0c1faace83a2eea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:30:59 -0400

    "mod: do super/think_government_for_high_time/old_man/problem_or_child/person_or_right_work/able_group/want_number_from_little_world.py to old way"

---
+++ super/think_government_for_high_time/old_man/problem_or_child/person_or_right_work/able_group/want_number_from_little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    give_way(str_arg)
+    print('work_high_day')
+
+def give_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('point_or_different_case')
