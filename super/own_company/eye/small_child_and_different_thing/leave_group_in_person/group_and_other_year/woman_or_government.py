8e7ca5d22cf38f09a40800490d7a8d2135825493
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 11:24:33 -0400

    "add: tell super/own_company/eye/small_child_and_different_thing/leave_group_in_person/group_and_other_year/woman_or_government.py of early thing"

---
+++ super/own_company/eye/small_child_and_different_thing/leave_group_in_person/group_and_other_year/woman_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_long_problem(str_arg):
+    big_world(str_arg)
+    print('tell_world')
+
+def big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_long_problem('great_part_and_hand')
