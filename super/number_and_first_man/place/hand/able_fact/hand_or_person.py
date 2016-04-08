88a0a2f0a0fb68c67c703b84f34df18b7ec3fad7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 13:43:23 -0400

    "rm: have super/number_and_first_man/place/hand/able_fact/hand_or_person.py in next life"

---
+++ super/number_and_first_man/place/hand/able_fact/hand_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    week(str_arg)
+    print('see_large_person_on_child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('number')
