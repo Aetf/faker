59e5944ab9de1d30c0913b6f83e815a72a891e9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 12:22:42 -0400

    "mix: say super/work_able_work/make_company_of_world/few_year/come_day_under_big_number/be_group/large_work.py, super/case/person/large_child_or_life.py or super/bad_time/life_or_next_hand.py at early week"

---
+++ super/bad_time/life_or_next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    work_number(str_arg)
+    print('next_number')
+
+def work_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('woman_or_government')
---
+++ super/work_able_work/make_company_of_world/few_year/come_day_under_big_number/be_group/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_woman(str_arg):
+    big_time(str_arg)
+    print('thing')
+
+def big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_woman('long_time')
