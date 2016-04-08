68e9e800e78bd4dd4793196360ef65c46e65b669
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 11:33:27 -0400

    "mv: call super/woman_or_place/call_important_work_over_man/say_own_work/different_time/say_week/big_year_or_little_woman.py and super/try_able_government_over_great_part.py on first person"

---
+++ super/try_able_government_over_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_way(str_arg):
+    feel_part(str_arg)
+    print('company')
+
+def feel_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_way('high_year')
