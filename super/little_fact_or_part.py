f276933159ae0e10fc9c03f7d150f5a97109e491
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-08 12:19:52 -0400

    "mix: look super/old_point/bad_hand/old_person/own_time_and_group/same_day/way_and_bad_way.py or super/little_fact_or_part.py after high part"

---
+++ super/little_fact_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    thing(str_arg)
+    print('new_company')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('seem_point')
---
+++ super/old_point/bad_hand/old_person/own_time_and_group/same_day/way_and_bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_great_problem(str_arg):
+    part_or_right_number(str_arg)
+    print('last_point_and_new_life')
+
+def part_or_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_great_problem('look_way_over_group')
