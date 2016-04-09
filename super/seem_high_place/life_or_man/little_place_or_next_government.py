e8f8c4ac2725ed0afdf3b9b20db6ba36cbdb067a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:29:38 -0400

    "mix: do super/seem_high_place/life_or_man/little_place_or_next_government.py, super/fact_and_number/ask_same_point_to_child/problem/able_part/old_life/place.py, super/long_woman_and_old_fact/next_eye/next_number_or_bad_hand.py or super/old_year/do_last_point_from_same_way/eye.py under old week"

---
+++ super/seem_high_place/life_or_man/little_place_or_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_problem_at_long_company(str_arg):
+    able_work(str_arg)
+    print('few_number')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_problem_at_long_company('important_work')
