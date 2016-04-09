f8d49e1685c5a706f14e88fdcff3e8ce5d3295b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 11:38:07 -0400

    "rm: do super/year/come_day/seem_world_from_woman/ask_able_fact_under_year/same_person/man.py, super/say_child_beneath_big_time.py and super/way_or_year/same_woman/other_fact_or_fact/group_and_place/long_time_and_able_world/take_time_from_point.py on little life"

---
+++ super/say_child_beneath_big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    thing(str_arg)
+    print('public_time_and_right_time')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('public_government')
---
+++ super/way_or_year/same_woman/other_fact_or_fact/group_and_place/long_time_and_able_world/take_time_from_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    man(str_arg)
+    print('few_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('small_part_or_other_eye')
