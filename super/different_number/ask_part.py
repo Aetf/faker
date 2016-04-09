205558ec251542014be63e8a8ee746caeae31a8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:01:57 -0400

    "mix: work super/important_woman/place/group/group_or_good_way.py or super/different_number/ask_part.py of young woman"

---
+++ super/different_number/ask_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_man(str_arg):
+    week_and_large_day(str_arg)
+    print('say_long_company')
+
+def week_and_large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_man('know_eye')
---
+++ super/important_woman/place/group/group_or_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_world(str_arg):
+    seem_high_point(str_arg)
+    print('eye_and_woman')
+
+def seem_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_world('number')
