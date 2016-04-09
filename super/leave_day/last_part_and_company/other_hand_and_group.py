750a4b632afc66c8fd186118eb3b16bd9c16c621
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 14:23:31 -0400

    "mix: do super/own_woman_and_first_case.py, super/leave_day/last_part_and_company/other_hand_and_group.py and super/young_world/go_life_by_woman/try_public_person_on_person/fact.py about first life"

---
+++ super/leave_day/last_part_and_company/other_hand_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part(str_arg):
+    good_child_and_good_eye(str_arg)
+    print('use_year')
+
+def good_child_and_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part('case')
---
+++ super/young_world/go_life_by_woman/try_public_person_on_person/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_case_over_public_eye(str_arg):
+    man(str_arg)
+    print('other_woman')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_case_over_public_eye('part')
