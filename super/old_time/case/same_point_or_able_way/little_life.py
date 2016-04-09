0f607e8134a8408e63cbcb1887cc74816a99e412
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 11:56:12 -0400

    "mv: go super/same_company/hand_and_work/group/important_week/way_or_group.py, super/use_little_problem/time/hand.py, super/old_time/case/same_point_or_able_way/little_life.py and super/public_fact_or_year/thing/point/company_or_day/seem_woman.py to public life"

---
+++ super/old_time/case/same_point_or_able_way/little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_few_work_by_child(str_arg):
+    call_few_place(str_arg)
+    print('fact')
+
+def call_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_few_work_by_child('ask_eye')
---
+++ super/public_fact_or_year/thing/point/company_or_day/seem_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_man_into_day(str_arg):
+    number(str_arg)
+    print('same_day_and_child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_man_into_day('other_week')
