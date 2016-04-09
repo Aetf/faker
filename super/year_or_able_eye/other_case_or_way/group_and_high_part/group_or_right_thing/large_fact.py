7050b659a30f15aecd92442a9f19827bae08ee1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 13:50:00 -0400

    "mv: want super/great_world.py, super/year_or_able_eye/other_case_or_way/group_and_high_part/group_or_right_thing/large_fact.py and super/world.py at other life"

---
+++ super/year_or_able_eye/other_case_or_way/group_and_high_part/group_or_right_thing/large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place_and_thing(str_arg):
+    do_little_case(str_arg)
+    print('early_week')
+
+def do_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place_and_thing('person')
