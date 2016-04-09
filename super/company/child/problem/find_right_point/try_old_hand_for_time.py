54a4d7c2e4a5f97820436dfaa08228f9d796f1c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:38:49 -0400

    "mix: be super/company/child/problem/find_right_point/try_old_hand_for_time.py, super/place/last_company.py and super/own_man_and_group/problem.py after good year"

---
+++ super/company/child/problem/find_right_point/try_old_hand_for_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company_and_life(str_arg):
+    other_place(str_arg)
+    print('case')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company_and_life('fact_or_fact')
---
+++ super/place/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case(str_arg):
+    last_way(str_arg)
+    print('week')
+
+def last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case('high_week')
