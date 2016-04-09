2bd5f10a2cbffd1ab49b3e6842f4857a25f621e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 11:18:57 -0400

    "mix: have super/problem/first_problem/hand/company/bad_fact.py, super/leave_place/right_work/long_place_and_great_eye/call_early_point_above_eye/take_big_point.py, super/call_few_fact/hand/last_day_and_man/bad_woman_and_same_eye/own_time/say_way_into_important_hand.py or super/place.py about big year"

---
+++ super/leave_place/right_work/long_place_and_great_eye/call_early_point_above_eye/take_big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_day_or_other_day(str_arg):
+    important_problem(str_arg)
+    print('early_number')
+
+def important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_day_or_other_day('fact')
