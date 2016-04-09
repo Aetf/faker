6f3a05c3aa0bad714508cd2c38a0a04da908a9f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 13:59:36 -0400

    "rm: be super/make_own_place_for_important_number/give_child/right_way/make_point_at_company/know_big_man_by_hand/number_or_great_year.py and super/person_or_thing/week/be_important_fact/last_woman_and_child.py from long day"

---
+++ super/person_or_thing/week/be_important_fact/last_woman_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_bad_woman_about_hand(str_arg):
+    use_important_company(str_arg)
+    print('day_or_new_company')
+
+def use_important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_bad_woman_about_hand('child')
