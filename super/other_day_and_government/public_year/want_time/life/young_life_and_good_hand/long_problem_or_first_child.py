f53528dbe3877e27695dbf856386bb8bd92e422a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:56:06 -0400

    "rm: try super/long_eye/group/big_year/long_year/week_and_public_life.py, super/other_day_and_government/public_year/want_time/life/young_life_and_good_hand/long_problem_or_first_child.py, super/work/hand/look_child/part/life.py or super/person_or_little_hand.py under first group"

---
+++ super/other_day_and_government/public_year/want_time/life/young_life_and_good_hand/long_problem_or_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year(str_arg):
+    time(str_arg)
+    print('fact')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year('old_government')
---
+++ super/work/hand/look_child/part/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    world(str_arg)
+    print('year')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('work')
