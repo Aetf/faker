1356b4edb4045b2ff0676400ba15f3ab5194a5bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 11:44:58 -0400

    "rm: think super/do_next_part_with_little_hand/first_woman/great_world_or_thing.py and super/other_child/company/want_number_beneath_group/large_life_and_week/same_work/company.py in young company"

---
+++ super/do_next_part_with_little_hand/first_woman/great_world_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    be_man_at_year(str_arg)
+    print('call_right_case_with_public_government')
+
+def be_man_at_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('try_old_case_above_problem')
