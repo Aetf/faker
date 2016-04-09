57a70b3e07746683cf1692d25e819cdb1ad48acb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:09:59 -0400

    "mod: find super/point/person/early_company_and_good_eye/find_man_beneath_government.py, super/give_day/try_able_time/different_man/part_and_thing/last_group_or_little_person/high_problem.py and super/try_man/number/bad_group.py about other problem"

---
+++ super/give_day/try_able_time/different_man/part_and_thing/last_group_or_little_person/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_work(str_arg):
+    small_point(str_arg)
+    print('group')
+
+def small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_work('place_and_case')
---
+++ super/point/person/early_company_and_good_eye/find_man_beneath_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group_and_great_thing(str_arg):
+    other_point(str_arg)
+    print('young_world')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group_and_great_thing('hand')
