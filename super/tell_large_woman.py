6f317018f4d75f9bd5c8a1026e40091bcaa12b36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:34:54 -0400

    "rm: take super/tell_large_woman.py above old person"

---
+++ super/tell_large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man_or_big_way(str_arg):
+    large_way(str_arg)
+    print('different_day')
+
+def large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man_or_big_way('important_year_and_fact')
