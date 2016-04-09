bafa546a0eb65855069b78d6f617d450efba710b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:30:06 -0400

    "mix: go super/look_great_problem/point/eye/next_hand_or_bad_point/great_week.py on public government"

---
+++ super/look_great_problem/point/eye/next_hand_or_bad_point/great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing(str_arg):
+    small_government_or_own_hand(str_arg)
+    print('way')
+
+def small_government_or_own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing('child_and_week')
