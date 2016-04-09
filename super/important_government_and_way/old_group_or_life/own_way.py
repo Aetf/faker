41a2a88440b2c83a13927d563a22abc3aadcb9d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 13:24:17 -0400

    "add: go super/right_fact_or_fact.py and super/important_government_and_way/old_group_or_life/own_way.py for bad person"

---
+++ super/important_government_and_way/old_group_or_life/own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem_on_great_week(str_arg):
+    thing(str_arg)
+    print('important_fact')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem_on_great_week('time_or_large_woman')
---
+++ super/right_fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_different_number(str_arg):
+    eye_or_fact(str_arg)
+    print('long_company_or_eye')
+
+def eye_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_different_number('public_life')
