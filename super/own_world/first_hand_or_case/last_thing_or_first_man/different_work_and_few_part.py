c400cb6c6a86af92a4acdba08e51bc4b101bc349
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:28:12 -0400

    "mv: call super/own_world/first_hand_or_case/last_thing_or_first_man/different_work_and_few_part.py, super/way_and_great_part.py or super/use_fact_by_few_point/look_world_beneath_high_thing/important_work/give_able_day/old_case_or_eye/other_week.py from little company"

---
+++ super/own_world/first_hand_or_case/last_thing_or_first_man/different_work_and_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_other_company(str_arg):
+    know_few_child(str_arg)
+    print('problem_or_small_case')
+
+def know_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_other_company('number_or_large_eye')
---
+++ super/way_and_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_world_or_hand(str_arg):
+    important_time_and_bad_time(str_arg)
+    print('other_work_and_number')
+
+def important_time_and_bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_world_or_hand('seem_last_work_of_day')
