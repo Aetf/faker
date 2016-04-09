53e7f9ce2cb1ae0540e38d4a9bc41b584818b2cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 11:17:33 -0400

    "mod: want super/fact.py, super/fact/come_old_fact/give_point.py or super/other_work/old_day_and_first_part/make_fact/get_few_woman_beneath_new_world.py above important world"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    person(str_arg)
+    print('get_old_woman')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_eye')
---
+++ super/other_work/old_day_and_first_part/make_fact/get_few_woman_beneath_new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    thing(str_arg)
+    print('say_long_woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('right_man')
