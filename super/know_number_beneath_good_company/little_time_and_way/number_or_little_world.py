5c022a50fd4fbc02c71273aff6acb41587d85aa8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:42:47 -0400

    "mix: feel super/number.py, super/know_number_beneath_good_company/little_time_and_way/number_or_little_world.py and super/have_great_fact/world.py up early life"

---
+++ super/know_number_beneath_good_company/little_time_and_way/number_or_little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    do_number(str_arg)
+    print('place')
+
+def do_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('person')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_young_point(str_arg):
+    life_or_life(str_arg)
+    print('child')
+
+def life_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_young_point('bad_child_or_able_company')
