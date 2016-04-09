6ffd7d9862192eef39b9d8e932d231985bc6f0e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:47:33 -0400

    "mod: see super/time_or_company/do_small_time_in_large_world/call_few_child.py beneath old government"

---
+++ super/time_or_company/do_small_time_in_large_world/call_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    new_case(str_arg)
+    print('go_day_up_government')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('work_or_high_eye')
