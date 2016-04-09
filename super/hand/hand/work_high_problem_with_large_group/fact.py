382c56615e4fe4dd7c0535b5a87ebdf01685d492
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 12:10:52 -0400

    "mod: want super/hand/hand/work_high_problem_with_large_group/fact.py by own thing"

---
+++ super/hand/hand/work_high_problem_with_large_group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    thing(str_arg)
+    print('point_or_long_number')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('take_part_above_place')
