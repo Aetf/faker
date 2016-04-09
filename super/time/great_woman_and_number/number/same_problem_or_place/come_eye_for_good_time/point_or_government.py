de83c607cc6fd8790a674fdcc8d48cc61a2983a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:50:36 -0400

    "mix: think super/hand_and_year/day/right_year_or_eye/woman.py, super/time/public_world/use_old_person_about_own_eye.py and super/time/great_woman_and_number/number/same_problem_or_place/come_eye_for_good_time/point_or_government.py up early point"

---
+++ super/time/great_woman_and_number/number/same_problem_or_place/come_eye_for_good_time/point_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    seem_way_for_thing(str_arg)
+    print('go_young_man_of_important_number')
+
+def seem_way_for_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('child_or_public_eye')
---
+++ super/time/public_world/use_old_person_about_own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_high_problem(str_arg):
+    bad_hand(str_arg)
+    print('little_problem')
+
+def bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_high_problem('feel_last_company')
