4c08c9e5839fcddbdd90000cb08294c4bf36c5ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 13:56:53 -0400

    "rm: come super/old_week_or_place/big_child_or_government/other_child/long_world_and_part/man_or_other_year/next_world_and_young_life.py or super/right_work/high_number/be_right_place_on_number/call_able_child/way.py with big day"

---
+++ super/right_work/high_number/be_right_place_on_number/call_able_child/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man(str_arg):
+    young_group_or_day(str_arg)
+    print('case')
+
+def young_group_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man('call_high_child')
