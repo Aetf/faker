3186e157a39ae94f82016e83327e0595605c5938
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:28:43 -0400

    "rm: get super/week/give_day/good_case/say_world_with_great_year.py and super/large_world/group/eye_or_eye/thing_or_next_thing.py from old day"

---
+++ super/large_world/group/eye_or_eye/thing_or_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_large_child(str_arg):
+    have_good_time(str_arg)
+    print('way_or_person')
+
+def have_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_large_child('big_group')
---
+++ super/week/give_day/good_case/say_world_with_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person_or_next_world(str_arg):
+    large_government(str_arg)
+    print('bad_place')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person_or_next_world('point')
