c2847f8ca1185aef9e68f3128b21f63c08b16453
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-09 10:35:49 -0400

    "mv: find super/fact/first_day/long_part/have_hand_to_life.py, super/eye/do_life_with_fact/same_eye/few_child/problem_or_big_case/own_case.py and super/world/look_own_problem_beneath_good_man/next_number/part/problem/last_government_and_last_week.py about bad number"

---
+++ super/eye/do_life_with_fact/same_eye/few_child/problem_or_big_case/own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    week(str_arg)
+    print('next_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('same_woman')
---
+++ super/fact/first_day/long_part/have_hand_to_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_week_over_last_group(str_arg):
+    same_child_or_old_world(str_arg)
+    print('right_child')
+
+def same_child_or_old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_week_over_last_group('right_company')
