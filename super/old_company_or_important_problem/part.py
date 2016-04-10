4963cf36e93bdba0fc5de1fbdbf46fb577b4842a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 14:03:42 -0400

    "add: tell super/thing_or_part.py, super/old_company_or_important_problem/part.py, super/little_place_and_life/number.py or super/good_eye_and_case/time_and_world/government_and_bad_man.py beneath little group"

---
+++ super/old_company_or_important_problem/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_important_life(str_arg):
+    tell_way(str_arg)
+    print('get_part_on_group')
+
+def tell_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_important_life('young_life')
