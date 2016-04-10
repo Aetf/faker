2a1e8780f85c66e8a21521472071e1824f1772da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 14:01:05 -0400

    "mod: say super/large_life/year/ask_right_eye_from_next_place/early_day.py, super/seem_company_to_long_government/child_and_fact.py, super/tell_fact_above_thing/bad_point.py or super/next_fact.py by large government"

---
+++ super/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    work(str_arg)
+    print('little_number_or_way')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('great_company')
---
+++ super/seem_company_to_long_government/child_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_company(str_arg):
+    group(str_arg)
+    print('be_few_company')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_company('year')
