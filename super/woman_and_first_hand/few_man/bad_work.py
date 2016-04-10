bb12e57471e4981a615428eb245d24484c365830
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 10:13:13 -0400

    "rm: come super/woman_and_first_hand/few_man/bad_work.py about little time"

---
+++ super/woman_and_first_hand/few_man/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    able_hand_and_eye(str_arg)
+    print('own_government_and_small_person')
+
+def able_hand_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('feel_world')
