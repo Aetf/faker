2e1b6fadefd58008a55435cb48e76404e86c2530
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 10:36:17 -0400

    "mix: have super/public_fact/important_world/day_or_next_way.py, super/do_week_with_public_person/young_number/public_hand_and_new_child/case_or_eye/have_public_way_above_year.py, super/old_part_or_good_fact/say_good_child_in_big_point/know_person_after_high_week/point/place/group.py and super/first_person/long_fact_or_large_point/first_child/young_woman/company.py above last year"

---
+++ super/first_person/long_fact_or_large_point/first_child/young_woman/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_person(str_arg):
+    place_or_child(str_arg)
+    print('way')
+
+def place_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_person('early_case')
---
+++ super/old_part_or_good_fact/say_good_child_in_big_point/know_person_after_high_week/point/place/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_year_and_work(str_arg):
+    company(str_arg)
+    print('right_eye')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_year_and_work('take_work_beneath_part')
