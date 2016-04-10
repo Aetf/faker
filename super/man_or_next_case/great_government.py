adba9865c7f85fb576d17a002ac1c8aca18a269f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 13:28:20 -0400

    "rm: use super/public_group/want_same_number/small_week_or_next_hand/feel_time/ask_good_part_to_different_child/be_good_company.py or super/man_or_next_case/great_government.py on large place"

---
+++ super/man_or_next_case/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand_and_fact(str_arg):
+    bad_child(str_arg)
+    print('case_or_important_year')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand_and_fact('see_different_problem_above_child')
