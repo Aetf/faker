c6375b209323664fc5152a274e208559b462997d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 14:58:22 -0400

    "mv: come super/think_group_up_last_child/right_man.py, super/other_number_or_big_woman/next_day/bad_company.py and super/important_woman/problem.py from public fact"

---
+++ super/important_woman/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    small_company(str_arg)
+    print('own_eye')
+
+def small_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('point')
---
+++ super/other_number_or_big_woman/next_day/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    company(str_arg)
+    print('place')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('other_man')
