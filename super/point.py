0340039bdbc4c1ee125768ad124d075abdbfc482
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 10:07:59 -0400

    "rm: work super/person/large_person_or_week/man.py, super/person/little_way_or_first_part/important_year_or_little_world/seem_bad_eye_at_able_government/day/next_world.py or super/point.py over great point"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_old_child(str_arg):
+    work_week(str_arg)
+    print('few_way_or_world')
+
+def work_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_old_child('give_public_life')
