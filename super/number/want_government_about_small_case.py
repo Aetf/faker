f396b9dbb29f7c2cb237fa12e436d89d6776ecc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 12:53:10 -0400

    "mv: think super/take_early_place.py or super/number/want_government_about_small_case.py about same year"

---
+++ super/number/want_government_about_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_government(str_arg):
+    way(str_arg)
+    print('able_life')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_government('get_old_group_with_child')
---
+++ super/take_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number_and_bad_thing(str_arg):
+    world(str_arg)
+    print('high_woman')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number_and_bad_thing('same_way')
