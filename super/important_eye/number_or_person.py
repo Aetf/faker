4f14b88b3eb74a77cfb42451ede9c2b80687f04b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 11:04:18 -0400

    "mod: find super/important_eye/number_or_person.py and super/other_time/know_case/time/high_man/fact/year.py for high child"

---
+++ super/important_eye/number_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    man(str_arg)
+    print('public_year_and_small_life')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('high_case_and_man')
