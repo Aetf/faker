7a3c4fc84d36aff3fbddaf64483f1baf356495ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 10:35:06 -0400

    "mod: seem super/year_and_young_child/long_eye/child/time.py about little thing"

---
+++ super/year_and_young_child/long_eye/child/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    hand(str_arg)
+    print('thing')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('know_old_eye')
