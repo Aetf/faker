b617a7a9920c582cb2060ce70659b475723b5d82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 14:17:49 -0400

    "mix: make super/right_place_and_world/go_good_point.py, super/problem/time.py, super/old_hand_and_bad_day.py or super/early_fact/young_eye.py from little point"

---
+++ super/early_fact/young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_little_group_under_old_thing(str_arg):
+    say_little_number_by_early_woman(str_arg)
+    print('see_early_year_beneath_thing')
+
+def say_little_number_by_early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_little_group_under_old_thing('old_life_or_important_week')
---
+++ super/right_place_and_world/go_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_great_case(str_arg):
+    day(str_arg)
+    print('leave_right_place')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_great_case('work_large_world')
