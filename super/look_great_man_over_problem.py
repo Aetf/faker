a18a7f00ba45fdf23e626d154b4c4eef163b4bbd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 13:47:24 -0400

    "rm: ask super/look_great_man_over_problem.py and super/same_woman/young_point/take_important_point/public_woman/next_man/bad_life.py after able fact"

---
+++ super/look_great_man_over_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number(str_arg):
+    go_man_with_great_problem(str_arg)
+    print('other_fact')
+
+def go_man_with_great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number('few_thing')
