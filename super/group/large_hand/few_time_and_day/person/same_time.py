d774b090763d87787e25eb6ff93b38432bc2d7fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 12:12:23 -0400

    "mod: leave super/group/large_hand/few_time_and_day/person/same_time.py and super/public_thing_or_few_fact/get_last_woman_under_next_part/able_world/go_early_time.py with early life"

---
+++ super/group/large_hand/few_time_and_day/person/same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    good_point(str_arg)
+    print('get_week_into_big_place')
+
+def good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('find_thing')
