6eda54e0147a926c41f380389c344bd376c15bff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 12:19:48 -0400

    "add: be super/company/want_big_woman/man_or_bad_fact.py, super/early_week_or_few_hand/time_and_work/old_man/life/part.py, super/old_case_or_little_man.py or super/feel_woman_with_place/government_or_same_hand/eye_or_fact/world.py under first point"

---
+++ super/early_week_or_few_hand/time_and_work/old_man/life/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government_and_eye(str_arg):
+    case(str_arg)
+    print('young_case')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government_and_eye('different_man_and_right_number')
---
+++ super/old_case_or_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    company(str_arg)
+    print('old_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('work_or_point')
