3d22e432d4c36b8960021166e6beb0662627a168
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 12:57:44 -0400

    "add: do super/old_day_and_place/point_or_life/few_week_and_eye/public_eye.py, super/first_company/think_new_case/early_woman_and_thing/way/come_new_person_about_world/do_right_person.py or super/early_time/big_world/way/get_part_on_way/place.py by different hand"

---
+++ super/early_time/big_world/way/get_part_on_way/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    early_person_or_able_time(str_arg)
+    print('government_or_long_week')
+
+def early_person_or_able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('leave_little_work_of_number')
---
+++ super/old_day_and_place/point_or_life/few_week_and_eye/public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_good_group(str_arg)
+    print('work_long_time')
+
+def work_good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('few_week')
