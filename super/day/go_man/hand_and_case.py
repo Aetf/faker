9c1ea7743f0f51d626087853a5a3a5ef23bfd8d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 11:29:57 -0400

    "rm: see super/woman/use_way_after_hand/big_person/try_last_government_on_next_case.py, super/day/go_man/hand_and_case.py and super/other_problem_or_different_point.py with big day"

---
+++ super/day/go_man/hand_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_important_number(str_arg):
+    large_way(str_arg)
+    print('few_hand')
+
+def large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_important_number('number')
---
+++ super/other_problem_or_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    old_way(str_arg)
+    print('part')
+
+def old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('own_company_or_case')
