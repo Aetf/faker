38c490cf4ee1ee96d987c3dd765acfdd262ba6f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-10 10:40:12 -0400

    "rm: get super/part_and_same_work.py and super/next_day.py with high way"

---
+++ super/next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work(str_arg):
+    day(str_arg)
+    print('woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work('be_point_in_case')
---
+++ super/part_and_same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_new_company(str_arg):
+    few_life_or_large_week(str_arg)
+    print('man')
+
+def few_life_or_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_new_company('life_or_week')
