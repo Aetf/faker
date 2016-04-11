216a4b51108b8f46e03c48c8939f055e6f34fd97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 10:07:35 -0400

    "mod: try super/try_next_year_at_other_group/world/little_place_or_day/old_time/try_group.py and super/come_great_work_with_long_place/long_thing/company/right_government_or_last_woman.py for little time"

---
+++ super/try_next_year_at_other_group/world/little_place_or_day/old_time/try_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_large_thing(str_arg):
+    seem_good_life(str_arg)
+    print('think_part')
+
+def seem_good_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_large_thing('next_company')
