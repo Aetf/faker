4a3a496030c52c37ce99d81ecd564c7cc9cc6908
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 10:50:05 -0400

    "add: seem super/old_hand.py about own world"

---
+++ super/old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    child(str_arg)
+    print('life')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('last_place')
