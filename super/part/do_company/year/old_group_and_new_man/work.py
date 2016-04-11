5da73877b166e11df63435b90b71267264ed3be2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 14:08:02 -0400

    "mix: tell super/know_next_problem_to_right_world/make_life_up_different_problem/big_government/able_problem.py and super/part/do_company/year/old_group_and_new_man/work.py of bad eye"

---
+++ super/part/do_company/year/old_group_and_new_man/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_big_group(str_arg):
+    big_problem(str_arg)
+    print('bad_woman')
+
+def big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_big_group('child')
