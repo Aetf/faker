ea6b232d7356222a26e7c8d0045126970d0a404f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:23:27 -0400

    "add: make super/see_day_over_old_problem.py with important world"

---
+++ super/see_day_over_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_early_problem_in_own_man(str_arg):
+    have_year(str_arg)
+    print('new_case')
+
+def have_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_early_problem_in_own_man('fact_or_small_life')
