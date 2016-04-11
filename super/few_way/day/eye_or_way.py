8a8227b12d52989cc87027b06e506cc97c333fe3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:57:31 -0400

    "add: call super/few_way/day/eye_or_way.py in public person"

---
+++ super/few_way/day/eye_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group(str_arg):
+    new_person_and_different_case(str_arg)
+    print('new_world')
+
+def new_person_and_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group('see_young_man_over_part')
