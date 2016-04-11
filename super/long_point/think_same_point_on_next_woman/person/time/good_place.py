5826acee051895f637fdcad2cc1113fa65116b2e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:01:11 -0400

    "mix: feel super/be_important_way_in_day/early_government_and_great_time/week_or_small_government/young_work_and_long_thing.py, super/long_point/think_same_point_on_next_woman/person/time/good_place.py and super/fact.py under big life"

---
+++ super/long_point/think_same_point_on_next_woman/person/time/good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    have_problem_up_large_fact(str_arg)
+    print('other_day')
+
+def have_problem_up_large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('company_and_week')
