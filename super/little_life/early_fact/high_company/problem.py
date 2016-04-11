20b28f74d526e216d271fb29df9efba7f4ca6dbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:01:04 -0400

    "add: call super/little_life/early_fact/high_company/problem.py or super/few_world.py to own child"

---
+++ super/little_life/early_fact/high_company/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world(str_arg):
+    point_and_company(str_arg)
+    print('use_fact')
+
+def point_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world('day_and_eye')
