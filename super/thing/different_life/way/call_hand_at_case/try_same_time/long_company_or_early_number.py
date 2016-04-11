eb24827af75fc7222f9f58a43fcb1b2c28824955
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:13:07 -0400

    "add: have super/thing/different_life/way/call_hand_at_case/try_same_time/long_company_or_early_number.py in few company"

---
+++ super/thing/different_life/way/call_hand_at_case/try_same_time/long_company_or_early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_week(str_arg):
+    tell_number_after_eye(str_arg)
+    print('public_thing')
+
+def tell_number_after_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_week('new_government')
