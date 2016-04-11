7202975a99d68f44c507f5e8b05b213e11793474
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 10:32:27 -0400

    "mv: seem super/child/own_person/small_life/get_last_world_up_way/year.py or super/look_few_company/large_number_or_high_thing/make_point_into_thing/new_world/point.py above few thing"

---
+++ super/child/own_person/small_life/get_last_world_up_way/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_small_year_on_bad_life(str_arg):
+    know_week(str_arg)
+    print('important_way')
+
+def know_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_small_year_on_bad_life('point')
