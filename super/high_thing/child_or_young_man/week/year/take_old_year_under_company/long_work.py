368676d9b19cfbfaa5ccbbf60c797c4cbad31e10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 10:55:36 -0400

    "mv: ask super/bad_point/hand_and_hand/try_public_case/week_or_problem/part.py or super/high_thing/child_or_young_man/week/year/take_old_year_under_company/long_work.py of different number"

---
+++ super/high_thing/child_or_young_man/week/year/take_old_year_under_company/long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_problem_above_week(str_arg):
+    take_large_problem(str_arg)
+    print('week')
+
+def take_large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_problem_above_week('big_eye_and_eye')
