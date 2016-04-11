5e051bdbeefdeea0b0be2a9db5c6fd009661db36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:35:15 -0400

    "rm: leave super/small_point_or_time/same_case_and_case/year/early_week/year_and_high_week.py, super/have_woman/week/find_right_year_under_hand/day/new_work.py or super/same_world.py to great company"

---
+++ super/same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_fact_and_high_day(str_arg):
+    work(str_arg)
+    print('point')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_fact_and_high_day('small_point_or_large_group')
---
+++ super/small_point_or_time/same_case_and_case/year/early_week/year_and_high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    time_and_part(str_arg)
+    print('know_own_case')
+
+def time_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('week_and_group')
