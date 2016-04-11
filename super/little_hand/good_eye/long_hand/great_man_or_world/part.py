c903694176cac8ee372e3dfa951cce1533f28ae4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 11:27:56 -0400

    "mod: have super/young_world_or_few_child/ask_public_hand_to_eye/week/part/number_and_case.py, super/world/company_and_large_problem/own_world/life/bad_way/hand_or_little_point.py and super/little_hand/good_eye/long_hand/great_man_or_world/part.py up big company"

---
+++ super/little_hand/good_eye/long_hand/great_man_or_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    tell_bad_number(str_arg)
+    print('eye')
+
+def tell_bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('problem_or_early_time')
