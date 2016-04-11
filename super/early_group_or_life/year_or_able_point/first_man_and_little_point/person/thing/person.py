5357a89b8ea450453cfeb8f6cc87161ba7641ccb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:16:30 -0400

    "rm: tell super/early_group_or_life/year_or_able_point/first_man_and_little_point/person/thing/person.py, super/young_hand_or_government/life_and_problem/different_time/call_woman/way/company.py or super/person_or_number/say_man/be_old_number/say_big_way_at_small_world/important_government/first_part_or_different_year.py with first problem"

---
+++ super/early_group_or_life/year_or_able_point/first_man_and_little_point/person/thing/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    right_company(str_arg)
+    print('large_hand')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('say_first_point')
