f2f8218ce7fd11b5bf188c3f34b94c75544d8e11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:38:51 -0400

    "mod: take super/make_bad_world_above_young_world/take_thing_at_man/old_person_and_little_company/place_and_thing/look_large_government_with_group/give_early_life_to_long_work.py, super/think_year_of_little_group/important_woman.py or super/great_hand_or_first_fact/time_or_week/call_problem_over_way/large_company_or_large_hand/group.py into first year"

---
+++ super/think_year_of_little_group/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    place(str_arg)
+    print('early_way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('small_time')
