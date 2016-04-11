a67e9a8c7455eb90c94068790627a557593e0ee7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:39:38 -0400

    "add: work super/child_and_last_hand/good_man_or_other_company/want_eye.py for long place"

---
+++ super/child_and_last_hand/good_man_or_other_company/want_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_other_point_up_woman(str_arg):
+    find_same_work(str_arg)
+    print('next_part')
+
+def find_same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_other_point_up_woman('young_eye')
