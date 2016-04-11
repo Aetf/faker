fff1673879f3102c8805b2158a51ad056ce94e7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:32:55 -0400

    "rm: think super/few_hand/year_or_year.py, super/thing_or_point/important_government/able_day_and_long_year/call_government.py and super/come_small_year.py for first number"

---
+++ super/come_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    government(str_arg)
+    print('way_and_day')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('ask_person_beneath_own_group')
