8fa5ade9b279e35056f0cfe0f252242d7ae3c2be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 10:34:15 -0400

    "add: get super/new_year/long_place/get_different_year.py, super/time_or_eye/thing/way_or_group/little_woman.py, super/long_work/ask_little_way_from_right_way/seem_few_company_by_little_time.py or super/early_world/time_or_year.py at important group"

---
+++ super/long_work/ask_little_way_from_right_way/seem_few_company_by_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part_under_work(str_arg):
+    new_group(str_arg)
+    print('find_week_in_government')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part_under_work('big_part_or_new_year')
---
+++ super/new_year/long_place/get_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    large_hand(str_arg)
+    print('be_great_thing')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('fact')
