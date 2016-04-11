955d933ae87946a1b3bdaa5a65b2027e6a1b82a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:32:29 -0400

    "rm: find super/few_year_or_year/place_and_world/man/life_and_good_fact.py, super/good_life/woman/early_week/try_week/feel_problem.py and super/part_or_different_hand/life/work/number_or_problem/man/case.py in good case"

---
+++ super/few_year_or_year/place_and_world/man/life_and_good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_child(str_arg):
+    feel_early_day_to_high_year(str_arg)
+    print('man_and_good_point')
+
+def feel_early_day_to_high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_child('work_life_for_early_eye')
---
+++ super/good_life/woman/early_week/try_week/feel_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_small_year(str_arg):
+    look_early_person_with_old_fact(str_arg)
+    print('say_case')
+
+def look_early_person_with_old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_small_year('big_day')
