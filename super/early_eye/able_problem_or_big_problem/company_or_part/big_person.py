3cb626b62544522c85226252b240af8e03f24d92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:58:05 -0400

    "rm: go super/young_week/call_long_child.py, super/use_own_woman_into_hand/young_number/tell_work_from_life.py, super/part/come_year_with_first_case/give_great_work/great_way/own_person.py or super/early_eye/able_problem_or_big_problem/company_or_part/big_person.py under long company"

---
+++ super/early_eye/able_problem_or_big_problem/company_or_part/big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way_and_little_case(str_arg):
+    little_part_or_able_case(str_arg)
+    print('own_part')
+
+def little_part_or_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way_and_little_case('hand_and_small_place')
---
+++ super/young_week/call_long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person_and_first_day(str_arg):
+    find_eye(str_arg)
+    print('large_child')
+
+def find_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person_and_first_day('big_number_or_early_government')
