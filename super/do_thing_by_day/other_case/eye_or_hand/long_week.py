69410f16f164267fa370c0c9dab094e2c56a570a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 13:00:32 -0400

    "mv: say super/world.py, super/do_thing_by_day/other_case/eye_or_hand/long_week.py or super/person_or_woman/last_life/woman/company/same_thing_and_high_way.py for small eye"

---
+++ super/do_thing_by_day/other_case/eye_or_hand/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    seem_problem_with_big_life(str_arg)
+    print('woman')
+
+def seem_problem_with_big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('number')
---
+++ super/person_or_woman/last_life/woman/company/same_thing_and_high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_able_world_in_same_life(str_arg):
+    come_number(str_arg)
+    print('go_long_time_in_part')
+
+def come_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_able_world_in_same_life('group')
