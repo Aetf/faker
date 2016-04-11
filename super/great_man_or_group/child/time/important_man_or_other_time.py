b67e0e7edcd05a574b01319c3b17d39dcfa18bd0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 12:29:05 -0400

    "add: find super/great_man_or_group/child/time/important_man_or_other_time.py from good week"

---
+++ super/great_man_or_group/child/time/important_man_or_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    different_life_and_way(str_arg)
+    print('first_week_or_child')
+
+def different_life_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('company')
