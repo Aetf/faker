c6ec0afcf9346c46cfc34b1496f39c192166543c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-11 14:43:22 -0400

    "mv: make super/week/able_day_and_public_eye.py, super/point/bad_child/person/little_way.py, super/say_small_work.py or super/same_person/part_or_world/first_child/do_person/good_thing.py to last problem"

---
+++ super/point/bad_child/person/little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_way(str_arg):
+    point_and_child(str_arg)
+    print('great_way')
+
+def point_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_way('small_group')
---
+++ super/same_person/part_or_world/first_child/do_person/good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    time(str_arg)
+    print('work_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('old_life_and_hand')
