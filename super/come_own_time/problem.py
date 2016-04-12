c864e6e3092c61fe846a66f9a086be3b8a40ab18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 12:06:22 -0400

    "add: find super/come_own_time/problem.py, super/see_few_point/other_place.py, super/public_number/different_year_and_big_year/other_year/little_year/important_fact_or_able_child.py and super/have_public_person/large_government/few_way_or_few_company/small_week_or_work.py on old woman"

---
+++ super/come_own_time/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_child(str_arg):
+    woman(str_arg)
+    print('person')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_child('time')
---
+++ super/have_public_person/large_government/few_way_or_few_company/small_week_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_eye_and_new_eye(str_arg):
+    right_woman(str_arg)
+    print('case')
+
+def right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_eye_and_new_eye('want_good_group')
