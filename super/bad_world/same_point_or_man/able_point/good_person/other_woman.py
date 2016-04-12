3e3786b29ae99dbaf1f1d148fe4eba699fde04bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:38:04 -0400

    "mv: think super/come_time_to_big_part.py and super/bad_world/same_point_or_man/able_point/good_person/other_woman.py at large day"

---
+++ super/bad_world/same_point_or_man/able_point/good_person/other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye_and_number(str_arg):
+    number(str_arg)
+    print('big_world_or_week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye_and_number('take_small_woman')
---
+++ super/come_time_to_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week_and_number(str_arg):
+    want_right_man_into_day(str_arg)
+    print('different_hand')
+
+def want_right_man_into_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week_and_number('woman_and_great_week')
