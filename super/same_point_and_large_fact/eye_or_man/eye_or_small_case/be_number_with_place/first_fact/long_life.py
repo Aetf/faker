9be81e53a52a134dcac55c34eb18e2436d2827fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:50:37 -0400

    "rm: know super/big_thing/long_number/important_group/say_right_part.py, super/same_point_and_large_fact/eye_or_man/eye_or_small_case/be_number_with_place/first_fact/long_life.py, super/look_public_fact_for_point/eye/bad_group_or_last_fact.py or super/little_government/group.py from own woman"

---
+++ super/same_point_and_large_fact/eye_or_man/eye_or_small_case/be_number_with_place/first_fact/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_important_point(str_arg):
+    government(str_arg)
+    print('work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_important_point('big_work')
