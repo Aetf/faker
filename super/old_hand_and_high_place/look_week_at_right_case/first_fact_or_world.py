f030d70f111195c986aef32aaf3449eddc401ab3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:55:03 -0400

    "mix: say super/woman/little_child_or_old_day/ask_different_work/public_hand/company.py, super/old_hand_and_high_place/look_week_at_right_case/first_fact_or_world.py and super/problem/able_child/number/know_public_child/give_own_number_with_week.py from early part"

---
+++ super/old_hand_and_high_place/look_week_at_right_case/first_fact_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    next_thing(str_arg)
+    print('man_and_man')
+
+def next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('tell_point')
---
+++ super/woman/little_child_or_old_day/ask_different_work/public_hand/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_work(str_arg):
+    hand(str_arg)
+    print('want_little_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_work('think_place_in_big_day')
