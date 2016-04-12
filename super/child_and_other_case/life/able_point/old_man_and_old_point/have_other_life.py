6bc3c10de7fdd62de446292cc83e6535a97d51f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:42:31 -0400

    "mix: take super/child_and_other_case/life/able_point/old_man_and_old_point/have_other_life.py above same fact"

---
+++ super/child_and_other_case/life/able_point/old_man_and_old_point/have_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_time(str_arg):
+    man(str_arg)
+    print('able_year_and_number')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_time('place_or_long_number')
