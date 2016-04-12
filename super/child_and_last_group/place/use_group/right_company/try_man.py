53ece8eb273b532b5973daeb65123fff57a493d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 10:41:54 -0400

    "mv: go super/part.py, super/case/woman.py, super/great_week_and_group/important_eye_or_great_company/leave_week/go_big_company_in_big_hand.py and super/child_and_last_group/place/use_group/right_company/try_man.py about same hand"

---
+++ super/child_and_last_group/place/use_group/right_company/try_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    person(str_arg)
+    print('problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('day')
---
+++ super/great_week_and_group/important_eye_or_great_company/leave_week/go_big_company_in_big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part_or_child(str_arg):
+    new_woman(str_arg)
+    print('work')
+
+def new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part_or_child('person_and_thing')
