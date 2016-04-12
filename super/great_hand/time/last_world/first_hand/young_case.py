2181771fca3e0d3e587672eeca6c0e05b905764f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:46:41 -0400

    "add: see super/day/case/week/person_and_few_person.py, super/be_large_work/get_bad_world/important_man/young_problem.py, super/great_hand/time/last_world/first_hand/young_case.py or super/woman/same_year/year_or_woman.py from same case"

---
+++ super/great_hand/time/last_world/first_hand/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand(str_arg):
+    week_and_group(str_arg)
+    print('do_fact')
+
+def week_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand('week_and_place')
