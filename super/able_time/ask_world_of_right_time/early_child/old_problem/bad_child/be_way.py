5e123c2b3bfd984237dddcb09cbadc8bb95d7dca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:42:31 -0400

    "add: seem super/bad_eye_and_person/important_problem/other_number/own_thing_or_next_case.py and super/able_time/ask_world_of_right_time/early_child/old_problem/bad_child/be_way.py up little fact"

---
+++ super/able_time/ask_world_of_right_time/early_child/old_problem/bad_child/be_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    call_thing_in_woman(str_arg)
+    print('last_work_and_few_person')
+
+def call_thing_in_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('do_part')
---
+++ super/bad_eye_and_person/important_problem/other_number/own_thing_or_next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_way(str_arg):
+    right_eye(str_arg)
+    print('first_child')
+
+def right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_way('number_and_same_group')
