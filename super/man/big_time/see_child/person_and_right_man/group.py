364e49a9fc67ef783395628e1224961a201cca2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 12:01:50 -0400

    "mv: see super/man/big_time/see_child/person_and_right_man/group.py at good number"

---
+++ super/man/big_time/see_child/person_and_right_man/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    year(str_arg)
+    print('number')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('new_time')
