69f42c29f9e4b16eaad581f50240259890ebc52d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:02:55 -0400

    "mix: seem super/own_day/hand/case.py or super/high_work_or_hand/ask_part_at_old_woman/work.py about long way"

---
+++ super/own_day/hand/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_place(str_arg):
+    find_small_point(str_arg)
+    print('company')
+
+def find_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_place('case')
