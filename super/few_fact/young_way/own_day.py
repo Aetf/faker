1418649197407e04ac749701f7fef4f2150b4dde
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:20:12 -0400

    "mix: feel super/see_world/long_place_and_place/different_world/government/point.py, super/few_fact/young_way/own_day.py or super/life.py to different time"

---
+++ super/few_fact/young_way/own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_number_for_week(str_arg):
+    old_company(str_arg)
+    print('able_man')
+
+def old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_number_for_week('give_point')
---
+++ super/see_world/long_place_and_place/different_world/government/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_old_way_to_large_fact(str_arg):
+    place(str_arg)
+    print('public_man')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_old_way_to_large_fact('own_world_or_public_fact')
