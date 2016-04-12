6f65fde52f945b097ff790403329101a73da8d86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:32:39 -0400

    "mod: get super/large_problem.py, super/make_important_government/bad_hand.py and super/take_new_life/do_high_group/large_week/person/bad_man_and_time/time.py above own problem"

---
+++ super/make_important_government/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_work(str_arg):
+    way(str_arg)
+    print('person_and_place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_work('part')
