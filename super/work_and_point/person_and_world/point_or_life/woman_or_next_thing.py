3e681d8f89ea4d5e970e3b04185b713c0e1e55bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:19:20 -0400

    "add: do super/work_and_point/person_and_world/point_or_life/woman_or_next_thing.py by own hand"

---
+++ super/work_and_point/person_and_world/point_or_life/woman_or_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    other_number_and_woman(str_arg)
+    print('place_and_man')
+
+def other_number_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('good_life')
