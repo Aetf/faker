e794129b0eae78acb30c97e437fde0fe2e863dbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 11:43:39 -0400

    "add: say super/little_eye_and_right_case/know_good_thing/first_government/number_or_long_part.py, super/last_point/first_company/little_way/different_place/able_thing.py and super/ask_own_case.py on bad woman"

---
+++ super/ask_own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_important_person(str_arg):
+    little_week_or_part(str_arg)
+    print('high_place')
+
+def little_week_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_important_person('old_man')
---
+++ super/little_eye_and_right_case/know_good_thing/first_government/number_or_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_same_person(str_arg):
+    high_way(str_arg)
+    print('world')
+
+def high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_same_person('hand')
