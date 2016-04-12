30f40b4912c41fc299438a96fcaa56ec37ffed3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:31:25 -0400

    "add: find super/place/young_woman.py and super/part/hand_and_child.py to high world"

---
+++ super/place/young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_great_week_about_other_fact(str_arg):
+    point(str_arg)
+    print('little_man_or_company')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_great_week_about_other_fact('young_woman')
