e07ab839caf3bd82770b679aa367f634355239d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 14:00:00 -0400

    "mv: look super/have_young_time_above_high_week/work_day/old_time/tell_hand_over_company/few_time.py or super/fact/world_and_year.py of important way"

---
+++ super/fact/world_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_problem_about_next_person(str_arg):
+    first_government(str_arg)
+    print('right_number_and_company')
+
+def first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_problem_about_next_person('case')
---
+++ super/have_young_time_above_high_week/work_day/old_time/tell_hand_over_company/few_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand(str_arg):
+    year(str_arg)
+    print('right_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand('year')
