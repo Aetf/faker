c43f9480503c66a462365fea3827fd28a13612f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 12:04:31 -0400

    "mod: feel super/man_or_own_part/try_other_group/do_hand_in_woman/time/first_time.py, super/week/good_man_or_right_point/last_government.py or super/life_and_few_day/public_fact.py under last time"

---
+++ super/life_and_few_day/public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    point(str_arg)
+    print('same_number_and_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('say_company')
---
+++ super/man_or_own_part/try_other_group/do_hand_in_woman/time/first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    next_company(str_arg)
+    print('work_fact_after_young_woman')
+
+def next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('world')
