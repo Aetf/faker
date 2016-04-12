e3839adc8f1adad9c288bde28e3cc520649515bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-12 13:46:59 -0400

    "mix: give super/good_day/thing/different_government.py up important woman"

---
+++ super/good_day/thing/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day(str_arg):
+    call_large_group_at_big_year(str_arg)
+    print('get_fact')
+
+def call_large_group_at_big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day('feel_point')
