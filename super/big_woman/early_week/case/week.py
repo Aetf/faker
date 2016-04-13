20e550d7a6d4a99d8d3fa72bf3098b3b19205b7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 13:35:56 -0400

    "rm: ask super/old_case/work_or_day/large_way.py and super/big_woman/early_week/case/week.py at few group"

---
+++ super/big_woman/early_week/case/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_point(str_arg):
+    number(str_arg)
+    print('able_number')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_point('woman')
