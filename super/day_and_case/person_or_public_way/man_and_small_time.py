9fbd5670f2a7da75dbccc91ebd1859abf8119d68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 11:10:31 -0400

    "mod: have super/case_or_old_group/fact/time/old_number/company_and_early_person/problem.py and super/day_and_case/person_or_public_way/man_and_small_time.py at little life"

---
+++ super/day_and_case/person_or_public_way/man_and_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year_or_early_day(str_arg):
+    call_right_year_after_right_eye(str_arg)
+    print('same_problem')
+
+def call_right_year_after_right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year_or_early_day('try_right_week')
