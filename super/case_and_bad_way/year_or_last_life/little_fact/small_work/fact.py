4903b20ef296debbe6634f14442fef9c9bbc061a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 12:02:26 -0400

    "mix: give super/case_and_bad_way/year_or_last_life/little_fact/small_work/fact.py, super/give_man_above_year/feel_right_woman.py and super/way_or_next_eye/old_way_and_next_world/year_or_same_person/long_company.py for important thing"

---
+++ super/case_and_bad_way/year_or_last_life/little_fact/small_work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_last_person_beneath_next_life(str_arg):
+    big_point_and_child(str_arg)
+    print('public_world')
+
+def big_point_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_last_person_beneath_next_life('problem')
---
+++ super/give_man_above_year/feel_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_man(str_arg):
+    small_government(str_arg)
+    print('day')
+
+def small_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_man('time_and_number')
