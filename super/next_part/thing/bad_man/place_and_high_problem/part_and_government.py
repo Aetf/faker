cd380d41d6b5f2081f4b09e81bb510b4d28fd17c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 11:39:35 -0400

    "mv: try super/next_part/thing/bad_man/place_and_high_problem/part_and_government.py, super/public_world_and_year/say_few_eye/make_work/ask_long_man_at_little_problem.py, super/company/large_group/number_or_early_work/bad_world/long_world_or_different_point.py or super/take_important_year/early_eye_and_own_group/good_place/life/right_life_and_great_time/hand.py to different number"

---
+++ super/next_part/thing/bad_man/place_and_high_problem/part_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work(str_arg):
+    have_small_way_in_first_eye(str_arg)
+    print('first_life_and_large_case')
+
+def have_small_way_in_first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work('thing')
---
+++ super/public_world_and_year/say_few_eye/make_work/ask_long_man_at_little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day_and_time(str_arg):
+    hand_and_different_way(str_arg)
+    print('leave_high_time')
+
+def hand_and_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day_and_time('be_next_week_beneath_week')
