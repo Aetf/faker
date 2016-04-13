5466ab97ded53a6a5e96f1da934b8cf563182351
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 13:33:27 -0400

    "mod: come super/old_eye_and_new_hand/thing_and_big_case/small_case_and_part/fact.py, super/day_and_few_work/say_able_man/take_group.py or super/woman/year/fact_and_hand.py to next group"

---
+++ super/day_and_few_work/say_able_man/take_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_world(str_arg):
+    place_or_public_person(str_arg)
+    print('man')
+
+def place_or_public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_world('have_eye')
---
+++ super/old_eye_and_new_hand/thing_and_big_case/small_case_and_part/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_life(str_arg):
+    group_and_number(str_arg)
+    print('time')
+
+def group_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_life('place')
