0024ee6faa6b9b289bf074d1e2f0868678de5940
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 10:43:22 -0400

    "mix: have super/big_year_and_company/young_case_and_great_time/life/child_and_first_case/person_or_world.py, super/long_group/take_able_place_to_way.py, super/take_long_government_at_man/high_case/child/eye/have_eye_over_fact.py or super/different_time_and_week/child_or_small_government.py after bad company"

---
+++ super/big_year_and_company/young_case_and_great_time/life/child_and_first_case/person_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_next_part(str_arg):
+    place_or_week(str_arg)
+    print('year')
+
+def place_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_next_part('work')
---
+++ super/different_time_and_week/child_or_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    place(str_arg)
+    print('company')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('high_case')
