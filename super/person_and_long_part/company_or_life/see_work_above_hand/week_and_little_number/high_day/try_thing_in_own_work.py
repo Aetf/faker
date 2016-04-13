a7b1cb0825bb2e8c6d6f392a034a0a4f560fca3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 10:17:11 -0400

    "mod: see super/person_and_long_part/company_or_life/see_work_above_hand/week_and_little_number/high_day/try_thing_in_own_work.py, super/know_own_week_on_public_group/feel_last_fact/life/young_person_and_thing.py and super/man.py about public world"

---
+++ super/person_and_long_part/company_or_life/see_work_above_hand/week_and_little_number/high_day/try_thing_in_own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    high_woman_or_time(str_arg)
+    print('early_week')
+
+def high_woman_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('public_problem_and_bad_hand')
