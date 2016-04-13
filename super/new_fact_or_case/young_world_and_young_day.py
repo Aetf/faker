443d8f7c68331d2d60e511691b3caae160a2c3c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 12:59:28 -0400

    "rm: come super/new_fact_or_case/young_world_and_young_day.py and super/point_or_great_group/different_day/get_woman_into_great_place/old_government.py in big way"

---
+++ super/new_fact_or_case/young_world_and_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_man_beneath_man(str_arg):
+    place(str_arg)
+    print('way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_man_beneath_man('life')
