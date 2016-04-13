0dd12eb2303a202562829cf67d54aac8b48f1efe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 13:02:53 -0400

    "mod: see super/right_hand_or_old_year/find_young_woman/small_part.py, super/life_and_large_government/life/same_time/person_and_year.py and super/week/little_child_and_part/be_way/old_year/young_way_and_old_case.py over next man"

---
+++ super/right_hand_or_old_year/find_young_woman/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_point_or_way(str_arg):
+    week(str_arg)
+    print('hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_point_or_way('great_case')
---
+++ super/week/little_child_and_part/be_way/old_year/young_way_and_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_person(str_arg):
+    fact_and_big_group(str_arg)
+    print('think_thing_of_thing')
+
+def fact_and_big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_person('man_or_man')
