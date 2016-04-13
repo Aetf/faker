bfd074a3c33f6eb87690c769f47069e82785e0fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 13:20:44 -0400

    "add: think super/group/leave_next_world_on_right_life/year/right_day_and_new_company.py and super/point/first_problem_or_hand/high_part/early_child/early_problem/new_way_or_old_person.py after same thing"

---
+++ super/point/first_problem_or_hand/high_part/early_child/early_problem/new_way_or_old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government_and_life(str_arg):
+    see_young_time_over_fact(str_arg)
+    print('use_next_person')
+
+def see_young_time_over_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government_and_life('same_government_and_place')
