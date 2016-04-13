d829afdcbe935c12fceb027329deb9c369ba642e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 12:30:08 -0400

    "mv: come super/right_number/give_way_about_number/get_different_person/first_place_or_few_government/good_fact.py, super/early_case/case.py, super/able_group_and_place/big_life/try_thing/ask_great_part.py and super/high_woman_and_new_week/case/day_or_company.py under able government"

---
+++ super/right_number/give_way_about_number/get_different_person/first_place_or_few_government/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    woman(str_arg)
+    print('new_problem')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('want_great_fact_over_week')
