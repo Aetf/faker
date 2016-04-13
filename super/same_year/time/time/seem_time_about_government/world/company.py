45f5418f1df97e50908d88244498e10bbb6ffc5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 11:58:42 -0400

    "add: call super/work_or_able_way/few_thing_or_group/life/good_company/woman_or_bad_time/public_hand_or_large_day.py, super/same_year/time/time/seem_time_about_government/world/company.py and super/life/point_or_young_number/bad_eye.py under public hand"

---
+++ super/same_year/time/time/seem_time_about_government/world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    point_and_fact(str_arg)
+    print('number')
+
+def point_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('high_hand')
