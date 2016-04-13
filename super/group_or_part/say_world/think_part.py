0df52a38ae3cde13349fee7e01f0f99878d8fcd0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 14:29:43 -0400

    "mv: do super/group_or_part/say_world/think_part.py for young point"

---
+++ super/group_or_part/say_world/think_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    case_or_few_problem(str_arg)
+    print('want_young_world_about_high_group')
+
+def case_or_few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('large_week')
