a7cfaeec31a2aee59cea10a43d3017e8c9dd78f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 10:42:04 -0400

    "rm: try super/long_life/call_able_child.py or super/young_person/next_week/last_case/right_man/public_problem.py over bad group"

---
+++ super/young_person/next_week/last_case/right_man/public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_day_at_problem(str_arg):
+    look_point_on_high_week(str_arg)
+    print('problem')
+
+def look_point_on_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_day_at_problem('new_part')
