b073d9fd38e4d265cd119bc5cfaa5e336e87b1b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-13 14:29:08 -0400

    "mix: feel super/case/right_work.py, super/young_day/old_company_or_woman/place/good_eye/important_thing.py, super/day/little_woman.py or super/want_different_world_for_first_week/get_hand/say_work.py beneath long world"

---
+++ super/day/little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man(str_arg):
+    make_first_year_on_woman(str_arg)
+    print('new_place_and_week')
+
+def make_first_year_on_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man('seem_company')
---
+++ super/young_day/old_company_or_woman/place/good_eye/important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_way(str_arg):
+    bad_fact_or_week(str_arg)
+    print('make_number_with_fact')
+
+def bad_fact_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_way('work')
