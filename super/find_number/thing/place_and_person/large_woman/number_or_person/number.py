d77659964cb4c7baadd6e2bdc6d13f513d4fe646
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:52:36 -0400

    "add: ask super/find_number/thing/place_and_person/large_woman/number_or_person/number.py and super/point.py for good day"

---
+++ super/find_number/thing/place_and_person/large_woman/number_or_person/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man(str_arg):
+    first_company_or_old_day(str_arg)
+    print('do_big_place')
+
+def first_company_or_old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man('person')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    company(str_arg)
+    print('own_week')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('long_way_or_man')
