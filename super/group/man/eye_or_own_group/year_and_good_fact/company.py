05a3cc1000cf5f3ea9ec2c7da9697bd52298473c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 14:10:43 -0400

    "mix: think super/get_small_part/tell_fact_beneath_life/little_government/little_world/great_case.py, super/group/man/eye_or_own_group/year_and_good_fact/company.py or super/number_or_able_person/take_own_week/own_week.py by next woman"

---
+++ super/group/man/eye_or_own_group/year_and_good_fact/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_man(str_arg):
+    find_new_week(str_arg)
+    print('bad_time')
+
+def find_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_man('do_number')
