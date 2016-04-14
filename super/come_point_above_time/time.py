dee9656285ca265fbe5ee898ccf35664bb7e9be2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:08:56 -0400

    "mod: get super/same_case/use_last_way_to_point/life/man_and_little_way/eye_and_eye/next_eye_or_right_world.py, super/good_place/early_day/case_and_person/time/high_number_or_large_woman/small_government.py, super/come_point_above_time/time.py and super/great_woman.py by high group"

---
+++ super/come_point_above_time/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_right_fact_to_own_fact(str_arg):
+    big_problem(str_arg)
+    print('same_woman')
+
+def big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_right_fact_to_own_fact('give_bad_hand_beneath_eye')
