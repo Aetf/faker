a5a0b868d7514293a097857066fe2a3d1b64cfb9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 13:48:21 -0400

    "rm: call super/great_way_and_problem/long_place/get_case_above_different_world.py or super/woman/make_old_man/be_government_for_old_work/part/feel_company/long_way.py to last man"

---
+++ super/great_way_and_problem/long_place/get_case_above_different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    take_point(str_arg)
+    print('look_able_work_in_different_hand')
+
+def take_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('bad_child')
---
+++ super/woman/make_old_man/be_government_for_old_work/part/feel_company/long_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_point_and_good_hand(str_arg):
+    think_part_up_right_woman(str_arg)
+    print('want_point')
+
+def think_part_up_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_point_and_good_hand('feel_public_eye')
