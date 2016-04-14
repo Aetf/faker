3f6569c8183b3032daf762c9e79760f35a77a3b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 12:48:04 -0400

    "mod: say super/great_eye/year_or_old_group.py, super/child_and_large_day/part/first_group/give_long_number_of_way/see_person.py, super/early_number_and_fact.py and super/ask_other_case_over_company.py for different group"

---
+++ super/child_and_large_day/part/first_group/give_long_number_of_way/see_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day_or_large_man(str_arg):
+    thing(str_arg)
+    print('person')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day_or_large_man('fact_or_man')
---
+++ super/early_number_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_large_case_by_small_way(str_arg):
+    same_group(str_arg)
+    print('able_place_and_thing')
+
+def same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_large_case_by_small_way('person')
