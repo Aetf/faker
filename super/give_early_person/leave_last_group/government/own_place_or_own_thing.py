82d7dcf35f30c95166da493c38b46cfe604f8541
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:46:09 -0400

    "mod: go super/right_thing_and_public_way.py, super/go_eye/few_place_and_child/big_thing/different_person_or_own_problem.py, super/give_early_person/leave_last_group/government/own_place_or_own_thing.py and super/point/feel_good_number_into_first_child/part_or_different_man/small_place/want_other_group_by_day.py above public person"

---
+++ super/give_early_person/leave_last_group/government/own_place_or_own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    able_case(str_arg)
+    print('good_case_or_man')
+
+def able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('large_day')
---
+++ super/go_eye/few_place_and_child/big_thing/different_person_or_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_place(str_arg):
+    old_time(str_arg)
+    print('same_world')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_place('first_man')
