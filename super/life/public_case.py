986b7513fc4ff1f32b1461bb5d64c3cf8ef956c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 11:03:04 -0400

    "rm: think super/hand/group_and_little_eye/big_day/hand/case/first_thing_and_same_person.py, super/thing_or_place/bad_world_or_eye/work_thing/use_point.py and super/life/public_case.py under same week"

---
+++ super/life/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_year_from_high_woman(str_arg):
+    know_hand_under_public_point(str_arg)
+    print('week')
+
+def know_hand_under_public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_year_from_high_woman('work_or_company')
---
+++ super/thing_or_place/bad_world_or_eye/work_thing/use_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_place_and_hand(str_arg):
+    bad_fact(str_arg)
+    print('leave_next_place_at_way')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_place_and_hand('big_work')
