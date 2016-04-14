503f832f47d21da204faa44d6ae7ecb2ab5a26a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 14:05:49 -0400

    "mod: work super/large_world/early_child/world/make_company_after_high_work.py from new day"

---
+++ super/large_world/early_child/world/make_company_after_high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_company_and_right_time(str_arg):
+    way(str_arg)
+    print('good_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_company_and_right_time('way_or_important_year')
