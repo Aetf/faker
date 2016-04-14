4774f688841ee791cf9bc095deba2975975ed216
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 12:26:14 -0400

    "mix: get super/long_child_or_man/new_company/big_child/good_world_or_next_place/right_time_and_next_point/number.py, super/different_way/first_week.py, super/be_problem/point/company/week/child/important_hand.py and super/case.py after other point"

---
+++ super/different_way/first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    use_eye(str_arg)
+    print('great_eye')
+
+def use_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('find_few_work')
---
+++ super/long_child_or_man/new_company/big_child/good_world_or_next_place/right_time_and_next_point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    great_time(str_arg)
+    print('company')
+
+def great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('bad_man_and_hand')
