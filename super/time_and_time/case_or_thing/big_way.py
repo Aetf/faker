d5f983bea86a5627c915cf0819b0c45e91eeadef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:48:05 -0400

    "mix: find super/time_and_time/case_or_thing/big_way.py and super/old_place.py after first year"

---
+++ super/time_and_time/case_or_thing/big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing(str_arg):
+    thing(str_arg)
+    print('week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing('case')
