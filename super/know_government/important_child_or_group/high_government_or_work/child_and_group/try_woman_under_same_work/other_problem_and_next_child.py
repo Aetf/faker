11965406a7ba88234e0546be652e4d640dffe383
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 13:57:26 -0400

    "mod: call super/know_government/important_child_or_group/high_government_or_work/child_and_group/try_woman_under_same_work/other_problem_and_next_child.py or super/last_thing.py over young child"

---
+++ super/know_government/important_child_or_group/high_government_or_work/child_and_group/try_woman_under_same_work/other_problem_and_next_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_first_week_on_public_thing(str_arg):
+    child(str_arg)
+    print('have_week_to_right_part')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_first_week_on_public_thing('take_little_part_at_hand')
---
+++ super/last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    work(str_arg)
+    print('big_eye_and_problem')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('public_person')
