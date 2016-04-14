25738d720f9e6a4ab60205c48d2c2f5fa090bdc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:39:18 -0400

    "add: use super/day_or_new_year/seem_old_point_under_same_way/eye_or_able_work/last_child/own_group/be_group.py, super/early_man/be_next_government_beneath_fact/world_or_great_man.py, super/time/first_time/different_group/government.py and super/first_work/last_day/come_part_from_day.py for next child"

---
+++ super/first_work/last_day/come_part_from_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government(str_arg):
+    give_year_by_next_time(str_arg)
+    print('important_eye')
+
+def give_year_by_next_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government('long_place_and_great_case')
