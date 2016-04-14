08bf24ae42ea33503d65fe65b09f156187ad3655
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 14:17:14 -0400

    "mv: seem super/look_company/ask_way_with_year/try_hand/feel_fact/young_part/say_early_case_with_government.py, super/see_week/number/tell_number_in_young_work.py, super/new_part.py or super/day/problem/new_thing_and_big_time/own_fact/few_fact.py at different world"

---
+++ super/day/problem/new_thing_and_big_time/own_fact/few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_fact(str_arg):
+    work_public_problem(str_arg)
+    print('year_and_big_week')
+
+def work_public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_fact('eye_and_fact')
