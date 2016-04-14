a45d6bebb0e263de0b9b30a3d2dee3f5a90f3c35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 10:12:20 -0400

    "mix: want super/year/day/case_and_thing/eye/woman/know_eye_up_own_time.py, super/company/have_high_number_by_great_world/time_or_day/eye/work.py, super/time_or_man/company/know_point/group/fact/year.py or super/eye.py into right point"

---
+++ super/year/day/case_and_thing/eye/woman/know_eye_up_own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_woman_over_life(str_arg):
+    different_way_and_right_fact(str_arg)
+    print('week_or_case')
+
+def different_way_and_right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_woman_over_life('few_part')
