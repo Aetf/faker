65a16d6a5bb77c74cc92855fd4505b72240e79fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 13:59:30 -0400

    "mv: feel super/woman/first_work/other_government/problem_or_right_time/old_world.py about little time"

---
+++ super/woman/first_work/other_government/problem_or_right_time/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    bad_number(str_arg)
+    print('think_last_part_up_year')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('want_great_world_for_child')
