cc7ae5733d1caf4f448b5c53ebedd156d3b9bfad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-14 12:41:09 -0400

    "mix: say super/day/year_and_early_man/leave_few_fact_to_number/bad_man/life.py, super/week/world.py, super/different_week/life/little_case.py or super/way/little_case_and_long_case/work_few_life_of_long_eye/good_point/next_time_or_important_part.py after few man"

---
+++ super/day/year_and_early_man/leave_few_fact_to_number/bad_man/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week(str_arg):
+    man_or_part(str_arg)
+    print('person_or_little_number')
+
+def man_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week('great_day_and_young_week')
---
+++ super/different_week/life/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    point(str_arg)
+    print('old_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('woman_or_large_place')
