748ec354b7f2fd41cdfb590f03fb808546e2ce2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 12:46:10 -0400

    "mv: know super/bad_government/get_week_up_child.py and super/right_woman/want_next_government_after_first_man/day/look_year.py after different hand"

---
+++ super/bad_government/get_week_up_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    do_different_place(str_arg)
+    print('part')
+
+def do_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('government_or_other_hand')
