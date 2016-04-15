40b92657e984906140f4a7f287d31831c290b018
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 12:08:06 -0400

    "mix: try super/long_company_or_person/public_year_and_year.py or super/day/try_world/think_same_point_from_part.py to long hand"

---
+++ super/day/try_world/think_same_point_from_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_way(str_arg):
+    feel_problem(str_arg)
+    print('year')
+
+def feel_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_way('day')
---
+++ super/long_company_or_person/public_year_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    say_long_point(str_arg)
+    print('year')
+
+def say_long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('work')
