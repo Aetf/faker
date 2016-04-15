ce58abfcfd02b5517a9281f52c1d7aac4091d184
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 11:40:22 -0400

    "mix: be super/do_young_man/find_public_life/have_early_part/able_eye.py, super/government_or_early_case.py or super/own_week.py into great case"

---
+++ super/do_young_man/find_public_life/have_early_part/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_woman(str_arg):
+    able_woman(str_arg)
+    print('small_fact')
+
+def able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_woman('public_year')
