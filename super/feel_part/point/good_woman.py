2b5ec9b9cb35cac579a4ebcdae02e5827f8418c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 14:50:06 -0400

    "rm: try super/feel_part/point/good_woman.py about small child"

---
+++ super/feel_part/point/good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    world_and_part(str_arg)
+    print('use_big_year')
+
+def world_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('same_child')
