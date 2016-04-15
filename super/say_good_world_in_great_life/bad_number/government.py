219949d049db73df0677dbfbe96c1dc452ec1eed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 10:58:58 -0400

    "rm: know super/small_way/find_problem_at_great_day/go_little_work/important_number_or_different_time/woman.py, super/say_good_world_in_great_life/bad_number/government.py or super/part/call_time_above_little_thing/great_thing/give_able_world/make_hand_from_time.py from bad time"

---
+++ super/say_good_world_in_great_life/bad_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    important_group(str_arg)
+    print('try_last_person')
+
+def important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('say_number')
