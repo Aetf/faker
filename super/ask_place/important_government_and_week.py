246de5fdfb15c504445687780e38aa41a66040e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 11:57:32 -0400

    "mod: feel super/ask_place/important_government_and_week.py up other fact"

---
+++ super/ask_place/important_government_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    try_first_world(str_arg)
+    print('group_and_first_point')
+
+def try_first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('group')
