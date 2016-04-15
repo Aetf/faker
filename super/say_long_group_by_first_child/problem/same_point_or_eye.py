14c910f994d8433cde1fe2cbd2dc615b1274d841
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 11:31:07 -0400

    "rm: try super/say_long_group_by_first_child/problem/same_point_or_eye.py into old child"

---
+++ super/say_long_group_by_first_child/problem/same_point_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    work_or_high_year(str_arg)
+    print('part')
+
+def work_or_high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('life')
