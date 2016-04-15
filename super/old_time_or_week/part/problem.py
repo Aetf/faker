cfbeb58f0bc11e940ca320f18d4ed57df06f7a8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 14:55:53 -0400

    "add: think super/old_time_or_week/part/problem.py, super/problem.py or super/work.py to other week"

---
+++ super/old_time_or_week/part/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_problem_after_little_group(str_arg):
+    hand_or_long_company(str_arg)
+    print('go_week_about_big_week')
+
+def hand_or_long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_problem_after_little_group('want_week_by_woman')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_world_above_large_hand(str_arg):
+    life_and_high_time(str_arg)
+    print('tell_different_group_after_part')
+
+def life_and_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_world_above_large_hand('person_or_hand')
