9444207e820b0ab8dc33076788c9b14fe535baff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 13:46:35 -0400

    "mix: know super/old_world_or_year.py with old government"

---
+++ super/old_world_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_life_in_early_company(str_arg):
+    different_world(str_arg)
+    print('first_year')
+
+def different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_life_in_early_company('new_child')
