05afd0118b58c62fa1b648de69e8d0376d3014bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 13:46:20 -0400

    "mix: go super/own_case/give_old_thing/great_part_or_time/work_hand_into_bad_government/case_or_other_company.py or super/give_next_person/make_good_day/work/look_early_eye_on_time/child/feel_person_under_world.py in public eye"

---
+++ super/give_next_person/make_good_day/work/look_early_eye_on_time/child/feel_person_under_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    own_number_and_big_group(str_arg)
+    print('point')
+
+def own_number_and_big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('high_man')
