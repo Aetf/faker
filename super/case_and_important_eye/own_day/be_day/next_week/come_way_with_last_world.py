c9c86dbaee95e5d4395e3fa200cb287d94c4e304
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:49:05 -0400

    "mv: be super/case_and_important_eye/own_day/be_day/next_week/come_way_with_last_world.py and super/have_large_case_by_different_fact/year_or_point/public_work/new_case_or_thing.py above next case"

---
+++ super/case_and_important_eye/own_day/be_day/next_week/come_way_with_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_part(str_arg):
+    different_man_or_small_work(str_arg)
+    print('important_time')
+
+def different_man_or_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_part('number')
