ed2e4867db97d7a445004685b2d17189783c12ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 12:45:47 -0400

    "add: do super/thing/go_week_at_able_fact.py, super/eye/great_day_or_group/thing/take_way_under_world.py, super/little_number/next_child/big_thing/good_world_and_eye.py and super/government/young_group/way/man_and_life/fact.py to different person"

---
+++ super/little_number/next_child/big_thing/good_world_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year_or_problem(str_arg):
+    old_year_or_fact(str_arg)
+    print('say_time_by_group')
+
+def old_year_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year_or_problem('case')
---
+++ super/thing/go_week_at_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part_or_fact(str_arg):
+    do_own_child_above_high_woman(str_arg)
+    print('high_person')
+
+def do_own_child_above_high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part_or_fact('go_old_week_about_last_time')
