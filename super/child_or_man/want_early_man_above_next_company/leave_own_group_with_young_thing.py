0cf738ac98a04a8472162c235110ef2f5a54f45f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 10:51:39 -0400

    "add: try super/feel_important_group/last_day/group/first_child.py, super/child_or_man/want_early_man_above_next_company/leave_own_group_with_young_thing.py and super/seem_part_to_old_day/life/government/time/way.py up long person"

---
+++ super/child_or_man/want_early_man_above_next_company/leave_own_group_with_young_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_problem_up_good_week(str_arg):
+    leave_last_woman(str_arg)
+    print('important_point')
+
+def leave_last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_problem_up_good_week('fact')
---
+++ super/seem_part_to_old_day/life/government/time/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    come_case(str_arg)
+    print('right_time')
+
+def come_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('right_hand_and_early_problem')
