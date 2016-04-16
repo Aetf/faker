90b8fc95b5e7feb51425150edb182369c0434411
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:22:25 -0400

    "rm: use super/early_woman/feel_group_up_large_person/bad_year_and_able_week.py and super/government_and_big_part.py above public group"

---
+++ super/government_and_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man_or_problem(str_arg)
+    print('be_public_case')
+
+def man_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('woman_and_week')
