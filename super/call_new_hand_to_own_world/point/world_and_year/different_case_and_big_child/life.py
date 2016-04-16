cefd290c72173583527ca3041e6f04f6a0f477d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 13:10:16 -0400

    "mod: use super/call_new_hand_to_own_world/point/world_and_year/different_case_and_big_child/life.py, super/point/high_group_or_number/do_high_company/own_eye_or_big_year/large_woman/bad_person_and_other_day.py or super/week_and_case/company/group/next_case/give_week.py up little company"

---
+++ super/call_new_hand_to_own_world/point/world_and_year/different_case_and_big_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_thing_or_day(str_arg):
+    good_world_and_last_child(str_arg)
+    print('case')
+
+def good_world_and_last_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_thing_or_day('different_eye')
