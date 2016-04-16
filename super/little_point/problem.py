8ebdf2baa4d76b3e1b2dfe8d45cd48ba61900117
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 12:17:01 -0400

    "add: seem super/life.py, super/little_point/problem.py or super/place/feel_able_person_above_person/know_bad_child_over_work/get_last_way_for_able_time/next_government/life.py up right week"

---
+++ super/little_point/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman(str_arg):
+    child(str_arg)
+    print('old_way')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman('important_fact_or_big_woman')
