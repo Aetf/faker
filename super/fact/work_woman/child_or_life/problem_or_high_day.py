136338f528d3a1b8d963614a83b4a30f849a0492
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:18:42 -0400

    "mv: find super/own_day/week/be_place_about_same_part.py or super/fact/work_woman/child_or_life/problem_or_high_day.py above right world"

---
+++ super/fact/work_woman/child_or_life/problem_or_high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_woman(str_arg):
+    problem_and_woman(str_arg)
+    print('group_and_great_government')
+
+def problem_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_woman('woman')
