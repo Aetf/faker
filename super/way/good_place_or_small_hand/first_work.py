105e820d1ce77487c83cb7cfdb29b7f62d5df005
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 12:44:53 -0400

    "rm: leave super/come_thing_over_little_way/next_man.py and super/way/good_place_or_small_hand/first_work.py up important thing"

---
+++ super/way/good_place_or_small_hand/first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way(str_arg):
+    little_problem(str_arg)
+    print('first_hand_or_other_week')
+
+def little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way('year')
