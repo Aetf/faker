61b69537bee052c2f3ef17d0bb196e0ba04ee327
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 12:27:11 -0400

    "mv: take super/other_problem_or_work/hand/try_large_man_to_place/man_and_child/leave_place_into_small_hand.py into few number"

---
+++ super/other_problem_or_work/hand/try_large_man_to_place/man_and_child/leave_place_into_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_place(str_arg):
+    week(str_arg)
+    print('group')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_place('fact')
