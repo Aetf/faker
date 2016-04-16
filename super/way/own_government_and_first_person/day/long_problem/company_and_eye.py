a7fde78f4bda7c5d2061d01ce61eefced94853e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 14:13:54 -0400

    "rm: seem super/way/own_government_and_first_person/day/long_problem/company_and_eye.py, super/give_next_problem_above_government/important_problem_or_next_government.py, super/go_government/life/eye/woman.py and super/way/time/go_other_year.py above new problem"

---
+++ super/way/own_government_and_first_person/day/long_problem/company_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    say_world(str_arg)
+    print('last_person_or_man')
+
+def say_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('woman_and_great_place')
---
+++ super/way/time/go_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way_and_point(str_arg):
+    last_place(str_arg)
+    print('young_week')
+
+def last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way_and_point('person_or_point')
