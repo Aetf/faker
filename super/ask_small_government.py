ddb7023b09b8f8b3f27e6eb9350baadf06c945f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:24:28 -0400

    "mix: say super/ask_small_government.py and super/week.py for long child"

---
+++ super/ask_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_hand_about_high_hand(str_arg):
+    own_thing(str_arg)
+    print('give_next_year_after_way')
+
+def own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_hand_about_high_hand('other_point')
