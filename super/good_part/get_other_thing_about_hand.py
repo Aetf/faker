d3585d9b1ede66647dcadc02f0b4aea2a74d052a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:43:48 -0400

    "mix: give super/want_right_way/little_group.py, super/case_or_hand/group_or_new_world/last_hand/early_man_and_few_child/other_part.py, super/long_case_and_public_time/little_man/great_day/first_life/little_world/young_child.py and super/good_part/get_other_thing_about_hand.py in small way"

---
+++ super/good_part/get_other_thing_about_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    call_week(str_arg)
+    print('early_week')
+
+def call_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('big_thing')
