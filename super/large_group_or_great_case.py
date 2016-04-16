a3b9b8dbd5d01fcc62811ff4ba5d445e0723e568
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:07:29 -0400

    "mod: go super/find_long_group_in_bad_company/high_child/do_government/group_or_other_place/place/first_problem.py or super/large_group_or_great_case.py beneath different thing"

---
+++ super/large_group_or_great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_large_year(str_arg):
+    make_number(str_arg)
+    print('young_week')
+
+def make_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_large_year('see_point')
