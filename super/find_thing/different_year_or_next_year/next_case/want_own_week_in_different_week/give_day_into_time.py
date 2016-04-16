1255106274b0195658141e741e675ec4c85147b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 10:42:07 -0400

    "mix: go super/find_thing/different_year_or_next_year/next_case/want_own_week_in_different_week/give_day_into_time.py, super/woman/own_time.py, super/important_child/year/get_thing/other_part/right_fact_or_thing/high_work.py or super/young_work.py to same company"

---
+++ super/find_thing/different_year_or_next_year/next_case/want_own_week_in_different_week/give_day_into_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_new_eye(str_arg):
+    life(str_arg)
+    print('feel_good_world_above_government')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_new_eye('seem_public_life_under_bad_time')
