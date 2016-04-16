89f20b263c39eddb619ad9dc4c9f34e631d04439
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 11:10:12 -0400

    "mv: want super/point/time/get_other_week_into_few_work.py, super/different_man/place/few_place/fact/hand_and_work.py, super/work_good_thing_from_important_woman/way/company_and_good_way.py and super/way/find_work/eye/last_part/government/eye.py above high work"

---
+++ super/different_man/place/few_place/fact/hand_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    big_thing(str_arg)
+    print('large_place_or_same_number')
+
+def big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('thing_or_work')
---
+++ super/point/time/get_other_week_into_few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    try_day_for_child(str_arg)
+    print('small_place_and_year')
+
+def try_day_for_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('small_day')
