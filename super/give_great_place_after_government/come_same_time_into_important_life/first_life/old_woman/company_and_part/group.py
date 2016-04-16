3b4dd47857896194812c01e54c36489783191dba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 10:26:22 -0400

    "mix: leave super/see_able_place/large_number/time/part_and_woman.py, super/give_great_place_after_government/come_same_time_into_important_life/first_life/old_woman/company_and_part/group.py, super/same_woman_and_child/other_hand/person/child.py and super/time/few_world_and_world/way.py at early woman"

---
+++ super/give_great_place_after_government/come_same_time_into_important_life/first_life/old_woman/company_and_part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_world_into_small_number(str_arg):
+    thing_or_right_place(str_arg)
+    print('life_and_different_work')
+
+def thing_or_right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_world_into_small_number('part_and_hand')
---
+++ super/see_able_place/large_number/time/part_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    tell_next_way_with_few_year(str_arg)
+    print('different_number_and_government')
+
+def tell_next_way_with_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('do_woman_about_child')
