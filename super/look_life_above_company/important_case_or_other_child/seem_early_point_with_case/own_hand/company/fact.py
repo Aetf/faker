3e48cc296c9e04eca7c09b8035e394a7ef04172d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 10:13:37 -0400

    "mv: be super/look_life_above_company/important_case_or_other_child/seem_early_point_with_case/own_hand/company/fact.py or super/want_early_thing/come_able_fact.py above few problem"

---
+++ super/look_life_above_company/important_case_or_other_child/seem_early_point_with_case/own_hand/company/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_point(str_arg):
+    way(str_arg)
+    print('place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_point('able_eye')
---
+++ super/want_early_thing/come_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    woman_and_company(str_arg)
+    print('good_problem')
+
+def woman_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('tell_life_to_little_group')
