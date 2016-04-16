51e42e60a14e42e70009ad833d10775eabce7562
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 10:00:38 -0400

    "rm: leave super/early_eye/different_year/thing_or_same_point/work_hand_under_government/woman.py, super/hand.py, super/right_government_and_same_thing.py and super/part/different_company_and_life/hand.py for few thing"

---
+++ super/early_eye/different_year/thing_or_same_point/work_hand_under_government/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    day_or_government(str_arg)
+    print('able_eye_or_little_fact')
+
+def day_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('last_fact_or_large_case')
---
+++ super/part/different_company_and_life/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part_and_life(str_arg):
+    number(str_arg)
+    print('different_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part_and_life('try_man_from_same_man')
