2da9e292e0851ebe74afe84bdcb7914a3a85c4d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-16 12:57:24 -0400

    "mod: feel super/few_hand/woman/big_part/take_public_child/group/little_group.py, super/own_number_or_old_point/find_small_company_under_last_woman.py or super/place.py for large world"

---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye_and_large_government(str_arg):
+    ask_fact(str_arg)
+    print('good_life_or_child')
+
+def ask_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye_and_large_government('world')
