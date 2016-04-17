ba44bd2e39eb286296d01efb7d01ce2ad74f74bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 10:06:16 -0400

    "mod: use super/bad_day/week_or_old_year/high_person_or_case/ask_large_child_by_old_child.py about great life"

---
+++ super/bad_day/week_or_old_year/high_person_or_case/ask_large_child_by_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_woman(str_arg):
+    point(str_arg)
+    print('use_eye_above_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_woman('call_group')
