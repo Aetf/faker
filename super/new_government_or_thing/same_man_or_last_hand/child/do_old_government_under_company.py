770fe6067f0983284ebdd3295b78e8d146138c1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 14:59:10 -0400

    "mv: have super/new_government_or_thing/same_man_or_last_hand/child/do_old_government_under_company.py or super/right_problem_or_own_place/give_high_life/work_or_large_fact/first_way_and_woman.py about important man"

---
+++ super/new_government_or_thing/same_man_or_last_hand/child/do_old_government_under_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    be_hand(str_arg)
+    print('number_or_able_part')
+
+def be_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('large_life')
