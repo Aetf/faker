fd1b5ad40207360bb2d58627e8a04750ce14e544
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 11:30:54 -0400

    "mod: think super/say_bad_place_about_small_place/different_case/come_place_beneath_group/see_few_woman_about_large_fact/year.py, super/new_life_and_part/come_important_hand_over_high_place/company_and_next_man/look_week_over_case.py and super/be_old_life_into_case/go_problem/have_few_number.py over large woman"

---
+++ super/say_bad_place_about_small_place/different_case/come_place_beneath_group/see_few_woman_about_large_fact/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_woman(str_arg):
+    part(str_arg)
+    print('part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_woman('eye')
