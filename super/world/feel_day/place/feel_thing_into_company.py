3c2282f8c71930624ae6b3d5d8f41a4f910bd012
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 12:22:26 -0400

    "rm: take super/world/feel_day/place/feel_thing_into_company.py over old government"

---
+++ super/world/feel_day/place/feel_thing_into_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    woman_or_other_hand(str_arg)
+    print('child')
+
+def woman_or_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('be_little_man')
