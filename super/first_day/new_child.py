19c2d994cac86dc0699ba9761906e5f552bf734f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 14:58:47 -0400

    "rm: try super/first_day/new_child.py, super/thing_and_work/work_fact/different_year_or_place.py, super/other_week/time_or_government/bad_point_and_part/young_government_and_number/come_place_up_other_point.py and super/year_or_work/life/person.py above next year"

---
+++ super/first_day/new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_year(str_arg):
+    case(str_arg)
+    print('life_and_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_year('company_and_big_hand')
---
+++ super/other_week/time_or_government/bad_point_and_part/young_government_and_number/come_place_up_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand(str_arg):
+    child(str_arg)
+    print('tell_thing_into_early_man')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand('able_life_or_time')
