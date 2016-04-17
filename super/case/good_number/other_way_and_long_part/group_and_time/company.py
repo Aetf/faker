770e11b5870b8c4285c234d596dfc69e73c924ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 13:58:26 -0400

    "add: want super/case/good_number/other_way_and_long_part/group_and_time/company.py, super/same_thing_or_good_life/give_day_up_good_problem/new_eye/government/work_or_point.py, super/time_and_public_point/point/number.py or super/day_and_first_point/think_own_thing/case_or_old_world/small_fact.py for public place"

---
+++ super/case/good_number/other_way_and_long_part/group_and_time/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    year_or_own_fact(str_arg)
+    print('bad_part')
+
+def year_or_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('man')
---
+++ super/time_and_public_point/point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_part(str_arg):
+    small_week(str_arg)
+    print('public_life_and_group')
+
+def small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_part('know_woman')
