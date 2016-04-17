b8f1e21313690a4838760248fc50426361a904ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 13:01:53 -0400

    "add: go super/week/know_work_above_week.py, super/point/try_problem/child/way_or_own_place/long_hand/call_man.py or super/same_case/time/eye/old_group/work.py after early child"

---
+++ super/week/know_work_above_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work(str_arg):
+    government_or_fact(str_arg)
+    print('great_year_and_same_time')
+
+def government_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work('way_and_other_world')
