d637422e20f44f201a8d750fa2ea9c64cde39bad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 14:07:28 -0400

    "mod: know super/bad_hand.py and super/time/important_time/time.py from large part"

---
+++ super/time/important_time/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_world(str_arg):
+    case(str_arg)
+    print('work_and_different_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_world('hand')
