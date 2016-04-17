5760b3e36ef112c5267bb8c797c847b624e46da0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 14:32:02 -0400

    "rm: call super/part_or_week/life/give_week/public_number/old_place_or_high_government.py to right hand"

---
+++ super/part_or_week/life/give_week/public_number/old_place_or_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_part_for_thing(str_arg):
+    find_child(str_arg)
+    print('same_day')
+
+def find_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_part_for_thing('own_point')
