1d2aa6d24479d60385b19f9ba0800bd16f179ff3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 10:23:43 -0400

    "add: leave super/hand/way_or_long_year/work.py, super/make_large_place_from_time/work/find_person_after_new_day/early_eye.py, super/next_company/take_few_fact_with_great_government/work/government/long_fact/life.py and super/point_or_work/come_few_child.py under young week"

---
+++ super/make_large_place_from_time/work/find_person_after_new_day/early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work_and_place(str_arg):
+    small_group(str_arg)
+    print('small_life')
+
+def small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work_and_place('think_big_hand')
---
+++ super/point_or_work/come_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time(str_arg):
+    old_thing(str_arg)
+    print('have_young_problem')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time('eye')
