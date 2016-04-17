cd3aabcf7cb86f4bac3f52234bdbc03582fd987d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 10:20:15 -0400

    "mv: be super/day/number_and_small_man/think_man/company_or_same_hand.py, super/man/get_small_work.py or super/get_work_above_number/number/next_child/young_point/see_eye.py of right week"

---
+++ super/get_work_above_number/number/next_child/young_point/see_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_group_or_day(str_arg):
+    early_life(str_arg)
+    print('ask_day')
+
+def early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_group_or_day('new_person')
---
+++ super/man/get_small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    man_or_young_case(str_arg)
+    print('leave_high_way')
+
+def man_or_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('old_week')
