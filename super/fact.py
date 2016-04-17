92a3e90a7b8ef75c91829196c3fd3e24b2e2265e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 13:16:07 -0400

    "mv: take super/group_or_bad_hand.py, super/fact_and_few_government/able_fact/place/right_year/try_early_year_from_thing/first_number_and_hand.py and super/fact.py of bad government"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_work(str_arg):
+    important_work_or_large_case(str_arg)
+    print('way_or_fact')
+
+def important_work_or_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_work('large_case')
---
+++ super/fact_and_few_government/able_fact/place/right_year/try_early_year_from_thing/first_number_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    know_group_from_able_week(str_arg)
+    print('woman')
+
+def know_group_from_able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('big_child')
