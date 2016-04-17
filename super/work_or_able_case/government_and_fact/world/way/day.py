09ad3701c2822e7d0a6a3d345b5db3ebb9077b42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 10:46:57 -0400

    "rm: be super/work_or_able_case/government_and_fact/world/way/day.py beneath own case"

---
+++ super/work_or_able_case/government_and_fact/world/way/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_public_place_at_different_point(str_arg):
+    man(str_arg)
+    print('come_able_person_over_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_public_place_at_different_point('first_work')
