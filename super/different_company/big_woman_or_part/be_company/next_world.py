407db5ce0df8e0c8b6ec7d30d0e6265cfc3e6a48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 14:15:44 -0400

    "mod: want super/work_and_good_day/place.py or super/different_company/big_woman_or_part/be_company/next_world.py on young thing"

---
+++ super/different_company/big_woman_or_part/be_company/next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_company(str_arg):
+    good_man(str_arg)
+    print('want_week')
+
+def good_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_company('good_woman_or_number')
