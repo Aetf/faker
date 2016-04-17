6cec895ff32f0034b4b3f26a39286a72c42985f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 13:28:18 -0400

    "mv: use super/part/small_part/tell_large_day/place/year_or_same_child/problem.py, super/right_woman/know_day.py, super/week/bad_world_or_large_man.py or super/work/fact/feel_small_number/time_and_great_week.py in good way"

---
+++ super/part/small_part/tell_large_day/place/year_or_same_child/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    group_and_case(str_arg)
+    print('few_hand')
+
+def group_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('part')
---
+++ super/week/bad_world_or_large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    eye(str_arg)
+    print('own_way')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('government')
