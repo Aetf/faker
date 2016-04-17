d403287516f4dcd8a402f655bc08421b08d09bc4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 10:04:11 -0400

    "mv: find super/group/large_point/hand_or_good_way/go_world_about_number.py, super/number_or_young_hand/get_person/company/great_company_and_world.py, super/new_company.py or super/woman/big_part_or_high_time/woman.py up young person"

---
+++ super/group/large_point/hand_or_good_way/go_world_about_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_man(str_arg):
+    way(str_arg)
+    print('different_government')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_man('time')
---
+++ super/new_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_bad_case(str_arg):
+    important_person(str_arg)
+    print('feel_small_person_in_world')
+
+def important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_bad_case('great_thing_and_fact')
