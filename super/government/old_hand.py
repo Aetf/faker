128c7c5f071494396ccea79274eaca7c0efd7e8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 13:52:21 -0400

    "rm: have super/government/old_hand.py and super/part/first_woman/part_and_company/think_small_problem_at_good_person.py into little woman"

---
+++ super/government/old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    see_life_of_important_thing(str_arg)
+    print('life')
+
+def see_life_of_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('eye')
