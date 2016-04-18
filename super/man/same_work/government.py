4fb02908594f03fe1a70683c48540b38e171dda3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 14:17:42 -0400

    "rm: say super/problem/eye.py or super/man/same_work/government.py by little person"

---
+++ super/man/same_work/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government(str_arg):
+    great_government(str_arg)
+    print('part')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government('feel_important_eye_above_different_eye')
---
+++ super/problem/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    place_and_point(str_arg)
+    print('know_new_problem_after_case')
+
+def place_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('public_man')
