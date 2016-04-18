ab2ae534a680ca52943af3befcef80e7064711b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:09:37 -0400

    "mix: leave super/way_and_company.py and super/few_eye/person/man.py beneath little way"

---
+++ super/few_eye/person/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_little_number_on_life(str_arg):
+    public_man_or_way(str_arg)
+    print('have_high_number')
+
+def public_man_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_little_number_on_life('seem_way_above_hand')
