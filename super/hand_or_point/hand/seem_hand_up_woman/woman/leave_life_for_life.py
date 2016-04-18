15c27faf9d1f68ffc478af5e458882c82bfec161
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 14:47:50 -0400

    "mv: get super/hand_or_point/hand/seem_hand_up_woman/woman/leave_life_for_life.py above next point"

---
+++ super/hand_or_point/hand/seem_hand_up_woman/woman/leave_life_for_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    eye(str_arg)
+    print('year')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('young_group')
