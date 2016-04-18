dccc2c647a7f6a1b033b6617705728e850d02771
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:34:42 -0400

    "mv: look super/case_and_woman/say_hand_to_own_fact/high_hand_and_bad_time/world/ask_time.py, super/world/take_high_time_beneath_important_day/life/do_part/new_problem_or_long_person.py, super/time/other_life_and_year/year/man/company_or_life.py or super/man/week/hand/little_eye.py into early thing"

---
+++ super/man/week/hand/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    old_number_or_own_world(str_arg)
+    print('first_time')
+
+def old_number_or_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('week')
---
+++ super/world/take_high_time_beneath_important_day/life/do_part/new_problem_or_long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_eye(str_arg):
+    woman(str_arg)
+    print('old_fact')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_eye('long_man_and_young_place')
