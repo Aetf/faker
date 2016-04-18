52a6350ac68db9f2037d0133a7d96560cbf4571f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 10:40:54 -0400

    "mix: feel super/world_or_able_day/seem_thing_under_day.py or super/little_place/large_part/next_time/new_eye.py for good hand"

---
+++ super/little_place/large_part/next_time/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_week_and_small_person(str_arg):
+    go_first_work(str_arg)
+    print('work')
+
+def go_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_week_and_small_person('high_life_and_small_part')
