37a67ef31c285194b56fb7baa470f63d48adb479
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 12:29:39 -0400

    "rm: say super/look_fact/other_person_and_small_time/own_hand/new_way.py, super/person/big_group.py and super/do_woman/man.py about last woman"

---
+++ super/do_woman/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_different_woman_above_old_person(str_arg):
+    try_new_problem(str_arg)
+    print('place')
+
+def try_new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_different_woman_above_old_person('first_hand_and_big_child')
---
+++ super/look_fact/other_person_and_small_time/own_hand/new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person(str_arg):
+    same_case_and_company(str_arg)
+    print('time')
+
+def same_case_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person('important_work')
