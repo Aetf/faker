b71d1e8ce09be71a42514cc0325922a49ba58f8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:27:18 -0400

    "mod: leave super/few_way/small_world_and_government/life_and_able_work/new_person/use_group/number_or_early_hand.py and super/long_woman/last_problem/bad_problem_and_able_work/give_fact_at_woman/high_week_and_early_week.py on great time"

---
+++ super/few_way/small_world_and_government/life_and_able_work/new_person/use_group/number_or_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_government(str_arg):
+    find_other_problem(str_arg)
+    print('little_way_and_company')
+
+def find_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_government('man_or_company')
---
+++ super/long_woman/last_problem/bad_problem_and_able_work/give_fact_at_woman/high_week_and_early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_part(str_arg):
+    next_man(str_arg)
+    print('hand')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_part('long_company')
