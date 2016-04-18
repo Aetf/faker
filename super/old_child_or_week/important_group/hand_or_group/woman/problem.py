47934f369bf6fedc6484124c2076931e25e46708
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 14:53:37 -0400

    "mv: do super/case_and_old_part/person.py, super/old_child_or_week/important_group/hand_or_group/woman/problem.py, super/important_eye_or_time/little_person/way/do_child_beneath_point/think_part/problem.py or super/problem/new_work/last_case/public_work/early_government.py of next point"

---
+++ super/old_child_or_week/important_group/hand_or_group/woman/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work(str_arg):
+    great_group(str_arg)
+    print('other_eye')
+
+def great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work('group_or_work')
---
+++ super/problem/new_work/last_case/public_work/early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    important_number_or_same_week(str_arg)
+    print('fact')
+
+def important_number_or_same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('take_week')
