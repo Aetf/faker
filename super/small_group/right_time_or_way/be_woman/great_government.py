05c4df3e0be85d69e0abfd042557937c7151bea4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 10:49:54 -0400

    "mv: find super/small_group/right_time_or_way/be_woman/great_government.py, super/first_company_or_hand/point.py, super/large_eye/see_good_eye/life/time.py and super/old_year/tell_old_eye/point_or_company/own_hand/day/large_number_and_bad_fact.py in little group"

---
+++ super/small_group/right_time_or_way/be_woman/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    other_company(str_arg)
+    print('problem')
+
+def other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('place')
