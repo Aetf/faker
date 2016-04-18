85b34a203d06d0c24d9e146d7753943a69ab8a8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:26:02 -0400

    "mod: find super/able_group_or_right_hand/world_and_large_fact.py, super/feel_great_case_to_day/different_government_and_woman/week/place/own_life.py and super/last_man.py beneath important group"

---
+++ super/feel_great_case_to_day/different_government_and_woman/week/place/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_number_to_little_work(str_arg):
+    say_large_man_after_problem(str_arg)
+    print('case_or_first_fact')
+
+def say_large_man_after_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_number_to_little_work('other_place')
