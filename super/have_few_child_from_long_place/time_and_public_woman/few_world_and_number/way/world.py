554c5dda170105410a5a40cddda53b2ced6a1bc7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 10:46:19 -0400

    "rm: go super/have_few_child_from_long_place/time_and_public_woman/few_world_and_number/way/world.py or super/look_thing_from_important_man/say_point_under_man/government/problem/little_number/seem_public_number.py by few government"

---
+++ super/have_few_child_from_long_place/time_and_public_woman/few_world_and_number/way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    high_thing(str_arg)
+    print('way')
+
+def high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('try_last_work_beneath_government')
---
+++ super/look_thing_from_important_man/say_point_under_man/government/problem/little_number/seem_public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    old_case_or_public_hand(str_arg)
+    print('part_or_work')
+
+def old_case_or_public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('seem_work_at_first_case')
