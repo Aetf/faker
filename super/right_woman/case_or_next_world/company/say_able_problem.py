2feac42363cb3981c211f53d0d03018042464bd0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:19:43 -0400

    "add: do super/right_woman/case_or_next_world/company/say_able_problem.py above own problem"

---
+++ super/right_woman/case_or_next_world/company/say_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_world(str_arg):
+    part(str_arg)
+    print('old_number')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_world('man_or_fact')
