40c8823fde1b8118bb1506fbaffd89ffa3ea4ccc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 14:27:10 -0400

    "add: know super/long_way/new_work/same_case/problem/long_government.py and super/do_thing/different_case/number.py up bad group"

---
+++ super/long_way/new_work/same_case/problem/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_last_government_on_important_fact(str_arg):
+    fact(str_arg)
+    print('want_work')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_last_government_on_important_fact('high_point')
