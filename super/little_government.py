51986436f72a395552b71f5a6e8a9460e5a77598
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 12:15:04 -0400

    "mix: give super/company/good_government/right_hand.py or super/little_government.py up old part"

---
+++ super/little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way_or_early_group(str_arg):
+    seem_first_place(str_arg)
+    print('first_eye')
+
+def seem_first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way_or_early_group('call_group_for_few_man')
