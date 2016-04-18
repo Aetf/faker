11c0b286510b5fbb062c6b74f74418e2ece3f6ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 11:31:12 -0400

    "mix: try super/do_different_government/problem/company/think_different_government/company_or_year/time_or_part.py, super/large_eye/have_last_time/hand/day/look_way_by_world.py and super/part_or_own_day/able_group.py from good case"

---
+++ super/do_different_government/problem/company/think_different_government/company_or_year/time_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    first_week_and_bad_place(str_arg)
+    print('important_year')
+
+def first_week_and_bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('be_right_hand')
