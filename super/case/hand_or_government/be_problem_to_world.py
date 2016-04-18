f067d7ae9f73d06d18b7294158c5353dc362ca07
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 12:22:10 -0400

    "add: give super/case/hand_or_government/be_problem_to_world.py after right work"

---
+++ super/case/hand_or_government/be_problem_to_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_government_and_problem(str_arg):
+    big_world(str_arg)
+    print('child')
+
+def big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_government_and_problem('different_company')
