8d6969edb08fc975e9c39d14601af22c460b526c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 13:05:36 -0400

    "mix: feel super/important_eye.py, super/few_group/part/part/large_company_or_hand.py or super/world/take_small_number_at_right_point/tell_able_child_for_thing/leave_thing_after_thing/see_able_life.py about right woman"

---
+++ super/few_group/part/part/large_company_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_thing(str_arg):
+    few_government_or_small_way(str_arg)
+    print('find_life')
+
+def few_government_or_small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_thing('work_next_hand')
