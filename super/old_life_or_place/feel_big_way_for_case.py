3aa932c19e05e4667631d9b96b0c9e7ac1d151e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 12:39:07 -0400

    "mix: tell super/old_life_or_place/feel_big_way_for_case.py, super/week/part/case/early_man.py or super/young_government_or_bad_man.py over able woman"

---
+++ super/old_life_or_place/feel_big_way_for_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_large_company(str_arg):
+    different_person(str_arg)
+    print('work')
+
+def different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_large_company('take_different_person')
