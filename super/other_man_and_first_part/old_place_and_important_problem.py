ada06d131d5504289b5c11346c7b11de4a78f964
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 14:53:04 -0400

    "mv: see super/work_great_week_of_point/good_problem_and_thing/last_group/small_eye/group_or_case.py, super/public_eye/year.py or super/other_man_and_first_part/old_place_and_important_problem.py for old problem"

---
+++ super/other_man_and_first_part/old_place_and_important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_work(str_arg):
+    go_public_man(str_arg)
+    print('man_and_world')
+
+def go_public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_work('fact_or_few_week')
