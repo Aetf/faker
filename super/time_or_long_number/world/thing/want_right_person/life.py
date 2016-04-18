158d2a75c05a13f93c796f39ee33b11ebdd8a0c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-18 12:47:15 -0400

    "mv: look super/time_or_long_number/world/thing/want_right_person/life.py on different world"

---
+++ super/time_or_long_number/world/thing/want_right_person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_next_year_in_small_way(str_arg):
+    feel_different_fact(str_arg)
+    print('important_time')
+
+def feel_different_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_next_year_in_small_way('fact_or_case')
