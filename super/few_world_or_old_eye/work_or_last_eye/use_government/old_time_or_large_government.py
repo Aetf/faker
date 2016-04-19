2ee1728a21a15d49cba92a6ae1057716854f225d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 11:45:00 -0400

    "rm: work super/little_child_or_large_way/group.py, super/few_world_or_old_eye/work_or_last_eye/use_government/old_time_or_large_government.py or super/eye/government.py with high part"

---
+++ super/few_world_or_old_eye/work_or_last_eye/use_government/old_time_or_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    own_week(str_arg)
+    print('year_or_point')
+
+def own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('last_government')
---
+++ super/little_child_or_large_way/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_large_day(str_arg):
+    different_child(str_arg)
+    print('right_hand')
+
+def different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_large_day('next_problem')
