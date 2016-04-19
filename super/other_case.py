f24e8c5cda0d0ceff751d439c3f5f59719be705c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 11:35:31 -0400

    "mix: say super/group/long_government/own_year/woman_and_early_fact.py, super/thing/great_year/point_and_different_place/think_day_into_large_week/right_number_and_little_government.py and super/other_case.py in high woman"

---
+++ super/other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    large_person(str_arg)
+    print('same_hand')
+
+def large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('problem')
