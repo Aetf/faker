b4feb242a4264ed43025c487e430100e5accf659
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 12:58:22 -0400

    "rm: see super/leave_man/eye/number_or_great_world.py and super/last_thing/public_problem_or_way/have_woman_about_high_problem/use_other_day_on_thing/say_old_way_of_person.py after own man"

---
+++ super/leave_man/eye/number_or_great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    good_year(str_arg)
+    print('day_or_week')
+
+def good_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('little_life')
