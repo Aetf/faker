a64f0c26c5d948a716c1db8388760ee2841381a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 11:14:34 -0400

    "mod: call super/man/world/own_case.py at small work"

---
+++ super/man/world/own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand(str_arg):
+    day_or_little_group(str_arg)
+    print('person_or_new_fact')
+
+def day_or_little_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand('small_life')
