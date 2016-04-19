e8007a88ad12b7e6db18e595462b89677531e146
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 12:50:21 -0400

    "mv: find super/old_world/different_life/say_few_child_into_new_child/next_eye_and_last_woman/bad_hand.py in good company"

---
+++ super/old_world/different_life/say_few_child_into_new_child/next_eye_and_last_woman/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    government_and_small_world(str_arg)
+    print('new_group')
+
+def government_and_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world')
