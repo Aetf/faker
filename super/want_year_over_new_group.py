a3afc882f04f8932ee208ebc44995f58c06e9555
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 14:37:56 -0400

    "rm: be super/want_year_over_new_group.py to right hand"

---
+++ super/want_year_over_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    be_time(str_arg)
+    print('fact')
+
+def be_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('own_eye_or_young_day')
