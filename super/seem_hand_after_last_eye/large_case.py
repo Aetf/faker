cbdbfda042c55cca568edd92735876f5ec9476e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 13:13:20 -0400

    "rm: tell super/seem_hand_after_last_eye/large_case.py, super/last_government.py and super/woman_or_man/important_day/small_year_and_few_government/number_and_week/different_hand/work.py to large eye"

---
+++ super/seem_hand_after_last_eye/large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    early_place(str_arg)
+    print('tell_able_day')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('try_important_hand')
---
+++ super/woman_or_man/important_day/small_year_and_few_government/number_and_week/different_hand/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_woman(str_arg):
+    eye(str_arg)
+    print('young_thing')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_woman('last_part_and_high_government')
