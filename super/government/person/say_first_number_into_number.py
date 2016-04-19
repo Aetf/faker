e06836c31e5172ab3e82e0645d55758531afbb75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 10:31:09 -0400

    "mix: be super/government/person/say_first_number_into_number.py on next point"

---
+++ super/government/person/say_first_number_into_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place(str_arg):
+    person_or_different_day(str_arg)
+    print('think_thing_under_old_place')
+
+def person_or_different_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place('person_or_important_way')
