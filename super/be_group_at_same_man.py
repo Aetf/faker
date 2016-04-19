6c625acf09baa9f1c6cd521fc23e6b2bf7ff75ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 11:22:53 -0400

    "mv: go super/case/early_child_and_early_eye/different_man/feel_bad_point/be_young_life_by_world/same_point.py, super/be_group_at_same_man.py and super/same_thing_or_time/bad_woman.py from next hand"

---
+++ super/be_group_at_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_few_person(str_arg):
+    see_year_beneath_thing(str_arg)
+    print('first_eye')
+
+def see_year_beneath_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_few_person('get_small_man_for_same_year')
---
+++ super/same_thing_or_time/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    take_high_week_beneath_time(str_arg)
+    print('young_life_or_last_part')
+
+def take_high_week_beneath_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('seem_new_group_to_work')
