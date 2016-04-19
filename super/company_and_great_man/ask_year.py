cc3e23738c3aed277a93bb7ca5699a27d2a57a4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 14:49:51 -0400

    "add: think super/take_day_for_world/woman/time_or_early_problem.py, super/government/early_person_and_number/do_government/other_world_and_eye/ask_little_case/long_point_or_week.py, super/important_year.py or super/company_and_great_man/ask_year.py after young hand"

---
+++ super/company_and_great_man/ask_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week_or_person(str_arg):
+    child(str_arg)
+    print('government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week_or_person('own_fact_or_time')
---
+++ super/government/early_person_and_number/do_government/other_world_and_eye/ask_little_case/long_point_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_part_or_high_week(str_arg):
+    different_way(str_arg)
+    print('new_group_and_important_way')
+
+def different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_part_or_high_week('early_week')
