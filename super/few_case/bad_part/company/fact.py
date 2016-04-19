16c32f15379f9161b9374c75c9ba48180c008d63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-19 10:43:22 -0400

    "mix: want super/few_case/bad_part/company/fact.py or super/right_problem/new_work/young_person_and_new_problem/have_little_part_about_man/week/different_year_or_number.py into early company"

---
+++ super/few_case/bad_part/company/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    thing(str_arg)
+    print('woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('right_point')
---
+++ super/right_problem/new_work/young_person_and_new_problem/have_little_part_about_man/week/different_year_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_few_government_on_work(str_arg):
+    other_group(str_arg)
+    print('tell_young_problem')
+
+def other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_few_government_on_work('same_year_and_woman')
