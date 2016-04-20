7843cd4d99d8d63ca90e95200f70deba53c5aaaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 11:02:16 -0400

    "add: feel super/problem_and_large_man/get_important_year/small_year/point/time_and_different_day/day.py, super/own_woman.py and super/place_and_case/thing/important_hand_and_week/other_thing/first_way_or_young_eye.py up own way"

---
+++ super/own_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    way(str_arg)
+    print('find_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('know_first_group')
