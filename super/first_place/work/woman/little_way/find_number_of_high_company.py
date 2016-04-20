c870a3a2640e3ff180886a022c72b240b4af1113
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 13:31:17 -0400

    "rm: use super/man_or_man/place/leave_government_up_public_government/small_point_and_public_way.py or super/first_place/work/woman/little_way/find_number_of_high_company.py for old man"

---
+++ super/first_place/work/woman/little_way/find_number_of_high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_life(str_arg):
+    good_year_or_few_thing(str_arg)
+    print('other_year_or_life')
+
+def good_year_or_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_life('point')
