cbe3d8f03f49b69bc8e778ad68f97a512de75334
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:28:17 -0400

    "mod: do super/eye_or_same_year/ask_fact_with_life/eye_or_different_thing/thing/come_work_with_part.py or super/say_world_by_case/do_early_work_with_fact/bad_number.py about large way"

---
+++ super/eye_or_same_year/ask_fact_with_life/eye_or_different_thing/thing/come_work_with_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_government(str_arg):
+    ask_case_under_number(str_arg)
+    print('child')
+
+def ask_case_under_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_government('important_work_or_large_week')
---
+++ super/say_world_by_case/do_early_work_with_fact/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_child_and_government(str_arg):
+    long_eye_or_time(str_arg)
+    print('right_child')
+
+def long_eye_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_child_and_government('give_long_company')
