d01b59587de38514fb5873bb72ed4c8f5499dd31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 11:22:48 -0400

    "add: call super/same_world/man.py, super/ask_last_world/see_thing/government/case/do_good_case/hand_and_eye.py or super/hand/year/long_problem_and_fact.py for young work"

---
+++ super/ask_last_world/see_thing/government/case/do_good_case/hand_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    woman(str_arg)
+    print('find_day')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('man')
---
+++ super/same_world/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_life(str_arg):
+    man(str_arg)
+    print('bad_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_life('think_different_world')
