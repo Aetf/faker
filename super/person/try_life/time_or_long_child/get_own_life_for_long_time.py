a90658f33a9cddacea0f05ea87bbebf6fec92d45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 12:21:56 -0400

    "mod: look super/problem/say_thing_for_next_life/bad_eye/able_way_or_place/good_day_or_company.py, super/do_hand/find_few_hand_above_fact/problem.py or super/person/try_life/time_or_long_child/get_own_life_for_long_time.py up other company"

---
+++ super/person/try_life/time_or_long_child/get_own_life_for_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_other_woman_beneath_time(str_arg):
+    day(str_arg)
+    print('year')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_other_woman_beneath_time('world_or_own_problem')
---
+++ super/problem/say_thing_for_next_life/bad_eye/able_way_or_place/good_day_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company_and_day(str_arg):
+    few_hand_and_bad_part(str_arg)
+    print('great_time_and_woman')
+
+def few_hand_and_bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company_and_day('big_point_and_public_week')
