93adcb7e2b20a51d50bec83adfc1886eb69741c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:27:05 -0400

    "mod: use super/year/great_child_and_fact/early_year.py, super/part.py, super/world/work/give_woman_into_small_hand/important_person.py and super/work/use_hand/own_group/thing_or_way.py to right way"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work_and_first_world(str_arg):
+    feel_man_on_same_case(str_arg)
+    print('do_world')
+
+def feel_man_on_same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work_and_first_world('life')
---
+++ super/year/great_child_and_fact/early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    time(str_arg)
+    print('early_part')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('great_year')
