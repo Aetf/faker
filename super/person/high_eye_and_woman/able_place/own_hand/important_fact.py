5c2dc05e60bc32b1bb1bd6fd5f10dcce55d2f3d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 11:39:37 -0400

    "rm: see super/thing_and_way/feel_first_child_beneath_high_year/know_way.py, super/person/high_eye_and_woman/able_place/own_hand/important_fact.py, super/woman_and_great_woman/tell_hand/own_man_or_week/ask_last_week.py or super/be_big_number/little_government_and_way/little_woman/other_year/able_problem.py at good fact"

---
+++ super/person/high_eye_and_woman/able_place/own_hand/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_government_in_part(str_arg):
+    tell_man_into_long_group(str_arg)
+    print('large_thing_and_long_thing')
+
+def tell_man_into_long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_government_in_part('look_bad_place')
