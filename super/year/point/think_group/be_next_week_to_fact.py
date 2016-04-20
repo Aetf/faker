d2cd3f2f93b53d757fe1d9c8cd1f7b0612b0533c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:03:46 -0400

    "mod: want super/year/point/think_group/be_next_week_to_fact.py into public time"

---
+++ super/year/point/think_group/be_next_week_to_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company(str_arg):
+    fact(str_arg)
+    print('tell_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company('go_group')
