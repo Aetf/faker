517777267d93754571b4b437df14ff9741d4bae9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:55:24 -0400

    "rm: find super/leave_point/big_hand_and_last_place/next_time/look_bad_woman_over_first_point.py over first eye"

---
+++ super/leave_point/big_hand_and_last_place/next_time/look_bad_woman_over_first_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    give_new_work(str_arg)
+    print('last_day_or_important_person')
+
+def give_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('first_year_or_young_group')
