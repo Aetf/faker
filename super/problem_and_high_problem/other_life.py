c9e383deb30eac1ec65d327c53594c8b3e82f6c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 11:41:33 -0400

    "mix: make super/problem_and_high_problem/other_life.py, super/little_group_or_big_child/high_year/first_way.py and super/thing/first_week_or_able_point/try_problem_with_good_work.py over right number"

---
+++ super/problem_and_high_problem/other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact(str_arg):
+    early_time_and_young_eye(str_arg)
+    print('come_part')
+
+def early_time_and_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact('person')
