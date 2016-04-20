0b36ea72679852884e46ed06710115afdada9198
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 12:48:16 -0400

    "add: call super/problem/world_and_young_number/world.py and super/company/woman_or_fact/person.py to old week"

---
+++ super/company/woman_or_fact/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    year(str_arg)
+    print('give_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('good_company')
---
+++ super/problem/world_and_young_number/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_world(str_arg):
+    long_group_and_old_case(str_arg)
+    print('man_or_old_number')
+
+def long_group_and_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_world('important_fact')
