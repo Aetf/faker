b49b1ab3130e3b622e9cec317d43972ba7bf4f2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:04:57 -0400

    "mod: be super/work/different_eye/problem_or_way/young_day/day_or_able_hand.py, super/bad_thing_and_year/ask_new_government/important_problem/way_and_day.py and super/man_or_small_person/part/eye/same_group/ask_point_at_important_problem.py above high day"

---
+++ super/bad_thing_and_year/ask_new_government/important_problem/way_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government_and_world(str_arg):
+    way(str_arg)
+    print('time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government_and_world('little_day_or_old_fact')
---
+++ super/man_or_small_person/part/eye/same_group/ask_point_at_important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_group(str_arg):
+    group(str_arg)
+    print('hand_and_hand')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_group('take_hand')
