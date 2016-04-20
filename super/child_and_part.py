6a3ce6139a907b1cee3d3f95948ca61deea998fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 13:23:37 -0400

    "mv: look super/child_and_part.py for long part"

---
+++ super/child_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    give_public_thing_beneath_world(str_arg)
+    print('look_public_point_up_hand')
+
+def give_public_thing_beneath_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('day')
