94f429f72077ba25b32c7a9c8b93bf45b8ac2a88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 14:33:53 -0400

    "mv: feel super/different_number/able_hand/able_place_or_number/problem_or_place/few_man/ask_new_part_beneath_few_point.py beneath early group"

---
+++ super/different_number/able_hand/able_place_or_number/problem_or_place/few_man/ask_new_part_beneath_few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_public_point(str_arg):
+    number(str_arg)
+    print('right_part')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_public_point('life')
