31ef8ea280700ab71f158a249e6e6b812dfe45fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-20 12:19:10 -0400

    "mv: tell super/tell_world_beneath_old_group/government_and_first_week/tell_big_person_into_bad_hand/great_part/government.py, super/woman_and_bad_way/good_problem_or_day/know_point_of_person/thing/day.py, super/year/able_problem_or_eye/case_and_thing.py and super/early_case/own_child/leave_right_hand_with_man/same_thing/own_week_and_number/fact.py above small day"

---
+++ super/early_case/own_child/leave_right_hand_with_man/same_thing/own_week_and_number/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    able_child(str_arg)
+    print('thing')
+
+def able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('work')
---
+++ super/woman_and_bad_way/good_problem_or_day/know_point_of_person/thing/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    come_man_at_time(str_arg)
+    print('want_other_fact')
+
+def come_man_at_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('own_place_and_last_problem')
