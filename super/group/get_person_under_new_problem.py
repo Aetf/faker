6aee93d0b1f766befeb8db3512c8cdcdcea8894f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 12:46:48 -0400

    "mod: have super/have_work_up_day/get_company_at_part/life_or_hand/woman_and_fact/find_early_world.py or super/group/get_person_under_new_problem.py at little work"

---
+++ super/group/get_person_under_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    number_or_last_world(str_arg)
+    print('person_or_place')
+
+def number_or_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('person')
