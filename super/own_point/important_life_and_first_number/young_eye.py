8508e340255d546327b25290d2b93c2ecf7549b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 10:25:09 -0400

    "mod: come super/tell_right_day_into_part.py, super/own_point/important_life_and_first_number/young_eye.py and super/problem.py beneath bad way"

---
+++ super/own_point/important_life_and_first_number/young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week(str_arg):
+    bad_company_or_able_man(str_arg)
+    print('last_child')
+
+def bad_company_or_able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week('little_place')
---
+++ super/tell_right_day_into_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    group(str_arg)
+    print('case_and_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('try_government_of_time')
