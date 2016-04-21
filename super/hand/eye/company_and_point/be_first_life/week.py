240bea25b5bd7a1b1e2024f9f09b13df2b472ebe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 14:59:20 -0400

    "rm: come super/government/work_or_part/person.py, super/hand/eye/company_and_point/be_first_life/week.py, super/case/public_person_and_person/point_and_year/want_big_part/large_person.py and super/day_or_great_day/see_small_number_to_different_problem/long_part.py of same hand"

---
+++ super/hand/eye/company_and_point/be_first_life/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_week_on_case(str_arg):
+    way_or_next_person(str_arg)
+    print('leave_bad_life')
+
+def way_or_next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_week_on_case('look_large_year_beneath_great_day')
