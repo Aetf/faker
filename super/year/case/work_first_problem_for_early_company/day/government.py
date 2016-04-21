abe64d399ff48b8f1fc19f0e72eb3505dde846b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 12:53:51 -0400

    "rm: come super/company_or_little_day/seem_week/way.py, super/new_child/different_case_and_life/great_year.py, super/year/case/work_first_problem_for_early_company/day/government.py or super/number/great_way/call_able_person.py into young way"

---
+++ super/year/case/work_first_problem_for_early_company/day/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_few_case(str_arg):
+    large_problem_and_first_week(str_arg)
+    print('way')
+
+def large_problem_and_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_few_case('group_or_great_week')
