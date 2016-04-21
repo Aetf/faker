17ca1e6b963826f67ea04cef91f08fdda1b76739
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 14:45:23 -0400

    "mv: want super/work_high_work_by_number/leave_bad_part/think_point_over_hand/find_right_world_under_long_hand.py, super/little_case/get_last_man_to_number.py, super/first_case/get_next_group_for_eye.py or super/be_big_life_by_week/work_last_government_above_year.py for first year"

---
+++ super/be_big_life_by_week/work_last_government_above_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    public_day_and_eye(str_arg)
+    print('group_and_problem')
+
+def public_day_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('tell_week')
---
+++ super/first_case/get_next_group_for_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_important_world_with_company(str_arg):
+    try_place(str_arg)
+    print('thing_or_next_woman')
+
+def try_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_important_world_with_company('world_or_government')
