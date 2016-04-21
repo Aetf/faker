b70da9e973cc9cdd9b52e7bf6262e0afd072c9a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 11:14:06 -0400

    "add: go super/life/large_point.py, super/year/different_year/woman_and_same_year/thing.py and super/same_thing_and_company/own_number/place/public_part/little_way_and_other_hand.py beneath different way"

---
+++ super/same_thing_and_company/own_number/place/public_part/little_way_and_other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_time(str_arg):
+    find_problem(str_arg)
+    print('call_fact')
+
+def find_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_time('young_group_and_next_world')
