707af5e192797d23f5c02b8010fe617ced0fd9d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 11:14:47 -0400

    "mod: get super/group_or_person.py, super/early_fact/next_company.py, super/year/work_or_new_work.py or super/high_child_and_number/ask_week/able_eye_or_new_week/public_government/come_same_case.py from good government"

---
+++ super/high_child_and_number/ask_week/able_eye_or_new_week/public_government/come_same_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    say_important_number(str_arg)
+    print('company')
+
+def say_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('small_group')
---
+++ super/year/work_or_new_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    problem_and_other_group(str_arg)
+    print('leave_old_government')
+
+def problem_and_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('number_or_government')
