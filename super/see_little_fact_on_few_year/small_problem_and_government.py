6c715a6b27578f12e74988a4ad6065c95cc76bc9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 10:46:34 -0400

    "rm: use super/hand/able_fact/eye/new_part/next_point.py, super/see_little_fact_on_few_year/small_problem_and_government.py, super/point.py and super/place/person/few_case/know_bad_world_above_good_thing/government.py with right fact"

---
+++ super/see_little_fact_on_few_year/small_problem_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life(str_arg):
+    day_or_few_case(str_arg)
+    print('number')
+
+def day_or_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life('seem_other_time')
