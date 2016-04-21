05aa9c03008d85fe403d131aa8e034f5b9212bdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 13:20:17 -0400

    "rm: call super/ask_case_into_point/high_child/young_man_or_next_week.py, super/group.py and super/high_time/few_group/company/small_way_or_great_time/feel_woman.py into first hand"

---
+++ super/high_time/few_group/company/small_way_or_great_time/feel_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_way_up_group(str_arg):
+    go_last_life_of_early_place(str_arg)
+    print('other_company')
+
+def go_last_life_of_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_way_up_group('work')
