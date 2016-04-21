8252b6ec3f58f362588f8382cdb397144bcc56b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 10:07:31 -0400

    "mod: come super/big_hand/case_or_woman/come_man_in_other_group.py or super/think_small_world_over_point/use_big_world_after_life/big_thing.py on large company"

---
+++ super/big_hand/case_or_woman/come_man_in_other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place(str_arg):
+    new_way_or_same_eye(str_arg)
+    print('young_place')
+
+def new_way_or_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place('part')
