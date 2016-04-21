40754f3d99e7b89f816a2df94d617d6d4ae95bbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 10:20:25 -0400

    "add: come super/fact.py, super/fact/work_or_first_woman/way/part.py or super/look_person/find_old_number_over_person/little_week/look_early_work.py about great case"

---
+++ super/fact/work_or_first_woman/way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    know_person(str_arg)
+    print('own_woman')
+
+def know_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('fact_and_government')
