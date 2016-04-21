9e14856900d81c90b68bfc0ff653c1f826b2635e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 11:23:25 -0400

    "add: see super/next_day_or_work/high_world.py of important life"

---
+++ super/next_day_or_work/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_other_thing(str_arg):
+    think_bad_problem(str_arg)
+    print('right_case')
+
+def think_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_other_thing('young_part_and_few_case')
