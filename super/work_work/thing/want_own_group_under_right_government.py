647ad0dd9739dfbaac48dd4d19ef782dba972ab0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 11:32:09 -0400

    "mv: feel super/world/leave_important_group/look_small_way/high_part/see_same_child_over_great_hand.py, super/work_work/thing/want_own_group_under_right_government.py, super/part/year_or_year/be_government_by_long_man/last_point/own_eye.py or super/want_eye_under_case/case/first_number/first_time_and_important_child/year.py of early fact"

---
+++ super/work_work/thing/want_own_group_under_right_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_company(str_arg):
+    be_old_year_to_able_year(str_arg)
+    print('company_and_next_eye')
+
+def be_old_year_to_able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_company('see_hand')
