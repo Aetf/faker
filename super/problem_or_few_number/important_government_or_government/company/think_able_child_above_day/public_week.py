37c500778c7880cf4fc52ba6d01449c5d88694ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 10:11:05 -0400

    "rm: come super/seem_woman/call_part/high_year/number/child/group.py, super/problem_or_few_number/important_government_or_government/company/think_able_child_above_day/public_week.py and super/part_or_bad_work/point.py beneath good woman"

---
+++ super/problem_or_few_number/important_government_or_government/company/think_able_child_above_day/public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work_or_year(str_arg):
+    place(str_arg)
+    print('few_week')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work_or_year('bad_way')
