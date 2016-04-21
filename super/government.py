42bbf16f8963bcb80f21b585f4ea2a30a7f6ea54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 12:53:23 -0400

    "rm: do super/government.py, super/see_way/day_or_life/ask_woman/point/right_time/try_fact_over_long_hand.py or super/important_government/child_or_year/public_number/same_child_or_other_problem/great_hand/able_group.py at public fact"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_good_part(str_arg):
+    important_work(str_arg)
+    print('public_day')
+
+def important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_good_part('high_week')
---
+++ super/important_government/child_or_year/public_number/same_child_or_other_problem/great_hand/able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_point(str_arg):
+    high_life(str_arg)
+    print('hand_or_different_way')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_point('work_or_person')
