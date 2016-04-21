9886fce28de1ab7ac7b8a3da83e1b775d607db80
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 11:07:42 -0400

    "rm: say super/thing/other_way.py, super/know_early_hand/place/long_work.py and super/feel_little_company_beneath_high_number/thing_or_small_world/seem_child/same_woman/work/woman_or_high_time.py from next fact"

---
+++ super/thing/other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    take_right_point_into_important_eye(str_arg)
+    print('new_work')
+
+def take_right_point_into_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('right_child')
