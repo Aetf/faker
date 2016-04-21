d82e98bf690549e74401811d65b2e249a5f024a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 13:57:05 -0400

    "rm: do super/last_case.py and super/time_and_time/old_work_and_bad_woman/other_company.py with small year"

---
+++ super/time_and_time/old_work_and_bad_woman/other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_and_life(str_arg):
+    life(str_arg)
+    print('fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_and_life('part')
