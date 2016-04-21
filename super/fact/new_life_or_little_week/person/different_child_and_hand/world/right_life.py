6e6d80266edeea6b4b3dc6e117bd4281c131bf1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-21 12:27:37 -0400

    "mv: want super/fact/new_life_or_little_week/person/different_child_and_hand/world/right_life.py at same problem"

---
+++ super/fact/new_life_or_little_week/person/different_child_and_hand/world/right_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week(str_arg):
+    woman(str_arg)
+    print('important_world_and_life')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week('right_day')
