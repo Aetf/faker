70e048f970d5370cd54d9d6f707f41f88c8e6c9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 11:37:40 -0400

    "add: make super/great_group/other_problem/go_last_life_with_company.py, super/see_big_hand/hand/call_fact_under_woman/fact_or_case.py, super/few_man.py and super/long_part/part/important_day/have_different_thing_into_fact.py for high fact"

---
+++ super/few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_able_number_up_bad_day(str_arg):
+    give_person_in_work(str_arg)
+    print('long_way_or_day')
+
+def give_person_in_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_able_number_up_bad_day('feel_government')
---
+++ super/great_group/other_problem/go_last_life_with_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_public_place_into_part(str_arg):
+    think_fact_after_big_man(str_arg)
+    print('make_life_after_man')
+
+def think_fact_after_big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_public_place_into_part('have_problem_after_big_eye')
