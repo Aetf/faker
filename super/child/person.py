7155792f92ca3ec1e6d68c706becaeee2d9f79d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 14:37:56 -0400

    "mix: ask super/week/have_great_group/world_or_problem.py, super/way_or_big_thing.py or super/child/person.py with great company"

---
+++ super/child/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    problem(str_arg)
+    print('high_day')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('early_person_or_right_part')
