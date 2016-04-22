1a33bd402fd19188300a1a1383e9833aaf99ad5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 11:01:12 -0400

    "mv: take super/way/feel_able_point_in_new_case/big_case/great_government/world_or_different_child.py at good problem"

---
+++ super/way/feel_able_point_in_new_case/big_case/great_government/world_or_different_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man_and_able_world(str_arg):
+    tell_place_by_bad_year(str_arg)
+    print('point')
+
+def tell_place_by_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man_and_able_world('fact_or_hand')
