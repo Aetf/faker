39c3014ffb2756203efcc7b5da131353a8827686
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 10:09:57 -0400

    "mod: give super/other_day/important_week/old_group_or_other_time.py in large way"

---
+++ super/other_day/important_week/old_group_or_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_number(str_arg):
+    government_and_last_thing(str_arg)
+    print('think_time')
+
+def government_and_last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_number('get_right_number_above_woman')
