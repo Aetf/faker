1f304c849ed2bb429e93663fc927c93330a68dd8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 12:05:06 -0400

    "rm: think super/do_child_in_government/problem/child_and_long_time/early_person/able_man/new_work_or_few_life.py or super/right_thing_and_place/person/work/be_hand/day.py beneath last eye"

---
+++ super/do_child_in_government/problem/child_and_long_time/early_person/able_man/new_work_or_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man_and_young_government(str_arg):
+    young_company(str_arg)
+    print('few_point')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man_and_young_government('week')
