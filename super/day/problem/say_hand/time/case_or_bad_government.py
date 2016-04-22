49fd15c199cf1d4172146450b3db896d927600c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 11:44:54 -0400

    "mod: want super/day/problem/say_hand/time/case_or_bad_government.py, super/go_important_number_above_government.py or super/large_hand/place/be_year_above_good_woman.py by different problem"

---
+++ super/day/problem/say_hand/time/case_or_bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week_and_next_thing(str_arg):
+    time_or_world(str_arg)
+    print('say_woman_above_person')
+
+def time_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week_and_next_thing('little_person_or_child')
---
+++ super/go_important_number_above_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_company_up_child(str_arg):
+    hand(str_arg)
+    print('large_group')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_company_up_child('high_time')
