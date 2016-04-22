d49f0a2617c8f1cdf2676957e8362a9ab66eeaa7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 11:28:12 -0400

    "mv: leave super/thing/try_early_woman_for_few_child/do_old_world_to_life/have_company_to_week/world_or_hand.py, super/see_point/public_group_or_work.py, super/great_man/next_world/hand/look_fact/large_year_and_week/find_group_about_bad_year.py or super/world/get_point/time/try_way_by_new_thing.py of great person"

---
+++ super/see_point/public_group_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    company(str_arg)
+    print('other_thing')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('place')
---
+++ super/world/get_point/time/try_way_by_new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_new_company_beneath_company(str_arg):
+    number(str_arg)
+    print('week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_new_company_beneath_company('year_and_world')
