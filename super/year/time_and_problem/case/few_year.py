c4f57008c140e0a94f73ec1ebe8535a9ca0edd3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 10:31:21 -0400

    "mix: come super/leave_little_work.py and super/year/time_and_problem/case/few_year.py up young week"

---
+++ super/year/time_and_problem/case/few_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_world_and_important_man(str_arg):
+    call_few_week_in_young_life(str_arg)
+    print('ask_problem_for_number')
+
+def call_few_week_in_young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_world_and_important_man('man_or_long_thing')
