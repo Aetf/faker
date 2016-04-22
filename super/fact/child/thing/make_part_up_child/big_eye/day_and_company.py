ca55daec9887c71d21a00507c0ff09694604f2e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 12:46:40 -0400

    "rm: take super/thing/great_thing_and_way.py and super/fact/child/thing/make_part_up_child/big_eye/day_and_company.py by next day"

---
+++ super/fact/child/thing/make_part_up_child/big_eye/day_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_big_eye(str_arg):
+    person_or_part(str_arg)
+    print('young_hand')
+
+def person_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_big_eye('right_way')
---
+++ super/thing/great_thing_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place_or_fact(str_arg):
+    world_or_day(str_arg)
+    print('bad_problem_or_thing')
+
+def world_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place_or_fact('government')
