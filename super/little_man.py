e2ff0dc8aefc9ac278f73b7851b40b47a409172e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 13:54:42 -0400

    "add: tell super/point.py, super/point.py and super/little_man.py from young time"

---
+++ super/little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_part(str_arg):
+    big_fact(str_arg)
+    print('young_case')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_part('find_own_problem')
