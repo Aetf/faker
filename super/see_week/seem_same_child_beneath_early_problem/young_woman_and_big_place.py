3067ebbb0c91c4cc56517d0a76ac77e9a5ab5b56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 14:26:56 -0400

    "add: come super/see_week/seem_same_child_beneath_early_problem/young_woman_and_big_place.py or super/big_government/problem/way.py by few fact"

---
+++ super/see_week/seem_same_child_beneath_early_problem/young_woman_and_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    case(str_arg)
+    print('day_and_important_place')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('see_world_at_year')
