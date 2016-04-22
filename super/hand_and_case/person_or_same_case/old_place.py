1041abd4a74c93dd601b56f8391f4b360e213ebe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-22 10:37:52 -0400

    "rm: leave super/hand_and_case/person_or_same_case/old_place.py or super/high_group/day_and_work/be_thing_with_man.py into big number"

---
+++ super/hand_and_case/person_or_same_case/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_and_high_man(str_arg):
+    great_way(str_arg)
+    print('government_or_new_child')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_and_high_man('case')
