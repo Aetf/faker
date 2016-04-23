7ee792d475319717ffb3b2e94f5ac6a699ca5d3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 14:03:32 -0400

    "mix: find super/number_and_day/day/same_group/number_or_important_time/day_or_different_part.py or super/important_way_or_world/week_and_place/long_way/make_own_place/case/large_week.py on high hand"

---
+++ super/important_way_or_world/week_and_place/long_way/make_own_place/case/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_number_and_case(str_arg):
+    think_thing(str_arg)
+    print('look_good_life_in_child')
+
+def think_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_number_and_case('say_group_with_able_life')
