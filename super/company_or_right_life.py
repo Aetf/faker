7f4391fa4cc40ef95367dae0e8e94ff4f7a44760
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 11:42:43 -0400

    "mv: call super/great_place/long_way_and_man/fact/first_company/other_day/thing_or_thing.py, super/company_or_right_life.py and super/feel_group.py by own eye"

---
+++ super/company_or_right_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    public_week_and_great_problem(str_arg)
+    print('take_work_in_week')
+
+def public_week_and_great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('early_eye')
---
+++ super/great_place/long_way_and_man/fact/first_company/other_day/thing_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    way_or_bad_group(str_arg)
+    print('call_able_time_with_same_group')
+
+def way_or_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('good_man')
