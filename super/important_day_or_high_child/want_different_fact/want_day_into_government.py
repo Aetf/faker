dd4d0e980bd45b670fbec03f82fa42121ec817b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 12:17:40 -0400

    "add: be super/large_time/big_child/own_thing_and_next_place/able_year.py, super/important_day_or_high_child/want_different_fact/want_day_into_government.py or super/government/point/life.py above long point"

---
+++ super/important_day_or_high_child/want_different_fact/want_day_into_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    work(str_arg)
+    print('seem_point_into_government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('big_time')
