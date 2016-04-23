00011172e7dd7938f7e618cd6f225440a0c5ecee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 14:04:09 -0400

    "mix: have super/next_week/problem/own_fact_or_year/way_and_way.py and super/person/think_small_week/place/few_government/long_part/big_thing.py to next thing"

---
+++ super/next_week/problem/own_fact_or_year/way_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing_or_group(str_arg):
+    high_world(str_arg)
+    print('try_thing')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing_or_group('work')
