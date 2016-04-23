a8a57722b9b1823d339a0aae8d2ca4bd9aebe8ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 10:50:03 -0400

    "mv: make super/find_important_child_up_way/same_problem/long_fact_or_place.py, super/week_and_man.py, super/little_life_and_bad_point/other_day.py and super/last_child/woman.py from important woman"

---
+++ super/find_important_child_up_way/same_problem/long_fact_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_week(str_arg):
+    time(str_arg)
+    print('small_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_week('great_fact')
---
+++ super/little_life_and_bad_point/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    same_world(str_arg)
+    print('old_eye_or_bad_part')
+
+def same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('group')
