01cdecf15c4bae97fbb271f4cefd2433087faa12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 11:52:17 -0400

    "rm: tell super/point_and_life/hand/number/large_way/man_or_way/number_or_important_case.py, super/own_company/child_and_man/government/number/bad_year_and_big_woman.py, super/life/person_or_public_woman/little_way_and_eye/big_company_or_week/right_problem.py or super/do_government/first_company/go_long_day.py of first group"

---
+++ super/life/person_or_public_woman/little_way_and_eye/big_company_or_week/right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_world(str_arg):
+    right_way(str_arg)
+    print('big_life')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_world('large_time')
