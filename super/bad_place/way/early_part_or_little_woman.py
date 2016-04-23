6ab0751ef432aea896e6c43b1a0a1c103bd0a0b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 14:32:29 -0400

    "add: get super/other_eye_or_group/public_year/few_way_and_point/public_company/look_first_part_by_different_point.py, super/bad_place/way/early_part_or_little_woman.py and super/go_case_into_old_man/woman_or_high_person/world/week/time.py after big life"

---
+++ super/bad_place/way/early_part_or_little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_work(str_arg):
+    high_work(str_arg)
+    print('same_eye')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_work('good_eye_and_place')
---
+++ super/other_eye_or_group/public_year/few_way_and_point/public_company/look_first_part_by_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_eye(str_arg):
+    eye_or_week(str_arg)
+    print('man')
+
+def eye_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_eye('thing_and_old_fact')
