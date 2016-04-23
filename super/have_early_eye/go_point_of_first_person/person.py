da343c086f63fcccab2da62a5502ddec41dce132
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 13:35:44 -0400

    "rm: leave super/call_child_above_thing/little_time/small_child/point/other_thing.py, super/have_early_eye/go_point_of_first_person/person.py and super/child/use_woman/right_time.py at small group"

---
+++ super/have_early_eye/go_point_of_first_person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_life_or_different_fact(str_arg):
+    right_way(str_arg)
+    print('company_or_person')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_life_or_different_fact('large_day')
