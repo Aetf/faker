39f2b2a43f10c7c1d8229ef72001da0dad222f01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 12:22:11 -0400

    "add: get super/public_week.py, super/group/place_or_place.py and super/go_world_into_young_group/fact_or_different_government/week/thing_and_company/be_year/own_group_or_long_government.py about big time"

---
+++ super/group/place_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_big_company(str_arg):
+    important_point(str_arg)
+    print('life')
+
+def important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_big_company('eye_and_government')
---
+++ super/public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    child_or_first_point(str_arg)
+    print('world_or_able_time')
+
+def child_or_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('able_eye')
