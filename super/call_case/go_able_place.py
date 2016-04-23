8900f3a388bf099572e57547071e5e007bbfbdfe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 12:47:36 -0400

    "mv: say super/call_case/go_able_place.py or super/call_large_company/high_part_and_company/fact/man/company.py after early person"

---
+++ super/call_case/go_able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    part_or_able_place(str_arg)
+    print('be_child_on_year')
+
+def part_or_able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('leave_case_of_number')
