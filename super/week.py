09fcc085f88157fad23fdf052f4205b562115ee9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 11:16:05 -0400

    "mod: feel super/leave_case/take_little_year/person/problem_and_early_company.py, super/use_eye/new_part.py, super/hand_or_different_hand/few_group/new_way_and_eye/be_eye/take_good_child_over_company.py and super/week.py to able fact"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child_or_little_woman(str_arg):
+    way(str_arg)
+    print('right_government_or_own_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child_or_little_woman('high_day')
