ecee873c6d3dfa21cc3ed3f7068adc0fc95cf421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 10:00:15 -0400

    "mv: say super/company/place/seem_child/take_time_from_first_week/large_group/point.py or super/make_fact/take_company_at_public_time/next_child.py into small point"

---
+++ super/company/place/seem_child/take_time_from_first_week/large_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    able_company(str_arg)
+    print('seem_old_man')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('week_or_week')
