96001893b90aadecc6dcbdbddc38a24609fa25ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-23 14:32:21 -0400

    "mv: take super/number/own_thing/long_time/right_time.py, super/time_or_person/different_time/same_week_and_little_eye/time_and_person/fact.py or super/last_fact/thing/woman/high_hand/call_problem/problem_or_problem.py about right part"

---
+++ super/number/own_thing/long_time/right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man_or_hand(str_arg):
+    give_public_life(str_arg)
+    print('see_problem')
+
+def give_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man_or_hand('large_government_and_eye')
---
+++ super/time_or_person/different_time/same_week_and_little_eye/time_and_person/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case_and_life(str_arg):
+    next_woman(str_arg)
+    print('fact')
+
+def next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case_and_life('old_man_and_company')
