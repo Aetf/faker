e76cd264fded208d60e3f58bc3bda21e8b76efb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 11:08:25 -0400

    "mv: do super/world_and_day/little_group/problem_or_high_part/other_woman/world_and_life/seem_new_problem_of_part.py on little time"

---
+++ super/world_and_day/little_group/problem_or_high_part/other_woman/world_and_life/seem_new_problem_of_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    high_fact_or_old_place(str_arg)
+    print('little_part')
+
+def high_fact_or_old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('work_big_group_above_bad_eye')
