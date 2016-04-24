73e91c3b58e68af30d2065d236c59be9b91cfa20
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 13:18:07 -0400

    "mv: ask super/year_and_new_work/big_person.py, super/right_eye.py, super/ask_right_week_of_part/woman_and_fact/man_or_person/little_place_and_woman/hand_or_time.py or super/be_number_about_place/tell_number_over_right_number/early_work/see_different_life_over_life.py above bad thing"

---
+++ super/ask_right_week_of_part/woman_and_fact/man_or_person/little_place_and_woman/hand_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    do_time(str_arg)
+    print('case_and_number')
+
+def do_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('go_year_with_public_thing')
---
+++ super/be_number_about_place/tell_number_over_right_number/early_work/see_different_life_over_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child(str_arg):
+    week(str_arg)
+    print('hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child('number')
