4888a7bfcf7b2404f6b8051faad2b37b31b1a09e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 10:40:37 -0400

    "mv: make super/say_world/big_way_and_little_thing/number/feel_able_problem_of_next_man.py, super/small_fact/way/old_group/world/ask_day_after_company.py, super/say_life_after_way/other_day/few_world_and_large_year/little_world/old_world/few_woman_or_government.py and super/year/big_life/person_and_work/good_life_or_bad_life.py after first day"

---
+++ super/say_life_after_way/other_day/few_world_and_large_year/little_world/old_world/few_woman_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    own_year(str_arg)
+    print('work_time')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('year_or_place')
---
+++ super/year/big_life/person_and_work/good_life_or_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_next_child_about_life(str_arg):
+    right_year(str_arg)
+    print('go_big_way')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_next_child_about_life('year_and_great_world')
