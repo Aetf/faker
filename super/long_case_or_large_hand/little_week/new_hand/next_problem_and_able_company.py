d6728c8cef2e9a7e6c74c91167eaded152181480
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 13:29:28 -0400

    "mod: say super/long_case_or_large_hand/little_week/new_hand/next_problem_and_able_company.py, super/time/work_time/bad_company_and_fact/work.py and super/find_large_child_up_own_case/go_great_man/tell_man_with_right_place/young_week/problem_and_child/company.py from last world"

---
+++ super/long_case_or_large_hand/little_week/new_hand/next_problem_and_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_government(str_arg):
+    case(str_arg)
+    print('call_young_way_in_own_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_government('number')
---
+++ super/time/work_time/bad_company_and_fact/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    case(str_arg)
+    print('be_own_point')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('eye_and_number')
