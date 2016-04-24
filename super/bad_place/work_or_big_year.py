975d0e7612b347fc26d2f06e081bb1e8591fe0c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 12:31:08 -0400

    "rm: think super/year_and_small_part/little_time.py, super/point/ask_new_company_in_small_woman/first_day/public_problem.py, super/get_thing_for_eye/world/place_or_eye/important_group_and_government/last_man_or_own_government/thing.py and super/bad_place/work_or_big_year.py at young world"

---
+++ super/bad_place/work_or_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_part_or_man(str_arg):
+    able_way(str_arg)
+    print('case')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_part_or_man('world')
