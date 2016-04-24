a8ff2885161a2b8dbc01f06ba23ced518caae6d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 14:12:03 -0400

    "mod: give super/know_eye_to_early_woman/small_week/get_week/point/give_public_world_with_year/work_or_problem.py of other government"

---
+++ super/know_eye_to_early_woman/small_week/get_week/point/give_public_world_with_year/work_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_old_life(str_arg):
+    person(str_arg)
+    print('bad_person')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_old_life('use_small_thing')
