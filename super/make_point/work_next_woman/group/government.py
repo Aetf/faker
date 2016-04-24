b815b845c496431dacb46a2d32b7f169d379e333
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 10:47:25 -0400

    "mix: tell super/last_case/early_fact_or_new_hand/number_and_long_time/place.py or super/make_point/work_next_woman/group/government.py by long eye"

---
+++ super/make_point/work_next_woman/group/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_point(str_arg):
+    young_child(str_arg)
+    print('fact')
+
+def young_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_point('use_person')
