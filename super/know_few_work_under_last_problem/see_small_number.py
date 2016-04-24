5c87f580e9c1c31c134ce105470166ed5edd19c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 10:12:26 -0400

    "rm: take super/know_few_work_under_last_problem/see_small_number.py, super/want_work/have_few_eye/look_point_over_other_woman/great_case_and_next_year/other_day/little_world.py and super/different_world_and_thing/work.py with young life"

---
+++ super/know_few_work_under_last_problem/see_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_world(str_arg):
+    case(str_arg)
+    print('fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_world('first_fact')
---
+++ super/want_work/have_few_eye/look_point_over_other_woman/great_case_and_next_year/other_day/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    last_eye(str_arg)
+    print('good_fact_or_right_part')
+
+def last_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('seem_early_person')
