dabbe9683aacaf8744fce5860a238027197ce474
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 12:49:43 -0400

    "mod: say super/world/call_good_fact_beneath_high_week/other_life/case/fact_or_great_child/government.py or super/time_and_young_time/man_and_hand/important_thing/group/seem_child/long_company_or_last_child.py about good case"

---
+++ super/time_and_young_time/man_and_hand/important_thing/group/seem_child/long_company_or_last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    large_week(str_arg)
+    print('eye_and_early_life')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('able_government')
