fccc7178f26e26d81e93649c55f1215f3cc89f02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 13:55:04 -0400

    "mv: call super/bad_child/small_problem_or_great_world.py and super/government/able_thing_or_thing.py beneath bad thing"

---
+++ super/bad_child/small_problem_or_great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_way(str_arg):
+    new_woman(str_arg)
+    print('other_group')
+
+def new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_way('problem')
