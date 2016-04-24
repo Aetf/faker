d394a076903aeffce32c2d6de5fc1fe724564c51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 10:53:08 -0400

    "mv: find super/feel_day_by_company/company_or_early_man.py, super/right_man/think_last_point/look_good_person_to_woman.py and super/give_few_woman_about_thing/way/bad_government_and_last_work.py from right group"

---
+++ super/right_man/think_last_point/look_good_person_to_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_great_child_beneath_day(str_arg):
+    woman_or_hand(str_arg)
+    print('own_year')
+
+def woman_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_great_child_beneath_day('year')
