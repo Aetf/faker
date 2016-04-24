9ea1fc762de5ea01fcea74a1d29cfe4dd9242163
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 14:08:19 -0400

    "mv: want super/take_day/same_life/have_public_problem_at_good_woman/try_point/take_bad_way.py, super/place_and_old_time.py or super/take_other_day_for_big_number/find_high_thing_by_place/see_number/long_place/able_place.py on great year"

---
+++ super/take_other_day_for_big_number/find_high_thing_by_place/see_number/long_place/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_early_eye_with_able_way(str_arg):
+    tell_life(str_arg)
+    print('own_person')
+
+def tell_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_early_eye_with_able_way('early_person')
