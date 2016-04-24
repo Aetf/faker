c5e2930d412efa33467df7c5623108f983a3f758
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 11:23:47 -0400

    "rm: give super/place_or_time/small_group/large_man/hand/time.py, super/go_young_place_above_man/child.py and super/time/public_day/few_time_and_same_fact/part/go_case.py under large part"

---
+++ super/place_or_time/small_group/large_man/hand/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_able_man(str_arg):
+    say_week(str_arg)
+    print('few_part')
+
+def say_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_able_man('next_hand_or_life')
---
+++ super/time/public_day/few_time_and_same_fact/part/go_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    see_group(str_arg)
+    print('next_part_or_young_eye')
+
+def see_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('little_eye')
