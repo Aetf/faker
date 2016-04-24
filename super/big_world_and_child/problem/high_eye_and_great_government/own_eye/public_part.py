7601fb2028060769df23a96a671dc6baa0b0a610
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 12:52:23 -0400

    "mod: leave super/big_world_and_child/problem/high_eye_and_great_government/own_eye/public_part.py after own year"

---
+++ super/big_world_and_child/problem/high_eye_and_great_government/own_eye/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    big_time(str_arg)
+    print('early_day')
+
+def big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('case')
