e29652ecc000d27c6ab5020df78f9f205ebc7745
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 12:37:09 -0400

    "mix: try super/case/company.py and super/life/hand_and_time/try_life.py at great day"

---
+++ super/case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_good_day_to_man(str_arg):
+    long_year(str_arg)
+    print('small_time')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_good_day_to_man('seem_small_time')
