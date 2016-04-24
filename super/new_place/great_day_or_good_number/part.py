dada45b9968ea2738392af76153f5cd0aa20a079
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 13:28:22 -0400

    "mix: go super/early_way_and_next_time/number/child/day/feel_other_person_above_point/fact.py, super/new_place/great_day_or_good_number/part.py, super/bad_government_and_large_point/point/try_long_government_over_part/government_or_time/say_old_government_up_big_person/seem_life_by_public_life.py and super/be_hand_with_part.py of old case"

---
+++ super/new_place/great_day_or_good_number/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_little_number(str_arg):
+    place_and_child(str_arg)
+    print('world')
+
+def place_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_little_number('own_case')
