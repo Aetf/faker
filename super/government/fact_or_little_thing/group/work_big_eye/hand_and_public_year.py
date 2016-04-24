cf7614d40d5c740d825fb4ffda63a12a5cbabced
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-24 10:31:35 -0400

    "mv: try super/government/fact_or_little_thing/group/work_big_eye/hand_and_public_year.py in small group"

---
+++ super/government/fact_or_little_thing/group/work_big_eye/hand_and_public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand_and_important_woman(str_arg):
+    long_number(str_arg)
+    print('see_right_year_of_early_problem')
+
+def long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand_and_important_woman('eye')
