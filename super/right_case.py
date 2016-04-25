ce963475eb67c17e8c5d1445ef6f4fd5b736b749
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 12:05:40 -0400

    "add: do super/right_case.py of young day"

---
+++ super/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_number(str_arg):
+    man(str_arg)
+    print('important_case_or_different_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_number('company')
