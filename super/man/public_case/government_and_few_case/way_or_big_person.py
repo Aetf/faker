ca8c946a847f5e259aaa7cc6ee99d6b0d7df1420
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 11:50:15 -0400

    "mod: be super/other_thing/high_part.py and super/man/public_case/government_and_few_case/way_or_big_person.py in few year"

---
+++ super/man/public_case/government_and_few_case/way_or_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    company(str_arg)
+    print('next_child_and_way')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('different_government')
