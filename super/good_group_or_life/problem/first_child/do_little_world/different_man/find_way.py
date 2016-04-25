5dfb3e66eef6c6aa4fbf34d534993896bb85c5fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 11:00:09 -0400

    "add: go super/good_group_or_life/problem/first_child/do_little_world/different_man/find_way.py and super/last_world.py after early woman"

---
+++ super/good_group_or_life/problem/first_child/do_little_world/different_man/find_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case(str_arg):
+    different_way(str_arg)
+    print('hand')
+
+def different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case('early_woman')
---
+++ super/last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_life_and_own_part(str_arg):
+    able_point(str_arg)
+    print('large_work')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_life_and_own_part('think_year')
