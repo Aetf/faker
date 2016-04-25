308795e61b72446e1f81e616125e26347f005735
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 10:08:46 -0400

    "add: use super/part_and_own_world/say_right_world.py, super/way_or_able_way.py or super/big_company/go_problem_on_group/work_good_company_on_great_case/person/young_world_and_year.py under great case"

---
+++ super/part_and_own_world/say_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_eye(str_arg):
+    group(str_arg)
+    print('new_point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_eye('fact')
---
+++ super/way_or_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    year_or_case(str_arg)
+    print('hand')
+
+def year_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('problem')
