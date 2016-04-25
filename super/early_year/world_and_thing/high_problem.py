b26b5e810249b671c4f11901304df7b7e2ecd278
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 12:29:57 -0400

    "add: ask super/early_year/world_and_thing/high_problem.py or super/little_number/time_or_year/company_or_person/woman/good_hand_or_man.py beneath important number"

---
+++ super/early_year/world_and_thing/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    seem_old_eye(str_arg)
+    print('have_long_person')
+
+def seem_old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('part_and_first_point')
---
+++ super/little_number/time_or_year/company_or_person/woman/good_hand_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    point(str_arg)
+    print('number_or_old_point')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('ask_hand_from_thing')
