8d8a9e3c91ad20739bed74c64e2e84f3d32e2989
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 10:05:18 -0400

    "add: be super/great_life/give_problem/able_problem.py or super/give_early_thing_above_able_fact/case/group/part_or_first_part/place_and_problem/case.py under own case"

---
+++ super/give_early_thing_above_able_fact/case/group/part_or_first_part/place_and_problem/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week(str_arg):
+    hand_and_way(str_arg)
+    print('eye_and_young_year')
+
+def hand_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week('big_fact')
---
+++ super/great_life/give_problem/able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    large_day_or_company(str_arg)
+    print('high_number')
+
+def large_day_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('same_time')
