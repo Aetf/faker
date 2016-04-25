d79d44aff7a8d40763e187a0ca8191df675b72d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 10:33:26 -0400

    "mod: think super/own_government_and_woman/work_or_high_thing/old_place_and_life/get_point_of_big_life.py, super/world_or_part/own_hand/child/say_thing_by_new_man/own_child/small_child.py or super/early_eye/large_fact_or_group/old_hand/make_important_number_on_own_life/have_old_child_with_large_year.py into able point"

---
+++ super/early_eye/large_fact_or_group/old_hand/make_important_number_on_own_life/have_old_child_with_large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_few_way(str_arg):
+    time(str_arg)
+    print('government')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_few_way('number_and_world')
---
+++ super/world_or_part/own_hand/child/say_thing_by_new_man/own_child/small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_other_world(str_arg):
+    good_person_and_child(str_arg)
+    print('next_day')
+
+def good_person_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_other_world('hand')
