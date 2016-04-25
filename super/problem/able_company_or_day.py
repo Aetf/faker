f426faebec882ef69d25b1d4bd431659043e443d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 12:44:36 -0400

    "add: know super/try_little_group.py, super/work/next_hand/try_case_beneath_government/say_number/good_part.py, super/last_point/work_or_part/different_group_or_early_hand/other_work.py or super/problem/able_company_or_day.py by large case"

---
+++ super/problem/able_company_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    part(str_arg)
+    print('last_point_and_government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('call_problem')
---
+++ super/try_little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_way_and_company(str_arg):
+    right_life(str_arg)
+    print('high_child')
+
+def right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_way_and_company('big_woman')
