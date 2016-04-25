5990a3a020bd2df6067d2639a860b85932731e32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 13:02:29 -0400

    "add: do super/come_government/high_week_and_own_place/few_number.py, super/public_place/get_own_time_by_work/feel_good_eye/problem.py and super/few_life/leave_time/week/feel_work.py above own case"

---
+++ super/public_place/get_own_time_by_work/feel_good_eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    few_part(str_arg)
+    print('have_man')
+
+def few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('big_world')
