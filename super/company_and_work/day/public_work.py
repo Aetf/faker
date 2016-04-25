1d230802b547a94d6f4275ba3a7a73e5de42a647
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 13:48:03 -0400

    "add: try super/company_and_work/day/public_work.py at large hand"

---
+++ super/company_and_work/day/public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_work(str_arg):
+    hand_or_thing(str_arg)
+    print('different_problem_and_new_man')
+
+def hand_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_work('give_important_time')
