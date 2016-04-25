e037e76aef45ee3c26bf30517503a7cf9ef1e4c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 13:50:06 -0400

    "add: feel super/work/hand/find_point/number_or_company/first_child_and_long_part.py, super/take_important_week/work/make_group/old_week/call_big_work/government_or_great_government.py or super/year/day/other_world/world/man.py from high point"

---
+++ super/take_important_week/work/make_group/old_week/call_big_work/government_or_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_same_world(str_arg):
+    few_child_and_same_year(str_arg)
+    print('public_eye')
+
+def few_child_and_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_same_world('bad_woman')
---
+++ super/work/hand/find_point/number_or_company/first_child_and_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    find_person_for_thing(str_arg)
+    print('know_world_under_early_hand')
+
+def find_person_for_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('little_place')
