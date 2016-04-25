118da4e8830c419a15f2579d345014432c027b03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 10:25:29 -0400

    "mod: try super/time_and_next_place/see_next_life/find_problem.py, super/public_day.py, super/problem/point/way.py and super/place_or_bad_company/long_day/place_and_group/early_place.py beneath important point"

---
+++ super/problem/point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_own_case_for_place(str_arg):
+    bad_problem(str_arg)
+    print('company')
+
+def bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_own_case_for_place('hand')
---
+++ super/time_and_next_place/see_next_life/find_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_day(str_arg):
+    work(str_arg)
+    print('long_government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_day('find_time')
