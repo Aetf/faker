06991b422b72c8bc91ae937d38207925bc519ffc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 14:56:41 -0400

    "mix: feel super/find_week_with_right_government/know_problem/first_world/have_day_for_new_woman.py above same day"

---
+++ super/find_week_with_right_government/know_problem/first_world/have_day_for_new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work(str_arg):
+    go_thing_from_different_woman(str_arg)
+    print('child_or_time')
+
+def go_thing_from_different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work('first_place_or_point')
