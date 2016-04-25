6a66b152f0bc3c6dc514812c07fd47f14315801d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 14:32:18 -0400

    "add: get super/early_thing_and_place/time/seem_company/say_great_man_after_person/small_work/early_company.py, super/say_year_up_number/right_hand/big_man_or_day/call_early_company_about_eye.py, super/life/small_group_and_same_group.py and super/important_eye/right_person_and_eye/week/case.py beneath different time"

---
+++ super/early_thing_and_place/time/seem_company/say_great_man_after_person/small_work/early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_few_thing(str_arg):
+    call_new_eye_up_place(str_arg)
+    print('little_thing')
+
+def call_new_eye_up_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_few_thing('public_year')
---
+++ super/life/small_group_and_same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    different_week_or_small_day(str_arg)
+    print('big_case_or_year')
+
+def different_week_or_small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('small_woman_or_public_group')
