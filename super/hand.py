30c37b52bc09fa2fdf51d165d89d66a2490b0700
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-25 12:04:48 -0400

    "rm: take super/bad_group/see_thing/long_time/problem_and_bad_woman.py, super/old_day.py, super/hand.py or super/last_thing_and_first_woman/high_time.py to other part"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life(str_arg):
+    go_place(str_arg)
+    print('leave_different_woman')
+
+def go_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life('tell_little_life')
---
+++ super/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year_and_number(str_arg):
+    person(str_arg)
+    print('part')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year_and_number('long_point_or_way')
