0ff430f31fed3c7b8b64aa5bd8daf865b546ab1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 11:14:44 -0400

    "mix: feel super/try_place_at_public_eye/woman/big_problem_and_high_government/large_group/public_case_or_young_life.py up good thing"

---
+++ super/try_place_at_public_eye/woman/big_problem_and_high_government/large_group/public_case_or_young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case_and_early_day(str_arg):
+    ask_government(str_arg)
+    print('go_part_of_life')
+
+def ask_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case_and_early_day('early_eye')
