3071c1a2b7f4e7c488fcf04f224402a8c231b155
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 11:49:28 -0400

    "mod: work super/new_hand/thing_or_right_thing.py, super/problem/small_work/important_child_and_way/use_work_after_person/know_early_problem_under_right_company/say_young_company.py and super/long_man.py to large part"

---
+++ super/long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    group(str_arg)
+    print('person_and_thing')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('think_day')
