9f6594e93b334606fb12ebeaa6c3a730b7c2b59e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 14:03:00 -0400

    "mod: know super/week/great_place_or_week/tell_own_week_with_woman/work/point/man.py or super/seem_high_number_up_point.py over right group"

---
+++ super/seem_high_number_up_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    life(str_arg)
+    print('try_time')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('world_and_other_year')
