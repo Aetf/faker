0d954899c97edfa9e9a9ae9bd95bce0a24b9333f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 14:42:36 -0400

    "mv: use super/big_child/eye/leave_government_over_group/do_life_about_government/first_eye/get_day_on_old_day.py, super/year/company_and_own_thing/time_or_part/eye/place.py, super/good_world/work_time_into_part/point_or_good_thing/long_person_or_good_government.py and super/little_day/man/want_work_of_time/leave_few_man_after_place/work/large_eye_and_part.py by little day"

---
+++ super/good_world/work_time_into_part/point_or_good_thing/long_person_or_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    time_or_group(str_arg)
+    print('say_eye_beneath_different_case')
+
+def time_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('old_child_and_public_day')
---
+++ super/little_day/man/want_work_of_time/leave_few_man_after_place/work/large_eye_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government(str_arg):
+    way(str_arg)
+    print('public_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government('company')
