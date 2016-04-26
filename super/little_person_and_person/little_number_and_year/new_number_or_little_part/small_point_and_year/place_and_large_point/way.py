6e43172207c201438927d25b5c8f5d232a2006a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 14:06:45 -0400

    "rm: come super/own_way/important_hand/eye/do_man_after_group.py, super/early_work_and_other_problem/come_group/last_group.py and super/little_person_and_person/little_number_and_year/new_number_or_little_part/small_point_and_year/place_and_large_point/way.py about few world"

---
+++ super/little_person_and_person/little_number_and_year/new_number_or_little_part/small_point_and_year/place_and_large_point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_large_week_by_child(str_arg):
+    government_or_little_problem(str_arg)
+    print('government')
+
+def government_or_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_large_week_by_child('government')
