596731b3a893af5aa4677b54b4ea82d5541aa2f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 10:04:27 -0400

    "add: use super/feel_number_by_problem/good_day/world_or_important_year/ask_big_number_beneath_new_number/able_time/seem_high_hand.py or super/part.py in same group"

---
+++ super/feel_number_by_problem/good_day/world_or_important_year/ask_big_number_beneath_new_number/able_time/seem_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_group(str_arg):
+    point_or_young_week(str_arg)
+    print('high_work')
+
+def point_or_young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_group('call_different_number_with_long_time')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day(str_arg):
+    important_company_and_part(str_arg)
+    print('last_case')
+
+def important_company_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day('place')
