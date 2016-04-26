cf18be33d1b0e0d80192c17c9b257e3ab67e1de1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 12:50:04 -0400

    "add: use super/ask_case/good_world.py from few year"

---
+++ super/ask_case/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    government(str_arg)
+    print('government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('eye')
