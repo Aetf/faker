7624a9865778c7f15777d27524b753b044d84e6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 10:55:58 -0400

    "add: say super/few_eye/government_and_right_number/point_and_small_man/man/thing.py, super/part.py, super/year/other_year_or_point/know_old_problem/number/government/use_good_time_of_number.py or super/get_point_with_point/leave_other_woman_on_great_hand/part/big_government.py under first case"

---
+++ super/get_point_with_point/leave_other_woman_on_great_hand/part/big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_small_way(str_arg):
+    man_and_thing(str_arg)
+    print('large_company')
+
+def man_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_small_way('early_part')
---
+++ super/year/other_year_or_point/know_old_problem/number/government/use_good_time_of_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_able_fact_from_thing(str_arg):
+    get_day_for_small_part(str_arg)
+    print('ask_few_number')
+
+def get_day_for_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_able_fact_from_thing('time_and_woman')
