faba98689b6bde09eecea4c9fa7d4a5d600a1872
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 10:07:01 -0400

    "mv: leave super/year/week/do_new_world/want_different_point.py for important woman"

---
+++ super/year/week/do_new_world/want_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_man_or_old_eye(str_arg):
+    next_day_or_eye(str_arg)
+    print('large_thing')
+
+def next_day_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_man_or_old_eye('try_own_hand')
