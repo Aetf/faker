5f2a0aabd823ea4a0bca6f684886649d21d816c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 13:15:02 -0400

    "rm: look super/good_man_or_new_day.py beneath high eye"

---
+++ super/good_man_or_new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_child(str_arg):
+    world(str_arg)
+    print('same_woman')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_child('place_and_early_fact')
