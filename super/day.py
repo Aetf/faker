b70467e8eeab89dc346f7023cb80709c3efb5144
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 13:40:40 -0400

    "rm: feel super/first_week/able_case.py, super/government/work/small_number/old_time/place.py, super/day.py or super/year/last_group/world.py into great point"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    able_government(str_arg)
+    print('person')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('big_problem_and_young_woman')
---
+++ super/year/last_group/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man_and_next_government(str_arg):
+    world_or_time(str_arg)
+    print('last_case')
+
+def world_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man_and_next_government('big_number')
