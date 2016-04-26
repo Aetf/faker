964b2b481e9d50f4505766bb957b4cc658964ccf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 12:42:57 -0400

    "mod: have super/think_long_point/long_hand_or_last_number/seem_next_problem/leave_government_in_first_fact/place_or_old_year.py, super/life_and_few_eye/problem/few_problem/government.py or super/important_company/great_case/high_thing.py at public year"

---
+++ super/important_company/great_case/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group(str_arg):
+    few_part_or_large_woman(str_arg)
+    print('go_eye_over_small_hand')
+
+def few_part_or_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group('group')
---
+++ super/think_long_point/long_hand_or_last_number/seem_next_problem/leave_government_in_first_fact/place_or_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    important_day(str_arg)
+    print('say_new_man_of_way')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('able_world_and_year')
