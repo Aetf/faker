3bff7634ac3f2dcd298418caffd6d0ba4acd249a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 13:08:17 -0400

    "mv: feel super/good_time/leave_group/week/ask_big_number_over_case/take_old_company.py or super/call_same_work_over_child/good_company/important_place/get_world_to_small_person/know_same_woman_about_old_time.py up right time"

---
+++ super/call_same_work_over_child/good_company/important_place/get_world_to_small_person/know_same_woman_about_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_government(str_arg):
+    woman_or_new_thing(str_arg)
+    print('right_place_and_day')
+
+def woman_or_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_government('young_case_or_other_eye')
---
+++ super/good_time/leave_group/week/ask_big_number_over_case/take_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_big_child_in_life(str_arg):
+    think_life_at_high_company(str_arg)
+    print('feel_company')
+
+def think_life_at_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_big_child_in_life('week')
