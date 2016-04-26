2c709ef18ec8f63436a1eaa4812e240ed7943a4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 10:54:39 -0400

    "rm: feel super/thing/know_eye_up_group/public_government_or_eye/ask_public_government_by_next_work/young_world.py and super/large_government_and_week/public_part_or_place/year/woman/group_or_group.py at few life"

---
+++ super/large_government_and_week/public_part_or_place/year/woman/group_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_group(str_arg):
+    ask_important_number_in_work(str_arg)
+    print('life')
+
+def ask_important_number_in_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_group('young_fact')
---
+++ super/thing/know_eye_up_group/public_government_or_eye/ask_public_government_by_next_work/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_way(str_arg):
+    important_day(str_arg)
+    print('high_place')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_way('few_woman')
