bf476d4e27f1f85e773ccb64540ff8004e901255
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 11:36:57 -0400

    "mod: work super/see_year_to_child/time/long_year_and_same_person/right_year_and_child/week/important_year.py, super/government_and_person/bad_eye/seem_other_hand_into_new_man/big_case.py, super/little_place/large_life/next_hand_and_man/good_thing_or_early_person/way/get_new_time.py or super/go_public_work/look_group/child.py in new problem"

---
+++ super/little_place/large_life/next_hand_and_man/good_thing_or_early_person/way/get_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_thing(str_arg):
+    great_thing(str_arg)
+    print('important_woman')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_thing('same_world_and_big_week')
