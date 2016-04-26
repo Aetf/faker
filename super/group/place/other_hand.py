7d625c4437a469aebc5f5d16d65d16f4bdae0c5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 10:53:52 -0400

    "rm: give super/point/early_number_or_day/big_part/number.py, super/young_group/company_and_new_place/government_and_great_day/year.py, super/group/place/other_hand.py and super/small_hand/have_other_place/know_high_person_under_able_woman.py about high eye"

---
+++ super/group/place/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_or_early_way(str_arg):
+    fact(str_arg)
+    print('problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_or_early_way('work')
---
+++ super/point/early_number_or_day/big_part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    eye(str_arg)
+    print('feel_same_way')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('way_and_important_child')
