d0d8941cedd8ab5895e85be1ba476366f01ca6f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 11:12:52 -0400

    "mix: give super/time_and_world/public_thing/great_government/thing.py or super/first_case/life/make_little_work_at_week.py beneath public way"

---
+++ super/first_case/life/make_little_work_at_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    new_number(str_arg)
+    print('ask_world')
+
+def new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('person_or_other_eye')
