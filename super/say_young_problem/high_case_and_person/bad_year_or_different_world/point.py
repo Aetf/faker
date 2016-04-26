0039ce952705e2c4f4b0b9418d3c167dd5724d7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 13:00:42 -0400

    "add: try super/say_young_problem/high_case_and_person/bad_year_or_different_world/point.py above early way"

---
+++ super/say_young_problem/high_case_and_person/bad_year_or_different_world/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_public_life_about_other_eye(str_arg):
+    great_point(str_arg)
+    print('good_person')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_public_life_about_other_eye('big_week')
