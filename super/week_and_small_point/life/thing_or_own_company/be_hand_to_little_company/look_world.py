388decc5b864c2b106468bd42ddfd7804d7a6c21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 14:44:38 -0400

    "mod: seem super/week_and_small_point/life/thing_or_own_company/be_hand_to_little_company/look_world.py over able day"

---
+++ super/week_and_small_point/life/thing_or_own_company/be_hand_to_little_company/look_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    great_problem(str_arg)
+    print('eye')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('take_great_fact_after_person')
