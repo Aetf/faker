9416f1783d09afad5b73c0b6e1ab315cdaa424aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-26 11:50:45 -0400

    "rm: call super/right_woman.py, super/part/other_company/hand/way/great_work.py and super/way/have_group/seem_big_fact/eye_or_week.py into young part"

---
+++ super/part/other_company/hand/way/great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    small_thing_or_new_woman(str_arg)
+    print('woman_or_other_world')
+
+def small_thing_or_new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('fact_and_work')
---
+++ super/way/have_group/seem_big_fact/eye_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man(str_arg)
+    print('case_and_great_child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('able_year')
