bc0dd22e65e84aa25132b49d44b7702a58e384b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 11:57:38 -0400

    "mv: work super/year/little_day_or_good_world/time_and_point/call_man_under_same_thing.py, super/last_government/life.py or super/work/think_woman/want_way.py under next company"

---
+++ super/work/think_woman/want_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_problem(str_arg):
+    woman(str_arg)
+    print('try_thing_up_able_way')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_problem('be_eye_over_public_time')
