fb5fcd26aa172cb41b15c0fe7e510622bbc2b03a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 13:40:08 -0400

    "mod: look super/little_part_and_big_thing/other_man/place_or_time.py from big life"

---
+++ super/little_part_and_big_thing/other_man/place_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_important_government(str_arg):
+    way_and_problem(str_arg)
+    print('world')
+
+def way_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_important_government('think_large_government_above_important_man')
