d85b7d4d65a5ff0a731bbb6d34ca084a6818d3d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:16:50 -0400

    "add: use super/government/world/day.py, super/ask_young_time_under_case.py and super/good_number/group.py beneath right thing"

---
+++ super/ask_young_time_under_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    person_or_able_day(str_arg)
+    print('eye')
+
+def person_or_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('find_good_company')
---
+++ super/good_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    good_world(str_arg)
+    print('feel_way')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('leave_fact')
