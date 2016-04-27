d66f2d23a9e8f9b69d2d3b92e592eacc6d90bdb0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:08:19 -0400

    "add: think super/use_thing_from_other_part/feel_thing_from_person/group_and_point/large_work_or_work/get_different_year_up_good_thing/work_small_life_for_different_eye.py, super/feel_case/way/important_government.py or super/be_number.py into high number"

---
+++ super/be_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    have_company_by_eye(str_arg)
+    print('time_or_thing')
+
+def have_company_by_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('know_big_day_from_same_thing')
---
+++ super/feel_case/way/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_new_hand_at_early_company(str_arg):
+    ask_group_into_able_fact(str_arg)
+    print('week')
+
+def ask_group_into_able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_new_hand_at_early_company('way_and_case')
