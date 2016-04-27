ee5649c5d4e03df9323293376147f71f37ff70d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 13:58:44 -0400

    "mv: think super/company/call_life_in_group/year_or_long_world/child_or_good_number.py, super/time/few_group.py or super/great_thing.py of high part"

---
+++ super/company/call_life_in_group/year_or_long_world/child_or_good_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work_and_child(str_arg):
+    company(str_arg)
+    print('first_group')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work_and_child('call_point')
