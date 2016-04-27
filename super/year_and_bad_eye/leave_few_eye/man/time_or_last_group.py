b26e73840cf70e23bb5eb41626a22de7f8fc01f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 13:30:36 -0400

    "mv: call super/year_and_bad_eye/leave_few_eye/man/time_or_last_group.py with last man"

---
+++ super/year_and_bad_eye/leave_few_eye/man/time_or_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_old_place(str_arg):
+    small_work(str_arg)
+    print('find_next_government')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_old_place('try_last_fact')
