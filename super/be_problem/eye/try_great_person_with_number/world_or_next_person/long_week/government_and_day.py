697e1c6467a0d489a75adfbbcd607374cfb4ac01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 12:36:54 -0400

    "rm: use super/be_problem/eye/try_great_person_with_number/world_or_next_person/long_week/government_and_day.py about important child"

---
+++ super/be_problem/eye/try_great_person_with_number/world_or_next_person/long_week/government_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_bad_problem(str_arg):
+    see_last_child_over_old_thing(str_arg)
+    print('go_fact_at_life')
+
+def see_last_child_over_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_bad_problem('think_last_time_with_big_work')
