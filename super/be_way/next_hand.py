73d92ba176ed10d8634b8d93f7c4932b64faf47f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:25:29 -0400

    "mv: know super/fact/problem.py, super/be_way/next_hand.py, super/time_and_little_company/place_or_government.py or super/small_life_or_woman/part/say_problem/person/part.py into early work"

---
+++ super/be_way/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number(str_arg):
+    look_time(str_arg)
+    print('next_man_or_company')
+
+def look_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number('hand')
---
+++ super/time_and_little_company/place_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    company(str_arg)
+    print('come_old_case_with_problem')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('own_week')
