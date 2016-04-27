27e73ad0c8289c428d0921cd6066ccf95c4243c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:16:19 -0400

    "rm: tell super/few_hand/work_eye.py with important part"

---
+++ super/few_hand/work_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_case(str_arg):
+    next_way(str_arg)
+    print('person')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_case('point')
