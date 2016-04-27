2153780a0dd2047c91b259cda6e5e5007e32ff88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 11:33:35 -0400

    "mv: work super/life.py, super/take_same_world_on_week.py, super/have_thing_up_last_day.py and super/long_number/fact_and_work/early_thing/small_company/high_hand.py for same person"

---
+++ super/have_thing_up_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_week(str_arg):
+    think_time(str_arg)
+    print('next_year')
+
+def think_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_week('number')
---
+++ super/take_same_world_on_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    same_man(str_arg)
+    print('early_eye')
+
+def same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('high_child_and_next_number')
