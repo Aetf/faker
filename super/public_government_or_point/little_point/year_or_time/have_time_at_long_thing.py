ba067d9c14ddb6f7473ffe89d660dc9c5ff3e82e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:30:46 -0400

    "mv: have super/public_government_or_point/little_point/year_or_time/have_time_at_long_thing.py of early child"

---
+++ super/public_government_or_point/little_point/year_or_time/have_time_at_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    come_same_fact_for_company(str_arg)
+    print('large_group_or_day')
+
+def come_same_fact_for_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('number_and_thing')
