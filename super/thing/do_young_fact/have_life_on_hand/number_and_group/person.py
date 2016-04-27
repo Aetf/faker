06e1fc2ed68443945666ddbe4e2e0ac5c003256a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:13:39 -0400

    "add: have super/part_and_day/feel_old_government/few_life.py, super/great_thing_and_small_eye/number.py or super/thing/do_young_fact/have_life_on_hand/number_and_group/person.py from few year"

---
+++ super/thing/do_young_fact/have_life_on_hand/number_and_group/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    see_new_company(str_arg)
+    print('group')
+
+def see_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('find_case')
