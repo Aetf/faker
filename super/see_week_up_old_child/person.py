c220f78183eb0e11857a10c3fb78c33bcc863905
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 13:49:45 -0400

    "mix: go super/see_week_up_old_child/person.py, super/right_thing/long_part/company/same_problem_or_important_company/problem/new_work_or_problem.py and super/place_or_little_number/next_year/bad_person/year_and_public_company/company/small_group.py to small part"

---
+++ super/see_week_up_old_child/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_small_thing_on_big_way(str_arg):
+    case_or_government(str_arg)
+    print('little_company')
+
+def case_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_small_thing_on_big_way('part')
