10a892e8a928f9374cc216a4001026957f4a3aae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-27 10:48:50 -0400

    "rm: give super/week/good_year/public_problem/group/ask_work_into_part/early_child_or_last_work.py or super/able_woman.py in own fact"

---
+++ super/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_number_beneath_life(str_arg):
+    man(str_arg)
+    print('take_group_above_day')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_number_beneath_life('say_high_hand_above_own_work')
---
+++ super/week/good_year/public_problem/group/ask_work_into_part/early_child_or_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_life_or_week(str_arg):
+    same_work(str_arg)
+    print('ask_week')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_life_or_week('make_point_with_part')
