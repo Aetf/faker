9cef16fafb9ddcb27a11a64ecb8d82f2b12f00de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 11:52:02 -0400

    "mod: think super/right_thing_or_thing/same_thing.py, super/time_and_first_life.py or super/bad_man_and_last_case/early_time_or_man/tell_part_from_large_day/thing/work_last_year/first_way.py with different case"

---
+++ super/right_thing_or_thing/same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_part(str_arg):
+    man(str_arg)
+    print('year_and_eye')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_part('person')
