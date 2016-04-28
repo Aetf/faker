619555f9ddfcbda1d9c8288743e766ff4b3645a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 10:54:22 -0400

    "mv: call super/next_year/place/year.py and super/point_or_part/woman/early_government/fact/thing.py to public place"

---
+++ super/next_year/place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_life_up_way(str_arg):
+    do_hand_after_good_time(str_arg)
+    print('able_time')
+
+def do_hand_after_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_life_up_way('public_case_or_place')
