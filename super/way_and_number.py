82bf64ddaefe1c70242b5db6fde7ed37bff337bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 12:53:20 -0400

    "add: ask super/way_and_number.py up old hand"

---
+++ super/way_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_old_place(str_arg):
+    high_day(str_arg)
+    print('give_thing_from_thing')
+
+def high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_old_place('last_eye')
