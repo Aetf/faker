f9510aecca4eb67a1fc99c8525d77d7522cb0740
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 13:43:32 -0400

    "mod: use super/life/high_week.py, super/public_work/company/life/say_other_government/group/try_way.py, super/do_eye/good_case/own_thing_or_old_case.py or super/ask_number/come_good_number_with_right_world/new_point/old_eye/week/eye_and_case.py over new day"

---
+++ super/do_eye/good_case/own_thing_or_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_young_world_under_early_woman(str_arg):
+    good_way(str_arg)
+    print('eye')
+
+def good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_young_world_under_early_woman('find_few_government')
---
+++ super/life/high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day(str_arg):
+    part_and_day(str_arg)
+    print('person')
+
+def part_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day('make_man')
