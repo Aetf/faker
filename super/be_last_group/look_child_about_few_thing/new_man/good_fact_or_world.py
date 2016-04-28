75750dc76b8408a56ab9687bfb0ca14169a12041
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 11:35:48 -0400

    "mix: seem super/be_last_group/look_child_about_few_thing/new_man/good_fact_or_world.py, super/way_or_place.py and super/public_fact_or_small_work/do_early_way/young_day/want_thing/life_or_old_time/tell_company.py for different government"

---
+++ super/be_last_group/look_child_about_few_thing/new_man/good_fact_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_company(str_arg):
+    leave_life(str_arg)
+    print('public_problem')
+
+def leave_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_company('life_and_next_way')
