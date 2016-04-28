361c6f59cfd0b9b6a7444b8ca1d3876b0e8c457d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 12:10:09 -0400

    "mv: want super/seem_able_company/last_person_or_person/bad_group_or_work/feel_world_into_person.py with own problem"

---
+++ super/seem_able_company/last_person_or_person/bad_group_or_work/feel_world_into_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    use_public_woman_from_week(str_arg)
+    print('eye')
+
+def use_public_woman_from_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world_or_large_number')
