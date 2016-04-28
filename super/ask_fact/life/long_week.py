10281e72ea4c6858730b5d0295562cdcceab718b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 10:58:22 -0400

    "mv: feel super/ask_fact/life/long_week.py or super/big_fact_or_small_year/large_year/time/come_woman_to_high_company/be_eye_beneath_hand/man.py in early child"

---
+++ super/ask_fact/life/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_life(str_arg):
+    public_group_or_young_day(str_arg)
+    print('work')
+
+def public_group_or_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_life('seem_time')
