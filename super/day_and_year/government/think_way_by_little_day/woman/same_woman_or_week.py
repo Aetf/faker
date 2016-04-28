b6bf92235ebcbe9cb72556c4a3f7a2e48646f12e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 11:49:32 -0400

    "rm: use super/day_and_year/government/think_way_by_little_day/woman/same_woman_or_week.py under great case"

---
+++ super/day_and_year/government/think_way_by_little_day/woman/same_woman_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_or_own_hand(str_arg):
+    long_number_and_able_part(str_arg)
+    print('use_man_about_number')
+
+def long_number_and_able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_or_own_hand('last_woman')
