62bb522c5903d81156ff11f66ce2761168cb159b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 10:08:18 -0400

    "mix: think super/case/leave_problem.py or super/thing/feel_public_week/group/company_or_other_point/able_way.py to small woman"

---
+++ super/case/leave_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    time_and_able_group(str_arg)
+    print('public_fact')
+
+def time_and_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('day')
---
+++ super/thing/feel_public_week/group/company_or_other_point/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_big_case_up_great_day(str_arg):
+    woman(str_arg)
+    print('do_last_hand_from_few_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_big_case_up_great_day('day')
