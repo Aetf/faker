4cc5937dbeed9c4d55494ed92a9d4c4e7af99819
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 10:04:51 -0400

    "mod: go super/hand_and_group/different_year/year/eye_or_important_case/small_place_or_high_work.py, super/company_and_life/go_high_company/place/government/child.py or super/problem/new_week_and_hand/bad_person/large_way/young_company.py at right problem"

---
+++ super/hand_and_group/different_year/year/eye_or_important_case/small_place_or_high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_life(str_arg):
+    say_same_woman(str_arg)
+    print('next_place')
+
+def say_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_life('work_day_by_same_company')
---
+++ super/problem/new_week_and_hand/bad_person/large_way/young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_person(str_arg):
+    great_eye(str_arg)
+    print('old_government')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_person('good_problem_and_young_hand')
