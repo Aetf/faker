adae0d91c0cc613e43fe7c2876f199ef70985366
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 14:34:47 -0400

    "mod: do super/company_or_fact/way_or_old_time/big_problem_and_hand/government/think_world_for_great_government.py about little hand"

---
+++ super/company_or_fact/way_or_old_time/big_problem_and_hand/government/think_world_for_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work(str_arg):
+    different_point(str_arg)
+    print('able_case')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work('small_government')
