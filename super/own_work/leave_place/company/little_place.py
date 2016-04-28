ad9d28ddd3f724f5335b62400bd1e7839adad023
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 14:27:12 -0400

    "mix: ask super/own_work/leave_place/company/little_place.py or super/big_work_or_early_life/know_eye/public_work.py up long fact"

---
+++ super/own_work/leave_place/company/little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_same_life_into_eye(str_arg):
+    year_and_time(str_arg)
+    print('own_man')
+
+def year_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_same_life_into_eye('public_fact_and_company')
