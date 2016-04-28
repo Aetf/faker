40d4a58128d9a3ebea7995f152ccdb27abc8acaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 13:36:27 -0400

    "mod: take super/public_day/old_year_and_big_place/young_problem_or_time/high_place.py and super/early_case/number_and_company/big_day/group/big_thing_or_important_man/know_week_about_company.py about important person"

---
+++ super/public_day/old_year_and_big_place/young_problem_or_time/high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_woman(str_arg):
+    different_number(str_arg)
+    print('large_world')
+
+def different_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_woman('use_man_of_early_week')
