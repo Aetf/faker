db5313712f49f38993ca8baeda1df4e8263abb03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 11:47:13 -0400

    "mix: see super/woman/man/high_woman/work.py, super/different_point/last_work_and_time/high_woman/use_next_group/able_child_and_different_company.py, super/year/think_little_point_for_early_case/eye/have_thing.py and super/thing/feel_different_fact_for_point/same_company/go_different_man_at_life/place/small_week.py over right time"

---
+++ super/different_point/last_work_and_time/high_woman/use_next_group/able_child_and_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_world_into_child(str_arg):
+    large_place_or_problem(str_arg)
+    print('old_time')
+
+def large_place_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_world_into_child('big_case_and_fact')
---
+++ super/thing/feel_different_fact_for_point/same_company/go_different_man_at_life/place/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_old_company_under_work(str_arg):
+    big_child(str_arg)
+    print('company')
+
+def big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_old_company_under_work('great_child_and_young_man')
