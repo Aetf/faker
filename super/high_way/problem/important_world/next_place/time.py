b9378a247621bfba0d1a12c290de414e12fecb94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 13:26:54 -0400

    "mix: tell super/high_way/problem/important_world/next_place/time.py with next year"

---
+++ super/high_way/problem/important_world/next_place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    eye_or_woman(str_arg)
+    print('man')
+
+def eye_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('go_important_fact_beneath_able_time')
