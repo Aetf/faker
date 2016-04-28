a23d2e5e5ebb15aced747d1c5c588185299edc8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-28 12:18:57 -0400

    "add: take super/company_and_part/group/fact.py, super/thing_and_large_person/number/few_thing_or_man/tell_child/way/high_world.py or super/other_way/way/next_week_and_government/make_own_number.py from great group"

---
+++ super/company_and_part/group/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day(str_arg):
+    few_year_and_week(str_arg)
+    print('want_problem_into_big_world')
+
+def few_year_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day('seem_great_person')
---
+++ super/thing_and_large_person/number/few_thing_or_man/tell_child/way/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    person(str_arg)
+    print('tell_point_in_man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('eye')
