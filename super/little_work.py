13ddd25ba86c2a9e9e84a0d5055e0d388ebf8a5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 12:08:30 -0400

    "mix: call super/work/first_world_or_eye/man/different_case.py, super/little_work.py and super/tell_way/able_person_or_great_person/next_week_and_able_day.py after other place"

---
+++ super/little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_right_world_by_bad_person(str_arg):
+    world_or_case(str_arg)
+    print('hand_and_company')
+
+def world_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_right_world_by_bad_person('new_person_or_life')
---
+++ super/work/first_world_or_eye/man/different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_hand(str_arg):
+    thing_and_man(str_arg)
+    print('have_problem')
+
+def thing_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_hand('be_bad_government_after_great_week')
