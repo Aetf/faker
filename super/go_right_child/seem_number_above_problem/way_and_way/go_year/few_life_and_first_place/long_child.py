c4a53e92cd3043ab36e0b1ee16972c05cab38d50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 11:02:49 -0400

    "rm: find super/go_right_child/seem_number_above_problem/way_and_way/go_year/few_life_and_first_place/long_child.py for old world"

---
+++ super/go_right_child/seem_number_above_problem/way_and_way/go_year/few_life_and_first_place/long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand_and_person(str_arg):
+    year(str_arg)
+    print('tell_work_over_young_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand_and_person('world')
