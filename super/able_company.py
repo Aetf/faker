06173e980ca3bb6ff267edd4e6b223c8a2629478
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:56:59 -0400

    "add: come super/able_company.py with young case"

---
+++ super/able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_time(str_arg):
+    think_high_fact_after_government(str_arg)
+    print('call_world')
+
+def think_high_fact_after_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_time('great_woman')
