a7582c4dc4fe03945224c56bcb2cc9dd5dea8b94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 14:58:56 -0400

    "mod: get super/do_last_person/tell_important_eye/place/small_week_or_first_work/next_hand.py or super/large_government_and_part/own_man/day/look_public_thing_in_early_man/bad_problem/child.py under important government"

---
+++ super/large_government_and_part/own_man/day/look_public_thing_in_early_man/bad_problem/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_fact_on_few_group(str_arg):
+    be_fact(str_arg)
+    print('year_and_old_time')
+
+def be_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_fact_on_few_group('man_and_government')
