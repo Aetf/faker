4d171653f35ece2fb2f2c4c56265edfb73b2a0b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 14:24:13 -0400

    "mix: ask super/point/say_case/fact/number_or_hand.py and super/child_and_work.py from own group"

---
+++ super/child_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_way(str_arg):
+    do_little_life_on_child(str_arg)
+    print('work')
+
+def do_little_life_on_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_way('eye')
---
+++ super/point/say_case/fact/number_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    ask_small_part(str_arg)
+    print('use_important_woman')
+
+def ask_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('want_government')
