62491a86a27c9a9a2ecee3c2b8dba7dc1e06c656
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:31:15 -0400

    "mod: ask super/woman_or_life/company_and_first_case.py, super/large_thing/able_fact/try_hand/week/first_day/public_woman.py or super/good_number_or_bad_group/give_way/seem_day_from_case.py in same life"

---
+++ super/large_thing/able_fact/try_hand/week/first_day/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    own_part_or_woman(str_arg)
+    print('number')
+
+def own_part_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('high_number')
