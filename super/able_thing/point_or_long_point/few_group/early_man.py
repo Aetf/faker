1997823d90bb5b22c80ec0eafe633f1cbec22d56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 13:22:58 -0400

    "rm: give super/able_thing/point_or_long_point/few_group/early_man.py over other day"

---
+++ super/able_thing/point_or_long_point/few_group/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_person_over_high_work(str_arg):
+    next_eye_or_case(str_arg)
+    print('old_thing')
+
+def next_eye_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_person_over_high_work('important_place')
