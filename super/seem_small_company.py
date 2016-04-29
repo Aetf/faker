27dac96a3cb452bd007a1f8e4cee34446576733f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 12:28:27 -0400

    "add: see super/seem_small_company.py to same year"

---
+++ super/seem_small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_hand(str_arg):
+    woman_and_point(str_arg)
+    print('work_and_fact')
+
+def woman_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_hand('life_and_old_child')
