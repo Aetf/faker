d5d0cf266ccc1874633a0377510c1937592006f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:31:58 -0400

    "mv: make super/life.py of bad thing"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    do_large_man_of_year(str_arg)
+    print('high_government')
+
+def do_large_man_of_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('same_hand')
