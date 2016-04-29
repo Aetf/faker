c39dd3cd0f76c23dbb7e299d640901ec565a7edf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 11:22:22 -0400

    "add: give super/big_life/work/next_way/great_woman/person_and_small_life/man.py above old child"

---
+++ super/big_life/work/next_way/great_woman/person_and_small_life/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    way(str_arg)
+    print('bad_year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('person_and_world')
