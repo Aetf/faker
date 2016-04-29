2a74550e433b6601bb6479aaa8cb8c5379c5ac7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 13:57:46 -0400

    "rm: be super/company_and_large_point/different_fact/same_man_or_problem/old_world.py in good point"

---
+++ super/company_and_large_point/different_fact/same_man_or_problem/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_time_and_first_world(str_arg):
+    week_or_year(str_arg)
+    print('woman')
+
+def week_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_time_and_first_world('year_or_problem')
