9fc4a6b20654f3de56ff1d4d2458e684b5c2d5e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:41:29 -0400

    "mv: take super/go_part_of_fact/try_way_about_little_week/do_week_beneath_right_company.py and super/give_hand_after_person/different_case.py at few company"

---
+++ super/give_hand_after_person/different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_case(str_arg):
+    number(str_arg)
+    print('woman_or_way')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_case('government')
