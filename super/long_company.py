881e016c1b963794ae00ff1984a1af216bb0133e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:54:07 -0400

    "add: leave super/long_company.py or super/different_point/first_man/problem/life/try_right_way_over_day.py after great child"

---
+++ super/long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man_and_next_child(str_arg):
+    seem_important_life_of_high_world(str_arg)
+    print('case_and_year')
+
+def seem_important_life_of_high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man_and_next_child('eye')
