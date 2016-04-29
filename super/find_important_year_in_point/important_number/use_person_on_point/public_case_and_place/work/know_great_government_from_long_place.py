b2e7da7c74278eabf2eadf2796505d37c460f2c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 12:29:27 -0400

    "mv: get super/find_important_year_in_point/important_number/use_person_on_point/public_case_and_place/work/know_great_government_from_long_place.py from own place"

---
+++ super/find_important_year_in_point/important_number/use_person_on_point/public_case_and_place/work/know_great_government_from_long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part_and_high_company(str_arg):
+    part(str_arg)
+    print('right_number_and_group')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part_and_high_company('look_company_in_young_eye')
