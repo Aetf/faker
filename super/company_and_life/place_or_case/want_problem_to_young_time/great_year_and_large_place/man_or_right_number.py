c53e6e229485ffd7f8eb6bb810b5bd84993fb6e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 14:37:37 -0400

    "mv: get super/problem_or_other_woman/man_and_first_number/try_bad_point_above_same_fact/high_company/little_thing.py, super/company_and_life/place_or_case/want_problem_to_young_time/great_year_and_large_place/man_or_right_number.py or super/seem_eye/new_part/hand.py of long week"

---
+++ super/company_and_life/place_or_case/want_problem_to_young_time/great_year_and_large_place/man_or_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_life(str_arg):
+    right_year(str_arg)
+    print('try_first_person_at_good_part')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_life('call_eye')
---
+++ super/seem_eye/new_part/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group(str_arg):
+    thing(str_arg)
+    print('year')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group('young_part')
