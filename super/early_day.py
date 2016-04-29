dd1eb01673235f0af7b7dc278553d32c7525cf7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 10:12:27 -0400

    "add: come super/early_day.py after bad world"

---
+++ super/early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_bad_point_above_company(str_arg):
+    little_woman(str_arg)
+    print('life_and_man')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_bad_point_above_company('seem_different_person')
