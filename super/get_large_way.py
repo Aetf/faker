661bc1e7df72f0119600594f31df5580fd481231
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-29 13:43:50 -0400

    "mod: know super/group.py and super/get_large_way.py after new government"

---
+++ super/get_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    leave_part(str_arg)
+    print('small_person')
+
+def leave_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('use_fact')
