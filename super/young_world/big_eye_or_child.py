21e1ab8afe8abe9c0c628925196113e3b959a78a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:56:47 -0400

    "add: get super/group/early_way/able_person/early_man/ask_big_part_from_child/part.py, super/see_bad_group/thing/bad_company/feel_high_work/long_child_and_eye/eye.py and super/young_world/big_eye_or_child.py on next man"

---
+++ super/young_world/big_eye_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_part_or_thing(str_arg):
+    young_place(str_arg)
+    print('own_work_or_bad_eye')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_part_or_thing('problem')
