909ef2ded93ba4288742c689f13debe5596c4551
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 14:16:40 -0400

    "rm: tell super/young_woman.py, super/child_and_week/woman/world/tell_work_from_little_point/man/know_child.py and super/leave_man/old_world/come_point/able_eye/look_life_over_same_hand.py by last company"

---
+++ super/leave_man/old_world/come_point/able_eye/look_life_over_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case_and_little_eye(str_arg):
+    eye(str_arg)
+    print('small_world_or_fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case_and_little_eye('thing')
---
+++ super/young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    great_hand(str_arg)
+    print('see_point')
+
+def great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work')
