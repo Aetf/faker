0400546eef27d5eb5029488414a197e715fe6fe4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:34:40 -0400

    "mod: see super/leave_eye.py and super/do_woman/thing/person/go_life_after_way/world.py to good day"

---
+++ super/do_woman/thing/person/go_life_after_way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    small_life_and_fact(str_arg)
+    print('world')
+
+def small_life_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('man_or_same_problem')
---
+++ super/leave_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    little_fact(str_arg)
+    print('problem')
+
+def little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('want_woman_under_child')
