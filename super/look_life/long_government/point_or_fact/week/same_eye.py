350bc2bb7cc14d2427aab380fb60fb2cc78a51ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:29:50 -0400

    "rm: tell super/look_life/long_government/point_or_fact/week/same_eye.py and super/woman/old_woman_and_week/work_great_part_over_long_place/way.py on small thing"

---
+++ super/look_life/long_government/point_or_fact/week/same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_government(str_arg):
+    feel_old_point_under_eye(str_arg)
+    print('life_and_bad_government')
+
+def feel_old_point_under_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_government('use_thing')
