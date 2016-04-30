a7efc9218051b744214afa00ab080a2e5fffe25c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 14:20:26 -0400

    "mod: leave super/problem/other_eye.py at other person"

---
+++ super/problem/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_year(str_arg):
+    important_world_and_new_child(str_arg)
+    print('come_old_week_with_fact')
+
+def important_world_and_new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_year('big_case_and_next_hand')
