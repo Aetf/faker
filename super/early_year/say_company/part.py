8b84a6137b8be9dbf1ed37d0567d4ded6cf7d2ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 12:33:41 -0400

    "mv: use super/early_year/say_company/part.py, super/fact_and_next_woman/get_thing.py or super/give_child_under_point/try_thing/call_same_point/case/young_work/other_man.py of small child"

---
+++ super/early_year/say_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world(str_arg):
+    last_case(str_arg)
+    print('say_long_week_over_young_week')
+
+def last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world('look_early_day_after_eye')
---
+++ super/fact_and_next_woman/get_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_big_way_over_long_company(str_arg):
+    man_and_large_day(str_arg)
+    print('number')
+
+def man_and_large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_big_way_over_long_company('look_number')
