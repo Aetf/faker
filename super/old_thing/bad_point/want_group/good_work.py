8b8560202d596af0e5f701dd9b9857c1d3b02c5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:34:19 -0400

    "mix: try super/week/week_and_own_eye/important_eye_or_able_child/first_hand.py and super/old_thing/bad_point/want_group/good_work.py at same point"

---
+++ super/old_thing/bad_point/want_group/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point_and_good_problem(str_arg):
+    right_way_and_year(str_arg)
+    print('feel_child_from_hand')
+
+def right_way_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point_and_good_problem('person')
---
+++ super/week/week_and_own_eye/important_eye_or_able_child/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life(str_arg):
+    little_woman(str_arg)
+    print('eye')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life('work')
