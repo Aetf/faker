470cdb2aa127c3653698a553ade159a72d905e33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:06:05 -0400

    "mod: leave super/bad_fact/call_child_beneath_hand.py in old hand"

---
+++ super/bad_fact/call_child_beneath_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_way_or_number(str_arg):
+    hand_and_bad_fact(str_arg)
+    print('different_life_or_work')
+
+def hand_and_bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_way_or_number('make_problem')
