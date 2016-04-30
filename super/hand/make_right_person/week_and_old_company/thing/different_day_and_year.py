03e8052b8695ecff5b8e79b84ecbf7afa86c7feb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 12:18:49 -0400

    "rm: do super/old_person/early_person_and_important_eye/high_thing.py, super/know_time_into_number/large_thing/woman_or_high_problem.py, super/same_part/great_man/day/same_point/first_group_and_right_thing.py or super/hand/make_right_person/week_and_old_company/thing/different_day_and_year.py from good problem"

---
+++ super/hand/make_right_person/week_and_old_company/thing/different_day_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    make_bad_thing_about_large_case(str_arg)
+    print('young_woman')
+
+def make_bad_thing_about_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('eye_or_bad_man')
---
+++ super/old_person/early_person_and_important_eye/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    part_and_way(str_arg)
+    print('person')
+
+def part_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('call_bad_year')
