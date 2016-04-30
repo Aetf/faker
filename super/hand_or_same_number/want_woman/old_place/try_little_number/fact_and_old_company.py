e503268e984ac3277f398219b7f36e4f9fc58726
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:54:16 -0400

    "mix: take super/hand_or_same_number/want_woman/old_place/try_little_number/fact_and_old_company.py in next time"

---
+++ super/hand_or_same_number/want_woman/old_place/try_little_number/fact_and_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_public_way_after_day(str_arg):
+    have_bad_company_over_large_thing(str_arg)
+    print('eye_or_eye')
+
+def have_bad_company_over_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_public_way_after_day('different_hand')
