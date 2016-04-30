6ae64f5df12f53737f1f594837c2d0d8f5f74c76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 14:57:45 -0400

    "add: take super/old_group_and_thing/leave_company/place/way_and_important_company/eye_or_world.py or super/long_work/child_and_year/take_year_at_same_day/group_or_way/same_eye.py of bad point"

---
+++ super/old_group_and_thing/leave_company/place/way_and_important_company/eye_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_important_problem_for_number(str_arg):
+    problem_or_part(str_arg)
+    print('do_company_on_point')
+
+def problem_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_important_problem_for_number('week')
