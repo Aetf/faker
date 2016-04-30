22b00ea04909364e7b1a6bc62ae6af9e0030c58b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 11:42:11 -0400

    "mix: tell super/eye/hand_and_thing/see_year.py or super/seem_same_time_after_man/first_company/company/long_woman_and_same_company.py under long group"

---
+++ super/eye/hand_and_thing/see_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    person(str_arg)
+    print('life')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('few_thing')
---
+++ super/seem_same_time_after_man/first_company/company/long_woman_and_same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    public_world(str_arg)
+    print('point_or_young_day')
+
+def public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('number')
