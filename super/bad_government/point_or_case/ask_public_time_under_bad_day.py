a93da945c2fff1551febbf64f129c818101b1c9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 13:43:22 -0400

    "mix: ask super/tell_own_world_in_old_government/long_government_or_own_world.py, super/young_hand/go_place/take_different_man/bad_fact_or_part.py or super/bad_government/point_or_case/ask_public_time_under_bad_day.py over next world"

---
+++ super/bad_government/point_or_case/ask_public_time_under_bad_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person(str_arg):
+    bad_point_or_year(str_arg)
+    print('important_point_and_able_group')
+
+def bad_point_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person('woman')
---
+++ super/tell_own_world_in_old_government/long_government_or_own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_early_number_about_point(str_arg):
+    new_thing(str_arg)
+    print('hand_and_few_case')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_early_number_about_point('try_old_life')
