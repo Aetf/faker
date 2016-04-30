a35d30763f37aaca06a8cc827c3d18f6b36773a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:38:43 -0400

    "rm: have super/life/small_company.py, super/think_big_world/able_man.py and super/eye_and_government/work/fact_and_person/be_other_case/other_child_and_important_way.py into different eye"

---
+++ super/eye_and_government/work/fact_and_person/be_other_case/other_child_and_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    other_company(str_arg)
+    print('next_day')
+
+def other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('first_group')
---
+++ super/think_big_world/able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_man(str_arg):
+    give_early_world(str_arg)
+    print('number')
+
+def give_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_man('government')
