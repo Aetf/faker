17c75ba3a8a68a42df43778f59aac4f37362f016
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:05:19 -0400

    "add: seem super/large_government/few_hand/want_number/seem_same_number/eye/big_thing.py, super/new_hand/call_big_year.py or super/last_thing/first_problem/problem/fact.py beneath able point"

---
+++ super/last_thing/first_problem/problem/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand_or_high_point(str_arg):
+    good_person(str_arg)
+    print('think_long_woman')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand_or_high_point('hand')
---
+++ super/new_hand/call_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    large_time_or_high_point(str_arg)
+    print('give_problem_after_right_work')
+
+def large_time_or_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('big_government_and_small_place')
