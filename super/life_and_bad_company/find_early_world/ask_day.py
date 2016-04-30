65993507490873769c9867917e95e698943ddb24
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:41:35 -0400

    "rm: have super/life_and_bad_company/find_early_world/ask_day.py, super/see_own_world_in_problem/early_case/company/bad_person_or_year/ask_few_world_to_fact/way.py, super/go_thing_into_important_time/few_problem_or_high_point/able_problem/know_case.py and super/small_group/long_thing/good_case_and_high_fact/bad_part/great_company.py to big part"

---
+++ super/life_and_bad_company/find_early_world/ask_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company(str_arg):
+    man(str_arg)
+    print('young_life_and_early_year')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company('point')
---
+++ super/small_group/long_thing/good_case_and_high_fact/bad_part/great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_year_from_group(str_arg):
+    important_week_or_hand(str_arg)
+    print('feel_case_in_point')
+
+def important_week_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_year_from_group('seem_time')
