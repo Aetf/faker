cba26dee6e4a293cd54a6f189ea041d10d8faa12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 14:29:43 -0400

    "mv: want super/child_and_able_child/last_man/new_place_and_right_life/big_case.py, super/hand_and_number/year/long_place_or_little_fact/problem/next_time_or_part/other_woman.py or super/place_or_same_problem/young_time.py of high time"

---
+++ super/child_and_able_child/last_man/new_place_and_right_life/big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man(str_arg):
+    last_week_or_government(str_arg)
+    print('bad_year')
+
+def last_week_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man('long_company')
