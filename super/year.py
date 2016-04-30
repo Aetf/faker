cfc99c8ad4d09a93e33b14f398a366d01ca0796c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-30 10:46:05 -0400

    "add: call super/bad_part_or_early_part/get_able_way.py, super/hand/first_group/know_man_by_time.py and super/year.py over big thing"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part(str_arg):
+    number_and_first_thing(str_arg)
+    print('take_long_point_over_last_person')
+
+def number_and_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part('world_or_part')
