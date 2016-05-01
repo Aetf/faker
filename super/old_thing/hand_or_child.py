2c70a0addbe90425e918f76d8177b805651a1013
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 11:04:35 -0400

    "mix: seem super/old_thing/hand_or_child.py, super/point_or_early_company/public_government/right_week/work_or_life/great_year.py or super/other_number/small_person/tell_little_year_of_eye/public_company/find_year.py from bad thing"

---
+++ super/old_thing/hand_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    have_work(str_arg)
+    print('place')
+
+def have_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('way')
