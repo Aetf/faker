8b755fdcea8b84e01ec64e7f782dccf21636ae93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 13:52:57 -0400

    "mod: get super/next_point/large_week.py, super/right_time/man_and_problem/work_same_thing/public_man.py and super/long_fact_and_right_woman.py up few part"

---
+++ super/long_fact_and_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_case(str_arg):
+    old_problem(str_arg)
+    print('bad_group_and_man')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_case('great_case_or_long_man')
