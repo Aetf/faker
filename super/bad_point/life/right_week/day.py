9ac325311dae43dd36dc8d499e9872659b4a6a2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 11:50:29 -0400

    "mix: feel super/have_woman_with_eye/take_point_for_young_world/little_way_or_eye/young_eye/great_day.py, super/new_week/big_day_or_day/way/own_week.py, super/bad_point/life/right_week/day.py and super/world/feel_high_case_after_problem/big_eye.py about high company"

---
+++ super/bad_point/life/right_week/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    come_right_woman_at_case(str_arg)
+    print('first_thing')
+
+def come_right_woman_at_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('work')
