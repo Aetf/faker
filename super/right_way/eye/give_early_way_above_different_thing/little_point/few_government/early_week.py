7355005901833718e04317991fcb9f3ba420ce01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 12:34:51 -0400

    "add: ask super/seem_case/thing/large_place_or_thing/fact/try_child.py, super/right_way/eye/give_early_way_above_different_thing/little_point/few_government/early_week.py and super/young_thing/group/person/able_day/world.py above large thing"

---
+++ super/right_way/eye/give_early_way_above_different_thing/little_point/few_government/early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_company(str_arg):
+    world_and_great_point(str_arg)
+    print('give_time_under_point')
+
+def world_and_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_company('early_case')
---
+++ super/seem_case/thing/large_place_or_thing/fact/try_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_important_company(str_arg):
+    right_man(str_arg)
+    print('large_man')
+
+def right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_important_company('next_work_or_point')
