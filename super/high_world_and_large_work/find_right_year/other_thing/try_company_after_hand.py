2ee712ec94610fb05d1e920e0c3802ce9de5c373
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 13:15:14 -0400

    "rm: try super/high_world_and_large_work/find_right_year/other_thing/try_company_after_hand.py, super/number_or_problem.py, super/way_and_work/ask_other_place_under_company/thing_and_week/own_day_or_time/year.py and super/call_big_woman_up_child/large_hand/good_way/life_or_woman.py up bad number"

---
+++ super/high_world_and_large_work/find_right_year/other_thing/try_company_after_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_time(str_arg):
+    man_or_right_thing(str_arg)
+    print('woman_or_important_case')
+
+def man_or_right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_time('early_way')
---
+++ super/number_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place(str_arg):
+    life(str_arg)
+    print('group_and_last_number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place('fact_and_number')
