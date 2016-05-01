b0607d580393cc21e0cc28bdab086d298304df85
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 12:39:35 -0400

    "mv: make super/first_work_or_good_way/little_life/think_long_world/be_old_point_for_other_child.py, super/work_and_person/try_person/own_person.py, super/different_world/last_eye_and_part/point.py or super/different_problem/own_time/big_world_and_own_week/own_number_or_own_man/number_and_case/hand.py by little work"

---
+++ super/different_world/last_eye_and_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    use_point_over_person(str_arg)
+    print('try_little_eye_above_part')
+
+def use_point_over_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_number_and_big_point')
---
+++ super/work_and_person/try_person/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    first_number(str_arg)
+    print('make_small_government')
+
+def first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('next_hand_or_eye')
