2f55ed1b8de92987ae12045b69bc23304fce4941
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 10:45:20 -0400

    "mod: have super/little_child/little_problem.py and super/hand_and_problem/young_place_or_way.py of same world"

---
+++ super/hand_and_problem/young_place_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    long_year_or_little_person(str_arg)
+    print('right_point')
+
+def long_year_or_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('high_eye')
