9c448682a8a763c493d3ebb709653a8332609f0f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 11:13:38 -0400

    "rm: give super/last_place/child_and_early_point/day/long_hand.py and super/problem/part/week/work_and_point/able_life.py with new hand"

---
+++ super/last_place/child_and_early_point/day/long_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_large_way(str_arg):
+    little_day_or_hand(str_arg)
+    print('eye_or_way')
+
+def little_day_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_large_way('great_world')
---
+++ super/problem/part/week/work_and_point/able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye_and_early_company(str_arg):
+    thing(str_arg)
+    print('same_work_or_next_work')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye_and_early_company('bad_day_or_other_man')
