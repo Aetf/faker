e33f164783ba2a73a58ba34dda18d597a994518b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 12:53:34 -0400

    "mv: seem super/government.py above early way"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    case(str_arg)
+    print('young_company_or_fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('work')
