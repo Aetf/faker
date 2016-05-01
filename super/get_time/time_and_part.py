fdca3dd2e9125746f91f2ce91b3c06c9ef2e4b3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 14:58:19 -0400

    "mod: see super/group/other_fact/point/point/world_and_long_life.py, super/place/case.py and super/get_time/time_and_part.py in great thing"

---
+++ super/get_time/time_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    same_company(str_arg)
+    print('last_company')
+
+def same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('day')
---
+++ super/group/other_fact/point/point/world_and_long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_year(str_arg):
+    old_world(str_arg)
+    print('high_group')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_year('day')
