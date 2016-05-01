9625887db9036471465293daada6321e03072d3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 13:44:43 -0400

    "mv: have super/able_problem_and_next_work/first_time_or_hand/leave_good_company/know_first_way/great_person_or_week.py, super/few_week/early_problem_or_last_life/want_person/large_world_and_own_way.py or super/say_hand_at_other_child.py beneath high time"

---
+++ super/able_problem_and_next_work/first_time_or_hand/leave_good_company/know_first_way/great_person_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    high_number_and_hand(str_arg)
+    print('woman_or_place')
+
+def high_number_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('hand')
---
+++ super/say_hand_at_other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_high_week_beneath_big_woman(str_arg):
+    number(str_arg)
+    print('bad_thing_and_day')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_high_week_beneath_big_woman('small_eye')
