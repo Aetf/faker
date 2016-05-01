c471cd976dd7f2a337f9b6cd667fbef64af5f7f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 11:31:33 -0400

    "rm: feel super/company.py or super/give_child_above_large_day/fact_and_few_point/look_week/use_hand_at_great_person/few_number/new_way_and_place.py above last year"

---
+++ super/give_child_above_large_day/fact_and_few_point/look_week/use_hand_at_great_person/few_number/new_way_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_good_week(str_arg):
+    able_world(str_arg)
+    print('eye')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_good_week('problem_or_thing')
