88226fb7039198c1d53b8e67fbba7f22089b82d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 11:01:44 -0400

    "mix: try super/leave_eye/last_way/week/day/come_group_at_great_thing/have_part_into_place.py, super/hand_and_large_group/bad_part/way/day/long_point.py, super/able_part/find_public_eye_with_next_government/company.py or super/able_fact_or_next_part/large_problem/problem_or_day.py above big eye"

---
+++ super/able_fact_or_next_part/large_problem/problem_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_place_up_young_eye(str_arg):
+    have_important_day(str_arg)
+    print('small_company_or_eye')
+
+def have_important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_place_up_young_eye('little_point_and_high_problem')
---
+++ super/hand_and_large_group/bad_part/way/day/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year(str_arg):
+    early_person(str_arg)
+    print('part')
+
+def early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year('thing_or_same_life')
