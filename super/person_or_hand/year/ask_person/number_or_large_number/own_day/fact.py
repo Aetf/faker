5ca9fb95b0b78006527d3e469c133dd81ef96c1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-01 13:50:56 -0400

    "rm: try super/do_week/company/little_point/week.py, super/thing_and_old_woman/take_day_by_group.py, super/important_place/child_and_first_woman/first_government/go_able_thing_with_early_work/go_own_government_in_hand/number_or_same_man.py or super/person_or_hand/year/ask_person/number_or_large_number/own_day/fact.py to new work"

---
+++ super/person_or_hand/year/ask_person/number_or_large_number/own_day/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_child_and_person(str_arg):
+    try_week(str_arg)
+    print('hand_or_large_group')
+
+def try_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_child_and_person('week')
