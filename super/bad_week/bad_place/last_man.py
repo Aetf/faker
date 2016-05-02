11ad74c087d1d8b79ff7dce0f7394c2670d01558
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:49:09 -0400

    "add: know super/other_company/other_week_and_person.py, super/know_person/person/next_number/life/week/fact.py, super/bad_week/bad_place/last_man.py and super/good_part/call_old_woman_after_man/able_woman/group.py to large world"

---
+++ super/bad_week/bad_place/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    think_hand_about_life(str_arg)
+    print('do_long_week_for_able_number')
+
+def think_hand_about_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work_great_child')
---
+++ super/good_part/call_old_woman_after_man/able_woman/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    same_eye(str_arg)
+    print('problem')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('other_child_and_company')
