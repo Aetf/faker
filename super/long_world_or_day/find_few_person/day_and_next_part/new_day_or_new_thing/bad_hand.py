7ba43784a97531e96883a5c57b9e3bba409637b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:22:44 -0400

    "mix: give super/long_world_or_day/find_few_person/day_and_next_part/new_day_or_new_thing/bad_hand.py into good eye"

---
+++ super/long_world_or_day/find_few_person/day_and_next_part/new_day_or_new_thing/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    say_young_year(str_arg)
+    print('way_and_government')
+
+def say_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('new_case_or_last_problem')
