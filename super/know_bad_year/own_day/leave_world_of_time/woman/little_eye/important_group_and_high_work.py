6cf22b60737c084d10f630640887095ab46f8b68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:16:08 -0400

    "mod: give super/year/have_able_work_above_high_part/problem/come_different_woman_on_good_government.py, super/time/early_man.py and super/know_bad_year/own_day/leave_world_of_time/woman/little_eye/important_group_and_high_work.py from small group"

---
+++ super/know_bad_year/own_day/leave_world_of_time/woman/little_eye/important_group_and_high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_few_number(str_arg):
+    little_point(str_arg)
+    print('good_woman')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_few_number('say_child_by_big_year')
