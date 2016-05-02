2e95c49470f2351b9a6e17290acb8c81aa676ecf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 13:49:07 -0400

    "add: be super/come_group/woman.py, super/person/thing_or_small_company/long_man/week/old_way.py or super/eye/different_hand_or_week/company/ask_case/world/fact.py above important world"

---
+++ super/come_group/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    great_eye(str_arg)
+    print('day_and_few_man')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('use_other_child_with_company')
