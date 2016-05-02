b87d751e6a3a744a2d260438dca6fba3f9a1b22d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:48:38 -0400

    "mix: say super/early_time/big_child_or_work/good_group/company.py or super/early_company.py to important man"

---
+++ super/early_time/big_child_or_work/good_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye(str_arg):
+    part(str_arg)
+    print('tell_number_on_right_place')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye('problem')
