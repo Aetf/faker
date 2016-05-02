08eeab829c817a30c28ddedc25985ab49f9baf47
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:18:12 -0400

    "mv: feel super/number_and_child.py, super/have_next_work_of_problem/last_problem/high_person/way.py, super/number/give_great_government.py or super/own_work_or_thing/government_and_week/case_or_few_fact/case_or_day/call_last_person/first_thing_or_hand.py for long hand"

---
+++ super/own_work_or_thing/government_and_week/case_or_few_fact/case_or_day/call_last_person/first_thing_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_time(str_arg):
+    fact(str_arg)
+    print('problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_time('government')
