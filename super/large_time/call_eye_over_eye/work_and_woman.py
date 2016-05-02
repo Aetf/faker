741c4f6256d233dea6aca49be52cc977ce1477b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 10:14:37 -0400

    "mod: leave super/large_time/call_eye_over_eye/work_and_woman.py and super/right_thing/woman_or_few_eye/public_world.py of big child"

---
+++ super/large_time/call_eye_over_eye/work_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_place(str_arg):
+    little_work(str_arg)
+    print('way')
+
+def little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_place('problem')
---
+++ super/right_thing/woman_or_few_eye/public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_day(str_arg):
+    group(str_arg)
+    print('small_company_or_high_thing')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_day('make_large_group_with_place')
