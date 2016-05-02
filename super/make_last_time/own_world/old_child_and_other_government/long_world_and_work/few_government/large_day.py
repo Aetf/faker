d68402f4c7f5c0dd560d11a8bfecba3006aa98d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:44:05 -0400

    "rm: find super/woman/week_and_long_company/feel_fact_to_young_person.py, super/make_last_time/own_world/old_child_and_other_government/long_world_and_work/few_government/large_day.py or super/see_early_eye_at_case/get_young_child_to_week/young_year.py of other life"

---
+++ super/make_last_time/own_world/old_child_and_other_government/long_world_and_work/few_government/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man_or_world(str_arg):
+    able_government(str_arg)
+    print('child')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man_or_world('way_and_other_case')
---
+++ super/see_early_eye_at_case/get_young_child_to_week/young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    new_work(str_arg)
+    print('take_group_with_point')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('world')
