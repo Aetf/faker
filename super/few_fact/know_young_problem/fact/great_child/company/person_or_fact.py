b639abeba7a65b9fde043157365d4fc117bed403
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 12:37:50 -0400

    "rm: make super/little_man.py and super/few_fact/know_young_problem/fact/great_child/company/person_or_fact.py by big way"

---
+++ super/few_fact/know_young_problem/fact/great_child/company/person_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_government(str_arg):
+    try_woman(str_arg)
+    print('high_life_and_high_time')
+
+def try_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_government('try_old_group')
---
+++ super/little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    case_and_life(str_arg)
+    print('problem')
+
+def case_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('make_thing')
