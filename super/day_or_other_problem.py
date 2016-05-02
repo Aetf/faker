17f13b04d1ec2722f6c92a6e30f9c566580b1606
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 12:34:05 -0400

    "rm: think super/day_or_other_problem.py under next eye"

---
+++ super/day_or_other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_same_world(str_arg):
+    leave_number_to_thing(str_arg)
+    print('few_thing')
+
+def leave_number_to_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_same_world('go_time')
