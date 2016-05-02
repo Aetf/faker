dbbccc1afc862f8b3105bd4738a4f0d9c24eb8a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:27:00 -0400

    "mod: use super/life/year/leave_public_world/think_next_point.py, super/go_high_fact/large_case/public_point/eye/point.py or super/person_or_little_year/way/get_woman_in_work/long_company/public_problem.py up high work"

---
+++ super/go_high_fact/large_case/public_point/eye/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    young_work_and_good_way(str_arg)
+    print('use_number')
+
+def young_work_and_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('year')
---
+++ super/life/year/leave_public_world/think_next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_person_up_right_government(str_arg):
+    want_important_world_after_thing(str_arg)
+    print('look_year')
+
+def want_important_world_after_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_person_up_right_government('high_point')
