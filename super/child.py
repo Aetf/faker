fd2f9064cad15e56ffd80f83dd6e63f3ea82d03e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:33:55 -0400

    "rm: feel super/child.py under long man"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    small_time(str_arg)
+    print('last_number')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('big_child')
