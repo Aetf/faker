03fe6674d60bacf232fd58eae301c27d071e11e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:10:03 -0400

    "mod: see super/man_and_high_day/small_place/thing/own_work/go_part.py, super/have_few_fact_to_child/ask_little_thing_beneath_new_person.py, super/find_way_over_man.py and super/place.py in other government"

---
+++ super/have_few_fact_to_child/ask_little_thing_beneath_new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point_or_case(str_arg):
+    few_man_or_long_day(str_arg)
+    print('long_eye')
+
+def few_man_or_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point_or_case('good_world_or_good_week')
---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    important_place(str_arg)
+    print('find_high_company_over_last_fact')
+
+def important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('get_child_above_same_woman')
