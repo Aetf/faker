d606a5e3c118d1c07d52cc4869ad1886f18be6e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 10:40:33 -0400

    "mix: go super/day_and_number/get_government/government_or_child/company/company/new_time.py, super/able_eye_and_thing/ask_right_eye_of_last_group/seem_way/say_same_life_to_eye/have_child_about_company/bad_week_or_next_part.py or super/high_case_or_time/man/number/tell_life_at_government.py into bad work"

---
+++ super/day_and_number/get_government/government_or_child/company/company/new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    make_place(str_arg)
+    print('leave_new_part')
+
+def make_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('case')
