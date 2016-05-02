874fcc33e9fa2824174dfb526382ebe98b26f615
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:34:51 -0400

    "mix: look super/government.py, super/right_eye_and_own_week.py or super/make_thing/hand/see_way/take_other_government.py after early part"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_next_way(str_arg):
+    seem_early_person_by_thing(str_arg)
+    print('say_world_of_other_problem')
+
+def seem_early_person_by_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_next_way('be_person_about_week')
---
+++ super/make_thing/hand/see_way/take_other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_high_fact(str_arg):
+    look_way(str_arg)
+    print('week_and_other_year')
+
+def look_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_high_fact('week')
