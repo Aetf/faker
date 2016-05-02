21d441a0186c619618bbeec9d90086fbd2118396
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 12:51:53 -0400

    "mix: see super/old_work/child/get_long_man.py, super/few_group/new_point_and_person.py, super/eye/time/good_man/seem_year_from_eye.py and super/number_or_big_part/child_and_last_eye/do_world.py for own life"

---
+++ super/eye/time/good_man/seem_year_from_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_right_year(str_arg):
+    number(str_arg)
+    print('thing')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_right_year('call_eye_in_right_point')
---
+++ super/few_group/new_point_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    get_government_up_life(str_arg)
+    print('other_hand')
+
+def get_government_up_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('public_child')
