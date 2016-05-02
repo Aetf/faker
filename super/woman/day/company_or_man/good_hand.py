85adb7cdd3877910613b024f3320f87b9eecdb6b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:19:40 -0400

    "mix: see super/woman/day/company_or_man/good_hand.py at big group"

---
+++ super/woman/day/company_or_man/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    bad_number(str_arg)
+    print('old_problem')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('new_man_and_small_case')
