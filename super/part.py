5797ff608b35197de89863decba3b21b86e1f4de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 12:20:07 -0400

    "mod: call super/part.py of next time"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_child_and_public_company(str_arg):
+    thing(str_arg)
+    print('life_and_last_week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_child_and_public_company('government_and_problem')
