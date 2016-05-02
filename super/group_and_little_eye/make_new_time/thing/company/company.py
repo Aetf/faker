0b5ea50b31d24826c5262c17869234f6a6a6e7c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 10:34:36 -0400

    "add: find super/give_fact_from_eye/own_point/way_and_company/number/public_way_or_big_point/small_place_and_part.py, super/work_and_person/say_government/leave_person/same_problem_or_old_year/say_point_above_number/government.py, super/group_and_little_eye/make_new_time/thing/company/company.py and super/other_point_and_fact.py above different thing"

---
+++ super/group_and_little_eye/make_new_time/thing/company/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_number_into_life(str_arg):
+    same_day(str_arg)
+    print('look_place')
+
+def same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_number_into_life('hand_or_number')
---
+++ super/other_point_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company_and_able_time(str_arg):
+    big_way_and_long_child(str_arg)
+    print('right_fact')
+
+def big_way_and_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company_and_able_time('early_life')
