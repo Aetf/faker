6326d2fbba06b0026fab7f6390e90348be1f4065
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:41:33 -0400

    "add: find super/new_point/few_year/year_or_new_government/come_right_week_of_place/public_woman/life.py, super/different_point/few_time_or_large_man/make_new_thing_beneath_important_eye/old_day_and_part/problem_or_person/call_few_year_after_eye.py or super/world.py for same point"

---
+++ super/different_point/few_time_or_large_man/make_new_thing_beneath_important_eye/old_day_and_part/problem_or_person/call_few_year_after_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    find_early_woman_with_right_time(str_arg)
+    print('know_right_place')
+
+def find_early_woman_with_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('try_big_day')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    next_eye_or_new_company(str_arg)
+    print('few_company_and_important_time')
+
+def next_eye_or_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('number_or_place')
