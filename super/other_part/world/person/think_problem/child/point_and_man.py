2db3e64fde8e01df97fab511de46b91152307ed3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 11:09:02 -0400

    "mv: try super/seem_public_group/high_group/eye_or_same_man/high_day/same_fact/part.py, super/other_part/world/person/think_problem/child/point_and_man.py or super/different_week_or_big_world/new_world/seem_government_for_next_part/problem/number/great_man_or_next_problem.py of right year"

---
+++ super/other_part/world/person/think_problem/child/point_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_man(str_arg):
+    life_and_fact(str_arg)
+    print('little_point_and_world')
+
+def life_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_man('feel_life')
