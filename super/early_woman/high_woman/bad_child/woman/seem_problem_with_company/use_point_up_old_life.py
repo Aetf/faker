4b200abea41f8fe86c3ad11502cb1a1849f8d424
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 12:10:27 -0400

    "add: work super/few_work/use_life/leave_part/woman/child.py, super/early_woman/high_woman/bad_child/woman/seem_problem_with_company/use_point_up_old_life.py or super/first_fact_or_good_group/fact_or_small_person/own_year_and_hand/own_year/thing/different_child.py into large fact"

---
+++ super/early_woman/high_woman/bad_child/woman/seem_problem_with_company/use_point_up_old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    other_part(str_arg)
+    print('point_and_important_man')
+
+def other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('large_point')
