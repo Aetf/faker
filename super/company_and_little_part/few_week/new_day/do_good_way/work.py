67b0abd689fb6197caeb297568f79bb5ea680564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 14:50:31 -0400

    "mod: want super/company_and_little_part/few_week/new_day/do_good_way/work.py and super/week_or_small_person.py on large year"

---
+++ super/company_and_little_part/few_week/new_day/do_good_way/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_new_case_under_man(str_arg):
+    fact(str_arg)
+    print('hand_or_small_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_new_case_under_man('day_and_group')
---
+++ super/week_or_small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work_and_few_time(str_arg):
+    part_and_eye(str_arg)
+    print('new_year')
+
+def part_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work_and_few_time('eye')
