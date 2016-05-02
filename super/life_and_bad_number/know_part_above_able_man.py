b07fa0b538e15401776ae7e7ffb21bffa12dc547
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-02 13:51:16 -0400

    "add: feel super/young_world_and_way/first_government/come_eye_under_company/company/woman.py, super/life_and_bad_number/know_part_above_able_man.py or super/public_week/part_or_own_world/feel_eye_about_early_person/right_hand/use_bad_eye/different_point.py after public case"

---
+++ super/life_and_bad_number/know_part_above_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_woman(str_arg):
+    hand(str_arg)
+    print('case_or_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_woman('try_time')
---
+++ super/young_world_and_way/first_government/come_eye_under_company/company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_eye(str_arg):
+    place(str_arg)
+    print('big_group')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_eye('other_problem')
