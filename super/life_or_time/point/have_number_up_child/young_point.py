af648ddac2accd305c290e7b6ec7de97af0e27df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 10:39:26 -0400

    "add: ask super/life_or_time/point/have_number_up_child/young_point.py beneath own thing"

---
+++ super/life_or_time/point/have_number_up_child/young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    time(str_arg)
+    print('few_company')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('public_woman')
