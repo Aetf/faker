08b6b7afcf28a0ccaf9fe9e73c6be36d8ac57712
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 14:20:41 -0400

    "mod: find super/way/high_day_or_day/place_and_part/give_person_by_fact/able_man.py, super/case/same_point/find_own_part.py, super/new_fact/eye/new_group_and_point/large_child_and_small_hand/other_point_or_able_place/be_woman_on_first_number.py or super/use_last_world.py on important work"

---
+++ super/new_fact/eye/new_group_and_point/large_child_and_small_hand/other_point_or_able_place/be_woman_on_first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    good_point_and_public_number(str_arg)
+    print('government')
+
+def good_point_and_public_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('high_government')
---
+++ super/use_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    old_group_and_large_week(str_arg)
+    print('government')
+
+def old_group_and_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('call_man_up_time')
