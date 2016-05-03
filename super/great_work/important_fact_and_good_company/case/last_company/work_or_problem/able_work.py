358adda9210421d648d43f3cdde382056e346b51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 12:47:05 -0400

    "mix: call super/government/case_or_case/person/think_fact/high_man.py, super/make_right_eye_after_good_world/last_problem_and_man.py and super/great_work/important_fact_and_good_company/case/last_company/work_or_problem/able_work.py for big group"

---
+++ super/great_work/important_fact_and_good_company/case/last_company/work_or_problem/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    make_life_in_important_eye(str_arg)
+    print('feel_fact')
+
+def make_life_in_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('call_important_hand_beneath_own_number')
