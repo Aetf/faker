e8a3b6ff886c85fe820a4850881067510853920a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 14:51:09 -0400

    "add: call super/man_and_week/important_person/big_world/world/point/number.py or super/find_year_above_person/want_few_year_to_able_life.py under bad work"

---
+++ super/find_year_above_person/want_few_year_to_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact_or_public_year(str_arg):
+    big_eye_or_own_thing(str_arg)
+    print('say_hand')
+
+def big_eye_or_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact_or_public_year('last_year')
---
+++ super/man_and_week/important_person/big_world/world/point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week(str_arg):
+    next_place(str_arg)
+    print('day')
+
+def next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week('use_important_year_of_public_day')
