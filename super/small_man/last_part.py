ca14610379856e58ba3e55dc144ca75fa97b3bae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 14:06:38 -0400

    "mix: make super/small_man/last_part.py or super/bad_group/same_world/high_year.py over young person"

---
+++ super/small_man/last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day(str_arg):
+    able_life(str_arg)
+    print('look_case')
+
+def able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day('little_time')
