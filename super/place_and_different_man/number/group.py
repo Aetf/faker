86fa16ebeac0e27d4bc4eda685b0d8d27e86f8bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 13:13:42 -0400

    "mod: seem super/place_and_different_man/number/group.py or super/next_problem_or_public_point/company/person.py at old government"

---
+++ super/place_and_different_man/number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_week(str_arg):
+    life_and_long_woman(str_arg)
+    print('use_work')
+
+def life_and_long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_week('know_problem_with_small_fact')
