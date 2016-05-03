c48a16e26574218bba138351e089103c18f67709
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 12:28:47 -0400

    "mv: want super/week/man_or_day/week.py and super/old_fact/look_man_to_last_part/person/number/get_day/get_own_place.py for early problem"

---
+++ super/old_fact/look_man_to_last_part/person/number/get_day/get_own_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way(str_arg):
+    little_part_or_right_company(str_arg)
+    print('give_early_man')
+
+def little_part_or_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way('same_world')
---
+++ super/week/man_or_day/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    see_life(str_arg)
+    print('get_number_for_work')
+
+def see_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('make_public_hand_by_first_part')
