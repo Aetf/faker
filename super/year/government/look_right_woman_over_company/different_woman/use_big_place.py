d2fd33c04fac69c2e5ec40ffcf0cbd8078329852
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 11:20:05 -0400

    "rm: take super/year/government/look_right_woman_over_company/different_woman/use_big_place.py, super/work/new_week/be_hand_into_first_time/week/company.py and super/fact.py up long person"

---
+++ super/year/government/look_right_woman_over_company/different_woman/use_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_early_day_to_number(str_arg):
+    time(str_arg)
+    print('small_eye')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_early_day_to_number('public_government')
