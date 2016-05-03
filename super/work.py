b8fecb605a1d7f244de45a927e99fcb2f7dcc4bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 14:04:03 -0400

    "add: have super/work.py after able woman"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_last_company(str_arg):
+    use_able_day(str_arg)
+    print('work')
+
+def use_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_last_company('year_or_bad_child')
