1c7a46d795bbcf573ff35ee93f1471e8df4c2f5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 12:25:12 -0400

    "rm: work super/new_work/be_public_case.py, super/early_man/group/eye/long_person_and_government/say_same_number/thing.py, super/point/find_problem_by_own_group/eye_and_company/give_other_point/fact.py and super/case/bad_thing_or_next_case/make_year_above_hand/long_problem/high_group.py in little life"

---
+++ super/case/bad_thing_or_next_case/make_year_above_hand/long_problem/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_person(str_arg):
+    want_good_child(str_arg)
+    print('point')
+
+def want_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_person('public_eye')
---
+++ super/point/find_problem_by_own_group/eye_and_company/give_other_point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_point_above_time(str_arg):
+    work_or_good_work(str_arg)
+    print('own_eye_or_hand')
+
+def work_or_good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_point_above_time('number')
