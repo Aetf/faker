0523e4e5d9b47a40f3351ccb637c6ebcaec43191
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 11:57:14 -0400

    "mix: think super/long_person/leave_next_government_into_year/work/small_company.py and super/work_high_woman/life/case_or_same_fact/new_problem/year.py from own company"

---
+++ super/work_high_woman/life/case_or_same_fact/new_problem/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company(str_arg):
+    person(str_arg)
+    print('fact')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company('big_thing')
