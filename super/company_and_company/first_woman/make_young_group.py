6f5fbb760f7a240430f21020c4b3c03af117fc1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 11:11:54 -0400

    "mix: look super/company_and_company/first_woman/make_young_group.py and super/look_small_time_from_other_child/time/part/way/last_group.py beneath good day"

---
+++ super/company_and_company/first_woman/make_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number(str_arg):
+    high_way_and_high_thing(str_arg)
+    print('day')
+
+def high_way_and_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number('few_person_and_first_fact')
---
+++ super/look_small_time_from_other_child/time/part/way/last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    world(str_arg)
+    print('problem')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('big_government')
