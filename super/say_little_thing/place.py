c117818170b97a3abb530c5f17b556400f154a32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 13:02:58 -0400

    "rm: feel super/say_little_thing/place.py by little group"

---
+++ super/say_little_thing/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    give_life(str_arg)
+    print('early_year')
+
+def give_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('thing')
