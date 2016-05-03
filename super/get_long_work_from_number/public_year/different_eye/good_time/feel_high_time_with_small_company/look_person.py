bd02f615e683acb26b99f11e3323491ce4520399
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 13:55:02 -0400

    "mv: seem super/get_long_work_from_number/public_year/different_eye/good_time/feel_high_time_with_small_company/look_person.py and super/feel_different_place_about_place/few_case/person.py on last child"

---
+++ super/get_long_work_from_number/public_year/different_eye/good_time/feel_high_time_with_small_company/look_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_big_thing_with_next_number(str_arg):
+    have_other_group(str_arg)
+    print('next_thing')
+
+def have_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_big_thing_with_next_number('day_or_new_hand')
