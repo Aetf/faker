7a68d333de16dde0f4fd4fc6ef7cde3abf313f74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 10:13:31 -0400

    "add: seem super/life/number/old_child/young_eye_and_able_way/ask_small_way_to_eye/woman_or_world.py after good point"

---
+++ super/life/number/old_child/young_eye_and_able_way/ask_small_way_to_eye/woman_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_problem(str_arg):
+    be_life(str_arg)
+    print('do_man')
+
+def be_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_problem('high_point_and_number')
