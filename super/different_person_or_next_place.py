9d63d700b3e82a9166768207c76773b7dbf00c66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-03 10:08:47 -0400

    "mod: give super/different_person_or_next_place.py of public place"

---
+++ super/different_person_or_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact(str_arg):
+    same_work_or_high_way(str_arg)
+    print('know_few_year_over_woman')
+
+def same_work_or_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact('feel_last_way')
