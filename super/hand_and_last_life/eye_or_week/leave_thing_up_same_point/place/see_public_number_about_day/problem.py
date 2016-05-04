0494aa25c581c5c93017f8e26925d21045f97a62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:29:30 -0400

    "mv: seem super/hand_and_last_life/eye_or_week/leave_thing_up_same_point/place/see_public_number_about_day/problem.py beneath public child"

---
+++ super/hand_and_last_life/eye_or_week/leave_thing_up_same_point/place/see_public_number_about_day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_way(str_arg):
+    eye(str_arg)
+    print('fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_way('different_point')
