f715b4dd4fefb3afe55b7f14a66e857583dd4ac7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 12:33:06 -0400

    "mv: take super/day/long_group/think_right_number.py, super/new_number_or_number/problem/good_world_and_company.py and super/find_work/tell_woman/thing/do_woman_in_man/know_point_after_public_case.py by other day"

---
+++ super/day/long_group/think_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    part_and_new_thing(str_arg)
+    print('take_big_world_to_small_group')
+
+def part_and_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('come_next_case')
---
+++ super/find_work/tell_woman/thing/do_woman_in_man/know_point_after_public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    high_hand(str_arg)
+    print('leave_big_group_into_part')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('great_company_or_life')
