1f477df5306bcc708f0ad5a0f6107c52d6971e52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 11:56:26 -0400

    "rm: have super/big_way/child/old_day_and_long_world.py and super/right_place/different_case/big_eye_or_important_government.py above able year"

---
+++ super/big_way/child/old_day_and_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_same_hand_under_own_fact(str_arg):
+    work_problem(str_arg)
+    print('want_world')
+
+def work_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_same_hand_under_own_fact('life_and_own_problem')
