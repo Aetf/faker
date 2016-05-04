68e726325435066f724117ff2a268d5105f70068
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 10:46:00 -0400

    "mv: see super/public_person_and_person/high_thing.py about great person"

---
+++ super/public_person_and_person/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_way(str_arg):
+    world(str_arg)
+    print('old_fact_and_long_thing')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_way('make_high_group')
