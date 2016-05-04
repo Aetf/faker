595395bc8060c70668022fae41982ecb270e67c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 10:18:24 -0400

    "mv: have super/world_and_young_part/time/different_number/time/get_good_day/few_year.py or super/think_young_company.py for little government"

---
+++ super/think_young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    work_or_young_woman(str_arg)
+    print('get_old_work')
+
+def work_or_young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('young_hand_and_life')
