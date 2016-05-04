6feaeae2ce49a61e70a72f58b967eb1e8b31b250
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 14:38:47 -0400

    "add: leave super/work_and_great_government/number.py, super/little_woman/old_company_and_same_person/early_man/person_or_work/ask_world.py, super/first_eye_and_case/way.py or super/child_or_right_case/able_point_and_case.py above first world"

---
+++ super/work_and_great_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    see_day_into_next_work(str_arg)
+    print('man')
+
+def see_day_into_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('give_case_by_important_child')
