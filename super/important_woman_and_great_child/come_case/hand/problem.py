82488f738d001be997608a6a78d9785ab49b8df3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:32:08 -0400

    "mv: be super/good_fact/take_different_government_of_next_company/early_week.py, super/feel_old_way/person.py, super/important_woman_and_great_child/come_case/hand/problem.py or super/company.py on few group"

---
+++ super/important_woman_and_great_child/come_case/hand/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact_and_number(str_arg):
+    call_small_case(str_arg)
+    print('eye')
+
+def call_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact_and_number('child')
