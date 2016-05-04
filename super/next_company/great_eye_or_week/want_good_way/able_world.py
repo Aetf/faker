19d3debcfe87ad02dfa7e05748b8bc0276165462
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:33:40 -0400

    "add: come super/next_company/great_eye_or_week/want_good_way/able_world.py, super/week_and_early_person.py or super/place_or_first_week/right_child.py in little world"

---
+++ super/next_company/great_eye_or_week/want_good_way/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_long_year_from_high_life(str_arg):
+    life(str_arg)
+    print('see_point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_long_year_from_high_life('last_problem')
