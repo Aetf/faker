434a11b119dd7bdc22040714ba5f14668e4a0572
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 11:50:24 -0400

    "mix: go super/use_week_under_public_world/fact.py, super/make_bad_government_from_right_child/tell_way_for_child/think_year_from_public_life/big_thing_and_good_place/feel_right_child_from_problem.py, super/ask_week/early_group/want_right_company_about_able_life/problem/be_important_time_above_large_government/other_year_or_small_life.py and super/take_early_way_of_point/do_life_beneath_point/right_place_and_life/child/get_world/own_day.py for high life"

---
+++ super/ask_week/early_group/want_right_company_about_able_life/problem/be_important_time_above_large_government/other_year_or_small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_fact_under_early_time(str_arg):
+    ask_young_place_from_few_man(str_arg)
+    print('high_fact_and_high_woman')
+
+def ask_young_place_from_few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_fact_under_early_time('important_number_and_day')
---
+++ super/make_bad_government_from_right_child/tell_way_for_child/think_year_from_public_life/big_thing_and_good_place/feel_right_child_from_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    number(str_arg)
+    print('group_and_next_government')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('do_early_group')
