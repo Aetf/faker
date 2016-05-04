481fd8f5da7f8566e1cdb7a0aa6682fb833f3dc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 14:22:44 -0400

    "mod: use super/other_case_or_woman/company_or_life/know_young_time/year/next_year/public_case.py up right government"

---
+++ super/other_case_or_woman/company_or_life/know_young_time/year/next_year/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    be_place_from_world(str_arg)
+    print('part')
+
+def be_place_from_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('other_woman_and_year')
