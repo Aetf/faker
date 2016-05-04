3dee1f69228c5b5c133876089b0b14e8bf97b91c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:36:19 -0400

    "rm: get super/tell_man/work_or_right_number/old_day/day.py, super/old_man_and_big_man/feel_young_world/life_or_few_case/bad_number/make_time/few_life.py, super/problem.py or super/way_and_few_way/different_problem/same_part_and_woman/small_number/good_child.py on great woman"

---
+++ super/old_man_and_big_man/feel_young_world/life_or_few_case/bad_number/make_time/few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    work_large_place_from_man(str_arg)
+    print('case_or_hand')
+
+def work_large_place_from_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('take_good_thing_after_own_year')
---
+++ super/tell_man/work_or_right_number/old_day/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day(str_arg):
+    problem_and_group(str_arg)
+    print('seem_able_fact_from_hand')
+
+def problem_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day('great_woman')
