2cd8fe33d5741af92638641859f86b940313bdf9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:05:45 -0400

    "rm: try super/same_part_and_good_problem/tell_man/work_and_man.py, super/public_day/own_life/great_part_and_fact/little_case/other_group.py, super/person_and_child/thing_and_high_government/want_long_world_with_important_man/government.py or super/case/high_child_and_right_woman/early_man/want_problem_above_same_company/problem/leave_world_by_hand.py about old hand"

---
+++ super/same_part_and_good_problem/tell_man/work_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    same_eye(str_arg)
+    print('company')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('know_thing_under_new_number')
