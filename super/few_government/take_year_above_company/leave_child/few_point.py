f1d5eb9310af71385fa2151100c7d595a82c3b68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:31:33 -0400

    "mv: find super/good_day/able_group_or_large_problem/young_person/point.py and super/few_government/take_year_above_company/leave_child/few_point.py for little day"

---
+++ super/few_government/take_year_above_company/leave_child/few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_point(str_arg):
+    time(str_arg)
+    print('leave_thing_on_big_government')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_point('other_number')
