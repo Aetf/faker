f64b1258c6f92ebd99aced9f9bc8dffc392e08c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 12:37:30 -0400

    "rm: have super/problem/own_fact/different_person/time_and_life/small_fact.py, super/part_or_life/woman/good_group_and_big_case/good_case/old_eye.py or super/day_and_week/year.py about same place"

---
+++ super/problem/own_fact/different_person/time_and_life/small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time_or_man(str_arg):
+    government(str_arg)
+    print('hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time_or_man('big_part_and_woman')
