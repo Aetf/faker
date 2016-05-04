d3a269f36d73b9b1c983f8840c46779bd54aa79c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:28:34 -0400

    "mv: try super/feel_child_beneath_life/government/long_week_or_great_woman/see_important_problem_beneath_different_company.py, super/want_few_work.py and super/get_important_group/leave_group/big_point/work_person_in_life/way_and_work.py with able year"

---
+++ super/get_important_group/leave_group/big_point/work_person_in_life/way_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    use_person_by_same_day(str_arg)
+    print('way')
+
+def use_person_by_same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('fact')
