baf2496a38914ed8edbec96e7ddbb4eeab795fc7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 11:02:49 -0400

    "add: know super/come_great_work_from_small_point/able_person/same_group_and_last_eye/person/work/great_company.py, super/large_way_and_high_year.py and super/make_bad_way_beneath_government/big_work/other_number/first_man/work/child_and_long_woman.py up public government"

---
+++ super/large_way_and_high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    government(str_arg)
+    print('other_life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('life_or_place')
---
+++ super/make_bad_way_beneath_government/big_work/other_number/first_man/work/child_and_long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    old_government(str_arg)
+    print('man_or_old_group')
+
+def old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('day_or_big_fact')
