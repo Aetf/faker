5633280ed946a3d440c96495ec3b595311653076
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:28:40 -0400

    "mv: ask super/know_way/government.py and super/few_child_and_fact/number.py in large company"

---
+++ super/know_way/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    bad_fact(str_arg)
+    print('place')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('have_point')
