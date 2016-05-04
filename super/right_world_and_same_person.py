8b9f58c9fd9cb5098120dc72639550b4d43c56bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-04 13:43:54 -0400

    "mix: ask super/right_world_and_same_person.py or super/day_and_hand/come_part_into_place.py up great year"

---
+++ super/right_world_and_same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_early_time(str_arg):
+    year(str_arg)
+    print('man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_early_time('case')
