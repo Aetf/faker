f0ee90ca66d1acfaee6bb82bfe2f06461f51a647
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 10:05:11 -0400

    "mix: make super/young_week_or_young_group/place/point_and_long_world.py by new group"

---
+++ super/young_week_or_young_group/place/point_and_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_hand_and_able_year(str_arg):
+    do_small_point(str_arg)
+    print('use_point_to_great_person')
+
+def do_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_hand_and_able_year('be_work_beneath_government')
