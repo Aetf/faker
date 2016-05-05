35c572898f0eaea7f1216b726a6f3a7da9c872c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 10:44:31 -0400

    "mv: know super/problem/large_case.py, super/able_day.py and super/important_day/week.py under same time"

---
+++ super/able_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_bad_eye(str_arg):
+    leave_other_case_in_group(str_arg)
+    print('different_government')
+
+def leave_other_case_in_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_bad_eye('eye')
---
+++ super/problem/large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye(str_arg):
+    last_year(str_arg)
+    print('part')
+
+def last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye('part_and_thing')
