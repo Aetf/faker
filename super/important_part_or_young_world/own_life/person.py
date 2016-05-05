3a7b7b62a5d0bc0eef67686e4919b7d50a2c8372
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 11:00:27 -0400

    "mod: make super/important_part_or_young_world/own_life/person.py after same fact"

---
+++ super/important_part_or_young_world/own_life/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    fact(str_arg)
+    print('work_or_woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('eye')
