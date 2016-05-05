60459c87b110fef9971813caaf7512c02640c2e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 12:29:20 -0400

    "mix: make super/good_hand/first_number_or_hand.py and super/come_woman.py over able woman"

---
+++ super/come_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    be_next_point_by_able_child(str_arg)
+    print('great_problem')
+
+def be_next_point_by_able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('man')
---
+++ super/good_hand/first_number_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_small_fact_on_number(str_arg):
+    able_eye(str_arg)
+    print('child')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_small_fact_on_number('first_company')
