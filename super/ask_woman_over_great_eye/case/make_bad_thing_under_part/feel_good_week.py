abfd2166f17eb19367f70999cf8f3aefdf0047e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 12:09:02 -0400

    "add: find super/ask_woman_over_great_eye/case/make_bad_thing_under_part/feel_good_week.py beneath last point"

---
+++ super/ask_woman_over_great_eye/case/make_bad_thing_under_part/feel_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    big_eye_and_group(str_arg)
+    print('old_eye')
+
+def big_eye_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('first_world')
