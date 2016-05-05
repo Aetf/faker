2701774df2cf8124cf5a3a105bc82d359b6be283
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 14:26:58 -0400

    "mv: work super/bad_place_or_work/tell_own_company/little_part_or_little_time/woman_or_old_hand.py, super/say_life/thing_and_young_work/come_new_child/come_little_time/other_week/work_fact.py and super/large_number_or_case/work_life/young_case.py of few work"

---
+++ super/large_number_or_case/work_life/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    day_or_small_government(str_arg)
+    print('high_number')
+
+def day_or_small_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('year_and_child')
---
+++ super/say_life/thing_and_young_work/come_new_child/come_little_time/other_week/work_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_year(str_arg):
+    way(str_arg)
+    print('want_child')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_year('next_child')
