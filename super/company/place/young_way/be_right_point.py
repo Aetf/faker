d8f94344254107f9163e0b7860084c8dcc672406
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 11:37:36 -0400

    "mv: leave super/other_woman/company/do_new_case_up_government/new_case/point.py, super/first_part/company/high_work_or_child/high_woman/same_case.py, super/company/place/young_way/be_right_point.py and super/few_fact.py on last hand"

---
+++ super/company/place/young_way/be_right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    fact(str_arg)
+    print('company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('fact')
