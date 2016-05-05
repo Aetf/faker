a8815515804c27fb7193df5d6aa400fa07e499fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 12:03:20 -0400

    "mv: use super/feel_young_hand_above_bad_number/case/want_person_on_able_fact/important_company.py, super/man/small_time.py or super/last_day_or_great_fact/feel_new_work_beneath_next_fact/man.py by big point"

---
+++ super/feel_young_hand_above_bad_number/case/want_person_on_able_fact/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company_or_case(str_arg):
+    great_world_and_government(str_arg)
+    print('thing')
+
+def great_world_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company_or_case('able_part')
---
+++ super/man/small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    first_group(str_arg)
+    print('able_group')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('come_thing')
