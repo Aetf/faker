85d227dcae407704221240491d9ba264357be729
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 12:05:14 -0400

    "rm: know super/part/see_place_on_good_fact/different_time/try_day/time.py into few woman"

---
+++ super/part/see_place_on_good_fact/different_time/try_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day_or_fact(str_arg):
+    be_place(str_arg)
+    print('know_child')
+
+def be_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day_or_fact('young_company_or_year')
