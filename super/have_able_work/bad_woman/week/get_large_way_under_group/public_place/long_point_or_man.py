65396255fd8f18536db1e3794c18357482795cb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 12:32:09 -0400

    "mv: know super/have_able_work/bad_woman/week/get_large_way_under_group/public_place/long_point_or_man.py or super/way/next_government_or_small_fact/part.py about other place"

---
+++ super/have_able_work/bad_woman/week/get_large_way_under_group/public_place/long_point_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    number(str_arg)
+    print('right_case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('point')
---
+++ super/way/next_government_or_small_fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    find_old_group_from_great_government(str_arg)
+    print('public_week')
+
+def find_old_group_from_great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('say_life_after_life')
