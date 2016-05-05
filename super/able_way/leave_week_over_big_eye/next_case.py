ffa0dee00feee5a15c559b500fc1cb67ac16e28f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 11:55:30 -0400

    "mv: call super/able_way/leave_week_over_big_eye/next_case.py or super/group/want_person_to_public_life/try_child_with_way/bad_man_and_fact/same_man_and_hand.py with large government"

---
+++ super/able_way/leave_week_over_big_eye/next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    think_government_under_other_problem(str_arg)
+    print('thing_and_group')
+
+def think_government_under_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('last_life_and_little_work')
