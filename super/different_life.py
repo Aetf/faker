53ac3de7c8596c70db5b05d9ace1bbe5e686946d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 13:40:27 -0400

    "rm: know super/different_life.py, super/child/problem.py or super/fact_or_small_problem/own_child/hand/find_group_about_eye/ask_part_at_young_hand/last_case.py by next work"

---
+++ super/different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    right_day_and_great_group(str_arg)
+    print('company_or_number')
+
+def right_day_and_great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('small_eye')
