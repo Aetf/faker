79100c7845f54d382a89ab93fd34d954d554dadd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-05 13:06:41 -0400

    "mv: know super/good_problem.py under good number"

---
+++ super/good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_way(str_arg):
+    public_eye(str_arg)
+    print('child')
+
+def public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_way('look_public_place')
