008aae6085a891e72a21cba8ad61234931f9895d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:42:12 -0400

    "add: do super/case_and_point.py in long group"

---
+++ super/case_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    fact(str_arg)
+    print('ask_eye_at_long_year')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('year')
