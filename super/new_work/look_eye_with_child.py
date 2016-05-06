6118ceac93f3c8f18e94a0b8f101b6e420370877
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 10:06:19 -0400

    "add: do super/new_work/look_eye_with_child.py or super/first_person/group/able_eye.py with public eye"

---
+++ super/new_work/look_eye_with_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group(str_arg):
+    life(str_arg)
+    print('point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group('other_hand')
