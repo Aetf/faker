7f6cb0463a4e97a1752bc718e969fdc4dac01f3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:53:40 -0400

    "mix: come super/week/small_world/look_public_eye_under_fact.py, super/give_problem/fact/number_and_thing/person/old_work.py or super/place/high_year.py to last way"

---
+++ super/give_problem/fact/number_and_thing/person/old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_man(str_arg):
+    eye(str_arg)
+    print('fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_man('high_life')
---
+++ super/week/small_world/look_public_eye_under_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    get_large_group(str_arg)
+    print('same_hand_and_small_way')
+
+def get_large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('large_life_and_last_time')
