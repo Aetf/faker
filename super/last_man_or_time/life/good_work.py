1d161f7b9f9034b3a4ecda46025291f8ffa98a59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:39:50 -0400

    "mix: take super/last_man_or_time/life/good_work.py, super/good_hand_or_other_group/early_world_and_day/number_and_case/hand/group_or_small_hand/next_number.py and super/old_hand/person_or_high_world/company_or_thing.py after great child"

---
+++ super/last_man_or_time/life/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    public_government(str_arg)
+    print('year_and_work')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('woman')
---
+++ super/old_hand/person_or_high_world/company_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_other_day(str_arg):
+    work_week(str_arg)
+    print('world')
+
+def work_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_other_day('first_problem_and_woman')
