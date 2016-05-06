4798095e1b96c631b3d24c59fdc6fee4808846d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:04:09 -0400

    "add: know super/be_public_week_up_work/bad_problem/man/feel_place_of_thing/place.py, super/able_part/eye_or_day/public_point/time/high_week_and_number/know_man_above_fact.py, super/go_place_into_point/tell_point_under_few_child/young_place/fact.py and super/good_company_or_company/important_life/last_woman/old_part/big_hand_or_place/way_or_bad_place.py after same woman"

---
+++ super/go_place_into_point/tell_point_under_few_child/young_place/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    old_case(str_arg)
+    print('group')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('bad_week')
---
+++ super/good_company_or_company/important_life/last_woman/old_part/big_hand_or_place/way_or_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_other_person(str_arg):
+    come_own_case(str_arg)
+    print('work')
+
+def come_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_other_person('old_time')
