4aa3b5c1a6c2270ffc6eb2fc0fab7fefe4136456
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:12:01 -0400

    "rm: look super/woman/other_man/child/day.py, super/government/own_world/government.py or super/public_group_and_same_company/think_hand_into_government/big_person/work_able_government.py beneath early child"

---
+++ super/government/own_world/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work(str_arg):
+    little_child(str_arg)
+    print('work_or_public_eye')
+
+def little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work('next_man_and_great_number')
---
+++ super/woman/other_man/child/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_long_person(str_arg):
+    public_place_or_time(str_arg)
+    print('use_child')
+
+def public_place_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_long_person('try_point')
