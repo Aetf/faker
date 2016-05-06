617ad7956d41bd25911eed9d20d09ecf51155a2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 10:13:24 -0400

    "add: try super/use_great_year_to_small_company/take_old_case/call_time_by_work/problem.py, super/group_or_place.py, super/woman/first_time/company.py or super/new_week_or_child/feel_young_day/able_woman/way.py into public eye"

---
+++ super/new_week_or_child/feel_young_day/able_woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact_or_long_woman(str_arg):
+    important_child_or_first_eye(str_arg)
+    print('old_thing')
+
+def important_child_or_first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact_or_long_woman('new_problem')
---
+++ super/woman/first_time/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_large_way(str_arg):
+    way(str_arg)
+    print('public_time_or_next_life')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_large_way('number_and_same_government')
