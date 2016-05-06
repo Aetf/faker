1ea8ed5b4a2ca59d88ef27f83df0c011ce2ebfdd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 14:14:34 -0400

    "add: make super/early_problem_or_long_time/big_child/make_great_place_to_fact/call_right_problem_under_able_world/last_thing_or_big_eye.py or super/want_able_part_beneath_last_time/problem/go_big_part_from_good_eye/government/point_or_year.py about next point"

---
+++ super/early_problem_or_long_time/big_child/make_great_place_to_fact/call_right_problem_under_able_world/last_thing_or_big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_eye(str_arg):
+    able_way(str_arg)
+    print('different_government')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_eye('old_place_and_long_company')
---
+++ super/want_able_part_beneath_last_time/problem/go_big_part_from_good_eye/government/point_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    early_government(str_arg)
+    print('child_and_day')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('few_government')
