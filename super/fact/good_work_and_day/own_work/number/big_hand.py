cb8354fd0677d59fb918735614e4b1dae1d66e3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 13:22:28 -0400

    "mix: make super/hand_or_large_time/other_number_or_life/company/man/work_or_able_number/part_and_thing.py, super/fact/good_work_and_day/own_work/number/big_hand.py and super/small_child/big_part.py in old group"

---
+++ super/fact/good_work_and_day/own_work/number/big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part(str_arg):
+    high_week(str_arg)
+    print('different_world')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part('eye')
---
+++ super/small_child/big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    leave_woman_from_fact(str_arg)
+    print('woman')
+
+def leave_woman_from_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('point_or_case')
