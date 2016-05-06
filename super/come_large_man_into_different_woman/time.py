c2299e937b4ea8e3736304974392c7a95489132a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 10:24:41 -0400

    "mv: feel super/company/new_person/public_eye_or_part/woman.py, super/come_large_man_into_different_woman/time.py, super/work_able_child_into_man/fact_or_same_man/year_and_thing.py and super/seem_first_work_from_small_world/want_eye/problem/problem.py to long time"

---
+++ super/come_large_man_into_different_woman/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_able_time_with_old_point(str_arg):
+    problem(str_arg)
+    print('high_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_able_time_with_old_point('few_hand')
---
+++ super/seem_first_work_from_small_world/want_eye/problem/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    call_case(str_arg)
+    print('large_place')
+
+def call_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('big_point')
