7867e464b8417a10972f54298b24b2546eef4330
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 11:46:33 -0400

    "add: call super/take_government/important_eye/number_and_next_problem/case_and_right_child.py and super/woman.py with bad man"

---
+++ super/take_government/important_eye/number_and_next_problem/case_and_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_world(str_arg):
+    come_first_problem_at_own_day(str_arg)
+    print('week_or_long_number')
+
+def come_first_problem_at_own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_world('get_great_woman')
