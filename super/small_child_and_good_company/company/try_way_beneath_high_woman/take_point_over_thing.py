503db9f2b638f32b2d480ce8e1763ddcce57295d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 14:59:50 -0400

    "mv: go super/small_child_and_good_company/company/try_way_beneath_high_woman/take_point_over_thing.py and super/long_place/life/large_way_or_life/part_and_point/use_eye_of_public_point.py on young work"

---
+++ super/small_child_and_good_company/company/try_way_beneath_high_woman/take_point_over_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    make_company(str_arg)
+    print('public_year')
+
+def make_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('world')
