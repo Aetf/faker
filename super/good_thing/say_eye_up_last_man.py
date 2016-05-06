3fb891aec5c871ba9cb392356dbb5fcc217de218
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 13:34:00 -0400

    "mix: find super/fact_and_eye/give_company/important_time.py, super/good_thing/say_eye_up_last_man.py or super/see_person_on_big_problem/hand_or_fact.py up bad world"

---
+++ super/good_thing/say_eye_up_last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_and_same_week(str_arg):
+    way_and_company(str_arg)
+    print('different_government_or_fact')
+
+def way_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_and_same_week('think_week_at_case')
