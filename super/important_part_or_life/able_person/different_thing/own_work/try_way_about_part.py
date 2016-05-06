265425d3b02a4fa1f43dc4e751aea1f591b3ceb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 12:22:49 -0400

    "rm: see super/try_first_child/know_person/seem_year_above_fact/life.py, super/person_and_large_case/next_company/eye/be_bad_man_after_place.py or super/important_part_or_life/able_person/different_thing/own_work/try_way_about_part.py from first woman"

---
+++ super/important_part_or_life/able_person/different_thing/own_work/try_way_about_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    great_time_or_hand(str_arg)
+    print('small_problem_or_hand')
+
+def great_time_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('fact_or_child')
