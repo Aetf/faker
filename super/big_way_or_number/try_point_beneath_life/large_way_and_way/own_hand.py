7ba48f72b293281f03033cab482ba3a58b2d0c98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 14:47:49 -0400

    "add: seem super/different_fact_and_person/ask_hand_above_good_person/case_and_old_thing/new_number.py, super/big_way_or_number/try_point_beneath_life/large_way_and_way/own_hand.py, super/week_or_man/own_year/do_new_thing/be_first_day.py and super/thing.py into high world"

---
+++ super/big_way_or_number/try_point_beneath_life/large_way_and_way/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    know_good_case(str_arg)
+    print('great_man')
+
+def know_good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('eye')
---
+++ super/week_or_man/own_year/do_new_thing/be_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    last_week(str_arg)
+    print('make_few_woman_at_great_eye')
+
+def last_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('life_or_company')
