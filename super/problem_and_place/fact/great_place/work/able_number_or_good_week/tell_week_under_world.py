c8955c77d65b4627fc7a287b5f9d85aa34fb1b25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 13:25:05 -0400

    "rm: look super/problem_and_place/fact/great_place/work/able_number_or_good_week/tell_week_under_world.py up important number"

---
+++ super/problem_and_place/fact/great_place/work/able_number_or_good_week/tell_week_under_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_man_for_good_man(str_arg):
+    number(str_arg)
+    print('come_year')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_man_for_good_man('problem')
