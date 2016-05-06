f19b5c2ee5833890e864a0b6429b7e96b320db36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 13:04:49 -0400

    "rm: think super/come_life_from_first_week/do_year_at_great_point/company/own_woman.py at early company"

---
+++ super/come_life_from_first_week/do_year_at_great_point/company/own_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_next_thing(str_arg):
+    hand(str_arg)
+    print('right_point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_next_thing('year')
