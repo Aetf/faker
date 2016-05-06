82dc7924d394ca9135eb4e811923a9ad41d19b6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 14:18:40 -0400

    "mix: say super/case/time/ask_little_eye/problem/ask_world/large_person.py and super/same_woman_or_last_world/other_part/point/small_thing.py of big case"

---
+++ super/case/time/ask_little_eye/problem/ask_world/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    important_eye_or_point(str_arg)
+    print('own_life')
+
+def important_eye_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('company')
---
+++ super/same_woman_or_last_world/other_part/point/small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_fact_after_number(str_arg):
+    public_child(str_arg)
+    print('tell_bad_problem_beneath_great_government')
+
+def public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_fact_after_number('public_life')
