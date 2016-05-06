ec6ae8fd6bf2651f83f013adc4bec338ebc5f4db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-06 12:38:53 -0400

    "mix: tell super/think_work/small_place_or_bad_place.py with own number"

---
+++ super/think_work/small_place_or_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_work_over_day(str_arg):
+    group(str_arg)
+    print('case')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_work_over_day('person')
