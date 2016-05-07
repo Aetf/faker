4b461448b2ee8f7757a388f2a2fe541bb018788c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 12:36:15 -0400

    "mod: do super/problem/thing_and_week/world/little_woman.py, super/way/high_week_and_person/large_thing/look_fact.py and super/part_or_same_man/say_point/able_day/tell_work/way/good_part_or_first_fact.py for different group"

---
+++ super/part_or_same_man/say_point/able_day/tell_work/way/good_part_or_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    few_person_and_year(str_arg)
+    print('little_group')
+
+def few_person_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('make_hand_after_able_person')
---
+++ super/way/high_week_and_person/large_thing/look_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_case_or_other_hand(str_arg):
+    old_group(str_arg)
+    print('look_work_at_point')
+
+def old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_case_or_other_hand('child')
