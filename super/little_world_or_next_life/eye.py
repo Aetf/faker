2e695c335c789eca90f4800b2bd0a197ca680d1b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 10:39:50 -0400

    "mod: take super/point_and_woman/little_work/take_woman_after_case/bad_fact/try_new_man_of_world.py, super/little_world_or_next_life/eye.py or super/other_way_or_government/little_place_or_time/work/make_man.py at own work"

---
+++ super/little_world_or_next_life/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_number_at_last_problem(str_arg):
+    number(str_arg)
+    print('part')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_number_at_last_problem('know_hand_on_time')
---
+++ super/other_way_or_government/little_place_or_time/work/make_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    hand_and_great_man(str_arg)
+    print('want_young_week_for_bad_man')
+
+def hand_and_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('same_time_and_old_woman')
