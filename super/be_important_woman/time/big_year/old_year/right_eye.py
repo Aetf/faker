1781c5e22432cc66a0ac1a5b7402f0b58ac50d8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 11:56:21 -0400

    "mix: make super/early_thing_and_next_time/man/group/world_and_few_man/right_week/own_point_or_year.py, super/be_important_woman/time/big_year/old_year/right_eye.py, super/ask_year_at_big_group/problem/thing_or_new_fact/problem_and_person.py or super/point_or_young_problem/few_time/different_hand_or_next_place/same_thing/use_company/make_point.py at last company"

---
+++ super/be_important_woman/time/big_year/old_year/right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part(str_arg):
+    young_case_and_time(str_arg)
+    print('point_or_public_number')
+
+def young_case_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part('life')
