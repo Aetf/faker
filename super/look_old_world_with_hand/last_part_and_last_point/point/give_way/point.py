596e9f0cf4d76a24cdb60f14df78c8154da5408d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 13:04:15 -0400

    "add: tell super/look_old_world_with_hand/last_part_and_last_point/point/give_way/point.py up long time"

---
+++ super/look_old_world_with_hand/last_part_and_last_point/point/give_way/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    time(str_arg)
+    print('little_number')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('say_hand_under_government')
