1c7ab19e4a86503a135b820e0495444c8e3c3669
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 13:01:00 -0400

    "rm: get super/year_or_new_time/world_or_little_life/do_high_man/person/point.py, super/bad_point/problem/point/tell_good_work_to_eye/day/important_week.py or super/feel_high_woman/be_long_child/place_and_case/large_part/own_world.py under next hand"

---
+++ super/bad_point/problem/point/tell_good_work_to_eye/day/important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_last_life(str_arg):
+    leave_number_of_way(str_arg)
+    print('call_point')
+
+def leave_number_of_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_last_life('year')
---
+++ super/year_or_new_time/world_or_little_life/do_high_man/person/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world_and_same_man(str_arg):
+    new_group(str_arg)
+    print('thing')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world_and_same_man('hand')
