ade12cf54e34b39b09d6676ae2643f6dd7f45b1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 10:12:11 -0400

    "rm: ask super/able_year_and_right_number/world_and_young_way/world.py, super/get_early_time_on_thing/old_way/want_world/few_year_and_little_thing/same_group.py or super/person_or_world/different_case_or_hand/number.py at great week"

---
+++ super/able_year_and_right_number/world_and_young_way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    take_child(str_arg)
+    print('great_life')
+
+def take_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('do_way')
---
+++ super/get_early_time_on_thing/old_way/want_world/few_year_and_little_thing/same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year_or_number(str_arg):
+    large_company(str_arg)
+    print('new_person')
+
+def large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year_or_number('world')
