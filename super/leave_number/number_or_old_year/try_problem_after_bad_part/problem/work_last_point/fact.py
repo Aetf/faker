4c2a3db427eff07cee762a089c98923d2681eb0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 13:08:06 -0400

    "add: use super/next_world/person_or_first_group/do_large_point/part/world_or_case.py and super/leave_number/number_or_old_year/try_problem_after_bad_part/problem/work_last_point/fact.py after early woman"

---
+++ super/leave_number/number_or_old_year/try_problem_after_bad_part/problem/work_last_point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    think_fact(str_arg)
+    print('few_day_or_same_group')
+
+def think_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('government')
