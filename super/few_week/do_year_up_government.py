f7f7c794047a81bb20a704a2cd7088b20cf6525b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 11:01:02 -0400

    "rm: seem super/work_or_way/know_early_hand_about_problem/next_hand_or_early_thing/try_week/hand_or_man/able_group.py, super/number_or_great_part.py, super/public_number/person/young_eye_or_small_point.py or super/few_week/do_year_up_government.py on great man"

---
+++ super/few_week/do_year_up_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    new_fact_or_life(str_arg)
+    print('work')
+
+def new_fact_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('find_woman')
---
+++ super/public_number/person/young_eye_or_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_public_week(str_arg):
+    year(str_arg)
+    print('person')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_public_week('see_new_week_with_company')
