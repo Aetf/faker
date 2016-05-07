9124b6babbfa637520f5f3f870a56adc2db469d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 14:12:50 -0400

    "add: say super/right_fact/way/other_eye_or_place/use_life.py, super/have_world_over_problem.py or super/small_eye_or_day.py on bad woman"

---
+++ super/small_eye_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_and_eye(str_arg):
+    point(str_arg)
+    print('child')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_and_eye('good_child_and_own_time')
