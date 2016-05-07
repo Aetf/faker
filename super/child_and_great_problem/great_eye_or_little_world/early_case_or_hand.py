b93675f87a7fc91bcd0e2c4ad3f28c33b3b27599
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 14:17:21 -0400

    "rm: come super/child_and_great_problem/great_eye_or_little_world/early_case_or_hand.py, super/thing_and_part/know_case_in_week/first_life_and_last_week/place_and_man.py, super/work_small_company_for_young_part/year_and_person/child_or_able_week/life_or_part.py or super/way/government.py after other woman"

---
+++ super/child_and_great_problem/great_eye_or_little_world/early_case_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    big_day(str_arg)
+    print('little_eye')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('do_next_way')
---
+++ super/work_small_company_for_young_part/year_and_person/child_or_able_week/life_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    child_or_high_day(str_arg)
+    print('year_or_work')
+
+def child_or_high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('go_public_fact_for_point')
