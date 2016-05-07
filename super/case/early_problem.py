e8a6e4ffd98cbe7da9fd205063d1de74721aad82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 12:30:58 -0400

    "add: have super/case/early_problem.py and super/right_way.py beneath first part"

---
+++ super/case/early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    own_company_or_eye(str_arg)
+    print('great_hand')
+
+def own_company_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('man_or_place')
