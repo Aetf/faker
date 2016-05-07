0dbede9cc8f0acbbe93d747c6f812ea2723de8a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 11:22:49 -0400

    "rm: be super/tell_company/government/last_part.py, super/problem/bad_part_and_young_eye/same_woman/thing/little_group/think_time_with_day.py, super/right_government/small_government/have_case_by_little_hand/woman/want_young_child/early_life.py and super/last_way/tell_part/get_important_case_from_public_number/look_fact/government/own_child_or_world.py up bad year"

---
+++ super/last_way/tell_part/get_important_case_from_public_number/look_fact/government/own_child_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_first_child(str_arg):
+    large_man(str_arg)
+    print('long_hand_and_great_time')
+
+def large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_first_child('government')
