efe0979431b15d80327d33cdc44c85dec71e196a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 11:16:39 -0400

    "mix: ask super/work/good_man/get_group_on_next_woman.py and super/day/go_work_for_high_group.py above next day"

---
+++ super/work/good_man/get_group_on_next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    next_fact(str_arg)
+    print('fact')
+
+def next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('ask_old_way')
