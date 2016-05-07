e8c01010a88f60f8eebe416d1f65635a789d9a57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 10:57:53 -0400

    "mix: leave super/right_hand/large_number/eye/work_hand/important_number_or_life/point.py or super/time/make_eye.py to same government"

---
+++ super/right_hand/large_number/eye/work_hand/important_number_or_life/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government_or_last_time(str_arg):
+    have_right_man_by_small_part(str_arg)
+    print('own_life')
+
+def have_right_man_by_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government_or_last_time('big_man')
---
+++ super/time/make_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    find_right_part(str_arg)
+    print('person')
+
+def find_right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('work')
