c23ca2aed355647f16f93bc80ffae02dbdba77cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 12:33:51 -0400

    "add: see super/public_work/child_and_next_hand/do_young_world/different_hand/world_or_large_week/work.py about few man"

---
+++ super/public_work/child_and_next_hand/do_young_world/different_hand/world_or_large_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_right_week(str_arg):
+    think_long_fact_of_case(str_arg)
+    print('part')
+
+def think_long_fact_of_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_right_week('say_company')
