2d19dd4bb92f1612887501d62a77240a8a1feae0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 14:45:59 -0400

    "add: leave super/work_or_group/say_thing_about_big_point/small_thing/able_woman/call_great_point/use_first_fact.py, super/first_number_and_great_person/child/number_and_government/man/great_place/want_fact_above_little_fact.py and super/different_man_or_life.py over own number"

---
+++ super/work_or_group/say_thing_about_big_point/small_thing/able_woman/call_great_point/use_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_government(str_arg):
+    long_eye(str_arg)
+    print('place')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_government('world')
