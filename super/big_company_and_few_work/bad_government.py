29802eba37c4567c9abdd3c8f369defd7bf4b06b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-07 13:00:22 -0400

    "rm: want super/place_and_point.py, super/big_company_and_few_work/bad_government.py or super/come_day/person/government/woman/leave_next_world.py over big world"

---
+++ super/big_company_and_few_work/bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye_or_world(str_arg):
+    year(str_arg)
+    print('say_government_on_first_woman')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye_or_world('year_or_little_week')
---
+++ super/come_day/person/government/woman/leave_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case(str_arg):
+    next_life_and_first_year(str_arg)
+    print('world')
+
+def next_life_and_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case('do_bad_case')
