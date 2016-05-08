04d5c92e0733c86540a784a487e76add0597ca12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 11:15:39 -0400

    "rm: say super/day/government_or_woman.py and super/go_hand/tell_woman.py under young point"

---
+++ super/day/government_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    seem_person(str_arg)
+    print('person')
+
+def seem_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('point_or_little_week')
---
+++ super/go_hand/tell_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    large_time(str_arg)
+    print('hand')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('place')
