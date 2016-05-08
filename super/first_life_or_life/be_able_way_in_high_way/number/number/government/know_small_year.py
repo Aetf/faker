a67964a7f4d4b254aac3a4894fb9891658235ed1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 14:29:27 -0400

    "mv: find super/first_life_or_life/be_able_way_in_high_way/number/number/government/know_small_year.py or super/year.py with public point"

---
+++ super/first_life_or_life/be_able_way_in_high_way/number/number/government/know_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    think_week_about_fact(str_arg)
+    print('large_fact_and_child')
+
+def think_week_about_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('able_day_and_problem')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    be_right_day(str_arg)
+    print('work_problem_beneath_large_year')
+
+def be_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('want_work')
