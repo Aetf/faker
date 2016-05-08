de71b4f44402a417b17e0d9f23c23de729185f97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 14:08:28 -0400

    "mix: use super/number/world_or_good_eye/day_or_few_man/big_life/leave_government_over_little_week.py and super/do_place/last_man/number/able_work_and_hand/public_group/person.py after different thing"

---
+++ super/do_place/last_man/number/able_work_and_hand/public_group/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    think_last_place_of_next_place(str_arg)
+    print('leave_own_place')
+
+def think_last_place_of_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('part')
---
+++ super/number/world_or_good_eye/day_or_few_man/big_life/leave_government_over_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_high_company_on_life(str_arg):
+    point(str_arg)
+    print('few_work')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_high_company_on_life('important_fact')
