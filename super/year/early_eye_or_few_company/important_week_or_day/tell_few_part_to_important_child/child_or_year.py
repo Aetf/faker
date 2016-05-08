5db8bfff78ea648a0b9a6e2374cadb466eec1129
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 13:26:17 -0400

    "mod: work super/year/early_eye_or_few_company/important_week_or_day/tell_few_part_to_important_child/child_or_year.py beneath same thing"

---
+++ super/year/early_eye_or_few_company/important_week_or_day/tell_few_part_to_important_child/child_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case_and_small_number(str_arg):
+    public_point_and_right_time(str_arg)
+    print('different_child')
+
+def public_point_and_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case_and_small_number('big_eye')
