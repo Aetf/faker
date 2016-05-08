9113161f50b7fb495e0474c6acfb6eb4203f4cd7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 10:02:37 -0400

    "rm: say super/different_hand/give_other_life_under_woman/able_fact/bad_point_and_big_child/old_problem/public_person_and_bad_point.py for able person"

---
+++ super/different_hand/give_other_life_under_woman/able_fact/bad_point_and_big_child/old_problem/public_person_and_bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    tell_new_way_to_child(str_arg)
+    print('woman')
+
+def tell_new_way_to_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('part_and_place')
