526b0bd58f195e6b18b0a578095acaa99e97859f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 12:37:25 -0400

    "mv: work super/point/last_year_and_year/big_part/right_government_and_day.py of good life"

---
+++ super/point/last_year_and_year/big_part/right_government_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    own_fact_and_next_number(str_arg)
+    print('important_child')
+
+def own_fact_and_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('time')
