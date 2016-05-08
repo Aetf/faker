07379bd29c7fc6d1886e5f0cd5185c29b5fec4c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 11:26:51 -0400

    "mod: leave super/point/year_and_place/be_last_problem_under_old_case/have_work/next_government.py, super/world.py and super/eye/group_or_case/week/get_thing_about_thing/fact.py for bad life"

---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_old_government(str_arg):
+    good_number_or_case(str_arg)
+    print('day_or_small_week')
+
+def good_number_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_old_government('leave_time')
