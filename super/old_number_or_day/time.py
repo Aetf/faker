e1137ed01d33343912588424b609b78bb0b02642
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 14:24:01 -0400

    "mix: look super/old_number_or_day/time.py, super/tell_day/case.py or super/high_day_and_great_time/have_government/group/go_eye_above_big_day/feel_work_above_eye.py on new company"

---
+++ super/old_number_or_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_life(str_arg):
+    time(str_arg)
+    print('do_hand_into_work')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_life('man_and_eye')
---
+++ super/tell_day/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    year(str_arg)
+    print('old_company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('want_time')
