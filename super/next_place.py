3c1d845c8d29a976e69d7600f5e504920a1a000e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 13:19:17 -0400

    "add: look super/man_or_time/thing.py and super/next_place.py at big hand"

---
+++ super/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number(str_arg):
+    part_or_man(str_arg)
+    print('great_government')
+
+def part_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number('large_week_and_government')
