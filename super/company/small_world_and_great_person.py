a2e131bfac5bdbf15fb56128a5fefcab7d6264e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 11:30:58 -0400

    "add: tell super/company/small_world_and_great_person.py from few problem"

---
+++ super/company/small_world_and_great_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_person_into_new_life(str_arg):
+    leave_group_by_government(str_arg)
+    print('way')
+
+def leave_group_by_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_person_into_new_life('fact')
