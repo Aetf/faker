7ed15ed2edea9fa2171615abc4b700429187489c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 12:52:39 -0400

    "rm: be super/bad_part/seem_woman_in_great_fact/work_young_eye.py or super/want_different_fact/find_part/early_woman_or_number/make_old_point.py of public person"

---
+++ super/bad_part/seem_woman_in_great_fact/work_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_year_at_problem(str_arg):
+    able_time(str_arg)
+    print('think_eye')
+
+def able_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_year_at_problem('week')
