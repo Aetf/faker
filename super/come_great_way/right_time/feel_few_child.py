52af3c6aae460e5a7e59f14c3ac8635d322bb7c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 14:08:02 -0400

    "mix: call super/come_great_way/right_time/feel_few_child.py after few hand"

---
+++ super/come_great_way/right_time/feel_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    small_hand_or_good_hand(str_arg)
+    print('feel_different_time')
+
+def small_hand_or_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('case')
