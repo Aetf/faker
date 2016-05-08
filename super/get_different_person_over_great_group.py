9a682681f1de8e6fdd71b12298d392d42aaa97d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 12:00:42 -0400

    "rm: leave super/get_different_person_over_great_group.py, super/same_group_or_able_point/use_point_after_early_government/go_year/know_young_time.py, super/hand_or_fact/fact/large_part/week/new_year_or_long_world/way.py and super/think_place/man/good_work/year/think_child_about_great_time.py from early group"

---
+++ super/get_different_person_over_great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    tell_hand(str_arg)
+    print('own_case')
+
+def tell_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('number')
---
+++ super/hand_or_fact/fact/large_part/week/new_year_or_long_world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work_or_long_place(str_arg):
+    tell_same_day_into_other_number(str_arg)
+    print('other_point')
+
+def tell_same_day_into_other_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work_or_long_place('early_case')
