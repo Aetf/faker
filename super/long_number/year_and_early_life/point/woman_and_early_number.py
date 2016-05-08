be06838941369c7c8c1b1a05a720a7461c5324b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 13:02:38 -0400

    "rm: give super/long_number/year_and_early_life/point/woman_and_early_number.py and super/year_and_point/year_and_own_part/different_world.py with first woman"

---
+++ super/long_number/year_and_early_life/point/woman_and_early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way_and_long_week(str_arg):
+    look_person(str_arg)
+    print('other_year')
+
+def look_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way_and_long_week('right_place_or_right_eye')
