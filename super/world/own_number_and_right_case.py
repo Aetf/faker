2ffb8f90f1c61d5c41f64c46355d48fdc8208aab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 11:53:08 -0400

    "add: find super/world/own_number_and_right_case.py beneath small problem"

---
+++ super/world/own_number_and_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_world_and_good_company(str_arg):
+    life(str_arg)
+    print('time')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_world_and_good_company('great_number')
