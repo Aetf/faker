70cee87bbc50a76e3560176d4824a569332a5355
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 12:57:31 -0400

    "rm: think super/do_week_of_small_part/different_person/use_different_eye/next_world_or_problem/get_great_problem.py or super/week_and_group.py up new week"

---
+++ super/week_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    group(str_arg)
+    print('case')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('life')
