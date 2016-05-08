8ec577c0abaabc549547e22bed6c44704f9a497b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 10:40:49 -0400

    "rm: work super/try_place_for_number/case_and_different_point.py, super/world/work_company/bad_woman/look_company_beneath_point.py, super/problem.py or super/fact.py beneath first time"

---
+++ super/try_place_for_number/case_and_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    small_life(str_arg)
+    print('know_same_way')
+
+def small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('leave_company')
---
+++ super/world/work_company/bad_woman/look_company_beneath_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week(str_arg)
+    print('hand_or_long_world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('give_same_case')
