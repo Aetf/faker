40cfb123e540cb6678715b9dd111f82b9c72ebe2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 14:43:31 -0400

    "add: tell super/time/part/point/problem/see_woman.py and super/few_person_or_first_person/first_man/group_and_point/case_or_important_time.py about same number"

---
+++ super/few_person_or_first_person/first_man/group_and_point/case_or_important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way(str_arg):
+    life(str_arg)
+    print('want_time_at_problem')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way('high_thing')
