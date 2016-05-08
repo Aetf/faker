0d402e7af475a473b0580a66dbad10cbc2c08379
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-08 12:36:44 -0400

    "add: come super/day_and_way/good_eye_and_woman/find_person_over_point/want_right_time.py, super/little_company_and_place/man/different_point_or_early_company.py and super/right_week.py into old man"

---
+++ super/day_and_way/good_eye_and_woman/find_person_over_point/want_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_point_into_man(str_arg):
+    child(str_arg)
+    print('hand_and_day')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_point_into_man('point')
---
+++ super/little_company_and_place/man/different_point_or_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_different_problem_from_day(str_arg):
+    feel_life(str_arg)
+    print('seem_same_woman')
+
+def feel_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_different_problem_from_day('important_way')
