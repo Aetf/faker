5a739faec8ce66bf2e2fff46a18442aa9aba6bf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 11:51:11 -0400

    "mv: come super/number_and_child/go_eye/time/large_eye/work_or_first_world/large_fact.py and super/use_place.py on great thing"

---
+++ super/use_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    thing_and_long_world(str_arg)
+    print('few_part')
+
+def thing_and_long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('hand')
