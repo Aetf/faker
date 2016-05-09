50ad81afcdc071e66cbc3ca30c7e05f1b0827d5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 12:17:49 -0400

    "mv: have super/go_early_problem/big_time_and_good_child/time/person.py, super/few_life/world.py and super/find_long_man/high_thing/make_public_work_to_other_day/week/part.py of few way"

---
+++ super/few_life/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    first_thing(str_arg)
+    print('know_fact')
+
+def first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('small_way_and_small_government')
---
+++ super/go_early_problem/big_time_and_good_child/time/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_man(str_arg):
+    woman(str_arg)
+    print('person')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_man('week')
