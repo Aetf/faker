07c6d020ea4f42b73b570807e3f774913d7432ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 14:51:51 -0400

    "mv: come super/eye_or_own_fact/long_problem/tell_problem/other_way_or_work/time/good_group_and_able_life.py and super/person/life_or_different_thing/eye_and_right_hand.py under important fact"

---
+++ super/person/life_or_different_thing/eye_and_right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_part(str_arg):
+    own_work(str_arg)
+    print('long_work')
+
+def own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_part('way')
