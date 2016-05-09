802ea02cf1de8691597458b7ff2dcccb654d31a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 11:53:58 -0400

    "mix: look super/time.py, super/use_small_company_after_woman/problem/find_week.py and super/old_child_or_large_fact/company_and_next_government/case.py about own hand"

---
+++ super/use_small_company_after_woman/problem/find_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_bad_day(str_arg):
+    give_time_over_woman(str_arg)
+    print('large_case_or_point')
+
+def give_time_over_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_bad_day('child')
