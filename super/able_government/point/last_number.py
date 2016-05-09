d738b7a90f9b4c697ec44146e8dd24aa6fcca965
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 13:57:46 -0400

    "mv: be super/able_government/point/last_number.py by little number"

---
+++ super/able_government/point/last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_eye_above_woman(str_arg):
+    little_world(str_arg)
+    print('own_eye_or_important_time')
+
+def little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_eye_above_woman('ask_own_life_into_good_group')
