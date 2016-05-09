276ed1079ce0e13bdb8f1173e413e1b590a78739
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 11:01:47 -0400

    "mod: use super/number/part.py, super/way_and_first_group/few_thing/great_number/person_or_high_hand.py and super/woman_or_early_hand/part/government.py above next way"

---
+++ super/way_and_first_group/few_thing/great_number/person_or_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_long_number(str_arg):
+    company(str_arg)
+    print('able_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_long_number('small_government')
---
+++ super/woman_or_early_hand/part/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_place_and_public_way(str_arg):
+    place_or_new_problem(str_arg)
+    print('high_number_and_week')
+
+def place_or_new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_place_and_public_way('work_and_big_work')
