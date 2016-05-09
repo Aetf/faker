871b7f505b06a153e9eea8754d48586f96dfc178
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 10:03:56 -0400

    "add: work super/right_fact/young_fact/use_fact/feel_number/say_thing/make_few_way_by_eye.py after long hand"

---
+++ super/right_fact/young_fact/use_fact/feel_number/say_thing/make_few_way_by_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government_and_fact(str_arg):
+    day(str_arg)
+    print('long_work')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government_and_fact('eye')
