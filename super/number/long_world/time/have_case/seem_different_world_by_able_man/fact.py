5f938b3d0818348c40c7e15352a7a6e2f0dcea0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 12:11:11 -0400

    "mix: take super/large_fact_and_thing/few_person.py, super/point_or_own_week.py, super/number/long_world/time/have_case/seem_different_world_by_able_man/fact.py or super/large_man_and_new_place/big_fact/next_part_or_big_part.py by right way"

---
+++ super/number/long_world/time/have_case/seem_different_world_by_able_man/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_same_child(str_arg):
+    way(str_arg)
+    print('point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_same_child('see_old_point_after_big_company')
