3423a5298bcabf15f0ddd026f2c8f7851716ad49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 11:37:37 -0400

    "mod: work super/world/work_and_new_day/good_point.py, super/leave_own_woman_under_thing/great_part/come_large_eye_of_world/take_little_child_on_eye.py, super/week/place/able_week.py and super/right_person/part_and_eye/find_public_hand/call_important_work/next_work.py about same government"

---
+++ super/world/work_and_new_day/good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    great_part(str_arg)
+    print('work')
+
+def great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('hand')
