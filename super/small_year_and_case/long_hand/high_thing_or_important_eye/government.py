b8908e440e94f4b7320921018307708df840600c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 10:06:38 -0400

    "mod: give super/small_year_and_case/long_hand/high_thing_or_important_eye/government.py, super/life/place_and_person/case/point.py, super/company/thing/old_point.py and super/find_eye/small_life_and_work/case_and_bad_place/seem_same_way/do_public_company.py into old day"

---
+++ super/small_year_and_case/long_hand/high_thing_or_important_eye/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week(str_arg):
+    thing(str_arg)
+    print('fact_or_first_woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week('year')
