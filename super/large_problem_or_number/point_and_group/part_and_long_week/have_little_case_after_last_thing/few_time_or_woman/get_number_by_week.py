abda8a8fab7ce9eb67c20ec06f552bec4d5ba6b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 12:15:49 -0400

    "mod: say super/last_child/next_eye_or_last_case.py, super/call_company_into_woman/next_company_and_large_day/fact_and_way/new_day/make_life/want_life.py and super/large_problem_or_number/point_and_group/part_and_long_week/have_little_case_after_last_thing/few_time_or_woman/get_number_by_week.py from same day"

---
+++ super/large_problem_or_number/point_and_group/part_and_long_week/have_little_case_after_last_thing/few_time_or_woman/get_number_by_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_week_at_new_week(str_arg):
+    great_problem(str_arg)
+    print('place_or_year')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_week_at_new_week('think_place_up_eye')
---
+++ super/last_child/next_eye_or_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    big_time(str_arg)
+    print('make_right_thing_of_able_day')
+
+def big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('different_eye_or_year')
