9fdaa1b724fbd0a8be6bbfe396956ce97f1e9f08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 11:56:04 -0400

    "add: use super/fact/eye.py with few life"

---
+++ super/fact/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_right_thing_with_own_life(str_arg):
+    thing(str_arg)
+    print('day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_right_thing_with_own_life('company')
