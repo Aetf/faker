f78e6b30a61117ae9b747dea836d265920dccbb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 12:28:29 -0400

    "mv: seem super/man_and_same_hand.py and super/year/fact/different_year_or_large_group/problem_or_last_place/say_same_fact_at_other_company.py of little man"

---
+++ super/year/fact/different_year_or_large_group/problem_or_last_place/say_same_fact_at_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_number_at_high_time(str_arg):
+    child(str_arg)
+    print('make_large_child_about_way')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_number_at_high_time('own_hand_and_person')
