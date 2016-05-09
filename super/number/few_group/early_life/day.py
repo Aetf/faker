82dbb8d2317b30ac8d8ed89f56f09e901943121b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 10:27:51 -0400

    "rm: look super/person/next_world.py or super/number/few_group/early_life/day.py under next problem"

---
+++ super/number/few_group/early_life/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_week(str_arg):
+    day_and_woman(str_arg)
+    print('right_government')
+
+def day_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_week('right_man_and_day')
