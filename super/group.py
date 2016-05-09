b38f2a7f38959e2ffeef15dcd81f1966047d178d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-09 13:03:34 -0400

    "add: see super/group.py at important world"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_life(str_arg):
+    work_group(str_arg)
+    print('good_woman')
+
+def work_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_life('thing')
