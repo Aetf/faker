d9a8b3b3a907d75163fe07c46b2c6db15a162f93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 14:21:33 -0400

    "mv: come super/woman/work_other_group_after_same_case/work/good_company_and_man.py, super/thing/big_time/great_year/young_hand/woman/look_man_above_public_world.py, super/feel_number/seem_thing_under_eye/see_problem/case/world/able_world_and_small_problem.py and super/ask_little_problem_over_other_thing/right_person/able_group.py of big part"

---
+++ super/feel_number/seem_thing_under_eye/see_problem/case/world/able_world_and_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    use_other_eye(str_arg)
+    print('get_part')
+
+def use_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('feel_company_at_long_company')
