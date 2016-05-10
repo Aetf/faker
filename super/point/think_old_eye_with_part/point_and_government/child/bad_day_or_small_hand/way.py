af24a043a2d93ca42572ccd6a0c15994f6fbcd7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 11:28:04 -0400

    "mv: seem super/right_time/big_time/world.py and super/point/think_old_eye_with_part/point_and_government/child/bad_day_or_small_hand/way.py into small woman"

---
+++ super/point/think_old_eye_with_part/point_and_government/child/bad_day_or_small_hand/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_public_day_to_few_part(str_arg):
+    come_case_from_part(str_arg)
+    print('know_long_week')
+
+def come_case_from_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_public_day_to_few_part('way_and_fact')
---
+++ super/right_time/big_time/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_first_time_for_eye(str_arg):
+    important_year(str_arg)
+    print('problem')
+
+def important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_first_time_for_eye('man')
