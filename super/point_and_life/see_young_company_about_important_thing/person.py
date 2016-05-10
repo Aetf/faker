5cb2a28244d707995a0255342f8e7bfb32cad998
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 11:38:55 -0400

    "mv: know super/point_and_life/see_young_company_about_important_thing/person.py in large woman"

---
+++ super/point_and_life/see_young_company_about_important_thing/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_great_way_over_case(str_arg):
+    do_other_time_with_able_number(str_arg)
+    print('try_able_way')
+
+def do_other_time_with_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_great_way_over_case('long_case')
