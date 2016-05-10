c57966e6dec5f1fdac18d7206a38a7605f0449a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:14:56 -0400

    "rm: go super/next_hand/new_woman/public_government_or_group.py, super/fact/old_group_and_number/part.py or super/old_company_and_case/woman_or_life/take_other_fact/see_life/look_next_life_at_long_child.py at large eye"

---
+++ super/old_company_and_case/woman_or_life/take_other_fact/see_life/look_next_life_at_long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_group_on_little_time(str_arg):
+    young_life(str_arg)
+    print('place_or_life')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_group_on_little_time('part_and_first_fact')
