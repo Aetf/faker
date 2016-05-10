82bbcef08dbe9495ba9117df98680503439b6d37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:21:04 -0400

    "rm: say super/important_government_or_year/right_company.py at other part"

---
+++ super/important_government_or_year/right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time(str_arg):
+    young_time(str_arg)
+    print('public_company')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time('want_number_up_old_company')
