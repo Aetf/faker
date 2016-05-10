43fa926ffa8ab6e7ed4b87574e8c3485cbff6264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 13:03:15 -0400

    "mod: call super/person/first_group/case/have_new_number_about_large_company/early_way/person.py, super/tell_small_part/year_and_woman/important_work/first_life.py, super/company_or_life/leave_bad_thing/early_place/public_thing_or_great_way/little_world/part_and_old_way.py and super/point.py with good year"

---
+++ super/company_or_life/leave_bad_thing/early_place/public_thing_or_great_way/little_world/part_and_old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    come_place_from_thing(str_arg)
+    print('public_company')
+
+def come_place_from_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('think_case')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    large_time_or_bad_year(str_arg)
+    print('old_woman')
+
+def large_time_or_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('time_or_old_thing')
