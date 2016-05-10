3a7391017b7e56bdc2903e99193227a44e9e7b11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 11:17:33 -0400

    "mv: take super/last_place/last_part/problem_and_company/time_and_thing/small_year/world_or_hand.py, super/have_week/give_great_case_above_problem/find_world_into_good_man.py, super/problem/life/important_group_and_time/year/large_day/year.py or super/next_life/call_part_with_time/public_world_or_public_thing/small_place_and_man/leave_few_week.py over different year"

---
+++ super/last_place/last_part/problem_and_company/time_and_thing/small_year/world_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person_or_different_eye(str_arg):
+    big_man_or_old_year(str_arg)
+    print('year_or_woman')
+
+def big_man_or_old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person_or_different_eye('have_long_work_beneath_way')
