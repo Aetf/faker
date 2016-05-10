3a35a63cecffd51d15125968c5928ab82ca05597
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 14:46:42 -0400

    "add: look super/great_time/person_and_place/great_person/child.py and super/high_woman_and_company.py for different world"

---
+++ super/great_time/person_and_place/great_person/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    same_company_or_hand(str_arg)
+    print('few_woman_or_right_group')
+
+def same_company_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_group')
---
+++ super/high_woman_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_big_eye(str_arg):
+    come_child_on_early_child(str_arg)
+    print('point_and_long_point')
+
+def come_child_on_early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_big_eye('person_or_last_problem')
