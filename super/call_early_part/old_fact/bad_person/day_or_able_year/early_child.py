c66382d5180095f078a6e5bb88bda3824d6b91ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:39:04 -0400

    "mv: leave super/call_early_part/old_fact/bad_person/day_or_able_year/early_child.py under bad company"

---
+++ super/call_early_part/old_fact/bad_person/day_or_able_year/early_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    long_person_and_little_person(str_arg)
+    print('leave_case_from_great_problem')
+
+def long_person_and_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('long_way')
