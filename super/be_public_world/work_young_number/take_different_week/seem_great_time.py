4dcbadc256dfcc9ab06693094eeb54cd1754664c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 13:07:22 -0400

    "mod: work super/be_public_world/work_young_number/take_different_week/seem_great_time.py with young place"

---
+++ super/be_public_world/work_young_number/take_different_week/seem_great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    man(str_arg)
+    print('little_problem_or_number')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('problem')
