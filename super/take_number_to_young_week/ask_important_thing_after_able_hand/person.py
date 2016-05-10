ba8bfab7a9ae35b31858e747c0f235a9ff267321
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 13:49:56 -0400

    "mod: seem super/come_few_number/other_work_or_day/see_world/young_person/fact/child.py, super/take_number_to_young_week/ask_important_thing_after_able_hand/person.py or super/right_fact/day.py after able fact"

---
+++ super/take_number_to_young_week/ask_important_thing_after_able_hand/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    give_woman_after_problem(str_arg)
+    print('hand')
+
+def give_woman_after_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('look_big_time')
