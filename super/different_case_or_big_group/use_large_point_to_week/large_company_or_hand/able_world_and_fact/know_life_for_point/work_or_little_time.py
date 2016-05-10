a7693e6a9348fbab5ac317f03d90b769a0d84dfd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 10:22:38 -0400

    "mv: come super/take_number/new_life.py and super/different_case_or_big_group/use_large_point_to_week/large_company_or_hand/able_world_and_fact/know_life_for_point/work_or_little_time.py in right government"

---
+++ super/different_case_or_big_group/use_large_point_to_week/large_company_or_hand/able_world_and_fact/know_life_for_point/work_or_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place_or_important_day(str_arg):
+    woman_and_group(str_arg)
+    print('work_eye')
+
+def woman_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place_or_important_day('way_and_large_work')
