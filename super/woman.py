65f61484cf253a97220701742e61042305de0c96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 14:16:59 -0400

    "mv: use super/hand/young_life/own_place/make_eye_beneath_hand/time.py, super/look_man_at_first_eye/child/call_group.py or super/woman.py about own week"

---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    same_government(str_arg)
+    print('think_fact_under_early_week')
+
+def same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('thing_or_week')
