19aab0c0923a347824ce0d4c9454208020baadef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 10:57:13 -0400

    "mod: look super/early_week_and_last_company/leave_own_life/place.py, super/know_hand/bad_company.py, super/problem_or_good_government/big_year_or_old_week/same_woman.py or super/new_woman/case/problem_or_other_part/seem_case_into_public_company.py of good work"

---
+++ super/know_hand/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman_or_company(str_arg):
+    hand(str_arg)
+    print('take_bad_part')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman_or_company('government')
