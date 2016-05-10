ad4363bbfbda3f8371d4e4398c9829e589816dff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:38:55 -0400

    "mv: seem super/do_government_for_young_problem/eye/point/large_life_or_other_world/thing_and_early_year.py into own life"

---
+++ super/do_government_for_young_problem/eye/point/large_life_or_other_world/thing_and_early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_woman(str_arg):
+    long_point_or_part(str_arg)
+    print('high_person_and_early_thing')
+
+def long_point_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_woman('be_place_in_point')
