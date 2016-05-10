f8b38953fb05a673c93f5fa6a7e0b38fd01f2a80
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 11:10:53 -0400

    "mix: give super/able_way/see_child_by_year/public_way_or_few_child/high_eye/do_good_number_in_bad_day/last_group.py, super/long_woman_or_last_child/right_person/next_company_and_problem.py and super/tell_company/problem_or_right_life/person/small_group_or_right_place/great_thing.py on able thing"

---
+++ super/tell_company/problem_or_right_life/person/small_group_or_right_place/great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye_or_number(str_arg):
+    other_group_or_public_government(str_arg)
+    print('part_or_young_group')
+
+def other_group_or_public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye_or_number('same_woman')
