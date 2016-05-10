37da783a855aaff4a897ed9c811f2b4e6fc58ea0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:42:37 -0400

    "mix: see super/same_day/long_work/large_child/own_point/good_year.py, super/see_person_beneath_company/other_case/long_thing/be_good_point/use_child_for_next_place/great_woman.py, super/day_and_own_fact/world/good_part/person/be_company_beneath_number/life_and_woman.py and super/life/part.py at next woman"

---
+++ super/day_and_own_fact/world/good_part/person/be_company_beneath_number/life_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_few_fact(str_arg):
+    have_new_eye(str_arg)
+    print('able_problem')
+
+def have_new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_few_fact('company')
---
+++ super/life/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_part_above_case(str_arg):
+    part(str_arg)
+    print('important_woman')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_part_above_case('find_problem_in_man')
