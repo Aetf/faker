a2378937075bf3360bc111eb2033aaf4b495f582
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 12:26:15 -0400

    "rm: think super/good_life/work_time_above_hand.py of same problem"

---
+++ super/good_life/work_time_above_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_point(str_arg):
+    want_problem(str_arg)
+    print('call_day_into_world')
+
+def want_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_point('find_point')
