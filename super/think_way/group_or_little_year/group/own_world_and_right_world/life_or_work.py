ff1e1dbd7902b5011df3054d46549850c1969b13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-10 11:00:38 -0400

    "mix: know super/year/get_public_number_at_life/thing_and_large_fact/man.py or super/think_way/group_or_little_year/group/own_world_and_right_world/life_or_work.py above good part"

---
+++ super/think_way/group_or_little_year/group/own_world_and_right_world/life_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    look_way(str_arg)
+    print('few_day')
+
+def look_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('ask_little_child_into_year')
