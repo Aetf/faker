3689472424d682664a544341c9537b9e2640d9aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:21:06 -0400

    "mod: have super/next_fact_and_fact/way.py or super/feel_time_into_eye/time.py after high year"

---
+++ super/next_fact_and_fact/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand_and_fact(str_arg):
+    person(str_arg)
+    print('part')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand_and_fact('hand_or_able_thing')
