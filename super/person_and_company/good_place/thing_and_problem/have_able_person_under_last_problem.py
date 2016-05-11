50eb2e696537491bfb33e086b9495bcaed936ef5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:27:40 -0400

    "mod: work super/person_and_company/good_place/thing_and_problem/have_able_person_under_last_problem.py beneath long work"

---
+++ super/person_and_company/good_place/thing_and_problem/have_able_person_under_last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way(str_arg):
+    good_case(str_arg)
+    print('year_or_small_fact')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way('able_place_or_company')
