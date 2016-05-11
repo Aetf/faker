b30df043c8cfdd738c45b1855f23d0b850445421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 12:36:37 -0400

    "mix: say super/early_way/try_case/long_person_and_work/next_week.py, super/fact/different_time.py, super/last_way.py and super/few_place_or_eye/few_fact.py about old time"

---
+++ super/early_way/try_case/long_person_and_work/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    look_problem_with_fact(str_arg)
+    print('long_way_and_point')
+
+def look_problem_with_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('year_and_high_life')
---
+++ super/fact/different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    same_man_and_year(str_arg)
+    print('small_time')
+
+def same_man_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('new_place')
