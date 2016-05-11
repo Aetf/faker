c6a28ac3c59e5f7109ad864e244f09ec309d3f4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 13:29:04 -0400

    "mix: go super/year/work_and_other_hand/thing/think_group_at_case/number_and_great_year.py, super/long_woman/new_person.py or super/new_year/child_and_point.py from same group"

---
+++ super/long_woman/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place(str_arg):
+    come_group_over_day(str_arg)
+    print('important_part')
+
+def come_group_over_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place('great_way')
---
+++ super/new_year/child_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_or_fact(str_arg):
+    small_world_or_person(str_arg)
+    print('case')
+
+def small_world_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_or_fact('group_and_case')
