f3d9d5a5fec94f201209fa35424ab1d4bab5f7e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 14:11:57 -0400

    "mv: give super/use_same_fact/have_time.py, super/old_work.py or super/same_problem/new_person.py into young year"

---
+++ super/same_problem/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    important_person(str_arg)
+    print('high_year')
+
+def important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('long_number')
---
+++ super/use_same_fact/have_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_good_day(str_arg):
+    early_part_and_thing(str_arg)
+    print('ask_man_at_public_hand')
+
+def early_part_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_good_day('good_life')
