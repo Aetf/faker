2e4b08fe791f2e62b3d4b058b4829079859f4504
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 10:13:18 -0400

    "mod: ask super/eye/do_problem.py about new hand"

---
+++ super/eye/do_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    small_hand(str_arg)
+    print('look_child_over_big_woman')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('way')
