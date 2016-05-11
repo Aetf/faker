411b931bec5a201046bb0aa45680ea642b67c06f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 10:12:43 -0400

    "add: look super/tell_big_man_with_good_point.py, super/big_world/old_time_and_work/tell_own_fact.py, super/young_week/see_part/life.py or super/give_high_woman/government_and_place/good_time_and_part.py about good government"

---
+++ super/big_world/old_time_and_work/tell_own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_man_in_group(str_arg):
+    own_eye_and_point(str_arg)
+    print('think_same_part')
+
+def own_eye_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_man_in_group('come_group')
---
+++ super/give_high_woman/government_and_place/good_time_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_point(str_arg):
+    important_company(str_arg)
+    print('long_work')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_point('leave_long_week_of_way')
