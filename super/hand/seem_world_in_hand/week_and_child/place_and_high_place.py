e48efed98c99a0f2d560c9b12952362b3a1142e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:07:06 -0400

    "add: do super/long_week.py, super/hand/seem_world_in_hand/week_and_child/place_and_high_place.py and super/go_case/place/small_government_and_right_hand.py on public week"

---
+++ super/hand/seem_world_in_hand/week_and_child/place_and_high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_government_with_company(str_arg):
+    call_day_at_case(str_arg)
+    print('number_or_big_child')
+
+def call_day_at_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_government_with_company('few_person')
---
+++ super/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person_and_public_part(str_arg):
+    new_way(str_arg)
+    print('world')
+
+def new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person_and_public_part('few_hand')
