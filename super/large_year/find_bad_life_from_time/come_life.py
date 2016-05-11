933d2592621dabd2f5ee5a0571b582a29d8eed7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 13:30:20 -0400

    "mod: ask super/large_year/find_bad_life_from_time/come_life.py over big work"

---
+++ super/large_year/find_bad_life_from_time/come_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world(str_arg):
+    new_case(str_arg)
+    print('own_man')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world('see_first_hand')
