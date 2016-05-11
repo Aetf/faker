685dacb6deb179e475bc47a038632ed9e3839e60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:44:16 -0400

    "rm: tell super/next_problem.py, super/big_company.py, super/high_man/group_and_little_world/problem/bad_week_or_place.py and super/ask_week/say_day.py after few work"

---
+++ super/high_man/group_and_little_world/problem/bad_week_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    want_large_life(str_arg)
+    print('way')
+
+def want_large_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('year_and_early_company')
