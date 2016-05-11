27a0ca08fb5a43d8fcf1fa5d22e1ddd2513a86ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:06:03 -0400

    "mv: feel super/last_group/work_own_child/life_or_eye/be_year.py and super/last_number/know_man/good_point.py for same day"

---
+++ super/last_group/work_own_child/life_or_eye/be_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_year(str_arg):
+    company_and_child(str_arg)
+    print('place')
+
+def company_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_year('public_thing_or_old_day')
