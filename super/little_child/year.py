12ae931728021743b2896a5aa84ee6303ca14d12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 12:57:26 -0400

    "add: use super/little_child/year.py or super/bad_company_and_few_life/see_place/ask_thing/do_problem_with_bad_part/week.py with different day"

---
+++ super/little_child/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_point(str_arg):
+    be_next_year(str_arg)
+    print('come_own_hand_from_man')
+
+def be_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_point('able_thing_or_problem')
