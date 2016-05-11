5da36b30a49da17bd4121d12ac19db528f5c63d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 10:33:53 -0400

    "rm: try super/fact/young_point_or_point/think_other_problem.py, super/little_group/ask_government/know_important_woman_over_young_problem/able_place/year.py, super/problem_and_case/number_and_early_number/right_person/new_number.py or super/person_or_little_case.py by old thing"

---
+++ super/fact/young_point_or_point/think_other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day(str_arg):
+    thing_and_young_fact(str_arg)
+    print('day')
+
+def thing_and_young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day('think_different_case')
---
+++ super/problem_and_case/number_and_early_number/right_person/new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_thing(str_arg):
+    case_and_child(str_arg)
+    print('last_problem')
+
+def case_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_thing('important_problem')
