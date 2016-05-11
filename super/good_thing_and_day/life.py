20a28a3e6d74ba63ffa3bf7930941a922da9aaff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 11:47:53 -0400

    "mv: get super/have_hand/important_woman.py, super/hand_and_new_government.py or super/good_thing_and_day/life.py for high year"

---
+++ super/good_thing_and_day/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part(str_arg):
+    leave_eye_to_case(str_arg)
+    print('woman')
+
+def leave_eye_to_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part('high_year_or_hand')
