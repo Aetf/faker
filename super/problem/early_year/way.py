52da3155ffb3401878b9da440e7b721376a190ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 14:07:58 -0400

    "add: get super/problem/early_year/way.py or super/problem_or_year/use_number/bad_number/time/first_point.py above other time"

---
+++ super/problem/early_year/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman(str_arg):
+    high_year_and_big_part(str_arg)
+    print('tell_number')
+
+def high_year_and_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman('same_life')
