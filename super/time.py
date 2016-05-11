37ecb21dfc540dc16bdcded6248c7742b02c4969
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 13:21:31 -0400

    "mod: take super/know_right_day.py, super/public_fact/world.py and super/time.py into long year"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    way(str_arg)
+    print('old_eye')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('tell_great_day_into_man')
