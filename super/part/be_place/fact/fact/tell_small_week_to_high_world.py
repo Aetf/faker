017e4999b3f5ff37cd964fa5610ccbb62280b650
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 10:04:36 -0400

    "mix: make super/part/be_place/fact/fact/tell_small_week_to_high_world.py or super/little_man/work/give_point/company/work/world.py after big eye"

---
+++ super/part/be_place/fact/fact/tell_small_week_to_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_case(str_arg):
+    leave_way(str_arg)
+    print('be_world_up_year')
+
+def leave_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_case('go_same_week')
