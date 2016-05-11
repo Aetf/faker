c8e7b0f91ecb747c92e1da665ee812280304f811
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-11 12:56:58 -0400

    "mix: have super/thing.py of little time"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_long_problem(str_arg):
+    think_work(str_arg)
+    print('long_day')
+
+def think_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_long_problem('man')
