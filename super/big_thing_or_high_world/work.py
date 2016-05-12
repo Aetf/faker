a1340d82645e90f002a1c199c1be439e8abb5f7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:35:53 -0400

    "mv: try super/big_thing_or_high_world/work.py of old fact"

---
+++ super/big_thing_or_high_world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_problem(str_arg):
+    world(str_arg)
+    print('fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_problem('right_thing')
