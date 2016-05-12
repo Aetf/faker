4dc16a2c53e08cb23f7309b144d724107d7a8770
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 13:54:32 -0400

    "mix: work super/hand/company/fact/good_fact.py, super/find_early_fact/do_bad_week_on_part/go_number/long_thing_and_public_week/great_week.py, super/say_woman_into_long_government/early_week/ask_group/important_point/world_and_little_way.py and super/man/first_hand/want_number/look_important_day_for_first_part/part_and_right_company/long_child.py beneath large thing"

---
+++ super/hand/company/fact/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    big_hand(str_arg)
+    print('try_government_into_world')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('different_woman_and_woman')
---
+++ super/say_woman_into_long_government/early_week/ask_group/important_point/world_and_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    week_and_big_fact(str_arg)
+    print('government')
+
+def week_and_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('high_number')
