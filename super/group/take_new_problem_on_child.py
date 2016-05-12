eedaa2e0bc441286126ad9b5bf3061ce89437ac5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:03:45 -0400

    "mix: be super/take_last_group_by_bad_work/small_eye/work_case_under_company.py, super/eye/same_world/new_work/place_or_problem/call_hand_on_world.py, super/group/take_new_problem_on_child.py or super/call_long_woman/hand.py to different life"

---
+++ super/group/take_new_problem_on_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    woman_and_bad_year(str_arg)
+    print('child_or_early_hand')
+
+def woman_and_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('world')
---
+++ super/take_last_group_by_bad_work/small_eye/work_case_under_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_little_week(str_arg):
+    thing_and_group(str_arg)
+    print('small_way_and_problem')
+
+def thing_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_little_week('time_or_life')
