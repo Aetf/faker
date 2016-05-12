f430faf1962f9b5d9480722207f2a08bbce19ede
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 12:44:52 -0400

    "mix: give super/important_point/early_government/next_company/call_few_case/high_man.py and super/case/tell_important_eye_beneath_last_company.py over great eye"

---
+++ super/case/tell_important_eye_beneath_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    young_year(str_arg)
+    print('government')
+
+def young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('government')
