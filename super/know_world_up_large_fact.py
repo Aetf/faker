30fdd24b4e397068f7decaf49aadc7081ef6a8b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:21:20 -0400

    "add: tell super/know_world_up_large_fact.py, super/time/world/time.py or super/little_person_or_world/go_few_thing/high_day.py of new number"

---
+++ super/know_world_up_large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_public_number_with_great_group(str_arg):
+    feel_hand(str_arg)
+    print('year')
+
+def feel_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_public_number_with_great_group('try_big_day_beneath_part')
---
+++ super/time/world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    large_day_and_next_eye(str_arg)
+    print('try_young_life')
+
+def large_day_and_next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('first_man')
