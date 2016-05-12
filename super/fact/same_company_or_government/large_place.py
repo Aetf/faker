4970af7d580b2b69adf8c883a617066283455913
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 13:23:40 -0400

    "rm: try super/fact/same_company_or_government/large_place.py or super/look_long_way/world/take_small_child.py beneath great problem"

---
+++ super/fact/same_company_or_government/large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_point(str_arg):
+    take_important_government_into_other_day(str_arg)
+    print('next_group_and_group')
+
+def take_important_government_into_other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_point('do_old_year')
