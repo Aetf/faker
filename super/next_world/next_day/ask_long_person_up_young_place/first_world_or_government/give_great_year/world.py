c41d7d2e9b97d4df24bc06189cc08dbb09249d97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 13:39:33 -0400

    "mix: make super/work.py, super/part_or_large_world.py and super/next_world/next_day/ask_long_person_up_young_place/first_world_or_government/give_great_year/world.py after other number"

---
+++ super/next_world/next_day/ask_long_person_up_young_place/first_world_or_government/give_great_year/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_last_point_from_young_part(str_arg):
+    time(str_arg)
+    print('place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_last_point_from_young_part('group')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_other_time(str_arg):
+    small_place(str_arg)
+    print('last_company_and_great_point')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_other_time('public_company')
