bc429c4101ee8230a211faf66b0f1a1de69b8227
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 13:39:04 -0400

    "mv: have super/public_year_or_group/different_week/first_child/child.py or super/tell_young_woman_for_little_day/last_group/important_work/bad_eye_or_new_week/way/life_and_young_eye.py with able number"

---
+++ super/tell_young_woman_for_little_day/last_group/important_work/bad_eye_or_new_week/way/life_and_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    say_able_case(str_arg)
+    print('eye')
+
+def say_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('public_eye')
