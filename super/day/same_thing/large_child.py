68ec834fd9f636e25dbad56108a6214d35700ab2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 10:02:38 -0400

    "mv: make super/get_company/same_company/few_way_and_time/high_fact/high_case.py and super/day/same_thing/large_child.py at few point"

---
+++ super/day/same_thing/large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing_and_few_way(str_arg):
+    bad_life(str_arg)
+    print('part')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing_and_few_way('small_case')
