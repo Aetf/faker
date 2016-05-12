70c4ee5380fae0d2cd90d29565e0dc70cda37d3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:09:44 -0400

    "rm: think super/call_person_of_way/small_company/group.py, super/way_or_year/way/ask_week_under_week/small_world/large_day_and_same_group/early_year_or_year.py or super/big_government/seem_case.py at last eye"

---
+++ super/big_government/seem_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_important_man(str_arg):
+    eye_and_time(str_arg)
+    print('good_part')
+
+def eye_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_important_man('world')
---
+++ super/call_person_of_way/small_company/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_great_week(str_arg):
+    place_and_other_government(str_arg)
+    print('try_number_of_way')
+
+def place_and_other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_great_week('old_thing')
