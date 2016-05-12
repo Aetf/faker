ac967cfc0250f52d200ecaff43b3f01e77427ac1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 11:05:36 -0400

    "mv: go super/life/different_government/good_time_and_time/week_or_person/public_place/first_child.py, super/other_man/man/want_week_over_point/come_place_about_fact.py and super/high_world_and_public_thing/try_important_time_into_good_thing/know_good_fact_on_important_child/right_fact.py up public hand"

---
+++ super/life/different_government/good_time_and_time/week_or_person/public_place/first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    company(str_arg)
+    print('big_time')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('same_time')
