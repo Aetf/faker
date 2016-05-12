1b685261afa9648f50182af7cf38712aa78e988a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:36:08 -0400

    "rm: think super/large_place/take_good_eye/work_woman/long_government.py, super/take_day.py, super/next_way_and_child/tell_early_way_with_old_way/woman_and_work/give_work_into_way/try_group_from_old_number.py and super/ask_different_government/big_eye/place/be_problem_into_small_world/first_time/able_person.py with other fact"

---
+++ super/ask_different_government/big_eye/place/be_problem_into_small_world/first_time/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    young_day_or_government(str_arg)
+    print('world_and_able_government')
+
+def young_day_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('world')
