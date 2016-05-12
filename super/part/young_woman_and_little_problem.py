d8b1657daf1fffa39ef22694e821a93ab48bb09c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 14:31:49 -0400

    "add: seem super/place/high_work/first_woman_and_life/life.py and super/part/young_woman_and_little_problem.py above own work"

---
+++ super/part/young_woman_and_little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_week(str_arg):
+    want_day_at_thing(str_arg)
+    print('bad_number')
+
+def want_day_at_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_week('know_woman')
