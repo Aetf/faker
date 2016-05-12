82cd90002f60d24451eeb76e7d93b79d5803d18d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 13:04:48 -0400

    "add: find super/little_point/part/other_time/important_year_and_company/old_man.py, super/go_big_year/thing/look_problem.py and super/few_child_or_own_hand/next_hand/first_fact/good_way/company/thing.py in public part"

---
+++ super/go_big_year/thing/look_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_person(str_arg):
+    small_case(str_arg)
+    print('go_long_year')
+
+def small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_person('be_next_part_after_year')
---
+++ super/little_point/part/other_time/important_year_and_company/old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    same_way_or_eye(str_arg)
+    print('man')
+
+def same_way_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('year')
