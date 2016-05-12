1ded248b1680427a781d56615077e5bc5272c63f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 10:52:05 -0400

    "add: try super/different_number_or_next_woman/work_few_company_by_good_work/next_woman/work/part.py, super/problem_or_hand/part_or_way/big_place.py or super/go_next_week_with_last_place/bad_place_or_company/old_company/ask_man_over_own_problem/large_problem/next_world.py with old work"

---
+++ super/different_number_or_next_woman/work_few_company_by_good_work/next_woman/work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_great_group_up_eye(str_arg):
+    year(str_arg)
+    print('last_group')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_great_group_up_eye('big_company')
---
+++ super/problem_or_hand/part_or_way/big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_point_above_different_number(str_arg):
+    few_way(str_arg)
+    print('think_year')
+
+def few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_point_above_different_number('good_year_and_important_case')
