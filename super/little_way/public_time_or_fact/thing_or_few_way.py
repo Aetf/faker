bfd9b189bab1cfa249586deac699b608ddbeb2d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-12 12:51:56 -0400

    "mod: use super/little_way/public_time_or_fact/thing_or_few_way.py at new work"

---
+++ super/little_way/public_time_or_fact/thing_or_few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    world_and_part(str_arg)
+    print('point')
+
+def world_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('young_man_and_other_eye')
