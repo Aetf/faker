f5b17caa26c4a2afb4878b6a5be27ad90760ccda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:12:38 -0400

    "mix: do super/day_and_different_time/woman/first_hand/day.py, super/government.py and super/big_eye_or_work/day/week_or_case/have_group_beneath_day.py with important work"

---
+++ super/big_eye_or_work/day/week_or_case/have_group_beneath_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_new_number_beneath_few_point(str_arg):
+    work(str_arg)
+    print('first_company_and_large_case')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_new_number_beneath_few_point('different_year')
