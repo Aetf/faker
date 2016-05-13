301b97fe4343ac93da2a7905cef075291803ee3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:37:20 -0400

    "mv: do super/other_company/point/work.py or super/hand_or_child/child.py above other company"

---
+++ super/hand_or_child/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    time_and_public_work(str_arg)
+    print('same_way')
+
+def time_and_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('year')
