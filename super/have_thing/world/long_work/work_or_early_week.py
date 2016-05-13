2e5467a128423a476eb17b74e910ab4426ae9cab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 11:51:26 -0400

    "add: do super/have_thing/world/long_work/work_or_early_week.py in public world"

---
+++ super/have_thing/world/long_work/work_or_early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_important_point(str_arg):
+    way_or_great_company(str_arg)
+    print('say_world_with_world')
+
+def way_or_great_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_important_point('get_place_up_way')
