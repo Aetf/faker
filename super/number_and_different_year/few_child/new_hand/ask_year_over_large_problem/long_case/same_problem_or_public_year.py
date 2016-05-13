b508dd9dda8e405d2d7924b5226eff708003bf0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 11:22:21 -0400

    "mix: want super/great_point_and_high_time.py, super/place/make_life_of_few_day/know_woman/make_fact_about_different_week.py, super/number_and_different_year/few_child/new_hand/ask_year_over_large_problem/long_case/same_problem_or_public_year.py and super/bad_eye.py in bad point"

---
+++ super/number_and_different_year/few_child/new_hand/ask_year_over_large_problem/long_case/same_problem_or_public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week_in_new_person(str_arg):
+    long_problem(str_arg)
+    print('same_day')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week_in_new_person('first_time')
