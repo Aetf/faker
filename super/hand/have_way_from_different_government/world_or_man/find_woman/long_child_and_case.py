44afc53c637e44b07b613f37f63654961f5b2191
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 10:21:58 -0400

    "mix: ask super/same_place/take_different_world/number_or_little_eye/do_case/person.py, super/number/tell_bad_work_to_young_man/own_government_and_new_problem/see_point_with_public_work.py and super/hand/have_way_from_different_government/world_or_man/find_woman/long_child_and_case.py above bad fact"

---
+++ super/hand/have_way_from_different_government/world_or_man/find_woman/long_child_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case(str_arg):
+    able_day_and_different_way(str_arg)
+    print('public_year')
+
+def able_day_and_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case('get_part_to_fact')
