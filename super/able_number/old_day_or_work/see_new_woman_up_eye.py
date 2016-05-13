02561d62e48f3a01039f4bb8b057f0ba1bd242a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 13:14:57 -0400

    "mv: make super/able_number/old_day_or_work/see_new_woman_up_eye.py or super/eye_or_fact.py from new day"

---
+++ super/able_number/old_day_or_work/see_new_woman_up_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    new_way(str_arg)
+    print('first_child')
+
+def new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('man_and_fact')
---
+++ super/eye_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man(str_arg):
+    fact(str_arg)
+    print('company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man('group')
