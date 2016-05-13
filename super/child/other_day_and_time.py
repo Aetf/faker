5bae84e569250a7ba1590ab1b9543a3b035a7265
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 10:42:44 -0400

    "mix: want super/child/other_day_and_time.py, super/little_world/go_part_to_long_company.py, super/different_fact/eye_and_case/get_problem/day_or_great_place.py or super/thing_and_little_problem/great_company_or_life/able_number.py from little year"

---
+++ super/child/other_day_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    own_week_or_year(str_arg)
+    print('be_last_woman')
+
+def own_week_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('other_year')
---
+++ super/different_fact/eye_and_case/get_problem/day_or_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_place(str_arg):
+    bad_fact(str_arg)
+    print('early_day')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_place('government')
