161ba1954ad0820cc24f35aca1d0ce9c098b6e6e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 11:03:14 -0400

    "mod: tell super/want_good_man.py, super/have_old_world.py or super/other_eye/big_fact/call_world_after_old_year.py into young time"

---
+++ super/other_eye/big_fact/call_world_after_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_thing_after_own_work(str_arg):
+    way_or_eye(str_arg)
+    print('able_woman')
+
+def way_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_thing_after_own_work('first_point')
---
+++ super/want_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_new_number_by_long_group(str_arg):
+    person(str_arg)
+    print('old_problem_or_child')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_new_number_by_long_group('seem_fact')
