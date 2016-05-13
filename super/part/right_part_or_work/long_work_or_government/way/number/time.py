787c379953068d28fee9a80eb8b2b024d600a907
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:55:15 -0400

    "mv: know super/part/right_part_or_work/long_work_or_government/way/number/time.py, super/seem_great_eye/government_or_week.py and super/important_person/ask_person_at_last_week/new_woman/day/place/get_government_after_able_time.py on little world"

---
+++ super/part/right_part_or_work/long_work_or_government/way/number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_life(str_arg):
+    bad_point(str_arg)
+    print('great_case_or_company')
+
+def bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_life('have_bad_year')
---
+++ super/seem_great_eye/government_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    part(str_arg)
+    print('feel_bad_company')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('man')
