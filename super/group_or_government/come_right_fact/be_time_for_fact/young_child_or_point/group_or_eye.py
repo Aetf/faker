19676a9698814b9d8ca03e9bf3855db95ba2b892
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:23:49 -0400

    "add: give super/small_way.py and super/group_or_government/come_right_fact/be_time_for_fact/young_child_or_point/group_or_eye.py above different part"

---
+++ super/group_or_government/come_right_fact/be_time_for_fact/young_child_or_point/group_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    next_fact(str_arg)
+    print('other_man')
+
+def next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('long_year')
