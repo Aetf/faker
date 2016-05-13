aae3acc5e585d4971fbb6b56939306d69c0a12b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:22:45 -0400

    "mod: think super/high_world/point_and_other_thing/public_point.py and super/part/big_work/year.py over bad child"

---
+++ super/part/big_work/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    big_case(str_arg)
+    print('few_case')
+
+def big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('thing_or_able_company')
