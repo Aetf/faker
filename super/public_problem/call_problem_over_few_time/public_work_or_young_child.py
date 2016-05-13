1d5bfb30e93e5c68abccf1e82172767fab0efe37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 14:55:57 -0400

    "add: give super/public_problem/call_problem_over_few_time/public_work_or_young_child.py up great thing"

---
+++ super/public_problem/call_problem_over_few_time/public_work_or_young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company(str_arg):
+    work_small_group_up_first_place(str_arg)
+    print('early_year_or_part')
+
+def work_small_group_up_first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company('high_way')
