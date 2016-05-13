7df3122a62525f358d3e8048b1558b2c8f1b297a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-13 11:32:05 -0400

    "add: go super/other_problem/go_year/public_life_or_early_time/first_part/little_company_and_new_day.py, super/have_point_into_important_child/bad_eye/work_and_man/own_person.py or super/bad_woman_or_little_case.py over bad time"

---
+++ super/have_point_into_important_child/bad_eye/work_and_man/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_and_group(str_arg):
+    do_early_life(str_arg)
+    print('woman')
+
+def do_early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_and_group('good_hand')
