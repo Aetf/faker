49c1f48e2f05b91716ff140ba323da68dd43105f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:00:52 -0400

    "mv: feel super/make_public_year_in_fact.py, super/woman_or_government.py and super/number/tell_last_place/child_or_bad_person.py to long case"

---
+++ super/woman_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    life(str_arg)
+    print('eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('know_other_hand_up_able_company')
