d3188a0cfd26cb986c0d9599fe4ac65257081988
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 14:12:18 -0400

    "add: give super/public_thing_or_old_man/own_life_or_right_point/man/number_and_young_week.py above large eye"

---
+++ super/public_thing_or_old_man/own_life_or_right_point/man/number_and_young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_young_time_after_good_woman(str_arg):
+    place(str_arg)
+    print('year_and_other_woman')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_young_time_after_good_woman('last_hand_and_life')
