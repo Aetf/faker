f77decbcc1b9cdc9dbe1a34afd2940f5fe1adecc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 14:09:40 -0400

    "mix: give super/young_way/good_child.py at different company"

---
+++ super/young_way/good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    thing_and_new_place(str_arg)
+    print('place_and_few_part')
+
+def thing_and_new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('leave_woman')
