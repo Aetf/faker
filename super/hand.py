c00b80e9ccfaf2b4f7d05565adce41b23b632d6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:04:32 -0400

    "mv: see super/hand.py in little work"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_big_problem(str_arg):
+    give_group(str_arg)
+    print('first_government')
+
+def give_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_big_problem('think_thing_with_day')
