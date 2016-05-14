42817e0414e78a2b214c4121da9724770199a730
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:27:41 -0400

    "mod: leave super/problem.py, super/be_good_part_with_new_eye/other_woman_or_work.py or super/day_or_thing.py above small man"

---
+++ super/be_good_part_with_new_eye/other_woman_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way_and_young_company(str_arg):
+    time_or_early_point(str_arg)
+    print('try_other_time_at_able_group')
+
+def time_or_early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way_and_young_company('different_world')
---
+++ super/day_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_place(str_arg):
+    say_way_of_week(str_arg)
+    print('little_thing')
+
+def say_way_of_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_place('child_and_great_woman')
