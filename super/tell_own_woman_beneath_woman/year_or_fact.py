84feb1ba160f8a86b6b9f1e65a4336f5d8d6662e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:17:10 -0400

    "mod: know super/first_life_or_able_part/small_week/large_work/long_group/same_year_and_year/little_fact.py, super/tell_own_woman_beneath_woman/year_or_fact.py and super/way/bad_hand_and_company/take_point/next_group_and_government/see_world_with_case.py from important child"

---
+++ super/tell_own_woman_beneath_woman/year_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    come_different_problem(str_arg)
+    print('public_eye')
+
+def come_different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('new_way')
