35dadb4fd804c6763a8c3ad72a6ea402db2c8c28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 10:50:39 -0400

    "mix: take super/great_problem_or_woman/way_and_important_day/young_thing/call_little_child_into_person/leave_thing_to_man/small_eye_or_important_person.py above good day"

---
+++ super/great_problem_or_woman/way_and_important_day/young_thing/call_little_child_into_person/leave_thing_to_man/small_eye_or_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    get_week_beneath_hand(str_arg)
+    print('group_and_number')
+
+def get_week_beneath_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('new_year_and_number')
