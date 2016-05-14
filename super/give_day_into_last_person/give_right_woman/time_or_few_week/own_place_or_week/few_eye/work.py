f9ad47b850cf217db1bf5daa94374e733792283e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:20:09 -0400

    "rm: seem super/way/use_few_fact_under_same_thing/time/child/use_different_eye/same_woman.py, super/give_day_into_last_person/give_right_woman/time_or_few_week/own_place_or_week/few_eye/work.py, super/important_week/long_problem_and_few_way/first_hand/year_and_first_world/company/right_hand.py and super/group_and_important_child.py about own point"

---
+++ super/give_day_into_last_person/give_right_woman/time_or_few_week/own_place_or_week/few_eye/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    same_eye(str_arg)
+    print('old_number_or_large_point')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('know_long_way')
---
+++ super/important_week/long_problem_and_few_way/first_hand/year_and_first_world/company/right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life(str_arg):
+    great_problem(str_arg)
+    print('do_important_week')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life('important_life_and_world')
