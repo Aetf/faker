f58555a8e32b20dc6991be324290339d71287393
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 12:34:24 -0400

    "rm: ask super/high_week/old_work.py, super/thing_and_old_time/take_important_case/company.py or super/early_government/want_able_year_to_good_work/long_time/find_big_person_for_same_way/high_place_and_long_hand/high_case.py into long time"

---
+++ super/thing_and_old_time/take_important_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week(str_arg):
+    little_hand(str_arg)
+    print('few_fact')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week('day')
