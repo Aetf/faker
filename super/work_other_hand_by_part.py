42d4fa4c8d75536617e269dc4ca41c1d39d44a21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:31:27 -0400

    "mod: go super/work_other_hand_by_part.py or super/last_problem/fact/work_and_child/government_and_same_way/child_or_important_fact.py to good year"

---
+++ super/work_other_hand_by_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part_and_able_time(str_arg):
+    look_woman(str_arg)
+    print('work_young_group')
+
+def look_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part_and_able_time('hand_or_different_week')
