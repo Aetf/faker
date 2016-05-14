9a0fb743e820b0f29af8a8ea6d69d341f1269d66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:42:58 -0400

    "mv: feel super/high_way_and_thing/world/see_week_by_world/person_or_case.py in large point"

---
+++ super/high_way_and_thing/world/see_week_by_world/person_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government_and_next_point(str_arg):
+    young_fact(str_arg)
+    print('public_year_and_different_fact')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government_and_next_point('make_new_life')
