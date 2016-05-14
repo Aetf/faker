0342eaf4a015c6bc4355f0cf721f7fd3bbf11538
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:46:10 -0400

    "rm: think super/point/own_year.py for old government"

---
+++ super/point/own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_fact(str_arg):
+    large_work_or_life(str_arg)
+    print('year')
+
+def large_work_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_fact('work_or_point')
