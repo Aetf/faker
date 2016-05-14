fd8b9bb09299ad0f80e95d4eaa3e54c4ea7210b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:27:10 -0400

    "add: go super/own_time/week_and_part/good_life_or_day/hand_or_work/problem_or_company/good_week.py, super/make_new_number_in_hand/young_man_or_first_fact/point/problem/say_woman.py, super/look_own_time_up_life/tell_case_from_large_day/large_way_or_own_work.py or super/big_work/right_part/work/bad_place.py to large day"

---
+++ super/make_new_number_in_hand/young_man_or_first_fact/point/problem/say_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_big_thing(str_arg):
+    say_company_by_eye(str_arg)
+    print('come_important_work')
+
+def say_company_by_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_big_thing('little_fact')
