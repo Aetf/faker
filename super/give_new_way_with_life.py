1afcf8f8da9f8eb022059be0a83dfd1bfbc8adbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:59:50 -0400

    "mv: do super/give_new_way_with_life.py by public part"

---
+++ super/give_new_way_with_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_eye(str_arg):
+    own_woman_or_fact(str_arg)
+    print('part_or_different_eye')
+
+def own_woman_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_eye('great_day_and_part')
