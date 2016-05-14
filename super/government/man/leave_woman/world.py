39ab13b2f7bf46da1b18c46a58997b5586e0b2c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:31:41 -0400

    "mix: try super/government/man/leave_woman/world.py and super/think_little_case/last_government/eye/want_last_week_over_place/want_large_part_at_life/company.py into new hand"

---
+++ super/government/man/leave_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week(str_arg):
+    public_part(str_arg)
+    print('point')
+
+def public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week('go_part_with_child')
