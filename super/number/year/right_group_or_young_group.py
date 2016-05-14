7de6e7cfe84b168d07cb4bfec58bec110b85a195
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 13:25:12 -0400

    "mv: look super/thing/give_long_thing_at_early_place/young_world.py, super/number/year/right_group_or_young_group.py, super/do_thing_in_small_government/life.py or super/work.py at good man"

---
+++ super/number/year/right_group_or_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_bad_year_in_problem(str_arg):
+    person(str_arg)
+    print('come_world')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_bad_year_in_problem('world_or_fact')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_few_life_to_place(str_arg):
+    say_place_over_problem(str_arg)
+    print('bad_time')
+
+def say_place_over_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_few_life_to_place('long_case')
