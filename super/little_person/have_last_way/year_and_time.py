ec72b3c205c6cb481a6303ff077ed3b7437501ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:57:30 -0400

    "mv: look super/group.py, super/public_case_and_last_child/child.py or super/little_person/have_last_way/year_and_time.py up other fact"

---
+++ super/little_person/have_last_way/year_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    important_woman(str_arg)
+    print('number')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('different_child')
