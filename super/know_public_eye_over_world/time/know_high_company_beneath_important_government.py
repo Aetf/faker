8ec921038200a6ad944e245e5962d792e63888d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 11:47:00 -0400

    "add: give super/know_public_eye_over_world/time/know_high_company_beneath_important_government.py, super/number_or_other_week.py or super/last_world/new_fact/ask_large_government/go_high_hand_with_good_place/problem/time.py from same fact"

---
+++ super/know_public_eye_over_world/time/know_high_company_beneath_important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_part(str_arg):
+    new_government(str_arg)
+    print('new_fact')
+
+def new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_part('ask_able_hand_up_bad_part')
---
+++ super/last_world/new_fact/ask_large_government/go_high_hand_with_good_place/problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_important_year_into_work(str_arg):
+    different_child_or_life(str_arg)
+    print('point_or_few_life')
+
+def different_child_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_important_year_into_work('public_child')
