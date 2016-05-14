3212276e8682ef0818e2f56053bd4c596fd65e8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-14 14:39:46 -0400

    "add: leave super/group.py, super/give_point/case.py, super/woman/important_point/want_child_above_other_group.py or super/large_case/case/government/point/go_early_part.py about little fact"

---
+++ super/give_point/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_place(str_arg):
+    long_woman_and_year(str_arg)
+    print('early_world')
+
+def long_woman_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_place('man')
---
+++ super/large_case/case/government/point/go_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_time_for_own_person(str_arg):
+    case(str_arg)
+    print('man')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_time_for_own_person('part')
