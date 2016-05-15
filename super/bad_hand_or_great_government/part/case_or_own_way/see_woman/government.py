91e14c4db8f04079587c1094869ca7404f5dd60e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 13:13:25 -0400

    "mod: see super/bad_hand_or_great_government/part/case_or_own_way/see_woman/government.py or super/own_place_and_fact/long_time/woman/different_man.py by next person"

---
+++ super/bad_hand_or_great_government/part/case_or_own_way/see_woman/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work_and_life(str_arg):
+    place(str_arg)
+    print('call_same_man_into_small_problem')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work_and_life('time')
