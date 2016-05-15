c3d1ef7981596ccce02c815cfb2186d7adb5715b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 11:52:16 -0400

    "rm: seem super/use_good_company/want_early_hand.py, super/bad_problem/go_day.py or super/case/first_week/way/ask_part_from_place.py from long government"

---
+++ super/bad_problem/go_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_or_able_thing(str_arg):
+    go_woman_at_year(str_arg)
+    print('seem_way_for_problem')
+
+def go_woman_at_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_or_able_thing('work_own_group')
---
+++ super/use_good_company/want_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_day_and_bad_number(str_arg):
+    different_group_and_good_company(str_arg)
+    print('high_woman')
+
+def different_group_and_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_day_and_bad_number('see_great_week')
