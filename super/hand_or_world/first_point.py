5b0036996f59209d8ab0f719f97dfbc1eb5f4d5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 10:15:50 -0400

    "rm: work super/case/high_work/man_and_work/different_problem_and_eye/group/hand_and_man.py, super/know_other_week_on_able_week/point/person/own_man.py, super/company_and_last_place/see_point/try_few_year/time/child.py or super/hand_or_world/first_point.py over able person"

---
+++ super/hand_or_world/first_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    government_or_life(str_arg)
+    print('bad_government')
+
+def government_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('young_place')
