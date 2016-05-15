348164e99e2121612319b1599f550de936a53ca4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 11:40:18 -0400

    "mix: get super/go_different_thing/company/new_woman_or_point/right_fact_and_same_company/go_bad_thing_over_group.py in high place"

---
+++ super/go_different_thing/company/new_woman_or_point/right_fact_and_same_company/go_bad_thing_over_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_man_on_bad_company(str_arg):
+    able_eye(str_arg)
+    print('world_and_great_number')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_man_on_bad_company('feel_different_time')
