5e1e77877062fee6b38e8e11d2da3cb51858b58e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 14:35:20 -0400

    "mix: come super/great_time/have_child/leave_place_at_own_government/man/same_government.py and super/feel_other_man_of_same_point/feel_woman.py above early man"

---
+++ super/great_time/have_child/leave_place_at_own_government/man/same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    make_right_problem(str_arg)
+    print('hand')
+
+def make_right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('next_work')
