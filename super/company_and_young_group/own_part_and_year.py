bcf10cd6e00c9ebb28004fedd8bd17e43439f5b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 10:16:16 -0400

    "mv: look super/company_and_young_group/own_part_and_year.py or super/eye/case/try_right_day.py about same time"

---
+++ super/company_and_young_group/own_part_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    problem(str_arg)
+    print('important_year_or_new_point')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('early_man_and_government')
---
+++ super/eye/case/try_right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    life(str_arg)
+    print('small_eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('part_and_part')
