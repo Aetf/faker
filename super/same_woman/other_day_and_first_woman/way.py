cbc48839833baff8a06974f151280c04880f5f45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 11:28:08 -0400

    "add: say super/new_life_and_high_company/early_day_or_long_group.py, super/same_woman/other_day_and_first_woman/way.py or super/case/look_part_beneath_little_child.py under bad day"

---
+++ super/same_woman/other_day_and_first_woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_child(str_arg):
+    point_and_right_problem(str_arg)
+    print('use_own_part_up_small_group')
+
+def point_and_right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_child('other_year_or_company')
