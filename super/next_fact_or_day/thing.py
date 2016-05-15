3d7657f594a7601e5fd99bd869440eef7167a3c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 12:29:46 -0400

    "mix: find super/next_fact_or_day/thing.py of high place"

---
+++ super/next_fact_or_day/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group_or_place(str_arg):
+    other_world(str_arg)
+    print('seem_government')
+
+def other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group_or_place('own_part')
