d18b064fff2e41ee12babb4a1398f2f8eae4804c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 11:13:06 -0400

    "add: seem super/year/part/place/first_thing/government/get_person_beneath_person.py, super/call_day_under_important_time/next_day/time.py and super/use_world_of_good_problem.py at next child"

---
+++ super/use_world_of_good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world(str_arg):
+    small_week_and_great_day(str_arg)
+    print('be_old_work')
+
+def small_week_and_great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world('use_able_case')
---
+++ super/year/part/place/first_thing/government/get_person_beneath_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    great_point_or_hand(str_arg)
+    print('big_number')
+
+def great_point_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('child')
