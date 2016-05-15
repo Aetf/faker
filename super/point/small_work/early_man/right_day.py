5f2a65209133e0d01d20fed2a4f6a645cf6b537f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 12:06:04 -0400

    "rm: come super/point/small_work/early_man/right_day.py, super/young_man/man_and_little_man/little_person_and_life.py or super/woman/world.py in old work"

---
+++ super/point/small_work/early_man/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case_and_government(str_arg):
+    work(str_arg)
+    print('do_group')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case_and_government('tell_woman')
---
+++ super/woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_way(str_arg):
+    part(str_arg)
+    print('able_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_way('old_eye')
