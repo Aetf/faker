1a541ca1ece64064baeb27c089b98cc4a277b48f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 12:32:25 -0400

    "add: use super/man.py and super/next_work.py from first group"

---
+++ super/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    important_government(str_arg)
+    print('old_year_or_person')
+
+def important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('other_company_or_number')
