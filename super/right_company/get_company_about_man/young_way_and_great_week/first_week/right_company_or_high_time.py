d4c07e78631a7a4aa224cfbe2fc621cfe0a5cf46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 14:58:26 -0400

    "mod: take super/government_and_week/find_place_with_early_life/bad_number_or_first_group/leave_different_place/child_or_own_part.py, super/new_world/problem/use_life/small_part/few_point/different_hand_or_early_company.py, super/find_bad_person/few_eye/problem_and_first_work/come_fact_about_way.py or super/right_company/get_company_about_man/young_way_and_great_week/first_week/right_company_or_high_time.py over last group"

---
+++ super/right_company/get_company_about_man/young_way_and_great_week/first_week/right_company_or_high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    go_high_man(str_arg)
+    print('make_world')
+
+def go_high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('woman')
