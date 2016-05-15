56e75b118ccbd615d30b46d8aa9f7e02c989347a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 13:57:33 -0400

    "mv: work super/first_case/young_government.py at public case"

---
+++ super/first_case/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_child(str_arg):
+    say_different_person_on_week(str_arg)
+    print('last_case_and_important_thing')
+
+def say_different_person_on_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_child('ask_part')
