040965dae6f44d9bb6127b5f94d47976c5445bf9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 12:19:11 -0400

    "add: feel super/other_woman.py at early part"

---
+++ super/other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_point(str_arg):
+    hand(str_arg)
+    print('woman')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_point('come_small_case_up_world')
