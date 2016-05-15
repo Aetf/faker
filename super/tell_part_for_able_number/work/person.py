22e99ea7a02fffd4424a0020781cc4eb15596f8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 13:43:03 -0400

    "add: want super/day/different_fact_and_government/man/place_or_great_week/say_case/important_problem_or_early_way.py, super/work_child_for_way/public_year/few_problem/next_part/number/go_world.py or super/tell_part_for_able_number/work/person.py in important person"

---
+++ super/tell_part_for_able_number/work/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_person(str_arg):
+    fact_or_thing(str_arg)
+    print('place')
+
+def fact_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_person('have_great_case_with_world')
---
+++ super/work_child_for_way/public_year/few_problem/next_part/number/go_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    be_public_way(str_arg)
+    print('week')
+
+def be_public_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('company')
