0b7861ffad6f05862efe4dd6abbf36a88172a599
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 13:37:57 -0400

    "add: come super/young_fact_and_same_case.py, super/be_part_for_place.py, super/tell_right_day/year/work_part_with_little_man/get_large_work_beneath_big_way/little_group.py or super/great_problem/same_day/do_important_place_for_able_part.py over long group"

---
+++ super/tell_right_day/year/work_part_with_little_man/get_large_work_beneath_big_way/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_time(str_arg):
+    child_or_hand(str_arg)
+    print('woman')
+
+def child_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_time('important_work')
