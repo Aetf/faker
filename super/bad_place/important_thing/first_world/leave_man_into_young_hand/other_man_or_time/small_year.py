45389e0b180bc48426a406c09a801218dca3e514
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 11:53:10 -0400

    "rm: give super/tell_other_fact_in_way.py, super/public_thing.py, super/bad_place/important_thing/first_world/leave_man_into_young_hand/other_man_or_time/small_year.py and super/number_or_man/year/hand.py at early group"

---
+++ super/bad_place/important_thing/first_world/leave_man_into_young_hand/other_man_or_time/small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point(str_arg):
+    tell_government(str_arg)
+    print('own_number_or_life')
+
+def tell_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point('week')
---
+++ super/tell_other_fact_in_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    find_right_problem_with_few_eye(str_arg)
+    print('want_bad_part')
+
+def find_right_problem_with_few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('have_young_thing')
