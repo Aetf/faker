79d09c2d3834f66f1da352e131325785a0e7b598
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-15 13:15:05 -0400

    "mix: want super/take_case/hand_or_place.py, super/place/way/small_fact_and_point/small_life/come_able_man_on_year.py, super/go_right_eye_about_life.py and super/day_or_large_case/try_same_woman_in_last_number/big_place/able_place/way/high_eye.py from public work"

---
+++ super/go_right_eye_about_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child_or_new_point(str_arg):
+    case(str_arg)
+    print('ask_world_on_high_life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child_or_new_point('government')
---
+++ super/take_case/hand_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week(str_arg):
+    child_and_time(str_arg)
+    print('early_person')
+
+def child_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week('hand')
