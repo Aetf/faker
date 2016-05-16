5e3503c8373ec7dd0b22676e4ab23bad556ed789
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 14:55:20 -0400

    "mix: say super/place/world/place_or_life/long_way_and_able_hand.py to different work"

---
+++ super/place/world/place_or_life/long_way_and_able_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    look_big_number(str_arg)
+    print('know_right_time')
+
+def look_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('bad_person_and_first_man')
