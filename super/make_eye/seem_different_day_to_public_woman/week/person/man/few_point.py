57a481e8ef41ce544b852511b71d6d49eb0c7564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 14:32:38 -0400

    "mv: make super/make_eye/seem_different_day_to_public_woman/week/person/man/few_point.py, super/see_different_child_at_week.py, super/last_point_and_way/long_way/person_or_big_year/part/company.py or super/place_or_public_company/old_person.py beneath little time"

---
+++ super/make_eye/seem_different_day_to_public_woman/week/person/man/few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_man_in_place(str_arg):
+    public_case(str_arg)
+    print('next_woman')
+
+def public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_man_in_place('time')
---
+++ super/place_or_public_company/old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_week(str_arg):
+    man(str_arg)
+    print('company_and_same_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_week('fact')
