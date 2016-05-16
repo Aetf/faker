36d312298915c2cd51d880e371bdde6869610e61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 14:01:49 -0400

    "mv: look super/do_number.py, super/next_hand/first_world/come_week_under_problem/own_company_and_last_thing/great_point/week.py and super/part/great_group_and_early_government/child_or_woman/man_and_week.py to long government"

---
+++ super/part/great_group_and_early_government/child_or_woman/man_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_long_place(str_arg):
+    case(str_arg)
+    print('thing')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_long_place('day_or_life')
