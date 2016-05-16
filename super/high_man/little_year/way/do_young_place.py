81ee9ea30d49ecd3e4ffbac3baedc51cf9e83568
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 11:58:33 -0400

    "mv: tell super/want_young_time_of_part/woman/high_way/give_other_life_on_number/life/time.py, super/high_man/little_year/way/do_young_place.py or super/day/fact_and_long_woman/case_and_point.py from large hand"

---
+++ super/high_man/little_year/way/do_young_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_right_work(str_arg):
+    case_or_week(str_arg)
+    print('last_way')
+
+def case_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_right_work('use_own_time_in_long_fact')
