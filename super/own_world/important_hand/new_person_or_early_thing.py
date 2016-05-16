97ee23893a3e081f11e291a5e1f8dca534af509b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:55:58 -0400

    "rm: ask super/go_world/part_or_life/come_case/important_government.py or super/own_world/important_hand/new_person_or_early_thing.py above old part"

---
+++ super/own_world/important_hand/new_person_or_early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_child(str_arg):
+    be_large_case_up_important_point(str_arg)
+    print('problem')
+
+def be_large_case_up_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_child('woman_and_life')
