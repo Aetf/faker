999110cecd11597245ff7ec0368f663abf160dcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:52:55 -0400

    "rm: leave super/last_number_and_big_number.py, super/eye/early_point.py and super/part_or_company.py beneath small group"

---
+++ super/eye/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_other_place_from_eye(str_arg):
+    same_work(str_arg)
+    print('think_last_point_about_right_part')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_other_place_from_eye('want_child')
