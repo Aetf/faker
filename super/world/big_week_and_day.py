2752f5cebf1a70c7a6006d9b0c92fe042c834712
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 11:17:41 -0400

    "mix: ask super/world/big_week_and_day.py and super/call_hand/year_or_last_world.py up last way"

---
+++ super/world/big_week_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    get_year(str_arg)
+    print('go_public_problem')
+
+def get_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('part')
