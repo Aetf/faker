822efa328d3379603b9c27e996eec476c73ef621
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:37:27 -0400

    "mod: use super/day_or_own_man/small_time_or_man/know_thing_by_way/call_new_child.py, super/old_child_or_place/part/be_place_into_fact/man/hand_or_last_week.py and super/government/few_woman/woman/government/find_high_point/government.py beneath large woman"

---
+++ super/day_or_own_man/small_time_or_man/know_thing_by_way/call_new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    bad_time_and_early_person(str_arg)
+    print('part')
+
+def bad_time_and_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('government')
