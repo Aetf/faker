0f0536e1d3c0b9f83d2361f6605ab00ef585852a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:11:57 -0400

    "add: get super/small_group/own_company/fact/seem_man/great_time.py, super/little_number_or_bad_time/bad_woman/know_old_thing_above_other_case/great_man_and_part/want_old_hand_with_next_place/go_company_in_high_thing.py, super/get_man.py or super/world/few_day_and_woman.py under small year"

---
+++ super/little_number_or_bad_time/bad_woman/know_old_thing_above_other_case/great_man_and_part/want_old_hand_with_next_place/go_company_in_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_person(str_arg):
+    long_work_or_government(str_arg)
+    print('be_first_life_in_day')
+
+def long_work_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_person('point_and_last_point')
