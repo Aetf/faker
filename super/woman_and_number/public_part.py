35325f55d811b1a76854ed6e7a1ed8faee17392d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 13:51:32 -0400

    "mix: feel super/woman_and_number/public_part.py, super/work_or_right_child/want_year_above_government/number/year_and_different_time/leave_big_way.py, super/think_right_life/public_person/old_government/case_or_child/look_way/small_part_and_time.py and super/bad_group_and_first_company/say_point/life/company.py to same eye"

---
+++ super/woman_and_number/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_right_child_above_problem(str_arg):
+    hand(str_arg)
+    print('own_woman')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_right_child_above_problem('know_place')
