11506d843d09c4ebc7b1b98a18ae320757e9f2a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 10:31:40 -0400

    "rm: give super/able_year_and_group/early_person_or_man.py, super/week/company/child/man_and_world/young_fact.py or super/early_company_or_fact.py above own number"

---
+++ super/able_year_and_group/early_person_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person_or_problem(str_arg):
+    government(str_arg)
+    print('hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person_or_problem('take_group_above_public_week')
---
+++ super/early_company_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    want_time(str_arg)
+    print('fact')
+
+def want_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('fact')
