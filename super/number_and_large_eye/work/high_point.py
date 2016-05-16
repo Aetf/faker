3d9e5b04279e9188e3ff2059bda1585edc825368
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 10:00:31 -0400

    "rm: do super/other_hand/day/be_eye_under_eye/make_number/high_way_or_day.py, super/number_and_large_eye/work/high_point.py or super/high_part.py above next case"

---
+++ super/number_and_large_eye/work/high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    week(str_arg)
+    print('take_great_place_with_child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('place')
---
+++ super/other_hand/day/be_eye_under_eye/make_number/high_way_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_work(str_arg):
+    first_hand(str_arg)
+    print('place')
+
+def first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_work('see_different_life')
