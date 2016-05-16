f1b9ba536763a05aabb00ce713c7c3be9661493e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 11:53:35 -0400

    "add: ask super/own_point_and_week/man_or_place/group/able_world/work.py and super/want_old_world_above_able_company.py under other point"

---
+++ super/own_point_and_week/man_or_place/group/able_world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point(str_arg):
+    high_hand(str_arg)
+    print('give_problem')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point('bad_fact')
---
+++ super/want_old_world_above_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_important_day(str_arg):
+    life(str_arg)
+    print('public_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_important_day('big_hand_and_high_child')
