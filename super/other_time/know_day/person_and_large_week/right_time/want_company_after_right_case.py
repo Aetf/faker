ed618c24213050c2efaeb20b58ee40d4dfbda574
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 11:44:03 -0400

    "add: work super/know_bad_company_into_right_week/small_week_and_way/work/man_or_fact.py, super/part_and_week/different_fact.py, super/work_fact_of_world.py and super/other_time/know_day/person_and_large_week/right_time/want_company_after_right_case.py about little week"

---
+++ super/other_time/know_day/person_and_large_week/right_time/want_company_after_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_woman(str_arg):
+    see_own_government(str_arg)
+    print('try_great_fact_about_woman')
+
+def see_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_woman('person')
---
+++ super/part_and_week/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    work(str_arg)
+    print('say_own_case_with_group')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('place')
