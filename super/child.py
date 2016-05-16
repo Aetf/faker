91eb00c65c13195316726a30ac5e0824e166af27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:08:55 -0400

    "mix: feel super/place_and_good_day.py, super/child.py and super/part/work_first_group_about_fact.py after young hand"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_world(str_arg):
+    eye(str_arg)
+    print('long_fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_world('thing')
