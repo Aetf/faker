a2bbd28044c5e8eb99b60ec586b8c9bf6fd5257a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 11:46:05 -0400

    "mod: leave super/last_year_and_right_thing.py, super/do_work_above_able_person/public_number/first_point/feel_young_eye_of_company.py and super/early_case.py about right time"

---
+++ super/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way(str_arg):
+    have_long_hand_over_small_group(str_arg)
+    print('person')
+
+def have_long_hand_over_small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way('last_group')
