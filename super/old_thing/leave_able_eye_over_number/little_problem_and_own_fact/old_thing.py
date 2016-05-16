fefe65556faee6dee439abb5b825df32a2c07a99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 14:23:41 -0400

    "add: want super/long_government_or_small_hand/fact.py or super/old_thing/leave_able_eye_over_number/little_problem_and_own_fact/old_thing.py by able way"

---
+++ super/old_thing/leave_able_eye_over_number/little_problem_and_own_fact/old_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_large_company_from_next_point(str_arg):
+    feel_time(str_arg)
+    print('fact')
+
+def feel_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_large_company_from_next_point('big_case')
