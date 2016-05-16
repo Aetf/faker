8161f6c616bdc74ebfae4ad5a0053241638f8320
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-16 12:31:44 -0400

    "mix: want super/case.py, super/way_and_long_place/few_company.py and super/first_part/old_hand_and_big_case/few_problem/little_hand.py from early place"

---
+++ super/way_and_long_place/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    old_place(str_arg)
+    print('small_week')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('high_group_and_first_thing')
