0c2c64034dd3486c390ab4209edb30495b028485
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 13:32:07 -0400

    "rm: try super/small_child/right_work/large_week/right_group/life_or_case/woman_and_right_case.py and super/world/good_fact_and_group/day/bad_case_or_young_group/have_high_hand_under_thing.py over great child"

---
+++ super/small_child/right_work/large_week/right_group/life_or_case/woman_and_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_way(str_arg):
+    time(str_arg)
+    print('company_and_small_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_way('problem')
---
+++ super/world/good_fact_and_group/day/bad_case_or_young_group/have_high_hand_under_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_fact(str_arg):
+    little_life(str_arg)
+    print('say_eye')
+
+def little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_fact('last_group')
