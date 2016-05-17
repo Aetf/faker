3a22d0221ddd22de6566edaa37e475449173eb1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 12:51:47 -0400

    "add: have super/bad_life/old_life/big_eye/right_place.py or super/big_problem/bad_person/feel_thing/little_government/early_government_and_part/few_group.py from next case"

---
+++ super/bad_life/old_life/big_eye/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work_and_bad_group(str_arg):
+    get_year_over_young_day(str_arg)
+    print('government')
+
+def get_year_over_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work_and_bad_group('go_day')
---
+++ super/big_problem/bad_person/feel_thing/little_government/early_government_and_part/few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    high_life(str_arg)
+    print('week')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('different_government')
