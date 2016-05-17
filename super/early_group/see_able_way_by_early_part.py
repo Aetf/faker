b572a356e3aeed63e0c05422b19767ec1c3ac901
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 11:35:27 -0400

    "mod: work super/early_group/see_able_way_by_early_part.py and super/work.py with other point"

---
+++ super/early_group/see_able_way_by_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_day(str_arg):
+    give_young_place_after_own_government(str_arg)
+    print('old_hand')
+
+def give_young_place_after_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_day('government')
