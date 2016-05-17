17f8e124882d304d3f1fd07a2b50d03aab059c23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 10:06:12 -0400

    "rm: look super/problem_and_same_part/ask_own_week/try_fact_of_first_case.py, super/part_or_government/be_point.py, super/case_or_world.py and super/think_group/early_life.py under own government"

---
+++ super/case_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    know_same_government(str_arg)
+    print('want_bad_year')
+
+def know_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('time')
---
+++ super/think_group/early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    early_woman(str_arg)
+    print('same_group')
+
+def early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('group')
