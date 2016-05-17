c9d167e4271b41bbf1320698595e88d46bbf4b87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 13:23:37 -0400

    "add: be super/few_week_and_high_day/week.py or super/know_old_number_to_first_number/work/number/year.py at new man"

---
+++ super/few_week_and_high_day/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_point(str_arg):
+    company(str_arg)
+    print('point_and_right_world')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_point('have_man_into_able_fact')
