a44b09b65e94d97969c0a19e1038cadf42339c60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 11:18:18 -0400

    "rm: have super/take_own_eye_after_old_child/day_and_good_problem/small_eye/first_way/last_problem.py by young company"

---
+++ super/take_own_eye_after_old_child/day_and_good_problem/small_eye/first_way/last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    child(str_arg)
+    print('large_person_and_right_number')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('man')
