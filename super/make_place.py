b2f33236f9d8bc6958b488395bc5a1c98e47765a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 14:44:14 -0400

    "add: do super/make_place.py or super/different_group/child/other_fact_or_great_fact/new_person/find_point.py after right place"

---
+++ super/make_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    call_public_week_beneath_person(str_arg)
+    print('man')
+
+def call_public_week_beneath_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('ask_young_place')
