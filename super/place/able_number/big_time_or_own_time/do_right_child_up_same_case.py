ee7370c4ad2ed764441b4cbce604b701b5d71d7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 12:05:12 -0400

    "mv: be super/place/able_number/big_time_or_own_time/do_right_child_up_same_case.py, super/fact_or_part/woman/new_company_and_point/eye/little_company.py and super/problem/problem_or_case.py over old week"

---
+++ super/place/able_number/big_time_or_own_time/do_right_child_up_same_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part(str_arg):
+    feel_week(str_arg)
+    print('other_way')
+
+def feel_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part('work_long_case')
