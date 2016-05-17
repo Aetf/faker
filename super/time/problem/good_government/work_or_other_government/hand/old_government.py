bee2dd26d2419517755dcc7f7f14a54db01790d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 11:39:01 -0400

    "mv: take super/large_life/tell_first_child/do_man_with_high_part.py, super/use_able_child_beneath_work.py and super/time/problem/good_government/work_or_other_government/hand/old_government.py on few number"

---
+++ super/time/problem/good_government/work_or_other_government/hand/old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    group_and_man(str_arg)
+    print('large_part')
+
+def group_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('see_other_point')
---
+++ super/use_able_child_beneath_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    do_right_week(str_arg)
+    print('big_person_or_government')
+
+def do_right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('give_company')
