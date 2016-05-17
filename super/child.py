882da713811984b99e01b0bf1257cff2332e1fce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-17 13:31:39 -0400

    "mod: tell super/child.py, super/old_time.py or super/fact.py above high number"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    come_young_number(str_arg)
+    print('high_company')
+
+def come_young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('important_company')
