6ec3632b22075ca40d9b065423b46bd3dad6233b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 10:26:38 -0400

    "rm: work super/young_number/time.py or super/young_part_and_life.py over small man"

---
+++ super/young_number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    own_case(str_arg)
+    print('great_group')
+
+def own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('big_hand')
