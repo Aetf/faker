0547e268a4abd1e39b388282c7930c5cd76b6e42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 13:44:13 -0400

    "rm: try super/group/have_week_under_problem/point/world_and_own_part/go_week.py or super/call_thing/thing/place/have_company_up_group/hand.py of bad part"

---
+++ super/group/have_week_under_problem/point/world_and_own_part/go_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time(str_arg):
+    other_year(str_arg)
+    print('work_and_year')
+
+def other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time('say_world')
