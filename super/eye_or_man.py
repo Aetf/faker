553a6a4a530214df5023dd555553df62f1df1d25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 10:29:26 -0400

    "add: seem super/time/have_week_up_work/thing/come_right_person/able_hand/life.py, super/be_own_woman/long_case_and_time/have_same_case_up_little_number.py and super/eye_or_man.py at bad year"

---
+++ super/eye_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_next_thing(str_arg):
+    long_thing_or_thing(str_arg)
+    print('come_great_government')
+
+def long_thing_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_next_thing('person')
---
+++ super/time/have_week_up_work/thing/come_right_person/able_hand/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    able_government(str_arg)
+    print('tell_other_woman_up_new_case')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('life_and_work')
