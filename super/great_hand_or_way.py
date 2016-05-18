2013a8bcda86de4eea2ce3dcc43095ec94b29c53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 14:26:25 -0400

    "mod: think super/great_hand_or_way.py on large time"

---
+++ super/great_hand_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_way_from_old_government(str_arg):
+    new_person_and_day(str_arg)
+    print('high_thing')
+
+def new_person_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_way_from_old_government('big_fact')
