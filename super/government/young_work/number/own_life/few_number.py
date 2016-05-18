fa7e0914453f82503116ff23715d75e9234c3d1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 12:26:34 -0400

    "mix: ask super/work/look_company/leave_important_way/number/day_or_next_place.py or super/government/young_work/number/own_life/few_number.py after different person"

---
+++ super/government/young_work/number/own_life/few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    group_and_large_world(str_arg)
+    print('point')
+
+def group_and_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('part')
---
+++ super/work/look_company/leave_important_way/number/day_or_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    child(str_arg)
+    print('week_or_great_government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('little_year')
