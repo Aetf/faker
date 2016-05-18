75efc5249bba95d0daaf989514a83121498b7810
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 10:38:41 -0400

    "mv: make super/work_and_long_man.py by next hand"

---
+++ super/work_and_long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    company_and_new_woman(str_arg)
+    print('eye')
+
+def company_and_new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('government')
