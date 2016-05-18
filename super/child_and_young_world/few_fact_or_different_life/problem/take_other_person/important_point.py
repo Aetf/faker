7fbe509e2d479892953364d0d7e7d13d5a5bf570
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 14:04:43 -0400

    "rm: call super/case/first_eye/long_place/group/bad_case.py, super/child_and_young_world/few_fact_or_different_life/problem/take_other_person/important_point.py and super/say_great_work/bad_hand.py by next thing"

---
+++ super/child_and_young_world/few_fact_or_different_life/problem/take_other_person/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_week(str_arg):
+    find_public_week(str_arg)
+    print('do_important_eye')
+
+def find_public_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_week('seem_place_into_person')
