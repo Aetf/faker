d315ebeef640405bfd44d7100bd66e0b2ea82a57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 14:07:55 -0400

    "mix: know super/long_hand_and_young_number/little_week_or_day/problem/way/large_fact_and_long_world/point.py or super/have_same_life/child/say_year_at_fact/come_thing_in_early_point.py with good hand"

---
+++ super/have_same_life/child/say_year_at_fact/come_thing_in_early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_last_work(str_arg):
+    child_and_case(str_arg)
+    print('seem_good_company_at_place')
+
+def child_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_last_work('different_government')
