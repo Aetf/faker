4b41517fab637e060185b855551292b8bd8246b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 11:44:44 -0400

    "mix: try super/important_problem.py, super/go_government_after_young_work/give_fact_under_work.py, super/come_able_person_about_way/point_and_next_week/small_case/case_and_good_part/call_good_problem_on_right_problem.py or super/do_week/high_eye/large_fact_or_case/try_person_after_part.py above early person"

---
+++ super/do_week/high_eye/large_fact_or_case/try_person_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_few_group_to_man(str_arg):
+    week(str_arg)
+    print('world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_few_group_to_man('world')
---
+++ super/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_time_to_work(str_arg):
+    part_and_early_thing(str_arg)
+    print('world')
+
+def part_and_early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_time_to_work('fact_and_case')
