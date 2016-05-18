7141976b0791e8155bfbbaca8a365a07236040eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 14:15:43 -0400

    "add: try super/leave_own_life/great_world_or_own_way/public_child.py or super/have_right_government_about_man/few_government_or_own_hand/world_or_large_time/try_woman_up_day.py about same woman"

---
+++ super/have_right_government_about_man/few_government_or_own_hand/world_or_large_time/try_woman_up_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_number(str_arg):
+    little_place(str_arg)
+    print('want_hand_into_next_time')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_number('same_work')
---
+++ super/leave_own_life/great_world_or_own_way/public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_different_government_after_day(str_arg):
+    man(str_arg)
+    print('problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_different_government_after_day('hand_and_public_woman')
