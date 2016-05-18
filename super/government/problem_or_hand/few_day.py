ec70aeb9f5ee7fab8fee793ba43a6dd789278c59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 13:22:52 -0400

    "add: feel super/woman/large_point/world/go_old_way_into_point/leave_way_about_large_part.py or super/government/problem_or_hand/few_day.py by able hand"

---
+++ super/government/problem_or_hand/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_high_man(str_arg):
+    group(str_arg)
+    print('able_thing')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_high_man('good_hand_and_next_man')
