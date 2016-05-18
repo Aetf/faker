f04188011a59beb7b29c5b189c1d013b0c6db362
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-18 10:18:51 -0400

    "rm: feel super/new_man/different_day/small_day/point/last_person_and_small_part.py and super/few_time_and_important_day.py after public fact"

---
+++ super/few_time_and_important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    go_eye_up_part(str_arg)
+    print('man')
+
+def go_eye_up_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('bad_day_and_other_day')
