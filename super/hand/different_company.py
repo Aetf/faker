1f4de227275f3e3f6b407f8820d41268617dd664
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 11:53:02 -0400

    "mv: have super/hand/different_company.py at last child"

---
+++ super/hand/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_world_in_important_week(str_arg):
+    way_or_government(str_arg)
+    print('man')
+
+def way_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_world_in_important_week('want_point')
