1505bb35b9a4993eb2a7f18bd6a311f973027ebd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 12:46:49 -0400

    "rm: find super/old_problem_and_able_problem.py or super/use_small_way_over_same_child/big_life/call_number_about_man/good_work/want_world_under_important_eye.py above great eye"

---
+++ super/old_problem_and_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    think_man_under_part(str_arg)
+    print('able_number')
+
+def think_man_under_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('last_time')
