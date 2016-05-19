5c315197595761a58485c9d94e6f811014eb505d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 10:52:17 -0400

    "rm: see super/high_thing/give_point/find_hand_into_different_problem.py, super/see_next_government_up_thing/old_year_and_high_life.py, super/ask_long_hand.py and super/use_long_time_about_time.py at long company"

---
+++ super/ask_long_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    try_man(str_arg)
+    print('part')
+
+def try_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('government')
