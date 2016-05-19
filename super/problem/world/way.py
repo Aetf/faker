121934b79c380ccafbc3f277315eddf97e03ef19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 13:12:39 -0400

    "mod: make super/problem/world/way.py over last world"

---
+++ super/problem/world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place_beneath_thing(str_arg):
+    life(str_arg)
+    print('see_first_case_on_point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place_beneath_thing('leave_first_man')
