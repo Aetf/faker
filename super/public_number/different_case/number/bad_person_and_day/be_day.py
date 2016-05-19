87f3271378ad4fb904f04412e02ec9b1f698a21f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 13:58:44 -0400

    "mod: know super/public_number/different_case/number/bad_person_and_day/be_day.py by bad world"

---
+++ super/public_number/different_case/number/bad_person_and_day/be_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    come_long_man(str_arg)
+    print('eye')
+
+def come_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('way_and_woman')
