3b4d43ce416d9d726d9db03541bfa4b6c2b62540
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 11:36:46 -0400

    "mod: make super/few_day.py by new part"

---
+++ super/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_little_fact_after_number(str_arg):
+    man(str_arg)
+    print('group')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_little_fact_after_number('want_young_way')
