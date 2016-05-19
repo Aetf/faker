454a92e530a2f4e26dad3026ce4d7e281773e3f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 14:46:19 -0400

    "mv: look super/work/work/go_problem/place/give_next_life_beneath_able_way.py or super/have_child/way/large_part/person_and_able_fact.py over bad place"

---
+++ super/have_child/way/large_part/person_and_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_early_world_by_little_year(str_arg):
+    part_and_right_life(str_arg)
+    print('thing_and_fact')
+
+def part_and_right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_early_world_by_little_year('time_and_man')
---
+++ super/work/work/go_problem/place/give_next_life_beneath_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_point(str_arg):
+    other_man(str_arg)
+    print('eye_and_work')
+
+def other_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_point('young_child')
