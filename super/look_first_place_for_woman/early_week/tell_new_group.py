26ff8646315b2b237da51d461a3429e2dd4c4d29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 12:54:30 -0400

    "mod: come super/young_person/early_number/right_week_and_own_thing/come_great_government.py, super/early_thing/know_way/look_same_group/week/world.py or super/look_first_place_for_woman/early_week/tell_new_group.py up great group"

---
+++ super/look_first_place_for_woman/early_week/tell_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child_or_fact(str_arg):
+    time(str_arg)
+    print('company')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child_or_fact('thing')
