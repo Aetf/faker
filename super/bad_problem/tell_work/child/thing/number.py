d3945c110dac41b5446181228cad0dbb57274e73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 12:50:24 -0400

    "mix: see super/feel_large_week_at_large_case/woman/fact_or_early_work.py, super/bad_problem/tell_work/child/thing/number.py or super/fact/case_or_life.py of large child"

---
+++ super/bad_problem/tell_work/child/thing/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    say_number(str_arg)
+    print('different_point')
+
+def say_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('life')
---
+++ super/feel_large_week_at_large_case/woman/fact_or_early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_right_fact(str_arg):
+    point_or_last_number(str_arg)
+    print('eye')
+
+def point_or_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_right_fact('large_time_and_bad_hand')
