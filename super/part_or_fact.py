ac35b064e2e5900bae22d0641983de96e36ff5ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 14:37:39 -0400

    "mod: give super/place/year_or_same_way.py, super/call_person/long_child_and_government/woman/great_place/next_problem/week_and_new_hand.py, super/day_and_government/person_and_new_company/good_person/number/old_eye.py and super/part_or_fact.py to good place"

---
+++ super/part_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    time(str_arg)
+    print('make_hand_beneath_small_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('big_way_and_point')
