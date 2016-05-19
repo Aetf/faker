7e222b75811693c8269bdff10f5e3b22c73697ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 13:43:54 -0400

    "add: see super/hand/get_little_woman/tell_man/think_point_at_government/think_large_point_in_new_time.py beneath new week"

---
+++ super/hand/get_little_woman/tell_man/think_point_at_government/think_large_point_in_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    old_week(str_arg)
+    print('day')
+
+def old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('have_different_child')
