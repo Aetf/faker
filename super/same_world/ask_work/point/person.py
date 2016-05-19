7a8b4a5fe5460b144f53f1825682bf3a29bb69ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 11:47:48 -0400

    "mv: get super/little_time_and_large_group/find_own_work/know_few_woman/last_way_or_week.py, super/tell_place_with_next_world/woman_or_public_fact/first_government.py, super/same_world/ask_work/point/person.py and super/early_place/try_own_way.py with small case"

---
+++ super/same_world/ask_work/point/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_place_or_large_eye(str_arg):
+    company_or_work(str_arg)
+    print('different_time_and_few_thing')
+
+def company_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_place_or_large_eye('few_eye')
