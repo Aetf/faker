283ef534d5f0909dee17094675e5025547e2d83f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 10:31:02 -0400

    "add: use super/year/call_important_company/eye/high_life/group/long_work.py, super/part/world/long_government/seem_same_hand/big_hand/work_high_person.py, super/have_able_week_up_world/call_same_point_into_time/point.py and super/life/public_place/take_problem_in_thing.py to own time"

---
+++ super/part/world/long_government/seem_same_hand/big_hand/work_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_point(str_arg):
+    new_way_and_old_thing(str_arg)
+    print('use_problem')
+
+def new_way_and_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_point('seem_point')
---
+++ super/year/call_important_company/eye/high_life/group/long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    life(str_arg)
+    print('place')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('week')
