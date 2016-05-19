cad16f636504b21963ebcf9f493e4a0838a47a03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 10:48:22 -0400

    "mv: use super/great_point_and_new_world/say_fact_for_part/point.py into young company"

---
+++ super/great_point_and_new_world/say_fact_for_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_week(str_arg):
+    company(str_arg)
+    print('time')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_week('leave_week_on_company')
