163f6766bc18c9987493654b30be72634767b2bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-19 12:10:41 -0400

    "add: tell super/good_week_and_number.py, super/child_and_able_week/large_week.py, super/go_fact/important_child/time/fact_and_eye.py or super/work/day.py under early government"

---
+++ super/go_fact/important_child/time/fact_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem_and_place(str_arg):
+    person(str_arg)
+    print('want_life_about_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem_and_place('part')
---
+++ super/good_week_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_next_place(str_arg):
+    feel_child_to_year(str_arg)
+    print('important_point')
+
+def feel_child_to_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_next_place('group_or_group')
