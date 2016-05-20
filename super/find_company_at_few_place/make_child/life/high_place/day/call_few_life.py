7bb441aa2c882d4ab7bb167c913d4314d2ef0a5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:27:11 -0400

    "rm: give super/find_company_at_few_place/make_child/life/high_place/day/call_few_life.py in big number"

---
+++ super/find_company_at_few_place/make_child/life/high_place/day/call_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government(str_arg):
+    way_and_government(str_arg)
+    print('place')
+
+def way_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government('woman')
