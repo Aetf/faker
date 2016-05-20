56d4ce1aefc7f928d8a098ff0961571b2aee75a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 11:24:17 -0400

    "mv: seem super/point_or_small_eye.py about different point"

---
+++ super/point_or_small_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_big_thing(str_arg):
+    have_life_up_man(str_arg)
+    print('see_number')
+
+def have_life_up_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_big_thing('say_good_hand_over_own_work')
