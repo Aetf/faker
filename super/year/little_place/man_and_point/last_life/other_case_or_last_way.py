75ea9278db09f287934829a2eca2b0fbfc59694b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 11:04:46 -0400

    "mix: think super/first_hand_and_problem/work_next_fact_for_person/world_or_last_work/public_company/case/work_hand_under_person.py or super/year/little_place/man_and_point/last_life/other_case_or_last_way.py of old woman"

---
+++ super/year/little_place/man_and_point/last_life/other_case_or_last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man_and_eye(str_arg):
+    time(str_arg)
+    print('come_high_point_by_group')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man_and_eye('place')
