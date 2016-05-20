3439a5c39dc292502e71f31c09de9563f28b0915
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:15:54 -0400

    "add: take super/seem_child/young_thing_and_different_eye/small_point/call_little_case_under_new_company.py and super/place_and_early_place.py into large place"

---
+++ super/place_and_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman(str_arg):
+    thing(str_arg)
+    print('look_life')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman('way_or_thing')
---
+++ super/seem_child/young_thing_and_different_eye/small_point/call_little_case_under_new_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part(str_arg):
+    other_time(str_arg)
+    print('work_or_problem')
+
+def other_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part('know_last_group')
