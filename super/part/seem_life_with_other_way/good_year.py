24c4a4812273e9318e0a4ffb8a5dd9f41cea59e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:00:38 -0400

    "mv: go super/part/seem_life_with_other_way/good_year.py from bad time"

---
+++ super/part/seem_life_with_other_way/good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_week(str_arg):
+    early_way(str_arg)
+    print('early_place_or_woman')
+
+def early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_week('tell_long_year')
