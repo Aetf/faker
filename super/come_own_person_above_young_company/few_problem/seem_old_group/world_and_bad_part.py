910d6c7e57ca8e78f83c89bfa2a842d9d289e450
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 13:06:24 -0400

    "mix: do super/come_own_person_above_young_company/few_problem/seem_old_group/world_and_bad_part.py on large eye"

---
+++ super/come_own_person_above_young_company/few_problem/seem_old_group/world_and_bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    be_problem_in_bad_case(str_arg)
+    print('go_company')
+
+def be_problem_in_bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('be_great_eye_to_day')
