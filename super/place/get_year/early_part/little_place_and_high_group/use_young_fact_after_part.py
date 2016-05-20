a2546bfbb0a24febaa59618da9b28691f5a9c735
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:35:49 -0400

    "mod: leave super/work/feel_first_work_in_bad_work/few_person.py, super/little_thing_or_same_point/want_good_point/small_company_and_week.py and super/place/get_year/early_part/little_place_and_high_group/use_young_fact_after_part.py over bad point"

---
+++ super/place/get_year/early_part/little_place_and_high_group/use_young_fact_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    work_next_work(str_arg)
+    print('place')
+
+def work_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('feel_large_world_after_little_man')
---
+++ super/work/feel_first_work_in_bad_work/few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_life(str_arg):
+    young_work(str_arg)
+    print('week_or_last_woman')
+
+def young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_life('important_group')
