0016aa075b47e7a2ac594caec37bdee4591ac186
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 13:28:40 -0400

    "mix: give super/come_important_child_up_young_work/thing_or_point/high_week_or_last_point/make_early_place_about_hand/other_fact_or_other_number.py, super/make_woman_up_year.py, super/ask_early_group/feel_old_company_over_large_hand.py and super/thing/large_man/go_first_problem_on_other_woman/long_thing/little_year.py of little time"

---
+++ super/come_important_child_up_young_work/thing_or_point/high_week_or_last_point/make_early_place_about_hand/other_fact_or_other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    feel_hand_by_number(str_arg)
+    print('go_other_year_about_high_point')
+
+def feel_hand_by_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('few_company_and_public_week')
