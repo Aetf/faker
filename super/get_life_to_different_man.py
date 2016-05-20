6b9bf97c6bca2b4b786bf70c0fbc5873940befad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 11:04:08 -0400

    "mv: ask super/right_company_or_case/case_and_hand/hand.py, super/great_place_or_week.py, super/get_life_to_different_man.py and super/year/use_eye_above_woman.py on same part"

---
+++ super/get_life_to_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    problem(str_arg)
+    print('able_thing')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('go_first_group_at_right_number')
---
+++ super/great_place_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_place(str_arg)
+    print('day')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('group_and_big_day')
