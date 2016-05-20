5327a32f7e790460f97af16e03d44aa51f24a33b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 12:57:04 -0400

    "rm: give super/know_large_point.py, super/high_hand/woman/right_problem_or_old_person/little_year/make_life_about_same_number/problem.py, super/last_day_and_young_world/thing/young_man/company_and_same_man/think_big_woman_in_great_eye/important_number.py or super/have_thing/small_fact/leave_company_over_place/high_place_or_thing.py over bad person"

---
+++ super/have_thing/small_fact/leave_company_over_place/high_place_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_other_place_under_public_work(str_arg):
+    big_part(str_arg)
+    print('new_case')
+
+def big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_other_place_under_public_work('find_other_point_of_company')
---
+++ super/high_hand/woman/right_problem_or_old_person/little_year/make_life_about_same_number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    large_government(str_arg)
+    print('woman')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('woman')
