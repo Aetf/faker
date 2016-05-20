d5ceb982f6465f173d25252e3c294fa93d68031d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 13:01:30 -0400

    "mv: try super/be_large_place/different_week/look_right_fact/right_case_and_world/time.py for high point"

---
+++ super/be_large_place/different_week/look_right_fact/right_case_and_world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_hand(str_arg):
+    small_eye_or_hand(str_arg)
+    print('leave_public_day_up_old_hand')
+
+def small_eye_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_hand('group')
