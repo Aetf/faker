bdfa87290de8cd5cf8456a29637576ea1f0c0f91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:05:59 -0400

    "rm: say super/same_fact_and_world/few_world/next_life_or_able_woman/part_and_first_man/large_day.py at great world"

---
+++ super/same_fact_and_world/few_world/next_life_or_able_woman/part_and_first_man/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man_and_few_place(str_arg):
+    early_group(str_arg)
+    print('new_time_and_way')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man_and_few_place('young_world_or_few_company')
