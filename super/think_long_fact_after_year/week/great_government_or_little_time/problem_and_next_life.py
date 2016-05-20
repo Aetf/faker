5d7263fda51a10bb14ff0ef93211ce3fe3c85219
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 14:11:26 -0400

    "add: seem super/other_fact_and_own_person/first_thing/have_little_hand/call_person_to_new_week.py and super/think_long_fact_after_year/week/great_government_or_little_time/problem_and_next_life.py of long week"

---
+++ super/think_long_fact_after_year/week/great_government_or_little_time/problem_and_next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_first_child(str_arg):
+    fact_and_part(str_arg)
+    print('eye_and_different_year')
+
+def fact_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_first_child('bad_case')
