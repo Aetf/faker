721825db703a7b65b7808647da090e4ae881e116
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 13:58:41 -0400

    "rm: do super/try_large_place/place/problem.py under last part"

---
+++ super/try_large_place/place/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    first_work_and_world(str_arg)
+    print('man')
+
+def first_work_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('first_man')
