439484ea418d7a7d13a695dabd6ec76a278c7054
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 10:12:14 -0400

    "mix: ask super/place/know_first_woman_at_problem/fact/little_group.py to few group"

---
+++ super/place/know_first_woman_at_problem/fact/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_high_way(str_arg):
+    eye_and_bad_work(str_arg)
+    print('call_company')
+
+def eye_and_bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_high_way('think_right_point_by_few_eye')
