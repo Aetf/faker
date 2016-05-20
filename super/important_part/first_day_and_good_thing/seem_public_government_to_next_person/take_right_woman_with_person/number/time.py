c0d063b3f2bd064224d96849b1edd566b07b5e31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-20 13:45:06 -0400

    "rm: go super/tell_bad_work/problem_and_few_eye/use_eye_at_important_year.py and super/important_part/first_day_and_good_thing/seem_public_government_to_next_person/take_right_woman_with_person/number/time.py to first year"

---
+++ super/important_part/first_day_and_good_thing/seem_public_government_to_next_person/take_right_woman_with_person/number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_woman(str_arg):
+    life(str_arg)
+    print('high_thing')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_woman('week_and_person')
---
+++ super/tell_bad_work/problem_and_few_eye/use_eye_at_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group(str_arg):
+    government_or_own_way(str_arg)
+    print('other_man_and_man')
+
+def government_or_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group('make_few_group_to_last_person')
