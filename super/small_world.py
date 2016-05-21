59a8910473053779bb5de119f6bf0f2d9226cd35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:33:42 -0400

    "rm: be super/small_world.py and super/world_and_place/same_time/large_child_or_few_group/case/say_large_time.py of large fact"

---
+++ super/small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_great_group(str_arg):
+    big_way(str_arg)
+    print('leave_next_case_into_early_case')
+
+def big_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_great_group('fact')
