3eed1dc5fdc9926576598714f8224100a391c3c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 13:32:42 -0400

    "mod: see super/seem_government/other_eye/want_large_person/know_first_eye_over_big_woman/public_life/think_small_thing.py, super/know_company_about_new_person/public_group.py or super/group_and_number/fact/bad_time/place_or_young_thing/find_case.py over small life"

---
+++ super/know_company_about_new_person/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_number(str_arg):
+    bad_company(str_arg)
+    print('ask_day_beneath_fact')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_number('other_woman')
---
+++ super/seem_government/other_eye/want_large_person/know_first_eye_over_big_woman/public_life/think_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_last_place_in_able_company(str_arg):
+    other_hand(str_arg)
+    print('group')
+
+def other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_last_place_in_able_company('year')
