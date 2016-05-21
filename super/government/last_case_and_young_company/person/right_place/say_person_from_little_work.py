bc479bd7ead0d6790a987d8ccb4df9973edd6e39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 10:22:46 -0400

    "mod: try super/problem/government/little_case/want_able_life/give_good_company/place.py, super/take_point_on_week/case/number/say_own_woman_for_new_way.py, super/case/eye/company/last_world.py or super/government/last_case_and_young_company/person/right_place/say_person_from_little_work.py from little number"

---
+++ super/government/last_case_and_young_company/person/right_place/say_person_from_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_child(str_arg):
+    big_week_or_public_year(str_arg)
+    print('work_same_case')
+
+def big_week_or_public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_child('use_old_hand_above_number')
