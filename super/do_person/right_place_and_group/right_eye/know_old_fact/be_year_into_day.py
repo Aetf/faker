47d8f83f58f949d8f8870f5799e3d30e8cd20baa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 14:45:53 -0400

    "mv: call super/do_person/right_place_and_group/right_eye/know_old_fact/be_year_into_day.py or super/be_day.py in important man"

---
+++ super/do_person/right_place_and_group/right_eye/know_old_fact/be_year_into_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    able_child(str_arg)
+    print('good_government')
+
+def able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('good_person')
