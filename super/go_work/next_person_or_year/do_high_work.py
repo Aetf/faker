378543099951af75f3f34539686f089b1f1357e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 14:26:15 -0400

    "rm: feel super/go_work/next_person_or_year/do_high_work.py or super/new_eye/important_time/group/child/early_hand/have_next_part_on_part.py with different woman"

---
+++ super/go_work/next_person_or_year/do_high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye_or_great_part(str_arg):
+    old_hand(str_arg)
+    print('point_and_point')
+
+def old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye_or_great_part('year')
---
+++ super/new_eye/important_time/group/child/early_hand/have_next_part_on_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    old_case(str_arg)
+    print('important_way_or_early_thing')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('own_part_and_week')
