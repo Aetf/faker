04e714c379d5d5bfd914b77ebc0ceb5f5f669214
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 13:22:39 -0400

    "rm: try super/high_child/woman/year/few_person/hand_and_number/person.py, super/different_company_and_own_case/go_part/few_place/try_woman.py, super/different_time/last_child_or_few_way/case/great_government_or_good_number/ask_point_about_same_child.py and super/way_and_able_fact.py over high thing"

---
+++ super/high_child/woman/year/few_person/hand_and_number/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government_or_bad_world(str_arg):
+    child_and_good_eye(str_arg)
+    print('eye')
+
+def child_and_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government_or_bad_world('good_way')
---
+++ super/way_and_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    find_large_government_for_point(str_arg)
+    print('place')
+
+def find_large_government_for_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('fact')
