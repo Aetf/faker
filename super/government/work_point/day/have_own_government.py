7d9ba00f6123c414a8c1ce2ab2e3ec590a1c749a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 10:54:53 -0400

    "add: be super/government/work_point/day/have_own_government.py for little child"

---
+++ super/government/work_point/day/have_own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company_and_bad_year(str_arg):
+    long_part_or_child(str_arg)
+    print('look_right_eye')
+
+def long_part_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company_and_bad_year('time_or_same_hand')
