3f607046ab3d0f157b27caac3fab08814013dd32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:27:27 -0400

    "mix: go super/give_little_point/week.py or super/see_eye_under_man.py into right man"

---
+++ super/give_little_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_man(str_arg):
+    public_work_or_public_problem(str_arg)
+    print('important_company')
+
+def public_work_or_public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_man('last_group')
