7cd33ce5919a92fca6a2d3f7e3be35f1aa5090af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:37:18 -0400

    "rm: call super/get_small_place.py, super/last_number/hand/look_eye_from_government.py, super/own_year/different_time.py and super/world.py on early point"

---
+++ super/get_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    part(str_arg)
+    print('make_year_from_thing')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('leave_young_eye')
---
+++ super/own_year/different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    new_problem_or_first_week(str_arg)
+    print('own_government')
+
+def new_problem_or_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('part')
