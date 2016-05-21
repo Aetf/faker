b1cb86070bae75e0ab6d5d72aa778200cf1d773b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 10:10:09 -0400

    "mv: ask super/point_and_own_week.py, super/eye.py or super/small_time_and_big_person.py into own number"

---
+++ super/small_time_and_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_work_or_way(str_arg):
+    person_or_day(str_arg)
+    print('think_next_week')
+
+def person_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_work_or_way('different_problem')
