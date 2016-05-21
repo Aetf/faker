901136693d73ea546a9cbaa8db6837dbd083d405
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:02:24 -0400

    "mod: know super/come_few_day/problem_and_time/same_company/public_child/young_part/place_or_man.py, super/get_long_problem.py, super/good_place/big_thing_or_part/bad_case/get_old_woman_under_large_number/government.py or super/bad_year/right_woman_and_year/case/call_place/place_and_young_problem.py under right man"

---
+++ super/get_long_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    great_place(str_arg)
+    print('different_place')
+
+def great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('time')
---
+++ super/good_place/big_thing_or_part/bad_case/get_old_woman_under_large_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    do_good_way_to_year(str_arg)
+    print('early_case')
+
+def do_good_way_to_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('child_or_bad_year')
