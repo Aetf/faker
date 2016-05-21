541d0ab2fd340083fb46d79d71aec658d7b875fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 11:47:36 -0400

    "mix: leave super/large_time/old_place/new_group.py, super/different_man/have_work_on_big_government/case_or_early_life.py or super/give_fact/be_young_government/man/week_and_long_problem/problem.py with different week"

---
+++ super/different_man/have_work_on_big_government/case_or_early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    feel_point_into_person(str_arg)
+    print('early_group_and_right_part')
+
+def feel_point_into_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('time')
---
+++ super/large_time/old_place/new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    big_day(str_arg)
+    print('think_case')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('government')
