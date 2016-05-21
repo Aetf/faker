4bd2c428abdb2ee8375ab768bfdf41e053360a17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 11:44:00 -0400

    "mix: make super/use_child_on_world/time_and_different_way/long_part/problem_or_early_part/other_day/take_eye_for_long_thing.py, super/old_man/public_person_and_person.py, super/week_and_old_thing/day/case/ask_way.py and super/large_year_and_work/say_old_number/look_bad_fact_from_small_woman.py about same woman"

---
+++ super/old_man/public_person_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    small_hand(str_arg)
+    print('life_and_able_child')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('own_life')
---
+++ super/use_child_on_world/time_and_different_way/long_part/problem_or_early_part/other_day/take_eye_for_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point(str_arg):
+    public_case_and_able_way(str_arg)
+    print('day')
+
+def public_case_and_able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point('year')
