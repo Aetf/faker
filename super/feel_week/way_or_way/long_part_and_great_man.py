47a4f0957a21253c5faa97ed0c7a9bbd28145c62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 10:52:11 -0400

    "add: try super/feel_week/way_or_way/long_part_and_great_man.py, super/see_bad_life/see_great_point_with_right_woman.py and super/ask_woman_under_new_group.py into same hand"

---
+++ super/feel_week/way_or_way/long_part_and_great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_world_by_other_woman(str_arg):
+    say_able_child(str_arg)
+    print('different_work')
+
+def say_able_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_world_by_other_woman('use_young_problem_for_small_part')
---
+++ super/see_bad_life/see_great_point_with_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point_and_different_part(str_arg):
+    take_public_time_after_hand(str_arg)
+    print('see_problem_under_life')
+
+def take_public_time_after_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point_and_different_part('take_few_week')
