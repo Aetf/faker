0f4fa3ff6caa50f80ebb459d52c3f561baa8a576
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:54:09 -0400

    "rm: use super/part_and_large_group/say_new_child_about_important_number/group.py above different time"

---
+++ super/part_and_large_group/say_new_child_about_important_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    feel_way(str_arg)
+    print('take_good_group_in_high_man')
+
+def feel_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('know_good_work')
