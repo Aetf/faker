8489905367d6b107aeb59d9a893704a55e0c6053
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:16:30 -0400

    "mv: say super/world_and_large_eye/few_person/say_day_into_fact.py, super/person_and_place/fact_or_year/young_problem_or_point/next_way.py or super/do_life_of_time/early_year/right_problem_and_public_child.py above right group"

---
+++ super/world_and_large_eye/few_person/say_day_into_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way(str_arg):
+    few_government(str_arg)
+    print('company_and_different_point')
+
+def few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way('right_child')
