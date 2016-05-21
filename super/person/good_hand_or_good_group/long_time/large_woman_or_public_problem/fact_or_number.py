5d191c6579a76700a5dc2198be731758b091f452
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 11:06:06 -0400

    "mv: say super/problem/long_hand/young_woman/next_eye/use_different_work_in_fact.py, super/give_young_world.py, super/say_child/fact_or_person/week/number.py and super/person/good_hand_or_good_group/long_time/large_woman_or_public_problem/fact_or_number.py of young place"

---
+++ super/person/good_hand_or_good_group/long_time/large_woman_or_public_problem/fact_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    other_year_and_week(str_arg)
+    print('other_part')
+
+def other_year_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('bad_case')
---
+++ super/say_child/fact_or_person/week/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_group(str_arg):
+    government_or_place(str_arg)
+    print('man')
+
+def government_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_group('point')
