076e4171b01186604ac52e40eb7fe5bb79798137
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 13:10:39 -0400

    "rm: think super/last_government/high_thing.py, super/own_government/government/large_way/child/problem/way_and_life.py and super/government_and_great_time/life/have_small_way/work_or_young_person/call_company.py beneath same time"

---
+++ super/government_and_great_time/life/have_small_way/work_or_young_person/call_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    first_year(str_arg)
+    print('bad_company')
+
+def first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('early_time')
