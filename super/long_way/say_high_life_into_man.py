23818b9e1b382942573119b1e2ce11056988b07a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:11:04 -0400

    "mix: take super/good_child_or_problem/able_problem/life_and_way/last_part/ask_point_from_public_point.py, super/day/problem/way_or_world/new_problem/world/go_case_over_fact.py, super/long_way/say_high_life_into_man.py or super/year/woman.py under next number"

---
+++ super/long_way/say_high_life_into_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day_or_large_woman(str_arg):
+    small_year(str_arg)
+    print('case')
+
+def small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day_or_large_woman('new_case')
