4a2bdfcba3715ec133a58e26c05e4fe893567076
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 12:53:59 -0400

    "add: see super/long_time_or_few_company.py and super/different_problem_and_right_problem/first_person.py into early week"

---
+++ super/long_time_or_few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_fact(str_arg):
+    use_little_life(str_arg)
+    print('great_government')
+
+def use_little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_fact('fact')
