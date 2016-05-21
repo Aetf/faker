9c543141ba35efdb68589f475c4a1e72ecbb9fda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-21 13:57:43 -0400

    "rm: make super/young_week_and_year/last_year_or_work/number_or_good_thing/great_child_and_important_man/think_fact/long_part_and_day.py, super/say_part/work_large_woman_over_life/time.py, super/thing.py and super/old_government_or_person.py under great government"

---
+++ super/say_part/work_large_woman_over_life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_good_point(str_arg):
+    great_company_and_woman(str_arg)
+    print('small_problem')
+
+def great_company_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_good_point('first_person')
---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    long_life(str_arg)
+    print('other_eye_or_old_eye')
+
+def long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('work_other_work_about_place')
