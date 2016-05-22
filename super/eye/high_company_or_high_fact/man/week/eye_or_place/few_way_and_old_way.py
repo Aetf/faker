c14f335762743f436a2256ef81049db7720157ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:18:11 -0400

    "rm: call super/eye/high_company_or_high_fact/man/week/eye_or_place/few_way_and_old_way.py, super/fact/come_early_company_by_next_eye/first_fact_or_good_person/make_problem/time/call_time_for_high_life.py or super/same_world/want_place/different_eye.py from young problem"

---
+++ super/eye/high_company_or_high_fact/man/week/eye_or_place/few_way_and_old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_early_part(str_arg):
+    fact_and_work(str_arg)
+    print('make_thing')
+
+def fact_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_early_part('group')
