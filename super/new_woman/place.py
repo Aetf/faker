2d5b5af4b7a697052206be92635d7f04c97ef776
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 13:00:44 -0400

    "add: leave super/government/make_hand/small_place_or_old_child/life_or_own_world/own_way_or_part.py, super/world_and_new_way.py, super/woman_or_big_child/public_government/high_company_or_work/time.py and super/new_woman/place.py in great week"

---
+++ super/new_woman/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work(str_arg):
+    old_hand(str_arg)
+    print('go_public_way')
+
+def old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work('group')
---
+++ super/woman_or_big_child/public_government/high_company_or_work/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    big_group(str_arg)
+    print('way_and_place')
+
+def big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('give_small_person_under_own_eye')
