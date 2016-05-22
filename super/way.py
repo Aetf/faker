bcaa7e30d88661fe2aac7b1c8014d19dc1aeaaf6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:43:32 -0400

    "mv: tell super/way.py, super/get_week_at_able_child/try_next_group.py or super/own_eye_and_thing/week/day_or_company/be_large_point_about_number/child.py at bad time"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_important_woman(str_arg):
+    great_number(str_arg)
+    print('find_government_for_next_eye')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_important_woman('bad_world')
