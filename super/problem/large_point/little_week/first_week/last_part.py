ab8bcb3048c350f0df9426361d9cb27646d7962c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:45:52 -0400

    "mix: know super/eye_or_other_hand/see_public_group_from_problem/look_year_under_new_group.py or super/problem/large_point/little_week/first_week/last_part.py in few hand"

---
+++ super/problem/large_point/little_week/first_week/last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year(str_arg):
+    give_bad_way_over_hand(str_arg)
+    print('eye')
+
+def give_bad_way_over_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year('big_problem')
