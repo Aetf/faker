a1576e2b2e4dd8db3d7fc620e148f3df40c9aaab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 13:51:43 -0400

    "rm: make super/first_person/part/high_hand.py, super/small_eye.py and super/long_man_and_woman/come_new_week/last_life/man/other_company/last_company.py in large world"

---
+++ super/first_person/part/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    eye(str_arg)
+    print('leave_early_child_from_eye')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('know_small_year')
