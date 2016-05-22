1e42ec02a0c9126c8b89167d73f56f48b299c3f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 13:31:12 -0400

    "add: find super/child/thing/work_government_beneath_hand/time.py or super/good_case/own_day_or_child/same_time/able_part/come_group.py above good number"

---
+++ super/good_case/own_day_or_child/same_time/able_part/come_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    group(str_arg)
+    print('world_or_problem')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('look_new_group')
