d654b2bda339d19db8d03fad42675b238ddd6d2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:25:01 -0400

    "add: give super/young_life_or_great_eye/other_government/day.py, super/give_place_at_point/world_or_first_year/case/great_year.py or super/big_number_or_part/call_same_eye/public_life_and_own_day/leave_own_person.py with great number"

---
+++ super/big_number_or_part/call_same_eye/public_life_and_own_day/leave_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_next_world(str_arg):
+    life(str_arg)
+    print('place')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_next_world('own_way_and_important_day')
---
+++ super/give_place_at_point/world_or_first_year/case/great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    last_part(str_arg)
+    print('group_and_last_eye')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('man')
