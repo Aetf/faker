1533aa40f0672f129fefe157595be742c0d3bb43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 12:20:02 -0400

    "rm: work super/own_company/case_and_year/feel_company_on_big_point/good_time/old_child.py, super/child/eye/early_problem/tell_great_place/fact_or_little_part.py, super/woman/way_and_thing/be_other_hand.py or super/thing/group_and_long_number/woman/good_company/thing.py beneath last week"

---
+++ super/own_company/case_and_year/feel_company_on_big_point/good_time/old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    world_or_fact(str_arg)
+    print('woman_and_hand')
+
+def world_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('high_group')
---
+++ super/woman/way_and_thing/be_other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    fact_or_way(str_arg)
+    print('get_case_for_world')
+
+def fact_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('life')
