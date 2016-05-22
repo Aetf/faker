44e1df0d47bd84cc031a4ee5a0fb5033e636b85c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:35:50 -0400

    "rm: call super/government_or_able_problem/next_year/own_person.py, super/place/week_and_day/seem_part.py, super/company/case.py or super/look_next_child/time/say_great_part_in_time/work/try_small_work_after_point/hand_or_few_woman.py into first week"

---
+++ super/look_next_child/time/say_great_part_in_time/work/try_small_work_after_point/hand_or_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_right_hand(str_arg):
+    go_own_fact(str_arg)
+    print('week')
+
+def go_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_right_hand('want_world')
---
+++ super/place/week_and_day/seem_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_problem(str_arg):
+    hand(str_arg)
+    print('public_government_or_bad_number')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_problem('know_work_with_thing')
