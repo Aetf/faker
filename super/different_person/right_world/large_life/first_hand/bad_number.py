25e3d0301b1ea98ba5e8821241166fdabc407cd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:34:06 -0400

    "rm: know super/group/little_way_and_same_thing/government.py and super/different_person/right_world/large_life/first_hand/bad_number.py under high point"

---
+++ super/different_person/right_world/large_life/first_hand/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case(str_arg):
+    get_work_of_day(str_arg)
+    print('big_problem')
+
+def get_work_of_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case('group_or_own_day')
---
+++ super/group/little_way_and_same_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_place(str_arg):
+    early_year(str_arg)
+    print('problem')
+
+def early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_place('new_thing')
