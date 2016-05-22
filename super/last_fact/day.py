4c38ee400947153be747b6d69cd864425778caba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 14:00:42 -0400

    "mix: call super/last_fact/day.py and super/little_group/woman/say_right_week_with_person/ask_group.py at new year"

---
+++ super/last_fact/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    think_woman(str_arg)
+    print('way')
+
+def think_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('long_woman')
