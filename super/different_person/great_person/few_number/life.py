6ad33168a3afca4b785c0c39024de103ccd94615
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 12:59:19 -0400

    "add: say super/little_thing/work_and_point/little_case.py, super/great_point/group_and_place.py, super/different_person/great_person/few_number/life.py or super/part/big_eye/do_world/see_group_about_next_man/use_bad_time_for_bad_place.py on own day"

---
+++ super/different_person/great_person/few_number/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    part_or_world(str_arg)
+    print('part')
+
+def part_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('use_last_hand')
---
+++ super/little_thing/work_and_point/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government_and_work(str_arg):
+    child(str_arg)
+    print('use_different_number_at_early_year')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government_and_work('ask_world')
