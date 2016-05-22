81e615714ce2da5d00ce4e66ae1a743ce55608dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 11:42:56 -0400

    "rm: think super/small_eye/little_child/old_work/same_man/point_and_large_way.py, super/long_life.py and super/person_or_little_group/thing.py over big problem"

---
+++ super/small_eye/little_child/old_work/same_man/point_and_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_life_to_good_part(str_arg):
+    think_place(str_arg)
+    print('company')
+
+def think_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_life_to_good_part('call_old_world')
