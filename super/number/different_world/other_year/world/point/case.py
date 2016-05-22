49aac9af1db298bb362c2779de09b1fa3440a92a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-22 13:48:34 -0400

    "mod: give super/use_thing.py, super/few_case_or_first_time/high_group/last_person/public_point/year.py or super/number/different_world/other_year/world/point/case.py after public number"

---
+++ super/number/different_world/other_year/world/point/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_high_woman_on_long_person(str_arg):
+    first_child(str_arg)
+    print('long_government')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_high_woman_on_long_person('say_point_about_few_point')
---
+++ super/use_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_and_place(str_arg):
+    go_person(str_arg)
+    print('know_long_point_by_early_child')
+
+def go_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_and_place('next_work')
