683651f31cbbf812e047482f80ad9ce5fb3c1b3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:22:24 -0400

    "mix: get super/long_woman/long_time/bad_group.py at high group"

---
+++ super/long_woman/long_time/bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child_or_woman(str_arg):
+    early_time_or_day(str_arg)
+    print('use_big_day')
+
+def early_time_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child_or_woman('high_way')
