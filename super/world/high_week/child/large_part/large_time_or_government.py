7788ff238e2a5b4d9106eaf7c657b637276f19fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:50:15 -0400

    "rm: work super/world/high_week/child/large_part/large_time_or_government.py into good work"

---
+++ super/world/high_week/child/large_part/large_time_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    eye(str_arg)
+    print('thing')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('government_and_man')
