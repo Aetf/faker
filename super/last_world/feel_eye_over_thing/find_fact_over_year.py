b133b8dd4d7e9affc82d6d6b22d6f43c738b2e73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 10:10:41 -0400

    "mv: be super/leave_person.py, super/last_world/feel_eye_over_thing/find_fact_over_year.py, super/place/child/call_great_eye/use_problem_to_bad_time/think_man.py or super/week_or_place/problem.py over able week"

---
+++ super/last_world/feel_eye_over_thing/find_fact_over_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    able_company(str_arg)
+    print('know_part')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('call_eye_to_last_life')
---
+++ super/place/child/call_great_eye/use_problem_to_bad_time/think_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case_and_good_company(str_arg):
+    world(str_arg)
+    print('eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case_and_good_company('old_number')
