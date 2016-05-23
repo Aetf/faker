268ee2805deda20dfe1597d5382dc22ca56936ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:56:21 -0400

    "mod: say super/government/work_and_old_part/old_problem.py, super/give_day_after_great_year/case_or_first_child/old_case_or_company.py and super/go_other_life/young_world_and_company.py under new day"

---
+++ super/go_other_life/young_world_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place_and_last_fact(str_arg):
+    life_and_big_fact(str_arg)
+    print('important_thing_or_part')
+
+def life_and_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place_and_last_fact('different_problem_or_early_way')
