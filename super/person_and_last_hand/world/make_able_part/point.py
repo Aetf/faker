70666772c6ffed5e7c03059e2c3b863e9dc95533
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:54:46 -0400

    "add: give super/person_and_last_hand/world/make_able_part/point.py and super/be_great_woman_beneath_fact/thing/hand_or_last_time/problem_and_number/young_week_and_work/company_or_fact.py of high group"

---
+++ super/person_and_last_hand/world/make_able_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    next_eye(str_arg)
+    print('person')
+
+def next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('go_public_week')
