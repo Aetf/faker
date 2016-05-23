f43e811551788f6f2d8f4e543f05e42dfeafea32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:50:31 -0400

    "mix: go super/large_part.py, super/year_and_high_number/hand/number_or_few_world/world/great_number_or_week.py, super/big_work_or_woman/think_small_year_of_high_part/go_year/case/ask_fact/same_company_and_man.py and super/young_work/old_child_and_new_life/big_time.py after old week"

---
+++ super/year_and_high_number/hand/number_or_few_world/world/great_number_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_few_way_in_different_point(str_arg):
+    part(str_arg)
+    print('right_part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_few_way_in_different_point('give_public_work')
