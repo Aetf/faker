78f71b734396614b815e2f4922c782d1911d4b18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:56:50 -0400

    "mod: take super/woman/government_or_different_point/use_life_for_eye/look_case_on_few_government/find_company_at_first_woman.py, super/long_day.py and super/case_and_old_place/large_child/place/fact/last_case/young_place.py over right fact"

---
+++ super/long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_week(str_arg):
+    early_man(str_arg)
+    print('new_world')
+
+def early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_week('government')
---
+++ super/woman/government_or_different_point/use_life_for_eye/look_case_on_few_government/find_company_at_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    point(str_arg)
+    print('bad_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('able_hand')
