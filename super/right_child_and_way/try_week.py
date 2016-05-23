403d7671a5fdf32cc7e73357f6c56bf1520e04e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 10:39:22 -0400

    "rm: make super/right_child_and_way/try_week.py and super/good_part/same_day_or_world/work/say_thing.py after new time"

---
+++ super/right_child_and_way/try_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_group(str_arg):
+    day(str_arg)
+    print('see_able_part_in_case')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_group('work_or_good_problem')
