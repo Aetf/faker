4e8e10f73acdc999cef1893c3d80f5877023192d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:03:16 -0400

    "mod: get super/tell_case/long_fact/do_other_work/other_hand/different_problem/find_number.py after bad government"

---
+++ super/tell_case/long_fact/do_other_work/other_hand/different_problem/find_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_new_case(str_arg):
+    come_government(str_arg)
+    print('good_government')
+
+def come_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_new_case('see_day_at_week')
