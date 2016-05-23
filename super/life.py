bff19bf816164b2ad9b98b76bdf5427a4b6aefc0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:05:41 -0400

    "mix: call super/life.py or super/last_part/group_and_early_way/part_or_long_day/little_case/big_hand/large_time.py into same year"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    government(str_arg)
+    print('public_number_and_problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('woman_and_little_number')
