ee21e56873626465e96f26a6aff07faa736a80ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 14:26:22 -0400

    "mv: get super/problem/take_work_above_new_way.py for first person"

---
+++ super/problem/take_work_above_new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company(str_arg):
+    say_own_part(str_arg)
+    print('way')
+
+def say_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company('point')
