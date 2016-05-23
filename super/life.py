76acf556c87ec82d7ff1fd21ae4d6b72f01d306c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:57:10 -0400

    "mod: take super/life.py of new company"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_point(str_arg):
+    important_problem_or_part(str_arg)
+    print('look_government_over_large_fact')
+
+def important_problem_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_point('government')
