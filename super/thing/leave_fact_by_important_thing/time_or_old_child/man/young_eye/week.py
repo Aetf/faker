6d10304cf801ac35282a86b6be056a491aede5b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:19:32 -0400

    "rm: feel super/thing/leave_fact_by_important_thing/time_or_old_child/man/young_eye/week.py, super/tell_little_woman/few_company_and_part/be_small_day_to_own_thing/thing/have_bad_year.py and super/go_place_about_few_eye.py in bad eye"

---
+++ super/thing/leave_fact_by_important_thing/time_or_old_child/man/young_eye/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_thing_for_way(str_arg):
+    little_fact(str_arg)
+    print('group_and_same_way')
+
+def little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_thing_for_way('hand')
