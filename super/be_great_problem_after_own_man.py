ba66af39ba9d69529570c1a774c76443b2c5c3b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:04:01 -0400

    "rm: come super/fact/world.py, super/part/old_case/ask_first_problem/big_hand/bad_day_or_company.py, super/child/group_and_way/new_government/number.py and super/be_great_problem_after_own_man.py for able year"

---
+++ super/be_great_problem_after_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem_and_world(str_arg):
+    point(str_arg)
+    print('person')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem_and_world('able_government')
---
+++ super/child/group_and_way/new_government/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_year(str_arg):
+    long_week(str_arg)
+    print('part_or_woman')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_year('number_and_way')
