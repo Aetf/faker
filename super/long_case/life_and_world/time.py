212275e9c097c5f693df536b50cd93843c1770ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:14:23 -0400

    "add: give super/long_case/life_and_world/time.py on last man"

---
+++ super/long_case/life_and_world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_point(str_arg):
+    last_part(str_arg)
+    print('thing')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_point('know_high_man_with_new_week')
