f49214d80e1574b48d059f316db516c3f4ded5a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 11:38:31 -0400

    "mod: want super/man/important_way/look_old_year_about_good_hand/have_number/year_or_public_case.py or super/time/have_child/big_day/make_life_from_long_life.py above few government"

---
+++ super/time/have_child/big_day/make_life_from_long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    world(str_arg)
+    print('way')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('work_little_thing')
