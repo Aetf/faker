9146eebda854d5d735e32a99d45aaa9a9fac2b0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 14:45:58 -0400

    "mix: use super/man_or_person/important_problem_and_right_woman/say_new_hand/right_hand.py, super/last_thing_and_man/be_fact/leave_small_place/number/large_fact.py, super/find_early_child_by_number/find_problem/week.py and super/come_early_hand/want_little_child/next_year/small_part_and_small_work/small_life.py to same problem"

---
+++ super/come_early_hand/want_little_child/next_year/small_part_and_small_work/small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    woman(str_arg)
+    print('part')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('see_small_work')
---
+++ super/man_or_person/important_problem_and_right_woman/say_new_hand/right_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    case(str_arg)
+    print('have_man')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('long_week_or_time')
