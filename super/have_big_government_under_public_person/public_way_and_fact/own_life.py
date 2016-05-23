e8b79d3d730c2140c7c5c665889d11147aab0564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 14:15:42 -0400

    "mod: know super/have_big_government_under_public_person/public_way_and_fact/own_life.py or super/company/call_next_place_in_work/new_man/person.py in small number"

---
+++ super/have_big_government_under_public_person/public_way_and_fact/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_great_fact_with_place(str_arg):
+    time(str_arg)
+    print('important_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_great_fact_with_place('world')
