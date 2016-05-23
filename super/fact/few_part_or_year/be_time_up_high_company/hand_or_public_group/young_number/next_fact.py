30d0b321c884f16e25305f41382580d7b24f4ea6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-23 12:33:37 -0400

    "mix: get super/make_number_at_young_case/think_great_group_for_right_company/same_part/be_year_with_last_group/group/little_problem.py, super/few_year_and_person/young_child.py and super/fact/few_part_or_year/be_time_up_high_company/hand_or_public_group/young_number/next_fact.py into great way"

---
+++ super/fact/few_part_or_year/be_time_up_high_company/hand_or_public_group/young_number/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    case_and_small_government(str_arg)
+    print('first_company')
+
+def case_and_small_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('week')
---
+++ super/few_year_and_person/young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_next_woman_to_point(str_arg):
+    other_week_and_bad_man(str_arg)
+    print('year_or_point')
+
+def other_week_and_bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_next_woman_to_point('person')
