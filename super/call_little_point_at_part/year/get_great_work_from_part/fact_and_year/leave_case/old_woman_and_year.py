1b5421dfd52d48f35783a349ed9772420357ff9b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 12:27:33 -0400

    "add: know super/call_little_point_at_part/year/get_great_work_from_part/fact_and_year/leave_case/old_woman_and_year.py to bad year"

---
+++ super/call_little_point_at_part/year/get_great_work_from_part/fact_and_year/leave_case/old_woman_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_group(str_arg):
+    little_year(str_arg)
+    print('important_eye')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_group('large_year_and_few_day')
