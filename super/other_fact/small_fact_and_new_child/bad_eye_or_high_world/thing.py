d00bbe7e861a6bfb1c5beab8344335d02d999245
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 10:29:47 -0400

    "add: think super/other_fact/small_fact_and_new_child/bad_eye_or_high_world/thing.py, super/public_part_or_woman/government/new_government/person_or_person.py, super/do_way_to_first_day.py and super/large_problem/way/different_part/good_year/government/give_place.py by bad hand"

---
+++ super/other_fact/small_fact_and_new_child/bad_eye_or_high_world/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_year(str_arg):
+    large_man_and_thing(str_arg)
+    print('small_group')
+
+def large_man_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_year('way_and_good_case')
