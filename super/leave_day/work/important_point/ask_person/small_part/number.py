1871122d8caec5252defe7624546780fb500d461
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 13:04:20 -0400

    "rm: do super/leave_day/work/important_point/ask_person/small_part/number.py after large time"

---
+++ super/leave_day/work/important_point/ask_person/small_part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_work(str_arg):
+    leave_young_group(str_arg)
+    print('use_group')
+
+def leave_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_work('work')
