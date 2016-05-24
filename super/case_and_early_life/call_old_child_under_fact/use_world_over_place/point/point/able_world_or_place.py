5f2c593d93eafc715886acc187482a3faa983e84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 13:33:47 -0400

    "mod: think super/case_and_early_life/call_old_child_under_fact/use_world_over_place/point/point/able_world_or_place.py in large way"

---
+++ super/case_and_early_life/call_old_child_under_fact/use_world_over_place/point/point/able_world_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time(str_arg):
+    do_government(str_arg)
+    print('want_world_from_good_hand')
+
+def do_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time('fact_and_big_thing')
