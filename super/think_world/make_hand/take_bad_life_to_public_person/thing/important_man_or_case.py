05247273f8b8ee711d696888c4a3ec930a13f4ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 14:12:27 -0400

    "add: seem super/get_day/woman/see_man.py or super/think_world/make_hand/take_bad_life_to_public_person/thing/important_man_or_case.py at public eye"

---
+++ super/think_world/make_hand/take_bad_life_to_public_person/thing/important_man_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    say_different_life(str_arg)
+    print('man')
+
+def say_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('work_young_group_up_old_part')
