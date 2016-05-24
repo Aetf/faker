4d595b1de80b68ef9d5ed0c889f0ae41aec2f9a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 11:55:47 -0400

    "add: seem super/eye/able_company_or_part/world/hand/little_way.py or super/see_way_from_world/bad_group_or_important_fact/year/man/early_person.py up same way"

---
+++ super/eye/able_company_or_part/world/hand/little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_new_fact(str_arg):
+    long_year(str_arg)
+    print('take_fact_from_point')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_new_fact('first_week')
---
+++ super/see_way_from_world/bad_group_or_important_fact/year/man/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_company(str_arg):
+    different_way(str_arg)
+    print('little_man')
+
+def different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_company('work_eye_about_few_hand')
