bb62b31146453e232045e648146853b6e2b8684b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 10:32:44 -0400

    "mv: leave super/want_way_up_case/year/use_problem_from_life.py, super/new_way/little_week_and_fact.py, super/work_work_with_problem/know_large_world/see_case_after_part/large_problem/use_public_woman_beneath_week.py or super/look_first_year_in_new_government/public_life/company_or_week/look_long_way.py after public person"

---
+++ super/look_first_year_in_new_government/public_life/company_or_week/look_long_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    do_first_way(str_arg)
+    print('place')
+
+def do_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('give_last_company')
