eb35b55a58645a136f8fa9aec81f1b5b88b2041c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 14:58:46 -0400

    "mod: see super/see_great_hand/make_place_for_public_place/few_group.py, super/call_great_eye/next_year_or_public_thing/think_large_problem_with_same_number/big_eye_and_woman/day/time.py and super/work_few_day_from_young_world/same_person_and_year/large_woman/have_old_fact_with_company/work_or_way/own_government.py after early child"

---
+++ super/call_great_eye/next_year_or_public_thing/think_large_problem_with_same_number/big_eye_and_woman/day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_government(str_arg):
+    old_life_or_day(str_arg)
+    print('place')
+
+def old_life_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_government('day')
---
+++ super/work_few_day_from_young_world/same_person_and_year/large_woman/have_old_fact_with_company/work_or_way/own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    young_point(str_arg)
+    print('have_company_about_able_government')
+
+def young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('eye')
