f1db3773b8fc99ab41ef62845b2a115a38b42dcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 14:53:05 -0400

    "mod: see super/leave_hand/part/time.py for small problem"

---
+++ super/leave_hand/part/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    eye_and_great_world(str_arg)
+    print('hand_and_last_day')
+
+def eye_and_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('number_and_new_company')
