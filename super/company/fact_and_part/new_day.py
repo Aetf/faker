d26c7eb9813b3dfccdcd70ff0ced054080737444
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 12:32:50 -0400

    "mv: get super/company/fact_and_part/new_day.py beneath great place"

---
+++ super/company/fact_and_part/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    point(str_arg)
+    print('other_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('small_problem_and_high_hand')
