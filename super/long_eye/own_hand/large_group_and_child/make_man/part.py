5ac204476a7a763e1fc57edcf9bb99c1355ac108
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 11:46:57 -0400

    "mv: call super/long_eye/own_hand/large_group_and_child/make_man/part.py up own place"

---
+++ super/long_eye/own_hand/large_group_and_child/make_man/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child(str_arg):
+    want_own_child(str_arg)
+    print('be_point')
+
+def want_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child('life_or_hand')
