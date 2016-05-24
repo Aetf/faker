7f00597ab45107f39fdd8a319973cbd42f6890b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 11:23:06 -0400

    "rm: say super/large_hand_and_hand/new_woman/company/important_point_or_man/man.py by bad woman"

---
+++ super/large_hand_and_hand/new_woman/company/important_point_or_man/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_next_child(str_arg):
+    long_day_and_little_thing(str_arg)
+    print('little_fact')
+
+def long_day_and_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_next_child('place')
