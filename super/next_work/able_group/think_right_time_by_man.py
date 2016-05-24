8c952f376cff6ed6b6975b02f0fee766c6aa2010
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 11:30:20 -0400

    "mod: say super/case/ask_part_of_government/call_large_year/important_place.py, super/young_number/high_problem/high_place.py or super/next_work/able_group/think_right_time_by_man.py under early fact"

---
+++ super/next_work/able_group/think_right_time_by_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    life_or_able_company(str_arg)
+    print('high_man')
+
+def life_or_able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('last_eye')
---
+++ super/young_number/high_problem/high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_thing(str_arg):
+    number_or_long_person(str_arg)
+    print('work_week_under_right_government')
+
+def number_or_long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_thing('hand')
