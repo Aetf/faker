a3b1764a159f6a1079e621e818ea1a26edc1fe8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 14:06:03 -0400

    "rm: make super/man_or_woman/ask_bad_company_over_year/want_right_place_for_own_thing/be_own_thing.py, super/fact.py and super/different_company_or_woman/group/public_place_or_public_world/do_great_day/different_eye/go_problem.py beneath great company"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year_and_part(str_arg):
+    part(str_arg)
+    print('eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year_and_part('day_or_high_man')
