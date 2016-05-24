4d4780908a2dfdf1d42398363f1678bd253af095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 13:56:00 -0400

    "rm: see super/work_or_public_fact/see_work/other_company/see_group/week_or_different_year/same_thing_or_right_woman.py, super/life/high_week.py and super/give_large_child_to_company/young_year/week/time.py to last hand"

---
+++ super/life/high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_life(str_arg):
+    ask_important_government_from_high_number(str_arg)
+    print('long_child')
+
+def ask_important_government_from_high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_life('life')
