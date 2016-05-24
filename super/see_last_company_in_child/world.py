b546f0e86fb08d101e479ce856067797f5b7817a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-24 11:08:27 -0400

    "mix: take super/see_last_company_in_child/world.py and super/take_thing/work_public_number.py on large part"

---
+++ super/see_last_company_in_child/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day_and_life(str_arg):
+    bad_hand(str_arg)
+    print('next_part')
+
+def bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day_and_life('look_small_problem')
---
+++ super/take_thing/work_public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government(str_arg):
+    early_thing(str_arg)
+    print('same_way_and_long_time')
+
+def early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government('young_fact')
