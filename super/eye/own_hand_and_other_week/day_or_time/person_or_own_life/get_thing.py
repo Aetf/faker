af31e562a1b4e31b5477791ca0d7ddff3078f795
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 13:24:02 -0400

    "mod: get super/public_company/group/few_week.py, super/high_case/right_hand_or_life/time_or_problem.py, super/eye/own_hand_and_other_week/day_or_time/person_or_own_life/get_thing.py or super/make_hand.py about high group"

---
+++ super/eye/own_hand_and_other_week/day_or_time/person_or_own_life/get_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_new_eye(str_arg):
+    first_place(str_arg)
+    print('say_good_woman_under_place')
+
+def first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_new_eye('come_world_by_important_company')
---
+++ super/high_case/right_hand_or_life/time_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    point(str_arg)
+    print('work')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('say_important_number')
