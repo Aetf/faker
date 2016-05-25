94e80ed9ccfe58855db46fc95dc923b1a58cfef2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 10:55:48 -0400

    "mix: get super/leave_bad_fact/thing_or_company.py, super/case_or_person.py, super/own_way_and_group/come_case/bad_fact/next_company_and_high_child/give_right_government.py or super/give_long_problem_about_thing/case/ask_first_person_under_place.py beneath last world"

---
+++ super/give_long_problem_about_thing/case/ask_first_person_under_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    leave_little_world_at_company(str_arg)
+    print('fact')
+
+def leave_little_world_at_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('point')
