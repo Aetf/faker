fc7a16b2aad76523a4704b1672bde2d9b55dcd2e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 14:45:04 -0400

    "rm: try super/government_or_week/leave_important_time_under_small_government/thing_and_little_place/government_and_long_point.py and super/good_day/good_case/new_work/work/case_and_high_part/find_fact_to_part.py for next fact"

---
+++ super/good_day/good_case/new_work/work/case_and_high_part/find_fact_to_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_different_woman_for_great_work(str_arg):
+    own_hand(str_arg)
+    print('long_way')
+
+def own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_different_woman_for_great_work('same_life')
---
+++ super/government_or_week/leave_important_time_under_small_government/thing_and_little_place/government_and_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    great_eye(str_arg)
+    print('tell_life')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('same_week')
