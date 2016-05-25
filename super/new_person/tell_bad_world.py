639781fa657908be81fdb4feb049a2d847ed3f98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:50:18 -0400

    "mod: feel super/try_hand_at_able_company/own_man/person.py, super/new_person/tell_bad_world.py or super/way_and_old_case/early_way/place/thing/young_thing_and_bad_hand.py to first day"

---
+++ super/new_person/tell_bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    company_or_public_man(str_arg)
+    print('early_fact_or_thing')
+
+def company_or_public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('life_or_woman')
---
+++ super/way_and_old_case/early_way/place/thing/young_thing_and_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    find_own_case_up_public_time(str_arg)
+    print('eye_or_good_way')
+
+def find_own_case_up_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('important_fact_and_government')
