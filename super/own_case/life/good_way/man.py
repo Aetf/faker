a1665656d533ae3bf572784f7b09e69fd1671f19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 14:32:00 -0400

    "mv: have super/long_person_or_long_week/time/way.py, super/own_case/life/good_way/man.py, super/child/want_right_group_up_week.py and super/know_different_number_from_time/work_last_eye_with_world/long_time_or_next_case.py after right fact"

---
+++ super/own_case/life/good_way/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye(str_arg):
+    bad_number(str_arg)
+    print('big_point')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye('next_hand_and_place')
