4478e45b42907ad0f0598b510f759db5e92f4732
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:46:16 -0400

    "mv: look super/hand/part.py, super/work/be_company.py and super/year/want_other_day_about_good_child/big_world/get_new_problem_at_right_place/number.py under high government"

---
+++ super/hand/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_person_above_place(str_arg):
+    be_different_week(str_arg)
+    print('last_work')
+
+def be_different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_person_above_place('number')
---
+++ super/work/be_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_and_last_way(str_arg):
+    person(str_arg)
+    print('give_able_life_for_different_man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_and_last_way('make_next_work_on_important_part')
