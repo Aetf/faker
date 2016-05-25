7e763cc65be1337726900de0736c2f759406da03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:32:59 -0400

    "rm: be super/other_problem.py, super/have_next_way/ask_able_thing_under_young_fact/man_and_first_time/look_first_point_to_hand/go_fact_for_great_year/ask_thing.py, super/eye_and_young_part/ask_number_over_year.py and super/be_new_day_with_day.py up bad person"

---
+++ super/have_next_way/ask_able_thing_under_young_fact/man_and_first_time/look_first_point_to_hand/go_fact_for_great_year/ask_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    company(str_arg)
+    print('tell_first_week_up_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('new_part')
