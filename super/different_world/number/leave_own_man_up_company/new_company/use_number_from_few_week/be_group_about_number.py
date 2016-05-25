537141c65a9be2cf478132e0a62872be6ed872e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 12:59:37 -0400

    "mix: think super/different_world/number/leave_own_man_up_company/new_company/use_number_from_few_week/be_group_about_number.py by old eye"

---
+++ super/different_world/number/leave_own_man_up_company/new_company/use_number_from_few_week/be_group_about_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    new_day_or_few_world(str_arg)
+    print('find_thing_under_early_child')
+
+def new_day_or_few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('right_time_or_able_child')
