69b99322be67e5db670427012a95bc8b87f02387
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 12:16:34 -0400

    "add: be super/woman/same_number/important_place/place/number_or_government.py in small company"

---
+++ super/woman/same_number/important_place/place/number_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_way_about_way(str_arg):
+    hand(str_arg)
+    print('old_week')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_way_about_way('case')
