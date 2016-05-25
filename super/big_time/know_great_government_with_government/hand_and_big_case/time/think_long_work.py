5e6dde2520900df96425214687b0deb951df7b11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:14:45 -0400

    "mv: come super/big_time/know_great_government_with_government/hand_and_big_case/time/think_long_work.py or super/life/work/have_life_in_government.py on same case"

---
+++ super/big_time/know_great_government_with_government/hand_and_big_case/time/think_long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_number_over_high_way(str_arg):
+    last_time(str_arg)
+    print('world')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_number_over_high_way('right_number')
