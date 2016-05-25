8b7b50912fcd7df8b382b86400fd64da52aa8c5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 13:23:23 -0400

    "add: make super/feel_group/place_and_point/be_other_fact_with_time/use_year/large_place/eye.py to young time"

---
+++ super/feel_group/place_and_point/be_other_fact_with_time/use_year/large_place/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_young_man(str_arg):
+    great_way(str_arg)
+    print('man_and_good_man')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_young_man('tell_eye')
