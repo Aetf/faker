94f5fdfd7ad41156e0b5a1561143451d74c0f947
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 12:46:08 -0400

    "rm: leave super/problem.py for bad number"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_right_week(str_arg):
+    way(str_arg)
+    print('little_life')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_right_week('try_part_at_case')
