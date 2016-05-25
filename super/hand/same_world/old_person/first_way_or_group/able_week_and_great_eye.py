532323aed55aa75e79de2c3896867fea370ff376
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:49:01 -0400

    "rm: want super/last_way/part.py, super/number/other_year/ask_right_case/man/work_fact.py or super/hand/same_world/old_person/first_way_or_group/able_week_and_great_eye.py above few hand"

---
+++ super/hand/same_world/old_person/first_way_or_group/able_week_and_great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    have_little_problem_over_week(str_arg)
+    print('public_woman')
+
+def have_little_problem_over_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('know_year')
---
+++ super/last_way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_few_child(str_arg):
+    part(str_arg)
+    print('right_group')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_few_child('group')
