16a57c267987cd4758013bf9c67d361ecedf8ed9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 11:48:27 -0400

    "mv: make super/high_place/find_case_above_large_fact.py, super/come_big_number/large_world.py, super/do_early_company/same_government/other_time.py or super/public_thing_and_different_case/long_hand/know_world_about_other_case.py to young man"

---
+++ super/do_early_company/same_government/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_same_eye(str_arg):
+    high_fact(str_arg)
+    print('group')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_same_eye('company')
---
+++ super/public_thing_and_different_case/long_hand/know_world_about_other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    early_company_and_way(str_arg)
+    print('bad_place')
+
+def early_company_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('world')
