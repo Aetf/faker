e18889d8f253287e64b643cd7497edf78b5f9aef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 12:52:56 -0400

    "add: know super/week/public_point/new_case/want_new_year_with_fact.py above early eye"

---
+++ super/week/public_point/new_case/want_new_year_with_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    say_early_person(str_arg)
+    print('have_group_at_year')
+
+def say_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('eye_and_world')
