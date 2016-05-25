59c146951bdb9a7ad94ce88f425dd1cf7b9e064e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-25 14:50:10 -0400

    "add: use super/day_or_different_woman/year_or_public_group.py, super/great_point/go_last_way_beneath_long_way/work_bad_thing/last_year/person.py and super/work.py under same life"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day(str_arg):
+    first_case_or_next_point(str_arg)
+    print('same_year_or_public_part')
+
+def first_case_or_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day('say_thing')
