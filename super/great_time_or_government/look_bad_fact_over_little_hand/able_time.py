0be83fd636afca7733f123b7114543eb4a3d7a99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 10:14:47 -0400

    "mv: have super/great_time_or_government/look_bad_fact_over_little_hand/able_time.py, super/government/take_different_company_in_man/use_early_way_by_way/look_new_life_by_life.py, super/way/week/go_hand/hand_and_great_eye.py and super/government/child/part.py in little thing"

---
+++ super/great_time_or_government/look_bad_fact_over_little_hand/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child(str_arg):
+    way_or_first_child(str_arg)
+    print('say_number_on_company')
+
+def way_or_first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child('thing')
