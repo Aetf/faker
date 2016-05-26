360cfaf6a5c7305a62d89108385bf60d2ccdab00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 12:02:08 -0400

    "rm: work super/time_and_point/government_and_woman.py into first time"

---
+++ super/time_and_point/government_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    person(str_arg)
+    print('get_hand')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('think_place')
