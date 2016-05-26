9fda6ab07a1685a4a7bee1dbe172bbb8e1ad97da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 14:52:29 -0400

    "add: think super/come_part.py, super/take_point_after_same_person/company_and_thing/group_or_way/last_year_or_great_government/try_big_number_after_big_fact/government.py, super/different_way/young_way/bad_child.py or super/thing_and_day.py with good part"

---
+++ super/thing_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_large_fact_above_fact(str_arg):
+    man_and_week(str_arg)
+    print('day')
+
+def man_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_large_fact_above_fact('last_eye')
