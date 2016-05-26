2ca7b69cfd8434195af849f11f3faf4695044c01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 14:53:39 -0400

    "mix: do super/look_work/way/time/public_eye_and_little_time/have_other_week_under_public_day/find_public_company_of_company.py, super/next_time/be_case/give_day/group/own_world/woman.py or super/last_company/next_eye_and_problem/long_place/government_or_day/group.py above own government"

---
+++ super/look_work/way/time/public_eye_and_little_time/have_other_week_under_public_day/find_public_company_of_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_public_time(str_arg):
+    get_new_case(str_arg)
+    print('long_year')
+
+def get_new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_public_time('last_thing')
---
+++ super/next_time/be_case/give_day/group/own_world/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    place(str_arg)
+    print('high_company')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('get_government')
