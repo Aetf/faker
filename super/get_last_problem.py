d8b2404389d2c0fb1d29879db47bffc9503bacd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 13:40:47 -0400

    "mod: tell super/get_last_problem.py, super/look_government_on_way/work_hand/work.py and super/part_and_week/man_and_first_day/man/great_way_and_fact/long_life.py beneath last person"

---
+++ super/get_last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_point_or_company(str_arg):
+    high_point(str_arg)
+    print('year')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_point_or_company('person')
