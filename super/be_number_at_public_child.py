36906df0e6e8c99c686663eed00ad39f5c71cfba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 10:01:04 -0400

    "rm: come super/be_number_at_public_child.py from different company"

---
+++ super/be_number_at_public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_number(str_arg):
+    different_life(str_arg)
+    print('year')
+
+def different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_number('work_last_part_by_government')
