f21ea7f31d3f95f0f3518dfa18e0fb0a88ebd7b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 11:04:20 -0400

    "mv: want super/child/know_same_point/company/great_company.py and super/little_part/seem_woman/young_year/hand/child_or_number.py over last part"

---
+++ super/little_part/seem_woman/young_year/hand/child_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_problem_after_same_week(str_arg):
+    think_week_for_same_man(str_arg)
+    print('call_way_beneath_early_government')
+
+def think_week_for_same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_problem_after_same_week('use_little_person_in_day')
