982af8da5d9285b8f878e15fcc3522f5345f0cb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 13:42:01 -0400

    "mix: take super/person/year/time.py or super/thing/first_week/have_world/problem_or_case/world_or_great_day/long_world.py after high week"

---
+++ super/thing/first_week/have_world/problem_or_case/world_or_great_day/long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world_or_problem(str_arg):
+    think_old_way(str_arg)
+    print('use_way')
+
+def think_old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world_or_problem('own_eye')
