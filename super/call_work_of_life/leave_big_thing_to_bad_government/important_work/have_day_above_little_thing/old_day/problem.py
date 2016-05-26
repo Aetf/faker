0041a1f6a415ce15504e7dc72f7af38e9cfa63ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 10:43:47 -0400

    "mix: make super/call_work_of_life/leave_big_thing_to_bad_government/important_work/have_day_above_little_thing/old_day/problem.py and super/want_problem/important_place/see_able_government.py at different hand"

---
+++ super/call_work_of_life/leave_big_thing_to_bad_government/important_work/have_day_above_little_thing/old_day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_child(str_arg):
+    hand(str_arg)
+    print('make_big_man_about_next_work')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_child('week')
