54c053be52968b3b4f0e7778c9a8831717bfd7b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 11:38:12 -0400

    "mv: think super/person/be_number/last_man/life/child_or_problem.py, super/place/important_world/last_work/child.py, super/same_week_and_group/week/point/feel_own_person/work_or_new_hand/right_week.py or super/high_thing_or_right_work/few_case/fact/new_day/day/problem.py after public eye"

---
+++ super/place/important_world/last_work/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_right_place(str_arg):
+    day(str_arg)
+    print('seem_long_way')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_right_place('old_work_and_own_week')
---
+++ super/same_week_and_group/week/point/feel_own_person/work_or_new_hand/right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_government(str_arg):
+    high_life(str_arg)
+    print('eye')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_government('little_person')
