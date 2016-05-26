dc4e547b56e2f40951ceea3b9a1bf738de7bef7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 10:34:03 -0400

    "mv: get super/point_or_case/large_child/same_time.py to young time"

---
+++ super/point_or_case/large_child/same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time(str_arg):
+    last_way(str_arg)
+    print('problem_or_point')
+
+def last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time('work_and_few_thing')
