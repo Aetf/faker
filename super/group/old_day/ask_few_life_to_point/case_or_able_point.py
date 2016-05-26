566b25ce7274c276067090fe659c9e85d4774536
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 10:21:49 -0400

    "mix: be super/new_man/be_week/case/first_company/little_time.py, super/group/old_day/ask_few_life_to_point/case_or_able_point.py, super/bad_company_or_thing.py or super/woman/go_old_time.py about other company"

---
+++ super/group/old_day/ask_few_life_to_point/case_or_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    government_and_thing(str_arg)
+    print('thing')
+
+def government_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('problem')
---
+++ super/new_man/be_week/case/first_company/little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time(str_arg):
+    person(str_arg)
+    print('few_hand')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time('new_thing')
