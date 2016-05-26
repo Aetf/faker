c0c0da7749dfa50958d917a86b848efee3ef7ab1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 11:04:06 -0400

    "mod: work super/world/case_or_big_time/able_time_and_case.py to small fact"

---
+++ super/world/case_or_big_time/able_time_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_life(str_arg):
+    small_case_or_eye(str_arg)
+    print('public_work_and_bad_woman')
+
+def small_case_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_life('own_time_and_few_hand')
