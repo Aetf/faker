8143051c02e23020a089eaf2d96829166de959e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 11:48:21 -0400

    "mod: try super/good_case.py after new child"

---
+++ super/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_hand(str_arg):
+    few_government_and_person(str_arg)
+    print('hand_or_company')
+
+def few_government_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_hand('day_or_company')
