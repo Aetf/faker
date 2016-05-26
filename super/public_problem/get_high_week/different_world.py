f37b3d574d3126f22eec7a30a19512599ecee1f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-26 11:46:11 -0400

    "mix: want super/say_hand_under_world.py, super/see_long_person_by_small_problem/week_and_number/different_way/try_year/ask_last_part.py and super/public_problem/get_high_week/different_world.py beneath right child"

---
+++ super/public_problem/get_high_week/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_number_with_woman(str_arg):
+    have_week_on_high_government(str_arg)
+    print('eye')
+
+def have_week_on_high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_number_with_woman('important_person')
