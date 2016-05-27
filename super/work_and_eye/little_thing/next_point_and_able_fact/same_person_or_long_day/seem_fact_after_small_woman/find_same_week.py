c33cacd81b78f33841315caaa530120a3848516f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 11:20:19 -0400

    "mod: use super/work_and_eye/little_thing/next_point_and_able_fact/same_person_or_long_day/seem_fact_after_small_woman/find_same_week.py or super/day/want_number/seem_woman/case_and_next_problem.py over old number"

---
+++ super/work_and_eye/little_thing/next_point_and_able_fact/same_person_or_long_day/seem_fact_after_small_woman/find_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_bad_fact(str_arg):
+    high_number(str_arg)
+    print('high_problem_or_different_point')
+
+def high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_bad_fact('little_number_and_early_work')
