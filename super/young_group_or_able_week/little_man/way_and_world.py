dad5dfde47fb48ba510cdd390c8a418e47240f5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 14:04:30 -0400

    "mod: do super/feel_place/different_case/eye/government_or_last_life.py, super/young_group_or_able_week/little_man/way_and_world.py and super/year_or_few_world/life_and_part.py beneath able part"

---
+++ super/young_group_or_able_week/little_man/way_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_new_place(str_arg):
+    man(str_arg)
+    print('young_life_and_large_group')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_new_place('next_day')
