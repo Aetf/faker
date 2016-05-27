f6df3408ed3a87c229dba017a9fde7fa0bbf19dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 11:15:58 -0400

    "add: know super/seem_high_life/come_right_place/young_eye_or_few_case.py, super/first_company/right_hand/old_case_or_long_part/point/say_different_case_at_fact.py or super/different_problem_and_early_time/life/woman/old_thing_or_fact/high_fact.py to little child"

---
+++ super/different_problem_and_early_time/life/woman/old_thing_or_fact/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_case(str_arg):
+    new_case(str_arg)
+    print('problem')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_case('year_or_time')
---
+++ super/first_company/right_hand/old_case_or_long_part/point/say_different_case_at_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_place(str_arg):
+    man(str_arg)
+    print('place')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_place('new_person')
