b4efde4bd781553a3b8089693b505ad3e3343caa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 10:15:23 -0400

    "add: look super/work_thing_by_last_day/next_government/come_next_number_up_hand/day_or_man/seem_other_work_up_person.py with next part"

---
+++ super/work_thing_by_last_day/next_government/come_next_number_up_hand/day_or_man/seem_other_work_up_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing(str_arg):
+    year(str_arg)
+    print('try_public_place')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing('work_and_same_case')
