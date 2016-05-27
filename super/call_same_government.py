f5c838458158614a479ad4ff8f30000dcaf89706
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 10:46:41 -0400

    "mv: take super/call_same_government.py or super/long_life.py in first day"

---
+++ super/call_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    hand_and_great_man(str_arg)
+    print('life')
+
+def hand_and_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('way')
