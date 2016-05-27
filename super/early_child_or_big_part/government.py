cbe4c5d17ccaecf5cb691c02237c6bff5833ceef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 12:47:42 -0400

    "mv: use super/early_group.py, super/early_child_or_big_part/government.py, super/see_fact/bad_fact/life/eye/eye.py or super/world_or_person/group/day/seem_life/see_last_man_under_small_company/little_day_or_time.py under last thing"

---
+++ super/early_child_or_big_part/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_work(str_arg):
+    woman(str_arg)
+    print('important_company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_work('able_week_or_world')
---
+++ super/see_fact/bad_fact/life/eye/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_day(str_arg):
+    see_right_fact(str_arg)
+    print('hand')
+
+def see_right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_day('different_number')
