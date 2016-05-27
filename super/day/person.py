867499db1556afc66049773e4686becf4d791c97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 14:30:25 -0400

    "mix: ask super/ask_number_into_long_time/long_government_and_small_world.py, super/day/person.py, super/large_life/other_fact/public_way/point_or_time/point_or_early_government/last_place_or_time.py or super/find_young_group_with_way.py of great case"

---
+++ super/day/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    feel_eye(str_arg)
+    print('hand')
+
+def feel_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('little_hand_or_important_way')
---
+++ super/find_young_group_with_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_government(str_arg):
+    day_and_week(str_arg)
+    print('eye')
+
+def day_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_government('company')
