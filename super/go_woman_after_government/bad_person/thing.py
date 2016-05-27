59e3f9766a134690001b7cb3e660558d939f91f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 14:26:17 -0400

    "mod: have super/leave_young_case_to_large_woman/large_child/eye.py, super/little_problem/bad_day_or_own_problem/work.py or super/go_woman_after_government/bad_person/thing.py after early group"

---
+++ super/go_woman_after_government/bad_person/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company(str_arg):
+    number(str_arg)
+    print('little_week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company('public_way_and_long_hand')
---
+++ super/leave_young_case_to_large_woman/large_child/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way_and_year(str_arg):
+    big_government(str_arg)
+    print('woman')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way_and_year('own_person_or_life')
