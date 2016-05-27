85384074a1d3b11087cb85a713c74032fea642c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 12:04:07 -0400

    "add: ask super/person/first_case_and_right_group/different_world/next_fact_or_new_hand.py up last woman"

---
+++ super/person/first_case_and_right_group/different_world/next_fact_or_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_new_problem(str_arg):
+    see_time(str_arg)
+    print('great_life')
+
+def see_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_new_problem('little_woman_or_week')
