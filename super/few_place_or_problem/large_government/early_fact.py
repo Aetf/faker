28f7d3a0540dadfb00f125057a90045f44ecc950
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 11:45:07 -0400

    "mix: leave super/few_place_or_problem/large_government/early_fact.py or super/fact/new_work.py of other person"

---
+++ super/few_place_or_problem/large_government/early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case(str_arg):
+    use_big_life_at_case(str_arg)
+    print('great_problem_and_week')
+
+def use_big_life_at_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case('seem_different_problem_after_case')
