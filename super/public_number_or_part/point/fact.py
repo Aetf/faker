d26871806b7d193b94d66f8986a016f84f0315ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 11:15:32 -0400

    "add: find super/public_number_or_part/point/fact.py or super/fact_and_year/child/way/company/look_able_problem/man.py to few company"

---
+++ super/public_number_or_part/point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    different_life(str_arg)
+    print('get_last_company')
+
+def different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('last_number')
