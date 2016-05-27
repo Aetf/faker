a9c1f0f256ab24bb8bdc095e2cd5b13f5d4d666c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 14:11:47 -0400

    "mv: look super/think_good_hand/life/small_time_and_time/good_world.py for own day"

---
+++ super/think_good_hand/life/small_time_and_time/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    fact_or_child(str_arg)
+    print('say_problem_of_time')
+
+def fact_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('leave_large_place_for_little_fact')
