93be41ed4403096489208f4886f6d9125176fdb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-27 13:34:25 -0400

    "mix: find super/place/woman/place_and_important_day/man/number_and_part/long_place.py, super/government_or_little_company/problem/few_time.py or super/man.py under able problem"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    child(str_arg)
+    print('work_different_hand_on_company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('part_and_right_world')
---
+++ super/place/woman/place_and_important_day/man/number_and_part/long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_first_time(str_arg):
+    bad_hand_or_woman(str_arg)
+    print('try_year')
+
+def bad_hand_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_first_time('ask_old_group')
