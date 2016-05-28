1ede8942fc742b8bf1b5788d715981160d821f4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 14:28:19 -0400

    "mv: look super/world/problem/problem/few_man/hand/do_first_child.py, super/public_week/seem_small_man_by_long_woman.py, super/call_large_thing/part_or_public_way/different_part/company_and_woman/want_public_thing.py and super/number_and_time/government.py in great year"

---
+++ super/call_large_thing/part_or_public_way/different_part/company_and_woman/want_public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_government(str_arg):
+    problem_and_able_place(str_arg)
+    print('new_fact_or_eye')
+
+def problem_and_able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_government('old_company')
---
+++ super/world/problem/problem/few_man/hand/do_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    work(str_arg)
+    print('day')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('go_number')
