938e1f19cae33097a2a1716b308f19aef2e48763
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 10:20:37 -0400

    "mix: take super/small_eye/government/young_hand/give_place_up_woman.py, super/small_person/other_fact.py, super/work_public_person/ask_small_group_of_right_government/little_fact/want_next_fact/year.py or super/group/new_thing.py beneath able work"

---
+++ super/group/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_thing(str_arg):
+    other_government_and_good_child(str_arg)
+    print('first_point')
+
+def other_government_and_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_thing('woman_or_great_week')
---
+++ super/small_person/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    other_time_and_part(str_arg)
+    print('come_hand')
+
+def other_time_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('last_company_or_person')
