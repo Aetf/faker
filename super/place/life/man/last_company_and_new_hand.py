db70a4f31ff7112ec3da5d8c4ce1f65a6a4e280f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 11:45:40 -0400

    "add: come super/year/think_other_year_above_long_time.py, super/place/life/man/last_company_and_new_hand.py, super/work_or_woman/hand_or_number/thing_and_number/next_part/do_next_company/government.py and super/week/come_part_above_good_company.py over few case"

---
+++ super/place/life/man/last_company_and_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    try_small_week(str_arg)
+    print('go_bad_time_with_work')
+
+def try_small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('seem_fact_about_part')
---
+++ super/week/come_part_above_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_important_day(str_arg):
+    child(str_arg)
+    print('seem_year_in_work')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_important_day('year')
