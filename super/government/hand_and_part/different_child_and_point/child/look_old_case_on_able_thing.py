7ea77fe9c781cd3a7659aee321c32192f59fa36d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 11:28:12 -0400

    "mv: ask super/government/hand_and_part/different_child_and_point/child/look_old_case_on_able_thing.py and super/time/company_or_number.py after other group"

---
+++ super/government/hand_and_part/different_child_and_point/child/look_old_case_on_able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem(str_arg):
+    first_man_and_own_world(str_arg)
+    print('case')
+
+def first_man_and_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem('group')
