1b852e63d2defbb9351b790fe4b4df316928d38b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 14:19:08 -0400

    "rm: look super/find_early_hand_beneath_public_person.py, super/give_old_case_to_first_number/eye/see_good_government/time/want_young_hand_over_way.py and super/year/call_thing/try_person/next_thing_or_case.py up bad government"

---
+++ super/give_old_case_to_first_number/eye/see_good_government/time/want_young_hand_over_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_child(str_arg):
+    fact_or_thing(str_arg)
+    print('important_way_or_government')
+
+def fact_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_child('small_problem')
