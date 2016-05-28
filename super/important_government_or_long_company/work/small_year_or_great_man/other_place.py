ac162cf84e9c16e647c1dba20f961085659bafba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 12:33:26 -0400

    "mod: seem super/important_government_or_long_company/work/small_year_or_great_man/other_place.py at first way"

---
+++ super/important_government_or_long_company/work/small_year_or_great_man/other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_important_point(str_arg):
+    big_company(str_arg)
+    print('make_child')
+
+def big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_important_point('way')
