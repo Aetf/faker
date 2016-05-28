419220f58e48090299cc9559f990b76f735657a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 12:46:50 -0400

    "mv: work super/good_time.py by right way"

---
+++ super/good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    point(str_arg)
+    print('government')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('first_point')
