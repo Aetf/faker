61f54cbff1ffb6278185bd1bb31a81afde108ea4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 12:52:35 -0400

    "mv: know super/bad_man_and_large_child/find_last_day_up_large_time/big_child_and_week/look_hand_over_year.py or super/year.py at early part"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_right_life(str_arg):
+    say_world_about_big_government(str_arg)
+    print('point')
+
+def say_world_about_big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_right_life('year')
