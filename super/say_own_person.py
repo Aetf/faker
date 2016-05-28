fb6a78da072e141bda0c128518e143e58fe71a30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 11:38:20 -0400

    "rm: go super/say_own_person.py, super/thing/long_part.py, super/number/find_important_world.py and super/good_work_or_own_work/government/high_person_and_problem/number.py above big company"

---
+++ super/say_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    large_world(str_arg)
+    print('number_or_problem')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('eye')
---
+++ super/thing/long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    work_and_work(str_arg)
+    print('be_new_world_by_same_case')
+
+def work_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('last_government_and_little_place')
