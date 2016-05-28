1a505e1ab076b227cd76676c6e0805b80fbfd33b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 14:09:44 -0400

    "mod: give super/work_long_eye_at_world/woman/old_thing/first_number.py, super/look_child_on_own_thing/part/important_man/come_other_problem_in_same_eye/next_way.py, super/go_company/come_few_work.py and super/company/last_hand/important_week/year_and_number.py of high company"

---
+++ super/go_company/come_few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_world(str_arg):
+    next_number(str_arg)
+    print('little_person')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_world('new_time_and_other_man')
---
+++ super/work_long_eye_at_world/woman/old_thing/first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    week(str_arg)
+    print('week_and_government')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('call_public_life')
