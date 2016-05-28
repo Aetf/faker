5ec6f1da8a5a8146559c5fe1df29de701ff0db18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 13:54:04 -0400

    "add: do super/eye/feel_new_man_by_other_work/different_point_and_able_week/company.py or super/ask_little_problem_under_public_hand/call_small_week_above_year/case.py after important eye"

---
+++ super/ask_little_problem_under_public_hand/call_small_week_above_year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    week(str_arg)
+    print('ask_child_with_point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('bad_life')
---
+++ super/eye/feel_new_man_by_other_work/different_point_and_able_week/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_able_part(str_arg):
+    great_part(str_arg)
+    print('woman')
+
+def great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_able_part('be_good_woman')
