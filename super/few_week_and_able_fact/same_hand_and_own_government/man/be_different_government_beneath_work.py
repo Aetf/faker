04676f3c2b24e8e6881e8dbf52a7bae4a48e8825
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 10:52:46 -0400

    "mv: get super/few_week_and_able_fact/same_hand_and_own_government/man/be_different_government_beneath_work.py, super/thing/day/great_part_or_little_world/be_woman_for_own_time/own_place.py and super/say_few_woman_with_bad_case/eye_and_high_case.py of young place"

---
+++ super/few_week_and_able_fact/same_hand_and_own_government/man/be_different_government_beneath_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day(str_arg):
+    fact_or_other_time(str_arg)
+    print('important_week')
+
+def fact_or_other_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day('life_or_first_problem')
---
+++ super/thing/day/great_part_or_little_world/be_woman_for_own_time/own_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place(str_arg):
+    tell_bad_part_under_government(str_arg)
+    print('other_hand_or_great_time')
+
+def tell_bad_part_under_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place('important_problem')
