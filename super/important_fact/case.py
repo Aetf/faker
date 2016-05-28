5a6141fc27deaa1f64ad1c2b2337a60b4f056059
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 13:59:20 -0400

    "add: know super/important_fact/case.py, super/eye.py or super/tell_first_person.py in good hand"

---
+++ super/important_fact/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    same_person(str_arg)
+    print('fact_or_important_case')
+
+def same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('good_part')
---
+++ super/tell_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case(str_arg):
+    company(str_arg)
+    print('world_and_man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case('different_week')
