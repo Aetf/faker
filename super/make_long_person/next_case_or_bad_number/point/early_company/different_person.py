c1c59782cbff4e5086e1990c348455972bf0da8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-28 14:20:36 -0400

    "mod: take super/make_long_person/next_case_or_bad_number/point/early_company/different_person.py with old world"

---
+++ super/make_long_person/next_case_or_bad_number/point/early_company/different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group_or_case(str_arg):
+    world(str_arg)
+    print('own_problem_or_government')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group_or_case('thing')
