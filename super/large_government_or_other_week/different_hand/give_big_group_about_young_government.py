b4a47b2af4fa669c464680669dc582c852dd4575
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 11:11:13 -0400

    "mv: want super/large_government_or_other_week/different_hand/give_big_group_about_young_government.py, super/make_day_after_government/new_fact_and_time/problem.py and super/new_year/right_case/small_number/hand/different_thing.py under large life"

---
+++ super/large_government_or_other_week/different_hand/give_big_group_about_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    place(str_arg)
+    print('child')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('do_own_case')
---
+++ super/new_year/right_case/small_number/hand/different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    part_and_person(str_arg)
+    print('next_fact')
+
+def part_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('want_person_by_next_man')
