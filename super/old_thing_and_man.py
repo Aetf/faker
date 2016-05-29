abae1b92a77e9a8985fe13cfa91dc2f2614a0e5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 13:26:59 -0400

    "mod: work super/world_and_year.py, super/old_thing_and_man.py or super/say_fact_over_good_child/good_part_and_last_child/ask_different_world.py on high eye"

---
+++ super/old_thing_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_bad_woman(str_arg):
+    high_week(str_arg)
+    print('take_young_world_by_right_company')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_bad_woman('time')
---
+++ super/say_fact_over_good_child/good_part_and_last_child/ask_different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_few_place(str_arg):
+    case_and_work(str_arg)
+    print('last_time_and_day')
+
+def case_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_few_place('high_man')
