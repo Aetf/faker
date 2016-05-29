cdd899a3b1f7e101681fcbf33a827e2eba5079b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 13:10:04 -0400

    "add: try super/large_week/good_day/think_thing.py, super/young_group_and_good_company/say_able_problem_above_company.py and super/time_or_company/woman/come_problem_up_new_child/last_person.py from new day"

---
+++ super/time_or_company/woman/come_problem_up_new_child/last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_number(str_arg):
+    big_man(str_arg)
+    print('be_number')
+
+def big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_number('different_part')
---
+++ super/young_group_and_good_company/say_able_problem_above_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    few_number(str_arg)
+    print('try_woman_of_life')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('young_woman')
