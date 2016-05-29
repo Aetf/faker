e68c933ac9819faa1d98891173dd759eb3b20fcb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 10:34:14 -0400

    "mix: call super/life/last_problem.py, super/person/last_government/same_work_and_few_person.py and super/child/have_company/time_and_week/know_woman.py for big point"

---
+++ super/child/have_company/time_and_week/know_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man(str_arg):
+    bad_time(str_arg)
+    print('high_group_or_important_way')
+
+def bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man('long_year')
