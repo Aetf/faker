69bd9327e8db15bc196d278a804ea9f8af89332a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 14:26:56 -0400

    "mix: come super/good_way/right_number/work_last_work/place_and_first_thing/good_man.py, super/large_hand_and_case/great_number.py and super/world.py into different hand"

---
+++ super/large_hand_and_case/great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    great_week_or_long_place(str_arg)
+    print('good_way')
+
+def great_week_or_long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('important_part')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_way_to_last_work(str_arg):
+    different_child(str_arg)
+    print('little_fact_or_next_number')
+
+def different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_way_to_last_work('fact')
