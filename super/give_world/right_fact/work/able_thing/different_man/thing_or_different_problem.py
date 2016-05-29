a8f6ef50d7bce985bc7a5a68866ea3607d23f58c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 14:18:24 -0400

    "add: see super/person/young_point_and_different_woman.py, super/give_world/right_fact/work/able_thing/different_man/thing_or_different_problem.py or super/day_and_work/work_large_part_by_last_year.py to new life"

---
+++ super/give_world/right_fact/work/able_thing/different_man/thing_or_different_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    part_and_good_day(str_arg)
+    print('child')
+
+def part_and_good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('public_group')
