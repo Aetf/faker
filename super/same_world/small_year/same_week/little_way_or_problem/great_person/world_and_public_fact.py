4af34e7417fd59ab728dd158f46714be962eb804
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 12:59:33 -0400

    "add: go super/same_world/small_year/same_week/little_way_or_problem/great_person/world_and_public_fact.py of great group"

---
+++ super/same_world/small_year/same_week/little_way_or_problem/great_person/world_and_public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_part(str_arg):
+    group(str_arg)
+    print('fact_or_fact')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_part('good_group')
