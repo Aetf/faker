e2241646218b5a34b67788d9140dd20165468094
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 10:12:15 -0400

    "mod: get super/last_part/fact.py or super/say_company_up_little_life/case_and_year/day.py of bad life"

---
+++ super/last_part/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    week(str_arg)
+    print('look_first_time')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('great_company')
