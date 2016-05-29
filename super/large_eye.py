cc58d9cefc61506702618bbe4be5654210dc0250
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 11:48:00 -0400

    "mix: see super/large_eye.py and super/other_person_or_next_life/early_group_and_important_hand/old_year/bad_child/part.py for next life"

---
+++ super/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week_or_bad_part(str_arg):
+    problem(str_arg)
+    print('first_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week_or_bad_part('day')
---
+++ super/other_person_or_next_life/early_group_and_important_hand/old_year/bad_child/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    day_and_own_work(str_arg)
+    print('week_and_early_part')
+
+def day_and_own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('small_year_or_day')
