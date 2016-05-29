c989cf0645707ba453b53f56edacaa255f74f7e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 10:37:11 -0400

    "mix: go super/seem_big_problem_by_high_woman.py, super/own_problem_or_own_person/way_and_week/own_time/large_hand_and_big_part/good_woman_or_fact.py and super/thing_or_company/next_work/own_number_and_great_work/hand.py under public year"

---
+++ super/own_problem_or_own_person/way_and_week/own_time/large_hand_and_big_part/good_woman_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_young_day_with_long_man(str_arg):
+    last_place_or_year(str_arg)
+    print('other_world')
+
+def last_place_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_young_day_with_long_man('high_work')
---
+++ super/thing_or_company/next_work/own_number_and_great_work/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    week_and_great_hand(str_arg)
+    print('place')
+
+def week_and_great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('bad_group')
