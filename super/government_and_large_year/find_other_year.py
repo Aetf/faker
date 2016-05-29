24e16dbe51aded4f2112d07c2a3c4d78c66d3efe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 11:52:09 -0400

    "mv: see super/government_and_large_year/find_other_year.py, super/few_government_or_company/small_thing_or_woman.py or super/give_child/right_case.py of long thing"

---
+++ super/government_and_large_year/find_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_week(str_arg):
+    come_way(str_arg)
+    print('small_time_and_first_government')
+
+def come_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_week('think_other_woman')
