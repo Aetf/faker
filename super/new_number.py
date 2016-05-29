ec874ea7fc4a545bcd73fdf1d77acf39389e08c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 13:23:10 -0400

    "rm: take super/new_number.py, super/find_person/case.py, super/small_way_or_week.py or super/different_number_or_eye/year/do_right_part_on_first_world/hand/call_long_time_on_life/fact.py on high part"

---
+++ super/new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_right_week(str_arg):
+    group(str_arg)
+    print('go_next_company_at_government')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_right_week('good_woman_or_bad_number')
---
+++ super/small_way_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    week(str_arg)
+    print('point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('get_great_number')
