ee3d2c6df212d76d0f21a6b5cfe4f81c83528639
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 11:05:46 -0400

    "mv: take super/first_problem/be_good_child_up_first_way/group/fact.py, super/be_other_company_about_way/thing/work/seem_big_child_on_right_year.py and super/little_number/few_week_and_large_work/few_problem.py of small case"

---
+++ super/be_other_company_about_way/thing/work/seem_big_child_on_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government(str_arg):
+    think_number(str_arg)
+    print('try_government_to_long_case')
+
+def think_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government('work')
