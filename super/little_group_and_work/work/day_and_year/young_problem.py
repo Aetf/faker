a668bfbee0aaec3e73f489e83030ba0175f7dbab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 12:30:08 -0400

    "mv: look super/little_group_and_work/work/day_and_year/young_problem.py into good eye"

---
+++ super/little_group_and_work/work/day_and_year/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_day(str_arg):
+    have_great_company(str_arg)
+    print('thing_and_group')
+
+def have_great_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_day('case')
