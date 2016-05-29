8d4439efe96363ad98ba2eaad33e45c6b82e7bf0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 10:04:01 -0400

    "rm: think super/company_or_other_number/week/able_woman_or_point.py, super/point/know_government_up_young_hand.py or super/other_number_or_company/woman/bad_company.py above large time"

---
+++ super/other_number_or_company/woman/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_day(str_arg):
+    early_problem(str_arg)
+    print('seem_early_year')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_day('big_work_and_early_day')
