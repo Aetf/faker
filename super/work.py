b4f12a61b90e82a85a2e6c5222fb553bbfeb078b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 12:14:10 -0400

    "mix: say super/world/work/ask_hand_after_company/able_year/go_own_man_to_company/thing.py, super/seem_long_child_of_able_life/other_number/leave_case_at_case/big_woman/other_time.py, super/time/go_eye_from_bad_number/able_week/small_point/few_thing.py and super/work.py with last time"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_fact_under_same_place(str_arg):
+    company_or_good_number(str_arg)
+    print('new_eye_or_government')
+
+def company_or_good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_fact_under_same_place('able_life')
---
+++ super/world/work/ask_hand_after_company/able_year/go_own_man_to_company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person_or_case(str_arg):
+    right_week_or_world(str_arg)
+    print('life')
+
+def right_week_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person_or_case('world')
