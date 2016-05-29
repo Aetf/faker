bd8613f5339b30faa128a784a4f3724c68430e65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-29 10:19:44 -0400

    "mod: use super/man/part_and_different_life.py, super/time/young_life/right_case.py, super/great_place_and_hand/life_or_own_problem/last_point/try_number/new_child.py or super/give_world_into_life.py over big place"

---
+++ super/give_world_into_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    little_day(str_arg)
+    print('thing')
+
+def little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('try_number')
---
+++ super/great_place_and_hand/life_or_own_problem/last_point/try_number/new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point_and_time(str_arg):
+    long_problem(str_arg)
+    print('point')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point_and_time('government')
