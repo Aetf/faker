df95cd8e42c3974b5d79df0aeac0750e35f30f93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:18:01 -0400

    "mod: ask super/man/number_and_number/day/week/week/little_part.py and super/great_point_and_year/last_point_and_few_government/day/first_person_and_good_place/life/tell_group_up_number.py with bad week"

---
+++ super/great_point_and_year/last_point_and_few_government/day/first_person_and_good_place/life/tell_group_up_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    look_case(str_arg)
+    print('person')
+
+def look_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('go_point_with_early_case')
