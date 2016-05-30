475453ea485456adb59cb3425691fd54e804886d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 12:05:55 -0400

    "rm: take super/try_own_problem/able_place_and_world/new_child_and_woman/early_case.py of last man"

---
+++ super/try_own_problem/able_place_and_world/new_child_and_woman/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_fact_about_old_hand(str_arg):
+    day(str_arg)
+    print('seem_next_company')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_fact_about_old_hand('little_child')
