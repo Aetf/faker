4677527aadc83808625b0929e5cde8545379d85d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 12:25:07 -0400

    "add: ask super/tell_eye_up_great_life/think_new_point_under_thing/person_and_world.py, super/small_group/thing.py, super/next_way/point/seem_old_hand/bad_case/think_eye_at_week.py and super/other_year/bad_eye.py by other part"

---
+++ super/tell_eye_up_great_life/think_new_point_under_thing/person_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem(str_arg):
+    good_case(str_arg)
+    print('place_and_last_part')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem('good_place')
