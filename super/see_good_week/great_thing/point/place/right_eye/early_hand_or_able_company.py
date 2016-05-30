61028cc1167afbe9271ba0fb921c3313fe8b4f8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:56:00 -0400

    "mix: think super/see_good_week/great_thing/point/place/right_eye/early_hand_or_able_company.py and super/case/eye/other_time_and_day/way/see_high_year_for_other_hand/right_year_and_next_problem.py on different fact"

---
+++ super/see_good_week/great_thing/point/place/right_eye/early_hand_or_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye(str_arg):
+    last_number(str_arg)
+    print('good_way')
+
+def last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye('fact_and_group')
