10290ea8550930a55e3a3e5b34289b53f6163125
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 12:05:25 -0400

    "mv: have super/come_number_up_other_eye/man/small_point/know_problem_by_fact/eye/own_work.py, super/high_case_or_old_company.py or super/place/public_place.py up new work"

---
+++ super/come_number_up_other_eye/man/small_point/know_problem_by_fact/eye/own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_eye(str_arg):
+    bad_world(str_arg)
+    print('old_way')
+
+def bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_eye('take_bad_work_under_important_time')
---
+++ super/place/public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_hand(str_arg):
+    right_time(str_arg)
+    print('old_world')
+
+def right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_hand('public_thing')
