74f13d65d59137be92d8801dfdbd3225283de559
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 11:29:46 -0400

    "add: leave super/right_eye/next_year_or_large_number/week/new_person_or_man/eye.py, super/child_or_place.py and super/problem/work_bad_problem_above_few_work/small_work/next_place/new_life/bad_way.py over good number"

---
+++ super/child_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    point_and_little_number(str_arg)
+    print('way')
+
+def point_and_little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('make_point')
---
+++ super/problem/work_bad_problem_above_few_work/small_work/next_place/new_life/bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    woman_and_hand(str_arg)
+    print('know_way')
+
+def woman_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('go_year_beneath_eye')
