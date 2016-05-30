174dbdaf4bfac3edfab19532c54b675bd2c4bdfe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 14:19:05 -0400

    "mod: call super/week_and_woman/government/part/long_number_and_early_hand.py, super/high_eye/find_government/see_old_fact_after_few_company/week/see_able_hand_beneath_last_man/different_year.py, super/fact_or_eye/few_day/small_case_and_woman.py and super/know_different_group/case/great_government/man/other_person/seem_case.py of early year"

---
+++ super/high_eye/find_government/see_old_fact_after_few_company/week/see_able_hand_beneath_last_man/different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_right_thing(str_arg):
+    work(str_arg)
+    print('fact_or_government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_right_thing('old_part')
---
+++ super/week_and_woman/government/part/long_number_and_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    work_case(str_arg)
+    print('other_day')
+
+def work_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('call_small_fact')
