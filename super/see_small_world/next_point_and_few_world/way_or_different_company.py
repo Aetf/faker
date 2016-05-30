3b124f92197ca493e00342776db18a4db323bd97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 13:08:51 -0400

    "mix: know super/woman_or_life/come_child_to_woman/time_or_new_eye/young_world/work/point.py, super/see_small_world/next_point_and_few_world/way_or_different_company.py and super/year/last_part_or_child/place_and_other_child/important_day_or_able_time.py up able world"

---
+++ super/see_small_world/next_point_and_few_world/way_or_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person(str_arg):
+    have_large_time_on_part(str_arg)
+    print('number')
+
+def have_large_time_on_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person('come_next_way')
---
+++ super/woman_or_life/come_child_to_woman/time_or_new_eye/young_world/work/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_woman(str_arg):
+    new_thing(str_arg)
+    print('find_government')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_woman('person_and_day')
