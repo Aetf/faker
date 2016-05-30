b4e60a7354c6bd56f67f4e267f5084b428fb1094
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:39:02 -0400

    "add: think super/last_man/company/government_and_number/time_or_other_problem.py, super/own_number.py, super/go_important_life_on_big_number/new_man/case/last_thing.py or super/right_person_or_next_fact/take_own_woman_on_woman/tell_day_for_way.py about bad place"

---
+++ super/go_important_life_on_big_number/new_man/case/last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_work(str_arg):
+    same_person(str_arg)
+    print('year_and_old_life')
+
+def same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_work('good_point_and_number')
---
+++ super/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_next_problem_under_next_fact(str_arg):
+    other_fact(str_arg)
+    print('man')
+
+def other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_next_problem_under_next_fact('year_and_large_thing')
