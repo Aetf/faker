eddc21b51f6d6a6029d47c9975b08f6897fc9f7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:58:08 -0400

    "mod: go super/get_able_time/new_life.py to young person"

---
+++ super/get_able_time/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_thing_above_long_child(str_arg):
+    public_life(str_arg)
+    print('week_or_life')
+
+def public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_thing_above_long_child('year')
