3f579cf1adf33a30bd653760699b2d3b1b5c3fc5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:44:03 -0400

    "mv: know super/work_high_person/man_or_different_week/group/different_week/group/woman.py, super/tell_government/woman/world/man_or_man/bad_work_and_few_number/different_fact_or_child.py, super/try_problem.py or super/own_work.py after own man"

---
+++ super/own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    long_time(str_arg)
+    print('take_point_under_bad_man')
+
+def long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('use_week')
---
+++ super/work_high_person/man_or_different_week/group/different_week/group/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day_and_group(str_arg):
+    see_case_about_place(str_arg)
+    print('problem')
+
+def see_case_about_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day_and_group('old_company')
