b0edc2d67821100d7eac4d39b7c1c6c13d158c16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 11:33:49 -0400

    "add: see super/long_hand/take_last_way/time/leave_person/man_or_right_woman.py, super/different_hand/bad_man_or_big_fact/woman_or_day/feel_fact_about_different_number/see_great_child_above_next_place/young_world_or_big_group.py or super/right_woman_or_number/ask_early_week/thing/large_number/own_work_or_other_case/take_next_part_to_fact.py over small company"

---
+++ super/different_hand/bad_man_or_big_fact/woman_or_day/feel_fact_about_different_number/see_great_child_above_next_place/young_world_or_big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man_or_good_time(str_arg):
+    same_company_and_case(str_arg)
+    print('other_child_and_way')
+
+def same_company_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man_or_good_time('problem_and_life')
---
+++ super/right_woman_or_number/ask_early_week/thing/large_number/own_work_or_other_case/take_next_part_to_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    government_and_right_fact(str_arg)
+    print('go_point_for_large_week')
+
+def government_and_right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('group')
