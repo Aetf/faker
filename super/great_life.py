8a5e4d931ce4b22244aa2061d6d288037cec3e6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:59:31 -0400

    "mv: have super/great_life.py after first year"

---
+++ super/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person(str_arg):
+    tell_bad_fact_under_world(str_arg)
+    print('large_thing')
+
+def tell_bad_fact_under_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person('long_child_and_large_company')
