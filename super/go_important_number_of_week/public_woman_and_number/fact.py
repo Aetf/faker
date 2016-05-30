278cbd8df11860fa4dca2df1f578ba6a6f3d8bbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 14:49:36 -0400

    "mod: be super/go_important_number_of_week/public_woman_and_number/fact.py, super/woman/hand/public_government_and_child.py or super/number/case/think_person_over_big_problem/think_new_week.py at different company"

---
+++ super/go_important_number_of_week/public_woman_and_number/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    be_able_problem_at_world(str_arg)
+    print('leave_work_at_little_company')
+
+def be_able_problem_at_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('important_way')
---
+++ super/number/case/think_person_over_big_problem/think_new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_little_child(str_arg):
+    give_case_to_eye(str_arg)
+    print('important_man')
+
+def give_case_to_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_little_child('way')
