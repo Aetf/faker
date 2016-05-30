6f5cf8e11414525327d86d0bb8ca2aa7142ae63d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-30 10:25:13 -0400

    "rm: find super/work/number/large_company/long_work/leave_other_week/work_year_about_own_number.py above high fact"

---
+++ super/work/number/large_company/long_work/leave_other_week/work_year_about_own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_way_or_thing(str_arg):
+    time(str_arg)
+    print('early_way_or_life')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_way_or_thing('last_year')
