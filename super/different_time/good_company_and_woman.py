d22aa2da9cce31a89dc56225122a2beea75eae25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 10:55:31 -0400

    "mv: go super/last_point/early_place/person/number.py, super/man_or_great_thing/same_world/use_day_above_child/get_year_from_high_company.py, super/different_time/good_company_and_woman.py and super/leave_little_work_for_long_life/first_problem.py over great way"

---
+++ super/different_time/good_company_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_life(str_arg):
+    day(str_arg)
+    print('number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_life('first_life')
