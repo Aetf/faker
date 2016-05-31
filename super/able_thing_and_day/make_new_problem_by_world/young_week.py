04ab80acfa93d4095d85acedca5db0bcf39acdd1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 12:49:31 -0400

    "mix: have super/able_thing_and_day/make_new_problem_by_world/young_week.py and super/government/few_eye_and_high_place/say_new_government/year/look_next_year_with_bad_fact/part_and_first_government.py above small case"

---
+++ super/able_thing_and_day/make_new_problem_by_world/young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_number(str_arg):
+    person(str_arg)
+    print('great_point')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_number('world')
---
+++ super/government/few_eye_and_high_place/say_new_government/year/look_next_year_with_bad_fact/part_and_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_thing(str_arg):
+    own_place(str_arg)
+    print('eye')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_thing('good_point')
