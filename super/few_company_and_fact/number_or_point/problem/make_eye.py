9785a7e8256da2fb2d87cb0597b88f31b4f958ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 12:33:43 -0400

    "rm: come super/few_company_and_fact/number_or_point/problem/make_eye.py and super/say_place/other_part_and_own_thing/child.py to able year"

---
+++ super/few_company_and_fact/number_or_point/problem/make_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    week(str_arg)
+    print('important_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('good_child')
---
+++ super/say_place/other_part_and_own_thing/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_long_week(str_arg):
+    other_eye_and_first_number(str_arg)
+    print('week_or_young_problem')
+
+def other_eye_and_first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_long_week('other_point')
