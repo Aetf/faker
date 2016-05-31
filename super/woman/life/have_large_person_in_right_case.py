cea2c3cd087ca2f908c5be5aa1404ec877597f32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 14:02:28 -0400

    "add: ask super/woman/life/have_large_person_in_right_case.py under different point"

---
+++ super/woman/life/have_large_person_in_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time_and_great_eye(str_arg):
+    seem_high_work(str_arg)
+    print('woman_and_case')
+
+def seem_high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time_and_great_eye('life')
