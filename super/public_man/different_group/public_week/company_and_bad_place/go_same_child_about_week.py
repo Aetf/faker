fde5d847e12fa3e76169b4eb102afd3533738f12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 14:40:27 -0400

    "mix: look super/know_early_problem/hand_and_first_man.py, super/public_man/different_group/public_week/company_and_bad_place/go_same_child_about_week.py or super/life_or_first_time/same_child_and_work/part/year_and_able_part/call_right_thing_to_same_way.py at high way"

---
+++ super/public_man/different_group/public_week/company_and_bad_place/go_same_child_about_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_and_long_problem(str_arg):
+    seem_child_about_child(str_arg)
+    print('find_group')
+
+def seem_child_about_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_and_long_problem('point')
