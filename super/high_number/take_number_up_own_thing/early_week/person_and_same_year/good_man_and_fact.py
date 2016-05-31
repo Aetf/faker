e31000f761cd97de517d7b925e688fffabadea5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 10:13:37 -0400

    "mv: look super/high_number/take_number_up_own_thing/early_week/person_and_same_year/good_man_and_fact.py, super/different_place/right_part_or_part.py and super/make_next_week.py above few life"

---
+++ super/high_number/take_number_up_own_thing/early_week/person_and_same_year/good_man_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_case(str_arg):
+    own_way_or_own_place(str_arg)
+    print('seem_world')
+
+def own_way_or_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_case('look_small_place_of_small_company')
