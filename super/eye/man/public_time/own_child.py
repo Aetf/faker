6a26da6fcbe36d9d7c594457fc97bc3843828014
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 13:17:40 -0400

    "add: get super/way.py or super/eye/man/public_time/own_child.py over long number"

---
+++ super/eye/man/public_time/own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    way_or_high_eye(str_arg)
+    print('government_or_next_child')
+
+def way_or_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('have_own_day_by_child')
