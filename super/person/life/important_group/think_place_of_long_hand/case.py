f6eb4356336d6c6553d665e17fd3a456d76c25cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 14:48:13 -0400

    "add: get super/person/life/important_group/think_place_of_long_hand/case.py after good time"

---
+++ super/person/life/important_group/think_place_of_long_hand/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    old_child(str_arg)
+    print('try_long_woman')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('fact')
