0da766e0622530ec4a39cc6aa6d194d88c176491
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 10:11:38 -0400

    "add: go super/own_case_and_hand/call_number/say_world_into_good_year/know_young_place_of_same_company/great_man_and_person.py, super/government_and_hand/group/few_point/work_part.py and super/great_way/young_man_and_able_world/great_hand/little_company_and_bad_part/world.py of big case"

---
+++ super/own_case_and_hand/call_number/say_world_into_good_year/know_young_place_of_same_company/great_man_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    case(str_arg)
+    print('few_fact_and_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('hand')
