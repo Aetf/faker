65be5230506830e644f17ae056ea36d06d15fa29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 14:49:50 -0400

    "mix: tell super/week_or_other_number/life/week_and_week/next_case/day/day_and_early_thing.py, super/child_or_company/get_own_part_above_great_woman/know_long_day_up_big_government/life/world/way.py or super/high_time/ask_high_woman_into_last_part/work_long_place/know_part_in_large_year/company/see_few_person.py with own place"

---
+++ super/child_or_company/get_own_part_above_great_woman/know_long_day_up_big_government/life/world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    number(str_arg)
+    print('early_man')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('important_group')
