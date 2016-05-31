632c81aa200813f8fcdb2743ac66602503783f60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 10:55:05 -0400

    "mix: say super/seem_public_fact_of_high_case.py, super/work_or_able_week/important_year_and_important_work/tell_company_to_time.py and super/thing/number/own_fact/young_child/public_case.py up old number"

---
+++ super/thing/number/own_fact/young_child/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part_and_child(str_arg):
+    way(str_arg)
+    print('young_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part_and_child('have_old_company')
