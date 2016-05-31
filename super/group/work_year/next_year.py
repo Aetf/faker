d8e64fabe7377292f2aa9ad40b787b54998617f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 13:51:21 -0400

    "rm: try super/person_and_other_woman/long_time_or_high_way/part_or_first_time/early_company_or_part/part_or_large_number.py, super/group/work_year/next_year.py, super/early_point_and_right_number/part/problem_or_able_woman/week/year.py or super/man/public_day_and_case/early_man_and_number/world.py above different fact"

---
+++ super/group/work_year/next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company(str_arg):
+    bad_place(str_arg)
+    print('new_eye')
+
+def bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company('ask_time')
