b90f3ec33310604977d9b8e46ea906b2d5dd5c42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 11:10:59 -0400

    "mix: know super/number_and_way/few_part/right_part/old_point_or_great_part/number_and_person.py, super/long_part/have_year_to_next_life/public_place_and_group/small_work_or_right_fact.py and super/place/year.py into first point"

---
+++ super/long_part/have_year_to_next_life/public_place_and_group/small_work_or_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point_and_hand(str_arg):
+    world(str_arg)
+    print('old_problem')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point_and_hand('life')
---
+++ super/place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way(str_arg):
+    thing(str_arg)
+    print('group_or_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way('number')
