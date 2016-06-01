084a75a1a18b76c5cdf3f836c203a37da7f1763a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 13:23:30 -0400

    "mod: work super/next_government.py and super/important_child.py for little woman"

---
+++ super/next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    use_hand_above_young_point(str_arg)
+    print('other_child')
+
+def use_hand_above_young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('old_eye')
