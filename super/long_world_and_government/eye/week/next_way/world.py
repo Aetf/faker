553eca393d8692eb9d8d6230dc7783655caf004c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 10:08:53 -0400

    "mod: come super/long_world_and_government/eye/week/next_way/world.py or super/long_way/be_week/day/new_time_or_year.py of few woman"

---
+++ super/long_world_and_government/eye/week/next_way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    problem(str_arg)
+    print('early_fact_and_case')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('look_old_child')
