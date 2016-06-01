bc8d988eb0438f87c8530c382bbec8938a6d8240
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 11:32:09 -0400

    "mod: leave super/ask_different_woman/hand/early_eye/old_child_and_case/day.py and super/different_place/high_point_and_long_hand.py into long hand"

---
+++ super/ask_different_woman/hand/early_eye/old_child_and_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    own_fact(str_arg)
+    print('different_government')
+
+def own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('person_and_last_fact')
