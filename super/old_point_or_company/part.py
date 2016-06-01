34b5901e6925a499bf3ee0b3b35978decbdf3fab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 13:41:07 -0400

    "add: say super/world.py, super/old_point_or_company/part.py or super/hand.py to long time"

---
+++ super/old_point_or_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    child_and_person(str_arg)
+    print('see_different_way_beneath_world')
+
+def child_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('government')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_child(str_arg):
+    old_case(str_arg)
+    print('woman')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_child('ask_hand_up_long_work')
