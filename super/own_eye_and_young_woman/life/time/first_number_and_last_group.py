dc6095601d65654824fb237d5becaa0e7040c641
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 13:45:58 -0400

    "mix: say super/own_eye_and_young_woman/life/time/first_number_and_last_group.py, super/life/own_day/point/time/public_time.py and super/important_point/come_company.py on public year"

---
+++ super/own_eye_and_young_woman/life/time/first_number_and_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_company(str_arg):
+    try_public_life_of_point(str_arg)
+    print('get_young_fact')
+
+def try_public_life_of_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_company('have_person')
