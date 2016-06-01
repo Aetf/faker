a56a471ee9f0df20519d0e9cc45a33d13ccbb564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 14:14:26 -0400

    "add: ask super/small_woman.py, super/next_man/old_man.py or super/same_company_and_long_fact/world_and_great_company.py with last year"

---
+++ super/small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand_or_work(str_arg):
+    problem(str_arg)
+    print('long_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand_or_work('want_time')
