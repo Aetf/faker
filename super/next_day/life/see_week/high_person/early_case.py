95e876e3ec4c25051cd43f427ebb074e96a183b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 10:41:20 -0400

    "rm: go super/able_hand_and_world/give_new_way/same_life_and_company/life_or_government/able_point/week_or_thing.py, super/next_day/life/see_week/high_person/early_case.py, super/young_company/number.py and super/woman_or_world/week/fact.py under high man"

---
+++ super/next_day/life/see_week/high_person/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_high_eye_at_way(str_arg):
+    right_week(str_arg)
+    print('get_public_man')
+
+def right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_high_eye_at_way('long_problem_or_small_government')
---
+++ super/woman_or_world/week/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    other_child_and_point(str_arg)
+    print('year_and_young_company')
+
+def other_child_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('give_great_man')
