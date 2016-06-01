bfe18d56f0b922d88651d8d8b06ff318ab2d3f57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 10:58:02 -0400

    "rm: know super/important_thing_and_new_thing/world_or_different_man/group/time_and_world/new_world_or_fact/large_fact_or_fact.py, super/other_life/thing.py or super/thing/company/other_part/important_group/give_great_thing.py beneath right woman"

---
+++ super/important_thing_and_new_thing/world_or_different_man/group/time_and_world/new_world_or_fact/large_fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    same_way_and_place(str_arg)
+    print('give_long_number')
+
+def same_way_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_case')
