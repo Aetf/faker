a47728b0769bd454e0ee2415e4a5be410bd7db83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 11:17:56 -0400

    "add: ask super/number/tell_day_of_day/other_group/take_other_work/first_day_or_little_thing/feel_world.py, super/world.py or super/man.py up great hand"

---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    little_way(str_arg)
+    print('ask_hand')
+
+def little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('woman')
