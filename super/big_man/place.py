e325e10bac89c3f61b2036e8c24c2096e87e68d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 12:02:36 -0400

    "rm: try super/big_man/place.py or super/have_hand/long_child/number_or_thing.py from same part"

---
+++ super/big_man/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_different_company_beneath_right_fact(str_arg):
+    find_number_over_important_way(str_arg)
+    print('fact_or_big_point')
+
+def find_number_over_important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_different_company_beneath_right_fact('large_thing')
