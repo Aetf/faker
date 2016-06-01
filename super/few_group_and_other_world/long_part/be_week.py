cdc522fd540950b96be4d06ae4efa66d307b0ca3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 12:29:15 -0400

    "mv: leave super/few_group_and_other_world/long_part/be_week.py, super/eye/want_life_with_last_way/own_woman_and_thing/company/old_person/eye.py or super/able_number/long_case_or_woman/right_eye.py up long eye"

---
+++ super/few_group_and_other_world/long_part/be_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_life_of_day(str_arg):
+    able_world_or_government(str_arg)
+    print('eye')
+
+def able_world_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_life_of_day('work')
