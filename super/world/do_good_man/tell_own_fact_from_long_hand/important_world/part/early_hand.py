2096e82932da80ec05f75b3e10ea3ffaa1763c6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 10:53:33 -0400

    "mv: work super/new_work.py and super/world/do_good_man/tell_own_fact_from_long_hand/important_world/part/early_hand.py by old number"

---
+++ super/world/do_good_man/tell_own_fact_from_long_hand/important_world/part/early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_large_part_after_point(str_arg):
+    person(str_arg)
+    print('small_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_large_part_after_point('good_way_or_time')
