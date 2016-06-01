815c44b82749d23cbc949faa5fdb3ec8e46e31e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 14:56:26 -0400

    "mod: go super/point/young_world_and_old_way/week.py, super/different_time_or_work/right_time/long_day_and_world.py, super/find_case/fact_and_new_company/early_place.py or super/long_thing.py with able part"

---
+++ super/find_case/fact_and_new_company/early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_woman(str_arg):
+    go_other_point_at_company(str_arg)
+    print('government')
+
+def go_other_point_at_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_woman('own_number')
---
+++ super/point/young_world_and_old_way/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    woman(str_arg)
+    print('come_case')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('long_man')
