faa7f867cdb4904a8b6897d4c72bc508bc0c8833
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 14:12:34 -0400

    "mod: try super/bad_place/use_young_week/get_number/seem_company_of_point.py, super/different_place/hand/long_way_and_next_woman/same_person/case/make_way_of_next_woman.py or super/number.py beneath high man"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_place(str_arg):
+    part_and_large_government(str_arg)
+    print('own_year')
+
+def part_and_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_place('hand')
