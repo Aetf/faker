498392e456fb28008d28728f48d170d1b77eaf44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 14:47:51 -0400

    "add: want super/right_problem.py or super/life/look_world.py with new part"

---
+++ super/life/look_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    own_group(str_arg)
+    print('long_eye')
+
+def own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('different_woman')
---
+++ super/right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_long_year(str_arg):
+    think_man(str_arg)
+    print('come_other_group')
+
+def think_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_long_year('different_part')
