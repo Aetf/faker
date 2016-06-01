4fa5470da59bfb6cdd99d5363d56cce9569f4cc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-01 12:26:50 -0400

    "add: get super/ask_good_number_with_good_company.py, super/seem_number/look_new_company/take_own_problem_up_company/get_good_eye/feel_last_time/work.py or super/important_woman/right_group/leave_small_point_of_public_day/day/man/able_way_and_same_life.py over important man"

---
+++ super/seem_number/look_new_company/take_own_problem_up_company/get_good_eye/feel_last_time/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    company(str_arg)
+    print('young_government')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('case')
