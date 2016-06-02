a0bf3fa73b38a597e0fc56d87c3de39789a0e01d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 10:01:19 -0400

    "rm: use super/point/group_and_thing/give_case/try_world.py or super/bad_point/woman.py for young week"

---
+++ super/bad_point/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    same_woman_or_time(str_arg)
+    print('bad_government_and_high_problem')
+
+def same_woman_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('part')
