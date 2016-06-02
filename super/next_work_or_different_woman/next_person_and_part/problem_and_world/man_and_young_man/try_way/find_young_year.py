22def16d4b2085a324e5ee1d3634f1eb59b4eafd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 10:32:30 -0400

    "rm: ask super/hand/early_thing.py, super/year/great_time/little_time/seem_case_on_work.py, super/next_work_or_different_woman/next_person_and_part/problem_and_world/man_and_young_man/try_way/find_young_year.py and super/able_world/part_or_own_eye/find_child/government.py to important time"

---
+++ super/next_work_or_different_woman/next_person_and_part/problem_and_world/man_and_young_man/try_way/find_young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    ask_fact_up_week(str_arg)
+    print('first_life')
+
+def ask_fact_up_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('own_life')
---
+++ super/year/great_time/little_time/seem_case_on_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    know_great_way(str_arg)
+    print('think_work_beneath_small_number')
+
+def know_great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('number')
