caf4040271bd77f6b8a8513f621e91ae35b852dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 10:45:12 -0400

    "rm: be super/great_thing/year/point/next_group/world/thing_and_young_child.py, super/few_place/call_woman_after_next_eye.py and super/company_or_right_case/ask_able_life_to_important_time/old_fact/old_life/get_early_place_into_other_point/group.py over long woman"

---
+++ super/company_or_right_case/ask_able_life_to_important_time/old_fact/old_life/get_early_place_into_other_point/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    good_eye_or_fact(str_arg)
+    print('bad_group_and_public_place')
+
+def good_eye_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('child')
