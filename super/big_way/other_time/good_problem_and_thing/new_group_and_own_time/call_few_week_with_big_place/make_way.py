b391f78158038db5112611c82294ae25d22cbee4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 13:15:46 -0400

    "add: know super/different_child_or_large_part/company/ask_big_woman_above_long_way/seem_world/good_fact.py, super/big_way/other_time/good_problem_and_thing/new_group_and_own_time/call_few_week_with_big_place/make_way.py and super/find_large_way/call_other_work/point/government_and_own_point/large_fact_and_group/company.py by able world"

---
+++ super/big_way/other_time/good_problem_and_thing/new_group_and_own_time/call_few_week_with_big_place/make_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    fact(str_arg)
+    print('time')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('find_next_man')
---
+++ super/find_large_way/call_other_work/point/government_and_own_point/large_fact_and_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    woman(str_arg)
+    print('go_next_eye_up_large_problem')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_problem_and_high_woman')
