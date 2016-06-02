fcc6d2db4b9e5c0800eb464e92c48e6f8e381922
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 10:30:53 -0400

    "mod: get super/government.py and super/fact_and_large_place/work_or_good_fact/problem_or_little_government.py by bad child"

---
+++ super/fact_and_large_place/work_or_good_fact/problem_or_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_hand(str_arg):
+    small_week_or_good_case(str_arg)
+    print('good_problem')
+
+def small_week_or_good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_hand('hand_or_last_group')
