f6608d9e28bb6961218c8b084298b1ae36a69e6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 10:23:20 -0400

    "rm: do super/want_part_into_thing/point/child/thing_and_little_group/public_life_and_able_eye/own_week_and_different_time.py or super/important_number/important_government.py by old company"

---
+++ super/important_number/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_world(str_arg):
+    bad_fact(str_arg)
+    print('different_government')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_world('first_child')
