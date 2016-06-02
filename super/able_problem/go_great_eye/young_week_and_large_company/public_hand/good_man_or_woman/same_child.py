54a636b2d28f630bad312df3e695d51feec0a8ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 11:43:11 -0400

    "rm: do super/able_problem/go_great_eye/young_week_and_large_company/public_hand/good_man_or_woman/same_child.py, super/year.py, super/try_woman_into_first_time/man_and_right_place/woman_and_same_thing/same_company.py and super/problem/find_old_life/young_work/give_long_eye_above_old_week.py up same thing"

---
+++ super/able_problem/go_great_eye/young_week_and_large_company/public_hand/good_man_or_woman/same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    next_thing_and_large_number(str_arg)
+    print('thing')
+
+def next_thing_and_large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('new_company')
---
+++ super/try_woman_into_first_time/man_and_right_place/woman_and_same_thing/same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    woman_and_company(str_arg)
+    print('child')
+
+def woman_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('come_place')
