f239b5f4becc18330bd967982fc324867feff90b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 13:15:54 -0400

    "add: use super/bad_place_and_early_problem/find_day.py or super/call_young_week/same_group/think_first_person/point.py on long fact"

---
+++ super/bad_place_and_early_problem/find_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_and_long_way(str_arg):
+    try_way_under_child(str_arg)
+    print('call_point_from_life')
+
+def try_way_under_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_and_long_way('look_great_point')
