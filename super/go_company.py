022280a99c50a9047f5c06eee8e5bb20d6a9f1ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 11:32:35 -0400

    "rm: give super/go_company.py up last hand"

---
+++ super/go_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_other_woman(str_arg):
+    other_point(str_arg)
+    print('problem')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_other_woman('tell_week')
