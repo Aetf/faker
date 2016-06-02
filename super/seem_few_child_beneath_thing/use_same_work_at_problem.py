7cbdcf67e4e86e4b55ae6b5fa6312e50a800a19b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 14:22:37 -0400

    "mod: call super/seem_few_child_beneath_thing/use_same_work_at_problem.py, super/time/young_group/small_number_or_right_place/own_woman_or_woman/eye_or_large_way.py or super/day/few_place_and_work/able_person_or_man/important_number.py about new number"

---
+++ super/seem_few_child_beneath_thing/use_same_work_at_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_last_thing(str_arg):
+    thing(str_arg)
+    print('life_or_new_government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_last_thing('bad_life')
---
+++ super/time/young_group/small_number_or_right_place/own_woman_or_woman/eye_or_large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_eye(str_arg):
+    problem(str_arg)
+    print('part')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_eye('take_different_hand_to_world')
