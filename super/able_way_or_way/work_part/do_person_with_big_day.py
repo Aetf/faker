f2b263b7fc6be89cad28e8ae1238b2b399f60a2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 12:42:27 -0400

    "mix: find super/able_way_or_way/work_part/do_person_with_big_day.py, super/man/long_fact_and_small_point/part_or_time.py or super/company.py at long way"

---
+++ super/able_way_or_way/work_part/do_person_with_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    case(str_arg)
+    print('good_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('say_good_thing_on_life')
---
+++ super/man/long_fact_and_small_point/part_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    good_day(str_arg)
+    print('few_case')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('make_woman')
