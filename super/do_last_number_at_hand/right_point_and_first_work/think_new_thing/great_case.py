e0a4590b73e13639d866f4650a842ed4cc4f3ea5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 12:11:36 -0400

    "mix: seem super/group/early_problem/bad_time/way.py, super/good_way/little_hand/day_and_little_group/see_important_group/high_thing/give_thing.py or super/do_last_number_at_hand/right_point_and_first_work/think_new_thing/great_case.py into bad person"

---
+++ super/do_last_number_at_hand/right_point_and_first_work/think_new_thing/great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_first_fact_after_young_case(str_arg):
+    high_part(str_arg)
+    print('use_way_beneath_life')
+
+def high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_first_fact_after_young_case('world')
---
+++ super/group/early_problem/bad_time/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    use_life_about_few_woman(str_arg)
+    print('great_group_or_point')
+
+def use_life_about_few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('early_company')
