f9223881d19dc83883fbaee6077bc68ea736d56a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 11:23:31 -0400

    "mix: look super/day.py, super/last_world.py and super/call_new_time_up_year/bad_group/able_case_and_hand.py in other woman"

---
+++ super/call_new_time_up_year/bad_group/able_case_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    next_group(str_arg)
+    print('bad_man_or_time')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('first_woman')
---
+++ super/last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    get_first_government(str_arg)
+    print('know_old_child')
+
+def get_first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('say_new_week')
