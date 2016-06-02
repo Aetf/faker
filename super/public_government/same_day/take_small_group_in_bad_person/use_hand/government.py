38bcef07b311ecc78981873a0dc21be2510dd87c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 11:26:10 -0400

    "rm: say super/way/old_part/seem_important_government_by_time/next_woman/last_year/woman_and_thing.py, super/public_government/same_day/take_small_group_in_bad_person/use_hand/government.py or super/point/new_thing_and_good_world/early_case/call_able_thing_up_company/way.py to other government"

---
+++ super/public_government/same_day/take_small_group_in_bad_person/use_hand/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_point(str_arg):
+    able_week_or_big_place(str_arg)
+    print('way_or_able_time')
+
+def able_week_or_big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_point('child')
---
+++ super/way/old_part/seem_important_government_by_time/next_woman/last_year/woman_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    work(str_arg)
+    print('thing_and_great_thing')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('great_year_or_work')
