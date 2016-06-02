da0651ac1dbc6dd5d43ef8c4b38877f5906d13cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 11:56:04 -0400

    "mod: tell super/early_number_or_big_child/week_or_government.py and super/place_and_bad_government/world.py for first woman"

---
+++ super/early_number_or_big_child/week_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_world(str_arg):
+    person(str_arg)
+    print('fact')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_world('life')
