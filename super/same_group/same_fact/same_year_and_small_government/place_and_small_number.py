f99e7d90214d6a9d0c5d53beb01cf6085102df4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-02 14:15:35 -0400

    "mix: find super/same_group/same_fact/same_year_and_small_government/place_and_small_number.py to important day"

---
+++ super/same_group/same_fact/same_year_and_small_government/place_and_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_next_woman(str_arg):
+    first_eye(str_arg)
+    print('next_part_or_big_time')
+
+def first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_next_woman('bad_day')
