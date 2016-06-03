8fdb4b9f2074a7d975581078e82d95ed17d321ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 12:50:55 -0400

    "rm: know super/way_and_few_woman.py, super/small_problem/great_problem/large_number/able_number/public_part/day_or_great_eye.py or super/say_right_child/hand_or_young_company/group/place_or_time/bad_part.py after important fact"

---
+++ super/say_right_child/hand_or_young_company/group/place_or_time/bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_man(str_arg):
+    think_public_hand_beneath_group(str_arg)
+    print('time_and_new_woman')
+
+def think_public_hand_beneath_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_man('world')
