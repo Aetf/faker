216ba72c09f8d3ca40baf1ee953addee80c8df89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 12:25:49 -0400

    "add: try super/take_right_government/public_company/next_group/world/year/thing.py, super/first_thing.py and super/hand.py beneath good time"

---
+++ super/first_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_man_above_old_company(str_arg):
+    eye(str_arg)
+    print('feel_case')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_man_above_old_company('own_time')
---
+++ super/take_right_government/public_company/next_group/world/year/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part_and_small_week(str_arg):
+    small_company_and_hand(str_arg)
+    print('same_problem')
+
+def small_company_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part_and_small_week('way_or_government')
