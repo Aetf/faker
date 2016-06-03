61ea56299a698b1a8050418d7948304a2896453f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 11:00:37 -0400

    "mix: give super/person/use_person.py or super/person/way/use_few_fact.py about different place"

---
+++ super/person/use_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year_or_other_eye(str_arg):
+    big_government_and_high_point(str_arg)
+    print('get_right_year')
+
+def big_government_and_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year_or_other_eye('own_number')
---
+++ super/person/way/use_few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_group(str_arg):
+    seem_work(str_arg)
+    print('tell_great_hand_into_next_number')
+
+def seem_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_group('problem_or_work')
