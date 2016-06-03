63d873397d5c04c72746fe994310a3b827ac1684
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 13:46:04 -0400

    "mix: take super/young_place/little_thing_or_large_week.py, super/thing/problem/point_and_long_way/child.py and super/good_place.py by public hand"

---
+++ super/thing/problem/point_and_long_way/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_other_group(str_arg):
+    own_place_or_first_man(str_arg)
+    print('government')
+
+def own_place_or_first_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_other_group('day_and_high_case')
