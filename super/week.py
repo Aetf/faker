487246d445e53a759b0a9a50928756a3db2970d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 10:41:01 -0400

    "mv: make super/week.py or super/week/child/be_eye/other_place.py into right problem"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    world(str_arg)
+    print('long_week')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('great_thing_or_woman')
