82a9840b018b208ae6a3d828dc0628eb718ec134
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 10:45:00 -0400

    "add: leave super/early_place/eye/point_and_year/government_or_long_case/thing/right_world.py after public woman"

---
+++ super/early_place/eye/point_and_year/government_or_long_case/thing/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_long_number_after_man(str_arg):
+    early_thing(str_arg)
+    print('tell_long_group_on_own_year')
+
+def early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_long_number_after_man('child')
