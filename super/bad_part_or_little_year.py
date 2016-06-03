b34eb455f5a013fec84a57c61726cf863112f791
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 14:18:34 -0400

    "mix: see super/day/try_time_by_woman.py, super/bad_part_or_little_year.py and super/company_and_high_case/few_life/point/number/problem_and_right_child.py at small hand"

---
+++ super/bad_part_or_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    seem_able_woman_to_next_problem(str_arg)
+    print('problem')
+
+def seem_able_woman_to_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('part_or_child')
---
+++ super/company_and_high_case/few_life/point/number/problem_and_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government(str_arg):
+    same_hand(str_arg)
+    print('week')
+
+def same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government('have_week')
