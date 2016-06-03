55d2e305a18c647a8ec9abee44b19de216e07f19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 11:57:40 -0400

    "mv: seem super/young_world_or_time.py and super/tell_next_thing_with_first_week/bad_case/early_number_and_week/eye_and_early_company/important_life_and_big_time/know_big_eye.py by old way"

---
+++ super/tell_next_thing_with_first_week/bad_case/early_number_and_week/eye_and_early_company/important_life_and_big_time/know_big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact(str_arg):
+    public_government(str_arg)
+    print('young_problem')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact('other_eye')
---
+++ super/young_world_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    high_life(str_arg)
+    print('day')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('child')
