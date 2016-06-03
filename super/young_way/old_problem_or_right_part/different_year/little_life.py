c7dc498058741d716fa8a3525359ef5bd166d388
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 12:52:14 -0400

    "mv: be super/ask_point/get_young_year/government/day/able_year_or_life/go_part.py, super/young_way/old_problem_or_right_part/different_year/little_life.py or super/eye/bad_problem_or_young_year/different_life.py about old eye"

---
+++ super/young_way/old_problem_or_right_part/different_year/little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place_and_few_group(str_arg):
+    get_life_over_public_eye(str_arg)
+    print('time_or_old_eye')
+
+def get_life_over_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place_and_few_group('able_man')
