7f7e253394fdb7c9c681b3cfeeb7185e92c671c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 14:45:53 -0400

    "add: make super/group_and_place/child_and_way/small_case.py and super/week/public_part.py under next fact"

---
+++ super/week/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    find_company(str_arg)
+    print('right_case')
+
+def find_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('try_early_life_after_number')
