721e635bd4ec5475bd687a93991a7888c6e09796
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 12:37:25 -0400

    "add: have super/make_large_woman/leave_part/think_thing_above_few_person/little_fact.py and super/know_same_time.py under next problem"

---
+++ super/make_large_woman/leave_part/think_thing_above_few_person/little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    get_group(str_arg)
+    print('say_next_way')
+
+def get_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('day_and_early_life')
