499f43d0029eb6604178dbbd658a2c0273568b44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 11:35:55 -0400

    "add: have super/point/try_hand_to_world/high_group.py or super/government_or_young_place/day.py over bad case"

---
+++ super/government_or_young_place/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand_or_big_year(str_arg):
+    time(str_arg)
+    print('public_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand_or_big_year('next_child')
