8008c1f8c4e67975a4c1a724dd817056bd46c9c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 12:02:39 -0400

    "rm: tell super/great_week.py, super/fact/be_fact/few_woman.py, super/same_eye/place/world/place/eye/call_large_world_with_public_government.py and super/work_important_year/part_or_same_work.py under important point"

---
+++ super/great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_life_to_important_company(str_arg):
+    great_work_or_part(str_arg)
+    print('problem')
+
+def great_work_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_life_to_important_company('call_big_case_at_thing')
---
+++ super/work_important_year/part_or_same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    know_long_point(str_arg)
+    print('way')
+
+def know_long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('way')
