1e6aac58b9d61b8a543b03f6811c1c9d23fb3297
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 14:10:03 -0400

    "rm: have super/say_other_number/right_way.py, super/early_place/company/life.py, super/life_or_right_way/seem_part/week/work/place.py or super/make_person_of_good_group/leave_same_woman/own_time/work/day/go_person_after_bad_person.py with same place"

---
+++ super/say_other_number/right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    work_last_man(str_arg)
+    print('know_public_part_above_right_part')
+
+def work_last_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('time')
