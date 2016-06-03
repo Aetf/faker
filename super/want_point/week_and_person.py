b294e919b1b488e6f9e2e993e04693e475626836
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 10:35:31 -0400

    "mv: tell super/get_government/old_point/hand/large_day_or_right_point/new_point/call_government.py, super/want_point/week_and_person.py, super/different_time/number_and_part/high_man_or_different_government.py or super/way.py about important time"

---
+++ super/want_point/week_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    old_group(str_arg)
+    print('few_year_and_child')
+
+def old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('ask_early_year_into_few_week')
