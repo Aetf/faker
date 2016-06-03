2dcc3d35f3258c0d3625ca2df652a11b0a491df0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 10:52:00 -0400

    "rm: call super/next_eye/be_important_place/child/hand/problem/old_point.py in big time"

---
+++ super/next_eye/be_important_place/child/hand/problem/old_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact_or_hand(str_arg):
+    own_way(str_arg)
+    print('life')
+
+def own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact_or_hand('long_eye')
