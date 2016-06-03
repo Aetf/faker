a73e0bbaba8c29e3bad1da2b833a96d2113dcb18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-03 10:59:11 -0400

    "mv: be super/new_hand.py or super/group_and_world/own_case/world.py with right hand"

---
+++ super/new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    use_case_into_same_eye(str_arg)
+    print('time_and_way')
+
+def use_case_into_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('large_time_or_next_number')
