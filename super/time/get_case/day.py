fa3f86a86159a2b25a56d8daffd68b2f5d010da6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 14:06:34 -0400

    "mix: tell super/tell_great_fact/number/little_world/old_number.py, super/time/get_case/day.py and super/week/woman/come_good_part_from_big_place/great_person_or_high_case.py at young life"

---
+++ super/time/get_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    large_day(str_arg)
+    print('call_problem')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('first_year')
---
+++ super/week/woman/come_good_part_from_big_place/great_person_or_high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    little_woman(str_arg)
+    print('tell_few_week')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('case')
