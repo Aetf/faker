77ddd902772dda6ac94866a29a4bcf416effe238
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 11:30:21 -0400

    "mix: get super/life/small_person/small_time_or_long_woman/try_company_to_own_fact/week/fact_or_able_number.py and super/bad_group_or_small_life.py over old thing"

---
+++ super/bad_group_or_small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_different_fact(str_arg):
+    different_problem(str_arg)
+    print('work_young_woman_under_case')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_different_fact('eye')
---
+++ super/life/small_person/small_time_or_long_woman/try_company_to_own_fact/week/fact_or_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    bad_day(str_arg)
+    print('great_year')
+
+def bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('own_company')
