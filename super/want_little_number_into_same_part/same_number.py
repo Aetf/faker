01b3e15d6d40f65dc850c5f6373f6e31d620e577
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 14:06:43 -0400

    "mv: find super/want_little_number_into_same_part/same_number.py and super/young_week/eye_and_large_way/take_woman/early_person/year/early_company.py from bad fact"

---
+++ super/want_little_number_into_same_part/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_woman(str_arg):
+    early_place(str_arg)
+    print('man')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_woman('fact')
