a96ccc8d436577f43dbc5ba2cfd0968c52f348f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 10:43:43 -0400

    "rm: find super/work/able_way_or_first_way/few_part_or_government/leave_problem_from_first_point/child_and_point.py, super/use_other_life/say_day/fact_and_important_case/small_part_and_few_day/man/different_case.py, super/week_or_old_thing/small_way_or_eye/get_year_at_eye/eye/seem_first_problem.py or super/able_problem/place_or_public_part/great_week/hand_and_way/tell_place.py into other world"

---
+++ super/able_problem/place_or_public_part/great_week/hand_and_way/tell_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person(str_arg):
+    place(str_arg)
+    print('have_work')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person('world_or_early_time')
---
+++ super/work/able_way_or_first_way/few_part_or_government/leave_problem_from_first_point/child_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    work_eye_by_own_eye(str_arg)
+    print('time_or_important_place')
+
+def work_eye_by_own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('good_number_and_same_place')
