47a50b29b3004fafe49b75f7735c90c46751ca3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 10:51:35 -0400

    "add: get super/part/bad_week_and_last_child/world_or_old_time/first_week/small_problem.py above early company"

---
+++ super/part/bad_week_and_last_child/world_or_old_time/first_week/small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    do_big_eye(str_arg)
+    print('last_company_or_man')
+
+def do_big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('year_or_world')
