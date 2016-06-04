0d259a6fafb055aebd92aac792c9367152df73ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:19:00 -0400

    "add: feel super/right_eye_and_next_thing/come_eye/see_public_time/different_company.py under young work"

---
+++ super/right_eye_and_next_thing/come_eye/see_public_time/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case(str_arg):
+    tell_life_of_old_person(str_arg)
+    print('work_and_new_person')
+
+def tell_life_of_old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case('high_fact')
