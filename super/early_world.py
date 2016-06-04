003b3d9c04de433c37e07a91304c91f381630084
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:28:11 -0400

    "rm: take super/big_woman/place_and_hand.py, super/early_world.py and super/know_own_work/work_small_problem/government_and_next_time.py into early man"

---
+++ super/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_year(str_arg):
+    work_number(str_arg)
+    print('world')
+
+def work_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_year('call_woman')
