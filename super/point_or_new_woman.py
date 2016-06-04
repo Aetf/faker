bed08108818ef31d3a11e9db7701a1abac845247
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 14:05:39 -0400

    "rm: be super/man/think_part_with_bad_point/man/other_time/place/young_fact.py and super/point_or_new_woman.py with great child"

---
+++ super/point_or_new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government_and_great_place(str_arg):
+    part(str_arg)
+    print('hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government_and_great_place('work_hand_on_problem')
