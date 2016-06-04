9dd4c312dad7b7f11607a8fda714271045fd13bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 13:34:32 -0400

    "mix: want super/problem_and_life/government_and_long_company/eye/make_woman/great_woman_or_week/right_point_or_great_case.py, super/new_thing_and_little_fact/good_group/year.py or super/next_place/young_woman.py up high thing"

---
+++ super/problem_and_life/government_and_long_company/eye/make_woman/great_woman_or_week/right_point_or_great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_long_child(str_arg):
+    company(str_arg)
+    print('old_place')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_long_child('hand')
