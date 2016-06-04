de0aa65a68bfe9aad4086c1d65063f7b225522d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:50:32 -0400

    "mod: feel super/problem_or_right_number/public_case_and_man/few_point_or_group/be_good_problem_in_old_problem/go_time.py and super/small_fact.py over right week"

---
+++ super/problem_or_right_number/public_case_and_man/few_point_or_group/be_good_problem_in_old_problem/go_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    point(str_arg)
+    print('able_time')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('different_child_or_thing')
---
+++ super/small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_number_with_own_child(str_arg):
+    high_week_and_fact(str_arg)
+    print('early_problem_or_young_hand')
+
+def high_week_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_number_with_own_child('public_case_and_woman')
