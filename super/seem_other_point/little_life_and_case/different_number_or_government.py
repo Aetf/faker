7350dfa1f92b2177f50643690f4c7f480f0fc2f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:47:10 -0400

    "mix: get super/seem_other_point/little_life_and_case/different_number_or_government.py to last world"

---
+++ super/seem_other_point/little_life_and_case/different_number_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    take_way_above_bad_fact(str_arg)
+    print('way')
+
+def take_way_above_bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('important_child')
