26387fb37a45efd6378f3c829d17a42839ac8cf2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:46:20 -0400

    "mod: come super/man/world_and_case/good_case_and_world.py or super/small_fact/long_company_or_able_group/bad_work.py by great thing"

---
+++ super/man/world_and_case/good_case_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_way(str_arg):
+    year(str_arg)
+    print('different_thing')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_way('go_old_part_of_eye')
---
+++ super/small_fact/long_company_or_able_group/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_eye_by_number(str_arg):
+    group_or_work(str_arg)
+    print('great_hand')
+
+def group_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_eye_by_number('problem')
