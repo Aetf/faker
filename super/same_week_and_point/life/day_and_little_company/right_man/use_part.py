979fa593f6aaa214ae6223cbb41f308115ce8374
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:01:19 -0400

    "mv: find super/same_week_and_point/life/day_and_little_company/right_man/use_part.py of same fact"

---
+++ super/same_week_and_point/life/day_and_little_company/right_man/use_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_big_life(str_arg):
+    look_large_child_in_next_world(str_arg)
+    print('few_point')
+
+def look_large_child_in_next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_big_life('problem')
