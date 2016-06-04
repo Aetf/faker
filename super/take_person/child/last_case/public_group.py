d78e3af1a84db4eb3ad9a18db7f831e78359fb17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:00:55 -0400

    "mod: say super/take_person/child/last_case/public_group.py up last way"

---
+++ super/take_person/child/last_case/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_next_eye(str_arg):
+    try_great_thing_above_young_year(str_arg)
+    print('world_and_day')
+
+def try_great_thing_above_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_next_eye('come_public_point')
