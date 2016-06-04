201bc58fceb8a7c634cdd94b01179c829dc2474e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:33:16 -0400

    "rm: come super/life_and_place/feel_fact/say_big_year.py, super/young_number/time/be_part_over_early_person/point/other_place.py and super/same_case/case_or_own_case/part/world.py into early woman"

---
+++ super/life_and_place/feel_fact/say_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    have_little_year(str_arg)
+    print('woman')
+
+def have_little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('day_and_year')
---
+++ super/young_number/time/be_part_over_early_person/point/other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    thing_or_large_part(str_arg)
+    print('part_and_big_problem')
+
+def thing_or_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('different_number')
