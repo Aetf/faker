42a68389f313daf393ef00e0b5af715dfc6dcefb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 11:19:51 -0400

    "add: leave super/last_problem/good_way_or_same_number.py or super/first_hand.py by big group"

---
+++ super/last_problem/good_way_or_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    hand(str_arg)
+    print('next_eye_or_young_part')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('part')
