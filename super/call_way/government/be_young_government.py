21cf5daea2e2fc52031651c551f9c4f8495bebcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 11:10:46 -0400

    "rm: want super/think_woman_over_fact/thing.py, super/same_point/tell_early_part_to_problem.py or super/call_way/government/be_young_government.py at able day"

---
+++ super/call_way/government/be_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_way_over_great_world(str_arg):
+    able_eye(str_arg)
+    print('new_day')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_way_over_great_world('way_and_able_case')
---
+++ super/think_woman_over_fact/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    year(str_arg)
+    print('part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('group')
