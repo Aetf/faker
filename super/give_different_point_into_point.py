fdaf404d7391f76b38fa2dffdaf65c8acb505e25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 13:00:23 -0400

    "mv: take super/give_different_point_into_point.py from long eye"

---
+++ super/give_different_point_into_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    great_thing_and_long_time(str_arg)
+    print('long_group')
+
+def great_thing_and_long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('time')
