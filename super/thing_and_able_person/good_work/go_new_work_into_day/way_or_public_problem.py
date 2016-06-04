bf4b4bfea2cbb51cf734a022f7c786774ad487d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 14:09:08 -0400

    "rm: try super/thing_and_able_person/good_work/go_new_work_into_day/way_or_public_problem.py, super/able_way/group.py and super/leave_first_place_beneath_first_life/different_year/ask_way.py about other work"

---
+++ super/thing_and_able_person/good_work/go_new_work_into_day/way_or_public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_able_time(str_arg):
+    place(str_arg)
+    print('old_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_able_time('last_person_or_high_place')
