e7ba9777ecb65080fbe10424dcb52e65e4951b76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 14:54:11 -0400

    "mv: find super/point.py by important company"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_few_hand(str_arg):
+    week_and_week(str_arg)
+    print('last_company')
+
+def week_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_few_hand('think_thing_after_bad_company')
