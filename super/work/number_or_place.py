b67caf2e6f5a0629ae646813dfab18b872e04e50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 10:46:21 -0400

    "mix: want super/leave_number_under_fact/number/have_point_with_last_week.py, super/ask_own_week_in_first_point/last_number/take_week/eye_or_life/give_first_eye_to_hand/way.py and super/work/number_or_place.py above other part"

---
+++ super/work/number_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    good_world(str_arg)
+    print('hand')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('find_hand_with_government')
