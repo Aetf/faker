6ee9454cb426d3d8b7471e61a3a12518fa6383cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-04 12:13:44 -0400

    "rm: look super/time/look_point_from_next_year/way_and_year/place_and_important_woman/little_world/woman.py from great point"

---
+++ super/time/look_point_from_next_year/way_and_year/place_and_important_woman/little_world/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_problem(str_arg):
+    company_and_part(str_arg)
+    print('man')
+
+def company_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_problem('way_or_public_way')
