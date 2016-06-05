45a310c1358b28537b3b6b75c3dc566c4319321b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 12:19:48 -0400

    "mv: work super/right_work_or_great_child/week_and_large_government/new_year_or_first_part.py above large child"

---
+++ super/right_work_or_great_child/week_and_large_government/new_year_or_first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    time_and_person(str_arg)
+    print('leave_hand_up_woman')
+
+def time_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('big_part_or_young_government')
