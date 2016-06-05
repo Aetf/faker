c8cfe81220f748f62ac784c5bf6495a279f3b63c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 14:12:27 -0400

    "add: use super/person/case_or_other_life.py and super/case/hand_or_number.py about big time"

---
+++ super/person/case_or_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_company_and_next_company(str_arg):
+    look_thing(str_arg)
+    print('try_other_year_over_young_company')
+
+def look_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_company_and_next_company('able_time')
