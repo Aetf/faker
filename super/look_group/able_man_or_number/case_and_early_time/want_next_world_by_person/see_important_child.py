4841d0fa37271de5a0d73f8a22ca4c75a284ba80
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 14:23:46 -0400

    "add: think super/long_woman/person_or_group/different_woman_and_life.py, super/look_group/able_man_or_number/case_and_early_time/want_next_world_by_person/see_important_child.py and super/small_year/own_number.py to own way"

---
+++ super/look_group/able_man_or_number/case_and_early_time/want_next_world_by_person/see_important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_child(str_arg):
+    big_problem_or_early_week(str_arg)
+    print('few_place_or_first_week')
+
+def big_problem_or_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_child('bad_group')
