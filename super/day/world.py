b33c55722457780c7e7073e900463e36efb90e4f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 10:36:14 -0400

    "mix: be super/day/world.py, super/have_little_week/hand_and_young_work.py and super/early_thing/number/seem_part/eye/own_case/fact_or_big_person.py from same day"

---
+++ super/day/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case(str_arg):
+    important_point(str_arg)
+    print('case')
+
+def important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case('small_year')
---
+++ super/have_little_week/hand_and_young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    tell_big_work_above_public_world(str_arg)
+    print('work_thing')
+
+def tell_big_work_above_public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('same_man_and_company')
