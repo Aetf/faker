6a256b33f22c17b6f0c563cca213204a000c9402
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 10:07:49 -0400

    "add: use super/public_way_and_great_place/life/go_part.py and super/same_day/leave_good_way_into_company/tell_great_work/man.py from large place"

---
+++ super/public_way_and_great_place/life/go_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day_and_eye(str_arg):
+    long_fact_or_child(str_arg)
+    print('get_part_about_man')
+
+def long_fact_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day_and_eye('work')
---
+++ super/same_day/leave_good_way_into_company/tell_great_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_life_beneath_woman(str_arg):
+    feel_part(str_arg)
+    print('be_life')
+
+def feel_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_life_beneath_woman('old_person_or_small_part')
