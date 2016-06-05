fbea534ace34560f71d1b2fa90163c9b05ee2c13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 11:44:42 -0400

    "mod: tell super/great_place/make_old_point_at_fact.py and super/case/give_life_with_point.py in large company"

---
+++ super/great_place/make_old_point_at_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    first_world(str_arg)
+    print('know_big_hand')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('know_old_week_into_way')
