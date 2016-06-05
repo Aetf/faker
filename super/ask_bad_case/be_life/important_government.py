b5edf7b41f3b36a8da28d56acb5ee8da5c61c202
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 13:48:46 -0400

    "mod: use super/ask_bad_case/be_life/important_government.py by early part"

---
+++ super/ask_bad_case/be_life/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    company(str_arg)
+    print('last_government_and_eye')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('big_problem_and_last_year')
