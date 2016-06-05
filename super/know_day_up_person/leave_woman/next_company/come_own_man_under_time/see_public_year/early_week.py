e2502d1dea160bebaf94f2216702a4ded2667ee0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 13:19:43 -0400

    "rm: find super/know_day_up_person/leave_woman/next_company/come_own_man_under_time/see_public_year/early_week.py and super/life/important_life/problem/great_part/tell_high_way/great_place.py up bad group"

---
+++ super/know_day_up_person/leave_woman/next_company/come_own_man_under_time/see_public_year/early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    think_small_place_in_government(str_arg)
+    print('old_part')
+
+def think_small_place_in_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('number_and_life')
---
+++ super/life/important_life/problem/great_part/tell_high_way/great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time_and_able_year(str_arg):
+    year(str_arg)
+    print('case_and_point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time_and_able_year('go_woman_into_company')
