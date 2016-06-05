d53f920d9a37161885e4a12ad82250940b81cf39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 13:26:24 -0400

    "mv: give super/man_or_part.py or super/part/small_part_and_point/small_thing/number.py by big life"

---
+++ super/man_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_same_hand(str_arg):
+    long_problem_or_place(str_arg)
+    print('own_eye')
+
+def long_problem_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_same_hand('think_important_case')
---
+++ super/part/small_part_and_point/small_thing/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_or_hand(str_arg)
+    print('company')
+
+def work_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('go_problem_from_group')
