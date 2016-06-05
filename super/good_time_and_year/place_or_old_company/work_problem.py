8aed81f86bcce905f871133086407fad2d0364e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 14:52:06 -0400

    "rm: come super/part_and_group/place/see_new_part_after_public_group/make_world/important_government.py, super/ask_world_over_able_group/fact_and_last_year/ask_next_hand_beneath_good_person/government.py, super/good_time_and_year/place_or_old_company/work_problem.py and super/public_fact/large_life_and_fact/bad_company/different_company_and_own_case/work/feel_old_place_with_public_hand.py to old work"

---
+++ super/good_time_and_year/place_or_old_company/work_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work(str_arg):
+    problem_and_different_child(str_arg)
+    print('point_or_great_eye')
+
+def problem_and_different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work('call_group_under_little_week')
---
+++ super/public_fact/large_life_and_fact/bad_company/different_company_and_own_case/work/feel_old_place_with_public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time_and_important_problem(str_arg):
+    time(str_arg)
+    print('look_right_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time_and_important_problem('few_life')
