2d7d18966a6eb19a547d3e2aa72d62c0a108995f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 14:01:01 -0400

    "mix: think super/long_year_or_next_thing/child_or_number.py, super/find_public_government.py, super/get_young_point.py or super/use_little_world_up_eye/young_place_and_day/day_or_last_woman.py up small way"

---
+++ super/find_public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    big_number(str_arg)
+    print('child_and_day')
+
+def big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('old_work')
---
+++ super/use_little_world_up_eye/young_place_and_day/day_or_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    week(str_arg)
+    print('next_number')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('woman')
