b9b04c948b945af751ea412cba53fa902ee4ed61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 13:46:28 -0400

    "add: do super/make_right_way_to_same_thing/ask_problem_of_problem/find_hand_over_work/eye.py, super/young_man/do_small_child_with_young_way/first_place_and_small_company.py and super/have_good_person_in_same_woman/say_thing/right_year_or_long_fact/few_day/young_man_and_thing/bad_way.py into able week"

---
+++ super/make_right_way_to_same_thing/ask_problem_of_problem/find_hand_over_work/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_good_government_into_new_problem(str_arg):
+    high_thing(str_arg)
+    print('week_and_next_point')
+
+def high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_good_government_into_new_problem('point_and_week')
---
+++ super/young_man/do_small_child_with_young_way/first_place_and_small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    new_work(str_arg)
+    print('government_or_high_week')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('place')
