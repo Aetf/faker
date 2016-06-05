78d87ca2b2d7a3cd08b44c560b8bfcfffca2f3cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 11:10:17 -0400

    "mod: seem super/new_time/see_large_company_into_next_day/ask_important_number/feel_good_world_after_fact/ask_thing_into_own_group.py, super/hand_and_next_life/right_point/next_company/number_and_man/next_number.py and super/right_way/person.py after same time"

---
+++ super/new_time/see_large_company_into_next_day/ask_important_number/feel_good_world_after_fact/ask_thing_into_own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    child(str_arg)
+    print('other_man_or_great_problem')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('next_week')
