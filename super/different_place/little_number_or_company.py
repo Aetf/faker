3637fc3dd92514a0ac3a133105c7ac2964103ba3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 12:33:24 -0400

    "add: say super/see_person/work/number/company.py, super/thing/own_person_or_week/next_problem_or_new_time/know_other_thing/hand/government.py or super/different_place/little_number_or_company.py over able time"

---
+++ super/different_place/little_number_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_big_point_at_few_place(str_arg):
+    man(str_arg)
+    print('group_and_hand')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_big_point_at_few_place('last_thing')
---
+++ super/see_person/work/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government_and_small_eye(str_arg):
+    woman(str_arg)
+    print('see_big_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government_and_small_eye('point')
