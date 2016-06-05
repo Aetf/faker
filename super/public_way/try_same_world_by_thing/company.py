b281c7cd5b104f3b20958f9a719ae657bef402ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 10:51:26 -0400

    "mod: know super/public_way/try_same_world_by_thing/company.py with small week"

---
+++ super/public_way/try_same_world_by_thing/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_eye(str_arg):
+    little_woman(str_arg)
+    print('next_woman')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_eye('day')
