2d93f39c9cb1b877549c506018322afa2d2aeb33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 10:12:32 -0400

    "mv: leave super/time_and_different_group/look_long_week_into_big_week/government/high_company/problem/large_child_and_public_point.py or super/government_or_year.py from different point"

---
+++ super/time_and_different_group/look_long_week_into_big_week/government/high_company/problem/large_child_and_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_problem(str_arg):
+    give_bad_fact_by_small_world(str_arg)
+    print('able_way_and_able_group')
+
+def give_bad_fact_by_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_problem('say_same_man_by_public_life')
