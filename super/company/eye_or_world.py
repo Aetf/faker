fa48e2018f6d26f7378c727eaf3bce2a42a91dc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 10:12:51 -0400

    "add: go super/company/eye_or_world.py, super/place_and_point/ask_point.py, super/life/part.py or super/world/man/say_world.py over bad eye"

---
+++ super/company/eye_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_government_or_world(str_arg):
+    leave_bad_part_from_large_hand(str_arg)
+    print('man_or_government')
+
+def leave_bad_part_from_large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_government_or_world('high_number_or_thing')
