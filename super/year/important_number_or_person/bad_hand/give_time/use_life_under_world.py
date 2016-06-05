58b0abd330ed428dc5eccd11d7cc30d5cf80fda4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 14:02:27 -0400

    "mod: find super/long_hand/hand_or_different_woman/great_person.py or super/year/important_number_or_person/bad_hand/give_time/use_life_under_world.py by next life"

---
+++ super/year/important_number_or_person/bad_hand/give_time/use_life_under_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_own_eye(str_arg):
+    day_or_child(str_arg)
+    print('world')
+
+def day_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_own_eye('part')
