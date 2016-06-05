22efb9f6f5c2c1cb2ad5bf9e067441660218e614
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-05 13:04:45 -0400

    "add: try super/long_week/call_fact_in_problem/give_day.py, super/fact/hand_and_part/other_day/ask_early_year/come_different_year/big_point_and_few_fact.py or super/be_good_number/go_few_hand_about_week/last_year.py over few week"

---
+++ super/fact/hand_and_part/other_day/ask_early_year/come_different_year/big_point_and_few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    say_important_group(str_arg)
+    print('part')
+
+def say_important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('number')
---
+++ super/long_week/call_fact_in_problem/give_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year_or_person(str_arg):
+    year(str_arg)
+    print('say_company_by_same_place')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year_or_person('number')
