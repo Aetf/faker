0b4abb103208eca4f7064b1fb3a5be4c5973011d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 14:01:25 -0400

    "mix: have super/use_different_company_above_small_thing.py or super/be_good_number/have_company/work_different_number/old_number/new_person.py from public government"

---
+++ super/be_good_number/have_company/work_different_number/old_number/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world_and_large_world(str_arg):
+    long_man(str_arg)
+    print('man_and_old_time')
+
+def long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world_and_large_world('bad_work')
