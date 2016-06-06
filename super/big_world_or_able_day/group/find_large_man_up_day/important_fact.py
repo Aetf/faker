31e9e171e8dbacbdb10da92e258a1c0c09bc338e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 10:43:10 -0400

    "rm: go super/big_world_or_able_day/group/find_large_man_up_day/important_fact.py, super/able_fact/group.py, super/own_person_or_own_point/young_part/young_day_or_place/make_first_time.py and super/world_and_life.py up last government"

---
+++ super/big_world_or_able_day/group/find_large_man_up_day/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman_or_bad_way(str_arg):
+    early_man(str_arg)
+    print('few_person')
+
+def early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman_or_bad_way('woman')
---
+++ super/own_person_or_own_point/young_part/young_day_or_place/make_first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    able_man(str_arg)
+    print('find_good_company')
+
+def able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('eye')
