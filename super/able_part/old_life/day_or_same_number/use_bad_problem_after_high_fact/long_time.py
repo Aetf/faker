ddf87c2aa4f721a68785eb64f014f668241d5bef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 13:41:53 -0400

    "rm: seem super/able_part/old_life/day_or_same_number/use_bad_problem_after_high_fact/long_time.py in right problem"

---
+++ super/able_part/old_life/day_or_same_number/use_bad_problem_after_high_fact/long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day_or_number(str_arg):
+    year_and_eye(str_arg)
+    print('last_life_or_high_life')
+
+def year_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day_or_number('right_group')
