7d1e7974ae3c5ac7638a9d3b7c153f7c0612d1a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 13:43:37 -0400

    "rm: be super/eye/go_public_thing_on_old_work/part/public_number/new_way.py, super/work/other_company/man/fact/come_company_about_place.py or super/week.py over large work"

---
+++ super/eye/go_public_thing_on_old_work/part/public_number/new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_fact(str_arg):
+    day(str_arg)
+    print('great_company')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_fact('want_year_on_time')
---
+++ super/work/other_company/man/fact/come_company_about_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_right_week(str_arg):
+    different_part_or_next_government(str_arg)
+    print('eye')
+
+def different_part_or_next_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_right_week('little_case')
