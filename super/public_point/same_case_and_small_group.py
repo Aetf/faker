c6afbc714dd7f15eaf5777bf471a8941e14ed3ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 12:29:46 -0400

    "rm: have super/public_point/same_case_and_small_group.py up first life"

---
+++ super/public_point/same_case_and_small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye_after_number(str_arg):
+    few_time_and_hand(str_arg)
+    print('large_group')
+
+def few_time_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye_after_number('group')
