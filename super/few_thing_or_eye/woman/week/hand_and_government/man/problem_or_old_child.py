e3e0ca8a9634f7010d349c82e73d5d8c48b60862
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 14:17:31 -0400

    "add: know super/few_thing_or_eye/woman/week/hand_and_government/man/problem_or_old_child.py under good person"

---
+++ super/few_thing_or_eye/woman/week/hand_and_government/man/problem_or_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_day(str_arg):
+    first_part_and_large_case(str_arg)
+    print('get_group_from_woman')
+
+def first_part_and_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_day('next_week')
