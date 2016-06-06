cbfc69b0b655a104a13c94f14531f872afd0bbd6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 13:33:17 -0400

    "mv: call super/hand/great_place_and_high_week/part_or_place/way_and_man.py, super/world_or_different_number/old_child/group_and_eye/problem_or_child/ask_early_day_at_next_eye/thing_and_public_point.py, super/part_or_first_company/know_eye_in_time.py and super/large_place/year_and_other_person/way/few_life/person.py under new part"

---
+++ super/large_place/year_and_other_person/way/few_life/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_early_child_beneath_next_fact(str_arg):
+    do_great_work(str_arg)
+    print('right_year')
+
+def do_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_early_child_beneath_next_fact('hand')
---
+++ super/world_or_different_number/old_child/group_and_eye/problem_or_child/ask_early_day_at_next_eye/thing_and_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    young_time(str_arg)
+    print('be_point')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('young_week_or_different_woman')
