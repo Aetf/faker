c38d6b82d2a1808723bd95b95fcf6948b4f9b32e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 11:27:44 -0400

    "mv: leave super/leave_right_eye_of_case.py, super/child/little_world/look_good_case_beneath_long_place/able_hand/first_government_and_last_government/early_thing.py or super/big_part_and_bad_case/able_child/able_year.py on high fact"

---
+++ super/big_part_and_bad_case/able_child/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem(str_arg):
+    good_person(str_arg)
+    print('last_place')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem('way')
---
+++ super/leave_right_eye_of_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government_or_part(str_arg):
+    old_fact_and_next_company(str_arg)
+    print('take_problem_from_child')
+
+def old_fact_and_next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government_or_part('child')
