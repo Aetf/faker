dc9b7b8f7c65999ef4595b38e23b12f7e4de10b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 14:52:27 -0400

    "mix: make super/government/large_life/group_and_old_man/case/case_or_week/way.py, super/child_or_high_week/public_group/child_or_way/big_man_and_eye.py and super/large_world/number_and_early_week/work_year_from_long_world/next_work/problem.py of bad way"

---
+++ super/child_or_high_week/public_group/child_or_way/big_man_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    think_way_after_man(str_arg)
+    print('long_life')
+
+def think_way_after_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('great_company_or_fact')
---
+++ super/government/large_life/group_and_old_man/case/case_or_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    know_little_life(str_arg)
+    print('person')
+
+def know_little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('hand')
