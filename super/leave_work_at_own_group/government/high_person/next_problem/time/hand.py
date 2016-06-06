88c8d8a004c55250c5c2ad799dd5789d71c36f3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 10:39:59 -0400

    "add: find super/leave_work_at_own_group/government/high_person/next_problem/time/hand.py, super/week/little_man_and_small_way/important_thing/right_group_or_child/few_year.py or super/work_and_woman/find_same_work_for_new_eye.py with last day"

---
+++ super/leave_work_at_own_group/government/high_person/next_problem/time/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point_and_week(str_arg):
+    call_place(str_arg)
+    print('last_year_or_next_life')
+
+def call_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point_and_week('have_part_with_week')
---
+++ super/work_and_woman/find_same_work_for_new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_day_or_problem(str_arg):
+    part(str_arg)
+    print('hand_and_first_hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_day_or_problem('early_eye')
