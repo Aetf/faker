948b22ef0d22da9e3e62133cb83710b83c2bea73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 12:39:18 -0400

    "mv: seem super/thing/feel_early_group_with_person/seem_woman/high_world_and_woman/great_day_or_way/group_and_day.py at little group"

---
+++ super/thing/feel_early_group_with_person/seem_woman/high_world_and_woman/great_day_or_way/group_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week_or_new_week(str_arg)
+    print('place_and_work')
+
+def week_or_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_part')
