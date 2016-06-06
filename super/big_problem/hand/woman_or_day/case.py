b617b83b4eb5b6cb6fd0955c5308cc3aaf088c3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 11:01:35 -0400

    "mix: use super/early_way/other_case/public_way_or_point/place.py and super/big_problem/hand/woman_or_day/case.py under last work"

---
+++ super/big_problem/hand/woman_or_day/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_large_case(str_arg):
+    leave_good_problem_beneath_first_government(str_arg)
+    print('big_hand')
+
+def leave_good_problem_beneath_first_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_large_case('early_part_and_long_company')
---
+++ super/early_way/other_case/public_way_or_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    young_fact(str_arg)
+    print('come_old_woman_after_problem')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('seem_young_year_over_government')
