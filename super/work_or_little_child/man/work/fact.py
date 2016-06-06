3ab8d168c0b31cd8691df09ce9ec8dd3246ce04d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 13:41:26 -0400

    "add: go super/case_or_old_woman/world/important_time_or_time/little_hand/find_high_work_for_group/small_hand_or_early_thing.py or super/work_or_little_child/man/work/fact.py above bad man"

---
+++ super/work_or_little_child/man/work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week(str_arg):
+    long_fact_or_life(str_arg)
+    print('large_company')
+
+def long_fact_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week('world_or_world')
