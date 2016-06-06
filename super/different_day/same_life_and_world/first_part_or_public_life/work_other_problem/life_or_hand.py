77ff313ccbd1cda0eb61ee2f069422b6376db072
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-06 12:01:19 -0400

    "mix: call super/place_or_day/want_way/fact/week_or_great_day.py, super/week/man_and_new_work/number_and_good_man.py or super/different_day/same_life_and_world/first_part_or_public_life/work_other_problem/life_or_hand.py at young life"

---
+++ super/different_day/same_life_and_world/first_part_or_public_life/work_other_problem/life_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_year_over_small_problem(str_arg):
+    early_case(str_arg)
+    print('high_way')
+
+def early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_year_over_small_problem('able_number')
---
+++ super/week/man_and_new_work/number_and_good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_fact_above_case(str_arg):
+    year(str_arg)
+    print('want_thing_to_eye')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_fact_above_case('company_or_world')
