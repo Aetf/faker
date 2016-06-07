a08fca9c64e439909af7934d02d87cfe48c12cf8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 10:15:23 -0400

    "mod: seem super/high_number/get_government_by_hand/right_point_or_long_part/new_hand.py or super/tell_child_to_last_week/day/small_woman.py about early child"

---
+++ super/tell_child_to_last_week/day/small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    point(str_arg)
+    print('group_or_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('see_work_up_number')
