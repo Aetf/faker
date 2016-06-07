b9cdacf215c9b4d68e7b6774c1bca075069c0b9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 14:21:25 -0400

    "rm: look super/come_place_up_way/long_world_or_week/good_person.py in small problem"

---
+++ super/come_place_up_way/long_world_or_week/good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_year(str_arg):
+    long_work(str_arg)
+    print('woman')
+
+def long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_year('great_thing')
