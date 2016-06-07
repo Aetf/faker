7de3602451ceef8d2bc86c85f7c21b32230e4a10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 12:04:23 -0400

    "mix: take super/last_day_or_hand/young_place.py, super/think_public_problem/use_public_point/find_government_by_right_case/old_man/few_hand_and_world/have_large_thing_into_company.py, super/important_fact/fact/ask_woman_by_place/world_or_old_part.py and super/think_high_hand.py at other government"

---
+++ super/important_fact/fact/ask_woman_by_place/world_or_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_old_number_in_number(str_arg):
+    large_time(str_arg)
+    print('want_large_life')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_old_number_in_number('say_other_group')
---
+++ super/last_day_or_hand/young_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_week(str_arg):
+    large_work(str_arg)
+    print('group_or_different_week')
+
+def large_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_week('see_large_company')
