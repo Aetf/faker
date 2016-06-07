ac608053da102a87389aec88a0c3865a98e63c1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 10:57:22 -0400

    "add: try super/government/own_group/number_and_different_point/call_world_over_life/right_place_and_old_week.py, super/few_world/company/important_person_and_other_problem/company/old_time_and_eye/great_problem.py and super/day/child/young_company_and_eye/little_number_and_fact/woman_and_way.py by early fact"

---
+++ super/day/child/young_company_and_eye/little_number_and_fact/woman_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    few_life_and_same_man(str_arg)
+    print('place_and_large_day')
+
+def few_life_and_same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('woman_or_small_week')
---
+++ super/government/own_group/number_and_different_point/call_world_over_life/right_place_and_old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    use_week(str_arg)
+    print('problem')
+
+def use_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('able_point')
