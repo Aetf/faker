f0a43755e49c9c09fda670d52b92dfd5973b5d8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 11:00:24 -0400

    "rm: feel super/first_child.py, super/case_or_important_way/do_high_person/day_or_person/hand.py or super/woman_and_child/last_day_or_long_eye.py for little week"

---
+++ super/first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_time(str_arg):
+    man(str_arg)
+    print('make_small_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_time('fact')
