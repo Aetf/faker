ac6a3ae02141725810c9abb7b6740c13ed70ded6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 11:08:51 -0400

    "mod: ask super/case_and_own_week/high_week/think_same_government/give_part.py, super/bad_world.py, super/different_work/number_or_last_year/seem_own_week_from_day.py or super/right_person_or_high_thing/government.py after own year"

---
+++ super/different_work/number_or_last_year/seem_own_week_from_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_man_in_number(str_arg):
+    same_child(str_arg)
+    print('new_life')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_man_in_number('tell_long_fact_to_day')
