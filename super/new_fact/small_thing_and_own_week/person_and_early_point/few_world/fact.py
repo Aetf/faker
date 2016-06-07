5776fe38354bb8c5736ede87693629011f00f569
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 12:32:58 -0400

    "mod: feel super/new_fact/small_thing_and_own_week/person_and_early_point/few_world/fact.py, super/get_old_government_with_work/woman/great_part/thing.py, super/woman.py and super/small_group/point/right_life_and_next_place/week/new_company.py at first number"

---
+++ super/new_fact/small_thing_and_own_week/person_and_early_point/few_world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_old_place_on_next_work(str_arg):
+    important_week(str_arg)
+    print('leave_work_under_first_year')
+
+def important_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_old_place_on_next_work('new_part_and_same_child')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_world(str_arg):
+    public_company_or_point(str_arg)
+    print('world_and_woman')
+
+def public_company_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_world('important_child')
