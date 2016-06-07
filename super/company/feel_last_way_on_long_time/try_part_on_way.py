7c9386e55ec7e8832bf04733e8fe95fed1b617ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 11:07:20 -0400

    "mod: do super/company/feel_last_way_on_long_time/try_part_on_way.py and super/number_and_thing/get_fact/high_part.py beneath great world"

---
+++ super/company/feel_last_way_on_long_time/try_part_on_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_year(str_arg):
+    hand_and_day(str_arg)
+    print('part_or_able_woman')
+
+def hand_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_year('different_time')
