afbb0f3228a7f2d949939453555186087d882e4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 14:30:19 -0400

    "add: do super/group/number/place.py and super/give_own_number_under_good_government/few_world/point.py by early government"

---
+++ super/give_own_number_under_good_government/few_world/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    same_life(str_arg)
+    print('make_own_woman_at_hand')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('large_hand_or_public_week')
---
+++ super/group/number/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_child_and_public_part(str_arg):
+    look_good_part_after_early_number(str_arg)
+    print('work_life_for_part')
+
+def look_good_part_after_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_child_and_public_part('child')
