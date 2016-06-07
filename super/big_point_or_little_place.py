d4013722076f4b098b02b58417d32b2f0744bb50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 12:25:35 -0400

    "mod: make super/big_point_or_little_place.py of first hand"

---
+++ super/big_point_or_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world(str_arg):
+    big_man_and_place(str_arg)
+    print('new_world')
+
+def big_man_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world('time')
