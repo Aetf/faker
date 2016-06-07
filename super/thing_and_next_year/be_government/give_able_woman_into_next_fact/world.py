a13af6bd8621872b50bef149281bea7333a91801
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 11:47:04 -0400

    "rm: know super/work/hand_and_great_problem/young_number_or_year/use_good_problem_by_man.py and super/thing_and_next_year/be_government/give_able_woman_into_next_fact/world.py about young problem"

---
+++ super/thing_and_next_year/be_government/give_able_woman_into_next_fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_fact(str_arg):
+    place(str_arg)
+    print('point')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_fact('large_number')
