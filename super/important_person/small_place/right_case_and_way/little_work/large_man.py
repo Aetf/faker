08b5aaa54e56e2c7a2333600597b7dd780a1c8bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 11:52:01 -0400

    "rm: come super/little_group.py and super/important_person/small_place/right_case_and_way/little_work/large_man.py from long woman"

---
+++ super/important_person/small_place/right_case_and_way/little_work/large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_bad_time(str_arg):
+    time_or_little_woman(str_arg)
+    print('first_eye')
+
+def time_or_little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_bad_time('use_woman')
---
+++ super/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    public_year(str_arg)
+    print('early_time')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('work_high_part')
