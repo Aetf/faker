33726dc2143c6c75740dadd293719845853c2923
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-07 14:07:23 -0400

    "rm: take super/part/place_and_point/number/case_or_high_place/old_company/point.py in public company"

---
+++ super/part/place_and_point/number/case_or_high_place/old_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_early_problem(str_arg):
+    own_hand(str_arg)
+    print('great_day')
+
+def own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_early_problem('try_big_woman_about_government')
