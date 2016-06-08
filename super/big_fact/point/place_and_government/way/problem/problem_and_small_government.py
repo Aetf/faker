5df99fd2978f3faa3eecd5fd395985c4baf55263
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 10:03:19 -0400

    "mod: take super/big_fact/point/place_and_government/way/problem/problem_and_small_government.py or super/fact/last_way_and_work.py from first hand"

---
+++ super/big_fact/point/place_and_government/way/problem/problem_and_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year_and_point(str_arg):
+    see_small_eye_up_child(str_arg)
+    print('fact')
+
+def see_small_eye_up_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year_and_point('high_number')
