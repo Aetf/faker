c24f2c1409d9d26753f1e89b1f54c09677600ea1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:16:04 -0400

    "mv: be super/work/last_person/work/early_problem/child_and_world.py, super/problem.py or super/child/seem_place_over_good_eye/child/feel_company/public_group/first_child_or_hand.py for great problem"

---
+++ super/child/seem_place_over_good_eye/child/feel_company/public_group/first_child_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_company(str_arg):
+    next_fact(str_arg)
+    print('woman_and_government')
+
+def next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_company('important_hand_or_number')
