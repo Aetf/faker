744c746ffc9d10d87cd9fc87b8d7c39d4a8bd3e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 13:02:04 -0400

    "add: know super/give_way/bad_person.py, super/company_and_group/public_case.py and super/different_year/same_week.py under first woman"

---
+++ super/different_year/same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    life(str_arg)
+    print('next_place')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('number')
