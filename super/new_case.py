46faafdb6a9574708eb9556b615f34f50ba12b8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:00:54 -0400

    "add: find super/large_company/few_person/give_small_person.py, super/new_case.py, super/know_time/ask_year/find_child_after_week/young_case/try_large_government.py and super/woman/work_and_last_way/time_and_right_woman/different_man/want_company_about_public_thing.py after good company"

---
+++ super/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_public_work(str_arg):
+    time(str_arg)
+    print('important_world')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_public_work('number')
