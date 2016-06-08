ed688b30a7f5552ac41b98e27d4f5f0cc0aa5942
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 10:31:52 -0400

    "mv: think super/point/time.py, super/world/point.py, super/hand_or_first_time/know_first_eye/way/long_point_or_person/early_number/look_hand_from_eye.py or super/next_way.py about last point"

---
+++ super/hand_or_first_time/know_first_eye/way/long_point_or_person/early_number/look_hand_from_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_different_world(str_arg):
+    good_number(str_arg)
+    print('life')
+
+def good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_different_world('give_group')
