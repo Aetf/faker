b0f14b2e4bebc2722f238b96e51bf474c5d6a46d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 12:06:21 -0400

    "add: look super/number/find_way_of_other_way/great_fact/problem/hand/number.py, super/bad_day_and_own_man/part_or_old_week/good_hand/think_old_hand.py or super/tell_long_point/able_place/small_week/eye_and_way.py into small fact"

---
+++ super/bad_day_and_own_man/part_or_old_week/good_hand/think_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_man(str_arg):
+    group_or_important_thing(str_arg)
+    print('way_and_point')
+
+def group_or_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_man('group_and_work')
---
+++ super/tell_long_point/able_place/small_week/eye_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    bad_group(str_arg)
+    print('company')
+
+def bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('young_group')
