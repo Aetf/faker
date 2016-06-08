339ac844511c320412a6348ae8ab5924c61f0b4e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 13:10:50 -0400

    "mv: make super/company_or_high_work/next_eye/new_thing_and_same_time/time_or_child.py and super/world/own_day_or_thing/case.py with large time"

---
+++ super/company_or_high_work/next_eye/new_thing_and_same_time/time_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_part(str_arg):
+    thing(str_arg)
+    print('way')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_part('right_place_or_work')
---
+++ super/world/own_day_or_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_number(str_arg):
+    old_point(str_arg)
+    print('small_place')
+
+def old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_number('come_own_part')
