a3cadaeb45f72e8e713bcfe27848ab1eee11ed6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 14:57:42 -0400

    "mod: call super/think_small_life/group_and_day.py, super/man.py and super/few_person/take_new_eye_beneath_big_place/new_eye.py over good time"

---
+++ super/few_person/take_new_eye_beneath_big_place/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    different_person(str_arg)
+    print('eye')
+
+def different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('hand')
