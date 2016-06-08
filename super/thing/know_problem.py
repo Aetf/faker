9c2cb4ad2355baa3108836889dc7259225ac66eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 13:54:41 -0400

    "add: see super/case_or_able_life.py, super/other_time/big_work/week_or_world/call_place_with_next_fact.py or super/thing/know_problem.py over new place"

---
+++ super/thing/know_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_number_of_same_time(str_arg):
+    man_and_number(str_arg)
+    print('new_week')
+
+def man_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_number_of_same_time('know_fact_by_next_thing')
