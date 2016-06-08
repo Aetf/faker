bfc990ef5431c8457f51c044fa2aff2380c51d30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 14:22:04 -0400

    "mod: look super/new_part/find_child_on_time/seem_early_group.py over different thing"

---
+++ super/new_part/find_child_on_time/seem_early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    important_world(str_arg)
+    print('young_work')
+
+def important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('same_life')
