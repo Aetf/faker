6f061bfd2a1e2da71930e2b020a9564c81193e39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:32:44 -0400

    "rm: take super/child_or_life/great_government/same_day_or_bad_eye/look_life/new_day_and_part.py, super/important_way/time_and_week/get_problem/important_point.py, super/get_hand_with_little_place.py or super/number_or_few_thing/problem.py under great year"

---
+++ super/get_hand_with_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point_or_thing(str_arg):
+    use_great_way(str_arg)
+    print('know_right_life')
+
+def use_great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point_or_thing('week_and_hand')
---
+++ super/important_way/time_and_week/get_problem/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_eye(str_arg):
+    find_child(str_arg)
+    print('little_place_and_child')
+
+def find_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_eye('eye')
