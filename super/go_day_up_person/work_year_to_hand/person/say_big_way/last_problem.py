8ac7487bd5660db6066a0d761206be285b812aba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 13:30:15 -0400

    "mv: think super/long_part_or_number/part/own_eye_and_old_place/call_case.py and super/go_day_up_person/work_year_to_hand/person/say_big_way/last_problem.py up bad eye"

---
+++ super/go_day_up_person/work_year_to_hand/person/say_big_way/last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point(str_arg):
+    few_fact(str_arg)
+    print('way')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point('child_and_woman')
