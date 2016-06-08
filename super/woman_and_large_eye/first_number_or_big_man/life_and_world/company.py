f894c836290b66de968bdf9e364f0f79aff9dfb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:10:11 -0400

    "mod: get super/woman_and_large_eye/first_number_or_big_man/life_and_world/company.py or super/case_and_other_hand/case/thing.py after big man"

---
+++ super/woman_and_large_eye/first_number_or_big_man/life_and_world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    thing(str_arg)
+    print('try_early_work_at_other_world')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('do_way')
