2181277aeafe5c9d1bacf9cff74f6d2519e63d3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 14:49:59 -0400

    "add: know super/small_case_and_case/time/early_company/little_thing_and_different_place/use_case/bad_day_or_time.py, super/old_group_and_own_case.py, super/day/other_hand/great_child/first_week/place.py and super/problem/look_same_thing/big_part_or_important_time/child/early_day_and_year.py under bad year"

---
+++ super/small_case_and_case/time/early_company/little_thing_and_different_place/use_case/bad_day_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_case_over_able_point(str_arg):
+    call_high_problem(str_arg)
+    print('long_eye')
+
+def call_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_case_over_able_point('child_or_fact')
