e96e50759ce0340095477d694fba76f92b0e1ad3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 13:03:57 -0400

    "add: make super/ask_big_way_at_time/day/look_early_week/try_thing_into_long_child/next_work/use_company_on_part.py, super/first_part/important_eye/life/own_week_or_part.py or super/hand/able_company_or_government/different_world/able_way/life_or_long_hand.py of next part"

---
+++ super/ask_big_way_at_time/day/look_early_week/try_thing_into_long_child/next_work/use_company_on_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_child(str_arg):
+    able_fact(str_arg)
+    print('few_life_and_public_year')
+
+def able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_child('leave_new_year')
