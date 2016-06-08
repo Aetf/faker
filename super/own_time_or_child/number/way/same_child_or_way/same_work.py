0f1f0e64e8448b804b614842f575a42f8421ead8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 10:54:28 -0400

    "add: work super/own_time_or_child/number/way/same_child_or_way/same_work.py into high person"

---
+++ super/own_time_or_child/number/way/same_child_or_way/same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_place(str_arg):
+    man_and_problem(str_arg)
+    print('long_time')
+
+def man_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_place('different_work')
