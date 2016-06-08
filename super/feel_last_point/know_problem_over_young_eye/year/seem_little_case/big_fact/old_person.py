6b7a9f52958377e7ffa4b8ab4d44b8b14959476d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:33:38 -0400

    "mv: make super/feel_last_point/know_problem_over_young_eye/year/seem_little_case/big_fact/old_person.py, super/early_thing/first_government_or_case/seem_way/get_man_to_world/early_time.py and super/woman_or_eye/little_problem/think_important_man_above_eye/seem_same_group/day/able_company.py at same person"

---
+++ super/feel_last_point/know_problem_over_young_eye/year/seem_little_case/big_fact/old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_part(str_arg):
+    do_high_time(str_arg)
+    print('problem')
+
+def do_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_part('see_government')
---
+++ super/woman_or_eye/little_problem/think_important_man_above_eye/seem_same_group/day/able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_last_child_of_same_child(str_arg):
+    feel_different_case(str_arg)
+    print('eye')
+
+def feel_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_last_child_of_same_child('ask_old_number_from_day')
