addf4e489a04f6ea0589a137e41a4bab8866149d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 12:42:04 -0400

    "mv: use super/see_year.py up few work"

---
+++ super/see_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    do_number(str_arg)
+    print('give_work')
+
+def do_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('high_case')
