ef8989a56ccd17fd3b609fa860793cc8f0bce299
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 12:47:52 -0400

    "add: try super/able_life_or_different_government/think_year/other_year_or_way/first_man.py, super/first_work_and_fact.py, super/fact_or_long_way/next_point_and_public_case/small_year_and_life/try_part_from_year.py or super/woman_or_old_year/government_and_way.py from small government"

---
+++ super/fact_or_long_way/next_point_and_public_case/small_year_and_life/try_part_from_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_number(str_arg):
+    government(str_arg)
+    print('same_week')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_number('bad_child')
---
+++ super/first_work_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_fact(str_arg):
+    fact(str_arg)
+    print('give_early_life')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_fact('see_work_above_good_woman')
