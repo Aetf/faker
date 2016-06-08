e17ccf18b855fd892390e1658f7d4c8f45660c48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:03:12 -0400

    "rm: tell super/have_small_fact_up_person.py, super/next_number.py or super/public_world/long_point/next_world_or_day/hand.py up long problem"

---
+++ super/next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_public_child_at_important_thing(str_arg):
+    problem(str_arg)
+    print('first_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_public_child_at_important_thing('part_and_early_year')
---
+++ super/public_world/long_point/next_world_or_day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_thing(str_arg):
+    last_part_or_public_man(str_arg)
+    print('company_or_group')
+
+def last_part_or_public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_thing('week')
