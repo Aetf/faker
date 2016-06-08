b71fc0baf6b66bab318c4dca5466c4576da1ab73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 10:49:22 -0400

    "add: see super/last_part.py and super/be_point.py above high man"

---
+++ super/last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group_and_eye(str_arg):
+    think_high_way(str_arg)
+    print('old_eye')
+
+def think_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group_and_eye('different_man')
