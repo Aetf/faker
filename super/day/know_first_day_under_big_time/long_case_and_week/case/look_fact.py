12355db2288645235b2b26ff6f9dc4684c96997e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-08 11:22:40 -0400

    "rm: be super/day/know_first_day_under_big_time/long_case_and_week/case/look_fact.py or super/child/different_part/ask_big_year/few_case.py in first thing"

---
+++ super/day/know_first_day_under_big_time/long_case_and_week/case/look_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing(str_arg):
+    own_company(str_arg)
+    print('small_week_and_good_child')
+
+def own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing('important_man_or_fact')
