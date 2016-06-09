ecc0f635fc25668446c43e6fcc5592aa741f3433
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:00:32 -0400

    "rm: have super/thing_and_old_group/early_place.py from good hand"

---
+++ super/thing_and_old_group/early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    life(str_arg)
+    print('case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('feel_own_week')
