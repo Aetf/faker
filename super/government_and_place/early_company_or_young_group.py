bcd7ccc929014be48ae5c2fc343225803ff13d13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 14:05:30 -0400

    "mod: be super/government_and_place/early_company_or_young_group.py, super/take_early_time_in_world/thing.py or super/know_way_of_government/make_different_place/own_thing/way_or_government/hand_and_part.py under big problem"

---
+++ super/government_and_place/early_company_or_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_point_by_work(str_arg):
+    high_way(str_arg)
+    print('new_world')
+
+def high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_point_by_work('hand')
