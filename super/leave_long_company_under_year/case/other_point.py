796dc25640b36defda03f10469603dac72a61c61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 12:20:45 -0400

    "mod: seem super/leave_long_company_under_year/case/other_point.py to other week"

---
+++ super/leave_long_company_under_year/case/other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_time(str_arg):
+    little_week(str_arg)
+    print('world_or_early_way')
+
+def little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_time('little_life_and_next_time')
