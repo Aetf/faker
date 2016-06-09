4b6a4ae4718131507bc13caae0948e77032ab349
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 14:46:40 -0400

    "rm: get super/few_eye_or_eye.py under last place"

---
+++ super/few_eye_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    young_fact_or_work(str_arg)
+    print('large_company_and_person')
+
+def young_fact_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('eye')
