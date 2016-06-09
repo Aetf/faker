a6769c92bed3d796985ea65d5167b745ab0bc8cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 10:29:25 -0400

    "add: look super/give_person_up_same_government/week.py, super/work/try_other_place/problem_or_time/big_life_and_right_work.py, super/last_government_or_part/year/early_case_and_case/good_case/go_year.py or super/great_part/part_or_number/high_week_or_number/ask_old_number_of_thing/long_eye.py on new hand"

---
+++ super/give_person_up_same_government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_work_or_hand(str_arg):
+    think_next_fact(str_arg)
+    print('know_hand_into_same_eye')
+
+def think_next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_work_or_hand('early_fact_or_part')
