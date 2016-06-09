076b7563a1c1644122f337f9e5f9b127e86e6e13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:15:54 -0400

    "add: work super/hand/be_time_up_fact.py and super/able_number_or_company.py from high part"

---
+++ super/hand/be_time_up_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    great_fact(str_arg)
+    print('big_company_and_new_case')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('seem_year_above_great_place')
