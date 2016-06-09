658bfea332bfc8804a1b3f5618f97275ab3d5ec6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 10:26:46 -0400

    "mv: work super/own_government/work_big_day/right_time_and_week/point_or_life/do_man/last_hand.py, super/see_company/next_time_and_important_government/number_or_little_child/look_child/want_first_life/know_small_child.py and super/world/person.py from next week"

---
+++ super/own_government/work_big_day/right_time_and_week/point_or_life/do_man/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_large_man(str_arg):
+    eye(str_arg)
+    print('think_small_case_under_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_large_man('part')
