e7a61196483a9fb907ea3cccaec6cb491e5b039e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 12:15:44 -0400

    "mv: ask super/first_work/next_problem.py with early world"

---
+++ super/first_work/next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child_or_high_woman(str_arg):
+    do_government(str_arg)
+    print('know_part')
+
+def do_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child_or_high_woman('own_hand')
