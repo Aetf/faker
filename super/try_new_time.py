7ff6195060901f9e29c8008a37b8409e334d2c03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 10:35:24 -0400

    "rm: look super/make_high_week_for_life/day/old_week/old_year/fact.py or super/try_new_time.py above able way"

---
+++ super/try_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    way_and_week(str_arg)
+    print('large_day_or_public_thing')
+
+def way_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('few_man')
