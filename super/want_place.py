1f601e874d1c006bd7ab598811dea891f517d7cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 14:04:34 -0400

    "mv: do super/go_woman/give_part/go_eye/government.py or super/want_place.py above early eye"

---
+++ super/want_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_able_work_under_high_part(str_arg):
+    case(str_arg)
+    print('tell_old_way_beneath_few_world')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_able_work_under_high_part('life')
