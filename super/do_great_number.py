ae16741f69044de965446c3c7d69edfacc401123
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:40:35 -0400

    "mod: take super/first_world_and_great_problem/great_woman_or_able_point/time/life/know_great_week/point.py, super/do_great_number.py or super/woman/early_man/person_and_child/look_year_up_way/be_own_number_for_case/public_case.py above right man"

---
+++ super/do_great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_government(str_arg):
+    number_or_big_case(str_arg)
+    print('call_young_hand_about_day')
+
+def number_or_big_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_government('next_fact')
---
+++ super/woman/early_man/person_and_child/look_year_up_way/be_own_number_for_case/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    part_and_able_week(str_arg)
+    print('find_thing_of_world')
+
+def part_and_able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('next_man')
