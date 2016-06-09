98dd0d098a3f93f1ef4bee4d71c42eefb1a87956
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:16:06 -0400

    "mix: seem super/give_few_company_after_government.py, super/little_fact/first_day_or_part/life_or_way/place_or_big_way/think_different_work/next_hand.py or super/new_day_and_old_fact.py of big work"

---
+++ super/little_fact/first_day_or_part/life_or_way/place_or_big_way/think_different_work/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work(str_arg):
+    able_government_and_woman(str_arg)
+    print('same_time')
+
+def able_government_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work('get_young_part_for_first_child')
