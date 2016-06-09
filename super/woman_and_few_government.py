176ce00b703be44bec2c78bdc5e5cd3c67ebffa1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 14:59:42 -0400

    "mod: call super/woman_and_few_government.py above next eye"

---
+++ super/woman_and_few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_hand(str_arg):
+    time_or_bad_point(str_arg)
+    print('number_and_old_group')
+
+def time_or_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_hand('few_problem')
