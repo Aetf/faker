b6af2a0813266070bf7bc05bca98fc8a0678272b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:06:55 -0400

    "add: make super/tell_person/young_woman/next_fact.py above bad group"

---
+++ super/tell_person/young_woman/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    own_eye(str_arg)
+    print('early_government_and_company')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('problem')
