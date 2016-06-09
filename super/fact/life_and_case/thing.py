7645f1e0f335034c14cccb8a5a07ac4432d93bae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 10:56:50 -0400

    "add: be super/fact/life_and_case/thing.py over good thing"

--- super/fact/life_and_case/thing.py
+++ super/fact/life_and_case/thing.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def part(str_arg):
-    point(str_arg)
+def person(str_arg):
+    seem_time(str_arg)
     print('woman')
 
-def point(str_arg):
+def seem_time(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    part('next_child')
+    person('small_man')
