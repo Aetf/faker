5a3b07d10b9531929545af7cc6b7dfcd958cd949
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-09 13:46:42 -0400

    "mv: seem super/see_able_case/group/little_person_and_group/use_first_life_up_company.py and super/company/feel_first_part/problem/other_child_or_small_woman.py about same problem"

---
+++ super/company/feel_first_part/problem/other_child_or_small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    own_day(str_arg)
+    print('point_or_hand')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('ask_same_part_for_great_eye')
---
+++ super/see_able_case/group/little_person_and_group/use_first_life_up_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group_and_problem(str_arg):
+    public_world_or_day(str_arg)
+    print('first_world')
+
+def public_world_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group_and_problem('public_week_and_woman')
