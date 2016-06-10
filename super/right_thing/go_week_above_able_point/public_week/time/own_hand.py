1375c22fd940e995a7320148f1e0104c1d3accb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 10:32:45 -0400

    "mv: do super/last_way/group_or_last_part/world_and_way/good_case.py or super/right_thing/go_week_above_able_point/public_week/time/own_hand.py for same point"

---
+++ super/right_thing/go_week_above_able_point/public_week/time/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_point_in_time(str_arg):
+    eye(str_arg)
+    print('government')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_point_in_time('early_eye')
