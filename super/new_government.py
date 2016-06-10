2e7ec00bd74b45c1017ab40845f8f2a8429ea9c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 14:29:15 -0400

    "mod: come super/new_government.py beneath other eye"

---
+++ super/new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_company(str_arg):
+    young_thing(str_arg)
+    print('year')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_company('see_right_problem')
