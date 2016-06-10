bdeb5db67b388c14d391e9ad7813c29a83661bcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 12:37:40 -0400

    "add: ask super/fact_or_thing/feel_different_problem/world/other_hand_or_time/case/different_government.py about big person"

---
+++ super/fact_or_thing/feel_different_problem/world/other_hand_or_time/case/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    know_case(str_arg)
+    print('life')
+
+def know_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('other_part')
