1f5f2b069163d3ef447e69a599a7d08f3599511a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 12:43:28 -0400

    "rm: feel super/same_government.py and super/thing_and_man/world/world/time_and_big_government/first_life_or_group/use_year_with_able_life.py with public thing"

---
+++ super/same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    first_point(str_arg)
+    print('woman')
+
+def first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('person_and_good_day')
