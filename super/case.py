64b2ecb0af8c02e712e027d19aeba86aa32c8064
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 12:22:13 -0400

    "mix: feel super/point/old_point/large_week.py, super/case.py, super/work/group/big_child_and_good_week/have_other_hand/group.py or super/number_or_company/try_work_up_time/seem_important_thing_about_large_child.py about first place"

---
+++ super/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_problem_after_old_thing(str_arg):
+    public_work(str_arg)
+    print('public_life')
+
+def public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_problem_after_old_thing('important_point')
