77eb0b27dfa02c8291b47c9b96ce238985d9765e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 13:05:03 -0400

    "mv: try super/say_last_week_after_next_group/give_important_government_under_work/little_group_and_man.py and super/time/great_eye/think_last_week/call_good_person_about_time/early_woman/point.py up bad case"

---
+++ super/say_last_week_after_next_group/give_important_government_under_work/little_group_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child(str_arg):
+    eye(str_arg)
+    print('little_year')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child('week_or_life')
---
+++ super/time/great_eye/think_last_week/call_good_person_about_time/early_woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_government_under_good_child(str_arg):
+    great_company(str_arg)
+    print('work_big_part_from_world')
+
+def great_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_government_under_good_child('number_and_high_number')
