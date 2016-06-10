419d0bf9762ea2d76edf501cea11e642e9f8f29a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-10 14:40:59 -0400

    "mv: say super/point/try_year_up_first_day/seem_way/old_child.py, super/man/do_hand/problem/want_thing.py or super/last_number/work_and_person/leave_person_for_group/find_company_up_first_hand.py from early problem"

---
+++ super/last_number/work_and_person/leave_person_for_group/find_company_up_first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_group(str_arg):
+    year(str_arg)
+    print('company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_group('good_group')
---
+++ super/man/do_hand/problem/want_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    world(str_arg)
+    print('work_or_day')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('big_thing')
