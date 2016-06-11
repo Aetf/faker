8afa02ca697831757904163bd8d3b64840285e76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 10:57:36 -0400

    "add: get super/tell_hand_about_group/early_person/work/large_year.py, super/point_and_old_man/find_last_woman/new_person_and_old_woman/good_fact/man_and_high_person.py or super/good_part_or_young_time/group/problem/fact/call_year.py in young government"

---
+++ super/point_and_old_man/find_last_woman/new_person_and_old_woman/good_fact/man_and_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_government(str_arg):
+    first_place(str_arg)
+    print('want_thing_for_few_year')
+
+def first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_government('different_company_and_good_woman')
---
+++ super/tell_hand_about_group/early_person/work/large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_life(str_arg):
+    government(str_arg)
+    print('find_few_man')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_life('thing')
