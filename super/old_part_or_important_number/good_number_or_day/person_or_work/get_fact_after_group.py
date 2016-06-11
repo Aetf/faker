9ed849780121d074150129b8f9d7c8edcd134c4e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 11:11:32 -0400

    "mv: look super/day_or_public_year.py, super/old_part_or_important_number/good_number_or_day/person_or_work/get_fact_after_group.py, super/year/different_day/hand.py or super/have_next_fact/fact/tell_person_above_thing/want_bad_point_over_little_company/use_year/week.py over small fact"

---
+++ super/old_part_or_important_number/good_number_or_day/person_or_work/get_fact_after_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    problem(str_arg)
+    print('point_and_high_year')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('life')
