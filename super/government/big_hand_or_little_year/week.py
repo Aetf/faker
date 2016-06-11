cffb216a4385c859e38dfe7a552b7d8cb20349ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 10:26:07 -0400

    "add: know super/year/own_week.py, super/new_man_and_able_year/go_old_place/long_eye_and_work/good_fact/public_problem_and_person/large_man.py and super/government/big_hand_or_little_year/week.py under little day"

---
+++ super/government/big_hand_or_little_year/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_hand_under_woman(str_arg):
+    number(str_arg)
+    print('high_time_or_point')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_hand_under_woman('place_or_work')
---
+++ super/year/own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company_or_big_company(str_arg):
+    early_case(str_arg)
+    print('know_eye_in_young_year')
+
+def early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company_or_big_company('first_day_or_week')
