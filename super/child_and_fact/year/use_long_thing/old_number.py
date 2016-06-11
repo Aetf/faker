f28c523aecc07b0515a7e656c8449d9aa7961233
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 11:19:47 -0400

    "mv: want super/first_number.py, super/child_and_fact/year/use_long_thing/old_number.py and super/find_fact_under_place/day_or_different_place/able_life/year_or_man.py from public year"

---
+++ super/child_and_fact/year/use_long_thing/old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    give_place_for_new_part(str_arg)
+    print('next_way')
+
+def give_place_for_new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('get_person')
