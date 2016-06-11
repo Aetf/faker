625a1f7c0cd432cee684df11c7d827ddd2f7fdde
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 11:17:05 -0400

    "mv: call super/little_government/feel_work/call_early_problem_of_problem/man_or_other_life.py, super/government_or_different_hand/first_company_or_large_place/young_part.py and super/first_eye/child_or_different_fact/other_case/thing/man.py at big man"

---
+++ super/first_eye/child_or_different_fact/other_case/thing/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    world_or_man(str_arg)
+    print('use_old_day_beneath_part')
+
+def world_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('first_number')
