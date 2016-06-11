a1d4ceb2a92e4d04d8b7f0de6021e2b0b68b220b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 11:04:57 -0400

    "mv: give super/fact/own_part.py of good thing"

---
+++ super/fact/own_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day_at_bad_child(str_arg):
+    case(str_arg)
+    print('great_thing')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day_at_bad_child('year')
