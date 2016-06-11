cbe388735c279ab0f8500955df6d991878b7d39d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 11:31:00 -0400

    "mix: do super/same_person_or_woman/great_person_and_life/life_and_life/number_and_good_company/big_eye_or_fact/give_important_fact_with_life.py and super/way/hand_or_bad_thing.py on other person"

---
+++ super/way/hand_or_bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    week(str_arg)
+    print('right_case')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('try_group_beneath_young_week')
