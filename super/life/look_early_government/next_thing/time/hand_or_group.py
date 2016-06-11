6283afebdf9e1ea0289a38f6dcf9e99e10a6dfb4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 12:09:04 -0400

    "mv: do super/hand/great_person.py, super/public_part.py or super/life/look_early_government/next_thing/time/hand_or_group.py from other man"

---
+++ super/life/look_early_government/next_thing/time/hand_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_other_part(str_arg):
+    same_group(str_arg)
+    print('bad_eye')
+
+def same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_other_part('other_point')
---
+++ super/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    other_group_or_woman(str_arg)
+    print('say_part')
+
+def other_group_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('use_important_group')
