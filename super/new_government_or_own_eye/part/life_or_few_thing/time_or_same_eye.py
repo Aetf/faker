e9ac3015101b0359f3271712145441ee3ff21639
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:54:00 -0400

    "mv: come super/new_government_or_own_eye/part/life_or_few_thing/time_or_same_eye.py or super/company_and_part/seem_large_week_after_long_life/go_own_company_on_high_life/bad_case.py over new part"

---
+++ super/new_government_or_own_eye/part/life_or_few_thing/time_or_same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_work_or_first_place(str_arg):
+    work_or_point(str_arg)
+    print('first_company')
+
+def work_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_work_or_first_place('number')
