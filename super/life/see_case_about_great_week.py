522504c53aa2669a62bdea82556eaf5e06a9bbcb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:48:56 -0400

    "mv: look super/day/man/know_point/large_time/long_government.py, super/tell_new_company_of_other_person/good_eye/know_next_part/do_week/part_and_early_child/eye.py and super/life/see_case_about_great_week.py into first company"

---
+++ super/life/see_case_about_great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_hand_at_life(str_arg):
+    big_woman(str_arg)
+    print('day')
+
+def big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_hand_at_life('few_week_and_own_time')
