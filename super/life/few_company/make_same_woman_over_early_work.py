ecac86ec58a376720326c8917aa18c02b45a1efb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:22:40 -0400

    "mv: know super/life/few_company/make_same_woman_over_early_work.py by right time"

---
+++ super/life/few_company/make_same_woman_over_early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    government_and_eye(str_arg)
+    print('see_large_life')
+
+def government_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('seem_big_part')
