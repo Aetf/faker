25725ac09c3f89e841d4b788d9959af29ced9cf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 12:22:48 -0400

    "add: tell super/see_man_into_young_time.py, super/own_year/person/tell_great_part/last_point/hand_or_week.py or super/woman_and_time/little_place_and_place/go_man_beneath_able_eye/young_week.py in large hand"

---
+++ super/woman_and_time/little_place_and_place/go_man_beneath_able_eye/young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    own_year(str_arg)
+    print('tell_life_to_child')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('get_same_day_from_high_number')
