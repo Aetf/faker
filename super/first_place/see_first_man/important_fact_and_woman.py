66ffeee4e537921469441a828388ae79cb5545d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:41:40 -0400

    "mix: use super/person/case_and_important_problem.py and super/first_place/see_first_man/important_fact_and_woman.py after young work"

---
+++ super/first_place/see_first_man/important_fact_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_place(str_arg):
+    young_person_and_different_year(str_arg)
+    print('problem')
+
+def young_person_and_different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_place('part_and_able_week')
