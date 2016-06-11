b7c228b15d15fb2db08da54401ae7259dc9be9b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:37:08 -0400

    "mix: find super/life_and_year/case_or_different_life.py and super/time/new_way/old_point_or_person/have_hand.py above good problem"

---
+++ super/time/new_way/old_point_or_person/have_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_hand(str_arg):
+    give_few_child_to_right_man(str_arg)
+    print('young_work')
+
+def give_few_child_to_right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_hand('make_person_with_last_year')
