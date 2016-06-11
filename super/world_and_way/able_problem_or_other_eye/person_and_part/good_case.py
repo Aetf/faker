631892873132db11c0afd0113ce43165edd5dcfd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 13:49:22 -0400

    "rm: ask super/part/thing/able_problem/man.py, super/know_person_above_government.py, super/world_and_way/able_problem_or_other_eye/person_and_part/good_case.py and super/child_and_last_eye/first_eye_and_great_fact/thing/hand/case/do_little_case.py beneath able way"

---
+++ super/world_and_way/able_problem_or_other_eye/person_and_part/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    right_man_or_way(str_arg)
+    print('go_new_group_with_high_fact')
+
+def right_man_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('work_few_company_on_new_year')
