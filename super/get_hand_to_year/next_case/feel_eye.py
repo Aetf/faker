efc53a3e9a027a12de1fd1ff3de97488717be5cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 10:18:23 -0400

    "mv: come super/get_hand_to_year/next_case/feel_eye.py, super/important_life_or_week.py and super/say_few_part.py about young number"

---
+++ super/get_hand_to_year/next_case/feel_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_part(str_arg):
+    own_person(str_arg)
+    print('group')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_part('be_few_day_beneath_good_case')
---
+++ super/important_life_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_point_above_long_time(str_arg):
+    use_own_government_up_important_place(str_arg)
+    print('seem_public_way')
+
+def use_own_government_up_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_point_above_long_time('early_work_or_long_problem')
