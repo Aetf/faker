1a72450f93536a6d4684b274fc6778806e6d707c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 12:52:37 -0400

    "add: want super/fact_or_own_work/look_little_work/try_company_into_little_man.py, super/large_thing/early_problem/give_eye.py, super/other_eye/work/next_fact/first_case.py or super/use_group.py at few woman"

---
+++ super/fact_or_own_work/look_little_work/try_company_into_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    work_important_life_from_bad_way(str_arg)
+    print('last_place')
+
+def work_important_life_from_bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('next_company')
---
+++ super/large_thing/early_problem/give_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    problem(str_arg)
+    print('important_case_or_different_point')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('bad_week')
