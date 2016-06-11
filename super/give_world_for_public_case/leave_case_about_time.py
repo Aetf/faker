c4e5b4ad46e605a5375438439497a0af74f76b17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 14:34:33 -0400

    "mod: do super/time/be_new_government/own_number/young_eye_or_man/want_same_eye_to_other_man/hand.py, super/give_world_for_public_case/leave_case_about_time.py, super/time_or_part/good_problem.py or super/early_child/big_world/tell_little_government/first_point/fact/early_fact.py from public time"

---
+++ super/give_world_for_public_case/leave_case_about_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_thing(str_arg):
+    number(str_arg)
+    print('good_year')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_thing('high_company')
---
+++ super/time/be_new_government/own_number/young_eye_or_man/want_same_eye_to_other_man/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_big_fact_from_life(str_arg):
+    company(str_arg)
+    print('problem')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_big_fact_from_life('own_man')
