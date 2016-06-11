65919d8b76051b0ac9afe3349b14426dd8269851
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-11 13:57:45 -0400

    "mix: give super/feel_place/time.py and super/say_number/government/go_right_hand_by_first_point.py about big group"

---
+++ super/feel_place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week(str_arg):
+    find_hand(str_arg)
+    print('number')
+
+def find_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week('make_point')
