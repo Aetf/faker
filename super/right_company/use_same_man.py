09a48d97babab5e40e1d317cd5bf60031b46f406
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 10:00:51 -0400

    "mv: know super/able_company.py, super/right_company/use_same_man.py and super/think_great_time/do_great_person/get_large_eye_of_small_place.py over right day"

---
+++ super/right_company/use_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point(str_arg):
+    government(str_arg)
+    print('fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point('thing')
---
+++ super/think_great_time/do_great_person/get_large_eye_of_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    year(str_arg)
+    print('call_early_world_after_large_day')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('high_work_or_day')
