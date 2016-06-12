1f58233eba69dc062c67be896d7d7ba66d22956b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:49:20 -0400

    "mix: find super/case.py and super/use_early_government/problem.py of other fact"

---
+++ super/use_early_government/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_week_beneath_long_place(str_arg):
+    part(str_arg)
+    print('first_point')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_week_beneath_long_place('few_week')
