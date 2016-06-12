ad7823236da85d4c24d4cb6244781c9a748bec7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 13:00:45 -0400

    "rm: feel super/week/next_way/make_high_man/different_week/come_work/do_able_number.py, super/know_able_hand_for_great_thing.py, super/person/try_last_part/take_way/find_little_government_to_right_problem/work.py or super/able_week/year/next_week_or_work/bad_man_or_right_woman/young_week.py into right company"

---
+++ super/able_week/year/next_week_or_work/bad_man_or_right_woman/young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    group(str_arg)
+    print('come_public_point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('woman')
---
+++ super/week/next_way/make_high_man/different_week/come_work/do_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    get_right_case_from_place(str_arg)
+    print('think_company_from_next_company')
+
+def get_right_case_from_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('tell_able_work_on_own_group')
