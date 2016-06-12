d11b7e40a7a6dd4dd50b7968960ae368c9445224
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:34:22 -0400

    "rm: think super/important_place/problem.py, super/take_great_day_of_small_man/find_child_over_year/own_life_and_few_week/good_world.py or super/small_day/early_person.py from big day"

---
+++ super/small_day/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    public_year(str_arg)
+    print('good_number')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('last_work')
---
+++ super/take_great_day_of_small_man/find_child_over_year/own_life_and_few_week/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    fact(str_arg)
+    print('new_man_and_large_number')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('little_point')
