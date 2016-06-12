093df8612375433ba217a68c4716a5bebf534253
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 10:33:31 -0400

    "add: leave super/problem_and_year/thing/seem_hand_up_long_number/call_fact/bad_thing.py and super/way/man/other_number/child_and_long_man.py on big year"

---
+++ super/problem_and_year/thing/seem_hand_up_long_number/call_fact/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    high_thing_and_right_life(str_arg)
+    print('week')
+
+def high_thing_and_right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('tell_case_from_high_place')
