bfc4bdca806c5207784ed81ff2c2824f2ac2db71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 13:08:24 -0400

    "rm: look super/be_next_man/young_child_and_problem/last_woman_and_different_time.py on high fact"

---
+++ super/be_next_man/young_child_and_problem/last_woman_and_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    work(str_arg)
+    print('young_part_or_woman')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('week_and_child')
