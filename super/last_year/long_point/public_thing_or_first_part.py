f541d1f2fdf9cfb0d8822847652cfefd33653d86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 14:34:14 -0400

    "add: come super/last_year/long_point/public_thing_or_first_part.py, super/place/eye/work/day.py, super/woman_and_group.py or super/same_life_and_good_company/time/first_eye/great_week.py after good world"

---
+++ super/last_year/long_point/public_thing_or_first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing(str_arg):
+    company(str_arg)
+    print('own_world')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing('year_or_new_eye')
---
+++ super/same_life_and_good_company/time/first_eye/great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_company(str_arg):
+    eye_or_last_government(str_arg)
+    print('young_problem_or_new_part')
+
+def eye_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_company('long_person_or_point')
