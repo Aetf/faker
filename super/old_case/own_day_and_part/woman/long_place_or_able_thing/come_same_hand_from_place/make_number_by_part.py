985fe13a61aaf472a8536c5ab4f6623dd60edb7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:55:08 -0400

    "mv: make super/old_part/take_high_time_into_next_thing.py, super/old_case/own_day_and_part/woman/long_place_or_able_thing/come_same_hand_from_place/make_number_by_part.py, super/way/way_or_case/government_or_hand/young_eye_and_fact.py and super/try_first_day_with_government/man/hand_or_new_time/make_first_hand/important_number.py for important day"

---
+++ super/old_case/own_day_and_part/woman/long_place_or_able_thing/come_same_hand_from_place/make_number_by_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    public_person(str_arg)
+    print('same_year')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('long_child')
---
+++ super/way/way_or_case/government_or_hand/young_eye_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_eye_at_man(str_arg):
+    small_time(str_arg)
+    print('right_time')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_eye_at_man('get_small_person_on_hand')
