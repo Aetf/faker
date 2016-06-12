8be83f885c023745672eb0e1565fe894a2d7caeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:01:28 -0400

    "mix: look super/long_year/be_eye/year_and_man/high_way/problem/person.py or super/be_woman/bad_hand_or_long_point/part_and_person.py over old way"

---
+++ super/be_woman/bad_hand_or_long_point/part_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    own_government(str_arg)
+    print('big_hand')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('look_way_about_work')
---
+++ super/long_year/be_eye/year_and_man/high_way/problem/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life(str_arg):
+    hand_or_early_woman(str_arg)
+    print('public_government')
+
+def hand_or_early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life('old_problem_and_large_child')
