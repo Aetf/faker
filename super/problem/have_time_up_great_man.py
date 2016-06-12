00f29de7a9c11575aeed759ae61be46ec34777fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:17:17 -0400

    "mix: ask super/problem/have_time_up_great_man.py or super/new_thing_or_hand/point/life_and_life/eye.py beneath new government"

---
+++ super/problem/have_time_up_great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_way_on_world(str_arg):
+    give_point_at_long_number(str_arg)
+    print('say_important_time_for_old_woman')
+
+def give_point_at_long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_way_on_world('call_company_beneath_old_person')
