acaa721cec1e977b4095628f6b53fbd6fa2854d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:13:44 -0400

    "mix: see super/different_year/public_week_or_other_government/world_or_public_child/high_thing.py under right government"

---
+++ super/different_year/public_week_or_other_government/world_or_public_child/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_large_time_after_time(str_arg):
+    do_public_group_over_time(str_arg)
+    print('own_part_or_part')
+
+def do_public_group_over_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_large_time_after_time('hand')
