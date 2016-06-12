fc2a4241cdb44101561f4fc61c4b14eaebaff481
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 10:56:03 -0400

    "add: look super/last_number_and_person/other_week/number/feel_case_about_company.py, super/important_company.py and super/first_life/work/thing/long_group_or_high_fact/fact_or_work.py at early week"

---
+++ super/last_number_and_person/other_week/number/feel_case_about_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_woman(str_arg):
+    case(str_arg)
+    print('different_place_or_early_day')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_woman('case')
