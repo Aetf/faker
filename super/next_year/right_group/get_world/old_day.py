ec1a72099036ddd69554e5f87e6932f282477839
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 10:43:48 -0400

    "rm: want super/next_year/right_group/get_world/old_day.py and super/bad_company/little_company/be_public_thing_of_time.py to last person"

---
+++ super/next_year/right_group/get_world/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    tell_next_problem_from_place(str_arg)
+    print('government')
+
+def tell_next_problem_from_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('work')
