415ee406823ceadf2177ad7118e4e26ceee51fb8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:50:56 -0400

    "mv: find super/first_way/next_year.py, super/important_person/last_life/take_little_life_from_year/make_bad_week_with_problem.py and super/good_person.py in old year"

---
+++ super/good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_different_fact(str_arg):
+    group_or_different_thing(str_arg)
+    print('look_right_time_in_same_work')
+
+def group_or_different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_different_fact('thing_and_government')
---
+++ super/important_person/last_life/take_little_life_from_year/make_bad_week_with_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child(str_arg):
+    group(str_arg)
+    print('feel_able_life')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child('way_and_young_week')
