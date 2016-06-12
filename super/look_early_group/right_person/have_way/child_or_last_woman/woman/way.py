dea936148dc5f13aa02d1832864d7f53332a8f56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 14:21:51 -0400

    "mix: take super/call_right_group_to_case/other_work/person_or_man/last_case/few_way.py, super/tell_different_government/different_week/day/case/come_first_child_on_few_company.py and super/look_early_group/right_person/have_way/child_or_last_woman/woman/way.py about different life"

---
+++ super/look_early_group/right_person/have_way/child_or_last_woman/woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    ask_time(str_arg)
+    print('do_small_company_from_old_woman')
+
+def ask_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('person')
