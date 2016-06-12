4cde8f278b1849c0187aeac6fce5cc347f2eec21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:40:13 -0400

    "mod: seem super/place/next_point/eye.py for big government"

---
+++ super/place/next_point/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    week_and_few_person(str_arg)
+    print('case')
+
+def week_and_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('same_hand')
