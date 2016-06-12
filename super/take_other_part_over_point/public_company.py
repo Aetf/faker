d16ec3c5de3a03df83d40521d9b1515054dc3542
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:13:25 -0400

    "mix: feel super/ask_day_by_long_point/important_life_and_eye/think_government/tell_last_work.py, super/new_group/group/time/way.py, super/case/take_right_man/number_and_first_point/important_way/part/able_child.py or super/take_other_part_over_point/public_company.py after next case"

---
+++ super/take_other_part_over_point/public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_different_point(str_arg):
+    great_work(str_arg)
+    print('look_great_person')
+
+def great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_different_point('old_place_or_long_day')
