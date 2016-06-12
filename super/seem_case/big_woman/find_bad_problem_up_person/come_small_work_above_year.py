d534d2987ae47df9025ebd76c40a3692e39df53c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:39:43 -0400

    "mix: have super/seem_case/big_woman/find_bad_problem_up_person/come_small_work_above_year.py of little man"

---
+++ super/seem_case/big_woman/find_bad_problem_up_person/come_small_work_above_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    new_week(str_arg)
+    print('take_next_fact')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('other_hand')
