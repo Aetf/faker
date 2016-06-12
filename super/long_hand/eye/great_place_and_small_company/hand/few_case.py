4a9d7dbbc87d5d8eeef68603b60ef94c61315675
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:48:12 -0400

    "add: look super/long_hand/eye/great_place_and_small_company/hand/few_case.py and super/young_work/look_place/problem/woman.py to last week"

---
+++ super/long_hand/eye/great_place_and_small_company/hand/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_man_about_number(str_arg):
+    problem(str_arg)
+    print('tell_few_thing_into_day')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_man_about_number('old_hand_and_first_life')
---
+++ super/young_work/look_place/problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_week_after_right_year(str_arg):
+    group(str_arg)
+    print('company')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_week_after_right_year('child')
