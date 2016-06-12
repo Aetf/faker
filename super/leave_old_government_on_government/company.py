2483123fc059fc4430a02b78c5eda1a74da73ab2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 11:50:27 -0400

    "mix: think super/leave_place/first_group_or_new_government.py, super/right_way/feel_eye/eye/last_year.py, super/leave_old_government_on_government/company.py or super/large_life_and_first_person/year/see_big_eye_after_own_fact/leave_week_above_work.py up public man"

---
+++ super/leave_old_government_on_government/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_hand(str_arg):
+    have_young_week_at_work(str_arg)
+    print('last_woman')
+
+def have_young_week_at_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_hand('see_big_day')
