d4b8235b645a90be1f868a19f91f53bd8dfac3e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-12 12:36:51 -0400

    "rm: get super/few_child_or_little_life/have_first_world.py or super/large_fact_or_big_person/large_eye/same_life_or_early_week/bad_way_or_small_eye/different_place.py under first work"

---
+++ super/large_fact_or_big_person/large_eye/same_life_or_early_week/bad_way_or_small_eye/different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_place_and_part(str_arg):
+    see_bad_year_on_work(str_arg)
+    print('government')
+
+def see_bad_year_on_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_place_and_part('part')
