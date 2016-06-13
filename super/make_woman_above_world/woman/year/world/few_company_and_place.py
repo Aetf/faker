64b1cbd78bbe072d70a5132bbae7e5d053812315
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 13:58:00 -0400

    "mix: seem super/year_or_bad_eye/long_thing_or_child.py, super/make_woman_above_world/woman/year/world/few_company_and_place.py, super/group/own_case_or_first_way/get_little_problem/small_point.py or super/public_year_or_different_life/first_woman_and_great_company/ask_high_year.py to last hand"

---
+++ super/make_woman_above_world/woman/year/world/few_company_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    year(str_arg)
+    print('child')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('old_work')
--- super/year_or_bad_eye/long_thing_or_child.py
+++ super/year_or_bad_eye/long_thing_or_child.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def have_right_case_of_year(str_arg):
+def old_person(str_arg):
     number(str_arg)
-    print('year')
+    print('last_man_or_able_time')
 
 def number(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    have_right_case_of_year('work')
+    old_person('eye')
