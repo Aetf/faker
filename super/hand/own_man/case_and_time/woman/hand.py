543ad895d67d5f42e4b2ee4e530953bfb83169a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 13:56:20 -0400

    "mix: leave super/hand/own_man/case_and_time/woman/hand.py and super/point/right_hand_or_long_company/next_part/case/use_early_life/work_and_different_case.py over little way"

---
+++ super/hand/own_man/case_and_time/woman/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_next_man(str_arg):
+    last_day_or_first_case(str_arg)
+    print('life_or_woman')
+
+def last_day_or_first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_next_man('own_person')
