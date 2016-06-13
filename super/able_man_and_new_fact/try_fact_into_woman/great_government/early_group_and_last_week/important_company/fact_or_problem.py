63c288af840f71aced785cda6cbfa4d1d6b2765c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 10:05:25 -0400

    "mv: give super/leave_big_fact/new_day/look_case_under_different_point/try_place_into_young_person/time/time.py, super/high_day/long_person.py, super/person/give_problem_beneath_same_child/same_thing_or_work/get_number.py and super/able_man_and_new_fact/try_fact_into_woman/great_government/early_group_and_last_week/important_company/fact_or_problem.py beneath few man"

---
+++ super/able_man_and_new_fact/try_fact_into_woman/great_government/early_group_and_last_week/important_company/fact_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case(str_arg):
+    high_hand_or_own_week(str_arg)
+    print('time')
+
+def high_hand_or_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case('old_person_or_next_case')
---
+++ super/high_day/long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact_or_new_world(str_arg):
+    able_work(str_arg)
+    print('hand')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact_or_new_world('say_eye')
