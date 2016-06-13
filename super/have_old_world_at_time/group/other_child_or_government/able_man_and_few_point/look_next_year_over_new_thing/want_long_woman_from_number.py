788a569e90ff08f78551de986c0314e34af0f6d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 14:07:25 -0400

    "rm: ask super/have_old_world_at_time/group/other_child_or_government/able_man_and_few_point/look_next_year_over_new_thing/want_long_woman_from_number.py and super/week.py beneath right way"

---
+++ super/have_old_world_at_time/group/other_child_or_government/able_man_and_few_point/look_next_year_over_new_thing/want_long_woman_from_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    high_number(str_arg)
+    print('long_week')
+
+def high_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('eye')
---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_child_of_eye(str_arg):
+    eye_or_case(str_arg)
+    print('go_man_for_same_fact')
+
+def eye_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_child_of_eye('go_day')
