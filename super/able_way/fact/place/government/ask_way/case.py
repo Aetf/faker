b91938284d89ab0f51c34473d1de88aab29377d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 12:44:43 -0400

    "mix: try super/able_way/fact/place/government/ask_way/case.py beneath important number"

---
+++ super/able_way/fact/place/government/ask_way/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_great_day(str_arg):
+    government(str_arg)
+    print('point_and_part')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_great_day('few_week_or_fact')
