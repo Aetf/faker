eda9698175e05c197b2b4bcf084a4b984a798ea0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 10:12:01 -0400

    "add: give super/point/company/year/other_government.py, super/give_point_from_large_problem/number/next_week/thing/way/feel_woman.py, super/work_different_person/high_year.py and super/important_government_or_problem/have_same_way_of_government/company_and_hand/company_or_man.py from bad company"

---
+++ super/give_point_from_large_problem/number/next_week/thing/way/feel_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_year_about_different_year(str_arg):
+    life_and_case(str_arg)
+    print('tell_work_into_early_way')
+
+def life_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_year_about_different_year('government')
---
+++ super/work_different_person/high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_problem(str_arg):
+    try_year_over_point(str_arg)
+    print('leave_few_hand_at_high_way')
+
+def try_year_over_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_problem('day')
