8ffb3e68d93189463d5d048e48129cbbf0c76117
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 12:32:32 -0400

    "rm: say super/new_day_or_company.py or super/early_fact/time_or_year/think_government_of_year/have_problem_on_last_world/other_problem/bad_person_or_important_hand.py for few person"

---
+++ super/early_fact/time_or_year/think_government_of_year/have_problem_on_last_world/other_problem/bad_person_or_important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_point_from_number(str_arg):
+    bad_day(str_arg)
+    print('little_child')
+
+def bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_point_from_number('say_different_government')
---
+++ super/new_day_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place(str_arg):
+    eye(str_arg)
+    print('have_right_case_on_young_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place('part_or_bad_life')
