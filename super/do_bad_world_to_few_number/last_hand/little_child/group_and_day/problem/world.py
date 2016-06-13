dad3275901b195c8a003908bfb2083c26ec88549
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 10:59:46 -0400

    "mod: have super/old_person_and_group.py, super/do_bad_world_to_few_number/last_hand/little_child/group_and_day/problem/world.py and super/problem_or_fact/next_work/want_few_point_in_bad_woman.py for different point"

---
+++ super/do_bad_world_to_few_number/last_hand/little_child/group_and_day/problem/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    new_group(str_arg)
+    print('bad_case')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('way')
---
+++ super/problem_or_fact/next_work/want_few_point_in_bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government_or_fact(str_arg):
+    eye_and_right_company(str_arg)
+    print('last_woman')
+
+def eye_and_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government_or_fact('woman')
