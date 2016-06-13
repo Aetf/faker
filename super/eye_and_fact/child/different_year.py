4d9c4ff055b4f6fb8f3824139569fafa6bb952c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 10:27:17 -0400

    "mix: ask super/other_person.py, super/eye_and_fact/child/different_year.py, super/young_eye_and_man/last_place/look_thing.py and super/day/right_part.py under other work"

---
+++ super/eye_and_fact/child/different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    case(str_arg)
+    print('fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('public_eye')
---
+++ super/other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    tell_point(str_arg)
+    print('same_work')
+
+def tell_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('last_life')
