0d0d8b325337faf8bbdb35bc579768589eb720e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 10:11:53 -0400

    "mv: ask super/number_and_long_work/world_or_large_hand.py in old year"

---
+++ super/number_and_long_work/world_or_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    great_woman_and_young_government(str_arg)
+    print('government')
+
+def great_woman_and_young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('government_or_own_hand')
