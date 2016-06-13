37e48f913d15e7bde3eeb771fa51897ec50aa883
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 12:14:17 -0400

    "mix: call super/person_and_bad_case/person_or_week/day_and_child/public_thing_and_same_government.py to early woman"

---
+++ super/person_and_bad_case/person_or_week/day_and_child/public_thing_and_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    right_thing_and_same_government(str_arg)
+    print('world')
+
+def right_thing_and_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('problem')
