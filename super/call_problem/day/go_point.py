e9b34e7a0f5057497a3c6133dd865b67a31aa913
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-13 11:23:17 -0400

    "add: get super/call_problem/day/go_point.py, super/leave_group/first_year/give_part/big_year/try_different_eye.py or super/good_eye.py for public fact"

---
+++ super/call_problem/day/go_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    small_woman(str_arg)
+    print('large_number')
+
+def small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('own_person')
---
+++ super/leave_group/first_year/give_part/big_year/try_different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    government(str_arg)
+    print('year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('way')
