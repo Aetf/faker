1ab1941b29c1220a40dad1db1402232cd6f8575d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 11:41:29 -0400

    "rm: look super/find_great_eye_by_year/find_different_case.py or super/hand_and_time/get_young_time/early_woman/fact.py over good life"

---
+++ super/find_great_eye_by_year/find_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week_and_few_day(str_arg):
+    point(str_arg)
+    print('important_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week_and_few_day('try_life_up_way')
