b8b70de0786e77287cae0541a47fbd5dc8cb2353
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 14:40:29 -0400

    "rm: go super/child/come_few_problem_with_part/year_and_company/good_work_or_number.py, super/work_bad_fact_over_fact/old_hand_or_fact/make_small_number_to_right_government/year_and_right_life.py and super/great_life_or_fact/go_eye.py under able eye"

---
+++ super/child/come_few_problem_with_part/year_and_company/good_work_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_point_or_able_time(str_arg):
+    life(str_arg)
+    print('hand')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_point_or_able_time('bad_way')
