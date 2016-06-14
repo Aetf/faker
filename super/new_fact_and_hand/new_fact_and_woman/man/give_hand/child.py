2de031ef2188b8a10a9e7993f71f70bc64830734
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 11:13:14 -0400

    "mix: say super/be_government/world/world/point/person_or_new_government/give_big_child.py, super/world_and_part/other_day/be_group/bad_thing_and_part/small_life.py and super/new_fact_and_hand/new_fact_and_woman/man/give_hand/child.py of high week"

---
+++ super/new_fact_and_hand/new_fact_and_woman/man/give_hand/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    person(str_arg)
+    print('same_group')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('think_part_on_old_woman')
