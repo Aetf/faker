3ac7e7cebec6350d5b7bb06143f81039df585aef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 10:26:31 -0400

    "mod: feel super/few_hand_and_government/week.py to different woman"

---
+++ super/few_hand_and_government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_right_case(str_arg):
+    large_group(str_arg)
+    print('take_group')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_right_case('world_and_able_government')
