78301bad082564e9717dbfd681355be3a4d4bf94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 11:51:51 -0400

    "mv: want super/say_group.py, super/right_week.py or super/way/few_government_and_large_week.py under old fact"

---
+++ super/right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    case(str_arg)
+    print('man_and_company')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('child_and_early_part')
---
+++ super/way/few_government_and_large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    other_work_and_day(str_arg)
+    print('see_group_to_old_place')
+
+def other_work_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('number')
