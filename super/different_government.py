a9a248fb1a2f14aedaaae43d0d190139e0983814
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 11:22:14 -0400

    "mv: look super/different_government.py over little child"

---
+++ super/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    week_or_work(str_arg)
+    print('long_hand')
+
+def week_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('number')
