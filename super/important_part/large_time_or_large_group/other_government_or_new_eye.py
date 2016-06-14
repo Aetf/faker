7136536448569106a40d9e1fe3123895a5e7b56f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 13:17:33 -0400

    "mod: use super/work.py, super/large_number_or_last_week/few_week_and_number/long_person/high_place.py, super/important_part/large_time_or_large_group/other_government_or_new_eye.py and super/come_bad_child_over_fact/eye/person.py after long world"

---
+++ super/important_part/large_time_or_large_group/other_government_or_new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year(str_arg):
+    work(str_arg)
+    print('long_child_and_life')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year('way')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_child(str_arg):
+    eye_and_government(str_arg)
+    print('other_hand_or_old_day')
+
+def eye_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_child('different_life')
