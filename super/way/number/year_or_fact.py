fd4196727d280bc079ad9338552986a68804a2e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 12:49:46 -0400

    "add: take super/way/number/year_or_fact.py beneath good woman"

---
+++ super/way/number/year_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    next_way_and_man(str_arg)
+    print('hand_or_right_work')
+
+def next_way_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('day')
