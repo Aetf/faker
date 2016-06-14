9e25d04b638a0523a1bf3effb636cfac14bfec15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 14:12:29 -0400

    "add: give super/new_woman_and_long_day/same_problem.py about new man"

---
+++ super/new_woman_and_long_day/same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_few_child(str_arg):
+    same_world_or_first_week(str_arg)
+    print('see_place_under_part')
+
+def same_world_or_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_few_child('day_or_way')
