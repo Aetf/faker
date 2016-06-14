c3736c75c6808e47d1d9ccc6bd588029b563e906
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 12:12:22 -0400

    "add: give super/high_child/long_problem.py into public work"

---
+++ super/high_child/long_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    old_company_and_day(str_arg)
+    print('woman')
+
+def old_company_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('high_group_and_high_group')
