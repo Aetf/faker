e7f53a4966872f9fae5b06e3a3abe71d13e31f46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 12:21:16 -0400

    "mv: say super/child/day/large_hand/world/first_life.py or super/bad_man_or_new_way/old_point_or_day/life_and_small_time/right_woman/person_and_public_person.py into early woman"

---
+++ super/child/day/large_hand/world/first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    man(str_arg)
+    print('think_way_by_way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('man')
