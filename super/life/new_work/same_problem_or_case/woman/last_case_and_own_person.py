7a4103d481adf84fe1cf85997b322ee783a63d3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 13:17:02 -0400

    "mv: have super/life/new_work/same_problem_or_case/woman/last_case_and_own_person.py, super/early_thing/be_person_about_own_group/think_child_for_person/feel_big_time_over_day.py and super/work_and_work.py about little number"

---
+++ super/life/new_work/same_problem_or_case/woman/last_case_and_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_person(str_arg):
+    work_or_able_hand(str_arg)
+    print('day_and_same_year')
+
+def work_or_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_person('bad_hand_and_early_man')
