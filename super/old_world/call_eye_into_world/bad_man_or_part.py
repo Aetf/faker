3780ae5f27adb7fbf3c85749bf53bde109cddd6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 14:13:02 -0400

    "mix: ask super/great_hand/same_man/way_and_good_fact/point.py, super/old_world/call_eye_into_world/bad_man_or_part.py and super/want_problem_to_large_place/same_company.py above little woman"

---
+++ super/old_world/call_eye_into_world/bad_man_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time_or_day(str_arg):
+    world(str_arg)
+    print('world')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time_or_day('ask_old_way')
