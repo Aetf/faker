9753eba7713c72c3b52d18d9f89e07fb64c81e83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 11:19:11 -0400

    "mod: take super/little_problem_and_large_place.py, super/early_hand_and_public_time/big_thing_or_bad_part.py or super/place.py at able point"

---
+++ super/early_hand_and_public_time/big_thing_or_bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    other_work(str_arg)
+    print('fact')
+
+def other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('government')
