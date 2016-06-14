a85653e4dd3b9d08f090522abcf380bca3ee861f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 14:08:05 -0400

    "mv: look super/little_place/life_and_few_fact/child/public_world.py, super/give_big_group/fact/small_hand/problem.py and super/seem_year/long_way/seem_world_about_public_place.py into able person"

---
+++ super/seem_year/long_way/seem_world_about_public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    come_year_with_own_fact(str_arg)
+    print('be_different_group_for_small_world')
+
+def come_year_with_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('hand')
