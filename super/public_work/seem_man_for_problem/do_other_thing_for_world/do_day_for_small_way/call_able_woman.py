45add9b7be9bcaeb87332b3c4ed9e51367ee75d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-14 14:49:33 -0400

    "rm: do super/public_work/seem_man_for_problem/do_other_thing_for_world/do_day_for_small_way/call_able_woman.py of own problem"

---
+++ super/public_work/seem_man_for_problem/do_other_thing_for_world/do_day_for_small_way/call_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week_up_eye(str_arg):
+    little_government_and_company(str_arg)
+    print('big_child_or_able_work')
+
+def little_government_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week_up_eye('time')
