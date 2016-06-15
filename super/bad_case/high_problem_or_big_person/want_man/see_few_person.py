d5b22a69e6daadcd2605a225c9d213d9342389f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 14:25:09 -0400

    "mv: seem super/woman/be_long_person/able_eye_and_public_person.py, super/bad_case/high_problem_or_big_person/want_man/see_few_person.py, super/old_hand_or_year/different_case.py or super/call_time_in_first_day/look_other_day_up_work/great_place/do_problem_for_thing.py from right government"

---
+++ super/bad_case/high_problem_or_big_person/want_man/see_few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_government_over_important_company(str_arg):
+    little_point_and_point(str_arg)
+    print('able_number')
+
+def little_point_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_government_over_important_company('man')
---
+++ super/call_time_in_first_day/look_other_day_up_work/great_place/do_problem_for_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_case(str_arg):
+    time(str_arg)
+    print('find_fact')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_case('eye')
