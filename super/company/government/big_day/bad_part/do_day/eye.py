930264caf33201ed5c1f2d1c3c2deb973749d749
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 14:11:50 -0400

    "add: leave super/company/government/big_day/bad_part/do_day/eye.py or super/world.py for long hand"

---
+++ super/company/government/big_day/bad_part/do_day/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_child(str_arg):
+    great_way_or_same_life(str_arg)
+    print('way_and_important_way')
+
+def great_way_or_same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_child('eye')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    life(str_arg)
+    print('eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('day_and_large_part')
