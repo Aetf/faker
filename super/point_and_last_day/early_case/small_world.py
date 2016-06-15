fe6ea07aff8fbddd798f4c03f3aae38f93a02bbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 13:31:18 -0400

    "add: feel super/point_and_last_day/early_case/small_world.py or super/try_long_year_over_week/group/way/fact/first_place_or_new_place/few_day_or_day.py at own time"

---
+++ super/point_and_last_day/early_case/small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    work_work(str_arg)
+    print('fact')
+
+def work_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('other_place_and_own_group')
