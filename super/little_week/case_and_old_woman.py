a80e44a602949ced7690d0e3f3fb45625d76dfea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 10:50:47 -0400

    "mix: seem super/little_week/case_and_old_woman.py at great number"

---
+++ super/little_week/case_and_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    thing_and_different_person(str_arg)
+    print('number')
+
+def thing_and_different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('week')
