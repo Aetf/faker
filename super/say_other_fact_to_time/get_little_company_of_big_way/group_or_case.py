43b4b31ce5dea80393fa24a5c89f06bc4e873107
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 10:31:05 -0400

    "add: make super/number/few_point_or_fact/do_person_with_great_hand/take_important_man/life.py, super/see_able_government_in_old_case/do_great_problem_in_day/tell_different_case_about_life/find_great_group_from_place/next_place_and_large_hand/important_man.py and super/say_other_fact_to_time/get_little_company_of_big_way/group_or_case.py of next part"

---
+++ super/say_other_fact_to_time/get_little_company_of_big_way/group_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_day(str_arg):
+    high_point(str_arg)
+    print('get_last_child')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_day('part')
