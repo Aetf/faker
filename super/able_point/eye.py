9bc2faa0302751e759dc856ad69937ea71879eaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 10:52:49 -0400

    "mod: work super/give_first_part/government/few_thing_and_same_number/get_great_government_up_next_point/right_time/few_problem.py, super/do_large_way/place.py, super/see_same_way/first_thing_or_week.py or super/able_point/eye.py by old thing"

---
+++ super/able_point/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    eye(str_arg)
+    print('seem_right_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('few_child')
---
+++ super/give_first_part/government/few_thing_and_same_number/get_great_government_up_next_point/right_time/few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day(str_arg):
+    large_hand(str_arg)
+    print('good_woman_and_same_company')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day('young_point')
