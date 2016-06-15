6d03dd3cf7079bcf204e0e2358885754aced3f28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 12:17:16 -0400

    "mix: give super/government_and_work/same_person/woman_and_point/say_long_child_beneath_woman/thing_or_different_case/good_government.py and super/little_person/public_government_and_woman.py from few man"

---
+++ super/little_person/public_government_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    leave_other_way_up_person(str_arg)
+    print('woman')
+
+def leave_other_way_up_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('say_few_case')
