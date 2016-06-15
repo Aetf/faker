930542f732c539c80c2beeba549709764819f469
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 11:49:26 -0400

    "mv: do super/try_able_woman_over_large_week/say_own_fact.py, super/good_way_or_big_company/point/problem_or_part/find_big_point/big_case/work_or_long_week.py, super/large_group/year/man/man/large_place.py and super/other_part/good_week_or_child.py by able work"

---
+++ super/good_way_or_big_company/point/problem_or_part/find_big_point/big_case/work_or_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    point(str_arg)
+    print('good_child_and_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('do_next_thing')
