8cf04e6e4ba59d3acef04b0304e7450df05aaa8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 10:59:21 -0400

    "mv: call super/child_and_last_week/early_man/small_part/eye.py and super/few_week_and_important_group/want_different_work_in_fact/long_week_and_thing/company_or_way/great_group_and_old_work/year.py of great group"

---
+++ super/few_week_and_important_group/want_different_work_in_fact/long_week_and_thing/company_or_way/great_group_and_old_work/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point_and_year(str_arg):
+    large_point(str_arg)
+    print('child_or_next_time')
+
+def large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point_and_year('work')
