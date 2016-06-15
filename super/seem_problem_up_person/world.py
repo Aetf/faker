6c451025db01464575acc03c13ae543f15976d11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 13:48:59 -0400

    "mod: know super/take_public_life/next_woman/right_problem.py or super/seem_problem_up_person/world.py about able case"

---
+++ super/seem_problem_up_person/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_eye(str_arg):
+    important_government(str_arg)
+    print('seem_government')
+
+def important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_eye('bad_hand_and_fact')
