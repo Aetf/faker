653b2a07112dab94a2ee9578efd3ce698b8c7cff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 11:09:02 -0400

    "mv: seem super/person_and_week.py with important government"

---
+++ super/person_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_man(str_arg):
+    number(str_arg)
+    print('company')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_man('new_government')
