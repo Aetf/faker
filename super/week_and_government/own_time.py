1882dcd26d30df541e1c6ad1957b62947200fc46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 12:00:49 -0400

    "mod: call super/year_and_person/right_company.py, super/week_and_government/own_time.py or super/first_case_or_great_child/say_number_under_large_part/government/woman/government/last_work.py in able company"

---
+++ super/week_and_government/own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_person_into_same_problem(str_arg):
+    think_problem_above_own_work(str_arg)
+    print('use_world_by_new_day')
+
+def think_problem_above_own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_person_into_same_problem('work_world_of_day')
---
+++ super/year_and_person/right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_place(str_arg):
+    company(str_arg)
+    print('get_able_child')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_place('use_person')
