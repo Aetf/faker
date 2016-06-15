2b3cf1b0dfed0648a85312bb50d3bc99e80d63be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 12:24:58 -0400

    "add: make super/tell_woman_after_big_time/bad_work/next_point/seem_different_week.py beneath few work"

---
+++ super/tell_woman_after_big_time/bad_work/next_point/seem_different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    company_or_government(str_arg)
+    print('person_and_life')
+
+def company_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('look_large_group')
