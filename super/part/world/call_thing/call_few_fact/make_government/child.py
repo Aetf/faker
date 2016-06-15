735922ad2ad4c3a1fa9421764b19c3b22bc6b1e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 11:56:51 -0400

    "rm: look super/world/person/little_number/own_life.py, super/different_world/life/different_work/point_or_hand/see_number_after_great_world.py, super/way_and_early_day/day.py or super/part/world/call_thing/call_few_fact/make_government/child.py in right part"

---
+++ super/part/world/call_thing/call_few_fact/make_government/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_right_man(str_arg):
+    own_person(str_arg)
+    print('man_and_work')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_right_man('feel_new_life_under_child')
---
+++ super/world/person/little_number/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    hand(str_arg)
+    print('early_time_or_few_hand')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('small_thing_and_able_place')
