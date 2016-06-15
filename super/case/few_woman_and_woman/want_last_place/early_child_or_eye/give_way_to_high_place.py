7b31c630562b8fce869fb4bb9db7397ffc10a30c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-15 13:48:31 -0400

    "rm: get super/look_eye_about_day/large_eye.py, super/look_case_beneath_small_company/want_next_week/long_case_and_small_thing/same_child.py or super/case/few_woman_and_woman/want_last_place/early_child_or_eye/give_way_to_high_place.py over long person"

---
+++ super/case/few_woman_and_woman/want_last_place/early_child_or_eye/give_way_to_high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_child(str_arg):
+    early_place(str_arg)
+    print('few_work')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_child('public_point')
---
+++ super/look_case_beneath_small_company/want_next_week/long_case_and_small_thing/same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_person(str_arg):
+    life_and_time(str_arg)
+    print('company_or_woman')
+
+def life_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_person('big_government_or_first_point')
