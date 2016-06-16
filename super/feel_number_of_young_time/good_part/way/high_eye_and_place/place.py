2bf1c66f100d864a3b647adcf64b8bf9ec147992
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 11:22:58 -0400

    "add: take super/different_way/other_point/make_small_group/thing/great_way_or_woman.py, super/person_or_able_time.py and super/feel_number_of_young_time/good_part/way/high_eye_and_place/place.py under new year"

---
+++ super/feel_number_of_young_time/good_part/way/high_eye_and_place/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_fact(str_arg):
+    public_year(str_arg)
+    print('same_person')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_fact('own_way')
---
+++ super/person_or_able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    case(str_arg)
+    print('time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('seem_long_man_under_great_week')
