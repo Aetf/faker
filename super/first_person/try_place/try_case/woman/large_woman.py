0a3de461cf8b927044b18eab808f9aff644b4a23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 12:51:40 -0400

    "mv: say super/first_person/try_place/try_case/woman/large_woman.py, super/new_hand.py and super/early_thing/same_child/way.py from young number"

---
+++ super/first_person/try_place/try_case/woman/large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_great_number_on_next_group(str_arg):
+    government(str_arg)
+    print('high_time')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_great_number_on_next_group('few_world')
---
+++ super/new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    high_part(str_arg)
+    print('see_problem')
+
+def high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('way')
