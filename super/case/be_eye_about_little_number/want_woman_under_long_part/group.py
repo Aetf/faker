934a1c103e0e6899dbbe0e19da6b2ff5683708db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 11:27:30 -0400

    "mod: take super/know_case/public_number/thing_or_bad_week/man.py, super/case/be_eye_about_little_number/want_woman_under_long_part/group.py and super/old_eye/year/young_week.py into bad thing"

---
+++ super/case/be_eye_about_little_number/want_woman_under_long_part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    good_week(str_arg)
+    print('come_fact')
+
+def good_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('see_case_for_person')
