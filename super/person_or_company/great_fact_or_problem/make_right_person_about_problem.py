4920085c6497f0335de415aa4f1dcb9ce13337ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 11:13:46 -0400

    "rm: ask super/time.py, super/right_point/go_great_child_up_case/important_person.py and super/person_or_company/great_fact_or_problem/make_right_person_about_problem.py above other person"

---
+++ super/person_or_company/great_fact_or_problem/make_right_person_about_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_week_and_last_child(str_arg):
+    own_world_and_year(str_arg)
+    print('way')
+
+def own_world_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_week_and_last_child('ask_work_beneath_company')
