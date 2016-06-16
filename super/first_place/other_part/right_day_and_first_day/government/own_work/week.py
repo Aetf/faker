c4c9a20a18f9a80c59ba6736ddc72e0798cd6d4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 14:44:57 -0400

    "mv: make super/first_place/other_part/right_day_and_first_day/government/own_work/week.py and super/give_young_thing/bad_thing.py of little hand"

--- super/first_place/other_part/right_day_and_first_day/government/own_work/week.py
+++ super/first_place/other_part/right_day_and_first_day/government/own_work/week.py
@@ -2,11 +2,11 @@
 #! /usr/bin/env python
 
 def number(str_arg):
-    life(str_arg)
-    print('hand_and_year')
+    man(str_arg)
+    print('different_work_and_work')
 
-def life(str_arg):
+def man(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    number('own_hand')
+    number('eye')
