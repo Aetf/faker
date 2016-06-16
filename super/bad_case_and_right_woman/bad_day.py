526ebb97057f313cc88fb6e03e18a861660819d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 13:52:37 -0400

    "mix: make super/bad_case_and_right_woman/bad_day.py, super/want_large_way_in_able_company/life/few_world/other_work.py or super/year_or_young_government/few_year_or_part/take_hand/try_able_woman_from_eye.py for public life"

---
+++ super/bad_case_and_right_woman/bad_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_few_hand_above_way(str_arg):
+    important_week(str_arg)
+    print('important_number_and_important_thing')
+
+def important_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_few_hand_above_way('work')
