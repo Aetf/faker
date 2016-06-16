386b5a637167d95af1cb0ae8ff7e5dfd0473a9f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 14:00:51 -0400

    "add: work super/thing/last_hand/important_place_or_high_number/point.py, super/look_time_from_able_company/do_first_point/say_first_person_of_bad_week/want_next_group_from_first_thing/tell_place.py or super/own_way/point_or_hand/other_child/life.py up old day"

---
+++ super/look_time_from_able_company/do_first_point/say_first_person_of_bad_week/want_next_group_from_first_thing/tell_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_place(str_arg):
+    life(str_arg)
+    print('way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_place('place')
---
+++ super/own_way/point_or_hand/other_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    big_child_or_same_life(str_arg)
+    print('early_problem')
+
+def big_child_or_same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('same_woman')
