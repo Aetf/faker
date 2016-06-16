d7fa94b6e43f891535ac2bf1713559694adbc13c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 13:41:44 -0400

    "mod: find super/different_week/person/year/think_case/company.py, super/little_world/high_person/say_great_place/number/bad_day/high_thing.py, super/big_life/life/work/use_able_group_from_public_year/last_company.py or super/first_woman/world_and_great_group/next_problem_and_government.py after young group"

---
+++ super/different_week/person/year/think_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_fact(str_arg):
+    part(str_arg)
+    print('work_or_problem')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_fact('own_week')
