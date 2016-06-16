3f0d04cd86ef4d6813f910c45c138caad6d2f069
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 10:23:57 -0400

    "rm: go super/day/last_number/year.py, super/small_way_and_eye/world/work_few_government_above_young_life/number.py and super/be_case/big_life.py under old point"

---
+++ super/be_case/big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    place_and_work(str_arg)
+    print('way')
+
+def place_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('life')
---
+++ super/day/last_number/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    look_life_at_way(str_arg)
+    print('get_number')
+
+def look_life_at_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('woman_or_point')
