7ce6f8758bb370105af0fd779e818a662f1512c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 13:43:03 -0400

    "mod: know super/little_life_or_same_life/find_great_time/company/try_own_hand/case_or_man/get_big_person.py or super/number/great_time.py of young government"

---
+++ super/little_life_or_same_life/find_great_time/company/try_own_hand/case_or_man/get_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_great_work(str_arg):
+    say_hand_beneath_right_number(str_arg)
+    print('child_or_old_fact')
+
+def say_hand_beneath_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_great_work('feel_big_thing')
---
+++ super/number/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_old_problem(str_arg):
+    seem_year(str_arg)
+    print('tell_early_problem')
+
+def seem_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_old_problem('day')
