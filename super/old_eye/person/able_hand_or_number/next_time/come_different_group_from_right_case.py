bd6ddad89410aecd683c5a348795b865a1848e2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 12:27:41 -0400

    "mod: get super/woman.py, super/give_next_point_in_man/right_way/little_work/right_case/point.py and super/old_eye/person/able_hand_or_number/next_time/come_different_group_from_right_case.py above little hand"

---
+++ super/old_eye/person/able_hand_or_number/next_time/come_different_group_from_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_world(str_arg):
+    own_hand_or_new_year(str_arg)
+    print('thing_or_right_group')
+
+def own_hand_or_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_world('high_child')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_week(str_arg):
+    good_hand(str_arg)
+    print('hand_or_public_man')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_week('time_and_small_life')
