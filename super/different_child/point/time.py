bbb3de185835553377274aef83eac1a180c8f972
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 12:40:42 -0400

    "rm: work super/group_or_woman/life/same_child.py, super/good_way/hand/point/woman/able_place_and_good_eye.py, super/place_or_company.py and super/different_child/point/time.py of good problem"

---
+++ super/different_child/point/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_eye(str_arg):
+    company(str_arg)
+    print('person')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_eye('last_year_and_problem')
---
+++ super/group_or_woman/life/same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_place_from_person(str_arg):
+    come_different_way(str_arg)
+    print('different_child')
+
+def come_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_place_from_person('new_way')
