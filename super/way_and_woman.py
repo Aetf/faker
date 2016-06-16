54cad674b602e7515a992753a517c131e1e293b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 10:19:00 -0400

    "rm: give super/way_and_woman.py to early time"

---
+++ super/way_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_part(str_arg):
+    do_long_part_over_big_thing(str_arg)
+    print('leave_old_point_of_long_time')
+
+def do_long_part_over_big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_part('do_first_life')
