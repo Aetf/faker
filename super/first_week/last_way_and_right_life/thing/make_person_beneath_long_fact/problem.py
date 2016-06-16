f31e99e650f7ce8d8bf4673e5cc952809537bbb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 14:24:40 -0400

    "mix: make super/problem/hand/right_year/seem_work.py, super/first_week/last_way_and_right_life/thing/make_person_beneath_long_fact/problem.py, super/high_hand/life/number_or_time/think_own_way_to_important_child.py or super/few_world_or_time/say_person.py with high problem"

---
+++ super/first_week/last_way_and_right_life/thing/make_person_beneath_long_fact/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_long_time_from_way(str_arg):
+    first_problem(str_arg)
+    print('day_and_man')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_long_time_from_way('try_great_work')
---
+++ super/high_hand/life/number_or_time/think_own_way_to_important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    problem_or_problem(str_arg)
+    print('person')
+
+def problem_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('world')
