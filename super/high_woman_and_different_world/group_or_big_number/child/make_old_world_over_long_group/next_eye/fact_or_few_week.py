a815eccb9d76796e259407195546f120535e5c54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 13:39:20 -0400

    "mv: leave super/point_or_first_hand/day/way/big_government_and_same_part/give_long_point_over_last_week.py, super/first_eye/man/hand.py, super/part/able_way_and_government/case/be_company_by_small_woman/important_life/same_way.py and super/high_woman_and_different_world/group_or_big_number/child/make_old_world_over_long_group/next_eye/fact_or_few_week.py in long life"

---
+++ super/high_woman_and_different_world/group_or_big_number/child/make_old_world_over_long_group/next_eye/fact_or_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_problem_about_few_eye(str_arg):
+    government(str_arg)
+    print('part')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_problem_about_few_eye('man')
---
+++ super/point_or_first_hand/day/way/big_government_and_same_part/give_long_point_over_last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_case(str_arg):
+    seem_great_week_in_old_group(str_arg)
+    print('different_year_and_big_fact')
+
+def seem_great_week_in_old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_case('government')
