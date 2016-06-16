2bb34de689a7a61519694aba5787809cc853a2ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 12:03:28 -0400

    "rm: call super/call_right_government/right_case_or_right_part/try_child_of_fact/way/case.py, super/high_hand/government_or_few_part/bad_year.py and super/large_eye/see_time_under_thing/problem_or_first_government/problem/have_large_day_about_place/high_man.py of small point"

---
+++ super/high_hand/government_or_few_part/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_or_point(str_arg)
+    print('other_government_and_work')
+
+def way_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('small_case')
