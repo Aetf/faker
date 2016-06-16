28195ab22550711a1cffa5465413d569b201f8b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-16 14:15:27 -0400

    "mv: say super/part_and_person/first_week/government/long_place.py up other case"

---
+++ super/part_and_person/first_week/government/long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_man_up_problem(str_arg):
+    day(str_arg)
+    print('fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_man_up_problem('thing_and_person')
