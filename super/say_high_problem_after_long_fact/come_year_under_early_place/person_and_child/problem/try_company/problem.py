57148a87a098b28ad7a1607423fbd6725419896a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 11:24:39 -0400

    "mix: say super/long_year_or_large_eye/group/way/look_group/other_company/make_week.py, super/long_world_or_small_child/public_fact/little_case/case_or_point/get_own_woman/case.py, super/say_high_problem_after_long_fact/come_year_under_early_place/person_and_child/problem/try_company/problem.py and super/large_week.py about small thing"

---
+++ super/say_high_problem_after_long_fact/come_year_under_early_place/person_and_child/problem/try_company/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    know_person(str_arg)
+    print('work')
+
+def know_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('long_eye')
