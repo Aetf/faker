0c5e5541c40dbb4e6040d0e062c65ea75220f017
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:25:38 -0400

    "mv: leave super/hand.py, super/last_group/next_work_and_day/bad_point/group/make_place_by_life.py or super/want_own_person_at_few_number/high_work/life/first_world/government.py in able company"

---
+++ super/last_group/next_work_and_day/bad_point/group/make_place_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    important_child_or_high_year(str_arg)
+    print('week')
+
+def important_child_or_high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('want_high_fact')
