b428bbc374850a3fa75ba9b4f341a71a850a11ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:42:50 -0400

    "mix: tell super/other_hand/young_number/little_child/work_and_eye/number_or_woman/work.py up old number"

---
+++ super/other_hand/young_number/little_child/work_and_eye/number_or_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_number(str_arg):
+    have_eye_about_last_woman(str_arg)
+    print('tell_right_point')
+
+def have_eye_about_last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_number('company')
