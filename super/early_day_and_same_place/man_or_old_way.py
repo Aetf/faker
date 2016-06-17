a5bbf6bd6f4ede266684bee388f5b2c1370e7e67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:38:22 -0400

    "add: give super/say_first_day_about_group/thing_or_important_point/point/company/way_or_early_time/young_place.py and super/early_day_and_same_place/man_or_old_way.py for able child"

---
+++ super/early_day_and_same_place/man_or_old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    woman_and_next_point(str_arg)
+    print('little_place_and_high_man')
+
+def woman_and_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('able_point_and_big_place')
---
+++ super/say_first_day_about_group/thing_or_important_point/point/company/way_or_early_time/young_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_way_from_old_eye(str_arg):
+    other_woman(str_arg)
+    print('great_woman')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_way_from_old_eye('say_big_way_at_week')
