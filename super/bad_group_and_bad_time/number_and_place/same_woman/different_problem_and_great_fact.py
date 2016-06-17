69b9f93603ee7a5c683936fb546899d692271a42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 11:04:22 -0400

    "add: find super/bad_group_and_bad_time/number_and_place/same_woman/different_problem_and_great_fact.py over high eye"

---
+++ super/bad_group_and_bad_time/number_and_place/same_woman/different_problem_and_great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_work(str_arg):
+    know_number_up_different_way(str_arg)
+    print('week')
+
+def know_number_up_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_work('say_government_with_place')
