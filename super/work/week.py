7f207a9778f1347d735850b31c3ef6259c609e50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:38:04 -0400

    "mix: give super/way_and_bad_problem/government.py, super/work/week.py, super/child/way.py and super/world_and_other_time/young_group.py from old woman"

---
+++ super/work/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_own_thing_at_little_time(str_arg):
+    bad_week_or_public_life(str_arg)
+    print('other_company')
+
+def bad_week_or_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_own_thing_at_little_time('man_and_able_eye')
