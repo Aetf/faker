910d841a2cada007f5784ecd830665345990fada
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:35:05 -0400

    "rm: have super/little_government/day/eye/number_or_hand/same_time_and_government/great_world.py or super/want_first_week/tell_large_eye_with_new_company/leave_high_place/try_public_group/young_eye_or_long_way/new_day.py in new life"

---
+++ super/want_first_week/tell_large_eye_with_new_company/leave_high_place/try_public_group/young_eye_or_long_way/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_problem_and_hand(str_arg):
+    group(str_arg)
+    print('feel_public_case')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_problem_and_hand('way')
