2d41d1e99797ae6d8c64306830549aee188c8be9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:16:45 -0400

    "add: leave super/own_part_and_great_child/leave_great_time_over_old_way/first_way/week_or_week/next_child/fact_or_bad_person.py or super/world_and_case/give_old_life_by_hand.py under high day"

---
+++ super/world_and_case/give_old_life_by_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    come_other_week(str_arg)
+    print('new_person_or_life')
+
+def come_other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('eye_and_large_case')
