2b231e608453ae0e70de65b4a477c3f61e0c88c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:09:58 -0400

    "mix: have super/hand/new_government/work/long_fact_and_part.py, super/next_group/new_life_or_year/thing/man/small_problem.py, super/do_part/eye/leave_man/find_great_case_for_same_day/last_government/case.py or super/other_case/own_child.py beneath different life"

---
+++ super/next_group/new_life_or_year/thing/man/small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man(str_arg):
+    group(str_arg)
+    print('life_or_big_woman')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man('life')
