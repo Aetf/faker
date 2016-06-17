4665915c8605d1dc856d31e471f623a47b6b8636
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:11:33 -0400

    "mod: leave super/part_or_important_way/give_high_woman/young_fact/few_work/number_or_bad_part/old_problem.py over right company"

---
+++ super/part_or_important_way/give_high_woman/young_fact/few_work/number_or_bad_part/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person(str_arg):
+    few_number(str_arg)
+    print('think_great_day_of_new_day')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person('long_man_or_way')
