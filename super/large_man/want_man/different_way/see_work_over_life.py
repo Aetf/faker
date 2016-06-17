84d30c380cad94b808c05c3fb0cdc3b6eb3bba62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:28:01 -0400

    "mv: give super/new_child/feel_young_thing/year_and_early_week/long_child/group.py, super/other_world/large_point_or_company/bad_part/same_case/big_eye.py, super/large_man/want_man/different_way/see_work_over_life.py and super/great_week/big_week_and_place/case/group_and_few_day/know_woman.py to old child"

---
+++ super/large_man/want_man/different_way/see_work_over_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life_or_fact(str_arg):
+    way_and_eye(str_arg)
+    print('small_day')
+
+def way_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life_or_fact('right_group_or_little_work')
---
+++ super/new_child/feel_young_thing/year_and_early_week/long_child/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_able_week(str_arg):
+    public_year_or_right_year(str_arg)
+    print('different_fact_and_able_week')
+
+def public_year_or_right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_able_week('large_company')
