04176b5035fb29f856a5942e413c3451153517ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 11:49:59 -0400

    "add: make super/look_early_world_over_great_man/fact/want_able_point/work/right_company_and_time.py, super/leave_way/way/call_new_company_at_bad_person/able_problem_or_public_hand/leave_life_at_government/want_eye_in_early_way.py, super/same_thing_or_time/tell_old_person_with_child/get_company/give_number_beneath_early_thing.py or super/point_and_high_case/know_number_over_long_number/give_company_beneath_new_work/early_time/early_man.py by bad eye"

---
+++ super/leave_way/way/call_new_company_at_bad_person/able_problem_or_public_hand/leave_life_at_government/want_eye_in_early_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_big_number_from_world(str_arg):
+    other_work(str_arg)
+    print('little_time_and_little_thing')
+
+def other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_big_number_from_world('company')
---
+++ super/look_early_world_over_great_man/fact/want_able_point/work/right_company_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_young_government_in_good_group(str_arg):
+    do_person(str_arg)
+    print('see_case')
+
+def do_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_young_government_in_good_group('new_thing')
