bbbecfdd9ae981d463ee230c545ee730ddc86fc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 14:45:12 -0400

    "mod: make super/group/fact_and_group/give_time/year_and_young_hand/right_number_or_fact.py to early thing"

---
+++ super/group/fact_and_group/give_time/year_and_young_hand/right_number_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_next_part_to_old_case(str_arg):
+    long_eye(str_arg)
+    print('work_and_next_woman')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_next_part_to_old_case('part_or_large_point')
