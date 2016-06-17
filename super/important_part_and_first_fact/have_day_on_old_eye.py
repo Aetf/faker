c07de41a77adda013ecbf7c9260772f8d3457d34
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 11:21:10 -0400

    "mix: give super/own_week.py or super/important_part_and_first_fact/have_day_on_old_eye.py of great case"

---
+++ super/important_part_and_first_fact/have_day_on_old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_last_woman(str_arg):
+    life(str_arg)
+    print('right_group_and_part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_last_woman('work')
