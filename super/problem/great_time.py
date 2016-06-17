67d85ef48ed051232c170621fe4d4e8ebe8739c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:47:46 -0400

    "mod: use super/problem/great_time.py under important problem"

---
+++ super/problem/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way_or_old_number(str_arg):
+    thing(str_arg)
+    print('ask_big_eye_from_small_fact')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way_or_old_number('company')
