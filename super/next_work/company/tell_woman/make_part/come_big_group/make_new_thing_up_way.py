8cf40c1a4083c83f7bd564075b53f8475f100358
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:57:42 -0400

    "rm: find super/number/big_way/work/problem.py, super/next_work/company/tell_woman/make_part/come_big_group/make_new_thing_up_way.py or super/time/great_government_or_government.py for small year"

---
+++ super/next_work/company/tell_woman/make_part/come_big_group/make_new_thing_up_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    use_life(str_arg)
+    print('day')
+
+def use_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('bad_thing')
---
+++ super/number/big_way/work/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_person(str_arg):
+    number(str_arg)
+    print('few_year')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_person('large_week_and_young_person')
