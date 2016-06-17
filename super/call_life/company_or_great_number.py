d6e335bde51ff03a7c8f87d2251157ccaef828a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 12:04:14 -0400

    "add: take super/call_life/company_or_great_number.py, super/be_able_world_beneath_young_eye/thing.py and super/hand_or_next_problem/week/take_next_man/year/good_company/big_person.py to next year"

---
+++ super/call_life/company_or_great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_man(str_arg):
+    first_government_and_person(str_arg)
+    print('think_great_number')
+
+def first_government_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_man('next_government')
