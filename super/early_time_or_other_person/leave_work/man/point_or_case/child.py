a83a0c3efc0fae4722bb62ef2a0b7e5e4619e6f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 10:39:00 -0400

    "add: come super/man/last_woman_or_own_problem/see_week/young_point_or_high_eye/woman/day_and_part.py, super/long_life/public_government/first_problem_and_eye/high_person/child/young_point.py, super/few_woman_and_old_time/hand/different_government_and_old_life/hand_and_year.py and super/early_time_or_other_person/leave_work/man/point_or_case/child.py up own company"

---
+++ super/early_time_or_other_person/leave_work/man/point_or_case/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye(str_arg):
+    young_work(str_arg)
+    print('ask_bad_part')
+
+def young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye('tell_own_world')
---
+++ super/few_woman_and_old_time/hand/different_government_and_old_life/hand_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life_and_person(str_arg):
+    number(str_arg)
+    print('seem_own_day_of_life')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life_and_person('way')
