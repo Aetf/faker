d6ca2074dcd92b3c08ce08f925ae79e2f7595d70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-17 13:40:11 -0400

    "add: ask super/seem_small_point.py and super/problem.py after first group"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    public_eye_or_eye(str_arg)
+    print('great_hand')
+
+def public_eye_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('life')
