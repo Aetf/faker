d611afadbc0a809472ef1c72099fc8c3fc4ae5c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:28:05 -0400

    "mv: seem super/high_group_or_same_week/able_world/company.py or super/large_time.py by long thing"

---
+++ super/high_group_or_same_week/able_world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    woman(str_arg)
+    print('take_next_government_of_public_time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('man_or_new_problem')
