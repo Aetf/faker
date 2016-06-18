3f563a577f2e7b15169121fcfb731390bcab4fe7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 10:21:48 -0400

    "rm: know super/company/ask_day_after_life.py at different place"

---
+++ super/company/ask_day_after_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case(str_arg):
+    able_government(str_arg)
+    print('government_and_hand')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case('new_company_or_first_group')
