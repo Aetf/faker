333f28820ccb1a71733991d15914333e57a2fc28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 14:00:15 -0400

    "mod: find super/old_day_and_small_eye/case/say_government/work_big_person/high_life_or_next_fact/number.py above little way"

---
+++ super/old_day_and_small_eye/case/say_government/work_big_person/high_life_or_next_fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    eye_and_own_man(str_arg)
+    print('work_way_about_big_child')
+
+def eye_and_own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('government')
