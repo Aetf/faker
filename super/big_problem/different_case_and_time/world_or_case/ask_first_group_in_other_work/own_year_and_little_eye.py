f6fdc82602f0a84d7e859600365bdc14ca0d0ea9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 12:24:24 -0400

    "mv: be super/big_problem/different_case_and_time/world_or_case/ask_first_group_in_other_work/own_year_and_little_eye.py into early year"

---
+++ super/big_problem/different_case_and_time/world_or_case/ask_first_group_in_other_work/own_year_and_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_problem(str_arg):
+    government(str_arg)
+    print('early_government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_problem('day')
