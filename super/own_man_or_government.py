4b0a9276966c5c031f62f0b7c424b72666de808d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 11:59:44 -0400

    "add: look super/own_man_or_government.py, super/early_man/important_place/year_and_own_government/small_problem_or_place/place_or_point.py or super/few_time/next_person/government.py after other part"

---
+++ super/own_man_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_company(str_arg):
+    young_time(str_arg)
+    print('go_point')
+
+def young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_company('give_little_part_with_man')
