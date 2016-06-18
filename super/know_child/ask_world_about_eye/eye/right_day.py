cd59d22cc366a979ae37a4c13fc8d2d3eed9664c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 14:05:25 -0400

    "rm: want super/know_child/ask_world_about_eye/eye/right_day.py, super/see_week/have_government_of_own_company/able_time_and_good_way/time/important_part_or_important_time.py or super/make_work/problem/world/know_place/go_life.py over old life"

---
+++ super/know_child/ask_world_about_eye/eye/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_number_or_world(str_arg):
+    call_new_life_in_woman(str_arg)
+    print('own_week_or_case')
+
+def call_new_life_in_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_number_or_world('world_or_next_government')
---
+++ super/see_week/have_government_of_own_company/able_time_and_good_way/time/important_part_or_important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    government(str_arg)
+    print('world')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('hand')
