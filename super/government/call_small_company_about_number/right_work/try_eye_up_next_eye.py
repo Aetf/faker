bb0439955462d6ea4f3001877cb008425f62a1f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 10:45:12 -0400

    "rm: feel super/government/call_small_company_about_number/right_work/try_eye_up_next_eye.py, super/early_world/high_work/right_year.py, super/eye_or_good_point/world.py or super/next_eye_or_other_group.py from own year"

---
+++ super/government/call_small_company_about_number/right_work/try_eye_up_next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government_or_bad_day(str_arg):
+    day_or_company(str_arg)
+    print('old_thing_or_old_life')
+
+def day_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government_or_bad_day('important_hand')
---
+++ super/next_eye_or_other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    young_place(str_arg)
+    print('make_large_point')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('get_first_world')
