5be30394bcba4f593524829fc568ef34e4887d0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 11:00:52 -0400

    "add: work super/work/case/bad_government.py, super/different_problem_or_day.py or super/thing/person/woman/tell_few_child/own_point_or_big_eye.py from bad case"

---
+++ super/thing/person/woman/tell_few_child/own_point_or_big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_child_with_long_person(str_arg):
+    try_next_week_under_great_problem(str_arg)
+    print('say_little_problem')
+
+def try_next_week_under_great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_child_with_long_person('place')
