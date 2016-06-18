47be18a394cfd37f0fc4564a4940c3c9a1fa21a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:44:07 -0400

    "mv: look super/case/part/next_case.py up different woman"

---
+++ super/case/part/next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_problem(str_arg):
+    first_thing(str_arg)
+    print('work')
+
+def first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_problem('able_government')
