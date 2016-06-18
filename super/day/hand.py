139671186b3c2b7826e548a6a70df983ad559d5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:54:10 -0400

    "add: do super/day/hand.py and super/give_small_woman.py into early time"

---
+++ super/day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_big_hand(str_arg):
+    young_woman(str_arg)
+    print('time')
+
+def young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_big_hand('last_hand')
