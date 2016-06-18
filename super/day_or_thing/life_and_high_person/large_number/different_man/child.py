f37300f941c24a7760bd2a8864b62ec56801bbdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 14:35:40 -0400

    "rm: do super/day_or_thing/life_and_high_person/large_number/different_man/child.py with same life"

---
+++ super/day_or_thing/life_and_high_person/large_number/different_man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    see_point_beneath_week(str_arg)
+    print('thing')
+
+def see_point_beneath_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('bad_point')
