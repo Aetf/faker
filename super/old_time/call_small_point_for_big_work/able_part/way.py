971a0d06aa633bd2f3cb23b65c31897cdd4ae966
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 11:03:23 -0400

    "rm: see super/old_time/call_small_point_for_big_work/able_part/way.py in right work"

---
+++ super/old_time/call_small_point_for_big_work/able_part/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    year(str_arg)
+    print('important_year_and_hand')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('company')
