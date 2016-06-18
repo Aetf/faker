ad259859e822cdbc4e02b5c54c8e431c0a570dfa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 10:59:32 -0400

    "rm: be super/new_year/make_long_fact/take_next_number.py, super/young_point/do_company/life_or_hand/first_point.py and super/child/public_place/life/see_own_point/young_child_or_man.py in large place"

---
+++ super/new_year/make_long_fact/take_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    thing(str_arg)
+    print('government')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('point')
