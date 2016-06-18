35974ef18d5b7ce84793753518ab216249942d7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:18:22 -0400

    "mix: tell super/last_government/year_and_day/place/few_time_or_work/man.py, super/great_group/group_and_great_work/do_large_place_beneath_child/take_small_point_by_case.py and super/life/life_or_group/company.py of first day"

---
+++ super/great_group/group_and_great_work/do_large_place_beneath_child/take_small_point_by_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_few_life_from_government(str_arg):
+    good_fact(str_arg)
+    print('ask_number_beneath_government')
+
+def good_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_few_life_from_government('world')
---
+++ super/life/life_or_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_year_in_big_way(str_arg):
+    bad_point_and_child(str_arg)
+    print('new_number')
+
+def bad_point_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_year_in_big_way('number')
