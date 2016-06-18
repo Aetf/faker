fbf199ef49c97d9ad3db5cf08a5ab5d3a1d28d13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 12:25:18 -0400

    "mod: ask super/want_man/person.py, super/feel_week_to_great_child.py, super/year.py and super/number_and_big_life.py above old thing"

---
+++ super/number_and_big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    think_person_at_different_woman(str_arg)
+    print('work')
+
+def think_person_at_different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('large_part')
