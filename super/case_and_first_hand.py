691d99052d7260c361afb4882ec600f9b5d0d329
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:58:34 -0400

    "add: think super/large_woman_or_important_week/place/child/time_or_good_part.py and super/case_and_first_hand.py into other group"

---
+++ super/case_and_first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    early_work(str_arg)
+    print('long_man')
+
+def early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('year')
