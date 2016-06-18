153b735e6f435b11a3f8d155e647e77785e1d503
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 12:58:46 -0400

    "mix: take super/high_woman_or_little_case/first_work.py, super/right_group/person/tell_long_year_of_own_group/find_day_up_different_world/day_or_man.py, super/number/different_thing.py and super/point/old_part_and_place.py with same world"

---
+++ super/high_woman_or_little_case/first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_group(str_arg):
+    ask_way_of_other_place(str_arg)
+    print('high_part')
+
+def ask_way_of_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_group('different_group')
