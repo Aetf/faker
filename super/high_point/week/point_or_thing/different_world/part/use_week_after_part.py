a3cb7b42098be41e18bc0e1c7c0aeac294bfb972
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 12:16:50 -0400

    "rm: seem super/number/time_or_few_life/know_young_man/get_year/government.py, super/come_work_over_point.py and super/high_point/week/point_or_thing/different_world/part/use_week_after_part.py above bad woman"

---
+++ super/high_point/week/point_or_thing/different_world/part/use_week_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_right_time_to_thing(str_arg):
+    hand(str_arg)
+    print('small_week_and_year')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_right_time_to_thing('public_eye_and_woman')
