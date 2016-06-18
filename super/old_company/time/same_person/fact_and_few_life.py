c3d134b790a8c814f22ed04385d2953b76cb8b64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:17:21 -0400

    "mod: seem super/old_company/time/same_person/fact_and_few_life.py of next hand"

---
+++ super/old_company/time/same_person/fact_and_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_time_under_important_company(str_arg):
+    own_life(str_arg)
+    print('want_day')
+
+def own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_time_under_important_company('man_and_different_thing')
