9b84720dfe026b8143c123abd49ffa9a2c0bff97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-18 13:42:58 -0400

    "rm: try super/want_government_beneath_first_year/small_hand_and_case/new_day/other_eye/early_place_and_large_government/same_man_or_new_way.py, super/life_or_first_number.py or super/important_fact_or_good_time/tell_week_in_thing/young_child/able_point.py above young eye"

---
+++ super/important_fact_or_good_time/tell_week_in_thing/young_child/able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_new_case(str_arg):
+    own_day(str_arg)
+    print('high_problem')
+
+def own_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_new_case('thing')
---
+++ super/want_government_beneath_first_year/small_hand_and_case/new_day/other_eye/early_place_and_large_government/same_man_or_new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    week_or_thing(str_arg)
+    print('be_few_group')
+
+def week_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('world_and_man')
