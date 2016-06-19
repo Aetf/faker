ce3961be2ce2df962ee2fb9bb8c6e48d5080bb1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 11:26:23 -0400

    "mod: leave super/week/work/take_number_in_person/man/next_work.py or super/hand/other_time.py to right group"

---
+++ super/hand/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work_and_first_work(str_arg):
+    bad_person_or_thing(str_arg)
+    print('try_right_eye_above_high_world')
+
+def bad_person_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work_and_first_work('case')
---
+++ super/week/work/take_number_in_person/man/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company_up_high_fact(str_arg):
+    number(str_arg)
+    print('big_problem')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company_up_high_fact('large_woman')
