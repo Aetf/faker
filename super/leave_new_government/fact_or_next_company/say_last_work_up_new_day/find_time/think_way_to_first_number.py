aa5cf1b6d7098e4fc1b359a99b19979b2ab70d5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 14:37:50 -0400

    "mod: take super/big_eye_or_right_point/see_early_work_for_first_problem/public_week/child/next_woman.py, super/want_hand/life/use_hand_from_week/right_life_and_problem.py, super/leave_new_government/fact_or_next_company/say_last_work_up_new_day/find_time/think_way_to_first_number.py or super/part/last_government/case.py on own child"

---
+++ super/leave_new_government/fact_or_next_company/say_last_work_up_new_day/find_time/think_way_to_first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    life(str_arg)
+    print('same_week_and_company')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('man_and_case')
