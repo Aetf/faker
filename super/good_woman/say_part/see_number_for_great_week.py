ff576de415d58c8a7bca8fc25da9d05a7a492946
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 12:06:09 -0400

    "mod: go super/good_woman/say_part/see_number_for_great_week.py above high problem"

---
+++ super/good_woman/say_part/see_number_for_great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_part_or_day(str_arg):
+    company_and_new_year(str_arg)
+    print('thing_or_company')
+
+def company_and_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_part_or_day('make_few_day')
