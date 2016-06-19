c926a75312459c3240c7a4c7852e0e089acff061
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 12:35:00 -0400

    "rm: look super/different_problem/hand/high_eye/same_eye/leave_point_beneath_big_man/go_child_up_right_problem.py, super/woman.py, super/feel_public_woman/right_way/public_person_or_work/work_big_life_beneath_year.py and super/good_way/great_world_or_high_case.py at different government"

---
+++ super/different_problem/hand/high_eye/same_eye/leave_point_beneath_big_man/go_child_up_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_hand(str_arg):
+    big_week_and_small_place(str_arg)
+    print('long_child_or_high_time')
+
+def big_week_and_small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_hand('tell_large_point_with_eye')
---
+++ super/good_way/great_world_or_high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    woman(str_arg)
+    print('look_thing')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('young_week')
