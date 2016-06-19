ab0c7a8696ccd7483464d92d79d0e6226f471ab6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 12:09:24 -0400

    "mod: give super/different_child/see_company.py and super/case/want_same_time/work_good_way/think_old_world/world_and_same_person.py under young government"

---
+++ super/different_child/see_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_long_person(str_arg):
+    other_case_and_good_child(str_arg)
+    print('work')
+
+def other_case_and_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_long_person('new_world_or_point')
