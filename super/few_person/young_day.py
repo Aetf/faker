93c2a57e1585fa9c173a38c3fa4339e84be5c926
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 11:42:47 -0400

    "add: do super/few_person/young_day.py, super/woman_or_woman/tell_work/woman_and_part/want_work_with_high_life/bad_fact/large_week_and_great_point.py, super/important_way/big_week_or_eye.py and super/old_week/other_eye_and_week/work_right_work.py of right fact"

---
+++ super/few_person/young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    woman_or_new_work(str_arg)
+    print('last_point')
+
+def woman_or_new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('hand')
---
+++ super/old_week/other_eye_and_week/work_right_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    way_or_great_group(str_arg)
+    print('man')
+
+def way_or_great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('person_or_work')
