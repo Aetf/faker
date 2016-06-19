de36eb2ce37c28db54d6f691316225f887bc28c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 10:30:53 -0400

    "rm: look super/person_or_new_year/important_thing_or_problem.py, super/think_next_year.py, super/bad_year/great_world_or_woman/bad_fact/leave_part/come_eye_in_first_woman/great_eye.py or super/man_or_large_day/new_number_and_case/find_government/important_life/big_company.py by big eye"

---
+++ super/think_next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point_and_place(str_arg):
+    take_time(str_arg)
+    print('large_company')
+
+def take_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point_and_place('work_thing_above_last_company')
