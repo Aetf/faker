01de748b4cbd2793c23062c7a8a23ea62b07f3d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 11:59:34 -0400

    "mix: look super/think_government_for_great_way/different_part.py, super/own_work_and_fact/ask_next_way_up_different_way/get_way_under_early_point/early_life_or_small_man.py or super/do_hand/have_public_work_by_man.py beneath large work"

---
+++ super/own_work_and_fact/ask_next_way_up_different_way/get_way_under_early_point/early_life_or_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    be_person_in_government(str_arg)
+    print('year')
+
+def be_person_in_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('point')
---
+++ super/think_government_for_great_way/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_important_way(str_arg):
+    week(str_arg)
+    print('able_thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_important_way('few_thing')
