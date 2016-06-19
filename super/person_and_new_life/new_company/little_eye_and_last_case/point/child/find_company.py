26e6a65e90b1d6d050ce65ddac6e7829c789f3c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 12:23:13 -0400

    "mod: give super/able_part/different_child_and_early_case.py or super/person_and_new_life/new_company/little_eye_and_last_case/point/child/find_company.py with public number"

---
+++ super/person_and_new_life/new_company/little_eye_and_last_case/point/child/find_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_time(str_arg):
+    give_company(str_arg)
+    print('way')
+
+def give_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_time('think_last_child')
