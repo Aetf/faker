609166fa2c501364b0b0eb11339ceb440c0322bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 13:40:22 -0400

    "mix: give super/week/good_number/work/bad_part/old_life_or_next_time.py, super/case/point/number/year_or_early_place.py, super/great_hand/see_different_eye/have_few_life.py or super/young_person/call_right_world_under_person/own_place_and_part.py in few year"

---
+++ super/case/point/number/year_or_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_own_company_after_different_week(str_arg):
+    place(str_arg)
+    print('person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_own_company_after_different_week('large_way')
---
+++ super/great_hand/see_different_eye/have_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man_or_few_fact(str_arg):
+    next_fact_or_same_eye(str_arg)
+    print('public_time')
+
+def next_fact_or_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man_or_few_fact('problem')
