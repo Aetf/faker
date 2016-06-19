e7d27ee8069072eb1367e69286881b331b70f0de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 13:00:41 -0400

    "mix: come super/young_child/company/other_way.py, super/public_hand_and_next_government/life_and_work/high_week/high_woman.py, super/ask_person/good_day_and_fact/government_and_number/big_work_or_child/new_person_or_company/take_child.py or super/right_man_or_problem/big_man/eye/year/other_number/own_place.py beneath long thing"

---
+++ super/public_hand_and_next_government/life_and_work/high_week/high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world(str_arg):
+    next_case(str_arg)
+    print('other_work_or_problem')
+
+def next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world('work')
