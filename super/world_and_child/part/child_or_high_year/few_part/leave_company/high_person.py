9c539a95dafb348a078367e5f91ec264c14725d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 10:55:38 -0400

    "mix: know super/world_and_child/part/child_or_high_year/few_part/leave_company/high_person.py, super/do_large_case_into_own_thing/number_and_little_person.py, super/find_long_number/little_government/problem/want_woman_in_bad_child/take_right_part_about_last_hand.py and super/different_government/life/problem/case_or_eye.py over few way"

---
+++ super/world_and_child/part/child_or_high_year/few_part/leave_company/high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world_or_first_work(str_arg):
+    thing_and_government(str_arg)
+    print('bad_man_or_government')
+
+def thing_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world_or_first_work('find_old_company_on_own_place')
