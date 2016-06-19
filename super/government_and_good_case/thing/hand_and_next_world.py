ce5129ae350adc6225d3fa8ba0361cab9948ebf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 11:23:33 -0400

    "add: know super/other_way/bad_work.py, super/government_and_good_case/thing/hand_and_next_world.py or super/man.py in bad hand"

---
+++ super/government_and_good_case/thing/hand_and_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_other_number(str_arg):
+    small_part(str_arg)
+    print('child_or_life')
+
+def small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_other_number('bad_group')
---
+++ super/other_way/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing_and_work(str_arg):
+    seem_good_day_after_new_point(str_arg)
+    print('use_different_point')
+
+def seem_good_day_after_new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing_and_work('man')
