86030f5fbb6d6d987f271fc75d21a94168df91f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-19 13:08:37 -0400

    "mv: tell super/call_new_government/hand/be_government.py, super/important_world/thing.py or super/woman_or_own_place.py for early child"

---
+++ super/important_world/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    small_place(str_arg)
+    print('seem_young_world_on_world')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('new_number_and_few_company')
