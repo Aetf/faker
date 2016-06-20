1817946cfc607b199e91ed6c3598c0eb77eab78c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 13:32:52 -0400

    "mod: have super/place/large_place/come_old_problem/group.py and super/woman/government/person_and_large_part.py from last company"

---
+++ super/woman/government/person_and_large_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye_or_first_man(str_arg):
+    time(str_arg)
+    print('small_part')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye_or_first_man('see_big_group_from_important_group')
