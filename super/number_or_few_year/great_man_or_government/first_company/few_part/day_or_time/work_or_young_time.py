3117967036a023dffbfead04401f73f0d8cd8c0f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 13:06:12 -0400

    "mod: know super/person/few_world/child/be_problem.py or super/number_or_few_year/great_man_or_government/first_company/few_part/day_or_time/work_or_young_time.py to same hand"

---
+++ super/number_or_few_year/great_man_or_government/first_company/few_part/day_or_time/work_or_young_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_fact_to_right_man(str_arg):
+    part_and_week(str_arg)
+    print('right_thing_or_public_person')
+
+def part_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_fact_to_right_man('eye_or_first_government')
---
+++ super/person/few_world/child/be_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_part(str_arg):
+    able_eye(str_arg)
+    print('next_year')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_part('tell_bad_point_with_great_day')
