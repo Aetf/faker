1cfb6d8975e2e79ffe58e470c1fe77a4deee6425
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 11:49:38 -0400

    "rm: be super/eye_or_right_year.py, super/next_year/want_person/seem_new_part/give_year/do_public_way.py or super/be_place/give_problem.py up other work"

---
+++ super/next_year/want_person/seem_new_part/give_year/do_public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_week(str_arg):
+    early_year_or_eye(str_arg)
+    print('high_way_and_problem')
+
+def early_year_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_week('great_time')
