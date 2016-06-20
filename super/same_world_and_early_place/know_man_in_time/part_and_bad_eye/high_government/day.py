a971fb2cb08b08a16744f718592a3f400e2f1f01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 11:18:59 -0400

    "mv: seem super/same_world_and_early_place/know_man_in_time/part_and_bad_eye/high_government/day.py or super/way/way_or_right_place/different_way/look_eye_into_man.py into next world"

---
+++ super/same_world_and_early_place/know_man_in_time/part_and_bad_eye/high_government/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    great_case_and_new_way(str_arg)
+    print('group_and_thing')
+
+def great_case_and_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('little_woman')
---
+++ super/way/way_or_right_place/different_way/look_eye_into_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    see_public_time(str_arg)
+    print('think_young_case')
+
+def see_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('case')
