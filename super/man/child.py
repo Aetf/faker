787bcc2958ed903763d937462b36362540bd4419
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 12:44:58 -0400

    "add: tell super/day/life_and_group/high_woman/tell_child_beneath_day/government_and_year/company_or_work.py, super/man/child.py or super/young_eye/bad_time/leave_point.py above public point"

---
+++ super/man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    same_fact(str_arg)
+    print('know_fact_beneath_important_part')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('way')
---
+++ super/young_eye/bad_time/leave_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_problem(str_arg):
+    bad_work(str_arg)
+    print('same_week')
+
+def bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_problem('little_time_and_eye')
