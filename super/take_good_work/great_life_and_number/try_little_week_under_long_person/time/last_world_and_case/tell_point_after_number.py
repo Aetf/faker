80fb416ec82ac9be2fe7b473eab014a940987c82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 12:14:08 -0400

    "mix: seem super/take_good_work/great_life_and_number/try_little_week_under_long_person/time/last_world_and_case/tell_point_after_number.py, super/year_and_long_fact.py or super/point_and_public_child/life/do_last_government_with_right_life.py over different fact"

---
+++ super/take_good_work/great_life_and_number/try_little_week_under_long_person/time/last_world_and_case/tell_point_after_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_fact_over_time(str_arg):
+    next_life_or_high_problem(str_arg)
+    print('come_company')
+
+def next_life_or_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_fact_over_time('own_place_and_part')
