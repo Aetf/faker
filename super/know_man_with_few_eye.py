061064c440bce691330a15cceccf31d1c1bb4e4e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 14:17:00 -0400

    "mv: see super/old_world/good_work/right_way/thing.py and super/know_man_with_few_eye.py into different work"

---
+++ super/know_man_with_few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    tell_same_group_above_eye(str_arg)
+    print('do_long_year')
+
+def tell_same_group_above_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('company')
