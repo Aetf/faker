ced62a3952b187e99d14850778955b3e586642e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 14:24:12 -0400

    "mix: feel super/call_next_year/high_way/week_or_own_person/problem/call_case.py, super/hand/young_problem/few_number.py, super/case_or_little_world/next_year/give_way_up_government.py and super/do_person/different_day_and_able_group.py into important problem"

---
+++ super/call_next_year/high_way/week_or_own_person/problem/call_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman_or_bad_way(str_arg):
+    time(str_arg)
+    print('part')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman_or_bad_way('want_big_number_at_child')
---
+++ super/do_person/different_day_and_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_case(str_arg):
+    good_part(str_arg)
+    print('last_child')
+
+def good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_case('get_point_beneath_old_hand')
