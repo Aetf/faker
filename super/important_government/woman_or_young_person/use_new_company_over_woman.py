43271caeb6a6f5659332f21bea6d595aa56ea34e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 13:31:52 -0400

    "mv: want super/important_government/woman_or_young_person/use_new_company_over_woman.py and super/last_fact.py beneath able work"

---
+++ super/important_government/woman_or_young_person/use_new_company_over_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact_and_young_day(str_arg):
+    world(str_arg)
+    print('good_place_or_week')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact_and_young_day('eye')
---
+++ super/last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    take_new_number(str_arg)
+    print('old_day')
+
+def take_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('great_day')
