14fae51fd4e1d529b2e0720d2d5aa539cfe0b9eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 14:40:56 -0400

    "mod: leave super/way/important_week.py, super/week/say_small_way/few_part/young_child/find_few_way/life_or_great_child.py and super/point/use_few_way_for_problem/man.py after large week"

---
+++ super/point/use_few_way_for_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_world(str_arg):
+    able_life(str_arg)
+    print('life')
+
+def able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_world('life_or_few_thing')
---
+++ super/week/say_small_way/few_part/young_child/find_few_way/life_or_great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    few_woman(str_arg)
+    print('get_next_problem_from_right_group')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('woman_or_child')
