8e6799b376daa28eca380b3c9eeff050491fbd0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 10:33:53 -0400

    "add: use super/other_work/different_company/little_work/great_part.py, super/ask_same_time_from_thing/life_or_able_part/young_thing/use_problem_beneath_way.py and super/hand/life/company_and_day.py under next point"

---
+++ super/ask_same_time_from_thing/life_or_able_part/young_thing/use_problem_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    bad_hand(str_arg)
+    print('feel_week_for_fact')
+
+def bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('way')
---
+++ super/hand/life/company_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_man(str_arg):
+    little_government_or_week(str_arg)
+    print('man_and_early_company')
+
+def little_government_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_man('great_number')
