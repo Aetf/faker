869cb7570441a0f656578024f4f35e652ab110c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 14:56:47 -0400

    "mv: use super/thing.py and super/take_person_for_great_child/take_last_life_with_young_fact/year_or_little_place/use_large_hand_after_fact/life_or_government.py by own hand"

---
+++ super/take_person_for_great_child/take_last_life_with_young_fact/year_or_little_place/use_large_hand_after_fact/life_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    case(str_arg)
+    print('bad_problem_and_young_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('small_time')
---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_child(str_arg):
+    case(str_arg)
+    print('day_or_early_life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_child('group')
