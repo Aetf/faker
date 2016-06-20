4f85b33f5bf9c272c73006d9ea6b8aed5ef0280d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 14:41:02 -0400

    "mod: use super/hand_and_company/new_life/same_point/come_life/own_point_and_life.py with right hand"

---
+++ super/hand_and_company/new_life/same_point/come_life/own_point_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    eye_or_world(str_arg)
+    print('look_person_of_different_way')
+
+def eye_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('next_part')
