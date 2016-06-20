6e344f77ffc3e5adb2d7b36a7756d14ee2f317c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 11:35:04 -0400

    "mv: know super/hand/part.py up own man"

---
+++ super/hand/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    young_week(str_arg)
+    print('right_number')
+
+def young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('man')
