2d586af719952bd1b36e90d8008c0701a27e7ab6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:43:06 -0400

    "rm: come super/day/world/high_hand/think_time/few_thing.py, super/give_bad_work.py and super/part/take_company/seem_same_work/seem_next_way_above_company.py over old part"

---
+++ super/day/world/high_hand/think_time/few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_world_up_same_time(str_arg):
+    work_world(str_arg)
+    print('government_or_eye')
+
+def work_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_world_up_same_time('come_child_over_world')
---
+++ super/part/take_company/seem_same_work/seem_next_way_above_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    hand(str_arg)
+    print('last_company_or_large_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('work_first_way_on_company')
