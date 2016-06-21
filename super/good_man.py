1cfe26892da3758c142ac15271e9f7830eb1b149
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:20:58 -0400

    "add: have super/good_man.py of right year"

---
+++ super/good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    hand(str_arg)
+    print('look_man')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('other_woman')
