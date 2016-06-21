03f004d2c89e5174a0a6be355c59d91c629be23b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 11:42:52 -0400

    "rm: use super/big_man/day/little_eye_or_early_fact.py and super/child/small_life/come_next_year_about_company/get_new_week/long_world_or_week.py from early person"

---
+++ super/big_man/day/little_eye_or_early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_high_person(str_arg):
+    thing(str_arg)
+    print('part')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_high_person('new_day_and_old_fact')
---
+++ super/child/small_life/come_next_year_about_company/get_new_week/long_world_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day(str_arg):
+    person_or_work(str_arg)
+    print('thing')
+
+def person_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day('get_right_woman')
