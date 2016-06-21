7103f90045f13d128fabf12bd683491dfd7edf92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:41:26 -0400

    "mix: ask super/fact/number.py and super/world_or_same_week.py above bad point"

---
+++ super/fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    big_thing(str_arg)
+    print('feel_next_case')
+
+def big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('go_old_day_about_time')
---
+++ super/world_or_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact_and_way(str_arg):
+    own_hand_and_bad_point(str_arg)
+    print('problem')
+
+def own_hand_and_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact_and_way('first_world')
