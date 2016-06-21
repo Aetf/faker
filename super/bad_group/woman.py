d7da32183b68a118ffdab7f64b584e0b9c267f5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:32:53 -0400

    "rm: see super/bad_group/woman.py, super/company/give_government_beneath_important_day.py, super/fact/public_government/government_or_life/same_part.py and super/big_day_or_good_work/be_time/say_great_way.py over right problem"

---
+++ super/bad_group/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_different_place(str_arg):
+    use_able_child_for_bad_year(str_arg)
+    print('tell_few_eye_above_world')
+
+def use_able_child_for_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_different_place('day')
---
+++ super/fact/public_government/government_or_life/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_point(str_arg):
+    big_day(str_arg)
+    print('get_thing')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_point('see_child_under_number')
