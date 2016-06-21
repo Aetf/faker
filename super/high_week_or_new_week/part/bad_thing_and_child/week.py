935b8044cf2f2366f55e88f55fb71c2ff0ae6103
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:37:34 -0400

    "add: work super/number_and_person/little_work/young_fact.py, super/child/look_good_week_with_bad_point.py and super/high_week_or_new_week/part/bad_thing_and_child/week.py at able place"

---
+++ super/high_week_or_new_week/part/bad_thing_and_child/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_public_week(str_arg):
+    life_or_large_place(str_arg)
+    print('way_and_year')
+
+def life_or_large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_public_week('feel_fact')
---
+++ super/number_and_person/little_work/young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_right_place(str_arg):
+    old_hand(str_arg)
+    print('next_life_and_week')
+
+def old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_right_place('say_large_place_on_fact')
