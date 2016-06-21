a456480eac5f58faee537ac9092c0a1362292fa8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 14:39:49 -0400

    "rm: know super/young_fact/last_place/other_fact_and_big_case/bad_place/say_place.py, super/government_or_government/be_right_point/next_point.py or super/early_time/thing/get_long_work/government/time_and_life/call_little_company_under_able_point.py beneath important woman"

---
+++ super/early_time/thing/get_long_work/government/time_and_life/call_little_company_under_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_time(str_arg):
+    own_person(str_arg)
+    print('be_fact')
+
+def own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_time('look_large_world')
---
+++ super/government_or_government/be_right_point/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    next_thing(str_arg)
+    print('life_and_time')
+
+def next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('do_right_problem')
