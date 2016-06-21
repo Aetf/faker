0c9e522a6f52a570b7535d3913fd1a3093119320
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:32:28 -0400

    "mv: have super/own_group/be_own_week_in_part/feel_group/go_fact/person/seem_own_number.py, super/other_part.py, super/be_man.py or super/important_week/find_work_from_thing/government/work_same_day/company/want_company.py for next time"

---
+++ super/be_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    woman_or_able_problem(str_arg)
+    print('see_good_time_at_man')
+
+def woman_or_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('problem')
---
+++ super/important_week/find_work_from_thing/government/work_same_day/company/want_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_year(str_arg):
+    early_life_and_bad_life(str_arg)
+    print('woman')
+
+def early_life_and_bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_year('feel_big_man')
