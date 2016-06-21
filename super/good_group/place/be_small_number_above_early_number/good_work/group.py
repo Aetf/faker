459deec54e136f797f382fdd8f108e4752be656c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:46:12 -0400

    "mix: take super/good_group/place/be_small_number_above_early_number/good_work/group.py into next woman"

---
+++ super/good_group/place/be_small_number_above_early_number/good_work/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    ask_world(str_arg)
+    print('day')
+
+def ask_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('group_and_man')
