ff60f18c113e39f13f012acb835e4fa0ae15939a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 13:50:13 -0400

    "add: see super/try_little_group_under_public_day/week_or_right_government/year_and_other_work.py, super/problem/government.py and super/important_year.py for important man"

---
+++ super/important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_long_life(str_arg):
+    right_person(str_arg)
+    print('call_great_case_at_important_number')
+
+def right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_long_life('few_eye_or_small_week')
---
+++ super/try_little_group_under_public_day/week_or_right_government/year_and_other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_group_with_little_group(str_arg):
+    part(str_arg)
+    print('small_person')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_group_with_little_group('thing')
