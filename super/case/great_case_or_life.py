bbcdd7fc6025093c997b143d35294f91fbdf97b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:09:19 -0400

    "mod: give super/case/great_case_or_life.py at public place"

---
+++ super/case/great_case_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    own_problem(str_arg)
+    print('place')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('public_world')
