1de973df6177c73cd9a8d6c898762583983e25d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 13:57:30 -0400

    "mv: make super/work/know_point_for_government/few_point/long_life.py, super/world_or_new_case/go_company/own_number.py or super/see_problem_into_big_man/important_week/hand/time_or_group/person_and_place/government.py from large group"

---
+++ super/see_problem_into_big_man/important_week/hand/time_or_group/person_and_place/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_day_and_child(str_arg):
+    ask_old_way_about_hand(str_arg)
+    print('man')
+
+def ask_old_way_about_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_day_and_child('number')
---
+++ super/work/know_point_for_government/few_point/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    great_hand_and_part(str_arg)
+    print('point')
+
+def great_hand_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('point_and_good_fact')
