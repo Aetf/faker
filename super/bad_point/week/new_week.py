5c9a983d9fe4a4d8ee7a8416e0fc05a704b7c4fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 14:57:12 -0400

    "mv: try super/bad_point/week/new_week.py, super/use_company_up_man.py, super/thing_or_world/last_week/get_old_fact_by_time/new_life.py and super/man/life/year_or_place/great_man_or_little_life.py above early government"

---
+++ super/bad_point/week/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week_or_great_week(str_arg):
+    fact(str_arg)
+    print('old_world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week_or_great_week('right_week')
---
+++ super/man/life/year_or_place/great_man_or_little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    point(str_arg)
+    print('give_big_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('use_woman')
