e3a0e83800af1c21b6c72fac567821365b9f2194
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 11:54:16 -0400

    "mix: seem super/good_man/old_woman.py and super/year_and_able_group/right_world_and_bad_day/same_week_or_year/other_hand_and_different_government/new_time/same_hand.py in next child"

---
+++ super/good_man/old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    own_woman_and_different_point(str_arg)
+    print('call_first_man_on_bad_place')
+
+def own_woman_and_different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('early_week_and_good_hand')
