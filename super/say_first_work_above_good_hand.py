51c8e39c7f01097ce731d0e9293e6c39591ed061
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:08:28 -0400

    "rm: give super/world/different_company_or_week/large_week_or_last_place.py, super/new_thing_and_great_child/case/right_part/group/say_next_fact/child.py, super/same_woman.py and super/say_first_work_above_good_hand.py on important week"

---
+++ super/say_first_work_above_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    get_life(str_arg)
+    print('new_number')
+
+def get_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('woman')
