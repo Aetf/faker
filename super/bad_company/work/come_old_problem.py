05607cca36e3f8ee50115d88d57cbe7c9dae6ff7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:56:29 -0400

    "mod: find super/look_person/place/work_last_case_by_first_number/do_small_man_into_number/company/year.py and super/bad_company/work/come_old_problem.py under right hand"

---
+++ super/bad_company/work/come_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_day_or_good_group(str_arg):
+    find_problem(str_arg)
+    print('go_week_about_large_child')
+
+def find_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_day_or_good_group('year')
---
+++ super/look_person/place/work_last_case_by_first_number/do_small_man_into_number/company/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_day(str_arg):
+    make_public_problem(str_arg)
+    print('right_person')
+
+def make_public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_day('give_last_government_by_important_man')
