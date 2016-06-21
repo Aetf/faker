66e94673f9117f6fc9135e939cda685716466265
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:52:04 -0400

    "mod: look super/case_or_group/old_eye/big_eye/make_year_above_great_time/long_time/find_own_company_over_government.py under bad government"

---
+++ super/case_or_group/old_eye/big_eye/make_year_above_great_time/long_time/find_own_company_over_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    early_group(str_arg)
+    print('think_new_year_over_young_woman')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('hand')
