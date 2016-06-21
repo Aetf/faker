d492e501cc5fefb20bfc7c9214bbaff0fab69a92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 10:16:43 -0400

    "rm: work super/number/different_woman/man/large_company_or_high_person/woman_or_few_group/seem_young_week.py, super/world_and_way.py or super/right_case/early_work/thing/large_child/give_bad_case_up_place/think_large_time_by_company.py with able part"

---
+++ super/number/different_woman/man/large_company_or_high_person/woman_or_few_group/seem_young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_other_group(str_arg):
+    early_hand_and_woman(str_arg)
+    print('company_and_group')
+
+def early_hand_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_other_group('want_point_after_man')
