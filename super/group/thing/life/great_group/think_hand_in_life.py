0726c0d8dc5d5c76ad30e0ac1ddc23e9ee061929
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:24:27 -0400

    "add: do super/early_case/point/first_eye/eye.py, super/try_next_fact_after_problem/good_thing/old_company_or_work/want_man/see_great_week/year.py or super/group/thing/life/great_group/think_hand_in_life.py up big thing"

---
+++ super/group/thing/life/great_group/think_hand_in_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    see_time_up_next_company(str_arg)
+    print('make_year')
+
+def see_time_up_next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('time')
