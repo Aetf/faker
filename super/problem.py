1aa6fb1a566dccc1c269e8b69749d9e982dfc843
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-21 12:51:07 -0400

    "rm: take super/problem.py, super/other_case/call_company/young_week_or_world/case/different_child/week_and_point.py, super/group_and_place.py and super/fact/be_public_day.py to bad day"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_eye(str_arg):
+    able_place_and_man(str_arg)
+    print('group_or_little_time')
+
+def able_place_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_eye('company_and_other_eye')
