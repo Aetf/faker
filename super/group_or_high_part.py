9db72496b19ed5a30bcdf7cdf624e86d64d841b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 12:16:40 -0400

    "rm: take super/next_child/young_company/work/make_large_life/seem_few_number_over_point/life_and_group.py, super/company/own_person_and_time/group_and_last_world/high_life/do_life.py and super/group_or_high_part.py of big child"

---
+++ super/group_or_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_man(str_arg):
+    other_person_and_group(str_arg)
+    print('next_work_or_able_number')
+
+def other_person_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_man('company_or_group')
