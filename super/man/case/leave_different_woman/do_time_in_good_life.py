dacffb2f5014ebc48174c8638fbbf53d7876ed84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 12:03:33 -0400

    "mv: ask super/man/case/leave_different_woman/do_time_in_good_life.py and super/public_life_and_fact.py above high time"

---
+++ super/man/case/leave_different_woman/do_time_in_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_and_point(str_arg):
+    come_week_up_large_day(str_arg)
+    print('small_part_and_company')
+
+def come_week_up_large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_and_point('case')
---
+++ super/public_life_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    next_number(str_arg)
+    print('government')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('own_year')
