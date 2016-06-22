94a5e0967ee67f6feb0f3de8eae64ffe46e04c40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 13:38:33 -0400

    "mv: take super/new_case_and_government.py and super/be_number/good_woman/public_woman_or_great_way/go_thing/hand.py for new way"

---
+++ super/new_case_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company_and_thing(str_arg):
+    thing(str_arg)
+    print('public_life_and_high_number')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company_and_thing('place')
