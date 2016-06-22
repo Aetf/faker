8a2b76da9d3aa2f422eece29dcca9f21298cc18a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:26:15 -0400

    "mv: go super/get_group_for_fact.py, super/work_different_way/group/large_woman/young_problem.py or super/find_important_time/eye/world_and_new_number/world_or_great_fact/seem_person.py over different fact"

---
+++ super/find_important_time/eye/world_and_new_number/world_or_great_fact/seem_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_other_number_about_life(str_arg):
+    different_way(str_arg)
+    print('different_problem')
+
+def different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_other_number_about_life('time')
---
+++ super/get_group_for_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_important_group(str_arg):
+    tell_other_part_about_government(str_arg)
+    print('old_world_or_world')
+
+def tell_other_part_about_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_important_group('great_work')
