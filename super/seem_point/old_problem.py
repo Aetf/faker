60b3bade030d1139b84581d537230a6969acd920
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 11:02:01 -0400

    "add: do super/seem_point/old_problem.py from old week"

---
+++ super/seem_point/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    leave_other_life(str_arg)
+    print('see_part_into_company')
+
+def leave_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('day')
