3e60ef869934ff5032470a1d9652d4fe3656c421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:44:01 -0400

    "mix: get super/group_and_same_place/get_man/world/same_time/new_hand/hand.py, super/important_child.py, super/think_week.py and super/old_eye/case_or_eye.py at big government"

---
+++ super/important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_child(str_arg):
+    eye(str_arg)
+    print('have_other_world_up_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_child('world')
---
+++ super/think_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_work_under_new_time(str_arg):
+    government_or_number(str_arg)
+    print('eye')
+
+def government_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_work_under_new_time('place')
