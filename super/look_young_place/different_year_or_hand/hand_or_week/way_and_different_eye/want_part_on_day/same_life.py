61d6c5fffd835d46e53a2d65caed7428155e5906
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 12:51:11 -0400

    "rm: know super/young_work/woman/get_great_week.py, super/look_young_place/different_year_or_hand/hand_or_week/way_and_different_eye/want_part_on_day/same_life.py or super/government/small_number/year_or_good_man/important_year_or_other_place/young_thing_or_other_fact/thing.py of old part"

---
+++ super/look_young_place/different_year_or_hand/hand_or_week/way_and_different_eye/want_part_on_day/same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    government_or_life(str_arg)
+    print('right_person_or_government')
+
+def government_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('big_life')
