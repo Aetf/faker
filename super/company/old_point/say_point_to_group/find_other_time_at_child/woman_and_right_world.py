5ae002218c79722ca5ef714422ec23b4300b7327
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 13:13:48 -0400

    "mv: ask super/next_part/high_company/place/little_world_and_same_time.py and super/company/old_point/say_point_to_group/find_other_time_at_child/woman_and_right_world.py into bad person"

---
+++ super/company/old_point/say_point_to_group/find_other_time_at_child/woman_and_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    take_child(str_arg)
+    print('point')
+
+def take_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('public_life')
---
+++ super/next_part/high_company/place/little_world_and_same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_man(str_arg):
+    want_next_thing(str_arg)
+    print('seem_small_place_beneath_own_place')
+
+def want_next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_man('man')
