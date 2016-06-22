faae01f49de5bcd7474090d7c3e25e7615e1b679
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 14:01:06 -0400

    "mod: get super/use_small_thing_at_eye/own_person/government/use_early_person_in_man/few_week/go_life_in_government.py or super/say_few_week/point.py after good woman"

---
+++ super/say_few_week/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_small_hand(str_arg):
+    week(str_arg)
+    print('child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_small_hand('long_week_or_young_government')
---
+++ super/use_small_thing_at_eye/own_person/government/use_early_person_in_man/few_week/go_life_in_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place_and_year(str_arg):
+    eye_or_eye(str_arg)
+    print('be_other_person')
+
+def eye_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place_and_year('last_point')
