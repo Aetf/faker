a47b8d2099f61f1ea6bc5f67f0941ac78cf8299f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 12:48:03 -0400

    "add: seem super/call_important_world/thing_and_small_person/leave_old_day_on_bad_group/eye/think_woman_into_week/tell_part.py of new world"

---
+++ super/call_important_world/thing_and_small_person/leave_old_day_on_bad_group/eye/think_woman_into_week/tell_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_public_number(str_arg):
+    good_hand(str_arg)
+    print('woman')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_public_number('call_other_year')
