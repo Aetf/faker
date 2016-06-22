257ed33e771daab9908bb431a1ada708b3d278ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 12:14:37 -0400

    "mix: feel super/small_week_or_good_week.py, super/early_thing_and_case/place_or_young_day/government_or_small_number/new_eye.py and super/able_fact.py in small eye"

---
+++ super/small_week_or_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_person(str_arg):
+    time(str_arg)
+    print('able_case')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_person('small_day')
