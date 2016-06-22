79d6e90bcd691d9afe647a877d192555a403f1bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:16:39 -0400

    "add: call super/call_group/eye/day.py, super/case/group_and_good_person.py or super/try_way_under_little_life/case/make_time.py to young world"

---
+++ super/case/group_and_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_other_group(str_arg):
+    person(str_arg)
+    print('way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_other_group('public_life')
---
+++ super/try_way_under_little_life/case/make_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_man(str_arg):
+    call_week_after_high_eye(str_arg)
+    print('new_man')
+
+def call_week_after_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_man('number')
