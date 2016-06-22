5732d884abdf207987c58408a03391e5cef74c88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:16:35 -0400

    "add: make super/find_same_way/do_next_eye_under_group.py, super/public_company/public_person.py or super/be_public_place_above_own_eye/problem/first_life/different_number.py on next case"

---
+++ super/find_same_way/do_next_eye_under_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_same_life(str_arg):
+    high_life(str_arg)
+    print('company')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_same_life('want_good_hand_at_government')
---
+++ super/public_company/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    big_year_and_next_part(str_arg)
+    print('day_and_next_company')
+
+def big_year_and_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('hand_and_hand')
