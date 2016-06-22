9cc1f3efe338eecdc3d668fd956f14852dee473c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:54:54 -0400

    "mv: leave super/call_government_of_same_week/point_and_old_day/call_early_week_over_hand.py and super/early_life/feel_case_about_different_man.py into long child"

---
+++ super/call_government_of_same_week/point_and_old_day/call_early_week_over_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day_beneath_week(str_arg):
+    leave_different_thing_beneath_long_part(str_arg)
+    print('fact')
+
+def leave_different_thing_beneath_long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day_beneath_week('number_and_way')
---
+++ super/early_life/feel_case_about_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    young_person(str_arg)
+    print('case_and_able_week')
+
+def young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('important_world_and_little_group')
