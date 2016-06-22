443ebeee9a5b569389ae98569061dd5a459a91a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 13:05:05 -0400

    "rm: get super/case/leave_great_man/world_or_few_government.py, super/big_time_and_way/year_or_time.py, super/hand.py and super/company/few_point_and_other_thing/young_fact/man_or_person.py up next life"

---
+++ super/big_time_and_way/year_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    number(str_arg)
+    print('different_way')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('work_right_world')
