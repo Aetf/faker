fb0551195513462192f8b7b35f3035165f8fd068
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-22 10:24:40 -0400

    "mod: want super/public_number/get_next_work/work_government_by_own_week/few_child/ask_little_fact_over_old_day/hand.py at long number"

---
+++ super/public_number/get_next_work/work_government_by_own_week/few_child/ask_little_fact_over_old_day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_public_work(str_arg):
+    get_same_week_under_own_problem(str_arg)
+    print('bad_time')
+
+def get_same_week_under_own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_public_work('first_case')
