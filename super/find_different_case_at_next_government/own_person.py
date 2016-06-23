7fc8d2038eb6a5590aeb44171e03c1428f7d1c7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 14:46:58 -0400

    "rm: get super/find_different_case_at_next_government/own_person.py over good thing"

---
+++ super/find_different_case_at_next_government/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day_and_high_week(str_arg):
+    small_day(str_arg)
+    print('small_hand')
+
+def small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day_and_high_week('feel_right_fact')
