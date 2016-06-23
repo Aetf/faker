ee5d425b2a00920dc6289481103bbdf91b65cc22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 11:08:30 -0400

    "add: know super/few_child/company.py, super/life/time/ask_public_world_over_thing/world.py or super/woman/last_fact/look_early_year.py beneath little company"

---
+++ super/life/time/ask_public_world_over_thing/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_government(str_arg):
+    great_point(str_arg)
+    print('long_part')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_government('fact_and_long_hand')
---
+++ super/woman/last_fact/look_early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    other_time(str_arg)
+    print('great_child')
+
+def other_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('year')
