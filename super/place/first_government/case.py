cb7d2f8c3efca02c13261f9b9178916b11fc8faa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 13:28:26 -0400

    "add: call super/next_eye_or_part/eye.py, super/place/first_government/case.py or super/time/look_new_problem/able_company/hand_or_case.py above different man"

---
+++ super/place/first_government/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    point(str_arg)
+    print('see_long_group_of_new_time')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('high_company')
---
+++ super/time/look_new_problem/able_company/hand_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    world(str_arg)
+    print('own_work')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('able_group')
