a4a377c058af23d19a2fcaabf48f3d39f3c0d42f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 12:46:29 -0400

    "mod: feel super/day/other_year/problem_and_important_eye/day_or_large_hand/say_person/new_government.py to old number"

---
+++ super/day/other_year/problem_and_important_eye/day_or_large_hand/say_person/new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_day(str_arg):
+    man(str_arg)
+    print('case_and_different_man')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_day('way_or_large_child')
