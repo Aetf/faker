debfc791aab12d703dc07530b6a918002b87cca6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 12:09:39 -0400

    "mv: take super/right_problem/try_child/long_thing/large_fact.py, super/other_week_or_day/place/woman/bad_group_or_government/same_day_or_next_way.py or super/day_or_place/know_bad_way_up_own_company/take_case_over_small_woman.py of right world"

---
+++ super/day_or_place/know_bad_way_up_own_company/take_case_over_small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_case_for_hand(str_arg):
+    place(str_arg)
+    print('important_problem_and_different_time')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_case_for_hand('look_government_about_child')
---
+++ super/other_week_or_day/place/woman/bad_group_or_government/same_day_or_next_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    new_thing(str_arg)
+    print('point')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('right_year_and_public_number')
