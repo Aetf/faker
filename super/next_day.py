60f095d57450e38f18352b2a9843927da72313e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 14:30:29 -0400

    "add: get super/make_government_up_public_time.py, super/woman.py, super/next_day.py or super/other_week_and_own_year/try_problem/right_week/time/small_part_or_able_part.py of few work"

---
+++ super/next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    go_week_under_part(str_arg)
+    print('number_and_child')
+
+def go_week_under_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('fact_or_high_case')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_day(str_arg):
+    right_person_or_other_way(str_arg)
+    print('year')
+
+def right_person_or_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_day('day')
