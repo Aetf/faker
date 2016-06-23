f23baa8c84e3919fa12f49cf793259383937e03c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 13:12:52 -0400

    "add: try super/new_work/try_point/government_and_woman/find_bad_world/way_or_week/number_or_first_thing.py or super/man/do_case_about_able_part.py at last part"

---
+++ super/man/do_case_about_able_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing(str_arg):
+    tell_other_government(str_arg)
+    print('big_place_or_person')
+
+def tell_other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing('work_other_person')
---
+++ super/new_work/try_point/government_and_woman/find_bad_world/way_or_week/number_or_first_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    work_point(str_arg)
+    print('large_child')
+
+def work_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('group')
