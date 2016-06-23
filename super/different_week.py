56e1ab6702f5507a87da82d6b8ac09e09ca90e8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 13:23:41 -0400

    "rm: ask super/different_week.py and super/group/look_own_day_in_day/person/same_child/life/time_and_eye.py above big company"

---
+++ super/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person_and_last_eye(str_arg):
+    eye(str_arg)
+    print('right_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person_and_last_eye('government_or_fact')
---
+++ super/group/look_own_day_in_day/person/same_child/life/time_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    small_fact_and_point(str_arg)
+    print('man')
+
+def small_fact_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('work_little_man')
