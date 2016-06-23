632209b96cb2dbb4402931057410001ab6450463
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 12:01:28 -0400

    "add: think super/way/few_hand/case.py, super/government.py or super/fact_and_few_part.py to long group"

---
+++ super/fact_and_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    point_or_same_world(str_arg)
+    print('use_other_woman_for_case')
+
+def point_or_same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('public_hand')
---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_thing(str_arg):
+    government(str_arg)
+    print('fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_thing('way')
