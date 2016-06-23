c6d92d1bfad51741fdc4539fc1f87cda8a1b888e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 12:42:42 -0400

    "add: try super/great_life/world/get_place_over_important_woman/place/make_next_problem/eye_and_able_company.py or super/make_little_life/different_life/right_thing/group.py on old government"

---
+++ super/make_little_life/different_life/right_thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_same_way(str_arg):
+    place(str_arg)
+    print('be_early_eye')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_same_way('day')
