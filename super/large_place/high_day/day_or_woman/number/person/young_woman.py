99993ed228a8fd8a8c2197523d03b185a1e2c2a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 12:01:43 -0400

    "mod: seem super/day/take_way/last_life/eye/number.py, super/large_place/high_day/day_or_woman/number/person/young_woman.py and super/part/year/take_early_person_in_small_person/think_same_part_on_little_way.py under great person"

---
+++ super/large_place/high_day/day_or_woman/number/person/young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company(str_arg):
+    man(str_arg)
+    print('little_time')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company('place_and_life')
---
+++ super/part/year/take_early_person_in_small_person/think_same_part_on_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    high_way(str_arg)
+    print('thing')
+
+def high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('call_young_woman')
