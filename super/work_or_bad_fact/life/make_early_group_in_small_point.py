48bd5ff834e85e9a5c15964239b87d8703dda5fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 14:44:26 -0400

    "rm: feel super/work_or_bad_fact/life/make_early_group_in_small_point.py, super/early_group/same_woman/government_and_early_life.py or super/eye/high_time/good_time_or_fact.py under important woman"

---
+++ super/work_or_bad_fact/life/make_early_group_in_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government_and_bad_part(str_arg):
+    person_or_large_world(str_arg)
+    print('get_week')
+
+def person_or_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government_and_bad_part('own_government')
