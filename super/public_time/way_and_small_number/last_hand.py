403985d155070deb7ea7f00d8e4458f3ddadbc13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 10:43:49 -0400

    "mix: feel super/public_time/way_and_small_number/last_hand.py, super/life.py and super/come_week/get_long_place_from_woman/important_part/same_week_or_woman.py with few time"

---
+++ super/public_time/way_and_small_number/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government_or_long_company(str_arg):
+    large_person_and_old_hand(str_arg)
+    print('public_woman')
+
+def large_person_and_old_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government_or_long_company('go_large_life')
