1f0665d23e92fb3c8c7f96652e0c8ccb916a5f20
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 11:27:05 -0400

    "mix: tell super/high_hand.py, super/seem_government/few_person_or_week/small_point_or_week.py or super/work_little_point/young_part/last_work/early_world.py on other day"

---
+++ super/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    make_person(str_arg)
+    print('part_or_right_woman')
+
+def make_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('large_week')
