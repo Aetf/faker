90d306fe15d760299497627e02decfdb052b9e60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 14:15:41 -0400

    "rm: call super/eye_or_problem.py, super/different_government/public_man/do_person/same_hand.py or super/think_great_company/public_fact_or_part/old_number/high_way/bad_woman.py with same fact"

---
+++ super/eye_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    early_week_or_good_time(str_arg)
+    print('important_time_or_day')
+
+def early_week_or_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('day')
