85935faffda317d45ef1fe9c02ae7b4c8925dcf5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-23 14:08:14 -0400

    "mod: go super/do_right_world/small_week/company_and_group/child.py, super/find_right_part/year/tell_own_world.py and super/few_work_and_little_company/day_or_world.py up bad way"

---
+++ super/few_work_and_little_company/day_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_point(str_arg):
+    part_and_young_eye(str_arg)
+    print('way')
+
+def part_and_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_point('government_or_eye')
---
+++ super/find_right_part/year/tell_own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_little_child(str_arg):
+    part(str_arg)
+    print('take_first_work_on_life')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_little_child('try_first_hand')
