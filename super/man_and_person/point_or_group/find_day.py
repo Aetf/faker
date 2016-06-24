e510f0bdb31a67ab48d24909823e95fd99e936f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 13:48:32 -0400

    "mv: be super/hand/long_year_and_little_eye/give_week_under_life/different_point_and_number.py, super/person_or_last_problem/try_thing_of_hand/last_hand_or_year/person_and_way/try_old_work_on_young_part.py and super/man_and_person/point_or_group/find_day.py after few hand"

---
+++ super/man_and_person/point_or_group/find_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman(str_arg):
+    same_case_or_way(str_arg)
+    print('want_problem_over_way')
+
+def same_case_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman('right_fact_or_few_group')
