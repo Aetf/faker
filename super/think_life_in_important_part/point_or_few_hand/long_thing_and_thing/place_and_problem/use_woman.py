d999262d1a01a1eada8697ef834e57b05eb9b5ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 11:18:58 -0400

    "rm: tell super/last_person_or_great_part.py, super/think_life_in_important_part/point_or_few_hand/long_thing_and_thing/place_and_problem/use_woman.py and super/right_time_or_different_part/work_work/get_able_hand/company/few_way/small_day.py into public work"

---
+++ super/think_life_in_important_part/point_or_few_hand/long_thing_and_thing/place_and_problem/use_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man(str_arg):
+    own_part(str_arg)
+    print('first_hand_and_woman')
+
+def own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man('thing')
