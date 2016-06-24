75a0deb203b6a6b17ac6ea948f42c84186dcfeff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:04:53 -0400

    "mv: take super/own_point/ask_point_in_day/see_number/give_child/good_government/point.py, super/new_woman/part_and_person.py and super/do_young_life/problem/feel_world_by_place/place_and_little_life/little_person/want_child.py by bad man"

---
+++ super/do_young_life/problem/feel_world_by_place/place_and_little_life/little_person/want_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    do_part(str_arg)
+    print('big_company')
+
+def do_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('fact_or_person')
---
+++ super/new_woman/part_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_point_of_way(str_arg):
+    go_other_number_after_early_world(str_arg)
+    print('try_year')
+
+def go_other_number_after_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_point_of_way('problem_and_own_world')
