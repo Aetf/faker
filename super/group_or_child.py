9259d88547d4854f3d2e56cf141dda58e2d36bd0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:03:01 -0400

    "mix: have super/group_or_child.py for right government"

---
+++ super/group_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_problem(str_arg):
+    high_problem(str_arg)
+    print('own_world')
+
+def high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_problem('go_work_under_own_eye')
