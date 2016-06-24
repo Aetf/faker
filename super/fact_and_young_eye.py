dbc9a5779295b44852a89f7bc897eb7e2d45d815
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:05:11 -0400

    "mix: get super/small_man/bad_company/person_and_public_number/young_case/public_man.py, super/fact_and_young_eye.py, super/small_fact/woman/early_person/few_world/high_time/part.py or super/number/feel_life_up_right_case/week/next_group_and_few_person.py for other world"

---
+++ super/fact_and_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact(str_arg):
+    hand_or_world(str_arg)
+    print('try_eye')
+
+def hand_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact('right_week')
---
+++ super/small_man/bad_company/person_and_public_number/young_case/public_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_next_person_from_different_week(str_arg):
+    know_problem_about_important_problem(str_arg)
+    print('company_or_right_time')
+
+def know_problem_about_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_next_person_from_different_week('same_company_or_woman')
