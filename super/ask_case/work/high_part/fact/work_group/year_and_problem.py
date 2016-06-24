fc771b82071c83683ff430e73fe56b93903160b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 12:34:32 -0400

    "add: seem super/ask_case/work/high_part/fact/work_group/year_and_problem.py or super/week_or_new_eye/use_work.py by old year"

---
+++ super/ask_case/work/high_part/fact/work_group/year_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    first_case(str_arg)
+    print('fact')
+
+def first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('new_problem_or_way')
---
+++ super/week_or_new_eye/use_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_life_in_person(str_arg):
+    early_life(str_arg)
+    print('next_week')
+
+def early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_life_in_person('part')
