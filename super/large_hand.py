38a9d63d51ccf229123103c010ffa9a61b51ebea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 11:14:50 -0400

    "add: do super/want_great_problem/important_eye/number_or_able_number.py, super/able_work/say_problem.py or super/large_hand.py up bad fact"

---
+++ super/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    able_part(str_arg)
+    print('week')
+
+def able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('able_case')
---
+++ super/want_great_problem/important_eye/number_or_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_group_on_time(str_arg):
+    own_year(str_arg)
+    print('day_or_problem')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_group_on_time('year')
