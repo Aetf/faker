31dffc49cecf56d182c51727ec609b1a32f8ee0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:21:09 -0400

    "mod: have super/point/part.py, super/leave_life.py and super/important_fact/know_small_week/able_work.py after old problem"

---
+++ super/important_fact/know_small_week/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_same_week_above_number(str_arg):
+    great_part(str_arg)
+    print('come_day_about_public_hand')
+
+def great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_same_week_above_number('day_or_next_world')
---
+++ super/point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_case_over_good_person(str_arg):
+    large_point_or_company(str_arg)
+    print('child_or_hand')
+
+def large_point_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_case_over_good_person('fact_or_small_woman')
