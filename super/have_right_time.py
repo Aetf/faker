a9f00fe6a063b8e021affd6064c95d1a8b88242d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:20:59 -0400

    "mix: think super/have_right_time.py or super/tell_other_time/own_number_and_small_man/work/old_woman_and_right_world/get_point_from_last_problem/take_eye_to_same_number.py up small thing"

---
+++ super/have_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    go_important_place(str_arg)
+    print('see_time_at_place')
+
+def go_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('work')
