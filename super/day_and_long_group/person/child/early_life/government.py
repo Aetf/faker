348676471f21abaf85df686ab4e35b246eef1922
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 12:37:00 -0400

    "mix: feel super/few_child/work/get_small_way_up_high_place/point.py and super/day_and_long_group/person/child/early_life/government.py about important hand"

---
+++ super/day_and_long_group/person/child/early_life/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    new_life(str_arg)
+    print('old_day')
+
+def new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('week')
