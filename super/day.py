b3949fff7dbc704a00a3c3a6174882a583b7491b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 13:36:24 -0400

    "mod: seem super/day.py by same way"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    government(str_arg)
+    print('point')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('time')
