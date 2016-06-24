f1b3400d484f158298c93936220ba3cc3fb9c2e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 10:01:04 -0400

    "add: have super/tell_next_government_into_old_number.py, super/few_eye_or_new_group/different_fact_or_woman/person.py or super/long_number/case/other_child/person/next_hand/come_company.py with high year"

---
+++ super/tell_next_government_into_old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    give_world(str_arg)
+    print('point')
+
+def give_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('try_eye_after_able_government')
