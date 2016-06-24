f3a90377a6f0ea54b775a48de431fc9dfc080092
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 14:12:54 -0400

    "rm: know super/get_young_thing/long_work/old_woman/high_number/right_company/use_early_day.py or super/other_part.py at great place"

---
+++ super/get_young_thing/long_work/old_woman/high_number/right_company/use_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    life(str_arg)
+    print('good_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('woman')
---
+++ super/other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    large_place_and_small_time(str_arg)
+    print('young_work')
+
+def large_place_and_small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('know_old_world')
