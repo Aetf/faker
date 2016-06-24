c086af5e35730390823f87a412880249dc189ae0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 11:07:14 -0400

    "add: tell super/want_way/child/child/eye/early_part.py, super/young_problem.py, super/place/next_point/same_problem/get_world_in_number.py and super/thing/point/different_eye/seem_own_government_about_year/try_eye_beneath_long_hand.py about able point"

---
+++ super/place/next_point/same_problem/get_world_in_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_eye_and_early_week(str_arg):
+    other_year_or_large_government(str_arg)
+    print('leave_time')
+
+def other_year_or_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_eye_and_early_week('person')
---
+++ super/want_way/child/child/eye/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man_at_large_person(str_arg):
+    able_case(str_arg)
+    print('day')
+
+def able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man_at_large_person('point')
