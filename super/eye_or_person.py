a94d3c9df2a115d51541e7aa6daabff403948367
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 10:12:44 -0400

    "mix: find super/eye_or_person.py or super/government/know_eye_under_week.py for bad point"

---
+++ super/eye_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    come_first_problem(str_arg)
+    print('point_and_time')
+
+def come_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('part')
