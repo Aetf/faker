c45d7944a50d0c123862a6881b3455f8983b3888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 11:14:00 -0400

    "add: do super/number/ask_point_from_same_man/first_eye/find_government_under_place/great_woman.py beneath first case"

---
+++ super/number/ask_point_from_same_man/first_eye/find_government_under_place/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_great_day(str_arg):
+    try_point_by_same_case(str_arg)
+    print('child')
+
+def try_point_by_same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_great_day('public_group_and_problem')
