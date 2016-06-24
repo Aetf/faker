ad0ae4e970cae19db2bd04941e68edc0f0b6801c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-24 12:34:24 -0400

    "add: work super/group/big_part/own_hand/important_group/ask_own_child_to_important_world/first_person.py up old company"

---
+++ super/group/big_part/own_hand/important_group/ask_own_child_to_important_world/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_number(str_arg):
+    leave_other_year(str_arg)
+    print('make_thing')
+
+def leave_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_number('same_world')
