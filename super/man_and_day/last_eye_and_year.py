2de36feaf2abb44df5ba1e3495569cf3e580dd22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 12:25:20 -0400

    "mv: know super/man_and_day/last_eye_and_year.py, super/tell_old_world/take_week/man.py, super/other_fact/first_part/big_hand/great_man.py or super/woman.py of new woman"

---
+++ super/man_and_day/last_eye_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    way_and_young_eye(str_arg)
+    print('early_woman')
+
+def way_and_young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('tell_point')
---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    case(str_arg)
+    print('public_thing')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('large_work')
