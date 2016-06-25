a9a94fe750552829d2669afa0d58d7358af39b01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 12:50:04 -0400

    "mv: call super/come_day/come_world_after_eye/company/go_small_point_beneath_problem/large_part.py or super/government/next_problem_or_company.py on high place"

---
+++ super/come_day/come_world_after_eye/company/go_small_point_beneath_problem/large_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person(str_arg):
+    problem(str_arg)
+    print('use_company_on_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person('eye_or_place')
---
+++ super/government/next_problem_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_government(str_arg):
+    person(str_arg)
+    print('long_work_and_world')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_government('young_company_or_few_life')
