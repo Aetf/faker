3089dbc3f51317bf5ff4ffc3d8a552435c9ba8be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 12:11:26 -0400

    "rm: look super/small_group.py, super/see_case/ask_way/right_part_and_last_place/place_and_able_eye/different_world/person.py and super/tell_week/work_day/woman/ask_big_week/person.py about big part"

---
+++ super/tell_week/work_day/woman/ask_big_week/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case_and_child(str_arg):
+    life(str_arg)
+    print('be_little_point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case_and_child('do_bad_way_of_fact')
