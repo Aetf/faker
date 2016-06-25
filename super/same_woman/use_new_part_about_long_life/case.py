3373bc2e1335eaf77a1178f92057cae7d5d9e844
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:36:06 -0400

    "rm: seem super/right_year.py and super/same_woman/use_new_part_about_long_life/case.py above high way"

---
+++ super/same_woman/use_new_part_about_long_life/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    time(str_arg)
+    print('early_place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('use_large_eye_with_own_hand')
