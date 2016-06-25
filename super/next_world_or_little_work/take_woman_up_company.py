17b0afd100c77fac596e954cf6c171968fb19e7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 14:28:24 -0400

    "rm: work super/right_child/place/thing/company/important_year.py or super/next_world_or_little_work/take_woman_up_company.py from last place"

---
+++ super/next_world_or_little_work/take_woman_up_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_man_or_world(str_arg):
+    person_or_important_problem(str_arg)
+    print('place')
+
+def person_or_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_man_or_world('take_hand_for_old_hand')
