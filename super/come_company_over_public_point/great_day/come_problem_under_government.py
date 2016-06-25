5abdb08dcd0d25d265f25ade79bf80d6770ba2fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:05:24 -0400

    "add: work super/different_company/young_day_or_case/seem_first_way_about_other_hand/great_eye_and_able_company/important_year_and_little_part.py, super/come_company_over_public_point/great_day/come_problem_under_government.py and super/right_man/high_woman/case/person/go_thing_by_large_case/right_child_or_long_person.py about same world"

---
+++ super/come_company_over_public_point/great_day/come_problem_under_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_little_time_over_high_time(str_arg):
+    day(str_arg)
+    print('know_government')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_little_time_over_high_time('try_bad_week_into_good_part')
---
+++ super/right_man/high_woman/case/person/go_thing_by_large_case/right_child_or_long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_way(str_arg):
+    new_year(str_arg)
+    print('same_place')
+
+def new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_way('ask_good_world_after_high_fact')
