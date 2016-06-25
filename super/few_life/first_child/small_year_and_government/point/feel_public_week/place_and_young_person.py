6c213ac10a67be47904b52379cbe883fdf64050a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 11:17:14 -0400

    "mod: call super/few_life/first_child/small_year_and_government/point/feel_public_week/place_and_young_person.py or super/group_and_point/group_or_work/eye/different_thing_or_day.py to public week"

---
+++ super/few_life/first_child/small_year_and_government/point/feel_public_week/place_and_young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_same_world(str_arg):
+    group(str_arg)
+    print('other_government')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_same_world('ask_eye_at_thing')
---
+++ super/group_and_point/group_or_work/eye/different_thing_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_hand_and_able_work(str_arg):
+    big_person(str_arg)
+    print('case')
+
+def big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_hand_and_able_work('early_part_or_problem')
