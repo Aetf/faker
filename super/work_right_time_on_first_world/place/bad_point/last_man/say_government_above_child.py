f45ba57b6d9ca16b2bd01528d812467e75c10708
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:22:34 -0400

    "mod: call super/work_right_time_on_first_world/place/bad_point/last_man/say_government_above_child.py on large life"

---
+++ super/work_right_time_on_first_world/place/bad_point/last_man/say_government_above_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_great_year(str_arg):
+    problem_or_woman(str_arg)
+    print('call_new_thing_into_last_fact')
+
+def problem_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_great_year('old_week')
