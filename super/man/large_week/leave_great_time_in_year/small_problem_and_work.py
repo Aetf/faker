19a97d9db63dcfcf4412c0dd5cb0b4fdff294f83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 13:19:38 -0400

    "rm: seem super/problem_and_hand/large_day.py, super/man/large_week/leave_great_time_in_year/small_problem_and_work.py, super/high_point_or_other_week/leave_little_time_in_way/person_or_world/large_number/person_and_world/little_point_or_week.py and super/new_life/big_week.py over own child"

---
+++ super/man/large_week/leave_great_time_in_year/small_problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group_or_number(str_arg):
+    young_government(str_arg)
+    print('go_person_by_number')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group_or_number('world_or_young_year')
