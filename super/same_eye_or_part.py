ffb2c12bd601cf8e5c692f6769510020de1880dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 13:22:40 -0400

    "rm: find super/same_eye_or_part.py in different part"

---
+++ super/same_eye_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_old_person(str_arg):
+    year(str_arg)
+    print('case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_old_person('small_group_and_woman')
