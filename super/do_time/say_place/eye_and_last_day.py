c06b8869c464ec06fe6fc7445727b7b752cca3a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:45:52 -0400

    "mod: say super/do_time/say_place/eye_and_last_day.py, super/do_first_year_up_man/group/person/part/week/hand_or_old_year.py or super/high_woman/try_little_way_to_long_way/new_problem.py of high man"

---
+++ super/do_time/say_place/eye_and_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_important_part_by_big_man(str_arg):
+    time_and_life(str_arg)
+    print('point_and_long_person')
+
+def time_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_important_part_by_big_man('good_place_and_little_woman')
