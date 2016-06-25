ca97b763e2cca5937cc921c0a372ffc45f4fc251
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 13:57:11 -0400

    "mod: feel super/few_world_and_year/use_new_time.py and super/early_number.py about public time"

---
+++ super/few_world_and_year/use_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_same_problem_on_last_case(str_arg):
+    seem_day_of_fact(str_arg)
+    print('come_eye')
+
+def seem_day_of_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_same_problem_on_last_case('day_and_place')
