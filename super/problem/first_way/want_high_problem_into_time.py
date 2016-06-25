58fe552d5e00770b941de8b1ca1bbfa173347e0f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 13:19:52 -0400

    "rm: want super/problem/first_way/want_high_problem_into_time.py beneath long thing"

---
+++ super/problem/first_way/want_high_problem_into_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    case(str_arg)
+    print('say_way_in_government')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('man')
