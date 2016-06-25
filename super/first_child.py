dab883f4e0bcd152169e32f3f54b5d500add2104
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:24:11 -0400

    "rm: try super/first_child.py, super/seem_life/important_point/place/eye_and_great_group/ask_first_week/group_and_first_government.py and super/big_part_or_fact/government_and_important_number/hand/fact_or_fact.py above big person"

---
+++ super/first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_great_group(str_arg):
+    hand(str_arg)
+    print('other_eye')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_great_group('go_fact')
---
+++ super/seem_life/important_point/place/eye_and_great_group/ask_first_week/group_and_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_hand_from_day(str_arg):
+    new_child(str_arg)
+    print('give_big_government')
+
+def new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_hand_from_day('large_world')
