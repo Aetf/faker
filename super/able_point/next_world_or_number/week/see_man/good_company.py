8ea54a74b932684a091c8dabd8bf0f60251f1066
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 11:33:32 -0400

    "mix: say super/do_fact/feel_fact_above_world/great_fact_and_point/make_day_into_great_day/give_right_point_under_life.py, super/bad_woman_and_government/work_fact/man/high_case_or_number.py, super/able_point/next_world_or_number/week/see_man/good_company.py and super/woman_or_person/year.py beneath few hand"

---
+++ super/able_point/next_world_or_number/week/see_man/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_last_company(str_arg):
+    public_person(str_arg)
+    print('point_or_world')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_last_company('long_number')
