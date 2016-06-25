79db0f01deb48d182e2882ea3a68279238000cfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 12:14:12 -0400

    "mix: be super/hand_and_point/make_world_beneath_problem/ask_good_point_by_company/little_year/problem_and_other_point.py in right work"

---
+++ super/hand_and_point/make_world_beneath_problem/ask_good_point_by_company/little_year/problem_and_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_next_case(str_arg):
+    think_important_point(str_arg)
+    print('place')
+
+def think_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_next_case('early_man')
