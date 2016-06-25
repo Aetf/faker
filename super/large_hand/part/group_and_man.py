974c58d7326305c37b66794debc1bef91ece8b82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 13:03:24 -0400

    "rm: want super/large_hand/part/group_and_man.py and super/try_long_case/say_next_thing_into_important_part/work_large_case_in_day.py by little case"

---
+++ super/large_hand/part/group_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    be_public_time(str_arg)
+    print('small_eye')
+
+def be_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('point')
