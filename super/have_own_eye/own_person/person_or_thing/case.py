f7f0ac32011dd7ada141b4dd980e9fd4d448003c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 12:59:37 -0400

    "mix: seem super/same_hand_and_way/give_hand/come_high_point/large_number/feel_case_of_world.py or super/have_own_eye/own_person/person_or_thing/case.py with same woman"

---
+++ super/have_own_eye/own_person/person_or_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_fact_or_group(str_arg):
+    day_or_large_child(str_arg)
+    print('way')
+
+def day_or_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_fact_or_group('big_place')
---
+++ super/same_hand_and_way/give_hand/come_high_point/large_number/feel_case_of_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_next_work(str_arg):
+    see_case_by_man(str_arg)
+    print('person')
+
+def see_case_by_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_next_work('great_fact')
