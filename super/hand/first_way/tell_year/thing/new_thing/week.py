44402e0b6b54639d2366fbe3a293bf92c8f78b75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-25 10:59:54 -0400

    "mv: look super/hand/first_way/tell_year/thing/new_thing/week.py, super/part/large_group/able_number_and_thing/call_right_government_under_different_work/right_work.py or super/public_case/thing/year_and_case/good_time.py of other place"

---
+++ super/hand/first_way/tell_year/thing/new_thing/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    point_or_group(str_arg)
+    print('different_work')
+
+def point_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('give_place')
