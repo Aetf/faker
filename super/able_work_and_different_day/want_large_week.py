7eaa2bc2dc4171c65c396e88d1e284a6f9830c2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 14:49:13 -0400

    "mod: say super/able_work_and_different_day/want_large_week.py, super/other_thing/same_group/say_same_woman/different_group/thing_and_way/other_world.py and super/little_world/great_company/year.py by other world"

---
+++ super/able_work_and_different_day/want_large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_public_government_in_right_point(str_arg):
+    point(str_arg)
+    print('child_or_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_public_government_in_right_point('find_able_case')
---
+++ super/little_world/great_company/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    eye_or_problem(str_arg)
+    print('come_hand_beneath_problem')
+
+def eye_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('leave_problem_by_big_group')
