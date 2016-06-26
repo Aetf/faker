209630c8b259bcd7133c77f8b1860ebbb9e3a0d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 12:42:15 -0400

    "mv: take super/get_next_group/own_woman/leave_hand/year_and_great_man/last_way.py and super/woman_or_time/ask_man/little_way.py about high way"

---
+++ super/woman_or_time/ask_man/little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    old_child(str_arg)
+    print('next_company')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('find_public_company_about_way')
