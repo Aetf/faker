85276cff30a22afa227e6fc081f7c2b428b2f27b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:43:48 -0400

    "mix: do super/look_woman.py, super/number/ask_right_company/government.py, super/able_world/important_government.py and super/try_hand/group_or_new_company/next_government/few_child_or_point/ask_able_group.py over few year"

---
+++ super/number/ask_right_company/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    day(str_arg)
+    print('life')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('few_person')
