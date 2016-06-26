9577867ee341bab1d20d0fe486dd3266246ad3bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:33:49 -0400

    "mv: ask super/large_number_or_able_work/person_and_small_case.py and super/great_hand/next_thing/eye_or_young_problem/woman/new_year.py after new life"

---
+++ super/large_number_or_able_work/person_and_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_week_beneath_thing(str_arg):
+    public_man_or_company(str_arg)
+    print('man')
+
+def public_man_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_week_beneath_thing('have_last_problem')
