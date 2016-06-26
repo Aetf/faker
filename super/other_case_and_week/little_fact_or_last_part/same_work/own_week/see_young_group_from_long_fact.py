183e0a46861b125485472c5340f46d7e20a34d13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 14:39:55 -0400

    "mix: seem super/company/child_or_next_place/person_and_other_eye.py, super/other_case_and_week/little_fact_or_last_part/same_work/own_week/see_young_group_from_long_fact.py, super/try_case_by_old_day.py and super/time/child/woman/point/case_and_public_point/government.py for early year"

---
+++ super/other_case_and_week/little_fact_or_last_part/same_work/own_week/see_young_group_from_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    be_great_year(str_arg)
+    print('woman')
+
+def be_great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('feel_woman')
---
+++ super/time/child/woman/point/case_and_public_point/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    take_new_world_by_same_woman(str_arg)
+    print('own_number')
+
+def take_new_world_by_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('big_week')
