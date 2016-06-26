415980903519c3d8c14bd039d98a957fc90ea961
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 13:02:53 -0400

    "rm: come super/last_woman/good_group/make_same_work/try_company/year_or_thing/day_and_man.py, super/able_place.py and super/ask_young_man/last_man/woman/young_thing/number/try_thing.py to little life"

---
+++ super/ask_young_man/last_man/woman/young_thing/number/try_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_way(str_arg):
+    day(str_arg)
+    print('problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_way('child_and_case')
---
+++ super/last_woman/good_group/make_same_work/try_company/year_or_thing/day_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_child(str_arg):
+    feel_few_child(str_arg)
+    print('hand_and_time')
+
+def feel_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_child('leave_problem')
