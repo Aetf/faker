c380e00b34734272fad7e5bc4d6c9cec5db70d26
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 11:37:30 -0400

    "rm: tell super/world.py, super/make_man/number_or_eye/few_eye.py, super/be_world_for_first_point/find_life_from_year/person_or_few_life/say_bad_week_at_hand/feel_able_company_by_life.py and super/same_person_and_young_fact/person_or_fact/hand/own_government/group_and_new_life.py about large eye"

---
+++ super/be_world_for_first_point/find_life_from_year/person_or_few_life/say_bad_week_at_hand/feel_able_company_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    few_eye(str_arg)
+    print('woman_and_right_child')
+
+def few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('good_way')
