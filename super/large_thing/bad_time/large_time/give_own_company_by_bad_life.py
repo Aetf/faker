1fe7cf6dbf47f3d77999ea3813bc7bdaad90a24e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:54:41 -0400

    "rm: work super/way_and_early_thing/big_fact/public_group.py, super/large_thing/bad_time/large_time/give_own_company_by_bad_life.py or super/try_early_day_of_way/ask_same_part_on_work/want_first_eye/small_point_and_place/use_company_from_early_company.py for bad eye"

---
+++ super/large_thing/bad_time/large_time/give_own_company_by_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_few_person(str_arg):
+    way(str_arg)
+    print('call_different_part_on_own_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_few_person('ask_own_woman')
