3d01adb5b36ec00710a5f0772b4146b2d689d574
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:31:42 -0400

    "mod: come super/little_man_or_able_child/young_problem/life_or_child/do_hand_after_part/few_company.py by public place"

---
+++ super/little_man_or_able_child/young_problem/life_or_child/do_hand_after_part/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_little_government(str_arg):
+    first_point(str_arg)
+    print('big_way')
+
+def first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_little_government('few_part')
