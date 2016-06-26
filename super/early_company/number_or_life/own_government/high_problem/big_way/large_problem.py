31ba5b3e5094e1c6ae94683731f6b0855e1d34ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 13:22:16 -0400

    "mv: want super/early_company/number_or_life/own_government/high_problem/big_way/large_problem.py of good world"

---
+++ super/early_company/number_or_life/own_government/high_problem/big_way/large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand(str_arg):
+    public_man_or_number(str_arg)
+    print('old_case_or_day')
+
+def public_man_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand('last_thing_and_small_year')
