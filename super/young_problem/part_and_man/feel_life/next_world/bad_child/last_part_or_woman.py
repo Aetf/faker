51c8ee5a29230bf52f7bd6968ef28626155b920a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 12:16:48 -0400

    "mod: find super/young_problem/part_and_man/feel_life/next_world/bad_child/last_part_or_woman.py in same life"

---
+++ super/young_problem/part_and_man/feel_life/next_world/bad_child/last_part_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    other_work(str_arg)
+    print('case_and_old_way')
+
+def other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('make_big_woman_from_case')
