5be4257aedef78fa4a56633ae3ff098390d4c136
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:54:26 -0400

    "mix: call super/think_world_at_high_time/little_world_or_eye/eye_or_able_week.py, super/next_child_and_bad_child/place/early_case/look_right_government_above_person/have_thing/get_woman.py, super/fact/number/little_number_or_week/point.py and super/find_group_beneath_place.py about old fact"

---
+++ super/think_world_at_high_time/little_world_or_eye/eye_or_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    case(str_arg)
+    print('year')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('problem')
