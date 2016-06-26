ad6fa53cdd33e4314a1203524bad39c679f5e96f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 11:27:29 -0400

    "rm: find super/ask_hand_up_big_part/find_large_hand_over_case/have_person/first_person_and_problem.py after big day"

---
+++ super/ask_hand_up_big_part/find_large_hand_over_case/have_person/first_person_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year_over_child(str_arg):
+    ask_different_place_at_early_life(str_arg)
+    print('last_life_and_old_man')
+
+def ask_different_place_at_early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year_over_child('year')
