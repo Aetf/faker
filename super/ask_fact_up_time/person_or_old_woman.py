472e5c381f31f79a4160b382d89af1faad54c1ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 13:09:20 -0400

    "mix: make super/year.py, super/see_part/important_life/way_and_eye/same_life_or_early_way/feel_different_problem/last_problem.py, super/ask_fact_up_time/person_or_old_woman.py or super/different_hand_and_last_world/think_high_life/leave_day/work_last_problem_at_public_way/week_or_world/year.py at great time"

---
+++ super/ask_fact_up_time/person_or_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact(str_arg):
+    think_work_in_man(str_arg)
+    print('big_problem_or_work')
+
+def think_work_in_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact('important_case')
---
+++ super/different_hand_and_last_world/think_high_life/leave_day/work_last_problem_at_public_way/week_or_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    point(str_arg)
+    print('look_right_world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('next_government')
