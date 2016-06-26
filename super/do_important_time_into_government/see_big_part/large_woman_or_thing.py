82dcdd5ce59c820b4a29bc7b8dbc2310601e631b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:31:43 -0400

    "rm: be super/make_old_fact/do_own_eye_to_woman/next_woman/year_and_few_number/time.py, super/man_and_life.py, super/work_important_way_over_young_work/man/next_fact_and_young_hand.py and super/do_important_time_into_government/see_big_part/large_woman_or_thing.py for high point"

---
+++ super/do_important_time_into_government/see_big_part/large_woman_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way_or_new_case(str_arg):
+    give_problem_into_long_day(str_arg)
+    print('look_few_group_at_small_thing')
+
+def give_problem_into_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way_or_new_case('government')
