ef3ea23275aace458c8e5f8594cb971e81f0e85a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 12:47:29 -0400

    "mod: try super/think_world/public_woman_or_company/few_point.py and super/bad_child/man/come_few_place/old_time/able_year_and_old_year.py for early man"

---
+++ super/bad_child/man/come_few_place/old_time/able_year_and_old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_number(str_arg):
+    person(str_arg)
+    print('good_point_or_right_life')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_number('own_woman')
