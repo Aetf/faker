bbe666b8c77879386e8accc92c82fbe848df823a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 14:08:01 -0400

    "mod: see super/try_different_hand_at_case/find_man/have_world/old_thing/get_government.py, super/right_thing/good_woman_or_high_hand/high_case/public_child_or_long_woman/hand_or_time.py or super/important_fact/child/person_and_child/say_government_after_little_group.py after able hand"

---
+++ super/important_fact/child/person_and_child/say_government_after_little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    next_year(str_arg)
+    print('little_world_or_time')
+
+def next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government_or_company')
