f5cc402c77674c406dd41d4be5dfb6c99b4955bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-26 10:32:44 -0400

    "rm: have super/call_life/young_person.py with few case"

---
+++ super/call_life/young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    point(str_arg)
+    print('last_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('know_place_with_first_problem')
