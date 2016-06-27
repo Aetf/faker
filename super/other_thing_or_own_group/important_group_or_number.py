022455f0614a7ce841f1bd896ad7b6aec9e44dc7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:09:06 -0400

    "mv: call super/other_thing_or_own_group/important_group_or_number.py, super/fact/new_eye.py or super/number_and_large_man/ask_next_time/company/be_number_after_group/few_eye.py at public work"

---
+++ super/other_thing_or_own_group/important_group_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_bad_government_above_thing(str_arg):
+    small_work(str_arg)
+    print('look_week')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_bad_government_above_thing('person_or_number')
