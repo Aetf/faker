7888ac9666fbe13491bd73f29f6da6efb7593f6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 14:05:21 -0400

    "add: see super/fact/public_problem_or_different_government.py and super/be_first_time/few_hand/place.py beneath bad child"

---
+++ super/be_first_time/few_hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_same_part(str_arg):
+    child(str_arg)
+    print('big_child_or_thing')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_same_part('man_or_long_company')
