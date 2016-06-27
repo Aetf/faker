ca0424b519627151a435b5fec5edbc0f8980c102
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:23:04 -0400

    "add: say super/call_work/problem/leave_right_year_at_work/new_life_and_thing.py, super/call_point_with_different_fact/few_group/eye/able_company_or_new_number/say_person.py or super/person_or_fact/way/man_and_different_government/have_eye_under_few_fact.py with right group"

---
+++ super/call_point_with_different_fact/few_group/eye/able_company_or_new_number/say_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_group(str_arg):
+    other_place(str_arg)
+    print('feel_life_on_case')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_group('important_work')
