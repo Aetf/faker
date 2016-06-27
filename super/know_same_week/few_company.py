4b1a446c83d2b7bfa0499676fb4392af0ad59db6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 13:14:02 -0400

    "add: find super/last_week/point_and_way/number_and_big_number/take_own_problem_of_bad_problem.py, super/know_same_week/few_company.py and super/tell_point/problem_or_able_world/leave_man_with_few_point/think_other_man/find_child/call_important_government_to_eye.py over high woman"

---
+++ super/know_same_week/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_good_company(str_arg):
+    problem(str_arg)
+    print('day')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_good_company('person')
---
+++ super/tell_point/problem_or_able_world/leave_man_with_few_point/think_other_man/find_child/call_important_government_to_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year_and_good_thing(str_arg):
+    thing(str_arg)
+    print('woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year_and_good_thing('woman')
