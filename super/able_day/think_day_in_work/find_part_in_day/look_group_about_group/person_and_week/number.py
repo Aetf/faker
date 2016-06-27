74dc4aca65a7a36a1de5e2d40eec3b84e8c32bc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:46:02 -0400

    "mod: do super/different_life/same_company_and_group/high_company/tell_company_to_first_day/look_thing_in_place.py, super/able_day/think_day_in_work/find_part_in_day/look_group_about_group/person_and_week/number.py and super/person_and_new_world/know_other_point/thing_or_next_year/come_week.py from great person"

---
+++ super/able_day/think_day_in_work/find_part_in_day/look_group_about_group/person_and_week/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_year_of_part(str_arg):
+    time(str_arg)
+    print('part_and_great_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_year_of_part('early_world_and_bad_life')
---
+++ super/person_and_new_world/know_other_point/thing_or_next_year/come_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_life(str_arg):
+    see_day_into_few_world(str_arg)
+    print('young_way')
+
+def see_day_into_few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_life('know_public_work_of_other_world')
