433fff77aa549eb2556cfaf78fa44721caa87baf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 13:07:32 -0400

    "mod: ask super/bad_person_or_important_part/number/bad_case.py, super/hand_or_case.py and super/large_hand/young_time/place/new_man_and_year/day.py into public life"

---
+++ super/bad_person_or_important_part/number/bad_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_fact(str_arg):
+    work_able_way(str_arg)
+    print('great_work')
+
+def work_able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_fact('bad_day')
