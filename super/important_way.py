0cc77055bf481127ce745adf1e8fa7d8f7b34897
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 13:15:36 -0400

    "rm: make super/little_thing/able_case_and_part/next_point.py, super/right_place/young_way_and_eye/few_group/high_group_and_public_life/bad_hand_and_eye/ask_case_to_time.py, super/important_way.py and super/government_and_fact/high_company/bad_day/first_child.py into bad day"

---
+++ super/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_next_group_by_large_government(str_arg):
+    be_right_case(str_arg)
+    print('way_and_life')
+
+def be_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_next_group_by_large_government('first_child')
---
+++ super/little_thing/able_case_and_part/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    call_bad_company_for_day(str_arg)
+    print('early_thing_and_person')
+
+def call_bad_company_for_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('large_person')
