50bee8f9630298411d9b4b275f1fe247dc9bddc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:41:33 -0400

    "mod: give super/next_place_and_own_hand/man.py and super/day_or_long_government/person.py to right day"

---
+++ super/next_place_and_own_hand/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    man(str_arg)
+    print('long_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('different_year')
