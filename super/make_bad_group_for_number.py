2ea262b66dd3e44356eb4a444cee42866252f9d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 14:54:07 -0400

    "mod: seem super/big_life/public_man/right_eye/time_or_other_eye/call_bad_child/make_important_man.py and super/make_bad_group_for_number.py to own week"

---
+++ super/make_bad_group_for_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    same_government(str_arg)
+    print('work')
+
+def same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('life')
