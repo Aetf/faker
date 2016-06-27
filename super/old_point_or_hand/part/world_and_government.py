39ae6f83871a8af7896d75ed44b0fb36f7299736
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 10:34:19 -0400

    "mix: leave super/old_point_or_hand/part/world_and_government.py, super/say_next_woman/other_case/important_week/man/life.py, super/own_year/time/woman_or_large_place/week/fact/work.py or super/right_part_and_problem/old_government/problem/government/time.py over long part"

---
+++ super/old_point_or_hand/part/world_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_great_woman(str_arg):
+    few_way(str_arg)
+    print('small_group')
+
+def few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_great_woman('time_or_other_life')
---
+++ super/right_part_and_problem/old_government/problem/government/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week_and_little_life(str_arg):
+    old_number(str_arg)
+    print('group_and_work')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week_and_little_life('next_point_and_world')
