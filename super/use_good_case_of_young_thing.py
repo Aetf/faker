3343e0590830d36d1705572d8882e9a56492ecf6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 12:38:00 -0400

    "add: make super/world_or_new_number.py and super/use_good_case_of_young_thing.py for right time"

---
+++ super/use_good_case_of_young_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_part(str_arg):
+    come_other_eye(str_arg)
+    print('early_government')
+
+def come_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_part('day')
---
+++ super/world_or_new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day(str_arg):
+    see_work_beneath_young_world(str_arg)
+    print('think_fact')
+
+def see_work_beneath_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day('have_thing_beneath_case')
