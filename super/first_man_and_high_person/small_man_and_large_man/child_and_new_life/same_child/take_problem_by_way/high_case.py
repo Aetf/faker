71250fae1ffe253c8ee67cecceaeb8b4f86115b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 13:52:41 -0400

    "add: give super/thing_or_same_number/time_and_next_group/great_group_and_thing.py, super/first_man_and_high_person/small_man_and_large_man/child_and_new_life/same_child/take_problem_by_way/high_case.py, super/have_world_after_fact/group/last_case/year.py or super/company.py from other place"

---
+++ super/first_man_and_high_person/small_man_and_large_man/child_and_new_life/same_child/take_problem_by_way/high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    same_number(str_arg)
+    print('take_day_by_first_place')
+
+def same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('point')
---
+++ super/have_world_after_fact/group/last_case/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_fact(str_arg):
+    thing(str_arg)
+    print('early_thing')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_fact('time')
