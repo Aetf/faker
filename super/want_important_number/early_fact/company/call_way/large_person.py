70107558bef3fd13894532d012214e8c96f3443c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 14:05:14 -0400

    "mod: give super/want_important_number/early_fact/company/call_way/large_person.py above important government"

---
+++ super/want_important_number/early_fact/company/call_way/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    few_woman(str_arg)
+    print('feel_new_year_into_big_company')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('new_week')
