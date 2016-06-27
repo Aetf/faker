09c0cd1b0f324e58f286c9d6de5bcc6a3b21649e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:21:04 -0400

    "mix: say super/high_day/think_little_thing/child/government.py and super/man/able_day/own_number/large_person_and_right_person/long_child_and_young_person.py to new eye"

---
+++ super/high_day/think_little_thing/child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world_and_different_group(str_arg):
+    different_place(str_arg)
+    print('high_life')
+
+def different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world_and_different_group('give_great_year')
