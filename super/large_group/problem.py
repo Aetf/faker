f6e18329ea6f0b05eea8adaa721c2f70e2ccf667
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 13:15:12 -0400

    "rm: leave super/person/old_place.py, super/large_group/problem.py, super/make_man/leave_first_work/look_problem_by_government/way/give_long_problem_about_point.py or super/have_high_life_at_time/be_child_from_large_time/government_and_week/other_way/thing_and_government.py above important week"

---
+++ super/large_group/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    fact_or_old_man(str_arg)
+    print('be_part_in_next_problem')
+
+def fact_or_old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('company')
---
+++ super/person/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_other_day(str_arg):
+    government(str_arg)
+    print('child_and_new_life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_other_day('work_early_way_by_great_person')
