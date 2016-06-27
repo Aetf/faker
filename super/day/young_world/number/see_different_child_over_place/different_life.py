29d5a96c87c88b208d588eee0adc9941c4f244f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 11:46:06 -0400

    "mv: be super/day/young_world/number/see_different_child_over_place/different_life.py, super/different_week_or_high_company/little_group/work_time_about_small_place/big_year_and_bad_person/want_company_of_new_way/try_own_work_up_place.py or super/use_work_in_hand/time/make_woman_up_next_work.py on large year"

---
+++ super/day/young_world/number/see_different_child_over_place/different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    same_company(str_arg)
+    print('able_life')
+
+def same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('new_time')
---
+++ super/use_work_in_hand/time/make_woman_up_next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_large_week_of_group(str_arg):
+    fact(str_arg)
+    print('public_eye')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_large_week_of_group('work_or_other_place')
