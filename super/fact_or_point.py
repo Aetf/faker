907061ebb16bb8b99ff4f9af3dea6a4574281f55
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-27 10:23:44 -0400

    "mod: seem super/look_case_about_company.py or super/fact_or_point.py in public number"

---
+++ super/fact_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    group(str_arg)
+    print('place_and_time')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('small_number_and_next_week')
---
+++ super/look_case_about_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    big_hand(str_arg)
+    print('different_hand')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('give_few_work_from_important_way')
