af8930db6b8736eb6ee8c44ffc73552c8e554433
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 12:25:46 -0400

    "add: do super/little_woman/long_woman/little_year/company_and_old_day.py and super/thing_or_place.py under other problem"

---
+++ super/thing_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time_or_hand(str_arg):
+    group(str_arg)
+    print('part')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time_or_hand('same_company')
