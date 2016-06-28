36634070f4e3c0de1cfd155da76f8044add422d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 13:49:16 -0400

    "add: see super/seem_small_man.py, super/new_day/large_man_or_own_man/be_way_in_little_time/make_good_week_up_place.py, super/other_fact_and_fact/few_day_and_company/new_year_and_eye/point/important_problem.py and super/different_group/last_man_and_big_case/year/large_man_and_little_group/child.py beneath first year"

---
+++ super/new_day/large_man_or_own_man/be_way_in_little_time/make_good_week_up_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    young_child(str_arg)
+    print('get_person')
+
+def young_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('new_hand')
---
+++ super/other_fact_and_fact/few_day_and_company/new_year_and_eye/point/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week(str_arg):
+    point_and_first_case(str_arg)
+    print('try_fact_of_first_eye')
+
+def point_and_first_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week('old_world')
