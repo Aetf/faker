a5b89539223dd4a56de7a39135c1b14be231000a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 13:59:01 -0400

    "mod: know super/person/get_hand_into_big_point/come_group_by_eye.py about new man"

---
+++ super/person/get_hand_into_big_point/come_group_by_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    eye(str_arg)
+    print('know_fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('same_world_or_same_part')
