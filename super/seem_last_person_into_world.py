88420179b3cd6ea1eca61c818324b882148eaec8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 10:37:56 -0400

    "rm: get super/seem_last_person_into_world.py under great government"

---
+++ super/seem_last_person_into_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    bad_time(str_arg)
+    print('go_new_person')
+
+def bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('important_problem')
