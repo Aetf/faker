2eaadbaf1f8a400a557dfcd46936689252858bc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 12:15:21 -0400

    "mod: go super/year_and_right_case/have_few_person/do_fact_after_small_time/be_great_group/see_person/high_place_and_bad_woman.py, super/different_group_and_long_time/able_person_or_world/right_problem_and_point/small_hand_or_good_point/make_number_in_case.py, super/hand_or_case.py or super/feel_eye/man_and_company/group/important_man.py on other way"

---
+++ super/hand_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    large_part(str_arg)
+    print('look_important_woman_of_work')
+
+def large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('look_company_for_public_man')
