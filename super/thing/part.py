06a497897cb75a0f968bb9b4118fa4de069e88d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 10:42:06 -0400

    "mv: find super/thing/part.py and super/little_child_or_government/early_number.py from bad week"

---
+++ super/thing/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number_or_company(str_arg):
+    high_woman(str_arg)
+    print('early_way')
+
+def high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number_or_company('part')
