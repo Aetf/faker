1b6aaed53cbc312b86a0187d7dd290dfeb179114
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 13:04:21 -0400

    "add: ask super/first_work/same_fact.py, super/hand/good_woman_or_high_part/fact_and_man/new_government/world.py, super/day/public_work/use_first_company/leave_thing/woman/last_company_or_important_case.py or super/week_and_last_case/person/number_and_first_case/give_year/time/week.py up long group"

---
+++ super/day/public_work/use_first_company/leave_thing/woman/last_company_or_important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    good_world_and_point(str_arg)
+    print('big_way')
+
+def good_world_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('other_part')
---
+++ super/first_work/same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_large_part(str_arg):
+    do_important_place_for_life(str_arg)
+    print('problem')
+
+def do_important_place_for_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_large_part('big_thing_or_fact')
