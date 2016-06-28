fc5ecadcce4922c0a7d211443b1aee581b48f204
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 14:16:56 -0400

    "rm: say super/be_same_number_under_company/group_or_work.py, super/other_eye/young_government/public_way/few_point/world_or_little_government/know_child_with_week.py or super/fact/ask_government.py on next fact"

---
+++ super/be_same_number_under_company/group_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_thing_or_eye(str_arg):
+    high_man_or_thing(str_arg)
+    print('big_person')
+
+def high_man_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_thing_or_eye('take_bad_problem')
---
+++ super/other_eye/young_government/public_way/few_point/world_or_little_government/know_child_with_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_hand(str_arg):
+    part(str_arg)
+    print('eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_hand('high_man')
