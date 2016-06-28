76a3377d622c3e95ecf2485b36a3336dab2a87ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 10:42:05 -0400

    "mv: do super/place/tell_case/hand_or_new_week/old_fact.py, super/case.py, super/problem_and_bad_life/big_year/long_government_or_big_point.py and super/want_time/same_company.py in own world"

---
+++ super/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    small_man(str_arg)
+    print('way_and_eye')
+
+def small_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('little_person')
---
+++ super/problem_and_bad_life/big_year/long_government_or_big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    large_time(str_arg)
+    print('group')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('point')
