a0312ab3d8ee8d5e4ce734e7e978e05d124802e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 14:03:09 -0400

    "mv: look super/world_or_able_child.py about new group"

---
+++ super/world_or_able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    large_person(str_arg)
+    print('leave_new_part')
+
+def large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('young_world')
