cc85ea1bce99eb1d544ced43abcbafb9ce1cf66f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 14:59:21 -0400

    "add: want super/new_life/try_government_with_big_point/person/look_number_on_other_fact.py, super/ask_big_problem/early_thing_and_week/come_number.py and super/early_number_and_small_fact/group_and_place/case/few_life.py by few thing"

---
+++ super/ask_big_problem/early_thing_and_week/come_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year_or_government(str_arg):
+    eye(str_arg)
+    print('give_way_at_woman')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year_or_government('day')
---
+++ super/new_life/try_government_with_big_point/person/look_number_on_other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    little_group(str_arg)
+    print('child_and_week')
+
+def little_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('hand')
