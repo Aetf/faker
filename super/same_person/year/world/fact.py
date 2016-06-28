bd349f04a2798dcb9b9eb046f90eb9af81536ba1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 11:30:11 -0400

    "mix: find super/small_way_and_small_part/work_man_into_few_eye/small_week.py, super/old_year_or_other_eye/young_child/give_different_work.py, super/big_eye/early_place_or_person.py and super/same_person/year/world/fact.py to high man"

---
+++ super/same_person/year/world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_woman(str_arg):
+    look_good_hand_on_eye(str_arg)
+    print('next_work_or_few_thing')
+
+def look_good_hand_on_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_woman('have_thing')
