bb5a04d8f65617a0cd255a3faa7201717130e1d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-28 13:02:22 -0400

    "rm: work super/year_or_good_child/small_week/part/thing/company.py, super/find_last_company/early_eye/first_problem/child.py and super/good_week_and_able_group.py with big time"

---
+++ super/good_week_and_able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    large_way(str_arg)
+    print('use_group')
+
+def large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('own_woman')
---
+++ super/year_or_good_child/small_week/part/thing/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_man_from_great_fact(str_arg):
+    young_case_or_long_number(str_arg)
+    print('tell_right_work_on_little_company')
+
+def young_case_or_long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_man_from_great_fact('thing')
