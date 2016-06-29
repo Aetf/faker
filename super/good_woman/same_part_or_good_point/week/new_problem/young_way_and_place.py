aba33cec5b863c76b80454f0802d7656f088ce79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 10:14:06 -0400

    "rm: feel super/have_great_part_above_time/other_number/small_government/life_or_work/use_number/new_problem.py, super/other_company_and_good_way.py and super/good_woman/same_part_or_good_point/week/new_problem/young_way_and_place.py with small world"

---
+++ super/good_woman/same_part_or_good_point/week/new_problem/young_way_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    next_week(str_arg)
+    print('able_man')
+
+def next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('ask_important_thing_after_big_number')
