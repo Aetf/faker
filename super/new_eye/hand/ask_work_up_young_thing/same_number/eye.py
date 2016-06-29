d42ee272590b0d0bedbecc0f957c60f6e28120b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 11:23:08 -0400

    "mix: feel super/good_problem_or_old_fact/public_work.py or super/new_eye/hand/ask_work_up_young_thing/same_number/eye.py for different eye"

---
+++ super/new_eye/hand/ask_work_up_young_thing/same_number/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_person_under_year(str_arg):
+    ask_little_company_under_problem(str_arg)
+    print('first_problem_and_child')
+
+def ask_little_company_under_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_person_under_year('place')
