1e4c11eab93ea11f5859f1646e66f97f2e038dd4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:12:56 -0400

    "mod: say super/seem_problem/find_right_group_at_first_part/new_case.py or super/be_number_at_thing/old_woman/point/work/child.py at public thing"

---
+++ super/seem_problem/find_right_group_at_first_part/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    woman_and_point(str_arg)
+    print('large_place')
+
+def woman_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('get_company')
