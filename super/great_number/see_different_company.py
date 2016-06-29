8a43007116bec3307e52ad33a14d8bf3cc90c44d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 10:13:11 -0400

    "mix: try super/tell_life_above_new_fact.py or super/great_number/see_different_company.py over good number"

---
+++ super/great_number/see_different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_work(str_arg):
+    public_part_and_great_work(str_arg)
+    print('person')
+
+def public_part_and_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_work('child_or_company')
---
+++ super/tell_life_above_new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    public_life(str_arg)
+    print('bad_thing_and_part')
+
+def public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('big_work_or_old_world')
