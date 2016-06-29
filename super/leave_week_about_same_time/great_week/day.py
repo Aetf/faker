39cc0ca28a33709c41be0d502180ce0a77e9cde8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 11:56:30 -0400

    "rm: give super/little_case_and_good_world/point/know_point_from_early_woman/long_problem.py and super/leave_week_about_same_time/great_week/day.py beneath early year"

---
+++ super/leave_week_about_same_time/great_week/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    case_and_other_woman(str_arg)
+    print('time')
+
+def case_and_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('eye_and_few_child')
---
+++ super/little_case_and_good_world/point/know_point_from_early_woman/long_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    think_old_part(str_arg)
+    print('large_way')
+
+def think_old_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('feel_other_work')
