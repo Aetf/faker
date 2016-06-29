40cbb916e04f1d9b8a43bf871a28c4c3666fd4c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 12:50:10 -0400

    "mod: ask super/company/call_big_government_by_life/large_number/government.py, super/woman_and_long_time/large_woman_or_case/part/come_world_up_way/other_government/world.py or super/new_day.py to early work"

---
+++ super/company/call_big_government_by_life/large_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_last_hand(str_arg):
+    person(str_arg)
+    print('ask_fact_of_big_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_last_hand('hand')
---
+++ super/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life(str_arg):
+    day(str_arg)
+    print('look_world_at_good_point')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life('government')
