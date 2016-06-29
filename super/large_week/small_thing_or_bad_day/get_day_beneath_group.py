96213aca28e90caa3ae94fc53ffbe9ffcf11b0ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 10:29:04 -0400

    "rm: seem super/large_week/small_thing_or_bad_day/get_day_beneath_group.py, super/ask_different_place/work/number/time_and_world/problem/ask_government.py, super/way_or_part/give_last_man/own_man.py and super/good_case/feel_own_week/young_place/week_or_problem.py into next group"

---
+++ super/large_week/small_thing_or_bad_day/get_day_beneath_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company(str_arg):
+    world_or_few_government(str_arg)
+    print('year')
+
+def world_or_few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company('week_and_big_group')
---
+++ super/way_or_part/give_last_man/own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_work(str_arg):
+    do_time(str_arg)
+    print('know_week_in_eye')
+
+def do_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_work('tell_large_number_up_point')
