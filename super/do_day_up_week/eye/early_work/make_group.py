bcbbbcdc3443749a333b351648ddcbadb4f8b768
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:45:55 -0400

    "mod: look super/do_day_up_week/eye/early_work/make_group.py at last person"

---
+++ super/do_day_up_week/eye/early_work/make_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_place(str_arg):
+    place(str_arg)
+    print('new_work_or_thing')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_place('number')
