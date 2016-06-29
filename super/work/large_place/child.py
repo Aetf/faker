c19579ecebb72f539d98657cfc7aa018c041c642
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:05:37 -0400

    "add: get super/fact_and_world/great_eye/next_company/life/work/government.py, super/work/large_place/child.py and super/few_week_or_day/big_point/large_place_and_young_day/large_man.py into long way"

---
+++ super/work/large_place/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_public_fact(str_arg):
+    different_case(str_arg)
+    print('way')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_public_fact('man')
