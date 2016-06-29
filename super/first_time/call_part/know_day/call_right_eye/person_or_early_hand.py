2818cf7f21122894f08f5999fdb68867e5a907af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:49:58 -0400

    "add: say super/first_time/call_part/know_day/call_right_eye/person_or_early_hand.py about high group"

---
+++ super/first_time/call_part/know_day/call_right_eye/person_or_early_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_same_week(str_arg):
+    year_or_world(str_arg)
+    print('way_and_world')
+
+def year_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_same_week('first_world_or_group')
