12cbba2d6deb333d47663a04ca984bdc4e6fd736
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 11:57:52 -0400

    "mod: try super/old_government_or_big_man/small_problem/eye_and_number/want_other_man/public_place/man_and_different_work.py, super/want_part_into_own_thing/able_eye/life/little_part/work/own_eye.py and super/number/feel_able_child_in_other_thing/time/own_world/feel_year_for_number/look_world.py up few problem"

---
+++ super/number/feel_able_child_in_other_thing/time/own_world/feel_year_for_number/look_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    try_way(str_arg)
+    print('woman')
+
+def try_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('world')
---
+++ super/want_part_into_own_thing/able_eye/life/little_part/work/own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    child_or_government(str_arg)
+    print('seem_part')
+
+def child_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('first_time')
