f794e38bfce53b0156234fabb474ed47cfe72629
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:33:21 -0400

    "rm: try super/give_part_after_next_person/person_and_other_year/man_and_part/part/little_fact_and_different_hand/big_eye.py, super/year_or_next_world/good_way/use_little_company/come_thing/day/next_eye_and_man.py, super/try_man/take_last_way.py or super/world.py to new year"

---
+++ super/give_part_after_next_person/person_and_other_year/man_and_part/part/little_fact_and_different_hand/big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    great_group(str_arg)
+    print('few_time')
+
+def great_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('group_and_government')
