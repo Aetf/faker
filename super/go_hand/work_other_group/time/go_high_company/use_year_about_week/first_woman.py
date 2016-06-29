f012c12434147ac50cec3fc76b3f6d9201fea61d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 10:01:13 -0400

    "mod: think super/go_hand/work_other_group/time/go_high_company/use_year_about_week/first_woman.py or super/number_or_other_fact/company/long_work/know_new_world_on_same_work.py under great point"

---
+++ super/go_hand/work_other_group/time/go_high_company/use_year_about_week/first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_number_in_bad_government(str_arg):
+    high_time_and_problem(str_arg)
+    print('other_woman')
+
+def high_time_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_number_in_bad_government('important_place_or_different_place')
