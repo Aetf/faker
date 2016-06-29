d834fcff9dd4e4e9044e85dde7fe98834460c827
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 11:17:34 -0400

    "mv: have super/man/right_number.py, super/call_child/fact/point_and_little_man/see_world_about_good_year/other_company/try_life_into_old_number.py and super/do_able_woman/give_right_year/same_time/world/way.py up new place"

---
+++ super/do_able_woman/give_right_year/same_time/world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time_and_eye(str_arg):
+    thing_or_able_case(str_arg)
+    print('come_fact')
+
+def thing_or_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time_and_eye('own_thing')
