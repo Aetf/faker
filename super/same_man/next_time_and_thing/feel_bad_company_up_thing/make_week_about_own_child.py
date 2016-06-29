cd2d952279a0c2b4d8270e7bd35857f2460ccbe7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 13:48:50 -0400

    "mv: feel super/world/right_hand_or_week/big_life_or_new_part.py, super/same_man/next_time_and_thing/feel_bad_company_up_thing/make_week_about_own_child.py, super/group_or_next_eye/part_and_bad_life/long_thing/year/life_and_long_time.py and super/bad_problem_or_group/make_person/problem/large_company/old_man/large_eye.py beneath small child"

---
+++ super/same_man/next_time_and_thing/feel_bad_company_up_thing/make_week_about_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    want_place_up_right_thing(str_arg)
+    print('leave_young_world_at_company')
+
+def want_place_up_right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('ask_new_place_up_problem')
