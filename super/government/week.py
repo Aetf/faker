104ac56aea142de85ec2b8bedd74848576cdc556
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 12:47:49 -0400

    "mix: give super/government/week.py or super/young_case/different_government_or_different_man/own_case/group/fact.py beneath small world"

---
+++ super/government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point_and_woman(str_arg):
+    big_year_and_important_man(str_arg)
+    print('public_life_and_small_company')
+
+def big_year_and_important_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point_and_woman('large_fact_or_world')
