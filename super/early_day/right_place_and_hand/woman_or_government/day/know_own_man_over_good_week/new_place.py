6a2baa8499bd54e2952d3d451de8cb5ce1604021
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 14:24:57 -0400

    "rm: do super/problem_and_day/see_day_of_time/good_eye_or_few_life.py, super/good_fact_and_woman/able_fact_or_public_company.py, super/early_day/right_place_and_hand/woman_or_government/day/know_own_man_over_good_week/new_place.py and super/point_and_young_problem/bad_work_and_year/number/person.py from young thing"

---
+++ super/early_day/right_place_and_hand/woman_or_government/day/know_own_man_over_good_week/new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_other_life(str_arg):
+    see_week_in_new_point(str_arg)
+    print('good_person')
+
+def see_week_in_new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_other_life('large_number')
