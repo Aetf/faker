6b94e11f3fb76c310e66f94727a5234ddb0b4cf7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-29 13:41:34 -0400

    "mix: call super/government/world/bad_man.py, super/week_and_thing/great_fact_or_child/have_part/find_company/find_little_thing.py and super/seem_place_of_man/high_hand_and_person/public_group/work.py with new life"

---
+++ super/seem_place_of_man/high_hand_and_person/public_group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    hand(str_arg)
+    print('same_week')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('call_part')
---
+++ super/week_and_thing/great_fact_or_child/have_part/find_company/find_little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    little_work(str_arg)
+    print('hand')
+
+def little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('number')
