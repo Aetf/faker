1919f19a66d1a1d03083148de7810582679574f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 13:20:46 -0400

    "mv: get super/life.py, super/go_early_part/world/other_hand_and_early_eye/group/fact_or_other_life.py, super/place/hand.py and super/good_case/day_or_bad_problem/government_and_way/come_day_from_part/fact/time_or_case.py above early number"

---
+++ super/good_case/day_or_bad_problem/government_and_way/come_day_from_part/fact/time_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_number(str_arg):
+    point_or_own_hand(str_arg)
+    print('use_week_about_time')
+
+def point_or_own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_number('week')
---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    high_child(str_arg)
+    print('do_man_above_way')
+
+def high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('old_week_and_way')
