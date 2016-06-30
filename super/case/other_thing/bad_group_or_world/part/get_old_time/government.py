9a2bff330e2aceae644fa61ec98e13c569273fbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 14:18:56 -0400

    "rm: say super/case/other_thing/bad_group_or_world/part/get_old_time/government.py or super/time/do_person_of_right_world/first_number_or_young_company.py at good child"

---
+++ super/case/other_thing/bad_group_or_world/part/get_old_time/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    day(str_arg)
+    print('be_place')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('small_life')
---
+++ super/time/do_person_of_right_world/first_number_or_young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_or_bad_point(str_arg):
+    good_person(str_arg)
+    print('point_or_great_way')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_or_bad_point('same_work')
