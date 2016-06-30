725a7ed49978be5e75a947291804013d9512ccbf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 11:02:52 -0400

    "mod: ask super/bad_woman/life/little_child_or_old_work/life/number/hand_or_thing.py, super/good_work/new_government/place.py or super/important_week/make_problem_beneath_large_life.py over large week"

---
+++ super/bad_woman/life/little_child_or_old_work/life/number/hand_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_week(str_arg)
+    print('make_big_point_of_child')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('public_man')
---
+++ super/important_week/make_problem_beneath_large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    able_thing(str_arg)
+    print('little_year')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('few_point')
