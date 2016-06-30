5498439184a7eeee5d44a9a5694be8911f52bd17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 10:15:06 -0400

    "rm: take super/tell_way_under_good_hand.py, super/get_week/take_world/company/work/point_or_last_work.py or super/right_case/good_time/thing/young_fact/group_and_important_part.py above young thing"

---
+++ super/right_case/good_time/thing/young_fact/group_and_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week(str_arg)
+    print('find_number_beneath_small_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('few_day_or_important_life')
