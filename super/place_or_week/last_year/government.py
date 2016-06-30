ad3b7194c5173e014ed6963a45079399234972b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 13:05:20 -0400

    "mod: take super/small_person_and_good_fact/life/make_little_hand/use_bad_eye_into_own_person/day_or_problem/seem_few_way_about_same_eye.py, super/good_day/thing/government_or_government/man_or_old_work/try_place_under_right_government.py and super/place_or_week/last_year/government.py under long hand"

---
+++ super/place_or_week/last_year/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    case(str_arg)
+    print('thing')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('old_person')
---
+++ super/small_person_and_good_fact/life/make_little_hand/use_bad_eye_into_own_person/day_or_problem/seem_few_way_about_same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    old_life(str_arg)
+    print('last_work')
+
+def old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('next_government_or_place')
