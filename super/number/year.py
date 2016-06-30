a04aad75aef128e27022c61a8a0bb47ecc2fafc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 12:55:59 -0400

    "mv: come super/life_or_next_group.py and super/number/year.py at next world"

---
+++ super/number/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work_and_woman(str_arg):
+    person(str_arg)
+    print('different_problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work_and_woman('early_day')
