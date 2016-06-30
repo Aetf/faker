8baa72502458f397ac1f67c4fbc0198c75433dec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 11:04:41 -0400

    "mod: tell super/fact/small_fact/large_way/take_right_fact/time/make_problem.py and super/same_child/look_way/try_day.py from good group"

---
+++ super/same_child/look_way/try_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company_and_high_problem(str_arg):
+    seem_first_world_about_part(str_arg)
+    print('life_or_early_person')
+
+def seem_first_world_about_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company_and_high_problem('great_part')
