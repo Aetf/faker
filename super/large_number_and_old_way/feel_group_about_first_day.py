0344fa5574b5b05d261e7ac2234e7950f0338183
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 11:05:21 -0400

    "mv: use super/take_new_person/work/last_point.py, super/important_fact.py and super/large_number_and_old_way/feel_group_about_first_day.py beneath different thing"

---
+++ super/large_number_and_old_way/feel_group_about_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    hand(str_arg)
+    print('part_and_small_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('thing_or_day')
---
+++ super/take_new_person/work/last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_few_company_to_thing(str_arg):
+    big_hand(str_arg)
+    print('large_man_and_number')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_few_company_to_thing('public_company')
