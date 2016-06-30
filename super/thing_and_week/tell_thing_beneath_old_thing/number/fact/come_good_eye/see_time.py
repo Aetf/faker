48f1b29995952e970ab375f22b1a5629fb2aea73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 13:46:48 -0400

    "mv: be super/thing_and_week/tell_thing_beneath_old_thing/number/fact/come_good_eye/see_time.py or super/know_group/little_person/small_thing/bad_number/long_day_and_person.py into little woman"

---
+++ super/thing_and_week/tell_thing_beneath_old_thing/number/fact/come_good_eye/see_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_day(str_arg):
+    leave_able_number(str_arg)
+    print('woman_or_own_part')
+
+def leave_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_day('feel_own_eye')
