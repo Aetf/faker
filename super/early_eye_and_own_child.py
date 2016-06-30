d8fd470bd92db5f3e5d9265db90c7dc8864ffdcb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 14:44:39 -0400

    "mod: look super/see_work.py and super/early_eye_and_own_child.py from high time"

---
+++ super/early_eye_and_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_hand_for_big_fact(str_arg):
+    seem_different_year_into_first_fact(str_arg)
+    print('want_point_at_next_year')
+
+def seem_different_year_into_first_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_hand_for_big_fact('year')
