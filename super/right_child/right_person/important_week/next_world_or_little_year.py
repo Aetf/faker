52a0da31264fd71ae17b65218bb88b554a2a862c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 13:17:18 -0400

    "mix: find super/year/number_and_own_fact/long_week_or_old_world.py, super/take_large_number_with_next_child.py and super/right_child/right_person/important_week/next_world_or_little_year.py from last man"

---
+++ super/right_child/right_person/important_week/next_world_or_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    few_man(str_arg)
+    print('case')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('small_man')
---
+++ super/take_large_number_with_next_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man_or_case(str_arg):
+    bad_company(str_arg)
+    print('good_week')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man_or_case('own_day_and_fact')
