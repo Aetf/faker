52b580b3bb576171f16707dd47884b456a957a42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 14:24:18 -0400

    "rm: have super/hand/be_bad_way_to_problem/ask_new_fact_in_young_problem/case/time/last_week_and_child.py, super/work/tell_part/find_last_thing/ask_right_problem_up_woman/other_company/see_large_number_after_problem.py and super/year_and_eye.py of early part"

---
+++ super/hand/be_bad_way_to_problem/ask_new_fact_in_young_problem/case/time/last_week_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_first_person(str_arg):
+    eye_or_great_time(str_arg)
+    print('go_young_world')
+
+def eye_or_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_first_person('hand')
---
+++ super/year_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_day(str_arg):
+    seem_old_government(str_arg)
+    print('early_government_or_hand')
+
+def seem_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_day('same_number')
