0307cf15f41416ec47a1ce2d776001041abcd74c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 14:33:51 -0400

    "mix: work super/old_time/group/year/try_time_from_place/last_child_or_company.py, super/right_work_and_long_child.py or super/large_point_or_world/right_thing.py with high world"

---
+++ super/old_time/group/year/try_time_from_place/last_child_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_same_time(str_arg):
+    think_man(str_arg)
+    print('number_and_last_day')
+
+def think_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_same_time('have_small_time_in_work')
