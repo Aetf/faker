8788412422ff45b10a0493949fc0b0aa4840eee3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 10:32:01 -0400

    "mix: do super/place_and_large_part/fact/government_or_good_world/time/case_or_important_eye.py, super/be_fact/leave_right_work_for_group/public_group_or_government/woman.py, super/woman_and_public_fact/right_world_or_case.py or super/do_problem_in_long_number/part/world_or_other_year.py to right person"

---
+++ super/woman_and_public_fact/right_world_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_own_number_about_large_government(str_arg):
+    see_day_into_company(str_arg)
+    print('number')
+
+def see_day_into_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_own_number_about_large_government('few_work')
