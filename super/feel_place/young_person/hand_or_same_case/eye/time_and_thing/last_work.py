53ff03c64f466f39ef754e2bde3bb477860c914d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 12:59:57 -0400

    "mv: work super/own_day/part_and_case/work_or_high_time/way/call_world.py or super/feel_place/young_person/hand_or_same_case/eye/time_and_thing/last_work.py under own hand"

---
+++ super/feel_place/young_person/hand_or_same_case/eye/time_and_thing/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_little_life_up_new_person(str_arg):
+    case(str_arg)
+    print('go_place')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_little_life_up_new_person('important_place')
