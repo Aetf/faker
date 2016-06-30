7ea7850fc9f14c0b9d6f30b9be232fd9e489f578
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 11:07:24 -0400

    "add: look super/old_part/day.py, super/small_child/right_week/think_thing_under_other_fact.py, super/man/give_group_from_number/feel_little_group/time_and_good_day/other_part_and_large_group.py or super/fact_and_year/young_problem_and_person/hand.py about new time"

---
+++ super/old_part/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    point_and_case(str_arg)
+    print('different_government')
+
+def point_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('life')
---
+++ super/small_child/right_week/think_thing_under_other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_point(str_arg):
+    find_little_case_about_life(str_arg)
+    print('work_case')
+
+def find_little_case_about_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_point('first_part')
