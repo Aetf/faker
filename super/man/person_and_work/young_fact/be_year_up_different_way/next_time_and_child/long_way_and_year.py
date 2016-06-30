855d227cfd8bb84b6e2825eabcfe061ff3f2df91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 10:02:58 -0400

    "mod: want super/high_world.py, super/day/fact.py and super/man/person_and_work/young_fact/be_year_up_different_way/next_time_and_child/long_way_and_year.py by important fact"

---
+++ super/man/person_and_work/young_fact/be_year_up_different_way/next_time_and_child/long_way_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_point(str_arg):
+    do_group_up_young_government(str_arg)
+    print('woman')
+
+def do_group_up_young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_point('hand')
