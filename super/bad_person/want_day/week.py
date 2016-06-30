d9cc34261c73ab043bda9ecd58071bcf32399f09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 11:40:30 -0400

    "mod: feel super/hand/use_part_up_good_part.py or super/bad_person/want_day/week.py in old number"

---
+++ super/bad_person/want_day/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    last_company(str_arg)
+    print('man')
+
+def last_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('large_place_and_person')
