78a8d7c4f9169bd771a53a77c8a9559a4bbdd6b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-30 12:10:38 -0400

    "mv: want super/next_way_and_young_man.py, super/think_great_government/early_life.py and super/early_government_or_company/company/go_week_into_little_year/young_work_and_point/next_case_or_own_child/fact.py above young way"

---
+++ super/early_government_or_company/company/go_week_into_little_year/young_work_and_point/next_case_or_own_child/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_big_thing(str_arg):
+    feel_small_world_for_problem(str_arg)
+    print('call_few_person_into_able_year')
+
+def feel_small_world_for_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_big_thing('different_fact')
---
+++ super/next_way_and_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_person(str_arg):
+    day_or_place(str_arg)
+    print('group_or_bad_woman')
+
+def day_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_person('child_and_other_fact')
