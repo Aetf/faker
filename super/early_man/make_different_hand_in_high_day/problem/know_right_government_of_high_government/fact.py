c68c5603a82be127cebf26b7b2a04120013a68b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 13:40:03 -0400

    "mod: leave super/early_man/make_different_hand_in_high_day/problem/know_right_government_of_high_government/fact.py about public person"

---
+++ super/early_man/make_different_hand_in_high_day/problem/know_right_government_of_high_government/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_old_case(str_arg):
+    work(str_arg)
+    print('seem_different_child')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_old_case('good_government_and_large_number')
