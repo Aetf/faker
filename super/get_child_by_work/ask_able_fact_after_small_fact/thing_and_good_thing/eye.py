ec630244aaded6ccf120e91d32de85a51e3c825f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 11:00:09 -0400

    "add: go super/get_child_by_work/ask_able_fact_after_small_fact/thing_and_good_thing/eye.py from next time"

---
+++ super/get_child_by_work/ask_able_fact_after_small_fact/thing_and_good_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    thing(str_arg)
+    print('big_number')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('point_and_fact')
