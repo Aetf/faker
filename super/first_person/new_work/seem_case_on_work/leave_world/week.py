5c494aaeb18fe01c7bc5143a6b4ed10cf42b617d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 10:54:32 -0400

    "mod: seem super/look_group/person_or_able_life/large_day.py, super/be_able_week/call_early_week_at_right_hand/life/ask_early_year.py, super/first_person/new_work/seem_case_on_work/leave_world/week.py or super/last_work/child/eye/company.py beneath high number"

---
+++ super/first_person/new_work/seem_case_on_work/leave_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_right_week(str_arg):
+    last_group_or_way(str_arg)
+    print('see_next_hand')
+
+def last_group_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_right_week('be_thing_beneath_work')
---
+++ super/look_group/person_or_able_life/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    new_problem(str_arg)
+    print('other_point')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('early_group_or_big_place')
