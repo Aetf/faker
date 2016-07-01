096ec3da74848c8924f9a718df273a03e058f876
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 11:10:35 -0400

    "add: make super/person_and_time.py, super/public_part/public_person/get_long_work_to_woman/work/last_person_and_fact/child.py or super/know_week_in_year/time_or_first_case/want_woman/part_or_last_week/come_good_way.py in able week"

---
+++ super/know_week_in_year/time_or_first_case/want_woman/part_or_last_week/come_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    government(str_arg)
+    print('get_few_case')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('have_early_time')
