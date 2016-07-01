29f5acffd58d7b407fb4c6b7fa7988d256888fe2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 11:09:21 -0400

    "add: see super/get_great_day_in_thing/great_world/last_company/few_work_and_case/man/young_group.py, super/company_or_next_eye.py, super/say_next_man_of_fact/other_day/early_woman/year_and_first_problem/thing_or_part.py or super/large_man/say_week_after_work/great_problem_and_world/early_man.py in next part"

---
+++ super/large_man/say_week_after_work/great_problem_and_world/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    hand(str_arg)
+    print('young_work')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('small_number_and_new_time')
