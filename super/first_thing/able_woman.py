a0878b5a608908c79574367d720e0a15bc6a6082
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 10:52:42 -0400

    "mv: feel super/old_man/part/government_or_fact/want_long_part/public_place/long_part.py, super/young_person/part.py or super/first_thing/able_woman.py in new part"

---
+++ super/first_thing/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    go_little_year_to_problem(str_arg)
+    print('right_year')
+
+def go_little_year_to_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('have_part_from_great_number')
