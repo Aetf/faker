b960fc27e900f337b4d797d13c15018ddf51b1cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 14:39:27 -0400

    "rm: see super/place/do_case_about_person/own_group_and_man/small_man_and_eye.py or super/different_child/year/person/place/own_part/try_big_week.py up different child"

---
+++ super/different_child/year/person/place/own_part/try_big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_work_or_young_fact(str_arg):
+    young_person(str_arg)
+    print('big_problem')
+
+def young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_work_or_young_fact('high_year')
