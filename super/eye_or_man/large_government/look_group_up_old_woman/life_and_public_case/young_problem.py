b8fd51a3a7c78b7b530b18190b92d4b24d070de6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 12:33:35 -0400

    "mv: call super/first_part_or_public_eye/see_early_hand/eye/eye_or_new_man/company/able_work.py, super/eye_or_man/large_government/look_group_up_old_woman/life_and_public_case/young_problem.py and super/child/problem.py about other time"

---
+++ super/eye_or_man/large_government/look_group_up_old_woman/life_and_public_case/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    big_fact(str_arg)
+    print('next_hand')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('own_day')
---
+++ super/first_part_or_public_eye/see_early_hand/eye/eye_or_new_man/company/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man(str_arg):
+    fact(str_arg)
+    print('come_world_at_early_day')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man('first_year_or_way')
