529e82b887aa80c29a51d4f5bf3d0db6a4dcf7a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 13:33:03 -0400

    "mv: look super/point/leave_first_way_about_different_point/get_thing/number/woman/take_little_way.py about large case"

---
+++ super/point/leave_first_way_about_different_point/get_thing/number/woman/take_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man(str_arg):
+    hand_or_small_woman(str_arg)
+    print('person')
+
+def hand_or_small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man('government')
