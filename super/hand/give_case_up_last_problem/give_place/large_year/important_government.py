d785b1eb76cf2b4e47bfda29b55422df711488ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 10:01:44 -0400

    "rm: want super/hand/give_case_up_last_problem/give_place/large_year/important_government.py and super/work_and_new_government/problem_and_company/long_child/place_and_problem/same_group.py about next part"

---
+++ super/hand/give_case_up_last_problem/give_place/large_year/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    try_world(str_arg)
+    print('large_life')
+
+def try_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('week')
