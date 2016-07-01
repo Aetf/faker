fa414a4ffb0c41b16bcb2b99150c1c8ccb4ed447
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 12:40:51 -0400

    "mix: do super/own_hand_or_problem/go_fact/child_and_fact/long_hand_and_woman.py and super/try_last_company/find_way/next_group/year_and_company.py after right week"

---
+++ super/own_hand_or_problem/go_fact/child_and_fact/long_hand_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day_and_thing(str_arg):
+    little_case(str_arg)
+    print('great_company')
+
+def little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day_and_thing('life_and_same_company')
---
+++ super/try_last_company/find_way/next_group/year_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    be_other_woman(str_arg)
+    print('thing')
+
+def be_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('young_day_and_person')
