2b1c48705e2b55070b9bc63a370ab8d29c009449
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 12:19:59 -0400

    "rm: give super/place_or_old_problem/life.py and super/important_thing/group/time_or_important_hand/number_or_life/number/different_woman_or_thing.py over own group"

---
+++ super/place_or_old_problem/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_man_of_bad_thing(str_arg):
+    young_year_and_little_case(str_arg)
+    print('good_world_and_own_government')
+
+def young_year_and_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_man_of_bad_thing('woman')
