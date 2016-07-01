21a8a97c6b130ddbc162eb017a145518fb583a76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 10:22:47 -0400

    "add: do super/different_work/bad_case.py or super/big_hand_or_person/good_way/look_few_man_to_little_life/world_or_day/first_government/eye.py by little man"

---
+++ super/different_work/bad_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    hand(str_arg)
+    print('right_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('new_fact')
