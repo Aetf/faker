1cd3455f58cc42820e0e3e0701cf32fc92e9a103
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 10:16:29 -0400

    "mix: do super/public_life/man/eye_or_government/feel_hand/big_time.py, super/part/fact_or_public_man/eye_or_time/old_case_and_bad_way/call_work_in_good_place/place_or_few_day.py, super/important_way/thing/have_group/time/find_public_person/point.py or super/place/person.py after small problem"

---
+++ super/place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye(str_arg):
+    able_child_and_early_man(str_arg)
+    print('woman_and_year')
+
+def able_child_and_early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye('man')
