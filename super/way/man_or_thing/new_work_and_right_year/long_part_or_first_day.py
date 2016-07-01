cfd53f2f3ac812577312f364fc26001d9efd5ad7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 14:24:41 -0400

    "add: give super/able_world.py, super/way/man_or_thing/new_work_and_right_year/long_part_or_first_day.py and super/problem/year/own_child/look_problem/work_or_government/go_great_life_to_case.py beneath early case"

---
+++ super/way/man_or_thing/new_work_and_right_year/long_part_or_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_number(str_arg):
+    important_problem_or_life(str_arg)
+    print('ask_case')
+
+def important_problem_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_number('leave_man')
