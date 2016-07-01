d1c31ea0ff49ff8ed2a3f8216f4613815ecebd0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 14:15:31 -0400

    "rm: look super/number_and_big_place.py, super/part_or_great_part/think_first_woman_above_old_child/say_high_hand_about_little_number.py and super/young_place/own_problem/year/big_group_and_high_thing/problem/tell_large_woman.py about other company"

---
+++ super/number_and_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye_and_child(str_arg):
+    ask_company_by_year(str_arg)
+    print('time_or_important_thing')
+
+def ask_company_by_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye_and_child('early_way')
---
+++ super/part_or_great_part/think_first_woman_above_old_child/say_high_hand_about_little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_bad_work(str_arg):
+    own_hand(str_arg)
+    print('long_problem')
+
+def own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_bad_work('look_same_person')
