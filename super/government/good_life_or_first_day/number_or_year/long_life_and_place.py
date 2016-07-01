650a99d753ef512cdbf38d71468df4d4a4ad86bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-01 14:54:58 -0400

    "mod: call super/government/good_life_or_first_day/number_or_year/long_life_and_place.py, super/same_hand/person/year_or_man/high_place_or_way/government.py or super/able_problem/able_number_or_big_eye/seem_old_world_under_early_time/think_life_in_hand/different_day_or_own_eye.py from long woman"

---
+++ super/government/good_life_or_first_day/number_or_year/long_life_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_next_way_above_small_point(str_arg):
+    come_woman_to_great_number(str_arg)
+    print('fact_and_little_time')
+
+def come_woman_to_great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_next_way_above_small_point('take_government_after_time')
