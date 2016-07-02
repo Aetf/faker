047aa71e1d3bd8faaa989c8b32088143b0fc16f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 10:19:29 -0400

    "rm: want super/government.py into big person"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    case(str_arg)
+    print('life')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('day')
