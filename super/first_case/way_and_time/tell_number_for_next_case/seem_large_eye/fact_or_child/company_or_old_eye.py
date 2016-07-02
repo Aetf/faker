4d49823f9996520eb5985943b7681e3fc919696b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 12:00:21 -0400

    "add: ask super/first_case/way_and_time/tell_number_for_next_case/seem_large_eye/fact_or_child/company_or_old_eye.py, super/have_point/point/ask_different_group_on_life.py and super/point/see_great_man_in_point/same_number/right_child_or_company/able_eye.py by new work"

---
+++ super/first_case/way_and_time/tell_number_for_next_case/seem_large_eye/fact_or_child/company_or_old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child_and_right_problem(str_arg):
+    eye(str_arg)
+    print('important_work')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child_and_right_problem('place')
---
+++ super/point/see_great_man_in_point/same_number/right_child_or_company/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_important_week(str_arg):
+    company_or_large_child(str_arg)
+    print('try_new_time_of_right_week')
+
+def company_or_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_important_week('group')
