f1b3fec16bb19a5323a5f8e00865e545ec7eb9e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 14:56:02 -0400

    "mv: call super/thing/have_long_life/know_fact_on_world/other_eye.py in next work"

---
+++ super/thing/have_long_life/know_fact_on_world/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_place(str_arg):
+    call_work_on_case(str_arg)
+    print('life')
+
+def call_work_on_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_place('big_government')
