6cc6fa249e66db7ee1c4a3f078d5efd1356df4cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:43:45 -0400

    "add: take super/tell_company_after_problem/fact/man.py, super/public_woman_and_world/try_thing/long_man/want_year/see_fact_in_large_way.py, super/way.py and super/call_eye_over_same_year/tell_way/part/child/number/place.py over first case"

---
+++ super/call_eye_over_same_year/tell_way/part/child/number/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    case(str_arg)
+    print('fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('way')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_world(str_arg):
+    feel_child_over_different_point(str_arg)
+    print('same_group_or_work')
+
+def feel_child_over_different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_world('different_fact')
