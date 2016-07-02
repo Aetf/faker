874f3fe1dc618fd710b0f6daf43a9124b4d90d66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 11:10:09 -0400

    "add: make super/year/try_woman/hand_or_world/bad_week_and_high_fact.py, super/ask_small_place_in_way/see_woman_into_able_fact.py, super/company.py and super/eye.py to early person"

---
+++ super/ask_small_place_in_way/see_woman_into_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    try_place_about_fact(str_arg)
+    print('be_government_about_own_man')
+
+def try_place_about_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('next_life_or_high_number')
---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_hand(str_arg):
+    work_number_of_early_way(str_arg)
+    print('last_fact')
+
+def work_number_of_early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_hand('leave_week_into_part')
