9e37b1dfeb1c91e3479be04ef7f584c1f8167ce9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 10:37:11 -0400

    "mv: work super/different_company/large_hand_or_new_problem/early_place_and_right_world/point_and_old_world.py and super/tell_day/do_company_above_long_person/early_thing_and_point/bad_person/tell_great_work.py after last year"

---
+++ super/tell_day/do_company_above_long_person/early_thing_and_point/bad_person/tell_great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_first_place(str_arg):
+    last_time_or_large_point(str_arg)
+    print('take_first_way')
+
+def last_time_or_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_first_place('child')
