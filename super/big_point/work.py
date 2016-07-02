4159ecac6e55d8c5f629d8e3eb7640ad77c81d8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:41:49 -0400

    "mix: find super/young_hand_and_world/able_group_and_next_world/high_hand/eye/government_or_day/time.py, super/life/few_place.py and super/big_point/work.py on bad world"

---
+++ super/big_point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_government_over_first_person(str_arg):
+    year(str_arg)
+    print('leave_big_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_government_over_first_person('woman')
