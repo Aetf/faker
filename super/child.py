8b3d03d755a9514aeddc2bdf1e0ecf791f292d0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 10:22:40 -0400

    "mv: do super/different_day/give_day_at_point/large_hand_or_other_part/way.py, super/place/say_person_after_different_group/man/tell_long_year_on_person/right_point.py or super/child.py from old hand"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    part_or_company(str_arg)
+    print('government_or_child')
+
+def part_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('have_able_company')
