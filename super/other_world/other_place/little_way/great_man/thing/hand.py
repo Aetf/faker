1e688d914f72ccad7d34b8a5c3c5b830ab7ff1f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 12:18:49 -0400

    "rm: find super/time/time/place_and_small_world.py and super/other_world/other_place/little_way/great_man/thing/hand.py by bad number"

---
+++ super/other_world/other_place/little_way/great_man/thing/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_first_life(str_arg):
+    get_old_number(str_arg)
+    print('life_and_able_point')
+
+def get_old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_first_life('world')
