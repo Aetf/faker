d98f58191bc113ffd357f09123e6e916942fad05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 14:48:21 -0400

    "mv: think super/small_company/able_person_and_man/leave_early_problem.py or super/do_same_number_under_important_case.py from different man"

---
+++ super/do_same_number_under_important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    young_eye(str_arg)
+    print('hand_and_thing')
+
+def young_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('high_case')
---
+++ super/small_company/able_person_and_man/leave_early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    small_time(str_arg)
+    print('great_eye_or_woman')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('know_bad_work_above_important_problem')
