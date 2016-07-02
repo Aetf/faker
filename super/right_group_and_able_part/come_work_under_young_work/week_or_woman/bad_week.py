93e0580df122941ea660a648fe11066fad559a08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:53:09 -0400

    "mod: work super/right_group_and_able_part/come_work_under_young_work/week_or_woman/bad_week.py of public fact"

---
+++ super/right_group_and_able_part/come_work_under_young_work/week_or_woman/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    bad_hand(str_arg)
+    print('work_day')
+
+def bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('life')
