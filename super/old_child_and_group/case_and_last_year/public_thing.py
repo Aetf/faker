ac63ce5eb769ce3238e260141a6838b997a075e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 14:47:28 -0400

    "mix: know super/group_or_large_government.py, super/old_child_and_group/case_and_last_year/public_thing.py and super/leave_early_number/day_and_woman/go_own_hand_about_old_man/large_time/big_time.py in other week"

---
+++ super/old_child_and_group/case_and_last_year/public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    right_way_and_young_day(str_arg)
+    print('young_eye_or_right_work')
+
+def right_way_and_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('person')
