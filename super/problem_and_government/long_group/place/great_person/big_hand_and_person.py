e49049010ee2c1c2f0cb86c8eae5c5e2d6be53b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 11:13:29 -0400

    "mix: think super/problem_and_government/long_group/place/great_person/big_hand_and_person.py or super/use_different_man_after_last_year/way/able_point/fact/place/seem_bad_way.py from young person"

---
+++ super/problem_and_government/long_group/place/great_person/big_hand_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    first_way_and_new_hand(str_arg)
+    print('point')
+
+def first_way_and_new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('other_group')
