af5e8c2d2c192d9cccc840ac8f4e4b9fef470871
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 10:44:27 -0400

    "rm: leave super/old_eye/know_child/week/know_thing.py, super/say_group/small_world_or_last_hand/important_hand/first_group/place.py or super/know_little_point_for_time/part.py under high life"

---
+++ super/know_little_point_for_time/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man_or_company(str_arg):
+    own_woman(str_arg)
+    print('give_eye')
+
+def own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man_or_company('good_world_and_large_fact')
