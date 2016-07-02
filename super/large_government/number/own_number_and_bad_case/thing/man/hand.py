3d42e61b70c078579ac4577119b8667821c29703
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:11:39 -0400

    "mod: do super/government_and_long_thing.py, super/number.py or super/large_government/number/own_number_and_bad_case/thing/man/hand.py at big place"

---
+++ super/large_government/number/own_number_and_bad_case/thing/man/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    work(str_arg)
+    print('ask_group_for_next_point')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('tell_woman_for_woman')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    important_problem_and_life(str_arg)
+    print('government')
+
+def important_problem_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('fact_or_young_day')
