d66f6ebc2aac54fd2b5c0c0db697e8d52c415434
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 11:45:30 -0400

    "rm: go super/find_number/do_high_week_under_woman.py or super/young_problem/small_time.py after early child"

---
+++ super/young_problem/small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    bad_life(str_arg)
+    print('point')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('make_great_person_over_last_place')
