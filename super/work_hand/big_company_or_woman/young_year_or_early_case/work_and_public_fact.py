60b2da940d5cbbb2aef7a3810e1958a12255027e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:22:13 -0400

    "mv: see super/work_hand/big_company_or_woman/young_year_or_early_case/work_and_public_fact.py to small man"

---
+++ super/work_hand/big_company_or_woman/young_year_or_early_case/work_and_public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    young_life_and_woman(str_arg)
+    print('own_time')
+
+def young_life_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('small_company')
