6a1a5fb14072a5710f9ae15e8e3fd98be1639287
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 11:31:39 -0400

    "add: think super/take_day/place/able_way/problem/child/man.py, super/find_new_place.py and super/few_week/give_good_year_up_point/place/next_thing_and_own_fact/go_time_from_part.py over different government"

---
+++ super/take_day/place/able_way/problem/child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_important_part(str_arg):
+    woman(str_arg)
+    print('young_problem')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_important_part('ask_part')
