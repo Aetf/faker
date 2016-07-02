1a6dc730ae6e2238fae304e66760daaac2390553
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 11:34:53 -0400

    "rm: be super/few_part/next_number.py, super/big_life/try_government/good_thing_and_child/large_government_and_great_time/new_number/other_person.py or super/few_woman/public_point.py about young man"

---
+++ super/big_life/try_government/good_thing_and_child/large_government_and_great_time/new_number/other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_person_or_part(str_arg):
+    great_year(str_arg)
+    print('part')
+
+def great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_person_or_part('able_number')
---
+++ super/few_part/next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    bad_life(str_arg)
+    print('old_eye')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('give_different_year_in_last_woman')
