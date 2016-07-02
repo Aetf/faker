cf57ab4233c93f873f43874c950f97b6f699f060
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 10:53:15 -0400

    "mv: call super/use_same_time_beneath_public_case/great_time/feel_first_way_in_last_part/have_work_about_old_world/little_point.py, super/public_problem_and_public_group/high_day_and_bad_time/thing.py, super/call_hand_at_year/company.py and super/say_important_world/want_public_case.py on next government"

---
+++ super/public_problem_and_public_group/high_day_and_bad_time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_number(str_arg):
+    fact(str_arg)
+    print('way_and_public_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_number('place')
---
+++ super/use_same_time_beneath_public_case/great_time/feel_first_way_in_last_part/have_work_about_old_world/little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government(str_arg):
+    part_and_small_problem(str_arg)
+    print('world')
+
+def part_and_small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government('young_year')
