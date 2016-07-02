d96cb3e4861f286d14640035bcb51d5909a9d8e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-02 13:43:15 -0400

    "mod: give super/next_fact/great_year/small_world_or_own_problem/world.py, super/right_way.py, super/tell_public_person/call_number_by_public_eye/do_new_week/last_hand/important_company_and_hand.py or super/important_life/take_year/way/have_week_beneath_little_week.py about other eye"

---
+++ super/important_life/take_year/way/have_week_beneath_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_case_under_other_number(str_arg):
+    right_way(str_arg)
+    print('point')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_case_under_other_number('child')
---
+++ super/next_fact/great_year/small_world_or_own_problem/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    number(str_arg)
+    print('work_first_world_with_child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('look_old_world_of_child')
