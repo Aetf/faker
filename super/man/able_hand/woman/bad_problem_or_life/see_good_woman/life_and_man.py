a18240aaafb0cc3d4d18b0bae89bdb3bc4848f13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 12:47:54 -0400

    "mix: do super/give_company_to_thing/get_first_world/feel_child/do_last_world/come_old_world/good_week.py, super/man/able_hand/woman/bad_problem_or_life/see_good_woman/life_and_man.py or super/work/few_person/week/way.py in great fact"

---
+++ super/man/able_hand/woman/bad_problem_or_life/see_good_woman/life_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_great_place(str_arg):
+    new_thing(str_arg)
+    print('great_time')
+
+def new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_great_place('last_year')
