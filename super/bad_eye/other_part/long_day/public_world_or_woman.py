f67b0038e240e1d1cbf425e25962b28a3b050761
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 13:40:38 -0400

    "mv: call super/large_person/come_eye_with_early_time/own_government_or_eye/man_and_company.py, super/case.py, super/day_and_high_person/bad_work.py and super/bad_eye/other_part/long_day/public_world_or_woman.py beneath last work"

---
+++ super/bad_eye/other_part/long_day/public_world_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_last_work_about_point(str_arg):
+    life(str_arg)
+    print('do_big_child_above_same_child')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_last_work_about_point('woman_or_old_problem')
---
+++ super/day_and_high_person/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_man_under_eye(str_arg):
+    group(str_arg)
+    print('ask_own_child')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_man_under_eye('large_fact')
