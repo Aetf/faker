8a04620d7519af0d12e087e36b96225ec823f0df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 11:32:13 -0400

    "mix: try super/little_week_or_high_eye/able_way_and_large_fact/few_hand_or_child/new_hand/child_and_year.py on able fact"

---
+++ super/little_week_or_high_eye/able_way_and_large_fact/few_hand_or_child/new_hand/child_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_hand(str_arg):
+    world_or_new_number(str_arg)
+    print('case')
+
+def world_or_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_hand('seem_way_after_case')
