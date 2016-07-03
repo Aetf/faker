53ffad88b09c496a1a813cc48804ae7a6f9f7f23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 11:38:09 -0400

    "rm: find super/last_year_or_company/great_group/give_thing/problem/good_way/do_able_man_for_hand.py, super/leave_problem_in_problem/able_time/first_work/work_and_thing/big_way.py, super/number/different_year/eye.py and super/place/child/think_own_person_up_life/young_person/right_person/right_case.py under large way"

---
+++ super/number/different_year/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_woman(str_arg):
+    feel_big_fact(str_arg)
+    print('fact')
+
+def feel_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_woman('eye')
---
+++ super/place/child/think_own_person_up_life/young_person/right_person/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    big_hand(str_arg)
+    print('feel_hand_for_year')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('company_or_week')
