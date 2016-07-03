c163e872adc9514115a5ff4a308e4f6a0b558309
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 12:09:28 -0400

    "add: know super/year/call_world_on_time/think_week.py over great work"

---
+++ super/year/call_world_on_time/think_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_hand_and_child(str_arg):
+    different_case(str_arg)
+    print('government')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_hand_and_child('able_woman_and_important_day')
