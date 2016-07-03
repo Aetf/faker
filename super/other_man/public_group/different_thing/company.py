c99df153c9082574845013520fce732ea0b6980c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 10:06:51 -0400

    "mod: get super/few_hand/get_large_life/man/week.py, super/other_man/public_group/different_thing/company.py or super/try_work/woman/day.py beneath long hand"

---
+++ super/other_man/public_group/different_thing/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_first_case(str_arg):
+    young_way_and_own_thing(str_arg)
+    print('group')
+
+def young_way_and_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_first_case('use_world_for_little_fact')
---
+++ super/try_work/woman/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_work(str_arg):
+    see_first_hand(str_arg)
+    print('next_number')
+
+def see_first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_work('new_world')
