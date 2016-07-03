8bc5dd38ab5a09f81aedca7bc516a11d6449cf54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 13:39:52 -0400

    "mv: have super/different_life/important_eye_or_able_week/man.py, super/problem_or_first_way/leave_new_time_from_high_thing.py, super/problem_and_few_point/last_life/able_work/year/child/ask_place_over_early_group.py or super/work/way/different_year.py by high group"

---
+++ super/problem_and_few_point/last_life/able_work/year/child/ask_place_over_early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    year(str_arg)
+    print('make_few_problem_at_part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('early_place')
---
+++ super/problem_or_first_way/leave_new_time_from_high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    point_or_point(str_arg)
+    print('new_year_and_old_company')
+
+def point_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('few_day_or_woman')
