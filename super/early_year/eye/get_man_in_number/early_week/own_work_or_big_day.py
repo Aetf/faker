654501c7e40bea66b198c87ddedcf5b78221247b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 14:35:05 -0400

    "add: use super/way/woman/public_eye.py, super/early_year/eye/get_man_in_number/early_week/own_work_or_big_day.py and super/work_or_other_place/think_part_at_eye/get_few_group.py with same case"

---
+++ super/early_year/eye/get_man_in_number/early_week/own_work_or_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_world(str_arg):
+    first_world_or_fact(str_arg)
+    print('part_or_small_place')
+
+def first_world_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_world('year')
---
+++ super/work_or_other_place/think_part_at_eye/get_few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_government(str_arg):
+    great_point(str_arg)
+    print('young_part')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_government('high_group')
