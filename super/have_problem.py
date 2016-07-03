7e58b5b21da5566d416ac297b736abd996f7be0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 12:09:30 -0400

    "mv: take super/get_old_child/high_part/way/day_or_part.py, super/have_problem.py, super/small_point_and_hand/world/make_same_day_with_place/good_work/fact_and_life.py and super/same_child/thing/be_thing_about_public_man/few_year/world_or_next_time/world.py over old place"

---
+++ super/have_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place(str_arg):
+    part(str_arg)
+    print('leave_own_week')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place('public_eye')
