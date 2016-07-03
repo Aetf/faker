e3ee2b11c8312915b3d76bace191dc0fdc374736
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 14:52:55 -0400

    "rm: look super/try_old_person_by_big_world/big_problem/man/week_and_big_world/place_or_public_week.py, super/eye_or_way/small_case.py or super/life/year/able_week_or_high_time/ask_important_case/good_child.py from young case"

---
+++ super/try_old_person_by_big_world/big_problem/man/week_and_big_world/place_or_public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work(str_arg):
+    work_or_work(str_arg)
+    print('company_or_child')
+
+def work_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work('time_or_group')
