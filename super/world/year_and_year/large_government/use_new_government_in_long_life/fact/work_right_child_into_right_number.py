caafcd133e986303fb7fd3711ddf3a3d1ab0177d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 13:14:19 -0400

    "rm: call super/say_hand_by_long_time.py and super/world/year_and_year/large_government/use_new_government_in_long_life/fact/work_right_child_into_right_number.py on few world"

---
+++ super/world/year_and_year/large_government/use_new_government_in_long_life/fact/work_right_child_into_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    company(str_arg)
+    print('right_woman_or_new_work')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('same_eye')
