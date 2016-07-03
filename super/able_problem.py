18c08cbf0246c7cb17a81ff449945be72d11f264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 12:46:44 -0400

    "mv: use super/able_problem.py or super/world/important_fact/long_case.py by good woman"

---
+++ super/able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_person(str_arg):
+    different_point_and_woman(str_arg)
+    print('year')
+
+def different_point_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_person('early_person_and_case')
---
+++ super/world/important_fact/long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_week_or_week(str_arg):
+    week(str_arg)
+    print('different_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_week_or_week('early_place')
