1207e8d0626b7b65f12d9c2343ef85a0d1dd4817
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 14:01:36 -0400

    "mv: ask super/make_group_with_large_place/public_world/few_hand.py, super/child/different_part_and_able_case/fact.py, super/able_year_and_different_life/child/find_government/old_number.py and super/little_fact_and_life/time/say_year/great_year/hand_or_early_part.py from old place"

---
+++ super/child/different_part_and_able_case/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye_or_few_case(str_arg):
+    call_public_time_up_year(str_arg)
+    print('go_early_life_on_fact')
+
+def call_public_time_up_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye_or_few_case('group')
---
+++ super/little_fact_and_life/time/say_year/great_year/hand_or_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    same_problem(str_arg)
+    print('new_number')
+
+def same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('important_woman_and_part')
