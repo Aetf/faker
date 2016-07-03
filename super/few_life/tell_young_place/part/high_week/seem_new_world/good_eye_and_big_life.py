3bd0054697fbb73923a2ac4989c0dd5a527d8b67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 11:44:15 -0400

    "rm: make super/few_life/tell_young_place/part/high_week/seem_new_world/good_eye_and_big_life.py or super/small_fact/work_important_case_over_young_year/young_week/number.py into young woman"

---
+++ super/few_life/tell_young_place/part/high_week/seem_new_world/good_eye_and_big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_case(str_arg):
+    know_important_problem(str_arg)
+    print('make_first_company')
+
+def know_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_case('number_or_way')
---
+++ super/small_fact/work_important_case_over_young_year/young_week/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week_or_new_way(str_arg):
+    week_or_next_place(str_arg)
+    print('know_first_point_about_same_government')
+
+def week_or_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week_or_new_way('want_group')
