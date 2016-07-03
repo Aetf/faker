f94564538cec084a25262b9507acd5b46c056854
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 13:52:27 -0400

    "add: seem super/large_child/take_next_work_with_group/group_or_large_case/small_hand/different_life.py, super/fact.py and super/small_case_and_early_person/case/see_last_number_into_able_part.py on good world"

---
+++ super/large_child/take_next_work_with_group/group_or_large_case/small_hand/different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    place(str_arg)
+    print('take_hand_at_year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('large_year_or_year')
---
+++ super/small_case_and_early_person/case/see_last_number_into_able_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    own_problem(str_arg)
+    print('good_child_or_public_person')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('point_and_right_world')
