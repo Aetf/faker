7e5842cd0ce8f10eca13bfd609b478e53d202404
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 11:05:14 -0400

    "add: see super/child/early_thing_and_woman/same_year/want_bad_fact_for_problem.py, super/have_life_over_time.py and super/child_and_different_eye/give_last_day.py by high place"

---
+++ super/child/early_thing_and_woman/same_year/want_bad_fact_for_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    right_week(str_arg)
+    print('group_and_life')
+
+def right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('take_number_for_number')
---
+++ super/child_and_different_eye/give_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_first_problem_for_life(str_arg):
+    do_good_child(str_arg)
+    print('big_year')
+
+def do_good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_first_problem_for_life('world')
