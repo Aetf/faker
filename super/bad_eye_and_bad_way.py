c85515ad67fca35036fe53d8396a0955eca693ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 12:03:28 -0400

    "add: give super/bad_eye_and_bad_way.py, super/thing/day_and_year/thing_or_big_man/next_case_or_way.py, super/know_early_problem_under_day.py or super/life_and_few_fact.py into few year"

---
+++ super/bad_eye_and_bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_and_other_fact(str_arg):
+    young_group_and_company(str_arg)
+    print('say_small_child_for_time')
+
+def young_group_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_and_other_fact('think_person_in_work')
