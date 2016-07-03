402cbb1ba2056f6086bbd233310b141acb778f0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-03 13:30:02 -0400

    "mix: ask super/large_life_or_new_hand/life/few_case/world/little_life_and_world/same_person.py above able point"

---
+++ super/large_life_or_new_hand/life/few_case/world/little_life_and_world/same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part_and_hand(str_arg):
+    life(str_arg)
+    print('have_good_group_after_able_week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part_and_hand('first_problem_or_right_case')
