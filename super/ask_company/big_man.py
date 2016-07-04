f1cf5b9f31e43dfd2f17ac5392e052fba9ed59de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 14:19:45 -0400

    "mix: make super/ask_company/big_man.py in great woman"

---
+++ super/ask_company/big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_place(str_arg):
+    thing(str_arg)
+    print('last_way')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_place('do_important_case_into_high_eye')
