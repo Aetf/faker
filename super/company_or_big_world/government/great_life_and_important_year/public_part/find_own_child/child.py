e89bfd57b74fa681c49f9e1f0ef8bc9e028deda4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 12:13:56 -0400

    "mod: use super/company_or_big_world/government/great_life_and_important_year/public_part/find_own_child/child.py for new company"

---
+++ super/company_or_big_world/government/great_life_and_important_year/public_part/find_own_child/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_child(str_arg):
+    problem(str_arg)
+    print('old_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_child('go_group')
