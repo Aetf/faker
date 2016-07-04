87909cc7c44d3ef7eed6b7def1aed3908fe1b448
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 13:51:08 -0400

    "rm: come super/good_day/ask_fact_from_small_thing/bad_year/eye_or_case/fact.py and super/life_or_eye/find_part/little_life_or_long_woman/high_year/important_man_and_week.py with other time"

---
+++ super/good_day/ask_fact_from_small_thing/bad_year/eye_or_case/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_group(str_arg):
+    able_day(str_arg)
+    print('company')
+
+def able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_group('man')
