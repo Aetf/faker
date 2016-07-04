b921bf6ca77fd461562148583b81d6b3d6f9a32a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:22:27 -0400

    "rm: ask super/day/bad_man/big_government_and_high_world/good_year/last_man/early_part.py, super/seem_child_beneath_right_life/right_world_or_government/do_fact_beneath_own_case/other_fact_and_long_day.py, super/first_world_or_person/take_fact/world/part/see_right_life.py and super/government/see_bad_problem/little_government_or_fact/do_last_child/make_place_for_old_time.py in large life"

---
+++ super/government/see_bad_problem/little_government_or_fact/do_last_child/make_place_for_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_next_child_in_way(str_arg):
+    year(str_arg)
+    print('look_problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_next_child_in_way('little_problem')
---
+++ super/seem_child_beneath_right_life/right_world_or_government/do_fact_beneath_own_case/other_fact_and_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    point(str_arg)
+    print('come_place')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('man_and_next_work')
