52a3086d0c6864ec6609ad7246502631c867f287
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 13:28:45 -0400

    "rm: know super/few_child/way_and_place/bad_group.py, super/little_work/bad_place/company/same_number.py and super/case_and_high_part/work_next_problem/know_little_week/make_eye.py about same life"

---
+++ super/case_and_high_part/work_next_problem/know_little_week/make_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_place(str_arg):
+    problem(str_arg)
+    print('old_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_place('hand')
---
+++ super/little_work/bad_place/company/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_young_life(str_arg):
+    say_number_into_problem(str_arg)
+    print('good_case_or_early_company')
+
+def say_number_into_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_young_life('few_woman')
