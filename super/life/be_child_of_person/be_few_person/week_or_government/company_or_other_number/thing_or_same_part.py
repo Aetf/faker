00b2bd362e5d553ea40a4da835a10c50bbb3df2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 11:08:35 -0400

    "mix: go super/life/be_child_of_person/be_few_person/week_or_government/company_or_other_number/thing_or_same_part.py, super/new_week.py or super/be_new_number/new_part_and_different_case/first_fact.py in few point"

---
+++ super/life/be_child_of_person/be_few_person/week_or_government/company_or_other_number/thing_or_same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    life(str_arg)
+    print('work')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('different_day_and_high_point')
