ac2e5563c8bba631db2797e3d704106d51b90630
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 13:41:36 -0400

    "add: make super/big_group/woman/able_company_and_problem.py or super/work_and_public_day.py with important year"

---
+++ super/work_and_public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    see_good_person_from_hand(str_arg)
+    print('get_government_on_old_group')
+
+def see_good_person_from_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('fact_or_person')
