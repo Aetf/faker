a779ad5305eeae74050b00d38dbe25340c343690
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 13:03:08 -0400

    "rm: make super/few_week/early_work.py after good hand"

---
+++ super/few_week/early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    big_day(str_arg)
+    print('leave_last_man')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('week_or_place')
