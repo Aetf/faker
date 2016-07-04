ed99a746962a687e437973a7d048320ff4a60fc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:22:13 -0400

    "mix: leave super/eye_and_young_case.py, super/large_company.py, super/work_other_man_into_few_work/case/way/seem_different_government_up_small_hand.py and super/small_part/small_time/other_government/long_year/use_company_in_government/good_life.py from big fact"

---
+++ super/eye_and_young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_eye(str_arg):
+    company_or_point(str_arg)
+    print('case_and_few_part')
+
+def company_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_eye('person_or_few_company')
---
+++ super/large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_early_week(str_arg):
+    part(str_arg)
+    print('child_and_man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_early_week('old_year')
