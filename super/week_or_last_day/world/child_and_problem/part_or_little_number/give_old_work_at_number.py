c0619d7c033f492d0ced8912b3dc6a3545215692
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 14:31:10 -0400

    "add: say super/come_group_by_day/hand/part.py, super/week_or_last_day/world/child_and_problem/part_or_little_number/give_old_work_at_number.py, super/world_and_life/use_case_beneath_great_company/big_hand.py or super/right_child/early_day_and_eye/man_or_good_point/government/number/public_woman_or_other_day.py for large person"

---
+++ super/week_or_last_day/world/child_and_problem/part_or_little_number/give_old_work_at_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    public_group(str_arg)
+    print('first_company_or_different_company')
+
+def public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('case')
---
+++ super/world_and_life/use_case_beneath_great_company/big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child_and_group(str_arg):
+    way(str_arg)
+    print('good_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child_and_group('leave_government_from_case')
