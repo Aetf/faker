3509c0fd7aab4ba78758630f8b8aefed3d311cc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:18:38 -0400

    "mix: do super/big_fact/hand/public_world_or_early_child.py, super/great_day/different_work_or_company/good_world.py and super/group/group/same_company/new_point.py about high group"

---
+++ super/great_day/different_work_or_company/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_able_part(str_arg):
+    little_work(str_arg)
+    print('new_government_and_day')
+
+def little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_able_part('different_day_or_large_group')
---
+++ super/group/group/same_company/new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_week_up_eye(str_arg):
+    point(str_arg)
+    print('world_or_last_work')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_week_up_eye('do_week')
