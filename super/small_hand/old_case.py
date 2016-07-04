f3b5961f6a0353654ac194f3fdd7c35886e885b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 14:23:00 -0400

    "mod: try super/small_hand/old_case.py with other fact"

---
+++ super/small_hand/old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    tell_good_case_beneath_long_number(str_arg)
+    print('hand_and_work')
+
+def tell_good_case_beneath_long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('small_group')
