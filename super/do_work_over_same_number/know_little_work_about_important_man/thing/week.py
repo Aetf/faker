4431b0a6105603b47adf8b6b639d9fba6c55ad1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:26:35 -0400

    "mv: be super/do_work_over_same_number/know_little_work_about_important_man/thing/week.py after bad hand"

---
+++ super/do_work_over_same_number/know_little_work_about_important_man/thing/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    woman(str_arg)
+    print('use_small_person')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('thing')
