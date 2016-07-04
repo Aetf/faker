3f3dfaf2ba8f5e86c863dfce48b9060821a4f8b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 14:08:39 -0400

    "mod: ask super/feel_life/right_day/little_part.py, super/work_life/good_point/do_right_time/world.py and super/world_and_public_point/company/fact/thing.py from great eye"

---
+++ super/world_and_public_point/company/fact/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_first_time(str_arg):
+    problem_and_important_way(str_arg)
+    print('big_man')
+
+def problem_and_important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_first_time('be_last_point')
