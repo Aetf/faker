91a95d3ca434b467caa54a8c1dd3cec4c0552d7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:38:38 -0400

    "mv: take super/other_company/own_time_and_life/life/part.py, super/bad_government/person.py, super/leave_public_thing_after_place.py or super/different_part/government_or_early_week/government_or_large_eye/important_man_or_able_point/thing.py in last government"

---
+++ super/leave_public_thing_after_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    eye(str_arg)
+    print('week')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('large_time')
