d5972692035a6cf4d7617fd318313e84248189dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 10:31:17 -0400

    "add: seem super/leave_first_time/world/time/first_life_and_public_part/last_person.py into different week"

---
+++ super/leave_first_time/world/time/first_life_and_public_part/last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way(str_arg):
+    leave_few_problem(str_arg)
+    print('child_and_big_woman')
+
+def leave_few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way('work')
