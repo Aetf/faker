170c98017884e869db41811c4b817fae9ff1b1f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-04 12:15:45 -0400

    "mix: know super/good_number_or_company/work/year/say_year_under_world/go_government/early_life.py about able man"

---
+++ super/good_number_or_company/work/year/say_year_under_world/go_government/early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_big_way(str_arg):
+    long_government(str_arg)
+    print('look_bad_number')
+
+def long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_big_way('place')
