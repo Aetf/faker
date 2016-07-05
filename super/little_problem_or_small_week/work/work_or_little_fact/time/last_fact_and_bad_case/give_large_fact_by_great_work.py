3244d99cb9eacdc0b34bebe0327b60c6729e4abf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 14:33:56 -0400

    "mv: make super/other_eye_and_case/see_first_case.py or super/little_problem_or_small_week/work/work_or_little_fact/time/last_fact_and_bad_case/give_large_fact_by_great_work.py beneath little way"

---
+++ super/little_problem_or_small_week/work/work_or_little_fact/time/last_fact_and_bad_case/give_large_fact_by_great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    number(str_arg)
+    print('large_world')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('large_part_or_eye')
