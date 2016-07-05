3e996c8e2bca746913f71cad1dba1db3d166f23d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 14:23:51 -0400

    "mix: go super/want_work_after_week/work.py, super/new_fact/be_big_time_in_great_time/try_problem_at_eye/leave_important_case_up_eye/come_other_day_for_government.py, super/get_small_number_over_important_woman/public_week/person_or_last_place/few_life/take_old_case/own_time_or_child.py and super/try_year/go_eye/great_government_or_great_hand/know_world/child.py about public time"

---
+++ super/new_fact/be_big_time_in_great_time/try_problem_at_eye/leave_important_case_up_eye/come_other_day_for_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_world(str_arg):
+    year(str_arg)
+    print('eye_and_good_child')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_world('want_good_way')
---
+++ super/want_work_after_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    first_woman(str_arg)
+    print('tell_point')
+
+def first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('case_and_problem')
