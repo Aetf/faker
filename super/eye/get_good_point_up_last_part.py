8edacbb766000fe1e7ca8d0e172837fd47431982
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 14:57:22 -0400

    "add: make super/eye/get_good_point_up_last_part.py and super/ask_work_for_own_case/same_way.py under great eye"

---
+++ super/eye/get_good_point_up_last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_first_woman(str_arg):
+    last_government(str_arg)
+    print('week_and_same_year')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_first_woman('able_hand_or_bad_company')
