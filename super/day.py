d0f172e10cbe8f95d8444a3af71498359dd3d197
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 10:53:07 -0400

    "mix: use super/day.py and super/want_life/do_old_time/place/place/last_life.py up big case"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye(str_arg):
+    same_day(str_arg)
+    print('new_woman')
+
+def same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye('see_great_week_with_problem')
