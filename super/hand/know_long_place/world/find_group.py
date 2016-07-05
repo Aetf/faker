390e0213f4978fe7844b79d1180f8119646cc443
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 10:11:52 -0400

    "mv: tell super/hand/know_long_place/world/find_group.py and super/man/large_hand_and_group/be_number_in_few_hand.py on old week"

---
+++ super/hand/know_long_place/world/find_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_hand(str_arg):
+    year(str_arg)
+    print('able_work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_hand('old_time')
