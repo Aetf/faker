5ac0c7d10e8b1709b7dcaca42ca52736349aefe2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 13:06:08 -0400

    "rm: look super/own_group_or_eye/company/take_person_on_way/high_year.py under public man"

---
+++ super/own_group_or_eye/company/take_person_on_way/high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way(str_arg):
+    place_or_year(str_arg)
+    print('young_day')
+
+def place_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way('small_man')
