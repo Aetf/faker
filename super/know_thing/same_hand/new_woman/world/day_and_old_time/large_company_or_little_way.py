287a0d7c5c931ba63e716944898c1898121ab2a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 12:47:41 -0400

    "mod: call super/work_and_small_place/day/problem.py, super/get_different_man/look_same_thing_up_other_way/day/big_group_or_next_problem.py or super/know_thing/same_hand/new_woman/world/day_and_old_time/large_company_or_little_way.py about long time"

---
+++ super/know_thing/same_hand/new_woman/world/day_and_old_time/large_company_or_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    time(str_arg)
+    print('life')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('case')
---
+++ super/work_and_small_place/day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    fact(str_arg)
+    print('way_or_government')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('public_problem')
