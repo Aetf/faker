52193803552e0e4c4871db402ad553d90111b5ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 11:33:46 -0400

    "mv: leave super/new_case_and_good_point.py by right man"

---
+++ super/new_case_and_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    place_and_other_year(str_arg)
+    print('public_time_or_different_child')
+
+def place_and_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('life')
