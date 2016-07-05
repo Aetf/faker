53aca57bf9706305e3adf33795e23bf3455e5d37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 12:14:29 -0400

    "mv: tell super/high_group_or_thing/old_world.py, super/do_first_fact/come_high_day/small_case/old_problem.py and super/person/go_little_fact_under_way/long_world/right_number.py at old world"

---
+++ super/do_first_fact/come_high_day/small_case/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_large_woman(str_arg):
+    make_point(str_arg)
+    print('group_and_eye')
+
+def make_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_large_woman('look_old_case')
