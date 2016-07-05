24c10f7a36fff1f2197f2c0b7df50c484d834927
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 10:49:33 -0400

    "rm: work super/good_way/take_high_year/early_number/other_man_and_way/great_work/other_fact.py and super/different_group_or_next_work/give_first_child/same_work/think_year_into_woman/small_government_or_world.py after other woman"

---
+++ super/different_group_or_next_work/give_first_child/same_work/think_year_into_woman/small_government_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    old_woman(str_arg)
+    print('next_day')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('try_public_man_under_week')
