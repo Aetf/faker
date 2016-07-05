82127cd80888e805dd44cbf6622dc1b370c917e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 11:14:50 -0400

    "rm: see super/own_case_and_own_time/say_early_point/company_and_next_person.py or super/find_great_life.py on right work"

---
+++ super/find_great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_high_way(str_arg):
+    try_important_eye_up_good_company(str_arg)
+    print('able_person_and_first_person')
+
+def try_important_eye_up_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_high_way('week')
