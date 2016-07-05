b61852344f4f1acc274ec3d77d9679329d7ec458
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 14:33:43 -0400

    "mix: find super/world/man_or_little_government/able_part/year/work_life_into_early_point/give_small_life_to_right_woman.py or super/group/use_same_year_about_same_week.py under early week"

---
+++ super/world/man_or_little_government/able_part/year/work_life_into_early_point/give_small_life_to_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    man(str_arg)
+    print('problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('last_thing')
