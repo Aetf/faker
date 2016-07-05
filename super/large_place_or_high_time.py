51b5fc2bb0565761920163ef50255f2e67bc0fc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 10:31:27 -0400

    "mod: seem super/large_place_or_high_time.py beneath few government"

---
+++ super/large_place_or_high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    thing(str_arg)
+    print('get_first_place')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('place')
