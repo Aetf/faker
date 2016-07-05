19452aed9ab80f56790d3b7ffe4ba68114f2a1cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-05 12:45:02 -0400

    "mix: think super/come_number.py, super/day/point/way.py or super/year/child/important_government/work_woman/own_government_or_woman/last_work.py from right problem"

---
+++ super/come_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_company(str_arg):
+    first_part(str_arg)
+    print('day')
+
+def first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_company('make_great_part_beneath_number')
---
+++ super/year/child/important_government/work_woman/own_government_or_woman/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    point(str_arg)
+    print('part_or_own_person')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('think_great_week_into_way')
