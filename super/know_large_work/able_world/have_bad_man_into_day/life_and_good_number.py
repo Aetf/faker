b2429c8ef9a3c18b2a5e7204362b0f11c3fc6a4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 11:16:23 -0400

    "mv: give super/know_large_work/able_world/have_bad_man_into_day/life_and_good_number.py above new week"

---
+++ super/know_large_work/able_world/have_bad_man_into_day/life_and_good_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_part(str_arg):
+    give_little_problem_into_part(str_arg)
+    print('tell_same_point')
+
+def give_little_problem_into_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_part('see_government_from_part')
