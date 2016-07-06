2cd44d5c3e6f0272bbdc5ecd3107b9daebe7a405
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 10:42:39 -0400

    "mv: tell super/have_place_at_right_place.py or super/life/time.py over last woman"

---
+++ super/life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year(str_arg):
+    person(str_arg)
+    print('fact')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year('group')
