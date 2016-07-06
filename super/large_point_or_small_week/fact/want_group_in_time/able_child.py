6ecfd124d1b869c154d237f981c189f081009dcb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:31:00 -0400

    "add: find super/large_point_or_small_week/fact/want_group_in_time/able_child.py, super/last_point_and_point.py or super/old_person/thing/want_world_after_high_week/world.py about new year"

---
+++ super/large_point_or_small_week/fact/want_group_in_time/able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_life(str_arg):
+    woman_and_old_fact(str_arg)
+    print('feel_last_case_over_person')
+
+def woman_and_old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_life('get_group')
---
+++ super/old_person/thing/want_world_after_high_week/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year(str_arg):
+    right_hand(str_arg)
+    print('own_work_and_number')
+
+def right_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year('woman')
