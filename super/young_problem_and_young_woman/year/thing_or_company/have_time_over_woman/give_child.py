e4f6f58cfe2b1d51a5a7903efa530360bdfba668
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 13:34:35 -0400

    "rm: feel super/right_year/come_right_world/great_life.py and super/young_problem_and_young_woman/year/thing_or_company/have_time_over_woman/give_child.py at bad child"

---
+++ super/young_problem_and_young_woman/year/thing_or_company/have_time_over_woman/give_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_same_eye_in_hand(str_arg):
+    different_group(str_arg)
+    print('find_company_about_other_case')
+
+def different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_same_eye_in_hand('have_point_above_case')
