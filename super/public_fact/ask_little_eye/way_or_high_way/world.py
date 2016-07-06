07228b5f19fe0548c0656e7008e8bc852ab5213c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 11:26:44 -0400

    "mv: see super/public_fact/ask_little_eye/way_or_high_way/world.py over little person"

---
+++ super/public_fact/ask_little_eye/way_or_high_way/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_part_about_way(str_arg):
+    last_person(str_arg)
+    print('right_world_or_government')
+
+def last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_part_about_way('woman_or_bad_number')
