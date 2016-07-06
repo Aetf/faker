95c7e9b3f79cdb17a3a08aaa581e3955ace902ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 14:43:30 -0400

    "mix: be super/leave_point/large_year/long_government_and_other_case/high_day/thing.py, super/problem/find_early_number_of_big_life/different_group_and_important_way/case/tell_early_eye.py and super/problem_or_eye/group.py with last way"

---
+++ super/problem/find_early_number_of_big_life/different_group_and_important_way/case/tell_early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world(str_arg):
+    right_number(str_arg)
+    print('small_place')
+
+def right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world('young_time')
