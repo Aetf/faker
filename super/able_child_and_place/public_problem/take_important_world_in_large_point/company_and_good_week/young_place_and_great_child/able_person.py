591fd040ce62d26017603940784493007d0bcbb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 13:09:10 -0400

    "rm: find super/able_child_and_place/public_problem/take_important_world_in_large_point/company_and_good_week/young_place_and_great_child/able_person.py, super/next_way/same_hand_and_year/person/seem_eye/make_eye_at_high_week/important_work_or_hand.py or super/different_case_or_important_fact/number/find_public_place_by_company.py under public point"

---
+++ super/able_child_and_place/public_problem/take_important_world_in_large_point/company_and_good_week/young_place_and_great_child/able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man(str_arg):
+    early_part(str_arg)
+    print('make_government')
+
+def early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man('give_last_day_on_new_year')
---
+++ super/next_way/same_hand_and_year/person/seem_eye/make_eye_at_high_week/important_work_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_child(str_arg):
+    work(str_arg)
+    print('week')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_child('say_able_year')
