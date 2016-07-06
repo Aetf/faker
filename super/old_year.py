66b85162268d79b3f213337f654917c00deb1f45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:43:40 -0400

    "mod: leave super/old_year.py beneath small man"

---
+++ super/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group(str_arg):
+    life(str_arg)
+    print('try_world_above_first_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group('world')
