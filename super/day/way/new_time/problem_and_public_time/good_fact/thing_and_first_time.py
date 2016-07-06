eb67123cd70291a283716f65cdb86e3c12960d43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 13:12:26 -0400

    "add: use super/day/way/new_time/problem_and_public_time/good_fact/thing_and_first_time.py beneath high government"

---
+++ super/day/way/new_time/problem_and_public_time/good_fact/thing_and_first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_own_problem(str_arg):
+    point(str_arg)
+    print('eye_and_man')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_own_problem('world')
