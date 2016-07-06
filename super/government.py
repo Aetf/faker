8175681919e53ae6de1776ed20f0d0ee40712a2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:42:45 -0400

    "mix: have super/number/right_day/young_life/woman.py, super/work.py or super/government.py about large day"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman_or_company(str_arg):
+    able_point(str_arg)
+    print('government_and_life')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman_or_company('year')
---
+++ super/number/right_day/young_life/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    find_time(str_arg)
+    print('man_and_last_problem')
+
+def find_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('feel_good_year_beneath_bad_government')
