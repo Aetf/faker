2896ed5fa20686d13ef66035380a8c18140fc74f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 11:53:34 -0400

    "mv: feel super/give_number/give_work.py, super/case/case/next_part_and_long_company/world/fact.py and super/thing_and_different_eye/able_man/point_and_new_company/get_great_number.py on able time"

---
+++ super/case/case/next_part_and_long_company/world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point(str_arg):
+    government(str_arg)
+    print('good_government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point('get_great_part')
---
+++ super/give_number/give_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day(str_arg):
+    life(str_arg)
+    print('come_high_world_at_public_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day('important_life')
