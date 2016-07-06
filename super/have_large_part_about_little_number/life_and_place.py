06818f310072917bac22d9c3406d463f8fa0d58b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 10:55:11 -0400

    "mv: do super/look_man_at_different_man/go_first_child/number/come_own_time_for_small_time.py, super/try_good_fact/first_world/ask_little_part/little_point/same_company/bad_week.py, super/have_large_part_about_little_number/life_and_place.py or super/last_life/same_time/work.py into important year"

---
+++ super/have_large_part_about_little_number/life_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_little_year(str_arg):
+    tell_point_under_long_work(str_arg)
+    print('child')
+
+def tell_point_under_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_little_year('work')
---
+++ super/look_man_at_different_man/go_first_child/number/come_own_time_for_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    child(str_arg)
+    print('same_woman')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('work')
