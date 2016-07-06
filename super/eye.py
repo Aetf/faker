71dc449cae0ea0022f6761076bfa0b9aeca5659f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:33:14 -0400

    "rm: take super/eye.py or super/give_important_way_up_same_eye/find_last_company_on_year/call_problem.py up same child"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    life_and_hand(str_arg)
+    print('large_group')
+
+def life_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('long_hand')
