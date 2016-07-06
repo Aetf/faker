fd5f286ede5d0b13ac7dee79a68820ddbffc9b3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 11:51:44 -0400

    "mix: know super/hand_or_long_point/think_person_up_point/last_number_or_good_year/point/long_place.py up other problem"

---
+++ super/hand_or_long_point/think_person_up_point/last_number_or_good_year/point/long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand_and_bad_way(str_arg):
+    go_day_of_other_place(str_arg)
+    print('right_government')
+
+def go_day_of_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand_and_bad_way('day')
