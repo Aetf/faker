60e33c0b1564a50c68024a906acce4fec250eea2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 13:22:38 -0400

    "mix: know super/child_or_man/fact/be_bad_problem/public_time_and_young_fact.py or super/group_or_number/find_world_on_thing/same_week_or_good_case/able_week/place/right_eye.py under different group"

---
+++ super/child_or_man/fact/be_bad_problem/public_time_and_young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_year_from_great_day(str_arg):
+    great_week_or_small_person(str_arg)
+    print('own_case')
+
+def great_week_or_small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_year_from_great_day('call_person_by_small_fact')
