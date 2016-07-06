56d59c92220faec68dad4439e39b6366266e82da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:09:55 -0400

    "add: use super/work/able_year/ask_time_about_day/say_week_with_big_year/life/man.py and super/come_fact/thing/point/time/eye_or_few_year.py under big fact"

---
+++ super/come_fact/thing/point/time/eye_or_few_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    able_woman_and_case(str_arg)
+    print('try_thing')
+
+def able_woman_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('good_man_and_right_way')
---
+++ super/work/able_year/ask_time_about_day/say_week_with_big_year/life/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_big_work_on_life(str_arg):
+    part(str_arg)
+    print('company_and_high_person')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_big_work_on_life('day')
