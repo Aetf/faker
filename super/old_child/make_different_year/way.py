f8cc7a0f8af26a5aa741145ba21704eb7eb2d66b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 14:39:48 -0400

    "mix: leave super/important_place_and_big_woman/time/case/hand/woman_and_man/tell_problem.py, super/year/eye/way_and_own_case/first_person_or_public_government/number.py, super/work/tell_next_child_for_large_part/other_life.py and super/old_child/make_different_year/way.py after own time"

---
+++ super/old_child/make_different_year/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    time(str_arg)
+    print('person_or_own_point')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('high_government_or_week')
