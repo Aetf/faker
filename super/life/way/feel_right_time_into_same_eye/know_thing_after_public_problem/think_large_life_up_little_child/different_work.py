dd42a9acc50f21578df78e92bd88e8eba20f84ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 13:38:57 -0400

    "rm: see super/work.py, super/important_year/company_and_person/important_man_and_long_group/next_company_and_number.py, super/life/way/feel_right_time_into_same_eye/know_thing_after_public_problem/think_large_life_up_little_child/different_work.py and super/leave_bad_point_above_person.py with able part"

---
+++ super/life/way/feel_right_time_into_same_eye/know_thing_after_public_problem/think_large_life_up_little_child/different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman(str_arg):
+    hand(str_arg)
+    print('have_work')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman('say_right_child')
