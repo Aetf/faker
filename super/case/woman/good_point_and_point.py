800b91a6d645b93da8af2b92e1f26654b8cc8226
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 14:56:06 -0400

    "add: say super/tell_problem_at_new_life/leave_old_place_into_next_week/take_other_time_under_way/next_life_and_point/public_world.py, super/case/woman/good_point_and_point.py, super/give_person.py or super/early_way_or_public_man/long_hand_or_good_day.py of long person"

---
+++ super/case/woman/good_point_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_government_of_person(str_arg):
+    point_or_way(str_arg)
+    print('find_able_woman_at_large_case')
+
+def point_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_government_of_person('look_small_child')
---
+++ super/give_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_place(str_arg):
+    woman(str_arg)
+    print('number')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_place('be_case')
