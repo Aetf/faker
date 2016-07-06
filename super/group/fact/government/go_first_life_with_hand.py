cc7e3f6304640bd34e0b0cb110d6300df5d019d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-06 12:17:28 -0400

    "mv: feel super/group/fact/government/go_first_life_with_hand.py to long hand"

---
+++ super/group/fact/government/go_first_life_with_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_first_company(str_arg):
+    point(str_arg)
+    print('long_way_or_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_first_company('other_day_and_young_man')
