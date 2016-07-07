a46b72ea3912b2607aa4b415b27658022ff334c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:52:19 -0400

    "mod: make super/find_same_hand/week_or_company/man.py, super/bad_government_or_government/own_week/leave_child.py and super/time/think_own_man/woman.py above small woman"

---
+++ super/bad_government_or_government/own_week/leave_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    point(str_arg)
+    print('other_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('company_or_able_problem')
---
+++ super/find_same_hand/week_or_company/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_good_part(str_arg):
+    place(str_arg)
+    print('important_way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_good_part('person')
