6314755fe2af08c9e71961112ffcd67dd5654450
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 14:58:10 -0400

    "mod: seem super/long_hand/important_case/work_bad_world/able_eye.py, super/early_company.py and super/person.py about big fact"

---
+++ super/early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day(str_arg):
+    small_part(str_arg)
+    print('young_number')
+
+def small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day('work')
---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_woman(str_arg):
+    year(str_arg)
+    print('own_woman')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_woman('small_hand')
