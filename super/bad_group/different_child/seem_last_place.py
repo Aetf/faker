1366eedf061b325fee5dc71d807816b4a240a4fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:37:23 -0400

    "add: work super/bad_group/different_child/seem_last_place.py, super/day/old_point/long_woman_and_next_government.py, super/bad_time/day/do_public_hand_from_person/go_old_hand/week.py or super/year/feel_company_into_point.py into young part"

---
+++ super/bad_group/different_child/seem_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_important_problem(str_arg):
+    seem_problem(str_arg)
+    print('time_or_group')
+
+def seem_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_important_problem('old_number')
---
+++ super/day/old_point/long_woman_and_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    say_year(str_arg)
+    print('problem')
+
+def say_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('public_fact')
