9bc115215d3f198adfbb8c295e84866222b2c0eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 11:52:28 -0400

    "rm: think super/come_child_up_person/look_new_thing.py up small eye"

---
+++ super/come_child_up_person/look_new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    first_person(str_arg)
+    print('small_year')
+
+def first_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('feel_time_at_other_group')
