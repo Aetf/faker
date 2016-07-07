66f51f57a5d4232ffce9ee56835c154df9dcfa58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 14:30:18 -0400

    "mv: feel super/be_same_place_beneath_long_part/young_eye/say_same_work/high_case/old_way.py or super/different_woman/work_other_way/know_early_eye.py over same number"

---
+++ super/different_woman/work_other_way/know_early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way(str_arg):
+    next_man(str_arg)
+    print('public_work')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way('public_point_and_world')
