04e8398b22530bbab4814aa7a3bf5edd246b6f3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:49:32 -0400

    "mix: seem super/eye/good_case/life.py, super/bad_person/think_person_at_new_time/come_few_thing/first_work_and_eye/bad_person_and_large_world/try_same_time.py and super/other_fact_or_year.py into same part"

---
+++ super/eye/good_case/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life(str_arg):
+    number(str_arg)
+    print('early_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life('get_world')
---
+++ super/other_fact_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    first_part(str_arg)
+    print('problem_or_few_time')
+
+def first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('eye')
