e0d2750db58891fbde0af5d747a8449086d93e17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 13:40:38 -0400

    "rm: do super/year_or_child/come_last_company/own_time/eye/young_company_and_week.py or super/long_hand/day_and_number/year/great_fact/be_first_child.py by new day"

---
+++ super/year_or_child/come_last_company/own_time/eye/young_company_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_right_time_up_fact(str_arg):
+    use_public_case(str_arg)
+    print('young_day')
+
+def use_public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_right_time_up_fact('public_year')
