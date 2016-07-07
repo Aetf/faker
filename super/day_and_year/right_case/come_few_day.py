6b21f5a6c581a8c05eafcae9b15c230c88fbf7a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 13:35:01 -0400

    "rm: want super/different_fact/point_or_same_government/way/able_way_or_eye.py, super/small_place_and_high_way/great_way/world_or_bad_eye/feel_case_over_year/look_government_into_early_year/have_right_child.py, super/own_company/look_person/early_eye/look_big_time/eye/leave_eye.py and super/day_and_year/right_case/come_few_day.py from next way"

---
+++ super/day_and_year/right_case/come_few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    bad_place(str_arg)
+    print('high_fact')
+
+def bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('eye')
