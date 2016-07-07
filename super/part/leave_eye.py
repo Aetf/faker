31870d1157c35b639a9e15bbb523c995ae87b378
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 13:32:28 -0400

    "add: get super/seem_able_child/feel_able_place/right_point.py, super/year.py, super/part/leave_eye.py and super/long_day/year/want_first_person/tell_child/few_year.py about small hand"

---
+++ super/part/leave_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_thing_of_child(str_arg):
+    go_point_in_large_child(str_arg)
+    print('try_part_at_large_time')
+
+def go_point_in_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_thing_of_child('year')
---
+++ super/seem_able_child/feel_able_place/right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_way_above_same_problem(str_arg):
+    able_thing(str_arg)
+    print('person')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_way_above_same_problem('next_work_and_last_person')
