98e9fef2ea42c263d4b84c7ab526f516509b5ce8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:19:09 -0400

    "mix: give super/last_woman/week_or_few_government.py, super/use_day_in_man/be_old_way/woman/same_life.py, super/thing/time/point_and_bad_woman/own_company/find_new_day_above_place.py and super/part/number_or_hand/woman_and_problem/see_same_child_by_long_woman/other_number/little_week.py with early case"

---
+++ super/use_day_in_man/be_old_way/woman/same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    tell_right_case(str_arg)
+    print('first_life')
+
+def tell_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('new_week_and_company')
