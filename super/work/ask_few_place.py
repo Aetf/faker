ea8bbd4ab4b5c96e63a5ef3ce18b7e7eebd99d23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 11:09:18 -0400

    "rm: be super/part/same_government/government_or_good_company/little_case/group/hand_and_early_person.py or super/work/ask_few_place.py of big man"

---
+++ super/work/ask_few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_world(str_arg):
+    try_group_from_world(str_arg)
+    print('tell_man_to_way')
+
+def try_group_from_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_world('feel_woman')
