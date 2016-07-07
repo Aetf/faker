b01cb3cb3e296e5a653d235b41491209b7387026
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 10:06:10 -0400

    "mix: find super/good_day/large_day_or_right_government/first_year/problem_or_different_week/good_life.py or super/government/next_way/company/man_or_next_world/work_or_early_man/own_thing.py to right way"

---
+++ super/good_day/large_day_or_right_government/first_year/problem_or_different_week/good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_eye(str_arg):
+    do_person(str_arg)
+    print('year')
+
+def do_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_eye('next_hand')
---
+++ super/government/next_way/company/man_or_next_world/work_or_early_man/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case_or_young_work(str_arg):
+    thing(str_arg)
+    print('leave_point')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case_or_young_work('point_and_problem')
