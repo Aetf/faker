feb1e351fc2e646a379ff5ca3db5bd2515200636
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:54:11 -0400

    "mv: tell super/last_person/next_week/find_number.py or super/way.py up first point"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_same_hand_at_way(str_arg):
+    find_case_for_last_fact(str_arg)
+    print('early_world_and_right_woman')
+
+def find_case_for_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_same_hand_at_way('number_and_point')
