2bcbbcd39c3cedc2e71b5446497b06248398319d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 12:41:21 -0400

    "mv: ask super/bad_fact_or_fact/think_company_in_hand.py and super/high_life/point/own_problem/go_first_fact.py above large group"

---
+++ super/high_life/point/own_problem/go_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    woman(str_arg)
+    print('point')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('able_place')
