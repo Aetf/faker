50fc24e07ea1631f506d03602201f0b44a60569c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 14:43:59 -0400

    "mix: work super/week/other_eye.py and super/large_woman.py above early place"

---
+++ super/week/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    call_group(str_arg)
+    print('little_day')
+
+def call_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('large_point_and_fact')
