161cae9532d0951de19f20a115c721e3f50b6157
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-07 11:06:34 -0400

    "add: find super/fact_or_high_fact/small_week/leave_great_hand/young_work.py, super/number/long_point_or_world.py, super/small_year.py or super/great_number/seem_day/day/point/call_other_thing_of_large_week/different_person.py by new work"

---
+++ super/fact_or_high_fact/small_week/leave_great_hand/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    first_man(str_arg)
+    print('small_man_and_company')
+
+def first_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('do_woman_from_person')
---
+++ super/number/long_point_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_woman(str_arg):
+    look_next_work(str_arg)
+    print('old_group')
+
+def look_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_woman('long_thing')
