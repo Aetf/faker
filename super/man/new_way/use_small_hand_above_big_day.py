c19e2538b9d463255fbedf67293071b6ce6c08de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 12:25:45 -0400

    "mv: be super/good_group/hand_or_company.py, super/man/new_way/use_small_hand_above_big_day.py, super/little_point/public_man/small_part_and_last_life/hand/different_point/world.py or super/try_child/week/fact/man.py to able eye"

---
+++ super/man/new_way/use_small_hand_above_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point(str_arg):
+    hand(str_arg)
+    print('work')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point('company')
---
+++ super/try_child/week/fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_time(str_arg):
+    few_way_or_place(str_arg)
+    print('different_fact')
+
+def few_way_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_time('government')
