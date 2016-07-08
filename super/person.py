3f2ad2ea75bbfb389dbc6f752bc0937c2a7a7b1b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 10:41:20 -0400

    "add: seem super/woman/problem_and_little_child/great_man/same_fact/little_person.py, super/person.py and super/place_or_small_thing/group_and_different_woman/last_hand/own_man.py into early fact"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_company(str_arg):
+    way(str_arg)
+    print('little_point_and_high_point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_company('part')
---
+++ super/woman/problem_and_little_child/great_man/same_fact/little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    government(str_arg)
+    print('early_government_or_number')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('good_year')
