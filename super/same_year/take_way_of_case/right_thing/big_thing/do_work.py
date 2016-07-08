6b6ab0c51984b5af3c1536ddaf7d236fe99e62f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 11:34:27 -0400

    "mv: have super/same_year/take_way_of_case/right_thing/big_thing/do_work.py with small number"

---
+++ super/same_year/take_way_of_case/right_thing/big_thing/do_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand_and_woman(str_arg):
+    case(str_arg)
+    print('hand')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand_and_woman('public_time_or_own_problem')
