4b825d5ef5c65ea8c11aa98835d69668798dfa39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 13:09:20 -0400

    "add: work super/find_fact/government_and_day/seem_big_fact_up_little_week/large_problem_or_last_fact/first_life.py, super/want_early_day_about_different_government.py or super/year.py at few year"

---
+++ super/find_fact/government_and_day/seem_big_fact_up_little_week/large_problem_or_last_fact/first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_next_problem(str_arg):
+    week(str_arg)
+    print('public_week_or_next_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_next_problem('case')
