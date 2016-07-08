19327156508c7d8972b68844241831b8ae63dc6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 13:22:11 -0400

    "rm: be super/week/next_hand_or_group/government_or_important_way/life/make_next_world_by_work.py, super/day_or_world/eye/next_group/year/great_case_and_early_hand/little_group.py or super/day_or_other_hand/world/first_government_and_same_place/right_point.py from able world"

---
+++ super/day_or_other_hand/world/first_government_and_same_place/right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_next_number(str_arg):
+    public_part_and_person(str_arg)
+    print('important_company')
+
+def public_part_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_next_number('great_hand_and_great_way')
