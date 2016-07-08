f5ad1763122e7a46c8c563e9ef353cf27375137d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 14:47:05 -0400

    "rm: come super/right_number/world.py under same eye"

---
+++ super/right_number/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_hand(str_arg):
+    life_or_important_world(str_arg)
+    print('work')
+
+def life_or_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_hand('new_year')
