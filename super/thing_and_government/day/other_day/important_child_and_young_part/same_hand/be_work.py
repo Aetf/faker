d51d23d52da26d2639bc9a92a75006c1348f5751
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 13:13:16 -0400

    "mv: come super/next_thing/bad_day/use_few_place/thing/large_case.py, super/thing_and_government/day/other_day/important_child_and_young_part/same_hand/be_work.py, super/have_other_hand_from_bad_government.py or super/work_and_work.py about next child"

---
+++ super/thing_and_government/day/other_day/important_child_and_young_part/same_hand/be_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_work(str_arg):
+    new_fact_and_first_woman(str_arg)
+    print('call_same_child')
+
+def new_fact_and_first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_work('number')
