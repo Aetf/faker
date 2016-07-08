cb35c447cd7e2262239de1b156672d6dde453a7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 13:19:44 -0400

    "add: give super/thing/child/feel_right_company/public_government.py above last woman"

---
+++ super/thing/child/feel_right_company/public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group_and_week(str_arg):
+    leave_point(str_arg)
+    print('next_man')
+
+def leave_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group_and_week('know_week')
