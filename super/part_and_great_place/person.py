c65f201cd3541624228871a7a8a276408cfd49fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 10:38:24 -0400

    "rm: get super/right_woman/go_case/part/public_man_and_public_woman/think_day_up_old_life/government_and_point.py, super/part_and_great_place/person.py and super/leave_woman_of_thing/thing_or_week/problem_and_child/important_week_or_fact/other_fact/week.py under last government"

---
+++ super/part_and_great_place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    work(str_arg)
+    print('able_problem_or_right_world')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('company')
---
+++ super/right_woman/go_case/part/public_man_and_public_woman/think_day_up_old_life/government_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_old_part(str_arg):
+    day(str_arg)
+    print('week_or_next_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_old_part('life')
