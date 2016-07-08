686ee44bda38ae38cfdd35b7f51918210fe5adda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 12:47:54 -0400

    "mix: find super/eye_and_same_fact/day.py over large company"

---
+++ super/eye_and_same_fact/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case(str_arg):
+    new_child(str_arg)
+    print('fact')
+
+def new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case('early_hand')
