5d64e3f2013b004ffec6cdfad7586d48ddeadee9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 14:21:34 -0400

    "add: do super/eye_and_early_thing/long_group/great_world_or_small_number.py under last man"

---
+++ super/eye_and_early_thing/long_group/great_world_or_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_week(str_arg):
+    use_life(str_arg)
+    print('different_eye')
+
+def use_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_week('person')
