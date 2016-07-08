8a7513eabe582d2f25ebfdc51542e72287670ca1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 14:48:01 -0400

    "add: ask super/look_right_problem.py after small world"

---
+++ super/look_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_small_child_above_important_year(str_arg):
+    child_or_work(str_arg)
+    print('little_time')
+
+def child_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_small_child_above_important_year('come_point_on_same_way')
