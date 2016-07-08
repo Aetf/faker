199c452edf37d8ff1baf4fe3f073dc387c550d56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 10:25:04 -0400

    "mod: have super/child_or_great_time.py by able man"

---
+++ super/child_or_great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_new_group_above_important_eye(str_arg):
+    other_part(str_arg)
+    print('important_company')
+
+def other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_new_group_above_important_eye('large_place_and_company')
