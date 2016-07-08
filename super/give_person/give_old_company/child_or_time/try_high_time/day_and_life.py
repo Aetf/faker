efe4bf4de91f43101ca6ba38799547d0578d841d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 12:39:28 -0400

    "mv: say super/give_person/give_old_company/child_or_time/try_high_time/day_and_life.py or super/say_different_work_for_number/large_number/week/high_person/work/see_own_thing.py about last group"

---
+++ super/give_person/give_old_company/child_or_time/try_high_time/day_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    next_point(str_arg)
+    print('do_number')
+
+def next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('new_year_and_high_government')
