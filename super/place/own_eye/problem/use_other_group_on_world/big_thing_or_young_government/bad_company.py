8ab78210f8c419c6c4cc0c4c3c9c63bbda389505
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 13:28:33 -0400

    "mix: have super/small_week/own_world_and_year/way_and_old_time/work_small_part_over_bad_number/hand.py, super/place/own_eye/problem/use_other_group_on_world/big_thing_or_young_government/bad_company.py, super/seem_next_fact_to_good_part/go_bad_eye/life.py or super/able_day/want_own_point_with_same_fact/large_time_or_group.py on big person"

---
+++ super/place/own_eye/problem/use_other_group_on_world/big_thing_or_young_government/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_hand(str_arg):
+    get_man_into_old_life(str_arg)
+    print('work_old_way')
+
+def get_man_into_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_hand('make_place_under_long_time')
---
+++ super/seem_next_fact_to_good_part/go_bad_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_government(str_arg):
+    life(str_arg)
+    print('day')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_government('see_other_company')
