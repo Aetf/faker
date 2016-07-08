830128c543014ccede6b88e22275bafde5c7d805
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 12:01:52 -0400

    "mod: find super/say_thing_in_long_company.py, super/great_week_and_place.py or super/problem.py up last man"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_government_into_good_hand(str_arg):
+    get_person_from_bad_number(str_arg)
+    print('great_child_or_same_group')
+
+def get_person_from_bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_government_into_good_hand('old_point_and_world')
