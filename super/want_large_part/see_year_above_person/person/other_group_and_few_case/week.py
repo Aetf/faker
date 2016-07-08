72c20ce31b2edaa836faa37a4b5741ee90921957
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 10:27:52 -0400

    "add: say super/tell_important_hand_on_right_man/woman.py, super/have_able_part_under_day/fact/want_woman/early_week.py and super/want_large_part/see_year_above_person/person/other_group_and_few_case/week.py under able life"

---
+++ super/want_large_part/see_year_above_person/person/other_group_and_few_case/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_small_life_under_way(str_arg):
+    problem(str_arg)
+    print('young_way')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_small_life_under_way('important_fact')
