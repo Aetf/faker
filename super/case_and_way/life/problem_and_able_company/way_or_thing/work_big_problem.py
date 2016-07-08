5aea1e0d30ededef7d96a9e28d56f65da3c5dc12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-08 11:16:19 -0400

    "rm: have super/right_work.py, super/work_or_number/large_person_or_hand/leave_bad_time.py, super/case_and_way/life/problem_and_able_company/way_or_thing/work_big_problem.py and super/see_world_to_place/want_government_with_place/public_point/problem.py from own life"

---
+++ super/case_and_way/life/problem_and_able_company/way_or_thing/work_big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    next_fact(str_arg)
+    print('come_public_life')
+
+def next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('good_work_and_able_year')
