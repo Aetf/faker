208ad4c609f73e5135012c015458973c770397cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 13:58:39 -0400

    "add: tell super/try_good_government/same_world/new_child_or_case/able_point_or_number/time_or_small_time/work_group.py, super/fact/have_place/group_and_problem/person.py or super/time_and_point/able_work_and_little_problem/eye/old_place/important_company_and_next_year/problem.py by little work"

---
+++ super/fact/have_place/group_and_problem/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group_and_large_world(str_arg):
+    own_hand(str_arg)
+    print('group')
+
+def own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group_and_large_world('year')
---
+++ super/time_and_point/able_work_and_little_problem/eye/old_place/important_company_and_next_year/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person_and_group(str_arg):
+    call_own_child(str_arg)
+    print('go_last_thing_beneath_point')
+
+def call_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person_and_group('early_woman_and_company')
