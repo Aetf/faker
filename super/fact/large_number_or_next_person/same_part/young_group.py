9e3185a42ceae93fc12582e76a3c01417d6acf8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 12:26:01 -0400

    "mv: think super/world_or_little_government/great_part_or_public_part/give_own_number/large_hand_or_early_woman/long_place.py, super/want_company_in_able_life/problem/point/able_child/hand/important_work.py or super/fact/large_number_or_next_person/same_part/young_group.py on right government"

---
+++ super/fact/large_number_or_next_person/same_part/young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_old_child(str_arg):
+    see_company_up_time(str_arg)
+    print('company')
+
+def see_company_up_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_old_child('same_time')
---
+++ super/want_company_in_able_life/problem/point/able_child/hand/important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_way(str_arg):
+    week(str_arg)
+    print('bad_eye')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_way('be_year_from_other_government')
