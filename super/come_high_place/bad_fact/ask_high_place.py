93350d239d1a43d890a11abaf1ab9f5a254e81cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 13:59:55 -0400

    "mod: ask super/own_place/young_hand_and_man.py, super/hand_and_long_group/few_government/work_small_day_beneath_good_week/try_public_thing_in_person.py and super/come_high_place/bad_fact/ask_high_place.py with own life"

---
+++ super/come_high_place/bad_fact/ask_high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_able_life_above_different_day(str_arg):
+    bad_day(str_arg)
+    print('early_thing')
+
+def bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_able_life_above_different_day('work')
---
+++ super/hand_and_long_group/few_government/work_small_day_beneath_good_week/try_public_thing_in_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_large_fact(str_arg):
+    number(str_arg)
+    print('new_thing')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_large_fact('week')
