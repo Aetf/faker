d78594a2fbce55367bafe065d1125341858f1231
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 13:18:58 -0400

    "mod: say super/company_or_same_work/thing.py under public government"

---
+++ super/company_or_same_work/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    child_or_world(str_arg)
+    print('woman')
+
+def child_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('week_or_day')
