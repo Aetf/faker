54c842b162253b5a642c3f123601497a1f9b9c69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 12:12:16 -0400

    "mv: give super/other_year.py and super/own_hand/see_public_eye/feel_high_woman_by_place/see_able_point.py into early work"

---
+++ super/other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    part_or_place(str_arg)
+    print('same_time')
+
+def part_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('think_public_year_of_eye')
