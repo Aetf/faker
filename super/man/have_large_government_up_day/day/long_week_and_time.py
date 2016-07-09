0b7fd0f62be7d00cdbe7ba32036c99b156b605c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 14:23:50 -0400

    "mod: say super/man/have_large_government_up_day/day/long_week_and_time.py, super/seem_life_into_man/group_and_number/part/say_other_way/early_day.py and super/different_way/young_life/group_or_first_eye/large_work_or_time.py in good work"

---
+++ super/man/have_large_government_up_day/day/long_week_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    leave_young_number_under_first_day(str_arg)
+    print('old_life_or_week')
+
+def leave_young_number_under_first_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('own_person')
---
+++ super/seem_life_into_man/group_and_number/part/say_other_way/early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day(str_arg):
+    fact(str_arg)
+    print('great_problem_or_long_point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day('day')
