058ced4e67853b669d0a51bcbeb62f4fca1152e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 13:06:48 -0400

    "mv: come super/point/feel_child/other_week/time_and_year/case.py above great person"

---
+++ super/point/feel_child/other_week/time_and_year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    thing_or_old_thing(str_arg)
+    print('right_part_and_young_week')
+
+def thing_or_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('week')
