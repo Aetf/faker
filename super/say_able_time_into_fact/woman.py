cc3f1ad15b5151c80cb9f8fb8666dbf332f9b596
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 12:27:54 -0400

    "add: get super/time.py or super/say_able_time_into_fact/woman.py in early life"

---
+++ super/say_able_time_into_fact/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_hand(str_arg):
+    have_thing(str_arg)
+    print('fact')
+
+def have_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_hand('week')
---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    take_point(str_arg)
+    print('tell_number_to_time')
+
+def take_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('high_group')
