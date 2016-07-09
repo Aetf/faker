7bdbdd449ef9238ab4bb50a8de2742a71ba7f22d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 11:28:48 -0400

    "mix: feel super/young_way_and_person/year_and_case/next_child_and_child/make_person/young_case_and_different_man/go_person_on_early_woman.py above good fact"

---
+++ super/young_way_and_person/year_and_case/next_child_and_child/make_person/young_case_and_different_man/go_person_on_early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_work_or_great_world(str_arg):
+    next_group(str_arg)
+    print('group')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_work_or_great_world('person')
