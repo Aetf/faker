3069d527a9878f4f6eb84fc0e49affbeed4235d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 14:36:41 -0400

    "rm: use super/last_world_and_old_eye/tell_work_in_great_case/point_and_new_year/problem_or_day/fact/old_problem_and_early_fact.py, super/government/few_group/small_company.py, super/high_company_or_part.py or super/person_and_old_problem/big_problem_or_year/next_week/small_time/government/government.py on bad eye"

--- super/government/few_group/small_company.py
+++ super/government/few_group/small_company.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def small_day(str_arg):
+def company(str_arg):
     fact(str_arg)
-    print('great_problem_or_long_point')
+    print('big_place')
 
 def fact(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    small_day('day')
+    company('place')
---
+++ super/high_company_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government_and_big_time(str_arg):
+    part(str_arg)
+    print('first_hand')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government_and_big_time('problem_or_place')
