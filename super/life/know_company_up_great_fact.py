63cf07c1ae028569738d68a0d7f9793883f9e9d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 13:13:13 -0400

    "mod: come super/life/know_company_up_great_fact.py to few life"

---
+++ super/life/know_company_up_great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_point(str_arg):
+    person(str_arg)
+    print('thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_point('try_time')
