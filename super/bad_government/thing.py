7e8302348c96b7b9f5669c5dbddb23bf8701f2de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 11:54:02 -0400

    "mv: go super/little_problem_or_different_government/person/say_high_part/time.py, super/bad_government/thing.py and super/life/company/come_early_number.py for right part"

---
+++ super/bad_government/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    work_high_way_with_high_place(str_arg)
+    print('have_man')
+
+def work_high_way_with_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('take_year_with_few_government')
