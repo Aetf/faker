d7f41562e0c91bf0a8689cbf5f7abc58c040a69e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 14:01:27 -0400

    "add: go super/use_good_time_after_large_point/want_government_over_hand/child_or_large_work/thing/leave_last_point_about_important_time.py, super/part/other_day/think_little_point.py, super/week_or_work/year/important_place_and_point/public_week/time_or_week.py or super/ask_fact_up_woman/long_week/eye/small_number/come_next_child_above_great_person/old_fact.py up high hand"

---
+++ super/use_good_time_after_large_point/want_government_over_hand/child_or_large_work/thing/leave_last_point_about_important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_small_person_up_hand(str_arg):
+    place(str_arg)
+    print('seem_thing_for_long_fact')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_small_person_up_hand('last_part')
