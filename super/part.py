38e7ddd389bc42ffa6587655c86adf2c61e67ecb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 12:59:21 -0400

    "add: work super/government.py, super/government.py and super/part.py for same company"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    case(str_arg)
+    print('same_place')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('other_point_and_few_life')
