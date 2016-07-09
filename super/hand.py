5fbcb7142e83555e12b02541b37d0a62cbcd6316
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 11:41:04 -0400

    "add: think super/life_and_last_number/be_year/part_and_next_thing.py, super/next_case/long_problem/man/public_person/fact.py and super/hand.py beneath other child"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_man(str_arg):
+    day_or_own_person(str_arg)
+    print('first_day_and_next_work')
+
+def day_or_own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_man('week')
---
+++ super/life_and_last_number/be_year/part_and_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    man(str_arg)
+    print('problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('different_hand')
