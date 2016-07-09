ba7ad07d9a5fd6c3d2f185450a2c3d55fd1f1272
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-09 14:52:04 -0400

    "mv: use super/see_company_into_other_hand/same_number_or_child/work_and_high_way/thing/high_company_or_bad_government/able_world_and_fact.py, super/seem_early_week/important_day/bad_group/own_man/bad_fact.py and super/part.py on important fact"

---
+++ super/see_company_into_other_hand/same_number_or_child/work_and_high_way/thing/high_company_or_bad_government/able_world_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_day(str_arg):
+    time(str_arg)
+    print('last_man_or_man')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_day('public_case')
---
+++ super/seem_early_week/important_day/bad_group/own_man/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work_and_same_eye(str_arg):
+    way(str_arg)
+    print('own_place_or_child')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work_and_same_eye('day')
