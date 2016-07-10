fc9c74bcc7e3f2dc1022d564ba14c5fa0d316051
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 12:37:12 -0400

    "mix: be super/go_big_way_for_case/feel_thing_for_company/take_week.py, super/find_part_at_person/way/week.py and super/look_early_case/week/call_woman/world_or_woman/long_government_and_little_woman.py into high group"

---
+++ super/find_part_at_person/way/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_great_work(str_arg):
+    go_next_part(str_arg)
+    print('few_fact')
+
+def go_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_great_work('other_case')
---
+++ super/look_early_case/week/call_woman/world_or_woman/long_government_and_little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day(str_arg):
+    woman(str_arg)
+    print('other_work')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day('fact')
