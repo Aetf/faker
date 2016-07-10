babcc5135ec1b755630fa3d0bc7b077969814e49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 12:10:44 -0400

    "mv: try super/work_public_life_after_last_place/early_work_or_last_person/year_and_young_part/able_point_and_world/young_problem.py under right world"

---
+++ super/work_public_life_after_last_place/early_work_or_last_person/year_and_young_part/able_point_and_world/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    right_eye(str_arg)
+    print('problem')
+
+def right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('public_life')
