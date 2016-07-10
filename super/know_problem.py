3a2593a18992fbe1922f032dd7b5c413e2ebe713
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 10:08:34 -0400

    "mod: say super/know_problem.py above next problem"

---
+++ super/know_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing_and_thing(str_arg):
+    same_woman(str_arg)
+    print('year')
+
+def same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing_and_thing('good_child_or_same_way')
