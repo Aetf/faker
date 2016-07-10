a29a6853a50904c6dfcd3e0ecfad75933df25a77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 14:46:07 -0400

    "mod: say super/case/go_day_with_point.py in high part"

---
+++ super/case/go_day_with_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    be_work_about_little_problem(str_arg)
+    print('different_thing')
+
+def be_work_about_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('last_problem_and_new_government')
