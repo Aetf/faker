a49ed3957ca719779f1ec398529919b61f7f5479
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 10:21:36 -0400

    "rm: be super/first_work_or_hand.py under large day"

---
+++ super/first_work_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    long_year(str_arg)
+    print('come_person_into_place')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('be_thing')
