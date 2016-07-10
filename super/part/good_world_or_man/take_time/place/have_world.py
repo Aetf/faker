fe677796ebd327920baf4237391452af995a14af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 14:36:35 -0400

    "rm: look super/part/good_world_or_man/take_time/place/have_world.py, super/part/come_old_thing_under_right_group/time_and_long_day/thing/few_company_and_thing.py and super/young_way/good_world.py about right child"

---
+++ super/part/good_world_or_man/take_time/place/have_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_hand(str_arg):
+    other_hand_and_same_work(str_arg)
+    print('try_man_above_number')
+
+def other_hand_and_same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_hand('young_man_or_public_way')
