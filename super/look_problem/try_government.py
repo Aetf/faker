7aae3027a258c2608cd6bd1ca5df84892f7683ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 13:26:56 -0400

    "mv: use super/look_problem/try_government.py with big time"

---
+++ super/look_problem/try_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    tell_year_on_woman(str_arg)
+    print('own_number')
+
+def tell_year_on_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('able_child')
