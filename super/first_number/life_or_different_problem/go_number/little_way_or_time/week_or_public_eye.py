e4d4a7e21127b6929aa98330aaf374a03508390e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 11:18:23 -0400

    "mix: have super/use_life/give_good_world/way/other_year_or_point.py, super/know_man_over_large_eye/say_small_work.py or super/first_number/life_or_different_problem/go_number/little_way_or_time/week_or_public_eye.py in young case"

---
+++ super/first_number/life_or_different_problem/go_number/little_way_or_time/week_or_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    same_group(str_arg)
+    print('high_number')
+
+def same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('world_or_small_world')
---
+++ super/know_man_over_large_eye/say_small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_way_of_number(str_arg):
+    long_woman(str_arg)
+    print('government')
+
+def long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_way_of_number('place')
