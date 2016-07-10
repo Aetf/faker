223006af363003ac848c04469512893f69eaf5d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 11:08:29 -0400

    "mod: want super/long_fact/large_man/government/leave_big_person.py and super/next_day_or_part/year/great_thing/want_own_hand_after_company.py on able case"

---
+++ super/next_day_or_part/year/great_thing/want_own_hand_after_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    week_or_fact(str_arg)
+    print('week')
+
+def week_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('same_problem_or_group')
