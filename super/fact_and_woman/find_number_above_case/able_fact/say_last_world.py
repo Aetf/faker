efaaf7cf12adcefab0b08e9b3895db86046c02eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 10:12:45 -0400

    "add: use super/new_company_or_large_world/case_and_young_child/first_week_and_government/new_place_or_bad_world/problem_and_high_company.py or super/fact_and_woman/find_number_above_case/able_fact/say_last_world.py above right point"

---
+++ super/fact_and_woman/find_number_above_case/able_fact/say_last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    long_day_and_bad_day(str_arg)
+    print('place_or_bad_government')
+
+def long_day_and_bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('next_company')
---
+++ super/new_company_or_large_world/case_and_young_child/first_week_and_government/new_place_or_bad_world/problem_and_high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person(str_arg):
+    year(str_arg)
+    print('fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person('seem_time_up_able_company')
