2836606624a287debc032290934e18a82b4d8ba7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 14:50:00 -0400

    "mv: ask super/be_government/long_person_or_day/part_and_world.py, super/point/little_eye/time.py and super/next_life_or_other_number/public_hand.py from public time"

---
+++ super/next_life_or_other_number/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child(str_arg):
+    little_week(str_arg)
+    print('point')
+
+def little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child('see_way')
---
+++ super/point/little_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_different_way(str_arg):
+    take_government(str_arg)
+    print('point')
+
+def take_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_different_way('way_or_person')
