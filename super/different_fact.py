817b04373e83c90be752214cb92ae9b90c58bc29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 10:20:20 -0400

    "mv: make super/bad_eye.py, super/different_fact.py, super/seem_number.py and super/other_woman_and_life/next_fact/find_bad_hand_up_old_work.py of bad man"

---
+++ super/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_point_with_world(str_arg):
+    big_hand_and_high_hand(str_arg)
+    print('great_case')
+
+def big_hand_and_high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_point_with_world('know_great_world')
---
+++ super/seem_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way(str_arg):
+    place(str_arg)
+    print('man_or_case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way('big_thing_or_case')
