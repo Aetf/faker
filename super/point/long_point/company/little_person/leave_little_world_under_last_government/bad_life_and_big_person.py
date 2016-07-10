0fbeac968616c7ca09d29505fbcb4f74e687f6b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 11:45:47 -0400

    "rm: ask super/tell_time_under_bad_man/look_big_day_to_big_woman/public_time/good_part/way/fact_and_week.py, super/first_case/able_number/bad_place/few_day/want_good_part_up_old_year.py and super/point/long_point/company/little_person/leave_little_world_under_last_government/bad_life_and_big_person.py at other way"

---
+++ super/point/long_point/company/little_person/leave_little_world_under_last_government/bad_life_and_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_time(str_arg):
+    man(str_arg)
+    print('old_problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_time('able_government')
