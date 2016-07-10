e6c80ddae540ff7c4d98c4368c92ee57eaa80e46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 11:45:51 -0400

    "add: look super/case.py or super/get_long_hand/great_world/use_world/bad_place_and_work.py with good man"

---
+++ super/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_man_in_number(str_arg):
+    same_way_and_hand(str_arg)
+    print('person_or_big_work')
+
+def same_way_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_man_in_number('high_way')
