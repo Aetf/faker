1c3de46486c12b6a1a5cd77d9c099cd2112e9bde
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 11:08:47 -0400

    "rm: have super/first_government/few_week/call_hand/early_day/little_case/little_number_and_small_child.py, super/come_number_under_number.py and super/day_and_last_company/point/new_government/want_good_number_over_person/eye.py of last number"

---
+++ super/come_number_under_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    high_life_and_same_life(str_arg)
+    print('point')
+
+def high_life_and_same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_world')
