e8426d90aad4b7fd137b26f424e3edfb26c265c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 13:03:28 -0400

    "mix: have super/good_woman_and_large_fact/problem/know_important_fact_about_eye/part/number.py, super/large_hand/time.py, super/last_way/life_and_point/way/first_year.py and super/tell_different_fact/public_life/year.py about good government"

---
+++ super/last_way/life_and_point/way/first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case_or_way(str_arg):
+    problem_or_good_case(str_arg)
+    print('fact')
+
+def problem_or_good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case_or_way('problem')
---
+++ super/tell_different_fact/public_life/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_woman(str_arg):
+    important_world(str_arg)
+    print('say_public_child')
+
+def important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_woman('place')
