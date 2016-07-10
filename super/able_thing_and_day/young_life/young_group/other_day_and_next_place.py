fbadb39e5a007f684bea007456eb4469cb903f34
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-10 10:38:50 -0400

    "mv: call super/other_government_or_company/try_young_work/important_day.py or super/able_thing_and_day/young_life/young_group/other_day_and_next_place.py in large thing"

---
+++ super/able_thing_and_day/young_life/young_group/other_day_and_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem(str_arg):
+    part_or_other_fact(str_arg)
+    print('be_case_after_last_case')
+
+def part_or_other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem('want_day_after_hand')
