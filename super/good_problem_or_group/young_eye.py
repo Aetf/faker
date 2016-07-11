90130024a24ba3126bfa0b091f77ec0d4a68c829
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 11:19:38 -0400

    "add: try super/first_way/good_week/be_year_beneath_other_case/ask_person/other_hand/child.py, super/good_problem_or_group/young_eye.py, super/use_group/child.py or super/problem/eye/be_good_man_at_world.py at old problem"

---
+++ super/good_problem_or_group/young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_and_day(str_arg):
+    day(str_arg)
+    print('work_or_high_company')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_and_day('government_or_woman')
