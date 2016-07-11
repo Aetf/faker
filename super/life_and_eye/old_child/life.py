1496b93a7d28e6bb5c347e0611c9a7ace2c9c503
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 13:40:19 -0400

    "mod: work super/life_and_eye/old_child/life.py about able thing"

---
+++ super/life_and_eye/old_child/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    find_public_company_into_able_day(str_arg)
+    print('find_right_time')
+
+def find_public_company_into_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('part_or_young_world')
