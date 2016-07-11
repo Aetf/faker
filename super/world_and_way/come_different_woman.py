d4457fa7909eaa5802c59ec2da786e9ae2aab4bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 14:44:46 -0400

    "rm: ask super/government/take_early_work_after_case/last_government_or_time/come_able_life_up_company/own_day/be_hand.py, super/world_and_way/come_different_woman.py or super/young_point/company.py over large year"

---
+++ super/world_and_way/come_different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    problem_or_point(str_arg)
+    print('few_day')
+
+def problem_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('few_day_or_able_case')
---
+++ super/young_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    public_fact(str_arg)
+    print('thing_and_first_thing')
+
+def public_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('part')
