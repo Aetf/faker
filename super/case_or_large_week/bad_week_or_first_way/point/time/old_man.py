a05b4de8df466d06d95db6b0e9c2b0d69a124b37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 12:15:47 -0400

    "mod: find super/case_or_large_week/bad_week_or_first_way/point/time/old_man.py above last part"

---
+++ super/case_or_large_week/bad_week_or_first_way/point/time/old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_world(str_arg):
+    life_and_day(str_arg)
+    print('part_or_big_man')
+
+def life_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_world('way')
