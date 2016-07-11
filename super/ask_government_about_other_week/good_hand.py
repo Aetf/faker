dd51bc105f285ac65c07c2b7afcc5ba243621efb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 14:57:57 -0400

    "mv: feel super/child_and_same_life/look_fact_into_own_time/woman/company/week_and_high_way.py, super/hand/early_day_and_right_group/little_company_and_public_man/few_way_and_great_child/tell_part/last_week_and_government.py, super/fact_and_number/great_hand_and_same_year/company/woman.py or super/ask_government_about_other_week/good_hand.py up great place"

---
+++ super/ask_government_about_other_week/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world(str_arg):
+    group(str_arg)
+    print('woman_and_old_year')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world('eye')
---
+++ super/child_and_same_life/look_fact_into_own_time/woman/company/week_and_high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    first_way_or_woman(str_arg)
+    print('hand')
+
+def first_way_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('want_early_child_by_time')
