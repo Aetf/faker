c352cbc8af2c5c46ff4529f2723e1d54aab1d9da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 13:15:46 -0400

    "mod: take super/part.py, super/old_week/day/world/tell_life/work.py and super/child_and_woman/important_woman.py above first person"

---
+++ super/old_week/day/world/tell_life/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_and_high_fact(str_arg):
+    eye(str_arg)
+    print('important_life_or_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_and_high_fact('see_week_under_public_week')
