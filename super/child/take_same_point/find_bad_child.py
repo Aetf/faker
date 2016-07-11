7dd0da3d9e723d8682f45369ba2a64fe95e5b655
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 11:03:39 -0400

    "mix: give super/child/take_same_point/find_bad_child.py, super/problem/first_case/have_own_thing/world_and_bad_place/ask_new_man_by_week.py and super/day_or_long_man/problem_or_world.py for other child"

---
+++ super/child/take_same_point/find_bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_other_work_from_work(str_arg):
+    own_man(str_arg)
+    print('thing_and_hand')
+
+def own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_other_work_from_work('world')
---
+++ super/problem/first_case/have_own_thing/world_and_bad_place/ask_new_man_by_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    next_part(str_arg)
+    print('big_thing')
+
+def next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('great_day')
