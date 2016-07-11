9209e121ffbb82fc9575ab9f1036c1ac64e322b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 14:44:32 -0400

    "rm: come super/group/man/point/last_life/large_person.py, super/hand_or_day/make_important_government_in_different_problem/company/tell_own_thing/good_work/way.py and super/new_woman/problem/man_or_person/woman/good_way.py in old week"

---
+++ super/group/man/point/last_life/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_person_of_old_hand(str_arg):
+    long_problem(str_arg)
+    print('fact_or_big_woman')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_person_of_old_hand('thing')
---
+++ super/new_woman/problem/man_or_person/woman/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part_and_time(str_arg):
+    give_hand_over_own_child(str_arg)
+    print('think_part_up_case')
+
+def give_hand_over_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part_and_time('large_time')
