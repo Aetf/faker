790291ea84701ac93f96ebb5e202733a1585e62c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 11:23:39 -0400

    "mix: seem super/be_man.py or super/work/bad_woman/know_old_man_above_bad_way/problem_and_next_life/new_place_or_woman.py from high thing"

---
+++ super/be_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_able_week(str_arg):
+    eye_or_bad_person(str_arg)
+    print('place')
+
+def eye_or_bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_able_week('find_number_on_work')
---
+++ super/work/bad_woman/know_old_man_above_bad_way/problem_and_next_life/new_place_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_place(str_arg):
+    government_and_high_woman(str_arg)
+    print('number_and_fact')
+
+def government_and_high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_place('first_year')
