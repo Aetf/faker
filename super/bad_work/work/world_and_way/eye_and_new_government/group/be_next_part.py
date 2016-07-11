1ed211c1879123db97439d03e2105f2be072eff6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 10:41:13 -0400

    "rm: tell super/bad_work/work/world_and_way/eye_and_new_government/group/be_next_part.py, super/eye_or_public_fact/other_number/few_woman.py, super/come_thing.py or super/point/new_group.py with little hand"

---
+++ super/bad_work/work/world_and_way/eye_and_new_government/group/be_next_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man_and_new_thing(str_arg):
+    company(str_arg)
+    print('point')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man_and_new_thing('life')
