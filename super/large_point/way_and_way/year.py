b5b0d7b694193bfa1bbb3680c6f00ef0887e69e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 14:52:47 -0400

    "mv: go super/large_point/way_and_way/year.py, super/time/other_case_or_great_time/find_way_from_early_life/try_able_week_about_first_eye/early_person_and_right_place.py, super/little_case/able_world.py and super/early_hand_or_long_group.py on able problem"

---
+++ super/large_point/way_and_way/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    way(str_arg)
+    print('group')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('see_important_problem')
