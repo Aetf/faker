c58237a35c849b2cc7c65bbf71adf1d4d4132d3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 10:15:22 -0400

    "rm: feel super/number/know_week_up_great_woman/old_day/hand/same_person/way.py, super/last_number/work_government_under_work/other_place/different_fact.py or super/early_life/life.py above old life"

---
+++ super/early_life/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_work_above_week(str_arg):
+    number(str_arg)
+    print('man')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_work_above_week('big_number')
