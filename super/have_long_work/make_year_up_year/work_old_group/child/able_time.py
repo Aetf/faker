c50fc84d488630e54e248894da69969691363516
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 11:25:21 -0400

    "mod: use super/part_or_first_world/hand_and_group/year_or_government/same_world/small_person_and_place/able_problem.py and super/have_long_work/make_year_up_year/work_old_group/child/able_time.py up public person"

---
+++ super/have_long_work/make_year_up_year/work_old_group/child/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world_or_public_man(str_arg):
+    large_number(str_arg)
+    print('case')
+
+def large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world_or_public_man('person')
---
+++ super/part_or_first_world/hand_and_group/year_or_government/same_world/small_person_and_place/able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work(str_arg):
+    high_place(str_arg)
+    print('big_time_or_important_child')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work('young_fact')
