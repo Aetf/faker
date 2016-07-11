fc32ac283437ea8f4ad314c228d97c96e20e3079
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-11 11:01:11 -0400

    "mv: call super/great_time/woman/see_child_into_next_week/problem/government_and_woman.py for early company"

---
+++ super/great_time/woman/see_child_into_next_week/problem/government_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    new_year_and_person(str_arg)
+    print('come_same_child_over_place')
+
+def new_year_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('case')
