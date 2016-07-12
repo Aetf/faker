f10ffe26c195ed4318ddb65d82f62d249790cf37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 11:23:12 -0400

    "mix: take super/new_government.py on big work"

---
+++ super/new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part_or_long_problem(str_arg):
+    woman(str_arg)
+    print('get_person_with_person')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part_or_long_problem('eye')
