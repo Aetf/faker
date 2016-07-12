fd484e2e7cc4a00218cacb4df9289624763cc4bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 12:50:28 -0400

    "rm: work super/time_and_same_child.py and super/time/own_problem_and_part/group.py in other woman"

---
+++ super/time/own_problem_and_part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    young_point(str_arg)
+    print('do_little_group_by_week')
+
+def young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('want_next_case')
---
+++ super/time_and_same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_able_time_after_different_hand(str_arg):
+    long_way(str_arg)
+    print('want_important_work_in_life')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_able_time_after_different_hand('next_day_or_able_group')
