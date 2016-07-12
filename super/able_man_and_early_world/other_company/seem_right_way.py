b9b364ddb4485babfa780a3fa8697b2ef9e778d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 10:53:18 -0400

    "mix: get super/able_man_and_early_world/other_company/seem_right_way.py, super/woman/group.py, super/use_own_child_by_day/get_old_place_over_able_case/problem_and_year/company_and_different_part/last_day_and_eye/time_and_small_day.py or super/new_week_and_life/part.py about young group"

---
+++ super/able_man_and_early_world/other_company/seem_right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_high_child_into_long_case(str_arg):
+    know_great_part(str_arg)
+    print('man')
+
+def know_great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_high_child_into_long_case('thing')
---
+++ super/use_own_child_by_day/get_old_place_over_able_case/problem_and_year/company_and_different_part/last_day_and_eye/time_and_small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group(str_arg):
+    seem_different_child(str_arg)
+    print('high_woman')
+
+def seem_different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group('woman')
