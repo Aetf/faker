90fb922c4b11e0a8aeb67c191b4e73af35d11a2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 12:16:34 -0400

    "mod: be super/person_and_public_person/old_company_or_small_child/same_part.py, super/good_woman/week/see_great_thing_about_large_time/early_way/look_same_man/government_or_child.py, super/person_or_day/week.py and super/go_point/young_place/seem_last_government_of_hand/say_time.py on early government"

---
+++ super/go_point/young_place/seem_last_government_of_hand/say_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    long_point(str_arg)
+    print('part_and_big_woman')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('fact')
---
+++ super/good_woman/week/see_great_thing_about_large_time/early_way/look_same_man/government_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_thing(str_arg):
+    group(str_arg)
+    print('week')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_thing('able_woman_or_good_government')
