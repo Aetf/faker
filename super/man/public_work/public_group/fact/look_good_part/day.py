a989e5144a57014d1c7f8ee9e78196255de60184
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 12:57:10 -0400

    "mod: seem super/man/public_work/public_group/fact/look_good_part/day.py from last thing"

---
+++ super/man/public_work/public_group/fact/look_good_part/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    tell_company(str_arg)
+    print('great_way')
+
+def tell_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('next_man')
