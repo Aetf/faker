c9978b3f1ff5321e75eaccf4c5e10579b85e4c06
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 11:50:04 -0400

    "mix: work super/work_long_person_beneath_bad_child/say_right_woman/long_place_or_small_place.py, super/part/take_few_way/last_world/part/year_or_point.py and super/few_child_and_bad_thing/last_point_and_new_woman/problem.py of important problem"

---
+++ super/few_child_and_bad_thing/last_point_and_new_woman/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_week(str_arg):
+    call_early_point_to_big_child(str_arg)
+    print('take_able_hand')
+
+def call_early_point_to_big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_week('high_woman_and_able_part')
