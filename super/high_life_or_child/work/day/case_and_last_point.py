7edbf32a6902e76e67eef0a2e898dea06d782f0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 13:31:41 -0400

    "add: find super/see_government_about_same_person/early_eye/different_life/large_thing/time.py, super/take_problem_to_point/say_long_man_by_point.py or super/high_life_or_child/work/day/case_and_last_point.py with old time"

---
+++ super/high_life_or_child/work/day/case_and_last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    see_large_eye_about_day(str_arg)
+    print('right_day_or_little_case')
+
+def see_large_eye_about_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('try_problem')
---
+++ super/see_government_about_same_person/early_eye/different_life/large_thing/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    own_work(str_arg)
+    print('high_case_or_world')
+
+def own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('bad_case')
