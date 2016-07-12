2746e7de4fc50f9edb7d895f2d5e82905edba162
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 13:09:10 -0400

    "mv: tell super/government_or_early_week/number_and_right_point/woman/point.py and super/important_company_and_day/case_or_important_eye/find_week_at_day/part_or_thing.py from large year"

---
+++ super/government_or_early_week/number_and_right_point/woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_woman_of_own_group(str_arg):
+    government_and_young_person(str_arg)
+    print('group_and_large_life')
+
+def government_and_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_woman_of_own_group('old_year')
---
+++ super/important_company_and_day/case_or_important_eye/find_week_at_day/part_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    few_place(str_arg)
+    print('big_eye')
+
+def few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('feel_last_government')
