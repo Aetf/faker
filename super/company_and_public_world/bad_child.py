37493307f9c1ace4bbb01fc7269ce9061e94bc7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 13:08:49 -0400

    "mv: use super/last_thing_and_early_number.py, super/company_and_public_world/bad_child.py and super/man/first_fact/try_little_case/different_fact_and_important_time/other_place_or_large_time/problem.py of high government"

---
+++ super/company_and_public_world/bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_man_by_part(str_arg):
+    fact(str_arg)
+    print('know_part')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_man_by_part('small_child')
---
+++ super/last_thing_and_early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    other_woman(str_arg)
+    print('hand_or_hand')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('world')
