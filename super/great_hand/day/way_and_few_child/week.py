e701060182618d30aeb9dcc44c917f156bd0dcda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 14:49:27 -0400

    "mix: work super/great_hand/day/way_and_few_child/week.py up little eye"

---
+++ super/great_hand/day/way_and_few_child/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    right_person(str_arg)
+    print('big_work')
+
+def right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('company')
