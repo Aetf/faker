2bafac9106a4f2118603cd265490799f3c18e4fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-12 14:26:59 -0400

    "mix: say super/look_part_with_work/time.py, super/leave_large_problem/be_next_eye_for_fact/good_group_or_own_person/get_hand_on_important_company.py, super/important_way/year/little_fact.py and super/government/first_problem_and_life/same_person_or_man/great_life_and_time.py at other point"

---
+++ super/important_way/year/little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    eye(str_arg)
+    print('be_part_in_time')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('ask_old_time_from_government')
---
+++ super/leave_large_problem/be_next_eye_for_fact/good_group_or_own_person/get_hand_on_important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work(str_arg)
+    print('big_life')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('place')
