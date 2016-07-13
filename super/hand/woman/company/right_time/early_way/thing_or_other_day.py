00f0bc55e7b3d520143be33a60458da84fa8fa08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:51:49 -0400

    "mix: ask super/hand/few_work/get_first_life.py or super/hand/woman/company/right_time/early_way/thing_or_other_day.py into high group"

---
+++ super/hand/woman/company/right_time/early_way/thing_or_other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_little_way_into_old_government(str_arg):
+    find_year(str_arg)
+    print('say_company_over_eye')
+
+def find_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_little_way_into_old_government('last_way')
