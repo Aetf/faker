75547d0e0bb720dd06cd06f1ea2ff78c1b58fb4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:03:19 -0400

    "mv: know super/big_point.py at good way"

---
+++ super/big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    leave_thing(str_arg)
+    print('high_week')
+
+def leave_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('make_thing')
