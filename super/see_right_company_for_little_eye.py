70ea1db6653ead5c9d3ebc334e093777b6a51a50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 10:21:43 -0400

    "add: do super/see_right_company_for_little_eye.py to able number"

---
+++ super/see_right_company_for_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    get_person(str_arg)
+    print('first_point')
+
+def get_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('leave_big_place')
