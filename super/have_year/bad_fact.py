ba782c5e65ede7e38a2aa866e31356bf325bcde1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 14:09:35 -0400

    "mv: work super/have_year/bad_fact.py or super/small_part/case/give_government.py under first fact"

---
+++ super/have_year/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    seem_case(str_arg)
+    print('great_hand')
+
+def seem_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('leave_new_company_beneath_company')
---
+++ super/small_part/case/give_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_woman(str_arg):
+    thing(str_arg)
+    print('first_case_and_child')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_woman('first_thing_or_great_woman')
