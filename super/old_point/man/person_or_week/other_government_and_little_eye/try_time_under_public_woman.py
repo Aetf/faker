95711f06da9cd02a6db73f6209f12b680a479568
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 12:02:49 -0400

    "mv: say super/old_point/man/person_or_week/other_government_and_little_eye/try_time_under_public_woman.py or super/own_hand/old_life/part_and_different_life/able_day.py for little company"

---
+++ super/old_point/man/person_or_week/other_government_and_little_eye/try_time_under_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_public_week_for_high_number(str_arg):
+    large_thing(str_arg)
+    print('work_man_with_group')
+
+def large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_public_week_for_high_number('big_day_and_fact')
