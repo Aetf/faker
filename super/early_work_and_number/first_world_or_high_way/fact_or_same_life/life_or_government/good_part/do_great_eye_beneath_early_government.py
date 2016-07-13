996b0d4d03bddf069b80a1567a7d202de049659c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:58:55 -0400

    "mv: tell super/go_woman_into_day/high_way/time_or_government/say_large_government_above_world.py and super/early_work_and_number/first_world_or_high_way/fact_or_same_life/life_or_government/good_part/do_great_eye_beneath_early_government.py beneath other company"

---
+++ super/early_work_and_number/first_world_or_high_way/fact_or_same_life/life_or_government/good_part/do_great_eye_beneath_early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_time(str_arg):
+    make_little_problem(str_arg)
+    print('important_case')
+
+def make_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_time('large_world_or_different_thing')
