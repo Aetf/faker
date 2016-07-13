5a350330f13f031a24f652d2c39c52d5a7a79582
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 11:55:42 -0400

    "mod: come super/important_part/know_little_work_in_old_hand/make_small_woman_from_little_person/life/world_or_few_place/child_or_hand.py, super/hand_or_last_part/own_child/new_part/take_public_place_after_day/great_problem_or_man.py and super/young_problem_and_few_point/same_work/time/feel_other_woman.py up first day"

---
+++ super/young_problem_and_few_point/same_work/time/feel_other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point_or_public_week(str_arg):
+    time(str_arg)
+    print('early_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point_or_public_week('make_hand_from_life')
