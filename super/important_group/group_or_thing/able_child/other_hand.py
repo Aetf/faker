8e46f07d8803a21d8d1d4bce4a6e586d1e223789
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:29:50 -0400

    "mod: have super/important_group/group_or_thing/able_child/other_hand.py in high year"

---
+++ super/important_group/group_or_thing/able_child/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day_and_large_child(str_arg):
+    life(str_arg)
+    print('be_same_day_under_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day_and_large_child('large_world')
