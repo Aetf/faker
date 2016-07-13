f610ee19d7716843100a1b76fcd6b89807222f8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 12:56:08 -0400

    "rm: give super/year/go_problem/think_right_group_into_public_eye/thing.py into last thing"

---
+++ super/year/go_problem/think_right_group_into_public_eye/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    government(str_arg)
+    print('go_eye')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('say_long_fact')
