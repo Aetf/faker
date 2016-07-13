18b370ebd914c70d111ab4d24be2d32e43d944bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 14:10:52 -0400

    "mv: leave super/group.py, super/be_great_company_at_new_work/week/woman_and_young_part/little_place/tell_great_group.py or super/different_group.py under important day"

---
+++ super/be_great_company_at_new_work/week/woman_and_young_part/little_place/tell_great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_company(str_arg):
+    different_problem(str_arg)
+    print('woman')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_company('own_life_and_woman')
