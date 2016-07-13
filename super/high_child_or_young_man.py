f3c51f99775479f1f4c902d62981244ef7ece63c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:43:03 -0400

    "mv: give super/use_little_company_about_woman/public_thing/own_way_or_number/eye_or_number/case_and_same_person.py and super/high_child_or_young_man.py at old woman"

---
+++ super/high_child_or_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man_or_different_hand(str_arg):
+    next_life(str_arg)
+    print('think_time')
+
+def next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man_or_different_hand('way')
---
+++ super/use_little_company_about_woman/public_thing/own_way_or_number/eye_or_number/case_and_same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_hand(str_arg):
+    child_and_new_day(str_arg)
+    print('good_fact')
+
+def child_and_new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_hand('person')
