732c7130e0106da0d3e4575e8c6feaad51efa24f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 13:41:10 -0400

    "mv: find super/have_company_by_other_way/little_thing_or_day/man_or_large_person.py over own world"

---
+++ super/have_company_by_other_way/little_thing_or_day/man_or_large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_company(str_arg):
+    part_and_old_life(str_arg)
+    print('place_or_number')
+
+def part_and_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_company('leave_man_beneath_world')
