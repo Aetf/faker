f8759f33faa7e202166a58a0bca2285aabdf6683
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 10:26:51 -0400

    "mix: think super/feel_important_problem/woman.py above able way"

---
+++ super/feel_important_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    high_work(str_arg)
+    print('early_group')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('new_problem_or_early_child')
