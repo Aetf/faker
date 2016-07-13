fc417d8719bafe9ba1dbe2743f009d13d67e633b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 14:13:29 -0400

    "mv: use super/new_company_and_same_week/point/want_big_hand_by_work/world.py, super/small_eye/tell_day/year/little_thing/good_point_and_world.py, super/large_eye/part/case/old_woman_or_woman.py and super/see_problem_in_government/use_public_year/year.py over right place"

---
+++ super/large_eye/part/case/old_woman_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person(str_arg):
+    feel_little_company(str_arg)
+    print('large_case')
+
+def feel_little_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person('thing')
---
+++ super/see_problem_in_government/use_public_year/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number(str_arg):
+    great_person(str_arg)
+    print('way_or_few_case')
+
+def great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number('part_and_bad_way')
