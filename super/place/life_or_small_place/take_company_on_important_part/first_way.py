31e8406f2fd5ddb91990c35f7b1e6fd2b937193d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 14:15:23 -0400

    "add: feel super/place/life_or_small_place/take_company_on_important_part/first_way.py in few hand"

---
+++ super/place/life_or_small_place/take_company_on_important_part/first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    good_day(str_arg)
+    print('great_year')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('government_and_bad_day')
