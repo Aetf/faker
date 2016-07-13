60c9029252fb4c0871d9040b91d2e75fd0e3f564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 11:35:37 -0400

    "mix: be super/next_week/think_day/point/tell_man/high_problem/time.py by early child"

---
+++ super/next_week/think_day/point/tell_man/high_problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man_on_public_hand(str_arg):
+    woman(str_arg)
+    print('say_early_life_from_same_year')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man_on_public_hand('fact')
