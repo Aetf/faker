f747a7298f3cf3f1c2698a395b38cafe30264928
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-13 11:51:45 -0400

    "mod: ask super/time_and_other_time/young_thing/feel_woman/world/find_case_with_different_day/life.py about important thing"

---
+++ super/time_and_other_time/young_thing/feel_woman/world/find_case_with_different_day/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    same_number(str_arg)
+    print('hand_or_big_government')
+
+def same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('place')
