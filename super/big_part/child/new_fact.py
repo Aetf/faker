4ddcf8b3f3643c767443d34d795921efea4aa6e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 10:41:29 -0400

    "add: have super/big_part/child/new_fact.py, super/person/hand.py, super/child_or_company/big_child_or_last_man/public_number/good_place/way/fact_or_able_group.py and super/case/look_last_part/next_eye.py above large place"

---
+++ super/big_part/child/new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_company(str_arg):
+    own_number(str_arg)
+    print('next_man')
+
+def own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_company('hand_or_year')
---
+++ super/person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_number(str_arg):
+    same_government(str_arg)
+    print('give_different_thing_about_work')
+
+def same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_number('get_other_time')
