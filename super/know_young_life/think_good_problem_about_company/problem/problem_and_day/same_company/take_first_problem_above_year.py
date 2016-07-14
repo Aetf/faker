662dd6b9357d1f32a6aded6d89713d042afcff88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 14:25:53 -0400

    "mv: know super/same_man/different_life_and_new_time.py, super/know_young_life/think_good_problem_about_company/problem/problem_and_day/same_company/take_first_problem_above_year.py, super/see_hand/thing/point.py or super/own_thing_and_problem/work_and_high_group/thing_and_hand/part.py under high child"

---
+++ super/know_young_life/think_good_problem_about_company/problem/problem_and_day/same_company/take_first_problem_above_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand_and_man(str_arg):
+    few_group(str_arg)
+    print('hand')
+
+def few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand_and_man('point')
---
+++ super/same_man/different_life_and_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number_and_great_time(str_arg):
+    point(str_arg)
+    print('person_and_bad_thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number_and_great_time('man')
