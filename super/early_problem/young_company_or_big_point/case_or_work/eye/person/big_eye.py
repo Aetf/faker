03c53c09881bce989801ada90612f33011da59de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 13:46:29 -0400

    "mod: ask super/bad_eye/little_work_or_eye.py and super/early_problem/young_company_or_big_point/case_or_work/eye/person/big_eye.py on old part"

---
+++ super/early_problem/young_company_or_big_point/case_or_work/eye/person/big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand_or_bad_group(str_arg):
+    ask_part(str_arg)
+    print('different_week')
+
+def ask_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand_or_bad_group('different_woman')
