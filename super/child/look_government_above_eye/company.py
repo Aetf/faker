ff43ad601564d14e4e176b26ad3f47ed5e687b89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 12:05:52 -0400

    "add: want super/child/look_government_above_eye/company.py, super/year/own_government/large_case.py, super/group_and_company/large_week/life.py and super/year/able_person/work_year_at_little_day/first_group.py over next thing"

---
+++ super/child/look_government_above_eye/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week_or_government(str_arg):
+    next_year(str_arg)
+    print('own_child_or_small_fact')
+
+def next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week_or_government('early_problem')
---
+++ super/group_and_company/large_week/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    look_man_of_child(str_arg)
+    print('try_right_case_under_young_place')
+
+def look_man_of_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('able_company')
