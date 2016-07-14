bff17a3334eceedb59fc38b8ceee13e35ad0ab01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 14:06:26 -0400

    "mix: want super/same_woman.py, super/number_and_same_work/year_and_place/old_group/big_way.py, super/seem_bad_place/child_or_point/young_woman_and_last_man/day_and_able_man.py or super/use_early_life_about_long_government.py up right group"

---
+++ super/same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_and_other_company(str_arg):
+    child_and_part(str_arg)
+    print('eye')
+
+def child_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_and_other_company('great_person')
---
+++ super/seem_bad_place/child_or_point/young_woman_and_last_man/day_and_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_last_hand(str_arg):
+    person(str_arg)
+    print('person_or_bad_woman')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_last_hand('try_woman_above_first_life')
