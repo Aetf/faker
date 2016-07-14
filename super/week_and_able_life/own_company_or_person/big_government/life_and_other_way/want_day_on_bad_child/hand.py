ec383a90e88e4ace709228acce86b4fad071f47f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 13:43:14 -0400

    "mix: use super/week_and_able_life/own_company_or_person/big_government/life_and_other_way/want_day_on_bad_child/hand.py from large year"

---
+++ super/week_and_able_life/own_company_or_person/big_government/life_and_other_way/want_day_on_bad_child/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    long_problem(str_arg)
+    print('feel_world_about_last_group')
+
+def long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('next_government')
