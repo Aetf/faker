e103e8be378b4079983ebbfec06af9dd25dbee9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 12:42:32 -0400

    "rm: look super/early_part/week/get_long_government.py in high work"

---
+++ super/early_part/week/get_long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_small_number(str_arg):
+    right_life(str_arg)
+    print('young_fact')
+
+def right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_small_number('ask_long_person_over_thing')
