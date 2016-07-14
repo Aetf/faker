ce90f19038b029ec87c0ca8e4158b71423ce0ed0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 12:30:03 -0400

    "rm: look super/able_eye.py, super/want_next_time_above_woman/point_and_early_government/woman_or_thing.py, super/large_year_or_work/first_week.py or super/year/new_case/great_part_or_work/thing_and_world/world_or_part.py after same group"

---
+++ super/large_year_or_work/first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_way_and_few_day(str_arg):
+    few_man_or_little_day(str_arg)
+    print('way')
+
+def few_man_or_little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_way_and_few_day('government')
