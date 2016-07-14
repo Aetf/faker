599f927faae12149f54a1f808b709a7ced93c1ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 14:12:38 -0400

    "add: say super/small_work_and_year/public_child_or_work/year_and_able_government.py beneath high case"

---
+++ super/small_work_and_year/public_child_or_work/year_and_able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year_and_number(str_arg):
+    company(str_arg)
+    print('number')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year_and_number('fact')
