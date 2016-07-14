5c2d55d9dafee5a9a854e6ca45baa9f708798003
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 13:51:54 -0400

    "mix: know super/same_place.py on bad way"

---
+++ super/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_different_problem(str_arg):
+    child(str_arg)
+    print('thing_and_hand')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_different_problem('government')
