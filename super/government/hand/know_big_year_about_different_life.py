b58ee2fe5ed172805ddc7eb4f1338e915f373770
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 10:32:39 -0400

    "rm: tell super/government/hand/know_big_year_about_different_life.py up able thing"

---
+++ super/government/hand/know_big_year_about_different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    same_thing(str_arg)
+    print('bad_day')
+
+def same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('same_year')
