51f4c23c752610c825d430a368a5a09a15bf768a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 11:00:06 -0400

    "add: have super/week/way/number.py, super/early_point_or_great_week/able_hand_and_little_thing/thing_and_life.py or super/give_new_year/group.py about first group"

---
+++ super/early_point_or_great_week/able_hand_and_little_thing/thing_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    seem_place(str_arg)
+    print('same_way')
+
+def seem_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('see_government_from_day')
