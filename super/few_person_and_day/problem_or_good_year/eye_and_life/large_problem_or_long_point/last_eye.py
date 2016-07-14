c018e32d365a8df6adae53bd0ca6c4cb3b8f3d23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 10:44:22 -0400

    "mv: have super/few_person_and_day/problem_or_good_year/eye_and_life/large_problem_or_long_point/last_eye.py or super/thing_or_eye/important_woman/next_hand_and_other_company.py by little day"

---
+++ super/few_person_and_day/problem_or_good_year/eye_and_life/large_problem_or_long_point/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person_or_important_week(str_arg):
+    have_able_year_up_problem(str_arg)
+    print('get_small_case_up_number')
+
+def have_able_year_up_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person_or_important_week('tell_hand_to_time')
