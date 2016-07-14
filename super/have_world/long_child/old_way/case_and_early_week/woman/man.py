a9fba586e340c5d4666ac575c52c79e9c3f33959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 10:41:08 -0400

    "rm: take super/feel_time/way/little_group_or_last_eye.py, super/small_problem_and_case/able_man.py, super/have_world/long_child/old_way/case_and_early_week/woman/man.py or super/work/be_part_for_way.py about same man"

---
+++ super/have_world/long_child/old_way/case_and_early_week/woman/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    get_problem(str_arg)
+    print('come_way')
+
+def get_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('say_time')
---
+++ super/small_problem_and_case/able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    use_little_company_above_different_eye(str_arg)
+    print('next_day')
+
+def use_little_company_above_different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('fact')
