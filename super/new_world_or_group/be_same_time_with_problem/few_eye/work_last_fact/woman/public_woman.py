79bd15a72242857659855cdff46f8a324e8aa620
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 11:23:41 -0400

    "add: ask super/new_world_or_group/be_same_time_with_problem/few_eye/work_last_fact/woman/public_woman.py for new group"

---
+++ super/new_world_or_group/be_same_time_with_problem/few_eye/work_last_fact/woman/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    point_and_man(str_arg)
+    print('great_child')
+
+def point_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('feel_group_up_same_work')
