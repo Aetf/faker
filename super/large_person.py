116308cd03c85011a52878d8da79ebda15c6fc86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 11:05:30 -0400

    "mod: ask super/tell_child/eye.py, super/leave_big_life/work/know_woman.py and super/large_person.py about next child"

---
+++ super/large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_important_government(str_arg):
+    get_week(str_arg)
+    print('work_and_point')
+
+def get_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_important_government('few_year')
