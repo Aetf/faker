e8fab2ed317a9465bf78715e51534dde83a4492a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 14:23:01 -0400

    "mv: call super/problem/work/other_person_and_bad_group.py or super/year_or_same_company/number_or_large_time/have_number_by_person.py for old number"

---
+++ super/problem/work/other_person_and_bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_way(str_arg):
+    number(str_arg)
+    print('person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_way('other_year')
