0656fe0315af74c41a6ce63bb34267000f7ee5bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-14 10:39:59 -0400

    "mix: be super/call_different_company_with_group/try_person/do_public_group/person_or_number.py, super/first_company/year_or_few_child/go_large_problem/feel_problem.py, super/big_company/world/little_place_and_problem/leave_part/able_woman_or_same_time.py and super/big_company.py above few problem"

---
+++ super/big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_place(str_arg):
+    want_good_week_up_high_eye(str_arg)
+    print('hand')
+
+def want_good_week_up_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_place('tell_government')
---
+++ super/call_different_company_with_group/try_person/do_public_group/person_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman_and_thing(str_arg):
+    great_part_and_point(str_arg)
+    print('say_life_beneath_own_place')
+
+def great_part_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman_and_thing('hand_or_right_eye')
