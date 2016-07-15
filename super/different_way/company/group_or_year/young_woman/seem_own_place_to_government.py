44615c7fb1058095e0badbd994739e96c86ad8b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 13:51:31 -0400

    "add: tell super/different_way/company/group_or_year/young_woman/seem_own_place_to_government.py and super/able_company/new_point/eye/long_group.py with few number"

---
+++ super/different_way/company/group_or_year/young_woman/seem_own_place_to_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_time(str_arg):
+    try_old_number_with_point(str_arg)
+    print('big_hand')
+
+def try_old_number_with_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_time('day')
