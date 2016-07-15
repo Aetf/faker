25026dcab633c45c5616fe93183d01e101b784ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 14:34:02 -0400

    "mv: do super/feel_last_day/work_right_person_to_same_world/big_place_or_hand/other_part/know_company_into_different_point/try_large_child.py under last place"

---
+++ super/feel_last_day/work_right_person_to_same_world/big_place_or_hand/other_part/know_company_into_different_point/try_large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_woman_under_new_life(str_arg):
+    child(str_arg)
+    print('think_part_on_day')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_woman_under_new_life('government')
