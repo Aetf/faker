8fb76af2eaa80c2f849a31631e2f6c9ae6801a1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:59:20 -0400

    "mod: take super/group/see_early_time_on_little_person/point.py, super/new_eye_and_point.py, super/person_or_problem/part_or_public_person/point/man.py or super/bad_eye/number/time_or_year.py by big time"

---
+++ super/new_eye_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part(str_arg):
+    time(str_arg)
+    print('number')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part('new_problem_or_large_world')
---
+++ super/person_or_problem/part_or_public_person/point/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    see_same_government_up_woman(str_arg)
+    print('last_number_or_last_case')
+
+def see_same_government_up_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('able_thing')
