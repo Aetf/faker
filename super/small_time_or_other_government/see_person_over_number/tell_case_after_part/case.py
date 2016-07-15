852bb04bedeb264a4652cd8367f05f4658d1b5c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:16:37 -0400

    "add: try super/year_or_group/go_man_for_world/time_or_other_day.py or super/small_time_or_other_government/see_person_over_number/tell_case_after_part/case.py about good company"

---
+++ super/small_time_or_other_government/see_person_over_number/tell_case_after_part/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_public_person(str_arg):
+    life_or_company(str_arg)
+    print('large_life')
+
+def life_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_public_person('first_place_and_group')
---
+++ super/year_or_group/go_man_for_world/time_or_other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    man_and_bad_eye(str_arg)
+    print('thing_and_few_time')
+
+def man_and_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('first_thing')
