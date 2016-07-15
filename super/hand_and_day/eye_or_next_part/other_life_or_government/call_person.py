edad3ba68cb58fe77287b00ae9a42c98902c3ec9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 13:54:14 -0400

    "add: leave super/right_world/year/first_life_and_world/little_government_and_next_group/new_group_or_new_case.py or super/hand_and_day/eye_or_next_part/other_life_or_government/call_person.py into right company"

---
+++ super/hand_and_day/eye_or_next_part/other_life_or_government/call_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_week(str_arg):
+    try_thing(str_arg)
+    print('place')
+
+def try_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_week('company')
---
+++ super/right_world/year/first_life_and_world/little_government_and_next_group/new_group_or_new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_world_at_public_child(str_arg):
+    right_life_or_fact(str_arg)
+    print('call_high_place_from_year')
+
+def right_life_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_world_at_public_child('problem')
