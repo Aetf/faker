0ba9820215bab1757b77dbc900f745b8dc054bda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:15:50 -0400

    "rm: ask super/come_small_work.py, super/high_man_or_group.py, super/new_case/next_person_or_able_life.py or super/good_case/other_part/have_life/problem_or_part/try_thing_by_bad_thing/seem_same_child_in_place.py after good problem"

---
+++ super/high_man_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    hand_and_public_eye(str_arg)
+    print('world')
+
+def hand_and_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('next_man')
---
+++ super/new_case/next_person_or_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_first_child(str_arg):
+    large_day_and_hand(str_arg)
+    print('own_person_or_first_woman')
+
+def large_day_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_first_child('feel_way')
