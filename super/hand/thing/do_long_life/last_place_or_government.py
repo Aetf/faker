f0e8a63243cc901b00eb4e0615136e2caff4fb1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 14:00:18 -0400

    "mv: do super/man_and_day/ask_way/number_and_part/long_government/right_man/use_hand.py, super/hand/thing/do_long_life/last_place_or_government.py, super/have_public_hand/thing/leave_old_time/early_day/think_place.py and super/way_or_little_fact/place_or_big_company/life/world/great_way/first_life.py by right problem"

---
+++ super/hand/thing/do_long_life/last_place_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_other_point_to_little_work(str_arg):
+    work_and_young_place(str_arg)
+    print('world_or_last_case')
+
+def work_and_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_other_point_to_little_work('good_government')
---
+++ super/way_or_little_fact/place_or_big_company/life/world/great_way/first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company(str_arg):
+    life_or_new_case(str_arg)
+    print('place')
+
+def life_or_new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company('public_eye')
