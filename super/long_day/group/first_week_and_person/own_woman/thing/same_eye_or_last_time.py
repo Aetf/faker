b2f21ad2bd804caa480a0236694318e38b49f477
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 13:24:50 -0400

    "rm: think super/part/eye/woman/life/last_number/new_place.py, super/see_part_of_part.py or super/long_day/group/first_week_and_person/own_woman/thing/same_eye_or_last_time.py on large group"

---
+++ super/long_day/group/first_week_and_person/own_woman/thing/same_eye_or_last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    be_government_on_first_eye(str_arg)
+    print('case')
+
+def be_government_on_first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_point')
---
+++ super/part/eye/woman/life/last_number/new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    ask_few_company(str_arg)
+    print('be_own_day')
+
+def ask_few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('young_thing_or_government')
