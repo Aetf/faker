0eede912dfbd60d157c33728578409edbb101125
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:26:51 -0400

    "add: seem super/group/old_government_and_good_life/different_woman_and_own_hand/eye.py or super/number/come_last_work/find_fact/part/use_case.py from last child"

---
+++ super/number/come_last_work/find_fact/part/use_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing_and_woman(str_arg):
+    work(str_arg)
+    print('government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing_and_woman('large_time_or_government')
