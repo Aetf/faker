7792ec40b1d2047b289e491054c9a2172241557f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:06:53 -0400

    "rm: see super/come_different_part_to_day.py, super/good_problem/early_place/last_woman_and_woman/man_and_eye.py, super/young_case/first_part/old_number_and_other_company.py and super/woman_and_year/child/next_case.py beneath small way"

---
+++ super/come_different_part_to_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    be_small_week_on_fact(str_arg)
+    print('come_case')
+
+def be_small_week_on_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('different_problem_and_different_case')
---
+++ super/woman_and_year/child/next_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point_or_same_problem(str_arg):
+    woman(str_arg)
+    print('company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point_or_same_problem('man_and_eye')
