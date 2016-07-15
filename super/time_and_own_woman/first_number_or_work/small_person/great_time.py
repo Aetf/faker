db475e465170c7152fcc61491759a5618868fbcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 12:11:27 -0400

    "mod: see super/time_and_own_woman/first_number_or_work/small_person/great_time.py, super/work/different_work_or_long_person/person/leave_time/early_part/public_life.py, super/important_case/young_government/way/place_or_important_work.py and super/government/come_case_for_place/know_large_government_beneath_thing.py above little day"

---
+++ super/time_and_own_woman/first_number_or_work/small_person/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    say_high_company(str_arg)
+    print('world')
+
+def say_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('day')
