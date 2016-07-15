fbaebbada1ac039f325349fd211f4de00c5f9654
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 14:48:35 -0400

    "mod: tell super/important_hand/public_part_and_able_fact/last_hand.py above small woman"

---
+++ super/important_hand/public_part_and_able_fact/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_child(str_arg):
+    thing(str_arg)
+    print('work_old_number_above_man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_child('new_problem')
