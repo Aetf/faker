212104b50064a61525da2c66ff6419235688fa9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 14:59:21 -0400

    "add: do super/give_old_place_with_long_world/early_problem/good_year/place.py into right person"

---
+++ super/give_old_place_with_long_world/early_problem/good_year/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    early_company_and_eye(str_arg)
+    print('tell_bad_day_from_government')
+
+def early_company_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('fact')
