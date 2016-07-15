c8c90bd3ca7df72176df466e3ba7d3eb858ee5e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 14:29:01 -0400

    "mix: call super/place.py or super/man.py from early group"

---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number(str_arg):
+    fact(str_arg)
+    print('big_man')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number('big_thing')
