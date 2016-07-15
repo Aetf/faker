fbb38b0b1745185b5271c5dd781ebb68bdecc65d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-15 13:18:36 -0400

    "mix: find super/year/own_man/eye_or_thing.py, super/bad_company_and_woman/find_way/eye/time_and_high_case.py, super/think_big_year_by_great_person/go_company_above_day.py and super/person/world_or_place/year/world_and_part.py for public world"

---
+++ super/person/world_or_place/year/world_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    child(str_arg)
+    print('day_and_old_man')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('problem_and_problem')
