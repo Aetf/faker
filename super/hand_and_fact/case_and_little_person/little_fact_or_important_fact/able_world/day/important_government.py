8235bc5163ce27a60d2b4222cb381d6e2f977e64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 13:50:32 -0400

    "mix: work super/hand_and_fact/case_and_little_person/little_fact_or_important_fact/able_world/day/important_government.py at able eye"

---
+++ super/hand_and_fact/case_and_little_person/little_fact_or_important_fact/able_world/day/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_day(str_arg):
+    tell_time(str_arg)
+    print('point_or_different_year')
+
+def tell_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_day('government')
