7e51bfd95f283f8b39cb6dc8e1b82d61db0a0409
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 12:38:09 -0400

    "mv: want super/next_woman/make_early_man/seem_young_group/group/little_day.py, super/look_work.py or super/tell_few_child_into_few_child/new_company/next_place/right_place.py from early year"

---
+++ super/tell_few_child_into_few_child/new_company/next_place/right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    other_point(str_arg)
+    print('bad_time_and_old_part')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_way_and_early_eye')
