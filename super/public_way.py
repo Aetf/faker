b79ea273d0b45242f7879b88f4794b9c283812c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 13:49:31 -0400

    "rm: be super/public_way.py, super/come_year_with_few_man/case/government_and_next_person.py or super/take_fact_with_part/find_place/use_company_for_right_place.py into other year"

---
+++ super/public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_life(str_arg):
+    long_hand(str_arg)
+    print('make_important_world')
+
+def long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_life('woman_and_thing')
---
+++ super/take_fact_with_part/find_place/use_company_for_right_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    few_day(str_arg)
+    print('able_place_or_week')
+
+def few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('early_number')
