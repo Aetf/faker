fb147f1bed2d6cc431db816dd0310aede8369c25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 11:56:34 -0400

    "mv: see super/year.py and super/use_few_case.py over public person"

---
+++ super/use_few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_early_life(str_arg):
+    seem_new_way(str_arg)
+    print('call_first_number')
+
+def seem_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_early_life('find_big_man')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    leave_early_time_under_good_company(str_arg)
+    print('problem_and_fact')
+
+def leave_early_time_under_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('large_person')
