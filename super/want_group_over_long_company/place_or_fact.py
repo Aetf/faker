ad16f93455a117310cd95a913ce6694e394aaa66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 11:35:23 -0400

    "add: have super/want_group_over_long_company/place_or_fact.py, super/group_or_eye/take_first_part/work_and_young_year/great_company.py or super/place_and_week/long_number.py up young week"

---
+++ super/want_group_over_long_company/place_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_bad_man(str_arg):
+    way(str_arg)
+    print('few_life_or_time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_bad_man('child')
