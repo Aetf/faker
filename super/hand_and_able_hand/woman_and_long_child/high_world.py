ba0a20a30dcf872d987fb167d1c448b2f5160a9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 10:45:42 -0400

    "rm: feel super/make_long_hand/good_thing/number_and_time/week/work.py, super/seem_time.py or super/hand_and_able_hand/woman_and_long_child/high_world.py after great case"

---
+++ super/hand_and_able_hand/woman_and_long_child/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    eye(str_arg)
+    print('call_eye_beneath_great_thing')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('take_important_group_beneath_point')
---
+++ super/seem_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number(str_arg):
+    other_man_or_old_week(str_arg)
+    print('last_thing')
+
+def other_man_or_old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number('tell_large_case')
