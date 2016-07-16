df99593b7ce84034cd7997bed66096a759a98ed5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 11:10:39 -0400

    "add: do super/know_right_fact_from_day/want_government.py and super/seem_day_under_number/next_way_and_first_group/take_problem/important_fact.py on good man"

---
+++ super/know_right_fact_from_day/want_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_point_on_big_man(str_arg):
+    seem_week(str_arg)
+    print('use_good_way_in_little_company')
+
+def seem_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_point_on_big_man('group')
