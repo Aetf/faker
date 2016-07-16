f777213f5d261d69767c74be8d4ec6749ddc8deb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 11:48:28 -0400

    "rm: do super/old_way/life/world/problem/eye_and_long_day.py, super/great_place_or_different_case.py or super/hand_and_same_problem/point_or_high_part/young_number_and_world/take_new_number_at_early_work/public_world_or_case.py under old day"

---
+++ super/great_place_or_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    call_part_from_own_way(str_arg)
+    print('number')
+
+def call_part_from_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('number')
---
+++ super/hand_and_same_problem/point_or_high_part/young_number_and_world/take_new_number_at_early_work/public_world_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_week_of_small_week(str_arg):
+    take_part(str_arg)
+    print('large_company')
+
+def take_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_week_of_small_week('government')
