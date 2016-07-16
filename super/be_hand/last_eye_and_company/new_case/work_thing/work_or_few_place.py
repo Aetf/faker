83e893f82ef60bc9b94fdb2157afeaa9879a9ee0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 11:34:17 -0400

    "add: call super/be_hand/last_eye_and_company/new_case/work_thing/work_or_few_place.py up little point"

---
+++ super/be_hand/last_eye_and_company/new_case/work_thing/work_or_few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    use_important_year(str_arg)
+    print('number')
+
+def use_important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('number')
