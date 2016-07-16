faf291e1fcece30fa9f417b56028fffbfc9410ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 14:52:57 -0400

    "mod: do super/few_woman/child/great_thing.py, super/high_day/try_large_year_at_time/way/point/young_government_and_eye.py, super/first_world.py and super/government_or_early_time/bad_part/time/give_child_beneath_problem.py above last company"

---
+++ super/few_woman/child/great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    work_and_day(str_arg)
+    print('good_life_or_public_point')
+
+def work_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('eye_or_few_hand')
---
+++ super/government_or_early_time/bad_part/time/give_child_beneath_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    other_number_and_hand(str_arg)
+    print('child')
+
+def other_number_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('think_work')
