8e201e07342e9e05e83e02ba31a657d9f78e5273
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 10:15:53 -0400

    "mix: say super/week_and_eye/point_and_work/seem_last_thing_about_part.py at high fact"

---
+++ super/week_and_eye/point_and_work/seem_last_thing_about_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_number_of_company(str_arg):
+    find_part_about_part(str_arg)
+    print('right_way')
+
+def find_part_about_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_number_of_company('person_and_hand')
