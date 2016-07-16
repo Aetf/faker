8673b584c2e8c9977eab34056e10e05d7f422c88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 13:08:08 -0400

    "mod: get super/seem_public_case_about_public_point/thing_or_point/different_woman.py with same fact"

---
+++ super/seem_public_case_about_public_point/thing_or_point/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_week(str_arg):
+    same_week(str_arg)
+    print('eye')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_week('public_life')
