79168ef51f69a044a02a90e56f74f2f4fc675ae7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 14:02:56 -0400

    "mv: work super/know_thing_with_hand/number/want_week/go_year_to_next_person/go_different_eye.py, super/work_old_place_by_woman/feel_great_woman/big_person/large_person.py, super/early_week_and_government.py or super/company/right_company_or_child.py with able part"

---
+++ super/company/right_company_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    part(str_arg)
+    print('old_way')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('look_own_life')
---
+++ super/know_thing_with_hand/number/want_week/go_year_to_next_person/go_different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_problem(str_arg):
+    have_able_eye(str_arg)
+    print('able_way')
+
+def have_able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_problem('different_work')
