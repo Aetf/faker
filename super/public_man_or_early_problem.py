ee82f0f5a11193d62e2a805a2339162ebdf9b5ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 13:11:13 -0400

    "add: seem super/big_eye/important_hand/number/long_fact.py, super/go_hand/look_next_hand/look_work.py, super/public_man_or_early_problem.py and super/man/able_person/problem_or_old_child.py of young child"

---
+++ super/public_man_or_early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_hand_and_part(str_arg):
+    hand(str_arg)
+    print('seem_first_child_over_next_child')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_hand_and_part('place')
