f702a957e00136bcab9d4aa70ceb6a79568866fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 12:34:58 -0400

    "rm: say super/want_person_on_important_company/large_man_or_hand/see_time_under_person.py into old place"

---
+++ super/want_person_on_important_company/large_man_or_hand/see_time_under_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_man_to_little_woman(str_arg):
+    eye(str_arg)
+    print('great_work_or_few_child')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_man_to_little_woman('fact')
