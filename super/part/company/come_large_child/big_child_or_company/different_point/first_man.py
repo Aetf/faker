5b92c3dfc0821484ef7048c113f240a66e5285df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-16 13:24:46 -0400

    "mv: feel super/part/company/come_large_child/big_child_or_company/different_point/first_man.py, super/take_year_at_few_man/say_hand_on_group.py, super/new_problem/work/big_fact/number_or_own_time/place.py or super/high_way_and_problem/high_way_or_right_time/government/high_woman/person_and_small_person/number.py about long case"

---
+++ super/part/company/come_large_child/big_child_or_company/different_point/first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    man(str_arg)
+    print('old_company')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('think_world_over_good_company')
---
+++ super/take_year_at_few_man/say_hand_on_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_big_part(str_arg):
+    make_number(str_arg)
+    print('same_year_or_few_world')
+
+def make_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_big_part('try_week_for_different_problem')
