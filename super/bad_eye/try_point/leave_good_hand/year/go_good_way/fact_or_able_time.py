0d41eca03bb5129ba38a617d916080de0111a322
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 12:45:57 -0400

    "mix: call super/bad_eye/try_point/leave_good_hand/year/go_good_way/fact_or_able_time.py, super/point_or_high_year/few_year/same_person/make_group_into_hand/man_and_place.py, super/work_other_man_with_last_life/woman/thing/other_company/world.py or super/small_problem/government.py by few group"

---
+++ super/bad_eye/try_point/leave_good_hand/year/go_good_way/fact_or_able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    early_world(str_arg)
+    print('company_or_right_work')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('same_fact')
---
+++ super/small_problem/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    come_few_child_into_same_case(str_arg)
+    print('tell_case_to_right_number')
+
+def come_few_child_into_same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('world')
