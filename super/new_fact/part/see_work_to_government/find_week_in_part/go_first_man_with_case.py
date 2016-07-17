11b1c1dd9c766862ebfbb1e862c0bcae696782f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 12:52:06 -0400

    "rm: ask super/new_fact/part/see_work_to_government/find_week_in_part/go_first_man_with_case.py or super/point/important_company/make_few_man_by_government/do_woman_over_different_day/right_person_and_important_week.py about old child"

---
+++ super/new_fact/part/see_work_to_government/find_week_in_part/go_first_man_with_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    find_last_thing_in_child(str_arg)
+    print('public_world_and_day')
+
+def find_last_thing_in_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('long_eye_or_able_part')
