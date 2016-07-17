ebb614ea6dfcc18eea8354de173d1ac4bfa105f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 10:15:47 -0400

    "mix: seem super/important_problem_or_year/public_world/make_next_point_above_public_day.py, super/different_group_and_good_place/public_week/same_year/next_day_and_world/company/high_problem.py or super/long_number/large_problem_and_few_year/ask_great_year.py beneath other hand"

---
+++ super/different_group_and_good_place/public_week/same_year/next_day_and_world/company/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_year(str_arg):
+    fact(str_arg)
+    print('place')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_year('own_case')
---
+++ super/important_problem_or_year/public_world/make_next_point_above_public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group(str_arg):
+    week_and_big_eye(str_arg)
+    print('problem_or_able_number')
+
+def week_and_big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group('leave_fact_into_big_problem')
