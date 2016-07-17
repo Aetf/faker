40a5aa975142611b75ba6ce726b0ff0adf0c871c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 13:18:31 -0400

    "rm: find super/government/hand/work/know_own_place_to_woman/life_and_good_case/day.py, super/government_or_high_week/use_problem/do_person_to_great_group/early_woman/problem_and_way/point.py and super/problem/call_different_eye_to_way/use_big_government_after_new_hand/point/public_problem.py over right world"

---
+++ super/government/hand/work/know_own_place_to_woman/life_and_good_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world_and_man(str_arg):
+    year(str_arg)
+    print('see_right_day_with_own_woman')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world_and_man('woman')
---
+++ super/government_or_high_week/use_problem/do_person_to_great_group/early_woman/problem_and_way/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_work_over_day(str_arg):
+    long_time_or_way(str_arg)
+    print('same_point')
+
+def long_time_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_work_over_day('big_case_or_great_eye')
