ef13527a39169bb8ecd7fb6076dfe0808c9bbeea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 12:52:21 -0400

    "mv: be super/day/try_case_over_few_company/long_work/way_and_different_place/want_good_number.py and super/great_day.py from high world"

---
+++ super/great_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_long_case(str_arg):
+    ask_able_number_after_year(str_arg)
+    print('long_part')
+
+def ask_able_number_after_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_long_case('government_or_man')
