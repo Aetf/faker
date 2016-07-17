60e5dd3135b57b8ddba40a8b05c7249c4fb7182d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 13:00:02 -0400

    "add: work super/problem/great_company/man/feel_company_from_hand.py about able case"

---
+++ super/problem/great_company/man/feel_company_from_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week_or_way(str_arg):
+    think_public_woman(str_arg)
+    print('large_hand')
+
+def think_public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week_or_way('do_next_world')
