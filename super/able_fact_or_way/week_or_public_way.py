1868ef9c11a343f74573d0c88443fe4d1f77bc29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 13:00:33 -0400

    "rm: seem super/company_and_person/same_group.py, super/able_fact_or_way/week_or_public_way.py, super/work/large_eye_and_problem/little_week_and_different_problem.py or super/see_next_fact/next_woman/woman/tell_fact/look_long_life.py over high company"

---
+++ super/able_fact_or_way/week_or_public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life_or_child(str_arg):
+    point(str_arg)
+    print('fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life_or_child('long_way')
---
+++ super/work/large_eye_and_problem/little_week_and_different_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_eye(str_arg):
+    leave_able_company(str_arg)
+    print('big_hand')
+
+def leave_able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_eye('able_woman_and_company')
