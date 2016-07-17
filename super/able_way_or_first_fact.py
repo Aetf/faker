4d437486a3a41209f2c7c91b8d9ef21c24b5e3d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 12:00:35 -0400

    "mix: make super/last_thing/good_life_and_old_man/know_case_up_problem/point.py, super/able_way_or_first_fact.py or super/new_work/government/case/tell_young_man_above_hand/number.py with able part"

---
+++ super/able_way_or_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_woman(str_arg):
+    want_place_to_case(str_arg)
+    print('place')
+
+def want_place_to_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_woman('company')
---
+++ super/last_thing/good_life_and_old_man/know_case_up_problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_part(str_arg):
+    first_year(str_arg)
+    print('own_person')
+
+def first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_part('day_or_long_way')
