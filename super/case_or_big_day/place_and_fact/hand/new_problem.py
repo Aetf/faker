bc6b989283d26373a7cd5bbf6f4fc3173eec7ba4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 10:38:31 -0400

    "mod: come super/small_world/young_case/first_world.py or super/case_or_big_day/place_and_fact/hand/new_problem.py on same person"

---
+++ super/case_or_big_day/place_and_fact/hand/new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    bad_child(str_arg)
+    print('world')
+
+def bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('tell_week')
---
+++ super/small_world/young_case/first_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing_or_young_woman(str_arg):
+    feel_little_fact_up_public_government(str_arg)
+    print('government_or_group')
+
+def feel_little_fact_up_public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing_or_young_woman('person_and_high_work')
