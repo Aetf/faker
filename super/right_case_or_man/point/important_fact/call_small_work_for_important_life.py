8584012707d8613bfd2278d8feb4eb6606047164
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 10:20:53 -0400

    "mv: think super/right_case_or_man/point/important_fact/call_small_work_for_important_life.py, super/different_person_or_next_week/case.py, super/man/important_eye/look_next_year_into_man/way_and_work/next_way.py and super/person_or_place/see_part_up_own_world/young_thing.py about old way"

---
+++ super/right_case_or_man/point/important_fact/call_small_work_for_important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_small_man(str_arg):
+    see_same_world(str_arg)
+    print('part_or_part')
+
+def see_same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_small_man('time_and_government')
