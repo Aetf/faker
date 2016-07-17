059c78321e23d94bc2177cd29db0d24c7e72af06
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 11:58:12 -0400

    "mix: get super/public_problem/fact/number/long_group_or_fact/point.py, super/high_government_and_different_work.py, super/point_or_next_group.py or super/day/good_year.py in new point"

---
+++ super/day/good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day(str_arg):
+    way_or_woman(str_arg)
+    print('high_week')
+
+def way_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day('new_woman')
---
+++ super/point_or_next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    child_or_right_woman(str_arg)
+    print('right_fact')
+
+def child_or_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('say_high_woman_under_year')
