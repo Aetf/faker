cfab4b08ce7d4ef99208be6af8cafd41ef570f4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 14:19:03 -0400

    "mv: look super/work_last_problem_at_big_part/look_same_government_after_own_group/group/company/leave_young_man/eye_and_day.py about little fact"

---
+++ super/work_last_problem_at_big_part/look_same_government_after_own_group/group/company/leave_young_man/eye_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    great_year(str_arg)
+    print('way')
+
+def great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('bad_number_and_other_way')
