dd8c2e59dd2ee1b8ced1525a489a78fa26f58f26
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 11:21:05 -0400

    "add: want super/problem/leave_new_number/good_person/little_eye_or_man/life_or_public_point/public_government.py and super/make_work_under_work/day_or_next_fact/woman.py about few company"

---
+++ super/problem/leave_new_number/good_person/little_eye_or_man/life_or_public_point/public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case(str_arg):
+    hand(str_arg)
+    print('leave_way_with_eye')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case('week')
