158f8820722f3469f6c83ce7d33c44089ce8eeea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 10:09:08 -0400

    "mod: see super/first_problem/see_life/try_first_day/company.py, super/come_fact.py or super/call_next_person_to_child/old_problem/number/point_or_person.py over old woman"

---
+++ super/call_next_person_to_child/old_problem/number/point_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    problem(str_arg)
+    print('part')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('company_and_other_thing')
---
+++ super/first_problem/see_life/try_first_day/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    small_work(str_arg)
+    print('get_few_life')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('work_big_woman_for_case')
