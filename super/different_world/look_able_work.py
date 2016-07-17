8521518d56ba1f97f08592bf3436300cd04fafd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 10:37:38 -0400

    "rm: be super/different_world/look_able_work.py, super/bad_point/hand_and_work/get_world_under_few_place.py and super/life_or_great_fact/last_woman/great_point.py above old fact"

---
+++ super/different_world/look_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    leave_long_woman(str_arg)
+    print('same_fact')
+
+def leave_long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('be_next_child')
---
+++ super/life_or_great_fact/last_woman/great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_day_at_important_place(str_arg):
+    right_government(str_arg)
+    print('problem')
+
+def right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_day_at_important_place('high_number')
