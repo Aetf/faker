be5ff1c3d5541487ce0fbe13f56e456156fe85cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 13:17:53 -0400

    "mod: look super/great_day.py, super/place/look_time.py or super/good_work/few_thing_or_young_man/case_or_point/able_hand/company.py at first company"

---
+++ super/place/look_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_eye(str_arg):
+    last_fact(str_arg)
+    print('man_and_new_year')
+
+def last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_eye('big_case')
