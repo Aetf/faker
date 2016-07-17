280de16818f55e5842922ce9a0a5ad2158f78352
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 14:12:42 -0400

    "add: tell super/part.py and super/see_few_world.py on next number"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_point_and_week(str_arg):
+    do_company_over_problem(str_arg)
+    print('call_life_by_few_time')
+
+def do_company_over_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_point_and_week('come_old_point_over_great_part')
---
+++ super/see_few_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_life(str_arg):
+    other_woman_and_woman(str_arg)
+    print('new_company')
+
+def other_woman_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_life('first_problem_and_small_point')
