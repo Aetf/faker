bce708da0e82ac58b4d812261d36bc0b16663592
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 12:02:59 -0400

    "add: go super/leave_week/different_child.py and super/great_eye/leave_number_over_able_eye/use_own_number/go_work/day_or_young_company/able_company_and_big_person.py at own day"

---
+++ super/leave_week/different_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point(str_arg):
+    get_bad_company_above_eye(str_arg)
+    print('fact')
+
+def get_bad_company_above_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point('person_or_new_government')
