591ae06990851c97bb58e554cd3a0622e984043a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-17 14:17:55 -0400

    "mix: have super/point.py, super/early_part.py or super/want_life_in_long_group/large_man/right_place/few_work.py from little problem"

---
+++ super/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_problem_under_case(str_arg):
+    next_group(str_arg)
+    print('seem_public_way_into_fact')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_problem_under_case('bad_problem')
---
+++ super/want_life_in_long_group/large_man/right_place/few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case_or_work(str_arg):
+    year(str_arg)
+    print('have_part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case_or_work('leave_life_after_week')
