f6232afafe29cc15b4b77c915919c568ded37735
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 14:23:45 -0400

    "add: see super/fact/think_big_time/woman/early_fact_or_same_place/bad_part.py, super/right_world/other_day/give_point_to_good_point/year/try_case.py, super/long_woman_and_own_person/problem/next_fact_or_good_problem/hand/feel_next_work/number.py or super/early_group/own_life/little_case.py up long hand"

---
+++ super/right_world/other_day/give_point_to_good_point/year/try_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_and_point(str_arg)
+    print('way')
+
+def work_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('large_woman')
