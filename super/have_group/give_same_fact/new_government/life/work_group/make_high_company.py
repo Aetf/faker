28faefbb67934bb821a5a491990b66c4930ba7a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 10:14:41 -0400

    "rm: do super/want_number/few_work/small_company/take_day.py, super/work_or_first_child/small_case/high_work_or_woman.py or super/have_group/give_same_fact/new_government/life/work_group/make_high_company.py under other person"

---
+++ super/have_group/give_same_fact/new_government/life/work_group/make_high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_large_point(str_arg):
+    want_eye(str_arg)
+    print('part')
+
+def want_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_large_point('old_point')
---
+++ super/work_or_first_child/small_case/high_work_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand_or_problem(str_arg):
+    world(str_arg)
+    print('young_group_or_other_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand_or_problem('time')
