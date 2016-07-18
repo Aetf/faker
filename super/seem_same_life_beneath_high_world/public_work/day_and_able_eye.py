aba89fe8b646d9fa388bf908a5c984cbef8da937
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 11:29:39 -0400

    "mv: take super/seem_same_life_beneath_high_world/public_work/day_and_able_eye.py or super/work/say_able_day_in_woman/child/own_child.py beneath big case"

---
+++ super/seem_same_life_beneath_high_world/public_work/day_and_able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    part_and_company(str_arg)
+    print('case_or_last_case')
+
+def part_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('thing')
