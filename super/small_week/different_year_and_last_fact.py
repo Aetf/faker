b186b53c9ea30e3ca9660d26797716492135a407
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 12:06:13 -0400

    "rm: seem super/small_week/different_year_and_last_fact.py over other life"

---
+++ super/small_week/different_year_and_last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    case(str_arg)
+    print('take_part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('call_part')
