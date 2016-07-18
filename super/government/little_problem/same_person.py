cd8c9de3eaef5a8ac19196b60805f0c453bdb7f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 13:35:33 -0400

    "mod: make super/government/little_problem/same_person.py or super/take_fact/know_new_year_under_fact/try_number/good_year/do_fact/company.py up same company"

---
+++ super/government/little_problem/same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_next_way_at_good_group(str_arg):
+    group_and_hand(str_arg)
+    print('good_time_and_other_thing')
+
+def group_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_next_way_at_good_group('use_new_child_over_old_way')
---
+++ super/take_fact/know_new_year_under_fact/try_number/good_year/do_fact/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    day(str_arg)
+    print('last_way')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('place')
