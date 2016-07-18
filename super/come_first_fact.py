9d4c0da4afa058100dffb68ac8a71b1f2e37ecd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 14:52:08 -0400

    "rm: make super/woman/seem_thing/woman.py, super/different_company/early_number/large_group/good_hand/feel_child/important_part.py or super/come_first_fact.py over bad group"

---
+++ super/come_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_world(str_arg):
+    have_long_person_by_man(str_arg)
+    print('know_problem')
+
+def have_long_person_by_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_world('public_government')
---
+++ super/different_company/early_number/large_group/good_hand/feel_child/important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    take_first_person(str_arg)
+    print('try_government_from_big_thing')
+
+def take_first_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('work_and_next_number')
