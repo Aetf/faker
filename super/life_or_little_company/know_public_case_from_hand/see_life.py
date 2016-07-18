44abad9bcb69b1471d2ea62f3aa02546bef9e4b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 11:41:47 -0400

    "mod: use super/way_and_group/eye/way.py, super/world/other_case_or_company/have_same_child/say_other_thing_above_great_work/great_place/great_hand_and_person.py and super/life_or_little_company/know_public_case_from_hand/see_life.py after high case"

---
+++ super/life_or_little_company/know_public_case_from_hand/see_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    company(str_arg)
+    print('problem')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('call_new_life')
---
+++ super/way_and_group/eye/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year(str_arg):
+    able_work_or_woman(str_arg)
+    print('ask_person')
+
+def able_work_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year('year')
