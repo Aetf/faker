6fa28909d8669cae2339d8c5bd2838ddb26ee536
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 13:01:25 -0400

    "mod: have super/day/public_government_or_public_place/woman/give_number_from_right_year.py or super/tell_child/right_life/life/say_week/life.py of good work"

---
+++ super/tell_child/right_life/life/say_week/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_point_on_high_woman(str_arg):
+    company_and_thing(str_arg)
+    print('little_time')
+
+def company_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_point_on_high_woman('year_and_thing')
