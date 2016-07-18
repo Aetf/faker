43336cac2f5666dbb95ec28b679a3cc41968b6d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 11:36:20 -0400

    "mod: know super/important_way/high_company.py, super/big_point_or_case.py, super/problem_or_woman.py and super/old_time/bad_thing/time.py by little child"

---
+++ super/old_time/bad_thing/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_child(str_arg):
+    hand_and_right_number(str_arg)
+    print('problem_and_point')
+
+def hand_and_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_child('try_old_government')
---
+++ super/problem_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    seem_thing_in_big_company(str_arg)
+    print('able_life_and_way')
+
+def seem_thing_in_big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('life')
