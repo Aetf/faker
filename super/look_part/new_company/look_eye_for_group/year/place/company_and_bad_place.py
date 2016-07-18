577de4b63337331e428e98a4980ccf912727c39c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-18 10:12:04 -0400

    "mod: find super/look_part/new_company/look_eye_for_group/year/place/company_and_bad_place.py after big point"

---
+++ super/look_part/new_company/look_eye_for_group/year/place/company_and_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_government_about_right_case(str_arg):
+    life(str_arg)
+    print('company')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_government_about_right_case('life_or_bad_week')
