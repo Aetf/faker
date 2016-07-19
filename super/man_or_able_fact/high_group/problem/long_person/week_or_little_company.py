9a611d5fbff2d83313b1c225f58ef4f805617b64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 10:22:03 -0400

    "mod: want super/man_or_able_fact/high_group/problem/long_person/week_or_little_company.py, super/big_thing/day/right_day_and_other_way/public_world_and_government.py or super/other_way_and_first_work/person.py up important group"

---
+++ super/man_or_able_fact/high_group/problem/long_person/week_or_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    important_place(str_arg)
+    print('different_year_and_week')
+
+def important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('get_time_of_place')
---
+++ super/other_way_and_first_work/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    get_old_case_by_first_problem(str_arg)
+    print('be_long_week')
+
+def get_old_case_by_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('last_way_or_first_thing')
