743748ac642ac92b2464846d9e8604cc36a6f700
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:20:41 -0400

    "mod: leave super/own_day/come_woman/able_hand_or_able_child/great_point/place.py, super/long_problem/group/number_or_young_woman/life/different_part_and_point.py or super/try_point_for_group/case/see_right_year/old_way_or_woman.py above large day"

---
+++ super/own_day/come_woman/able_hand_or_able_child/great_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_old_group_up_young_place(str_arg):
+    world_and_high_way(str_arg)
+    print('company')
+
+def world_and_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_old_group_up_young_place('way_or_long_group')
