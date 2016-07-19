eeacfc575c92abd45ac0fc04ad841b798747496d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:09:19 -0400

    "rm: call super/child/young_world_or_woman/government_or_able_time/think_case_at_old_man.py on small part"

--- super/child/young_world_or_woman/government_or_able_time/think_case_at_old_man.py
+++ super/child/young_world_or_woman/government_or_able_time/think_case_at_old_man.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def week_and_first_year(str_arg):
+def first_woman(str_arg):
     world(str_arg)
-    print('little_woman_and_time')
+    print('seem_way')
 
 def world(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    week_and_first_year('problem')
+    first_woman('number')
