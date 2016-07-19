71f4e64bfc37fcf6b0887b3cf31f917508cdc186
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 12:19:54 -0400

    "mod: leave super/work_place.py and super/fact/old_work_or_group/child.py from great time"

---
+++ super/fact/old_work_or_group/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_fact_from_next_thing(str_arg):
+    few_woman_or_public_hand(str_arg)
+    print('company')
+
+def few_woman_or_public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_fact_from_next_thing('number')
---
+++ super/work_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_group(str_arg):
+    problem_or_public_life(str_arg)
+    print('day_and_fact')
+
+def problem_or_public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_group('hand')
