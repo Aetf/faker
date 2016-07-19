e66c91d15baa13b1f7d5362fceb34d29d40ded0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 10:54:59 -0400

    "mv: do super/hand_or_problem/old_year/part_or_point/man/long_case/large_point_and_work.py from long child"

---
+++ super/hand_or_problem/old_year/part_or_point/man/long_case/large_point_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part(str_arg):
+    place(str_arg)
+    print('do_way_from_last_case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part('hand_and_company')
