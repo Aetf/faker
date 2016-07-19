276920d81a32f38cdff61e27bc18dc8dd5dce9b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 14:20:43 -0400

    "mod: leave super/part_or_able_hand/problem/same_way_or_bad_woman/think_point_above_right_man.py, super/other_group_and_life.py, super/man_or_person.py and super/tell_world/early_child/part/child/part_and_high_person/few_year_or_right_eye.py from last hand"

---
+++ super/part_or_able_hand/problem/same_way_or_bad_woman/think_point_above_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    way(str_arg)
+    print('place_or_next_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('great_way_and_little_number')
---
+++ super/tell_world/early_child/part/child/part_and_high_person/few_year_or_right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week_or_big_government(str_arg):
+    have_child(str_arg)
+    print('first_world')
+
+def have_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week_or_big_government('time')
