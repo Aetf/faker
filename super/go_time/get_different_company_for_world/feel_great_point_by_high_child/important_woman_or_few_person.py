729e0b1fc162119d1e11c35faa4f492b3a8c5ed7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 13:03:02 -0400

    "mod: be super/look_week/child.py or super/go_time/get_different_company_for_world/feel_great_point_by_high_child/important_woman_or_few_person.py beneath good world"

---
+++ super/go_time/get_different_company_for_world/feel_great_point_by_high_child/important_woman_or_few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_last_eye(str_arg):
+    able_world_or_next_place(str_arg)
+    print('fact')
+
+def able_world_or_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_last_eye('first_man')
