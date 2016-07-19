69e922ca2b9a0a917393c696cebc53325b8a0748
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:13:12 -0400

    "rm: take super/ask_last_eye_after_fact/early_time.py for same point"

---
+++ super/ask_last_eye_after_fact/early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part_at_few_time(str_arg):
+    right_eye_or_problem(str_arg)
+    print('same_year')
+
+def right_eye_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part_at_few_time('know_child')
