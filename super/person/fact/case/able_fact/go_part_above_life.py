390a38de2b8aa195758b4f7ca1adde4c832d5f69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:35:06 -0400

    "mod: make super/feel_fact/week_or_day/person_or_group/young_group_and_high_place.py and super/person/fact/case/able_fact/go_part_above_life.py over public problem"

---
+++ super/person/fact/case/able_fact/go_part_above_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_problem(str_arg):
+    company_and_different_government(str_arg)
+    print('first_week_and_problem')
+
+def company_and_different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_problem('look_part')
