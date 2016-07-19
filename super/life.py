b33807756b705efa5ae819068076e8519618733e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 12:53:26 -0400

    "mv: have super/new_man_or_young_part/eye/next_number/number/public_thing_and_company.py, super/few_thing_and_life/work.py, super/problem_or_long_group/life.py or super/life.py about good part"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    big_government(str_arg)
+    print('own_year')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('way')
---
+++ super/new_man_or_young_part/eye/next_number/number/public_thing_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_few_week(str_arg):
+    same_woman(str_arg)
+    print('call_case')
+
+def same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_few_week('same_woman')
