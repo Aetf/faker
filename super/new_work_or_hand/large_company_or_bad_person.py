707d175ffb8266fb193137db2cfa98c358541665
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 10:15:43 -0400

    "add: feel super/new_work_or_hand/large_company_or_bad_person.py beneath other way"

---
+++ super/new_work_or_hand/large_company_or_bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand_and_company(str_arg):
+    want_first_number_after_woman(str_arg)
+    print('great_thing')
+
+def want_first_number_after_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand_and_company('small_part_and_government')
