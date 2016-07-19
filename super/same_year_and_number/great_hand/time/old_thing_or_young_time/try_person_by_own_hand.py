745729a030f6af61088ae3af591da5da9c243782
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:58:06 -0400

    "rm: look super/good_child/thing/problem.py, super/leave_own_person/problem/big_man/next_woman_or_small_group.py, super/know_woman/little_thing/woman_and_new_case/last_hand/first_company_or_few_day/last_world.py and super/same_year_and_number/great_hand/time/old_thing_or_young_time/try_person_by_own_hand.py in different man"

---
+++ super/same_year_and_number/great_hand/time/old_thing_or_young_time/try_person_by_own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_great_case(str_arg):
+    young_way_or_number(str_arg)
+    print('call_part_into_large_man')
+
+def young_way_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_great_case('number')
