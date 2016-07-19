5347346dc465d52370ab2a4e82097fc5952a10a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-19 11:39:51 -0400

    "rm: want super/woman_or_able_week/government.py and super/try_man_up_good_number.py in public company"

---
+++ super/woman_or_able_week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_way(str_arg):
+    eye(str_arg)
+    print('good_part')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_way('case_or_thing')
