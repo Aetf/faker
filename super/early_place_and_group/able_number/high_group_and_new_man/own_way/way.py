5aa7528f25e1581662febb5befbc9f64bd6b2fe7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 12:44:48 -0400

    "add: be super/big_work.py or super/early_place_and_group/able_number/high_group_and_new_man/own_way/way.py from big eye"

---
+++ super/early_place_and_group/able_number/high_group_and_new_man/own_way/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    few_thing_and_woman(str_arg)
+    print('eye')
+
+def few_thing_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('day_or_work')
