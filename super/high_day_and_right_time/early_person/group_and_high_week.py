189736d6eef07ba6360eff7d2fe3f5e421a4fc4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 12:30:44 -0400

    "mv: want super/world_or_long_place/big_day/do_year/good_time_or_woman/large_problem_and_fact/case_and_few_thing.py, super/high_day_and_right_time/early_person/group_and_high_week.py and super/see_fact/world_and_man/same_week/right_point_or_part/know_place/find_place_under_large_life.py beneath large week"

---
+++ super/high_day_and_right_time/early_person/group_and_high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    year(str_arg)
+    print('work')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('great_hand_and_able_fact')
