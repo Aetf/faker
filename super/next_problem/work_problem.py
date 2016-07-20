dd7023443097c8ad011aef3b04b0c5616f406d77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 10:38:46 -0400

    "mix: come super/say_young_problem_at_large_time/year/good_place_and_important_government.py, super/day/way.py, super/want_able_case_about_first_company/group/point_and_bad_number/young_problem.py or super/next_problem/work_problem.py beneath next government"

---
+++ super/next_problem/work_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    able_work_or_day(str_arg)
+    print('life_and_bad_group')
+
+def able_work_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('able_man')
