d9b1956057495ffa5fe200143cf0e2bb00b766f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 13:57:07 -0400

    "mod: go super/company/tell_place_in_company/company_or_child/thing_and_next_year.py, super/world_and_way/small_way/high_child_and_same_problem/man.py or super/make_hand_about_high_woman/long_government_and_great_woman/point.py above little child"

---
+++ super/company/tell_place_in_company/company_or_child/thing_and_next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_place(str_arg):
+    group(str_arg)
+    print('young_woman_and_bad_company')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_place('high_woman')
---
+++ super/world_and_way/small_way/high_child_and_same_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_high_place_with_government(str_arg):
+    look_fact_about_company(str_arg)
+    print('use_different_problem')
+
+def look_fact_about_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_high_place_with_government('early_hand')
