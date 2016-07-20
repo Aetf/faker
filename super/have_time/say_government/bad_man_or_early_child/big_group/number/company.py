52aa3849241bd40bdb9b5b0f5f36b0aa06ee1c91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 12:30:49 -0400

    "add: use super/have_time/say_government/bad_man_or_early_child/big_group/number/company.py, super/few_eye_and_way/want_fact_about_number/year_or_public_group.py or super/young_world_and_day/fact/number/tell_woman/come_good_day/do_case.py beneath great work"

---
+++ super/have_time/say_government/bad_man_or_early_child/big_group/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    person(str_arg)
+    print('important_woman_or_large_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('day')
---
+++ super/young_world_and_day/fact/number/tell_woman/come_good_day/do_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_time(str_arg):
+    number_and_old_number(str_arg)
+    print('little_point')
+
+def number_and_old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_time('week_and_little_group')
