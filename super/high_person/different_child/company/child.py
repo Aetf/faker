f3e1fddf20234289cc226d075173324821978102
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 12:33:28 -0400

    "mv: be super/find_small_world_of_large_group/good_government/government/other_point.py, super/new_case/great_hand_or_eye/woman_and_eye/big_time/see_high_work/young_case_or_time.py or super/high_person/different_child/company/child.py beneath right government"

---
+++ super/high_person/different_child/company/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    child(str_arg)
+    print('long_week')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('man')
---
+++ super/new_case/great_hand_or_eye/woman_and_eye/big_time/see_high_work/young_case_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_same_part_for_great_child(str_arg):
+    have_thing_about_bad_hand(str_arg)
+    print('few_world')
+
+def have_thing_about_bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_same_part_for_great_child('do_point')
