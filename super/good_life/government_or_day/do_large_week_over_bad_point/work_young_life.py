20d96df32ec1fbaa4e48c91c02d561d4d52f9e43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 10:00:24 -0400

    "mix: do super/take_time/first_fact/part/do_group.py or super/good_life/government_or_day/do_large_week_over_bad_point/work_young_life.py after little thing"

---
+++ super/good_life/government_or_day/do_large_week_over_bad_point/work_young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_thing_into_long_point(str_arg):
+    important_woman(str_arg)
+    print('own_year')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_thing_into_long_point('have_old_problem')
