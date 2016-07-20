d366ebbc6e2edb6e93817e76e4ded042675b7686
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 13:09:11 -0400

    "mod: have super/come_important_company_to_woman/say_own_case_up_work/man_or_place/want_different_problem_for_place/week/big_person.py, super/man/know_number/hand/woman/find_case/say_year_from_group.py or super/first_week_and_person/go_different_problem_into_child/day_and_new_person/last_man/use_new_way_beneath_big_government/bad_hand.py over first man"

---
+++ super/first_week_and_person/go_different_problem_into_child/day_and_new_person/last_man/use_new_way_beneath_big_government/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_part(str_arg):
+    same_life_and_important_place(str_arg)
+    print('case_or_first_day')
+
+def same_life_and_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_part('own_thing_and_good_person')
