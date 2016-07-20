77b00e58975d7808699c493c21a509c5c8891f89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 10:56:05 -0400

    "mod: see super/great_case/next_world/fact/last_man_or_right_government/see_company.py and super/tell_number_in_other_man/big_week/new_time.py beneath new place"

---
+++ super/great_case/next_world/fact/last_man_or_right_government/see_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye(str_arg):
+    first_man(str_arg)
+    print('think_young_point_up_long_number')
+
+def first_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye('see_group')
