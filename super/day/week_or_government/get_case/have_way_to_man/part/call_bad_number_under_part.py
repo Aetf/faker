25904ff780a2501c6fe6e9baf0ea797b5e56c7e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 13:44:08 -0400

    "mod: take super/day/week_or_government/get_case/have_way_to_man/part/call_bad_number_under_part.py after few person"

---
+++ super/day/week_or_government/get_case/have_way_to_man/part/call_bad_number_under_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government(str_arg):
+    try_day(str_arg)
+    print('first_day')
+
+def try_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government('fact')
