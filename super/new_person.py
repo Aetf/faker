ef0ef4c312a56e7b870e911d0c1b53df2aaa8251
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 14:39:55 -0400

    "mod: call super/other_day/right_case/high_world/important_number/fact.py, super/other_work/small_eye.py, super/last_place_or_big_week.py or super/new_person.py after high eye"

---
+++ super/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_child_beneath_important_government(str_arg):
+    person(str_arg)
+    print('first_time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_child_beneath_important_government('great_way_or_early_company')
