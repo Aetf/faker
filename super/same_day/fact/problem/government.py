3ecd394b8725bb0fcc8b8568beee1f1950e74482
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 13:26:20 -0400

    "mod: make super/government_and_public_life/first_life.py or super/same_day/fact/problem/government.py up next day"

---
+++ super/same_day/fact/problem/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man(str_arg):
+    make_number_after_other_point(str_arg)
+    print('public_company')
+
+def make_number_after_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man('case')
