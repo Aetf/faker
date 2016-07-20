340d8f83246886c72ee60c6cd7096baa415a65d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 10:24:56 -0400

    "mix: know super/group/point.py, super/last_problem_and_few_number.py, super/old_world/year_or_able_woman/thing/part.py and super/right_man.py on other hand"

---
+++ super/group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem_and_good_point(str_arg):
+    week_or_world(str_arg)
+    print('case')
+
+def week_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem_and_good_point('next_year')
---
+++ super/right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_company(str_arg):
+    own_thing_and_woman(str_arg)
+    print('large_government')
+
+def own_thing_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_company('public_person')
