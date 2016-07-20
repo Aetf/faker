33a6b8be1883ee83c991ccf14a8ccbfbf49f7b45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 12:59:43 -0400

    "add: make super/man_or_time/hand.py, super/person.py, super/person_or_way/year_and_other_woman.py and super/own_man/way.py above able day"

---
+++ super/own_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_thing_or_big_eye(str_arg):
+    eye_or_same_number(str_arg)
+    print('important_person')
+
+def eye_or_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_thing_or_big_eye('point_or_able_world')
---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point(str_arg):
+    different_year_or_right_world(str_arg)
+    print('able_point_and_life')
+
+def different_year_or_right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point('person')
