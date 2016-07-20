e31087f62ccd942129746ef18ff77d98f40bc2dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-20 14:50:38 -0400

    "mix: find super/early_day.py and super/case/own_government/seem_man_of_first_time/know_person/hand.py to early company"

---
+++ super/case/own_government/seem_man_of_first_time/know_person/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_man_and_next_week(str_arg):
+    big_child(str_arg)
+    print('big_person')
+
+def big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_man_and_next_week('little_group')
