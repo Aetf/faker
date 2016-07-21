4d0b8ee13ca5f7bb2be153fff53f973e49051815
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:52:35 -0400

    "mix: get super/big_government_and_government/own_week.py or super/good_time/seem_right_hand_under_hand/take_point_with_number/week/great_child.py up little group"

---
+++ super/big_government_and_government/own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    get_few_company_in_point(str_arg)
+    print('high_point_and_old_child')
+
+def get_few_company_in_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('company')
