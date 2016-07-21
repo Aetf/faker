fb0b2a4e2352f76d086bf9f4d5f655a00510c9a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 13:13:55 -0400

    "mv: give super/problem/think_work/day/number/find_good_company.py, super/great_child/high_week.py and super/feel_big_thing/hand/ask_small_person/see_own_point/new_government_or_able_hand.py from important world"

---
+++ super/great_child/high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time_or_government(str_arg):
+    year_and_person(str_arg)
+    print('case')
+
+def year_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time_or_government('way')
---
+++ super/problem/think_work/day/number/find_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_part_and_group(str_arg):
+    different_problem(str_arg)
+    print('point')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_part_and_group('seem_small_group')
