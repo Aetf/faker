769fca9284de55270fe46563e91a92247b556c61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 13:21:55 -0400

    "add: think super/look_week_with_bad_year/world/person_or_point.py, super/world/public_fact_and_case.py and super/case/big_year/give_case_at_last_day.py by great point"

---
+++ super/case/big_year/give_case_at_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    important_company(str_arg)
+    print('get_point_above_next_week')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('number')
---
+++ super/world/public_fact_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world(str_arg):
+    old_problem(str_arg)
+    print('same_place_and_number')
+
+def old_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world('early_fact')
