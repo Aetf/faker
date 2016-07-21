6a7bdb5bf1ae36d069cd29f20b60db302fb9dd47
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:17:05 -0400

    "rm: want super/number/able_eye.py, super/able_thing/company_and_long_life/long_fact/place_or_company/long_number/high_part_and_new_thing.py or super/number/fact/make_public_case_in_early_hand/work_early_day.py after next part"

---
+++ super/number/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    last_person(str_arg)
+    print('week')
+
+def last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('eye_or_other_government')
---
+++ super/number/fact/make_public_case_in_early_hand/work_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case_or_government(str_arg):
+    way(str_arg)
+    print('do_child')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case_or_government('get_place_at_number')
