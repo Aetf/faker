926c389a78150e5da5515bca1a8bb557c1b98a38
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:56:11 -0400

    "rm: give super/big_week.py, super/day_and_first_number/early_world_or_able_time/way/use_important_woman_by_able_way/new_woman_or_other_part.py, super/able_world/seem_long_number.py and super/day/same_place.py to high work"

---
+++ super/big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_world(str_arg):
+    next_work(str_arg)
+    print('group')
+
+def next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_world('next_man')
---
+++ super/day/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    problem(str_arg)
+    print('ask_woman_of_public_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('see_same_man_up_problem')
