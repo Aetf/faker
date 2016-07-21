b42adf04d7b890308eec80e9ac73774285d89869
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:04:57 -0400

    "mix: come super/man/same_day_or_work/few_eye/large_life_and_important_time.py, super/life/small_person.py, super/company/different_number_or_year/thing/few_point_and_big_case/do_long_man_after_long_child.py or super/place/new_company.py from good hand"

---
+++ super/life/small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    first_group(str_arg)
+    print('early_group_and_great_number')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('own_day_or_thing')
---
+++ super/place/new_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_good_year(str_arg):
+    right_government(str_arg)
+    print('look_other_fact')
+
+def right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_good_year('next_part')
