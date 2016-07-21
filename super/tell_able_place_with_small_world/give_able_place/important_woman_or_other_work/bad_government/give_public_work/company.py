dd51712e51fdf951055773d5a752531c0219b439
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 13:25:08 -0400

    "rm: tell super/tell_able_place_with_small_world/give_able_place/important_woman_or_other_work/bad_government/give_public_work/company.py into other person"

---
+++ super/tell_able_place_with_small_world/give_able_place/important_woman_or_other_work/bad_government/give_public_work/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work(str_arg):
+    group(str_arg)
+    print('point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work('part_or_man')
