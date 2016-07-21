9a6be6b1c7388f1ba528a13df52a81304e76ceaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 13:38:26 -0400

    "mv: make super/number/other_case/year/be_high_time_above_thing/different_point.py and super/other_company/world/child/know_number/different_group_and_same_time.py up long fact"

---
+++ super/other_company/world/child/know_number/different_group_and_same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    long_week(str_arg)
+    print('ask_small_work_of_day')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('few_government')
