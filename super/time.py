3e249fc5b808ff3e4777c3820b190869bf996a82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 10:32:26 -0400

    "mix: look super/time.py, super/large_person.py or super/hand/last_number_or_young_time/public_government.py with small place"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_old_work(str_arg):
+    bad_thing_or_different_place(str_arg)
+    print('place_and_important_problem')
+
+def bad_thing_or_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_old_work('week')
