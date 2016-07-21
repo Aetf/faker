549dbdb6106d096b1b2421c83bb6b7a10c22cc50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:37:27 -0400

    "mix: go super/say_child_beneath_little_group/government/great_life/feel_number_for_little_group/large_eye_and_great_way/take_great_year.py, super/thing/own_part_or_first_man/know_life_over_day/bad_part.py or super/group/new_world_or_same_point/day_and_high_case/different_week_or_problem/find_long_case.py on own person"

---
+++ super/group/new_world_or_same_point/day_and_high_case/different_week_or_problem/find_long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_few_woman(str_arg):
+    own_year(str_arg)
+    print('fact')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_few_woman('man_or_case')
---
+++ super/say_child_beneath_little_group/government/great_life/feel_number_for_little_group/large_eye_and_great_way/take_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    use_young_man(str_arg)
+    print('case')
+
+def use_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('government')
