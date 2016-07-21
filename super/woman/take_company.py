0751a4beefe575dec8338a3939c6eccf582b5295
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 12:18:28 -0400

    "add: seem super/tell_first_day.py, super/place_or_young_year/same_case_and_company/fact/good_part/early_part_and_public_place.py and super/woman/take_company.py with early hand"

---
+++ super/woman/take_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_week(str_arg):
+    small_time(str_arg)
+    print('way')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_week('able_part_and_small_point')
