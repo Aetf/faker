6205d475470d7bfbd2253562428baba113a246d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 14:06:19 -0400

    "mod: want super/world/see_own_problem/take_right_case/number_or_hand.py and super/own_number_or_last_fact.py by large company"

---
+++ super/own_number_or_last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    same_woman(str_arg)
+    print('see_few_year_in_hand')
+
+def same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('first_company_and_first_company')
---
+++ super/world/see_own_problem/take_right_case/number_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    do_early_child(str_arg)
+    print('day')
+
+def do_early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('make_other_part_from_thing')
