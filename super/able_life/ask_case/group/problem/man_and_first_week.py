0e345000b6a8131f5659129253975e73155227cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 12:10:42 -0400

    "rm: take super/think_company_beneath_public_number/ask_early_point_of_good_world/next_part/right_time_or_good_group.py, super/want_different_case/other_way_and_woman/take_long_way/public_man/little_number.py, super/seem_hand_under_way.py or super/able_life/ask_case/group/problem/man_and_first_week.py up same person"

---
+++ super/able_life/ask_case/group/problem/man_and_first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world(str_arg):
+    few_point(str_arg)
+    print('world_and_point')
+
+def few_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world('try_new_hand_beneath_life')
