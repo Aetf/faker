3308d31dcf06220ae2acd7d7352bcba6eb03f969
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-21 10:27:34 -0400

    "mod: look super/part/little_case.py by large man"

---
+++ super/part/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    bad_day(str_arg)
+    print('day')
+
+def bad_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('feel_own_man')
