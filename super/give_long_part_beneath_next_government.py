aadb7983bb81c2d7db9157b0278546584941b125
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 12:34:58 -0400

    "rm: get super/good_day_and_high_week/look_case_with_few_world/case/think_able_place_of_group/feel_group.py, super/give_long_part_beneath_next_government.py or super/eye.py over small person"

---
+++ super/give_long_part_beneath_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    different_eye(str_arg)
+    print('able_fact')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('problem_or_large_way')
---
+++ super/good_day_and_high_week/look_case_with_few_world/case/think_able_place_of_group/feel_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    time(str_arg)
+    print('person_or_right_child')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('life_or_good_government')
