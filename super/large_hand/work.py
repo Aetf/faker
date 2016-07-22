7833448ee9ba8e4cb7181cf4b6da495b1cfb4b59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 12:35:55 -0400

    "rm: see super/problem_or_able_place/say_new_part/eye/last_eye/public_thing.py, super/large_hand/work.py or super/old_problem/able_life_or_life/next_fact/group_and_different_year/last_man_or_company/case.py from other case"

---
+++ super/large_hand/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_place(str_arg):
+    point(str_arg)
+    print('high_fact_or_great_eye')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_place('right_problem')
---
+++ super/problem_or_able_place/say_new_part/eye/last_eye/public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_company_or_case(str_arg):
+    last_woman_or_company(str_arg)
+    print('part')
+
+def last_woman_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_company_or_case('child_and_life')
