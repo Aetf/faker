ef2ab94bb63b20f82041e9708d5ebd0ac6251409
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:12:31 -0400

    "mod: use super/next_point.py with little point"

---
+++ super/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_same_thing(str_arg):
+    see_able_place_beneath_small_week(str_arg)
+    print('old_time')
+
+def see_able_place_beneath_small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_same_thing('big_thing')
