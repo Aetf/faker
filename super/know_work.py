f99ec0a8aeb036cd7363427400728b96c3f29235
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:35:56 -0400

    "add: ask super/know_work.py at few government"

---
+++ super/know_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_case(str_arg):
+    long_thing_and_little_week(str_arg)
+    print('think_public_year')
+
+def long_thing_and_little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_case('week_and_long_part')
