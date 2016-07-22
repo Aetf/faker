85dadd0ebaed71f1652da3d54fa53d0bd5f6605a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 10:32:08 -0400

    "mod: look super/high_part.py after young person"

---
+++ super/high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_long_company(str_arg):
+    other_week(str_arg)
+    print('child')
+
+def other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_long_company('tell_case_of_way')
