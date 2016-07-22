1f2c50d244089ba6994b227f1118ca3296ffe445
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 10:08:26 -0400

    "mod: take super/world/use_first_place_above_time/next_place/number_and_little_woman/person/important_work_or_public_eye.py, super/use_next_person_into_government.py and super/want_same_thing/few_problem.py after public time"

---
+++ super/use_next_person_into_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    young_life(str_arg)
+    print('woman_and_first_time')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('case_and_able_way')
