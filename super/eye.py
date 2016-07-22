96fbdd5821c3b0f9628206a3ef7a13b4929ea564
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:05:06 -0400

    "add: leave super/eye.py or super/public_way_or_early_company.py into large year"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    early_case_or_week(str_arg)
+    print('high_number')
+
+def early_case_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('large_fact_and_number')
---
+++ super/public_way_or_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_good_thing(str_arg):
+    hand_and_hand(str_arg)
+    print('first_thing_or_fact')
+
+def hand_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_good_thing('do_case_by_public_person')
