a2cc7809255ae4fc3f729213f879cf270f541156
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:44:30 -0400

    "rm: want super/old_woman_and_point/first_company_or_long_company/important_week/know_new_time/last_hand.py, super/know_different_company_from_old_life/thing_or_work/important_life.py, super/say_right_year_by_same_government/come_week/other_government_and_long_woman/tell_right_child_under_next_thing/tell_same_life.py or super/problem.py over different number"

---
+++ super/know_different_company_from_old_life/thing_or_work/important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child(str_arg):
+    same_company_and_important_day(str_arg)
+    print('large_world')
+
+def same_company_and_important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child('same_thing')
---
+++ super/old_woman_and_point/first_company_or_long_company/important_week/know_new_time/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    woman(str_arg)
+    print('long_company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('life')
