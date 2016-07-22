73db0a0f62a956d8b9ffc0cde8a0ea4cbf9343d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 10:34:19 -0400

    "add: ask super/last_day_and_way/number/feel_child.py, super/woman.py, super/small_time_and_part/see_company_about_way/bad_time/fact_and_big_man.py and super/last_place/year_and_world/thing/group/point.py up own week"

---
+++ super/small_time_and_part/see_company_about_way/bad_time/fact_and_big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_early_day(str_arg):
+    feel_part(str_arg)
+    print('different_week_and_first_hand')
+
+def feel_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_early_day('important_time_or_thing')
