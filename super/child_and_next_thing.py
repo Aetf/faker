41a76c4fd607d9045f967089651ffc210b8088d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 11:26:37 -0400

    "mod: think super/child_and_next_thing.py and super/see_great_government/big_eye_and_company/case/group_or_big_number/little_person.py at small number"

---
+++ super/child_and_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_point(str_arg):
+    know_small_week_about_time(str_arg)
+    print('group')
+
+def know_small_week_about_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_point('high_place_or_time')
