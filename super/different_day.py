ff07ce939326c8a4922f98b13e0450ed5ba51012
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:35:12 -0400

    "add: look super/long_person/little_hand.py, super/different_day.py, super/world_or_life.py and super/feel_group/fact/case/old_week_and_group.py of long week"

---
+++ super/different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    day(str_arg)
+    print('same_part')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('new_government')
---
+++ super/feel_group/fact/case/old_week_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    thing_and_thing(str_arg)
+    print('look_person_at_long_place')
+
+def thing_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('point_and_high_group')
