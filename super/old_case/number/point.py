f3da804682e72cf9c671be271bdcc2ea8041b1c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 14:53:20 -0400

    "mv: leave super/old_case/number/point.py over last work"

---
+++ super/old_case/number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_problem(str_arg):
+    company(str_arg)
+    print('want_part')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_problem('early_day')
