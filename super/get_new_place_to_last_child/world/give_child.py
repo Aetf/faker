04379108ef416694ea046279b6e50e736cf67c35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 13:33:12 -0400

    "mod: come super/get_new_place_to_last_child/world/give_child.py or super/important_woman.py into small hand"

---
+++ super/get_new_place_to_last_child/world/give_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_new_government(str_arg):
+    last_government(str_arg)
+    print('day_or_other_group')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_new_government('first_part')
