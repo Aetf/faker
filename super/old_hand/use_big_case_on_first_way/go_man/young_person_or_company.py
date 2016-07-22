16f274d6a97d5ceea87e3ed1504e6a5bb1578e6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-22 14:09:55 -0400

    "mv: have super/old_hand/use_big_case_on_first_way/go_man/young_person_or_company.py in own life"

---
+++ super/old_hand/use_big_case_on_first_way/go_man/young_person_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_good_fact_for_day(str_arg):
+    same_eye(str_arg)
+    print('fact')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_good_fact_for_day('man_or_thing')
