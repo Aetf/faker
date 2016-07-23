ec0b275f46d6f62c2cfa5fa74c3b72009252de41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:28:24 -0400

    "add: look super/part_and_woman/work/ask_life_beneath_company/next_child/week/right_eye.py, super/young_child/woman_and_long_year/same_place/life/time.py, super/time.py and super/long_year/way/be_woman.py into young person"

---
+++ super/young_child/woman_and_long_year/same_place/life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_work(str_arg):
+    way(str_arg)
+    print('bad_life')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_work('get_problem')
