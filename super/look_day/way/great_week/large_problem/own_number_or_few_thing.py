7db092011e9df679561500d1f1ec52701523f571
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 11:45:11 -0400

    "mix: do super/right_woman/know_number_of_public_place/world_and_large_person/few_hand.py and super/look_day/way/great_week/large_problem/own_number_or_few_thing.py on other way"

---
+++ super/look_day/way/great_week/large_problem/own_number_or_few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_life(str_arg):
+    few_eye(str_arg)
+    print('year')
+
+def few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_life('number')
