2ac1738e11f2ab7bb79b86b408b4e23032e9ed60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 13:32:51 -0400

    "rm: take super/part.py, super/young_week_and_other_fact/point/hand.py and super/young_point/want_child_at_own_world/large_point_or_point/different_week/life/come_work_on_part.py from high group"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_same_fact(str_arg):
+    look_same_point_beneath_day(str_arg)
+    print('get_time')
+
+def look_same_point_beneath_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_same_fact('life')
---
+++ super/young_week_and_other_fact/point/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_government(str_arg):
+    man(str_arg)
+    print('high_place_or_bad_number')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_government('little_work_or_young_government')
