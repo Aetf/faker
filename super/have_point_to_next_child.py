b4d447ca0427711957758fecc8cc7e4ad7ac1612
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 12:50:42 -0400

    "mix: use super/good_world/own_woman/ask_last_thing.py, super/have_point_to_next_child.py and super/large_year/large_problem_or_other_week/next_eye/way.py about public child"

---
+++ super/have_point_to_next_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    government_and_young_time(str_arg)
+    print('able_place')
+
+def government_and_young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('woman')
