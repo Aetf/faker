652edf0d3cd6e86d20cccf7817e569fb79674a9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:55:19 -0400

    "mv: call super/first_number.py, super/make_man_over_own_case/try_last_world_of_time/able_group_or_next_hand/first_place/group/feel_eye_at_right_number.py, super/part.py and super/small_case_and_work/important_number/see_good_fact.py from few week"

---
+++ super/make_man_over_own_case/try_last_world_of_time/able_group_or_next_hand/first_place/group/feel_eye_at_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year_for_fact(str_arg):
+    get_life_of_small_way(str_arg)
+    print('important_company')
+
+def get_life_of_small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year_for_fact('get_eye')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way_and_place(str_arg):
+    same_life(str_arg)
+    print('first_year_or_different_man')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way_and_place('find_able_case')
