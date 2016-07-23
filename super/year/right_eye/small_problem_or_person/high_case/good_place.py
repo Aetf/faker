d510acdabea86927e39075d62fc0e152eae87ef4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:15:33 -0400

    "rm: call super/year/right_eye/small_problem_or_person/high_case/good_place.py by early way"

---
+++ super/year/right_eye/small_problem_or_person/high_case/good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_other_person(str_arg):
+    leave_fact_above_man(str_arg)
+    print('few_government')
+
+def leave_fact_above_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_other_person('large_case')
