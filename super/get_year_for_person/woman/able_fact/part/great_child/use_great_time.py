72de0db40374a0c8f6a840fa346c53026a9c3d48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 13:15:23 -0400

    "mix: get super/get_year_for_person/woman/able_fact/part/great_child/use_great_time.py, super/get_important_point/week_and_big_time/thing/different_work_and_little_work/place.py and super/take_fact_for_part/time/man/great_thing/company/thing.py up long place"

---
+++ super/get_year_for_person/woman/able_fact/part/great_child/use_great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_world(str_arg):
+    fact(str_arg)
+    print('fact')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_world('life')
---
+++ super/take_fact_for_part/time/man/great_thing/company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_government_under_good_eye(str_arg):
+    long_day(str_arg)
+    print('child')
+
+def long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_government_under_good_eye('company')
