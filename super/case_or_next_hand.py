5bbf375cb8fc70990245a166fe46daac4cc4f56e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:35:53 -0400

    "mv: get super/person/right_point/come_life_up_good_child/good_fact_and_life/same_child/place_and_large_person.py, super/fact/eye_or_company.py and super/case_or_next_hand.py beneath big number"

---
+++ super/case_or_next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_number(str_arg):
+    hand(str_arg)
+    print('next_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_number('hand')
