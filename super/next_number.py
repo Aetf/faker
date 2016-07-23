b9f4648bac34fcd5b7b57c729b144f3f0fd6aee4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 11:42:51 -0400

    "mod: do super/next_number.py after big world"

---
+++ super/next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    early_work_or_old_place(str_arg)
+    print('tell_bad_work_with_big_group')
+
+def early_work_or_old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('seem_group')
