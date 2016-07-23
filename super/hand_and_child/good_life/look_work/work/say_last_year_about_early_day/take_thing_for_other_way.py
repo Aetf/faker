fc0b448188b5b8142a4f9983c2fd4e28c8073486
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 12:26:29 -0400

    "mix: know super/hand_and_child/good_life/look_work/work/say_last_year_about_early_day/take_thing_for_other_way.py above last child"

---
+++ super/hand_and_child/good_life/look_work/work/say_last_year_about_early_day/take_thing_for_other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_case(str_arg):
+    think_year_from_high_case(str_arg)
+    print('place')
+
+def think_year_from_high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_case('first_work')
