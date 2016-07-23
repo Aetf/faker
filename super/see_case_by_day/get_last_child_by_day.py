afe95b43176966c6e134cdd3d6944edf16c2d85f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:18:16 -0400

    "mod: feel super/see_case_by_day/get_last_child_by_day.py for next problem"

---
+++ super/see_case_by_day/get_last_child_by_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_person_or_first_hand(str_arg):
+    week(str_arg)
+    print('week')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_person_or_first_hand('go_problem')
