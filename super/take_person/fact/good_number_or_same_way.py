c3456631eec2d61e190347d537a47cd779eb66c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:40:45 -0400

    "mv: find super/try_work_from_young_point.py, super/way/government/old_government/large_hand_and_point.py, super/take_person/fact/good_number_or_same_way.py or super/know_early_way_from_day/day_and_man/little_group/problem/case.py for early person"

---
+++ super/take_person/fact/good_number_or_same_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_company(str_arg):
+    company_or_next_problem(str_arg)
+    print('few_week')
+
+def company_or_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_company('large_child')
---
+++ super/try_work_from_young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place_or_person(str_arg):
+    man(str_arg)
+    print('last_eye')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place_or_person('way')
