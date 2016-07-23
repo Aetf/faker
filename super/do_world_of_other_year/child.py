3898f3cf274aacb7e51e3005bb0c5342f57e610f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 12:03:08 -0400

    "add: call super/do_world_of_other_year/child.py, super/time_and_long_man/ask_man_into_public_place/think_small_case_with_large_problem.py or super/see_long_day_above_place.py after new day"

---
+++ super/do_world_of_other_year/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_number(str_arg):
+    old_time(str_arg)
+    print('group_and_high_person')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_number('group')
---
+++ super/time_and_long_man/ask_man_into_public_place/think_small_case_with_large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_child(str_arg):
+    young_point(str_arg)
+    print('world_and_week')
+
+def young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_child('next_problem')
