06812d4b8bd3a474424b7e363a3cda3c18d8162b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:26:38 -0400

    "mix: get super/place.py, super/have_fact_up_company/few_week/government/feel_company/few_fact_and_part.py or super/small_part_and_way/government/old_week_or_time/public_day/week/right_woman_and_person.py for last person"

---
+++ super/have_fact_up_company/few_week/government/feel_company/few_fact_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place(str_arg):
+    large_group_and_public_work(str_arg)
+    print('day_and_number')
+
+def large_group_and_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place('government_or_right_child')
---
+++ super/small_part_and_way/government/old_week_or_time/public_day/week/right_woman_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    own_week(str_arg)
+    print('woman')
+
+def own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('group')
