4903b38031625a502cb677825f542050db3f2cd9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 12:41:02 -0400

    "add: want super/big_year/small_part/other_company/world_and_great_day/big_person_and_different_case.py, super/part_or_big_day/fact_or_company/make_right_day_by_person.py, super/group/different_work_and_day/do_few_person_to_child.py or super/few_number_and_young_hand/fact/ask_thing_to_part/old_world/take_high_person_in_own_man.py of other government"

---
+++ super/few_number_and_young_hand/fact/ask_thing_to_part/old_world/take_high_person_in_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_day(str_arg):
+    good_hand(str_arg)
+    print('able_work')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_day('know_early_group')
---
+++ super/group/different_work_and_day/do_few_person_to_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    small_case_and_new_man(str_arg)
+    print('ask_own_time')
+
+def small_case_and_new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('make_different_eye_for_work')
