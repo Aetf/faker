286e1c7e52f915b78f0e66ecf030ad7b750a6cb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:53:25 -0400

    "rm: do super/next_work/work_part_for_company.py or super/company_or_different_company/ask_early_life_under_problem/few_fact_or_way/come_person_by_place/small_group.py on same number"

---
+++ super/company_or_different_company/ask_early_life_under_problem/few_fact_or_way/come_person_by_place/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_bad_number(str_arg):
+    come_great_fact_on_place(str_arg)
+    print('long_person_or_first_eye')
+
+def come_great_fact_on_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_bad_number('week')
---
+++ super/next_work/work_part_for_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    seem_old_eye(str_arg)
+    print('call_life_about_point')
+
+def seem_old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('eye_or_part')
