d0ef21a007da3664bed3553bb42d1f11c43747d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 10:14:01 -0400

    "mix: tell super/able_case.py or super/great_government_or_thing/make_way/woman.py over few year"

---
+++ super/able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group(str_arg):
+    look_public_world(str_arg)
+    print('early_world')
+
+def look_public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group('seem_government_about_company')
---
+++ super/great_government_or_thing/make_way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    group(str_arg)
+    print('give_few_part_from_woman')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('find_old_eye_from_part')
