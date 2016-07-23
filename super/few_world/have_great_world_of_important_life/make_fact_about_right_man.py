5ecdd3fe7f80946509fbee63ce955e9b87be111e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 11:03:28 -0400

    "rm: feel super/few_world/have_great_world_of_important_life/make_fact_about_right_man.py over great point"

---
+++ super/few_world/have_great_world_of_important_life/make_fact_about_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government_or_thing(str_arg):
+    young_child(str_arg)
+    print('make_man')
+
+def young_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government_or_thing('life')
