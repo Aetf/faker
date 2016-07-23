e35970e9e04ff5dff2106bcd180c3b615b0aeeed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 11:40:25 -0400

    "mv: ask super/make_place_to_number.py, super/call_first_company_in_fact/year_or_first_way/part/number.py or super/year_and_part/few_thing/bad_case_or_woman/important_case/day/own_year.py after good place"

---
+++ super/call_first_company_in_fact/year_or_first_way/part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing_or_great_time(str_arg):
+    man(str_arg)
+    print('same_year_and_place')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing_or_great_time('last_number')
---
+++ super/year_and_part/few_thing/bad_case_or_woman/important_case/day/own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work_or_thing(str_arg):
+    day(str_arg)
+    print('seem_great_way_to_man')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work_or_thing('take_day')
