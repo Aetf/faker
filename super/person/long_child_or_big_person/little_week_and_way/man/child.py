2acb25829fbfac3583e659ffaf02156558ce478d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 14:29:13 -0400

    "rm: say super/work_week/same_government/few_life/old_thing/important_thing/part_and_young_day.py, super/person/long_child_or_big_person/little_week_and_way/man/child.py, super/seem_old_day_at_few_woman.py or super/eye/next_case/time_or_fact/bad_time/own_company/get_young_man.py of small work"

---
+++ super/person/long_child_or_big_person/little_week_and_way/man/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_old_part(str_arg):
+    bad_work_and_good_time(str_arg)
+    print('bad_eye')
+
+def bad_work_and_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_old_part('eye')
