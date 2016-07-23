191e2496e0890c1e70cc79ec68dce2ba42105d8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-23 13:45:09 -0400

    "mix: find super/day/ask_day.py with high place"

---
+++ super/day/ask_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_next_life(str_arg):
+    man_and_bad_number(str_arg)
+    print('life')
+
+def man_and_bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_next_life('young_work')
