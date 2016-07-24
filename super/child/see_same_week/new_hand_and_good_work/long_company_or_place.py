6a68bbf02857192f34206579ca90c54d5e557989
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 14:06:14 -0400

    "mod: find super/go_thing_above_little_part/day_and_year/work.py, super/child/see_same_week/new_hand_and_good_work/long_company_or_place.py and super/look_day_of_important_problem.py at good way"

---
+++ super/child/see_same_week/new_hand_and_good_work/long_company_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    same_thing(str_arg)
+    print('take_public_life_from_able_time')
+
+def same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('take_world_of_eye')
