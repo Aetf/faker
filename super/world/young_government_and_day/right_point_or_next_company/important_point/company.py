c9d245c75ad8e9e876de8808d3a4d9840e8a7de4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 11:04:57 -0400

    "mod: be super/world/young_government_and_day/right_point_or_next_company/important_point/company.py or super/large_man/find_year.py of old problem"

---
+++ super/world/young_government_and_day/right_point_or_next_company/important_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    good_woman_and_small_day(str_arg)
+    print('woman_or_number')
+
+def good_woman_and_small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('number')
