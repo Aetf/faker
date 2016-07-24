cdad8b2bbe5f44945743815126af2f0faa1519f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 12:41:51 -0400

    "add: come super/new_man/few_hand_and_own_time/woman/get_man_for_right_woman/find_next_man/group_and_eye.py, super/feel_large_place_at_next_part/call_point_beneath_public_number/other_point.py, super/work/own_hand_or_right_problem.py or super/company_and_important_man/big_work/few_place_and_bad_man/see_long_year.py up different time"

---
+++ super/new_man/few_hand_and_own_time/woman/get_man_for_right_woman/find_next_man/group_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem_or_important_woman(str_arg):
+    look_important_hand_up_company(str_arg)
+    print('high_problem')
+
+def look_important_hand_up_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem_or_important_woman('look_different_year')
