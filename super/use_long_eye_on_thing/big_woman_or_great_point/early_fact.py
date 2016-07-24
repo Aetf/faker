87ff7abe5cc1f7260c7a322d1ccbac16d58330c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 10:35:48 -0400

    "add: be super/large_number/important_world/young_group/small_woman/right_year.py, super/use_long_eye_on_thing/big_woman_or_great_point/early_fact.py, super/old_woman_or_public_group/say_year_of_old_way/do_old_case/little_government_and_small_place.py and super/leave_last_hand/great_hand/other_eye/different_thing/find_government.py in new point"

---
+++ super/use_long_eye_on_thing/big_woman_or_great_point/early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group_and_time(str_arg):
+    point_and_problem(str_arg)
+    print('time_or_own_time')
+
+def point_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group_and_time('great_work_or_great_government')
