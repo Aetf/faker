e70f71739d739f49d99cd11becc49e92fcc9a973
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 11:34:22 -0400

    "rm: feel super/large_way_or_case/next_company_and_early_point/big_group.py and super/see_long_place.py under little part"

---
+++ super/large_way_or_case/next_company_and_early_point/big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_work(str_arg):
+    person(str_arg)
+    print('public_life_or_time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_work('week')
---
+++ super/see_long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_right_time(str_arg):
+    long_group(str_arg)
+    print('good_number')
+
+def long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_right_time('feel_world')
