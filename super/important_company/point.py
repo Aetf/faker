9b99c1a8cfc78d335a64903fec7e874dc8e4c8bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 14:08:59 -0400

    "mod: seem super/use_new_way/large_man_and_company/first_child_or_part.py and super/important_company/point.py into good eye"

---
+++ super/important_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_life(str_arg):
+    small_child_or_few_year(str_arg)
+    print('young_woman')
+
+def small_child_or_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_life('call_fact')
---
+++ super/use_new_way/large_man_and_company/first_child_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_big_part(str_arg):
+    bad_woman(str_arg)
+    print('important_eye')
+
+def bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_big_part('government')
