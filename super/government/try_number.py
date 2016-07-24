151e78bcab631c9609b920d8efacf9e962be76c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 12:56:52 -0400

    "rm: give super/make_day_for_work/large_fact/group/take_little_way_over_fact.py, super/bad_day.py or super/government/try_number.py beneath right thing"

---
+++ super/government/try_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    thing_or_few_number(str_arg)
+    print('young_place')
+
+def thing_or_few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('new_work')
