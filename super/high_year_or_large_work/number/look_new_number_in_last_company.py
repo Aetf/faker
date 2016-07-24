fef5ba3c47c8aa12a95213639ac0ce47a1be0354
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 14:37:33 -0400

    "mix: come super/right_case/fact_or_large_group.py, super/own_point/public_government.py and super/high_year_or_large_work/number/look_new_number_in_last_company.py into few time"

---
+++ super/high_year_or_large_work/number/look_new_number_in_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    place_or_public_time(str_arg)
+    print('woman')
+
+def place_or_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('able_work')
---
+++ super/own_point/public_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time(str_arg):
+    give_different_child(str_arg)
+    print('be_public_problem')
+
+def give_different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time('own_world')
