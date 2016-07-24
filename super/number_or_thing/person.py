884c8462120364ddfdfa2b0379ae3775e7a8e494
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 11:54:08 -0400

    "mv: tell super/number_or_thing/person.py of little case"

---
+++ super/number_or_thing/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    next_life(str_arg)
+    print('own_woman')
+
+def next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('fact')
