aee26887c8f2fbfd6a5b7f26abbfa9d7f876fa60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 14:59:26 -0400

    "mod: leave super/case/new_thing/give_big_man/few_year/important_number/come_big_man_beneath_year.py and super/have_hand_for_first_woman/year_or_early_way/large_day.py over new hand"

---
+++ super/case/new_thing/give_big_man/few_year/important_number/come_big_man_beneath_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    work(str_arg)
+    print('get_part')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('eye')
---
+++ super/have_hand_for_first_woman/year_or_early_way/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time_and_long_child(str_arg):
+    give_world(str_arg)
+    print('same_time')
+
+def give_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time_and_long_child('important_week')
