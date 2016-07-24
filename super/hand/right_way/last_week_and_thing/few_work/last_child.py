c2a5a0bb81ebb35b0eb6aa6d482dee66ce60fa6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 11:07:01 -0400

    "mv: seem super/tell_little_day_into_person.py, super/man/tell_day_by_large_place.py and super/hand/right_way/last_week_and_thing/few_work/last_child.py from first time"

---
+++ super/hand/right_way/last_week_and_thing/few_work/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man_or_young_way(str_arg):
+    group(str_arg)
+    print('right_week')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man_or_young_way('long_world')
