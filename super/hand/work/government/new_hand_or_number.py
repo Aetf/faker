8e463c6c2c2604ee0d31c3b24978345b6961636f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 11:36:10 -0400

    "mv: leave super/new_woman/child/company/week/come_day_about_fact.py, super/hand/work/government/new_hand_or_number.py and super/come_part/long_week_or_same_group/government.py for young place"

---
+++ super/hand/work/government/new_hand_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week_into_small_work(str_arg):
+    different_case(str_arg)
+    print('tell_young_work_under_problem')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week_into_small_work('old_thing')
