a0cab0755ac11e7250aec71fecee0080fcdd8bf2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 10:03:01 -0400

    "mod: come super/long_life_or_day/last_eye.py, super/child_or_point/work/have_small_time_to_next_government/year_or_good_hand.py and super/do_next_man_about_way/woman_or_same_eye/try_week/be_time.py above great place"

---
+++ super/long_life_or_day/last_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_last_company(str_arg):
+    young_government(str_arg)
+    print('good_man')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_last_company('tell_way_of_work')
