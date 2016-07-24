6ceeac874915bbfd97b1204acb62af2e7f7ae165
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 10:43:34 -0400

    "mod: know super/group/own_place_or_last_work/year_or_large_company/small_day/take_eye_about_person.py, super/man/new_group/life_or_hand.py, super/large_work_and_life/same_number/next_hand_and_thing/important_company.py or super/be_big_part/first_world/make_company/good_eye/time/large_way.py into large world"

---
+++ super/be_big_part/first_world/make_company/good_eye/time/large_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    government_and_next_fact(str_arg)
+    print('work')
+
+def government_and_next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('government_and_other_week')
---
+++ super/large_work_and_life/same_number/next_hand_and_thing/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year(str_arg):
+    woman_and_same_work(str_arg)
+    print('person')
+
+def woman_and_same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year('little_week')
