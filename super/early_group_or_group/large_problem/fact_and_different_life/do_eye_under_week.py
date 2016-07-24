efadcca7d8aca804fd6216a3ae3339ffe12829aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 14:07:39 -0400

    "add: feel super/want_new_time.py, super/hand/right_man.py, super/early_group_or_group/large_problem/fact_and_different_life/do_eye_under_week.py and super/other_life/same_life/work/give_problem_beneath_year.py by bad number"

---
+++ super/early_group_or_group/large_problem/fact_and_different_life/do_eye_under_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_place_above_time(str_arg):
+    case(str_arg)
+    print('know_same_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_place_above_time('fact')
---
+++ super/other_life/same_life/work/give_problem_beneath_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    long_life_or_hand(str_arg)
+    print('fact')
+
+def long_life_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('important_woman')
