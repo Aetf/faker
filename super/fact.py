8667c5ebc63ef0ff3c3306256b154de91a96113a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 13:54:52 -0400

    "add: look super/great_man_or_fact/early_time_and_last_child/problem_or_part.py, super/thing_or_way/great_thing/long_woman/early_world/right_person/last_group.py and super/fact.py to same part"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_person(str_arg)
+    print('way')
+
+def good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('early_problem')
