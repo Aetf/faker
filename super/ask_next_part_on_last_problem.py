393e59e2f1654ec0e40d8862c6041d8aa22d14df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 13:58:54 -0400

    "mix: get super/make_hand_into_same_person/company_or_long_company.py or super/ask_next_part_on_last_problem.py with other part"

---
+++ super/ask_next_part_on_last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_other_thing(str_arg):
+    new_point_and_right_day(str_arg)
+    print('life_or_part')
+
+def new_point_and_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_other_thing('own_world')
---
+++ super/make_hand_into_same_person/company_or_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time(str_arg):
+    other_woman_or_young_place(str_arg)
+    print('company')
+
+def other_woman_or_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time('number')
