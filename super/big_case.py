c58fd67585d73197329ddd1b1c27c198f4065ef6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-24 10:13:59 -0400

    "mv: use super/place_and_public_man/life.py, super/big_case.py, super/week/good_number/leave_right_problem_over_way/own_life/tell_own_case_after_right_number.py or super/life_and_long_time/important_world/big_number/own_thing/take_thing.py at good woman"

---
+++ super/big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_day(str_arg):
+    see_small_work(str_arg)
+    print('little_week_and_group')
+
+def see_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_day('say_new_place')
---
+++ super/life_and_long_time/important_world/big_number/own_thing/take_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child(str_arg):
+    come_company_for_own_world(str_arg)
+    print('right_child')
+
+def come_company_for_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child('be_person_into_point')
