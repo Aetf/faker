c2034e4e83126b6ad7773c4044583c5297baef94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 12:34:15 -0400

    "add: have super/important_life_and_fact/company_or_woman/thing_or_little_hand/number_and_little_work.py, super/great_way_and_bad_problem/company.py and super/man/work_public_child_for_life/new_way_and_other_government/point.py over high man"

---
+++ super/important_life_and_fact/company_or_woman/thing_or_little_hand/number_and_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_early_eye(str_arg):
+    able_government_and_problem(str_arg)
+    print('public_way_or_thing')
+
+def able_government_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_early_eye('small_eye')
