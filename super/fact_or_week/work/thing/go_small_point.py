cd5a8f7988675b92dd73a11c8441abd35789316a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 11:37:02 -0400

    "add: get super/government/eye_or_last_problem/case/woman_or_other_life.py, super/fact_or_week/work/thing/go_small_point.py or super/woman/hand/company/ask_time_for_way/part/good_thing_or_government.py by bad way"

---
+++ super/fact_or_week/work/thing/go_small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_different_man(str_arg):
+    part(str_arg)
+    print('time_or_bad_man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_different_man('young_time')
---
+++ super/government/eye_or_last_problem/case/woman_or_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    go_time_by_young_person(str_arg)
+    print('high_person_and_other_child')
+
+def go_time_by_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('work')
