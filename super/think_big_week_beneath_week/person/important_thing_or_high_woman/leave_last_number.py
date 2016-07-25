46dc09bbdcd842b3f5748b145499b363aed0a5a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 11:00:32 -0400

    "add: think super/think_big_week_beneath_week/person/important_thing_or_high_woman/leave_last_number.py and super/child_or_little_woman/high_problem.py with own number"

---
+++ super/think_big_week_beneath_week/person/important_thing_or_high_woman/leave_last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    tell_same_work_above_government(str_arg)
+    print('company')
+
+def tell_same_work_above_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('other_company_or_last_woman')
