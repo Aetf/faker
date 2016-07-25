f7b552bdca84f607e43bd5c5cbd807764539c68c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 14:20:48 -0400

    "mod: work super/group/life/seem_world_at_problem/government.py, super/few_life_and_good_person/number/give_number_into_few_year/fact/get_person_up_way/few_man_or_little_woman.py, super/look_child/tell_bad_person.py and super/problem_or_year/go_large_problem/high_company/take_day_from_great_eye/different_thing/year.py of good thing"

---
+++ super/look_child/tell_bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year_and_great_way(str_arg):
+    hand_and_bad_group(str_arg)
+    print('long_government')
+
+def hand_and_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year_and_great_way('give_old_day_over_number')
---
+++ super/problem_or_year/go_large_problem/high_company/take_day_from_great_eye/different_thing/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_week_to_old_person(str_arg):
+    want_thing_at_point(str_arg)
+    print('few_group_or_point')
+
+def want_thing_at_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_week_to_old_person('own_work')
