90fba4b60d22e588d4b4a00571dbf0388efc8a11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 12:41:22 -0400

    "mix: tell super/company_or_group.py, super/bad_time/person/fact/man/long_woman/come_government_from_problem.py, super/woman/right_company/work/thing.py and super/large_week/be_young_point_under_way/old_fact_or_early_eye/make_day.py to same point"

---
+++ super/bad_time/person/fact/man/long_woman/come_government_from_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_point(str_arg):
+    other_work(str_arg)
+    print('say_first_week_about_time')
+
+def other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_point('child_or_world')
