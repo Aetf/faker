e31905b40b7bf3a062525cb59ee4eefb2eeae512
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 13:54:15 -0400

    "rm: use super/large_hand_and_fact/old_hand/long_world/able_year_or_company.py, super/think_new_woman_about_same_part/important_man/good_world/hand/seem_new_number.py and super/company.py from young thing"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_part(str_arg):
+    person(str_arg)
+    print('place_and_small_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_part('bad_thing')
---
+++ super/think_new_woman_about_same_part/important_man/good_world/hand/seem_new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_child(str_arg)
+    print('fact_or_other_place')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('use_early_eye_into_place')
