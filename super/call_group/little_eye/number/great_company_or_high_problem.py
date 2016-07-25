cf4af32aa3c0fffc487f1eddfeb342cc8d3b37cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 10:47:57 -0400

    "mix: get super/other_work/take_young_company/few_fact/time_or_person/tell_place_into_little_government/call_man.py, super/time_or_old_part/look_week.py and super/call_group/little_eye/number/great_company_or_high_problem.py to new place"

---
+++ super/call_group/little_eye/number/great_company_or_high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    long_time(str_arg)
+    print('right_man')
+
+def long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('try_year_about_fact')
---
+++ super/time_or_old_part/look_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    long_child_or_day(str_arg)
+    print('find_young_woman')
+
+def long_child_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('work_place')
