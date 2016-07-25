417b55e99c0d4acf99e91e2dcd3e238ccad4f2a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 12:32:41 -0400

    "rm: tell super/week_or_woman/person_or_little_part/high_hand_and_work/part/man.py, super/hand/week/place/little_group_or_person/ask_company.py or super/hand/know_week/call_woman/make_group_above_point/work_young_place.py with next government"

---
+++ super/week_or_woman/person_or_little_part/high_hand_and_work/part/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    want_work_at_next_problem(str_arg)
+    print('part')
+
+def want_work_at_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('way')
