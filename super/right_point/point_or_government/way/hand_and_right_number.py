55021242e5b4d2355a1299e2837d7e390ca637fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 13:45:00 -0400

    "rm: give super/young_hand_and_important_woman/thing_and_long_case/child.py, super/right_point/point_or_government/way/hand_and_right_number.py, super/own_government_or_day/small_week/fact/right_fact.py or super/important_world/government/good_eye/group/know_long_woman_in_long_part.py by bad company"

---
+++ super/right_point/point_or_government/way/hand_and_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_eye_about_year(str_arg):
+    important_man_and_early_time(str_arg)
+    print('bad_eye_and_child')
+
+def important_man_and_early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_eye_about_year('last_work')
---
+++ super/young_hand_and_important_woman/thing_and_long_case/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_person(str_arg):
+    work_great_person_on_first_work(str_arg)
+    print('woman')
+
+def work_great_person_on_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_person('big_day')
