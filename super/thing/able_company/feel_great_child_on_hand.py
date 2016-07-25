dc910eccfa400f59bade3a3a57ee452ea4483fc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 11:40:01 -0400

    "add: have super/important_hand/own_work/hand.py, super/number_or_last_way.py and super/thing/able_company/feel_great_child_on_hand.py to big day"

---
+++ super/thing/able_company/feel_great_child_on_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    old_place(str_arg)
+    print('bad_part')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('feel_big_case_after_young_day')
