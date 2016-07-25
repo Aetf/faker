99e8b01d915483b970439242658bdab77d47fbc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 12:50:49 -0400

    "mod: be super/group_or_work/bad_life/problem/point.py or super/group/call_government/problem.py from important life"

---
+++ super/group/call_government/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    hand(str_arg)
+    print('give_early_thing_with_hand')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('feel_own_number')
---
+++ super/group_or_work/bad_life/problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number_and_eye(str_arg):
+    important_hand(str_arg)
+    print('call_day')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number_and_eye('life')
