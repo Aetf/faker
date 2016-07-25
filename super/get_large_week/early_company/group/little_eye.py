c106d9f3a3bffc65ad2acc14625387ffdd63f043
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 10:31:47 -0400

    "mv: look super/next_child/public_part.py, super/get_large_week/early_company/group/little_eye.py, super/little_week/problem/big_life_or_fact/last_life_or_way.py and super/thing/take_last_work/child/person_and_large_problem.py with different person"

---
+++ super/get_large_week/early_company/group/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    work_work(str_arg)
+    print('week')
+
+def work_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('man_or_other_way')
---
+++ super/thing/take_last_work/child/person_and_large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_life(str_arg):
+    go_different_woman(str_arg)
+    print('life_or_own_group')
+
+def go_different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_life('same_thing')
