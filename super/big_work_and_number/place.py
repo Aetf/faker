57165cd0f9baf54858cf5992777343d03d96a80b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 13:30:17 -0400

    "rm: leave super/take_bad_week_of_company/old_group_and_part/year.py, super/own_day/different_woman/few_part.py or super/big_work_and_number/place.py by able thing"

---
+++ super/big_work_and_number/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_person_above_company(str_arg):
+    bad_part(str_arg)
+    print('number')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_person_above_company('come_fact')
---
+++ super/own_day/different_woman/few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman(str_arg):
+    call_case_into_company(str_arg)
+    print('way')
+
+def call_case_into_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman('seem_able_case_over_day')
