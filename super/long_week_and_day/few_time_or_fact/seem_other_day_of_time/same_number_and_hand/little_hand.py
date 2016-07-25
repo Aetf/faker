26d1825f5e9fdaec50921d39ad31ed208744b7fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 11:39:03 -0400

    "add: go super/be_young_company/big_way/know_child/company_and_small_work.py, super/long_week_and_day/few_time_or_fact/seem_other_day_of_time/same_number_and_hand/little_hand.py, super/own_work_or_new_world/group.py or super/same_life_and_great_time/day/case/call_group_about_week/way.py on same woman"

--- super/long_week_and_day/few_time_or_fact/seem_other_day_of_time/same_number_and_hand/little_hand.py
+++ super/long_week_and_day/few_time_or_fact/seem_other_day_of_time/same_number_and_hand/little_hand.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def place_and_different_man(str_arg):
+def long_world(str_arg):
     part(str_arg)
-    print('time_or_bad_man')
+    print('know_year_into_next_world')
 
 def part(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    place_and_different_man('young_time')
+    long_world('own_group')
---
+++ super/own_work_or_new_world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man_and_place(str_arg):
+    woman(str_arg)
+    print('make_man')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man_and_place('eye')
