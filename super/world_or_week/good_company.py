d786acbf2bfa3422409d0ecb76b2a6a5a790e6d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 12:17:11 -0400

    "mod: come super/life_and_few_company.py and super/world_or_week/good_company.py from young point"

---
+++ super/world_or_week/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman(str_arg):
+    problem(str_arg)
+    print('man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman('important_eye_and_group')
