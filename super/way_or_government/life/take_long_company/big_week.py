fda3406b44f5849526046ebbb849c1df12d9a11a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-25 14:33:53 -0400

    "rm: give super/world_or_person.py or super/way_or_government/life/take_long_company/big_week.py up little life"

---
+++ super/way_or_government/life/take_long_company/big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    person(str_arg)
+    print('other_woman_and_great_man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('case_and_time')
---
+++ super/world_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    find_long_eye_over_different_way(str_arg)
+    print('small_group_and_week')
+
+def find_long_eye_over_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('life')
