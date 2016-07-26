63de3a7c6da989d97d0d4944726d9e308e72f68b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 13:06:47 -0400

    "rm: go super/week/say_woman_by_old_world/small_case/place/child_and_new_thing/give_great_problem_to_large_work.py from own group"

---
+++ super/week/say_woman_by_old_world/small_case/place/child_and_new_thing/give_great_problem_to_large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    small_government_and_able_point(str_arg)
+    print('part')
+
+def small_government_and_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('different_place')
