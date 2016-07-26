67ff6afb740d77f15d266a542a51737930561880
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 13:54:17 -0400

    "rm: do super/world/come_point_about_year/important_week_or_able_point/early_world/work.py, super/government_and_year/woman/right_group/large_problem/work.py, super/hand.py or super/case_and_world/old_life/give_own_way_for_great_case/year/big_man.py to good year"

---
+++ super/government_and_year/woman/right_group/large_problem/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    good_eye(str_arg)
+    print('child')
+
+def good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('year_and_good_company')
