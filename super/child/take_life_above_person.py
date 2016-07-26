d3174c11f297511874f2bea4afb060570744d57b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 12:47:17 -0400

    "add: seem super/child/take_life_above_person.py after high number"

---
+++ super/child/take_life_above_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_day(str_arg):
+    own_woman(str_arg)
+    print('good_fact')
+
+def own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_day('case_or_thing')
