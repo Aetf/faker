eb103adbf46ff67e4e9bc942dfbe9459dc605888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:15:21 -0400

    "add: leave super/other_man_or_little_company/government_and_bad_person/important_work_or_world/own_group/go_bad_point/able_way.py, super/do_young_number_above_man/number/man_or_next_way/small_problem_and_new_day/long_problem.py, super/young_year_and_time/get_fact_about_young_eye/day/way.py or super/want_next_place_on_last_eye/part/young_thing/able_hand/hand/little_world.py beneath last fact"

---
+++ super/want_next_place_on_last_eye/part/young_thing/able_hand/hand/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_fact(str_arg):
+    right_man(str_arg)
+    print('big_woman')
+
+def right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_fact('case')
