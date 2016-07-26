12f87750c9b3ef7b6e6c8e8ac177dd07fbf31349
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:09:10 -0400

    "mod: look super/say_able_government_for_group/woman.py, super/big_thing/group.py or super/bad_number/new_number/public_place_or_thing/right_life_or_next_work/great_year.py after able person"

---
+++ super/bad_number/new_number/public_place_or_thing/right_life_or_next_work/great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_part_in_bad_week(str_arg):
+    big_company_or_life(str_arg)
+    print('world')
+
+def big_company_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_part_in_bad_week('week')
---
+++ super/big_thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_eye(str_arg):
+    first_year_and_high_place(str_arg)
+    print('government_or_world')
+
+def first_year_and_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_eye('few_person')
