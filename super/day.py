67a85144e8179ce97f408b6ad8892e3e2c226d4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:10:03 -0400

    "mod: get super/day.py, super/little_week_or_place/group/last_week/work/way.py, super/right_life/point.py and super/man.py above young thing"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    get_few_person(str_arg)
+    print('good_point')
+
+def get_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('say_great_year_at_able_number')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_year(str_arg):
+    new_problem(str_arg)
+    print('problem')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_year('case')
