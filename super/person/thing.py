1dc3b6a5373ce4340cb87a081c2ffaa78b16d963
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 10:11:20 -0400

    "rm: be super/person/thing.py or super/different_world_or_work/young_way_and_thing/take_other_number_of_child/right_part_or_important_work/old_point/think_same_time.py under right problem"

---
+++ super/person/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_place_by_public_part(str_arg):
+    first_fact(str_arg)
+    print('important_problem')
+
+def first_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_place_by_public_part('few_time')
