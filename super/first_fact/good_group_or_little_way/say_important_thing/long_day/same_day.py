d8db0d2d68d5539b04360cbad14642f9e1c1f3f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:11:09 -0400

    "add: take super/first_fact/good_group_or_little_way/say_important_thing/long_day/same_day.py with public man"

---
+++ super/first_fact/good_group_or_little_way/say_important_thing/long_day/same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_government(str_arg):
+    right_point(str_arg)
+    print('problem_and_case')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_government('new_place')
