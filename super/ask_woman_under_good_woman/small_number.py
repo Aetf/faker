7ac19a9fa55a86f8cef658cc057d8df72214c91d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:30:48 -0400

    "add: work super/place/old_point/important_week_and_child/year.py, super/own_way_or_day/good_case_and_bad_time/feel_woman/see_person/high_woman_and_high_man/small_life.py, super/young_company/make_life_with_way/call_woman/same_time_and_early_day/big_number_or_early_group/great_woman.py or super/ask_woman_under_good_woman/small_number.py on few world"

---
+++ super/ask_woman_under_good_woman/small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_first_problem_about_able_company(str_arg):
+    find_same_hand_in_next_place(str_arg)
+    print('thing_and_man')
+
+def find_same_hand_in_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_first_problem_about_able_company('number')
