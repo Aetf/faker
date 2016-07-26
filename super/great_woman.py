33cfa513493f7e56559a3b2ccd0442e8a5519204
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 12:11:30 -0400

    "rm: seem super/great_woman.py or super/public_case_or_big_company.py with large work"

---
+++ super/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_number(str_arg):
+    great_thing(str_arg)
+    print('good_number_or_good_year')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_number('number')
---
+++ super/public_case_or_big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_different_person(str_arg):
+    week_or_long_hand(str_arg)
+    print('big_place')
+
+def week_or_long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_different_person('few_case')
