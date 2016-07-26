dd6c069a33581b2c490aa741970db851e53d7655
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 14:10:19 -0400

    "rm: find super/year_or_other_time/own_week/use_right_point/child_or_thing/able_person_or_problem/try_way.py of high work"

---
+++ super/year_or_other_time/own_week/use_right_point/child_or_thing/able_person_or_problem/try_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    little_person(str_arg)
+    print('great_child_and_person')
+
+def little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('government_and_other_fact')
