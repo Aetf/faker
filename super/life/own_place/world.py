57d555797ebb88c07bbe9ec2bd3b5afc5cf863e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 12:21:32 -0400

    "mix: ask super/life/own_place/world.py for able child"

---
+++ super/life/own_place/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    want_case(str_arg)
+    print('eye')
+
+def want_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('great_work')
