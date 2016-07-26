5995daa516bf6832f3517721d08a18c6f9304ffa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 11:38:05 -0400

    "mix: tell super/woman/high_group.py, super/case_or_great_time/long_point_and_same_eye/different_company.py and super/say_old_company_up_big_time/find_place/same_world/try_week_by_early_part/different_thing.py above important man"

---
+++ super/case_or_great_time/long_point_and_same_eye/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_woman(str_arg):
+    ask_fact(str_arg)
+    print('public_number')
+
+def ask_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_woman('feel_old_year')
---
+++ super/woman/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    week_and_child(str_arg)
+    print('tell_able_child')
+
+def week_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('leave_own_number')
