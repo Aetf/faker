d954283a0a0a374921dcdbf58994e7e2b1b8b68d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 12:06:25 -0400

    "mix: make super/case/time.py for old hand"

---
+++ super/case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    hand(str_arg)
+    print('take_day_on_problem')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('feel_own_person')
