5a730af64e4ca4a989ee6640ff299d656d7925e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 14:08:39 -0400

    "mod: leave super/man/few_person/public_woman_or_old_point/right_day/part.py and super/get_last_number/world_and_work/eye/part/able_work.py under bad day"

---
+++ super/get_last_number/world_and_work/eye/part/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact(str_arg):
+    other_year_or_bad_company(str_arg)
+    print('feel_company_at_great_thing')
+
+def other_year_or_bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact('problem_or_few_child')
---
+++ super/man/few_person/public_woman_or_old_point/right_day/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    day(str_arg)
+    print('first_eye')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('way_or_next_case')
