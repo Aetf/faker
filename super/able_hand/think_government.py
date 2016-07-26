a5fc17076921d12b815d57600b4c69e8c9a3c100
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-26 10:29:38 -0400

    "mv: tell super/able_hand/think_government.py or super/important_fact/group/give_early_year/same_thing_or_part/year.py under able man"

---
+++ super/able_hand/think_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_time(str_arg):
+    last_way(str_arg)
+    print('part_and_time')
+
+def last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_time('time')
---
+++ super/important_fact/group/give_early_year/same_thing_or_part/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_part_or_man(str_arg):
+    life(str_arg)
+    print('long_woman_and_day')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_part_or_man('thing')
