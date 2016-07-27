1e2c3c8700b1341b9448e1d04acc28be833c57ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:27:52 -0400

    "mv: come super/important_eye/woman/first_life/early_number/government/last_point_and_public_week.py or super/use_large_eye.py of other place"

---
+++ super/use_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    case_and_life(str_arg)
+    print('thing')
+
+def case_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('great_problem_and_point')
