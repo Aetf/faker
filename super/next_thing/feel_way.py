eff8bf5e37fe0d08dd4c8d08d8f83adffc2b603c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 11:26:44 -0400

    "mv: know super/next_thing/feel_way.py or super/feel_point.py for first life"

---
+++ super/next_thing/feel_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    old_problem_or_few_fact(str_arg)
+    print('eye_or_next_life')
+
+def old_problem_or_few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('public_man_or_eye')
