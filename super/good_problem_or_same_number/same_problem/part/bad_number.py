29a6d79966a1eba1b82ba3deb7d1a56fe3714088
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:07:16 -0400

    "rm: tell super/good_problem_or_same_number/same_problem/part/bad_number.py and super/next_child/good_point/person_or_day.py about different man"

---
+++ super/good_problem_or_same_number/same_problem/part/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_man(str_arg):
+    different_life(str_arg)
+    print('ask_eye_after_long_day')
+
+def different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_man('eye')
---
+++ super/next_child/good_point/person_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_number(str_arg):
+    little_fact(str_arg)
+    print('thing_and_work')
+
+def little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_number('long_eye_and_young_year')
