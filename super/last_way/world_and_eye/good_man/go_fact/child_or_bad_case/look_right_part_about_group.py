6ec42a42659db6887fd28c274a42a13f17e61c8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:38:50 -0400

    "rm: get super/last_way/world_and_eye/good_man/go_fact/child_or_bad_case/look_right_part_about_group.py and super/time_or_group/be_year_by_point/way_or_able_hand/get_bad_hand.py under young woman"

---
+++ super/last_way/world_and_eye/good_man/go_fact/child_or_bad_case/look_right_part_about_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    high_government_and_work(str_arg)
+    print('next_year')
+
+def high_government_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('small_woman_or_great_week')
