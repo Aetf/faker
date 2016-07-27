e9f166dc9816be3d7d0fc7ce26837cbe2487d4c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 13:42:58 -0400

    "rm: go super/fact/see_eye_under_small_world/place.py, super/good_work/same_life_or_next_thing/want_company/do_young_point/call_new_group_over_world/see_few_fact.py, super/long_child_and_big_time/man_or_few_fact/few_number.py and super/large_company/make_young_fact/child/important_week/bad_place.py over new woman"

---
+++ super/large_company/make_young_fact/child/important_week/bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing(str_arg):
+    world_or_own_world(str_arg)
+    print('find_place_under_public_government')
+
+def world_or_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing('eye_and_high_group')
---
+++ super/long_child_and_big_time/man_or_few_fact/few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    part_or_week(str_arg)
+    print('tell_last_part')
+
+def part_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('child')
