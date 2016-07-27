adab0ba22c7603213bfb8e9c949bd38f01e3ec52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 13:23:28 -0400

    "mod: take super/government.py above big child"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_world_for_bad_government(str_arg):
+    new_problem(str_arg)
+    print('first_time')
+
+def new_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_world_for_bad_government('bad_world')
