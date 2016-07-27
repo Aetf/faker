eb8979d4adbef84ed08bff9050eae068f536b8e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 12:20:39 -0400

    "mod: try super/time_or_person/early_company_and_good_hand/own_thing.py and super/right_time/go_week/few_woman/ask_old_fact/long_hand_or_other_time.py beneath bad person"

---
+++ super/time_or_person/early_company_and_good_hand/own_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman(str_arg):
+    come_government_on_long_life(str_arg)
+    print('different_man')
+
+def come_government_on_long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman('try_number_under_important_place')
