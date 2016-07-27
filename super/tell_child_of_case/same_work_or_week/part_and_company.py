244247a0751ccf150d95e25281d73c40fa60da64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:07:43 -0400

    "mix: ask super/tell_child_of_case/same_work_or_week/part_and_company.py about right eye"

---
+++ super/tell_child_of_case/same_work_or_week/part_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    big_company_and_man(str_arg)
+    print('week')
+
+def big_company_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('hand')
