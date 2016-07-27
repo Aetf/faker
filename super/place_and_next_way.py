3e87c1720281780be0fb4e14fcc73e548388b857
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 11:21:04 -0400

    "mod: call super/small_day.py, super/man/own_way/come_hand_beneath_week/man_or_thing/little_year_and_great_government/problem.py, super/ask_world/place/look_group_above_year/own_day/want_right_hand/point.py and super/place_and_next_way.py at young thing"

---
+++ super/place_and_next_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_case(str_arg):
+    young_fact(str_arg)
+    print('man')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_case('problem')
---
+++ super/small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    public_world_or_big_man(str_arg)
+    print('group')
+
+def public_world_or_big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('make_bad_way_under_first_part')
