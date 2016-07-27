f97083abc907a5e2fe1362dc58bc11bf36554fe7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 12:05:46 -0400

    "mv: use super/same_day/same_fact_or_same_life/case/point/part.py into other place"

---
+++ super/same_day/same_fact_or_same_life/case/point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_case(str_arg):
+    way(str_arg)
+    print('first_year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_case('think_little_part_by_eye')
