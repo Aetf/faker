26fe9f88cf3be3e3588370254ed66c15737263d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 14:58:02 -0400

    "rm: make super/eye/large_week/child/little_work/work/call_woman.py, super/next_person/government_or_new_part/ask_big_company_by_important_place/way/next_week.py and super/come_able_child_above_way/woman/company/government_or_next_part/fact.py from long child"

---
+++ super/next_person/government_or_new_part/ask_big_company_by_important_place/way/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    thing_and_early_day(str_arg)
+    print('thing')
+
+def thing_and_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('call_high_man_into_good_year')
