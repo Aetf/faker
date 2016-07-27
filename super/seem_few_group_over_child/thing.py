a76fb2ba74983de4036cb8435e2a882732319eba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 14:16:23 -0400

    "mv: go super/few_way_or_point/big_child/problem_or_point/new_fact/long_company.py, super/week/last_day_and_long_man/tell_case/year_or_thing.py, super/way/try_world.py and super/seem_few_group_over_child/thing.py with big place"

---
+++ super/seem_few_group_over_child/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    first_way(str_arg)
+    print('use_big_problem_above_person')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('know_different_eye_to_case')
---
+++ super/week/last_day_and_long_man/tell_case/year_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_hand(str_arg):
+    problem(str_arg)
+    print('good_man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_hand('last_work')
