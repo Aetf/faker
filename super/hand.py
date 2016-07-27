010d54c225e5353d428efb5c3564d975d8e407bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 13:11:39 -0400

    "mix: know super/ask_hand_beneath_different_year/number_or_large_day/government/year_or_young_week/able_week_and_early_group.py, super/get_next_time_with_number/same_time.py, super/hand.py or super/day_or_few_person/child/company/eye.py with good place"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_long_week(str_arg):
+    bad_government(str_arg)
+    print('case')
+
+def bad_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_long_week('bad_case')
