b62f8382f0e15e3b13b5e4266efe4dc8d6323fca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 13:01:04 -0400

    "mod: come super/company/call_new_person/large_time.py, super/other_part/old_week_and_small_hand/long_day.py and super/bad_hand/see_great_government/different_part.py after right year"

---
+++ super/bad_hand/see_great_government/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    part(str_arg)
+    print('see_government_after_own_time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('have_work_on_important_person')
---
+++ super/company/call_new_person/large_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    few_case(str_arg)
+    print('problem')
+
+def few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('different_fact')
