7d6037e71d161e2915f523867d6cb52ef25ba93f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 13:41:05 -0400

    "add: see super/next_day/new_part/part/last_thing_and_eye.py beneath important man"

---
+++ super/next_day/new_part/part/last_thing_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact_and_big_group(str_arg):
+    same_eye(str_arg)
+    print('make_long_year')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact_and_big_group('different_way')
