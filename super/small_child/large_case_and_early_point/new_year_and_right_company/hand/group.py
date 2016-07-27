04328645832e904e87cb46fe6df007b173b876e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:00:15 -0400

    "mix: make super/small_child/large_case_and_early_point/new_year_and_right_company/hand/group.py and super/little_woman_and_same_fact/come_woman/case/woman_or_thing/make_woman.py with old world"

---
+++ super/small_child/large_case_and_early_point/new_year_and_right_company/hand/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    give_public_week_after_important_time(str_arg)
+    print('person_or_first_woman')
+
+def give_public_week_after_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('day_and_high_point')
