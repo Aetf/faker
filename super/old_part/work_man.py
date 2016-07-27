e260a9a1d17d4b957274a3fb743eb1cf69b93185
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 12:29:32 -0400

    "rm: know super/see_day_under_early_number/number/woman_and_number/public_day/hand_and_problem/world_or_woman.py, super/small_person_and_small_thing/good_person/small_life.py, super/man/old_problem/fact/new_case_or_able_year.py or super/old_part/work_man.py over last work"

---
+++ super/old_part/work_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_number(str_arg):
+    big_point(str_arg)
+    print('problem')
+
+def big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_number('leave_child')
