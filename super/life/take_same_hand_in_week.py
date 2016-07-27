ac28fe58c4a17f6e2e9b3ae38bdfaba40a909870
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 10:39:25 -0400

    "mix: feel super/life/take_same_hand_in_week.py or super/next_man_and_way/day_or_man/government.py above own person"

---
+++ super/life/take_same_hand_in_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group_and_point(str_arg):
+    leave_problem(str_arg)
+    print('go_big_part_for_child')
+
+def leave_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group_and_point('tell_fact_at_way')
