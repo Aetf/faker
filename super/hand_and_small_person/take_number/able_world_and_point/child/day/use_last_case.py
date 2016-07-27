8884569ce20caab3fd926393d249f2e51203f092
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-27 11:52:52 -0400

    "add: take super/hand_and_small_person/take_number/able_world_and_point/child/day/use_last_case.py and super/different_case/high_point/year/old_world_and_long_hand/find_big_week_for_child/public_person.py beneath bad child"

---
+++ super/hand_and_small_person/take_number/able_world_and_point/child/day/use_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_part(str_arg):
+    seem_way(str_arg)
+    print('eye')
+
+def seem_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_part('number')
