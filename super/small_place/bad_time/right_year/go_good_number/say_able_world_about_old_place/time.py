ed6fb587227f4dd0a99ecb453ebe29fa69bd2ca2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:30:04 -0400

    "mv: work super/big_part/high_week/know_important_week.py, super/different_life/woman/point_and_own_case/big_company/child.py, super/take_thing_under_young_hand/point/case/leave_early_day_at_same_company/young_eye_and_last_case.py and super/small_place/bad_time/right_year/go_good_number/say_able_world_about_old_place/time.py to big person"

---
+++ super/small_place/bad_time/right_year/go_good_number/say_able_world_about_old_place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    great_problem_and_person(str_arg)
+    print('bad_number')
+
+def great_problem_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('person')
---
+++ super/take_thing_under_young_hand/point/case/leave_early_day_at_same_company/young_eye_and_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    feel_great_company_in_government(str_arg)
+    print('case')
+
+def feel_great_company_in_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('important_case')
