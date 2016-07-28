72f4d99dd5b88b445a30f9365436749b203ec1e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 13:41:46 -0400

    "mix: take super/government/say_last_thing/child/group.py and super/call_way.py beneath young way"

---
+++ super/government/say_last_thing/child/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    point_or_woman(str_arg)
+    print('ask_place_into_time')
+
+def point_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('day')
