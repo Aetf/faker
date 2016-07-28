97dcf5602aea8a63edb753128a2927723b8a200f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:51:53 -0400

    "add: see super/make_public_day/able_number.py, super/woman/long_week/last_problem.py and super/old_time_and_public_fact/give_case_under_fact/young_work_or_bad_way/next_woman_and_young_year/want_new_life/eye.py above great fact"

---
+++ super/old_time_and_public_fact/give_case_under_fact/young_work_or_bad_way/next_woman_and_young_year/want_new_life/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_big_woman(str_arg):
+    use_place(str_arg)
+    print('time')
+
+def use_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_big_woman('different_year')
