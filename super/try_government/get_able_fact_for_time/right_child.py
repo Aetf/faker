94c19acfb339588ca957b460190601344627f657
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 13:30:48 -0400

    "rm: take super/life/case.py, super/call_week_above_good_week/man/problem_and_high_number.py and super/try_government/get_able_fact_for_time/right_child.py on few work"

---
+++ super/try_government/get_able_fact_for_time/right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_case(str_arg):
+    fact(str_arg)
+    print('important_eye_or_right_year')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_case('life_and_great_child')
