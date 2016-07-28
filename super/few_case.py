6b175973267dac5527d8ca6cf8817ae532246aa6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 14:10:22 -0400

    "mv: call super/few_case.py about small life"

---
+++ super/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_right_life(str_arg):
+    other_eye(str_arg)
+    print('get_next_child')
+
+def other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_right_life('next_person_and_eye')
