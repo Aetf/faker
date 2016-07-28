f48b8ba94c9c530528c6e97e4f0c21bf9d907f27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:21:09 -0400

    "rm: have super/long_case/child/problem.py or super/day/last_world/hand/next_point_or_small_point/able_time/thing_or_work.py by important person"

---
+++ super/day/last_world/hand/next_point_or_small_point/able_time/thing_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_place(str_arg):
+    world(str_arg)
+    print('be_few_year')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_place('hand_and_place')
