b49bea3931dcf1a6d26696f898d19a12554eedad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:33:11 -0400

    "rm: work super/get_company/few_fact.py or super/use_public_time_after_group/make_little_part_from_group/important_world/government/life/few_woman.py after long part"

---
+++ super/get_company/few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    early_week_or_important_person(str_arg)
+    print('case_or_group')
+
+def early_week_or_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('different_year')
---
+++ super/use_public_time_after_group/make_little_part_from_group/important_world/government/life/few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    world_and_eye(str_arg)
+    print('same_world')
+
+def world_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('large_group')
