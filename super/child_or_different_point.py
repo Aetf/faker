feafaaed970d249e5ff111c06ce321f9bcc4f10f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 10:58:13 -0400

    "mv: call super/place/woman/want_few_way_for_large_case/old_part/come_long_week/new_eye.py, super/next_thing_or_new_number/first_eye.py and super/child_or_different_point.py into last fact"

---
+++ super/child_or_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work(str_arg):
+    new_hand(str_arg)
+    print('big_man_or_part')
+
+def new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work('make_world_in_new_thing')
---
+++ super/place/woman/want_few_way_for_large_case/old_part/come_long_week/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    new_day(str_arg)
+    print('time')
+
+def new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('leave_point_over_world')
