4fbc6ae85b6895363438cd6cef9279b6fb162b46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:05:40 -0400

    "add: call super/fact.py after new eye"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day(str_arg):
+    try_year(str_arg)
+    print('long_person_or_life')
+
+def try_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day('young_person')
