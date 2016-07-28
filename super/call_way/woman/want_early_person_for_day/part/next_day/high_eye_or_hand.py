9e63e928f437f55cdbd556bb9885dc8c979d3f05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:04:01 -0400

    "mix: give super/seem_woman_above_different_number.py, super/call_way/woman/want_early_person_for_day/part/next_day/high_eye_or_hand.py and super/do_work/find_right_point/part_or_hand/company/take_world.py from own eye"

---
+++ super/call_way/woman/want_early_person_for_day/part/next_day/high_eye_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_group(str_arg):
+    group_and_first_part(str_arg)
+    print('work')
+
+def group_and_first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_group('make_next_government_about_life')
---
+++ super/seem_woman_above_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    world(str_arg)
+    print('take_fact_from_eye')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('year')
