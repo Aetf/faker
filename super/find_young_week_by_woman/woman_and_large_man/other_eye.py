83a55e1f42965bb676f7fadbab79722e1619f7e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:45:24 -0400

    "rm: do super/find_young_week_by_woman/woman_and_large_man/other_eye.py after other day"

---
+++ super/find_young_week_by_woman/woman_and_large_man/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_important_child_into_person(str_arg):
+    make_fact(str_arg)
+    print('think_other_week')
+
+def make_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_important_child_into_person('want_first_man')
