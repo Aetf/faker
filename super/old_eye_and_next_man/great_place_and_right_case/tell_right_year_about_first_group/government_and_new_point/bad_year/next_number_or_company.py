4fb1ca513cd37432e847e6361512671a0390b03b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:16:34 -0400

    "mv: take super/small_group/high_child/important_week/give_large_world_above_important_life/problem_or_new_man/hand.py or super/old_eye_and_next_man/great_place_and_right_case/tell_right_year_about_first_group/government_and_new_point/bad_year/next_number_or_company.py in great company"

---
+++ super/old_eye_and_next_man/great_place_and_right_case/tell_right_year_about_first_group/government_and_new_point/bad_year/next_number_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man(str_arg):
+    young_man(str_arg)
+    print('same_company_or_fact')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man('other_problem_and_person')
