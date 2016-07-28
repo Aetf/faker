8642b7194955f72658705390e9c06da39be03a89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 14:52:10 -0400

    "mod: have super/year/child/leave_time/eye_or_hand/part.py beneath same number"

---
+++ super/year/child/leave_time/eye_or_hand/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    problem(str_arg)
+    print('same_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('see_way_from_able_work')
