36f0431bfa9a5a9acbb0818cf246f831e3d273dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:57:06 -0400

    "mv: know super/week/important_eye/go_small_person/way/government_or_fact/come_day.py from long year"

---
+++ super/week/important_eye/go_small_person/way/government_or_fact/come_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_week(str_arg):
+    get_hand(str_arg)
+    print('last_fact')
+
+def get_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_week('old_group')
