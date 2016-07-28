a8fc6032b852b8685bad3934476fdc105e327aae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:20:43 -0400

    "mix: work super/fact/young_government.py of own year"

---
+++ super/fact/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year_and_year(str_arg):
+    same_way_or_work(str_arg)
+    print('say_part')
+
+def same_way_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year_and_year('leave_next_problem')
