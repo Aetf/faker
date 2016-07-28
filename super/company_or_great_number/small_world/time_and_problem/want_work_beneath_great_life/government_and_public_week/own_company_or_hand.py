278af187caf5d392e2896762925f418d74a70d62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 10:49:13 -0400

    "mod: make super/company_or_great_number/small_world/time_and_problem/want_work_beneath_great_life/government_and_public_week/own_company_or_hand.py by young case"

---
+++ super/company_or_great_number/small_world/time_and_problem/want_work_beneath_great_life/government_and_public_week/own_company_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact(str_arg):
+    old_way(str_arg)
+    print('long_part')
+
+def old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact('work')
