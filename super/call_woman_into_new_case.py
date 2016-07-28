4ce5a884ea03830f9d98a203a2492f2a42e573d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 10:04:11 -0400

    "add: take super/different_problem/leave_group_on_person/want_next_world/small_hand/eye/early_child.py, super/call_woman_into_new_case.py, super/large_fact/public_company.py or super/public_day/day/different_world/case.py from big hand"

---
+++ super/call_woman_into_new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    government(str_arg)
+    print('day')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('early_child')
---
+++ super/large_fact/public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_thing(str_arg):
+    government_and_young_woman(str_arg)
+    print('large_person')
+
+def government_and_young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_thing('point_or_good_year')
