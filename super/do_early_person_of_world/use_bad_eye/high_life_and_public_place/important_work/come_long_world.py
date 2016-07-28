3efd9b7a0ae60ae81d05beb2e86e681424a893ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:16:28 -0400

    "rm: come super/do_early_person_of_world/use_bad_eye/high_life_and_public_place/important_work/come_long_world.py and super/say_fact/leave_other_life_by_large_point.py about little way"

---
+++ super/do_early_person_of_world/use_bad_eye/high_life_and_public_place/important_work/come_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_life(str_arg)
+    print('call_time')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('little_day_or_own_number')
