1d015d867ce428cd4b9913ae277b2f3ffa0dd9ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 12:30:59 -0400

    "add: get super/right_point_and_high_work.py and super/world/big_world_or_person/great_thing/small_point_or_long_way/tell_work_into_person/look_week_of_able_way.py into large man"

---
+++ super/world/big_world_or_person/great_thing/small_point_or_long_way/tell_work_into_person/look_week_of_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_child_and_case(str_arg):
+    early_part_and_new_year(str_arg)
+    print('good_case_or_woman')
+
+def early_part_and_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_child_and_case('company')
