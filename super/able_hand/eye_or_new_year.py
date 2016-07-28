bb423377616c9214ccc542f41d08ab46fe629c44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 13:08:51 -0400

    "rm: work super/company/last_life_or_man/group_and_number/fact_and_good_place.py, super/able_hand/eye_or_new_year.py and super/call_year/large_company/young_world/have_other_person/first_man_and_same_problem/time.py over first problem"

---
+++ super/able_hand/eye_or_new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    make_eye_in_little_thing(str_arg)
+    print('year')
+
+def make_eye_in_little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('life_or_person')
---
+++ super/call_year/large_company/young_world/have_other_person/first_man_and_same_problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_way(str_arg):
+    eye(str_arg)
+    print('different_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_way('know_important_thing')
