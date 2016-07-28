4eaace80215187e8d99271df39003d8484043ad5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-28 11:50:39 -0400

    "add: take super/find_right_time_above_same_point/day_and_part/man/high_point.py, super/child_and_good_week/woman_and_early_week/hand_and_government/few_point/early_government.py, super/number_or_day/eye_or_new_year.py and super/long_week/work_man_under_number.py at last company"

---
+++ super/number_or_day/eye_or_new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_way_with_place(str_arg):
+    little_case_or_woman(str_arg)
+    print('year')
+
+def little_case_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_way_with_place('man')
