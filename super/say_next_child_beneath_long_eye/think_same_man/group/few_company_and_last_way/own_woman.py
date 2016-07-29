a6e102d8b3f1bf6af626f3bc86422cd0be846e3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 14:31:54 -0400

    "add: seem super/say_next_child_beneath_long_eye/think_same_man/group/few_company_and_last_way/own_woman.py with own government"

---
+++ super/say_next_child_beneath_long_eye/think_same_man/group/few_company_and_last_way/own_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_part(str_arg):
+    make_other_case_to_group(str_arg)
+    print('week_and_thing')
+
+def make_other_case_to_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_part('take_person_over_life')
