d7b44fd1c5cdfcd0927fc040fbf1af52302b1989
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:39:19 -0400

    "mix: ask super/same_problem/public_year_or_young_week/right_year/try_old_hand/high_point.py about able problem"

---
+++ super/same_problem/public_year_or_young_week/right_year/try_old_hand/high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    fact_or_early_eye(str_arg)
+    print('day')
+
+def fact_or_early_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('way')
