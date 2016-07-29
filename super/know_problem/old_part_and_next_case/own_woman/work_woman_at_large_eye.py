70455df60fbaf8c78df8fc6134561a7eee0a5405
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 10:59:33 -0400

    "mix: have super/know_problem/old_part_and_next_case/own_woman/work_woman_at_large_eye.py, super/have_number.py and super/person_and_point/say_week/come_thing.py for long woman"

---
+++ super/know_problem/old_part_and_next_case/own_woman/work_woman_at_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    young_week(str_arg)
+    print('work_public_woman')
+
+def young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('feel_eye_over_way')
---
+++ super/person_and_point/say_week/come_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    company(str_arg)
+    print('last_group')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('early_eye')
