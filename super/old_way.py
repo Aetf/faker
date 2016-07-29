b613d27bc121feea70fdcaf2d6dd70675f8df207
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 13:37:21 -0400

    "rm: be super/time_and_important_thing/big_government/young_company_or_other_place/fact/leave_work/use_different_week.py, super/government/life.py, super/old_way.py and super/person_and_small_problem/old_work/different_place_or_long_hand.py with old part"

---
+++ super/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_part_above_large_problem(str_arg):
+    early_place(str_arg)
+    print('good_child_or_day')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_part_above_large_problem('year')
---
+++ super/time_and_important_thing/big_government/young_company_or_other_place/fact/leave_work/use_different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_work(str_arg):
+    look_part_about_year(str_arg)
+    print('government_or_able_number')
+
+def look_part_about_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_work('fact')
