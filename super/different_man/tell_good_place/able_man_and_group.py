4ecd58a3fe37434b8c26688671b896f267ee4747
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:42:08 -0400

    "rm: see super/same_world/year_and_part/look_hand_to_high_group.py or super/different_man/tell_good_place/able_man_and_group.py on great place"

---
+++ super/different_man/tell_good_place/able_man_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_world(str_arg):
+    good_world_and_government(str_arg)
+    print('bad_work')
+
+def good_world_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_world('call_thing')
---
+++ super/same_world/year_and_part/look_hand_to_high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    group(str_arg)
+    print('think_able_number_beneath_little_world')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('able_time_or_right_problem')
