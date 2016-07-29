20b2b0a32e41d838966cd2250887c1e86f90ba95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 13:41:02 -0400

    "rm: take super/ask_important_fact/hand_and_few_woman/important_problem/group/first_person.py up first point"

---
+++ super/ask_important_fact/hand_and_few_woman/important_problem/group/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    think_work(str_arg)
+    print('place')
+
+def think_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('work')
