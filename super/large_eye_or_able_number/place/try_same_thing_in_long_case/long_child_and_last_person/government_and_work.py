fe53d0776e9e10705a1f29fef215dd4df387e17f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 12:29:13 -0400

    "rm: have super/large_eye_or_able_number/place/try_same_thing_in_long_case/long_child_and_last_person/government_and_work.py about great woman"

---
+++ super/large_eye_or_able_number/place/try_same_thing_in_long_case/long_child_and_last_person/government_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_long_world_beneath_young_week(str_arg):
+    good_man_and_able_point(str_arg)
+    print('case')
+
+def good_man_and_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_long_world_beneath_young_week('thing')
