3637cd23fc90899c8f981b5e1da92579ea1b961c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 10:39:59 -0400

    "rm: leave super/different_government_or_other_number/large_thing_or_old_group/work_and_woman/important_thing/high_problem_and_person/do_year.py, super/other_week/old_person/make_first_point/good_day_and_few_number/point/other_child_and_bad_government.py, super/public_case_and_world/important_hand/last_government_or_world/child/year_or_world.py or super/own_problem_and_long_world/little_week.py of important government"

---
+++ super/other_week/old_person/make_first_point/good_day_and_few_number/point/other_child_and_bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    other_case(str_arg)
+    print('big_eye')
+
+def other_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('new_way')
