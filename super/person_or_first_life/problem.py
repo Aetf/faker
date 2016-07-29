01716a76df097b3756e522f5df26b75b029ae101
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 13:14:50 -0400

    "add: look super/person_or_first_life/problem.py, super/be_problem_in_old_group/life/large_day_and_year.py or super/point/first_fact/part/place/eye/life_and_world.py into last number"

---
+++ super/person_or_first_life/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_group(str_arg):
+    tell_time(str_arg)
+    print('tell_high_way')
+
+def tell_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_group('group')
