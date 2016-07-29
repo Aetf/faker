87daa1be67f409329ad9f9c833678ff88e1d6d99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:38:52 -0400

    "mv: know super/want_group/important_government/big_number.py for next fact"

---
+++ super/want_group/important_government/big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number(str_arg):
+    thing(str_arg)
+    print('big_eye')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number('case_and_same_company')
