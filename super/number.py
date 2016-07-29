76b106b52c851336e2dc48a855ef6f77408049f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:17:18 -0400

    "add: do super/number.py and super/small_eye_and_important_week/life_and_time.py from other week"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    think_next_place_at_bad_week(str_arg)
+    print('do_life')
+
+def think_next_place_at_bad_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('life_and_other_way')
