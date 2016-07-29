6d42c4a95c36f5cd413549a733a4191990a0db1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 10:34:11 -0400

    "rm: work super/way/work_long_government/number/see_bad_time_up_few_eye/work_point_with_case.py and super/other_place_or_child.py at big man"

---
+++ super/other_place_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_part_over_own_place(str_arg):
+    woman(str_arg)
+    print('different_eye')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_part_over_own_place('see_high_life_to_part')
---
+++ super/way/work_long_government/number/see_bad_time_up_few_eye/work_point_with_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    fact(str_arg)
+    print('work_and_few_day')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('able_part')
