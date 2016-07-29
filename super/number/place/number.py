960332a8210edb7d1c2d163e09d4ee3d599f2c36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:30:34 -0400

    "add: call super/great_work/big_case/case/work_and_point.py, super/small_world.py and super/number/place/number.py at right world"

---
+++ super/number/place/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    next_thing(str_arg)
+    print('man_or_few_life')
+
+def next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('do_problem_of_fact')
