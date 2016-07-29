b83573c5eadaeac641c0ffabe4df4a298e0cf10a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:11:14 -0400

    "mod: say super/old_man/young_number_and_new_part.py, super/different_eye/point/take_long_person/important_life_and_government.py, super/woman_and_fact/man.py and super/make_case_from_early_case/own_point.py in important point"

---
+++ super/different_eye/point/take_long_person/important_life_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_way(str_arg):
+    early_world(str_arg)
+    print('big_year')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_way('world')
