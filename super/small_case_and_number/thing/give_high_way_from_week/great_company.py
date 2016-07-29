d3906eecfc1d8618cc0ce1e63101fb0b05d45dbf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 13:49:42 -0400

    "rm: go super/small_case_and_number/thing/give_high_way_from_week/great_company.py under early day"

---
+++ super/small_case_and_number/thing/give_high_way_from_week/great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_government(str_arg):
+    child_and_way(str_arg)
+    print('long_hand')
+
+def child_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_government('fact_or_other_person')
