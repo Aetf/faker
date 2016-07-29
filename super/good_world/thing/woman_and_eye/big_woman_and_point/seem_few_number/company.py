556bec559bb12e5f15238be98be223bd53452e49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 10:42:35 -0400

    "mod: look super/good_world/thing/woman_and_eye/big_woman_and_point/seem_few_number/company.py about last man"

---
+++ super/good_world/thing/woman_and_eye/big_woman_and_point/seem_few_number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_man(str_arg):
+    have_new_company_after_problem(str_arg)
+    print('think_case_up_week')
+
+def have_new_company_after_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_man('long_problem_and_great_time')
