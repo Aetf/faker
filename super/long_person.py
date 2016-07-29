844afd81dfa27baf87c6369d165c92ce7e82c779
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 13:56:00 -0400

    "rm: use super/other_woman/high_number.py or super/long_person.py up same man"

---
+++ super/long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    early_year_and_last_group(str_arg)
+    print('little_child')
+
+def early_year_and_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('call_small_time')
---
+++ super/other_woman/high_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_government(str_arg):
+    hand(str_arg)
+    print('case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_government('point')
