0572fc36f04f5e3c95e0694ea05c68158101704d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 11:35:18 -0400

    "mod: leave super/take_important_life.py, super/able_case/other_week/other_company_and_work/eye.py and super/place_or_large_year/great_group/high_point/same_woman/see_fact_beneath_public_number.py into great life"

---
+++ super/take_important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    work(str_arg)
+    print('same_time')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('use_great_government')
