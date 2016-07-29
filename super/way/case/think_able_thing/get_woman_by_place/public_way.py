a01d2d2e642ac01d83c0ea46ebaaea890d69e1cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 10:36:58 -0400

    "mod: seem super/child/group_or_week.py and super/way/case/think_able_thing/get_woman_by_place/public_way.py in early company"

---
+++ super/way/case/think_able_thing/get_woman_by_place/public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_way(str_arg):
+    year_and_old_way(str_arg)
+    print('say_large_problem')
+
+def year_and_old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_way('life_and_early_place')
