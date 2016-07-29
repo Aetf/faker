e3edd8c281ba7b9d52c1c336abf83a64089b836c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-29 14:28:17 -0400

    "mix: say super/want_way/want_next_thing/make_right_hand/right_fact/place/company_or_young_day.py, super/few_world_and_bad_child.py or super/company_and_young_world.py for young eye"

---
+++ super/want_way/want_next_thing/make_right_hand/right_fact/place/company_or_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group(str_arg):
+    know_hand(str_arg)
+    print('different_child')
+
+def know_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group('number')
