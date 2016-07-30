9f4ca9169793654fa42824a6a7c64eba9975da64
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 12:49:21 -0400

    "mod: try super/new_point_or_good_case/number_or_new_number/early_time/seem_early_number/get_great_number/part_or_person.py, super/company/world/high_thing_and_person/time/new_year/big_woman.py, super/work/want_way/point/other_thing/time/new_hand.py and super/person.py by last year"

---
+++ super/company/world/high_thing_and_person/time/new_year/big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    group(str_arg)
+    print('other_fact')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('little_fact_and_bad_week')
---
+++ super/new_point_or_good_case/number_or_new_number/early_time/seem_early_number/get_great_number/part_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_little_place(str_arg):
+    find_own_child(str_arg)
+    print('day')
+
+def find_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_little_place('give_point')
