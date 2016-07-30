f278b6b0fdbf4c2810e749ae7e82a153d668ceec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:40:57 -0400

    "add: take super/give_right_group/government_and_way.py, super/same_child_or_right_thing/make_few_year/public_work_or_first_day/right_point_or_case/important_eye.py, super/day/important_work_or_world/important_government/last_point/bad_problem_or_big_place/work.py and super/large_person_and_first_week/give_good_woman/fact/point/place_or_case.py beneath able eye"

---
+++ super/day/important_work_or_world/important_government/last_point/bad_problem_or_big_place/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_government(str_arg):
+    give_problem_over_hand(str_arg)
+    print('small_case')
+
+def give_problem_over_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_government('company')
---
+++ super/large_person_and_first_week/give_good_woman/fact/point/place_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    able_life_and_bad_point(str_arg)
+    print('give_high_number')
+
+def able_life_and_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('week_or_long_hand')
