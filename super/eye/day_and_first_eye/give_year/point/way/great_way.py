8b9471d4024292e7467df6103800fe937b7284a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:20:48 -0400

    "mod: come super/eye/day_and_first_eye/give_year/point/way/great_way.py over own number"

---
+++ super/eye/day_and_first_eye/give_year/point/way/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_right_fact(str_arg):
+    make_company(str_arg)
+    print('high_man')
+
+def make_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_right_fact('early_group')
