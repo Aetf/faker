6a396b3786de3a21d64e3c4f3d19fcfd0f5fb5cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:19:19 -0400

    "mv: tell super/be_good_place/good_part/take_problem_in_thing/ask_hand.py and super/big_fact/have_group_up_time/high_woman/fact_and_week/man.py in right place"

---
+++ super/big_fact/have_group_up_time/high_woman/fact_and_week/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    government(str_arg)
+    print('public_fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('say_high_fact')
