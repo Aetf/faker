7e8156d4ae257b7ed9db319ab62c2a66b36a141c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 13:53:50 -0400

    "mv: try super/time/good_life/large_hand.py for own woman"

---
+++ super/time/good_life/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world_or_different_person(str_arg):
+    small_case(str_arg)
+    print('week_and_life')
+
+def small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world_or_different_person('long_time_and_group')
