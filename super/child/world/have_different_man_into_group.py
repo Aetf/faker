142056d2e2ec472d49b0bc894b6d702e5534e0ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 14:13:36 -0400

    "mod: know super/old_group_and_week/have_day/do_problem.py, super/big_life_and_time/try_good_place/leave_child/give_point/thing/know_year_up_early_case.py, super/young_hand/great_life/early_life/leave_world/next_eye.py and super/child/world/have_different_man_into_group.py beneath little company"

---
+++ super/child/world/have_different_man_into_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    first_group(str_arg)
+    print('important_time_and_early_problem')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('government_or_week')
---
+++ super/young_hand/great_life/early_life/leave_world/next_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man(str_arg):
+    little_eye(str_arg)
+    print('last_company_or_fact')
+
+def little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man('leave_young_year_in_great_group')
