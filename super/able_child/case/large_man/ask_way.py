5fd4f0f419c7b1c932494f3458d22b7257394cfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 14:19:02 -0400

    "mod: seem super/able_child/case/large_man/ask_way.py or super/say_big_woman_with_work/way_and_woman/new_life_and_person/thing/young_day.py with other government"

---
+++ super/able_child/case/large_man/ask_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_other_case_up_person(str_arg):
+    man(str_arg)
+    print('call_big_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_other_case_up_person('life')
---
+++ super/say_big_woman_with_work/way_and_woman/new_life_and_person/thing/young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_company(str_arg):
+    different_case(str_arg)
+    print('call_old_part_after_early_time')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_company('good_government')
