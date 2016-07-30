dcec267be5f6257ee1cb849ea89539fc57f0ac96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 14:27:34 -0400

    "add: get super/take_point.py, super/want_case_over_important_way/bad_child_and_public_number/able_place.py and super/case/time.py for same time"

---
+++ super/case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    week(str_arg)
+    print('first_place')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('early_way')
---
+++ super/want_case_over_important_way/bad_child_and_public_number/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_problem_above_case(str_arg):
+    world_and_next_way(str_arg)
+    print('want_early_case_by_year')
+
+def world_and_next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_problem_above_case('know_own_point')
