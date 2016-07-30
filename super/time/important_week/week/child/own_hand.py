d8c9d549fcda1943d9a46a445cef65db87b04923
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:37:02 -0400

    "add: call super/work_week.py, super/time/important_week/week/child/own_hand.py and super/seem_group_to_life.py into last child"

---
+++ super/time/important_week/week/child/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part(str_arg):
+    small_life_or_point(str_arg)
+    print('part')
+
+def small_life_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part('day_and_different_way')
---
+++ super/work_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    good_day(str_arg)
+    print('new_work')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('try_place_with_great_point')
