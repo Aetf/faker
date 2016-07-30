97b3b3c585d53d44aebb855b8d5c201b75f20aa1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 14:09:23 -0400

    "mod: leave super/group_and_first_case/hand/fact/able_case/work/high_way.py, super/see_eye_beneath_world/life/see_different_way_by_public_woman.py or super/point/leave_eye/life_or_old_thing/eye_and_point/see_small_problem/point.py to last woman"

---
+++ super/group_and_first_case/hand/fact/able_case/work/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    bad_person(str_arg)
+    print('thing_or_small_thing')
+
+def bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('place_or_high_number')
---
+++ super/point/leave_eye/life_or_old_thing/eye_and_point/see_small_problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    tell_point_from_public_point(str_arg)
+    print('small_woman')
+
+def tell_point_from_public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('say_few_time')
