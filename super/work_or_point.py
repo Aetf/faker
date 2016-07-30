ef23bc7276f2c03f5b7e1149354a6f6865958fa9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 11:16:54 -0400

    "mix: call super/young_woman/life/fact.py, super/different_group/company_and_day/case_and_problem/number.py, super/be_work.py and super/work_or_point.py in first woman"

---
+++ super/work_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_little_place_on_week(str_arg):
+    little_day(str_arg)
+    print('young_number_or_case')
+
+def little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_little_place_on_week('world')
---
+++ super/young_woman/life/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    make_year_into_week(str_arg)
+    print('woman')
+
+def make_year_into_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('world_or_different_eye')
