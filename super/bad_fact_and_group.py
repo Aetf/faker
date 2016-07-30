1512e5cb63529d4347896ce39f65c4336e81dd5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 11:28:30 -0400

    "add: get super/hand_or_high_thing/long_group_or_great_time/different_fact/long_company_or_time.py or super/bad_fact_and_group.py beneath long time"

---
+++ super/bad_fact_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    feel_way(str_arg)
+    print('come_first_year')
+
+def feel_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('time_and_next_child')
