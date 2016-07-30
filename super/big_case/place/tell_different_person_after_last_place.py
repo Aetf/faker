e6a43dd53eb75f89757ffa4ddb12066a41ef2687
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 11:50:32 -0400

    "rm: feel super/big_case/place/tell_different_person_after_last_place.py on able year"

---
+++ super/big_case/place/tell_different_person_after_last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_fact_from_large_man(str_arg):
+    bad_life(str_arg)
+    print('have_problem_with_hand')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_fact_from_large_man('first_way_and_woman')
