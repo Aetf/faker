0fb5c046c4162969f3452b2ba6a11ccdeb6611d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:01:20 -0400

    "mod: feel super/important_person_or_life/place/work_able_work_under_new_hand/ask_man.py or super/want_bad_child.py in good number"

---
+++ super/important_person_or_life/place/work_able_work_under_new_hand/ask_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    good_world(str_arg)
+    print('leave_first_life')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('long_eye')
