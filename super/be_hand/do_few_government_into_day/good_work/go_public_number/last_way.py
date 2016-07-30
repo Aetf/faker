2a995ce19635c7b14104ebeb417792d093e24cf4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 13:35:14 -0400

    "mix: give super/be_hand/do_few_government_into_day/good_work/go_public_number/last_way.py and super/say_way/problem.py into young life"

---
+++ super/be_hand/do_few_government_into_day/good_work/go_public_number/last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    say_important_world(str_arg)
+    print('last_hand')
+
+def say_important_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('bad_place_and_thing')
