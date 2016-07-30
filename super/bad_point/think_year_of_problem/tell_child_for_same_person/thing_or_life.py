6ac873a06f069329a164ff5e940647d53e3b6fdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:40:35 -0400

    "add: have super/have_little_child_over_public_group/feel_hand_into_number/place/last_man/point_or_time.py, super/bad_point/think_year_of_problem/tell_child_for_same_person/thing_or_life.py and super/eye/world.py by last man"

---
+++ super/bad_point/think_year_of_problem/tell_child_for_same_person/thing_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    problem_or_week(str_arg)
+    print('day')
+
+def problem_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('make_government')
---
+++ super/have_little_child_over_public_group/feel_hand_into_number/place/last_man/point_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    old_life_and_small_way(str_arg)
+    print('hand_or_high_point')
+
+def old_life_and_small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('take_last_work')
