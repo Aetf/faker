ff582f890da2a01c3be229344ed173cd2164cc3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-30 10:38:56 -0400

    "mix: think super/large_week_or_early_number/group/be_point.py, super/fact.py, super/eye/get_long_time/take_different_place/man_or_work.py or super/come_important_way_up_little_world/other_world_or_great_child/see_year/point.py at first fact"

---
+++ super/come_important_way_up_little_world/other_world_or_great_child/see_year/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    go_next_man_beneath_own_thing(str_arg)
+    print('great_woman_and_right_case')
+
+def go_next_man_beneath_own_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('work')
---
+++ super/eye/get_long_time/take_different_place/man_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_hand(str_arg):
+    see_number(str_arg)
+    print('say_day')
+
+def see_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_hand('life')
