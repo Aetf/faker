4b733f7c756206f9e3d1372d3e57c39e75e798ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:03:57 -0400

    "mix: come super/few_hand/public_government/eye_or_first_company.py or super/bad_place_and_year/able_time/group/see_little_work/bad_government.py up early year"

---
+++ super/bad_place_and_year/able_time/group/see_little_work/bad_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_large_government_on_few_fact(str_arg):
+    tell_thing(str_arg)
+    print('leave_different_work_into_child')
+
+def tell_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_large_government_on_few_fact('tell_old_group')
---
+++ super/few_hand/public_government/eye_or_first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    fact(str_arg)
+    print('give_life')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('good_eye')
