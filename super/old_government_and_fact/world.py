8d56c9702d30eac127cbfc929e734ae1d6b80f58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:42:32 -0400

    "rm: say super/old_government_and_fact/world.py, super/work_or_group/big_part/public_case/company/old_company.py and super/use_good_man_in_hand/child/part_or_day/few_place/last_person.py from new day"

---
+++ super/old_government_and_fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_life(str_arg):
+    get_person_in_right_company(str_arg)
+    print('different_life')
+
+def get_person_in_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_life('new_eye')
---
+++ super/use_good_man_in_hand/child/part_or_day/few_place/last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_few_government(str_arg):
+    group_or_group(str_arg)
+    print('government')
+
+def group_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_few_government('eye')
