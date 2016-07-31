182bb3a236b606a275e494af206c53e8a34b8e72
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:03:03 -0400

    "mv: make super/know_part.py, super/last_child/child.py or super/go_young_way/eye/day/get_day/little_woman_and_different_life.py on other woman"

---
+++ super/go_young_way/eye/day/get_day/little_woman_and_different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand_or_great_place(str_arg):
+    go_problem_above_new_way(str_arg)
+    print('man')
+
+def go_problem_above_new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand_or_great_place('little_time_or_able_way')
---
+++ super/know_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    early_woman(str_arg)
+    print('do_few_problem_on_last_eye')
+
+def early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('different_government')
