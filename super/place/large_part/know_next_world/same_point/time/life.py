845400a5a51558fc9600af8f0c7be5f03ee7a0b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:24:30 -0400

    "add: take super/week/work/good_man.py, super/work_eye_for_world/fact/want_last_part/case.py and super/place/large_part/know_next_world/same_point/time/life.py on little year"

---
+++ super/place/large_part/know_next_world/same_point/time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    fact(str_arg)
+    print('few_time_and_work')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('way')
