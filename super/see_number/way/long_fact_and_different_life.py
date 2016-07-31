2f4759920afe8cbbcd6b6b1861289781c63cbdc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 12:55:27 -0400

    "mod: use super/eye_and_good_point.py and super/see_number/way/long_fact_and_different_life.py for important government"

---
+++ super/see_number/way/long_fact_and_different_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_case_beneath_point(str_arg):
+    case(str_arg)
+    print('old_part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_case_beneath_point('last_year')
