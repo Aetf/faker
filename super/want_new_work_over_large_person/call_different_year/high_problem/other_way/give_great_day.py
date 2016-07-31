a521cd0b97b0628909ae27061a399cf586bd0b6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:21:21 -0400

    "add: call super/group_or_hand/come_way_to_next_time/ask_government/hand/long_man/find_woman.py, super/want_new_work_over_large_person/call_different_year/high_problem/other_way/give_great_day.py or super/group_or_few_problem/be_life_under_own_way/come_own_world/next_problem.py after public child"

---
+++ super/want_new_work_over_large_person/call_different_year/high_problem/other_way/give_great_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    little_hand(str_arg)
+    print('thing')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('point')
