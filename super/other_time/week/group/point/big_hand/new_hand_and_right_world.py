7737bfe2e3964350205d1771eee0dacc32ff30c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 11:22:42 -0400

    "mix: seem super/public_day/last_woman_or_small_woman/know_high_government_above_great_woman/problem/man.py, super/other_time/week/group/point/big_hand/new_hand_and_right_world.py or super/work_government_above_few_problem/first_hand/group.py for good part"

---
+++ super/other_time/week/group/point/big_hand/new_hand_and_right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man(str_arg):
+    feel_first_child_beneath_big_person(str_arg)
+    print('group')
+
+def feel_first_child_beneath_big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man('next_time_or_other_part')
