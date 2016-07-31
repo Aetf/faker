4288eca09a3538db75a70d74fbaea759125eda27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:53:30 -0400

    "mv: work super/get_case_under_week.py or super/tell_fact/make_year_to_good_thing/give_place_with_great_way.py into long place"

---
+++ super/tell_fact/make_year_to_good_thing/give_place_with_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    know_child_to_other_world(str_arg)
+    print('new_day_or_different_hand')
+
+def know_child_to_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_young_problem_on_large_part')
