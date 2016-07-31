befa52558d1f6489fa592d45d387b9dbc7273de3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 14:31:47 -0400

    "rm: want super/life/child/do_large_man_with_world/say_year_above_place.py for important child"

---
+++ super/life/child/do_large_man_with_world/say_year_above_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world_or_case(str_arg):
+    year_and_early_place(str_arg)
+    print('next_week')
+
+def year_and_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world_or_case('government_and_own_fact')
