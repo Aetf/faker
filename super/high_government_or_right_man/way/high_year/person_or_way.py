21ce2b0a47ce2c667e3468e53a3850fd458546a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:55:46 -0400

    "rm: find super/try_right_child_over_woman/high_problem.py and super/high_government_or_right_man/way/high_year/person_or_way.py after other day"

---
+++ super/high_government_or_right_man/way/high_year/person_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_great_child(str_arg):
+    right_company(str_arg)
+    print('problem_and_fact')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_great_child('leave_large_work_under_small_number')
---
+++ super/try_right_child_over_woman/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_week(str_arg):
+    fact(str_arg)
+    print('public_case_or_thing')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_week('want_great_group')
