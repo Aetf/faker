58e3d6a198478278604005b0c382cb329020069a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:21:54 -0400

    "mod: give super/bad_place/government/government.py over early company"

---
+++ super/bad_place/government/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    get_last_thing_in_small_place(str_arg)
+    print('high_life')
+
+def get_last_thing_in_small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('person')
