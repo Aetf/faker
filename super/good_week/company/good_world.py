fce92beee2b1f7916dbec593b11b6f03c0bf8568
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:16:40 -0400

    "mod: know super/person/life/person/long_part/think_next_child_up_high_part.py, super/important_case_or_new_way/seem_place_into_big_problem/small_day_or_few_year/woman_and_part.py and super/good_week/company/good_world.py to different person"

---
+++ super/good_week/company/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life(str_arg):
+    other_fact_and_new_child(str_arg)
+    print('thing')
+
+def other_fact_and_new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life('eye_and_same_case')
---
+++ super/important_case_or_new_way/seem_place_into_big_problem/small_day_or_few_year/woman_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_eye(str_arg):
+    look_place(str_arg)
+    print('find_part_beneath_group')
+
+def look_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_eye('public_child_and_last_place')
