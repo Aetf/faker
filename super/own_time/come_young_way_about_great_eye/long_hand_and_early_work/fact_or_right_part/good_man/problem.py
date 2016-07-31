67029a17dcc5a675cd1bb1051ff50862abc9f23a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:17:25 -0400

    "mod: think super/own_time/come_young_way_about_great_eye/long_hand_and_early_work/fact_or_right_part/good_man/problem.py at own place"

---
+++ super/own_time/come_young_way_about_great_eye/long_hand_and_early_work/fact_or_right_part/good_man/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    week(str_arg)
+    print('world_and_different_company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('bad_point')
