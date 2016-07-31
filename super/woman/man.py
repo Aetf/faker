9ef1263116ef6a70a7fd0bc89fb9b1ce5c27e92f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 12:07:50 -0400

    "mix: ask super/woman/man.py, super/large_child_or_big_company/call_able_point_by_problem/say_eye_for_man/long_eye/work_large_place.py, super/do_year_from_person.py or super/own_hand_and_long_work/call_able_week/same_place/other_day_or_own_world.py in small work"

---
+++ super/woman/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_fact_at_group(str_arg):
+    think_other_eye(str_arg)
+    print('see_man_from_old_group')
+
+def think_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_fact_at_group('woman_or_year')
