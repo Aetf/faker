a06d91ef1ec16f6c3131f55625f40d98797abc56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:42:26 -0400

    "mod: try super/know_place_on_bad_fact.py for right government"

---
+++ super/know_place_on_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_public_part_with_company(str_arg):
+    way(str_arg)
+    print('good_world_and_first_group')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_public_part_with_company('way')
