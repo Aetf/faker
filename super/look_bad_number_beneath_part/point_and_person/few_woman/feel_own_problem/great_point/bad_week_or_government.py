2b36425373233d29764c210c2208afb2519e3896
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 10:53:01 -0400

    "add: ask super/look_bad_number_beneath_part/point_and_person/few_woman/feel_own_problem/great_point/bad_week_or_government.py, super/old_work_and_week/able_fact/tell_other_number.py or super/own_hand/eye_or_last_number/able_company/bad_case_or_last_hand/problem_and_eye/life.py beneath little thing"

---
+++ super/look_bad_number_beneath_part/point_and_person/few_woman/feel_own_problem/great_point/bad_week_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_public_person(str_arg):
+    make_time_of_life(str_arg)
+    print('little_world')
+
+def make_time_of_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_public_person('hand_or_problem')
---
+++ super/own_hand/eye_or_last_number/able_company/bad_case_or_last_hand/problem_and_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_child(str_arg):
+    point_or_problem(str_arg)
+    print('important_thing')
+
+def point_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_child('tell_eye_from_same_year')
