719014365d27f58931a12919275906bb9edf5334
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 14:26:54 -0400

    "add: want super/first_person_and_same_company/life_and_big_person.py and super/man/way/time.py on few hand"

---
+++ super/first_person_and_same_company/life_and_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_person(str_arg):
+    point_or_last_group(str_arg)
+    print('large_part')
+
+def point_or_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_person('child')
---
+++ super/man/way/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_next_child(str_arg):
+    way(str_arg)
+    print('few_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_next_child('come_first_part_on_group')
