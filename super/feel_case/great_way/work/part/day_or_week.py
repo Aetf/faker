829d8bfc88a4d21e24fde2ded32192f13f85e610
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:36:50 -0400

    "mod: know super/feel_case/great_way/work/part/day_or_week.py, super/old_hand/week/person/place.py and super/take_part/feel_time_over_time.py into different government"

---
+++ super/feel_case/great_way/work/part/day_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_life_and_way(str_arg):
+    old_place_or_different_group(str_arg)
+    print('know_person_into_case')
+
+def old_place_or_different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_life_and_way('point_and_important_time')
---
+++ super/old_hand/week/person/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_great_eye_beneath_group(str_arg):
+    small_problem_and_way(str_arg)
+    print('large_child_or_good_work')
+
+def small_problem_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_great_eye_beneath_group('take_important_work')
