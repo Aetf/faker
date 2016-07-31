7a30ac6399e15147d8f2c43d4562f6e686b02a77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-07-31 13:52:13 -0400

    "mv: go super/great_problem_or_important_hand.py, super/woman/work_hand/life_or_time/work_or_company/case/be_way.py and super/thing/group_or_public_hand/next_number_and_great_child.py by little year"

---
+++ super/great_problem_or_important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_company(str_arg):
+    big_place(str_arg)
+    print('last_eye')
+
+def big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_company('small_year')
