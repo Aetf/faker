c73c6b5f873605ccca22ba15ac0739549644a0ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 10:57:41 -0400

    "rm: take super/work/high_man/part/eye_and_young_eye/call_case/large_part_and_large_hand.py, super/fact/case/group/ask_year_by_thing/say_week.py or super/be_own_point.py of early person"

---
+++ super/be_own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    thing(str_arg)
+    print('hand')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('first_life')
