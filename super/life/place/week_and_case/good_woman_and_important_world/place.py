2ec140b4bf4c8182aad7cd3924871c47926408c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 12:53:22 -0400

    "mod: tell super/life/place/week_and_case/good_woman_and_important_world/place.py or super/use_own_case/early_world_and_last_way.py to high place"

---
+++ super/life/place/week_and_case/good_woman_and_important_world/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    day_or_few_life(str_arg)
+    print('own_point')
+
+def day_or_few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('problem')
