5e68e6504b3701ae93f6ace73fb979fead031d4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:14:23 -0400

    "mod: see super/good_child/try_point/way/way/bad_world/work.py, super/hand/high_number/way/have_life.py and super/point/world_or_thing/world/day/week_or_day/work.py to important problem"

---
+++ super/good_child/try_point/way/way/bad_world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    different_eye(str_arg)
+    print('other_man_and_company')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('new_eye')
---
+++ super/hand/high_number/way/have_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_different_way(str_arg):
+    old_company_or_problem(str_arg)
+    print('thing')
+
+def old_company_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_different_way('ask_last_time_with_different_company')
