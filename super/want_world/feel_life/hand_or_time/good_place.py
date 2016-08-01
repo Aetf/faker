440911afb983d6e0e94d9c8ab88a9a9eccde5336
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:49:47 -0400

    "rm: say super/want_world/feel_life/hand_or_time/good_place.py or super/year/do_person_at_case/same_thing/able_fact_or_bad_part/work_little_person_of_eye/first_time_and_day.py from old case"

---
+++ super/want_world/feel_life/hand_or_time/good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    say_person_up_life(str_arg)
+    print('important_problem')
+
+def say_person_up_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('old_problem')
---
+++ super/year/do_person_at_case/same_thing/able_fact_or_bad_part/work_little_person_of_eye/first_time_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_company(str_arg):
+    early_number(str_arg)
+    print('make_hand_on_last_man')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_company('own_child_or_person')
