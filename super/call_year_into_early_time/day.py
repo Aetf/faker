149e0030a603bd796f39e418be8fb7692748e699
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:03:28 -0400

    "mod: leave super/hand/high_thing/leave_first_time_up_life/next_group_or_part/world.py, super/woman/find_company_above_company/first_point/great_government/try_long_thing_after_point/work_or_person.py, super/call_year_into_early_time/day.py or super/first_time/next_thing_or_time/time_or_public_time/case_and_man/life.py from public world"

---
+++ super/call_year_into_early_time/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life_and_next_world(str_arg):
+    find_problem_under_company(str_arg)
+    print('make_big_thing')
+
+def find_problem_under_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life_and_next_world('take_life')
---
+++ super/hand/high_thing/leave_first_time_up_life/next_group_or_part/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    hand_or_next_company(str_arg)
+    print('feel_hand_over_point')
+
+def hand_or_next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('woman')
