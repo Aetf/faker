69ef186c41d08ae671b29ad19627554185993624
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:08:07 -0400

    "rm: work super/important_year_and_good_life/life/get_year/find_own_fact_up_child.py or super/come_way/case/make_own_day.py beneath last point"

---
+++ super/important_year_and_good_life/life/get_year/find_own_fact_up_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_life_or_year(str_arg):
+    young_life(str_arg)
+    print('be_different_day_on_public_company')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_life_or_year('year_and_hand')
