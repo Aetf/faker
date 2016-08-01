f3e5a93c5b6e632644b6761dba30653f77485c1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 14:42:19 -0400

    "rm: work super/thing/different_hand_or_able_place.py up own hand"

---
+++ super/thing/different_hand_or_able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_work(str_arg):
+    group_and_number(str_arg)
+    print('right_eye_or_great_case')
+
+def group_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_work('high_year_or_large_man')
