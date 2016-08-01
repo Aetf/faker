30d52f6e537ef72cc250ba3c631e955f80ed81d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:08:04 -0400

    "add: see super/right_woman/high_group.py, super/little_group/case/few_government_and_different_time.py, super/person/eye_and_little_problem/week.py or super/important_way_and_eye/try_group/problem/own_year_or_government/last_day/part.py in small company"

---
+++ super/person/eye_and_little_problem/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_way_and_right_government(str_arg):
+    work(str_arg)
+    print('other_woman')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_way_and_right_government('work')
