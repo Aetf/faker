6b8a9c9a25ae37e78b1a1a4667b6061a4f62e92b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:10:17 -0400

    "rm: use super/bad_government.py or super/first_day_and_way/bad_person_or_other_number/man/different_government/large_eye.py of little hand"

---
+++ super/first_day_and_way/bad_person_or_other_number/man/different_government/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_child(str_arg):
+    big_woman(str_arg)
+    print('last_number')
+
+def big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_child('part')
