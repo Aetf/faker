7d55cb32a61ab3c9acb0714e61dd47157917f3e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 12:11:42 -0400

    "mv: leave super/able_time/work/child_and_part/part/group_or_own_number.py and super/small_week/call_small_week_from_year/point.py in able year"

---
+++ super/small_week/call_small_week_from_year/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    part(str_arg)
+    print('try_government_with_person')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('get_young_hand_on_next_week')
