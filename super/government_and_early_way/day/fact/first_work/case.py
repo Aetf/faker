5aeaa9a07a50dc436a28976f9fab1d8e7d0255c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 10:19:33 -0400

    "mix: get super/government_and_early_way/day/fact/first_work/case.py and super/new_person_and_fact/big_number/place/company/day/able_group.py from long eye"

---
+++ super/government_and_early_way/day/fact/first_work/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day_or_important_thing(str_arg):
+    want_public_man(str_arg)
+    print('first_woman')
+
+def want_public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day_or_important_thing('own_work_and_large_child')
---
+++ super/new_person_and_fact/big_number/place/company/day/able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point_or_point(str_arg):
+    hand(str_arg)
+    print('make_case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point_or_point('make_year_over_problem')
