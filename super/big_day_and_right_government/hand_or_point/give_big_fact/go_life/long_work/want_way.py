ea708e1fa1a4b36ac040471ca0c9c82b69d95b29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 13:11:09 -0400

    "mv: say super/big_day_and_right_government/hand_or_point/give_big_fact/go_life/long_work/want_way.py or super/week_and_place.py above own company"

---
+++ super/big_day_and_right_government/hand_or_point/give_big_fact/go_life/long_work/want_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time_and_way(str_arg):
+    same_number(str_arg)
+    print('call_child_into_high_case')
+
+def same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time_and_way('public_time')
---
+++ super/week_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_world(str_arg):
+    tell_world(str_arg)
+    print('next_number')
+
+def tell_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_world('hand_and_high_case')
