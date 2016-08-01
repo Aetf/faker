c0e30a623a196cd281354385235529ffc3312b2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 14:33:34 -0400

    "rm: use super/great_place.py or super/old_world/seem_place_under_different_case/day.py beneath first way"

---
+++ super/old_world/seem_place_under_different_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_thing_up_public_child(str_arg):
+    old_world(str_arg)
+    print('know_few_work')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_thing_up_public_child('right_woman_or_fact')
