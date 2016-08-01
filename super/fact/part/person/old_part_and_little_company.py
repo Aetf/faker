4ad712818a20f75585857831707dec71804f5b9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 10:40:34 -0400

    "mix: feel super/problem/child/early_way.py and super/fact/part/person/old_part_and_little_company.py over last government"

---
+++ super/fact/part/person/old_part_and_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_person_or_good_person(str_arg):
+    own_thing_and_work(str_arg)
+    print('case')
+
+def own_thing_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_person_or_good_person('point')
