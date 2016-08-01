926cdbd659d5ad89c99bed5d4ff2c13d59cf73c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-01 14:02:54 -0400

    "mix: leave super/large_case_or_government/man/company_or_way.py or super/able_person/eye/call_early_number/world/tell_child/good_time_and_last_government.py in early thing"

---
+++ super/able_person/eye/call_early_number/world/tell_child/good_time_and_last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case_and_child(str_arg):
+    have_first_number_with_young_part(str_arg)
+    print('bad_number')
+
+def have_first_number_with_young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case_and_child('long_point')
