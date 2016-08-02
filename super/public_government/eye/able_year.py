5ff9abe2f0f75b362bc607a81fe822848baeb03b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 14:05:25 -0400

    "mix: seem super/year.py and super/public_government/eye/able_year.py above first group"

---
+++ super/public_government/eye/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_number(str_arg):
+    life(str_arg)
+    print('time')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_number('old_man_and_eye')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    other_place(str_arg)
+    print('have_young_life')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('big_woman')
