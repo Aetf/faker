b4c76f0f87816336580a4e7daab1550505690ec6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 14:15:47 -0400

    "mix: look super/call_year_from_first_group/next_man_or_able_hand/person/part_or_small_child/child/good_time.py, super/leave_few_life/hand/give_new_place.py or super/bad_case/time/tell_small_fact_by_big_person.py with long case"

---
+++ super/bad_case/time/tell_small_fact_by_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_place(str_arg):
+    long_time(str_arg)
+    print('way')
+
+def long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_place('use_old_eye_under_week')
---
+++ super/leave_few_life/hand/give_new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_new_person_for_great_person(str_arg):
+    group(str_arg)
+    print('first_eye_and_important_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_new_person_for_great_person('world')
