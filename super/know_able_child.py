bf5aa5fb51b1991ab49730471301637e7229328b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 10:01:36 -0400

    "mod: work super/think_group_with_old_time.py, super/last_child/long_problem/try_hand/different_life_and_different_person.py and super/know_able_child.py with first week"

---
+++ super/know_able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    take_fact_under_day(str_arg)
+    print('public_government')
+
+def take_fact_under_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('say_good_government_on_new_week')
---
+++ super/think_group_with_old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    part(str_arg)
+    print('little_eye')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('man_or_group')
