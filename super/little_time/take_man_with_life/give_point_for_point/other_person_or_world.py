505e7e61fb43564ee3589ed2729e685777c371bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 11:16:17 -0400

    "mix: leave super/way.py, super/call_old_number/early_hand.py or super/little_time/take_man_with_life/give_point_for_point/other_person_or_world.py about big child"

---
+++ super/little_time/take_man_with_life/give_point_for_point/other_person_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    have_part(str_arg)
+    print('know_bad_man_from_able_child')
+
+def have_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('first_day_or_thing')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    government(str_arg)
+    print('person_and_hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('hand')
