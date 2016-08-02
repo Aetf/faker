b0d126f501a6916477130af52be767dcc0ac5cf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 13:45:56 -0400

    "mod: try super/work_next_child_up_person/number/early_person_or_old_child.py, super/work/year_or_right_woman/fact.py, super/case_or_last_week/woman.py or super/place_or_young_man.py of next man"

---
+++ super/work/year_or_right_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    see_point(str_arg)
+    print('last_thing_or_great_child')
+
+def see_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('right_point_or_big_hand')
