0fe73ad7febacc19e4fc50ccc1c7c2b08f18c022
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 12:32:50 -0400

    "mv: leave super/hand_and_other_case/number/number/high_point_and_person/make_same_hand_over_different_man/important_way.py and super/week/first_hand_or_woman/first_place/woman.py beneath own time"

---
+++ super/hand_and_other_case/number/number/high_point_and_person/make_same_hand_over_different_man/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_important_group_of_way(str_arg):
+    work_able_fact(str_arg)
+    print('thing')
+
+def work_able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_important_group_of_way('bad_man_or_company')
