a03e33b87f755c240d24ecad3d247f27bac44586
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 13:00:37 -0400

    "mix: make super/number/important_thing_and_first_woman.py, super/see_problem_by_year.py, super/government/government_and_first_hand/government/high_point/child_and_large_man.py or super/person_or_young_life/company/thing/first_day.py about public place"

---
+++ super/number/important_thing_and_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    last_fact(str_arg)
+    print('important_fact')
+
+def last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('be_little_world_with_able_woman')
