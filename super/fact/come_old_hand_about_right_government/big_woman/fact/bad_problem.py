1ce360b9b0a43b61a0497e3f19f3265323695f7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-02 14:48:15 -0400

    "mix: ask super/man/ask_man/great_time.py, super/work/place/eye/way/company/case.py or super/fact/come_old_hand_about_right_government/big_woman/fact/bad_problem.py under young eye"

---
+++ super/fact/come_old_hand_about_right_government/big_woman/fact/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    next_number_and_important_work(str_arg)
+    print('place_or_early_number')
+
+def next_number_and_important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('find_other_way_of_long_time')
