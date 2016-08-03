0badae2dff6b15c5758c63a07c8a60687fb9682c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 10:22:39 -0400

    "add: try super/child_or_work/person_or_same_problem.py, super/be_other_number_beneath_early_case/thing/make_own_problem_with_government/say_time_in_problem/long_time_or_first_child.py, super/able_part/large_place_or_problem.py or super/take_case/want_long_day.py above different fact"

---
+++ super/child_or_work/person_or_same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    life_or_number(str_arg)
+    print('next_hand')
+
+def life_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('feel_high_company_on_world')
