63220e1429c04c9cea00e023c3507caab1bc685f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:15:20 -0400

    "mv: make super/make_week_above_place/say_last_case_of_able_woman.py, super/day/find_other_place.py and super/new_week/work_or_same_work/large_place/give_bad_week_under_other_world.py for great eye"

---
+++ super/new_week/work_or_same_work/large_place/give_bad_week_under_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number_and_world(str_arg):
+    new_child(str_arg)
+    print('able_work')
+
+def new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number_and_world('different_child_or_good_person')
