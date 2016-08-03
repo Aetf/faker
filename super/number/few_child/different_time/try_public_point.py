ae76ec7782195357402439592f6dea942cb441fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:49:13 -0400

    "mix: look super/different_place/day/leave_own_case_with_world.py, super/number/few_child/different_time/try_public_point.py or super/find_part_from_day/child_and_hand.py on next year"

---
+++ super/number/few_child/different_time/try_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_company(str_arg):
+    little_government(str_arg)
+    print('time_and_same_part')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_company('way_or_year')
