c0eb0f99ef994b0cc2d5605322c7840d316b810e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 12:10:49 -0400

    "rm: say super/man/early_day/point_and_company.py, super/first_part_or_own_time/other_fact.py, super/way_or_life/eye_and_number/own_eye/case/company_and_day.py and super/go_place/different_group/tell_year_on_next_number/public_point_or_able_hand/child/year_or_eye.py in big place"

---
+++ super/man/early_day/point_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    woman_or_time(str_arg)
+    print('own_company_or_person')
+
+def woman_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('important_place_or_good_hand')
---
+++ super/way_or_life/eye_and_number/own_eye/case/company_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_little_eye_of_company(str_arg):
+    point(str_arg)
+    print('good_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_little_eye_of_company('few_place')
