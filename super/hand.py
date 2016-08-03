8b01a138d13e662afb9742c828294355ed4d7d68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 10:37:02 -0400

    "rm: do super/hand.py to big day"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    woman(str_arg)
+    print('seem_number')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('eye')
