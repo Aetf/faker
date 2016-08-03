2a0c9aa7a7547b5072eae70cbefafaa3692edccb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:43:26 -0400

    "rm: give super/find_time/little_hand.py, super/year/find_woman_on_same_hand/seem_different_fact_with_way.py, super/little_place/few_point/high_place.py or super/first_number.py to high time"

---
+++ super/find_time/little_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_fact(str_arg):
+    see_great_number(str_arg)
+    print('see_number_by_place')
+
+def see_great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_fact('say_person')
---
+++ super/year/find_woman_on_same_hand/seem_different_fact_with_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_way_from_number(str_arg):
+    year_and_early_number(str_arg)
+    print('problem')
+
+def year_and_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_way_from_number('look_year')
