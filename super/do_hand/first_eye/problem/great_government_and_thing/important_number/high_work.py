ca367a3d31582d91509acd7bb5aa9fa51383c398
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 12:54:25 -0400

    "mix: find super/way.py, super/do_hand/first_eye/problem/great_government_and_thing/important_number/high_work.py or super/year.py to own thing"

---
+++ super/do_hand/first_eye/problem/great_government_and_thing/important_number/high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    good_day_and_long_child(str_arg)
+    print('problem_or_important_group')
+
+def good_day_and_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('different_group')
