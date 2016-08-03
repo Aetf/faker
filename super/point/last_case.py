9e668bf5f542a20bcdcb5c87ace66f4603ef5a57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 13:26:20 -0400

    "mod: say super/point/last_case.py with few number"

---
+++ super/point/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    look_eye_for_other_work(str_arg)
+    print('last_time_and_world')
+
+def look_eye_for_other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('say_life_for_first_part')
