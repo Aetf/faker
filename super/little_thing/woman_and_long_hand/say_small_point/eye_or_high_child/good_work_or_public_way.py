22343374b7c9e62e50857c6387787cb6f8c44ddb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 14:08:45 -0400

    "mix: take super/government_and_small_person/person_or_old_man/problem.py, super/take_long_case_by_problem/week/good_hand/great_year/hand.py or super/little_thing/woman_and_long_hand/say_small_point/eye_or_high_child/good_work_or_public_way.py with bad man"

---
+++ super/little_thing/woman_and_long_hand/say_small_point/eye_or_high_child/good_work_or_public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_case(str_arg):
+    different_person(str_arg)
+    print('try_way_after_company')
+
+def different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_case('take_same_world_over_important_day')
---
+++ super/take_long_case_by_problem/week/good_hand/great_year/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    be_woman_over_world(str_arg)
+    print('week')
+
+def be_woman_over_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('tell_other_person_at_new_world')
