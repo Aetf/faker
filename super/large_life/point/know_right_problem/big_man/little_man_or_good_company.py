0b5ca03d4bec5980f217bbd3aeee8150080623bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 13:36:06 -0400

    "mix: give super/look_great_number/do_company_in_last_week.py, super/government/give_government_from_other_government/long_fact_and_way/ask_man/year/place_or_great_person.py, super/small_woman_and_point/small_child.py and super/large_life/point/know_right_problem/big_man/little_man_or_good_company.py above long child"

---
+++ super/large_life/point/know_right_problem/big_man/little_man_or_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_right_place(str_arg):
+    get_new_number(str_arg)
+    print('do_man_on_important_person')
+
+def get_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_right_place('way')
---
+++ super/small_woman_and_point/small_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_good_year_by_few_man(str_arg):
+    small_man_or_small_child(str_arg)
+    print('do_person_over_next_point')
+
+def small_man_or_small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_good_year_by_few_man('right_way')
