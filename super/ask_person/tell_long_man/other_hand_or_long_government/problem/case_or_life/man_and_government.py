c658e6a569930cc6e90a43a2527fcd072ba4bcc9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 13:23:50 -0400

    "mix: work super/point/fact/public_part_or_bad_day/high_government.py and super/ask_person/tell_long_man/other_hand_or_long_government/problem/case_or_life/man_and_government.py in long fact"

---
+++ super/ask_person/tell_long_man/other_hand_or_long_government/problem/case_or_life/man_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_child(str_arg):
+    old_life_or_great_eye(str_arg)
+    print('next_man')
+
+def old_life_or_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_child('time_or_hand')
---
+++ super/point/fact/public_part_or_bad_day/high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_number_about_thing(str_arg):
+    early_place_and_child(str_arg)
+    print('part_and_early_group')
+
+def early_place_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_number_about_thing('point')
