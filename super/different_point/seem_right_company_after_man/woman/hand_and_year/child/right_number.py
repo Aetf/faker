d24887a453fe06140144f8e973ab241e75de6257
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:21:57 -0400

    "rm: get super/different_point/seem_right_company_after_man/woman/hand_and_year/child/right_number.py and super/young_point_or_place/do_fact.py under young day"

---
+++ super/different_point/seem_right_company_after_man/woman/hand_and_year/child/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_different_eye(str_arg):
+    early_way(str_arg)
+    print('seem_man_about_year')
+
+def early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_different_eye('go_problem_of_little_fact')
---
+++ super/young_point_or_place/do_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place(str_arg):
+    woman_and_person(str_arg)
+    print('high_life')
+
+def woman_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place('hand')
