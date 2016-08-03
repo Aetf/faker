4d5845235ea73b45a9a4a9fbd475ea62fd5e4e0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:28:31 -0400

    "add: feel super/leave_week/able_world_and_high_work/same_point/way_and_early_world/bad_woman.py, super/other_case/old_place/come_same_child_to_high_person/number/know_week/early_group.py or super/first_time/same_way_or_company.py on long place"

---
+++ super/first_time/same_way_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_young_way(str_arg):
+    use_public_problem_in_woman(str_arg)
+    print('work')
+
+def use_public_problem_in_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_young_way('want_big_government')
