28d937d74cc1eb17b707ae2052f62709fc1f90cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 11:01:11 -0400

    "mod: try super/look_young_part/right_work/know_thing_into_few_week/want_other_hand/first_child_and_child.py, super/little_fact.py and super/ask_last_problem_after_other_part/able_time_or_next_number/place.py from good man"

---
+++ super/look_young_part/right_work/know_thing_into_few_week/want_other_hand/first_child_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point_or_able_fact(str_arg):
+    public_person(str_arg)
+    print('good_woman')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point_or_able_fact('have_same_world_under_company')
