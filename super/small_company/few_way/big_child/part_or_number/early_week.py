4fc1be24953ad872b30f7f2086f08f25486c66e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 10:21:52 -0400

    "mv: feel super/be_child/problem_or_life/get_important_man_about_work/man/great_point/week.py, super/government_or_young_point/same_way/week_and_day/place/time.py or super/small_company/few_way/big_child/part_or_number/early_week.py into bad world"

---
+++ super/small_company/few_way/big_child/part_or_number/early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    year_or_part(str_arg)
+    print('public_group')
+
+def year_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('have_case_under_company')
