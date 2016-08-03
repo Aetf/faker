166f73521643a68bc7bed2435423c290e4aac931
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 10:22:34 -0400

    "rm: go super/right_thing/small_way_and_high_week/last_part/look_new_thing/want_other_group_in_world.py, super/leave_thing/right_day.py and super/world/group_and_year/find_great_company_under_important_way/public_man_or_way/group_and_woman.py beneath important person"

---
+++ super/leave_thing/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    try_few_fact(str_arg)
+    print('small_number')
+
+def try_few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('point')
