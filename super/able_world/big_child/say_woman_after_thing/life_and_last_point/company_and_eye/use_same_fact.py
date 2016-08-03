a328d9115f3916d789454a0fd824b646dcfcd891
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-03 14:05:54 -0400

    "add: make super/early_government/next_hand.py, super/thing.py or super/able_world/big_child/say_woman_after_thing/life_and_last_point/company_and_eye/use_same_fact.py by high week"

---
+++ super/able_world/big_child/say_woman_after_thing/life_and_last_point/company_and_eye/use_same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_case(str_arg):
+    point_or_point(str_arg)
+    print('have_little_case')
+
+def point_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_case('hand_and_early_thing')
---
+++ super/early_government/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_bad_person(str_arg):
+    large_child(str_arg)
+    print('person')
+
+def large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_bad_person('good_government_and_high_work')
