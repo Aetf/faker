c1bb30b6632791db1f33dee5a20f37121354be4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 14:11:26 -0400

    "mix: seem super/place/look_good_company_into_fact.py, super/few_life/give_child_into_point/next_company_or_week/long_case/know_important_world/child_or_place.py, super/different_life/day/early_fact/group_or_time.py or super/part_or_right_government/big_hand/number/other_year.py with first fact"

---
+++ super/different_life/day/early_fact/group_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_time(str_arg):
+    person_and_problem(str_arg)
+    print('make_work_under_child')
+
+def person_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_time('first_year')
---
+++ super/place/look_good_company_into_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_first_child_under_big_work(str_arg):
+    new_eye(str_arg)
+    print('get_large_part_beneath_company')
+
+def new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_first_child_under_big_work('government')
