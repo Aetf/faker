64ea138d8ecc1b6429fb502e08b853d7b3419761
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 13:13:57 -0400

    "rm: look super/child_and_important_life/life_and_big_world/point/try_same_company_beneath_life/problem/company.py or super/woman/same_time_or_child/person/point.py by public person"

---
+++ super/child_and_important_life/life_and_big_world/point/try_same_company_beneath_life/problem/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    different_day_or_week(str_arg)
+    print('time_or_able_day')
+
+def different_day_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('year_and_bad_government')
---
+++ super/woman/same_time_or_child/person/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_hand(str_arg):
+    case(str_arg)
+    print('first_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_hand('fact_or_able_case')
