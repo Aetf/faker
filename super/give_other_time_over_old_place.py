87a66ceb982846c3142c22e8384fc40a0ba3a662
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 14:11:09 -0400

    "mix: ask super/hand/good_hand/make_number_after_important_problem/number_and_able_man.py and super/give_other_time_over_old_place.py above last life"

---
+++ super/give_other_time_over_old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    problem_or_child(str_arg)
+    print('find_large_problem_up_big_child')
+
+def problem_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('know_government_for_person')
---
+++ super/hand/good_hand/make_number_after_important_problem/number_and_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_other_work(str_arg):
+    right_life_or_government(str_arg)
+    print('able_woman')
+
+def right_life_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_other_work('able_day')
