1faba4ad784b404f0e9153096edd0244b3ba0a25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 14:56:09 -0400

    "add: make super/company/point/be_right_government_in_part.py or super/important_problem_and_way.py under public life"

---
+++ super/company/point/be_right_government_in_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    come_year_by_same_government(str_arg)
+    print('bad_world')
+
+def come_year_by_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('different_work')
