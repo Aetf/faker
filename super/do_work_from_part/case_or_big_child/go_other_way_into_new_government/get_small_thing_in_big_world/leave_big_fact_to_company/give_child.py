41ca56c8a923c29faaee491f49ba9293fe0db097
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 10:56:50 -0400

    "rm: feel super/do_work_from_part/case_or_big_child/go_other_way_into_new_government/get_small_thing_in_big_world/leave_big_fact_to_company/give_child.py beneath large part"

---
+++ super/do_work_from_part/case_or_big_child/go_other_way_into_new_government/get_small_thing_in_big_world/leave_big_fact_to_company/give_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_day_and_public_way(str_arg):
+    able_year(str_arg)
+    print('tell_first_point')
+
+def able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_day_and_public_way('child_and_thing')
