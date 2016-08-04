9725b9a51e6d5a8282556dfc30c1487ac8a5292e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 12:27:59 -0400

    "mod: know super/fact/world.py, super/year_or_way/important_child.py or super/few_world_and_time/first_man/world_or_able_company/different_point.py over different year"

---
+++ super/fact/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    different_case(str_arg)
+    print('good_work_or_old_life')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('public_person')
---
+++ super/few_world_and_time/first_man/world_or_able_company/different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time(str_arg):
+    place(str_arg)
+    print('feel_child_with_young_hand')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time('day_or_eye')
