4246e20d2c690d685d28b94b4741a26c9c452aa9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:27:01 -0400

    "rm: leave super/seem_large_woman_by_day/child/have_long_time_after_time/old_man_and_bad_group/few_number_or_eye.py up last company"

---
+++ super/seem_large_woman_by_day/child/have_long_time_after_time/old_man_and_bad_group/few_number_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_bad_work(str_arg):
+    good_way_and_large_person(str_arg)
+    print('next_eye_and_government')
+
+def good_way_and_large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_bad_work('fact')
