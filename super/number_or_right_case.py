68e4584cd63c2d6b7d1d64940193d628b115b5a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 14:50:21 -0400

    "rm: find super/number_or_right_case.py up first time"

---
+++ super/number_or_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_different_woman_of_big_man(str_arg):
+    know_group(str_arg)
+    print('right_day_and_government')
+
+def know_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_different_woman_of_big_man('different_number')
