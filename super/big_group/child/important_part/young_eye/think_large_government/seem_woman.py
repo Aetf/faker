6cb6619f85ab5d473ac7d1d40ae8b3267366b049
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:25:56 -0400

    "mix: come super/know_life_over_number/ask_week_about_point/get_problem_over_week/able_man/make_hand/long_week.py, super/come_hand/see_next_group/point/able_point.py or super/big_group/child/important_part/young_eye/think_large_government/seem_woman.py over new life"

---
+++ super/big_group/child/important_part/young_eye/think_large_government/seem_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_woman(str_arg):
+    right_person(str_arg)
+    print('young_work')
+
+def right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_woman('know_large_child_to_case')
---
+++ super/come_hand/see_next_group/point/able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman(str_arg):
+    life(str_arg)
+    print('do_life_in_life')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman('important_woman')
