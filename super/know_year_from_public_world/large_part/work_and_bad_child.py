26bceb9f88bf0ba4688771117fdf673c89707c46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 12:12:13 -0400

    "mv: take super/public_man/company.py or super/know_year_from_public_world/large_part/work_and_bad_child.py of high number"

---
+++ super/know_year_from_public_world/large_part/work_and_bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_next_eye(str_arg):
+    right_world(str_arg)
+    print('thing')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_next_eye('problem')
