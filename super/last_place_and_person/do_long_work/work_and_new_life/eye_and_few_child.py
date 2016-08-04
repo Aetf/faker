c830751525cf460ec893af99843a27513f61cd3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 12:40:17 -0400

    "rm: be super/seem_old_eye_for_work/point/part.py, super/big_week/child/little_point.py, super/last_place_and_person/do_long_work/work_and_new_life/eye_and_few_child.py and super/early_part/world/make_thing/use_group/problem.py over important thing"

---
+++ super/last_place_and_person/do_long_work/work_and_new_life/eye_and_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_eye(str_arg):
+    first_work_and_early_person(str_arg)
+    print('company_or_eye')
+
+def first_work_and_early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_eye('other_work_and_first_life')
---
+++ super/seem_old_eye_for_work/point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_little_problem_with_problem(str_arg):
+    use_big_company_at_few_child(str_arg)
+    print('do_woman_over_woman')
+
+def use_big_company_at_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_little_problem_with_problem('see_time')
