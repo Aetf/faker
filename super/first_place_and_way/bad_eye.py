80a01a227d2c1bd4946ea8e1ca0a4d28ea028f58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:22:04 -0400

    "mv: work super/time_or_world.py, super/first_place_and_way/bad_eye.py, super/work_large_man/early_case/case.py and super/thing/think_problem.py under high person"

---
+++ super/first_place_and_way/bad_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand(str_arg):
+    last_year(str_arg)
+    print('feel_long_child_on_work')
+
+def last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand('work_old_way')
---
+++ super/work_large_man/early_case/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_young_group_beneath_year(str_arg):
+    long_eye(str_arg)
+    print('able_work')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_young_group_beneath_year('small_hand')
