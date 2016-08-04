2083eb1953eedc195188ca31800a20a664a5d072
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:43:48 -0400

    "mod: tell super/long_work/world/part_or_time/small_week.py and super/different_group_and_young_woman/first_company_or_case/long_life/day.py on old life"

---
+++ super/different_group_and_young_woman/first_company_or_case/long_life/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    group_or_next_world(str_arg)
+    print('leave_new_work_in_next_case')
+
+def group_or_next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('case_and_child')
---
+++ super/long_work/world/part_or_time/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place(str_arg):
+    person(str_arg)
+    print('leave_problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place('try_early_life')
