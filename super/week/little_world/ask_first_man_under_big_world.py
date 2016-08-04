6308962de8569a68f8f4a3ffeb885b46c18428a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 14:41:37 -0400

    "rm: be super/week/little_world/ask_first_man_under_big_world.py from public life"

---
+++ super/week/little_world/ask_first_man_under_big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    high_group_or_old_person(str_arg)
+    print('good_hand')
+
+def high_group_or_old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('public_week')
