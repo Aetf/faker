10037a09d9cd8893e3d2129d639fedf54bce897c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:06:40 -0400

    "mix: take super/ask_other_company_at_week/number/woman/last_woman/eye.py and super/large_day/different_part_and_small_government/different_world.py at same thing"

---
+++ super/ask_other_company_at_week/number/woman/last_woman/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_government(str_arg):
+    time(str_arg)
+    print('hand')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_government('high_year_and_work')
