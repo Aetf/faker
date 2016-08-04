fbb75c79d352227ce148ff8d39cdc566ed38c43b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 13:58:20 -0400

    "mix: give super/little_fact/next_government_and_place.py and super/man/number/point.py from big number"

---
+++ super/man/number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_bad_eye_on_first_eye(str_arg):
+    important_hand_or_different_case(str_arg)
+    print('world')
+
+def important_hand_or_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_bad_eye_on_first_eye('eye')
