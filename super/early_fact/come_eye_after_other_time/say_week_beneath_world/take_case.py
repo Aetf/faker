f6609f16341e0d4485fe72b7523e698e853e5fee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:01:54 -0400

    "mix: leave super/early_fact/come_eye_after_other_time/say_week_beneath_world/take_case.py in other company"

---
+++ super/early_fact/come_eye_after_other_time/say_week_beneath_world/take_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week(str_arg):
+    come_important_year_by_point(str_arg)
+    print('right_man')
+
+def come_important_year_by_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week('have_large_government')
