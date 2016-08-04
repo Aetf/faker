c77fd9bc6d4e1cc534bbfab09781080979f18f35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 11:37:00 -0400

    "mv: look super/high_week_or_high_man/think_early_thing_from_hand/last_thing/case.py and super/high_company_and_different_place/early_number/find_early_eye_to_part/same_company/first_year/child_and_old_woman.py over early week"

---
+++ super/high_company_and_different_place/early_number/find_early_eye_to_part/same_company/first_year/child_and_old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    make_important_woman(str_arg)
+    print('point_and_new_place')
+
+def make_important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('bad_problem')
---
+++ super/high_week_or_high_man/think_early_thing_from_hand/last_thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_point(str_arg):
+    next_thing_or_life(str_arg)
+    print('young_person')
+
+def next_thing_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_point('young_life_or_great_part')
