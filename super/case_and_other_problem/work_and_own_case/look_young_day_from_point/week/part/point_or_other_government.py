df0f495996a28094fe0acb48e6b78039b11882ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-04 10:03:45 -0400

    "rm: look super/case_and_other_problem/work_and_own_case/look_young_day_from_point/week/part/point_or_other_government.py, super/important_hand/feel_government_about_right_time/place.py and super/life.py under next way"

---
+++ super/case_and_other_problem/work_and_own_case/look_young_day_from_point/week/part/point_or_other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    feel_early_group_over_great_week(str_arg)
+    print('little_place')
+
+def feel_early_group_over_great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('fact')
---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_day(str_arg):
+    life_or_long_week(str_arg)
+    print('same_man')
+
+def life_or_long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_day('life')
