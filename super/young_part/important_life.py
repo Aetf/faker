779c22f1c4c5fddb2117c46d493b758c9f183399
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 12:55:23 -0400

    "rm: work super/young_part/important_life.py or super/place.py about different part"

---
+++ super/young_part/important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_fact(str_arg):
+    other_life(str_arg)
+    print('look_long_eye')
+
+def other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_fact('last_government_or_week')
