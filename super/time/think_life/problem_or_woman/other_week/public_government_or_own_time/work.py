6fafb50cfafddeafcb9ee13ee9667a6b634db3be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 14:32:12 -0400

    "mv: look super/young_way/find_fact_into_other_child/world/fact/large_thing/get_group_into_child.py and super/time/think_life/problem_or_woman/other_week/public_government_or_own_time/work.py from important year"

---
+++ super/time/think_life/problem_or_woman/other_week/public_government_or_own_time/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_place(str_arg):
+    know_child_about_right_day(str_arg)
+    print('tell_woman_under_important_group')
+
+def know_child_about_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_place('thing')
---
+++ super/young_way/find_fact_into_other_child/world/fact/large_thing/get_group_into_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    point_or_week(str_arg)
+    print('bad_part')
+
+def point_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('group_or_last_man')
