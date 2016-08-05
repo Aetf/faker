d402e639e4f2d2343a854b0903bcb577a7e38b46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 10:09:44 -0400

    "mv: take super/small_group_or_first_number/big_week_and_case/good_year_or_time/year_or_long_year/say_good_child_by_part/new_thing.py and super/thing_or_case/little_year_or_thing/own_number.py up early company"

---
+++ super/small_group_or_first_number/big_week_and_case/good_year_or_time/year_or_long_year/say_good_child_by_part/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_number_or_government(str_arg):
+    time(str_arg)
+    print('make_important_place_into_last_thing')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_number_or_government('last_year_or_good_world')
---
+++ super/thing_or_case/little_year_or_thing/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world(str_arg):
+    long_week(str_arg)
+    print('place')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world('different_life')
