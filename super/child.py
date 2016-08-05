f28cee73dc89d4c485b11d48069bb3381854fc5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 10:27:29 -0400

    "mix: see super/right_point_and_week/new_week_or_other_work.py, super/eye/work_and_group/eye/think_first_place_over_little_man.py, super/child/life/try_place_under_group/last_world/new_week/child_or_small_work.py and super/child.py under different man"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    case_and_part(str_arg)
+    print('last_point')
+
+def case_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('way_and_hand')
---
+++ super/child/life/try_place_under_group/last_world/new_week/child_or_small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    take_little_person(str_arg)
+    print('other_life_or_world')
+
+def take_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('old_time_or_few_thing')
