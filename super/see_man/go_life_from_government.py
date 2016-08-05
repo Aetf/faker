8473838975a9f3616e92f6e8d198dca69588e18b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 14:51:50 -0400

    "mv: ask super/see_man/go_life_from_government.py over high fact"

---
+++ super/see_man/go_life_from_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person_and_early_government(str_arg):
+    thing(str_arg)
+    print('new_week')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person_and_early_government('want_hand_for_small_thing')
