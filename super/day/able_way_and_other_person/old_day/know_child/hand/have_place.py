2fb43173891c3fafbcec94f4ada35c0e087421c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 12:56:22 -0400

    "mix: take super/day/able_way_and_other_person/old_day/know_child/hand/have_place.py and super/make_early_year/great_year/long_year/long_life/world/person.py in big government"

---
+++ super/day/able_way_and_other_person/old_day/know_child/hand/have_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_child(str_arg):
+    try_little_fact(str_arg)
+    print('work')
+
+def try_little_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_child('see_long_point_on_woman')
