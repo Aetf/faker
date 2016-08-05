4b097d52d0c7f52e706900f5ce19178bdfa035b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 10:26:42 -0400

    "add: try super/point/own_hand/big_way_and_able_man.py at other problem"

---
+++ super/point/own_hand/big_way_and_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    company(str_arg)
+    print('come_different_hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('woman_or_few_world')
