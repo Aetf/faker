a747709f3553a589cd5b4999126b34c5cb30d92a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 10:50:27 -0400

    "add: know super/take_group/new_group/important_time.py, super/company/do_world/person.py or super/do_part_above_new_time/get_world/take_year/day_and_other_case.py on able eye"

---
+++ super/company/do_world/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact_and_person(str_arg):
+    same_part(str_arg)
+    print('time')
+
+def same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact_and_person('point')
---
+++ super/do_part_above_new_time/get_world/take_year/day_and_other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_thing(str_arg):
+    bad_fact(str_arg)
+    print('want_right_case')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_thing('day_and_next_man')
