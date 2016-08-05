bf4993563208631514569d64ce0bf0d0d8d51565
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 12:02:59 -0400

    "rm: look super/want_good_eye_on_different_time/man_or_small_time/life_or_big_time.py up few person"

---
+++ super/want_good_eye_on_different_time/man_or_small_time/life_or_big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    next_eye(str_arg)
+    print('eye')
+
+def next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('first_world')
