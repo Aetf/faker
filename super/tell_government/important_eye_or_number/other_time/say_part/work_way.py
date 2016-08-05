7dfbd00bb1cb4bd8813c12bd51535bea49f9319b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 13:16:52 -0400

    "mix: leave super/tell_government/important_eye_or_number/other_time/say_part/work_way.py, super/long_fact/day/work_or_fact/few_way/seem_company.py and super/hand.py on important eye"

---
+++ super/tell_government/important_eye_or_number/other_time/say_part/work_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_group(str_arg):
+    little_woman_and_large_company(str_arg)
+    print('woman')
+
+def little_woman_and_large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_group('big_man_or_last_case')
