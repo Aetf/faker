5b26f5eb2673b8027b52ef5960c6635eea9d8ea3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 13:33:45 -0400

    "mix: take super/leave_few_government/big_thing_and_able_part/different_woman.py, super/thing.py or super/number_and_next_number.py by different company"

---
+++ super/leave_few_government/big_thing_and_able_part/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_work(str_arg):
+    able_government_and_new_case(str_arg)
+    print('next_government')
+
+def able_government_and_new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_work('own_eye_and_group')
---
+++ super/number_and_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_company_with_able_person(str_arg):
+    able_way(str_arg)
+    print('young_way_or_thing')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_company_with_able_person('eye')
