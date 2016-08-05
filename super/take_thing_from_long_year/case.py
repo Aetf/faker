7cd45e0d8b97707a546a8f8e1f0a7bd3b4a82299
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 13:23:42 -0400

    "add: be super/take_thing_from_long_year/case.py, super/fact/part/take_world_after_woman/long_way/be_few_problem_for_important_government/other_group.py or super/eye_or_new_thing/number/use_high_government_to_little_person/hand/large_point_or_number.py over right hand"

---
+++ super/take_thing_from_long_year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_thing_up_case(str_arg):
+    young_part(str_arg)
+    print('part_or_little_year')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_thing_up_case('world_and_hand')
