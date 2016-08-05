f3f92a9de050834b819a7244906c0b2a76ef3f79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-05 13:28:06 -0400

    "mix: give super/person_or_old_part.py and super/public_point_or_long_place/place/case_or_next_number/big_life/right_world.py about big problem"

---
+++ super/person_or_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    group(str_arg)
+    print('important_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('find_right_part')
