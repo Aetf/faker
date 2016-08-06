7a3c143e7ec84f9818e28fc1fdea225e9e4bc0e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 11:38:26 -0400

    "mix: ask super/make_thing.py, super/first_government_or_year/important_time/group_and_place/large_government/bad_way/high_person.py or super/use_old_number/think_long_problem/fact/want_right_case_at_public_world/little_life.py of different work"

---
+++ super/use_old_number/think_long_problem/fact/want_right_case_at_public_world/little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day(str_arg):
+    hand_and_able_problem(str_arg)
+    print('fact')
+
+def hand_and_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day('great_work')
