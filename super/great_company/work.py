4aa1a08f410788a71673b43f7cb0d415263fc8bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 14:25:26 -0400

    "mod: work super/great_company/work.py into early child"

---
+++ super/great_company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_week(str_arg):
+    part(str_arg)
+    print('own_day_and_number')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_week('bad_world')
