a672819f853f746a18e3252c1c4946e92cc2153a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 13:00:51 -0400

    "add: say super/give_early_man_to_same_case/work_hand/person/get_able_place/able_time_and_great_problem/world_or_same_life.py or super/group/have_high_group_above_new_man/problem/small_thing.py over new life"

---
+++ super/give_early_man_to_same_case/work_hand/person/get_able_place/able_time_and_great_problem/world_or_same_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_own_case(str_arg):
+    big_point(str_arg)
+    print('time_and_large_day')
+
+def big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_own_case('time')
