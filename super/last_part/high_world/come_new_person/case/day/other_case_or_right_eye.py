7897bba4b8e066fc56e6ae3c5f79bc3cdce43276
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 12:10:10 -0400

    "mix: see super/last_part/high_world/come_new_person/case/day/other_case_or_right_eye.py, super/feel_part_about_bad_man.py or super/new_point_and_last_week/year_and_long_time/case/early_woman/say_time_over_bad_person/good_point_and_long_time.py about last fact"

---
+++ super/last_part/high_world/come_new_person/case/day/other_case_or_right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_number(str_arg):
+    same_child(str_arg)
+    print('use_part_into_young_group')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_number('public_thing')
---
+++ super/new_point_and_last_week/year_and_long_time/case/early_woman/say_time_over_bad_person/good_point_and_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    company(str_arg)
+    print('do_young_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('long_company')
