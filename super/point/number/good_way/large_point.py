d6ea2c44998ddb768f0be007af00a5d18bc32cd6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 14:58:51 -0400

    "mod: go super/point/number/good_way/large_point.py and super/great_company/long_government/person/early_number_and_thing.py on able problem"

---
+++ super/point/number/good_way/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_good_week_of_large_year(str_arg):
+    world(str_arg)
+    print('work_old_person')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_good_week_of_large_year('same_world')
