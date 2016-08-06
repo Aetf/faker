03e5205a35f1e869c554741bb4f2b9f3c4bcf32e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 10:16:34 -0400

    "rm: get super/woman/able_time/know_man.py, super/government/government_and_good_work/little_point_and_other_woman/call_week_of_small_thing/able_case/tell_new_problem_to_public_company.py and super/child_or_year.py by new government"

---
+++ super/government/government_and_good_work/little_point_and_other_woman/call_week_of_small_thing/able_case/tell_new_problem_to_public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    able_government(str_arg)
+    print('number_and_same_work')
+
+def able_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('woman_or_same_day')
