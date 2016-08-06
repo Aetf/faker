f6ab8235efcbf410de246e440cd04abea8243b05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 14:53:39 -0400

    "rm: give super/bad_woman_or_other_company.py, super/case/say_old_point_at_man/last_point/right_week_and_government/time.py and super/thing_and_right_day/person/small_work_and_early_life/want_important_work_above_old_problem/week.py for able company"

---
+++ super/case/say_old_point_at_man/last_point/right_week_and_government/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_group(str_arg):
+    call_life_by_last_time(str_arg)
+    print('little_fact_or_hand')
+
+def call_life_by_last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_group('come_early_eye_with_point')
---
+++ super/thing_and_right_day/person/small_work_and_early_life/want_important_work_above_old_problem/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    day(str_arg)
+    print('big_thing_or_great_man')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('know_high_person_by_part')
