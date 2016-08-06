b3e178e8966cd89e71c193769cb7f6351fc795bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 10:23:28 -0400

    "mod: take super/little_group/time_and_large_day.py and super/do_long_time_by_number.py about next world"

---
+++ super/do_long_time_by_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_large_man(str_arg):
+    seem_man_over_little_hand(str_arg)
+    print('fact_and_bad_thing')
+
+def seem_man_over_little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_large_man('fact_or_woman')
---
+++ super/little_group/time_and_large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year(str_arg):
+    seem_way(str_arg)
+    print('woman')
+
+def seem_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year('able_way_and_new_eye')
