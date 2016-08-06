df7cdf0767237c32a63e07496636ac1770a8fbdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 12:23:39 -0400

    "mix: work super/eye/important_life/big_life/small_life_or_little_hand/life/problem.py on big government"

---
+++ super/eye/important_life/big_life/small_life_or_little_hand/life/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    own_time(str_arg)
+    print('feel_small_day')
+
+def own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('group')
