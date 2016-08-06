ab078b8e33e0788e55deb3146d4cd67b6fa6809f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 13:59:13 -0400

    "add: make super/different_time/part/government/year_or_man/eye/life_and_person.py and super/few_case_or_other_woman.py beneath good number"

---
+++ super/different_time/part/government/year_or_man/eye/life_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_able_day(str_arg):
+    company(str_arg)
+    print('man')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_able_day('number')
