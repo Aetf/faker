1d51bef4766765e51b596d4a62faaea3d0b4d3a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 10:37:46 -0400

    "mod: see super/right_problem.py and super/same_eye/public_fact_and_early_point/person/high_thing/last_way.py under big man"

---
+++ super/same_eye/public_fact_and_early_point/person/high_thing/last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    next_world_or_place(str_arg)
+    print('ask_company')
+
+def next_world_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('new_way')
