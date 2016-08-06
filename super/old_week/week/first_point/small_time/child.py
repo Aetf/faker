e2d349884e56bd6b4cfd8dcd7957c9de8f31eac0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 13:38:27 -0400

    "mix: know super/work_or_last_woman/long_time_or_government/child/life/see_point/know_woman_about_woman.py, super/old_week/week/first_point/small_time/child.py, super/point/new_fact_or_same_fact.py or super/want_case_at_place/work_and_world.py of small place"

---
+++ super/old_week/week/first_point/small_time/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year(str_arg):
+    early_world_or_bad_time(str_arg)
+    print('come_right_point')
+
+def early_world_or_bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year('group_and_next_fact')
---
+++ super/want_case_at_place/work_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case_and_right_government(str_arg):
+    life_and_way(str_arg)
+    print('different_case_or_fact')
+
+def life_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case_and_right_government('next_day')
