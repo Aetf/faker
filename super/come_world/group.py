5819655232a7742945e3a90be94c50b164a5d9b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 13:22:33 -0400

    "mv: feel super/part/go_small_thing_above_fact/company/good_woman_and_small_year/ask_fact_beneath_work/little_government.py, super/same_fact/man/time/group/find_way.py, super/come_world/group.py or super/hand_and_right_life/give_small_life_under_woman/use_old_fact_on_other_way/man/own_person.py over small thing"

---
+++ super/come_world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way_and_next_government(str_arg):
+    right_fact_or_small_day(str_arg)
+    print('hand')
+
+def right_fact_or_small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way_and_next_government('group')
