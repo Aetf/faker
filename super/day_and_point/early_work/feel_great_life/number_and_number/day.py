a8a014adafdd6ed455b2891165f0c1d21a36b2de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 11:17:59 -0400

    "rm: feel super/day_and_point/early_work/feel_great_life/number_and_number/day.py, super/feel_week/man_and_number.py and super/company_or_next_life/want_day.py of next number"

---
+++ super/day_and_point/early_work/feel_great_life/number_and_number/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_next_fact(str_arg):
+    eye_or_young_case(str_arg)
+    print('big_number')
+
+def eye_or_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_next_fact('use_company')
---
+++ super/feel_week/man_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_last_part_over_other_time(str_arg):
+    give_woman(str_arg)
+    print('person')
+
+def give_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_last_part_over_other_time('use_number_about_good_problem')
