293d321b6d280b8d599b205c93150cd6cba8a486
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 12:01:35 -0400

    "mod: say super/get_great_day/next_government/hand.py for same thing"

---
+++ super/get_great_day/next_government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_number(str_arg):
+    old_part_or_bad_group(str_arg)
+    print('problem')
+
+def old_part_or_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_number('child_or_last_time')
