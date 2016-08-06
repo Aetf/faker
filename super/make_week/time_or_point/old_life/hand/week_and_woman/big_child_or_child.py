fec2c5acc8e0d24a1da1537300451b25057fd40d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 13:40:38 -0400

    "add: come super/make_week/time_or_point/old_life/hand/week_and_woman/big_child_or_child.py, super/person/get_time_on_problem/group_and_good_day/young_case.py, super/same_week/woman/case_and_man.py and super/government/week/long_work/life.py with little child"

---
+++ super/make_week/time_or_point/old_life/hand/week_and_woman/big_child_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_important_place(str_arg):
+    long_work(str_arg)
+    print('place_and_year')
+
+def long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_important_place('few_world')
---
+++ super/person/get_time_on_problem/group_and_good_day/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    right_eye_and_important_time(str_arg)
+    print('woman_or_next_year')
+
+def right_eye_and_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('person')
