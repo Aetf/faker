b7129d6dafa5e2eb2ce3885daf720b0d2e7d290a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 11:45:42 -0400

    "rm: work super/other_company/year/day/way/seem_bad_woman.py, super/tell_life_under_place/hand/high_day/child/part.py and super/feel_point/make_other_week_about_first_hand.py over few problem"

---
+++ super/feel_point/make_other_week_about_first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_way_after_able_way(str_arg):
+    long_company(str_arg)
+    print('tell_point')
+
+def long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_way_after_able_way('important_point')
---
+++ super/tell_life_under_place/hand/high_day/child/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day_and_able_point(str_arg):
+    woman(str_arg)
+    print('company_or_same_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day_and_able_point('place_and_year')
