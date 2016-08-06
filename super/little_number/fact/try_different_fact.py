96205819127215a8571761678bfa551d0a368ba3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 10:16:26 -0400

    "mod: leave super/little_number/fact/try_different_fact.py and super/think_fact/look_old_woman_of_hand/first_life/big_hand/different_week.py under long problem"

---
+++ super/little_number/fact/try_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part(str_arg):
+    seem_different_year(str_arg)
+    print('few_year_or_life')
+
+def seem_different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part('good_time')
---
+++ super/think_fact/look_old_woman_of_hand/first_life/big_hand/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_eye(str_arg):
+    eye_or_thing(str_arg)
+    print('have_long_part_for_long_world')
+
+def eye_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_eye('tell_group')
