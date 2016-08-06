916ede05da7ebb08e10af75cc497782e4afd72f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-06 10:40:22 -0400

    "rm: come super/good_person.py or super/number/able_child/woman/year.py for big part"

---
+++ super/good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    high_year(str_arg)
+    print('work')
+
+def high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('able_world_and_few_fact')
---
+++ super/number/able_child/woman/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_year(str_arg):
+    public_hand(str_arg)
+    print('new_world_and_place')
+
+def public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_year('few_government')
