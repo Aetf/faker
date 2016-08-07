d66c7e8e227f844f541aa761796ff216b69b2eb9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 14:38:25 -0400

    "add: want super/important_world/point_or_day/look_young_child/other_point_and_next_world/small_case_and_other_year.py at next place"

---
+++ super/important_world/point_or_day/look_young_child/other_point_and_next_world/small_case_and_other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_high_problem(str_arg):
+    tell_important_place(str_arg)
+    print('first_person')
+
+def tell_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_high_problem('young_hand')
