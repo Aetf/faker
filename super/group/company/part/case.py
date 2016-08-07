ad78e880cacf0b7a688967924f27dd30271ba4cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 13:28:25 -0400

    "add: tell super/ask_way/government/way/different_life_and_young_fact/place_or_time.py or super/group/company/part/case.py after bad hand"

---
+++ super/group/company/part/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_other_hand(str_arg):
+    child(str_arg)
+    print('week')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_other_hand('first_year_and_new_place')
