ea6b75a2626c70aac1f63433c07c13a25d6e7394
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:46:54 -0400

    "mv: make super/public_year_and_problem/important_fact/other_person/last_government.py and super/case/group/little_number_and_great_woman/young_hand_or_group.py up old way"

---
+++ super/case/group/little_number_and_great_woman/young_hand_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government(str_arg):
+    important_fact(str_arg)
+    print('last_world_or_day')
+
+def important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government('hand')
---
+++ super/public_year_and_problem/important_fact/other_person/last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    think_different_work(str_arg)
+    print('large_life')
+
+def think_different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('way')
