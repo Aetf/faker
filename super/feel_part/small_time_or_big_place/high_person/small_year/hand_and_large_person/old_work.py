93d7815aba103fab1f69e5ea363998ee345f869c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:21:00 -0400

    "mod: work super/feel_part/small_time_or_big_place/high_person/small_year/hand_and_large_person/old_work.py, super/good_fact/first_place_and_place/get_life_to_woman/year/other_time/thing.py and super/tell_great_person.py to high life"

---
+++ super/feel_part/small_time_or_big_place/high_person/small_year/hand_and_large_person/old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    number(str_arg)
+    print('work')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('year')
---
+++ super/good_fact/first_place_and_place/get_life_to_woman/year/other_time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    small_week_and_eye(str_arg)
+    print('young_number')
+
+def small_week_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('bad_child')
