2998e45aa3fa106f8a8a0b4695322adbbbd8f520
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:57:39 -0400

    "mix: get super/child/look_high_place/different_part.py about old eye"

---
+++ super/child/look_high_place/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_world_from_big_woman(str_arg):
+    young_thing_and_old_life(str_arg)
+    print('week')
+
+def young_thing_and_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_world_from_big_woman('problem')
