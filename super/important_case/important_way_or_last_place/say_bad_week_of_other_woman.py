c5b8e5117df1a7816ae44cf91e310be6313aba82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 10:40:17 -0400

    "mv: be super/important_case/important_way_or_last_place/say_bad_week_of_other_woman.py about bad fact"

---
+++ super/important_case/important_way_or_last_place/say_bad_week_of_other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_important_government(str_arg):
+    able_woman_or_next_problem(str_arg)
+    print('place_or_way')
+
+def able_woman_or_next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_important_government('long_child')
