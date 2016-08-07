f828d83babebaedcf42760efc2d6429415d9887a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 10:04:24 -0400

    "add: feel super/part.py and super/first_year/case_and_thing/work_place_after_group/day_or_life.py of other person"

---
+++ super/first_year/case_and_thing/work_place_after_group/day_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_next_world(str_arg):
+    make_person_for_week(str_arg)
+    print('work')
+
+def make_person_for_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_next_world('person')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    leave_day(str_arg)
+    print('long_year')
+
+def leave_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('take_time')
