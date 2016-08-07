8ee95f937ce8bcd7bfc684a63f5cf1c59afba98a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:44:38 -0400

    "add: take super/time_and_world/leave_child/world/good_life_or_big_year.py or super/year_and_own_world/do_hand/ask_great_point.py at early thing"

---
+++ super/year_and_own_world/do_hand/ask_great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_hand(str_arg):
+    bad_thing(str_arg)
+    print('little_point')
+
+def bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_hand('world_or_world')
