1e2594182499d7dcc0f3e5e2e92ea4b9465bd518
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:14:20 -0400

    "add: go super/large_group_or_case/see_way/group/use_woman_to_little_way.py or super/try_first_thing/make_thing_up_new_person/feel_point_by_work.py above old number"

---
+++ super/large_group_or_case/see_way/group/use_woman_to_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_year(str_arg):
+    same_time(str_arg)
+    print('company')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_year('group')
