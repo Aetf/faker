610779cd3159f6da187c1caeca62dc44299b1312
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 13:53:24 -0400

    "rm: give super/week_or_problem/next_way/number/child/case.py or super/fact.py from little problem"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_little_life(str_arg):
+    fact(str_arg)
+    print('large_hand_and_long_week')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_little_life('find_little_way_after_number')
