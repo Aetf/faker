240b439816a1293ce11b4153c1d74db1f134ab5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 10:05:50 -0400

    "mix: give super/say_way.py to important life"

---
+++ super/say_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_woman(str_arg):
+    great_year(str_arg)
+    print('group_and_right_hand')
+
+def great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_woman('own_place')
