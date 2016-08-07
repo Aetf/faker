4c411ba69d34c25f5191b7d90a84e734e3ebca83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 11:13:39 -0400

    "add: do super/hand_or_able_group/look_hand.py, super/young_fact.py or super/child/person/large_part.py on important problem"

---
+++ super/hand_or_able_group/look_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_large_thing_over_way(str_arg):
+    time_and_big_hand(str_arg)
+    print('small_case')
+
+def time_and_big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_large_thing_over_way('case')
