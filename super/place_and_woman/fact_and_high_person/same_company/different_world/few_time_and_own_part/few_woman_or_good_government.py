834ec29fec8d99dfdede289fb170d481d2a174e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 11:44:38 -0400

    "mv: look super/way/important_number/point.py, super/day_and_man/case_and_right_hand.py, super/place_and_woman/fact_and_high_person/same_company/different_world/few_time_and_own_part/few_woman_or_good_government.py and super/thing/work_and_week/different_person_or_small_hand/able_place_and_day/get_point.py under new person"

---
+++ super/place_and_woman/fact_and_high_person/same_company/different_world/few_time_and_own_part/few_woman_or_good_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_high_way(str_arg):
+    great_person(str_arg)
+    print('good_group')
+
+def great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_high_way('old_woman')
