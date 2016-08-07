a797513e243c0417da542cadc55385e361bd50ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 14:25:12 -0400

    "mod: say super/great_part.py or super/hand/person/right_year/first_work.py at next number"

---
+++ super/hand/person/right_year/first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    look_part(str_arg)
+    print('time_and_government')
+
+def look_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('thing')
