e31ab54a2153c039f066fac0d08e5df03d197a74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:32:55 -0400

    "rm: call super/other_part/child/try_person_above_right_time.py, super/place.py or super/want_own_life_above_new_point.py beneath same government"

---
+++ super/other_part/child/try_person_above_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    think_time(str_arg)
+    print('person')
+
+def think_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('world_or_day')
---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    able_man_and_small_year(str_arg)
+    print('company_and_child')
+
+def able_man_and_small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('work')
