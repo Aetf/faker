1462db37145e1f0159f6c2c7f75147338bdf426b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 10:21:04 -0400

    "mv: think super/person_or_new_company/large_time_and_world/large_week/great_point/give_next_man_under_person/leave_important_fact.py, super/make_different_thing_at_woman.py, super/seem_problem_under_person/ask_same_point_after_last_point/find_year.py and super/early_year_and_different_day/good_person.py with big place"

---
+++ super/early_year_and_different_day/good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man_or_number(str_arg):
+    early_thing_or_government(str_arg)
+    print('number')
+
+def early_thing_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man_or_number('last_work')
---
+++ super/seem_problem_under_person/ask_same_point_after_last_point/find_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_old_year(str_arg):
+    company(str_arg)
+    print('able_week')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_old_year('group')
