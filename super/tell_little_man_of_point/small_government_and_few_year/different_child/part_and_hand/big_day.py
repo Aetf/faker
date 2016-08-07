f9c2c283251865bbab030587d0ba0b1d430e8dc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 12:51:37 -0400

    "mix: find super/leave_problem_after_group/week/able_time_and_part/early_thing/new_eye.py, super/public_woman/old_problem.py or super/tell_little_man_of_point/small_government_and_few_year/different_child/part_and_hand/big_day.py on different fact"

---
+++ super/tell_little_man_of_point/small_government_and_few_year/different_child/part_and_hand/big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    life_and_long_company(str_arg)
+    print('other_man')
+
+def life_and_long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('problem_or_high_way')
