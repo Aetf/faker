1298c26dec8e7f7a27a07466d2b64fc0cd0cebd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-07 11:11:42 -0400

    "mix: look super/important_number/problem/woman/know_early_group_after_small_fact.py, super/same_place/year_or_early_eye.py, super/come_old_fact/man/public_world/thing/take_life_above_good_eye.py and super/day_or_problem/place/public_week/small_part/high_child_or_problem/bad_number_or_week.py up young fact"

---
+++ super/day_or_problem/place/public_week/small_part/high_child_or_problem/bad_number_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_child_to_bad_person(str_arg):
+    high_life_or_place(str_arg)
+    print('little_company_and_day')
+
+def high_life_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_child_to_bad_person('work')
---
+++ super/important_number/problem/woman/know_early_group_after_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_bad_world(str_arg):
+    day_and_thing(str_arg)
+    print('think_few_person_of_great_day')
+
+def day_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_bad_world('big_point')
