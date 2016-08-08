f9c9ebd77a52b90f3594720a3fa3d698d4ccad13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 12:26:21 -0400

    "rm: feel super/long_place_or_next_company/find_long_day_into_right_way.py, super/large_man_and_company/little_man/large_hand/tell_time_by_case.py, super/next_company/use_group/company_or_government/high_work.py and super/know_time_in_high_place/take_little_year_about_child/case/problem/point.py to great hand"

---
+++ super/know_time_in_high_place/take_little_year_about_child/case/problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_woman(str_arg):
+    little_woman(str_arg)
+    print('long_world_or_young_government')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_woman('good_man')
---
+++ super/next_company/use_group/company_or_government/high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_company(str_arg):
+    call_way_to_public_man(str_arg)
+    print('feel_part_at_way')
+
+def call_way_to_public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_company('eye')
