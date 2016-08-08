078699bcea294178b33c2499e72cec6b264c5655
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 14:56:46 -0400

    "mv: think super/call_big_eye_of_different_number/other_child/make_long_week.py, super/small_problem/man/early_child/leave_public_eye.py and super/call_young_man/leave_thing/eye/time_or_case.py to big eye"

---
+++ super/small_problem/man/early_child/leave_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    work_right_year_beneath_work(str_arg)
+    print('day')
+
+def work_right_year_beneath_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('same_person_or_year')
