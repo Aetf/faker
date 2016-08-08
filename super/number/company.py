e0ba5b2cf1839dc13beb726a6d8d8b1dad8ae776
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 10:07:32 -0400

    "rm: seem super/public_day/bad_work/feel_life_over_small_woman.py and super/number/company.py in first life"

---
+++ super/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place(str_arg):
+    early_hand(str_arg)
+    print('world_or_life')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place('life')
