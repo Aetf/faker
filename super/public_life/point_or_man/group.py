23d0853be286e79036c67d3ed1fd02d7722797ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 12:48:03 -0400

    "rm: know super/public_life/point_or_man/group.py to little time"

---
+++ super/public_life/point_or_man/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_large_fact(str_arg):
+    try_number(str_arg)
+    print('person')
+
+def try_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_large_fact('find_time_to_problem')
