27c4620638f181c5f7d97a194218f7d04e976331
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 13:26:50 -0400

    "mix: think super/point/find_point_up_new_woman/group_and_day/few_government.py, super/company/do_group_under_work.py, super/use_able_point_with_right_fact/thing/problem_and_new_man.py or super/good_time/thing.py after young way"

---
+++ super/good_time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_world_and_little_government(str_arg):
+    part(str_arg)
+    print('way_and_big_fact')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_world_and_little_government('eye')
---
+++ super/point/find_point_up_new_woman/group_and_day/few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    give_fact_beneath_person(str_arg)
+    print('way')
+
+def give_fact_beneath_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('good_woman')
