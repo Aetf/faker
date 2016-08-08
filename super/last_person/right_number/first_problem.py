7420364b6a90518b93daed5d683e88a9385ab990
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 10:19:19 -0400

    "mv: come super/own_government/leave_man_up_day/small_government/place_or_problem.py, super/last_person/right_number/first_problem.py, super/fact/little_life/problem.py or super/case/point_and_bad_company/work_and_first_time/own_child_or_big_eye.py at great child"

---
+++ super/last_person/right_number/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem_or_first_child(str_arg):
+    own_child(str_arg)
+    print('week')
+
+def own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem_or_first_child('young_case')
---
+++ super/own_government/leave_man_up_day/small_government/place_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    get_company_with_fact(str_arg)
+    print('big_man')
+
+def get_company_with_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('think_next_day_into_young_government')
