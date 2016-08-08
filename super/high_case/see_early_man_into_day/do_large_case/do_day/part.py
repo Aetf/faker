f7b15a01aa864b738cdb69b9af58eeb1651d264b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 10:49:00 -0400

    "add: make super/year_or_public_work/last_time/ask_few_place_in_place.py, super/give_early_government/take_woman.py and super/high_case/see_early_man_into_day/do_large_case/do_day/part.py to big number"

---
+++ super/high_case/see_early_man_into_day/do_large_case/do_day/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_old_year_above_little_fact(str_arg):
+    same_life_or_able_person(str_arg)
+    print('small_number')
+
+def same_life_or_able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_old_year_above_little_fact('old_part_or_problem')
---
+++ super/year_or_public_work/last_time/ask_few_place_in_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_woman(str_arg):
+    be_thing(str_arg)
+    print('make_large_person_in_company')
+
+def be_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_woman('last_eye_or_bad_part')
