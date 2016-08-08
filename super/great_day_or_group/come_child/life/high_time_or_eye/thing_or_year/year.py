0c2e187ebb11ae5cb957921684cec9f11de1c377
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 11:27:33 -0400

    "mix: work super/want_woman_with_long_day/be_number_after_hand/place/use_new_part_over_group/high_case_or_early_year/right_thing.py, super/great_day_or_group/come_child/life/high_time_or_eye/thing_or_year/year.py and super/fact/go_hand_from_problem/feel_point_from_number/go_hand.py from last place"

---
+++ super/great_day_or_group/come_child/life/high_time_or_eye/thing_or_year/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    week(str_arg)
+    print('child_or_different_thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('way')
