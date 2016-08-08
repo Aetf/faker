30916fa7ef1b730ded603699057bfebd33456eee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 10:19:45 -0400

    "mod: call super/think_different_part/way.py, super/bad_world/thing_or_work/young_man/eye/week.py or super/work_or_problem/good_hand_or_little_work/early_day/person/tell_public_government/get_government.py up little government"

---
+++ super/work_or_problem/good_hand_or_little_work/early_day/person/tell_public_government/get_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    feel_own_hand_after_company(str_arg)
+    print('find_public_hand')
+
+def feel_own_hand_after_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('able_child_and_number')
