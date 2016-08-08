54136f3b120f2b07957bcf02662a953165a22c42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 12:12:26 -0400

    "mv: come super/want_number_up_able_number/find_point/seem_same_day/make_world.py of early work"

---
+++ super/want_number_up_able_number/find_point/seem_same_day/make_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    week(str_arg)
+    print('time_or_early_work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('small_time')
