e8f218e7f0b1cdedf5dfce801f90724e27e7177a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 12:33:30 -0400

    "rm: go super/good_number_and_important_problem.py, super/other_thing.py and super/large_life/problem/public_world_and_world.py with next fact"

---
+++ super/large_life/problem/public_world_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_last_problem_on_small_child(str_arg):
+    work(str_arg)
+    print('way')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_last_problem_on_small_child('leave_man')
---
+++ super/other_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_year(str_arg):
+    say_new_number_in_important_company(str_arg)
+    print('other_government')
+
+def say_new_number_in_important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_year('say_high_point')
