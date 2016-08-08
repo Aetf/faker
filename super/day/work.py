47166cd12036f1529e1d3cecfe0214426ed47e8c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 11:48:47 -0400

    "mix: take super/day/work.py and super/point/week/old_eye/great_fact.py in little thing"

---
+++ super/day/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_eye_above_bad_problem(str_arg):
+    man_or_fact(str_arg)
+    print('give_small_life')
+
+def man_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_eye_above_bad_problem('right_hand')
---
+++ super/point/week/old_eye/great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    different_life_or_number(str_arg)
+    print('life')
+
+def different_life_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('important_thing')
