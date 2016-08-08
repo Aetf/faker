2839161111c6e812f3e2f7664791be835775db21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 10:54:17 -0400

    "mix: get super/own_person/public_child.py from few hand"

---
+++ super/own_person/public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_world(str_arg):
+    way(str_arg)
+    print('eye_and_early_woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_world('world')
