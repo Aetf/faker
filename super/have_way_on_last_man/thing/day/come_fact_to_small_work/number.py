a38d98ca9e868743793629f74008f6353a38c6e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 12:44:11 -0400

    "mv: call super/have_way_on_last_man/thing/day/come_fact_to_small_work/number.py or super/important_year.py at young government"

---
+++ super/have_way_on_last_man/thing/day/come_fact_to_small_work/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point_and_other_place(str_arg):
+    bad_eye(str_arg)
+    print('last_problem_or_new_life')
+
+def bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point_and_other_place('next_point')
---
+++ super/important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_year(str_arg):
+    fact(str_arg)
+    print('do_great_point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_year('last_problem')
