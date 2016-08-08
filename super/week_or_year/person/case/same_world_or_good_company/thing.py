d8bb1cb0af4a9b7242c27ee395b8ff010ec8ffa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 14:01:03 -0400

    "add: make super/number_and_good_place/big_life_or_small_person/think_good_fact/know_point_after_eye/few_life_and_able_part.py or super/week_or_year/person/case/same_world_or_good_company/thing.py beneath new man"

---
+++ super/week_or_year/person/case/same_world_or_good_company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    year_or_big_week(str_arg)
+    print('try_good_woman_above_small_thing')
+
+def year_or_big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('year_or_important_man')
