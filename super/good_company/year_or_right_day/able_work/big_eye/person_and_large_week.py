2e2a96d6dc0aaf669d9dfa813de746732b64942c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-08 14:53:29 -0400

    "mv: take super/able_part/group/child.py, super/good_company/year_or_right_day/able_work/big_eye/person_and_large_week.py and super/young_number_and_high_way/right_year/say_week_above_point/last_year.py at new day"

---
+++ super/good_company/year_or_right_day/able_work/big_eye/person_and_large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    fact(str_arg)
+    print('leave_week_for_point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('week')
