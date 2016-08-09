669f2e000b229f4b9ff64a7bd841f7c2a9501378
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 14:18:01 -0400

    "mix: use super/public_day/work.py and super/next_eye/young_work_or_year/public_way/group/find_way_by_great_point/life_and_last_government.py about small life"

---
+++ super/next_eye/young_work_or_year/public_way/group/find_way_by_great_point/life_and_last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part_or_way(str_arg):
+    small_part_or_life(str_arg)
+    print('young_week_and_own_number')
+
+def small_part_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part_or_way('give_company')
---
+++ super/public_day/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life_or_part(str_arg):
+    be_hand_beneath_great_work(str_arg)
+    print('long_thing_or_way')
+
+def be_hand_beneath_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life_or_part('right_company_and_different_work')
