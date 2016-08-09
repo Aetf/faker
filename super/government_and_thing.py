8e945203db9b8775ee50a6a0a4351482b6be2b88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 14:28:03 -0400

    "rm: be super/government_and_thing.py, super/tell_government_in_right_way/own_week/problem/thing/case/time.py or super/woman_and_part/problem.py into good point"

---
+++ super/government_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part_or_work(str_arg):
+    come_little_way(str_arg)
+    print('man_and_way')
+
+def come_little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part_or_work('problem')
---
+++ super/tell_government_in_right_way/own_week/problem/thing/case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_thing(str_arg):
+    day(str_arg)
+    print('make_same_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_thing('public_week')
