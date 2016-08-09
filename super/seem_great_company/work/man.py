802f7154aae1915738dc13a6d4199a8b2c5fe128
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:18:28 -0400

    "add: do super/little_place/place/big_fact/problem/give_little_thing_from_good_child/do_way.py or super/seem_great_company/work/man.py on important work"

---
+++ super/seem_great_company/work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_government(str_arg):
+    think_high_company(str_arg)
+    print('public_world')
+
+def think_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_government('new_week')
