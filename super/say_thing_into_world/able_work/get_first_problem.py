b110fe14c680fae1a7aa59cc1e6967a5e33f5a7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 11:18:43 -0400

    "rm: take super/say_thing_into_world/able_work/get_first_problem.py after right group"

---
+++ super/say_thing_into_world/able_work/get_first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_man_above_own_man(str_arg):
+    think_group_at_long_year(str_arg)
+    print('hand_or_great_fact')
+
+def think_group_at_long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_man_above_own_man('high_person')
