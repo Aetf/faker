f87f9390467eee2c7839f9b3babfd3be67a510a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 13:20:10 -0400

    "mv: have super/young_way/high_work/last_government_or_week/case/work_life.py or super/right_hand/small_problem_or_place/hand/tell_case/world/look_number_above_own_week.py over little person"

---
+++ super/right_hand/small_problem_or_place/hand/tell_case/world/look_number_above_own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_woman(str_arg):
+    week(str_arg)
+    print('come_man')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_woman('last_work_and_eye')
---
+++ super/young_way/high_work/last_government_or_week/case/work_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_person(str_arg):
+    little_time_or_other_company(str_arg)
+    print('world_and_week')
+
+def little_time_or_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_person('young_place_and_old_child')
