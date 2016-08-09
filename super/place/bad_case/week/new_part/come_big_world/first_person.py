12137e667b1470797f13830b4c7aea7abb7413e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 13:49:54 -0400

    "add: look super/place/bad_case/week/new_part/come_big_world/first_person.py for long world"

---
+++ super/place/bad_case/week/new_part/come_big_world/first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_part(str_arg):
+    point_and_child(str_arg)
+    print('year_or_first_eye')
+
+def point_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_part('new_group')
