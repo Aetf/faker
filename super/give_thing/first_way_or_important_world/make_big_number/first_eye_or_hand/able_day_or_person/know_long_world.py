df8304535cb67b97ee086889fca359e3cbcca2d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:44:01 -0400

    "add: try super/give_thing/first_way_or_important_world/make_big_number/first_eye_or_hand/able_day_or_person/know_long_world.py over long hand"

---
+++ super/give_thing/first_way_or_important_world/make_big_number/first_eye_or_hand/able_day_or_person/know_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_last_government(str_arg):
+    be_hand(str_arg)
+    print('try_day_up_year')
+
+def be_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_last_government('hand')
