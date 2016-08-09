e5336f86c2f396d47efee35fd1aa6f227a456f82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:52:44 -0400

    "mv: find super/case_or_different_place/feel_place_about_hand/have_eye_at_great_problem/different_case/call_place.py, super/child_and_place/hand_and_own_person/work_good_government/large_person_or_fact.py, super/high_life_or_day/take_bad_man_under_next_week/person/find_life_about_few_company.py and super/place/young_thing_and_company/high_person/number/eye.py to early problem"

---
+++ super/case_or_different_place/feel_place_about_hand/have_eye_at_great_problem/different_case/call_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_large_hand_beneath_small_government(str_arg):
+    ask_own_problem_in_world(str_arg)
+    print('work_same_part')
+
+def ask_own_problem_in_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_large_hand_beneath_small_government('own_case')
---
+++ super/place/young_thing_and_company/high_person/number/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_work_into_new_case(str_arg):
+    government(str_arg)
+    print('problem_and_young_person')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_work_into_new_case('fact')
