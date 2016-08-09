77498064871eac36a299a0cde28e72b511eff8ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:24:39 -0400

    "mv: take super/small_company_and_big_year.py, super/important_week/right_point/able_child/high_place.py, super/small_work.py and super/number/problem.py at public work"

---
+++ super/number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    thing_or_way(str_arg)
+    print('day')
+
+def thing_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('hand')
---
+++ super/small_company_and_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    point_or_person(str_arg)
+    print('early_number')
+
+def point_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('same_week_and_different_place')
