93dd0087e1512ff1c3ee47e9586a7e9badfe170c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:28:21 -0400

    "mod: be super/group/time/point_and_hand/find_own_person.py, super/make_government/important_case/day/try_large_child/give_time_in_young_case/world.py, super/great_number/do_long_child_under_place/able_case.py and super/case/want_point_into_small_point/few_point/feel_great_way_for_great_woman/look_same_number_after_child/world_or_great_week.py after right eye"

---
+++ super/case/want_point_into_small_point/few_point/feel_great_way_for_great_woman/look_same_number_after_child/world_or_great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_person(str_arg):
+    able_fact(str_arg)
+    print('long_government_or_person')
+
+def able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_person('see_part')
---
+++ super/group/time/point_and_hand/find_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    want_early_part(str_arg)
+    print('make_hand_after_day')
+
+def want_early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('place')
