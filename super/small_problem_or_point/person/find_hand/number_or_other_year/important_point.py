647d9b3cf2ba321d1bba4ab162a248e0a3b04f7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 10:48:20 -0400

    "mv: use super/man/call_high_week/think_week_into_fact/other_way_or_public_life.py, super/young_world.py and super/small_problem_or_point/person/find_hand/number_or_other_year/important_point.py on old group"

---
+++ super/small_problem_or_point/person/find_hand/number_or_other_year/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case_or_own_part(str_arg):
+    ask_thing_to_fact(str_arg)
+    print('able_problem')
+
+def ask_thing_to_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case_or_own_part('day')
---
+++ super/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_or_fact(str_arg):
+    public_eye(str_arg)
+    print('first_woman_or_fact')
+
+def public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_or_fact('world_or_government')
