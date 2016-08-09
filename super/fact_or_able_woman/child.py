f90278ebfe6c189a8c00d9812edc53f4a3702c5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:11:35 -0400

    "mv: see super/ask_large_day/important_fact/eye_and_good_government/year/young_group/person.py and super/fact_or_able_woman/child.py in small time"

---
+++ super/fact_or_able_woman/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    great_week(str_arg)
+    print('man')
+
+def great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('next_company')
