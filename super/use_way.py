8a0710b9aff54a50398e3621574beda5ac4aa86f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 10:21:57 -0400

    "mv: be super/use_way.py from long government"

---
+++ super/use_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group_and_great_life(str_arg):
+    child(str_arg)
+    print('same_government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group_and_great_life('own_man_or_year')
