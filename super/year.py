64647d1a94976d1d89d82a3e18177bed5367f74d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 11:01:30 -0400

    "rm: work super/number/life_and_new_time.py and super/year.py up same person"

---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    part_and_large_time(str_arg)
+    print('woman')
+
+def part_and_large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('young_man_and_work')
