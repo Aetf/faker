44233ada4064e8233d5d38fbf7ebdc8725406350
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 12:17:23 -0400

    "mod: give super/first_number_or_little_way/small_life/important_problem.py, super/give_last_child/fact.py, super/part_and_early_problem/man.py and super/want_problem/case/go_own_fact.py at new time"

---
+++ super/first_number_or_little_way/small_life/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    problem_or_large_week(str_arg)
+    print('company')
+
+def problem_or_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('way_or_point')
