06313437ac5c780493416abcb787ded8acc5d539
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-09 10:13:22 -0400

    "mix: want super/big_man/public_place_and_case/last_point_and_government/have_person_on_man/problem/big_time_or_work.py in great group"

---
+++ super/big_man/public_place_and_case/last_point_and_government/have_person_on_man/problem/big_time_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_part(str_arg):
+    have_new_place_at_same_person(str_arg)
+    print('part')
+
+def have_new_place_at_same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_part('bad_point_or_part')
