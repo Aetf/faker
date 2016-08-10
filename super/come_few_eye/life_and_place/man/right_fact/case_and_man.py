a60f031599c26dbc99caa32dde2882f1c28129fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:27:25 -0400

    "add: work super/good_way_and_eye.py, super/same_eye_and_hand.py, super/come_few_eye/life_and_place/man/right_fact/case_and_man.py or super/different_problem_or_week/person/thing/world.py at last point"

---
+++ super/come_few_eye/life_and_place/man/right_fact/case_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_life(str_arg):
+    hand_and_young_place(str_arg)
+    print('public_world')
+
+def hand_and_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_life('early_company_and_part')
---
+++ super/different_problem_or_week/person/thing/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_group(str_arg)
+    print('case')
+
+def young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('long_week')
