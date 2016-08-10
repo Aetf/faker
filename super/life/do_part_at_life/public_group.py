ef2be8a74e02b70a39768c1df543ee5d02921a43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 10:01:36 -0400

    "mv: take super/life/do_part_at_life/public_group.py up few person"

---
+++ super/life/do_part_at_life/public_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_person(str_arg):
+    early_case_and_few_government(str_arg)
+    print('work_other_world')
+
+def early_case_and_few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_person('be_person_up_company')
