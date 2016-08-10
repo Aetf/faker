d9b1efd67602553b52c767331b50431609c47340
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 10:14:22 -0400

    "mod: give super/day/take_woman_into_own_number/right_person_or_young_eye/long_government_or_part/part.py and super/use_same_week.py above next problem"

---
+++ super/day/take_woman_into_own_number/right_person_or_young_eye/long_government_or_part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_problem(str_arg):
+    bad_company(str_arg)
+    print('public_eye_or_new_hand')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_problem('thing')
---
+++ super/use_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    other_point(str_arg)
+    print('week')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('use_great_work')
