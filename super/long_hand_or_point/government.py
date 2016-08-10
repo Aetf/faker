e33baeef9a9d539a5c35a0970c3ff5529f09d9f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:34:03 -0400

    "mv: ask super/long_hand_or_point/government.py and super/tell_company/tell_day_over_year/tell_group/get_good_work_by_important_case/part/use_next_way.py to last number"

---
+++ super/long_hand_or_point/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_fact(str_arg):
+    look_different_eye(str_arg)
+    print('know_public_thing_under_life')
+
+def look_different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_fact('first_fact')
---
+++ super/tell_company/tell_day_over_year/tell_group/get_good_work_by_important_case/part/use_next_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    long_work_and_life(str_arg)
+    print('way')
+
+def long_work_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('last_time')
