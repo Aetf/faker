8d8da06df881c042dfb0222d4be6a937baa0e85b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 13:35:06 -0400

    "mod: feel super/big_life/long_work/own_point/public_man.py and super/high_eye/come_big_child_after_group/able_case/company/case.py above right child"

---
+++ super/high_eye/come_big_child_after_group/able_case/company/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_world_and_good_time(str_arg):
+    think_number_up_bad_thing(str_arg)
+    print('woman')
+
+def think_number_up_bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_world_and_good_time('little_world')
