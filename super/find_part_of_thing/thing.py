0a0cf7c20c4ec33e002e02bfc77d2fea83d55c95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 12:19:19 -0400

    "mix: be super/look_government_on_first_company/call_eye.py, super/find_part_of_thing/thing.py and super/work/go_life_of_work/hand/problem_or_early_year/good_person/person.py to right year"

---
+++ super/find_part_of_thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_high_case_under_high_hand(str_arg):
+    life_or_part(str_arg)
+    print('work_or_time')
+
+def life_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_high_case_under_high_hand('number_or_large_government')
---
+++ super/work/go_life_of_work/hand/problem_or_early_year/good_person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand_or_important_way(str_arg):
+    thing(str_arg)
+    print('great_life')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand_or_important_way('right_thing')
