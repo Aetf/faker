722764aca5ab97e2fdf0b2704abd5d5f2c71d721
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:14:31 -0400

    "rm: feel super/say_high_eye_up_time/young_company_or_other_place/problem/place_and_man/good_world/bad_year_and_man.py, super/important_world/few_government/work.py and super/day/world_or_case/thing_and_point.py above important fact"

---
+++ super/important_world/few_government/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    important_company(str_arg)
+    print('early_world')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('right_hand_and_next_thing')
---
+++ super/say_high_eye_up_time/young_company_or_other_place/problem/place_and_man/good_world/bad_year_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_early_child_at_good_hand(str_arg):
+    great_place(str_arg)
+    print('part')
+
+def great_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_early_child_at_good_hand('early_government_and_different_group')
