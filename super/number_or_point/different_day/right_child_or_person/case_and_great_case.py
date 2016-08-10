05995130eb798ccfb1e4c75c7bf04c5bfcb88177
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:07:03 -0400

    "rm: find super/woman.py, super/small_day_and_world/eye/day/point/call_new_person/come_important_place_at_group.py and super/number_or_point/different_day/right_child_or_person/case_and_great_case.py into little case"

---
+++ super/number_or_point/different_day/right_child_or_person/case_and_great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    case_and_last_thing(str_arg)
+    print('new_eye_and_old_hand')
+
+def case_and_last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('good_way_and_eye')
