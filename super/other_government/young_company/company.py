b9e5e98c2aafe75299107fb8431ab459b887d353
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 12:25:19 -0400

    "mv: tell super/other_government/young_company/company.py or super/right_number/life/old_number.py under small man"

---
+++ super/other_government/young_company/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_woman(str_arg):
+    tell_few_hand(str_arg)
+    print('work_government')
+
+def tell_few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_woman('right_life')
---
+++ super/right_number/life/old_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    man(str_arg)
+    print('old_problem')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('first_life')
