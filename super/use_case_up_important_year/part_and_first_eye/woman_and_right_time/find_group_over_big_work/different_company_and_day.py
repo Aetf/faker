bc708198403f86e65eee403fa19479c7c917512c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:38:31 -0400

    "mv: seem super/use_case_up_important_year/part_and_first_eye/woman_and_right_time/find_group_over_big_work/different_company_and_day.py at important woman"

---
+++ super/use_case_up_important_year/part_and_first_eye/woman_and_right_time/find_group_over_big_work/different_company_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_place_or_point(str_arg):
+    same_time(str_arg)
+    print('own_part')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_place_or_point('big_group')
