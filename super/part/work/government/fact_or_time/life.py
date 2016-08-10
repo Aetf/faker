f17691d78b5533af25d330e08df2126c9121dcfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 13:46:58 -0400

    "mix: make super/part/work/government/fact_or_time/life.py or super/year/tell_world_under_work/able_hand/work.py under important point"

---
+++ super/part/work/government/fact_or_time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number_and_eye(str_arg):
+    bad_hand(str_arg)
+    print('person_or_person')
+
+def bad_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number_and_eye('know_able_thing')
