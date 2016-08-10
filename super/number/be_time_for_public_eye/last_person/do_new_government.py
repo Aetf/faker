8e825c289c586c8cbb7541bfb4aa0e6b1349871b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 14:42:43 -0400

    "add: give super/number/be_time_for_public_eye/last_person/do_new_government.py, super/government.py, super/young_week/early_way_and_problem.py and super/person.py into great day"

---
+++ super/number/be_time_for_public_eye/last_person/do_new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man_or_group(str_arg):
+    year(str_arg)
+    print('want_other_year_on_big_thing')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man_or_group('thing')
---
+++ super/young_week/early_way_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place_under_bad_way(str_arg):
+    company(str_arg)
+    print('work_thing')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place_under_bad_way('know_long_eye')
