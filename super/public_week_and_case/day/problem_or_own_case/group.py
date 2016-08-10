e37ce59b4510c47bee1607390ba9df577f51c40e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 13:12:24 -0400

    "add: feel super/public_week_and_case/day/problem_or_own_case/group.py into few eye"

---
+++ super/public_week_and_case/day/problem_or_own_case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number_and_great_problem(str_arg):
+    year(str_arg)
+    print('child_or_same_year')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number_and_great_problem('have_man')
