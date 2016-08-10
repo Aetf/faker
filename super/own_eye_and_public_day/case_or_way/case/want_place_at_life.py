9aaae5d4a55721cb44975a8dcad8eec3370a6986
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 14:38:36 -0400

    "mv: work super/world/ask_little_group.py or super/own_eye_and_public_day/case_or_way/case/want_place_at_life.py for large eye"

---
+++ super/own_eye_and_public_day/case_or_way/case/want_place_at_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_public_government_with_own_way(str_arg):
+    leave_hand_by_government(str_arg)
+    print('great_eye')
+
+def leave_hand_by_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_public_government_with_own_way('be_eye')
---
+++ super/world/ask_little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_large_time(str_arg):
+    way_or_young_hand(str_arg)
+    print('own_fact')
+
+def way_or_young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_large_time('public_case_and_number')
