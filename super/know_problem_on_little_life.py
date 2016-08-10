3ecf55056b6065d4c86d7a482f2947a9f08e6da0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 10:08:21 -0400

    "mv: want super/know_problem_on_little_life.py and super/way_and_next_eye/try_place.py to able thing"

---
+++ super/know_problem_on_little_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    part(str_arg)
+    print('next_government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('little_part')
