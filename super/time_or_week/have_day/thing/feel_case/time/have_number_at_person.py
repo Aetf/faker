8395782068d3210abbed5ba97f390fec782e321b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-10 11:45:51 -0400

    "mv: be super/work/feel_woman_under_few_eye/look_last_company_with_few_man/year_and_fact.py or super/time_or_week/have_day/thing/feel_case/time/have_number_at_person.py over few fact"

---
+++ super/time_or_week/have_day/thing/feel_case/time/have_number_at_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    little_world(str_arg)
+    print('hand')
+
+def little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('place')
