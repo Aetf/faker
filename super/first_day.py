e6ba15eac3d41c9080137df8f13147112af44d57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 12:06:06 -0400

    "add: give super/first_day.py of other group"

---
+++ super/first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_work(str_arg):
+    try_important_part(str_arg)
+    print('be_eye')
+
+def try_important_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_work('get_part')
