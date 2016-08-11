de06bcd53cee736dc41868160de06b8210460166
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:58:45 -0400

    "mix: take super/hand_and_same_place/place/problem/world/same_part_or_year/big_number.py or super/important_point_or_great_group/own_life.py beneath old man"

---
+++ super/important_point_or_great_group/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    have_group(str_arg)
+    print('big_person')
+
+def have_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('get_problem_at_big_woman')
