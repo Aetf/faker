472a3e7c41b9ffb1c65593bc695514741620a8bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 10:22:21 -0400

    "mod: call super/week.py of good case"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    see_problem(str_arg)
+    print('right_year')
+
+def see_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('man')
