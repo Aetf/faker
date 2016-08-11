cbce112514ea8436db8b5bda10b7a6ccb6bcfa3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:43:52 -0400

    "add: try super/long_day/long_world/high_life_or_point/large_place/small_group_or_world.py, super/company/thing/important_fact.py or super/come_place/bad_work/child/old_hand_or_point.py to great person"

---
+++ super/come_place/bad_work/child/old_hand_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_woman(str_arg):
+    know_year(str_arg)
+    print('important_work')
+
+def know_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_woman('group')
