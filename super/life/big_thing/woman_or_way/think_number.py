14e9b134bf09f7de48101509619c33c3ba5c52ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:36:32 -0400

    "add: say super/life/big_thing/woman_or_way/think_number.py, super/same_child/important_point.py and super/go_place_of_own_part/young_child/big_eye/different_work.py to last hand"

---
+++ super/life/big_thing/woman_or_way/think_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_eye(str_arg):
+    other_thing(str_arg)
+    print('woman')
+
+def other_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_eye('few_thing_or_problem')
---
+++ super/same_child/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world(str_arg):
+    give_hand_of_big_number(str_arg)
+    print('take_case_over_point')
+
+def give_hand_of_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world('say_early_fact')
