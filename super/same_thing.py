40d86093230f378f52a4df8d5c90f01e79569cac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 10:53:59 -0400

    "add: be super/number_and_fact/say_large_way.py, super/think_life_above_day/last_man_or_right_person/year_and_government/thing_or_day/man/early_group_and_small_part.py and super/same_thing.py beneath good government"

---
+++ super/same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    eye(str_arg)
+    print('fact')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('work_able_time_under_way')
