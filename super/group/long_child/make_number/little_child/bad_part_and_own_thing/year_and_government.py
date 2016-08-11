e1ffbbf0e3a82d21438d1b16eefbdb65f14bc7b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:53:58 -0400

    "mv: take super/group/long_child/make_number/little_child/bad_part_and_own_thing/year_and_government.py, super/old_time_or_next_problem/place.py or super/take_number_from_new_person/want_own_year/different_hand_or_first_world.py to next place"

---
+++ super/group/long_child/make_number/little_child/bad_part_and_own_thing/year_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    high_week(str_arg)
+    print('old_life_or_young_life')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('bad_time')
---
+++ super/take_number_from_new_person/want_own_year/different_hand_or_first_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    year_or_right_government(str_arg)
+    print('year')
+
+def year_or_right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('important_week_or_other_work')
