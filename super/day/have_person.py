34ae9f98e8c4f74fc27ca78850fcf92ca5b09131
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 11:15:51 -0400

    "mix: feel super/day/have_person.py and super/make_case_by_new_year.py on long thing"

---
+++ super/day/have_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_government(str_arg):
+    know_place(str_arg)
+    print('point')
+
+def know_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_government('work')
