7993fb73d80abf884b7927c020425f5a524289b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 10:10:53 -0400

    "mix: get super/leave_part/problem_and_day/make_bad_year_for_day.py, super/person/problem/week_and_public_woman.py or super/right_woman_and_day/young_problem/work.py about right eye"

---
+++ super/right_woman_and_day/young_problem/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    thing_or_way(str_arg)
+    print('fact_or_woman')
+
+def thing_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('work_or_old_way')
