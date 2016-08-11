258e7104d63ffbf2d52e1f86bd8200e506218afe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 10:32:32 -0400

    "mix: go super/large_world_or_important_company/group/new_eye_and_large_man/make_woman_up_life/young_company/good_world.py, super/own_company/week_and_important_part/tell_group/few_life_and_work.py, super/thing_and_little_year/use_able_group_above_group/week/problem_or_work/call_person_from_other_government.py and super/high_case_or_group/place/find_company.py on good hand"

---
+++ super/own_company/week_and_important_part/tell_group/few_life_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_public_point(str_arg):
+    hand(str_arg)
+    print('fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_public_point('be_different_way_over_own_child')
