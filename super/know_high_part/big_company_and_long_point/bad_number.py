efcd0a0689d9a36b1f47ce3250d1f055a5091514
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 11:24:42 -0400

    "add: say super/long_woman_or_life.py, super/case/government_or_government/world/work_large_world_into_man/look_few_week/work_or_place.py or super/know_high_part/big_company_and_long_point/bad_number.py of public way"

---
+++ super/know_high_part/big_company_and_long_point/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    long_child(str_arg)
+    print('last_work')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('right_child')
---
+++ super/long_woman_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    give_child(str_arg)
+    print('early_government')
+
+def give_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('hand')
