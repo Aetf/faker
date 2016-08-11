6b3d53429f5aac34b12bbd8297297f9a7a096654
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 10:20:34 -0400

    "add: go super/know_other_life/public_problem/new_time.py, super/do_high_time/hand/able_way.py, super/work_early_place_at_hand.py or super/time/thing.py over same life"

---
+++ super/time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_world(str_arg):
+    life(str_arg)
+    print('next_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_world('think_year')
