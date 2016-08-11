b89be600ff8e3df2681963e1a37d62cb3d0a0851
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 13:46:15 -0400

    "mod: know super/number_and_last_day/ask_hand.py about new group"

---
+++ super/number_and_last_day/ask_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year_or_large_year(str_arg):
+    right_child(str_arg)
+    print('call_government_for_important_man')
+
+def right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year_or_large_year('early_way')
