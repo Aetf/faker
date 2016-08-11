c605279009ba65ebc88511ef63b1760026a43f63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 12:05:13 -0400

    "mv: take super/own_work_or_small_person/look_work_to_last_point/case/high_part/ask_week.py or super/small_case_and_eye/little_number_and_company.py from high year"

---
+++ super/own_work_or_small_person/look_work_to_last_point/case/high_part/ask_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    look_place(str_arg)
+    print('woman')
+
+def look_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('great_life_or_person')
---
+++ super/small_case_and_eye/little_number_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_different_hand(str_arg):
+    high_time(str_arg)
+    print('government')
+
+def high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_different_hand('case')
