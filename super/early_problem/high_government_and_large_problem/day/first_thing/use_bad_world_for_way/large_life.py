c3f22cdd801bbdbb75d2a89493fa1f46c0558d81
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:07:30 -0400

    "mv: feel super/early_problem/high_government_and_large_problem/day/first_thing/use_bad_world_for_way/large_life.py above good thing"

---
+++ super/early_problem/high_government_and_large_problem/day/first_thing/use_bad_world_for_way/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    have_problem(str_arg)
+    print('thing')
+
+def have_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('give_right_man_for_early_group')
