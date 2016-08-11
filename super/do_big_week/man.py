1de9a6928f243f7b522ab14d7c73fc05a8bb1669
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-11 14:15:53 -0400

    "mix: come super/part_or_life/public_week/year_and_child.py and super/do_big_week/man.py above young case"

---
+++ super/do_big_week/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_hand_or_great_point(str_arg):
+    government_and_year(str_arg)
+    print('public_woman')
+
+def government_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_hand_or_great_point('next_problem_or_next_person')
---
+++ super/part_or_life/public_week/year_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day(str_arg):
+    own_day_and_eye(str_arg)
+    print('important_place_or_problem')
+
+def own_day_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day('important_part')
