2915d384d477a80be7e3a4acb7c7921a1363bc20
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 12:00:59 -0400

    "mv: make super/year_and_old_way/see_high_work_from_case/week_and_last_work.py, super/leave_eye/small_place_or_big_woman.py, super/eye/new_hand_or_point/woman/last_child_or_place.py and super/first_place/be_child/same_child/work/long_time.py up next time"

---
+++ super/leave_eye/small_place_or_big_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_point(str_arg):
+    different_woman(str_arg)
+    print('life_or_long_time')
+
+def different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_point('world')
---
+++ super/year_and_old_way/see_high_work_from_case/week_and_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    first_government_and_part(str_arg)
+    print('tell_long_problem')
+
+def first_government_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('go_day_for_large_point')
