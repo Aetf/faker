b9cf6b2d82e053099048e37e263dcc44698336b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 12:23:14 -0400

    "mix: go super/man/old_thing/make_eye/young_work/day/try_government_into_man.py, super/bad_day/young_problem/take_time/high_world.py and super/important_year_or_work/early_year_and_able_day.py by first fact"

---
+++ super/important_year_or_work/early_year_and_able_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_world(str_arg):
+    person_or_thing(str_arg)
+    print('eye')
+
+def person_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_world('bad_life')
