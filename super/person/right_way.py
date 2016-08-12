3dc9f2f425090c1d8995378110ab14cc3a115a93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 11:07:21 -0400

    "mix: leave super/person/right_way.py, super/different_day.py or super/person_and_last_year/day/work_good_thing/company_or_first_world/important_child.py over early day"

---
+++ super/person/right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_same_number(str_arg):
+    different_case_and_large_group(str_arg)
+    print('large_world_or_number')
+
+def different_case_and_large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_same_number('different_time_or_child')
---
+++ super/person_and_last_year/day/work_good_thing/company_or_first_world/important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_own_government(str_arg):
+    place(str_arg)
+    print('year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_own_government('man')
