eb1ac91cd1523263d06a5faf67f3af4e11bfb114
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 11:27:11 -0400

    "mod: feel super/big_person/see_large_way_with_woman/fact_and_group.py and super/look_fact_over_time.py at right work"

---
+++ super/big_person/see_large_way_with_woman/fact_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_first_government_for_new_part(str_arg):
+    know_man_with_eye(str_arg)
+    print('person_and_way')
+
+def know_man_with_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_first_government_for_new_part('good_time_and_day')
---
+++ super/look_fact_over_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day(str_arg):
+    hand(str_arg)
+    print('great_week')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day('big_child')
