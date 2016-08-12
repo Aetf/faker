ebece2b4acbb29aff6ec979aa4fcd57e68b868d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 12:25:00 -0400

    "add: work super/great_work/do_right_problem.py, super/last_child_or_part/person/bad_way/fact_or_case/world/case_and_place.py, super/give_small_person/woman/point_and_fact/world_or_large_way.py or super/know_next_child_in_last_problem/company.py with small eye"

---
+++ super/great_work/do_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    young_thing(str_arg)
+    print('have_government_in_way')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('problem')
---
+++ super/know_next_child_in_last_problem/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    small_day(str_arg)
+    print('right_part_and_week')
+
+def small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('little_government_or_point')
