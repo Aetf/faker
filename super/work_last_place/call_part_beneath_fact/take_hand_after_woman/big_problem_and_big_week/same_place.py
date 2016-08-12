21505a0ca66ca3077108c14f80e3107b3ab49234
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 12:49:31 -0400

    "mix: say super/work_last_place/call_part_beneath_fact/take_hand_after_woman/big_problem_and_big_week/same_place.py by small year"

---
+++ super/work_last_place/call_part_beneath_fact/take_hand_after_woman/big_problem_and_big_week/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_old_world(str_arg):
+    work_case(str_arg)
+    print('day')
+
+def work_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_old_world('go_last_place')
