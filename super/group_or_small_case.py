91f7991a063a6f314a475c1dee0c6fc5b6476aad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 13:23:24 -0400

    "mv: think super/high_case/other_person/way/young_life_and_long_part.py, super/know_week_from_little_child/fact/small_part/own_point.py or super/group_or_small_case.py beneath able problem"

---
+++ super/group_or_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    few_work_or_own_eye(str_arg)
+    print('call_right_person_for_different_number')
+
+def few_work_or_own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('long_problem')
---
+++ super/know_week_from_little_child/fact/small_part/own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_long_number_of_public_life(str_arg):
+    problem(str_arg)
+    print('next_government_and_new_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_long_number_of_public_life('man_or_fact')
