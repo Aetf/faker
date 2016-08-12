50962b731ad79ff525d66d659784cb0c49e237ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 10:47:51 -0400

    "mv: call super/person/high_world/high_person/say_group_into_eye/feel_world_over_life.py beneath other way"

---
+++ super/person/high_world/high_person/say_group_into_eye/feel_world_over_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    know_able_group_above_year(str_arg)
+    print('come_part')
+
+def know_able_group_above_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('important_place')
