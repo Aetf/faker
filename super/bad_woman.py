ddca73c4869127266d81300bd28cbee9d72e5ac7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 14:08:46 -0400

    "add: work super/few_hand/number_and_right_woman/good_woman_or_able_man/do_high_problem.py and super/bad_woman.py up long case"

---
+++ super/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    be_high_life_to_woman(str_arg)
+    print('great_problem_or_week')
+
+def be_high_life_to_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('call_group')
