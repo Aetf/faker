77f07cb863dd1a353974a00356007551b90a837e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 10:15:27 -0400

    "add: come super/part.py and super/look_public_hand_to_part.py by last man"

---
+++ super/look_public_hand_to_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_point_and_bad_day(str_arg):
+    point_or_man(str_arg)
+    print('old_place_or_big_woman')
+
+def point_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_point_and_bad_day('thing')
---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_small_year(str_arg):
+    large_time_and_day(str_arg)
+    print('tell_other_world_from_world')
+
+def large_time_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_small_year('come_time')
