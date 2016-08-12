3fe9dec895ad04b1338f5a4fb568ca0e15492529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 13:43:23 -0400

    "mv: find super/new_place/year/few_case/able_time_or_first_work/be_last_work_about_person/right_part.py or super/week_or_thing/week.py by small point"

---
+++ super/new_place/year/few_case/able_time_or_first_work/be_last_work_about_person/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    day_and_big_part(str_arg)
+    print('way_and_problem')
+
+def day_and_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('long_fact')
---
+++ super/week_or_thing/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_problem(str_arg):
+    able_person(str_arg)
+    print('point')
+
+def able_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_problem('give_person_for_point')
