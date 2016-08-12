a733025c1ee444aecf218a4bec4d68d64aaf9bba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 13:28:00 -0400

    "mv: go super/child_or_last_company/problem/company/public_part/child.py, super/own_problem/do_different_life_under_new_world/next_thing/important_child_and_problem/want_day/work.py, super/group.py or super/little_time_and_young_day.py at right world"

---
+++ super/little_time_and_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_early_case_after_eye(str_arg):
+    part(str_arg)
+    print('old_woman')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_early_case_after_eye('see_fact_after_high_group')
