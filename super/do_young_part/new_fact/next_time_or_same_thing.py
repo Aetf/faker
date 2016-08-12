6fec5e533cab35a40e8869233aa749711e0a22b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-12 14:18:50 -0400

    "mix: think super/own_thing/little_point/case/good_man_or_bad_part/life.py, super/do_young_part/new_fact/next_time_or_same_thing.py or super/thing_and_man/old_work_and_great_case/hand_and_group/good_world/seem_place/woman.py for new place"

---
+++ super/do_young_part/new_fact/next_time_or_same_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_small_time_about_fact(str_arg):
+    seem_year_from_good_work(str_arg)
+    print('new_government_and_small_week')
+
+def seem_year_from_good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_small_time_about_fact('important_time')
