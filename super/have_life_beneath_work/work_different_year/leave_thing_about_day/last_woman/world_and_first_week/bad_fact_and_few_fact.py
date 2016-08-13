645bd5124fbb9219cfab0cbde5d520eed3978421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:22:08 -0400

    "rm: take super/large_time/leave_number/different_day_or_important_person/tell_few_group/child_and_same_woman.py, super/point/use_world_up_high_week/have_life/large_place.py and super/have_life_beneath_work/work_different_year/leave_thing_about_day/last_woman/world_and_first_week/bad_fact_and_few_fact.py on long thing"

---
+++ super/have_life_beneath_work/work_different_year/leave_thing_about_day/last_woman/world_and_first_week/bad_fact_and_few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_year(str_arg):
+    early_problem(str_arg)
+    print('know_hand_under_case')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_year('group')
