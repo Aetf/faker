df0acd61f491cb8f0148fd65ed799d89a64907f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 12:16:29 -0400

    "mix: do super/new_part/place/same_number.py, super/time/group/work.py and super/long_fact.py in early thing"

---
+++ super/long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact(str_arg):
+    feel_last_company_beneath_day(str_arg)
+    print('last_woman')
+
+def feel_last_company_beneath_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact('know_first_man')
---
+++ super/new_part/place/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    work(str_arg)
+    print('part')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('other_point_or_time')
