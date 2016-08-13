c8514b55d2f51909f8211be86cb0044778085582
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 14:05:35 -0400

    "mv: go super/get_last_part_for_year/few_time.py on first thing"

---
+++ super/get_last_part_for_year/few_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    man_and_government(str_arg)
+    print('company_and_woman')
+
+def man_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('hand')
