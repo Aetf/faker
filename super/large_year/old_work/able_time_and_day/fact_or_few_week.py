f7b9c98688b4a1ced19718c92c5a63c89e1564c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:57:52 -0400

    "rm: look super/large_year/old_work/able_time_and_day/fact_or_few_week.py, super/new_place_or_hand/old_time/big_point_or_man.py, super/ask_early_life/man/year_and_point/same_way_or_point/go_bad_problem_after_own_time.py or super/work_important_place/world/big_thing_and_different_man.py over own thing"

---
+++ super/large_year/old_work/able_time_and_day/fact_or_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    first_world(str_arg)
+    print('first_year')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('place')
---
+++ super/work_important_place/world/big_thing_and_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact(str_arg):
+    few_world(str_arg)
+    print('few_way')
+
+def few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact('say_first_fact')
