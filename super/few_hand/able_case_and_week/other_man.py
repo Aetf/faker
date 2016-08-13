22be00059d9eaa8fdfc2bdec7092e9708a0b756d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:10:53 -0400

    "rm: seem super/year/child/try_own_point_with_last_day.py or super/few_hand/able_case_and_week/other_man.py about large government"

---
+++ super/few_hand/able_case_and_week/other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    year(str_arg)
+    print('number_and_long_year')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('able_company_and_eye')
---
+++ super/year/child/try_own_point_with_last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part_on_point(str_arg):
+    company_or_child(str_arg)
+    print('long_life')
+
+def company_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part_on_point('person_or_government')
