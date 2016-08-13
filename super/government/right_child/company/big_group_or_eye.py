6fb3f236e68d2a1c8c930562549344b5b4fd0d02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:55:23 -0400

    "mod: seem super/government/right_child/company/big_group_or_eye.py, super/ask_person_by_important_government.py, super/high_woman/old_child_or_life/use_year_beneath_own_day/call_life/year_and_number/point_or_part.py or super/place.py to old number"

---
+++ super/government/right_child/company/big_group_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_time_above_high_woman(str_arg):
+    long_fact_or_company(str_arg)
+    print('important_number')
+
+def long_fact_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_time_above_high_woman('next_woman')
