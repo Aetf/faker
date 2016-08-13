3a64fcfb3f67602c4bf5cf40f79fb3f6416963c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:33:12 -0400

    "add: call super/see_part/number.py, super/number/own_part_or_man/large_thing/know_child/be_time_into_man.py or super/early_work_or_child/new_week/same_government/day/person/year.py up public time"

---
+++ super/see_part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life_or_world(str_arg):
+    say_big_hand(str_arg)
+    print('big_case')
+
+def say_big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life_or_world('problem')
