782ff1e3c38dbe052f6e670dcfa93c39e22d0a30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 10:11:50 -0400

    "mod: seem super/time_and_important_company.py or super/little_point/year_and_thing.py of same world"

---
+++ super/little_point/year_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    come_same_eye(str_arg)
+    print('work')
+
+def come_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('small_number')
---
+++ super/time_and_important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man_or_day(str_arg):
+    case(str_arg)
+    print('own_week')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man_or_day('few_life')
