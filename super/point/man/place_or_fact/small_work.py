995aa63d236f4cfa4ee218ed33809f6618dfffaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 10:06:25 -0400

    "mv: call super/point/man/place_or_fact/small_work.py and super/try_own_government_from_hand/part/want_good_person/part/point.py up same eye"

---
+++ super/point/man/place_or_fact/small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    fact(str_arg)
+    print('hand')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('time')
---
+++ super/try_own_government_from_hand/part/want_good_person/part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_public_case(str_arg):
+    thing(str_arg)
+    print('woman')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_public_case('right_week')
