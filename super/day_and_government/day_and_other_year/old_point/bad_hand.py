9cf44b9136e3093aa1415c5b5fba6118c059a879
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 10:28:25 -0400

    "mod: want super/year/world.py or super/day_and_government/day_and_other_year/old_point/bad_hand.py over next problem"

---
+++ super/day_and_government/day_and_other_year/old_point/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    world(str_arg)
+    print('public_man')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('government_or_small_company')
