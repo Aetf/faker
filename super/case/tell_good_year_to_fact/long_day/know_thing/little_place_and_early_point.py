9cd1c6fba35a5a2362ac4a6cb5fd1590adea5a53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 10:23:41 -0400

    "mod: ask super/case/tell_good_year_to_fact/long_day/know_thing/little_place_and_early_point.py or super/first_man.py of last company"

---
+++ super/case/tell_good_year_to_fact/long_day/know_thing/little_place_and_early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case(str_arg):
+    young_company(str_arg)
+    print('time')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case('tell_life')
---
+++ super/first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    go_person_of_able_day(str_arg)
+    print('bad_man')
+
+def go_person_of_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('company')
