2e3c6fcee1d0bfaf5acfba8a890b6322701580e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 14:43:18 -0400

    "mix: tell super/high_world/woman/find_fact_of_week.py and super/high_fact_and_good_woman/way_and_right_number/few_group/important_group/part.py in first group"

---
+++ super/high_fact_and_good_woman/way_and_right_number/few_group/important_group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_person(str_arg):
+    place_or_great_way(str_arg)
+    print('long_work_and_point')
+
+def place_or_great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_person('government')
---
+++ super/high_world/woman/find_fact_of_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    make_world(str_arg)
+    print('eye_or_eye')
+
+def make_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('place')
