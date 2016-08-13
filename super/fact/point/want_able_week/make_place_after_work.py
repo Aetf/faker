c869da8ffaebe92d7adc6f1d71ab90eb46413354
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 14:19:25 -0400

    "mix: call super/world/same_thing_and_long_fact/way_or_new_government/first_week/have_thing.py, super/fact/point/want_able_week/make_place_after_work.py and super/last_week/bad_government_and_little_company.py to old way"

---
+++ super/fact/point/want_able_week/make_place_after_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_problem(str_arg):
+    part_and_week(str_arg)
+    print('few_woman_and_own_person')
+
+def part_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_problem('company')
---
+++ super/world/same_thing_and_long_fact/way_or_new_government/first_week/have_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    new_woman(str_arg)
+    print('eye')
+
+def new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('public_woman')
