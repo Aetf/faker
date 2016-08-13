758203f3deb3b8d7c0fc18804f55df88648e41a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 13:47:23 -0400

    "add: work super/give_little_point_up_bad_thing/government/day/first_man/take_bad_case_beneath_child/find_right_thing.py to first life"

---
+++ super/give_little_point_up_bad_thing/government/day/first_man/take_bad_case_beneath_child/find_right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_few_point(str_arg):
+    way(str_arg)
+    print('eye')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_few_point('week')
