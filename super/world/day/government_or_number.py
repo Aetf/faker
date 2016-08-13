c99d29ef1e04f254345bc2d2786ef392ade8270a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-13 14:42:38 -0400

    "mv: take super/world_or_good_man/year/feel_case/number_or_child/time.py, super/world/day/government_or_number.py, super/case/look_long_time_to_long_way/man_or_important_child.py or super/bad_life_or_fact/right_work_and_eye/other_child/new_thing/long_work/leave_new_year.py in first day"

---
+++ super/world/day/government_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_able_point_at_bad_case(str_arg):
+    government_and_little_week(str_arg)
+    print('place_and_woman')
+
+def government_and_little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_able_point_at_bad_case('first_case')
