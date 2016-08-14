4cd8f3a2f66fa0f5cc85613dd47b685ec5a32561
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 10:39:02 -0400

    "mod: think super/early_government/fact_or_new_day.py, super/place/great_group_or_bad_place/high_case.py and super/small_point/new_thing/own_company/take_point_for_little_company/last_week_and_work/child.py about same eye"

---
+++ super/early_government/fact_or_new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_new_time_on_fact(str_arg):
+    other_way(str_arg)
+    print('different_case')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_new_time_on_fact('person')
---
+++ super/place/great_group_or_bad_place/high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_last_part(str_arg):
+    work_or_able_year(str_arg)
+    print('way')
+
+def work_or_able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_last_part('early_year')
