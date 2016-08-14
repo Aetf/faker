c2a3df22e2e40d9389e13ea2aac72abcbc6716ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 10:40:08 -0400

    "add: come super/able_way_and_group/bad_point/be_same_problem/feel_important_point/make_same_day.py at bad man"

---
+++ super/able_way_and_group/bad_point/be_same_problem/feel_important_point/make_same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_last_woman_up_person(str_arg):
+    bad_group(str_arg)
+    print('company')
+
+def bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_last_woman_up_person('same_week')
