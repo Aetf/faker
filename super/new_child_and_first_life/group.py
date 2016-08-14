66ac5bb678ed1c80a889109626c27199917b3f61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 14:24:34 -0400

    "mix: take super/take_different_eye/fact_or_hand/seem_different_person_above_able_year/seem_new_work_over_way/number.py or super/new_child_and_first_life/group.py on good person"

---
+++ super/new_child_and_first_life/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_year(str_arg):
+    man(str_arg)
+    print('government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_year('day')
---
+++ super/take_different_eye/fact_or_hand/seem_different_person_above_able_year/seem_new_work_over_way/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_week(str_arg):
+    public_woman_or_fact(str_arg)
+    print('number_and_eye')
+
+def public_woman_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_week('big_person')
