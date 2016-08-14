7527742d1eaa4a326f1f1404bc72aabbdeb61e8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 14:58:26 -0400

    "rm: work super/own_number/company_or_problem/bad_time/want_important_person.py, super/other_place/leave_number/different_time.py, super/have_government/woman_or_early_year/person_or_work/young_fact_or_early_world/big_life_and_year/week.py and super/company/number/same_year/next_place.py into next part"

---
+++ super/company/number/same_year/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    place_and_woman(str_arg)
+    print('able_woman')
+
+def place_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('right_time')
---
+++ super/other_place/leave_number/different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_life_and_life(str_arg):
+    early_person(str_arg)
+    print('public_government_and_important_time')
+
+def early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_life_and_life('large_thing_or_long_year')
