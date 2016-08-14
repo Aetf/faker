70b368020e8ec1676781628428a60708882eda0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 14:52:57 -0400

    "mod: come super/next_world.py, super/fact/small_work_or_bad_hand.py and super/first_place_and_public_case/big_week.py into big child"

---
+++ super/fact/small_work_or_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work_and_case(str_arg):
+    bad_eye(str_arg)
+    print('problem')
+
+def bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work_and_case('child')
