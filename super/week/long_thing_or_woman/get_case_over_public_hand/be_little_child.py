6ec804542b73514fae95e53e3685e02be83e8555
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 13:13:30 -0400

    "rm: call super/early_man/say_long_year/know_world_into_young_problem/place/world_or_different_case.py or super/week/long_thing_or_woman/get_case_over_public_hand/be_little_child.py with few man"

---
+++ super/week/long_thing_or_woman/get_case_over_public_hand/be_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group(str_arg):
+    thing_or_year(str_arg)
+    print('company_or_time')
+
+def thing_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group('ask_last_group_beneath_woman')
