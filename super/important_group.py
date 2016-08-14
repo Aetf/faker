cf56415a071fa83236da9253e68eb734b2e2b7aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 11:59:00 -0400

    "rm: do super/early_thing/thing/able_time/first_week.py and super/important_group.py beneath large man"

---
+++ super/important_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_number(str_arg):
+    company(str_arg)
+    print('early_point_or_right_life')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_number('give_child')
