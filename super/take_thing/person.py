7c840640ff8159851e807fe76a11e5e94b7a8a11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 12:40:27 -0400

    "mv: go super/take_thing/person.py, super/do_big_fact/person/come_eye/world/hand_or_problem.py, super/take_life_at_woman/world/child.py and super/work_way_for_group/case/few_number/great_work.py for great company"

---
+++ super/take_thing/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    world(str_arg)
+    print('next_child')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('feel_same_way')
---
+++ super/work_way_for_group/case/few_number/great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_place(str_arg):
+    fact_and_different_part(str_arg)
+    print('eye')
+
+def fact_and_different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_place('young_world')
