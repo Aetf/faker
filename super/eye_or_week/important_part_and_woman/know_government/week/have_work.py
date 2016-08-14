54717141fea0f9b9931b1fe181b81face2b2a56c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 11:32:02 -0400

    "mod: be super/child/small_place/first_work/fact_or_way/company.py, super/man/do_problem_after_last_group/know_new_woman_on_government/life/world/part.py or super/eye_or_week/important_part_and_woman/know_government/week/have_work.py to new eye"

---
+++ super/eye_or_week/important_part_and_woman/know_government/week/have_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_important_year_over_eye(str_arg):
+    good_day(str_arg)
+    print('want_bad_hand_to_problem')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_important_year_over_eye('different_case')
---
+++ super/man/do_problem_after_last_group/know_new_woman_on_government/life/world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    government(str_arg)
+    print('know_life_over_last_work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('same_child')
