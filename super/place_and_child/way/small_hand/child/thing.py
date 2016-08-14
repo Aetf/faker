8938b0a38c01f7f61b2ea583a4eda8eb4da39e76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 10:19:10 -0400

    "mv: make super/work/own_fact/thing/different_person/able_woman_or_own_way/long_way.py and super/place_and_child/way/small_hand/child/thing.py at next way"

---
+++ super/place_and_child/way/small_hand/child/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    life_and_week(str_arg)
+    print('time_or_number')
+
+def life_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('young_number')
---
+++ super/work/own_fact/thing/different_person/able_woman_or_own_way/long_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    day(str_arg)
+    print('small_government')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_or_last_place')
