62db88691f0186948700f4fec6179f582399e988
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 12:07:04 -0400

    "rm: have super/ask_important_government/seem_different_government/work_time_with_able_group/know_big_person.py, super/long_hand/place/large_eye_or_new_work/come_next_year_to_work/number/woman.py or super/old_way.py about bad place"

---
+++ super/ask_important_government/seem_different_government/work_time_with_able_group/know_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person(str_arg):
+    year(str_arg)
+    print('call_work_from_public_part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person('life_or_young_number')
---
+++ super/long_hand/place/large_eye_or_new_work/come_next_year_to_work/number/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    first_woman(str_arg)
+    print('large_man')
+
+def first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('place_or_person')
