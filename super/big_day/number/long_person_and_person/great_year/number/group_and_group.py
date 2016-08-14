36dc4230d39848283284d4b0de98fbe24180229c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 14:08:04 -0400

    "rm: take super/big_day/number/long_person_and_person/great_year/number/group_and_group.py, super/own_case.py or super/time/company.py of next hand"

---
+++ super/big_day/number/long_person_and_person/great_year/number/group_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    own_company(str_arg)
+    print('other_man_and_time')
+
+def own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('bad_group')
---
+++ super/own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    small_place(str_arg)
+    print('eye')
+
+def small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('time_or_same_part')
