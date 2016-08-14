f5a0c8edd0b017395b5486454a58ffe88959d927
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 12:24:20 -0400

    "mv: feel super/fact/long_group_or_person/do_case/public_person_or_work.py, super/fact/case/next_woman.py, super/eye.py and super/hand_or_hand/fact/take_next_person/little_life_or_great_thing.py about old eye"

---
+++ super/fact/long_group_or_person/do_case/public_person_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman(str_arg):
+    able_fact_or_different_way(str_arg)
+    print('have_man')
+
+def able_fact_or_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman('government')
---
+++ super/hand_or_hand/fact/take_next_person/little_life_or_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_man(str_arg):
+    government(str_arg)
+    print('take_number_above_point')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_man('part')
