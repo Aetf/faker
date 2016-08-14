37d1a55d8b88defe93e94bfcc548591159c81928
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 11:58:48 -0400

    "mix: be super/early_work_or_long_fact/good_way/right_point_or_point.py beneath next group"

---
+++ super/early_work_or_long_fact/good_way/right_point_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way(str_arg):
+    important_part_and_hand(str_arg)
+    print('week')
+
+def important_part_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way('see_high_point_beneath_person')
