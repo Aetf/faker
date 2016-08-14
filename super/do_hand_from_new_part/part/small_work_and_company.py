c935c90d54558cae1eee08047c7b34e49dfea746
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 12:47:00 -0400

    "add: give super/do_hand_from_new_part/part/small_work_and_company.py in few year"

---
+++ super/do_hand_from_new_part/part/small_work_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day(str_arg):
+    fact(str_arg)
+    print('tell_case_from_large_problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day('large_day')
