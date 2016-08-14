ac0486d6f23c1934c86362515baa6870c1268ce9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 12:36:03 -0400

    "mv: look super/first_hand/call_thing_about_government/high_number/feel_way_under_public_company/take_able_child_about_right_eye/important_company_or_next_government.py under large group"

---
+++ super/first_hand/call_thing_about_government/high_number/feel_way_under_public_company/take_able_child_about_right_eye/important_company_or_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_case(str_arg):
+    important_world_or_big_company(str_arg)
+    print('want_other_fact_up_world')
+
+def important_world_or_big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_case('time_and_important_woman')
