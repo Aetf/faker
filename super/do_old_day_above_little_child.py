ff1f66c236eab79878ddb84d453fc36382a42e65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-14 13:52:53 -0400

    "mv: ask super/leave_world.py or super/do_old_day_above_little_child.py on big time"

---
+++ super/do_old_day_above_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact_and_own_eye(str_arg):
+    young_place(str_arg)
+    print('problem')
+
+def young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact_and_own_eye('be_other_eye_up_right_person')
---
+++ super/leave_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    work_person(str_arg)
+    print('work_or_work')
+
+def work_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('time')
