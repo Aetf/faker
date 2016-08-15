c8a090f1c995444407ed7be0b140e4b73444d525
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 14:03:38 -0400

    "mod: use super/large_life/make_part.py, super/next_number/place_or_point/say_able_time_for_other_part/find_good_world_in_government.py and super/same_number_or_group/know_early_company/time.py over public fact"

---
+++ super/large_life/make_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    year(str_arg)
+    print('way')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('little_week_and_new_fact')
---
+++ super/next_number/place_or_point/say_able_time_for_other_part/find_good_world_in_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_part_and_early_time(str_arg):
+    hand(str_arg)
+    print('high_way')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_part_and_early_time('ask_other_man')
