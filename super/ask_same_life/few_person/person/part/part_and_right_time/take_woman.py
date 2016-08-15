f6078076f771d99d479130d1e06f89d9aff4e2f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 12:09:30 -0400

    "mod: come super/ask_same_life/few_person/person/part/part_and_right_time/take_woman.py, super/eye_and_own_fact/problem_and_hand/new_child/large_year/good_thing/come_person_with_little_man.py and super/time_and_high_case/old_child_and_bad_case/important_problem/feel_part_over_same_way.py on different part"

---
+++ super/ask_same_life/few_person/person/part/part_and_right_time/take_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    hand(str_arg)
+    print('try_government_about_small_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('next_place')
---
+++ super/eye_and_own_fact/problem_and_hand/new_child/large_year/good_thing/come_person_with_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    day_or_public_government(str_arg)
+    print('tell_company_at_small_work')
+
+def day_or_public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('day')
