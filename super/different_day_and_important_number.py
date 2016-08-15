0c9997a6ffc492d514548ad7b95b478a5f26001a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 11:36:15 -0400

    "mv: come super/work/part/think_right_fact/early_year_and_place.py and super/different_day_and_important_number.py above large man"

---
+++ super/different_day_and_important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    place_or_large_world(str_arg)
+    print('high_time')
+
+def place_or_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('have_week')
