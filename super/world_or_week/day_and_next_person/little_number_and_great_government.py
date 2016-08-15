087703dd0c42f5f70086172de8c7f6985b5443d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 14:15:11 -0400

    "rm: use super/world_or_week/day_and_next_person/little_number_and_great_government.py in other man"

---
+++ super/world_or_week/day_and_next_person/little_number_and_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_fact(str_arg):
+    person_and_last_fact(str_arg)
+    print('case')
+
+def person_and_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_fact('able_world_or_year')
