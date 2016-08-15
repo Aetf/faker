06bc1c5677f792f1f6329bf02ff95f3a92aad358
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 12:04:09 -0400

    "rm: tell super/big_work/first_woman.py, super/man.py, super/early_child/ask_different_man/number/see_case_by_able_case/fact_or_able_woman.py and super/few_group_and_little_thing.py on small world"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_hand_at_few_place(str_arg):
+    number(str_arg)
+    print('big_time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_hand_at_few_place('little_thing_and_woman')
