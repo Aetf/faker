7ec13a32b8d14df1eb7cb43332a62bb4efeb8dcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 11:25:42 -0400

    "mod: make super/first_day/early_problem_or_place/point/week.py, super/high_part_or_own_case/thing_or_important_company.py and super/call_big_time/new_fact_and_week.py at own year"

---
+++ super/call_big_time/new_fact_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    leave_woman_by_small_government(str_arg)
+    print('group')
+
+def leave_woman_by_small_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('do_public_man')
