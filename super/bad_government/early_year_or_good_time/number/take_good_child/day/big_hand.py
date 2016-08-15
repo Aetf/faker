b4838da9944e58f4f852ce0c0289a3453384aa67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 10:09:19 -0400

    "rm: feel super/do_world/call_next_woman_under_big_case/person/hand_or_government/way_or_time/good_man_and_day.py, super/bad_government/early_year_or_good_time/number/take_good_child/day/big_hand.py and super/time_or_woman/little_point/problem/different_part_or_problem/high_life/high_company_or_small_place.py on little woman"

---
+++ super/bad_government/early_year_or_good_time/number/take_good_child/day/big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_way(str_arg):
+    do_large_company(str_arg)
+    print('first_world_and_person')
+
+def do_large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_way('leave_small_government_on_work')
---
+++ super/time_or_woman/little_point/problem/different_part_or_problem/high_life/high_company_or_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_child_or_next_eye(str_arg):
+    problem(str_arg)
+    print('want_able_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_child_or_next_eye('man')
