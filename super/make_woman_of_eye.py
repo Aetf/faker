e760f46a5d182ebd311a8e562ea8e3d7cda5ab3b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 12:08:00 -0400

    "mix: be super/make_woman_of_eye.py in different group"

---
+++ super/make_woman_of_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_problem(str_arg):
+    young_thing_and_thing(str_arg)
+    print('seem_last_time_to_government')
+
+def young_thing_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_problem('year_and_high_company')
