35342361b48c43d23a8456a65d8cd09e9171ba0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 10:33:30 -0400

    "add: look super/important_problem/year.py and super/ask_eye_into_life/man_or_group.py on right child"

---
+++ super/important_problem/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_number_and_way(str_arg):
+    man(str_arg)
+    print('eye')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_number_and_way('high_day')
