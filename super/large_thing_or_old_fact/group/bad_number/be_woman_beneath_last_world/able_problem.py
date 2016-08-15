298ca36cba4dffe4f1e7eb5c984d7a10279beba6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 10:15:46 -0400

    "mv: tell super/hand/come_way/hand/early_work.py, super/large_thing_or_old_fact/group/bad_number/be_woman_beneath_last_world/able_problem.py or super/think_great_problem_with_eye/try_small_eye/week.py beneath big way"

---
+++ super/large_thing_or_old_fact/group/bad_number/be_woman_beneath_last_world/able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_long_life(str_arg):
+    time(str_arg)
+    print('give_point')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_long_life('person')
---
+++ super/think_great_problem_with_eye/try_small_eye/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_young_company(str_arg):
+    way(str_arg)
+    print('take_bad_company')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_young_company('week')
