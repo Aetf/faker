07e74a5bf370636482084c828f134e8c7ba76a32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 11:13:10 -0400

    "mod: know super/government/be_big_eye_of_young_case/life_and_own_number/problem.py, super/last_fact/big_thing_and_big_week/company.py, super/hand/problem/able_woman/way/bad_year/child.py or super/place_and_fact/child_or_day/week_and_same_woman.py up next group"

---
+++ super/government/be_big_eye_of_young_case/life_and_own_number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_point(str_arg):
+    next_world_and_day(str_arg)
+    print('day')
+
+def next_world_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_point('person_and_case')
---
+++ super/hand/problem/able_woman/way/bad_year/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman_and_young_hand(str_arg):
+    want_next_child_for_child(str_arg)
+    print('number')
+
+def want_next_child_for_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman_and_young_hand('case')
