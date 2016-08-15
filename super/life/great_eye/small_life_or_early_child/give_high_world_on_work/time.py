aedbff789d1bd95347add4b1bff39fee703e43a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 12:01:49 -0400

    "mv: have super/high_work/year_and_old_point.py, super/go_life/part_or_own_problem/call_first_man/bad_problem/world.py, super/life/great_eye/small_life_or_early_child/give_high_world_on_work/time.py or super/number/want_first_life/place/eye_or_young_time/person/year_and_problem.py up new world"

---
+++ super/life/great_eye/small_life_or_early_child/give_high_world_on_work/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_woman_above_person(str_arg):
+    problem_and_own_company(str_arg)
+    print('thing')
+
+def problem_and_own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_woman_above_person('look_new_life_above_bad_number')
