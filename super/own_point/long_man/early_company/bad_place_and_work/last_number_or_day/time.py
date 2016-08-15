09f71a733a9d2ea0b920769ec25ea8dc8de682e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-15 14:43:53 -0400

    "rm: try super/own_point/long_man/early_company/bad_place_and_work/last_number_or_day/time.py about small woman"

---
+++ super/own_point/long_man/early_company/bad_place_and_work/last_number_or_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week_or_long_person(str_arg):
+    bad_part(str_arg)
+    print('eye')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week_or_long_person('time_and_same_woman')
