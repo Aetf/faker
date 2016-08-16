b38716dcb0f6285d46b633feb35e2d595e8d6b92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 10:07:32 -0400

    "rm: do super/young_week_and_different_life/week/say_thing_for_problem/problem/high_number/right_life.py, super/man/come_time_above_last_case/high_fact_or_young_part/way_or_important_group/week_or_life/life.py or super/great_thing_and_same_eye/think_great_way_on_company/great_hand_and_last_year/person/same_person/able_part.py by great woman"

---
+++ super/great_thing_and_same_eye/think_great_way_on_company/great_hand_and_last_year/person/same_person/able_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing_and_own_year(str_arg):
+    know_same_day_over_part(str_arg)
+    print('good_hand_or_group')
+
+def know_same_day_over_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing_and_own_year('week')
---
+++ super/young_week_and_different_life/week/say_thing_for_problem/problem/high_number/right_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    different_year(str_arg)
+    print('get_large_thing_of_eye')
+
+def different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('old_day')
