edf8fbcefc7841429e0c810d81e0e724963923c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 12:49:03 -0400

    "mv: do super/old_case/fact_and_person/life.py on other child"

---
+++ super/old_case/fact_and_person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_way_or_week(str_arg):
+    young_life(str_arg)
+    print('early_world')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_way_or_week('other_eye')
