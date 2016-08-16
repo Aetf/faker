319fcc42c337548175567250d84ca7bcf470d62a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 12:38:38 -0400

    "rm: get super/eye/child_and_important_child/same_woman_and_week/way_or_old_problem.py up small woman"

---
+++ super/eye/child_and_important_child/same_woman_and_week/way_or_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_few_time_on_case(str_arg):
+    leave_big_person_up_thing(str_arg)
+    print('high_hand_or_next_week')
+
+def leave_big_person_up_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_few_time_on_case('problem')
