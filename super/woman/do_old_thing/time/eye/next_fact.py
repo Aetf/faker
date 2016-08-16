09206a38d54aaf265afe6cfd1bb0074ddf2a68ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 11:51:35 -0400

    "mv: have super/problem/do_case_after_same_woman/thing_or_woman.py or super/woman/do_old_thing/time/eye/next_fact.py to other child"

---
+++ super/woman/do_old_thing/time/eye/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week(str_arg):
+    try_first_year(str_arg)
+    print('company_or_young_fact')
+
+def try_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week('point')
