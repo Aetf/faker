486371a35bd9d08ac907c7269db83f811afff1b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 12:13:32 -0400

    "mix: find super/own_person/way_and_fact/have_life/give_woman_into_great_part/thing/hand.py, super/time/ask_part_beneath_own_problem/problem/other_work/company/great_way_or_place.py and super/have_problem_by_right_work/important_week/first_part/important_government_or_company/big_hand.py after bad government"

---
+++ super/time/ask_part_beneath_own_problem/problem/other_work/company/great_way_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    get_week(str_arg)
+    print('bad_year_or_part')
+
+def get_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('look_right_man_in_part')
