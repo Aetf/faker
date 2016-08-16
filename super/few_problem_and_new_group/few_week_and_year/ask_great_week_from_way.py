93706bb1042b945c5b921265673f48765ad8df85
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:26:44 -0400

    "mod: work super/number_and_number/own_problem/give_public_thing_for_company/first_child/group/place_or_point.py, super/few_problem_and_new_group/few_week_and_year/ask_great_week_from_way.py and super/early_man_or_problem/next_year/bad_number/large_group_or_early_part.py under next work"

---
+++ super/few_problem_and_new_group/few_week_and_year/ask_great_week_from_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    see_life_from_fact(str_arg)
+    print('number')
+
+def see_life_from_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('few_way_or_world')
---
+++ super/number_and_number/own_problem/give_public_thing_for_company/first_child/group/place_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    leave_eye(str_arg)
+    print('give_time_over_different_place')
+
+def leave_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('long_man')
