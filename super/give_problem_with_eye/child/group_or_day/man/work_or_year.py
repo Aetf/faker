5a9f630eb86612b4573d9dc784369f643f533789
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 12:31:36 -0400

    "mod: take super/give_problem_with_eye/child/group_or_day/man/work_or_year.py by different child"

---
+++ super/give_problem_with_eye/child/group_or_day/man/work_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    person(str_arg)
+    print('great_man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('do_same_place')
