886cca380b2b064a7a2774ceaee1c9f32a682ef6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:38:32 -0400

    "mv: see super/part/point/do_company_in_woman.py, super/think_case/few_number/few_part/woman.py, super/world/important_fact.py or super/old_government.py over early group"

---
+++ super/world/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    new_hand(str_arg)
+    print('eye')
+
+def new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('different_time_or_last_fact')
