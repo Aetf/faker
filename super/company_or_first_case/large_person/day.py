7185cc2fa3b0e4575d94a931900ded1c4394f10d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 10:21:23 -0400

    "add: say super/own_woman_or_number/other_man_and_case.py, super/time_and_young_week/early_problem_or_eye/high_man/next_problem_or_eye/early_work_or_good_fact/take_problem_over_young_point.py, super/company_or_first_case/large_person/day.py or super/old_child/year/work_or_great_year/bad_eye_and_able_child/little_place.py in early company"

---
+++ super/company_or_first_case/large_person/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_time_into_eye(str_arg):
+    world(str_arg)
+    print('group_and_long_woman')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_time_into_eye('know_world')
