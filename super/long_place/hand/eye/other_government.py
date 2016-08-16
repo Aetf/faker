2d053ea0d5c2061eea1ac3261903db393a070b4f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 11:41:42 -0400

    "mix: do super/long_place/hand/eye/other_government.py or super/other_part.py after able company"

---
+++ super/long_place/hand/eye/other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_woman_and_fact(str_arg):
+    large_problem(str_arg)
+    print('small_place')
+
+def large_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_woman_and_fact('first_fact')
---
+++ super/other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_new_way(str_arg):
+    big_fact(str_arg)
+    print('big_work')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_new_way('long_part')
