def6baa13b96d429187293acad6a089cfc86ffe7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:59:46 -0400

    "add: call super/case/week/old_day/use_problem_under_fact/old_life.py and super/same_world/woman_and_child/long_day/ask_year/child.py to able world"

---
+++ super/case/week/old_day/use_problem_under_fact/old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_early_woman(str_arg):
+    old_work(str_arg)
+    print('woman')
+
+def old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_early_woman('take_few_case')
