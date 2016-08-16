eee5107a0d5c5b882e34c5f1e5fc845d25e7fb76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:51:36 -0400

    "add: be super/take_long_government/small_thing/early_day/have_way_into_bad_eye/seem_big_case.py and super/man/few_time/place_and_own_problem/year.py for little woman"

---
+++ super/man/few_time/place_and_own_problem/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_first_woman(str_arg):
+    work_day_beneath_new_thing(str_arg)
+    print('own_fact_or_big_life')
+
+def work_day_beneath_new_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_first_woman('tell_woman_of_able_man')
---
+++ super/take_long_government/small_thing/early_day/have_way_into_bad_eye/seem_big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_able_child_in_last_fact(str_arg):
+    new_government(str_arg)
+    print('case')
+
+def new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_able_child_in_last_fact('little_number')
