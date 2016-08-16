a87dba02ad7e91bfd1e57cc5a4c3e0c7775d23d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:32:11 -0400

    "mix: see super/world.py, super/large_place_or_government.py or super/way/number/problem/work_work/world/hand.py beneath right child"

---
+++ super/way/number/problem/work_work/world/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year(str_arg):
+    think_right_hand(str_arg)
+    print('come_week_at_thing')
+
+def think_right_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year('get_day')
