66413eeb1117b6d2de5965e26f213cdfeed5b621
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 14:43:14 -0400

    "mix: come super/new_company/go_own_problem.py and super/different_day/little_child/great_work_or_small_eye/early_company/early_world/woman_and_last_fact.py into next number"

---
+++ super/new_company/go_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    day_and_case(str_arg)
+    print('person_and_point')
+
+def day_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('high_case')
