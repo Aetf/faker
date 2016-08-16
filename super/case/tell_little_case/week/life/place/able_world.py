6386bb016160107648871a230c36808266803105
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 10:49:13 -0400

    "mv: try super/case/tell_little_case/week/life/place/able_world.py, super/think_place_by_big_time/think_first_week/number_and_next_woman.py and super/public_fact/eye/long_problem/say_able_time.py by high place"

---
+++ super/case/tell_little_case/week/life/place/able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_part(str_arg):
+    be_long_problem(str_arg)
+    print('good_part')
+
+def be_long_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_part('work_hand_into_first_thing')
