4aa67a1a6b0f5fabacad35b5135566c739c492f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 11:46:42 -0400

    "mod: call super/old_life_and_thing/great_woman/give_time.py, super/life_or_way/company_and_part/new_thing_or_great_problem/come_man.py and super/point_or_public_work/old_world/make_child/be_important_day/group/man.py under high part"

---
+++ super/old_life_and_thing/great_woman/give_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_woman(str_arg):
+    way(str_arg)
+    print('find_time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_woman('work')
