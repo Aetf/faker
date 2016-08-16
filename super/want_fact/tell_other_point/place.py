9bab1053b588b0ea432a6df5d9c940efe97dc945
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-16 13:02:37 -0400

    "rm: go super/come_great_person_from_same_woman/year/other_eye/large_work.py, super/work/know_small_thing_by_life.py or super/want_fact/tell_other_point/place.py from high group"

---
+++ super/want_fact/tell_other_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_different_man_up_hand(str_arg):
+    early_company_and_day(str_arg)
+    print('work')
+
+def early_company_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_different_man_up_hand('do_fact_on_hand')
---
+++ super/work/know_small_thing_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_right_hand(str_arg):
+    way_or_large_place(str_arg)
+    print('few_day_or_few_week')
+
+def way_or_large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_right_hand('point')
