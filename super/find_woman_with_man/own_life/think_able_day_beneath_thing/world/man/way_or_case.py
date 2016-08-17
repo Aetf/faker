d0e8531644d78787f752d0d24abd6d17241a46b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 12:30:28 -0400

    "rm: make super/find_woman_with_man/own_life/think_able_day_beneath_thing/world/man/way_or_case.py or super/group/thing_and_child/hand_or_hand.py about young group"

---
+++ super/find_woman_with_man/own_life/think_able_day_beneath_thing/world/man/way_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    year_and_number(str_arg)
+    print('work_and_different_group')
+
+def year_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('old_hand')
