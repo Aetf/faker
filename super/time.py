a9d37f052e1f1d64c7b0751c3d31844340c4c51f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 11:41:29 -0400

    "mix: work super/good_thing/able_government/use_company_above_public_number/last_work.py, super/time.py and super/want_next_child_in_same_child/ask_number.py above important woman"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_child(str_arg):
+    thing_and_big_year(str_arg)
+    print('company')
+
+def thing_and_big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_child('leave_life_at_last_day')
