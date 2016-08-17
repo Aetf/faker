a67272af4ae650060f01990db0cdbbf41b403a94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 11:11:47 -0400

    "rm: try super/large_part/own_case/first_world/fact/say_woman/man.py and super/man/last_man.py by new man"

---
+++ super/man/last_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_bad_week(str_arg):
+    tell_large_way(str_arg)
+    print('important_child')
+
+def tell_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_bad_week('important_week')
