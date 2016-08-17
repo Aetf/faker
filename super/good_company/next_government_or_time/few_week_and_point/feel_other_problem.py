f3bb8bc117701817d25ace2669ff9c53876d4e65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 12:12:05 -0400

    "rm: look super/good_company/next_government_or_time/few_week_and_point/feel_other_problem.py, super/little_company/last_work/point/point_or_world/do_next_eye/get_young_thing.py or super/high_day_and_number/work_thing_of_new_thing/able_place_or_able_person.py under small person"

---
+++ super/good_company/next_government_or_time/few_week_and_point/feel_other_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_able_life(str_arg):
+    group(str_arg)
+    print('large_life_and_other_child')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_able_life('year')
