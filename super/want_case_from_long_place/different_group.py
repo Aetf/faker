2f6a1479029c75e1c0eadd3900332e0ba5060eee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 12:20:27 -0400

    "mv: call super/place/next_year_or_early_case/important_thing_or_man/get_way.py, super/group/person/different_year/small_world_or_little_part/last_problem_or_new_man.py and super/want_case_from_long_place/different_group.py with right man"

---
+++ super/want_case_from_long_place/different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman(str_arg):
+    child(str_arg)
+    print('place')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman('last_number_and_work')
