96dce7b2e1ae24063bcc571f493955b14d0b56ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 10:31:30 -0400

    "mix: give super/world_and_part/early_thing.py, super/day.py, super/want_person_above_early_man.py or super/way/get_day/have_different_time.py by last group"

---
+++ super/want_person_above_early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_little_case(str_arg):
+    few_group(str_arg)
+    print('way')
+
+def few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_little_case('way')
---
+++ super/way/get_day/have_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_early_case_above_old_man(str_arg)
+    print('large_man_and_last_problem')
+
+def work_early_case_above_old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('see_year_to_able_week')
