cff5534a5c52c19dbc67f70bb499f3af69d17949
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 14:09:35 -0400

    "mix: give super/say_able_life/work_or_work.py and super/woman.py for bad place"

---
+++ super/say_able_life/work_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    see_little_place_in_place(str_arg)
+    print('work')
+
+def see_little_place_in_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('try_year')
