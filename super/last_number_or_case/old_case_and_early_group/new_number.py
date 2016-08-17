c8a460d1de22ef4236e83a6ec14ce386829493d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 13:02:13 -0400

    "add: find super/person/little_child/young_hand_and_same_fact/day_or_early_person.py, super/problem_or_long_eye.py, super/bad_person/feel_thing_by_public_day/way.py or super/last_number_or_case/old_case_and_early_group/new_number.py beneath great way"

---
+++ super/last_number_or_case/old_case_and_early_group/new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    thing_or_big_place(str_arg)
+    print('call_year')
+
+def thing_or_big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('first_number')
---
+++ super/person/little_child/young_hand_and_same_fact/day_or_early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_part(str_arg):
+    get_person(str_arg)
+    print('big_way_or_other_woman')
+
+def get_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_part('make_world')
