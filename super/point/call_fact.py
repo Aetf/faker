8bc1814a5508be05568378eef77abd04ed11b973
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 12:43:53 -0400

    "mix: give super/point/call_fact.py under own hand"

---
+++ super/point/call_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_young_work(str_arg):
+    few_eye(str_arg)
+    print('week')
+
+def few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_young_work('know_number_from_own_year')
