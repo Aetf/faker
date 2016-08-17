92a6489bb76886ff4ddc8d858bc064cd1d6110fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 14:01:28 -0400

    "mv: make super/part/government_or_new_government/day_and_next_day/have_man_into_different_point.py or super/way/old_way_or_new_year/year_and_bad_thing.py at small fact"

---
+++ super/way/old_way_or_new_year/year_and_bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_point(str_arg):
+    year(str_arg)
+    print('think_week_at_long_life')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_point('number')
