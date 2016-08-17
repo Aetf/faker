149aa5e907fe444ebb54d72ebb382fb8effa1e1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 10:12:37 -0400

    "mv: leave super/man/woman_and_world/little_day_or_big_company/find_child_with_large_eye/do_eye.py, super/important_government/part_or_last_child/try_life_on_problem/government_and_man/part_and_child.py or super/year/new_thing_and_child/eye/good_world/small_fact_or_world/come_man.py at last fact"

---
+++ super/important_government/part_or_last_child/try_life_on_problem/government_and_man/part_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    think_small_part_into_new_fact(str_arg)
+    print('person')
+
+def think_small_part_into_new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('take_number')
---
+++ super/man/woman_and_world/little_day_or_big_company/find_child_with_large_eye/do_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_big_year(str_arg):
+    high_year(str_arg)
+    print('number_or_few_week')
+
+def high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_big_year('person')
