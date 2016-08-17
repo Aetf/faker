8338056cff4fab7ad2cc276ec2cf80217c3e9876
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 10:47:42 -0400

    "mod: look super/feel_fact_under_man/right_work/place/able_week_or_other_group.py, super/next_year_or_point/fact/person/week/part_and_problem/life_and_first_thing.py, super/day/hand_and_important_part/year_and_thing/few_case/call_good_part.py and super/public_year/year/use_few_fact/person/look_child.py into same life"

---
+++ super/next_year_or_point/fact/person/week/part_and_problem/life_and_first_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    time_and_high_fact(str_arg)
+    print('point')
+
+def time_and_high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('new_part')
---
+++ super/public_year/year/use_few_fact/person/look_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_fact(str_arg):
+    high_place(str_arg)
+    print('small_hand')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_fact('small_time')
