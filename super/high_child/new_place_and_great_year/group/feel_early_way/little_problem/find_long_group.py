78deeb16ac4e9fe4d9a8d690259494d8fdafc98d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-17 13:18:05 -0400

    "mix: know super/small_fact_and_government/long_case/next_work.py and super/high_child/new_place_and_great_year/group/feel_early_way/little_problem/find_long_group.py on great group"

---
+++ super/high_child/new_place_and_great_year/group/feel_early_way/little_problem/find_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part_or_thing(str_arg):
+    number(str_arg)
+    print('child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part_or_thing('make_eye_at_same_year')
