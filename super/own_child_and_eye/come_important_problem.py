2a9653675e5d52591025709075730544c1a77a12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:20:04 -0400

    "mix: try super/public_person/person/work/high_year_and_little_place/life_and_early_number/tell_number.py and super/own_child_and_eye/come_important_problem.py by big group"

---
+++ super/own_child_and_eye/come_important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    come_new_work_by_place(str_arg)
+    print('high_government')
+
+def come_new_work_by_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('early_fact')
---
+++ super/public_person/person/work/high_year_and_little_place/life_and_early_number/tell_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person(str_arg):
+    way(str_arg)
+    print('time')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person('woman')
