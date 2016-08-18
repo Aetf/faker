9f0db1f7409c878b1b589c96177e796402ae94cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 13:59:00 -0400

    "rm: leave super/be_large_year/right_group_and_world/leave_first_week_up_big_hand/same_eye/old_part_and_few_problem.py, super/try_early_way_by_part.py, super/week/other_hand/last_company/old_year/go_place_above_point/go_old_year_in_first_man.py or super/own_day.py from young case"

---
+++ super/be_large_year/right_group_and_world/leave_first_week_up_big_hand/same_eye/old_part_and_few_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    way_or_next_year(str_arg)
+    print('part_or_child')
+
+def way_or_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('call_government')
