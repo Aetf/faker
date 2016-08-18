6a0daedcad56a307c63eca217d51b72435bfdcd8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:16:24 -0400

    "add: seem super/next_week_and_place.py or super/hand/group/right_eye_and_new_way/same_company_and_thing/child_or_early_person.py about long eye"

---
+++ super/next_week_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_eye_under_small_number(str_arg):
+    government_or_place(str_arg)
+    print('last_world_or_number')
+
+def government_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_eye_under_small_number('case_or_fact')
