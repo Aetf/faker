cdb7c36b9e8079a46db0e0454c09ec77ecaf4b32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 14:56:26 -0400

    "mix: want super/know_person.py or super/young_eye.py of new child"

---
+++ super/know_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    person(str_arg)
+    print('man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('next_case_and_able_day')
