96132e8ea43c276fc5f6a3db34e37d59b99eaa6e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 13:58:41 -0400

    "rm: think super/case/world/feel_last_time_about_time.py, super/thing/leave_man/fact/come_good_time_after_public_fact/see_case_for_way/bad_place_or_right_group.py or super/go_important_person/big_government/new_part_and_point.py for able year"

---
+++ super/thing/leave_man/fact/come_good_time_after_public_fact/see_case_for_way/bad_place_or_right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    leave_government(str_arg)
+    print('big_day')
+
+def leave_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('new_thing')
