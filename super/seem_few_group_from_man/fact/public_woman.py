7543c168e0357920d6a231b7f76daf11a6a8fd12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 11:57:36 -0400

    "add: use super/seem_few_group_from_man/fact/public_woman.py, super/leave_government_at_child/eye_or_young_thing/want_eye/look_fact/first_year_or_child/young_world_or_life.py, super/person/right_group_or_last_point/way.py or super/high_hand/bad_government/new_man_and_time/use_bad_year_up_big_place/way.py under different eye"

---
+++ super/seem_few_group_from_man/fact/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year(str_arg):
+    want_part_after_man(str_arg)
+    print('child')
+
+def want_part_after_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year('ask_world')
