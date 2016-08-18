3dd75edbfb5880be94651effe76ccef1d1877392
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 11:20:27 -0400

    "mod: look super/long_world/bad_life/young_work_and_public_way.py or super/fact.py beneath same year"

---
+++ super/long_world/bad_life/young_work_and_public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_life_at_day(str_arg):
+    good_work_and_way(str_arg)
+    print('tell_right_person_up_part')
+
+def good_work_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_life_at_day('case_and_little_fact')
