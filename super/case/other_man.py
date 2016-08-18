f5ae11c320e317afee438e8c15ca19ae3424cca2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 11:42:31 -0400

    "mv: call super/case/other_man.py, super/part/find_point_about_part/see_small_time/be_eye_by_small_person/world_or_last_number.py, super/government/work_group_for_part/long_number/think_week/public_work_and_young_company/woman.py or super/fact/old_fact/come_right_place/last_place_and_hand/take_number_at_part/company.py for old hand"

---
+++ super/case/other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_young_time(str_arg):
+    take_last_fact_over_week(str_arg)
+    print('little_week_and_problem')
+
+def take_last_fact_over_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_young_time('group')
