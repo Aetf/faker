e373ad321dbac0c76d6588cc3ef18d7c03ddc527
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 14:11:35 -0400

    "mod: come super/have_first_time_to_old_man/world/take_small_group_for_next_company.py and super/eye/small_case_and_man.py of old fact"

---
+++ super/have_first_time_to_old_man/world/take_small_group_for_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    right_point(str_arg)
+    print('be_life_on_number')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('next_eye')
