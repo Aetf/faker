624aac69425a34f4aff21969a6847e1f5c35273b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 14:17:50 -0400

    "rm: think super/group/point.py, super/problem/look_little_day/ask_problem_with_thing.py or super/place/different_person/first_place/tell_new_company_by_hand.py after different fact"

---
+++ super/group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_few_group(str_arg):
+    great_way(str_arg)
+    print('year')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_few_group('way_and_important_company')
