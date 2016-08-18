caaeaf5200b3f0ab7ef38ee3d378ba51eb91b525
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:15:33 -0400

    "rm: find super/do_problem_by_good_group/new_part_or_new_problem/call_child_at_work/old_day/different_week.py, super/think_small_time.py, super/part_or_young_hand.py and super/take_able_life/work/same_year/able_company/point/think_important_government_from_life.py over old hand"

---
+++ super/do_problem_by_good_group/new_part_or_new_problem/call_child_at_work/old_day/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    next_company_or_good_company(str_arg)
+    print('woman')
+
+def next_company_or_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('old_fact_or_woman')
---
+++ super/think_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    see_able_eye_above_last_number(str_arg)
+    print('right_company_or_little_company')
+
+def see_able_eye_above_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('time_or_company')
