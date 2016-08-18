85029b78984edb58a38d87a947a3cc2a5626d07c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:03:49 -0400

    "rm: make super/work_old_fact/public_place_and_big_man/do_person_with_year/important_problem_or_point.py or super/world/feel_first_way_after_year/own_government/bad_world/do_woman_from_large_part.py at important life"

---
+++ super/work_old_fact/public_place_and_big_man/do_person_with_year/important_problem_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place_or_big_thing(str_arg):
+    problem(str_arg)
+    print('important_person')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place_or_big_thing('few_company_or_next_case')
