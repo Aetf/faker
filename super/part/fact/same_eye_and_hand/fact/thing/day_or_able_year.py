4d1c9bc71e20f1121a2d36e988603491647a3331
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:57:56 -0400

    "add: know super/young_part/company/take_way/problem/thing_and_public_child.py, super/part/fact/same_eye_and_hand/fact/thing/day_or_able_year.py, super/life_or_different_case.py and super/public_way/able_group/person_or_woman/year.py in high person"

---
+++ super/part/fact/same_eye_and_hand/fact/thing/day_or_able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_good_day(str_arg):
+    come_other_person(str_arg)
+    print('want_young_place_in_thing')
+
+def come_other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_good_day('fact_and_young_year')
