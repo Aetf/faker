3c049f28bf5fc4c806932cab834af97dece416dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 14:17:07 -0400

    "mod: do super/important_world_and_thing/woman/child_and_part/part/work_or_big_government.py, super/get_number_about_next_group/have_eye/public_number/few_person.py, super/find_government/try_bad_place/man.py and super/few_time.py by long fact"

---
+++ super/few_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    different_week_and_way(str_arg)
+    print('part')
+
+def different_week_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('person')
---
+++ super/find_government/try_bad_place/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_hand(str_arg)
+    print('right_case')
+
+def same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('person')
