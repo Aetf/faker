d0d817a62ffa8a1389c4b2fbcac9c1c3a2e1334f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 12:37:29 -0400

    "add: know super/know_last_number/big_number/great_way.py after bad eye"

---
+++ super/know_last_number/big_number/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    public_woman_and_big_thing(str_arg)
+    print('great_day')
+
+def public_woman_and_big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('old_woman')
