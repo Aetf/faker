dd6ea8f9ccac90e684b2f203e5e164aa3c7e15ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:44:57 -0400

    "mv: work super/go_large_number_into_child/hand_and_person.py, super/early_eye.py and super/next_part/try_few_problem/woman/ask_long_child.py from next child"

---
+++ super/early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work(str_arg):
+    different_way_or_way(str_arg)
+    print('thing_and_week')
+
+def different_way_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work('do_next_time_at_first_case')
---
+++ super/next_part/try_few_problem/woman/ask_long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    large_work(str_arg)
+    print('great_place_and_thing')
+
+def large_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('man_or_life')
