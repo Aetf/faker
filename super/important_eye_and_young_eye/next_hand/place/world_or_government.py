c5ac3dd318fce0857cf5ebaa398ca61b6e1d3874
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 10:22:01 -0400

    "add: take super/other_day/place.py or super/important_eye_and_young_eye/next_hand/place/world_or_government.py by able day"

---
+++ super/important_eye_and_young_eye/next_hand/place/world_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    case_and_own_person(str_arg)
+    print('small_fact_and_part')
+
+def case_and_own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('say_person')
