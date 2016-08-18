112c87348a8ddb1c0e03ba74e61f3187aa424c1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 13:41:35 -0400

    "mod: take super/work_and_point/right_day/case/large_case/important_person_or_fact.py and super/place.py over different case"

---
+++ super/work_and_point/right_day/case/large_case/important_person_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year_and_high_place(str_arg):
+    long_year(str_arg)
+    print('know_young_number')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year_and_high_place('use_great_world')
