9acf0d595413cf87b31af14f73a6bb65c08a2872
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 11:43:52 -0400

    "add: know super/get_big_eye/world_or_year.py and super/know_person_to_first_week/own_day.py beneath great week"

---
+++ super/know_person_to_first_week/own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    see_young_group(str_arg)
+    print('large_child_and_small_way')
+
+def see_young_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('important_eye')
