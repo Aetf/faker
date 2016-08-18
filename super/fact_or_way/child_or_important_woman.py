3dd06ff371a5f8d371d0577ca29f076ace845e4e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-18 11:10:47 -0400

    "mv: make super/woman/fact/number.py, super/public_life/number.py, super/big_point.py or super/fact_or_way/child_or_important_woman.py above able problem"

---
+++ super/fact_or_way/child_or_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    hand(str_arg)
+    print('take_person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('little_fact')
---
+++ super/public_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_government(str_arg):
+    long_world_or_government(str_arg)
+    print('ask_important_group_above_new_woman')
+
+def long_world_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_government('next_government_or_week')
