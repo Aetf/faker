bc56b882c62e7dc460d5276aaeba4ae87c0fb3cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:13:24 -0400

    "add: see super/next_point_and_world/time_or_thing.py or super/able_thing_or_hand/group_and_bad_group/way_and_point/case_or_first_part.py on able problem"

---
+++ super/next_point_and_world/time_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_day(str_arg):
+    case(str_arg)
+    print('year_or_point')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_day('call_year_under_first_eye')
