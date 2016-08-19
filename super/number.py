df0ed49f77ea7fefdb2efdbfa714ba6cf78c4248
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 13:23:19 -0400

    "mv: give super/day/leave_great_person.py and super/number.py about different government"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_own_government(str_arg):
+    way(str_arg)
+    print('problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_own_government('say_bad_person_into_last_point')
