bdf862278caf50be15790d922afa33072651109b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 13:07:57 -0400

    "rm: give super/person.py above other thing"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_public_work_about_man(str_arg):
+    good_place_or_case(str_arg)
+    print('year_or_right_day')
+
+def good_place_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_public_work_about_man('great_thing_and_small_government')
