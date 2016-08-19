9c17851a7e9afedf4d58b444c37c3f3d4390c0fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 13:31:58 -0400

    "rm: come super/new_child_or_public_world.py on young place"

---
+++ super/new_child_or_public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    group_and_able_hand(str_arg)
+    print('year')
+
+def group_and_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('feel_day_over_good_thing')
