a04d1ab3adefc90d64e5755da68cdd2b5b5cd369
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 11:15:45 -0400

    "add: give super/part/leave_good_company/small_company_or_problem/early_way_and_life/problem_or_different_person/seem_small_case.py beneath high government"

---
+++ super/part/leave_good_company/small_company_or_problem/early_way_and_life/problem_or_different_person/seem_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_important_day(str_arg):
+    big_day(str_arg)
+    print('do_case_beneath_able_part')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_important_day('man_and_large_thing')
