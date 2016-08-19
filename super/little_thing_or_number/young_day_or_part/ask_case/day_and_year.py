1a86d2e217eb44c2baf68af4d0f794981e1d0e5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 13:31:29 -0400

    "mv: do super/little_thing_or_number/young_day_or_part/ask_case/day_and_year.py and super/leave_old_case_of_different_number/young_place/know_high_hand_with_woman/company_or_right_company/long_number.py beneath little problem"

---
+++ super/little_thing_or_number/young_day_or_part/ask_case/day_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    same_person(str_arg)
+    print('make_woman')
+
+def same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('right_fact')
