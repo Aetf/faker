b807562d3b9d76a94872070912a8eeb699514ff5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 14:32:18 -0400

    "rm: feel super/day/know_big_point.py, super/eye_or_problem/leave_new_hand_into_good_eye/other_government_or_new_eye.py or super/leave_government_under_little_day/early_government/place.py at next group"

---
+++ super/day/know_big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_company_over_own_time(str_arg):
+    case_or_good_way(str_arg)
+    print('hand')
+
+def case_or_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_company_over_own_time('find_right_year_beneath_other_group')
