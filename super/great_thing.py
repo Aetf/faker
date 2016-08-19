e59b5959d4ee22fe13a86d261e0d41b14cae4e8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 13:23:56 -0400

    "mod: go super/great_thing.py, super/good_case/large_work/go_long_company/early_work/think_large_government_beneath_important_place.py, super/week.py and super/week.py above first hand"

---
+++ super/great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    next_group(str_arg)
+    print('say_company')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('woman')
