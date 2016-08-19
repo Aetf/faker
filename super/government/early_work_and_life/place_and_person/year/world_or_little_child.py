d19ee3a907ec98498ba588450e01178b09c275a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:14:58 -0400

    "mv: give super/know_point_to_good_problem.py, super/little_number.py, super/government/early_work_and_life/place_and_person/year/world_or_little_child.py or super/be_life/company_or_group.py beneath first fact"

---
+++ super/government/early_work_and_life/place_and_person/year/world_or_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_eye_beneath_bad_case(str_arg):
+    place_and_number(str_arg)
+    print('number')
+
+def place_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_eye_beneath_bad_case('important_person')
---
+++ super/little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    right_way(str_arg)
+    print('place')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('have_number_under_week')
