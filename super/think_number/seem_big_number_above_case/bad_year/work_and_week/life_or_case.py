cc480578181c25d516875e130f21165ec2e56441
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:39:05 -0400

    "rm: leave super/think_number/seem_big_number_above_case/bad_year/work_and_week/life_or_case.py, super/want_able_government/next_fact/get_part/next_person.py and super/number.py up new hand"

---
+++ super/think_number/seem_big_number_above_case/bad_year/work_and_week/life_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_few_week(str_arg):
+    able_case_or_large_woman(str_arg)
+    print('little_number')
+
+def able_case_or_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_few_week('know_few_government')
---
+++ super/want_able_government/next_fact/get_part/next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    seem_year_at_few_case(str_arg)
+    print('good_hand_or_problem')
+
+def seem_year_at_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('work_new_time_in_little_government')
