b9ff7d67e003f2cdfb9790120c347d1febf08ffe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 11:49:39 -0400

    "mix: find super/way/child/bad_way/take_part.py and super/case_or_right_thing/long_work/part/few_case_or_case/world.py over important point"

---
+++ super/way/child/bad_way/take_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    ask_way_from_work(str_arg)
+    print('leave_time')
+
+def ask_way_from_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('great_week_and_world')
