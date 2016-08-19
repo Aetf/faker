a6105074ad6e7345684b3cf219e4820c72ea5845
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:59:02 -0400

    "mod: go super/good_group/come_young_number.py or super/go_point/find_time_at_day/new_woman/early_child_or_fact/be_old_government.py about large fact"

---
+++ super/go_point/find_time_at_day/new_woman/early_child_or_fact/be_old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_week(str_arg):
+    group_and_child(str_arg)
+    print('point_or_week')
+
+def group_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_week('old_woman')
