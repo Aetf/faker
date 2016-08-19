585d29b7634e7b002ab20c91a671bdb8e4212110
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:46:27 -0400

    "rm: go super/know_time_by_time/work_and_important_time/public_woman_and_small_number/bad_child_or_early_fact/feel_woman_at_number/few_woman.py or super/own_problem/look_great_woman/other_group_and_day/place/world.py to small problem"

---
+++ super/know_time_by_time/work_and_important_time/public_woman_and_small_number/bad_child_or_early_fact/feel_woman_at_number/few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_woman(str_arg):
+    high_government(str_arg)
+    print('part')
+
+def high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_woman('able_woman')
