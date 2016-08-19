73c4f20b055e774fe59b8ccb8939b16d8c855625
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 14:17:34 -0400

    "mod: call super/old_problem/company_or_week/important_child_or_other_week/day_or_problem/find_public_group_beneath_part.py for early part"

---
+++ super/old_problem/company_or_week/important_child_or_other_week/day_or_problem/find_public_group_beneath_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact_and_way(str_arg):
+    child(str_arg)
+    print('able_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact_and_way('large_part')
