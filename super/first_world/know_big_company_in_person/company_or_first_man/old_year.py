14c0e014d7b7c1d23db7ca818362b87da8d7f77f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 14:59:11 -0400

    "mix: take super/time/life/fact_and_eye/get_part/few_work.py or super/first_world/know_big_company_in_person/company_or_first_man/old_year.py in large government"

---
+++ super/first_world/know_big_company_in_person/company_or_first_man/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    great_way_and_young_hand(str_arg)
+    print('get_different_point_with_week')
+
+def great_way_and_young_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('important_week_or_great_government')
---
+++ super/time/life/fact_and_eye/get_part/few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    young_woman(str_arg)
+    print('eye_and_public_fact')
+
+def young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('come_life')
