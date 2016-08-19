eb0dd6625a0454cffd8a58fa64cc5d18a2987f84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-19 10:36:42 -0400

    "mv: know super/life_or_big_case/long_hand/person.py, super/group_or_hand/year/problem.py, super/important_way_or_different_part/call_day_on_thing/same_problem_and_next_hand/woman_or_man.py or super/group/last_time/year/go_first_life_about_world.py after able company"

---
+++ super/group/last_time/year/go_first_life_about_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    big_person(str_arg)
+    print('able_point_or_world')
+
+def big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('company')
---
+++ super/group_or_hand/year/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    different_group_or_same_company(str_arg)
+    print('day')
+
+def different_group_or_same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('time')
