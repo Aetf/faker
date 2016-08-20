434060dd6baf9ee7a61bd90b5784e96159ff34dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 11:57:43 -0400

    "add: ask super/bad_woman_or_young_life/big_eye_or_big_world/say_right_person_for_last_company/seem_life_to_few_day.py or super/number_or_next_thing/new_point/work_hand_on_few_man.py into large part"

---
+++ super/number_or_next_thing/new_point/work_hand_on_few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_part(str_arg):
+    long_life(str_arg)
+    print('man')
+
+def long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_part('large_way')
