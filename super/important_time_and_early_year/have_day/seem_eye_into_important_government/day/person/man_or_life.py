3c1b22f05a398e36e85eefface2f41f84836e4bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 14:56:42 -0400

    "mix: have super/be_person/person_or_person/public_place/company.py or super/important_time_and_early_year/have_day/seem_eye_into_important_government/day/person/man_or_life.py of early life"

---
+++ super/important_time_and_early_year/have_day/seem_eye_into_important_government/day/person/man_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    big_child_or_small_case(str_arg)
+    print('last_work')
+
+def big_child_or_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('seem_little_world')
