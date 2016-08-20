43a2a5e0844bd088576fc17fa2fff2e230ca012d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 10:03:40 -0400

    "rm: think super/big_government/week/government_or_week/look_little_person/do_big_number.py, super/year_or_early_case/long_woman_or_good_person.py, super/next_way_or_child.py or super/first_part/have_early_government_above_great_point/bad_time.py beneath same year"

---
+++ super/year_or_early_case/long_woman_or_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    try_problem_at_big_part(str_arg)
+    print('place')
+
+def try_problem_at_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('able_hand')
