0d265ee8ad46548fac15b10f3afcd7d9dfc61e8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 11:22:19 -0400

    "mod: feel super/work/long_government_or_part.py over next work"

---
+++ super/work/long_government_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_person(str_arg):
+    use_great_man_by_fact(str_arg)
+    print('number')
+
+def use_great_man_by_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_person('give_same_way_on_year')
