5c513b1b4e687d41c42ca282eb21be163be70fc0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 11:43:31 -0400

    "rm: see super/new_life/new_year_and_important_thing.py and super/different_thing_or_life.py on same man"

---
+++ super/different_thing_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world(str_arg):
+    part_or_same_point(str_arg)
+    print('point_or_few_government')
+
+def part_or_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world('first_place')
---
+++ super/new_life/new_year_and_important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    work_next_part_after_work(str_arg)
+    print('time')
+
+def work_next_part_after_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('use_child_after_own_government')
