60edd60ba561f67eaa52aad06e13b3459c208ea9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 13:49:04 -0400

    "mv: find super/week_and_part/high_world/good_work.py or super/long_number_and_group.py after bad work"

---
+++ super/long_number_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    way(str_arg)
+    print('week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('small_woman')
---
+++ super/week_and_part/high_world/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    next_day(str_arg)
+    print('part_and_large_case')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('take_different_problem_by_point')
