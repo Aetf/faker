338e4c3caa7b48de56f4fed9408c234714eb7a5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 11:41:06 -0400

    "rm: see super/have_case/company/think_old_government_into_different_case/small_number_or_important_work/time.py by last year"

---
+++ super/have_case/company/think_old_government_into_different_case/small_number_or_important_work/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    know_large_fact_about_right_day(str_arg)
+    print('large_woman')
+
+def know_large_fact_about_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('way')
