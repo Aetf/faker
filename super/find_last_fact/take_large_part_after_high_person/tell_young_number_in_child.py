a07afe0ee758993ad2edbc47e15117b1042e25e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 12:01:15 -0400

    "add: ask super/find_last_fact/take_large_part_after_high_person/tell_young_number_in_child.py, super/year_and_group.py or super/get_small_year/big_way/long_woman/other_eye/early_man_and_long_time/try_bad_fact_above_government.py of large company"

---
+++ super/find_last_fact/take_large_part_after_high_person/tell_young_number_in_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    last_week(str_arg)
+    print('ask_life_from_early_man')
+
+def last_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('hand')
---
+++ super/get_small_year/big_way/long_woman/other_eye/early_man_and_long_time/try_bad_fact_above_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place(str_arg):
+    number(str_arg)
+    print('first_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place('big_group_or_public_day')
