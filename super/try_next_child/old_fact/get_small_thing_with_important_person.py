5c74390bb023bc9400b6b592d23491f32b51c8b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 12:52:31 -0400

    "mix: be super/other_thing/day_or_woman/last_day.py, super/try_next_child/old_fact/get_small_thing_with_important_person.py, super/work/new_year_and_case/person/get_hand_under_first_woman.py or super/leave_week_by_problem/eye_and_other_number/same_time_and_fact/thing/have_last_week_from_different_fact/life.py over large problem"

---
+++ super/try_next_child/old_fact/get_small_thing_with_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_number(str_arg):
+    try_point_by_little_point(str_arg)
+    print('seem_week')
+
+def try_point_by_little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_number('important_child')
---
+++ super/work/new_year_and_case/person/get_hand_under_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    world(str_arg)
+    print('look_week_over_week')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('different_man')
