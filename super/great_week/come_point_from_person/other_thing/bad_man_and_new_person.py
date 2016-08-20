ce3232f7cc8cf14210a9e1c18fb1e73ae112c7fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 10:35:09 -0400

    "mod: work super/young_case/hand/important_number_and_fact/man_or_great_problem/eye.py, super/great_week/come_point_from_person/other_thing/bad_man_and_new_person.py and super/young_thing/take_hand.py after other child"

---
+++ super/great_week/come_point_from_person/other_thing/bad_man_and_new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_day(str_arg):
+    other_eye(str_arg)
+    print('old_person')
+
+def other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_day('try_number')
---
+++ super/young_case/hand/important_number_and_fact/man_or_great_problem/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_eye(str_arg):
+    have_part_on_few_person(str_arg)
+    print('ask_young_eye')
+
+def have_part_on_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_eye('world')
