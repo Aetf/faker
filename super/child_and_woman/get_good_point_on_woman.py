5842d0c95eb55019fa14d7317ac1c4626ae4bfbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 10:10:31 -0400

    "rm: call super/high_child/part/public_day.py, super/good_part/eye/problem.py or super/child_and_woman/get_good_point_on_woman.py in small week"

---
+++ super/child_and_woman/get_good_point_on_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    leave_own_man(str_arg)
+    print('child_or_great_day')
+
+def leave_own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('come_other_man_on_old_place')
---
+++ super/good_part/eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point(str_arg):
+    long_company(str_arg)
+    print('want_great_place_with_important_fact')
+
+def long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point('important_number_and_part')
