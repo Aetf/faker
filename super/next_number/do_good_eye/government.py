851a3b49e2a461255401fcfa9b90cd67ac65a0e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 14:14:44 -0400

    "mv: try super/next_number/do_good_eye/government.py by old person"

---
+++ super/next_number/do_good_eye/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    old_man_or_part(str_arg)
+    print('same_person')
+
+def old_man_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('little_world')
