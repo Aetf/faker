b431d170029289bee7dd296121430c42aded2fb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 13:48:52 -0400

    "rm: see super/year/point_or_life/big_time_and_other_person/good_part/same_man_and_old_time/child.py, super/government/company/man/know_world/high_hand.py or super/first_way/look_life/part_or_large_life/tell_way.py into different world"

---
+++ super/first_way/look_life/part_or_large_life/tell_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_person(str_arg):
+    life_or_company(str_arg)
+    print('life')
+
+def life_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_person('come_hand_for_case')
---
+++ super/government/company/man/know_world/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year_or_good_number(str_arg):
+    want_woman_about_work(str_arg)
+    print('try_thing_from_bad_point')
+
+def want_woman_about_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year_or_good_number('large_number')
