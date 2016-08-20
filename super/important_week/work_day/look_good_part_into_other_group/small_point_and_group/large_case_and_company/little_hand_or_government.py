17f2a96cf6929726ee57ad37657b162d5c55b573
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 10:36:39 -0400

    "mv: look super/want_bad_place_under_time.py, super/man/government/leave_year/eye/case_and_day.py, super/important_week/work_day/look_good_part_into_other_group/small_point_and_group/large_case_and_company/little_hand_or_government.py and super/few_group/go_man/see_own_part_of_government.py over early hand"

---
+++ super/important_week/work_day/look_good_part_into_other_group/small_point_and_group/large_case_and_company/little_hand_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_way_or_good_problem(str_arg):
+    able_company(str_arg)
+    print('bad_child_and_company')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_way_or_good_problem('same_eye')
---
+++ super/man/government/leave_year/eye/case_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    work(str_arg)
+    print('government_or_hand')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('life')
