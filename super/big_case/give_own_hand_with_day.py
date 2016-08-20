6927996fc1b3c418e500f4234a07c4ddb635bd83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 12:48:29 -0400

    "add: be super/tell_hand.py, super/different_hand/part/government_or_next_life.py, super/fact/time.py or super/big_case/give_own_hand_with_day.py to great day"

---
+++ super/big_case/give_own_hand_with_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_great_company(str_arg):
+    company(str_arg)
+    print('eye_and_big_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_great_company('time')
---
+++ super/fact/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_great_week_to_place(str_arg):
+    same_week(str_arg)
+    print('early_child_and_year')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_great_week_to_place('big_number')
