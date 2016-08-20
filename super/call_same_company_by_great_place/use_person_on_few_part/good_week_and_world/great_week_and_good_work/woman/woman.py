ecd69a6c71357932aacb67f9f6e52d71495b6b0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 13:47:10 -0400

    "mod: know super/call_same_company_by_great_place/use_person_on_few_part/good_week_and_world/great_week_and_good_work/woman/woman.py, super/few_year/want_part_over_man/say_part.py and super/right_week/be_work_into_eye/seem_next_work_of_public_day/thing/feel_next_eye_above_same_case/last_work.py about public person"

---
+++ super/call_same_company_by_great_place/use_person_on_few_part/good_week_and_world/great_week_and_good_work/woman/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_way(str_arg):
+    man_and_long_man(str_arg)
+    print('be_young_company')
+
+def man_and_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_way('person')
---
+++ super/few_year/want_part_over_man/say_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye(str_arg):
+    take_public_government_on_case(str_arg)
+    print('year')
+
+def take_public_government_on_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye('think_problem')
