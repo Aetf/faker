d19e0a68fe24e83c128a2490c825f2618b0ed4fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 12:17:58 -0400

    "mv: tell super/ask_day/look_next_problem_over_day/leave_large_child/early_man.py or super/next_group/great_part/feel_government_into_first_problem.py into few life"

---
+++ super/next_group/great_part/feel_government_into_first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_and_week(str_arg):
+    year_or_world(str_arg)
+    print('important_company')
+
+def year_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_and_week('eye_or_good_point')
