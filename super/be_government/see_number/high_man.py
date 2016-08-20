63a1ba7fcd52c4c60462f4397a53e63869dd255b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 12:07:08 -0400

    "add: have super/be_government/see_number/high_man.py, super/next_person.py, super/woman/fact_and_important_group/work_or_part/woman.py and super/eye/think_life_under_good_world/same_time.py by great part"

---
+++ super/be_government/see_number/high_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    fact(str_arg)
+    print('problem')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_place_or_week')
---
+++ super/woman/fact_and_important_group/work_or_part/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_year(str_arg):
+    fact(str_arg)
+    print('give_last_year')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_year('big_woman_or_thing')
