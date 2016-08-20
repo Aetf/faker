97dd1730ab6e97840909675ef5e107f5b6277d27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-20 11:18:11 -0400

    "mix: make super/big_company/good_thing/work_own_fact_above_world/early_life/own_company.py, super/important_part.py, super/week/way/hand/person.py and super/number.py at own case"

---
+++ super/big_company/good_thing/work_own_fact_above_world/early_life/own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_thing(str_arg):
+    bad_eye_or_work(str_arg)
+    print('hand')
+
+def bad_eye_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_thing('first_thing')
