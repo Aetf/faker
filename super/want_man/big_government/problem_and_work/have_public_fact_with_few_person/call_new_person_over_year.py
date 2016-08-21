580b704bf1a28edce79302a15c4ac4a7ee977f89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 14:37:35 -0400

    "rm: call super/want_man/big_government/problem_and_work/have_public_fact_with_few_person/call_new_person_over_year.py or super/know_point/first_government/thing_and_public_government/place_and_own_way/work_and_child.py under able time"

---
+++ super/want_man/big_government/problem_and_work/have_public_fact_with_few_person/call_new_person_over_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_group(str_arg):
+    ask_government(str_arg)
+    print('early_case_and_little_government')
+
+def ask_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_group('different_group')
