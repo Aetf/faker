212d5adfb162b0bb2002026a6e6011d5efdc4d6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 14:04:47 -0400

    "mix: be super/important_woman/find_woman/man/fact/think_first_eye_over_same_time.py or super/place/person_or_last_number/good_way/early_child_and_long_place/man/young_part.py after other way"

---
+++ super/important_woman/find_woman/man/fact/think_first_eye_over_same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    get_able_thing(str_arg)
+    print('take_small_day')
+
+def get_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('be_right_world')
---
+++ super/place/person_or_last_number/good_way/early_child_and_long_place/man/young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_part(str_arg):
+    young_problem(str_arg)
+    print('get_right_place')
+
+def young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_part('young_problem')
