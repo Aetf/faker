2c790fbc6c34e8cc21d59f35cfd23ca5b1279cbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 13:43:33 -0400

    "mv: work super/find_old_number_over_company/take_life/high_company/try_different_woman_beneath_able_group/day_or_high_child/think_few_point_after_child.py, super/say_person_after_man.py and super/have_life/day.py above next man"

---
+++ super/find_old_number_over_company/take_life/high_company/try_different_woman_beneath_able_group/day_or_high_child/think_few_point_after_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman(str_arg):
+    last_child(str_arg)
+    print('few_case')
+
+def last_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman('problem_or_own_case')
---
+++ super/have_life/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    try_life(str_arg)
+    print('group')
+
+def try_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('point')
