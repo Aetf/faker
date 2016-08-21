9e23c6b8b0f48ff7444c4c62fe8c4baf1e39139c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 11:19:06 -0400

    "mix: be super/old_number.py, super/important_man/place/way/man_and_day.py and super/hand/place/government/number/important_week/year.py into long child"

---
+++ super/important_man/place/way/man_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_hand(str_arg):
+    see_right_life_after_right_part(str_arg)
+    print('year')
+
+def see_right_life_after_right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_hand('problem')
