77005353e5cce8f43aceb1b024f73f1f359813ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 14:58:36 -0400

    "mix: go super/last_thing_or_eye/young_government.py or super/first_place.py at other problem"

---
+++ super/first_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_problem_and_fact(str_arg):
+    last_woman_or_first_thing(str_arg)
+    print('get_first_child')
+
+def last_woman_or_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_problem_and_fact('use_part_for_point')
