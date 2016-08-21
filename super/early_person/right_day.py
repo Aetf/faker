9d1ff4d376d1f46d7895c1fdfdb8dca7365a94e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 10:11:53 -0400

    "mv: do super/early_person/right_day.py in few problem"

---
+++ super/early_person/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    year_and_number(str_arg)
+    print('ask_man')
+
+def year_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('find_high_world')
