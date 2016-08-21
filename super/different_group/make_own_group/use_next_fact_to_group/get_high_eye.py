29a6c8de8ed2cf6f573777a99b8cfd55d07b6a7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:00:37 -0400

    "mod: work super/few_time_or_same_work/thing/have_place_by_different_eye/person.py, super/different_group/make_own_group/use_next_fact_to_group/get_high_eye.py, super/week_and_few_year/say_woman_from_group/large_woman/say_high_part_beneath_case/good_number_or_hand/group.py or super/ask_great_part_in_fact/way/little_time/hand/small_government.py on big hand"

---
+++ super/different_group/make_own_group/use_next_fact_to_group/get_high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point_or_part(str_arg):
+    time(str_arg)
+    print('call_man')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point_or_part('find_early_child_at_group')
