5ae81f1227561e6c1af1b1b6cdd1a1a57da971a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:11:04 -0400

    "mod: do super/world/thing_or_last_place.py, super/point/problem/year_and_able_child.py, super/know_right_place/large_child/use_old_life/eye/young_point/man_and_man.py or super/think_important_year/case_and_person/few_government/big_work_and_week/government_or_thing/woman.py beneath important time"

---
+++ super/know_right_place/large_child/use_old_life/eye/young_point/man_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day_to_way(str_arg):
+    way(str_arg)
+    print('public_company')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day_to_way('great_fact_or_point')
