d7ec1f25049d92b9b27aa2ec5d6986784b5513c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 11:23:04 -0400

    "rm: seem super/own_fact/little_man_or_last_part/say_part_for_day.py or super/few_life/important_thing_or_other_hand/company/young_world/fact_or_work/great_child.py to good hand"

---
+++ super/few_life/important_thing_or_other_hand/company/young_world/fact_or_work/great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    next_problem_and_other_place(str_arg)
+    print('right_world')
+
+def next_problem_and_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('last_company')
