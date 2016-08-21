8d38c5feb4b2d6f1f3f84f706ee51c4b21a6f4a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:28:55 -0400

    "mv: leave super/work_and_point/get_different_part/different_world_or_same_year/last_hand_and_life.py, super/group/next_way/have_number.py and super/hand/want_last_year_by_next_company.py about young time"

---
+++ super/hand/want_last_year_by_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_own_day(str_arg):
+    long_year(str_arg)
+    print('work')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_own_day('company')
