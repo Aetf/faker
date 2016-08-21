c152ba0bacb814cf2d5fcb5c7358c834e181d52f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:49:29 -0400

    "mv: think super/call_woman.py, super/week_or_place/way/early_government_and_first_child/new_eye/important_problem/seem_point_for_part.py or super/able_hand_or_woman/bad_group_or_life/own_problem_or_group/thing_or_different_place.py under bad person"

---
+++ super/week_or_place/way/early_government_and_first_child/new_eye/important_problem/seem_point_for_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point_or_time(str_arg):
+    call_number_for_first_child(str_arg)
+    print('number')
+
+def call_number_for_first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point_or_time('next_fact_and_able_part')
