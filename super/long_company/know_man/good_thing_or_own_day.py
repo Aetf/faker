f8b377adbb2de3584e43dff0260a3cdc3af11b00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 14:48:16 -0400

    "mod: think super/long_company/know_man/good_thing_or_own_day.py of right problem"

---
+++ super/long_company/know_man/good_thing_or_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye_or_point(str_arg):
+    problem(str_arg)
+    print('able_life')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye_or_point('important_case_or_last_number')
