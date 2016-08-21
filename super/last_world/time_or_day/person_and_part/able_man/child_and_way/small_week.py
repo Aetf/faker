adf7208f9006477326f4804ef07fbee30926e3a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:08:51 -0400

    "mv: seem super/leave_old_place_in_person/young_woman_or_eye.py or super/last_world/time_or_day/person_and_part/able_man/child_and_way/small_week.py to good eye"

---
+++ super/last_world/time_or_day/person_and_part/able_man/child_and_way/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_good_hand_on_young_company(str_arg):
+    place(str_arg)
+    print('thing')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_good_hand_on_young_company('thing_and_part')
