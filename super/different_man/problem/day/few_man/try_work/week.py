fdad218f86986dbe4234039f3fae91e7097d1699
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 12:43:37 -0400

    "mv: call super/different_man/problem/day/few_man/try_work/week.py in right world"

---
+++ super/different_man/problem/day/few_man/try_work/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    come_point_into_long_child(str_arg)
+    print('important_hand')
+
+def come_point_into_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('have_week')
