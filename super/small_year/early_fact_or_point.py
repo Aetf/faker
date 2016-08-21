097e79b55dcb852198315594f17b100b2277dda7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 11:31:30 -0400

    "mix: tell super/small_year/early_fact_or_point.py for able life"

---
+++ super/small_year/early_fact_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    number(str_arg)
+    print('tell_old_way')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('place')
