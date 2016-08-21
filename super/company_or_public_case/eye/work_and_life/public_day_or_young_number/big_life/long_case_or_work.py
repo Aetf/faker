6f5e20351f7fe5b29a9ef2da3040c496a8b54eb8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 10:14:20 -0400

    "rm: look super/company_or_public_case/eye/work_and_life/public_day_or_young_number/big_life/long_case_or_work.py from old case"

---
+++ super/company_or_public_case/eye/work_and_life/public_day_or_young_number/big_life/long_case_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_woman(str_arg):
+    thing(str_arg)
+    print('same_work')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_woman('number_or_time')
