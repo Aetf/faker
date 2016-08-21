83d0334cbabef3e18c16dbb39d133e1cbb6c4c88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 10:59:23 -0400

    "add: ask super/place/different_place_and_point.py, super/government/important_thing_or_bad_person.py, super/problem_and_own_point/year_or_problem/ask_fact_above_public_place.py and super/life/give_group/eye/week/get_point_on_young_man.py for public hand"

---
+++ super/life/give_group/eye/week/get_point_on_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    old_world_and_government(str_arg)
+    print('call_life')
+
+def old_world_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('tell_problem')
---
+++ super/problem_and_own_point/year_or_problem/ask_fact_above_public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_point(str_arg):
+    part_and_young_company(str_arg)
+    print('place')
+
+def part_and_young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_point('time_or_case')
