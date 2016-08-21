57dcc56ecb79a264c34a9bf18639806bdb6008ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 14:15:20 -0400

    "mv: say super/seem_high_group/large_work/old_man.py under right thing"

---
+++ super/seem_high_group/large_work/old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_year_up_young_woman(str_arg):
+    way(str_arg)
+    print('high_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_year_up_young_woman('know_case')
