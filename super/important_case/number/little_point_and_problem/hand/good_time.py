177d493c4e977de9f385eb3d5708e51b95f8fe1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 13:55:45 -0400

    "mod: think super/important_case/number/little_point_and_problem/hand/good_time.py and super/new_eye/time/right_place/have_problem_up_part.py under great world"

---
+++ super/important_case/number/little_point_and_problem/hand/good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_problem(str_arg):
+    first_problem(str_arg)
+    print('point')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_problem('little_company_and_hand')
