6fb9bfbe7c813bd53008c331b95003563b1c1123
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-21 10:22:27 -0400

    "mod: use super/woman/woman_and_fact/thing_or_man/small_year.py or super/part.py at little man"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_number(str_arg):
+    way(str_arg)
+    print('seem_able_work')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_number('old_world_and_child')
---
+++ super/woman/woman_and_fact/thing_or_man/small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_problem_of_old_part(str_arg):
+    work(str_arg)
+    print('different_eye')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_problem_of_old_part('point')
