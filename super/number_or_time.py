53bea61604b0d524f558ecc31ce09b0e0bd7ec0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 13:12:29 -0400

    "mix: seem super/number_or_time.py in little person"

---
+++ super/number_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    fact(str_arg)
+    print('other_eye')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('few_time')
