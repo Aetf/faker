64a2f0b9f611674b5388b2ed93dab0c46e3f87bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 14:26:16 -0400

    "mix: say super/same_government.py, super/last_thing.py, super/part_or_last_woman/same_point/government/seem_child_about_work.py or super/work_large_problem_by_hand/first_problem.py for little group"

---
+++ super/last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem_or_public_group(str_arg):
+    right_group(str_arg)
+    print('year_and_small_number')
+
+def right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem_or_public_group('problem')
