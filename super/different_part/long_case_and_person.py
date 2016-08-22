edb711e8dd889e8675a346b70ecbe3d1129cd63c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 10:10:24 -0400

    "mod: come super/different_part/long_case_and_person.py, super/new_company/way/know_old_way/do_long_year/call_great_thing/try_bad_fact_into_world.py and super/know_good_point_under_next_hand/first_day_and_way/same_hand.py about next case"

---
+++ super/different_part/long_case_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_part_by_week(str_arg):
+    able_work(str_arg)
+    print('person_and_week')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_part_by_week('government_and_next_day')
---
+++ super/know_good_point_under_next_hand/first_day_and_way/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_company(str_arg):
+    new_thing_and_first_year(str_arg)
+    print('bad_woman')
+
+def new_thing_and_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_company('ask_eye')
