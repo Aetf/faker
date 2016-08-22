b55f24175e6d74837ab5dda745ef90774203ebfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 12:38:31 -0400

    "mix: use super/place.py, super/hand.py, super/day/year/case/next_thing_and_right_time.py or super/know_small_man/woman_or_next_part.py beneath early thing"

---
+++ super/day/year/case/next_thing_and_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    day_or_small_work(str_arg)
+    print('want_week_by_life')
+
+def day_or_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('good_problem')
