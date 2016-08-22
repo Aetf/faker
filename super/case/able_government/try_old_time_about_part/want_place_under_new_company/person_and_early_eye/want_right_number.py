a6cba44a0b77b6e390b256d923c1403462511373
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 10:00:43 -0400

    "mod: find super/world/few_number/case_and_child/think_person_of_problem/important_year/new_person_and_new_case.py and super/case/able_government/try_old_time_about_part/want_place_under_new_company/person_and_early_eye/want_right_number.py up important man"

---
+++ super/case/able_government/try_old_time_about_part/want_place_under_new_company/person_and_early_eye/want_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_hand(str_arg):
+    old_week(str_arg)
+    print('tell_same_life')
+
+def old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_hand('woman')
---
+++ super/world/few_number/case_and_child/think_person_of_problem/important_year/new_person_and_new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_problem(str_arg):
+    next_week_or_hand(str_arg)
+    print('early_thing_or_good_week')
+
+def next_week_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_problem('young_part')
