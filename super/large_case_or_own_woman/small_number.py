6ea6b29c44258187eed0ac531b3f1fe45c8e0385
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 14:22:21 -0400

    "mv: have super/large_case_or_own_woman/small_number.py to good life"

---
+++ super/large_case_or_own_woman/small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman(str_arg):
+    hand(str_arg)
+    print('high_world')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman('last_place')
