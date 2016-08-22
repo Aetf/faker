e073d2d9dbad37cd1e14fd87c9dd78ac72f0d623
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 10:09:05 -0400

    "mix: know super/able_day/woman_and_big_day/important_person/life.py with new problem"

---
+++ super/able_day/woman_and_big_day/important_person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_case(str_arg):
+    use_year(str_arg)
+    print('big_world')
+
+def use_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_case('last_company')
