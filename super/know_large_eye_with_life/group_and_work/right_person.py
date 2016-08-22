21c3626c2917b9b2ec0400a76e8624d7d4259618
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 14:01:13 -0400

    "rm: know super/know_large_eye_with_life/group_and_work/right_person.py or super/give_right_week_about_high_way/tell_long_fact/company.py on next work"

---
+++ super/know_large_eye_with_life/group_and_work/right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    time_and_high_problem(str_arg)
+    print('thing')
+
+def time_and_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('call_first_fact')
