68703ca3b1faad8be86804c37af05252d54be8c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 11:37:12 -0400

    "add: have super/give_small_man/same_case/make_thing_up_woman/government/see_way_up_person.py, super/man_or_work/problem/new_life.py and super/able_case_or_same_group/year/part_or_new_group/ask_place_in_last_week.py after small year"

---
+++ super/give_small_man/same_case/make_thing_up_woman/government/see_way_up_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    life_and_other_hand(str_arg)
+    print('see_high_day_at_work')
+
+def life_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('person')
