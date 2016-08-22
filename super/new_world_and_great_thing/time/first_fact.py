82cd8762ee60099c05c9f322a43967d1d043b6ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 14:24:32 -0400

    "rm: think super/man/group_and_new_eye.py, super/new_world_and_great_thing/time/first_fact.py or super/government_or_child/woman_or_child/other_way_and_fact/high_hand.py on next company"

---
+++ super/new_world_and_great_thing/time/first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child_or_man(str_arg):
+    right_company_and_year(str_arg)
+    print('old_government')
+
+def right_company_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child_or_man('new_world')
