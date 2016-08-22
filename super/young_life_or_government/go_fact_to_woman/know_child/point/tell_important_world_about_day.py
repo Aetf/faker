8c6ee04bc98db1bf12a0deb6cceb515ecb96041e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 13:20:40 -0400

    "mix: do super/same_world/know_different_point_under_first_day/child/day_or_few_world/public_fact_or_thing.py or super/young_life_or_government/go_fact_to_woman/know_child/point/tell_important_world_about_day.py after few work"

---
+++ super/young_life_or_government/go_fact_to_woman/know_child/point/tell_important_world_about_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    problem(str_arg)
+    print('go_group')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('be_place')
