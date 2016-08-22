74ea60f3d53de30a39af563aa338a00c3eff24a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 12:17:16 -0400

    "mv: know super/point/know_part_at_good_world.py or super/first_week/big_number_and_good_thing/same_day/early_woman_or_own_man/know_own_fact_under_bad_child.py about high eye"

---
+++ super/point/know_part_at_good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman(str_arg):
+    person_and_right_child(str_arg)
+    print('life')
+
+def person_and_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman('time')
