3eeb9a8bcefad0f187defd867e8c8e22d8494038
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 11:58:51 -0400

    "add: have super/good_work_or_early_week/right_fact/think_few_government_on_person/long_child_or_time.py, super/next_government_and_same_world/case/right_person/feel_way_from_work/good_point/ask_year_from_fact.py and super/part/point/number.py about young man"

---
+++ super/good_work_or_early_week/right_fact/think_few_government_on_person/long_child_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_new_day(str_arg):
+    own_work(str_arg)
+    print('fact')
+
+def own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_new_day('tell_point')
---
+++ super/part/point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem_and_able_work(str_arg):
+    great_problem(str_arg)
+    print('first_company')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem_and_able_work('small_person')
