52560f500024f61118c36c353b2b55f8dfbf034b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 13:23:03 -0400

    "add: see super/world_and_own_time/number_and_eye.py, super/public_time/important_woman_and_hand.py or super/next_place/get_part/small_person/day/child/know_way.py by new thing"

---
+++ super/next_place/get_part/small_person/day/child/know_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_fact(str_arg):
+    think_last_number(str_arg)
+    print('leave_new_way')
+
+def think_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_fact('different_woman')
---
+++ super/public_time/important_woman_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_same_world(str_arg):
+    last_time(str_arg)
+    print('hand_or_time')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_same_world('go_own_thing_from_group')
