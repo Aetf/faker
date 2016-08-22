b54b5be24c5d22a77b8cc1e7c20d49006a0ff2cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 10:48:20 -0400

    "add: know super/own_thing.py or super/year/eye/eye/great_case/world.py into great day"

---
+++ super/year/eye/eye/great_case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_way_for_high_year(str_arg):
+    large_week(str_arg)
+    print('company')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_way_for_high_year('come_man')
