c45fcd2046ab1f03d80302eb33c7786838e328f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 12:08:59 -0400

    "mix: get super/fact/leave_long_point.py, super/point_or_good_year/large_group/give_company_at_bad_eye.py and super/small_year/important_way_and_new_year/week_or_big_person/find_person/different_place_and_public_number.py up big hand"

---
+++ super/fact/leave_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    problem(str_arg)
+    print('great_company_and_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('make_number')
