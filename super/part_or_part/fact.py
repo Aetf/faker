bb0ce49dae0d95b5c64977605e92cc594eb10b89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-22 14:35:57 -0400

    "rm: make super/want_old_group_on_right_hand/few_week/next_time_or_child/next_day.py, super/problem.py, super/first_thing.py or super/part_or_part/fact.py under new week"

---
+++ super/part_or_part/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_year_and_right_child(str_arg):
+    feel_fact(str_arg)
+    print('little_thing')
+
+def feel_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_year_and_right_child('government')
---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    different_point(str_arg)
+    print('work')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('big_place')
