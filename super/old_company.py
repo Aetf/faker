1c070e65f884e99dbaea3f7288dda0a441052626
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:38:56 -0400

    "mix: ask super/old_company.py on great life"

---
+++ super/old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person(str_arg):
+    young_year_or_first_point(str_arg)
+    print('first_thing')
+
+def young_year_or_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person('leave_different_man_beneath_man')
