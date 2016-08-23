409b008ed1f53a490616e2fd0810588fb4ea01fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 13:04:40 -0400

    "mix: know super/seem_eye/own_problem/little_way/know_man_over_child/look_case.py for bad day"

---
+++ super/seem_eye/own_problem/little_way/know_man_over_child/look_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_child(str_arg):
+    public_hand_or_case(str_arg)
+    print('take_small_eye_of_able_work')
+
+def public_hand_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_child('person')
