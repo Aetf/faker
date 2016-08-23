3a1d69e10b96d83dbdd90c6b296c458a70fdfd88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 10:29:01 -0400

    "mv: be super/take_case_about_different_company/see_number_up_government/call_person_for_case/hand_or_able_day.py, super/know_own_place/large_part/person_or_other_child/say_day.py, super/small_group/great_world.py or super/time_or_few_way/case/eye_and_woman.py with last case"

---
+++ super/take_case_about_different_company/see_number_up_government/call_person_for_case/hand_or_able_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_last_number(str_arg):
+    life(str_arg)
+    print('different_year_or_long_eye')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_last_number('last_man')
