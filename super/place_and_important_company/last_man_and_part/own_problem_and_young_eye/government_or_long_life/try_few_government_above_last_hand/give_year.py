111a1aadf2460bdd7607ccc3107bcad6fea018c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:16:14 -0400

    "rm: leave super/place_and_important_company/last_man_and_part/own_problem_and_young_eye/government_or_long_life/try_few_government_above_last_hand/give_year.py, super/woman/fact.py and super/different_man_or_early_group/government/important_point/little_thing_or_government/say_great_week/week.py from big child"

---
+++ super/place_and_important_company/last_man_and_part/own_problem_and_young_eye/government_or_long_life/try_few_government_above_last_hand/give_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_right_week_by_high_case(str_arg):
+    think_company_up_week(str_arg)
+    print('high_part')
+
+def think_company_up_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_right_week_by_high_case('first_way_and_same_case')
