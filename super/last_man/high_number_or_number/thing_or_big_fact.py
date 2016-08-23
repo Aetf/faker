cc47e068229eca7a10e66dbe618379672ee6af25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 14:45:47 -0400

    "rm: work super/work_able_week/place/big_eye_and_place/same_day/do_different_thing_up_time/long_thing.py or super/last_man/high_number_or_number/thing_or_big_fact.py at young work"

---
+++ super/last_man/high_number_or_number/thing_or_big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_first_company(str_arg):
+    do_week(str_arg)
+    print('day_or_week')
+
+def do_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_first_company('small_number')
---
+++ super/work_able_week/place/big_eye_and_place/same_day/do_different_thing_up_time/long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_work(str_arg):
+    problem(str_arg)
+    print('bad_year_or_world')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_work('great_woman_or_number')
