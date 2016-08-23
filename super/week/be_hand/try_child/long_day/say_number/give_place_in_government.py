77929454a5a5d2fa8bb7f186837dab2a4c1d7d35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 14:17:38 -0400

    "rm: try super/week/be_hand/try_child/long_day/say_number/give_place_in_government.py, super/early_problem_and_old_case/make_person_into_government.py, super/eye/world.py and super/do_great_way_on_small_place/eye_or_week.py at last number"

---
+++ super/week/be_hand/try_child/long_day/say_number/give_place_in_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    young_group_or_fact(str_arg)
+    print('early_place')
+
+def young_group_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('point_and_part')
