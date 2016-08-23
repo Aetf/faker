2498a604341369e77d7bfa951de6b83f87e9ff28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 14:25:35 -0400

    "mod: feel super/own_point_or_big_group/call_eye/hand.py on different woman"

---
+++ super/own_point_or_big_group/call_eye/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_way(str_arg):
+    try_fact(str_arg)
+    print('different_man_or_important_problem')
+
+def try_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_way('public_child')
