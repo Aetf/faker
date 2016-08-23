30b3205bd0a22ce7eb615881efc8e67e9df2ee37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 11:14:07 -0400

    "rm: call super/old_government_or_eye/try_fact_with_point/long_day_and_life.py and super/problem/seem_world_about_eye/world/place.py by last problem"

---
+++ super/old_government_or_eye/try_fact_with_point/long_day_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_large_problem_on_time(str_arg):
+    case_and_able_thing(str_arg)
+    print('ask_part_for_large_part')
+
+def case_and_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_large_problem_on_time('same_hand')
---
+++ super/problem/seem_world_about_eye/world/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    think_time(str_arg)
+    print('other_way_and_large_place')
+
+def think_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('person')
