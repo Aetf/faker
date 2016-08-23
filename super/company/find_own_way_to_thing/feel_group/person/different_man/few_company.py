6604b2a77c76c82f3d04098b82d8bf44f6a188cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:39:33 -0400

    "mod: be super/place/say_thing_beneath_old_company/problem_or_week/good_thing_or_little_government.py, super/company/find_own_way_to_thing/feel_group/person/different_man/few_company.py, super/ask_own_place_about_few_company/week/woman_or_important_company/large_child/public_case_or_place.py or super/life/think_work/public_thing_or_first_part.py after own thing"

---
+++ super/company/find_own_way_to_thing/feel_group/person/different_man/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    go_number(str_arg)
+    print('try_week')
+
+def go_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('work_or_time')
---
+++ super/place/say_thing_beneath_old_company/problem_or_week/good_thing_or_little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time(str_arg):
+    do_time_about_important_man(str_arg)
+    print('fact_and_young_week')
+
+def do_time_about_important_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time('person_and_old_government')
