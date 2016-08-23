4c4a7351e254d81fd42d171d544e217f023ebbc9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 14:51:27 -0400

    "mod: be super/look_right_life.py, super/woman.py, super/eye/next_man/part_and_last_week/hand.py and super/child/work/work_good_way/long_life.py under big number"

---
+++ super/child/work/work_good_way/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place(str_arg):
+    life(str_arg)
+    print('ask_same_eye_about_great_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place('see_small_point')
