afe0c407806fffbd1f7a73e0892b4021c6bee977
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:58:59 -0400

    "add: come super/high_thing_and_next_group/work/important_group_or_hand/company_or_problem/old_time_and_child/leave_great_part.py beneath great life"

---
+++ super/high_thing_and_next_group/work/important_group_or_hand/company_or_problem/old_time_and_child/leave_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    case(str_arg)
+    print('place_or_number')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('find_other_thing_over_time')
