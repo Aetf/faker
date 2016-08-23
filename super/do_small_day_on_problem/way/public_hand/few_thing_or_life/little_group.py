be2e49ae89d090f5de2c162f47db968d7fe58eb1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:22:40 -0400

    "mv: think super/point/little_government.py and super/do_small_day_on_problem/way/public_hand/few_thing_or_life/little_group.py about first man"

---
+++ super/do_small_day_on_problem/way/public_hand/few_thing_or_life/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way(str_arg):
+    have_old_case_of_thing(str_arg)
+    print('world')
+
+def have_old_case_of_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way('first_life')
