56ddc6e0f74eed8fd3ec589d2ccf4908dbf66ee7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:38:05 -0400

    "mv: ask super/group.py by new year"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_number(str_arg):
+    old_work(str_arg)
+    print('find_part')
+
+def old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_number('good_eye')
