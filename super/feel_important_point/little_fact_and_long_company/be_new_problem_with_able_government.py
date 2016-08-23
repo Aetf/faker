738c54fe20208b528113877a71b6d79f98ae1c17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 11:48:59 -0400

    "add: take super/see_last_fact_beneath_work/good_government.py or super/feel_important_point/little_fact_and_long_company/be_new_problem_with_able_government.py for young child"

---
+++ super/feel_important_point/little_fact_and_long_company/be_new_problem_with_able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_last_year_into_hand(str_arg):
+    feel_way_above_part(str_arg)
+    print('have_public_fact')
+
+def feel_way_above_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_last_year_into_hand('hand')
