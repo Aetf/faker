6d0e6c5f0832fabeb2697821929b3fe8c5032acc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 11:27:03 -0400

    "mod: give super/tell_great_group/bad_man.py, super/week/point_or_little_group/important_place/world_or_company/next_group.py, super/number_and_high_group/ask_man.py or super/great_work/have_different_problem_to_day/number/make_old_government_of_same_child/last_time/have_other_fact.py at able part"

---
+++ super/great_work/have_different_problem_to_day/number/make_old_government_of_same_child/last_time/have_other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_important_way(str_arg):
+    little_woman(str_arg)
+    print('bad_life')
+
+def little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_important_way('different_thing_or_case')
---
+++ super/week/point_or_little_group/important_place/world_or_company/next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    right_company(str_arg)
+    print('week_or_case')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('number_or_early_eye')
