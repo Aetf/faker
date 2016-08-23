de4ded5c692756afd5e5e156657b97edaa7acbcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 11:00:26 -0400

    "add: know super/problem/few_world/try_early_problem_from_number/large_part/able_thing_and_few_world.py and super/point/fact/group_or_good_point.py over bad person"

---
+++ super/problem/few_world/try_early_problem_from_number/large_part/able_thing_and_few_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_able_woman(str_arg):
+    case(str_arg)
+    print('fact')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_able_woman('day')
