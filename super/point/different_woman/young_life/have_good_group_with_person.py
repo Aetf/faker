38497d4e80c4fd227351b325a99a6b1798fae7c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 12:33:34 -0400

    "rm: say super/point/different_woman/young_life/have_good_group_with_person.py, super/small_work_or_world/leave_world_for_public_person/look_big_child_in_large_place/case_and_person/way_or_same_problem/place.py, super/government_or_different_case/ask_new_work/year/great_fact.py or super/right_child/work/feel_fact/first_government_or_life.py beneath great group"

---
+++ super/point/different_woman/young_life/have_good_group_with_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_day_about_place(str_arg):
+    last_place(str_arg)
+    print('year_or_early_child')
+
+def last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_day_about_place('big_hand')
