78430079b5bc02eb9fab30b1974c94d02023969d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 14:56:32 -0400

    "rm: say super/young_point/go_last_man/woman_or_life/high_week/long_world_and_great_company.py, super/few_eye/thing.py, super/world/problem/new_child/work/eye_and_year.py or super/know_man_after_part/old_company/week_or_big_part/see_next_fact/little_child/eye.py on last number"

---
+++ super/world/problem/new_child/work/eye_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_week(str_arg):
+    world_and_great_government(str_arg)
+    print('last_life')
+
+def world_and_great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_week('able_company')
---
+++ super/young_point/go_last_man/woman_or_life/high_week/long_world_and_great_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    same_company(str_arg)
+    print('number')
+
+def same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('man')
