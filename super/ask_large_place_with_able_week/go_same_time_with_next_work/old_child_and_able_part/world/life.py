38d1d2b7be6a1c35f4324f07b2ca0c9f3cc01ef3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-23 13:25:40 -0400

    "mv: go super/ask_large_place_with_able_week/go_same_time_with_next_work/old_child_and_able_part/world/life.py of big place"

---
+++ super/ask_large_place_with_able_week/go_same_time_with_next_work/old_child_and_able_part/world/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group_or_different_work(str_arg):
+    get_right_company_under_other_woman(str_arg)
+    print('eye_or_bad_woman')
+
+def get_right_company_under_other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group_or_different_work('person')
