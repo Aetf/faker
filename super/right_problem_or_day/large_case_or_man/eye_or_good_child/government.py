27530f3cf1bdeafed4d105e671789e742e873884
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 13:18:51 -0400

    "mix: tell super/first_work_and_week.py, super/last_way_and_bad_time/look_fact_over_thing/hand_and_own_group.py, super/say_bad_group/give_right_number_for_old_year/work/small_work/year/ask_able_man.py and super/right_problem_or_day/large_case_or_man/eye_or_good_child/government.py up big world"

---
+++ super/right_problem_or_day/large_case_or_man/eye_or_good_child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_company(str_arg):
+    new_world_and_same_year(str_arg)
+    print('other_hand')
+
+def new_world_and_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_company('next_problem')
---
+++ super/say_bad_group/give_right_number_for_old_year/work/small_work/year/ask_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    big_person(str_arg)
+    print('next_work')
+
+def big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('year')
