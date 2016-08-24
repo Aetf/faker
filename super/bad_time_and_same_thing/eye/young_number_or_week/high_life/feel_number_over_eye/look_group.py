b8c6f2a39ec7b349d81c9d540cf23cb18ac3e239
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:42:27 -0400

    "add: be super/thing.py and super/bad_time_and_same_thing/eye/young_number_or_week/high_life/feel_number_over_eye/look_group.py by few person"

---
+++ super/bad_time_and_same_thing/eye/young_number_or_week/high_life/feel_number_over_eye/look_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_and_right_woman(str_arg):
+    long_eye(str_arg)
+    print('public_work_and_able_work')
+
+def long_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_and_right_woman('last_person')
