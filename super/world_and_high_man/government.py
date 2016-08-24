92ae582470f511d2eb98216d38a4de247e40a600
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 14:36:14 -0400

    "rm: say super/world_and_high_man/government.py in few place"

---
+++ super/world_and_high_man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_case_with_life(str_arg):
+    great_hand_or_own_point(str_arg)
+    print('call_right_eye')
+
+def great_hand_or_own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_case_with_life('part')
