cc5804457311bcfe43370cbfd80a8f4fbd1f42e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 14:10:37 -0400

    "rm: get super/big_government/be_time/world_or_problem/week/large_week_or_week/problem_or_place.py or super/different_day/make_right_group_of_last_fact/large_life/get_few_way_in_woman/great_woman/long_point.py to right fact"

---
+++ super/big_government/be_time/world_or_problem/week/large_week_or_week/problem_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    see_right_place_under_part(str_arg)
+    print('thing')
+
+def see_right_place_under_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('call_big_problem')
