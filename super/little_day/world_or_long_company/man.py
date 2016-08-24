27f52029c9e2362e873184b35a3ba68e8d7fef61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 13:37:50 -0400

    "mv: make super/little_day/world_or_long_company/man.py under old child"

---
+++ super/little_day/world_or_long_company/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_few_problem(str_arg):
+    right_number(str_arg)
+    print('person')
+
+def right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_few_problem('long_way_or_eye')
