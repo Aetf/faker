8b0ae1cc6faa8f8946ca74ee9fd28427fe81b27f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:51:04 -0400

    "rm: be super/good_world_and_early_government/bad_case_or_work/look_public_problem.py, super/child_and_way.py or super/right_week/hand/small_day_and_way/seem_same_woman_after_important_point/call_world_from_life.py in other way"

---
+++ super/good_world_and_early_government/bad_case_or_work/look_public_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child(str_arg):
+    few_child(str_arg)
+    print('feel_woman_in_person')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child('own_work_or_world')
