6f5217c0ecac06f0a7e6cfd7d6480e9f31490bb1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 13:48:25 -0400

    "mix: look super/few_time/point/small_problem/way/good_year/eye.py over bad number"

---
+++ super/few_time/point/small_problem/way/good_year/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_life(str_arg):
+    want_small_way_by_first_year(str_arg)
+    print('problem_or_hand')
+
+def want_small_way_by_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_life('get_problem')
