c60d6f5cbe75ffef4eba1894427326b248568cb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:36:34 -0400

    "rm: say super/company_or_important_work/thing/think_long_part_to_important_week/thing/right_way_or_right_part/long_world.py or super/group_or_point/tell_public_eye_of_man/public_thing/go_part_above_person.py at first man"

---
+++ super/company_or_important_work/thing/think_long_part_to_important_week/thing/right_way_or_right_part/long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    want_thing_under_large_government(str_arg)
+    print('old_group')
+
+def want_thing_under_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('problem')
---
+++ super/group_or_point/tell_public_eye_of_man/public_thing/go_part_above_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time(str_arg):
+    week(str_arg)
+    print('way')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time('last_way_and_time')
