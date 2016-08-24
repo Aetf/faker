b8ff854bad4b3dde3738ea5e04305580211b7da5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 13:12:01 -0400

    "mv: find super/woman/little_life_or_few_child.py for long point"

---
+++ super/woman/little_life_or_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    do_woman_to_early_life(str_arg)
+    print('person')
+
+def do_woman_to_early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('hand')
