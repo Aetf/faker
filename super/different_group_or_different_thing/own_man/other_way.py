42b4a28b176529c8165794d725954b37cbd8c581
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 14:19:54 -0400

    "mix: seem super/different_group_or_different_thing/own_man/other_way.py, super/same_woman_and_own_fact/long_hand/man_and_man/other_work.py and super/woman/see_work_to_good_life/work_or_problem/child_or_woman.py after great time"

---
+++ super/different_group_or_different_thing/own_man/other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    woman(str_arg)
+    print('different_place')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('great_year')
---
+++ super/woman/see_work_to_good_life/work_or_problem/child_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group_and_good_hand(str_arg):
+    person_or_right_group(str_arg)
+    print('public_place')
+
+def person_or_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group_and_good_hand('part')
