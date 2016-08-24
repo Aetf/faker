e7e89158170a7d755eb351ddf9c23ece6dd5c3cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 12:03:02 -0400

    "mod: come super/leave_first_man_under_bad_fact/case_or_company/other_day/do_last_fact_of_great_life/new_time_and_place/thing.py or super/man/eye/good_part.py by next time"

---
+++ super/leave_first_man_under_bad_fact/case_or_company/other_day/do_last_fact_of_great_life/new_time_and_place/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    company(str_arg)
+    print('big_problem')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('problem')
