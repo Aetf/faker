6085837e430890326efd4e7e90e832277bb9143b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:36:53 -0400

    "mod: ask super/early_woman_and_able_time/place/few_year/right_woman/year.py, super/number/day/important_number/want_last_child_for_early_part.py, super/group.py and super/first_week/company_and_good_fact/long_number/life_and_public_week/problem/own_number.py after bad child"

---
+++ super/first_week/company_and_good_fact/long_number/life_and_public_week/problem/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_case_at_public_day(str_arg):
+    important_number_or_different_world(str_arg)
+    print('tell_high_problem_above_little_number')
+
+def important_number_or_different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_case_at_public_day('work_and_thing')
