abd1f6772dc1df75d48fa1c34b0dfdc750c8d36f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:50:46 -0400

    "add: get super/call_week_over_government.py and super/large_man.py for last case"

---
+++ super/call_week_over_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    small_man_or_bad_child(str_arg)
+    print('point_or_different_child')
+
+def small_man_or_bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('new_way')
