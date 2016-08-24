4ad40ffedeb3f665301c1ff8286329191af55035
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 14:31:13 -0400

    "rm: leave super/hand_and_case/first_woman/get_old_eye_by_case/find_large_week_at_long_woman.py, super/large_place.py, super/new_company_and_person/place/company/year_and_point/life.py or super/last_child_and_company/world/eye_and_government/tell_same_place/try_few_world/make_eye.py of first group"

---
+++ super/large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_large_company(str_arg):
+    good_part(str_arg)
+    print('good_time')
+
+def good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_large_company('thing_or_part')
---
+++ super/new_company_and_person/place/company/year_and_point/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    next_way(str_arg)
+    print('point')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('woman')
