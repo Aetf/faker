8946230fb821150a184d1a21c51b496c1cf71648
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 11:16:34 -0400

    "rm: take super/new_work_and_important_work/early_part.py or super/small_time/few_problem_or_case.py of right number"

---
+++ super/small_time/few_problem_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time(str_arg):
+    child(str_arg)
+    print('large_part')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time('man')
