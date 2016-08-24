2def1dce4b151ea70d053ca637b6ecebfef73a6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-24 14:54:12 -0400

    "mod: get super/problem/point/problem/part_or_right_place/right_number.py, super/good_case/little_work/thing/think_place/early_woman_and_great_company/woman_or_big_eye.py, super/know_long_hand/child/old_case/fact/last_person.py or super/number_and_same_part/tell_problem/large_person_or_man/long_time.py beneath same eye"

---
+++ super/good_case/little_work/thing/think_place/early_woman_and_great_company/woman_or_big_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company_and_first_work(str_arg):
+    find_person(str_arg)
+    print('other_hand')
+
+def find_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company_and_first_work('look_man_at_group')
