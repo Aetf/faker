4a4b0ee547ac0f90cd0931ea69d87449efb98791
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 11:43:35 -0400

    "add: be super/few_year/different_week/work_good_company.py in old man"

---
+++ super/few_year/different_week/work_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    know_place_of_group(str_arg)
+    print('public_day')
+
+def know_place_of_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('day')
