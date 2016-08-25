89c117ddaa45f5886653222d59d666432d46e541
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 11:35:15 -0400

    "mod: think super/man/able_woman_or_person/new_year_and_important_year.py from next world"

---
+++ super/man/able_woman_or_person/new_year_and_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_child(str_arg):
+    seem_last_case(str_arg)
+    print('make_great_person_above_part')
+
+def seem_last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_child('day')
