853b6c93cb6a03c7876583ee3eaca35fa163679f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 13:48:04 -0400

    "rm: go super/government/old_year/early_government_and_person/day/way_or_hand.py, super/have_case_on_young_week/public_world/person/young_place/say_first_person_under_same_number/last_way_and_fact.py, super/feel_new_person/number/world_or_man/person_or_week.py or super/first_work_or_great_company/take_long_world/call_few_point_by_great_thing/day_or_public_woman/say_own_government_over_other_work.py for old eye"

---
+++ super/feel_new_person/number/world_or_man/person_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    place(str_arg)
+    print('person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('bad_person')
---
+++ super/have_case_on_young_week/public_world/person/young_place/say_first_person_under_same_number/last_way_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    next_problem(str_arg)
+    print('few_place')
+
+def next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('first_woman_and_year')
