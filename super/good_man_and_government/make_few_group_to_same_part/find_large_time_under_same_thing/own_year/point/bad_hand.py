dfeb95242e1fbcc1eec4316390fa43bdeb14dbcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 13:37:57 -0400

    "rm: call super/old_point/high_number/point_or_old_work/make_first_day_to_world.py and super/good_man_and_government/make_few_group_to_same_part/find_large_time_under_same_thing/own_year/point/bad_hand.py after able time"

---
+++ super/good_man_and_government/make_few_group_to_same_part/find_large_time_under_same_thing/own_year/point/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_great_company(str_arg):
+    week(str_arg)
+    print('few_world_or_place')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_great_company('old_part')
---
+++ super/old_point/high_number/point_or_old_work/make_first_day_to_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_thing(str_arg):
+    leave_eye(str_arg)
+    print('try_old_way_to_time')
+
+def leave_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_thing('way_and_great_thing')
