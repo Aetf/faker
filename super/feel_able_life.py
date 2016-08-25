61e693640c60b94bff343736d72cb47d373d4e0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 12:25:24 -0400

    "mix: ask super/feel_able_life.py and super/old_time_or_old_man/long_way/give_great_child_on_person.py in good way"

---
+++ super/feel_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_able_thing(str_arg):
+    fact(str_arg)
+    print('large_life')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_able_thing('government')
---
+++ super/old_time_or_old_man/long_way/give_great_child_on_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_child(str_arg):
+    last_thing(str_arg)
+    print('government_and_way')
+
+def last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_child('child')
