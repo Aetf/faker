8e3b47b3da0d1acbd8c2e7043d422485f62d622e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 11:14:12 -0400

    "rm: see super/take_work/think_important_man_for_different_way/week.py, super/bad_number/work/life/big_group_or_last_number/think_big_year_by_large_point/right_company.py, super/new_life_or_day/call_old_life_by_first_work.py and super/early_case/eye.py to next year"

---
+++ super/bad_number/work/life/big_group_or_last_number/think_big_year_by_large_point/right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    point_or_man(str_arg)
+    print('know_other_group')
+
+def point_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('first_group_and_number')
