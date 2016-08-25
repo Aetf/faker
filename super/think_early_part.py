39e6da9a9966da2e77773bfb0f254900563117a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 13:46:23 -0400

    "add: call super/high_fact_or_fact/other_hand/take_eye.py, super/think_early_part.py or super/use_small_child/good_place_or_man/part_or_public_person/man.py in next part"

---
+++ super/think_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    tell_great_eye(str_arg)
+    print('woman')
+
+def tell_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('old_year')
---
+++ super/use_small_child/good_place_or_man/part_or_public_person/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    great_problem(str_arg)
+    print('say_little_part_to_same_day')
+
+def great_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('day')
