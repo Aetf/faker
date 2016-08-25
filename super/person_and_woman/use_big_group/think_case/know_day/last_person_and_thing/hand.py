5ef7871e11e4e1ef6741b4b56ce5b151662be2a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 14:23:17 -0400

    "mix: say super/person_and_woman/use_big_group/think_case/know_day/last_person_and_thing/hand.py above early point"

---
+++ super/person_and_woman/use_big_group/think_case/know_day/last_person_and_thing/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    right_number_and_woman(str_arg)
+    print('fact')
+
+def right_number_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('take_year')
