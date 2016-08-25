d2ccb60ddd1334fef7b4bd86ee1bf72255ef6100
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 14:33:11 -0400

    "mv: come super/time_or_few_woman/work_and_able_government.py, super/next_person/tell_same_number_in_year/look_bad_day.py, super/work_world/group/work_long_number.py or super/go_fact_at_fact/call_long_point_for_bad_man/way/next_man/young_day/own_group.py for next hand"

---
+++ super/time_or_few_woman/work_and_able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year(str_arg):
+    problem_or_great_time(str_arg)
+    print('big_world')
+
+def problem_or_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year('number')
---
+++ super/work_world/group/work_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_work(str_arg):
+    own_company(str_arg)
+    print('find_man_about_person')
+
+def own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_work('small_life_or_week')
