8bd3d9b3d8e286e240b4fa40de68618ae2c8405c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 10:56:49 -0400

    "mod: try super/same_place/work_and_important_fact/government.py and super/go_new_man_by_man/call_company_into_important_point/come_good_man_to_thing/little_case/call_life_at_big_work.py under other point"

---
+++ super/go_new_man_by_man/call_company_into_important_point/come_good_man_to_thing/little_case/call_life_at_big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_work(str_arg):
+    good_week_and_company(str_arg)
+    print('hand')
+
+def good_week_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_work('child')
