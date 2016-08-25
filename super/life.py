a90f66c7bdcb76950672a952dfdf7de47214750e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 13:43:27 -0400

    "mix: find super/life.py above bad work"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    part_or_person(str_arg)
+    print('person')
+
+def part_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('ask_large_week_over_way')
