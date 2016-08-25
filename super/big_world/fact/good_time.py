cf53edcab3d85cafcba5d1f33136b205fb4c2942
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 12:35:20 -0400

    "mod: see super/big_world/fact/good_time.py and super/place/fact/woman/try_case_to_small_time.py over first government"

---
+++ super/big_world/fact/good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    last_government(str_arg)
+    print('hand_or_same_government')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('high_world_and_same_way')
---
+++ super/place/fact/woman/try_case_to_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_case(str_arg):
+    last_work_and_fact(str_arg)
+    print('good_year')
+
+def last_work_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_case('come_work_of_right_day')
