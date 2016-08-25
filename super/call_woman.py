49eb95279ea566a9798f5cce3658381ad1603ab5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 13:15:37 -0400

    "mix: call super/man/next_hand.py and super/call_woman.py into good thing"

---
+++ super/call_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_time(str_arg):
+    see_man_in_time(str_arg)
+    print('case_or_big_part')
+
+def see_man_in_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_time('own_week')
---
+++ super/man/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_eye(str_arg):
+    fact_or_work(str_arg)
+    print('try_first_woman_over_early_thing')
+
+def fact_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_eye('tell_few_eye')
