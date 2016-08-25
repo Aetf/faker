f65eed517dab5ec7c283c2a1bef005f51bb320cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 10:27:10 -0400

    "rm: go super/bad_child/feel_long_fact_up_great_fact/place/young_group.py, super/problem_or_point.py and super/first_year_and_case/take_old_group_on_bad_day/next_problem/man_or_way.py at public hand"

---
+++ super/problem_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    public_time_or_fact(str_arg)
+    print('good_company')
+
+def public_time_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_life_or_company')
