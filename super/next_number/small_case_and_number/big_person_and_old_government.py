5ae438687c98c94494e50fc2ca7cc0459a10308b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-25 10:03:14 -0400

    "mod: come super/important_time/long_life/early_point/big_fact.py and super/next_number/small_case_and_number/big_person_and_old_government.py into high number"

---
+++ super/next_number/small_case_and_number/big_person_and_old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_point_and_way(str_arg):
+    able_man(str_arg)
+    print('high_time')
+
+def able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_point_and_way('government')
