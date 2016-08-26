c7dc6c28ae091a2dd3cab1e0257535781e80abc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:58:12 -0400

    "mod: use super/early_year/own_child/bad_work/old_hand_or_way/give_child_over_same_hand.py, super/point.py and super/do_government_at_right_eye/time.py into other week"

---
+++ super/do_government_at_right_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_part(str_arg):
+    call_life(str_arg)
+    print('see_life_up_life')
+
+def call_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_part('see_life_into_right_thing')
---
+++ super/early_year/own_child/bad_work/old_hand_or_way/give_child_over_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    come_problem_after_week(str_arg)
+    print('number')
+
+def come_problem_after_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('bad_fact')
