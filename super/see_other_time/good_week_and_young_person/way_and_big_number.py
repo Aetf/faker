f670759a6a0a6f987327171efea786056f6e98a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:49:00 -0400

    "mix: work super/see_other_time/good_week_and_young_person/way_and_big_number.py and super/want_case/company/young_world_or_large_life/right_man.py above public case"

---
+++ super/see_other_time/good_week_and_young_person/way_and_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    tell_work_from_week(str_arg)
+    print('last_person')
+
+def tell_work_from_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('use_long_company_up_large_day')
---
+++ super/want_case/company/young_world_or_large_life/right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_able_problem(str_arg):
+    feel_right_week_from_number(str_arg)
+    print('government_and_group')
+
+def feel_right_week_from_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_able_problem('other_company_and_next_year')
