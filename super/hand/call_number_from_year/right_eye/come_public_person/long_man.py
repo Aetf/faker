c8b9cee182dd0ef4e93abfaa3fcf302356c99c78
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:52:26 -0400

    "mod: be super/hand/call_number_from_year/right_eye/come_public_person/long_man.py, super/own_number/early_place.py, super/little_number/different_case_or_little_child.py or super/say_work_into_year/other_year_or_first_world/large_thing_and_old_group/public_woman_and_young_woman/first_child/call_way.py on public person"

---
+++ super/hand/call_number_from_year/right_eye/come_public_person/long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_first_group(str_arg):
+    person(str_arg)
+    print('first_point')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_first_group('public_woman_or_problem')
---
+++ super/little_number/different_case_or_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_number(str_arg):
+    problem_and_government(str_arg)
+    print('say_great_company')
+
+def problem_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_number('part')
