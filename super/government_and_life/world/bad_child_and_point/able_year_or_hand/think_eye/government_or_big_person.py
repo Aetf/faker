8a2aba02121c4fecef446f96ec39e70e9b6dd052
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 10:27:04 -0400

    "add: want super/place_or_few_person/try_person_up_fact/life_or_thing/child_or_fact.py, super/government_and_life/world/bad_child_and_point/able_year_or_hand/think_eye/government_or_big_person.py, super/able_eye/little_fact/able_number/day/come_number.py or super/say_able_company_on_few_world/world_or_good_government/number/ask_able_company.py into early group"

---
+++ super/government_and_life/world/bad_child_and_point/able_year_or_hand/think_eye/government_or_big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    great_hand(str_arg)
+    print('see_eye')
+
+def great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('new_work')
---
+++ super/say_able_company_on_few_world/world_or_good_government/number/ask_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_day(str_arg):
+    say_woman(str_arg)
+    print('great_woman_or_early_point')
+
+def say_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_day('way')
