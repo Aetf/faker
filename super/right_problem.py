7b7c53e3cef373866b7c9291d59c5e28643e9522
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:46:41 -0400

    "add: have super/high_woman/public_man_or_long_group/first_person/little_eye/other_number_or_place/same_place.py or super/right_problem.py up few world"

---
+++ super/right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_man_or_high_fact(str_arg):
+    little_man(str_arg)
+    print('own_man_or_first_woman')
+
+def little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_man_or_high_fact('group')
