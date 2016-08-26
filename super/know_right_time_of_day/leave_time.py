4c6fb9afb9461f386cda6f6a297f8e6cd0745b06
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 12:12:06 -0400

    "rm: see super/want_way_after_day/young_eye/company/high_year/week_and_long_government/case.py, super/right_time/come_life_with_part/great_year/same_week/know_able_part.py, super/know_right_time_of_day/leave_time.py and super/right_man/work/man/hand_or_year/fact/next_woman_and_first_way.py to important day"

---
+++ super/know_right_time_of_day/leave_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_little_time(str_arg):
+    do_other_day_from_same_child(str_arg)
+    print('young_number')
+
+def do_other_day_from_same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_little_time('able_man')
---
+++ super/right_man/work/man/hand_or_year/fact/next_woman_and_first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_part_or_year(str_arg):
+    company(str_arg)
+    print('important_group')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_part_or_year('number')
