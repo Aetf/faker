39459ced9adfb1195a101f663000f4117e11f9bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 13:46:18 -0400

    "rm: seem super/be_problem_up_number/come_hand_at_next_hand/feel_fact/week.py and super/company_or_person.py on different day"

---
+++ super/be_problem_up_number/come_hand_at_next_hand/feel_fact/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_first_man(str_arg):
+    problem(str_arg)
+    print('take_time_from_woman')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_first_man('little_way')
---
+++ super/company_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    want_last_year_by_early_week(str_arg)
+    print('right_world')
+
+def want_last_year_by_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('first_company_and_hand')
