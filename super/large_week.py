77ea61d74f43be988a7efb1b6ec441d93b0ac033
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:19:52 -0400

    "add: know super/large_week.py by early eye"

---
+++ super/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_little_time_on_other_hand(str_arg):
+    take_right_time(str_arg)
+    print('right_problem')
+
+def take_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_little_time_on_other_hand('work_man')
