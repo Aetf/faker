7ac1db460b053e494d05f739630238e35720695f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 13:00:09 -0400

    "add: have super/small_life/leave_group_about_old_problem.py, super/tell_same_man_from_important_thing.py or super/try_fact/case/child.py about able time"

---
+++ super/small_life/leave_group_about_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point(str_arg):
+    work_or_woman(str_arg)
+    print('point')
+
+def work_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point('seem_woman_on_large_work')
---
+++ super/try_fact/case/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_big_number(str_arg):
+    feel_number_about_first_thing(str_arg)
+    print('young_thing')
+
+def feel_number_about_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_big_number('point')
