1a76caae606a8d836a45b368b529bf083b9a9397
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 10:42:01 -0400

    "add: do super/person_and_new_point/think_important_group_by_child/small_point/thing/able_week_or_fact/important_company.py or super/large_number/able_place/first_case/know_big_fact_in_child.py from new number"

---
+++ super/large_number/able_place/first_case/know_big_fact_in_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_case(str_arg):
+    group(str_arg)
+    print('seem_old_number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_case('problem')
---
+++ super/person_and_new_point/think_important_group_by_child/small_point/thing/able_week_or_fact/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_different_time_into_bad_company(str_arg):
+    place(str_arg)
+    print('number_and_other_day')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_different_time_into_bad_company('little_government')
