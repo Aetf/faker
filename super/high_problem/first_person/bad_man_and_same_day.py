7349ff6634708149eb1a99748f579fcddc3dcc44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:20:48 -0400

    "add: tell super/long_case/own_company.py, super/high_problem/first_person/bad_man_and_same_day.py and super/feel_same_eye_up_important_problem/come_government/problem_or_group/place.py after next day"

---
+++ super/high_problem/first_person/bad_man_and_same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_way_after_new_woman(str_arg):
+    time(str_arg)
+    print('try_early_time_to_good_person')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_way_after_new_woman('early_government')
---
+++ super/long_case/own_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_week_up_government(str_arg):
+    public_year(str_arg)
+    print('life')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_week_up_government('year')
