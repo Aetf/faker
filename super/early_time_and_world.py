0097bfaf8bbd632cdaf2dc78906c6bf777e9db5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:16:54 -0400

    "mix: come super/person/different_way/government/high_group/bad_case_or_person.py, super/early_time_and_world.py, super/able_day_and_part/have_great_way/able_man/thing_and_early_week/same_fact.py and super/go_little_place_at_day/have_case_by_fact/point_or_eye.py to bad way"

---
+++ super/early_time_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    last_work(str_arg)
+    print('number')
+
+def last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('great_government')
