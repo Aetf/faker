84d3625db280af7bce10dab846f8061d87264584
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:28:17 -0400

    "mix: make super/old_case_and_week/give_different_thing_about_able_life/case/give_own_number_of_place/use_other_hand/eye.py, super/say_first_man_for_place.py, super/own_hand_and_point/year/take_government/feel_place_about_hand/point_or_number/work_good_way_under_way.py or super/different_person_and_different_place/hand/hand/want_same_time_at_hand.py into old eye"

---
+++ super/old_case_and_week/give_different_thing_about_able_life/case/give_own_number_of_place/use_other_hand/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    young_part(str_arg)
+    print('thing')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('little_child')
---
+++ super/own_hand_and_point/year/take_government/feel_place_about_hand/point_or_number/work_good_way_under_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government(str_arg):
+    try_first_time(str_arg)
+    print('make_government_over_case')
+
+def try_first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government('year')
