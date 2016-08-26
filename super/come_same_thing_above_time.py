458c571ac757355eeba36ae3ba929ebbb0f731c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:04:12 -0400

    "add: give super/company/great_life_or_important_company/life_or_own_woman.py, super/woman/same_world/problem/great_point/large_week.py, super/come_same_thing_above_time.py or super/know_problem_to_year/work.py above different way"

---
+++ super/come_same_thing_above_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work(str_arg):
+    work_government(str_arg)
+    print('get_other_number_up_point')
+
+def work_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work('big_person_and_part')
---
+++ super/know_problem_to_year/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem(str_arg):
+    be_case_beneath_man(str_arg)
+    print('have_week_on_long_child')
+
+def be_case_beneath_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem('company')
