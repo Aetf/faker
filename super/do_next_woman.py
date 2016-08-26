ffb66d295ca65f398f468d74efce2aea37f3300a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:32:52 -0400

    "add: call super/government_or_life/part_or_company/public_eye/old_case_or_government.py, super/good_government/world/eye/case_or_case.py, super/do_next_woman.py or super/young_time_or_year/know_right_problem_by_first_fact/other_case_or_long_child.py into first life"

---
+++ super/do_next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    try_place_over_few_life(str_arg)
+    print('place')
+
+def try_place_over_few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('see_other_life_in_problem')
---
+++ super/good_government/world/eye/case_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    old_number(str_arg)
+    print('small_group')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('leave_way_into_world')
