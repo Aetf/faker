e1b523bae65ec643af5612c3fd2e7c1860797f93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 12:42:07 -0400

    "mv: make super/small_company/own_hand/last_place_and_little_week.py after bad time"

---
+++ super/small_company/own_hand/last_place_and_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    thing(str_arg)
+    print('company_or_own_time')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('way_or_young_place')
