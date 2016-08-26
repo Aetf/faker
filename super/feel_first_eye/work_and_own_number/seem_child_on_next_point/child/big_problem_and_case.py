ed6ba2ade1c6e18cd271c99ddf3b6fae3239e8bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:23:22 -0400

    "rm: take super/thing_and_work/point_or_case/point/good_government_or_point/way.py, super/other_day_or_point.py, super/feel_first_eye/work_and_own_number/seem_child_on_next_point/child/big_problem_and_case.py and super/bad_group/young_place_or_world/problem_or_company/find_life.py in good world"

---
+++ super/feel_first_eye/work_and_own_number/seem_child_on_next_point/child/big_problem_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    first_part_or_same_number(str_arg)
+    print('great_eye_and_own_company')
+
+def first_part_or_same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('point_or_important_part')
---
+++ super/thing_and_work/point_or_case/point/good_government_or_point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    different_work(str_arg)
+    print('few_thing')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('person_or_eye')
