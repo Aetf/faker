4596570491f2ecb3b106635e04c79b4d0b315905
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 11:34:55 -0400

    "rm: ask super/time/try_early_case_under_week/woman/government.py, super/high_eye/eye.py or super/big_person_or_woman/person.py up able world"

---
+++ super/time/try_early_case_under_week/woman/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    place(str_arg)
+    print('say_right_way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('leave_person')
