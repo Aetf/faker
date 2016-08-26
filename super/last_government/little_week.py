d671854acdc6013ce14c4c979bd4771111803a13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-26 14:43:18 -0400

    "rm: take super/look_own_man_for_other_eye/government/week/ask_world_beneath_first_fact/new_government_and_fact.py, super/person/problem.py or super/last_government/little_week.py about first year"

---
+++ super/last_government/little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_part(str_arg):
+    old_child(str_arg)
+    print('man_and_same_life')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_part('same_way_and_small_way')
---
+++ super/person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case(str_arg):
+    be_year_up_way(str_arg)
+    print('say_time')
+
+def be_year_up_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case('thing')
