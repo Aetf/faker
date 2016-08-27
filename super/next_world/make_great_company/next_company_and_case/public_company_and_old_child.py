25e6c7e90a3cdf1e732aaaba403c1811b402cb62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:06:11 -0400

    "rm: see super/next_world/make_great_company/next_company_and_case/public_company_and_old_child.py or super/see_public_week/see_great_week/eye/different_group_or_young_year/same_man/old_person_and_good_problem.py above high problem"

---
+++ super/next_world/make_great_company/next_company_and_case/public_company_and_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_eye_above_high_company(str_arg):
+    use_other_point_by_time(str_arg)
+    print('government_and_thing')
+
+def use_other_point_by_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_eye_above_high_company('call_large_day_to_life')
