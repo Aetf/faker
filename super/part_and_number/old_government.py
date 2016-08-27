6417217a6d2f47883e152931f3aabe0ce33fde1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 13:28:01 -0400

    "mix: make super/woman/get_year_beneath_time/week.py and super/part_and_number/old_government.py to other case"

---
+++ super/part_and_number/old_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    bad_person(str_arg)
+    print('little_part_or_fact')
+
+def bad_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('last_person_or_woman')
