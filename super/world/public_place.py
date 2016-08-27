d652d2d2ce541847ca25f0e087545bdac2020c5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 12:41:23 -0400

    "rm: be super/world/public_place.py or super/problem.py after great woman"

---
+++ super/world/public_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_bad_year(str_arg):
+    find_next_life(str_arg)
+    print('problem')
+
+def find_next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_bad_year('problem')
