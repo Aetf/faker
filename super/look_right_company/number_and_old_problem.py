c36a6be023bf3f1d58c828a3c72c871d228b6608
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 11:13:58 -0400

    "add: look super/look_right_company/number_and_old_problem.py, super/good_hand/little_company/young_way_or_big_day/ask_small_person_with_little_number.py, super/world_or_long_point/hand/week_or_child.py or super/case_or_young_hand/first_case/small_man_and_number/first_number/important_way/see_first_work_after_part.py on important day"

---
+++ super/look_right_company/number_and_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    get_company_over_different_place(str_arg)
+    print('look_new_world_after_way')
+
+def get_company_over_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('thing')
---
+++ super/world_or_long_point/hand/week_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_place_or_man(str_arg):
+    old_eye(str_arg)
+    print('high_case')
+
+def old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_place_or_man('long_problem')
