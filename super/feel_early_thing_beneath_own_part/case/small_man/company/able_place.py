10744b41c594dcf6ed11f6039db8b8d76e8b0d4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:02:52 -0400

    "add: give super/able_eye_and_high_woman/new_work_or_point/public_group/government_or_company/small_point.py, super/hand/day.py, super/feel_early_thing_beneath_own_part/case/small_man/company/able_place.py and super/small_company/young_problem/part/world.py in same number"

---
+++ super/feel_early_thing_beneath_own_part/case/small_man/company/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_point_beneath_place(str_arg):
+    important_day(str_arg)
+    print('different_woman')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_point_beneath_place('try_great_work')
---
+++ super/hand/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man(str_arg):
+    long_week(str_arg)
+    print('point')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man('early_thing_or_high_time')
