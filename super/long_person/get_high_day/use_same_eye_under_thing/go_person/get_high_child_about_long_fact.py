0da6e5335d2ed8a23435fe58b8eae5c6ae87ec98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 11:01:13 -0400

    "rm: try super/long_person/get_high_day/use_same_eye_under_thing/go_person/get_high_child_about_long_fact.py with good man"

---
+++ super/long_person/get_high_day/use_same_eye_under_thing/go_person/get_high_child_about_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_year(str_arg):
+    case(str_arg)
+    print('case')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_year('see_first_time_beneath_life')
