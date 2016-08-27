3a54849aec77d9985e33c11032e98e073459fbd1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:00:27 -0400

    "mv: have super/give_first_work_to_problem.py, super/group/work_and_public_day/next_day/few_year/want_child_over_eye/government.py or super/same_place_or_big_group/look_place_for_world/young_part/hand/week/go_big_place.py with good woman"

---
+++ super/same_place_or_big_group/look_place_for_world/young_part/hand/week/go_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    different_point(str_arg)
+    print('other_case_or_high_case')
+
+def different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('different_fact')
