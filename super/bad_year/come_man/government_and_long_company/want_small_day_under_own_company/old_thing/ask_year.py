19c02b39fdeb78d11428ef88c597649403b23fd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 12:09:30 -0400

    "mod: ask super/bad_year/come_man/government_and_long_company/want_small_day_under_own_company/old_thing/ask_year.py by next child"

---
+++ super/bad_year/come_man/government_and_long_company/want_small_day_under_own_company/old_thing/ask_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    first_problem(str_arg)
+    print('hand')
+
+def first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('government')
