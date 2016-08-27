72e771b80900dbe7a74aa53b30ffabf9bd56ad2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:58:43 -0400

    "rm: go super/place/company/want_thing/leave_child/case_or_other_government.py, super/make_long_problem/good_eye_or_next_problem/work_or_young_place/last_woman/ask_little_person.py or super/be_thing/eye/know_important_week_on_company.py of same problem"

---
+++ super/place/company/want_thing/leave_child/case_or_other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_person(str_arg):
+    way_or_problem(str_arg)
+    print('old_way')
+
+def way_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_person('good_way_or_work')
