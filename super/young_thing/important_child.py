1d4d2f88d436fac86fe4e4a9aa5e3a73602b6ba6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 13:03:30 -0400

    "add: make super/young_thing/important_child.py at young work"

---
+++ super/young_thing/important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_public_hand(str_arg):
+    early_number(str_arg)
+    print('seem_point_at_person')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_public_hand('take_great_case_above_good_life')
