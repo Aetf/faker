77a11b48d697d7b2a382472282a2be9d7204e315
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 11:07:07 -0400

    "mod: find super/big_child/thing.py in last company"

---
+++ super/big_child/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case_or_part(str_arg):
+    important_woman(str_arg)
+    print('person')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case_or_part('world')
