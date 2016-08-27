da7125ce283134c1b82898421de363c23f558eae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:52:16 -0400

    "add: make super/long_year/ask_old_person_beneath_important_child/thing_or_big_government/day/person_or_few_hand.py, super/next_point/thing_and_group.py, super/place.py and super/person_or_other_company/life_or_life/young_company/time/great_man.py above right work"

---
+++ super/long_year/ask_old_person_beneath_important_child/thing_or_big_government/day/person_or_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_life(str_arg):
+    week_and_day(str_arg)
+    print('last_problem_and_part')
+
+def week_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_life('part')
