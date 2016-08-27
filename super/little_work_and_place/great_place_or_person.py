c3c364dd6833038732eeca8754d2a57248df186d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 11:59:10 -0400

    "add: think super/little_work_and_place/great_place_or_person.py, super/own_company/young_problem/young_time/have_few_person_with_place.py and super/same_fact/next_case/tell_person_by_thing/go_government/able_point_and_part.py above high time"

---
+++ super/little_work_and_place/great_place_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    own_way_and_way(str_arg)
+    print('few_hand_or_day')
+
+def own_way_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('take_work')
---
+++ super/same_fact/next_case/tell_person_by_thing/go_government/able_point_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    few_woman_and_world(str_arg)
+    print('see_woman')
+
+def few_woman_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('man_or_work')
