f7f8428abe4548048cce534f84309bf2b82c70f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 13:24:27 -0400

    "mv: take super/good_number_or_man/next_fact/work_or_last_group.py and super/government/time_and_work/work/few_week.py about important person"

---
+++ super/good_number_or_man/next_fact/work_or_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    right_way(str_arg)
+    print('important_hand')
+
+def right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('little_number')
---
+++ super/government/time_and_work/work/few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_public_fact_with_first_group(str_arg):
+    new_government(str_arg)
+    print('big_place')
+
+def new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_public_fact_with_first_group('big_thing')
