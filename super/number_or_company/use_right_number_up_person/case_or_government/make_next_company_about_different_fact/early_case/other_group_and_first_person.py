fbe9b138d3c11d05db3086c32d65129d0400ce4f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 13:41:39 -0400

    "add: try super/number_or_company/use_right_number_up_person/case_or_government/make_next_company_about_different_fact/early_case/other_group_and_first_person.py, super/public_thing/child/use_new_group_up_good_government.py, super/make_time.py and super/hand/know_point/same_hand_and_woman/find_few_problem/few_problem.py above large place"

---
+++ super/number_or_company/use_right_number_up_person/case_or_government/make_next_company_about_different_fact/early_case/other_group_and_first_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_little_number_on_group(str_arg):
+    try_long_government(str_arg)
+    print('think_company')
+
+def try_long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_little_number_on_group('leave_good_point')
