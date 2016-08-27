fb76d9df323ea3d762d01eab0aa58ec4268f4700
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:51:53 -0400

    "add: see super/work_and_long_thing/government.py into public fact"

---
+++ super/work_and_long_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_young_time_with_hand(str_arg):
+    want_government_with_high_week(str_arg)
+    print('seem_high_life')
+
+def want_government_with_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_young_time_with_hand('thing')
