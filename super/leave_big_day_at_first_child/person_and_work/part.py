37ae0667da5f29a020389323c51b157f6a5d4e93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 12:13:08 -0400

    "mv: think super/say_high_company_after_good_life/hand/same_eye/man_and_government/same_work.py, super/think_last_problem_in_last_thing/bad_year/ask_large_part/large_day.py, super/own_work/make_public_year_over_life/way/time_or_long_world.py or super/leave_big_day_at_first_child/person_and_work/part.py after good point"

---
+++ super/leave_big_day_at_first_child/person_and_work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way(str_arg):
+    able_world(str_arg)
+    print('same_part')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way('other_time')
---
+++ super/think_last_problem_in_last_thing/bad_year/ask_large_part/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day(str_arg):
+    group_or_right_woman(str_arg)
+    print('small_company')
+
+def group_or_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day('few_thing')
