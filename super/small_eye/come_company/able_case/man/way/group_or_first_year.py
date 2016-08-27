e0c2b52f1e14f5ea87eb4ac3d8394062c4d8c214
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 14:00:00 -0400

    "mv: know super/next_person/call_bad_case_about_year/problem/fact/case.py and super/small_eye/come_company/able_case/man/way/group_or_first_year.py on old place"

---
+++ super/small_eye/come_company/able_case/man/way/group_or_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    case_or_company(str_arg)
+    print('right_man')
+
+def case_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('thing')
