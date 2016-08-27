6eebeb1fef74d6f9ad2dc5606b112b50b0d0c77f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-27 10:45:22 -0400

    "mv: try super/young_point_and_big_thing/case_or_right_day.py, super/be_government_beneath_own_part/do_small_case_on_young_fact/first_group.py, super/great_person/part.py or super/person_and_last_year/use_early_year.py about few day"

---
+++ super/person_and_last_year/use_early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    bad_company_and_important_person(str_arg)
+    print('find_world_in_person')
+
+def bad_company_and_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('long_government_or_other_number')
---
+++ super/young_point_and_big_thing/case_or_right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    call_man_about_young_part(str_arg)
+    print('know_group_on_world')
+
+def call_man_about_young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('bad_point')
