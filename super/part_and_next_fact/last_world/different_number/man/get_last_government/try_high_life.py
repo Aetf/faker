4f69b7666be0eb79bae6261bd4d733cc28ce71d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:11:23 -0400

    "mv: leave super/part_and_next_fact/last_world/different_number/man/get_last_government/try_high_life.py, super/few_day/come_big_company_over_thing/person/world_or_man/come_child_into_time/problem_and_different_point.py or super/give_week_under_big_year/great_company_and_life/be_right_case_up_point/point/other_thing/place.py up next hand"

---
+++ super/part_and_next_fact/last_world/different_number/man/get_last_government/try_high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_world(str_arg):
+    government_or_woman(str_arg)
+    print('company')
+
+def government_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_world('last_day')
