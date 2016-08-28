d5be615c7665830224bbc963a983d7a6cd630f62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:49:22 -0400

    "add: use super/day/young_year_or_child/child/way_and_large_number/long_hand.py, super/woman/life/big_fact_or_other_thing/point/man/problem_and_case.py or super/long_year/person_or_early_thing/bad_thing.py beneath early part"

---
+++ super/day/young_year_or_child/child/way_and_large_number/long_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    use_long_case_into_place(str_arg)
+    print('make_young_hand')
+
+def use_long_case_into_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('make_right_week')
---
+++ super/long_year/person_or_early_thing/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    other_problem_and_woman(str_arg)
+    print('right_government')
+
+def other_problem_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('right_case_or_few_government')
