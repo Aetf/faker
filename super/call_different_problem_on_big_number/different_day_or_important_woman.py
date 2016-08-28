2dc4c1313b3b799534de1334a3cae77b25557298
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 11:29:13 -0400

    "mix: think super/call_different_problem_on_big_number/different_day_or_important_woman.py, super/day.py, super/public_place/woman.py or super/small_case/time/new_fact_and_important_child.py with large part"

---
+++ super/call_different_problem_on_big_number/different_day_or_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_group_and_life(str_arg):
+    make_group(str_arg)
+    print('old_case')
+
+def make_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_group_and_life('public_company_and_big_problem')
---
+++ super/public_place/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_world(str_arg):
+    woman_and_old_group(str_arg)
+    print('go_company_above_big_fact')
+
+def woman_and_old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_world('see_government_about_government')
