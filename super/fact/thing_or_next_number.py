9bc2e7f8694e32c6c68282877b0813be60d4b43a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 10:08:16 -0400

    "rm: see super/early_woman_or_same_child/right_thing_and_large_way/bad_work/try_next_problem/tell_first_place_beneath_way/bad_case.py, super/group/place/time/great_problem.py, super/last_government/important_way.py and super/fact/thing_or_next_number.py about great person"

---
+++ super/fact/thing_or_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life(str_arg):
+    last_place(str_arg)
+    print('company_and_point')
+
+def last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life('last_point_or_little_time')
---
+++ super/group/place/time/great_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    week(str_arg)
+    print('company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('say_number_by_right_year')
