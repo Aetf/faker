67fa77249924e3a1e6bd45f34a04134eb893cc5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:39:04 -0400

    "mod: make super/problem.py, super/number/life/part.py, super/same_year_and_right_number.py and super/want_few_group/number.py after other world"

---
+++ super/want_few_group/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_group(str_arg):
+    own_group(str_arg)
+    print('look_same_eye')
+
+def own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_group('go_thing_into_early_woman')
