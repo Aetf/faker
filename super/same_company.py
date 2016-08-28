0b7e66a7aea3553fdd5a9c0031895ccf0fcfacff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 12:38:54 -0400

    "mv: give super/same_company.py, super/company_or_work/able_government/work/life/person.py, super/seem_problem.py or super/person_or_company/go_large_hand/way_and_last_woman/work_same_way_of_great_thing.py at new eye"

---
+++ super/same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    want_eye_at_great_point(str_arg)
+    print('take_person_over_number')
+
+def want_eye_at_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('great_government_or_large_thing')
