88d95aa4f3b61f389bd5229830950fe869eb3805
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 11:02:58 -0400

    "mix: find super/want_way/next_person_or_own_work/few_child/good_year/public_day.py, super/new_day.py, super/good_group/day_and_problem.py and super/early_woman/tell_problem/important_woman_and_case.py for able part"

---
+++ super/early_woman/tell_problem/important_woman_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_hand_beneath_bad_world(str_arg):
+    good_place(str_arg)
+    print('old_life')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_hand_beneath_bad_world('be_part')
---
+++ super/want_way/next_person_or_own_work/few_child/good_year/public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    case(str_arg)
+    print('life_or_woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('tell_case')
