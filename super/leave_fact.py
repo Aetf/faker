a473656f8d76109af216e7d786aa327234bef959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:48:08 -0400

    "mod: say super/important_time/life/public_company_or_few_number/other_hand.py, super/leave_fact.py, super/other_number.py or super/right_work/new_group/eye.py above small number"

---
+++ super/leave_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    world(str_arg)
+    print('big_life_or_next_time')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('find_right_company')
---
+++ super/right_work/new_group/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child(str_arg):
+    eye_and_place(str_arg)
+    print('able_man')
+
+def eye_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child('problem')
