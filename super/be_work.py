ef96eac57487409855e9e907c84697d732666593
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 11:55:00 -0400

    "mod: give super/look_week_after_long_way/hand_or_little_year.py and super/be_work.py from good point"

---
+++ super/be_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    tell_little_time_up_own_man(str_arg)
+    print('place')
+
+def tell_little_time_up_own_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('year')
---
+++ super/look_week_after_long_way/hand_or_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_man(str_arg):
+    work(str_arg)
+    print('tell_different_number')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_man('time')
