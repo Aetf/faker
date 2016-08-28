579dd7171bfa7f3373119285330734287105215c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:39:51 -0400

    "rm: feel super/large_company/able_case/tell_problem_by_fact/number.py, super/group.py and super/child/point.py above old place"

---
+++ super/child/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world(str_arg):
+    feel_able_group(str_arg)
+    print('place')
+
+def feel_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world('public_problem')
---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time(str_arg):
+    way(str_arg)
+    print('fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time('life_and_last_woman')
