a68780142847aeb3dfcba87df09789f0a848bc72
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:59:20 -0400

    "mix: find super/bad_thing/world.py at next company"

---
+++ super/bad_thing/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    different_thing(str_arg)
+    print('tell_large_place')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('different_week')
