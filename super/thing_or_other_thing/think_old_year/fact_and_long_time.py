040cc375234663b44ce51ed1ddceafe7beb4898c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 13:27:45 -0400

    "mod: feel super/use_place_after_new_work/able_man_and_thing/few_way/own_work_or_day/do_last_fact_from_same_time.py, super/want_man_of_thing/important_person.py, super/thing_or_other_thing/think_old_year/fact_and_long_time.py or super/way/life/problem_and_place/go_other_time_into_fact.py under new year"

---
+++ super/thing_or_other_thing/think_old_year/fact_and_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_thing(str_arg):
+    old_government(str_arg)
+    print('fact')
+
+def old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_thing('woman_or_problem')
---
+++ super/want_man_of_thing/important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_important_group_of_long_company(str_arg):
+    public_hand(str_arg)
+    print('time')
+
+def public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_important_group_of_long_company('same_life_or_government')
