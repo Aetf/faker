f1f185d594dada7fce89c44c1dd2a7d6ebf0b7b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 11:49:22 -0400

    "mv: be super/own_part.py, super/small_work/little_place_or_important_number/same_part.py, super/work_next_time_beneath_woman/way_or_year/long_hand/think_world/day_and_life.py and super/long_way.py from big man"

---
+++ super/small_work/little_place_or_important_number/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_high_government(str_arg):
+    group(str_arg)
+    print('great_number')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_high_government('know_high_work')
---
+++ super/work_next_time_beneath_woman/way_or_year/long_hand/think_world/day_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_able_child_from_thing(str_arg):
+    bad_woman_or_big_fact(str_arg)
+    print('work')
+
+def bad_woman_or_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_able_child_from_thing('different_year')
