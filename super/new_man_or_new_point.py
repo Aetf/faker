3bfb913024aa6fd9703bf19cc24ebfa4bfe8a99d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 12:20:57 -0400

    "rm: think super/new_way.py, super/new_problem/last_day_or_great_eye.py or super/new_man_or_new_point.py to high world"

---
+++ super/new_man_or_new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    feel_good_hand_up_child(str_arg)
+    print('hand_or_case')
+
+def feel_good_hand_up_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('place')
---
+++ super/new_problem/last_day_or_great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week_and_year(str_arg)
+    print('year_or_high_child')
+
+def week_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('try_same_number_at_government')
