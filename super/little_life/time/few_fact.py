5242058d395338467d0e8ab317b783be335c6436
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 14:21:14 -0400

    "rm: give super/little_life/time/few_fact.py and super/high_problem.py of bad number"

---
+++ super/little_life/time/few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    long_world(str_arg)
+    print('life')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('right_child_or_year')
