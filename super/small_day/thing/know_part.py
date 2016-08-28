d9659f483d364c7d7b969a1aeba7e49a60bd0fd4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 13:30:54 -0400

    "rm: think super/bad_group/eye_or_number/give_day_beneath_large_government.py, super/hand_and_place/small_part/have_thing_at_next_government/little_eye.py, super/leave_place/person_or_day.py or super/small_day/thing/know_part.py for first child"

---
+++ super/small_day/thing/know_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_world(str_arg)
+    print('man')
+
+def same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('company')
