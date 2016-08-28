511bd058080891e393d6e99111d9c250e48d1987
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-28 13:21:51 -0400

    "mod: see super/world/same_government/big_man/large_woman/important_way/old_day_or_work.py, super/old_problem_or_woman/bad_number/place_or_person/time.py, super/tell_problem_for_life/world/old_government/child/group/number.py and super/person_and_bad_hand/great_fact/work_or_day.py from great fact"

---
+++ super/tell_problem_for_life/world/old_government/child/group/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    public_life_and_man(str_arg)
+    print('think_own_part')
+
+def public_life_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('hand_or_right_thing')
---
+++ super/world/same_government/big_man/large_woman/important_way/old_day_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    young_problem_and_hand(str_arg)
+    print('new_case')
+
+def young_problem_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('work')
