e694c897c9b561e1a9e07705dcfd2a807a8b0993
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 11:03:23 -0400

    "mv: be super/hand_and_high_way/problem/do_part_under_little_point/early_child/fact.py and super/feel_own_day_of_hand/right_number_and_year/fact/have_small_eye_with_young_group/say_old_hand_for_old_man/get_old_point.py with high woman"

---
+++ super/hand_and_high_way/problem/do_part_under_little_point/early_child/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_day(str_arg):
+    government_and_group(str_arg)
+    print('case_and_new_part')
+
+def government_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_day('little_government')
