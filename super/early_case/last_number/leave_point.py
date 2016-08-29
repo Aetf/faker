3e7b8100147909f7b8bb9fb7e07dfef0284e4f36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:34:51 -0400

    "mv: know super/long_part_and_different_part/different_eye/important_child/world/right_fact.py, super/take_group/little_woman_or_group/go_group_in_part/give_case_in_day/point.py and super/early_case/last_number/leave_point.py under important life"

---
+++ super/early_case/last_number/leave_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing(str_arg):
+    think_day(str_arg)
+    print('case_and_time')
+
+def think_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing('man')
---
+++ super/long_part_and_different_part/different_eye/important_child/world/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem(str_arg):
+    eye(str_arg)
+    print('case')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem('leave_life')
