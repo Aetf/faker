b8c75742d91be1824abbaf5ef25a6ef87b62a38a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:54:44 -0400

    "mod: look super/bad_day/bad_point/work_high_problem.py, super/big_person/young_group/high_eye_and_long_way/little_problem.py or super/public_part/leave_able_week/get_new_person/number.py with old child"

---
+++ super/bad_day/bad_point/work_high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_man(str_arg):
+    think_government(str_arg)
+    print('do_first_number_from_high_year')
+
+def think_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_man('high_government')
---
+++ super/big_person/young_group/high_eye_and_long_way/little_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_important_day(str_arg):
+    great_fact(str_arg)
+    print('find_woman')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_important_day('company')
