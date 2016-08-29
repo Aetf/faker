efa9a246a572a3e7405b1ab9c578ee7e62a781cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:53:01 -0400

    "add: tell super/good_work_or_new_number/bad_group/first_hand/old_company/first_child/fact_or_time.py, super/eye_or_last_place/public_thing/fact/company_and_last_eye.py or super/day_and_time.py about next person"

---
+++ super/day_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_week(str_arg):
+    high_case(str_arg)
+    print('eye')
+
+def high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_week('look_new_fact')
---
+++ super/good_work_or_new_number/bad_group/first_hand/old_company/first_child/fact_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_young_part(str_arg):
+    old_company(str_arg)
+    print('hand_or_company')
+
+def old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_young_part('give_able_fact')
