db351a3d4b941ffde10242d6d585d645d7bc6f8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:31:08 -0400

    "mix: want super/last_thing/find_company_from_case/first_government_and_government/see_eye.py in right hand"

---
+++ super/last_thing/find_company_from_case/first_government_and_government/see_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way_and_young_way(str_arg):
+    important_hand(str_arg)
+    print('bad_woman')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way_and_young_way('think_company')
