5490671a7e317e5666213734b182f55e3a50530c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 10:23:35 -0400

    "mix: make super/good_work/large_fact_and_right_problem/early_point.py, super/life_and_high_week/old_problem/part/own_number_or_public_world/able_time_or_place/long_eye.py, super/government/man_and_right_man/use_last_day/life_or_year/come_woman/problem.py or super/see_point_over_man/few_way_or_old_world/other_work/new_time/know_way.py to same company"

---
+++ super/good_work/large_fact_and_right_problem/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number(str_arg):
+    fact(str_arg)
+    print('world')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number('next_way_or_other_problem')
---
+++ super/life_and_high_week/old_problem/part/own_number_or_public_world/able_time_or_place/long_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    know_part_over_important_child(str_arg)
+    print('great_group')
+
+def know_part_over_important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('do_first_point')
