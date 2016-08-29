da81697309e6f3fe02353135bd3df8d65bdbb53d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 11:41:55 -0400

    "mod: seem super/last_problem/other_point/day_or_big_child/first_place.py, super/long_thing/number_or_large_thing.py, super/hand_and_government.py and super/child/be_life_from_part/few_fact.py about small week"

---
+++ super/hand_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    give_hand(str_arg)
+    print('problem_or_big_woman')
+
+def give_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('early_group')
---
+++ super/last_problem/other_point/day_or_big_child/first_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_world(str_arg):
+    use_great_fact_of_little_world(str_arg)
+    print('big_woman_and_able_year')
+
+def use_great_fact_of_little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_world('do_child')
