d8e4f38ea11f3d06a27623be187db3990b82f672
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:44:37 -0400

    "mix: know super/bad_part/eye/woman/life_or_part.py or super/use_long_group_over_great_life/find_part.py from few woman"

---
+++ super/bad_part/eye/woman/life_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_great_life(str_arg):
+    go_same_part_beneath_person(str_arg)
+    print('get_number_into_person')
+
+def go_same_part_beneath_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_great_life('early_part')
