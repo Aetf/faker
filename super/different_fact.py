57115b277c8adaf802202c8322a14c1815b1d0b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 11:37:10 -0400

    "mv: be super/different_fact.py, super/last_part/first_group_and_eye/number.py, super/eye_and_number/know_old_government.py or super/get_important_way_over_big_government/same_life/come_large_eye_of_number/few_time_and_man/high_world/good_time_and_small_hand.py above big point"

---
+++ super/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    right_number(str_arg)
+    print('large_eye_or_part')
+
+def right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('place_or_great_world')
