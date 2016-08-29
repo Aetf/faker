5d23166cc624455171131beefc357503585d95f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:52:11 -0400

    "mix: look super/high_thing/want_company_in_number/long_world_or_large_case/early_company/other_year_or_day.py or super/part_and_life.py from early week"

---
+++ super/high_thing/want_company_in_number/long_world_or_large_case/early_company/other_year_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    good_case(str_arg)
+    print('new_government')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('large_place')
---
+++ super/part_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_man(str_arg):
+    think_week(str_arg)
+    print('high_man')
+
+def think_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_man('next_thing')
