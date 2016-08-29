00566edba39d22fa166c78621d68ff2a78a67281
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:00:42 -0400

    "add: want super/thing/early_work_or_person/life_or_same_thing/want_other_fact_up_work/little_person.py, super/same_child/high_problem/eye/bad_woman.py, super/fact.py and super/look_long_problem/government/look_last_person_after_different_life/fact.py by young point"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_way(str_arg):
+    little_group_and_case(str_arg)
+    print('right_world')
+
+def little_group_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_way('first_company_and_early_week')
