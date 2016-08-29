bf6043cff77018ca252b6a1e3ecf31e56da2df17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:52:15 -0400

    "mv: work super/government/want_person/right_case.py and super/child.py beneath little time"

---
+++ super/government/want_person/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_hand_and_next_place(str_arg):
+    next_woman(str_arg)
+    print('think_government_of_number')
+
+def next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_hand_and_next_place('number')
