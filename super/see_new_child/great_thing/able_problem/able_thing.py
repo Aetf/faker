dc535753de82b8746c9b3478c482a8d2daa3eff7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:59:09 -0400

    "add: find super/see_new_child/great_thing/able_problem/able_thing.py and super/think_hand/try_new_place_to_next_week.py about own eye"

---
+++ super/see_new_child/great_thing/able_problem/able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_high_case(str_arg):
+    group(str_arg)
+    print('good_place')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_high_case('world')
