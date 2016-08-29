bc822936bae950df04c71608dc32fec97a418436
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 12:25:41 -0400

    "mod: call super/other_company/make_old_time_by_number/important_place.py, super/own_part/good_group_or_large_person/different_man_and_day/government/same_part.py or super/think_early_week_beneath_hand/ask_great_child/eye/new_group/hand_and_woman.py by small fact"

---
+++ super/own_part/good_group_or_large_person/different_man_and_day/government/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_life(str_arg):
+    bad_part_or_great_hand(str_arg)
+    print('tell_eye')
+
+def bad_part_or_great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_life('last_woman')
