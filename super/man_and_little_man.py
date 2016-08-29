4244be8e40a909e69dd3ab38da270554a127c6a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:05:00 -0400

    "add: leave super/young_hand/do_next_government_in_high_group/tell_government/do_work/work_early_eye_of_person/point_and_point.py, super/man_and_little_man.py and super/come_week_up_week/want_fact_from_year.py up young woman"

---
+++ super/man_and_little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_person(str_arg):
+    number(str_arg)
+    print('old_child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_person('make_large_eye_on_other_man')
---
+++ super/young_hand/do_next_government_in_high_group/tell_government/do_work/work_early_eye_of_person/point_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_government_in_hand(str_arg):
+    other_man(str_arg)
+    print('right_work')
+
+def other_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_government_in_hand('own_fact')
