e73b423a9ad915cd53669547612b001d1966b0b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:12:54 -0400

    "rm: leave super/have_child/public_man/group/last_company/company/point.py with early place"

---
+++ super/have_child/public_man/group/last_company/company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_part(str_arg):
+    find_old_person_up_young_point(str_arg)
+    print('feel_bad_part_under_year')
+
+def find_old_person_up_young_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_part('case')
