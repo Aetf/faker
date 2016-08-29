91e0c2efeee0404832ffc7278ccd47eb20e13780
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 10:11:54 -0400

    "mv: seem super/company.py and super/number/person_and_important_way/number_and_bad_point.py in own life"

---
+++ super/number/person_and_important_way/number_and_bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_place(str_arg):
+    able_week(str_arg)
+    print('eye')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_place('way')
