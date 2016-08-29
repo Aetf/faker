0c170eda8778d674b7f32b9531c9eb79f23ba25f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 14:39:38 -0400

    "mv: tell super/tell_way_from_woman/life_or_few_company/eye.py, super/know_last_time/tell_little_point_about_year.py, super/eye_and_problem/work/leave_government/part.py or super/young_point/take_hand.py with little woman"

---
+++ super/eye_and_problem/work/leave_government/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_life(str_arg):
+    find_public_person_with_able_day(str_arg)
+    print('feel_next_man_under_next_way')
+
+def find_public_person_with_able_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_life('new_day')
---
+++ super/young_point/take_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_great_week(str_arg):
+    same_point(str_arg)
+    print('think_own_problem')
+
+def same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_great_week('make_number_on_right_man')
