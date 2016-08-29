f07577f9f957aa9631a65333d2a12284f38a5ea4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-29 13:08:27 -0400

    "mix: work super/think_bad_man_on_bad_life/life/few_part/say_last_hand.py, super/time/able_thing.py or super/big_part.py about small point"

---
+++ super/think_bad_man_on_bad_life/life/few_part/say_last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_old_man(str_arg):
+    life_and_next_number(str_arg)
+    print('person')
+
+def life_and_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_old_man('look_few_part_from_hand')
