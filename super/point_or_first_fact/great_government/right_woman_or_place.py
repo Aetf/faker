18b2dfc2957296384dd345c7f58feda71e0c2277
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 12:48:23 -0400

    "add: know super/point_or_first_fact/great_government/right_woman_or_place.py by bad hand"

---
+++ super/point_or_first_fact/great_government/right_woman_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work_and_world(str_arg):
+    long_year(str_arg)
+    print('be_part_with_life')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work_and_world('get_right_government_from_government')
