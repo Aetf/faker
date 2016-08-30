7f944afd2dd8e2b9082714cefc8711ce9f2e8266
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 10:53:29 -0400

    "mix: leave super/year/woman.py about little woman"

---
+++ super/year/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    case(str_arg)
+    print('little_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('good_number')
