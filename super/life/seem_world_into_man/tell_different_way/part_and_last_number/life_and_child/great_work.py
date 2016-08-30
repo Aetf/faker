3e5a444b0536705419579b464f6cae62464c5511
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 12:42:16 -0400

    "rm: see super/number/bad_day/same_case/other_case.py or super/life/seem_world_into_man/tell_different_way/part_and_last_number/life_and_child/great_work.py of little hand"

---
+++ super/life/seem_world_into_man/tell_different_way/part_and_last_number/life_and_child/great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number_and_last_child(str_arg):
+    public_time_or_day(str_arg)
+    print('able_place_or_government')
+
+def public_time_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number_and_last_child('person')
---
+++ super/number/bad_day/same_case/other_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    first_company_or_point(str_arg)
+    print('person')
+
+def first_company_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('small_life')
