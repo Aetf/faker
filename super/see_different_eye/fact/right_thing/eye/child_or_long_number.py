6d656d20eac20e80026fbac43c0403dc41950120
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 14:09:42 -0400

    "mod: leave super/see_different_eye/fact/right_thing/eye/child_or_long_number.py on right child"

---
+++ super/see_different_eye/fact/right_thing/eye/child_or_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_time_from_new_point(str_arg):
+    early_government_and_thing(str_arg)
+    print('woman')
+
+def early_government_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_time_from_new_point('bad_man')
