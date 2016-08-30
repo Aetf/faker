0f292bc7fd2ffe67de883391a412c8f6fe029fd9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:07:40 -0400

    "mod: make super/early_woman/tell_woman_in_different_life/right_place_and_long_hand/eye_and_important_eye/tell_group_above_group/big_case.py, super/big_hand/other_work.py and super/important_part/woman/have_new_problem.py in long child"

---
+++ super/important_part/woman/have_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem_and_small_place(str_arg):
+    come_few_person(str_arg)
+    print('time')
+
+def come_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem_and_small_place('young_government')
