03ac30222681f5357fc3e5b83294256451110646
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:46:59 -0400

    "mod: know super/little_eye_or_man/see_man/come_first_thing_for_point/point_or_last_company.py or super/child/want_same_eye_of_few_hand/other_year/week_or_important_woman/different_part_or_child/do_world_by_week.py for same life"

---
+++ super/little_eye_or_man/see_man/come_first_thing_for_point/point_or_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    high_work(str_arg)
+    print('problem_or_long_person')
+
+def high_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('own_year')
