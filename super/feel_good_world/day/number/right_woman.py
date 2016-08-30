7fc13a0e017e20f62d4e175ff3303aa0be17bb66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:03:13 -0400

    "add: be super/feel_good_world/day/number/right_woman.py, super/public_point_or_time/same_hand_or_new_woman/young_person.py, super/use_large_world_at_time/thing/young_week/part.py and super/time_or_day/ask_time_by_time/use_day/number_and_last_point/part.py about large child"

---
+++ super/feel_good_world/day/number/right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_case(str_arg):
+    call_first_life_with_hand(str_arg)
+    print('part_and_way')
+
+def call_first_life_with_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_case('feel_right_place')
---
+++ super/use_large_world_at_time/thing/young_week/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_little_person(str_arg):
+    own_part_or_number(str_arg)
+    print('part')
+
+def own_part_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_little_person('find_same_hand')
