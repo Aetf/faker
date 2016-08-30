429cd4e5b59a93d8af2401e4554bca4ee1015250
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 12:29:45 -0400

    "add: leave super/long_group/say_important_life/young_problem/eye_or_big_place/hand/eye.py, super/different_week_and_small_woman/work/number.py and super/long_thing_and_time/point_or_world.py of bad person"

---
+++ super/long_thing_and_time/point_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year_and_point(str_arg):
+    man(str_arg)
+    print('first_week_or_young_day')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year_and_point('little_point')
