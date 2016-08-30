313eaf801dfb6ac46ebaaa2371e5e38d7a75f5b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 13:01:50 -0400

    "mod: try super/point_or_day/way_and_week.py, super/thing.py and super/say_child/world_or_hand.py on good man"

---
+++ super/point_or_day/way_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    see_world_in_time(str_arg)
+    print('look_company_over_week')
+
+def see_world_in_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('different_place_and_long_child')
---
+++ super/say_child/world_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_way(str_arg):
+    first_person_and_fact(str_arg)
+    print('way')
+
+def first_person_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_way('young_life')
