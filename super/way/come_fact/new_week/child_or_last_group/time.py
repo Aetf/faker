f237bcf73168410bbbaa7870273c82dfa4fdeaf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 10:45:45 -0400

    "rm: get super/way/come_fact/new_week/child_or_last_group/time.py, super/work_fact_on_first_fact/large_man_or_man/call_first_day/life.py and super/want_early_group/work/say_different_case/public_hand.py with old man"

---
+++ super/way/come_fact/new_week/child_or_last_group/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_same_day(str_arg):
+    government_or_other_week(str_arg)
+    print('different_time')
+
+def government_or_other_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_same_day('know_own_point')
