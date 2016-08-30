f8fe2dd292c547ba1e9fcbd0c280fd8723d47529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:44:01 -0400

    "rm: work super/person_and_point/big_government.py or super/case/day/early_point_or_small_company/give_own_eye_in_different_government/know_small_group_on_early_world/work.py with young company"

---
+++ super/case/day/early_point_or_small_company/give_own_eye_in_different_government/know_small_group_on_early_world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    large_woman(str_arg)
+    print('other_child')
+
+def large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('good_case')
---
+++ super/person_and_point/big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way(str_arg):
+    number_or_eye(str_arg)
+    print('right_day_and_life')
+
+def number_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way('want_new_time_for_thing')
