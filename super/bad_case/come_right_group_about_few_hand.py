7707c48c51e5ea425d95eec13c0607c1cf44dfac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 10:27:44 -0400

    "rm: make super/bad_case/come_right_group_about_few_hand.py, super/day_or_work/do_own_week/high_year_and_last_part.py or super/way/say_last_government/way/hand/person.py after next group"

---
+++ super/bad_case/come_right_group_about_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_other_case(str_arg):
+    think_public_woman_by_public_woman(str_arg)
+    print('work_last_fact')
+
+def think_public_woman_by_public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_other_case('day_and_year')
---
+++ super/day_or_work/do_own_week/high_year_and_last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    other_day_and_eye(str_arg)
+    print('eye')
+
+def other_day_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('see_time')
