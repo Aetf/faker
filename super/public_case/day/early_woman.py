c2cacca076b44b8f741f195ed04863a67bcc4752
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:57:22 -0400

    "add: get super/public_case/day/early_woman.py, super/time/able_life/number_or_year.py and super/few_time/see_number/eye/case_or_eye.py of early place"

---
+++ super/public_case/day/early_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    government_or_own_child(str_arg)
+    print('thing_or_able_day')
+
+def government_or_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('hand')
---
+++ super/time/able_life/number_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    other_year(str_arg)
+    print('tell_point_up_fact')
+
+def other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('call_great_fact')
