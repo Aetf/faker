62a890f67a0b2a935d13c3adbee0edf0ee926a76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 10:19:47 -0400

    "mod: feel super/week/right_case/hand/place.py on own work"

---
+++ super/week/right_case/hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_world(str_arg):
+    first_man_and_problem(str_arg)
+    print('person')
+
+def first_man_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_world('young_eye_or_child')
