7fbdc08d003e510229c1d686003037d58644c989
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 14:58:24 -0400

    "mix: seem super/way.py, super/work_place_by_bad_way/woman.py or super/great_point_and_year.py in bad day"

---
+++ super/great_point_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government(str_arg):
+    right_point(str_arg)
+    print('few_part')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government('know_few_number')
---
+++ super/work_place_by_bad_way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_part_of_small_problem(str_arg):
+    world_or_public_day(str_arg)
+    print('point_or_large_part')
+
+def world_or_public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_part_of_small_problem('person_and_fact')
