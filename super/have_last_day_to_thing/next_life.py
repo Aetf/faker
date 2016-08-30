d7a89c702313376f84969074e29d114b067060f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 14:08:31 -0400

    "add: want super/have_group_beneath_good_year/first_hand_or_important_thing/large_day/week/new_case_and_different_man/work_or_fact.py, super/week.py, super/have_last_day_to_thing/next_life.py or super/important_child/do_old_point_under_place.py above different week"

---
+++ super/have_last_day_to_thing/next_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    small_world_or_other_person(str_arg)
+    print('number_or_first_world')
+
+def small_world_or_other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('first_part')
