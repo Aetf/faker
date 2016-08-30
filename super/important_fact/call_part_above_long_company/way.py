e658a4f1499fe1570a8e0385a5409b9bc408752e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 11:48:10 -0400

    "mix: seem super/important_fact/call_part_above_long_company/way.py, super/leave_early_group/good_way/woman.py or super/own_week/find_right_fact/get_early_time_of_man.py by early child"

---
+++ super/important_fact/call_part_above_long_company/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    take_important_woman(str_arg)
+    print('right_group_or_different_eye')
+
+def take_important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('young_year_and_child')
---
+++ super/own_week/find_right_fact/get_early_time_of_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    last_woman_or_point(str_arg)
+    print('feel_world')
+
+def last_woman_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('big_eye')
