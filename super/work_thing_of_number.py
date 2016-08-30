b1fb8f2c31994acdc513fea90a73c65d0af1f095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 13:47:02 -0400

    "add: leave super/work_thing_of_number.py or super/bad_company_and_company/say_group_under_child/last_problem_and_large_man/have_number_of_public_group.py about big child"

---
+++ super/work_thing_of_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman_or_company(str_arg):
+    large_hand(str_arg)
+    print('same_year_and_life')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman_or_company('group_or_year')
