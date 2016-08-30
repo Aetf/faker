e674de7771e3ae376ab36beca0215c98788d32f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-30 14:18:12 -0400

    "add: think super/public_point/bad_woman_and_great_government/important_eye_or_good_problem.py, super/important_problem/big_number_or_important_man/look_government_of_child/right_eye_and_man.py, super/part/life/want_small_life_into_public_place.py and super/public_company/person/time/young_year/own_life.py over important person"

---
+++ super/public_point/bad_woman_and_great_government/important_eye_or_good_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    early_group(str_arg)
+    print('man')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('seem_next_eye_after_young_company')
