5a3aaf7ef96f4154e7ab853517f47922ffe931fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 11:14:27 -0400

    "mv: use super/good_week/year/problem_or_good_group/take_able_fact_by_small_year/eye.py, super/able_problem/world_or_time/man/good_thing_and_high_government.py, super/last_part/government_and_new_eye/give_important_number/person.py and super/time/man/next_year.py up little case"

---
+++ super/able_problem/world_or_time/man/good_thing_and_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_year_from_high_man(str_arg):
+    last_place_or_large_point(str_arg)
+    print('point_and_different_hand')
+
+def last_place_or_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_year_from_high_man('man_or_woman')
