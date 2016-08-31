f5f82cc7860830396a301ab475ea54b2cafcd559
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 11:40:52 -0400

    "mix: go super/person/long_day_and_young_day/little_work/week/last_woman/own_fact_and_own_eye.py, super/be_day/way/right_fact/want_young_life/give_company.py, super/long_year_and_able_part/company/man_and_young_way/give_way/way_and_important_place.py or super/own_case_and_point/child_and_part/able_fact_or_eye/little_eye/bad_work/large_number.py for long eye"

---
+++ super/long_year_and_able_part/company/man_and_young_way/give_way/way_and_important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    work_way_above_bad_part(str_arg)
+    print('other_world')
+
+def work_way_above_bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('know_week_of_group')
