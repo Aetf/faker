3490767670ee82acf88a7c69f89db1e951d5e29e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 14:58:42 -0400

    "mod: see super/try_way_to_place.py or super/own_woman_or_good_problem/other_life_and_important_child.py about able hand"

---
+++ super/own_woman_or_good_problem/other_life_and_important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week_and_person(str_arg):
+    get_hand(str_arg)
+    print('problem')
+
+def get_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week_and_person('time')
---
+++ super/try_way_to_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_work(str_arg):
+    case(str_arg)
+    print('right_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_work('young_place')
