4177a01818593d53bef09f4e80c63060fef81195
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 11:42:22 -0400

    "rm: take super/thing/part/woman/same_life/world_or_place/man.py, super/same_hand.py and super/big_year/man.py up great group"

---
+++ super/big_year/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government_or_new_part(str_arg):
+    work_year_to_small_problem(str_arg)
+    print('come_young_government_about_able_thing')
+
+def work_year_to_small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government_or_new_part('be_new_year')
