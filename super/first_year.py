57d579da331788c8a716a22c94aee45cd4d22dff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 13:02:44 -0400

    "rm: make super/first_year.py from own place"

---
+++ super/first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    get_long_thing(str_arg)
+    print('long_work')
+
+def get_long_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('leave_problem')
