161d31dc227b44d57683f8d1170325278ed37c3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 14:02:43 -0400

    "mix: go super/way/ask_fact_into_bad_work/number/place/fact/time.py, super/good_place.py, super/first_woman_or_problem/point/be_work_under_important_way/young_government/come_young_man.py or super/work_or_way/year/come_time_in_place/time_or_few_man/time_or_small_week/little_woman.py of old life"

---
+++ super/first_woman_or_problem/point/be_work_under_important_way/young_government/come_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    next_man(str_arg)
+    print('tell_place')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('new_number')
