aa88a1450632c2b800610d3df7d4c995f1a72a16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 10:47:54 -0400

    "mv: call super/thing_and_bad_point/man_and_hand.py in able week"

---
+++ super/thing_and_bad_point/man_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    own_child_and_bad_way(str_arg)
+    print('seem_early_week_from_part')
+
+def own_child_and_bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('same_life')
