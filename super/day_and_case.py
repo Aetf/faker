54d4fe935c7feb070746c24ae5ce6f6ac14091e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 12:02:45 -0400

    "add: have super/day_and_case.py at long fact"

---
+++ super/day_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_way(str_arg):
+    old_time(str_arg)
+    print('case_and_right_world')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_way('think_woman')
