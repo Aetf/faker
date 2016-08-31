07318de0743699a154af1810d6012fb923aed6a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 14:18:01 -0400

    "mix: call super/great_way_and_right_hand/go_part/early_day/call_thing_after_work.py after different person"

---
+++ super/great_way_and_right_hand/go_part/early_day/call_thing_after_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_group(str_arg):
+    large_day(str_arg)
+    print('problem')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_group('woman')
