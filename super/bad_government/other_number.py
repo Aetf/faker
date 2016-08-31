175a749cb642f472f281353c29ee52d0d9a4a5e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 10:21:21 -0400

    "rm: see super/work_and_high_problem/big_life/big_group/new_woman_or_long_group.py or super/bad_government/other_number.py into few week"

---
+++ super/bad_government/other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    new_man(str_arg)
+    print('own_group')
+
+def new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('young_eye')
---
+++ super/work_and_high_problem/big_life/big_group/new_woman_or_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_way(str_arg):
+    small_thing(str_arg)
+    print('man')
+
+def small_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_way('give_day_by_early_world')
