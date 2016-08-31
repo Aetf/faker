7f03ebbea077cbcf36481a80938e6385e69beb2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 13:32:38 -0400

    "add: say super/first_way/be_week/different_part.py and super/great_place/person_or_way/tell_work_into_small_problem.py at first problem"

---
+++ super/first_way/be_week/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_big_case(str_arg):
+    last_place_and_great_person(str_arg)
+    print('large_government')
+
+def last_place_and_great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_big_case('eye')
