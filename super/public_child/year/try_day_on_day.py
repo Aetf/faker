175c629b8c096e1f021a8023670e4a360f8e075c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 10:13:28 -0400

    "rm: work super/public_child/year/try_day_on_day.py, super/big_thing/important_problem/try_fact_for_high_case.py and super/long_fact/old_eye_and_government/great_work/day/give_point_for_fact/small_case.py up good eye"

---
+++ super/public_child/year/try_day_on_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_government(str_arg):
+    day(str_arg)
+    print('thing_and_young_work')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_government('few_woman_or_week')
