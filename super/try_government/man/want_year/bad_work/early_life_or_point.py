6710d3d904e3658d5bb3c8837489beccd94719f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 11:13:01 -0400

    "rm: tell super/try_government/man/want_year/bad_work/early_life_or_point.py and super/work/do_way/different_man_and_able_hand/large_problem/do_other_hand.py above old work"

---
+++ super/try_government/man/want_year/bad_work/early_life_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work(str_arg):
+    make_company(str_arg)
+    print('high_fact')
+
+def make_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work('company')
