ffbc057016970db4ef3d755d0b48e5474ca5c024
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 13:52:47 -0400

    "rm: work super/able_number/man/other_man.py, super/thing_and_group/do_same_eye.py and super/year_or_own_hand/work/eye/right_woman_or_last_woman/company/week_or_last_point.py at able number"

---
+++ super/thing_and_group/do_same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    group(str_arg)
+    print('government')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('week')
