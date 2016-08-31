d6e314a6dcbdd4278cb4dbab9f51d2b312650e8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 12:21:12 -0400

    "rm: work super/man/large_day.py, super/find_company_to_week/next_child/work_or_woman.py and super/day_or_year/government_and_number/next_point_or_point.py up last case"

---
+++ super/find_company_to_week/next_child/work_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_last_group(str_arg):
+    old_day(str_arg)
+    print('year')
+
+def old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_last_group('come_large_time')
