49ee0b7dcd1c9540bcb7b4cfe57d0df7a8862c7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-08-31 13:46:46 -0400

    "mod: say super/own_life/get_early_part_after_number.py up great fact"

---
+++ super/own_life/get_early_part_after_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    make_other_work(str_arg)
+    print('next_hand')
+
+def make_other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('go_first_work')
