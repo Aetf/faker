4c48ef7387f8e14df107aefa1b04901ba4b25e67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:12:01 -0400

    "mv: use super/different_company/have_problem/small_number_or_man/take_point/new_company_and_problem/first_part.py and super/long_work_and_part/work/think_great_government/small_part/ask_able_person.py over last place"

---
+++ super/different_company/have_problem/small_number_or_man/take_point/new_company_and_problem/first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    have_time(str_arg)
+    print('last_part')
+
+def have_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('small_place_or_day')
---
+++ super/long_work_and_part/work/think_great_government/small_part/ask_able_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    first_way_and_problem(str_arg)
+    print('point')
+
+def first_way_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('important_hand_or_case')
