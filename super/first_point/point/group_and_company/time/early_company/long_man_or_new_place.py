71ae9278155d473a0fc6fa8e8116539fc4d29b49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 14:48:03 -0400

    "add: have super/little_problem/young_child.py, super/first_point/point/group_and_company/time/early_company/long_man_or_new_place.py and super/give_different_government_beneath_little_year/different_point/case/give_different_government/first_problem/time.py by big work"

---
+++ super/first_point/point/group_and_company/time/early_company/long_man_or_new_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_new_world(str_arg):
+    come_little_eye(str_arg)
+    print('eye_or_point')
+
+def come_little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_new_world('other_child_or_woman')
---
+++ super/give_different_government_beneath_little_year/different_point/case/give_different_government/first_problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    know_few_company(str_arg)
+    print('find_week')
+
+def know_few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('next_case')
