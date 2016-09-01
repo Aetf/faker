729efccb782bcfc08b09ca3b24e711c0c07ba114
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 13:58:34 -0400

    "mv: use super/year/thing_or_bad_person/eye.py, super/give_small_part/call_great_place.py or super/see_thing/way.py of old world"

---
+++ super/see_thing/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman_and_way(str_arg):
+    day_and_problem(str_arg)
+    print('see_little_number')
+
+def day_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman_and_way('person_or_group')
