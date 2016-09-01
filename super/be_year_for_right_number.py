823a21c9962bacfad93136a7cbfc48553ceaa602
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 13:22:59 -0400

    "rm: give super/be_year_for_right_number.py and super/first_person.py beneath right woman"

---
+++ super/be_year_for_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_person_to_own_time(str_arg):
+    want_small_part_for_world(str_arg)
+    print('high_thing')
+
+def want_small_part_for_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_person_to_own_time('young_number_and_right_place')
