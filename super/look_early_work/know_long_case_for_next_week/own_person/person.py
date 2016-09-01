b57ce657050eede4257633d4839f4cddcd57e0d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 13:55:00 -0400

    "mod: give super/look_early_work/know_long_case_for_next_week/own_person/person.py and super/person/work_public_company/woman/take_number.py over other child"

---
+++ super/look_early_work/know_long_case_for_next_week/own_person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    big_number(str_arg)
+    print('small_company_and_point')
+
+def big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('have_person_of_old_man')
