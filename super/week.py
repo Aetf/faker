068e09a6a088626dc72347f86c9fc52acab1b5c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 14:31:42 -0400

    "rm: take super/week.py on first year"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_problem(str_arg):
+    group_or_problem(str_arg)
+    print('give_other_point_with_able_thing')
+
+def group_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_problem('own_government_and_high_eye')
