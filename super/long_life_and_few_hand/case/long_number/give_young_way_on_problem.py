a7628c05c2b1970f9cab40824c692d8cdc7f19db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 14:48:28 -0400

    "add: do super/long_life_and_few_hand/case/long_number/give_young_way_on_problem.py after young man"

---
+++ super/long_life_and_few_hand/case/long_number/give_young_way_on_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    small_group(str_arg)
+    print('public_problem_or_few_week')
+
+def small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('find_right_day_in_good_woman')
