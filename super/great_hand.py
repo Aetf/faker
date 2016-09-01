c4b3be62ecaa21bf3dd1dd0766be6205eac1c8c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:12:26 -0400

    "mod: take super/big_case/few_company_and_year/group.py, super/great_hand.py and super/public_child/person/want_little_group/case/young_case_or_world/thing.py of small world"

---
+++ super/great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_thing_or_own_week(str_arg):
+    little_place(str_arg)
+    print('have_child_of_woman')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_thing_or_own_week('part')
---
+++ super/public_child/person/want_little_group/case/young_case_or_world/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_young_week(str_arg):
+    large_world(str_arg)
+    print('take_day')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_young_week('work')
