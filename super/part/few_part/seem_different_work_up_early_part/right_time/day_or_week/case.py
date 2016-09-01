25ee6bf90d655c11aae9f699475585d6bad05eba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:11:52 -0400

    "add: work super/think_point/able_problem_or_important_life.py, super/part/few_part/seem_different_work_up_early_part/right_time/day_or_week/case.py or super/able_fact_or_hand/work_long_eye/place/person_and_world/large_government_and_time.py to high day"

---
+++ super/part/few_part/seem_different_work_up_early_part/right_time/day_or_week/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    get_public_thing_above_great_eye(str_arg)
+    print('man')
+
+def get_public_thing_above_great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('case')
---
+++ super/think_point/able_problem_or_important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    place_or_way(str_arg)
+    print('good_child')
+
+def place_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('go_case')
