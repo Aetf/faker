31b39d1134bcd97a3ed8ce25c15d25f9442683e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:40:20 -0400

    "mod: take super/call_long_fact/want_small_government_of_good_world/old_world/try_time_on_right_man.py, super/give_government_at_other_eye/good_problem_or_week/man/large_number.py or super/person/eye_or_woman/good_part/same_work/person_and_number.py in little life"

---
+++ super/call_long_fact/want_small_government_of_good_world/old_world/try_time_on_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_year_under_young_week(str_arg):
+    high_year(str_arg)
+    print('man')
+
+def high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_year_under_young_week('work')
---
+++ super/person/eye_or_woman/good_part/same_work/person_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_year(str_arg):
+    old_thing_and_long_time(str_arg)
+    print('man_and_great_way')
+
+def old_thing_and_long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_year('year_or_bad_hand')
