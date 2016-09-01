698080a8b4d81c553885b17aa5742f3aad9de7a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 14:36:29 -0400

    "add: see super/different_woman/other_world_or_important_point/world_or_thing/bad_time.py above little hand"

---
+++ super/different_woman/other_world_or_important_point/world_or_thing/bad_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_life(str_arg):
+    able_year(str_arg)
+    print('person')
+
+def able_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_life('public_group')
