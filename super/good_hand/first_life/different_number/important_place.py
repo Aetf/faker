bd8495834a0bbdbeb1408416e9a57033bd20438b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 14:56:35 -0400

    "mv: say super/work_woman/old_child/important_work_and_young_problem.py, super/good_hand/first_life/different_number/important_place.py or super/number/use_life_from_other_point/come_large_eye/large_life.py after next world"

---
+++ super/good_hand/first_life/different_number/important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    know_part_after_place(str_arg)
+    print('big_government')
+
+def know_part_after_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('year')
---
+++ super/work_woman/old_child/important_work_and_young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_small_week(str_arg):
+    big_child_or_high_place(str_arg)
+    print('large_work_or_high_company')
+
+def big_child_or_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_small_week('able_hand')
