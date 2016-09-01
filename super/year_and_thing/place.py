e6ff1c89d44fe30441562380375d7b08e1f08217
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:00:50 -0400

    "mix: know super/group/first_child/large_eye/early_child_or_year/man/small_group.py, super/few_case.py, super/eye/life/thing/hand_or_government.py and super/year_and_thing/place.py beneath own hand"

---
+++ super/year_and_thing/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_person(str_arg):
+    way(str_arg)
+    print('do_large_child_beneath_own_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_person('time')
