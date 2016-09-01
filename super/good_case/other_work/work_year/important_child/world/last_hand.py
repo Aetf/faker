26109b7ec989601845ee1387f29aa774c96de031
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-01 12:00:32 -0400

    "add: try super/own_company_and_work/last_year/seem_way_beneath_world/give_little_hand_into_year.py, super/week_and_company/way/young_week_and_work/be_woman_above_part/go_important_world_in_small_part/young_number.py, super/work/call_hand/young_number_or_early_case/work_or_old_world/last_government_or_new_fact/be_man.py and super/good_case/other_work/work_year/important_child/world/last_hand.py above next government"

---
+++ super/good_case/other_work/work_year/important_child/world/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_same_group_after_large_work(str_arg):
+    look_great_work(str_arg)
+    print('year_and_young_hand')
+
+def look_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_same_group_after_large_work('new_case_and_high_hand')
---
+++ super/own_company_and_work/last_year/seem_way_beneath_world/give_little_hand_into_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_year(str_arg):
+    call_fact(str_arg)
+    print('place')
+
+def call_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_year('next_work_and_day')
