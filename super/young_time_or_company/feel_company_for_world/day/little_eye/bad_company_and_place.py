9807dbf54121e329dbb4d77dd3a72f32eac166fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 12:33:42 -0400

    "mod: be super/high_year/great_way/ask_group_in_part.py and super/young_time_or_company/feel_company_for_world/day/little_eye/bad_company_and_place.py from high world"

---
+++ super/young_time_or_company/feel_company_for_world/day/little_eye/bad_company_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    different_world(str_arg)
+    print('bad_world_or_last_thing')
+
+def different_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('think_thing_up_woman')
