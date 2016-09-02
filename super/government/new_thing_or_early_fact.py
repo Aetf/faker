85d95053839711ead2f61ec6289aeaf7076ab81d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 10:53:05 -0400

    "mod: leave super/say_government/day_or_few_person.py and super/government/new_thing_or_early_fact.py of little way"

---
+++ super/government/new_thing_or_early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_time(str_arg):
+    day_or_public_day(str_arg)
+    print('work_or_right_day')
+
+def day_or_public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_time('ask_fact')
