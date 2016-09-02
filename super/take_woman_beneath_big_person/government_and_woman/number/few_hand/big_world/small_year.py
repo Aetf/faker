956355f619ad0a263224ed7c6d57ac1400420a91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 13:27:40 -0400

    "mix: tell super/way.py and super/take_woman_beneath_big_person/government_and_woman/number/few_hand/big_world/small_year.py with high world"

---
+++ super/take_woman_beneath_big_person/government_and_woman/number/few_hand/big_world/small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    part_or_work(str_arg)
+    print('work_or_few_place')
+
+def part_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('able_hand')
