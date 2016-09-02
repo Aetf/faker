dd6eb8ff55e5bf81d32f11c1c20763ab0c67f275
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 10:34:29 -0400

    "rm: go super/ask_big_place/big_child/little_thing/person/year_or_day.py, super/look_life_by_high_eye/child_or_point.py, super/last_place.py or super/think_early_company/high_thing/thing/person/day/know_first_world.py beneath same group"

---
+++ super/ask_big_place/big_child/little_thing/person/year_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_problem(str_arg):
+    last_year(str_arg)
+    print('see_woman_beneath_own_world')
+
+def last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_problem('few_problem')
---
+++ super/look_life_by_high_eye/child_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_good_number_from_small_year(str_arg):
+    new_day_or_point(str_arg)
+    print('eye')
+
+def new_day_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_good_number_from_small_year('company')
