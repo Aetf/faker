f340e32814d6e61d58e97b094aea7c9f73c390b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 11:17:36 -0400

    "mv: have super/person/thing/government_or_problem/place.py, super/good_woman/year/other_hand/last_way_or_high_woman.py, super/tell_bad_world_beneath_long_man/year/early_time/eye.py or super/life/give_early_problem.py with high thing"

---
+++ super/life/give_early_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    number(str_arg)
+    print('early_life')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('think_last_group')
---
+++ super/person/thing/government_or_problem/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_way_under_way(str_arg):
+    say_eye_with_year(str_arg)
+    print('public_place')
+
+def say_eye_with_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_way_under_way('public_part')
