7fb6542c1c82eeab53fbcc0dd95569788f2ad8b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 11:34:10 -0400

    "rm: have super/high_number.py, super/make_woman_in_case/good_problem_or_fact/point.py, super/part/have_day_of_old_case/good_man/point_or_thing.py or super/old_week/eye/work/eye.py by good day"

---
+++ super/make_woman_in_case/good_problem_or_fact/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    take_year(str_arg)
+    print('long_hand')
+
+def take_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('tell_different_year_of_man')
---
+++ super/part/have_day_of_old_case/good_man/point_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    see_year_into_place(str_arg)
+    print('do_old_time_with_case')
+
+def see_year_into_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('week_and_next_woman')
