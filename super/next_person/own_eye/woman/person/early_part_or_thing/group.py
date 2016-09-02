710b35235737b6ebb2a324e9b9538e74b49c8bd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 13:29:08 -0400

    "mod: use super/group/have_big_hand_up_place/call_work_into_place/high_number/look_other_government.py and super/next_person/own_eye/woman/person/early_part_or_thing/group.py on important time"

---
+++ super/next_person/own_eye/woman/person/early_part_or_thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing_or_high_work(str_arg):
+    fact(str_arg)
+    print('able_government_or_long_fact')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing_or_high_work('time')
