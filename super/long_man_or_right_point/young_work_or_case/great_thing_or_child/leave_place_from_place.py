4f0e495e5bb04e2dd7a5b2b1e39b6431d0efef1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 11:14:26 -0400

    "rm: make super/long_man_or_right_point/young_work_or_case/great_thing_or_child/leave_place_from_place.py on first part"

---
+++ super/long_man_or_right_point/young_work_or_case/great_thing_or_child/leave_place_from_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person(str_arg):
+    public_part(str_arg)
+    print('give_man_beneath_early_hand')
+
+def public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person('life')
