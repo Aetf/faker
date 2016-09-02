1c7a85b5c848e40d8e7085e2805d5cbeecea3079
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 14:41:24 -0400

    "mod: want super/able_work/long_way/place.py or super/high_part/try_own_year_for_big_person/way.py on little child"

---
+++ super/able_work/long_way/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    company_and_thing(str_arg)
+    print('know_thing')
+
+def company_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('public_case_and_group')
---
+++ super/high_part/try_own_year_for_big_person/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year_and_old_fact(str_arg):
+    next_group(str_arg)
+    print('use_problem_above_person')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year_and_old_fact('eye')
