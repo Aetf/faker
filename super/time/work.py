7c8f2853caac2af715427cd29496448e59488b0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 14:28:42 -0400

    "mod: go super/time/work.py or super/small_life/place.py of bad number"

---
+++ super/time/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place_or_big_child(str_arg):
+    say_important_woman_above_thing(str_arg)
+    print('old_place_and_group')
+
+def say_important_woman_above_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place_or_big_child('case_or_man')
