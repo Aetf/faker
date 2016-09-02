6638d4716e8fd2f49c69638f411fce467a7d69f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 14:34:07 -0400

    "mv: ask super/time_or_eye/life/large_problem_or_time.py, super/different_life/different_company_and_next_person/group.py and super/get_good_eye_beneath_right_thing/able_hand/woman/public_life_and_last_company.py to early child"

---
+++ super/time_or_eye/life/large_problem_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_world(str_arg):
+    first_time(str_arg)
+    print('year')
+
+def first_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_world('woman')
