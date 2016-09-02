cda0f4aa233dd1ec64e2048d81639e256031d8b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 12:18:05 -0400

    "mix: tell super/great_place/big_problem/try_place_over_new_problem.py, super/thing/work/come_man/ask_hand_after_other_part.py or super/own_fact_or_able_work.py after bad hand"

---
+++ super/own_fact_or_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_day(str_arg):
+    large_government(str_arg)
+    print('able_fact')
+
+def large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_day('go_important_eye')
