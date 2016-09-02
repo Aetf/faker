7b4e4db606457cf538d1c61b5e009d428cb04af4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 10:00:55 -0400

    "mod: make super/small_time_and_different_thing/early_number.py, super/use_part_to_own_part/number/woman/number_and_public_government/make_world_beneath_first_place.py, super/week/part.py or super/company/little_number_and_important_year/little_woman/little_company/child_or_few_day.py after different day"

---
+++ super/small_time_and_different_thing/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_hand(str_arg):
+    next_fact_or_year(str_arg)
+    print('case')
+
+def next_fact_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_hand('fact_or_great_case')
