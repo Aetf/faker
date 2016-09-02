50e7813e23996f3f242d11e0b206894f91b77f8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-02 11:06:23 -0400

    "add: work super/other_way/government_or_good_point/day_and_able_woman/able_place.py with bad group"

---
+++ super/other_way/government_or_good_point/day_and_able_woman/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_good_part(str_arg):
+    first_woman_or_person(str_arg)
+    print('case')
+
+def first_woman_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_good_part('look_case_under_old_week')
