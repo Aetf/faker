cdd84600a1fc35dfe59c1c683710cd34bed0978d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 10:46:38 -0400

    "mix: come super/case_and_few_problem/way_and_person/young_year/new_point/eye/same_year.py under first person"

---
+++ super/case_and_few_problem/way_and_person/young_year/new_point/eye/same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company_or_company(str_arg):
+    long_man(str_arg)
+    print('same_case')
+
+def long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company_or_company('find_old_case_of_small_man')
