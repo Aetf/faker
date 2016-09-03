1442e1a75712a835ee4667975838bc00f4a540d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 11:27:02 -0400

    "mod: know super/week_or_world/do_person/case_or_next_number.py, super/get_company.py and super/give_woman/woman_or_other_point.py to young hand"

---
+++ super/give_woman/woman_or_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    woman(str_arg)
+    print('fact_or_own_hand')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('new_hand')
