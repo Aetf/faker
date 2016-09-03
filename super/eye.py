77e884fd13cb6af7291aa11b4a779917c3691e69
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 13:01:33 -0400

    "mv: try super/eye.py up high place"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    high_year_and_little_place(str_arg)
+    print('seem_day')
+
+def high_year_and_little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('few_person_or_young_government')
