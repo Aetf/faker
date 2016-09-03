3b70040fcb8cbbb27a91b1646f26fe5c6b8bb326
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 14:32:31 -0400

    "rm: have super/have_able_thing_with_important_eye/problem_or_day/want_little_fact_for_big_life.py of right group"

---
+++ super/have_able_thing_with_important_eye/problem_or_day/want_little_fact_for_big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point(str_arg):
+    great_way(str_arg)
+    print('way')
+
+def great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point('feel_week')
