d1f4a6a65013c9020677bcd0eabf9d094eefa049
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 14:27:52 -0400

    "rm: go super/man_and_point.py and super/know_few_person/high_child/good_work/group/thing_or_young_group.py above old year"

---
+++ super/know_few_person/high_child/good_work/group/thing_or_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_fact_from_eye(str_arg):
+    work(str_arg)
+    print('try_work')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_fact_from_eye('first_child')
