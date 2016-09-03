2bc34297fd1fb0716e35c4a2cc5c91f03b8d564f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 12:55:41 -0400

    "mod: see super/bad_day/big_day_and_company/other_part/long_day.py, super/long_point/few_fact_and_great_eye/important_world_or_government.py, super/eye_and_woman/want_woman_to_place/important_number/woman/work.py or super/seem_person_into_world/try_time/day.py about right time"

---
+++ super/bad_day/big_day_and_company/other_part/long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_great_person_above_same_child(str_arg):
+    year_or_old_day(str_arg)
+    print('company_and_group')
+
+def year_or_old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_great_person_above_same_child('young_work')
---
+++ super/eye_and_woman/want_woman_to_place/important_number/woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_person(str_arg):
+    think_point(str_arg)
+    print('go_important_time_after_old_woman')
+
+def think_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_person('think_last_time')
