f50e2a663844ba3a66dbfbe12b052a8800fbbfb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 13:08:49 -0400

    "mix: ask super/same_place.py into new place"

---
+++ super/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    thing_and_way(str_arg)
+    print('give_week')
+
+def thing_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('few_thing')
