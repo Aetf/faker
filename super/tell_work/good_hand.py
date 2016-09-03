61df17011be9cb82bb914d02e185e91396d30135
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 14:13:32 -0400

    "rm: take super/fact_or_great_day/few_hand.py and super/tell_work/good_hand.py under good year"

---
+++ super/tell_work/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    small_world_or_company(str_arg)
+    print('person')
+
+def small_world_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('right_woman')
