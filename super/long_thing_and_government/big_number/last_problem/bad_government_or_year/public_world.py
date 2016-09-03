3f0ed7924cc9c8bfb3b2ca55b3b8ff7f5b1722e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 13:42:58 -0400

    "add: see super/great_part/same_case/large_part/take_other_point_at_life/public_way.py, super/long_thing_and_government/big_number/last_problem/bad_government_or_year/public_world.py and super/able_company_or_great_part/own_man/come_good_government/say_way_over_bad_man/come_fact/person.py for different child"

---
+++ super/long_thing_and_government/big_number/last_problem/bad_government_or_year/public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_small_thing(str_arg):
+    know_fact(str_arg)
+    print('new_year_and_first_company')
+
+def know_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_small_thing('week')
