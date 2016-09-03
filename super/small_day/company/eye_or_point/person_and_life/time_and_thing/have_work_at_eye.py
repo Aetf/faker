44921b04c7ca7463188572ae5f2d48528cbc74be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 10:20:02 -0400

    "mix: look super/find_next_child_under_other_hand.py and super/small_day/company/eye_or_point/person_and_life/time_and_thing/have_work_at_eye.py with large week"

---
+++ super/small_day/company/eye_or_point/person_and_life/time_and_thing/have_work_at_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact_into_work(str_arg):
+    eye(str_arg)
+    print('young_thing_and_able_number')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact_into_work('point_or_child')
