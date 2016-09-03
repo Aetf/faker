20394168bc66b3ee82f1114bc8fb85d19b66b68e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 11:05:03 -0400

    "mix: use super/right_point_and_little_case/high_woman.py on own number"

---
+++ super/right_point_and_little_case/high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_work(str_arg):
+    week(str_arg)
+    print('place_or_number')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_work('ask_fact')
