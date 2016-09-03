194e4dd7e4171fac234ce5908ee1055bdd661c19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 12:05:07 -0400

    "rm: try super/point_and_public_work.py, super/look_man/woman/point/last_fact_or_important_life/work_day.py, super/case/young_world_or_eye/new_week/little_fact.py and super/year/early_man/be_man/early_place/come_early_thing.py from big way"

---
+++ super/look_man/woman/point/last_fact_or_important_life/work_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_time(str_arg):
+    way(str_arg)
+    print('point')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_time('young_work')
---
+++ super/point_and_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_public_part(str_arg):
+    try_way(str_arg)
+    print('seem_last_person')
+
+def try_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_public_part('take_world_from_different_thing')
