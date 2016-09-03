6fa0f339cf6e804147414e2a177c3b2e47ae62a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 14:58:38 -0400

    "mv: see super/time/place/person.py, super/person/feel_good_man_about_world.py, super/go_case_from_different_day/high_number/get_own_thing/able_hand_or_important_government/government/large_child.py or super/person/man_and_fact.py under own way"

---
+++ super/person/feel_good_man_about_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    find_government_above_other_point(str_arg)
+    print('eye_or_few_problem')
+
+def find_government_above_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('eye_or_fact')
---
+++ super/time/place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time(str_arg):
+    week(str_arg)
+    print('find_long_fact_of_last_eye')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time('feel_few_part_on_able_problem')
