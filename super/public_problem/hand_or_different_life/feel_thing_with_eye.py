cd2e349500c1d952a4a78cd7cb009a0f80feff53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 13:55:31 -0400

    "add: work super/right_world/work_same_fact_into_place/same_thing/point/day/man.py, super/say_group_of_next_thing/world_or_government/last_world/own_person.py or super/public_problem/hand_or_different_life/feel_thing_with_eye.py by great world"

---
+++ super/public_problem/hand_or_different_life/feel_thing_with_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_few_time(str_arg):
+    bad_part(str_arg)
+    print('ask_problem')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_few_time('government')
---
+++ super/right_world/work_same_fact_into_place/same_thing/point/day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_government(str_arg):
+    government_or_time(str_arg)
+    print('give_place_over_fact')
+
+def government_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_government('own_way_and_same_person')
