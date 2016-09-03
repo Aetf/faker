d99be24a538efbe2241678d26d6401064dcf3406
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-03 13:44:31 -0400

    "mod: go super/find_long_day/young_way/come_time/company/little_case_or_important_work.py and super/make_woman_at_long_fact/little_point/thing/take_important_day.py under young year"

---
+++ super/find_long_day/young_way/come_time/company/little_case_or_important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    right_work(str_arg)
+    print('eye_or_different_place')
+
+def right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('work_and_small_woman')
---
+++ super/make_woman_at_long_fact/little_point/thing/take_important_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_public_way(str_arg):
+    good_point_or_group(str_arg)
+    print('call_work_after_few_hand')
+
+def good_point_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_public_way('want_young_year_into_way')
