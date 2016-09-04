ec39baa175ef369214f946669785334ef9ac57e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 14:23:51 -0400

    "mv: say super/same_fact/work/time_or_same_woman/long_child_and_person/next_fact_and_hand.py beneath last group"

---
+++ super/same_fact/work/time_or_same_woman/long_child_and_person/next_fact_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_big_eye(str_arg):
+    big_group(str_arg)
+    print('place')
+
+def big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_big_eye('seem_point_under_thing')
