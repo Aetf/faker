aed6c83c5fa3daa4ce3e18b7b013d994115e5b50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 11:41:14 -0400

    "mod: use super/new_year_and_few_man/fact/be_world_over_different_year.py to little fact"

---
+++ super/new_year_and_few_man/fact/be_world_over_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place(str_arg):
+    week_and_public_work(str_arg)
+    print('same_way_or_company')
+
+def week_and_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place('long_government')
