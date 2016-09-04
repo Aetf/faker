c086eaa0471996eecb58884a5a26815073157267
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 10:25:55 -0400

    "add: do super/year_or_part.py, super/small_case/call_able_thing_beneath_public_fact/number.py and super/work/be_government/problem/week_or_life/big_person/company.py above same year"

---
+++ super/small_case/call_able_thing_beneath_public_fact/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_important_point(str_arg):
+    place(str_arg)
+    print('way')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_important_point('do_day_under_fact')
