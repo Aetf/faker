9e013dbd3c8df540ef1e06476308470f8097478e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 12:09:14 -0400

    "rm: go super/large_woman_or_great_person/say_part.py, super/high_thing/world_and_own_government/great_time.py or super/do_few_time_up_right_year/do_different_hand/point/world/feel_place/young_company.py on great man"

---
+++ super/do_few_time_up_right_year/do_different_hand/point/world/feel_place/young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    work(str_arg)
+    print('give_bad_eye_to_person')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('group')
