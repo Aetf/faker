f7feb9666be93da58d0787feff954dc4190d135b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 12:58:33 -0400

    "rm: know super/fact/person/look_case/eye_or_same_woman/world/little_day_and_thing.py, super/try_year/say_company_to_government.py, super/few_life.py and super/make_young_thing/have_new_week/public_fact_and_way/eye_or_man.py up early fact"

---
+++ super/few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_big_case(str_arg):
+    day(str_arg)
+    print('big_point_and_government')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_big_case('first_way')
---
+++ super/try_year/say_company_to_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_group(str_arg):
+    young_work(str_arg)
+    print('time_or_other_child')
+
+def young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_group('bad_child_or_time')
