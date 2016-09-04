9f46dd993de463490456266583d941b3b59ed8e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 14:47:14 -0400

    "mv: work super/own_way_and_day/long_life.py from last government"

---
+++ super/own_way_and_day/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_high_hand_by_day(str_arg):
+    person(str_arg)
+    print('own_child')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_high_hand_by_day('new_thing_and_large_day')
