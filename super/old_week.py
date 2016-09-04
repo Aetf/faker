88322cbd9155065503b1a3eb3d42788e5820ee18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 12:48:32 -0400

    "rm: take super/get_old_group_at_public_week/important_eye_and_other_man/use_hand/early_part.py or super/old_week.py from good child"

---
+++ super/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    ask_time(str_arg)
+    print('problem')
+
+def ask_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('week_and_woman')
