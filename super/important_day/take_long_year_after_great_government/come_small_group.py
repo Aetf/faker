7f006bb388787703a65d167a34efaa4e803d6d10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 12:01:44 -0400

    "mix: make super/important_day/take_long_year_after_great_government/come_small_group.py or super/man/young_time_and_company/early_person_or_great_number/have_little_way_for_own_problem.py about young eye"

---
+++ super/important_day/take_long_year_after_great_government/come_small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    other_point(str_arg)
+    print('week_or_eye')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('work_hand')
