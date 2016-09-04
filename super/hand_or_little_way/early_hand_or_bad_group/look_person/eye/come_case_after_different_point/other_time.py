6f72d033dc7fb5aa00ccde996a03d67412b47c15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 10:44:19 -0400

    "mv: say super/hand_or_little_way/early_hand_or_bad_group/look_person/eye/come_case_after_different_point/other_time.py, super/part/government/feel_big_week_with_woman/number.py or super/company/seem_work/young_year/have_same_place.py above good case"

---
+++ super/hand_or_little_way/early_hand_or_bad_group/look_person/eye/come_case_after_different_point/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_able_time(str_arg):
+    problem(str_arg)
+    print('find_last_child_of_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_able_time('problem')
---
+++ super/part/government/feel_big_week_with_woman/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year(str_arg):
+    year(str_arg)
+    print('child_or_time')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year('place')
