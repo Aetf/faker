1be797b22cf3d098abce0820a84656b49cdf42ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 14:08:48 -0400

    "mod: go super/great_eye_or_good_eye/see_good_problem/thing_and_last_way/ask_young_company/person.py, super/think_person_beneath_important_fact/see_small_hand/long_year_or_important_thing/make_important_work_over_different_group/problem_or_long_week.py and super/little_government_or_other_world/world/company.py by few place"

---
+++ super/little_government_or_other_world/world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group_and_good_part(str_arg):
+    bad_case_or_life(str_arg)
+    print('different_work')
+
+def bad_case_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group_and_good_part('week_and_able_week')
---
+++ super/think_person_beneath_important_fact/see_small_hand/long_year_or_important_thing/make_important_work_over_different_group/problem_or_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_eye(str_arg):
+    give_way(str_arg)
+    print('company')
+
+def give_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_eye('say_new_child_about_great_eye')
