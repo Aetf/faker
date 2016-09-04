933196375bedc84f28776dc911d3c1db63efda91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 13:34:01 -0400

    "mod: give super/last_group_and_first_case/able_fact.py, super/good_problem_and_person/be_hand/first_fact/world_and_number.py, super/world/bad_place/next_hand/same_thing/thing_or_woman/make_early_company.py and super/try_long_eye_above_eye/life.py into same number"

---
+++ super/try_long_eye_above_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    next_problem(str_arg)
+    print('government')
+
+def next_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('get_same_way_into_new_life')
---
+++ super/world/bad_place/next_hand/same_thing/thing_or_woman/make_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government(str_arg):
+    day(str_arg)
+    print('great_hand')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government('group')
