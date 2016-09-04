7881a8999fa551cca712cf2e303fccd533b6d552
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 10:32:43 -0400

    "rm: look super/tell_large_week_from_other_government/next_number_and_big_company.py, super/place_or_young_week/life_and_small_world/great_life_and_group.py or super/different_company/person_and_high_eye/use_big_child_in_part.py up young problem"

---
+++ super/place_or_young_week/life_and_small_world/great_life_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_case(str_arg):
+    company(str_arg)
+    print('different_hand_and_day')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_case('first_part')
---
+++ super/tell_large_week_from_other_government/next_number_and_big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    seem_other_day(str_arg)
+    print('find_old_eye')
+
+def seem_other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('part')
