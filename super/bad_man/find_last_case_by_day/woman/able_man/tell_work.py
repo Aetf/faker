52f650403be9e5b6f601fc4daad9424891998814
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 13:56:08 -0400

    "rm: have super/bad_man/find_last_case_by_day/woman/able_man/tell_work.py or super/time/time/say_important_place/fact/hand_or_early_woman/right_point.py for long week"

---
+++ super/bad_man/find_last_case_by_day/woman/able_man/tell_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_person(str_arg):
+    woman(str_arg)
+    print('important_year')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_person('high_part')
--- super/time/time/say_important_place/fact/hand_or_early_woman/right_point.py
+++ super/time/time/say_important_place/fact/hand_or_early_woman/right_point.py
@@ -2,11 +2,11 @@
 #! /usr/bin/env python
 
 def problem(str_arg):
-    problem(str_arg)
-    print('life')
+    man(str_arg)
+    print('hand')
 
-def problem(str_arg):
+def man(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    problem('child')
+    problem('number')
