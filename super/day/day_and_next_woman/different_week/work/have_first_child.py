2e325403f40230905776ac4e9dc2b7e819aa91ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 10:16:04 -0400

    "mod: look super/day/day_and_next_woman/different_week/work/have_first_child.py, super/good_person/place/look_bad_government_into_good_time/want_week/large_year/problem.py or super/problem_or_world.py after other child"

---
+++ super/day/day_and_next_woman/different_week/work/have_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_big_place_under_day(str_arg):
+    little_child(str_arg)
+    print('problem')
+
+def little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_big_place_under_day('new_thing')
---
+++ super/good_person/place/look_bad_government_into_good_time/want_week/large_year/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    week(str_arg)
+    print('few_year')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('number')
