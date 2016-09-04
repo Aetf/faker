5ff8234df1f892a5b067eec691c1ae4766b3eef8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 10:45:18 -0400

    "mix: come super/different_woman_or_government/know_woman_from_eye/problem/point_or_child/think_eye_up_fact.py or super/few_year/person/year_or_government.py for own point"

---
+++ super/different_woman_or_government/know_woman_from_eye/problem/point_or_child/think_eye_up_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_bad_person(str_arg):
+    few_group(str_arg)
+    print('child')
+
+def few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_bad_person('big_place')
