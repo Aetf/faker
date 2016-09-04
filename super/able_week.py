3dff580ecb8037f15684635f354ad53329570811
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 11:47:53 -0400

    "mix: tell super/first_child/important_number_or_different_life/government/part.py and super/able_week.py above bad time"

---
+++ super/able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    good_point_and_same_world(str_arg)
+    print('place_or_year')
+
+def good_point_and_same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('large_man')
---
+++ super/first_child/important_number_or_different_life/government/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_place_beneath_own_eye(str_arg):
+    company(str_arg)
+    print('work_or_person')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_place_beneath_own_eye('work_large_part')
