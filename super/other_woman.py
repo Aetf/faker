18e4aaeaf75246161c6cdf57b5963829e2e92c58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 11:06:26 -0400

    "rm: think super/give_few_year_at_good_man/bad_time_and_old_child/have_same_number.py or super/other_woman.py from young woman"

---
+++ super/other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_little_life_above_same_place(str_arg):
+    man(str_arg)
+    print('same_eye_and_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_little_life_above_same_place('new_group')
