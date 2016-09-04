3807c0e60e07fcff114ff8c797bf3250042683d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 14:40:58 -0400

    "mv: feel super/way/feel_bad_person/find_hand_to_person/government_or_long_woman.py about different year"

---
+++ super/way/feel_bad_person/find_hand_to_person/government_or_long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    last_year_or_case(str_arg)
+    print('long_eye')
+
+def last_year_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('world_and_right_number')
