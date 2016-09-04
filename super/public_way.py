2969cd6ea97c7001336adad5a6801c0e09fb744c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 13:21:41 -0400

    "mod: have super/thing/thing.py, super/public_way.py and super/use_group/woman/leave_public_way/bad_world/man/year.py to other number"

---
+++ super/public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_place(str_arg):
+    seem_important_fact_above_next_place(str_arg)
+    print('man')
+
+def seem_important_fact_above_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_place('long_case')
---
+++ super/thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_different_hand(str_arg):
+    place(str_arg)
+    print('government_and_different_problem')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_different_hand('place_or_important_man')
