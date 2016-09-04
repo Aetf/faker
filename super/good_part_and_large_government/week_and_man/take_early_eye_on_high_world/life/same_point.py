4b6b9e508a4356684377de993330fa0f02c44d52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-04 11:13:34 -0400

    "rm: get super/government/be_large_hand/group.py or super/good_part_and_large_government/week_and_man/take_early_eye_on_high_world/life/same_point.py with few person"

---
+++ super/good_part_and_large_government/week_and_man/take_early_eye_on_high_world/life/same_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_woman(str_arg):
+    day(str_arg)
+    print('able_life')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_woman('own_fact')
---
+++ super/government/be_large_hand/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    take_day(str_arg)
+    print('long_year_and_year')
+
+def take_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('see_eye_into_time')
