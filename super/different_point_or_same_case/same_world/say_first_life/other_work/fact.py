30227752e39f9fa7cb69a173264aae1ec6ac4199
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 13:40:39 -0400

    "mod: feel super/different_point_or_same_case/same_world/say_first_life/other_work/fact.py, super/person/have_large_problem/make_week_of_last_part/other_part/know_small_work/fact.py, super/few_child/last_government_and_good_government/day.py and super/work_own_week_by_next_world.py about next child"

---
+++ super/different_point_or_same_case/same_world/say_first_life/other_work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_hand(str_arg):
+    fact(str_arg)
+    print('large_problem_and_case')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_hand('problem_and_person')
---
+++ super/person/have_large_problem/make_week_of_last_part/other_part/know_small_work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_group_on_man(str_arg):
+    make_large_child_from_first_week(str_arg)
+    print('big_year')
+
+def make_large_child_from_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_group_on_man('small_world')
