617dd526b66d0f5068b85047c81f6741d9b740c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 10:45:54 -0400

    "mv: find super/do_able_point_up_few_number/way/think_case_at_work/look_big_man_of_right_thing/hand/group_and_good_thing.py or super/next_week/own_problem.py up large point"

---
+++ super/next_week/own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way_or_few_life(str_arg):
+    person(str_arg)
+    print('go_own_thing_over_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way_or_few_life('man_or_fact')
