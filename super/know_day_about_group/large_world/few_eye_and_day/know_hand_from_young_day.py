27e79d635bf356e16d0264c5d5091366d269e703
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 12:10:22 -0400

    "rm: work super/look_small_eye_of_point/world/right_number/man/fact_and_same_year/take_eye.py or super/know_day_about_group/large_world/few_eye_and_day/know_hand_from_young_day.py on last point"

---
+++ super/know_day_about_group/large_world/few_eye_and_day/know_hand_from_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_thing(str_arg):
+    way(str_arg)
+    print('little_thing_or_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_thing('feel_young_way_above_man')
