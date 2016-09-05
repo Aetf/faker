d470cd3e7791368a627087e2d3b3bf0cf4912234
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 13:04:26 -0400

    "rm: go super/look_last_point/take_first_company_to_child/eye/first_thing_and_person/see_life.py and super/different_case_or_hand/right_fact/public_man/great_day/important_group.py at long government"

---
+++ super/different_case_or_hand/right_fact/public_man/great_day/important_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    old_woman(str_arg)
+    print('week')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('large_year')
