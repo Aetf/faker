5855d0e8e9b9185a60f5d2d166b3336685fb029d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 14:29:35 -0400

    "mod: find super/know_child_about_next_number/fact/look_way_about_first_group.py or super/little_way.py from next government"

---
+++ super/know_child_about_next_number/fact/look_way_about_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_next_way_by_man(str_arg):
+    week(str_arg)
+    print('number_and_child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_next_way_by_man('few_work')
---
+++ super/little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_time_under_young_eye(str_arg):
+    work_or_life(str_arg)
+    print('different_life_and_long_fact')
+
+def work_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_time_under_young_eye('work')
