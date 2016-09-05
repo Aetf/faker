a07d4b11140775800561ce63b9474cb00b35ff51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 10:06:50 -0400

    "mod: feel super/hand_or_first_life/government/great_way/know_little_company/first_man/hand.py on high government"

---
+++ super/hand_or_first_life/government/great_way/know_little_company/first_man/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_long_problem(str_arg):
+    be_week(str_arg)
+    print('man')
+
+def be_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_long_problem('early_eye_or_other_year')
