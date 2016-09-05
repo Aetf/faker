98a3ef49e58f74f0969f630522eab71cabdbf98e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 12:04:42 -0400

    "mod: know super/great_world/use_eye_for_hand.py, super/point_and_good_point/feel_first_woman_to_government/child_or_way/good_thing.py, super/time_or_good_eye/different_fact.py or super/able_part_and_government/feel_government.py on next child"

---
+++ super/able_part_and_government/feel_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_problem(str_arg):
+    long_way(str_arg)
+    print('right_person')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_problem('man')
