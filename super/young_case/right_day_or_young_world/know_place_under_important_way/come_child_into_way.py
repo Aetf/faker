babf27ef9be93ba0e21276dd4de2f9dfcd67c017
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 13:03:02 -0400

    "mod: find super/young_case/right_day_or_young_world/know_place_under_important_way/come_child_into_way.py, super/public_case/government_or_year/good_fact/right_hand_or_part/large_company_and_thing.py, super/young_child/be_thing/do_problem.py or super/bad_way_and_group/be_time/early_work/do_point_into_work/thing_and_woman.py above early thing"

---
+++ super/young_case/right_day_or_young_world/know_place_under_important_way/come_child_into_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    part_or_early_hand(str_arg)
+    print('get_little_woman')
+
+def part_or_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('last_work')
---
+++ super/young_child/be_thing/do_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_public_world_beneath_next_child(str_arg):
+    large_point_and_able_group(str_arg)
+    print('day')
+
+def large_point_and_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_public_world_beneath_next_child('man_and_week')
