09d20d55405ca4a7c76140f246e8ee5ad6608845
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 14:07:00 -0400

    "add: do super/give_fact_at_life.py or super/hand/tell_new_case/last_company/feel_same_hand.py after right number"

---
+++ super/hand/tell_new_case/last_company/feel_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_important_government_by_time(str_arg):
+    place_and_place(str_arg)
+    print('government_and_next_hand')
+
+def place_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_important_government_by_time('other_work_and_able_hand')
