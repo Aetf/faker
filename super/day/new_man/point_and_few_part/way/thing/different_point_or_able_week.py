8b73659eaf9d202028802cb97489541816007487
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 14:48:52 -0400

    "add: go super/day/new_man/point_and_few_part/way/thing/different_point_or_able_week.py, super/hand.py or super/problem_and_old_government/person_or_government/fact/year.py for little company"

---
+++ super/day/new_man/point_and_few_part/way/thing/different_point_or_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    world(str_arg)
+    print('small_world')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('young_day_and_thing')
---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    try_day(str_arg)
+    print('know_part')
+
+def try_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('try_own_life_on_small_time')
