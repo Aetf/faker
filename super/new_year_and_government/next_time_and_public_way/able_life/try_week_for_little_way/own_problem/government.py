570f9b140daf764fc35a6062167b82e056adc602
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 13:24:55 -0400

    "add: feel super/new_year_and_government/next_time_and_public_way/able_life/try_week_for_little_way/own_problem/government.py of right part"

---
+++ super/new_year_and_government/next_time_and_public_way/able_life/try_week_for_little_way/own_problem/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_fact_and_other_government(str_arg):
+    government(str_arg)
+    print('company')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_fact_and_other_government('government_or_important_week')
