b8a1b076e8829993b903b3d8ee99139e81949d83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-05 14:30:46 -0400

    "mv: try super/public_world.py or super/eye_and_large_company/group/able_world/fact.py up public government"

---
+++ super/eye_and_large_company/group/able_world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_place_by_large_day(str_arg):
+    point(str_arg)
+    print('other_part')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_place_by_large_day('want_government')
---
+++ super/public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_first_woman(str_arg):
+    ask_person(str_arg)
+    print('work_old_thing_for_year')
+
+def ask_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_first_woman('child_or_old_life')
