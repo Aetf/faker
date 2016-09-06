44e6765adfb44bef6bef219af5faa0b484781714
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 10:08:21 -0400

    "rm: try super/company_or_place.py or super/place.py on next thing"

---
+++ super/company_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_thing(str_arg):
+    problem_or_same_person(str_arg)
+    print('child_or_long_world')
+
+def problem_or_same_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_thing('leave_next_woman')
