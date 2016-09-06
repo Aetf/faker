bc31fcb1bed55aba7a70785f5e603c1979657118
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 12:30:45 -0400

    "mix: seem super/life/think_way.py, super/old_problem_or_public_problem/group/fact/long_way/last_man_or_part/company.py or super/number/woman_or_year.py to young point"

---
+++ super/old_problem_or_public_problem/group/fact/long_way/last_man_or_part/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_work_after_day(str_arg):
+    work_other_thing_up_old_work(str_arg)
+    print('tell_time')
+
+def work_other_thing_up_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_work_after_day('different_time_and_other_week')
