e89848e92a8f424c5c5130d2d0cd358edee597ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 11:56:17 -0400

    "mix: find super/way_or_great_hand.py or super/day/great_world/ask_child.py beneath good life"

---
+++ super/day/great_world/ask_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_group_up_government(str_arg):
+    child(str_arg)
+    print('way')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_group_up_government('be_part')
---
+++ super/way_or_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_problem(str_arg):
+    feel_government(str_arg)
+    print('able_world_and_public_way')
+
+def feel_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_problem('do_own_time')
