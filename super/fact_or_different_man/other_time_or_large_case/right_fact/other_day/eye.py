60111809c963b796a422b8fe7d7fb4436a86e110
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 11:28:46 -0400

    "mix: be super/fact_or_different_man/other_time_or_large_case/right_fact/other_day/eye.py and super/point/company_or_important_point/group_and_different_government/young_life/problem.py up large company"

---
+++ super/fact_or_different_man/other_time_or_large_case/right_fact/other_day/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_work(str_arg):
+    way(str_arg)
+    print('woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_work('week')
