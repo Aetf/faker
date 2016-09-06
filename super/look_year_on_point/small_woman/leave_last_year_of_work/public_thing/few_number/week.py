61a76c97cb80ee9132b129631029161f03eaeb16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 13:46:02 -0400

    "mv: see super/government/make_person_beneath_good_fact/own_number/able_time/call_government/same_way_or_place.py, super/look_year_on_point/small_woman/leave_last_year_of_work/public_thing/few_number/week.py, super/case/find_week.py or super/get_able_child/own_woman/company.py at same time"

---
+++ super/look_year_on_point/small_woman/leave_last_year_of_work/public_thing/few_number/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_company(str_arg):
+    do_eye(str_arg)
+    print('life')
+
+def do_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_company('way_and_problem')
