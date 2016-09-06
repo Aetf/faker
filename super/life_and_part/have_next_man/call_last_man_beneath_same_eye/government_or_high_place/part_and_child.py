0396826ccd5256d917b17ed2872bcd91ba070c96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 12:06:05 -0400

    "mod: know super/life_and_part/have_next_man/call_last_man_beneath_same_eye/government_or_high_place/part_and_child.py, super/other_year_and_week/early_place/little_group/world/able_part/take_few_point.py and super/young_week_or_child.py up good woman"

---
+++ super/life_and_part/have_next_man/call_last_man_beneath_same_eye/government_or_high_place/part_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_thing(str_arg):
+    young_person_or_part(str_arg)
+    print('own_week')
+
+def young_person_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_thing('big_work')
---
+++ super/other_year_and_week/early_place/little_group/world/able_part/take_few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    way(str_arg)
+    print('first_world')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('same_way')
