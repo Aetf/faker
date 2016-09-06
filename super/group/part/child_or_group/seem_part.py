b59199f3fcd003c42968969e3e820076fa7bdd4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 14:28:35 -0400

    "mod: see super/group/part/child_or_group/seem_part.py or super/new_child_or_large_group/child_or_bad_world/little_way_and_long_world/place.py to last hand"

---
+++ super/group/part/child_or_group/seem_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person(str_arg):
+    go_different_part(str_arg)
+    print('big_work')
+
+def go_different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person('young_part_and_other_company')
