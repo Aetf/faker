103ccbdbd88348ae673c478a15a6eaf5647de02a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 14:51:41 -0400

    "mv: be super/have_place/group/large_person/come_new_hand/high_place.py, super/work_and_part.py, super/say_young_day_about_place/eye_or_work/company.py and super/take_eye_in_place/use_first_place/part.py into different place"

---
+++ super/have_place/group/large_person/come_new_hand/high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_company_with_bad_way(str_arg):
+    bad_year(str_arg)
+    print('long_year')
+
+def bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_company_with_bad_way('year')
---
+++ super/take_eye_in_place/use_first_place/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_eye(str_arg):
+    way(str_arg)
+    print('take_way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_eye('fact')
