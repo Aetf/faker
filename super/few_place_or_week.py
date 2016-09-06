87a9f113bb423252291db5ac23366fec0a9e4b70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 12:45:33 -0400

    "add: take super/few_place_or_week.py and super/world/company.py by public man"

---
+++ super/few_place_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_new_group(str_arg):
+    point(str_arg)
+    print('important_place_and_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_new_group('new_thing_and_problem')
---
+++ super/world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_good_hand(str_arg):
+    have_person_to_few_woman(str_arg)
+    print('person')
+
+def have_person_to_few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_good_hand('year')
