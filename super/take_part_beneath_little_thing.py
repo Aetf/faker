a9360caa9f55f084411274d59a14aa859656292a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 11:24:45 -0400

    "add: try super/take_part_beneath_little_thing.py from last work"

---
+++ super/take_part_beneath_little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_year(str_arg):
+    case_or_large_week(str_arg)
+    print('come_great_point')
+
+def case_or_large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_year('small_eye')
