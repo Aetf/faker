9cc32bbfead2af8a55223eb3ee538067972946e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 12:38:42 -0400

    "add: give super/think_same_person.py, super/new_person.py, super/come_time_at_small_day.py or super/young_person/hand_and_great_fact/different_way/little_place/think_few_time/big_part.py under big time"

---
+++ super/come_time_at_small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_company_in_day(str_arg):
+    long_world(str_arg)
+    print('old_life_or_early_person')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_company_in_day('new_day')
---
+++ super/think_same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    government(str_arg)
+    print('know_woman_of_first_problem')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('early_year')
