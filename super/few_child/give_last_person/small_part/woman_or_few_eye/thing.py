30bbcf10d8c44b7c78aac09a34b8f464899d78d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 11:33:53 -0400

    "rm: know super/same_woman.py, super/few_child/give_last_person/small_part/woman_or_few_eye/thing.py, super/woman_and_good_number/work_or_important_week.py and super/have_point_beneath_year/little_man/group/place/other_fact.py in old government"

---
+++ super/few_child/give_last_person/small_part/woman_or_few_eye/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_number_after_government(str_arg):
+    day(str_arg)
+    print('number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_number_after_government('work')
---
+++ super/woman_and_good_number/work_or_important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work(str_arg):
+    hand_and_small_life(str_arg)
+    print('own_way_or_good_case')
+
+def hand_and_small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work('government_and_world')
