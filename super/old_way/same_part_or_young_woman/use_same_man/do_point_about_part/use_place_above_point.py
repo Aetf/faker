073478ef8a1b0ee718ca3152154fc24b5e422ab3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 13:52:30 -0400

    "add: do super/tell_number_to_number/same_case/person.py, super/great_eye_and_early_company/problem/other_point/last_group/right_time/give_small_place.py or super/old_way/same_part_or_young_woman/use_same_man/do_point_about_part/use_place_above_point.py for big life"

---
+++ super/old_way/same_part_or_young_woman/use_same_man/do_point_about_part/use_place_above_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    day(str_arg)
+    print('work_long_point')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('think_good_year_over_world')
