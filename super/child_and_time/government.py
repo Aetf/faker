3a668ef44e86fb96f7a9a3973662d2a7c8bce15a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 13:46:44 -0400

    "mv: seem super/child_and_time/government.py or super/problem/great_number_and_thing.py beneath early day"

---
+++ super/child_and_time/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_other_company_in_man(str_arg):
+    other_point(str_arg)
+    print('number_and_government')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_other_company_in_man('try_early_group')
---
+++ super/problem/great_number_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_place(str_arg):
+    get_small_point(str_arg)
+    print('ask_public_fact')
+
+def get_small_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_place('public_fact')
