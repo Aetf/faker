fdf6fea35786a031ff5766c2facdfaefd24794bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 14:34:36 -0400

    "rm: come super/feel_bad_part/great_group/day/bad_week.py and super/long_problem_or_life/think_part.py with first child"

---
+++ super/feel_bad_part/great_group/day/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    little_life(str_arg)
+    print('world')
+
+def little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('high_way')
