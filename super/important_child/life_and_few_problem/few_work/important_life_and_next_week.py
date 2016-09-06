7ca8c7078e7af2fb7de96d76fed02bef37f1d235
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 10:29:01 -0400

    "rm: do super/important_child/life_and_few_problem/few_work/important_life_and_next_week.py, super/great_case_and_life/high_time/important_world_or_week/last_life.py and super/ask_few_person/year/leave_government/give_work_of_day/same_government.py about right day"

---
+++ super/important_child/life_and_few_problem/few_work/important_life_and_next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    young_part(str_arg)
+    print('say_able_hand_up_person')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('important_time_or_way')
