ab5a5cf5eafce4ff399472c06ef65c03714f1496
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 13:01:55 -0400

    "rm: be super/important_life_or_own_time/first_eye/old_company_or_first_way/place/know_bad_point_after_part.py, super/day/place/group.py or super/big_time/bad_world_or_work/give_next_person/early_case.py by good person"

---
+++ super/day/place/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_first_number(str_arg):
+    week(str_arg)
+    print('year')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_first_number('number')
---
+++ super/important_life_or_own_time/first_eye/old_company_or_first_way/place/know_bad_point_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point(str_arg):
+    early_fact(str_arg)
+    print('know_world_from_big_case')
+
+def early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point('own_fact_and_man')
