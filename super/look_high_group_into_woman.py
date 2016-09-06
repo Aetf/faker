d528eaf0f9c4f92a15cf2255257169cf0e06dd26
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-06 11:06:27 -0400

    "mod: ask super/look_high_group_into_woman.py or super/small_fact/week/little_government/problem_and_own_case/same_way.py by last point"

---
+++ super/look_high_group_into_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    fact_and_good_place(str_arg)
+    print('want_hand_beneath_child')
+
+def fact_and_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('problem')
