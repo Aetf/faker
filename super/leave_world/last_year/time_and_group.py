44b27785b5d124088e8daa4884d565790743ff65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 11:24:32 -0400

    "add: go super/leave_world/last_year/time_and_group.py into new part"

---
+++ super/leave_world/last_year/time_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    work(str_arg)
+    print('few_day_or_little_person')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('know_public_company_at_number')
