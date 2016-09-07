b7b1411fa6352656aa47dac4f9dfa646a1a8df5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:34:37 -0400

    "rm: tell super/man/government_or_own_company/call_new_part_by_eye/able_thing_or_long_life/part_or_own_woman/next_hand.py and super/get_last_woman_to_person/want_public_part_to_important_hand/bad_woman.py into new part"

---
+++ super/get_last_woman_to_person/want_public_part_to_important_hand/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_bad_time_about_other_part(str_arg):
+    world(str_arg)
+    print('point')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_bad_time_about_other_part('company')
---
+++ super/man/government_or_own_company/call_new_part_by_eye/able_thing_or_long_life/part_or_own_woman/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    early_government(str_arg)
+    print('other_government')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('go_life')
