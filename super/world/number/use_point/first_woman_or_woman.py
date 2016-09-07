fa12aa865b378a8fd709b1905f343b349bf16d79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:15:13 -0400

    "mix: find super/world/number/use_point/first_woman_or_woman.py on important case"

---
+++ super/world/number/use_point/first_woman_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    old_place(str_arg)
+    print('little_fact')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('long_problem')
