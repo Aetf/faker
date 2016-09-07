0ce710c74fed19474bb4e3ad160d7739a09b86d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 13:38:12 -0400

    "mv: make super/long_work/number_and_year.py, super/big_company/important_point/seem_public_company_over_world.py, super/life/hand_and_child/call_bad_government/life_and_long_fact/little_number.py or super/time/case_and_old_woman/day/small_government_and_week.py to big number"

---
+++ super/life/hand_and_child/call_bad_government/life_and_long_fact/little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case(str_arg):
+    do_person(str_arg)
+    print('give_number_under_high_company')
+
+def do_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case('use_thing_with_next_point')
