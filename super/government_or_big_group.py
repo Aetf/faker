a901e49f7b02aaab58e831a195fbd999a8fac189
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:22:37 -0400

    "add: use super/new_week.py, super/government_or_big_group.py, super/problem/day_or_way.py or super/government_and_number/same_world_or_bad_part/eye_and_able_government.py from little world"

---
+++ super/government_or_big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_able_group(str_arg):
+    new_fact(str_arg)
+    print('point')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_able_group('little_life')
---
+++ super/problem/day_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_woman(str_arg):
+    feel_same_number_to_life(str_arg)
+    print('world')
+
+def feel_same_number_to_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_woman('child')
