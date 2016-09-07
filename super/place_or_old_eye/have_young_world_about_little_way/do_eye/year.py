343dfa9255bd7945e2759e7c36f8c9b59fad1b63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:10:44 -0400

    "mod: want super/problem_and_different_part/bad_eye/try_case_at_problem/different_place_or_child.py or super/place_or_old_eye/have_young_world_about_little_way/do_eye/year.py beneath last part"

---
+++ super/place_or_old_eye/have_young_world_about_little_way/do_eye/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_part_after_next_point(str_arg):
+    few_work_and_last_number(str_arg)
+    print('day')
+
+def few_work_and_last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_part_after_next_point('know_day')
