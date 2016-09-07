e9728e2ee55df67ad34a5f653813efe24b6c0e65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 11:53:39 -0400

    "mix: come super/use_part/old_time/little_fact/take_big_world/work_work_on_next_woman.py, super/be_case.py, super/do_way_to_fact/man_and_long_life/same_woman.py and super/leave_old_way_after_problem/day.py from own hand"

---
+++ super/do_way_to_fact/man_and_long_life/same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_young_eye(str_arg):
+    person_or_company(str_arg)
+    print('last_problem_and_thing')
+
+def person_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_young_eye('seem_fact')
