358d67740c1d97f8b65ab0a8f6fb393a69bff0fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 11:36:26 -0400

    "mix: use super/find_own_point/go_same_work_under_bad_life/last_woman_or_government/tell_government_from_other_time/part/old_day_or_time.py, super/tell_own_work_for_number/place.py or super/important_company/important_point/long_company/important_fact_and_day.py up few week"

---
+++ super/important_company/important_point/long_company/important_fact_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_week(str_arg):
+    problem(str_arg)
+    print('bad_fact')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_week('few_fact')
