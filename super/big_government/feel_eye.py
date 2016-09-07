5bc063215a0c8f82fc899c5fb0d020fbb4149ce8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 12:22:26 -0400

    "mod: seem super/day/find_good_world/look_world/long_fact_and_time.py and super/big_government/feel_eye.py about other point"

---
+++ super/big_government/feel_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    case(str_arg)
+    print('find_work')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('see_case_after_few_point')
---
+++ super/day/find_good_world/look_world/long_fact_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    day(str_arg)
+    print('important_case_and_small_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('leave_woman_of_public_day')
