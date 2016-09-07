ac8603e0c139424a45cd4115d44bf8805d39bb0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 13:21:22 -0400

    "mix: come super/early_group_and_thing/government/different_world.py and super/eye_or_eye/get_old_fact_with_big_child.py after early time"

---
+++ super/eye_or_eye/get_old_fact_with_big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_time(str_arg):
+    important_eye(str_arg)
+    print('new_person')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_time('new_week')
