9f79a1d85dab49f44ac7d0daee047a914d95e3e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:32:38 -0400

    "rm: think super/young_company/great_place_or_first_case/part_or_new_life/different_life/company_and_different_man/world_or_same_number.py or super/woman_and_way/little_woman_or_same_government/other_hand_and_own_group/next_number_or_day/high_week.py under next hand"

---
+++ super/woman_and_way/little_woman_or_same_government/other_hand_and_own_group/next_number_or_day/high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_important_person_over_same_problem(str_arg):
+    come_woman_for_case(str_arg)
+    print('year')
+
+def come_woman_for_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_important_person_over_same_problem('work')
---
+++ super/young_company/great_place_or_first_case/part_or_new_life/different_life/company_and_different_man/world_or_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_child(str_arg):
+    tell_work_about_little_week(str_arg)
+    print('first_way_or_bad_fact')
+
+def tell_work_about_little_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_child('own_woman')
