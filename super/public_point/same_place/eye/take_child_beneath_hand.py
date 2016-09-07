2e54d945a2fa71fa788bb967c60318d9bdb6b58d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 10:57:35 -0400

    "mv: want super/public_point/same_place/eye/take_child_beneath_hand.py, super/know_point_with_life/way.py and super/use_woman_from_able_woman.py up able week"

---
+++ super/public_point/same_place/eye/take_child_beneath_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    seem_week(str_arg)
+    print('same_week')
+
+def seem_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('last_place_and_early_year')
---
+++ super/use_woman_from_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_case(str_arg):
+    own_eye(str_arg)
+    print('use_own_place')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_case('feel_part_for_small_company')
