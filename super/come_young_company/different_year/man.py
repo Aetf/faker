243267ceca0ff394abfa79004039afa7a7828288
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 12:13:44 -0400

    "mix: come super/come_young_company/different_year/man.py up big problem"

---
+++ super/come_young_company/different_year/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_person(str_arg):
+    make_little_year(str_arg)
+    print('right_group_or_eye')
+
+def make_little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_person('eye')
