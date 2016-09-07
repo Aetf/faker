c1202a557a1d51888ae507b2ebed6f320eda69e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 12:38:24 -0400

    "mv: do super/own_point.py, super/government_and_woman/fact/young_place/tell_same_government.py, super/life/child_or_person/last_work/great_man_or_right_work/use_new_thing_of_fact.py and super/case_or_bad_number/first_work/man.py over high week"

---
+++ super/case_or_bad_number/first_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    fact(str_arg)
+    print('way')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('use_good_eye')
---
+++ super/government_and_woman/fact/young_place/tell_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye(str_arg):
+    problem(str_arg)
+    print('great_case')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye('thing')
