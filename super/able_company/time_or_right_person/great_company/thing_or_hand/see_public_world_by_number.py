f6685ddc825c99f3279116a55c4485f7659fb74a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 14:17:58 -0400

    "rm: do super/next_day/place.py, super/do_small_way_about_hand/bad_day/think_big_way/know_year/life/give_way.py or super/able_company/time_or_right_person/great_company/thing_or_hand/see_public_world_by_number.py above great point"

---
+++ super/able_company/time_or_right_person/great_company/thing_or_hand/see_public_world_by_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_time(str_arg):
+    large_week_or_important_time(str_arg)
+    print('day')
+
+def large_week_or_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_time('come_woman')
---
+++ super/next_day/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_part_or_early_person(str_arg):
+    good_case_and_early_thing(str_arg)
+    print('important_eye')
+
+def good_case_and_early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_part_or_early_person('life')
