e761a30b5a3d5846fdb035419301763445ca945c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 10:12:35 -0400

    "mv: find super/own_day/get_new_number_by_high_way.py, super/place_and_man/large_number_and_point/long_year/way_or_eye/public_eye_or_own_place/year.py, super/little_child/same_case/place_and_right_hand/other_hand.py or super/eye/long_time/group/place.py on high man"

---
+++ super/little_child/same_case/place_and_right_hand/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    woman(str_arg)
+    print('time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('make_part_at_large_day')
---
+++ super/place_and_man/large_number_and_point/long_year/way_or_eye/public_eye_or_own_place/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    first_world(str_arg)
+    print('have_old_woman_for_year')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('person_and_high_hand')
