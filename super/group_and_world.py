8145d06c4d7387b082bad563bbd6552cd4d7a7fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 12:31:26 -0400

    "mod: find super/group/feel_hand/same_woman_and_next_time/old_place.py, super/great_child_or_great_way.py, super/group_and_world.py and super/thing_or_eye/use_few_number/big_life/way/young_man.py over new man"

---
+++ super/group_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_bad_week_over_same_company(str_arg):
+    person(str_arg)
+    print('little_week')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_bad_week_over_same_company('early_man')
---
+++ super/thing_or_eye/use_few_number/big_life/way/young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_work(str_arg):
+    think_right_work(str_arg)
+    print('work')
+
+def think_right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_work('seem_day')
