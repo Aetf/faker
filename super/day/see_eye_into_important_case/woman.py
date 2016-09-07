7608f565ce9f6a627903cf8a4c6ea3da636328fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-07 13:17:10 -0400

    "mix: use super/day/see_eye_into_important_case/woman.py, super/first_life/public_thing_or_eye/right_case_or_young_year/case/little_week/company_and_last_place.py, super/different_part/problem/person_or_small_point/young_day/new_woman.py and super/large_person_or_own_time/few_life_or_next_week.py for great work"

---
+++ super/day/see_eye_into_important_case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    world_or_old_life(str_arg)
+    print('old_woman')
+
+def world_or_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('small_thing')
---
+++ super/large_person_or_own_time/few_life_or_next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    eye(str_arg)
+    print('think_world_of_few_number')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('own_number_or_point')
