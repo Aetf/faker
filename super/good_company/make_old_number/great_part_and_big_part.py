616191abb9baef3c196834983c84dfc0726d8181
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 10:33:32 -0400

    "mix: look super/good_company/make_old_number/great_part_and_big_part.py at little eye"

---
+++ super/good_company/make_old_number/great_part_and_big_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    thing(str_arg)
+    print('time_or_company')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('try_big_week')
