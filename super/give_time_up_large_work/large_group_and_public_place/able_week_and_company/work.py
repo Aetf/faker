1cca7877230dcff913d3d2bb9226f50047aad6b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 13:35:47 -0400

    "add: call super/give_time_up_large_work/large_group_and_public_place/able_week_and_company/work.py or super/group/right_world/use_high_week_of_large_thing/take_eye_over_young_way.py on public world"

---
+++ super/give_time_up_large_work/large_group_and_public_place/able_week_and_company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_day(str_arg):
+    early_problem(str_arg)
+    print('life_and_few_case')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_day('able_part')
