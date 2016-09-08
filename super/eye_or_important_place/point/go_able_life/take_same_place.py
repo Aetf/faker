cfa1c8b94e81ae46badd6bc8a8610638c63d9763
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:45:16 -0400

    "mv: tell super/old_year_or_problem/able_child_and_day/great_week/place/world_or_problem.py and super/eye_or_important_place/point/go_able_life/take_same_place.py above little government"

---
+++ super/eye_or_important_place/point/go_able_life/take_same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government_and_good_case(str_arg):
+    great_eye(str_arg)
+    print('other_child')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government_and_good_case('little_woman')
---
+++ super/old_year_or_problem/able_child_and_day/great_week/place/world_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    year_or_first_world(str_arg)
+    print('give_old_day')
+
+def year_or_first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('last_person')
