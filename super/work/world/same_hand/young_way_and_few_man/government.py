4b9bfee832637dcd4d4f6d4e0dd5ffd536cd013b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 12:57:45 -0400

    "mv: know super/fact/large_year_or_number/next_part.py, super/work/world/same_hand/young_way_and_few_man/government.py, super/new_work/say_big_world_about_same_part/give_life/early_hand.py or super/come_public_life/company/see_good_year/small_day_and_eye/other_world.py above early eye"

---
+++ super/work/world/same_hand/young_way_and_few_man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    case_and_next_number(str_arg)
+    print('early_group')
+
+def case_and_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('week')
