a231c81827b7ac2d3e5116fae123dc362045d63e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:44:09 -0400

    "mod: find super/use_young_fact_on_child/eye/number.py, super/different_man.py or super/man/work_day_from_place.py on long week"

---
+++ super/different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_young_man(str_arg):
+    good_number_and_good_time(str_arg)
+    print('right_number')
+
+def good_number_and_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_young_man('go_important_company')
---
+++ super/use_young_fact_on_child/eye/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_same_man(str_arg):
+    have_woman(str_arg)
+    print('able_fact_and_world')
+
+def have_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_same_man('thing')
