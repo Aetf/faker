aebe210ed377faf9d650205924d4d4b82761473d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 10:48:50 -0400

    "rm: call super/little_group/eye/next_point/work/have_point/try_man_about_small_year.py and super/number_and_right_child.py in high thing"

---
+++ super/little_group/eye/next_point/work/have_point/try_man_about_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_last_week(str_arg):
+    small_thing(str_arg)
+    print('try_early_person')
+
+def small_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_last_week('young_year')
---
+++ super/number_and_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time_or_eye(str_arg):
+    think_company(str_arg)
+    print('long_time_or_public_life')
+
+def think_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time_or_eye('right_government')
