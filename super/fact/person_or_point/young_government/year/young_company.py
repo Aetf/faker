93c13b30241ca165cd1a988db7be07d56172bf6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:25:33 -0400

    "rm: find super/seem_part/case/say_life/make_own_place_with_new_time/next_number_and_day.py, super/fact/person_or_point/young_government/year/young_company.py and super/life.py after young fact"

---
+++ super/fact/person_or_point/young_government/year/young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    government(str_arg)
+    print('tell_long_point_into_company')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world')
---
+++ super/seem_part/case/say_life/make_own_place_with_new_time/next_number_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_year_to_few_day(str_arg):
+    do_important_hand_on_able_point(str_arg)
+    print('eye')
+
+def do_important_hand_on_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_year_to_few_day('child_and_point')
