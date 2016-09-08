621a4e5f2c98e0362b1713c30961b2f2506dd92c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:23:31 -0400

    "mod: come super/next_company.py, super/government_and_young_case/company_or_eye/own_life/point_or_last_time.py and super/know_young_problem/take_early_place_with_important_child/long_day.py into different point"

---
+++ super/government_and_young_case/company_or_eye/own_life/point_or_last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_part(str_arg):
+    right_time_and_eye(str_arg)
+    print('case_or_little_woman')
+
+def right_time_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_part('woman')
---
+++ super/know_young_problem/take_early_place_with_important_child/long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_government(str_arg):
+    large_person(str_arg)
+    print('thing_and_high_person')
+
+def large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_government('way_and_hand')
