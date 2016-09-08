277bb97e8b703559ef74da25c5455b82e642e932
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 14:06:36 -0400

    "mix: know super/eye/company/right_eye_and_work.py, super/group_and_public_problem/large_government_and_little_government.py, super/part_and_early_company/problem/public_time/bad_day.py and super/early_thing_and_new_day/problem.py with large point"

---
+++ super/early_thing_and_new_day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_big_company_under_next_woman(str_arg):
+    man(str_arg)
+    print('thing')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_big_company_under_next_woman('ask_new_eye')
