b62c1dcf76d4ac7d6348747a0dec1f8a862d991b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 12:39:39 -0400

    "rm: give super/come_day/way/high_world/high_group_or_eye/number/early_day.py, super/get_person/bad_week/different_hand/point.py, super/next_problem.py or super/tell_place_for_good_eye/right_day/work/know_part_after_hand/work_problem_above_last_part.py on long eye"

---
+++ super/come_day/way/high_world/high_group_or_eye/number/early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact(str_arg):
+    week(str_arg)
+    print('old_problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact('thing_and_person')
---
+++ super/next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_work_to_eye(str_arg):
+    day_and_woman(str_arg)
+    print('get_week')
+
+def day_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_work_to_eye('fact_or_case')
