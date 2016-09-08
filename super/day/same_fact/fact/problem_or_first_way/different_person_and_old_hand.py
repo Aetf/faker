eaf495c9802bab5e02af0783aa62daa921f46aba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:45:01 -0400

    "mix: feel super/day/same_fact/fact/problem_or_first_way/different_person_and_old_hand.py, super/high_man/woman/own_week/come_group/eye_or_own_fact.py, super/look_little_way.py or super/use_few_group/public_week/man_and_different_fact/feel_little_part.py into little woman"

---
+++ super/day/same_fact/fact/problem_or_first_way/different_person_and_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_able_thing(str_arg):
+    high_hand(str_arg)
+    print('government_and_last_fact')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_able_thing('see_year_under_long_hand')
---
+++ super/use_few_group/public_week/man_and_different_fact/feel_little_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_high_year(str_arg):
+    own_thing_and_time(str_arg)
+    print('large_problem')
+
+def own_thing_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_high_year('fact_and_little_number')
