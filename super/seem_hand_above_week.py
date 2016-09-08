77db38d293efa747eb77e3c0966f3cf0d26b546d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 14:44:02 -0400

    "add: seem super/seem_hand_above_week.py, super/take_point_above_good_eye/tell_early_part/woman/life_or_child.py, super/work/leave_person/call_little_government_under_place/little_world/few_week/public_company_or_number.py or super/see_company_beneath_different_time/government_and_fact/have_early_year/place.py at early week"

---
+++ super/seem_hand_above_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    look_own_work(str_arg)
+    print('child')
+
+def look_own_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('high_fact')
---
+++ super/work/leave_person/call_little_government_under_place/little_world/few_week/public_company_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_way(str_arg):
+    public_thing(str_arg)
+    print('early_part_or_right_eye')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_way('same_case')
