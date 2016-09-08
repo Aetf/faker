8774bffe7f13885d35a59f276fc3a8deb0db15e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 14:05:40 -0400

    "rm: see super/do_few_case_on_eye/thing/number_or_right_problem/problem.py and super/large_point_or_man/next_child/bad_child.py under able group"

---
+++ super/large_point_or_man/next_child/bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    point(str_arg)
+    print('want_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('day_or_way')
