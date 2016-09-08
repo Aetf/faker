6b0cc5a6f81fbfe13d0ab5f53cb7ff22fde10f8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 14:46:47 -0400

    "rm: be super/woman_and_company/next_day/government.py and super/get_first_person_into_right_child/early_eye_and_bad_child/know_young_company_above_other_life/point.py beneath next government"

---
+++ super/woman_and_company/next_day/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_big_problem(str_arg):
+    problem(str_arg)
+    print('early_company')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_big_problem('case')
