1ae1182544945f618a6ba70211a93cef854c79ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 13:11:10 -0400

    "add: be super/case/number/small_work.py, super/early_number/year_and_few_government/problem_or_work/give_person_over_great_group/problem_and_hand.py or super/time/bad_hand_and_group.py by long hand"

---
+++ super/case/number/small_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    eye_and_man(str_arg)
+    print('other_government')
+
+def eye_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_life')
