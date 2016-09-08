65285710299fe35be25ed5cce4fcc3e6b317ad0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 10:56:11 -0400

    "rm: think super/group/hand_and_own_world/thing/different_person/look_place_for_man/case.py or super/problem_and_little_thing/own_hand.py to right part"

---
+++ super/group/hand_and_own_world/thing/different_person/look_place_for_man/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man_or_day(str_arg):
+    few_child(str_arg)
+    print('place')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man_or_day('other_thing')
