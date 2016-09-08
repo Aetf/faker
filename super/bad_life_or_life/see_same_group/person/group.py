d689145fb67942823efecec1ee52d15e5684ead2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 12:51:14 -0400

    "mod: know super/bad_life_or_life/see_same_group/person/group.py in good work"

---
+++ super/bad_life_or_life/see_same_group/person/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    early_company_and_young_person(str_arg)
+    print('give_eye')
+
+def early_company_and_young_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('ask_point')
