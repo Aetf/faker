29297ce89f0665127cfda1946cc12e2c4fea590a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-08 11:07:25 -0400

    "mod: give super/small_week_or_place/year/go_good_government/person/hand.py and super/own_problem_or_case/long_week_or_public_work/world.py after own problem"

---
+++ super/own_problem_or_case/long_week_or_public_work/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_day(str_arg):
+    go_hand(str_arg)
+    print('long_man')
+
+def go_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_day('number')
