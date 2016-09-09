a4d3b6bbef7529cea5af3de8f19d0904c19f2ea2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 14:00:59 -0400

    "add: work super/give_fact_with_good_place/eye/able_way.py or super/tell_hand/government_or_little_person/life_and_bad_number/have_week/time.py at new point"

---
+++ super/give_fact_with_good_place/eye/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day(str_arg):
+    fact_and_problem(str_arg)
+    print('important_place')
+
+def fact_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day('number_or_place')
