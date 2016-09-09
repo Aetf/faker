80533481c948368abf8211931b0461fc9b538292
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 12:25:38 -0400

    "rm: give super/different_year.py and super/young_company/day_or_number/few_thing/find_problem/new_life.py into little day"

---
+++ super/young_company/day_or_number/few_thing/find_problem/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    hand_and_early_thing(str_arg)
+    print('fact_and_day')
+
+def hand_and_early_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('look_point_for_case')
