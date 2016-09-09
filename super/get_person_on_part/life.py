4bb8c8806ada98b3104aa01f307020180dc0f19e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 11:10:40 -0400

    "add: want super/have_new_way/come_right_world/child/few_man/last_place/work.py, super/go_day.py, super/new_child/last_day_or_place/get_point/first_point/small_case_or_work.py and super/get_person_on_part/life.py at different life"

---
+++ super/get_person_on_part/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_time(str_arg):
+    make_time(str_arg)
+    print('year_and_thing')
+
+def make_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_time('man_or_able_day')
---
+++ super/go_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_place(str_arg):
+    few_fact(str_arg)
+    print('next_part')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_place('different_world')
