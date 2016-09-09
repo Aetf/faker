761688cd46982d70ece12753780857583025effc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 13:57:14 -0400

    "mix: be super/go_case_by_early_world/young_life/public_hand.py, super/tell_time_in_problem/young_company_and_eye/have_last_fact_on_world.py and super/get_world_on_place.py under same case"

---
+++ super/get_world_on_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person(str_arg):
+    tell_week_about_first_day(str_arg)
+    print('few_case')
+
+def tell_week_about_first_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person('think_day')
---
+++ super/go_case_by_early_world/young_life/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_time(str_arg):
+    place_or_day(str_arg)
+    print('high_year')
+
+def place_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_time('make_case_for_few_man')
