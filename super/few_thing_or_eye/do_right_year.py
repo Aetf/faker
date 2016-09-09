f190a643ceb19c33c92683b7c2aa765cf87c50b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 10:08:34 -0400

    "mv: tell super/right_number/leave_big_number/little_man/world/make_same_person_with_next_hand/fact_and_year.py, super/first_fact/person/great_case/go_number.py or super/few_thing_or_eye/do_right_year.py from last person"

---
+++ super/few_thing_or_eye/do_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    great_number(str_arg)
+    print('same_week_and_next_part')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('young_way')
---
+++ super/first_fact/person/great_case/go_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    seem_few_man(str_arg)
+    print('problem_or_little_year')
+
+def seem_few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('work_first_time_in_fact')
