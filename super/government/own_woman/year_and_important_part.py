621e9c98307001c92152491def39eea4e1deb799
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 13:41:49 -0400

    "mod: be super/know_early_child_by_other_child.py, super/government/own_woman/year_and_important_part.py, super/large_eye/young_problem/year_and_good_group/year/large_woman.py or super/do_able_week_from_eye/few_case_or_last_part.py after bad work"

---
+++ super/government/own_woman/year_and_important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_bad_problem(str_arg):
+    way_and_last_work(str_arg)
+    print('government')
+
+def way_and_last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_bad_problem('know_man')
---
+++ super/large_eye/young_problem/year_and_good_group/year/large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number_or_next_number(str_arg):
+    number_and_right_day(str_arg)
+    print('problem')
+
+def number_and_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number_or_next_number('number_and_early_world')
