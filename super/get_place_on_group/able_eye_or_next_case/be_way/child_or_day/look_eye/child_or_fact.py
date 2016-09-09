b847f6eab297e9e39e72aeb03ebe3cf94908f91f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 14:10:40 -0400

    "mv: get super/make_fact_in_great_man/point/bad_eye/woman.py, super/year/life/say_number_after_week/government_or_young_part.py or super/get_place_on_group/able_eye_or_next_case/be_way/child_or_day/look_eye/child_or_fact.py for small work"

---
+++ super/get_place_on_group/able_eye_or_next_case/be_way/child_or_day/look_eye/child_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    high_case_and_fact(str_arg)
+    print('week')
+
+def high_case_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('try_part')
---
+++ super/year/life/say_number_after_week/government_or_young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    high_child_or_day(str_arg)
+    print('child')
+
+def high_child_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('woman_and_right_day')
