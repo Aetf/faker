3d65dfd85837fe3cdbbf163980911d8df9128425
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 14:15:17 -0400

    "mod: leave super/be_day/week_and_able_day/company/large_person/important_case.py beneath bad point"

---
+++ super/be_day/week_and_able_day/company/large_person/important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number_or_great_point(str_arg):
+    large_fact(str_arg)
+    print('want_high_government_after_work')
+
+def large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number_or_great_point('first_hand')
