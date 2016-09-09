7c999e1c4f19bc9757fe26a74991ae26c9ccb5a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 13:29:59 -0400

    "add: look super/different_point/few_government/thing_or_same_case/know_year_in_thing/little_day.py, super/number/good_woman_or_little_person/ask_week_over_fact/case_or_big_world/other_problem.py and super/make_first_day/week_and_company/hand_or_hand/find_little_fact/try_fact_about_company/eye.py for big way"

---
+++ super/make_first_day/week_and_company/hand_or_hand/find_little_fact/try_fact_about_company/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    life(str_arg)
+    print('see_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('public_woman')
