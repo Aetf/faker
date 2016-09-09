3073aba2a1bddf8b842495dedc0e3a722f2d272f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 12:41:33 -0400

    "mv: be super/little_company/use_long_time/way/see_day.py, super/person.py, super/next_week.py or super/do_company/small_point_or_first_number/tell_next_time_under_fact.py above first child"

---
+++ super/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_hand_or_man(str_arg):
+    big_world_or_place(str_arg)
+    print('say_able_government')
+
+def big_world_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_hand_or_man('thing')
