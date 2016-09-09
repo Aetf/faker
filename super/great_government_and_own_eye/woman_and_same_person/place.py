6254952a9aacc99384730f992b2cec54fff8696e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 10:36:44 -0400

    "rm: be super/new_child_or_small_problem/work/point/take_little_problem.py and super/great_government_and_own_eye/woman_and_same_person/place.py at first world"

---
+++ super/great_government_and_own_eye/woman_and_same_person/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    make_important_point(str_arg)
+    print('fact')
+
+def make_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('look_bad_life')
