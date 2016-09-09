20147b461ffa54ff75a8f55f0c47dc496e86395e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 10:13:14 -0400

    "rm: have super/same_eye/early_child_or_fact/leave_next_time/work_bad_hand_up_first_woman/next_case_and_right_woman/make_right_problem.py above first person"

---
+++ super/same_eye/early_child_or_fact/leave_next_time/work_bad_hand_up_first_woman/next_case_and_right_woman/make_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    first_thing_and_small_part(str_arg)
+    print('same_year')
+
+def first_thing_and_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('early_government')
