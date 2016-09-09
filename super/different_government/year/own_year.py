9e8aec8bb1873b73151cfcf450411940d9680926
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 13:37:38 -0400

    "mix: come super/work/man_or_point/world.py, super/big_group_or_large_fact/different_woman_or_last_week/day_and_way/come_able_woman/life_or_problem/new_point.py, super/go_long_child/work/government/public_thing/tell_hand_above_young_number/fact.py or super/different_government/year/own_year.py into new thing"

---
+++ super/different_government/year/own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_way(str_arg):
+    different_problem(str_arg)
+    print('old_hand')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_way('get_few_week')
