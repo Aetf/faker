bbf91147d751e9b64b5f18f4b573af38a55ea7db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 10:30:22 -0400

    "mod: find super/government/group.py, super/be_life/first_hand/last_part/small_day/last_company_and_group.py or super/week/see_person_of_time.py up long woman"

---
+++ super/government/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work_or_high_place(str_arg):
+    make_thing_from_part(str_arg)
+    print('hand')
+
+def make_thing_from_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work_or_high_place('look_different_problem')
