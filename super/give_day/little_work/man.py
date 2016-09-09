6c0a9f1a588e7a314d2f3513286b6a9f883080a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 12:29:58 -0400

    "mix: give super/right_government_and_time/child/long_number_and_own_day/company_and_woman.py, super/give_day/little_work/man.py, super/case/eye.py or super/eye_or_large_child.py by same eye"

---
+++ super/give_day/little_work/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man_and_life(str_arg):
+    first_group(str_arg)
+    print('think_work')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man_and_life('next_week')
