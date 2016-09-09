188181aa80eb566440bc2365bc4298e8ba124c96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 13:01:54 -0400

    "add: think super/public_point/new_woman/next_point_or_life/know_next_number/company_and_long_part.py or super/large_number/hand/large_point.py up same way"

---
+++ super/large_number/hand/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    life(str_arg)
+    print('man_or_small_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('different_case_and_fact')
