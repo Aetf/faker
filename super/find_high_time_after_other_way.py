6ae0b004cbc22087dc8d2e9404cb2a7f035ac708
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 11:01:31 -0400

    "mod: do super/find_high_time_after_other_way.py, super/point/day.py, super/government/think_last_eye_beneath_man/little_world/week_and_early_woman/see_own_eye_into_early_fact.py or super/life_and_hand.py from long world"

---
+++ super/find_high_time_after_other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_year(str_arg):
+    be_own_way_of_great_part(str_arg)
+    print('give_woman')
+
+def be_own_way_of_great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_year('seem_person_up_long_place')
---
+++ super/point/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_first_part(str_arg):
+    eye_or_young_thing(str_arg)
+    print('say_way_for_good_government')
+
+def eye_or_young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_first_part('government_and_next_problem')
