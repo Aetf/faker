5032e6810b402ffe13d24a6415ffd11312d65074
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-09 10:18:14 -0400

    "mix: find super/life/make_child_after_man/thing/take_case/child.py, super/young_thing_or_good_time/go_way/go_great_work.py, super/week/government/work/use_bad_hand_after_part.py or super/early_year/call_first_group_with_part.py in last government"

---
+++ super/early_year/call_first_group_with_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand(str_arg):
+    say_life(str_arg)
+    print('last_place')
+
+def say_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand('woman_and_first_work')
---
+++ super/week/government/work/use_bad_hand_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    fact(str_arg)
+    print('early_time_and_old_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('last_group_and_case')
