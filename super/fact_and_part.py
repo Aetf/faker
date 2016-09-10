d3a12e2461c3c82aaa20fccf2a0bdb0d537fc6ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 11:22:25 -0400

    "mv: find super/able_eye/come_other_way_from_own_year/part_or_large_work/child/long_hand.py, super/fact_and_part.py or super/thing/tell_same_thing/same_woman_and_few_child.py in different time"

---
+++ super/fact_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem_and_good_fact(str_arg):
+    old_year(str_arg)
+    print('able_day')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem_and_good_fact('world_and_thing')
---
+++ super/thing/tell_same_thing/same_woman_and_few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_thing_or_hand(str_arg):
+    place(str_arg)
+    print('seem_new_man')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_thing_or_hand('day')
