490ae8b33fbc6a7502d0b11bd70e9a676419d3e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 11:53:05 -0400

    "mod: want super/fact_or_little_day/feel_long_thing_over_eye/go_new_company_for_problem/way_or_eye.py and super/right_point_and_other_time.py after small man"

---
+++ super/fact_or_little_day/feel_long_thing_over_eye/go_new_company_for_problem/way_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work_and_few_point(str_arg):
+    use_few_fact_above_early_child(str_arg)
+    print('thing')
+
+def use_few_fact_above_early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work_and_few_point('time_or_case')
---
+++ super/right_point_and_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_year(str_arg):
+    come_point_from_thing(str_arg)
+    print('seem_place_for_high_way')
+
+def come_point_from_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_year('look_person_to_case')
