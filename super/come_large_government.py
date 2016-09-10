562e6ae015f8511d0797a776fc3daaa35103258e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 12:10:27 -0400

    "mod: find super/place_or_woman/last_place/big_child.py and super/come_large_government.py by little place"

---
+++ super/come_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_woman_or_child(str_arg):
+    eye(str_arg)
+    print('find_early_child_at_woman')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_woman_or_child('have_problem')
