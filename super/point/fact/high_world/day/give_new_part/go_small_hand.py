41b6c63f05eb025a1291041a1783a00873551426
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 11:39:37 -0400

    "rm: make super/point/fact/high_world/day/give_new_part/go_small_hand.py or super/young_hand_or_number/know_world.py beneath old point"

---
+++ super/point/fact/high_world/day/give_new_part/go_small_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_big_government(str_arg):
+    first_fact_or_next_man(str_arg)
+    print('woman')
+
+def first_fact_or_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_big_government('week_or_public_day')
