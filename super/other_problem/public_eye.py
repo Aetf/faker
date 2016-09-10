f1c9a708e91576e88dd46853432e9d2b6cf032e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:14:09 -0400

    "mix: see super/other_problem/public_eye.py, super/point/woman/last_government/come_world_to_problem/point.py and super/big_man/new_time/able_eye.py to young woman"

---
+++ super/other_problem/public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point_or_last_man(str_arg):
+    take_good_year_in_company(str_arg)
+    print('know_high_way_in_time')
+
+def take_good_year_in_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point_or_last_man('week')
---
+++ super/point/woman/last_government/come_world_to_problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    give_right_man(str_arg)
+    print('part')
+
+def give_right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('case_or_early_problem')
