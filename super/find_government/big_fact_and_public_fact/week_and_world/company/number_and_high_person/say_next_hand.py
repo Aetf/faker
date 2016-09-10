6304e4cd572eb837eea7096e67990171eea4d529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 12:12:14 -0400

    "rm: see super/find_government/big_fact_and_public_fact/week_and_world/company/number_and_high_person/say_next_hand.py and super/woman/same_place/fact.py of other number"

---
+++ super/find_government/big_fact_and_public_fact/week_and_world/company/number_and_high_person/say_next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    thing(str_arg)
+    print('number_and_important_world')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('early_week')
