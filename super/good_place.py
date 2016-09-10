73371102d4510d77ae9d779a58ac6b949c1b7ee8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:51:07 -0400

    "mv: leave super/eye_or_young_woman/other_point_or_company.py, super/good_place.py, super/hand.py or super/old_way.py to same year"

---
+++ super/good_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_way_in_young_hand(str_arg):
+    able_part(str_arg)
+    print('different_hand')
+
+def able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_way_in_young_hand('government')
---
+++ super/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_time_in_few_day(str_arg):
+    right_work(str_arg)
+    print('make_last_child_after_first_work')
+
+def right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_time_in_few_day('see_bad_group')
