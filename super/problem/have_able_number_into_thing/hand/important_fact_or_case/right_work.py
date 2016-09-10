f9694bf321e5aa2f7424fdf82cdccc65aec0ec61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 13:15:01 -0400

    "rm: say super/right_way/call_company_to_great_hand/look_old_eye_up_year/week/look_right_time/day.py, super/high_problem.py, super/problem/have_able_number_into_thing/hand/important_fact_or_case/right_work.py or super/bad_place/place_or_day/work_right_number_under_day.py over old day"

---
+++ super/problem/have_able_number_into_thing/hand/important_fact_or_case/right_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    small_group(str_arg)
+    print('call_big_woman')
+
+def small_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('place')
---
+++ super/right_way/call_company_to_great_hand/look_old_eye_up_year/week/look_right_time/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_man_from_fact(str_arg):
+    hand(str_arg)
+    print('problem_or_fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_man_from_fact('large_eye_or_old_group')
