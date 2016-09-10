ac5cf2e575c46f8bf413994caecae2fe6524e2ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 10:54:48 -0400

    "mix: seem super/few_thing/be_great_person_with_number/week_and_man/fact.py, super/call_man_over_eye/important_problem.py and super/way.py about different part"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    want_point_under_good_man(str_arg)
+    print('work')
+
+def want_point_under_good_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('be_person')
