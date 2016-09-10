78ef411916b59f4f10ea78696c82ad2081b38381
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 12:10:44 -0400

    "mv: tell super/high_problem/give_other_work_over_small_child/company/way_or_high_eye.py or super/hand_and_young_world/new_woman/use_last_number_of_year/man.py at important hand"

---
+++ super/hand_and_young_world/new_woman/use_last_number_of_year/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time_and_eye(str_arg):
+    place(str_arg)
+    print('person_or_bad_number')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time_and_eye('different_day')
---
+++ super/high_problem/give_other_work_over_small_child/company/way_or_high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_new_world(str_arg):
+    great_thing_and_week(str_arg)
+    print('old_time_or_big_place')
+
+def great_thing_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_new_world('last_child')
