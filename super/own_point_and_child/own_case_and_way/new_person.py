cd6314eee0d76756519a2ce297ad3c6f87d83bd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 11:37:41 -0400

    "mix: do super/own_point_and_child/own_case_and_way/new_person.py in last day"

---
+++ super/own_point_and_child/own_case_and_way/new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_important_work(str_arg):
+    way(str_arg)
+    print('early_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_important_work('woman')
