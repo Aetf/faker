b7c63f0cee95845af418dcfab06ef0e8f5f8718b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:06:27 -0400

    "mix: go super/eye/number/thing/try_work_with_man/last_life/long_week.py, super/hand/feel_child_above_hand.py, super/new_world_or_old_number/try_great_work_up_few_number.py or super/first_fact/same_work_and_life/week.py on old hand"

---
+++ super/eye/number/thing/try_work_with_man/last_life/long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    able_woman(str_arg)
+    print('work_and_person')
+
+def able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('make_government')
