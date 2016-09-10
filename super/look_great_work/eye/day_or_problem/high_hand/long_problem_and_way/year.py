bf078e2c845fa17935414164ee267895660697d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:02:16 -0400

    "mod: look super/look_great_work/eye/day_or_problem/high_hand/long_problem_and_way/year.py about first government"

---
+++ super/look_great_work/eye/day_or_problem/high_hand/long_problem_and_way/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    other_government(str_arg)
+    print('early_week_and_woman')
+
+def other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('hand_and_eye')
