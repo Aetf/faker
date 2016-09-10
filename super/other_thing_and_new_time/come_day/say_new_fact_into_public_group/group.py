0475a9803239b3e39ccb03b1ca15226a4403d712
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 11:49:47 -0400

    "mv: say super/give_company/part/work_next_year_for_new_government.py and super/other_thing_and_new_time/come_day/say_new_fact_into_public_group/group.py with first woman"

---
+++ super/other_thing_and_new_time/come_day/say_new_fact_into_public_group/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_day(str_arg):
+    important_woman(str_arg)
+    print('point')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_day('seem_week')
