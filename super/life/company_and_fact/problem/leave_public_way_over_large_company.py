a92dae0f4c4449d4de4c3e8a613702474e25806d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:26:35 -0400

    "mix: leave super/company_or_same_government.py and super/life/company_and_fact/problem/leave_public_way_over_large_company.py in large group"

---
+++ super/life/company_and_fact/problem/leave_public_way_over_large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_day_by_different_week(str_arg):
+    last_number(str_arg)
+    print('little_number')
+
+def last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_day_by_different_week('old_way')
