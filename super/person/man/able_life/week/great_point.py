8885466d3b7dbd2c6facfd276f6ca4abe99e73a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-10 14:55:49 -0400

    "rm: come super/small_problem/part/right_fact_or_next_number/be_time_about_year/important_fact.py, super/work_point/number/leave_long_week.py, super/want_good_number/make_way_by_hand/good_company/call_way.py and super/person/man/able_life/week/great_point.py about different world"

---
+++ super/person/man/able_life/week/great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group(str_arg):
+    great_eye(str_arg)
+    print('part')
+
+def great_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group('large_world')
---
+++ super/small_problem/part/right_fact_or_next_number/be_time_about_year/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    new_point(str_arg)
+    print('able_part')
+
+def new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('use_large_point_up_important_way')
