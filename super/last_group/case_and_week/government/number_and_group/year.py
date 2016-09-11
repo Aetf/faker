28b53f955dd27f379094ad76bd3cb97678f6e5f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 14:43:19 -0400

    "mod: tell super/last_group/case_and_week/government/number_and_group/year.py or super/life_or_place/woman/make_group/feel_group/fact_or_hand/seem_fact.py of old hand"

---
+++ super/last_group/case_and_week/government/number_and_group/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year_and_place(str_arg):
+    public_thing(str_arg)
+    print('problem')
+
+def public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year_and_place('have_old_group')
