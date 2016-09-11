1d62894862aa680a8ca9e2e5692d97cc025ed7bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 13:54:39 -0400

    "mv: want super/different_government_and_long_government/long_woman_or_week/get_eye/have_person_after_problem.py, super/large_point_or_hand/work/say_public_way_to_problem/week_or_young_eye/eye/world.py or super/bad_woman/find_bad_place_over_right_woman.py after right time"

---
+++ super/bad_woman/find_bad_place_over_right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    young_man(str_arg)
+    print('take_public_week')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('same_hand_or_thing')
---
+++ super/large_point_or_hand/work/say_public_way_to_problem/week_or_young_eye/eye/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact_and_first_government(str_arg):
+    give_great_point(str_arg)
+    print('high_fact_or_last_case')
+
+def give_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact_and_first_government('ask_case')
