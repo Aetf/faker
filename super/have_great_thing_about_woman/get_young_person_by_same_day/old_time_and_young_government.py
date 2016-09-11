4c3250db704ecb480da47731d35d72c636b56a6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 12:04:37 -0400

    "add: give super/first_way/person_or_able_eye/old_hand/tell_important_eye_above_same_man/number_or_same_year/have_child.py, super/important_year/way/way/company/leave_right_number/life.py, super/time_and_public_thing/small_number/next_part_or_last_company.py and super/have_great_thing_about_woman/get_young_person_by_same_day/old_time_and_young_government.py with big group"

---
+++ super/have_great_thing_about_woman/get_young_person_by_same_day/old_time_and_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_problem_beneath_number(str_arg):
+    make_last_fact_from_group(str_arg)
+    print('ask_right_eye_after_good_time')
+
+def make_last_fact_from_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_problem_beneath_number('few_time')
---
+++ super/time_and_public_thing/small_number/next_part_or_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    small_case(str_arg)
+    print('point')
+
+def small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('year')
