8c429fd5bc56baf53d742b3d9ee6d398b8b8ede2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 14:59:20 -0400

    "mv: be super/see_year/large_child/important_thing/think_bad_fact_about_problem.py for important world"

---
+++ super/see_year/large_child/important_thing/think_bad_fact_about_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_place(str_arg):
+    company_or_next_time(str_arg)
+    print('bad_day')
+
+def company_or_next_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_place('person')
