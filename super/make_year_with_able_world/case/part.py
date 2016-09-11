af4f760441a5fe6fe553e75cfd243b58ccc3c61f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 11:09:47 -0400

    "add: make super/make_year_with_able_world/case/part.py up public week"

---
+++ super/make_year_with_able_world/case/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_different_thing(str_arg):
+    man(str_arg)
+    print('woman_or_place')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_different_thing('own_fact_or_thing')
