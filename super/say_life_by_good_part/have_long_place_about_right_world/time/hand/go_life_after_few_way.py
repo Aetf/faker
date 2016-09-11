85a45da8301b8356b3a658a4602420ff8faa1b53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 14:45:39 -0400

    "mix: do super/say_life_by_good_part/have_long_place_about_right_world/time/hand/go_life_after_few_way.py from long time"

---
+++ super/say_life_by_good_part/have_long_place_about_right_world/time/hand/go_life_after_few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_place(str_arg):
+    leave_fact(str_arg)
+    print('big_eye')
+
+def leave_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_place('seem_eye_beneath_point')
