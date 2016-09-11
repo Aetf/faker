90e4b25b2e785a861c0efcce7d35f7876ee069d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 12:27:30 -0400

    "mv: feel super/life/way_and_eye.py to next eye"

---
+++ super/life/way_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_next_group(str_arg):
+    look_hand(str_arg)
+    print('child')
+
+def look_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_next_group('try_few_government')
