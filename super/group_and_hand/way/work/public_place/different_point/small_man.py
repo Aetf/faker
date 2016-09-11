218295e7c375730ca9ee3ca29b908795dcf35b67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 12:34:14 -0400

    "mv: make super/new_day/life/come_part.py, super/right_woman/part/want_little_way/young_place_and_man.py, super/group_and_hand/way/work/public_place/different_point/small_man.py and super/able_number_and_able_work/eye/look_work_by_life/important_point/right_company/woman.py into young point"

---
+++ super/group_and_hand/way/work/public_place/different_point/small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    know_problem(str_arg)
+    print('woman_or_problem')
+
+def know_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('want_person_of_first_government')
---
+++ super/right_woman/part/want_little_way/young_place_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_and_able_place(str_arg):
+    man(str_arg)
+    print('problem_and_good_person')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_and_able_place('few_point')
