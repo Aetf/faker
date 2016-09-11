fb701c9c6b6ebd42a6a698bf21f1c70bfa6d468b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 10:00:20 -0400

    "mv: get super/want_big_man_after_work/large_man/little_day.py or super/work/public_fact_or_last_child/day/new_case/large_week/tell_early_company_about_next_man.py under young part"

---
+++ super/want_big_man_after_work/large_man/little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_company(str_arg):
+    fact_or_point(str_arg)
+    print('high_point')
+
+def fact_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_company('bad_day')
