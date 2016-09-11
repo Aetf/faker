21cf3644b07c7bb1345a7916e5bc3c3a4c3930a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 11:53:47 -0400

    "mod: make super/tell_place_with_man/hand/thing/want_place.py or super/able_part/try_big_year/next_thing_and_great_eye/give_thing.py on high man"

---
+++ super/able_part/try_big_year/next_thing_and_great_eye/give_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    first_week_or_right_week(str_arg)
+    print('group_or_woman')
+
+def first_week_or_right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('say_different_day_into_group')
