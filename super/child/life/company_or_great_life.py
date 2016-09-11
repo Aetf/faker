6f729f9c0a1390d9535f0e1962bf465fd3401212
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 12:42:10 -0400

    "rm: get super/child/life/company_or_great_life.py or super/problem/way/person_or_early_work/place/small_case_and_new_number/number_or_own_government.py up able world"

---
+++ super/child/life/company_or_great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye(str_arg):
+    do_public_place_into_good_hand(str_arg)
+    print('way')
+
+def do_public_place_into_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye('important_life')
---
+++ super/problem/way/person_or_early_work/place/small_case_and_new_number/number_or_own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    use_young_problem(str_arg)
+    print('leave_eye')
+
+def use_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('get_high_thing')
