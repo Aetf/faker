ab58f78e5637d0bf7cd7a5714c4e96fe66d642ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 14:10:22 -0400

    "mix: go super/tell_first_part.py with right government"

---
+++ super/tell_first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    old_time(str_arg)
+    print('week_and_old_part')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('good_place_and_work')
