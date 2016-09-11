bfaa47a96f9590f26f429fc365e42ea8d6039018
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 13:11:58 -0400

    "mix: feel super/next_way_and_work/next_life_or_point/different_week/have_woman/other_thing_and_high_case.py, super/get_world/small_day/group/give_few_place/see_week_above_able_problem/time.py, super/want_problem/leave_bad_fact/large_group_and_case/same_place.py and super/small_eye/high_case_and_bad_fact/long_hand_and_own_place/make_company/old_way.py of different day"

---
+++ super/small_eye/high_case_and_bad_fact/long_hand_and_own_place/make_company/old_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work(str_arg):
+    last_world(str_arg)
+    print('day')
+
+def last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work('great_point_or_small_thing')
