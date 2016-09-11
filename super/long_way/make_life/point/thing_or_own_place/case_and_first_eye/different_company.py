b607d543a5763b4c00cfc2ce9ff38538b2fb36f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 11:10:31 -0400

    "rm: work super/government/life_or_time/few_company_and_right_government/get_early_case_for_company/woman_and_early_way.py or super/long_way/make_life/point/thing_or_own_place/case_and_first_eye/different_company.py by good company"

---
+++ super/long_way/make_life/point/thing_or_own_place/case_and_first_eye/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_day_or_bad_government(str_arg):
+    week(str_arg)
+    print('day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_day_or_bad_government('year')
