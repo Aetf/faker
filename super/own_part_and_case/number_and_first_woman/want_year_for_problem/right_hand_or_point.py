40ec5aed26aab043bffb2fcd1b02cfc16d29f715
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 10:34:57 -0400

    "mix: go super/own_part_and_case/number_and_first_woman/want_year_for_problem/right_hand_or_point.py or super/find_thing_from_world.py under high group"

---
+++ super/own_part_and_case/number_and_first_woman/want_year_for_problem/right_hand_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_case(str_arg):
+    leave_large_problem_at_own_case(str_arg)
+    print('young_place_or_good_number')
+
+def leave_large_problem_at_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_case('tell_long_hand')
