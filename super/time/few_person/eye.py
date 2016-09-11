7b6d91d5a030b52d6ecba963c87e2f00ef427a0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-11 13:20:25 -0400

    "mod: think super/young_world_and_part.py, super/man/be_next_company.py, super/time/few_person/eye.py and super/problem_or_way/day/look_next_week/different_world/same_world.py for important government"

---
+++ super/time/few_person/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day(str_arg):
+    give_little_hand(str_arg)
+    print('part')
+
+def give_little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day('thing')
---
+++ super/young_world_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_time_or_woman(str_arg):
+    number(str_arg)
+    print('thing_and_first_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_time_or_woman('place')
