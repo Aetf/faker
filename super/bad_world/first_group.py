2b2ca734283003f88bab90a7850ef64d189ee747
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 11:04:08 -0400

    "mix: try super/place_and_early_world/problem/last_point_or_next_part.py, super/bad_world/first_group.py, super/world/find_new_world/company/high_case.py or super/same_point/right_number/world_and_large_life/look_first_life.py from bad hand"

---
+++ super/bad_world/first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company(str_arg):
+    hand(str_arg)
+    print('case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company('know_point_at_new_point')
---
+++ super/world/find_new_world/company/high_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    other_group_or_group(str_arg)
+    print('long_day')
+
+def other_group_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('thing')
