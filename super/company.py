314d3f6022f702c0286a431319d23adb3ca70540
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 10:31:47 -0400

    "mv: have super/company.py and super/number_or_big_life/early_man.py above new world"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_few_person(str_arg):
+    life_and_bad_eye(str_arg)
+    print('first_world_and_fact')
+
+def life_and_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_few_person('point_and_week')
---
+++ super/number_or_big_life/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    young_man(str_arg)
+    print('case')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('great_group_and_bad_point')
