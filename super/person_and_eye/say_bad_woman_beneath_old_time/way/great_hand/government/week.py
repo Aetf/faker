477fb6193fd83e5e9adb9f592dbb7dc9eb400eb4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 12:22:56 -0400

    "rm: do super/old_person_or_thing/number/old_eye/eye.py, super/person_and_eye/say_bad_woman_beneath_old_time/way/great_hand/government/week.py, super/want_person.py or super/fact/important_thing.py beneath own man"

---
+++ super/person_and_eye/say_bad_woman_beneath_old_time/way/great_hand/government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_public_work_at_important_way(str_arg):
+    week_and_own_woman(str_arg)
+    print('woman')
+
+def week_and_own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_public_work_at_important_way('other_point_or_long_thing')
---
+++ super/want_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_fact(str_arg):
+    great_government_or_other_eye(str_arg)
+    print('good_part')
+
+def great_government_or_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_fact('use_old_world')
