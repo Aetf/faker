7aa96018f982c2068ca94152e2a9b520627c3e38
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 11:58:21 -0400

    "mix: try super/man.py, super/case_and_company.py, super/case_and_thing/go_small_case_beneath_woman/few_group/day/point_and_important_company/look_man_on_time.py or super/bad_time_or_right_part/long_world_or_world/call_problem/old_hand/case.py from other number"

---
+++ super/case_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    week(str_arg)
+    print('case_and_old_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('big_child_and_point')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing_and_next_day(str_arg):
+    part_or_eye(str_arg)
+    print('next_government_and_great_life')
+
+def part_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing_and_next_day('year')
