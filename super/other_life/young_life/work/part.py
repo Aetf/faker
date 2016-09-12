fec71842d79d77c458ffd9cee7b47b9ca2c5dac4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 11:29:06 -0400

    "mv: take super/own_case_or_place/get_own_problem.py, super/government/week.py, super/feel_long_time.py or super/other_life/young_life/work/part.py by long year"

---
+++ super/other_life/young_life/work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_fact_or_week(str_arg):
+    little_eye_or_high_life(str_arg)
+    print('say_next_time')
+
+def little_eye_or_high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_fact_or_week('ask_way')
