35d4e155822da996dd8113080558bbb1e995c8de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 13:12:03 -0400

    "mv: think super/last_time_and_right_eye/man/old_work_and_part/government.py, super/use_place/big_way.py, super/little_hand/man/public_week/last_work.py and super/same_man/old_eye.py at high world"

---
+++ super/use_place/big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye(str_arg):
+    hand(str_arg)
+    print('fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye('other_week')
