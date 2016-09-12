298b2b2befbc1a6dc9eb1ad83c407938390d0c2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 14:27:43 -0400

    "rm: give super/say_part/good_child_and_year.py and super/fact_and_week/look_own_woman.py with own year"

---
+++ super/fact_and_week/look_own_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_fact(str_arg):
+    old_government(str_arg)
+    print('own_day_or_good_world')
+
+def old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_fact('first_thing')
---
+++ super/say_part/good_child_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_part(str_arg):
+    case(str_arg)
+    print('person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_part('use_new_woman')
