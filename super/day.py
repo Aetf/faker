e7a5539ace4caae46640fe5d776a34fef202867a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 13:49:48 -0400

    "mv: want super/fact/able_part.py, super/man_and_government/new_thing_or_world/seem_old_work/part.py or super/day.py of other number"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    own_year(str_arg)
+    print('bad_thing')
+
+def own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('seem_same_day')
---
+++ super/man_and_government/new_thing_or_world/seem_old_work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_life_or_woman(str_arg)
+    print('way')
+
+def first_life_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('great_eye')
