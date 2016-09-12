adcac6d55ebf216c467e313f40a556e9f307250e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 12:17:07 -0400

    "mv: go super/public_life/work.py over own life"

---
+++ super/public_life/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    hand(str_arg)
+    print('company')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('few_government')
