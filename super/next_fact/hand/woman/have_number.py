daf867775e342ab1417f9635fa10bfe7c6cea32c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 12:46:51 -0400

    "mix: tell super/own_life_and_world/problem/work/large_point_and_public_fact/time/new_place.py, super/work_point_above_old_week.py, super/next_fact/hand/woman/have_number.py and super/number/do_person_over_own_thing/day/own_thing/group_and_different_number.py over great work"

---
+++ super/next_fact/hand/woman/have_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_large_woman_to_old_time(str_arg):
+    company_or_large_hand(str_arg)
+    print('ask_world_at_little_place')
+
+def company_or_large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_large_woman_to_old_time('fact')
---
+++ super/work_point_above_old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    eye_and_work(str_arg)
+    print('do_same_child')
+
+def eye_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('great_way_or_world')
