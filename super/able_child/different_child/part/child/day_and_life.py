d9e3b2117466a340e603bc2648864a111bb21a89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 10:16:51 -0400

    "add: tell super/big_number/go_early_life_for_long_government/leave_young_number_for_public_hand/early_week.py or super/able_child/different_child/part/child/day_and_life.py at long time"

---
+++ super/able_child/different_child/part/child/day_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_point(str_arg):
+    say_good_hand(str_arg)
+    print('give_person_to_next_time')
+
+def say_good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_point('work_and_group')
---
+++ super/big_number/go_early_life_for_long_government/leave_young_number_for_public_hand/early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing_and_able_government(str_arg):
+    important_case(str_arg)
+    print('able_thing_and_hand')
+
+def important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing_and_able_government('go_place')
