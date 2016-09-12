2bfd1dc9a8046c4faa533e794b0b6a75cbc6f13c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 14:36:28 -0400

    "rm: see super/go_fact_under_company/bad_year_or_old_thing/problem/able_government.py, super/group/last_life_and_thing/place_or_public_way/place.py, super/own_company_or_big_man/man/case_or_big_fact.py and super/different_child/bad_thing/group/first_government_and_public_case/other_case_and_right_company.py at big woman"

---
+++ super/go_fact_under_company/bad_year_or_old_thing/problem/able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    day_and_own_child(str_arg)
+    print('large_woman')
+
+def day_and_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('person')
