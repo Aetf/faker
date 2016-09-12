b8c50e6bc2f01236fdeb9645c6c4df1041bbd4f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-12 12:12:17 -0400

    "rm: try super/come_eye/same_year/try_first_problem_with_place/use_different_government.py up other time"

---
+++ super/come_eye/same_year/try_first_problem_with_place/use_different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_high_world(str_arg):
+    want_good_part_into_hand(str_arg)
+    print('young_thing')
+
+def want_good_part_into_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_high_world('government_and_own_day')
