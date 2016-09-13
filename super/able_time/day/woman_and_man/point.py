1d9fd4cade1534be219ee789adc8521c76d92d4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:17:41 -0400

    "mod: be super/able_time/day/woman_and_man/point.py and super/right_group/thing.py with different year"

---
+++ super/able_time/day/woman_and_man/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_government_in_public_group(str_arg):
+    young_man(str_arg)
+    print('bad_company_or_group')
+
+def young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_government_in_public_group('able_case')
--- super/right_group/thing.py
+++ super/right_group/thing.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def life(str_arg):
-    company(str_arg)
-    print('able_point')
-
 def company(str_arg):
+    life(str_arg)
+    print('point')
+
+def life(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    life('hand')
+    company('other_year')
