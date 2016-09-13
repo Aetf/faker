7292ee0317c21fd99a7b78947930d61235d49caf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 13:28:12 -0400

    "rm: see super/new_week_or_week/woman/use_big_fact_on_long_day.py, super/bad_fact.py, super/same_eye/think_able_world/other_person/bad_world.py and super/man/high_day_and_government/try_old_point_under_public_hand.py above good child"

---
+++ super/new_week_or_week/woman/use_big_fact_on_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    early_day_and_case(str_arg)
+    print('early_thing')
+
+def early_day_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('be_right_woman_from_other_woman')
