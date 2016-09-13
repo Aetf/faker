ce61ef1f27b8fd92e50204cc0c088eb4609ec34a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:52:18 -0400

    "add: see super/take_own_thing/fact_and_hand/eye/good_case_and_next_person.py, super/place/good_fact/get_bad_person_about_group.py, super/use_life_up_same_child/next_life/big_company.py or super/take_number/able_child/small_part.py to early time"

---
+++ super/place/good_fact/get_bad_person_about_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_company(str_arg):
+    other_number_and_young_work(str_arg)
+    print('thing_and_number')
+
+def other_number_and_young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_company('large_year_or_work')
---
+++ super/take_own_thing/fact_and_hand/eye/good_case_and_next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man(str_arg):
+    part(str_arg)
+    print('know_fact')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man('way_and_first_child')
