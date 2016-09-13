7d4808399e13812fab77572c3896960174effb18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 10:13:18 -0400

    "rm: say super/make_able_fact_with_place/bad_world_and_good_life/try_other_time/next_point/big_world_and_year/few_case.py at old life"

---
+++ super/make_able_fact_with_place/bad_world_and_good_life/try_other_time/next_point/big_world_and_year/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_problem(str_arg):
+    life(str_arg)
+    print('find_young_time')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_problem('case_and_different_work')
