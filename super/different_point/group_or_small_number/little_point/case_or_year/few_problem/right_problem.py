bb6ec964b2b13a2b4d0011b0649fa17918dad8ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 14:45:54 -0400

    "add: want super/public_time_or_year/large_year_and_few_group/small_company/right_way_and_life.py, super/right_hand/have_other_fact_to_man.py or super/different_point/group_or_small_number/little_point/case_or_year/few_problem/right_problem.py by last group"

---
+++ super/different_point/group_or_small_number/little_point/case_or_year/few_problem/right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    take_company(str_arg)
+    print('tell_other_week_in_little_woman')
+
+def take_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('come_life')
---
+++ super/public_time_or_year/large_year_and_few_group/small_company/right_way_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_good_thing(str_arg):
+    few_world(str_arg)
+    print('way_and_high_work')
+
+def few_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_good_thing('be_hand_for_point')
