3d59e376e1446e0d07b440373676462d08135401
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 14:08:17 -0400

    "add: be super/last_life_and_life/group/day_or_long_case/problem/woman/leave_early_way_up_other_world.py under bad fact"

---
+++ super/last_life_and_life/group/day_or_long_case/problem/woman/leave_early_way_up_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    problem_or_problem(str_arg)
+    print('have_eye_up_way')
+
+def problem_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('long_place')
