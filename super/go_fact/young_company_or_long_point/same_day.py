12b4fe7751d7691f129acc84e809d129ef3c74e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:44:25 -0400

    "add: call super/leave_young_part/say_way/company/important_time/high_fact_or_new_life.py, super/person/seem_fact_by_place/old_man.py, super/go_fact/young_company_or_long_point/same_day.py or super/work/right_way/right_year/take_early_group/company.py up small case"

---
+++ super/go_fact/young_company_or_long_point/same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    point_and_group(str_arg)
+    print('hand_or_right_government')
+
+def point_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('eye')
---
+++ super/work/right_way/right_year/take_early_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    get_day(str_arg)
+    print('life_or_important_eye')
+
+def get_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('early_way')
