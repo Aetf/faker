fc7d1a7b227a320b2e249a10d3240342f5b5ad59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:23:08 -0400

    "mod: ask super/new_group/want_different_man/part.py over right man"

---
+++ super/new_group/want_different_man/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    woman(str_arg)
+    print('company_or_company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('fact')
