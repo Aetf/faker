b0ef43c3cb2ca75f8bf7c65931b91b13ccf8f09e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 10:44:11 -0400

    "mod: ask super/case/number.py, super/find_long_week/life_and_case/life_and_next_life.py and super/few_child.py by able group"

---
+++ super/case/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_fact(str_arg):
+    use_company_about_part(str_arg)
+    print('place')
+
+def use_company_about_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_fact('long_part')
