0013f7d7801dfc071eb36a5ae47ccd7e886ef579
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:12:13 -0400

    "add: take super/try_group_beneath_long_thing.py, super/high_point/make_first_number_over_last_company.py or super/case.py up own number"

---
+++ super/high_point/make_first_number_over_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    week(str_arg)
+    print('life')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('government_or_new_hand')
---
+++ super/try_group_beneath_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    man(str_arg)
+    print('able_child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('give_public_week_beneath_day')
