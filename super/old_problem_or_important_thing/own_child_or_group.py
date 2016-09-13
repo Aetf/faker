6777d8e0a7e7c34f8b8a44491f244dbad7f484c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 10:51:17 -0400

    "mv: see super/good_problem_or_way/different_thing/work/time/day_or_young_time.py and super/old_problem_or_important_thing/own_child_or_group.py by small woman"

---
+++ super/old_problem_or_important_thing/own_child_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    little_day_and_point(str_arg)
+    print('think_few_woman_over_part')
+
+def little_day_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('young_woman_or_new_life')
