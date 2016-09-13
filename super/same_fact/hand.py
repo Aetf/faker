0586b4425a5a7be5722a8aa90ae4c8ffc0adc5c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 11:03:38 -0400

    "mv: want super/same_fact/hand.py from young way"

---
+++ super/same_fact/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_place(str_arg):
+    early_day(str_arg)
+    print('different_part_or_part')
+
+def early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_place('person')
