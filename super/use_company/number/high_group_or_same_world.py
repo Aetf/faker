47ff5942404dbbd80aec97095839f4bbff745704
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 14:30:31 -0400

    "add: look super/use_company/number/high_group_or_same_world.py on old number"

---
+++ super/use_company/number/high_group_or_same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    group_and_company(str_arg)
+    print('different_fact')
+
+def group_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('little_point_and_man')
