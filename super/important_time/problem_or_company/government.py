6dc0508a0f4e40943a69db9967952c3dcdaf7e00
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 10:12:33 -0400

    "rm: see super/eye/high_place/case_or_public_man/public_part.py or super/important_time/problem_or_company/government.py at little government"

---
+++ super/important_time/problem_or_company/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point(str_arg):
+    important_thing_or_person(str_arg)
+    print('man')
+
+def important_thing_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point('new_week')
