feebd421dccdd3c14581eba96ce5d4f8a4260b43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-13 13:43:35 -0400

    "mod: come super/next_week/time.py or super/other_day.py from right world"

---
+++ super/next_week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    way(str_arg)
+    print('able_time_and_man')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('new_place')
---
+++ super/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    work_or_long_day(str_arg)
+    print('woman')
+
+def work_or_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('little_place')
