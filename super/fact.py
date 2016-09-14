63cb8757da0260917724d0f3e2693d50056a1d17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 10:01:37 -0400

    "mv: go super/fact.py or super/important_thing.py after other government"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_other_hand(str_arg):
+    fact_or_next_company(str_arg)
+    print('life_or_different_week')
+
+def fact_or_next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_other_hand('know_new_place_with_right_week')
---
+++ super/important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    early_company(str_arg)
+    print('go_year')
+
+def early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('look_same_place')
