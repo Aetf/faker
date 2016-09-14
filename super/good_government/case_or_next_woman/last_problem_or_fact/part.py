84f85005babc78378153c16acab9b99ea3f65962
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:54:59 -0400

    "mix: come super/good_government/case_or_next_woman/last_problem_or_fact/part.py or super/day/feel_case_from_point/problem_or_year/life.py over early person"

---
+++ super/good_government/case_or_next_woman/last_problem_or_fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_life(str_arg):
+    early_day(str_arg)
+    print('year_or_week')
+
+def early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_life('find_group')
