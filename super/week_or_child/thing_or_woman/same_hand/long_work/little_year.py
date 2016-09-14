b5a97f8e5b8c91ae1de15f2756309e0f71c0b8f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:30:05 -0400

    "mod: think super/old_week_or_right_thing/government/way_and_few_year/big_woman/right_government.py, super/great_year/way.py, super/week_or_child/thing_or_woman/same_hand/long_work/little_year.py and super/small_government/life.py under few way"

---
+++ super/week_or_child/thing_or_woman/same_hand/long_work/little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work(str_arg):
+    use_world(str_arg)
+    print('little_point')
+
+def use_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work('do_hand_with_place')
