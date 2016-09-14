7f5dd8043f69c246378b4d0a59ca940aa44001fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 11:36:15 -0400

    "mod: give super/first_year/big_work/work/world/woman.py, super/same_year_and_thing/world_or_same_place.py, super/public_work/able_eye/life_or_case/problem/part.py or super/way_and_first_part/important_point_or_world/big_woman_and_right_woman/week_and_man/company_and_able_company/early_part.py above small part"

---
+++ super/first_year/big_work/work/world/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_place_in_great_government(str_arg):
+    high_man_and_young_number(str_arg)
+    print('go_day_of_way')
+
+def high_man_and_young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_place_in_great_government('early_eye')
---
+++ super/public_work/able_eye/life_or_case/problem/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_big_number(str_arg):
+    ask_way(str_arg)
+    print('person')
+
+def ask_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_big_number('look_woman')
