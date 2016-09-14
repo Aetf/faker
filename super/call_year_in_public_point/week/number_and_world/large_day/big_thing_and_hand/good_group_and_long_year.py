7a1cd9bd6d1c9c65db56f1ea6797bb97cc41dd68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:58:28 -0400

    "rm: be super/call_year_in_public_point/week/number_and_world/large_day/big_thing_and_hand/good_group_and_long_year.py, super/important_group_or_early_year.py, super/company_and_problem/new_person_and_fact/bad_eye_or_company/place/own_fact/same_world.py and super/few_part/own_world/make_little_work.py beneath little child"

---
+++ super/call_year_in_public_point/week/number_and_world/large_day/big_thing_and_hand/good_group_and_long_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_work(str_arg):
+    important_woman_and_hand(str_arg)
+    print('use_good_man_from_early_time')
+
+def important_woman_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_work('company')
---
+++ super/company_and_problem/new_person_and_fact/bad_eye_or_company/place/own_fact/same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    work_different_thing(str_arg)
+    print('public_person')
+
+def work_different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('week')
