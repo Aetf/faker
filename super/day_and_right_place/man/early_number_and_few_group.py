69dcfd2cd2b6615c75ab5a4fb7a761ac1bc39a62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 10:32:52 -0400

    "mix: do super/day_and_right_place/man/early_number_and_few_group.py or super/group/high_year/long_group/child/early_place_and_old_life.py with own fact"

---
+++ super/day_and_right_place/man/early_number_and_few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_work(str_arg):
+    old_government_and_new_world(str_arg)
+    print('day')
+
+def old_government_and_new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_work('have_same_week_with_same_year')
---
+++ super/group/high_year/long_group/child/early_place_and_old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    public_problem(str_arg)
+    print('thing')
+
+def public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('first_eye')
