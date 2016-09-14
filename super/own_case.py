970e296f9ea4b72d18b8f6b48c0c2cfe0c90d676
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:31:19 -0400

    "mod: give super/own_case.py into new child"

---
+++ super/own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand_or_other_work(str_arg):
+    life(str_arg)
+    print('big_year_or_big_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand_or_other_work('use_life_for_group')
