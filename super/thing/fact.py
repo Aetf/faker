9b7026de49876894ac7530672f48894754527442
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 10:02:19 -0400

    "rm: get super/bad_year/new_child/young_world/last_part/problem.py or super/thing/fact.py for own time"

---
+++ super/thing/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_point(str_arg):
+    ask_eye_into_work(str_arg)
+    print('leave_large_work')
+
+def ask_eye_into_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_point('man_or_bad_group')
