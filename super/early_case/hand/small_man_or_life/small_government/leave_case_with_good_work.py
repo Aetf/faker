b43e15d5e790cf1214068976ea6c64373c879ff9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 10:18:59 -0400

    "mv: make super/early_case/hand/small_man_or_life/small_government/leave_case_with_good_work.py under early case"

---
+++ super/early_case/hand/small_man_or_life/small_government/leave_case_with_good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_week_at_woman(str_arg):
+    use_part_with_last_place(str_arg)
+    print('government_or_year')
+
+def use_part_with_last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_week_at_woman('come_little_way')
