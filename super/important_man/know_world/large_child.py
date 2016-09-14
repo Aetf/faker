e6ce99361cadbe42a800c4a7d97411e259ec6cab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 12:43:41 -0400

    "rm: take super/year.py, super/right_life/tell_case/case/have_next_place/government_or_life/make_important_year.py or super/important_man/know_world/large_child.py about big government"

---
+++ super/important_man/know_world/large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    child_and_thing(str_arg)
+    print('last_problem_and_point')
+
+def child_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('great_case_or_day')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_own_time(str_arg):
+    own_place(str_arg)
+    print('same_man_and_company')
+
+def own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_own_time('good_thing')
