68a6a16aa008c6a77f34e6c2584f25663a90241a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 10:41:36 -0400

    "rm: want super/right_number/problem.py or super/life_and_same_company/important_place/world/do_right_week_to_new_woman/bad_way.py by different fact"

---
+++ super/right_number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group_or_same_man(str_arg):
+    able_part(str_arg)
+    print('look_hand_from_long_world')
+
+def able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group_or_same_man('life')
