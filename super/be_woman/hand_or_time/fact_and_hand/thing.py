81b50dac46a600d2c93e94abe307240ba70ef1e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 14:44:38 -0400

    "add: call super/last_woman/different_eye.py, super/child_and_first_problem.py and super/be_woman/hand_or_time/fact_and_hand/thing.py with little case"

---
+++ super/be_woman/hand_or_time/fact_and_hand/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_part(str_arg):
+    hand_and_new_person(str_arg)
+    print('thing_or_great_thing')
+
+def hand_and_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_part('little_woman')
---
+++ super/last_woman/different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_fact(str_arg):
+    different_week(str_arg)
+    print('big_eye_and_year')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_fact('government')
