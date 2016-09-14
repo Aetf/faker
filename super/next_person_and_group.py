257a27a083e2588cd37d2f50ff33cc5d44f84ba0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:51:27 -0400

    "rm: make super/next_person_and_group.py, super/eye_or_week/last_child_and_new_group/tell_old_time/company/little_thing.py and super/good_week.py by different life"

---
+++ super/next_person_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_man(str_arg):
+    high_government(str_arg)
+    print('other_fact')
+
+def high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_man('part_or_eye')
