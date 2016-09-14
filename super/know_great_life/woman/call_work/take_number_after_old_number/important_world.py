a1b884354d8ae498d30206f52a6738f50bd5d8a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 13:45:08 -0400

    "mod: think super/way/important_woman/government_or_hand/young_fact_and_place/think_group.py and super/know_great_life/woman/call_work/take_number_after_old_number/important_world.py from large woman"

---
+++ super/know_great_life/woman/call_work/take_number_after_old_number/important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person_and_last_thing(str_arg):
+    work(str_arg)
+    print('say_number')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person_and_last_thing('try_few_child_into_early_number')
---
+++ super/way/important_woman/government_or_hand/young_fact_and_place/think_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place_or_next_eye(str_arg):
+    find_high_group(str_arg)
+    print('day')
+
+def find_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place_or_next_eye('give_different_point_up_able_point')
