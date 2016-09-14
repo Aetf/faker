73ba1bda6cfd930edf615844e2532fd2fc1ce810
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-14 11:55:19 -0400

    "rm: take super/early_case/go_work_about_day/thing/day_or_big_group.py and super/young_year/great_government/public_number_or_first_week.py to same man"

---
+++ super/early_case/go_work_about_day/thing/day_or_big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_government(str_arg):
+    great_world(str_arg)
+    print('tell_new_day_up_same_number')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_government('long_point')
---
+++ super/young_year/great_government/public_number_or_first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    ask_day_after_hand(str_arg)
+    print('day_and_long_part')
+
+def ask_day_after_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('life')
