8a487bbf31fed5bb090276e45316f1477e9b2ba4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 12:35:53 -0400

    "mix: find super/case/small_woman.py, super/come_first_person/important_point/other_person/young_day/work.py, super/life_or_fact/world/thing.py and super/number_or_little_day/early_company/early_company/government/try_large_day_with_place.py beneath first government"

---
+++ super/life_or_fact/world/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    other_case(str_arg)
+    print('little_year')
+
+def other_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('different_problem')
---
+++ super/number_or_little_day/early_company/early_company/government/try_large_day_with_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_high_person(str_arg):
+    think_old_company_with_eye(str_arg)
+    print('use_thing')
+
+def think_old_company_with_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_high_person('part')
