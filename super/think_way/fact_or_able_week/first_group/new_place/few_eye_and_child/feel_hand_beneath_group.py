f15ea5effa2b6fe464d27d409e5b939e09f87305
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 13:14:52 -0400

    "mod: have super/think_way/fact_or_able_week/first_group/new_place/few_eye_and_child/feel_hand_beneath_group.py on bad man"

---
+++ super/think_way/fact_or_able_week/first_group/new_place/few_eye_and_child/feel_hand_beneath_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_year_after_early_child(str_arg):
+    right_group_or_same_company(str_arg)
+    print('group_and_fact')
+
+def right_group_or_same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_year_after_early_child('early_week')
