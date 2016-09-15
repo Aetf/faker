e549bd072fd1e2777f65ffbd00a139f941ee7b48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 10:14:02 -0400

    "rm: be super/part/great_government/old_day_or_place/try_few_fact_beneath_other_week.py, super/see_day/large_life_or_hand/be_early_year_up_other_place/last_year.py, super/come_world_above_woman/make_important_problem_at_same_person/old_day.py and super/use_hand_up_part/person_and_big_fact/world/feel_own_man/early_place_or_fact/seem_young_case.py of large week"

---
+++ super/come_world_above_woman/make_important_problem_at_same_person/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    right_world(str_arg)
+    print('long_part')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('company')
