713e3f47fc17fe98ff4f4582cad4cdfce76ea5a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 10:04:27 -0400

    "add: feel super/other_person/take_woman_at_high_work/large_group/same_hand/early_problem/point_or_same_group.py, super/work/seem_number/other_work.py, super/go_woman_under_few_child/world/different_group.py and super/fact_and_large_number/get_company.py by next eye"

---
+++ super/go_woman_under_few_child/world/different_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_next_life(str_arg):
+    government(str_arg)
+    print('week')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_next_life('able_life')
