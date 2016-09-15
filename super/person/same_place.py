64ef2504d940bf0b9e3a1a4fbe7e0c26695b2f62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 10:46:22 -0400

    "mv: look super/person/same_place.py and super/old_hand/good_person.py up big child"

---
+++ super/person/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_problem(str_arg):
+    large_group(str_arg)
+    print('case')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_problem('place')
