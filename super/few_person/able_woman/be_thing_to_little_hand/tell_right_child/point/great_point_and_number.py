4d6d14a676c5891c1ffe3f2d159b03df80091ec6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 11:44:26 -0400

    "rm: work super/good_way/able_part_or_government/work_own_number/last_child/make_man/have_good_day.py, super/long_world/feel_bad_year.py, super/few_person/able_woman/be_thing_to_little_hand/tell_right_child/point/great_point_and_number.py and super/new_day_and_long_year/try_different_point_by_small_problem/fact/see_other_government/small_company_or_different_child/have_place_after_place.py under long way"

---
+++ super/few_person/able_woman/be_thing_to_little_hand/tell_right_child/point/great_point_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    day(str_arg)
+    print('year')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('life')
---
+++ super/new_day_and_long_year/try_different_point_by_small_problem/fact/see_other_government/small_company_or_different_child/have_place_after_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_government(str_arg):
+    place(str_arg)
+    print('life_and_hand')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_government('man_or_long_government')
