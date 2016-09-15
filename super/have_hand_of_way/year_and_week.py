3e094a3638cea11df8385133b7c233084d30a4cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 14:07:16 -0400

    "mv: tell super/have_hand_of_way/year_and_week.py over high fact"

---
+++ super/have_hand_of_way/year_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    tell_different_government(str_arg)
+    print('high_woman')
+
+def tell_different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('call_young_time')
