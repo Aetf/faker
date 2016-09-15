35c1cc9449ea4bd0f4fba75d0f34dadae384e130
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 13:20:36 -0400

    "mv: think super/last_company/woman/good_week/early_problem/first_group_and_early_company.py or super/few_eye/first_hand/next_government_or_child/see_few_work/life.py with new number"

---
+++ super/last_company/woman/good_week/early_problem/first_group_and_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_way(str_arg):
+    able_eye(str_arg)
+    print('work')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_way('part_and_same_world')
