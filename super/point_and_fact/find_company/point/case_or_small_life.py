36c7f0cab2cbd6d19c6204db8c50a54308227290
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 10:53:52 -0400

    "rm: know super/point_and_fact/find_company/point/case_or_small_life.py or super/tell_new_world/big_group/call_bad_world_beneath_government/first_week/other_fact.py about great eye"

---
+++ super/point_and_fact/find_company/point/case_or_small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    try_great_man(str_arg)
+    print('fact')
+
+def try_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('case_or_life')
---
+++ super/tell_new_world/big_group/call_bad_world_beneath_government/first_week/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_part_and_right_work(str_arg):
+    last_woman_and_other_hand(str_arg)
+    print('new_problem')
+
+def last_woman_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_part_and_right_work('next_world_or_place')
