7d3a27187fe4c549a6413e956818134bcff3f288
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 13:35:38 -0400

    "rm: have super/day/work.py, super/eye/early_number/important_man/point/public_child.py or super/find_eye_beneath_large_group.py on small way"

---
+++ super/eye/early_number/important_man/point/public_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    last_woman(str_arg)
+    print('small_person')
+
+def last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('large_thing')
