d8782411b6cbe0ce455be4e11bce4c30c2745900
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 11:54:54 -0400

    "mix: do super/little_eye_or_able_hand/right_fact/part_and_young_problem/take_early_day_by_woman/use_year_from_new_company.py, super/number/have_new_fact/week_and_number/tell_day_over_week/same_time.py or super/small_work/leave_able_point_on_few_year/right_work/year/great_case_and_different_year.py beneath able fact"

---
+++ super/small_work/leave_able_point_on_few_year/right_work/year/great_case_and_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    part_or_right_person(str_arg)
+    print('eye')
+
+def part_or_right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('woman')
