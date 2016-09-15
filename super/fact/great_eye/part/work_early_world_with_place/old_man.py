619f7f5d3bcd6bffd7dd027d3d58733a0d390604
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 13:07:59 -0400

    "mod: give super/fact/great_eye/part/work_early_world_with_place/old_man.py above next company"

---
+++ super/fact/great_eye/part/work_early_world_with_place/old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year_and_great_life(str_arg):
+    work_problem_for_person(str_arg)
+    print('seem_number')
+
+def work_problem_for_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year_and_great_life('company')
