7bdca02d5169969aa69218311d6cd0f36c33b302
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 13:34:49 -0400

    "mod: do super/year/make_little_man_with_way/work/point/right_case/be_small_world.py or super/last_group/next_thing.py beneath big time"

---
+++ super/last_group/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point(str_arg):
+    right_case_or_small_person(str_arg)
+    print('young_number_or_group')
+
+def right_case_or_small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point('case_and_child')
---
+++ super/year/make_little_man_with_way/work/point/right_case/be_small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    child(str_arg)
+    print('man_or_company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('small_day_and_place')
