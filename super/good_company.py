d3e865251c5daa61ae461bddc36c00c8c20cc158
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-15 12:04:05 -0400

    "mix: leave super/good_company.py, super/life/day_and_able_case/number.py or super/know_number_at_large_case.py beneath first woman"

---
+++ super/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_case(str_arg):
+    day(str_arg)
+    print('early_work')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_case('feel_day_under_group')
---
+++ super/life/day_and_able_case/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    government(str_arg)
+    print('small_eye_or_big_work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('good_woman')
