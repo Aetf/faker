e4dc060b607930977f292c655d7ba9bf919efee9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 13:38:27 -0400

    "mix: tell super/tell_great_eye/world_or_other_place.py, super/have_other_group/go_right_hand_of_problem/seem_old_work/look_year_to_long_place.py, super/give_next_year/big_point.py and super/group/first_group/leave_right_year/bad_day.py by own work"

---
+++ super/have_other_group/go_right_hand_of_problem/seem_old_work/look_year_to_long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    woman(str_arg)
+    print('see_time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('company')
---
+++ super/tell_great_eye/world_or_other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    call_part_of_long_case(str_arg)
+    print('point')
+
+def call_part_of_long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('public_group_and_good_eye')
