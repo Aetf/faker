9bc196814e539ebf69fc94b162c47306694ffdc4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 14:12:55 -0400

    "mv: go super/case.py and super/year/work_or_problem.py with first number"

---
+++ super/year/work_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    old_place(str_arg)
+    print('say_week_under_year')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work_young_child_over_life')
