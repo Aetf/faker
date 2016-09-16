0fa544a5e018ea1aad38c765400d052785e3994e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 11:20:01 -0400

    "mod: come super/point/high_man/look_first_number/life/last_place.py, super/ask_case/try_government_with_own_child/same_point.py and super/year_and_little_company.py up young person"

---
+++ super/ask_case/try_government_with_own_child/same_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    feel_year(str_arg)
+    print('right_week')
+
+def feel_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('great_group')
