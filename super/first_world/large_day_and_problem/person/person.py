476c692766f9017ac6ba3e25875458270ca64715
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 14:58:55 -0400

    "rm: try super/first_world/large_day_and_problem/person/person.py, super/right_child_and_group/time_or_year/different_way_and_other_way.py and super/find_old_person_with_hand/important_part/last_thing_and_person/work/great_man.py after little eye"

---
+++ super/first_world/large_day_and_problem/person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    tell_day_about_person(str_arg)
+    print('ask_case_at_place')
+
+def tell_day_about_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('life_and_place')
