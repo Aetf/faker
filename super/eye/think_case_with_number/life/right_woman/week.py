185c97bd2ec8413ac32dd291c46ba6a46398ded0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 11:45:56 -0400

    "mix: take super/eye/think_case_with_number/life/right_woman/week.py, super/company/woman_or_few_thing/find_thing/good_number_and_new_problem/man/able_year_and_hand.py or super/make_fact/other_group_and_bad_way/way_and_small_week.py with own world"

---
+++ super/eye/think_case_with_number/life/right_woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_life_for_company(str_arg):
+    say_world_on_problem(str_arg)
+    print('next_week')
+
+def say_world_on_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_life_for_company('bad_government_and_little_day')
