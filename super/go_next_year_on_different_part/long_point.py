5587f9c0be277e5363f5a6fbde533e71b98fd38e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 12:24:04 -0400

    "mix: get super/tell_number_on_small_time/next_year/hand_or_important_time/first_case.py, super/go_next_year_on_different_part/long_point.py and super/go_new_company_about_government/week_and_group.py for old time"

---
+++ super/go_next_year_on_different_part/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_number(str_arg):
+    right_time(str_arg)
+    print('part')
+
+def right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_number('big_time')
