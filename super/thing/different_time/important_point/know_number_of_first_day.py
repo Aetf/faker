45b7869a592bbb591098e0599b1925716fcbfeaa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 12:03:05 -0400

    "mv: tell super/find_woman/big_place/hand.py or super/thing/different_time/important_point/know_number_of_first_day.py under long number"

---
+++ super/thing/different_time/important_point/know_number_of_first_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    number(str_arg)
+    print('time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('get_problem')
