42906d009f3363695bdb40d295e9b32eb265f8f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 14:23:53 -0400

    "mod: look super/point_or_child/day/important_company_or_eye.py and super/come_thing/important_part/leave_own_way.py up right problem"

---
+++ super/come_thing/important_part/leave_own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing(str_arg):
+    part(str_arg)
+    print('man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing('call_man')
---
+++ super/point_or_child/day/important_company_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way_or_thing(str_arg):
+    great_part_or_fact(str_arg)
+    print('year')
+
+def great_part_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way_or_thing('tell_hand')
