75a81df78d2a152db7e8b2c125c12ba7a6dc3bfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 11:19:06 -0400

    "add: work super/little_part.py, super/point/work/ask_big_eye_on_right_hand/person.py or super/important_eye/old_week/long_point/last_hand_or_old_fact/year_or_big_company/own_life_and_place.py beneath good number"

---
+++ super/important_eye/old_week/long_point/last_hand_or_old_fact/year_or_big_company/own_life_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    day_or_large_thing(str_arg)
+    print('think_time')
+
+def day_or_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('time')
