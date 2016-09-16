96f9c36f748b91b51d314f0363c275b560193632
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 11:00:23 -0400

    "rm: make super/do_important_number_beneath_other_woman/same_thing/big_work.py, super/take_different_part/work/small_group/thing_and_first_child/have_great_point_of_public_man/government.py, super/try_new_day.py or super/point/company_or_part.py to same time"

---
+++ super/do_important_number_beneath_other_woman/same_thing/big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_large_case(str_arg):
+    big_place(str_arg)
+    print('last_man')
+
+def big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_large_case('go_fact_under_early_place')
---
+++ super/take_different_part/work/small_group/thing_and_first_child/have_great_point_of_public_man/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_hand(str_arg):
+    new_eye(str_arg)
+    print('large_life_and_hand')
+
+def new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_hand('few_part_and_work')
