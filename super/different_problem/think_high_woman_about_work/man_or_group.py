1e02611ea97baf1b62d6c6157164a0fa958161d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 11:17:00 -0400

    "mv: see super/different_problem/think_high_woman_about_work/man_or_group.py and super/person.py for high world"

---
+++ super/different_problem/think_high_woman_about_work/man_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work(str_arg):
+    new_world(str_arg)
+    print('last_year')
+
+def new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work('right_work')
