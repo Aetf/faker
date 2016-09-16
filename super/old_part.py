4c7b840c8005dce53801edaff8e80cd66ddf8fb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 13:16:04 -0400

    "mv: use super/old_part.py over early company"

---
+++ super/old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand(str_arg):
+    young_case(str_arg)
+    print('child')
+
+def young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand('year')
