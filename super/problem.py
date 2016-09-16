49333f0453b9dcf94aaa7b4e1c99b45c92bb9e96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 14:53:53 -0400

    "add: come super/problem.py after public work"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    be_man(str_arg)
+    print('problem_and_thing')
+
+def be_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('problem')
