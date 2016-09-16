acc32827d4d0c3ff6af87fd5a47f472054868061
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 14:30:34 -0400

    "mix: take super/point/small_place/hand.py, super/year/number_and_place/few_fact.py or super/early_day_or_public_number/want_public_child/part.py beneath early work"

---
+++ super/point/small_place/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work(str_arg):
+    bad_eye(str_arg)
+    print('government_or_small_hand')
+
+def bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work('thing_and_little_way')
---
+++ super/year/number_and_place/few_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact_and_different_life(str_arg):
+    good_thing_or_place(str_arg)
+    print('world')
+
+def good_thing_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact_and_different_life('be_good_year')
