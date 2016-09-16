bee6c5cfef7a28b74848b99b6ae90bd3d14bf3ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-16 12:56:59 -0400

    "rm: make super/hand/life/have_last_world/big_woman_or_group.py and super/high_place/other_hand_and_child/eye/go_group.py beneath next year"

---
+++ super/high_place/other_hand_and_child/eye/go_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year_and_world(str_arg):
+    group(str_arg)
+    print('fact')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year_and_world('tell_man_beneath_little_day')
