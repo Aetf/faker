8c1257fcff1971e422998038171005697cb337a8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 11:05:51 -0400

    "mv: call super/week_and_old_point.py or super/seem_fact_on_world/day_or_few_year/work.py beneath different work"

---
+++ super/seem_fact_on_world/day_or_few_year/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child(str_arg):
+    part_and_government(str_arg)
+    print('ask_world_from_life')
+
+def part_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child('try_few_work')
