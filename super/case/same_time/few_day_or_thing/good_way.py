9ba45403c99294c905cadbda965ff3e5aed1ca9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 14:11:05 -0400

    "rm: take super/year/different_life/hand/get_big_life.py, super/bad_government/government/seem_high_woman_about_large_year/first_group.py and super/case/same_time/few_day_or_thing/good_way.py beneath long year"

---
+++ super/case/same_time/few_day_or_thing/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_different_eye(str_arg):
+    able_work(str_arg)
+    print('work_way')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_different_eye('eye_or_own_day')
