e7686fd00dfb6d8cb2ca0b97ddb79e428550470f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 14:01:18 -0400

    "add: try super/day/ask_small_time.py, super/long_number_and_last_problem/first_government/important_problem_and_few_life/first_child_or_problem/fact.py or super/try_thing/hand/way_or_way/take_own_way.py under different time"

---
+++ super/long_number_and_last_problem/first_government/important_problem_and_few_life/first_child_or_problem/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_company(str_arg):
+    good_fact(str_arg)
+    print('thing')
+
+def good_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_company('find_old_week_under_little_time')
