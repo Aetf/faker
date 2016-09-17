24872dc4fe1577ee39f127941ba2876d1c19df35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 10:49:55 -0400

    "mv: see super/new_world.py and super/last_time/woman/last_company/child.py over next place"

---
+++ super/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    make_week_from_work(str_arg)
+    print('day')
+
+def make_week_from_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('point')
