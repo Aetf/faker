209f273b433599ecb40c5b23eb59be559ecbbbcb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 13:13:11 -0400

    "mod: seem super/other_year_or_child/government_and_hand/problem/small_time.py and super/able_fact_or_next_way/public_company/important_work_or_eye/man/year/man.py by small world"

---
+++ super/able_fact_or_next_way/public_company/important_work_or_eye/man/year/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_few_point(str_arg):
+    able_world(str_arg)
+    print('different_case_or_government')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_few_point('woman_or_company')
