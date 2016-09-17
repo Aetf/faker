767f713080b3b4cc10d84e52a4122088136a9bfd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 14:21:14 -0400

    "rm: find super/other_company_or_child/group/call_world_with_great_part/try_important_thing_from_public_company.py and super/bad_company_and_little_eye/ask_woman_under_high_world/do_fact/point/own_life_or_high_point/same_time.py from old place"

---
+++ super/bad_company_and_little_eye/ask_woman_under_high_world/do_fact/point/own_life_or_high_point/same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man_at_different_government(str_arg):
+    large_place(str_arg)
+    print('say_time')
+
+def large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man_at_different_government('seem_able_number')
---
+++ super/other_company_or_child/group/call_world_with_great_part/try_important_thing_from_public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_good_case_for_last_number(str_arg):
+    fact_or_day(str_arg)
+    print('small_group_and_day')
+
+def fact_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_good_case_for_last_number('high_group')
