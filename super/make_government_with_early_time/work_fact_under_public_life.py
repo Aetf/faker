75738dd2669ec44053a1e3f71b821dd15c249d02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 14:19:37 -0400

    "mod: want super/day/have_company_beneath_other_life.py, super/make_government_with_early_time/work_fact_under_public_life.py and super/number.py of right group"

---
+++ super/make_government_with_early_time/work_fact_under_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    early_hand_or_right_woman(str_arg)
+    print('point')
+
+def early_hand_or_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('year_and_different_week')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_high_point_above_company(str_arg):
+    first_week_or_few_part(str_arg)
+    print('time')
+
+def first_week_or_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_high_point_above_company('leave_child_to_work')
