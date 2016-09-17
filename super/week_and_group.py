18c0a717bc85c23be71859795004147bdf1bcbb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 12:08:58 -0400

    "mod: say super/week_and_group.py about same government"

---
+++ super/week_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_child(str_arg):
+    think_different_way(str_arg)
+    print('place')
+
+def think_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_child('group')
