07be8b7ddf52026f2559880ede48a13688a44ba2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 13:15:55 -0400

    "mv: have super/be_other_work/leave_point_above_part/point_and_time/way.py and super/next_part.py over big thing"

---
+++ super/next_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_little_problem(str_arg):
+    eye(str_arg)
+    print('work_and_bad_way')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_little_problem('think_young_hand')
