47305047212a42ef6de2644a961329afff7c3bb8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 13:00:50 -0400

    "mv: have super/world_or_person.py, super/man_and_fact/little_hand/same_way.py or super/part_or_thing.py up good company"

---
+++ super/man_and_fact/little_hand/same_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_person_and_important_week(str_arg):
+    week(str_arg)
+    print('life')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_person_and_important_week('next_year_or_large_woman')
---
+++ super/world_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_point(str_arg):
+    public_time(str_arg)
+    print('other_work_or_right_government')
+
+def public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_point('number')
