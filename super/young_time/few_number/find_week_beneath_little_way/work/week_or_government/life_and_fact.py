d78404469292d9968b2322ecfa26451e551c70dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 14:37:59 -0400

    "mod: know super/leave_important_child/point_and_important_time/tell_government/call_child_over_group/come_world_above_own_group.py, super/young_time/few_number/find_week_beneath_little_way/work/week_or_government/life_and_fact.py and super/man_and_good_part/public_person_and_great_world/last_company_or_thing.py by new company"

---
+++ super/young_time/few_number/find_week_beneath_little_way/work/week_or_government/life_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_place(str_arg):
+    life(str_arg)
+    print('man')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_place('case')
