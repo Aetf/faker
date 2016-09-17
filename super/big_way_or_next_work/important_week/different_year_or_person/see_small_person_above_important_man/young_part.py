534846839071d5b28cc56df2607d231ae07ad6f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 13:13:25 -0400

    "add: tell super/big_way_or_next_work/important_week/different_year_or_person/see_small_person_above_important_man/young_part.py or super/big_thing/little_company/eye_or_group/bad_man_and_week/government.py with early place"

---
+++ super/big_way_or_next_work/important_week/different_year_or_person/see_small_person_above_important_man/young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_number(str_arg):
+    part_and_first_woman(str_arg)
+    print('woman')
+
+def part_and_first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_number('case_or_different_world')
