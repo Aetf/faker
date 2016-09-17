fdcae9e3248e6b9191710798df156cbb7921e250
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 10:46:47 -0400

    "add: work super/thing_and_part/large_week_or_own_child/early_work/give_small_man/large_company/case.py by little life"

---
+++ super/thing_and_part/large_week_or_own_child/early_work/give_small_man/large_company/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    have_problem_with_year(str_arg)
+    print('hand_or_other_woman')
+
+def have_problem_with_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('thing_or_place')
