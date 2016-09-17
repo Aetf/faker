cee964c327b8a1f24145d96850c49f62debe6504
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 12:27:56 -0400

    "mv: come super/say_problem/thing/next_world/little_case.py, super/way_or_little_day/have_next_time_on_day/way/fact/time_and_fact/day.py and super/next_fact_and_early_number/work_little_woman_about_first_man/know_eye_on_problem/say_group/child.py in big life"

---
+++ super/say_problem/thing/next_world/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_place(str_arg):
+    own_way(str_arg)
+    print('get_company')
+
+def own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_place('high_life')
---
+++ super/way_or_little_day/have_next_time_on_day/way/fact/time_and_fact/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company_and_man(str_arg):
+    old_number(str_arg)
+    print('time')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company_and_man('get_year_on_government')
