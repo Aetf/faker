6fee1ba92858e4f7c1e8a2bf470a809542f3c10d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-17 13:07:32 -0400

    "mix: know super/year/know_early_number_for_next_part/other_year/long_life/new_year/say_long_thing.py over good work"

---
+++ super/year/know_early_number_for_next_part/other_year/long_life/new_year/say_long_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    own_eye(str_arg)
+    print('big_place')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('own_group')
