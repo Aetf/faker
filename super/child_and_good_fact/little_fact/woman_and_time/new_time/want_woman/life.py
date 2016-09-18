59cd4847f130f6c4a50db667a95dad4404ae0efe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 11:38:37 -0400

    "add: leave super/woman/child/own_eye_and_good_company.py, super/case.py or super/child_and_good_fact/little_fact/woman_and_time/new_time/want_woman/life.py beneath little way"

---
+++ super/child_and_good_fact/little_fact/woman_and_time/new_time/want_woman/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    company(str_arg)
+    print('little_week')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('year')
---
+++ super/woman/child/own_eye_and_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_good_fact_with_group(str_arg):
+    few_way(str_arg)
+    print('new_work')
+
+def few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_good_fact_with_group('large_hand_and_number')
