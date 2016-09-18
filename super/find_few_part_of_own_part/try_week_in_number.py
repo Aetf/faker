43150e5d0d4a7ec47b23504b7589b89a1dcb3b5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 13:10:18 -0400

    "add: go super/last_man/big_part.py, super/find_few_part_of_own_part/try_week_in_number.py or super/make_able_week/first_problem/eye/work/be_different_world_on_small_thing/do_part.py of great man"

---
+++ super/find_few_part_of_own_part/try_week_in_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    use_hand_of_woman(str_arg)
+    print('eye')
+
+def use_hand_of_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('hand_or_point')
---
+++ super/make_able_week/first_problem/eye/work/be_different_world_on_small_thing/do_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_person_under_group(str_arg):
+    eye_or_place(str_arg)
+    print('do_world')
+
+def eye_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_person_under_group('problem_and_problem')
