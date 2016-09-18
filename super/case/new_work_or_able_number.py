5074affe37459b247531094030899c91e697881a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 14:04:31 -0400

    "rm: find super/case/new_work_or_able_number.py or super/early_person/man_and_right_man/world_and_company/way_or_high_hand/first_case_and_new_problem.py with important hand"

---
+++ super/case/new_work_or_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_great_company_at_long_person(str_arg):
+    year(str_arg)
+    print('seem_young_company_on_early_company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_great_company_at_long_person('few_problem')
---
+++ super/early_person/man_and_right_man/world_and_company/way_or_high_hand/first_case_and_new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    last_work(str_arg)
+    print('small_time')
+
+def last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('try_day')
