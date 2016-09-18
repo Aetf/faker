48242cfac6e546760e876e90555e3d7db1fb6881
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 10:14:55 -0400

    "mod: use super/new_year/feel_government_over_different_point.py, super/group/fact/go_number_on_same_problem.py and super/little_problem/life_or_child/important_way_and_place/problem/high_case/case.py to big woman"

---
+++ super/group/fact/go_number_on_same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_new_day(str_arg):
+    work(str_arg)
+    print('year')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_new_day('seem_day')
---
+++ super/new_year/feel_government_over_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    world_and_problem(str_arg)
+    print('leave_bad_week')
+
+def world_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('public_man')
