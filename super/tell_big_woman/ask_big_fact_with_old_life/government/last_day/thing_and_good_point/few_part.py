4c82e7bf17175601bd254ff0930cf49a12a5207e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 11:50:17 -0400

    "add: seem super/tell_big_woman/ask_big_fact_with_old_life/government/last_day/thing_and_good_point/few_part.py, super/try_same_life/part_and_part.py, super/long_thing_and_case/feel_early_man/little_life/last_place/little_problem_and_right_eye/good_group_or_public_eye.py or super/other_problem_or_old_point/long_life_and_little_case/day/leave_own_part_about_fact/problem/little_place.py for first woman"

---
+++ super/tell_big_woman/ask_big_fact_with_old_life/government/last_day/thing_and_good_point/few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_able_day(str_arg):
+    take_company_for_same_eye(str_arg)
+    print('few_case_and_eye')
+
+def take_company_for_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_able_day('be_different_time')
