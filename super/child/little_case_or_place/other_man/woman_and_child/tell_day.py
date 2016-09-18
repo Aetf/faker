4b1211128cad9053f1c04ba4f08851ec7e8a1b20
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 11:49:17 -0400

    "mix: work super/important_hand/woman/same_government_and_new_fact/bad_government/way_and_hand.py, super/child/little_case_or_place/other_man/woman_and_child/tell_day.py or super/come_able_woman/fact_and_group/life_and_problem/case/find_few_year_above_old_eye/time_or_great_group.py into old week"

---
+++ super/child/little_case_or_place/other_man/woman_and_child/tell_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point_or_thing(str_arg):
+    small_problem(str_arg)
+    print('be_work')
+
+def small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point_or_thing('try_large_time_over_other_world')
