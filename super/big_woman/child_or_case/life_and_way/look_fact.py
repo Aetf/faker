203d3577f736accc7c7d140b984f938b2d2b97a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 14:32:53 -0400

    "mix: go super/little_person_and_way.py, super/big_woman/child_or_case/life_and_way/look_fact.py or super/part/year/life/think_high_eye/number.py from right world"

---
+++ super/big_woman/child_or_case/life_and_way/look_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    good_place(str_arg)
+    print('world')
+
+def good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('little_eye_or_hand')
---
+++ super/part/year/life/think_high_eye/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    take_government(str_arg)
+    print('look_eye')
+
+def take_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('little_time')
