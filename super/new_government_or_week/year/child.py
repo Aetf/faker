c34b7959c8ef4b481d480544c1ac8309654ce26d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 13:59:27 -0400

    "add: seem super/part/want_thing/take_man_at_year/next_eye/look_old_number_from_life.py, super/new_government_or_week/year/child.py, super/own_number/bad_company/great_work/way_or_first_point.py or super/long_woman_and_public_place.py with bad hand"

---
+++ super/new_government_or_week/year/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    say_big_problem(str_arg)
+    print('little_year')
+
+def say_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('go_government_of_different_day')
