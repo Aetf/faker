1ff1b302976711b6b8561e11ff72e7ccad9c9f85
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 10:16:15 -0400

    "mod: call super/same_part_or_man/last_part/early_thing_and_number/high_world/part_and_week.py, super/company/use_man/group_and_few_week.py, super/company/see_right_group.py or super/give_thing.py to last fact"

---
+++ super/company/see_right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_life(str_arg):
+    other_year(str_arg)
+    print('hand')
+
+def other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_life('problem_or_last_day')
---
+++ super/give_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    able_way_or_life(str_arg)
+    print('work')
+
+def able_way_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('use_man_after_company')
