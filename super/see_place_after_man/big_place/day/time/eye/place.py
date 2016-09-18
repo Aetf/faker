996dfeeaf27eeccc72e37380a7d54317da7f290d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 14:54:20 -0400

    "add: give super/see_place_after_man/big_place/day/time/eye/place.py, super/ask_fact/government/use_eye_at_different_person.py and super/part_and_new_world/big_week/do_great_woman/long_man/hand.py for young group"

---
+++ super/see_place_after_man/big_place/day/time/eye/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number(str_arg):
+    next_group(str_arg)
+    print('thing_and_other_fact')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number('able_time_and_week')
