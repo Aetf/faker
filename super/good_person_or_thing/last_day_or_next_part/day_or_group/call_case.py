aadf5778c61ebf8eba3bed0093228d0b237149fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 13:10:59 -0400

    "add: feel super/good_person_or_thing/last_day_or_next_part/day_or_group/call_case.py after few year"

---
+++ super/good_person_or_thing/last_day_or_next_part/day_or_group/call_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    bad_point_and_own_time(str_arg)
+    print('feel_first_time_of_high_woman')
+
+def bad_point_and_own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('be_man')
