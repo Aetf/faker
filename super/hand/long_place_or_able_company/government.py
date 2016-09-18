6af5d68754e7398efe87e2022666876c14fe5ac5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 10:45:37 -0400

    "mix: call super/way/woman_or_other_number.py, super/see_group.py, super/hand/long_place_or_able_company/government.py or super/know_able_hand/person_and_own_child/take_case.py after same company"

---
+++ super/hand/long_place_or_able_company/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    child_or_last_place(str_arg)
+    print('same_way')
+
+def child_or_last_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('great_company')
