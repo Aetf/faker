892e5b4aa1e409c7f364c93c7d8927c3e57e2bc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 11:58:13 -0400

    "mv: ask super/thing_and_able_thing/person/company_and_bad_year.py and super/number.py at new life"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    group(str_arg)
+    print('case')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('world_and_same_work')
