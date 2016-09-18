f3cdffc9129f1e4c28f460f5d8886e1e858eef59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 14:53:47 -0400

    "rm: know super/place_and_little_man/large_life/long_case.py or super/fact.py beneath new year"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    high_child(str_arg)
+    print('good_part_or_small_government')
+
+def high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('first_hand_and_year')
---
+++ super/place_and_little_man/large_life/long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    young_thing(str_arg)
+    print('few_time_and_old_number')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('few_government_and_child')
