f52e6703cc05e3cd5a3aa89d13d54d420219fc29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 13:45:09 -0400

    "mix: give super/give_woman/long_child_or_early_point.py, super/bad_point_and_point/eye/man/time/next_hand/world.py, super/next_problem.py and super/first_point_or_new_government/company_and_point/try_own_thing.py of right government"

---
+++ super/next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    try_life_about_own_way(str_arg)
+    print('child')
+
+def try_life_about_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('early_person')
