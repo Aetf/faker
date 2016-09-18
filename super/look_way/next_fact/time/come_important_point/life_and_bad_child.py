45f32f4f99e35a7fca0d66f73bd23a98b9df16ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-18 12:23:17 -0400

    "mix: call super/feel_bad_point_for_day/man.py or super/look_way/next_fact/time/come_important_point/life_and_bad_child.py from same government"

---
+++ super/look_way/next_fact/time/come_important_point/life_and_bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    bad_thing_or_old_fact(str_arg)
+    print('number_or_person')
+
+def bad_thing_or_old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('woman')
