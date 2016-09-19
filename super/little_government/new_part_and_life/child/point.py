989dbf995dd53770bb82912075ac0b6daa77ce70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 13:12:43 -0400

    "mix: know super/little_government/new_part_and_life/child/point.py or super/early_child/way_and_week/small_man_and_same_group.py to right group"

---
+++ super/little_government/new_part_and_life/child/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company(str_arg):
+    use_person(str_arg)
+    print('come_first_eye_up_great_way')
+
+def use_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company('public_person')
