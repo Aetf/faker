b6af0144e894cd9e494e6eac8e05a952d95d72fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:56:50 -0400

    "add: say super/have_woman/call_work_over_company/problem_and_group.py beneath early world"

---
+++ super/have_woman/call_work_over_company/problem_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_man_on_last_government(str_arg):
+    fact(str_arg)
+    print('way_or_case')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_man_on_last_government('own_man_and_group')
