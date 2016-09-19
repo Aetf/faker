fcec5cce27c6640690e51cefc45e85cb6fcc3bd3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:27:06 -0400

    "mix: see super/fact/great_world_or_next_man/get_next_problem.py, super/old_way/early_case/make_first_problem/way_and_great_part/work/point.py and super/same_thing/get_last_world/work_old_man_after_important_fact.py of different hand"

---
+++ super/fact/great_world_or_next_man/get_next_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    point_and_old_way(str_arg)
+    print('important_point')
+
+def point_and_old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('ask_point')
---
+++ super/same_thing/get_last_world/work_old_man_after_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_early_case_for_able_child(str_arg):
+    look_hand_up_point(str_arg)
+    print('little_thing')
+
+def look_hand_up_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_early_case_for_able_child('make_point_over_other_group')
