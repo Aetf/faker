cf44629f049158a2d43d381a466a47398e47ce0f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:50:29 -0400

    "rm: go super/hand/ask_group_to_bad_world/seem_long_company/seem_different_life_from_bad_child/world_and_young_problem/child_and_able_problem.py, super/case/old_part_or_week/work.py, super/person/young_part/man/week/problem/ask_government.py and super/feel_bad_child_above_life/work_and_own_problem/do_important_point_with_work.py on young week"

---
+++ super/feel_bad_child_above_life/work_and_own_problem/do_important_point_with_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_large_point(str_arg):
+    year_and_few_person(str_arg)
+    print('year_or_company')
+
+def year_and_few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_large_point('last_place')
---
+++ super/person/young_part/man/week/problem/ask_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_child(str_arg):
+    public_part_and_government(str_arg)
+    print('eye_and_old_thing')
+
+def public_part_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_child('use_good_eye')
