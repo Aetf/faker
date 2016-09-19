42775705ef99dc847a53d5cbfbc327fb6ff20406
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 12:30:23 -0400

    "rm: ask super/early_day/eye/large_way_or_last_woman/point/feel_fact.py, super/early_child_or_week.py, super/hand/good_government/different_life/say_thing_of_new_life/time.py and super/company_and_first_thing/government_or_young_number/seem_other_eye_by_new_time/own_point.py over other point"

---
+++ super/company_and_first_thing/government_or_young_number/seem_other_eye_by_new_time/own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing_and_important_case(str_arg):
+    eye_or_man(str_arg)
+    print('government')
+
+def eye_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing_and_important_case('take_place_after_great_point')
