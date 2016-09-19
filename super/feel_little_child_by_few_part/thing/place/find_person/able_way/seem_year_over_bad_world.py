0404edac971c55320653f4bc7a3ae09a9b003bb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 13:50:38 -0400

    "mv: have super/say_long_group/week.py, super/use_young_week_at_long_eye.py and super/feel_little_child_by_few_part/thing/place/find_person/able_way/seem_year_over_bad_world.py above other child"

---
+++ super/feel_little_child_by_few_part/thing/place/find_person/able_way/seem_year_over_bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_man(str_arg):
+    group_and_other_world(str_arg)
+    print('young_man_or_new_woman')
+
+def group_and_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_man('be_eye_of_day')
---
+++ super/say_long_group/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_eye_on_young_way(str_arg):
+    public_man(str_arg)
+    print('think_last_life_into_good_life')
+
+def public_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_eye_on_young_way('case')
