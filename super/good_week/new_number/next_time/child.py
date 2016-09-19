44ea5b3793273c2afd6866788a2a8c6011c8702a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:34:50 -0400

    "add: come super/good_week/new_number/next_time/child.py on first number"

---
+++ super/good_week/new_number/next_time/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    case(str_arg)
+    print('public_day')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('place_or_little_woman')
