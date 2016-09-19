31a1065b233cf9bcf5145df59e05264e2a39553f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 12:39:16 -0400

    "add: feel super/leave_next_person/company/week_and_good_day/eye/point/see_case_over_woman.py beneath old number"

---
+++ super/leave_next_person/company/week_and_good_day/eye/point/see_case_over_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_early_way_after_important_company(str_arg):
+    public_point(str_arg)
+    print('little_world_or_part')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_early_way_after_important_company('public_life_or_few_case')
