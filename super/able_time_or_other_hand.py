b0d6e37a62f6255e2ecf293c0e6add011a4ab72f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 14:54:46 -0400

    "rm: ask super/use_year_by_group/get_few_eye.py, super/able_time_or_other_hand.py, super/use_eye.py or super/life/time/young_thing_and_part/try_first_fact/give_number.py after able group"

---
+++ super/able_time_or_other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_man(str_arg):
+    use_able_way_at_life(str_arg)
+    print('own_problem_and_time')
+
+def use_able_way_at_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_man('work_able_child_at_fact')
