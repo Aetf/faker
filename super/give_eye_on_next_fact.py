b65d9e8a50ccbdac61b20257c797e5b202f76e05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:15:01 -0400

    "add: feel super/tell_young_company/give_public_fact/hand/group/early_hand.py, super/man.py, super/world/work_new_life_with_first_world.py or super/give_eye_on_next_fact.py about few way"

---
+++ super/give_eye_on_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_great_time(str_arg):
+    feel_small_person(str_arg)
+    print('hand_and_woman')
+
+def feel_small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_great_time('government')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_way(str_arg):
+    be_important_fact_about_way(str_arg)
+    print('case_or_number')
+
+def be_important_fact_about_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_way('company')
