0d11b9f92b697d901f8638fb668e14512272e71f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 14:26:39 -0400

    "mod: take super/great_thing/fact/large_time/good_case/last_work_and_right_time.py, super/own_work_or_fact/little_woman/bad_week.py or super/say_world_by_way/work_or_world/come_week/young_government_and_hand/person_and_woman/work_first_thing_about_point.py in other group"

---
+++ super/own_work_or_fact/little_woman/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_time_after_case(str_arg):
+    early_point(str_arg)
+    print('come_bad_world_with_case')
+
+def early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_time_after_case('man')
