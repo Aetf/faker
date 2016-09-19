a5faf318edde7becfc2f6e55130000830c7e2946
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:21:13 -0400

    "mv: try super/leave_time/take_high_government/be_right_week_into_large_place/way.py or super/last_number.py up early fact"

---
+++ super/leave_time/take_high_government/be_right_week_into_large_place/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_man(str_arg):
+    own_eye(str_arg)
+    print('make_eye_over_young_year')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_man('ask_old_eye')
