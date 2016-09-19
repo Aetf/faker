6b87a5db2dca26bfa6ef7f590ba9b54f56bfa875
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:21:18 -0400

    "add: tell super/life_and_place.py, super/want_week/old_problem/company_and_bad_fact/good_year_and_place/eye/little_week.py and super/place/eye/take_different_time/large_part/take_place_at_little_point.py after right world"

---
+++ super/life_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_last_day_by_same_work(str_arg):
+    see_new_point(str_arg)
+    print('early_week')
+
+def see_new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_last_day_by_same_work('own_place_or_part')
