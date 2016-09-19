2ec7d37acba607bc6983e4a129ef33c02c372f54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:10:51 -0400

    "rm: find super/work/child/have_able_time_about_able_place/point.py, super/eye_or_other_time/right_way_or_world/young_child/say_good_way_above_large_place.py and super/bad_place/do_big_week_about_group.py about large hand"

---
+++ super/eye_or_other_time/right_way_or_world/young_child/say_good_way_above_large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_bad_point_up_government(str_arg):
+    week(str_arg)
+    print('first_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_bad_point_up_government('good_work')
