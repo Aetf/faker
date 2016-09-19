682c6e423db6c40f3b4ffa9a3f2c55adbd72176c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:54:43 -0400

    "rm: do super/next_thing.py from other world"

--- super/next_thing.py
+++ super/next_thing.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def case(str_arg):
+def old_work(str_arg):
     work(str_arg)
-    print('week')
+    print('day_and_time')
 
 def work(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    case('big_time_or_government')
+    old_work('small_child')
