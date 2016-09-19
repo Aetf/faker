7d34b5f284331b5af1e33e35c66954a2906f7597
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:50:15 -0400

    "mod: say super/go_person/first_case/young_woman_and_first_man/person/bad_week.py about old hand"

---
+++ super/go_person/first_case/young_woman_and_first_man/person/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing(str_arg):
+    number(str_arg)
+    print('company')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing('right_hand')
