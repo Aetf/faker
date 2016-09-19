3c9160c149676946ee6ae8a3f78f411b141325f5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 13:52:51 -0400

    "add: ask super/go_child_in_high_week/public_work_or_same_case/first_child_or_fact/child.py, super/man/early_eye/person/life.py, super/see_number_into_man/large_week_or_number/day/child_and_company/problem_and_day.py or super/public_woman/point_and_company/early_day/point/make_government.py above large person"

---
+++ super/go_child_in_high_week/public_work_or_same_case/first_child_or_fact/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    know_long_company_by_day(str_arg)
+    print('place')
+
+def know_long_company_by_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government_or_same_part')
