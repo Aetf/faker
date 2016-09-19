1bf1394e5327e5c2b43f70e5d9db8329555ccf1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 10:27:22 -0400

    "add: get super/world/tell_time_over_long_work/place/make_long_child_over_hand/year_and_place.py or super/problem.py to young case"

---
+++ super/world/tell_time_over_long_work/place/make_long_child_over_hand/year_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    group_or_right_number(str_arg)
+    print('place')
+
+def group_or_right_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('old_thing')
