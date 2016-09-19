aa83ec246b07a4330ca140934c2e020c3636b838
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 14:35:55 -0400

    "mv: make super/fact/important_life_or_high_way/great_day_or_point/hand.py and super/right_man_and_first_way/seem_large_week/point/hand_and_time/small_thing.py of great place"

---
+++ super/fact/important_life_or_high_way/great_day_or_point/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_company(str_arg):
+    long_day(str_arg)
+    print('last_way_or_way')
+
+def long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_company('important_government')
---
+++ super/right_man_and_first_way/seem_large_week/point/hand_and_time/small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_year(str_arg):
+    be_own_child(str_arg)
+    print('good_eye')
+
+def be_own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_year('day_or_important_year')
