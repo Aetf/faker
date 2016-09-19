47982512712b2a3134b6a75b594fb64e4418d264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 11:28:44 -0400

    "mv: see super/take_eye_on_bad_fact.py under early person"

---
+++ super/take_eye_on_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    few_life(str_arg)
+    print('important_way')
+
+def few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('leave_eye')
