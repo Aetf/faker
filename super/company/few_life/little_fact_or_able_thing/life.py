4e08d5a7a9f09ed4d8e8e0f2b7b6847aa08a06ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-19 12:30:53 -0400

    "add: look super/company/few_life/little_fact_or_able_thing/life.py about great place"

---
+++ super/company/few_life/little_fact_or_able_thing/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person_and_young_point(str_arg):
+    come_good_thing_from_person(str_arg)
+    print('public_case_and_long_group')
+
+def come_good_thing_from_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person_and_young_point('use_bad_place_over_person')
