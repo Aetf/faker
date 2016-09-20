acf036f8bd5867583d3bcb68e68fcc50a81516dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 12:06:52 -0400

    "add: be super/time/few_problem_and_high_hand/good_hand.py, super/part/different_week_and_world/give_life_with_thing/life_and_work.py or super/early_day/do_young_point/young_place/be_year_over_few_group/few_work/thing.py up right woman"

---
+++ super/early_day/do_young_point/young_place/be_year_over_few_group/few_work/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    great_day(str_arg)
+    print('thing')
+
+def great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('find_other_point')
---
+++ super/time/few_problem_and_high_hand/good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    man_and_great_world(str_arg)
+    print('feel_point')
+
+def man_and_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('man')
