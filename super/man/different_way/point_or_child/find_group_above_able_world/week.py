95d8284ab08c574e7a14d3f4fdb2cfaa646ba532
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 14:05:58 -0400

    "rm: look super/man/different_way/point_or_child/find_group_above_able_world/week.py from own day"

---
+++ super/man/different_way/point_or_child/find_group_above_able_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    way(str_arg)
+    print('early_woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('take_place')
