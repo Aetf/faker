25d162f983ad9a282117b637d8d5dcc0e3d9f44b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 13:49:02 -0400

    "add: ask super/small_day.py on important work"

---
+++ super/small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_case(str_arg):
+    good_eye(str_arg)
+    print('small_problem')
+
+def good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_case('last_work')
