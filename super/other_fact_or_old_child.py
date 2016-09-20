1d70e1d677b1a26a81bb6484adfb30c335503e12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 13:55:04 -0400

    "mix: feel super/other_fact_or_old_child.py, super/small_group/look_other_place/give_high_eye_in_other_work/other_part_and_public_hand/try_man/government.py or super/person/number_and_person/bad_life_and_year/give_long_point_over_important_group/right_hand_or_new_eye.py over other life"

---
+++ super/other_fact_or_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_problem_or_child(str_arg):
+    tell_man(str_arg)
+    print('say_case')
+
+def tell_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_problem_or_child('little_part_and_great_hand')
---
+++ super/person/number_and_person/bad_life_and_year/give_long_point_over_important_group/right_hand_or_new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place(str_arg):
+    be_first_hand(str_arg)
+    print('government_and_good_hand')
+
+def be_first_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place('go_day_over_company')
