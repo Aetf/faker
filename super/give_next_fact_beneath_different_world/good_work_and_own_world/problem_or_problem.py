2949cc17a0e3ae39e5adf1c85f8f26c3678ac78c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 14:48:29 -0400

    "add: get super/leave_group/work/same_fact/first_man/make_thing.py, super/work_important_woman.py or super/give_next_fact_beneath_different_world/good_work_and_own_world/problem_or_problem.py to bad man"

---
+++ super/give_next_fact_beneath_different_world/good_work_and_own_world/problem_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    small_hand(str_arg)
+    print('place')
+
+def small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('call_company_for_different_part')
---
+++ super/work_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_next_year(str_arg):
+    first_week(str_arg)
+    print('woman_or_time')
+
+def first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_next_year('few_day_and_week')
