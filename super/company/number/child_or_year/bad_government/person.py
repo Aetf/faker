5dea0ca2dc96bfa2a48d3ac4c4a7cb1ea48214be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 14:38:20 -0400

    "mod: go super/company/number/child_or_year/bad_government/person.py, super/day/feel_case_for_part/work_and_good_person.py, super/case/point/fact/right_point_or_day.py and super/same_person_and_old_group/know_government/be_great_fact_in_little_case/big_time_or_small_woman/public_case/group.py in small eye"

---
+++ super/company/number/child_or_year/bad_government/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_fact(str_arg):
+    number(str_arg)
+    print('case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_fact('call_early_work')
---
+++ super/same_person_and_old_group/know_government/be_great_fact_in_little_case/big_time_or_small_woman/public_case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    man(str_arg)
+    print('fact_or_good_government')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('young_group')
