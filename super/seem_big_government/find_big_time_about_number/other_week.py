6416bb514ac1b3690400a600476a159b28986878
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 13:55:27 -0400

    "rm: look super/seem_big_government/find_big_time_about_number/other_week.py or super/work_few_group_by_problem/high_work_or_week/try_own_case_in_time.py above different person"

---
+++ super/seem_big_government/find_big_time_about_number/other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    thing_or_woman(str_arg)
+    print('say_public_woman_under_company')
+
+def thing_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('way_and_man')
---
+++ super/work_few_group_by_problem/high_work_or_week/try_own_case_in_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    point_or_problem(str_arg)
+    print('use_place')
+
+def point_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('bad_person')
