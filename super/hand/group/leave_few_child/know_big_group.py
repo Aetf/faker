35bb8c3a536eda2d817f6f4acdf6a94b1823abe9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 11:32:08 -0400

    "add: be super/get_public_number.py, super/hand/group/leave_few_child/know_big_group.py, super/different_world.py and super/other_place/hand_or_problem.py over few point"

---
+++ super/hand/group/leave_few_child/know_big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    few_way(str_arg)
+    print('week')
+
+def few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('old_year_or_hand')
---
+++ super/other_place/hand_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    point(str_arg)
+    print('get_same_work')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('own_man')
