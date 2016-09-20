4414ae4727211816a06f04e95783c9c36f3a4252
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 11:27:37 -0400

    "mv: make super/use_public_fact/large_number_and_day.py and super/way/early_world/part_or_public_number.py over new year"

---
+++ super/way/early_world/part_or_public_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    ask_old_world(str_arg)
+    print('make_group_above_other_group')
+
+def ask_old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('old_problem_and_group')
