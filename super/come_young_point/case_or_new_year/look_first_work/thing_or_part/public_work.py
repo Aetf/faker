798549a7c286a14a67f9fe2f26a2ac8f70b87d13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 12:34:55 -0400

    "mix: tell super/point_and_right_life.py or super/come_young_point/case_or_new_year/look_first_work/thing_or_part/public_work.py in right way"

---
+++ super/come_young_point/case_or_new_year/look_first_work/thing_or_part/public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_other_problem(str_arg):
+    eye(str_arg)
+    print('group_and_few_child')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_other_problem('right_day_and_good_problem')
