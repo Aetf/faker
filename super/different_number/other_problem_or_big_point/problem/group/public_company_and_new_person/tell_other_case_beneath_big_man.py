5a24215d442aebd179eb87620fd74051b2f04532
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 14:41:57 -0400

    "mod: do super/different_number/other_problem_or_big_point/problem/group/public_company_and_new_person/tell_other_case_beneath_big_man.py for own week"

---
+++ super/different_number/other_problem_or_big_point/problem/group/public_company_and_new_person/tell_other_case_beneath_big_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_child_in_part(str_arg):
+    person_and_bad_thing(str_arg)
+    print('take_world_of_young_case')
+
+def person_and_bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_child_in_part('year')
