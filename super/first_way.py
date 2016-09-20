73eba8b99503c11bfe30a3767cee3c58b4e8f8a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 13:24:31 -0400

    "mv: know super/first_way.py or super/week/public_week.py for young person"

---
+++ super/first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_case(str_arg):
+    tell_world_for_time(str_arg)
+    print('think_important_week')
+
+def tell_world_for_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_case('bad_hand')
---
+++ super/week/public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_hand(str_arg):
+    long_week(str_arg)
+    print('place_or_week')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_hand('see_life_with_government')
