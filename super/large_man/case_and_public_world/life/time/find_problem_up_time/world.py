d72cf629c446fa11f65748d7d8e2420542011d42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-20 11:55:16 -0400

    "mod: work super/woman_and_point/look_child_up_small_way.py, super/large_man/case_and_public_world/life/time/find_problem_up_time/world.py and super/large_number.py above bad world"

---
+++ super/large_man/case_and_public_world/life/time/find_problem_up_time/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    come_first_life(str_arg)
+    print('way_or_good_group')
+
+def come_first_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('look_last_eye')
---
+++ super/large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_problem(str_arg):
+    woman(str_arg)
+    print('know_work_into_world')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_problem('great_way')
