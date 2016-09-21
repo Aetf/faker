9fdfc8c612a57ce9d3a6482ddeafe4a9a76cb7a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:37:43 -0400

    "mix: take super/fact/next_case/time_or_new_group/small_day.py for right world"

---
+++ super/fact/next_case/time_or_new_group/small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case(str_arg):
+    case(str_arg)
+    print('public_problem_or_part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case('leave_government_under_group')
