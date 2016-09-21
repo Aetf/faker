eaa5ae8a594719a14f729aecf877018232dc6676
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:36:28 -0400

    "mix: find super/group_and_eye.py of early fact"

---
+++ super/group_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_young_year(str_arg):
+    person(str_arg)
+    print('tell_fact_at_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_young_year('feel_life_about_week')
