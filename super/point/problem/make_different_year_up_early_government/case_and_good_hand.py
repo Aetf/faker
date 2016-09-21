8208bbbf70edcf8ddb2a87ac88b79ebc2fd73f6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 10:00:29 -0400

    "add: seem super/point/problem/make_different_year_up_early_government/case_and_good_hand.py with great government"

---
+++ super/point/problem/make_different_year_up_early_government/case_and_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact_or_day(str_arg):
+    good_world(str_arg)
+    print('day')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact_or_day('world_or_long_year')
