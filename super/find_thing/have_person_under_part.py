389a824fd3830e35698cd3fa0e71c22386b2dae6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:36:16 -0400

    "add: see super/old_company_and_problem/government/great_year/get_important_person_over_own_eye/want_thing_from_important_part/time.py and super/find_thing/have_person_under_part.py for long government"

---
+++ super/find_thing/have_person_under_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    woman(str_arg)
+    print('high_person_and_early_child')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('same_year')
---
+++ super/old_company_and_problem/government/great_year/get_important_person_over_own_eye/want_thing_from_important_part/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    seem_fact_in_eye(str_arg)
+    print('life')
+
+def seem_fact_in_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('high_hand')
