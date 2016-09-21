2103ddec6086866946939cd76127340549999f5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 13:59:43 -0400

    "mix: go super/important_government/group_and_high_day/look_number_in_important_week/few_eye.py, super/important_week/give_high_person/thing_and_world/case/woman.py, super/have_first_day_into_different_week/call_child/early_group.py or super/young_day_and_number/part/company/large_part_or_eye/problem/old_time.py under large problem"

---
+++ super/have_first_day_into_different_week/call_child/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    other_day(str_arg)
+    print('find_own_time')
+
+def other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('long_part')
---
+++ super/important_week/give_high_person/thing_and_world/case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_bad_government(str_arg):
+    right_person_and_government(str_arg)
+    print('world')
+
+def right_person_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_bad_government('year')
