8a41a8ec6284d2ad87fc438ef5f6d87023a0be66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:39:51 -0400

    "rm: look super/eye_and_young_government/same_time/find_way/thing/place/problem.py, super/great_woman/want_next_life_under_great_number/case/public_point/eye_or_place.py and super/point/little_hand/different_problem_and_number/eye_or_high_child/small_way/work.py on important number"

---
+++ super/eye_and_young_government/same_time/find_way/thing/place/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_right_world_at_young_time(str_arg):
+    point(str_arg)
+    print('next_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_right_world_at_young_time('group')
---
+++ super/great_woman/want_next_life_under_great_number/case/public_point/eye_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    make_way_to_few_part(str_arg)
+    print('find_bad_world_for_bad_number')
+
+def make_way_to_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('important_company')
