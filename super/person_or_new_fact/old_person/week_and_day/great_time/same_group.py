992637ae559a3c23048e95ddb5ee57fbf98981f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:16:12 -0400

    "rm: know super/person_or_new_fact/old_person/week_and_day/great_time/same_group.py of old world"

---
+++ super/person_or_new_fact/old_person/week_and_day/great_time/same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man(str_arg):
+    time(str_arg)
+    print('hand_or_high_child')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man('same_week')
