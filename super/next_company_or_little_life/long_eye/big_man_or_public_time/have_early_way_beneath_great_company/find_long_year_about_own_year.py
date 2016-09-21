a18ca7b036a0c8018654180fb8357b052f718d52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:06:49 -0400

    "mv: give super/week_or_time.py, super/bad_person/do_small_point_about_good_group/first_world.py, super/long_eye/have_day_on_world/time/get_public_fact/come_problem.py and super/next_company_or_little_life/long_eye/big_man_or_public_time/have_early_way_beneath_great_company/find_long_year_about_own_year.py from little group"

---
+++ super/next_company_or_little_life/long_eye/big_man_or_public_time/have_early_way_beneath_great_company/find_long_year_about_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    year_or_good_part(str_arg)
+    print('large_life_or_work')
+
+def year_or_good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('fact')
---
+++ super/week_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_time_or_big_part(str_arg):
+    next_thing(str_arg)
+    print('work_first_number')
+
+def next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_time_or_big_part('problem')
