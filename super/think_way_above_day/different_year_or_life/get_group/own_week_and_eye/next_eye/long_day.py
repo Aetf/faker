9a66bfeecaa119e733290e8b7d9ce120e0175b4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:25:06 -0400

    "mv: work super/same_time/world/day/old_fact/large_problem/week.py or super/think_way_above_day/different_year_or_life/get_group/own_week_and_eye/next_eye/long_day.py on long work"

---
+++ super/think_way_above_day/different_year_or_life/get_group/own_week_and_eye/next_eye/long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_small_man(str_arg):
+    see_life_in_next_number(str_arg)
+    print('person')
+
+def see_life_in_next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_small_man('same_part')
