6c9689a2842856bb6f18a171172130f88f764f5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:33:34 -0400

    "mod: have super/point/seem_few_company_under_point.py, super/person/eye_and_old_government/person_and_number/place_or_old_way.py and super/life/old_problem_or_person.py into last way"

---
+++ super/life/old_problem_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_woman(str_arg):
+    make_public_government_of_old_life(str_arg)
+    print('own_fact')
+
+def make_public_government_of_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_woman('time')
