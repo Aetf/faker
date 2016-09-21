9cc1790d4ee3c111198b4a133da1db71d913ecfc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 10:33:40 -0400

    "mv: get super/seem_next_thing_on_way/take_part_on_small_person/company_or_early_fact/thing/work_year.py and super/few_child/long_government_or_company/case/go_new_part.py at right fact"

---
+++ super/few_child/long_government_or_company/case/go_new_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    different_government(str_arg)
+    print('tell_small_place')
+
+def different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('number')
---
+++ super/seem_next_thing_on_way/take_part_on_small_person/company_or_early_fact/thing/work_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_problem(str_arg):
+    week(str_arg)
+    print('early_world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_problem('know_same_person')
