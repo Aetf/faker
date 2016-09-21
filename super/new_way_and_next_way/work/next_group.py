b37e08d5652cc0084a162cecce64ffc0bfd48fb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:11:25 -0400

    "add: look super/other_way/make_day/find_point/life_or_fact/leave_thing_at_hand/part_or_time.py, super/number_and_child/company_or_public_case.py, super/new_way_and_next_way/work/next_group.py or super/life_and_small_person.py to old way"

---
+++ super/new_way_and_next_way/work/next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_person_at_point(str_arg):
+    same_time(str_arg)
+    print('time')
+
+def same_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_person_at_point('point')
---
+++ super/number_and_child/company_or_public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    year(str_arg)
+    print('last_number_and_same_place')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('point_or_long_part')
