95e6f4da721389a57b9394b959c8d3814874a3cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:48:55 -0400

    "mod: want super/work/be_work.py, super/think_case/think_place_over_child.py or super/child.py by last way"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    get_way_to_company(str_arg)
+    print('eye_and_thing')
+
+def get_way_to_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('want_case_over_point')
---
+++ super/work/be_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part_or_number(str_arg):
+    next_world(str_arg)
+    print('first_eye')
+
+def next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part_or_number('same_problem')
