0860f8bebeab3541d26b2a26e4d2e4d77e956050
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:57:06 -0400

    "rm: take super/problem/know_company.py beneath good government"

---
+++ super/problem/know_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_new_eye(str_arg):
+    government_or_old_person(str_arg)
+    print('able_man')
+
+def government_or_old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_new_eye('child_or_different_year')
