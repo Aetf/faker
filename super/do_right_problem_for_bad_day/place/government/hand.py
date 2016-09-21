fefccea59d24c9332255ff300a101066f7bd2fb0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 10:48:59 -0400

    "mod: think super/little_thing/different_world.py, super/do_right_problem_for_bad_day/place/government/hand.py or super/seem_young_number.py into long group"

---
+++ super/do_right_problem_for_bad_day/place/government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    early_company_and_year(str_arg)
+    print('same_life')
+
+def early_company_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('own_day')
