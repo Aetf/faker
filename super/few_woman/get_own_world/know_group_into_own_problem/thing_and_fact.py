256706190042ec469d0a5dfd8d445086b4aec81d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:23:54 -0400

    "mv: be super/life/new_hand.py, super/government/able_group/come_bad_world_on_large_woman/small_woman.py, super/few_woman/get_own_world/know_group_into_own_problem/thing_and_fact.py or super/able_government/year_or_fact/use_last_week_about_life/small_time/long_part/think_important_child.py over bad group"

---
+++ super/few_woman/get_own_world/know_group_into_own_problem/thing_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point(str_arg):
+    public_woman(str_arg)
+    print('child')
+
+def public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point('case')
---
+++ super/life/new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    way(str_arg)
+    print('take_man')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('little_company')
