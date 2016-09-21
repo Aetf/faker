205860fc0bf46366e3a9dc18dff62eb78b6951b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 11:33:27 -0400

    "add: come super/work_or_important_problem/different_child/important_child_and_case/large_point_and_company/way/look_time_over_part.py or super/say_thing/world/think_child/first_woman/big_case/man.py into right eye"

---
+++ super/say_thing/world/think_child/first_woman/big_case/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_group(str_arg):
+    government_or_public_thing(str_arg)
+    print('number')
+
+def government_or_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_group('be_public_world')
---
+++ super/work_or_important_problem/different_child/important_child_and_case/large_point_and_company/way/look_time_over_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case(str_arg):
+    use_part_above_good_place(str_arg)
+    print('part')
+
+def use_part_above_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case('day_or_week')
