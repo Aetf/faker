2f1984606396f9ec94c0fd0a07d2179eac843b3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-21 12:25:17 -0400

    "mod: make super/big_work_and_first_thing/leave_bad_life/public_company/make_week_from_young_work/case_or_problem/life_or_problem.py, super/high_child/other_time/next_place/large_world.py and super/do_able_part/work.py at next way"

---
+++ super/big_work_and_first_thing/leave_bad_life/public_company/make_week_from_young_work/case_or_problem/life_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_week_for_hand(str_arg):
+    other_government_or_great_woman(str_arg)
+    print('take_last_eye_at_right_woman')
+
+def other_government_or_great_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_week_for_hand('thing')
---
+++ super/do_able_part/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_woman(str_arg):
+    good_hand(str_arg)
+    print('high_life')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_woman('time')
