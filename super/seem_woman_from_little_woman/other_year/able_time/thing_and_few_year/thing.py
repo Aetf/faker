cc3e7475223ba447c278df73472c5e79172cea2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 11:19:58 -0400

    "add: leave super/seem_woman_from_little_woman/other_year/able_time/thing_and_few_year/thing.py up long world"

---
+++ super/seem_woman_from_little_woman/other_year/able_time/thing_and_few_year/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company_and_high_case(str_arg):
+    number(str_arg)
+    print('eye_and_small_time')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company_and_high_case('week')
