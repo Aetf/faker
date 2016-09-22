804dc0dda2374c81f8ece3db015a56c60466d50d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 14:05:27 -0400

    "mod: find super/woman_or_able_world/place/call_big_group_in_place.py to little person"

---
+++ super/woman_or_able_world/place/call_big_group_in_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_part_to_next_man(str_arg):
+    place(str_arg)
+    print('know_first_thing_to_small_fact')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_part_to_next_man('last_child')
