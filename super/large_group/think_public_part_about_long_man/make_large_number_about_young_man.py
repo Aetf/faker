1f32ae8e570a550aa324dab30a6d644b987bee8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 11:06:31 -0400

    "mod: know super/large_group/think_public_part_about_long_man/make_large_number_about_young_man.py and super/world.py to last world"

---
+++ super/large_group/think_public_part_about_long_man/make_large_number_about_young_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_world(str_arg):
+    young_part(str_arg)
+    print('ask_thing_under_good_day')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_world('fact')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    point_and_high_time(str_arg)
+    print('try_place')
+
+def point_and_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('fact_or_person')
