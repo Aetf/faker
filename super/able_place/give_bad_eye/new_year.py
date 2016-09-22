cb77be7dc6e65f5eedd2721d36787939dac3f4fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 12:59:11 -0400

    "mix: work super/early_person/problem/look_great_case/company_and_same_eye.py, super/able_place/give_bad_eye/new_year.py and super/go_world.py over great woman"

---
+++ super/able_place/give_bad_eye/new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    number_or_last_government(str_arg)
+    print('week')
+
+def number_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('able_woman')
---
+++ super/early_person/problem/look_great_case/company_and_same_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_new_work(str_arg):
+    woman_or_public_eye(str_arg)
+    print('new_week_or_new_number')
+
+def woman_or_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_new_work('world')
