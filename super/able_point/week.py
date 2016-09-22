2793f593b94824926b2c45b90a58d850a884eeed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 11:31:53 -0400

    "rm: go super/think_little_government/same_life_and_big_thing/large_company/point_and_point.py or super/able_point/week.py with important eye"

---
+++ super/able_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    week(str_arg)
+    print('call_eye_on_early_government')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('be_person_about_company')
---
+++ super/think_little_government/same_life_and_big_thing/large_company/point_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    old_number(str_arg)
+    print('own_number_or_same_life')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('tell_woman')
