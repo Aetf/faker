f3871d9f6e16df5fbcfb54901269d61645996c76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 12:06:30 -0400

    "rm: have super/place.py, super/find_part.py, super/day/high_child_and_young_day/use_world/way/way/long_man.py or super/find_young_week_on_company/last_number_or_woman/try_government/little_day/problem_or_large_thing/number_or_life.py of new place"

---
+++ super/find_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_next_child_at_young_thing(str_arg):
+    life(str_arg)
+    print('high_day_and_great_week')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_next_child_at_young_thing('different_place')
