f4069831afffa4e34f648e466fa197383cd8eadd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 10:53:35 -0400

    "rm: say super/time.py into little time"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_part(str_arg):
+    child(str_arg)
+    print('leave_long_day_after_long_place')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_part('do_great_fact_into_little_woman')
