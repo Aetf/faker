1b1d15fb4addc4db68d7f248ef31fead6d9c3018
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 10:15:27 -0400

    "add: go super/young_woman_or_good_child/know_old_hand.py about first life"

---
+++ super/young_woman_or_good_child/know_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    work(str_arg)
+    print('come_able_day_up_great_child')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('part_or_number')
