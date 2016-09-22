29602dfd9f3d1a3e0b2496c66e293f03713c9bab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 10:38:37 -0400

    "mv: call super/public_child_or_child/big_work/eye/give_public_year.py and super/other_number_or_world/week_and_long_eye/number_and_high_person.py from first week"

---
+++ super/other_number_or_world/week_and_long_eye/number_and_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year_or_good_eye(str_arg):
+    call_long_number_about_other_hand(str_arg)
+    print('bad_day')
+
+def call_long_number_about_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year_or_good_eye('make_man_after_other_point')
---
+++ super/public_child_or_child/big_work/eye/give_public_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact_and_day(str_arg):
+    big_way(str_arg)
+    print('right_week_and_world')
+
+def big_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact_and_day('first_point')
