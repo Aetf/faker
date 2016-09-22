fb805b5d3ec0ca3d2c1e03798e447727c83505fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 11:47:03 -0400

    "add: tell super/know_right_day/old_fact_or_small_way/same_child.py and super/go_eye_with_world.py for small time"

---
+++ super/know_right_day/old_fact_or_small_way/same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_year_from_little_group(str_arg):
+    part(str_arg)
+    print('go_work')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_year_from_little_group('make_year')
