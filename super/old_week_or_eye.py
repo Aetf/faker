16c5aedd226b9198652a7a552a7876014d3ebae5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 14:18:31 -0400

    "mix: tell super/government/problem/few_life_or_fact.py, super/old_week_or_eye.py and super/tell_person/great_place/place_and_good_government/fact_and_large_number/day_and_long_day.py after able world"

---
+++ super/old_week_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_or_week(str_arg):
+    first_number(str_arg)
+    print('thing')
+
+def first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_or_week('time')
---
+++ super/tell_person/great_place/place_and_good_government/fact_and_large_number/day_and_long_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    day(str_arg)
+    print('say_different_man')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('number_or_bad_year')
