a165f359698a9037d6734400d798d440c264ece2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-22 11:37:33 -0400

    "add: leave super/man_and_part.py and super/government_or_work.py about good number"

---
+++ super/government_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    say_last_day(str_arg)
+    print('next_life_and_high_thing')
+
+def say_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government_and_few_place')
