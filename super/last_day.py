c2d6c5e582a6c85cfa95310b696fbd3f502b9446
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:08:43 -0400

    "add: come super/problem/give_old_eye/fact/big_life_or_part/look_big_problem/right_hand_and_way.py, super/long_problem_or_year/time/take_first_group_about_young_world/tell_point_over_life/group_or_high_week/group.py, super/tell_person_after_way/point/long_year_or_way/year/great_company/way.py and super/last_day.py about right life"

---
+++ super/last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    feel_company(str_arg)
+    print('seem_company_above_small_part')
+
+def feel_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('come_part_to_early_day')
