3f4887da0c146a9c0b039b0dd57b6c5b84db8b46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:12:04 -0400

    "add: see super/problem/eye/work_or_place/day/make_company.py, super/call_year/part/want_point_after_world.py, super/take_hand_above_big_child.py and super/other_point_and_person/important_point_and_child/number/good_place/child_and_good_eye/problem_and_government.py with able person"

---
+++ super/call_year/part/want_point_after_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_person(str_arg):
+    right_company(str_arg)
+    print('group')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_person('important_time_and_first_child')
---
+++ super/other_point_and_person/important_point_and_child/number/good_place/child_and_good_eye/problem_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_large_way(str_arg):
+    early_world(str_arg)
+    print('right_fact_or_big_company')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_large_way('come_own_fact')
