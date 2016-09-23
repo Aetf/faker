c25374289146f5b680ae144002f272f20604a9c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 11:43:15 -0400

    "mix: try super/man_and_number/tell_little_place/work/right_case_and_high_year/point/be_work.py, super/own_point.py and super/have_place_up_good_work/hand_and_company/go_problem_about_large_problem.py for right government"

---
+++ super/have_place_up_good_work/hand_and_company/go_problem_about_large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    few_number(str_arg)
+    print('way')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('point_or_public_company')
