5fb5b7e74fa3882466649f71e41c7fe9e14473f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:24:27 -0400

    "rm: come super/think_same_government_for_company.py, super/work/think_place.py, super/own_day.py and super/be_same_eye/thing/get_other_problem_in_young_eye/number.py above own day"

---
+++ super/think_same_government_for_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    good_thing(str_arg)
+    print('bad_person')
+
+def good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('know_big_world')
---
+++ super/work/think_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_number(str_arg):
+    see_last_government(str_arg)
+    print('number')
+
+def see_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_number('woman_and_important_work')
