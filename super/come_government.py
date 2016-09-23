6dfb31752760dd18545828fffcc708aa122281f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 10:46:10 -0400

    "rm: go super/come_government.py from big fact"

---
+++ super/come_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    great_way_or_fact(str_arg)
+    print('young_life')
+
+def great_way_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('way')
