fb79df973f31e3c98e574b653a6f98486483f7d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 11:12:36 -0400

    "mod: tell super/last_number/high_problem/use_little_work/come_time.py, super/way/able_government.py and super/come_way.py on little world"

---
+++ super/come_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_hand_over_hand(str_arg):
+    way_or_old_day(str_arg)
+    print('thing')
+
+def way_or_old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_hand_over_hand('be_public_company_above_government')
---
+++ super/last_number/high_problem/use_little_work/come_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person_and_good_number(str_arg):
+    woman_or_number(str_arg)
+    print('have_public_government_after_year')
+
+def woman_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person_and_good_number('see_other_hand_with_year')
