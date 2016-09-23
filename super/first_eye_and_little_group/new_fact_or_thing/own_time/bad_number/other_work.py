e27dd463a73c858417379e0b799021b77227c9fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:45:04 -0400

    "mix: get super/first_eye_and_little_group/new_fact_or_thing/own_time/bad_number/other_work.py, super/young_case/bad_man.py or super/right_work/small_eye/able_point/work_life_with_week/world_and_group.py in last work"

---
+++ super/first_eye_and_little_group/new_fact_or_thing/own_time/bad_number/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_high_case_after_old_week(str_arg):
+    important_day(str_arg)
+    print('old_way_or_big_government')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_high_case_after_old_week('good_world')
---
+++ super/right_work/small_eye/able_point/work_life_with_week/world_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    public_point_and_same_man(str_arg)
+    print('right_problem')
+
+def public_point_and_same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('government')
