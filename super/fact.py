ca22d25f9d21465ef673e5e9fe036cc412476016
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 13:33:11 -0400

    "mod: work super/new_government_or_first_case/young_company/public_thing/part.py, super/fact/company/man_or_next_company/work/time_and_work.py, super/different_woman_and_large_world/public_number/man/large_person/want_company.py and super/fact.py at different life"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_new_life(str_arg):
+    eye(str_arg)
+    print('man_or_little_week')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_new_life('small_hand_or_woman')
---
+++ super/new_government_or_first_case/young_company/public_thing/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_eye_after_year(str_arg):
+    child(str_arg)
+    print('number_and_problem')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_eye_after_year('first_day_or_long_life')
