f17ad99c67970e4d32fb5d12c423f59372675673
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 12:19:33 -0400

    "add: be super/big_government/public_thing.py, super/use_public_fact/thing/early_way_or_small_number/eye/great_point.py, super/able_point_or_next_part/part/thing/few_work.py and super/first_case/bad_company_or_bad_work/seem_group/day/woman.py over early government"

---
+++ super/able_point_or_next_part/part/thing/few_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_few_day(str_arg):
+    number_and_able_number(str_arg)
+    print('work_thing_with_last_child')
+
+def number_and_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_few_day('able_year_and_same_group')
---
+++ super/first_case/bad_company_or_bad_work/seem_group/day/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    leave_part(str_arg)
+    print('number')
+
+def leave_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('big_case')
