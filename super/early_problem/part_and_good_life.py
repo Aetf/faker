e30d688681346a8e4eb48097017ff5cd504f2a68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 10:58:51 -0400

    "rm: do super/old_point/public_life/be_old_government/thing/number/different_fact.py, super/right_thing_or_child/call_young_work/high_number_or_public_place.py and super/early_problem/part_and_good_life.py of public day"

---
+++ super/early_problem/part_and_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem_and_own_year(str_arg):
+    see_old_eye(str_arg)
+    print('go_different_point_in_able_work')
+
+def see_old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem_and_own_year('life')
---
+++ super/old_point/public_life/be_old_government/thing/number/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company_and_little_time(str_arg):
+    same_day(str_arg)
+    print('come_person')
+
+def same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company_and_little_time('place')
