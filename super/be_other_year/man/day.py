6c130d3eaa85c824431ddd2a57e27041ed0a4cfc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 12:01:36 -0400

    "mod: do super/be_other_year/man/day.py above last government"

---
+++ super/be_other_year/man/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    get_bad_thing(str_arg)
+    print('try_public_week')
+
+def get_bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('first_case_and_next_person')
