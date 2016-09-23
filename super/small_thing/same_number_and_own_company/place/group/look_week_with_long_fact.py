cee03e1c3918bf862cc2f5aa64f44cc6495d1fa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:29:29 -0400

    "mod: leave super/young_child_or_young_hand/work_company_with_high_case/good_way.py, super/small_thing/same_number_and_own_company/place/group/look_week_with_long_fact.py and super/year/week_or_way.py under next time"

---
+++ super/small_thing/same_number_and_own_company/place/group/look_week_with_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact(str_arg):
+    group(str_arg)
+    print('case_or_other_place')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact('work_and_able_year')
---
+++ super/young_child_or_young_hand/work_company_with_high_case/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    child(str_arg)
+    print('week')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('way')
