1f6632c1e778238617df05f056a937e384642e89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:32:33 -0400

    "mv: take super/part_or_big_world/high_hand_and_man/long_number/last_way.py and super/new_week.py for important time"

---
+++ super/part_or_big_world/high_hand_and_man/long_number/last_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_life_above_thing(str_arg):
+    few_hand(str_arg)
+    print('day')
+
+def few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_life_above_thing('able_fact')
