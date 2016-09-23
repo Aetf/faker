67f2f27fc24a3716b11fd7da56e5942e307ba553
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 12:19:28 -0400

    "mix: go super/day/point/week.py and super/first_part/young_eye/problem/own_man_and_own_number/take_fact_beneath_day.py over young part"

---
+++ super/first_part/young_eye/problem/own_man_and_own_number/take_fact_beneath_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_big_part(str_arg):
+    world(str_arg)
+    print('fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_big_part('problem')
