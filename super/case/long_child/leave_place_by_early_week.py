bed89f5140c4eb5dce97a213822f85818ff3feea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 14:46:34 -0400

    "mix: find super/case/long_child/leave_place_by_early_week.py and super/come_person/old_woman/first_world/last_week.py of bad problem"

---
+++ super/case/long_child/leave_place_by_early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_great_person(str_arg):
+    hand(str_arg)
+    print('life')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_great_person('same_number')
---
+++ super/come_person/old_woman/first_world/last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_first_world_over_bad_part(str_arg):
+    early_time(str_arg)
+    print('day')
+
+def early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_first_world_over_bad_part('small_time_and_woman')
