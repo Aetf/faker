b6f5b1867438ea093f3688caef2a5ccee470aef1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-23 12:13:37 -0400

    "mv: do super/public_case/few_group.py, super/give_new_day/try_time.py or super/good_government/know_same_government/early_woman/little_number/go_different_company_for_old_world/first_way.py up young problem"

---
+++ super/good_government/know_same_government/early_woman/little_number/go_different_company_for_old_world/first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world(str_arg):
+    call_young_man(str_arg)
+    print('place')
+
+def call_young_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world('give_number_of_way')
---
+++ super/public_case/few_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_number(str_arg):
+    new_year_and_eye(str_arg)
+    print('try_work')
+
+def new_year_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_number('same_work_and_week')
