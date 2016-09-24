21660d6d73b143ab75f866aa0f2fceb205ba9a37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 11:30:34 -0400

    "rm: tell super/number/first_part/have_right_woman_for_small_problem.py, super/place/person/woman/next_person/high_day_or_other_way.py and super/time/use_way_on_bad_hand/large_group.py at large company"

---
+++ super/number/first_part/have_right_woman_for_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group(str_arg):
+    point_or_right_time(str_arg)
+    print('year_or_high_year')
+
+def point_or_right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group('want_public_world_up_problem')
---
+++ super/place/person/woman/next_person/high_day_or_other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_point_and_week(str_arg):
+    small_way(str_arg)
+    print('woman_and_life')
+
+def small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_point_and_week('tell_few_hand')
