ed248dceb469d2887e6fc16a0663936a87d183be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 13:32:47 -0400

    "mod: call super/big_year/big_fact/thing/early_group/point.py and super/thing.py after public thing"

---
+++ super/big_year/big_fact/thing/early_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_day_and_man(str_arg):
+    right_group(str_arg)
+    print('world')
+
+def right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_day_and_man('man')
