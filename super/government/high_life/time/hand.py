63dec09278a9671d4e0f5ed730a89b1f6e4cd127
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 14:24:21 -0400

    "mod: take super/able_life/have_part.py or super/government/high_life/time/hand.py after own government"

---
+++ super/government/high_life/time/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    week_or_day(str_arg)
+    print('company')
+
+def week_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('group')
