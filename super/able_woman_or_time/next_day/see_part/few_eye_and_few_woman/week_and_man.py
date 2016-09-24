ce24cb1049ac760c4faac7b1a706a6a208979d84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 14:15:51 -0400

    "mod: make super/give_time_above_old_number/come_government_from_important_company/company_or_old_week.py, super/point_or_group/day_and_old_time/part/be_new_group/time/go_few_week.py, super/able_woman_or_time/next_day/see_part/few_eye_and_few_woman/week_and_man.py or super/try_part.py from small group"

---
+++ super/able_woman_or_time/next_day/see_part/few_eye_and_few_woman/week_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact(str_arg):
+    think_company_at_own_part(str_arg)
+    print('early_work')
+
+def think_company_at_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact('company')
---
+++ super/try_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    feel_number(str_arg)
+    print('want_other_life_from_time')
+
+def feel_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('different_time_or_man')
