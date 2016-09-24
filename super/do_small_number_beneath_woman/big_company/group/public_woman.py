b88eb9236bef3f03adadd269edc3254b02a2d9cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 11:34:33 -0400

    "add: be super/do_small_number_beneath_woman/big_company/group/public_woman.py above first number"

---
+++ super/do_small_number_beneath_woman/big_company/group/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_place(str_arg):
+    able_eye(str_arg)
+    print('next_eye_or_hand')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_place('good_way')
