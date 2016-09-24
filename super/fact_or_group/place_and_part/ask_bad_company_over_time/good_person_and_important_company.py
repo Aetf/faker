8678018ebdd8a7b8f0ab11dfe617372a1ee9ad92
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 10:25:08 -0400

    "rm: come super/fact_or_group/place_and_part/ask_bad_company_over_time/good_person_and_important_company.py, super/way_and_time.py and super/thing/early_life/government_and_small_way/long_point/high_year_and_high_hand.py to few company"

---
+++ super/fact_or_group/place_and_part/ask_bad_company_over_time/good_person_and_important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_old_point(str_arg):
+    place_or_new_company(str_arg)
+    print('think_right_work_beneath_hand')
+
+def place_or_new_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_old_point('small_year')
---
+++ super/thing/early_life/government_and_small_way/long_point/high_year_and_high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_same_time(str_arg):
+    case_and_week(str_arg)
+    print('number')
+
+def case_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_same_time('large_place')
