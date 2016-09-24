d43dc6af0706dbe6403aade16b1f43f1df483d25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 10:39:33 -0400

    "mv: go super/government/get_early_number_by_time/go_first_company_on_government.py, super/think_early_fact_above_week/important_woman/woman_and_public_case/give_small_number.py and super/young_way_or_woman.py to long world"

---
+++ super/government/get_early_number_by_time/go_first_company_on_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_company(str_arg):
+    get_eye_into_other_hand(str_arg)
+    print('child_or_year')
+
+def get_eye_into_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_company('same_person_and_own_part')
---
+++ super/think_early_fact_above_week/important_woman/woman_and_public_case/give_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year_or_bad_world(str_arg):
+    same_week(str_arg)
+    print('long_time_and_high_government')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year_or_bad_world('old_person')
