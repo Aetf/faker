32ae06c4ce7047d59193ad665cf68614f03dbb0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 12:36:01 -0400

    "mod: make super/way_and_different_eye/long_year/use_few_fact_under_right_day/eye/year/find_able_person_into_next_number.py, super/call_next_group/different_woman/old_time.py and super/person/use_public_eye_by_problem/child/part/different_place/long_life.py into great case"

---
+++ super/call_next_group/different_woman/old_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_problem(str_arg):
+    small_group_and_last_day(str_arg)
+    print('life')
+
+def small_group_and_last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_problem('get_year_under_other_person')
