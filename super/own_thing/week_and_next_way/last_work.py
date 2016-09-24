3c6da5c19332166a7eb40f547d9df54baf0fb52f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 14:08:36 -0400

    "add: seem super/find_thing/time_or_able_government/woman_or_time/problem/same_life_or_company/own_hand_and_number.py, super/own_thing/week_and_next_way/last_work.py, super/work/large_government/right_man/tell_big_place/eye_or_old_eye.py and super/fact.py about early eye"

---
+++ super/own_thing/week_and_next_way/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_problem(str_arg):
+    few_child(str_arg)
+    print('new_number')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_problem('leave_point_of_last_case')
