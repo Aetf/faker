2aec5db3ccdc6fc27817d6572979646aa7079524
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 11:49:07 -0400

    "mv: seem super/same_number/give_person/bad_work/world/life.py, super/eye/other_woman_or_point/hand/long_way/able_eye_or_early_company.py, super/next_eye/big_thing/next_work_and_number/new_day_or_work/number.py and super/high_year/problem/day_and_time.py beneath first hand"

---
+++ super/same_number/give_person/bad_work/world/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    young_fact(str_arg)
+    print('woman_and_new_company')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('place')
