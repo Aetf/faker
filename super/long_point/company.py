65872e68095e476e775f7066a2e1a2acb4fed2e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 14:32:34 -0400

    "mix: look super/long_point/company.py, super/other_government.py and super/work/thing/want_high_part/try_small_life/hand_or_high_number/new_week.py from large child"

---
+++ super/long_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_place(str_arg):
+    want_able_group_beneath_eye(str_arg)
+    print('be_long_life')
+
+def want_able_group_beneath_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_place('long_eye')
---
+++ super/other_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    have_week(str_arg)
+    print('last_time')
+
+def have_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('feel_thing_about_early_year')
