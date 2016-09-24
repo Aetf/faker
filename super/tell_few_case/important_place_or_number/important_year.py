b17dcbce1c39eaece5ef65c38ca5a25fca6d911f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 13:12:25 -0400

    "mv: think super/different_problem/group/different_problem.py, super/tell_few_case/important_place_or_number/important_year.py or super/same_place/be_number_for_right_hand/able_case/life/work_work_into_way.py after young time"

---
+++ super/tell_few_case/important_place_or_number/important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    fact_or_important_point(str_arg)
+    print('same_part')
+
+def fact_or_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('try_few_group_under_person')
