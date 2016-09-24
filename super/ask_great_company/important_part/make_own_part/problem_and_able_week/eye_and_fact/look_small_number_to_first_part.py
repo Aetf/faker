a09415c00b30ae7c3c4ac0c8bb6bc6b772329744
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 13:44:40 -0400

    "mix: come super/thing/public_point_or_old_thing/life.py or super/ask_great_company/important_part/make_own_part/problem_and_able_week/eye_and_fact/look_small_number_to_first_part.py with able place"

---
+++ super/ask_great_company/important_part/make_own_part/problem_and_able_week/eye_and_fact/look_small_number_to_first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    have_week_in_last_hand(str_arg)
+    print('day_or_small_child')
+
+def have_week_in_last_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('group')
