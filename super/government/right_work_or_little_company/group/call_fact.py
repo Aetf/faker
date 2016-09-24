f019e7d484cab168a5446f20fff75ad4df80953b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 13:19:13 -0400

    "mod: do super/little_year/world/company_and_large_government/large_work.py and super/government/right_work_or_little_company/group/call_fact.py for great number"

---
+++ super/government/right_work_or_little_company/group/call_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    place(str_arg)
+    print('do_problem_under_hand')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('have_place_up_important_thing')
