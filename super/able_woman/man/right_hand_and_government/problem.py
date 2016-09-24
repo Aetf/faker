35e6068a076f73fc099d74ed7c10cd7d5707bf3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 10:51:29 -0400

    "mod: know super/time_and_point/small_week_or_high_thing/small_problem.py or super/able_woman/man/right_hand_and_government/problem.py under other government"

---
+++ super/able_woman/man/right_hand_and_government/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    thing_and_new_woman(str_arg)
+    print('different_part_and_government')
+
+def thing_and_new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('make_eye')
---
+++ super/time_and_point/small_week_or_high_thing/small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_group_over_small_person(str_arg):
+    hand_and_next_thing(str_arg)
+    print('good_government')
+
+def hand_and_next_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_group_over_small_person('early_person')
