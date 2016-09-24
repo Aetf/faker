ea3b4affbd8c10d5ba4df36f29e9788339bb96fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 14:36:57 -0400

    "mod: give super/thing/bad_week_or_point/get_part_at_problem/new_woman_or_different_group/group.py and super/last_point_or_world/feel_part_by_last_person/small_year_and_early_number/hand.py for different child"

---
+++ super/thing/bad_week_or_point/get_part_at_problem/new_woman_or_different_group/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_part(str_arg):
+    fact(str_arg)
+    print('new_way_or_big_place')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_part('time')
