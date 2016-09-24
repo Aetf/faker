b4d70ccef5fa3502f184e198ad8046538e8b4c16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-24 10:16:21 -0400

    "add: say super/other_company/work_long_life_at_first_woman.py, super/company/seem_company_up_day/eye.py or super/few_way_or_part/year_and_woman.py up little company"

---
+++ super/few_way_or_part/year_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_day(str_arg):
+    young_part(str_arg)
+    print('take_old_company_for_part')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_day('old_life_or_place')
---
+++ super/other_company/work_long_life_at_first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    work_or_different_man(str_arg)
+    print('life')
+
+def work_or_different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('do_great_number')
