44a6b402755e6c4870e9f91b9f2f829585c99f2e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 14:13:37 -0400

    "add: do super/other_thing/part/person/say_hand/small_government_and_week/eye.py, super/think_case_in_small_place/last_thing_and_other_way/next_group.py, super/fact.py or super/life/bad_hand.py after other point"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_big_part(str_arg):
+    public_eye(str_arg)
+    print('own_fact_or_thing')
+
+def public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_big_part('work_public_company')
---
+++ super/life/bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_place_with_group(str_arg):
+    first_group_and_important_point(str_arg)
+    print('case')
+
+def first_group_and_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_place_with_group('last_hand')
