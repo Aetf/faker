e60b68681838b4b77336266b722d942f4a2194aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 13:03:47 -0400

    "add: say super/group/time_or_right_fact/child/group.py, super/other_world_and_place/day/big_place/part.py, super/last_world_and_next_part/week_and_year/day.py or super/seem_different_place.py at public person"

---
+++ super/other_world_and_place/day/big_place/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    go_good_point_with_company(str_arg)
+    print('place')
+
+def go_good_point_with_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('high_way')
---
+++ super/seem_different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    high_hand(str_arg)
+    print('child')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('group')
