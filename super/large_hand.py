3fc819f58a50310df794606d471729f1636fafd9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 10:01:07 -0400

    "rm: use super/thing_and_point/bad_child.py or super/large_hand.py with large government"

---
+++ super/large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_company(str_arg):
+    high_fact(str_arg)
+    print('place')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_company('important_child')
