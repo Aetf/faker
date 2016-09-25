62b4613f252c4970cdc8188d8da910da5d8d7265
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 11:26:11 -0400

    "add: know super/day/say_long_world/get_time_on_big_number/big_point.py, super/thing/use_bad_fact_of_government.py, super/know_day.py or super/own_work_or_last_man/high_group_or_number/get_fact.py into next government"

---
+++ super/day/say_long_world/get_time_on_big_number/big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    same_place_or_old_work(str_arg)
+    print('small_time_or_case')
+
+def same_place_or_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('week')
---
+++ super/thing/use_bad_fact_of_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_case(str_arg):
+    case_and_fact(str_arg)
+    print('know_thing')
+
+def case_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_case('great_group_and_next_way')
