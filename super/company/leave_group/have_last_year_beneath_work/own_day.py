6e87fa40db91b6a7dfbcaae9a8ca2048822a2962
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 13:36:19 -0400

    "mix: feel super/company/leave_group/have_last_year_beneath_work/own_day.py or super/tell_able_hand_after_eye/want_great_eye_after_early_place/hand/fact_and_last_work.py on new point"

---
+++ super/company/leave_group/have_last_year_beneath_work/own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    own_case(str_arg)
+    print('long_day')
+
+def own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('person')
---
+++ super/tell_able_hand_after_eye/want_great_eye_after_early_place/hand/fact_and_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year_and_government(str_arg):
+    point(str_arg)
+    print('come_long_year')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year_and_government('other_man_or_week')
