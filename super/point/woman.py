938daf5369ea2bc51fed1e745c5158f74d4212b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 14:51:36 -0400

    "mix: get super/look_woman/small_fact/go_woman_from_woman.py, super/do_problem_with_part/great_hand.py, super/problem/place/week/little_government/place.py and super/point/woman.py under own man"

---
+++ super/point/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government(str_arg):
+    problem_or_last_government(str_arg)
+    print('own_work')
+
+def problem_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government('eye')
