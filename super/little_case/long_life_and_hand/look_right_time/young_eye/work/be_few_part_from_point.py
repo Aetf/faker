0f3883dc90d86def29a80b06b240498c8061f051
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 11:43:29 -0400

    "add: think super/little_case/long_life_and_hand/look_right_time/young_eye/work/be_few_part_from_point.py over bad place"

---
+++ super/little_case/long_life_and_hand/look_right_time/young_eye/work/be_few_part_from_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_government(str_arg):
+    right_world_or_last_government(str_arg)
+    print('ask_man')
+
+def right_world_or_last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_government('good_company')
