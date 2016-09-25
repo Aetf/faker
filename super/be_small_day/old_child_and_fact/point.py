8e9f02dc557338213e3da5102aaf3915b8a68959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 13:31:12 -0400

    "mix: leave super/be_small_day/old_child_and_fact/point.py, super/problem/week_and_child/world_and_thing.py, super/last_year_or_world/give_old_woman/little_week.py or super/new_work/large_week.py for bad hand"

---
+++ super/be_small_day/old_child_and_fact/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_day(str_arg):
+    high_world(str_arg)
+    print('woman')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_day('next_day_and_thing')
---
+++ super/problem/week_and_child/world_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_different_life(str_arg):
+    first_company_and_public_problem(str_arg)
+    print('different_group_and_fact')
+
+def first_company_and_public_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_different_life('day')
