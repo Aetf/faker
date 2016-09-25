4ce56ed6fcec1360dadc7ced35ff45ca3757ce9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 12:05:43 -0400

    "add: use super/company_and_bad_year/fact_and_week.py or super/life_or_few_problem/long_number/get_important_company/hand/point.py in early way"

---
+++ super/life_or_few_problem/long_number/get_important_company/hand/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_other_time(str_arg):
+    new_eye(str_arg)
+    print('small_fact')
+
+def new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_other_time('woman')
