50329e6e7d05c7dee5ce2ee6548166dad3089813
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 10:55:46 -0400

    "add: get super/eye/say_young_government_from_little_thing/find_able_place/know_great_week/public_week.py, super/thing/year/small_eye/eye/government_or_person.py and super/little_case/group/good_point/think_public_life_of_new_man/same_way/last_point.py in small eye"

---
+++ super/eye/say_young_government_from_little_thing/find_able_place/know_great_week/public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    year(str_arg)
+    print('come_high_number')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('use_few_problem')
