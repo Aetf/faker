372714b528b2e6ece5da6b658e0760cd65a69a56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-25 14:15:25 -0400

    "rm: seem super/young_point/life_or_day/point.py, super/thing/group/group/thing_and_next_company/thing_or_fact/woman_or_good_point.py and super/same_part_and_fact.py about public government"

---
+++ super/young_point/life_or_day/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_new_part_above_few_fact(str_arg):
+    company_or_group(str_arg)
+    print('world_or_child')
+
+def company_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_new_part_above_few_fact('world_and_other_work')
