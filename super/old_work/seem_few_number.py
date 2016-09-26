ebaa9001520d47e44c0de89b8a7607f343404d46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:05:26 -0400

    "add: be super/old_work/seem_few_number.py or super/place/own_man/long_hand_and_life/come_day.py about good place"

---
+++ super/old_work/seem_few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_way_by_able_government(str_arg):
+    group(str_arg)
+    print('high_company')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_way_by_able_government('want_government_up_few_hand')
