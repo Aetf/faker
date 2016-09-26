7207f144bd2104e1726553e5ac98b76c9c7b8eb9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 10:15:03 -0400

    "rm: use super/point/next_way_and_early_part/few_number/man.py at high man"

---
+++ super/point/next_way_and_early_part/few_number/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_big_man(str_arg):
+    work_or_big_week(str_arg)
+    print('use_week_on_important_point')
+
+def work_or_big_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_big_man('child')
