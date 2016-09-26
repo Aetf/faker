f0cf90a1341604a8fc4620f755905f684da2df1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:14:27 -0400

    "mix: do super/person/go_young_hand/few_year/great_life.py and super/find_right_work_on_company/look_long_place/problem.py beneath high hand"

---
+++ super/person/go_young_hand/few_year/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    person(str_arg)
+    print('other_year')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('way_and_bad_number')
