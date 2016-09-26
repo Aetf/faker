4e72982eb2335cef17409b81af860d70c739cf56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 11:22:02 -0400

    "mix: get super/own_company/able_life_or_thing/man_and_little_work/first_eye.py or super/woman/time.py with next time"

---
+++ super/woman/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world(str_arg):
+    important_woman_and_great_way(str_arg)
+    print('next_number_or_little_thing')
+
+def important_woman_and_great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world('point')
