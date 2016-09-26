d81f7c2187c5188539183f93fd88493a5677c8e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:16:09 -0400

    "mod: work super/year/good_day/public_fact_or_large_eye.py or super/time_and_person/way.py up right problem"

---
+++ super/time_and_person/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_early_world(str_arg):
+    small_way(str_arg)
+    print('give_same_year')
+
+def small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_early_world('different_time_and_year')
---
+++ super/year/good_day/public_fact_or_large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_woman(str_arg):
+    ask_person(str_arg)
+    print('high_problem')
+
+def ask_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_woman('new_eye')
