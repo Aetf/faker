8bf84a8e5c8c52cc1b795b919e5f614fd48390ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:56:47 -0400

    "rm: work super/government.py and super/work_same_place_on_new_company/give_week_above_high_place.py with same fact"

---
+++ super/work_same_place_on_new_company/give_week_above_high_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    large_fact(str_arg)
+    print('part')
+
+def large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('public_time')
