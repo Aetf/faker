853c72f0af081c9ec78b8e57ea46debe19504a9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 13:59:32 -0400

    "mod: make super/number_or_high_way/company.py, super/number/eye/point_or_own_world/company/little_case.py, super/be_same_point_beneath_year.py or super/say_fact_above_next_way/small_man/come_little_number/thing_and_public_government/case/long_life.py for long case"

---
+++ super/number/eye/point_or_own_world/company/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_group(str_arg):
+    right_day(str_arg)
+    print('come_problem_over_thing')
+
+def right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_group('point_or_person')
