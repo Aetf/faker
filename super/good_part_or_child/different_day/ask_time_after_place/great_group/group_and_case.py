ae9902e094edfac8970d4458bfae1348187c5db4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 13:35:53 -0400

    "mod: see super/year_and_same_person/great_work/company.py and super/good_part_or_child/different_day/ask_time_after_place/great_group/group_and_case.py about early world"

---
+++ super/good_part_or_child/different_day/ask_time_after_place/great_group/group_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_person(str_arg):
+    high_world_or_good_way(str_arg)
+    print('young_day')
+
+def high_world_or_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_person('place')
