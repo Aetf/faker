9ce7e34add78a495d272e78afb689bb62f29ed54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 13:30:28 -0400

    "mix: do super/young_thing/woman/government_and_group/high_hand/point/long_person.py or super/ask_high_point_for_small_week/right_time/call_bad_point.py above small year"

---
+++ super/ask_high_point_for_small_week/right_time/call_bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    important_way(str_arg)
+    print('big_year_and_great_company')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('want_group_into_old_problem')
