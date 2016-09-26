fcd9c1c253cd5c63f8b4849dd218096645214df8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:50:59 -0400

    "mix: be super/person_and_week/woman/year/get_early_man_by_right_problem/way.py or super/company/good_problem/life/man/able_fact_or_first_week.py into high life"

---
+++ super/person_and_week/woman/year/get_early_man_by_right_problem/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    know_great_case_about_number(str_arg)
+    print('great_life')
+
+def know_great_case_about_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('first_way')
