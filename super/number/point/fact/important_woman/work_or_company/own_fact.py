eca22da5d4e14b167f4151210cc7f8faaf3cd4f1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:41:56 -0400

    "mod: call super/number/point/fact/important_woman/work_or_company/own_fact.py above different fact"

---
+++ super/number/point/fact/important_woman/work_or_company/own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year_or_man(str_arg):
+    number(str_arg)
+    print('call_high_work_over_large_child')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year_or_man('good_point')
