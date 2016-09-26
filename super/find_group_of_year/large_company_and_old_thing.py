eb7d1aa89d14c233a9047b20ee989a28447073ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 11:35:12 -0400

    "mod: give super/look_case/have_old_point.py, super/find_group_of_year/large_company_and_old_thing.py, super/world_and_other_way/day/group/week/great_week_or_good_thing.py and super/government_and_important_part/same_person/case.py by new fact"

---
+++ super/find_group_of_year/large_company_and_old_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_new_fact_up_man(str_arg):
+    work_or_part(str_arg)
+    print('say_high_eye_over_group')
+
+def work_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_new_fact_up_man('good_company')
--- super/government_and_important_part/same_person/case.py
+++ super/government_and_important_part/same_person/case.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def man_and_part(str_arg):
+def time_and_public_hand(str_arg):
     case(str_arg)
-    print('group')
+    print('different_fact')
 
 def case(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    man_and_part('right_life')
+    time_and_public_hand('time_or_company')
