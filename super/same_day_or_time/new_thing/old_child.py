b6d8acfe48e10d5572ae0e349e2a4cc23f9d3e8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 13:54:40 -0400

    "mix: feel super/same_day_or_time/new_thing/old_child.py about right problem"

---
+++ super/same_day_or_time/new_thing/old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_place(str_arg):
+    look_hand(str_arg)
+    print('other_problem_and_great_problem')
+
+def look_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_place('week')
