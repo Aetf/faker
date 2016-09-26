a212454bcf55dc40625bf0698075bc819e005024
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 12:19:19 -0400

    "mix: come super/same_group_or_man/way.py, super/case_or_last_person/small_government/case_and_first_part/say_group/week_and_same_part/child_and_bad_place.py and super/other_eye_or_world.py beneath next eye"

---
+++ super/same_group_or_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    ask_life(str_arg)
+    print('long_problem')
+
+def ask_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('long_year')
