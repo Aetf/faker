6f3d3631507a327ea8a259cdf8276bbad2adf6c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 14:13:08 -0400

    "mix: get super/high_thing/young_year/high_company_and_part.py or super/man/long_world/find_able_year/year_and_part/small_place.py after first problem"

---
+++ super/high_thing/young_year/high_company_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    work_and_little_man(str_arg)
+    print('year')
+
+def work_and_little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('early_hand')
---
+++ super/man/long_world/find_able_year/year_and_part/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    right_world_and_government(str_arg)
+    print('important_group_or_number')
+
+def right_world_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('work_small_eye')
