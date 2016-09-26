3f5dd5027d16d2493d7d4d0b7ab30d53d4326bf5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-26 12:04:54 -0400

    "rm: ask super/week_and_way/leave_person_from_year/last_time_and_long_way/same_group.py, super/person.py, super/new_man.py or super/fact/number_and_old_year/tell_number/get_good_child_to_own_woman/see_man/place.py into same part"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman(str_arg):
+    important_eye_and_company(str_arg)
+    print('time_and_day')
+
+def important_eye_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman('long_day')
