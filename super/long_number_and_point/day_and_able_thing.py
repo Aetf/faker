49536f6f422aa7adeaab7a69ce3a1381bc91a70a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:27:25 -0400

    "mv: get super/long_number_and_point/day_and_able_thing.py or super/ask_important_world/able_work_and_thing/make_year.py above important problem"

---
+++ super/long_number_and_point/day_and_able_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_long_woman(str_arg):
+    give_public_person_from_own_point(str_arg)
+    print('own_woman_or_work')
+
+def give_public_person_from_own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_long_woman('world_and_hand')
