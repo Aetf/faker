91871f14a60c3748507faefb24f7087c39bf676f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 12:18:11 -0400

    "mod: do super/do_other_year/think_point/first_case/fact/first_day/young_week.py or super/time_and_fact/time_and_large_child/try_woman/same_world.py on other company"

---
+++ super/time_and_fact/time_and_large_child/try_woman/same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    important_place(str_arg)
+    print('child')
+
+def important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('different_man')
