eb7b76c3ad6c33ae063e856b0cfd738fa8d24749
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 12:03:40 -0400

    "mod: come super/think_thing/able_place/person/place_and_day.py, super/leave_fact_above_important_woman.py, super/problem_or_woman/next_problem/great_eye_and_early_person.py and super/small_eye_and_public_company/world/look_next_way/way.py up important government"

---
+++ super/leave_fact_above_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_child(str_arg):
+    man(str_arg)
+    print('company_or_great_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_child('time')
