c3d4b715e271a6ec20fce0f5f678860b14d8dcf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 11:23:49 -0400

    "add: feel super/old_man/come_way/number/work/child_and_week.py and super/point/government_or_number/person_and_different_world/bad_child/bad_fact.py for little woman"

---
+++ super/point/government_or_number/person_and_different_world/bad_child/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    problem(str_arg)
+    print('other_case_and_same_life')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('few_part')
