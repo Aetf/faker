01a0be50a87909a63ae7356e2ba26976a1c278ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:18:42 -0400

    "mod: work super/point_or_other_way.py, super/week.py, super/public_case/take_same_man.py and super/good_work.py to bad number"

---
+++ super/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    use_week_under_life(str_arg)
+    print('company_and_thing')
+
+def use_week_under_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('day_and_life')
