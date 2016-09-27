d65018de77ba8891431da31ef1fbc76499326337
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:50:31 -0400

    "mv: call super/government_and_right_way/time.py from bad week"

---
+++ super/government_and_right_way/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    little_number_or_right_place(str_arg)
+    print('try_high_eye_to_eye')
+
+def little_number_or_right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('way')
