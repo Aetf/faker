b548cbc8fa82af8f04e5ff3d538323e4456e4352
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 11:29:47 -0400

    "rm: work super/company_or_life/small_woman.py and super/part/do_work_by_right_case/different_work/world/ask_person/work.py on different place"

---
+++ super/company_or_life/small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    able_case_or_right_child(str_arg)
+    print('right_company')
+
+def able_case_or_right_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('old_problem')
