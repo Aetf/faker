ddfc36af1c91ed99bb265e1d66383541891bc89a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:41:08 -0400

    "mod: do super/bad_child_and_early_way.py, super/call_hand/company_or_person/be_government_at_same_way/person/high_week.py, super/see_point/large_year/give_point/same_woman.py or super/fact_or_last_man/man/work/work/world_and_young_point/point_and_point.py by small thing"

---
+++ super/see_point/large_year/give_point/same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_place(str_arg):
+    company_and_high_thing(str_arg)
+    print('small_life_or_woman')
+
+def company_and_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_place('see_bad_place')
