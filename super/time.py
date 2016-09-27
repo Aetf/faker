9b879265fded1d8bdb9df0b50b85d99aa66548c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 11:05:57 -0400

    "add: look super/time.py under little work"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    number_or_different_company(str_arg)
+    print('good_place')
+
+def number_or_different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('problem')
