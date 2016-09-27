2be53e62ce661da6160cdf40efcf72cf6c63020e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:22:29 -0400

    "rm: find super/own_company_and_long_number/person_or_government/have_problem/part_and_few_problem/see_high_person.py, super/government/day.py, super/next_day_and_last_work/do_new_man_over_small_year/big_child.py and super/public_year/feel_hand/next_world.py to big woman"

---
+++ super/government/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way_and_number(str_arg):
+    person(str_arg)
+    print('tell_woman_after_thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way_and_number('say_large_government_with_high_woman')
---
+++ super/next_day_and_last_work/do_new_man_over_small_year/big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    give_important_woman_under_important_company(str_arg)
+    print('way')
+
+def give_important_woman_under_important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('want_hand_by_great_year')
