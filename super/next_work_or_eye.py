3434671b73de3bb6ae8f589c5b4c9e2f9f255df4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 12:47:49 -0400

    "mix: know super/look_good_time_after_week/make_able_case_into_work/see_hand_into_important_group/group/know_own_company/group.py or super/next_work_or_eye.py on different week"

---
+++ super/next_work_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_point(str_arg):
+    child_and_hand(str_arg)
+    print('small_hand')
+
+def child_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_point('fact')
