51fd68db17925403f227dbeb5dd41302d8f76a89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 10:19:33 -0400

    "rm: come super/own_company_or_group/eye/tell_person.py and super/group/good_week/seem_point.py by old man"

---
+++ super/group/good_week/seem_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time(str_arg):
+    work_way(str_arg)
+    print('day')
+
+def work_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time('old_week')
