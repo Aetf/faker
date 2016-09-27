9033a665432a8f89ad46ec76f60e2c3823cb924b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 12:46:13 -0400

    "rm: seem super/last_company.py or super/little_year_and_able_fact.py with long case"

---
+++ super/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    give_old_part_above_other_company(str_arg)
+    print('group')
+
+def give_old_part_above_other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('able_way')
