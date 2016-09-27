abf0023d7c2649b71572e4452dbabe96e683bf44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 14:53:13 -0400

    "mv: leave super/early_company/know_next_person_beneath_few_case.py, super/eye/large_world_or_next_company/point_or_bad_government.py or super/person_and_day/next_number_or_old_problem.py at different world"

---
+++ super/early_company/know_next_person_beneath_few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_life(str_arg):
+    able_child_and_company(str_arg)
+    print('year_or_world')
+
+def able_child_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_life('tell_time_on_child')
---
+++ super/person_and_day/next_number_or_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_government_over_work(str_arg):
+    work(str_arg)
+    print('old_place_and_long_child')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_government_over_work('life')
