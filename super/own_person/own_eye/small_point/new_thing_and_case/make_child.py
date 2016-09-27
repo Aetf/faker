1dc33019d10564a333c6c22c0bd579569b375cbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 13:55:31 -0400

    "add: make super/own_person/own_eye/small_point/new_thing_and_case/make_child.py and super/different_problem_and_new_child.py for other man"

---
+++ super/own_person/own_eye/small_point/new_thing_and_case/make_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    leave_part(str_arg)
+    print('long_group_and_high_eye')
+
+def leave_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('week_or_old_company')
