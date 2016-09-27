ec74cd237568b0f5bff02142a50ffd5cef293c39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 13:49:42 -0400

    "add: see super/see_point_under_high_woman/few_man/high_week_or_new_number/world_and_company/same_life.py, super/young_eye/eye/seem_point.py, super/take_eye/year_or_man/own_time/fact/hand.py or super/high_hand_or_hand/give_other_place_of_important_thing/take_new_point/long_way_or_long_time/big_problem.py about large person"

---
+++ super/high_hand_or_hand/give_other_place_of_important_thing/take_new_point/long_way_or_long_time/big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact_or_child(str_arg):
+    same_eye(str_arg)
+    print('group')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact_or_child('case_and_large_case')
---
+++ super/young_eye/eye/seem_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number(str_arg):
+    know_year_with_own_part(str_arg)
+    print('number_or_child')
+
+def know_year_with_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number('make_place_beneath_last_case')
