1bfed845561d57023adccb0cb57eea859a416255
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-27 11:25:42 -0400

    "add: want super/bad_time.py, super/government.py, super/man/find_hand/long_group/group/child.py or super/see_right_problem_over_large_hand/public_part_and_great_year.py up old hand"

---
+++ super/bad_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    new_group(str_arg)
+    print('early_fact')
+
+def new_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('large_year_and_government')
