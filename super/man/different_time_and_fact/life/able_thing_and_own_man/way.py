4478dff44f7ad83b96f2e07eabae4c901996573d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 11:23:55 -0400

    "add: tell super/man/different_time_and_fact/life/able_thing_and_own_man/way.py, super/child/group_and_high_eye.py and super/right_case.py from different eye"

---
+++ super/man/different_time_and_fact/life/able_thing_and_own_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_work(str_arg):
+    few_problem(str_arg)
+    print('great_government')
+
+def few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_work('find_old_company_into_part')
