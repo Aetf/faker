59bb0e57774407d1ddc02bbe184e9b6aae464505
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 13:58:40 -0400

    "mv: want super/government_or_company/hand_or_group/life/high_day/man.py or super/hand_or_eye/case/new_fact.py to able woman"

---
+++ super/government_or_company/hand_or_group/life/high_day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_eye(str_arg):
+    world(str_arg)
+    print('want_part_from_week')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_eye('find_child_into_point')
