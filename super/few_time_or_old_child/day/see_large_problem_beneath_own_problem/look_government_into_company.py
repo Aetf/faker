53d65b7f57d8e74174c182971ad46ff6c68b6ef7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 13:57:42 -0400

    "mv: see super/few_time_or_old_child/day/see_large_problem_beneath_own_problem/look_government_into_company.py or super/problem/make_hand/place_and_fact.py after other year"

---
+++ super/few_time_or_old_child/day/see_large_problem_beneath_own_problem/look_government_into_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    small_hand_or_man(str_arg)
+    print('little_time')
+
+def small_hand_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('big_child')
---
+++ super/problem/make_hand/place_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    man(str_arg)
+    print('have_young_hand_from_point')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('bad_time_and_right_eye')
