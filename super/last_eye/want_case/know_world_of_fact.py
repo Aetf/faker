222a0d84ac4f5c49ecf24f787aa159058c6c3067
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 13:54:38 -0400

    "mv: feel super/tell_first_company/point_or_own_child/bad_eye/group/man_or_next_child.py and super/last_eye/want_case/know_world_of_fact.py by large thing"

---
+++ super/last_eye/want_case/know_world_of_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_fact(str_arg):
+    place(str_arg)
+    print('important_problem')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_fact('life')
