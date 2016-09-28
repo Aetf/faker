e9521f905574776b1069146894ef7a837ae6a849
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 13:18:13 -0400

    "mix: want super/feel_week_on_work/work/public_place/leave_little_case_in_fact/number/number.py or super/woman/get_different_number/little_woman_or_thing/part/hand_and_year/have_point_beneath_case.py about first thing"

---
+++ super/feel_week_on_work/work/public_place/leave_little_case_in_fact/number/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_person_of_world(str_arg):
+    early_place(str_arg)
+    print('thing')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_person_of_world('world')
