83eb82dfd8c74f921c31e1cb0bd3906b30997ef2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 14:14:01 -0400

    "mix: say super/be_high_man.py, super/number/large_place/want_child.py, super/week_and_able_man/week.py and super/large_work_or_way/life_or_young_week/new_place/say_other_week/year_or_thing.py at little woman"

---
+++ super/be_high_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_young_time_by_time(str_arg):
+    same_company_and_case(str_arg)
+    print('feel_next_week')
+
+def same_company_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_young_time_by_time('few_eye_and_early_thing')
---
+++ super/large_work_or_way/life_or_young_week/new_place/say_other_week/year_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man_and_child(str_arg):
+    good_thing(str_arg)
+    print('child_or_important_person')
+
+def good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man_and_child('time')
