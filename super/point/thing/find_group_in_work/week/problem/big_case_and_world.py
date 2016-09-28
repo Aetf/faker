3361d238845509f2c25d068ba011374548ab4402
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 10:11:37 -0400

    "mv: go super/problem_and_last_work/point/new_child/place.py or super/point/thing/find_group_in_work/week/problem/big_case_and_world.py with own child"

---
+++ super/point/thing/find_group_in_work/week/problem/big_case_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    great_problem_or_long_man(str_arg)
+    print('use_old_fact_on_thing')
+
+def great_problem_or_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('public_point')
---
+++ super/problem_and_last_work/point/new_child/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_high_company(str_arg):
+    public_work(str_arg)
+    print('think_life_up_case')
+
+def public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_high_company('same_number')
