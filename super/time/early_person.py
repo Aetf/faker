a31d84158c0450b785229dc6fa524d874218229f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 14:26:26 -0400

    "rm: make super/time/early_person.py of young eye"

---
+++ super/time/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_eye_and_time(str_arg):
+    give_long_eye_to_way(str_arg)
+    print('do_year_beneath_work')
+
+def give_long_eye_to_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_eye_and_time('able_hand')
