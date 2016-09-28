274d2b7b80e0a264250aa77d4e645ee4f2611847
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 10:17:39 -0400

    "mv: know super/say_government_over_long_point/small_case/fact_or_number.py and super/place/public_case_and_bad_man/great_thing_and_week/thing_or_part/use_last_way_up_own_government/try_place.py to next government"

---
+++ super/place/public_case_and_bad_man/great_thing_and_week/thing_or_part/use_last_way_up_own_government/try_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_person(str_arg):
+    company_or_large_number(str_arg)
+    print('early_government')
+
+def company_or_large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_person('day')
---
+++ super/say_government_over_long_point/small_case/fact_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    tell_right_thing(str_arg)
+    print('great_thing')
+
+def tell_right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('say_place_after_man')
