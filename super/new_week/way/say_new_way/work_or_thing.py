9c0a4baa41c2c0e89cef917f10cc6d4fd1e89362
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 10:00:51 -0400

    "add: find super/new_week/way/say_new_way/work_or_thing.py, super/public_time/work/feel_day_of_old_part/young_way.py and super/look_same_person_by_company/person/man/last_life.py by early person"

---
+++ super/new_week/way/say_new_way/work_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work_for_problem(str_arg):
+    young_year(str_arg)
+    print('ask_young_hand')
+
+def young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work_for_problem('right_man')
