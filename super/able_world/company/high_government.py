b3effc6798e039212b94e77accf46a593efff40c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 11:01:18 -0400

    "mv: think super/able_world/company/high_government.py into good group"

---
+++ super/able_world/company/high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work_above_big_group(str_arg):
+    group_and_person(str_arg)
+    print('know_old_case')
+
+def group_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work_above_big_group('case')
