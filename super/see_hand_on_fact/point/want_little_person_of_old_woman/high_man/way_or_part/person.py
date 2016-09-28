b1f3559d841c97c78781d964b56672d53c721d29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-28 12:25:29 -0400

    "mix: see super/see_hand_on_fact/point/want_little_person_of_old_woman/high_man/way_or_part/person.py or super/important_problem_or_company/week_or_problem.py by few place"

---
+++ super/see_hand_on_fact/point/want_little_person_of_old_woman/high_man/way_or_part/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_company_with_old_year(str_arg):
+    own_government(str_arg)
+    print('new_woman')
+
+def own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_company_with_old_year('find_high_person')
