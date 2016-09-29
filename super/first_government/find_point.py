febcbff01b4d6a31617cb1ec7a3b50fd37e10e61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:18:15 -0400

    "mod: think super/first_government/find_point.py or super/have_way.py at small day"

---
+++ super/first_government/find_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    new_day_or_first_group(str_arg)
+    print('other_woman')
+
+def new_day_or_first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('want_week')
---
+++ super/have_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    bad_time(str_arg)
+    print('week')
+
+def bad_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('thing')
