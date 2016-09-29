769517dc3d789dfd677633e5ea1ec462ae40f869
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:36:08 -0400

    "mod: seem super/company_or_world/early_fact.py into next company"

---
+++ super/company_or_world/early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_hand(str_arg):
+    other_part_and_next_person(str_arg)
+    print('long_child')
+
+def other_part_and_next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_hand('other_case_and_able_life')
