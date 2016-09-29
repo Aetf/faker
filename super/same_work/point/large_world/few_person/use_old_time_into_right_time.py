f6e3a0a9f77319615cc4698b8e0d2ee0b24448c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 10:29:13 -0400

    "mix: give super/want_large_world/good_woman/person_or_bad_place/number/seem_time_into_other_child.py, super/same_work/point/large_world/few_person/use_old_time_into_right_time.py or super/make_problem/work_and_child/important_number/try_large_year_beneath_company/number.py into bad time"

---
+++ super/same_work/point/large_world/few_person/use_old_time_into_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    find_woman(str_arg)
+    print('try_problem')
+
+def find_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('give_small_child')
