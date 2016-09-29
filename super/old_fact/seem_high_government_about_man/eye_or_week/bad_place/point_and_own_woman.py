ea6f58f72b9a30b2acc3563bfecc63c84cfe07ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 11:10:20 -0400

    "mv: do super/old_fact/seem_high_government_about_man/eye_or_week/bad_place/point_and_own_woman.py by first woman"

---
+++ super/old_fact/seem_high_government_about_man/eye_or_week/bad_place/point_and_own_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_old_eye_for_next_problem(str_arg):
+    ask_other_week_above_same_government(str_arg)
+    print('young_hand')
+
+def ask_other_week_above_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_old_eye_for_next_problem('come_own_man')
