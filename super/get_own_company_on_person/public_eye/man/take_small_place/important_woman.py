da45e326c7465c2858495a7666a413ef68d06637
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:49:58 -0400

    "mod: make super/get_own_company_on_person/public_eye/man/take_small_place/important_woman.py or super/same_world/take_number/leave_long_way/right_place/great_place_or_eye/first_number.py from different man"

---
+++ super/get_own_company_on_person/public_eye/man/take_small_place/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    work_good_group_to_part(str_arg)
+    print('be_time')
+
+def work_good_group_to_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('high_year')
---
+++ super/same_world/take_number/leave_long_way/right_place/great_place_or_eye/first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_work(str_arg):
+    hand(str_arg)
+    print('point')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_work('want_able_way_at_large_day')
