8f2f86ca4c045fbbe7403c5362a8b42d3229db98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:52:42 -0400

    "mv: want super/other_group/little_place.py, super/make_same_point_into_work/high_fact_and_big_way/number_and_right_life/year/number.py and super/fact/large_problem/take_old_eye.py in little eye"

---
+++ super/other_group/little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_year(str_arg):
+    world(str_arg)
+    print('man')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_year('own_problem')
