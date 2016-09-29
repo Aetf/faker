5698fe719c6343f90065fe6856e0749de3605212
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 10:40:49 -0400

    "add: make super/company/number/point/little_year/able_year.py into old number"

---
+++ super/company/number/point/little_year/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    early_part(str_arg)
+    print('few_way')
+
+def early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('old_time')
