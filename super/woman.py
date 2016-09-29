e40753e3728fa6d060129545ce7fb9944018839b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 11:06:15 -0400

    "mix: take super/woman.py by same part"

---
+++ super/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_man_from_day(str_arg):
+    try_person_under_bad_eye(str_arg)
+    print('make_work_to_bad_place')
+
+def try_person_under_bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_man_from_day('company_and_first_woman')
