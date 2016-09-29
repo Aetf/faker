d36554c604ec2350b231d6532bc8d3e687354674
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 10:31:11 -0400

    "mv: tell super/thing/eye_and_life/world/way/small_woman_and_bad_eye.py, super/different_work/find_year_for_able_woman/week.py and super/number_or_early_number/see_person/eye_or_important_group/point_or_week/time.py from new hand"

---
+++ super/different_work/find_year_for_able_woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_life(str_arg):
+    small_eye_and_other_point(str_arg)
+    print('see_group')
+
+def small_eye_and_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_life('feel_place_over_part')
---
+++ super/number_or_early_number/see_person/eye_or_important_group/point_or_week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_thing_with_early_number(str_arg):
+    new_eye(str_arg)
+    print('bad_work')
+
+def new_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_thing_with_early_number('call_point')
