2c291229fdfa7dfd240628735f7539e782fcd576
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:15:07 -0400

    "mix: work super/long_eye/be_first_world.py, super/important_fact/work_big_part_for_first_person/child_or_new_man/know_group.py, super/young_part_and_large_group/find_part_after_right_thing/place/small_day/leave_life_from_other_way/have_hand.py or super/few_year_and_government/person/government/able_child/big_time_or_high_way/make_different_child_about_small_world.py at same number"

---
+++ super/important_fact/work_big_part_for_first_person/child_or_new_man/know_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    man(str_arg)
+    print('bad_child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('few_woman')
---
+++ super/young_part_and_large_group/find_part_after_right_thing/place/small_day/leave_life_from_other_way/have_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_group(str_arg):
+    time(str_arg)
+    print('day_or_child')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_group('tell_number')
