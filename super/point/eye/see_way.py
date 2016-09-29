2c550f8ef9c8378234262e568bb109f51b5091fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:54:57 -0400

    "mod: take super/think_eye/time.py, super/point/eye/see_way.py, super/public_year/hand_and_company.py or super/think_place_over_young_time/think_company/hand.py beneath early child"

---
+++ super/point/eye/see_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_time_under_long_hand(str_arg):
+    hand(str_arg)
+    print('important_case_or_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_time_under_long_hand('important_day')
---
+++ super/think_eye/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_time(str_arg):
+    get_child(str_arg)
+    print('life_or_old_man')
+
+def get_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_time('place')
