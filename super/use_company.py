44527fad06ac8029ae6d66ad0cfe80c740bf8070
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:21:26 -0400

    "mv: know super/use_company.py into small group"

---
+++ super/use_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_hand_by_group(str_arg):
+    fact_or_thing(str_arg)
+    print('week')
+
+def fact_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_hand_by_group('world')
