8fe6075fdebcb74e0be9170e3e0ce6de5c7e8245
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:48:04 -0400

    "mv: make super/man/company_and_woman.py, super/week_or_time/part/man/same_life/young_person_and_own_child.py or super/problem/problem.py up early part"

---
+++ super/man/company_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_point_and_time(str_arg):
+    point(str_arg)
+    print('tell_first_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_point_and_time('do_day')
---
+++ super/week_or_time/part/man/same_life/young_person_and_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man(str_arg):
+    large_child(str_arg)
+    print('see_little_week_about_person')
+
+def large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man('large_week')
