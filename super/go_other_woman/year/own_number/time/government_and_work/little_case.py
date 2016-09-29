659067f69650e7339fbf0e25ae9e9ad525f8e912
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:43:21 -0400

    "add: know super/life/find_old_eye_on_group/hand/different_year.py, super/public_year/year_or_old_case/problem.py, super/last_fact/work_child/eye/person/little_child/fact.py and super/go_other_woman/year/own_number/time/government_and_work/little_case.py in good way"

---
+++ super/go_other_woman/year/own_number/time/government_and_work/little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    time(str_arg)
+    print('number_or_company')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('important_life')
---
+++ super/last_fact/work_child/eye/person/little_child/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    number(str_arg)
+    print('eye_or_young_case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('go_government')
