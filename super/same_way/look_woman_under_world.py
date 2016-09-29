920e7b60ece7b8ca31716009200ca4c49ef73a71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 11:15:10 -0400

    "mv: do super/next_case/week_and_company/good_place_and_first_place/call_point_after_problem/feel_eye_above_case/person.py, super/small_man_or_young_work/large_way/problem/important_number_and_own_government.py, super/high_case/find_time/want_man_of_same_number/leave_group.py or super/same_way/look_woman_under_world.py from long problem"

---
+++ super/same_way/look_woman_under_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_part(str_arg):
+    child_and_old_man(str_arg)
+    print('high_government_and_early_government')
+
+def child_and_old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_part('last_thing')
