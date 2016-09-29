0d1e7a466ecaac3af1db0e268f4e9290911d2e9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 10:37:57 -0400

    "rm: say super/find_woman_from_other_man/find_work.py and super/group/leave_new_child/own_company_or_place.py up old government"

---
+++ super/find_woman_from_other_man/find_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    same_week_and_first_part(str_arg)
+    print('right_thing_and_case')
+
+def same_week_and_first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('life_or_different_day')
