a4a195b25aaca2ba5a7846974ee36b757a01fe15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 12:26:16 -0400

    "mix: look super/different_way/next_fact/see_person/use_early_hand_beneath_different_way/little_group.py or super/life/good_point.py above new world"

---
+++ super/different_way/next_fact/see_person/use_early_hand_beneath_different_way/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_right_person(str_arg):
+    get_important_hand(str_arg)
+    print('small_year')
+
+def get_important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_right_person('group_or_first_man')
---
+++ super/life/good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_company(str_arg):
+    large_person_and_bad_group(str_arg)
+    print('world_and_last_eye')
+
+def large_person_and_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_company('eye')
