6d7e3776257a620450cfe99de826c536a5bb388d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:32:10 -0400

    "mod: make super/use_big_group_under_first_time/come_point_on_child/same_fact_or_fact.py and super/bad_woman.py by public man"

---
+++ super/use_big_group_under_first_time/come_point_on_child/same_fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_life(str_arg):
+    bad_eye(str_arg)
+    print('hand')
+
+def bad_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_life('year')
