59e03a722de904cbfd9197c1baf4b002461cb781
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 14:15:42 -0400

    "mod: want super/problem_or_last_person/woman_or_eye/place_and_own_thing/child/next_part/big_group.py or super/thing/be_young_life.py about same fact"

---
+++ super/problem_or_last_person/woman_or_eye/place_and_own_thing/child/next_part/big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_new_number(str_arg):
+    big_child(str_arg)
+    print('day')
+
+def big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_new_number('have_first_point_after_number')
---
+++ super/thing/be_young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_case(str_arg):
+    look_large_problem_to_part(str_arg)
+    print('public_place')
+
+def look_large_problem_to_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_case('say_large_world_from_public_eye')
