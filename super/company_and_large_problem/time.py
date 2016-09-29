6879a69f2c04ef600f4da3988331468741dc0cd9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 13:52:41 -0400

    "mod: take super/company_and_large_problem/time.py above little life"

---
+++ super/company_and_large_problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    great_thing_or_new_man(str_arg)
+    print('other_time_or_same_life')
+
+def great_thing_or_new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('different_man')
