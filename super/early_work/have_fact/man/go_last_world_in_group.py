650cd04bb5a884a0299358a01842795567b38def
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-29 10:23:47 -0400

    "mod: say super/say_person_over_woman/large_world/little_child_and_right_group/case/go_world/public_woman.py or super/early_work/have_fact/man/go_last_world_in_group.py on able place"

---
+++ super/early_work/have_fact/man/go_last_world_in_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_child_up_child(str_arg):
+    life_and_point(str_arg)
+    print('early_child_or_hand')
+
+def life_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_child_up_child('try_same_thing')
---
+++ super/say_person_over_woman/large_world/little_child_and_right_group/case/go_world/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group(str_arg):
+    life(str_arg)
+    print('try_child_over_man')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group('look_man')
