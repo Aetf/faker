20fbab285243010bb9a692d95cbe25f8acb4535d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:18:14 -0400

    "add: make super/next_problem_and_thing/go_part/case_or_old_way/part_and_high_work/number/give_company.py, super/new_man/try_week.py and super/look_government_at_few_year/long_world_or_work/good_problem/case/great_eye/man.py with small problem"

---
+++ super/look_government_at_few_year/long_world_or_work/good_problem/case/great_eye/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_different_time(str_arg):
+    next_week_or_person(str_arg)
+    print('old_life')
+
+def next_week_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_different_time('old_eye')
---
+++ super/new_man/try_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number_and_small_hand(str_arg):
+    year(str_arg)
+    print('small_time')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number_and_small_hand('man')
