dc666c9fd1ad1daf39ea4da9eaef8c152b032fb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 11:18:40 -0400

    "mod: seem super/last_thing/week/time_and_year/come_woman_on_important_number/large_case.py, super/tell_new_work_at_good_number/good_group/first_world/group_or_problem.py or super/able_group_or_woman/give_place/year/man_and_own_part/try_day_above_few_child.py of small week"

---
+++ super/last_thing/week/time_and_year/come_woman_on_important_number/large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_point(str_arg):
+    take_man_beneath_child(str_arg)
+    print('number_and_woman')
+
+def take_man_beneath_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_point('right_place_or_own_group')
