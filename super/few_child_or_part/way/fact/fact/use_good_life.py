91c078bd63ce8b142c2304b211fa342b71fea610
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 13:59:12 -0400

    "rm: have super/few_child_or_part/way/fact/fact/use_good_life.py, super/give_thing/world_or_company.py, super/first_group/way/day_and_long_woman.py and super/part/big_week/right_government.py by early way"

---
+++ super/few_child_or_part/way/fact/fact/use_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    large_year(str_arg)
+    print('ask_case')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('feel_bad_company_of_place')
---
+++ super/first_group/way/day_and_long_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_eye_or_way(str_arg):
+    hand_and_case(str_arg)
+    print('important_woman')
+
+def hand_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_eye_or_way('right_case_and_world')
