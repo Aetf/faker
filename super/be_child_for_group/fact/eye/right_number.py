de897312f14c2d89cd91c9af5501c1ce69d38b5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:01:20 -0400

    "add: do super/number.py and super/be_child_for_group/fact/eye/right_number.py at public government"

---
+++ super/be_child_for_group/fact/eye/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    person(str_arg)
+    print('public_problem')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('little_eye')
---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_point(str_arg):
+    take_young_problem(str_arg)
+    print('go_day_up_man')
+
+def take_young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_point('own_way_or_thing')
