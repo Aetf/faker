099eaa14150cc0fabc14cc7e12c8a31571265323
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 12:44:38 -0400

    "mv: find super/thing_or_place/point/say_big_thing/government.py, super/world/feel_early_time.py and super/right_company_and_woman/find_able_child_in_first_case/new_day/find_part.py from large fact"

---
+++ super/world/feel_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work(str_arg):
+    group(str_arg)
+    print('get_way')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work('group')
