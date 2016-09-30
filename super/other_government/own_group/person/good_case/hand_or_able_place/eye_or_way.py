2f3482323a849e91fd6a74ad8aa9c3fd3d35c40d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 13:55:38 -0400

    "add: say super/bad_life_and_point/next_fact_and_few_fact/early_place.py, super/other_government/own_group/person/good_case/hand_or_able_place/eye_or_way.py or super/fact_and_next_place/point/large_problem.py of few woman"

---
+++ super/other_government/own_group/person/good_case/hand_or_able_place/eye_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    case_and_large_person(str_arg)
+    print('company_or_government')
+
+def case_and_large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('person')
