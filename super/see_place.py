192ec2b2204c75cd6a72c218d33de0ed6443edfa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 13:52:57 -0400

    "mix: give super/see_place.py over small problem"

---
+++ super/see_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_part(str_arg):
+    case(str_arg)
+    print('other_world_or_government')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_part('good_government')
