744f27facef5298d1515b5339becb541dfe1b011
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 11:43:05 -0400

    "mod: get super/leave_long_woman/woman_or_good_government/week/ask_year/own_person_or_day.py, super/small_place_or_same_government/try_little_way_of_person/great_place_or_next_thing/be_right_world.py and super/new_time/public_problem.py over bad day"

---
+++ super/leave_long_woman/woman_or_good_government/week/ask_year/own_person_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_problem_over_own_day(str_arg):
+    work_old_time(str_arg)
+    print('company')
+
+def work_old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_problem_over_own_day('same_hand_and_good_work')
