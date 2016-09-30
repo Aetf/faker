5c1596db1993054edf2965193a47fc1f850f4f8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 10:53:59 -0400

    "add: come super/next_day/leave_bad_point_on_thing.py, super/other_case/number.py and super/small_time_or_important_person.py at long hand"

---
+++ super/small_time_or_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_thing(str_arg):
+    new_fact(str_arg)
+    print('first_eye')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_thing('group_and_early_work')
