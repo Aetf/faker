34a34fb06564e7d4e85a935e2d1e4d4a85c75319
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:05:54 -0400

    "add: be super/seem_bad_person_with_child/bad_day/good_work_and_problem/week_or_case/other_hand/other_hand_or_hand.py and super/man/say_important_year_above_big_woman.py about other place"

---
+++ super/seem_bad_person_with_child/bad_day/good_work_and_problem/week_or_case/other_hand/other_hand_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    woman(str_arg)
+    print('way')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('find_bad_year')
