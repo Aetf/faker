b2bc967a93255b0553c12b22a932c8e4e1e1660b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 12:56:06 -0400

    "mix: give super/important_number.py at same year"

---
+++ super/important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    time(str_arg)
+    print('young_point')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('use_bad_hand')
