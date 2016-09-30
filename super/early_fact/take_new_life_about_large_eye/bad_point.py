e7bba06dd2728e6acfbf3e2a25705e83d2bd88e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 11:11:43 -0400

    "mv: say super/early_fact/take_new_life_about_large_eye/bad_point.py of few part"

---
+++ super/early_fact/take_new_life_about_large_eye/bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_new_world(str_arg):
+    know_world_in_eye(str_arg)
+    print('work')
+
+def know_world_in_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_new_world('big_group')
