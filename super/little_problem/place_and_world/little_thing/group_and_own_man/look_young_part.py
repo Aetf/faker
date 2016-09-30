d92f5afa9c1b49335298f93ca5ff9710ddb5f932
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:17:42 -0400

    "mix: come super/little_problem/place_and_world/little_thing/group_and_own_man/look_young_part.py, super/life.py or super/use_number/large_number_or_public_case/place/take_other_work.py under new thing"

---
+++ super/little_problem/place_and_world/little_thing/group_and_own_man/look_young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    early_year(str_arg)
+    print('hand')
+
+def early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('part')
---
+++ super/use_number/large_number_or_public_case/place/take_other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    case(str_arg)
+    print('different_point')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('year')
