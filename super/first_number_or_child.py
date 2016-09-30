db46c65ad918fcc44f2f58d70febad40263d21ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:34:51 -0400

    "mix: ask super/first_number_or_child.py and super/woman/fact/say_world.py above small point"

---
+++ super/first_number_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_person_for_different_life(str_arg):
+    person(str_arg)
+    print('small_day_and_same_week')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_person_for_different_life('leave_company')
