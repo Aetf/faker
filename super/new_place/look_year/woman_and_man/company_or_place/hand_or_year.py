c159f32b1fa4ce06b3616b42994d3f0045b3a753
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 11:17:26 -0400

    "rm: say super/new_place/look_year/woman_and_man/company_or_place/hand_or_year.py, super/person_and_high_group/know_part/go_large_eye/man/own_world/company.py and super/great_place/good_day/small_point.py by few year"

---
+++ super/new_place/look_year/woman_and_man/company_or_place/hand_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    little_hand(str_arg)
+    print('person')
+
+def little_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('government')
---
+++ super/person_and_high_group/know_part/go_large_eye/man/own_world/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    take_day_at_early_week(str_arg)
+    print('ask_next_place_by_group')
+
+def take_day_at_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('give_first_thing_for_company')
