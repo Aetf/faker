fe4ccf27ab0507294aa4e4fa03e96ea3e858791a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 12:59:01 -0400

    "rm: take super/have_public_man/bad_life/ask_large_fact/old_work/go_way.py for large person"

---
+++ super/have_public_man/bad_life/ask_large_fact/old_work/go_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_public_group(str_arg):
+    make_other_point_over_problem(str_arg)
+    print('ask_point')
+
+def make_other_point_over_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_public_group('group')
