d59aadede6edb7e5ba92d6f186421f095bd809cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 10:00:13 -0400

    "mix: have super/young_company.py or super/go_young_group_from_able_day/give_part/woman_or_right_part/public_life/case/first_eye.py beneath high part"

---
+++ super/go_young_group_from_able_day/give_part/woman_or_right_part/public_life/case/first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    try_woman_from_person(str_arg)
+    print('great_fact')
+
+def try_woman_from_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('public_group')
