d8242f1f8617ee01a967e902969f82322e01adb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 14:00:00 -0400

    "add: think super/different_thing_and_work/have_year/long_week_and_different_woman/know_new_time/week.py, super/little_case/call_way/number_and_part/tell_old_life/life/little_year.py or super/same_way_or_same_hand.py about great man"

---
+++ super/same_way_or_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    year_or_thing(str_arg)
+    print('say_life_into_early_number')
+
+def year_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('day_or_big_week')
