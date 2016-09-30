970bec8c62a91c249aa8252a1c4b4fb6dd47321c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 10:00:54 -0400

    "add: say super/world_or_long_thing.py, super/important_world/year_or_few_way.py or super/place/hand.py for same group"

---
+++ super/important_world/year_or_few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    number(str_arg)
+    print('find_young_thing_under_week')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('look_way_over_old_problem')
---
+++ super/place/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_government_on_week(str_arg):
+    case_and_own_fact(str_arg)
+    print('want_place_at_person')
+
+def case_and_own_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_government_on_week('go_place')
