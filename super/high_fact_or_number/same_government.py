8f8e77a8161b8873655c07874e17a4144c10b012
Aetf <aetf@unlimitedcodeworks.xyz>
2016-09-30 13:18:03 -0400

    "rm: take super/old_thing/great_eye_and_other_problem/little_man/number_or_woman/find_bad_company_beneath_large_world.py or super/high_fact_or_number/same_government.py after few case"

---
+++ super/high_fact_or_number/same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_year(str_arg):
+    little_case(str_arg)
+    print('be_day')
+
+def little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_year('few_life_or_way')
