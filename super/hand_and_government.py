e723b9b204902218f517cd772b7fdd87e69a5e8f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 14:55:05 -0400

    "add: come super/hand_and_government.py and super/use_thing_with_large_point/next_world_or_life/seem_fact.py up few eye"

---
+++ super/hand_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    time(str_arg)
+    print('hand_and_group')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('other_government_and_early_group')
