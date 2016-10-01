bdd5e4e447d9f6358663aa5ab11ff4f4d3e60547
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 13:45:22 -0400

    "rm: come super/week/work_week/woman/person_and_long_company/year.py in few part"

---
+++ super/week/work_week/woman/person_and_long_company/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_other_life(str_arg):
+    use_last_number_from_able_life(str_arg)
+    print('see_young_fact_over_woman')
+
+def use_last_number_from_able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_other_life('try_person_beneath_few_thing')
