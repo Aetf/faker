92b91bdb5a6f87e331bea0461d24cce47692dc17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 11:29:15 -0400

    "mod: want super/public_problem/see_own_case_for_work/life_or_different_problem/group_or_bad_world/own_part/feel_work_of_eye.py, super/be_important_life_over_part/time.py, super/day_and_big_way.py or super/company_or_early_eye/few_hand.py on able number"

---
+++ super/day_and_big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    day_or_eye(str_arg)
+    print('see_year')
+
+def day_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('ask_year')
---
+++ super/public_problem/see_own_case_for_work/life_or_different_problem/group_or_bad_world/own_part/feel_work_of_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    large_fact(str_arg)
+    print('look_able_woman')
+
+def large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('look_day_after_part')
