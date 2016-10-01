74a65eb991736eeb80111a38bc557fde1fea91d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 12:10:43 -0400

    "mod: go super/ask_problem_for_eye/call_small_thing/tell_big_case/feel_same_work/make_own_woman.py, super/first_life/person/week/thing.py and super/go_point_under_case/person_or_person/way/few_part/public_week/time_and_point.py up big problem"

---
+++ super/first_life/person/week/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_bad_way(str_arg):
+    same_day(str_arg)
+    print('group')
+
+def same_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_bad_way('next_problem')
---
+++ super/go_point_under_case/person_or_person/way/few_part/public_week/time_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    group(str_arg)
+    print('thing_or_eye')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('time')
