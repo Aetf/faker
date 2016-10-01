b3700f5332021448c3c6184a6b4692b6e5d78940
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 13:46:12 -0400

    "mod: take super/old_day/able_part/able_part/go_other_way_to_life/able_year.py or super/new_woman_or_thing.py into first place"

---
+++ super/old_day/able_part/able_part/go_other_way_to_life/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_few_place(str_arg):
+    good_child(str_arg)
+    print('high_year')
+
+def good_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_few_place('get_early_thing_after_government')
