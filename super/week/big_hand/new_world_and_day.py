8153bffd64644f6a64f73d67ed2915860170b5fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:16:04 -0400

    "rm: make super/week/big_hand/new_world_and_day.py, super/government/good_time_or_good_year.py and super/person/old_hand.py at high year"

---
+++ super/week/big_hand/new_world_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_great_year(str_arg):
+    work_and_right_point(str_arg)
+    print('important_week')
+
+def work_and_right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_great_year('way')
