fa1676755468dbdfe1a9200fd2918a1b0a35ace6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:33:55 -0400

    "mix: say super/world/government_or_place/point/try_way_in_large_point/day_or_early_eye.py or super/next_life/young_year_and_woman.py over different world"

---
+++ super/next_life/young_year_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_right_woman(str_arg):
+    week_and_thing(str_arg)
+    print('right_point')
+
+def week_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_right_woman('bad_world')
