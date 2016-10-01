d520f13aaa732a38e501aeee2dd82c2aa90b76bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 11:18:24 -0400

    "mod: ask super/week_or_able_time/use_week_beneath_right_life/find_number.py, super/important_group/find_eye_to_different_person/way.py, super/world/large_time_and_eye/company_or_big_world/large_day.py or super/point/bad_week_and_able_man/work/use_few_man/part.py after last part"

---
+++ super/world/large_time_and_eye/company_or_big_world/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company_and_good_world(str_arg):
+    other_woman(str_arg)
+    print('company')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company_and_good_world('next_part_and_thing')
