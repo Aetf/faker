25f1c0b8daad9a704d3d6aab1bbcaf3d51a0fefa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 12:57:31 -0400

    "mod: get super/say_right_problem/bad_place/way.py, super/ask_new_time/important_life_and_work/leave_next_time_over_old_problem/important_work.py, super/problem/day/high_world/get_number/point.py or super/man_or_point/important_point.py of able fact"

---
+++ super/man_or_point/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    new_case(str_arg)
+    print('ask_new_place_by_way')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('call_year_on_bad_year')
---
+++ super/say_right_problem/bad_place/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    early_company_or_public_day(str_arg)
+    print('call_case_under_case')
+
+def early_company_or_public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('work')
