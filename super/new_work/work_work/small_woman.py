20597970fba290d3200265ce59921af8d4d1e095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 11:54:05 -0400

    "mv: be super/early_number/first_company_or_problem/group_or_able_child/ask_early_person_into_different_fact.py, super/new_work/work_work/small_woman.py, super/want_public_government_in_child/large_year_and_number.py or super/large_number_or_important_case.py over important man"

---
+++ super/new_work/work_work/small_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_little_day_by_little_child(str_arg):
+    own_hand(str_arg)
+    print('important_person')
+
+def own_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_little_day_by_little_child('great_company')
---
+++ super/want_public_government_in_child/large_year_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_last_man(str_arg):
+    good_time(str_arg)
+    print('world_and_world')
+
+def good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_last_man('good_child_or_same_case')
