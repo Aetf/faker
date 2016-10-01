ca38a4875403667db5ec2042e25708983c0fb7a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 14:29:21 -0400

    "mv: work super/place/know_way_over_thing/come_able_hand/other_year/same_time_or_great_eye.py, super/work/come_long_place_in_right_work/ask_first_case/work_week/get_important_way/big_thing.py and super/eye/early_person_or_world/old_number_and_big_problem/fact_or_fact.py over few time"

---
+++ super/eye/early_person_or_world/old_number_and_big_problem/fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_hand(str_arg):
+    good_way_and_child(str_arg)
+    print('small_problem')
+
+def good_way_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_hand('life')
---
+++ super/work/come_long_place_in_right_work/ask_first_case/work_week/get_important_way/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_person(str_arg):
+    bad_woman(str_arg)
+    print('public_problem_and_thing')
+
+def bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_person('get_person_about_able_eye')
