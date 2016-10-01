7dc17b75d41e1c34cd712f4b4f385a8357065fdc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 12:32:03 -0400

    "mix: leave super/great_man.py or super/large_week/old_hand/high_work_and_person.py from young fact"

---
+++ super/great_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_point(str_arg):
+    great_government(str_arg)
+    print('point')
+
+def great_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_point('big_case_or_way')
