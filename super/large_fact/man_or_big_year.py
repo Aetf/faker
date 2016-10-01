825ede91a91e7035b5a7d04f8fd230bb088cdeab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 11:23:16 -0400

    "rm: try super/think_little_year_from_government.py or super/large_fact/man_or_big_year.py above last person"

---
+++ super/large_fact/man_or_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    tell_time(str_arg)
+    print('think_world_under_woman')
+
+def tell_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('work_or_public_company')
