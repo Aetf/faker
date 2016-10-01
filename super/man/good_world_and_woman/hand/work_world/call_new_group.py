0b146d471eff0c24e0d579abc54fd65f524a8a14
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:15:59 -0400

    "mod: look super/man/good_world_and_woman/hand/work_world/call_new_group.py for same government"

---
+++ super/man/good_world_and_woman/hand/work_world/call_new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case(str_arg):
+    case(str_arg)
+    print('right_week')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case('be_own_child')
