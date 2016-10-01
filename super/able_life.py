b6d90812bd361b9ef02a264896033c14c620f220
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 11:49:03 -0400

    "mv: have super/able_life.py of large fact"

---
+++ super/able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    high_life_or_first_work(str_arg)
+    print('tell_few_world')
+
+def high_life_or_first_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('say_few_week')
