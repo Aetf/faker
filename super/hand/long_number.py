1e9359b9c216ac70780816aa90dbc3d578c0d77f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:06:46 -0400

    "mod: call super/hand/long_number.py with long world"

---
+++ super/hand/long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    see_great_number(str_arg)
+    print('go_different_work')
+
+def see_great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('work')
