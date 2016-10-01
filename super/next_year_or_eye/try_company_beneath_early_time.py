d40e95908e027189bf6f008c0f93c7dad1338026
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 12:24:48 -0400

    "add: know super/use_able_work/public_year/hand/important_person/last_hand/life.py, super/next_year_or_eye/try_company_beneath_early_time.py and super/government/different_person/day/old_way.py for public life"

---
+++ super/next_year_or_eye/try_company_beneath_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_person(str_arg):
+    use_week(str_arg)
+    print('number_and_place')
+
+def use_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_person('way')
