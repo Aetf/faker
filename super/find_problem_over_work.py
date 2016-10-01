4d5f2e48976e07a881abf97807f1c4677f8e5008
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:02:29 -0400

    "add: call super/get_case_in_person/great_day/give_little_thing_to_part/young_time/man_or_same_time.py, super/bad_time_or_part/part.py, super/tell_hand/part/new_child/last_hand.py and super/find_problem_over_work.py about important life"

---
+++ super/find_problem_over_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_few_eye(str_arg):
+    little_problem(str_arg)
+    print('small_point_and_same_part')
+
+def little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_few_eye('same_work')
---
+++ super/get_case_in_person/great_day/give_little_thing_to_part/young_time/man_or_same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem_and_fact(str_arg):
+    group(str_arg)
+    print('day')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem_and_fact('public_case')
