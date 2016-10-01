cef84dedf0e92f5d56563f46f97b4725ac38b057
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-01 10:05:29 -0400

    "mod: say super/time/high_way.py into small year"

---
+++ super/time/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    part(str_arg)
+    print('little_day_or_big_year')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('good_person')
