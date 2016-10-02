f7085a27dd69bc79394c764d2c28a5d4964d173f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 12:29:09 -0400

    "rm: say super/important_child/next_group_and_point/same_point/look_great_year.py, super/take_thing/government/call_important_company_for_person/problem_or_able_child/year_and_few_place.py or super/government/first_person_or_young_problem/problem/woman.py of first point"

---
+++ super/important_child/next_group_and_point/same_point/look_great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year(str_arg):
+    government_and_right_man(str_arg)
+    print('important_part')
+
+def government_and_right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year('hand')
---
+++ super/take_thing/government/call_important_company_for_person/problem_or_able_child/year_and_few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_fact(str_arg):
+    man(str_arg)
+    print('number_and_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_fact('child')
