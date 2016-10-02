40bd14eb0e130f10e4465eb4f90d15c159f50f89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 10:48:59 -0400

    "add: know super/big_thing/man/do_man_of_old_week/feel_week/public_hand.py, super/man_or_able_woman/man_or_part/new_case_and_case/way/have_public_world/part.py and super/find_woman_for_man/important_part_or_important_group/say_good_time_from_first_week/get_company.py beneath own man"

---
+++ super/big_thing/man/do_man_of_old_week/feel_week/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_different_year(str_arg):
+    day(str_arg)
+    print('way')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_different_year('last_fact')
---
+++ super/find_woman_for_man/important_part_or_important_group/say_good_time_from_first_week/get_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_point(str_arg):
+    think_small_work(str_arg)
+    print('other_work_or_own_world')
+
+def think_small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_point('year')
