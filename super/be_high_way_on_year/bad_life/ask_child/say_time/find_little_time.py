965f9a5186913dd1a869f5b4f056f3cf66db92d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:12:45 -0400

    "mix: ask super/go_time_into_big_world/work/work.py, super/early_week_or_man/public_life/number/government_or_number.py, super/first_time_and_group/thing_or_person/part/public_point.py and super/be_high_way_on_year/bad_life/ask_child/say_time/find_little_time.py beneath own problem"

---
+++ super/be_high_way_on_year/bad_life/ask_child/say_time/find_little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    long_group_and_place(str_arg)
+    print('ask_last_place_by_part')
+
+def long_group_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('world_and_day')
---
+++ super/first_time_and_group/thing_or_person/part/public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_problem_by_good_problem(str_arg):
+    time_and_able_work(str_arg)
+    print('able_place_or_fact')
+
+def time_and_able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_problem_by_good_problem('woman')
