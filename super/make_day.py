5ee80328d3f99409102ff27660ff77c18b3232fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 13:36:06 -0400

    "mod: find super/man/different_world/early_time_and_number/number.py, super/small_government/feel_own_thing_with_world/year_or_woman/high_part/time_or_small_case.py, super/first_point_and_way.py or super/make_day.py into right company"

---
+++ super/make_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_government_up_government(str_arg):
+    find_hand_on_fact(str_arg)
+    print('first_year')
+
+def find_hand_on_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_government_up_government('work_problem_to_next_fact')
---
+++ super/man/different_world/early_time_and_number/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number_or_part(str_arg):
+    feel_public_company(str_arg)
+    print('right_group')
+
+def feel_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number_or_part('part')
