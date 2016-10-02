f0b076020605e0ec0f13a8af375585c8c68d1194
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 14:07:43 -0400

    "rm: take super/few_group/give_work_in_same_place/little_government.py and super/life/same_government/work_able_case.py of early life"

---
+++ super/life/same_government/work_able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_young_government(str_arg):
+    first_person(str_arg)
+    print('first_way')
+
+def first_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_young_government('case')
