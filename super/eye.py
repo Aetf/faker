ad5240af26c0fb15057f7a1213d701c4a509c58b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:15:49 -0400

    "add: call super/eye.py, super/large_person/way_and_own_woman/own_part_or_hand/world/able_child/try_company.py or super/look_fact/different_man/eye/young_government/little_man/long_fact.py over small woman"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    want_early_time_with_new_number(str_arg)
+    print('leave_different_eye')
+
+def want_early_time_with_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('government')
