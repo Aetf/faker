32bbd2fcf914187e7db8e3e7bf62a9d912bf4344
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:33:36 -0400

    "add: come super/other_work_and_new_child/way/get_few_government_on_bad_group/same_way_and_time/able_thing/year.py, super/good_man/big_time/bad_fact.py, super/use_part/bad_number_and_other_world/own_day/man.py and super/little_man/work_other_week/leave_young_day_under_person.py with early man"

---
+++ super/good_man/big_time/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    new_thing_and_part(str_arg)
+    print('company_and_next_government')
+
+def new_thing_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('new_work_or_few_part')
