2aaf3ae1f9512f73e9334619c4b208a15f9cebdd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 14:49:31 -0400

    "mod: take super/part/high_part_and_same_number.py and super/man/good_problem/try_right_child/seem_year_to_big_week/way.py of able person"

---
+++ super/man/good_problem/try_right_child/seem_year_to_big_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group(str_arg):
+    small_day_and_place(str_arg)
+    print('last_part')
+
+def small_day_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group('work_or_new_hand')
