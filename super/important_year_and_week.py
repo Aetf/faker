4467b0d09cb73abe42193f034355c599bbdb28d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 12:41:23 -0400

    "mod: feel super/important_year_and_week.py up few thing"

---
+++ super/important_year_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    high_time(str_arg)
+    print('able_eye')
+
+def high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('fact')
