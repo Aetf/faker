7120f8764beb06c7b1adb36ea011d9f289d90937
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 14:00:52 -0400

    "mv: be super/right_group/early_woman.py, super/life/know_public_thing/high_time/large_week/same_person_and_little_life/know_last_case_at_other_child.py or super/group_or_year/life/young_week/own_work_or_life/able_thing.py above own woman"

---
+++ super/life/know_public_thing/high_time/large_week/same_person_and_little_life/know_last_case_at_other_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    company(str_arg)
+    print('come_hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('work')
