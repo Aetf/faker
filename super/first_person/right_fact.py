8b1950471e2133c4e987675f8e4832a59e938551
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:50:51 -0400

    "add: call super/first_person/right_fact.py and super/woman/same_point.py by important government"

---
+++ super/first_person/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group_or_hand(str_arg):
+    call_little_woman(str_arg)
+    print('long_year_or_person')
+
+def call_little_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group_or_hand('man_and_same_way')
