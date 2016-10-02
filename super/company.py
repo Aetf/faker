7c8101e24bf3505d21e9baafb0397597c89e8a89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 10:45:46 -0400

    "add: tell super/place.py or super/company.py from small group"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    person(str_arg)
+    print('next_man_and_world')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('way')
