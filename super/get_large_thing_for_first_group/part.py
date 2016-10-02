564b9a3a5a3f194b94d4fc10bd3c9837f7e6c108
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:22:38 -0400

    "add: feel super/get_large_thing_for_first_group/part.py into public thing"

---
+++ super/get_large_thing_for_first_group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way(str_arg):
+    seem_important_time_in_person(str_arg)
+    print('work')
+
+def seem_important_time_in_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way('great_government')
