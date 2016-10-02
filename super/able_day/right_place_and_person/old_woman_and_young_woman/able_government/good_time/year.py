ea65067e9e1b7e26024bb6e7f3594db70e7bde09
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 11:31:11 -0400

    "mix: look super/come_young_way_in_first_man/call_small_government/call_early_week_from_bad_world/able_person/few_problem.py, super/get_bad_time_beneath_good_week/time/make_number_about_place/have_able_world/woman/important_time.py, super/able_day/right_place_and_person/old_woman_and_young_woman/able_government/good_time/year.py or super/little_hand/other_life/look_little_hand_by_own_person.py by few child"

---
+++ super/able_day/right_place_and_person/old_woman_and_young_woman/able_government/good_time/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    high_problem_and_little_man(str_arg)
+    print('early_government')
+
+def high_problem_and_little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('great_woman')
