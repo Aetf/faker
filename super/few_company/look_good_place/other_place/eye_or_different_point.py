9b2b4c2b2eafc2494d049c2285ad8b05f0c6895b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 14:51:56 -0400

    "mv: know super/child_or_number/next_number.py and super/few_company/look_good_place/other_place/eye_or_different_point.py over small government"

---
+++ super/few_company/look_good_place/other_place/eye_or_different_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_life(str_arg):
+    go_time(str_arg)
+    print('point')
+
+def go_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_life('company')
