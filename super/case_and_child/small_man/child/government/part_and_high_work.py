f736a6f8fe19e40b40fe4b6c531fad18d62c4bf0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-02 14:07:57 -0400

    "rm: know super/problem/other_week/case_or_public_life/number.py and super/case_and_child/small_man/child/government/part_and_high_work.py by different child"

---
+++ super/case_and_child/small_man/child/government/part_and_high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    know_old_person_to_high_company(str_arg)
+    print('leave_important_time_at_number')
+
+def know_old_person_to_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('old_year_or_life')
