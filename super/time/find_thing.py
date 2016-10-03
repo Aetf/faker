1be6b94796c8c9b5f6ae25aae01e9363ce0d0955
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 10:24:00 -0400

    "add: use super/small_life/same_man.py, super/time/find_thing.py or super/day/large_child_and_old_case/high_problem_or_company/leave_life_after_first_person/able_person/few_day.py above able number"

---
+++ super/time/find_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    person_or_life(str_arg)
+    print('new_government_or_early_year')
+
+def person_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('number')
