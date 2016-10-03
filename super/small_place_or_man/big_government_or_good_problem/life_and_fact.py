a24cd836c15eeaff92bd9475e00352c53b47bf07
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 11:52:04 -0400

    "mix: feel super/long_number_and_problem/new_place/think_child_from_bad_time/seem_same_person_over_small_work.py, super/small_place_or_man/big_government_or_good_problem/life_and_fact.py and super/great_year/early_week/world_or_place/do_early_year_with_person/world/feel_man_about_able_year.py above other company"

---
+++ super/small_place_or_man/big_government_or_good_problem/life_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work_or_last_life(str_arg):
+    great_life_or_great_world(str_arg)
+    print('man')
+
+def great_life_or_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work_or_last_life('first_company_and_day')
