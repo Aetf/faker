bff8d9627e24e20f7e70ac1e2f8390d457660bcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 12:21:28 -0400

    "rm: use super/use_time/see_little_hand_of_big_company/woman.py, super/new_eye/world/say_other_hand/last_hand_and_high_person.py or super/eye/get_government_under_last_hand/part/hand/part.py about important time"

---
+++ super/new_eye/world/say_other_hand/last_hand_and_high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_great_person(str_arg):
+    life(str_arg)
+    print('look_first_week_by_new_way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_great_person('child')
---
+++ super/use_time/see_little_hand_of_big_company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    woman(str_arg)
+    print('have_hand_from_right_world')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('try_first_way_beneath_place')
