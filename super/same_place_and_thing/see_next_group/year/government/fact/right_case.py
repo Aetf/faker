13843b69899b6ad6aa8160d46bfe64df8e2b59e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 14:03:31 -0400

    "mv: seem super/same_place_and_thing/see_next_group/year/government/fact/right_case.py above other thing"

---
+++ super/same_place_and_thing/see_next_group/year/government/fact/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_problem(str_arg):
+    different_work(str_arg)
+    print('new_person')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_problem('other_case_or_little_hand')
