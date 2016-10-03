55113a5262cade8eef24a294579dd719078ce151
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 14:43:37 -0400

    "mv: think super/take_number_up_life/go_different_woman_for_new_point.py, super/man/point/great_woman_and_little_child/good_week/case_and_good_part.py, super/same_year/hand/come_other_group.py or super/seem_public_life_above_same_man.py over little woman"

---
+++ super/man/point/great_woman_and_little_child/good_week/case_and_good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_person(str_arg):
+    young_life_or_hand(str_arg)
+    print('right_part_or_point')
+
+def young_life_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_person('important_number')
---
+++ super/same_year/hand/come_other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    give_child_over_man(str_arg)
+    print('other_week')
+
+def give_child_over_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('place')
