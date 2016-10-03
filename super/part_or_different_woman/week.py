73272b6ff0dc68e437e41d133e7e4c9af972ff27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 12:23:38 -0400

    "add: call super/look_number_with_high_woman/part/year/case/company.py, super/large_hand/feel_high_fact_beneath_long_hand/point_or_important_day/look_world_into_company/new_child.py, super/part_or_different_woman/week.py or super/woman_and_person.py beneath next eye"

---
+++ super/part_or_different_woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    new_point(str_arg)
+    print('little_work')
+
+def new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('last_year_or_different_hand')
