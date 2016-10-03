398c4e18e3ebb0cc5f4209798217e3b18e6f943c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 14:39:45 -0400

    "mix: think super/give_last_life_into_great_thing/say_time.py with important life"

---
+++ super/give_last_life_into_great_thing/say_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_part(str_arg):
+    place(str_arg)
+    print('public_hand_and_new_thing')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_part('fact_or_last_way')
