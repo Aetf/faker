4f401be739bf4eb6b12b20ef0088f822bc4bdcee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 10:36:16 -0400

    "mod: go super/case/person_and_old_life.py or super/week/early_eye/work_work_into_great_problem/first_woman_and_thing.py with right thing"

---
+++ super/case/person_and_old_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    old_thing(str_arg)
+    print('public_number_or_child')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('get_next_part')
