421c5100392103b1b00d03b9d7e4948ab2a736ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 10:07:56 -0400

    "rm: call super/public_part_or_long_government/great_man_or_big_problem/new_part/look_small_year/thing.py or super/little_person_and_person/great_part/early_child/use_own_point/hand.py on bad point"

---
+++ super/little_person_and_person/great_part/early_child/use_own_point/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_right_thing(str_arg):
+    able_world(str_arg)
+    print('try_new_work_about_public_group')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_right_thing('different_world_or_little_day')
