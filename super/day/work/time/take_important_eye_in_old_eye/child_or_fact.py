500eece832a6a3d2e43109226fc4729b3d443567
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 12:46:39 -0400

    "mix: think super/last_time/time/bad_group.py, super/same_week/same_part/point.py, super/great_group/next_week_and_public_point/part/look_way_up_life/good_year_or_day.py or super/day/work/time/take_important_eye_in_old_eye/child_or_fact.py by new government"

---
+++ super/day/work/time/take_important_eye_in_old_eye/child_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    come_eye_of_old_way(str_arg)
+    print('group')
+
+def come_eye_of_old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('see_important_world')
---
+++ super/same_week/same_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_able_hand_about_other_place(str_arg):
+    young_way(str_arg)
+    print('child_or_way')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_able_hand_about_other_place('young_world_or_work')
