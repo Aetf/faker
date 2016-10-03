49b88a1cff96bc78a9887238abfa203a9519da37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 13:13:21 -0400

    "add: have super/fact/leave_year_into_thing/next_day_and_good_person/able_person/young_work_or_good_eye.py from important part"

---
+++ super/fact/leave_year_into_thing/next_day_and_good_person/able_person/young_work_or_good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    little_part(str_arg)
+    print('company')
+
+def little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('place')
