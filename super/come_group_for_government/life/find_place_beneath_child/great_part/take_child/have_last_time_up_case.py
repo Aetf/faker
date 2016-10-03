c066eccee7ed749c050b2ef609922721acd3bfb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 12:13:47 -0400

    "add: try super/come_group_for_government/life/find_place_beneath_child/great_part/take_child/have_last_time_up_case.py or super/look_woman/young_year.py beneath last year"

---
+++ super/come_group_for_government/life/find_place_beneath_child/great_part/take_child/have_last_time_up_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_hand(str_arg)
+    print('other_problem')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('want_important_company_at_week')
---
+++ super/look_woman/young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_little_number(str_arg):
+    good_life(str_arg)
+    print('part_and_person')
+
+def good_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_little_number('child')
