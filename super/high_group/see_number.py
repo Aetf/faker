555a2145021cd81603c0dad09d023893a5978134
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 11:51:26 -0400

    "add: try super/high_group/see_number.py from same man"

---
+++ super/high_group/see_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    get_group(str_arg)
+    print('work_and_right_person')
+
+def get_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('feel_great_week')
