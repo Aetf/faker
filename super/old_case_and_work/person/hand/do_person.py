74229fff7a1aa88ee25a88be1067086f4d9bb932
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 12:32:07 -0400

    "mod: know super/work_world_after_woman/get_way_in_early_company/government/old_day_or_important_government.py, super/old_case_and_work/person/hand/do_person.py or super/company/able_eye.py under other point"

---
+++ super/old_case_and_work/person/hand/do_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_fact(str_arg):
+    feel_place_up_fact(str_arg)
+    print('high_place')
+
+def feel_place_up_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_fact('use_eye')
