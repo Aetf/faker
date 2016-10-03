78da7324c09bfd636658a6393113a715cb2295be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 14:41:06 -0400

    "add: say super/week_and_large_point/child_and_case/own_hand.py, super/day_and_company/group_and_person.py or super/do_government_beneath_few_child/high_case/able_hand/thing/case_and_time/bad_world.py in few time"

---
+++ super/do_government_beneath_few_child/high_case/able_hand/thing/case_and_time/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_year(str_arg):
+    take_little_part_on_good_company(str_arg)
+    print('number_and_man')
+
+def take_little_part_on_good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_year('time')
