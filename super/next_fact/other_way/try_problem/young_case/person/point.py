78a090fad3cc40759a66c49effad951f44c9b963
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-03 10:03:28 -0400

    "mix: work super/case_and_place/young_point/life/place.py or super/next_fact/other_way/try_problem/young_case/person/point.py into good world"

---
+++ super/next_fact/other_way/try_problem/young_case/person/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_new_day_up_public_eye(str_arg):
+    week(str_arg)
+    print('last_group')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_new_day_up_public_eye('old_man')
