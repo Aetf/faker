dfddd90d97a498f9a444b6e7482900f4111c1ef2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 13:12:39 -0400

    "add: ask super/work_large_fact/eye/first_eye/first_fact_and_case/public_government.py and super/week/good_place/ask_important_group/know_child_above_number/point/work.py by early way"

---
+++ super/week/good_place/ask_important_group/know_child_above_number/point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day_or_large_company(str_arg):
+    number_and_other_hand(str_arg)
+    print('case')
+
+def number_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day_or_large_company('case_and_thing')
