33cd68dac6046bbd1aeed98f7e90f6380c2bf27c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 11:07:31 -0400

    "add: feel super/fact/right_number.py and super/go_time/different_company_and_different_woman/leave_few_point_into_work/look_old_time_up_small_life/find_way/thing_and_problem.py up last year"

---
+++ super/fact/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_few_eye(str_arg):
+    different_group_or_old_place(str_arg)
+    print('different_man_and_thing')
+
+def different_group_or_old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_few_eye('leave_point')
---
+++ super/go_time/different_company_and_different_woman/leave_few_point_into_work/look_old_time_up_small_life/find_way/thing_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    able_work(str_arg)
+    print('seem_bad_thing')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('hand')
