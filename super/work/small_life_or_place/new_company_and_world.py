45b782e846b4fc16ad1cd3a09f24e323bfee79b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 12:19:44 -0400

    "mix: look super/different_group/old_person.py or super/work/small_life_or_place/new_company_and_world.py over public day"

---
+++ super/work/small_life_or_place/new_company_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life_and_old_man(str_arg):
+    life(str_arg)
+    print('good_part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life_and_old_man('hand_and_eye')
