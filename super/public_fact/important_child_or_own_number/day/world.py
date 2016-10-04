7e88ad611cd675d45194f7f740cdc12fda52023d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 12:02:14 -0400

    "mix: make super/public_fact/important_child_or_own_number/day/world.py and super/woman/year/life.py above large fact"

---
+++ super/public_fact/important_child_or_own_number/day/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company(str_arg):
+    few_life_or_day(str_arg)
+    print('great_case')
+
+def few_life_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company('have_able_time_by_other_eye')
---
+++ super/woman/year/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_week_above_company(str_arg):
+    man(str_arg)
+    print('day_and_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_week_above_company('next_week_and_good_company')
