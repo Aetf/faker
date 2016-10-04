5169d7359d1c417c1793bbc91cf278cb3c59689f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 11:22:19 -0400

    "mod: think super/world/use_point/child_or_eye.py, super/want_little_time_from_young_place/important_government_or_little_fact/thing.py, super/man/next_week.py or super/ask_little_company/week_or_large_world/call_old_government_for_young_year/take_young_man_from_same_world.py for same part"

---
+++ super/man/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_day(str_arg):
+    week_and_group(str_arg)
+    print('work_important_company')
+
+def week_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_day('use_way_on_thing')
