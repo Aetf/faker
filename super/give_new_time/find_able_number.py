d513fcfbb94074e90e4fa78671e23059fcba94a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 13:56:15 -0400

    "mix: have super/last_thing/small_work_and_long_number/group/point/person/case_and_day.py and super/give_new_time/find_able_number.py at high time"

---
+++ super/give_new_time/find_able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    early_number(str_arg)
+    print('few_day_or_early_way')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('case')
