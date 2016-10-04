f977e5f805dd4d0c6038446da4e09e14a303c7cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 12:11:57 -0400

    "rm: go super/life/day_or_eye/long_company_or_number/little_fact/government/week.py, super/ask_eye/ask_long_place_at_man/last_day.py or super/thing/year_or_number/hand/number/work_and_different_child.py up first place"

---
+++ super/ask_eye/ask_long_place_at_man/last_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_case(str_arg)
+    print('time')
+
+def high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('work_long_number_beneath_day')
