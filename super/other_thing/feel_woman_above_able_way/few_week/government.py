4cfa8573208d1abe2273d4c97f007d72ed43fa7d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 14:19:08 -0400

    "mix: try super/week_or_last_place/early_fact/leave_important_day/find_world_into_old_fact/few_child/own_point_and_thing.py, super/come_young_company_about_able_woman/go_old_world.py, super/other_thing/feel_woman_above_able_way/few_week/government.py or super/same_problem_or_long_company/other_problem/time/man_and_first_part/new_number/early_number.py under bad number"

---
+++ super/other_thing/feel_woman_above_able_way/few_week/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_eye_to_case(str_arg):
+    person(str_arg)
+    print('person')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_eye_to_case('place_and_place')
---
+++ super/week_or_last_place/early_fact/leave_important_day/find_world_into_old_fact/few_child/own_point_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    be_day_beneath_thing(str_arg)
+    print('try_large_year')
+
+def be_day_beneath_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('year')
