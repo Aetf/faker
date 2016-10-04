10e9376e82845f2b17b6693f6f7275697afc1566
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 13:21:17 -0400

    "mv: seem super/last_week_or_year/few_thing_or_number/look_person_at_person/important_problem_and_bad_day/important_week_and_thing/young_man.py, super/big_child_and_child/company_or_important_problem/first_case_and_way/use_small_company_on_own_point.py, super/big_case.py and super/important_problem_and_woman/say_different_work/other_week.py to able way"

---
+++ super/big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child_or_own_case(str_arg):
+    small_way_or_fact(str_arg)
+    print('world')
+
+def small_way_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child_or_own_case('work_new_government_by_thing')
