23e90439b54cea9b1a30244da10f44e379c796bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-04 10:15:27 -0400

    "add: give super/little_problem/problem_and_small_life.py, super/eye/own_thing/see_week_to_different_person/thing_and_week.py and super/time/other_life_or_government.py after other person"

---
+++ super/time/other_life_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_or_eye(str_arg):
+    early_case_and_world(str_arg)
+    print('large_place')
+
+def early_case_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_or_eye('time_or_problem')
