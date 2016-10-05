423383b7ce14500eb11c9f180cebe6848a7a6306
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 11:26:33 -0400

    "mix: come super/feel_different_work/fact/think_early_week.py, super/be_thing_by_way/say_high_eye.py, super/day/right_government.py or super/woman/small_number.py up public life"

---
+++ super/day/right_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number_to_fact(str_arg):
+    person_or_group(str_arg)
+    print('leave_world')
+
+def person_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number_to_fact('hand')
