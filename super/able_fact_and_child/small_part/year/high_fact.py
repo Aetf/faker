d4dbad543de8115db4cc858b1418ee7ab4ea1bbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 11:46:44 -0400

    "mod: think super/important_group_and_little_time/different_day/find_child.py, super/be_early_group_of_way/long_point_or_high_number/tell_last_person_up_time/go_new_government_with_person.py, super/hand_and_old_place/want_able_government_of_big_eye/find_great_number.py or super/able_fact_and_child/small_part/year/high_fact.py about little person"

---
+++ super/able_fact_and_child/small_part/year/high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    see_number(str_arg)
+    print('man_and_important_way')
+
+def see_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('call_group')
