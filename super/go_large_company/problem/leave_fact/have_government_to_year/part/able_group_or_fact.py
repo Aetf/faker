00e43f9552e58f794b11af57eb1ee139cf667b5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 12:31:29 -0400

    "mv: give super/way/early_company_or_little_point/take_government_to_number/point_and_other_life/use_young_time_by_few_man/last_world.py, super/go_large_company/problem/leave_fact/have_government_to_year/part/able_group_or_fact.py or super/way/old_world/bad_eye.py beneath right point"

---
+++ super/go_large_company/problem/leave_fact/have_government_to_year/part/able_group_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_government_over_young_work(str_arg):
+    last_group_or_few_part(str_arg)
+    print('child_or_small_eye')
+
+def last_group_or_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_government_over_young_work('do_long_child_in_early_woman')
---
+++ super/way/early_company_or_little_point/take_government_to_number/point_and_other_life/use_young_time_by_few_man/last_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    go_week(str_arg)
+    print('part_or_group')
+
+def go_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('old_company')
