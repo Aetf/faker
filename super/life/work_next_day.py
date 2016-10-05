b3baeaeb50c8be10941beefea39dcd8e6178e268
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 13:03:19 -0400

    "mod: work super/own_part/seem_small_child_to_problem/work_and_year/able_person/year/next_world.py, super/take_year/world/see_early_day.py, super/man/hand/way/government_or_time.py and super/life/work_next_day.py at public problem"

---
+++ super/life/work_next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way(str_arg):
+    case(str_arg)
+    print('leave_high_problem')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way('great_number')
---
+++ super/own_part/seem_small_child_to_problem/work_and_year/able_person/year/next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman_and_person(str_arg):
+    give_large_world_for_young_year(str_arg)
+    print('point_and_good_number')
+
+def give_large_world_for_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman_and_person('feel_problem')
