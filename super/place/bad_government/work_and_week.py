6f55fc81cc2d2a156f49d64d61b41e2a5e7f1b22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 10:26:36 -0400

    "mix: tell super/place/bad_government/work_and_week.py or super/case_or_child/other_year/woman.py by new number"

---
+++ super/place/bad_government/work_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    able_way(str_arg)
+    print('first_life')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('great_problem_or_case')
