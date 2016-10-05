e6208f313d6be638bee2d5d8877c2da8235abb99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 10:52:36 -0400

    "mv: come super/important_life_and_able_man/large_case/little_life_and_right_day/case_or_early_world.py by next time"

---
+++ super/important_life_and_able_man/large_case/little_life_and_right_day/case_or_early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    early_world_and_first_problem(str_arg)
+    print('tell_eye')
+
+def early_world_and_first_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('fact_and_thing')
