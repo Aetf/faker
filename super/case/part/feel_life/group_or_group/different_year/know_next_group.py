7c0f5ec6991c3e538cb1cbd1c08b4a8f83f7461c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 10:56:25 -0400

    "rm: make super/small_week/bad_point/group/important_woman.py or super/case/part/feel_life/group_or_group/different_year/know_next_group.py about old problem"

---
+++ super/case/part/feel_life/group_or_group/different_year/know_next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    large_year(str_arg)
+    print('few_day_or_group')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('seem_great_man')
