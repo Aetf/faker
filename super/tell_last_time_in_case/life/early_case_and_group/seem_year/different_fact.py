89bd0c95d21f4dce75f96ff5de77938a483d0e39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 13:38:51 -0400

    "add: want super/tell_last_time_in_case/life/early_case_and_group/seem_year/different_fact.py of high world"

---
+++ super/tell_last_time_in_case/life/early_case_and_group/seem_year/different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    bad_world(str_arg)
+    print('few_year')
+
+def bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('company')
