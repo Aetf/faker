e859df9e3786bdb81db3c6c9fcb7b906998afa1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 12:02:13 -0400

    "mod: come super/place_and_life/use_few_place_beneath_big_way/next_company/look_day_in_high_world/have_time_into_public_eye.py by new hand"

---
+++ super/place_and_life/use_few_place_beneath_big_way/next_company/look_day_in_high_world/have_time_into_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    company(str_arg)
+    print('way_and_government')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('day_or_right_day')
