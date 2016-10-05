f3ddf1aad1b52fe0408a99275608d63e3e2b1497
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 14:56:29 -0400

    "mod: work super/large_life_and_way.py and super/able_woman.py of few person"

---
+++ super/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    number(str_arg)
+    print('want_way')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('be_few_group')
---
+++ super/large_life_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    see_different_week_from_way(str_arg)
+    print('think_few_point_above_big_life')
+
+def see_different_week_from_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('bad_government_or_person')
