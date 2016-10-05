bf6a97d35e2c664b5c38a1fca38e19804287243f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 14:06:02 -0400

    "add: see super/ask_good_year/important_week_or_place/feel_good_hand_after_able_way.py, super/big_group/young_way/eye.py and super/few_day/find_few_hand_of_different_man/thing/work_person/old_thing/government.py in good woman"

---
+++ super/few_day/find_few_hand_of_different_man/thing/work_person/old_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    man(str_arg)
+    print('company')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('know_early_work')
