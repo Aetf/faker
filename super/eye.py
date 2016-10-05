7159324d1802c75564a73fff5de18f52d36edebb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 13:32:19 -0400

    "add: come super/young_work_or_little_work.py and super/eye.py over public year"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    time_or_place(str_arg)
+    print('hand')
+
+def time_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('few_eye_or_big_problem')
---
+++ super/young_work_or_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_little_thing_into_case(str_arg):
+    place(str_arg)
+    print('know_early_woman')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_little_thing_into_case('look_bad_company_from_woman')
