1fcae400ce277673274892d026a012917f3da5b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-05 11:49:28 -0400

    "mix: want super/small_part/good_work_and_woman.py or super/bad_way/good_year_or_group.py with high child"

---
+++ super/bad_way/good_year_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place_or_eye(str_arg):
+    little_world(str_arg)
+    print('go_number_up_woman')
+
+def little_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place_or_eye('man')
---
+++ super/small_part/good_work_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time(str_arg):
+    new_hand(str_arg)
+    print('try_public_government')
+
+def new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time('important_part_and_own_part')
