a93e039d5f5b756b31197bb3eb13fdfba3337449
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 11:20:54 -0400

    "add: try super/other_world/thing/ask_problem_beneath_other_case.py or super/long_fact_or_large_thing.py into large world"

---
+++ super/long_fact_or_large_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye_or_early_place(str_arg):
+    case(str_arg)
+    print('woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye_or_early_place('case_and_first_eye')
