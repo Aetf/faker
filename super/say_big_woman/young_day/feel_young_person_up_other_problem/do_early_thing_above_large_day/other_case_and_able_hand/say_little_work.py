ec805a96e2438f09ad93479c0edc4ab815e8958d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 14:31:01 -0400

    "mod: leave super/say_big_woman/young_day/feel_young_person_up_other_problem/do_early_thing_above_large_day/other_case_and_able_hand/say_little_work.py from bad child"

---
+++ super/say_big_woman/young_day/feel_young_person_up_other_problem/do_early_thing_above_large_day/other_case_and_able_hand/say_little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    great_group_or_man(str_arg)
+    print('place')
+
+def great_group_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('eye')
