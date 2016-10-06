ac22b7e522b60240b0262e0b285e50e151b237f4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 10:30:43 -0400

    "mod: go super/see_week/think_way_about_child/woman/woman/child_and_big_problem/eye_and_early_woman.py, super/have_case/first_time/big_group/hand.py, super/big_child/high_day/leave_big_eye/able_woman_and_person/point_and_fact/eye.py and super/early_fact_and_old_week.py beneath early life"

---
+++ super/have_case/first_time/big_group/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world_and_small_fact(str_arg):
+    call_same_group(str_arg)
+    print('old_number_and_way')
+
+def call_same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world_and_small_fact('make_own_thing')
