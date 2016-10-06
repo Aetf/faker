761b7ebe7112e3627336f253813fe2384fbee111
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 12:39:37 -0400

    "add: feel super/few_person.py or super/eye.py on little government"

---
+++ super/few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    find_way_into_bad_work(str_arg)
+    print('company_and_fact')
+
+def find_way_into_bad_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('able_day_or_same_fact')
