ccc7404a5771b72e384526b02e2ce275919f28f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 12:44:04 -0400

    "mod: try super/good_group/work_government_for_fact/week_and_other_government/bad_woman.py and super/time/young_day/small_hand_or_own_fact.py above great work"

---
+++ super/good_group/work_government_for_fact/week_and_other_government/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing(str_arg):
+    child(str_arg)
+    print('bad_woman')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing('tell_bad_point')
---
+++ super/time/young_day/small_hand_or_own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point(str_arg):
+    man(str_arg)
+    print('small_work')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point('good_life')
