5797ce03a81c55624ecd1df5b52801ac3ae5eff8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 13:31:08 -0400

    "mix: have super/way/group.py and super/public_group/small_person.py into few person"

---
+++ super/public_group/small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_person_on_bad_day(str_arg):
+    hand_or_new_part(str_arg)
+    print('good_day')
+
+def hand_or_new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_person_on_bad_day('new_company')
---
+++ super/way/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_person(str_arg):
+    high_child(str_arg)
+    print('day')
+
+def high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_person('last_government')
