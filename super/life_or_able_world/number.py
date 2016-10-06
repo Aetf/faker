80141fe39e5f0efb8eb3f9d003508e3f9a7a3678
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 14:21:26 -0400

    "mod: give super/think_group_about_group/next_company/work_way_with_last_woman.py and super/life_or_able_world/number.py to old life"

---
+++ super/life_or_able_world/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_group(str_arg):
+    number(str_arg)
+    print('way_or_big_world')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_group('able_day')
---
+++ super/think_group_about_group/next_company/work_way_with_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_time(str_arg):
+    know_right_day_in_small_part(str_arg)
+    print('company_or_way')
+
+def know_right_day_in_small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_time('week_or_year')
