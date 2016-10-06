07186e3eb33310a344fe73a020790effd6f9ec10
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 14:23:41 -0400

    "rm: get super/hand/man/last_time_and_last_point.py, super/company_or_company.py or super/young_case/point/time_and_work/want_young_government_to_case.py on old woman"

---
+++ super/company_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    bad_work_and_right_government(str_arg)
+    print('man')
+
+def bad_work_and_right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('give_first_number')
---
+++ super/hand/man/last_time_and_last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    look_world_by_case(str_arg)
+    print('do_different_year_over_man')
+
+def look_world_by_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('able_week_and_public_number')
