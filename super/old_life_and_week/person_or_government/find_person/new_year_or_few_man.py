640cb5b3b0cb3e9d726fc25f6884cd775ffc7e23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 13:01:08 -0400

    "mod: go super/old_life_and_week/person_or_government/find_person/new_year_or_few_man.py, super/important_time/little_world/great_life/group/large_person_or_first_way.py, super/man_or_life/little_woman/woman_and_company/other_week.py or super/year.py over old hand"

---
+++ super/old_life_and_week/person_or_government/find_person/new_year_or_few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    own_child_or_man(str_arg)
+    print('important_place_or_year')
+
+def own_child_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('few_place')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    be_few_part(str_arg)
+    print('other_time')
+
+def be_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('be_own_child_about_new_place')
