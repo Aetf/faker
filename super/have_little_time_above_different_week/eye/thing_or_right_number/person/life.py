14c0d633a02ad77c26e1ca391d431b96ad52ec07
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 12:30:07 -0400

    "add: try super/have_little_time_above_different_week/eye/thing_or_right_number/person/life.py and super/man/first_man/new_world.py about own group"

---
+++ super/have_little_time_above_different_week/eye/thing_or_right_number/person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_fact(str_arg):
+    big_government(str_arg)
+    print('first_man')
+
+def big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_fact('bad_part')
---
+++ super/man/first_man/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_week_over_day(str_arg):
+    eye(str_arg)
+    print('problem_or_thing')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_week_over_day('find_big_child')
