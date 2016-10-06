10908953085a5105e05e6bfa58e8ae9709f39551
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 14:13:30 -0400

    "mv: try super/tell_early_eye_for_government/world/work_child/number_or_work/thing.py, super/early_person.py, super/fact.py or super/high_point_or_big_fact/think_company/few_group_or_person.py beneath public eye"

---
+++ super/early_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_other_time(str_arg):
+    life_or_other_life(str_arg)
+    print('child')
+
+def life_or_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_other_time('few_work')
---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    case_and_own_life(str_arg)
+    print('think_part')
+
+def case_and_own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('tell_point_over_life')
