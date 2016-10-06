41ef9ebda485d2811d3594e093d4e5042df36ba7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 12:17:16 -0400

    "add: say super/life/group_or_public_thing/give_bad_man/go_early_number.py, super/own_fact_or_old_life/person_or_thing.py, super/company.py or super/man/company_and_good_place.py to young company"

---
+++ super/life/group_or_public_thing/give_bad_man/go_early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_world_about_young_thing(str_arg):
+    young_company(str_arg)
+    print('day')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_world_about_young_thing('leave_great_problem_above_large_place')
---
+++ super/own_fact_or_old_life/person_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_company_with_work(str_arg):
+    next_life_or_right_person(str_arg)
+    print('first_person_and_big_part')
+
+def next_life_or_right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_company_with_work('other_time_and_important_week')
