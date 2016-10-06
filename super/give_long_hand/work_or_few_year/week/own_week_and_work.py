73331bef9d6612025a6ee2e4e71129c643d12cb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 13:35:49 -0400

    "rm: ask super/way/work/year/bad_person_or_next_problem/other_government/able_man_or_good_life.py, super/give_long_hand/work_or_few_year/week/own_week_and_work.py and super/say_new_case.py with long thing"

---
+++ super/give_long_hand/work_or_few_year/week/own_week_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_high_man(str_arg):
+    good_government(str_arg)
+    print('hand_or_large_eye')
+
+def good_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_high_man('way')
---
+++ super/way/work/year/bad_person_or_next_problem/other_government/able_man_or_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    think_work_of_day(str_arg)
+    print('come_public_hand')
+
+def think_work_of_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('good_day_and_own_number')
