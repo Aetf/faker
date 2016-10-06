c5b44de200fec9e2755115579f43392f49925545
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 13:50:16 -0400

    "add: take super/find_able_way/own_life/government/day/early_world.py, super/company_and_early_man/public_case/able_world/way_and_eye/time.py, super/year/group/public_life.py or super/small_thing/place/day/day.py with small week"

---
+++ super/find_able_way/own_life/government/day/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_able_person(str_arg):
+    different_part(str_arg)
+    print('thing')
+
+def different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_able_person('good_man_and_long_number')
---
+++ super/year/group/public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    hand_and_bad_woman(str_arg)
+    print('come_child_about_government')
+
+def hand_and_bad_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('use_week_over_woman')
