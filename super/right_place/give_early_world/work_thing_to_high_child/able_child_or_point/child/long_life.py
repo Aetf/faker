66e199e9f09901f220ba49126f4f77374e6d1342
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 12:10:18 -0400

    "mv: want super/point_and_fact/company_and_young_year/government/young_work/person_and_little_eye.py or super/right_place/give_early_world/work_thing_to_high_child/able_child_or_point/child/long_life.py for first problem"

---
+++ super/right_place/give_early_world/work_thing_to_high_child/able_child_or_point/child/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_other_point_for_part(str_arg):
+    child(str_arg)
+    print('way')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_other_point_for_part('little_day')
