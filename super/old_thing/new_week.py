0ed0628d5e0ea5c568d743d7ab2abe86ddd39bec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-06 13:36:58 -0400

    "mv: leave super/time_or_number/group_or_right_number/day_or_problem.py, super/group/look_person_with_last_part/work/find_important_company/right_government/large_life.py, super/bad_child.py and super/old_thing/new_week.py on last time"

---
+++ super/old_thing/new_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_government(str_arg):
+    great_point(str_arg)
+    print('government_and_young_year')
+
+def great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_government('case_and_point')
---
+++ super/time_or_number/group_or_right_number/day_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_day_into_fact(str_arg):
+    important_day(str_arg)
+    print('problem_and_big_number')
+
+def important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_day_into_fact('child_and_work')
