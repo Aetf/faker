e079f985607241c0d392c05da7cc51ceb5eba6eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:47:36 -0400

    "mix: call super/small_point/bad_government/come_high_way_beneath_eye.py, super/think_child/number/year_and_large_problem/group_or_company/long_child/seem_company_in_hand.py or super/early_part_and_last_part/say_way_to_same_week.py by able week"

---
+++ super/think_child/number/year_and_large_problem/group_or_company/long_child/seem_company_in_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_early_hand_into_important_part(str_arg):
+    eye(str_arg)
+    print('look_life_with_man')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_early_hand_into_important_part('new_hand_or_point')
