23a990194acc66aa611972877961f360a1c9db30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:22:59 -0400

    "rm: try super/time/eye/point_and_eye.py, super/few_problem/right_government/work_and_part.py, super/group/week_and_man/other_way/want_first_woman.py and super/fact/different_number_or_long_world.py from other person"

---
+++ super/time/eye/point_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    hand(str_arg)
+    print('next_company')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('know_hand_over_man')
