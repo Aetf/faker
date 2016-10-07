12b68a614c2ae0fbbfb1b5de8aa19f463fffa145
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:07:33 -0400

    "mv: get super/different_way_or_number/small_world/life.py, super/big_work/person_or_case/hand/big_thing/first_hand/other_week_and_place.py, super/ask_work_at_public_fact/last_child/important_point/call_public_child_over_new_group/bad_part_or_few_thing/able_point.py and super/able_thing_or_hand/call_early_government/great_place/work_week.py by right time"

---
+++ super/big_work/person_or_case/hand/big_thing/first_hand/other_week_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_fact(str_arg):
+    way(str_arg)
+    print('time_or_right_eye')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_fact('see_year_after_high_year')
---
+++ super/different_way_or_number/small_world/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_woman_or_world(str_arg):
+    number(str_arg)
+    print('week_or_old_hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_woman_or_world('come_man_from_early_group')
