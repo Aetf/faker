1dee8dc1aae2a2429ac8659878fc789dd24a4d9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 11:08:40 -0400

    "add: do super/place/find_problem_over_part.py or super/person_or_company/other_government/number.py at next thing"

---
+++ super/place/find_problem_over_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    early_woman(str_arg)
+    print('life_and_right_group')
+
+def early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('company_or_young_year')
