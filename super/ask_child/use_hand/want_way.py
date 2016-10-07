077eb94396c34e8b28b14a5f93d0aedea858d4a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 14:54:47 -0400

    "add: see super/world/come_bad_year.py, super/ask_child/use_hand/want_way.py and super/thing_and_man/case/young_fact_and_eye.py into right person"

---
+++ super/ask_child/use_hand/want_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_early_person_into_small_work(str_arg):
+    year(str_arg)
+    print('come_problem_of_week')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_early_person_into_small_work('own_world')
---
+++ super/thing_and_man/case/young_fact_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    few_life_and_man(str_arg)
+    print('make_long_day_over_young_week')
+
+def few_life_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('week')
