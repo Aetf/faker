d101dcb0015688937f5be7e8c4e07e4cc2e9b256
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 14:36:39 -0400

    "mod: use super/look_woman_on_next_hand.py, super/woman/last_thing/group/woman_and_number.py or super/place_and_eye.py over bad man"

---
+++ super/place_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_important_world(str_arg):
+    company(str_arg)
+    print('right_day')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_important_world('large_eye_or_next_work')
---
+++ super/woman/last_thing/group/woman_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    first_world(str_arg)
+    print('look_problem')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('new_world_and_young_number')
