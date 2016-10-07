4ad5017e2c1d315b4a96751b30de2c0dce060f34
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:08:26 -0400

    "mix: see super/right_day_or_new_world/leave_little_case/company/have_group.py by big number"

---
+++ super/right_day_or_new_world/leave_little_case/company/have_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_time(str_arg):
+    able_government_or_last_life(str_arg)
+    print('work')
+
+def able_government_or_last_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_time('next_eye_or_day')
