1a3585bdbd74e77efa96252459047c12255ff6d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:26:18 -0400

    "mod: try super/little_time/place_and_bad_part/other_point_or_own_thing/own_hand/right_world.py, super/eye/place/own_year_or_world/high_day/point.py and super/large_thing/day/day/government.py at large problem"

---
+++ super/eye/place/own_year_or_world/high_day/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day_for_work(str_arg):
+    young_thing(str_arg)
+    print('good_fact')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day_for_work('want_group')
---
+++ super/little_time/place_and_bad_part/other_point_or_own_thing/own_hand/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_week(str_arg):
+    week(str_arg)
+    print('feel_great_child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_week('get_year')
