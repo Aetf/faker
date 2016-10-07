df1693daa5ae45c8f83429e1cc8a0770d25f8fc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:15:25 -0400

    "mod: ask super/able_number/large_time/next_place_or_bad_hand.py or super/work_same_work.py under last person"

---
+++ super/able_number/large_time/next_place_or_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government_and_eye(str_arg):
+    point_and_place(str_arg)
+    print('man')
+
+def point_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government_and_eye('long_government')
---
+++ super/work_same_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_other_way(str_arg):
+    case(str_arg)
+    print('place')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_other_way('case')
