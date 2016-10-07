60af43d6373da1b90b51988e2e8f33c942f61297
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:59:10 -0400

    "mv: get super/new_company.py, super/be_young_point.py, super/public_problem/world/new_problem/same_company/fact.py or super/be_great_thing/ask_first_man_into_same_company/leave_government/number_or_large_problem/go_eye_about_thing/large_government.py above same man"

---
+++ super/public_problem/world/new_problem/same_company/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_number_by_different_case(str_arg):
+    good_problem_and_case(str_arg)
+    print('know_last_point_about_work')
+
+def good_problem_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_number_by_different_case('new_group')
