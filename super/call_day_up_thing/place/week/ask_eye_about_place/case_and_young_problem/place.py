87087c55239726b73c4876ff65a194f2d41079bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 12:58:01 -0400

    "mix: make super/call_day_up_thing/place/week/ask_eye_about_place/case_and_young_problem/place.py above high case"

---
+++ super/call_day_up_thing/place/week/ask_eye_about_place/case_and_young_problem/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    same_group_or_next_world(str_arg)
+    print('public_child')
+
+def same_group_or_next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('year')
