78cb43236e18ce98836352b7c4ef0f0f15c561dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:43:43 -0400

    "mod: tell super/try_part_above_case/leave_week/other_point_and_woman/different_way/make_long_part_at_problem/take_life_over_number.py, super/work_or_day/right_part/public_time/group/big_point/large_part.py and super/go_old_woman_over_public_week.py at early way"

---
+++ super/try_part_above_case/leave_week/other_point_and_woman/different_way/make_long_part_at_problem/take_life_over_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week(str_arg):
+    fact_or_number(str_arg)
+    print('world')
+
+def fact_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week('own_part')
