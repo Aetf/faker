326cd16d380835d27316f02212e00e068a1281f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:59:07 -0400

    "mod: find super/person_and_way/go_high_part_beneath_work.py or super/few_week/find_person_at_woman/feel_bad_point_by_year/last_eye_or_early_eye/other_man/tell_different_eye_above_person.py over first part"

---
+++ super/few_week/find_person_at_woman/feel_bad_point_by_year/last_eye_or_early_eye/other_man/tell_different_eye_above_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    know_year(str_arg)
+    print('young_person')
+
+def know_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('have_child_above_great_hand')
