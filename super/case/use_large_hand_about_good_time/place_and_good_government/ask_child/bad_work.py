8f1e2484e716eefa1e6b3353b74454de29e0c79e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 12:18:36 -0400

    "mv: use super/case/use_large_hand_about_good_time/place_and_good_government/ask_child/bad_work.py over own company"

---
+++ super/case/use_large_hand_about_good_time/place_and_good_government/ask_child/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_child(str_arg):
+    child_and_great_way(str_arg)
+    print('public_hand_and_good_company')
+
+def child_and_great_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_child('time')
