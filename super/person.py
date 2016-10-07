8d5e05725ded03fdc9f9d8491e47adccb637ef79
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 12:54:27 -0400

    "rm: do super/person.py about old part"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_hand_on_same_work(str_arg):
+    year(str_arg)
+    print('other_fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_hand_on_same_work('case_and_little_year')
