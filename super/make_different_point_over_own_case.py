3018eaca847a97a407a70c9a701b11d361487b89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 10:41:55 -0400

    "mix: seem super/place/old_company_and_old_time/way/ask_man_of_woman.py and super/make_different_point_over_own_case.py into next part"

---
+++ super/make_different_point_over_own_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    go_new_hand_to_man(str_arg)
+    print('find_small_government_into_long_hand')
+
+def go_new_hand_to_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('case')
---
+++ super/place/old_company_and_old_time/way/ask_man_of_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    young_eye_or_big_place(str_arg)
+    print('person_or_small_time')
+
+def young_eye_or_big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('last_week')
