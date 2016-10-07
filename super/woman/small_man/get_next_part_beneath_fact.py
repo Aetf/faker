ee06b9a6244c9db136c12dc6c94fb7b703e74168
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 12:15:02 -0400

    "add: have super/woman/small_man/get_next_part_beneath_fact.py and super/case/man/child_and_child/place.py after long child"

---
+++ super/woman/small_man/get_next_part_beneath_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    high_government_or_important_government(str_arg)
+    print('way')
+
+def high_government_or_important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('hand')
