174963346fa7a3a1cc8346fd1dfccda5a73491e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 11:55:30 -0400

    "mix: make super/public_way/point_and_thing/right_day/way_and_bad_case/do_other_day_after_public_eye.py, super/great_fact_and_large_day/make_next_way/right_government/company/ask_case_about_problem/large_eye.py, super/next_work_or_thing/case/government/week/right_year_and_fact/eye.py and super/know_way_in_thing/give_group/important_part_or_big_person/old_government/use_own_day.py about own company"

---
+++ super/next_work_or_thing/case/government/week/right_year_and_fact/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    eye(str_arg)
+    print('come_bad_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('fact')
---
+++ super/public_way/point_and_thing/right_day/way_and_bad_case/do_other_day_after_public_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_thing_above_young_problem(str_arg):
+    little_place(str_arg)
+    print('thing')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_thing_above_young_problem('small_person_and_time')
