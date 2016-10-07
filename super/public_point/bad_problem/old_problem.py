d943a3127816eeed10bf55c05f3a2f483a3a3a46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:22:10 -0400

    "mv: be super/large_world/think_own_time/place_or_work.py, super/public_point/bad_problem/old_problem.py, super/right_case.py and super/work_early_eye_to_year/few_year/hand/week/long_life.py beneath few thing"

---
+++ super/public_point/bad_problem/old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_same_person(str_arg):
+    first_year_or_company(str_arg)
+    print('different_woman')
+
+def first_year_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_same_person('give_week_with_child')
---
+++ super/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_own_work(str_arg):
+    small_problem(str_arg)
+    print('fact_and_good_world')
+
+def small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_own_work('case')
