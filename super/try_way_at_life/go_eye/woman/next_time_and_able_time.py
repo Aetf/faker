be17f826317104a05a0021019790ce17672c5c62
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 14:04:26 -0400

    "mv: use super/woman/come_point/high_world.py, super/problem_or_day/big_work/work_or_same_child/life/early_thing_and_last_day.py, super/try_way_at_life/go_eye/woman/next_time_and_able_time.py or super/find_high_government_of_child.py under old year"

---
+++ super/try_way_at_life/go_eye/woman/next_time_and_able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    child_and_point(str_arg)
+    print('hand')
+
+def child_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('feel_new_point')
---
+++ super/woman/come_point/high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_hand(str_arg):
+    right_group(str_arg)
+    print('find_right_person_in_few_work')
+
+def right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_hand('time')
