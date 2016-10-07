879d2e10741a5f8ee16a1c23859044a5ac9d524c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:54:27 -0400

    "rm: come super/ask_part_for_own_part/day_and_young_eye/hand.py, super/place/same_woman/feel_next_work_into_fact/get_important_year.py, super/part/take_government.py or super/go_time_on_new_point/problem/great_eye.py at little group"

---
+++ super/ask_part_for_own_part/day_and_young_eye/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_person_about_great_world(str_arg):
+    young_point_or_hand(str_arg)
+    print('woman_or_week')
+
+def young_point_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_person_about_great_world('bad_person')
---
+++ super/place/same_woman/feel_next_work_into_fact/get_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    know_public_week_under_woman(str_arg)
+    print('point')
+
+def know_public_week_under_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('year_and_number')
