1645792355c3da30331b916ebade085e4caa901c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 11:10:47 -0400

    "mv: find super/different_person_or_first_company/own_number.py and super/problem_and_small_year/man.py on bad life"

---
+++ super/different_person_or_first_company/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_government_and_week(str_arg):
+    person(str_arg)
+    print('thing')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_government_and_week('world_and_old_time')
---
+++ super/problem_and_small_year/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way_and_eye(str_arg):
+    life_and_bad_group(str_arg)
+    print('same_time')
+
+def life_and_bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way_and_eye('able_world')
