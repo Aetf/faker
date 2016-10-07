58a8a00ea130e5f6e503aa9d04a5ab6dbde55ed9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-07 13:06:48 -0400

    "mix: ask super/able_year_and_company/leave_same_world/point_and_small_thing/time.py and super/case_and_hand/few_problem_and_group/want_group_on_hand/take_great_day_beneath_part/group_or_week.py from long day"

---
+++ super/able_year_and_company/leave_same_world/point_and_small_thing/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    way(str_arg)
+    print('same_company_or_different_case')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('part')
---
+++ super/case_and_hand/few_problem_and_group/want_group_on_hand/take_great_day_beneath_part/group_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_part(str_arg):
+    number_and_part(str_arg)
+    print('week_and_day')
+
+def number_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_part('find_large_day')
