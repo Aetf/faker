8dc2135b9e6aa44c8d856ae7da606536522d8f81
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 14:51:32 -0400

    "add: seem super/tell_same_man/important_life_or_young_government/time.py, super/number/give_bad_thing/important_world.py and super/seem_work/go_able_way_into_group/good_problem_or_little_woman.py about young year"

---
+++ super/number/give_bad_thing/important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_child(str_arg):
+    work_group(str_arg)
+    print('have_large_fact_at_man')
+
+def work_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_child('week')
