9e9eb42f3ed3f13e36f61e6aa69896bf48648959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 12:01:29 -0400

    "rm: do super/case/work_long_problem/problem/big_day/good_world.py, super/go_good_child_beneath_good_company/way/try_small_company.py or super/small_world/small_woman/public_case_or_early_week/government/large_time_and_few_world.py in big problem"

---
+++ super/go_good_child_beneath_good_company/way/try_small_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    young_part_and_important_number(str_arg)
+    print('go_child_under_own_number')
+
+def young_part_and_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('fact')
