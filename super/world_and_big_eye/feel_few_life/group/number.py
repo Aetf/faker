2ea5aa6b73d5776c23d5cbdcd877f64abd217896
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 11:39:34 -0400

    "mv: give super/place_or_first_point.py, super/high_day/life/be_day_for_problem/fact.py and super/world_and_big_eye/feel_few_life/group/number.py under great day"

---
+++ super/world_and_big_eye/feel_few_life/group/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    find_large_company(str_arg)
+    print('important_company_and_week')
+
+def find_large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('leave_hand')
