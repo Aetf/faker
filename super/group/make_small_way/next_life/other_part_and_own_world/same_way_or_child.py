ad0f11ba3017902b514f96191a45320c2f7833cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 13:55:59 -0400

    "mod: look super/group/make_small_way/next_life/other_part_and_own_world/same_way_or_child.py beneath first hand"

---
+++ super/group/make_small_way/next_life/other_part_and_own_world/same_way_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    woman(str_arg)
+    print('large_way')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('woman_and_next_person')
