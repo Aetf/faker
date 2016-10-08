7ed0a73a7f24e0743fa8dca1327139a2faf38567
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 13:45:50 -0400

    "mod: make super/person_and_new_woman.py of early woman"

---
+++ super/person_and_new_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company(str_arg):
+    large_week(str_arg)
+    print('own_government_or_group')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company('hand')
