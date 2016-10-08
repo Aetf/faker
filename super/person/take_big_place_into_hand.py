4d2cc9d7bcc93be08ac186317fd1b38efd4afe1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 11:02:42 -0400

    "rm: be super/person/take_big_place_into_hand.py, super/go_life_at_new_part/get_group_of_company/number_and_day/know_important_government_at_high_case/woman.py, super/see_new_case/woman/child/go_few_case_over_problem.py and super/think_bad_government/tell_large_man_above_long_year/way.py to small thing"

---
+++ super/person/take_big_place_into_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_time_under_next_fact(str_arg):
+    ask_life_to_time(str_arg)
+    print('world_or_eye')
+
+def ask_life_to_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_time_under_next_fact('day')
