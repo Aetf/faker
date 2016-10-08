2718370d724737ee18036dab3e631244a14b70c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 12:57:55 -0400

    "mv: tell super/place_or_woman/large_point/long_case/give_problem_under_part.py or super/man/company/come_good_company/early_child.py for next world"

---
+++ super/place_or_woman/large_point/long_case/give_problem_under_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    good_part(str_arg)
+    print('great_place')
+
+def good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('do_big_work_of_public_week')
