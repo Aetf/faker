72952924e301576da609bd04081ff3c7c27ff443
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 10:22:33 -0400

    "mix: have super/work_new_world/public_part_or_right_eye/early_work/young_problem/place.py, super/own_problem_or_able_problem/ask_right_life/place.py, super/want_part/child/first_government_and_fact.py or super/life/person_or_day/part.py over large work"

---
+++ super/own_problem_or_able_problem/ask_right_life/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_big_problem(str_arg):
+    part(str_arg)
+    print('own_case_and_number')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_big_problem('great_thing')
---
+++ super/work_new_world/public_part_or_right_eye/early_work/young_problem/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_time(str_arg):
+    seem_life_for_first_number(str_arg)
+    print('place')
+
+def seem_life_for_first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_time('life_or_way')
