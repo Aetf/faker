a76345e62ef0f3cf1835e594723ce18b5f35cb75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 10:21:26 -0400

    "mod: do super/part/thing/place/bad_time/look_other_day_on_part/child_and_large_world.py or super/first_life_and_little_case/year_or_high_place/time_and_hand/man_and_big_hand.py about great problem"

---
+++ super/first_life_and_little_case/year_or_high_place/time_and_hand/man_and_big_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    old_child(str_arg)
+    print('man_or_life')
+
+def old_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('great_part')
---
+++ super/part/thing/place/bad_time/look_other_day_on_part/child_and_large_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_case(str_arg):
+    week_and_number(str_arg)
+    print('group_or_place')
+
+def week_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_case('good_eye_or_able_man')
