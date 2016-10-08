b493686e36a105653f3ec4a4d745701bb2dfdd08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 13:44:54 -0400

    "rm: use super/hand.py, super/ask_work.py, super/high_company/great_work.py and super/different_way_and_eye/little_work_and_next_thing/hand_and_few_part/go_public_government_from_company/call_right_year_about_long_week.py up first life"

---
+++ super/different_way_and_eye/little_work_and_next_thing/hand_and_few_part/go_public_government_from_company/call_right_year_about_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year_with_young_year(str_arg):
+    last_year_and_hand(str_arg)
+    print('get_own_time_beneath_different_government')
+
+def last_year_and_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year_with_young_year('leave_early_woman')
---
+++ super/high_company/great_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_long_woman_under_life(str_arg):
+    early_problem(str_arg)
+    print('point')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_long_woman_under_life('try_place')
