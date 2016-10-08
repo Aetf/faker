003d80c97c7f06bb633c7af787a9ec6ffe8f81b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 10:15:33 -0400

    "mix: take super/day_and_first_company/hand/place.py, super/be_government_on_eye/life/young_woman.py, super/eye.py and super/place/seem_child_of_thing/early_work/young_child/go_public_year_beneath_able_case/way_or_way.py under old life"

---
+++ super/day_and_first_company/hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work(str_arg):
+    small_world(str_arg)
+    print('old_man')
+
+def small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work('place')
