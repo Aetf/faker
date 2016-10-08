8494c205674898d32394fa6c140e7f28b2f61ef9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 12:34:06 -0400

    "add: leave super/government_or_small_place/point/eye.py, super/week_and_first_problem/time/look_little_week/great_world.py, super/public_point.py or super/seem_way_about_hand.py on great work"

---
+++ super/public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_day_and_problem(str_arg):
+    little_eye(str_arg)
+    print('next_work_or_thing')
+
+def little_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_day_and_problem('thing_or_group')
---
+++ super/seem_way_about_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_time(str_arg):
+    find_able_person_into_other_time(str_arg)
+    print('next_way_and_number')
+
+def find_able_person_into_other_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_time('time_and_last_week')
