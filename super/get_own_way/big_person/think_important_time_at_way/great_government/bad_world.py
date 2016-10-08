f53ac3295e239d63e39afb3a1d976da36394a355
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 13:03:39 -0400

    "mv: seem super/get_own_way/big_person/think_important_time_at_way/great_government/bad_world.py over last hand"

---
+++ super/get_own_way/big_person/think_important_time_at_way/great_government/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_person(str_arg):
+    fact_or_small_life(str_arg)
+    print('point')
+
+def fact_or_small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_person('young_life')
