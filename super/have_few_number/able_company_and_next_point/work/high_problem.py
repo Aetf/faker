43dd052cd4f0916276ac5ce94e542117292ab80f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 12:21:39 -0400

    "add: feel super/year/last_work/think_work_at_time.py and super/have_few_number/able_company_and_next_point/work/high_problem.py in great day"

---
+++ super/have_few_number/able_company_and_next_point/work/high_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_hand_and_large_point(str_arg):
+    world_or_last_world(str_arg)
+    print('world')
+
+def world_or_last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_hand_and_large_point('come_good_work_beneath_large_child')
