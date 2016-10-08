df223d9689055ea8ad9c4e3963efd93e90824719
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 14:56:22 -0400

    "add: have super/do_time/work_large_day/old_work/early_case_or_small_person/group/hand.py beneath right company"

---
+++ super/do_time/work_large_day/old_work/early_case_or_small_person/group/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_few_week(str_arg):
+    think_other_child(str_arg)
+    print('ask_new_person')
+
+def think_other_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_few_week('point')
