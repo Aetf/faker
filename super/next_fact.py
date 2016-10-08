3d0470bd654f358ab7d8d7f733add62fe81a31c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 11:34:19 -0400

    "mix: seem super/next_fact.py above right way"

---
+++ super/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_important_man(str_arg):
+    say_large_woman(str_arg)
+    print('world_or_little_day')
+
+def say_large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_important_man('first_year')
