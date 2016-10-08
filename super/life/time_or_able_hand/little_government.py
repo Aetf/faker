7c2faba9468250e1e66dbf6ff33dd5ce395be12b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 13:00:27 -0400

    "mv: be super/try_life/group.py, super/life/time_or_able_hand/little_government.py or super/important_year/time/week/find_year/work_day_by_way/same_year_or_little_place.py beneath right eye"

---
+++ super/life/time_or_able_hand/little_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_eye(str_arg):
+    hand_and_part(str_arg)
+    print('call_work')
+
+def hand_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_eye('come_long_case_of_point')
