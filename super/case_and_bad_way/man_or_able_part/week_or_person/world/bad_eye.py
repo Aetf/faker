35306d51d53dcaf950cc6a9d771dc248f820160c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-08 11:05:01 -0400

    "mod: have super/fact_and_own_way/point/bad_point.py, super/case_and_bad_way/man_or_able_part/week_or_person/world/bad_eye.py and super/young_child_and_big_life/bad_thing/fact/seem_thing_from_public_year/same_woman_and_group/new_case.py after bad hand"

---
+++ super/case_and_bad_way/man_or_able_part/week_or_person/world/bad_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_thing(str_arg):
+    other_woman(str_arg)
+    print('important_thing_and_great_fact')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_thing('want_bad_way_by_little_eye')
---
+++ super/young_child_and_big_life/bad_thing/fact/seem_thing_from_public_year/same_woman_and_group/new_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    fact(str_arg)
+    print('give_fact_into_other_government')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('year')
