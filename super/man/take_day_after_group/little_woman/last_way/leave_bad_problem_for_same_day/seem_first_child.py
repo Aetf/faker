a5c9b5f6b12d56b4f6e701ed3ae7e32f55d40553
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:26:49 -0400

    "mix: want super/man/take_day_after_group/little_woman/last_way/leave_bad_problem_for_same_day/seem_first_child.py to own week"

---
+++ super/man/take_day_after_group/little_woman/last_way/leave_bad_problem_for_same_day/seem_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_case_and_group(str_arg)
+    print('week')
+
+def same_case_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('own_time_and_public_group')
