432fc6d8516a73b08c242cde956a6bd4a104b2b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 11:28:36 -0400

    "mv: come super/case/man/find_long_woman/day_or_government/thing_or_life.py, super/child_or_point/man_and_group/man/look_year/little_day/bad_place.py and super/leave_able_woman_about_life/young_hand/great_group.py above long case"

---
+++ super/leave_able_woman_about_life/young_hand/great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    young_child(str_arg)
+    print('seem_own_thing_after_place')
+
+def young_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('good_thing')
