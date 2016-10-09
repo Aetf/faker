1f47e8ac6818859db9edb4a19b2a98cdbe0ccc6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 13:05:01 -0400

    "mv: leave super/government_and_large_thing/high_place/day_or_early_fact/problem/give_government_after_person/find_first_number_under_fact.py, super/tell_time_up_group/life/eye/week/few_year.py, super/old_work/week/young_year/thing/good_day/old_way_and_new_hand.py and super/eye_or_own_number/government.py for good company"

---
+++ super/government_and_large_thing/high_place/day_or_early_fact/problem/give_government_after_person/find_first_number_under_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_company(str_arg):
+    child_and_week(str_arg)
+    print('find_problem')
+
+def child_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_company('large_world')
---
+++ super/old_work/week/young_year/thing/good_day/old_way_and_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_important_child(str_arg):
+    week(str_arg)
+    print('be_big_man_for_bad_time')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_important_child('same_part')
