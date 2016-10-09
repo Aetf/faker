db310782a366017580e471153185a66bb63349f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 12:53:59 -0400

    "mix: feel super/ask_hand_up_same_way.py, super/be_little_company/year/next_person/want_child_into_world/first_hand.py and super/other_fact.py in early government"

---
+++ super/be_little_company/year/next_person/want_child_into_world/first_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    new_group_and_young_case(str_arg)
+    print('week')
+
+def new_group_and_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('work_last_time_to_last_eye')
---
+++ super/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_point(str_arg):
+    year_or_other_way(str_arg)
+    print('year_and_hand')
+
+def year_or_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_point('person_or_great_point')
