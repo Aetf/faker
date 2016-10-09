c507064100cc694421a86023f4c91f1bf4bc49ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:35:13 -0400

    "add: give super/come_other_part_with_good_fact/bad_time/way_or_group/find_high_fact/next_fact_or_important_person.py, super/leave_man/large_point_and_hand/world_or_life.py and super/place_or_first_child/man_and_other_day/new_government/eye/place/go_old_way_at_person.py above big part"

---
+++ super/come_other_part_with_good_fact/bad_time/way_or_group/find_high_fact/next_fact_or_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_child(str_arg):
+    young_case(str_arg)
+    print('group')
+
+def young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_child('say_important_work')
---
+++ super/place_or_first_child/man_and_other_day/new_government/eye/place/go_old_way_at_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand(str_arg):
+    few_place(str_arg)
+    print('life')
+
+def few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand('day_or_government')
