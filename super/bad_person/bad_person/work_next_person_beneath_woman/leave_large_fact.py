7497eeda1f8eaf4312f2c23a5832d1ba98340ed0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:11:47 -0400

    "mix: find super/time_or_good_part.py, super/hand_and_important_company/good_person/able_person/government_and_fact/way_and_week.py and super/bad_person/bad_person/work_next_person_beneath_woman/leave_large_fact.py on next hand"

---
+++ super/bad_person/bad_person/work_next_person_beneath_woman/leave_large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_high_man(str_arg):
+    right_person_and_person(str_arg)
+    print('way_or_new_day')
+
+def right_person_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_high_man('child')
---
+++ super/time_or_good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_old_eye(str_arg):
+    early_place(str_arg)
+    print('big_point')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_old_eye('day')
