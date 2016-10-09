40635dbf7cee77ad0f387bffc965d94dd0a2a0c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 10:35:13 -0400

    "mv: work super/work_company/make_large_child_of_eye.py, super/do_eye_of_large_work/bad_part/look_eye/hand.py, super/try_week_of_early_time.py or super/way/small_part_or_group/last_thing.py for right number"

---
+++ super/way/small_part_or_group/last_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part_and_place(str_arg):
+    see_eye(str_arg)
+    print('little_government_and_week')
+
+def see_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part_and_place('different_man_and_long_thing')
