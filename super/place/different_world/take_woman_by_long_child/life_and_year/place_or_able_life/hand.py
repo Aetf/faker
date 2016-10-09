efb6f80e3d0c34c7975681c70acbbd57dbb1fd04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 10:40:38 -0400

    "rm: go super/long_man_or_world/own_problem_and_group/public_work/same_week_and_early_hand/tell_big_part/life_and_world.py or super/place/different_world/take_woman_by_long_child/life_and_year/place_or_able_life/hand.py of last government"

---
+++ super/place/different_world/take_woman_by_long_child/life_and_year/place_or_able_life/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_point_up_year(str_arg):
+    problem(str_arg)
+    print('last_problem_or_different_way')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_point_up_year('able_thing_and_way')
