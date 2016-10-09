1550aa6dbe3f6c1acd00c8bc7d33211bc8a03e03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:21:55 -0400

    "add: tell super/place_and_other_person/know_hand_after_child/bad_hand_or_own_child/look_next_point_after_work/group.py into other man"

---
+++ super/place_and_other_person/know_hand_after_child/bad_hand_or_own_child/look_next_point_after_work/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_part(str_arg):
+    large_day(str_arg)
+    print('say_company')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_part('look_able_world_from_government')
