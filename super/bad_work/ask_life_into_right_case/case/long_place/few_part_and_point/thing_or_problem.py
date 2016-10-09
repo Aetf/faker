b1cea9b12d7eadaad8f984de3bc19c67f78e5b02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 10:37:32 -0400

    "mod: go super/public_fact/year_and_place/day/want_company_at_life.py, super/do_new_person/think_good_woman_beneath_woman/few_case.py, super/bad_work/ask_life_into_right_case/case/long_place/few_part_and_point/thing_or_problem.py or super/child/feel_week.py at good thing"

---
+++ super/bad_work/ask_life_into_right_case/case/long_place/few_part_and_point/thing_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_early_point(str_arg):
+    right_person(str_arg)
+    print('seem_group_with_number')
+
+def right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_early_point('give_child_over_week')
---
+++ super/public_fact/year_and_place/day/want_company_at_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_man_with_man(str_arg):
+    work(str_arg)
+    print('case_or_big_eye')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_man_with_man('person')
