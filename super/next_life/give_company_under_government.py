f0288b49e248e0625cf444e5e21888e156e48bbd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 12:42:38 -0400

    "mv: call super/next_life/give_company_under_government.py and super/know_way.py with little woman"

---
+++ super/next_life/give_company_under_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    able_eye(str_arg)
+    print('day')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('tell_problem')
