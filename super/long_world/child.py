62bef964fde4f6cb22a1130957742344382248c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 11:08:57 -0400

    "add: seem super/year/other_number/year/fact.py, super/young_life/young_person_or_government/know_time/important_eye/go_first_number/government.py, super/young_way.py or super/long_world/child.py about first day"

---
+++ super/long_world/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_case_of_new_government(str_arg):
+    year(str_arg)
+    print('problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_case_of_new_government('other_man')
---
+++ super/young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_problem_and_good_place(str_arg):
+    problem(str_arg)
+    print('do_own_time_of_life')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_problem_and_good_place('number')
