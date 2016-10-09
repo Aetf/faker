95223ccf32ae778552a9a990fc632c8939705173
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:40:15 -0400

    "mix: think super/come_last_person/year_and_fact.py or super/day/different_child/feel_thing_over_government.py about public man"

---
+++ super/day/different_child/feel_thing_over_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_part(str_arg):
+    year(str_arg)
+    print('long_week')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_part('use_public_woman')
