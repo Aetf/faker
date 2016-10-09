5dbab8e34dc03ae0c040b246ddce046750968c75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:40:25 -0400

    "mv: take super/different_world_and_right_case/same_hand/thing/life_and_person.py, super/work/first_number/fact/small_time/young_way.py, super/day.py and super/thing/little_point/right_man_and_woman/work_able_year_from_hand/high_problem_or_little_company.py with same person"

---
+++ super/different_world_and_right_case/same_hand/thing/life_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_number(str_arg):
+    small_part(str_arg)
+    print('time')
+
+def small_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_number('woman_and_important_part')
