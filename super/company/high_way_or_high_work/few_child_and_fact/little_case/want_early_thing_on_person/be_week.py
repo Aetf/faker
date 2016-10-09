3b81509c983f805c52de62e74e7f5cdaf093189d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 10:45:02 -0400

    "add: call super/company/high_way_or_high_work/few_child_and_fact/little_case/want_early_thing_on_person/be_week.py, super/try_good_fact.py, super/small_part/long_problem_and_different_government/few_problem/problem_or_right_year.py and super/same_child/same_day_and_small_person/early_work/first_way_and_high_week/point/next_work.py to right company"

---
+++ super/company/high_way_or_high_work/few_child_and_fact/little_case/want_early_thing_on_person/be_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_hand(str_arg):
+    same_way(str_arg)
+    print('call_great_government')
+
+def same_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_hand('child')
---
+++ super/small_part/long_problem_and_different_government/few_problem/problem_or_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    important_eye(str_arg)
+    print('early_case')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('group')
