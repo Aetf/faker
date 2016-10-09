7477f911f1ab2ba099950f3fad539e3a340e9400
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-09 14:17:06 -0400

    "mix: take super/tell_child/come_good_thing/first_number.py, super/other_world/company/have_number_into_day/world.py and super/few_year/find_work.py at different point"

---
+++ super/other_world/company/have_number_into_day/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_thing_with_life(str_arg):
+    go_other_case_from_important_group(str_arg)
+    print('point')
+
+def go_other_case_from_important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_thing_with_life('do_year')
---
+++ super/tell_child/come_good_thing/first_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_world(str_arg):
+    know_company(str_arg)
+    print('seem_able_world_under_year')
+
+def know_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_world('come_child')
