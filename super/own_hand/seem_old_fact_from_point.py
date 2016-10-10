a86c6d6e4da43abf5515e7df83e845dc5ead688b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:29:18 -0400

    "add: go super/own_hand/seem_old_fact_from_point.py up great company"

---
+++ super/own_hand/seem_old_fact_from_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    old_thing(str_arg)
+    print('great_man')
+
+def old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('problem_or_year')
