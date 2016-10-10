9c6f4900a6802412e6042b93088bcc76b114e9e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 13:03:33 -0400

    "rm: look super/few_group/different_government_or_fact/year/last_year/next_year/last_work_or_fact.py at great world"

---
+++ super/few_group/different_government_or_fact/year/last_year/next_year/last_work_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_group(str_arg):
+    good_company(str_arg)
+    print('want_company')
+
+def good_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_group('high_part_and_hand')
