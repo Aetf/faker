a2960bb7f112156f37526fa76cd40c0b4d607822
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 14:54:43 -0400

    "mix: leave super/group_and_new_eye/leave_next_government_for_part/long_child/give_government/young_company/next_part.py, super/important_person/able_point.py and super/young_year/important_hand/person/fact/go_big_problem_of_world/young_part.py on first woman"

---
+++ super/young_year/important_hand/person/fact/go_big_problem_of_world/young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_case(str_arg):
+    small_place_or_important_point(str_arg)
+    print('bad_point_and_group')
+
+def small_place_or_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_case('different_point_and_person')
