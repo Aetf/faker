27df049c4088b351a8d4b205a28720629d3180cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:49:17 -0400

    "rm: want super/life_or_thing.py on right world"

---
+++ super/life_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_early_work(str_arg):
+    old_fact(str_arg)
+    print('case')
+
+def old_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_early_work('ask_thing_beneath_old_hand')
