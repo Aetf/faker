7366f3e1311b641b558ce1639e8575721b769667
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 12:20:41 -0400

    "rm: work super/old_woman/take_first_group_about_good_point/point.py or super/group/year/number_and_week/way/give_day_beneath_different_day.py of high part"

---
+++ super/old_woman/take_first_group_about_good_point/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_person(str_arg):
+    know_company(str_arg)
+    print('early_child')
+
+def know_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_person('give_new_woman_to_great_hand')
