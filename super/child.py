882e7a6344b2883264cb80946e44b083fd611ebf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 14:04:24 -0400

    "mix: find super/other_part/go_way_after_day.py or super/child.py from old case"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_little_world(str_arg):
+    other_group(str_arg)
+    print('leave_day_after_good_way')
+
+def other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_little_world('look_day')
