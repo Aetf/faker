66a4af0dacdcc2cc896e7c86f9b617caa6fbce9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:39:14 -0400

    "mix: make super/number_or_point/public_group/little_fact/day/hand.py for next problem"

---
+++ super/number_or_point/public_group/little_fact/day/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    large_world(str_arg)
+    print('early_point_and_way')
+
+def large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('thing_and_large_part')
