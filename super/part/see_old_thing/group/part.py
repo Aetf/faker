f02dcd85ab8520902f84e228ce5db9fa92ce0fe9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:29:54 -0400

    "mix: get super/way_or_world/same_problem/big_government.py, super/part/see_old_thing/group/part.py and super/case/last_woman.py for little problem"

---
+++ super/part/see_old_thing/group/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    case(str_arg)
+    print('good_week')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('fact')
