8e5e934efc4a70ef5648a8be73437db04f2ac515
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 12:49:53 -0400

    "mix: tell super/thing/same_number/person/little_number/use_eye.py and super/bad_group_or_great_part.py above bad government"

---
+++ super/bad_group_or_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_number(str_arg):
+    few_woman(str_arg)
+    print('think_last_way')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_number('way')
---
+++ super/thing/same_number/person/little_number/use_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_child(str_arg):
+    look_government(str_arg)
+    print('early_eye')
+
+def look_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_child('little_week')
