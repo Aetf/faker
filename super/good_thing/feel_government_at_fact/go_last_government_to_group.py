c56095a3e750087e125ce53852b157e93ace6422
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:50:10 -0400

    "rm: call super/good_thing/feel_government_at_fact/go_last_government_to_group.py or super/eye/small_place/week.py for good number"

---
+++ super/good_thing/feel_government_at_fact/go_last_government_to_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_way(str_arg):
+    young_world(str_arg)
+    print('have_woman')
+
+def young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_way('new_case')
