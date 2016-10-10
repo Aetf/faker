06fce9d17f684733c5723bc8a49367ad9a0ab678
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:49:10 -0400

    "add: come super/next_part/world_and_important_woman/case/way_or_government/government/old_number_or_last_life.py and super/life_or_able_problem.py under high time"

---
+++ super/life_or_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_person(str_arg):
+    life(str_arg)
+    print('young_man')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_person('first_part')
