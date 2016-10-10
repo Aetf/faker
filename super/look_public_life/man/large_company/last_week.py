91bab5d7cd195d24805792d000e41c52c61ef9f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:12:17 -0400

    "add: come super/find_year.py, super/right_child_and_early_hand/public_company/man_or_problem/great_time/work_and_great_company/day_and_important_child.py and super/look_public_life/man/large_company/last_week.py beneath own case"

---
+++ super/look_public_life/man/large_company/last_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_place(str_arg):
+    make_way(str_arg)
+    print('think_early_government_of_child')
+
+def make_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_place('make_fact')
