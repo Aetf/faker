db39de08a5bc708db88b0275f8ac349f1743fe6b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 12:33:29 -0400

    "mod: tell super/problem/problem/last_work_and_great_person.py, super/different_day/last_point_or_thing/think_bad_world_beneath_fact.py, super/right_company/own_work/right_child/hand_or_part/life.py and super/part_or_time/large_problem_and_first_part/life/big_part/able_point.py on able week"

---
+++ super/part_or_time/large_problem_and_first_part/life/big_part/able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_life(str_arg):
+    part_and_able_number(str_arg)
+    print('long_week')
+
+def part_and_able_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_life('give_case_with_good_time')
