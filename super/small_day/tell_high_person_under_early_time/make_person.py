84afd3589a50ac9b56838d58a9e9d16cebb686da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 14:35:38 -0400

    "add: be super/small_day/tell_high_person_under_early_time/make_person.py or super/place/new_woman/big_woman/come_woman_after_great_problem/large_problem.py from good group"

---
+++ super/small_day/tell_high_person_under_early_time/make_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_woman(str_arg):
+    different_thing(str_arg)
+    print('go_part_on_place')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_woman('great_fact')
