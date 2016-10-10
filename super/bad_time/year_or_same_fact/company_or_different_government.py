76ae1ab45dceea59aee64c91456f5f58d2cbc937
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:36:43 -0400

    "mix: see super/world/same_company/be_high_person_above_government/point.py, super/woman/early_year_and_bad_case/give_little_week/small_life.py and super/bad_time/year_or_same_fact/company_or_different_government.py above different year"

---
+++ super/bad_time/year_or_same_fact/company_or_different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_child_or_company(str_arg):
+    eye(str_arg)
+    print('new_number_or_high_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_child_or_company('different_woman')
---
+++ super/woman/early_year_and_bad_case/give_little_week/small_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_thing(str_arg):
+    last_problem(str_arg)
+    print('person')
+
+def last_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_thing('life')
