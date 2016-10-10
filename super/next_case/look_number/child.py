582b2af1360b46941089a74b0b30c9f196690e66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:00:38 -0400

    "mod: go super/next_case/look_number/child.py, super/new_person/right_government/year_and_bad_way/company_or_old_group.py or super/try_own_way/bad_number.py by small work"

---
+++ super/next_case/look_number/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    right_week_or_world(str_arg)
+    print('day')
+
+def right_week_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('look_way')
---
+++ super/try_own_way/bad_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_man(str_arg):
+    man(str_arg)
+    print('seem_part')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_man('be_number')
