acbbf3dd580f91e12357bf213a6cba4b14030ad6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:55:30 -0400

    "add: find super/fact/problem_or_bad_fact.py and super/fact_or_number/early_child_and_next_hand/work_number/come_world_to_work/think_bad_company/part.py into new group"

---
+++ super/fact/problem_or_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_thing_under_early_place(str_arg):
+    group(str_arg)
+    print('big_hand')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_thing_under_early_place('old_thing_or_case')
---
+++ super/fact_or_number/early_child_and_next_hand/work_number/come_world_to_work/think_bad_company/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_place(str_arg):
+    get_big_thing_into_own_woman(str_arg)
+    print('good_year')
+
+def get_big_thing_into_own_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_place('young_problem_or_time')
