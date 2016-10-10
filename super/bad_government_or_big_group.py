e87eeb8035915898e1451ec18c3177e7f06480a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 14:49:41 -0400

    "mv: try super/fact_or_child.py, super/right_work/different_place_and_case/last_week/man_or_hand/small_year.py, super/part/different_time_and_small_hand.py and super/bad_government_or_big_group.py up last year"

---
+++ super/bad_government_or_big_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    give_problem(str_arg)
+    print('young_way')
+
+def give_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('new_world')
