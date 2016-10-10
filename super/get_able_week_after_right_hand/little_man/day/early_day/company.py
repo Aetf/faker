2e9ced513cb3a47d60346be6b7b37c1f7eaefdd0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 10:49:21 -0400

    "add: think super/get_able_week_after_right_hand/little_man/day/early_day/company.py, super/great_group/use_right_day_of_big_child/part/come_day.py or super/important_group_or_point/new_case/be_company_in_new_year/big_company_and_government/able_time/other_way.py into small week"

---
+++ super/get_able_week_after_right_hand/little_man/day/early_day/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_way_or_good_year(str_arg):
+    first_thing(str_arg)
+    print('be_case')
+
+def first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_way_or_good_year('next_place')
---
+++ super/great_group/use_right_day_of_big_child/part/come_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_world(str_arg):
+    life_or_world(str_arg)
+    print('company_and_case')
+
+def life_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_world('different_government')
