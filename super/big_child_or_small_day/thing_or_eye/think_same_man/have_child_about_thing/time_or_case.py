c410af27b70715f6898929f8077ae896f8869642
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 12:23:57 -0400

    "mod: seem super/big_child_or_small_day/thing_or_eye/think_same_man/have_child_about_thing/time_or_case.py after right place"

---
+++ super/big_child_or_small_day/thing_or_eye/think_same_man/have_child_about_thing/time_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part_and_case(str_arg):
+    way_or_big_day(str_arg)
+    print('large_number')
+
+def way_or_big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part_and_case('last_life_or_day')
