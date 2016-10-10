741c014242b85914acf2d8e366d0c7d9e7abf1c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-10 11:10:25 -0400

    "mix: want super/next_woman/high_hand.py, super/take_high_problem/use_day/high_group_or_work.py or super/find_person.py after able problem"

---
+++ super/next_woman/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_old_thing(str_arg):
+    think_other_hand(str_arg)
+    print('important_group')
+
+def think_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_old_thing('number')
---
+++ super/take_high_problem/use_day/high_group_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_group(str_arg):
+    use_man(str_arg)
+    print('young_world')
+
+def use_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_group('big_world')
