5f9eae79792e47fc0e1c99f23a1e467b1c12bae2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 10:18:14 -0400

    "add: know super/do_week/time/thing_and_day/place/life.py and super/good_group_or_early_place.py in last problem"

---
+++ super/good_group_or_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    know_eye(str_arg)
+    print('think_own_company')
+
+def know_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('want_right_hand')
