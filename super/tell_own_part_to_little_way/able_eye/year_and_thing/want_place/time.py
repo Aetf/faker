e14a4ad97abc96125a9902db0699f69a7f50af5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 12:18:02 -0400

    "mv: use super/hand_and_point.py and super/tell_own_part_to_little_way/able_eye/year_and_thing/want_place/time.py up big fact"

---
+++ super/tell_own_part_to_little_way/able_eye/year_and_thing/want_place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_part_on_next_eye(str_arg):
+    bad_thing(str_arg)
+    print('make_world')
+
+def bad_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_part_on_next_eye('week')
