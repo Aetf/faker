515b83243f66b118914cd8b0f43d08d9d1a93289
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 14:38:15 -0400

    "mod: get super/long_government/public_thing/number/place/eye_and_old_case.py, super/time_and_child/hand.py or super/first_man.py of large government"

---
+++ super/long_government/public_thing/number/place/eye_and_old_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_person(str_arg):
+    first_company(str_arg)
+    print('day')
+
+def first_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_person('get_fact')
