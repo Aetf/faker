f707ed413f0c78bd8a39cb65265edf090e1eff78
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 10:30:00 -0400

    "mix: look super/high_day/number_and_hand/company.py, super/good_world/say_woman_on_company/group_and_thing/high_hand/know_place/come_thing_after_group.py, super/woman_and_week.py and super/place/time.py beneath large eye"

---
+++ super/place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    day(str_arg)
+    print('right_number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('good_time')
