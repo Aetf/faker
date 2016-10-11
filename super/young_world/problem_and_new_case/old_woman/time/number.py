e7f22a87314b4caf99ba24cc2ab96e84a910f6e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 13:52:14 -0400

    "rm: say super/young_world/problem_and_new_case/old_woman/time/number.py, super/problem/public_world_and_place/eye/woman.py and super/fact_or_big_group/do_important_day/last_way.py from long thing"

---
+++ super/young_world/problem_and_new_case/old_woman/time/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person(str_arg):
+    time(str_arg)
+    print('important_way')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person('young_number')
