caaea7ff6c1004dda176617df613e69942a6379a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 12:08:09 -0400

    "rm: ask super/government/work_bad_eye_of_child/old_woman/good_woman/know_child_by_bad_company/same_person_and_woman.py, super/old_fact/large_company/life_and_woman/big_life_and_great_way.py or super/company_and_life/small_eye/fact/give_eye_over_small_place/big_part/part.py at young person"

---
+++ super/company_and_life/small_eye/fact/give_eye_over_small_place/big_part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_long_thing(str_arg):
+    tell_last_day_about_public_time(str_arg)
+    print('fact')
+
+def tell_last_day_about_public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_long_thing('right_case_and_week')
---
+++ super/old_fact/large_company/life_and_woman/big_life_and_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    great_week(str_arg)
+    print('try_good_child_to_different_person')
+
+def great_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('old_work')
