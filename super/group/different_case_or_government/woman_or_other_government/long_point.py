948e1dae80facd757af68ebb040cf0bb19717976
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 10:16:33 -0400

    "mod: try super/point.py or super/group/different_case_or_government/woman_or_other_government/long_point.py about little case"

---
+++ super/group/different_case_or_government/woman_or_other_government/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_young_problem(str_arg):
+    life_and_able_hand(str_arg)
+    print('woman')
+
+def life_and_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_young_problem('own_person')
