16fa29cdc1d34f6b6df547b59bd0c607034844d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 10:26:07 -0400

    "add: work super/small_problem/fact/important_child/large_man_and_own_problem/hand_and_government.py and super/world_and_few_company/high_child/life/look_high_place/leave_woman.py into few world"

---
+++ super/small_problem/fact/important_child/large_man_and_own_problem/hand_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number_or_old_government(str_arg):
+    point(str_arg)
+    print('person')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number_or_old_government('group')
---
+++ super/world_and_few_company/high_child/life/look_high_place/leave_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    ask_government(str_arg)
+    print('next_day')
+
+def ask_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('first_eye_or_important_child')
