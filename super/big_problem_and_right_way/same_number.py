04cd10b5229ac8931262f4a1d6a4ed3623a4796b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 13:34:42 -0400

    "rm: make super/young_child/get_long_work_to_time/large_year_or_early_person/important_time_or_woman/other_time/part.py and super/big_problem_and_right_way/same_number.py on right man"

---
+++ super/big_problem_and_right_way/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_other_person(str_arg):
+    problem(str_arg)
+    print('little_woman')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_other_person('use_year')
---
+++ super/young_child/get_long_work_to_time/large_year_or_early_person/important_time_or_woman/other_time/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_high_week(str_arg):
+    last_place_and_little_company(str_arg)
+    print('hand')
+
+def last_place_and_little_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_high_week('know_group')
