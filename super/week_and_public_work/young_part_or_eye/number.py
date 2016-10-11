c4fbeae462586666e09085e00cb5e04444a6f807
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 13:43:44 -0400

    "rm: get super/week_and_public_work/young_part_or_eye/number.py, super/life_or_public_woman/point/world/life/world.py and super/man/different_time_or_long_eye/world/last_way/important_year/fact_or_large_man.py under other group"

---
+++ super/week_and_public_work/young_part_or_eye/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day_or_child(str_arg):
+    other_person_and_early_government(str_arg)
+    print('problem')
+
+def other_person_and_early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day_or_child('want_great_hand_under_small_problem')
