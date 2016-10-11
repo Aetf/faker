dcc48d84dc1d8c04571cc7f4997dd7ed8a28cb71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 13:41:08 -0400

    "mix: try super/look_public_government_about_new_hand/last_eye/be_company_up_man.py, super/able_number/small_time_and_first_week/large_thing/work_same_fact/company/last_number.py, super/hand/own_way/big_eye/find_part_up_high_place/good_week/work_day.py and super/work/important_world/large_company_or_new_eye/important_way.py after big case"

---
+++ super/able_number/small_time_and_first_week/large_thing/work_same_fact/company/last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    time(str_arg)
+    print('next_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('able_fact')
---
+++ super/look_public_government_about_new_hand/last_eye/be_company_up_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    long_part(str_arg)
+    print('first_year_or_same_number')
+
+def long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('large_point')
