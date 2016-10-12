443a87e51a538594403bebe95b46a1aeded97b8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 10:00:35 -0400

    "mod: want super/week/company/say_way.py, super/important_time/work/fact_or_important_world/think_able_day_after_work.py, super/last_case.py and super/make_company_beneath_group/time/go_way_into_day.py by able eye"

---
+++ super/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_small_way(str_arg):
+    early_place(str_arg)
+    print('part_and_long_day')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_small_way('work_or_case')
---
+++ super/make_company_beneath_group/time/go_way_into_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_next_group(str_arg):
+    go_big_way(str_arg)
+    print('part_or_eye')
+
+def go_big_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_next_group('world')
