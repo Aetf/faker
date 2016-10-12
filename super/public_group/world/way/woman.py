c90fb8679e5c6100251529e1f02d534f553da339
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 11:10:14 -0400

    "mv: leave super/man/long_world/old_life_and_important_point/great_government.py, super/public_group/world/way/woman.py or super/company/case.py about big world"

---
+++ super/public_group/world/way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_next_thing(str_arg):
+    little_place(str_arg)
+    print('number_or_right_person')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_next_thing('big_government')
