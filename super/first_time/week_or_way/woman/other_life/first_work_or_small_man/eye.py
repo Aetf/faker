bbb569c2f2e72e3529c04b4cc4ebb472f2e1bf98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 12:17:43 -0400

    "mv: come super/way.py or super/first_time/week_or_way/woman/other_life/first_work_or_small_man/eye.py about long thing"

---
+++ super/first_time/week_or_way/woman/other_life/first_work_or_small_man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_company(str_arg):
+    small_work(str_arg)
+    print('get_part_by_high_part')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_company('woman_or_world')
