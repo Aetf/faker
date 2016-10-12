0ef2ca98b0023685b3443948414185b473039418
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 11:30:28 -0400

    "rm: ask super/small_person/different_week.py, super/government.py or super/same_person.py up important point"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_large_man(str_arg):
+    government(str_arg)
+    print('want_new_life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_large_man('look_old_week')
---
+++ super/small_person/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_number(str_arg):
+    woman_or_world(str_arg)
+    print('time')
+
+def woman_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_number('first_part')
