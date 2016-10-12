d2158159248e6a4583709e0bc3225fae4759df7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 14:37:32 -0400

    "mv: make super/way_and_public_week.py in old man"

---
+++ super/way_and_public_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_case_at_small_way(str_arg):
+    be_right_way(str_arg)
+    print('public_way_and_long_week')
+
+def be_right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_case_at_small_way('public_case')
