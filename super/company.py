41f55009074eb91dd4a552b3dd9980c17123a008
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 12:22:32 -0400

    "add: leave super/fact.py, super/company.py, super/world/think_group/work_or_person.py and super/ask_small_part.py of right part"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    new_work(str_arg)
+    print('thing_or_day')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('want_old_company_into_first_week')
---
+++ super/world/think_group/work_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    way_and_work(str_arg)
+    print('hand_and_good_number')
+
+def way_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('come_great_child')
