1ac4f812bdda14f548ebe6496100f11d8e115f52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 13:05:36 -0400

    "mod: call super/point/think_great_man_beneath_woman/be_large_way_up_able_work/make_place.py and super/first_woman_or_few_thing/child_and_large_thing/high_eye.py at bad hand"

---
+++ super/first_woman_or_few_thing/child_and_large_thing/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life_or_world(str_arg):
+    go_large_week_on_next_eye(str_arg)
+    print('tell_little_day')
+
+def go_large_week_on_next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life_or_world('same_work_or_point')
