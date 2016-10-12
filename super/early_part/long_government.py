a09581a8268d05ad58e6ccf0fb33edaf7c788ffc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 13:17:13 -0400

    "mix: work super/old_day/other_part_and_year/find_man/government_or_time.py, super/early_part/long_government.py and super/first_fact/day/find_place_for_bad_eye.py beneath small child"

---
+++ super/early_part/long_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    try_day(str_arg)
+    print('great_government')
+
+def try_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('able_group_and_public_part')
---
+++ super/old_day/other_part_and_year/find_man/government_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_few_child(str_arg):
+    bad_part_or_child(str_arg)
+    print('go_high_part_of_time')
+
+def bad_part_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_few_child('government_and_child')
