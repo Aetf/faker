76e4169bf0ef8a693bf58a206eecd1c1fe502960
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 13:52:39 -0400

    "mv: ask super/come_young_group_in_small_eye/child_or_world/public_week/high_child/last_way/feel_world_up_little_week.py, super/small_number.py or super/get_year_under_government.py on great life"

---
+++ super/small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_important_company(str_arg):
+    young_week(str_arg)
+    print('important_group')
+
+def young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_important_company('week_or_different_number')
