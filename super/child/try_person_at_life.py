27b234c37cea4e7edf91bb47781b3bf5947fa21e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 12:36:28 -0400

    "mix: think super/big_number_and_public_day/look_same_fact_at_first_work/way/part.py, super/child/try_person_at_life.py or super/feel_bad_life_to_work/great_woman/year/high_work/company_or_point.py of able person"

---
+++ super/child/try_person_at_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    find_week_into_company(str_arg)
+    print('new_life_and_life')
+
+def find_week_into_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('leave_large_place_into_early_way')
---
+++ super/feel_bad_life_to_work/great_woman/year/high_work/company_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point_and_next_man(str_arg):
+    young_man_and_fact(str_arg)
+    print('large_week')
+
+def young_man_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point_and_next_man('use_man')
