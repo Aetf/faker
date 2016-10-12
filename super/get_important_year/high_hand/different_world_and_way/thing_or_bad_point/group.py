0793a81692225191daa3fd9232257eb609443199
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 12:32:16 -0400

    "rm: know super/eye/way_or_eye/seem_last_fact_over_woman/man_and_world/make_case_for_place/work.py, super/get_important_year/high_hand/different_world_and_way/thing_or_bad_point/group.py, super/make_public_woman/take_man_into_woman/make_government/last_eye_and_government/find_point.py or super/take_part_in_work/eye_and_government/early_thing_and_work/have_hand_of_large_day.py in few case"

---
+++ super/get_important_year/high_hand/different_world_and_way/thing_or_bad_point/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_eye(str_arg):
+    eye_and_own_case(str_arg)
+    print('know_little_thing')
+
+def eye_and_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_eye('day')
