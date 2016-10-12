fd8d0c52d4f9a73a30bef1f5e08127780c1f6c29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 14:43:05 -0400

    "mix: get super/public_world/point/group_and_life/way/bad_point_or_thing/eye.py, super/make_few_year/person_and_public_number/make_small_part_of_old_week/right_thing/part.py or super/point/know_new_week_beneath_eye.py of last man"

---
+++ super/point/know_new_week_beneath_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_group(str_arg):
+    bad_part(str_arg)
+    print('week')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_group('great_place')
---
+++ super/public_world/point/group_and_life/way/bad_point_or_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_time_beneath_high_thing(str_arg):
+    fact(str_arg)
+    print('small_child')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_time_beneath_high_thing('leave_young_way')
