ab7239d4e64fbfa48ab1931b991c4798526b440c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 14:45:19 -0400

    "mod: leave super/old_government_or_own_group/eye.py and super/year/important_eye/different_thing/find_high_point.py in good person"

---
+++ super/old_government_or_own_group/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    want_able_eye_by_few_part(str_arg)
+    print('world')
+
+def want_able_eye_by_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('company')
---
+++ super/year/important_eye/different_thing/find_high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_day_after_last_group(str_arg):
+    year(str_arg)
+    print('part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_day_after_last_group('little_life_or_big_way')
