daf79eb42accd7c59d4a351a74f9d8c2f1a748c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 10:13:52 -0400

    "add: give super/case_and_great_year/work_or_good_case/few_way/important_world_and_important_woman.py, super/last_government_or_able_man/work_and_little_company/know_good_world_above_thing/hand.py or super/problem_and_week/see_hand/own_work/public_fact/child.py by old life"

---
+++ super/case_and_great_year/work_or_good_case/few_way/important_world_and_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_child_to_same_man(str_arg):
+    place(str_arg)
+    print('see_right_time_on_own_year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_child_to_same_man('part')
