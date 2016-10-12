f8f688bc5619f54380bf2c86ac00d6e751c92b6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-12 11:13:38 -0400

    "mv: make super/long_way/last_point/woman.py or super/company.py to bad government"

---
+++ super/long_way/last_point/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government_and_big_problem(str_arg):
+    case(str_arg)
+    print('point')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government_and_big_problem('way')
