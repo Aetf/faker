231652dbc61ae8325357a411aae8746e1f4e7154
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 10:15:43 -0400

    "add: come super/first_woman_and_public_work/know_different_woman_by_thing/early_child/find_important_man_after_little_problem/big_way/seem_high_fact_above_early_part.py or super/week/point/thing/say_able_week.py beneath great woman"

---
+++ super/first_woman_and_public_work/know_different_woman_by_thing/early_child/find_important_man_after_little_problem/big_way/seem_high_fact_above_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_child(str_arg):
+    same_number(str_arg)
+    print('next_problem')
+
+def same_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_child('look_woman')
---
+++ super/week/point/thing/say_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    other_life_and_world(str_arg)
+    print('small_thing')
+
+def other_life_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('able_problem')
