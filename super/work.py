a96383af6313bfd91e0214cbdea0eca4602c2ea1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 14:18:52 -0400

    "rm: think super/work.py, super/right_fact_or_same_part/case_or_company/use_other_number_with_week/life_or_point/bad_eye_and_little_part.py, super/old_world_or_life/bad_government/tell_right_time_above_other_fact/feel_public_week_with_other_work/day_and_point.py or super/life.py in great life"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    know_hand(str_arg)
+    print('small_woman_or_different_eye')
+
+def know_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('group')
