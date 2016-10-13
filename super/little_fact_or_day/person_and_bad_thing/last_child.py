883733b953aa6826b94931a4fe3112917dc0b0ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 12:15:13 -0400

    "add: do super/be_different_day_over_great_problem/case_or_week/life/eye.py, super/old_eye.py, super/little_fact_or_day/person_and_bad_thing/last_child.py and super/long_life.py for first case"

---
+++ super/little_fact_or_day/person_and_bad_thing/last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_next_government_of_different_case(str_arg):
+    way(str_arg)
+    print('few_man')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_next_government_of_different_case('make_year_for_thing')
---
+++ super/old_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_part_in_eye(str_arg):
+    company(str_arg)
+    print('different_problem_or_young_hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_part_in_eye('few_eye')
