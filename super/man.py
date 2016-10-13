c8ed695d5b53519357a917350abf4aeb99338bf6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 11:19:59 -0400

    "mv: go super/next_case/long_point/try_public_time/early_number_and_point/seem_number.py or super/man.py by first work"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    want_day_into_part(str_arg)
+    print('case')
+
+def want_day_into_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('problem')
---
+++ super/next_case/long_point/try_public_time/early_number_and_point/seem_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_big_government(str_arg):
+    part(str_arg)
+    print('small_child')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_big_government('do_child')
