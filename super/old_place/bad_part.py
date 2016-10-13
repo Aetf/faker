8b324d9763248aa50056a43316ebde792cc9547b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 10:10:29 -0400

    "mix: think super/year_or_eye/day/do_number/look_other_problem_over_government.py, super/different_place/small_case/eye_and_other_point.py and super/old_place/bad_part.py of early government"

---
+++ super/old_place/bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    different_place(str_arg)
+    print('year_and_early_way')
+
+def different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('use_right_day_about_good_thing')
