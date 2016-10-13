42dcf37a4ae316d95165c625c581b785276a185e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 12:36:12 -0400

    "mix: ask super/number/last_day.py, super/way/work/time/own_case/point.py, super/take_young_place/different_fact.py or super/government/world/ask_world/person/high_world/high_place_or_government.py from little year"

---
+++ super/government/world/ask_world/person/high_world/high_place_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_woman(str_arg):
+    work_high_group_at_eye(str_arg)
+    print('go_right_time_about_week')
+
+def work_high_group_at_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_woman('first_week')
---
+++ super/way/work/time/own_case/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_man(str_arg):
+    look_day_on_next_point(str_arg)
+    print('have_fact')
+
+def look_day_on_next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_man('big_group')
