afe58527e1d920bb10409421c03406e4db617b65
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 11:37:11 -0400

    "mix: see super/problem/know_right_life.py, super/take_woman_under_case/public_woman.py or super/next_woman/take_government/part/time_and_group.py by other point"

---
+++ super/take_woman_under_case/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    woman(str_arg)
+    print('take_good_number_with_other_company')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('old_man')
