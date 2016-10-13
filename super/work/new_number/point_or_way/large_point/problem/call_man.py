316397b29ed8faa5fb971b091d47f6f26274a451
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 11:42:19 -0400

    "mix: look super/work/new_number/point_or_way/large_point/problem/call_man.py into big part"

---
+++ super/work/new_number/point_or_way/large_point/problem/call_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_bad_man(str_arg):
+    call_small_point_for_place(str_arg)
+    print('other_group')
+
+def call_small_point_for_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_bad_man('bad_life')
