c0b47ae6de114bf39df1496ab8e5f3f4fb0e87e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 12:10:35 -0400

    "mod: find super/important_work_and_hand/thing/other_way_or_world.py, super/think_company.py, super/day.py and super/seem_right_child/great_hand_and_big_case/own_company/child/first_eye_and_life.py in different problem"

---
+++ super/important_work_and_hand/thing/other_way_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_place_in_few_day(str_arg):
+    next_man(str_arg)
+    print('try_man_above_man')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_place_in_few_day('problem')
---
+++ super/think_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    do_public_year(str_arg)
+    print('good_government_or_point')
+
+def do_public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('other_hand')
