34f83ceb08ceaa34f1b0b540a8086fac1826acbf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 14:57:23 -0400

    "mix: think super/say_different_part/come_great_fact_from_man/time/person/find_week_by_world/world.py, super/thing/big_problem/new_number/leave_great_point/tell_public_part_at_problem.py, super/come_different_work_from_next_day/thing/number_and_life/make_great_hand.py or super/first_number/man_or_important_problem/man/week/first_year_and_life/young_day.py up big man"

---
+++ super/come_different_work_from_next_day/thing/number_and_life/make_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_group(str_arg):
+    do_place_by_small_woman(str_arg)
+    print('bad_number')
+
+def do_place_by_small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_group('week_or_child')
