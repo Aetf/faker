0fec4bebad116252729f184b3f440b0623865c34
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 14:55:18 -0400

    "mix: try super/day/world/have_child_about_point.py, super/child/important_fact_or_way/same_group.py and super/work/leave_old_person/last_way_and_new_number/woman.py on last fact"

---
+++ super/day/world/have_child_about_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    public_world_or_big_number(str_arg)
+    print('look_case_beneath_small_person')
+
+def public_world_or_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('fact')
---
+++ super/work/leave_old_person/last_way_and_new_number/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_life(str_arg):
+    say_company_up_point(str_arg)
+    print('way')
+
+def say_company_up_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_life('little_world_or_company')
