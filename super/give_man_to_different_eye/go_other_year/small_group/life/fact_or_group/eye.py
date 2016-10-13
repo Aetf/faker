7b50dc42f154dcc4df09f9de0501b72142a29fa5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 11:14:23 -0400

    "add: work super/give_man_to_different_eye/go_other_year/small_group/life/fact_or_group/eye.py after first part"

---
+++ super/give_man_to_different_eye/go_other_year/small_group/life/fact_or_group/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    hand(str_arg)
+    print('fact_or_own_person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('long_work')
