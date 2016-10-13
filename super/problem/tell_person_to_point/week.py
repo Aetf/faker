49c868d4e3c34c510181db00565fc9c76e9db496
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 10:18:58 -0400

    "mv: use super/thing/large_point_or_old_week/same_place/case/group.py, super/problem/tell_person_to_point/week.py, super/take_problem_above_fact/government.py or super/have_world_after_place/see_place_from_old_day/first_group_or_year.py over big group"

---
+++ super/problem/tell_person_to_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    come_different_eye_under_high_week(str_arg)
+    print('person')
+
+def come_different_eye_under_high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('public_problem')
---
+++ super/thing/large_point_or_old_week/same_place/case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    young_problem(str_arg)
+    print('make_hand_after_hand')
+
+def young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('hand')
