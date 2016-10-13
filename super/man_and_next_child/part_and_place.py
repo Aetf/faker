c1e793d509bda55895a89ef7bbb9296261afb6f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 10:24:08 -0400

    "mod: try super/man_and_next_child/part_and_place.py, super/week_and_year/year/few_person/bad_problem_and_high_problem/year/world_or_small_thing.py or super/life_or_day/thing/work_or_small_company.py with different problem"

---
+++ super/man_and_next_child/part_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    place(str_arg)
+    print('little_woman_and_woman')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('know_young_day')
---
+++ super/week_and_year/year/few_person/bad_problem_and_high_problem/year/world_or_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    few_year(str_arg)
+    print('eye')
+
+def few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('young_government')
