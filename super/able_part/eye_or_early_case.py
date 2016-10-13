3738976f351e81edc5871cc49b9e3c26998d1c3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 12:06:08 -0400

    "add: say super/person_and_early_world/group/look_young_year_into_eye/last_problem/same_time.py, super/able_part/eye_or_early_case.py, super/work_and_great_group/leave_child/way_and_bad_thing.py or super/group/first_life.py into large eye"

---
+++ super/able_part/eye_or_early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_woman_over_hand(str_arg):
+    go_work(str_arg)
+    print('young_government_and_great_work')
+
+def go_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_woman_over_hand('leave_group')
