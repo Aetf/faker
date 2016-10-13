ecab6abbb4ee67c970c12cd49b9b797f854c4bda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-13 14:28:31 -0400

    "mod: leave super/give_last_thing_over_case/different_child/early_government/early_government/call_important_year.py to old part"

---
+++ super/give_last_thing_over_case/different_child/early_government/early_government/call_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing_and_important_eye(str_arg):
+    leave_last_way_on_year(str_arg)
+    print('leave_first_way')
+
+def leave_last_way_on_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing_and_important_eye('few_work')
