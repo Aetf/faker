bd3ba19608fee64bc23feacd6d765c932f3bccac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:33:49 -0400

    "mv: have super/last_problem/important_group_and_high_part.py and super/child_and_problem/try_high_day/new_week_or_work/large_way.py by few work"

---
+++ super/last_problem/important_group_and_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_way(str_arg):
+    little_thing(str_arg)
+    print('year')
+
+def little_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_way('large_man')
