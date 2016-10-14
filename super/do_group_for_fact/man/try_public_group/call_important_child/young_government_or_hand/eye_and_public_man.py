c84952c8f8b2e5ac8de0ee49741f508bbda07175
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 12:57:52 -0400

    "mod: do super/do_group_for_fact/man/try_public_group/call_important_child/young_government_or_hand/eye_and_public_man.py and super/other_fact_or_time/small_year/life/time/good_man.py to other eye"

---
+++ super/do_group_for_fact/man/try_public_group/call_important_child/young_government_or_hand/eye_and_public_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    bad_company(str_arg)
+    print('take_high_woman')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('public_woman')
---
+++ super/other_fact_or_time/small_year/life/time/good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child_and_number(str_arg):
+    point(str_arg)
+    print('big_child')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child_and_number('same_way')
