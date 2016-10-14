114f2506d8b7c214bcd345d99974923e50a5fa67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 14:52:53 -0400

    "mix: seem super/government/problem/hand/first_person/right_company_and_last_company/government_or_important_point.py beneath young week"

---
+++ super/government/problem/hand/first_person/right_company_and_last_company/government_or_important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    hand(str_arg)
+    print('person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('few_government_and_man')
