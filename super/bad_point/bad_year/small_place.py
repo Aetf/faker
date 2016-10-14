ba73054d871d93958154070257e8606b52270f83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 13:08:01 -0400

    "rm: tell super/bad_point/bad_year/small_place.py, super/year/fact/woman_or_next_company.py, super/fact.py and super/group.py of old year"

---
+++ super/bad_point/bad_year/small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    year(str_arg)
+    print('place')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('different_number')
---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    get_bad_part_of_public_world(str_arg)
+    print('use_right_world')
+
+def get_bad_part_of_public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('old_place')
