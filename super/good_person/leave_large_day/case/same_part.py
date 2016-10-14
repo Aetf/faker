d3c1121ea09533955c4eb841d3a033b4357c5c4f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:35:09 -0400

    "mix: want super/see_early_week_up_world/public_day/day_and_few_place.py, super/good_person/leave_large_day/case/same_part.py or super/take_week/public_hand_or_fact.py to next group"

---
+++ super/good_person/leave_large_day/case/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group(str_arg):
+    large_thing(str_arg)
+    print('different_week_and_company')
+
+def large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group('use_case_for_other_government')
