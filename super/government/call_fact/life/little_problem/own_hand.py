2ab0205a7120edab0bfb8488201c23a3e0ea9776
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 11:27:22 -0400

    "mod: try super/government/call_fact/life/little_problem/own_hand.py or super/small_thing/look_company_up_case/go_young_company_after_small_world/same_point/public_life.py with important person"

---
+++ super/government/call_fact/life/little_problem/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_small_eye(str_arg):
+    child(str_arg)
+    print('think_bad_hand_into_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_small_eye('other_work')
---
+++ super/small_thing/look_company_up_case/go_young_company_after_small_world/same_point/public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_long_woman(str_arg):
+    point_or_hand(str_arg)
+    print('work')
+
+def point_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_long_woman('last_place')
