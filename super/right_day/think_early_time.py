83616a53f531860a1305a8a08ac7eccf242e1e2f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:55:47 -0400

    "mv: try super/want_life_about_thing.py, super/right_day/think_early_time.py, super/next_world/good_group_and_case/own_way_and_child.py or super/able_world/way/tell_hand_after_important_woman/tell_last_point_from_company/own_year_or_bad_woman.py up early man"

---
+++ super/right_day/think_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    woman_or_place(str_arg)
+    print('next_work_and_last_thing')
+
+def woman_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('week_or_high_time')
---
+++ super/want_life_about_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    want_bad_problem(str_arg)
+    print('see_person_up_case')
+
+def want_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('know_company')
