eca9db8b82734e913000b9999ccd3b009172f23f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:41:16 -0400

    "add: look super/place_or_own_government/look_fact_for_important_week/week/able_week/other_point.py, super/number_and_world/point_or_large_woman/great_problem_or_good_time/have_company/last_problem/point.py or super/see_thing/first_fact/right_way/see_own_eye.py after young week"

---
+++ super/number_and_world/point_or_large_woman/great_problem_or_good_time/have_company/last_problem/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_group_in_old_hand(str_arg):
+    own_child(str_arg)
+    print('small_work')
+
+def own_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_group_in_old_hand('early_fact')
---
+++ super/place_or_own_government/look_fact_for_important_week/week/able_week/other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week(str_arg):
+    able_eye_or_life(str_arg)
+    print('know_own_work_over_next_problem')
+
+def able_eye_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week('life')
