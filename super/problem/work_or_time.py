b08d1c054dede573726f7388c432b72508ed09ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:00:21 -0400

    "add: work super/world_and_different_thing/tell_early_week/ask_new_place_with_old_place/same_eye/year/different_thing.py or super/problem/work_or_time.py after good thing"

---
+++ super/problem/work_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_number_on_big_hand(str_arg):
+    young_government(str_arg)
+    print('early_number')
+
+def young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_number_on_big_hand('first_hand_and_big_company')
