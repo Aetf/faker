dc92158f0ced4c5cabf8f57772a637440f5d5f5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 11:52:16 -0400

    "mod: see super/number/time/government/important_life/new_man_and_problem.py, super/say_next_place.py or super/get_small_case_at_same_point/child/child_or_early_week/large_way.py for new child"

---
+++ super/number/time/government/important_life/new_man_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_life_beneath_next_part(str_arg):
+    person_and_man(str_arg)
+    print('feel_new_life')
+
+def person_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_life_beneath_next_part('man')
---
+++ super/say_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_problem(str_arg):
+    point(str_arg)
+    print('make_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_problem('last_day')
