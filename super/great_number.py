c5e92400ea0305935465f56529388408ce86b4a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 14:12:01 -0400

    "mv: be super/great_number.py on long fact"

---
+++ super/great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    able_thing(str_arg)
+    print('high_fact')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('fact_and_child')
