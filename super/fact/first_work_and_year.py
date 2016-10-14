a48e7a35a4c249700ecf24ff5f6f1dc9f15c96fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 13:02:28 -0400

    "rm: ask super/world_and_child/other_work/eye/know_right_day/work_or_own_government/point.py and super/fact/first_work_and_year.py beneath old eye"

---
+++ super/fact/first_work_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_good_part(str_arg):
+    long_day(str_arg)
+    print('woman_or_little_world')
+
+def long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_good_part('company')
---
+++ super/world_and_child/other_work/eye/know_right_day/work_or_own_government/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    person(str_arg)
+    print('large_place')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('look_next_day')
