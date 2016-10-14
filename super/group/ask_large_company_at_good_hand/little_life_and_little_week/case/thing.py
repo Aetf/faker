5300b0a1d08eab2ae858c5bb3c8a28ac19965417
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 10:37:15 -0400

    "rm: seem super/leave_old_part/case/first_part/use_point/case.py, super/number/use_small_time/do_world_by_last_child/woman/case.py, super/group/ask_large_company_at_good_hand/little_life_and_little_week/case/thing.py or super/early_way_or_next_part/problem/same_way_and_public_day/find_work/day_or_great_hand.py at small way"

---
+++ super/group/ask_large_company_at_good_hand/little_life_and_little_week/case/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem_and_group(str_arg):
+    call_high_fact(str_arg)
+    print('place')
+
+def call_high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem_and_group('year')
