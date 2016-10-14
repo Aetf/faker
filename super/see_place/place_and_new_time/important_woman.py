0b8eb61375f35d2836abc4c8af6d0d0256cd0795
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 11:27:17 -0400

    "add: be super/say_place/problem/want_high_child/work/come_little_week/fact_or_old_thing.py and super/see_place/place_and_new_time/important_woman.py about small company"

---
+++ super/see_place/place_and_new_time/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    say_woman_with_person(str_arg)
+    print('leave_fact_after_company')
+
+def say_woman_with_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('tell_government')
