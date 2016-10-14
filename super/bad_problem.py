9eadb39d76631328cc7dc0197954fb85d25c9b67
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 14:39:33 -0400

    "add: be super/number_and_time/right_week/able_year/new_government/new_life/see_life_from_old_case.py, super/year/year/public_woman_and_new_world/other_number.py and super/bad_problem.py with first week"

---
+++ super/bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_important_thing(str_arg):
+    fact(str_arg)
+    print('bad_world_or_place')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_important_thing('child')
---
+++ super/year/year/public_woman_and_new_world/other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    other_world_and_problem(str_arg)
+    print('find_group')
+
+def other_world_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('old_group')
