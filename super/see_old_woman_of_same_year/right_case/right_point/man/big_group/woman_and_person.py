1d2ce55b8e85babecd0b9cb91353a617f6921a76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 14:31:42 -0400

    "add: come super/woman_or_next_hand/own_life/public_child/be_same_eye/little_eye.py, super/case_and_number/find_work/do_thing_with_bad_number/bad_place/good_company/be_group_for_first_world.py, super/find_bad_week_by_number/bad_case/few_company.py and super/see_old_woman_of_same_year/right_case/right_point/man/big_group/woman_and_person.py over long world"

---
+++ super/see_old_woman_of_same_year/right_case/right_point/man/big_group/woman_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_person(str_arg):
+    say_world(str_arg)
+    print('have_young_work')
+
+def say_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_person('same_man_and_large_government')
---
+++ super/woman_or_next_hand/own_life/public_child/be_same_eye/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman_and_eye(str_arg):
+    know_group(str_arg)
+    print('high_eye')
+
+def know_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman_and_eye('hand_or_other_woman')
