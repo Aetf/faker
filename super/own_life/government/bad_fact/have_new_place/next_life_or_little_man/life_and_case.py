2626e797582ffbb9832998ef30e251ff7dd37ab0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 11:19:52 -0400

    "mod: leave super/own_life/government/bad_fact/have_new_place/next_life_or_little_man/life_and_case.py up right case"

---
+++ super/own_life/government/bad_fact/have_new_place/next_life_or_little_man/life_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_year(str_arg):
+    bad_place(str_arg)
+    print('great_part')
+
+def bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_year('get_work_at_point')
