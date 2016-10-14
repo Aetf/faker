d02a154b1bd7e98987724a4ced0c7488c9e548ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-14 12:24:50 -0400

    "mix: look super/problem_or_great_government.py to first work"

---
+++ super/problem_or_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    early_work_or_point(str_arg)
+    print('leave_company_at_little_time')
+
+def early_work_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('part')
