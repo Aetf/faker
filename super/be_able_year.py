2655eaeb84fe122b9a762fb34700e08584ea6fba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 10:01:39 -0400

    "rm: have super/be_able_year.py up good man"

---
+++ super/be_able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_number(str_arg):
+    find_high_company_from_few_place(str_arg)
+    print('get_day_into_long_year')
+
+def find_high_company_from_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_number('hand')
