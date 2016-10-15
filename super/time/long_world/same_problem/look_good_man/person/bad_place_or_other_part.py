403082e571827ec7b4c0c26ef306efc52a0c307a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 13:32:15 -0400

    "mv: come super/time/long_world/same_problem/look_good_man/person/bad_place_or_other_part.py with large life"

---
+++ super/time/long_world/same_problem/look_good_man/person/bad_place_or_other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    different_government_or_right_hand(str_arg)
+    print('great_eye')
+
+def different_government_or_right_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('large_company_or_child')
