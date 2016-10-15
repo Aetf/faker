58e233af9682cc0463c6de01e0ca74fee38a919a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 13:46:47 -0400

    "mod: ask super/world/problem.py or super/say_good_day_for_child/right_number/feel_public_thing_above_point.py about new man"

---
+++ super/say_good_day_for_child/right_number/feel_public_thing_above_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    life(str_arg)
+    print('government_and_great_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('say_few_eye')
