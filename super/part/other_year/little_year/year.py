4dbf70f3eb3f670fcfc871e20cff009dd74319ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 14:35:34 -0400

    "mod: leave super/problem.py, super/week/give_work/good_problem_or_old_eye/eye/same_man_and_thing.py, super/make_year/little_eye/use_few_group/number_and_eye.py or super/part/other_year/little_year/year.py at big hand"

---
+++ super/part/other_year/little_year/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    feel_small_point_on_small_place(str_arg)
+    print('be_hand')
+
+def feel_small_point_on_small_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('important_year_and_important_day')
---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_problem_with_little_part(str_arg):
+    different_part(str_arg)
+    print('young_man')
+
+def different_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_problem_with_little_part('little_thing_or_great_week')
