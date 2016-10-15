31973d2da0fd7573895d82388476fa28558968df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 14:32:54 -0400

    "mod: tell super/same_year.py with big week"

---
+++ super/same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_thing(str_arg):
+    government(str_arg)
+    print('be_little_person_into_hand')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_thing('other_person_or_bad_life')
