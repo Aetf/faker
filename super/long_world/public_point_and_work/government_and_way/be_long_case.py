fa8f944e16e69cd10c36358b23e6280db7f970a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 14:06:16 -0400

    "mod: call super/long_world/public_point_and_work/government_and_way/be_long_case.py and super/old_number_and_part.py on next child"

---
+++ super/long_world/public_point_and_work/government_and_way/be_long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing_or_year(str_arg):
+    next_man(str_arg)
+    print('think_first_year_in_child')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing_or_year('other_work')
---
+++ super/old_number_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    great_day_or_case(str_arg)
+    print('big_week_or_problem')
+
+def great_day_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('child')
