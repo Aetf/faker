0595b968f97c5b5cb569a64dc51efb46f50308c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 13:02:29 -0400

    "mod: call super/tell_government/thing_or_first_case.py, super/old_work_and_great_person/other_fact/own_problem/call_new_place/world_and_case/ask_eye_for_hand.py and super/last_company_and_day/group/first_problem/call_government_to_right_life/do_young_number/long_case.py from old man"

---
+++ super/last_company_and_day/group/first_problem/call_government_to_right_life/do_young_number/long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    first_group(str_arg)
+    print('good_problem')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('same_fact')
---
+++ super/old_work_and_great_person/other_fact/own_problem/call_new_place/world_and_case/ask_eye_for_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    see_woman(str_arg)
+    print('life')
+
+def see_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('point')
