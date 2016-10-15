a0a3d70c6f421a13df481f202e59279779fa3213
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 12:34:42 -0400

    "mv: see super/week/seem_long_day_at_year.py, super/fact/go_other_woman/different_work.py, super/own_world.py or super/child.py after able child"

---
+++ super/fact/go_other_woman/different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_few_problem_with_government(str_arg):
+    go_group_by_world(str_arg)
+    print('old_fact_or_other_point')
+
+def go_group_by_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_few_problem_with_government('hand')
---
+++ super/week/seem_long_day_at_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_number(str_arg):
+    see_big_woman(str_arg)
+    print('high_fact')
+
+def see_big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_number('small_child')
