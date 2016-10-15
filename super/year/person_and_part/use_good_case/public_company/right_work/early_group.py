93ad54453ab9cb73cc41d934a66d986b39fc13f2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 13:00:16 -0400

    "mix: want super/year/person_and_part/use_good_case/public_company/right_work/early_group.py at bad time"

---
+++ super/year/person_and_part/use_good_case/public_company/right_work/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    great_life(str_arg)
+    print('next_week')
+
+def great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('company')
