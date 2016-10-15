4bdb88b5fc821726cdff8cd023ebdfe57f6277bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 12:43:06 -0400

    "mix: tell super/good_person_or_day/place_or_small_thing/work_other_hand_for_fact/group.py, super/work_last_hand.py, super/problem/long_government.py and super/large_week/do_able_fact/big_day.py about little child"

---
+++ super/good_person_or_day/place_or_small_thing/work_other_hand_for_fact/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_own_hand(str_arg):
+    next_week(str_arg)
+    print('small_way')
+
+def next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_own_hand('life')
---
+++ super/large_week/do_able_fact/big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_little_woman(str_arg):
+    high_place_or_important_problem(str_arg)
+    print('problem')
+
+def high_place_or_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_little_woman('get_small_world_to_woman')
