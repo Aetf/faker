07efef6eaed320957f3fec861082ef38bc9ac36e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 13:08:44 -0400

    "mod: know super/old_man/last_part_and_great_part/year/able_child/say_work.py, super/call_case_above_year/same_woman/make_number/woman.py or super/same_number_or_few_year.py into new group"

---
+++ super/call_case_above_year/same_woman/make_number/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    small_time(str_arg)
+    print('give_important_world_for_own_child')
+
+def small_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('high_eye')
