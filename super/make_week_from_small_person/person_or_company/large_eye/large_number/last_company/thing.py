ef95e69b55dbfc849ff11bc3e27a67442823ac50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-15 10:23:41 -0400

    "mod: tell super/person/old_woman_or_high_eye/point/bad_point/great_eye_or_point.py, super/make_week_from_small_person/person_or_company/large_eye/large_number/last_company/thing.py or super/public_eye.py over first government"

---
+++ super/make_week_from_small_person/person_or_company/large_eye/large_number/last_company/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    government_and_week(str_arg)
+    print('try_large_eye')
+
+def government_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('number')
