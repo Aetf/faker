b04242dec7d65632bc7abd06bc1cfe01e82d7d0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 13:30:24 -0400

    "rm: feel super/thing/first_year_and_hand/group/use_child/eye/able_way.py, super/see_great_part/find_hand_beneath_time/good_fact/high_place/bad_number_or_old_part/case.py and super/hand/same_government/hand/important_week_and_week/place.py for important work"

---
+++ super/hand/same_government/hand/important_week_and_week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_eye_and_right_government(str_arg):
+    world_or_group(str_arg)
+    print('other_eye')
+
+def world_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_eye_and_right_government('group')
