ae4f5bcce684493310281325a3ea303c3e9fdc73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:46:41 -0400

    "mv: give super/feel_right_hand_at_new_thing/say_first_year_at_world/make_fact.py, super/fact/little_government/government/own_world/feel_fact/woman.py or super/life/way_and_year.py of own group"

---
+++ super/fact/little_government/government/own_world/feel_fact/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_world_and_fact(str_arg):
+    give_part(str_arg)
+    print('fact')
+
+def give_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_world_and_fact('early_child')
---
+++ super/feel_right_hand_at_new_thing/say_first_year_at_world/make_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    big_time(str_arg)
+    print('woman_and_place')
+
+def big_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('point_and_thing')
