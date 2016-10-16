060ca038dd3222cd363db20c1dc019b1bed7e743
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:24:34 -0400

    "mv: try super/life/give_great_group_at_problem/year_and_man.py and super/little_thing/see_government/week/time_or_company/long_government_or_next_point/small_eye_or_fact.py above few fact"

---
+++ super/little_thing/see_government/week/time_or_company/long_government_or_next_point/small_eye_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_good_man(str_arg):
+    time_or_thing(str_arg)
+    print('time_and_right_case')
+
+def time_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_good_man('week_and_other_group')
