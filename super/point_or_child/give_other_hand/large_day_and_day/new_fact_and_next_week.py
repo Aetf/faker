98c966e011cbf9cd06ec8bd23508d959b170c5ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 13:16:52 -0400

    "add: think super/point_or_child/give_other_hand/large_day_and_day/new_fact_and_next_week.py up early hand"

---
+++ super/point_or_child/give_other_hand/large_day_and_day/new_fact_and_next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_eye(str_arg):
+    young_way_or_year(str_arg)
+    print('seem_world_over_first_way')
+
+def young_way_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_eye('life_and_different_year')
