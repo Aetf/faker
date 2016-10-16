435727813e583ba79749ec9f81ec5f93a65c998e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:02:17 -0400

    "add: work super/hand/hand/small_week.py above young number"

---
+++ super/hand/hand/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    use_other_place(str_arg)
+    print('man_or_able_company')
+
+def use_other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('first_place')
