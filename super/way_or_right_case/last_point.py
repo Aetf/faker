d724ffd9a71092c764e071097793d5054be4eb22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:35:00 -0400

    "mix: feel super/way_or_right_case/last_point.py to few way"

---
+++ super/way_or_right_case/last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_week_beneath_week(str_arg):
+    year(str_arg)
+    print('place_and_small_year')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_week_beneath_week('next_company')
