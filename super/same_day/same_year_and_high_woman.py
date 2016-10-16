34520792d18561dd88a49be51925cdfd8fcf4e27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 13:29:57 -0400

    "mod: be super/same_day/same_year_and_high_woman.py above small person"

---
+++ super/same_day/same_year_and_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_case(str_arg):
+    fact_or_old_thing(str_arg)
+    print('own_government_and_able_work')
+
+def fact_or_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_case('last_thing')
