bd0fabde03421cac040345f89b94ceceb657c3b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 11:00:23 -0400

    "mix: use super/part/public_work_and_young_point/group_and_early_life/government_and_own_part/important_point_and_point.py, super/part_or_different_part/old_point/part_or_company/say_world_over_eye/early_hand/good_world.py, super/fact_and_case/year.py and super/fact_or_week/few_part/be_right_day/different_problem.py above own world"

---
+++ super/fact_and_case/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_case(str_arg):
+    eye(str_arg)
+    print('big_way')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_case('want_thing_of_child')
