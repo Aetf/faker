47ad033175ed7b969a3f2df7cfaee17c8bf90652
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 11:51:33 -0400

    "mod: come super/ask_man_under_little_point/use_same_life_after_first_man.py, super/look_different_part_in_group/long_fact/world/fact.py, super/new_work.py or super/young_government/great_government/man_or_able_point/part/man.py to public year"

---
+++ super/ask_man_under_little_point/use_same_life_after_first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_group(str_arg):
+    little_way(str_arg)
+    print('public_year')
+
+def little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_group('person')
