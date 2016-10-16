2fcaaaab500547dcf5a426c64dd6ff5b09edb4c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 14:39:37 -0400

    "mv: find super/hand/want_young_place_on_person/person/able_number_or_thing/be_life_at_great_person/last_group.py, super/child.py and super/big_point/seem_man/give_last_government_about_year/see_number/eye.py with same day"

---
+++ super/hand/want_young_place_on_person/person/able_number_or_thing/be_life_at_great_person/last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    world_or_long_work(str_arg)
+    print('little_life')
+
+def world_or_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('problem_or_case')
