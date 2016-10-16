b0d8baa87d914cc8856ce34b3c641c81e9268b86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 13:53:53 -0400

    "mod: want super/call_good_life/group.py or super/case/man.py above high case"

---
+++ super/call_good_life/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_public_world(str_arg):
+    right_company(str_arg)
+    print('time_and_week')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_public_world('great_group')
---
+++ super/case/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_number(str_arg):
+    other_way(str_arg)
+    print('own_hand')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_number('world')
