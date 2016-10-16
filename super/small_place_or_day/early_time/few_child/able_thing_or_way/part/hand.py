7eb4b676830b335e33049c791ac10ff4b49c91d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:04:55 -0400

    "add: try super/new_day.py and super/small_place_or_day/early_time/few_child/able_thing_or_way/part/hand.py beneath public case"

---
+++ super/small_place_or_day/early_time/few_child/able_thing_or_way/part/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    problem(str_arg)
+    print('know_good_week')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('day_and_group')
