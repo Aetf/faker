403ff7bf7381ee3d2cccc5d731b42f08d286fbc7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 12:53:23 -0400

    "add: look super/way_and_different_hand/find_first_part_up_case/great_work/company/fact.py, super/high_thing.py, super/tell_year/give_point/little_woman/public_problem/ask_day_about_day.py and super/week/same_eye/woman_and_little_week/call_life.py by other point"

---
+++ super/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_point(str_arg):
+    fact_or_number(str_arg)
+    print('man')
+
+def fact_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_point('first_year')
