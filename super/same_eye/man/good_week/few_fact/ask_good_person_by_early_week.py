849ac187b6d659430421e471d7628cc64941ea70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 10:59:34 -0400

    "add: do super/same_eye/man/good_week/few_fact/ask_good_person_by_early_week.py from own man"

---
+++ super/same_eye/man/good_week/few_fact/ask_good_person_by_early_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_way(str_arg):
+    know_number(str_arg)
+    print('big_group_and_right_point')
+
+def know_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_way('leave_high_child')
