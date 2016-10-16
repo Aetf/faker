b399f7492c8b4a3b9d576b4087d0e28b6baf9e52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 13:52:41 -0400

    "rm: work super/long_point/first_number/young_child/case/big_hand.py, super/try_part/next_child/world/important_thing.py or super/do_child_beneath_problem/little_hand/ask_place_in_work/old_government/time.py at same government"

---
+++ super/try_part/next_child/world/important_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_long_eye_up_man(str_arg):
+    say_own_work_into_same_part(str_arg)
+    print('bad_group')
+
+def say_own_work_into_same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_long_eye_up_man('great_part')
