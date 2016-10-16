891635990a70b16d40a938a029ca0ce29a475835
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-16 14:12:50 -0400

    "rm: see super/hand/do_group_at_problem.py by large eye"

---
+++ super/hand/do_group_at_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    good_way(str_arg)
+    print('first_fact')
+
+def good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('great_life')
