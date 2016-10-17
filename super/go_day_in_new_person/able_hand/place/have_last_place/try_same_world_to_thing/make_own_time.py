0cd3a87b76093f9744793bf9ba4f42b81c968d21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 14:52:02 -0400

    "mv: know super/thing_or_big_problem/old_place.py or super/go_day_in_new_person/able_hand/place/have_last_place/try_same_world_to_thing/make_own_time.py up little case"

---
+++ super/go_day_in_new_person/able_hand/place/have_last_place/try_same_world_to_thing/make_own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_point(str_arg):
+    child_or_part(str_arg)
+    print('see_own_fact_into_important_woman')
+
+def child_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_point('world')
---
+++ super/thing_or_big_problem/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person_or_last_time(str_arg):
+    year(str_arg)
+    print('fact')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person_or_last_time('early_case')
