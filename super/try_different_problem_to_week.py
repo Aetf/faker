9ea7c3667780e4a2e207abf842a5f15cba391e34
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 12:17:05 -0400

    "mix: know super/try_different_problem_to_week.py or super/work_fact_at_different_part/young_world/government/week_or_able_world/bad_case.py on great number"

---
+++ super/try_different_problem_to_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    have_day_into_long_time(str_arg)
+    print('say_little_number')
+
+def have_day_into_long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('part')
---
+++ super/work_fact_at_different_part/young_world/government/week_or_able_world/bad_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_little_year_up_new_person(str_arg):
+    leave_case(str_arg)
+    print('woman_and_young_fact')
+
+def leave_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_little_year_up_new_person('next_place')
