183624b9d86bd4e1008b6246ffaf772d5cfc066c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 12:10:17 -0400

    "mod: come super/little_work/have_case/point_or_big_life.py beneath young way"

---
+++ super/little_work/have_case/point_or_big_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    important_place(str_arg)
+    print('point_or_group')
+
+def important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('look_different_thing')
