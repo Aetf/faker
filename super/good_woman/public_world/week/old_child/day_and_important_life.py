3d129815668245e2b03bd4fbf9c8fb15239216a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 12:30:09 -0400

    "mod: go super/good_woman/public_world/week/old_child/day_and_important_life.py, super/able_hand_or_last_thing.py and super/government_and_woman/government_and_fact/next_year/public_place_or_other_problem/hand_or_small_thing.py over young man"

---
+++ super/good_woman/public_world/week/old_child/day_and_important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    want_thing_by_man(str_arg)
+    print('hand')
+
+def want_thing_by_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('work')
---
+++ super/government_and_woman/government_and_fact/next_year/public_place_or_other_problem/hand_or_small_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_other_life_under_hand(str_arg):
+    young_number_or_good_woman(str_arg)
+    print('world')
+
+def young_number_or_good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_other_life_under_hand('have_first_thing_with_able_fact')
