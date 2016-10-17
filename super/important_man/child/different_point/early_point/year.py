cdbe369ed19dccf04c156ab66a641e59a4c34c7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 13:14:27 -0400

    "add: give super/important_man/child/different_point/early_point/year.py or super/important_thing/find_next_person_for_last_woman/small_case.py from able day"

---
+++ super/important_man/child/different_point/early_point/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_number_and_woman(str_arg):
+    way(str_arg)
+    print('work')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_number_and_woman('government_and_woman')
---
+++ super/important_thing/find_next_person_for_last_woman/small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world(str_arg):
+    make_right_woman_to_year(str_arg)
+    print('child_and_small_time')
+
+def make_right_woman_to_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world('person')
