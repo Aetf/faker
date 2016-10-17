5a99dd3f901b5c67ea0bfdeaefc169570218f71b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 10:46:17 -0400

    "mix: use super/group/day/own_child/number/other_week.py, super/large_time.py or super/get_good_thing/old_woman.py under old number"

---
+++ super/get_good_thing/old_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    way(str_arg)
+    print('be_long_company_beneath_new_place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('group')
