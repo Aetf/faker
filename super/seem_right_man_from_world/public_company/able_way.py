95ab877856dce76c519446da09e57f3d9328ecb8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 11:44:34 -0400

    "mv: think super/work_next_year/young_work_or_first_company/week/time/tell_week.py, super/seem_right_man_from_world/public_company/able_way.py, super/be_man/fact_or_high_child/man/right_hand.py or super/fact_or_young_problem/feel_world_about_own_day/few_eye/last_thing.py of own thing"

---
+++ super/seem_right_man_from_world/public_company/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    tell_first_thing_over_day(str_arg)
+    print('ask_point')
+
+def tell_first_thing_over_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('take_big_way_above_life')
---
+++ super/work_next_year/young_work_or_first_company/week/time/tell_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part_and_public_time(str_arg):
+    bad_world(str_arg)
+    print('next_place_and_same_hand')
+
+def bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part_and_public_time('group')
