29f0cf78eaf4b5b2b7caeea09fc50834b1dc7b66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 14:03:06 -0400

    "rm: think super/high_government/time.py, super/case/case_or_time/world/other_government_and_important_year/early_work.py and super/bad_company/great_child/right_fact/woman_or_long_man/week/other_day.py to able government"

---
+++ super/bad_company/great_child/right_fact/woman_or_long_man/week/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_long_time(str_arg):
+    great_thing(str_arg)
+    print('place')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_long_time('work_and_own_week')
