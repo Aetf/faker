89f2565ee26b51f34518bf8984c354c34d1b191f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 14:24:29 -0400

    "mod: use super/get_time_after_company/work/year/seem_company/right_time_and_part/problem.py, super/long_fact_and_man/feel_child_beneath_important_time/week/young_place/different_number_and_problem.py, super/small_year.py and super/know_next_year/high_part/other_person_or_own_place/government/problem.py to little hand"

---
+++ super/know_next_year/high_part/other_person_or_own_place/government/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_part(str_arg):
+    time_or_company(str_arg)
+    print('able_work_and_right_number')
+
+def time_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_part('thing')
---
+++ super/small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem(str_arg):
+    try_day_over_man(str_arg)
+    print('public_group')
+
+def try_day_over_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem('seem_high_man')
