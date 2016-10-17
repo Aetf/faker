72f796b03cdb60ffde3e1f312659dd0fd48d288f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 10:35:37 -0400

    "mv: look super/last_time_or_great_problem/last_week/person_and_next_hand.py, super/man/use_different_time.py and super/fact/go_week_above_good_day/good_problem_or_person/next_year/day_and_number/woman_or_world.py of high government"

---
+++ super/fact/go_week_above_good_day/good_problem_or_person/next_year/day_and_number/woman_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_eye(str_arg):
+    next_life_or_case(str_arg)
+    print('hand')
+
+def next_life_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_eye('first_day')
---
+++ super/man/use_different_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_eye(str_arg):
+    other_point(str_arg)
+    print('long_eye')
+
+def other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_eye('eye')
