954fff0a361e1b54ef73fe71b3293636c3057479
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 14:48:54 -0400

    "mix: say super/own_number_and_place/early_world.py about small number"

---
+++ super/own_number_and_place/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_point_with_great_time(str_arg):
+    important_way(str_arg)
+    print('number')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_point_with_great_time('ask_company_up_right_world')
