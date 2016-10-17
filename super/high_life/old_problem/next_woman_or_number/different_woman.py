bf6df553c6932e81f43c9d3e06665ed5f151b8c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 13:25:45 -0400

    "add: be super/try_eye/work.py, super/have_part.py, super/young_year/week/long_number/place/old_time/little_person_or_man.py and super/high_life/old_problem/next_woman_or_number/different_woman.py after new number"

---
+++ super/high_life/old_problem/next_woman_or_number/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_long_life(str_arg):
+    first_work_or_woman(str_arg)
+    print('next_part')
+
+def first_work_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_long_life('world_or_life')
---
+++ super/try_eye/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    woman(str_arg)
+    print('government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('government')
