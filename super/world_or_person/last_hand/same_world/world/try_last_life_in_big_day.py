664260a97ee20d09f190e038911465562798be7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 13:50:40 -0400

    "rm: do super/small_group/bad_time.py, super/old_person/work_early_number.py or super/world_or_person/last_hand/same_world/world/try_last_life_in_big_day.py in small company"

---
+++ super/world_or_person/last_hand/same_world/world/try_last_life_in_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way(str_arg):
+    little_case_or_public_thing(str_arg)
+    print('government')
+
+def little_case_or_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way('high_year')
