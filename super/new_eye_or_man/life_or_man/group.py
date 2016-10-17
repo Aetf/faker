92a91eee58576c07f39cf7772070325e537b2e49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 10:22:00 -0400

    "mod: feel super/great_case/work_first_man_about_good_year.py, super/own_case_and_own_part/last_problem/case/want_time_under_person.py, super/new_eye_or_man/life_or_man/group.py and super/want_time/part/able_person/different_man_and_early_woman/seem_new_life.py above other government"

---
+++ super/new_eye_or_man/life_or_man/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    eye(str_arg)
+    print('world')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('get_young_week')
---
+++ super/own_case_and_own_part/last_problem/case/want_time_under_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    want_great_case_beneath_company(str_arg)
+    print('large_time')
+
+def want_great_case_beneath_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('part')
