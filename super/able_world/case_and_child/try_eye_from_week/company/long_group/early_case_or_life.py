a32e26f93912c39bc5cbe8fa99aecb8ca7346284
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-17 13:26:41 -0400

    "rm: go super/able_world/case_and_child/try_eye_from_week/company/long_group/early_case_or_life.py up early place"

---
+++ super/able_world/case_and_child/try_eye_from_week/company/long_group/early_case_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    feel_other_life(str_arg)
+    print('hand')
+
+def feel_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('right_man_or_important_day')
