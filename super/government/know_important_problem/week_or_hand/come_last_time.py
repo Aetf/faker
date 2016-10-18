8ddff170032cdc938f2ca1c86adf9ecb159866e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 13:10:17 -0400

    "add: feel super/use_able_man/ask_way/bad_year_and_great_point.py, super/long_child_and_good_week/way/work_great_group/give_good_problem_with_problem.py, super/government/know_important_problem/week_or_hand/come_last_time.py or super/little_point_and_point/different_point/time/long_point/leave_long_case.py to small person"

---
+++ super/government/know_important_problem/week_or_hand/come_last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_new_year(str_arg):
+    week(str_arg)
+    print('person_or_great_thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_new_year('public_point')
---
+++ super/use_able_man/ask_way/bad_year_and_great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_group(str_arg):
+    small_way(str_arg)
+    print('case')
+
+def small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_group('public_week')
