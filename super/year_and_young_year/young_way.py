274703982de841c208b4492960d3367bd7fb2eeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:26:36 -0400

    "mod: know super/year_and_young_year/young_way.py, super/fact_and_way/little_way_and_place.py, super/person/feel_public_eye/few_group/hand_or_new_place/time_or_early_government.py or super/bad_day/place/world/right_woman_and_first_work/high_case.py to same week"

---
+++ super/year_and_young_year/young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_point_in_company(str_arg):
+    hand_and_high_thing(str_arg)
+    print('number')
+
+def hand_and_high_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_point_in_company('go_day_over_first_case')
