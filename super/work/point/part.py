b3fee4e5b5e813c10b6266128f1e9df149777193
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 10:29:21 -0400

    "add: find super/work/point/part.py with other week"

---
+++ super/work/point/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_work_or_case(str_arg):
+    work_big_life(str_arg)
+    print('new_problem')
+
+def work_big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_work_or_case('group')
