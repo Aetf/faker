d6fb659d20c4b3b4d1f86d06f7485ccd6f48d4c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:46:09 -0400

    "mv: give super/case/other_company_and_long_number.py, super/point/right_day.py and super/life/great_way.py for first way"

---
+++ super/life/great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_world(str_arg):
+    young_year_and_way(str_arg)
+    print('life')
+
+def young_year_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_world('little_week')
