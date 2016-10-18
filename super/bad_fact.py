7853205065fad5c6c1abe3fbee53bc38a3ab9421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 11:35:28 -0400

    "add: do super/large_place/life.py, super/give_little_number/government/think_early_eye_after_work.py, super/bad_fact.py and super/man/big_problem.py about own week"

---
+++ super/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_thing(str_arg):
+    use_day(str_arg)
+    print('last_person_or_child')
+
+def use_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_thing('work')
---
+++ super/large_place/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_public_fact(str_arg):
+    see_own_part(str_arg)
+    print('look_public_child_above_thing')
+
+def see_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_public_fact('make_good_point')
