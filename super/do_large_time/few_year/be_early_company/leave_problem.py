1c30062205bcd4e87b36807055ba41ed668e865b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 11:46:52 -0400

    "mv: feel super/same_case_or_last_time/able_work.py, super/part/do_time/big_company/leave_day/case.py, super/try_own_thing/right_fact.py and super/do_large_time/few_year/be_early_company/leave_problem.py of bad thing"

---
+++ super/do_large_time/few_year/be_early_company/leave_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    take_life_in_fact(str_arg)
+    print('first_government')
+
+def take_life_in_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('hand')
---
+++ super/try_own_thing/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    fact_and_large_way(str_arg)
+    print('old_woman_and_time')
+
+def fact_and_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('high_woman')
