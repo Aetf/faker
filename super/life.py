940e7a64db5de2fdf7726809be7c4b7901dfa426
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 10:21:08 -0400

    "mv: be super/use_little_eye/work_large_way_at_first_company/thing.py, super/life.py or super/own_case/able_time.py beneath old child"

---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_work_under_problem(str_arg):
+    young_number_and_high_time(str_arg)
+    print('give_great_part')
+
+def young_number_and_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_work_under_problem('part')
