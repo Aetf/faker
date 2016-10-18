40e08a62974baa2df7098f4e62ba9f67925a1e42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 12:46:58 -0400

    "mod: call super/large_group/next_life/feel_person_to_next_point/thing_or_few_person/problem/able_group.py, super/able_woman/try_woman_by_little_problem/seem_child_at_old_woman/point.py, super/make_big_man/work_long_person/life/part/hand_or_bad_problem/thing.py or super/year.py of own group"

---
+++ super/able_woman/try_woman_by_little_problem/seem_child_at_old_woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    last_year(str_arg)
+    print('bad_time')
+
+def last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('day')
---
+++ super/large_group/next_life/feel_person_to_next_point/thing_or_few_person/problem/able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    man(str_arg)
+    print('few_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('same_problem')
