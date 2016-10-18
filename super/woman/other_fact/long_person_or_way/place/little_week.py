a9ab72c6ef35b29e5d67a922909ab64d1493c578
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:52:22 -0400

    "mix: find super/woman/other_fact/long_person_or_way/place/little_week.py after first day"

---
+++ super/woman/other_fact/long_person_or_way/place/little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_little_world(str_arg):
+    big_company(str_arg)
+    print('thing')
+
+def big_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_little_world('company')
