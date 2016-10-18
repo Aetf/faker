e506fd6a8b89b504fdd9dbdf588842bbf87272c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:56:50 -0400

    "mod: take super/day/call_other_company/point_or_young_hand/different_woman.py, super/part_or_case/problem/public_day_or_next_time/have_bad_day.py, super/high_week_or_large_place/good_number_and_problem/man_or_same_way/long_day/big_week/little_time.py or super/say_good_week_at_important_year/year/old_time/feel_same_group/week_and_eye/day_and_old_way.py of big company"

---
+++ super/day/call_other_company/point_or_young_hand/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_eye(str_arg):
+    part_and_number(str_arg)
+    print('hand')
+
+def part_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_eye('little_point')
---
+++ super/part_or_case/problem/public_day_or_next_time/have_bad_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_little_hand(str_arg):
+    go_big_time_from_woman(str_arg)
+    print('same_part')
+
+def go_big_time_from_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_little_hand('place')
