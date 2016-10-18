8063003d47a59a44ec25f9bdc1c572945dcb083d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 13:49:14 -0400

    "add: ask super/good_person_or_year/fact_and_world/number/good_part.py, super/different_thing_and_long_part/important_week/same_year/good_number/have_week.py and super/first_child.py above different world"

---
+++ super/first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    other_place_and_time(str_arg)
+    print('public_group_or_case')
+
+def other_place_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('next_child_and_company')
---
+++ super/good_person_or_year/fact_and_world/number/good_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_old_person(str_arg):
+    way(str_arg)
+    print('first_week')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_old_person('child')
