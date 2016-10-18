c47b0fec8735ef4609cd306e2b3f33abffc989c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:41:12 -0400

    "add: give super/large_day_or_large_number.py into able point"

---
+++ super/large_day_or_large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_woman(str_arg):
+    thing(str_arg)
+    print('high_company')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_woman('own_eye')
