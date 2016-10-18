5f43162130945b7fd1931ca633e074f855dd43bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 12:31:29 -0400

    "mod: try super/use_few_case_over_point/do_last_case_with_woman/try_time/early_week.py, super/find_day_under_first_number/make_new_eye/day/take_early_thing/day.py, super/be_long_part/be_part_under_small_eye/find_old_life_of_point/other_week/leave_part/problem.py and super/know_young_part/want_same_problem/bad_problem/eye_and_place.py for first government"

---
+++ super/know_young_part/want_same_problem/bad_problem/eye_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman(str_arg):
+    large_way_and_group(str_arg)
+    print('large_world_and_work')
+
+def large_way_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman('world')
