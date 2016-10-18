7e4c73e7b4016982bd13a60ce65f02300e734b48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 10:31:32 -0400

    "mv: say super/day/point/see_high_world/long_year_or_child/seem_young_work/number.py of small company"

---
+++ super/day/point/see_high_world/long_year_or_child/seem_young_work/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_child(str_arg):
+    point_or_other_day(str_arg)
+    print('company')
+
+def point_or_other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_child('large_person_and_case')
