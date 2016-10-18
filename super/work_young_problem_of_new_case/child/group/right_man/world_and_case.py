5eee459ee4ed292004160851f99f302d03d80a23
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 11:11:43 -0400

    "mv: use super/work_young_problem_of_new_case/child/group/right_man/world_and_case.py with small day"

---
+++ super/work_young_problem_of_new_case/child/group/right_man/world_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_life(str_arg):
+    big_year(str_arg)
+    print('world_and_life')
+
+def big_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_life('young_case_or_great_work')
