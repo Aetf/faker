0ac8602c3a86a1eb1608df07b8694d5d4e980d78
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 14:41:29 -0400

    "rm: find super/give_person/own_fact_and_part.py, super/part/life.py or super/small_man/good_life_and_right_man.py over little case"

---
+++ super/give_person/own_fact_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_day_at_few_thing(str_arg):
+    fact_and_point(str_arg)
+    print('important_child')
+
+def fact_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_day_at_few_thing('last_day_or_work')
