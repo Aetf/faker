d755e1888bf1ae27eb12d96a2752dc2f8f3105da
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 13:06:48 -0400

    "add: say super/person_or_government/case_and_point/next_work/fact.py, super/large_part_or_long_part/man/man/public_company/own_person_and_young_work/work_or_right_fact.py or super/few_week.py beneath right point"

---
+++ super/large_part_or_long_part/man/man/public_company/own_person_and_young_work/work_or_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_time(str_arg):
+    number_or_own_part(str_arg)
+    print('first_life')
+
+def number_or_own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_time('great_government_and_own_fact')
