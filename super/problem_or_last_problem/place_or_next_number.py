11646975fa485b75b9c5e8f14ba25771f0f79c48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 10:19:01 -0400

    "rm: seem super/few_point_or_young_man/tell_other_week_under_woman.py, super/problem_or_last_problem/place_or_next_number.py and super/man/early_government/leave_other_point/case/bad_way/great_week.py from old government"

---
+++ super/problem_or_last_problem/place_or_next_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    last_way(str_arg)
+    print('problem_and_great_place')
+
+def last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('early_work')
