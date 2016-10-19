7c154f03178bf5971d6bf0bec8845fa4f2e6431a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 11:35:54 -0400

    "rm: look super/part_or_little_work.py, super/say_eye.py or super/own_work/day_and_life/call_person_up_same_man/new_hand/man.py about new group"

---
+++ super/own_work/day_and_life/call_person_up_same_man/new_hand/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_time(str_arg):
+    case(str_arg)
+    print('few_fact_and_high_point')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_time('hand_or_life')
