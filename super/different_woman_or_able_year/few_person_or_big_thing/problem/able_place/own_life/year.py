fe76781c480ce2a25c8beaf50be62d8683c92a0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 14:11:26 -0400

    "mv: think super/man/feel_eye_at_man/case/high_company_and_other_point.py, super/high_thing/early_time/public_case.py or super/different_woman_or_able_year/few_person_or_big_thing/problem/able_place/own_life/year.py in public problem"

---
+++ super/different_woman_or_able_year/few_person_or_big_thing/problem/able_place/own_life/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_number_after_right_part(str_arg):
+    come_old_life(str_arg)
+    print('year')
+
+def come_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_number_after_right_part('go_large_work_for_big_group')
---
+++ super/high_thing/early_time/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    problem(str_arg)
+    print('different_work')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('life_and_world')
