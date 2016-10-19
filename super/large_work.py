2135e34f0e6ea0efaafb0bbea337df79679137be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 13:34:45 -0400

    "mix: take super/little_problem/company/know_time_above_next_fact.py, super/large_work.py, super/week/year/day.py and super/look_number_at_problem/child/group/be_public_world_over_bad_fact.py after long work"

---
+++ super/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    point(str_arg)
+    print('young_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('few_way_or_old_work')
---
+++ super/look_number_at_problem/child/group/be_public_world_over_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_problem(str_arg):
+    right_company(str_arg)
+    print('able_fact')
+
+def right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_problem('year_or_week')
