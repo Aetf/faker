d2dcced0af85987f3bbde1e77b145f75db4eb041
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 14:24:47 -0400

    "add: feel super/tell_place/hand_or_time/large_point.py up large government"

---
+++ super/tell_place/hand_or_time/large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_company(str_arg):
+    old_day_or_own_case(str_arg)
+    print('work')
+
+def old_day_or_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_company('know_thing_from_place')
