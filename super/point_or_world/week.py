ca3972834b3c0642d26edfd7d886eb3f2ad56284
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 12:02:48 -0400

    "mv: ask super/point_or_world/week.py about different life"

---
+++ super/point_or_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_government(str_arg):
+    big_eye(str_arg)
+    print('eye')
+
+def big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_government('first_life_or_number')
