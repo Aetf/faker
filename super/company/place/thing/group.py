3ac342508a5c6a98167e5386f9b5525e36ca7e74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 10:21:55 -0400

    "mv: seem super/company/place/thing/group.py into right number"

---
+++ super/company/place/thing/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_small_person(str_arg):
+    life_and_large_hand(str_arg)
+    print('case_and_next_company')
+
+def life_and_large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_small_person('woman')
