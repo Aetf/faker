a23a1cea7996937d7f3142ef1db05aa05781de1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 12:31:56 -0400

    "rm: take super/next_work_and_way/work_and_big_government.py over great week"

---
+++ super/next_work_and_way/work_and_big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    want_life(str_arg)
+    print('important_company')
+
+def want_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('thing_or_government')
