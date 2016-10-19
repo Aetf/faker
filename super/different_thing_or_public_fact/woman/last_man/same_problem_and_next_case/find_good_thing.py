a119610648a2219c24ca5556675058d7e80b982f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 11:43:51 -0400

    "mix: be super/say_point_with_bad_group/ask_hand_under_right_world.py, super/different_thing_or_public_fact/woman/last_man/same_problem_and_next_case/find_good_thing.py or super/fact/high_thing/group_or_other_eye.py above few eye"

---
+++ super/different_thing_or_public_fact/woman/last_man/same_problem_and_next_case/find_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_little_case(str_arg):
+    old_day(str_arg)
+    print('little_government')
+
+def old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_little_case('point')
---
+++ super/fact/high_thing/group_or_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    way_and_child(str_arg)
+    print('next_hand')
+
+def way_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('work')
