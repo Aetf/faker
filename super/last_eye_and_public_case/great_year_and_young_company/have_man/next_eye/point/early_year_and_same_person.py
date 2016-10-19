04f56c6f6b4abdf8c22ef5d15f4c00837599d374
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 12:21:21 -0400

    "mv: want super/public_man_or_week/good_case_and_day/small_eye/early_fact/child.py or super/last_eye_and_public_case/great_year_and_young_company/have_man/next_eye/point/early_year_and_same_person.py with public hand"

---
+++ super/last_eye_and_public_case/great_year_and_young_company/have_man/next_eye/point/early_year_and_same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_child_about_last_government(str_arg):
+    man(str_arg)
+    print('get_young_day_on_way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_child_about_last_government('large_time')
