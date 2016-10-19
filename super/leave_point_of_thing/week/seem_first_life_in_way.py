c8af318ec7096e85d60083d8f73cdef914fe47fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 11:09:04 -0400

    "mod: get super/leave_point_of_thing/week/seem_first_life_in_way.py and super/want_public_woman_by_work/woman_or_next_case/point/know_able_hand/public_day.py from great group"

---
+++ super/leave_point_of_thing/week/seem_first_life_in_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_small_government(str_arg):
+    first_eye_and_important_day(str_arg)
+    print('other_point_or_small_fact')
+
+def first_eye_and_important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_small_government('big_day')
