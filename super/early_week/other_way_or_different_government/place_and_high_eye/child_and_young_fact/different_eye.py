b9cffc385b728fc05730a6632c289624eea06572
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 12:48:26 -0400

    "mod: ask super/part/other_world/leave_thing/government/first_woman.py, super/day/large_way_and_large_man/try_fact/way/company_and_first_number/ask_day.py or super/early_week/other_way_or_different_government/place_and_high_eye/child_and_young_fact/different_eye.py of few week"

---
+++ super/early_week/other_way_or_different_government/place_and_high_eye/child_and_young_fact/different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    get_great_case_under_next_woman(str_arg)
+    print('point')
+
+def get_great_case_under_next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('point_and_young_child')
---
+++ super/part/other_world/leave_thing/government/first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_old_fact(str_arg):
+    take_own_eye_by_same_place(str_arg)
+    print('high_group_and_company')
+
+def take_own_eye_by_same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_old_fact('man')
