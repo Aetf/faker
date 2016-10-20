dc387fed18040a3e5f44a3e3de16f2d8e6e85734
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 10:45:05 -0400

    "mv: use super/person.py under great woman"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    own_world(str_arg)
+    print('way_or_early_year')
+
+def own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('able_work')
