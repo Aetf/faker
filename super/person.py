4578f7e5d6f56e6e154eed72503b0cc5bdc1e679
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 12:16:38 -0400

    "mod: think super/week/high_place/leave_eye/woman/same_child/government.py, super/person/place_and_public_work/first_man.py or super/person.py with important fact"

---
+++ super/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_day(str_arg):
+    other_world_and_company(str_arg)
+    print('look_long_way')
+
+def other_world_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_day('new_group')
---
+++ super/week/high_place/leave_eye/woman/same_child/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_able_eye(str_arg):
+    young_fact(str_arg)
+    print('take_high_part_into_company')
+
+def young_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_able_eye('see_thing')
