3ffd413d5b312d64b72d6694ba9b1104a5dcfb8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 11:18:26 -0400

    "mod: use super/large_woman_and_part/able_woman.py into different week"

---
+++ super/large_woman_and_part/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_thing(str_arg):
+    call_number(str_arg)
+    print('thing_or_long_place')
+
+def call_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_thing('other_hand_and_large_place')
