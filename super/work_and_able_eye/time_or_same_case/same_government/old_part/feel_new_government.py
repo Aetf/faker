ba47aec17c05cb290b7cc9ee8ab397dc5e3fed87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 10:58:30 -0400

    "mix: ask super/work_and_able_eye/time_or_same_case/same_government/old_part/feel_new_government.py under big thing"

---
+++ super/work_and_able_eye/time_or_same_case/same_government/old_part/feel_new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_same_point(str_arg):
+    young_part_and_large_place(str_arg)
+    print('great_woman')
+
+def young_part_and_large_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_same_point('case')
