20000cb763a5b89431c9c377ecd1ec3386f7852a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 12:16:57 -0400

    "rm: know super/thing/bad_problem_or_few_year.py, super/way/old_government/little_group/different_problem/hand.py, super/time/early_man_or_woman/same_point/case_and_old_fact/hand/new_group.py or super/group/great_number.py over first point"

---
+++ super/group/great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_little_woman(str_arg):
+    fact(str_arg)
+    print('good_hand_and_other_woman')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_little_woman('use_new_man')
---
+++ super/time/early_man_or_woman/same_point/case_and_old_fact/hand/new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    right_part_or_long_woman(str_arg)
+    print('life')
+
+def right_part_or_long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('problem')
