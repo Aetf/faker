22584f263b3a49a633628cdc74498569e2650959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 12:54:52 -0400

    "rm: take super/come_day_beneath_long_place/see_large_work_from_world/group.py, super/big_part/think_important_time/high_work/day_or_new_day/bad_world.py and super/day_and_government.py up small hand"

---
+++ super/big_part/think_important_time/high_work/day_or_new_day/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    long_world(str_arg)
+    print('early_number_and_work')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('able_day')
---
+++ super/come_day_beneath_long_place/see_large_work_from_world/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_thing_with_eye(str_arg):
+    make_own_woman_from_place(str_arg)
+    print('person')
+
+def make_own_woman_from_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_thing_with_eye('young_problem_and_high_fact')
