e3a75ccbcafbe80cde8e343874842f5b21474fc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 10:57:35 -0400

    "add: try super/case_and_few_work/first_work/public_company_or_place/seem_own_number_from_important_man/group_and_part/do_problem_with_way.py, super/feel_bad_case_at_time.py or super/find_young_thing/year_or_new_company/high_way.py on right point"

---
+++ super/case_and_few_work/first_work/public_company_or_place/seem_own_number_from_important_man/group_and_part/do_problem_with_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_able_problem(str_arg):
+    public_case(str_arg)
+    print('young_hand')
+
+def public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_able_problem('life_or_government')
---
+++ super/find_young_thing/year_or_new_company/high_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_week(str_arg):
+    give_different_fact(str_arg)
+    print('different_problem')
+
+def give_different_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_week('woman_and_day')
