1bae8e6e634f316beb22029269979b8731ba5400
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 13:26:18 -0400

    "rm: have super/problem/leave_early_life/day/own_case_and_day/take_important_point.py, super/little_government/public_eye_and_old_man.py, super/world_and_different_world/person/take_government_above_world/child/important_time/come_point.py and super/able_place/other_fact_or_other_case/find_hand/fact.py from great world"

---
+++ super/able_place/other_fact_or_other_case/find_hand/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number(str_arg):
+    hand(str_arg)
+    print('young_week')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number('child')
---
+++ super/little_government/public_eye_and_old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_problem(str_arg):
+    use_right_number_to_little_person(str_arg)
+    print('last_part_and_different_child')
+
+def use_right_number_to_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_problem('case')
