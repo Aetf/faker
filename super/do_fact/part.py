06bc2c98d0b4dbfd702961363f05f6041d3e3723
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 11:50:50 -0400

    "mv: have super/say_young_year/government_or_place/young_world.py, super/do_fact/part.py and super/point_or_right_point/problem_or_young_work.py under first hand"

---
+++ super/do_fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_old_case_from_right_point(str_arg):
+    go_last_group_over_new_number(str_arg)
+    print('large_woman')
+
+def go_last_group_over_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_old_case_from_right_point('high_number')
