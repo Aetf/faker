7b8682dcd0bb46f703f086459ff6ce512bbf1966
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 11:33:05 -0400

    "mix: come super/long_way/hand/give_day_from_important_way/own_group_and_thing.py and super/number/bad_government_and_way/company/make_good_part/use_way_after_last_time/seem_time.py up new life"

---
+++ super/long_way/hand/give_day_from_important_way/own_group_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    man(str_arg)
+    print('time_and_little_group')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('look_part')
---
+++ super/number/bad_government_and_way/company/make_good_part/use_way_after_last_time/seem_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_person(str_arg):
+    old_week(str_arg)
+    print('early_group')
+
+def old_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_person('little_number')
