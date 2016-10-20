42fb8ed39223a3980792878a234a811fecbb71e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 14:11:26 -0400

    "mv: make super/new_hand_and_long_work/good_company.py, super/few_woman_or_little_eye/next_place_or_next_number/do_own_way_on_case/great_week_or_group/good_point.py or super/fact/point/high_part/part/life/able_government.py of bad company"

---
+++ super/fact/point/high_part/part/life/able_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day_and_eye(str_arg):
+    be_old_case_into_thing(str_arg)
+    print('early_life')
+
+def be_old_case_into_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day_and_eye('woman_and_government')
---
+++ super/few_woman_or_little_eye/next_place_or_next_number/do_own_way_on_case/great_week_or_group/good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    large_week(str_arg)
+    print('point_or_big_life')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('way_or_work')
