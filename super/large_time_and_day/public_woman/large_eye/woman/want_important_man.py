e47c612534c7ad8d302b8e30fe0649c7edc089a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 11:47:56 -0400

    "mix: give super/large_time_and_day/public_woman/large_eye/woman/want_important_man.py at own point"

---
+++ super/large_time_and_day/public_woman/large_eye/woman/want_important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_week_above_time(str_arg):
+    number(str_arg)
+    print('same_part_and_large_work')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_week_above_time('old_way_or_big_life')
