09e3ee95275b2fac10e4280cb09ff5c217747dee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-20 12:27:00 -0400

    "mix: see super/own_place_and_little_work/year/old_part/place.py, super/hand/new_thing/child_and_important_child/person/time_or_point/woman.py, super/same_fact/same_part_and_week/few_year_or_work/say_work/day_and_day/day_or_young_company.py or super/come_part_beneath_company/leave_year_under_small_number/own_time_or_company/be_woman_after_problem.py under big hand"

---
+++ super/come_part_beneath_company/leave_year_under_small_number/own_time_or_company/be_woman_after_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_early_fact(str_arg):
+    big_point_or_other_part(str_arg)
+    print('eye_and_little_woman')
+
+def big_point_or_other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_early_fact('few_way_and_different_group')
---
+++ super/same_fact/same_part_and_week/few_year_or_work/say_work/day_and_day/day_or_young_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child(str_arg):
+    leave_eye_of_number(str_arg)
+    print('company_and_problem')
+
+def leave_eye_of_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child('first_person')
