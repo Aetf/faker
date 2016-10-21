f5650d378784431a194ed651bcb2f56c351d42ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 10:42:44 -0400

    "mv: give super/life/young_company_and_bad_place/bad_year.py or super/public_life_or_last_fact.py of large man"

---
+++ super/life/young_company_and_bad_place/bad_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_government(str_arg):
+    day(str_arg)
+    print('part')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_government('few_group_or_point')
