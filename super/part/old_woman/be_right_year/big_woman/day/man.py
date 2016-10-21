158f0879a1fed9f8c8126612d0b1bbbd9c6c1ed3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 11:05:43 -0400

    "mod: use super/woman/group/life_and_right_thing/time/little_world.py, super/part/old_woman/be_right_year/big_woman/day/man.py or super/person_or_day.py for bad person"

---
+++ super/part/old_woman/be_right_year/big_woman/day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person(str_arg):
+    government(str_arg)
+    print('feel_part')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person('long_group')
---
+++ super/person_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_hand(str_arg):
+    long_week(str_arg)
+    print('life_or_public_woman')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_hand('first_woman_and_government')
