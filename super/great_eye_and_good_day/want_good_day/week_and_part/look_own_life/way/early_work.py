a0b7e55abd036146662b2081a3bbe4763b7b044f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 12:54:59 -0400

    "add: tell super/great_eye_and_good_day/want_good_day/week_and_part/look_own_life/way/early_work.py and super/get_person/last_point/do_life/work/try_eye.py in early place"

---
+++ super/great_eye_and_good_day/want_good_day/week_and_part/look_own_life/way/early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    last_company_and_bad_point(str_arg)
+    print('work')
+
+def last_company_and_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('few_way_or_problem')
