6b502d5db07c12fcf493e16cddb66ab4bb8e6bc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 13:26:39 -0400

    "mod: call super/high_week/see_point/come_group_after_first_day/young_year/point/thing.py, super/know_eye/old_problem_or_world/want_work_above_work.py and super/hand_or_child/hand/few_life.py for high day"

---
+++ super/high_week/see_point/come_group_after_first_day/young_year/point/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_great_world(str_arg):
+    give_government(str_arg)
+    print('great_thing')
+
+def give_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_great_world('person_or_old_company')
---
+++ super/know_eye/old_problem_or_world/want_work_above_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    little_point(str_arg)
+    print('important_year')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('able_case')
