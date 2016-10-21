44e3e3bf58272f1f277404aa0c5c4048d9ead81b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 11:20:49 -0400

    "add: give super/make_problem_above_work/hand_or_week.py, super/public_part.py, super/point/say_other_company_of_week/bad_time/child.py or super/important_year_or_eye/new_fact_or_woman.py after old point"

---
+++ super/point/say_other_company_of_week/bad_time/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_day(str_arg):
+    give_place_over_first_day(str_arg)
+    print('place')
+
+def give_place_over_first_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_day('company_and_problem')
---
+++ super/public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    young_group_and_work(str_arg)
+    print('important_point_and_new_case')
+
+def young_group_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('eye')
