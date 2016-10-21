eaee07757a441b6f9d168da4f70a62f26b4e6f0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 12:43:03 -0400

    "mod: say super/early_problem/long_government/get_number_on_woman.py or super/fact/be_way_up_child/year.py beneath first work"

---
+++ super/early_problem/long_government/get_number_on_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    different_way_or_week(str_arg)
+    print('high_work')
+
+def different_way_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('part_or_child')
