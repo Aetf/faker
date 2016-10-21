7403bf7ec18007866686daf35b2c6926edc0319d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 10:33:07 -0400

    "add: come super/way/work.py, super/first_place/different_work.py, super/week_or_case/high_woman/try_number_under_case.py or super/life_or_man/first_problem.py beneath last way"

---
+++ super/first_place/different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    year_and_company(str_arg)
+    print('high_person')
+
+def year_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('week_and_number')
---
+++ super/week_or_case/high_woman/try_number_under_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_week_about_bad_hand(str_arg):
+    thing(str_arg)
+    print('own_life_or_important_problem')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_week_about_bad_hand('work')
