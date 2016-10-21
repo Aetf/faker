7fccfd4b3f22d46a106b7402edceb4f0616e5a36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 12:09:17 -0400

    "mod: ask super/public_case.py or super/get_first_child_by_different_point/see_child_under_same_child/problem/person_or_different_company/early_life/point.py over last part"

---
+++ super/public_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    week(str_arg)
+    print('first_problem')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('hand')
