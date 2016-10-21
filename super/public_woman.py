c9ded497c72089f4184ba83be1075cd6b60024d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 11:56:10 -0400

    "mod: make super/public_woman.py under public woman"

---
+++ super/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    young_day(str_arg)
+    print('place')
+
+def young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('see_young_place')
