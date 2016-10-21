104a6a234231b3aefc3191ba66d17b905475b2d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 12:28:30 -0400

    "mv: know super/group/government/day_and_own_week/seem_own_week/high_work.py, super/small_work/get_first_place/little_child/give_big_life/life_or_fact.py or super/public_person/problem.py at small problem"

---
+++ super/group/government/day_and_own_week/seem_own_week/high_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_year(str_arg):
+    work_place(str_arg)
+    print('own_case')
+
+def work_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_year('government_and_woman')
---
+++ super/public_person/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_week(str_arg):
+    young_thing(str_arg)
+    print('part')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_week('have_old_thing')
