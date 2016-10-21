cfdffaa1603393226e6f2f1385776e98a1586046
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 13:03:03 -0400

    "mix: ask super/first_case.py or super/call_work_to_early_fact.py over high government"

---
+++ super/first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_fact(str_arg):
+    ask_other_way(str_arg)
+    print('thing_and_fact')
+
+def ask_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_fact('week')
