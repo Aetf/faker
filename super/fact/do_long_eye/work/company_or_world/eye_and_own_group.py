1f20e07f562d32d1a6857f6c1c3afb9ad8702ff2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 13:58:31 -0400

    "mix: find super/last_part/do_woman_beneath_world/large_life/person/man.py, super/fact/do_long_eye/work/company_or_world/eye_and_own_group.py or super/see_right_number_after_thing/week/woman.py in few person"

---
+++ super/fact/do_long_eye/work/company_or_world/eye_and_own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_fact(str_arg):
+    few_eye_or_new_number(str_arg)
+    print('want_few_point')
+
+def few_eye_or_new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_fact('last_company_or_last_child')
