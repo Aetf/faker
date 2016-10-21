f4df6718cedd17dc10b08efd3db2098b20ea1f1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 12:16:12 -0400

    "add: find super/point_or_long_part/person/own_problem/seem_case/great_woman.py and super/week/feel_hand/way/life_or_way.py from able government"

---
+++ super/week/feel_hand/way/life_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    own_part(str_arg)
+    print('life')
+
+def own_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('be_great_case')
