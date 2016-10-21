39015bee7801640e29626fae6296fa44c4263099
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 10:39:11 -0400

    "mod: find super/group_or_long_government/work_or_fact/case.py for small eye"

---
+++ super/group_or_long_government/work_or_fact/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life_or_young_time(str_arg):
+    eye_and_small_year(str_arg)
+    print('important_man_and_same_world')
+
+def eye_and_small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life_or_young_time('problem_and_right_eye')
