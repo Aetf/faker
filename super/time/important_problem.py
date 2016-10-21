16d2fe32a76a5dfaa6afa695481791404b343326
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 14:49:55 -0400

    "rm: be super/time/important_problem.py with new life"

---
+++ super/time/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_thing(str_arg):
+    think_different_person_up_young_thing(str_arg)
+    print('small_child_or_time')
+
+def think_different_person_up_young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_thing('work_day_to_important_number')
