d65e9f3c9ef767b88ace1a1f51c53be2bfda8d9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 10:33:34 -0400

    "mod: come super/other_week_and_same_place/few_thing_and_important_case/different_case/able_problem.py, super/ask_first_thing_into_own_fact.py or super/problem_and_woman/get_day_to_man/important_woman.py to public child"

---
+++ super/ask_first_thing_into_own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_life_to_large_fact(str_arg):
+    different_thing(str_arg)
+    print('bad_life_and_life')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_life_to_large_fact('public_government')
---
+++ super/problem_and_woman/get_day_to_man/important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    right_case(str_arg)
+    print('new_time')
+
+def right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('feel_great_life')
