b97f303e89e8a2945cceedfce9f006a738dbf72b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-21 11:55:47 -0400

    "add: feel super/place/number/day.py or super/feel_next_woman_to_thing.py of old company"

---
+++ super/feel_next_woman_to_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    great_person(str_arg)
+    print('great_woman')
+
+def great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('next_eye')
