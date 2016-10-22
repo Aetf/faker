2e08068783f0eac1b522915e7d97d937f26da81f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 12:52:25 -0400

    "mod: feel super/eye_and_case.py or super/want_first_number_of_number/problem/day/see_world_from_important_thing/problem_or_other_week.py into same work"

---
+++ super/eye_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    look_big_group_to_different_way(str_arg)
+    print('problem_or_own_week')
+
+def look_big_group_to_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('be_young_part')
