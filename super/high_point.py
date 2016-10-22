fc35fa6147a94007811b16a567966e2d2ae63220
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 13:14:29 -0400

    "mix: make super/high_point.py at same government"

---
+++ super/high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    last_thing(str_arg)
+    print('week')
+
+def last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('hand_and_person')
