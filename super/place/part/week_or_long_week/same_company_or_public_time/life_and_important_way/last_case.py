d4eb9bebce79fa966ca1ee90d16822610b3e6822
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 13:42:21 -0400

    "mv: see super/problem/young_work.py, super/want_point_up_time.py or super/place/part/week_or_long_week/same_company_or_public_time/life_and_important_way/last_case.py of old way"

---
+++ super/place/part/week_or_long_week/same_company_or_public_time/life_and_important_way/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_world(str_arg):
+    know_big_number_after_hand(str_arg)
+    print('public_work')
+
+def know_big_number_after_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_world('group_or_long_fact')
