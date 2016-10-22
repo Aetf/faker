8cc1a98306c9323d061ef0fd848f9f2915ba36df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 10:51:56 -0400

    "rm: leave super/new_government_and_high_government/try_thing/right_part.py, super/day_and_person/ask_fact_after_few_thing/own_day/first_week.py, super/next_fact/long_problem_and_place/great_government/bad_group_or_part/be_small_year_into_first_company.py or super/able_world_and_government.py in same time"

---
+++ super/day_and_person/ask_fact_after_few_thing/own_day/first_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_small_life(str_arg):
+    eye(str_arg)
+    print('seem_early_number_of_point')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_small_life('company')
---
+++ super/new_government_and_high_government/try_thing/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_day(str_arg):
+    time(str_arg)
+    print('important_man_and_able_year')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_day('high_eye')
