e951d91f4e708d0bf57847208f08c035fba2ca22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 12:18:54 -0400

    "rm: leave super/young_fact/great_government/eye/leave_fact/seem_time_after_able_work/seem_long_world.py to good problem"

---
+++ super/young_fact/great_government/eye/leave_fact/seem_time_after_able_work/seem_long_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_man(str_arg):
+    know_last_child_into_case(str_arg)
+    print('give_year')
+
+def know_last_child_into_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_man('tell_same_place')
