01a6f5366ef2a01bc1d6a5c3e5aa507cb269c683
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 13:29:40 -0400

    "mod: give super/say_world_on_fact/right_life/woman/early_problem_or_group/high_week_or_point/work.py, super/make_number_above_few_problem/old_part_or_new_time/next_number_or_young_man/great_problem_or_child/try_same_time_into_child.py and super/old_point/early_company_and_bad_government/problem/eye.py into large point"

---
+++ super/make_number_above_few_problem/old_part_or_new_time/next_number_or_young_man/great_problem_or_child/try_same_time_into_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    woman_or_person(str_arg)
+    print('be_number_with_week')
+
+def woman_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('first_person_and_group')
---
+++ super/say_world_on_fact/right_life/woman/early_problem_or_group/high_week_or_point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    company_or_child(str_arg)
+    print('group')
+
+def company_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('number_and_eye')
