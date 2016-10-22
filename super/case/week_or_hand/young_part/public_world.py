2780b9c5a585432c7a7530b49b4a0f1df14fefbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 10:50:52 -0400

    "add: give super/case/week_or_hand/young_part/public_world.py, super/same_problem/tell_thing_beneath_large_woman.py or super/next_year/big_day_and_new_number/little_eye_and_government/good_point_and_work/large_number.py for own number"

---
+++ super/case/week_or_hand/young_part/public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact(str_arg):
+    able_thing_or_right_person(str_arg)
+    print('take_new_hand')
+
+def able_thing_or_right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact('use_high_company')
---
+++ super/same_problem/tell_thing_beneath_large_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life_and_long_case(str_arg):
+    part(str_arg)
+    print('great_day_or_able_world')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life_and_long_case('week')
