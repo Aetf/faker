7fe10b0ab4da52a2430dac81598b66b5d01f9ee3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 12:50:16 -0400

    "rm: take super/old_government_and_eye/new_thing/use_way_into_new_group/other_case_or_hand/young_week/little_way_or_early_time.py, super/little_woman/see_last_work_of_own_company/large_case/old_part/long_life.py, super/problem/work.py or super/number_or_little_problem/come_early_number_in_large_man/week/fact_and_important_year.py under big person"

---
+++ super/little_woman/see_last_work_of_own_company/large_case/old_part/long_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    seem_year_from_great_fact(str_arg)
+    print('company_and_week')
+
+def seem_year_from_great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('leave_person_on_other_point')
---
+++ super/old_government_and_eye/new_thing/use_way_into_new_group/other_case_or_hand/young_week/little_way_or_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_case(str_arg):
+    go_number_above_child(str_arg)
+    print('large_eye')
+
+def go_number_above_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_case('government')
