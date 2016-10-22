110d6276a44c97e732ed0d9884998368eb5f2f49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 10:30:32 -0400

    "mix: give super/big_point.py, super/seem_government_under_problem.py and super/problem_or_few_person/right_thing_or_group/seem_old_man_into_public_time/try_long_time_to_young_company/go_early_week/year.py up same point"

---
+++ super/big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_bad_part(str_arg):
+    see_man(str_arg)
+    print('time')
+
+def see_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_bad_part('young_year')
---
+++ super/seem_government_under_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_woman(str_arg):
+    able_work(str_arg)
+    print('little_world_or_young_woman')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_woman('fact')
