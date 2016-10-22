a60bd2c0be6c10ade01cf1104489c2ce9115722d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 10:06:00 -0400

    "rm: say super/big_thing/work_bad_part_on_good_time/life/few_case/leave_child_at_man/government_and_few_number.py or super/large_work/big_group_or_world/small_time.py under great eye"

---
+++ super/big_thing/work_bad_part_on_good_time/life/few_case/leave_child_at_man/government_and_few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man_or_high_life(str_arg):
+    feel_eye_with_fact(str_arg)
+    print('have_old_woman')
+
+def feel_eye_with_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man_or_high_life('know_point_by_life')
