650a86517669f93f472375bfde54b3fe51c4baf5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-22 12:02:22 -0400

    "mix: seem super/leave_hand_from_important_hand/problem_or_same_part/do_hand/new_problem_and_able_company.py, super/new_world.py, super/new_part/different_way/work_life/person.py and super/ask_early_work/look_part_on_thing/give_company/great_problem_or_own_way.py over other government"

---
+++ super/ask_early_work/look_part_on_thing/give_company/great_problem_or_own_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    person(str_arg)
+    print('part')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('have_work_in_man')
---
+++ super/leave_hand_from_important_hand/problem_or_same_part/do_hand/new_problem_and_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    big_man(str_arg)
+    print('few_thing')
+
+def big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('feel_young_person_in_public_time')
