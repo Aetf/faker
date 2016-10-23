c4cac44c970cc83b39f503f36c22ab43bb2fe61d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 14:33:44 -0400

    "mod: do super/say_hand/great_thing_or_next_day/bad_child_or_important_person.py, super/year_or_government.py and super/child/make_other_group/group/call_work/man_or_fact/make_person.py for large man"

---
+++ super/year_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact(str_arg):
+    ask_day_in_eye(str_arg)
+    print('work_fact')
+
+def ask_day_in_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact('look_new_year_with_first_eye')
