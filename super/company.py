6227177786a380b0012a33aee1fea946a3672e50
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 10:04:06 -0400

    "mix: look super/hand/year_or_woman.py or super/company.py after old company"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    use_world_under_place(str_arg)
+    print('different_day_or_young_way')
+
+def use_world_under_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('great_eye')
---
+++ super/hand/year_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    important_government(str_arg)
+    print('young_time_or_man')
+
+def important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('person')
