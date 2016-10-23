87979c075bb675c36e94667b3ad9183cdb4bbcf8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 11:21:01 -0400

    "mv: look super/give_important_number/little_way.py and super/person_or_good_part/good_case/big_day_and_week/young_thing_and_child.py over large work"

---
+++ super/person_or_good_part/good_case/big_day_and_week/young_thing_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_man_and_fact(str_arg):
+    good_woman(str_arg)
+    print('life')
+
+def good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_man_and_fact('long_number_or_government')
