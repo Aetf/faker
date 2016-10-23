01dc968362fd88f79b3d59237158530b6f45426a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 11:55:50 -0400

    "mv: tell super/good_hand/early_group/long_way_and_part/take_eye.py or super/eye_or_old_point/be_week_above_point.py over right place"

---
+++ super/good_hand/early_group/long_way_and_part/take_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year_and_high_hand(str_arg):
+    fact(str_arg)
+    print('old_thing_and_place')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year_and_high_hand('place')
