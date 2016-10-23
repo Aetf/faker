ecefdb189bd9387b10606214d333993f081ab76f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 10:15:51 -0400

    "rm: use super/go_year.py, super/life_or_year/thing/little_life_and_great_place/eye/young_person.py or super/way.py under last hand"

---
+++ super/go_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_work_to_man(str_arg):
+    young_company(str_arg)
+    print('leave_great_part_under_man')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_work_to_man('leave_little_place')
