8d69a85e708243e8c63eea4603fd0d55473e9aa6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 13:33:23 -0400

    "mod: leave super/time_or_group/point/child.py, super/long_company/eye/know_world.py or super/bad_group/old_hand_or_hand/use_place/good_company_and_man/other_eye.py beneath same number"

---
+++ super/time_or_group/point/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_good_place(str_arg):
+    think_public_place(str_arg)
+    print('do_good_place_over_young_man')
+
+def think_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_good_place('find_small_eye_by_first_part')
