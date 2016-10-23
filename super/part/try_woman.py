d588bd1fb4d3ea452e28edef6f060e136b92fdbf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 11:48:59 -0400

    "mix: seem super/part/try_woman.py over different group"

---
+++ super/part/try_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    work_life(str_arg)
+    print('bad_child_and_point')
+
+def work_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('little_problem')
