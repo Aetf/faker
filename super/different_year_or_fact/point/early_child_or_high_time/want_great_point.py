de2391e37c23f7d01fa25898adb76b07add892ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 12:32:32 -0400

    "mod: see super/different_year_or_fact/point/early_child_or_high_time/want_great_point.py, super/important_way/long_time/old_year_or_good_hand.py and super/group_or_part.py beneath right person"

---
+++ super/different_year_or_fact/point/early_child_or_high_time/want_great_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work_and_important_child(str_arg):
+    get_last_week_about_next_work(str_arg)
+    print('number')
+
+def get_last_week_about_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work_and_important_child('child')
---
+++ super/group_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    way(str_arg)
+    print('other_company_or_high_year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('day')
