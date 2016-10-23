d99c2d17aaad76141806d59a990c92f2446254d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 14:56:29 -0400

    "mod: come super/few_life/early_thing/work.py, super/time/year/new_year/first_year/other_place.py or super/eye/different_man.py under last group"

---
+++ super/few_life/early_thing/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman_or_little_case(str_arg):
+    company(str_arg)
+    print('own_world')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman_or_little_case('point_and_high_fact')
