53ae6c59f61cc5860ae82fe2db5aa7071b50c562
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 13:00:25 -0400

    "mod: see super/week.py from last place"

---
+++ super/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child(str_arg):
+    world(str_arg)
+    print('next_number_and_year')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child('bad_work_and_life')
