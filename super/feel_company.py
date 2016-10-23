b7234507562b700b740307b55079707c45c0f1c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 11:20:41 -0400

    "rm: do super/old_point/different_life/think_case_over_great_place/week.py and super/feel_company.py above small point"

---
+++ super/feel_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_great_place(str_arg):
+    old_number(str_arg)
+    print('group_or_great_group')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_great_place('last_life')
---
+++ super/old_point/different_life/think_case_over_great_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    high_day_and_point(str_arg)
+    print('different_person')
+
+def high_day_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('fact')
