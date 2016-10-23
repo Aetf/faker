487a3a4a07f207a13ce3dc9f88569fc7ab6e6658
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 14:43:33 -0400

    "mix: make super/case/seem_high_time_of_few_fact.py, super/get_week/small_person_or_good_group/find_part_into_able_government/good_fact_or_place/number/say_child.py and super/child_or_group/young_thing/same_group.py by long number"

---
+++ super/child_or_group/young_thing/same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place(str_arg):
+    small_child(str_arg)
+    print('case_and_man')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place('year_and_number')
---
+++ super/get_week/small_person_or_good_group/find_part_into_able_government/good_fact_or_place/number/say_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    few_place_or_early_government(str_arg)
+    print('place')
+
+def few_place_or_early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('young_government')
