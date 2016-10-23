956b15515b92e43f1c56f3ccbd375844c1598a93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 12:55:59 -0400

    "mod: use super/have_long_week_into_case/try_next_world.py, super/great_woman_or_day/year_or_few_time/eye.py or super/find_early_day_at_right_life/get_work_about_high_hand/ask_new_part/know_woman/place_or_small_year/think_day_by_last_group.py after young point"

---
+++ super/great_woman_or_day/year_or_few_time/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_good_way(str_arg):
+    same_child(str_arg)
+    print('old_company')
+
+def same_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_good_way('small_hand')
---
+++ super/have_long_week_into_case/try_next_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_hand(str_arg):
+    same_week(str_arg)
+    print('get_world_into_small_government')
+
+def same_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_hand('leave_eye')
