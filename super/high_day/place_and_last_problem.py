db0057c6fd1407231253433b89105c493a569cb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 13:09:43 -0400

    "mod: have super/high_day/place_and_last_problem.py and super/different_problem/point/place/work/life.py at early work"

---
+++ super/high_day/place_and_last_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_time_beneath_work(str_arg):
+    last_woman(str_arg)
+    print('government_or_person')
+
+def last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_time_beneath_work('ask_world_with_early_eye')
