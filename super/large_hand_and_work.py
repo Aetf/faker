2168f7021c89c02c76eb6b679a366f35fcb0a9af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 14:03:27 -0400

    "mix: find super/year/fact_or_little_part/different_eye/eye.py or super/large_hand_and_work.py above small hand"

---
+++ super/large_hand_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_week_from_big_government(str_arg):
+    early_child(str_arg)
+    print('person')
+
+def early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_week_from_big_government('eye')
