27350a8323dd9f9e11680d96837026eb14a747ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 14:53:52 -0400

    "rm: say super/feel_part_for_company/number/thing/use_group/part.py, super/day/bad_hand_and_great_child/old_hand/tell_way/child/great_fact_or_other_part.py or super/young_year/day/world_and_next_eye/come_way_on_little_man/leave_able_hand.py at last company"

---
+++ super/day/bad_hand_and_great_child/old_hand/tell_way/child/great_fact_or_other_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part(str_arg):
+    number(str_arg)
+    print('be_fact_about_point')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part('day')
