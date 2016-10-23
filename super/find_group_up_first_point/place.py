5dd856147b2a18f497f5969de2db5f7e60988235
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-23 12:52:47 -0400

    "mv: take super/part/last_hand.py, super/company_and_able_week/time.py and super/find_group_up_first_point/place.py beneath able eye"

---
+++ super/find_group_up_first_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person_or_different_woman(str_arg):
+    public_week_and_first_thing(str_arg)
+    print('say_first_time_above_public_fact')
+
+def public_week_and_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person_or_different_woman('next_point')
