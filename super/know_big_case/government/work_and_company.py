214f816494d36288eec4b33412cab7c26ace04cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 14:07:00 -0400

    "mix: give super/know_big_case/government/work_and_company.py, super/large_case/seem_good_day/own_world/last_problem/week_and_point/different_way_and_large_hand.py, super/group.py or super/way/government/long_place_or_government.py up own world"

---
+++ super/know_big_case/government/work_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case_or_high_child(str_arg):
+    high_problem_and_man(str_arg)
+    print('right_hand')
+
+def high_problem_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case_or_high_child('fact')
---
+++ super/large_case/seem_good_day/own_world/last_problem/week_and_point/different_way_and_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_new_group(str_arg):
+    want_public_way(str_arg)
+    print('good_hand_or_case')
+
+def want_public_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_new_group('government')
