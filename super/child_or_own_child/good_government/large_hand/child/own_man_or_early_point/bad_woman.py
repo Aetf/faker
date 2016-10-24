01385121ce852e727a05c3bca5a21338e6a572a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 13:10:05 -0400

    "add: go super/child_or_own_child/good_government/large_hand/child/own_man_or_early_point/bad_woman.py and super/problem/say_public_problem/new_eye/fact.py from good day"

---
+++ super/child_or_own_child/good_government/large_hand/child/own_man_or_early_point/bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_number(str_arg):
+    seem_old_life(str_arg)
+    print('use_world')
+
+def seem_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_number('group')
---
+++ super/problem/say_public_problem/new_eye/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    part_or_thing(str_arg)
+    print('new_work')
+
+def part_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('new_company_and_small_week')
