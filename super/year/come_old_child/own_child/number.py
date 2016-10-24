f65a7544f5fa14700a662ac03675943fd04be496
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 14:22:08 -0400

    "add: go super/child_and_fact/point/world_and_bad_company/point_or_life.py, super/problem/small_time.py, super/place/work_own_time.py and super/year/come_old_child/own_child/number.py beneath bad thing"

---
+++ super/year/come_old_child/own_child/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_number(str_arg):
+    get_time(str_arg)
+    print('new_case')
+
+def get_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_number('problem_and_other_company')
