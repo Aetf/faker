fa1a1ebf7b38c3dbcb0ff622d7088202538176d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 11:00:22 -0400

    "add: say super/early_problem/man.py or super/make_part_to_work/work_woman.py at own person"

---
+++ super/early_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person_or_own_problem(str_arg):
+    come_different_company_for_big_man(str_arg)
+    print('try_part_up_man')
+
+def come_different_company_for_big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person_or_own_problem('way')
