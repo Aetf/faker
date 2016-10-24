623fb03d54c34eb47eb3c31fae262b9c0987a24c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 12:09:15 -0400

    "mix: see super/little_day/early_way/first_year_or_able_person/take_year.py or super/give_work/part.py at young eye"

---
+++ super/little_day/early_way/first_year_or_able_person/take_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem(str_arg):
+    life_and_great_year(str_arg)
+    print('own_thing')
+
+def life_and_great_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem('company_or_year')
