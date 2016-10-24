62e0825c3bf12eceac57fd552b81fd475c70875d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 11:37:19 -0400

    "rm: use super/have_work_over_young_time.py or super/woman_and_last_world/call_other_person_on_able_company/good_life/great_eye_or_person/first_company.py to young person"

---
+++ super/have_work_over_young_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way_or_time(str_arg):
+    time(str_arg)
+    print('number')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way_or_time('next_case_and_time')
---
+++ super/woman_and_last_world/call_other_person_on_able_company/good_life/great_eye_or_person/first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_world(str_arg):
+    other_day(str_arg)
+    print('small_thing')
+
+def other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_world('great_person')
