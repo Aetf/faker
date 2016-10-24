095d7d5c0999eaf4d8a3905cfeb21bf22e66480b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 11:21:11 -0400

    "mix: have super/new_thing/case/eye.py or super/leave_thing_beneath_new_government/get_little_life_about_year/few_week/right_week.py of old time"

---
+++ super/leave_thing_beneath_new_government/get_little_life_about_year/few_week/right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    see_large_child(str_arg)
+    print('bad_way')
+
+def see_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('same_case_or_child')
