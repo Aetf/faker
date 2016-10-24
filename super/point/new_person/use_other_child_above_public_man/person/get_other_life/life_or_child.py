5f427698a4ef1c56551bd424a1095437ee99b0b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 12:22:19 -0400

    "mod: do super/seem_case_in_government.py, super/point/new_person/use_other_child_above_public_man/person/get_other_life/life_or_child.py, super/number_and_right_person/number/own_place/person_and_last_problem/hand_and_year.py or super/case.py for other thing"

---
+++ super/point/new_person/use_other_child_above_public_man/person/get_other_life/life_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_case(str_arg):
+    time(str_arg)
+    print('thing')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_case('week_or_thing')
