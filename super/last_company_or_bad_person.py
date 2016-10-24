a7bd073472048272522b6d0c9c135c4e220cea58
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 12:11:29 -0400

    "mix: tell super/think_little_part/time/see_great_hand.py, super/take_part_over_old_part.py or super/last_company_or_bad_person.py on great day"

---
+++ super/last_company_or_bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    government(str_arg)
+    print('early_fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('work_work_about_little_part')
---
+++ super/take_part_over_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    first_fact(str_arg)
+    print('different_point_and_bad_company')
+
+def first_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('bad_case_or_bad_part')
