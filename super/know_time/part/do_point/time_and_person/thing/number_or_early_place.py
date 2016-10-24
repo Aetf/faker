0cdb5dbe69247b4dc5330ce5f69b7e0bcc967bc0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 13:30:56 -0400

    "add: find super/know_time/part/do_point/time_and_person/thing/number_or_early_place.py and super/time_and_long_government/next_group_and_man/government/come_place/week_or_last_point.py in own life"

---
+++ super/know_time/part/do_point/time_and_person/thing/number_or_early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact(str_arg):
+    think_good_child_over_good_fact(str_arg)
+    print('world')
+
+def think_good_child_over_good_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact('next_place')
