d97b5d7ff152c60a392a0c86b02564be08823742
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 10:10:50 -0400

    "add: ask super/person.py and super/way/go_first_government_up_old_place/fact/leave_own_group_with_right_day/way_and_important_group/thing.py over big eye"

---
+++ super/way/go_first_government_up_old_place/fact/leave_own_group_with_right_day/way_and_important_group/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_thing(str_arg):
+    right_life(str_arg)
+    print('life')
+
+def right_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_thing('way')
