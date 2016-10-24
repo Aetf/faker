17a8cdd72ef84bf8e16fcce70a9b3ab7bfafeec1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 10:47:03 -0400

    "mod: make super/hand_and_time/work_work/do_able_hand/high_week_or_old_time/problem/use_bad_part_at_few_week.py of first group"

---
+++ super/hand_and_time/work_work/do_able_hand/high_week_or_old_time/problem/use_bad_part_at_few_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_different_thing_by_few_thing(str_arg):
+    first_way(str_arg)
+    print('child_or_number')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_different_thing_by_few_thing('take_other_part_beneath_group')
