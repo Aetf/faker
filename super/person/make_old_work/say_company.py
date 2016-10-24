d53104e50483881325b0666d2f1f365e6067bbbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 10:12:46 -0400

    "mv: feel super/person/make_old_work/say_company.py, super/old_hand.py and super/important_case_or_point/world.py about last man"

---
+++ super/person/make_old_work/say_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_problem(str_arg):
+    few_child(str_arg)
+    print('eye')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_problem('first_group')
