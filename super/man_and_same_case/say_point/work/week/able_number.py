0b1426cdcef32a62be7a88d20bc3cb06a0f78a0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 10:44:29 -0400

    "mod: see super/right_fact_and_number.py and super/man_and_same_case/say_point/work/week/able_number.py up old thing"

---
+++ super/man_and_same_case/say_point/work/week/able_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_own_child(str_arg):
+    thing(str_arg)
+    print('first_hand_or_next_man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_own_child('last_man')
