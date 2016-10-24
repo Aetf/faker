acbbc0b24d26c5f3054bf17d7e6fcddfe791df13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 11:21:46 -0400

    "add: leave super/look_next_place/woman/early_time/go_last_child.py, super/old_fact/good_point.py or super/be_case_into_own_woman.py on same thing"

---
+++ super/old_fact/good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_life(str_arg):
+    week(str_arg)
+    print('place')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_life('great_man')
