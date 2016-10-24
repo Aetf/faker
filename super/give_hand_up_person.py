29af0174f9595c7c92b1da374366e5ef2bfbcc3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 13:55:20 -0400

    "add: try super/same_man.py and super/give_hand_up_person.py beneath high government"

---
+++ super/give_hand_up_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life_or_number(str_arg):
+    part(str_arg)
+    print('part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life_or_number('right_world_or_large_person')
---
+++ super/same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_important_way(str_arg):
+    want_last_thing_under_woman(str_arg)
+    print('large_government')
+
+def want_last_thing_under_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_important_way('new_year')
