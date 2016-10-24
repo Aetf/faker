b8d897f14a35c5d134a9369459c3e21fa4a33dec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-24 12:15:41 -0400

    "mv: use super/year/government.py and super/good_man/group/few_group/same_life_or_part/different_group_and_fact/think_fact.py to next thing"

---
+++ super/good_man/group/few_group/same_life_or_part/different_group_and_fact/think_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_early_time_beneath_week(str_arg):
+    big_person_or_early_day(str_arg)
+    print('case_or_first_time')
+
+def big_person_or_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_early_time_beneath_week('hand_and_important_week')
