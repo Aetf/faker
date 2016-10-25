afa2d8a943b9c40609f56186252691ded896d956
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 12:56:33 -0400

    "mv: give super/important_fact/world/few_child/do_child/woman/group.py and super/young_day/see_early_part/know_person.py above next government"

---
+++ super/young_day/see_early_part/know_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    next_week(str_arg)
+    print('point')
+
+def next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('same_point')
