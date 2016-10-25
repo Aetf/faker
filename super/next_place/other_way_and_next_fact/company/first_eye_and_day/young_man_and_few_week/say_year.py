b1203cb1051b2618d4d954c974ced5fe1a302e9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:56:11 -0400

    "mod: want super/next_place/other_way_and_next_fact/company/first_eye_and_day/young_man_and_few_week/say_year.py and super/year_or_next_year.py on long company"

---
+++ super/next_place/other_way_and_next_fact/company/first_eye_and_day/young_man_and_few_week/say_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    go_few_day_with_little_child(str_arg)
+    print('own_government')
+
+def go_few_day_with_little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('able_way_or_young_week')
