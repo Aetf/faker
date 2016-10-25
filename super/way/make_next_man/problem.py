906b10d467d838dcf50d676855ac1b20736913e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 11:16:54 -0400

    "add: ask super/way/make_next_man/problem.py or super/first_government/different_man.py on old life"

---
+++ super/way/make_next_man/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_world(str_arg):
+    group_and_able_group(str_arg)
+    print('able_child_and_good_point')
+
+def group_and_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_world('go_child')
