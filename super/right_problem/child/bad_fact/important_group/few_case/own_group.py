e4ff2a74412b4ce555929c6f42124fc34232dc16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 12:48:44 -0400

    "mix: go super/thing_and_part/be_day_about_government/large_man/eye/public_point_or_work/early_time_and_thing.py, super/right_problem/child/bad_fact/important_group/few_case/own_group.py or super/want_young_person/child/important_place.py beneath new life"

---
+++ super/right_problem/child/bad_fact/important_group/few_case/own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number(str_arg):
+    seem_small_way(str_arg)
+    print('first_week_or_point')
+
+def seem_small_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number('other_fact')
---
+++ super/thing_and_part/be_day_about_government/large_man/eye/public_point_or_work/early_time_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    group(str_arg)
+    print('tell_year_into_same_year')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('look_part_above_woman')
