e11eac4f6e71abc43b0b5fa82f9ee56ae8a07791
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 11:36:15 -0400

    "mix: want super/long_time/thing.py, super/think_case.py and super/feel_last_man_above_problem/world.py over good day"

---
+++ super/feel_last_man_above_problem/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_thing(str_arg):
+    same_point_or_high_place(str_arg)
+    print('week_and_big_day')
+
+def same_point_or_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_thing('work')
