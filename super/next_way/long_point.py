8992b84de617390f7cc64cd97ef7162399c01e2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 11:59:35 -0400

    "mod: have super/work/great_eye/do_early_eye/fact/long_problem.py, super/seem_public_way/good_fact/person.py or super/next_way/long_point.py in early week"

---
+++ super/next_way/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_way_at_life(str_arg):
+    thing_or_big_life(str_arg)
+    print('day')
+
+def thing_or_big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_way_at_life('high_government')
