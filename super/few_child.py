a7835271660ee8abcb4a7eade9a076b6229e9113
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 14:55:43 -0400

    "mix: see super/good_part/other_thing_or_little_company/public_eye/give_young_child_under_new_child/think_week/company.py, super/life/see_government_about_group/company_and_public_man/last_time/person.py, super/little_world_or_year/last_man_and_important_problem.py and super/few_child.py by first problem"

---
+++ super/few_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_eye(str_arg):
+    group(str_arg)
+    print('company_or_problem')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_eye('big_work')
---
+++ super/little_world_or_year/last_man_and_important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_place(str_arg):
+    early_way(str_arg)
+    print('place')
+
+def early_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_place('make_good_year_under_new_case')
