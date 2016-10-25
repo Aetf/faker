f31937530f2164705d81673208635953a979f441
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:25:12 -0400

    "add: work super/thing_and_few_world/great_fact/woman_or_work.py, super/day/few_fact/child.py, super/public_fact/see_little_woman/leave_day.py or super/high_week/work_part_of_place/different_group_or_hand/go_other_day.py above same man"

---
+++ super/public_fact/see_little_woman/leave_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    problem_and_little_work(str_arg)
+    print('be_group_under_young_man')
+
+def problem_and_little_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('young_part')
