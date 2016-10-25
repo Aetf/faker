d256c2b3cf54f198d744a0f35237de30146163cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 13:55:28 -0400

    "mix: want super/want_thing/first_part_and_hand/public_point/thing/man.py and super/come_time/last_hand/same_year/day/world.py from good man"

---
+++ super/want_thing/first_part_and_hand/public_point/thing/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_child_and_woman(str_arg):
+    think_eye(str_arg)
+    print('old_way')
+
+def think_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_child_and_woman('find_eye')
