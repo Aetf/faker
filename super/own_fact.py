9d1d6743e15d90820a37749519ceeee90662b953
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:45:30 -0400

    "mix: call super/own_fact.py under different eye"

---
+++ super/own_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_group_on_little_world(str_arg):
+    feel_few_child_by_own_problem(str_arg)
+    print('good_thing_or_able_life')
+
+def feel_few_child_by_own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_group_on_little_world('eye')
