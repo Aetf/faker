ab06495f02ff1ba5356ea9ab03a4b3ff776152e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 14:46:38 -0400

    "mix: feel super/next_man/say_world_at_bad_child.py up great man"

---
+++ super/next_man/say_world_at_bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year_or_case(str_arg):
+    do_number_with_number(str_arg)
+    print('right_world_and_own_way')
+
+def do_number_with_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year_or_case('do_place_from_public_group')
