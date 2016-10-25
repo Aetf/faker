de337ef985aad1a4ded067e145a80a7ce266a80e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:49:54 -0400

    "mod: use super/want_public_day/hand/time/next_fact.py or super/few_world/large_man/point_and_person/big_week.py on other child"

---
+++ super/few_world/large_man/point_and_person/big_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_part_into_way(str_arg):
+    bad_group_and_child(str_arg)
+    print('few_week')
+
+def bad_group_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_part_into_way('last_case_or_week')
---
+++ super/want_public_day/hand/time/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact(str_arg):
+    know_government(str_arg)
+    print('long_person_or_same_group')
+
+def know_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact('eye')
