301826f512539fb1a2b2a8812755ffb00f8cb37f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:50:20 -0400

    "mod: have super/world/number/little_hand/different_man_and_early_year/last_group.py over small place"

---
+++ super/world/number/little_hand/different_man_and_early_year/last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_bad_person(str_arg):
+    come_next_point_after_part(str_arg)
+    print('woman')
+
+def come_next_point_after_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_bad_person('tell_eye')
