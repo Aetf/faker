4e6170cb59bc6a0e779251242216bcb08f488d04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 12:52:40 -0400

    "mix: work super/group_and_day/man/number/part_or_government/give_young_point_after_few_week/public_person.py, super/make_person.py, super/do_early_hand_at_man/big_way.py or super/important_group_and_time/good_hand/person/give_last_woman_with_year/use_able_place.py under right place"

---
+++ super/important_group_and_time/good_hand/person/give_last_woman_with_year/use_able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    eye_or_thing(str_arg)
+    print('use_next_woman')
+
+def eye_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('work_next_fact')
