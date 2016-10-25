a104893ffb243b0a4958fa8daf2b521b91469a73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 11:05:57 -0400

    "mod: seem super/make_young_case.py by next woman"

---
+++ super/make_young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_same_part_up_problem(str_arg):
+    long_way(str_arg)
+    print('make_problem')
+
+def long_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_same_part_up_problem('right_way')
