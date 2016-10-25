21cafa4e71ddf6b0c669960126042f10eaa87b13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 11:27:16 -0400

    "mv: leave super/have_first_place/problem/long_case_or_thing/way_and_old_woman/use_big_woman/own_week_and_public_point.py and super/woman_or_place.py about little case"

---
+++ super/woman_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_company_up_place(str_arg):
+    want_place(str_arg)
+    print('number')
+
+def want_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_company_up_place('large_year')
