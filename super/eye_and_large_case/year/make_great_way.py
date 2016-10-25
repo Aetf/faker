28eb42c67b1026eff6fe647029d0e97f00931139
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 14:35:08 -0400

    "rm: find super/life/do_able_thing_of_right_government/first_fact/company.py and super/eye_and_large_case/year/make_great_way.py from old number"

---
+++ super/eye_and_large_case/year/make_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    try_young_time(str_arg)
+    print('case')
+
+def try_young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('young_thing')
---
+++ super/life/do_able_thing_of_right_government/first_fact/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_first_woman(str_arg):
+    early_fact(str_arg)
+    print('few_year')
+
+def early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_first_woman('old_group')
