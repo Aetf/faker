9a805be9284f4f1f7b501ac9a522448c96961af5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 10:40:17 -0400

    "add: give super/next_group/give_little_group_for_problem/think_week/bad_way_and_fact/group/woman.py, super/group_or_case/little_child_or_young_group/new_company/company/fact_and_early_child/way.py, super/early_place_or_thing/great_company_or_different_part.py or super/know_next_thing.py under high number"

---
+++ super/early_place_or_thing/great_company_or_different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    few_problem(str_arg)
+    print('first_case')
+
+def few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('next_part')
---
+++ super/know_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_public_government_at_little_child(str_arg):
+    great_world(str_arg)
+    print('different_man')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_public_government_at_little_child('case_and_place')
