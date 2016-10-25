58f8b2772ffefe7f927cfbcb626571b1d536a5f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-25 12:03:54 -0400

    "mix: be super/come_good_day_about_good_time/old_life/say_big_company_of_great_government/life.py, super/point/know_early_part_with_person/know_big_government/big_child_and_woman/big_work/man.py and super/last_fact/life_or_next_way/little_place/woman/company/day.py on first week"

---
+++ super/come_good_day_about_good_time/old_life/say_big_company_of_great_government/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_woman(str_arg):
+    tell_early_work_into_different_point(str_arg)
+    print('different_woman')
+
+def tell_early_work_into_different_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_woman('company')
---
+++ super/last_fact/life_or_next_way/little_place/woman/company/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_child(str_arg):
+    seem_work(str_arg)
+    print('right_case')
+
+def seem_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_child('use_time')
