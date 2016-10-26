0027d9a26ff82408ab1d45dd09e7ede8ae5cf8a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 11:05:43 -0400

    "add: give super/able_time.py from young eye"

---
+++ super/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_old_eye(str_arg):
+    hand(str_arg)
+    print('new_day_and_day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_old_eye('old_case')
