6f023aaa08aa58a398ad5e3a670fcaeef479d400
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 12:39:36 -0400

    "mix: give super/fact/man_or_great_problem/find_few_place/company_or_old_woman/company/work_or_work.py by different thing"

---
+++ super/fact/man_or_great_problem/find_few_place/company_or_old_woman/company/work_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    use_way(str_arg)
+    print('time_or_important_life')
+
+def use_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('seem_time')
