db1b45c675279691a051073df65091af2fc0fc72
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 12:36:33 -0400

    "add: try super/fact/work_own_number_under_woman.py over public eye"

---
+++ super/fact/work_own_number_under_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_government(str_arg):
+    important_company(str_arg)
+    print('large_part')
+
+def important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_government('eye_or_small_day')
