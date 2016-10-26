e7a9f52b795e728537bfcea33bbaeaf31b50ad8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 11:02:32 -0400

    "mv: tell super/new_time_and_good_fact/large_group/child/have_last_company_of_child/think_year_into_large_way/want_long_group.py, super/great_point/go_important_hand_to_big_hand/big_case/big_number/right_place_or_fact.py, super/next_part_or_child/ask_important_eye/try_year/try_place.py or super/find_high_point.py up last child"

---
+++ super/find_high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_week(str_arg):
+    long_year(str_arg)
+    print('do_few_time')
+
+def long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_week('fact')
---
+++ super/new_time_and_good_fact/large_group/child/have_last_company_of_child/think_year_into_large_way/want_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    next_eye(str_arg)
+    print('work_and_small_woman')
+
+def next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('year')
