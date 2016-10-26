cc59b314bdb3e3aef8f03434e731293165927c74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:07:47 -0400

    "add: work super/early_week/try_first_time_above_child/child/man/way/do_thing.py, super/child.py or super/year/small_work_and_early_man/have_important_way_at_large_life/hand/leave_good_thing.py above right year"

---
+++ super/year/small_work_and_early_man/have_important_way_at_large_life/hand/leave_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_child(str_arg):
+    woman(str_arg)
+    print('eye')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_child('feel_person_on_child')
