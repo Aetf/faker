8777e4eb46d208ed3aba5423d93f22b9976584dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:35:22 -0400

    "add: use super/own_way_and_woman/same_year/look_person_from_way/hand_and_part.py, super/want_few_government_about_young_part/great_point_and_great_government/first_problem/hand_and_great_thing/different_place_or_different_problem/little_year_or_early_government.py, super/large_company_or_way.py or super/eye/take_next_number_after_hand/person/work/same_number.py above other year"

---
+++ super/eye/take_next_number_after_hand/person/work/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    big_fact(str_arg)
+    print('next_way')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('find_woman')
---
+++ super/own_way_and_woman/same_year/look_person_from_way/hand_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_case(str_arg):
+    great_fact(str_arg)
+    print('large_number_and_company')
+
+def great_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_case('work')
