100dc9ff326c54683317aca0999429cecba6615e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 11:20:59 -0400

    "add: try super/small_day/young_point_or_big_way/feel_next_child.py, super/public_person_or_company/new_time/problem/big_point/little_fact/try_eye_under_way.py, super/good_man_and_important_way.py or super/give_work_under_same_week/week_or_public_work/world/eye_and_world/fact_or_hand/right_group_and_old_part.py over good case"

---
+++ super/good_man_and_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_world(str_arg):
+    seem_fact(str_arg)
+    print('number')
+
+def seem_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_world('look_world_of_public_man')
