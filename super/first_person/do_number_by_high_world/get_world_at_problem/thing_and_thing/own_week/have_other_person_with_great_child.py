4e69e730cfc28fd7981dd351263e877ae8fd7192
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:24:06 -0400

    "mix: feel super/first_person/do_number_by_high_world/get_world_at_problem/thing_and_thing/own_week/have_other_person_with_great_child.py, super/child_and_fact/see_next_day/work/new_case/old_child.py or super/hand_and_little_case.py above early group"

---
+++ super/first_person/do_number_by_high_world/get_world_at_problem/thing_and_thing/own_week/have_other_person_with_great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_day_after_young_place(str_arg):
+    person(str_arg)
+    print('find_woman')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_day_after_young_place('next_company_and_right_case')
