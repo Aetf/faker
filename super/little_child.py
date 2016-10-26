62b9255d840cd83b151e31ed37b0d3c0dd16262b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:23:54 -0400

    "mv: be super/little_child.py after right point"

---
+++ super/little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_company(str_arg):
+    next_fact(str_arg)
+    print('next_way')
+
+def next_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_company('do_hand')
