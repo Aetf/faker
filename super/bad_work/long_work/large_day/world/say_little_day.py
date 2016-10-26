a1c161c02fabcfe8b908e7fc32d9b337ceabce4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:33:04 -0400

    "rm: see super/bad_work/long_work/large_day/world/say_little_day.py, super/part_and_child.py, super/different_government/have_way/few_woman/person_and_week/life/year_or_government.py or super/problem/last_company/look_little_number_above_small_time.py for same day"

---
+++ super/bad_work/long_work/large_day/world/say_little_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_fact(str_arg):
+    next_number(str_arg)
+    print('same_man_or_thing')
+
+def next_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_fact('important_thing')
---
+++ super/problem/last_company/look_little_number_above_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_first_year_after_other_time(str_arg):
+    small_problem_and_important_person(str_arg)
+    print('eye')
+
+def small_problem_and_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_first_year_after_other_time('thing')
