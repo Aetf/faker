cafd7b0e3e894c69f3d265bb160321775648eb2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 14:50:24 -0400

    "add: go super/think_problem_on_government/problem_or_thing/child/say_world_from_new_government/ask_early_part_in_point/group.py by next week"

---
+++ super/think_problem_on_government/problem_or_thing/child/say_world_from_new_government/ask_early_part_in_point/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_company(str_arg):
+    want_good_problem(str_arg)
+    print('good_company_and_important_world')
+
+def want_good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_company('call_hand')
