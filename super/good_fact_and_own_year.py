34f9d99e9b8b81b5690024804ab666671dd98ed1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 12:16:48 -0400

    "mod: know super/good_fact_and_own_year.py with young way"

---
+++ super/good_fact_and_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_bad_fact_for_person(str_arg):
+    long_fact(str_arg)
+    print('own_world_or_same_week')
+
+def long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_bad_fact_for_person('call_world_above_woman')
