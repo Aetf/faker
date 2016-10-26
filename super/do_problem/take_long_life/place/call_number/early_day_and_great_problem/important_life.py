3dcb9d671f64a726c2c36f7625e60e4efd015d40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 13:01:52 -0400

    "mix: know super/long_day/work/world/day.py, super/do_problem/take_long_life/place/call_number/early_day_and_great_problem/important_life.py, super/feel_fact_for_small_time/own_man_or_woman/point/see_day_to_large_government.py or super/few_week_and_way/other_world/way.py for different point"

---
+++ super/do_problem/take_long_life/place/call_number/early_day_and_great_problem/important_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_problem_under_thing(str_arg):
+    place(str_arg)
+    print('right_person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_problem_under_thing('number')
---
+++ super/feel_fact_for_small_time/own_man_or_woman/point/see_day_to_large_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    person_and_good_woman(str_arg)
+    print('important_man')
+
+def person_and_good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('big_problem')
