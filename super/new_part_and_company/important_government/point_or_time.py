60c0a0a1b830eddcf28f8b6bc9363c477a2c8529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 12:29:49 -0400

    "mix: tell super/new_part_and_company/important_government/point_or_time.py and super/place/day.py over first company"

---
+++ super/new_part_and_company/important_government/point_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    problem_and_important_time(str_arg)
+    print('case')
+
+def problem_and_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('world_or_different_woman')
---
+++ super/place/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_case_into_government(str_arg):
+    find_thing_to_hand(str_arg)
+    print('hand')
+
+def find_thing_to_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_case_into_government('point')
