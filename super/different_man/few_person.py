6b0d8f1179ecbef07eb300cfb261d6bc198deb75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 10:55:28 -0400

    "mix: feel super/part_or_few_company/leave_public_woman_beneath_big_life.py, super/want_fact/life.py or super/different_man/few_person.py from right point"

---
+++ super/different_man/few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    life(str_arg)
+    print('new_part')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('same_child_or_able_place')
