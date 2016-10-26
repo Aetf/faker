8973f968f4ede813db570391a88b83f7222b4b08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 10:23:26 -0400

    "mv: look super/old_number_or_high_work/make_big_thing_up_little_way/able_place/right_eye/part.py, super/number/point/next_company/work_and_year/work_man_from_hand/little_time.py, super/thing_and_child/public_day/small_time.py or super/tell_big_life/year/large_child/world/part_and_life/young_fact.py after same eye"

---
+++ super/number/point/next_company/work_and_year/work_man_from_hand/little_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work_or_few_person(str_arg):
+    right_woman(str_arg)
+    print('think_time_into_child')
+
+def right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work_or_few_person('long_thing')
