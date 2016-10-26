2370418dd32259c2c60a106470258b3f5af499e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-26 11:56:37 -0400

    "rm: get super/see_long_hand_above_bad_world/child_and_important_week.py and super/new_week.py of high work"

---
+++ super/see_long_hand_above_bad_world/child_and_important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    own_point(str_arg)
+    print('know_own_eye')
+
+def own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('different_work_or_right_person')
