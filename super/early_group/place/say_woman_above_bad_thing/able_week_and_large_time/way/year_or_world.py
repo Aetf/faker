e9f402b3dd487b0c75adb767ba47ceabc3e43125
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:50:24 -0400

    "rm: get super/early_group/place/say_woman_above_bad_thing/able_week_and_large_time/way/year_or_world.py and super/world/seem_life/number.py above big woman"

---
+++ super/early_group/place/say_woman_above_bad_thing/able_week_and_large_time/way/year_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_different_point(str_arg):
+    make_small_group_at_long_work(str_arg)
+    print('find_old_case')
+
+def make_small_group_at_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_different_point('part')
