b3b1fffe2275633778d3f4383a98cd5ae4f2a172
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 11:10:43 -0400

    "mod: know super/last_group/say_high_problem/other_point/little_hand.py and super/see_other_way/problem.py into large number"

---
+++ super/last_group/say_high_problem/other_point/little_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_number_or_case(str_arg):
+    use_work(str_arg)
+    print('person')
+
+def use_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_number_or_case('give_young_person')
---
+++ super/see_other_way/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    think_point(str_arg)
+    print('person')
+
+def think_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('child_and_number')
