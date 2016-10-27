7f028c51256b018489bf246e5b96de8b9a9ae4c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 13:27:57 -0400

    "mv: be super/point_and_eye/big_child/child_and_hand/child/important_part/eye.py on public government"

---
+++ super/point_and_eye/big_child/child_and_hand/child/important_part/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    big_person(str_arg)
+    print('tell_place_with_next_thing')
+
+def big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('case')
