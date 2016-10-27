3573256c15698094e693984d2172acdc6cdd1d87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 10:21:41 -0400

    "mix: use super/long_part/think_bad_company_beneath_year/year_and_problem.py of little week"

---
+++ super/long_part/think_bad_company_beneath_year/year_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place(str_arg):
+    woman(str_arg)
+    print('same_eye')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place('first_week')
