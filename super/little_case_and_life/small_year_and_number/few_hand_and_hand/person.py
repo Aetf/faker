019799bc5b0d3f332a5d0cee84055de206bfec96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 13:17:32 -0400

    "rm: give super/little_case_and_life/small_year_and_number/few_hand_and_hand/person.py, super/person_or_day/thing/early_world.py or super/important_part/try_work/company/big_time/bad_child/time_and_early_place.py into next year"

---
+++ super/little_case_and_life/small_year_and_number/few_hand_and_hand/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    first_part_or_government(str_arg)
+    print('long_number')
+
+def first_part_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('group_and_case')
---
+++ super/person_or_day/thing/early_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_year(str_arg):
+    child(str_arg)
+    print('take_high_fact')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_year('little_fact')
