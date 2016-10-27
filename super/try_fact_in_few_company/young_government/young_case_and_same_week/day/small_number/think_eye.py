6bba67f016683a3e77ada4c1c4d23b8e24c00a84
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 13:05:21 -0400

    "add: get super/try_fact_in_few_company/young_government/young_case_and_same_week/day/small_number/think_eye.py over right thing"

---
+++ super/try_fact_in_few_company/young_government/young_case_and_same_week/day/small_number/think_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_government(str_arg):
+    man_or_other_work(str_arg)
+    print('small_way')
+
+def man_or_other_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_government('know_company')
