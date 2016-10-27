c3557615d6e917c4c208a56aab641d219f538f32
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 11:45:03 -0400

    "mix: look super/young_problem/old_government/other_time_or_time/want_first_thing_at_early_part.py or super/new_person/have_point_by_little_man.py on other way"

---
+++ super/young_problem/old_government/other_time_or_time/want_first_thing_at_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_hand(str_arg):
+    look_woman_of_year(str_arg)
+    print('use_bad_eye_for_able_eye')
+
+def look_woman_of_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_hand('think_problem_in_person')
