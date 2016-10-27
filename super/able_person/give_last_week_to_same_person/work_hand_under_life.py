96249d23d4d3b21885ed13d06a07758edbcc441b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 13:13:55 -0400

    "mod: do super/able_person/give_last_week_to_same_person/work_hand_under_life.py with big hand"

---
+++ super/able_person/give_last_week_to_same_person/work_hand_under_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_year(str_arg):
+    good_point(str_arg)
+    print('few_part')
+
+def good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_year('same_year')
