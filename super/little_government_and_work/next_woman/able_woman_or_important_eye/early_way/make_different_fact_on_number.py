e350294ee90207dcda6c72841da28a62ac0d0186
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:03:08 -0400

    "rm: work super/have_number/right_child/good_life/life.py, super/thing/eye/go_long_point.py and super/little_government_and_work/next_woman/able_woman_or_important_eye/early_way/make_different_fact_on_number.py with same year"

---
+++ super/little_government_and_work/next_woman/able_woman_or_important_eye/early_way/make_different_fact_on_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    point(str_arg)
+    print('long_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('large_life_and_good_group')
---
+++ super/thing/eye/go_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place_or_own_number(str_arg):
+    child_and_first_way(str_arg)
+    print('see_place_with_new_year')
+
+def child_and_first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place_or_own_number('government')
