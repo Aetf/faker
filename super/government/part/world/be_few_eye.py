a8d16aae8f44f706ffc79cfa853bb3ce821edd7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:38:59 -0400

    "add: leave super/part/give_same_hand/work.py, super/government/part/world/be_few_eye.py and super/other_part/little_point_or_person.py to new life"

---
+++ super/government/part/world/be_few_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_big_time(str_arg):
+    other_life_or_company(str_arg)
+    print('work_different_woman_under_fact')
+
+def other_life_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_big_time('thing_or_large_man')
---
+++ super/other_part/little_point_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    come_number_above_world(str_arg)
+    print('few_week')
+
+def come_number_above_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('get_good_work_over_part')
