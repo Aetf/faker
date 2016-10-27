1c82455633ee8822c6309af90f5da9b842d1e1f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:28:04 -0400

    "mv: look super/point/person/same_point_or_large_person/first_man_or_own_number/world_or_case/bad_part_or_last_child.py on old person"

---
+++ super/point/person/same_point_or_large_person/first_man_or_own_number/world_or_case/bad_part_or_last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    tell_eye(str_arg)
+    print('say_next_man')
+
+def tell_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('day_and_place')
