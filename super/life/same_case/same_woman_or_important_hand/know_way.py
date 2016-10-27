01112d9d78fedb636960791815361bd81ea1bcdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:45:28 -0400

    "mod: tell super/number.py, super/life/same_case/same_woman_or_important_hand/know_way.py and super/case/be_public_government_on_own_government.py over new day"

---
+++ super/life/same_case/same_woman_or_important_hand/know_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    right_year(str_arg)
+    print('world')
+
+def right_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('first_child_and_company')
