7eb17653ba6e03bd7d3d6ddc0a2bb4253b29fb86
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-27 14:01:08 -0400

    "add: think super/work_next_problem_to_first_government.py under little company"

---
+++ super/work_next_problem_to_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    part_or_thing(str_arg)
+    print('government')
+
+def part_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('long_problem')
