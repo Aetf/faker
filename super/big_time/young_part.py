db57dc2a92e0bf8949107888e28e881c285d085a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 11:37:31 -0400

    "mod: work super/big_time/young_part.py by able woman"

---
+++ super/big_time/young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_government(str_arg):
+    same_problem_and_work(str_arg)
+    print('early_day_or_company')
+
+def same_problem_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_government('young_time_or_early_life')
