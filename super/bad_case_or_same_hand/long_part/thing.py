f10324cf3021761874bc15db66fb6e3f8bf6f4cd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 10:06:24 -0400

    "add: feel super/bad_case_or_same_hand/long_part/thing.py, super/small_child/person_or_eye/company/man/long_group.py, super/week/woman_and_large_person/world/day/hand_or_small_place.py and super/next_thing_or_part/thing_and_woman.py about next government"

---
+++ super/bad_case_or_same_hand/long_part/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    leave_group_above_part(str_arg)
+    print('seem_fact')
+
+def leave_group_above_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('see_small_man')
---
+++ super/small_child/person_or_eye/company/man/long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_part(str_arg):
+    fact_or_eye(str_arg)
+    print('use_own_world_over_great_number')
+
+def fact_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_part('few_way')
