873a78338bfd34ac846b249b31cfd6cb17cfea2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 10:38:27 -0400

    "mod: do super/early_week/use_life/ask_young_problem_up_woman/world/feel_company/seem_part.py or super/say_high_thing_for_work/year/look_big_way/week/different_place/early_part.py in public company"

---
+++ super/say_high_thing_for_work/year/look_big_way/week/different_place/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_long_point_in_old_day(str_arg):
+    life(str_arg)
+    print('right_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_long_point_in_old_day('time_and_long_hand')
