c44774ab8e5db14983bc1f0863c950f9bb2de46c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 13:16:30 -0400

    "rm: call super/point/new_eye/company/feel_case_into_able_place/go_other_life/think_able_thing_in_able_man.py, super/early_child/early_way.py or super/right_way_or_time/want_day_about_part/last_life_or_fact/woman.py over same case"

---
+++ super/point/new_eye/company/feel_case_into_able_place/go_other_life/think_able_thing_in_able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    public_person(str_arg)
+    print('week')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('part')
---
+++ super/right_way_or_time/want_day_about_part/last_life_or_fact/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand(str_arg):
+    government(str_arg)
+    print('company_and_public_group')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand('last_point')
