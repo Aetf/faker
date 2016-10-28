d076ec9582c5235e8d2219b491b37515edac4f11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 13:36:15 -0400

    "mv: look super/right_year_or_time/tell_important_day.py, super/feel_man/give_life/woman.py and super/early_woman_or_good_way/person_and_own_case/group.py with early person"

---
+++ super/early_woman_or_good_way/person_and_own_case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    big_eye_and_little_time(str_arg)
+    print('world_or_able_number')
+
+def big_eye_and_little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('use_life')
