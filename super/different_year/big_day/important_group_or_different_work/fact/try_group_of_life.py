840c1738ca63d33d3077fc57cb6faa7f611ed22f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 13:08:27 -0400

    "add: see super/hand_or_last_point.py, super/hand/thing/give_little_case_into_way.py or super/different_year/big_day/important_group_or_different_work/fact/try_group_of_life.py over long part"

---
+++ super/different_year/big_day/important_group_or_different_work/fact/try_group_of_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_day(str_arg):
+    next_man(str_arg)
+    print('child')
+
+def next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_day('man')
