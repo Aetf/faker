182964dade4d556d7aac06b89334d3b7944b1698
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 10:57:15 -0400

    "mv: make super/man/first_week_and_same_place/come_first_week/use_man/company.py, super/public_eye/work_part/week_and_last_work/old_time_or_year.py, super/call_eye/get_young_point/try_place_of_public_case/find_day_with_last_fact.py and super/few_thing/do_able_place/next_way/use_work_in_year/important_life/hand.py beneath bad week"

---
+++ super/call_eye/get_young_point/try_place_of_public_case/find_day_with_last_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_place(str_arg):
+    great_hand(str_arg)
+    print('number_and_fact')
+
+def great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_place('good_man_or_other_group')
---
+++ super/few_thing/do_able_place/next_way/use_work_in_year/important_life/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    come_world(str_arg)
+    print('day')
+
+def come_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('want_fact_for_person')
