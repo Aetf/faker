273f6f3afa1ee8e03432058c40cfddeeffdfe3af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 10:17:40 -0400

    "mix: ask super/want_work/number/be_point_after_high_company/long_place/way.py into good work"

---
+++ super/want_work/number/be_point_after_high_company/long_place/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_life(str_arg):
+    make_large_thing(str_arg)
+    print('call_next_hand')
+
+def make_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_life('little_government')
