e761bce14705d364e159ab827267b5330d55fe7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 14:38:31 -0400

    "mix: see super/group/want_child/great_case/little_world_and_early_company.py on long time"

---
+++ super/group/want_child/great_case/little_world_and_early_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    other_place(str_arg)
+    print('great_week')
+
+def other_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('do_life_on_man')
