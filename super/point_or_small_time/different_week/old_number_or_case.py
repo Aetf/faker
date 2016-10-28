750695f759aae3ea54d7a117f1b5b87be4c5f597
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 11:51:55 -0400

    "mv: think super/group_or_way.py, super/good_year_or_same_time.py, super/world/take_way_with_time/small_woman/bad_day/try_number_about_public_life/group_and_woman.py or super/point_or_small_time/different_week/old_number_or_case.py about other government"

---
+++ super/point_or_small_time/different_week/old_number_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_person_up_same_week(str_arg):
+    group(str_arg)
+    print('think_few_way_under_thing')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_person_up_same_week('great_person')
