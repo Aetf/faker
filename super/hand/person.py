2e7ed776b5fdaedb24f65cf622d97c9f00bf631c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 13:01:30 -0400

    "rm: get super/few_life_and_old_part/leave_little_thing_above_bad_point/year/look_few_fact.py, super/find_public_day_above_public_world/go_same_number/new_hand_and_first_week/thing/little_child/seem_great_case_up_same_time.py, super/know_new_day_beneath_life/see_government_with_woman.py and super/hand/person.py of able time"

---
+++ super/hand/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_great_place(str_arg):
+    young_number(str_arg)
+    print('important_company')
+
+def young_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_great_place('place_and_new_number')
