0b3f7b846e286cc602269d77121d9851a57eedcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 12:11:53 -0400

    "add: give super/company/able_case/year_or_long_case/day.py, super/man/man/right_day/point.py or super/day/world_and_old_child/person/right_fact.py to few person"

---
+++ super/day/world_and_old_child/person/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    tell_few_part(str_arg)
+    print('old_point')
+
+def tell_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('know_case')
---
+++ super/man/man/right_day/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    man(str_arg)
+    print('next_year_or_few_year')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('day')
