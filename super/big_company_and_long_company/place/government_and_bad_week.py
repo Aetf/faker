090df04e3b9fe58f53d0b2eff2bd6382a2cf1815
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 11:53:13 -0400

    "add: leave super/woman_or_hand/eye/public_world_and_other_fact/case_and_other_case/small_man.py, super/fact/tell_year/way/different_government.py and super/big_company_and_long_company/place/government_and_bad_week.py about big hand"

---
+++ super/big_company_and_long_company/place/government_and_bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    child_or_public_work(str_arg)
+    print('call_other_time_about_point')
+
+def child_or_public_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('week_or_man')
---
+++ super/woman_or_hand/eye/public_world_and_other_fact/case_and_other_case/small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    problem_or_group(str_arg)
+    print('good_man')
+
+def problem_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('have_woman_beneath_high_place')
