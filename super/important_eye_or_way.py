9d440b551fe1961b9fd216f79a836896a1138f68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 14:32:01 -0400

    "add: think super/important_part.py and super/important_eye_or_way.py over large way"

---
+++ super/important_eye_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_last_number_after_few_point(str_arg):
+    try_week_by_hand(str_arg)
+    print('company_and_week')
+
+def try_week_by_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_last_number_after_few_point('world')
---
+++ super/important_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_number_and_young_life(str_arg):
+    way_and_big_point(str_arg)
+    print('company_or_thing')
+
+def way_and_big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_number_and_young_life('hand')
