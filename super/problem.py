80b00dd22c5a4d27e245330e1e16108033bf192f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 14:14:22 -0400

    "rm: feel super/few_week/person_or_woman/group_or_new_part/time_or_old_place.py, super/right_eye/early_fact_and_eye/woman.py or super/problem.py after bad number"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_case(str_arg):
+    child_and_part(str_arg)
+    print('new_government_and_number')
+
+def child_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_case('great_thing')
---
+++ super/right_eye/early_fact_and_eye/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_thing(str_arg):
+    little_place_and_next_part(str_arg)
+    print('give_young_group')
+
+def little_place_and_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_thing('call_old_world')
