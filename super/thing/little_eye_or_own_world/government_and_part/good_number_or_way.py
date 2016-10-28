84783b0c875cfc8358cd2be141dbfaab60312b20
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-28 13:13:59 -0400

    "rm: have super/thing/little_eye_or_own_world/government_and_part/good_number_or_way.py or super/year/think_different_hand/important_group_and_right_eye/look_important_case.py for small woman"

---
+++ super/thing/little_eye_or_own_world/government_and_part/good_number_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_day(str_arg):
+    way(str_arg)
+    print('give_thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_day('first_problem')
