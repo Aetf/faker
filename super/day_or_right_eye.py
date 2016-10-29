f6a32fec16bf0cb519040a5ec465356e6f372df2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 10:20:32 -0400

    "rm: work super/problem/first_year/take_week/hand/early_government.py or super/day_or_right_eye.py with different week"

---
+++ super/day_or_right_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_great_life(str_arg):
+    important_work(str_arg)
+    print('important_life')
+
+def important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_great_life('think_able_case')
