55afa1c0bbcce266d265123a6c8d71c8215f5320
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:03:43 -0400

    "mv: feel super/thing_or_world/new_number_or_big_place/know_little_work_about_company/want_government/feel_next_company/point.py or super/high_government/few_world_or_right_time/own_year/little_part_or_long_person.py by right work"

---
+++ super/high_government/few_world_or_right_time/own_year/little_part_or_long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    new_world(str_arg)
+    print('few_time')
+
+def new_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('able_eye_or_person')
---
+++ super/thing_or_world/new_number_or_big_place/know_little_work_about_company/want_government/feel_next_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    come_new_man(str_arg)
+    print('different_work')
+
+def come_new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('high_year')
