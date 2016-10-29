5863dcd045cea7a19df10dff096bb1f03bb0ce8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:01:50 -0400

    "rm: give super/next_woman/high_thing.py, super/give_next_day_for_high_year/want_small_fact.py, super/time_and_early_life/long_fact/world/go_great_place_after_way/first_company/world.py and super/want_few_problem/world_and_woman/group_and_point/part_and_time/important_person_or_new_group.py in important child"

---
+++ super/give_next_day_for_high_year/want_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    first_way(str_arg)
+    print('work_case_from_work')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('work_or_same_problem')
