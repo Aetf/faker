044267d638ef1f9379f8f123b2c4a5e841a03732
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:33:50 -0400

    "add: look super/world/group/place/world/world.py up little fact"

---
+++ super/world/group/place/world/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_hand(str_arg):
+    long_case(str_arg)
+    print('work_part_with_world')
+
+def long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_hand('government_and_large_place')
