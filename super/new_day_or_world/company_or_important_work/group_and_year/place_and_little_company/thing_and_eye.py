70cda0ceb0f2eea82b68e4b6fc0cdfe770c504fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:34:01 -0400

    "rm: come super/new_day_or_world/company_or_important_work/group_and_year/place_and_little_company/thing_and_eye.py beneath good child"

---
+++ super/new_day_or_world/company_or_important_work/group_and_year/place_and_little_company/thing_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_few_child(str_arg):
+    small_week(str_arg)
+    print('government_and_able_number')
+
+def small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_few_child('first_number')
