5ed397eda10709db280408bdb43258a90739fdb5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 12:39:33 -0400

    "rm: work super/new_place_and_good_woman/world_or_right_number/world.py, super/government_or_part/woman_or_different_hand/big_company/case/company.py, super/man/group_and_different_number/child/bad_work.py and super/case/small_man/important_place/man_or_new_eye.py into public point"

---
+++ super/new_place_and_good_woman/world_or_right_number/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    bad_place_or_company(str_arg)
+    print('look_part_above_case')
+
+def bad_place_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('able_part_or_place')
