cd9988be4b2c7ca2c8b8a5726ee64155a5a700d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 10:31:36 -0400

    "mv: leave super/want_own_person_about_woman/find_own_eye/day/year.py and super/big_case/feel_work_into_government/number/hand_and_next_life/few_woman/new_work.py from young woman"

---
+++ super/want_own_person_about_woman/find_own_eye/day/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life(str_arg):
+    call_man_under_early_life(str_arg)
+    print('part')
+
+def call_man_under_early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life('day')
