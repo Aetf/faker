c3e0e3f624266121a6d4c4fac80cd683c2e33f22
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 11:04:57 -0400

    "rm: use super/week/same_way/find_small_person/be_man_for_last_part.py and super/point/child/find_small_case_with_thing.py from old week"

---
+++ super/point/child/find_small_case_with_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_time(str_arg):
+    life(str_arg)
+    print('hand')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_time('same_way_and_time')
---
+++ super/week/same_way/find_small_person/be_man_for_last_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem(str_arg):
+    important_company_or_day(str_arg)
+    print('next_case')
+
+def important_company_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem('public_place')
