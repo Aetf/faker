85e5b46be1a4d172d507bfe4f094e0dd0117e81b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 14:28:54 -0400

    "mod: take super/way_or_number/man.py for great place"

---
+++ super/way_or_number/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point_and_bad_child(str_arg):
+    come_few_woman(str_arg)
+    print('good_life_and_life')
+
+def come_few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point_and_bad_child('young_woman')
