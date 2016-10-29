dabb9647280de75c95db748559710c364bdaf6c3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:58:55 -0400

    "mix: know super/thing/see_time/fact/feel_same_number_over_young_way/year.py above long work"

---
+++ super/thing/see_time/fact/feel_same_number_over_young_way/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_point(str_arg):
+    child(str_arg)
+    print('week_and_able_man')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_point('say_large_man')
