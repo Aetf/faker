e461c6bd225cca348d672ac0c5cf021fbf110451
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 11:25:57 -0400

    "rm: say super/government.py, super/time_and_other_person/small_eye.py or super/take_government_in_problem/find_good_day_beneath_next_eye/world/hand/life_or_next_child/high_way.py by young case"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_big_week(str_arg):
+    next_life(str_arg)
+    print('want_point_to_few_week')
+
+def next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_big_week('government')
