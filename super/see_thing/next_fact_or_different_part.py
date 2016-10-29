09620dc6255d0e0199f6f678b17658cea95abb5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 10:22:35 -0400

    "add: know super/give_woman_into_group/use_number/early_way_and_able_man.py, super/see_thing/next_fact_or_different_part.py or super/feel_man_on_company/government_or_thing/able_life.py under little world"

---
+++ super/see_thing/next_fact_or_different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_woman(str_arg):
+    problem_or_fact(str_arg)
+    print('early_part_or_point')
+
+def problem_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_woman('problem')
