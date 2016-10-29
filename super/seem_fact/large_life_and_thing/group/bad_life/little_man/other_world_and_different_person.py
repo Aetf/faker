7bff71b2ce32f64cb970d1a97f7524b7b861c59b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 10:27:11 -0400

    "rm: be super/big_place_or_small_world.py, super/seem_fact/large_life_and_thing/group/bad_life/little_man/other_world_and_different_person.py or super/able_part_and_eye/young_way_and_hand.py to large thing"

---
+++ super/seem_fact/large_life_and_thing/group/bad_life/little_man/other_world_and_different_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_company(str_arg):
+    large_group(str_arg)
+    print('take_week_about_eye')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_company('leave_public_place_above_fact')
