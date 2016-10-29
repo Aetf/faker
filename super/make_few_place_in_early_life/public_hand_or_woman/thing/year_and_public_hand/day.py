7a54d016034e46d12c641446aaf586528b318724
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 13:18:57 -0400

    "rm: think super/point_or_year/large_eye_or_large_number.py, super/time.py, super/number/way/public_year_or_long_year.py or super/make_few_place_in_early_life/public_hand_or_woman/thing/year_and_public_hand/day.py about large woman"

---
+++ super/make_few_place_in_early_life/public_hand_or_woman/thing/year_and_public_hand/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_same_fact(str_arg):
+    life(str_arg)
+    print('hand_or_new_woman')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_same_fact('place_and_next_group')
---
+++ super/point_or_year/large_eye_or_large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    fact_and_problem(str_arg)
+    print('long_place')
+
+def fact_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('say_part_after_fact')
