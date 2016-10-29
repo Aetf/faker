85cf2f94faabde2df56961557deab3d87345d4bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 14:46:07 -0400

    "mod: work super/large_year/small_day/give_day_into_large_fact/little_world_or_young_man/government/week.py and super/get_public_day_by_person/know_own_hand/do_life/first_work/say_day_above_time.py with young group"

---
+++ super/large_year/small_day/give_day_into_large_fact/little_world_or_young_man/government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_person(str_arg):
+    high_person(str_arg)
+    print('group')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_person('person_and_man')
