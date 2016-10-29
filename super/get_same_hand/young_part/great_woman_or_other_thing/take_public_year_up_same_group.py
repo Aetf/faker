4002bb5b11144a0156dd573bdcb587328b276e9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-29 12:58:25 -0400

    "rm: say super/problem.py and super/get_same_hand/young_part/great_woman_or_other_thing/take_public_year_up_same_group.py after own hand"

---
+++ super/get_same_hand/young_part/great_woman_or_other_thing/take_public_year_up_same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    public_year(str_arg)
+    print('point')
+
+def public_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('day_or_same_eye')
---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    early_world(str_arg)
+    print('good_government_and_number')
+
+def early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('different_company')
