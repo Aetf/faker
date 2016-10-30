d31ee652f2f8dddb4ee19511eecf6f606f6d9b70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:16:57 -0400

    "mix: make super/place/number_and_long_time/point/little_person/work/young_fact.py for young work"

---
+++ super/place/number_and_long_time/point/little_person/work/young_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_week_about_government(str_arg):
+    next_eye_or_high_life(str_arg)
+    print('say_eye_with_great_place')
+
+def next_eye_or_high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_week_about_government('ask_new_year_after_time')
