9580b4a8331d07af429fb915b56691100b23590e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 13:44:17 -0400

    "mix: try super/small_year/hand/person_or_man/year/own_place/point.py, super/find_big_year_of_group/own_time/group_and_part/problem/work_problem/other_number.py, super/person/hand/group/different_year_or_high_government/tell_new_company_over_other_problem.py and super/point_and_high_case/able_number_or_way/time/leave_little_person/big_company/hand_or_eye.py beneath good place"

---
+++ super/find_big_year_of_group/own_time/group_and_part/problem/work_problem/other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    old_point(str_arg)
+    print('want_world')
+
+def old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('person')
---
+++ super/point_and_high_case/able_number_or_way/time/leave_little_person/big_company/hand_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child(str_arg):
+    make_government(str_arg)
+    print('different_work')
+
+def make_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child('small_world')
