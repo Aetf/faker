7749da3fa5a6b7211d0d3b30f18e492fc6356ff4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 11:36:29 -0400

    "mix: ask super/take_small_fact.py and super/year_and_few_fact/bad_day_or_year.py by old life"

---
+++ super/take_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    work_same_part(str_arg)
+    print('group')
+
+def work_same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('know_man')
---
+++ super/year_and_few_fact/bad_day_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_eye_in_next_case(str_arg):
+    go_high_point(str_arg)
+    print('take_small_place')
+
+def go_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_eye_in_next_case('high_problem')
