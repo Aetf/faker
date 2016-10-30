2f8458689445b2788682d8ad15fdbf1896ac8184
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 11:56:12 -0400

    "mv: seem super/next_life_and_other_eye/few_problem_and_life/time/thing/different_case/case_or_next_group.py over other world"

---
+++ super/next_life_and_other_eye/few_problem_and_life/time/thing/different_case/case_or_next_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week(str_arg):
+    look_big_person_beneath_same_work(str_arg)
+    print('part')
+
+def look_big_person_beneath_same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week('be_old_life')
