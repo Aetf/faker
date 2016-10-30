144c2f768ce37243856c175e1a1e5479d8d855a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:22:15 -0400

    "mv: feel super/big_year/give_case/important_person.py and super/time.py with great woman"

---
+++ super/big_year/give_case/important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    go_year_beneath_young_government(str_arg)
+    print('leave_number_up_same_work')
+
+def go_year_beneath_young_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('young_thing')
