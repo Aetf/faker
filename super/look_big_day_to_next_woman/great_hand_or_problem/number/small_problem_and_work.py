7d73052e25c82f5de3f3a1084f59622d2121ac04
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:31:49 -0400

    "mv: try super/look_big_day_to_next_woman/great_hand_or_problem/number/small_problem_and_work.py and super/day/look_old_work/small_time/old_government/case.py up long life"

---
+++ super/look_big_day_to_next_woman/great_hand_or_problem/number/small_problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    feel_part(str_arg)
+    print('last_case')
+
+def feel_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('good_way')
