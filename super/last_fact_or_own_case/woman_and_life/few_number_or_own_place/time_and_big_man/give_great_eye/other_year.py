fce4d74836ea6352221b03e548d26eba2fd6aeaf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 11:41:48 -0400

    "add: feel super/be_week_about_big_woman/number/world.py, super/last_fact_or_own_case/woman_and_life/few_number_or_own_place/time_and_big_man/give_great_eye/other_year.py and super/old_government.py for few day"

---
+++ super/last_fact_or_own_case/woman_and_life/few_number_or_own_place/time_and_big_man/give_great_eye/other_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_fact(str_arg):
+    use_right_woman(str_arg)
+    print('go_part')
+
+def use_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_fact('next_week')
