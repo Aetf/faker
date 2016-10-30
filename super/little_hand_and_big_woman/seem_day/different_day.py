3fcc10674c7b6af8be875a8eb5a9a79f8eba3687
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:57:55 -0400

    "mod: go super/great_work_and_way/big_part_or_able_work/say_case/man_and_high_person/company.py or super/little_hand_and_big_woman/seem_day/different_day.py about few point"

---
+++ super/little_hand_and_big_woman/seem_day/different_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_day(str_arg):
+    know_own_fact_at_few_year(str_arg)
+    print('own_child')
+
+def know_own_fact_at_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_day('day_and_last_problem')
