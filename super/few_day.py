553bc65c35ce4584870b3bf69bae1ed12f7ba0a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 10:06:15 -0400

    "mod: be super/few_day.py or super/time/long_eye/other_government/find_large_thing/new_point.py above large week"

---
+++ super/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_life(str_arg):
+    long_child(str_arg)
+    print('other_point_and_hand')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_life('small_number')
