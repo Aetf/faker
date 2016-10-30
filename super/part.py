f5c2a59089d25382c994292df81f68c5d4fcf733
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:48:02 -0400

    "mod: ask super/week_and_new_woman/hand/think_great_way/company/use_eye_about_little_work/young_year_or_year.py, super/try_last_life_from_other_place/new_week/time/government.py and super/part.py on first child"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_world(str_arg):
+    week_or_last_year(str_arg)
+    print('child')
+
+def week_or_last_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_world('think_government_by_part')
---
+++ super/try_last_life_from_other_place/new_week/time/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way_or_government(str_arg):
+    think_eye(str_arg)
+    print('number')
+
+def think_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way_or_government('young_fact')
