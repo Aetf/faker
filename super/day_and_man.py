909ddf65ebaf2a0f31a7503a7bb6733902078741
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 11:38:40 -0400

    "add: come super/problem/big_world_and_last_company/problem_or_part/work_child.py, super/day_and_man.py, super/small_place.py and super/woman/few_world/high_problem/number.py of different government"

---
+++ super/day_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_place(str_arg):
+    high_woman(str_arg)
+    print('other_week')
+
+def high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_place('place_or_great_hand')
---
+++ super/woman/few_world/high_problem/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_large_year(str_arg):
+    public_point(str_arg)
+    print('case_or_fact')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_large_year('place')
