7354a67e64770611af72649423a20d8baf1c71d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 10:00:53 -0400

    "mix: call super/get_year/new_government/time_and_good_woman/public_point/way/good_thing.py or super/man/time/able_way.py for same eye"

---
+++ super/man/time/able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_first_man(str_arg):
+    seem_day(str_arg)
+    print('young_case_and_case')
+
+def seem_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_first_man('group')
