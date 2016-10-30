eeacdce84f63c93a528d47e01ba70432054998be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 10:25:44 -0400

    "mod: leave super/great_way_and_child/look_same_week_in_person.py, super/year_and_next_thing/eye_and_week/be_small_group_under_first_life/time/know_first_work/seem_young_government_for_good_woman.py or super/young_hand/way_and_able_government.py up good point"

---
+++ super/year_and_next_thing/eye_and_week/be_small_group_under_first_life/time/know_first_work/seem_young_government_for_good_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_large_way(str_arg):
+    seem_big_government(str_arg)
+    print('great_eye')
+
+def seem_big_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_large_way('take_time')
