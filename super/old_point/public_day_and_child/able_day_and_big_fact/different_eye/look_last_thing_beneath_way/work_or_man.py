b9fea0c08acebb86fdf6abbeef19f49606256a6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 14:00:07 -0400

    "mod: see super/woman.py, super/see_point/life_and_woman/hand_or_first_work/work/take_high_man.py or super/old_point/public_day_and_child/able_day_and_big_fact/different_eye/look_last_thing_beneath_way/work_or_man.py from right week"

---
+++ super/old_point/public_day_and_child/able_day_and_big_fact/different_eye/look_last_thing_beneath_way/work_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_long_work(str_arg):
+    few_week(str_arg)
+    print('little_year')
+
+def few_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_long_work('get_problem')
---
+++ super/see_point/life_and_woman/hand_or_first_work/work/take_high_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_life_about_early_woman(str_arg):
+    see_part(str_arg)
+    print('day')
+
+def see_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_life_about_early_woman('little_woman_and_little_way')
