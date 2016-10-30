ce52165baa86272a2a3de38e675409b20bcf704c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 10:25:17 -0400

    "mix: use super/good_man/last_hand/fact/other_work/world/take_early_time.py from first part"

---
+++ super/good_man/last_hand/fact/other_work/world/take_early_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    bad_case(str_arg)
+    print('case')
+
+def bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('eye')
