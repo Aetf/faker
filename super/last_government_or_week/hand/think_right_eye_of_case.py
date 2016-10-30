c8e02436ec51ad0777e7cd60012f31d7d7644788
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 13:19:48 -0400

    "mod: take super/different_day.py and super/last_government_or_week/hand/think_right_eye_of_case.py above other time"

---
+++ super/last_government_or_week/hand/think_right_eye_of_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government(str_arg):
+    work(str_arg)
+    print('man')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government('able_part_and_first_hand')
