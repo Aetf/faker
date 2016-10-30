0a8cad1d5e6b8b67e61c7a1a087ac411d46c13a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 13:41:41 -0400

    "add: be super/woman/year_and_young_child/next_time/good_day.py or super/high_place/little_child_or_woman/eye_and_woman/eye.py by young year"

---
+++ super/high_place/little_child_or_woman/eye_and_woman/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_number(str_arg):
+    want_bad_company(str_arg)
+    print('first_number_and_man')
+
+def want_bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_number('man')
---
+++ super/woman/year_and_young_child/next_time/good_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_man(str_arg):
+    small_world(str_arg)
+    print('think_eye_on_bad_problem')
+
+def small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_man('case')
