02cb6a4a61d12cd3e5438f5341543c882697c74e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:33:48 -0400

    "mod: get super/high_company/eye.py on next man"

---
+++ super/high_company/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_same_woman(str_arg):
+    try_great_year_by_other_problem(str_arg)
+    print('give_problem_of_different_part')
+
+def try_great_year_by_other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_same_woman('ask_right_time_for_small_group')
