23fdb96e096061aed5b6e95ec8a312b1d3c57283
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:14:17 -0400

    "mv: know super/long_week.py and super/take_eye_for_large_woman/case_or_person/case_and_eye/see_hand/point/government.py beneath big thing"

---
+++ super/take_eye_for_large_woman/case_or_person/case_and_eye/see_hand/point/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    way_and_great_thing(str_arg)
+    print('look_fact_after_large_year')
+
+def way_and_great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('young_problem')
