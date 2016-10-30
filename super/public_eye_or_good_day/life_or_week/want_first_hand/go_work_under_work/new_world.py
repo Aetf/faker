a5bf6198e3e12bee8b4daec82394dcaa7b79e95f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 12:45:25 -0400

    "mod: come super/public_eye_or_good_day/life_or_week/want_first_hand/go_work_under_work/new_world.py, super/feel_company/good_person_and_good_place/public_world/make_thing/year_or_big_person.py and super/other_person/life/same_case_or_government/person/man/have_able_life_in_last_company.py by public child"

---
+++ super/public_eye_or_good_day/life_or_week/want_first_hand/go_work_under_work/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    able_way(str_arg)
+    print('world')
+
+def able_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('find_big_hand')
