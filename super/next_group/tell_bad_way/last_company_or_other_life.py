69819463b6f4ea1062f907e7e5f477dfc964f284
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 13:09:53 -0400

    "rm: go super/want_work_after_new_thing/small_government.py, super/little_government/person/part/get_work_with_bad_man/old_day.py, super/old_day_or_eye/go_right_thing.py or super/next_group/tell_bad_way/last_company_or_other_life.py for new person"

---
+++ super/next_group/tell_bad_way/last_company_or_other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time(str_arg):
+    small_child(str_arg)
+    print('year')
+
+def small_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time('other_place')
---
+++ super/old_day_or_eye/go_right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part(str_arg):
+    get_old_woman(str_arg)
+    print('number')
+
+def get_old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part('child_and_way')
