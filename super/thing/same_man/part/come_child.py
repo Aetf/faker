84434ffa13bd1bc2e33c8670b6153d8d9ac4bbb6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-30 10:08:14 -0400

    "add: come super/thing/same_man/part/come_child.py by right fact"

---
+++ super/thing/same_man/part/come_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_hand(str_arg):
+    big_fact_or_company(str_arg)
+    print('eye')
+
+def big_fact_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_hand('small_eye')
