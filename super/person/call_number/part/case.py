d19051efc82a1b0f6e27f4379701dc63a4adcd01
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:30:19 -0400

    "rm: think super/right_group/say_person_under_small_hand/call_few_child/early_week_and_person.py and super/person/call_number/part/case.py into able part"

---
+++ super/person/call_number/part/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    early_person(str_arg)
+    print('be_thing')
+
+def early_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('first_way')
