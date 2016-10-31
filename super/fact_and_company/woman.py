b30492bbbf52068367b0cfc695aa1ef8bc1b73f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:40:04 -0400

    "add: call super/fact_and_company/woman.py, super/high_life_and_company.py or super/person/bad_place.py up little fact"

---
+++ super/fact_and_company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem_or_same_government(str_arg):
+    child(str_arg)
+    print('get_work')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem_or_same_government('number')
---
+++ super/high_life_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    young_hand_or_problem(str_arg)
+    print('high_government_and_other_part')
+
+def young_hand_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('part_or_right_number')
