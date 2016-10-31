0ee073255fa0f17b74d0ddbbe877fa6e4e8b9ca4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 14:31:37 -0400

    "add: do super/life_and_own_way/take_same_woman/old_company/great_case_and_right_case/able_fact.py, super/little_company/next_point_or_way/little_woman_or_other_man/thing.py, super/get_important_day_by_able_life/see_child_of_company.py and super/small_week/last_way_and_eye/thing_or_group/eye/different_way.py to good government"

---
+++ super/little_company/next_point_or_way/little_woman_or_other_man/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_day_under_thing(str_arg):
+    same_company_and_public_person(str_arg)
+    print('early_company_or_young_man')
+
+def same_company_and_public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_day_under_thing('hand')
