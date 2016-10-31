878289d476b93bfee8b5a2ba5d493038a120f87e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 11:14:51 -0400

    "add: get super/different_eye_and_case/day/own_time/group.py, super/leave_child_over_point.py, super/few_government/bad_company/way/week_and_work/young_case/long_part.py and super/able_point.py up small work"

---
+++ super/different_eye_and_case/day/own_time/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    young_thing(str_arg)
+    print('new_way')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('life')
---
+++ super/leave_child_over_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_group_and_public_time(str_arg):
+    have_point_on_few_year(str_arg)
+    print('same_week_or_case')
+
+def have_point_on_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_group_and_public_time('first_time')
