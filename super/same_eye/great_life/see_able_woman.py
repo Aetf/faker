b269d5245e9f524e44f63b1ba3d7cac41bae9ad3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:01:20 -0400

    "rm: see super/big_year/come_right_way.py, super/same_eye/great_life/see_able_woman.py and super/see_thing/important_way/look_time_under_young_company/work_different_time/use_little_eye_up_long_woman.py on important work"

---
+++ super/same_eye/great_life/see_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number_or_part(str_arg):
+    young_group_and_different_person(str_arg)
+    print('few_time_or_other_work')
+
+def young_group_and_different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number_or_part('thing_and_day')
