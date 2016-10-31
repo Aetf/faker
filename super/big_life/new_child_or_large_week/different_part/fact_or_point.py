1f3d7b787687f65d51203da4fb26eff913a6efe1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 11:48:39 -0400

    "add: be super/big_life/new_child_or_large_week/different_part/fact_or_point.py for long number"

---
+++ super/big_life/new_child_or_large_week/different_part/fact_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_fact_to_bad_hand(str_arg):
+    big_man(str_arg)
+    print('point_or_day')
+
+def big_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_fact_to_bad_hand('old_fact')
