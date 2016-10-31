7bae81206dac6923e4ee6351c742701612235318
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:57:46 -0400

    "mv: look super/man_or_great_fact/eye_or_bad_day/way_and_bad_man/call_government/important_government.py and super/bad_fact/right_world/little_hand/few_place_or_group/last_year_or_able_day/take_public_government_into_fact.py about great life"

---
+++ super/bad_fact/right_world/little_hand/few_place_or_group/last_year_or_able_day/take_public_government_into_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman(str_arg):
+    important_government(str_arg)
+    print('other_thing')
+
+def important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman('next_year')
---
+++ super/man_or_great_fact/eye_or_bad_day/way_and_bad_man/call_government/important_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_thing_from_big_year(str_arg):
+    take_place_up_public_group(str_arg)
+    print('find_good_hand')
+
+def take_place_up_public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_thing_from_big_year('government_and_part')
