93c5060989127220240f6a4baeea965baaafde9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 13:42:46 -0400

    "mv: be super/hand/feel_important_work.py, super/great_thing/last_point/place/eye.py, super/fact_or_first_government/case_or_other_work/few_hand/work/right_way.py and super/first_life/work/day/day_and_world/call_hand.py at good fact"

---
+++ super/fact_or_first_government/case_or_other_work/few_hand/work/right_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_year(str_arg):
+    case(str_arg)
+    print('early_way')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_year('bad_day_and_child')
