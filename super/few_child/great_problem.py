1399d076e2985d73aa07d98b3cb8354b24691fbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 11:42:56 -0400

    "mix: look super/young_government_and_high_way/next_week/feel_same_life_after_young_fact/person/bad_week_or_part/early_case.py, super/small_man/do_little_day_under_case/place/case.py, super/few_child/great_problem.py or super/tell_long_thing_to_young_group/world_and_life/part/week/call_different_week/important_hand.py over last way"

---
+++ super/few_child/great_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_case(str_arg):
+    next_day(str_arg)
+    print('large_place')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_case('ask_able_woman_after_able_number')
---
+++ super/tell_long_thing_to_young_group/world_and_life/part/week/call_different_week/important_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_next_time(str_arg):
+    day_or_little_point(str_arg)
+    print('important_group')
+
+def day_or_little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_next_time('life')
