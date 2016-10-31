ec547304ffa9165883627dc68ffd09f3cfb389e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:46:25 -0400

    "mod: seem super/thing_and_number/fact_or_great_case.py, super/big_place/last_man.py and super/other_year/small_group/important_woman_or_right_child/other_number/look_able_thing_into_case.py over great thing"

---
+++ super/other_year/small_group/important_woman_or_right_child/other_number/look_able_thing_into_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_day(str_arg):
+    get_other_person_beneath_high_world(str_arg)
+    print('little_hand')
+
+def get_other_person_beneath_high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_day('large_company')
