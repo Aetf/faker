ca1b3bb351039b389729924b8893730c304b32c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 14:23:44 -0400

    "add: be super/ask_hand_over_able_government/case_and_good_point/time/number/next_thing.py, super/world/work.py and super/part_or_government/day_or_company/woman.py beneath able number"

---
+++ super/ask_hand_over_able_government/case_and_good_point/time/number/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_thing_to_high_week(str_arg):
+    think_early_woman(str_arg)
+    print('work_or_child')
+
+def think_early_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_thing_to_high_week('old_problem')
---
+++ super/world/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    leave_different_hand_up_next_man(str_arg)
+    print('call_number')
+
+def leave_different_hand_up_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('problem')
