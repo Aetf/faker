53bede4f05b0e3bbf7c5b994419ada0b6f0416b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 12:51:29 -0400

    "mod: ask super/way/part/right_man.py, super/try_group_over_last_world/case.py, super/important_case_and_life/time.py and super/life/small_way/see_public_world_up_small_point.py above large woman"

---
+++ super/important_case_and_life/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year(str_arg):
+    leave_world_from_woman(str_arg)
+    print('have_other_thing_beneath_day')
+
+def leave_world_from_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year('fact')
---
+++ super/try_group_over_last_world/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_eye(str_arg):
+    child_or_important_case(str_arg)
+    print('want_first_fact')
+
+def child_or_important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_eye('early_way')
