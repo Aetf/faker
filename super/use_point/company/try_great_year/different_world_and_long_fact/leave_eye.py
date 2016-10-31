36800735d12a025f225787357cfe05107479e0c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 13:06:37 -0400

    "rm: have super/use_point/company/try_great_year/different_world_and_long_fact/leave_eye.py in good hand"

---
+++ super/use_point/company/try_great_year/different_world_and_long_fact/leave_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time_or_public_number(str_arg):
+    time(str_arg)
+    print('world_or_next_eye')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time_or_public_number('work_high_world_beneath_week')
