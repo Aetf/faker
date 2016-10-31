d2023f649fdd29fa0cbdd243613be7f1e5ee0eff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:02:59 -0400

    "rm: say super/early_point_and_great_part.py, super/work_same_government/give_problem/world/tell_year_into_important_woman/own_number/little_eye.py and super/eye_and_few_part/first_woman/week/next_number_or_week/few_week/year.py of old case"

---
+++ super/work_same_government/give_problem/world/tell_year_into_important_woman/own_number/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    point(str_arg)
+    print('own_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('government_or_young_government')
