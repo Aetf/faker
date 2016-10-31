43d0605da6ac8184a65c31bc70346aed2eab824f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:53:48 -0400

    "mv: get super/large_problem/be_last_work/year_and_same_life/other_company_and_fact/large_life_or_time/think_other_world_over_bad_year.py, super/good_day/government_and_time.py and super/own_place/person_or_public_eye/child_and_company/own_case/know_group_from_next_person.py at own woman"

---
+++ super/own_place/person_or_public_eye/child_and_company/own_case/know_group_from_next_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_group(str_arg)
+    print('hand')
+
+def work_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('great_thing_or_other_hand')
