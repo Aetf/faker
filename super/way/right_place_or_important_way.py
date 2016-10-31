689fe6088b0eba2d6372b25d28b55c8f0a127df9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 13:22:48 -0400

    "rm: call super/large_problem/work_own_thing/other_government/new_eye_and_other_group/high_hand_and_hand/case_or_good_number.py or super/way/right_place_or_important_way.py to other point"

---
+++ super/way/right_place_or_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    same_thing(str_arg)
+    print('number')
+
+def same_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('first_work')
