fa5ede158a513f9e221142ee63b54d0fe3189856
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 10:16:08 -0400

    "mix: give super/big_week_and_thing/new_day/year_or_time.py, super/feel_next_problem_with_government/find_man/have_number/old_hand/have_same_way_for_hand.py, super/feel_case/old_work_and_big_group/try_different_number/place.py or super/great_day/own_hand.py in great eye"

---
+++ super/feel_case/old_work_and_big_group/try_different_number/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_next_hand_into_woman(str_arg):
+    same_company(str_arg)
+    print('own_group')
+
+def same_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_next_hand_into_woman('time_or_day')
