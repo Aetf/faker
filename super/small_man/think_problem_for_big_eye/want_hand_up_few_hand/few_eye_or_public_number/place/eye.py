d3c2464b65bc4e701617e985a631e8ad5c269948
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-31 12:20:26 -0400

    "mix: look super/small_man/think_problem_for_big_eye/want_hand_up_few_hand/few_eye_or_public_number/place/eye.py, super/feel_company_with_day/few_woman.py or super/new_point/own_eye/thing/place_or_same_work/problem/life_or_place.py after next week"

---
+++ super/small_man/think_problem_for_big_eye/want_hand_up_few_hand/few_eye_or_public_number/place/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week_and_number(str_arg):
+    leave_small_woman_for_first_thing(str_arg)
+    print('old_government_or_fact')
+
+def leave_small_woman_for_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week_and_number('right_hand_or_world')
