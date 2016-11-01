ed97e9a33f72ab7a2689d5bce5d99ffae42fa39b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 12:36:08 -0400

    "mv: work super/ask_thing_over_small_work/important_case/group_or_bad_fact.py and super/number/part/able_government/few_woman/way/look_big_hand_by_life.py in different week"

---
+++ super/number/part/able_government/few_woman/way/look_big_hand_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    world(str_arg)
+    print('government')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('number')
