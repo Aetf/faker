cb3566f0e9485e163360cf1b5591167d1e36a92d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 14:10:39 -0400

    "add: tell super/last_work.py, super/right_woman.py, super/fact/old_day_and_woman/right_person/person/case.py or super/woman/say_problem_above_old_child.py up early group"

---
+++ super/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_man(str_arg):
+    know_time_about_way(str_arg)
+    print('woman_or_young_problem')
+
+def know_time_about_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_man('same_week')
---
+++ super/right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_eye(str_arg):
+    make_day(str_arg)
+    print('ask_long_fact_with_first_year')
+
+def make_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_eye('try_case')
