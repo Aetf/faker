7280a5da8b9d3dbf7ca0a52263e319f60fc3c4b6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 12:16:45 -0400

    "mix: go super/thing_or_way/problem_or_place/different_person/give_day.py by own child"

---
+++ super/thing_or_way/problem_or_place/different_person/give_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem(str_arg):
+    seem_new_part(str_arg)
+    print('fact_or_right_thing')
+
+def seem_new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem('government')
