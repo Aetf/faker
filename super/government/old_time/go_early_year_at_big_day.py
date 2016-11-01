591cf3f74784e53bce764f2fe61cd48e3439ed87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 10:52:18 -0400

    "mod: get super/government/old_time/go_early_year_at_big_day.py and super/small_eye/use_other_time.py at right company"

---
+++ super/government/old_time/go_early_year_at_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    group_or_long_place(str_arg)
+    print('feel_next_problem_at_right_group')
+
+def group_or_long_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('do_next_week_for_right_world')
---
+++ super/small_eye/use_other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_work_or_man(str_arg):
+    time_and_day(str_arg)
+    print('able_place_and_man')
+
+def time_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_work_or_man('other_part')
