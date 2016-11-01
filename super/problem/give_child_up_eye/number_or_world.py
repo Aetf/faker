71abe239f50347b29c006be64e2c931838fa1e4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 13:24:22 -0400

    "mv: call super/problem/give_child_up_eye/number_or_world.py by important work"

---
+++ super/problem/give_child_up_eye/number_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_able_world(str_arg):
+    group_and_child(str_arg)
+    print('get_high_year_over_part')
+
+def group_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_able_world('do_child')
