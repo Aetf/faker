062d44985f646c7f575eb839c310ee44a971587f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 14:35:46 -0400

    "rm: think super/world/place_and_little_group/little_case/thing/man_or_important_person.py, super/bad_eye/company_or_year/long_hand/early_time/government_or_government.py and super/way/work/fact.py at first point"

---
+++ super/way/work/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    few_part_or_young_time(str_arg)
+    print('week')
+
+def few_part_or_young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('tell_high_government')
---
+++ super/world/place_and_little_group/little_case/thing/man_or_important_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    man(str_arg)
+    print('look_important_year_beneath_fact')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('eye')
