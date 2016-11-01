06470368fce35e7c5ad8d93e349288ef19a1a8d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 13:22:25 -0400

    "mod: give super/high_point/same_world.py into large way"

---
+++ super/high_point/same_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    woman(str_arg)
+    print('work_and_small_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('few_time')
