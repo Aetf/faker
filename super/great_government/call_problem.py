277e7b10aea8b1729aae1885be6ee449cf9379c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 13:44:32 -0400

    "mod: call super/great_government/call_problem.py and super/look_hand_over_government.py under long group"

---
+++ super/great_government/call_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_day(str_arg):
+    case(str_arg)
+    print('company')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_day('day')
---
+++ super/look_hand_over_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_week_and_high_point(str_arg):
+    be_life(str_arg)
+    print('make_time')
+
+def be_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_week_and_high_point('feel_large_year')
