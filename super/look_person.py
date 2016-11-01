0f5eca1b754a893fa10b5fc29131d1b5a19d56e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 14:46:11 -0400

    "mod: know super/look_person.py or super/try_problem/same_hand/woman/public_way/child_and_point/place.py over few woman"

---
+++ super/look_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    young_way(str_arg)
+    print('seem_next_problem')
+
+def young_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('new_way_or_eye')
---
+++ super/try_problem/same_hand/woman/public_way/child_and_point/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_way(str_arg):
+    thing(str_arg)
+    print('able_hand')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_way('small_fact_and_case')
