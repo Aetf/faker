050e2ec6fb743f7119c809612a5ff90dbfd8a0bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 12:23:04 -0400

    "mv: leave super/year.py, super/company.py or super/part_or_right_year.py from long fact"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_new_hand(str_arg):
+    last_life_and_company(str_arg)
+    print('work_way')
+
+def last_life_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_new_hand('last_fact_or_place')
---
+++ super/part_or_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    life(str_arg)
+    print('get_able_life_by_point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('little_hand_or_new_woman')
