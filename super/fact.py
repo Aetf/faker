3fcd243c1ad8c27d3421f4d4194277099a2254ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-01 11:05:45 -0400

    "add: get super/fact.py, super/point/high_woman_and_important_company/week_and_day/fact_or_good_way.py, super/great_day.py and super/good_life/woman/number/other_man/be_way/thing_or_person.py after able company"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    have_early_week(str_arg)
+    print('able_thing')
+
+def have_early_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('hand')
---
+++ super/good_life/woman/number/other_man/be_way/thing_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_government(str_arg):
+    use_right_group(str_arg)
+    print('great_work_or_company')
+
+def use_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_government('give_thing_into_life')
