39f0ac5d1cdf31c6f27ce9be976980d8c171b2bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 11:10:42 -0400

    "add: take super/think_company_by_long_point/good_part/get_problem_with_part/high_point_or_few_fact/good_child.py into public place"

---
+++ super/think_company_by_long_point/good_part/get_problem_with_part/high_point_or_few_fact/good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    problem_or_way(str_arg)
+    print('little_fact')
+
+def problem_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('same_child')
