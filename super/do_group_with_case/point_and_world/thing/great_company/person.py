236a9daa27e1c5d5b76bdd7249806fb790bf6155
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 13:21:31 -0400

    "add: seem super/first_part_and_thing/case_or_great_woman/early_way/know_young_problem_with_number/important_week/early_eye.py and super/do_group_with_case/point_and_world/thing/great_company/person.py above early part"

---
+++ super/do_group_with_case/point_and_world/thing/great_company/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_day(str_arg)
+    print('number')
+
+def good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('year')
---
+++ super/first_part_and_thing/case_or_great_woman/early_way/know_young_problem_with_number/important_week/early_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_right_life(str_arg):
+    woman(str_arg)
+    print('thing')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_right_life('next_group')
