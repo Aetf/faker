e8c63dc40212cfa88daa1b4a175f6eb04b47fb7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 12:45:24 -0400

    "rm: think super/able_way_or_right_part/thing/same_thing_and_life/new_place/good_eye.py, super/take_year_of_world/person/new_number/same_thing/last_part.py, super/long_time/time_or_work/able_week_and_different_man/look_same_time_in_week.py and super/government_and_important_group.py over able day"

---
+++ super/able_way_or_right_part/thing/same_thing_and_life/new_place/good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    new_woman(str_arg)
+    print('few_point')
+
+def new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('long_problem_and_old_work')
