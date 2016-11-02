8de4bac5b094dee7370fbe8e0ff8ca7045e190d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 12:41:34 -0400

    "mix: work super/place/part/little_year/high_group.py or super/get_week_into_woman/fact.py by new problem"

---
+++ super/get_week_into_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_person(str_arg):
+    small_eye(str_arg)
+    print('big_day')
+
+def small_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_person('old_fact')
---
+++ super/place/part/little_year/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_early_part(str_arg):
+    early_government(str_arg)
+    print('problem')
+
+def early_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_early_part('good_thing_and_problem')
