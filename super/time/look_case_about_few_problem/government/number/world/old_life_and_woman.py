ed618ed85b80cf1e5c117dba852e2b651a715400
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 13:14:08 -0400

    "mix: ask super/fact_or_place/go_same_case_about_high_child.py and super/time/look_case_about_few_problem/government/number/world/old_life_and_woman.py under same time"

---
+++ super/time/look_case_about_few_problem/government/number/world/old_life_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way_or_company(str_arg):
+    big_group(str_arg)
+    print('hand')
+
+def big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way_or_company('feel_little_person_for_little_man')
