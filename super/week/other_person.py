ca294d6fceac1159e38cb22d859dc3b1de4c2795
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 14:06:49 -0400

    "mix: know super/week/other_person.py or super/problem_and_life/year/group/feel_last_thing_with_company/year/try_group.py under different place"

---
+++ super/week/other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_government(str_arg):
+    great_government_and_same_government(str_arg)
+    print('point')
+
+def great_government_and_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_government('year')
