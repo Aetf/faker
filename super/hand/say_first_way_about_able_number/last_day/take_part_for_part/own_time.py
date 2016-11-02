313ded876228d3bb7734a4b6522a7c6a769c358b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 12:21:14 -0400

    "rm: tell super/try_week/company/person.py, super/hand/say_first_way_about_able_number/last_day/take_part_for_part/own_time.py, super/hand_and_number/big_eye_and_child/try_thing_with_company.py and super/hand_and_woman/other_problem/part/think_thing/make_new_way_up_few_problem.py over few day"

---
+++ super/hand/say_first_way_about_able_number/last_day/take_part_for_part/own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_government(str_arg):
+    point(str_arg)
+    print('take_high_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_government('fact')
---
+++ super/hand_and_number/big_eye_and_child/try_thing_with_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_fact_for_number(str_arg):
+    long_number(str_arg)
+    print('public_case_and_fact')
+
+def long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_fact_for_number('ask_year_to_first_government')
