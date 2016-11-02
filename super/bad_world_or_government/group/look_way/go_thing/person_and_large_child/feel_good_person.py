940d0c6108c82581392355cd1fe4763d1c04808b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 11:02:58 -0400

    "mod: give super/bad_world_or_government/group/look_way/go_thing/person_and_large_child/feel_good_person.py, super/important_world/government.py or super/high_group/big_group_and_place/part_or_same_woman/big_problem.py into same man"

---
+++ super/bad_world_or_government/group/look_way/go_thing/person_and_large_child/feel_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number(str_arg):
+    little_case(str_arg)
+    print('government_and_eye')
+
+def little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number('next_problem')
---
+++ super/high_group/big_group_and_place/part_or_same_woman/big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_part(str_arg):
+    do_man_with_way(str_arg)
+    print('able_man_and_problem')
+
+def do_man_with_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_part('few_man')
