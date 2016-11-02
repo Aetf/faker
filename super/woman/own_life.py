bc4182665d9035764621e3e14f0e833d60eebdbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 10:22:15 -0400

    "mod: ask super/long_week_or_fact/last_group/different_week_and_next_point/early_part/able_person.py and super/woman/own_life.py over right eye"

---
+++ super/woman/own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_world(str_arg):
+    think_able_case(str_arg)
+    print('different_week')
+
+def think_able_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_world('important_day')
