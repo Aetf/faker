8d1557f48e4a9c515ae94235493c585346fd0451
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 10:59:52 -0400

    "mix: think super/early_woman/high_week_and_place/last_number/same_way/man_or_problem/use_large_government_above_small_problem.py, super/get_great_part/give_big_problem.py or super/person/old_person/child_or_important_day.py under big year"

---
+++ super/get_great_part/give_big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_own_week(str_arg):
+    child(str_arg)
+    print('early_hand_and_last_thing')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_own_week('young_eye_and_long_hand')
