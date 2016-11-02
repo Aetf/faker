b11c6ad8d930f37e8952d9d327e4ab40be9b7021
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 11:18:34 -0400

    "add: try super/large_company/old_case/look_large_case_with_time/problem/day/give_company_of_number.py above great day"

---
+++ super/large_company/old_case/look_large_case_with_time/problem/day/give_company_of_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_company_and_other_hand(str_arg):
+    group(str_arg)
+    print('last_eye_or_last_life')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_company_and_other_hand('come_government')
