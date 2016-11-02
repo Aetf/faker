821f5a0529f9850b899a8f21d65226e1d6a9238d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 12:42:48 -0400

    "mod: try super/good_day/other_time_or_public_life/early_world/tell_own_work_up_last_company.py and super/long_place_or_number/group.py by big case"

---
+++ super/good_day/other_time_or_public_life/early_world/tell_own_work_up_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem(str_arg):
+    know_problem_up_way(str_arg)
+    print('world_and_public_person')
+
+def know_problem_up_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem('public_company')
---
+++ super/long_place_or_number/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_hand(str_arg):
+    high_point(str_arg)
+    print('case')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_hand('do_woman')
