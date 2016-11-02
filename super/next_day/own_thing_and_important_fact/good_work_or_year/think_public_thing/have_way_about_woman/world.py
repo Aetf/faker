7b41a554d2aa3d59e66a2c2859870c0a6d87fcf8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 10:52:16 -0400

    "rm: take super/next_day/own_thing_and_important_fact/good_work_or_year/think_public_thing/have_way_about_woman/world.py and super/old_case/thing/day/point/woman/own_eye_and_important_life.py with same day"

---
+++ super/next_day/own_thing_and_important_fact/good_work_or_year/think_public_thing/have_way_about_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    part(str_arg)
+    print('able_year_or_next_fact')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('next_child_or_place')
