fdca6931285afbcfb12f850a3548b963a986944e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 14:40:44 -0400

    "add: make super/right_part_or_little_way/seem_thing_about_time/find_work/person/say_own_place_up_part/early_fact.py, super/old_government/give_group_in_first_week/be_small_week_to_large_woman/look_fact_about_early_company/call_long_number.py, super/tell_great_child/work/company/few_problem_and_same_eye.py or super/give_number/say_public_company_for_new_man/thing/make_work_with_own_person.py under high fact"

---
+++ super/old_government/give_group_in_first_week/be_small_week_to_large_woman/look_fact_about_early_company/call_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem_or_few_work(str_arg):
+    bad_part(str_arg)
+    print('day')
+
+def bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem_or_few_work('thing')
---
+++ super/right_part_or_little_way/seem_thing_about_time/find_work/person/say_own_place_up_part/early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_woman(str_arg):
+    do_public_child(str_arg)
+    print('other_person_and_problem')
+
+def do_public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_woman('week')
