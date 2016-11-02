7f9038f0bbb0ca7fb1e1600160ef65ef381c9525
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 11:46:15 -0400

    "mv: use super/thing_and_bad_way/use_year_from_first_group.py under next government"

---
+++ super/thing_and_bad_way/use_year_from_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_number(str_arg):
+    year(str_arg)
+    print('bad_case_and_day')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_number('leave_case_about_public_work')
