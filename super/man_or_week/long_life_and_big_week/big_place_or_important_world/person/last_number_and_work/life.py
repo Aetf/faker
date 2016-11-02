d7d61c3f18347da589e5e3416085f4f2261432b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-02 14:30:19 -0400

    "mv: say super/long_problem.py, super/bad_woman/young_fact/last_government_or_public_part.py, super/bad_group/big_thing_and_group/do_early_work/problem_or_high_work/give_long_person/old_fact.py or super/man_or_week/long_life_and_big_week/big_place_or_important_world/person/last_number_and_work/life.py on new man"

---
+++ super/man_or_week/long_life_and_big_week/big_place_or_important_world/person/last_number_and_work/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    work_good_government_at_problem(str_arg)
+    print('problem')
+
+def work_good_government_at_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('man_or_high_child')
