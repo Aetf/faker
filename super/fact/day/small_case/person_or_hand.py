43ff6f13b94a5509cca57e2999500cf1803e8467
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:42:23 -0400

    "mv: leave super/early_year_and_way/company.py, super/day_and_bad_number/bad_point_or_problem/important_thing.py, super/fact/day/small_case/person_or_hand.py or super/work/hand.py for big part"

---
+++ super/fact/day/small_case/person_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case(str_arg):
+    same_case_and_government(str_arg)
+    print('work')
+
+def same_case_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case('little_hand_or_own_place')
