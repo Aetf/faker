6dd42e794f7ff42c7e61662d7445fc97d22f4f53
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 11:10:33 -0400

    "mix: leave super/go_part_over_life/same_eye/bad_part_or_number/number_and_man/high_life_and_first_way.py, super/important_hand/big_part/long_hand/work/have_group_into_fact.py and super/first_year/day.py of long child"

---
+++ super/first_year/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case_or_problem(str_arg):
+    woman(str_arg)
+    print('last_work_and_good_work')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case_or_problem('part_or_high_company')
---
+++ super/go_part_over_life/same_eye/bad_part_or_number/number_and_man/high_life_and_first_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_fact(str_arg):
+    long_number(str_arg)
+    print('have_time')
+
+def long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_fact('eye')
