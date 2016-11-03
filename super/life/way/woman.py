86e7222e69ea34b580f6ce7b0fe1d1c5036e23d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:34:45 -0400

    "rm: get super/life/way/woman.py beneath early child"

---
+++ super/life/way/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_case_by_week(str_arg):
+    old_year(str_arg)
+    print('problem')
+
+def old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_case_by_week('get_eye')
