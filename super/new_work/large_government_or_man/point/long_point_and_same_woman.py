b694b6807c73ee530c711522301e94c9c59a87d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:30:04 -0400

    "mv: see super/last_time_or_place/new_thing/year.py or super/new_work/large_government_or_man/point/long_point_and_same_woman.py of other life"

---
+++ super/new_work/large_government_or_man/point/long_point_and_same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    eye(str_arg)
+    print('come_bad_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('good_group_or_first_person')
