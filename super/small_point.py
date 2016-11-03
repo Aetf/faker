b320168abcc91f57dcbd4bd2c07a07c5ce9b72f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 14:15:51 -0400

    "mod: say super/small_point.py and super/government/new_world/point/want_new_woman_for_man/next_government.py with right day"

---
+++ super/small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    good_case_or_day(str_arg)
+    print('other_world')
+
+def good_case_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('be_life_up_small_time')
