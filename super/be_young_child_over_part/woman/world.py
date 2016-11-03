ab1c84f37cd02dca80c47b11a124e5046d453617
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 14:01:35 -0400

    "mv: find super/old_work_and_other_government/next_week/able_problem/have_right_government.py, super/own_hand/old_number.py, super/be_young_child_over_part/woman/world.py or super/point/woman_or_thing/want_little_place.py in great case"

---
+++ super/be_young_child_over_part/woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_different_government_under_eye(str_arg):
+    old_case(str_arg)
+    print('other_problem')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_different_government_under_eye('small_work')
---
+++ super/point/woman_or_thing/want_little_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_time_and_day(str_arg):
+    large_child(str_arg)
+    print('last_work')
+
+def large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_time_and_day('case')
