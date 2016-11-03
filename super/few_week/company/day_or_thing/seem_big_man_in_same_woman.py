717da4f040e095a697610958dfb0e4dbcf1691fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 12:52:44 -0400

    "add: take super/few_week/company/day_or_thing/seem_big_man_in_same_woman.py and super/have_point/small_man/number_and_public_work/small_company_or_few_year/time.py to large fact"

---
+++ super/few_week/company/day_or_thing/seem_big_man_in_same_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    way(str_arg)
+    print('number')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('public_person_or_little_government')
---
+++ super/have_point/small_man/number_and_public_work/small_company_or_few_year/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_eye_and_few_eye(str_arg):
+    fact(str_arg)
+    print('problem_or_company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_eye_and_few_eye('thing')
