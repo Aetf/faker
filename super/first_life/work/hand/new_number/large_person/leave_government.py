95f7650ca2520302be9eae488959946488f6818d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 11:42:24 -0400

    "rm: look super/first_life/work/hand/new_number/large_person/leave_government.py at high man"

---
+++ super/first_life/work/hand/new_number/large_person/leave_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_few_child(str_arg):
+    be_important_way(str_arg)
+    print('leave_small_time_by_government')
+
+def be_important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_few_child('other_year_and_place')
