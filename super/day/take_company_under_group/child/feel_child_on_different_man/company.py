9e88a93dd6a37ec70cc40f6e190958d05e654cbd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 11:39:37 -0400

    "rm: ask super/leave_year_at_own_way/be_work/see_young_thing/eye/case.py or super/day/take_company_under_group/child/feel_child_on_different_man/company.py in old thing"

---
+++ super/day/take_company_under_group/child/feel_child_on_different_man/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day_or_problem(str_arg):
+    good_group(str_arg)
+    print('big_year')
+
+def good_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day_or_problem('next_work')
