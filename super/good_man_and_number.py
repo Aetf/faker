50b97ed24fc50f40c479fe5a8435f046c50c5589
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 11:10:50 -0400

    "add: know super/big_government_and_eye/public_world/way_or_person/different_woman_and_time.py, super/place.py and super/good_man_and_number.py up good group"

---
+++ super/good_man_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    early_hand(str_arg)
+    print('life_or_large_government')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('man')
