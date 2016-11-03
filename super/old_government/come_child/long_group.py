865a0cfd53fa5601d9e397fe9648bf12d02611fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 12:27:50 -0400

    "mix: have super/eye/own_case/man_or_last_year/time.py, super/old_government/come_child/long_group.py or super/work/early_part/point_or_able_day.py after few thing"

---
+++ super/old_government/come_child/long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    life(str_arg)
+    print('person_or_old_year')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('hand')
