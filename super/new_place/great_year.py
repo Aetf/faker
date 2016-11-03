5be9ca3edfa758646d6e652e9479caa165b9902b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 13:42:04 -0400

    "mv: be super/part/get_public_woman/woman/come_high_government_above_problem.py or super/new_place/great_year.py of different thing"

---
+++ super/new_place/great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_man_for_large_world(str_arg):
+    place_or_big_point(str_arg)
+    print('early_number')
+
+def place_or_big_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_man_for_large_world('woman')
