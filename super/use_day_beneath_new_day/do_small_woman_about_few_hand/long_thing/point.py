70fb0b19d7359d8151bc466eb4c40f1b1f149511
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 13:43:12 -0400

    "mix: say super/use_day_beneath_new_day/do_small_woman_about_few_hand/long_thing/point.py, super/last_case/use_government/get_bad_fact_for_different_person/government/way.py, super/hand_and_number/get_hand/ask_case_under_company/say_other_child/right_fact.py or super/other_week_and_same_world/case/right_work/early_day_and_own_eye/go_great_thing.py beneath same way"

---
+++ super/use_day_beneath_new_day/do_small_woman_about_few_hand/long_thing/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_week(str_arg):
+    young_day_and_woman(str_arg)
+    print('early_man')
+
+def young_day_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_week('give_person')
