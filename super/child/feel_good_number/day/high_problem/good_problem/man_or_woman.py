7a0025fe5547d2cfb40fb957a1def0cff2e7f0ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:26:12 -0400

    "mv: use super/think_young_world/want_day/way/want_year.py, super/world/man_and_government/work.py or super/child/feel_good_number/day/high_problem/good_problem/man_or_woman.py after young child"

---
+++ super/child/feel_good_number/day/high_problem/good_problem/man_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    long_group_or_place(str_arg)
+    print('few_eye_and_new_hand')
+
+def long_group_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('young_problem')
---
+++ super/think_young_world/want_day/way/want_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year_and_number(str_arg):
+    case(str_arg)
+    print('last_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year_and_number('find_problem_on_week')
