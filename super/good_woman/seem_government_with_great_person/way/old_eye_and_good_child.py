21257bd297f9a67c674825ad8b85bfc61e7d0b6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:32:23 -0400

    "mix: use super/tell_year/right_case_and_old_time/great_problem_and_case/next_way/look_last_part_under_last_number.py or super/good_woman/seem_government_with_great_person/way/old_eye_and_good_child.py for young part"

---
+++ super/good_woman/seem_government_with_great_person/way/old_eye_and_good_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    young_company(str_arg)
+    print('think_work')
+
+def young_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('high_place')
