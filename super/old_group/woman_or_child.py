5505fc588d343b64a3aba57f9a33483c8b67cd02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 12:57:32 -0400

    "rm: seem super/public_woman_or_long_woman/good_time/give_life.py or super/old_group/woman_or_child.py to old work"

---
+++ super/old_group/woman_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    number_and_man(str_arg)
+    print('first_person')
+
+def number_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('same_problem')
---
+++ super/public_woman_or_long_woman/good_time/give_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_government(str_arg):
+    point(str_arg)
+    print('work_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_government('time')
