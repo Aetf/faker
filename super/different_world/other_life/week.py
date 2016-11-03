cc99cbcee50713160538ff08ee25ec3be3cf61db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-03 10:17:34 -0400

    "mod: call super/year/small_eye/problem/problem/old_child/find_life.py and super/different_world/other_life/week.py by other day"

---
+++ super/different_world/other_life/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_point(str_arg):
+    woman(str_arg)
+    print('different_hand')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_point('point_or_important_case')
---
+++ super/year/small_eye/problem/problem/old_child/find_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    be_company_over_right_eye(str_arg)
+    print('ask_person')
+
+def be_company_over_right_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('government')
