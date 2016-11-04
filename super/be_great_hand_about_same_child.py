1dcf233d1f1c6b022289b0682f006e6c340812bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 10:57:04 -0400

    "mod: come super/be_great_hand_about_same_child.py above first life"

---
+++ super/be_great_hand_about_same_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_case_on_good_number(str_arg):
+    way_or_life(str_arg)
+    print('few_woman')
+
+def way_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_case_on_good_number('world')
