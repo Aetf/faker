7279d4361db5f57d3bf09ea1f91b548738e4886b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:09:20 -0400

    "rm: make super/get_good_thing.py with new problem"

---
+++ super/get_good_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day(str_arg):
+    point(str_arg)
+    print('first_government_or_week')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day('company')
