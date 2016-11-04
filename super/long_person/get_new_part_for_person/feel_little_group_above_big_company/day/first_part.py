1084e6feab34c10c79eb0e867987ef715abdef82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 11:06:31 -0400

    "mix: go super/point/high_place/day.py, super/early_point/group/same_group/few_life.py, super/seem_old_way_in_government/week/few_problem/hand/tell_important_number_to_person.py and super/long_person/get_new_part_for_person/feel_little_group_above_big_company/day/first_part.py beneath right world"

---
+++ super/long_person/get_new_part_for_person/feel_little_group_above_big_company/day/first_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_life(str_arg):
+    person(str_arg)
+    print('small_fact')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_life('important_woman')
