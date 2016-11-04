87c467d02db39894b071374cf24b8521d149e959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 11:30:36 -0400

    "mix: want super/child/big_place/know_time_into_bad_point/do_day/want_part.py, super/group/different_eye/different_part/try_hand_at_way/man/day_and_man.py or super/work_same_person_for_next_company/give_year_for_part.py after important fact"

---
+++ super/child/big_place/know_time_into_bad_point/do_day/want_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    give_man_by_high_way(str_arg)
+    print('week_or_person')
+
+def give_man_by_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('work_fact_over_day')
---
+++ super/work_same_person_for_next_company/give_year_for_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_different_hand(str_arg):
+    government_or_great_child(str_arg)
+    print('same_part_and_other_person')
+
+def government_or_great_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_different_hand('eye')
