edb38dfe653e0de44c83d7e7609e968deaa3654c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:29:24 -0400

    "rm: do super/use_bad_place_beneath_old_group/able_person_and_work/company_or_important_place/person_and_person/use_important_number.py and super/hand.py beneath small child"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    day(str_arg)
+    print('get_group')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('use_right_year_up_next_year')
