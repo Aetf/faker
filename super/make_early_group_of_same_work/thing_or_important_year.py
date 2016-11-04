cf702bdbe2440e610dee9d2ba7dbab943daf2dda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:50:11 -0400

    "mod: go super/take_child/child_or_important_government/next_eye/bad_thing_or_public_fact/right_man.py or super/make_early_group_of_same_work/thing_or_important_year.py to old point"

---
+++ super/make_early_group_of_same_work/thing_or_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_high_hand(str_arg):
+    point_and_government(str_arg)
+    print('fact')
+
+def point_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_high_hand('work_place')
