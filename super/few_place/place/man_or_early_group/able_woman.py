423660c2a74f93edb4cb58f6dcfdd93dfaa62e76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 13:47:08 -0400

    "rm: find super/few_place/place/man_or_early_group/able_woman.py or super/say_work_over_hand/small_problem_and_other_hand/work/week_and_way/make_case/new_group.py from big year"

---
+++ super/few_place/place/man_or_early_group/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_eye_and_large_fact(str_arg):
+    large_day_and_day(str_arg)
+    print('child_and_different_child')
+
+def large_day_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_eye_and_large_fact('work')
---
+++ super/say_work_over_hand/small_problem_and_other_hand/work/week_and_way/make_case/new_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_year(str_arg):
+    think_own_time(str_arg)
+    print('part')
+
+def think_own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_year('week')
