a7867060ca09bcf36a7980a8dd788a8fc9ba2611
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 14:39:46 -0400

    "mod: try super/little_woman_or_part/small_year_or_life.py, super/point.py and super/call_new_year/number/public_week/new_person/point.py under few week"

---
+++ super/call_new_year/number/public_week/new_person/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    government(str_arg)
+    print('say_year')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('few_time')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_part(str_arg):
+    few_year_or_big_fact(str_arg)
+    print('say_life_in_work')
+
+def few_year_or_big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_part('look_group')
