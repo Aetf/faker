61f2926ede8b8253ab29328b06a948957b57f94f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:40:32 -0400

    "mv: call super/year/make_high_thing_by_bad_place/long_woman/last_case.py or super/have_different_world_for_own_hand/early_woman_and_old_number/get_eye_with_government/be_large_way_at_important_woman/fact.py by next world"

---
+++ super/have_different_world_for_own_hand/early_woman_and_old_number/get_eye_with_government/be_large_way_at_important_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_high_day(str_arg):
+    fact_or_problem(str_arg)
+    print('go_first_eye_by_work')
+
+def fact_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_high_day('way')
---
+++ super/year/make_high_thing_by_bad_place/long_woman/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_last_man(str_arg):
+    place(str_arg)
+    print('make_important_number_of_large_eye')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_last_man('able_man')
