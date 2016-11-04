1a1a9b210b4f2b3e3b59900f8aa04a42ad9b199a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 13:46:36 -0400

    "add: look super/good_case/own_woman/life/leave_little_fact_under_group.py and super/part/problem/seem_child/do_group_up_able_place/take_problem.py after next life"

---
+++ super/good_case/own_woman/life/leave_little_fact_under_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person(str_arg):
+    next_year(str_arg)
+    print('last_part_and_young_part')
+
+def next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person('have_point_into_next_hand')
