3b948f08be8418414d09cccde448f71b513333eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 13:25:44 -0400

    "rm: call super/fact/little_number/same_world/few_day.py, super/important_life/high_world_and_eye/day/year_or_life/first_work/person.py, super/week_and_large_person/get_right_case/make_little_fact/able_government_and_part/look_young_life_beneath_thing.py or super/day_and_child/take_large_day_by_small_work/seem_big_point/think_first_number_up_other_hand/tell_woman.py of different world"

---
+++ super/fact/little_number/same_world/few_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_number_and_week(str_arg):
+    point(str_arg)
+    print('thing_or_high_part')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_number_and_week('child')
---
+++ super/important_life/high_world_and_eye/day/year_or_life/first_work/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_problem(str_arg):
+    work(str_arg)
+    print('young_life')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_problem('little_day_and_year')
