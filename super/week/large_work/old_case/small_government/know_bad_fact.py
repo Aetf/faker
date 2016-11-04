077822d1c2496e5499ad34cf42a0cb8695aa9152
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 11:14:38 -0400

    "add: make super/week/large_work/old_case/small_government/know_bad_fact.py, super/thing/same_year.py or super/woman/last_problem_or_important_number/old_year_and_fact/man/company.py to young place"

---
+++ super/week/large_work/old_case/small_government/know_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_company(str_arg):
+    number(str_arg)
+    print('ask_thing_about_case')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_company('problem')
