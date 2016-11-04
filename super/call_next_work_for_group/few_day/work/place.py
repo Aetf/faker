0cf4605d15ca06f4bb92b53e3f665d661e4e025a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 11:05:12 -0400

    "add: say super/call_next_work_for_group/few_day/work/place.py in new time"

---
+++ super/call_next_work_for_group/few_day/work/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    use_life(str_arg)
+    print('first_week')
+
+def use_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('own_week')
