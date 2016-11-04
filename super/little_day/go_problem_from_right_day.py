9b3697314ebcfcefac2dc999ed8d655b54c7aaeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 11:46:46 -0400

    "mix: call super/little_day/go_problem_from_right_day.py and super/time_and_important_point/give_number/part.py for different group"

---
+++ super/little_day/go_problem_from_right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem_or_large_group(str_arg):
+    large_company_and_long_hand(str_arg)
+    print('tell_thing')
+
+def large_company_and_long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem_or_large_group('great_fact')
