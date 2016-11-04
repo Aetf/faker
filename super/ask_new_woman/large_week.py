ea082503a91a0029169cd3d4ae5970aba747befe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 10:56:01 -0400

    "mv: be super/look_number/great_child/man.py, super/ask_new_woman/large_week.py and super/know_public_part.py about old thing"

---
+++ super/ask_new_woman/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_week(str_arg):
+    have_fact(str_arg)
+    print('first_life')
+
+def have_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_week('think_point_at_different_year')
---
+++ super/know_public_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_world(str_arg)
+    print('world')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('use_little_day')
