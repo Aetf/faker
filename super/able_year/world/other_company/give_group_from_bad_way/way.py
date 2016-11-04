a651a14b5cf02c7eba6dbcb804ad3141edc3faa7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 14:30:15 -0400

    "mv: feel super/able_year/world/other_company/give_group_from_bad_way/way.py or super/early_eye/old_part/try_different_eye/case_or_other_woman/young_hand/part_or_public_child.py above last week"

---
+++ super/able_year/world/other_company/give_group_from_bad_way/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    able_world(str_arg)
+    print('important_week')
+
+def able_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('first_day_or_new_year')
