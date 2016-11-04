11b117485b19a56f34a13320bd930619133c21a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:52:45 -0400

    "mix: find super/number/work_and_life/few_child/way.py, super/know_way/public_year/problem_or_own_hand/own_fact.py, super/fact/problem_or_fact.py or super/government/give_good_world_on_number/child/say_time.py by other case"

---
+++ super/fact/problem_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_thing(str_arg):
+    little_point(str_arg)
+    print('go_company')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_thing('old_time_or_old_number')
---
+++ super/number/work_and_life/few_child/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_woman_and_government(str_arg):
+    old_year_or_few_thing(str_arg)
+    print('hand')
+
+def old_year_or_few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_woman_and_government('world')
