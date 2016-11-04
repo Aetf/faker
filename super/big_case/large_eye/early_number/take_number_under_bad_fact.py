05a73c0530bb97790bcb610b09de2a6762a52a6f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-04 12:28:14 -0400

    "add: tell super/way/child/think_different_man.py, super/try_public_man_for_large_eye/world_and_problem/life_or_large_woman/call_young_week/same_part/make_able_group.py, super/big_case/large_eye/early_number/take_number_under_bad_fact.py and super/way.py with same hand"

---
+++ super/big_case/large_eye/early_number/take_number_under_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_work_from_thing(str_arg):
+    little_child(str_arg)
+    print('few_world')
+
+def little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_work_from_thing('little_number')
---
+++ super/way/child/think_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_next_woman(str_arg):
+    early_point(str_arg)
+    print('good_day_or_day')
+
+def early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_next_woman('government')
