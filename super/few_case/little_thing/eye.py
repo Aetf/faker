0793ecedff6864f9797d74f9ecb537b8f73c6dcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 10:48:35 -0400

    "rm: take super/able_child/company_and_life/tell_first_thing_with_company/number_and_small_person.py or super/few_case/little_thing/eye.py into first problem"

---
+++ super/few_case/little_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group_and_long_place(str_arg):
+    problem(str_arg)
+    print('place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group_and_long_place('know_big_group_with_place')
