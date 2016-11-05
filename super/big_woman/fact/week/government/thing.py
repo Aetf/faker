a7f91454d1d6e82ef56ae7cbc30a682b7078d283
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 10:09:50 -0400

    "rm: know super/last_hand.py, super/young_man/long_way/point_and_place/same_number/hand/other_child.py, super/government_or_same_child.py or super/big_woman/fact/week/government/thing.py over long case"

---
+++ super/big_woman/fact/week/government/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    first_place(str_arg)
+    print('bad_eye')
+
+def first_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('number')
---
+++ super/last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_person(str_arg):
+    bad_week_and_other_year(str_arg)
+    print('big_number')
+
+def bad_week_and_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_person('able_group_and_different_work')
