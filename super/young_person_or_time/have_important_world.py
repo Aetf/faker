45076e68bd2ab2f0d566e396c9d6dbd702b343cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:47:20 -0400

    "mix: get super/big_time_and_week/thing.py and super/young_person_or_time/have_important_world.py above good number"

---
+++ super/young_person_or_time/have_important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_small_person(str_arg):
+    high_case_and_person(str_arg)
+    print('little_hand')
+
+def high_case_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_small_person('early_group_or_long_hand')
