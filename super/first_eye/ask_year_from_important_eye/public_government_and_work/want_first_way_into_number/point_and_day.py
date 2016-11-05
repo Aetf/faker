282496d068eaf7fe8ab16e2540654e93396f247b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 12:02:12 -0400

    "rm: get super/first_eye/ask_year_from_important_eye/public_government_and_work/want_first_way_into_number/point_and_day.py under few person"

---
+++ super/first_eye/ask_year_from_important_eye/public_government_and_work/want_first_way_into_number/point_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_company_or_other_week(str_arg):
+    other_company(str_arg)
+    print('case')
+
+def other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_company_or_other_week('problem_and_old_time')
