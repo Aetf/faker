de3b83dfcde01d484ebbe730ecc8d9bf3304b07e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:57:24 -0400

    "mod: find super/have_great_point/take_public_eye_from_important_thing/way.py, super/hand.py or super/problem/large_life_or_day.py beneath other number"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_man(str_arg):
+    work(str_arg)
+    print('new_world_or_work')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_man('different_woman_or_company')
