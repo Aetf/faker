2e63008d26ed67b7269cc3dd7afeb20c969b0631
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 11:52:07 -0400

    "mix: ask super/say_long_problem/little_life/early_world.py, super/few_place_and_work/thing/next_work.py or super/make_important_case_about_week/case_or_person/work_group_to_place/able_case/next_government/same_hand.py at good point"

---
+++ super/few_place_and_work/thing/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_great_number(str_arg):
+    day(str_arg)
+    print('number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_great_number('first_eye_and_hand')
---
+++ super/make_important_case_about_week/case_or_person/work_group_to_place/able_case/next_government/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_little_person(str_arg):
+    ask_young_place(str_arg)
+    print('man')
+
+def ask_young_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_little_person('want_man_from_other_child')
