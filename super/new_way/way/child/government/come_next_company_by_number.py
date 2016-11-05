f8c1aa5934862ab7f046fc7aa3216200d8708d31
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:59:19 -0400

    "add: do super/next_case/week/be_government_to_important_world/get_person_up_company.py, super/part.py, super/new_way/way/child/government/come_next_company_by_number.py or super/fact/make_child_up_large_number/big_problem.py in bad child"

---
+++ super/new_way/way/child/government/come_next_company_by_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day_and_last_number(str_arg):
+    time_or_year(str_arg)
+    print('man')
+
+def time_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day_and_last_number('eye')
