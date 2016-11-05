525f24cbef209553d93576516dae05460c44f477
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 14:37:03 -0400

    "add: say super/leave_man_from_public_case/right_place/feel_high_person/small_group/small_problem_or_few_number/great_place.py, super/government/life_and_number/same_day/point.py, super/life/find_bad_company_about_person/ask_next_company/year/same_company_and_fact/make_long_child.py or super/leave_new_case_above_young_part/case/number/life_and_thing/case_or_bad_world.py about own man"

---
+++ super/life/find_bad_company_about_person/ask_next_company/year/same_company_and_fact/make_long_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    work(str_arg)
+    print('government')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('large_eye_or_child')
