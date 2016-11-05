7b308f2dd31f6361788713c3449a061a70f4d496
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 14:00:29 -0400

    "mix: think super/look_same_government_after_high_man/want_point/get_good_day_about_woman.py, super/small_person/week.py, super/able_life_and_government/point_and_big_fact.py and super/few_case/problem/time/way/able_hand.py with big hand"

---
+++ super/able_life_and_government/point_and_big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    part_or_good_way(str_arg)
+    print('number_or_first_number')
+
+def part_or_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('woman_or_way')
---
+++ super/small_person/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    important_case(str_arg)
+    print('different_number_or_important_eye')
+
+def important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('come_place_above_few_part')
