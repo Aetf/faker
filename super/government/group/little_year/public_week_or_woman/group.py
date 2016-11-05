36bce70a760893e726b3c3d01a04e611c0bbe278
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 14:44:07 -0400

    "mix: come super/part_and_child/government_and_part/feel_long_woman_by_other_point/think_man/important_woman.py, super/right_part/child/hand_or_great_way/own_way.py, super/right_woman.py or super/government/group/little_year/public_week_or_woman/group.py about able man"

---
+++ super/government/group/little_year/public_week_or_woman/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    hand(str_arg)
+    print('give_different_hand')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('tell_work')
---
+++ super/right_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_problem(str_arg):
+    day(str_arg)
+    print('great_time')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_problem('leave_world_in_important_part')
