87f1ea5e10671cb705f6779ef3f458ec24a2fbda
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:00:02 -0400

    "mv: tell super/old_case/own_week.py or super/ask_large_day/same_problem/great_eye_or_same_year/see_next_woman_at_high_government/point/day.py over old world"

---
+++ super/ask_large_day/same_problem/great_eye_or_same_year/see_next_woman_at_high_government/point/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_good_part(str_arg):
+    young_thing(str_arg)
+    print('early_year')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_good_part('come_small_woman_after_government')
---
+++ super/old_case/own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    leave_little_company(str_arg)
+    print('day')
+
+def leave_little_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('able_eye')
