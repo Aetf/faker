ad0584c3526163a298d40b4db1351c7555873efa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:23:35 -0400

    "mix: get super/get_day/make_world/bad_eye/day/man_and_time/woman_and_high_woman.py on able world"

---
+++ super/get_day/make_world/bad_eye/day/man_and_time/woman_and_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_life(str_arg):
+    great_place_or_high_point(str_arg)
+    print('world_or_little_world')
+
+def great_place_or_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_life('problem')
