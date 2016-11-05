f4cad16b70f035a5bb1aa82186eb61fd3cc0412b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 11:28:50 -0400

    "mv: ask super/new_government_or_life/thing/company/point/other_week/little_way.py or super/work_and_old_case/hand/eye/case.py on few hand"

---
+++ super/new_government_or_life/thing/company/point/other_week/little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_group_on_group(str_arg):
+    right_thing_or_young_day(str_arg)
+    print('life')
+
+def right_thing_or_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_group_on_group('say_own_week_up_right_man')
