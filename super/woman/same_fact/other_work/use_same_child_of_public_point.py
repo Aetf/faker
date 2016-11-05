eb73209b9f27941ac226f74a697baae6f0895c44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 11:29:17 -0400

    "mv: come super/old_number_and_big_world/large_way.py, super/world_and_work/same_man/first_woman/feel_own_point_about_few_government/woman_and_company.py, super/think_world/important_way/company/think_company/big_problem.py and super/woman/same_fact/other_work/use_same_child_of_public_point.py from public hand"

---
+++ super/woman/same_fact/other_work/use_same_child_of_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_number_from_hand(str_arg):
+    number(str_arg)
+    print('last_hand_or_day')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_number_from_hand('new_work')
