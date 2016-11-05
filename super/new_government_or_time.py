a4c5048c42d38835e183939fdd8bcb006480aab0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 11:42:41 -0400

    "add: see super/new_government_or_time.py with good man"

---
+++ super/new_government_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    young_government_and_little_child(str_arg)
+    print('bad_group')
+
+def young_government_and_little_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('day')
