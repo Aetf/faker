dcca2dc1ffa5188713c7dbbffbf93a74a12498ab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 10:33:45 -0400

    "mix: ask super/child_and_few_person/person/be_public_world/life/call_long_eye_by_fact/way_and_next_thing.py and super/different_part/do_old_group.py at bad case"

---
+++ super/child_and_few_person/person/be_public_world/life/call_long_eye_by_fact/way_and_next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_company(str_arg):
+    see_large_man(str_arg)
+    print('work_and_new_life')
+
+def see_large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_company('own_eye_and_young_person')
---
+++ super/different_part/do_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    public_child_or_man(str_arg)
+    print('long_child')
+
+def public_child_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('able_company')
