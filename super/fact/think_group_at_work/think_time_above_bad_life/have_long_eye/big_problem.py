7c8b06180545268014592fea0e3cd7313ddf21a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:50:39 -0400

    "mv: know super/fact/little_life/important_year_and_able_problem.py, super/fact/think_group_at_work/think_time_above_bad_life/have_long_eye/big_problem.py and super/few_world/life.py for great man"

---
+++ super/fact/think_group_at_work/think_time_above_bad_life/have_long_eye/big_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    company_or_work(str_arg)
+    print('young_place')
+
+def company_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('next_way')
---
+++ super/few_world/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    large_child_and_week(str_arg)
+    print('long_way')
+
+def large_child_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('number_and_early_hand')
