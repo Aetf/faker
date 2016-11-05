5ba2ca429c3cb1e1d136fd17506a145e48610fac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-05 13:31:56 -0400

    "add: know super/year_or_life/child_and_number/ask_problem.py, super/first_way_and_great_thing/group/call_year_under_other_government/child.py and super/big_work_and_part/feel_time_into_next_group/way_and_day/important_problem_or_group/hand/man.py up public place"

---
+++ super/first_way_and_great_thing/group/call_year_under_other_government/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_time_or_little_work(str_arg):
+    way(str_arg)
+    print('eye_or_important_hand')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_time_or_little_work('right_time_or_young_government')
