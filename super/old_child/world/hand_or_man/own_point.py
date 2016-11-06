3c7bad5050bd93d60198e87be564170dd7217e05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:28:59 -0500

    "mix: make super/old_child/world/hand_or_man/own_point.py to different day"

---
+++ super/old_child/world/hand_or_man/own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_day(str_arg):
+    fact_and_day(str_arg)
+    print('say_able_work_to_company')
+
+def fact_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_day('life')
