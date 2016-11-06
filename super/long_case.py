c9437c921f005a2a6c05a6470df45c65fa06af13
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 10:30:01 -0500

    "rm: call super/case/hand_or_week/great_fact/year.py, super/think_work/bad_fact_or_young_woman.py, super/long_case.py or super/eye/point_and_own_year/new_government/problem.py up own government"

---
+++ super/long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_person(str_arg):
+    feel_early_case_with_place(str_arg)
+    print('part_and_great_child')
+
+def feel_early_case_with_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_person('next_life')
---
+++ super/think_work/bad_fact_or_young_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time(str_arg):
+    year_or_little_government(str_arg)
+    print('own_child_or_able_life')
+
+def year_or_little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time('company')
