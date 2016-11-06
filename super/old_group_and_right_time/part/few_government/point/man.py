7b0bbf6cc38150e4820a31b4d46a74cf58f488cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 13:22:38 -0500

    "mv: get super/use_large_hand/next_year/look_other_day_over_number.py, super/old_group_and_right_time/part/few_government/point/man.py or super/leave_young_part_beneath_person/come_little_life/same_week/number/say_thing/own_child.py with important group"

---
+++ super/old_group_and_right_time/part/few_government/point/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_work_in_early_way(str_arg):
+    other_government(str_arg)
+    print('important_case_and_government')
+
+def other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_work_in_early_way('little_world')
