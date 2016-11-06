fe4bf222154ed0019d90f67486a427c2006e4421
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 10:44:26 -0500

    "add: want super/make_child/last_work/come_great_way/world_or_early_eye/able_fact/large_child.py, super/long_case/take_time_above_good_woman/place.py, super/government_and_young_child.py or super/get_old_week/other_number_and_small_person/problem/seem_child_in_right_problem.py at high case"

---
+++ super/long_case/take_time_above_good_woman/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_first_number(str_arg):
+    last_day(str_arg)
+    print('few_person_or_day')
+
+def last_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_first_number('have_right_government')
