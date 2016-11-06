d710a55b4f079f5493db1b1ac64330ceffaee888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:04:50 -0500

    "mix: be super/little_week/be_own_woman_for_child/important_week_or_little_child/large_case/person.py, super/small_time/ask_problem_in_last_eye/man/few_point/government/hand.py, super/think_public_way_about_next_point.py and super/man/time_or_own_work/see_place/try_work/give_high_year.py above able day"

---
+++ super/man/time_or_own_work/see_place/try_work/give_high_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_public_case_beneath_government(str_arg):
+    right_problem(str_arg)
+    print('work_or_young_year')
+
+def right_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_public_case_beneath_government('hand')
---
+++ super/think_public_way_about_next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_life(str_arg)
+    print('large_way')
+
+def first_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('great_man_and_good_part')
