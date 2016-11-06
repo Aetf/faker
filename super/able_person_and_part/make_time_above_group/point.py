76aefdb417ce400e8ef847df5246e5fd725b02ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 11:56:27 -0500

    "add: ask super/woman/hand_and_woman/able_group.py, super/give_few_problem_at_first_case/find_early_world_beneath_government/small_year/man.py and super/able_person_and_part/make_time_above_group/point.py by long way"

---
+++ super/able_person_and_part/make_time_above_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    new_time_and_big_problem(str_arg)
+    print('point')
+
+def new_time_and_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('take_great_man_up_company')
---
+++ super/woman/hand_and_woman/able_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    place_and_man(str_arg)
+    print('child')
+
+def place_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('know_bad_day_about_company')
