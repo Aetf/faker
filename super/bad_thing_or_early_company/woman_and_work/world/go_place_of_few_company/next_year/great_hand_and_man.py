56ef91c17115b5f34a985cb7ad5f260c9721338b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 10:07:26 -0500

    "mod: use super/long_person_and_person/child_or_public_world/important_company/seem_problem/use_small_work_about_few_hand/problem.py and super/bad_thing_or_early_company/woman_and_work/world/go_place_of_few_company/next_year/great_hand_and_man.py from young woman"

---
+++ super/bad_thing_or_early_company/woman_and_work/world/go_place_of_few_company/next_year/great_hand_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_part(str_arg):
+    important_woman(str_arg)
+    print('seem_time')
+
+def important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_part('high_work_or_group')
