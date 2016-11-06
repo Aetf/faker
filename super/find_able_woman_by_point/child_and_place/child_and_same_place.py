2a7ae88fa0fba361843ba050a158b8eea50e2d87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:15:45 -0500

    "mix: work super/little_case/able_way/feel_world_of_able_child/new_government_and_large_group/high_man.py and super/find_able_woman_by_point/child_and_place/child_and_same_place.py after great case"

---
+++ super/find_able_woman_by_point/child_and_place/child_and_same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    have_little_time(str_arg)
+    print('get_high_fact_over_group')
+
+def have_little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('place_or_day')
