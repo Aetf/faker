166a3ed77b5e75f190a98d781fb515f445a26ddb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 10:09:09 -0500

    "rm: use super/point_or_bad_company/eye/part_and_public_man/other_man_or_good_eye.py, super/big_eye_or_day/give_woman/place/large_life_or_time.py, super/woman.py and super/last_group/public_point/fact_or_point/day/right_problem.py over early work"

---
+++ super/big_eye_or_day/give_woman/place/large_life_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week_and_other_woman(str_arg):
+    life(str_arg)
+    print('hand_or_life')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week_and_other_woman('right_problem')
---
+++ super/point_or_bad_company/eye/part_and_public_man/other_man_or_good_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_woman_or_same_eye(str_arg):
+    hand(str_arg)
+    print('fact')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_woman_or_same_eye('feel_man_into_public_point')
