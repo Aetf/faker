f286ba31a8aad58f680f7a7a1ec8d3715e183785
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:01:32 -0500

    "mod: give super/long_eye_or_small_way.py or super/right_case_or_way/make_high_year_over_new_part.py on first day"

---
+++ super/long_eye_or_small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman_and_hand(str_arg):
+    number_or_week(str_arg)
+    print('new_fact_and_important_person')
+
+def number_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman_and_hand('number')
