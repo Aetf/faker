a0bf7450aedf3355bd69292fa296d4a3de58bf39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:12:00 -0500

    "add: try super/good_child_and_place/good_group/early_eye.py, super/group_or_life.py, super/woman/week/woman_or_company.py and super/try_able_thing_for_next_work.py under long thing"

---
+++ super/try_able_thing_for_next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    problem(str_arg)
+    print('next_eye')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('bad_life')
