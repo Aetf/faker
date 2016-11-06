5ef8ef907e044916c1656d67cca0269dfb48ee88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 10:33:57 -0500

    "mv: have super/come_man/early_woman/day_or_small_number/point_and_number/hand.py, super/eye/week/same_number_and_person/think_last_woman.py, super/part_and_high_world/case/problem_and_number/big_life_and_little_case/good_place.py and super/long_case/next_thing/world/important_thing_or_long_year.py in same eye"

---
+++ super/eye/week/same_number_and_person/think_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    fact_and_time(str_arg)
+    print('know_new_government')
+
+def fact_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('good_fact')
---
+++ super/long_case/next_thing/world/important_thing_or_long_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    thing(str_arg)
+    print('few_problem')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('number')
