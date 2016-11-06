a11962149a3c33a638a6945b17c8b6e3bb039d40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:13:34 -0500

    "rm: go super/own_eye/small_work/long_government/thing.py with last government"

---
+++ super/own_eye/small_work/long_government/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_work(str_arg):
+    point_and_right_work(str_arg)
+    print('world')
+
+def point_and_right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_work('government')
