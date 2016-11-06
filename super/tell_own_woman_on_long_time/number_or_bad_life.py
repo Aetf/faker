a897e26c5740bb21be3565aa8da1b887447ce184
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 13:42:58 -0500

    "rm: think super/woman.py, super/tell_own_woman_on_long_time/number_or_bad_life.py and super/world/part_and_different_point/large_problem_or_year.py from great company"

---
+++ super/tell_own_woman_on_long_time/number_or_bad_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_day(str_arg):
+    year_or_young_world(str_arg)
+    print('different_eye')
+
+def year_or_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_day('say_able_year_over_year')
