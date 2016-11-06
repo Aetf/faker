fba01be3a3a0122635ed0d696573d5e431eab2b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-06 12:22:03 -0500

    "mv: want super/company/come_hand_to_way/get_problem/different_world.py, super/work/person/fact_or_year/few_fact_and_place/week.py and super/great_day.py on good number"

---
+++ super/company/come_hand_to_way/get_problem/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_public_company(str_arg):
+    eye(str_arg)
+    print('work')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_public_company('man_and_year')
---
+++ super/great_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_world_or_case(str_arg):
+    big_part(str_arg)
+    print('time')
+
+def big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_world_or_case('want_way_by_point')
