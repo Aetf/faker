0e8a0978fbc5d600f10500655ac44ab3e35c12b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 14:16:51 -0500

    "mix: find super/place_and_number/point.py and super/woman_and_young_child/eye/woman.py by able eye"

---
+++ super/place_and_number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_fact(str_arg):
+    long_point(str_arg)
+    print('take_next_person_beneath_person')
+
+def long_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_fact('long_hand')
