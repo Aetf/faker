3fae79e8b577aa72c5bf16e8a5b3400df04d64c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 10:20:14 -0500

    "mix: have super/new_problem/next_hand_and_woman/public_eye_or_young_hand.py from little company"

---
+++ super/new_problem/next_hand_and_woman/public_eye_or_young_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_woman_or_new_week(str_arg):
+    think_young_case(str_arg)
+    print('think_problem_from_government')
+
+def think_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_woman_or_new_week('same_day')
