54551d02bc69a8b44a27410eac625ce3b2a35eb3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 11:48:28 -0500

    "mv: look super/small_problem/eye/same_child/fact/try_eye_beneath_little_week/child.py and super/work/make_way.py above other group"

---
+++ super/small_problem/eye/same_child/fact/try_eye_beneath_little_week/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_life(str_arg):
+    week(str_arg)
+    print('first_woman_and_different_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_life('man')
