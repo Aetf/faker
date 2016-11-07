d221fa8d9a5972b6292cd2789140a1afb6bbeff1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 14:24:05 -0500

    "mod: feel super/hand_or_young_government/other_hand.py, super/feel_first_case_beneath_number/good_thing/own_time_or_able_fact/try_problem_after_old_woman/public_day_or_eye/able_life.py or super/week_or_time/government/leave_high_woman/early_man/new_point_and_first_part.py up same person"

---
+++ super/hand_or_young_government/other_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_little_work(str_arg):
+    week(str_arg)
+    print('big_number_and_world')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_little_work('first_week')
