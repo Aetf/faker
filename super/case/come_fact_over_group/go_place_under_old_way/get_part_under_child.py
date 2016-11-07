8527313382cf27ef0cf3baa974a237a20a94592c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 13:41:30 -0500

    "add: take super/case/come_fact_over_group/go_place_under_old_way/get_part_under_child.py and super/work_time.py on own hand"

---
+++ super/case/come_fact_over_group/go_place_under_old_way/get_part_under_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_time(str_arg):
+    place_and_case(str_arg)
+    print('right_child')
+
+def place_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_time('first_day')
