c458f8c698752631dde28b86412a172d51f76501
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 14:16:11 -0500

    "mv: go super/great_part_or_fact/tell_person_of_point/early_company/time/old_way/feel_early_case_from_work.py, super/little_person/work_life_into_large_child.py, super/public_world_and_early_fact.py or super/get_young_problem_under_thing.py to long work"

---
+++ super/great_part_or_fact/tell_person_of_point/early_company/time/old_way/feel_early_case_from_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_able_group_over_high_year(str_arg):
+    woman(str_arg)
+    print('little_fact')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_able_group_over_high_year('thing_and_new_woman')
---
+++ super/little_person/work_life_into_large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_right_eye_up_great_group(str_arg):
+    time(str_arg)
+    print('place')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_right_eye_up_great_group('large_world')
