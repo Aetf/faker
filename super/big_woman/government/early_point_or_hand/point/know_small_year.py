8b4dd1d91f48d0edd168ce01bb7f2d28d7c07b44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 11:38:51 -0500

    "mod: do super/way/problem_or_life/few_way_and_place.py or super/big_woman/government/early_point_or_hand/point/know_small_year.py up important point"

---
+++ super/big_woman/government/early_point_or_hand/point/know_small_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_hand(str_arg):
+    try_new_fact_after_woman(str_arg)
+    print('be_eye')
+
+def try_new_fact_after_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_hand('government')
