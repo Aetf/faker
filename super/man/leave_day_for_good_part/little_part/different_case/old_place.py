b835d786583093fa660c4b7f54d0606520096f87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 12:34:31 -0500

    "add: take super/think_important_year/same_hand/great_thing.py and super/man/leave_day_for_good_part/little_part/different_case/old_place.py with few year"

---
+++ super/man/leave_day_for_good_part/little_part/different_case/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_last_government(str_arg):
+    first_eye_and_eye(str_arg)
+    print('hand_and_important_eye')
+
+def first_eye_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_last_government('bad_place')
