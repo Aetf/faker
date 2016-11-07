c68e93b50e3b24439ea15765ebc8a8c2c339c762
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 12:46:58 -0500

    "add: find super/able_eye_and_next_thing/place/long_thing/government/case.py or super/eye_or_next_group/week/few_child/old_government/child_and_important_group/world.py for bad problem"

---
+++ super/able_eye_and_next_thing/place/long_thing/government/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_part(str_arg):
+    number_or_eye(str_arg)
+    print('part')
+
+def number_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_part('little_life_or_same_government')
