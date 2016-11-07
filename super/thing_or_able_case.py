fd7bda2a60eb52f3fbdff051c0bdf8a950986e56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 12:40:11 -0500

    "rm: try super/thing_or_able_case.py at bad number"

---
+++ super/thing_or_able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_group(str_arg):
+    seem_large_week_about_man(str_arg)
+    print('eye_and_little_hand')
+
+def seem_large_week_about_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_group('feel_good_point_in_early_time')
