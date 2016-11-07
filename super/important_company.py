81f55ca3234a66eff41c8fefc5ff16443dec9826
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 13:02:42 -0500

    "mix: leave super/tell_way_about_woman.py and super/important_company.py for small work"

---
+++ super/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_and_able_group(str_arg):
+    last_world(str_arg)
+    print('child')
+
+def last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_and_able_group('place_or_eye')
