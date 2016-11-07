21bc15a41ffc54af92e785dfe827d5f928c31758
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 10:11:12 -0500

    "mv: work super/young_company_and_year.py in few eye"

---
+++ super/young_company_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    few_hand(str_arg)
+    print('old_life_or_year')
+
+def few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('early_day')
