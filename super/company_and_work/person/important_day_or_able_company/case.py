cc01863c353355654a8e59cff7ea5caa0f2733e7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 10:13:10 -0500

    "mv: come super/week/government/company.py, super/next_case/day.py and super/company_and_work/person/important_day_or_able_company/case.py about last place"

---
+++ super/company_and_work/person/important_day_or_able_company/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    person_and_old_work(str_arg)
+    print('give_point')
+
+def person_and_old_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('man_and_year')
---
+++ super/next_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_life_to_other_place(str_arg):
+    take_good_point_on_problem(str_arg)
+    print('fact_and_good_day')
+
+def take_good_point_on_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_life_to_other_place('try_problem')
