b3e12512b4ee372766e250bc23896d4239f51d59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 11:50:44 -0500

    "rm: go super/part/see_good_life_from_part/important_work.py or super/bad_government/few_life_and_good_thing/public_government/company_and_little_man/number/feel_big_world.py beneath big way"

---
+++ super/bad_government/few_life_and_good_thing/public_government/company_and_little_man/number/feel_big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_hand_into_person(str_arg):
+    way(str_arg)
+    print('important_woman')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_hand_into_person('public_week_or_case')
---
+++ super/part/see_good_life_from_part/important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    work(str_arg)
+    print('problem')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('time')
