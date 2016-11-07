68ffdd1bd93370830537a56ba8c2639b4a282f0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 11:06:05 -0500

    "mv: find super/same_child/child/try_place_with_good_point.py and super/do_week_above_place.py into large man"

---
+++ super/same_child/child/try_place_with_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_work(str_arg):
+    eye_or_next_eye(str_arg)
+    print('little_company')
+
+def eye_or_next_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_work('case')
