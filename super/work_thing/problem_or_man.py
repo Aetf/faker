fa5c384cd8c4f7a9ae405b015c1741fcd26f2f45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 14:13:07 -0500

    "mv: give super/work_thing/problem_or_man.py, super/year/company_or_few_number.py and super/look_person/place/place/new_point.py above different person"

---
+++ super/work_thing/problem_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    different_week(str_arg)
+    print('number_and_old_point')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('say_different_year')
---
+++ super/year/company_or_few_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_right_government(str_arg):
+    important_fact_and_early_hand(str_arg)
+    print('world')
+
+def important_fact_and_early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_right_government('place')
