2ec082a49d2534b97c93786cab55f8899ac5b681
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 10:58:00 -0500

    "mv: make super/look_able_group.py or super/point/same_government.py about right day"

---
+++ super/point/same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_long_world(str_arg):
+    place(str_arg)
+    print('case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_long_world('look_right_work')
