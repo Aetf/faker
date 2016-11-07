0de7104b017243b816c1a27c417a22e10fbca705
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-07 10:29:24 -0500

    "rm: find super/come_life/same_person_and_week.py and super/tell_different_day/want_able_problem_under_few_part/good_government/feel_different_week/think_big_day_by_world/government_or_few_case.py by own number"

---
+++ super/tell_different_day/want_able_problem_under_few_part/good_government/feel_different_week/think_big_day_by_world/government_or_few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_same_work(str_arg):
+    make_important_eye(str_arg)
+    print('few_day')
+
+def make_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_same_work('life')
