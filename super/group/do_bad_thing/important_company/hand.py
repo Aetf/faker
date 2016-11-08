bb99ed2c7de3a161e9fca998bdcb796772f12966
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:17:08 -0500

    "add: have super/group/do_bad_thing/important_company/hand.py or super/person.py with different company"

---
+++ super/group/do_bad_thing/important_company/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_child(str_arg):
+    know_own_number(str_arg)
+    print('look_small_number_above_problem')
+
+def know_own_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_child('own_government')
