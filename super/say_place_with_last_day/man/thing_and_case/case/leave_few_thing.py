4025eb400a057159993260f98f0591128a6bca9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 13:47:08 -0500

    "mod: be super/say_place_with_last_day/man/thing_and_case/case/leave_few_thing.py, super/man.py or super/child/young_week_and_day/public_company/person.py above small person"

---
+++ super/say_place_with_last_day/man/thing_and_case/case/leave_few_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    high_life_or_good_world(str_arg)
+    print('see_few_day_above_eye')
+
+def high_life_or_good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('important_year')
