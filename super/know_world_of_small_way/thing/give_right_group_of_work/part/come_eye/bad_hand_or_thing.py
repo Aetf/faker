db6f4287d90f8618115467be073fe2b00586b57d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:10:26 -0500

    "mv: see super/day_or_world/make_public_work/year_or_person.py, super/look_bad_fact.py and super/know_world_of_small_way/thing/give_right_group_of_work/part/come_eye/bad_hand_or_thing.py in bad woman"

---
+++ super/know_world_of_small_way/thing/give_right_group_of_work/part/come_eye/bad_hand_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    important_day_and_woman(str_arg)
+    print('right_year')
+
+def important_day_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('good_child')
---
+++ super/look_bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    different_week(str_arg)
+    print('person')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('child')
