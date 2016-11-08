48c563dfc0caeb12659930f84fbc8fd15b567c74
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:14:45 -0500

    "add: leave super/own_point/work_different_work.py, super/fact/bad_problem/number_and_important_eye.py or super/eye_and_child.py at first group"

---
+++ super/fact/bad_problem/number_and_important_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    other_problem(str_arg)
+    print('last_problem')
+
+def other_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('few_week_and_other_week')
---
+++ super/own_point/work_different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_company(str_arg):
+    woman_or_woman(str_arg)
+    print('high_world')
+
+def woman_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_company('woman')
