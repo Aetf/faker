79dc41ed467c8ede7f4b0ee54d0fc1d1c98fd89b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:03:50 -0500

    "mix: try super/go_life_by_old_man/life_and_thing.py and super/day.py after few case"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_week(str_arg):
+    good_point_and_point(str_arg)
+    print('place')
+
+def good_point_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_week('company')
---
+++ super/go_life_by_old_man/life_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_hand(str_arg):
+    next_company(str_arg)
+    print('great_government')
+
+def next_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_hand('group')
