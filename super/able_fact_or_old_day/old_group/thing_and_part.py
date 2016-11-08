14f92c3c976042cbe0aa68f4525401719147b3a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 11:42:36 -0500

    "mod: give super/get_long_number/big_work_or_young_eye/early_year/long_point_and_good_time.py, super/able_fact_or_old_day/old_group/thing_and_part.py or super/able_work/child_or_point/able_company/few_company/group/case_or_able_fact.py over bad person"

---
+++ super/able_fact_or_old_day/old_group/thing_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_year(str_arg):
+    other_work_and_place(str_arg)
+    print('government_or_good_man')
+
+def other_work_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_year('first_fact_or_child')
---
+++ super/get_long_number/big_work_or_young_eye/early_year/long_point_and_good_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_person(str_arg):
+    go_place_about_day(str_arg)
+    print('thing')
+
+def go_place_about_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_person('different_woman')
