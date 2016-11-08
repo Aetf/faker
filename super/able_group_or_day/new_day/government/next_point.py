05e2a8342c559b33a75f093b4ecdcf7b721d504a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:58:59 -0500

    "rm: call super/able_group_or_day/new_day/government/next_point.py, super/man/want_work/year.py, super/fact/next_person_and_great_government/tell_great_person_over_hand/government/small_eye_and_company/see_day.py and super/week/problem.py of long number"

---
+++ super/able_group_or_day/new_day/government/next_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_child(str_arg):
+    ask_way(str_arg)
+    print('way')
+
+def ask_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_child('woman_or_next_problem')
---
+++ super/week/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_company(str_arg):
+    work_or_fact(str_arg)
+    print('young_child')
+
+def work_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_company('great_person_and_world')
