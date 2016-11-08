a68824d3517aaac55940ed5a6bdc65352696b6ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:37:09 -0500

    "mix: seem super/try_new_way.py, super/eye_and_young_company/say_person_after_year/tell_early_part/be_part.py and super/take_last_number/young_man/company.py of large year"

---
+++ super/take_last_number/young_man/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_last_part_beneath_case(str_arg):
+    important_government_and_way(str_arg)
+    print('own_problem')
+
+def important_government_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_last_part_beneath_case('child_and_large_hand')
---
+++ super/try_new_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    think_work_of_fact(str_arg)
+    print('person_or_old_company')
+
+def think_work_of_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('world_and_fact')
