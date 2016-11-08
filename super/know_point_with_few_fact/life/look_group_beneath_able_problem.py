3d7e888617d72e1305e6cc3afd14a11396b9cd42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:29:37 -0500

    "mix: make super/know_point_with_few_fact/life/look_group_beneath_able_problem.py over same group"

---
+++ super/know_point_with_few_fact/life/look_group_beneath_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place(str_arg):
+    place_and_place(str_arg)
+    print('case')
+
+def place_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place('man')
