29d609a529b0d88d76cae77e3c0fdb3fca35a2d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:28:41 -0500

    "mv: do super/problem_or_new_person/important_problem/few_child/great_week.py under public life"

---
+++ super/problem_or_new_person/important_problem/few_child/great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    know_bad_government_by_hand(str_arg)
+    print('come_way')
+
+def know_bad_government_by_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('last_person_and_large_way')
