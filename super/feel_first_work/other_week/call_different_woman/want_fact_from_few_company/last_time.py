d915f88e1dd8d75cc85f6781f1669b4da2946015
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 11:38:35 -0500

    "rm: say super/give_large_case_from_year.py, super/other_company/see_small_year_under_person/come_way/old_year_and_old_number/way/thing.py or super/feel_first_work/other_week/call_different_woman/want_fact_from_few_company/last_time.py at large point"

---
+++ super/feel_first_work/other_week/call_different_woman/want_fact_from_few_company/last_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    public_way(str_arg)
+    print('ask_early_place')
+
+def public_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('old_time')
---
+++ super/give_large_case_from_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_last_woman(str_arg):
+    little_person_and_young_week(str_arg)
+    print('case')
+
+def little_person_and_young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_last_woman('case')
