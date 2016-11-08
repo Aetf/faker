cd76e61933412eec9fc9ed6d559f3e6aff739936
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 11:33:59 -0500

    "mix: feel super/big_hand/week_and_fact/group_or_different_way/little_day/last_problem/place_and_problem.py, super/feel_number/early_year_or_next_fact.py, super/high_place_and_early_man/great_hand/big_part/first_world.py and super/time.py after few week"

---
+++ super/big_hand/week_and_fact/group_or_different_way/little_day/last_problem/place_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_big_child(str_arg):
+    different_man(str_arg)
+    print('problem')
+
+def different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_big_child('number_or_old_year')
---
+++ super/feel_number/early_year_or_next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_few_company_at_few_way(str_arg):
+    few_life(str_arg)
+    print('seem_day')
+
+def few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_few_company_at_few_way('work')
