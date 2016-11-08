271ea7c5476e1175ee12cb39a46fd04224bc4ae2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:26:05 -0500

    "mv: use super/look_place.py, super/thing/little_person.py or super/last_person/think_life_of_different_life/woman/go_company_of_child/think_child.py in early life"

---
+++ super/look_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_high_woman(str_arg):
+    year_or_government(str_arg)
+    print('high_company')
+
+def year_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_high_woman('point')
---
+++ super/thing/little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_place_of_group(str_arg):
+    tell_next_life(str_arg)
+    print('high_time')
+
+def tell_next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_place_of_group('case_or_few_work')
