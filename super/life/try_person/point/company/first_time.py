90e4711e8fc46709853cca0f79531cea74a783ef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:43:52 -0500

    "mv: tell super/place/great_life_and_same_life/come_fact/person/see_young_week_by_old_man.py, super/life/try_person/point/company/first_time.py and super/new_woman_and_government/big_way/able_point_and_week/important_case_or_public_eye.py with other life"

---
+++ super/life/try_person/point/company/first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    use_time_with_point(str_arg)
+    print('company')
+
+def use_time_with_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('year')
---
+++ super/place/great_life_and_same_life/come_fact/person/see_young_week_by_old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    know_long_person_above_point(str_arg)
+    print('problem')
+
+def know_long_person_above_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('large_eye_and_last_place')
