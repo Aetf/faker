a35e2080c0ca194b59b30d05a0ff64faa58266d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 10:09:39 -0500

    "add: make super/important_problem.py and super/little_work/have_good_problem/man.py over bad week"

---
+++ super/little_work/have_good_problem/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_way_and_little_thing(str_arg):
+    give_fact(str_arg)
+    print('public_company')
+
+def give_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_way_and_little_thing('person_or_important_man')
