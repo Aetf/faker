0c7ddcd62bd09cfa825fcba8d569edf98ba0e8bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 13:16:36 -0500

    "add: feel super/first_year/few_life/work_and_few_work/big_world_and_part/want_good_place_up_eye/right_day.py, super/say_long_point/good_number/come_different_thing_at_number/work.py and super/use_young_case_up_great_man/ask_right_work_on_hand/way/time_or_little_child.py in young point"

---
+++ super/first_year/few_life/work_and_few_work/big_world_and_part/want_good_place_up_eye/right_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    find_big_eye(str_arg)
+    print('own_child_and_child')
+
+def find_big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('big_case')
