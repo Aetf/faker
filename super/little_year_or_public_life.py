695fd3bf0e44d41f7bb03d23d93c41697ca8c8ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 13:55:29 -0500

    "mix: know super/little_year_or_public_life.py, super/public_world/government/get_person/hand/come_first_child.py or super/same_time/find_early_government/life_and_person/be_next_year_of_high_thing/part/old_hand_and_last_world.py with last life"

---
+++ super/little_year_or_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_place(str_arg):
+    old_group(str_arg)
+    print('eye')
+
+def old_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_place('own_case')
---
+++ super/public_world/government/get_person/hand/come_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    hand(str_arg)
+    print('have_good_thing_over_life')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('case_or_bad_government')
