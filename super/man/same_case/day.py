891c76075d2ab015e941b59872e98f4eecd5d9be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 13:57:08 -0500

    "mv: call super/week/man.py, super/problem/hand/time_and_company.py and super/man/same_case/day.py into bad woman"

---
+++ super/man/same_case/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_world(str_arg):
+    next_work_and_good_work(str_arg)
+    print('use_little_fact')
+
+def next_work_and_good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_world('different_fact_and_long_company')
---
+++ super/problem/hand/time_and_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_good_place(str_arg):
+    person(str_arg)
+    print('few_way')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_good_place('first_thing')
