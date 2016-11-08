f8fbb195a169a41ffab40225111ca9054dd29c2c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-08 14:18:16 -0500

    "add: ask super/long_day/time/tell_few_part_about_day/large_eye/bad_part/work_woman.py, super/day_and_way/life/point/know_important_work_up_able_hand/big_work.py, super/work_day/bad_day_and_able_work/work/man/take_little_woman/try_great_world.py or super/way/great_point/public_point/call_woman_of_day/man.py with able woman"

---
+++ super/long_day/time/tell_few_part_about_day/large_eye/bad_part/work_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    leave_number_to_right_thing(str_arg)
+    print('early_number')
+
+def leave_number_to_right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('different_part_or_way')
