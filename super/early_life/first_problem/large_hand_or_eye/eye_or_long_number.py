ed386a2d9ed3b367fd65689f85d5b75704e93e7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 10:50:51 -0500

    "mix: go super/early_life/first_problem/large_hand_or_eye/eye_or_long_number.py, super/important_year/hand/work/bad_part.py and super/want_child_by_year/time/part_and_thing.py to good time"

---
+++ super/early_life/first_problem/large_hand_or_eye/eye_or_long_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    big_problem(str_arg)
+    print('thing')
+
+def big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('few_year')
---
+++ super/important_year/hand/work/bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_high_group_for_man(str_arg):
+    see_work(str_arg)
+    print('right_number')
+
+def see_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_high_group_for_man('life')
