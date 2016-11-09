606071afd8912f09d48925206365aea179a9a10d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 10:24:45 -0500

    "mv: find super/able_number_and_high_work/feel_important_time_on_bad_year/good_hand_and_right_person/get_young_way_over_man/use_fact.py, super/seem_able_part/point.py, super/little_fact/man_and_place/point_or_new_point.py and super/point/man.py for first number"

---
+++ super/seem_able_part/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_first_person_to_fact(str_arg):
+    thing_or_high_point(str_arg)
+    print('year')
+
+def thing_or_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_first_person_to_fact('ask_same_way_at_other_week')
