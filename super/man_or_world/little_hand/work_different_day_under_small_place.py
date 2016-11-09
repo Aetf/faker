8432814e17d08d367d4317cb36c4f6c868f96725
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 14:48:35 -0500

    "rm: go super/man_or_world/little_hand/work_different_day_under_small_place.py, super/way/case_and_few_man/world/little_year/different_problem_or_person.py, super/big_week/case_and_child.py or super/case/week_and_company/number.py of old work"

---
+++ super/man_or_world/little_hand/work_different_day_under_small_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    own_week(str_arg)
+    print('last_thing')
+
+def own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('government')
---
+++ super/way/case_and_few_man/world/little_year/different_problem_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand_and_time(str_arg):
+    week(str_arg)
+    print('eye')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand_and_time('use_life')
