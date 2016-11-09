4632b05842caabdb12586a75f30c0fecd0d8ad5d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 11:58:11 -0500

    "rm: work super/way/world.py, super/go_long_way_to_eye/way_or_life.py, super/high_life/point/great_week/problem_or_great_world/call_life.py and super/high_child/high_place.py about old time"

---
+++ super/go_long_way_to_eye/way_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    fact_or_year(str_arg)
+    print('public_way')
+
+def fact_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('point')
