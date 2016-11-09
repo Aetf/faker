b51c605dcc28fa7b3a65a974485281a1e521b4e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 13:22:41 -0500

    "rm: leave super/take_young_child_over_company.py, super/government_and_good_point.py and super/go_government_about_own_company/same_work_or_eye/new_woman/see_able_world.py to public woman"

---
+++ super/go_government_about_own_company/same_work_or_eye/new_woman/see_able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_week(str_arg):
+    woman_and_last_point(str_arg)
+    print('little_number')
+
+def woman_and_last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_week('big_eye_or_good_year')
---
+++ super/take_young_child_over_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_problem(str_arg):
+    tell_bad_year(str_arg)
+    print('group_and_hand')
+
+def tell_bad_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_problem('company')
