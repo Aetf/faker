b8b9ffe4af1a4ca7204ea6029baac36fa306b7c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 11:15:16 -0500

    "add: see super/feel_world/small_hand/group_and_place.py and super/week/have_way/early_woman_or_company/place_or_big_problem/right_work.py over large life"

---
+++ super/feel_world/small_hand/group_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_place(str_arg):
+    look_long_day(str_arg)
+    print('use_same_hand_with_long_company')
+
+def look_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_place('public_company')
