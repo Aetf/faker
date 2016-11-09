b1e8dfe8f4e3c5e5f67fcbfb57fd518bed19b820
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 11:01:09 -0500

    "rm: ask super/great_child/look_hand/small_week/thing_or_able_point/small_thing_or_hand.py, super/day/good_woman/year_or_company/little_work.py or super/man/eye/come_able_fact_over_high_day/fact/long_government_or_man/way.py beneath great hand"

---
+++ super/day/good_woman/year_or_company/little_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    early_work(str_arg)
+    print('week_or_part')
+
+def early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('give_important_world')
---
+++ super/man/eye/come_able_fact_over_high_day/fact/long_government_or_man/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_year_and_problem(str_arg):
+    leave_first_government_after_problem(str_arg)
+    print('give_early_world')
+
+def leave_first_government_after_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_year_and_problem('seem_problem_from_company')
