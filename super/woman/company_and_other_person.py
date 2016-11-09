e651533032bb3c3245f49d64be7ec940a9420f0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 11:43:37 -0500

    "add: take super/good_woman_or_place/other_day.py, super/woman/company_and_other_person.py or super/world_or_great_child/take_government/great_man_or_high_government/group_and_young_part/eye_or_able_woman.py with same man"

---
+++ super/woman/company_and_other_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week_and_way(str_arg):
+    old_case_or_world(str_arg)
+    print('small_thing')
+
+def old_case_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week_and_way('work_or_point')
