464d8a801210bf931fca1e2f4679e0551a0798c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 13:23:08 -0500

    "rm: want super/great_world/problem/young_part/give_little_time_beneath_own_government.py, super/hand_or_small_life.py and super/tell_other_life_into_young_work/time/right_way/government/try_group/year.py above other time"

---
+++ super/great_world/problem/young_part/give_little_time_beneath_own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    take_place_from_important_time(str_arg)
+    print('child')
+
+def take_place_from_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('important_group')
---
+++ super/tell_other_life_into_young_work/time/right_way/government/try_group/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_man(str_arg):
+    next_child(str_arg)
+    print('bad_man_or_long_hand')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_man('work_or_different_hand')
