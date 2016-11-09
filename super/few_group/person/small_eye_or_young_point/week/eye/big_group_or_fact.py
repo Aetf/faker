bb9dd7c1c203dec867544e22a9f12167c83a124b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 10:25:44 -0500

    "add: give super/right_person/use_important_week_for_first_place/use_right_world.py, super/few_group/person/small_eye_or_young_point/week/eye/big_group_or_fact.py and super/old_day/point_and_public_work.py over large woman"

---
+++ super/few_group/person/small_eye_or_young_point/week/eye/big_group_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_fact_or_child(str_arg):
+    see_thing_on_early_case(str_arg)
+    print('ask_time')
+
+def see_thing_on_early_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_fact_or_child('few_life_and_few_thing')
---
+++ super/old_day/point_and_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman_and_small_part(str_arg):
+    woman(str_arg)
+    print('company_or_own_way')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman_and_small_part('new_problem_or_able_person')
