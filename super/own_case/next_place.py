f37ff53b2a7f10a186b4e52d589640e74ea77674
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 12:01:13 -0500

    "mix: feel super/get_little_problem/time/call_number_into_big_person/man/other_company.py, super/own_case/next_place.py, super/world_or_young_group/ask_long_thing_about_life/same_place_and_good_group/child/first_case.py and super/case/public_week.py with little group"

---
+++ super/own_case/next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    make_man(str_arg)
+    print('week_or_life')
+
+def make_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('last_case_or_important_number')
---
+++ super/world_or_young_group/ask_long_thing_about_life/same_place_and_good_group/child/first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_day(str_arg):
+    woman_and_right_way(str_arg)
+    print('go_year')
+
+def woman_and_right_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_day('time')
