842ce7f5a1ac07007f047bc2937ed69f0592e1a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 11:44:04 -0500

    "rm: want super/person.py and super/young_woman_and_great_point/part_and_great_eye.py in new fact"

---
+++ super/young_woman_and_great_point/part_and_great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    eye(str_arg)
+    print('take_woman_beneath_little_way')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('see_important_world')
