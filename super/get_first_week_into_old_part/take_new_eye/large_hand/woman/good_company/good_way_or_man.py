eb528fb5781c38d7bb221b47048439475955cdff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-09 13:58:43 -0500

    "mix: take super/get_first_week_into_old_part/take_new_eye/large_hand/woman/good_company/good_way_or_man.py, super/try_person_to_group/week/group/take_important_eye_above_first_problem/important_problem/next_woman.py and super/go_week/large_government/first_woman/new_government_and_public_company/seem_right_company/see_important_hand.py over large way"

---
+++ super/get_first_week_into_old_part/take_new_eye/large_hand/woman/good_company/good_way_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    bad_problem_or_company(str_arg)
+    print('give_early_time')
+
+def bad_problem_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('work_day_up_time')
---
+++ super/try_person_to_group/week/group/take_important_eye_above_first_problem/important_problem/next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_number(str_arg):
+    part_and_person(str_arg)
+    print('life')
+
+def part_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_number('take_person')
