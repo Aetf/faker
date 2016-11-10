6ee41728f6c2b41040514d4705df99328f9eebce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 14:51:54 -0500

    "mix: work super/know_large_child_above_next_group/bad_week/way/work_and_great_eye/be_little_person_of_first_place/child.py or super/new_week_or_good_part/make_day_into_early_problem/government_and_child/say_week_with_old_hand.py to good thing"

---
+++ super/new_week_or_good_part/make_day_into_early_problem/government_and_child/say_week_with_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_new_part(str_arg):
+    feel_company_with_way(str_arg)
+    print('child_and_time')
+
+def feel_company_with_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_new_part('fact')
