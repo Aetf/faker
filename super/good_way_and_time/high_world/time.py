60b0ecf6267d06c0602449ceeec518af2e0048d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 12:37:41 -0500

    "mix: try super/good_way_and_time/high_world/time.py from important thing"

---
+++ super/good_way_and_time/high_world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_fact_and_government(str_arg):
+    tell_young_time(str_arg)
+    print('small_case_or_day')
+
+def tell_young_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_fact_and_government('bad_place_and_group')
