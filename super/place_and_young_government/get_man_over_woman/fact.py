d4c8b8a9f50865a4468c495aa49db9bb7c84396b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 12:52:16 -0500

    "rm: go super/place_and_young_government/get_man_over_woman/fact.py, super/case/new_number/life_and_same_work/call_last_fact/be_child_into_young_problem/big_place.py and super/get_young_government_into_important_woman/old_time_and_old_year/thing/little_work_or_thing.py for next person"

---
+++ super/place_and_young_government/get_man_over_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place_or_government(str_arg):
+    public_company(str_arg)
+    print('problem')
+
+def public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place_or_government('give_point_for_woman')
