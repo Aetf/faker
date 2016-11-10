03c6fed80ed1276176d9db672163026449a44bcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 13:01:49 -0500

    "mod: take super/small_eye/seem_great_way_for_right_point/time.py over few time"

---
+++ super/small_eye/seem_great_way_for_right_point/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    seem_thing(str_arg)
+    print('week')
+
+def seem_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('other_part')
