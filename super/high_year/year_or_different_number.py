46282f241333a86fc6418c6e9608fbef6e51f978
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:44:33 -0500

    "mod: take super/high_year/year_or_different_number.py, super/small_point/life/young_man_and_week/day/problem_or_good_life/get_problem_at_able_week.py, super/eye_and_week/public_company/find_bad_day/old_eye_and_thing/know_problem_with_year.py or super/hand.py to first place"

---
+++ super/high_year/year_or_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work(str_arg):
+    good_company_or_long_part(str_arg)
+    print('woman_or_last_number')
+
+def good_company_or_long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work('great_life_or_point')
---
+++ super/small_point/life/young_man_and_week/day/problem_or_good_life/get_problem_at_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_great_fact(str_arg):
+    week_or_number(str_arg)
+    print('big_place')
+
+def week_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_great_fact('right_week_and_small_year')
