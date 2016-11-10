7c1ff8f89e8bde99273355572416569ef836ff6c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 14:03:43 -0500

    "mv: take super/few_work/hand/company_and_eye/year_or_right_point/child.py, super/high_man/tell_work/place_and_next_life/leave_old_way_on_time/want_able_case/big_week_and_public_fact.py and super/point_or_eye/group/world/have_young_thing_with_large_case/person.py under new eye"

---
+++ super/few_work/hand/company_and_eye/year_or_right_point/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_case(str_arg):
+    use_new_problem_under_different_group(str_arg)
+    print('different_part')
+
+def use_new_problem_under_different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_case('thing')
