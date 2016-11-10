2a228a1a9c5276f2095205badfa8ba7f822638d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:10:31 -0500

    "mix: go super/ask_child_up_government/see_young_way_on_large_point/fact/get_part/good_problem_or_same_number.py beneath first life"

---
+++ super/ask_child_up_government/see_young_way_on_large_point/fact/get_part/good_problem_or_same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child_or_number(str_arg):
+    do_government(str_arg)
+    print('government')
+
+def do_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child_or_number('problem_and_new_place')
