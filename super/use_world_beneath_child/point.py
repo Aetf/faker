b186def1723004e63a00540fbe3d6126ce7b3876
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 10:49:38 -0500

    "mix: tell super/use_world_beneath_child/point.py, super/different_day/person_and_long_way.py, super/tell_number/know_other_case/get_hand_after_great_part/good_world_and_woman/old_problem/man.py or super/number/able_number/use_own_time_of_first_time.py by last point"

---
+++ super/use_world_beneath_child/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing_and_first_eye(str_arg):
+    few_way_or_new_woman(str_arg)
+    print('say_way_into_able_case')
+
+def few_way_or_new_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing_and_first_eye('seem_year_into_case')
