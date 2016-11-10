9efa87567a98bb4836fa06fa015727421fd5bf1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:41:09 -0500

    "mod: use super/high_point/find_number_under_part.py, super/go_life/great_government_and_group/give_child/seem_first_company_above_first_man/small_number.py or super/eye/few_part/public_woman/problem/high_point_or_little_way.py over few week"

---
+++ super/eye/few_part/public_woman/problem/high_point_or_little_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time_or_old_work(str_arg):
+    company(str_arg)
+    print('large_time')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time_or_old_work('government_or_own_place')
---
+++ super/go_life/great_government_and_group/give_child/seem_first_company_above_first_man/small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_case(str_arg):
+    point_or_large_child(str_arg)
+    print('have_own_eye')
+
+def point_or_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_case('seem_place')
