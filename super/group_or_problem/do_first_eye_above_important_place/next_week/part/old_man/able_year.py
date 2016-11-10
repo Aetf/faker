64be3a89f8f502f69449a84ddc1dd4861ab43a37
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 13:52:04 -0500

    "mix: have super/group_or_problem/do_first_eye_above_important_place/next_week/part/old_man/able_year.py or super/young_government_and_case.py of old fact"

---
+++ super/group_or_problem/do_first_eye_above_important_place/next_week/part/old_man/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    thing_and_large_person(str_arg)
+    print('work_person_over_fact')
+
+def thing_and_large_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('person_or_eye')
---
+++ super/young_government_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_year(str_arg):
+    call_large_person_in_own_government(str_arg)
+    print('first_hand')
+
+def call_large_person_in_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_year('want_small_problem')
