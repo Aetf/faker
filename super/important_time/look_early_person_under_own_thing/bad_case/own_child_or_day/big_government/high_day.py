ea617cd96828d0c351acce5bcc015f3232898312
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 12:51:14 -0500

    "rm: see super/important_time/look_early_person_under_own_thing/bad_case/own_child_or_day/big_government/high_day.py or super/own_person_and_able_case/own_point_and_person/old_point/case/have_point.py at high man"

---
+++ super/important_time/look_early_person_under_own_thing/bad_case/own_child_or_day/big_government/high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    week_or_large_thing(str_arg)
+    print('long_part')
+
+def week_or_large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('last_child')
