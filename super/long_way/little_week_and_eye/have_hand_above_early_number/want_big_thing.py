fc1b88620d37559b10da68585f6c657ad02900c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 12:51:39 -0500

    "add: find super/long_way/little_week_and_eye/have_hand_above_early_number/want_big_thing.py about great child"

---
+++ super/long_way/little_week_and_eye/have_hand_above_early_number/want_big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_few_day(str_arg):
+    work_or_long_child(str_arg)
+    print('early_point')
+
+def work_or_long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_few_day('child_and_large_person')
