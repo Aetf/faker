fa93ecb8ce686321ce719a89f37c137d607dcd89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:40:51 -0500

    "mv: give super/life_or_public_day/important_government/make_group.py, super/woman_and_child/same_woman/place.py, super/little_week/thing_or_few_world/child/give_own_man/little_point/fact_and_large_hand.py or super/important_year_and_different_man/number/get_little_woman/way/way/group_and_hand.py in high child"

---
+++ super/little_week/thing_or_few_world/child/give_own_man/little_point/fact_and_large_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    number(str_arg)
+    print('company_and_man')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('work')
---
+++ super/woman_and_child/same_woman/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_week(str_arg):
+    time(str_arg)
+    print('bad_company')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_week('big_work_and_right_day')
