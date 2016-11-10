1723e6956879228b86638e0d7100ad3be223ff46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 13:41:53 -0500

    "mv: think super/person_or_place/ask_long_point_from_world/great_point/bad_year/government.py or super/look_big_number/right_work/ask_high_woman_under_part.py by other man"

---
+++ super/person_or_place/ask_long_point_from_world/great_point/bad_year/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_life(str_arg):
+    take_new_person(str_arg)
+    print('great_life')
+
+def take_new_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_life('look_same_woman')
