d1a8bf67186185a65ed1dc8ebd58253438fd1b3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:26:15 -0500

    "mix: give super/life_and_point/leave_young_case_by_great_person/same_week/work_eye_into_great_number/time.py, super/problem/bad_way/important_life/leave_important_person_by_fact.py or super/week_or_public_woman.py in public life"

---
+++ super/life_and_point/leave_young_case_by_great_person/same_week/work_eye_into_great_number/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_way(str_arg):
+    last_point(str_arg)
+    print('important_time_or_life')
+
+def last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_way('number')
---
+++ super/week_or_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_eye(str_arg):
+    world(str_arg)
+    print('able_problem')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_eye('public_group')
