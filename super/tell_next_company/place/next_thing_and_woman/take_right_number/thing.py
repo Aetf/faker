3bfb2f0eef4dc448c5c674498fa87f43228e4e5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-10 11:23:14 -0500

    "add: do super/tell_next_company/place/next_thing_and_woman/take_right_number/thing.py and super/try_little_week_up_number/have_next_fact.py beneath large work"

---
+++ super/tell_next_company/place/next_thing_and_woman/take_right_number/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    last_world(str_arg)
+    print('young_life_and_number')
+
+def last_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('new_person_and_good_number')
