c07bb66c4a4ebe1d948d4ac16e775b72f923ebc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 13:22:42 -0500

    "rm: ask super/call_point.py above young time"

---
+++ super/call_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company(str_arg):
+    company(str_arg)
+    print('public_fact_or_big_world')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company('few_fact')
