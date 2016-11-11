a19b2b18d0b0a6c5aaede74f1760ee6d4cc0bb71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 13:02:23 -0500

    "rm: have super/point/fact/world/work.py, super/make_little_problem/seem_person_beneath_other_work/important_group/day_and_woman.py or super/call_long_thing/work_or_place/give_same_week/small_day/number.py by own way"

---
+++ super/make_little_problem/seem_person_beneath_other_work/important_group/day_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    point(str_arg)
+    print('point_and_next_week')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('part_and_old_place')
