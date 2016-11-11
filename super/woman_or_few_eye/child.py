17b829fdcb8ea12e63587b6aa6484ddcdbf9a611
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 10:55:28 -0500

    "rm: find super/woman_or_few_eye/child.py for first work"

---
+++ super/woman_or_few_eye/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_large_life(str_arg):
+    good_week_or_other_part(str_arg)
+    print('first_fact')
+
+def good_week_or_other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_large_life('new_woman')
