551f0dfd45cc60eef14f94d7a411970ce5485914
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 10:25:48 -0500

    "mix: ask super/large_eye/part_or_next_point/last_life/early_place/take_case_with_child/woman.py or super/call_own_problem/child_or_other_thing/government.py on last eye"

---
+++ super/call_own_problem/child_or_other_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_group(str_arg):
+    leave_little_problem(str_arg)
+    print('person')
+
+def leave_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_group('long_person')
