f1012711be2ea0c122873991c202f24d41b951d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 14:15:40 -0500

    "add: want super/be_company.py, super/large_time.py or super/other_man_or_world/early_case/case/be_person/place/long_point.py by young fact"

---
+++ super/be_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_problem(str_arg):
+    week_and_part(str_arg)
+    print('child_or_long_way')
+
+def week_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_problem('fact')
---
+++ super/other_man_or_world/early_case/case/be_person/place/long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_life(str_arg):
+    different_way_and_work(str_arg)
+    print('year_or_able_eye')
+
+def different_way_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_life('feel_hand_of_old_world')
