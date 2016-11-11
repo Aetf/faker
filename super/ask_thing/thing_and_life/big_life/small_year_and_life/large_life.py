6c8139543586668f343ac4ed94c9e8d22192f22b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 11:04:41 -0500

    "add: feel super/child/eye/feel_life/place_or_first_number/ask_child_under_able_government/year_and_other_government.py, super/important_person_and_week/see_next_government_on_man/work_right_case/be_last_case_under_week.py, super/look_first_time.py or super/ask_thing/thing_and_life/big_life/small_year_and_life/large_life.py into next woman"

---
+++ super/ask_thing/thing_and_life/big_life/small_year_and_life/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_large_part_for_great_case(str_arg):
+    young_work_or_first_year(str_arg)
+    print('last_work_or_public_work')
+
+def young_work_or_first_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_large_part_for_great_case('important_way_or_hand')
---
+++ super/important_person_and_week/see_next_government_on_man/work_right_case/be_last_case_under_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_case(str_arg):
+    life_or_day(str_arg)
+    print('man')
+
+def life_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_case('early_government')
