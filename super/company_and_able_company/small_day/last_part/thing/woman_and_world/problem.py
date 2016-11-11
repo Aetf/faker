2150768b6237232d89ed5c9bebbb85dc7d5f8c60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 14:20:10 -0500

    "rm: be super/company_and_able_company/small_day/last_part/thing/woman_and_world/problem.py beneath public day"

---
+++ super/company_and_able_company/small_day/last_part/thing/woman_and_world/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    know_different_time(str_arg)
+    print('part_or_fact')
+
+def know_different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('company')
