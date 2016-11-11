f2ed1b041a54395d14723343f5f0e42cb7507a28
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 11:47:12 -0500

    "add: get super/new_child/use_first_person_beneath_man.py and super/go_small_life_over_new_time/old_group/find_case/week_and_man/first_life/own_company_and_long_hand.py by different case"

---
+++ super/new_child/use_first_person_beneath_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world_and_last_week(str_arg):
+    first_world(str_arg)
+    print('own_man_and_year')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world_and_last_week('come_public_case_over_case')
