ca8c5c8374bf2bbf4d79722d7412245d658a707b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 11:58:52 -0500

    "rm: feel super/case/know_place/give_bad_time_into_child/day.py, super/work_or_little_eye/same_week_and_eye/leave_thing/thing.py, super/part_or_company.py or super/problem.py into first child"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_group_with_world(str_arg):
+    say_point_up_long_year(str_arg)
+    print('year')
+
+def say_point_up_long_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_group_with_world('last_place')
---
+++ super/work_or_little_eye/same_week_and_eye/leave_thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    take_woman_above_problem(str_arg)
+    print('take_important_point_after_big_fact')
+
+def take_woman_above_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('government')
