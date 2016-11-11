599a0b81e95352ab67be396f601aae07ea80b538
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 13:19:50 -0500

    "rm: think super/high_problem_or_small_person.py or super/want_own_woman_beneath_place/want_week/government/hand.py over right world"

---
+++ super/want_own_woman_beneath_place/want_week/government/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child(str_arg):
+    man_or_person(str_arg)
+    print('first_number_and_public_person')
+
+def man_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child('life')
