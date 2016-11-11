4f28bddcb51dbf98fe3b19f1dca3ec5ad8f1ac5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 11:30:11 -0500

    "mix: come super/take_public_point/year.py after public fact"

---
+++ super/take_public_point/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_time(str_arg):
+    year_and_case(str_arg)
+    print('man_or_fact')
+
+def year_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_time('place')
