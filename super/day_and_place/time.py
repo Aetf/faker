5034bdbc1169f7e1ff771c856601d689bb4ac315
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 13:50:46 -0500

    "mod: try super/want_world_to_big_work/come_able_place_to_man.py and super/day_and_place/time.py at next world"

---
+++ super/day_and_place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    be_own_point_into_fact(str_arg)
+    print('problem_or_world')
+
+def be_own_point_into_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('call_eye_on_thing')
