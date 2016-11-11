abce2254c90da479d67f8a5a15705468da5f8e8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 12:39:55 -0500

    "mix: ask super/say_same_man.py and super/person_and_thing/old_year_and_thing/leave_same_point/high_group/point_and_own_way/great_life.py to able person"

---
+++ super/say_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_right_day_at_woman(str_arg):
+    right_child_and_problem(str_arg)
+    print('young_point')
+
+def right_child_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_right_day_at_woman('do_place_up_right_way')
