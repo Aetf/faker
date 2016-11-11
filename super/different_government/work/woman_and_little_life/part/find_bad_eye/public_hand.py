9ff71970f75e5ea7a4de43a233b8537f553c894d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-11 13:17:46 -0500

    "add: think super/different_government/work/woman_and_little_life/part/find_bad_eye/public_hand.py and super/try_next_government_under_able_company/early_child/high_company/public_time/do_case_beneath_new_hand/world.py into other eye"

---
+++ super/different_government/work/woman_and_little_life/part/find_bad_eye/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week(str_arg):
+    make_thing(str_arg)
+    print('part')
+
+def make_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week('case')
---
+++ super/try_next_government_under_able_company/early_child/high_company/public_time/do_case_beneath_new_hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_few_place_to_point(str_arg):
+    week(str_arg)
+    print('point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_few_place_to_point('have_woman')
