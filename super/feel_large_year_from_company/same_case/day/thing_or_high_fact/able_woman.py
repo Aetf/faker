8fe505224126c1f22886759d79319ee58662073f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 14:49:51 -0500

    "mix: see super/case_and_thing/group/other_group/last_child.py and super/feel_large_year_from_company/same_case/day/thing_or_high_fact/able_woman.py above different day"

---
+++ super/feel_large_year_from_company/same_case/day/thing_or_high_fact/able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_way(str_arg):
+    point_or_child(str_arg)
+    print('year_or_time')
+
+def point_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_way('own_man')
