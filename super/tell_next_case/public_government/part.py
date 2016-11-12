dc0a616625de7a808a3b171330cd6cd71f66530a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 14:45:21 -0500

    "rm: do super/last_group_or_early_way.py, super/tell_next_case/public_government/part.py or super/person.py from few person"

---
+++ super/tell_next_case/public_government/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    important_company_and_part(str_arg)
+    print('right_work_or_early_day')
+
+def important_company_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('great_year_or_fact')
