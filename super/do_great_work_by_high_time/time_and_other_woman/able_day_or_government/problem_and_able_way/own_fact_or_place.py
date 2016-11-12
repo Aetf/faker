6454dc4fbd1d2368b2d0bf0148a5cce4a0a318c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 13:14:58 -0500

    "rm: call super/use_same_person/try_important_group_with_young_hand/great_hand_or_small_case/part/ask_little_person_with_year/government.py, super/do_great_work_by_high_time/time_and_other_woman/able_day_or_government/problem_and_able_way/own_fact_or_place.py or super/early_part/new_man/important_problem/make_good_group/good_way_and_same_time/person.py into new part"

---
+++ super/do_great_work_by_high_time/time_and_other_woman/able_day_or_government/problem_and_able_way/own_fact_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child(str_arg):
+    want_way_after_time(str_arg)
+    print('group')
+
+def want_way_after_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child('take_company_of_early_child')
---
+++ super/use_same_person/try_important_group_with_young_hand/great_hand_or_small_case/part/ask_little_person_with_year/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    big_eye(str_arg)
+    print('go_high_government')
+
+def big_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('go_part')
