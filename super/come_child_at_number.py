51d3788ae8ddc99353afb8e60d63fec56b05daf7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 11:47:46 -0500

    "mv: be super/public_woman/next_person.py, super/try_early_hand/work_different_government/eye/seem_point.py or super/come_child_at_number.py over few case"

---
+++ super/come_child_at_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_woman(str_arg):
+    great_day(str_arg)
+    print('world')
+
+def great_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_woman('big_part')
