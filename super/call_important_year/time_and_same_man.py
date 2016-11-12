351c2a0083158a2cfcc45a1d65274b8f027a446b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 11:46:00 -0500

    "mod: look super/call_important_year/time_and_same_man.py at first place"

---
+++ super/call_important_year/time_and_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_eye(str_arg):
+    right_life_and_part(str_arg)
+    print('few_fact')
+
+def right_life_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_eye('person')
