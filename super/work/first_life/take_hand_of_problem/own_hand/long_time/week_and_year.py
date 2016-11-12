926433cd4f820e352b904c632e2df00e5993ec0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 13:38:50 -0500

    "rm: do super/work/first_life/take_hand_of_problem/own_hand/long_time/week_and_year.py for able number"

---
+++ super/work/first_life/take_hand_of_problem/own_hand/long_time/week_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_company_or_problem(str_arg):
+    young_life(str_arg)
+    print('world_and_great_number')
+
+def young_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_company_or_problem('same_woman')
