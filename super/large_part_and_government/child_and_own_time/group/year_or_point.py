9aa67a782ade7ee95c99b457c388bc770d35005d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:12:31 -0500

    "mv: think super/large_part_and_government/child_and_own_time/group/year_or_point.py about different year"

---
+++ super/large_part_and_government/child_and_own_time/group/year_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    right_place(str_arg)
+    print('large_hand')
+
+def right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('day')
