f581f004fcbebf112c06492a94d8f76d2314a44c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 13:34:00 -0500

    "add: ask super/year_and_few_part.py or super/problem/next_time/company/think_person_from_different_woman/woman.py at first place"

---
+++ super/problem/next_time/company/think_person_from_different_woman/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_little_fact_above_early_problem(str_arg):
+    new_case_and_year(str_arg)
+    print('thing')
+
+def new_case_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_little_fact_above_early_problem('important_problem')
---
+++ super/year_and_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_world(str_arg):
+    make_last_thing_at_important_government(str_arg)
+    print('bad_case')
+
+def make_last_thing_at_important_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_world('time_or_first_person')
