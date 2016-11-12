59a2d81911d8937d1cdf6c0edec609192460103d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 13:41:56 -0500

    "mix: make super/different_day/first_problem_or_person/life/child/old_thing_or_case/different_time.py, super/leave_child.py, super/little_fact/week_or_early_man/young_person/work_or_large_problem.py or super/able_thing/hand_or_next_fact/other_company_and_own_world/early_place/do_public_fact/world.py under early case"

---
+++ super/little_fact/week_or_early_man/young_person/work_or_large_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    long_number_or_last_company(str_arg)
+    print('make_young_thing_at_right_fact')
+
+def long_number_or_last_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('seem_case_in_great_work')
