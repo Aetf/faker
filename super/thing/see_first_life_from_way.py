f3437769a3d656695f8a01eb85223839c03d132d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 14:59:25 -0500

    "mod: come super/thing/see_first_life_from_way.py and super/seem_eye/call_first_way_in_early_thing.py from big time"

---
+++ super/thing/see_first_life_from_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    first_company(str_arg)
+    print('tell_child_under_fact')
+
+def first_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('part')
