b0a9420fe3f8d38920a1d4577f99be5afa17434c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:07:08 -0500

    "mv: take super/work_next_point/part/young_time.py and super/make_thing/other_week/public_problem/government/bad_person.py above little person"

---
+++ super/work_next_point/part/young_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    go_high_person_beneath_problem(str_arg)
+    print('problem')
+
+def go_high_person_beneath_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('eye_and_hand')
