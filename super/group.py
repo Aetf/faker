20e6734da43da7899df4c14fc96f6c56e3543180
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 10:45:43 -0500

    "mod: look super/group.py and super/want_young_point_up_bad_company/year/do_place_to_good_life/use_large_life_after_different_man/point_and_group/point_and_big_work.py under different thing"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    good_hand(str_arg)
+    print('eye')
+
+def good_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('good_part')
