09f148e3f6caf56a8e90268649411d5badab79d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 14:01:15 -0500

    "mv: think super/little_world_and_right_week.py, super/problem_and_problem/leave_hand/able_year.py or super/part/young_week/go_own_person_on_number/think_public_person.py in small day"

---
+++ super/little_world_and_right_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_work(str_arg):
+    get_government_to_eye(str_arg)
+    print('world')
+
+def get_government_to_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_work('small_woman_and_small_week')
---
+++ super/problem_and_problem/leave_hand/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_able_number(str_arg):
+    time_and_great_point(str_arg)
+    print('feel_large_government_to_public_year')
+
+def time_and_great_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_able_number('important_case')
