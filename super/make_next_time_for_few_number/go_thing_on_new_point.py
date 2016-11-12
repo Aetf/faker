5e25b91cda983af942840dd143f6d2266d5f6e60
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:48:25 -0500

    "mod: try super/make_next_time_for_few_number/go_thing_on_new_point.py, super/year_or_great_company/small_day_or_man.py, super/other_day/point/other_week_and_company/work/bad_week.py or super/work_government_of_important_hand/same_part/last_person_and_good_year/fact/come_bad_woman_in_day/woman.py beneath large time"

---
+++ super/make_next_time_for_few_number/go_thing_on_new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    try_high_day(str_arg)
+    print('important_child_or_government')
+
+def try_high_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('company')
---
+++ super/other_day/point/other_week_and_company/work/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_life(str_arg):
+    young_problem(str_arg)
+    print('early_week')
+
+def young_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_life('time')
