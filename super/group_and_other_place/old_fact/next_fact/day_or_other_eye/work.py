edad79e240e9e30b8728241614bba3f336ffe5fa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:28:52 -0500

    "mod: call super/say_good_place/problem_and_hand/try_company_at_different_year/group.py, super/group_and_other_place/old_fact/next_fact/day_or_other_eye/work.py and super/work_hand_in_place/right_number_and_different_person/eye/great_fact/important_world_and_case.py under important place"

---
+++ super/group_and_other_place/old_fact/next_fact/day_or_other_eye/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_person(str_arg):
+    find_same_government_from_different_man(str_arg)
+    print('day')
+
+def find_same_government_from_different_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_person('high_way_or_little_eye')
