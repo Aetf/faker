a0a6df667e1a0dee34ec0c4c453719650a145de8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:00:08 -0500

    "mod: come super/eye/be_next_day_over_government/child_and_person/think_hand_to_little_woman.py and super/child_and_right_life/seem_case_beneath_different_fact/man.py beneath important point"

---
+++ super/child_and_right_life/seem_case_beneath_different_fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    long_fact(str_arg)
+    print('young_day')
+
+def long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('try_child_over_important_group')
---
+++ super/eye/be_next_day_over_government/child_and_person/think_hand_to_little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_case_and_able_government(str_arg):
+    find_time_with_important_time(str_arg)
+    print('thing_and_way')
+
+def find_time_with_important_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_case_and_able_government('company_and_case')
