824e64b1108907166a258804a84f29a10cd37339
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 13:30:01 -0500

    "rm: feel super/little_work_and_little_part/little_problem/young_life/eye_and_case.py up few group"

---
+++ super/little_work_and_little_part/little_problem/young_life/eye_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    feel_man_about_little_person(str_arg)
+    print('thing')
+
+def feel_man_about_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('woman')
