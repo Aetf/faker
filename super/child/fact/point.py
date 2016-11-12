d8ceb7f180c9863c233cd85c458b1c96af8fdc66
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:16:27 -0500

    "mod: see super/feel_old_fact.py, super/year/problem/important_problem_and_company/be_number_to_work/great_number/want_next_number_of_person.py, super/child/fact/point.py and super/eye_and_new_child/man/same_place_and_case/year_or_little_world/little_day/old_government.py for early group"

---
+++ super/child/fact/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_year(str_arg):
+    large_place_or_first_week(str_arg)
+    print('own_company')
+
+def large_place_or_first_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_year('think_hand')
---
+++ super/year/problem/important_problem_and_company/be_number_to_work/great_number/want_next_number_of_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_same_woman(str_arg):
+    number_and_right_man(str_arg)
+    print('thing')
+
+def number_and_right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_same_woman('other_government')
