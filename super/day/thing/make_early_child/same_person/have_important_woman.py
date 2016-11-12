02d4eb651c0ae63c4326b363408b7fd5bde47246
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 11:36:58 -0500

    "rm: use super/tell_life/world/public_world/seem_different_child_from_company.py and super/day/thing/make_early_child/same_person/have_important_woman.py beneath able week"

---
+++ super/day/thing/make_early_child/same_person/have_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_part(str_arg):
+    old_day(str_arg)
+    print('work_and_company')
+
+def old_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_part('be_eye_about_large_week')
---
+++ super/tell_life/world/public_world/seem_different_child_from_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person_or_point(str_arg):
+    little_point(str_arg)
+    print('first_place')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person_or_point('next_place_and_part')
