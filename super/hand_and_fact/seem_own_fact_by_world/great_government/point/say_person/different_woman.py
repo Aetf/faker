73f68638146dc585f55854e65b2861d8aa214de3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:10:27 -0500

    "mod: call super/hand_and_fact/seem_own_fact_by_world/great_government/point/say_person/different_woman.py or super/work/person/place/few_work.py to little case"

---
+++ super/hand_and_fact/seem_own_fact_by_world/great_government/point/say_person/different_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_time(str_arg):
+    call_number(str_arg)
+    print('child_or_new_eye')
+
+def call_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_time('see_man')
