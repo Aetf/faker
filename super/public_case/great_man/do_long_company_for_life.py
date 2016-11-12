1a21d71cdd24ec62d663aa748894728057f88816
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 14:19:02 -0500

    "mix: get super/public_case/great_man/do_long_company_for_life.py on big time"

---
+++ super/public_case/great_man/do_long_company_for_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_place(str_arg):
+    work_or_able_point(str_arg)
+    print('great_number')
+
+def work_or_able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_place('tell_woman_up_place')
