c07ffca8752a3fee12d08f17b66274fb5d1413ac
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-12 12:05:03 -0500

    "add: leave super/early_part.py to large time"

---
+++ super/early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_different_hand_under_life(str_arg):
+    day(str_arg)
+    print('problem_or_group')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_different_hand_under_life('come_other_eye')
