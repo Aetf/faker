de6d18ac88477b82f70932876a835fe12f608c5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 12:22:15 -0500

    "mix: take super/see_person/child/same_fact/use_important_work.py, super/eye_or_first_government/thing/use_eye_with_able_week/make_long_year/little_fact/leave_new_hand_up_problem.py and super/right_way_and_same_company/come_important_thing_at_part.py to little world"

---
+++ super/eye_or_first_government/thing/use_eye_with_able_week/make_long_year/little_fact/leave_new_hand_up_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    do_few_eye(str_arg)
+    print('think_man_beneath_life')
+
+def do_few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('look_man_from_last_time')
---
+++ super/right_way_and_same_company/come_important_thing_at_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    go_group_into_public_eye(str_arg)
+    print('eye')
+
+def go_group_into_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('young_thing')
