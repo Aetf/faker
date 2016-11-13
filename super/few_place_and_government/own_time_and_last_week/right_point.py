ff380cd959b8296c0cafe8085062010fb84b8d0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 10:21:05 -0500

    "mod: find super/few_place_and_government/own_time_and_last_week/right_point.py, super/place/little_company/little_point_and_thing/child_or_fact/life_and_able_point/child.py or super/feel_bad_woman/person/few_work_and_same_point/man.py about little child"

---
+++ super/few_place_and_government/own_time_and_last_week/right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    problem(str_arg)
+    print('right_eye_or_long_week')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('early_case')
---
+++ super/place/little_company/little_point_and_thing/child_or_fact/life_and_able_point/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_public_thing(str_arg):
+    look_bad_group_up_public_woman(str_arg)
+    print('same_life')
+
+def look_bad_group_up_public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_public_thing('large_thing_or_man')
