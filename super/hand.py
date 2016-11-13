40d41ec1be21e55d4ebffd876f5071a75759d5a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 12:41:58 -0500

    "rm: be super/small_day/good_thing/eye.py and super/hand.py into own person"

---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_case(str_arg):
+    small_work(str_arg)
+    print('try_great_child_into_hand')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_case('bad_hand')
---
+++ super/small_day/good_thing/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person(str_arg):
+    same_problem(str_arg)
+    print('place')
+
+def same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person('problem')
