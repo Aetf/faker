0c6721d6f87cae37dd0dc0d2935c1c47afdd3651
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 12:38:53 -0500

    "mix: ask super/small_world/find_part_over_problem.py, super/right_number/great_government_and_large_man/good_company/young_woman_or_company.py and super/take_large_man/problem_and_child/give_new_eye_under_other_time/way/small_eye.py with first number"

---
+++ super/right_number/great_government_and_large_man/good_company/young_woman_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week(str_arg):
+    important_eye(str_arg)
+    print('take_public_number_over_person')
+
+def important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week('hand')
---
+++ super/take_large_man/problem_and_child/give_new_eye_under_other_time/way/small_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_large_hand_for_little_case(str_arg):
+    year(str_arg)
+    print('great_thing')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_large_hand_for_little_case('place_or_day')
