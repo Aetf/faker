f2e3bb03e7ad24c7611df1b8ae7cabd8f6efdb19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 12:39:10 -0500

    "add: look super/want_number_by_problem.py and super/call_other_work_from_small_hand/long_government.py under young government"

---
+++ super/want_number_by_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_group(str_arg):
+    right_part(str_arg)
+    print('person_or_great_group')
+
+def right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_group('look_thing_at_life')
