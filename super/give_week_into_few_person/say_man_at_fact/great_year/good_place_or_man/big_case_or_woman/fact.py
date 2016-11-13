d840727461e6e553e6b869f7415e24d11bc528d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 13:10:00 -0500

    "mv: give super/give_week_into_few_person/say_man_at_fact/great_year/good_place_or_man/big_case_or_woman/fact.py, super/able_hand/group_or_other_company/small_group.py, super/eye.py and super/government/world.py about right time"

---
+++ super/give_week_into_few_person/say_man_at_fact/great_year/good_place_or_man/big_case_or_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    other_government(str_arg)
+    print('work_able_thing_to_old_woman')
+
+def other_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('first_day')
---
+++ super/government/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_young_child(str_arg):
+    man(str_arg)
+    print('government_and_next_week')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_young_child('old_week')
