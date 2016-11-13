e5f73dc003e6942ba53c1493b71f964c5ad40c43
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 10:10:31 -0500

    "mix: have super/problem/own_fact_or_own_part/go_world_beneath_part.py, super/able_part_and_way/ask_work/do_few_place_on_eye/thing.py or super/make_world/leave_thing_about_number/make_same_life_above_great_eye/long_day/make_number/case_or_long_man.py beneath old point"

---
+++ super/problem/own_fact_or_own_part/go_world_beneath_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_new_part(str_arg):
+    way(str_arg)
+    print('bad_way_and_high_thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_new_part('woman')
