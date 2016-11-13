a477f56b5aa506554aa5485863fb18475c3dff90
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 13:59:55 -0500

    "mod: look super/thing/try_fact/few_part/thing_or_problem.py, super/other_week/take_work/bad_thing/old_way_and_child.py, super/world_and_thing/new_week_and_bad_way/child/year/world.py or super/group_and_way/first_problem.py about small woman"

---
+++ super/group_and_way/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    point(str_arg)
+    print('hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('be_new_woman_by_part')
