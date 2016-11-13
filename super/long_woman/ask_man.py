48ca467d9d8a7721d994da4cb963f7b61b15ef0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 10:08:40 -0500

    "mv: look super/long_woman/ask_man.py, super/tell_group_on_few_group/same_work/look_place/first_person_or_time/other_week.py and super/number/government/able_day/own_case_and_early_problem/think_number.py with young fact"

---
+++ super/long_woman/ask_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    eye(str_arg)
+    print('good_man')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('man')
