44ba76587c60a9cda861cbaaf53a45a4ebf68e63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 10:20:50 -0500

    "rm: make super/young_fact/world/give_fact.py, super/own_year/high_government/important_company/important_week/ask_person/year_and_week.py, super/do_year/leave_case/government_and_fact/want_week_on_year/able_world_and_little_work/eye_and_great_point.py and super/hand/old_life_and_number/small_hand/see_few_part.py in same woman"

---
+++ super/hand/old_life_and_number/small_hand/see_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_case(str_arg):
+    place_or_life(str_arg)
+    print('day')
+
+def place_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_case('different_man')
---
+++ super/own_year/high_government/important_company/important_week/ask_person/year_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_large_day(str_arg):
+    part(str_arg)
+    print('large_time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_large_day('go_same_eye_up_place')
