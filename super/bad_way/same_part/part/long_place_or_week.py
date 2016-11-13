747f75417748b4c3322cc465f6a91a96dc0eaf1a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 13:29:57 -0500

    "mod: seem super/bad_way/same_part/part/long_place_or_week.py on own year"

---
+++ super/bad_way/same_part/part/long_place_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_group_or_next_hand(str_arg):
+    right_world(str_arg)
+    print('do_woman_above_way')
+
+def right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_group_or_next_hand('early_work')
