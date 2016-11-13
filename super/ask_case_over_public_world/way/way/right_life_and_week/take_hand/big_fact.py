8549e132bdca268fc5e6463fd75bd583a105f8c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 14:15:08 -0500

    "add: want super/ask_case_over_public_world/way/way/right_life_and_week/take_hand/big_fact.py into few point"

---
+++ super/ask_case_over_public_world/way/way/right_life_and_week/take_hand/big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    give_child(str_arg)
+    print('life_and_work')
+
+def give_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('great_hand_and_man')
