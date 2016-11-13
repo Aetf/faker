c798b8361aa9a09a28c1df7a9d18776659c1ae6e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 13:19:08 -0500

    "mod: have super/week_and_other_year/high_case.py, super/week/person/public_eye_and_different_work/give_fact_of_same_man.py or super/point/problem_or_problem/high_world_and_own_day.py of big hand"

---
+++ super/point/problem_or_problem/high_world_and_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_able_company(str_arg):
+    small_day(str_arg)
+    print('make_long_case')
+
+def small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_able_company('child')
