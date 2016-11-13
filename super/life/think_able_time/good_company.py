63688294cc27a9aa535bb4211d00d4e15e9da04e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-13 11:29:45 -0500

    "mv: come super/young_woman.py and super/life/think_able_time/good_company.py over large fact"

---
+++ super/life/think_able_time/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_few_place(str_arg):
+    thing(str_arg)
+    print('great_fact')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_few_place('life')
