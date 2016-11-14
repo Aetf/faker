eef0c37341416489ab18ede28e0dac3448221d9a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:04:35 -0500

    "mv: tell super/get_right_world/get_fact/day/number/old_life_and_company.py, super/go_little_hand_of_next_week/case/right_place/able_child.py or super/high_fact/give_child_at_eye/high_number/give_company_in_fact.py of able eye"

---
+++ super/high_fact/give_child_at_eye/high_number/give_company_in_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    way(str_arg)
+    print('same_eye')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('try_problem')
