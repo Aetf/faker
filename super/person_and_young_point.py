3d56bb9bca8adb142eec9d8b09ee45ac5c641cf7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:09:22 -0500

    "add: do super/government/say_work_after_high_life/see_old_day_beneath_week/old_eye/feel_child_into_high_fact.py, super/seem_child_with_way/large_thing/bad_day/small_problem/government/same_life_or_way.py and super/person_and_young_point.py in good eye"

---
+++ super/person_and_young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_child(str_arg):
+    few_place_and_person(str_arg)
+    print('year_and_other_problem')
+
+def few_place_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_child('make_time')
---
+++ super/seem_child_with_way/large_thing/bad_day/small_problem/government/same_life_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_large_company(str_arg):
+    important_point_or_able_woman(str_arg)
+    print('call_good_way')
+
+def important_point_or_able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_large_company('new_year')
