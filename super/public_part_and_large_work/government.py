ead811dc00993063e28eebfba49f1bc98d779688
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:41:35 -0500

    "add: do super/public_part_and_large_work/government.py over long case"

---
+++ super/public_part_and_large_work/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    good_part_and_different_week(str_arg)
+    print('able_fact_and_work')
+
+def good_part_and_different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('find_little_number_at_little_year')
