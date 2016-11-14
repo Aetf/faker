fb9c629afa775460dd4b087647c20f78e78f7215
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 13:50:56 -0500

    "mix: think super/world.py of next thing"

---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    company(str_arg)
+    print('problem_or_case')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('other_world')
