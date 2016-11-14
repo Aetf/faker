3b1c9f86ef94fbeff35fceb9b3136762886e9af4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:11:51 -0500

    "mix: come super/time_and_world/want_company_into_last_group.py and super/young_day/large_time/able_child_and_year/long_woman_or_good_life/same_year/bad_fact.py beneath good thing"

---
+++ super/time_and_world/want_company_into_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_new_company_above_government(str_arg):
+    old_place(str_arg)
+    print('next_woman')
+
+def old_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_new_company_above_government('fact')
