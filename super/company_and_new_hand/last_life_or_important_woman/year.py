12e6adae45e30b7979bf2acd094d5a960d8d2e2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:40:29 -0500

    "mv: look super/company_and_new_hand/last_life_or_important_woman/year.py over right eye"

---
+++ super/company_and_new_hand/last_life_or_important_woman/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    different_case(str_arg)
+    print('long_woman_and_able_way')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('work_new_government')
