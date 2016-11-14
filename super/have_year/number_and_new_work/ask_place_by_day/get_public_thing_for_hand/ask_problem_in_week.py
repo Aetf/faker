39152da6bd446c0ef50a8a90dd3689fb5938979e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:06:03 -0500

    "rm: see super/year_and_problem/time.py, super/have_year/number_and_new_work/ask_place_by_day/get_public_thing_for_hand/ask_problem_in_week.py, super/point_and_work/same_number_and_other_problem/large_way_or_man.py or super/think_group/eye_or_long_fact.py into large child"

---
+++ super/have_year/number_and_new_work/ask_place_by_day/get_public_thing_for_hand/ask_problem_in_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_case(str_arg):
+    little_place(str_arg)
+    print('hand')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_case('fact')
---
+++ super/year_and_problem/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_world(str_arg):
+    child(str_arg)
+    print('able_group_and_public_person')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_world('think_bad_work')
