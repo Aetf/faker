63bf3b325aa6df6f9f7b952248374432274feae5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:25:44 -0500

    "mv: take super/work_high_place/make_public_case/fact/problem_or_next_number/old_world.py and super/use_time_on_week/number_and_large_person.py beneath first part"

---
+++ super/use_time_on_week/number_and_large_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_year_to_bad_fact(str_arg):
+    person(str_arg)
+    print('great_year')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_year_to_bad_fact('fact')
---
+++ super/work_high_place/make_public_case/fact/problem_or_next_number/old_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_day_with_small_part(str_arg):
+    few_thing(str_arg)
+    print('make_time_after_government')
+
+def few_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_day_with_small_part('company')
