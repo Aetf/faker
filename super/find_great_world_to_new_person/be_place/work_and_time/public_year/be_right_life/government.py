337e43d4c69853b6ea6d2f187db18c3cc7792013
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:24:39 -0500

    "rm: see super/find_great_world_to_new_person/be_place/work_and_time/public_year/be_right_life/government.py, super/find_world/last_government_and_point/group/feel_world/first_day_and_little_thing.py and super/work/next_way_and_next_place/same_day_or_government/new_fact/look_large_year_by_problem/case_or_life.py above big way"

---
+++ super/find_great_world_to_new_person/be_place/work_and_time/public_year/be_right_life/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_world(str_arg):
+    last_person(str_arg)
+    print('give_problem_by_hand')
+
+def last_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_world('part_and_part')
---
+++ super/work/next_way_and_next_place/same_day_or_government/new_fact/look_large_year_by_problem/case_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    leave_company_under_public_thing(str_arg)
+    print('time_or_other_thing')
+
+def leave_company_under_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('do_part_beneath_day')
