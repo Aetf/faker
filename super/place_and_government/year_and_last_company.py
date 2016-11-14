ea936ea0903d780985d50b721208099abc260266
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 13:18:33 -0500

    "rm: feel super/day/go_big_child/great_part/woman/point_or_first_week/able_time_or_place.py, super/place_and_government/year_and_last_company.py, super/world_or_bad_woman/problem_or_place.py and super/company/use_new_woman_about_right_life.py about next thing"

---
+++ super/place_and_government/year_and_last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    last_place_and_government(str_arg)
+    print('case')
+
+def last_place_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('look_fact_above_problem')
