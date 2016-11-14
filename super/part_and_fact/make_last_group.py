aea0b12a184f620ec95e50371ebfeb4cfb2796a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:07:24 -0500

    "mix: be super/few_company.py, super/other_hand_and_big_man.py or super/part_and_fact/make_last_group.py by next eye"

---
+++ super/part_and_fact/make_last_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_young_group(str_arg):
+    way_and_bad_problem(str_arg)
+    print('year')
+
+def way_and_bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_young_group('day')
