fce3073ddbf2c3921e530398c0780f2876d50a30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:49:28 -0500

    "rm: want super/company/day/year_or_next_year/first_life/group/make_big_number_about_new_time.py in last time"

---
+++ super/company/day/year_or_next_year/first_life/group/make_big_number_about_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    hand(str_arg)
+    print('important_point_or_government')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('same_child_or_problem')
