3f5cdcdde9d9044d82e27b8442f00f121d0c8b99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 11:21:04 -0500

    "mv: look super/point_and_other_company/right_time_or_life/right_man/seem_part_for_last_week/right_world/right_problem_and_next_company.py over old case"

---
+++ super/point_and_other_company/right_time_or_life/right_man/seem_part_for_last_week/right_world/right_problem_and_next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_good_company_on_fact(str_arg):
+    day(str_arg)
+    print('company_and_number')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_good_company_on_fact('see_man_beneath_next_man')
