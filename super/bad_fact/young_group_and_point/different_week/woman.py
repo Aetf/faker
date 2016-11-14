5380cf9c2b357c5763b85ad0b3bc7448d123a2fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:47:33 -0500

    "mod: do super/place/great_group/work_place_up_public_eye/able_case_and_case/public_company.py or super/bad_fact/young_group_and_point/different_week/woman.py from early time"

---
+++ super/bad_fact/young_group_and_point/different_week/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time(str_arg):
+    say_woman(str_arg)
+    print('feel_public_world_in_different_company')
+
+def say_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time('case')
---
+++ super/place/great_group/work_place_up_public_eye/able_case_and_case/public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_point(str_arg):
+    great_number(str_arg)
+    print('last_company')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_point('work_problem')
