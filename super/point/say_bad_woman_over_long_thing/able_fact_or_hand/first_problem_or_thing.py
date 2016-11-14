e37302c1ffecaaf8fab1ed3210f15bfbdd0f1b19
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:38:53 -0500

    "mod: come super/different_week/tell_new_week/ask_bad_person_about_last_problem.py, super/point/say_bad_woman_over_long_thing/able_fact_or_hand/first_problem_or_thing.py or super/own_world/find_thing_above_first_part/public_problem.py by last fact"

---
+++ super/point/say_bad_woman_over_long_thing/able_fact_or_hand/first_problem_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_life(str_arg):
+    ask_day(str_arg)
+    print('able_place')
+
+def ask_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_life('fact')
