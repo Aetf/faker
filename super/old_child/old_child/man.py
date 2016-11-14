ee2b6540e78dbf743ac5487ef990406bcfa13fee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:32:03 -0500

    "rm: get super/old_child/old_child/man.py on old part"

---
+++ super/old_child/old_child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_big_work(str_arg):
+    say_government_about_man(str_arg)
+    print('child')
+
+def say_government_about_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_big_work('few_year')
