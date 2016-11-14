655a4f0277ff2b61c28b193f3a13aacbe469ab1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:51:33 -0500

    "add: give super/other_place/large_work.py with high person"

---
+++ super/other_place/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_world(str_arg):
+    problem_or_child(str_arg)
+    print('look_last_government')
+
+def problem_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_world('leave_old_part')
