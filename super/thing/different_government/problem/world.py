c2c6c22f0b80b8243d05cc9d1195412784f1147c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:15:42 -0500

    "mod: make super/few_group_or_number/make_little_government_at_part.py or super/thing/different_government/problem/world.py in next world"

---
+++ super/thing/different_government/problem/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_group_and_problem(str_arg):
+    fact_and_eye(str_arg)
+    print('time')
+
+def fact_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_group_and_problem('important_thing')
