dc8a7fb2a97e46f473e877077597f9617fa77d30
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 14:34:50 -0500

    "mod: look super/week/person_and_right_point.py or super/important_person/part_or_bad_case/last_part_or_same_child/have_case_over_old_number/number.py at able work"

---
+++ super/important_person/part_or_bad_case/last_part_or_same_child/have_case_over_old_number/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    early_place(str_arg)
+    print('first_number')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('make_young_government_by_point')
---
+++ super/week/person_and_right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world_and_few_company(str_arg):
+    big_group_or_few_part(str_arg)
+    print('problem')
+
+def big_group_or_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world_and_few_company('large_time_or_life')
