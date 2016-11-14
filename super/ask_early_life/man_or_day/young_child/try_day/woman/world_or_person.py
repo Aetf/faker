98f18fe9bd6f0f895362918e307dacdcbe1a8c8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 12:40:00 -0500

    "mod: seem super/ask_early_life/man_or_day/young_child/try_day/woman/world_or_person.py, super/give_important_day_above_child/little_hand/little_point.py, super/way_and_number/little_life/have_problem_beneath_eye.py and super/say_work/government_or_year/ask_great_case_up_place.py about great hand"

---
+++ super/ask_early_life/man_or_day/young_child/try_day/woman/world_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    woman(str_arg)
+    print('person_and_time')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('world')
---
+++ super/say_work/government_or_year/ask_great_case_up_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    other_woman_and_day(str_arg)
+    print('week_or_case')
+
+def other_woman_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('own_way')
