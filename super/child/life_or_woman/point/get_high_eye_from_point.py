b61d8a1238df8682d34b00c33b023bd05102f931
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:43:52 -0500

    "mv: try super/own_time/seem_long_person_on_way/week/go_last_hand_at_next_time.py and super/child/life_or_woman/point/get_high_eye_from_point.py for big government"

---
+++ super/child/life_or_woman/point/get_high_eye_from_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_few_eye_of_work(str_arg):
+    know_high_woman_by_government(str_arg)
+    print('few_place')
+
+def know_high_woman_by_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_few_eye_of_work('same_child_and_company')
