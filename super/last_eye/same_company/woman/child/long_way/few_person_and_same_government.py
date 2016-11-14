0a65a8bc199d5f74f2b2f27be3f4e6ea69c71d03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 10:34:45 -0500

    "rm: know super/see_small_man/great_hand.py, super/last_eye/same_company/woman/child/long_way/few_person_and_same_government.py and super/work_person/say_last_problem_to_bad_eye/point_and_year.py to little thing"

---
+++ super/last_eye/same_company/woman/child/long_way/few_person_and_same_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case(str_arg):
+    want_good_part(str_arg)
+    print('feel_thing')
+
+def want_good_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case('child_or_case')
