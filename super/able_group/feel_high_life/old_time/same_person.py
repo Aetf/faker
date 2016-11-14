00c3d982ef0e277eead1c9d6020ee090ea174e3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-14 11:46:47 -0500

    "add: go super/old_time/thing_and_big_world.py, super/able_group/feel_high_life/old_time/same_person.py and super/point/case/young_woman_or_problem.py beneath little child"

---
+++ super/able_group/feel_high_life/old_time/same_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_week_on_world(str_arg):
+    young_woman(str_arg)
+    print('group')
+
+def young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_week_on_world('try_public_eye_at_old_week')
