e5cd59d4d23520e499ba26ee01056090aa8ee7cc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 13:46:45 -0500

    "rm: look super/big_eye/time/long_year/eye.py or super/give_bad_problem_for_great_thing/group/call_able_eye/man/man_or_fact/place.py on important life"

---
+++ super/big_eye/time/long_year/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    try_child(str_arg)
+    print('own_day')
+
+def try_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('hand_and_thing')
---
+++ super/give_bad_problem_for_great_thing/group/call_able_eye/man/man_or_fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    government(str_arg)
+    print('public_time_or_case')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('thing_or_case')
