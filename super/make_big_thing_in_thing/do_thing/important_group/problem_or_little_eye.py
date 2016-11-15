13ea4c10055b3693dc069391c5767ddb3459ddf3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 11:46:55 -0500

    "add: go super/make_big_thing_in_thing/do_thing/important_group/problem_or_little_eye.py and super/own_time/first_woman/see_hand/time_or_high_person/go_large_work.py into few life"

---
+++ super/make_big_thing_in_thing/do_thing/important_group/problem_or_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    next_company_or_early_fact(str_arg)
+    print('young_company')
+
+def next_company_or_early_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('great_year_or_young_hand')
