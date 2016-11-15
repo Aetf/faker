74979cda51f7635b31d816037eb9bc2441185d7f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 13:21:09 -0500

    "rm: seem super/same_part/place/early_year.py of old company"

---
+++ super/same_part/place/early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_or_new_child(str_arg):
+    person(str_arg)
+    print('man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_or_new_child('give_work_over_big_point')
