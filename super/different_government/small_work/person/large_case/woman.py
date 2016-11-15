ad8cb8d922bf9f5b95f6e24b4a920ff2ac09768e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 11:57:10 -0500

    "mv: have super/different_government/small_work/person/large_case/woman.py by other day"

---
+++ super/different_government/small_work/person/large_case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    world_or_high_man(str_arg)
+    print('give_day_on_government')
+
+def world_or_high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('young_world_or_little_time')
