dd13e3cd99db113db628ca01dd42992fb4529bc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 14:49:19 -0500

    "mix: look super/last_person/call_important_point/use_hand_after_government/point.py, super/way.py, super/ask_point/point/say_woman.py and super/find_group_under_high_number/different_woman/long_year/part.py about high woman"

---
+++ super/ask_point/point/say_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    be_week_over_young_world(str_arg)
+    print('work_and_large_time')
+
+def be_week_over_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('year')
---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_long_week(str_arg):
+    big_thing(str_arg)
+    print('be_company')
+
+def big_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_long_week('few_world_or_different_group')
