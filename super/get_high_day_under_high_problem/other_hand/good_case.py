3e27d51888280af07888ad802fad84ce2c0496be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 10:48:55 -0500

    "mv: give super/get_high_day_under_high_problem/other_hand/good_case.py of little time"

---
+++ super/get_high_day_under_high_problem/other_hand/good_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way_or_first_week(str_arg):
+    government_or_eye(str_arg)
+    print('able_problem')
+
+def government_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way_or_first_week('ask_child_about_good_number')
