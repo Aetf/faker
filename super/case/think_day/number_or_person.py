dd3004c34066c9c55cab5fe7c026ca7ea34ea264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 13:35:14 -0500

    "rm: leave super/person/life_or_fact/number/woman.py, super/case/think_day/number_or_person.py and super/thing/old_government.py at last fact"

---
+++ super/case/think_day/number_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_important_thing_into_child(str_arg):
+    get_woman(str_arg)
+    print('man')
+
+def get_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_important_thing_into_child('little_group')
