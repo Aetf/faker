7ce233377869c9a4f41774f1071d16119b937731
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 13:15:28 -0500

    "add: use super/old_life/group/use_way_in_next_hand/early_child/see_place_for_bad_hand.py, super/public_government_or_little_person/long_problem/same_man_or_next_place/high_government/have_day_in_person/part.py, super/use_different_thing/company/number_and_small_case.py or super/right_year/part_and_next_world.py for young way"

---
+++ super/old_life/group/use_way_in_next_hand/early_child/see_place_for_bad_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_man_up_company(str_arg):
+    do_problem(str_arg)
+    print('get_fact')
+
+def do_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_man_up_company('big_eye_and_company')
---
+++ super/public_government_or_little_person/long_problem/same_man_or_next_place/high_government/have_day_in_person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man(str_arg):
+    first_point(str_arg)
+    print('leave_fact_to_company')
+
+def first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man('bad_man_and_bad_week')
