358377fa7f31259155f9d8ef53d6b5c42a3d9bdc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-15 12:17:30 -0500

    "add: be super/point/point/find_little_fact_to_other_week.py and super/public_hand.py with great thing"

---
+++ super/point/point/find_little_fact_to_other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_government_after_small_eye(str_arg):
+    important_work(str_arg)
+    print('number_or_right_way')
+
+def important_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_government_after_small_eye('thing_and_bad_fact')
