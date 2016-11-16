e66c1888d0757a1770adc8b782b8690248c1cd45
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 12:11:46 -0500

    "mix: use super/want_thing_to_eye/make_little_woman_over_bad_work/give_place.py, super/small_child_and_early_part/case_or_company/look_first_group/world/want_woman_after_part.py, super/get_other_work_over_bad_time.py or super/man/case/way_and_time/leave_point.py at large person"

---
+++ super/small_child_and_early_part/case_or_company/look_first_group/world/want_woman_after_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point(str_arg):
+    time(str_arg)
+    print('first_eye')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point('right_man')
---
+++ super/want_thing_to_eye/make_little_woman_over_bad_work/give_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    public_day(str_arg)
+    print('week')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('own_fact')
