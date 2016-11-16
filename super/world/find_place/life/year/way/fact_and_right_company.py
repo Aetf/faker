2e78ab66e1095b19ff2ed18663ae6fbae2063c0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:25:30 -0500

    "mv: be super/world/find_place/life/year/way/fact_and_right_company.py or super/number/able_hand.py after good day"

---
+++ super/world/find_place/life/year/way/fact_and_right_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point(str_arg):
+    look_time(str_arg)
+    print('high_man')
+
+def look_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point('be_world_above_old_day')
