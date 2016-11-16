564b78db2428a656a14486189a4dc8dd30bce630
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 13:36:48 -0500

    "rm: go super/thing/young_company_and_other_time/same_number/public_year/old_man/ask_part.py, super/go_woman/world.py or super/say_man_from_bad_hand.py after public week"

---
+++ super/thing/young_company_and_other_time/same_number/public_year/old_man/ask_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_week(str_arg):
+    different_thing(str_arg)
+    print('few_day')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_week('way')
