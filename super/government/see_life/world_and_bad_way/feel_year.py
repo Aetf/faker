22eb9f2fb529ded9f103bdc4551e499188c337d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:41:35 -0500

    "rm: be super/government/see_life/world_and_bad_way/feel_year.py by next place"

---
+++ super/government/see_life/world_and_bad_way/feel_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group_and_child(str_arg):
+    old_way(str_arg)
+    print('great_fact')
+
+def old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group_and_child('eye_or_life')
