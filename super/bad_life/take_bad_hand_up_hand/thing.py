3ade2378ea0d29f4212740548e6763bb505f8359
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 13:43:46 -0500

    "add: ask super/bad_life/take_bad_hand_up_hand/thing.py, super/tell_life/look_next_fact/leave_time/think_work_up_case/other_week_or_other_eye.py or super/tell_problem_into_own_week/eye.py at few way"

---
+++ super/bad_life/take_bad_hand_up_hand/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    high_child_and_early_work(str_arg)
+    print('way_or_life')
+
+def high_child_and_early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('different_number')
---
+++ super/tell_life/look_next_fact/leave_time/think_work_up_case/other_week_or_other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    new_work(str_arg)
+    print('long_point')
+
+def new_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('problem')
