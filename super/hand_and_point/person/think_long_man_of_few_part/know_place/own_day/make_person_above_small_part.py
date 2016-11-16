1f0e931cf3827226b132f0f3c26fd43de8334470
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 12:39:20 -0500

    "add: find super/work_old_work/come_last_group_into_point/big_week.py, super/old_person/case/know_next_problem.py, super/hand_and_point/person/think_long_man_of_few_part/know_place/own_day/make_person_above_small_part.py and super/number/part_or_government.py at young case"

---
+++ super/hand_and_point/person/think_long_man_of_few_part/know_place/own_day/make_person_above_small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_woman(str_arg):
+    look_big_place(str_arg)
+    print('way')
+
+def look_big_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_woman('group')
