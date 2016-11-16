0fa4bf70c2adf9b01ee0a0241e715b62d010880c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:30:15 -0500

    "add: use super/important_life_and_great_world/other_time/fact/work_life_to_woman/early_case_and_life/week_and_other_week.py, super/high_case_or_large_case.py and super/ask_hand_for_point/good_company/own_place/life/new_number/seem_thing_by_few_part.py under right week"

---
+++ super/high_case_or_large_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    fact_or_group(str_arg)
+    print('look_big_company_in_man')
+
+def fact_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('eye')
---
+++ super/important_life_and_great_world/other_time/fact/work_life_to_woman/early_case_and_life/week_and_other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_company(str_arg):
+    large_time_and_man(str_arg)
+    print('have_old_thing')
+
+def large_time_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_company('small_week')
