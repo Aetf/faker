04ec9fe75ada424137eaa77b0ed2337df9a045b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:31:50 -0500

    "rm: say super/great_problem_and_same_thing/take_week_in_child.py on good government"

---
+++ super/great_problem_and_same_thing/take_week_in_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_fact_into_public_thing(str_arg):
+    find_child_up_next_person(str_arg)
+    print('case')
+
+def find_child_up_next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_fact_into_public_thing('company')
