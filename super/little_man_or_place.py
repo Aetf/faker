a14e27eb5a53741851ae50d77ea0c976d4757caf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 13:41:27 -0500

    "mv: want super/little_man_or_place.py under high way"

---
+++ super/little_man_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day_and_group(str_arg):
+    do_company_on_next_place(str_arg)
+    print('next_eye')
+
+def do_company_on_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day_and_group('large_number_or_case')
