81459cd018e92168a03341ac85b93915eda77e8a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 12:17:09 -0500

    "rm: seem super/young_government.py and super/man_or_little_work/old_child/know_right_way.py for new part"

---
+++ super/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    week(str_arg)
+    print('good_point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('little_point_and_few_number')
