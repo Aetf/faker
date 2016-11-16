beecd6623ce2b57f1b51cce4af848b5c776510a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:56:50 -0500

    "mv: ask super/case/feel_large_way_after_eye/part_or_early_day/find_world/young_company_or_early_woman/old_work_and_own_week.py, super/fact/week.py or super/time.py beneath young child"

---
+++ super/case/feel_large_way_after_eye/part_or_early_day/find_world/young_company_or_early_woman/old_work_and_own_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_company(str_arg):
+    able_life_or_work(str_arg)
+    print('important_case')
+
+def able_life_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_company('fact')
---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_next_eye_above_old_way(str_arg):
+    way_and_case(str_arg)
+    print('other_week')
+
+def way_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_next_eye_above_old_way('other_case_and_case')
