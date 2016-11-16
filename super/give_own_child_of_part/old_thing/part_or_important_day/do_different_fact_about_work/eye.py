f5739e68444b2eecc4d541ca889c2ad7378f30ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 12:10:34 -0500

    "mv: take super/government.py, super/know_time/know_eye/right_fact/number/able_eye.py, super/different_group_or_world/be_man/thing_or_life/take_great_work/fact_and_other_eye/call_large_part.py or super/give_own_child_of_part/old_thing/part_or_important_day/do_different_fact_about_work/eye.py into little year"

---
+++ super/give_own_child_of_part/old_thing/part_or_important_day/do_different_fact_about_work/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_company(str_arg):
+    company(str_arg)
+    print('hand')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_company('able_person_and_first_year')
---
+++ super/know_time/know_eye/right_fact/number/able_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group(str_arg):
+    come_little_case(str_arg)
+    print('early_year')
+
+def come_little_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group('problem')
