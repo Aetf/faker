26e5311b05fee227c71e45302a630629c5bce602
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 11:24:06 -0500

    "mv: go super/fact/own_government/know_hand.py, super/get_first_point_at_woman/long_life/feel_number_to_government/do_point/day.py or super/high_thing/see_eye.py under different year"

---
+++ super/high_thing/see_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    long_man(str_arg)
+    print('place')
+
+def long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('hand')
