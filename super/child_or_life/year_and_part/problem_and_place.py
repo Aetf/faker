421462a69ae32b3b0417f93105f901778ecec364
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:11:34 -0500

    "mv: feel super/child_or_life/year_and_part/problem_and_place.py, super/high_world/feel_public_day_from_fact/life_and_next_person/early_person.py or super/tell_government/next_child.py for new work"

---
+++ super/child_or_life/year_and_part/problem_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_number(str_arg):
+    company(str_arg)
+    print('own_number')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_number('work')
---
+++ super/tell_government/next_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    good_government_or_bad_child(str_arg)
+    print('feel_year')
+
+def good_government_or_bad_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('next_day')
