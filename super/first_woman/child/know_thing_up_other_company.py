bd57edbfbf206d41a583d684db620dbd0908f47b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:18:42 -0500

    "rm: leave super/next_part_and_person.py and super/first_woman/child/know_thing_up_other_company.py by young group"

---
+++ super/first_woman/child/know_thing_up_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_work(str_arg):
+    other_man_and_good_thing(str_arg)
+    print('world')
+
+def other_man_and_good_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_work('different_case_and_small_work')
---
+++ super/next_part_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_fact(str_arg):
+    woman(str_arg)
+    print('year_or_early_woman')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_fact('little_woman')
