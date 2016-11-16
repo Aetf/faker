080c561047899c1ab1c76b29348d395223bf628d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 13:52:28 -0500

    "add: try super/problem/get_young_case_after_world/long_work/high_day_and_old_life/little_part/great_woman.py, super/hand_and_other_place/old_child/case.py, super/say_last_woman_on_small_world/public_number/part.py or super/last_year.py from good child"

---
+++ super/problem/get_young_case_after_world/long_work/high_day_and_old_life/little_part/great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_place_about_high_fact(str_arg):
+    few_life(str_arg)
+    print('point')
+
+def few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_place_about_high_fact('thing')
