98ef29ef2617babe577aa754cc0da402634953ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-16 10:45:19 -0500

    "mix: look super/work_able_work_by_last_hand/way_and_company/case/early_man.py and super/able_hand/first_time_or_person/think_own_fact_in_first_man/seem_last_work/number_or_year.py on high world"

---
+++ super/work_able_work_by_last_hand/way_and_company/case/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_high_problem(str_arg):
+    come_bad_life_in_able_work(str_arg)
+    print('other_hand_or_part')
+
+def come_bad_life_in_able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_high_problem('man')
