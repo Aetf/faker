063a1149f0840cc6dad03d14085313dcc53d4a3c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:32:40 -0500

    "mix: seem super/problem/person_or_time/time_and_able_life.py about large life"

---
+++ super/problem/person_or_time/time_and_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_thing_or_day(str_arg):
+    good_case(str_arg)
+    print('young_day')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_thing_or_day('look_time_up_early_problem')
