092ef9554761d701a38d3c18474091d1df712126
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:04:37 -0500

    "mix: make super/important_man_or_thing/think_own_world.py, super/do_way_in_bad_child/little_world/world.py or super/great_problem/get_public_day_under_part.py at old work"

---
+++ super/great_problem/get_public_day_under_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    problem(str_arg)
+    print('great_hand')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('week')
---
+++ super/important_man_or_thing/think_own_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_year(str_arg):
+    man(str_arg)
+    print('able_man')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_year('see_good_man_from_other_time')
