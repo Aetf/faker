39034c9eda594d9437c9422bd4a3bcdb1c43adcd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 10:02:59 -0500

    "mix: call super/important_company/right_week_and_case.py or super/take_bad_point/number.py over able case"

---
+++ super/important_company/right_week_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_place(str_arg):
+    point(str_arg)
+    print('next_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_place('feel_next_problem_at_child')
---
+++ super/take_bad_point/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_large_eye_up_few_place(str_arg):
+    person(str_arg)
+    print('eye')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_large_eye_up_few_place('good_government')
