d9e8790e6f406ae2d1acc8133b13282b44518684
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:52:59 -0500

    "mv: know super/seem_fact.py, super/use_day/small_case_or_week/case/ask_week_about_bad_man/person.py and super/world_or_number/woman_or_case.py over good case"

---
+++ super/use_day/small_case_or_week/case/ask_week_about_bad_man/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    man(str_arg)
+    print('way')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('point')
---
+++ super/world_or_number/woman_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_work(str_arg):
+    big_group(str_arg)
+    print('few_thing')
+
+def big_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_work('few_thing')
