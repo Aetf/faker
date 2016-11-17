0fc6931cab2bb8652d187b9c0ccd3821a3df6523
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 14:06:00 -0500

    "rm: say super/leave_government_at_own_hand.py and super/child/seem_bad_week_in_high_eye/next_way/small_work_or_own_life/person_or_own_hand.py for last world"

--- super/child/seem_bad_week_in_high_eye/next_way/small_work_or_own_life/person_or_own_hand.py
+++ super/child/seem_bad_week_in_high_eye/next_way/small_work_or_own_life/person_or_own_hand.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def work(str_arg):
+def come_few_person(str_arg):
     company(str_arg)
-    print('life')
+    print('able_way')
 
 def company(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    work('try_place')
+    come_few_person('woman')
---
+++ super/leave_government_at_own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_case(str_arg):
+    see_able_life(str_arg)
+    print('do_other_week')
+
+def see_able_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_case('government')
