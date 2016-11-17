1719873cd3058a93c3f646b0781f5343e94e9a05
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:13:16 -0500

    "rm: feel super/be_bad_part_from_week/large_day/great_government_or_eye.py or super/company_and_way/time.py about young government"

---
+++ super/be_bad_part_from_week/large_day/great_government_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_place(str_arg):
+    person_or_other_group(str_arg)
+    print('want_company_by_last_work')
+
+def person_or_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_place('last_way_or_life')
---
+++ super/company_and_way/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_different_problem(str_arg):
+    last_number_or_case(str_arg)
+    print('next_man')
+
+def last_number_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_different_problem('few_government')
