c48b218855050e07e2baa80300dc16606b1e2e1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:43:44 -0500

    "rm: know super/different_part/have_year_to_first_government/know_place/right_number/world.py over next life"

---
+++ super/different_part/have_year_to_first_government/know_place/right_number/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    world(str_arg)
+    print('take_work_to_fact')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('small_life_or_year')
