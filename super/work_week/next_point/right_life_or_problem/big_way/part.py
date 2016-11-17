56d63fd459eb87bbdc2ed6b2cf02218c7956c146
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 10:32:13 -0500

    "mod: use super/work_week/next_point/right_life_or_problem/big_way/part.py above public way"

---
+++ super/work_week/next_point/right_life_or_problem/big_way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    point(str_arg)
+    print('thing')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('time_or_place')
