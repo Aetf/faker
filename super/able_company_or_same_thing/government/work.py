daaa22d37d76107dd681d1d2543b525258914667
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:31:26 -0500

    "mix: give super/government/first_part/part/small_company_or_world/hand_and_man/child.py, super/able_company_or_same_thing/government/work.py and super/eye/early_life_and_few_year/life/next_company_and_eye/point.py of different eye"

---
+++ super/able_company_or_same_thing/government/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    good_time(str_arg)
+    print('big_group_and_little_group')
+
+def good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('case')
