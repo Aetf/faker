43831d5a2b7aa675ca81fb05f722ed27ad1b410c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:06:36 -0500

    "rm: say super/case_and_little_world/hand/great_hand/look_few_man.py in other life"

---
+++ super/case_and_little_world/hand/great_hand/look_few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_able_number(str_arg):
+    same_man(str_arg)
+    print('other_work')
+
+def same_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_able_number('government_and_life')
