fe8928bfdf246c4720bef317b1b93da0061a580d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:03:47 -0500

    "add: come super/new_world/get_year/long_man/large_year/work_and_company.py and super/get_company/seem_life_after_group.py at different government"

---
+++ super/get_company/seem_life_after_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_woman(str_arg):
+    part(str_arg)
+    print('see_year')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_woman('old_woman')
