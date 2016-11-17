e4e41cdf1783db05ff9cb841754e5f108074af4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:10:46 -0500

    "mv: use super/woman/feel_early_part_with_own_case/woman_and_person/important_thing_and_public_day.py and super/young_world/great_eye/old_place.py beneath old place"

---
+++ super/woman/feel_early_part_with_own_case/woman_and_person/important_thing_and_public_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_child_or_bad_number(str_arg):
+    same_world(str_arg)
+    print('think_child')
+
+def same_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_child_or_bad_number('week_or_bad_hand')
