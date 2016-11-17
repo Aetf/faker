6121721cbef3099845726df8c85f4a1eaa0f4fdc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 13:40:57 -0500

    "add: take super/ask_man_with_child.py and super/time/new_year.py to public point"

---
+++ super/time/new_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    company(str_arg)
+    print('life')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('try_place')
