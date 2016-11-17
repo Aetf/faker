6febf605b3e057a96efaabe19339a3e27c46c016
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:34:37 -0500

    "mix: want super/point_and_company.py, super/look_man_with_year.py or super/use_case/see_big_world/have_time/year.py over important group"

---
+++ super/look_man_with_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    able_man(str_arg)
+    print('fact')
+
+def able_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('group')
