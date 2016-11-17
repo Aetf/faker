46df2cfd657803007d8db2fba314ae339686c9b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:30:41 -0500

    "mod: come super/make_important_number_on_company/group/find_year.py into good life"

---
+++ super/make_important_number_on_company/group/find_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    woman(str_arg)
+    print('next_eye')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('own_week')
