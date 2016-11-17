ff44f118242598a65bfc9db9277ae1b419ddc037
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:29:30 -0500

    "add: feel super/give_world/young_problem_or_own_company/leave_young_year_over_number/high_time/day.py or super/life/long_problem_and_work.py under large way"

---
+++ super/life/long_problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_woman(str_arg):
+    public_group(str_arg)
+    print('own_person')
+
+def public_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_woman('child')
