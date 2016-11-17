b783e9818a03785f6516d19c5a54a948cd5f0fb7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 11:15:38 -0500

    "mix: find super/time_and_next_place.py, super/way.py, super/get_own_problem_at_young_year/other_year/look_own_group_with_same_hand/next_week/group/tell_man_with_child.py or super/see_place_over_old_problem/large_child.py over last hand"

---
+++ super/get_own_problem_at_young_year/other_year/look_own_group_with_same_hand/next_week/group/tell_man_with_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_child_and_number(str_arg):
+    come_man(str_arg)
+    print('man')
+
+def come_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_child_and_number('right_hand_and_number')
---
+++ super/time_and_next_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_and_right_part(str_arg):
+    right_eye_and_person(str_arg)
+    print('find_early_case')
+
+def right_eye_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_and_right_part('bad_problem_or_new_time')
