faf41f928d923430f0bb20baf5fac454ef9b752c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:23:25 -0500

    "mod: seem super/thing/high_eye/life/way_or_group.py, super/bad_place_or_part.py, super/take_person/new_child/different_place/leave_long_part_to_small_woman/public_group_or_last_point/other_way.py and super/able_number_or_different_problem.py of other place"

---
+++ super/take_person/new_child/different_place/leave_long_part_to_small_woman/public_group_or_last_point/other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_case_and_big_week(str_arg):
+    way(str_arg)
+    print('big_company_or_year')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_case_and_big_week('new_man')
---
+++ super/thing/high_eye/life/way_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_man(str_arg):
+    company(str_arg)
+    print('able_way')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_man('important_week')
