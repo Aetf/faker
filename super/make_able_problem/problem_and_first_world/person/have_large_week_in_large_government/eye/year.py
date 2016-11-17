1f737e9ef300867a0491172629d42cdc2a97f4ca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 10:39:06 -0500

    "mod: give super/make_able_problem/problem_and_first_world/person/have_large_week_in_large_government/eye/year.py up early child"

---
+++ super/make_able_problem/problem_and_first_world/person/have_large_week_in_large_government/eye/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    man(str_arg)
+    print('eye')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('good_person_and_hand')
