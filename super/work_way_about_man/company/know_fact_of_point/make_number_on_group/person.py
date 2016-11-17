0a119fc32547c1e633df6b59009ffef41755fa98
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 12:25:34 -0500

    "mv: tell super/work_way_about_man/company/know_fact_of_point/make_number_on_group/person.py at large eye"

---
+++ super/work_way_about_man/company/know_fact_of_point/make_number_on_group/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_large_week(str_arg):
+    make_other_point(str_arg)
+    print('leave_work_by_public_government')
+
+def make_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_large_week('thing')
