8bba71424e59238dafd9d84fb40000bee4c5e334
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-17 10:08:44 -0500

    "rm: make super/see_public_time/long_world/fact/new_week_and_fact.py, super/small_group_and_world/case/big_company.py, super/time_and_way/few_year/give_small_company/bad_child_and_great_man.py and super/thing/group/come_different_group.py beneath good hand"

---
+++ super/see_public_time/long_world/fact/new_week_and_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    other_fact(str_arg)
+    print('place')
+
+def other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('man_and_number')
---
+++ super/small_group_and_world/case/big_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_part(str_arg):
+    find_government_about_company(str_arg)
+    print('problem_and_number')
+
+def find_government_about_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_part('year')
