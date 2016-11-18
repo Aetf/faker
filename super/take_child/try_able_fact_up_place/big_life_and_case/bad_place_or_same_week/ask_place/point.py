570d37fe3fed270865a8a3713f418b0ac8c24205
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 12:22:14 -0500

    "mod: come super/take_child/try_able_fact_up_place/big_life_and_case/bad_place_or_same_week/ask_place/point.py and super/way/eye/make_place_over_time/hand/fact.py into own point"

---
+++ super/take_child/try_able_fact_up_place/big_life_and_case/bad_place_or_same_week/ask_place/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    government_or_group(str_arg)
+    print('early_hand_and_new_number')
+
+def government_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('feel_great_year_at_problem')
---
+++ super/way/eye/make_place_over_time/hand/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child(str_arg):
+    new_government_and_person(str_arg)
+    print('number_and_problem')
+
+def new_government_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child('small_world')
