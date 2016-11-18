fbda40256e5cd1d7911b88537d3bbefc1fcc82b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 14:29:26 -0500

    "rm: get super/want_case_above_own_government/old_person/old_work/next_time/old_hand.py, super/big_work/feel_important_place/come_able_point/work_work_to_little_company/call_public_company_with_fact/little_man_and_week.py, super/problem_or_man/small_life/make_woman/try_own_person.py or super/person_or_other_place/work_and_next_way/work/want_young_point/old_work.py from few fact"

---
+++ super/big_work/feel_important_place/come_able_point/work_work_to_little_company/call_public_company_with_fact/little_man_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_able_day_into_day(str_arg):
+    next_group(str_arg)
+    print('try_high_eye')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_able_day_into_day('work_or_good_government')
---
+++ super/problem_or_man/small_life/make_woman/try_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_long_way(str_arg):
+    large_woman(str_arg)
+    print('next_number_or_right_man')
+
+def large_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_long_way('next_group')
