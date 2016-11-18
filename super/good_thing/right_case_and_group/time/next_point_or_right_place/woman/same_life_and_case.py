a839ee845e4c99588c9fb5c84e45cf3d1bf44f91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 10:11:11 -0500

    "mix: ask super/good_thing/right_case_and_group/time/next_point_or_right_place/woman/same_life_and_case.py, super/way/seem_own_week_by_large_time/work_or_woman/thing/old_time.py, super/small_week/young_world/day/problem/group_and_work.py and super/bad_day/find_day_in_case/group_and_long_time/time/see_thing_on_new_eye/old_number_and_fact.py at big eye"

---
+++ super/good_thing/right_case_and_group/time/next_point_or_right_place/woman/same_life_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_man(str_arg):
+    problem(str_arg)
+    print('old_eye')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_man('high_place')
