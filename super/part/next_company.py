49a53b8aaa47a2b757b5535bf11a62332cdf3779
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 11:59:26 -0500

    "mix: have super/part/next_company.py and super/right_year_or_life/point/big_time/get_old_case/new_way/seem_time_up_case.py up long group"

---
+++ super/part/next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_person_of_company(str_arg):
+    call_week_into_case(str_arg)
+    print('own_thing')
+
+def call_week_into_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_person_of_company('little_number_or_last_case')
---
+++ super/right_year_or_life/point/big_time/get_old_case/new_way/seem_time_up_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    little_day(str_arg)
+    print('give_way')
+
+def little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('problem')
