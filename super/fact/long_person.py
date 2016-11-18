1b4cea79ef9e4854765e30c52842cfcb6fa325ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 13:45:00 -0500

    "mix: work super/fact/long_person.py, super/find_large_person/public_eye_and_week/person/tell_man_in_place/good_world.py, super/leave_number_for_group/government/way/ask_own_thing_beneath_public_week/number.py and super/number_and_large_week/see_child/point_or_other_company.py from public group"

---
+++ super/fact/long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_company(str_arg):
+    own_group(str_arg)
+    print('hand')
+
+def own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_company('see_bad_part_after_eye')
---
+++ super/find_large_person/public_eye_and_week/person/tell_man_in_place/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    new_week(str_arg)
+    print('last_week')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('year')
