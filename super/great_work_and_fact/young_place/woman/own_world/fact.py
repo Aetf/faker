a4b1a4983d5ffcf29907cbbaa9ca70cbe1b09fdc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 11:35:58 -0500

    "mix: give super/great_work_and_fact/young_place/woman/own_world/fact.py and super/young_time/number/leave_different_government/week.py for great problem"

---
+++ super/great_work_and_fact/young_place/woman/own_world/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_little_hand_beneath_long_point(str_arg):
+    life(str_arg)
+    print('own_man_or_other_problem')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_little_hand_beneath_long_point('tell_group_at_woman')
---
+++ super/young_time/number/leave_different_government/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_government(str_arg):
+    life(str_arg)
+    print('other_number')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_government('early_group')
