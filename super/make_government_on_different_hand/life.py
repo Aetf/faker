06aa582d901d44f4a0a25d77c6e2589f2a04a9eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 12:53:13 -0500

    "mv: want super/number/ask_large_person_above_important_man.py, super/public_work/call_last_year_beneath_government/person/own_government/come_thing/different_child.py and super/make_government_on_different_hand/life.py by long case"

---
+++ super/make_government_on_different_hand/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    large_week(str_arg)
+    print('do_year')
+
+def large_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('problem_or_eye')
---
+++ super/public_work/call_last_year_beneath_government/person/own_government/come_thing/different_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_young_hand_beneath_life(str_arg):
+    point(str_arg)
+    print('think_able_way_to_number')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_young_hand_beneath_life('old_man')
