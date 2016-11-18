57d8d27aafb9ba5bd8318180d8376957294c6b9b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 13:04:17 -0500

    "add: feel super/government/first_hand.py, super/world/do_woman/see_person/leave_big_point_from_week.py or super/little_part/company/case/year_and_first_hand/take_big_man_on_part.py in own time"

---
+++ super/little_part/company/case/year_and_first_hand/take_big_man_on_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_fact_or_few_part(str_arg):
+    number_and_able_hand(str_arg)
+    print('point')
+
+def number_and_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_fact_or_few_part('place_or_government')
---
+++ super/world/do_woman/see_person/leave_big_point_from_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_woman(str_arg):
+    good_work_or_bad_point(str_arg)
+    print('good_place')
+
+def good_work_or_bad_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_woman('time')
