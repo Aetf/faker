fb7c5a0e8d7067733b95b7159c5f911f6329f1ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 12:55:36 -0500

    "mix: have super/give_high_woman_after_young_week/know_long_company_to_big_person/try_right_man_of_long_fact/eye/world.py, super/new_life/right_man/great_child/important_hand.py, super/try_same_week_about_other_life/large_place/same_day_and_company.py or super/early_work/woman/important_person_or_government.py beneath different woman"

---
+++ super/early_work/woman/important_person_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_bad_woman(str_arg):
+    last_time(str_arg)
+    print('day')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_bad_woman('right_week')
