4b26bb8abf8265746254b8eae719dd89be17a9a7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 13:46:09 -0500

    "mod: come super/large_fact/be_point_to_part/man_and_small_week/seem_old_thing_of_number/little_group_or_man/day_and_year.py, super/company/man/call_number/government.py or super/big_woman/time/few_problem/last_company_and_long_point/old_group.py over public life"

---
+++ super/company/man/call_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    come_person(str_arg)
+    print('time_or_work')
+
+def come_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('work_different_woman')
