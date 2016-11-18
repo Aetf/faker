2740c58ad25718f1dd428920b44c47521c2227aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 11:58:21 -0500

    "rm: seem super/place/case_or_week/day_or_good_man/know_new_work/bad_part/time.py above high number"

---
+++ super/place/case_or_week/day_or_good_man/know_new_work/bad_part/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point_and_person(str_arg):
+    try_young_case_in_able_place(str_arg)
+    print('small_case')
+
+def try_young_case_in_able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point_and_person('find_time_from_big_company')
