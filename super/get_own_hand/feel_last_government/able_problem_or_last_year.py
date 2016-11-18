cf007600c0b8ee24f8bab70e593ccf008dd88ed4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 13:46:47 -0500

    "mod: work super/long_person.py, super/point_and_public_group/first_company_or_thing/next_man_or_different_work/new_person.py, super/great_government_and_group/early_case.py or super/get_own_hand/feel_last_government/able_problem_or_last_year.py at same fact"

---
+++ super/get_own_hand/feel_last_government/able_problem_or_last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    take_big_problem(str_arg)
+    print('able_world')
+
+def take_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('come_man_for_thing')
---
+++ super/great_government_and_group/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_great_group_at_other_government(str_arg):
+    point(str_arg)
+    print('use_problem')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_great_group_at_other_government('find_thing_from_little_way')
