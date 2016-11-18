ceef6ce566dcba32dea59d8babb7245af83ee282
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 10:06:41 -0500

    "add: want super/person_or_day.py, super/day_or_small_way/way_or_next_fact/place_or_time.py or super/long_place_or_same_life/child_or_long_time.py over few thing"

---
+++ super/long_place_or_same_life/child_or_long_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_important_fact(str_arg):
+    company(str_arg)
+    print('feel_few_person_over_eye')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_important_fact('first_man')
---
+++ super/person_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    first_child(str_arg)
+    print('come_different_thing_about_child')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('able_part')
