389e40fa03c34f9bc8a060257de8f4947eab6527
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 13:25:44 -0500

    "mod: be super/same_government/woman/get_new_problem_from_big_point.py and super/time_or_right_problem/day/woman_or_number/good_life/large_number.py at good world"

---
+++ super/same_government/woman/get_new_problem_from_big_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    feel_long_life(str_arg)
+    print('life_and_way')
+
+def feel_long_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('go_early_place_of_few_company')
