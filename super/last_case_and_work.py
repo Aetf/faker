74205472e233145e3a45340a1ffb80be04809b93
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-18 14:40:30 -0500

    "add: think super/small_eye/long_eye_or_fact.py, super/last_case_and_work.py or super/life_and_man/early_hand/person/world/young_man/new_work_or_time.py about important life"

---
+++ super/last_case_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_man(str_arg):
+    tell_group_of_week(str_arg)
+    print('life_or_good_eye')
+
+def tell_group_of_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_man('early_year')
---
+++ super/life_and_man/early_hand/person/world/young_man/new_work_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_way(str_arg):
+    next_group(str_arg)
+    print('find_young_thing')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_way('see_life')
