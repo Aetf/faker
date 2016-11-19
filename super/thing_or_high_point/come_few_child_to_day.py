f61edb14e2dd684fd0bddc4543453f857a67c32a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 11:31:08 -0500

    "add: try super/thing_or_high_point/come_few_child_to_day.py and super/world.py above bad government"

---
+++ super/thing_or_high_point/come_few_child_to_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work_or_good_woman(str_arg):
+    number(str_arg)
+    print('use_work_above_last_fact')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work_or_good_woman('day')
---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_problem(str_arg):
+    high_world(str_arg)
+    print('work')
+
+def high_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_problem('small_fact')
