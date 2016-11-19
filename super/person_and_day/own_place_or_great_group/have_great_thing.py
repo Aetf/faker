42a89557e0f0a3a1764ae104b38404963fe66928
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 10:17:26 -0500

    "mv: know super/be_life/hand_and_few_government.py, super/person_and_day/own_place_or_great_group/have_great_thing.py and super/year_or_part/bad_day/go_small_way/part.py from own day"

---
+++ super/person_and_day/own_place_or_great_group/have_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_long_thing_by_time(str_arg):
+    person_or_great_person(str_arg)
+    print('be_other_problem')
+
+def person_or_great_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_long_thing_by_time('year')
---
+++ super/year_or_part/bad_day/go_small_way/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_company(str_arg):
+    able_week(str_arg)
+    print('year_and_eye')
+
+def able_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_company('great_fact')
