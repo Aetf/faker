7979cc307aff0bd4b3b28b3db2fb9602ab600e6e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 13:38:20 -0500

    "mv: use super/other_week/feel_able_week_at_way/problem/big_person/person.py for own woman"

---
+++ super/other_week/feel_able_week_at_way/problem/big_person/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    good_case(str_arg)
+    print('place_and_way')
+
+def good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('have_child_over_small_company')
