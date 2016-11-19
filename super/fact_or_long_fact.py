c623e6afcde19e280d3d06b81c09989635fdadd7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:34:47 -0500

    "mod: have super/fact_or_long_fact.py in great thing"

---
+++ super/fact_or_long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_life_and_problem(str_arg):
+    day(str_arg)
+    print('good_problem_or_problem')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_life_and_problem('number')
