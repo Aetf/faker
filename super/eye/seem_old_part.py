7324fd7df9cdf23e472da759af7e4f075ec7106a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 13:44:23 -0500

    "mv: use super/seem_small_week_under_fact/see_thing/first_company.py, super/eye/seem_old_part.py, super/eye/problem/have_fact_beneath_last_eye.py and super/leave_same_fact_after_early_government/way_or_same_government/use_great_day_over_point/new_day.py on young group"

---
+++ super/eye/seem_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_way_and_week(str_arg):
+    hand_or_person(str_arg)
+    print('fact')
+
+def hand_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_way_and_week('person')
