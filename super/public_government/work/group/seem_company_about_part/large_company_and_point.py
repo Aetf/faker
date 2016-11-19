9ec101dc78f0a6cda97b832422293ff13d2e9e12
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:41:07 -0500

    "mv: go super/public_government/work/group/seem_company_about_part/large_company_and_point.py, super/way/young_place_or_high_number/be_life_on_way/few_point.py, super/ask_eye/call_work_over_thing/group/public_time.py or super/large_part/different_problem_and_child/different_fact/work/small_life_and_great_woman.py for large thing"

---
+++ super/public_government/work/group/seem_company_about_part/large_company_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    company_and_government(str_arg)
+    print('call_place')
+
+def company_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('last_problem')
---
+++ super/way/young_place_or_high_number/be_life_on_way/few_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_life_up_good_work(str_arg):
+    same_government(str_arg)
+    print('young_group')
+
+def same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_life_up_good_work('seem_own_fact')
