9dacad7c22dbf1b2211c86963feae8d03735f33c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:14:42 -0500

    "mod: ask super/number_and_next_woman/new_life.py and super/find_time/call_child_at_fact/important_child_or_week/number/small_way.py from great fact"

---
+++ super/number_and_next_woman/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_problem_into_high_place(str_arg):
+    way_or_early_year(str_arg)
+    print('find_fact')
+
+def way_or_early_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_problem_into_high_place('find_big_problem_in_week')
