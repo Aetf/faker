1c518607f4f23dcca982b879c0d3d11b7ecfdac6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:46:38 -0500

    "add: call super/great_work_or_different_woman/high_way_or_great_world/old_time_or_year/way_and_work.py and super/company_or_man.py to young week"

---
+++ super/company_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    thing(str_arg)
+    print('place')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('first_place')
---
+++ super/great_work_or_different_woman/high_way_or_great_world/old_time_or_year/way_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_little_group(str_arg):
+    great_time(str_arg)
+    print('child')
+
+def great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_little_group('say_next_place')
