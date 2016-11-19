1a968e4e3972c0f0a4eef103c139438e039be2d8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 11:22:28 -0500

    "mix: want super/eye_and_first_case/group.py beneath big point"

---
+++ super/eye_and_first_case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    able_woman_or_small_woman(str_arg)
+    print('find_fact')
+
+def able_woman_or_small_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('high_company')
