273848fcf47de837f712978b0e5f8659e89019a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:34:21 -0500

    "rm: see super/eye/next_life/good_child/important_thing_or_bad_hand.py and super/government_or_right_time/little_man.py from young day"

---
+++ super/government_or_right_time/little_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_government(str_arg):
+    company(str_arg)
+    print('way')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_government('life')
