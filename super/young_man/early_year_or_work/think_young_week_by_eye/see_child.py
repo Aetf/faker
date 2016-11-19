a187eb1bab4e1bcbfe234ac02f47f127e4c50e9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 11:11:23 -0500

    "rm: call super/part/able_world_and_little_time/have_young_life_into_last_person/same_company/early_week/case.py, super/little_thing_and_important_week/get_time/great_fact.py and super/young_man/early_year_or_work/think_young_week_by_eye/see_child.py for long hand"

---
+++ super/young_man/early_year_or_work/think_young_week_by_eye/see_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    year(str_arg)
+    print('case')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('early_thing_and_other_life')
