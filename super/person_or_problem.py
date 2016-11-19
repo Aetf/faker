e0e5f6c6cced9df4b0d66ebb3a648440d93cc690
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:36:05 -0500

    "mod: seem super/person_or_problem.py above other week"

---
+++ super/person_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_other_week(str_arg):
+    important_part_or_day(str_arg)
+    print('own_hand')
+
+def important_part_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_other_week('thing')
