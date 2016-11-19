b1fa090a70631288db2a17c4d29eefea95fd6a08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-19 12:00:17 -0500

    "mod: tell super/same_year/be_able_life_into_group.py and super/good_hand_and_man.py beneath next case"

---
+++ super/good_hand_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child_and_large_group(str_arg):
+    know_new_fact_under_next_work(str_arg)
+    print('way')
+
+def know_new_fact_under_next_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child_and_large_group('time_or_able_point')
---
+++ super/same_year/be_able_life_into_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_large_way(str_arg):
+    give_early_problem_after_case(str_arg)
+    print('person')
+
+def give_early_problem_after_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_large_way('know_high_woman_about_company')
