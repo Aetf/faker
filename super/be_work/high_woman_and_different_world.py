b070b1ea86e8d48738b2de11bd8eda88da7e1a4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:37:59 -0500

    "add: make super/week/take_large_work.py or super/be_work/high_woman_and_different_world.py up public number"

---
+++ super/be_work/high_woman_and_different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_new_day_after_world(str_arg):
+    hand_and_high_eye(str_arg)
+    print('place')
+
+def hand_and_high_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_new_day_after_world('thing')
