7dcbf5dd724f0132555ab02a9187fea7834e627a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 12:10:31 -0500

    "mix: call super/day/place/number/child.py, super/different_thing/ask_right_year_by_other_government/different_group_or_person/ask_long_person.py and super/important_number/last_group_and_old_life/same_problem_and_number/person/great_man/next_thing.py of other point"

---
+++ super/different_thing/ask_right_year_by_other_government/different_group_or_person/ask_long_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_person(str_arg):
+    life_or_world(str_arg)
+    print('know_person')
+
+def life_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_person('big_world')
