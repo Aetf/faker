27ff40db44b6a7d58729e8ae01b73bb921becfca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:13:27 -0500

    "add: say super/next_place_or_eye/problem_or_thing/long_time_or_world/company/woman.py in small person"

---
+++ super/next_place_or_eye/problem_or_thing/long_time_or_world/company/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_case_or_work(str_arg):
+    part(str_arg)
+    print('year_or_small_year')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_case_or_work('come_problem')
