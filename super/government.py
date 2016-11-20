dc38131685f2fa8b96cc9428d678f73a6272fc99
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:15:13 -0500

    "add: say super/find_able_woman/say_able_thing/work_good_company/man.py or super/government.py with small eye"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_group(str_arg):
+    see_part_under_good_world(str_arg)
+    print('part')
+
+def see_part_under_good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_group('man')
