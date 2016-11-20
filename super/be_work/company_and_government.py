635b98219ca6f2d06979af37ee4f28e0a048ba7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:41:08 -0500

    "mix: have super/high_day.py and super/be_work/company_and_government.py up small person"

---
+++ super/be_work/company_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_group(str_arg):
+    say_government(str_arg)
+    print('day')
+
+def say_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_group('tell_number')
