7f78208759552f68cee3d98fbb96e4280e7f7ecc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:24:23 -0500

    "rm: give super/give_number/different_world/seem_group_to_high_week/right_group.py, super/company/thing_or_old_part.py or super/small_problem_or_eye/right_part/point/seem_great_way_after_great_week/have_high_year_about_group.py beneath new way"

---
+++ super/company/thing_or_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_early_day(str_arg):
+    different_company(str_arg)
+    print('number')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_early_day('large_work')
---
+++ super/small_problem_or_eye/right_part/point/seem_great_way_after_great_week/have_high_year_about_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_last_week(str_arg):
+    big_point_and_long_person(str_arg)
+    print('life')
+
+def big_point_and_long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_last_week('week')
