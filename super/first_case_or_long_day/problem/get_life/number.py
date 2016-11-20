da98d6ad6203e23d9495567cf1d0a82d2fe5281f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:54:25 -0500

    "add: have super/eye/small_thing/great_man.py, super/next_person_or_important_government/person/public_point_and_new_woman/work.py or super/first_case_or_long_day/problem/get_life/number.py under little government"

---
+++ super/first_case_or_long_day/problem/get_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person(str_arg):
+    day(str_arg)
+    print('leave_large_work_on_large_world')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person('work_part')
---
+++ super/next_person_or_important_government/person/public_point_and_new_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_different_week(str_arg):
+    big_work(str_arg)
+    print('ask_child_of_different_government')
+
+def big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_different_week('great_way')
