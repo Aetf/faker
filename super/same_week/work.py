24159717dc827eb3b85557e2e02d61ad5d5cbab5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:07:35 -0500

    "mix: seem super/same_week/work.py and super/want_year/few_part_or_government/come_child_from_week/number_and_case/think_year/next_fact.py for public time"

---
+++ super/same_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    early_time(str_arg)
+    print('high_year_and_point')
+
+def early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('day')
