eb9a219e7d63f086d1f93d09b9785b8447171086
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 11:20:58 -0500

    "add: leave super/person/life/own_way_and_large_time/make_able_woman.py, super/old_life/have_person_in_first_day/place_or_great_point.py or super/use_young_man/world/big_week/old_company_or_number.py up large way"

---
+++ super/person/life/own_way_and_large_time/make_able_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_group(str_arg):
+    find_hand(str_arg)
+    print('want_way')
+
+def find_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_group('man')
