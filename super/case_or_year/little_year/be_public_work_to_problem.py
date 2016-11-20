0d310694a4bc99cba97dfe4e18114f9cd8d927fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:02:48 -0500

    "mod: leave super/day/place/bad_company/time_and_day/seem_old_number_over_other_company/work.py, super/case_or_year/little_year/be_public_work_to_problem.py or super/know_early_case_of_point/say_man_beneath_time.py by other group"

---
+++ super/case_or_year/little_year/be_public_work_to_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_child(str_arg):
+    want_world_above_week(str_arg)
+    print('new_government')
+
+def want_world_above_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_child('bad_company_or_good_thing')
