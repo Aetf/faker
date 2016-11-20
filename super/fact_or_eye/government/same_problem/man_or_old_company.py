c9a2568816a4334547ad9a44b20685b11efa5bd2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:31:01 -0500

    "rm: think super/see_group/bad_place/week.py and super/fact_or_eye/government/same_problem/man_or_old_company.py of first work"

---
+++ super/fact_or_eye/government/same_problem/man_or_old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_great_person_by_year(str_arg):
+    think_day_under_case(str_arg)
+    print('young_problem_or_number')
+
+def think_day_under_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_great_person_by_year('child')
---
+++ super/see_group/bad_place/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_number(str_arg):
+    long_number(str_arg)
+    print('call_way_about_man')
+
+def long_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_number('leave_week')
