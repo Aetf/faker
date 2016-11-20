3139fcb10b85ae04608e83225e310e4375fb87c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:31:33 -0500

    "rm: call super/part.py or super/company.py from small place"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man_or_old_point(str_arg):
+    life(str_arg)
+    print('feel_fact')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man_or_old_point('new_number_or_young_thing')
