b9c25e0b0048e8909acec98023a6ef350b3bbe59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 14:44:50 -0500

    "mix: get super/large_case/hand/tell_last_thing/public_fact.py and super/able_week/first_number/know_important_place_at_fact/point.py with few government"

---
+++ super/large_case/hand/tell_last_thing/public_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_fact(str_arg):
+    place(str_arg)
+    print('number')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_fact('great_thing_and_case')
