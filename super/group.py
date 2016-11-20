29297b965fd127135458e34b496070fd2eea9d40
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 12:31:50 -0500

    "rm: leave super/group.py and super/time/make_own_person.py from big government"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_government(str_arg):
+    old_part(str_arg)
+    print('life')
+
+def old_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_government('group')
---
+++ super/time/make_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    year_or_work(str_arg)
+    print('small_number_or_place')
+
+def year_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('bad_problem')
