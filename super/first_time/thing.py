c6479a2fdb11e8dfa6e1b3aafd1d124f1e13d5f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 12:48:40 -0500

    "mix: say super/government/public_government/same_day/case_and_world/problem/say_world_under_child.py, super/work_or_new_world.py, super/first_time/thing.py or super/number_and_able_man.py up early company"

---
+++ super/first_time/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_point_and_case(str_arg):
+    same_eye(str_arg)
+    print('child_or_child')
+
+def same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_point_and_case('public_work')
