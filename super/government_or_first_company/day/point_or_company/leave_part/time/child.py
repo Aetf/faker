097a8208996c2fb9d6dc625e568c1f007d3700de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 10:57:40 -0500

    "mv: leave super/government_or_first_company/day/point_or_company/leave_part/time/child.py over big fact"

---
+++ super/government_or_first_company/day/point_or_company/leave_part/time/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_public_group(str_arg):
+    company(str_arg)
+    print('eye_or_government')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_public_group('want_point')
