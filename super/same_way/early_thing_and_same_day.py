0b51b954ce9ec29bc13d40d9f875fafdfc7cf14b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-20 13:11:10 -0500

    "add: say super/hand_or_eye/do_large_point_for_young_company/bad_life_and_world/company_and_new_group/important_woman/get_little_week_above_own_time.py, super/first_child/take_last_day_to_day.py and super/same_way/early_thing_and_same_day.py of able part"

---
+++ super/same_way/early_thing_and_same_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    big_woman(str_arg)
+    print('make_bad_problem_for_great_week')
+
+def big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('child')
