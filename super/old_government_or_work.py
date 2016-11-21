d782b6adb9409cc98925fb4dd1f7c7e5d9d3ca1b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 12:51:21 -0500

    "rm: have super/old_government_or_work.py for new work"

---
+++ super/old_government_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    say_world_on_point(str_arg)
+    print('public_week')
+
+def say_world_on_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('first_hand')
