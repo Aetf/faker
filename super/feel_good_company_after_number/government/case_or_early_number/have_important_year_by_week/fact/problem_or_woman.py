07cc2127bc3171da97708788e95ca07a023edcdb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 14:01:33 -0500

    "rm: have super/life/use_same_part/right_way.py, super/feel_good_company_after_number/government/case_or_early_number/have_important_year_by_week/fact/problem_or_woman.py or super/part/see_world_to_eye/own_place/young_part/old_woman.py after same government"

---
+++ super/feel_good_company_after_number/government/case_or_early_number/have_important_year_by_week/fact/problem_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem(str_arg):
+    high_fact(str_arg)
+    print('company_and_next_company')
+
+def high_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem('important_eye')
