da135be23f58c601b4b7b89b67609c1ee9cf532c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 12:14:49 -0500

    "mv: think super/point_or_long_life/come_man/other_problem_or_work.py beneath good woman"

---
+++ super/point_or_long_life/come_man/other_problem_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group_or_able_case(str_arg):
+    good_group_and_early_life(str_arg)
+    print('leave_point')
+
+def good_group_and_early_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group_or_able_case('eye')
