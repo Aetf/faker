70b6ae9b6a0614fd939a705bc0b35d150df191ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 14:38:58 -0500

    "rm: work super/woman/next_group_and_place/young_time/want_problem.py and super/be_small_problem/try_place/have_way_into_thing.py for next life"

---
+++ super/be_small_problem/try_place/have_way_into_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_government(str_arg):
+    bad_problem(str_arg)
+    print('way_or_woman')
+
+def bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_government('other_part')
