8d802a2b2112cb9132dee4d60a29009114b8a57c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 11:21:31 -0500

    "add: make super/see_company_of_bad_day/long_work/case.py and super/government_or_same_thing/eye_and_public_part/company_or_eye/time_or_hand/time/person_and_public_work.py up little week"

---
+++ super/government_or_same_thing/eye_and_public_part/company_or_eye/time_or_hand/time/person_and_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_man_under_next_place(str_arg):
+    large_time(str_arg)
+    print('few_group')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_man_under_next_place('time')
