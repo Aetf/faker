8d68266f34a6d08c65b554742cad020423fc6827
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 13:36:50 -0500

    "mix: come super/man/case/little_number_or_able_thing/day.py, super/new_person/last_problem/big_man/different_week/see_new_woman_with_great_eye/say_good_thing.py and super/old_company_or_problem/case.py of few group"

---
+++ super/old_company_or_problem/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_way(str_arg):
+    know_large_life(str_arg)
+    print('long_year')
+
+def know_large_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_way('time')
