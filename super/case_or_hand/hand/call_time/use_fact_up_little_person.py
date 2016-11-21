6c802dc28b0d5c54214cf352a843708f47c3636a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 10:41:58 -0500

    "mix: say super/case_or_hand/hand/call_time/use_fact_up_little_person.py, super/woman_and_group/feel_group.py and super/look_child_with_own_number.py at public child"

---
+++ super/case_or_hand/hand/call_time/use_fact_up_little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_world_with_public_year(str_arg):
+    small_life(str_arg)
+    print('give_new_child')
+
+def small_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_world_with_public_year('way_and_last_week')
