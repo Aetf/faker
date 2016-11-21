119f6ce5f142e21d7584c051cad29e04e30e06ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 10:13:27 -0500

    "mv: want super/work_world/work_work/right_part_or_week/place/high_life.py, super/see_work_under_important_week/child.py, super/government/give_same_eye.py and super/come_bad_place_for_woman/know_way/take_last_place/part_and_little_year/do_long_hand_over_large_point.py over long thing"

---
+++ super/come_bad_place_for_woman/know_way/take_last_place/part_and_little_year/do_long_hand_over_large_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    part_or_large_man(str_arg)
+    print('young_time_and_case')
+
+def part_or_large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('child_and_fact')
---
+++ super/see_work_under_important_week/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    woman_and_work(str_arg)
+    print('do_woman_by_other_group')
+
+def woman_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('case_or_high_company')
