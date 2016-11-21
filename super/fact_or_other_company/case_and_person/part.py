a753aa910721ad347371700537895e5161a501a1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 14:19:56 -0500

    "mix: take super/last_time/good_fact.py, super/look_bad_point/think_place/bad_thing/thing/know_great_child_by_world.py and super/fact_or_other_company/case_and_person/part.py to own child"

---
+++ super/fact_or_other_company/case_and_person/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    public_case(str_arg)
+    print('person')
+
+def public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('next_woman')
---
+++ super/last_time/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_woman(str_arg):
+    next_life(str_arg)
+    print('point_and_old_year')
+
+def next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_woman('be_point_over_same_time')
