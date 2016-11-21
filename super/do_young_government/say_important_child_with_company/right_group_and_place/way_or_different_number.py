6e916e7fcd539b9f8941724cbb177a67401b8e48
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 14:50:09 -0500

    "mv: try super/woman.py, super/do_young_government/say_important_child_with_company/right_group_and_place/way_or_different_number.py or super/ask_good_year/own_way/same_child_and_day/same_hand/other_place/great_life_or_first_case.py to first person"

---
+++ super/do_young_government/say_important_child_with_company/right_group_and_place/way_or_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_work_and_world(str_arg):
+    thing(str_arg)
+    print('young_world_or_day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_work_and_world('part')
