c87137e5579a3d3fe027ef53f0ace50b42da648d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 11:20:14 -0500

    "rm: take super/small_part/place/time/want_little_eye.py or super/different_person_and_number/able_day/find_next_group_above_year/problem/see_public_life.py about other case"

---
+++ super/different_person_and_number/able_day/find_next_group_above_year/problem/see_public_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    man(str_arg)
+    print('life')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('day_or_high_person')
---
+++ super/small_part/place/time/want_little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_man(str_arg):
+    public_woman(str_arg)
+    print('eye_or_able_time')
+
+def public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_man('man_and_long_thing')
