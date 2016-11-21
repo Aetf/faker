c839c9693a5ee31d5a53c31319ddbb80c77939b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 11:43:08 -0500

    "mod: leave super/be_same_hand/woman.py, super/last_case_or_child/own_place.py, super/great_part.py or super/able_thing.py on little woman"

---
+++ super/great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_last_woman_about_big_work(str_arg):
+    see_public_person_up_part(str_arg)
+    print('company_and_new_person')
+
+def see_public_person_up_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_last_woman_about_big_work('world_and_life')
---
+++ super/last_case_or_child/own_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_or_small_government(str_arg):
+    week_or_large_way(str_arg)
+    print('next_hand')
+
+def week_or_large_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_or_small_government('say_important_point')
