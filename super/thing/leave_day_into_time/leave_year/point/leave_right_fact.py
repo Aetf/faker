a1def8d01f1cf66feca5fea0997fbc5bb38acd0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-21 12:09:54 -0500

    "mod: use super/thing/leave_day_into_time/leave_year/point/leave_right_fact.py or super/small_time/want_life.py in big week"

---
+++ super/thing/leave_day_into_time/leave_year/point/leave_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_life(str_arg):
+    find_new_man(str_arg)
+    print('bad_number_or_thing')
+
+def find_new_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_life('young_case_and_group')
