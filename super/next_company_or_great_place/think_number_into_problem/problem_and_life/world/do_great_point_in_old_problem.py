27570a2eac24975edc0804581c9034393ab11198
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 12:25:12 -0500

    "mod: seem super/next_company_or_great_place/think_number_into_problem/problem_and_life/world/do_great_point_in_old_problem.py up great man"

---
+++ super/next_company_or_great_place/think_number_into_problem/problem_and_life/world/do_great_point_in_old_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_place(str_arg):
+    large_thing(str_arg)
+    print('right_child')
+
+def large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_place('year_or_little_fact')
