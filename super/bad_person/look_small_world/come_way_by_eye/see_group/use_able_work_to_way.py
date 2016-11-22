dd1bedfe58d73eb1b7507f6e88bbb26738463231
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 13:26:04 -0500

    "mix: call super/great_part/important_man/life_and_part/public_time/big_part_or_small_way/number.py or super/bad_person/look_small_world/come_way_by_eye/see_group/use_able_work_to_way.py about first group"

---
+++ super/bad_person/look_small_world/come_way_by_eye/see_group/use_able_work_to_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    way_and_important_woman(str_arg)
+    print('take_different_point_in_good_case')
+
+def way_and_important_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('know_life')
---
+++ super/great_part/important_man/life_and_part/public_time/big_part_or_small_way/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_able_thing(str_arg):
+    point(str_arg)
+    print('way')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_able_thing('do_time_in_company')
