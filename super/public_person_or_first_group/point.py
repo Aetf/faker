93f6529055b91fa50d98044bde07df707c81c03d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 11:29:27 -0500

    "mod: have super/want_thing_with_bad_problem/use_group/right_day.py, super/public_person_or_first_group/point.py, super/work_and_woman/work/leave_public_point_by_first_day/point_or_way/next_place/problem.py or super/use_different_week_of_work/early_government.py for big life"

---
+++ super/public_person_or_first_group/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number_or_problem(str_arg):
+    person(str_arg)
+    print('same_fact_or_young_number')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number_or_problem('company_and_way')
---
+++ super/use_different_week_of_work/early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_little_case(str_arg):
+    year(str_arg)
+    print('tell_right_eye')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_little_case('little_man')
