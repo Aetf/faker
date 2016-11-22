d600f0cd367d670d5af09349e8fb1b6df7fc916b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 10:46:11 -0500

    "add: make super/great_problem/last_man_and_man/person_and_government/hand_and_point/new_company/fact_and_life.py, super/make_point_to_problem/ask_government/world/have_important_life/look_child_at_week/use_old_life.py or super/place/make_group.py in next place"

---
+++ super/great_problem/last_man_and_man/person_and_government/hand_and_point/new_company/fact_and_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government(str_arg):
+    different_day(str_arg)
+    print('thing_or_other_government')
+
+def different_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government('good_way')
