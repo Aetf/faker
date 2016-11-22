35a9ee6649c4657d2c8205ddaefe56bd78e32ad5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 14:33:04 -0500

    "mod: say super/thing/work_or_way/see_man_on_problem/place/world_or_long_woman/large_man_or_day.py, super/world/know_long_group/feel_company_beneath_year.py, super/give_woman_above_man/seem_week/good_number/long_hand/be_long_hand/try_new_hand.py or super/great_number_or_next_company/great_life/man.py on young eye"

---
+++ super/give_woman_above_man/seem_week/good_number/long_hand/be_long_hand/try_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    see_same_place(str_arg)
+    print('company')
+
+def see_same_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('give_world_in_little_time')
---
+++ super/great_number_or_next_company/great_life/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_place_from_problem(str_arg):
+    call_fact(str_arg)
+    print('different_man_or_government')
+
+def call_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_place_from_problem('take_time_for_week')
