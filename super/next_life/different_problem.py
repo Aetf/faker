42b56b2f478ed180271376a24daf01ff219ca20c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 12:20:33 -0500

    "rm: come super/person/life.py and super/next_life/different_problem.py by first point"

---
+++ super/next_life/different_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    week(str_arg)
+    print('thing')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('life_and_public_problem')
---
+++ super/person/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    few_child_or_good_person(str_arg)
+    print('next_problem_or_point')
+
+def few_child_or_good_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('few_way')
