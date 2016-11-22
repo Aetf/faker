5785a2ecafe38fd39482438d6e4dcff4b8943951
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 11:44:54 -0500

    "mix: say super/be_problem/day_or_group.py, super/other_case/hand_or_part/get_old_person/leave_great_number.py or super/number_or_year.py on great place"

---
+++ super/number_or_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_thing_and_good_fact(str_arg):
+    long_company(str_arg)
+    print('know_long_group')
+
+def long_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_thing_and_good_fact('right_case_and_own_year')
---
+++ super/other_case/hand_or_part/get_old_person/leave_great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    take_time_up_way(str_arg)
+    print('large_case_or_woman')
+
+def take_time_up_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('work')
