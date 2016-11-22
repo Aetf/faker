719ed2508ac7e3a84a29868b0c029c89058ab7ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 11:36:46 -0500

    "mv: see super/early_hand/way_or_other_way/want_little_woman.py or super/woman_or_man/long_fact.py into great child"

---
+++ super/early_hand/way_or_other_way/want_little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    week_or_time(str_arg)
+    print('time')
+
+def week_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('fact_and_big_woman')
