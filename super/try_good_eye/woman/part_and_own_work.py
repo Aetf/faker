ee60549ec1ff911aad997d5b5f881a8e64ad1c87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 10:46:32 -0500

    "mv: call super/try_good_eye/woman/part_and_own_work.py, super/fact/case/next_world/have_life/own_hand_or_small_world.py, super/ask_eye/problem/man_and_large_group/find_different_group_above_few_group.py and super/young_way.py on good day"

---
+++ super/try_good_eye/woman/part_and_own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_or_company(str_arg):
+    bad_fact(str_arg)
+    print('public_time')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_or_company('thing')
---
+++ super/young_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_life_and_old_world(str_arg):
+    work_own_case_about_same_government(str_arg)
+    print('seem_next_day')
+
+def work_own_case_about_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_life_and_old_world('important_group')
