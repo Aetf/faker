2bace46334fcf8232f9b38fefec39aefd9fc0c0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 13:12:29 -0500

    "add: work super/next_thing.py and super/able_fact/leave_able_world.py from little number"

---
+++ super/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_time(str_arg):
+    eye_or_important_place(str_arg)
+    print('high_number')
+
+def eye_or_important_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_time('week')
