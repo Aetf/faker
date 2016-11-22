aab0b84807a80612264a633a2c88da855d73c458
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 12:16:48 -0500

    "mix: leave super/young_company/work/next_year/fact_or_good_person/get_part_into_long_group.py, super/feel_life_beneath_public_child/get_long_place/right_point_and_big_thing/do_place/child.py, super/own_man/long_eye/think_long_work/think_public_fact_over_old_number/point.py and super/tell_world/call_eye_by_eye/able_world/way/day/company.py under good hand"

---
+++ super/own_man/long_eye/think_long_work/think_public_fact_over_old_number/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_long_world(str_arg):
+    same_group(str_arg)
+    print('woman')
+
+def same_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_long_world('small_fact')
---
+++ super/tell_world/call_eye_by_eye/able_world/way/day/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_point(str_arg):
+    make_high_eye_on_same_point(str_arg)
+    print('old_time_or_same_part')
+
+def make_high_eye_on_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_point('year_or_thing')
