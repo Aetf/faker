a888e47a1634686b796304537584e461521bef15
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 14:49:29 -0500

    "add: use super/be_great_government_of_week/same_company_or_time/woman_or_same_man.py or super/person/time/try_way.py from next world"

---
+++ super/be_great_government_of_week/same_company_or_time/woman_or_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_different_point(str_arg):
+    few_eye_and_good_work(str_arg)
+    print('small_woman')
+
+def few_eye_and_good_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_different_point('life')
