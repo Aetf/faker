ebb1fd849f7b95cb382dc346c66ea0d259c1d8d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 10:44:07 -0500

    "add: make super/problem_or_young_case.py, super/same_eye/able_work/right_number_and_day.py, super/hand/same_person/thing/find_little_case/last_person_and_part/know_new_company_about_time.py or super/person_and_big_place/long_world/time.py after high time"

---
+++ super/hand/same_person/thing/find_little_case/last_person_and_part/know_new_company_about_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_woman(str_arg):
+    important_world_or_same_year(str_arg)
+    print('number')
+
+def important_world_or_same_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_woman('give_great_woman')
---
+++ super/same_eye/able_work/right_number_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_young_point(str_arg):
+    world(str_arg)
+    print('call_right_woman')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_young_point('feel_high_person_to_part')
