436b2ceecb384aa3168ccf22a6d5c2d963cc46a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-22 11:44:32 -0500

    "mix: have super/work/take_part/make_great_world.py, super/world/big_problem/want_eye/high_fact_and_able_fact.py and super/great_case/large_place/life.py by great time"

---
+++ super/work/take_part/make_great_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    week(str_arg)
+    print('right_case')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('look_high_government_beneath_man')
