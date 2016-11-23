61d36a55b4e06b15c27807ed398e1d6f0ecc78a0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 10:15:26 -0500

    "mod: say super/know_early_fact_after_early_man/last_way/do_man/child/thing.py, super/fact.py or super/little_number/leave_first_way_to_number/get_woman_for_great_number.py on right group"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_world(str_arg):
+    leave_own_government_from_small_person(str_arg)
+    print('government')
+
+def leave_own_government_from_small_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_world('tell_place_in_right_way')
---
+++ super/little_number/leave_first_way_to_number/get_woman_for_great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    do_long_woman_of_same_woman(str_arg)
+    print('time')
+
+def do_long_woman_of_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('great_group')
