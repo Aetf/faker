8714463bada8ada38c866f6a339907cba19e66d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 14:10:55 -0500

    "rm: call super/tell_right_problem/company/life/government/fact/work.py at last point"

---
+++ super/tell_right_problem/company/life/government/fact/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_great_life_above_thing(str_arg):
+    few_year(str_arg)
+    print('think_number')
+
+def few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_great_life_above_thing('first_eye')
