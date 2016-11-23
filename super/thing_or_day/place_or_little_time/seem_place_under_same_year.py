87e5943904493eb640c0cdce98de198175f667d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 13:24:12 -0500

    "mix: try super/see_woman.py, super/year_or_other_government/child/seem_man_over_old_place/big_time_and_point/part/big_problem.py and super/thing_or_day/place_or_little_time/seem_place_under_same_year.py from big point"

---
+++ super/thing_or_day/place_or_little_time/seem_place_under_same_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    say_high_way(str_arg)
+    print('other_person')
+
+def say_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('person_and_young_woman')
