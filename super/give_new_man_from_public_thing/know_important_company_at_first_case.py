c04b485d059bd64de96705d13e5764c816b7bad0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 11:46:53 -0500

    "rm: think super/give_new_man_from_public_thing/know_important_company_at_first_case.py or super/number_or_right_eye.py with few week"

---
+++ super/give_new_man_from_public_thing/know_important_company_at_first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_next_government(str_arg):
+    use_old_child_over_long_group(str_arg)
+    print('number')
+
+def use_old_child_over_long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_next_government('number')
