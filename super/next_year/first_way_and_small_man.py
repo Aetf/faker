02575fa080187e4a71f4390b3c45e8c9fda8b63d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 12:44:34 -0500

    "mv: see super/take_life/world_or_company/make_great_woman.py, super/next_year/first_way_and_small_man.py or super/work_case_over_fact/eye_or_good_person.py after great part"

---
+++ super/next_year/first_way_and_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_and_group(str_arg)
+    print('point_or_next_world')
+
+def way_and_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('government')
---
+++ super/take_life/world_or_company/make_great_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_year(str_arg):
+    day(str_arg)
+    print('case')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_year('world')
