8c3980385db064dedb13a421c0e1801b41a58893
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 10:41:29 -0500

    "add: call super/thing.py and super/company/government_and_time/other_man/go_great_point/person_and_hand/time_and_early_eye.py to same case"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_week(str_arg):
+    high_thing_or_same_problem(str_arg)
+    print('case_and_time')
+
+def high_thing_or_same_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_week('other_time_or_thing')
