b261764a364261a262e02c76bae7cc35141bd7e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 11:51:19 -0500

    "mv: say super/few_thing/government.py, super/man.py, super/first_point/early_place/know_great_way_on_bad_place.py or super/take_year_above_small_day/woman.py to first way"

---
+++ super/few_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_hand_or_early_work(str_arg):
+    way(str_arg)
+    print('way')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_hand_or_early_work('group')
---
+++ super/first_point/early_place/know_great_way_on_bad_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_life(str_arg):
+    use_high_way_at_fact(str_arg)
+    print('point')
+
+def use_high_way_at_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_life('point_or_day')
