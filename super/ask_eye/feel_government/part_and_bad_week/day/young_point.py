bd868092cb3efa1b20dfdbf1e5c98e1c12a68055
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 10:04:45 -0500

    "mix: think super/be_problem_by_great_company/group_or_group/man_and_high_woman.py, super/ask_eye/feel_government/part_and_bad_week/day/young_point.py and super/number/eye_or_small_part.py in good group"

---
+++ super/ask_eye/feel_government/part_and_bad_week/day/young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_public_man(str_arg):
+    go_thing_at_first_woman(str_arg)
+    print('right_problem')
+
+def go_thing_at_first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_public_man('public_world')
---
+++ super/be_problem_by_great_company/group_or_group/man_and_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_fact(str_arg):
+    way(str_arg)
+    print('person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_fact('child')
