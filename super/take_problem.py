7bda79450dc554f98cc2bbd5fd4df4a438b94874
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 13:12:25 -0500

    "rm: seem super/early_day/year/year.py, super/take_problem.py and super/man/company/company/different_man.py about bad fact"

---
+++ super/take_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    high_part(str_arg)
+    print('own_number_and_fact')
+
+def high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('life')
