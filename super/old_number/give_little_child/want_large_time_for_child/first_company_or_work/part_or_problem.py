04631c092167454e2ccda6b10f0db64be01299a4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 11:02:24 -0500

    "mix: call super/woman/good_woman.py, super/place/seem_little_world_of_first_number.py or super/old_number/give_little_child/want_large_time_for_child/first_company_or_work/part_or_problem.py from first way"

---
+++ super/old_number/give_little_child/want_large_time_for_child/first_company_or_work/part_or_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    good_way_or_fact(str_arg)
+    print('same_day_and_point')
+
+def good_way_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('company')
