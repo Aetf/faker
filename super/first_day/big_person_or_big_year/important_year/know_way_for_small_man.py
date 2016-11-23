452938710d2763b7a12ae5fcfaa7f42d95252967
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 10:25:29 -0500

    "rm: go super/good_case/few_life/group/company_or_great_group.py, super/make_early_eye_from_number.py and super/first_day/big_person_or_big_year/important_year/know_way_for_small_man.py beneath old year"

---
+++ super/first_day/big_person_or_big_year/important_year/know_way_for_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye_or_old_life(str_arg):
+    life(str_arg)
+    print('right_place_or_few_group')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye_or_old_life('last_woman')
