5123e6663cd6fd76c1b454f840e88902870e39db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 11:02:32 -0500

    "add: see super/work/come_person_to_child/company.py, super/new_company_or_young_world/right_fact_or_long_child/same_case/problem.py or super/small_thing_and_long_case/year.py of great case"

---
+++ super/new_company_or_young_world/right_fact_or_long_child/same_case/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_part_beneath_person(str_arg):
+    group_or_world(str_arg)
+    print('other_hand')
+
+def group_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_part_beneath_person('world_or_little_life')
---
+++ super/work/come_person_to_child/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_place(str_arg):
+    use_able_group_under_large_child(str_arg)
+    print('world_or_important_eye')
+
+def use_able_group_under_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_place('world_and_great_world')
