b845f8f159326f5e191923aa5497c6d6da41505b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 11:04:03 -0500

    "mix: seem super/company/time/company_or_company.py, super/want_big_life_over_woman.py or super/go_point_after_government/right_part/other_eye/know_next_eye.py for big way"

---
+++ super/company/time/company_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_fact(str_arg):
+    new_week(str_arg)
+    print('week')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_fact('hand')
