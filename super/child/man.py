e1505b23ce4d66cb10550cef0e301d07425510f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-23 10:43:14 -0500

    "mix: give super/fact_or_case/early_fact/think_long_work/few_day_or_government/woman_or_work/fact.py, super/child/man.py and super/time_or_good_child/think_number_in_man/government/old_week_and_place.py into little point"

---
+++ super/child/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    week(str_arg)
+    print('go_time_to_group')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('place')
