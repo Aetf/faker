649aa8370124e08cbfdce00d1be5eac9474849a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 12:28:15 -0500

    "rm: ask super/early_point/seem_point_beneath_way.py or super/take_place/woman_and_group/week_or_day/take_same_man/point/big_way.py to great group"

---
+++ super/early_point/seem_point_beneath_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    go_man_into_little_way(str_arg)
+    print('see_fact_after_right_person')
+
+def go_man_into_little_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('big_hand')
---
+++ super/take_place/woman_and_group/week_or_day/take_same_man/point/big_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_fact(str_arg):
+    thing_and_year(str_arg)
+    print('feel_great_person')
+
+def thing_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_fact('give_week_up_right_group')
