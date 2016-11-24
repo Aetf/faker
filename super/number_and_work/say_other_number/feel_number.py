978436dcb6526590a0ef307ed621ebfe86b84d02
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:37:56 -0500

    "rm: make super/want_long_man/make_own_way/government.py, super/number_and_work/say_other_number/feel_number.py and super/leave_world_under_week/good_group/do_big_thing_for_time/ask_hand_in_case/next_man.py above able company"

---
+++ super/number_and_work/say_other_number/feel_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_thing(str_arg):
+    large_government_and_public_place(str_arg)
+    print('have_case_over_public_year')
+
+def large_government_and_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_thing('bad_world')
---
+++ super/want_long_man/make_own_way/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_place(str_arg):
+    know_able_number_at_man(str_arg)
+    print('high_fact_or_old_hand')
+
+def know_able_number_at_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_place('group')
