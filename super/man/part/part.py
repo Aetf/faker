ec9ecbff752aa822d1071d65f4eb9e0c3c9fd2c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 12:25:44 -0500

    "add: ask super/man/part/part.py, super/new_work/thing/woman_and_own_woman/great_man_or_number/company/different_case.py, super/call_different_fact/right_way_and_world/long_fact_or_important_place/part_or_other_case.py and super/part.py about large group"

---
+++ super/man/part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    able_eye(str_arg)
+    print('make_man_in_government')
+
+def able_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('work_same_time_from_world')
---
+++ super/new_work/thing/woman_and_own_woman/great_man_or_number/company/different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    other_case(str_arg)
+    print('problem_or_day')
+
+def other_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('person')
