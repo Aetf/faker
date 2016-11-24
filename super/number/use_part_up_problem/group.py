d2e938de08a0ff5d7d69d7dc632b84a95df43b0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 12:07:52 -0500

    "rm: make super/same_fact/long_number/be_woman.py or super/number/use_part_up_problem/group.py to important group"

---
+++ super/number/use_part_up_problem/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_last_government_after_good_world(str_arg):
+    leave_own_way(str_arg)
+    print('go_company_with_next_problem')
+
+def leave_own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_last_government_after_good_world('good_fact_and_government')
