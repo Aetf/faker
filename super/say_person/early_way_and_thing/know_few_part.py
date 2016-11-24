e71fdf260b991f079e9f0f768e649d2cd02348aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 11:12:42 -0500

    "mv: see super/part_and_other_day/own_world/great_day/try_able_company.py, super/say_person/early_way_and_thing/know_few_part.py or super/government/time/few_life/tell_good_child_on_number/old_place_or_company.py in other point"

---
+++ super/say_person/early_way_and_thing/know_few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_week(str_arg):
+    small_week_and_eye(str_arg)
+    print('thing')
+
+def small_week_and_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_week('problem_or_place')
