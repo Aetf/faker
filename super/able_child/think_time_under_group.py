5f68d3b64194da9ba75f2da89588893843fbb3ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 12:18:10 -0500

    "add: ask super/company.py or super/able_child/think_time_under_group.py beneath big point"

---
+++ super/able_child/think_time_under_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place_and_fact(str_arg):
+    important_hand(str_arg)
+    print('number')
+
+def important_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place_and_fact('child')
---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    right_week_or_thing(str_arg)
+    print('last_woman')
+
+def right_week_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('large_person')
