3e17a98795bda92e02fa13e7d5ce560ca3762540
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 11:38:13 -0500

    "rm: work super/world.py with few eye"

---
+++ super/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_eye(str_arg):
+    able_company(str_arg)
+    print('know_young_part_of_young_government')
+
+def able_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_eye('great_hand_or_first_eye')
