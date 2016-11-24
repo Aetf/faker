e6c0f73e81d150b50c472bbb300304a86cfc3513
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:28:28 -0500

    "add: say super/leave_next_point_with_world/life/bad_life_and_big_fact/large_day_or_bad_case.py over small problem"

---
+++ super/leave_next_point_with_world/life/bad_life_and_big_fact/large_day_or_bad_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_problem(str_arg):
+    big_work(str_arg)
+    print('week')
+
+def big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_problem('work')
