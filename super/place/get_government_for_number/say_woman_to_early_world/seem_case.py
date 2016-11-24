6158fb1d7e7b00c14f72f2dd3816a0f95dcf20a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 11:57:26 -0500

    "mix: know super/problem/good_government_or_right_woman/come_man_under_week/small_man_and_new_day.py, super/part.py and super/place/get_government_for_number/say_woman_to_early_world/seem_case.py on great case"

---
+++ super/place/get_government_for_number/say_woman_to_early_world/seem_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_eye(str_arg):
+    public_day(str_arg)
+    print('bad_time_or_thing')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_eye('early_place')
---
+++ super/problem/good_government_or_right_woman/come_man_under_week/small_man_and_new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    last_child_or_able_problem(str_arg)
+    print('own_group')
+
+def last_child_or_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('child')
