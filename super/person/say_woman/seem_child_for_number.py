56d0b63cd1ac3c739ff08f4d719304dae9e55a0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:04:14 -0500

    "mix: look super/public_fact/world/use_work_with_bad_work/problem.py, super/person/say_woman/seem_child_for_number.py or super/year.py into new week"

---
+++ super/person/say_woman/seem_child_for_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_next_time(str_arg):
+    own_world(str_arg)
+    print('problem')
+
+def own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_next_time('life')
