bc7c177da162e5be293c2390438bedf15f5f7912
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:25:40 -0500

    "mv: go super/small_company/old_case_and_work.py, super/right_man_or_person.py and super/find_other_group/next_life/group_or_young_day/place_or_few_number.py in new company"

---
+++ super/right_man_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    get_case(str_arg)
+    print('child_or_early_place')
+
+def get_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('right_number')
---
+++ super/small_company/old_case_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_child(str_arg):
+    small_hand_or_good_place(str_arg)
+    print('part')
+
+def small_hand_or_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_child('same_case')
