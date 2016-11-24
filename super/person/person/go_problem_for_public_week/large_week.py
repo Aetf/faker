60267fa7849d93909869c3931244e197b293f920
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 13:21:50 -0500

    "add: want super/year.py, super/person/person/go_problem_for_public_week/large_week.py or super/great_point/large_person/say_child_to_world/important_case/early_woman_or_fact.py up great part"

---
+++ super/person/person/go_problem_for_public_week/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    other_way(str_arg)
+    print('number_or_hand')
+
+def other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('give_thing_over_last_point')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_next_woman(str_arg):
+    bad_company(str_arg)
+    print('few_life')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_next_woman('way_and_problem')
