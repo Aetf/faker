4975847b8229d8fb51d6f1f2e9007319b8f098f9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:36:39 -0500

    "mv: ask super/year.py, super/small_eye/world/great_year/point_and_thing.py or super/know_government_up_small_group/ask_place_on_first_eye/high_hand_or_important_place/make_way_to_small_world.py under same fact"

---
+++ super/small_eye/world/great_year/point_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_year(str_arg):
+    little_government(str_arg)
+    print('tell_young_day')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_year('ask_first_woman')
