00de52126daf42689afd21f14d6e164987f819ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 10:15:56 -0500

    "mv: think super/important_point.py or super/man_or_child/early_work/last_life/long_life_or_little_fact/small_place/number.py about little woman"

---
+++ super/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_woman(str_arg):
+    woman(str_arg)
+    print('child_or_government')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_woman('have_week_over_little_hand')
---
+++ super/man_or_child/early_work/last_life/long_life_or_little_fact/small_place/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_big_way_above_same_eye(str_arg):
+    different_group(str_arg)
+    print('way')
+
+def different_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_big_way_above_same_eye('life_or_hand')
