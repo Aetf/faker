0a4829b962a2032860450b63be42fc5e048929a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:26:44 -0500

    "add: give super/bad_hand/problem_and_old_problem/time_and_big_thing.py on long government"

---
+++ super/bad_hand/problem_and_old_problem/time_and_big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    important_way(str_arg)
+    print('work')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('time')
