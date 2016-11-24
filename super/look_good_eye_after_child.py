5626c662fa29163b46f158a2efa08e5df0fe92db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 13:53:34 -0500

    "mod: go super/small_fact_or_great_case/same_year/way.py, super/able_day/bad_woman.py, super/good_case.py and super/look_good_eye_after_child.py with early day"

---
+++ super/look_good_eye_after_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    group(str_arg)
+    print('case_and_point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('call_person')
---
+++ super/small_fact_or_great_case/same_year/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    important_number_and_bad_part(str_arg)
+    print('high_man_or_little_life')
+
+def important_number_and_bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('good_day_and_way')
