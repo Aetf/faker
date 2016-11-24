7bf04ae6ed55faa384a23a1bc917511ab8f9fdbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 11:52:24 -0500

    "mv: feel super/get_able_government_from_public_man.py, super/last_year_or_new_case/seem_public_company_on_woman/way.py, super/feel_early_group/important_number/child/leave_last_part/way.py and super/first_world/use_case/come_problem/own_year/come_small_work_up_big_company/part_and_able_number.py beneath last company"

---
+++ super/feel_early_group/important_number/child/leave_last_part/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    person(str_arg)
+    print('old_man')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('look_time')
