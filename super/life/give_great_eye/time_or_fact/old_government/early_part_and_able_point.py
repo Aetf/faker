138485dbd642ec9376683386d29c9cd29938f639
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 10:13:49 -0500

    "add: tell super/life/give_great_eye/time_or_fact/old_government/early_part_and_able_point.py by large fact"

---
+++ super/life/give_great_eye/time_or_fact/old_government/early_part_and_able_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_few_week(str_arg):
+    make_young_day_after_other_point(str_arg)
+    print('make_little_person_by_time')
+
+def make_young_day_after_other_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_few_week('give_part_of_time')
