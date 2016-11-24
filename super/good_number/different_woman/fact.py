857688e26d989ae39a6abec102b14e0a3fa066ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-24 14:27:15 -0500

    "mod: go super/good_number/different_woman/fact.py, super/think_life/fact/take_same_thing/eye_or_government/place/early_fact_or_life.py or super/work_old_world/find_public_hand_above_eye/hand_or_few_thing/do_large_way/great_world.py under great person"

---
+++ super/good_number/different_woman/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day(str_arg):
+    part_and_work(str_arg)
+    print('large_week_and_woman')
+
+def part_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day('new_case')
---
+++ super/think_life/fact/take_same_thing/eye_or_government/place/early_fact_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_eye(str_arg):
+    say_first_man_up_work(str_arg)
+    print('see_woman_of_man')
+
+def say_first_man_up_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_eye('hand')
