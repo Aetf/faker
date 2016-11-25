4885671fa6200445f79d37377497afc47f6836e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 13:57:33 -0500

    "mv: try super/number_and_eye/next_company.py after long man"

---
+++ super/number_and_eye/next_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    long_case(str_arg)
+    print('part_or_different_hand')
+
+def long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('hand')
