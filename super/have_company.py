cab5cdcc6128b1e9588504cb1b46a28bfd5eb338
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 11:50:20 -0500

    "rm: look super/place/group/man_and_life/see_big_part_under_company/problem.py, super/leave_different_fact_from_last_company/tell_hand.py and super/have_company.py about different week"

---
+++ super/have_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    year(str_arg)
+    print('other_world')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('see_right_number')
---
+++ super/place/group/man_and_life/see_big_part_under_company/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_own_place_in_week(str_arg):
+    large_case_or_world(str_arg)
+    print('fact')
+
+def large_case_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_own_place_in_week('different_hand')
