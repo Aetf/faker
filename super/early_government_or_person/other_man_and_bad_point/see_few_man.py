5e601a21235b6f34a89b75f38bc2abca535a7d25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 11:01:21 -0500

    "mod: go super/early_government_or_person/other_man_and_bad_point/see_few_man.py, super/same_man_and_way/important_year_and_few_group/right_fact/tell_other_government_at_day/thing_or_early_company.py and super/child_or_small_case/first_group.py in high hand"

---
+++ super/early_government_or_person/other_man_and_bad_point/see_few_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year_with_problem(str_arg):
+    different_case(str_arg)
+    print('look_next_man_of_new_case')
+
+def different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year_with_problem('public_life_and_problem')
