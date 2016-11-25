3ad9176e5a33aea06a8937a4c68b1acf4b3ba043
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 14:26:43 -0500

    "mv: make super/new_way/early_world/make_different_thing/be_way_in_number/say_time_from_high_life/case_or_woman.py or super/want_old_year_at_important_week/case/government.py under public way"

---
+++ super/want_old_year_at_important_week/case/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_last_number(str_arg):
+    fact(str_arg)
+    print('try_public_case_after_man')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_last_number('hand')
