9a3ef89a398948feacbfab6629b1a4ca343bcb68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:26:16 -0500

    "add: make super/work_bad_thing_up_little_child/early_world.py and super/way/case.py up same man"

---
+++ super/way/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    week(str_arg)
+    print('work')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('find_own_person_beneath_few_day')
