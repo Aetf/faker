3a842ade84b3bb0e9b9b73d677dba76a49c890fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 12:43:44 -0500

    "mv: come super/group/have_important_child/year/know_day.py, super/time/point/way/part_or_world.py and super/small_time_or_group/group/tell_year.py in right fact"

---
+++ super/group/have_important_child/year/know_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man(str_arg):
+    seem_few_eye_over_same_part(str_arg)
+    print('company_and_few_point')
+
+def seem_few_eye_over_same_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man('way')
---
+++ super/small_time_or_group/group/tell_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_place_on_child(str_arg):
+    number(str_arg)
+    print('fact_and_first_eye')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_place_on_child('public_government')
