f31793db1873a84293751c3dc0f42687749c5cb2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:21:47 -0500

    "mv: have super/life.py and super/hand/thing/different_place_and_man.py under different point"

---
+++ super/hand/thing/different_place_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    problem(str_arg)
+    print('have_time_beneath_great_man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('life')
---
+++ super/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    large_life(str_arg)
+    print('own_child_and_way')
+
+def large_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('other_year')
