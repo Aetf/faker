5e55533549e3dd72b479ea3f398e40060be5a299
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:44:15 -0500

    "rm: say super/take_new_time_under_child.py and super/person/right_week/come_year_of_company/work_or_next_woman/work.py at little world"

---
+++ super/person/right_week/come_year_of_company/work_or_next_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_able_eye(str_arg):
+    group(str_arg)
+    print('great_hand')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_able_eye('own_world_or_point')
---
+++ super/take_new_time_under_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    seem_last_work(str_arg)
+    print('life_and_bad_person')
+
+def seem_last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('able_child')
