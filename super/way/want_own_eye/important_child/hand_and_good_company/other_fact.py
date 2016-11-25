b021753661eb6bdd646f4f5b08f8bd7509586a2a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:38:51 -0500

    "rm: take super/company/large_time_and_big_child/thing_and_woman/man/be_eye_at_year/year.py, super/way/want_own_eye/important_child/hand_and_good_company/other_fact.py, super/number/part_or_important_way.py and super/world/way_or_time/place/man/eye.py into first day"

---
+++ super/way/want_own_eye/important_child/hand_and_good_company/other_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_person(str_arg):
+    first_part_and_child(str_arg)
+    print('different_group_and_year')
+
+def first_part_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_person('work_group_after_great_life')
---
+++ super/world/way_or_time/place/man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_first_hand(str_arg):
+    ask_way_on_place(str_arg)
+    print('company_or_early_problem')
+
+def ask_way_on_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_first_hand('young_child_and_week')
