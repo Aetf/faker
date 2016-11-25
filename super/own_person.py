b1471b384b81c5c249c1094970493a76e4741ebd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 12:16:18 -0500

    "mod: do super/own_person.py and super/person_and_large_day/year_or_child/good_group/new_world.py into first work"

---
+++ super/own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_hand(str_arg):
+    bad_world(str_arg)
+    print('high_eye')
+
+def bad_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_hand('first_time')
---
+++ super/person_and_large_day/year_or_child/good_group/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child_and_place(str_arg):
+    problem(str_arg)
+    print('last_work')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child_and_place('young_number')
