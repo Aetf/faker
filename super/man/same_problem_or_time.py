ebd236e7ae55f5db25e007ac53dc459893c0c567
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 13:55:40 -0500

    "mix: look super/group_or_point.py, super/big_time/right_life.py, super/man/same_problem_or_time.py or super/eye/look_young_government_by_case/ask_large_hand.py at right group"

---
+++ super/man/same_problem_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group_or_bad_life(str_arg):
+    fact_and_man(str_arg)
+    print('week')
+
+def fact_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group_or_bad_life('different_child')
