7387af45359e7d0960fa9827105c0c6f9e3c5d5a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:38:55 -0500

    "mix: see super/important_point/group_and_long_work.py beneath able way"

---
+++ super/important_point/group_and_long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_next_case(str_arg):
+    work(str_arg)
+    print('group')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_next_case('case_or_other_way')
