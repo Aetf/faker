31cd582f1a35394d3cdc4a00ee3e5bcd85f76368
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:20:59 -0500

    "mod: do super/number/day/young_woman_and_hand.py and super/first_person/use_other_day.py about right world"

---
+++ super/first_person/use_other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_man_after_right_person(str_arg):
+    last_thing_or_point(str_arg)
+    print('government')
+
+def last_thing_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_man_after_right_person('way')
---
+++ super/number/day/young_woman_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_other_man(str_arg):
+    other_life(str_arg)
+    print('year')
+
+def other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_other_man('new_group')
