3821eae12a417f6b5804adde7e4f3cdfa5998b80
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 12:52:32 -0500

    "rm: know super/problem.py about public way"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    long_fact(str_arg)
+    print('fact')
+
+def long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('long_group')
