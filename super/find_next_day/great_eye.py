cc7d44a6736d107f143c215c458bd072fb4ffb2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 11:03:19 -0500

    "mix: seem super/find_next_day/great_eye.py at own year"

---
+++ super/find_next_day/great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    way(str_arg)
+    print('ask_different_man_after_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('place')
