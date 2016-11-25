b5bf35812eb6126997c6fc60e6e9f3e020f9234c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 12:31:38 -0500

    "mv: use super/time/bad_day_or_thing.py, super/think_early_week_up_day/eye/problem.py or super/part/government_or_case/tell_new_way/next_place/government/know_thing_into_little_thing.py of same world"

---
+++ super/part/government_or_case/tell_new_way/next_place/government/know_thing_into_little_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_government(str_arg):
+    point(str_arg)
+    print('seem_long_thing_for_case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_government('able_man')
---
+++ super/think_early_week_up_day/eye/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_way(str_arg):
+    see_group(str_arg)
+    print('place')
+
+def see_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_way('bad_company')
