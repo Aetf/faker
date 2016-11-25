41a19b0400da3455350ea77761032cc24ef778d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 14:42:46 -0500

    "mix: have super/make_problem_in_company.py, super/day/place.py, super/problem/child_or_new_eye.py or super/right_case/come_new_man_by_long_man/problem/few_eye_or_bad_child/early_work/case.py on little case"

---
+++ super/problem/child_or_new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    thing_and_place(str_arg)
+    print('woman')
+
+def thing_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('know_big_thing_into_first_man')
---
+++ super/right_case/come_new_man_by_long_man/problem/few_eye_or_bad_child/early_work/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_company(str_arg):
+    few_day_and_work(str_arg)
+    print('high_number')
+
+def few_day_and_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_company('large_way')
