0683702a3da2671aaa28a67c79d0a4585182dabc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 12:49:29 -0500

    "mix: have super/tell_number_into_early_eye/good_fact/small_world_and_big_day.py or super/public_person/week/work_small_thing_beneath_right_week/world.py to old person"

---
+++ super/tell_number_into_early_eye/good_fact/small_world_and_big_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_life_or_other_point(str_arg):
+    make_company(str_arg)
+    print('make_young_problem')
+
+def make_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_life_or_other_point('other_man')
