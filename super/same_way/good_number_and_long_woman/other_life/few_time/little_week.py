091185938318e34f20df83141b31cde813001f4a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 10:59:37 -0500

    "rm: use super/woman/say_few_number_into_large_person/give_old_work_with_way/place/be_right_hand_of_person.py, super/same_way/good_number_and_long_woman/other_life/few_time/little_week.py, super/look_government/government/problem.py and super/little_eye/case.py to large thing"

---
+++ super/same_way/good_number_and_long_woman/other_life/few_time/little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number(str_arg):
+    seem_different_hand(str_arg)
+    print('woman_and_case')
+
+def seem_different_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number('world')
---
+++ super/woman/say_few_number_into_large_person/give_old_work_with_way/place/be_right_hand_of_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year(str_arg):
+    man(str_arg)
+    print('bad_hand')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year('old_company')
