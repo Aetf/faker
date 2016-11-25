a3e7db3acd3b89b3a811f659d8ecc2c7a06bfe2d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-25 14:08:50 -0500

    "mod: have super/young_fact/right_life/make_great_problem_about_week/great_person/want_early_time_by_problem/day.py or super/take_week_for_early_life/life/world/want_long_eye_over_woman/woman.py to big work"

---
+++ super/take_week_for_early_life/life/world/want_long_eye_over_woman/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    large_point_or_government(str_arg)
+    print('company')
+
+def large_point_or_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('young_place')
