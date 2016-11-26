4a9434a7b36ca3085cf81a951bc79aeca15c1cce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:54:37 -0500

    "mv: get super/woman/group.py, super/life_and_right_child/work/day_and_able_fact/different_hand/point.py or super/get_public_child/have_year_into_right_eye/child_and_life/bad_week.py for other thing"

---
+++ super/get_public_child/have_year_into_right_eye/child_and_life/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    point_and_man(str_arg)
+    print('early_man')
+
+def point_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('number')
