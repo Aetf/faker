0c5a333dff6bb4ff90f2fb96c3875eca5c4e2a4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:07:10 -0500

    "mv: take super/look_large_problem/new_place/early_world_and_problem/case/time.py in first company"

---
+++ super/look_large_problem/new_place/early_world_and_problem/case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_company(str_arg):
+    know_first_person_to_work(str_arg)
+    print('same_case')
+
+def know_first_person_to_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_company('good_man')
