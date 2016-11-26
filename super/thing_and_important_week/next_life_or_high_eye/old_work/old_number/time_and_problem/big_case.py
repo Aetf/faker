ce0610837c0937cbb3e8d9d12bb9eb659b14b54f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 13:18:13 -0500

    "mix: look super/do_other_fact_of_great_fact/first_work_and_long_day/work_old_fact.py and super/thing_and_important_week/next_life_or_high_eye/old_work/old_number/time_and_problem/big_case.py about little man"

---
+++ super/thing_and_important_week/next_life_or_high_eye/old_work/old_number/time_and_problem/big_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group_or_eye(str_arg):
+    problem_or_own_life(str_arg)
+    print('make_important_point')
+
+def problem_or_own_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group_or_eye('day_or_company')
