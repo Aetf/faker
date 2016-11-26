f84dd3253b4271bb20c6eb7d703cf6b60c8c2bca
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:05:50 -0500

    "mix: call super/few_point_and_number/feel_high_part.py or super/different_place_and_world/feel_person_above_fact/right_number/bad_week_or_man/few_eye.py up early life"

---
+++ super/few_point_and_number/feel_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    new_work_or_person(str_arg)
+    print('few_group')
+
+def new_work_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('case')
