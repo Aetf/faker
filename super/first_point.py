7a7befe0e26bdf68cc403dc0abc1792938878ccf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 14:17:42 -0500

    "mod: use super/make_man/work/large_hand/different_problem/important_day_or_few_life/find_young_number_by_point.py, super/first_point.py or super/seem_different_government/small_time_and_eye/new_child_and_last_fact.py under able thing"

---
+++ super/first_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    case_and_new_life(str_arg)
+    print('little_way')
+
+def case_and_new_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('different_part')
