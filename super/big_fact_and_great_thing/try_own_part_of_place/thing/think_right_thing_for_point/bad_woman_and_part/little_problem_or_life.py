86c3468e820ed32644c710535eeb0130b6b298f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:51:30 -0500

    "rm: look super/able_government/case.py or super/big_fact_and_great_thing/try_own_part_of_place/thing/think_right_thing_for_point/bad_woman_and_part/little_problem_or_life.py with old place"

---
+++ super/big_fact_and_great_thing/try_own_part_of_place/thing/think_right_thing_for_point/bad_woman_and_part/little_problem_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work(str_arg):
+    long_hand(str_arg)
+    print('able_problem')
+
+def long_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work('week')
