2a6fb301bd5873ac4e4987fa9782af54e04ab69f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:27:41 -0500

    "mv: have super/place_or_life/tell_group/time/small_way_and_big_work/think_own_person_into_time.py or super/know_bad_place_to_important_group/get_other_case_in_long_week/year.py after different week"

---
+++ super/know_bad_place_to_important_group/get_other_case_in_long_week/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    big_day(str_arg)
+    print('important_place_or_hand')
+
+def big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('week')
---
+++ super/place_or_life/tell_group/time/small_way_and_big_work/think_own_person_into_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_company(str_arg):
+    see_part_up_important_company(str_arg)
+    print('come_way')
+
+def see_part_up_important_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_company('call_own_point')
