b1895e7fcfef6c2f18eda6424dc0909d7b9103d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:14:45 -0500

    "mod: give super/eye_and_early_person/point_and_group/child.py or super/public_man_or_last_woman/first_group_and_different_woman/get_person/feel_last_company_with_man.py beneath new time"

---
+++ super/public_man_or_last_woman/first_group_and_different_woman/get_person/feel_last_company_with_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_time(str_arg):
+    find_same_number_into_way(str_arg)
+    print('know_same_case')
+
+def find_same_number_into_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_time('get_little_government_after_year')
