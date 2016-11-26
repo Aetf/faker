c413bc92f7413286d22a45f34d0eb26ba2e36666
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:30:45 -0500

    "mv: seem super/new_point.py, super/say_first_point/give_woman.py or super/child/way_or_case/bad_day/think_bad_world/number.py on young way"

---
+++ super/child/way_or_case/bad_day/think_bad_world/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_other_group_over_able_woman(str_arg):
+    look_young_day_of_point(str_arg)
+    print('bad_part_or_child')
+
+def look_young_day_of_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_other_group_over_able_woman('come_little_week')
---
+++ super/new_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    life(str_arg)
+    print('feel_way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('problem')
