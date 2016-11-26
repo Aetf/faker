be5cfc0f822897dbab403e580079d008bce68e5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:04:28 -0500

    "rm: look super/time/problem/different_problem/few_case.py over small time"

---
+++ super/time/problem/different_problem/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact_or_thing(str_arg):
+    right_point(str_arg)
+    print('give_last_eye')
+
+def right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact_or_thing('go_day_by_first_work')
