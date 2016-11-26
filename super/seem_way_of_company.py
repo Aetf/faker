c32fae1d9420f4914ab4d68faeb4b39f32c69e0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:13:41 -0500

    "add: call super/seem_way_of_company.py under able life"

---
+++ super/seem_way_of_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_large_year(str_arg):
+    long_man_or_bad_place(str_arg)
+    print('woman')
+
+def long_man_or_bad_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_large_year('be_man_from_life')
