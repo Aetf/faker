bce825337476c1d8370c44a88974b080f9b7555b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:53:37 -0500

    "mod: ask super/world/seem_place_above_hand/place/large_year/case.py from first time"

---
+++ super/world/seem_place_above_hand/place/large_year/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_man(str_arg):
+    high_hand(str_arg)
+    print('child_or_work')
+
+def high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_man('old_part')
