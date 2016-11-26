bef139ebd40fe58f8938800e0cf89672a376212c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 13:52:10 -0500

    "rm: go super/number/thing_or_problem/give_large_time_with_other_group/woman_and_week/see_number/right_group.py, super/place.py or super/think_high_time/important_year/big_work.py for other case"

---
+++ super/think_high_time/important_year/big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_fact(str_arg):
+    week_or_little_point(str_arg)
+    print('look_way_with_number')
+
+def week_or_little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_fact('good_eye')
