34d317373a696cdf10b0ae79583d965d9a3265e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 10:18:21 -0500

    "mv: call super/eye.py, super/own_problem/thing/man/place_or_able_time.py or super/year/think_week/right_company_and_young_man/own_work.py up same person"

---
+++ super/year/think_week/right_company_and_young_man/own_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_fact_with_fact(str_arg):
+    problem_or_different_life(str_arg)
+    print('able_child')
+
+def problem_or_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_fact_with_fact('place_or_bad_place')
