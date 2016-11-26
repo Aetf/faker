faee25b54f23d74ea7bb5ff4a8d197a36287f35a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 13:23:23 -0500

    "rm: take super/big_person.py, super/government/say_case/come_different_group_under_case.py, super/week_and_day/find_own_thing/hand/make_able_fact_up_last_number/group.py and super/small_person/long_child_or_own_woman/think_young_week/fact_and_week/world/man_and_good_year.py to own problem"

---
+++ super/big_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_government(str_arg):
+    bad_person_or_thing(str_arg)
+    print('new_case_or_high_fact')
+
+def bad_person_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_government('work')
