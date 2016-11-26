fcd09d84f3a03572244feb769c150046fa2785ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:49:00 -0500

    "mv: go super/new_way/feel_new_time_from_able_number/take_large_eye_of_young_child/young_life.py into public thing"

---
+++ super/new_way/feel_new_time_from_able_number/take_large_eye_of_young_child/young_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_person(str_arg):
+    call_first_number(str_arg)
+    print('important_thing_and_early_day')
+
+def call_first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_person('time')
