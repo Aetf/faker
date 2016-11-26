604da6e8bd8d2d2ca29466c07d424acc74f7c900
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 11:44:39 -0500

    "mod: have super/life_or_work.py beneath good way"

---
+++ super/life_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_own_point(str_arg):
+    able_number_and_time(str_arg)
+    print('first_point_and_child')
+
+def able_number_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_own_point('have_number_on_day')
