1d72aabba4fc3291ddebc2cee8742440f055a5ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-26 13:27:13 -0500

    "mod: think super/go_good_way_on_able_day/work_world_up_place/fact_or_long_week/work_or_few_work/eye.py, super/important_eye/large_day_and_world/big_day.py and super/think_long_fact_about_thing/life_and_group/try_group_into_thing.py into little thing"

---
+++ super/think_long_fact_about_thing/life_and_group/try_group_into_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_long_hand(str_arg):
+    feel_place_beneath_world(str_arg)
+    print('high_problem')
+
+def feel_place_beneath_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_long_hand('own_time')
