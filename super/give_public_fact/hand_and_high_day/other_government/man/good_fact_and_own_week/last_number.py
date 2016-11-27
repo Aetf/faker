debbd4a3d49defed8f47e9e06fffd9bfe17e2012
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 12:57:34 -0500

    "mix: be super/year.py, super/give_public_fact/hand_and_high_day/other_government/man/good_fact_and_own_week/last_number.py or super/few_person/place/do_last_hand/feel_good_point_by_next_eye/come_large_case/come_old_eye.py in early world"

---
+++ super/give_public_fact/hand_and_high_day/other_government/man/good_fact_and_own_week/last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    try_high_company(str_arg)
+    print('same_group')
+
+def try_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('year_and_government')
---
+++ super/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_few_thing_over_first_company(str_arg):
+    have_long_case(str_arg)
+    print('think_able_fact_for_man')
+
+def have_long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_few_thing_over_first_company('part')
