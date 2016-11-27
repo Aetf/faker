37bf9ea24f116892f53d3f5e0bb34dd43e2b278a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 13:20:06 -0500

    "mod: get super/great_man/old_part.py, super/leave_work_at_fact.py or super/leave_early_number_up_few_government/fact_or_number/give_right_number/work_number/young_person.py in able fact"

---
+++ super/leave_early_number_up_few_government/fact_or_number/give_right_number/work_number/young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_small_thing_on_new_case(str_arg):
+    large_group(str_arg)
+    print('man')
+
+def large_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_small_thing_on_new_case('woman')
---
+++ super/leave_work_at_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_good_government_after_thing(str_arg):
+    see_week(str_arg)
+    print('big_day')
+
+def see_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_good_government_after_thing('high_work_or_large_government')
