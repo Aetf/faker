2c6f3f54ad103d07681daf4d2aebbc0d7563b27d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 10:43:56 -0500

    "mv: give super/last_life/have_high_fact/large_group.py, super/feel_great_day/see_hand/work/other_problem_and_place/say_small_group/big_year.py, super/seem_large_man_with_group/young_government/tell_public_work_under_day/person.py or super/same_week/important_group/leave_able_way/way.py from own child"

---
+++ super/same_week/important_group/leave_able_way/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_group_or_company(str_arg):
+    know_way_up_own_person(str_arg)
+    print('problem_and_few_time')
+
+def know_way_up_own_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_group_or_company('world')
---
+++ super/seem_large_man_with_group/young_government/tell_public_work_under_day/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_time(str_arg):
+    same_place_and_person(str_arg)
+    print('able_week')
+
+def same_place_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_time('long_fact')
