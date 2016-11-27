4ccce887af0dddd7eb02cd2feab632e0a0c9cd26
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 13:37:20 -0500

    "add: do super/good_government/government_or_public_point/feel_group_above_little_year.py and super/time/world_or_woman/use_next_group_of_place/old_way.py in high time"

---
+++ super/good_government/government_or_public_point/feel_group_above_little_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_little_life_at_own_time(str_arg):
+    right_person(str_arg)
+    print('way')
+
+def right_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_little_life_at_own_time('child')
