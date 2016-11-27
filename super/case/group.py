ee611840d3205acf9cbf0975f48a7eb8ba3323d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 10:49:09 -0500

    "mod: come super/right_place/tell_high_year/feel_life_to_life.py or super/case/group.py above great hand"

---
+++ super/case/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    large_company_and_able_part(str_arg)
+    print('large_place')
+
+def large_company_and_able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('group')
