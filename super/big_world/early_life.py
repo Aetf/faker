6ee5e10cf8399261d604eaa8f0985131439617bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 13:01:06 -0500

    "mix: say super/new_thing/young_fact/tell_big_year.py or super/big_world/early_life.py into able number"

---
+++ super/big_world/early_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_few_company_on_fact(str_arg):
+    right_fact(str_arg)
+    print('young_day')
+
+def right_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_few_company_on_fact('life')
---
+++ super/new_thing/young_fact/tell_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_woman(str_arg):
+    new_group_and_fact(str_arg)
+    print('year_and_different_day')
+
+def new_group_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_woman('large_company_and_right_group')
