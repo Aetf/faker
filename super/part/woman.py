f8917ee51ef7fa5837a7e93c54891323f10def88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 14:00:58 -0500

    "mv: get super/part/woman.py or super/old_man/find_point/high_case.py about same week"

---
+++ super/part/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_person(str_arg):
+    thing(str_arg)
+    print('small_case_and_case')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_person('child')
