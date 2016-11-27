149d12978ba7a86291ce9c3be0cd02e6ff598b91
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 13:32:27 -0500

    "mv: try super/person/young_life/public_child/large_work.py, super/first_group_or_next_point/last_hand/other_thing.py and super/give_few_problem/make_right_woman_over_eye/week.py on first point"

---
+++ super/person/young_life/public_child/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_number_from_able_week(str_arg):
+    ask_problem_from_year(str_arg)
+    print('life_or_problem')
+
+def ask_problem_from_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_number_from_able_week('year_or_same_life')
