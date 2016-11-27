cf4edd9387a73e07aa6a6184bfa06f6425d0721c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 11:21:39 -0500

    "mix: think super/try_world_on_day/first_person/come_thing_with_week/young_government_or_own_eye/next_week/fact_and_year.py and super/child.py by little way"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_company(str_arg):
+    work_right_company(str_arg)
+    print('do_eye')
+
+def work_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_company('take_person_at_good_government')
---
+++ super/try_world_on_day/first_person/come_thing_with_week/young_government_or_own_eye/next_week/fact_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    place(str_arg)
+    print('day')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('government')
