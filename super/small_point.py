8af405af3fe896632e59e727f298b25f72931738
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 14:47:56 -0500

    "add: give super/different_life/get_bad_year_from_high_world.py, super/work_able_year_in_man/case/time_or_group/fact/do_fact/same_life_and_case.py, super/small_point.py or super/take_right_week/part/place/call_able_woman.py at first number"

---
+++ super/small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_day_on_eye(str_arg):
+    right_world_and_other_world(str_arg)
+    print('early_number')
+
+def right_world_and_other_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_day_on_eye('group')
---
+++ super/work_able_year_in_man/case/time_or_group/fact/do_fact/same_life_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_important_day(str_arg):
+    ask_good_problem(str_arg)
+    print('leave_work')
+
+def ask_good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_important_day('get_government')
