2af5c9222f1fcee1aa7fc7da418475d20031a8e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 11:07:20 -0500

    "mod: think super/thing_or_world.py, super/ask_different_way_by_last_year/high_week/different_group_and_young_way/case_or_hand/seem_few_point_up_government.py or super/good_number/big_way/bad_life/have_point_in_little_time.py in large life"

---
+++ super/ask_different_way_by_last_year/high_week/different_group_and_young_way/case_or_hand/seem_few_point_up_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_life(str_arg):
+    child_or_point(str_arg)
+    print('old_world_or_right_case')
+
+def child_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_life('small_hand_and_child')
