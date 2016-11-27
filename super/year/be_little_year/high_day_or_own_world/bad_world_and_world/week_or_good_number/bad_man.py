4d0676c2276d6bdde6b708d0edaabda9d8384874
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 10:30:07 -0500

    "mv: find super/year/be_little_year/high_day_or_own_world/bad_world_and_world/week_or_good_number/bad_man.py and super/big_life_and_old_man/know_number.py to good woman"

---
+++ super/year/be_little_year/high_day_or_own_world/bad_world_and_world/week_or_good_number/bad_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_public_company(str_arg):
+    government_or_company(str_arg)
+    print('young_year')
+
+def government_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_public_company('long_government')
