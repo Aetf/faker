0e0d166d74c7854ee99af104022372ac4cb87555
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 12:02:38 -0500

    "mix: come super/own_hand_or_hand.py and super/few_thing/look_life/child_or_fact/little_eye.py into good year"

---
+++ super/few_thing/look_life/child_or_fact/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    company(str_arg)
+    print('think_public_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('problem')
---
+++ super/own_hand_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week_and_person(str_arg):
+    take_long_thing(str_arg)
+    print('old_fact')
+
+def take_long_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week_and_person('case')
