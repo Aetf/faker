5a2a53c2a70be229b6bee3fccec512f69e722531
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-27 10:38:12 -0500

    "mv: seem super/right_number.py and super/time/young_case/early_number/come_early_year_for_life/get_child_of_public_number/government.py from great hand"

---
+++ super/time/young_case/early_number/come_early_year_for_life/get_child_of_public_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_bad_day(str_arg):
+    day(str_arg)
+    print('first_week')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_bad_day('fact_or_woman')
