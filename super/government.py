b8f840edf7d4a76acd2b1a5d59eade41319d5d08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 10:46:34 -0500

    "rm: try super/government.py, super/public_problem/go_point/try_place.py or super/life_or_point/young_point_or_work/same_government_and_group/same_fact_or_person/young_child/problem.py in able place"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_week(str_arg):
+    same_fact(str_arg)
+    print('case')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_week('bad_thing')
---
+++ super/life_or_point/young_point_or_work/same_government_and_group/same_fact_or_person/young_child/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_way(str_arg):
+    big_thing_or_day(str_arg)
+    print('life')
+
+def big_thing_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_way('tell_group')
