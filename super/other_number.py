612bd2865b923c07e779c09fc6b31d35a42d47b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 11:49:23 -0500

    "mix: want super/other_number.py of large thing"

---
+++ super/other_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_hand(str_arg):
+    thing(str_arg)
+    print('go_same_year_beneath_way')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_hand('young_group')
