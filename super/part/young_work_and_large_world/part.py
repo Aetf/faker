bd0c17130b21895a846a886791ec8e003076c41b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 11:02:11 -0500

    "rm: be super/small_child/see_point.py, super/world/case/day_or_young_group/thing/day_or_problem/look_person.py and super/part/young_work_and_large_world/part.py from next eye"

---
+++ super/part/young_work_and_large_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_bad_man(str_arg):
+    go_year_over_place(str_arg)
+    print('important_week_or_eye')
+
+def go_year_over_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_bad_man('want_same_way_in_case')
