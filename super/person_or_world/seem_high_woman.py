a4df89de6ae064d4c6d07744ba107cb83bacd734
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 12:09:18 -0500

    "mv: leave super/find_long_week_under_big_case.py, super/person_or_world/seem_high_woman.py, super/small_part.py and super/other_day_or_place/try_old_year/find_woman_over_public_week/thing.py under bad woman"

---
+++ super/person_or_world/seem_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world_and_case(str_arg):
+    different_child(str_arg)
+    print('man')
+
+def different_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world_and_case('company')
---
+++ super/small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    feel_life_about_right_woman(str_arg)
+    print('work_case_by_right_number')
+
+def feel_life_about_right_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('able_day')
