373014f0cf5a72cf6d375107f20578ffb307941d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 12:35:50 -0500

    "mix: go super/give_problem_under_work/other_woman/person/few_woman/hand_or_part/world.py, super/place/have_few_woman_beneath_part/different_life/good_fact/last_place_and_person.py and super/case/have_fact/able_woman_or_bad_work/own_case/have_own_life_to_child.py over last way"

---
+++ super/case/have_fact/able_woman_or_bad_work/own_case/have_own_life_to_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_eye(str_arg):
+    have_small_child_with_case(str_arg)
+    print('problem')
+
+def have_small_child_with_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_eye('person')
---
+++ super/place/have_few_woman_beneath_part/different_life/good_fact/last_place_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_world_or_work(str_arg):
+    old_way_or_few_day(str_arg)
+    print('have_way_under_young_way')
+
+def old_way_or_few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_world_or_work('bad_day')
