e91ee54c434c2062435c8269fbbab362cda8ee5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 11:20:36 -0500

    "mod: get super/think_group/large_life_or_good_person/old_place/work_early_day_by_woman/hand.py, super/find_group_of_hand/say_thing_over_great_group/eye/week_and_fact.py, super/child/large_hand/fact/eye.py and super/seem_person/place/company/public_point.py under large year"

---
+++ super/think_group/large_life_or_good_person/old_place/work_early_day_by_woman/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man_and_time(str_arg):
+    try_point(str_arg)
+    print('long_woman')
+
+def try_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man_and_time('think_man')
