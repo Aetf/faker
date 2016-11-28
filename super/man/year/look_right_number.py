53cc6b4d16ced419cf5fb2019019b19f5feb247c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 10:38:52 -0500

    "mix: get super/man/year/look_right_number.py, super/large_thing.py or super/next_week_and_small_number/have_next_government/right_world.py up last work"

---
+++ super/man/year/look_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    point(str_arg)
+    print('world')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('come_next_work')
---
+++ super/next_week_and_small_number/have_next_government/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_day_beneath_old_work(str_arg):
+    world_or_person(str_arg)
+    print('week')
+
+def world_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_day_beneath_old_work('life_and_public_point')
