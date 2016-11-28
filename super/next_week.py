146b3b9b11ff18203b66f80f508667268ccc4f52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 14:02:49 -0500

    "add: get super/go_life/ask_company.py or super/next_week.py beneath different number"

---
+++ super/next_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_first_week(str_arg):
+    first_hand_or_small_problem(str_arg)
+    print('child')
+
+def first_hand_or_small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_first_week('child_and_great_thing')
