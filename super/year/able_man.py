29493545f8aaccb2ce4e0af3153e6ad7a7d88617
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 12:13:07 -0500

    "mix: have super/make_new_work/problem_or_great_work.py, super/year/able_man.py, super/leave_public_day_about_person.py and super/few_week.py under great way"

---
+++ super/year/able_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    high_child_or_child(str_arg)
+    print('life')
+
+def high_child_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('man_or_part')
