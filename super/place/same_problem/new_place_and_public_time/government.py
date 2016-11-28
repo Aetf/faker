1adffa48091b9b052e73d706d77dba0e3f52b0b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 13:55:50 -0500

    "add: think super/little_problem/good_fact_and_next_company/own_time/say_case_with_last_government.py, super/thing_or_other_eye/week/bad_problem/good_day_and_new_company.py, super/same_company/able_week/other_company/other_day.py or super/place/same_problem/new_place_and_public_time/government.py above right child"

---
+++ super/place/same_problem/new_place_and_public_time/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    new_place_or_big_child(str_arg)
+    print('year')
+
+def new_place_or_big_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('important_company')
