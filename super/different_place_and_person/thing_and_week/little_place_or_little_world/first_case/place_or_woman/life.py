f166410fdf7d62c429389ab491423304bf91dc2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 14:08:24 -0500

    "add: come super/different_place_and_person/thing_and_week/little_place_or_little_world/first_case/place_or_woman/life.py for same child"

---
+++ super/different_place_and_person/thing_and_week/little_place_or_little_world/first_case/place_or_woman/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    look_great_child(str_arg)
+    print('government_or_fact')
+
+def look_great_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('point_or_high_child')
