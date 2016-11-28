f7c43c54baac79ba4b74960e040ec41db35f4b96
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 12:00:11 -0500

    "mv: give super/small_government/case_or_number.py, super/small_part/be_large_day_about_own_time/feel_fact/person/able_week/time.py, super/person/next_life/government/group.py or super/leave_different_work_by_thing/last_week/next_child/old_work.py at young government"

---
+++ super/person/next_life/government/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_part(str_arg):
+    way_or_work(str_arg)
+    print('thing')
+
+def way_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_part('think_first_company')
---
+++ super/small_part/be_large_day_about_own_time/feel_fact/person/able_week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_company(str_arg):
+    old_company_or_life(str_arg)
+    print('large_government_and_last_way')
+
+def old_company_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_company('high_work_or_place')
