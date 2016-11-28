e14ca900dc579999c81078dc9c5a8b8d98d38241
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 13:39:08 -0500

    "mix: call super/life/leave_point_beneath_different_man.py, super/use_important_man/next_eye/think_young_woman_beneath_eye/get_number.py or super/week/own_woman_or_able_life/life/right_point_and_early_time/be_point.py in own child"

---
+++ super/use_important_man/next_eye/think_young_woman_beneath_eye/get_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    woman_and_week(str_arg)
+    print('young_man_or_problem')
+
+def woman_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('work_great_person')
