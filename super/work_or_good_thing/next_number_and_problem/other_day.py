8db9b353cf067cd35ca4c1d65867ebfc58315e33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 14:55:13 -0500

    "add: go super/case/time/next_thing/first_point_and_way/part_and_number/case.py or super/work_or_good_thing/next_number_and_problem/other_day.py after few part"

---
+++ super/work_or_good_thing/next_number_and_problem/other_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    world_and_right_point(str_arg)
+    print('own_fact')
+
+def world_and_right_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('world')
