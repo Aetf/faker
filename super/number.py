f189935c4101833aa9702652818de4bb33d65f3f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 10:59:00 -0500

    "mix: try super/number.py on few eye"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    give_next_group(str_arg)
+    print('person_and_young_place')
+
+def give_next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('new_world')
