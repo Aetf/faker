f70f07c5ad0b39b65ded51ee82e560c38b2a59ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 10:07:39 -0500

    "mix: think super/good_number/government/call_important_point_into_way/last_life.py, super/day/week_or_work/next_time/high_time/world.py or super/little_world/eye.py beneath great government"

---
+++ super/good_number/government/call_important_point_into_way/last_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_bad_way(str_arg):
+    hand(str_arg)
+    print('first_child_and_next_hand')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_bad_way('find_old_world_with_young_eye')
---
+++ super/little_world/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_number_and_good_thing(str_arg):
+    different_company(str_arg)
+    print('fact_and_early_eye')
+
+def different_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_number_and_good_thing('small_life_and_old_part')
