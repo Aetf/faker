12be5cb229b75bad96b06d3b292972aef24d9230
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 14:00:36 -0500

    "mv: work super/small_eye/government.py, super/year_and_old_part/work/say_case/woman.py and super/bad_eye/find_place/young_week.py after young part"

---
+++ super/bad_eye/find_place/young_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_young_point(str_arg):
+    problem(str_arg)
+    print('early_work')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_young_point('hand')
---
+++ super/year_and_old_part/work/say_case/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_number_over_place(str_arg):
+    last_group(str_arg)
+    print('thing')
+
+def last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_number_over_place('last_way')
