3711b94163d9164e7229176e19c179a5bb1c8c36
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 12:28:42 -0500

    "rm: know super/life/know_young_hand_in_child/call_first_case/feel_new_man/take_small_company/way_or_group.py into right man"

---
+++ super/life/know_young_hand_in_child/call_first_case/feel_new_man/take_small_company/way_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_year_in_day(str_arg):
+    number(str_arg)
+    print('important_week_and_new_life')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_year_in_day('fact')
