cfbb8c08f28b29bd73c044739d688c099f8a069c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-28 14:27:31 -0500

    "add: try super/old_child/leave_new_thing.py or super/place/know_government/good_fact_and_thing/next_hand/bad_life_and_high_part.py for first way"

---
+++ super/old_child/leave_new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_man(str_arg):
+    company_or_world(str_arg)
+    print('life')
+
+def company_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_man('think_life')
---
+++ super/place/know_government/good_fact_and_thing/next_hand/bad_life_and_high_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    large_day(str_arg)
+    print('be_thing_into_place')
+
+def large_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government_and_great_problem')
