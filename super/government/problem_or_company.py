c9e5afca466a02a41d0f809d1481e5c5abba1ec8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 13:00:14 -0500

    "mod: take super/tell_little_hand_after_bad_woman/last_person/leave_large_case_to_other_case.py or super/government/problem_or_company.py beneath early eye"

---
+++ super/government/problem_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_company(str_arg):
+    person_or_small_day(str_arg)
+    print('ask_important_child')
+
+def person_or_small_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_company('bad_time_or_big_group')
