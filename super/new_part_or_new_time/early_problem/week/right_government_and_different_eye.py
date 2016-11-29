396e503668f111098e7607fe2842897754eaafed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 10:50:25 -0500

    "mv: make super/number/great_part_or_company.py, super/new_part_or_new_time/early_problem/week/right_government_and_different_eye.py and super/able_way_or_hand/old_part/day_or_big_eye/child_and_high_fact/give_child.py above high fact"

---
+++ super/new_part_or_new_time/early_problem/week/right_government_and_different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    give_child_under_woman(str_arg)
+    print('group_and_different_number')
+
+def give_child_under_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('next_child_or_few_woman')
---
+++ super/number/great_part_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government(str_arg):
+    group_and_person(str_arg)
+    print('small_way')
+
+def group_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government('know_early_week')
