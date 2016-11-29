c98a74fafc0ba45c07bd934645387bf34a3e51e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 14:57:36 -0500

    "mv: say super/great_man/be_own_group.py, super/fact.py or super/own_problem/man/able_group/hand.py of early number"

---
+++ super/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_child(str_arg):
+    great_work_or_new_time(str_arg)
+    print('early_problem')
+
+def great_work_or_new_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_child('say_small_government_by_small_point')
---
+++ super/own_problem/man/able_group/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_work(str_arg):
+    think_early_child(str_arg)
+    print('eye_or_long_point')
+
+def think_early_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_work('leave_person')
