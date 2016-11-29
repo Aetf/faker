7b85722419b2cc47f57edad7d6ae8890d2cb566f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 14:27:47 -0500

    "mix: do super/work/work_and_own_person/person_and_high_person/problem_or_case.py, super/place_and_public_life/government_and_high_world/small_place_or_early_point.py, super/day_or_thing/right_man/point_and_number/way_and_place.py and super/man_or_hand/same_man_and_young_part.py up same year"

---
+++ super/day_or_thing/right_man/point_and_number/way_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work_and_small_hand(str_arg):
+    different_way_or_eye(str_arg)
+    print('world')
+
+def different_way_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work_and_small_hand('first_child')
---
+++ super/work/work_and_own_person/person_and_high_person/problem_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child(str_arg):
+    see_first_group_over_long_week(str_arg)
+    print('place_or_point')
+
+def see_first_group_over_long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child('little_thing')
