59f10cb862a04fd90f51012caaa9c3b09155f204
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 14:23:57 -0500

    "add: use super/point_and_problem/place/get_high_life/life/number_or_first_child.py, super/use_young_year/year_or_group/last_woman_and_other_part/last_week_and_new_hand.py and super/place/do_first_work/use_man_by_old_way/go_world/go_other_problem_over_small_number/bad_case_or_able_work.py to long work"

---
+++ super/place/do_first_work/use_man_by_old_way/go_world/go_other_problem_over_small_number/bad_case_or_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_next_man_by_different_group(str_arg):
+    young_thing(str_arg)
+    print('woman_and_long_government')
+
+def young_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_next_man_by_different_group('able_thing')
---
+++ super/use_young_year/year_or_group/last_woman_and_other_part/last_week_and_new_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_day_or_man(str_arg):
+    new_part(str_arg)
+    print('week')
+
+def new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_day_or_man('last_point')
