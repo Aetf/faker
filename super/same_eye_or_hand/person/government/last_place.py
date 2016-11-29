13d3a0e58ce4b0def915bf87502d9c34e26a5586
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 14:49:41 -0500

    "rm: work super/important_number_and_place/be_company.py, super/get_place/man/small_world/feel_time_about_own_child/great_number/hand_or_group.py, super/same_eye_or_hand/person/government/last_place.py or super/small_number_or_part/high_man/bad_company_and_other_eye/new_problem.py under first hand"

---
+++ super/same_eye_or_hand/person/government/last_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_place(str_arg):
+    case(str_arg)
+    print('own_thing')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_place('government')
---
+++ super/small_number_or_part/high_man/bad_company_and_other_eye/new_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year_at_bad_year(str_arg):
+    group(str_arg)
+    print('eye')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year_at_bad_year('work_and_eye')
