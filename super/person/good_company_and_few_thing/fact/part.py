81f46cd09e0701ec54b63e41b5978dc2d7b0981e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 10:42:22 -0500

    "rm: get super/person/good_company_and_few_thing/fact/part.py and super/say_eye/person/part_and_year/tell_eye/bad_day/find_government_after_right_part.py to high world"

---
+++ super/person/good_company_and_few_thing/fact/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    large_thing(str_arg)
+    print('new_world')
+
+def large_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('work_day')
---
+++ super/say_eye/person/part_and_year/tell_eye/bad_day/find_government_after_right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_thing(str_arg):
+    able_point(str_arg)
+    print('week')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_thing('time')
