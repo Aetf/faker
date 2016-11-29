f3a91f10f10a031f167e0bf52e15f4079acf082b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 11:58:49 -0500

    "mod: say super/few_child/small_company/young_eye/want_child_beneath_time.py for early week"

---
+++ super/few_child/small_company/young_eye/want_child_beneath_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_group_at_child(str_arg):
+    year(str_arg)
+    print('same_man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_group_at_child('fact_or_same_week')
