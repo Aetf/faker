0d611ca6c2050c629301ee2e6eba02d83088f199
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 10:28:20 -0500

    "rm: think super/woman_and_day/great_number/good_time/new_case/government/say_last_person_with_place.py from old fact"

---
+++ super/woman_and_day/great_number/good_time/new_case/government/say_last_person_with_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    early_problem(str_arg)
+    print('child_or_young_life')
+
+def early_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('person')
