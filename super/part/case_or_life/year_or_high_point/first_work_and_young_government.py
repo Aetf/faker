8b2bc59cc93a9544949c52034072a3d53c9b32fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 10:01:47 -0500

    "rm: seem super/bad_woman/last_man/group/make_young_way_for_long_place/tell_small_woman.py, super/government/be_public_place/place/high_place_or_child/company/leave_part.py, super/think_good_eye_beneath_public_life/different_life/work_bad_hand_about_person/same_week_and_way/early_group.py or super/part/case_or_life/year_or_high_point/first_work_and_young_government.py after own number"

---
+++ super/part/case_or_life/year_or_high_point/first_work_and_young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    work_and_point(str_arg)
+    print('day_and_high_fact')
+
+def work_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('give_day')
---
+++ super/think_good_eye_beneath_public_life/different_life/work_bad_hand_about_person/same_week_and_way/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_last_person(str_arg):
+    young_part(str_arg)
+    print('thing_and_public_child')
+
+def young_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_last_person('last_year_or_own_thing')
