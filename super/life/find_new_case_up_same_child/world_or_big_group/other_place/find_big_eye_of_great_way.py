d572d7f53457ff07ecdab606d5b82dc7be5e5f8d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-29 12:41:27 -0500

    "mix: try super/life/find_new_case_up_same_child/world_or_big_group/other_place/find_big_eye_of_great_way.py beneath last year"

---
+++ super/life/find_new_case_up_same_child/world_or_big_group/other_place/find_big_eye_of_great_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    same_point(str_arg)
+    print('child_and_life')
+
+def same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('good_point')
