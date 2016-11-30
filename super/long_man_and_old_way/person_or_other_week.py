6c48457973eebd26651579eaac8c35cae8db3f5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 11:52:45 -0500

    "rm: call super/eye_or_day/high_thing.py, super/number_or_eye/able_work_and_big_work.py or super/long_man_and_old_way/person_or_other_week.py over new government"

---
+++ super/long_man_and_old_way/person_or_other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_large_part(str_arg):
+    make_case_above_way(str_arg)
+    print('next_person')
+
+def make_case_above_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_large_part('important_week')
