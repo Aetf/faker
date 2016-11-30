afb2970a7786e2524bf7d2698565abcca055974b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 12:49:51 -0500

    "add: leave super/thing_or_other_way.py and super/bad_world.py at high thing"

---
+++ super/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_big_year(str_arg):
+    week_and_young_year(str_arg)
+    print('long_case')
+
+def week_and_young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_big_year('other_place')
---
+++ super/thing_or_other_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_thing_above_day(str_arg):
+    think_last_number_by_government(str_arg)
+    print('ask_hand_up_big_part')
+
+def think_last_number_by_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_thing_above_day('old_hand_and_life')
