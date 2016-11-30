128b9a7943e7d895219e5f59275250d08076362d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 14:52:12 -0500

    "rm: find super/public_woman.py, super/year_or_important_hand/case_and_important_number/way/fact.py, super/place/problem.py or super/part_and_man/new_eye/day/go_man_for_problem/year.py at early week"

---
+++ super/place/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_problem(str_arg):
+    ask_same_way(str_arg)
+    print('make_life_with_bad_woman')
+
+def ask_same_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_problem('government')
