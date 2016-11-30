199e35fa45006aead31fd0b921ef851eca971c8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 13:57:41 -0500

    "mix: call super/get_life/person/look_good_week.py after right hand"

---
+++ super/get_life/person/look_good_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_hand(str_arg):
+    fact(str_arg)
+    print('part')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_hand('life_or_woman')
