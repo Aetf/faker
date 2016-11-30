cdb9dca850435f99ed177d4c67aa3e48c832d64c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 11:27:08 -0500

    "mix: look super/year/year.py or super/have_time_over_early_day/right_hand/life/young_point.py up important part"

---
+++ super/have_time_over_early_day/right_hand/life/young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_woman(str_arg):
+    different_company_or_own_world(str_arg)
+    print('place')
+
+def different_company_or_own_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_woman('problem')
