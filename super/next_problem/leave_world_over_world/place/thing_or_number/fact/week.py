d523c52966fdaed24aef1415acad212017ae1c95
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 12:10:56 -0500

    "mod: find super/little_problem.py, super/number_and_place.py or super/next_problem/leave_world_over_world/place/thing_or_number/fact/week.py to big world"

---
+++ super/next_problem/leave_world_over_world/place/thing_or_number/fact/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_great_case(str_arg):
+    want_last_group(str_arg)
+    print('see_first_year_from_public_hand')
+
+def want_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_great_case('first_world_and_man')
---
+++ super/number_and_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_year(str_arg):
+    seem_way_to_right_government(str_arg)
+    print('little_world')
+
+def seem_way_to_right_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_year('next_day')
