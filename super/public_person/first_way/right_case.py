c026c537fc2d9c4314818756249e6c43b28002d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 12:12:07 -0500

    "mv: feel super/public_person/first_way/right_case.py and super/point/small_thing_or_able_woman/think_large_week_about_little_problem/high_week_or_hand/give_world_at_thing/new_place.py under great life"

---
+++ super/public_person/first_way/right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year(str_arg):
+    call_early_week_at_place(str_arg)
+    print('old_group')
+
+def call_early_week_at_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year('fact')
