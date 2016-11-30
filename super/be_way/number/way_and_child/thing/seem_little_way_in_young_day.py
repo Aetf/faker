291d576eb759784248dfced87d22eab97dbb10c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 14:38:32 -0500

    "mix: get super/be_way/number/way_and_child/thing/seem_little_way_in_young_day.py, super/high_year/other_day.py, super/new_hand/long_company_and_important_world/want_long_government_beneath_bad_work/person_or_bad_point.py or super/eye_and_woman/eye/next_hand/right_part/important_government/first_number_or_way.py by good week"

---
+++ super/be_way/number/way_and_child/thing/seem_little_way_in_young_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    child_and_life(str_arg)
+    print('leave_point')
+
+def child_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('case')
