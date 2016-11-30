c249fcc5841f37e31d2a3c7ddf8e60ac703981a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 13:09:29 -0500

    "mix: do super/old_way/different_time.py, super/able_part/problem/go_young_world/number/young_group.py, super/come_important_year/old_year/life_or_last_life.py and super/hand_and_little_woman/want_time_to_hand/see_place_from_life/important_company.py with long company"

---
+++ super/able_part/problem/go_young_world/number/young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_great_government(str_arg):
+    part(str_arg)
+    print('seem_few_time_in_child')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_great_government('important_group')
