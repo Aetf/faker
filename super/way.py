0e9d474c8dc608b2ae62b6f73c88b44f39cdc8de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 11:15:01 -0500

    "mv: have super/way.py under early number"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_problem(str_arg):
+    little_problem_and_world(str_arg)
+    print('try_few_company')
+
+def little_problem_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_problem('man_and_early_fact')
