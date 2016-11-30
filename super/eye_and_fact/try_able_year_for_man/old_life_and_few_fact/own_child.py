ddd1d0c90c5367e824e5af24257646f7142fd264
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 11:09:10 -0500

    "add: ask super/way/think_woman_above_group/try_own_point/think_public_part/have_world.py, super/eye_or_point/good_week_or_place/other_company.py and super/eye_and_fact/try_able_year_for_man/old_life_and_few_fact/own_child.py in old point"

---
+++ super/eye_and_fact/try_able_year_for_man/old_life_and_few_fact/own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_group(str_arg):
+    time(str_arg)
+    print('day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_group('number')
