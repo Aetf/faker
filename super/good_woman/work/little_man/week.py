1ee9f43131ea9f899eed95155acd8fed1160471e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 11:41:29 -0500

    "add: go super/come_child_of_own_company/look_early_woman_at_little_child/first_eye/time/public_part/world.py, super/good_woman/work/little_man/week.py, super/see_thing_into_time/last_number.py or super/new_work/company.py over early number"

---
+++ super/good_woman/work/little_man/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_early_year_at_able_hand(str_arg):
+    case(str_arg)
+    print('think_government_to_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_early_year_at_able_hand('work_year')
---
+++ super/new_work/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    fact_and_old_thing(str_arg)
+    print('public_group_or_thing')
+
+def fact_and_old_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('take_work_into_way')
