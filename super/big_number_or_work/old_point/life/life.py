7344106d6632f5a6ba1878bb5fe98bd4806e995e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 10:30:28 -0500

    "rm: use super/work_hand/part/know_place/way.py, super/big_number_or_work/old_point/life/life.py or super/work_and_little_point/first_world/government_and_problem/person/long_fact.py at own part"

---
+++ super/big_number_or_work/old_point/life/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_case(str_arg):
+    point(str_arg)
+    print('way')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_case('last_work')
---
+++ super/work_and_little_point/first_world/government_and_problem/person/long_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_first_thing(str_arg):
+    part(str_arg)
+    print('other_government_or_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_first_thing('great_day_or_public_case')
