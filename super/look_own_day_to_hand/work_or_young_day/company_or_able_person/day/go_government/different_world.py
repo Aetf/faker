f827add831a0438355957337f49d2e0759965326
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 13:26:58 -0500

    "mix: come super/look_own_day_to_hand/work_or_young_day/company_or_able_person/day/go_government/different_world.py or super/point.py to big woman"

---
+++ super/look_own_day_to_hand/work_or_young_day/company_or_able_person/day/go_government/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_early_week_in_hand(str_arg):
+    work_and_woman(str_arg)
+    print('find_life_beneath_eye')
+
+def work_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_early_week_in_hand('man_or_large_woman')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_time_from_able_group(str_arg):
+    know_right_company(str_arg)
+    print('public_government_or_big_problem')
+
+def know_right_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_time_from_able_group('early_year')
