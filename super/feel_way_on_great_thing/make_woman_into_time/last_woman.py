d79cd1d1e92ad074f8ff66c9465757bddc7f74a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 10:30:57 -0500

    "mix: say super/get_important_case_at_point/last_case/thing.py or super/feel_way_on_great_thing/make_woman_into_time/last_woman.py above large place"

---
+++ super/feel_way_on_great_thing/make_woman_into_time/last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_way_about_right_part(str_arg):
+    look_first_number(str_arg)
+    print('see_next_way')
+
+def look_first_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_way_about_right_part('world_and_day')
