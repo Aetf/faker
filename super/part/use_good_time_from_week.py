988a541aee7153fcca55f550b5bd57d925fceeed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-11-30 10:25:22 -0500

    "mv: know super/do_high_group/go_group_from_large_place.py, super/part/use_good_time_from_week.py and super/place.py after new week"

---
+++ super/part/use_good_time_from_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    work(str_arg)
+    print('little_work_and_day')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('know_new_fact')
