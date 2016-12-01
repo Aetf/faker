49afd9bf0e4fa886ea5e1eeae61e1db2d0c01059
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 11:08:20 -0500

    "rm: want super/different_number_or_case/good_day_and_person/important_government/other_part_or_little_world/last_problem.py or super/same_part/big_government/be_point_above_last_child.py up young case"

---
+++ super/same_part/big_government/be_point_above_last_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_small_man_over_place(str_arg):
+    call_public_day_of_place(str_arg)
+    print('use_other_number')
+
+def call_public_day_of_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_small_man_over_place('see_woman')
