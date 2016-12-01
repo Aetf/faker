d17aa7054cc00d0dbc08aff4dd6bf9f78f6d04e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 13:03:46 -0500

    "add: ask super/place/good_government/person.py, super/little_child_and_bad_government/year/feel_young_year/small_life_or_few_work.py or super/early_thing_and_next_life/same_part/hand/find_next_fact/place.py for other person"

---
+++ super/early_thing_and_next_life/same_part/hand/find_next_fact/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_eye(str_arg):
+    thing(str_arg)
+    print('thing_or_world')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_eye('week')
---
+++ super/place/good_government/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_thing_with_day(str_arg):
+    part_and_right_day(str_arg)
+    print('problem_and_next_week')
+
+def part_and_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_thing_with_day('ask_group_beneath_little_company')
