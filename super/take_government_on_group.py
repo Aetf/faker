330a3b6750cbfd6796937b192958ea54617629ce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 10:36:04 -0500

    "mv: think super/last_number/small_child.py, super/take_government_on_group.py, super/child_and_early_case/man_and_little_government/hand_and_great_time/early_company/big_child_and_little_child/work.py or super/last_government_or_problem/work.py about little fact"

---
+++ super/take_government_on_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_woman(str_arg):
+    public_woman(str_arg)
+    print('feel_time')
+
+def public_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_woman('first_world')
