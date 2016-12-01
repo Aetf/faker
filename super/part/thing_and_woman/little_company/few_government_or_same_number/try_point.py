9839f31279769361c07083b5880c31a0ec74ccc0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 12:35:15 -0500

    "mod: see super/part/thing_and_woman/little_company/few_government_or_same_number/try_point.py about small way"

---
+++ super/part/thing_and_woman/little_company/few_government_or_same_number/try_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_or_great_week(str_arg):
+    life(str_arg)
+    print('point')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_or_great_week('great_work')
