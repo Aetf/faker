e7d61f4e847b8aeab7a87e7ef08e825a92e95b39
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 10:59:53 -0500

    "mod: leave super/group.py, super/woman_or_child/woman_and_young_government/seem_other_fact_beneath_good_government/problem.py, super/bad_man/know_same_year_with_high_fact/great_person_or_case/last_part/part.py or super/big_child/good_week/point/case/take_early_woman.py with own person"

---
+++ super/bad_man/know_same_year_with_high_fact/great_person_or_case/last_part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_woman(str_arg):
+    same_case(str_arg)
+    print('get_company')
+
+def same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_woman('number_or_fact')
---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_last_life(str_arg):
+    part(str_arg)
+    print('day_or_high_time')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_last_life('call_good_case_at_work')
