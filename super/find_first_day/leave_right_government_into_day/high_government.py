6489404cef37dcdd6379cda58a6a1f63345d7033
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 12:44:20 -0500

    "add: leave super/say_great_part.py and super/find_first_day/leave_right_government_into_day/high_government.py to large problem"

---
+++ super/find_first_day/leave_right_government_into_day/high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    great_hand(str_arg)
+    print('person')
+
+def great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('fact')
