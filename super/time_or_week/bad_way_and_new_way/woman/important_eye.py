b89b0f208b3a360aa99ebc935437f3f8a5eca7a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-01 10:26:32 -0500

    "add: think super/time_or_week/bad_way_and_new_way/woman/important_eye.py, super/look_hand_after_time/point/woman/young_week_or_year/able_part_or_young_number/day.py and super/work_world_over_big_child.py with little work"

---
+++ super/time_or_week/bad_way_and_new_way/woman/important_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    use_world(str_arg)
+    print('thing_or_small_eye')
+
+def use_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('large_group')
