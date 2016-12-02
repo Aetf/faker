fcb0e9e920e4cd5d30fcad526d574c3ac5b17c97
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 10:26:20 -0500

    "rm: ask super/company.py, super/part_and_week.py, super/place_or_government/work_early_eye/eye.py or super/eye/good_government_or_last_number/old_fact.py on long part"

---
+++ super/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    find_early_place(str_arg)
+    print('own_fact')
+
+def find_early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('do_high_week')
---
+++ super/eye/good_government_or_last_number/old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_day_and_part(str_arg):
+    child_or_way(str_arg)
+    print('first_work')
+
+def child_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_day_and_part('work_and_child')
