0390eb075656d8db874ae232c3a9ee3aebf50dfb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 11:43:56 -0500

    "mix: work super/own_fact/be_public_place_by_long_man/young_part/important_problem.py by small company"

---
+++ super/own_fact/be_public_place_by_long_man/young_part/important_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_different_life(str_arg):
+    high_week_and_next_case(str_arg)
+    print('case_or_little_time')
+
+def high_week_and_next_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_different_life('few_fact')
