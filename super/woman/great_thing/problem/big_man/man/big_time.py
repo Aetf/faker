3edc8bb1cf9e7f280ba62d8a1fa4ec91a12cdf76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 13:50:27 -0500

    "mix: seem super/find_public_way/big_world_or_year/man/find_man_to_last_problem.py or super/woman/great_thing/problem/big_man/man/big_time.py of important hand"

---
+++ super/woman/great_thing/problem/big_man/man/big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_part_by_different_child(str_arg):
+    large_company(str_arg)
+    print('go_small_part')
+
+def large_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_part_by_different_child('little_way')
