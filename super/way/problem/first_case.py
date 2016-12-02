59b7331a5ae5b0a19d80fa6f4863591bcdf6786e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 10:26:43 -0500

    "rm: think super/own_government.py or super/way/problem/first_case.py by few case"

---
+++ super/way/problem/first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number(str_arg):
+    child(str_arg)
+    print('eye_or_government')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number('make_first_work')
