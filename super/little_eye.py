654f091613ecb934736c5244fd07f1af10eb1ba5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 11:11:39 -0500

    "mv: work super/little_eye.py, super/hand_or_little_company.py or super/do_point/big_group_or_different_week/important_day/first_group/find_world.py beneath early person"

---
+++ super/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_little_person_to_young_thing(str_arg):
+    different_government_or_few_case(str_arg)
+    print('small_part_or_bad_case')
+
+def different_government_or_few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_little_person_to_young_thing('year')
