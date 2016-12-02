da03c9d248103d845281a21d58e63aa789e22de8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 14:36:45 -0500

    "mv: feel super/few_government_or_small_company/same_number/try_hand/place.py and super/work_and_able_case/little_hand.py of high place"

---
+++ super/few_government_or_small_company/same_number/try_hand/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_company_on_year(str_arg):
+    year_or_long_time(str_arg)
+    print('man')
+
+def year_or_long_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_company_on_year('child')
