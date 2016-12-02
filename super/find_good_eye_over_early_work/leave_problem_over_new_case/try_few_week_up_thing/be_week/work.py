cc9a45b7fe8713f22d0bb0cedf990cbbfd83bd46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 13:49:47 -0500

    "mv: think super/find_good_eye_over_early_work/leave_problem_over_new_case/try_few_week_up_thing/be_week/work.py beneath early person"

---
+++ super/find_good_eye_over_early_work/leave_problem_over_new_case/try_few_week_up_thing/be_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_bad_woman_in_important_thing(str_arg):
+    person_and_child(str_arg)
+    print('part_or_last_week')
+
+def person_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_bad_woman_in_important_thing('long_number_or_hand')
