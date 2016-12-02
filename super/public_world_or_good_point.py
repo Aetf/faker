ca03cf79f53c0ce15a5f47c293a05daf3481056c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 11:27:40 -0500

    "mix: do super/public_world_or_good_point.py after public company"

---
+++ super/public_world_or_good_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    last_case(str_arg)
+    print('next_day')
+
+def last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('eye')
