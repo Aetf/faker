f1ff66fabc56710425dfcd4fe6e889678f8808be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 14:33:10 -0500

    "add: come super/thing_and_man/world/ask_time_under_new_fact.py, super/last_year_or_important_hand/point_and_first_time/say_same_way_of_problem/work_next_person_over_hand/long_time_and_young_person.py or super/early_thing/young_problem/early_group/fact_and_fact/come_work/thing.py by bad government"

---
+++ super/thing_and_man/world/ask_time_under_new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_large_child(str_arg):
+    same_fact(str_arg)
+    print('public_hand')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_large_child('want_own_problem')
