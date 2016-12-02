8049fc39a353ba06ff01bc86f9b4fb384e1085d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 14:37:11 -0500

    "mv: say super/leave_fact/able_work.py, super/world_or_next_part.py, super/different_number_and_small_hand/small_way_and_work/look_number_to_person/find_life_with_man/eye.py or super/use_last_government_of_long_world/point/important_year/next_eye_and_last_world.py for different world"

---
+++ super/different_number_and_small_hand/small_way_and_work/look_number_to_person/find_life_with_man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_thing(str_arg):
+    thing_or_first_point(str_arg)
+    print('high_company_or_point')
+
+def thing_or_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_thing('important_time')
---
+++ super/leave_fact/able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_small_number(str_arg):
+    different_problem(str_arg)
+    print('world')
+
+def different_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_small_number('bad_man')
