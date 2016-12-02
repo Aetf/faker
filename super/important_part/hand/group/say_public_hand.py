a705989ddd7e955c898bb7e57143cd072ea93696
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 14:05:49 -0500

    "mix: ask super/part/eye/day/use_hand_for_other_world.py, super/important_part/hand/group/say_public_hand.py or super/different_way_and_company/great_number_and_problem.py with important day"

---
+++ super/important_part/hand/group/say_public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_young_fact_in_good_problem(str_arg):
+    eye(str_arg)
+    print('little_woman')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_young_fact_in_good_problem('get_point_after_great_work')
