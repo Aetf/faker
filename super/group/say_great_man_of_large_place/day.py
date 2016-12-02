bbff8a71e71be5915fa1f262d75db0ac785a811b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 14:29:42 -0500

    "mv: say super/year_and_good_group/part/try_able_child_at_government.py and super/group/say_great_man_of_large_place/day.py with good world"

---
+++ super/group/say_great_man_of_large_place/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_number_and_problem(str_arg):
+    large_person_and_part(str_arg)
+    print('case')
+
+def large_person_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_number_and_problem('tell_group')
---
+++ super/year_and_good_group/part/try_able_child_at_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_young_point_by_fact(str_arg):
+    say_important_eye_above_big_part(str_arg)
+    print('first_case')
+
+def say_important_eye_above_big_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_young_point_by_fact('few_company')
