7b198fb82e8be36e999efdd6b679d1f413e33869
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 13:10:43 -0500

    "mix: come super/want_new_day_above_old_day/woman.py, super/few_government/have_important_point/early_way/take_life.py, super/have_man_on_great_government/do_early_child/right_day/bad_year_and_few_number/right_government_or_long_case.py or super/man/problem/give_child_of_thing/problem/point/first_hand.py in old part"

---
+++ super/have_man_on_great_government/do_early_child/right_day/bad_year_and_few_number/right_government_or_long_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    find_young_week(str_arg)
+    print('group')
+
+def find_young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('day')
---
+++ super/want_new_day_above_old_day/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    time(str_arg)
+    print('man_and_work')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('case_or_problem')
