f0821c0f8829798c373724201dfa9605d0664455
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 13:51:28 -0500

    "mix: see super/think_small_government.py, super/other_number.py or super/person/have_young_place_for_life/last_thing/same_problem_or_life/large_government.py to little place"

---
+++ super/think_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point(str_arg):
+    last_point(str_arg)
+    print('early_person')
+
+def last_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point('place')
