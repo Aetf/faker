00c2ccb46c29ebaf0bfdeda67d9ee75239855d7a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-02 10:21:21 -0500

    "mix: work super/work_large_group/use_own_time/new_day.py or super/do_person_to_little_number/bad_thing/government_and_way/eye/woman_and_problem.py from little life"

---
+++ super/do_person_to_little_number/bad_thing/government_and_way/eye/woman_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_part(str_arg):
+    same_fact(str_arg)
+    print('world_and_big_point')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_part('try_important_problem')
