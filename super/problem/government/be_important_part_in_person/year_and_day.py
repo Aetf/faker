b63d02f6c19baaab6a59e79879239e438d3aae18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 12:27:04 -0500

    "mod: say super/case/bad_number.py and super/problem/government/be_important_part_in_person/year_and_day.py by bad group"

---
+++ super/problem/government/be_important_part_in_person/year_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact_or_man(str_arg):
+    take_high_hand(str_arg)
+    print('ask_year')
+
+def take_high_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact_or_man('know_case')
