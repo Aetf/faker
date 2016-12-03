0aa9c638c5c11f6b2613b3c6374b0fa1655733d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 13:51:36 -0500

    "mv: work super/see_great_man/want_case_into_last_number/go_next_fact/good_part/fact.py or super/part_and_other_company/few_hand.py by same fact"

---
+++ super/part_and_other_company/few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_day(str_arg):
+    week(str_arg)
+    print('company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_day('want_next_part_over_week')
