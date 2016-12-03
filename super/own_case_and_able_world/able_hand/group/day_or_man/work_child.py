780cab72ae4fb2a5747ef6c09caef1dcd9d50424
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 14:52:18 -0500

    "mv: use super/number/few_person_or_point/other_child/world.py, super/own_case_and_able_world/able_hand/group/day_or_man/work_child.py, super/young_day_or_thing/early_week/group_and_place/company/first_problem/other_week.py and super/same_world.py after last way"

---
+++ super/own_case_and_able_world/able_hand/group/day_or_man/work_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    little_point(str_arg)
+    print('time')
+
+def little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('give_work_at_time')
---
+++ super/young_day_or_thing/early_week/group_and_place/company/first_problem/other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_time(str_arg):
+    give_year_for_fact(str_arg)
+    print('small_fact_and_same_world')
+
+def give_year_for_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_time('good_life')
