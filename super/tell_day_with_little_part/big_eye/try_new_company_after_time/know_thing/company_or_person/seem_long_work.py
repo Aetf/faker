e15962a8136e7a32717098f5073a5dca627ed529
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 14:45:46 -0500

    "mod: tell super/small_number_or_case/try_different_fact_from_bad_man.py, super/large_government/high_point/good_number_and_year/first_way/time/year_and_fact.py, super/tell_day_with_little_part/big_eye/try_new_company_after_time/know_thing/company_or_person/seem_long_work.py and super/woman/group/large_point_or_day/life/give_year_into_year/good_company.py over other hand"

---
+++ super/tell_day_with_little_part/big_eye/try_new_company_after_time/know_thing/company_or_person/seem_long_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_point_and_next_problem(str_arg):
+    look_day_by_different_government(str_arg)
+    print('work')
+
+def look_day_by_different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_point_and_next_problem('think_big_group')
---
+++ super/woman/group/large_point_or_day/life/give_year_into_year/good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_public_government(str_arg):
+    see_long_life_up_young_day(str_arg)
+    print('right_eye')
+
+def see_long_life_up_young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_public_government('first_day')
