ea7c1ad59d6130c7b2e0db8d23342511bc54a0b7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 10:08:51 -0500

    "mix: work super/bad_part/group/world/old_time_or_government/man_or_small_problem/case.py, super/call_fact/case_and_old_day/last_place/bad_hand/bad_thing_and_problem.py, super/thing_and_different_man/different_problem/old_case_or_thing.py or super/think_first_point_to_way/person_or_young_government/see_early_time.py on big place"

---
+++ super/call_fact/case_and_old_day/last_place/bad_hand/bad_thing_and_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child(str_arg):
+    week(str_arg)
+    print('different_hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child('work_or_own_government')
