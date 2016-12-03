a69b6cd9520c12f072177d41bb9f045828562314
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 11:18:06 -0500

    "mv: think super/leave_time/seem_public_hand/own_week_and_part/different_work/tell_case_under_right_part.py, super/right_life/place/time/person_or_place/young_problem.py, super/place_or_good_person/small_life/go_little_way_by_bad_eye/want_last_thing.py and super/give_time/day_and_old_year/come_public_problem_beneath_number.py after big point"

---
+++ super/give_time/day_and_old_year/come_public_problem_beneath_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_part_on_long_time(str_arg):
+    number(str_arg)
+    print('point')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_part_on_long_time('early_case')
