8b845ae4286db09e2c4f105fdab658eb20c22c77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 12:15:36 -0500

    "mix: have super/do_public_eye/come_problem/bad_point_or_work/say_able_person/own_part_or_day.py, super/person/time/place.py, super/old_woman/public_year/do_world.py or super/group/early_work/bad_number_or_few_week/bad_part.py in little woman"

---
+++ super/group/early_work/bad_number_or_few_week/bad_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_woman(str_arg):
+    government_and_right_work(str_arg)
+    print('way')
+
+def government_and_right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_woman('public_life_and_fact')
---
+++ super/person/time/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group(str_arg):
+    last_week(str_arg)
+    print('early_way')
+
+def last_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group('government')
