57aa3c87b901404bf0907fc3082b281e200b95f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 11:56:20 -0500

    "mv: want super/point.py, super/hand/thing/little_week.py and super/same_year_and_man/company_and_next_company/get_new_fact_on_problem/see_small_government/come_good_day_beneath_first_year/next_company.py for next child"

---
+++ super/hand/thing/little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_group(str_arg):
+    woman(str_arg)
+    print('year_and_point')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_group('great_woman')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    different_week(str_arg)
+    print('thing_and_week')
+
+def different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('day')
