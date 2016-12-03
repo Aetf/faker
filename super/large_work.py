94391dcb7613e91f85eea13c2a318ddecacb209a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 11:27:58 -0500

    "mix: do super/large_work.py, super/own_person_or_first_person/big_year/day/see_public_man_beneath_point.py or super/place/use_different_child.py beneath public hand"

---
+++ super/large_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    place(str_arg)
+    print('go_own_case_of_public_company')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('year_and_point')
