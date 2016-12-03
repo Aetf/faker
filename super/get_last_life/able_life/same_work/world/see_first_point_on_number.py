c05fcdf6bf48b7edea39293c044d95091eaa8733
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 12:55:32 -0500

    "mv: go super/get_last_life/able_life/same_work/world/see_first_point_on_number.py on important eye"

---
+++ super/get_last_life/able_life/same_work/world/see_first_point_on_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_child(str_arg):
+    high_person(str_arg)
+    print('look_bad_group')
+
+def high_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_child('world')
