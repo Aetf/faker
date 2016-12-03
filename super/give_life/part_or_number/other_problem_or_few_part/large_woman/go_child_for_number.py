fee538e75a8ecd2b2728922728fe8554eb68576a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 10:36:43 -0500

    "add: leave super/small_work_and_person.py and super/give_life/part_or_number/other_problem_or_few_part/large_woman/go_child_for_number.py on old way"

---
+++ super/give_life/part_or_number/other_problem_or_few_part/large_woman/go_child_for_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_hand(str_arg):
+    company_and_large_world(str_arg)
+    print('public_woman')
+
+def company_and_large_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_hand('give_different_point_to_number')
