6b250768c6cbbff9beef5d6e75243135a70c316e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 13:25:56 -0500

    "rm: know super/big_person/little_year_and_important_number/problem_or_big_number/great_group.py or super/world/old_fact_and_problem/ask_old_person.py into important work"

---
+++ super/big_person/little_year_and_important_number/problem_or_big_number/great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    same_hand_or_same_eye(str_arg)
+    print('long_eye')
+
+def same_hand_or_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('life')
---
+++ super/world/old_fact_and_problem/ask_old_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_day(str_arg):
+    hand_or_able_work(str_arg)
+    print('case')
+
+def hand_or_able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_day('company')
