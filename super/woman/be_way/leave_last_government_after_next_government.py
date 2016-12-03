300baf069f3d779e6e93572533948f601320a63b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 12:55:02 -0500

    "mv: have super/different_part/early_number_or_important_place/company_or_point/great_world/work.py, super/long_person/man/right_week_and_point/large_problem_or_bad_case/high_person_and_fact.py, super/work/seem_young_number/public_child/want_public_thing/be_person/child_or_large_company.py and super/woman/be_way/leave_last_government_after_next_government.py into large man"

---
+++ super/woman/be_way/leave_last_government_after_next_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_fact(str_arg):
+    few_day(str_arg)
+    print('woman')
+
+def few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_fact('man')
