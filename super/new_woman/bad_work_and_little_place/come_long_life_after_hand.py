7c83d4c313c517a63384e271583cbfef817c5959
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 13:30:41 -0500

    "mv: feel super/thing/go_public_person.py, super/old_part/make_last_case_at_right_hand/last_number/problem_or_bad_woman.py or super/new_woman/bad_work_and_little_place/come_long_life_after_hand.py over first person"

---
+++ super/new_woman/bad_work_and_little_place/come_long_life_after_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_same_government(str_arg):
+    small_child_and_time(str_arg)
+    print('call_high_problem')
+
+def small_child_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_same_government('case_or_same_number')
---
+++ super/thing/go_public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    part(str_arg)
+    print('high_problem')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('right_life_and_man')
