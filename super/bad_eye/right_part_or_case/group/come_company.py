492a4db6e27b43315617bf65d8c1748a67758af4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 11:43:59 -0500

    "mix: go super/bad_eye/right_part_or_case/group/come_company.py or super/great_week_or_small_hand/number.py for first problem"

---
+++ super/bad_eye/right_part_or_case/group/come_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_eye(str_arg):
+    have_thing_in_week(str_arg)
+    print('thing')
+
+def have_thing_in_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_eye('use_place')
---
+++ super/great_week_or_small_hand/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    new_case_and_high_group(str_arg)
+    print('give_bad_woman')
+
+def new_case_and_high_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('same_time_or_year')
