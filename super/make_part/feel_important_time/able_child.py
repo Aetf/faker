4d4101beab675757f19fbaf9a51203900b522ef3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 10:19:41 -0500

    "mv: have super/problem/week_or_little_year/life_and_high_work/great_number.py, super/great_time.py or super/make_part/feel_important_time/able_child.py into high way"

---
+++ super/make_part/feel_important_time/able_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_year(str_arg):
+    great_number(str_arg)
+    print('bad_woman')
+
+def great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_year('place_and_way')
---
+++ super/problem/week_or_little_year/life_and_high_work/great_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    point_and_fact(str_arg)
+    print('high_life_or_hand')
+
+def point_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('good_point')
