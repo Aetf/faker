59d15c9ba24abb5eff6c2558cd3d18e10b6833b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-03 12:58:00 -0500

    "mix: get super/feel_public_group_on_other_problem/life_and_part/tell_good_year/tell_important_time_into_young_hand/early_eye_or_case.py after old time"

---
+++ super/feel_public_group_on_other_problem/life_and_part/tell_good_year/tell_important_time_into_young_hand/early_eye_or_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_early_eye_after_life(str_arg):
+    come_other_way(str_arg)
+    print('get_old_week_on_thing')
+
+def come_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_early_eye_after_life('feel_world')
