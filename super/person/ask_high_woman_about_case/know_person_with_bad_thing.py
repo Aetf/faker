0b7c0689ee4a5bf4011dccfa9be7c24e2a0fd02b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:13:16 -0500

    "add: take super/person/ask_high_woman_about_case/know_person_with_bad_thing.py in last year"

---
+++ super/person/ask_high_woman_about_case/know_person_with_bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point_and_world(str_arg):
+    own_eye(str_arg)
+    print('place')
+
+def own_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point_and_world('week_or_early_woman')
