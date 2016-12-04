34cc21c82d5383a726ecef1f584587651795861b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 11:24:06 -0500

    "rm: do super/call_right_world/able_place/few_company.py or super/next_week_and_bad_place/public_work/point/fact.py under young company"

---
+++ super/call_right_world/able_place/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    way(str_arg)
+    print('woman_and_few_work')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('leave_great_place_to_high_hand')
---
+++ super/next_week_and_bad_place/public_work/point/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    man(str_arg)
+    print('point')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('take_government_after_bad_person')
