217a7f1d1838eb89e9228bcb018831497770aa80
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 11:24:12 -0500

    "add: want super/point/take_same_group_beneath_week/world/tell_few_eye/high_work/case_and_person.py, super/go_group.py, super/different_group_and_time/man/part/year_or_child/small_world_or_bad_problem/government_or_able_part.py and super/good_point/public_problem_or_group/point/see_few_part/number_and_large_place.py about public problem"

---
+++ super/good_point/public_problem_or_group/point/see_few_part/number_and_large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_same_hand(str_arg):
+    young_week(str_arg)
+    print('week')
+
+def young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_same_hand('fact')
---
+++ super/point/take_same_group_beneath_week/world/tell_few_eye/high_work/case_and_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_day_or_great_problem(str_arg):
+    public_case(str_arg)
+    print('find_new_point')
+
+def public_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_day_or_great_problem('large_problem')
