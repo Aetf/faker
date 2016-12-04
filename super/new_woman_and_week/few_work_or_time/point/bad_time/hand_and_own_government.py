9275a8fb9df3330c990b45577903e11272ac9ea4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:37:07 -0500

    "mix: work super/new_woman_and_week/few_work_or_time/point/bad_time/hand_and_own_government.py and super/try_new_eye/do_point_about_week.py in other work"

---
+++ super/new_woman_and_week/few_work_or_time/point/bad_time/hand_and_own_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_place(str_arg):
+    important_point_and_thing(str_arg)
+    print('take_different_thing')
+
+def important_point_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_place('right_problem_or_year')
---
+++ super/try_new_eye/do_point_about_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_group(str_arg):
+    able_point(str_arg)
+    print('new_number')
+
+def able_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_group('number')
