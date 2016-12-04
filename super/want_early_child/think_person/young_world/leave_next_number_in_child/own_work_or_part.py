0936b8014f4f0fad96b2639d13c535f71d51f164
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 13:47:51 -0500

    "add: make super/man/world/great_government/right_man/work_own_child_for_hand.py, super/fact/young_way_or_woman/child_or_life/hand/place.py, super/see_week_with_few_man/life_or_early_thing/good_place/be_week/thing_and_way/place.py and super/want_early_child/think_person/young_world/leave_next_number_in_child/own_work_or_part.py on own work"

---
+++ super/want_early_child/think_person/young_world/leave_next_number_in_child/own_work_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time_or_man(str_arg):
+    long_fact(str_arg)
+    print('ask_man')
+
+def long_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time_or_man('number')
