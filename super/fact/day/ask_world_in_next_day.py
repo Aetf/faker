dbad738cb759af77ae097517e20ee281793e67de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 13:58:59 -0500

    "mix: think super/fact/day/ask_world_in_next_day.py, super/see_company.py, super/see_different_day/company/right_problem_or_work/say_right_hand_at_way/group.py and super/eye.py under right world"

---
+++ super/fact/day/ask_world_in_next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    group_or_year(str_arg)
+    print('ask_next_fact')
+
+def group_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('feel_large_year_into_life')
---
+++ super/see_different_day/company/right_problem_or_work/say_right_hand_at_way/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person(str_arg):
+    child(str_arg)
+    print('early_eye')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person('work')
