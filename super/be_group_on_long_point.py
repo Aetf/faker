5e1c420ee84cec3ebc0870ff5c8e3615ea71e104
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 14:09:49 -0500

    "mv: leave super/be_group_on_long_point.py by first work"

---
+++ super/be_group_on_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_number_or_little_fact(str_arg):
+    few_fact(str_arg)
+    print('look_hand_beneath_way')
+
+def few_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_number_or_little_fact('small_fact_and_next_company')
