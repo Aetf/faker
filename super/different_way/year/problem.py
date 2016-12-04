c5f1c2f81c0440ab53964f8ebec42d011bdc5816
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 13:43:38 -0500

    "mix: ask super/old_person_or_next_group.py and super/different_way/year/problem.py into public hand"

---
+++ super/different_way/year/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_year_of_time(str_arg):
+    early_company(str_arg)
+    print('large_person_or_problem')
+
+def early_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_year_of_time('feel_new_way')
