7286cb762263a81041d2aa6191d6a98948aaeda8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:13:18 -0500

    "mv: go super/new_eye/important_man/leave_young_case/child.py, super/take_great_point_after_day/little_case_and_company/look_case_with_few_company/next_company/large_life/little_year.py, super/group/important_life_or_bad_man/do_case_above_bad_place/number_and_thing/think_woman_up_person/fact.py or super/place/time.py over next number"

---
+++ super/group/important_life_or_bad_man/do_case_above_bad_place/number_and_thing/think_woman_up_person/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_government(str_arg):
+    case_or_work(str_arg)
+    print('big_point')
+
+def case_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_government('different_woman_and_year')
---
+++ super/place/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_work_over_new_work(str_arg):
+    young_day(str_arg)
+    print('woman')
+
+def young_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_work_over_new_work('small_world')
