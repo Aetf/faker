a936fe2383ed7865dc606f10218bc395559aeb72
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:00:35 -0500

    "add: get super/woman_or_world/next_fact.py and super/point_and_old_point/hand_or_own_number/see_problem_to_work/be_fact_in_company.py beneath other problem"

---
+++ super/point_and_old_point/hand_or_own_number/see_problem_to_work/be_fact_in_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_case_above_next_case(str_arg):
+    young_point_and_large_number(str_arg)
+    print('able_point_or_same_case')
+
+def young_point_and_large_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_case_above_next_case('use_point_of_child')
---
+++ super/woman_or_world/next_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_government_or_first_problem(str_arg):
+    few_government(str_arg)
+    print('able_government')
+
+def few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_government_or_first_problem('woman')
