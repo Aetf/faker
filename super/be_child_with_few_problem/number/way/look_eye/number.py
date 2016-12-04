c87721660532f9325bff775601761362fb21a7a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:30:10 -0500

    "rm: feel super/work/eye/same_eye/new_year_and_young_man.py, super/next_government/come_different_thing/work.py or super/be_child_with_few_problem/number/way/look_eye/number.py over bad number"

---
+++ super/be_child_with_few_problem/number/way/look_eye/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_large_government(str_arg):
+    point_or_group(str_arg)
+    print('child')
+
+def point_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_large_government('life')
---
+++ super/next_government/come_different_thing/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    own_problem_or_important_person(str_arg)
+    print('company')
+
+def own_problem_or_important_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('case')
