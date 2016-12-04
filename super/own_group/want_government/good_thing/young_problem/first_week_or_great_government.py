0abbcd9d4eb16f1638779cd8a63a1e76b87f7499
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:55:54 -0500

    "rm: try super/own_group/want_government/good_thing/young_problem/first_week_or_great_government.py and super/tell_work/world/work/woman/person.py in old life"

---
+++ super/own_group/want_government/good_thing/young_problem/first_week_or_great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    little_number(str_arg)
+    print('few_world')
+
+def little_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('woman')
