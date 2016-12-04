7e31f45a8500f8af6f1d7103ab598d178e1d1473
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:29:03 -0500

    "mod: call super/time/right_week/take_company/group.py, super/long_government/little_year_and_group/number_and_problem/week/bad_eye_and_few_eye/want_able_fact.py or super/take_world_after_fact/last_group/small_part/week/work_different_hand_of_group.py up right way"

---
+++ super/long_government/little_year_and_group/number_and_problem/week/bad_eye_and_few_eye/want_able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    number(str_arg)
+    print('be_right_hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('company')
---
+++ super/time/right_week/take_company/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    small_work(str_arg)
+    print('work_next_work_on_next_place')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('next_child_and_different_week')
