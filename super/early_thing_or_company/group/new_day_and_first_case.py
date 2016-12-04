549385ead2e3cd01ea3d2309c5d62f8b664b7288
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 14:19:21 -0500

    "add: work super/do_way_of_way/next_government.py, super/early_thing_or_company/group/new_day_and_first_case.py, super/try_eye_at_first_point/child/big_work/big_thing/get_long_part.py or super/different_problem.py for right life"

---
+++ super/early_thing_or_company/group/new_day_and_first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_old_hand(str_arg):
+    point(str_arg)
+    print('want_big_life_on_hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_old_hand('man')
---
+++ super/try_eye_at_first_point/child/big_work/big_thing/get_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    ask_large_case(str_arg)
+    print('old_company_or_own_week')
+
+def ask_large_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('woman')
