45fe929f2dd037d90feb08cb44de2391a54607f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 12:02:40 -0500

    "mv: find super/first_government/able_hand/new_eye_and_person/point/woman/group_and_able_life.py above good number"

---
+++ super/first_government/able_hand/new_eye_and_person/point/woman/group_and_able_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    point_or_same_government(str_arg)
+    print('important_person')
+
+def point_or_same_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('small_week')
