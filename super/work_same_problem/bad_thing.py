50d85cb4c7ad0c5878643a54795d20df71c55392
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 14:02:20 -0500

    "add: take super/work_same_problem/bad_thing.py over little part"

---
+++ super/work_same_problem/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    different_woman(str_arg)
+    print('give_high_time')
+
+def different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('try_few_way')
