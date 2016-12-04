a96f196380bf470f4d2aa41954c3dd5a1e98df9f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 10:41:25 -0500

    "mix: think super/day_and_way/leave_person.py, super/leave_little_world_with_small_time/world_and_man/number_or_same_eye/take_man_with_own_work.py, super/way_or_world/life/public_eye/case.py and super/little_year/own_fact/small_world/way.py about young time"

---
+++ super/little_year/own_fact/small_world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_large_problem_to_year(str_arg):
+    part(str_arg)
+    print('government')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_large_problem_to_year('small_government')
---
+++ super/way_or_world/life/public_eye/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_company(str_arg):
+    problem(str_arg)
+    print('have_thing_on_new_place')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_company('new_eye')
