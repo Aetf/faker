966703def3d056617c3c196c47cbdebccf5b71c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 11:23:39 -0500

    "rm: try super/small_government.py and super/first_time_or_different_part/tell_public_week.py under large work"

---
+++ super/small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_time(str_arg):
+    know_old_company(str_arg)
+    print('day_or_old_company')
+
+def know_old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_time('last_time')
