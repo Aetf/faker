c6f6efeb3f56c00e8830e5f18d17c57bc27db786
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 12:02:44 -0500

    "rm: try super/think_first_time/company/fact/get_great_eye.py and super/other_number/week_or_public_eye/want_fact/high_company_and_place/part_or_case/problem_or_big_child.py about high day"

---
+++ super/other_number/week_or_public_eye/want_fact/high_company_and_place/part_or_case/problem_or_big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_young_group(str_arg):
+    next_way(str_arg)
+    print('point')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_young_group('few_point')
