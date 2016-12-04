f1cf19863cb597dcc944e0befa435a4ad00cc6d9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-04 12:02:18 -0500

    "add: know super/next_day.py, super/great_work/eye/work/long_work/problem/old_eye_or_next_day.py and super/able_place/way/good_work/take_different_week_under_different_government/life_or_important_government.py of high government"

---
+++ super/great_work/eye/work/long_work/problem/old_eye_or_next_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    own_way(str_arg)
+    print('look_point_over_problem')
+
+def own_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('hand')
