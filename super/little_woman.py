5bcbf227a2d4c201ef9f5766386101c652d2ce33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 14:19:00 -0500

    "mod: take super/get_part_from_number/have_bad_group.py and super/little_woman.py after good way"

---
+++ super/little_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_government_and_day(str_arg):
+    call_man(str_arg)
+    print('small_time_and_able_number')
+
+def call_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_government_and_day('give_week_over_big_point')
