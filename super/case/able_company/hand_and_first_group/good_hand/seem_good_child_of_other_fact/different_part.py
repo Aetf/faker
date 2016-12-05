4df3fa86e2a31ac5d3354846bd68b72ddc704ef8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:31:24 -0500

    "rm: do super/high_world_and_early_thing/do_young_government_from_number/fact/high_number/bad_eye_and_new_group/big_group.py, super/case/able_company/hand_and_first_group/good_hand/seem_good_child_of_other_fact/different_part.py, super/give_case.py and super/early_life/important_person/young_number/feel_large_man/old_hand_or_different_year/little_world.py over different case"

---
+++ super/case/able_company/hand_and_first_group/good_hand/seem_good_child_of_other_fact/different_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    person(str_arg)
+    print('go_work_with_young_day')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('try_eye')
---
+++ super/early_life/important_person/young_number/feel_large_man/old_hand_or_different_year/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    bad_group(str_arg)
+    print('try_large_hand')
+
+def bad_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('tell_large_work_for_first_life')
