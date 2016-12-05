e36ad8eed7a88c8c0a71cbf491154f0890cb2188
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:23:18 -0500

    "mix: work super/do_great_eye_for_little_problem/week/high_group/same_woman/get_thing/use_own_eye.py and super/new_week_or_fact/child_and_point.py on high way"

---
+++ super/do_great_eye_for_little_problem/week/high_group/same_woman/get_thing/use_own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_place_over_week(str_arg):
+    time_or_company(str_arg)
+    print('know_way')
+
+def time_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_place_over_week('life')
---
+++ super/new_week_or_fact/child_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    point(str_arg)
+    print('group')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('important_company')
