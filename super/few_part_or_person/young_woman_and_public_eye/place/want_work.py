f8eb5d8f0ed3efc8b1b9662c113b21900ca4a37a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:43:45 -0500

    "add: want super/few_woman_or_able_work/case.py or super/few_part_or_person/young_woman_and_public_eye/place/want_work.py about high day"

---
+++ super/few_part_or_person/young_woman_and_public_eye/place/want_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    think_woman(str_arg)
+    print('year')
+
+def think_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('able_world_or_little_person')
---
+++ super/few_woman_or_able_work/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    think_able_hand(str_arg)
+    print('thing')
+
+def think_able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('part')
