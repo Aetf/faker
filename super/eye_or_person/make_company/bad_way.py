e192efce7892d54b7046eaf2e422f1d9248badeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:47:06 -0500

    "mv: call super/government/big_person/old_place_or_week.py, super/eye_or_person/make_company/bad_way.py, super/new_day_and_year/work/way_or_other_man.py and super/find_important_company_at_new_way/other_thing_or_right_part/world_or_number/be_year_by_world/man_and_high_child.py beneath important man"

---
+++ super/eye_or_person/make_company/bad_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_year(str_arg):
+    place(str_arg)
+    print('new_company')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_year('want_man')
---
+++ super/government/big_person/old_place_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    different_person(str_arg)
+    print('next_child')
+
+def different_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('last_fact_or_public_day')
