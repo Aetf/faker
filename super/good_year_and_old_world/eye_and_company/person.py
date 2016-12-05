53cc341afb3e493a770655a1074a3cf00124d1c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 13:53:49 -0500

    "add: feel super/good_year_and_old_world/eye_and_company/person.py after old person"

---
+++ super/good_year_and_old_world/eye_and_company/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    number_or_next_person(str_arg)
+    print('day')
+
+def number_or_next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('good_life_and_woman')
