fb99d5de6ae13d8697e48b909881986e9b82d3c9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:40:56 -0500

    "add: give super/hand_and_time/first_company/small_time.py, super/first_world/year.py and super/want_little_day.py beneath important life"

---
+++ super/first_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    new_number_and_small_year(str_arg)
+    print('bad_man')
+
+def new_number_and_small_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('young_eye_and_way')
