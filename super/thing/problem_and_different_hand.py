4f1f5954a302fec7cd9c29455f19755d3a40b744
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:40:57 -0500

    "mv: work super/thing/problem_and_different_hand.py and super/company/give_work_from_public_fact/world.py with little case"

---
+++ super/thing/problem_and_different_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_work(str_arg):
+    little_part(str_arg)
+    print('place')
+
+def little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_work('early_woman')
