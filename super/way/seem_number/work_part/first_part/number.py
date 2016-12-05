997878d20b981cf6e3c88b567a5b8e8e6aab87be
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 10:10:17 -0500

    "rm: go super/large_week_and_bad_way/able_year_and_last_week.py, super/way/seem_number/work_part/first_part/number.py, super/world/time.py or super/little_place_or_next_government/have_case_in_young_time/good_woman.py about little hand"

---
+++ super/way/seem_number/work_part/first_part/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_child_or_little_work(str_arg):
+    leave_number(str_arg)
+    print('hand')
+
+def leave_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_child_or_little_work('feel_young_life_about_small_problem')
---
+++ super/world/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_bad_part(str_arg):
+    problem_and_place(str_arg)
+    print('world')
+
+def problem_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_bad_part('small_number')
