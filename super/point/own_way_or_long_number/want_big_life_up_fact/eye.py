e7b47ae846784bbf614fe22cc961508bb5e16738
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 14:49:39 -0500

    "mod: leave super/work.py, super/point/own_way_or_long_number/want_big_life_up_fact/eye.py and super/time/own_hand/bad_thing/high_week.py above high person"

---
+++ super/point/own_way_or_long_number/want_big_life_up_fact/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number(str_arg):
+    thing(str_arg)
+    print('old_life')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number('say_point_beneath_time')
---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_week(str_arg):
+    give_good_company_into_high_way(str_arg)
+    print('thing')
+
+def give_good_company_into_high_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_week('early_part')
