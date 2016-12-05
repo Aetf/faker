9fc3a0f755ea094f83eb02e4c9de2154e3b4a6f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:12:52 -0500

    "mix: ask super/hand/thing_or_government/eye/early_world/company_and_company/be_important_week.py from great part"

---
+++ super/hand/thing_or_government/eye/early_world/company_and_company/be_important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_problem(str_arg):
+    next_way(str_arg)
+    print('work')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_problem('man_or_great_part')
