2721fb9f5495808c3e6f48f6ac8b9a1e6d7190e9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:17:29 -0500

    "add: leave super/try_case/week.py by long problem"

---
+++ super/try_case/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_or_number(str_arg):
+    large_hand(str_arg)
+    print('little_point')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_or_number('world')
