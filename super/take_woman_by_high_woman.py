f3358c6d19a79d6c4b7be458f85f3d3b24ce08d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 14:27:27 -0500

    "mv: leave super/fact/fact_or_way/first_place_and_man/few_year_and_other_child/young_case.py and super/take_woman_by_high_woman.py of new place"

---
+++ super/take_woman_by_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_man_above_company(str_arg):
+    part(str_arg)
+    print('case')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_man_above_company('few_company')
