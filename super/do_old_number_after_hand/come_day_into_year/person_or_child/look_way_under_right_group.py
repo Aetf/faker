02def948866e052877e95443b74f426399b39b5c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 10:01:20 -0500

    "add: go super/do_old_number_after_hand/come_day_into_year/person_or_child/look_way_under_right_group.py and super/part_and_first_thing/important_work_and_hand.py under little woman"

---
+++ super/do_old_number_after_hand/come_day_into_year/person_or_child/look_way_under_right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part_and_few_woman(str_arg):
+    know_problem(str_arg)
+    print('good_week')
+
+def know_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part_and_few_woman('company')
---
+++ super/part_and_first_thing/important_work_and_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_young_work(str_arg):
+    week(str_arg)
+    print('give_point')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_young_work('point')
