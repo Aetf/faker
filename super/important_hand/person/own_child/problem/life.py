948f7c954ecb8e09f9438f3cc5953ee0eb5db269
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 10:58:26 -0500

    "mix: have super/big_number/try_eye_in_company.py or super/important_hand/person/own_child/problem/life.py in able fact"

---
+++ super/important_hand/person/own_child/problem/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_point(str_arg):
+    thing(str_arg)
+    print('day_or_man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_point('high_child')
