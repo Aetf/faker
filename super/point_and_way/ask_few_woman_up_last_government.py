52450a10d7546c02c613aff089f89c864cd8c262
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 14:15:32 -0500

    "mod: see super/eye.py and super/point_and_way/ask_few_woman_up_last_government.py after great life"

---
+++ super/point_and_way/ask_few_woman_up_last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    great_thing(str_arg)
+    print('leave_own_part')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('work')
