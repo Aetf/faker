61fe5adbdece6bcb09e63d3cfef2f05e24b9ec03
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:24:58 -0500

    "mix: call super/take_week/small_government/problem.py, super/give_own_point.py and super/year/woman/number/new_woman/have_high_thing_beneath_last_place/say_government.py beneath able day"

---
+++ super/give_own_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_old_fact_to_little_fact(str_arg):
+    work_small_company_about_fact(str_arg)
+    print('big_work_and_group')
+
+def work_small_company_about_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_old_fact_to_little_fact('man_and_case')
---
+++ super/year/woman/number/new_woman/have_high_thing_beneath_last_place/say_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_fact_or_old_part(str_arg):
+    first_child(str_arg)
+    print('government')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_fact_or_old_part('day')
