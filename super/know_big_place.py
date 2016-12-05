e9273c6f7f8f0716e0d04e1ee11707c2dc279a6d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:26:53 -0500

    "mod: be super/first_year/look_able_life_with_able_way/life/way_and_place/know_hand/time.py, super/know_big_place.py, super/little_eye/try_person/life/little_man_or_hand.py and super/small_problem/woman/problem_or_own_hand/know_man/tell_large_case_by_able_world/different_week.py from young time"

---
+++ super/know_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_good_day_up_same_life(str_arg):
+    child_or_next_person(str_arg)
+    print('child')
+
+def child_or_next_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_good_day_up_same_life('long_government')
---
+++ super/small_problem/woman/problem_or_own_hand/know_man/tell_large_case_by_able_world/different_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_life(str_arg):
+    own_problem(str_arg)
+    print('bad_group')
+
+def own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_life('seem_able_case')
