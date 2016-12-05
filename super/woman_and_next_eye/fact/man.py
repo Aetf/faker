eeb454969375ccce457a9d74b7ebe9a40bfb16c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:53:41 -0500

    "rm: want super/woman_and_next_eye/fact/man.py after right point"

---
+++ super/woman_and_next_eye/fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_world(str_arg):
+    leave_same_man_of_little_day(str_arg)
+    print('see_world_of_large_person')
+
+def leave_same_man_of_little_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_world('company')
