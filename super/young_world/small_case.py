fc3c307adbca03937543a44177518cae6259727b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 12:36:01 -0500

    "add: call super/day_and_life/fact/good_woman_and_work/few_government.py, super/young_world/small_case.py or super/point/little_case_and_way/right_work_and_place/good_man.py into different place"

---
+++ super/young_world/small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_case_or_early_group(str_arg):
+    early_day_and_other_way(str_arg)
+    print('do_company_from_government')
+
+def early_day_and_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_case_or_early_group('old_company_and_same_number')
