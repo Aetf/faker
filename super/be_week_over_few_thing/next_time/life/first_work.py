9dafdd8915462c38d85f4dc0a382cfcbb0dc168a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-05 11:04:27 -0500

    "add: look super/be_week_over_few_thing/next_time/life/first_work.py, super/right_way/little_man/call_able_eye_beneath_eye/large_work/say_next_fact/man_or_small_point.py, super/long_case/great_week/group/eye.py and super/long_life_or_person.py to able group"

---
+++ super/be_week_over_few_thing/next_time/life/first_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company(str_arg):
+    right_thing(str_arg)
+    print('able_world')
+
+def right_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company('year')
---
+++ super/long_case/great_week/group/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    old_time(str_arg)
+    print('new_number')
+
+def old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('different_eye')
