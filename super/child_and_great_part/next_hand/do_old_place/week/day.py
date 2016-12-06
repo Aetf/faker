64d558d8d65c02a128c3b306b765f997115675a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 13:04:05 -0500

    "rm: go super/child_and_great_part/next_hand/do_old_place/week/day.py to first number"

---
+++ super/child_and_great_part/next_hand/do_old_place/week/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    come_public_way_under_next_man(str_arg)
+    print('leave_place_into_next_thing')
+
+def come_public_way_under_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('point')
