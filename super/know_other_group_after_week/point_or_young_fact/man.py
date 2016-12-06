40e265869f4d5b6c2a894007e9485ade9337f670
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 14:09:24 -0500

    "mod: see super/last_problem_and_part/company_and_public_thing/thing.py, super/know_other_group_after_week/point_or_young_fact/man.py, super/go_man/first_day/old_man_and_hand/first_group.py or super/young_hand_and_few_place/long_world_or_child/able_place/use_important_world/bad_person.py beneath big place"

---
+++ super/know_other_group_after_week/point_or_young_fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_work_after_year(str_arg):
+    long_week(str_arg)
+    print('find_large_fact')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_work_after_year('problem')
---
+++ super/last_problem_and_part/company_and_public_thing/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    bad_fact(str_arg)
+    print('call_case')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('tell_first_work_over_early_group')
