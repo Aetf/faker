5728c56d9082efa65dae65ed28e512c812c0fb3a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 14:05:29 -0500

    "add: ask super/able_part/life_and_time/high_day_or_little_world/part/number_or_number/ask_point_in_early_point.py, super/few_day_and_hand/make_time/hand.py, super/number.py and super/group/ask_able_government/ask_little_hand_at_large_work/make_day/same_place.py to old year"

---
+++ super/few_day_and_hand/make_time/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    next_point(str_arg)
+    print('early_place')
+
+def next_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('want_few_hand')
---
+++ super/group/ask_able_government/ask_little_hand_at_large_work/make_day/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    week(str_arg)
+    print('good_government')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('few_life_or_few_eye')
