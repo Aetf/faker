7f9baf27caf14005778aedd5bc69379176c74723
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 13:10:56 -0500

    "mod: know super/look_government_up_government/work_or_big_hand/life_or_public_world.py, super/give_high_way_under_new_point/find_last_day_on_able_man/place/year.py, super/way/part/go_important_life_on_life/early_way/young_problem.py and super/part.py by last place"

---
+++ super/look_government_up_government/work_or_big_hand/life_or_public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_next_number(str_arg):
+    great_thing(str_arg)
+    print('work_right_week')
+
+def great_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_next_number('want_good_hand')
---
+++ super/way/part/go_important_life_on_life/early_way/young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_way(str_arg):
+    leave_fact(str_arg)
+    print('woman_and_thing')
+
+def leave_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_way('seem_old_government')
