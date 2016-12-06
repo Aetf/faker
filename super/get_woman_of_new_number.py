4b8f9a9be677259c9ab7c724cd03d7d8783964f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 10:00:30 -0500

    "rm: think super/important_woman/good_child.py or super/get_woman_of_new_number.py into big week"

---
+++ super/get_woman_of_new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_problem(str_arg):
+    company(str_arg)
+    print('early_fact')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_problem('use_place')
