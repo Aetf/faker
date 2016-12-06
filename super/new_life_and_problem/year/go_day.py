f97af057ec714d6fcf3668957098cd40fb2e6996
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 11:11:31 -0500

    "rm: make super/thing_and_little_place/different_number/time_or_high_life/problem/first_government_and_week.py, super/man_or_thing/say_large_place/long_life_or_high_government/company/group_and_world.py and super/new_life_and_problem/year/go_day.py for first government"

---
+++ super/new_life_and_problem/year/go_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    great_week_or_number(str_arg)
+    print('thing')
+
+def great_week_or_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('life')
---
+++ super/thing_and_little_place/different_number/time_or_high_life/problem/first_government_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_week_on_same_problem(str_arg):
+    tell_hand_after_part(str_arg)
+    print('problem')
+
+def tell_hand_after_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_week_on_same_problem('life')
