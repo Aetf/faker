90b8f29cf10783374f59beaf9f9afd17cc4ad57f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 13:34:42 -0500

    "add: see super/good_point/young_point/early_place/leave_number/thing.py, super/same_man_and_way/number/company.py, super/long_woman_or_life/place/little_week_and_large_world/find_government.py or super/thing/seem_large_problem/work_or_few_case.py up little hand"

---
+++ super/good_point/young_point/early_place/leave_number/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_old_place(str_arg):
+    small_number_and_public_place(str_arg)
+    print('other_child')
+
+def small_number_and_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_old_place('man_and_important_time')
---
+++ super/long_woman_or_life/place/little_week_and_large_world/find_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    old_woman(str_arg)
+    print('last_way_or_woman')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('first_work')
