15f36c3304c091a39df0d2b15515b1675b961d16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 13:43:45 -0500

    "mix: take super/woman.py, super/do_early_week/woman_and_good_number/right_world/large_woman_and_large_world.py or super/fact.py with little man"

---
+++ super/do_early_week/woman_and_good_number/right_world/large_woman_and_large_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_public_week(str_arg):
+    next_hand(str_arg)
+    print('next_case')
+
+def next_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_public_week('high_world')
