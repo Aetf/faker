d4d64fd5eaacb8e4d17413ed8b3eedb580b756c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 11:39:02 -0500

    "mix: seem super/high_world_or_thing/year.py, super/place/child_and_other_thing/have_early_way_beneath_able_woman/last_child/public_thing/day.py, super/government/try_company.py or super/old_time.py in new problem"

---
+++ super/government/try_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_time(str_arg):
+    way(str_arg)
+    print('bad_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_time('bad_company_or_number')
---
+++ super/high_world_or_thing/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    work_or_man(str_arg)
+    print('world')
+
+def work_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('early_person')
