fee30f7f6fd1efb68f0248d3f1898cfff165e0bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 11:00:02 -0500

    "mv: do super/make_old_woman/same_part_or_man/person_and_point/next_man.py, super/first_year/good_case/tell_person_after_large_government/next_hand/feel_different_woman_up_problem/child_and_place.py or super/company_or_group.py after long group"

---
+++ super/company_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    high_life(str_arg)
+    print('right_group')
+
+def high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('point')
---
+++ super/make_old_woman/same_part_or_man/person_and_point/next_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_other_woman_about_eye(str_arg):
+    young_hand_and_great_hand(str_arg)
+    print('look_man_at_high_company')
+
+def young_hand_and_great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_other_woman_about_eye('next_eye')
