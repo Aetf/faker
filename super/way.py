3c9f376ff1204eeef8f1df9f7dc7e736233044c2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 13:38:35 -0500

    "rm: give super/government/way_or_different_company/little_time/man/get_company/high_time.py, super/life.py and super/way.py up different time"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_new_week_of_hand(str_arg):
+    have_time(str_arg)
+    print('life_and_number')
+
+def have_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_new_week_of_hand('world')
