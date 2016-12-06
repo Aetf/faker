e40d133eb4a29c60573f2543dc8bbdad083af355
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-06 12:30:43 -0500

    "rm: take super/use_hand_from_eye.py and super/bad_point.py into few time"

---
+++ super/bad_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_work_on_high_world(str_arg):
+    hand(str_arg)
+    print('case')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_work_on_high_world('life_and_first_thing')
