7a50cdc4268122a3c6fe2d7681b001020d39d82f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 10:39:39 -0500

    "mix: go super/first_woman/public_problem_or_good_group/right_person/case/child/large_company.py, super/next_case/important_person/hand/new_problem_and_case.py and super/long_world/great_eye_or_number/hand.py on bad way"

---
+++ super/next_case/important_person/hand/new_problem_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_part(str_arg):
+    world_and_place(str_arg)
+    print('take_fact_after_woman')
+
+def world_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_part('man_and_way')
