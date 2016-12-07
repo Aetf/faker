93bbe3f79152b21c2bbe668a5a825e367964eec5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 10:20:26 -0500

    "mix: see super/company_and_work/see_large_hand/life_and_hand/case/get_public_man_by_group.py, super/look_good_woman/little_group/woman_and_next_child.py or super/man/man_or_place/use_company/life/other_year_or_case.py up good problem"

---
+++ super/company_and_work/see_large_hand/life_and_hand/case/get_public_man_by_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_thing_into_public_hand(str_arg):
+    hand_and_life(str_arg)
+    print('high_child')
+
+def hand_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_thing_into_public_hand('different_child')
