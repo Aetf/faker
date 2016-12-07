122445fd81a141776b384a16c79fce828f104125
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 13:17:18 -0500

    "mv: make super/company/way/want_important_world.py or super/few_part/great_child_or_fact.py in other life"

---
+++ super/few_part/great_child_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_own_person_in_good_child(str_arg):
+    tell_life(str_arg)
+    print('new_hand_or_last_time')
+
+def tell_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_own_person_in_good_child('seem_life')
