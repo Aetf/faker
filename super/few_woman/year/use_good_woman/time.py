abf69138286eab727724e5d966c8c1f0caf2da49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 12:04:21 -0500

    "mix: do super/few_woman/year/use_good_woman/time.py about able person"

---
+++ super/few_woman/year/use_good_woman/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    day(str_arg)
+    print('first_fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('number')
