88a8f3c868ca1f2f6a9b0d1d5c46c81468fab2e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 13:29:07 -0500

    "mix: ask super/say_small_child/small_case/same_company.py, super/own_world_and_big_time.py or super/same_day/man.py with first person"

---
+++ super/own_world_and_big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_high_eye(str_arg):
+    little_place(str_arg)
+    print('old_eye')
+
+def little_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_high_eye('do_problem')
---
+++ super/say_small_child/small_case/same_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    eye(str_arg)
+    print('see_last_day')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('say_week')
