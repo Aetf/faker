8cf2c66755464cfefc6aa86cb00b7ed95dd15502
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 11:41:06 -0500

    "rm: ask super/big_woman/point/see_public_point.py, super/use_government_of_day/time.py, super/number_or_bad_man/next_number/few_point.py or super/time_and_place/child_and_number/eye.py under other fact"

---
+++ super/big_woman/point/see_public_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    point(str_arg)
+    print('feel_world_of_fact')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('first_life')
---
+++ super/use_government_of_day/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_problem(str_arg):
+    old_man_and_child(str_arg)
+    print('woman')
+
+def old_man_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_problem('early_year')
