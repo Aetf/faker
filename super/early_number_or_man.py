00412e66dc1b5b92eba955bd9c28cdca9c1d58d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 10:25:06 -0500

    "mix: leave super/early_number_or_man.py, super/go_problem/place/few_fact/give_bad_fact_by_next_person.py and super/part.py beneath high year"

---
+++ super/early_number_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    able_hand(str_arg)
+    print('number')
+
+def able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('bad_way')
