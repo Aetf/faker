8bd60bfb11fd2bf36d962b6e73ab29d1831a1ba3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 13:04:33 -0500

    "add: come super/young_point/work_or_child/bad_person/right_child.py and super/life/important_life_or_company/important_case.py above small time"

---
+++ super/life/important_life_or_company/important_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    get_way_over_child(str_arg)
+    print('hand_and_small_number')
+
+def get_way_over_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('great_problem_and_different_government')
