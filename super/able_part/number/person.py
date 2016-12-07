5944bc7d9bc4fcd50d39a41165de9f39c8810260
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 11:20:31 -0500

    "rm: use super/next_life/have_day/ask_place_of_way/able_place.py, super/able_part/number/person.py or super/group_or_different_part/same_person.py after same man"

---
+++ super/able_part/number/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_young_fact_after_old_day(str_arg):
+    man_or_way(str_arg)
+    print('important_woman_and_government')
+
+def man_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_young_fact_after_old_day('own_life')
---
+++ super/next_life/have_day/ask_place_of_way/able_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_world(str_arg):
+    week(str_arg)
+    print('hand_and_government')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_world('able_company')
