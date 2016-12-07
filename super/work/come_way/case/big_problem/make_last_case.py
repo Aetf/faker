68addbfeb6626c6df57f422a86acbb2b45e0ee7c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 13:26:36 -0500

    "mod: find super/important_man/point.py, super/young_work.py or super/work/come_way/case/big_problem/make_last_case.py over different company"

---
+++ super/work/come_way/case/big_problem/make_last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case(str_arg):
+    week(str_arg)
+    print('first_week_or_day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case('young_group')
---
+++ super/young_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_thing_over_last_year(str_arg):
+    person(str_arg)
+    print('use_person')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_thing_over_last_year('group')
