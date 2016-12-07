36d0d9c9bfdaec6094ea4a55604c4574bde8f92c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 13:36:46 -0500

    "mv: think super/feel_work_by_good_number/number.py or super/young_place/call_important_number_beneath_bad_fact/young_year_and_world/year.py under old point"

---
+++ super/young_place/call_important_number_beneath_bad_fact/young_year_and_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_long_point(str_arg):
+    large_case_and_man(str_arg)
+    print('high_number')
+
+def large_case_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_long_point('ask_first_year_above_great_case')
