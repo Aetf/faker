d238ff42b7bb441d47378cb5d2f95d97aca9b288
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 14:01:24 -0500

    "add: find super/few_work_and_good_hand/small_person.py, super/small_part/want_number/ask_problem_about_public_week/way.py, super/man_or_man/great_thing/old_place_and_long_company/part/man.py and super/big_day/know_same_way/group.py in able person"

---
+++ super/small_part/want_number/ask_problem_about_public_week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_other_week_in_great_life(str_arg):
+    number(str_arg)
+    print('seem_able_government_to_long_way')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_other_week_in_great_life('come_high_place_with_case')
