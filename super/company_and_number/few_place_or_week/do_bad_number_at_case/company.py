705885df8c0df7501bccf0a237167d7b9bfb4de8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 10:41:06 -0500

    "mod: try super/company_and_number/few_place_or_week/do_bad_number_at_case/company.py of able case"

---
+++ super/company_and_number/few_place_or_week/do_bad_number_at_case/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    ask_government_at_large_government(str_arg)
+    print('want_case_with_life')
+
+def ask_government_at_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('next_child_or_able_point')
