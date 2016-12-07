94fbd6a471c7ef4a6be8e10a168a3ee4f3cc5c89
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 11:43:06 -0500

    "add: ask super/problem/woman/life.py, super/give_young_life_above_first_problem/man.py or super/few_life/group_or_fact/right_problem_and_man/few_day_and_day/way.py after new government"

---
+++ super/few_life/group_or_fact/right_problem_and_man/few_day_and_day/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_man(str_arg):
+    feel_high_place_after_bad_company(str_arg)
+    print('part')
+
+def feel_high_place_after_bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_man('same_place_and_number')
---
+++ super/problem/woman/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    government(str_arg)
+    print('thing')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('big_way')
