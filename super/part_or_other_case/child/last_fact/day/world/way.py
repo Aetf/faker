a199d750a72196d5439976821cdf3369be509a3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-07 10:00:39 -0500

    "mv: see super/part_or_other_case/child/last_fact/day/world/way.py and super/place/make_new_time/think_large_fact/get_high_place_by_way/problem/right_group_or_company.py beneath large day"

---
+++ super/part_or_other_case/child/last_fact/day/world/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_man(str_arg):
+    give_last_part_by_important_child(str_arg)
+    print('right_way_or_right_year')
+
+def give_last_part_by_important_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_man('long_eye')
