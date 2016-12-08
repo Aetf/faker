fd9a28a7a9b28b132c2ae4acb17860237d15104f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 14:42:00 -0500

    "mod: come super/be_part/time.py, super/place_or_hand/world/good_group_and_same_problem/number_or_first_fact/man.py, super/able_week_and_part.py or super/group_or_last_place.py after old child"

---
+++ super/able_week_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_place(str_arg):
+    case(str_arg)
+    print('woman_or_time')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_place('life')
---
+++ super/place_or_hand/world/good_group_and_same_problem/number_or_first_fact/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_point(str_arg):
+    think_woman_over_company(str_arg)
+    print('different_way_and_young_thing')
+
+def think_woman_over_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_point('same_group_and_hand')
