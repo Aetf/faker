91f191d5628c42c97d7f37345836c4120ebcf376
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 11:43:41 -0500

    "add: call super/other_week/eye.py, super/big_problem_or_work/able_place/man_or_fact/early_life_and_same_week/early_week/say_public_year_up_good_child.py and super/go_own_year/old_fact/have_woman_into_old_point/big_fact_and_child.py in long way"

---
+++ super/other_week/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    group_and_day(str_arg)
+    print('old_thing')
+
+def group_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('go_able_point')
