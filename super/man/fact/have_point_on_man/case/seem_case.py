40bbffefb47b9b1ce9212658728b1dddb1067e76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 10:36:57 -0500

    "mv: look super/few_hand_or_fact/call_small_case_for_large_number/see_own_point/same_time.py, super/man/fact/have_point_on_man/case/seem_case.py or super/see_young_hand/do_woman_about_child/ask_different_work/young_year_and_young_man/come_good_way.py under last government"

---
+++ super/man/fact/have_point_on_man/case/seem_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    long_government_and_next_day(str_arg)
+    print('life')
+
+def long_government_and_next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('say_day')
