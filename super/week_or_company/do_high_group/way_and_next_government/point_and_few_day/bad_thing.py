112f8801c0ffc591eb1ffa50ada45565a5460dd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 10:50:34 -0500

    "mv: try super/see_new_eye_to_number/case.py, super/get_old_work/place/day_or_little_day/life.py, super/say_public_person_for_case.py or super/week_or_company/do_high_group/way_and_next_government/point_and_few_day/bad_thing.py at long group"

---
+++ super/week_or_company/do_high_group/way_and_next_government/point_and_few_day/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life_and_big_world(str_arg):
+    company_and_day(str_arg)
+    print('year_or_child')
+
+def company_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life_and_big_world('early_work_or_next_fact')
