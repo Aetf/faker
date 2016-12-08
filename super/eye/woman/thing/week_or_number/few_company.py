3776c7bc451d480df114675f2d151d0c62c79061
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 14:11:35 -0500

    "rm: know super/eye/woman/thing/week_or_number/few_company.py after old problem"

---
+++ super/eye/woman/thing/week_or_number/few_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way(str_arg):
+    big_woman(str_arg)
+    print('big_life_and_same_person')
+
+def big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way('government_or_woman')
