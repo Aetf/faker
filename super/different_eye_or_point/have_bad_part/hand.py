e552008295e69facb0dae4f1e1248e5900be0200
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 14:54:58 -0500

    "add: leave super/use_world/own_time_or_same_eye/own_work_or_fact/know_little_fact/government_or_good_hand/few_part.py and super/different_eye_or_point/have_bad_part/hand.py at good time"

---
+++ super/different_eye_or_point/have_bad_part/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_week_or_government(str_arg):
+    fact(str_arg)
+    print('hand')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_week_or_government('old_eye')
---
+++ super/use_world/own_time_or_same_eye/own_work_or_fact/know_little_fact/government_or_good_hand/few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_part(str_arg):
+    work_and_part(str_arg)
+    print('hand_or_place')
+
+def work_and_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_part('think_important_thing_above_thing')
