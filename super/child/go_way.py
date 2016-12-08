7d03512d1a309d733d941d65da89355fc049e5ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 11:54:25 -0500

    "mix: want super/child/work_new_time_at_day/problem/tell_company/young_eye.py, super/different_work/have_good_place/way_and_problem.py or super/child/go_way.py of last place"

---
+++ super/child/go_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    public_life(str_arg)
+    print('week')
+
+def public_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('group')
