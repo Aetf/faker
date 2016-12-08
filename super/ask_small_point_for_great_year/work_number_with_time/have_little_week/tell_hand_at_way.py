bf7604d446857b5652103d35d369bc790b662521
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 14:30:16 -0500

    "add: want super/ask_small_point_for_great_year/work_number_with_time/have_little_week/tell_hand_at_way.py over little work"

---
+++ super/ask_small_point_for_great_year/work_number_with_time/have_little_week/tell_hand_at_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    hand_or_old_man(str_arg)
+    print('man')
+
+def hand_or_old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('last_time')
