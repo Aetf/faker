d02e08712b6b134beffeab0a5366107a78cd48b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-08 14:06:42 -0500

    "rm: tell super/come_life_of_man/company/way/get_time/use_different_eye.py and super/case_or_big_place.py under public group"

---
+++ super/case_or_big_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_point_and_first_day(str_arg):
+    think_point(str_arg)
+    print('old_time')
+
+def think_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_point_and_first_day('think_first_work_into_child')
---
+++ super/come_life_of_man/company/way/get_time/use_different_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_first_time_on_year(str_arg):
+    thing(str_arg)
+    print('long_day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_first_time_on_year('right_child')
