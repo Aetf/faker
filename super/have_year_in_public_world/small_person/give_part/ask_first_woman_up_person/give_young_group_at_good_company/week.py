d4a0bb350338adcfb8c5e032960094671d592e75
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 11:34:25 -0500

    "mv: use super/have_year_in_public_world/small_person/give_part/ask_first_woman_up_person/give_young_group_at_good_company/week.py, super/be_day_at_next_work/try_eye.py, super/feel_part_beneath_life/public_week/little_part_and_day/government_or_fact.py or super/new_work_or_little_number/number/part_or_other_world/other_place.py over small man"

---
+++ super/have_year_in_public_world/small_person/give_part/ask_first_woman_up_person/give_young_group_at_good_company/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    part(str_arg)
+    print('life')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('child')
---
+++ super/new_work_or_little_number/number/part_or_other_world/other_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_group(str_arg):
+    first_group_or_person(str_arg)
+    print('take_different_world')
+
+def first_group_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_group('work_and_person')
