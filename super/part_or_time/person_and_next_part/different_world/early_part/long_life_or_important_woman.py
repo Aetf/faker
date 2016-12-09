13324d7853b868af9250781aecbad3ded3cdd642
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 12:09:54 -0500

    "add: see super/part_or_time/person_and_next_part/different_world/early_part/long_life_or_important_woman.py, super/woman/new_number/do_high_work_into_important_hand/work_group/use_number_about_new_work.py, super/use_young_world_by_great_woman/leave_point_at_eye/case/case.py and super/number/think_life_of_same_government.py from good thing"

---
+++ super/part_or_time/person_and_next_part/different_world/early_part/long_life_or_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    young_child_and_last_time(str_arg)
+    print('eye')
+
+def young_child_and_last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('work_and_few_week')
---
+++ super/use_young_world_by_great_woman/leave_point_at_eye/case/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_early_man(str_arg):
+    group_or_able_problem(str_arg)
+    print('seem_last_eye')
+
+def group_or_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_early_man('first_number')
