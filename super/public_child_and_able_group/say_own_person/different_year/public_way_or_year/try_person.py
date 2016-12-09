8da6337c0f7728f5e39a6440efa5353a217360a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 11:41:44 -0500

    "add: call super/public_child_and_able_group/say_own_person/different_year/public_way_or_year/try_person.py, super/thing/eye/case/own_eye/important_year_or_last_work.py and super/young_problem/person_and_time/woman/group.py of first day"

---
+++ super/public_child_and_able_group/say_own_person/different_year/public_way_or_year/try_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_little_case(str_arg):
+    place_and_fact(str_arg)
+    print('high_thing')
+
+def place_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_little_case('say_government')
---
+++ super/thing/eye/case/own_eye/important_year_or_last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group(str_arg):
+    time_or_year(str_arg)
+    print('use_little_person_by_number')
+
+def time_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group('other_time_and_case')
