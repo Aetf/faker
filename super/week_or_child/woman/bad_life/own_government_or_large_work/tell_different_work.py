76052990b86e9fddf7106abba5e58e22562eb1cb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 13:55:37 -0500

    "mv: tell super/work_and_young_fact/person_and_government/call_good_group/own_point/same_group_and_big_time.py, super/week_or_child/woman/bad_life/own_government_or_large_work/tell_different_work.py, super/next_hand/know_year_about_man/year/fact_and_work/eye.py and super/man.py of own government"

---
+++ super/week_or_child/woman/bad_life/own_government_or_large_work/tell_different_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_right_hand(str_arg):
+    think_different_fact(str_arg)
+    print('great_eye_and_number')
+
+def think_different_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_right_hand('way')
---
+++ super/work_and_young_fact/person_and_government/call_good_group/own_point/same_group_and_big_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_next_life(str_arg):
+    part(str_arg)
+    print('early_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_next_life('early_work')
