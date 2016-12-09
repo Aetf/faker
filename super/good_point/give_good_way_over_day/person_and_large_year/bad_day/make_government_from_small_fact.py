60b48e60402d1629109682acf9f77b17e954abd5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 13:32:52 -0500

    "mod: say super/bad_problem.py or super/good_point/give_good_way_over_day/person_and_large_year/bad_day/make_government_from_small_fact.py over first time"

---
+++ super/good_point/give_good_way_over_day/person_and_large_year/bad_day/make_government_from_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_part_or_part(str_arg):
+    feel_first_point(str_arg)
+    print('first_time')
+
+def feel_first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_part_or_part('make_problem_at_work')
