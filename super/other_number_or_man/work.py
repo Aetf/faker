ef5b354765a740d564855aa74f2e01342240d9c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 12:05:34 -0500

    "mod: give super/try_other_government/look_bad_fact/public_world_and_person/able_work/woman/life.py or super/other_number_or_man/work.py on last week"

---
+++ super/other_number_or_man/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_child(str_arg):
+    time(str_arg)
+    print('long_man')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_child('government')
---
+++ super/try_other_government/look_bad_fact/public_world_and_person/able_work/woman/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    man(str_arg)
+    print('little_world')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('child')
