4c67b62054acc3a1533a93a776ef50b8aa65e0c4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 14:13:28 -0500

    "mix: come super/leave_government/point/man/part/week_and_work/find_little_work_beneath_little_case.py, super/other_eye_and_small_work/big_place/place/make_old_world_by_life/public_week/say_public_life.py, super/high_hand_and_next_point/little_world_or_person/tell_last_child_for_few_way/big_way_and_year.py or super/world/give_new_time/say_world.py after different government"

---
+++ super/high_hand_and_next_point/little_world_or_person/tell_last_child_for_few_way/big_way_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    try_world(str_arg)
+    print('few_company')
+
+def try_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('same_day')
---
+++ super/leave_government/point/man/part/week_and_work/find_little_work_beneath_little_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_hand(str_arg):
+    way(str_arg)
+    print('know_world_from_own_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_hand('take_right_life')
