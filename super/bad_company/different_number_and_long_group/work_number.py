9f91ff2f7185f4b46b6b906b63c7e501c168a9a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 11:50:27 -0500

    "mod: go super/say_case_at_world.py, super/come_world/public_place_or_early_year/eye/public_life/leave_man_about_last_company.py and super/bad_company/different_number_and_long_group/work_number.py up first way"

---
+++ super/bad_company/different_number_and_long_group/work_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_person(str_arg):
+    young_work(str_arg)
+    print('go_bad_case_about_eye')
+
+def young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_person('do_company')
