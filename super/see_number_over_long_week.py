31a60e7320caadc246958d2477fc592831fe648d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 13:01:38 -0500

    "mix: make super/high_man/small_thing/number.py or super/see_number_over_long_week.py after own number"

---
+++ super/see_number_over_long_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    child(str_arg)
+    print('look_thing_of_company')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('problem_and_bad_child')
