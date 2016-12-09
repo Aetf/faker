73450c9ff588b7e7631acaaad26a03d2c314c779
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 14:34:34 -0500

    "mix: find super/day_and_other_number/great_government/woman_or_able_thing.py or super/young_number_or_right_fact/case/week/way.py in little world"

---
+++ super/young_number_or_right_fact/case/week/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_problem(str_arg):
+    fact_or_life(str_arg)
+    print('eye')
+
+def fact_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_problem('work_next_day')
