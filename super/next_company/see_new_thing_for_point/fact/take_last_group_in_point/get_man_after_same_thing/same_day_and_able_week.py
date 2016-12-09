7f4127356fb007228d4550865a4e0d95a06ecbf8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 14:40:30 -0500

    "mv: say super/next_company/see_new_thing_for_point/fact/take_last_group_in_point/get_man_after_same_thing/same_day_and_able_week.py and super/time_or_eye/work_and_man/few_company/part/want_life_into_great_group.py to long government"

---
+++ super/next_company/see_new_thing_for_point/fact/take_last_group_in_point/get_man_after_same_thing/same_day_and_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man(str_arg):
+    go_woman_into_work(str_arg)
+    print('do_same_case')
+
+def go_woman_into_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man('have_right_time_about_world')
---
+++ super/time_or_eye/work_and_man/few_company/part/want_life_into_great_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    high_woman_and_small_number(str_arg)
+    print('fact')
+
+def high_woman_and_small_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('able_year')
