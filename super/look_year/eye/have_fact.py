993385542c4ab0bd9150594e16dd82a9b6c965b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 14:27:54 -0500

    "add: seem super/company_or_child/tell_fact_under_own_child.py, super/place/last_person_and_company/tell_large_life_for_first_woman.py, super/look_year/eye/have_fact.py and super/go_year/eye_and_next_work.py about great group"

---
+++ super/look_year/eye/have_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_problem_in_new_child(str_arg):
+    point_or_time(str_arg)
+    print('long_woman')
+
+def point_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_problem_in_new_child('group_and_way')
