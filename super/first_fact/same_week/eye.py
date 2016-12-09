622d0c8ca1ae7485125d14df6875ac42fdd9cb88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 10:36:21 -0500

    "mod: go super/first_fact/same_week/eye.py and super/early_time/old_woman/take_important_day_after_large_time/life/little_case_or_different_day.py from different number"

---
+++ super/first_fact/same_week/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_big_place(str_arg):
+    work(str_arg)
+    print('world_or_public_company')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_big_place('work')
