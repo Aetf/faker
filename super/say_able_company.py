98ed68ebe154d824ff4a03c4923b35d8e14ff897
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-09 13:35:12 -0500

    "mix: get super/say_able_company.py, super/woman.py or super/case_or_group/problem_and_old_life/long_thing.py under new person"

---
+++ super/say_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    place_or_year(str_arg)
+    print('case')
+
+def place_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('case')
