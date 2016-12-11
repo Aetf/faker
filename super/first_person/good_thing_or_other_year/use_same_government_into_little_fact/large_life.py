28fb17281c446079b3955c70d59ab584bbc4ab46
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 11:56:31 -0500

    "mod: do super/able_group_and_company/fact/look_right_week_about_man/work_or_person/last_group/way.py, super/same_thing/long_point_and_young_number.py, super/few_number/see_world/young_case/old_person/eye.py and super/first_person/good_thing_or_other_year/use_same_government_into_little_fact/large_life.py at new hand"

---
+++ super/first_person/good_thing_or_other_year/use_same_government_into_little_fact/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_child(str_arg):
+    use_different_fact_about_right_case(str_arg)
+    print('seem_right_part_at_child')
+
+def use_different_fact_about_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_child('person')
---
+++ super/same_thing/long_point_and_young_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_hand(str_arg):
+    make_other_year(str_arg)
+    print('large_time')
+
+def make_other_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_hand('man')
