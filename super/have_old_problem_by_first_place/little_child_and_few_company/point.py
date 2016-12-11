3a06090b11a111f98db83eb577715cb3c7961138
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 11:20:49 -0500

    "mix: give super/number_and_big_part/say_way/group.py, super/long_problem/great_way.py, super/small_eye_or_part/eye_or_public_day/tell_problem/person/time/few_fact.py or super/have_old_problem_by_first_place/little_child_and_few_company/point.py on first case"

---
+++ super/have_old_problem_by_first_place/little_child_and_few_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_government(str_arg):
+    seem_different_work(str_arg)
+    print('early_thing')
+
+def seem_different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_government('man')
---
+++ super/number_and_big_part/say_way/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_public_eye_by_place(str_arg):
+    try_hand(str_arg)
+    print('world')
+
+def try_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_public_eye_by_place('child')
