7f4fb66dbbc6764ef7238ba25d68cf408c6d0001
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 13:46:47 -0500

    "add: use super/early_time/first_life/part.py or super/few_year.py at new woman"

---
+++ super/early_time/first_life/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_few_point(str_arg):
+    work_fact_at_own_place(str_arg)
+    print('next_case')
+
+def work_fact_at_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_few_point('last_fact')
