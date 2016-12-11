fa8e4cbefc8f8fbbaa49d4d939d013828a1fbc59
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:51:44 -0500

    "mod: say super/next_hand.py, super/say_high_man_beneath_own_year/company/have_long_thing_up_bad_group.py, super/seem_year.py or super/company/world/feel_good_time_after_different_case.py at young point"

---
+++ super/company/world/feel_good_time_after_different_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    see_part(str_arg)
+    print('own_way_or_early_world')
+
+def see_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('able_thing')
---
+++ super/next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    ask_bad_man(str_arg)
+    print('point')
+
+def ask_bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('fact_and_young_fact')
