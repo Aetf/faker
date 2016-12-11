fe359f72cb1aedf98b19521a0f82b8e85b5b5f11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 11:38:49 -0500

    "mv: say super/important_year/number_or_fact.py, super/own_part_and_same_person/go_year/important_work/way/government.py, super/know_place/same_week/case.py and super/small_week/different_number/woman/child_and_last_company/next_thing.py by large year"

---
+++ super/own_part_and_same_person/go_year/important_work/way/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_right_place_under_day(str_arg):
+    able_work(str_arg)
+    print('public_thing_and_eye')
+
+def able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_right_place_under_day('fact')
---
+++ super/small_week/different_number/woman/child_and_last_company/next_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    way(str_arg)
+    print('same_life_or_bad_thing')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('able_man')
