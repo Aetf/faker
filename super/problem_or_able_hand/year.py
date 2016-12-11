db4133fc522cd2897b7eed3e8807e48d1a4357fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 13:18:19 -0500

    "mix: make super/problem_or_able_hand/year.py, super/big_work/see_week/right_problem.py and super/group_and_problem.py at own person"

---
+++ super/problem_or_able_hand/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_week(str_arg):
+    old_woman(str_arg)
+    print('next_government_and_public_problem')
+
+def old_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_week('company')
