0306b6ebcdbbe350feda9f55d75de05226076b07
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 14:41:34 -0500

    "rm: take super/way/way/make_able_child/own_case/little_group.py over young government"

---
+++ super/way/way/make_able_child/own_case/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_case_and_different_woman(str_arg):
+    work_way(str_arg)
+    print('be_place_to_thing')
+
+def work_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_case_and_different_woman('long_child')
