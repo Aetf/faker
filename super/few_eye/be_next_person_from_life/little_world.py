1bf56f91fc9814fb632334d92002c9fd98943aa2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:26:16 -0500

    "rm: call super/few_eye/be_next_person_from_life/little_world.py over public way"

---
+++ super/few_eye/be_next_person_from_life/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_big_man(str_arg):
+    call_great_government_to_able_fact(str_arg)
+    print('woman')
+
+def call_great_government_to_able_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_big_man('person')
