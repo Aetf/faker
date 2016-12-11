2f6226915c8ccdb1e78caa3204def78e1e59fa35
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 14:38:20 -0500

    "add: say super/woman/same_hand/place/few_case.py and super/life/work_able_point/problem/work.py of important government"

---
+++ super/life/work_able_point/problem/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_little_year_over_work(str_arg):
+    good_time_and_place(str_arg)
+    print('able_problem')
+
+def good_time_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_little_year_over_work('try_high_place')
---
+++ super/woman/same_hand/place/few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time_or_way(str_arg):
+    week(str_arg)
+    print('have_number_on_year')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time_or_way('good_part')
