41cbec936c51c9416943554d585332934831f40c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:58:06 -0500

    "mod: tell super/case_or_early_man/right_day/new_number/take_life.py and super/world_or_small_life/life_or_early_day.py in first person"

---
+++ super/case_or_early_man/right_day/new_number/take_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_part(str_arg):
+    large_government_or_day(str_arg)
+    print('small_problem')
+
+def large_government_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_part('fact')
