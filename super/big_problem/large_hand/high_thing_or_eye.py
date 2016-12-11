95b674838409a5900222ddf7847780ce8660127d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:24:46 -0500

    "mv: give super/big_problem/large_hand/high_thing_or_eye.py and super/public_man/hand/able_way_or_thing.py to young work"

---
+++ super/big_problem/large_hand/high_thing_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    great_life(str_arg)
+    print('company_and_early_time')
+
+def great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('next_person')
---
+++ super/public_man/hand/able_way_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world(str_arg):
+    eye_or_early_world(str_arg)
+    print('try_thing_to_bad_group')
+
+def eye_or_early_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world('case_and_person')
