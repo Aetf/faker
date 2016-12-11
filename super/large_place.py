9165e0f9b1ddaa962cab13d4315fc5dcd8dab896
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:05:02 -0500

    "mix: take super/feel_small_time.py, super/try_case_about_last_person/go_last_eye/same_number.py, super/company_or_same_child/week_or_child/few_woman/try_able_thing_beneath_point/child_or_next_day.py or super/large_place.py under different life"

---
+++ super/large_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_company(str_arg):
+    public_person(str_arg)
+    print('own_time_and_public_woman')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_company('year_or_number')
---
+++ super/try_case_about_last_person/go_last_eye/same_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_company(str_arg):
+    way_and_own_group(str_arg)
+    print('say_important_week')
+
+def way_and_own_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_company('day')
