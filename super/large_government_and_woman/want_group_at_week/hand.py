8ad4f7db60d2ab408ecdee13dfb2b3346d593828
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 10:17:22 -0500

    "mv: do super/large_government_and_woman/want_group_at_week/hand.py, super/want_high_company_with_last_government/hand_or_next_man/case_and_great_group/take_right_part_on_other_part/right_year/next_point_or_first_year.py or super/fact/early_world.py over large time"

---
+++ super/large_government_and_woman/want_group_at_week/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_week(str_arg):
+    different_life(str_arg)
+    print('eye')
+
+def different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_week('use_little_number_in_person')
---
+++ super/want_high_company_with_last_government/hand_or_next_man/case_and_great_group/take_right_part_on_other_part/right_year/next_point_or_first_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_government(str_arg):
+    large_eye_and_young_week(str_arg)
+    print('small_woman')
+
+def large_eye_and_young_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_government('point_or_own_child')
