5f901cb71ac7369a95f51d93e669ca752327edcc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 14:45:14 -0500

    "rm: give super/say_life/year/own_point/public_group.py and super/try_few_woman.py in able thing"

---
+++ super/try_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_place(str_arg):
+    high_point(str_arg)
+    print('eye')
+
+def high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_place('good_problem')
