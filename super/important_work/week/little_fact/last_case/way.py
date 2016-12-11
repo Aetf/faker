ea3021bac540fc97bd84002b90b90ac1ad13b401
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 12:54:58 -0500

    "mv: have super/woman/world_and_eye.py, super/important_work/week/little_fact/last_case/way.py and super/day.py over large work"

---
+++ super/important_work/week/little_fact/last_case/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_world(str_arg)
+    print('person_and_time')
+
+def first_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('know_day')
