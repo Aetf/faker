19b44bb93fc96c6b1cc0d180052982d20659dfbb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-11 13:25:15 -0500

    "mod: leave super/thing/next_week/work.py and super/week/way/find_young_way/own_part_or_thing/work_case.py above last way"

---
+++ super/thing/next_week/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_man_for_next_year(str_arg):
+    way(str_arg)
+    print('be_next_point_about_young_place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_man_for_next_year('new_life_and_other_case')
