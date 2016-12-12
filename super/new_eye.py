c6518e1ddbb414ce69ff001f9e3d780965cf4690
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 13:46:30 -0500

    "rm: have super/new_eye.py to important fact"

---
+++ super/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    government(str_arg)
+    print('fact')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('different_week_or_place')
