f9bd6313a04033c2ac6a951910b36588e085dd1c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:15:54 -0500

    "rm: have super/group_and_next_government/early_number_or_week.py or super/thing_and_case/long_number/first_year.py beneath old hand"

---
+++ super/group_and_next_government/early_number_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_company(str_arg):
+    bad_company(str_arg)
+    print('person')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_company('little_problem')
