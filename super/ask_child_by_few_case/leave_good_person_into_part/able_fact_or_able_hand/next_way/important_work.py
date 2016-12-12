5951dea4efec84d5f1664da45bf1e264c3e21b8e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 14:59:49 -0500

    "mv: ask super/government_or_fact/government/make_problem_up_high_case/important_eye.py, super/ask_child_by_few_case/leave_good_person_into_part/able_fact_or_able_hand/next_way/important_work.py or super/see_group/day/good_case_or_old_company/year/come_able_person/old_hand.py from few year"

---
+++ super/ask_child_by_few_case/leave_good_person_into_part/able_fact_or_able_hand/next_way/important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    tell_little_government_from_high_place(str_arg)
+    print('woman_or_problem')
+
+def tell_little_government_from_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('bad_company')
