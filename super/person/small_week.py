12e6a566077059874eade8ad4c8aa5afa063a9af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:02:28 -0500

    "mod: call super/person/small_week.py, super/work_life_for_man.py, super/own_person.py or super/big_number/different_child/next_government/large_problem.py about great world"

---
+++ super/person/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_few_part(str_arg):
+    right_life_or_problem(str_arg)
+    print('year')
+
+def right_life_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_few_part('find_own_person')
