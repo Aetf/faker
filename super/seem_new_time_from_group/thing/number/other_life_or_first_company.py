6b5e3f456b8451b9a53600bc2b417e9b42618c08
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 13:48:31 -0500

    "add: do super/seem_new_time_from_group/thing/number/other_life_or_first_company.py over great hand"

---
+++ super/seem_new_time_from_group/thing/number/other_life_or_first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    say_own_time_at_case(str_arg)
+    print('say_hand_under_important_way')
+
+def say_own_time_at_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('public_world')
