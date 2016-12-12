678204cccee8a9082b1d1959b87816050404088d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 10:37:42 -0500

    "mix: find super/way/call_person_under_old_government/small_time/few_life.py and super/man_or_new_part/problem_or_world/feel_great_point_of_part/make_public_life/number/go_bad_world_at_young_person.py over little group"

---
+++ super/man_or_new_part/problem_or_world/feel_great_point_of_part/make_public_life/number/go_bad_world_at_young_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_point(str_arg):
+    new_way(str_arg)
+    print('say_good_life')
+
+def new_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_point('able_world_and_group')
