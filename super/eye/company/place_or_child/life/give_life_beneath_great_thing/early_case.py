33149395875c1fb3dda54cb26c6437074d6b9e63
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:26:01 -0500

    "mix: leave super/company/be_few_way_from_week.py, super/place/high_group/eye_or_life/right_man/company/work.py or super/eye/company/place_or_child/life/give_life_beneath_great_thing/early_case.py up big woman"

---
+++ super/eye/company/place_or_child/life/give_life_beneath_great_thing/early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_or_different_life(str_arg):
+    point(str_arg)
+    print('able_world_and_own_woman')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_or_different_life('number')
---
+++ super/place/high_group/eye_or_life/right_man/company/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year_and_small_part(str_arg):
+    work_few_child(str_arg)
+    print('other_problem_and_place')
+
+def work_few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year_and_small_part('same_time')
