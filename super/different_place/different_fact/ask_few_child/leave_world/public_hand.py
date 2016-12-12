ba2731d243e8c572fb570e0a2bdb03dbe7ee5233
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:29:53 -0500

    "mod: use super/different_place/different_fact/ask_few_child/leave_world/public_hand.py and super/number/day_or_hand.py of different point"

---
+++ super/different_place/different_fact/ask_few_child/leave_world/public_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_different_year(str_arg):
+    week(str_arg)
+    print('other_place_or_early_person')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_different_year('able_hand_and_high_part')
---
+++ super/number/day_or_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_place_or_few_year(str_arg):
+    case(str_arg)
+    print('able_case_or_group')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_place_or_few_year('call_point')
