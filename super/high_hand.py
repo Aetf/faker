b1cfb1dad0a89c6ba4ef75c204b4781f00c60982
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 14:32:10 -0500

    "mix: find super/different_thing_or_eye.py and super/high_hand.py into great life"

---
+++ super/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    world_and_few_eye(str_arg)
+    print('come_company')
+
+def world_and_few_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('place_and_eye')
