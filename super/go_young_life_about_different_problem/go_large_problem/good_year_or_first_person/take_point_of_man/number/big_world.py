b3071b7c5954b3e01592d7751ebcdc694c88f9cf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:06:30 -0500

    "mix: come super/work_and_child/part/other_man/number.py, super/say_man_after_small_company/big_fact.py and super/go_young_life_about_different_problem/go_large_problem/good_year_or_first_person/take_point_of_man/number/big_world.py into little work"

---
+++ super/go_young_life_about_different_problem/go_large_problem/good_year_or_first_person/take_point_of_man/number/big_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_point(str_arg):
+    time(str_arg)
+    print('make_good_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_point('young_case')
---
+++ super/say_man_after_small_company/big_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    same_hand_and_little_part(str_arg)
+    print('way')
+
+def same_hand_and_little_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('time_and_time')
