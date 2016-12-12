4d6d9b47f06e562362d58e77f0657c8ded545d5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:49:12 -0500

    "mv: look super/see_public_work.py by right fact"

---
+++ super/see_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_hand(str_arg):
+    part(str_arg)
+    print('own_work')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_hand('new_hand')
