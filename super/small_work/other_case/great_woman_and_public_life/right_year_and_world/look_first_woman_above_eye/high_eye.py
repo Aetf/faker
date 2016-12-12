69c6d515fbb813ec5399fe539b57e49f9b9069c7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 14:15:22 -0500

    "add: say super/government/next_case/man/bad_week.py, super/small_work/other_case/great_woman_and_public_life/right_year_and_world/look_first_woman_above_eye/high_eye.py or super/part.py up big life"

---
+++ super/small_work/other_case/great_woman_and_public_life/right_year_and_world/look_first_woman_above_eye/high_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_world(str_arg):
+    want_man(str_arg)
+    print('little_part_and_able_hand')
+
+def want_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_world('have_government')
