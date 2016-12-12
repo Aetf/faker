89cf9d64180fb7d1d948c084e67e63ccaf6c2c57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:29:02 -0500

    "rm: tell super/next_part_and_last_place/long_work/give_good_eye/able_time/public_woman.py, super/fact/tell_able_year/way_or_time/say_different_case_under_able_company/day/early_thing.py or super/make_new_woman/hand_or_high_work.py of early life"

---
+++ super/fact/tell_able_year/way_or_time/say_different_case_under_able_company/day/early_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_point(str_arg)
+    print('large_group')
+
+def first_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('important_eye_or_woman')
---
+++ super/next_part_and_last_place/long_work/give_good_eye/able_time/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_time_or_great_time(str_arg):
+    government_and_young_work(str_arg)
+    print('give_number')
+
+def government_and_young_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_time_or_great_time('case')
