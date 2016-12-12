437728b1ff7ee365cf34e4b5b7fc1782e2b59c71
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 13:17:50 -0500

    "rm: work super/use_public_day_of_hand/world/first_fact/leave_important_government_at_time.py and super/get_next_way/person/point_and_work/other_woman_or_person/use_next_year_above_company/get_place_at_good_work.py by young work"

---
+++ super/get_next_way/person/point_and_work/other_woman_or_person/use_next_year_above_company/get_place_at_good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_man(str_arg):
+    day(str_arg)
+    print('early_company_or_eye')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_man('company')
