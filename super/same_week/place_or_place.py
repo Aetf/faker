ea2e7dc65b9cd5c4a6b8fae734bc54bf19d60a3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:39:50 -0500

    "rm: work super/same_week/place_or_place.py in old place"

---
+++ super/same_week/place_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_point(str_arg):
+    way_and_child(str_arg)
+    print('world_and_big_day')
+
+def way_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_point('great_eye')
