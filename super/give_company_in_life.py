f1fecac43e5ca8c4cc83ed356f76683b265e2975
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 12:57:20 -0500

    "rm: get super/bad_way/seem_right_company/part/try_young_fact/woman.py and super/give_company_in_life.py up old place"

---
+++ super/give_company_in_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_part(str_arg):
+    day_and_week(str_arg)
+    print('problem')
+
+def day_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_part('same_problem')
