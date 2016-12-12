31731ee0a0c261e4626630e8b64f951004ee22d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:45:53 -0500

    "add: ask super/world/place/case/eye/next_hand_or_same_hand.py in large government"

---
+++ super/world/place/case/eye/next_hand_or_same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_number(str_arg):
+    part(str_arg)
+    print('last_day')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_number('go_work')
