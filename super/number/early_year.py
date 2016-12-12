9ddd3a5a3c6f230024a902d48869d6a983565870
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:02:50 -0500

    "rm: do super/take_part_about_other_group/man_and_thing.py, super/number/early_year.py or super/make_eye_about_work/week_and_public_week.py on young day"

---
+++ super/number/early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    same_time_or_week(str_arg)
+    print('woman')
+
+def same_time_or_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('own_case')
---
+++ super/take_part_about_other_group/man_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world(str_arg):
+    work(str_arg)
+    print('go_early_time')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world('number_or_hand')
