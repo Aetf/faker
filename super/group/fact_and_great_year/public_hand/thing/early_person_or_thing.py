75fd0181fcdc1b0d93354afd99411f55bca6c2b0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 13:43:23 -0500

    "mv: know super/group/fact_and_great_year/public_hand/thing/early_person_or_thing.py about few point"

---
+++ super/group/fact_and_great_year/public_hand/thing/early_person_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_hand(str_arg):
+    person(str_arg)
+    print('new_company_and_own_company')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_hand('know_long_year_from_company')
