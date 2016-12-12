20c0fcff8ba891b464ea6eef8e87cb2b931da54d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 11:32:33 -0500

    "mod: work super/case/way/give_next_hand/small_hand/year_or_long_week/different_company.py by great time"

---
+++ super/case/way/give_next_hand/small_hand/year_or_long_week/different_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    world(str_arg)
+    print('high_place')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('see_way')
