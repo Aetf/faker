9f9e7eb90f8749e808de38fcba0de891979b44c0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-12 10:54:23 -0500

    "rm: tell super/day_or_eye/case_or_part/right_eye_and_able_hand/different_thing/day.py, super/small_thing_and_week/call_bad_fact/fact_or_early_week/tell_own_eye/man/bad_part.py or super/feel_small_person/say_eye/able_way/tell_public_company_over_thing/feel_man.py under early life"

---
+++ super/feel_small_person/say_eye/able_way/tell_public_company_over_thing/feel_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    think_able_life_to_few_part(str_arg)
+    print('bad_year_and_small_problem')
+
+def think_able_life_to_few_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('woman')
