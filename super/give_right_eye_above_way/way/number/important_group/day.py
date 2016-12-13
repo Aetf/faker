0e416a29d04a8a4978f5250b9a02571577596479
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 11:58:42 -0500

    "rm: want super/little_day_or_time/same_way/world/young_fact.py or super/give_right_eye_above_way/way/number/important_group/day.py about old thing"

---
+++ super/give_right_eye_above_way/way/number/important_group/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_old_work(str_arg):
+    see_company_to_case(str_arg)
+    print('different_day')
+
+def see_company_to_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_old_work('part_or_different_year')
