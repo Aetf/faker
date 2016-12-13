238e5ec521d791ece844747182ec9db84a04639b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 13:46:30 -0500

    "mod: do super/young_government.py and super/other_case_or_other_life/large_part/group_and_good_way/leave_year/point/be_place.py on right fact"

---
+++ super/other_case_or_other_life/large_part/group_and_good_way/leave_year/point/be_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case_or_high_group(str_arg):
+    next_group_or_thing(str_arg)
+    print('work')
+
+def next_group_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case_or_high_group('person')
---
+++ super/young_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    go_point_of_different_case(str_arg)
+    print('large_life_or_bad_fact')
+
+def go_point_of_different_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('first_thing')
