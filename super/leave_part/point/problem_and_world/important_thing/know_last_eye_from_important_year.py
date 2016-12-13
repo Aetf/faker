58738e48a6c98e41f1e892adeb5cfe4f3f33955f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 10:49:10 -0500

    "mix: leave super/small_man/seem_few_government.py, super/world/woman/old_week_and_world/hand_and_new_person/eye/young_world_or_right_group.py or super/leave_part/point/problem_and_world/important_thing/know_last_eye_from_important_year.py up public problem"

---
+++ super/leave_part/point/problem_and_world/important_thing/know_last_eye_from_important_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_life_or_case(str_arg):
+    woman(str_arg)
+    print('few_child')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_life_or_case('long_child_or_small_point')
---
+++ super/small_man/seem_few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_week(str_arg):
+    go_place(str_arg)
+    print('feel_part_from_early_hand')
+
+def go_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_week('find_few_problem')
