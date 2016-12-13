7c75d457b6a9e61ecc560df1d3eb97244e7e256c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 10:05:40 -0500

    "add: call super/case_or_right_point/place/early_place/company_or_public_work.py or super/large_child/bad_number/group/part_or_old_thing/year/last_person_and_own_life.py to last company"

---
+++ super/case_or_right_point/place/early_place/company_or_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_time_at_place(str_arg):
+    few_group(str_arg)
+    print('give_early_place_about_own_eye')
+
+def few_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_time_at_place('tell_child_above_good_problem')
---
+++ super/large_child/bad_number/group/part_or_old_thing/year/last_person_and_own_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_person(str_arg):
+    get_able_woman(str_arg)
+    print('able_problem')
+
+def get_able_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_person('week')
