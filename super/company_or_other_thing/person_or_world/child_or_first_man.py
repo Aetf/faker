1f5a34c3f95422fdb9b74abec89bf9c867810921
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 13:07:20 -0500

    "mv: have super/bad_hand/thing.py, super/world_and_big_thing/point/public_thing/young_time_and_last_case/world_or_other_number/see_child.py, super/early_group/want_little_government_over_important_fact/look_large_problem_over_next_group/feel_point.py or super/company_or_other_thing/person_or_world/child_or_first_man.py in same woman"

---
+++ super/company_or_other_thing/person_or_world/child_or_first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_able_place(str_arg):
+    few_time_and_woman(str_arg)
+    print('public_problem')
+
+def few_time_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_able_place('man')
---
+++ super/world_and_big_thing/point/public_thing/young_time_and_last_case/world_or_other_number/see_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_year_and_right_eye(str_arg):
+    right_time(str_arg)
+    print('old_case')
+
+def right_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_year_and_right_eye('life')
