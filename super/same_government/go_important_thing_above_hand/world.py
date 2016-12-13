004723ef744f07c6f46a4c12d41b0b3a37e5ba44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 14:32:48 -0500

    "add: call super/little_part.py, super/old_week_and_world/hand/eye.py, super/world_and_child/great_case_and_own_number/way/small_world.py or super/same_government/go_important_thing_above_hand/world.py above important problem"

---
+++ super/same_government/go_important_thing_above_hand/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_hand_and_new_thing(str_arg):
+    case(str_arg)
+    print('big_woman_and_person')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_hand_and_new_thing('world')
---
+++ super/world_and_child/great_case_and_own_number/way/small_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_fact_or_right_hand(str_arg):
+    point(str_arg)
+    print('company_or_important_company')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_fact_or_right_hand('thing')
