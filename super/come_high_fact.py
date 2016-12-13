8b32201617c04831308eed8926aee5a0350382ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 12:48:14 -0500

    "mod: ask super/work/want_fact.py or super/come_high_fact.py of old company"

---
+++ super/come_high_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_early_group(str_arg):
+    way_or_year(str_arg)
+    print('early_work_or_first_world')
+
+def way_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_early_group('government')
