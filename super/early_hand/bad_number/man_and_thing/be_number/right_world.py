31d1725fdca9375606b1ee1255625d4eb47ee307
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 13:46:17 -0500

    "mod: say super/public_year_and_year.py or super/early_hand/bad_number/man_and_thing/be_number/right_world.py beneath same government"

---
+++ super/early_hand/bad_number/man_and_thing/be_number/right_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_large_government_with_way(str_arg):
+    tell_public_person_beneath_thing(str_arg)
+    print('same_case_or_public_work')
+
+def tell_public_person_beneath_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_large_government_with_way('way_and_other_way')
