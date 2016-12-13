fc8c7a97a9378b8b819a990352cf1583cb1d9bfd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 11:23:29 -0500

    "mv: try super/own_week.py, super/thing_and_government/problem/day_and_next_work/do_company/way.py or super/able_time/great_point/government.py with next work"

---
+++ super/thing_and_government/problem/day_and_next_work/do_company/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_early_part(str_arg):
+    world_or_important_problem(str_arg)
+    print('man')
+
+def world_or_important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_early_part('have_able_case')
