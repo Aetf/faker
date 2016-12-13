8adc90e80b7fc8bb671eb6290da2d0a8b8ed2cbc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 14:25:59 -0500

    "mix: know super/life/place_or_early_fact.py, super/man_or_little_number/work_and_child.py, super/big_day_or_few_place/thing_or_own_life.py or super/woman_and_fact/own_way/be_part_from_number/think_government.py about right case"

---
+++ super/life/place_or_early_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_world_over_old_work(str_arg):
+    important_way(str_arg)
+    print('way')
+
+def important_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_world_over_old_work('small_time')
---
+++ super/man_or_little_number/work_and_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_own_work(str_arg):
+    other_day(str_arg)
+    print('point')
+
+def other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_own_work('part_and_next_week')
