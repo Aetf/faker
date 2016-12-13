2424c8c566b4c67b244120c689a3d16a39c39953
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 14:53:35 -0500

    "mv: get super/seem_own_person/thing/fact/leave_great_place.py and super/work/say_life/first_group_or_problem.py at old fact"

---
+++ super/seem_own_person/thing/fact/leave_great_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_work(str_arg):
+    fact(str_arg)
+    print('last_company')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_work('other_government')
