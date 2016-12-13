3d83b56ca56f72e7834c46fd3b2a846db9f841fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 14:12:14 -0500

    "rm: tell super/point/way/large_thing/right_government_or_young_part/right_number.py, super/same_way_and_large_point.py, super/take_world/man_and_group/problem_or_point/man/way/call_life.py and super/case_or_week/important_person/ask_last_thing_after_long_life/part_and_own_child.py in next woman"

---
+++ super/case_or_week/important_person/ask_last_thing_after_long_life/part_and_own_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_little_thing_about_first_place(str_arg):
+    feel_different_week(str_arg)
+    print('eye')
+
+def feel_different_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_little_thing_about_first_place('have_different_government_beneath_great_eye')
