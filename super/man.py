5edc717110ccc2d2b3e6e3c4ff99ac097c6eb9b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 11:56:58 -0500

    "mv: look super/man.py up different way"

---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_life(str_arg):
+    go_world(str_arg)
+    print('next_thing_or_last_problem')
+
+def go_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_life('work')
