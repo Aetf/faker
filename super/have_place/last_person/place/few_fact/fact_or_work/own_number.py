d4ce30c8535c840f37cacf9d7587049aad7b43de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 14:52:47 -0500

    "mix: come super/have_place/last_person/place/few_fact/fact_or_work/own_number.py or super/woman/ask_thing_from_eye/thing/little_thing/number_and_week.py from bad child"

---
+++ super/have_place/last_person/place/few_fact/fact_or_work/own_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    thing(str_arg)
+    print('hand')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('say_high_place_into_eye')
---
+++ super/woman/ask_thing_from_eye/thing/little_thing/number_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    government(str_arg)
+    print('look_number')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('call_small_man_from_high_woman')
