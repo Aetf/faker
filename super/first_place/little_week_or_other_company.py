060d648a730d9f78daab91bf6fad37a084a97036
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 12:55:48 -0500

    "mod: think super/first_place/little_week_or_other_company.py, super/seem_good_number_at_next_hand/group/way/tell_first_problem_above_young_point/last_world/able_group.py or super/woman_or_person/number_and_new_life.py for long problem"

---
+++ super/first_place/little_week_or_other_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_group(str_arg):
+    do_old_time_into_thing(str_arg)
+    print('work_way_from_early_thing')
+
+def do_old_time_into_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_group('child')
---
+++ super/woman_or_person/number_and_new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    day_and_next_part(str_arg)
+    print('few_case')
+
+def day_and_next_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('able_eye')
