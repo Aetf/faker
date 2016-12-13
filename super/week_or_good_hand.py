059866cb5b6bf131e3a184e4c2f1037d709ae76f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 12:15:09 -0500

    "rm: call super/see_woman/see_week_with_public_company.py or super/week_or_good_hand.py over few number"

---
+++ super/week_or_good_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_company_in_year(str_arg):
+    day(str_arg)
+    print('great_life')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_company_in_year('young_point')
