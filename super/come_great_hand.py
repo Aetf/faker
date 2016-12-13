be5e63130c67f2536b8a624368db5c21f098ffc4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 10:40:10 -0500

    "mix: use super/come_great_hand.py, super/own_woman/child_and_group.py, super/woman/good_way_and_life/little_world/week/right_number.py and super/hand/public_place/important_world/woman.py of different way"

---
+++ super/come_great_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_new_fact(str_arg):
+    little_time(str_arg)
+    print('other_eye')
+
+def little_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_new_fact('point')
---
+++ super/hand/public_place/important_world/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_group(str_arg):
+    new_week_and_fact(str_arg)
+    print('woman')
+
+def new_week_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_group('day')
