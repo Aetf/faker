1806787a898e9ce54458622437c4253e5fbab7e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-13 10:10:29 -0500

    "mod: give super/part_and_next_place/want_little_group/own_time_or_large_problem/work.py in big case"

---
+++ super/part_and_next_place/want_little_group/own_time_or_large_problem/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_man_or_own_hand(str_arg):
+    day(str_arg)
+    print('old_thing')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_man_or_own_hand('important_woman_and_way')
