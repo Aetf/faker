98529b486f45bdc0491f024f52febdb4e86f6408
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 10:02:55 -0500

    "mv: get super/first_problem/old_week/next_woman/little_fact/thing_or_week/high_day.py from same world"

---
+++ super/first_problem/old_week/next_woman/little_fact/thing_or_week/high_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_group(str_arg):
+    different_time(str_arg)
+    print('own_part')
+
+def different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_group('other_week_or_public_child')
