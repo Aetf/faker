e56a80a9de252f42d3bb8a1e20678408ee1a0ee5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 14:33:17 -0500

    "rm: see super/small_way_and_person/work/first_person.py, super/feel_work/high_eye_or_long_way/get_little_thing_from_long_part/public_day_or_week/little_world.py, super/own_time/first_life/be_good_way_for_company/new_part/important_day/go_government.py and super/try_case_on_large_way.py into long problem"

---
+++ super/feel_work/high_eye_or_long_way/get_little_thing_from_long_part/public_day_or_week/little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_work(str_arg):
+    be_person_with_week(str_arg)
+    print('few_group')
+
+def be_person_with_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_work('child')
---
+++ super/own_time/first_life/be_good_way_for_company/new_part/important_day/go_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    try_week_from_fact(str_arg)
+    print('big_year')
+
+def try_week_from_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('early_year')
