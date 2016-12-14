9280f01178db5af04bfcc2c1603b7563f1f8a822
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 12:44:53 -0500

    "mod: give super/day/give_fact/large_way/life.py and super/big_person/small_group/problem.py at public place"

---
+++ super/big_person/small_group/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_time(str_arg):
+    woman(str_arg)
+    print('look_good_week')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_time('try_last_woman')
