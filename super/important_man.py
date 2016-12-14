795c5d24070ee55eff6974f83c319a2d2c990f1e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 10:19:36 -0500

    "mix: take super/few_man/week_and_first_group/make_last_company_of_man/time_and_able_time/public_part_and_last_group.py or super/important_man.py in same person"

---
+++ super/important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work(str_arg):
+    man(str_arg)
+    print('work_last_case')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work('man')
