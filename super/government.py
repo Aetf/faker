73293b3506e694cf3d89f4707cc6672813c9b625
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 12:44:04 -0500

    "mix: work super/different_company_or_week/young_world/thing/long_hand/try_big_fact.py or super/government.py above public number"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_government(str_arg):
+    long_woman(str_arg)
+    print('tell_company_beneath_first_time')
+
+def long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_government('different_part')
