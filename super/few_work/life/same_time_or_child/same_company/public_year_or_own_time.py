920ed0fc9a05b5dec935a0f562c3320c22e93fcf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 10:26:24 -0500

    "mv: leave super/few_work/life/same_time_or_child/same_company/public_year_or_own_time.py, super/eye/look_world_on_point.py and super/great_part/man/have_man_beneath_life/company_and_important_place/big_eye_and_old_child.py into first week"

---
+++ super/few_work/life/same_time_or_child/same_company/public_year_or_own_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_new_week(str_arg):
+    look_person(str_arg)
+    print('year')
+
+def look_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_new_week('call_next_problem_with_company')
