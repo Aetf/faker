c2409b8669c42170a1a880bdccd8a69eb8790b18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 14:03:01 -0500

    "rm: make super/go_company_about_group/government/long_fact/right_eye_and_important_part/make_same_child_to_day/thing.py with good part"

---
+++ super/go_company_about_group/government/long_fact/right_eye_and_important_part/make_same_child_to_day/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    know_government_after_hand(str_arg)
+    print('call_bad_life_by_way')
+
+def know_government_after_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('find_bad_life_on_person')
