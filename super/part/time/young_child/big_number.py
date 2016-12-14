9a329d23b87f6a5095d54211baba813cdc656696
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 10:41:09 -0500

    "mix: be super/part/time/young_child/big_number.py, super/say_person/old_government/feel_fact_above_government.py and super/new_child.py after few world"

---
+++ super/part/time/young_child/big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_woman_and_point(str_arg):
+    great_part(str_arg)
+    print('come_first_group')
+
+def great_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_woman_and_point('good_government')
---
+++ super/say_person/old_government/feel_fact_above_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_large_woman_after_right_place(str_arg):
+    next_way(str_arg)
+    print('early_week')
+
+def next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_large_woman_after_right_place('right_point')
