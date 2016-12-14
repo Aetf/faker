b79ae4d0d917f86c49b23ed2f9d21e080ba6d1c6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 14:23:51 -0500

    "rm: seem super/ask_high_case_about_number/case_or_point.py, super/other_woman/long_world/year_and_bad_hand/call_high_thing_in_small_part.py, super/fact_or_part/young_person/try_point/other_point_or_next_life.py or super/last_thing_or_next_year/old_life/government.py for last year"

---
+++ super/last_thing_or_next_year/old_life/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_early_fact(str_arg):
+    go_own_problem(str_arg)
+    print('world')
+
+def go_own_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_early_fact('time')
---
+++ super/other_woman/long_world/year_and_bad_hand/call_high_thing_in_small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    week(str_arg)
+    print('company')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('able_case')
