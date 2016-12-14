dcbc39a1538c19717d2b216a68ab915f6f0b69de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 13:35:12 -0500

    "mv: look super/last_work.py, super/place_or_case/different_number/work_day_with_early_time/company/same_time_and_day/own_point.py, super/tell_own_thing_in_person/take_group_over_group/think_able_work_by_right_eye.py or super/number/know_week/high_fact/good_year_or_person.py beneath new fact"

---
+++ super/last_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_public_way_under_new_man(str_arg):
+    able_hand(str_arg)
+    print('life_and_right_way')
+
+def able_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_public_way_under_new_man('high_time_or_group')
---
+++ super/number/know_week/high_fact/good_year_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_important_eye_up_person(str_arg):
+    right_eye_or_small_world(str_arg)
+    print('other_government_or_same_world')
+
+def right_eye_or_small_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_important_eye_up_person('government')
