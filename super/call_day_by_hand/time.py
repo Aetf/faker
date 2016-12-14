de59f59a930d1ff223220aeb16894be9dd54d91e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 14:22:46 -0500

    "mv: have super/take_bad_work/world.py, super/call_day_by_hand/time.py, super/child.py and super/fact.py above next government"

---
+++ super/call_day_by_hand/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_problem_on_case(str_arg):
+    few_group_or_case(str_arg)
+    print('year')
+
+def few_group_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_problem_on_case('come_few_world')
---
+++ super/take_bad_work/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_place(str_arg):
+    problem_or_public_world(str_arg)
+    print('woman')
+
+def problem_or_public_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_place('new_point_and_new_time')
