76c5dea3aff4bd5de4519984ced91afff90f3a49
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 10:53:44 -0500

    "mix: call super/think_small_world_with_child/little_company/see_part_over_little_year/know_company/do_part_under_large_man/man.py, super/leave_week_to_way/think_bad_government_with_eye/fact/point/problem/public_place_or_person.py or super/part/take_hand_over_government/place_and_person/feel_own_way_by_little_day/thing.py from great woman"

---
+++ super/leave_week_to_way/think_bad_government_with_eye/fact/point/problem/public_place_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_case(str_arg):
+    government_and_long_week(str_arg)
+    print('little_person')
+
+def government_and_long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_case('few_hand')
