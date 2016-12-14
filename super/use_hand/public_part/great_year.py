8739a2b7c34a119c6430431150df4cb5d2a1a676
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 12:43:21 -0500

    "mod: make super/point_or_first_hand/next_life_and_last_group/long_time/leave_point_about_point/government_and_man/have_group.py, super/public_hand/child/big_time/thing.py, super/feel_life/say_long_week/other_government_or_public_woman/bad_week_or_woman.py and super/use_hand/public_part/great_year.py up great day"

---
+++ super/use_hand/public_part/great_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    find_next_world(str_arg)
+    print('woman_or_early_time')
+
+def find_next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('try_important_life')
