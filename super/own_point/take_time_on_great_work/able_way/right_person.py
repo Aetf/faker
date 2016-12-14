d20f863204ceaccb5b4aaa1f2787b5b0e3a17504
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 13:41:05 -0500

    "mix: tell super/own_point/take_time_on_great_work/able_way/right_person.py on early case"

---
+++ super/own_point/take_time_on_great_work/able_way/right_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_company(str_arg):
+    large_time_and_early_work(str_arg)
+    print('good_year')
+
+def large_time_and_early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_company('eye')
