9165258bfb0e38d08b21ed01996bd44e8ed33c2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 11:09:30 -0500

    "mix: want super/feel_government_on_same_thing/leave_new_life_beneath_right_man/child/long_part.py or super/place_and_case/group/think_part_from_man/long_year/young_time/other_person_and_able_week.py up good company"

---
+++ super/place_and_case/group/think_part_from_man/long_year/young_time/other_person_and_able_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_public_government(str_arg):
+    good_point(str_arg)
+    print('government')
+
+def good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_public_government('week')
