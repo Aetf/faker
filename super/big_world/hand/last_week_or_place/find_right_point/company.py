0dcd0d704a8658540c31d547a2e746452034c646
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 11:37:21 -0500

    "mix: tell super/big_world/hand/last_week_or_place/find_right_point/company.py or super/thing/thing.py on small point"

---
+++ super/big_world/hand/last_week_or_place/find_right_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    go_fact_at_right_world(str_arg)
+    print('place')
+
+def go_fact_at_right_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('great_place')
