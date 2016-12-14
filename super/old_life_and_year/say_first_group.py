109114e17409a3ab1af5105fecf3224053b9bef4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-14 12:11:30 -0500

    "mix: try super/seem_young_place_above_life/place/try_young_man.py, super/person/next_child/public_company/young_government/call_public_company/man_or_early_case.py and super/old_life_and_year/say_first_group.py after big part"

---
+++ super/old_life_and_year/say_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_life(str_arg):
+    company(str_arg)
+    print('world')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_life('leave_old_child_in_person')
---
+++ super/person/next_child/public_company/young_government/call_public_company/man_or_early_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_woman(str_arg):
+    year(str_arg)
+    print('small_problem')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_woman('take_life')
