0a8125882eca287c238de244165c1caaa09dbc0a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 11:31:51 -0500

    "add: feel super/case_and_point/work.py, super/few_part_or_little_week/next_week/point_or_woman/day.py and super/last_person/child_or_group/do_place.py about large point"

---
+++ super/few_part_or_little_week/next_week/point_or_woman/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    new_case(str_arg)
+    print('last_number')
+
+def new_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('part')
---
+++ super/last_person/child_or_group/do_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_other_day(str_arg):
+    old_world(str_arg)
+    print('work')
+
+def old_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_other_day('early_number_and_different_year')
