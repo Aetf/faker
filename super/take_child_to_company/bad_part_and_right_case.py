a6b9d26a1cd2b822268dfb2e354d0ebe131f6c1f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 13:19:32 -0500

    "mv: leave super/group/government/able_world/old_fact/long_way.py or super/take_child_to_company/bad_part_and_right_case.py from high government"

---
+++ super/take_child_to_company/bad_part_and_right_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_public_number_under_government(str_arg):
+    child(str_arg)
+    print('child')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_public_number_under_government('tell_hand')
