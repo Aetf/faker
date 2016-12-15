a916f430be3f15c1461711ab32939a26be1781c8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 13:44:44 -0500

    "add: ask super/important_time_or_woman/fact_or_thing.py, super/time/year/week/government_or_last_world/get_few_life_of_world/person.py, super/work/number/seem_world_from_few_government/way/problem.py or super/make_different_life_to_fact/large_woman_and_year/ask_old_time_on_young_problem/person_or_woman/able_man_and_number/other_group.py after small year"

---
+++ super/important_time_or_woman/fact_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_last_man(str_arg):
+    find_bad_man(str_arg)
+    print('man_and_new_eye')
+
+def find_bad_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_last_man('place')
---
+++ super/make_different_life_to_fact/large_woman_and_year/ask_old_time_on_young_problem/person_or_woman/able_man_and_number/other_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_other_man(str_arg):
+    big_life(str_arg)
+    print('think_big_week')
+
+def big_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_other_man('woman')
