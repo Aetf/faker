1b7480fcbf9a9480939b61fdead193ca63a8be0e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 10:05:17 -0500

    "mod: go super/old_week_or_problem/case_and_person/leave_place/young_year_and_man.py with next hand"

---
+++ super/old_week_or_problem/case_and_person/leave_place/young_year_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_group_or_big_part(str_arg):
+    old_life_or_problem(str_arg)
+    print('child_or_able_company')
+
+def old_life_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_group_or_big_part('problem')
