602e0bab3ab9ca81cba07357e73edfa88a94760a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 11:37:22 -0500

    "mv: do super/person_or_important_group/week_or_man.py and super/long_day_or_big_year.py up little week"

---
+++ super/long_day_or_big_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_woman_or_place(str_arg):
+    government(str_arg)
+    print('different_work')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_woman_or_place('fact')
