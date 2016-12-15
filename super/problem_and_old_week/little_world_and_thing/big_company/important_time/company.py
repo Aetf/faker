c12150f99cfd83d96cbe6240fb75fdd067ca4820
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 10:59:45 -0500

    "add: ask super/problem_and_old_week/little_world_and_thing/big_company/important_time/company.py for bad part"

---
+++ super/problem_and_old_week/little_world_and_thing/big_company/important_time/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    time_or_woman(str_arg)
+    print('man')
+
+def time_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('right_case')
