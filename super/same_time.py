a2bb22fc85d0b2be146020e39b7ae7de2eeba26f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 14:47:27 -0500

    "rm: make super/know_last_fact_at_man/seem_man/leave_first_child/large_man/person.py, super/same_time.py or super/world.py above great case"

---
+++ super/same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def feel_child_after_year(str_arg):
+    use_big_woman(str_arg)
+    print('public_case_and_world')
+
+def use_big_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    feel_child_after_year('see_different_case')
