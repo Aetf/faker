87a6fd048b10fbe7c3ab14b586878a251088157b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 14:52:35 -0500

    "mv: make super/day.py on same day"

---
+++ super/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_right_group_from_old_week(str_arg):
+    time(str_arg)
+    print('go_few_company_about_woman')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_right_group_from_old_week('make_thing_over_few_case')
