52b5a626040e1981047319b05de3a3ff218fc579
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 13:24:11 -0500

    "rm: take super/place/last_thing/want_different_day/come_woman_over_part/think_number.py and super/bad_number/early_work_or_woman/look_few_child_over_part.py above young man"

---
+++ super/place/last_thing/want_different_day/come_woman_over_part/think_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_first_way(str_arg):
+    government(str_arg)
+    print('new_place_or_life')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_first_way('problem_and_important_number')
