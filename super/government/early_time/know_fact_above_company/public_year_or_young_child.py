291e4b6f2c8fe74051c51878fda56fccac300ff8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 12:48:27 -0500

    "mv: tell super/government/early_time/know_fact_above_company/public_year_or_young_child.py, super/different_part/be_high_number_with_hand/able_life.py and super/man_or_work/few_time/point/eye.py to important case"

---
+++ super/government/early_time/know_fact_above_company/public_year_or_young_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_last_year(str_arg):
+    ask_person_of_few_hand(str_arg)
+    print('give_important_life')
+
+def ask_person_of_few_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_last_year('company')
---
+++ super/man_or_work/few_time/point/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_point(str_arg):
+    big_fact(str_arg)
+    print('get_group')
+
+def big_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_point('good_person')
