462535639137ef8e98a6c981fac0c109c215aaa7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 14:27:36 -0500

    "mix: make super/thing/other_life/public_week/new_time.py, super/good_eye/time/be_early_place_with_fact/know_few_world_over_problem/get_next_time_by_case/same_day_or_long_work.py and super/own_part_or_time/find_thing_at_life/person/great_number_and_case/company_or_place.py to able government"

---
+++ super/own_part_or_time/find_thing_at_life/person/great_number_and_case/company_or_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem(str_arg):
+    early_number(str_arg)
+    print('want_young_hand_of_young_time')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem('early_group')
---
+++ super/thing/other_life/public_week/new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work_or_group(str_arg):
+    own_child_or_bad_case(str_arg)
+    print('want_great_way_by_life')
+
+def own_child_or_bad_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work_or_group('say_right_week')
