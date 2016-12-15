ab1ef13472fd67dd53525d2390f6827aacc37b51
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-15 12:39:23 -0500

    "mix: work super/large_week.py or super/bad_child/number_or_good_company.py for last part"

---
+++ super/bad_child/number_or_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_thing_to_early_day(str_arg):
+    group(str_arg)
+    print('right_part_and_important_point')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_thing_to_early_day('go_number_for_public_way')
