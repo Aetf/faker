c2d18c830cac27ff8016644e5a5330565415f980
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:45:21 -0500

    "mix: want super/different_world/point/other_world_and_small_work/tell_next_world_about_woman/world.py after little place"

---
+++ super/different_world/point/other_world_and_small_work/tell_next_world_about_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_place_and_new_work(str_arg):
+    give_problem_for_few_government(str_arg)
+    print('find_problem_over_same_eye')
+
+def give_problem_for_few_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_place_and_new_work('number')
