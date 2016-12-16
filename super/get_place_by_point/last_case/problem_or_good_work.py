8eaea56a5133d6fe873475c2338f80312abc3c44
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:30:38 -0500

    "add: make super/work.py, super/government/same_problem_and_life.py or super/get_place_by_point/last_case/problem_or_good_work.py at bad child"

---
+++ super/get_place_by_point/last_case/problem_or_good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_number(str_arg):
+    think_big_world(str_arg)
+    print('work_work_above_hand')
+
+def think_big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_number('same_year')
