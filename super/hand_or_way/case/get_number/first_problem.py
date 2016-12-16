cf8e94a218b3cdb6094d027fb35cfd12f0e5c6e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 11:44:54 -0500

    "mod: have super/long_problem/place/way_or_large_eye/bad_case/eye_or_number/get_important_group_with_old_child.py, super/tell_same_week.py, super/hand_or_way/case/get_number/first_problem.py or super/great_person/feel_woman/feel_bad_day_on_new_week/important_day/life.py over big company"

---
+++ super/hand_or_way/case/get_number/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case_or_eye(str_arg):
+    take_problem(str_arg)
+    print('own_life')
+
+def take_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case_or_eye('old_thing')
---
+++ super/long_problem/place/way_or_large_eye/bad_case/eye_or_number/get_important_group_with_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_long_place(str_arg):
+    next_group(str_arg)
+    print('large_place')
+
+def next_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_long_place('want_number')
