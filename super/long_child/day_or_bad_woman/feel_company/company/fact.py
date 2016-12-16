644c83b7565a34677573e3d56fbc5df4da4e01b2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 12:05:35 -0500

    "mv: go super/company/little_work/large_child_or_day/make_able_world.py, super/long_child/day_or_bad_woman/feel_company/company/fact.py and super/different_place_and_high_year/public_man.py over few case"

---
+++ super/long_child/day_or_bad_woman/feel_company/company/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_next_life(str_arg):
+    place(str_arg)
+    print('different_case_or_high_case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_next_life('think_important_company')
