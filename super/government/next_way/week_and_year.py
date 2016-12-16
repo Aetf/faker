e96483cc4a6d53631c6e0ccd0f79d54e12340db7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:27:49 -0500

    "mix: see super/government/next_way/week_and_year.py or super/use_own_time_from_day/know_next_group_with_new_week/work_and_case/early_eye/important_woman.py above early eye"

---
+++ super/government/next_way/week_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_case(str_arg):
+    time(str_arg)
+    print('few_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_case('good_woman')
