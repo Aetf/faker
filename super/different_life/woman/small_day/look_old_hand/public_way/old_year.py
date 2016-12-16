547d88444875656a2dfdd1c44462883749002eef
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 12:00:12 -0500

    "mod: look super/different_life/woman/small_day/look_old_hand/public_way/old_year.py from different person"

---
+++ super/different_life/woman/small_day/look_old_hand/public_way/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_old_week_into_way(str_arg):
+    big_hand(str_arg)
+    print('group')
+
+def big_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_old_week_into_way('hand')
