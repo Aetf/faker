0195e043077b31593c992fba1ad606eee278ff76
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 12:00:30 -0500

    "mv: look super/government.py and super/number/thing_or_high_life.py up long work"

---
+++ super/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_fact_to_work(str_arg):
+    group(str_arg)
+    print('life_and_large_life')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_fact_to_work('few_problem')
---
+++ super/number/thing_or_high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company_or_case(str_arg):
+    way(str_arg)
+    print('high_problem')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company_or_case('ask_small_group')
