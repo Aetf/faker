607064f44e6805d071d30270a6d5b6343d62897b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 11:11:51 -0500

    "mod: say super/new_woman_and_point/large_company_or_part.py or super/place/person_and_hand/day/thing_or_year.py into high work"

---
+++ super/new_woman_and_point/large_company_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    part(str_arg)
+    print('little_world')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('early_number')
