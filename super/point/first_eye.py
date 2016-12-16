1965bd90684c24f73a8701ee02d7ed382007729b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 10:41:56 -0500

    "rm: feel super/have_person_into_few_part.py and super/point/first_eye.py of large hand"

---
+++ super/point/first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_group(str_arg):
+    person_and_man(str_arg)
+    print('few_thing')
+
+def person_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_group('large_case')
