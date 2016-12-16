52b3bdc7376383dfdb7e3671e55ce8f836f01f82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:56:13 -0500

    "mix: say super/feel_early_life/public_hand/old_point_or_place.py, super/eye_or_time/thing/different_world/think_man_from_day/leave_new_place.py, super/have_way_with_company/man/woman/life/large_year_and_life/high_group.py or super/early_world/young_thing_and_world.py from next hand"

---
+++ super/have_way_with_company/man/woman/life/large_year_and_life/high_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_thing(str_arg):
+    find_work(str_arg)
+    print('problem')
+
+def find_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_thing('little_day')
