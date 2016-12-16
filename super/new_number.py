e7dc9036e524e3ea9f5bc46150600a2dbb46793e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 12:50:53 -0500

    "add: know super/world/person.py and super/new_number.py under small way"

---
+++ super/new_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_fact(str_arg):
+    group(str_arg)
+    print('long_world')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_fact('case_or_hand')
