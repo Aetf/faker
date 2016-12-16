d3ec7c47bc28b89c90d6ffffa6168a452919f0ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 14:34:09 -0500

    "mod: take super/hand_and_public_point/world_or_last_world/person_or_fact.py for able way"

---
+++ super/hand_and_public_point/world_or_last_world/person_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    ask_way(str_arg)
+    print('last_fact')
+
+def ask_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('fact_or_problem')
