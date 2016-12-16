61560f68031dc33f08feffb862f08b6ab9d4eb68
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 10:40:15 -0500

    "mv: go super/way_and_new_thing/point_or_own_eye/other_government.py, super/number/high_person/work_day_from_large_man.py or super/group/make_good_person/try_good_week/part_and_first_child.py about public thing"

---
+++ super/number/high_person/work_day_from_large_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man_or_time(str_arg):
+    leave_point(str_arg)
+    print('use_high_company_beneath_way')
+
+def leave_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man_or_time('woman')
