07c3498fb8691671709630a9776adcadf7c446e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:06:29 -0500

    "mv: look super/go_great_time/see_year_into_big_problem/own_child.py, super/next_problem/first_place/go_small_time_by_important_number.py, super/old_case/child/see_small_place_of_few_life.py or super/come_group_up_other_company/other_child/new_company_or_next_woman/own_hand_and_case/get_man_by_part.py of long life"

---
+++ super/next_problem/first_place/go_small_time_by_important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_hand_for_fact(str_arg):
+    see_person_after_week(str_arg)
+    print('number')
+
+def see_person_after_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_hand_for_fact('man')
---
+++ super/old_case/child/see_small_place_of_few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way(str_arg):
+    place(str_arg)
+    print('know_same_child_under_old_thing')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way('good_man')
