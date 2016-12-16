99e6916ed78a5944899cee1b62c59fd142d447ed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 10:22:40 -0500

    "mod: go super/case_or_woman/new_problem/work_or_first_part/case.py or super/world/new_way_and_number/man.py on public number"

---
+++ super/case_or_woman/new_problem/work_or_first_part/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_life_over_different_child(str_arg):
+    young_hand_or_man(str_arg)
+    print('part')
+
+def young_hand_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_life_over_different_child('early_child')
