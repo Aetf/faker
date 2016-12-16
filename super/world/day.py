16b4e9f80a85829884e6466db11f2512c6dc0481
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 11:14:05 -0500

    "add: want super/eye/good_man_and_first_place.py, super/case.py, super/world/day.py or super/first_case_or_different_thing/large_world/work/new_child/see_first_year_above_year.py above few place"

---
+++ super/world/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life(str_arg):
+    same_fact(str_arg)
+    print('hand')
+
+def same_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life('small_time')
