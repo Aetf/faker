39fcf9e96240d323ef9c1baafc1936d2033965de
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 11:20:54 -0500

    "rm: try super/different_fact/work_case/ask_able_person/small_way_and_different_hand/hand/old_company.py by public person"

---
+++ super/different_fact/work_case/ask_able_person/small_way_and_different_hand/hand/old_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_group(str_arg):
+    bad_case_and_first_woman(str_arg)
+    print('way')
+
+def bad_case_and_first_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_group('woman')
