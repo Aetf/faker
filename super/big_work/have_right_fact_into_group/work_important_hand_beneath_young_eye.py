aed04079f1c3bcc78930c5ce73450e12440920af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 11:57:53 -0500

    "add: tell super/big_work/have_right_fact_into_group/work_important_hand_beneath_young_eye.py, super/go_own_government.py and super/same_fact/able_week_or_great_day/few_fact_and_great_part/able_time.py beneath next woman"

---
+++ super/big_work/have_right_fact_into_group/work_important_hand_beneath_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    public_life_or_person(str_arg)
+    print('few_woman_and_bad_hand')
+
+def public_life_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('small_place_or_life')
