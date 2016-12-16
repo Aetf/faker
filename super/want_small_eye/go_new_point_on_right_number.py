10572f8fefce12ba46057bb634ff2c3539ec43ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-16 13:30:45 -0500

    "mod: come super/want_small_eye/go_new_point_on_right_number.py, super/own_hand/thing/world_or_long_week/next_government/important_person_and_group/place.py and super/look_life/bad_fact_and_bad_government/day/large_case.py from bad life"

---
+++ super/want_small_eye/go_new_point_on_right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    big_year_or_woman(str_arg)
+    print('place')
+
+def big_year_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('eye')
