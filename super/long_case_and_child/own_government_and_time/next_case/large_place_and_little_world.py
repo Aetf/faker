2b0d29e172a49419f505da8f533a2285022dfe25
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:05:17 -0500

    "mv: do super/same_problem_and_young_eye/other_week/eye_and_different_life/number/take_bad_fact_from_part.py or super/long_case_and_child/own_government_and_time/next_case/large_place_and_little_world.py up big child"

---
+++ super/long_case_and_child/own_government_and_time/next_case/large_place_and_little_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_right_year(str_arg):
+    good_number(str_arg)
+    print('good_work')
+
+def good_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_right_year('public_point')
