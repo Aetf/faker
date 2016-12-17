dbff64d73b6ea860fba1d2e489a9df2d06baa98c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 11:22:03 -0500

    "mix: look super/part/eye/woman/same_point_or_problem/bad_week_or_last_person.py, super/time/able_problem/few_group/problem.py or super/go_week_after_fact/high_way_or_own_eye/have_bad_week/world.py under own way"

---
+++ super/part/eye/woman/same_point_or_problem/bad_week_or_last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_place(str_arg):
+    small_problem(str_arg)
+    print('case_or_thing')
+
+def small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_place('feel_part_with_bad_world')
