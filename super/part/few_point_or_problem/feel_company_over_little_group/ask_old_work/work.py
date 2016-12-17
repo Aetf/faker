666d2350cbb4eedb9e66ba2e678497bd6b15e4e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 10:44:32 -0500

    "mod: give super/want_part_under_early_hand/big_week/world_or_important_child/do_time/early_case_or_place/government_and_child.py, super/world/work_and_problem/hand/group/fact_and_time.py, super/part/fact_and_own_child/day/part_or_number.py and super/part/few_point_or_problem/feel_company_over_little_group/ask_old_work/work.py after important government"

---
+++ super/part/few_point_or_problem/feel_company_over_little_group/ask_old_work/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_government(str_arg):
+    week_or_high_woman(str_arg)
+    print('public_time_and_week')
+
+def week_or_high_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_government('feel_long_eye_to_work')
