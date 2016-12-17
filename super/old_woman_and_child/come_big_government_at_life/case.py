db47670618a2d7da79a541762b8a061d47eb4ae3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:20:34 -0500

    "rm: get super/old_woman_and_child/come_big_government_at_life/case.py with old way"

---
+++ super/old_woman_and_child/come_big_government_at_life/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    go_woman(str_arg)
+    print('high_year')
+
+def go_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('hand_and_part')
