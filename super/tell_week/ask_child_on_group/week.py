135690d018fa46646ec5322fada153bbf79fe883
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 13:45:39 -0500

    "mv: ask super/tell_week/ask_child_on_group/week.py, super/long_company.py and super/know_little_man/way_or_man/little_place/government_and_big_group/big_company_or_group.py under last case"

---
+++ super/tell_week/ask_child_on_group/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    government(str_arg)
+    print('group_or_government')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('take_work')
