7ed61d4ec281e294bafef86409dff26a8fd7a91b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:45:13 -0500

    "mod: be super/child_and_first_time/find_few_thing_after_company/early_fact_or_year.py, super/bad_place_and_eye/good_hand/have_right_man/big_hand.py and super/place_and_big_world/few_hand_or_time/say_day_about_week.py in large person"

---
+++ super/place_and_big_world/few_hand_or_time/say_day_about_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    have_place_on_case(str_arg)
+    print('point')
+
+def have_place_on_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('way')
