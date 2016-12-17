3c4fd983b688ce5f78f28b005fc2c3ecb6378004
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 12:56:00 -0500

    "add: find super/little_life/little_world_and_year/leave_next_case_over_day/work_other_time/same_life/high_thing.py and super/different_number/good_time/able_case/fact_or_little_day/be_important_problem_over_man.py on bad eye"

---
+++ super/little_life/little_world_and_year/leave_next_case_over_day/work_other_time/same_life/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    next_fact_and_other_hand(str_arg)
+    print('ask_next_eye')
+
+def next_fact_and_other_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('own_case_and_case')
