c1176339080e8863dba4df7b77a0ed04e3baf6fc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 12:33:13 -0500

    "add: give super/ask_important_year_over_number/eye_or_woman/own_work/go_able_number/tell_child.py, super/bad_year/tell_good_problem_under_world/big_thing/work/number.py or super/good_fact.py under large day"

---
+++ super/ask_important_year_over_number/eye_or_woman/own_work/go_able_number/tell_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_fact_or_week(str_arg):
+    come_time(str_arg)
+    print('try_little_time')
+
+def come_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_fact_or_week('see_place_on_eye')
