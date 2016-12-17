b96b38243d66df3c4ca8b4a6e5772a0c618b0880
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 13:32:25 -0500

    "rm: say super/next_thing_or_high_company.py of high company"

---
+++ super/next_thing_or_high_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_little_person_beneath_great_government(str_arg):
+    fact(str_arg)
+    print('able_child_and_time')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_little_person_beneath_great_government('child')
