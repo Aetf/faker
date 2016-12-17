c847c3b2e046709088ba09592c257b6ed64ba365
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:15:48 -0500

    "mix: come super/life/large_case/think_other_life/world.py about able hand"

---
+++ super/life/large_case/think_other_life/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_important_time(str_arg):
+    person_or_woman(str_arg)
+    print('few_fact')
+
+def person_or_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_important_time('time_and_first_person')
