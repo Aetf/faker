8f2baebef07ced5b6335199d1436eadce8eb01c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 13:57:13 -0500

    "mod: tell super/ask_thing/early_year.py in high day"

---
+++ super/ask_thing/early_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    call_hand_for_fact(str_arg)
+    print('young_work_or_person')
+
+def call_hand_for_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('big_woman')
