e683ef5a2dc7b6be28e3a1bd3df09a5ca756b056
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 11:59:40 -0500

    "add: give super/ask_new_week/case_or_place/long_day_or_public_company/say_fact_with_great_woman/know_time_for_thing.py, super/year_or_person/find_group.py, super/group/first_government.py or super/next_life/fact/say_part.py about different thing"

---
+++ super/ask_new_week/case_or_place/long_day_or_public_company/say_fact_with_great_woman/know_time_for_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_point_about_company(str_arg):
+    new_company_or_hand(str_arg)
+    print('good_woman')
+
+def new_company_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_point_about_company('new_thing_or_young_week')
