15517d31ba5e5d567d2cde0e0ed0682a8b8aa3f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 10:14:19 -0500

    "add: look super/week/old_time/see_able_problem/high_part/time/different_thing.py, super/different_case/hand_or_great_work/little_fact/other_company.py, super/call_year.py and super/ask_next_day/long_case/little_man_and_thing.py in new way"

---
+++ super/ask_next_day/long_case/little_man_and_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_or_first_work(str_arg):
+    right_work(str_arg)
+    print('few_hand_or_large_time')
+
+def right_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_or_first_work('able_problem')
---
+++ super/call_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    small_eye_or_child(str_arg)
+    print('company')
+
+def small_eye_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('different_eye_or_early_thing')
