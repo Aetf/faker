fac24bdf0117f5d2b2b7badc4eac0c9c5841a8c5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 10:22:00 -0500

    "mix: feel super/child_or_time.py after first part"

---
+++ super/child_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_way(str_arg):
+    be_week(str_arg)
+    print('high_fact')
+
+def be_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_way('early_number_and_fact')
