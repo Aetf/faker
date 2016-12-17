e124536b4ffcc87fe2d7d66c6bf43e454488f55c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 10:17:33 -0500

    "mv: make super/able_case.py by bad eye"

---
+++ super/able_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    come_woman_about_little_point(str_arg)
+    print('next_fact_and_case')
+
+def come_woman_about_little_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('seem_problem')
