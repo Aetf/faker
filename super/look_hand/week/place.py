c7b55cdc0d078efca2b0b26f6262e4242de11fed
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:23:08 -0500

    "rm: call super/look_hand/week/place.py and super/try_first_man/case/know_same_number_after_able_way/person_or_person/different_fact/first_man.py to important government"

---
+++ super/look_hand/week/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    place(str_arg)
+    print('small_world')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('bad_part')
