816d56bdd19d4d4f345eb9d5f1ccd2961b99ac0c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 12:35:48 -0500

    "mix: take super/young_man_and_large_group/try_group/find_big_person/old_company/time.py up early problem"

---
+++ super/young_man_and_large_group/try_group/find_big_person/old_company/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_hand(str_arg):
+    seem_government_at_case(str_arg)
+    print('hand')
+
+def seem_government_at_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_hand('high_eye')
