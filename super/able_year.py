dc9d6abf27e3ce500cff2829b6d057e3edb58759
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 11:26:30 -0500

    "mod: ask super/be_number/child/hand/go_other_man_at_time.py, super/able_year.py and super/want_high_world.py with other case"

---
+++ super/able_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_group(str_arg):
+    give_same_fact_above_good_way(str_arg)
+    print('thing')
+
+def give_same_fact_above_good_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_group('child')
---
+++ super/want_high_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_child_of_different_day(str_arg):
+    think_high_group_above_part(str_arg)
+    print('go_point')
+
+def think_high_group_above_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_child_of_different_day('small_group')
