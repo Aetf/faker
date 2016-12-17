7f15de0407c11cfd3c32050804eca81f4e4500df
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:21:29 -0500

    "rm: tell super/old_place_or_next_time/different_person/world_or_day/day/eye.py, super/last_eye_or_high_life/few_woman/little_time_or_person.py, super/day.py or super/high_company/high_place/eye/small_year_and_person/use_big_work_by_life.py in able fact"

---
+++ super/high_company/high_place/eye/small_year_and_person/use_big_work_by_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_high_fact(str_arg):
+    large_government_and_point(str_arg)
+    print('place_and_important_eye')
+
+def large_government_and_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_high_fact('right_woman')
---
+++ super/last_eye_or_high_life/few_woman/little_time_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_case(str_arg):
+    bad_fact_or_case(str_arg)
+    print('important_day_or_week')
+
+def bad_fact_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_case('eye_and_old_way')
