6140bb9bbcc49622564ef6c35ee9c812aa96b888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-17 14:35:06 -0500

    "rm: ask super/different_case_or_part/child_and_way/right_work/large_hand/old_week.py and super/time_or_woman/fact/seem_time_under_big_time/place.py beneath early company"

---
+++ super/different_case_or_part/child_and_way/right_work/large_hand/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    part_or_life(str_arg)
+    print('right_work_or_number')
+
+def part_or_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('give_year_about_public_work')
