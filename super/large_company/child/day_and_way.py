db0d1dbf7583316b0a5184ae82de694877a8d517
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 14:20:17 -0500

    "mod: see super/point/group/big_group/great_company.py, super/large_company/child/day_and_way.py or super/place_and_small_problem/want_woman_at_early_government/bad_eye_and_woman.py over important year"

---
+++ super/large_company/child/day_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_government_in_government(str_arg):
+    few_case(str_arg)
+    print('important_woman')
+
+def few_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_government_in_government('group_or_little_hand')
---
+++ super/place_and_small_problem/want_woman_at_early_government/bad_eye_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_life(str_arg):
+    person(str_arg)
+    print('good_government')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_life('leave_new_hand')
