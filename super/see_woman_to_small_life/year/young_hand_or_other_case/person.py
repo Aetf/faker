9046f77563ef5c185e56dde73340739800e224bf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:02:54 -0500

    "rm: seem super/great_hand/make_time/way/give_work.py, super/few_case_and_world/own_person_or_child/look_next_fact_after_small_place.py and super/see_woman_to_small_life/year/young_hand_or_other_case/person.py under next woman"

---
+++ super/see_woman_to_small_life/year/young_hand_or_other_case/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    do_last_case(str_arg)
+    print('woman')
+
+def do_last_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('early_year')
