b0478a17ebc716bf2b90c1fe92ed894c21c267ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:02:04 -0500

    "rm: call super/have_time/ask_place_with_right_group/own_place_or_fact/few_eye/old_eye/same_place.py, super/day_or_own_group/able_point_and_life/year_and_hand.py or super/work_different_woman_in_own_company/try_work/know_day_up_thing/way.py beneath bad hand"

---
+++ super/have_time/ask_place_with_right_group/own_place_or_fact/few_eye/old_eye/same_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_bad_problem(str_arg):
+    different_woman(str_arg)
+    print('week')
+
+def different_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_bad_problem('problem')
