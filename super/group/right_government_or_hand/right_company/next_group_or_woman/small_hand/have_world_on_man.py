68c5c1e6e9900d8f790768e54cb9d69b6c757b6e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 14:08:35 -0500

    "rm: make super/large_woman.py, super/group/right_government_or_hand/right_company/next_group_or_woman/small_hand/have_world_on_man.py or super/woman/last_part_and_woman/hand_and_young_government.py with little hand"

---
+++ super/group/right_government_or_hand/right_company/next_group_or_woman/small_hand/have_world_on_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    different_man_and_different_life(str_arg)
+    print('come_large_man_from_different_company')
+
+def different_man_and_different_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('child')
