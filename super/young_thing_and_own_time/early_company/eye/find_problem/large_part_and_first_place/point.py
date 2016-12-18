b76f8f3534ba64331c052bc92e73cc95f3e7eeab
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 13:37:48 -0500

    "rm: come super/young_thing_and_own_time/early_company/eye/find_problem/large_part_and_first_place/point.py under early week"

---
+++ super/young_thing_and_own_time/early_company/eye/find_problem/large_part_and_first_place/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    different_thing(str_arg)
+    print('place')
+
+def different_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('person')
