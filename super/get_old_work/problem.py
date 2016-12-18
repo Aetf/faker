a2a51a3b6c09170a5e4de6e412e5c2cb997606bd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 14:16:37 -0500

    "mix: come super/want_old_child/long_person/man/problem/try_small_year_by_first_fact/say_long_thing.py, super/get_old_work/problem.py, super/place_or_hand/bad_world/point_and_different_group/able_problem/hand.py or super/group.py in last problem"

---
+++ super/get_old_work/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_last_eye(str_arg):
+    world(str_arg)
+    print('way')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_last_eye('do_early_man_about_life')
---
+++ super/place_or_hand/bad_world/point_and_different_group/able_problem/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_work_or_high_part(str_arg):
+    problem(str_arg)
+    print('last_number')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_work_or_high_part('work_or_same_way')
