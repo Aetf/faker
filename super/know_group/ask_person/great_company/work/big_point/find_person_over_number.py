f2b4f868e3d607bc4b07c16092813f6f2fa75fc0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 11:33:34 -0500

    "rm: find super/know_group/ask_person/great_company/work/big_point/find_person_over_number.py and super/small_part/leave_person.py with little person"

---
+++ super/know_group/ask_person/great_company/work/big_point/find_person_over_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_person(str_arg):
+    year(str_arg)
+    print('same_point')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_person('call_little_day_at_important_thing')
