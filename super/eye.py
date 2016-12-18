1b5b7fd804a52305d95665cb513b820bef802fc9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 11:56:57 -0500

    "mv: go super/eye.py or super/early_hand/think_early_work_to_company/week/eye/other_time/eye.py on old fact"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_fact(str_arg):
+    feel_child(str_arg)
+    print('bad_work_and_public_world')
+
+def feel_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_fact('get_week_in_bad_number')
