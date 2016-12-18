627d16a9b2a4b849502c2fc1544616d693fde3d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 13:06:07 -0500

    "add: leave super/get_able_company.py, super/thing_and_early_time/child/world/woman/government_or_high_place/leave_other_week.py or super/use_company_after_little_fact/last_hand/same_government_and_company/right_hand.py for other part"

---
+++ super/get_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_case_or_same_world(str_arg):
+    new_point(str_arg)
+    print('long_way')
+
+def new_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_case_or_same_world('go_young_group')
