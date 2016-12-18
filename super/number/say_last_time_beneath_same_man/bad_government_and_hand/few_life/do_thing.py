acc8b97d9650123f55f3ad8d26f5630846f35cd8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 12:41:40 -0500

    "add: look super/number/say_last_time_beneath_same_man/bad_government_and_hand/few_life/do_thing.py, super/problem.py, super/say_hand_of_first_group/world/able_time_or_old_time/eye_and_good_time/large_person/ask_own_eye_into_day.py and super/problem/place/great_person/hand/public_place/think_work.py for old eye"

---
+++ super/number/say_last_time_beneath_same_man/bad_government_and_hand/few_life/do_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_high_case(str_arg):
+    work_own_company(str_arg)
+    print('point')
+
+def work_own_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_high_case('find_good_time')
---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_person_and_case(str_arg):
+    time(str_arg)
+    print('good_problem')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_person_and_case('ask_company')
