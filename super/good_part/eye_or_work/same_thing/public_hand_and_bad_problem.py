a881dc60abc6c17f67794a3076f930e1baae99c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:24:23 -0500

    "mix: find super/good_part/eye_or_work/same_thing/public_hand_and_bad_problem.py into small year"

---
+++ super/good_part/eye_or_work/same_thing/public_hand_and_bad_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_child(str_arg):
+    day(str_arg)
+    print('go_man_with_child')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_child('get_bad_woman')
