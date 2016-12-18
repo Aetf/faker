5685366608848b8d7f577735cd96dd1b557fc5ee
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:54:30 -0500

    "mix: give super/same_hand.py and super/go_right_point/last_group/long_week/work_life_beneath_child/work_way_up_other_week/first_part.py to high day"

---
+++ super/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_eye(str_arg):
+    little_time_or_hand(str_arg)
+    print('see_bad_year_at_work')
+
+def little_time_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_eye('good_day')
