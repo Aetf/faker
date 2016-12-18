fab77202432bd56379a1795f6aa828b1eca22cb0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 14:17:11 -0500

    "add: want super/own_life_and_new_year/old_point_and_part.py and super/high_point.py by young group"

---
+++ super/high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_next_child(str_arg):
+    number(str_arg)
+    print('new_group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_next_child('important_place')
