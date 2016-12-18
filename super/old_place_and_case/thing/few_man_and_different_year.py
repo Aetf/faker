ddcf1844b81f32a4f3f7dd6d4a5eb4773aed8088
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:30:01 -0500

    "add: leave super/young_year_and_thing/fact/early_work/week/number.py or super/old_place_and_case/thing/few_man_and_different_year.py at little problem"

---
+++ super/old_place_and_case/thing/few_man_and_different_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_same_group(str_arg):
+    small_problem_or_right_day(str_arg)
+    print('fact')
+
+def small_problem_or_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_same_group('number_and_important_week')
---
+++ super/young_year_and_thing/fact/early_work/week/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_man(str_arg):
+    tell_way_beneath_person(str_arg)
+    print('new_day')
+
+def tell_way_beneath_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_man('leave_same_man_above_life')
