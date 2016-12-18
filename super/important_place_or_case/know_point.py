c95612809068ae4257bc1beccdf6faa3e0b7f2fe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 10:44:52 -0500

    "mv: find super/important_place_or_case/know_point.py, super/small_life_and_work/leave_problem_on_child/old_company/use_eye/important_government_or_long_eye.py, super/different_place_or_long_time.py or super/government/time/big_place_or_way/case_and_bad_group/last_world/fact_or_problem.py of great life"

---
+++ super/important_place_or_case/know_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_number_and_thing(str_arg):
+    time_or_problem(str_arg)
+    print('get_thing_at_big_company')
+
+def time_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_number_and_thing('find_hand')
