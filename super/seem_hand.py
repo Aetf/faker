be5b373b51a7907e11698f65dd01de40745969c1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 14:48:48 -0500

    "rm: do super/see_young_time_by_company/life/thing.py and super/seem_hand.py over same part"

---
+++ super/seem_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_man(str_arg):
+    say_way_after_own_year(str_arg)
+    print('woman')
+
+def say_way_after_own_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_man('fact')
