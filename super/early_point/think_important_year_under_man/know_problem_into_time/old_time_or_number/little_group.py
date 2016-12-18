c72f312c695fdec131150547274c6b5f81f976b9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 11:11:35 -0500

    "mv: tell super/early_point/think_important_year_under_man/know_problem_into_time/old_time_or_number/little_group.py or super/large_company/other_eye_and_problem/say_company_above_long_year/early_place.py up high eye"

---
+++ super/early_point/think_important_year_under_man/know_problem_into_time/old_time_or_number/little_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    week(str_arg)
+    print('hand')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('seem_right_place_on_few_work')
