24e6388c4c580c0beb2455a8c0f1c1e1b78adbf4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-18 11:21:03 -0500

    "rm: leave super/see_way/great_fact/own_man/important_year_and_early_hand/have_long_company_from_last_eye/life.py and super/time_and_good_fact/early_place/long_group/part/be_long_place.py into same life"

---
+++ super/see_way/great_fact/own_man/important_year_and_early_hand/have_long_company_from_last_eye/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_problem_and_eye(str_arg):
+    long_world(str_arg)
+    print('next_government')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_problem_and_eye('take_government')
---
+++ super/time_and_good_fact/early_place/long_group/part/be_long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    leave_long_person(str_arg)
+    print('work')
+
+def leave_long_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('last_hand')
