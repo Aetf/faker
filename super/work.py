ae20ac61c349a438b6b83383b3bcec4121047fe3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 14:15:08 -0500

    "mv: see super/go_life/time/good_point/ask_right_company/problem/see_big_hand_over_company.py, super/work.py or super/case/week/company_or_government/great_place/government_or_important_time.py from next way"

---
+++ super/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_eye(str_arg):
+    number_and_way(str_arg)
+    print('do_last_life_of_week')
+
+def number_and_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_eye('case_and_public_life')
