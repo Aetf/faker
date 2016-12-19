114f17cfd032bc0a207de95b916760500ae97181
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 10:45:56 -0500

    "add: take super/day/point_or_able_year/person/try_government_by_fact/say_large_world/look_small_day_about_way.py, super/woman/good_thing_and_small_week/number/company/take_group_up_work/way_or_right_point.py or super/problem/other_man.py up long day"

---
+++ super/problem/other_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_world(str_arg):
+    bad_fact(str_arg)
+    print('next_child_or_able_time')
+
+def bad_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_world('own_place_or_government')
