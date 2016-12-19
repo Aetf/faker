74d7e8740424aa3fe1ed6cbd63f9711d342a145e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:53:03 -0500

    "rm: say super/big_government_or_time/different_thing.py or super/come_week_on_way/life/have_important_person_above_last_woman/little_eye.py into next place"

---
+++ super/come_week_on_way/life/have_important_person_above_last_woman/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    number(str_arg)
+    print('eye')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('old_time')
