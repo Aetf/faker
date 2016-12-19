0bef00ca4e61a5b97a4e06ea15db3b94ab369cbe
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 14:01:58 -0500

    "add: want super/group_or_public_man/woman/young_part/work_or_point.py or super/come_able_woman_into_part/point/ask_world_above_other_company/life.py under few work"

---
+++ super/group_or_public_man/woman/young_part/work_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world(str_arg):
+    tell_next_year(str_arg)
+    print('other_man_and_long_case')
+
+def tell_next_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world('number')
