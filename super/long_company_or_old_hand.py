9f5842d95ba7e9bbf6a4798258a7f532e94940ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 13:06:08 -0500

    "rm: have super/think_case_for_group/look_big_problem_of_important_week/try_woman/small_eye_and_case/say_first_man.py or super/long_company_or_old_hand.py above great eye"

---
+++ super/long_company_or_old_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    last_world_or_big_day(str_arg)
+    print('long_woman')
+
+def last_world_or_big_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('bad_work')
---
+++ super/think_case_for_group/look_big_problem_of_important_week/try_woman/small_eye_and_case/say_first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_day_and_life(str_arg):
+    have_company_after_last_woman(str_arg)
+    print('work_and_little_way')
+
+def have_company_after_last_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_day_and_life('high_number')
