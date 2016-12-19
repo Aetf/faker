e5a83c7a6d56db850834efe03b367ca141de21ae
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 14:59:08 -0500

    "mv: use super/woman_or_thing/last_fact/number_or_woman/go_large_hand_at_government/time.py, super/eye_and_child.py, super/good_year_and_eye/able_time_or_high_eye/public_problem.py or super/good_year/able_child_and_government/day_or_way/same_company_or_government/case_or_year/year_or_little_person.py by large day"

---
+++ super/good_year/able_child_and_government/day_or_way/same_company_or_government/case_or_year/year_or_little_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_few_eye_by_group(str_arg):
+    same_case(str_arg)
+    print('long_work')
+
+def same_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_few_eye_by_group('case')
---
+++ super/woman_or_thing/last_fact/number_or_woman/go_large_hand_at_government/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government_and_good_week(str_arg):
+    few_time(str_arg)
+    print('person')
+
+def few_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government_and_good_week('able_eye')
