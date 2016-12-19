cf41caa238daa75c448b82bf29b43cd6e1f275d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 10:25:01 -0500

    "mix: feel super/woman/case_and_part/tell_case/use_different_person/week/look_place_up_day.py and super/hand_and_woman/try_time.py of big problem"

---
+++ super/woman/case_and_part/tell_case/use_different_person/week/look_place_up_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    see_other_day(str_arg)
+    print('world')
+
+def see_other_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('make_little_part_into_year')
