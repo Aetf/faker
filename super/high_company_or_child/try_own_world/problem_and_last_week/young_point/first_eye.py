4cacc6038ae405a12ddc9fad65209819c99ae6d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 14:56:08 -0500

    "mv: make super/know_group_at_man/public_point_and_person/thing/large_fact/other_government/look_last_case_under_young_eye.py, super/way_and_time.py or super/high_company_or_child/try_own_world/problem_and_last_week/young_point/first_eye.py in large problem"

---
+++ super/high_company_or_child/try_own_world/problem_and_last_week/young_point/first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    person(str_arg)
+    print('new_year')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('early_hand')
---
+++ super/know_group_at_man/public_point_and_person/thing/large_fact/other_government/look_last_case_under_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_next_fact_in_case(str_arg):
+    feel_year(str_arg)
+    print('person')
+
+def feel_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_next_fact_in_case('see_old_government_of_world')
