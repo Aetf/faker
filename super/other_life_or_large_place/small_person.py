799696f4cdb6966804eca8f755da3b690cf91d73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:47:00 -0500

    "rm: take super/other_life_or_large_place/small_person.py and super/important_work/place_or_problem/tell_point_above_life/child/child.py about first fact"

---
+++ super/other_life_or_large_place/small_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    go_young_world_up_good_place(str_arg)
+    print('good_company')
+
+def go_young_world_up_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('bad_day')
