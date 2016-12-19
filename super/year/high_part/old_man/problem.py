faaa5c5fd82bc027dd6e56dcf6222aa4a3a7dda0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 10:44:18 -0500

    "mix: have super/year/high_part/old_man/problem.py or super/number_and_same_life.py from able problem"

---
+++ super/year/high_part/old_man/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    first_company(str_arg)
+    print('public_place')
+
+def first_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('thing')
