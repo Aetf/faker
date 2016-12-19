a0cd7e516bdc085c4c6e558a8498c9a400795b7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 10:13:22 -0500

    "add: make super/do_woman_about_good_thing/case/company_or_little_week/woman/high_point_and_year/other_life.py up bad case"

---
+++ super/do_woman_about_good_thing/case/company_or_little_week/woman/high_point_and_year/other_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_week(str_arg):
+    high_case(str_arg)
+    print('good_person')
+
+def high_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_week('person_or_place')
