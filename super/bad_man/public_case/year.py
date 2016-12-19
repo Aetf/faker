921e643b48239842bbfa7b41613849f8244ad650
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 14:56:33 -0500

    "mv: ask super/early_way_and_life/give_high_week/good_year/year.py, super/bad_man/public_case/year.py, super/great_number/time/public_number_or_different_year/different_case.py and super/way_and_last_time/thing/know_group.py for important woman"

---
+++ super/bad_man/public_case/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_long_hand(str_arg):
+    government(str_arg)
+    print('get_long_part_from_number')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_long_hand('child_or_big_child')
