219b11ba76c2bcd9221e1fcaf6c156ee9acd72dc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:13:18 -0500

    "add: get super/long_world_or_part.py and super/make_part.py with same case"

---
+++ super/long_world_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_great_person(str_arg):
+    able_thing(str_arg)
+    print('place')
+
+def able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_great_person('do_old_company')
