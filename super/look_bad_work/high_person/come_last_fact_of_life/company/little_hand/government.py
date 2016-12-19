67cb4af888aeca1bd9f57060212463afd62056fb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:17:09 -0500

    "mix: say super/give_part.py, super/woman/week.py and super/look_bad_work/high_person/come_last_fact_of_life/company/little_hand/government.py beneath new number"

---
+++ super/look_bad_work/high_person/come_last_fact_of_life/company/little_hand/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_thing_and_large_eye(str_arg):
+    point(str_arg)
+    print('life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_thing_and_large_eye('few_part')
---
+++ super/woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_point(str_arg):
+    make_case_about_eye(str_arg)
+    print('thing')
+
+def make_case_about_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_point('big_person_or_week')
