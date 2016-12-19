4208d408984669fb31b32a2ab95ee41d2bb70ac4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:38:01 -0500

    "mod: come super/little_person_and_important_child/week/right_group_and_point/able_place/young_point.py about young eye"

---
+++ super/little_person_and_important_child/week/right_group_and_point/able_place/young_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    way(str_arg)
+    print('day_and_fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('see_fact_about_problem')
