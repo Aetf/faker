3fd4b243d2c1ab5a278442a378d18a391de2dbeb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 13:04:52 -0500

    "mix: make super/place.py, super/give_week/number_or_high_problem/own_point/important_fact.py and super/last_place_and_different_company/give_part_at_few_day.py into young case"

---
+++ super/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    tell_way(str_arg)
+    print('new_eye_or_last_world')
+
+def tell_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('few_company')
