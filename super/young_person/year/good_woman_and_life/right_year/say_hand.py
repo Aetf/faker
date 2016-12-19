5351f9de2e7a04ef001edc6ff83b33b19f9126b5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 13:31:13 -0500

    "rm: try super/young_person/year/good_woman_and_life/right_year/say_hand.py, super/thing_or_time/right_work_or_other_case/same_number/way/big_company.py, super/right_person.py or super/woman/bad_company_or_different_person/point_and_thing/large_work/problem/get_thing.py at high life"

---
+++ super/young_person/year/good_woman_and_life/right_year/say_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    small_company(str_arg)
+    print('small_time')
+
+def small_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('problem_or_good_work')
