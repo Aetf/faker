7b27e23a3f74a25b4b55de1e36b53c048d527259
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 13:50:37 -0500

    "add: feel super/place/person/tell_life/seem_other_place/world_or_bad_life.py or super/work/great_day/big_place/thing/see_important_problem_for_great_person.py beneath next hand"

---
+++ super/work/great_day/big_place/thing/see_important_problem_for_great_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    first_group(str_arg)
+    print('take_place')
+
+def first_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('young_work_and_small_eye')
