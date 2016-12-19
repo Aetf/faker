f10f43457c5e6a922bbbc9242a78eb2b064db22a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 11:46:43 -0500

    "add: think super/company_and_week/see_person/old_place.py above large woman"

---
+++ super/company_and_week/see_person/old_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_woman_of_hand(str_arg):
+    new_number(str_arg)
+    print('see_next_person_to_old_eye')
+
+def new_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_woman_of_hand('fact')
