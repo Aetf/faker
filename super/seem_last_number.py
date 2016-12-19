c7074e0b1721df895166690b0b1e626ea007693a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-19 13:19:23 -0500

    "add: make super/seem_last_number.py, super/do_same_man_at_thing/find_way/few_way/go_case_on_world/world.py and super/other_child/first_world_and_other_woman/see_own_work_about_case/work/able_fact/young_child.py on public eye"

---
+++ super/seem_last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    part_or_great_work(str_arg)
+    print('say_year')
+
+def part_or_great_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('feel_great_year')
