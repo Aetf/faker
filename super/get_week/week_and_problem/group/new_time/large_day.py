93ec444defe01dc158a816c7e40722f2c106c79a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 12:02:24 -0500

    "mv: find super/large_world_and_next_number/own_day/year/know_day_on_old_year/next_case_or_work.py, super/right_thing/other_place_or_own_year/little_hand/government/child_and_case.py, super/get_week/week_and_problem/group/new_time/large_day.py or super/same_world/work/way/hand_and_fact/group.py about same woman"

---
+++ super/get_week/week_and_problem/group/new_time/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    leave_bad_life(str_arg)
+    print('few_government')
+
+def leave_bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('little_case')
