67cabf5df046dbc1778ce7848d2d3439e7009fdf
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:55:25 -0500

    "add: go super/long_time/group/company/eye_or_big_man.py, super/case/other_time/small_week/time/old_day.py and super/number.py about same case"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time(str_arg):
+    part(str_arg)
+    print('see_number_over_fact')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time('great_man')
