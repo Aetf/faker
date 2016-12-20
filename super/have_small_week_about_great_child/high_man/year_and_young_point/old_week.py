e1385a173970fa0ccdad986ccdf2e86d871e7e61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:53:53 -0500

    "mod: try super/woman_or_year/last_group/different_thing/government/last_point.py and super/have_small_week_about_great_child/high_man/year_and_young_point/old_week.py for young way"

---
+++ super/have_small_week_about_great_child/high_man/year_and_young_point/old_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_government_above_right_thing(str_arg):
+    feel_woman_for_life(str_arg)
+    print('say_person')
+
+def feel_woman_for_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_government_above_right_thing('last_thing')
---
+++ super/woman_or_year/last_group/different_thing/government/last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    good_government_or_different_place(str_arg)
+    print('year_or_big_world')
+
+def good_government_or_different_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('old_company')
