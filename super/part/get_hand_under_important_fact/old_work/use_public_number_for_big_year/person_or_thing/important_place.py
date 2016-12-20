6546b82a3ac669e2bc32ba0abf8d51bc69a2d117
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:22:03 -0500

    "mv: do super/way_or_early_problem/use_hand/number/day.py, super/young_government_or_young_woman/long_place/thing_and_little_man/first_point_or_small_child.py, super/seem_number/large_part_and_company/thing_and_government.py and super/part/get_hand_under_important_fact/old_work/use_public_number_for_big_year/person_or_thing/important_place.py into long woman"

---
+++ super/part/get_hand_under_important_fact/old_work/use_public_number_for_big_year/person_or_thing/important_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    right_man(str_arg)
+    print('large_company_and_hand')
+
+def right_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('use_time')
---
+++ super/way_or_early_problem/use_hand/number/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    take_different_problem_of_high_life(str_arg)
+    print('number')
+
+def take_different_problem_of_high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('same_day')
