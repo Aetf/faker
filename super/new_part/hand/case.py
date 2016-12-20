8eb515af6d0a567eae1bdd88cac4894351f2d7a2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:05:53 -0500

    "add: work super/different_life/other_government_or_bad_group/early_point.py, super/place.py, super/day_or_company/ask_large_problem_up_good_world/early_place/long_child.py and super/new_part/hand/case.py after next world"

---
+++ super/new_part/hand/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    life_or_new_year(str_arg)
+    print('bad_thing')
+
+def life_or_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('tell_next_group')
