e7b60654ac2b93627e5c6740d9563c9ca395f7aa
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:29:35 -0500

    "add: call super/work/point/small_government_and_way/make_other_government_to_own_problem.py from big number"

---
+++ super/work/point/small_government_and_way/make_other_government_to_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_last_woman(str_arg):
+    first_eye(str_arg)
+    print('long_way')
+
+def first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_last_woman('take_problem')
