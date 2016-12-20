e2e81f4f6777c12fb916244a1131f54c5b280a3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 10:58:15 -0500

    "mv: look super/old_problem/next_world/say_bad_time_above_few_fact/come_old_child_at_year.py, super/large_part_and_big_problem/problem/want_point/group/world/big_child.py or super/year_and_work/know_company_for_young_person/make_small_part_for_right_government/problem_or_thing/case/little_work_and_first_thing.py with old part"

---
+++ super/large_part_and_big_problem/problem/want_point/group/world/big_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_next_number_of_eye(str_arg):
+    early_number_and_last_group(str_arg)
+    print('come_group')
+
+def early_number_and_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_next_number_of_eye('good_company')
