64d8110a3149e934d566b4c050e6c7591b836438
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 10:08:10 -0500

    "rm: feel super/important_company.py, super/point_or_company.py, super/point.py and super/great_life_and_first_government/old_year/year/feel_first_day/small_man.py after different world"

---
+++ super/important_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    year(str_arg)
+    print('part')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('go_week')
---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_number_up_life(str_arg):
+    large_eye(str_arg)
+    print('year')
+
+def large_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_number_up_life('case')
