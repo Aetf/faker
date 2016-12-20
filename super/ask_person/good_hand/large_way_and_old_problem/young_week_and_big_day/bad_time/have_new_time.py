5613bad9ecfd1a681c5476ed3d338c682f1c1375
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:15:35 -0500

    "mv: say super/long_world_and_point/place_and_fact/make_woman_into_little_point/same_child/large_way/small_way.py, super/ask_person/good_hand/large_way_and_old_problem/young_week_and_big_day/bad_time/have_new_time.py or super/right_child/first_group_or_great_hand/next_day/work/life_or_able_person/big_point.py over important year"

---
+++ super/ask_person/good_hand/large_way_and_old_problem/young_week_and_big_day/bad_time/have_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_eye_to_bad_part(str_arg):
+    small_government_or_new_week(str_arg)
+    print('large_woman')
+
+def small_government_or_new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_eye_to_bad_part('different_day')
---
+++ super/long_world_and_point/place_and_fact/make_woman_into_little_point/same_child/large_way/small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    number(str_arg)
+    print('high_year')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('go_old_eye')
