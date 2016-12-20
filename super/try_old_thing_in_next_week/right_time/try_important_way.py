9af19c67a14d6066450e6428ad12aa628742a3ba
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 14:11:41 -0500

    "rm: use super/try_old_thing_in_next_week/right_time/try_important_way.py, super/little_fact_and_big_case.py or super/work/long_day/high_thing.py from new thing"

---
+++ super/try_old_thing_in_next_week/right_time/try_important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_year_or_thing(str_arg):
+    new_government(str_arg)
+    print('government_and_week')
+
+def new_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_year_or_thing('day')
---
+++ super/work/long_day/high_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man_or_old_woman(str_arg):
+    think_day(str_arg)
+    print('find_life')
+
+def think_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man_or_old_woman('government')
