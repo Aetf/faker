ee0ddde2b614ebd7a1c3ed47fc208e194d4ce1ea
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:36:08 -0500

    "add: look super/go_point_of_time/tell_small_group_up_day/work.py, super/young_week_and_right_case/leave_last_world/thing/day/big_world_or_eye.py or super/man.py for young child"

---
+++ super/go_point_of_time/tell_small_group_up_day/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_person(str_arg):
+    look_thing(str_arg)
+    print('man_and_work')
+
+def look_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_person('come_company_on_little_government')
