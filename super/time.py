e64d45f95e70e018855d3b702f8c9b389140c78d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 10:03:20 -0500

    "add: tell super/group.py, super/time.py or super/first_way_and_time/part/give_great_case_up_world/work_government_over_young_company.py to important point"

---
+++ super/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_time(str_arg):
+    child(str_arg)
+    print('other_eye')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_time('right_part')
