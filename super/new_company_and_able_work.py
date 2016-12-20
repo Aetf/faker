b6c1b14cad7fa008bd7d66120a109d9b0ce437e5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 10:48:49 -0500

    "add: say super/new_company_and_able_work.py or super/company/person.py for new point"

---
+++ super/new_company_and_able_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    way_and_life(str_arg)
+    print('way')
+
+def way_and_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('first_life')
