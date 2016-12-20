3975b4a3e4c00bf0ec03f9a188ba818ec62c2745
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 13:28:18 -0500

    "mv: feel super/world_or_first_eye/thing_and_case.py over big problem"

---
+++ super/world_or_first_eye/thing_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    last_group_or_other_eye(str_arg)
+    print('man_and_young_life')
+
+def last_group_or_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('child')
