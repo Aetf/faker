911870cdeb8767c586e7fdf08e76e27228ef6d77
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 14:57:57 -0500

    "mv: try super/number/come_woman_above_high_hand/work/long_man.py, super/know_bad_time_under_woman/point_and_few_person/have_last_woman/point_or_important_work/new_eye.py and super/part/look_good_group/problem/call_life/world/high_case.py of large company"

---
+++ super/know_bad_time_under_woman/point_and_few_person/have_last_woman/point_or_important_work/new_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_first_government(str_arg):
+    high_company(str_arg)
+    print('world')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_first_government('own_day_or_company')
