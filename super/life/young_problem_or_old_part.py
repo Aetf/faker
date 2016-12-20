2e804eda3dc8aa4b1ea65917d67f8d3b79051333
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 14:46:32 -0500

    "rm: find super/life/young_problem_or_old_part.py and super/way_or_other_way/person.py with good day"

---
+++ super/life/young_problem_or_old_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_way(str_arg):
+    get_thing(str_arg)
+    print('hand_and_bad_day')
+
+def get_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_way('company')
---
+++ super/way_or_other_way/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    other_thing(str_arg)
+    print('get_little_point_after_way')
+
+def other_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('say_important_fact_under_day')
