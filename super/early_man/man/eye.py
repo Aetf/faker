8548601d8e07ef5f7f78fbe8617161cef945585c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 11:57:32 -0500

    "add: make super/early_man/man/eye.py, super/work/right_point/eye/large_eye/place_and_way.py and super/way.py into small company"

---
+++ super/early_man/man/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    work(str_arg)
+    print('use_company_into_woman')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('man_and_last_person')
