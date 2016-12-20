59bd6e0cd3ff4fcf654b98ca24b43c8175da1c73
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 11:32:28 -0500

    "rm: give super/first_woman/able_life/call_person.py and super/part_and_time/call_problem_after_place/thing.py about public part"

---
+++ super/part_and_time/call_problem_after_place/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_man(str_arg):
+    case(str_arg)
+    print('make_work')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_man('way')
