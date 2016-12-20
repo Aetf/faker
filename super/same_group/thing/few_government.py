05dfc42bdc22bf371aa3145571083761a8df58a3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 12:17:33 -0500

    "add: feel super/last_case/small_group/large_man/different_person/time_and_woman.py, super/same_group/thing/few_government.py, super/week_and_few_fact/point/important_case.py or super/leave_long_problem/young_group/make_world/young_person/few_problem_or_new_number.py from young woman"

---
+++ super/same_group/thing/few_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    thing_and_number(str_arg)
+    print('seem_point_on_year')
+
+def thing_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('ask_child')
