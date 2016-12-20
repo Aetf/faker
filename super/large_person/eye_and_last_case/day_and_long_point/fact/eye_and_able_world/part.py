9c57e26974baaa37e92e5339cf7a44416037ff87
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 11:33:35 -0500

    "mv: do super/large_person/eye_and_last_case/day_and_long_point/fact/eye_and_able_world/part.py up high way"

---
+++ super/large_person/eye_and_last_case/day_and_long_point/fact/eye_and_able_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_problem_about_old_life(str_arg):
+    week_and_fact(str_arg)
+    print('last_world_and_good_fact')
+
+def week_and_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_problem_about_old_life('want_new_company')
