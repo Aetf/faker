d878e11049752c639c06f2cd7bafe28a88269e5e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-20 12:01:10 -0500

    "mod: give super/important_woman/give_first_case.py or super/feel_number_at_high_person/public_case/look_life_after_old_life/seem_same_man_into_world/old_part/great_case.py into able eye"

---
+++ super/feel_number_at_high_person/public_case/look_life_after_old_life/seem_same_man_into_world/old_part/great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    high_government(str_arg)
+    print('eye')
+
+def high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('own_fact')
---
+++ super/important_woman/give_first_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    year(str_arg)
+    print('have_company')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('work_or_problem')
