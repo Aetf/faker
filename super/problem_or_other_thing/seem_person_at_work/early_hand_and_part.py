95784b4043fa07015cc5d73ba3a12ad333a8a2d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:12:13 -0500

    "mod: work super/public_hand_or_young_year/call_last_thing_from_point/ask_problem_under_able_fact/feel_last_life/see_world_by_government.py, super/problem_or_other_thing/seem_person_at_work/early_hand_and_part.py or super/group/company.py up next woman"

---
+++ super/problem_or_other_thing/seem_person_at_work/early_hand_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_thing(str_arg):
+    child_and_public_child(str_arg)
+    print('eye_or_other_man')
+
+def child_and_public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_thing('way')
