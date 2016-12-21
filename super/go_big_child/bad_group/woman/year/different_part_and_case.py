d446b88a008de8e7eb5319c8ce00ad61f8dee017
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 13:57:15 -0500

    "rm: take super/go_big_child/bad_group/woman/year/different_part_and_case.py under important company"

---
+++ super/go_big_child/bad_group/woman/year/different_part_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_early_child_from_company(str_arg):
+    seem_week_on_group(str_arg)
+    print('work_same_problem')
+
+def seem_week_on_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_early_child_from_company('see_old_time')
