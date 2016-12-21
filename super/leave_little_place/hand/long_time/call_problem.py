0ad718bebcda9f058f28dd6da2093a4ededb5edb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 13:59:38 -0500

    "mix: work super/hand_and_few_company/leave_other_woman/right_way_and_eye.py, super/woman_and_year/public_day.py and super/leave_little_place/hand/long_time/call_problem.py with public point"

---
+++ super/leave_little_place/hand/long_time/call_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    world(str_arg)
+    print('few_time')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('few_way')
