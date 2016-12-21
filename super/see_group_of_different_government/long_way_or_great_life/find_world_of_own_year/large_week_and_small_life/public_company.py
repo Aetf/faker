504f3a1dfffd70c8ea6229c31249e9ee16fa8bc3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:14:12 -0500

    "mv: go super/child_and_fact/own_problem/child/number_or_first_government.py or super/see_group_of_different_government/long_way_or_great_life/find_world_of_own_year/large_week_and_small_life/public_company.py by last group"

---
+++ super/see_group_of_different_government/long_way_or_great_life/find_world_of_own_year/large_week_and_small_life/public_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_early_child(str_arg):
+    man(str_arg)
+    print('give_day_into_own_year')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_early_child('important_day_or_year')
