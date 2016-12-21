b617409fade93c87090ba27a503c9a5daa2f87e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 10:28:21 -0500

    "add: make super/go_case.py to first work"

---
+++ super/go_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    feel_other_part(str_arg)
+    print('tell_small_eye_at_eye')
+
+def feel_other_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('great_way')
