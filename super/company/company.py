d719aeddab9de115292f99883d885cb6467ffb6a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:07:11 -0500

    "mix: think super/life/great_eye_and_own_problem.py or super/company/company.py after important person"

---
+++ super/company/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_number_or_important_work(str_arg):
+    point(str_arg)
+    print('leave_person_up_day')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_number_or_important_work('call_public_child_over_able_day')
---
+++ super/life/great_eye_and_own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_point(str_arg):
+    try_life(str_arg)
+    print('time')
+
+def try_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_point('ask_able_life')
