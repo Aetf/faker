a614afe5fd6d3c49fe214cc31871824d475984b4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 13:29:54 -0500

    "mix: go super/work_or_bad_man/same_week/woman/want_fact.py up same work"

---
+++ super/work_or_bad_man/same_week/woman/want_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_number(str_arg):
+    week(str_arg)
+    print('number')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_number('leave_little_hand_at_large_day')
