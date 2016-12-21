401844e3102c5d4c6748d610d6949bd0d21a85b3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 10:56:20 -0500

    "mix: call super/case/same_eye/have_man_under_great_company/young_day/eye_and_large_woman/group.py and super/few_year/think_early_day.py with few man"

---
+++ super/few_year/think_early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_woman(str_arg):
+    old_company(str_arg)
+    print('first_person')
+
+def old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_woman('look_child_above_child')
