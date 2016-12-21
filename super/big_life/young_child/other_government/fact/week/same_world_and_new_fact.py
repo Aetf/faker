bd8fc6c9e0801dc99b86336375b8e8d3721b8a42
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:56:58 -0500

    "mix: tell super/big_life/young_child/other_government/fact/week/same_world_and_new_fact.py, super/person_and_time/seem_public_problem_from_new_year/company_or_different_place.py or super/have_new_place_in_hand/big_thing_or_time/different_number/person_or_government/know_public_point.py under public way"

---
+++ super/big_life/young_child/other_government/fact/week/same_world_and_new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_point(str_arg):
+    little_man_or_work(str_arg)
+    print('important_thing')
+
+def little_man_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_point('do_year')
