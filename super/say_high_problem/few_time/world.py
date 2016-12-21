a09915d47a1e34aac726f89ad0a3efe462377450
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 13:50:03 -0500

    "mv: go super/say_high_problem/few_time/world.py, super/early_world/same_hand_or_new_government.py, super/place_or_part/give_large_week_on_place/take_early_week_into_work.py and super/part.py for new child"

---
+++ super/say_high_problem/few_time/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    week_and_time(str_arg)
+    print('big_number_and_part')
+
+def week_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('number_and_right_life')
