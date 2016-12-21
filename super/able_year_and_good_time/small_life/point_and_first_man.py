f4fae5ec288b547fb1e4aa40f6ab34eba161640c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:11:57 -0500

    "mv: think super/want_important_way/come_large_world/own_work.py, super/public_world_and_eye/be_number_after_hand/child_and_own_group/eye/way.py and super/able_year_and_good_time/small_life/point_and_first_man.py up little fact"

---
+++ super/able_year_and_good_time/small_life/point_and_first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_case(str_arg):
+    old_hand_or_other_fact(str_arg)
+    print('large_way_and_time')
+
+def old_hand_or_other_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_case('see_child_to_place')
