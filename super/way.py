6777e46b64f274dc6571e4f332656c9d1a64cd2e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 10:44:56 -0500

    "mix: come super/new_problem/group/last_way_and_thing.py or super/way.py on next year"

---
+++ super/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_man(str_arg):
+    long_world(str_arg)
+    print('feel_part')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_man('little_woman')
