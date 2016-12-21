88a23347ca69cfcb421c9338f85be275f39c25e4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 14:31:16 -0500

    "mod: leave super/able_place/want_own_group.py or super/do_small_way_from_good_woman/time/number/seem_big_company/go_hand_to_different_case.py over great part"

---
+++ super/able_place/want_own_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_government(str_arg):
+    long_world(str_arg)
+    print('find_part')
+
+def long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_government('life_and_public_life')
