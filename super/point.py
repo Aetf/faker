7d73c48fbff6da357aad078227f0a3a2b8108e1d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 14:32:36 -0500

    "add: call super/part/world.py, super/world/year/find_old_group/time/find_next_hand_up_eye.py and super/point.py from early life"

---
+++ super/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    come_small_eye(str_arg)
+    print('seem_person')
+
+def come_small_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('little_company')
