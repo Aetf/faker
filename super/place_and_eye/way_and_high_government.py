c00f95ffd81997984f74c31a59882528cb87bf8b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 12:18:31 -0500

    "add: want super/big_case/next_part_and_day/tell_same_part_to_big_part.py and super/place_and_eye/way_and_high_government.py at early eye"

---
+++ super/place_and_eye/way_and_high_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    next_time(str_arg)
+    print('great_problem')
+
+def next_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('use_number')
