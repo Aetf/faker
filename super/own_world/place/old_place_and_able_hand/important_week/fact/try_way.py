b0227dae107c4082b7bb1012fd3ea403a919a369
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 11:40:02 -0500

    "add: do super/think_other_week_into_company/time/part/company/week_or_same_part.py and super/own_world/place/old_place_and_able_hand/important_week/fact/try_way.py of large work"

---
+++ super/own_world/place/old_place_and_able_hand/important_week/fact/try_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_work(str_arg):
+    day(str_arg)
+    print('get_early_point_over_able_part')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_work('number')
