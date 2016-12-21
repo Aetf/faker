d035cda71500411a8d890e625efe68e4daeac13d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 14:55:43 -0500

    "mv: come super/way_and_woman/place_or_young_problem/big_way_or_bad_day.py, super/young_man/ask_right_point/find_high_year_over_right_world/man/number/large_case.py or super/call_company/year_and_week/find_same_man_of_able_time.py of big day"

---
+++ super/call_company/year_and_week/find_same_man_of_able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_year(str_arg):
+    work(str_arg)
+    print('place')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_year('important_child')
