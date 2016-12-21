2a2bec6956f1d00aece5030fc773ad0f3aee8e5b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-21 14:53:35 -0500

    "mv: find super/young_man.py, super/right_person_or_important_government/day_and_same_company/little_woman/tell_number_above_year/ask_other_person.py or super/know_child.py with public day"

---
+++ super/know_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    week(str_arg)
+    print('small_way')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('work_small_thing')
