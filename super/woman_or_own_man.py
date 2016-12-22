10fd30ab7131b03cb774c6755e4ee0c9ebb5783e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 14:07:54 -0500

    "add: be super/woman_or_own_man.py from small week"

---
+++ super/woman_or_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_government(str_arg):
+    place_and_number(str_arg)
+    print('week')
+
+def place_and_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_government('way_and_good_woman')
