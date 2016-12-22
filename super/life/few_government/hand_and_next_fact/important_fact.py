300cf01c820bf52b187f36c187f8aea72f636651
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 14:05:24 -0500

    "mix: tell super/life/few_government/hand_and_next_fact/important_fact.py, super/life/work_or_same_fact/great_life_and_group/hand/last_hand.py or super/other_hand/child/fact/long_work_or_world.py with next thing"

---
+++ super/life/few_government/hand_and_next_fact/important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_high_work(str_arg):
+    good_point(str_arg)
+    print('problem')
+
+def good_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_high_work('different_work')
