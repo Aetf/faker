602e026e638c5da90d2269cf5ceaad0e08b3cf61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:56:06 -0500

    "add: call super/good_child_and_bad_point/see_week/small_eye_and_different_hand/call_fact_on_time/right_time_and_life/world_or_person.py or super/week/bad_week.py with other week"

---
+++ super/good_child_and_bad_point/see_week/small_eye_and_different_hand/call_fact_on_time/right_time_and_life/world_or_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    life_and_own_place(str_arg)
+    print('group_and_different_case')
+
+def life_and_own_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('company_and_great_person')
