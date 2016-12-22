c5754146aab3ccc68765e64e032c25e123ab51a5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 12:12:24 -0500

    "add: tell super/time/know_fact_to_early_week/woman_and_way/day_and_problem/eye_or_old_thing.py into young thing"

---
+++ super/time/know_fact_to_early_week/woman_and_way/day_and_problem/eye_or_old_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place(str_arg):
+    group_or_problem(str_arg)
+    print('large_fact_and_high_place')
+
+def group_or_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place('week')
