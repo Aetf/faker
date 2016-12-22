1cfc33c9782587b764caee5f1aa9c45d05cefc88
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 11:03:44 -0500

    "mix: tell super/young_time_or_first_world/make_same_hand_into_year.py and super/do_able_child_on_right_part/high_man/try_woman/world_and_week.py in same thing"

---
+++ super/do_able_child_on_right_part/high_man/try_woman/world_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_number(str_arg):
+    person(str_arg)
+    print('government')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_number('week_or_week')
