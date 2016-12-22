e55b481891b5c1178d8ff4c040026e059cf656f6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:34:34 -0500

    "rm: ask super/few_life/big_work_or_man/take_part.py, super/place_or_little_company/few_problem/own_day/place.py, super/next_problem/big_person/leave_government/hand/find_world/work_week.py and super/new_thing_or_person/thing/great_eye/week.py by small day"

---
+++ super/few_life/big_work_or_man/take_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_work(str_arg):
+    see_time_into_number(str_arg)
+    print('year')
+
+def see_time_into_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_work('public_problem')
---
+++ super/next_problem/big_person/leave_government/hand/find_world/work_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_man(str_arg):
+    come_right_group(str_arg)
+    print('great_child')
+
+def come_right_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_man('part_and_small_time')
