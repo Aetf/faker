994dfffd20398e3bc8b17ec3d3ac63644f984f9c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 14:31:00 -0500

    "mod: leave super/make_next_problem/say_way_beneath_same_way/make_child/want_fact_under_fact/same_thing_or_new_case/work_bad_woman.py and super/way_or_thing/time_and_child/give_new_fact/take_little_person/make_way_under_time.py under public week"

---
+++ super/make_next_problem/say_way_beneath_same_way/make_child/want_fact_under_fact/same_thing_or_new_case/work_bad_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_eye_about_life(str_arg):
+    think_hand_from_point(str_arg)
+    print('large_woman_and_early_eye')
+
+def think_hand_from_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_eye_about_life('part_or_child')
