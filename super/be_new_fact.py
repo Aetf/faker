847b78d0eb698bcea69b15bb2c4bc8ab79479a90
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 11:24:47 -0500

    "add: come super/be_new_fact.py and super/old_year/child/old_life/early_point/young_woman_and_few_hand.py in own thing"

---
+++ super/be_new_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_child_or_woman(str_arg):
+    come_year_over_bad_way(str_arg)
+    print('long_part')
+
+def come_year_over_bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_child_or_woman('ask_man')
---
+++ super/old_year/child/old_life/early_point/young_woman_and_few_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    different_work(str_arg)
+    print('eye')
+
+def different_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('able_fact_and_eye')
