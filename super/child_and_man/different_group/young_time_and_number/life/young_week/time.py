2280b1e1f7c85240dd8f61bf74edccaa985c2898
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:12:44 -0500

    "rm: know super/feel_year/ask_hand/find_next_time/group/day_and_man.py or super/child_and_man/different_group/young_time_and_number/life/young_week/time.py from few fact"

---
+++ super/child_and_man/different_group/young_time_and_number/life/young_week/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_woman_into_problem(str_arg):
+    number(str_arg)
+    print('group')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_woman_into_problem('know_important_way_over_child')
