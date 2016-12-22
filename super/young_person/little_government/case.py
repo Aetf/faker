7e6dc1ec660cbf1155af84479dfc326d3d92375b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:46:55 -0500

    "mix: tell super/week/fact/number.py and super/young_person/little_government/case.py in right problem"

---
+++ super/young_person/little_government/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    early_work(str_arg)
+    print('person_or_company')
+
+def early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('little_way')
