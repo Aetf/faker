a73a6dbf9c808bec1f3a3e1527fe67cedd1de2d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 13:00:13 -0500

    "add: make super/bad_thing_or_number/part.py by small number"

---
+++ super/bad_thing_or_number/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_number(str_arg):
+    week_or_good_woman(str_arg)
+    print('place_or_day')
+
+def week_or_good_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_number('man_or_man')
