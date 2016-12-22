7cce039a19552c9b5ed3fe81d24c748cec917ced
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 12:12:22 -0500

    "mod: say super/big_part/right_part.py at important time"

---
+++ super/big_part/right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group_and_hand(str_arg):
+    work_or_eye(str_arg)
+    print('child')
+
+def work_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group_and_hand('day')
