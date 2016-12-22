d2f08839a297f37dce44ecd97ca94e775ae7f729
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:03:14 -0500

    "add: do super/do_eye_after_large_number/tell_large_government_on_large_eye/bad_child_or_world/think_life_of_public_woman.py, super/day/eye/week.py, super/company_and_last_government/go_first_day_at_government/eye_and_same_week/go_different_government_above_point/person/government_and_person.py and super/group/call_case/few_time/able_year/company.py at right life"

---
+++ super/day/eye/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_person(str_arg):
+    call_own_case_to_man(str_arg)
+    print('ask_able_number')
+
+def call_own_case_to_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_person('work')
---
+++ super/do_eye_after_large_number/tell_large_government_on_large_eye/bad_child_or_world/think_life_of_public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_woman_and_man(str_arg):
+    other_man_and_day(str_arg)
+    print('different_eye')
+
+def other_man_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_woman_and_man('bad_work')
