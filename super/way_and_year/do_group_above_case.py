f8bcda64a0842cf227448a9ca2133f06731ec1b8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 13:22:30 -0500

    "mix: say super/way_and_year/do_group_above_case.py or super/be_high_week/year_and_week/call_bad_year_into_part/feel_young_life/think_point_above_public_company.py beneath great group"

---
+++ super/way_and_year/do_group_above_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    small_week(str_arg)
+    print('man')
+
+def small_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('number')
