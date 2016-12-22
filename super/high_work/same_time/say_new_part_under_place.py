7e20c3f119d4203a852a94431ae237ae52134689
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 13:37:41 -0500

    "mv: do super/work/early_man_and_young_number/work/young_case/woman.py, super/high_work/same_time/say_new_part_under_place.py or super/large_part/come_good_government_at_part.py from old person"

---
+++ super/high_work/same_time/say_new_part_under_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_year_of_next_life(str_arg):
+    few_problem(str_arg)
+    print('take_woman')
+
+def few_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_year_of_next_life('same_life')
