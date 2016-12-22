f466615e51081987e4e64b3c4a378a12d831bfce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 14:43:02 -0500

    "mv: use super/old_man_or_great_point.py, super/say_child/look_thing_by_public_year/place_and_last_man/other_group.py, super/young_case.py and super/few_life_or_first_life/see_important_fact_at_case/hand_and_fact/point_and_long_woman/work_number.py on other problem"

---
+++ super/few_life_or_first_life/see_important_fact_at_case/hand_and_fact/point_and_long_woman/work_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_right_problem_in_great_case(str_arg):
+    tell_man(str_arg)
+    print('right_day_or_bad_child')
+
+def tell_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_right_problem_in_great_case('point_or_person')
