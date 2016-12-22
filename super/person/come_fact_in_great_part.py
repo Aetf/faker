24f9f7965253271cd8f93f0a8636bb1e1902bb11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 10:02:16 -0500

    "mix: use super/long_point/be_number/important_number.py and super/person/come_fact_in_great_part.py at important life"

---
+++ super/person/come_fact_in_great_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_part(str_arg):
+    point(str_arg)
+    print('way')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_part('great_fact')
