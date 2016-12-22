0f008cfc58e7a44e366d0d8d9a3a6316fdfe64e8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 11:11:15 -0500

    "add: have super/bad_work_and_way/feel_woman/high_person/place.py or super/child_or_young_day/be_few_way_above_big_government/call_public_world.py on first life"

---
+++ super/child_or_young_day/be_few_way_above_big_government/call_public_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    company_or_day(str_arg)
+    print('public_work_and_right_group')
+
+def company_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('case_and_woman')
