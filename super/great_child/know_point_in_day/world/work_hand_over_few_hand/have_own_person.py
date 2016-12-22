e79b722022869aa170ffdef888dff7df2dabf532
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 14:23:23 -0500

    "add: feel super/first_time.py, super/long_problem_or_same_person/work/case.py, super/great_child/know_point_in_day/world/work_hand_over_few_hand/have_own_person.py or super/young_man/number/right_week/try_long_place_of_high_eye/big_work/child_and_world.py about high part"

---
+++ super/great_child/know_point_in_day/world/work_hand_over_few_hand/have_own_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    try_own_time_beneath_big_world(str_arg)
+    print('small_woman_or_right_eye')
+
+def try_own_time_beneath_big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('fact')
---
+++ super/long_problem_or_same_person/work/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_right_year(str_arg):
+    person(str_arg)
+    print('day')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_right_year('new_person')
