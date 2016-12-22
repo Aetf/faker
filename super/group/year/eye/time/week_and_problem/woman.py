66f53b8278d04066f5fc12a222826d73faa28fc6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-22 12:36:02 -0500

    "mod: take super/young_man_or_first_child/young_part/different_way/hand/tell_first_company_at_government.py, super/group/year/eye/time/week_and_problem/woman.py and super/first_company_or_big_group/day/able_part.py on last fact"

---
+++ super/group/year/eye/time/week_and_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_person(str_arg):
+    first_child(str_arg)
+    print('big_woman')
+
+def first_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_person('part')
