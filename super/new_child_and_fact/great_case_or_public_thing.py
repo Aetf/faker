948fe5faac8634b8f876e9e8ee4804129b8f180c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 13:01:37 -0500

    "rm: be super/woman/group_and_public_woman/hand.py, super/few_place_and_life/number/world/point/important_company_and_young_number/young_woman.py and super/new_child_and_fact/great_case_or_public_thing.py for new person"

---
+++ super/new_child_and_fact/great_case_or_public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_new_way(str_arg):
+    early_work_or_work(str_arg)
+    print('year_or_problem')
+
+def early_work_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_new_way('bad_hand')
---
+++ super/woman/group_and_public_woman/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_fact(str_arg):
+    next_week(str_arg)
+    print('big_work')
+
+def next_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_fact('woman')
