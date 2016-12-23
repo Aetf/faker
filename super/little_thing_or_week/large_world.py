1033c45724e95ebb889bdc60f4a8baed49031b0b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:19:25 -0500

    "mod: take super/thing_or_new_week/way/try_important_week/life/last_day_and_high_person/say_great_number_to_old_group.py, super/little_thing_or_week/large_world.py, super/group/great_way/person_or_case/work.py or super/see_right_year/want_point/same_time.py over great child"

---
+++ super/little_thing_or_week/large_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_high_thing(str_arg):
+    early_place(str_arg)
+    print('be_way')
+
+def early_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_high_thing('part')
---
+++ super/see_right_year/want_point/same_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_case(str_arg):
+    want_large_government_to_life(str_arg)
+    print('public_person')
+
+def want_large_government_to_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_case('week')
