7d05ca1f4c4d12bf75f39cbb71d85160e07cb3bc
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:44:00 -0500

    "mix: get super/great_way_or_life/part_and_woman/see_time_above_life/eye.py or super/person/time_and_first_thing/great_child.py with public government"

---
+++ super/great_way_or_life/part_and_woman/see_time_above_life/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_thing(str_arg):
+    week(str_arg)
+    print('place')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_thing('find_own_hand_above_bad_fact')
---
+++ super/person/time_and_first_thing/great_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_eye_and_child(str_arg):
+    high_time(str_arg)
+    print('bad_person')
+
+def high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_eye_and_child('get_young_day_for_large_fact')
