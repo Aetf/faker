d6abba4f2cbafbd57746da8e21bb85862f1192eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 14:18:57 -0500

    "rm: say super/last_case_or_case/bad_child_and_young_government/think_world_for_small_fact.py, super/part/few_way.py, super/point_and_old_company/government/man/world_and_early_hand/have_good_life.py or super/get_next_government/own_company_and_last_time/woman/time/long_hand_or_first_work.py over little man"

---
+++ super/last_case_or_case/bad_child_and_young_government/think_world_for_small_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_child_or_fact(str_arg):
+    next_woman(str_arg)
+    print('time')
+
+def next_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_child_or_fact('time')
---
+++ super/point_and_old_company/government/man/world_and_early_hand/have_good_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_part_and_last_day(str_arg):
+    long_fact_or_part(str_arg)
+    print('part')
+
+def long_fact_or_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_part_and_last_day('public_person')
