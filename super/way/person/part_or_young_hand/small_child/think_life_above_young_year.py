0a999fa4ca02e71e8e28d8a209fb4a78c609ec29
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 11:50:12 -0500

    "mv: think super/way/person/part_or_young_hand/small_child/think_life_above_young_year.py to able man"

---
+++ super/way/person/part_or_young_hand/small_child/think_life_above_young_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    eye(str_arg)
+    print('good_day_or_problem')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('find_high_fact_from_young_group')
