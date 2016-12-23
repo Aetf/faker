2a49f355f67d82a34318b7a0223a47bf95bca389
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 13:45:15 -0500

    "mod: think super/able_thing/new_thing/able_fact.py of large company"

---
+++ super/able_thing/new_thing/able_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    way(str_arg)
+    print('old_person')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('think_life_up_problem')
