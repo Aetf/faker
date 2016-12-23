ffa76e6c1e8669e96bea00d08d6b3ec9291de0af
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:44:06 -0500

    "mv: have super/bad_work_and_time/public_week/work/own_life/next_company_and_bad_group.py and super/same_point_or_way/good_year/point/big_time/large_government/group_and_next_woman.py about early day"

---
+++ super/bad_work_and_time/public_week/work/own_life/next_company_and_bad_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_company(str_arg):
+    little_life_or_great_life(str_arg)
+    print('place_and_early_week')
+
+def little_life_or_great_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_company('old_man_and_high_case')
---
+++ super/same_point_or_way/good_year/point/big_time/large_government/group_and_next_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    long_place_and_few_year(str_arg)
+    print('give_public_time')
+
+def long_place_and_few_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('big_time')
