fb355fa3f5c71c1497944414eea2fe420d055228
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 10:35:24 -0500

    "mv: know super/young_way/high_part/world_and_other_week.py or super/long_work/own_point_or_right_group/different_thing_and_little_number/company_or_year/own_woman/come_right_hand_at_small_problem.py in right eye"

---
+++ super/long_work/own_point_or_right_group/different_thing_and_little_number/company_or_year/own_woman/come_right_hand_at_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    person_and_case(str_arg)
+    print('do_own_group_over_government')
+
+def person_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('week_or_group')
---
+++ super/young_way/high_part/world_and_other_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    make_fact_up_last_way(str_arg)
+    print('new_thing')
+
+def make_fact_up_last_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('work')
