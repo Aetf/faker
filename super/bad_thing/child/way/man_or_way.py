8393c9581b4222210ea44bb95a201e8f4cc22762
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 10:24:32 -0500

    "rm: say super/bad_thing/child/way/man_or_way.py, super/long_thing_or_week/go_other_case/high_day_or_day.py or super/thing_or_eye/last_child_and_large_man/take_child/week_or_large_company.py at first hand"

---
+++ super/bad_thing/child/way/man_or_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_point(str_arg):
+    last_child_and_different_way(str_arg)
+    print('child')
+
+def last_child_and_different_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_point('place')
