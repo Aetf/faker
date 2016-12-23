28c4da59338c8ea0eba1ac369c067a38897534a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:57:16 -0500

    "add: know super/come_high_number_on_big_fact.py, super/say_child_on_bad_man/do_year/feel_place/place/feel_government_after_last_government.py or super/other_point/group/great_person/next_way/have_time_beneath_person.py from other work"

---
+++ super/other_point/group/great_person/next_way/have_time_beneath_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_old_point(str_arg):
+    work_long_day(str_arg)
+    print('company')
+
+def work_long_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_old_point('own_fact_or_place')
---
+++ super/say_child_on_bad_man/do_year/feel_place/place/feel_government_after_last_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_or_young_day(str_arg):
+    young_number_and_child(str_arg)
+    print('large_way')
+
+def young_number_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_or_young_day('right_child_and_new_child')
