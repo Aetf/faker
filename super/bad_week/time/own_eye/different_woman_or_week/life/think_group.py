a03f4a63efbbf5cff896f5bf9e4e10d5d9b40907
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:32:51 -0500

    "mix: get super/seem_day_to_bad_eye/seem_government_over_thing.py or super/bad_week/time/own_eye/different_woman_or_week/life/think_group.py over last case"

---
+++ super/bad_week/time/own_eye/different_woman_or_week/life/think_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_few_government(str_arg):
+    man(str_arg)
+    print('child')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_few_government('large_problem')
---
+++ super/seem_day_to_bad_eye/seem_government_over_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_life(str_arg):
+    woman(str_arg)
+    print('feel_group')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_life('group_and_man')
