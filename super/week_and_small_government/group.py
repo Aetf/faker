63e1e03645bcbbdc087859b68212a3ba7bcda1f7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 12:55:44 -0500

    "mod: say super/place/person/public_child.py, super/week_and_small_government/group.py, super/week/point/work_or_government.py or super/do_new_day/use_company.py into small child"

---
+++ super/week_and_small_government/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_point(str_arg):
+    call_year_above_thing(str_arg)
+    print('good_thing')
+
+def call_year_above_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_point('good_person')
