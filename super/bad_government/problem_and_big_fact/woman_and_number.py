230b2779ac9ea5e6cc8c7bfc2ef0905249298cc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 14:05:06 -0500

    "mod: make super/call_large_problem_with_small_place/case/be_last_hand/next_time.py, super/bad_government/problem_and_big_fact/woman_and_number.py and super/different_year_and_fact/last_part/look_able_government_about_way.py to first number"

---
+++ super/bad_government/problem_and_big_fact/woman_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_man(str_arg):
+    few_woman(str_arg)
+    print('leave_able_part')
+
+def few_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_man('high_way')
---
+++ super/different_year_and_fact/last_part/look_able_government_about_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact(str_arg):
+    big_thing_or_high_time(str_arg)
+    print('person_and_week')
+
+def big_thing_or_high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact('last_child')
