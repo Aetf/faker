14565419f8ff168756fb276335f6f337afa630a9
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 14:17:16 -0500

    "mod: get super/large_point/able_place/good_life_and_important_thing/young_person.py or super/give_man/large_work_or_company/woman/right_case/part/man.py in next hand"

---
+++ super/give_man/large_work_or_company/woman/right_case/part/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_work(str_arg):
+    find_small_hand(str_arg)
+    print('day_and_little_way')
+
+def find_small_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_work('child_or_good_case')
