ade2f3c2f4d0f64dedfaad0b6d26ba985e1d2ffb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 10:38:42 -0500

    "mv: get super/first_place/large_man_and_same_group/new_fact_or_great_work/work_or_day/bad_fact.py up other part"

---
+++ super/first_place/large_man_and_same_group/new_fact_or_great_work/work_or_day/bad_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_able_place(str_arg):
+    place_or_way(str_arg)
+    print('few_woman')
+
+def place_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_able_place('problem')
