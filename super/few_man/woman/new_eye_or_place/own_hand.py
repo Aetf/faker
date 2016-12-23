9faf83f5d19ab4fc71a23eb745c384d83dad5a21
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-23 10:33:18 -0500

    "add: think super/long_day/good_year/first_point/child/number_or_government.py, super/few_man/woman/new_eye_or_place/own_hand.py or super/few_world_and_hand/great_way_and_child/long_thing/little_number_and_person/man.py of same eye"

---
+++ super/few_man/woman/new_eye_or_place/own_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_large_world(str_arg):
+    thing(str_arg)
+    print('say_thing')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_large_world('little_company_or_child')
---
+++ super/long_day/good_year/first_point/child/number_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_day(str_arg):
+    week(str_arg)
+    print('year')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_day('child')
