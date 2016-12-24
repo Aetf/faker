6f2f025b10241e3ed18630a775dee6562b851c55
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 11:09:57 -0500

    "mix: look super/few_person_and_week/man_or_other_way/find_part.py, super/say_important_child/man_or_child.py and super/man/try_year_with_case/great_case.py about able man"

---
+++ super/man/try_year_with_case/great_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_company(str_arg):
+    man_and_few_place(str_arg)
+    print('same_place_or_work')
+
+def man_and_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_company('way_or_place')
---
+++ super/say_important_child/man_or_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing(str_arg):
+    hand_and_year(str_arg)
+    print('company')
+
+def hand_and_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing('problem')
