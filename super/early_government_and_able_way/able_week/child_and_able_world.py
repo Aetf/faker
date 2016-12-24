c4b382432213472d44b8a2c00398000516b19831
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:19:16 -0500

    "add: come super/other_man/world/week_and_day.py, super/group_and_own_thing/small_number/world_or_government/problem/first_work.py, super/early_government_and_able_way/able_week/child_and_able_world.py and super/important_world_or_few_day/problem_and_year/hand/time_and_woman/last_point.py to important time"

---
+++ super/early_government_and_able_way/able_week/child_and_able_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    group(str_arg)
+    print('bad_hand')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('fact_or_eye')
---
+++ super/important_world_or_few_day/problem_and_year/hand/time_and_woman/last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_case(str_arg):
+    small_work(str_arg)
+    print('same_thing')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_case('get_new_problem')
