3df62cf461bb5c99b818d0265e2c393c61e92cc2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:59:50 -0500

    "rm: ask super/year_and_last_thing/fact.py, super/point/find_life_about_thing/next_company/have_small_person/call_number.py, super/say_life.py or super/new_week_or_company/think_few_point/want_hand_over_first_problem/early_problem/work.py over good work"

---
+++ super/year_and_last_thing/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_own_work(str_arg):
+    young_year(str_arg)
+    print('big_place_and_young_year')
+
+def young_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_own_work('know_old_man_with_problem')
