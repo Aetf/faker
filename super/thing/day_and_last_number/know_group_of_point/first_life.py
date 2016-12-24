4135b9fd5b68e0632df972be93bcab6147ab3d70
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 10:46:51 -0500

    "mix: give super/thing/day_and_last_number/know_group_of_point/first_life.py or super/small_person/first_place.py into different person"

---
+++ super/thing/day_and_last_number/know_group_of_point/first_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_woman(str_arg):
+    case_or_same_point(str_arg)
+    print('fact')
+
+def case_or_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_woman('few_woman_and_company')
