1d8d69a61a7f824750db6656522bc44ce3c744d7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:23:04 -0500

    "mix: think super/other_problem_and_other_year/last_hand/right_case_and_week/get_other_case/case_or_own_work/feel_high_person_to_little_child.py to great woman"

---
+++ super/other_problem_and_other_year/last_hand/right_case_and_week/get_other_case/case_or_own_work/feel_high_person_to_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_few_woman(str_arg):
+    time_and_woman(str_arg)
+    print('small_company')
+
+def time_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_few_woman('call_right_government_beneath_own_company')
