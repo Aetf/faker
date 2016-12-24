e3c7b06a8d30e0bdc5b173eb9abdf0426ad3c0e0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 10:50:15 -0500

    "mix: look super/own_company/good_thing_or_life/problem/see_world.py and super/time/problem_and_company/important_world.py under good child"

---
+++ super/time/problem_and_company/important_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    long_week(str_arg)
+    print('go_way')
+
+def long_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('life')
