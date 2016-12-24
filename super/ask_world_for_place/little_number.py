e66687b8b0068b52e6de9f302e5116599ce9901b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 11:13:37 -0500

    "mv: think super/try_day/group_or_way/high_year.py, super/ask_world_for_place/little_number.py, super/little_work/want_government.py or super/same_group_and_old_fact/tell_man/work.py at last person"

---
+++ super/ask_world_for_place/little_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    other_case_or_person(str_arg)
+    print('know_group')
+
+def other_case_or_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('young_way')
---
+++ super/same_group_and_old_fact/tell_man/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_fact(str_arg):
+    make_bad_part(str_arg)
+    print('great_company')
+
+def make_bad_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_fact('leave_young_point_under_world')
