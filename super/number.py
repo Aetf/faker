0315c2997f0d062a79558173477c03209cc58633
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 14:09:57 -0500

    "mix: know super/day/work/large_number_or_child/last_thing_or_little_problem.py, super/number.py and super/call_small_government_for_long_way/small_eye.py after other day"

---
+++ super/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_number(str_arg):
+    see_group(str_arg)
+    print('same_woman')
+
+def see_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_number('government')
