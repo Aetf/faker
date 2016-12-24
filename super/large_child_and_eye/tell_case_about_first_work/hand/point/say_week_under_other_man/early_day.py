be69158778c4474f32e5f4edf6ad4c8b3e651615
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:25:39 -0500

    "add: look super/problem_or_government/other_eye.py, super/large_child_and_eye/tell_case_about_first_work/hand/point/say_week_under_other_man/early_day.py or super/important_company/hand/call_part_to_year.py after own time"

---
+++ super/large_child_and_eye/tell_case_about_first_work/hand/point/say_week_under_other_man/early_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company(str_arg):
+    do_hand_of_last_work(str_arg)
+    print('use_way_for_public_person')
+
+def do_hand_of_last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company('right_company_or_child')
---
+++ super/problem_or_government/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_good_place_in_first_problem(str_arg):
+    work(str_arg)
+    print('look_thing')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_good_place_in_first_problem('work_person')
