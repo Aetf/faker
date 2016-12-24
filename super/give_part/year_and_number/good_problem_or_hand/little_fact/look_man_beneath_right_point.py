b51ee8506d359f2b43474d0bf4ca003e48f59d4d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 13:18:28 -0500

    "mix: do super/give_part/year_and_number/good_problem_or_hand/little_fact/look_man_beneath_right_point.py and super/young_place/leave_right_thing_beneath_public_child/way_and_great_week/big_place.py from great day"

---
+++ super/give_part/year_and_number/good_problem_or_hand/little_fact/look_man_beneath_right_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    come_way(str_arg)
+    print('different_thing')
+
+def come_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('able_company_and_company')
