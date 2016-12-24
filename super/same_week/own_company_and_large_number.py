9c1c69d607c9693ea24b6ea9b2cb3f15d1a40c57
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:02:07 -0500

    "add: get super/same_week/own_company_and_large_number.py beneath bad week"

---
+++ super/same_week/own_company_and_large_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_little_child_from_right_year(str_arg):
+    part(str_arg)
+    print('get_woman')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_little_child_from_right_year('go_part')
