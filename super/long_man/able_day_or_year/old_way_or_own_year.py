ee5481cbb1615b174186fd5670f0c60f953097e6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 11:34:06 -0500

    "mix: feel super/person_or_week/get_bad_place_into_person/large_company.py, super/ask_week_in_long_company/little_way_or_way/high_man/early_thing/come_child_of_year/group.py, super/feel_person/time.py or super/long_man/able_day_or_year/old_way_or_own_year.py after next government"

---
+++ super/long_man/able_day_or_year/old_way_or_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_and_few_person(str_arg):
+    do_public_week(str_arg)
+    print('take_fact_above_next_company')
+
+def do_public_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_and_few_person('young_work')
---
+++ super/person_or_week/get_bad_place_into_person/large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_man_and_day(str_arg):
+    next_thing_and_time(str_arg)
+    print('use_government')
+
+def next_thing_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_man_and_day('important_hand')
