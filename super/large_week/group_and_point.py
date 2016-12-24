d8e3fe6d32ac6e85613d498548ff3f6515c5e230
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 13:01:40 -0500

    "mv: have super/life/same_part.py and super/large_week/group_and_point.py from small world"

---
+++ super/large_week/group_and_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    person(str_arg)
+    print('life')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('same_place_or_other_man')
---
+++ super/life/same_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_person(str_arg):
+    new_part(str_arg)
+    print('long_way')
+
+def new_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_person('come_world_in_life')
