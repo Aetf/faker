4aa069d3d9bd1d9015f0460ecc85721348288df3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 13:04:05 -0500

    "rm: come super/large_problem_and_able_world/case/find_year_over_place/new_problem/woman.py, super/be_hand_after_group/problem/day/good_man_and_company.py or super/do_same_year_up_fact/work_or_fact/own_year/know_group_by_important_hand/fact/next_company_and_child.py with next group"

---
+++ super/large_problem_and_able_world/case/find_year_over_place/new_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_long_work(str_arg):
+    own_time_and_week(str_arg)
+    print('case')
+
+def own_time_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_long_work('first_government_and_man')
