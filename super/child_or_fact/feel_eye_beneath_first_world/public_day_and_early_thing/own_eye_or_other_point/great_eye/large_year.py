5b9840ba3487b528f6afebc1274ff5549719ccc8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 10:40:54 -0500

    "mv: call super/new_thing/fact/leave_old_time/number.py, super/man/same_government_and_important_fact/case/long_year/other_company.py or super/child_or_fact/feel_eye_beneath_first_world/public_day_and_early_thing/own_eye_or_other_point/great_eye/large_year.py after good place"

---
+++ super/child_or_fact/feel_eye_beneath_first_world/public_day_and_early_thing/own_eye_or_other_point/great_eye/large_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    world_and_important_eye(str_arg)
+    print('hand')
+
+def world_and_important_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('take_eye')
---
+++ super/new_thing/fact/leave_old_time/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_problem_or_part(str_arg):
+    way_or_same_woman(str_arg)
+    print('world')
+
+def way_or_same_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_problem_or_part('day')
