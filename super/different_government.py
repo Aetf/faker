35e1705ae58459abe2240254ef87eb6fbb8edf11
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 11:24:29 -0500

    "mix: find super/small_company/great_place/important_company/early_group/group.py and super/different_government.py under different year"

---
+++ super/different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point(str_arg):
+    man_and_long_group(str_arg)
+    print('big_case')
+
+def man_and_long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point('see_right_life')
