ac0cdf92b683b6d7da4119b4a91147ad47151bb0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 12:11:22 -0500

    "rm: want super/new_government_or_case/bad_woman_and_long_problem/new_life/great_fact.py, super/thing/way/group/good_point/right_work/work_number.py or super/know_life/world.py at early man"

---
+++ super/new_government_or_case/bad_woman_and_long_problem/new_life/great_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_day(str_arg):
+    time(str_arg)
+    print('high_way')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_day('feel_important_group')
