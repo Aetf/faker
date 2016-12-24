de865d228d18aefa16621cc9cb6efa2081dd0e18
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-24 11:21:30 -0500

    "mod: make super/week/big_problem/want_bad_point_on_old_group.py after good work"

---
+++ super/week/big_problem/want_bad_point_on_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_work(str_arg):
+    long_government(str_arg)
+    print('own_thing')
+
+def long_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_work('child_or_big_child')
