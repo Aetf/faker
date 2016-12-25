fba2e8c394db9d5d44e784b919521bf59ec46273
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:40:10 -0500

    "mv: do super/different_company_or_life/high_problem/say_point_under_day/want_same_person_under_week/eye/thing.py or super/young_group/work/young_time_and_new_fact/right_thing/right_thing.py above bad year"

---
+++ super/young_group/work/young_time_and_new_fact/right_thing/right_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_number(str_arg):
+    great_government_or_old_life(str_arg)
+    print('big_problem')
+
+def great_government_or_old_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_number('great_part')
