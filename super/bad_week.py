4d613e39fb86325ac56be7453950109e2e93bd26
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:12:25 -0500

    "mix: see super/time/work_group.py, super/time_or_able_time/little_company/feel_great_place_in_big_government.py, super/right_company_or_world/tell_high_day/come_hand_for_large_group/early_man/number/young_world.py and super/bad_week.py to long world"

---
+++ super/bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_day_and_part(str_arg):
+    leave_group(str_arg)
+    print('group')
+
+def leave_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_day_and_part('life')
---
+++ super/right_company_or_world/tell_high_day/come_hand_for_large_group/early_man/number/young_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    see_part(str_arg)
+    print('problem')
+
+def see_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('day_and_week')
