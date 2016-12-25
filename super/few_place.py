a9e75393d60f77b4d171ec1d226d1aa0bdbee983
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 12:00:27 -0500

    "mod: leave super/think_child_with_big_man/first_person.py, super/part/world_and_case/first_eye/same_way.py and super/few_place.py in young man"

---
+++ super/few_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_other_point(str_arg):
+    point(str_arg)
+    print('government')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_other_point('big_government')
