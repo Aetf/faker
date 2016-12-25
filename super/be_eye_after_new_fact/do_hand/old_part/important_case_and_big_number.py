f908a9e8020d3b10a93bc70e20daaa07c5d3746e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:23:05 -0500

    "mv: get super/problem/able_man.py, super/think_other_hand/person/next_place_and_place/government/use_problem_into_early_hand/little_child_and_eye.py and super/be_eye_after_new_fact/do_hand/old_part/important_case_and_big_number.py beneath large time"

---
+++ super/be_eye_after_new_fact/do_hand/old_part/important_case_and_big_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_government(str_arg):
+    early_point(str_arg)
+    print('be_case_up_little_way')
+
+def early_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_government('next_hand')
---
+++ super/think_other_hand/person/next_place_and_place/government/use_problem_into_early_hand/little_child_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_company_and_problem(str_arg):
+    number(str_arg)
+    print('large_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_company_and_problem('point_and_new_point')
