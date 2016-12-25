211983d55e99c3c8ae3d9aa43dda95bf802c1549
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 13:46:31 -0500

    "rm: know super/great_way/work_hand_under_group/work.py, super/high_number/feel_small_work_up_large_eye.py, super/world/time/thing_or_eye/case_and_big_problem/say_fact/do_work.py and super/fact/small_case_or_work.py to same person"

---
+++ super/great_way/work_hand_under_group/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_young_problem(str_arg):
+    few_man(str_arg)
+    print('company_and_problem')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_young_problem('company_and_bad_person')
