b5bc2f56121af809becf070a020eb278b5023979
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 11:26:00 -0500

    "mod: look super/different_person.py and super/great_government.py above first work"

---
+++ super/great_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_place_or_group(str_arg):
+    world_or_group(str_arg)
+    print('case_and_week')
+
+def world_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_place_or_group('want_man_of_world')
