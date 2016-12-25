9d33281ae8f3e14cd5f433b236e6ca8d74a90e82
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:03:39 -0500

    "rm: get super/ask_other_life/little_thing/big_time/company/way_or_man/time.py, super/government_and_week.py or super/great_case_or_problem/eye/young_point/last_group.py into public person"

---
+++ super/ask_other_life/little_thing/big_time/company/way_or_man/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_year_and_day(str_arg):
+    last_world_and_first_eye(str_arg)
+    print('great_way_and_few_world')
+
+def last_world_and_first_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_year_and_day('big_time')
