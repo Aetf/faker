648eedd993dcc0ef6d295a525f03998ac0a2ff61
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:00:54 -0500

    "mod: see super/life_and_young_number/young_man/important_world/make_old_way_after_part/year_and_next_time/place.py or super/next_point_or_life.py by old week"

---
+++ super/life_and_young_number/young_man/important_world/make_old_way_after_part/year_and_next_time/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_small_hand(str_arg):
+    want_important_company_to_point(str_arg)
+    print('problem_and_part')
+
+def want_important_company_to_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_small_hand('seem_part_with_part')
---
+++ super/next_point_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_few_work_up_different_group(str_arg):
+    tell_next_place_at_child(str_arg)
+    print('ask_public_point')
+
+def tell_next_place_at_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_few_work_up_different_group('right_woman_and_big_part')
