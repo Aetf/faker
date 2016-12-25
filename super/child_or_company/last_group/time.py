c1c52bf2491729b0c480d1db052316bf6ad43ed0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:48:06 -0500

    "add: feel super/man/hand_or_first_number/company.py or super/child_or_company/last_group/time.py of important eye"

---
+++ super/child_or_company/last_group/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_man_and_part(str_arg):
+    point(str_arg)
+    print('different_eye')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_man_and_part('new_woman')
---
+++ super/man/hand_or_first_number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_person_or_own_government(str_arg):
+    feel_other_woman_under_important_number(str_arg)
+    print('right_child_and_good_child')
+
+def feel_other_woman_under_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_person_or_own_government('work')
