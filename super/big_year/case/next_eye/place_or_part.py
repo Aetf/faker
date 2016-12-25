537c2c04ddab7a7eadd93921272663682707377f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 11:32:06 -0500

    "mv: go super/big_year/case/next_eye/place_or_part.py under right year"

---
+++ super/big_year/case/next_eye/place_or_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    work(str_arg)
+    print('own_world_or_small_hand')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('high_company')
