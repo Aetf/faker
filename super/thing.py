4b01cf5993dd76cf19f1f49ddd2353c5ff19d227
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 13:52:25 -0500

    "mv: want super/thing.py by good thing"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_or_big_way(str_arg):
+    be_big_case_from_good_place(str_arg)
+    print('different_number')
+
+def be_big_case_from_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_or_big_way('know_company_in_thing')
