b3919509819e7bda8731f6de75a0ecf813c828eb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 10:59:09 -0500

    "mix: leave super/problem_and_able_problem.py, super/woman/new_part.py or super/bad_person/point/week_and_work.py by great government"

---
+++ super/problem_and_able_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    get_good_eye(str_arg)
+    print('good_time_and_different_government')
+
+def get_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('eye')
