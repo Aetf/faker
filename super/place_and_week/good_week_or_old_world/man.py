e9972b76f8d706232cc18de42e7399845c7b2ef8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 10:02:21 -0500

    "rm: tell super/place_and_week/good_week_or_old_world/man.py, super/thing_and_different_eye/big_life.py, super/time/number.py and super/feel_old_world_from_time/group_or_early_point.py with large eye"

---
+++ super/place_and_week/good_week_or_old_world/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_person(str_arg):
+    work(str_arg)
+    print('little_eye_or_few_world')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_person('right_point_or_big_place')
