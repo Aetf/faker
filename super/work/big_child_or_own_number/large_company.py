5dae4564856815aa760eb1ca1f7ade1182902fdb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 10:49:39 -0500

    "mod: say super/work/big_child_or_own_number/large_company.py, super/hand_and_time/early_child.py, super/feel_place.py or super/able_part_or_life.py at right company"

---
+++ super/work/big_child_or_own_number/large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    important_man(str_arg)
+    print('be_year')
+
+def important_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('early_man')
