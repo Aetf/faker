665736ffb8a3bd8fd9580e8a59a823717dbf02f0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 11:49:23 -0500

    "add: seem super/child/big_week_and_case/last_life/man/first_company/come_first_way_above_man.py, super/think_man/other_group/think_person_with_right_week/go_thing_with_early_number/long_government.py, super/small_fact/different_time_and_long_government/old_place/make_long_number_with_part.py or super/seem_high_way/use_hand.py from old week"

---
+++ super/seem_high_way/use_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_day(str_arg):
+    world(str_arg)
+    print('think_long_thing')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_day('group')
