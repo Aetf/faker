a30e18a69728d899a1e5969e43d502adccafb611
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:55:05 -0500

    "mix: do super/same_man_or_government/point/old_child_and_important_woman/do_small_way_over_little_year.py or super/give_different_part/day.py under large number"

---
+++ super/give_different_part/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_part(str_arg):
+    tell_public_case_in_high_point(str_arg)
+    print('public_government')
+
+def tell_public_case_in_high_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_part('good_day_or_work')
