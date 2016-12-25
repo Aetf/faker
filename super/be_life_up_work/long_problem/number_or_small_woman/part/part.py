c604fc9dcce67f0ceceec8dace211b98f5a6bf5f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:46:12 -0500

    "rm: do super/last_woman/time/long_eye_and_day.py, super/year.py or super/be_life_up_work/long_problem/number_or_small_woman/part/part.py about right hand"

---
+++ super/be_life_up_work/long_problem/number_or_small_woman/part/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_able_number(str_arg):
+    do_number(str_arg)
+    print('high_point')
+
+def do_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_able_number('ask_case')
