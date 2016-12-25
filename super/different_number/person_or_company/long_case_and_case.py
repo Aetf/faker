d1e8e2e27c96814a88872c430b178d67d1d3b933
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 14:26:53 -0500

    "rm: leave super/different_number/person_or_company/long_case_and_case.py for small number"

---
+++ super/different_number/person_or_company/long_case_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_life(str_arg):
+    child_and_young_woman(str_arg)
+    print('last_number_and_woman')
+
+def child_and_young_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_life('place_or_long_way')
