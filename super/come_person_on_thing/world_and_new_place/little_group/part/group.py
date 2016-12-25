0a1e8b61207e30a192c09195e51e5eb1d69621a6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 10:12:27 -0500

    "mix: see super/say_company_by_person.py, super/use_woman_to_long_part/seem_part_on_early_thing/new_woman/point.py, super/come_person_on_thing/world_and_new_place/little_group/part/group.py or super/important_year.py to important world"

---
+++ super/come_person_on_thing/world_and_new_place/little_group/part/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_case(str_arg):
+    point_or_thing(str_arg)
+    print('tell_first_group_under_life')
+
+def point_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_case('give_able_life_after_time')
---
+++ super/use_woman_to_long_part/seem_part_on_early_thing/new_woman/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    way(str_arg)
+    print('little_fact_or_place')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('call_woman_from_fact')
