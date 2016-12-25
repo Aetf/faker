598339ba3252db4a1ac13a85e2b50f83f083d00c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-25 13:01:45 -0500

    "mv: say super/few_part.py about old day"

---
+++ super/few_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_man_up_person(str_arg):
+    case_or_man(str_arg)
+    print('ask_next_year')
+
+def case_or_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_man_up_person('see_time')
