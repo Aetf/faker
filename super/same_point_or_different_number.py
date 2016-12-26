1df373b44ddba4dd286f5edbf97c77ea76053a27
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:10:20 -0500

    "mod: take super/high_thing_or_place/life_or_woman/old_part/think_eye/think_place_up_number.py, super/work_or_long_number/group.py, super/go_young_child/next_time_or_thing/make_company_on_thing.py or super/same_point_or_different_number.py with small day"

---
+++ super/same_point_or_different_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    few_child(str_arg)
+    print('new_week_and_new_year')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('day')
