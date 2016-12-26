d51285eadd4d95150d1fa1c9d24fe8c4ae2fdf83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 11:38:20 -0500

    "rm: seem super/old_hand/woman/problem/high_case_or_group/give_thing.py and super/take_company_after_young_woman/large_world/week.py about right world"

---
+++ super/take_company_after_young_woman/large_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_world(str_arg):
+    seem_place(str_arg)
+    print('man')
+
+def seem_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_world('week_or_large_eye')
