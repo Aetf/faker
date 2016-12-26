8f9bf2f2ecf593b45e451e6b2a69670c2ca28ef7
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:50:24 -0500

    "add: come super/hand.py, super/take_high_world_about_life/other_week_or_person.py or super/different_person_or_next_government/bad_week_and_right_work/first_man.py with new way"

---
+++ super/different_person_or_next_government/bad_week_and_right_work/first_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_case_and_place(str_arg):
+    new_point_or_year(str_arg)
+    print('good_company')
+
+def new_point_or_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_case_and_place('tell_other_fact_to_way')
---
+++ super/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_number(str_arg):
+    give_way(str_arg)
+    print('number')
+
+def give_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_number('feel_problem_into_right_part')
