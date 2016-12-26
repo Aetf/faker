30735aeabdef1985486267a5320923cc7d5db428
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 13:26:40 -0500

    "mix: think super/public_man/part/think_part_of_way/problem_and_place/able_part_and_hand/feel_way_to_group.py, super/man/think_great_government_for_next_place/small_world/high_case/bad_part_and_year/time.py, super/woman.py or super/few_way/work/important_world.py for own part"

---
+++ super/public_man/part/think_part_of_way/problem_and_place/able_part_and_hand/feel_way_to_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_same_person(str_arg):
+    new_week(str_arg)
+    print('long_number')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_same_person('life_and_right_time')
