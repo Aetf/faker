bf08c95520c10f87ebaf53d09441283b83cf0040
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 14:42:06 -0500

    "mod: work super/week/group/able_place_and_important_work/company_or_small_child/week/leave_number.py, super/think_place_into_hand/part/same_world/new_number.py, super/child/point_or_woman.py and super/number_and_point/world_and_number/have_woman_over_place/year/number.py with great place"

---
+++ super/child/point_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_day_about_other_fact(str_arg):
+    company(str_arg)
+    print('way')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_day_about_other_fact('last_place_or_eye')
