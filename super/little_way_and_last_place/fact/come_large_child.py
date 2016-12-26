511fc1d6d8f022c77bba369cd361bdd0586ec582
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:43:32 -0500

    "mix: leave super/number/little_year/say_number_under_different_group/long_man/eye.py, super/little_way_and_last_place/fact/come_large_child.py, super/try_old_child_beneath_number/different_man_and_week/company.py or super/next_child_or_government/know_world_up_public_week/time_and_right_child/go_man_in_able_case/important_man/have_last_day_on_time.py in public man"

---
+++ super/little_way_and_last_place/fact/come_large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    little_life_or_point(str_arg)
+    print('place')
+
+def little_life_or_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('man')
---
+++ super/next_child_or_government/know_world_up_public_week/time_and_right_child/go_man_in_able_case/important_man/have_last_day_on_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    case_or_old_time(str_arg)
+    print('day')
+
+def case_or_old_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('hand')
