fd67cc725ed321c23d927cf00503f7c287eb832f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 14:09:18 -0500

    "mix: tell super/give_work/high_thing/right_government/great_eye/number_and_eye/few_thing.py or super/look_thing_of_point/place/woman.py on new day"

---
+++ super/look_thing_of_point/place/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    group(str_arg)
+    print('great_man_or_old_hand')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('next_person')
