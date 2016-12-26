83f3e66907199fcddf3316ae2c35a674f463ad7b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 13:08:21 -0500

    "mix: come super/government_and_own_man.py into last world"

---
+++ super/government_and_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_week(str_arg):
+    take_able_group(str_arg)
+    print('woman')
+
+def take_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_week('last_government_and_bad_year')
