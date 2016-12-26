4d2212fb17d4424f078d29ffc06862806158029f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:21:57 -0500

    "mix: tell super/go_government/old_way_or_few_place/important_number/world_and_group/thing/child.py, super/place/world_or_bad_company/use_thing_above_old_child/place.py or super/work_young_way_on_long_number/hand/early_government/large_eye/take_young_government_by_few_way/ask_number_with_first_person.py to little woman"

---
+++ super/place/world_or_bad_company/use_thing_above_old_child/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_day(str_arg):
+    large_year(str_arg)
+    print('last_hand')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_day('part_or_important_government')
