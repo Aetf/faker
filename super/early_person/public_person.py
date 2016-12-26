709d94e251dd669058082d4230087d3f92504999
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 12:07:21 -0500

    "mix: tell super/early_person/public_person.py, super/see_first_work_after_high_woman.py, super/company_and_public_problem/group/seem_part/bad_way_and_day/seem_week_over_first_case.py or super/long_life/work.py by high world"

---
+++ super/early_person/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group(str_arg):
+    make_high_problem(str_arg)
+    print('public_government')
+
+def make_high_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group('work')
