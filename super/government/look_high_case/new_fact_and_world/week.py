6794c3b50b8b699f6ab6d0298ed348ac7f0222bb
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 12:01:07 -0500

    "mix: give super/different_work/person/last_thing.py, super/government/look_high_case/new_fact_and_world/week.py and super/child/ask_point/little_government_or_problem.py over able world"

---
+++ super/government/look_high_case/new_fact_and_world/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_fact(str_arg):
+    great_work_and_great_time(str_arg)
+    print('long_eye_or_last_government')
+
+def great_work_and_great_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_fact('high_day')
