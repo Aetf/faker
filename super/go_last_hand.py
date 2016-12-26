90ba431e045e157b8443ad1458488b6b010d9f94
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 11:43:29 -0500

    "mv: call super/go_last_hand.py of old life"

---
+++ super/go_last_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_old_work(str_arg):
+    important_case_or_good_eye(str_arg)
+    print('case')
+
+def important_case_or_good_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_old_work('problem_or_big_problem')
