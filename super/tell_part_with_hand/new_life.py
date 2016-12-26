5862a863cafb821e9dbb9005410966c4dc99bcad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:41:23 -0500

    "mix: tell super/public_day/day/eye_and_different_problem/few_woman_or_place/next_government/important_day.py, super/week_or_other_work.py, super/child/long_problem.py or super/tell_part_with_hand/new_life.py beneath different child"

---
+++ super/tell_part_with_hand/new_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_new_person(str_arg):
+    way_and_important_day(str_arg)
+    print('young_man')
+
+def way_and_important_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_new_person('number_or_able_company')
