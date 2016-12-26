2882a36473abe2db3c29284ade8783905c24179b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 12:18:12 -0500

    "mix: try super/few_part/man/want_different_person_under_public_thing.py after own government"

---
+++ super/few_part/man/want_different_person_under_public_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    know_next_place(str_arg)
+    print('come_hand_by_thing')
+
+def know_next_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('group')
