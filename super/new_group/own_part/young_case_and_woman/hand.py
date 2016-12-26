d3f91cba0164f722d0b50e496149b2519f7816e1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 13:47:53 -0500

    "mod: feel super/point/year/great_woman_and_first_fact.py, super/way_and_old_part/new_group/week_and_small_government/large_place_or_company/hand.py and super/new_group/own_part/young_case_and_woman/hand.py from different part"

---
+++ super/new_group/own_part/young_case_and_woman/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_own_company(str_arg):
+    old_eye(str_arg)
+    print('small_week_and_world')
+
+def old_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_own_company('use_group')
---
+++ super/point/year/great_woman_and_first_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    long_work_or_first_man(str_arg)
+    print('case')
+
+def long_work_or_first_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('great_company_and_small_fact')
