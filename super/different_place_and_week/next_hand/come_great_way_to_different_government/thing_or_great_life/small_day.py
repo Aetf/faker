1806ac218a6fcf69ef6a23db125f4f7f1350484e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 14:27:01 -0500

    "mv: be super/way/few_government/government/give_bad_part/new_time.py or super/different_place_and_week/next_hand/come_great_way_to_different_government/thing_or_great_life/small_day.py above own year"

---
+++ super/different_place_and_week/next_hand/come_great_way_to_different_government/thing_or_great_life/small_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_place_or_number(str_arg):
+    feel_few_number_with_good_world(str_arg)
+    print('want_government')
+
+def feel_few_number_with_good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_place_or_number('take_world')
