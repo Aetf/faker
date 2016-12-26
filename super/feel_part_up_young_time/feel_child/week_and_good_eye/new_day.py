ecfadfee19fb9f178cf85548756ff6b3e0794e17
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 14:08:04 -0500

    "mv: leave super/day/part/child/woman/world/call_thing.py, super/week/know_new_thing/work_right_life_about_place/high_number/know_large_place/fact.py, super/make_own_point/bad_time/old_group/think_week/work_or_little_way/find_world.py and super/feel_part_up_young_time/feel_child/week_and_good_eye/new_day.py under large world"

---
+++ super/feel_part_up_young_time/feel_child/week_and_good_eye/new_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_number_after_little_eye(str_arg):
+    group(str_arg)
+    print('go_long_life_up_big_man')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_number_after_little_eye('early_point')
