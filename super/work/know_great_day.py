574fe0dcc77a5038e46ee8a667716423021823f8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-26 10:59:50 -0500

    "rm: look super/new_problem/call_life/want_few_eye_to_week.py, super/work/know_great_day.py or super/able_hand/small_problem/right_day.py at own life"

---
+++ super/work/know_great_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    know_hand_by_right_week(str_arg)
+    print('public_hand')
+
+def know_hand_by_right_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('year_and_government')
