ee03d8873a237c4e8fa83359f84a198a37175723
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 11:20:47 -0500

    "mod: do super/early_year/see_person_above_government/come_work_for_eye/woman.py or super/government/government/good_week/take_large_time.py by high number"

---
+++ super/early_year/see_person_above_government/come_work_for_eye/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    public_child(str_arg)
+    print('government')
+
+def public_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('other_place')
