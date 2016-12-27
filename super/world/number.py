47c7ea915c3dc4f7c1146ba6b2a7ec1516b0ab7e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 14:05:13 -0500

    "rm: know super/world/number.py for first life"

---
+++ super/world/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_life(str_arg):
+    go_own_week(str_arg)
+    print('problem')
+
+def go_own_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_life('hand')
