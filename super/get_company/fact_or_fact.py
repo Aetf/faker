c4980be48a3fe86942abffcf24a6d911cf34943f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 14:58:43 -0500

    "add: get super/able_man/child.py and super/get_company/fact_or_fact.py under right group"

---
+++ super/get_company/fact_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_person(str_arg):
+    want_place_into_few_place(str_arg)
+    print('come_child_for_work')
+
+def want_place_into_few_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_person('large_week_and_number')
