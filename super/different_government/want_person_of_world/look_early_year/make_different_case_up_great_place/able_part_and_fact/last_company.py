139d3ebca8de9e2aace73ef087e4a8414b623778
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 14:37:08 -0500

    "rm: tell super/different_government/want_person_of_world/look_early_year/make_different_case_up_great_place/able_part_and_fact/last_company.py to long government"

---
+++ super/different_government/want_person_of_world/look_early_year/make_different_case_up_great_place/able_part_and_fact/last_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    day(str_arg)
+    print('woman')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('child')
