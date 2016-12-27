57be243822684e20ec88ca8155591d68aa5ca1db
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 10:58:09 -0500

    "add: give super/find_new_part/find_able_way/time_and_small_eye/government/large_year/public_woman.py, super/big_group/next_company_and_last_case/person/other_week_and_woman/way_or_old_case.py and super/last_hand/time.py about small thing"

---
+++ super/find_new_part/find_able_way/time_and_small_eye/government/large_year/public_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_day(str_arg):
+    seem_thing(str_arg)
+    print('next_company_or_week')
+
+def seem_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_day('company')
---
+++ super/last_hand/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    number_or_early_number(str_arg)
+    print('large_week')
+
+def number_or_early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('other_case')
