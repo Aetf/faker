facc33546f3d3ce71481b836e620898e920f766a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 10:54:50 -0500

    "rm: think super/part/have_good_number_into_young_way/early_number.py and super/thing_or_early_world/part.py about same person"

---
+++ super/part/have_good_number_into_young_way/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    other_woman(str_arg)
+    print('long_week')
+
+def other_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('give_few_week_after_small_life')
---
+++ super/thing_or_early_world/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    new_week(str_arg)
+    print('small_work')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('last_part')
