33f1fd1df593dd0ead7d9a3bfd2bec21e48dca3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 12:35:29 -0500

    "mv: look super/great_woman_and_long_group.py and super/different_way/important_eye/find_work_with_bad_point/point_or_public_company/point/find_man_above_same_child.py by same person"

---
+++ super/great_woman_and_long_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_thing(str_arg):
+    time(str_arg)
+    print('eye_or_hand')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_thing('place_and_thing')
