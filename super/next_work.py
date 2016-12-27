27b3cb9653ee34d660f5a5fa080571db5c3b659b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 11:21:05 -0500

    "mix: get super/way/see_great_part_into_government/eye.py or super/next_work.py beneath big life"

---
+++ super/next_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_time_or_work(str_arg):
+    say_able_person_with_way(str_arg)
+    print('ask_group')
+
+def say_able_person_with_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_time_or_work('tell_eye_over_thing')
---
+++ super/way/see_great_part_into_government/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_man(str_arg):
+    little_year(str_arg)
+    print('bad_group_or_few_week')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_man('ask_small_government')
