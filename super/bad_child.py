f81d2600618e4fc2656bc4ab32b3c11968881b3d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 11:00:43 -0500

    "add: call super/same_company.py and super/bad_child.py after little man"

---
+++ super/bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_company_over_public_week(str_arg):
+    last_work(str_arg)
+    print('case')
+
+def last_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_company_over_public_week('say_first_problem_to_next_time')
