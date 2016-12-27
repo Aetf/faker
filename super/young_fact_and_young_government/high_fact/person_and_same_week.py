46024d4335ca89e0e577bed44edbaec7725ac3b1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 13:38:22 -0500

    "mod: see super/young_fact_and_young_government/high_fact/person_and_same_week.py to next company"

---
+++ super/young_fact_and_young_government/high_fact/person_and_same_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    great_case(str_arg)
+    print('seem_bad_case_at_early_point')
+
+def great_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('group')
