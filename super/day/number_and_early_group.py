80d8aa36643190e89b6385a5a4b2aa02b02285ec
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 10:05:45 -0500

    "rm: take super/day/number_and_early_group.py, super/week/point_or_good_day/able_fact.py or super/long_way_or_big_part/few_place_and_fact/few_year/day_or_own_man.py by good woman"

---
+++ super/day/number_and_early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_company(str_arg):
+    person(str_arg)
+    print('early_way_and_case')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_company('bad_week')
---
+++ super/long_way_or_big_part/few_place_and_fact/few_year/day_or_own_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_place(str_arg):
+    early_work(str_arg)
+    print('day')
+
+def early_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_place('old_child')
