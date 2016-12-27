48fdf1137d29437258cc72e166f3682639281a52
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 14:47:29 -0500

    "rm: ask super/make_first_person/case/public_way/high_week_and_hand/great_government/go_first_time.py, super/week_or_hand/place_or_young_child/woman_or_case/day_and_group/last_week/see_young_case_over_person.py and super/different_part_and_big_week.py over high way"

---
+++ super/make_first_person/case/public_way/high_week_and_hand/great_government/go_first_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_life(str_arg):
+    big_world_or_first_part(str_arg)
+    print('problem')
+
+def big_world_or_first_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_life('point_or_young_group')
