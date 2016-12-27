04e1b29f49e01925b7e0134ba4e89c7db4806008
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 11:37:07 -0500

    "mod: know super/want_long_fact_in_way/able_company_or_bad_week.py after new fact"

---
+++ super/want_long_fact_in_way/able_company_or_bad_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life_and_point(str_arg):
+    big_world(str_arg)
+    print('call_same_person_up_large_fact')
+
+def big_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life_and_point('little_year')
