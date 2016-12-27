0c1f9199390e8b1a7fb431836ae66e5ee0da974e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-27 11:32:17 -0500

    "rm: call super/go_group/old_world_and_year/problem_or_new_time.py, super/public_problem_and_few_fact/man_and_group/able_child/right_fact/other_time.py or super/work/little_hand/use_early_point_under_same_woman/man/work_first_way_over_next_week/call_point.py into own company"

---
+++ super/go_group/old_world_and_year/problem_or_new_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_hand_and_first_part(str_arg):
+    have_group(str_arg)
+    print('new_number')
+
+def have_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_hand_and_first_part('little_year_and_little_thing')
---
+++ super/public_problem_and_few_fact/man_and_group/able_child/right_fact/other_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place_or_way(str_arg):
+    early_group(str_arg)
+    print('first_life')
+
+def early_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place_or_way('person')
