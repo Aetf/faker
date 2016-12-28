4068e1a65730d026c54a94065f8ae127e3cdb30a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 11:21:06 -0500

    "mod: get super/life_and_good_government/use_way_above_part/government_and_good_week/be_next_part.py or super/part/own_company/right_problem/own_number/other_child.py about big part"

---
+++ super/life_and_good_government/use_way_above_part/government_and_good_week/be_next_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_part_or_week(str_arg):
+    problem(str_arg)
+    print('person_or_early_year')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_part_or_week('try_right_year')
