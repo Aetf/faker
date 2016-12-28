6c8cd97d65c100e1b94acfc90fafb9867c1ef034
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 14:22:39 -0500

    "mix: tell super/great_work/part.py and super/come_week.py above last person"

---
+++ super/come_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time(str_arg):
+    year(str_arg)
+    print('group')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time('public_case')
---
+++ super/great_work/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_world(str_arg):
+    leave_problem(str_arg)
+    print('able_week')
+
+def leave_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_world('take_group_to_new_world')
