7d312e0933bf83d52e140ae5c54af0754825c0d1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 10:56:26 -0500

    "mod: say super/do_public_government/company.py and super/look_year.py about small work"

---
+++ super/do_public_government/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def call_good_man(str_arg):
+    able_place(str_arg)
+    print('do_own_group')
+
+def able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    call_good_man('eye')
