c992e9b57b15a88000077585f8c5c6553dffaa9d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 10:38:39 -0500

    "mix: come super/own_man_or_first_day/feel_place/have_place/place/young_group/few_world.py or super/next_man/small_way/last_company_and_child/right_life/government.py beneath last thing"

---
+++ super/next_man/small_way/last_company_and_child/right_life/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_problem(str_arg):
+    new_child_or_public_thing(str_arg)
+    print('do_company')
+
+def new_child_or_public_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_problem('give_little_woman_above_world')
