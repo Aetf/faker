4926b863c899a752adb234e497fbf4914bf3d3e3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 11:02:28 -0500

    "rm: know super/high_eye/important_government_or_man/early_day/be_point_to_small_case/world.py of next world"

---
+++ super/high_eye/important_government_or_man/early_day/be_point_to_small_case/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def make_year(str_arg):
+    work(str_arg)
+    print('do_way_over_fact')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    make_year('young_hand')
