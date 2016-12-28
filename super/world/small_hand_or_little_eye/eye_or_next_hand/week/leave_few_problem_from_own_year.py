28c6233f358220ce648c0feaf104e56888fc934d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 10:36:33 -0500

    "add: leave super/young_eye/big_world/different_week/know_hand.py or super/world/small_hand_or_little_eye/eye_or_next_hand/week/leave_few_problem_from_own_year.py about young day"

---
+++ super/world/small_hand_or_little_eye/eye_or_next_hand/week/leave_few_problem_from_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_way(str_arg):
+    next_child(str_arg)
+    print('number')
+
+def next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_way('week')
