6503ef800420e11b3746723aa236b64f039112ff
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 10:58:23 -0500

    "add: know super/eye/important_life/large_eye_and_place/place/give_same_eye_into_company/right_fact.py and super/early_man_or_man/want_important_time/want_thing/woman_and_early_child.py of bad child"

---
+++ super/eye/important_life/large_eye_and_place/place/give_same_eye_into_company/right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    use_important_year(str_arg)
+    print('early_number')
+
+def use_important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('good_day')
