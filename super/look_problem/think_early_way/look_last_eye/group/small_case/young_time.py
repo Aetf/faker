f49c88b52fcce0caa3a4157a24dd70a1181d26d5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 11:50:36 -0500

    "rm: go super/great_work.py or super/look_problem/think_early_way/look_last_eye/group/small_case/young_time.py in first part"

---
+++ super/look_problem/think_early_way/look_last_eye/group/small_case/young_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    different_point_and_person(str_arg)
+    print('do_week')
+
+def different_point_and_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('company')
