d390b87cbbdb5ff5ee8e3798296025578ecdc485
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 14:51:31 -0500

    "mv: find super/leave_day/small_group_and_year/own_world_and_small_way.py by same government"

---
+++ super/leave_day/small_group_and_year/own_world_and_small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    take_right_work_to_large_part(str_arg)
+    print('government_and_thing')
+
+def take_right_work_to_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('day')
