d8843e6c32633c36459d1440ab6705ba404f100d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 11:32:54 -0500

    "add: come super/get_long_work_of_eye.py, super/work_or_world.py and super/young_problem/point/little_thing.py from high man"

---
+++ super/get_long_work_of_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_hand(str_arg):
+    bad_company(str_arg)
+    print('next_work_and_week')
+
+def bad_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_hand('place_and_day')
---
+++ super/work_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    other_person(str_arg)
+    print('tell_little_fact_above_small_fact')
+
+def other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('new_world_and_company')
