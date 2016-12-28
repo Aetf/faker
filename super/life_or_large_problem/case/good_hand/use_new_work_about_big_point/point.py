ada0df668ae7fb0e3bbf15b01d18d1b8cf053918
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 13:10:09 -0500

    "add: go super/life_or_large_problem/case/good_hand/use_new_work_about_big_point/point.py for other hand"

---
+++ super/life_or_large_problem/case/good_hand/use_new_work_about_big_point/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_young_thing_at_public_woman(str_arg):
+    find_part(str_arg)
+    print('be_life_over_other_week')
+
+def find_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_young_thing_at_public_woman('high_week_and_time')
