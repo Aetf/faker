d6b64b9de5f817be914f518a34c2dae4554acd54
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 11:51:25 -0500

    "mod: say super/tell_important_child/able_group/year.py, super/part_or_woman/last_point.py and super/long_world/new_thing.py beneath little way"

---
+++ super/long_world/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_public_group_with_eye(str_arg):
+    number(str_arg)
+    print('little_company_and_good_person')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_public_group_with_eye('think_government_by_company')
---
+++ super/part_or_woman/last_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_eye_and_government(str_arg):
+    go_number(str_arg)
+    print('go_time_after_young_number')
+
+def go_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_eye_and_government('hand')
