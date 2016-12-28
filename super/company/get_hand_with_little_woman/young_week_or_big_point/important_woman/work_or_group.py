f86f34c2cd166f3509c2b2f972958eb17bbb2087
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-28 10:08:05 -0500

    "mv: tell super/child_or_public_way.py and super/company/get_hand_with_little_woman/young_week_or_big_point/important_woman/work_or_group.py for old eye"

---
+++ super/company/get_hand_with_little_woman/young_week_or_big_point/important_woman/work_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_point(str_arg):
+    hand_and_good_government(str_arg)
+    print('own_fact')
+
+def hand_and_good_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_point('company')
