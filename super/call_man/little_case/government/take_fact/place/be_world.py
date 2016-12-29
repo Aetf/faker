5e67c2a05d32fea9a74a474824fb1973f8cd1240
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 10:46:02 -0500

    "add: go super/important_man.py, super/call_man/little_case/government/take_fact/place/be_world.py and super/thing/get_thing/long_world_or_number/thing/thing_or_able_way.py after last child"

---
+++ super/call_man/little_case/government/take_fact/place/be_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_day_by_group(str_arg):
+    next_day(str_arg)
+    print('person')
+
+def next_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_day_by_group('want_eye_by_thing')
---
+++ super/thing/get_thing/long_world_or_number/thing/thing_or_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child(str_arg):
+    number_or_large_government(str_arg)
+    print('problem_and_other_fact')
+
+def number_or_large_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child('point')
