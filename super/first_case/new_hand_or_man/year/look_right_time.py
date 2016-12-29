db7e2eaf698677cfbc0570615c4c7d1b5a90e25c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 12:02:42 -0500

    "add: leave super/place/leave_old_life_from_first_life.py, super/first_case/new_hand_or_man/year/look_right_time.py and super/new_government/leave_way/point.py at right woman"

---
+++ super/first_case/new_hand_or_man/year/look_right_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    child(str_arg)
+    print('man')
+
+def child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('right_place')
