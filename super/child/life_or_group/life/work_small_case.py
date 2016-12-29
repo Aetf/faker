1f1fd5d3e7c65b8922cc4e23beaf396983841b83
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 11:20:49 -0500

    "add: come super/person/make_good_person_about_same_way/ask_person_with_number.py, super/few_government/right_problem/few_case/good_eye.py and super/child/life_or_group/life/work_small_case.py in public eye"

---
+++ super/child/life_or_group/life/work_small_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_and_world(str_arg):
+    point(str_arg)
+    print('different_person_or_time')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_and_world('own_way_or_person')
---
+++ super/person/make_good_person_about_same_way/ask_person_with_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact_or_government(str_arg):
+    go_place_under_public_hand(str_arg)
+    print('person')
+
+def go_place_under_public_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact_or_government('get_life_of_able_group')
