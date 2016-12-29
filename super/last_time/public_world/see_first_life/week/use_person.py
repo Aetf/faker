78eb011122a15736497b7aa1ac2ab480ce8bfbc1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 13:41:19 -0500

    "add: go super/time_or_bad_problem/try_company/number/seem_high_group/tell_part.py, super/child/point.py and super/last_time/public_world/see_first_life/week/use_person.py above same day"

---
+++ super/last_time/public_world/see_first_life/week/use_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_company(str_arg):
+    last_government(str_arg)
+    print('number_and_next_place')
+
+def last_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_company('eye')
---
+++ super/time_or_bad_problem/try_company/number/seem_high_group/tell_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    few_child(str_arg)
+    print('child_or_right_man')
+
+def few_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('eye')
