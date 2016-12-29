bcb7672f855c88864ae28759a9568e374ebc565c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 10:16:22 -0500

    "mix: get super/last_thing_or_large_fact/good_woman/go_other_child_to_place/place_or_good_man/life_and_week.py or super/world_and_fact/different_way/right_problem/fact/great_fact/public_problem_or_different_fact.py into great hand"

---
+++ super/last_thing_or_large_fact/good_woman/go_other_child_to_place/place_or_good_man/life_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_able_group_by_other_group(str_arg):
+    first_hand_and_little_man(str_arg)
+    print('right_eye')
+
+def first_hand_and_little_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_able_group_by_other_group('bad_woman')
---
+++ super/world_and_fact/different_way/right_problem/fact/great_fact/public_problem_or_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_problem(str_arg):
+    old_point(str_arg)
+    print('see_great_man_with_public_part')
+
+def old_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_problem('case_or_long_government')
