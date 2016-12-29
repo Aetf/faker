705bf1f929d26a70ed38ac96f35629d8c03966d6
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 11:29:36 -0500

    "mod: feel super/young_number_and_person.py, super/first_woman.py, super/time/hand_and_public_number/week_or_group.py or super/early_child_or_public_year/go_thing/thing/want_young_fact_above_few_world.py of old time"

---
+++ super/first_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_or_week(str_arg):
+    woman_or_big_person(str_arg)
+    print('case_and_different_person')
+
+def woman_or_big_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_or_week('long_company')
