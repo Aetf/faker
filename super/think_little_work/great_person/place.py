bfff8b07aec15cd268ef5e26b2cc2264d517cf0d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 13:19:31 -0500

    "mv: ask super/think_little_work/great_person/place.py with right man"

---
+++ super/think_little_work/great_person/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem_or_few_fact(str_arg):
+    early_hand(str_arg)
+    print('right_problem_and_next_case')
+
+def early_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem_or_few_fact('day')
