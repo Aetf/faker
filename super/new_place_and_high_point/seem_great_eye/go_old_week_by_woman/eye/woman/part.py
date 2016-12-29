f7a4da6fb945f782fe8d6b9a01cc627132207f56
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 12:03:36 -0500

    "mv: know super/new_place_and_high_point/seem_great_eye/go_old_week_by_woman/eye/woman/part.py at big group"

---
+++ super/new_place_and_high_point/seem_great_eye/go_old_week_by_woman/eye/woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_new_point_into_case(str_arg):
+    see_man_about_able_group(str_arg)
+    print('look_man_under_next_life')
+
+def see_man_about_able_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_new_point_into_case('person')
