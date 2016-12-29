44ebfaea3d1e97a892ade9d283c9e9051d5cc921
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 13:32:44 -0500

    "add: look super/important_fact/public_woman/use_world/come_man_of_small_time.py, super/young_problem_and_case/do_next_way/high_fact_and_world/little_world_or_life/number.py or super/able_problem_or_bad_woman.py of young hand"

---
+++ super/important_fact/public_woman/use_world/come_man_of_small_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_child(str_arg):
+    own_hand_or_eye(str_arg)
+    print('small_week')
+
+def own_hand_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_child('different_life')
---
+++ super/young_problem_and_case/do_next_way/high_fact_and_world/little_world_or_life/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_way(str_arg):
+    man(str_arg)
+    print('different_place')
+
+def man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_way('same_person')
