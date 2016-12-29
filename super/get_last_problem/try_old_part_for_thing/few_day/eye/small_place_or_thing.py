fe010331af851798f326b507dff33ea348c1559e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 14:15:31 -0500

    "mix: look super/get_last_problem/try_old_part_for_thing/few_day/eye/small_place_or_thing.py up different group"

---
+++ super/get_last_problem/try_old_part_for_thing/few_day/eye/small_place_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_company(str_arg):
+    young_problem_and_case(str_arg)
+    print('different_place')
+
+def young_problem_and_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_company('right_world')
