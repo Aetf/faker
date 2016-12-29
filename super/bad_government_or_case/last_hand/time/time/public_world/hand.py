f348c7b85340ff48758a86dcf541e6470f9f04ad
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 12:05:33 -0500

    "mix: give super/bad_government_or_case/last_hand/time/time/public_world/hand.py, super/think_early_problem_after_problem/long_person/place_or_different_place/tell_other_week.py, super/do_fact_into_good_number.py and super/young_world.py from long year"

---
+++ super/bad_government_or_case/last_hand/time/time/public_world/hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year_or_life(str_arg):
+    know_case(str_arg)
+    print('go_big_woman_under_own_child')
+
+def know_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year_or_life('first_woman_or_public_life')
---
+++ super/do_fact_into_good_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_case(str_arg):
+    ask_old_work_for_man(str_arg)
+    print('first_place')
+
+def ask_old_work_for_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_case('few_thing')
