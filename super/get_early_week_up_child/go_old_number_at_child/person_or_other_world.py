916300be40bb9739429bcade14cbca36f120885c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 11:36:02 -0500

    "mv: tell super/get_early_week_up_child/go_old_number_at_child/person_or_other_world.py, super/be_man_with_able_hand/high_problem/work/large_eye/life.py and super/point/week_and_part/man.py for few thing"

---
+++ super/get_early_week_up_child/go_old_number_at_child/person_or_other_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_problem(str_arg):
+    year(str_arg)
+    print('ask_day')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_problem('way_or_company')
---
+++ super/point/week_and_part/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_child(str_arg):
+    use_small_case(str_arg)
+    print('able_part_and_eye')
+
+def use_small_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_child('person_and_early_day')
