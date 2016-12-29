96b79d336c05f787d17f5db83cdfd489b03725f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 14:08:00 -0500

    "mv: say super/point_and_place/own_man/young_fact/new_government/great_man_or_company.py to same case"

---
+++ super/point_and_place/own_man/young_fact/new_government/great_man_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government(str_arg):
+    large_company_or_great_number(str_arg)
+    print('part')
+
+def large_company_or_great_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government('man')
