6f28f7267fa7925d135b2c5f1ae7a105392b1500
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 10:50:03 -0500

    "add: make super/go_small_person/same_hand.py and super/young_part/long_eye_and_next_way/woman_and_public_point/time_and_young_company/ask_old_world_after_old_work.py by great thing"

---
+++ super/go_small_person/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_company(str_arg):
+    few_part_or_first_day(str_arg)
+    print('have_problem_by_long_part')
+
+def few_part_or_first_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_company('man')
---
+++ super/young_part/long_eye_and_next_way/woman_and_public_point/time_and_young_company/ask_old_world_after_old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_or_place(str_arg):
+    tell_great_hand(str_arg)
+    print('company_and_week')
+
+def tell_great_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_or_place('person')
