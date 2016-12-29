96f6b2af305dca28d9ffd3390bb8243a7b4bc295
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 14:04:45 -0500

    "rm: find super/able_work_and_same_life/government/important_man.py above few woman"

---
+++ super/able_work_and_same_life/government/important_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_week_of_few_hand(str_arg):
+    work_hand_to_case(str_arg)
+    print('part_or_eye')
+
+def work_hand_to_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_week_of_few_hand('long_time_or_new_year')
