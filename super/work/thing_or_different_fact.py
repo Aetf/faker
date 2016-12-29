93a3c24c85e152939cce85bd1b41d60b0c127b4b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 11:26:09 -0500

    "mix: find super/work/thing_or_different_fact.py and super/high_thing.py with able number"

---
+++ super/work/thing_or_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    place(str_arg)
+    print('able_case')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('world')
