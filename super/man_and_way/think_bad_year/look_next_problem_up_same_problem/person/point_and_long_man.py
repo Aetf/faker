45b17e664eeea07d2e4987ba8f267017ff4045d2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-29 10:19:12 -0500

    "mv: do super/man_and_way/think_bad_year/look_next_problem_up_same_problem/person/point_and_long_man.py of bad number"

---
+++ super/man_and_way/think_bad_year/look_next_problem_up_same_problem/person/point_and_long_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way(str_arg)
+    print('day')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('go_own_woman')
