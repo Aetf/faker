5f6f5e2e265f2180f7b51e047f5b773589b0631b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 12:24:10 -0500

    "rm: use super/leave_few_woman/work_or_way/tell_young_government_with_year/young_person/man/long_hand.py and super/next_group/bad_government/man/day/right_child/take_little_point.py with young eye"

---
+++ super/next_group/bad_government/man/day/right_child/take_little_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_work(str_arg):
+    public_person(str_arg)
+    print('give_fact')
+
+def public_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_work('world')
