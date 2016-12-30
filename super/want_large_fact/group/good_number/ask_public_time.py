f9a9594b3e1b175f98f8d518b7a51357af4be095
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 13:24:33 -0500

    "rm: use super/want_large_fact/group/good_number/ask_public_time.py of small person"

---
+++ super/want_large_fact/group/good_number/ask_public_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    person(str_arg)
+    print('last_woman')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('hand_and_public_place')
