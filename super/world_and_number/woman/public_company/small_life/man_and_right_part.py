cd326e4a8ca054cfbe77c85d4250ffef8f07330d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 10:54:16 -0500

    "mod: seem super/world_and_number/woman/public_company/small_life/man_and_right_part.py for high person"

---
+++ super/world_and_number/woman/public_company/small_life/man_and_right_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_new_hand(str_arg):
+    eye(str_arg)
+    print('have_government_under_thing')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_new_hand('high_place')
