f635c20ab7f4f885ecf50a60c3869cba3b7fd106
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 14:29:32 -0500

    "mix: leave super/part_and_other_part/good_life/next_child.py, super/part.py or super/say_man/big_point_or_group/be_point_under_early_man/look_first_part_by_big_group/large_number/life_and_right_child.py about high point"

---
+++ super/say_man/big_point_or_group/be_point_under_early_man/look_first_part_by_big_group/large_number/life_and_right_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    great_eye_or_able_part(str_arg)
+    print('way')
+
+def great_eye_or_able_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('big_hand')
