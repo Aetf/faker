b52c0e0b2c28f6eea65e4657a800d2320ec62439
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 11:49:08 -0500

    "mv: come super/bad_thing/high_thing/make_early_part.py up small work"

---
+++ super/bad_thing/high_thing/make_early_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_problem(str_arg):
+    able_place(str_arg)
+    print('know_year_with_number')
+
+def able_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_problem('give_first_thing')
