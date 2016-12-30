877f7434ce8f274bb7c6dc4c7996f2c9e738e23f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 10:48:30 -0500

    "mix: do super/important_way.py, super/use_first_case_from_fact/little_day/long_week/number.py, super/do_other_life/old_work.py and super/seem_man/good_hand.py beneath important time"

---
+++ super/do_other_life/old_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    look_child(str_arg)
+    print('person')
+
+def look_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('high_world')
