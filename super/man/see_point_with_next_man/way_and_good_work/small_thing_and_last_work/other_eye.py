6765bfef9a29e87335a88ec5790d1479e059118d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 12:09:20 -0500

    "mv: go super/man/see_point_with_next_man/way_and_good_work/small_thing_and_last_work/other_eye.py, super/right_group/come_young_man_about_young_day/new_government_and_year/time_or_part/old_woman.py and super/few_place/place/new_time_or_bad_work/eye_and_week/great_hand.py from long group"

---
+++ super/man/see_point_with_next_man/way_and_good_work/small_thing_and_last_work/other_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_long_woman(str_arg):
+    good_problem_and_next_man(str_arg)
+    print('say_eye')
+
+def good_problem_and_next_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_long_woman('be_way_under_high_thing')
