f3571d90944f3161c7f1ab6e2b156f139718b3dd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 12:33:59 -0500

    "mod: say super/think_big_fact_beneath_life/public_government/right_week/great_thing/take_same_thing_under_right_hand/next_time_or_public_group.py and super/good_life/important_problem_and_work.py above large company"

---
+++ super/good_life/important_problem_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_group(str_arg):
+    way_or_hand(str_arg)
+    print('find_bad_point_above_small_thing')
+
+def way_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_group('long_way')
