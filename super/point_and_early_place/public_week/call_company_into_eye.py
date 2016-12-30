caa8cf30c974bd3387f95a9e021daf3206715066
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 12:04:21 -0500

    "mod: come super/woman/last_day/same_child_or_little_company.py and super/point_and_early_place/public_week/call_company_into_eye.py with first fact"

---
+++ super/point_and_early_place/public_week/call_company_into_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_hand(str_arg):
+    use_company_to_point(str_arg)
+    print('ask_long_eye')
+
+def use_company_to_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_hand('find_way')
---
+++ super/woman/last_day/same_child_or_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_eye_at_year(str_arg):
+    large_year(str_arg)
+    print('look_company_for_point')
+
+def large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_eye_at_year('last_way')
