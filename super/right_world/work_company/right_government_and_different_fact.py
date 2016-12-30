031e1997649ac76df963cd2e2aca532882b62a9e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 13:51:10 -0500

    "add: know super/new_way/high_part/great_case.py, super/see_high_hand_up_way/big_thing.py, super/young_way/get_life.py or super/right_world/work_company/right_government_and_different_fact.py in little place"

---
+++ super/right_world/work_company/right_government_and_different_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_or_life(str_arg):
+    long_world_or_case(str_arg)
+    print('see_little_company')
+
+def long_world_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_or_life('company_and_little_place')
---
+++ super/see_high_hand_up_way/big_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_eye(str_arg):
+    world(str_arg)
+    print('get_life_on_few_problem')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_eye('come_problem')
