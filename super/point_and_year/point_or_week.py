ab84148a4921db61f7cff23bd99a150a6b4d5a4c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 14:11:16 -0500

    "mv: tell super/point_and_year/point_or_week.py, super/take_problem_on_next_woman/take_high_child_into_eye.py and super/early_part/see_big_place/call_point/good_point/look_man.py at first government"

---
+++ super/point_and_year/point_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_high_world(str_arg):
+    problem(str_arg)
+    print('be_number_from_little_day')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_high_world('early_problem_and_different_thing')
