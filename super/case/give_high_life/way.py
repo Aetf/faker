09a48fbdcfbc7a2ab69570f6929118f582fc4fb1
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 11:08:30 -0500

    "rm: tell super/part_or_work/eye/important_group/right_year/new_woman_or_world/great_work_or_group.py, super/case/give_high_life/way.py or super/child_or_year/tell_right_thing_beneath_able_case/able_part/large_place/tell_next_year_over_important_problem/think_point_up_great_thing.py after high life"

---
+++ super/case/give_high_life/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_group(str_arg):
+    time(str_arg)
+    print('early_way_and_bad_man')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_group('right_part')
---
+++ super/child_or_year/tell_right_thing_beneath_able_case/able_part/large_place/tell_next_year_over_important_problem/think_point_up_great_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_world_or_first_hand(str_arg):
+    life(str_arg)
+    print('old_case')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_world_or_first_hand('important_thing')
