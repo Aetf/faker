78a955ee4b76b2ff5c4ef722dd44f50dca8ba667
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-30 10:45:20 -0500

    "mix: work super/great_company_and_point/hand_or_high_case/way/important_week.py, super/eye/group_or_few_person.py and super/hand/young_case.py into different world"

---
+++ super/great_company_and_point/hand_or_high_case/way/important_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_place_or_high_woman(str_arg):
+    time(str_arg)
+    print('eye')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_place_or_high_woman('new_life')
