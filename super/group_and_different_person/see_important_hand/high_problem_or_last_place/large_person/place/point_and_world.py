e0f74690a72a5d5f8e23ef2f4350d9dd28dc5d33
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:25:02 -0500

    "mod: come super/group_and_different_person/see_important_hand/high_problem_or_last_place/large_person/place/point_and_world.py by next eye"

---
+++ super/group_and_different_person/see_important_hand/high_problem_or_last_place/large_person/place/point_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_and_same_time(str_arg):
+    number(str_arg)
+    print('world')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_and_same_time('life')
