bd278729c367a28aeb0c9674f5ed3bc0be5cc2d3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 10:28:49 -0500

    "mv: get super/bad_problem_and_government.py and super/great_day/company/government.py for bad eye"

---
+++ super/bad_problem_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_man(str_arg):
+    see_important_point(str_arg)
+    print('day')
+
+def see_important_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_man('old_year')
---
+++ super/great_day/company/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    good_life_or_company(str_arg)
+    print('own_place_and_different_week')
+
+def good_life_or_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('same_life_and_able_number')
