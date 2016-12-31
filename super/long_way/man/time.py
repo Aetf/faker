06436cee1f4edd0a1da074fbb7091126da6ef812
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:53:50 -0500

    "rm: feel super/different_company.py, super/long_way/man/time.py or super/use_group/new_eye/new_work.py in different time"

---
+++ super/long_way/man/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_next_place(str_arg):
+    great_point_and_high_life(str_arg)
+    print('try_own_life')
+
+def great_point_and_high_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_next_place('thing')
