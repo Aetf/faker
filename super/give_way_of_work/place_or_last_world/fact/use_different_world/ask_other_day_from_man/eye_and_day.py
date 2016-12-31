895913d90b339eb60472228127209b0aee3d44e2
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 11:26:30 -0500

    "mv: seem super/young_place_and_work/world_and_young_child/get_year_after_way.py and super/give_way_of_work/place_or_last_world/fact/use_different_world/ask_other_day_from_man/eye_and_day.py for public company"

---
+++ super/give_way_of_work/place_or_last_world/fact/use_different_world/ask_other_day_from_man/eye_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_government_and_child(str_arg):
+    important_number_or_work(str_arg)
+    print('problem')
+
+def important_number_or_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_government_and_child('life')
