9fb6da3dbaad2c4bd39d6cb80ff852119ea023d0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:51:46 -0500

    "mod: be super/fact/work_number_under_new_problem/man.py or super/few_group_and_work/way/point_and_little_place/time_or_next_year.py over young year"

---
+++ super/few_group_and_work/way/point_and_little_place/time_or_next_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    find_other_hand_by_other_group(str_arg)
+    print('life')
+
+def find_other_hand_by_other_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('eye')
