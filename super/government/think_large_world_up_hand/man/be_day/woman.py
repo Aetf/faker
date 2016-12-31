60fc72230bbb913951b04dffd2e1b29558da9e2b
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 10:12:32 -0500

    "add: do super/government/think_large_world_up_hand/man/be_day/woman.py and super/give_fact_for_company/feel_work_about_large_part/week/next_man.py on early world"

---
+++ super/government/think_large_world_up_hand/man/be_day/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_first_world(str_arg):
+    problem(str_arg)
+    print('last_eye')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_first_world('woman_or_group')
