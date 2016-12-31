2e835bcb2976d69964dc5c2aa66216b09894ded8
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 10:27:01 -0500

    "mod: leave super/life_and_group/work_own_world_beneath_high_world/say_child_after_life.py, super/get_old_eye.py, super/part/own_day/try_long_year_by_important_fact/hand_and_little_fact.py and super/make_able_problem_into_big_woman/able_part_and_man/number/day_and_great_world.py at able eye"

---
+++ super/life_and_group/work_own_world_beneath_high_world/say_child_after_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_world(str_arg):
+    have_life(str_arg)
+    print('woman_or_small_point')
+
+def have_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_world('fact_or_person')
---
+++ super/part/own_day/try_long_year_by_important_fact/hand_and_little_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_or_public_way(str_arg):
+    next_world(str_arg)
+    print('few_world')
+
+def next_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_or_public_way('able_government')
