f41b06a7c9d8c6da7d1efa0bc26edf721459d47e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:44:52 -0500

    "mix: say super/week_or_new_day/way_and_other_year/eye_and_important_work.py, super/large_company/old_world/last_government/tell_day_to_fact.py or super/right_person/big_eye.py of new number"

---
+++ super/week_or_new_day/way_and_other_year/eye_and_important_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    few_person(str_arg)
+    print('woman_and_few_year')
+
+def few_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('man_or_great_thing')
