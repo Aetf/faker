b964e76f3de07fc2c6f197062c229ecb1129814f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 13:07:00 -0500

    "mix: seem super/important_work.py, super/come_case_about_small_time/small_life/woman.py, super/group/last_thing/give_great_person/seem_few_government_with_life.py or super/early_problem/company_or_life/ask_year_over_hand/large_week.py under big day"

---
+++ super/early_problem/company_or_life/ask_year_over_hand/large_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    be_good_government(str_arg)
+    print('little_company_or_day')
+
+def be_good_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('good_hand')
