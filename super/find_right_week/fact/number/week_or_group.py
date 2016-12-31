442179aacec22d5e1e1902b76e6b4d8436b0b4f3
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 12:10:37 -0500

    "mix: be super/own_world/point.py and super/find_right_week/fact/number/week_or_group.py into long problem"

---
+++ super/find_right_week/fact/number/week_or_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    company_and_company(str_arg)
+    print('case')
+
+def company_and_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('important_company')
---
+++ super/own_world/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_woman(str_arg):
+    old_man(str_arg)
+    print('young_eye')
+
+def old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_woman('be_next_work_beneath_long_eye')
