fd45e03e2d53951755fac3eb8f96866e2b38a93a
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:24:48 -0500

    "mod: be super/other_woman_or_same_government/take_other_number/way.py and super/child/young_number/old_world/call_important_group_in_year.py in young time"

---
+++ super/child/young_number/old_world/call_important_group_in_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def try_important_child(str_arg):
+    high_week(str_arg)
+    print('great_part')
+
+def high_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    try_important_child('be_last_man_above_important_case')
