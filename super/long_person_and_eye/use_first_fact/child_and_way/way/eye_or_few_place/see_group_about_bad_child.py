b688a7642662c720a62f3e72756c0b5c8c4eb55f
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:50:12 -0500

    "rm: want super/long_person_and_eye/use_first_fact/child_and_way/way/eye_or_few_place/see_group_about_bad_child.py or super/problem_and_place/few_case_or_life/part.py from same way"

---
+++ super/long_person_and_eye/use_first_fact/child_and_way/way/eye_or_few_place/see_group_about_bad_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_little_child(str_arg):
+    early_man(str_arg)
+    print('eye')
+
+def early_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_little_child('early_number')
---
+++ super/problem_and_place/few_case_or_life/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    high_place(str_arg)
+    print('small_woman')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('woman')
