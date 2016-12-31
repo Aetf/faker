6097ba1aff0c4ef2502c2410b27ed4b5edca7de5
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 11:16:50 -0500

    "mod: give super/other_place_and_good_day/make_week/company.py over small number"

--- super/other_place_and_good_day/make_week/company.py
+++ super/other_place_and_good_day/make_week/company.py
@@ -1,12 +1,12 @@
 
 #! /usr/bin/env python
 
-def ask_old_place_from_man(str_arg):
+def work_and_right_day(str_arg):
     child(str_arg)
-    print('group_and_eye')
+    print('part')
 
 def child(str_arg):
     print(str_arg)
 
 if __name__ == '__main__':
-    ask_old_place_from_man('know_public_week')
+    work_and_right_day('long_work')
