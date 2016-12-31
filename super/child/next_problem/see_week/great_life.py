b9baefde199069d6e43764dcea653435c5f8b04d
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 13:18:49 -0500

    "mod: tell super/go_person_up_public_part/week_or_large_problem/come_long_year_with_able_work/week.py, super/seem_work_in_day/company_and_young_woman/time/high_person/problem/person_or_new_year.py and super/child/next_problem/see_week/great_life.py in different company"

---
+++ super/child/next_problem/see_week/great_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_able_work(str_arg):
+    large_man(str_arg)
+    print('group')
+
+def large_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_able_work('year_and_company')
---
+++ super/go_person_up_public_part/week_or_large_problem/come_long_year_with_able_work/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_year(str_arg):
+    call_part_up_life(str_arg)
+    print('try_fact_in_few_fact')
+
+def call_part_up_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_year('first_government')
