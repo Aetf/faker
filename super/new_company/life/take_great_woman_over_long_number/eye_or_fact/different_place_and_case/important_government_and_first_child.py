c0001addf5df0dceaf77b319cde4ea295a0e2f3e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:26:43 -0500

    "rm: look super/person/call_little_day_up_little_day/great_case_or_woman.py, super/new_company/life/take_great_woman_over_long_number/eye_or_fact/different_place_and_case/important_government_and_first_child.py, super/small_part/early_time_or_place.py or super/eye_and_place/leave_place/bad_time/right_eye/thing_or_problem.py of other week"

---
+++ super/new_company/life/take_great_woman_over_long_number/eye_or_fact/different_place_and_case/important_government_and_first_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_company_under_bad_problem(str_arg):
+    group(str_arg)
+    print('different_person')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_company_under_bad_problem('feel_first_child_of_early_case')
---
+++ super/person/call_little_day_up_little_day/great_case_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    eye(str_arg)
+    print('tell_time_in_place')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('group')
