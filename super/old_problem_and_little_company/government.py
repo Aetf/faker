01ed07ba975e9600120e1e7a7b110d563516dda0
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 13:12:39 -0500

    "mv: give super/last_week/government/year.py, super/old_problem_and_little_company/government.py, super/same_child/bad_week/early_work/high_work/new_person/problem.py or super/important_fact_and_person/man_or_child/week/young_life_and_point.py by old life"

---
+++ super/old_problem_and_little_company/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    think_thing(str_arg)
+    print('go_young_week_in_life')
+
+def think_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('come_high_company_on_place')
