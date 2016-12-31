b09ea08f1da4b694be479b326b615c56777e42fd
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 13:55:16 -0500

    "rm: want super/man/say_week_for_small_case/bad_work.py or super/know_person_about_eye/make_first_time_on_last_week/big_time/time_and_high_case/large_government/small_point.py about same group"

---
+++ super/know_person_about_eye/make_first_time_on_last_week/big_time/time_and_high_case/large_government/small_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_part(str_arg):
+    government_and_woman(str_arg)
+    print('child_and_place')
+
+def government_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_part('life_and_good_week')
---
+++ super/man/say_week_for_small_case/bad_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_year(str_arg):
+    seem_other_time_about_long_case(str_arg)
+    print('work_problem_beneath_bad_day')
+
+def seem_other_time_about_long_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_year('give_high_child')
