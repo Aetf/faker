86b86d23bbf9af449fa54fbaf0f371abc6859a14
Aetf <aetf@unlimitedcodeworks.xyz>
2016-12-31 14:11:11 -0500

    "add: go super/time_or_other_point/way/time_and_few_way.py in next year"

---
+++ super/time_or_other_point/way/time_and_few_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_part(str_arg):
+    place(str_arg)
+    print('want_part')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_part('part')
