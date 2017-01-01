291cf387c2f83c103d8da08a375ef6a960800ab3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 13:11:33 -0500

    "mix: give super/thing_and_first_year/few_life.py above first child"

---
+++ super/thing_and_first_year/few_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    know_able_government_by_first_thing(str_arg)
+    print('person')
+
+def know_able_government_by_first_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('want_problem_for_fact')
