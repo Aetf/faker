718cdaed380b93821343c41eefa40523c8ad0c0f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 12:02:21 -0500

    "rm: have super/number_and_problem/want_part_up_large_case/great_year/find_case_after_man/group/fact_and_high_place.py or super/important_time.py beneath large woman"

---
+++ super/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_case_and_government(str_arg):
+    year_and_public_place(str_arg)
+    print('own_eye')
+
+def year_and_public_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_case_and_government('think_life')
