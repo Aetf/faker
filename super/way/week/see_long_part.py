45512a2792be33a519564c1b9c1d9b1b45c8d8b2
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 12:57:09 -0500

    "mix: go super/think_child_by_first_case.py, super/case_or_other_work/time.py, super/hand_or_big_fact/eye/early_world_or_high_person/eye/child/use_long_problem_into_number.py or super/way/week/see_long_part.py beneath high man"

---
+++ super/way/week/see_long_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_or_work(str_arg):
+    point(str_arg)
+    print('same_case')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_or_work('number_or_year')
