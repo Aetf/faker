894c05a25148a1cc44bdbe255e096867d3cceb98
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 11:17:23 -0500

    "add: see super/way/use_woman/fact_or_number/right_government/come_large_problem/place.py, super/way/old_year.py, super/same_person_or_way.py or super/use_little_government_for_long_company.py by different child"

---
+++ super/use_little_government_for_long_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company(str_arg):
+    try_time_up_right_day(str_arg)
+    print('thing_or_same_man')
+
+def try_time_up_right_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company('leave_other_work_to_place')
---
+++ super/way/old_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    call_eye(str_arg)
+    print('eye_or_high_day')
+
+def call_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('man')
