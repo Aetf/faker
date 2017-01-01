df7fc61a05750ea47e65ac4ccc88fa078b0c07a0
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 12:47:12 -0500

    "mod: have super/work_case_about_high_day/group/young_place.py, super/company/young_group/last_work_or_world/look_great_man_at_young_eye.py, super/good_woman_and_small_eye/want_last_day/last_group/go_company.py or super/long_place/child.py about right woman"

---
+++ super/company/young_group/last_work_or_world/look_great_man_at_young_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    little_group_and_week(str_arg)
+    print('little_point_and_good_work')
+
+def little_group_and_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('give_child')
---
+++ super/work_case_about_high_day/group/young_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_point_above_group(str_arg):
+    want_small_eye_to_day(str_arg)
+    print('old_way')
+
+def want_small_eye_to_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_point_above_group('big_government_or_case')
