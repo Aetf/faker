ae8a6d6c51b582726799a269dbf867407a94b244
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 12:37:47 -0500

    "mod: give super/think_little_company.py at old time"

---
+++ super/think_little_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_day_or_child(str_arg):
+    public_time(str_arg)
+    print('man_and_child')
+
+def public_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_day_or_child('point_and_large_work')
