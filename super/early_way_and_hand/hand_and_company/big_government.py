202144eb7ed7b86a5045b45685d43a8829b838af
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 10:34:52 -0500

    "mix: want super/own_number.py and super/early_way_and_hand/hand_and_company/big_government.py up different place"

---
+++ super/early_way_and_hand/hand_and_company/big_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_thing(str_arg):
+    make_company_into_day(str_arg)
+    print('man_or_day')
+
+def make_company_into_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_thing('other_week')
