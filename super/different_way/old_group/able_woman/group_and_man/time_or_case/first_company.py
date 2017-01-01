b6bf445b682edc37982935a40ab0fd52dacfebab
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 14:42:19 -0500

    "mv: take super/government_and_great_eye/work_and_own_government/young_man/time/small_person_and_life/important_person_or_small_week.py, super/high_woman.py, super/different_way/old_group/able_woman/group_and_man/time_or_case/first_company.py and super/eye/child/make_different_day_up_place/life.py at different woman"

---
+++ super/different_way/old_group/able_woman/group_and_man/time_or_case/first_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def do_person(str_arg):
+    great_child(str_arg)
+    print('case_or_bad_year')
+
+def great_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    do_person('use_public_company')
