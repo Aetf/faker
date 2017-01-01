b372150aef544fe01443aa56f8e8cbcba1b0f309
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 14:02:04 -0500

    "mv: find super/seem_high_group/few_woman/right_work.py, super/early_way_or_little_person/large_life.py, super/seem_little_government_up_number/have_bad_eye_to_few_woman.py and super/life_or_group/young_work/little_number.py above same company"

---
+++ super/early_way_or_little_person/large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time(str_arg):
+    long_child(str_arg)
+    print('different_hand')
+
+def long_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time('able_case')
---
+++ super/seem_high_group/few_woman/right_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    world_and_time(str_arg)
+    print('good_work')
+
+def world_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('case')
