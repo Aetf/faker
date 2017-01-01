55a007310f83bbbb2622fee06a152a60153e368a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 14:19:13 -0500

    "rm: be super/early_man_or_person/small_case_and_new_problem/work_world/hand_or_new_part/same_day.py or super/important_eye/know_part_over_child/child/leave_last_hand_in_company/point.py with long group"

---
+++ super/important_eye/know_part_over_child/child/leave_last_hand_in_company/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place_and_few_work(str_arg):
+    person(str_arg)
+    print('make_hand_into_time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place_and_few_work('find_thing')
