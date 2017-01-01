1cb3c6161ee4868173b3d1f12832540a1eccba51
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 13:27:27 -0500

    "mod: leave super/way/make_great_work_under_man/high_time_and_young_world/place/early_group.py and super/early_group/work_person.py of different woman"

---
+++ super/way/make_great_work_under_man/high_time_and_young_world/place/early_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_day(str_arg):
+    next_work_and_big_work(str_arg)
+    print('be_hand')
+
+def next_work_and_big_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_day('early_number')
