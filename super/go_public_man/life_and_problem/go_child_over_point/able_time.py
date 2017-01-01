44085fe6bef8c35e4aa3c2b6aec5246db256049b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 11:40:13 -0500

    "add: have super/go_public_man/life_and_problem/go_child_over_point/able_time.py from next life"

---
+++ super/go_public_man/life_and_problem/go_child_over_point/able_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_eye_under_new_fact(str_arg):
+    same_group_and_little_life(str_arg)
+    print('public_woman')
+
+def same_group_and_little_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_eye_under_new_fact('feel_way_from_person')
