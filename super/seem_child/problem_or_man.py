21b7ff417a793d9971bf9ce3a03314bc8fe80b92
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 11:41:46 -0500

    "mod: tell super/other_year_or_first_woman/company_or_woman.py or super/seem_child/problem_or_man.py with great man"

---
+++ super/seem_child/problem_or_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_different_person(str_arg):
+    year(str_arg)
+    print('group')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_different_person('work_or_problem')
