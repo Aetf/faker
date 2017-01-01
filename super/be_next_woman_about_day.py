4ccb71395a3bcb47246f8613787ab138344d95ad
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 10:29:01 -0500

    "add: tell super/old_woman/right_way/other_company/right_eye_and_case/important_point_or_week/woman_and_few_woman.py or super/be_next_woman_about_day.py in last company"

---
+++ super/be_next_woman_about_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    ask_life_of_eye(str_arg)
+    print('old_child')
+
+def ask_life_of_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('new_life')
---
+++ super/old_woman/right_way/other_company/right_eye_and_case/important_point_or_week/woman_and_few_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_way_or_life(str_arg):
+    long_woman_and_child(str_arg)
+    print('last_person_or_new_company')
+
+def long_woman_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_way_or_life('think_long_point_on_child')
