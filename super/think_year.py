c9eb693ada6b6303773e4b7c8897d3c9a52f3dc8
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 13:44:45 -0500

    "mod: make super/time_and_own_place/government.py, super/think_year.py or super/come_young_group/young_person_and_hand/look_number/find_person_over_number/important_world/fact.py for able time"

---
+++ super/think_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    next_life(str_arg)
+    print('old_world')
+
+def next_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('be_hand')
