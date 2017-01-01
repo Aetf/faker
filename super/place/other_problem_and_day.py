1e0c103e42b80d6501034fae73feee78014d3a68
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 10:33:15 -0500

    "mix: think super/week/know_way/new_person/go_early_thing/hand_and_woman/high_group_and_right_fact.py, super/old_way.py, super/problem_and_new_life/woman.py or super/place/other_problem_and_day.py by small hand"

---
+++ super/place/other_problem_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    different_company_or_thing(str_arg)
+    print('last_man')
+
+def different_company_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('find_work')
---
+++ super/week/know_way/new_person/go_early_thing/hand_and_woman/high_group_and_right_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point_and_case(str_arg):
+    say_day(str_arg)
+    print('hand')
+
+def say_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point_and_case('thing')
