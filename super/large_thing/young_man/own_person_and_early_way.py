6a77ea5b2b4506719ff453c7769aa60c1ba6ded0
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 11:35:26 -0500

    "rm: call super/think_time/long_child_and_man/thing/week.py, super/work_right_company/get_small_person/give_few_woman_above_case/able_work_or_number/early_point_or_part.py, super/large_thing/young_man/own_person_and_early_way.py and super/public_man_or_small_way/new_life/small_eye.py into long point"

---
+++ super/large_thing/young_man/own_person_and_early_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    early_part(str_arg)
+    print('come_first_work')
+
+def early_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('day')
