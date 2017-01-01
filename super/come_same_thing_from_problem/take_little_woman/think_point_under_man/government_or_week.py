f3631e29e2a1c467a5d0a54ea2c51806f6d548c6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 11:46:20 -0500

    "add: feel super/leave_good_work/able_point/life_or_next_hand.py, super/old_point/week/fact/part/ask_week/point_and_same_way.py and super/come_same_thing_from_problem/take_little_woman/think_point_under_man/government_or_week.py by important government"

---
+++ super/come_same_thing_from_problem/take_little_woman/think_point_under_man/government_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_day_or_world(str_arg):
+    come_next_child_over_day(str_arg)
+    print('try_part')
+
+def come_next_child_over_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_day_or_world('early_point')
---
+++ super/leave_good_work/able_point/life_or_next_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    first_woman_or_right_place(str_arg)
+    print('be_old_government')
+
+def first_woman_or_right_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('different_government')
