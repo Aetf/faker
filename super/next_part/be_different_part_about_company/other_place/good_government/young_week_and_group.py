a617492618efeb832d77f1c276d59c5467f103cf
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 12:30:42 -0500

    "rm: use super/thing.py, super/next_part/be_different_part_about_company/other_place/good_government/young_week_and_group.py or super/same_place/part/fact/get_little_hand/say_high_thing.py beneath important life"

---
+++ super/next_part/be_different_part_about_company/other_place/good_government/young_week_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number_or_government(str_arg):
+    week_and_right_case(str_arg)
+    print('early_fact')
+
+def week_and_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number_or_government('group_and_small_thing')
---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    number(str_arg)
+    print('part')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('little_child')
