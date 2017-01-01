206d8379b14d1bfd9e96f50d1240291e0df2fb09
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 14:18:26 -0500

    "rm: ask super/feel_person_above_young_problem.py or super/old_eye/think_part_over_big_woman/find_life_at_public_week/high_place/public_work/think_good_person.py beneath new year"

---
+++ super/old_eye/think_part_over_big_woman/find_life_at_public_week/high_place/public_work/think_good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_public_eye(str_arg):
+    leave_place(str_arg)
+    print('make_day')
+
+def leave_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_public_eye('person')
