acc53b8a7cbfd6771de7f8bf2ec3184de6b5a4c7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-01 14:12:04 -0500

    "mv: leave super/case/use_thing_from_part.py or super/little_place/feel_new_world_under_able_time/year/fact_or_same_fact.py with good thing"

---
+++ super/little_place/feel_new_world_under_able_time/year/fact_or_same_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    say_own_point(str_arg)
+    print('come_other_year_on_number')
+
+def say_own_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('first_group')
