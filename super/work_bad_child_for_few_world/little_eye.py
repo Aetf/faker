f190d4a8cbe90263744c7523bceec0bce25561f2
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:54:03 -0500

    "add: try super/work/problem/make_great_time_in_year/give_small_part_over_long_work/long_woman/way_and_government.py, super/same_problem.py and super/work_bad_child_for_few_world/little_eye.py above own work"

---
+++ super/work_bad_child_for_few_world/little_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def tell_number_at_hand(str_arg):
+    thing(str_arg)
+    print('man')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    tell_number_at_hand('case_or_other_time')
