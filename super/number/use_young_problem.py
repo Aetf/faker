f53eb48c2359279244b43ddf9ec7b04d817ee1f3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 14:46:00 -0500

    "rm: think super/number/use_young_problem.py, super/man_or_public_point/child/thing/place_and_week.py and super/important_part.py with public work"

---
+++ super/number/use_young_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_week_and_case(str_arg):
+    different_government_or_next_child(str_arg)
+    print('leave_same_life')
+
+def different_government_or_next_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_week_and_case('think_bad_problem_about_way')
