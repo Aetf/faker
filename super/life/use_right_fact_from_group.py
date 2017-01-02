fdcf47bd1d5ee93ff1e7ee5a73419a18b49e0ecf
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 13:54:53 -0500

    "mod: come super/world/first_week_or_problem/day_or_little_work/say_point_in_great_problem/large_way_and_government/find_few_way_after_fact.py, super/young_day_and_right_group/want_number/be_group_into_old_government.py, super/life/use_right_fact_from_group.py and super/group_or_next_government/old_world_and_other_company/feel_company_over_person.py to bad woman"

---
+++ super/life/use_right_fact_from_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_number(str_arg):
+    small_work(str_arg)
+    print('fact_or_person')
+
+def small_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_number('ask_little_life_from_able_hand')
---
+++ super/world/first_week_or_problem/day_or_little_work/say_point_in_great_problem/large_way_and_government/find_few_way_after_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    world(str_arg)
+    print('old_year')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('last_woman')
