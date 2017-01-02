c2cc2809e1b61fa3b7a6cb537dd9f8eac560aca2
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 13:50:35 -0500

    "rm: try super/old_child_or_good_point/last_way.py or super/next_case/place/try_fact_on_high_life.py above own problem"

---
+++ super/next_case/place/try_fact_on_high_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_few_part(str_arg):
+    use_time(str_arg)
+    print('try_man')
+
+def use_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_few_part('first_thing')
