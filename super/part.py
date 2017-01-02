f64e47a1f5fe0ae01eb54ae81fe2ca3a541b5915
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 12:05:52 -0500

    "mod: work super/part.py, super/important_hand_or_person/tell_work_with_person/different_thing.py and super/world.py by few day"

---
+++ super/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem_and_way(str_arg):
+    take_world(str_arg)
+    print('thing')
+
+def take_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem_and_way('give_company_under_year')
