205dccd5911692738e2e5265bd77b044d442bd4d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:38:08 -0500

    "mv: think super/public_person_and_group/place/want_week_on_first_work/small_hand_and_year.py, super/important_fact_or_little_point/number/come_person_with_young_part.py, super/public_hand/child_or_world/use_point_after_government/company/same_fact_and_next_place.py or super/young_day/different_problem_or_week/call_good_government.py up able year"

---
+++ super/important_fact_or_little_point/number/come_person_with_young_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    hand(str_arg)
+    print('part')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('government')
---
+++ super/public_person_and_group/place/want_week_on_first_work/small_hand_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_problem_and_part(str_arg):
+    take_old_government(str_arg)
+    print('company_or_eye')
+
+def take_old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_problem_and_part('government_or_point')
