ba97738c6271e38117d334621ef14039de34bdff
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 10:19:18 -0500

    "mix: work super/world/week_or_thing/early_place/next_life_and_bad_place/feel_person_of_thing/early_point.py under great hand"

---
+++ super/world/week_or_thing/early_place/next_life_and_bad_place/feel_person_of_thing/early_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    place(str_arg)
+    print('point')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('do_hand_about_large_person')
