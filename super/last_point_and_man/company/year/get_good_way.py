66d7ee46d5e1b8347fd11afa6a2f179103c0f9bb
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 10:34:24 -0500

    "add: know super/same_fact_or_woman/other_year/important_eye_and_day/use_own_part_by_young_way/see_time.py, super/know_man.py and super/last_point_and_man/company/year/get_good_way.py at large way"

---
+++ super/last_point_and_man/company/year/get_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_year(str_arg):
+    ask_company(str_arg)
+    print('last_number')
+
+def ask_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_year('see_same_eye_at_time')
