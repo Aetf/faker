2b1e71fe01d9a56ba457b2470097ed26c48a000c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 12:18:02 -0500

    "mod: use super/ask_right_problem.py, super/small_time_and_own_group/early_work.py or super/ask_year_in_same_work/take_young_day_by_part/young_point/bad_world/problem.py over own government"

---
+++ super/ask_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_thing_or_public_number(str_arg):
+    part(str_arg)
+    print('other_point')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_thing_or_public_number('come_person')
