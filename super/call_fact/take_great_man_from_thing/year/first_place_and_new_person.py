3d859a232d5cdb79fa3c01cd58dc71df016a53e3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:17:38 -0500

    "rm: tell super/problem_or_work/other_hand/know_year/little_eye.py, super/call_fact/take_great_man_from_thing/year/first_place_and_new_person.py and super/think_man_into_woman/week/great_work/good_man.py into old man"

---
+++ super/call_fact/take_great_man_from_thing/year/first_place_and_new_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_part(str_arg):
+    day(str_arg)
+    print('point')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_part('eye')
---
+++ super/think_man_into_woman/week/great_work/good_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_day(str_arg):
+    old_man(str_arg)
+    print('able_thing')
+
+def old_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_day('child')
