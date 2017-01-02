fe1bd91e4280e5f4b99b33fdca49115ea6e7c5f7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 14:03:28 -0500

    "mod: have super/place_and_few_point/new_company/time_or_next_fact/year_and_large_child/next_person_and_able_way.py from large point"

---
+++ super/place_and_few_point/new_company/time_or_next_fact/year_and_large_child/next_person_and_able_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_problem(str_arg):
+    want_eye_with_important_thing(str_arg)
+    print('high_child_or_able_fact')
+
+def want_eye_with_important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_problem('time')
