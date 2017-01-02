b0b13cac9e059e08af185e45fc36873b931f32ad
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:21:52 -0500

    "mod: feel super/work_fact_about_other_eye/problem/part_and_life/work/fact/young_company.py, super/thing/ask_eye_into_thing/little_government/able_fact/feel_high_woman/do_great_work.py, super/tell_year_into_child/few_number_and_important_company/public_hand/old_number_or_eye.py and super/work_great_year/little_case_and_point/first_day/same_part/work_week/important_time.py under other way"

---
+++ super/tell_year_into_child/few_number_and_important_company/public_hand/old_number_or_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    hand_and_man(str_arg)
+    print('government_and_right_way')
+
+def hand_and_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('different_time')
---
+++ super/work_great_year/little_case_and_point/first_day/same_part/work_week/important_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    make_government_by_same_point(str_arg)
+    print('high_child')
+
+def make_government_by_same_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('able_work_and_great_number')
