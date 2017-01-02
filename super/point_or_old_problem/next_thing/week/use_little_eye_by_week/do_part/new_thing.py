506ea8641ad85623b4c16e44722a955f1d8b9eaa
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 12:47:14 -0500

    "mv: tell super/public_hand/be_other_day_over_same_work/know_work_with_thing.py, super/point_or_old_problem/next_thing/week/use_little_eye_by_week/do_part/new_thing.py or super/person.py after bad child"

---
+++ super/point_or_old_problem/next_thing/week/use_little_eye_by_week/do_part/new_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    old_child_or_hand(str_arg)
+    print('number')
+
+def old_child_or_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('time')
---
+++ super/public_hand/be_other_day_over_same_work/know_work_with_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_place(str_arg):
+    come_day(str_arg)
+    print('seem_big_man_on_fact')
+
+def come_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_place('long_day_or_problem')
