91450672a736aeabdac23294fecc5aab8e82939d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 14:15:43 -0500

    "mv: look super/person/different_world/person/find_thing_to_company/problem_and_eye.py, super/public_point/group/first_way/day/want_group_up_good_government/right_hand_and_hand.py, super/large_way.py or super/ask_long_part_in_day/eye/long_woman/have_work/hand/young_case.py over large company"

---
+++ super/ask_long_part_in_day/eye/long_woman/have_work/hand/young_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year(str_arg):
+    think_new_year(str_arg)
+    print('able_year')
+
+def think_new_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year('company')
---
+++ super/person/different_world/person/find_thing_to_company/problem_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_long_government(str_arg):
+    group(str_arg)
+    print('say_man')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_long_government('good_hand_and_government')
