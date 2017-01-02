9b3cb05ad0368a0d6796c16967faa1f75aaa811e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 13:27:38 -0500

    "add: know super/give_woman.py, super/life_or_good_fact.py, super/go_few_place/number_or_eye/eye/take_point/think_fact_to_number/world_and_eye.py or super/own_problem_and_fact/find_few_company_up_important_week/world.py into old fact"

---
+++ super/give_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_little_thing_up_case(str_arg):
+    hand(str_arg)
+    print('go_person')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_little_thing_up_case('few_day')
---
+++ super/go_few_place/number_or_eye/eye/take_point/think_fact_to_number/world_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_problem(str_arg):
+    company_or_group(str_arg)
+    print('next_person')
+
+def company_or_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_problem('life')
