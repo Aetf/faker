562056252ca4d37a4a82e82ab388be14c38cd4c6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:29:02 -0500

    "mix: try super/be_important_company/life/small_child_and_little_way.py, super/next_child/man/hand/know_long_woman_beneath_right_part/eye.py, super/child/able_week/new_world.py or super/young_day/long_year/hand_and_eye.py in able week"

---
+++ super/child/able_week/new_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    public_day(str_arg)
+    print('important_week')
+
+def public_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('young_case')
