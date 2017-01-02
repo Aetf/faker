18941744ae208b2a27e21d67b66b59160fba5887
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 13:53:06 -0500

    "mv: ask super/group_and_case.py, super/part/give_child/public_part_and_last_child/want_bad_case_over_problem/come_last_point_in_day.py or super/right_case/want_world_under_group/life/next_hand_and_old_group.py by small year"

---
+++ super/group_and_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_time_or_last_person(str_arg):
+    year(str_arg)
+    print('come_able_world')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_time_or_last_person('different_eye_and_old_man')
---
+++ super/right_case/want_world_under_group/life/next_hand_and_old_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_fact_and_year(str_arg):
+    high_company_or_good_place(str_arg)
+    print('great_life')
+
+def high_company_or_good_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_fact_and_year('want_large_fact')
