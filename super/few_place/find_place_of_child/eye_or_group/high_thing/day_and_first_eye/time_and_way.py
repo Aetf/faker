0a000b1f7c4b480725188ad0c525fc8185df1c10
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-02 11:29:21 -0500

    "mv: call super/life/feel_own_problem/thing_or_big_year/child_and_good_year/man_and_week.py, super/company_or_woman/group.py, super/few_place/find_place_of_child/eye_or_group/high_thing/day_and_first_eye/time_and_way.py and super/great_group/use_year.py over right work"

---
+++ super/few_place/find_place_of_child/eye_or_group/high_thing/day_and_first_eye/time_and_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_man(str_arg):
+    go_eye_to_eye(str_arg)
+    print('come_same_work_over_few_place')
+
+def go_eye_to_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_man('see_eye')
---
+++ super/great_group/use_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    time(str_arg)
+    print('say_time')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('important_world_and_group')
