a81c44a3d9b571014d387820681e26d5d6c72d43
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 11:44:10 -0500

    "mix: work super/go_part/different_fact/small_week_and_different_woman/feel_own_person.py, super/own_company_or_first_government.py and super/right_point/tell_first_company/same_woman_and_first_eye/work_world_over_little_work/child/same_hand.py beneath own hand"

---
+++ super/own_company_or_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_point(str_arg):
+    important_thing(str_arg)
+    print('next_problem')
+
+def important_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_point('problem')
---
+++ super/right_point/tell_first_company/same_woman_and_first_eye/work_world_over_little_work/child/same_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_day(str_arg):
+    fact(str_arg)
+    print('point')
+
+def fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_day('bad_point')
