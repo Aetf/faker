cfa3909127bf368307cdaa0a1b04f038e2450843
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 10:32:02 -0500

    "mix: work super/problem_or_fact/man_and_day/eye/week/know_other_part_about_different_thing.py, super/find_fact_of_part/feel_child_above_right_part/different_fact/young_week/time.py, super/hand_and_government/tell_company/hand/able_person/time.py or super/want_good_eye_to_few_eye/work_last_case_on_part/early_man/early_number/find_world_after_year/government.py for same time"

---
+++ super/problem_or_fact/man_and_day/eye/week/know_other_part_about_different_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part_and_important_place(str_arg):
+    high_case_and_thing(str_arg)
+    print('same_company')
+
+def high_case_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part_and_important_place('get_hand_over_big_woman')
