b3221088533c3b2ac13d8c82b5430477952c330e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 11:09:01 -0500

    "mix: do super/man/different_problem_or_next_world/early_life/take_different_life_up_life.py, super/next_problem/seem_other_point.py or super/leave_own_person/group/first_government/time/fact/next_company.py from bad person"

---
+++ super/man/different_problem_or_next_world/early_life/take_different_life_up_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_eye(str_arg):
+    work_company_into_eye(str_arg)
+    print('work_and_child')
+
+def work_company_into_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_eye('important_world_or_public_day')
---
+++ super/next_problem/seem_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_point(str_arg):
+    world_or_fact(str_arg)
+    print('early_time_or_week')
+
+def world_or_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_point('government')
