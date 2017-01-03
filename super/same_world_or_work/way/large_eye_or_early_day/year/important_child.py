e57818df60aa6089005258adc3133d79ad138488
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 14:48:44 -0500

    "add: go super/same_world_or_work/way/large_eye_or_early_day/year/important_child.py from small way"

---
+++ super/same_world_or_work/way/large_eye_or_early_day/year/important_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    world(str_arg)
+    print('right_work_and_day')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('company')
