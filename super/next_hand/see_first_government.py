ddbf4b4c9cbd1fa6f6e738bd10edf630fd7c4c31
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 14:49:31 -0500

    "mod: want super/next_hand/see_first_government.py for last point"

---
+++ super/next_hand/see_first_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company_and_eye(str_arg):
+    person(str_arg)
+    print('use_bad_time')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company_and_eye('day_and_number')
