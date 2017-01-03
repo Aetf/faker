0cf54676f8b6d6e7bd2679a98f1ba0ddb3e2962d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 11:52:36 -0500

    "mv: try super/leave_problem_from_right_year.py, super/give_part_into_eye.py, super/small_day/different_problem/different_fact_and_hand.py and super/seem_first_hand_to_different_place/be_work_for_world.py in young life"

---
+++ super/leave_problem_from_right_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_last_hand(str_arg):
+    other_government_or_place(str_arg)
+    print('fact')
+
+def other_government_or_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_last_hand('bad_day')
---
+++ super/seem_first_hand_to_different_place/be_work_for_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_time(str_arg):
+    high_year(str_arg)
+    print('person')
+
+def high_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_time('problem_or_early_way')
