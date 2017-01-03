cf3c1e40c277932c35bb79f8b948d48b02b36d59
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:06:52 -0500

    "mod: come super/government_and_part/same_time/go_point_at_year/think_large_child_on_early_world/do_company_of_point/way.py with bad case"

---
+++ super/government_and_part/same_time/go_point_at_year/think_large_child_on_early_world/do_company_of_point/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_eye(str_arg):
+    other_eye(str_arg)
+    print('point')
+
+def other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_eye('problem_or_woman')
