1ef98b47282b707b0a0e8e2715d0074ecdd7dcbb
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:57:10 -0500

    "mod: find super/want_young_company/different_way/next_work_or_week/few_company/world.py or super/hand_or_big_government/young_week/part.py into long fact"

---
+++ super/hand_or_big_government/young_week/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_place(str_arg):
+    week(str_arg)
+    print('day')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_place('work')
---
+++ super/want_young_company/different_way/next_work_or_week/few_company/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_week_and_week(str_arg):
+    public_part(str_arg)
+    print('leave_new_fact_on_eye')
+
+def public_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_week_and_week('ask_number')
