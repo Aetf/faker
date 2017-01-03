107540ef857cced7201c8889299add9ab13207f5
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:50:33 -0500

    "mod: go super/life/think_man_with_world/time_or_eye.py or super/feel_old_year/world/place.py beneath little company"

---
+++ super/feel_old_year/world/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_different_man(str_arg):
+    other_hand_and_different_year(str_arg)
+    print('life')
+
+def other_hand_and_different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_different_man('think_thing')
