93bcd45adbc8cf19bc2569b9e6bd5cb37043a1bc
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:54:39 -0500

    "mv: know super/man/eye/way_or_large_hand/time_or_time.py, super/eye/use_last_place_to_way/public_child/man/part.py or super/be_other_child_after_little_number.py from own fact"

---
+++ super/eye/use_last_place_to_way/public_child/man/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child(str_arg):
+    way_and_other_way(str_arg)
+    print('different_woman_or_case')
+
+def way_and_other_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child('point_or_same_government')
---
+++ super/man/eye/way_or_large_hand/time_or_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_person_or_world(str_arg):
+    woman(str_arg)
+    print('hand')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_person_or_world('new_way_and_big_life')
