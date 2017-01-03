ff4f3e0d03947c6b741e5f217cf3dc5ab5aadeda
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 13:18:41 -0500

    "mix: make super/work_woman_after_child.py for big child"

---
+++ super/work_woman_after_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_part(str_arg):
+    problem(str_arg)
+    print('woman_or_man')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_part('eye')
