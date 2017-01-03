a3f702bd732106c7d98a5bb9acedae47679ae62c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 10:47:50 -0500

    "rm: think super/big_part_and_way/eye_and_part/old_week_and_last_woman.py, super/place/fact/take_last_company_up_last_woman.py or super/life_and_next_point/want_point_for_early_person/life.py into little work"

---
+++ super/big_part_and_way/eye_and_part/old_week_and_last_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_government(str_arg):
+    early_number(str_arg)
+    print('thing')
+
+def early_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_government('part')
