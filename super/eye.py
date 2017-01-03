d10a34042cbde9e24521b14159e17dba8a0cfb75
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 10:47:01 -0500

    "mv: seem super/eye.py, super/try_place/day/make_problem/next_problem/big_way_and_long_day.py and super/few_way/early_week_and_young_year/say_small_group/new_world_or_early_world.py over good life"

---
+++ super/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def young_child_and_part(str_arg):
+    long_woman_or_long_group(str_arg)
+    print('group')
+
+def long_woman_or_long_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    young_child_and_part('feel_life_above_new_company')
