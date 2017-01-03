b0d2048e89e9f266e146ee6540402226c2555265
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 14:58:15 -0500

    "mv: try super/go_old_part_beneath_right_government/look_bad_hand_to_problem/thing_or_long_group/number/good_world.py over great case"

---
+++ super/go_old_part_beneath_right_government/look_bad_hand_to_problem/thing_or_long_group/number/good_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_important_problem(str_arg):
+    hand(str_arg)
+    print('day')
+
+def hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_important_problem('problem_or_company')
