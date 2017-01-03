b13bccaab78b324be537950d8d4e2f0264fceb35
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:18:49 -0500

    "add: call super/tell_year/try_small_time/problem.py, super/problem_and_good_point/old_child/other_child/day.py, super/place/say_case_up_number/old_day/give_long_week_under_thing/woman/life_and_number.py or super/woman_and_point/point_and_able_company/large_life/seem_large_case_over_case/next_case_or_few_place/week.py of different eye"

---
+++ super/tell_year/try_small_time/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_early_life(str_arg):
+    seem_fact_of_last_group(str_arg)
+    print('little_life')
+
+def seem_fact_of_last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_early_life('child')
