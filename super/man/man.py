c8540007bbb7d95be113193b8e6b8523ab6423ed
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 11:59:16 -0500

    "add: get super/small_number/large_problem/find_problem.py, super/person/time/thing.py and super/man/man.py over first life"

---
+++ super/man/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def take_high_problem(str_arg):
+    world(str_arg)
+    print('old_problem_and_hand')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    take_high_problem('number_and_great_part')
