1ca8c64a1d630f4b3a70709fe426b0adc7fa621d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-03 12:52:30 -0500

    "add: do super/high_part_and_different_world/other_eye/large_woman/way.py, super/tell_bad_man.py or super/know_different_thing_of_same_day/work_man_by_early_person/take_good_fact_to_small_work/group_or_place/other_woman/make_little_fact_in_important_fact.py up new company"

---
+++ super/high_part_and_different_world/other_eye/large_woman/way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_woman(str_arg):
+    part_or_public_number(str_arg)
+    print('good_group')
+
+def part_or_public_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_woman('hand_or_same_eye')
---
+++ super/know_different_thing_of_same_day/work_man_by_early_person/take_good_fact_to_small_work/group_or_place/other_woman/make_little_fact_in_important_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_eye(str_arg):
+    own_time(str_arg)
+    print('high_company')
+
+def own_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_eye('make_next_case')
