de402db53cba776ec515586504224e8f4f5ad713
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 10:47:25 -0500

    "mod: tell super/new_work.py, super/early_hand/week/part/see_high_point_by_year/woman_and_small_eye/other_year.py or super/person/come_person/next_life/use_great_person_on_problem/part_or_high_group/group_and_part.py to new company"

---
+++ super/person/come_person/next_life/use_great_person_on_problem/part_or_high_group/group_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def public_woman_and_great_life(str_arg):
+    try_large_point(str_arg)
+    print('number')
+
+def try_large_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    public_woman_and_great_life('tell_small_life_up_thing')
