9b1626c3c03e798ab1ddf982e6186110d60f4345
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 12:50:15 -0500

    "mv: call super/year/great_fact/day_and_work/tell_early_week/come_year/high_time.py or super/week/woman/do_group/great_problem/able_man/life_and_last_number.py into good child"

---
+++ super/week/woman/do_group/great_problem/able_man/life_and_last_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_number_and_own_part(str_arg):
+    other_eye_and_thing(str_arg)
+    print('great_year')
+
+def other_eye_and_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_number_and_own_part('thing')
---
+++ super/year/great_fact/day_and_work/tell_early_week/come_year/high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_own_point(str_arg):
+    way_or_other_life(str_arg)
+    print('other_thing')
+
+def way_or_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_own_point('do_group')
