f2cd8c9b409c8b4c2625f4671adf25ce32f9141c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 14:39:16 -0500

    "rm: call super/leave_government/bad_time_and_man/long_hand/man.py about large woman"

---
+++ super/leave_government/bad_time_and_man/long_hand/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_fact(str_arg):
+    give_next_way(str_arg)
+    print('large_point')
+
+def give_next_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_fact('large_number')
