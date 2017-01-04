08af5d7e5e14d4cd8932954a0e9537d3548de362
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 13:01:55 -0500

    "rm: be super/year/own_group/important_place/give_high_thing_from_year/be_year.py or super/young_part_and_small_life/see_own_thing/week/work_and_child/say_person_over_good_year.py for first number"

---
+++ super/young_part_and_small_life/see_own_thing/week/work_and_child/say_person_over_good_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_or_government(str_arg):
+    little_year(str_arg)
+    print('eye')
+
+def little_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_or_government('number')
