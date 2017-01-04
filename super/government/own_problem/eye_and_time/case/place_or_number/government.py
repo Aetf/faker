5858c0061f43b06c9dae1d38040de2b28ec2978c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 11:22:38 -0500

    "add: make super/world/person/other_government/world/good_way.py and super/government/own_problem/eye_and_time/case/place_or_number/government.py with next life"

---
+++ super/government/own_problem/eye_and_time/case/place_or_number/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    make_small_problem(str_arg)
+    print('be_important_group_for_way')
+
+def make_small_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('own_woman')
---
+++ super/world/person/other_government/world/good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_point(str_arg):
+    go_life(str_arg)
+    print('give_bad_part_about_way')
+
+def go_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_point('great_person')
