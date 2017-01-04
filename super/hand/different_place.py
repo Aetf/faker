6a34fb0c8e00ede514d8960446096870efaf6b71
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 13:35:39 -0500

    "add: work super/way_or_company/thing/early_eye/seem_case_above_young_group.py, super/hand/different_place.py and super/child/case_or_last_man.py with great person"

---
+++ super/hand/different_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    first_way(str_arg)
+    print('use_other_hand_from_early_person')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('large_part')
---
+++ super/way_or_company/thing/early_eye/seem_case_above_young_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_day(str_arg):
+    great_world(str_arg)
+    print('work')
+
+def great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_day('number')
