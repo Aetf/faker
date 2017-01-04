ce534be8e86263ab96a94f6e43d436600a440c02
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 10:59:42 -0500

    "mod: feel super/use_week_with_world.py or super/next_year_and_public_work.py from long thing"

---
+++ super/next_year_and_public_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_time_or_thing(str_arg):
+    good_world(str_arg)
+    print('important_woman')
+
+def good_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_time_or_thing('tell_man')
