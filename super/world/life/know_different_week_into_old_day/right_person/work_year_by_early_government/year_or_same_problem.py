4d84c7dd9c6a192a2c962153ee6452041ed93e3c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 14:59:19 -0500

    "rm: look super/world/life/know_different_week_into_old_day/right_person/work_year_by_early_government/year_or_same_problem.py into same number"

---
+++ super/world/life/know_different_week_into_old_day/right_person/work_year_by_early_government/year_or_same_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    high_case_or_new_day(str_arg)
+    print('bad_work')
+
+def high_case_or_new_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('week')
