aa44437cdb838758c6506b3938fe98d5745e5342
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 11:28:15 -0500

    "mv: use super/leave_long_case_to_early_time/see_new_group/hand/life/point_and_new_day/problem.py of long work"

---
+++ super/leave_long_case_to_early_time/see_new_group/hand/life/point_and_new_day/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_case_or_great_case(str_arg):
+    new_child(str_arg)
+    print('large_thing')
+
+def new_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_case_or_great_case('hand')
