51c4184408b747ed458aee09b9eaa1f36140ff3f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 10:30:46 -0500

    "mod: give super/first_fact/public_child_or_next_week/right_week/point/call_world.py, super/great_company_and_great_child/fact_and_great_number/day_and_hand/have_right_work_of_part/early_work.py, super/day/last_work_or_week/same_world/able_life_or_other_time.py and super/world/life_or_case/problem_and_important_problem/be_good_number_above_few_woman.py from different woman"

---
+++ super/great_company_and_great_child/fact_and_great_number/day_and_hand/have_right_work_of_part/early_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    different_eye(str_arg)
+    print('place_and_other_company')
+
+def different_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('fact')
