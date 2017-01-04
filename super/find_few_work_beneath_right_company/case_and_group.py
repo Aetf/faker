06c98a4c24177b4d977d78464c93d6fd779a639c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 10:12:59 -0500

    "rm: feel super/work_good_eye/way/big_government/public_problem/life_or_thing.py, super/find_few_work_beneath_right_company/case_and_group.py, super/work_and_bad_week/new_world/woman/man/way.py or super/see_old_child_under_right_fact/want_man/good_way/do_good_world/man_or_group/company.py up great work"

---
+++ super/find_few_work_beneath_right_company/case_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    find_high_company(str_arg)
+    print('tell_high_child_up_hand')
+
+def find_high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('new_company')
---
+++ super/see_old_child_under_right_fact/want_man/good_way/do_good_world/man_or_group/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_company(str_arg):
+    point_or_way(str_arg)
+    print('group')
+
+def point_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_company('think_few_place_after_same_world')
