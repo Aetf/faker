531508185f1d5c5d8b34d9fbd8aee599510487e7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 13:24:34 -0500

    "mod: say super/great_government/do_man_over_company/able_work_or_little_person/thing/number.py after bad thing"

---
+++ super/great_government/do_man_over_company/able_work_or_little_person/thing/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    do_little_way_above_old_year(str_arg)
+    print('come_problem')
+
+def do_little_way_above_old_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('do_different_life_in_world')
