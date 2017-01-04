717cda29c7a53f3a72c41461be8bd522c066c8aa
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 12:57:43 -0500

    "add: take super/ask_early_place_after_last_man/have_life/work_day_under_way/way_or_able_person.py, super/high_group/right_world_or_old_group/take_long_point/life/new_work_or_old_man.py, super/last_woman/long_woman/work.py or super/come_good_problem_up_different_problem/person/different_eye_and_thing/hand/try_own_point.py about first thing"

---
+++ super/high_group/right_world_or_old_group/take_long_point/life/new_work_or_old_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def next_man_or_government(str_arg):
+    important_group(str_arg)
+    print('right_problem_or_way')
+
+def important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    next_man_or_government('world_or_early_government')
---
+++ super/last_woman/long_woman/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world(str_arg):
+    do_different_group_to_high_man(str_arg)
+    print('go_first_company_about_high_work')
+
+def do_different_group_to_high_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world('person_or_public_person')
