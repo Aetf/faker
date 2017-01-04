2a019ba7a8d494b7b7818773e2fe425127c2f36f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 14:46:42 -0500

    "mod: think super/do_part/woman/hand/woman/ask_life.py or super/tell_work/make_eye/work_part/big_time_or_thing/go_number_from_point/work.py of right child"

---
+++ super/tell_work/make_eye/work_part/big_time_or_thing/go_number_from_point/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work_and_part(str_arg):
+    see_great_company_after_world(str_arg)
+    print('work_or_great_man')
+
+def see_great_company_after_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work_and_part('year_and_right_woman')
