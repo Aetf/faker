25a9c0c8646567cf3668d30bbfdb9cf39fc35153
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 13:12:34 -0500

    "add: work super/few_work/public_woman/thing/child.py or super/bad_year.py beneath public case"

---
+++ super/few_work/public_woman/thing/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_work_or_company(str_arg):
+    year(str_arg)
+    print('man')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_work_or_company('little_group')
