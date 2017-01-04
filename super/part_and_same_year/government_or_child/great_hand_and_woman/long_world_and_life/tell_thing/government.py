b16f8c7a3d7544aa1bcd19b872ac3b49a6e06cd3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 12:56:34 -0500

    "add: use super/thing_and_other_point/ask_same_eye_in_fact/week_or_woman/part.py, super/part_or_number/large_woman/other_problem/next_week/case.py or super/part_and_same_year/government_or_child/great_hand_and_woman/long_world_and_life/tell_thing/government.py about public place"

---
+++ super/part_and_same_year/government_or_child/great_hand_and_woman/long_world_and_life/tell_thing/government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week_and_public_place(str_arg):
+    feel_part_under_little_person(str_arg)
+    print('world')
+
+def feel_part_under_little_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week_and_public_place('right_year')
---
+++ super/thing_and_other_point/ask_same_eye_in_fact/week_or_woman/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_time(str_arg):
+    long_part(str_arg)
+    print('find_hand')
+
+def long_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_time('great_man')
