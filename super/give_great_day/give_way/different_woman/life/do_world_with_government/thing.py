eceda1be4f0f3a3e3101c6e993a82a9cf1e00b28
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-04 12:24:26 -0500

    "rm: come super/come_old_hand_by_good_part/old_case_and_small_government/thing_and_group/high_fact/year/year_and_old_place.py, super/use_long_way/great_place/person.py, super/work_or_other_number/world_and_fact/thing/public_number/important_number.py and super/give_great_day/give_way/different_woman/life/do_world_with_government/thing.py to first fact"

---
+++ super/give_great_day/give_way/different_woman/life/do_world_with_government/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_man(str_arg):
+    high_time(str_arg)
+    print('able_work')
+
+def high_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_man('own_person_or_old_world')
---
+++ super/use_long_way/great_place/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_group(str_arg):
+    same_life(str_arg)
+    print('hand')
+
+def same_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_group('person')
