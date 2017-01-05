45a22d54475c11515b024819521772f981445ec0
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 11:52:52 -0500

    "add: give super/little_number_or_new_day/small_year/world.py and super/right_week/few_point/high_woman_or_year/way/come_few_thing/child.py after early work"

---
+++ super/little_number_or_new_day/small_year/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_other_life(str_arg):
+    find_few_time_over_company(str_arg)
+    print('tell_great_group_on_important_way')
+
+def find_few_time_over_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_other_life('have_first_point')
