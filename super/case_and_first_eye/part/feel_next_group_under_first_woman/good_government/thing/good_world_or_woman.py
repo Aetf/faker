458bdda44464ec684e854d67bb726b08ce33cc1b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 12:37:27 -0500

    "add: see super/year_and_old_hand/find_small_problem_above_group/man/group/ask_high_world/important_time_and_person.py, super/case_and_first_eye/part/feel_next_group_under_first_woman/good_government/thing/good_world_or_woman.py and super/other_point_or_company.py over small world"

---
+++ super/case_and_first_eye/part/feel_next_group_under_first_woman/good_government/thing/good_world_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_child(str_arg):
+    say_hand(str_arg)
+    print('first_place')
+
+def say_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_child('number')
