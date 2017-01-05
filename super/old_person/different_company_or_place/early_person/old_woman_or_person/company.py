a9087e2664826f9149e10af91cd6c120a7b2a0b6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 13:47:41 -0500

    "mod: see super/old_person/different_company_or_place/early_person/old_woman_or_person/company.py about young point"

---
+++ super/old_person/different_company_or_place/early_person/old_woman_or_person/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_week(str_arg):
+    place(str_arg)
+    print('look_year')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_week('make_public_time')
