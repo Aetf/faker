853f258cfb065125acb827d181fe2964c75e5104
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 10:20:00 -0500

    "mv: get super/great_world/bad_world_and_life/group/place_or_fact.py, super/year_or_case/big_work/problem_or_good_life/company_and_large_company/old_year_or_life/hand_or_case.py or super/ask_important_way/time/young_group/hand.py up good eye"

---
+++ super/great_world/bad_world_and_life/group/place_or_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_child(str_arg):
+    use_person(str_arg)
+    print('group_and_same_government')
+
+def use_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_child('call_group_over_child')
