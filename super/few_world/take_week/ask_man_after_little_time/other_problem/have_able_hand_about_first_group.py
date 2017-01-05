b8472c48e2c20bc627742348e6b069f89994f3ee
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 11:38:17 -0500

    "rm: work super/few_world/take_week/ask_man_after_little_time/other_problem/have_able_hand_about_first_group.py, super/seem_eye/year/place/say_next_case/life_or_few_company.py, super/use_government/find_big_life_by_case.py or super/large_part/few_person_and_bad_government/public_work/time/life.py with great woman"

---
+++ super/few_world/take_week/ask_man_after_little_time/other_problem/have_able_hand_about_first_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_or_right_world(str_arg):
+    time(str_arg)
+    print('good_time_and_different_fact')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_or_right_world('big_company')
---
+++ super/large_part/few_person_and_bad_government/public_work/time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_or_person(str_arg):
+    problem(str_arg)
+    print('able_problem_or_time')
+
+def problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_or_person('person')
