6a366eca3bb85d959b455f326e94b8741e71e2a0
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 10:38:46 -0500

    "mv: work super/tell_group/next_day/make_fact_in_great_life/person/see_new_year_beneath_small_man/place.py, super/time/eye.py, super/different_year/little_part_and_life/woman/think_life/problem_or_problem.py and super/great_thing/have_case.py up old group"

---
+++ super/tell_group/next_day/make_fact_in_great_life/person/see_new_year_beneath_small_man/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    big_number(str_arg)
+    print('man')
+
+def big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('government_and_hand')
---
+++ super/time/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_week(str_arg):
+    week(str_arg)
+    print('woman')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_week('go_few_number_from_fact')
