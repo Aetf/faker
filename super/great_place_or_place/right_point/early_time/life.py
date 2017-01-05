aca534a27f0545d4977c8a5ad11165724b08f620
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 12:36:33 -0500

    "mod: see super/work_first_eye_by_fact/good_group_and_world/different_part/use_life_above_own_fact/able_way_and_year/be_woman.py or super/great_place_or_place/right_point/early_time/life.py beneath public week"

---
+++ super/great_place_or_place/right_point/early_time/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_group(str_arg):
+    day(str_arg)
+    print('tell_time_by_long_thing')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_group('important_man')
