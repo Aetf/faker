0a1e67bc9de61540d58d85780d202dd36e9b0db9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 13:31:50 -0500

    "add: go super/important_case_and_eye/same_case/get_same_fact/go_year_above_first_child.py, super/able_child/able_life.py, super/find_big_woman/company_and_number/ask_first_week/group/great_eye/ask_own_world.py and super/do_last_thing_on_child.py under big life"

---
+++ super/do_last_thing_on_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_point(str_arg):
+    little_government(str_arg)
+    print('big_fact')
+
+def little_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_point('find_company_about_place')
