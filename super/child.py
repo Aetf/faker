56e013b44d8ff39f78a65cac8444ee4851333fa7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 10:27:10 -0500

    "add: ask super/child.py, super/last_year/different_eye.py, super/small_company/first_case.py or super/work_or_good_eye/problem/group.py on small hand"

---
+++ super/child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    bad_life(str_arg)
+    print('seem_way_under_problem')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('large_day')
