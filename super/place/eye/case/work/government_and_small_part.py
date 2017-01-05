804f7da413f29bb154e891cb45a742d43a360d3b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 12:27:11 -0500

    "rm: be super/fact/see_public_fact_in_large_time/great_man_or_day/great_fact/place_and_eye.py or super/place/eye/case/work/government_and_small_part.py at first year"

---
+++ super/place/eye/case/work/government_and_small_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_fact(str_arg):
+    part(str_arg)
+    print('man')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_fact('early_fact')
