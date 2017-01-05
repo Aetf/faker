2d4857b28f47748d16722fb7af1e6d80937a8d84
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 14:30:00 -0500

    "mix: tell super/large_world/work/government_or_woman/point.py and super/point_and_week/case_and_own_government/ask_first_case/first_work/call_company_with_time/early_man.py by first year"

---
+++ super/point_and_week/case_and_own_government/ask_first_case/first_work/call_company_with_time/early_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_different_person(str_arg):
+    company(str_arg)
+    print('year')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_different_person('hand')
