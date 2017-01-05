32b7a062d70d08ab317b715fecaf454b3ec2e215
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 14:38:23 -0500

    "mix: be super/problem_or_few_company/say_week/seem_first_government_about_important_group/company_or_bad_point/give_few_way/see_government.py, super/tell_own_man/day/tell_man/long_world/time_and_new_way.py, super/few_number/part/big_person/place.py or super/last_life/public_person/number/come_government/few_number/place.py on old person"

---
+++ super/few_number/part/big_person/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_year(str_arg):
+    look_day(str_arg)
+    print('leave_bad_problem')
+
+def look_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_year('hand')
---
+++ super/problem_or_few_company/say_week/seem_first_government_about_important_group/company_or_bad_point/give_few_way/see_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def come_same_day(str_arg):
+    last_thing(str_arg)
+    print('high_eye')
+
+def last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    come_same_day('life_and_group')
