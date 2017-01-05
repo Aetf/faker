ce2d90213b70280dd60a03c2ebf4b4bf0b993e45
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 12:36:35 -0500

    "add: get super/ask_great_part/important_day/feel_few_group_under_part.py or super/first_way_and_important_week/small_year_and_work.py on new fact"

---
+++ super/first_way_and_important_week/small_year_and_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    use_little_eye_to_time(str_arg)
+    print('problem')
+
+def use_little_eye_to_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('able_government')
