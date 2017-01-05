14569c5a22985aefb3eb3f1b6a68888d767dffc9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 14:33:13 -0500

    "add: think super/bad_work/number.py or super/important_man/great_group/think_week.py from next way"

---
+++ super/bad_work/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_woman_beneath_woman(str_arg):
+    person(str_arg)
+    print('good_point_or_long_group')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_woman_beneath_woman('right_person')
---
+++ super/important_man/great_group/think_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def go_time_with_person(str_arg):
+    last_time(str_arg)
+    print('feel_child_after_place')
+
+def last_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    go_time_with_person('hand')
