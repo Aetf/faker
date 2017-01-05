f191273a9ff2917a942b3979a5f3f96dee8e0ea7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 12:57:33 -0500

    "mod: know super/few_world/last_group/small_week.py, super/able_time/company_or_right_fact/public_person/other_person_or_time.py, super/feel_child_with_number/able_person.py and super/young_week.py in big part"

---
+++ super/few_world/last_group/small_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_company_and_other_child(str_arg):
+    point(str_arg)
+    print('part_or_last_way')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_company_and_other_child('own_eye')
