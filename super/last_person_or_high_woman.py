268fb6e0738888b0b74e0bb3d9597e228b4804ac
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 13:11:50 -0500

    "mv: try super/last_person_or_high_woman.py after next hand"

---
+++ super/last_person_or_high_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    try_public_company(str_arg)
+    print('right_part')
+
+def try_public_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('company')
