8afb1621e5506439acad89bb5f47dcfea3ed7deb
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-05 10:44:55 -0500

    "mix: have super/child/day/little_case/small_way.py, super/come_few_place_about_work.py, super/big_point/make_same_government/company_and_point/great_point/week.py or super/different_government_or_government/want_number_with_part/early_woman/early_child/right_thing/give_few_work.py beneath able time"

---
+++ super/big_point/make_same_government/company_and_point/great_point/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_way_or_point(str_arg):
+    last_part(str_arg)
+    print('new_problem')
+
+def last_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_way_or_point('large_point')
---
+++ super/child/day/little_case/small_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_work(str_arg):
+    little_point_or_eye(str_arg)
+    print('way_or_group')
+
+def little_point_or_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_work('week')
