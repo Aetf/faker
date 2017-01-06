a80226ffa119d878bb93da0c6566fe509dabb4c4
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 14:22:13 -0500

    "rm: look super/place/big_child/feel_public_hand.py and super/place/good_time_or_long_place.py above large government"

---
+++ super/place/good_time_or_long_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_week_with_public_fact(str_arg):
+    do_point_with_life(str_arg)
+    print('fact')
+
+def do_point_with_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_week_with_public_fact('eye')
