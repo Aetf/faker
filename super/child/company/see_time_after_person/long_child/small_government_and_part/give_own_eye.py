3501d113a916508a4634414b7c267521cb0539b9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 11:30:30 -0500

    "add: call super/point_and_little_week.py and super/child/company/see_time_after_person/long_child/small_government_and_part/give_own_eye.py with high week"

---
+++ super/child/company/see_time_after_person/long_child/small_government_and_part/give_own_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_world_or_next_life(str_arg):
+    same_work(str_arg)
+    print('first_fact_or_government')
+
+def same_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_world_or_next_life('early_thing')
---
+++ super/point_and_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_week(str_arg):
+    thing(str_arg)
+    print('high_day')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_week('ask_group_by_year')
