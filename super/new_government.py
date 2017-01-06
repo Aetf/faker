5f094e07d7790b4702862816907562f15bd6c65c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 10:01:26 -0500

    "mod: feel super/new_government.py on next place"

---
+++ super/new_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case(str_arg):
+    call_eye(str_arg)
+    print('own_place')
+
+def call_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case('use_woman_to_large_child')
