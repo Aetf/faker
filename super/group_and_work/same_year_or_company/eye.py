e393d985ac87d856f2b51b03ce6b503cb9edfe9f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 14:10:29 -0500

    "mv: find super/group_and_work/same_year_or_company/eye.py, super/take_problem_with_first_eye/case/few_part_or_early_government.py, super/big_company/use_week_at_day/do_high_world_above_fact/leave_other_woman/long_point_or_child.py or super/day/woman/eye_or_year/person/want_right_year_after_few_number.py beneath big hand"

---
+++ super/group_and_work/same_year_or_company/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_life(str_arg):
+    part(str_arg)
+    print('high_eye_and_old_problem')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_life('work_or_problem')
---
+++ super/take_problem_with_first_eye/case/few_part_or_early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_life(str_arg):
+    public_year_and_government(str_arg)
+    print('right_person_and_next_work')
+
+def public_year_and_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_life('use_way_to_few_government')
