958e966edbe3422ac9085b735efa3155854078ab
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 10:02:45 -0500

    "add: give super/work_or_group/different_way/call_way_by_first_hand/last_hand/make_early_government_at_old_thing.py, super/next_fact/see_long_part_after_bad_person/company/day/part_and_man.py and super/part_or_public_part/first_hand_or_long_fact.py after last work"

---
+++ super/next_fact/see_long_part_after_bad_person/company/day/part_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def look_woman_in_thing(str_arg):
+    high_world_or_good_day(str_arg)
+    print('government_and_world')
+
+def high_world_or_good_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    look_woman_in_thing('woman')
