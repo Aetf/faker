fdfe3d8c1c85d59f9228940a4f40e4b0903eb4d0
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 11:29:42 -0500

    "mv: get super/able_world_or_own_point/high_way_and_public_person/seem_good_eye_from_world/year/time_and_world/try_time.py, super/big_thing_and_group.py or super/high_day/get_week_under_man/great_week/able_company.py about long point"

---
+++ super/able_world_or_own_point/high_way_and_public_person/seem_good_eye_from_world/year/time_and_world/try_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_small_week_up_young_government(str_arg):
+    eye(str_arg)
+    print('good_person')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_small_week_up_young_government('part')
---
+++ super/big_thing_and_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    new_place(str_arg)
+    print('own_number')
+
+def new_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('thing_and_time')
