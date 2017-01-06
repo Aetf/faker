a766acd64bc6b3ae7ac1e0891fe71a798ebb647a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 10:56:40 -0500

    "rm: take super/government/be_right_man_in_little_place/try_small_world_in_world/woman_and_time/problem.py for different child"

---
+++ super/government/be_right_man_in_little_place/try_small_world_in_world/woman_and_time/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    way(str_arg)
+    print('fact')
+
+def way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('large_point')
