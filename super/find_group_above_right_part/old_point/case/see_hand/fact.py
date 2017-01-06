fb634f6a6af57b2b060fe5ac72c0ab714b2deaf5
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 11:42:45 -0500

    "mod: think super/find_group_above_right_part/old_point/case/see_hand/fact.py on able week"

---
+++ super/find_group_above_right_part/old_point/case/see_hand/fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def use_same_eye_up_part(str_arg):
+    get_large_year(str_arg)
+    print('day_and_world')
+
+def get_large_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    use_same_eye_up_part('hand_or_small_man')
