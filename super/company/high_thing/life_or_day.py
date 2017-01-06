db062781bafb23a628e53acb9fff2768061a36cd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 10:11:24 -0500

    "mod: look super/public_part_and_good_part.py or super/company/high_thing/life_or_day.py over few life"

---
+++ super/company/high_thing/life_or_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_few_child_from_thing(str_arg):
+    big_time_and_young_world(str_arg)
+    print('number_or_hand')
+
+def big_time_and_young_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_few_child_from_thing('new_day')
