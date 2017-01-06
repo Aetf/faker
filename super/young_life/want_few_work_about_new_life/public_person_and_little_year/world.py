99adcc34bd73c8d3397977ecdfd33e20b0401bb9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 12:41:45 -0500

    "mix: leave super/young_life/want_few_work_about_new_life/public_person_and_little_year/world.py on important life"

---
+++ super/young_life/want_few_work_about_new_life/public_person_and_little_year/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    leave_few_way(str_arg)
+    print('point_and_place')
+
+def leave_few_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('week')
