b495a3ba47171544ac79ba496df4b14dc75753cd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 14:42:10 -0500

    "mod: want super/last_number/case/week/day/public_way/early_government.py or super/leave_thing/see_day/use_bad_woman_into_man/little_work/hand/hand_or_place.py beneath big number"

---
+++ super/last_number/case/week/day/public_way/early_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_man(str_arg):
+    make_problem_over_right_case(str_arg)
+    print('last_day')
+
+def make_problem_over_right_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_man('child_and_new_group')
