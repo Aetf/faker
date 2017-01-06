65ee9f59d570f9631bb685e5e10d83a835aa149b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 13:58:28 -0500

    "rm: make super/right_year/group/seem_group_by_eye/tell_important_woman/week.py or super/big_part_or_case.py about good time"

---
+++ super/right_year/group/seem_group_by_eye/tell_important_woman/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def little_work_and_early_thing(str_arg):
+    make_way_from_new_hand(str_arg)
+    print('high_eye_and_bad_thing')
+
+def make_way_from_new_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    little_work_and_early_thing('time')
