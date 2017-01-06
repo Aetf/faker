1865af8001b6fe78a35bedeb211a3c378b3803e7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 14:04:21 -0500

    "mod: call super/number/come_bad_life/world/look_person.py, super/important_work_and_point.py, super/think_company/feel_way/call_fact/old_eye/company.py and super/eye/person_and_thing.py to high group"

---
+++ super/number/come_bad_life/world/look_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way(str_arg):
+    old_way(str_arg)
+    print('seem_important_man_to_large_child')
+
+def old_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way('group_or_young_eye')
---
+++ super/think_company/feel_way/call_fact/old_eye/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_way(str_arg):
+    eye(str_arg)
+    print('say_life')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_way('first_life')
