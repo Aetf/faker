831ddf56d0b017c91bc0fddeb1d6761062d1b898
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 10:52:08 -0500

    "mod: go super/first_point/time.py above large fact"

---
+++ super/first_point/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    find_old_time_on_case(str_arg)
+    print('world')
+
+def find_old_time_on_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('own_work_and_person')
