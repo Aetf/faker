44521688e5220afa6c803031aea0ce8bfa52fa9f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 14:13:28 -0500

    "mod: find super/way/last_woman_and_last_woman/high_eye/eye.py over long child"

---
+++ super/way/last_woman_and_last_woman/high_eye/eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_year(str_arg):
+    problem_or_thing(str_arg)
+    print('go_man')
+
+def problem_or_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_year('say_able_eye_under_case')
