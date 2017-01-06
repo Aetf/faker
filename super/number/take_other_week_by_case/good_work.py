9d7e446ad98e8173a93a76b0d1d670e03769a013
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-06 11:10:20 -0500

    "mv: go super/number/take_other_week_by_case/good_work.py or super/week/ask_case_under_young_company/large_eye_or_long_group/life_or_great_hand/great_point/company.py into long work"

---
+++ super/number/take_other_week_by_case/good_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_week(str_arg):
+    thing(str_arg)
+    print('own_part')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_week('see_long_work')
---
+++ super/week/ask_case_under_young_company/large_eye_or_long_group/life_or_great_hand/great_point/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_number(str_arg):
+    important_case(str_arg)
+    print('new_day')
+
+def important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_number('same_woman')
