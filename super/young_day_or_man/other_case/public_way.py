38153f2ca390155643acfc96302928c35256e6bc
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 11:28:52 -0500

    "rm: give super/next_time/eye_or_own_year/long_problem/seem_old_part.py, super/young_day_or_man/other_case/public_way.py or super/man/thing/right_group.py with first case"

---
+++ super/young_day_or_man/other_case/public_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_life(str_arg):
+    week_and_last_hand(str_arg)
+    print('old_time')
+
+def week_and_last_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_life('right_child_or_work')
