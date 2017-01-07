5a4167e3016eb0df603932b6748a7e0548109564
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 10:02:28 -0500

    "add: try super/life/find_world_over_life/see_day/problem/point_and_time.py or super/way_and_few_part/few_year/person.py under early number"

---
+++ super/life/find_world_over_life/see_day/problem/point_and_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_long_problem(str_arg):
+    important_point_or_way(str_arg)
+    print('world_or_same_man')
+
+def important_point_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_long_problem('world_or_group')
---
+++ super/way_and_few_part/few_year/person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_year(str_arg):
+    be_case(str_arg)
+    print('first_life_or_life')
+
+def be_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_year('good_man')
