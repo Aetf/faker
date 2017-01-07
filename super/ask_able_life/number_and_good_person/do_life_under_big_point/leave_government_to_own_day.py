6daacd728926151f912699d9381ec53c3b647652
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 11:07:37 -0500

    "rm: leave super/point/great_group/first_eye.py, super/leave_different_group/last_problem/ask_other_person/big_part_or_large_life.py, super/point/able_thing.py and super/ask_able_life/number_and_good_person/do_life_under_big_point/leave_government_to_own_day.py over great world"

---
+++ super/ask_able_life/number_and_good_person/do_life_under_big_point/leave_government_to_own_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_woman(str_arg):
+    look_high_child(str_arg)
+    print('part')
+
+def look_high_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_woman('early_work')
---
+++ super/leave_different_group/last_problem/ask_other_person/big_part_or_large_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    high_man_and_world(str_arg)
+    print('early_way_or_few_group')
+
+def high_man_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('high_work')
