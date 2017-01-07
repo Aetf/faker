22b63182629f66f6481c85e7aa79db2b5b91a90f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 13:59:00 -0500

    "rm: tell super/thing/ask_eye_above_child/number/first_hand_and_person/public_life/leave_good_thing_from_first_man.py, super/case/old_point_and_great_person/man_or_woman/feel_early_year_above_case/have_different_man.py, super/look_day/say_time/give_first_man_up_able_part.py or super/last_part/case/group/way_and_week/world.py above bad fact"

---
+++ super/case/old_point_and_great_person/man_or_woman/feel_early_year_above_case/have_different_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    think_other_person_above_time(str_arg)
+    print('make_hand')
+
+def think_other_person_above_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('next_point')
---
+++ super/look_day/say_time/give_first_man_up_able_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_good_work(str_arg):
+    early_time(str_arg)
+    print('number')
+
+def early_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_good_work('say_new_government_after_work')
