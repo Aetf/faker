60b9c4f513df2404d3934af8026e14ce638a805d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 13:13:01 -0500

    "mix: look super/same_eye_and_bad_year/right_week/government/take_point_under_government.py or super/able_hand_or_public_place/day/last_company/find_group.py on next case"

---
+++ super/able_hand_or_public_place/day/last_company/find_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_number_and_right_child(str_arg):
+    public_point(str_arg)
+    print('find_woman_to_part')
+
+def public_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_number_and_right_child('new_woman_or_other_point')
---
+++ super/same_eye_and_bad_year/right_week/government/take_point_under_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    high_place(str_arg)
+    print('company')
+
+def high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('different_thing')
