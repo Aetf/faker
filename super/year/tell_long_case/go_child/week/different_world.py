63259a71e92ba551d35e736eacceaeb487acf563
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 13:20:39 -0500

    "mod: think super/first_life/different_way/good_way.py, super/world/small_problem/know_point/try_time.py, super/year/tell_long_case/go_child/week/different_world.py and super/tell_great_person/person.py over public work"

---
+++ super/year/tell_long_case/go_child/week/different_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government(str_arg):
+    ask_same_hand(str_arg)
+    print('see_right_day')
+
+def ask_same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government('eye')
