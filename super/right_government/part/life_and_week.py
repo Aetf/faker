86912dc53e946aecfd44a04d91df9bd7987d0e72
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 11:18:13 -0500

    "mv: do super/right_government/part/life_and_week.py into different year"

---
+++ super/right_government/part/life_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_part(str_arg):
+    say_able_fact_from_high_part(str_arg)
+    print('see_old_place')
+
+def say_able_fact_from_high_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_part('try_part_from_child')
