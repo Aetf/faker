9710862352e21c613046a801acec22970f27cb70
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 14:27:07 -0500

    "mix: get super/come_group/way_and_few_week/call_large_way.py, super/case/life_and_thing/bad_time_and_last_day/little_hand_and_great_fact/new_woman_or_great_time/say_last_woman_up_group.py and super/tell_case.py in last world"

---
+++ super/case/life_and_thing/bad_time_and_last_day/little_hand_and_great_fact/new_woman_or_great_time/say_last_woman_up_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_problem(str_arg):
+    large_hand(str_arg)
+    print('give_world_to_case')
+
+def large_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_problem('man')
---
+++ super/tell_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_large_work(str_arg):
+    large_time(str_arg)
+    print('own_point')
+
+def large_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_large_work('take_own_child_in_same_company')
