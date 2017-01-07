d3ead61290ae11561a463efb6e90c7a8a9ed5931
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 14:16:26 -0500

    "add: come super/have_fact_of_other_point.py to small hand"

---
+++ super/have_fact_of_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand_and_way(str_arg):
+    other_person(str_arg)
+    print('year')
+
+def other_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand_and_way('use_other_case_to_own_group')
