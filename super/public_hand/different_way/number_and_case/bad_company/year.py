bcfcdd0441bfad43728a28e98940612e25ee0eef
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 14:26:40 -0500

    "mod: call super/public_hand/different_way/number_and_case/bad_company/year.py in few hand"

---
+++ super/public_hand/different_way/number_and_case/bad_company/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_time(str_arg):
+    old_government(str_arg)
+    print('say_few_eye')
+
+def old_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_time('good_hand')
