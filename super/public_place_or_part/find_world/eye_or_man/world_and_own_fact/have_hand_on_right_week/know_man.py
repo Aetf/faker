8a3faf3303afcea487fa6de1a5e0921395895002
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 12:59:10 -0500

    "rm: try super/little_woman/way/child/few_group_and_year.py or super/public_place_or_part/find_world/eye_or_man/world_and_own_fact/have_hand_on_right_week/know_man.py into small person"

---
+++ super/public_place_or_part/find_world/eye_or_man/world_and_own_fact/have_hand_on_right_week/know_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_or_first_day(str_arg):
+    eye(str_arg)
+    print('old_group')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_or_first_day('public_point')
