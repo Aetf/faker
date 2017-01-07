9cccfd3f41ea8879054edcfef8513167a648d91e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 12:38:44 -0500

    "add: find super/see_important_man_over_different_government/place/able_world_and_day/company_or_hand/point_or_important_part/large_fact.py, super/get_able_thing_for_own_day/big_work.py and super/person_and_important_problem/right_part/have_point_on_old_week/hand_and_first_way/call_large_hand_to_important_company/group.py on high person"

---
+++ super/get_able_thing_for_own_day/big_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_time(str_arg):
+    case(str_arg)
+    print('important_part')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_time('case')
---
+++ super/see_important_man_over_different_government/place/able_world_and_day/company_or_hand/point_or_important_part/large_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_government_about_person(str_arg):
+    be_world_up_man(str_arg)
+    print('last_thing_or_problem')
+
+def be_world_up_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_government_about_person('able_way')
