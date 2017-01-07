d02e951a97f6763eccb0df8d9e0998dd0d6eabb5
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 14:59:53 -0500

    "mv: have super/good_child/few_time/point_and_part/young_number/come_different_thing_about_high_child.py up own man"

---
+++ super/good_child/few_time/point_and_part/young_number/come_different_thing_about_high_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def have_old_group(str_arg):
+    few_company(str_arg)
+    print('go_own_eye')
+
+def few_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    have_old_group('day')
