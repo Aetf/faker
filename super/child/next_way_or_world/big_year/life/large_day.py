a5ab57d647db41facbc9f01d4c9fedd224c72df3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 12:16:08 -0500

    "mod: work super/child/next_way_or_world/big_year/life/large_day.py, super/big_hand/work/work_child_to_year/next_life_or_great_eye.py or super/own_company/group_or_person/company/make_next_place_at_large_company.py of public person"

---
+++ super/child/next_way_or_world/big_year/life/large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_time_and_small_company(str_arg):
+    old_case(str_arg)
+    print('part')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_time_and_small_company('first_part_and_man')
---
+++ super/own_company/group_or_person/company/make_next_place_at_large_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_group(str_arg):
+    world(str_arg)
+    print('hand')
+
+def world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_group('problem_or_point')
