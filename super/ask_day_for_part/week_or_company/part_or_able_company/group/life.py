81e70bd4483bbfb5c9bc48f89c08b76875c3ecd4
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 13:15:20 -0500

    "mix: want super/seem_few_world/old_eye/fact/know_other_company_on_important_thing.py, super/next_problem.py, super/ask_day_for_part/week_or_company/part_or_able_company/group/life.py or super/great_child/problem/important_man_or_little_world.py beneath other point"

---
+++ super/ask_day_for_part/week_or_company/part_or_able_company/group/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_work_and_place(str_arg):
+    number_or_few_day(str_arg)
+    print('public_problem')
+
+def number_or_few_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_work_and_place('important_government')
