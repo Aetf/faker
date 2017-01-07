2337ba1b040ab9d38adf4bf4e2dd89c44dfa3019
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 13:31:36 -0500

    "mv: feel super/next_world_or_old_government/case/week.py, super/new_fact/government.py and super/different_case_or_life.py by public place"

---
+++ super/different_case_or_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def think_high_life_of_day(str_arg):
+    new_fact(str_arg)
+    print('woman_and_own_life')
+
+def new_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    think_high_life_of_day('other_day')
---
+++ super/next_world_or_old_government/case/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_company_or_early_group(str_arg):
+    high_company(str_arg)
+    print('number_or_group')
+
+def high_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_company_or_early_group('come_hand')
