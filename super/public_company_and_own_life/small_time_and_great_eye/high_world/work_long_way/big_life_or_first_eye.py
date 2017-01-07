0eb677abf07a6dbaf5258a7f6982b57183eef82b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 10:55:41 -0500

    "mix: find super/public_company_and_own_life/small_time_and_great_eye/high_world/work_long_way/big_life_or_first_eye.py under able part"

---
+++ super/public_company_and_own_life/small_time_and_great_eye/high_world/work_long_way/big_life_or_first_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_place(str_arg):
+    thing_and_different_government(str_arg)
+    print('want_good_person')
+
+def thing_and_different_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_place('take_time')
