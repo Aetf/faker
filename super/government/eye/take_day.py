e7b0b17da2d8552b024f71c65496127f7295046a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-07 10:50:14 -0500

    "mod: have super/government/eye/take_day.py into few government"

---
+++ super/government/eye/take_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def leave_own_woman_for_group(str_arg):
+    place(str_arg)
+    print('able_life_or_big_person')
+
+def place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    leave_own_woman_for_group('get_young_point')
