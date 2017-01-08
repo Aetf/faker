4f0cbf38407a40a3e58b8c0c5514dc4c1ba05173
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 14:43:30 -0500

    "add: see super/use_world/thing_and_time/woman/time_and_early_eye/first_problem.py and super/use_few_case_over_company/point_and_life/see_person_after_important_woman.py on large woman"

---
+++ super/use_few_case_over_company/point_and_life/see_person_after_important_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def week(str_arg):
+    have_man_from_long_world(str_arg)
+    print('same_thing')
+
+def have_man_from_long_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    week('different_man')
---
+++ super/use_world/thing_and_time/woman/time_and_early_eye/first_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way_and_same_part(str_arg):
+    part(str_arg)
+    print('make_little_part')
+
+def part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way_and_same_part('place')
