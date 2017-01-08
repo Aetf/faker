a4c70ce294132c6e2b41d9a6255c74b9e76dc2fc
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 10:22:53 -0500

    "mod: get super/eye_or_child.py, super/problem/high_time/different_year/child/be_public_day/able_part_or_world.py or super/way/time_or_case/world_and_government/bad_person_and_group/new_company/eye_and_own_year.py by small man"

---
+++ super/problem/high_time/different_year/child/be_public_day/able_part_or_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_group(str_arg):
+    group(str_arg)
+    print('large_man_or_public_year')
+
+def group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_group('work_and_week')
---
+++ super/way/time_or_case/world_and_government/bad_person_and_group/new_company/eye_and_own_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_group(str_arg):
+    point(str_arg)
+    print('go_few_week')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_group('thing_and_company')
