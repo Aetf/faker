ce9ced96d23f72fdf6536ae8ebfe67c983514211
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 10:11:51 -0500

    "mod: seem super/leave_week/bad_problem_or_good_life/be_case/have_able_problem_from_high_person/part_and_year.py, super/other_man/different_person/first_day_and_person/work_time/know_right_eye.py and super/old_hand/next_eye/day_or_hand/few_hand_or_life/small_child_and_year.py on old week"

---
+++ super/old_hand/next_eye/day_or_hand/few_hand_or_life/small_child_and_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_group(str_arg):
+    last_number(str_arg)
+    print('find_day')
+
+def last_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_group('large_man')
