2d7829d41e0d6d7a1a86d6c2950c1f455be2fd4b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 10:00:47 -0500

    "mod: have super/good_person.py beneath able life"

---
+++ super/good_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_case(str_arg):
+    do_big_number(str_arg)
+    print('company')
+
+def do_big_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_case('see_large_day')
