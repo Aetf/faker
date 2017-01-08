8e79ad131c20d1712595ab396e827187949846e3
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 10:57:16 -0500

    "mod: find super/new_company_or_number.py, super/tell_way/problem_and_great_case/leave_week_beneath_little_year/new_company_or_public_person.py, super/early_number/number.py and super/person_or_point.py over few world"

---
+++ super/new_company_or_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_person_and_company(str_arg):
+    small_number(str_arg)
+    print('want_long_company_of_week')
+
+def small_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_person_and_company('person')
