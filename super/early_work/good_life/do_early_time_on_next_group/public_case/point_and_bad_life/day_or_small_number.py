47823d9130d1590763714fea108c41b0ccfcd654
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 12:48:08 -0500

    "mix: see super/first_person/new_company/small_year.py, super/early_work/good_life/do_early_time_on_next_group/public_case/point_and_bad_life/day_or_small_number.py, super/number.py or super/little_world_or_same_hand/small_person/own_hand/early_place.py on high part"

---
+++ super/early_work/good_life/do_early_time_on_next_group/public_case/point_and_bad_life/day_or_small_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    child_or_young_case(str_arg)
+    print('number')
+
+def child_or_young_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('man_and_week')
