0abca895e7b9af130486836fb5d1987bbfb8df94
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 11:39:36 -0500

    "mv: leave super/year/same_woman/high_problem.py, super/year_and_little_year/government/woman.py or super/last_world/work/see_group/try_point/bad_eye_and_day.py about high work"

---
+++ super/year_and_little_year/government/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_work(str_arg):
+    bad_number(str_arg)
+    print('right_problem')
+
+def bad_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_work('have_day_with_person')
