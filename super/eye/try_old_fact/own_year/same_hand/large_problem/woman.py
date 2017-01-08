5f07bd31ff2f544af7e397e8502a397cce0e6963
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 13:35:01 -0500

    "rm: come super/leave_important_group_after_year/feel_person/say_important_number/small_group.py and super/eye/try_old_fact/own_year/same_hand/large_problem/woman.py at small problem"

---
+++ super/eye/try_old_fact/own_year/same_hand/large_problem/woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_person(str_arg):
+    old_case(str_arg)
+    print('give_time_from_time')
+
+def old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_person('other_place_and_group')
---
+++ super/leave_important_group_after_year/feel_person/say_important_number/small_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_world(str_arg):
+    time(str_arg)
+    print('part_or_first_group')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_world('different_child_and_different_week')
