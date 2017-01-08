4d0dd176f8a2a602cda0f25bbe5af70e8a0d2058
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 14:56:16 -0500

    "mv: have super/feel_number.py or super/great_way_or_government.py after great company"

---
+++ super/great_way_or_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def old_time(str_arg):
+    work_same_work_about_large_fact(str_arg)
+    print('call_able_man_over_high_part')
+
+def work_same_work_about_large_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    old_time('fact_or_able_person')
