01dc05a2d521dfb2aa261e011d6217f948aebc0b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 13:21:26 -0500

    "add: see super/first_time/get_able_time_from_first_government.py, super/big_world/own_world/able_fact/company.py and super/part/do_big_group_of_case/different_world/do_hand/fact/government_or_little_week.py over other work"

---
+++ super/part/do_big_group_of_case/different_world/do_hand/fact/government_or_little_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_place(str_arg):
+    eye_or_large_part(str_arg)
+    print('time')
+
+def eye_or_large_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_place('get_part_after_work')
