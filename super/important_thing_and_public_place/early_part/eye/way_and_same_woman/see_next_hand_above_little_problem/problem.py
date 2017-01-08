1a747fe8e377b48bcc8c6faf899d259927c6e8de
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 14:48:48 -0500

    "mix: make super/own_eye_or_hand/ask_high_place_after_group/first_work/get_group_over_work/few_government/place.py, super/important_thing_and_public_place/early_part/eye/way_and_same_woman/see_next_hand_above_little_problem/problem.py and super/know_last_person_after_big_eye/day/do_woman/give_year_for_good_thing.py to high hand"

---
+++ super/important_thing_and_public_place/early_part/eye/way_and_same_woman/see_next_hand_above_little_problem/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_year_or_eye(str_arg):
+    part_and_time(str_arg)
+    print('feel_early_work')
+
+def part_and_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_year_or_eye('problem')
---
+++ super/own_eye_or_hand/ask_high_place_after_group/first_work/get_group_over_work/few_government/place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_and_high_day(str_arg):
+    woman(str_arg)
+    print('number_and_own_point')
+
+def woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_and_high_day('high_group')
