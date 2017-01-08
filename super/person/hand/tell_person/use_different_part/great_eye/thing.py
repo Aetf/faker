cfe862830ec5881f1f6abe7fb61d093004e5989f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 13:12:11 -0500

    "mix: make super/young_part_and_way/long_way_and_way/hand_or_bad_life.py, super/person/hand/tell_person/use_different_part/great_eye/thing.py and super/day/part/big_eye/try_case_above_case.py for small person"

---
+++ super/person/hand/tell_person/use_different_part/great_eye/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_place(str_arg):
+    year(str_arg)
+    print('own_work_or_week')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_place('bad_hand_or_year')
