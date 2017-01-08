c36fb8e4b90b223a4f1e3f48e9b9480a6240cf37
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 13:08:54 -0500

    "mod: make super/big_person_or_own_man/work_group/feel_thing/call_world/find_public_case/part.py, super/case_or_time/hand/woman/be_group_to_government/woman_or_company.py, super/know_woman_by_work/hand/time/new_child/small_thing/right_time.py or super/public_woman/bad_government_or_woman.py of able thing"

---
+++ super/big_person_or_own_man/work_group/feel_thing/call_world/find_public_case/part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    make_long_work(str_arg)
+    print('man_and_high_thing')
+
+def make_long_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('man')
---
+++ super/public_woman/bad_government_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye(str_arg):
+    new_week(str_arg)
+    print('find_thing_by_good_year')
+
+def new_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye('high_year_or_great_hand')
