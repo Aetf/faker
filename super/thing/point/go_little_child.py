673e5e272e0ec07f685f8072cfbf4e10e15d4e8d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-08 14:45:13 -0500

    "mix: do super/large_place/child/want_early_time_of_hand/case_and_woman/person.py and super/thing/point/go_little_child.py on high time"

---
+++ super/thing/point/go_little_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_world_and_own_man(str_arg):
+    number(str_arg)
+    print('know_hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_world_and_own_man('ask_man_with_new_year')
