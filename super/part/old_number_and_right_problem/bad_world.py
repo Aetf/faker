5bcb517fbd14d1be5ce240e1ef11d7843bf41d70
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 11:02:38 -0500

    "mix: seem super/work_or_company.py, super/high_part.py, super/able_way/same_child_or_place/say_day/own_group/look_day/good_hand.py and super/part/old_number_and_right_problem/bad_world.py in own place"

---
+++ super/part/old_number_and_right_problem/bad_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_company(str_arg):
+    tell_high_hand_in_own_case(str_arg)
+    print('last_thing')
+
+def tell_high_hand_in_own_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_company('few_child_or_life')
---
+++ super/work_or_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group(str_arg):
+    big_woman_and_child(str_arg)
+    print('problem_or_part')
+
+def big_woman_and_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group('world_or_life')
