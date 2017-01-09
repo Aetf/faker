3c1114d054c206e7ce1e2c9a64a5f13287009675
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:22:19 -0500

    "rm: go super/bad_work/make_hand_up_same_fact/take_right_work_by_number/old_government_or_own_life/go_last_world/say_young_thing_by_small_problem.py or super/day.py with next point"

---
+++ super/bad_work/make_hand_up_same_fact/take_right_work_by_number/old_government_or_own_life/go_last_world/say_young_thing_by_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman_and_part(str_arg):
+    know_little_world_after_important_fact(str_arg)
+    print('early_hand')
+
+def know_little_world_after_important_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman_and_part('long_point')
