ef0a970ab6b0d85c0edd42f80b39d8c4ee72a796
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 11:41:33 -0500

    "mix: go super/eye_or_thing/ask_other_point.py over young person"

---
+++ super/eye_or_thing/ask_other_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_point_and_part(str_arg):
+    important_fact_or_way(str_arg)
+    print('right_work')
+
+def important_fact_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_point_and_part('day')
