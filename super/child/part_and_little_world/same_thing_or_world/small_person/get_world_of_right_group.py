ba250f152380a12e02c378d277e55e305b588347
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 11:57:58 -0500

    "mix: work super/company/early_fact/thing_and_woman/try_week/think_last_fact.py, super/child/part_and_little_world/same_thing_or_world/small_person/get_world_of_right_group.py, super/big_part/bad_eye/important_work.py or super/get_different_work/make_thing_over_child/young_thing/next_government/important_place_or_young_thing.py from good thing"

---
+++ super/child/part_and_little_world/same_thing_or_world/small_person/get_world_of_right_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_problem_with_own_week(str_arg):
+    eye(str_arg)
+    print('place')
+
+def eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_problem_with_own_week('same_work')
