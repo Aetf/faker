ce817fe5c8c0fa99305eccb4eaccddd0171b492f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 13:23:57 -0500

    "mix: see super/week_or_right_life/group_or_eye/life/large_eye.py from large child"

---
+++ super/week_or_right_life/group_or_eye/life/large_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def new_way(str_arg):
+    way_or_day(str_arg)
+    print('go_way')
+
+def way_or_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    new_way('go_large_case_with_young_life')
