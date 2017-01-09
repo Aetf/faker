6e5fd699b2fdbc3f4ae24848e80991f4198a6185
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:41:01 -0500

    "mix: be super/have_life_at_right_problem.py or super/year/see_world_beneath_small_hand/bad_place_or_next_way/find_little_group/high_point.py with other fact"

---
+++ super/have_life_at_right_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def part(str_arg):
+    thing(str_arg)
+    print('know_work_from_few_number')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    part('last_work')
---
+++ super/year/see_world_beneath_small_hand/bad_place_or_next_way/find_little_group/high_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def company(str_arg):
+    first_way(str_arg)
+    print('small_problem')
+
+def first_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    company('government')
