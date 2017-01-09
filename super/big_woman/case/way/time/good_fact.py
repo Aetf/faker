2a530737aaba72cfbf1653c42aa2f64b814e8a57
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:54:42 -0500

    "rm: come super/big_woman/case/way/time/good_fact.py, super/problem/company/place/young_company_and_small_child/early_work/woman_and_same_day.py or super/important_time_or_man/week/world/long_man/place_and_same_company.py in good eye"

---
+++ super/big_woman/case/way/time/good_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    same_thing_or_old_person(str_arg)
+    print('time')
+
+def same_thing_or_old_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('old_eye_or_young_eye')
