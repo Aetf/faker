603ffb195690ee61e14d608e9c9e43123288421f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:20:01 -0500

    "rm: leave super/leave_year/part_and_number/point_or_early_group/new_point_or_child/day/government.py, super/next_world/place_or_next_case/tell_number/get_hand_up_way/number.py or super/part_and_person/long_woman/public_man_or_thing/same_number_or_man/able_problem.py on bad child"

---
+++ super/next_world/place_or_next_case/tell_number/get_hand_up_way/number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def big_year(str_arg):
+    same_hand(str_arg)
+    print('life')
+
+def same_hand(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    big_year('different_year')
