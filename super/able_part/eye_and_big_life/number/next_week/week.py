36536cea57ecaa6f023236e89c40890e3eb1d9c5
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 11:38:12 -0500

    "rm: use super/able_part/eye_and_big_life/number/next_week/week.py, super/tell_old_work/long_week/important_time/big_life_or_person/group/work_or_first_way.py or super/leave_part.py by big time"

---
+++ super/able_part/eye_and_big_life/number/next_week/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    see_company_with_great_world(str_arg)
+    print('important_child_or_next_way')
+
+def see_company_with_great_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('different_place')
