09cf5eec389aea4222de585509703755a0d291c1
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:33:32 -0500

    "add: think super/bad_thing.py and super/old_child/group/first_fact/want_work.py into first number"

---
+++ super/bad_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_good_hand_beneath_point(str_arg):
+    world_and_large_child(str_arg)
+    print('find_eye')
+
+def world_and_large_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_good_hand_beneath_point('part')
