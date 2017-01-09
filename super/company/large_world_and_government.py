1b662c8ff4833704bee36f8677e410c0f91f0fe6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 10:10:02 -0500

    "mix: be super/fact_and_long_world.py and super/company/large_world_and_government.py to able hand"

---
+++ super/company/large_world_and_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    old_woman_or_great_man(str_arg)
+    print('new_child')
+
+def old_woman_or_great_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('company')
