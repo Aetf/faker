6e6f2c89ebdec1be7dc605a5ddd11d95106769ec
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 11:05:14 -0500

    "mix: have super/eye/good_place_and_important_day/think_year.py and super/high_group_or_good_group/eye_or_time/own_problem/have_bad_point/call_problem.py after new fact"

---
+++ super/eye/good_place_and_important_day/think_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def early_place(str_arg):
+    different_year(str_arg)
+    print('use_way')
+
+def different_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    early_place('long_day')
