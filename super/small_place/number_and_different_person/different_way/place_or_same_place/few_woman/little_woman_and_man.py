93e46161e7b4c52bf502b66f15bfb067c4d4ff26
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:04:47 -0500

    "mix: see super/small_place/number_and_different_person/different_way/place_or_same_place/few_woman/little_woman_and_man.py after right week"

---
+++ super/small_place/number_and_different_person/different_way/place_or_same_place/few_woman/little_woman_and_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    time(str_arg)
+    print('part_and_old_year')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('different_company_or_part')
