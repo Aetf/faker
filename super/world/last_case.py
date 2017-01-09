0887fd548cf32488130412584d06b9e0393cf3fd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 13:03:30 -0500

    "add: think super/own_thing/week_or_high_woman/have_first_day_beneath_important_man.py, super/great_part/be_place/new_case/work_or_part/great_eye_and_case/number.py or super/world/last_case.py at first number"

---
+++ super/world/last_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    thing(str_arg)
+    print('young_place')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('work_new_fact_under_own_thing')
