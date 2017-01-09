5e09ef21d67fc70802e2497ed68cc60fe0ffb988
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-09 12:16:11 -0500

    "add: know super/last_way/give_number_at_group/young_woman/number/company.py, super/long_problem_and_week.py or super/thing/work/tell_problem_for_child/own_work/big_year.py at young fact"

---
+++ super/last_way/give_number_at_group/young_woman/number/company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def good_time(str_arg):
+    good_work_and_last_fact(str_arg)
+    print('new_woman_or_point')
+
+def good_work_and_last_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    good_time('work_large_week_from_big_world')
