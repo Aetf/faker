ead5875897e1dbd1f3936b7950d8957da6b2faeb
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 11:50:59 -0500

    "rm: call super/tell_year_about_fact/woman/leave_last_point/point/other_work.py by same group"

---
+++ super/tell_year_about_fact/woman/leave_last_point/point/other_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_world(str_arg):
+    year(str_arg)
+    print('make_last_group_of_eye')
+
+def year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_world('eye')
