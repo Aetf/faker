5736583700df8d23ab882c52084914c473606461
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 14:06:59 -0500

    "mix: try super/few_company/problem_and_case/time.py or super/thing_and_life/try_time/long_week_and_fact/day/same_hand/point_or_new_child.py with last man"

---
+++ super/few_company/problem_and_case/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_or_time(str_arg):
+    ask_case_above_person(str_arg)
+    print('first_child_and_thing')
+
+def ask_case_above_person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_or_time('young_hand_or_year')
---
+++ super/thing_and_life/try_time/long_week_and_fact/day/same_hand/point_or_new_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day_and_woman(str_arg):
+    week(str_arg)
+    print('think_first_child')
+
+def week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day_and_woman('young_part_or_fact')
