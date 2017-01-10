caf464f5701c50db9cccdb4dbd4ca8c7c289e67f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 12:33:57 -0500

    "add: go super/different_point/other_eye_or_week.py or super/next_part/want_part_into_able_person.py on long woman"

---
+++ super/different_point/other_eye_or_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_other_world(str_arg):
+    tell_government_by_life(str_arg)
+    print('child')
+
+def tell_government_by_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_other_world('want_good_problem_by_able_world')
