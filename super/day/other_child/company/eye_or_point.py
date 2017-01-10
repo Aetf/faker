2a062170602f6f2b70e959462c5dc0d9022e817a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 13:11:28 -0500

    "rm: seem super/next_number_or_high_work/try_week/different_thing_or_government/different_day/eye_or_great_year.py and super/day/other_child/company/eye_or_point.py under old number"

---
+++ super/day/other_child/company/eye_or_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    bad_problem(str_arg)
+    print('bad_time')
+
+def bad_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('part')
