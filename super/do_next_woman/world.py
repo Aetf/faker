ee85c36cedd1c651d90f160bc17503be32e7c150
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 14:29:31 -0500

    "mod: know super/do_next_woman/world.py and super/large_year_or_own_time/go_hand/come_bad_year_to_way/problem/think_number.py to large case"

---
+++ super/do_next_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_and_small_fact(str_arg):
+    think_same_year_to_part(str_arg)
+    print('company')
+
+def think_same_year_to_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_and_small_fact('case_and_old_year')
