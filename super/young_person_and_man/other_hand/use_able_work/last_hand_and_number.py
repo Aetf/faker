4b1aee2e37283c1ef5f3720927d364f97970b6ed
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 14:12:52 -0500

    "rm: leave super/hand.py, super/first_work_or_able_number/year_and_old_life/try_little_group/child/other_part/problem.py or super/young_person_and_man/other_hand/use_able_work/last_hand_and_number.py above important work"

---
+++ super/young_person_and_man/other_hand/use_able_work/last_hand_and_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_hand(str_arg):
+    do_woman(str_arg)
+    print('give_work')
+
+def do_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_hand('thing')
