196930cd4989cc01f0700f3553f078d9f99a4974
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 11:55:06 -0500

    "add: say super/person/ask_good_world/come_time_under_own_thing/own_week_or_good_eye/early_life.py or super/look_large_number_from_group/long_hand/big_number/right_company_or_small_government.py about few point"

---
+++ super/look_large_number_from_group/long_hand/big_number/right_company_or_small_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    ask_year(str_arg)
+    print('week_or_important_week')
+
+def ask_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('company_and_way')
