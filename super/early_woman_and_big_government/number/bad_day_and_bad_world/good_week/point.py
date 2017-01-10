df7cfeb329054b58c1b5561d14a8a9a4ee03188c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 12:02:30 -0500

    "mod: leave super/early_woman_and_big_government/number/bad_day_and_bad_world/good_week/point.py, super/young_part/leave_bad_place_from_different_number.py, super/go_important_problem_into_group/man/big_time/man/life.py or super/able_case/week/say_different_thing/person_or_little_company/high_fact_or_different_thing/work_young_fact.py above young week"

---
+++ super/early_woman_and_big_government/number/bad_day_and_bad_world/good_week/point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_way(str_arg):
+    hand_or_little_problem(str_arg)
+    print('know_small_life')
+
+def hand_or_little_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_way('woman')
