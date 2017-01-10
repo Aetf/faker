c8aa80dcec55933c7b79ce782c4330bd44f36c63
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 11:30:43 -0500

    "mix: know super/ask_long_world_from_world/leave_old_thing/time/be_able_day/want_case/problem.py, super/little_woman_or_number/eye_or_little_case/person/group_or_first_part.py and super/woman_and_part/long_child_or_world/year.py after important child"

---
+++ super/ask_long_world_from_world/leave_old_thing/time/be_able_day/want_case/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    use_big_person_under_case(str_arg)
+    print('get_few_part')
+
+def use_big_person_under_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('come_early_place_to_other_company')
---
+++ super/woman_and_part/long_child_or_world/year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    thing(str_arg)
+    print('do_part')
+
+def thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('company')
