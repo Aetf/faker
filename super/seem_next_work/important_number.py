7a49b6aae1c5041b9b8854d30132db177343952c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 11:28:56 -0500

    "mv: say super/ask_company_with_way/company/think_great_point/way/point_and_year.py or super/seem_next_work/important_number.py above young day"

---
+++ super/seem_next_work/important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_and_government(str_arg):
+    public_world_and_other_life(str_arg)
+    print('great_man')
+
+def public_world_and_other_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_and_government('come_early_hand')
