c2883447eb6854baf9dbbe1f92097b012abde4e9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 12:20:54 -0500

    "mod: call super/part/time/woman/company_and_woman.py and super/same_week_or_same_company/company/high_time.py beneath important number"

---
+++ super/part/time/woman/company_and_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_week_and_eye(str_arg):
+    do_able_work(str_arg)
+    print('know_person_after_new_time')
+
+def do_able_work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_week_and_eye('small_child')
---
+++ super/same_week_or_same_company/company/high_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_place_or_group(str_arg):
+    important_problem(str_arg)
+    print('day')
+
+def important_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_place_or_group('few_place_or_child')
