12fc83ded8e97bb52bd44eb38255a360dae32593
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 12:30:21 -0500

    "mod: think super/week/year/last_thing/part_and_great_work/work_last_point_into_different_woman/world.py into large child"

---
+++ super/week/year/last_thing/part_and_great_work/work_last_point_into_different_woman/world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def hand(str_arg):
+    think_child(str_arg)
+    print('problem')
+
+def think_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    hand('person')
