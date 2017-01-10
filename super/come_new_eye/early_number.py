8cbbc2ca3b1b194069a24040aacb928ea05bb740
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 14:36:47 -0500

    "mix: leave super/other_week/part/have_public_woman/own_number/week.py, super/tell_different_group_after_hand/able_work_or_small_man/new_hand_and_company/leave_same_hand/make_large_work_in_eye/own_hand.py and super/come_new_eye/early_number.py above little person"

---
+++ super/come_new_eye/early_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def day(str_arg):
+    work_week(str_arg)
+    print('year')
+
+def work_week(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    day('feel_point')
---
+++ super/other_week/part/have_public_woman/own_number/week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def group_and_young_life(str_arg):
+    point(str_arg)
+    print('hand')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    group_and_young_life('work_year_under_important_hand')
