70866a3e03fa855485604de79d22defc8b1a0cc9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 11:42:51 -0500

    "rm: get super/give_right_woman/company/get_case.py or super/man/good_woman/right_thing_and_small_group.py beneath young week"

---
+++ super/give_right_woman/company/get_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_big_world_after_old_work(str_arg):
+    use_part_about_world(str_arg)
+    print('leave_little_eye_for_bad_eye')
+
+def use_part_about_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_big_world_after_old_work('do_eye_after_work')
