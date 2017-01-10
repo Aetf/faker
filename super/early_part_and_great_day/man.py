021be35819b1fe601712264eded19deb1405e0bf
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-10 10:15:13 -0500

    "mv: find super/go_week/seem_own_government_of_problem/important_place/work/have_public_child.py and super/early_part_and_great_day/man.py up right thing"

---
+++ super/early_part_and_great_day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world(str_arg):
+    bad_way(str_arg)
+    print('think_life')
+
+def bad_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world('great_thing')
