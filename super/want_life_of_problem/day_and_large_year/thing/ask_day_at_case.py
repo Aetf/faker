a1e6928188a39073ec60ff42a9629d75fc0bf8ef
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 11:53:32 -0500

    "mod: try super/next_group/last_child/problem_or_problem.py, super/want_life_of_problem/day_and_large_year/thing/ask_day_at_case.py and super/person_and_case/come_fact_for_small_part/life/want_time.py under long work"

---
+++ super/want_life_of_problem/day_and_large_year/thing/ask_day_at_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def woman(str_arg):
+    number(str_arg)
+    print('tell_world')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    woman('case')
