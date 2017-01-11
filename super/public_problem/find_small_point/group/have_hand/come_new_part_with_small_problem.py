f7ada03ab6059606da4cbdb6fe0c5dbd6af10fec
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 10:25:17 -0500

    "mv: ask super/able_thing_and_day/work_and_way/way_and_large_life.py, super/come_place/great_way/work.py, super/public_problem/find_small_point/group/have_hand/come_new_part_with_small_problem.py and super/part/public_man_or_early_child/few_eye/find_big_eye/early_government/company.py after long week"

---
+++ super/public_problem/find_small_point/group/have_hand/come_new_part_with_small_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def important_government_and_thing(str_arg):
+    different_fact(str_arg)
+    print('do_case_after_high_woman')
+
+def different_fact(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    important_government_and_thing('man')
