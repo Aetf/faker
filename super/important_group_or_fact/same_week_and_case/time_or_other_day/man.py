670755b4ff02dab7094b180670181d3f9fd54acd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 13:12:30 -0500

    "mix: have super/important_group_or_fact/same_week_and_case/time_or_other_day/man.py or super/person/ask_case_about_point.py on last number"

---
+++ super/important_group_or_fact/same_week_and_case/time_or_other_day/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def first_place(str_arg):
+    other_company(str_arg)
+    print('life')
+
+def other_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    first_place('child')
