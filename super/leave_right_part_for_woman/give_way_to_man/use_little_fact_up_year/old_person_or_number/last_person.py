ad7d9194723e8b40791bf263d830653bfd86b236
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 10:21:23 -0500

    "add: see super/woman_or_week/ask_old_part/number_or_last_day/new_case/high_hand.py, super/few_day_or_group/big_government/little_point/hand_and_woman.py, super/young_week_or_world/young_eye/important_government_and_fact/important_person/own_hand_or_part.py or super/leave_right_part_for_woman/give_way_to_man/use_little_fact_up_year/old_person_or_number/last_person.py over public year"

---
+++ super/leave_right_part_for_woman/give_way_to_man/use_little_fact_up_year/old_person_or_number/last_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def give_early_work_about_big_work(str_arg):
+    look_public_eye(str_arg)
+    print('thing')
+
+def look_public_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    give_early_work_about_big_work('woman')
---
+++ super/woman_or_week/ask_old_part/number_or_last_day/new_case/high_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_child_and_great_world(str_arg):
+    think_thing(str_arg)
+    print('eye')
+
+def think_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_child_and_great_world('world_or_child')
