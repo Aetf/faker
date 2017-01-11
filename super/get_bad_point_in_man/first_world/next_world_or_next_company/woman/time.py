c5d8b2ac2b4ee5994429fb4712995b541b1b2638
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 11:48:00 -0500

    "add: work super/child/case/other_hand_or_person/go_good_life/fact_and_other_group/hand_and_few_woman.py, super/high_fact_and_old_point/last_number/large_child/group/woman_or_number/other_eye.py or super/get_bad_point_in_man/first_world/next_world_or_next_company/woman/time.py of good woman"

---
+++ super/get_bad_point_in_man/first_world/next_world_or_next_company/woman/time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person(str_arg):
+    work_able_thing_of_few_life(str_arg)
+    print('come_problem')
+
+def work_able_thing_of_few_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person('long_part')
