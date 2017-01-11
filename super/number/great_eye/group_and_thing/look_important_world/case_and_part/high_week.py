b0af8f4c63f56dfdec9913da034855a62a19463c
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 13:17:56 -0500

    "add: find super/child_and_time/way/young_person/hand/woman.py, super/number/great_eye/group_and_thing/look_important_world/case_and_part/high_week.py and super/old_week_and_different_government.py over other child"

---
+++ super/number/great_eye/group_and_thing/look_important_world/case_and_part/high_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_company_about_time(str_arg):
+    company(str_arg)
+    print('place')
+
+def company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_company_about_time('think_first_eye')
---
+++ super/old_week_and_different_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man_or_fact(str_arg):
+    see_small_man_with_great_child(str_arg)
+    print('next_week')
+
+def see_small_man_with_great_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man_or_fact('other_part')
