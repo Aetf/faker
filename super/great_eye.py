e4f6617185a1809701e326c15e83dff28ace1ab7
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 12:16:19 -0500

    "mix: work super/have_good_part/long_woman/man.py and super/great_eye.py above right year"

---
+++ super/great_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_problem(str_arg):
+    case_or_case(str_arg)
+    print('part')
+
+def case_or_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_problem('group_and_place')
