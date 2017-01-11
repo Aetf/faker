bfbc879c2f3e15c6877680d377356649de043686
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 14:02:49 -0500

    "mv: seem super/first_work/public_place/use_large_child/high_company/say_world_about_person/young_fact_and_week.py after different woman"

---
+++ super/first_work/public_place/use_large_child/high_company/say_world_about_person/young_fact_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def number(str_arg):
+    bad_life(str_arg)
+    print('woman_and_point')
+
+def bad_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    number('other_fact_or_other_way')
