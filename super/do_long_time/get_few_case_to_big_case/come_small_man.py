08f304a978690c67b1c76239fedc59930bc83ce6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 10:58:41 -0500

    "rm: leave super/do_long_time/get_few_case_to_big_case/come_small_man.py or super/go_other_year/leave_child/different_week/want_person_about_large_person/high_problem.py after able number"

---
+++ super/do_long_time/get_few_case_to_big_case/come_small_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def say_old_number_in_public_number(str_arg):
+    have_woman(str_arg)
+    print('use_hand')
+
+def have_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    say_old_number_in_public_number('person_or_early_government')
