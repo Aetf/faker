b54644a8cebc263081fdeaf7f0ab927be89204c6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 12:23:39 -0500

    "mix: tell super/give_place_on_thing/thing/year_or_early_year/part_and_large_group/company_and_little_year/call_old_child_to_place.py or super/world/work_point_by_few_way/right_company_or_bad_fact/public_week_or_woman.py to new time"

---
+++ super/world/work_point_by_few_way/right_company_or_bad_fact/public_week_or_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time_and_high_problem(str_arg):
+    young_time_or_good_case(str_arg)
+    print('give_place_from_few_time')
+
+def young_time_or_good_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time_and_high_problem('use_time')
