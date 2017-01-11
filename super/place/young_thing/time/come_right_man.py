4e738768e086b80bdd90f947b329add2041d7832
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 11:33:06 -0500

    "mix: have super/government_and_small_woman/part.py and super/place/young_thing/time/come_right_man.py over small day"

---
+++ super/place/young_thing/time/come_right_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def way(str_arg):
+    new_hand_and_woman(str_arg)
+    print('high_week')
+
+def new_hand_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    way('bad_life_or_problem')
