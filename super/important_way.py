b06f923695e72612c58a5143db30d85c5e092864
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 13:04:59 -0500

    "rm: do super/important_way.py, super/life_or_part/case/large_woman_or_different_work/small_time_and_world.py and super/child/bad_fact/company/way_or_good_way/new_hand.py on great company"

---
+++ super/important_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing_or_person(str_arg):
+    make_important_number(str_arg)
+    print('long_man')
+
+def make_important_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing_or_person('man')
---
+++ super/life_or_part/case/large_woman_or_different_work/small_time_and_world.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    woman_or_time(str_arg)
+    print('group_and_big_eye')
+
+def woman_or_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('new_part')
