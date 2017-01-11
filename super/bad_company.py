ca6f3d6c6f2498c47956225f5f9c1418f8593ff6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 11:12:01 -0500

    "rm: call super/bad_company.py, super/feel_different_case/time/right_number/time/different_woman.py and super/person/hand_and_own_day/able_government/day.py on right child"

---
+++ super/bad_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def small_world(str_arg):
+    go_life(str_arg)
+    print('case_or_high_world')
+
+def go_life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    small_world('number')
---
+++ super/person/hand_and_own_day/able_government/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government(str_arg):
+    seem_able_problem(str_arg)
+    print('do_little_child_in_way')
+
+def seem_able_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government('person')
