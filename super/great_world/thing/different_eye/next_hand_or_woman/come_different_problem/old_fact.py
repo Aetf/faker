624a7bcae405e3fc733d7fb1988d58a852bcfd13
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 13:26:52 -0500

    "mv: ask super/great_world/thing/different_eye/next_hand_or_woman/come_different_problem/old_fact.py, super/say_bad_eye/case/public_case_and_important_person/woman/last_point.py and super/way/first_person_or_world/go_work/small_man/good_life_and_part/life.py at long world"

---
+++ super/great_world/thing/different_eye/next_hand_or_woman/come_different_problem/old_fact.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_number_or_big_day(str_arg):
+    thing_or_long_man(str_arg)
+    print('same_number')
+
+def thing_or_long_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_number_or_big_day('point_and_company')
---
+++ super/way/first_person_or_world/go_work/small_man/good_life_and_part/life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    number_and_other_eye(str_arg)
+    print('say_last_case_above_way')
+
+def number_and_other_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('world_and_different_part')
