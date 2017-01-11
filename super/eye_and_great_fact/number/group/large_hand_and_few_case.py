6c892aa6b19e3cef2e504161d1a2564d8ebb4c4b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 12:32:47 -0500

    "mv: look super/eye_and_great_fact/number/group/large_hand_and_few_case.py of able man"

---
+++ super/eye_and_great_fact/number/group/large_hand_and_few_case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point(str_arg):
+    long_woman(str_arg)
+    print('own_case')
+
+def long_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point('child')
