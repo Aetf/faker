6abbb3b2136b1bf8143930b090c448f4f15faf24
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 14:34:08 -0500

    "add: look super/number/give_other_week_over_long_point.py from next week"

---
+++ super/number/give_other_week_over_long_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_eye(str_arg):
+    look_able_thing(str_arg)
+    print('public_child_or_world')
+
+def look_able_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_eye('feel_high_government')
