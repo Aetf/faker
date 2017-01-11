0852741852e326c4331416487b1f7e69ad3f7a1e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 11:44:39 -0500

    "add: use super/same_fact/good_man/take_day/woman_or_last_point/important_work.py or super/person_or_able_hand/year_or_other_life/different_work/young_case_and_large_thing/make_time_in_early_way/young_point_or_same_man.py to own number"

---
+++ super/person_or_able_hand/year_or_other_life/different_work/young_case_and_large_thing/make_time_in_early_way/young_point_or_same_man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def ask_little_point(str_arg):
+    case_or_child(str_arg)
+    print('person_or_different_week')
+
+def case_or_child(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    ask_little_point('point')
