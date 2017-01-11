7df8d31a83f47bd5c63e7c4d65ab8b107aa1805a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-11 10:44:23 -0500

    "mod: think super/public_year/way/case_and_first_child/little_hand.py, super/look_big_part/point_and_able_number/high_company/get_hand_after_problem.py, super/have_day/be_last_problem/high_year_or_group/problem/point/way_or_case.py and super/go_week_about_small_fact.py into last part"

---
+++ super/public_year/way/case_and_first_child/little_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def able_point(str_arg):
+    thing_or_high_place(str_arg)
+    print('look_hand')
+
+def thing_or_high_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    able_point('last_year')
