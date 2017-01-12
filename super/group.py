644eaee69f89977d4a96073f56ebca89bbd01d52
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 12:34:44 -0500

    "mix: make super/group.py from small way"

---
+++ super/group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_year_and_day(str_arg)
+    print('early_life')
+
+def first_year_and_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('number')
