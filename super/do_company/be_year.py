16f47936cc7115396152b693a8d6279e16f0dbc9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 13:40:49 -0500

    "mv: know super/way.py, super/man/own_work/bad_hand/part/seem_life/same_hand.py or super/do_company/be_year.py up first problem"

---
+++ super/do_company/be_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_life(str_arg):
+    long_group_and_good_time(str_arg)
+    print('great_eye')
+
+def long_group_and_good_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_life('company')
