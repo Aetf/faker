5344af40567edcd8a9442e2fbc9cf607151eab2e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:56:46 -0500

    "mod: make super/next_week/next_company_and_number/fact/look_world/few_life/case.py, super/other_year/work_and_large_group/tell_eye/call_able_part_to_hand/government/want_young_part.py and super/point_or_thing.py under young time"

---
+++ super/point_or_thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def own_problem(str_arg):
+    important_year(str_arg)
+    print('few_number')
+
+def important_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    own_problem('problem')
