b27b02bb4c527f6ce0d8af5c33d76c7f9cfcfe4b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 12:15:50 -0500

    "rm: want super/life/last_case/own_problem.py or super/government/last_case/point_or_fact.py over new part"

---
+++ super/life/last_case/own_problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def last_child(str_arg):
+    company_or_world(str_arg)
+    print('give_next_part')
+
+def company_or_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    last_child('give_work')
