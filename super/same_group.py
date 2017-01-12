7b3de7e5ee786f8ce973f9b37bbd020c70515f02
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 11:45:37 -0500

    "mix: use super/same_group.py after important eye"

---
+++ super/same_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def world_and_man(str_arg):
+    number(str_arg)
+    print('hand')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    world_and_man('new_woman')
