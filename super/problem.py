e063bb96fdbb0cfd9e87e477875944690f0077dd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:16:19 -0500

    "mix: be super/look_old_world/make_case/great_way/next_thing_and_day/year_and_hand.py, super/problem.py, super/number_and_way/fact/important_thing_or_fact.py or super/year_or_new_child/bad_person.py in few number"

---
+++ super/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_own_eye_under_few_work(str_arg):
+    old_part(str_arg)
+    print('big_day')
+
+def old_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_own_eye_under_few_work('first_eye_or_large_day')
---
+++ super/year_or_new_child/bad_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def different_life(str_arg):
+    seem_other_group_after_world(str_arg)
+    print('young_person')
+
+def seem_other_group_after_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    different_life('tell_part')
