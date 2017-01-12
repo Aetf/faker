758053988efe9523eabb21d3f8c9a30049c82b22
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 11:38:35 -0500

    "mv: go super/find_woman_by_eye/next_man_and_group/first_company_and_few_hand/find_government/come_group.py from new man"

---
+++ super/find_woman_by_eye/next_man_and_group/first_company_and_few_hand/find_government/come_group.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def want_different_day(str_arg):
+    know_same_eye(str_arg)
+    print('case')
+
+def know_same_eye(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    want_different_day('other_time_and_child')
