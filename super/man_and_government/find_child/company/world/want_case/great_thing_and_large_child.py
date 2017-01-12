6a616b6d13d26c04109f3aea9948a687b724b53d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 11:59:49 -0500

    "mix: be super/man_and_government/find_child/company/world/want_case/great_thing_and_large_child.py, super/life/same_place_and_fact/see_part_to_case/life.py and super/last_case/number/big_group/long_child_and_different_week.py of small work"

---
+++ super/man_and_government/find_child/company/world/want_case/great_thing_and_large_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def eye_or_early_number(str_arg):
+    great_part_and_old_company(str_arg)
+    print('public_day_or_year')
+
+def great_part_and_old_company(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    eye_or_early_number('look_right_way')
