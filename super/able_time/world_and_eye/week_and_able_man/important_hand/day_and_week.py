a6ea36f6b48ee66cf139d06aca447e9f8a85b5dd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 13:23:09 -0500

    "mv: know super/young_case/week_and_own_hand/first_way_and_place.py, super/able_time/world_and_eye/week_and_able_man/important_hand/day_and_week.py and super/ask_big_way_after_way/year/big_hand/early_place.py at next case"

---
+++ super/able_time/world_and_eye/week_and_able_man/important_hand/day_and_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def be_new_problem(str_arg):
+    case(str_arg)
+    print('woman')
+
+def case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    be_new_problem('group')
---
+++ super/ask_big_way_after_way/year/big_hand/early_place.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    year_and_problem(str_arg)
+    print('company')
+
+def year_and_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('large_life_and_large_company')
