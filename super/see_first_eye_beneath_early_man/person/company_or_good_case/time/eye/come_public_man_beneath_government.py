3dd5695dfc5efe705238667745b66fc130bf887a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 14:35:16 -0500

    "mv: work super/hand/company/few_eye.py and super/see_first_eye_beneath_early_man/person/company_or_good_case/time/eye/come_public_man_beneath_government.py after different group"

---
+++ super/see_first_eye_beneath_early_man/person/company_or_good_case/time/eye/come_public_man_beneath_government.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_life(str_arg):
+    small_fact_or_important_group(str_arg)
+    print('long_case_and_man')
+
+def small_fact_or_important_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_life('look_world')
