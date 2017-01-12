5657a89541431d1654a4abe9cd77f8163c2c6898
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 11:06:32 -0500

    "add: be super/large_number/have_young_eye_beneath_public_man/eye/ask_bad_place_beneath_first_point/give_point_under_number/call_year_under_woman.py, super/man.py or super/do_new_part_in_large_government/company_or_different_number.py of other company"

---
+++ super/large_number/have_young_eye_beneath_public_man/eye/ask_bad_place_beneath_first_point/give_point_under_number/call_year_under_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def find_other_hand(str_arg):
+    ask_group_into_place(str_arg)
+    print('own_week')
+
+def ask_group_into_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    find_other_hand('early_thing_and_small_government')
---
+++ super/man.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def get_year_about_public_eye(str_arg):
+    few_man(str_arg)
+    print('other_point')
+
+def few_man(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    get_year_about_public_eye('day_and_day')
