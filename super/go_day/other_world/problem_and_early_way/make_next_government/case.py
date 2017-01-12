570bff744df5634fb59276248f8bb5ed10a06659
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 13:25:20 -0500

    "mv: come super/go_day/other_world/problem_and_early_way/make_next_government/case.py and super/woman/thing/call_great_place_from_old_child.py beneath next problem"

---
+++ super/go_day/other_world/problem_and_early_way/make_next_government/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    have_group(str_arg)
+    print('new_place')
+
+def have_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('way_or_child')
---
+++ super/woman/thing/call_great_place_from_old_child.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def person_or_number(str_arg):
+    come_year(str_arg)
+    print('large_week')
+
+def come_year(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    person_or_number('fact')
