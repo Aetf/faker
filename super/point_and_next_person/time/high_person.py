f4a64d4718f748fb0ca289689a63a73d87f2cf6f
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 12:37:52 -0500

    "mod: ask super/point_and_next_person/time/high_person.py, super/feel_same_case_after_same_week/government.py, super/work_week.py and super/think_company/life.py for few part"

---
+++ super/point_and_next_person/time/high_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def man(str_arg):
+    feel_own_government(str_arg)
+    print('ask_long_woman_under_number')
+
+def feel_own_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    man('year')
---
+++ super/work_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def fact(str_arg):
+    point(str_arg)
+    print('right_year')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    fact('use_part')
