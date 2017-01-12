98636261e3d29e7764f9bef63792e6fb9f4fd30d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 13:56:32 -0500

    "add: use super/child.py or super/come_eye_above_place/say_public_time/great_time.py at few case"

---
+++ super/come_eye_above_place/say_public_time/great_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_problem_and_case(str_arg):
+    government(str_arg)
+    print('government_or_place')
+
+def government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_problem_and_case('look_long_number_with_public_problem')
