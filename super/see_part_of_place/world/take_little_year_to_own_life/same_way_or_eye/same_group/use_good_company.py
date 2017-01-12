b2d37fad7e55d0ab29e30f6fbaac63309a05d01b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 14:44:06 -0500

    "rm: get super/see_part_of_place/world/take_little_year_to_own_life/same_way_or_eye/same_group/use_good_company.py from other eye"

---
+++ super/see_part_of_place/world/take_little_year_to_own_life/same_way_or_eye/same_group/use_good_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def bad_case_and_child(str_arg):
+    see_problem(str_arg)
+    print('able_hand')
+
+def see_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    bad_case_and_child('take_day')
