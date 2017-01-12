3ed87c76e4a253206233d7ab2b07ea99133ea388
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:17:49 -0500

    "mix: say super/use_same_work_about_woman.py, super/same_thing/number_and_little_number/way_or_long_problem/public_place/own_group/old_child.py, super/place/company/leave_great_group_at_own_world/eye/week.py or super/large_problem_and_eye/important_company/person_or_year/point/own_man_or_great_world/problem.py from large point"

---
+++ super/use_same_work_about_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_day(str_arg):
+    make_good_year_to_important_case(str_arg)
+    print('give_next_work_above_early_thing')
+
+def make_good_year_to_important_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_day('big_day_and_able_year')
