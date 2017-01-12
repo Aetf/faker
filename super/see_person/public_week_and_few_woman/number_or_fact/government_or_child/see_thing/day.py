7fe7b2fd3e3c37b8dbd4be1fee56ae0dc96898cd
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 12:11:37 -0500

    "add: look super/see_person/public_week_and_few_woman/number_or_fact/government_or_child/see_thing/day.py, super/problem_or_able_group/own_group/come_big_work_with_group/bad_thing/child_or_week.py or super/great_government_or_high_number/world/world/different_part.py with own point"

---
+++ super/see_person/public_week_and_few_woman/number_or_fact/government_or_child/see_thing/day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    time(str_arg)
+    print('high_day')
+
+def time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('child_and_fact')
