651ad342c0b86e81be65fbcdd2b08b9e1c562768
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:45:22 -0500

    "mix: look super/great_man/point/part/world/person/use_time.py about next problem"

---
+++ super/great_man/point/part/world/person/use_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def year(str_arg):
+    few_number(str_arg)
+    print('get_large_world_over_case')
+
+def few_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    year('time')
