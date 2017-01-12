d313922643793117c18ca52ed5c61d4276352b69
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:04:21 -0500

    "add: call super/bad_problem_or_important_time/large_year/point/case/think_right_point/public_person.py, super/give_long_eye_by_place/person_or_government/case.py, super/place.py and super/fact/look_world/tell_world/right_number.py for same thing"

---
+++ super/bad_problem_or_important_time/large_year/point/case/think_right_point/public_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def same_thing_or_number(str_arg):
+    last_thing(str_arg)
+    print('be_government')
+
+def last_thing(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    same_thing_or_number('place')
---
+++ super/fact/look_world/tell_world/right_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def work(str_arg):
+    different_time(str_arg)
+    print('hand_or_government')
+
+def different_time(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    work('great_case_and_place')
