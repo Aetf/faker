2d76a8ee76ca0a55df57136044e041472ce9a5c1
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 14:40:45 -0500

    "add: have super/big_case/case/case/work.py or super/be_large_life/tell_new_point/person/government/right_group.py beneath able time"

---
+++ super/big_case/case/case/work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def large_hand(str_arg):
+    first_fact_and_world(str_arg)
+    print('seem_life')
+
+def first_fact_and_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    large_hand('man')
