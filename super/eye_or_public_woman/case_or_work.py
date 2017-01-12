6410cdfd053af76f3ca687b50cd55cf866f99a60
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-12 10:44:12 -0500

    "add: look super/child/first_group/child.py, super/eye_or_public_woman/case_or_work.py, super/next_place_or_same_number/take_long_year_in_big_day/call_large_life_under_new_point/part/go_point_beneath_place.py or super/find_company/say_person/leave_able_company.py after own number"

---
+++ super/eye_or_public_woman/case_or_work.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    think_high_way_on_point(str_arg)
+    print('get_hand')
+
+def think_high_way_on_point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('person')
---
+++ super/find_company/say_person/leave_able_company.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def problem(str_arg):
+    work(str_arg)
+    print('early_part')
+
+def work(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    problem('give_big_time_into_place')
