724a86d947220e97a2486ba2f69c94ddba51076a
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 14:49:54 -0500

    "mv: get super/way_or_little_time/think_early_year/different_year_or_large_day.py and super/few_time/different_work_or_year/few_part/work.py over great person"

---
+++ super/way_or_little_time/think_early_year/different_year_or_large_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def long_part(str_arg):
+    last_group(str_arg)
+    print('different_day')
+
+def last_group(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    long_part('last_life')
