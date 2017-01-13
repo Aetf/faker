1cf668ca688f2c8d29233f25b15959350ca1e098
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 13:06:13 -0500

    "add: want super/thing.py beneath new hand"

---
+++ super/thing.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def high_person(str_arg):
+    good_problem(str_arg)
+    print('know_young_number_on_little_world')
+
+def good_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    high_person('know_woman')
