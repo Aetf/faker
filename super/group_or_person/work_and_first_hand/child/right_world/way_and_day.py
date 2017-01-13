8559007b7e07abcd1e1f2c16a279dfee14811300
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 10:40:53 -0500

    "add: tell super/company.py and super/group_or_person/work_and_first_hand/child/right_world/way_and_day.py after important group"

---
+++ super/group_or_person/work_and_first_hand/child/right_world/way_and_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def point(str_arg):
+    right_part(str_arg)
+    print('few_case')
+
+def right_part(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    point('hand')
