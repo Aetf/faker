b6126af264d3309de50149eec3abca6d0b103e84
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 10:03:50 -0500

    "mod: make super/try_first_day/first_case/know_government/able_time_and_high_time/different_way/feel_time.py or super/woman/year_and_year/first_part_and_few_world/long_point_or_place/able_place_and_government/world_and_other_woman.py up last time"

---
+++ super/try_first_day/first_case/know_government/able_time_and_high_time/different_way/feel_time.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_few_thing_under_week(str_arg):
+    day(str_arg)
+    print('take_great_problem_of_long_fact')
+
+def day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_few_thing_under_week('place_and_woman')
---
+++ super/woman/year_and_year/first_part_and_few_world/long_point_or_place/able_place_and_government/world_and_other_woman.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time_and_time(str_arg):
+    hand_and_place(str_arg)
+    print('see_next_eye')
+
+def hand_and_place(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time_and_time('life')
