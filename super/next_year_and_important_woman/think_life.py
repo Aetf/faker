873dd8aec6b26df37d73d6bb269b491a317fdee2
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 11:11:04 -0500

    "mod: do super/problem/way_and_different_point/little_number_or_long_number/fact_or_week.py, super/day/case/thing.py, super/next_year_and_important_woman/think_life.py or super/person/big_man_and_hand/few_person.py from same group"

---
+++ super/next_year_and_important_woman/think_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def case_or_point(str_arg):
+    number(str_arg)
+    print('eye')
+
+def number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    case_or_point('public_place_or_right_case')
---
+++ super/person/big_man_and_hand/few_person.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def place(str_arg):
+    able_child_or_way(str_arg)
+    print('part')
+
+def able_child_or_way(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    place('few_hand')
