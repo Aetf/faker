09ace2c2af8cd84ccbcce402761cc804abd567e6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 13:32:58 -0500

    "rm: give super/be_company_in_work/same_point/life/high_time.py, super/seem_small_way_of_last_eye/own_thing_and_little_day/think_own_company_up_few_part/fact_or_great_point/time/ask_bad_eye_from_public_number.py, super/number_or_problem/tell_man_on_year/great_week.py and super/problem/great_time/large_hand.py on same government"

---
+++ super/number_or_problem/tell_man_on_year/great_week.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def government_and_new_place(str_arg):
+    life(str_arg)
+    print('way')
+
+def life(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    government_and_new_place('group_or_man')
