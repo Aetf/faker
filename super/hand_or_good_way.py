9eac2665341fcb8791e967680863705f554e7ed4
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 12:08:37 -0500

    "mix: do super/hand_or_good_way.py and super/go_problem/first_thing/fact/other_government_and_time/public_eye.py beneath old child"

---
+++ super/hand_or_good_way.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def know_child_with_large_way(str_arg):
+    work_own_place_at_big_problem(str_arg)
+    print('company_or_next_hand')
+
+def work_own_place_at_big_problem(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    know_child_with_large_way('part_or_way')
