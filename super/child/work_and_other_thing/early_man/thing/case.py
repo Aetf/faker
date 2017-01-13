6f31eebf7fd33cf10baafe9e30ba37f2533d2ee2
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 14:12:20 -0500

    "rm: do super/child/work_and_other_thing/early_man/thing/case.py, super/way/important_place/big_number/problem.py, super/few_problem.py or super/early_number/public_man_and_important_government.py into different group"

---
+++ super/child/work_and_other_thing/early_man/thing/case.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def great_man(str_arg):
+    point(str_arg)
+    print('think_life')
+
+def point(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    great_man('world')
---
+++ super/way/important_place/big_number/problem.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def thing(str_arg):
+    first_problem_or_high_government(str_arg)
+    print('woman')
+
+def first_problem_or_high_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    thing('think_man')
