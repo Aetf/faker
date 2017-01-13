df60fcf3eab47e69f088adc5a8073d68425093e6
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 14:44:07 -0500

    "mv: get super/other_day/leave_day/work/old_day.py over important work"

---
+++ super/other_day/leave_day/work/old_day.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def other_time_and_public_time(str_arg):
+    old_number(str_arg)
+    print('able_life_or_fact')
+
+def old_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    other_time_and_public_time('old_company')
