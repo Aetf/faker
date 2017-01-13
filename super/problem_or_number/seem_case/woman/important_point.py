0e1b2de8013e87f7bff94374cbae847bcc8892a9
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 11:55:15 -0500

    "rm: get super/way/do_right_way_over_large_government.py, super/week_and_child/problem/bad_government.py, super/problem_or_number/seem_case/woman/important_point.py and super/tell_eye/know_public_group.py with old fact"

---
+++ super/problem_or_number/seem_case/woman/important_point.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def few_number(str_arg):
+    right_world_and_woman(str_arg)
+    print('few_thing')
+
+def right_world_and_woman(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    few_number('long_point')
