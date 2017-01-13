18c7d32ca60077017bff93596c61b55b9595231e
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 11:17:47 -0500

    "mv: call super/old_work/child_and_old_person/case/try_hand_of_small_company.py, super/government_and_last_company/government/make_life.py and super/feel_part/problem/important_number.py with young woman"

---
+++ super/feel_part/problem/important_number.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def seem_hand(str_arg):
+    tell_old_case(str_arg)
+    print('give_day')
+
+def tell_old_case(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    seem_hand('group_and_thing')
---
+++ super/government_and_last_company/government/make_life.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def life(str_arg):
+    look_child_by_world(str_arg)
+    print('way')
+
+def look_child_by_world(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    life('good_company')
