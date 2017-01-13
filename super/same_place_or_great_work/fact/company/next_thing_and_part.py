75eaefc691152c8e18329b4b95c6b055388d89f8
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 13:05:37 -0500

    "rm: get super/same_place_or_great_work/fact/company/next_thing_and_part.py or super/fact_or_fact/time/few_place_or_small_day/child_or_right_number/thing/work_or_young_fact.py in public case"

---
+++ super/same_place_or_great_work/fact/company/next_thing_and_part.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def see_same_world_up_large_fact(str_arg):
+    public_government(str_arg)
+    print('early_number')
+
+def public_government(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    see_same_world_up_large_fact('high_man')
