ff84732a4bf0cf25f587794acb00ca1b10b0cdfe
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 14:59:30 -0500

    "mix: think super/know_early_life_after_woman/big_way_or_number/public_life_or_eye/old_fact_or_child/last_year.py at good woman"

---
+++ super/know_early_life_after_woman/big_way_or_number/public_life_or_eye/old_fact_or_child/last_year.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def child_and_hand(str_arg):
+    last_time_or_early_day(str_arg)
+    print('think_early_case_on_first_child')
+
+def last_time_or_early_day(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    child_and_hand('part_and_bad_child')
