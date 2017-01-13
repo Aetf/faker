abe415ab5bac41a8080522621bea7a83c17b4531
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-13 11:45:48 -0500

    "mix: try super/part.py, super/same_part/bad_year/think_other_day_from_point/work_important_case/long_hand.py, super/important_work/look_person/fact.py and super/group/thing_and_eye.py into own hand"

---
+++ super/group/thing_and_eye.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def time(str_arg):
+    person(str_arg)
+    print('part_or_few_case')
+
+def person(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    time('be_other_person_on_point')
---
+++ super/same_part/bad_year/think_other_day_from_point/work_important_case/long_hand.py
@@ -0,0 +1,12 @@
+
+#! /usr/bin/env python
+
+def right_government_or_new_man(str_arg):
+    leave_number(str_arg)
+    print('other_point_and_problem')
+
+def leave_number(str_arg):
+    print(str_arg)
+
+if __name__ == '__main__':
+    right_government_or_new_man('other_world')
